14cc9437620d6aa2d833c2774aa528f140eeea7e
Sihong Park <pozt1234@gmail.com>
2018-11-08 20:49:42 +0900

    refactor: Lotto에서 유효성 체크하도록 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,10 +5,31 @@ import java.util.List;
 
 public class Lotto {
 
+	public static final int MIN_NUMBER = 1;
+	public static final int MAX_NUMBER = 45;
+	public static final int NUMBER_COUNT = 6;
+
 	private List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
+		if(!isValid(numbers)) {
+			throw new IllegalArgumentException(
+					String.format("%d ~ %d 사이의 중복되지 않은 숫자 %d개만 가능합니다.",
+							MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT));
+		}
 		this.numbers = numbers;
+	}
+
+	private boolean isValid(List<Integer> numbers) {
+		int count = (int) numbers.stream()
+				.distinct()
+				.filter(number -> hasNumberInRange(number))
+				.count();
+		return count == NUMBER_COUNT;
+	}
+
+	private boolean hasNumberInRange(Integer number) {
+		return MIN_NUMBER <= number && number <= MAX_NUMBER;
 	}
 
 	public int match(Lotto lotto) {
--- src/main/java/lotto/utils/LottoNumberGenerator.java
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -5,12 +5,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import lotto.domain.Lotto;
 
 public class LottoNumberGenerator {
-
-	public static final int LOTTO_MIN_NUMBER = 1;
-	public static final int LOTTO_MAX_NUMBER = 45;
-	public static final int PICK_COUNT = 6;
 
 	private List<Integer> numbers;
 
@@ -19,7 +16,7 @@ public class LottoNumberGenerator {
 	}
 
 	private List<Integer> createNumbers() {
-		return IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+		return IntStream.rangeClosed(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER)
 				.boxed()
 				.collect(Collectors.toList());
 	}
@@ -27,39 +24,18 @@ public class LottoNumberGenerator {
 	public List<Integer> pick() {
 		Collections.shuffle(numbers);
 		return numbers.stream()
-				.limit(PICK_COUNT)
+				.limit(Lotto.NUMBER_COUNT)
 				.collect(Collectors.toList());
 	}
 
 	public List<Integer> pick(String number) {
-		return validate(parseNumber(number));
+		return parseNumber(number);
 	}
 
 	private List<Integer> parseNumber(String number) {
 		return Arrays.stream(number.split("[\\s,]+"))
 				.map(Integer::valueOf)
 				.collect(Collectors.toList());
-	}
-
-	private List<Integer> validate(List<Integer> numbers) {
-		if(!isCorrectNumbers(numbers)) {
-			throw new IllegalArgumentException(
-					String.format("%d ~ %d 사이의 중복되지 않은 숫자 %d개만 가능합니다.",
-							LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, PICK_COUNT));
-		}
-		return numbers;
-	}
-
-	private boolean isCorrectNumbers(List<Integer> numbers) {
-		int count = (int) numbers.stream()
-				.distinct()
-				.filter(number -> hasNumberInRange(number))
-				.count();
-		return count == PICK_COUNT;
-	}
-
-	private boolean hasNumberInRange(Integer number) {
-		return LOTTO_MIN_NUMBER <= number && number <= LOTTO_MAX_NUMBER;
 	}
 
 }
