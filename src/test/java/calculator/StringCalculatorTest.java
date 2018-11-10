c05b2835074d95dbb308a9aa3daa5ba8b83a375c
ninezero90hy <ninezero90hy@gmail.com>
2018-11-10 19:03:26 +0900

    문자열 구현 초기화

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,11 +1,7 @@
 package calculator;
 
 import org.junit.FixMethodOrder;
-import org.junit.Test;
 import org.junit.runners.MethodSorters;
-
-import static calculator.StringCalculator.add;
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * TODO: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
@@ -15,45 +11,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SuppressWarnings({"NonAsciiCharacters", "SpellCheckingInspection"})
 @FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
 public class StringCalculatorTest {
-
-    @Test
-    public void 문자열이_NULL인_경우() {
-        assertThat(add(null))
-                .as("문자열이 NULL인 경우 0이 반환되야한다")
-                .isEqualTo(0);
-    }
-
-    @Test
-    public void 문자열이_공백인_경우() {
-        assertThat(add(""))
-                .as("문자열이 공백인 경우 0이 반환되야한다")
-                .isEqualTo(0);
-    }
-
-    @Test
-    public void 콤마로_구분하는_문자열_추가() {
-        assertThat(add("1,2"))
-                .as("합계가 3이 아니면 실패")
-                .isEqualTo(3);
-    }
-
-    @Test
-    public void 콤마와_콜론이_있는_문자열_추가() {
-        assertThat(add("1,2:3"))
-                .as("합계가 6이 아니면 실패")
-                .isEqualTo(6);
-    }
-
-    @Test
-    public void 문자열에서_커스텀_구분자를_사용한_경우() {
-        assertThat(add("//;\n1;2;3"))
-                .as("합계가 6이 아니면 실패")
-                .isEqualTo(6);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void 문자열에_구분자를_제외하고_숫자가_아닌_값이_있다면_런타임_예외가_발생해야한다() {
-        add("1;@;3");
-    }
 
 }
