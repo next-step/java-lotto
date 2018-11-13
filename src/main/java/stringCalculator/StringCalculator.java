cf1ba040a6513ed4507c7405a18128de93bb947d
dotorify <dotorify@gmail.com>
2018-11-13 22:07:52 +0900

    <refactor>: 문자열덧셈계산기 - 기능별 메소드 분리

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -1,5 +1,9 @@
 package stringCalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public static int add(String source) {
@@ -7,18 +11,26 @@ public class StringCalculator {
             return 0;
         }
 
-        return sum(split(source));
+        return sum(convertToIntegers(split(source)));
     }
 
-    private static String[] split(String source) {
-        return source.split(",");
+    private static List<String> split(String source) {
+        return Arrays.asList(source.split(","));
     }
 
-    private static int sum(String[] values) {
+    private static int sum(List<Integer> values) {
         int result = 0;
-        for (String value : values) {
-            result += Integer.parseInt(value);
+        for (int value : values) {
+            result += value;
         }
         return result;
+    }
+
+    private static List<Integer> convertToIntegers(List<String> values) {
+        List<Integer> results = new ArrayList<>();
+        for (String value : values) {
+            results.add(Integer.parseInt(value));
+        }
+        return results;
     }
 }
