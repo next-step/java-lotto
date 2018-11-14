efe9d7eb938bdb2cf00d6011b336d674ecdc59b5
dotorify <dotorify@gmail.com>
2018-11-15 00:58:34 +0900

    <refactor>: 로또1단계 - 로또 번호 생성 리팩토링

--- src/main/java/lotto/util/LotteryNumberGenerator.java
+++ src/main/java/lotto/util/LotteryNumberGenerator.java
@@ -1,12 +1,22 @@
 package lotto.util;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class LotteryNumberGenerator {
+    private static int MAX_NUMBER = 45;
+    private static int QUANTITY = 6;
 
     public static Set<Integer> generate() {
-        return new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<Integer> numbers = getNumbers();
+        Collections.shuffle(numbers);
+        return new HashSet<>(numbers.subList(0, QUANTITY));
+    }
+
+    private static List<Integer> getNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= MAX_NUMBER; i++) {
+            numbers.add(i);
+        }
+        return numbers;
     }
 }
