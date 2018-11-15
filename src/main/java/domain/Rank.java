3ea3d225cbb2cc3ecbffdf4166a1d4763d990512
금성현 <sunghyun.keum@woowahan.com>
2018-11-15 10:08:29 +0900

    돈을 나타내는 숫자는 _로 천단위 구분을 할 수 있다

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    FIRST                           (6, 2000000000),
-    SECOND                          (5, 1500000),
-    THIRD                           (4, 50000),
-    FOURTH                          (3, 5000),
+    FIRST                           (6, 2_000_000_000),
+    SECOND                          (5, 1_500_000),
+    THIRD                           (4, 5_0000),
+    FOURTH                          (3, 5_000),
     ETC                             (0, 0);
 
     private int matchCount;
