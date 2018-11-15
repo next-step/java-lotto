097f1dd6f516200a65ceb96a91860c12dfe36e8c
serena <noriming2@gmail.com>
2018-11-16 05:38:50 +0900

    add view

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import lotto.Money;
+import lotto.WinningNumber;
+
+import java.util.Scanner;
+
 public class InputView {
+    public static Money getPrice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        int price = scanner.nextInt();
+        return new Money(price);
+    }
+
+    public static WinningNumber getWinningNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String numbers = scanner.nextLine();
+        return new WinningNumber(numbers);
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,30 @@
 package lotto.view;
 
+import lotto.Lotto;
+import lotto.LottoComparator;
+
+import java.util.List;
+
 public class OutputView {
+    public static void printAmount(int quantity) {
+        System.out.println(quantity + "개를 구매했습니다.");
+    }
+
+    public static void printPurchaseLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
+        System.out.println();
+    }
+
+    public static void printCompareResult(LottoComparator lottoComparator) {
+        System.out.println();
+        System.out.println("당첨통계");
+        System.out.println("---------");
+        System.out.println(lottoComparator);
+    }
+
+    public static void printEaningRate(double earningRate) {
+        System.out.println("총 수익률은" + earningRate + "입니다.");
+    }
 }
