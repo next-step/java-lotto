d846ca500d1afa42c3d2eca1a4d8889c64f208f5
serena <noriming2@gmail.com>
2018-11-16 05:40:54 +0900

    add 로또번호생성기

--- src/main/java/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -2,15 +2,14 @@ package lotto;
 
 import java.util.*;
 
-public class LottoGenerator {
-    public static Lotto generator() {
+public class LottoNumberGenerator {
+    public static Set<Integer> generator() {
         Set<Integer> numbers = new TreeSet<>();
         while (numbers.size() < 6) {
             numbers.add(getRandomNumber());
         }
-        return new Lotto(numbers);
+        return numbers;
     }
-
     private static int getRandomNumber() {
         Random random = new Random();
         return random.nextInt(45) + 1;
