429e47687de0095fa2a4a7875a291220abe6b55d
dotorify <dotorify@gmail.com>
2018-11-18 01:26:28 +0900

    <refactor>: 로또 2단계 - WinningLotto 생성자 리팩토링

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,13 +1,34 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class WinningLotto {
-    private List<Integer> numbers;
+    private Set<Integer> numbers;
     private int bonusNumber;
 
-    public WinningLotto(List<Integer> numbers, int bonusNumber) {
+    private WinningLotto(Set<Integer> numbers, int bonusNumber) {
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public static WinningLotto from(List<Integer> numbers, int bonusNumber) {
+        return new WinningLotto(new HashSet<>(numbers), bonusNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningLotto that = (WinningLotto) o;
+        return bonusNumber == that.bonusNumber &&
+                Objects.equals(numbers, that.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers, bonusNumber);
     }
 }
--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -1,14 +1,22 @@
 package lotto.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class WinningLottoTest {
+    private WinningLotto winningLotto;
+
+    @Before
+    public void setup() {
+        winningLotto = WinningLotto.from(Arrays.asList(1, 2, 3, 4, 5), 7);
+    }
 
     @Test
     public void 생성() {
-        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5), 7);
+        assertThat(winningLotto).isEqualTo(WinningLotto.from(Arrays.asList(1, 2, 3, 4, 5), 7));
     }
 
 }
