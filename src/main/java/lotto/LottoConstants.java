1034f41b9ceadf8b7c5d13fabbe1a46d561e6e0c
dotorify <dotorify@gmail.com>
2018-11-18 21:32:14 +0900

    <fix>: 로또 2단계 - 1000단위 구분자 추가

--- src/main/java/lotto/LottoConstants.java
+++ src/main/java/lotto/LottoConstants.java
@@ -4,5 +4,5 @@ public class LottoConstants {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
     public static final int QUANTITY = 6;
-    public static final int PRICE_PER_ONE = 1000;
+    public static final int PRICE_PER_ONE = 1_000;
 }
