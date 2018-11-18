a4776cdd34fcf5c5b49989e1eb1807e9c168cee2
dotorify <dotorify@gmail.com>
2018-11-18 20:18:03 +0900

    <fix>: 로또 2단계 - Rank Enum 1000단위 구분자 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,11 +1,11 @@
 package lotto.model;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     MISS(0, 0);
 
     private static final int MIN_PRIZE_COUNT = 3;
