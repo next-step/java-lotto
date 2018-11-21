8073e0a11aaf439f11988cb0746d7dd475dc36a7
dotorify <dotorify@gmail.com>
2018-11-22 01:23:38 +0900

    <feature>: 로또 4단계 - 웹UI 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -3,20 +3,13 @@ package lotto.view;
 import lotto.model.Lotto;
 import lotto.model.Rank;
 import lotto.model.Statistics;
+import lotto.util.MessageConverter;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class ResultView {
-
-    public static void printBoughtHistory(List<Lotto> manualLottos, List<Lotto> autoLottos) {
-        System.out.println("수동으로 " + manualLottos.size() + "장, 자동으로 " + autoLottos.size() + "개를 구매했습니다.");
-
-        Stream.concat(manualLottos.stream(), autoLottos.stream())
-                .forEach(lotto -> System.out.println(lotto.toString()));
-    }
 
     public static void printBoughtHistory(List<Lotto> lottos, int countOfManual) {
         System.out.println("수동으로 " + countOfManual + "장, 자동으로 " + (lottos.size() - countOfManual) + "개를 구매했습니다.");
@@ -32,7 +25,7 @@ public class ResultView {
         countOfRank.keySet().stream()
             .sorted(Comparator.reverseOrder())
             .filter(rank -> rank.getCountOfMatch() != 0)
-            .map(rank -> convertToMatchMessage(rank, countOfRank.get(rank)))
+            .map(rank -> MessageConverter.convertToMatchMessage(rank, countOfRank.get(rank)))
             .forEach(System.out::println);
     }
 
@@ -43,13 +36,5 @@ public class ResultView {
         if (returnsOfInvestment < 1) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
-    }
-
-    private static String convertToMatchMessage(Rank rank, int countOfLotto) {
-        if (rank == Rank.SECOND) {
-            return rank.getCountOfMatch() + "개 일치, 보너스 볼 일치(" + rank.getWinningMoney() + "원)- " + countOfLotto + "개";
-        }
-
-        return rank.getCountOfMatch() + "개 일치 (" + rank.getWinningMoney() + "원)- " + countOfLotto + "개";
     }
 }
