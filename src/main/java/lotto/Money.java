8d085de3ccdbaa10a20f029588cf28fca56fee9f
serena <noriming2@gmail.com>
2018-11-26 11:40:34 +0900

    update 수익률계산 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,25 +1,25 @@
 package lotto;
 
 public class Money {
-    private static final int DEFAULT_PRICE = 1000;
+    private static final int DEFAULT_PRICE = 1_000;
     private int price;
 
     public Money(int price) {
-        this.price = price;
-        if (isCorrect()) {
+        if (isCorrect(price)) {
             throw new IllegalArgumentException(DEFAULT_PRICE + "원 단위로 입력할 수 있습니다.");
         }
+        this.price = price;
     }
-
-    public int getPrice() {
-        return this.price;
-    }
-
+    
     public int getQuntity() {
         return this.price / DEFAULT_PRICE;
     }
 
-    private boolean isCorrect() {
+    public double earningRate(double earning) {
+        return earning / this.price;
+    }
+
+    private boolean isCorrect(int price) {
         return 0 < (price % DEFAULT_PRICE);
     }
 }
