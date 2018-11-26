e08aab51e3730e4af6b0023175489d02d4e3a026
serena <noriming2@gmail.com>
2018-11-26 11:43:05 +0900

    update match 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -1,29 +1,41 @@
 package lotto;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class WinningNumber {
     private static final int MINIMUM_VALUE = 6;
-    private Set<Integer> winnings;
+    private Set<Integer> winningLotto;
 
     public WinningNumber(String data) {
-        winnings = new TreeSet<>();
-        toNumbersSet(data);
+        winningLotto = toNumbersSet(data);
     }
 
-    public Set<Integer> getWinningNumbers() {
+    public Map<LottoType, Integer> match(List<Lotto> purchasLottos) {
+        Map<LottoType, Integer> winnings = new HashMap<>();
+        for (Lotto lotto : purchasLottos) {
+            lotto.countbyComparingNumbers(winningLotto);
+            increaseWinningCount(winnings, lotto);
+        }
         return winnings;
     }
 
-    private void toNumbersSet(String data) {
+    private void increaseWinningCount(Map<LottoType, Integer> winnings, Lotto lotto) {
+        if (lotto.isWinning()) {
+            LottoType lottoType = lotto.findLottoType();
+            winnings.compute(lottoType, (key, winningCount) -> winningCount == null ? 1 : winningCount + 1);
+        }
+    }
+
+    private Set<Integer> toNumbersSet(String data) {
+        Set<Integer> numberSet = new HashSet<>();
         String[] datas = splitData(data);
         if (checkMinimum(datas))
             throw new IllegalArgumentException(MINIMUM_VALUE + "자리를 입력 하셔야 합니다.");
 
         for (String number : datas) {
-            winnings.add(Integer.parseInt(number));
+            numberSet.add(Integer.parseInt(number));
         }
+        return numberSet;
     }
 
     private boolean checkMinimum(String[] datas) {
