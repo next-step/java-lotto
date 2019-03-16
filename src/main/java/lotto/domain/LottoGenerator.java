2a370dc52da77d0d26b9bb85d4640f4f2ce2566f
devson <sonyc5720@gmail.com>
2019-03-16 15:06:21 +0900

    LottoGenerator: 로또 번호 생성기능 구현

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,49 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
+    private LottoGenerator() {
+    }
+
+    public static List<Integer> generate() {
+        return getSortedLottoNumbers();
+    }
+
+    private static List<Integer> getSortedLottoNumbers() {
+        List<Integer> lottoNumbers = getUnsortedLottoNumbers();
+        sort(lottoNumbers);
+        return lottoNumbers;
+    }
+
+    private static void sort(List<Integer> lottoNumbers) {
+        lottoNumbers.sort(Comparator.naturalOrder());
+    }
+
+
+    private static List<Integer> getUnsortedLottoNumbers() {
+        return getShuffledAllLottoNumbers().subList(0, NUMBER_OF_LOTTO_NUMBERS);
+    }
+
+    static List<Integer> getAllLottoNumbers() {
+        return IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> getShuffledAllLottoNumbers() {
+        List<Integer> allLottoNumbers = getAllLottoNumbers();
+        Collections.shuffle(allLottoNumbers);
+
+        return allLottoNumbers;
+    }
+
+}
