656374042df80c911ce8f32fa6009d2068cc63ae
devson <sonyc5720@gmail.com>
2019-03-16 20:24:18 +0900

    당첨액 필드 추가

--- src/main/java/lotto/enums/LottoRank.java
+++ src/main/java/lotto/enums/LottoRank.java
@@ -1,18 +1,24 @@
 package lotto.enums;
 
 public enum LottoRank {
-    FIRST(6),
-    SECOND(5),
-    THIRD(4),
-    FOURTH(3);
+    FIRST(6, 5_000),
+    SECOND(5, 50_000),
+    THIRD(4, 1_500_000),
+    FOURTH(3, 2_000_000_000);
 
     private int matchCount;
+    private int prizeMoney;
 
-    LottoRank(int matchCount) {
+    LottoRank(int matchCount, int prizeMoney) {
         this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
     }
 
     public int getMatchCount() {
         return matchCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
     }
 }
