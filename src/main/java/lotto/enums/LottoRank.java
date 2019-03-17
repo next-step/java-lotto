34f5add27822058b978794d7552af7f3fd1a81b6
devson <sonyc5720@gmail.com>
2019-03-18 08:16:22 +0900

    등수 5등까지로 확장

--- src/main/java/lotto/enums/LottoRank.java
+++ src/main/java/lotto/enums/LottoRank.java
@@ -2,9 +2,10 @@ package lotto.enums;
 
 public enum LottoRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000);
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
 
     private int matchCount;
     private long prizeMoney;
