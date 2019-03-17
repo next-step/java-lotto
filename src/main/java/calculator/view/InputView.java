934522e90b4e26edbab932e8ba162ea4e5fb156c
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-03-17 22:16:50 +0900

    fix : BufferdReader -> Scanner 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,22 +1,11 @@
 package calculator.view;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class InputView {
 
     public static String input() {
-//        Scanner scanner = new Scanner(System.in);
-//        return scanner.next();
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String expression="";
-        try {
-            expression = bufferedReader.readLine();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-        return expression;
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
