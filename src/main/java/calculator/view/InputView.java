b46d3bfa0f5855d202c1da9c27d05dae1a1c4289
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-03-18 08:04:52 +0900

    fix : 입력값 unescape 처리 ( custom구분자 인식 위함 )

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,11 +1,13 @@
 package calculator.view;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import java.util.Scanner;
 
 public class InputView {
 
     public static String input() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        return StringEscapeUtils.unescapeJava(scanner.nextLine());
     }
 }
