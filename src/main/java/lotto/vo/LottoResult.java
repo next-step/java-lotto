19abec4b49430a42f1d7d31e4de7c924ce4f0c8f
devson <sonyc5720@gmail.com>
2019-03-17 13:30:45 +0900

    1. 로또순위 필드값을 로또순위를 담는 일급컬렉션(LottoWinResult)으로 변경 2. 기존 LottoChecker에서 계산했던 총 당첨금액, 총 수익률에 대한 책임을 LottoResult으로 위임
    https://github.com/next-step/java-lotto/pull/92/files/0b743b226e836860e08b92bc94eae73f277191e7#r266203063

--- src/main/java/lotto/vo/LottoResult.java
+++ src/main/java/lotto/vo/LottoResult.java
@@ -1,33 +1,60 @@
 package lotto.vo;
 
+import lotto.domain.LottoMachine;
 import lotto.enums.LottoRank;
 
+import java.util.Arrays;
+
 public class LottoResult {
-    private final long first;
-    private final long second;
-    private final long third;
-    private final long fourth;
+    private final LottoWinResult lottoWinResult;
+    private final long cost;
 
     public LottoResult(long[] matchCounts) {
-        this.first = matchCounts[LottoRank.FIRST.getMatchCount()];
-        this.second = matchCounts[LottoRank.SECOND.getMatchCount()];
-        this.third = matchCounts[LottoRank.THIRD.getMatchCount()];
-        this.fourth = matchCounts[LottoRank.FOURTH.getMatchCount()];
+        this.lottoWinResult = new LottoWinResult(matchCounts);
+        this.cost = extractCost(matchCounts);
+    }
+
+    private long extractCost(long[] matchCounts) {
+        return LottoMachine.PRICE_OF_LOTTO * extractNumberOfLottos(matchCounts);
+    }
+
+    private long extractNumberOfLottos(long[] matchCounts) {
+        return Arrays.stream(matchCounts).sum();
     }
 
     public long getFirst() {
-        return first;
+        return lottoWinResult.getFirst();
     }
 
     public long getSecond() {
-        return second;
+        return lottoWinResult.getSecond();
     }
 
     public long getThird() {
-        return third;
+        return lottoWinResult.getThird();
     }
 
     public long getFourth() {
-        return fourth;
+        return lottoWinResult.getFourth();
+    }
+
+    long getCost() {
+        return this.cost;
+    }
+
+    public long getTotalPrizeMoney() {
+        long totalPrizeMoney = 0;
+
+        totalPrizeMoney += (LottoRank.FIRST.getPrizeMoney() * getFirst());
+        totalPrizeMoney += (LottoRank.SECOND.getPrizeMoney() * getSecond());
+        totalPrizeMoney += (LottoRank.THIRD.getPrizeMoney() * getThird());
+        totalPrizeMoney += (LottoRank.FOURTH.getPrizeMoney() * getFourth());
+
+        return totalPrizeMoney;
+
+    }
+
+    public double getTotalProfitRate() {
+        return (double) getTotalPrizeMoney() / this.cost;
     }
 }
