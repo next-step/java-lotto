bd9f0329acaa6dec4ef2cd07a3023e085b7903b5
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-03-18 22:15:32 +0900

    Lotto - 당첨개수, 수익률계산 클래스 생성

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,30 @@
+package lotto;
+
+public class LottoResult {
+
+    private static final int MIN_RANGE = 3;
+    private static final int MAX_RANGE = 6;
+
+    private int[] matchCount;
+
+    public LottoResult() {
+        matchCount = new int[7];
+    }
+
+    public void addMatchCount(int i) {
+        matchCount[i]++;
+    }
+
+    public int[] checkResult() {
+        return matchCount;
+    }
+
+    public double calculateRate(int money) {
+        double rate = 0;
+        for (int i = MIN_RANGE; i <= MAX_RANGE; i++) {
+            rate += Prize.typeOf(i).calculatePrizeMoney(matchCount[i]);
+        }
+        rate /= money;
+        return rate;
+    }
+}
