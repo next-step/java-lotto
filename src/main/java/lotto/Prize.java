8b610430bdb86c496645e3c478cbc02b2489a876
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-03-18 21:55:10 +0900

    Lotto - 순위별 상금액 계산 Enum 클래스 생성

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,61 @@
+package lotto;
+
+import java.util.Optional;
+
+public enum Prize {
+
+    FIRST(6) {
+        public long calculatePrizeMoney(int count) {
+            return count * FIRST_PRIZE_MONEY;
+        }
+    },
+
+    SECONDE(5) {
+        public long calculatePrizeMoney(int count) {
+            return count * SECOND_PRIZE_MONEY;
+        }
+    },
+
+    THIRD(4) {
+        public long calculatePrizeMoney(int count) {
+            return count * THIRD_PRIZE_MONEY;
+        }
+    },
+
+    FORTH(3) {
+        public long calculatePrizeMoney(int count) {
+            return count * FORTH_PRIZE_MONEY;
+        }
+    };
+
+    private static final int FIRST_PRIZE_MONEY = 2000000000;
+    private static final int SECOND_PRIZE_MONEY = 1500000;
+    private static final int THIRD_PRIZE_MONEY = 50000;
+    private static final int FORTH_PRIZE_MONEY = 5000;
+
+    private final int matchNumber;
+
+    Prize(int matchNumber) {
+        this.matchNumber = matchNumber;
+    }
+
+    public abstract long calculatePrizeMoney(int count);
+
+    public static Prize typeOf(int matchNumber) {
+        Prize[] prizes = Prize.values();
+        Prize result = null;
+
+        for (Prize prize : prizes) {
+            result = findPrize(result, prize, matchNumber);
+        }
+
+        return Optional.ofNullable(result).orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static Prize findPrize(Prize result, Prize prize, int matchNumber) {
+        if (prize.matchNumber == matchNumber) {
+            result = prize;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/lotto/PrizeTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrizeTest {
+
+    @Test
+    public void 타입구하기() {
+        Prize prize = Prize.typeOf(3);
+        assertThat(prize.name()).isEqualTo("FORTH");
+    }
+
+    @Test
+    public void first_금액구하기() {
+        Prize prize = Prize.typeOf(6);
+        long prizeMoney = prize.calculatePrizeMoney(1);
+
+        assertThat(prizeMoney).isEqualTo(2000000000);
+    }
+}
