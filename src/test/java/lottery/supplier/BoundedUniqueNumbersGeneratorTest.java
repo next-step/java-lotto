bb30d1d0c15e2ff1d635dc82577dc2bde8eda00a
jcheolho <fnel159@naver.com>
2019-03-19 03:48:54 +0900

    step1: NumbersGenerator 추가 - 랜덤번호 생성 용도 - 테스트를 위한 지정번호 생성 용도 - 유니크 랜덤번호목록 생성 테스트

---
+++ src/test/java/lottery/supplier/BoundedUniqueNumbersGeneratorTest.java
@@ -0,0 +1,22 @@
+package lottery.supplier;
+
+import org.junit.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoundedUniqueNumbersGeneratorTest {
+
+    @Test
+    public void test_중복없는_랜덤숫자_생성_범위제한() {
+        final int lowerBoundInclusive = 1;
+        final int upperBoundInclusive = 1000;
+        final int size = 1000;
+        final NumbersGenerator boundedUniqueNumbersGenerator =
+                new BoundedUniqueNumbersGenerator(lowerBoundInclusive, upperBoundInclusive);
+
+        assertThat(boundedUniqueNumbersGenerator.nextNumbers(size))
+                .hasSize(size)
+                .doesNotHaveDuplicates()
+                .allMatch(i -> i >= lowerBoundInclusive && i <= upperBoundInclusive);
+    }
+}
