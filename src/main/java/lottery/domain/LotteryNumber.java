ab7c7d3b026db50ff3608ae88874b9d96c554db9
jcheolho <fnel159@naver.com>
2019-03-19 03:42:32 +0900

    step1: 로또넘버 클래스 생성
    - 번호 생성 범위 테스트
    - 동일성 테스트
    - toString 테스트

---
+++ src/main/java/lottery/domain/LotteryNumber.java
@@ -0,0 +1,37 @@
+package lottery.domain;
+
+import java.util.Objects;
+
+public class LotteryNumber {
+
+    public static int UPPER_BOUND_INCLUSIVE = 65;
+    public static int LOWER_BOUND_INCLUSIVE = 1;
+
+    int number;
+
+    public LotteryNumber(int number) {
+        if (LOWER_BOUND_INCLUSIVE > number || UPPER_BOUND_INCLUSIVE < number) {
+            throw new IllegalArgumentException("number range is " + LOWER_BOUND_INCLUSIVE +
+                    " to " + UPPER_BOUND_INCLUSIVE);
+        }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LotteryNumber that = (LotteryNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+}
