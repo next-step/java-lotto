c9e5cc5fe9e465445f0fb91cb312ca685d26f7d1
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-03-18 23:18:45 +0900

    LottoGame class 추가 - 로또구매, 당첨번호 일치 개수 확인

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    private List<Lotto> lottos;
+    private LottoGenerator lottoGenerator;
+
+    public LottoGame() {
+        this.lottos = new ArrayList<>();
+        this.lottoGenerator = new LottoGenerator();
+    }
+
+    public List<Lotto> buy(int money) {
+        int tickets = money / LOTTO_PRICE;
+
+        for (int i = 0; i < tickets; i++) {
+            lottos.add(new Lotto(lottoGenerator.generate()));
+        }
+        return lottos;
+    }
+
+    public int[] checkLuckyCount(LottoResult lottoResult, List<Integer> luckyNumbers) {
+        for (Lotto lotto : lottos) {
+            lottoResult.addMatchCount(lotto.matchNumbers(luckyNumbers));
+        }
+        return lottoResult.checkResult();
+    }
+}
---
+++ src/test/java/lotto/LottoGameTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoGameTest {
+
+    @Test
+    public void 로또구매하기() {
+        LottoGame lottoGame = new LottoGame();
+        int money = 4000;
+        List<Lotto> lottos = lottoGame.buy(money);
+
+        assertThat(lottos.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void 일치개수_구하기() {
+        LottoGame lottoGame = new LottoGame();
+        List<Integer> luckyNumbers = new ArrayList<>();
+        for (int i = 0; i < 6 ; i++) {
+            luckyNumbers.add(0);
+        }
+
+        int[] matchCount = lottoGame.checkLuckyCount(new LottoResult(), luckyNumbers);
+
+        for (int i = 3; i <= 6 ; i++) {
+            System.out.println(matchCount[i]);
+            assertThat(matchCount[i]).isEqualTo(0);
+        }
+    }
+}
