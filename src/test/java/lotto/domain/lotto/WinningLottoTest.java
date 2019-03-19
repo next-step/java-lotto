6594531c5dfd936d7b6113dc206f144de3efa6fe
Namjun Kim <namjunemy@gmail.com>
2019-03-20 02:30:20 +0900

    테스트 코드 리팩토링(Arrays.asList())

--- src/test/java/lotto/domain/lotto/WinningLottoTest.java
+++ src/test/java/lotto/domain/lotto/WinningLottoTest.java
@@ -2,7 +2,7 @@ package lotto.domain.lotto;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
@@ -12,13 +12,7 @@ public class WinningLottoTest {
 
     @Test
     public void 임의의_로또_생성() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
         Lotto winningLotto = new WinningLotto(numbers, 10);
         assertThat(winningLotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
@@ -26,13 +20,7 @@ public class WinningLottoTest {
 
     @Test
     public void 임의의_로또_정렬_후_생성() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(3);
-        numbers.add(5);
-        numbers.add(2);
-        numbers.add(4);
-        numbers.add(6);
+        List<Integer> numbers = Arrays.asList(1, 3, 5, 2, 4, 6);
 
         Lotto winningLotto = new WinningLotto(numbers, 10);
         assertThat(winningLotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
@@ -40,66 +28,30 @@ public class WinningLottoTest {
 
     @Test
     public void 로또_숫자_매치_카운트_3개_일치() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         BasicLotto lotto = new BasicLotto(numbers);
 
-        numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(7);
-        numbers.add(8);
-        numbers.add(9);
+        numbers = Arrays.asList(1, 2, 3, 7, 8, 9);
         WinningLotto winningLotto = new WinningLotto(numbers, 10);
         assertThat(winningLotto.checkMatchNumbers(lotto)).isEqualTo(3);
     }
 
     @Test
     public void 로또_숫자_매치_카운트_6개_일치() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         BasicLotto lotto = new BasicLotto(numbers);
 
-        numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         WinningLotto winningLotto = new WinningLotto(numbers, 10);
         assertThat(winningLotto.checkMatchNumbers(lotto)).isEqualTo(6);
     }
 
     @Test
     public void 보너스볼_포함_여부_확인() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(10);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 10);
         BasicLotto lotto = new BasicLotto(numbers);
 
-        numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
+        numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         WinningLotto winningLotto = new WinningLotto(numbers, 10);
         assertTrue(winningLotto.checkBonusNumber(lotto));
     }
--- src/test/java/lotto/utils/TestLottoGenerator.java
+++ src/test/java/lotto/utils/TestLottoGenerator.java
@@ -1,19 +1,12 @@
 package lotto.utils;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestLottoGenerator implements LottoGenerator {
 
     public List<Integer> generate() {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
-
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         return numbers;
     }
 }
