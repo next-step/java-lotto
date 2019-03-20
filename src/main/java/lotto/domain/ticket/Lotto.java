65f851d642908b23d4a26aa51b071d27590086ef
devson <sonyc5720@gmail.com>
2019-03-20 12:52:07 +0900

    1. 기존 상수를 하위 클래스 LottoNumber, LottoNumbers로 이동 2. validation 역할을 하위 클래스 생성자에 위임

--- src/main/java/lotto/domain/ticket/Lotto.java
+++ src/main/java/lotto/domain/ticket/Lotto.java
@@ -1,49 +1,15 @@
 package lotto.domain.ticket;
 
 import lotto.enums.LottoRank;
-import lotto.util.LottoNumberValidator;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 45;
-    public static final int LOTTO_NUMBERS_SIZE = 6;
-
     private final LottoNumbers lottoNumbers;
-    private final int bonusNumber;
+    private final LottoNumber bonusNumber;
 
     public Lotto(List<Integer> lottoNumbers, int bonusNumber) {
-        checkLottoNumbers(lottoNumbers, bonusNumber);
-
         this.lottoNumbers = new LottoNumbers(lottoNumbers);
-        this.bonusNumber = bonusNumber;
-    }
-
-    private void checkLottoNumbers(List<Integer> lottoNumbers, int bonusNumber) {
-        if (!LottoNumberValidator.isLottoNumbersSizeCorrect(lottoNumbers)) {
-            throw new IllegalArgumentException("Lotto numbers size must be " + LOTTO_NUMBERS_SIZE);
-        }
-
-        List<Integer> lottoAndBonusNumbers = new ArrayList<>(lottoNumbers);
-        lottoAndBonusNumbers.add(bonusNumber);
-
-        if (!LottoNumberValidator.isLottoNumbersUnique(lottoAndBonusNumbers)) {
-            throw new IllegalArgumentException("Every lotto numbers must be unique");
-        }
-
-        if (!LottoNumberValidator.isLottoNumbersCorrect(lottoAndBonusNumbers)) {
-            throw new IllegalArgumentException("Lotto number must be between " + MIN_NUMBER + " and " + MAX_NUMBER);
-        }
-    }
-
-    public List<Integer> getNumbers() {
-        return this.lottoNumbers.getNumbers();
-    }
-
-    public int getBonusNumber() {
-        return bonusNumber;
+        this.bonusNumber = new LottoNumber(bonusNumber);
     }
 
     public int getMatchCounts(Lotto target) {
@@ -56,12 +22,7 @@ public class Lotto {
         return matchCounts;
     }
 
-    boolean isBonusNumberSame(Lotto target) {
-        return this.bonusNumber == target.bonusNumber;
-    }
-
-    @Override
-    public String toString() {
-        return this.lottoNumbers.getNumbers().toString() + " / " + this.bonusNumber;
+    public boolean isBonusNumberSame(Lotto target) {
+        return this.bonusNumber.equalsTo(target.bonusNumber);
     }
 }
--- src/test/java/lotto/domain/ticket/LottoTest.java
+++ src/test/java/lotto/domain/ticket/LottoTest.java
@@ -7,48 +7,24 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
     @Test
-    public void 로또_생성_시_번호가_중복되면_IlligalArgumentException() {
+    public void matchCount_구하기() {
         // given
-        List<Integer> duplicatedNumbers = Arrays.asList(1, 1, 3, 4, 5, 6);
-        List<Integer> containsBonusNumbers = Arrays.asList(1, 2, 3, 4, 5, 45);
         int bonusNumber = 45;
+        int otherBonusNumber = 44;
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6), bonusNumber);
+        Lotto second = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 16), bonusNumber);
+        Lotto third = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 16), otherBonusNumber);
 
         // when
+        int shouldBeSecondMatchCount = lotto.getMatchCounts(second);
+        int shouldBeThirdMatchCount = lotto.getMatchCounts(third);
+
         // then
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(duplicatedNumbers, bonusNumber));
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(containsBonusNumbers, bonusNumber));
-    }
-
-    @Test
-    public void 로또_생성_시_숫자가_6개가_아니면_IlligalArgumentException() {
-        // given
-        List<Integer> fiveNumbers = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> sixNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        List<Integer> sevenNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        int bonusNumber = 45;
-
-        // when
-        // then
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(fiveNumbers, bonusNumber));
-        new Lotto(sixNumbers, bonusNumber);
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(sevenNumbers, bonusNumber));
-    }
-
-    @Test
-    public void 로또_생성_시_숫자가_범위를_벗어나면_IlligalArgumentException() {
-        // given
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        int outOfMaxNumber = Lotto.MAX_NUMBER + 1;
-        int outOfMinNumber = Lotto.MIN_NUMBER - 1;
-
-        // when
-        // then
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(lottoNumbers, outOfMaxNumber));
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(lottoNumbers, outOfMinNumber));
+        assertThat(shouldBeSecondMatchCount).isEqualTo(LottoRank.SECOND.getMatchCount());
+        assertThat(shouldBeThirdMatchCount).isEqualTo(LottoRank.THIRD.getMatchCount());
     }
 
     @Test
@@ -69,23 +45,5 @@ public class LottoTest {
         // then
         assertThat(shouldBeTrue).isTrue();
         assertThat(shouldBeFalse).isFalse();
-    }
-
-    @Test
-    public void matchCount_구하기() {
-        // given
-        int bonusNumber = 45;
-        int otherBonusNumber = 44;
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6), bonusNumber);
-        Lotto second = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 16), bonusNumber);
-        Lotto third = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 16), otherBonusNumber);
-
-        // when
-        int shouldBeSecondMatchCount = lotto.getMatchCounts(second);
-        int shouldBeThirdMatchCount = lotto.getMatchCounts(third);
-
-        // then
-        assertThat(shouldBeSecondMatchCount).isEqualTo(LottoRank.SECOND.getMatchCount());
-        assertThat(shouldBeThirdMatchCount).isEqualTo(LottoRank.THIRD.getMatchCount());
     }
 }
