43ae54ac84d0acf8af3ee09ac3124c97318b20aa
hyoj <chohyojoy@gmail.com>
2019-03-20 21:36:07 +0900

    Step1 개선  - 숫자 가독성을 위한 _ 삽입

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
 public enum Prize {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000);
 
     private int havingCounts;
     private int money;
