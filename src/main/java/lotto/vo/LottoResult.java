13235432607b405b018e4a66a35570c5aedcf374
devson <sonyc5720@gmail.com>
2019-03-20 22:51:57 +0900

    Lotto 끼리 비교한 결과를 담는 VO 클래스 생성

--- src/main/java/lotto/vo/LottoResult.java
+++ src/main/java/lotto/vo/LottoResult.java
@@ -1,46 +1,19 @@
 package lotto.vo;
 
-import lotto.domain.LottoMachine;
-import lotto.enums.LottoRank;
-
-import java.util.Arrays;
-
 public class LottoResult {
-    private final LottoWinResult lottoWinResult;
-    private final long cost;
+    private int matchCount;
+    private boolean bonusNumberMatch;
 
-    public LottoResult(long[] matchCounts) {
-        this.lottoWinResult = new LottoWinResult(matchCounts);
-        this.cost = extractCost(matchCounts);
+    public LottoResult(int matchCount, boolean bonusNumberMatch) {
+        this.matchCount = matchCount;
+        this.bonusNumberMatch = bonusNumberMatch;
     }
 
-    private long extractCost(long[] matchCounts) {
-        return LottoMachine.LOTTO_PRICE * extractNumberOfLottos(matchCounts);
+    public int getMatchCount() {
+        return matchCount;
     }
 
-    private long extractNumberOfLottos(long[] matchCounts) {
-        return Arrays.stream(matchCounts).sum();
-    }
-
-    public long getNumberOfWin(LottoRank lottoRank) {
-        return lottoWinResult.get(lottoRank);
-    }
-
-    long getCost() {
-        return this.cost;
-    }
-
-    public long getTotalPrizeMoney() {
-        return Arrays.stream(LottoRank.values())
-                .mapToLong(lottoRank -> getPrizeMoneyOfLottoRank(lottoWinResult, lottoRank))
-                .sum();
-    }
-
-    private long getPrizeMoneyOfLottoRank(LottoWinResult lottoWinResult, LottoRank lottoRank) {
-        return lottoWinResult.get(lottoRank) * lottoRank.getPrizeMoney();
-    }
-
-    public double getTotalProfitRate() {
-        return (double) getTotalPrizeMoney() / this.cost;
+    public boolean isBonusNumberMatch() {
+        return bonusNumberMatch;
     }
 }
