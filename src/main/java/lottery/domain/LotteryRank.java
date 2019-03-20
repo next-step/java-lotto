19f4ece6b79d05e63d18c938859024a730dffdc6
jcheolho <fnel159@naver.com>
2019-03-20 20:37:17 +0900

    step1: 피드백반영: 숫자 _ 구분자 추가

--- src/main/java/lottery/domain/LotteryRank.java
+++ src/main/java/lottery/domain/LotteryRank.java
@@ -3,10 +3,10 @@ package lottery.domain;
 import java.util.Arrays;
 
 public enum LotteryRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
     NONE(0, 0);
 
     public final int winningCount;
