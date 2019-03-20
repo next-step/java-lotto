6fbc6fd9c9a5a24531e47f147c3074dc2dd6b1a0
devson <sonyc5720@gmail.com>
2019-03-20 11:14:24 +0900

    로또번호에 해당하는 int primitive 값을 담는 LottoNumber 생성

---
+++ src/main/java/lotto/domain/ticket/LottoNumber.java
@@ -0,0 +1,28 @@
+package lotto.domain.ticket;
+
+public class LottoNumber {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
+    private final int number;
+
+    LottoNumber(int number) {
+        if (!isLottoNumber(number)) {
+            throw new IllegalArgumentException("Lotto number must be between " + MIN_NUMBER + " and " + MAX_NUMBER);
+        }
+
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    private boolean isLottoNumber(int number) {
+        return MIN_NUMBER <= number && number <= MAX_NUMBER;
+    }
+
+    boolean equalsTo(LottoNumber lottoNumber) {
+        return this.number == lottoNumber.number;
+    }
+}
