4ed68a9e744c4366ebaae51f84a077a26e93962a
jcheolho <fnel159@naver.com>
2019-03-21 19:13:19 +0900

    step2: feat: 우승티켓 클래스 추가

---
+++ src/main/java/lottery/domain/WinningTicket.java
@@ -0,0 +1,26 @@
+package lottery.domain;
+
+import java.util.List;
+
+public class WinningTicket {
+
+    private LotteryTicket winningTicket;
+
+    private LotteryNumber bonusNumber;
+
+    public WinningTicket(List<Integer> winningNumbers, int bonusNumber) {
+        this.bonusNumber = new LotteryNumber(bonusNumber);
+        this.winningTicket = new LotteryTicket(winningNumbers);
+    }
+
+    public LotteryRank raffle(LotteryTicket ticket) {
+        final int count = (int) winningTicket.lotteryNumbers
+                .stream()
+                .filter(winningNumber -> ticket.lotteryNumbers.contains(winningNumber))
+                .count();
+
+        final boolean contains = ticket.lotteryNumbers.contains(bonusNumber);
+
+        return LotteryRank.generate(count, contains);
+    }
+}
