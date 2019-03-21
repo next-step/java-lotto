8cb1f43284ac9fc00f8742672f0e3e92f6868f4f
jcheolho <fnel159@naver.com>
2019-03-21 19:07:19 +0900

    step2: test: 5등 테스트 추가, 보너스볼 당첨 여부 인자전달

--- src/test/java/lottery/domain/LotteryRankTest.java
+++ src/test/java/lottery/domain/LotteryRankTest.java
@@ -8,31 +8,39 @@ public class LotteryRankTest {
 
     @Test
     public void test_1등() {
-        assertThat(LotteryRank.generate(6))
+        assertThat(LotteryRank.generate(6, false))
                 .isEqualTo(LotteryRank.FIRST);
     }
 
     @Test
     public void test_2등() {
-        assertThat(LotteryRank.generate(5))
+        assertThat(LotteryRank.generate(5, true))
                 .isEqualTo(LotteryRank.SECOND);
     }
 
     @Test
     public void test_3등() {
-        assertThat(LotteryRank.generate(4))
+        assertThat(LotteryRank.generate(5, false))
                 .isEqualTo(LotteryRank.THIRD);
     }
 
     @Test
     public void test_4등() {
-        assertThat(LotteryRank.generate(3))
+        assertThat(LotteryRank.generate(4, false))
                 .isEqualTo(LotteryRank.FOURTH);
     }
 
     @Test
+    public void test_5등() {
+        assertThat(LotteryRank.generate(3, false))
+                .isEqualTo(LotteryRank.FIFTH);
+    }
+
+    @Test
     public void test_당첨없음() {
-        assertThat(LotteryRank.generate(0))
-            .isEqualTo(LotteryRank.NONE);
+        assertThat(LotteryRank.generate(2, false))
+                .isEqualTo(LotteryRank.NONE);
+        assertThat(LotteryRank.generate(2, true))
+                .isEqualTo(LotteryRank.NONE);
     }
 }
