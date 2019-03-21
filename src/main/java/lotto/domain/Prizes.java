46df3c23672ba2b02dff68f37da6137a471a8c16
Park Dongwook <wanderhoward@gmail.com>
2019-03-21 22:25:39 +0900

    Step1. 피드백 반영 - 천 단위 구분을 위해 * 로 표기

--- src/main/java/lotto/domain/Prizes.java
+++ src/main/java/lotto/domain/Prizes.java
@@ -2,16 +2,17 @@ package lotto.domain;
 
 public enum Prizes {
 
-  THREE(3, 5000),
-  FOUR(4, 50000),
-  FIVE(5, 1500000),
-  SIX(6, 2000000000),
+  THREE(3, 5 * 1000),
+  FOUR(4, 50 * 1000),
+  FIVE(5, 1500 * 1000),
+  SIX(6, 2 * 1000 * 1000 * 1000),
   ;
 
   private int matchCount;
   private long money;
 
   Prizes(int matchCount, long money) {
+    this.matchCount = matchCount;
     this.money = money;
   }
 
