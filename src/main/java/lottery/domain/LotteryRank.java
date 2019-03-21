4d9d74c6ac383a1625dd73fcb5a43bde4c1cd702
jcheolho <fnel159@naver.com>
2019-03-21 19:08:25 +0900

    step2: feat: 5등 추가, generate 메소드에 보너스볼 당첨 여부 파라미터 추가

--- src/main/java/lottery/domain/LotteryRank.java
+++ src/main/java/lottery/domain/LotteryRank.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public enum LotteryRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NONE(0, 0);
 
     public final int winningCount;
@@ -18,9 +19,18 @@ public enum LotteryRank {
         this.revenue = revenue;
     }
 
-    public static LotteryRank generate(int countWinningNumbers) {
+    public static LotteryRank generate(int countWinningNumbers, boolean matchBonus) {
         return Arrays.stream(values())
-                .filter(rank -> rank.winningCount == countWinningNumbers)
+                .filter(rank -> {
+                    if (countWinningNumbers == 5) {
+                        if (matchBonus) {
+                            return rank == SECOND;
+                        }
+
+                        return rank == THIRD;
+                    }
+                    return rank.winningCount == countWinningNumbers;
+                })
                 .findFirst()
                 .orElse(NONE);
     }
