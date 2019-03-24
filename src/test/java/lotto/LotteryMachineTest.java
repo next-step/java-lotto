4dd206a041903b52b8ba2e588326334fe5c0c6e2
SGKIM94 <tkdrn8578@naver.com>
2019-03-24 14:03:40 +0900

    LotteryMachineTest  * 로또 수익률 계산 및 순위를 테스트하는 클래스

---
+++ src/test/java/lotto/LotteryMachineTest.java
@@ -0,0 +1,50 @@
+package lotto;
+
+import lotto.domain.*;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LotteryMachineTest {
+    final Set<Integer> lotteryNumber = new HashSet<>(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+
+    @Test
+    public void 로또_맞춘_개수_테스트() {
+
+        Lottery lottery = new Lottery(lotteryNumber);
+
+        Set<Integer> winnerLottery = new HashSet<>(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        lottery.generationLotto(winnerLottery);
+
+        assertThat(lottery.matchNumber(3, 4)).isEqualTo(4);
+    }
+    @Test
+    public void 로또_일등_테스트() {
+        Lottery lottery = new Lottery(lotteryNumber);
+
+        Set<Integer> inputLottery = new HashSet<>(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        Set<Integer> winnerLottery = new HashSet<>(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+
+        LotteryMachine lotteryMachine = new LotteryMachine(winnerLottery);
+
+        lottery.generationLotto(inputLottery);
+
+        assertThat(lotteryMachine.rankLottery(lottery)).isEqualTo(1);
+    }
+
+    @Test
+    public void 로또_상금_테스트() {
+        Lottery lottery = new Lottery(lotteryNumber);
+
+        Set<Integer> inputLottery = new HashSet<>(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+        Set<Integer> winnerLottery = new HashSet<>(new ArrayList<>(Arrays.asList(1,2,3,11,12,13)));
+
+        LotteryMachine lotteryMachine = new LotteryMachine(winnerLottery);
+
+        lottery.generationLotto(inputLottery);
+
+        assertThat(lotteryMachine.countProfit(lottery)).isEqualTo(50000);
+    }
+}
