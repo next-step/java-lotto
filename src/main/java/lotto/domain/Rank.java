3760fc1bb9ae667040041e49046874ea7d3ef5b2
hyoj <chohyojoy@gmail.com>
2019-03-24 18:59:04 +0900

    Rank 열거 타입 상수 생성시 일관된 포맷 적용을 위해 false 전달

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,23 +1,18 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
+    FIRST(6, 2_000_000_000, false),
     SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    SIXTH(2, 0),
-    SEVENTH(1, 0),
-    NONE(0, 0);
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    SIXTH(2, 0, false),
+    SEVENTH(1, 0, false),
+    NONE(0, 0, false);
 
     private int countOfMatch;
     private long money;
     private boolean hasBonus;
-
-    Rank(int countOfMatch, int money) {
-        this.countOfMatch = countOfMatch;
-        this.money = money;
-    }
 
     Rank(int countOfMatch, long money, boolean hasBonus) {
         this.countOfMatch = countOfMatch;
