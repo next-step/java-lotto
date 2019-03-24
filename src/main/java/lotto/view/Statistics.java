213b37a9d39c477d320fad2fd966c86e016091e0
SGKIM94 <tkdrn8578@naver.com>
2019-03-24 14:00:40 +0900

    view - Statistics  * 로또의 통계부분을 출력해주는 클래스

---
+++ src/main/java/lotto/view/Statistics.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import lotto.domain.Lotteries;
+import lotto.domain.LotteryMachine;
+
+import java.util.Map;
+
+public class Statistics {
+
+    public static void printStatistics(Lotteries lotteries, LotteryMachine lotteryMachine) {
+        System.out.println("당첨 통계");
+
+        Map<String, Integer> ranks = lotteries.makeAllLotteriesRank(lotteryMachine);
+
+        System.out.println("3개 일치 (5000원) " + ranks.get("first"));
+        System.out.println("4개 일치 (50000원) " + ranks.get("second"));
+        System.out.println("5개 일치 (150000원) " + ranks.get("third"));
+        System.out.println("6개 일치 (2000000000) " + ranks.get("fourth"));
+
+        System.out.println("총 수익률은 " + lotteries.calculateProfit(lotteryMachine) + " 입니다.");
+    }
+}
