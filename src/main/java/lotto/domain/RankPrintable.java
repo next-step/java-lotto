1ed584624d58e07e249225de291d32551a5c34de
SGKIM94 <tkdrn8578@naver.com>
2019-03-24 13:59:12 +0900

    RankPrintable, RankPrintableFactory  * 로또의 순위를 분기하는 인터페이스

---
+++ src/main/java/lotto/domain/RankPrintable.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface RankPrintable {
+    RankPrintable NONE = new RankPrintable() {
+        @Override
+        public void makeRanksForPrint(Map<String, Integer> ranks) {
+            ranks.put("first", 0);
+            ranks.put("second", 0);
+            ranks.put("third", 0);
+            ranks.put("fourth", 0);
+        }
+    };
+
+    void makeRanksForPrint(Map<String, Integer> ranks);
+}
+
+class First implements RankPrintable {
+    @Override
+    public void makeRanksForPrint(Map<String, Integer> ranks) {
+        ranks.put("first", ranks.get("first") + 1);
+    }
+}
+
+class Second implements RankPrintable {
+    @Override
+    public void makeRanksForPrint(Map<String, Integer> ranks) {
+        ranks.put("second", ranks.get("second") + 1);
+    }
+}
+
+class Third implements RankPrintable {
+    @Override
+    public void makeRanksForPrint(Map<String, Integer> ranks) {
+        ranks.put("third", ranks.get("third") + 1);
+    }
+}
+
+class Fourth implements RankPrintable {
+    @Override
+    public void makeRanksForPrint(Map<String, Integer> ranks) {
+        ranks.put("fourth", ranks.get("fourth") + 1);
+    }
+}
+
