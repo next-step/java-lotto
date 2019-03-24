5e015a02e8db9bb7003b340ab252d2eea959a29f
jcheolho <fnel159@naver.com>
2019-03-24 16:42:19 +0900

    step2: 피드백반영: 순위 필터링 기능 abstract match 메소드로 위임

--- src/main/java/lottery/domain/LotteryRank.java
+++ src/main/java/lottery/domain/LotteryRank.java
@@ -3,12 +3,42 @@ package lottery.domain;
 import java.util.Arrays;
 
 public enum LotteryRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000) {
+        @Override
+        protected boolean match(int winningCount, boolean matchBonus) {
+            return this.winningCount == winningCount;
+        }
+    },
+    SECOND(5, 30_000_000) {
+        @Override
+        protected boolean match(int winningCount, boolean matchBonus) {
+            return this.winningCount == winningCount && matchBonus;
+        }
+    },
+    THIRD(5, 1_500_000) {
+        @Override
+        protected boolean match(int winningCount, boolean matchBonus) {
+            return this.winningCount == winningCount && !matchBonus;
+        }
+    },
+    FOURTH(4, 50_000) {
+        @Override
+        protected boolean match(int winningCount, boolean matchBonus) {
+            return this.winningCount == winningCount;
+        }
+    },
+    FIFTH(3, 5_000) {
+        @Override
+        protected boolean match(int winningCount, boolean matchBonus) {
+            return this.winningCount == winningCount;
+        }
+    },
+    NONE(0, 0) {
+        @Override
+        protected boolean match(int winningCount, boolean matchBonus) {
+            return winningCount < FIFTH.winningCount;
+        }
+    };
 
     public final int winningCount;
 
@@ -19,23 +49,12 @@ public enum LotteryRank {
         this.revenue = revenue;
     }
 
+    protected abstract boolean match(int winningCount, boolean matchBonus);
+
     public static LotteryRank generate(int countWinningNumbers, boolean matchBonus) {
         return Arrays.stream(values())
-                .filter(rank -> {
-                    if (countWinningNumbers == 5) {
-                        return filterFiveMatches(matchBonus, rank);
-                    }
-                    return rank.winningCount == countWinningNumbers;
-                })
+                .filter(rank -> rank.match(countWinningNumbers, matchBonus))
                 .findFirst()
                 .orElse(NONE);
-    }
-
-    private static boolean filterFiveMatches(boolean matchBonus, LotteryRank rank) {
-        if (matchBonus) {
-            return rank == SECOND;
-        }
-
-        return rank == THIRD;
     }
 }
