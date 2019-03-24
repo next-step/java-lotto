c49ec9077156b0945d300a50b278d71e45ae0760
devson <sonyc5720@gmail.com>
2019-03-24 10:54:14 +0900

    수동 입력 관련 input 메서드 추가

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleInputView {
@@ -9,6 +11,24 @@ public class ConsoleInputView {
     public static long inputMoney(Scanner scanner) {
         System.out.println("구입금액을 입력해 주세요.");
         return Long.parseLong(scanner.nextLine());
+    }
+
+    public static long inputManualTimes(Scanner scanner) {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Long.parseLong(scanner.nextLine());
+    }
+
+    public static List<String> inputManualLottos(Scanner scanner, long times) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        List<String> lottos = new ArrayList<>();
+
+        for (int i = 0; i < times; i++) {
+            String lottoNumbers = scanner.nextLine();
+            lottos.add(lottoNumbers);
+        }
+
+        return lottos;
     }
 
     public static String inputWinnerNumbers(Scanner scanner) {
