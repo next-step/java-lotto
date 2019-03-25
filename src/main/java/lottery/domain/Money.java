74e1884c7a01948694b4e13d87460673fb0d2ddb
jcheolho <fnel159@naver.com>
2019-03-25 23:38:43 +0900

    step2: feat: Money 덧셈, 곱셈, 나눗셈 기능/테스트 추가

--- src/main/java/lottery/domain/Money.java
+++ src/main/java/lottery/domain/Money.java
@@ -23,6 +23,14 @@ public class Money {
         return Objects.hash(amount);
     }
 
+    public Money add(int addend) {
+        return new Money(this.amount + addend);
+    }
+
+    public Money times(int multiplier) {
+        return new Money(this.amount * multiplier);
+    }
+
     public Money divide(int dividend) {
         return new Money(this.amount / dividend);
     }
--- src/test/java/lottery/domain/MoneyTest.java
+++ src/test/java/lottery/domain/MoneyTest.java
@@ -17,10 +17,33 @@ public class MoneyTest {
     }
 
     @Test
-    public void test_나누기() {
+    public void test_덧셈() {
+        Money money = new Money(1000);
+
+        assertThat(money.add(1000))
+                .isEqualTo(new Money(2000));
+    }
+
+    @Test
+    public void test_곱셈() {
+        Money money = new Money(1000);
+
+        assertThat(money.times(14))
+                .isEqualTo(new Money(14000));
+    }
+
+    @Test
+    public void test_나눗셈() {
         Money money = new Money(14000);
 
         assertThat(money.divide(1000))
             .isEqualTo(new Money(14));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void test_0으로_나눔() {
+        Money money = new Money(14000);
+
+        money.divide(0);
     }
 }
