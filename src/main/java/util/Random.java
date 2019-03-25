32daedbd4b7478f91809bc4fccbb74d1157f392a
SGKIM94 <tkdrn8578@naver.com>
2019-03-25 23:45:54 +0900

    Random  * 랜덤 생성하는 메서드 분리 및 피드백 반영

--- src/main/java/util/Random.java
+++ src/main/java/util/Random.java
@@ -3,20 +3,24 @@ package util;
 import java.util.*;
 
 public class Random {
-    private static final int LOTTO_LENGTH = 6;
     private static final int LOTTO_MAX_VALUE = 45;
-    private static final int INITIALIZATION_RANDOM_NUMBER = 1;
 
 
     public static Set<Integer> generationRandom() {
-        List<Integer> random = new ArrayList<>();
-
-        for (int i = 0; i < LOTTO_MAX_VALUE; i++) {
-            random.add(i);
-        }
+        List<Integer> random = makeNumber();
 
         Collections.shuffle(random);
 
         return new HashSet<>(random.subList(0, 6));
+    }
+
+    public static List<Integer> makeNumber() {
+        List<Integer> random = new ArrayList<>();
+
+        for (int i = 1; i < LOTTO_MAX_VALUE + 1; i++) {
+            random.add(i);
+        }
+
+        return random;
     }
 }
