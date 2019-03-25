6a956220f0ab63e397d0acf6c92c7c8e3d203854
hyoj <chohyojoy@gmail.com>
2019-03-25 22:47:03 +0900

    불필요하게 List 로 감싸던 부분 제거

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -1,14 +1,11 @@
 package lotto.domain;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class LottoMatcher {
 
     public static MatchResults calculateMatchResults(final LottoList lottos, final WinningLotto winningLotto) {
         MatchResults matchResults = new MatchResults();
 
-        List<Rank> ranks = Arrays.asList(Rank.values());
+        Rank[] ranks = Rank.values();
 
         for (Rank rank : ranks) {
             matchResults.put(
