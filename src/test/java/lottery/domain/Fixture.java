f547c52e78b98b6fd3ab03985fe417ea21dc11cd
isisrin <isisrin@nate.com>
2019-03-27 02:32:20 +0900

    step2 구현 - 테스트 코드에 너무 중복이 많아 Fixture 추가

---
+++ src/test/java/lottery/domain/Fixture.java
@@ -0,0 +1,29 @@
+package lottery.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Fixture {
+
+    public static List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
+    public static LotteryNumber createDefaultLotteryNumber = new LotteryNumber(numbers);
+    public static Lottery oneToSixLottery = new Lottery(createDefaultLotteryNumber);
+    public static Lottery oneToSixBonusBallLottery = new Lottery(new LotteryNumber(numbers,7));
+
+    public static LotteryNumber createLotteryNumber(List<Integer> inputNumbers) {
+        return new LotteryNumber(inputNumbers);
+    }
+
+    public static LotteryNumber createLotteryNumberWithBonus(int bonusNumber, Integer... inputNumbers) {
+        return new LotteryNumber(Arrays.asList(inputNumbers), bonusNumber);
+    }
+
+    public static Lottery createLottery(List<Integer> inputNumbers) {
+        return new Lottery(createLotteryNumber(inputNumbers));
+    }
+
+    public static Lottery createLotteryWithBonus(int bonusNumber, Integer... inputNumbers) {
+        return new Lottery(createLotteryNumberWithBonus(bonusNumber, inputNumbers));
+    }
+
+}
