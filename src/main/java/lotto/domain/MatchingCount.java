e19278b0d582d59af335bf8a0e35599327968cb0
SGKIM94 <tkdrn8578@naver.com>
2019-03-26 21:44:01 +0900

    MatchingCount  * valueOf 메서드를 Stream을 이용하여 로직 수정

--- src/main/java/lotto/domain/MatchingCount.java
+++ src/main/java/lotto/domain/MatchingCount.java
@@ -1,43 +1,36 @@
 package lotto.domain;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public enum MatchingCount {
-
     FIRST(6, Ranking.FIRST_CLASS),
     SECOND(5, Ranking.SECOND_CLASS),
     THIRD(4, Ranking.THIRD_CLASS),
     FOURTH(3, Ranking.FOURTH_CLASS),
     ZERO(0, Ranking.ZERO_CLASS);
 
+
+    private static final int MINIMUM_MATCH_LOTTO = 3;
     int count;
     Ranking rank;
-
-    private static Map mappingByCountMatching = new HashMap<>();
 
     MatchingCount(int matchingCount, Ranking ranking) {
         this.count = matchingCount;
         this.rank = ranking;
     }
 
-    static {
-        for (MatchingCount matchingCount : MatchingCount.values()) {
-            mappingByCountMatching.put(matchingCount.count, matchingCount);
-        }
-    }
-
     public static MatchingCount valueOf(int matchCount) {
-        MatchingCount matchingCount = (MatchingCount) mappingByCountMatching.get(matchCount);
-
-        if (matchingCount == null){
-            throw new IllegalArgumentException("Not Enum constant was found for matchCount : " + matchCount);
-        }
-
-        return matchingCount;
+        return Arrays.stream(MatchingCount.values())
+                .filter(e -> e.count == matchCount)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Not Enum constant was found for matchCount : " + matchCount));
     }
 
-    public Ranking getRank() {
-        return this.rank;
+    public static Ranking getRank(int count) {
+        if (count < MINIMUM_MATCH_LOTTO) {
+            return MatchingCount.valueOf(0).rank;
+        }
+
+        return MatchingCount.valueOf(count).rank;
     }
 }
