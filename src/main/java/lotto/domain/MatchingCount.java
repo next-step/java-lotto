b143a198d48454cf47b764d7420a9a2de52a6bfb
SGKIM94 <tkdrn8578@naver.com>
2019-03-27 23:22:45 +0900

    MatchingCount  * 보너스볼 포함된 등수 체크하는 로직 추가

--- src/main/java/lotto/domain/MatchingCount.java
+++ src/main/java/lotto/domain/MatchingCount.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 public enum MatchingCount {
     FIRST(6, Ranking.FIRST_CLASS),
     SECOND(5, Ranking.SECOND_CLASS),
-    THIRD(4, Ranking.THIRD_CLASS),
-    FOURTH(3, Ranking.FOURTH_CLASS),
+    THIRD(5, Ranking.THIRD_CLASS),
+    FOURTH(4, Ranking.FOURTH_CLASS),
+    FIFTH(3, Ranking.FIFTH_CLASS),
     ZERO(0, Ranking.ZERO_CLASS);
 
 
     private static final int MINIMUM_MATCH_LOTTO = 3;
+    private static final int MATCHING_FIFTH_COUNT = 5;
     int count;
     Ranking rank;
 
@@ -22,15 +24,28 @@ public enum MatchingCount {
     public static MatchingCount valueOf(int matchCount) {
         return Arrays.stream(MatchingCount.values())
                 .filter(e -> e.count == matchCount)
+                .filter(f -> f.rank != Ranking.SECOND_CLASS)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Not Enum constant was found for matchCount : " + matchCount));
     }
 
-    public static Ranking getRank(int count) {
+    public static Ranking getRank(int count, boolean hasBonusNumber) {
         if (count < MINIMUM_MATCH_LOTTO) {
-            return MatchingCount.valueOf(0).rank;
+            return MatchingCount.ZERO.rank;
+        }
+
+        if(isMatchingFifth(count) && hasBonusNumber) {
+            return MatchingCount.SECOND.rank;
         }
 
         return MatchingCount.valueOf(count).rank;
+    }
+
+    public static boolean isMatchingFifth(int count) {
+        if (count == MATCHING_FIFTH_COUNT) {
+           return true;
+        }
+
+        return false;
     }
 }
