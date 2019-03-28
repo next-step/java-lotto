1d3b33dafbff5d94376443691fb4fa13ce062bef
isisrin <isisrin@nate.com>
2019-03-28 11:04:30 +0900

    step3 구현 (피드백 반영)  - Money가 자동,수동 값도 가지도록 수정

--- src/test/java/lottery/domain/MoneyTest.java
+++ src/test/java/lottery/domain/MoneyTest.java
@@ -7,16 +7,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MoneyTest {
 
     @Test
-    public void 로또한장구입() {
-        int result = new Money(1000).purchaseCount();
+    public void 로또구입() {
+        int result = new Money(1000, 0).randomCount();
         assertThat(result).isEqualTo(1);
 
-        result = new Money(2000).purchaseCount();
-        assertThat(result).isEqualTo(2);
+        Money moneyResult = new Money(2000, 1);
+        assertThat(moneyResult.manualCount()).isEqualTo(1);;
+        assertThat(moneyResult.randomCount()).isEqualTo(1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void 오백원테스트() {
-       new Money(500);
+       new Money(500, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 낸돈보다_수동_많이구입_테스트() {
+        new Money(1000, 2);
+        System.out.println(new Money(1000, 2).toString());
     }
 }
