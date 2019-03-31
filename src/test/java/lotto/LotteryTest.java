b151162a7a68c644aa9c1e4c2335c2c79054baa8
SGKIM94 <tkdrn8578@naver.com>
2019-03-31 21:09:27 +0900

    * 클래스 추가에 따른 객체 명 변경

--- src/test/java/lotto/LotteryTest.java
+++ src/test/java/lotto/LotteryTest.java
@@ -1,15 +1,12 @@
 package lotto;
 
-import lotto.domain.Lotteries;
-import lotto.domain.Lottery;
-import lotto.domain.LotteryNo;
-import lotto.domain.Ranking;
+import lotto.domain.*;
 import org.junit.Test;
 
 import java.util.*;
 
 import static lotto.controller.LotteryGame.main;
-import static lotto.domain.Lotteries.getsInitializedMap;
+import static lotto.domain.TotalLotteries.getsInitializedMap;
 import static lotto.domain.LotteryNo.makeLotteryNos;
 import static org.assertj.core.api.Assertions.assertThat;
 import static util.Random.generateRandom;
@@ -17,35 +14,34 @@ import static util.Random.makeNumber;
 
 
 public class LotteryTest {
-
     @Test
     public void 로또_구매_문자열() {
-        Lotteries lotteries = new Lotteries("1000");
-        assertThat(lotteries.lotteriesSize()).isEqualTo(1);
+        TotalLotteries totalLotteries = new TotalLotteries("1000");
+        assertThat(totalLotteries.countPurchaseQuantity()).isEqualTo(1);
     }
 
     @Test
     public void 로또_구매_숫자() {
-        Lotteries lotteries = new Lotteries(1000);
-        assertThat(lotteries.lotteriesSize()).isEqualTo(1);
+        TotalLotteries totalLotteries = new TotalLotteries(1000);
+        assertThat(totalLotteries.countPurchaseQuantity()).isEqualTo(1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void 금액입력_NULL_예외처리() {
-        Lotteries.checkPurchaseValidation(null);
+        TotalLotteries.checkPurchaseValidation(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void 금액입력_공백_예외처리() {
-        Lotteries.checkPurchaseValidation("");
+        TotalLotteries.checkPurchaseValidation("");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void 최소_금액_예외처리() {
-        Lotteries.checkPurchaseValidation("900");
+        TotalLotteries.checkPurchaseValidation("900");
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void 로또_중복_입력_테스트() {
         final Set<LotteryNo> lotteryNumber = makeLotteryNos(Arrays.asList(1, 1, 2, 3, 4, 5, 6));
 
@@ -54,9 +50,9 @@ public class LotteryTest {
 
     @Test
     public void 로또_생성_개수_검사() {
-        Lotteries lotteries = new Lotteries("15000");
+        TotalLotteries totalLotteries = new TotalLotteries("15000");
 
-        assertThat(15).isEqualTo(lotteries.lotteriesSize());
+        assertThat(15).isEqualTo(totalLotteries.countPurchaseQuantity());
     }
 
     @Test
@@ -71,9 +67,9 @@ public class LotteryTest {
 
     @Test
     public void 랜덤값_생성개수_테스트() {
-        Set<LotteryNo> random = generateRandom();
+        Lottery random = generateRandom();
 
-        assertThat(random.size()).isEqualTo(6);
+        assertThat(random.getLotterySize()).isEqualTo(6);
     }
 
     @Test
@@ -85,7 +81,7 @@ public class LotteryTest {
 
     @Test
     public void 로또_게임_테스트() {
-        // 출력 테스트를 위한 main함수
+        // 출력 테스트를 위한 main 함수
         main();
     }
 }
