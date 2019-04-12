fdbf1c8fd64a46fa1bfa0e20064587413ed7a398
RudiaMoon <impactyoung@gmail.com>
2019-04-13 00:19:26 +0900

    update Rank 2위

--- src/main/java/lotto/vo/Rank.java
+++ src/main/java/lotto/vo/Rank.java
@@ -5,17 +5,20 @@ import lotto.utils.Const;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST_PLACE(6, 2_000_000_000),
-    SECOND_PLACE(5, 1_500_000),
-    THIRD_PLACE(4, 50_000),
-    FOURTH_PLACE(3, 5_000),
-    FAILURE(0, 0);
+    FIRST_PLACE(6, false, 2_000_000_000),
+    SECOND_PLACE(5, true, 30_000_000),
+    THIRD_PLACE(5, false, 1_500_000),
+    FOURTH_PLACE(4, false, 50_000),
+    FIFTH_PLACE(3, false, 5_000),
+    FAILURE(0, false, 0);
 
     public static final int MINIMUM_WINNING_NUMBER = 3;
     private int numberOfMatches, reward;
+    private boolean isBonus;
 
-    Rank(int numberOfMatches, int reward) {
+    Rank(int numberOfMatches, boolean isBonus, int reward) {
         this.numberOfMatches = numberOfMatches;
+        this.isBonus = isBonus;
         this.reward = reward;
     }
 
@@ -39,15 +42,15 @@ public enum Rank {
         return sumProfit;
     }
 
+    public static Rank getRank(LotteryNumber winningNumber, LotteryNumber currentLottery) {
+        return winningNumber.match(currentLottery);
+    }
+
     public int getNumberOfMatches() {
         return numberOfMatches;
     }
 
     public int getReward() {
         return reward;
-    }
-
-    public static Rank getRank(LotteryNumber winningNumber, LotteryNumber currentLottery) {
-        return winningNumber.match(currentLottery);
     }
 }
