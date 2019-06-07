20a04c0672bd5554d573fc2104b92bc8322e1473
RudiaMoon <impactyoung@gmail.com>
2019-06-08 07:22:47 +0900

    Test 생성가능한_로또_갯수

--- src/main/java/vo/Money.java
+++ src/main/java/vo/Money.java
@@ -4,13 +4,17 @@ public class Money {
     public static final int LOTTERY_MONEY = 1000;
     private int money;
 
-    public Money(String money){
+    public Money(String money) {
         this(Integer.parseInt(money));
     }
 
     public Money(int money) {
-        if(money < LOTTERY_MONEY)
+        if (money < LOTTERY_MONEY)
             throw new IllegalArgumentException();
         this.money = money;
+    }
+
+    public int lotteryCount() {
+        return money / LOTTERY_MONEY;
     }
 }
--- src/test/java/vo/MoneyTest.java
+++ src/test/java/vo/MoneyTest.java
@@ -3,9 +3,23 @@ package vo;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MoneyTest {
     @Test(expected = IllegalArgumentException.class)
     public void 입력한_금액이_1000원_미만이면_exception(){
         new Money(900);
+    }
+
+    @Test
+    public void 생성가능한_로또_갯수() {
+        //Given
+        Money money = new Money("14000");
+
+        //When
+        int usingLotteryCount = money.lotteryCount();
+
+        //Then
+        assertThat(usingLotteryCount).isEqualTo(14);
     }
 }
