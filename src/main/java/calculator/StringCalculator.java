e87c4fa43777ae28062aa6e9f3a52b38fc3cb462
changjun89 <leechang0423@naver.com>
2019-06-21 01:25:54 +0900

    feat(calculator) 입력값이 null 혹은 빈값일때 0을 리턴한다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,14 @@ package calculator;
 
 public class StringCalculator {
 
-  public static String calculate(String input) {
-    return input;
+  public static int calculate(String input) {
+    if(isBlank(input)) {
+      return 0;
+    }
+    return Integer.parseInt(input);
+  }
+
+  private static boolean isBlank(String input) {
+    return input == null || "".equals(input.trim());
   }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,13 +3,23 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringCalculatorTest {
 
   @Test
   public void 숫자가_하나가_들어왔을때에는_하나만_리턴한다() {
     String input = "3";
-    assertThat(StringCalculator.calculate(input)).isEqualTo(input);
+    assertThat(StringCalculator.calculate(input)).isEqualTo(3);
+  }
+
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(strings = {"", "  "})
+  public void 입력값이_null_혹은_빈값일때_0을_리턴한다(String input) {
+    assertThat(StringCalculator.calculate(input)).isEqualTo(0);
   }
 
 }
