30967b4ea480d344127771386344e30c8800c897
changjun89 <leechang0423@naver.com>
2019-06-21 01:45:36 +0900

    feat(calculator) 입력값을 컴마(,) 와 콜론 (:) 구분자로 분리하여 리턴한다.

---
+++ src/main/java/calculator/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package calculator.utils;
+
+public class StringUtils {
+
+  public static String[] splitInputValue(String delimiter, String input) {
+    return input.split(delimiter);
+  }
+
+}
---
+++ src/test/java/calculator/utils/StringUtilsTest.java
@@ -0,0 +1,23 @@
+package calculator.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringUtilsTest {
+
+  @Test
+  public void 콤마로_인풋값을_분리한다() {
+    String input = "2,3";
+    assertThat(StringUtils.splitInputValue(",",input)).containsExactly("2","3");
+  }
+
+  @Test
+  public void 콤마와_콜론으로_인풋값을_분리한다() {
+    String input = "2,3:6:7";
+    assertThat(StringUtils.splitInputValue(",|:",input)).containsExactly("2","3","6","7");
+  }
+
+
+
+}
