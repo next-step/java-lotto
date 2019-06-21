98d8a7d500f9a92abbc0e36cf17ad1659a933835
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 09:45:19 +0900

    [test] 쉼표를 또는 콜론을 구분자로 가지는 문자열 입력 시 합을 반환한다. 테스트 추가

--- src/test/java/com/jaeyeonling/calculate/StringCalculatorTest.java
+++ src/test/java/com/jaeyeonling/calculate/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package com.jaeyeonling.calculate;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +14,16 @@ class StringCalculatorTest {
         final int returnValue = StringCalculator.add("");
 
         assertThat(returnValue).isEqualTo(0);
+    }
+
+    @DisplayName("쉼표를 또는 콜론을 구분자로 가지는 문자열 입력 시 합을 반환한다.")
+    @CsvSource({
+            "'1,2',3"
+    })
+    void should_expected_sum_when_add(final String expression, final int expected) {
+        final int returnValue = StringCalculator.add(expression);
+
+        assertThat(returnValue).isEqualTo(expected);
     }
 
 }
