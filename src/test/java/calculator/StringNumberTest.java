3ac3397b38cb596e0c1c19c871f0b55a104cb2e4
KIM JUNGHWAN <writer0713@naver.com>
2019-06-21 15:44:33 +0900

    #step1 [test] StringNumber의 parse exception test 추가
    - 음수 parse 시 RuntimeException
    - 숫자가 아닌 값 parse RuntimeException

--- src/test/java/calculator/StringNumberTest.java
+++ src/test/java/calculator/StringNumberTest.java
@@ -1,8 +1,10 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringNumberTest {
 
@@ -10,6 +12,18 @@ class StringNumberTest {
     void parse() {
         StringNumber number = new StringNumber("1");
         assertThat(number.parse()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("parse에 숫자가 아닌 값 넣을시 RuntimeException")
+    void parse_throwRuntimeException() {
+        assertThrows(RuntimeException.class, () -> new StringNumber("-a").parse());
+    }
+
+    @Test
+    @DisplayName("parse에 음수를 넣을시 RuntimeException")
+    void parse_throwRuntimeException2() {
+        assertThrows(RuntimeException.class, () -> new StringNumber("-1").parse());
     }
 
 }
