1a234c44a22d33df020f8cbe7782fa41cff82ab2
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 09:48:32 +0900

    [feat] add 구현

--- src/test/java/com/jaeyeonling/calculate/StringCalculator.java
+++ src/test/java/com/jaeyeonling/calculate/StringCalculator.java
@@ -1,10 +1,18 @@
 package com.jaeyeonling.calculate;
 
+import java.util.Arrays;
+
 final class StringCalculator {
 
     private StringCalculator() { }
 
-    static int add(String expression) {
-        return 0;
+    static int add(final String expression) {
+        if (expression.isEmpty()) {
+            return 0;
+        }
+
+        return Arrays.stream(expression.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
