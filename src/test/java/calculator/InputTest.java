12bba19ccdd67023bd65763f49c1c73991534cd4
KIM JUNGHWAN <writer0713@naver.com>
2019-06-21 15:33:20 +0900

    #step1 [refactor] split 테스트를 Input을 사용해서 하도록 수정
    기존에 StringCalculator.split을 테스트 했었지만, 이제 이 부분은 Input 객체가 맡게 되어 테스트 위치를 옮기고 Input.split을 사용하도록 수정

--- src/test/java/calculator/InputTest.java
+++ src/test/java/calculator/InputTest.java
@@ -1,11 +1,19 @@
 package calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class InputTest {
+
+    private String[] result;
+
+    @BeforeEach
+    void setUp() {
+        result = new String[]{"1", "2"};
+    }
 
     @Test
     void isEmpty() {
@@ -14,5 +22,26 @@ public class InputTest {
 
         Input input2 = new Input(null);
         assertThat(input2.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("사용자 입력값 split (with comma)")
+    void split() {
+        Input input = new Input("1,2");
+        assertThat(input.split()).containsExactly(result);
+    }
+
+    @Test
+    @DisplayName("사용자 입력값 split (with colon)")
+    void split2() {
+        Input input = new Input("1:2");
+        assertThat(input.split()).containsExactly(result);
+    }
+
+    @Test
+    @DisplayName("사용자 입력값 split (with custom delimiter")
+    void split3() {
+        Input input = new Input("//;\n1;2");
+        assertThat(input.split()).containsExactly(result);
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -26,33 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 public class StringCalculatorTest {
 
-    private String[] result;
-
-    @BeforeEach
-    void setUp() {
-        result = new String[]{"1", "2"};
-    }
-
-    @Test
-    @DisplayName("사용자 입력값 split (with comma)")
-    void split() {
-        assertThat(StringCalculator.split(new Input("1,2"))).containsExactly(result);
-    }
-
-    @Test
-    @DisplayName("사용자 입력값 split (with colon)")
-    void split2() {
-        Input input = new Input("1:2");
-        assertThat(StringCalculator.split(input)).containsExactly(result);
-    }
-
-    @Test
-    @DisplayName("사용자 입력값 split (with custom delimiter")
-    void split3() {
-        Input input = new Input("//;\n1;2");
-        assertThat(StringCalculator.split(input)).containsExactly(result);
-    }
-
     @Test
     @DisplayName("add 구현")
     void add() {
