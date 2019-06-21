faf488031f54772233ef658a373fc8567d04520b
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 09:47:13 +0900

    [test] ParameterizedTest 추가

--- src/test/java/com/jaeyeonling/calculate/StringCalculatorTest.java
+++ src/test/java/com/jaeyeonling/calculate/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package com.jaeyeonling.calculate;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +18,7 @@ class StringCalculatorTest {
     }
 
     @DisplayName("쉼표를 또는 콜론을 구분자로 가지는 문자열 입력 시 합을 반환한다.")
+    @ParameterizedTest
     @CsvSource({
             "'1,2',3"
     })
