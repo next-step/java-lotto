c3f90ec51c45d66a50153d5b7eadd50550280c24
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 10:42:56 +0900

    [refactor] 상수 분리

--- src/main/java/com/jaeyeonling/calculate/ExpressionParser.java
+++ src/main/java/com/jaeyeonling/calculate/ExpressionParser.java
@@ -2,9 +2,12 @@ package com.jaeyeonling.calculate;
 
 public final class ExpressionParser {
 
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String NEW_LINE = "\n";
+
     public static String parseExpressionStatement(final String expression) {
-        if (expression.startsWith("//")) {
-            return expression.split("\n")[1];
+        if (expression.startsWith(CUSTOM_PREFIX)) {
+            return expression.split(NEW_LINE)[1];
         }
 
         return expression;
