929e796b923233a563837473da0144593d4af382
daioooor36 <pd6016@naver.com>
2019-06-21 12:18:19 +0900

    test(StringAddCalculator) : 테스트코드 최초 생성

---
+++ src/main/java/step1/iksoo/StringAddCalculator/StringAddCalculator.java
@@ -0,0 +1,12 @@
+package step1.iksoo.StringAddCalculator;
+
+public class StringAddCalculator {
+
+	public int add(String text) {
+		if(text == null || text.isEmpty()) {
+			return 0;	
+		}
+		
+		return 1;
+	}
+}
