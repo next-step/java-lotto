ba2c85a4699c9b6ab7b6fba8e017bebddb70a292
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 12:47:16 +0900

    [test] StringUtils null 이나 빈 값 입력 시 true 테스트 추가

---
+++ src/main/java/com/jaeyeonling/calculate/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.jaeyeonling.calculate.utils;
+
+public final class StringUtils {
+
+    private StringUtils() { }
+
+    public static boolean isNullOrBlank(final String source) {
+        return false;
+    }
+}
