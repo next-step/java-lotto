71224c8ff91d590df876bbb702df32e14d274fe7
daioooor36 <pd6016@naver.com>
2019-06-21 14:58:35 +0900

    feat(calculator) : 문자열덧셈계산 기능 구현

--- src/main/java/step1/iksoo/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/step1/iksoo/StringAddCalculator/StringAddCalculator.java
@@ -1,12 +1,60 @@
 package step1.iksoo.StringAddCalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-	public int add(String text) {
-		if(text == null || text.isEmpty()) {
-			return 0;	
+	private String text;
+
+	public void StrngAddCalculator(String text) {
+		this.text = text;
+	}
+
+	public int add(String text) throws Exception {
+		if (text == null || text.isEmpty()) {
+			return 0;
 		}
-		
-		return 1;
+
+		this.text = text;
+
+		if (this.text.contains("//") && this.text.contains("\n")) {
+			return addStringCustom();
+		}
+
+		return addString();
+	}
+
+	private int addString() throws Exception {
+		String[] tokens = this.text.split(",|:");
+		isStringMinus(tokens);
+		return addStringArray(tokens);
+	}
+
+	private void isStringMinus(String[] tokens) throws Exception {
+		for (int i = 0; i < tokens.length; i++) {
+			if (Integer.parseInt(tokens[i]) < 0)
+				throw new IllegalArgumentException();
+		}
+
+	}
+
+	private int addStringArray(String[] tokens) {
+		int sumTokens = 0;
+		for (int i = 0; i < tokens.length; i++) {
+			sumTokens += Integer.parseInt(tokens[i]);
+		}
+		return sumTokens;
+	}
+
+	private int addStringCustom() {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(this.text);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			String[] tokens = m.group(2).split(customDelimiter);
+			// 덧셈 구현
+			return addStringArray(tokens);
+		}
+		return -1;
 	}
 }
--- src/test/java/step1/iksoo/StringAddCalculator/StringAddCalculatorTest.java
+++ src/test/java/step1/iksoo/StringAddCalculator/StringAddCalculatorTest.java
@@ -1,6 +1,7 @@
 package step1.iksoo.StringAddCalculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,32 @@ public class StringAddCalculatorTest {
 	@Test
 	public void add_null_또는_빈문자() throws Exception {
 		assertThat(cal.add(null)).isEqualTo(0);
+	}
+
+	@Test
+	public void add_숫자하나() throws Exception {
+		assertThat(cal.add("1")).isEqualTo(1);
+	}
+
+	@Test
+	public void add_쉼표구분자() throws Exception {
+		assertThat(cal.add("1,2")).isEqualTo(3);
+	}
+
+	@Test
+	public void add_쉼표_또는_콜론_구분자() throws Exception {
+		assertThat(cal.add("1,2:3")).isEqualTo(6);
+	}
+
+	@Test
+	public void add_custom_구분자() throws Exception {
+		assertThat(cal.add("//;\n1;2;3")).isEqualTo(6);
+	}
+
+	@Test
+	public void add_negative() throws Exception {
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			cal.add("-1,2,3");
+		});
 	}
 }
