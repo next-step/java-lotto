d02e9947d9cf4fd54b6061a1eb5847c2473c2037
KIM JUNGHWAN <writer0713@naver.com>
2019-06-21 15:20:03 +0900

    #step1 [test] Input Wrapper 객체 생성 및 Test 추가 (test fail)

---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Input {
+
+    private String input;
+
+    public Input(String input) {
+        this.input = input;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ src/test/java/calculator/InputTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class InputTest {
+
+    @Test
+    void isEmpty() {
+        Input input1 = new Input("");
+        assertThat(input1.isEmpty()).isTrue();
+    }
+}
