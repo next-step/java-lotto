8613a53c99d087993be31a093259c15f9f7b45ce
권병윤 <sksggg123@gmail.com>
2019-06-22 02:16:09 +0900

    [test] : 공백인지 체크하는 테스트

---
+++ src/main/java/edu/nextstep/calculate/ValidationUtils.java
@@ -0,0 +1,14 @@
+package edu.nextstep.calculate;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-lotto
+ * create date  : 2019-06-22 02:13
+ */
+public class ValidationUtils {
+}
---
+++ src/test/java/edu/nextstep/calculate/ValidationUtilsTest.java
@@ -0,0 +1,28 @@
+package edu.nextstep.calculate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-lotto
+ * create date  : 2019-06-22 02:13
+ */
+class ValidationUtilsTest {
+
+    @DisplayName("문자열의 유효성체크 (공백)")
+    @Test
+    void isSpace() {
+
+        assertThat(ValidationUtils.isSpace("")).isTrue();
+    }
+
+
+}
