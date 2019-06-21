f35ecdf59b6ac099604c8a830d89bf7d157d0219
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 10:11:55 +0900

    [refactor] separator 생성자로 보내기

--- src/main/java/com/jaeyeonling/calculate/Expression.java
+++ src/main/java/com/jaeyeonling/calculate/Expression.java
@@ -6,14 +6,24 @@ public class Expression {
 
     private static final String SEPARATOR = ",|:";
 
+    private final String separator;
     private final String expression;
 
-    private Expression(final String expression) {
+    private Expression(final String separator, final String expression) {
+        this.separator = separator;
         this.expression = expression;
     }
 
     public static Expression of(final String expression) {
-        return new Expression(expression);
+        if (!expression.startsWith("//")) {
+            return new Expression(SEPARATOR, expression);
+        }
+
+        final String[] splitExpression = expression.split("\n");
+        final String separator = splitExpression[0].substring(2);
+        final String value = splitExpression[1];
+
+        return new Expression(separator, value);
     }
 
     public int execute() {
@@ -21,17 +31,7 @@ public class Expression {
             return 0;
         }
 
-        if (expression.startsWith("//")) {
-            final String[] splitExpression = expression.split("\n");
-            final String separator = splitExpression[0].substring(2);
-            final String value = splitExpression[1];
-
-            return Arrays.stream(value.split(separator))
-                    .mapToInt(Integer::parseInt)
-                    .sum();
-        }
-
-        return Arrays.stream(expression.split(SEPARATOR))
+        return Arrays.stream(expression.split(separator))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
