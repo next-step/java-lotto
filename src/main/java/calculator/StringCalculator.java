6656895581e09a7ba173e878a7dc7588bbe951be
KIM JUNGHWAN <writer0713@naver.com>
2019-06-21 15:09:19 +0900

    #step1 [refactor] finally 블록 제거

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,6 +7,8 @@ import java.util.stream.Stream;
 public class StringCalculator {
 
     private static String DEFAULT_DELIMITER = "[,:]";
+    private static String NOT_A_NUMBER_EXCEPTION_MESSAGE = "숫자가 아닙니다.";;
+    private static String CANNOT_USE_NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "음수는 넣을수 없습니다.";
 
     static boolean isEmpty(String input) {
         return null == input || "".equals(input.trim());
@@ -26,10 +28,10 @@ public class StringCalculator {
         try {
             result = Integer.valueOf(stringNumber);
         } catch (NumberFormatException e) {
-            throw new RuntimeException("숫자가 아닙니다.");
-        } finally {
-            if (result < 0) throw new RuntimeException("음수는 넣을수 없습니다.");
+            throw new RuntimeException(NOT_A_NUMBER_EXCEPTION_MESSAGE);
         }
+
+        if (result < 0) throw new RuntimeException(CANNOT_USE_NEGATIVE_NUMBER_EXCEPTION_MESSAGE);
         return result;
     }
 
