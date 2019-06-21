4f8764f93aacf685f2d3def32fd7dc55bf1826f0
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 10:42:14 +0900

    [refactor] Expression 필요없는 코드 제거

--- src/main/java/com/jaeyeonling/calculate/Expression.java
+++ src/main/java/com/jaeyeonling/calculate/Expression.java
@@ -4,28 +4,16 @@ import java.util.Arrays;
 
 public class Expression {
 
-    private static final String SEPARATOR = ",|:";
-    private static final String CUSTOM_PREFIX = "//";
-    private static final String NEW_LINE = "\n";
-
-    private final String separator;
+    private final ExpressionSeparator separator;
     private final String expression;
 
-    private Expression(final String separator, final String expression) {
-        this.separator = separator;
-        this.expression = expression;
+    private Expression(final String expression) {
+        this.separator = ExpressionSeparator.of(expression);
+        this.expression = ExpressionParser.parseExpressionStatement(expression);
     }
 
     public static Expression of(final String expression) {
-        if (!expression.startsWith(CUSTOM_PREFIX)) {
-            return new Expression(SEPARATOR, expression);
-        }
-
-        final String[] splitExpression = expression.split(NEW_LINE);
-        final String separator = splitExpression[0].substring(CUSTOM_PREFIX.length());
-        final String value = splitExpression[1];
-
-        return new Expression(separator, value);
+        return new Expression(expression);
     }
 
     public int execute() {
@@ -33,7 +21,7 @@ public class Expression {
             return 0;
         }
 
-        return Arrays.stream(expression.split(separator))
+        return Arrays.stream(separator.separate(expression))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
