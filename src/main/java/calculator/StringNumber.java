cfe2afff35a94468926dff6b69913d584749d796
KIM JUNGHWAN <writer0713@naver.com>
2019-06-21 15:40:52 +0900

    #step1 [test] parse 메소드 테스트 추가 (test fail)

--- src/main/java/calculator/StringNumber.java
+++ src/main/java/calculator/StringNumber.java
@@ -1,4 +1,14 @@
 package calculator;
 
 public class StringNumber {
+
+    private final String stringNumber;
+
+    public StringNumber(String stringNumber) {
+        this.stringNumber = stringNumber;
+    }
+
+    public int parse() {
+        return 0;
+    }
 }
--- src/test/java/calculator/StringNumberTest.java
+++ src/test/java/calculator/StringNumberTest.java
@@ -1,7 +1,15 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 class StringNumberTest {
+
+    @Test
+    void parse() {
+        StringNumber number = new StringNumber("1");
+        assertThat(number.parse()).isEqualTo(1);
+    }
 
 }
