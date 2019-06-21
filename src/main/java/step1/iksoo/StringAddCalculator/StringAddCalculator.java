b25cf8300237f645c1729b32a97862758b785a5a
Iksoo Shin <pd6016@naver.com>
2019-06-22 00:42:06 +0900

    fix(Calculator) : 메소드 간 호출관계 수정 및 음수체크 로직 변

--- src/main/java/step1/iksoo/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/step1/iksoo/StringAddCalculator/StringAddCalculator.java
@@ -6,69 +6,56 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
     private String text;
 
-    public void StrngAddCalculator(String text) {
+    public int add(String text) {
         this.text = text;
-    }
-
-    public int add(String text) throws Exception {
-        this.text = text;
-
-        if (isEmpty()) {
+        if (isNotAddable(this.text)) {
             return 0;
         }
 
-        if (isCustomDelimiter()) {
-            return addStringCustom();
-        }
-
-        return addString();
+        return addString(this.text);
     }
 
-    private boolean isEmpty() {
-        if (this.text == null || this.text.isEmpty()) {
+    private boolean isNotAddable(String text) {
+        if (text == null || text.isEmpty() || isContainsMinus(text)) {
             return true;
         }
         return false;
     }
 
-    private boolean isCustomDelimiter() {
-        if (this.text.contains("//") && this.text.contains("\n")) {
-            return true;
-        }
-        return false;
-    }
-
-    private int addStringCustom() {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(this.text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
-            return addStringArray(tokens);
-        }
-        return -1;
-    }
-
-    private int addString() throws Exception {
-        String[] tokens = this.text.split(",|:");
-        if (isStringMinus(tokens)) {
-            throw new IllegalArgumentException();
-        }
-        return addStringArray(tokens);
-    }
-
-    private boolean isStringMinus(String[] tokens) throws Exception {
+    private boolean isContainsMinus(String text) {
+        String[] tokens = makeToken(text);
+        boolean isMinus = false;
         for (int i = 0; i < tokens.length; i++) {
-            if (Integer.parseInt(tokens[i]) < 0)
-                return true;
+            isMinus = isMinus || Integer.parseInt(tokens[i]) < 0 ? true : false;
+        }
+        return isIllegalArgument(isMinus);
+    }
+
+    private boolean isIllegalArgument(boolean isMinus) {
+        if (isMinus) {
+            throw new IllegalArgumentException("마이너스는 입력할 수 없어요.");
         }
         return false;
     }
 
-    private int addStringArray(String[] tokens) {
+    private int addString(String text) {
+        String[] tokens = makeToken(text);
+
         int sumTokens = 0;
         for (int i = 0; i < tokens.length; i++) {
             sumTokens += Integer.parseInt(tokens[i]);
         }
+
         return sumTokens;
+    }
+
+    private String[] makeToken(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+            return tokens;
+        }
+        return text.split(",|:");
     }
 }
--- src/main/java/step1/iksoo/StringAddCalculator/StringAddMain.java
+++ src/main/java/step1/iksoo/StringAddCalculator/StringAddMain.java
@@ -3,8 +3,8 @@ package step1.iksoo.StringAddCalculator;
 public class StringAddMain {
     static StringAddCalculator stringAddCalculator = new StringAddCalculator();
 
-    public static void main(String[] args) throws Exception {
-        int addResult = stringAddCalculator.add("8,-1");
+    public static void main(String[] args) {
+        int addResult = stringAddCalculator.add("2,5,6,88,3,4");
         Output.printTextln(addResult);
     }
 }
