617e2676ff93692eeafcd26dd86cc1a55b815f57
sogoagain <imyong0@gmail.com>
2019-06-22 02:18:24 +0900

    feat(커스텀구분자계산): 커스텀구분자로 결합된 입력값의 합을 구하는 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,27 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
-    public static final String REGEX_OF_DEFAULT_SEPARATOR = "[,:]";
+    private static final String REGEX_OF_DEFAULT_SEPARATOR = "[,:]";
+    private static final String REGEX_OF_CUSTOM_SEPARATOR = "//(.)\n(.*)";
 
     public static int calculate(String input) {
-        String[] elementsOninput = input.split(REGEX_OF_DEFAULT_SEPARATOR);
+        String[] elementsOninput = splitBySeparator(input);
         return sum(elementsOninput);
+    }
+
+    private static String[] splitBySeparator(String input) {
+        String[] elementsOninput = input.split(REGEX_OF_DEFAULT_SEPARATOR);
+
+        Matcher matcher = Pattern.compile(REGEX_OF_CUSTOM_SEPARATOR).matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            elementsOninput = matcher.group(2).split(customDelimiter);
+        }
+        return elementsOninput;
     }
 
     private static int sum(String[] elementsOninput) {
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -14,8 +14,20 @@ public class CalculatorTest {
             "'1,2:3', 6",
             "'15:25,35', 75"
     })
-    @DisplayName("기본 구분자를 이용 0을 포함한 자연수들의 합을 계산한다.")
+    @DisplayName("기본 구분자로 결합된 0을 포함한 자연수들의 합을 계산한다.")
     void sum_NaturalNumbers_WithDefaultSeparators_Calculated(String input, int expectedResult) {
+        int calculatedValue = Calculator.calculate(input);
+        assertThat(calculatedValue).isEqualTo(expectedResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//a\n1a2a3', 6",
+            "'//!\n10!20!30', 60",
+            "'//;\n15;25;35', 75"
+    })
+    @DisplayName("커스텀 구분자로 결합된 0을 포함한 자연수들의 합을 계산한다.")
+    void sum_NaturalNumbers_WithCustomSeparators_Calculated(String input, int expectedResult) {
         int calculatedValue = Calculator.calculate(input);
         assertThat(calculatedValue).isEqualTo(expectedResult);
     }
