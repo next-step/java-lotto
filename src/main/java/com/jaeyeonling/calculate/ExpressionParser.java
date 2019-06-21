321aa04d957c19a8664fc770a674140adc54f258
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 10:40:12 +0900

    [feat] 커스텀 계산식에서 순수한 계산식만 가져온다. 테스트 통과

--- src/main/java/com/jaeyeonling/calculate/ExpressionParser.java
+++ src/main/java/com/jaeyeonling/calculate/ExpressionParser.java
@@ -3,6 +3,10 @@ package com.jaeyeonling.calculate;
 public final class ExpressionParser {
 
     public static String parseExpressionStatement(final String expression) {
+        if (expression.startsWith("//")) {
+            return expression.split("\n")[1];
+        }
+
         return expression;
     }
 }
