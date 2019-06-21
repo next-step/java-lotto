1327e03a3d9d1e20f046df4b3e318c9374ce6e64
Juyoung <juy5790@outlook.com>
2019-06-22 03:18:39 +0900

    docs(calculator): Expression 구현

---
+++ src/main/java/calculator/Expression.java
@@ -0,0 +1,52 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Expression {
+
+    private static final Pattern CUSTOM_PATTERN_DELIMITER = Pattern.compile("//(.)\n(.*)");
+    private static final String PREFIX_CUSTOM_HAS_DELIMITER = "//";
+    static final String DEFAULT_DELIMITER = "[:,]";
+
+    private String delimiter;
+    private String tokens;
+
+    public Expression(String delimiter, String tokens) {
+        this.delimiter = delimiter;
+        this.tokens = tokens;
+    }
+
+    public static Expression from(String inputOfString) {
+        if (hasCustomDelimiter(inputOfString)) {
+            Matcher customMatcher = CUSTOM_PATTERN_DELIMITER.matcher(inputOfString);
+            if (customMatcher.find()) {
+                return new Expression(customMatcher.group(1), customMatcher.group(2));
+            }
+        }
+        return new Expression(DEFAULT_DELIMITER, inputOfString);
+    }
+
+    private static boolean hasCustomDelimiter(String inputOfString) {
+        return inputOfString.startsWith(PREFIX_CUSTOM_HAS_DELIMITER);
+    }
+
+    public void checkTokens() {
+        Pattern compile = Pattern.compile("^\\d+((" + delimiter + ")\\d+)*$");
+        if(!compile.matcher(tokens).matches()){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public String getTokens() {
+        return tokens;
+    }
+
+    public String[] numbers() {
+        return tokens.split(delimiter);
+    }
+}
---
+++ src/test/java/calculator/ExpressionTest.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static calculator.Expression.DEFAULT_DELIMITER;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExpressionTest {
+
+
+    @Test
+    void createExpressionByDefaultExpression() {
+        String inputOfString = "1,2,3";
+        Expression result =  Expression.from(inputOfString);
+        assertThat(result).isNotNull();
+        assertThat(result.getDelimiter()).isEqualTo(DEFAULT_DELIMITER);
+        assertThat(result.getTokens()).isEqualTo(inputOfString);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//;\n1;2;3', ';', '1;2;3'",
+            "'//a\n1a2a3', 'a', '1a2a3'"
+    })
+    void sumByInputWithCustomDelimiter(String inputOfEmpty, String delimiter, String token) {
+        Expression expression =  Expression.from(inputOfEmpty);
+        assertThat(expression.getDelimiter()).isEqualTo(delimiter);
+        assertThat(expression.getTokens()).isEqualTo(token);
+    }
+
+    @Test
+    void createExpressionByDefaultExpression2() {
+        String delimiter = ",";
+        String expression = "1,2,3";
+        Expression result = new Expression(delimiter, expression);
+        assertThat(result).isNotNull();
+        assertThat(result.getDelimiter()).isEqualTo(delimiter);
+        assertThat(result.getTokens()).isEqualTo(expression);
+    }
+}
