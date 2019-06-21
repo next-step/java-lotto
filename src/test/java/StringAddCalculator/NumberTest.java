52b05abf6051a2c2e230347f645c902e6c05c069
sunkyung <sunkyungch@gmail.com>
2019-06-22 00:19:06 +0900

    refactor(StringAddCalculator) 테스트 정리

--- src/test/java/StringAddCalculator/NumberTest.java
+++ src/test/java/StringAddCalculator/NumberTest.java
@@ -2,8 +2,6 @@ package StringAddCalculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIf;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberTest {
@@ -12,12 +10,6 @@ public class NumberTest {
     @Test
     @DisplayName("더하기")
     public void numberTest() {
-        Number five = new Number(5);
-        Number two = new Number(2);
-        Number seven = five.add(two);
-
-        assertThat(seven.getNumber()).isEqualTo(7);
-
         Number six = new Number(6);
         Number three = new Number(3);
         Number nine = six.add(three);
@@ -25,11 +17,9 @@ public class NumberTest {
         assertThat(nine).isEqualTo(new Number(9));
     }
 
-
     @Test
     @DisplayName("같은 Number인지 확인")
     public void equality() {
         assertThat(new Number(5)).isEqualTo(new Number(5));
     }
-
 }
