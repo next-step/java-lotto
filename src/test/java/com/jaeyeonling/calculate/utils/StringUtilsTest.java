7acd0ebad4107ab7784c45d1d475226ef62f5f69
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-21 12:50:07 +0900

    [test] StringUtils 값 입력 시 false 테스트 추가

--- src/test/java/com/jaeyeonling/calculate/utils/StringUtilsTest.java
+++ src/test/java/com/jaeyeonling/calculate/utils/StringUtilsTest.java
@@ -3,6 +3,7 @@ package com.jaeyeonling.calculate.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,4 +16,16 @@ class StringUtilsTest {
         final boolean isNullOrBlank = StringUtils.isNullOrBlank(source);
         assertThat(isNullOrBlank).isTrue();
     }
+
+    @DisplayName("값 입력 시 false")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "asdasds",
+            "sadasd!!!"
+    })
+    void should_return_false_when_value(final String source) {
+        final boolean isNullOrBlank = StringUtils.isNullOrBlank(source);
+        assertThat(isNullOrBlank).isFalse();
+    }
+
 }
