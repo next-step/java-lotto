dd90b598e099d6cefda62a1c9012d39baa036869
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 18:00:59 +0900

    [test] ParameterizedTest 적용

--- src/test/java/com/jaeyeonling/lotto/domain/LottoGameReportTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoGameReportTest.java
@@ -2,6 +2,8 @@ package com.jaeyeonling.lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,10 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoGameReportTest {
 
     @DisplayName("상금 별 당첨 횟수 반환")
-    @Test
-    void should_return_matchCount_by_prize() {
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1,
+            2,
+            100,
+            5000
+    })
+    void should_return_matchCount_by_prize(final int matchCount) {
         // given
-        final int matchCount = 1;
         final Map<LottoPrize, Integer> matchCountByRank = new HashMap<>();
         matchCountByRank.put(LottoPrize.JACKPOT, matchCount);
 
