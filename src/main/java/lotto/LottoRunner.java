0eb8a5330c00e6411a955da150d1dce9c9c939a0
KIM JUNGHWAN <writer0713@naver.com>
2019-06-22 15:12:05 +0900

    #step2 [test] 구입금액 물어보는 test 추가 (test fail)

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -3,6 +3,8 @@ package lotto;
 public class LottoRunner {
 
   public static void main(String[] args) {
-    System.out.println("lotto runner");
+
+    int amount = LottoInputView.askPurchaseAmount();
+    System.out.println(amount > 0);
   }
 }
