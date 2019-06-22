f6baf3e6153ae83501bbffba2b9b4e33b31567ae
1nteger <rhep0820@gmail.com>
2019-06-23 00:50:54 +0900

    [step1] refactor : stream을 사용하여 두 숫자의 합 반환

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -1,20 +1,22 @@
 package step1;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     private static final int ZERO = 0;
-    private static final String SEPARATOR_COMMA = ",";
+    private static final String DEFAULT_SEPARATOR = ",";
 
     public int calculate(String inputString) {
         if (inputString == null || "".equals(inputString)) {
             return ZERO;
         }
-        if (!inputString.contains(",")) {
+        if (!inputString.contains(DEFAULT_SEPARATOR)) {
             return Integer.parseInt(inputString);
         }
-        int calculations = ZERO;
-        for (String element : inputString.split(SEPARATOR_COMMA)) {
-            calculations += Integer.parseInt(element);
-        }
-        return calculations;
+        return Arrays
+                .stream(inputString.split(DEFAULT_SEPARATOR))
+                .map(Integer::parseInt)
+                .reduce((i1, i2) -> i1 + i2)
+                .get();
     }
 }
