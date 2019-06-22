48b33f4cf77972a54ea75242b451fe81c78c00dc
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 19:04:45 +0900

    [test] 로또 정보를 분석 후 수익률 확인 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/domain/LottoAnalyzerTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoAnalyzerTest.java
@@ -68,4 +68,36 @@ class LottoAnalyzerTest {
         // then
         assertThat(totalPrizeMoney).isEqualTo(expect);
     }
+
+
+    @DisplayName("로또 정보를 분석 후 수익률 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1,
+            2,
+            5
+    })
+    void should_return_returnOnInvestment_by_lottoAnalyzer(final int analyzeCount) {
+        // given
+        final Set<LottoNumber> lottoNumbers = new HashSet<>();
+        for (int lottoNumber = Env.MIN_LOTTO_NUMBER; lottoNumber <= Env.COUNT_OF_LOTTO_NUMBER_IN_LOTTO; lottoNumber++) {
+            lottoNumbers.add(new LottoNumber(lottoNumber));
+        }
+        final Lotto lotto = new Lotto(lottoNumbers);
+        final LottoAnalyzer lottoAnalyzer = new LottoAnalyzer(lotto);
+
+        final int totalBuyingMoney = analyzeCount * Env.PRICE_OF_LOTTO;
+
+        // when
+        for (int i = 0; i < analyzeCount; i++) {
+            lottoAnalyzer.analyze(lotto);
+        }
+        final LottoGameReport report = lottoAnalyzer.getReport();
+        final Money returnOnInvestment = report.getReturnOnInvestment();
+
+        final Money expect = new Money(LottoPrize.JACKPOT.getPrizeMoney() * analyzeCount / totalBuyingMoney);
+
+        // then
+        assertThat(returnOnInvestment).isEqualTo(expect);
+    }
 }
