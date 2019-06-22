eae0d862613c063c71c84a2dcf0887240a46bfa4
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 14:56:42 +0900

    [test] 로또 게임 구매 갯수 확인

--- src/test/java/com/jaeyeonling/lotto/domain/LottoGameTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoGameTest.java
@@ -3,6 +3,8 @@ package com.jaeyeonling.lotto.domain;
 import com.jaeyeonling.lotto.config.Env;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.List;
@@ -31,5 +33,26 @@ class LottoGameTest {
 
         // then
         assertThat(lottos).containsExactly(lotto);
+    }
+
+    @DisplayName("로또 게임 구매 갯수 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1,
+            10,
+            300,
+            400
+    })
+    void should_equals_lottoCount_when_buy(final int buyCount) {
+        // given
+        final LottoGame lottoGame = new LottoGame();
+
+        final Money money = new Money(Env.PRICE_OF_LOTTO * buyCount);
+
+        // when
+        final List<Lotto> lottos = lottoGame.buy(money);
+
+        // then
+        assertThat(lottos.size()).isEqualTo(buyCount);
     }
 }
