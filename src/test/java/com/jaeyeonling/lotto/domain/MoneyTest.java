c230869c8d89928db466b1129da2d041b9467f30
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 13:56:43 +0900

    [test] Money 로 잔액보다 더 비싼 물건 구매 시 예외처리 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/domain/MoneyTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/MoneyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class MoneyTest {
 
@@ -33,9 +34,30 @@ class MoneyTest {
 
         final Dealable item = () -> price;
 
+        // when
         money.buy(item);
 
         // then
         assertThat(money).isEqualTo(new Money(balanceValue - priceValue));
+    }
+
+    @DisplayName("Money 로 잔액보다 더 비싼 물건 구매 시 예외처리")
+    @Test
+    void should_throw_ArithmeticException_when_exceedingAmount() {
+
+        // given
+        final int balanceValue = 100;
+        final int priceValue = 1000;
+
+        final Money money = new Money(balanceValue);
+        final Money price = new Money(priceValue);
+
+        final Dealable item = () -> price;
+
+        // when / then
+        assertThatExceptionOfType(ArithmeticException.class)
+                .isThrownBy(() -> {
+                    money.buy(item);
+                });
     }
 }
