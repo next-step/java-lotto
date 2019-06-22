4df57549103748e2d716c474d58ad9fd835e6667
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 11:28:10 +0900

    [test] LottoNumber 최소 값 보다 낮은 값 생성 시 예외처리 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/LottoNumberTest.java
+++ src/test/java/com/jaeyeonling/lotto/LottoNumberTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoNumberTest {
 
@@ -20,5 +21,13 @@ class LottoNumberTest {
     void should_return_lottoNumber_when_maxLottoNumber() {
         LottoNumber lottoNumber = new LottoNumber(Env.MAX_LOTTO_NUMBER);
         assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("LottoNumber 최소 값(" + Env.MIN_LOTTO_NUMBER + ") 보다 낮은 값 생성 시 예외처리")
+    @Test
+    void should_throw_illegalArgumentException_when_shorterThanMin() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new LottoNumber(Env.MIN_LOTTO_NUMBER - 1);
+        });
     }
 }
