69c78b2fb24e7bb3a5438923455c28324aec1b85
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 18:45:56 +0900

    [test] 로또 정보를 분석 후 당첨 횟수 확인 테스트 추가

--- src/main/java/com/jaeyeonling/lotto/domain/LottoAnalyzer.java
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoAnalyzer.java
@@ -2,4 +2,17 @@ package com.jaeyeonling.lotto.domain;
 
 public class LottoAnalyzer {
 
+    private final Lotto winningLotto;
+
+    public LottoAnalyzer(final Lotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+    public LottoGameReport getReport() {
+        return null;
+    }
+
+    public void analyze(final Lotto lotto) {
+
+    }
 }
--- src/test/java/com/jaeyeonling/lotto/domain/LottoAnalyzerTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoAnalyzerTest.java
@@ -1,17 +1,42 @@
 package com.jaeyeonling.lotto.domain;
 
+import com.jaeyeonling.lotto.config.Env;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoAnalyzerTest {
 
-    @DisplayName("생성")
-    @Test
-    void create() {
-        final LottoAnalyzer lottoAnalyzer = new LottoAnalyzer();
+    @DisplayName("로또 정보를 분석 후 당첨 횟수 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1,
+            4,
+            5
+    })
+    void create(final int analyzeCount) {
+        // given
+        final Set<LottoNumber> lottoNumbers = new HashSet<>();
+        for (int lottoNumber = Env.MIN_LOTTO_NUMBER; lottoNumber <= Env.COUNT_OF_LOTTO_NUMBER_IN_LOTTO; lottoNumber++) {
+            lottoNumbers.add(new LottoNumber(lottoNumber));
+        }
+        final Lotto lotto = new Lotto(lottoNumbers);
+        final LottoAnalyzer lottoAnalyzer = new LottoAnalyzer(lotto);
 
-        assertThat(lottoAnalyzer).isNotNull();
+        // when
+        for (int i = 0; i < analyzeCount; i++) {
+            lottoAnalyzer.analyze(lotto);
+        }
+        final LottoGameReport report = lottoAnalyzer.getReport();
+        final int matchCount = report.getMatchCount(LottoPrize.JACKPOT);
+
+        // then
+        assertThat(matchCount).isEqualTo(analyzeCount);
     }
 }
