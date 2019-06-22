19f2915354340250ef60caf144448723dd89c9d3
1nteger <rhep0820@gmail.com>
2019-06-22 20:32:04 +0900

    [step1] test : 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환 테스트

--- README.md
+++ README.md
@@ -14,10 +14,10 @@
 - [ ] 빈 문자열 또는 null 값을 입력할 경우 0을 반환
     - [x] TEST FAIL
     - [x] TEST PASS
-    - [ ] REFACTORING
+    - [x] REFACTORING
 - [ ] 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
-    - [ ] TEST FAIL
-    - [ ] TEST PASS
+    - [x] TEST FAIL
+    - [x] TEST PASS
     - [ ] REFACTORING
 - [ ] 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환
     - [ ] TEST FAIL
--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package step1;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,5 +16,15 @@ public class StringCalculatorTest {
         //when
         //Then
         assertThat(calculator.calculate(inputString)).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "1", "2", "3"})
+    void 숫자_하나를_문자열로_입력할_경우_해당_숫자를_반환한다(String inputString) {
+        //given
+        StringCalculator calculator = new StringCalculator();
+        //when
+        //Then
+        assertThat(calculator.calculate(inputString)).isEqualTo(Integer.parseInt(inputString));
     }
 }
