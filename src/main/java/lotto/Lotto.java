93f445d988791892a869a126eabc23969c716bc7
KIM JUNGHWAN <writer0713@naver.com>
2019-06-22 16:00:28 +0900

    #step2 [feature] Lotto 생성시 6개의 random number를 가지도록 한다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Lotto {
 
-  public Lotto() {}
+  private final List<Integer> pickedNumbers;
+
+  public Lotto() {
+    this.pickedNumbers = this.pickNumbers();
+  }
+
+  private static List<Integer> pickNumbers() {
+    return new Random().ints(1, 46)
+            .distinct()
+            .limit(6)
+            .boxed()
+            .collect(Collectors.toList());
+  }
+
 
 }
