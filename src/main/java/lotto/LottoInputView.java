3ac87a65f97133d4163ceb06f980bd62e78ee8b2
KIM JUNGHWAN <writer0713@naver.com>
2019-06-22 15:17:26 +0900

    #step2 [feature] 구입금액에 숫자가 아닌 값 할 경우 처리

--- src/main/java/lotto/LottoInputView.java
+++ src/main/java/lotto/LottoInputView.java
@@ -1,17 +1,22 @@
 package lotto;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LottoInputView {
 
   private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+  private static final String NOT_A_NUMBER_INPUT_ERROR_MESSAGE = "숫자로 제대로 입력해주세요.";
 
   public static int askPurchaseAmount() {
     System.out.println(PURCHASE_AMOUNT_MESSAGE);
 
-    Scanner scanner = new Scanner(System.in);
-    int purchaseAmount = scanner.nextInt();
-
-    return purchaseAmount;
+    try {
+      Scanner scanner = new Scanner(System.in);
+      int purchaseAmount = scanner.nextInt();
+      return purchaseAmount;
+    } catch(InputMismatchException e) {
+      throw new IllegalArgumentException(NOT_A_NUMBER_INPUT_ERROR_MESSAGE);
+    }
   }
 }
