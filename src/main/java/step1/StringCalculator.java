b7b8a4694e2a2b8039a65b9ae124fcef71d1a6e6
1nteger <rhep0820@gmail.com>
2019-06-22 20:53:49 +0900

    [step1] refactor : @CsvSource 사용, 숫자 대신 상수 사용

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -1,10 +1,11 @@
 package step1;
 
 public class StringCalculator {
+    private static final int ZERO = 0;
 
     public int calculate(String inputString) {
         if (inputString == null || "".equals(inputString)) {
-            return 0;
+            return ZERO;
         }
         return Integer.parseInt(inputString);
     }
--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package step1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,23 +10,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
 
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
     @ParameterizedTest
     @NullAndEmptySource
     void 빈_문자열_또는_null_값을_입력할_경우_0을_반환한다(String inputString) {
         //given
-        StringCalculator calculator = new StringCalculator();
         //when
         //Then
         assertThat(calculator.calculate(inputString)).isEqualTo(0);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"-1", "0", "1", "2", "3"})
-    void 숫자_하나를_문자열로_입력할_경우_해당_숫자를_반환한다(String inputString) {
+    @CsvSource({"-1,-1", "0,0", "1,1"})
+    void 숫자_하나를_문자열로_입력할_경우_해당_숫자를_반환한다(String inputString, int expectedResult) {
         //given
-        StringCalculator calculator = new StringCalculator();
         //when
         //Then
-        assertThat(calculator.calculate(inputString)).isEqualTo(Integer.parseInt(inputString));
+        assertThat(calculator.calculate(inputString)).isEqualTo(expectedResult);
     }
 }
