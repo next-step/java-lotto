1fc4b306833548d4c53d2eae5447de700f42b449
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 11:29:13 +0900

    [feat] LottoNumber 최소 값 보다 낮은 값 생성 시 예외처리 구현

--- src/main/java/com/jaeyeonling/lotto/LottoNumber.java
+++ src/main/java/com/jaeyeonling/lotto/LottoNumber.java
@@ -1,10 +1,16 @@
 package com.jaeyeonling.lotto;
 
+import com.jaeyeonling.lotto.config.Env;
+
 public class LottoNumber {
 
     private final int number;
 
     LottoNumber(final int number) {
+        if (number < Env.MIN_LOTTO_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+        
         this.number = number;
     }
 }
