09f6f895f22551d8afad92e4066302be1789f084
KIM JUNGHWAN <writer0713@naver.com>
2019-06-22 16:41:18 +0900

    #step2 [feature] 구입한 로또들의 번호 출력 (test success)

--- src/main/java/lotto/LottoOutputView.java
+++ src/main/java/lotto/LottoOutputView.java
@@ -1,8 +1,14 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoOutputView {
   public static void print(int amount) {
     String msg = amount + "개를 구매했습니다.";
     System.out.println(msg);
+  }
+
+  public static void print(List<Lotto> lottos) {
+    lottos.forEach(System.out::println);
   }
 }
