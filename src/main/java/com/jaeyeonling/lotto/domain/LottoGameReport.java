d710f3dd94ca17a640310dc4f3ef21c8e11905b4
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 18:00:12 +0900

    [test] 상금 별 당첨 횟수 반환 테스트 추가

--- src/main/java/com/jaeyeonling/lotto/domain/LottoGameReport.java
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoGameReport.java
@@ -1,5 +1,16 @@
 package com.jaeyeonling.lotto.domain;
 
+import java.util.Map;
+
 public class LottoGameReport {
 
+    private final Map<LottoPrize, Integer> matchCountByLottoPrize;
+
+    public LottoGameReport(final Map<LottoPrize, Integer> matchCountByLottoPrize) {
+        this.matchCountByLottoPrize = matchCountByLottoPrize;
+    }
+
+    public int getMatchCount(final LottoPrize prize) {
+        return matchCountByLottoPrize.getOrDefault(prize, 0);
+    }
 }
--- src/test/java/com/jaeyeonling/lotto/domain/LottoGameReportTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoGameReportTest.java
@@ -3,16 +3,28 @@ package com.jaeyeonling.lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoGameReportTest {
 
-    @DisplayName("생성")
+    @DisplayName("상금 별 당첨 횟수 반환")
     @Test
-    void create() {
-        final LottoGameReport lottoGameReport = new LottoGameReport();
+    void should_return_matchCount_by_prize() {
+        // given
+        final int matchCount = 1;
+        final Map<LottoPrize, Integer> matchCountByRank = new HashMap<>();
+        matchCountByRank.put(LottoPrize.JACKPOT, matchCount);
 
-        assertThat(lottoGameReport).isNotNull();
+        final LottoGameReport lottoGameReport = new LottoGameReport(matchCountByRank);
+
+        // when
+        final int target = lottoGameReport.getMatchCount(LottoPrize.JACKPOT);
+
+        // then
+        assertThat(target).isEqualTo(matchCount);
     }
 
 }
