cdb313d734112388f3e298b6b504a75eb0bf0b24
wyparks2 <wyparks2@mrblue.com>
2019-06-22 16:28:17 +0900

    feat: 숫자 하나를 문자열로 입력할경우 해당 숫자를 그대로 반환

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,9 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,7 +21,15 @@ public class CalculatorTest {
         assertThat(calculator.add(input)).isEqualTo(0);
     }
 
-    // TODO 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "10", "100", "0"})
+    void add_숫자_하나를_문자열로_입력할경우_해당숫자를_반환(String input) {
+        Calculator calculator = new Calculator();
+        int expected = Integer.valueOf(input);
+
+        assertThat(calculator.add(input)).isEqualTo(expected);
+    }
+
 
     // TODO 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.
 
