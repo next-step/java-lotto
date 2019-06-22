f0e5b4fa007a43337024b467e823e2e3bc9d82b3
sunkyung <sunkyungch@gmail.com>
2019-06-22 12:12:03 +0900

    feature(StringSplitter) 커스텀 구분자를 이용한 문자열 분할

--- src/test/java/StringAddCalculator/StringSplitterTest.java
+++ src/test/java/StringAddCalculator/StringSplitterTest.java
@@ -9,8 +9,22 @@ public class StringSplitterTest {
 
     @Test
     @DisplayName("(,) 로 숫자를 나눈다")
-    public void split() {
+    public void splitComma() {
          String[] numbers = StringSplitter.split("1,2,3");
          assertThat(numbers).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    @DisplayName("(:) 으로 숫자를 나눈다")
+    public void splitColon() {
+        String[] numbers = StringSplitter.split("4:5:6:7");
+        assertThat(numbers).containsExactly("4", "5", "6", "7");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 숫자를 나눈다")
+    public void splitCustom() {
+        String[] numbers = StringSplitter.split("//=\n8=9=10");
+        assertThat(numbers).containsExactly("8", "9", "10");
     }
 }
