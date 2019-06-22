7fec2ce6b08ca8786a8916b6d2c522e319e16576
sunkyung <sunkyungch@gmail.com>
2019-06-22 11:37:01 +0900

    refactor(StringNumber) 불필요한 변수 제거

--- src/main/java/StringAddCalculator/StringNumber.java
+++ src/main/java/StringAddCalculator/StringNumber.java
@@ -4,12 +4,10 @@ import java.util.Objects;
 
 public class StringNumber {
 
-    private final String StringNumber;
     private int intNumber;
 
     public StringNumber(String number) {
         this.intNumber = convertInt(number);
-        this.StringNumber = number;
     }
 
     public StringNumber add(StringNumber addedStringNumber) {
@@ -25,23 +23,24 @@ public class StringNumber {
         return Integer.parseInt(number);
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         StringNumber that = (StringNumber) o;
-        return Objects.equals(StringNumber, that.StringNumber);
+        return intNumber == that.intNumber;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(StringNumber);
+        return Objects.hash(intNumber);
     }
 
     @Override
     public String toString() {
         return "StringNumber{" +
-                "number='" + StringNumber + '\'' +
+                "intNumber=" + intNumber +
                 '}';
     }
 }
