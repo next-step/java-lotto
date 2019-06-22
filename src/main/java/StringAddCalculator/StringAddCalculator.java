1b61cbb732ec6bd7225a9c74aa7d52c793c02cf3
sunkyung <sunkyungch@gmail.com>
2019-06-22 14:13:25 +0900

    refactor(StringAddCalculator)

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -2,21 +2,19 @@ package StringAddCalculator;
 
 public class StringAddCalculator {
 
-
     public static int add(String formula) {
         if(isNullOrEmpty(formula)) return 0;
-        return addtionCalculate(formula);
+        return additionCalculate(formula);
     }
 
     private static boolean isNullOrEmpty(String input) {
         return input == null || input.isEmpty();
     }
 
-
-    private static int addtionCalculate(String input) {
-        String[] numbers = StringSplitter.split(input);
-        StringNumbers stringNumbers = new StringNumbers(numbers);
+    private static int additionCalculate(String input) {
+        StringNumbers stringNumbers = new StringNumbers(StringSplitter.split(input));
         StringNumber resultNumber = stringNumbers.sum();
         return resultNumber.toInt();
+//        return new StringNumbers(StringSplitter.split(input)).sum().toInt(); << 이렇게도 가능 할 듯.
     }
 }
