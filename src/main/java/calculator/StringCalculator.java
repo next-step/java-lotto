7c56a8e43a98b8c043b038d934a31e7881a29426
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-22 20:50:35 +0900

    refactor(StringCalculator): extract magic number to const

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,17 @@ package calculator;
 import java.util.stream.Stream;
 
 public class StringCalculator {
+
+    private static final String EMPTY_STRING = "";
+    private static final String REGEX_SPLIT_STRING = ":|,";
+    private static final int ZERO = 0;
+
     public int splitAndSum(String string) {
-        if (string == null | string == "") {
-            return 0;
+
+        if (string == null | string.equals(EMPTY_STRING)) {
+            return ZERO;
         }
 
-        return Stream.of(string.split(":|,")).mapToInt(Integer::parseInt).sum();
+        return Stream.of(string.split(REGEX_SPLIT_STRING)).mapToInt(Integer::parseInt).sum();
     }
 }
