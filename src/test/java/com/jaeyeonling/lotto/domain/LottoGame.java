d1c8443218d260fe98c86c15c94e2543803f47a8
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 13:25:03 +0900

    [test] 로또 구매 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/domain/LottoGame.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoGame.java
@@ -1,7 +1,10 @@
 package com.jaeyeonling.lotto.domain;
 
+import java.util.List;
+
 public class LottoGame {
 
-
-
+    public List<Lotto> buy() {
+        return null;
+    }
 }
--- src/test/java/com/jaeyeonling/lotto/domain/LottoGameTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoGameTest.java
@@ -1,17 +1,27 @@
 package com.jaeyeonling.lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoGameTest {
 
-    @DisplayName("생성")
-    @Test
-    void create() {
-        LottoGame lottoGame = new LottoGame();
+    private LottoGame lottoGame;
 
-        assertThat(lottoGame).isNotNull();
+    @BeforeEach
+    void setUp() {
+        this.lottoGame = new LottoGame();
+    }
+
+    @DisplayName("로또 구매 테스트")
+    @Test
+    void should_return_lottos_when_buy() {
+        final List<Lotto> lottos = lottoGame.buy();
+
+        assertThat(lottos).isNotNull();
     }
 }
