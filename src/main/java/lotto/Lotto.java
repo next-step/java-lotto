fb8d3ac58a1b7e115a5723eb1580ebefeb1444c2
KIM JUNGHWAN <writer0713@naver.com>
2019-06-22 16:37:37 +0900

    #step2 [refactor] Lotto
    - 숫자 generate 하는 부분을 제거 
    - numbers를 생성자로 받아서 생성하도록 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,27 +1,17 @@
 package lotto;
 
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Lotto {
 
-  private final List<Integer> pickedNumbers;
+  private final List<Integer> generatedNumbers;
 
-  public Lotto() {
-    this.pickedNumbers = this.pickNumbers();
-  }
-
-  private static List<Integer> pickNumbers() {
-    return new Random().ints(1, 46)
-            .distinct()
-            .limit(6)
-            .boxed()
-            .collect(Collectors.toList());
+  public Lotto(List<Integer> generatedNumbers) {
+    this.generatedNumbers = generatedNumbers;
   }
 
   @Override
   public String toString() {
-    return pickedNumbers.toString();
+    return generatedNumbers.toString();
   }
 }
