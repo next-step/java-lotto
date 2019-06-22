fe792397113c0c1b4a60dd31a7e252577d9abf9e
wyparks2 <wyparks2@mrblue.com>
2019-06-22 16:17:20 +0900

    feat: Calculator 클래스 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-20
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class Calculator {
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-20
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class CalculatorTest {
+    @Test
+    void create() {
+        Calculator calculator = new Calculator();
+    }
+}
