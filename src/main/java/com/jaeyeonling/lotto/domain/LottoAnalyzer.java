1d60712d95dd1f5346610ef592b76cee4b5bc9cf
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 18:49:48 +0900

    [feat] 로또 정보를 분석 후 당첨 횟수 확인 구현

--- src/main/java/com/jaeyeonling/lotto/domain/LottoAnalyzer.java
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoAnalyzer.java
@@ -1,18 +1,29 @@
 package com.jaeyeonling.lotto.domain;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class LottoAnalyzer {
 
+    private final Map<LottoPrize, Integer> matchCountByLottoPrize = new ConcurrentHashMap<>();
     private final Lotto winningLotto;
 
     public LottoAnalyzer(final Lotto winningLotto) {
         this.winningLotto = winningLotto;
     }
 
-    public LottoGameReport getReport() {
-        return null;
+    public void analyze(final Lotto lotto) {
+        final int countOfMatch = winningLotto.countOfMatch(lotto);
+        final LottoPrize prize = LottoPrize.valueOf(countOfMatch);
+
+        incrementMatchCount(prize);
     }
 
-    public void analyze(final Lotto lotto) {
+    private void incrementMatchCount(final LottoPrize prize) {
+        matchCountByLottoPrize.put(prize, matchCountByLottoPrize.getOrDefault(prize, 0) + 1);
+    }
 
+    public LottoGameReport getReport() {
+        return new LottoGameReport(matchCountByLottoPrize);
     }
 }
