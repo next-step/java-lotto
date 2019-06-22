7f4f847ff1187058466e8ddd74ecc62c3e34b2d4
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 19:24:20 +0900

    [feat] InputView OutputView 추가

---
+++ src/main/java/com/jaeyeonling/lotto/view/ConsoleInputView.java
@@ -0,0 +1,60 @@
+package com.jaeyeonling.lotto.view;
+
+import com.jaeyeonling.lotto.config.Env;
+import com.jaeyeonling.lotto.domain.Lotto;
+import com.jaeyeonling.lotto.domain.LottoNumber;
+import com.jaeyeonling.lotto.domain.Money;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public final class ConsoleInputView {
+
+    private static final Scanner CONSOLE = new Scanner(System.in);
+
+    private ConsoleInputView() { }
+
+    public static Money readMoney() {
+        final int moneyValue = readIntWithMessage(Env.READ_MONEY_MESSAGE);
+        return new Money(moneyValue);
+    }
+
+    public static Lotto readWinningLotto() {
+        final String winningLottoValue = readStringWithMessage(Env.READ_WINNING_LOTTO_NUMBER_MESSAGE);
+
+        final Set<LottoNumber> lottoNumbers = Arrays.stream(splitWinningLottoNumber(winningLottoValue))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet());
+
+        return new Lotto(lottoNumbers);
+    }
+
+    private static String[] splitWinningLottoNumber(final String winningLottoNumber) {
+        return winningLottoNumber.split(Env.WINNING_LOTTO_NUMBER_SEPARATOR);
+    }
+
+    private static String readStringWithMessage(final String message) {
+        ConsoleOutputView.print(message);
+        return readString();
+    }
+
+    private static String readString() {
+        return CONSOLE.nextLine();
+    }
+
+    private static int readIntWithMessage(final String message) {
+        ConsoleOutputView.print(message);
+        return readInt();
+    }
+
+    private static int readInt() {
+        final int readValue = CONSOLE.nextInt();
+        CONSOLE.nextLine();
+
+        return readValue;
+    }
+}
---
+++ src/main/java/com/jaeyeonling/lotto/view/ConsoleOutputView.java
@@ -0,0 +1,57 @@
+package com.jaeyeonling.lotto.view;
+
+import com.jaeyeonling.lotto.config.Env;
+import com.jaeyeonling.lotto.domain.Lotto;
+import com.jaeyeonling.lotto.domain.LottoGameReport;
+import com.jaeyeonling.lotto.domain.LottoPrize;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+public final class ConsoleOutputView {
+
+    private static final PrintStream CONSOLE = System.out;
+
+    private ConsoleOutputView() { }
+
+    public static void printBuyingLotto(final List<Lotto> lottos) {
+        print(String.format(Env.BUYING_ALERT_MESSAGE, lottos.size()));
+        lottos.forEach(ConsoleOutputView::print);
+    }
+
+    public static void printReport(final LottoGameReport report) {
+        printWinningReportHeader();
+        printWinningReportMatchTemplate(report);
+        printWinningReportFooter(report);
+    }
+
+    private static void printWinningReportMatchTemplate(final LottoGameReport report) {
+        Arrays.stream(LottoPrize.values())
+                .forEach(prize -> printWinningReportMatchTemplate(prize, report));
+    }
+
+    private static void printWinningReportMatchTemplate(final LottoPrize prize,
+                                                        final LottoGameReport report) {
+        final String message = String.format(Env.WINNING_REPORT_MATCH_TEMPLATE,
+                prize.getCountOfMatch(), prize.getPrizeMoney(), report.getMatchCount(prize));
+
+        ConsoleOutputView.print(message);
+    }
+
+    public static void print(final Object message) {
+        CONSOLE.println(message);
+    }
+
+    public static void newline() {
+        CONSOLE.println();
+    }
+
+    private static void printWinningReportHeader() {
+        ConsoleOutputView.print(Env.WINNING_REPORT_HEADER);
+    }
+
+    private static void printWinningReportFooter(final LottoGameReport report) {
+        ConsoleOutputView.print(String.format(Env.WINNING_REPORT_FOOTER, report.getReturnOnInvestment()));
+    }
+}
