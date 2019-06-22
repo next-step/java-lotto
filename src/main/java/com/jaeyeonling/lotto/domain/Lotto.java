8c477c4c005f43bfc2aea5b53daba28d9c7ad3cc
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 12:00:30 +0900

    [feat] 로또 생성 시 로또 번호가 원하는 값 보다 적을 시 예외처리 구현

--- src/main/java/com/jaeyeonling/lotto/domain/Lotto.java
+++ src/main/java/com/jaeyeonling/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
 package com.jaeyeonling.lotto.domain;
 
+import com.jaeyeonling.lotto.config.Env;
+import com.jaeyeonling.lotto.exception.InvalidCountOfLottoNumberException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -9,6 +12,10 @@ public class Lotto {
     private final List<LottoNumber> lottoNumbers;
 
     Lotto(final Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != Env.COUNT_OF_LOTTO_NUMBER_IN_LOTTO) {
+            throw new InvalidCountOfLottoNumberException();
+        }
+
         this.lottoNumbers = new ArrayList<>(lottoNumbers);
     }
 }
