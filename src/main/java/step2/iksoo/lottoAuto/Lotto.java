c175b76f8b2a1e92d925e67975dfc2371c99c16b
Iksoo Shin <pd6016@naver.com>
2019-06-22 23:16:33 +0900

    fix(Lotto) : equals, hashCode 메소드 추가

--- src/main/java/step2/iksoo/lottoAuto/Lotto.java
+++ src/main/java/step2/iksoo/lottoAuto/Lotto.java
@@ -2,6 +2,7 @@ package step2.iksoo.lottoAuto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
     private final int LOTTO_SIZE = 6;
@@ -19,5 +20,19 @@ public class Lotto {
 
     public boolean isNumSize(int numSize) {
         return this.lottery.size() == numSize;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return LOTTO_SIZE == lotto.LOTTO_SIZE &&
+                Objects.equals(lottery, lotto.lottery);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(LOTTO_SIZE, lottery);
     }
 }
