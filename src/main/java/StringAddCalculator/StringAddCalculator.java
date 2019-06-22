c7fd78cac9fed43ef101317ee04e4212094f81d3
sunkyung <sunkyungch@gmail.com>
2019-06-22 14:08:06 +0900

    feature(StringAddCalculator) 문자열을 입력받아 값 계산 기능 구현

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -1,6 +1,22 @@
 package StringAddCalculator;
 
-import java.util.Arrays;
-
 public class StringAddCalculator {
+
+
+    public static int add(String formula) {
+        if(isNullOrEmpty(formula)) return 0;
+        return addtionCalculate(formula);
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+
+    private static int addtionCalculate(String input) {
+        String[] numbers = StringSplitter.split(input);
+        StringNumbers stringNumbers = new StringNumbers(numbers);
+        StringNumber resultNumber = stringNumbers.sum();
+        return resultNumber.toInt();
+    }
 }
