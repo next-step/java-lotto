8a8600f397c16d6d7542f7797e003a97e0730a23
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-22 20:44:41 +0900

    feat(StringCalculator ): return result when input string with colon

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import java.util.stream.Stream;
+
 public class StringCalculator {
     public int splitAndSum(String string) {
-        return 0;
+        if (string == null | string == "") {
+            return 0;
+        }
+
+        return Stream.of(string.split(":")).mapToInt(Integer::parseInt).sum();
     }
 }
