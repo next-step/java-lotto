8f912eb8221024dab27f1966784233724f4f935f
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 13:35:55 +0900

    [test] 로또 게임 구매 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/domain/LottoGameTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoGameTest.java
@@ -1,27 +1,33 @@
 package com.jaeyeonling.lotto.domain;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.jaeyeonling.lotto.config.Env;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoGameTest {
 
-    private LottoGame lottoGame;
-
-    @BeforeEach
-    void setUp() {
-        this.lottoGame = new LottoGame();
-    }
-
-    @DisplayName("로또 구매 테스트")
+    @DisplayName("로또 게임 구매 ")
     @Test
     void should_return_lottos_when_buy() {
+        // given
+        final Set<LottoNumber> lottoNumbers = new HashSet<>();
+        for (int lottoNumber = 1; lottoNumber <= Env.COUNT_OF_LOTTO_NUMBER_IN_LOTTO; lottoNumber++) {
+            lottoNumbers.add(new LottoNumber(lottoNumber));
+        }
+
+        final Lotto lotto = new Lotto(lottoNumbers);
+        final LottoGame lottoGame = new LottoGame(() -> lotto);
+
+        // when
         final List<Lotto> lottos = lottoGame.buy();
 
-        assertThat(lottos).isNotNull();
+        // then
+        assertThat(lottos).containsExactly(lotto);
     }
 }
