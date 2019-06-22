bceccea1956b2a55aad8d32b40975a68723ab362
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 13:50:24 +0900

    [test] Money로 물건 구매 시 잔액 차감 확인 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/domain/MoneyTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/MoneyTest.java
@@ -20,4 +20,22 @@ class MoneyTest {
         // then
         assertThat(money).isEqualTo(expect);
     }
+
+    @DisplayName("Money 로 물건 구매 시 잔액 차감 확인")
+    @Test
+    void should_earn_money_when_buy_item() {
+        // given
+        final int balanceValue = 1000;
+        final int priceValue = 100;
+
+        final Money money = new Money(balanceValue);
+        final Money price = new Money(priceValue);
+
+        final Dealable item = () -> price;
+
+        money.buy(item);
+
+        // then
+        assertThat(money).isEqualTo(new Money(balanceValue - priceValue));
+    }
 }
