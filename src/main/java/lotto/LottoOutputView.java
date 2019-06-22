d9c690c2d10cc70f87b32c806c8e198596d4af74
KIM JUNGHWAN <writer0713@naver.com>
2019-06-22 15:38:37 +0900

    #step2 [feature] 로또 구매한 개수 출력

---
+++ src/main/java/lotto/LottoOutputView.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class LottoOutputView {
+  public static void print(int amount) {
+    String msg = amount + "개를 구매했습니다.";
+    System.out.println(msg);
+  }
+}
--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoRunner {
 
   public static void main(String[] args) {
@@ -7,5 +9,9 @@ public class LottoRunner {
     int paid = LottoInputView.askPurchaseAmount();
     System.out.println(paid > 0);
 
+    List<Lotto> lottos = LottoGenerator.generate(paid);
+    int amount = lottos.size();
+
+    LottoOutputView.print(amount);
   }
 }
