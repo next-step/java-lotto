33963792e45d4788f2d51c23884b8d73f7fad644
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-22 14:13:09 +0900

    [test] Moeny 테스트 코드 ParameterizeTest 적용

--- src/test/java/com/jaeyeonling/lotto/domain/MoneyTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/MoneyTest.java
@@ -2,7 +2,9 @@ package com.jaeyeonling.lotto.domain;
 
 import com.jaeyeonling.lotto.exception.LowMoneyException;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -10,11 +12,14 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 class MoneyTest {
 
     @DisplayName("Money 잔액 확인")
-    @Test
-    void should_return_balance() {
-        // given
-        final int balanceValue = 1000;
-
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1_000,
+            20_000,
+            500_000,
+            1_000_000
+    })
+    void should_return_balance(final int balanceValue) {
         // when
         final Money money = new Money(balanceValue);
         final Money expect = new Money(balanceValue);
@@ -24,12 +29,16 @@ class MoneyTest {
     }
 
     @DisplayName("Money 로 물건 구매 시 잔액 차감 확인")
-    @Test
-    void should_earn_money_when_buy_item() {
+    @ParameterizedTest
+    @CsvSource({
+            "1000,100",
+            "1000,1000",
+            "500,500",
+            "10000000,10",
+    })
+    void should_earn_money_when_buy_item(final int balanceValue,
+                                         final int priceValue) {
         // given
-        final int balanceValue = 1000;
-        final int priceValue = 100;
-
         final Money money = new Money(balanceValue);
         final Money price = new Money(priceValue);
 
@@ -43,13 +52,16 @@ class MoneyTest {
     }
 
     @DisplayName("Money 로 잔액보다 더 비싼 물건 구매 시 예외처리")
-    @Test
-    void should_throw_ArithmeticException_when_exceedingAmount() {
-
+    @ParameterizedTest
+    @CsvSource({
+            "100,1000",
+            "1000,10000",
+            "500,5000",
+            "1,10000000",
+    })
+    void should_throw_ArithmeticException_when_exceedingAmount(final int balanceValue,
+                                                               final int priceValue) {
         // given
-        final int balanceValue = 100;
-        final int priceValue = 1000;
-
         final Money money = new Money(balanceValue);
         final Money price = new Money(priceValue);
 
