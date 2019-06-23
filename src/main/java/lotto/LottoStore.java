004d68f3f0755f2d58424a26a97544eec38d3b6d
pch8388 <pch838811@gmail.com>
2019-06-23 22:35:48 +0900

    feat : 음수나 0 이 입력되면 예외 발생

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -2,7 +2,23 @@ package lotto;
 
 public class LottoStore {
 
-  public static int buyLottoCount(String price) {
-    return Integer.parseInt(price) / 1000;
+  private static final int LOTTO_PRICE = 1000;
+
+  public static int buyLottoCount(final String input) {
+    final int price = validateInput(input);
+    if (price % LOTTO_PRICE != 0) {
+      throw new IllegalArgumentException(LOTTO_PRICE + "원 단위로 입력해야 합니다.");
+    }
+    return validateInput(input) / LOTTO_PRICE;
   }
+
+  private static int validateInput(final String input) {
+    final int price = Integer.parseInt(input);
+    if (price <= 0) {
+      throw new IllegalArgumentException("음수 또는 0을 입력할 수 없습니다. : " + input);
+    }
+    return price;
+  }
+
+
 }
