460fe7054b7870eafde1952da7d82de03daf0846
pch8388 <pch838811@gmail.com>
2019-06-24 00:11:45 +0900

    test : 같은 숫자 중복되면 예외 발생(red)

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -11,9 +11,15 @@ public class LottoTicketTest {
 
   @DisplayName("로또 번호는 6개만 가능")
   @Test
-
   void validateNumbers() {
     assertThatIllegalArgumentException()
         .isThrownBy(() -> new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+  }
+
+  @DisplayName("같은 숫자가 중복되지 않아야 함")
+  @Test
+  void validateDuplicate() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> new LottoTicket(Arrays.asList(1, 2, 2, 3, 4, 5)));
   }
 }
