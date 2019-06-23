56340cf920b023b5540ac568d2227a79a3a902d3
1nteger <rhep0820@gmail.com>
2019-06-24 01:11:05 +0900

    [step2] refactor : 매직넘버 대신 상수 사용, 로또번호 범위 예외처리 테스트

--- src/main/java/step2/LottoNumber.java
+++ src/main/java/step2/LottoNumber.java
@@ -1,11 +1,12 @@
 package step2;
 
 public class LottoNumber {
+    public static final String ALERT_OUT_OF_RANGE_OF_LOTTO_NUMBER = "로또번호는 1부터 45까지의 자연수만 가능합니다.";
     private int number;
 
     private LottoNumber(int number) {
         if (isOutOfRange(number)) {
-            throw new IllegalArgumentException("로또번호는 1부터 45까지의 자연수만 가능합니다.");
+            throw new IllegalArgumentException(ALERT_OUT_OF_RANGE_OF_LOTTO_NUMBER);
         }
         this.number = number;
     }
--- src/test/java/step2/LottoNumberTest.java
+++ src/test/java/step2/LottoNumberTest.java
@@ -1,8 +1,11 @@
 package step2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LottoNumberTest {
     @Test
@@ -14,5 +17,17 @@ public class LottoNumberTest {
         //When
         //Then
         assertThat(lottoNumber.getNumber()).isEqualTo(39);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    void 로또번호가_1보다_작고_45_보다_크면_예외가_발생한다(int num) {
+        //Given
+        //When
+        //Then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    LottoNumber.from(num);
+                }).withMessageContaining(LottoNumber.ALERT_OUT_OF_RANGE_OF_LOTTO_NUMBER);
     }
 }
