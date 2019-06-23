192c44640c575229b1e1e659fa201eac9de55f38
SangHyoun Yun <hainco@gmail.com>
2019-06-23 18:08:05 +0900

    refactor(calculator): Delimiter 에 equlas 및 hashCode 추가

--- src/main/java/Delimiter.java
+++ src/main/java/Delimiter.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Delimiter {
     private String delimiter;
 
@@ -7,5 +9,18 @@ public class Delimiter {
 
     public String getDelimiter() {
         return delimiter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Delimiter delimiter1 = (Delimiter) o;
+        return Objects.equals(delimiter, delimiter1.delimiter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(delimiter);
     }
 }
