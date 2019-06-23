44750f169e535ed1b322dade52e2fe72d0e77802
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-23 15:33:35 +0900

    [feature] money test 추가, 기능 구현

--- src/test/java/step2/LottoStoreTest.java
+++ src/test/java/step2/LottoStoreTest.java
@@ -11,7 +11,7 @@ public class LottoStoreTest {
     @Test
     void lotto() {
         LottoStore lottoStore = new LottoStore();
-        List<Lotto> lottos = lottoStore.buyLotto(new Money(14000));
+        List<Lotto> lottos = lottoStore.buyLotto(new Money(14000L));
         assertThat(lottos.size()).isEqualTo(0);
     }
 }
