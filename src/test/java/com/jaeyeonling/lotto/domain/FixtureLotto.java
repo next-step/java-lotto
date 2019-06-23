d81bf28442f02274ed72e0d71f844e5f6070f55c
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-23 22:16:48 +0900

    [test] fixture 여러 경우 사용할 수 있도록 수정

--- src/test/java/com/jaeyeonling/lotto/domain/FixtureLotto.java
+++ src/test/java/com/jaeyeonling/lotto/domain/FixtureLotto.java
@@ -3,6 +3,10 @@ package com.jaeyeonling.lotto.domain;
 public class FixtureLotto extends Lotto {
 
     FixtureLotto() {
-        super(new FixtureLottoNumbers());
+        super(new FixtureLottoNumbers(COUNT_OF_LOTTO_NUMBER));
+    }
+
+    FixtureLotto(final int startLottoNumber) {
+        super(new FixtureLottoNumbers(startLottoNumber, COUNT_OF_LOTTO_NUMBER));
     }
 }
