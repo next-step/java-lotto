e6c182473653f641c1ac767f177fefd52e5dca55
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-23 16:38:11 +0900

    [feature] LottoStore 예외 케이스 추가

--- src/main/java/step2/LottoStore.java
+++ src/main/java/step2/LottoStore.java
@@ -1,9 +1,31 @@
 package step2;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoStore {
-    public List<Lotto> buyLotto(Money money) {
-        return List.of();
+    public static final Money LOTTO_PRICE = new Money(1000L);
+
+    public List<Lotto> buyLotto(final Money money) {
+        validateMoney(money);
+        final int quantity = getLottoQuantity(money);
+        return makeLottos(quantity);
+    }
+
+    private void validateMoney(final Money money) {
+        if (money.getMoney() < LOTTO_PRICE.getMoney()) {
+            throw new IllegalArgumentException("로또는 한 개당 " + LOTTO_PRICE.getMoney() + "원 입니다.");
+        }
+    }
+
+    private int getLottoQuantity(final Money money) {
+        return (int) (money.getMoney() / LOTTO_PRICE.getMoney());
+    }
+
+    private List<Lotto> makeLottos(final int quantity) {
+        return IntStream.range(0, quantity)
+                        .mapToObj(i -> Lotto.create())
+                        .collect(Collectors.toList());
     }
 }
--- src/test/java/step2/LottoStoreTest.java
+++ src/test/java/step2/LottoStoreTest.java
@@ -5,13 +5,20 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoStoreTest {
+
+    @Test
+    void less_money() {
+        LottoStore lottoStore = new LottoStore();
+        assertThatIllegalArgumentException().isThrownBy(() -> lottoStore.buyLotto(new Money(500L)));
+    }
 
     @Test
     void lotto() {
         LottoStore lottoStore = new LottoStore();
         List<Lotto> lottos = lottoStore.buyLotto(new Money(14000L));
-        assertThat(lottos.size()).isEqualTo(0);
+        assertThat(lottos.size()).isEqualTo(14);
     }
 }
