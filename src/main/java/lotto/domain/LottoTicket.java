bf92b8e570c7f5bf0555fb1ed5b4d19a74b6bf06
pch8388 <pch838811@gmail.com>
2019-06-24 01:39:30 +0900

    refactor : 접근제어자 변경

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class LottoTicket {
 
-  public static final int LOTTO_NUMBER_SIZE = 6;
+  private static final int LOTTO_NUMBER_SIZE = 6;
   private final List<Integer> numbers;
 
   public LottoTicket(List<Integer> numbers) {
