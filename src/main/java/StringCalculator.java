dbcc942d8374999db84f3fbd6cdd16067cbdd791
Bentley <p2c2kbf@gmail.com>
2019-06-23 13:17:54 +0900

    feat:빈문자열 또는 null값 문자열 처리

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,12 @@
-public class StringCalator {
+import org.assertj.core.util.Strings;
+
+public class StringCalculator {
+
+    public int add(String input) {
+        if(Strings.isNullOrEmpty(input)) {
+            return 0;
+        }
+        return 1;
+    }
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
+    private StringCalculator cal;
+
+    @BeforeEach
+    public void setup() {
+        cal = new StringCalculator();
+    }
+
+    @Test
+    public void add_null_또는_빈문자() throws Exception  {
+        assertThat(cal.add(null)).isEqualTo(0);
+        assertThat(cal.add("")).isEqualTo(0);
+    }
+
+//    @Test
+//    public void add_숫자하나() throws Exception {
+//        int result = StringCalculator.splitAndSum("1");
+//        assertThat(result).isEqualTo(1);
+//    }
+//
+//    @Test
+//    @DisplayName("쉼표 구분자")
+//    public void splitAndSum_쉼표구분자() throws Exception {
+//        int result = StringCalculator.splitAndSum("1,2");
+//        assertThat(result).isEqualTo(1);
+//    }
 }
