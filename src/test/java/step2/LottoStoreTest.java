c886a30091c0b03f0a33fd0a8a79621173d4957d
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-24 01:29:57 +0900

    [test] test DisplayName 추가

--- src/test/java/step2/LottoStoreTest.java
+++ src/test/java/step2/LottoStoreTest.java
@@ -2,6 +2,7 @@ package step2;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,12 +11,14 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class LottoStoreTest {
 
     @Test
+    @DisplayName("로또 1장 금액인 1000원 보다 적은 돈을 넣는 경우")
     void less_money() {
         LottoStore lottoStore = new LottoStore();
         assertThatIllegalArgumentException().isThrownBy(() -> lottoStore.buyLotto(new Money(500L)));
     }
 
     @Test
+    @DisplayName("로또 14장 구입")
     void lotto() {
         LottoStore lottoStore = new LottoStore();
         List<Lotto> lottos = lottoStore.buyLotto(new Money(14000L));
