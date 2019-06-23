d244e143cdb2441e69c72bbefb303a9f789e2d25
Bentley <p2c2kbf@gmail.com>
2019-06-23 15:33:53 +0900

    feat: 음수 입력 처리/ 단위 테스트 추가/ 주석 추가

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,44 +1,65 @@
 import org.assertj.core.util.Strings;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
     private final static int SUM_FAILED = 0;
+    private final String PATTERN = "//(.)\n(.*)";
 
     public int add(String input) throws Exception {
-        if (Strings.isNullOrEmpty(input))
+        if (Strings.isNullOrEmpty(input)) // 빈 문자열과 null 값 처리
             return SUM_FAILED;
 
-        if (input.length() == 1)
+        if (input.length() == 1)          // 숫자 하나 처리
             return Integer.parseInt(input);
 
-        if (input.contains("//"))
+        if (input.contains("//"))         //  cumstom 구분자 처리
             return splitCustomSum(input);
 
-        return splitAndSum(input);
-    }
+        if (input.contains("-"))          // 음수 입력시 Exception 처리
+            throw new RuntimeException("입력값이 잘못되었습니다. 입력값 중에 음수가 있습니다.");
 
+        return splitAndSum(input);       // 구분자 ",", ":" 처리
+    }
+    // 구분자가 ",", ":" 일때 계산하는 함수
     private int splitAndSum(String input) {
         String[] result = input.split(",|:");
         return getSumofArray(result);
     }
-
+    // cumtom 구분자일때 계산하는 함수
     private int splitCustomSum(String input) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        Matcher m = Pattern.compile(PATTERN).matcher(input);
+        System.out.println("Matcher: " +m);
         if (m.find()) {
+            System.out.println("splitCustomSum");
             String customDelimiter = m.group(1);
             String[] tokens = m.group(2).split(customDelimiter);
             return getSumofArray(tokens);
         }
         return SUM_FAILED;
     }
-
+    // 구분자로 split 문자열을 덧셈해주는 함수
     private int getSumofArray(String[] inputs) {
+        System.out.println("getSumofArray");
         int sum = 0;
         for (int i = 0; i < inputs.length; ++i) {
+
             sum += Integer.parseInt(inputs[i]);
         }
         return sum;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("덧셈을 위한 문자열을 입력해주세요.");
+
+        StringCalculator stringCalculator = new StringCalculator();
+        int result = stringCalculator.add(sc.nextLine());
+        System.out.println("실행결과: " + result);
+
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTest {
     private StringCalculator cal;
@@ -13,33 +14,52 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void add_null_또는_빈문자() throws Exception  {
+    void add_null_또는_빈문자() throws Exception  {
         assertThat(cal.add(null)).isEqualTo(0);
         assertThat(cal.add("")).isEqualTo(0);
     }
 
     @Test
-    public void add_숫자하나2() throws Exception {
+    void add_숫자하나() throws Exception {
         assertThat(cal.add("1")).isEqualTo(1);
     }
 
     @Test
     @DisplayName("쉼표 구분자")
-    public void add_쉼표구분자() throws Exception {
+    void add_쉼표구분자() throws Exception {
         int result = cal.add("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     @DisplayName("쉼표_또는_콜론_구분자")
-    public void add_쉼표_또는_콜론_구분자() throws Exception {
+    void add_쉼표_또는_콜론_구분자() throws Exception {
         assertThat(cal.add("1,2:3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("custom_구분자")
-    public void add_custom_구분자() throws Exception {
-        assertThat(cal.add("//;\n1;2;3")).isEqualTo(6);
+    void add_custom_구분자() throws Exception {
+        assertThat(cal.add("//;\\n1;2;4")).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("add_음수값 ")
+    void add_negative() throws Exception {
+        assertThrows(RuntimeException.class, () -> {
+            cal.add("-1,2,3");
+        });
+    }
+
+    @Test
+    void getSumofArray() {
+        String[] inputs ={"1","2","3"};
+        int sum = 0;
+        for (int i = 0; i < inputs.length; ++i) {
+
+            sum += Integer.parseInt(inputs[i]);
+        }
+        assertThat(sum).isEqualTo(6);
     }
 
 
