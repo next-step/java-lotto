e772208808ec5a2e4b8290d339fd12e29ff3cb97
SangHyoun Yun <hainco@gmail.com>
2019-06-23 18:09:37 +0900

    refactor(calculator): calculator 패키지로 이동

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,3 +1,5 @@
+package calculator;
+
 public class Calculator {
     static int add(int num1, int num2) {
         return num1 + num2;
--- src/test/java/calculator/DelimiterTest.java
+++ src/test/java/calculator/DelimiterTest.java
@@ -1,3 +1,5 @@
+package calculator;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DelimiterTest {
     @Test
-    @DisplayName("원하는 String 으로 Delimiter 가 생성된다.")
+    @DisplayName("원하는 String 으로 calculator.Delimiter 가 생성된다.")
     void constructor() {
         Delimiter delimiter = new Delimiter(",");
         assertThat(delimiter.getDelimiter()).isEqualTo(",");
