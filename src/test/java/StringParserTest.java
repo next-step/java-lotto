82f165f6a6e3337190fe103e4644bba226b6acb3
SangHyoun Yun <hainco@gmail.com>
2019-06-23 17:35:20 +0900

    test(calculator): 구분자가 있는 테스트 추가

--- src/test/java/StringParserTest.java
+++ src/test/java/StringParserTest.java
@@ -1,17 +1,34 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringParserTest {
     @Test
-    void 숫자만있는_String_파싱() {
+    @DisplayName("숫자만 있는 스트링을 파싱")
+    void parseByDelimiter() {
         String input = "2";
         assertThat(StringParser.parseByDelimiter(input)).contains("2");
     }
 
     @Test
-    void 구분자와_숫자를_파싱() {
+    @DisplayName("컴마 구분자가 있는 스트링 파싱")
+    void parseByDelimiter2() {
         String input = "1,2";
         assertThat(StringParser.parseByDelimiter(input)).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("콜론 구분자가 있는 스트링 파싱")
+    void parseByDelimiter3() {
+        String input = "1:2";
+        assertThat(StringParser.parseByDelimiter(input)).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("컴마, 콜론 구분자가 있는 스트링 파싱")
+    void parseByDelimiter4() {
+        String input = "1,2,3:4:5";
+        assertThat(StringParser.parseByDelimiter(input)).contains("1", "2", "3", "4", "5");
     }
 }
