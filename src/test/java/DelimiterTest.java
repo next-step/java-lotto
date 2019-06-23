92af63c1dab97a00d50deac625aa50551ad03aea
SangHyoun Yun <hainco@gmail.com>
2019-06-23 18:02:18 +0900

    feat(calculator): Delimiter 클래스 및 테스트 추가

---
+++ src/test/java/DelimiterTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DelimiterTest {
+    @Test
+    @DisplayName("원하는 String 으로 Delimiter 가 생성된다.")
+    void constructor() {
+        Delimiter delimiter = new Delimiter(",");
+        assertThat(delimiter.getDelimiter()).isEqualTo(",");
+    }
+}
