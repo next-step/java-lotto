f556eefb824b748f4abc6959501f93780ed116f1
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-23 15:41:39 +0900

    [refactor] 잘못된 이름 수정

--- src/main/java/com/jaeyeonling/lotto/exception/InvalidCountOfLottoNumberException.java
+++ src/main/java/com/jaeyeonling/lotto/exception/InvalidCountOfLottoNumberException.java
@@ -6,7 +6,7 @@ public class InvalidCountOfLottoNumberException extends IllegalArgumentException
 
     private static final String ERROR_MESSAGE = "Lotto 생성 시 LottoNumber 의 갯수는 %d 개를 만족해야 합니다. (입력 갯수: %d)";
 
-    public InvalidCountOfLottoNumberException(final int inputLottoNumber) {
-        super(String.format(ERROR_MESSAGE, Lotto.COUNT_OF_LOTTO_NUMBER, inputLottoNumber));
+    public InvalidCountOfLottoNumberException(final int inputCountOfLottoNumber) {
+        super(String.format(ERROR_MESSAGE, Lotto.COUNT_OF_LOTTO_NUMBER, inputCountOfLottoNumber));
     }
 }
