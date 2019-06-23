ea81ebcba8e928b161a033910d9d8a15a2f5ec64
SangHyoun Yun <hainco@gmail.com>
2019-06-23 18:47:15 +0900

    refactor(calculator): PositiveNumber 의 equals 및 hashcode 추가

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Objects;
+
 public class PositiveNumber {
     public static final int MIN_NUMBER_INPUT = 0;
     private int number;
@@ -13,5 +15,18 @@ public class PositiveNumber {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositiveNumber that = (PositiveNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
