c939bb74a0f79145261896fc5fbf225b49f0c06a
pch8388 <pch838811@gmail.com>
2019-06-23 22:35:23 +0900

    test : 음수나 0 이 입력되면 예외 발생 (fail)

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -4,13 +4,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoGameTest {
 
   @DisplayName("로또 구입")
   @Test
   void create() {
-    int buyLottoCount = LottoStore.buyLottoCount("14000");
-    assertThat(buyLottoCount).isEqualTo(14);
+    assertThat(LottoStore.buyLottoCount("14000"))
+        .isEqualTo(14);
+  }
+
+  @DisplayName("음수나 0이 입력되면 예외가 발생")
+  @Test
+  void validateInput() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> LottoStore.buyLottoCount("0"));
   }
 }
