1e50b82a3ff8fdb7d66ad56dc6c25a4a8050cd95
pch8388 <pch838811@gmail.com>
2019-06-24 00:20:49 +0900

    test : 1~45 범위의 숫자가 들어오지 않으면 예외 발생(red)

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -22,4 +22,11 @@ public class LottoTicketTest {
     assertThatIllegalArgumentException()
         .isThrownBy(() -> new LottoTicket(Arrays.asList(1, 2, 2, 3, 4, 5)));
   }
+
+  @DisplayName("1~45 사이의 숫자가 아니어야 함")
+  @Test
+  void validateRangeNumber() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> new LottoTicket(Arrays.asList(0, 2, 3, 4, 5, 6)));
+  }
 }
