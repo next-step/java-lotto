eafd8a9beeb190e5a50d5ea211e5f7122a378d49
pch8388 <pch838811@gmail.com>
2019-06-24 00:09:29 +0900

    refactor : 로또 번호 사이즈 검증 메서드 분리

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public class LottoTicket {
 
+  public static final int LOTTO_NUMBER_SIZE = 6;
+  private final List<Integer> numbers;
+
   public LottoTicket(List<Integer> numbers) {
-    if (numbers.size() != 6) {
+    validateNumberSize(numbers);
+    this.numbers = numbers;
+  }
+
+  private void validateNumberSize(List<Integer> numbers) {
+    if (numbers.size() != LOTTO_NUMBER_SIZE) {
       throw new IllegalArgumentException("로또 번호는 6자리여야 합니다.");
     }
   }
