1ffeadecd6409b178fe9107b8ff8f50ec9c45022
Bentley <p2c2kbf@gmail.com>
2019-06-23 13:52:51 +0900

    feat: 숫자 하나/쉼표구분자/ 쉼표 또는 콜론 구분자 처리

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,11 +2,26 @@ import org.assertj.core.util.Strings;
 
 public class StringCalculator {
 
-    public int add(String input) {
-        if(Strings.isNullOrEmpty(input)) {
+    public int add(String input) throws Exception {
+        if (Strings.isNullOrEmpty(input)) {
             return 0;
         }
-        return 1;
+
+        if (input.length() == 1) {
+            return Integer.parseInt(input);
+        }
+
+        return splitAndSum(input);
+    }
+
+    public int splitAndSum(String input) {
+        String[] result = input.split(",|:");
+        int sum = 0;
+
+        for (int i = 0; i < result.length; ++i) {
+            sum+= Integer.parseInt(result[i]);
+        }
+        return sum;
     }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -18,16 +18,23 @@ public class StringCalculatorTest {
         assertThat(cal.add("")).isEqualTo(0);
     }
 
-//    @Test
-//    public void add_숫자하나() throws Exception {
-//        int result = StringCalculator.splitAndSum("1");
-//        assertThat(result).isEqualTo(1);
-//    }
-//
-//    @Test
-//    @DisplayName("쉼표 구분자")
-//    public void splitAndSum_쉼표구분자() throws Exception {
-//        int result = StringCalculator.splitAndSum("1,2");
-//        assertThat(result).isEqualTo(1);
-//    }
+    @Test
+    public void add_숫자하나2() throws Exception {
+        assertThat(cal.add("1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("쉼표 구분자")
+    public void add_쉼표구분자() throws Exception {
+        int result = cal.add("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("쉼표_또는_콜론_구분자")
+    public void add_쉼표_또는_콜론_구분자() throws Exception {
+        assertThat(cal.add("1,2:3")).isEqualTo(6);
+    }
+
+
 }
