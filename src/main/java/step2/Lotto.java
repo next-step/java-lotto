17d8f7e58c84c25a8edea666a331aec0a1c5d171
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-23 15:55:36 +0900

    [feature] lotto.create() 기능 구현

--- src/main/java/step2/Lotto.java
+++ src/main/java/step2/Lotto.java
@@ -1,9 +1,23 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final List<Integer> LOTTO_NUMBERS = new ArrayList<>();
+    private static final Integer LOTTO_MIN_NUMBER = 1;
+    private static final Integer LOTTO_MAX_NUMBER = 49;
+    private static final Integer LOTTO_NUMBER_SIZE = 6;
+
+    static {
+        for (int i = LOTTO_MIN_NUMBER; i <= LOTTO_MAX_NUMBER; i++) {
+            LOTTO_NUMBERS.add(i);
+        }
+    }
+
     public static List<Integer> create() {
-        return List.of();
+        Collections.shuffle(LOTTO_NUMBERS);
+        return LOTTO_NUMBERS.subList(0, LOTTO_NUMBER_SIZE);
     }
 }
--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -2,6 +2,7 @@ package step2;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,8 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoTest {
 
     @Test
+    @DisplayName("로또 생성")
     void lotto() {
         List<Integer> lottoNumbers = Lotto.create();
-        assertThat(lottoNumbers.size()).isEqualTo(0);
+        assertThat(lottoNumbers.size()).isEqualTo(6);
     }
 }
