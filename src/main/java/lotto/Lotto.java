b8d66b464f0145a09cd94678a8fdf6e7874dbd37
feelydh <feelydh@echomarketing.co.kr>
2019-06-25 01:14:53 +0900

    feat(lotto): 로또 번호 validate 추가, 이에따른 테스트 코드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,12 +7,14 @@ public class Lotto {
 
     private static final int LOTTO_IS_WINNER_NUMBER_COUNT = 1;
     private static final int LOTTO_ISNOT_WINNER_NUMBER_COUNT = 0;
+    private static final int LOTTO_DEFAULT_SIZE = 6;
     static final String LAST_WEEK_WINNUM_DELIMITER = ",";
     private List<Integer> lottoGameNumbers;
     private int winnerNumberCount = 0;
 
     public Lotto(List<Integer> lottoGameNumbers) {
         this.lottoGameNumbers = lottoGameNumbers;
+        lottoNumberValidationCheck();
     }
 
     public int getWinnerNumberCount() {
@@ -26,6 +28,12 @@ public class Lotto {
         }
     }
 
+    public String lottoNumber() {
+        return lottoGameNumbers.stream()
+                .map(num -> Integer.toString(num))
+                .collect(Collectors.joining(","));
+    }
+
     private int isWinnerNumber(String winNumber) {
         if (lottoGameNumbers.contains(Integer.parseInt(winNumber))) {
             return LOTTO_IS_WINNER_NUMBER_COUNT;
@@ -33,10 +41,20 @@ public class Lotto {
         return LOTTO_ISNOT_WINNER_NUMBER_COUNT;
     }
 
-    public String lottoNumber() {
+    private void lottoNumberValidationCheck() {
+        if (islottoNumberWrongSize() || islottoNumberDuplicate()) {
+            throw new IllegalStateException("로또 번호의 수가 6이 아니거나 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private boolean islottoNumberWrongSize() {
+        return lottoGameNumbers.size() != LOTTO_DEFAULT_SIZE;
+    }
+
+    private boolean islottoNumberDuplicate() {
         return lottoGameNumbers.stream()
-                .map(num -> Integer.toString(num))
-                .collect(Collectors.joining(","));
+                .distinct()
+                .count() != lottoGameNumbers.size();
     }
 
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,28 +1,19 @@
 package lotto;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
-
-    private List <Integer> lottoNumber;
-    private Lotto lotto;
-
-    @BeforeEach
-    void setUp() {
-        lottoNumber = Arrays.asList(new Integer[]{6, 13, 23, 29, 35, 42});
-        lotto = new Lotto(lottoNumber);
-    }
 
     @Test
     @DisplayName("입력한 당첨번호와 3개일치 테스트")
     void lottoWinning() {
+        Lotto lotto = new Lotto(Arrays.asList(new Integer[]{6, 13, 23, 29, 35, 42}));
         String LastWeekWinnerNumber = "1,2,3,13,35,42";
         lotto.winnerCheck(LastWeekWinnerNumber);
         assertThat(lotto.getWinnerNumberCount()).isEqualTo(3);
@@ -32,6 +23,7 @@ public class LottoTest {
     @Test
     @DisplayName("입력한 당첨번호와 6개일치 테스트")
     void lottoWinningAll() {
+        Lotto lotto = new Lotto(Arrays.asList(new Integer[]{6, 13, 23, 29, 35, 42}));
         String LastWeekWinnerNumber = "6,13,23,29,35,42";
         lotto.winnerCheck(LastWeekWinnerNumber);
         assertThat(lotto.getWinnerNumberCount()).isEqualTo(6);
@@ -43,6 +35,15 @@ public class LottoTest {
     void lottoNumber() {
         Lotto lotto = new Lotto(Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6}));
         assertThat(lotto.lottoNumber()).isEqualTo("1,2,3,4,5,6");
+    }
+
+    @Test
+    @DisplayName("로또 번호 중복 생성 테스트")
+    void lottoNumberError() {
+        assertThatThrownBy(() -> new Lotto(Arrays.asList(new Integer[]{1, 2, 3, 4, 6, 6})))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("로또 번호의 수가 6이 아니거나 중복된 숫자가 있습니다.");
+
     }
 
 }
