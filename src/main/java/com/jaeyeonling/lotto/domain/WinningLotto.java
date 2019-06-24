14106a559bfd1488710e188d790a9410b1fc4b8f
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-24 13:45:43 +0900

    [test] 당첨번호와 보너스 번호 충돌 시 예외처리 테스트 통과

--- src/main/java/com/jaeyeonling/lotto/domain/WinningLotto.java
+++ src/main/java/com/jaeyeonling/lotto/domain/WinningLotto.java
@@ -1,5 +1,7 @@
 package com.jaeyeonling.lotto.domain;
 
+import com.jaeyeonling.lotto.exception.ConflictLottoNumberException;
+
 public class WinningLotto {
 
     private final Lotto winningLotto;
@@ -7,6 +9,10 @@ public class WinningLotto {
 
     WinningLotto(final Lotto winningLotto,
                  final LottoNumber bonusLottoNumber) {
+        if (winningLotto.contains(bonusLottoNumber)) {
+            throw new ConflictLottoNumberException(bonusLottoNumber);
+        }
+
         this.winningLotto = winningLotto;
         this.bonusLottoNumber = bonusLottoNumber;
     }
