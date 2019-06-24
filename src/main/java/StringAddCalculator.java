c32b67bb9fd2f913780144795adb4d8413b587c9
Jason <jason.sohn28@gmail.com>
2019-06-25 02:20:32 +0900

    프로덕트 코드 수정. 음수입력 구현 예정

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,11 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static java.lang.Integer.parseInt;
 
 public class StringAddCalculator {
 
+    private static final int EMPTY_NUM = 0;
+    private static final String COMMA_OR_COLON_DELIMITER = ",|:";
+    private static final String CUSTOM_REGEX = "//(.)\n(.*)";
 
     public static int calculator(String input) {
         if (input == null || input.isEmpty()) {
-            return 0;
+            return EMPTY_NUM;
         }
 
         if (checkOneNumber(input)) {
@@ -22,13 +28,29 @@ public class StringAddCalculator {
     }
 
     private static int splitAndSum(String input) {
-        String[] numbers = input.split(",|:");
+        String[] numbers = input.split(COMMA_OR_COLON_DELIMITER);
+
+        return sum(numbers);
+    }
+
+    private static int splitRegexAndSum(String input) {
+        Matcher m = Pattern.compile(CUSTOM_REGEX).matcher(input);
+        int sum = 0;
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+
+            return sum(tokens);
+        }
+        return sum;
+    }
+
+    private static int sum(String[] numbers) {
         int sum = 0;
         int size = numbers.length;
         for (String aNumber : numbers) {
             sum += Integer.parseInt(aNumber);
         }
-
         return sum;
     }
 
