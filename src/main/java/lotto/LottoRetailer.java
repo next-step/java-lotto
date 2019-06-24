f5eafbb303429afc8546643c231581d8921749d2
SunKyung Cho <sunkyung@queryjet.com>
2019-06-24 19:42:37 +0900

    refactor(LottoStore) 로또 번호 생성 refactoring

--- src/main/java/lotto/LottoRetailer.java
+++ src/main/java/lotto/LottoRetailer.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LottoRetailer {
@@ -10,8 +9,7 @@ public class LottoRetailer {
 
         List<LottoNumbers> list = new ArrayList<>();
         for (int i = 0; i < quantity; i++) {
-            LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(1,2,3,4,5,6));
-            list.add(lottoNumbers);
+            list.add(new LottoNumbers(NumberGenerator.autoGenerate()));
         }
         return list;
     }
---
+++ src/main/java/lotto/NumberGenerator.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumberGenerator {
+
+    public static List<Integer> autoGenerate() {
+        List<Integer> numbers = IntStream.rangeClosed(1, 45)
+                .boxed()
+                .collect(Collectors.toList());
+        Collections.shuffle(numbers);
+        return numbers.stream()
+                .limit(6)
+                .collect(Collectors.toList());
+   }
+}
