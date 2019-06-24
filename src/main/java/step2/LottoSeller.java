8c29b04b08b9dea861cfbad6ffb711c4d4d7f613
1nteger <rhep0820@gmail.com>
2019-06-24 23:51:44 +0900

    [step2] refactor : 예외처리 추가, 구입할 로또복권 개수 계산 메서드 추가

--- src/main/java/step2/LottoSeller.java
+++ src/main/java/step2/LottoSeller.java
@@ -1,9 +1,27 @@
 package step2;
 
+import java.util.List;
+
 public class LottoSeller {
+    static final int PRICE_OF_A_LOTTO_TICKET = 1000;
+    static final String ALERT_MISSING_MONEY = "돈을 넣어주세요. 로또복권은 한 장당 1000원 입니다.";
 
     public int countLottoTickets(int inputMoney) {
-        //예외처리
-        return inputMoney / 1000;
+        validationInputMoney(inputMoney);
+        return inputMoney / PRICE_OF_A_LOTTO_TICKET;
+    }
+
+    public List<LottoTicket> issueLottoTicket(int numberOfTicketsToBuy) {
+        //LottoTickets 생성
+        //LottoTickets에 발행된 티켓 추가
+            //LottoTicketGenerator.issue();
+        //LottoTickets 반환
+
+    }
+
+    private void validationInputMoney(int inputMoney) {
+        if (inputMoney < PRICE_OF_A_LOTTO_TICKET) {
+            throw new IllegalArgumentException(ALERT_MISSING_MONEY);
+        }
     }
 }
--- src/test/java/step2/LottoSellerTest.java
+++ src/test/java/step2/LottoSellerTest.java
@@ -2,7 +2,10 @@ package step2;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LottoSellerTest {
     @Test
@@ -15,6 +18,36 @@ public class LottoSellerTest {
         int lottoTicketsToBuy = lottoSeller.countLottoTickets(inputMoney);
 
         //Then
-        assertThat(lottoTicketsToBuy).isEqualTo(14);
+        assertThat(lottoTicketsToBuy).isEqualTo(inputMoney/LottoSeller.PRICE_OF_A_LOTTO_TICKET);
+    }
+
+    @Test
+    void 입력받은_금액이_로또복권_한_장_가격보다_낮을_경우_예외가_발생한다() {
+        //Given
+        LottoSeller lottoSeller = new LottoSeller();
+        int inputInsufficientMoney = 900;
+        int inputNegativeNumber = -1;
+
+        //When
+        //Then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    lottoSeller.countLottoTickets(inputInsufficientMoney);
+                    lottoSeller.countLottoTickets(inputNegativeNumber);
+                }).withMessage(LottoSeller.ALERT_MISSING_MONEY);
+    }
+
+    @Test
+    void 구입할_개수만큼_로또복권을_발행한다() {
+        //Given
+        LottoSeller lottoSeller = new LottoSeller();
+        int inputMoney = 14000;
+        int numberOfTicketsToBuy = lottoSeller.countLottoTickets(inputMoney);
+
+        //When
+        List<LottoTicket> lottoTickets = lottoSeller.issueLottoTicket(numberOfTicketsToBuy);
+
+        //Then
+        assertThat(lottoTickets.size()).isEqualTo(numberOfTicketsToBuy);
     }
 }
