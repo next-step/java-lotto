6256f7f54a5b8ac34148c6039f973c6098a15fd1
권병윤 <sksggg123@gmail.com>
2019-06-25 00:32:25 +0900

    [feat] : Money 클래스 객체성생 및 발행개수 반환

---
+++ src/main/java/edu/nextstep/step2/Money.java
@@ -0,0 +1,31 @@
+package edu.nextstep.step2;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-lotto
+ * create date  : 2019-06-25 00:27
+ */
+public class Money {
+    private static final int DIVID_NUMBER = 1000;
+    private int extractCount;
+
+    public Money(int extractCount) {
+        validMoney(extractCount);
+        this.extractCount = extractCount / DIVID_NUMBER;
+    }
+
+    public int getNumberOfExtract() {
+        return this.extractCount;
+    }
+
+    private void validMoney(int extractCount) {
+        if (extractCount < DIVID_NUMBER) {
+            throw new IllegalArgumentException("투입금액이 적습니다.");
+        }
+    }
+}
