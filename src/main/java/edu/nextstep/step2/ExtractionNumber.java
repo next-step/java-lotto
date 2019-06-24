c78e32a7ae3badd611ae38d5f2e6b061e2a49bcf
권병윤 <sksggg123@gmail.com>
2019-06-24 23:07:43 +0900

    [feat] : ExtractNumber 객체생성 예외처리

---
+++ src/main/java/edu/nextstep/step2/ExtractionNumber.java
@@ -0,0 +1,30 @@
+package edu.nextstep.step2;
+
+import java.util.List;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-lotto
+ * create date  : 2019-06-24 23:00
+ */
+public class ExtractionNumber {
+
+    private static final int LIST_VALID_SIZE = 6;
+    private List<Number> exNumber;
+
+    public ExtractionNumber(List<Number> exNumber) {
+            validSize(exNumber);
+            this.exNumber = exNumber;
+    }
+
+    private void validSize(List<Number> exNumber) {
+        if(exNumber.size() != LIST_VALID_SIZE) {
+            throw new IllegalArgumentException("전달된 숫자가 6개가 아닙니다.");
+        }
+    }
+}
