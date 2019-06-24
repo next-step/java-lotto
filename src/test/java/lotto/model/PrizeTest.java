9ec7fe75fc15cce1875d29a06944f52357e8a2f0
KIM JUNGHWAN <writer0713@naver.com>
2019-06-24 19:00:03 +0900

    #step2 [refactor] 사용하지 않는 테스트 삭제

--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -11,13 +11,4 @@ public class PrizeTest {
         assertThat(Prize.valueOf(0)).isEqualTo(Prize.LOSE);
         assertThat(Prize.valueOf(3)).isEqualTo(Prize.FOURTH);
     }
-
-    @Test
-    void test_getters() {
-        assertThat(Prize.valueOf(0).getCountOfWinningNumbers()).isEqualTo(0);
-        assertThat(Prize.valueOf(0).getPrizeMoney()).isEqualTo(0);
-
-        assertThat(Prize.valueOf(3).getCountOfWinningNumbers()).isEqualTo(3);
-        assertThat(Prize.valueOf(3).getPrizeMoney()).isEqualTo(5000);
-    }
 }
