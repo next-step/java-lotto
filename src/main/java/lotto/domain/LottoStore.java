6e04c249b5098c92a781ab7a58ca58e1e3305158
pch8388 <pch838811@gmail.com>
2019-06-25 01:23:10 +0900

    refactor : 매개변수 타입 변경

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -4,18 +4,16 @@ public class LottoStore {
 
   private static final int LOTTO_PRICE = 1000;
 
-  public static int buyLottoCount(final String input) {
-    final int price = validateInput(input);
+  public static int buyLottoCount(final int price) {
     if (price % LOTTO_PRICE != 0) {
       throw new IllegalArgumentException(LOTTO_PRICE + "원 단위로 입력해야 합니다.");
     }
-    return price / LOTTO_PRICE;
+    return validateInput(price) / LOTTO_PRICE;
   }
 
-  private static int validateInput(final String input) {
-    final int price = Integer.parseInt(input);
+  private static int validateInput(final int price) {
     if (price <= 0) {
-      throw new IllegalArgumentException("음수 또는 0을 입력할 수 없습니다. : " + input);
+      throw new IllegalArgumentException("음수 또는 0을 입력할 수 없습니다. : " + price);
     }
     return price;
   }
