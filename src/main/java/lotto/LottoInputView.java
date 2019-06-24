b320473680dbbc3ab0329f9411946ce472e573ba
KIM JUNGHWAN <writer0713@naver.com>
2019-06-24 09:57:56 +0900

    #step2 [feature] 당첨번호 입력

--- src/main/java/lotto/LottoInputView.java
+++ src/main/java/lotto/LottoInputView.java
@@ -1,15 +1,20 @@
 package lotto;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class LottoInputView {
 
   private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+  private static final String WINNING_NUMBERS_MESSAGE = "지난 주 당첨번호를 입력해 주세요.";
   private static final String NOT_A_NUMBER_INPUT_ERROR_MESSAGE = "숫자로 제대로 입력해주세요.";
 
   public static int askPurchaseAmount() {
-    System.out.println(PURCHASE_AMOUNT_MESSAGE);
+    print(PURCHASE_AMOUNT_MESSAGE);
 
     try {
       Scanner scanner = new Scanner(System.in);
@@ -18,5 +23,25 @@ public class LottoInputView {
     } catch(InputMismatchException e) {
       throw new IllegalArgumentException(NOT_A_NUMBER_INPUT_ERROR_MESSAGE);
     }
+  }
+
+  public static Lotto askWinningNumbers() {
+    print(WINNING_NUMBERS_MESSAGE);
+
+    try {
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      List<Integer> winningNumbers = Stream.of(input.split(",( )?"))
+              .mapToInt(Integer::valueOf)
+              .boxed()
+              .collect(toList());
+      return new Lotto(winningNumbers);
+    } catch(InputMismatchException e) {
+      throw new IllegalArgumentException(NOT_A_NUMBER_INPUT_ERROR_MESSAGE);
+    }
+  }
+
+  private static void print(String msg) {
+    System.out.println(msg);
   }
 }
