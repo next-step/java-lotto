6ee598c3f66233b40a967e808d88160917d84696
KIM JUNGHWAN <writer0713@naver.com>
2019-06-24 10:40:51 +0900

    #step2 [feature] 당첨금 확인 구현 (test success)

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -1,7 +1,21 @@
 package lotto.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class Prize {
+
+    private static final Map<Integer, Integer> prize = new HashMap<>();
+
+    static {
+        prize.put(3, 5000);
+        prize.put(4, 50000);
+        prize.put(5, 1500000);
+        prize.put(6, 2000000000);
+    }
+
     public static int win(int count) {
-        return 0;
+        return Optional.ofNullable(prize.get(count)).orElse(0);
     }
 }
