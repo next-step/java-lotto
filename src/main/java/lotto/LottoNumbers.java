f52f14ac95d2d199c8905493b19a9001aa49356d
SunKyung Cho <sunkyung@queryjet.com>
2019-06-24 14:05:02 +0900

    refactor(LottoNumber) 상수사용 및 불필요한 테스트 제거

--- src/main/java/lotto/LottoNumbers.java
+++ src/main/java/lotto/LottoNumbers.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 
 public class LottoNumbers {
+
+    private final static int LOTTO_NUMBER_SIZE = 6;
     
     public LottoNumbers(List<Integer> numbers) {
         verifySize(numbers);
@@ -11,14 +13,14 @@ public class LottoNumbers {
     }
 
     private void verifySize(List<Integer> numbers) {
-        if(numbers.size() != 6) {
+        if(numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("6개의 숫자만 입력해야 합니다.");
         }
     }
 
     private void verifyDuplicationNumber(List<Integer> numbers) {
         HashSet<Integer> checkNumbers = new HashSet<>(numbers);
-        if(checkNumbers.size() < 6) {
+        if(checkNumbers.size() < LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("중복된 숫자는 허용되지 않습니다.");
         }
     }
--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -6,16 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoNumbersTest {
-
-    @Test
-    public void LottoTest() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        LottoNumbers lottoNumber = new LottoNumbers(numbers);
-    }
 
     @Test
     @DisplayName("중복된 숫자 예외처리")
