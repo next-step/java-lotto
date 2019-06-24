5f441d3dbf91ce4b55022bd95cf0aca616fc4b55
pch8388 <pch838811@gmail.com>
2019-06-25 02:43:06 +0900

    refactor : 출력을 위해 순서 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-  FIRST(6, 2_000_000_000),
-  THIRD(5, 1_500_000),
-  FOURTH(4, 50_000),
+  MISS(0, 0),
   FIFTH(3, 5_000),
-  MISS(0, 0);
+  FOURTH(4, 50_000),
+  THIRD(5, 1_500_000),
+  FIRST(6, 2_000_000_000);
 
   private int countOfMatch;
   private int winningMoney;
