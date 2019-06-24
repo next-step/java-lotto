a9e1ddef14a3a6ec0400ad5ed3fcaca13ae22b7e
전해성 <seong0428@gmail.com>
2019-06-25 01:18:06 +0900

    refactor #step2 - 로또 번호 리스트를 상수로 선언합니다

--- src/main/java/camp/nextstep/edu/lotto/LotteryApplication.java
+++ src/main/java/camp/nextstep/edu/lotto/LotteryApplication.java
@@ -1,19 +1,17 @@
 package camp.nextstep.edu.lotto;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LotteryApplication {
 
     private static final NaturalNumber PRICE_OF_LOTTERY = NaturalNumber.from(1000);
-    private static final int BEGIN_NUMBER = 1;
-    private static final int END_NUMBER = 45;
     private static final int SIZE = 6;
     private static final int ZERO = 0;
+    private static final List<Integer> CANDIDATE_NUMBERS = IntStream.rangeClosed(1, 45)
+            .boxed()
+            .collect(Collectors.toList());
 
     public NaturalNumber calculateNumberOfAvailableLotteries(NaturalNumber investment) {
         if (investment == null) {
@@ -30,9 +28,9 @@ public class LotteryApplication {
     }
 
     public Set<Integer> createLottery() {
-        final List<Integer> candidates = IntStream.rangeClosed(BEGIN_NUMBER, END_NUMBER).boxed().collect(Collectors.toList());
-        Collections.shuffle(candidates);
-        final List<Integer> selectedNumbers = candidates.subList(ZERO, SIZE);
+        final List<Integer> allNumbers = new ArrayList<>(CANDIDATE_NUMBERS);
+        Collections.shuffle(allNumbers);
+        final List<Integer> selectedNumbers = allNumbers.subList(ZERO, SIZE);
         return new HashSet<>(selectedNumbers);
     }
 }
