2af22c599e750fde952f93a575f1e94fddb51f65
SangHyoun Yun <hainco@gmail.com>
2019-06-25 00:59:10 +0900

    feat(calculator): StringParser 에 AdditionalDelimiter 관련 메서드 추가

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -2,9 +2,31 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringParser {
-    public static List<String> parseByDelimiter(String input) {
+    public static final String ADDITIONAL_DELIMITER_REGEX = "//(.)\n(.*)";
+    private Matcher matcher;
+    private String input;
+
+    public StringParser(String input) {
+        this.input = input;
+        matcher = Pattern.compile(ADDITIONAL_DELIMITER_REGEX).matcher(input);
+    }
+
+    public boolean hasAdditionalDelimiter() {
+        return matcher.find();
+    }
+
+    public List<String> parseByDelimiter() {
         return Arrays.asList(input.split(",|:"));
+    }
+
+    public String parseAdditionalDelimiter() {
+        if (!hasAdditionalDelimiter()) {
+            throw new RuntimeException("추가적인 구분자가 없는 String 입니다");
+        }
+        return matcher.group(1);
     }
 }
--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -10,34 +10,39 @@ public class StringParserTest {
     @DisplayName("숫자만 있는 스트링을 파싱")
     void parseByDelimiter() {
         String input = "2";
-        assertThat(StringParser.parseByDelimiter(input)).contains("2");
+        StringParser stringParser = new StringParser(input);
+        assertThat(stringParser.parseByDelimiter()).contains("2");
     }
 
     @Test
     @DisplayName("컴마 구분자가 있는 스트링 파싱")
     void parseByDelimiter2() {
         String input = "1,2";
-        assertThat(StringParser.parseByDelimiter(input)).contains("1", "2");
+        StringParser stringParser = new StringParser(input);
+        assertThat(stringParser.parseByDelimiter()).contains("1", "2");
     }
 
     @Test
     @DisplayName("콜론 구분자가 있는 스트링 파싱")
     void parseByDelimiter3() {
         String input = "1:2";
-        assertThat(StringParser.parseByDelimiter(input)).contains("1", "2");
+        StringParser stringParser = new StringParser(input);
+        assertThat(stringParser.parseByDelimiter()).contains("1", "2");
     }
 
     @Test
     @DisplayName("컴마, 콜론 구분자가 있는 스트링 파싱")
     void parseByDelimiter4() {
         String input = "1,2,3:4:5";
-        assertThat(StringParser.parseByDelimiter(input)).contains("1", "2", "3", "4", "5");
+        StringParser stringParser = new StringParser(input);
+        assertThat(stringParser.parseByDelimiter()).contains("1", "2", "3", "4", "5");
     }
 
     @Test
     @DisplayName("입력받은 스트링에서 구분자가 있으면 파싱한다")
     void parseDelimiter() {
         String input = "//;\n1;2;3";
-        assertThat(StringParser.parseDelimiter(input)).isEqualTo(";");
+        StringParser stringParser = new StringParser(input);
+        assertThat(stringParser.parseAdditionalDelimiter()).isEqualTo(";");
     }
 }
