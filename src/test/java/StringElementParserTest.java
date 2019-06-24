ce4770bd21cd3f433482bf915c444a102ba2a5ad
mommoo <fk2dhstmzk@>
2019-06-25 08:28:52 +0900

    test(StringElementParserTest) :  - 단일 구분자 또는 복수 구분자가 포함된 문자열을 받는 경우 요소가 파싱되는지 테스트 추가.

--- src/test/java/StringElementParserTest.java
+++ src/test/java/StringElementParserTest.java
@@ -1,6 +1,9 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,5 +20,18 @@ public class StringElementParserTest {
 
         Assertions.assertThat(elements).hasSize(1);
         Assertions.assertThat(elements).containsOnly("1");
+    }
+
+    @DisplayName("요소는 세개가 검출되어야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings={"1:2,3", "1,2,3", "1:2,3"})
+    public void testThreeElement(String string) {
+        List<String> elements = new ArrayList<>();
+
+        StringElementParser stringElementParser = new StringElementParser(string);
+        stringElementParser.parseElement(elements::add);
+
+        Assertions.assertThat(elements).hasSize(3);
+        Assertions.assertThat(elements).containsOnly("1", "2", "3");
     }
 }
