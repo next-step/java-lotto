a59795fd9c4f4738dbdc60d35ad12f3353f20838
한정수 <integer@zuminternet.com>
2019-06-24 16:09:41 +0900

    [step2] feat : 로또복권의 로또번호 개수가 6개가 아닌 경우 검증 메서드, 테스트 추가

--- src/main/java/step2/LottoTicket.java
+++ src/main/java/step2/LottoTicket.java
@@ -8,11 +8,19 @@ import java.util.Set;
 public class LottoTicket {
 
     static final String ALERT_NUMBER_OVERLAP = "중복된 로또 번호가 존재합니다.";
+    public static final String AlERT_DIFFERENT_SIZE_OF_NUMBERS = "로또번호의 개수가 6개가 아닙니다.";
     private final List<Integer> lottoTicket;
 
     LottoTicket(List<Integer> lottoTicket) {
+        validationNumberSize(lottoTicket);
         validationNumberOverlap(lottoTicket);
         this.lottoTicket = lottoTicket;
+    }
+
+    private void validationNumberSize(List<Integer> lottoTicket) {
+        if (lottoTicket.size() != LottoTicketGenerator.BASIC_LOTTO_SIZE) {
+            throw new IllegalArgumentException(AlERT_DIFFERENT_SIZE_OF_NUMBERS);
+        }
     }
 
     private void validationNumberOverlap(List<Integer> lottoTicket) {
--- src/test/java/step2/LottoTicketTest.java
+++ src/test/java/step2/LottoTicketTest.java
@@ -21,4 +21,20 @@ public class LottoTicketTest {
                     new LottoTicket(overlapNumbers);
                 }).withMessageContaining(LottoTicket.ALERT_NUMBER_OVERLAP);
     }
+
+    @Test
+    void 로또복권의_로또번호가_6개보다_작거나_클_경우_예외가_발생한다() {
+        //Given
+        List<Integer> lackOfNumbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> excessOfNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        //When
+        //Then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    new LottoTicket(lackOfNumbers);
+                    new LottoTicket(excessOfNumbers);
+                }).withMessageContaining(LottoTicket.AlERT_DIFFERENT_SIZE_OF_NUMBERS);
+    }
+
 }
