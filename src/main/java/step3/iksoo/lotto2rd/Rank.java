0905f266f268b2fa6d2c5ddb2ffeec9936b0cb10
Iksoo Shin <pd6016@naver.com>
2019-06-24 22:13:57 +0900

    feat(Rank) : 2등보너스볼을 추가한 Enum Rank 구현

---
+++ src/main/java/step3/iksoo/lotto2rd/Rank.java
@@ -0,0 +1,47 @@
+package step3.iksoo.lotto2rd;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000, "1등"),
+    SECOND(5, 30_000_000, "2등(보너스볼)"),
+    THIRD(5, 1_500_000, "3등"),
+    FOURTH(4, 50_000, "4등"),
+    FIFTH(3, 5_000, "5등");
+
+    private int countOfMatch;
+    private int winningMoney;
+    private String rankName;
+
+    Rank(int countOfMatch, int winningMoney, String rankName) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+        this.rankName = rankName;
+    }
+
+    public int getCountOfMatch() {
+        return this.countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return this.winningMoney;
+    }
+
+    public String getRankName() {
+        return this.rankName;
+    }
+
+    public static Rank valueOf(int countOfMatch, boolean matchBonus) {
+        Rank[] ranks = values();
+        return Arrays.stream(ranks)
+                .filter(n -> n.getCountOfMatch() == countOfMatch)
+                .map(n -> {
+                    if (n.getCountOfMatch() == THIRD.getCountOfMatch()) {
+                        return matchBonus ? SECOND : THIRD;
+                    }
+                    return n;
+                })
+                .findAny()
+                .orElse(null);
+    }
+}
---
+++ src/test/java/step3/iksoo/lotto2rd/RankTest.java
@@ -0,0 +1,27 @@
+package step3.iksoo.lotto2rd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RankTest {
+    @Test
+    void getCountOfMatchTest() {
+        assertThat(Rank.FIFTH.getCountOfMatch()).isEqualTo(3);
+    }
+
+    @Test
+    void getWinningMoneyTest() {
+        assertThat(Rank.FIFTH.getWinningMoney()).isEqualTo(5_000);
+    }
+
+    @Test
+    void getRankNameTest() {
+        assertThat(Rank.FIFTH.getRankName()).isEqualTo("5등");
+    }
+
+    @Test
+    void _2등_보너스볼() {
+        assertThat(Rank.valueOf(5, true).getWinningMoney()).isEqualTo(30_000_000);
+    }
+}
