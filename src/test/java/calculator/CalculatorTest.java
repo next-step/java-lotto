6b11b09cb2f8b5e906c2663e528d275f50a893e2
SangHyoun Yun <hainco@gmail.com>
2019-06-25 01:59:35 +0900

    test(calculator): 테스트에 이름 빠진것 수정

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,11 +1,13 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
     @Test
+    @DisplayName("두 숫자를 더한다")
     void addTwoNumber() {
         int num1 = 1;
         int num2 = 2;
