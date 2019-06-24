0ad0f658d9c5ca011438816df60d8c6f387760c4
rabbitfoot <wowyowowyo@gmail.com>
2019-06-24 10:45:41 +0900

    [feat] step1. 문자열 덧셈 계산기 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,70 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    private static final int EMPTY_NUM = 0;
+    private static final String COMMA_OR_COLON_DELIMITER = ",|:";
+    private static final String CUSTOM_REGEX = "//(.)\\n(.*)";
+    private static final String NUMBER_REGEX = "^[0-9]*$";
+
+    int add(String val) {
+        if (isNull(val) || isEmpty(val)) {
+            return EMPTY_NUM;
+        }
+        int result = 0;
+        for (String numStr : stringToNumStrings(val)) {
+            checkNumberString(numStr);
+            int num = stringToNum(numStr);
+            checkNegativeNum(num);
+            result += num;
+        }
+        return result;
+    }
+
+    private String[] stringToNumStrings(String val) {
+        Matcher matcher = Pattern.compile(CUSTOM_REGEX).matcher(val);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return val.split(COMMA_OR_COLON_DELIMITER);
+    }
+
+    private int stringToNum(String val) {
+        return Integer.parseInt(val);
+    }
+
+    private String removalWhitespace(String val) {
+        return val.trim();
+    }
+
+    private void checkNumberString(String val) throws RuntimeException {
+        if (!Pattern.matches(NUMBER_REGEX, val)) {
+            throw new RuntimeException("계산할 문자열은 숫자형태만 허용합니다.");
+        }
+    }
+
+    private void checkNegativeNum(int val) throws RuntimeException {
+        if (0 > val) {
+            throw new RuntimeException("계산할 문자열에 음수는 허용하지 않습니다.");
+        }
+    }
+
+    private boolean isNull(String val) {
+        return val == null;
+    }
+
+    private boolean isEmpty(String val) {
+        val = removalWhitespace(val);
+        return val.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        StringCalculator cal = new StringCalculator();
+        System.out.println("null 계산기 결과]"+cal.add(null));
+        System.out.println("[쉼표 또는 콜론 계산기 결과]"+cal.add("1,2:3:4,5"));
+        System.out.println("[쉼표 또는 콜론 계산기 결과]"+cal.add("//;\n1;2;3"));
+    }
+}
