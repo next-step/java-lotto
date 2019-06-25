8b92f855bf60de1d8cb8c3ababa8687bf19abbc8
rabbitfoot <wowyowowyo@gmail.com>
2019-06-25 22:23:30 +0900

    [step2/refactor] main 메소드 출력단위별 줄바꿈 추가

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -4,16 +4,22 @@ import lotto.domain.*;
 import lotto.view.LottoResultView;
 
 public class LottoController {
+
     public static void main(String[] args) {
+
         LottoResultView view = new LottoResultView();
         view.printPurchaseAmountGuide();
+
         LottoPublisher lottoPublisher = new LottoPublisher(view.getInputAmout());
         view.printBuyLottoCountGuide(lottoPublisher.getPublishLotto().size());
+
         view.printPublishLottoNumbers(lottoPublisher.getBuyLottoCount(),
                 lottoPublisher.getLottosNumbers());
+
         view.printWinnerNumbersGuide();
         RankReward rankReward = new RankReward(lottoPublisher.getPublishLottoRanks(
                 new WinnerLotto(view.getInputWinnerNumbers())));
+
         view.printWinnerStatisticsGuide();
         view.printRankGuide(LottoResultView.FIFTH_RANK_GUIDE,
                 rankReward.getGainRankCounts()[Rank.FIFTH.ordinal()]);
@@ -23,6 +29,7 @@ public class LottoController {
                 rankReward.getGainRankCounts()[Rank.THIRD.ordinal()]);
         view.printRankGuide(LottoResultView.FIRST_RANK_GUIDE,
                 rankReward.getGainRankCounts()[Rank.FIRST.ordinal()]);
+        
         view.printRevenueRatioGuide(lottoPublisher.getRevenueRatio(
                 rankReward.getRewardWinningMoney()),
                 RevenueRatio.valueOf(lottoPublisher.getRevenueRatio(
