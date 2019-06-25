01e96482adb24d58c67a7c77ae0cd0fbdbfc3bef
KIM JUNGHWAN <writer0713@naver.com>
2019-06-25 10:36:25 +0900

    #step2 [test] ReportTest 추가

---
+++ src/test/java/lotto/model/ReportTest.java
@@ -0,0 +1,51 @@
+package lotto.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ReportTest {
+
+    private Report report;
+
+    @BeforeEach
+    void setUp() {
+        report = new Report(getPrizeStatus(), getLottos());
+    }
+
+    private Map<Prize, Integer> getPrizeStatus() {
+        Map<Prize, Integer> prizeStatus = new HashMap<>();
+        prizeStatus.put(Prize.FIRST, 1);
+        prizeStatus.put(Prize.FOURTH, 1);
+        return prizeStatus;
+    }
+
+    private Lottos getLottos() {
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 8, 9, 10));
+        Lotto lotto2 = new Lotto(Arrays.asList(1, 2, 3, 4, 10, 11));
+        return new Lottos(Arrays.asList(lotto1, lotto2));
+    }
+
+    @Test
+    void rateOfReturn() {
+        double total = Prize.FIRST.getPrizeMoney() + Prize.FOURTH.getPrizeMoney();
+        double buyingMoney = Lotto.price * 2;
+        assertThat(report.rateOfReturn()).isEqualTo(total / buyingMoney);
+    }
+
+    @Test
+    void totalProfits() {
+        assertThat(report.totalProfits()).isEqualTo(Prize.FIRST.getPrizeMoney() + Prize.FOURTH.getPrizeMoney());
+    }
+
+    @Test
+    void buyingMoney() {
+        assertThat(report.buyingMoney()).isEqualTo(Lotto.price * 2);
+    }
+}
