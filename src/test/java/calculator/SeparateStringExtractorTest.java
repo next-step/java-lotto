992832f99958471444360cb4c268fb2c0489b247
seungkil choi <kgoon38@gmail.com>
2019-06-26 00:24:56 +0900

    [FAIL] 다양한 사용자지정 구분자 테스트 추가

--- src/test/java/calculator/SeparateStringExtractorTest.java
+++ src/test/java/calculator/SeparateStringExtractorTest.java
@@ -1,16 +1,34 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SeparateStringExtractorTest {
 
-    @Test
-    void extraction() {
+    @DisplayName("다양한 커스텀 구분자 테스트")
+    @ParameterizedTest
+    @MethodSource("provideExtractionSeparator")
+    void extractionSeparator(String expression, String expectSeparator) {
 
-        String separator = SeparateStringExtractor.execute("//;\n");
+        String separator = SeparateStringExtractor.execute(expression);
 
-        assertThat(separator).isEqualTo(";");
+        assertThat(separator).isEqualTo(expectSeparator);
+    }
+
+    private static Stream<Arguments> provideExtractionSeparator() {
+        return Stream.of(
+                Arguments.of("//;\n", ";"),
+                Arguments.of("//+\n", "+"),
+                Arguments.of("//**\n", "**"),
+                Arguments.of("//;+\n", ";+")
+        );
     }
 }
