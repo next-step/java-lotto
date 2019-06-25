9324a7073313345f66e7ac12c39a4b7bc5043cdb
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-25 12:30:52 +0900

    [feat] 리스트 합치기 구현

--- src/main/java/com/jaeyeonling/lotto/utils/ListUtils.java
+++ src/main/java/com/jaeyeonling/lotto/utils/ListUtils.java
@@ -1,12 +1,33 @@
 package com.jaeyeonling.lotto.utils;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.RandomAccess;
 
 public final class ListUtils {
 
     private ListUtils() { }
 
-    public static List<String> concat(final List<String> list1, final List<String> list2) {
-        return null;
+    public static <E> List<E> concat(final List<E> list1,
+                                     final List<E> list2) {
+        final List<E> concatList = createList(list1, list2);
+
+        concatList.addAll(list1);
+        concatList.addAll(list2);
+
+        return concatList;
+    }
+
+    private static <E> List<E> createList(final List<E> list1,
+                                          final List<E> list2) {
+        if (isRandomAccess(list1) && isRandomAccess(list1)) {
+            return new ArrayList<>(list1.size() + list2.size());
+        }
+        return new LinkedList<>();
+    }
+
+    private static boolean isRandomAccess(final List<?> list) {
+        return list instanceof RandomAccess;
     }
 }
