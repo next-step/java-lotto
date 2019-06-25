4244bd2132ea13c551c74b1636132591f812051e
KIM JUNGHWAN <writer0713@naver.com>
2019-06-25 10:07:25 +0900

    #step2 [refactor] 필요없는 주석 및 줄바꿈 제거

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -13,19 +13,16 @@ public class LottoRunner {
     int paid = LottoInputView.askPurchaseAmount();
 
     LottoGenerator lottoGenerator = new LottoGenerator(new RandomNumberGenerator());
-
     Lottos lottos = lottoGenerator.generate(paid);
 
     LottoOutputView.printCountOf(lottos);
     LottoOutputView.printPickedNumbersOf(lottos);
 
     List<Integer> winningNumbers = LottoInputView.askWinningNumbers();
+
     Analyzer analyzer = new Analyzer(winningNumbers);
     Report report = analyzer.analyze(lottos);
 
     LottoOutputView.print(report);
-
-
-    //LottoOutputView.printWinnings();
   }
 }
