f58c454a7b69c97d7aca8a8bb95af67c04892554
leechang0423 <leechang0423@naver.com>
2019-06-25 11:35:57 +0900

    [step2] refactor(lotto) 불필요 변수,import 삭제

--- src/main/java/lotto/LottoLauncher.java
+++ src/main/java/lotto/LottoLauncher.java
@@ -1,14 +1,11 @@
 package lotto;
 
-import java.util.Scanner;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoLauncher {
 
   public static void start() {
-    Scanner scanner = new Scanner(System.in);
-
     int purchaseAmount = InputView.askPurchaseAmount();
     int lottoQuantity = new Money(purchaseAmount).getAvailableQuantity();
     Quantity quantity = new Quantity(lottoQuantity);
