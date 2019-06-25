1df207b63c0bf3b92f99a59a10507ebb5b996869
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-25 10:39:19 +0900

    [test] 문자열로 된 로또 숫자들을 Lotto로 변환 테스트 추가

---
+++ src/main/java/com/jaeyeonling/lotto/utils/StringToLottoTransfer.java
@@ -0,0 +1,12 @@
+package com.jaeyeonling.lotto.utils;
+
+import com.jaeyeonling.lotto.domain.Lotto;
+
+public final class StringToLottoTransfer {
+
+    private StringToLottoTransfer() { }
+
+    public static Lotto transform(final String stringLottoNumbers) {
+        return null;
+    }
+}
