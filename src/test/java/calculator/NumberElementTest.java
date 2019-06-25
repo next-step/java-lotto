6c5e75825ad56defaffdd0feee1db56902058b5a
seungkil choi <kgoon38@gmail.com>
2019-06-26 02:34:03 +0900

    [FAIL] 숫자이외의 문자, 음수 테스트 케이스 추가

--- src/test/java/calculator/NumberElementTest.java
+++ src/test/java/calculator/NumberElementTest.java
@@ -1,11 +1,13 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberElementTest {
@@ -26,5 +28,21 @@ class NumberElementTest {
         NumberElement element = new NumberElement(stringValue);
 
         assertThat(element).isEqualTo(new NumberElement(0));
+    }
+
+    @DisplayName("숫자 이외의 문자변환 오류")
+    @Test
+    void parseNANException(){
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new NumberElement("!");
+        });
+    }
+
+    @DisplayName("음수 입력 오류")
+    @Test
+    void parseNegativeException(){
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new NumberElement("-1");
+        });
     }
 }
