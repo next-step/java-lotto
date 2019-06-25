ad27a89603cef584d3e5dc22685bded009f5f4b9
KIM JUNGHWAN <writer0713@naver.com>
2019-06-25 10:20:13 +0900

    #step2 [test] 구현되어 있지 않은 테스트 작성
    getter에 대한 test 작성

--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -11,4 +11,14 @@ public class PrizeTest {
         assertThat(Prize.valueOf(0)).isEqualTo(Prize.LOSE);
         assertThat(Prize.valueOf(3)).isEqualTo(Prize.FOURTH);
     }
+
+    @Test
+    void test_get_count_of_matching_numbers() {
+        assertThat(Prize.FIRST.getCountOfMatchingNumbers()).isEqualTo(6);
+    }
+
+    @Test
+    void test_get_prize_money() {
+        assertThat(Prize.SECOND.getPrizeMoney()).isEqualTo(1_500_000);
+    }
 }
