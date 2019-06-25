fab170d14fec7080f53d3cedcc09b8dfa650599e
seungkil choi <kgoon38@gmail.com>
2019-06-25 23:42:28 +0900

    [PASS] 빈 문자열은 제외하도록 수정

--- src/main/java/calculator/NumberElementCollection.java
+++ src/main/java/calculator/NumberElementCollection.java
@@ -1,16 +1,23 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberElementCollection {
 
-    String[] elements;
+    List<String> elements;
 
-    public NumberElementCollection(String[] elements){
-        this.elements = elements;
+    public NumberElementCollection(String[] source){
+        this.elements = new ArrayList<>();
+
+        for(int i = 0; i < source.length; i++){
+            if(!source[i].isEmpty()) {
+                this.elements.add(source[i]);
+            }
+        }
     }
 
     public int size() {
-        return elements.length;
+        return elements.size();
     }
 }
