49f63d50a524c6e92b12be182e2722cd7a6aee6b
seungkil choi <kgoon38@gmail.com>
2019-06-25 23:18:16 +0900

    [FAIL] 콜론이 2개 포함되는 케이스 추가

--- src/test/java/calculator/ExpressionParserTest.java
+++ src/test/java/calculator/ExpressionParserTest.java
@@ -2,19 +2,33 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExpressionParserTest {
 
     @DisplayName("콜론을 구분자가 포함된 표현식 파싱")
-    @Test
-    void parseWithColon() {
+    @ParameterizedTest
+    @MethodSource("provideParseWithColonResource")
+    void parseWithColon(String expression, int expectSize) {
 
-        NumberElements elements = ExpressionParser.parse("1:2");
+        NumberElements elements = ExpressionParser.parse(expression);
 
-        assertThat(elements.size()).isEqualTo(2);
+        assertThat(elements.size()).isEqualTo(expectSize);
+    }
+
+
+
+    private static Stream<Arguments> provideParseWithColonResource() {
+        return Stream.of(
+                Arguments.of("1:2", 2),
+                Arguments.of("1:2:3", 3)
+        );
     }
 }
