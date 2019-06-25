35c9696b2ddbc646227dadea75c09b62aa575888
seungkil choi <kgoon38@gmail.com>
2019-06-26 03:10:56 +0900

    [REFACTOR] 무의미한 코드 제거 (null 처리주체가 이동됨)

--- src/main/java/calculator/ExpressionParser.java
+++ src/main/java/calculator/ExpressionParser.java
@@ -2,13 +2,7 @@ package calculator;
 
 public class ExpressionParser {
 
-
-
     public static NumberElementCollection parse(String expression) {
-
-        if(expression == null){
-            return new NumberElementCollection(new String[0]);
-        }
 
         ExpressionSplitter expressionSplitter = new ExpressionSplitter(expression);
         String[] elements = expressionSplitter.split();
