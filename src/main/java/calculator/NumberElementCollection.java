e1ab098f6dff1917b997ccaf404396aea2009601
seungkil choi <kgoon38@gmail.com>
2019-06-26 00:00:21 +0900

    [PASS] 빈값 판단기준 변경

--- src/main/java/calculator/NumberElementCollection.java
+++ src/main/java/calculator/NumberElementCollection.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,10 +9,16 @@ public class NumberElementCollection {
     List<String> elements;
 
     public NumberElementCollection(String[] source){
-        this.elements = Arrays.stream(source).filter(value -> !value.isEmpty()).collect(Collectors.toList());
+        this.elements = Arrays.stream(source)
+                .filter(this::isNotEmpty)
+                .collect(Collectors.toList());
     }
 
     public int size() {
         return elements.size();
+    }
+
+    private boolean isNotEmpty(String value){
+        return !value.trim().isEmpty();
     }
 }
