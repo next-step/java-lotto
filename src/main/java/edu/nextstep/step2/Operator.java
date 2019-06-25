b50688a8a2b771daf52e686498c47820e137efa7
권병윤 <sksggg123@gmail.com>
2019-06-25 16:34:40 +0900

    [feat] : Enum 클래스 생성

---
+++ src/main/java/edu/nextstep/step2/Operator.java
@@ -0,0 +1,52 @@
+package edu.nextstep.step2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-lotto
+ * create date  : 2019-06-25 01:53
+ */
+public enum  Operator {
+
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+    private int match;
+    private int lotteryMoney;
+
+    Operator(int match, int lotteryMoney) {
+        this.match = match;
+        this.lotteryMoney = lotteryMoney;
+    }
+
+    public static Operator matchCheck(int operator) {
+        Operator[] operatorType = Operator.values();
+        return Arrays.stream(operatorType)
+                .filter(oper -> oper.match == operator)
+                .findFirst()
+                .get();
+    }
+
+    public static List<Operator> getOperators() {
+        return Arrays.stream(Operator.values()).collect(Collectors.toList());
+    }
+
+    public int getLotteryMoney() {
+        return this.lotteryMoney;
+    }
+
+    public int getMatch() {
+        return this.match;
+    }
+
+}
---
+++ src/test/java/edu/nextstep/step2/OperatorTest.java
@@ -0,0 +1,28 @@
+package edu.nextstep.step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-lotto
+ * create date  : 2019-06-25 02:27
+ */
+class OperatorTest {
+
+    @DisplayName("Values 값을 얻는다.")
+    @Test
+    void getValues() {
+        assertThat(Operator.getOperators().get(0) == Operator.THREE).isTrue();
+    }
+}
