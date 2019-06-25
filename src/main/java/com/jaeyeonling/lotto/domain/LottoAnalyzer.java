282271113cecb308b2d7ef5fa9c0b01e87c72e64
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-25 12:07:18 +0900

    [refactor] 코드 간결하게 수정

--- src/main/java/com/jaeyeonling/lotto/domain/LottoAnalyzer.java
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoAnalyzer.java
@@ -1,12 +1,11 @@
 package com.jaeyeonling.lotto.domain;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 public class LottoAnalyzer {
 
-    private static final int DEFAULT_MATCH_COUNT = 0;
     private static final int INCREMENT_MATCH_COUNT = 1;
 
     private final WinningLotto winningLotto;
@@ -20,7 +19,7 @@ public class LottoAnalyzer {
     }
 
     private Map<LottoPrize, Integer> countOfEachLottoPrizeByLottos(final List<Lotto> lottos) {
-        final Map<LottoPrize, Integer> countOfEachLottoPrize = new HashMap<>();
+        final Map<LottoPrize, Integer> countOfEachLottoPrize = new EnumMap<>(LottoPrize.class);
         for (final Lotto lotto : lottos) {
             final LottoPrize prize = winningLotto.match(lotto);
             countLottoPrize(countOfEachLottoPrize, prize);
@@ -31,7 +30,6 @@ public class LottoAnalyzer {
 
     private void countLottoPrize(final Map<LottoPrize, Integer> countOfEachLottoPrize,
                                  final LottoPrize prize) {
-        final int currentMatchCount = countOfEachLottoPrize.getOrDefault(prize, DEFAULT_MATCH_COUNT);
-        countOfEachLottoPrize.put(prize, currentMatchCount + INCREMENT_MATCH_COUNT);
+        countOfEachLottoPrize.merge(prize, INCREMENT_MATCH_COUNT, Integer::sum);
     }
 }
