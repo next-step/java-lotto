c67db83fe2934bd692da7d59dc6ec181dc4c5cee
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-25 23:23:29 +0900

    test(lotto): lotto throw exception when number count isn't 6

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class LottoTest {
@@ -14,5 +15,11 @@ class LottoTest {
     void getNumbers() {
         List<Integer> numbers = Arrays.asList(1, 4, 5, 6, 7, 8);
         assertThat(new Lotto(numbers).getNumbers().size()).isEqualTo(6);
+    }
+
+    @Test
+    void validate() {
+        List<Integer> numbers = Arrays.asList(1, 4, 2);
+        assertThatThrownBy(() -> new Lotto(numbers)).isInstanceOf(IllegalArgumentException.class);
     }
 }
