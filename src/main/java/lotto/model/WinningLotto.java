d0e052eddab3433174f2d88e2af31e07d94e741b
KIM JUNGHWAN <writer0713@naver.com>
2019-06-26 15:04:53 +0900

    #step3 [feature] WinningLotto 객체 추가

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,20 @@
+package lotto.model;
+
+public class WinningLotto {
+
+    private final Lotto lotto;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto lotto, int bonusNumber) {
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
