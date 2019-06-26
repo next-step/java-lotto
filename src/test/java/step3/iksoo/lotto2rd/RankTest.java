9ac4612da6d56a1e9dcb5d3dc0e37c0d813b1a9e
daioooor36 <pd6016@naver.com>
2019-06-26 23:09:55 +0900

    test(Rank) : 보너스이 없는 등수 테스트코드 추가

--- src/test/java/step3/iksoo/lotto2rd/RankTest.java
+++ src/test/java/step3/iksoo/lotto2rd/RankTest.java
@@ -7,7 +7,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RankTest {
 
     @Test
-    void _2등_보너스볼() {
+    void _2등_보너스볼_당첨() {
         assertThat(Rank.valueOf(5, true).getWinningMoney()).isEqualTo(Rank.SECOND.getWinningMoney());
+    }
+
+    @Test
+    void _4등_당첨() {
+        assertThat(Rank.valueOf(4, false).getWinningMoney()).isEqualTo(Rank.FOURTH.getWinningMoney());
     }
 }
