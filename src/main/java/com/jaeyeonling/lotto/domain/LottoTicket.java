79b5631cc201e30cf646d1c5c454808ad1eb1ec4
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-26 11:46:11 +0900

    [feat] 로또 티켓 생성 테스트 통과

--- src/main/java/com/jaeyeonling/lotto/domain/LottoTicket.java
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoTicket.java
@@ -1,5 +1,19 @@
 package com.jaeyeonling.lotto.domain;
 
+import com.jaeyeonling.lotto.utils.StringToLottoTransfer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoTicket {
+
+    private final List<Lotto> lottos;
+
+    public LottoTicket(final List<String> lottoNumbers) {
+        this.lottos = lottoNumbers.stream()
+                .map(StringToLottoTransfer::transform)
+                .collect(Collectors.toList());
+    }
+
 
 }
