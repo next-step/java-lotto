d265f61004dd684c578a3b2ef847f5cfd6f364f7
daioooor36 <pd6016@naver.com>
2019-06-26 23:08:20 +0900

    test(Rank) : 테스트가 필요한 부분까지 테스트코드 작성

--- src/test/java/step3/iksoo/lotto2rd/RankTest.java
+++ src/test/java/step3/iksoo/lotto2rd/RankTest.java
@@ -5,18 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RankTest {
-    @Test
-    void getCountOfMatchTest() {
-        assertThat(Rank.FIFTH.getCountOfMatch()).isEqualTo(3);
-    }
-
-    @Test
-    void getWinningMoneyTest() {
-        assertThat(Rank.FIFTH.getWinningMoney()).isEqualTo(5_000);
-    }
 
     @Test
     void _2등_보너스볼() {
-        assertThat(Rank.valueOf(5, true).getWinningMoney()).isEqualTo(30_000_000);
+        assertThat(Rank.valueOf(5, true).getWinningMoney()).isEqualTo(Rank.SECOND.getWinningMoney());
     }
 }
