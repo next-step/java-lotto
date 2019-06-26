76e6d6bd60f42cfbd255176c7e366c2e789ca67d
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-26 11:54:37 +0900

    [feat] LottoTicket 생성 시 LottoNumbers 빈값 예외처리 테스트 통과

--- src/main/java/com/jaeyeonling/lotto/domain/LottoTicket.java
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoTicket.java
@@ -1,5 +1,7 @@
 package com.jaeyeonling.lotto.domain;
 
+import com.jaeyeonling.lotto.exception.EmptyLottoNumberException;
+import com.jaeyeonling.lotto.utils.ListUtils;
 import com.jaeyeonling.lotto.utils.StringToLottoTransfer;
 
 import java.util.List;
@@ -10,6 +12,10 @@ public class LottoTicket {
     private final List<Lotto> lottos;
 
     public LottoTicket(final List<String> lottoNumbers) {
+        if (ListUtils.isNullOrEmpty(lottoNumbers)) {
+            throw new EmptyLottoNumberException();
+        }
+
         this.lottos = lottoNumbers.stream()
                 .map(StringToLottoTransfer::transform)
                 .collect(Collectors.toList());
--- src/main/java/com/jaeyeonling/lotto/exception/EmptyLottoNumberException.java
+++ src/main/java/com/jaeyeonling/lotto/exception/EmptyLottoNumberException.java
@@ -1,5 +1,11 @@
 package com.jaeyeonling.lotto.exception;
 
-public class EmptyLottoNumberException {
+public class EmptyLottoNumberException extends IllegalArgumentException {
 
+    private static final String ERROR_MESSAGE = "LottoNumbers 는 빈값 일 수 없습니다.";
+
+    public EmptyLottoNumberException() {
+        super(ERROR_MESSAGE);
+    }
 }
+
