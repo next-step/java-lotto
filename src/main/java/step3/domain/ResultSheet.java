c490769bd749be0ad289d88ed26d39449253888b
1nteger <rhep0820@gmail.com>
2019-06-26 21:34:04 +0900

    [step3] refactor : 로또 결과를 담는 Map에 각 등수에 해당하는 당첨로또티켓의 갯수를 담도록 수정

--- src/main/java/step3/domain/ResultSheet.java
+++ src/main/java/step3/domain/ResultSheet.java
@@ -1,26 +1,25 @@
 package step3.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ResultSheet {
 
-    private Map<LottoRank, List<LottoTicket>> resultSheet;
+    private Map<LottoRank, Integer> resultSheet;
 
-    private ResultSheet(Map<LottoRank, List<LottoTicket>> resultSheet) {
+    private ResultSheet(Map<LottoRank, Integer> resultSheet) {
         this.resultSheet = resultSheet;
     }
 
     public static ResultSheet getResult(List<LottoTicket> lottoTickets, LottoTicket luckyNumber) {
-        Map<LottoRank, List<LottoTicket>> lottoResultMap = new HashMap<>();
+        Map<LottoRank, Integer> lottoResultMap = new HashMap<>();
 
         for (LottoTicket ticket : lottoTickets) {
             LottoRank lottoRank = LottoRank.from(ticket.getNumberOfMatchedToLuckyNumber(luckyNumber));
-            List<LottoTicket> ticketsCorrespondingToRank = lottoResultMap.getOrDefault(lottoRank, new ArrayList<>());
-            ticketsCorrespondingToRank.add(ticket);
-            lottoResultMap.put(lottoRank, ticketsCorrespondingToRank);
+            int numberOfTicketsCorrespondingToRank = lottoResultMap.getOrDefault(lottoRank, 0);
+            ++numberOfTicketsCorrespondingToRank;
+            lottoResultMap.put(lottoRank, numberOfTicketsCorrespondingToRank);
         }
         return new ResultSheet(lottoResultMap);
     }
