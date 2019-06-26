9f3152d46dc0066eca0b4b052291b42e1b89b15b
KIM JUNGHWAN <writer0713@naver.com>
2019-06-26 15:07:38 +0900

    #step3 [refactor] Prize 수정
    - valueOf 메소드 추가 (countOfMatchingNumbers와 hasBonusBall 인자를 받아서 Prize를 return 하도록 한다)

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -4,23 +4,31 @@ import java.util.stream.Stream;
 
 public enum Prize {
 
-    LOSE(0, 0),
-    FOURTH(3,5_000),
-    THIRD(4,50_000),
-    SECOND(5,1_500_000),
-    FIRST(6,2_000_000_000);
+    LOSE(0, 0, false),
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false);
 
     private int countOfMatchingNumbers;
     private int prizeMoney;
+    private boolean hasBonusBall;
 
-    Prize(int countOfWinningNumbers, int prizeMoney) {
+    Prize(int countOfWinningNumbers, int prizeMoney, boolean hasBonusBall) {
         this.countOfMatchingNumbers = countOfWinningNumbers;
         this.prizeMoney = prizeMoney;
+        this.hasBonusBall = hasBonusBall;
     }
 
     public static Prize valueOf(int countOfMatchingNumbers) {
+        return valueOf(countOfMatchingNumbers, false);
+    }
+
+    public static Prize valueOf(int countOfMatchingNumbers, boolean hasBonusBall) {
         return Stream.of(Prize.values())
                 .filter(prize -> prize.countOfMatchingNumbers == countOfMatchingNumbers)
+                .filter(prize -> prize.hasBonusBall == hasBonusBall)
                 .findFirst()
                 .orElse(LOSE);
     }
