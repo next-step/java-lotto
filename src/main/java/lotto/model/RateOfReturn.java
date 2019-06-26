430326307484a178a53a71d3dc0f6d270c6f0867
Juyoung <juy5790@outlook.com>
2019-06-26 18:01:09 +0900

    refactor(lotto): 수익률 계산 버그수정

--- src/main/java/lotto/model/RateOfReturn.java
+++ src/main/java/lotto/model/RateOfReturn.java
@@ -5,6 +5,6 @@ public class RateOfReturn {
     public static double calculate(LottoResult lottoResult) {
         Money totalBuyMoney = lottoResult.calculateTotalBuyMoney();
         Money totalOfPrize = lottoResult.calculateTotalWinningMoney();
-        return totalBuyMoney.divide(totalOfPrize);
+        return totalOfPrize.divide(totalBuyMoney);
     }
 }
