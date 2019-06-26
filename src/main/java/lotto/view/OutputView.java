762f68ecada00cb344905b590f09fa37cd8b94ec
leechang0423 <leechang0423@naver.com>
2019-06-26 13:38:35 +0900

    [step4] feat(lotto) 수동번호에 대한 통계내용 outputView 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -15,7 +15,7 @@ import lotto.Statistics;
 public class OutputView {
 
   public static final String NUMBERS_DELIMITER = ", ";
-  public static final String PURCHASE_COUNT_FORMAT = "%d 개를 구매했습니다.";
+  public static final String PURCHASE_COUNT_FORMAT = "수동으로 %d장, 자동으로 %d 개를 구매했습니다.";
   public static final String LOTTO_NUMBERS_FORMAT = "[ %s ]";
   public static final String LOTTO_REWARDS_FORMAT = "%d개 일치 (%d원) - %d개";
   public static final String LOTTO_SECOND_REWARDS_FORMAT = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
@@ -24,8 +24,8 @@ public class OutputView {
   public static final String LOSS = "손해";
   public static final int BREAK_EVEN_POINT = 1;
 
-  public static void printPurchasedLottoQuantity(Quantity quantity) {
-    System.out.printf(PURCHASE_COUNT_FORMAT, quantity.getValue());
+  public static void printPurchasedLottoQuantity(Quantity manualQuantity, Quantity autoQuantity) {
+    System.out.printf(PURCHASE_COUNT_FORMAT, manualQuantity.getValue(),autoQuantity.getValue());
     printBlankLine();
   }
 
