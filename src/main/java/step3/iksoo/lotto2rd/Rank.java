cb1c304a50f7a30ccc4505f7c6342a9a6bca8f35
daioooor36 <pd6016@naver.com>
2019-06-26 21:50:51 +0900

    fix(Rank) : 매직넘버 제거

--- src/main/java/step3/iksoo/lotto2rd/Rank.java
+++ src/main/java/step3/iksoo/lotto2rd/Rank.java
@@ -3,7 +3,6 @@ package step3.iksoo.lotto2rd;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public enum Rank {
@@ -39,7 +38,7 @@ public enum Rank {
     public static List<Rank> winValues() {
         return Arrays.stream(values())
                 .sorted(Comparator.reverseOrder())
-                .filter(n -> n.getCountOfMatch() > 0)
+                .filter(n -> n.getCountOfMatch() > FAIL.getCountOfMatch())
                 .collect(Collectors.toList());
     }
 
