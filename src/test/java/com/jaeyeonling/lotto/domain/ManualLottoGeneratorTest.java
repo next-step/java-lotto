53d49c45e5ffe5421d15cdfcd1ae0f5ac4a4662c
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-26 12:54:49 +0900

    [test] 불필요한 코드 제거

--- src/test/java/com/jaeyeonling/lotto/domain/ManualLottoGeneratorTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/ManualLottoGeneratorTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ManualLottoGeneratorTest {
 
