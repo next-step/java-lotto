cfacef7d6ff014d82e7f51823eac338d4b5231fb
leechang0423 <leechang0423@naver.com>
2019-06-26 09:49:48 +0900

    [step3] feat(lotto) 금액이 로또 가격보다 낮을때에 대한 예외사항 처리

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -17,8 +17,16 @@ public class MoneyTest {
   public void 금액이_0보다_작을수없다() {
     assertThatThrownBy(() -> {
       new Money(-1);
-    }).isInstanceOf(RuntimeException.class)
+    }).isInstanceOf(IllegalArgumentException.class)
         .hasMessageContaining("금액은 0보다 커야 합니다.");
+  }
+
+  @Test
+  public void 금액이_로또하나_가격보다_낮으면_예외처리한다() {
+    assertThatThrownBy(() -> {
+      new Money(900);
+    }).isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("로또를 살 수 없습니다.");
   }
 
 }
