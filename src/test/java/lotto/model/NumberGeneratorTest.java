deb07ad8b3f8a8618911a6ed3e8eaad75ffc458b
KIM JUNGHWAN <writer0713@naver.com>
2019-06-26 15:30:58 +0900

    #step3 [refactor] 중복되는 테스트 제거
    randomNumberGenerate 테스트와 중복되는 테스트라서 삭제

--- src/test/java/lotto/model/NumberGeneratorTest.java
+++ src/test/java/lotto/model/NumberGeneratorTest.java
@@ -3,23 +3,10 @@ package lotto.model;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class NumberGeneratorTest {
-
-  @Test
-  void makeNumbers() {
-    List<Integer> numbers = new Random().ints(1, 46)
-            .distinct()
-            .limit(Lotto.COUNT_OF_NUMBERS)
-            .boxed()
-            .collect(Collectors.toList());
-
-    assertThat(numbers.size()).isEqualTo(Lotto.COUNT_OF_NUMBERS);
-  }
 
   @Test
   void randomNumberGenerate() {
