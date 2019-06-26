3f30162ba736102e4eb24c7e4dae6127bba1d669
leechang0423 <leechang0423@naver.com>
2019-06-26 13:37:38 +0900

    [step4] feat(lotto) 수동번호 입력받도록 inputView수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,8 @@ public class InputView {
   public final static String QUESTION_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
   public final static String QUESTION_LAST_WEEK_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
   public final static String QUESTION_BONUS_BALL_NUMBER = "보너스 볼을 입력해 주세요.";
+  public final static String QUESTION_MANUAL_LOTTO_BUY_QUANTITY = "수동으로 구매할 로또 수를 입력해 주세요.";
+  public final static String QUESTION_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
   static Scanner scanner = new Scanner(System.in);
 
   public static int askPurchaseAmount() {
@@ -24,4 +26,16 @@ public class InputView {
     return scanner.nextInt();
   }
 
+  public static int askManualLottoBuyQuantity() {
+    System.out.println(QUESTION_MANUAL_LOTTO_BUY_QUANTITY);
+    return scanner.nextInt();
+  }
+
+  public static void askManualLottoNumbers() {
+    System.out.println(QUESTION_MANUAL_LOTTO_NUMBERS);
+  }
+
+  public static String manualLottoNumberInput() {
+    return scanner.next();
+  }
 }
