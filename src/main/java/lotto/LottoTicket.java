63cfb08b87d44dc2105e5e913f3df57629c9475a
seungkil choi <seung-kil.choi@raizegls.com>
2019-06-27 01:33:45 +0900

    [PASS] 숫자 추가시 허용 최대 개수 체크 코드 추가

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,17 +1,24 @@
 package lotto;
 
 import lotto.exception.DuplicateNumberException;
+import lotto.exception.OutOfCountException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTicket {
 
+	private static final int MAX_COUNT = 6;
+
 	List<Integer> numbers = new ArrayList<>();
 
 	public void add(int number) {
 		if(numbers.contains(number)){
 			throw new DuplicateNumberException();
+		}
+
+		if(numbers.size() == MAX_COUNT){
+			throw new OutOfCountException();
 		}
 
 		numbers.add(number);
