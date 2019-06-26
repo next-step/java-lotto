c384a026ea1807d28c0afd74e918456d2be10e2a
sy.kim <kauksy9022@gmail.com>
2019-06-26 23:44:44 +0900

    fix(CalculatorTest): ValiadationTest 추가로 일부 역할 분담.

--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringAddCalculatorTest{
     private StringAddCalculator cal;
@@ -11,12 +12,6 @@ public class StringAddCalculatorTest{
     @BeforeEach
     public void setup() {
         cal = new StringAddCalculator();
-    }
-
-    @Test
-    public void add_null_또는_빈문자()  {
-        assertThat(cal.checkInputNumber(null)).isEqualTo(0);
-        assertThat(cal.checkInputNumber("")).isEqualTo(0);
     }
 
     @Test
@@ -42,4 +37,13 @@ public class StringAddCalculatorTest{
         int[] basicSplitInputNumber = {1, 2, 3};
         assertThat(cal.add(basicSplitInputNumber)).isEqualTo(6);
     }
+
+    @Test
+    public void add_negative() throws Exception {
+        int[] basicSplitInputNumber = {-1, 2, 3};
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            cal.add(basicSplitInputNumber);
+        });
+    }
+
 }
