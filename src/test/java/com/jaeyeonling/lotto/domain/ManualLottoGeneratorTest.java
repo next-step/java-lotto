283769dbd26b8c0e471d87e0f202ea1d60005dcd
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-26 12:48:04 +0900

    [test] 로또 여러개 수동 생성 테스트 추가

--- src/test/java/com/jaeyeonling/lotto/domain/ManualLottoGeneratorTest.java
+++ src/test/java/com/jaeyeonling/lotto/domain/ManualLottoGeneratorTest.java
@@ -2,7 +2,10 @@ package com.jaeyeonling.lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,5 +21,31 @@ class ManualLottoGeneratorTest {
         final Lotto expect = manualLottoGenerator.generate();
 
         assertThat(lotto).isEqualTo(expect);
+    }
+
+    @DisplayName("로또 여러개 수동 생성 ")
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1,
+            2,
+            23,
+            50,
+            1000
+    })
+    void should_return_manualLottos_when_generate(final int count) {
+        final List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new FixtureLotto());
+        }
+
+        final LottoGenerator manualLottoGenerator = new ManualLottoGenerator(lottos);
+
+        final List<Lotto> expect = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            expect.add(manualLottoGenerator.generate());
+        }
+
+        assertThat(lottos).hasSize(count);
+        assertThat(lottos).containsAll(expect);
     }
 }
