5aa8b3729c0cbe890e63b9210bd6cfe19f32c108
sy.kim <kauksy9022@gmail.com>
2019-06-26 18:22:47 +0900

    feat(Controller): Controller와 유사한 역할을 하는 메인.

---
+++ src/main/java/step1/CalculatorApplication.java
@@ -0,0 +1,15 @@
+package step1;
+
+public class CalulatorApplication {
+    private InputView inputView;
+
+    private CalulatorApplication(){
+        this.inputView = new InputView();
+
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
