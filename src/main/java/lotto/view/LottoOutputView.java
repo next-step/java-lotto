76e9b57bd7012eb7346c119d35fc2ee6ac90e254
KIM JUNGHWAN <writer0713@naver.com>
2019-06-26 15:52:24 +0900

    #step3 [refactor] OutputView 수정
    - 2등에 대한 output format 추가
    - format을 상수로 선언하여 사용

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -6,9 +6,15 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class LottoOutputView {
+
+  private static final String FORMAT_FOR_BUYING_LOTTOS = "%d 개를 구매했습니다.";
+  private static final String FORMAT_FOR_RATE_OF_RETURN = "총 수익률은 %.2f 입니다.";
+  private static final String REWARD_FORMAT_FOR_SECOND_PRIZE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
+  private static final String REWARD_FORMAT_FOR_COMMON_PRIZE = "%d개 일치 (%d원) - %d개";
+
   public static void printCountOf(Lottos lottos) {
-    String msg = lottos.getCount() + "개를 구매했습니다.";
-    System.out.println(msg);
+    System.out.printf(FORMAT_FOR_BUYING_LOTTOS, lottos.getCount());
+    printLineBreak();
   }
 
   public static void printPickedNumbersOf(Lottos lottos) {
@@ -23,11 +29,26 @@ public class LottoOutputView {
   private static void printResultOf(Map<Prize, Integer> prizeStatus) {
     Stream.of(Prize.values())
             .filter(prize -> prize != Prize.LOSE)
-            .forEach(prize -> System.out.println(
-                    String.format("%d개 일치 (%d원) - %d개", prize.getCountOfMatchingNumbers(), prize.getPrizeMoney(), prizeStatus.getOrDefault(prize, 0))));
+            .forEach(prize -> {
+              printRewardWith(prize, prizeStatus);
+              printLineBreak();
+            });
+  }
+
+  private static void printRewardWith(Prize prize, Map<Prize, Integer> prizeStatus) {
+    if (prize.equals(Prize.SECOND)) {
+      System.out.printf(REWARD_FORMAT_FOR_SECOND_PRIZE, prize.getCountOfMatchingNumbers(), prize.getPrizeMoney(), prizeStatus.getOrDefault(prize, 0));
+      return;
+    }
+    System.out.printf(REWARD_FORMAT_FOR_COMMON_PRIZE, prize.getCountOfMatchingNumbers(), prize.getPrizeMoney(), prizeStatus.getOrDefault(prize, 0));
+  }
+
+  private static void printLineBreak() {
+    System.out.println();
   }
 
   private static void print(double rateOfReturn) {
-    System.out.println(String.format("총 수익률은 %.2f 입니다.", rateOfReturn));
+    printLineBreak();
+    System.out.printf(FORMAT_FOR_RATE_OF_RETURN, rateOfReturn);
   }
 }
