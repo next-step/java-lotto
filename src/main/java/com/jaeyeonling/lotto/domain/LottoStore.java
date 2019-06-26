008903e55e3006955ab4d63ab8e7e3a57bfd0a88
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-26 11:24:46 +0900

    [test] 자동 구매 테스트 추가

---
+++ src/main/java/com/jaeyeonling/lotto/domain/LottoStore.java
@@ -0,0 +1,10 @@
+package com.jaeyeonling.lotto.domain;
+
+import java.util.List;
+
+public class LottoStore {
+
+    public List<Lotto> buyAutoByRemainingMoney(final Money money) {
+        return null;
+    }
+}
---
+++ src/test/java/com/jaeyeonling/lotto/domain/LottoStoreTest.java
@@ -0,0 +1,24 @@
+package com.jaeyeonling.lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoStoreTest {
+
+    @DisplayName("자동 구매 테스트")
+    @Test
+    void autoBuy() {
+        final int countOfLotto = 10;
+        final Money money = new Money(countOfLotto * Lotto.PRICE_VALUE);
+        final LottoStore lottoStore = new LottoStore();
+
+        final List<Lotto> lottos = lottoStore.buyAutoByRemainingMoney(money);
+
+        assertThat(lottos).hasSize(countOfLotto);
+    }
+
+}
