fdad146f7b1b335ec690d26575265451d5db5360
권병윤 <sksggg123@gmail.com>
2019-06-26 22:39:40 +0900

    [refactor] : enum 금액 가독성을 위한 수정

--- src/main/java/edu/nextstep/step3/Rank.java
+++ src/main/java/edu/nextstep/step3/Rank.java
@@ -17,10 +17,10 @@ import java.util.stream.Collectors;
 public enum Rank {
 
     ZERO(0, 0),
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000);
 
     private int match;
     private int lotteryMoney;
