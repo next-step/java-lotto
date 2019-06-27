bc0674670f79855867c6e764fab3272308d71037
KIM JUNGHWAN <writer0713@naver.com>
2019-06-27 17:26:56 +0900

    #step4 [test] LottoGenerator에 수동으로 입력한 숫자들로 로또 생성하는 테스트 작성 (test fail)

--- src/test/java/lotto/model/LottoGeneratorTest.java
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -4,6 +4,8 @@ import lotto.util.LottoNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -23,5 +25,29 @@ public class LottoGeneratorTest {
   void generate2() {
     Lotto lotto = LottoGenerator.generate(LottoNumberGenerator.generate(Stream.of(1, 2, 3, 4, 5, 6)));
     assertThat(lotto.contains(new LottoNumber(1))).isTrue();
+  }
+
+  @Test
+  @DisplayName("로또 group 수동생성")
+  void generateManually() {
+    List<LottoNumber> lottoNumbers = LottoNumberGenerator.generate(Stream.of(1, 2, 3, 4, 5, 6));
+    List<List<LottoNumber>> manualLottoNumbers = Arrays.asList(lottoNumbers);
+    Lottos lottos = LottoGenerator.generateManually(manualLottoNumbers);
+
+    assertThat(lottos.getCount()).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("로또 group 두개를 합성")
+  void union() {
+    Lotto lotto1 = new Lotto(LottoNumberGenerator.generate(Stream.of(1, 3, 5, 6, 9, 11)));
+    Lotto lotto2 = new Lotto(LottoNumberGenerator.generate(Stream.of(2, 4, 6, 8, 10, 12)));
+
+    Lottos lottos1 = new Lottos(Arrays.asList(lotto1));
+    Lottos lottos2 = new Lottos(Arrays.asList(lotto2));
+
+    Lottos union = LottoGenerator.union(lottos1, lottos2);
+
+    assertThat(union.getLottos()).contains(lotto1, lotto2);
   }
 }
