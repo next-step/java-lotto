87159babb2c8e727b0b873381463ec69cc0e5dfa
KIM JUNGHWAN <writer0713@naver.com>
2019-06-27 13:57:35 +0900

    #step4 [test] Money 객체 생성 및 구매가능개수 테스트 작성 (test fail)

---
+++ src/main/java/lotto/model/Money.java
@@ -0,0 +1,10 @@
+package lotto.model;
+
+public class Money {
+
+    private final int PAID;
+
+    public Money(int PAID) {
+        this.PAID = PAID;
+    }
+}
---
+++ src/test/java/lotto/model/MoneyTest.java
@@ -0,0 +1,21 @@
+package lotto.model;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class MoneyTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {15000, 15020})
+    void test_getPurchaseableQuantity(int value) {
+        Money money = new Money(value);
+        int quantity = money.getPurchaseableQuantity();
+
+        assertThat(quantity).isEqualTo(15);
+
+    }
+}
