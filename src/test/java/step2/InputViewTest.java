99e742f1b5fd504e82c702fad57e64634179f786
sy.kim <kauksy9022@gmail.com>
2019-06-27 16:08:24 +0900

    feat(InputViewTest): 입력받을 사항에 대한 테스트.

---
+++ src/test/java/step2/InputViewTest.java
@@ -0,0 +1,15 @@
+package step2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+}
