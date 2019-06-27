ce861780119f854daff5e1534a5b4fe9a02ba0ec
KIM JUNGHWAN <writer0713@naver.com>
2019-06-27 17:27:34 +0900

    #step4 [feature] 수동으로 로또번호 입력하고 생성하도록 구현 (test success)

--- src/main/java/lotto/model/LottoGenerator.java
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class LottoGenerator {
 
@@ -17,12 +20,24 @@ public class LottoGenerator {
     int quantity = paid.getPurchaseableQuantity();
     List<Lotto> lottos = IntStream.range(0, quantity)
             .mapToObj((int value) -> new Lotto(numberGenerator.generate(QUANTITY_OF_THE_NUMBER_TO_GENERATE)))
-            .collect(Collectors.toList());
+            .collect(toList());
 
     return new Lottos(lottos);
   }
 
   public static Lotto generate(List<LottoNumber> numbers) {
     return new Lotto(numbers);
+  }
+
+  public static Lottos generateManually(List<List<LottoNumber>> lottoNumbers) {
+    List<Lotto> lottos = lottoNumbers.stream().map(Lotto::new).collect(toList());
+    return new Lottos(lottos);
+  }
+
+  public static Lottos union(Lottos lottos1, Lottos lottos2) {
+    List<Lotto> unionLottos = new ArrayList<>();
+    unionLottos.addAll(lottos1.getLottos());
+    unionLottos.addAll(lottos2.getLottos());
+    return new Lottos(unionLottos);
   }
 }
