cbe114292af445de6e0b9cf30b7b21fb4b1bef58
seungkil choi <seung-kil.choi@raizegls.com>
2019-06-27 11:41:24 +0900

    [PASS] 값이 비었는 복권 반환해서 테스트 통과

--- src/main/java/lotto/LotteryStore.java
+++ src/main/java/lotto/LotteryStore.java
@@ -1,11 +1,19 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LotteryStore {
 
+	private static final int PRICE = 1000;
+
 	public List<LottoTicket> buy(int money) {
-		return new ArrayList<>();
+
+		int availableCount = money / PRICE;
+
+		return IntStream.range(0, availableCount)
+				.mapToObj(index -> new LottoTicket())
+				.collect(Collectors.toList());
 	}
 }
