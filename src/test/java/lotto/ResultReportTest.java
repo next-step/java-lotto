1b8025ae863c8b812b600bdb6f8e3a5d88239d6b
seungkil choi <seung-kil.choi@raizegls.com>
2019-06-27 15:55:47 +0900

    테스트 오류 확인 후 수정

--- src/test/java/lotto/ResultReportTest.java
+++ src/test/java/lotto/ResultReportTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,11 +20,11 @@ class ResultReportTest {
 	void verifyReward(LottoResult result, int expectReward) {
 
 		ResultReport report = new ResultReport();
-		report.increment(LottoResult.WIN_1ST);
+		report.increment(result);
 
 		int totalReward = report.totalReward();
 
-		assertThat(totalReward).isEqualTo(LottoResult.WIN_1ST.getReward());
+		assertThat(totalReward).isEqualTo(expectReward);
 	}
 
 	private static Stream<Arguments> provideSingleResult(){
