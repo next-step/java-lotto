0246b914e6dda1d30b2bb1cd57dc3cad3f9ec26e
1nteger <rhep0820@gmail.com>
2019-06-28 23:53:02 +0900

    [step4] refactor : 결과 출력 로직 개선

--- src/main/java/step4/view/OutputView.java
+++ src/main/java/step4/view/OutputView.java
@@ -7,8 +7,8 @@ import step4.domain.ResultSheet;
 
 public class OutputView {
     private static final String MESSAGE_FOR_TOTAL_NUMBER_OF_LOTTO_BOUGHT = "수동으로 %d개, 자동으로 %d개를 구매했습니다.";
-    private static final String MESSAGE_FOR_LOTTO_RESULT = "%d개 일치 (%d원) - %d개";
-    private static final String MESSAGE_FOR_ONLY_SECOND_PLACE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
+    private static final String MESSAGE_FOR_ALL_PLACE = "%d개 일치 (%d원) - %d개";
+    private static final String MESSAGE_FOR_SECOND_PLACE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개";
     private static final String MESSAGE_FOR_EARNING_RATE = "총 수익률은 %.2f 입니다.";
     private static final int PRIZE_FOR_LOSER = 0;
     private static final int SECOND_PLACE_PRIZE = 30_000_000;
@@ -29,23 +29,14 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---------");
         for (LottoRank rank : LottoRank.values()) {
-            printResultContent(result, rank);
+            String messageFormat =
+                    (rank.getPrizeMoney() == SECOND_PLACE_PRIZE) ? MESSAGE_FOR_SECOND_PLACE : MESSAGE_FOR_ALL_PLACE;
+            System.out.println(String.format(messageFormat,
+                    rank.getNumberOfMatchingNumber(),
+                    rank.getPrizeMoney(),
+                    result.getResultMap().getOrDefault(rank, PRIZE_FOR_LOSER)));
         }
         System.out.println(String.format(MESSAGE_FOR_EARNING_RATE, result.getEarningRate()));
     }
 
-    private static void printResultContent(ResultSheet result, LottoRank rank) {
-        if (rank.getPrizeMoney() == SECOND_PLACE_PRIZE) {
-            System.out.println(String.format(MESSAGE_FOR_ONLY_SECOND_PLACE,
-                    rank.getNumberOfMatchingNumber(),
-                    rank.getPrizeMoney(),
-                    result.getResultMap().getOrDefault(rank, PRIZE_FOR_LOSER)));
-        }
-        if (rank != LottoRank.LOSER && rank.getPrizeMoney() != SECOND_PLACE_PRIZE) {
-            System.out.println(String.format(MESSAGE_FOR_LOTTO_RESULT,
-                    rank.getNumberOfMatchingNumber(),
-                    rank.getPrizeMoney(),
-                    result.getResultMap().getOrDefault(rank, PRIZE_FOR_LOSER)));
-        }
-    }
 }
