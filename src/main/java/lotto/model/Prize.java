b8770c4c0e3eda8063e7d4c4c953428cdac1a636
Juyoung <juy5790@outlook.com>
2019-06-28 11:43:51 +0900

    refactor(lotto): Prize 비즈니스 로직 변경

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -4,40 +4,35 @@ import java.util.Arrays;
 
 public enum Prize {
 
-    FIRST(6, false, Money.won(2_000_000_000)),
-    SECOND(5, true, Money.won(30_000_000)),
-    THIRD(5, false, Money.won(1_500_000)),
-    FOURTH(4, false, Money.won(50_000)),
-    FIFTH(3, false, Money.won(5_000)),
-    NONE(0, false, Money.ZERO);
+    FIRST(6, Money.won(2_000_000_000)),
+    SECOND(5, Money.won(30_000_000)),
+    THIRD(5, Money.won(1_500_000)),
+    FOURTH(4, Money.won(50_000)),
+    FIFTH(3, Money.won(5_000)),
+    NONE(0, Money.ZERO);
 
     private final int matchCount;
-    private final boolean existBonus;
     private final Money money;
 
-    Prize(int matchCount, boolean existBonus, Money money) {
+    Prize(int matchCount, Money money) {
         this.matchCount = matchCount;
-        this.existBonus = existBonus;
         this.money = money;
     }
 
     static Prize of(int matchCount, boolean existBonus) {
         return Arrays.stream(Prize.values())
-                .filter(prize -> prize.isMatchPrize(matchCount, existBonus))
+                .filter(prize -> prize.isMatchCount(matchCount))
+                .filter(prize -> prize.isSecond(existBonus))
                 .findFirst()
                 .orElse(NONE);
-    }
-
-    private boolean isMatchPrize(int matchCount, boolean existBonus) {
-        return isMatchCount(matchCount) && hasBonus(existBonus);
     }
 
     private boolean isMatchCount(int matchCount) {
         return this.matchCount == matchCount;
     }
 
-    private boolean hasBonus(boolean existBonus) {
-        return this.existBonus == existBonus;
+    private boolean isSecond(boolean existBonus) {
+        return this != SECOND || existBonus;
     }
 
     public Money getMoney() {
