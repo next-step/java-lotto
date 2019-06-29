b990fe8581da8199cf1fcc411e09018945b0b473
sogoagain <imyong0@gmail.com>
2019-06-30 04:37:06 +0900

    refactor: Rank에서 당첨금을 Money 객체로 표현

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -3,17 +3,17 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, Money.valueOf(2_000_000_000)),
+    SECOND(5, Money.valueOf(30_000_000)),
+    THIRD(5, Money.valueOf(1_500_000)),
+    FOURTH(4, Money.valueOf(50_000)),
+    FIFTH(3, Money.valueOf(5_000)),
+    MISS(0, Money.valueOf(0));
 
+    private Money winningMoney;
     private int countOfMatch;
-    private int winningMoney;
 
-    Rank(int countOfMatch, int winningMoney) {
+    Rank(int countOfMatch, Money winningMoney) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
     }
@@ -29,7 +29,7 @@ public enum Rank {
                 .orElse(MISS);
     }
 
-    public int getWinningMoney() {
+    public Money getWinningMoney() {
         return winningMoney;
     }
 
