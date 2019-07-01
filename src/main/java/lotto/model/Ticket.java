d1ecec799e7a2cf7186fb474407dd1d960e6db32
sogoagain <imyong0@gmail.com>
2019-07-01 21:14:00 +0900

    refactor: Ticket 콘솔 출력을 toString()으로 구현

--- src/main/java/lotto/model/Ticket.java
+++ src/main/java/lotto/model/Ticket.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 public class Ticket {
     static final String OUT_OF_SIZE_MESSAGE = "Ticket에 담길 수 있는 Ball의 갯수가 아닙니다.";
     static final int SIZE = 6;
+    private static final String FORMAT_OF_TICKET = "[%s]";
+    private static final String DELIMITER = ", ";
 
     private final Set<Ball> balls;
 
@@ -65,5 +67,14 @@ public class Ticket {
     @Override
     public int hashCode() {
         return Objects.hash(balls);
+    }
+
+    @Override
+    public String toString() {
+        String string = balls.stream()
+                .map(Ball::toInteger)
+                .map(Object::toString)
+                .collect(Collectors.joining(DELIMITER));
+        return String.format(FORMAT_OF_TICKET, string);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,30 +4,19 @@ import lotto.model.Rank;
 import lotto.model.Statistics;
 import lotto.model.Ticket;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class OutputView {
     private static final String MESSAGE_OF_NUMBER_OF_TICKET = "%d개를 구매했습니다.";
-    private static final String FORMAT_OF_TICKET = "[%s]";
     private static final String MESSAGE_OF_RESULT_INTRO = "\n당첨 통계\n---------";
     private static final String MESSAGE_OF_WINNING_RESULT = "%d개 일치 (%s원)- %d개";
     private static final String MESSAGE_OF_SECOND_WINNING_RESULT = "%d개 일치, 보너스 볼 일치 (%s원)- %d개";
     private static final String MESSAGE_OF_PROFIT_RATIO = "총 수익률은 %.2f입니다.";
-    private static final String DELIMITER = ", ";
 
     public static void printNumberOfTickets(int numberOfTickets) {
         System.out.println(String.format(MESSAGE_OF_NUMBER_OF_TICKET, numberOfTickets));
     }
 
     public static void printTicket(Ticket ticket) {
-        List<Integer> numbers = ticket.getNumbers();
-
-        String joinedNumbers = numbers.stream()
-                .map(Object::toString)
-                .collect(Collectors.joining(DELIMITER));
-
-        System.out.println(String.format(FORMAT_OF_TICKET, joinedNumbers));
+        System.out.println(ticket);
     }
 
     public static void printResult(Statistics statistics) {
