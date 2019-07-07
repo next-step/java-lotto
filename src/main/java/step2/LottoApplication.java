6846edc33d885d7e6df1fd49b8159c743832a0c6
sy.kim <kauksy9022@gmail.com>
2019-07-07 18:09:12 +0900

    style(Application): resultView 추가로 로컬변수 제거

--- src/main/java/step2/LottoApplication.java
+++ src/main/java/step2/LottoApplication.java
@@ -2,12 +2,13 @@ package step2;
 
 public class LottoApplication {
 
-
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        int cash = inputView.inputCash();
-        String[] inputWinningNumbers = inputView.inputWinningNumbers();  // 공백 제거하지 않은 상테
+        ResultView resultView = new ResultView();
 
+        resultView.countLottoResult(inputView.inputCash());
+
+        String[] inputWinningNumbers = inputView.inputWinningNumbers();  // 공백 제거하지 않은 상테
 
 
     }
