d97ca2c5466d6ed21f5abba024161311bd79a9da
sy.kim <kauksy9022@gmail.com>
2019-07-07 17:45:20 +0900

    style(LottoApplication): inputView의 메소드명 수정

--- src/main/java/step2/LottoApplication.java
+++ src/main/java/step2/LottoApplication.java
@@ -6,7 +6,7 @@ public class LottoApplication {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         int cash = inputView.inputCash();
-        String[] inputwinningNumbers = inputView.inputwinningNumbers();  // 공백 제거하지 않은 상테
+        String[] inputWinningNumbers = inputView.inputWinningNumbers();  // 공백 제거하지 않은 상테
 
 
 
