0d083bedd540dca5dfd468ccb99d19e0b9483912
sy.kim <kauksy9022@gmail.com>
2019-07-07 17:43:12 +0900

    feat(InputValidationCheck): 입력받은 값 유효성 체크 class 추가

---
+++ src/main/java/step2/InputValidationCheck.java
@@ -0,0 +1,38 @@
+package step2;
+
+public class InputValidationCheck {
+
+    public static void checkEmpty(String cash) {
+        if (cash.isEmpty()) {
+            throw new IllegalArgumentException("구매금액이 없으면, 로또구매가 불가합니다.");
+        }
+    }
+
+    public static void checkCash(String cash) {
+        if (Integer.parseInt(cash) < Lotto.LOTTO_PRICE) {
+            throw new IllegalArgumentException("로또는" + Lotto.LOTTO_PRICE + " 원 입니다.");
+        }
+    }
+
+    public static void checkWinningNumbers(String[] inputWinningNumbers) {
+        int winningNumberLength = inputWinningNumbers.length;
+
+        if (winningNumberLength != Lotto.WINNING_NUMBERS_LENGTH - 1) {
+            throw new IllegalArgumentException("당첨번호는" + winningNumberLength + "개 입력하셔야 합니다.");
+        }
+    }
+
+    public static void checkWinningNumberRange(String[] winningNumbers) {
+        for (int i = 0; i < Lotto.WINNING_NUMBERS_LENGTH; i++) {
+            checkEachWinningNumber(Integer.parseInt(winningNumbers[i].trim()));
+        }
+    }
+
+    private static void checkEachWinningNumber(int winningNumber) {
+        if (winningNumber > Lotto.WINNING_NUMBERS_MAX ||
+                winningNumber < Lotto.WINNING_NUMBERS_MIN) {
+            throw new IllegalArgumentException(
+                    "당첨번호는" + Lotto.WINNING_NUMBERS_MIN + " ~ " + Lotto.WINNING_NUMBERS_MAX + "사이로 입력하셔야 합니다.");
+        }
+    }
+}
