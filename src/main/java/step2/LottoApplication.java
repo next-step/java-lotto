f07dde6eaff34c5a71cc398a47f455ad8ef969e5
sy.kim <kauksy9022@gmail.com>
2019-07-07 15:15:40 +0900

    feat(Application): lotto실행하기위한 application 생성

---
+++ src/main/java/step2/LottoApplication.java
@@ -0,0 +1,14 @@
+package step2;
+
+public class LottoApplication {
+
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int cash = inputView.inputCash();
+        String[] inputwinningNumbers = inputView.inputwinningNumbers();  // 공백 제거하지 않은 상테
+
+
+
+    }
+}
