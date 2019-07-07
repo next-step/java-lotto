29088ca01d0f7e0fbfed318fa60ce4db407e2ebe
sy.kim <kauksy9022@gmail.com>
2019-07-07 17:47:40 +0900

    feat(Lotto): Lotto Class 추가 및 lotto속성과 관련된 상수값 정의

---
+++ src/main/java/step2/Lotto.java
@@ -0,0 +1,10 @@
+package step2;
+
+public class Lotto {
+
+    static final int LOTTO_PRICE = 1000;
+    static final int WINNING_NUMBERS_LENGTH = 6;
+    static final int WINNING_NUMBERS_MIN = 1;
+    static final int WINNING_NUMBERS_MAX = 45;
+
+}
