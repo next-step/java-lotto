83b6db060df7850ed86db43d535e4528a51f79cc
sy.kim <kauksy9022@gmail.com>
2019-07-09 02:35:47 +0900

    fix(Lotto): 구매한 로또번호 생성 메서드 수정

--- src/main/java/step2/Lotto.java
+++ src/main/java/step2/Lotto.java
@@ -1,9 +1,6 @@
 package step2;
 
-import java.util.ArrayList;
-
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
 
@@ -11,23 +8,30 @@ public class Lotto {
     static final int WINNING_NUMBERS_LENGTH = 6;
     static final int WINNING_NUMBERS_MIN = 1;
     static final int WINNING_NUMBERS_MAX = 45;
-//    private  List<List<Integer>> purchasedLotto = new ArrayList<>();
 
-    public List<List<Integer>> generateStandardLotto(int lottoCount) {
+    public List<Integer> generateStandardLotto() {
         List<Integer> standardLotto = new ArrayList<>();
         for (int i = Lotto.WINNING_NUMBERS_MIN; i < Lotto.WINNING_NUMBERS_MAX + 1; i++) {
             standardLotto.add(i);
         }
-        return generateAutoLotto(standardLotto, lottoCount);
+        return standardLotto;
     }
 
-    private List<List<Integer>> generateAutoLotto(List<Integer> standardLotto, int lottoCount) {
-        List<List<Integer>> purchasedLotto = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            Collections.shuffle(standardLotto);
-            purchasedLotto.add(standardLotto.subList(0, WINNING_NUMBERS_LENGTH));
+    public Set<List<Integer>> purchasedLottos(List<Integer> standardLotto, int lottoCount) {
+        Set<List<Integer>> collectAutoLotto = new HashSet<>();
+
+        while (collectAutoLotto.size() < lottoCount) {
+            List<Integer> shuffleLottoNumbers = generateAutoLotto(standardLotto);
+            collectAutoLotto.add(new ArrayList<>(shuffleLottoNumbers));
         }
-//        LottoValidationCheck.checkpurchasedLottoCount(lottoCount, purchasedLotto);
-        return purchasedLotto;
+        return collectAutoLotto;
+    }
+
+    private List<Integer> generateAutoLotto(List<Integer> standardLotto) {
+        Collections.shuffle(standardLotto);
+        List<Integer> shuffleLottoNumbers = standardLotto.subList(0, 6);
+        Collections.sort(shuffleLottoNumbers);
+
+        return shuffleLottoNumbers;
     }
 }
