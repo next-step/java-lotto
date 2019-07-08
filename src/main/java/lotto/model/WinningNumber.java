b2b555b1f0a9e166bcf5278a6f5c152080e66318
choihyunjin <iamchoihyunjin@gmail.com>
2019-07-09 02:46:20 +0900

    feat(WinningNumber): add code throw Exception

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -6,8 +6,16 @@ public class WinningNumber {
     private Number bonusBall;
 
     public WinningNumber(LottoNumbers winningNumbers, Number bonusBall) {
+        validate(winningNumbers, bonusBall);
         this.winningNumbers = winningNumbers;
         this.bonusBall = bonusBall;
+    }
+
+    private void validate(LottoNumbers winningNumbers, Number bonusBall) {
+        if (winningNumbers.getNumbers().stream()
+                .anyMatch(number -> number.getValue() == bonusBall.getValue())) {
+            throw new IllegalArgumentException("보너스 볼과 당첨 번호는 중복 될 수 없습니다.");
+        }
     }
 
     LottoNumbers getWinningNumbers() {
