ac5e6d23f056b29a3135044741433662a1cdf3b6
sy.kim <kauksy9022@gmail.com>
2019-07-09 02:37:54 +0900

    add(LottoApplication): Lotto번호 생성 및 출력 추가

--- src/main/java/step2/LottoApplication.java
+++ src/main/java/step2/LottoApplication.java
@@ -1,15 +1,23 @@
 package step2;
 
+import java.util.*;
+
 public class LottoApplication {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
+        Lotto lotto = new Lotto();
 
-        resultView.countLottoResult(inputView.inputCash());
+        int lottoCount = inputView.inputCash();
+        resultView.countLottoResult(lottoCount);
+        List<Integer> standardLotto = lotto.generateStandardLotto();
+
+        Set<List<Integer>> purchasedLottos = lotto.purchasedLottos(standardLotto, lottoCount);
+        resultView.printPurchasedLottos(purchasedLottos);
 
         String[] inputWinningNumbers = inputView.inputWinningNumbers();  // 공백 제거하지 않은 상테
-
-
     }
+
+
 }
