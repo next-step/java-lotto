3a5a10d9b1d500bab9baa0e131bc8b202144a4c4
sy.kim <kauksy9022@gmail.com>
2019-07-09 02:33:22 +0900

    fix(LottoTest): Test케이스 수정 및 추가진행

--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -2,39 +2,37 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
+import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
+
+    private  Lotto lotto = new Lotto();
+
+    @DisplayName("로또번호 범위 확인: 1 ~ 45")
+    @Test
+    public void 로또번호_범위_확인() {
+        List<Integer> standardLottoNumbers = lotto.generateStandardLotto();
+        Set<List<Integer>> autoLottos = lotto.purchasedLottos(standardLottoNumbers,3);
+        assertThat(autoLottos.size()).isEqualTo(3);
+    }
 
     @DisplayName("생성 된 로또 수량 확인")
     @Test
     public void 생성_된_로또_수량확인() {
-        Lotto lotto = new Lotto();
-        List<List<Integer>> autoLottos = lotto.generateStandardLotto(3);
+        List<Integer> standardLottoNumbers = lotto.generateStandardLotto();
+        Set<List<Integer>> autoLottos = lotto.purchasedLottos(standardLottoNumbers,3);
         assertThat(autoLottos.size()).isEqualTo(3);
     }
 
     @DisplayName("생성 된 로또가 6자리인지 확인")
     @Test
     public void 생성_된_로또_자릿수_확인() {
-        Lotto lotto = new Lotto();
-        List<List<Integer>> autoLottos = lotto.generateStandardLotto(3);
+        List<Integer> standardLottoNumbers = lotto.generateStandardLotto();
+        Set<List<Integer>> autoLottos = lotto.purchasedLottos(standardLottoNumbers, 3);
         for (List<Integer> autoLotto : autoLottos) {
             assertThat(autoLotto.size()).isEqualTo(Lotto.WINNING_NUMBERS_LENGTH);
         }
     }
-
-//    @DisplayName("자동생성 로또 범위 확인: 1 ~ 45")
-//    @Test
-//    public void 자동생성_로또_범위_확인() {
-//        Lotto lotto = new Lotto();
-//        List<List<Integer>> autoLottos = lotto.generateStandardLotto(3);
-//        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkWinningNumberRange(winningNumbers));
-//    }
-
-
 }
