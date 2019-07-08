66f12bf06408f095d6f58854d64bbf39a5d48741
sy.kim <kauksy9022@gmail.com>
2019-07-09 02:44:10 +0900

    add(ResultView): printPurchasedLottos 메소드 추가

--- src/main/java/step2/ResultView.java
+++ src/main/java/step2/ResultView.java
@@ -1,7 +1,17 @@
 package step2;
 
+import java.util.List;
+import java.util.Set;
+
 public class ResultView {
     public void countLottoResult(int countLotto) {
         System.out.println(countLotto + "개를 구매했습니다.");
+    }
+
+    public void printPurchasedLottos(Set<List<Integer>> purchasedLottos) {
+        for (List<Integer> purchasedLotto : purchasedLottos) {
+            System.out.println(purchasedLotto);
+        }
+        System.out.println();
     }
 }
