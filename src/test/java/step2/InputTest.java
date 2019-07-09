953aabe392e718caca78f79e2acc606fe66d549f
sy.kim <kauksy9022@gmail.com>
2019-07-09 16:08:15 +0900

    add(InputTest): 중복 당첨번호 입력여부 확인하는 Test 추가

--- src/test/java/step2/InputTest.java
+++ src/test/java/step2/InputTest.java
@@ -23,9 +23,9 @@ public class InputTest {
 
     @DisplayName("당첨번호 입력받기 확인: 6자리 입력")
     @Test
-    public void 당첨번호_입력_자릿수_확인() {
-        String[] winningNumbers = "30, 2, 12, 22, 45, 8".split(",");
-        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkWinningNumbers(winningNumbers));
+    public void 입력받은_당첨번호_자릿수_확인() {
+        String[] winningNumbers = "30, 2, 12, 22, 45, 100".split(",");
+        assertThat(winningNumbers.length).isEqualTo(Lotto.WINNING_NUMBERS_LENGTH);
     }
 
     @DisplayName("당첨번호 범위 확인: 1 ~ 45")
@@ -35,4 +35,10 @@ public class InputTest {
         assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkWinningNumberRange(winningNumbers));
     }
 
+    @DisplayName("당첨번호 중복 확인")
+    @Test
+    public void 당첨번호_중복_확인() {
+        String[] winningNumbers = "30, 2, 12, 22, 45, 45".split(",");
+        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkOverlapWinningNumber(winningNumbers));
+    }
 }
