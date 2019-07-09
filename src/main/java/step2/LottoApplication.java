128ce30bad67e2daf4b33b75e2c63d22d9a3699c
sy.kim <kauksy9022@gmail.com>
2019-07-09 23:12:59 +0900

    add(Application): 각각의 메서드 호출하도록 수정

--- src/main/java/step2/LottoApplication.java
+++ src/main/java/step2/LottoApplication.java
@@ -8,15 +8,22 @@ public class LottoApplication {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
         Lotto lotto = new Lotto();
+        LottoLottery lottery = new LottoLottery();
 
         int lottoCount = inputView.inputCash();
         resultView.countLottoResult(lottoCount);
-        List<Integer> standardLotto = lotto.generateStandardLotto();
 
-        Set<List<Integer>> purchasedLottos = lotto.purchasedLottos(standardLotto, lottoCount);
+        List<Integer> standardLotto = lotto.generateStandardLotto();
+        Set<ArrayList<Integer>> purchasedLottos = lotto.purchasedLottos(standardLotto, lottoCount);
         resultView.printPurchasedLottos(purchasedLottos);
 
-        int[] winningNumbers = inputView.inputWinningNumbers();  // 공백 제거하지 않은 상테
+        int[] winningNumbers = inputView.inputWinningNumbers();
+        List<Integer> resultMatcing = lottery.lotteryStatics(purchasedLottos, winningNumbers);
+        Map<Integer, Integer> result = lottery.giveStatistic(resultMatcing);
+        resultView.printFinalResult(result);
+        double yield = lottery.calculationOfYield(lottoCount, result);
+        resultView.printYield(yield);
+
     }
 
 
