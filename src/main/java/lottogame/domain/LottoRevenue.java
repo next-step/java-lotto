72a5664af0e177d140010dc1ed5aace4dbbcadd0
Bentley <p2c2kbf@gmail.com>
2019-07-11 13:59:53 +0900

    refactor: revenue 소수점 두자리 출력방식 변경

--- src/main/java/lottogame/domain/LottoRevenue.java
+++ src/main/java/lottogame/domain/LottoRevenue.java
@@ -1,13 +1,11 @@
 package lottogame.domain;
 
 public class LottoRevenue {
-    private final static int SUB_POINT_ONE = 1;
-    private final static int SUB_POINT_TWO = 2;
-
     private double revenue;
 
-    LottoRevenue(double revenue){
+    LottoRevenue(double revenue) {
         this.revenue = revenue;
+
     }
 
     public String judgeResult() {
@@ -18,14 +16,7 @@ public class LottoRevenue {
         return "손해";
     }
 
-    // 계산된 수익율을 퍼센티지로 표현 (소수 2자리까지/ 단, 소수 한자리 일경우 한자리만 표현되도록 )
-    public Double getPercentOfRevenue() {
-        String[] result = String.valueOf(this.revenue).split("\\.");
-        String percent = null;
-        if (result[1].length() == SUB_POINT_ONE)
-            percent = result[0] + "." + result[1].substring(0, SUB_POINT_ONE);
-        if (result[1].length() > SUB_POINT_ONE)
-            percent = result[0] + "." + result[1].substring(0, SUB_POINT_TWO);
-        return Double.parseDouble(percent);
+    public double getRevenue() {
+        return revenue;
     }
 }
--- src/main/java/lottogame/view/ResultView.java
+++ src/main/java/lottogame/view/ResultView.java
@@ -2,11 +2,14 @@ package lottogame.view;
 
 import lottogame.domain.*;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class ResultView {
+    private final static String PATTERN = "#.##";
+
     public static void printLottoNumber(List<LottoNumber> lottoTotal, int sizeOfManualLotto) {
-        System.out.println("수동으로 " + sizeOfManualLotto + "개, 자동으로 " + (lottoTotal.size()-sizeOfManualLotto) + "개를 구매했습니다.");
+        System.out.println("수동으로 " + sizeOfManualLotto + "개, 자동으로 " + (lottoTotal.size() - sizeOfManualLotto) + "개를 구매했습니다.");
         for (LottoNumber lottoNumber : lottoTotal) {
             System.out.println(lottoNumber.getElement());
         }
@@ -15,6 +18,7 @@ public class ResultView {
 
     public static void printResult(LottoResult gameResult) {
         List<WinnerGroupDTO> winnerGroup = LottoRankData.makeWinnersGroup();
+        DecimalFormat form = new DecimalFormat(PATTERN);
 
         System.out.println("\n");
         System.out.println("당첨 통계");
@@ -25,6 +29,6 @@ public class ResultView {
                     System.out.println(winner.getMatchNum() + "개 일치 (" + winner.getPrize() + "원)- " + winner.getMatchCount() + "개");
                 });
 
-        System.out.print("총 수익률은  " + gameResult.getWinningRevenue().getPercentOfRevenue() + "입니다. 기준이 1이기 때문에 결과적으로 " + gameResult.getWinningRevenue().judgeResult() + "라는 의미임)");
+        System.out.print("총 수익률은  " + form.format(gameResult.getWinningRevenue().getRevenue()) + "입니다. 기준이 1이기 때문에 결과적으로 " + gameResult.getWinningRevenue().judgeResult() + "라는 의미임)");
     }
 }
