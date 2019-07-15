622575288d963fa1dcbae2afa4e8b6871c513fc7
Bentley <p2c2kbf@gmail.com>
2019-07-15 12:07:31 +0900

    refactor: LottoPriceTest 함수명 한글로 수정

--- src/test/java/lottogame/domain/LottoPriceTest.java
+++ src/test/java/lottogame/domain/LottoPriceTest.java
@@ -9,15 +9,15 @@ class LottoPriceTest {
     private LottoPrice price;
 
     @Test
-    void price_check() {
+    void 로또가격_체크() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> {
                     price = new LottoPrice(500);
-                });
+                }).withMessage("최소 금액 1000원 보다 작은 금액이 입력되었습니다. 다시 프로그램을 실행 후 올바른 가격을 입력해주세요.");
     }
 
     @Test
-    void countOfLottoGame_check() {
+    void 구매가격을_로또게임_횟수변환_체크() {
         price = new LottoPrice(5000);
         assertThat(price.convertPriceToNumberOfGame()).isEqualTo(5);
     }
