695ce9568cb1bd8c0d81e13de4ccc19050d90555
sy.kim <kauksy9022@gmail.com>
2019-07-18 20:51:52 +0900

    fix(LottoLottery): 대거 수정으로 상세설명은 다음줄에

--- src/main/java/step2/LottoLottery.java
+++ src/main/java/step2/LottoLottery.java
@@ -1,45 +1,29 @@
 package step2;
 
-import com.sun.corba.se.impl.orbutil.ObjectUtility;
-
 import java.util.*;
 
 public class LottoLottery {
 
-    static final int FIRST = 2_000_000_000;
-    static final int SECOND = 1_500_000;
-    static final int THIRD = 50_000;
-    static final int FOURTH = 5_000;
+    static final int MATCHING_LIMIT = 3;
+    private static final int MAKE_FLOAT = 100;
 
-    static final int MAKE_FLOAT = 100;
+    private static Map<Integer, Integer> statisticResult = new HashMap<>();
 
-    private Map<Integer, Integer> statisticResult = new HashMap<>();
-
-    public List<Integer> lotteryStatics(Set<ArrayList<Integer>> purchasedLottos, int[] winningNumbers) {
+    public List<Integer> lotteryStatics(Set<List<Integer>> purchasedLottos, int[] winningNumbers) {
         List<Integer> resultMatching = new ArrayList<>();
-        for (ArrayList<Integer> purchasedLotto : purchasedLottos) {
+        for (List<Integer> purchasedLotto : purchasedLottos) {
             resultMatching.add(matchWinningNumbers(purchasedLotto, winningNumbers));
         }
         return resultMatching;
     }
 
-    private int matchWinningNumbers(ArrayList<Integer> purchasedLotto, int[] winningNumbers) {
-        int matchCount = 0;
-
-        for (int winningNumber : winningNumbers) {
-            matchCount += matchEachNumber(purchasedLotto, winningNumber);
-        }
-        return matchCount;
+    private int matchWinningNumbers(List<Integer> purchasedLotto, int[] winningNumbers) {
+        return (int) Arrays.stream(winningNumbers)
+                .filter(purchasedLotto::contains)
+                .count();
     }
 
-    private int matchEachNumber(ArrayList<Integer> purchasedLotto, int winningNumbers) {
-        if (purchasedLotto.contains(winningNumbers)) {
-            return 1;
-        }
-        return 0;
-    }
-
-    // 통계를 내기위해...Map에 N개 일치하는(Key) 로또가 몇장인지 (Value) 저장
+    // 통계를 내기위해 Map에 N개 일치하는(Key) 로또가 몇장인지 (Value) 저장
     public Map<Integer, Integer> giveStatistic(List<Integer> resultMatching) {
         generateBasicStatistic();
         for (int i = 1; i < Lotto.WINNING_NUMBERS_LENGTH + 1; i++) {
@@ -59,19 +43,16 @@ public class LottoLottery {
     // resultMatching 은 구매로또가 당첨번호와 얼마나 숫자 일치하는지 저장해놓은 List
     private void summaryMatching(List<Integer> resultMatching, int matchCount) {
         if (resultMatching.contains(matchCount)) {
-            completeStatistic(resultMatching, matchCount);
+            int resultMatchCount = completeStatistic(resultMatching, matchCount);
+            statisticResult.put(matchCount, resultMatchCount);
         }
     }
 
     // N개 일치하는 로또가 몇장인지 저장 (Map에 put)
-    private void completeStatistic(List<Integer> resultMatching, int matchCount) {
-        int test = 0;
-        for (int matchingNumber : resultMatching) {
-            if (matchingNumber == matchCount) {
-                test += 1;
-                statisticResult.put(matchCount, test);
-            }
-        }
+    private int completeStatistic(List<Integer> resultMatching, int matchCount) {
+        return (int) Arrays.stream(resultMatching.toArray())
+                .filter(matchingNumber -> matchingNumber.equals(matchCount))
+                .count();
     }
 
     public double calculationOfYield(int lottoCount, Map<Integer, Integer> result) {
@@ -83,14 +64,14 @@ public class LottoLottery {
             removeUnavailableValue(result, i);
         }
         if (!result.isEmpty()) {
-            return (totalWard(result) / spandCash * MAKE_FLOAT) / MAKE_FLOAT;
+            return (totalPrize(result) / spandCash * MAKE_FLOAT) / MAKE_FLOAT;
         }
         return 0;
     }
 
     private void removeUnavailableValue(Map<Integer, Integer> result, int key) {
         // 1. 수익률 계산에 유효한 값은 map의 key가 3인 것부터 유효하므로 삭제
-        if (key < 3) {
+        if (key < MATCHING_LIMIT) {
             result.remove(key);
         }
         // 2. 1. 수익률 계산에 유효한 값은 value가 0 초과인 것이므로 삭제
@@ -99,22 +80,20 @@ public class LottoLottery {
         }
     }
 
-    private double totalWard(Map<Integer, Integer> result) {
+    private double totalPrize(Map<Integer, Integer> result) {
         double totalWard = 0;
-        if (!Objects.isNull(result.get(3)) && result.get(3) != 0) {
-            totalWard += result.get(3).doubleValue() * FOURTH;
+        for (int i = MATCHING_LIMIT; i < Lotto.WINNING_NUMBERS_LENGTH + 1; i++) {
+            totalWard += calculateTotalPrize(result, i);
         }
-        if (!Objects.isNull(result.get(4)) && result.get(4) != 0) {
-            totalWard += result.get(4).doubleValue() * THIRD;
-        }
-        if (!Objects.isNull(result.get(5)) && result.get(5) != 0) {
-            totalWard += result.get(5).doubleValue() * SECOND;
-        }
-        if (!Objects.isNull(result.get(6)) && result.get(6) != 0) {
-            totalWard += result.get(6).doubleValue() * FIRST;
-        }
-
         return totalWard;
+    }
+
+    private double calculateTotalPrize( Map<Integer, Integer> result, int num ){
+        double calculateResult = 0;
+        if (!Objects.isNull(result.get(num)) && result.get(num) != 0) {
+            calculateResult += result.get(num).doubleValue() * Prize.findByCountOfMatch(num).getWinningMoney();
+        }
+        return calculateResult;
     }
 }
 
