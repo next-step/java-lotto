de154de215f2d3be1cfb6618b74ab14441720cad
sy.kim <kauksy9022@gmail.com>
2019-07-18 21:05:35 +0900

    fix(LottoTest): ArrayList -> List로 변경

--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -3,38 +3,36 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
 
-    private  Lotto lotto = new Lotto();
+    private Lotto lotto = new Lotto();
 
     @DisplayName("로또번호 범위 확인: 1 ~ 45")
     @Test
     public void 로또번호_범위_확인() {
-        List<Integer> standardLottoNumbers = lotto.generateStandardLotto();
-        Set<ArrayList<Integer>> autoLottos = lotto.purchasedLottos(standardLottoNumbers,3);
+        Set<List<Integer>> autoLottos = lotto.purchasedLottos(3);
         assertThat(autoLottos.size()).isEqualTo(3);
     }
 
     @DisplayName("생성 된 로또 수량 확인")
     @Test
     public void 생성_된_로또_수량확인() {
-        List<Integer> standardLottoNumbers = lotto.generateStandardLotto();
-        Set<ArrayList<Integer>> autoLottos = lotto.purchasedLottos(standardLottoNumbers,3);
+        List<Integer> standardLottoNumbers = Lotto.generateStandardLotto();
+        Set<List<Integer>> autoLottos = lotto.purchasedLottos(3);
         assertThat(autoLottos.size()).isEqualTo(3);
     }
 
     @DisplayName("생성 된 로또가 6자리인지 확인")
     @Test
     public void 생성_된_로또_자릿수_확인() {
-        List<Integer> standardLottoNumbers = lotto.generateStandardLotto();
-        Set<ArrayList<Integer>> autoLottos = lotto.purchasedLottos(standardLottoNumbers, 3);
-        for (ArrayList<Integer> autoLotto : autoLottos) {
+        List<Integer> standardLottoNumbers = Lotto.generateStandardLotto();
+        Set<List<Integer>> autoLottos = lotto.purchasedLottos(3);
+        for (List<Integer> autoLotto : autoLottos) {
             assertThat(autoLotto.size()).isEqualTo(Lotto.WINNING_NUMBERS_LENGTH);
         }
     }
