586852ce9f91932611d995089d702be15e6d97d0
sy.kim <kauksy9022@gmail.com>
2019-07-18 20:49:01 +0900

    fix(Lotto): 필드에 standardLotto 정의 > 파라미터 간소화

--- src/main/java/step2/Lotto.java
+++ src/main/java/step2/Lotto.java
@@ -8,26 +8,27 @@ public class Lotto {
     static final int WINNING_NUMBERS_LENGTH = 6;
     static final int WINNING_NUMBERS_MIN = 1;
     static final int WINNING_NUMBERS_MAX = 45;
+    static List<Integer> standardLotto = new ArrayList<>();
 
     public static List<Integer> generateStandardLotto() {
-        List<Integer> standardLotto = new ArrayList<>();
+
         for (int i = Lotto.WINNING_NUMBERS_MIN; i < Lotto.WINNING_NUMBERS_MAX + 1; i++) {
             standardLotto.add(i);
         }
         return standardLotto;
     }
 
-    public Set<ArrayList<Integer>> purchasedLottos(List<Integer> standardLotto, int lottoCount) {
-        Set<ArrayList<Integer>> collectAutoLotto = new HashSet<>();
+    public Set<List<Integer>> purchasedLottos(int lottoCount) {
+        Set<List<Integer>> collectAutoLotto = new HashSet<>();
 
         while (collectAutoLotto.size() < lottoCount) {
-            List<Integer> shuffleLottoNumbers = generateAutoLotto(standardLotto);
+            List<Integer> shuffleLottoNumbers = generateAutoLotto();
             collectAutoLotto.add(new ArrayList<>(shuffleLottoNumbers));
         }
         return collectAutoLotto;
     }
 
-    private List<Integer> generateAutoLotto(List<Integer> standardLotto) {
+    private List<Integer> generateAutoLotto() {
         Collections.shuffle(standardLotto);
         List<Integer> shuffleLottoNumbers = standardLotto.subList(0, 6);
         Collections.sort(shuffleLottoNumbers);
