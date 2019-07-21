41cbf79d8d571fc22b6bfffcfc515060b47f080a
ks.choi <dv.mommoo@gmail.com>
2019-07-21 17:25:49 +0900

    feat(LottoNumber) :  - 숫자 wrapper class 작성.
    test(LottoNumber) :
     - 숫자 데이터 검증 코드 추가.
     - 숫자 데이터 크기 검증 코드 추가.
     - pool 이 정상작동 하는지 검증 코드 추가.

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoNumber {
+    public static final int MIN = 1;
+    public static final int MAX = 45;
+
+    private static final IllegalArgumentException NUMBER_SIZE_EXCEPTION
+            = new IllegalArgumentException("the lotto number is must between " + MIN + " and " + MAX);
+    private static final IllegalArgumentException NOT_NUMBER_DATA_EXCEPTION
+            = new IllegalArgumentException("the data is must be numberic data");
+    private static Map<Integer, LottoNumber> LOTTO_NUMBER_POOL = new HashMap<>();
+
+    private final int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber of(String numberString) {
+        if (isNotNumberData(numberString)) {
+            throw NOT_NUMBER_DATA_EXCEPTION;
+        }
+
+        int number = Integer.parseInt(numberString.trim());
+
+        if (isNotBetweenMinAndMax(number)) {
+            throw NUMBER_SIZE_EXCEPTION;
+        }
+
+        LOTTO_NUMBER_POOL.putIfAbsent(number, new LottoNumber(number));
+
+        return LOTTO_NUMBER_POOL.get(number);
+    }
+
+    public int get() {
+        return this.number;
+    }
+
+    private static boolean isNotBetweenMinAndMax(int number) {
+        return number < MIN || MAX < number;
+    }
+
+    private static boolean isNotNumberData(String number) {
+        try {
+            Integer.parseInt(number);
+            return false;
+        } catch (Exception e) {
+            return true;
+        }
+    }
+}
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoNumberTest {
+
+    @DisplayName("숫자 데이터가 아닌 경우 에러를 발생합니다.")
+    @Test
+    public void testIfNotNumbericDataThenInvokeException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.of("string"));
+    }
+
+    @DisplayName("숫자가 1보다 작거나 45 보다 크면 에러를 발생합니다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "55"})
+    public void testIfNotBetween1And45ThenInvokeException(String number) {
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.of(number));
+    }
+
+    @DisplayName("동일한 숫자 값으로 인스턴스를 얻었을 때, 주소가 동일한 인스턴스여야 합니다.")
+    @Test
+    public void testIfInputSameDataThenSameObject() {
+        String data1 = "1";
+        String data2 = "1";
+
+        assertThat(LottoNumber.of(data1)).isSameAs(LottoNumber.of(data2));
+    }
+}
