2fb511ebd5c2b4bbb5f0eaa576c325aa0bdcdb59
sy.kim <kauksy9022@gmail.com>
2019-10-12 11:14:09 +0900

    add(ValidationCheck, InputTest): 보너스 번호를 검증하는 로직과 이를 테스트하는 코드 추가

--- src/main/java/step3/InputValidationCheck.java
+++ src/main/java/step3/InputValidationCheck.java
@@ -5,21 +5,18 @@ import java.util.*;
 public class InputValidationCheck {
 
     public static void checkEmpty(String cash) {
-        if (cash.isEmpty()) {
+        if (cash.isEmpty())
             throw new IllegalArgumentException("구매금액이 없으면, 로또구매가 불가합니다.");
-        }
     }
 
     public static void checkCash(String cash) {
-        if (Integer.parseInt(cash) < Lotto.LOTTO_PRICE) {
+        if (Integer.parseInt(cash) < Lotto.LOTTO_PRICE)
             throw new IllegalArgumentException("로또는 1장에 " + Lotto.LOTTO_PRICE + " 원 입니다.");
-        }
     }
 
     public static void checkWinningNumberLength(String[] trimWinningNumbers) {
-        if (trimWinningNumbers.length != LottoGenerator.WINNING_NUMBERS_LENGTH) {
+        if (trimWinningNumbers.length != LottoGenerator.WINNING_NUMBERS_LENGTH)
             throw new IllegalArgumentException("당첨번호는" + LottoGenerator.WINNING_NUMBERS_LENGTH + "개를 입력하셔야 합니다.");
-        }
     }
 
     public static int[] checkWinningNumberRange(String[] trimWinningNumbers) {
@@ -32,16 +29,26 @@ public class InputValidationCheck {
         return winningNumbers;
     }
 
-    public static void checkEachWinningNumber(int winningNumber) {
-        if (winningNumber > Lotto.LOTTO_NUMBER_MAX || winningNumber < Lotto.LOTTO_NUMBER_MIN) {
-            throw new IllegalArgumentException("당첨번호와 보너스 볼은" + Lotto.LOTTO_NUMBER_MIN + " ~ " + Lotto.LOTTO_NUMBER_MAX + "사이로 입력하셔야 합니다.");
+    public static void checkOverlapWinningNumber(String[] winningNumbers) {
+        Set<String> uniqueWinningNumber = new HashSet<>(Arrays.asList(winningNumbers));
+        if (uniqueWinningNumber.size() != winningNumbers.length)
+            throw new IllegalArgumentException("당첨번호에 중복된 값이 있습니다.");
+    }
+
+    public static void checkBonusNumber(List<Integer> inputWinningNumberList) {
+        checkEachWinningNumber(Lotto.LOTTO_BONUS_NUMBER);
+        for (int winningNumber : inputWinningNumberList) {
+            checkOverlapBonusNumber(winningNumber);
         }
     }
 
-    public static void checkOverlapWinningNumber(String[] winningNumbers) {
-        Set<String> uniqueWinningNumber = new HashSet<>(Arrays.asList(winningNumbers));
-        if (uniqueWinningNumber.size() != winningNumbers.length) {
-            throw new IllegalArgumentException("당첨번호에 중복된 값이 있습니다.");
-        }
+    public static void checkEachWinningNumber(int bonusNumber) {
+        if (bonusNumber > Lotto.LOTTO_NUMBER_MAX || bonusNumber < Lotto.LOTTO_NUMBER_MIN)
+            throw new IllegalArgumentException("당첨번호와 보너스 볼은 " + Lotto.LOTTO_NUMBER_MIN + " ~ " + Lotto.LOTTO_NUMBER_MAX + "사이로 입력하셔야 합니다.");
+    }
+
+    public static void checkOverlapBonusNumber(int winningNumber) {
+        if (winningNumber == Lotto.LOTTO_BONUS_NUMBER)
+            throw new IllegalArgumentException("입력 된 당첨번호 중에 보너스 번호와 중복되는 값이 있습니다.");
     }
 }
--- src/test/java/step3/InputTest.java
+++ src/test/java/step3/InputTest.java
@@ -1,12 +1,28 @@
 package step3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputTest {
+
+    private List<Integer> lottoWinningNumbers = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        lottoWinningNumbers.add(0, 2);
+        lottoWinningNumbers.add(1, 6);
+        lottoWinningNumbers.add(2, 10);
+        lottoWinningNumbers.add(3, 27);
+        lottoWinningNumbers.add(4, 31);
+        lottoWinningNumbers.add(5, 42);
+
+    }
 
     @DisplayName("구매금액 입력 확인: empty 불가")
     @Test
@@ -39,7 +55,14 @@ public class InputTest {
     @DisplayName("당첨번호 중복 확인")
     @Test
     public void 당첨번호_중복_확인() {
-        String[] winningNumbers = "30, 2, 12, 22, 45, 45".split(",");
+        String[] winningNumbers = "30, 2, 22, 22, 45, 45".split(",");
         assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkOverlapWinningNumber(winningNumbers));
+    }
+
+    @DisplayName("보너스번호 중복/ 보너스번호 범위 확인")
+    @Test
+    public void 보너스번호_유효성_확인() {
+        Lotto.LOTTO_BONUS_NUMBER = 10;
+        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkBonusNumber(lottoWinningNumbers));
     }
 }
