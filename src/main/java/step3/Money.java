14b9104246f91bf1611e19b2825a57bab2ad05fb
sy.kim <kauksy9022@gmail.com>
2019-10-15 21:13:37 +0900

    Add: 로또 구매수와 처음 입력된 투입금액 return하는 메서드 추가

--- src/main/java/step3/Money.java
+++ src/main/java/step3/Money.java
@@ -1,7 +1,7 @@
 package step3;
 
 public class Money {
-    // TODO: LOTTO_PRICE의 사용범위 체크해서, private 지정해놓기
+
     private static final int LOTTO_PRICE = 1000;
     private static int inputCash;
 
@@ -10,9 +10,17 @@ public class Money {
         this.inputCash = inputCash / LOTTO_PRICE;
     }
 
+    public int getLottoCount(){
+        return this.inputCash;
+    }
+
+    public int getInputCash(){
+        return this.inputCash * LOTTO_PRICE;
+    }
+
     private static void validateMoney(int inputCash) {
-        if (inputCash < Lotto.LOTTO_PRICE)
-            throw new IllegalArgumentException("로또는 1장에 " + Lotto.LOTTO_PRICE + " 원 입니다.");
+        if (inputCash < LOTTO_PRICE)
+            throw new IllegalArgumentException("로또는 1장에 " + LOTTO_PRICE + " 원 입니다.");
     }
 
 
