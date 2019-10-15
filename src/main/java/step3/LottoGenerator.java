9629af42910c3ddd24f505ccbd1a9d92d197a430
sy.kim <kauksy9022@gmail.com>
2019-10-15 21:40:27 +0900

    Modify: LottoNumber 관련 상수 추가

--- src/main/java/step3/LottoGenerator.java
+++ src/main/java/step3/LottoGenerator.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 public class LottoGenerator {
 
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
     static final int WINNING_NUMBERS_LENGTH = 6;
     static final int BONUS_WINNING_VALID_NUMBER = 5; // 2등 당첨을 위한(보너스볼 일치), 일치해야하는 로또번호 갯수
     static private List<Integer> standardLotto = new ArrayList<>();
 
     public static void generateStandardLotto() {
-        for (int i = Lotto.LOTTO_NUMBER_MIN; i < Lotto.LOTTO_NUMBER_MAX + 1; i++) {
+        for (int i = LOTTO_NUMBER_MIN; i < LOTTO_NUMBER_MAX + 1; i++) {
             standardLotto.add(i);
         }
     }
