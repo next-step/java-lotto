f3db2bee3870be575fd0297b3e50f13b5748eff6
sy.kim <kauksy9022@gmail.com>
2019-10-16 19:34:15 +0900

    Refactor: 로또의 번호와 보너스 번호 관련 클래스 생성

---
+++ src/main/java/step3/LottoNumbers.java
@@ -0,0 +1,59 @@
+package step3;
+
+import java.util.*;
+
+public class LottoNumbers {
+
+    static int LOTTO_BONUS_NUMBER;
+    private List<Integer> winningNumbers;
+
+    public LottoNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = new ArrayList<>(winningNumbers);
+    }
+
+    public static LottoNumbers createLottoNumber(String[] inputWinningNumbers) {
+        checkWinningNumberLength(inputWinningNumbers);
+        checkOverlapWinningNumber(inputWinningNumbers);
+
+        LottoNumbers winningNumbers = new LottoNumbers(checkWinningNumberRange(inputWinningNumbers));
+        return winningNumbers;
+    }
+
+    private static void checkWinningNumberLength(String[] inputWinningNumbers) {
+        if (inputWinningNumbers.length != LottoGenerator.WINNING_NUMBERS_LENGTH)
+            throw new IllegalArgumentException("당첨번호는" + LottoGenerator.WINNING_NUMBERS_LENGTH + "개를 입력하셔야 합니다.");
+    }
+
+    private static void checkOverlapWinningNumber(String[] inputWinningNumbers) {
+        Set<String> uniqueWinningNumber = new HashSet<>(Arrays.asList(inputWinningNumbers));
+        if (uniqueWinningNumber.size() != inputWinningNumbers.length)
+            throw new IllegalArgumentException("당첨번호에 중복된 값이 있습니다.");
+    }
+
+    private static ArrayList<Integer> checkWinningNumberRange(String[] inputWinningNumbers) {
+        ArrayList<Integer> inputWinningNumberList = new ArrayList<>();
+        for (int i = 0; i < LottoGenerator.WINNING_NUMBERS_LENGTH; i++) {
+            checkEachWinningNumber(Integer.parseInt((inputWinningNumbers[i])));
+            inputWinningNumberList.add(Integer.parseInt((inputWinningNumbers[i])));
+        }
+        Collections.sort(inputWinningNumberList);
+        return inputWinningNumberList;
+    }
+
+    private static void checkEachWinningNumber(int bonusNumber) {
+        if (bonusNumber > 45 || bonusNumber < 1)
+            throw new IllegalArgumentException("당첨번호와 보너스 볼은 " + 1 + " ~ " + 45 + "사이로 입력하셔야 합니다.");
+    }
+
+    public static void checkBonusNumber(LottoNumbers inputWinningNumberList) {
+        checkEachWinningNumber(LOTTO_BONUS_NUMBER);
+        for (int winningNumber : inputWinningNumberList.winningNumbers) {
+            checkOverlapBonusNumber(winningNumber);
+        }
+    }
+
+    public static void checkOverlapBonusNumber(int winningNumber) {
+        if (winningNumber == LOTTO_BONUS_NUMBER)
+            throw new IllegalArgumentException("입력 된 당첨번호 중에 보너스 번호와 중복되는 값이 있습니다.");
+    }
+}
