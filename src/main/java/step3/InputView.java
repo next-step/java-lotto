1cf50eb97b851251a2363c3570ecea914243c19d
sy.kim <kauksy9022@gmail.com>
2019-10-16 19:30:04 +0900

    Modify: InputView에서 하던 로또 번호와 관련 된 일을 LottoNumbers로 위임

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -1,9 +1,6 @@
 package step3;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -11,36 +8,22 @@ public class InputView {
 
     public static int inputCash() {
         System.out.println("구입금액을 입력해 주세요.");
-        return  Integer.parseInt(scan.nextLine());
+        return Integer.parseInt(scan.nextLine());
     }
 
-    public static ArrayList<Integer> inputWinningNumbers() {
+    public static String[] inputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String[] inputWinningNumbers = scan.nextLine().split(",");
 
-        InputValidationCheck.checkWinningNumberLength(inputWinningNumbers);
-        InputValidationCheck.checkOverlapWinningNumber(inputWinningNumbers);
-
-        String[] trimWinningNumbers = new String[LottoGenerator.WINNING_NUMBERS_LENGTH];
         for (int i = 0; i < LottoGenerator.WINNING_NUMBERS_LENGTH; i++) {
-            trimWinningNumbers[i] = inputWinningNumbers[i].trim();
+            inputWinningNumbers[i] = inputWinningNumbers[i].trim();
         }
-
-        int[] winningNumbers = InputValidationCheck.checkWinningNumberRange(trimWinningNumbers);
-        Arrays.sort(winningNumbers);
-
-        ArrayList<Integer> inputWinningNumberList = new ArrayList<>();
-        for (int i = 0; i < LottoGenerator.WINNING_NUMBERS_LENGTH; i++) {
-            inputWinningNumberList.add(winningNumbers[i]);
-        }
-
-        return inputWinningNumberList;
+        return inputWinningNumbers;
     }
 
-    public static void bonusNumberAdd(List<Integer> inputWinningNumberList) {
+    public static void bonusNumberAdd(LottoNumbers inputWinningNumberList) {
         System.out.println("보너스 볼을 입력해주세요.");
-        Lotto.LOTTO_BONUS_NUMBER = scan.nextInt();
-
-        InputValidationCheck.checkBonusNumber(inputWinningNumberList);
+        LottoNumbers.LOTTO_BONUS_NUMBER = scan.nextInt();
+        LottoNumbers.checkBonusNumber(inputWinningNumberList);
     }
 }
