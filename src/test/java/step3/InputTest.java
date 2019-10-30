757feb9c44b98902fdb030d82d08bf42a31cc5f7
sy.kim <kauksy9022@gmail.com>
2019-10-30 22:09:30 +0900

    Revise 메소드 변경에 따른 테스트 변경

--- src/test/java/step3/InputTest.java
+++ src/test/java/step3/InputTest.java
@@ -4,65 +4,47 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputTest {
 
-    private List<Integer> lottoWinningNumbers = new ArrayList<>();
-
     @BeforeEach
     void setUp() {
-        lottoWinningNumbers.add(0, 2);
-        lottoWinningNumbers.add(1, 6);
-        lottoWinningNumbers.add(2, 10);
-        lottoWinningNumbers.add(3, 27);
-        lottoWinningNumbers.add(4, 31);
-        lottoWinningNumbers.add(5, 42);
-
-    }
-
-    @DisplayName("구매금액 입력 확인: empty 불가")
-    @Test
-    public void 구매금액_입력() {
-
-        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkEmpty(""));
+        System.out.println("로또 InputTest시작");
     }
 
     @DisplayName("구매금액 입력 확인: 1000원 미만 불가")
     @Test
     public void 구매액수_확인() {
 
-        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkCash("900"));
+        assertThatIllegalArgumentException().isThrownBy(() -> Money.validateMoney(900));
     }
 
     @DisplayName("당첨번호 입력받기 확인: 6자리 입력")
     @Test
     public void 입력받은_당첨번호_자릿수_확인() {
         String[] winningNumbers = "30, 2, 12, 22, 45, 100".split(",");
-        assertThat(winningNumbers.length).isEqualTo(LottoGenerator.WINNING_NUMBERS_LENGTH);
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkWinningNumberLength(winningNumbers));
     }
 
     @DisplayName("당첨번호 범위 확인: 1 ~ 45")
     @Test
     public void 당첨번호_범위_확인() {
         String[] winningNumbers = "30, 2, 12, 22, 45, 100".split(",");
-        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkWinningNumberRange(winningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkWinningNumberRange(winningNumbers));
     }
 
     @DisplayName("당첨번호 중복 확인")
     @Test
     public void 당첨번호_중복_확인() {
         String[] winningNumbers = "30, 2, 22, 22, 45, 45".split(",");
-        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkOverlapWinningNumber(winningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkOverlapWinningNumber(winningNumbers));
     }
 
     @DisplayName("보너스번호 중복/ 보너스번호 범위 확인")
     @Test
     public void 보너스번호_유효성_확인() {
-        Lotto.LOTTO_BONUS_NUMBER = 10;
-        assertThatIllegalArgumentException().isThrownBy(() -> InputValidationCheck.checkBonusNumber(lottoWinningNumbers));
+        LottoWinningNumbers.LOTTO_BONUS_NUMBER = 10;
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkOverlapBonusNumber(9));
     }
 }
