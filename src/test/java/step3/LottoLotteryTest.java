ca054e94809cb11191c6629d813f3da3d22186b6
sy.kim <kauksy9022@gmail.com>
2019-10-30 22:32:14 +0900

    Revise LottoLotteryTest

--- src/test/java/step3/LottoLotteryTest.java
+++ src/test/java/step3/LottoLotteryTest.java
@@ -5,45 +5,37 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 보너스 번호와 관련 된 로직이 있음
- * */
+ */
 public class LottoLotteryTest {
 
-    private Map<Integer, Integer> lottoStatistic = new HashMap<>();
+    private List<Integer> lottoWinningNumbers = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-        lottoStatistic.put(1, 2);
-        lottoStatistic.put(2, 2);
-        lottoStatistic.put(3, 2);
-        lottoStatistic.put(4, 0);
-        lottoStatistic.put(5, 1);
-        lottoStatistic.put(6, 0);
+        lottoWinningNumbers.add(0, 2);
+        lottoWinningNumbers.add(1, 6);
+        lottoWinningNumbers.add(2, 10);
+        lottoWinningNumbers.add(3, 27);
+        lottoWinningNumbers.add(4, 31);
+        lottoWinningNumbers.add(5, 42);
+
+        LottoWinningNumbers.LOTTO_BONUS_NUMBER = lottoWinningNumbers.get(5);
+
     }
 
-    @DisplayName("당첨과 관계 없는 값 삭제")
+    @DisplayName("보너스 당첨 여부 확인")
     @Test
-    void removeUnavailableValueTest() {
-        for (int i = 1; i < LottoGenerator.WINNING_NUMBERS_LENGTH + 1; i++) {
-            LottoLottery.removeUnavailableValue(lottoStatistic, i);
+    void purchasedLottoCompareBonusNumber() {
+        for (int i = 0; i < LottoGenerator.WINNING_NUMBERS_LENGTH; i++) {
+            LottoLottery.purchasedLottoCompareBonusNumber(lottoWinningNumbers, i);
         }
-        assertThat(lottoStatistic.size()).isEqualTo(2);
-    }
-
-    @DisplayName("당첨 금액 확인")
-    @Test
-    void totalWinningPrize() {
-        double totalWard = 0;
-        for (int i = LottoLottery.MATCHING_LIMIT; i < LottoGenerator.WINNING_NUMBERS_LENGTH + 1; i++) {
-            totalWard += lottoStatistic.get(i) * Prize.findByCountOfMatch(i).getWinningMoney();
-        }
-        double testTotalWard = Money.totalPrize(lottoStatistic);
-        assertThat(testTotalWard).isEqualTo(totalWard);
+        assertThat(LottoLottery.bonusCheck).isEqualTo(true);
     }
 }
