ba05a0fc3c0d8134237c7e9b4fdea13e272052ca
sy.kim <kauksy9022@gmail.com>
2019-10-30 13:12:37 +0900

    Implement Money클래스: 금액과 관련된 로직 및 필드를 가짐

--- src/main/java/step3/Money.java
+++ src/main/java/step3/Money.java
@@ -1,20 +1,23 @@
 package step3;
 
+import java.util.Map;
+
 public class Money {
 
     private static final int LOTTO_PRICE = 1000;
     private int inputCash;
+    private static long totalPrize = 0;
 
     public Money(int inputCash) {
         validateMoney(inputCash);
         this.inputCash = inputCash / LOTTO_PRICE;
     }
 
-    public int getLottoCount(){
+    public int getLottoCount() {
         return this.inputCash;
     }
 
-    public int getInputCash(){
+    public int getInputCash() {
         return this.inputCash * LOTTO_PRICE;
     }
 
@@ -23,5 +26,30 @@ public class Money {
             throw new IllegalArgumentException("로또는 1장에 " + LOTTO_PRICE + " 원 입니다.");
     }
 
+    public double winningPrize(Map<Integer, Integer> summaryResult) {
+        int spandMoney = getInputCash();
+        for (int key : summaryResult.keySet()) {
+            totalPrize += calculateTotalPrize(summaryResult, key);
+        }
 
+        if (totalPrize > 0) {
+            double earningRate = Math.abs(totalPrize - spandMoney);
+            return earningRate / spandMoney;
+        }
+        return 0;
+    }
+
+    private long calculateTotalPrize(Map<Integer, Integer> summaryResult, int key) {
+        if (LottoGenerator.BONUS_WINNING_VALID_NUMBER == key && LottoLottery.bonusCheck) {
+            return sumBonusPrize(summaryResult, key);
+        }
+        return Prize.matchOfCount(key, false).getWinningMoney() * summaryResult.get(key);
+    }
+
+    private long sumBonusPrize(Map<Integer, Integer> summaryResult, int key) {
+        long bonusPrize = Prize.matchOfCount(key, true).getWinningMoney() * LottoLottery.BONUS_WINNING_COUNT;
+        int thirdPrizeCount = summaryResult.get(key) - LottoLottery.BONUS_WINNING_COUNT;
+
+        return bonusPrize + (Prize.matchOfCount(key, false).getWinningMoney() * thirdPrizeCount);
+    }
 }
