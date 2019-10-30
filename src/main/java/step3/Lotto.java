7e9207241bb733d7e413a5447a6c0eae964a62e9
sy.kim <kauksy9022@gmail.com>
2019-10-30 13:15:15 +0900

    Revise 각 메소드를 간소화

--- src/main/java/step3/Lotto.java
+++ src/main/java/step3/Lotto.java
@@ -2,40 +2,43 @@ package step3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class Lotto {
 
-    static final int LOTTO_NUMBER_MIN = 1;
-    static final int LOTTO_NUMBER_MAX = 45;
-    static final int LOTTO_PRICE = 1000;
-    static int LOTTO_BONUS_NUMBER;
-
-    List<Integer> lotto;
+    private List<Integer> lotto;
 
     public Lotto(List<Integer> lotto) {
         this.lotto = new ArrayList<>(lotto);
     }
 
-    /**
-     * 구매한 각각의 로또번호와 당첨번호를 비교
-     * matchWinningNumbers 에서는 당첨번호 중 몇 개가 일치하는지 카운팅한다.
-     * */
     public static List<Integer> lotteryStatics(Set<Lotto> purchasedLottos, List<Integer> winningNumbers) {
         List<Integer> resultMatching = new ArrayList<>();
         for (Lotto purchasedLotto : purchasedLottos) {
-            resultMatching.add(LottoLottery.matchWinningNumbers(purchasedLotto, winningNumbers));
+            resultMatching.add(matchWinningNumbers(purchasedLotto, winningNumbers));
         }
         return resultMatching;
     }
 
-    // 통계를 내기위해 Map에 N개 일치하는(Key) 로또가 몇장인지 (Value) 저장
-    public static Map<Integer, Integer> giveStatistic(List<Integer> resultMatching) {
-        LottoLottery.generateBasicStatistic();
-        for (int i = 1; i < LottoGenerator.WINNING_NUMBERS_LENGTH + 1; i++) {
-            LottoLottery.summaryMatching(resultMatching, i);
+    private static int matchWinningNumbers(Lotto purchasedLotto, List<Integer> winningNumbers) {
+        final int count = (int) winningNumbers.stream()
+                .filter(purchasedLotto.lotto::contains)
+                .count();
+        return count;
+    }
+
+    public static void pickOutBonusWinningLotto(Set<Lotto> purchasedLottos) {
+        int indexCount = 0;
+        for (Lotto purchasedLotto : purchasedLottos) {
+            LottoLottery.purchasedLottoCompareBonusNumber(purchasedLotto.lotto, indexCount);
+            indexCount++;
         }
-        return LottoLottery.statisticResult;
+    }
+
+    public static void goPrint(Set<Lotto> purchasedLottos) {
+        for (Lotto purchasedLotto : purchasedLottos) {
+            ResultView.printPurchasedLottos(purchasedLotto.lotto);
+        }
+        System.out.println();
     }
 }
