e4b2ab07d47b8e7cbf59c57b0bdda7380bde322a
vagabond95 <progjh@gmail.com>
2019-11-01 00:51:53 +0900

    [feat] add_custom_구분자() 테스트를 통과하는 코드 구현

--- src/main/java/tdd/StringAddCalculator.java
+++ src/main/java/tdd/StringAddCalculator.java
@@ -1,15 +1,29 @@
 package tdd;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
     private static final int INIT_RESULT_VALUE = 0;
+    private static final String DEFAULT_DELIMITER = ",|:";
 
     public int add(String inputText) {
         if (inputText == null || inputText.isEmpty()) {
             return INIT_RESULT_VALUE;
         }
 
-        String[] numbers = inputText.split(",|:");
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputText);
+
+        String delimiter = DEFAULT_DELIMITER;
+        String text = inputText;
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            delimiter = delimiter + "|" + customDelimiter;
+            text = m.group(2);
+        }
+
+        String[] numbers = text.split(delimiter);
 
         int result = INIT_RESULT_VALUE;
         for (String each : numbers) {
