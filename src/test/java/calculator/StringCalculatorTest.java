88843ab5c9f87c09375a4575de84c8479ce5d282
Kim YoonHo <kimunoya@gmail.com>
2019-10-31 22:23:12 +0900

    test: 문자열 계산기 테스트 클래스 생성

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-10-31 22:18
+ */
+public class StringCalculatorTest {
+    @Test
+    void name() {
+    }
+}
