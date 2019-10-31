e6521a6e38a83d617e469f516aa75d3cad70a451
vagabond95 <progjh@gmail.com>
2019-11-01 00:43:11 +0900

    [feat] add_null_또는_빈문자() 테스트 코드를 통과하는 코드 추가

---
+++ src/main/java/tdd/StringAddCalculator.java
@@ -0,0 +1,12 @@
+package tdd;
+
+public class StringAddCalculator {
+
+    public int add(String inputText) {
+        if (inputText == null || inputText.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
