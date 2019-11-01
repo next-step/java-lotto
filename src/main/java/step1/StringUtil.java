0b4617485d95cde8399783062e5fa5710e281b20
naheenosaur <log.info.527@gmail.com>
2019-11-01 22:28:09 +0900

    feat(string util) : 입력한 delimiter로 split

--- src/main/java/step1/StringUtil.java
+++ src/main/java/step1/StringUtil.java
@@ -1,10 +1,32 @@
-
 package step1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringUtil {
+    private static final String REGEX_FOR_CUSTOM_DELIMITER = "//(.)\\\\n(.*)";
+    private static final String DEFAULT_REGEX_DELIMITER = ":|,";
+    private static final String REGEX_DELIMITER_WITH_CUSTOM_VALUE = ":|,|";
+
+    public static List<String> splitStrings(String input) {
+        Matcher delimiterMatcher = Pattern.compile(REGEX_FOR_CUSTOM_DELIMITER).matcher(input);
+
+        String delimiter = DEFAULT_REGEX_DELIMITER;
+        if (delimiterMatcher.find()) {
+            delimiter = REGEX_DELIMITER_WITH_CUSTOM_VALUE + delimiterMatcher.group(1);
+            input = delimiterMatcher.group(2);
+        }
+
+        return Arrays.stream(input.trim()
+                .split(delimiter))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
     public static List<Integer> parsePositiveInt(List<String> inputWords) {
         List<Integer> result = new ArrayList<>();
         for (String inputWord : inputWords) {
@@ -13,7 +35,7 @@ public class StringUtil {
         return result;
     }
 
-    public static Integer parsePositiveInt(String word) {
+    private static Integer parsePositiveInt(String word) {
         int number = parseInt(word);
         if (number < 0) {
             throw new RuntimeException();
@@ -21,7 +43,7 @@ public class StringUtil {
         return number;
     }
 
-    public static Integer parseInt(String word) {
+    private static Integer parseInt(String word) {
         try {
             return Integer.parseInt(word);
         } catch (NumberFormatException e) {
--- src/test/java/step1/CalculatorTest.java
+++ src/test/java/step1/CalculatorTest.java
@@ -10,9 +10,23 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class CalculatorTest {
+class CalculatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "//;\\n3;4;5",
+            "//;\\n3;4:5",
+            "//;\\n3:4,5",
+            "3:4:5",
+            "3,4,5"
+    })
+    void splitTest(String input) {
+        List<String> inputWords = StringUtil.splitStrings(input);
+        assertThat(inputWords).containsExactly("3", "4", "5");
+    }
+
     @Test
-    public void StringToIntTest() {
+    void StringToIntTest() {
         List<String> inputWords = Arrays.asList("3", "4", "5");
         List<Integer> parsedNumber = StringUtil.parsePositiveInt(inputWords);
         assertThat(parsedNumber).containsExactly(3, 4, 5);
@@ -22,7 +36,7 @@ public class CalculatorTest {
     @ValueSource(strings = {
             "e", "-3", ":"
     })
-    public void StringToIntThrowExceptionTest(String input) {
+    void StringToIntThrowExceptionTest(String input) {
         List<String> inputWords = Arrays.asList(input);
         assertThatThrownBy(() -> StringUtil.parsePositiveInt(inputWords)).isInstanceOf(RuntimeException.class);
     }
