9ac245bf9954d3fba98d8dc3dbde7c8c3863c5f9
WoohyunLee <whlee0525@gmail.com>
2019-11-02 01:31:11 +0900

    feat (sum text with minus number)

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -10,17 +10,15 @@ public class Calculator {
         return (obj instanceof Calculator);
     }
 
-
     public int calculateText(String text) {
-        if(isNullOrEmpty(text)) {
+        if (isNullOrEmpty(text)) {
             return 0;
         }
-
         return sumTokens(getTokens(text));
     }
 
-    private boolean isNullOrEmpty(String text){
-        if(text == null) {
+    private boolean isNullOrEmpty(String text) {
+        if (text == null) {
             return true;
         }
         if (text.isEmpty()) {
@@ -30,19 +28,27 @@ public class Calculator {
         return false;
     }
 
-    private String[] getTokens(String text){
+    private String[] getTokens(String text) {
         Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if(matcher.find()){
+        if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             return matcher.group(2).split(customDelimiter);
         }
         return text.split(",|:");
     }
 
-    private int sumTokens(String[] tokens){
+    private int sumTokens(String[] tokens) {
         int result = 0;
-        for(String token:tokens){
-            result += Integer.parseInt(token);
+        for (String token : tokens) {
+            result += makeValidInteger(token);
+        }
+        return result;
+    }
+
+    private int makeValidInteger(String token){
+        int result = Integer.parseInt(token);
+        if(result < 0){
+            throw new RuntimeException();
         }
         return result;
     }
--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package Calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CalculatorTest {
@@ -10,37 +11,50 @@ public class CalculatorTest {
     Calculator calculator;
 
     @BeforeEach
-    void start(){
+    void start() {
         calculator = new Calculator();
     }
 
     @Test
-    void 계산기_생성(){
+    void 계산기_생성() {
         assertThat(calculator).isEqualTo(new Calculator());
     }
 
     @Test
-    void 빈문자열_또는_null_입력(){
+    void 빈문자열_또는_null_입력() {
         assertThat(calculator.calculateText("")).isEqualTo(0);
         assertThat(calculator.calculateText(null)).isEqualTo(0);
     }
 
     @Test
-    void 단독_또는_콤마포함_계산(){
+    void 단독_또는_콤마포함_계산() {
         assertThat(calculator.calculateText("6")).isEqualTo(6);
         assertThat(calculator.calculateText("6,7")).isEqualTo(13);
     }
 
     @Test
-    void 콤마와_콜론포함_계산(){
+    void 콤마와_콜론포함_계산() {
         assertThat(calculator.calculateText("6,7:3")).isEqualTo(16);
     }
 
     @Test
-    void 커스텀_구분자_계산(){
+    void 커스텀_구분자_계산() {
         assertThat(calculator.calculateText("//;\n1;2;3")).isEqualTo(6);
     }
 
+    @Test
+    void 음수_계산(){
+        assertThatThrownBy(() -> {
+            calculator.calculateText("//;\n-1;2;3");
+        }).isInstanceOf(RuntimeException.class);
+    }
+
+ /*   @Test
+    void 음수_테스트(){
+        assertThatThrownBy(() -> {
+            calculator.makeValidInteger("-1");
+        }).isInstanceOf(RuntimeException.class);
+    }*/
 
 
 }
