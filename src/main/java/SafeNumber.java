2bc0c7ef6d18282353e0e88fcc3d25d0baf5d169
chanin <pci2676@gmail.com>
2019-11-01 13:01:51 +0900

    java-lotto step1 feat - 기능구현1  - 숫자 클래스 생성 및 테스트

---
+++ src/main/java/SafeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class SafeNumber {
+
+    private static final int MINIMUM_VALUE = 0;
+    private Integer number;
+
+    public SafeNumber(int number) {
+        if (number < MINIMUM_VALUE) {
+            throw new RuntimeException(number + " : 음수는 허용되지 않습니다.");
+        }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SafeNumber that = (SafeNumber) o;
+        return Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
