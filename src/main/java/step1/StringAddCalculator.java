4f2509355f117011cd6d0c36861950de8469e475
Jaeeun Na <lette1394@gmail.com>
2019-11-01 22:54:48 +0900

    refactor impl

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -1,29 +1,60 @@
 package step1;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class StringAddCalculator {
-    public int add(String input) {
-        if (Objects.isNull(input) || input.isEmpty()) {
+    private static final int CUSTOM_DELIMITER_BEGIN_INDEX = 2;
+    private static final int CUSTOM_DELIMITER_LENGTH = 1;
+
+    private static final int INPUT_BEGIN_INDEX_WITH_CUSTOM_DELIMITER = 4;
+
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String CRLF = "\n";
+
+    public int add(final String input) {
+        if (isNullOrEmpty(input)) {
             return 0;
         }
+        return addInternal(input);
+    }
 
-        String delimiter = "[,:]";
-        if (input.contains("\n")) {
-            delimiter = input.substring(2, 3);
-            input = input.substring(4);
+    private boolean isNullOrEmpty(final String input) {
+        return Objects.isNull(input) || input.isEmpty();
+    }
+
+    private int addInternal(final String input) {
+        if (hasCustomDelimiter(input)) {
+            final String customDelimiter = parseCustomDelimiter(input);
+            final String formattedInput = parseInputAfterCustomDelimiter(input);
+            return addInternal(formattedInput, customDelimiter);
         }
+        return addInternal(input, DEFAULT_DELIMITER);
+    }
 
-        int sum = 0;
-        final String[] split = input.split(delimiter);
-        for (final String s : split) {
-            final int i = Integer.parseInt(s);
-            if (i < 0){
-                throw new IllegalArgumentException();
-            }
-            sum += i;
+    private boolean hasCustomDelimiter(final String input) {
+        return input.contains(CRLF);
+    }
+
+    private String parseCustomDelimiter(final String input) {
+        return input.substring(CUSTOM_DELIMITER_BEGIN_INDEX, CUSTOM_DELIMITER_BEGIN_INDEX + CUSTOM_DELIMITER_LENGTH);
+    }
+
+    private String parseInputAfterCustomDelimiter(final String input) {
+        return input.substring(INPUT_BEGIN_INDEX_WITH_CUSTOM_DELIMITER);
+    }
+
+    private int addInternal(final String input, final String delimiter) {
+        return Arrays.stream(input.split(delimiter))
+                     .map(Integer::parseInt)
+                     .peek(this::ensurePositive)
+                     .mapToInt(Integer::intValue)
+                     .sum();
+    }
+
+    private void ensurePositive(final int input) {
+        if (input < 0) {
+            throw new IllegalArgumentException();
         }
-
-        return sum;
     }
 }
