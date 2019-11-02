21963487a66b925a40380b2e7bb546b534972011
Kim YoonHo <kimunoya@gmail.com>
2019-11-02 23:07:30 +0900

    feat: 로또 머신 객체 생성
    - 금액을 입력받아 로또 구매

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
+import lotto.domain.LottoNumberStrategy;
+import lotto.domain.PurchasedLotto;
+
+import java.util.*;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-02 22:27
+ */
+public class LottoMachine {
+
+    private static final LottoNumberStrategy LOTTO_NUMBER_STRATEGY = new LottoNumberStrategy();
+    private final LottoAmount lottoAmount;
+    private final PurchasedLotto purchasedLotto;
+
+    public LottoMachine(int amount) {
+        this.lottoAmount = new LottoAmount(amount);
+        this.purchasedLotto = new PurchasedLotto(purchaseLotto());
+    }
+
+    public List<Lotto> getLotto() {
+        return Collections.unmodifiableList(purchasedLotto.getPurchasedLotto());
+    }
+
+    private List<Lotto> purchaseLotto() {
+        List<Lotto> lotto = new ArrayList<>();
+        for (int i = 0; i < lottoAmount.getPurchasedLottoCount(); i++) {
+            lotto.add(new Lotto(LOTTO_NUMBER_STRATEGY.randomNumbers()));
+        }
+        return lotto;
+    }
+
+}
