ad72b35026f13eb7101aa5b8d72a68b582e043d0
Kim YoonHo <kimunoya@gmail.com>
2019-11-02 23:08:08 +0900

    feat: 구매된 로또 일급 컬렉션 생성

---
+++ src/main/java/lotto/domain/PurchasedLotto.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-02 22:46
+ */
+public class PurchasedLotto {
+
+    private final List<Lotto> purchasedLotto;
+
+    public PurchasedLotto(List<Lotto> purchasedLotto) {
+        this.purchasedLotto = purchasedLotto;
+    }
+
+    public List<Lotto> getPurchasedLotto() {
+        return this.purchasedLotto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PurchasedLotto that = (PurchasedLotto) o;
+        return Objects.equals(purchasedLotto, that.purchasedLotto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(purchasedLotto);
+    }
+}
