12b0f44aa9fe1bde01b2174d2d8dc7bf5ce4e3a9
vagabond95 <progjh@gmail.com>
2019-11-03 03:12:46 +0900

    [refactor] 공백, null 여부를 검사하는 로직을 메소드로 분리

--- src/main/java/tdd/StringAddCalculator.java
+++ src/main/java/tdd/StringAddCalculator.java
@@ -5,11 +5,15 @@ import static tdd.Numbers.INIT_RESULT_VALUE;
 public class StringAddCalculator {
 
     public int add(String inputText) {
-        if (inputText == null || inputText.isEmpty()) {
+        if (isEmptyText(inputText)) {
             return INIT_RESULT_VALUE;
         }
 
         Numbers numbers = new Numbers(InputParser.parseToNumberTexts(inputText));
         return numbers.addAll();
+    }
+
+    public boolean isEmptyText(String text) {
+        return text == null || text.isEmpty();
     }
 }
