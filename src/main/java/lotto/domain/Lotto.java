353a560c0c796216b2f0c8030e3ec24fae01c684
Kim YoonHo <kimunoya@gmail.com>
2019-11-02 23:52:33 +0900

    feat: 로또 객체 기능 추가
    - 로또 번호 검증
    - shuffle 기능
    - 로또 번호 매치 기능

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author : 김윤호
@@ -9,10 +12,40 @@ import java.util.Objects;
  * @date : 2019-11-02 21:45
  */
 public class Lotto {
+
+    private static final int LOTTO_MIN_NUM = 1;
+    private static final int LOTTO_MAX_NUM = 45;
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        checkValidateNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
+        shuffle();
+    }
+
+    private void checkValidateNumbers(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또는 6개의 숫자로 구성되어야 합니다.");
+        }
+
+        if (!lottoNumbers.stream().allMatch(number -> number >= LOTTO_MIN_NUM && number <= LOTTO_MAX_NUM)) {
+            throw new IllegalArgumentException("로또는 1부터 45의 숫자로 구성되어야 합니다.");
+        }
+    }
+
+    private void shuffle() {
+        Collections.shuffle(this.lottoNumbers);
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return this.lottoNumbers;
+    }
+
+    public Long matchLottoNumberCount(int[] numbers) {
+        List<Integer> matchNumbers = IntStream.of(numbers).boxed().collect(Collectors.toList());
+        return this.lottoNumbers.stream().filter(number -> matchNumbers.contains(number)).count();
     }
 
     @Override
