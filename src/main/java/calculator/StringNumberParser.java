843c1770eb260e96d351cca618822c8ad1ca7aec
dave915 <htaehyun@gmail.com>
2019-11-02 15:40:14 +0900

    [feat] 문자열에 // 와 \n 이 포함되어 있으면 // 와 \n 사이에 있는 문자를 구분자로 사용한다

--- src/main/java/calculator/StringNumberParser.java
+++ src/main/java/calculator/StringNumberParser.java
@@ -5,10 +5,39 @@ import java.util.List;
 
 public class StringNumberParser {
     private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final String CUSTOM_SEPARATOR_START_STRING = "//";
+    private static final String CUSTOM_SEPARATOR_END_STRING = "\n";
 
     public static List<Integer> parse(String string) {
-        String[] strings = string.split(DEFAULT_SEPARATOR);
+        String separator = getSeparator(string);
+        String[] strings = split(string, separator);
         return convertNumbers(strings);
+    }
+
+    private static String getSeparator(String string) {
+        if (hasCustomSeparator(string)) {
+            return findCustomSeparator(string);
+        }
+
+        return DEFAULT_SEPARATOR;
+    }
+
+    private static boolean hasCustomSeparator(String string) {
+        return string.contains(CUSTOM_SEPARATOR_START_STRING) && string.contains(CUSTOM_SEPARATOR_END_STRING);
+    }
+
+    private static String findCustomSeparator(String string) {
+        int startIndex = string.indexOf(CUSTOM_SEPARATOR_START_STRING) + CUSTOM_SEPARATOR_START_STRING.length();
+        int endIndex = string.indexOf(CUSTOM_SEPARATOR_END_STRING);
+        return string.substring(startIndex, endIndex);
+    }
+
+    private static String[] split(String string, String separator) {
+        if (hasCustomSeparator(string)) {
+            int beginIndex = string.indexOf(CUSTOM_SEPARATOR_END_STRING) + CUSTOM_SEPARATOR_END_STRING.length();
+            return string.substring(beginIndex).split(separator);
+        }
+        return string.split(separator);
     }
 
     private static List<Integer> convertNumbers(String[] strings) {
--- src/test/java/calculator/StringNumberParserTest.java
+++ src/test/java/calculator/StringNumberParserTest.java
@@ -15,4 +15,12 @@ public class StringNumberParserTest {
         List<Integer> number = StringNumberParser.parse(expectedString);
         assertThat(number).hasSize(3);
     }
+
+    @Test
+    @DisplayName("문자열에 // 와 \n 이 포함되어 있으면 // 와 \n 사이에 있는 문자를 구분자로 사용한다")
+    void parseByCustomSeparator() {
+        String expectedString = "//;\n1;2;3";
+        List<Integer> number = StringNumberParser.parse(expectedString);
+        assertThat(number).hasSize(3);
+    }
 }
