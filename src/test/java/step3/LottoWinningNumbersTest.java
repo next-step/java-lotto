f244d4aeefe224212dc7ec8db323adba67bbc6f6
sy.kim <kauksy9022@gmail.com>
2019-11-03 23:03:13 +0900

    [Revise]: Lotto클래스 수정, LottoNo클래스 추가에 따른 참조메소드 수정

--- src/test/java/step3/LottoWinningNumbersTest.java
+++ src/test/java/step3/LottoWinningNumbersTest.java
@@ -15,27 +15,27 @@ public class LottoWinningNumbersTest {
     @Test
     public void 입력받은_당첨번호_자릿수_확인() {
         String[] winningNumbers = "30, 2, 12, 22, 45, 5, 8".split(",");
-        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkWinningNumberLength(winningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.checkWinningNumberLength(winningNumbers));
     }
 
     @DisplayName("당첨번호 범위 확인: 1 ~ 45")
     @Test
     public void 당첨번호_범위_확인() {
         String[] winningNumbers = "30, 2, 12, 22, 45, 1".split(",");
-        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkWinningNumberRange(winningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.checkWinningNumberRange(winningNumbers));
     }
 
     @DisplayName("당첨번호 중복 확인")
     @Test
     public void 당첨번호_중복_확인() {
         String[] winningNumbers = "30, 2, 22, 22, 45, 45".split(",");
-        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkOverlapWinningNumber(winningNumbers));
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.checkOverlapWinningNumber(winningNumbers));
     }
 
     @DisplayName("보너스번호 중복/ 보너스번호 범위 확인")
     @Test
     public void 보너스번호_유효성_확인() {
-        LottoWinningNumbers.lottoBonusNumber = 10;
-        assertThatIllegalArgumentException().isThrownBy(() -> LottoWinningNumbers.checkOverlapBonusNumber(10));
+        Lotto.lottoBonusNumber = 10;
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNo.checkOverlapBonusNumber(10));
     }
 }
