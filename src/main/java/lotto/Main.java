8758e39e039ccf92f548d9f28a1d37750b620d93
dellife <seheekwon21@gmail.com>
2019-11-03 18:50:51 +0900

    [feat] 결과 출력하기

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,17 +1,38 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoService;
+import lotto.domain.WinnerType;
 import lotto.view.InputView;
+import lotto.view.ResultView;
+import util.Parser;
+
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
         InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
 
         int buyAmount = inputView.createBuyAmount();
-        System.out.println(buyAmount);
+
+        LottoService lottoService = new LottoService();
+        int number = lottoService.buyTicket(buyAmount);
+        List<Lotto> lottos = lottoService.createLottoTickets(number);
+
+        resultView.printBuyTicketNumber(number);
+        resultView.printBuyLottos(lottos);
 
         String winnerNumbers = inputView.createWinnerNumbers();
-        System.out.println(winnerNumbers);
+        Lotto winnerLotto = new Lotto(Parser.convertToInteger(winnerNumbers));
+        Map<WinnerType, Integer> winnerStats = lottoService.findWinnerStats(lottos, winnerLotto);
+        double yield = lottoService.findYield(winnerStats, number);
+
+        resultView.printWinnerStats(winnerStats);
+        resultView.printYield(yield);
+
     }
 }
--- src/main/java/lotto/domain/WinnerType.java
+++ src/main/java/lotto/domain/WinnerType.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 
 public enum WinnerType {
 
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
+    FOURTH(3, 5000),
     THIRD(4, 50000),
-    FOURTH(3, 5000);
+    SECOND(5, 1500000),
+    FIRST(6, 2000000000);
 
     private final int sameCount;
     private final int prize;
