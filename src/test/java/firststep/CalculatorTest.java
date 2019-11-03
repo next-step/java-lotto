2f823d1185541ab3cee0e377414082aa1bb49567
조성은 <chose@woowahan.com>
2019-11-03 18:15:25 +0900

    Step1 String Calculator (#309)
    * feat Add InputView
    
    * feat Add Calculator
    
    * refactor Delete txt file
    
    * refactor Code lines

---
+++ src/test/java/firststep/CalculatorTest.java
@@ -0,0 +1,42 @@
+package firststep;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    @DisplayName("숫자를 합한다")
+    void calculate() {
+        String[] input = new String[]{"1", "2", "3"};
+        assertThat(calculator.calculate(input)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("숫자를 0 을 return 한다")
+    void calculate2() {
+        String[] input = new String[]{"0"};
+        assertThat(calculator.calculate(input)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("음수는 RuntimeException")
+    void calculate3() {
+        String[] input = new String[]{"-1"};
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> calculator.calculate(input));
+    }
+
+    @Test
+    @DisplayName("숫자가 아니면 RuntimeException")
+    void calculate4() {
+        String[] input = new String[]{"test"};
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> calculator.calculate(input));
+    }
+
+}
