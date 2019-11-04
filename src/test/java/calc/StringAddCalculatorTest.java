58723e3c1993d7bc5847afbbace2ef937896c7a8
iltaek <iltaekkwon@gmail.com>
2019-11-04 21:22:34 +0900

    feat($step1): "숫자 하나를 문자열로 입력하면 해당 숫자를 반환테스트" 추가

--- src/test/java/calc/StringAddCalculatorTest.java
+++ src/test/java/calc/StringAddCalculatorTest.java
@@ -1,9 +1,12 @@
 package calc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringAddCalculatorTest {
 
@@ -18,5 +21,11 @@ public class StringAddCalculatorTest {
     void 빈문자열_null_입력시_0을_반환테스트() {
         assertThat(calculator.add(null)).isEqualTo(0);
         assertThat(calculator.add("")).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4"})
+    void 숫자_하나를_문자열로_입력하면_해당_숫자를_반환테스트(String input) {
+        assertThat(calculator.add(input)).isEqualTo(Integer.parseInt(input));
     }
 }
