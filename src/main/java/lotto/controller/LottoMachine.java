5eb13065c942c491f15a139e684506246f832551
vagabond95 <progjh@gmail.com>
2019-11-05 01:04:08 +0900

    [refactor] 로또 머신에서 상태를 제거

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -10,51 +10,41 @@ import java.util.Map;
 
 public class LottoMachine {
 
-    private final LottoView lottoView;
-    private LottoNumbers lottoNumbers;
-    private Money money;
-
-    public LottoMachine(LottoView lottoView) {
-        this.lottoView = lottoView;
-    }
-
-    public void start() {
+    // 상태를 제거하면 사이드 이펙트를 제거할 수 있다. 하지만, 기존의 상태를 활용했던 곳에 파라미터로 전달해줘야 하므로 파라미터 수가 늘어나게 된다. trade off?
+    private void start(LottoView lottoView) {
         try {
-            createLottoNumbers();
-            findWinningLotto();
+            Money money = getMoney(lottoView);
+            LottoNumbers lottoNumbers = createLottoNumbers(lottoView, money);
+
+            createLottoNumbers(lottoView, money);
+            findWinningLotto(lottoView, money, lottoNumbers);
         } catch (NumberFormatException exception) {
             lottoView.showConvertNumberError();
-            restart();
+            restart(lottoView);
         } catch (IllegalArgumentException exception) {
             lottoView.showErrorMessage(exception.getMessage());
-            restart();
+            restart(lottoView);
         }
     }
 
-    private void restart() {
+    private void restart(LottoView lottoView) {
         lottoView.showRestartMessage();
-        clear();
-        start();
+        start(lottoView);
     }
 
-    private void clear() {
-        lottoNumbers = null;
-        money = null;
+    private Money getMoney(LottoView lottoView) {
+       return new Money(lottoView.getMoney());
     }
 
-    private void createLottoNumbers() {
-        money = new Money(lottoView.getMoney());
-        lottoNumbers = new LottoNumbers(money.getLottoCount());
-
+    private LottoNumbers createLottoNumbers(LottoView lottoView, Money money) {
+        LottoNumbers lottoNumbers = new LottoNumbers(money.getLottoCount());
         lottoView.showLottoNumbers(lottoNumbers);
+
+        return lottoNumbers;
     }
 
-    private void findWinningLotto() {
-        checkWinningLottoNumber(new LottoNumber(lottoView.getWinningLottoNumbers()));
-    }
-
-    private void checkWinningLottoNumber(LottoNumber winningNumber) {
-        Map<LottoRank, Long> rankGroup = lottoNumbers.getRankGroup(winningNumber);
+    private void findWinningLotto(LottoView lottoView, Money money, LottoNumbers lottoNumbers) {
+        Map<LottoRank, Long> rankGroup = lottoNumbers.getRankGroup(new LottoNumber(lottoView.getWinningLottoNumbers()));
 
         lottoView.showRankResult(rankGroup);
         lottoView.showProfitRate(money.getProfitRate(rankGroup));
