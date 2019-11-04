536baafa291c2c3092ae6cc3357b9fa0a4d8a806
pplenty <newtype26@naver.com>
2019-11-05 02:53:15 +0900

    feat: 숫자 생성기 테스트 코드 작성

---
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import java.util.List;
+
+/**
+ * Created by yusik on 2019/11/05.
+ */
+public class NumberGenerator {
+
+    public NumberGenerator() {
+    }
+    public List<Integer> generate(int limit) {
+        return null;
+    }
+
+}
