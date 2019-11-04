f81d982cc0929c7ef87700395415d40eccffc62d
pplenty <newtype26@naver.com>
2019-11-05 01:32:37 +0900

    feat: 인풋 테스트코드 작성&구현

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,51 @@
+package lotto.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by yusik on 2019/11/05.
+ */
+public class InputViewTest {
+
+    @DisplayName("구입 금액 입력")
+    @ParameterizedTest
+    @CsvSource(value = {"14000:14000", "1000:1000"}, delimiter = ':')
+    void receiveAmount(String input, int output) {
+
+        // given
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        InputView inputView = new InputView(in);
+
+        // when
+        int amount = inputView.receiveAmount();
+
+        //then
+        assertThat(amount).isEqualTo(output);
+
+    }
+
+    @DisplayName("당첨 번호 입력")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:6", "15, 2:2"}, delimiter = ':')
+    void getInput(String input, int size) {
+
+        // given
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        InputView inputView = new InputView(in);
+
+        // when
+        List<Integer> winningNumbers = inputView.receiveWinningNumbers();
+
+        //then
+        assertThat(winningNumbers).hasSize(size);
+
+    }
+}
