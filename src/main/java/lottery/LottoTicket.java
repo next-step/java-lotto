ff4d6d6a83c9365f457d041f1c974ac565dd82a7
iltaek <iltaekkwon@gmail.com>
2019-11-05 14:48:28 +0900

    feat($step2): LottoTicker 일급콜렉션 정의 및 테스트코드 추가

---
+++ src/main/java/lottery/LottoTicket.java
@@ -0,0 +1,72 @@
+package lottery;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class LottoTicket {
+
+    private static final Integer LOTTO_TICKER_SIZE = 6;
+    private static final Predicate<Integer> LOTTO_VALID_NUMBER_RANGE = integer -> (0 < integer) && (integer < 46);
+    private static final String ILLEGAL_TICKET_SIZE = "유효하지 않은 티켓입니다.(입력한 숫자가 6개가 아닙니다.)";
+    private static final String ILLEGAL_TICKET_NUMBER = "유효하지 않은 티켓입니다.(유요하지 않은 숫자가 입력되었습니다.)";
+    private static final String NUMBER_DELIMITER = ", ";
+    private Set<Integer> lottoNumbers;
+
+    public LottoTicket(List<Integer> lottoNumbers) {
+        this.lottoNumbers = checkTicketValidation(new HashSet<>(lottoNumbers));
+    }
+
+    public LottoTicket(Integer... lottoNumber) {
+        this(Arrays.asList(lottoNumber));
+    }
+
+    public String getNumbers() {
+        return lottoNumbers.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining(NUMBER_DELIMITER));
+    }
+
+    private Set<Integer> checkTicketValidation(Set<Integer> lottoNumbers) {
+        if (!isValidSize(lottoNumbers)) {
+            throw new IllegalArgumentException(ILLEGAL_TICKET_SIZE);
+        }
+
+        if (!isValidNumbers(lottoNumbers)) {
+            throw new IllegalArgumentException(ILLEGAL_TICKET_NUMBER);
+        }
+
+        return lottoNumbers;
+    }
+
+    private boolean isValidNumbers(Set<Integer> lottoNumbers) {
+        return lottoNumbers.stream()
+            .allMatch(LOTTO_VALID_NUMBER_RANGE);
+    }
+
+    private boolean isValidSize(Set<Integer> lottoNumbers) {
+        return lottoNumbers.size() == LOTTO_TICKER_SIZE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LottoTicket that = (LottoTicket) o;
+
+        return lottoNumbers != null ? lottoNumbers.equals(that.lottoNumbers) : that.lottoNumbers == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return lottoNumbers != null ? lottoNumbers.hashCode() : 0;
+    }
+}
---
+++ src/test/java/lottery/LottoTicketTest.java
@@ -0,0 +1,45 @@
+package lottery;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketTest {
+
+    @Test
+    void 로또번호에_유효하지않은_값_입력시_예외발생테스트() {
+        assertThatIllegalArgumentException().isThrownBy(()-> {
+            new LottoTicket(0, 1, 2, 3, 4, 5);
+        });
+    }
+
+    @Test
+    void 로또번호에_유효하지않은_개수의_숫자_입력시_예외발생테스트() {
+        assertThatIllegalArgumentException().isThrownBy(()-> {
+            new LottoTicket(1, 2, 3, 4, 5);
+        });
+    }
+
+    @Test
+    void 로또번호에_같은_값_입력시_예외발생테스트() {
+        assertThatIllegalArgumentException().isThrownBy(()-> {
+            new LottoTicket(1, 1, 2, 3, 4, 5);
+        });
+    }
+
+    @Test
+    void 로또번호를_문자열로_리턴테스트() {
+        assertThat(new LottoTicket(1, 2, 3, 4, 5, 6).getNumbers()).isEqualTo("1, 2, 3, 4, 5, 6");
+    }
+
+    @Test
+    void 같은번호_티켓이_같은지_확인테스트() {
+        assertThat(new LottoTicket(1, 2, 3, 4, 5, 6)).isEqualTo(new LottoTicket(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    void 다른번호_티켓이_다른지_확인테스트() {
+        assertThat(new LottoTicket(1, 2, 3, 4, 5, 6)).isNotEqualTo(new LottoTicket(2, 3, 4, 5, 6, 7));
+    }
+}
