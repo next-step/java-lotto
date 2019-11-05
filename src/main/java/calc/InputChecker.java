a25be27cd6085f667332a02f295992449e673587
iltaek <iltaekkwon@gmail.com>
2019-11-05 09:22:38 +0900

    refactor($feedback): 오타 수정

--- src/main/java/calc/InputChecker.java
+++ src/main/java/calc/InputChecker.java
@@ -7,11 +7,11 @@ import java.util.regex.Pattern;
 
 public class InputChecker {
 
-    private static final String DEFAULT_INPUT_DELIMETER = ",|:";
-    private static final String CUSTOM_INPUT_DELIMETER_START = "//";
-    private static final String CUSTOM_INPUT_DELIMETER_END = "\n";
-    private static final Pattern INPUT_DELIMETER_PATTERN = Pattern
-        .compile(CUSTOM_INPUT_DELIMETER_START + "(.)" + CUSTOM_INPUT_DELIMETER_END + "(.*)");
+    private static final String DEFAULT_INPUT_DELIMITER = ",|:";
+    private static final String CUSTOM_INPUT_DELIMITER_START = "//";
+    private static final String CUSTOM_INPUT_DELIMITER_END = "\n";
+    private static final Pattern INPUT_DELIMITER_PATTERN = Pattern
+        .compile(CUSTOM_INPUT_DELIMITER_START + "(.)" + CUSTOM_INPUT_DELIMITER_END + "(.*)");
     private final Numbers numbers;
 
     public InputChecker(String input) {
@@ -23,13 +23,13 @@ public class InputChecker {
             return new Numbers(Arrays.asList(0));
         }
 
-        Matcher inputMatcher = INPUT_DELIMETER_PATTERN.matcher(input);
+        Matcher inputMatcher = INPUT_DELIMITER_PATTERN.matcher(input);
         if (inputMatcher.find()) {
-            String customDelimeter = inputMatcher.group(1);
-            return convertInputWithDelimeter(inputMatcher.group(2), customDelimeter);
+            String customDelimiter = inputMatcher.group(1);
+            return convertInputWithDelimeter(inputMatcher.group(2), customDelimiter);
         }
 
-        return convertInputWithDelimeter(input, DEFAULT_INPUT_DELIMETER);
+        return convertInputWithDelimeter(input, DEFAULT_INPUT_DELIMITER);
     }
 
     private Numbers convertInputWithDelimeter(String input,
