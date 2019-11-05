d562ba3cd765b1b1ae23b9599ae0c6d400d3d334
chanin <pci2676@gmail.com>
2019-11-05 22:22:28 +0900

    java-lotto step3 feat - step3 출력화면 수정

--- src/main/java/step2/view/OutputView.java
+++ src/main/java/step2/view/OutputView.java
@@ -2,13 +2,18 @@ package step2.view;
 
 import step2.domain.LottoResult;
 import step2.domain.LottoTicket;
+import step2.domain.PriceGroup;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class OutputView {
     private static final int LOTTO_PRICE = 1000;
     private static final String MATCH_RESULT_FORMAT = "%d개 일치 (%d)원 - %d 개";
+    private static final int NEED_TO_SHOW = 3;
     private static final String RATE_RESULT = "총 수익률은 %.2f입니다.";
+    private static final String BONUS_MATCH_RESULT_FORMAT = "%d개 일치, 보너스 볼 일치 (%d)원 - %d 개";
 
     public static int getPurchaseAmount(int money) {
         int amount = money / LOTTO_PRICE;
@@ -28,8 +33,19 @@ public class OutputView {
     }
 
     private static void showBoard(LottoResult lottoResult) {
-        lottoResult.getResult()
-                .forEach((key, value) -> System.out.println(String.format(MATCH_RESULT_FORMAT, key.getMatchAmount(), key.getPrice(), value)));
+        Map<PriceGroup, Integer> sortedResult = new TreeMap<>(lottoResult.getResult());
+        sortedResult.forEach(OutputView::showEachBoard);
+    }
+
+    private static void showEachBoard(PriceGroup priceGroup, int amount) {
+        if (priceGroup.getMatchAmount() < NEED_TO_SHOW) {
+            return;
+        }
+        if (PriceGroup.FIVE_WITH_BONUS.equals(priceGroup)) {
+            System.out.println(String.format(BONUS_MATCH_RESULT_FORMAT, priceGroup.getMatchAmount(), priceGroup.getPrice(), amount));
+            return;
+        }
+        System.out.println(String.format(MATCH_RESULT_FORMAT, priceGroup.getMatchAmount(), priceGroup.getPrice(), amount));
     }
 
     private static void showRate(LottoResult lottoResult) {
