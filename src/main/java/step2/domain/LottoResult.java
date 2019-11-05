3d4b061b1636d1d9bf50d9327756c9eadf2763f3
chanin <pci2676@gmail.com>
2019-11-05 20:40:42 +0900

    java-lotto step3 refactor - step2 피드백 반영

--- src/main/java/step2/domain/LottoResult.java
+++ src/main/java/step2/domain/LottoResult.java
@@ -8,18 +8,11 @@ import java.util.Map;
 public class LottoResult {
 
     private static final int TICKET_MONEY = 1000;
+    private static final int ADD = 1;
     private Map<PriceGroup, Integer> result;
 
     public LottoResult(WinningNumbers winningNumbers, List<LottoTicket> lottoTickets) {
         makeResult(winningNumbers, lottoTickets);
-    }
-
-    private Map<PriceGroup, Integer> initResult() {
-        Map<PriceGroup, Integer> result = new HashMap<>();
-        for (PriceGroup priceGroup : PriceGroup.values()) {
-            result.put(priceGroup, 0);
-        }
-        return result;
     }
 
     private void makeResult(WinningNumbers winningNumbers, List<LottoTicket> lottoTickets) {
@@ -28,9 +21,17 @@ public class LottoResult {
         for (LottoTicket lottoTicket : lottoTickets) {
             int matchAmount = winningNumbers.getMatchAmount(lottoTicket);
             PriceGroup priceGroup = PriceGroup.findPriceByMatchAmount(matchAmount);
-            result.put(priceGroup, result.get(priceGroup) + 1);
+            result.put(priceGroup, result.get(priceGroup) + ADD);
         }
         this.result = result;
+    }
+
+    private Map<PriceGroup, Integer> initResult() {
+        Map<PriceGroup, Integer> result = new HashMap<>();
+        for (PriceGroup priceGroup : PriceGroup.values()) {
+            result.put(priceGroup, 0);
+        }
+        return result;
     }
 
     public Map<PriceGroup, Integer> getResult() {
