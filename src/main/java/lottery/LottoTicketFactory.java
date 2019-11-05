161035fb1639142de08c5a8149f54524f86929fc
iltaek <iltaekkwon@gmail.com>
2019-11-05 22:15:34 +0900

    feat($step2): LottoTicketFactory와 테스트 추가

---
+++ src/main/java/lottery/LottoTicketFactory.java
@@ -0,0 +1,29 @@
+package lottery;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoTicketFactory {
+
+    private static final List<Integer> LOTTO_NUMBERS = IntStream.range(1, 46)
+                                                                .boxed()
+                                                                .collect(Collectors.toList());
+
+
+
+    public static List<LottoTicket> generateRandomGames(int numberOfTicket) {
+        List<LottoTicket> randomGames = new ArrayList<>();
+        IntStream.range(0, numberOfTicket)
+            .forEach(i -> randomGames.add(generateRandomGame()));
+        return randomGames;
+    }
+
+    private static LottoTicket generateRandomGame() {
+        Collections.shuffle(LOTTO_NUMBERS, new Random());
+        return new LottoTicket(LOTTO_NUMBERS.subList(0, 6));
+    }
+}
---
+++ src/test/java/lottery/LottoTicketFactoryTest.java
@@ -0,0 +1,17 @@
+package lottery;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoTicketFactoryTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8})
+    void 요구한_개수만큼_로또티켓_생성테스트(int numberOfTickets) {
+        List<LottoTicket> tickets = LottoTicketFactory.generateRandomGames(numberOfTickets);
+        assertThat(tickets.size()).isEqualTo(numberOfTickets);
+    }
+}
