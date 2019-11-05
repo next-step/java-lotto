6500e71dca4b06a6d2a10dc433929f8f14a0eb2f
iltaek <iltaekkwon@gmail.com>
2019-11-05 22:26:38 +0900

    refactor($step2): LottoTicket이 정렬된 로또번호를 갖도록 수정

--- src/main/java/lottery/LottoTicket.java
+++ src/main/java/lottery/LottoTicket.java
@@ -1,6 +1,8 @@
 package lottery;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,7 +16,7 @@ public class LottoTicket {
     private static final String ILLEGAL_TICKET_SIZE = "유효하지 않은 티켓입니다.(입력한 숫자가 6개가 아닙니다.)";
     private static final String ILLEGAL_TICKET_NUMBER = "유효하지 않은 티켓입니다.(유요하지 않은 숫자가 입력되었습니다.)";
     private static final String NUMBER_DELIMITER = ", ";
-    private Set<Integer> lottoNumbers;
+    private List<Integer> lottoNumbers;
 
     public LottoTicket(List<Integer> lottoNumbers) {
         this.lottoNumbers = checkTicketValidation(new HashSet<>(lottoNumbers));
@@ -24,7 +26,7 @@ public class LottoTicket {
         this(Arrays.asList(lottoNumber));
     }
 
-    private Set<Integer> checkTicketValidation(Set<Integer> lottoNumbers) {
+    private List<Integer> checkTicketValidation(Set<Integer> lottoNumbers) {
         if (!isValidSize(lottoNumbers)) {
             throw new IllegalArgumentException(ILLEGAL_TICKET_SIZE);
         }
@@ -33,6 +35,11 @@ public class LottoTicket {
             throw new IllegalArgumentException(ILLEGAL_TICKET_NUMBER);
         }
 
+        return convertSortedNumber(new ArrayList<>(lottoNumbers));
+    }
+
+    private List<Integer> convertSortedNumber(List<Integer> lottoNumbers) {
+        Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
 
