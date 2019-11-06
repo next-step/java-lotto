f1224bc9bd8d3e7d5481c13d506b72e15aa4fb44
oh29oh29 <hyukjae89@gmail.com>
2019-11-06 22:00:12 +0900

    feat 숫자 입력 예외 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,19 +1,42 @@
 package lotto.view;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
+    private static final int NOT_NUMBER = -1;
+
     public static int inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return new Scanner(System.in).nextInt();
+        int amount;
+        do {
+            amount = input();
+        } while (amount == NOT_NUMBER);
+
+        return amount;
     }
 
     public static int inputDirectCount() {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        return new Scanner(System.in).nextInt();
+        int count;
+        do {
+            count = input();
+        } while (count == NOT_NUMBER);
+
+        return count;
+    }
+
+    private static int input() {
+        try {
+            return new Scanner(System.in).nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("숫자만 입력해 주세요.");
+        }
+
+        return NOT_NUMBER;
     }
 
     public static List<String> inputDirectLottoNumbers(final int directCount) {
