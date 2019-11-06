5636d03f71ccd9e7e612e14aa1475bc93dcaab8e
iltaek <iltaekkwon@gmail.com>
2019-11-06 22:07:00 +0900

    refactor($feedback): 사용하지않는 코드 제거

--- src/test/java/lottery/domain/LottoTicketsTest.java
+++ src/test/java/lottery/domain/LottoTicketsTest.java
@@ -3,21 +3,16 @@ package lottery.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoTicketsTest {
 
-    private LottoTicketSeller seller;
-    private List<Integer> winNumbers;
     private List<LottoTicket> tickets = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-        seller = new LottoTicketSeller();
-        winNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         tickets.add(new LottoTicket(1, 2, 3, 4, 5, 6));
         tickets.add(new LottoTicket(1, 2, 3, 4, 5, 7));
     }
