04e055ff62d97ccd45af7d0dbd0b659054006a62
jahun.kim <jahun.kim@linecorp.com>
2019-11-07 02:47:50 +0900

    feat(data) : Add enum class Rank to define grade and prize

---
+++ src/main/java/lotto/data/Rank.java
@@ -0,0 +1,35 @@
+package lotto.data;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(4, 1_500_000),
+    FOURTH(3, 50_000),
+    FIFTH(2, 5_000),
+    MISS(0, 0);
+
+    private int matchCount;
+    private int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Rank valueOf(int matchCount) {
+        return Arrays.stream(Rank.values())
+              .filter(rank -> rank.matchCount == matchCount)
+              .findAny().orElse(Rank.MISS);
+    }
+}
+
