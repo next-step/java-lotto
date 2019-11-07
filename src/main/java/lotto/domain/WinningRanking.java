b0f14cc1eeb63625766579e4c8c1d6ad92d0b5e0
pplenty <newtype26@naver.com>
2019-11-07 20:41:48 +0900

    refactor: 피드백 반영 - 치명적 실수 수정

--- src/main/java/lotto/domain/WinningRanking.java
+++ src/main/java/lotto/domain/WinningRanking.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public enum WinningRanking {
 
     FIRST(6, 2_000_000_000, false),
-    SECOND(5, 1_500_000, true),
+    SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000, false),
     FIFTH(3, 5_000, false),
