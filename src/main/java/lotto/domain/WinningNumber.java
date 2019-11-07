865249220e4b1353e2d0e655849bf98a2663d31f
Kim YoonHo <kimunoya@gmail.com>
2019-11-08 01:36:14 +0900

    feat: 로또 당첨 번호 객체 추가
    - 로또 당첨결과 리스트 리턴

---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-07 14:49
+ */
+public class WinningNumber {
+
+    private final List<Integer> winningNumbers;
+
+    public WinningNumber(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public List<LottoWinningAmount> getWinningAmount(List<Lotto> purchasedLotto) {
+        List<LottoWinningAmount> winningAmounts = new ArrayList<>();
+        purchasedLotto.forEach(lotto -> {
+            winningAmounts.add(LottoWinningAmount.findWinningAmount(lotto.matchLottoNumberCount(this.winningNumbers)));
+        });
+
+        return Collections.unmodifiableList(winningAmounts);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningNumber that = (WinningNumber) o;
+        return Objects.equals(winningNumbers, that.winningNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winningNumbers);
+    }
+}
