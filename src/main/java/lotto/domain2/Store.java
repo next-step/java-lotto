1426fe4c12776d3d070f398b472d35c4ece87efb
oh29oh29 <hyukjae89@gmail.com>
2019-11-08 23:08:57 +0900

    feat 로또 가게에서 로또 발급

--- src/main/java/lotto/domain2/Store.java
+++ src/main/java/lotto/domain2/Store.java
@@ -3,10 +3,20 @@ package lotto.domain2;
 import java.util.Objects;
 
 public class Store {
+    private static final int LOTTO_PRICE = 1000;
     private final LottoMachine lottoMachine;
 
     public Store(final LottoMachine lottoMachine) {
         this.lottoMachine = lottoMachine;
+    }
+
+    public Lottos issueLotto(final int money) {
+        final int countOfLotto = money / LOTTO_PRICE;
+        if (countOfLotto == 0) {
+            throw new IllegalArgumentException(String.format("로또 하나의 구매 가격은 %s원 입니다.", LOTTO_PRICE));
+        }
+
+        return lottoMachine.issue(countOfLotto);
     }
 
     @Override
--- src/test/java/lotto/domain2/StoreTest.java
+++ src/test/java/lotto/domain2/StoreTest.java
@@ -1,8 +1,11 @@
 package lotto.domain2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StoreTest {
 
@@ -10,5 +13,22 @@ public class StoreTest {
     void 생성() {
         Store store = new Store(new LottoMachine());
         assertThat(store).isEqualTo(new Store(new LottoMachine()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = 1000)
+    void 로또발급(final int money) {
+        Store store = new Store(new LottoMachine());
+        Lottos lottos = store.issueLotto(money);
+        assertThat(lottos.count()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = 999)
+    void 로또발급_구매가격미달(final int money) {
+        Store store = new Store(new LottoMachine());
+        assertThatThrownBy(() -> {
+            store.issueLotto(money);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
