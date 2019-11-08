bbfd47dd35baee685b05b742cfb2b326b959fd0f
Kim YoonHo <kimunoya@gmail.com>
2019-11-09 01:15:32 +0900

    feat: 로또 도메인 생성
    - 기존 코드를 삭제하고 새로운 로또 도메인 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,38 +1,27 @@
 package lotto.domain;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 /**
  * @author : 김윤호
  * @version : 1.0
- * @date : 2019-11-02 21:45
+ * @date : 2019-11-09 01:05
  */
 public class Lotto {
 
-    private final List<Integer> lottoNumbers;
-
-    public Lotto(Integer... lottoNumbers) {
-        this(Arrays.asList(lottoNumbers));
-    }
+    private List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개로 이루어져야 합니다.");
+        }
+
+        if (lottoNumbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException("로또 번호는 중복이 불가능 합니다.");
+        }
+
         this.lottoNumbers = lottoNumbers;
-        shuffle();
-    }
-
-    private void shuffle() {
-        Collections.shuffle(this.lottoNumbers);
-    }
-
-    public List<Integer> getLottoNumbers() {
-        return this.lottoNumbers;
-    }
-
-    public int matchLottoNumberCount(List<Integer> winningNumbers) {
-        return (int) this.lottoNumbers.stream().filter(number -> winningNumbers.contains(number)).count();
     }
 
     @Override
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,85 +1,35 @@
 package lotto.domain;
 
-import lotto.LottoOrder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * @author : 김윤호
  * @version : 1.0
- * @date : 2019-11-02 21:43
+ * @date : 2019-11-09 01:03
  */
 public class LottoTest {
-
     @Test
-    void create_lotto_test() {
+    void 생성() {
         Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
-        assertThat(lotto).isNotEqualTo(new Lotto(Arrays.asList(1,2,3,4,5,6)));
+        assertThat(lotto).isEqualTo(new Lotto(Arrays.asList(1,2,3,4,5,6)));
     }
 
     @Test
-    void create_lotto_number_test() {
-        LottoNumberStrategy lottoNumberStrategy = new LottoNumberStrategy();
-        List<Integer> lottoNumbers = lottoNumberStrategy.generateLottoNumber();
-        assertThat(lottoNumbers).hasSize(6);
-        assertThat(lottoNumbers).allMatch(value -> value >= 1 && value <= 45);
+    void 로또_번호_갯수_검증() {
+        assertThatThrownBy(() -> {
+            new Lotto(Arrays.asList(1,2,3,4,5));
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void lotto_shuffle_test() {
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
-        assertThat(lotto).isNotEqualTo(new Lotto(Arrays.asList(1,2,3,4,5,6)));
-    }
-
-    @Test
-    void 구매된_로또_갯수() {
-        LottoOrder lottoOrder = new LottoOrder(14000);
-        assertThat(lottoOrder.getLottoAmount()).isEqualTo(14);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4,5,6", "2,3,4,5", "3,a,b,c,de,f"})
-    void input_lotto_number_test(String lottoNumber) {
-        String[] numbers = lottoNumber.split(",");
-        for (String number : numbers) {
-            is_number(number);
-            check_lotto_number_range(Integer.parseInt(number));
-        }
-
-        assertThat(numbers).hasSize(6);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1", "2"})
-    void check_lotto_number_range(int number) {
-        final int minValue = 1;
-        final int maxValue = 45;
-        assertThat(number >= minValue && number <= maxValue).isTrue();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1", "2"})
-    void is_number(String number) {
-        final Pattern NUMERIC = Pattern.compile("^[0-9]+$");
-        assertThat(NUMERIC.matcher(number).matches()).isTrue();
-    }
-
-    @Test
-    void 당첨조회() {
-        Integer[] number = {1,2,3,4,5,6};
-        List<Integer> winningNumbers = Arrays.asList(number);
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
-        assertThat(lotto.matchLottoNumberCount(winningNumbers)).isEqualTo(6);
+    void 로또_번호_중복_번호_검증() {
+        assertThatThrownBy(() -> {
+            new Lotto(Arrays.asList(1,2,3,4,5,5));
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
-
