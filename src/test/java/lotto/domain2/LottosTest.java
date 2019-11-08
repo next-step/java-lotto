9d7f48eb11d0536a4abac4a8138e5f0e3e72a89b
oh29oh29 <hyukjae89@gmail.com>
2019-11-08 22:20:05 +0900

    refactor 로또묶음 객체 생성

---
+++ src/test/java/lotto/domain2/LottosTest.java
@@ -0,0 +1,26 @@
+package lotto.domain2;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6"})
+    void 생성(final String input) {
+        final String[] inputSplit = input.split(",");
+        final List<Integer> numbers = new ArrayList<>();
+        for (String number : inputSplit) {
+            numbers.add(Integer.parseInt(number));
+        }
+
+        Lottos lottos = new Lottos(Collections.singletonList(new Lotto(numbers)));
+        assertThat(lottos).isEqualTo(new Lottos(Collections.singletonList(new Lotto(numbers))));
+    }
+}
