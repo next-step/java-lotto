4a75a86f2b1979fac65e9cf06dc2cc93646204f0
oh29oh29 <hyukjae89@gmail.com>
2019-11-08 22:07:05 +0900

    refactor 로또 객체 추가

---
+++ src/main/java/lotto/domain2/Lotto.java
@@ -0,0 +1,34 @@
+package lotto.domain2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+    private static final int NUMBER_COUNT = 6;
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(final List<Integer> numbers) {
+        if (NUMBER_COUNT != numbers.size()) {
+            throw new IllegalArgumentException(String.format("로또번호의 개수는 %s개만 가능합니다.", NUMBER_COUNT));
+        }
+
+        this.lottoNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            lottoNumbers.add(LottoNumber.of(number));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
+    }
+}
---
+++ src/test/java/lotto/domain2/LottoTest.java
@@ -0,0 +1,35 @@
+package lotto.domain2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTest {
+
+    @Test
+    void 생성() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lotto).isEqualTo(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,2,3,4,5", "1,2,3,4,5,6,7"})
+    void 로또번호_개수_부족하거나_많을때(final String input) {
+        String[] inputSplit = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : inputSplit) {
+            numbers.add(Integer.parseInt(number));
+        }
+
+        assertThatThrownBy(() -> {
+            new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
