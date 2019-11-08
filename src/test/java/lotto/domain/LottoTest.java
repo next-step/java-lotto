bb470847f4294c3ae0cabbd930816254ac036b5d
Kim YoonHo <kimunoya@gmail.com>
2019-11-09 01:34:40 +0900

    test: 정적 팩토리 메서드를 이용하여 로또 객체 생성 테스트 코드 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -15,21 +15,33 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class LottoTest {
     @Test
     void 생성() {
-        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
-        assertThat(lotto).isEqualTo(new Lotto(Arrays.asList(1,2,3,4,5,6)));
+        Lotto lotto = Lotto.of(Arrays.asList(1,2,3,4,5,6));
+        assertThat(lotto).isEqualTo(Lotto.of(Arrays.asList(1,2,3,4,5,6)));
     }
 
     @Test
     void 로또_번호_갯수_검증() {
         assertThatThrownBy(() -> {
-            new Lotto(Arrays.asList(1,2,3,4,5));
+            Lotto.of(Arrays.asList(1,2,3,4,5));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 로또_번호_중복_번호_검증() {
         assertThatThrownBy(() -> {
-            new Lotto(Arrays.asList(1,2,3,4,5,5));
+            Lotto.of((Arrays.asList(1,2,3,4,5,5)));
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 생성_가변인자() {
+        Lotto lotto = Lotto.of(1,2,3,4,5,6);
+        assertThat(lotto).isEqualTo(Lotto.of(Arrays.asList(1,2,3,4,5,6)));
+    }
+
+    @Test
+    void 생성_문자열() {
+        Lotto lotto = Lotto.of("1,2,3,4,5,6");
+        assertThat(lotto).isEqualTo(Lotto.of(Arrays.asList(1,2,3,4,5,6)));
     }
 }
