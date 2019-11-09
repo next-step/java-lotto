0c74f84f8cb3ac8ef44bf221d7b4b68b239eb96b
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 19:03:20 +0900

    feat(domain) : Add manual lotto feature

--- src/main/java/lotto/VendingMachine.java
+++ src/main/java/lotto/VendingMachine.java
@@ -1,16 +1,20 @@
 package lotto;
 
+import java.util.List;
+
 import lotto.data.Lottos;
 
 public class VendingMachine {
     private Wallet wallet;
+    private List<Lotto> manualNumbers;
 
-    public VendingMachine(int budget) {
-        this.wallet = new Wallet(budget);
+    public VendingMachine(Wallet wallet, List<Lotto> manualNumbers) {
+        this.wallet = wallet;
+        this.manualNumbers = manualNumbers;
     }
 
     public Lottos buy() {
-        return new Lottos(wallet);
+        return new Lottos(wallet, manualNumbers);
     }
 
     public int getExpend() {
