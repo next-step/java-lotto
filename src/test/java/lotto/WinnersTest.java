4a65d287261b7225a1a27c2071d4bfc84514caff
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 18:07:57 +0900

    test() : Fix winners test code

--- src/test/java/lotto/WinnersTest.java
+++ src/test/java/lotto/WinnersTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.data.LottoNumbers;
@@ -15,33 +16,13 @@ class WinnersTest {
     private Winners winners;
 
     @Test
-    void sort_grade() {
-        Lottos lottos = lottoGenerate(10);
-        LottoNumbers winner = winnerGenerate(0);
-        winners = new Winners(lottos, winner);
-
-        assertThat(winners.getResult().get(Rank.FIRST)).isEqualTo(1);
-        assertThat(winners.getResult().get(Rank.FIFTH)).isEqualTo(1);
-        assertThat(winners.getResult().get(Rank.MISS)).isEqualTo(5);
-    }
-
-    @Test
     void total_earning() {
         Lottos lottos = lottoGenerate(2);
-        LottoNumbers winner = winnerGenerate(0);
-        winners = new Winners(lottos, winner);
-
-        assertThat(winners.totalEarning()).isEqualTo(Rank.FIRST.getPrize() + Rank.SECOND.getPrize());
-    }
-
-    @Test
-    void match_bonus() {
-        Lottos lottos = lottoGenerate(10);
         LottoNumbers winner = winnerGenerate(0);
         int bonus = 7;
         winners = new Winners(lottos, winner, bonus);
 
-        assertThat(winners.getResult().get(Rank.SECOND)).isEqualTo(1);
+        assertThat(winners.getTotalEarning()).isEqualTo(Rank.FIRST.getPrize() + Rank.SECOND.getPrize());
     }
 
     private LottoNumbers winnerGenerate(int weight) {
