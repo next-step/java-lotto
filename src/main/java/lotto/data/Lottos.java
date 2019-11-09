1a445cd3b73d9c431c4107e1582e346f1aeea45b
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 18:06:32 +0900

    feat(data) : Add generate lotto instance and rank

--- src/main/java/lotto/data/Lottos.java
+++ src/main/java/lotto/data/Lottos.java
@@ -1,8 +1,12 @@
 package lotto.data;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import lotto.Lotto;
+import lotto.Wallet;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -11,11 +15,31 @@ public class Lottos {
         this.lottos = lottos;
     }
 
+    public Lottos(Wallet wallet) {
+        lottos = initLottos(wallet);
+    }
+
     public int getCount() {
         return lottos.size();
     }
 
+    public List<Rank> rankLottos(LottoNumbers winningNumbers, int bonus) {
+        return lottos.stream()
+                     .map(lotto -> Rank.valueOf(lotto.matchCount(winningNumbers), lotto.isMatchBonus(bonus)))
+                     .collect(toList());
+    }
+
     public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    private List<Lotto> initLottos(Wallet wallet) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        while (wallet.isEnoughBudget()) {
+            lottos.add(new Lotto());
+        }
+
         return lottos;
     }
 }
