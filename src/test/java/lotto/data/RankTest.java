3601f1f5e331c2c300b1dbc76def1788e5ef69ad
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 18:14:54 +0900

    test() : Fix rank test code

--- src/test/java/lotto/data/RankTest.java
+++ src/test/java/lotto/data/RankTest.java
@@ -9,9 +9,11 @@ class RankTest {
     @Test
     void contains_bonus() {
         assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.valueOf(6, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOf(6, true)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(4, true)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.valueOf(0, true)).isEqualTo(Rank.MISS);
+        assertThat(Rank.valueOf(0, false)).isEqualTo(Rank.MISS);
     }
 }
