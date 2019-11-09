62c15db7ae0a78e7bebcc69762ebfe058479db22
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 19:11:09 +0900

    test() : Add manual lotto feature test code

--- src/test/java/lotto/VendingMachineTest.java
+++ src/test/java/lotto/VendingMachineTest.java
@@ -1,16 +1,35 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import lotto.data.LottoNumbers;
 
 public class VendingMachineTest {
 
     private VendingMachine vendingMachine;
 
+    @DisplayName("buy_without_manual_lotto")
     @Test
     void buy_lotto() {
-        vendingMachine = new VendingMachine(10000);
+        vendingMachine = new VendingMachine(new Wallet(10000), Collections.emptyList());
         assertThat(vendingMachine.buy().getCount()).isEqualTo(10);
+    }
+
+    @DisplayName("buy_contains_manual_lotto")
+    @Test
+    void buy_lotto_manual() {
+        assertThatThrownBy(() -> new VendingMachine(new Wallet(3999), Arrays.asList(
+                new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6))))
+        )).isInstanceOf(RuntimeException.class);
     }
 }
