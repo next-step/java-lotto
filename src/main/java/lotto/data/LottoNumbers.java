099d5b54b126fa2fa30624e5d9ccc2c15c07d057
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 18:04:53 +0900

    feat(data) : Add sorting in constructor

--- src/main/java/lotto/data/LottoNumbers.java
+++ src/main/java/lotto/data/LottoNumbers.java
@@ -8,12 +8,17 @@ public class LottoNumbers {
 
     public LottoNumbers(List<Integer> numbers) {
         this.numbers = numbers;
+        Collections.sort(numbers);
     }
 
     public int matchCount(LottoNumbers winningNumbers) {
         return (int) winningNumbers.getNumbers().stream()
                                    .filter(numbers::contains)
                                    .count();
+    }
+
+    public boolean isMatchBonus(int bonus) {
+        return numbers.contains(bonus);
     }
 
     public List<Integer> getNumbers() {
