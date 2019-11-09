cc7616ad4e6a887955f45826c64d62187411e299
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 17:20:54 +0900

    test() : Add InputParser test code

---
+++ src/test/java/lotto/controller/LottoInputParserTest.java
@@ -0,0 +1,68 @@
+package lotto.controller;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.data.LottoNumbers;
+import lotto.input.InputReader;
+
+class LottoInputParserTest {
+    @DisplayName("bonus_exceeded_value")
+    @Test
+    void validate_bonus_min() {
+        LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+        InputReader inputReader1 = new TestInputReader("0", "1, 2, 3, 4, 5, 6");
+        InputReader inputReader2 = new TestInputReader("46", "1, 2, 3, 4, 5, 6");
+
+        assertThatThrownBy(() -> LottoInputParser.getBonus(inputReader1, winningNumbers)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> LottoInputParser.getBonus(inputReader2, winningNumbers)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("winning_number_counts_range")
+    @Test
+    void validate_winning_number_count() {
+        InputReader inputReader1 = new TestInputReader("45", "1, 2, 3, 4, 5, 6, 7");
+        InputReader inputReader2 = new TestInputReader("45", "1, 2, 3, 4, 5");
+
+        assertThatThrownBy(() -> LottoInputParser.getWinningInput(inputReader1)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> LottoInputParser.getWinningInput(inputReader2)).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("bonus_number_duplicate")
+    @Test
+    void validate_duplicate_number() {
+        LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+        InputReader inputReader = new TestInputReader("6", "1, 2, 3, 4, 5, 6");
+
+        assertThatThrownBy(() -> LottoInputParser.getBonus(inputReader, winningNumbers)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static class TestInputReader implements InputReader {
+        private final String readIntValue;
+        private final String readLineValue;
+
+        private TestInputReader(String readIntValue, String readLineValue) {
+            this.readIntValue = readIntValue;
+            this.readLineValue = readLineValue;
+        }
+
+        @Override
+        public Optional<Integer> readInt(String comment) {
+            try {
+                return Optional.of(Integer.valueOf(readIntValue));
+            } catch (NumberFormatException e) {
+                return Optional.empty();
+            }
+        }
+
+        @Override
+        public String readLine(String comment) {
+            return readLineValue;
+        }
+    }
+}
