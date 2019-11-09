28bdd9a78043280e6a52e8608acb4c3b4d836089
Kim YoonHo <kimunoya@gmail.com>
2019-11-10 00:48:44 +0900

    feat: 로또 구매금액을 입력하면 로또를 구매해주는 기능 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,7 +1,12 @@
 package lotto;
 
-import lotto.view.LottoInputView;
-import lotto.view.LottoResultView;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
+import lotto.domain.LottoGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : 김윤호
@@ -9,8 +14,18 @@ import lotto.view.LottoResultView;
  * @date : 2019-11-02 22:36
  */
 public class LottoMachine {
-    public static void main(String[] args) {
-        LottoOrder lottoMachine = new LottoOrder(LottoInputView.inputPurchaseAmount());
-        LottoResultView.printLotto(lottoMachine.getLotto());
+
+    private final LottoAmount lottoAmount;
+    private final List<Lotto> lottos;
+
+    public LottoMachine(LottoAmount lottoAmount) {
+        this.lottoAmount = lottoAmount;
+        this.lottos = new ArrayList<>();
+    }
+
+    public void purchase(LottoGenerator lottoGenerator) {
+        for (int amount = 0; amount < lottoAmount.amount(); amount++) {
+            lottos.add(Lotto.of(lottoGenerator.generate()));
+        }
     }
 }
