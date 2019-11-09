da99eb74c2bfd10550b96b110af050b23e4ae2c1
jahun1105@naver.com <jahun.kim@linecorp.com>
2019-11-09 18:13:12 +0900

    fix() : Remove unused class

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import StringCalculator.IntegerAdder;
-
-public class Main {
-    public static void main(String[] args) {
-        String input = "//&\\n1&2&3&4";
-        IntegerAdder adder = new IntegerAdder(input);
-        System.out.println(adder.calculate());
-    }
-}
