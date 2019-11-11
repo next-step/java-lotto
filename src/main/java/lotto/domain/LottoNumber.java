f9457e661b3b8b1ffbe4eac922521968c3c070bb
vagabond95 <progjh@gmail.com>
2019-11-11 23:04:52 +0900

    [refactor] LottoNumber 를 콜렉션에서 사용하기 위한 기본 메소드 override

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,8 +1,8 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.Objects;
 
-public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
 
     public static final int MAX_NUMBER = 45;
     public static final int MIN_NUMBER = 1;
@@ -13,13 +13,33 @@ public class LottoNumber {
         this.number = number;
     }
 
-    public boolean hasMatchedNumber(List<Integer> numbers) {
-        return numbers
+    public boolean hasMatchedNumber(LottoNumbers lottoNumbers) {
+        return lottoNumbers
+                .getValue()
                 .stream()
+                .map(LottoNumber::getValue)
                 .anyMatch(number -> this.number == number);
     }
 
-    int getValue() {
+    public int getValue() {
         return number;
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return Integer.compare(number, o.getValue());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
