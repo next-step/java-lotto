cd91f0dcd9f137e20ed8a3eead4f5a1466bee350
vagabond95 <progjh@gmail.com>
2019-11-11 23:25:12 +0900

    [style] reformat classes

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, false,2_000_000_000),
-    SECOND(5,true, 30_000_000),
-    THIRD(5,true, 1_500_000),
-    FOURTH(4,false, 50_000),
-    FIFTH(3,false, 5_000),
-    NOT_MATCH(0,false, 0);
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, true, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NOT_MATCH(0, false, 0);
 
     private int matchCount;
     private boolean needCheckBonusNumber;
@@ -21,7 +21,7 @@ public enum LottoRank {
     }
 
     public static LottoRank find(long matchCount, BonusNumberStrategy bonusNumberStrategy) {
-        LottoRank lottoRank =  Arrays.stream(LottoRank.values())
+        LottoRank lottoRank = Arrays.stream(LottoRank.values())
                 .filter(rank -> rank.matchCount == matchCount)
                 .findFirst()
                 .orElse(NOT_MATCH);
