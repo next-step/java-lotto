d6f0a90bdf3667bd15de8a8ba19068b902aad35d
cwpark723 <chiwon7942@naver.com>
2019-11-12 14:00:11 +0900

    (refactor) Step2 money final키워드 수정

--- src/main/java/com/lotto/model/Money.java
+++ src/main/java/com/lotto/model/Money.java
@@ -4,7 +4,8 @@ public class Money {
 
     private static final int LOTTO_PRICE_PER_ONE = 1000;
     private static final String OUT_OF_MONEY = "로또를 살 수 있는 금액이 부족합니다.";
-    private final int money;
+
+    private int money;
 
     public Money(int money) {
         this.money = money;
