5ef98670d94bf41e66d0c90ccd444bc03773e823
sy.kim <kauksy9022@gmail.com>
2019-11-13 15:08:07 +0900

    [Update]: Lotto의 List<Integer> 원시값을 List<LottoNumber>로 포장

--- src/main/java/step3/Lotto.java
+++ src/main/java/step3/Lotto.java
@@ -4,67 +4,45 @@ import java.util.*;
 
 public class Lotto {
 
-    static final int LOTTO_NUMBER_MIN = 1;
-    static final int LOTTO_NUMBER_MAX = 45;
-    static int lottoBonusNumber;
+    private List<LottoNumber> lotto;
 
-    private List<Integer> newLotto;
-
-    public Lotto(List<Integer> newLotto) {
-        this.newLotto = new ArrayList<>(newLotto);
+    public Lotto() {
     }
 
-    public static Lotto creatLottoWinningNumbers(String[] inputNewLottoNumbers) {
-        checkWinningNumberLength(inputNewLottoNumbers);
-        checkOverlapWinningNumber(inputNewLottoNumbers);
-
-        return new Lotto(checkWinningNumberRange(inputNewLottoNumbers));
+    public Lotto(List<LottoNumber> lotto) {
+        this.lotto = new ArrayList<>(lotto);
     }
 
-    public static List<Integer> valueOf(Lotto lotto) {
-        return lotto.newLotto;
+    public static List<LottoNumber> valueOf(Lotto lotto) {
+        return lotto.lotto;
     }
 
-    public static void checkWinningNumberLength(String[] inputNewLottoNumbers) {
-        if (inputNewLottoNumbers.length != LottoGenerator.WINNING_NUMBERS_LENGTH)
-            throw new IllegalArgumentException("당첨번호는" + LottoGenerator.WINNING_NUMBERS_LENGTH + "개를 입력하셔야 합니다.");
-    }
-
-    public static void checkOverlapWinningNumber(String[] inputNewLottoNumbers) {
-        Set<String> uniqueWinningNumber = new HashSet<>(Arrays.asList(inputNewLottoNumbers));
-        if (uniqueWinningNumber.size() != inputNewLottoNumbers.length)
-            throw new IllegalArgumentException("당첨번호에 중복된 값이 있습니다.");
-    }
-
-    public static ArrayList<Integer> checkWinningNumberRange(String[] inputNewLottoNumbers) {
-        ArrayList<Integer> inputWinningNumberList = new ArrayList<>();
-        for (int i = 0; i < LottoGenerator.WINNING_NUMBERS_LENGTH; i++) {
-            LottoNumber.checkEachNumber(Integer.parseInt((inputNewLottoNumbers[i])));
-            inputWinningNumberList.add(Integer.parseInt((inputNewLottoNumbers[i])));
-        }
-        Collections.sort(inputWinningNumberList);
-        return inputWinningNumberList;
-    }
-
-    public static List<Integer> lotteryStatics(Set<Lotto> purchasedLottos, List<Integer> winningNumbers) {
-        List<Integer> resultMatching = new ArrayList<>();
+    public static List<LottoNumber> lotteryStatics(Set<Lotto> purchasedLottos, Lotto winningNumbers) {
+        List<LottoNumber> resultMatching = new ArrayList<>();
         for (Lotto purchasedLotto : purchasedLottos) {
-            resultMatching.add(matchWinningNumbers(purchasedLotto, winningNumbers));
+            int add = 0;
+            add = +matchWinningNumbers(Lotto.valueOf(purchasedLotto), Lotto.valueOf(winningNumbers));
+            resultMatching.add(new LottoNumber(add, false));
         }
         return resultMatching;
     }
 
-    private static int matchWinningNumbers(Lotto purchasedLotto, List<Integer> winningNumbers) {
-        final int count = (int) winningNumbers.stream()
-                .filter(purchasedLotto.newLotto::contains)
-                .count();
+    private static int matchWinningNumbers(List<LottoNumber> purchasedLotto, List<LottoNumber> winningNumbers) {
+        Iterator<LottoNumber> iter = purchasedLotto.iterator();
+        int count = 0;
+        while (iter.hasNext()) {
+            LottoNumber compareNumber = iter.next();
+            count += (int) winningNumbers.stream()
+                    .filter(x -> x.equals(compareNumber))
+                    .count();
+        }
         return count;
     }
 
     public static void pickOutBonusWinningLotto(Set<Lotto> purchasedLottos) {
         int indexCount = 0;
         for (Lotto purchasedLotto : purchasedLottos) {
-            LottoLottery.purchasedLottoCompareBonusNumber(purchasedLotto.newLotto, indexCount);
+            LottoLottery.purchasedLottoCompareBonusNumber(purchasedLotto.lotto, indexCount);
             indexCount++;
         }
     }
