1d834ac4a6664750a789158c0d8d6af5acfbfdd5
liquidjoo <liquidjoo@gmail.com>
2019-11-16 01:24:17 +0900

    feat : LottoNo Validation

--- src/main/java/lotto/domain/LottoNo.java
+++ src/main/java/lotto/domain/LottoNo.java
@@ -3,10 +3,21 @@ package lotto.domain;
 import java.util.Objects;
 
 public class LottoNo {
+
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MIN_NUMBER = 1;
+
     private Integer number;
 
     public LottoNo(Integer number) {
         this.number = number;
+        validate();
+    }
+
+    private void validate() {
+        if (this.number > LOTTO_MAX_NUMBER || this.number < LOTTO_MIN_NUMBER) {
+            throw new IllegalArgumentException("잘 못된 로또 숫자 입니다.");
+        }
     }
 
     @Override
--- src/test/java/lotto/LottoNoTest.java
+++ src/test/java/lotto/LottoNoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoNoTest {
 
@@ -18,5 +19,13 @@ public class LottoNoTest {
     @Test
     void constructorTest() {
         assertThat(lottoNo).isEqualTo(new LottoNo(5));
+    }
+
+    @Test
+    void validateLottoNoTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            LottoNo lottoNo = new LottoNo(0);
+            LottoNo lottoNo2 = new LottoNo(55);
+        });
     }
 }
