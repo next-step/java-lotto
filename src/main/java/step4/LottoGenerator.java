02fc5ad6d709f01b011b755f743dd87439572c73
sy.kim <kauksy9022@gmail.com>
2019-11-19 11:39:58 +0900

    [Update]: 수동로또를 만드는 makeManualLottos 메소드 추가

--- src/main/java/step4/LottoGenerator.java
+++ src/main/java/step4/LottoGenerator.java
@@ -7,6 +7,7 @@ public class LottoGenerator {
     static final int WINNING_NUMBERS_LENGTH = 6;
     static final int BONUS_WINNING_VALID_NUMBER = 5; // 2등 당첨을 위한(보너스볼 일치), 일치해야하는 로또번호 갯수
     static private List<LottoNumber> standardLotto = new ArrayList<>();
+    static private Scanner scan = new Scanner(System.in);
 
     public static void generateStandardLotto() {
         for (int i = LottoNumber.LOTTO_NUMBER_MIN; i < LottoNumber.LOTTO_NUMBER_MAX + 1; i++) {
@@ -15,7 +16,7 @@ public class LottoGenerator {
     }
 
     public static Set<Lotto> purchasedLottos(int lottoCount) {
-        Set<Lotto> collectAutoLotto = new HashSet<>();
+        Set<Lotto> collectAutoLotto = new LinkedHashSet<>();
 
         while (collectAutoLotto.size() < lottoCount) {
             List<LottoNumber> shuffleLottoNumbers = generateAutoLotto();
@@ -30,5 +31,17 @@ public class LottoGenerator {
         Collections.sort(shuffleLottoNumbers);
 
         return shuffleLottoNumbers;
+    }
+
+    public static Lotto makeManualLottos() {
+        String[] inputWinningNumbers = scan.nextLine().split(",");
+        for (int i = 0; i < LottoGenerator.WINNING_NUMBERS_LENGTH; i++) {
+            inputWinningNumbers[i] = inputWinningNumbers[i].trim();
+        }
+        List<LottoNumber> lotto = LottoNumber.splitLottoNumbers(inputWinningNumbers);
+
+        List<LottoNumber> manualLottoNumbers = lotto.subList(0, 6);
+        Collections.sort(manualLottoNumbers);
+        return new Lotto(manualLottoNumbers);
     }
 }
