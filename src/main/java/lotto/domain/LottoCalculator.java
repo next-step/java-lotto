11465ce440959d390576bcbba653471011ccf8c4
Kim YoonHo <kimunoya@gmail.com>
2019-11-20 20:46:35 +0900

    refactor: 클래스명 변경

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -7,18 +7,18 @@ import java.util.Objects;
  * @version : 1.0
  * @date : 2019-11-09 02:19
  */
-public class LottoAmount {
+public class LottoCalculator {
 
     private static final int LOTTO_PRICE = 1000;
     private static final int INIT_COUNT = 0;
     private final int amount;
     private final int manualLottoCount;
 
-    private LottoAmount(int amount) {
+    private LottoCalculator(int amount) {
         this(amount, INIT_COUNT);
     }
 
-    private LottoAmount(int amount, int manualLottoCount) {
+    private LottoCalculator(int amount, int manualLottoCount) {
         if (amount < LOTTO_PRICE) {
             throw new IllegalArgumentException("로또를 구입하려면 최소 1000원이 필요합니다.");
         }
@@ -26,16 +26,16 @@ public class LottoAmount {
         this.manualLottoCount = manualLottoCount;
     }
 
-    public static final LottoAmount of(int amount) {
-        return new LottoAmount(amount);
+    public static final LottoCalculator of(int amount) {
+        return new LottoCalculator(amount);
     }
 
-    public static final LottoAmount ofManual(int amount, int manualLottoCount) {
-        return new LottoAmount(amount, manualLottoCount);
+    public static final LottoCalculator ofManual(int amount, int manualLottoCount) {
+        return new LottoCalculator(amount, manualLottoCount);
     }
 
-    public LottoAmount decrease() {
-        return new LottoAmount(amount - LOTTO_PRICE, manualLottoCount);
+    public LottoCalculator decrease() {
+        return new LottoCalculator(amount - LOTTO_PRICE, manualLottoCount);
     }
 
     public int getAutoLottoCount() {
@@ -54,7 +54,7 @@ public class LottoAmount {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        LottoAmount that = (LottoAmount) o;
+        LottoCalculator that = (LottoCalculator) o;
         return amount == that.amount &&
                 manualLottoCount == that.manualLottoCount;
     }
