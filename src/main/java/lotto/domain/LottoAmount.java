141f86aea54d1d7ae6803ef938b2820e78b784a7
Kim YoonHo <kimunoya@gmail.com>
2019-11-20 20:58:51 +0900

    feat: 로또 구매 개수를 구하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-20 20:46
+ */
+public class LottoAmount {
+
+    private static final int LOTTO_PRICE = 1000;
+    private final int amount;
+
+    public LottoAmount(int purchaseAmount) {
+        if (purchaseAmount < LOTTO_PRICE) {
+            throw new IllegalArgumentException("로또를 구입하려면 최소 1000원이 필요합니다.");
+        }
+        this.amount = purchaseAmount;
+    }
+
+    public int getLottoAmount() {
+        return this.amount / LOTTO_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoAmount that = (LottoAmount) o;
+        return amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
