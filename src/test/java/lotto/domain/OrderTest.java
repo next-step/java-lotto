d361a731a3d2ad1a5bbed29dd990546067a1e359
choihyunjin <iamchoihyunjin@gmail.com>
2019-12-22 17:50:50 +0900

    Add test code into orderclass throw wrong order exception

--- src/test/java/lotto/domain/OrderTest.java
+++ src/test/java/lotto/domain/OrderTest.java
@@ -1,20 +1,30 @@
 package lotto.domain;
 
+import lotto.common.exception.WrongOrderException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OrderTest {
+
     @Test
-    void 오더가_잘_받아지는_지_확인한다() {
+    void 오더가_잘_받아지는__확인한다() {
         Order order = new Order.Builder()
                 .payment(4000)
-                .selfNumber(Arrays.asList("2,3,4,5,1,6","4,5,6,7,2,1"))
+                .selfNumber(Arrays.asList("2,3,4,5,1,6", "4,5,6,7,2,1"))
                 .build();
 
         assertThat(order.getPayment()).isEqualTo(4000);
         assertThat(order.getSelfNumbers().size()).isEqualTo(2);
+    }
+
+    @Test
+    void 로또_가격보다_낮은_금액이_입력되면_WRONG_ORDER_EXCEPTION_발생() {
+        assertThatThrownBy(() -> new Order.Builder()
+                .payment(500)
+                .build()).isInstanceOf(WrongOrderException.class);
     }
 }
