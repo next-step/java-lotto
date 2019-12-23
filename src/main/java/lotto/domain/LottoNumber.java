0c0dbb8bc62b1b98b3531adb5747d2119134d155
choihyunjin <iamchoihyunjin@gmail.com>
2019-12-23 16:27:51 +0900

    Add code throw exception when wrong range

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,7 +1,22 @@
 package lotto.domain;
 
+import lotto.common.exception.LottoServiceException;
+
 public class LottoNumber {
-    public static LottoNumber of(int i) {
-        return null;
+
+    private int value;
+
+    private LottoNumber(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < LottoPolicy.LOTTO_MINIMUM_NUMBER || value > LottoPolicy.LOTTO_MAXIMUM_NUMBER)
+            throw new LottoServiceException(LottoError.WRONG_LOTTO_RANGE);
+    }
+
+    static LottoNumber of(int value) {
+        return new LottoNumber(value);
     }
 }
