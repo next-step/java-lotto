a4d01697529be5d9ab1e3c0869c1f5684e0835df
LeeSeungHee <palmseung@gmail.com>
2020-03-26 01:38:57 +0900

    test : Add StringAddCalculatorTest.splitTest

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,4 +2,8 @@ public class StringAddCalculator {
     public static boolean isNullOrEmpty(String input) {
         return input == null || input.isEmpty();
     }
+
+    public static String[] splitByDefault(String input){
+        return null;
+    }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,5 +9,11 @@ public class StringAddCalculatorTest {
     @NullAndEmptySource
     void isNullOrEmptyTest(String input) {
         assertThat(StringAddCalculator.isNullOrEmpty(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,4,5:3", "23,4:2", "2,24,20,1,5:5"}, delimiter = ':')
+    void splitTest(String input, int expected) {
+        assertThat(StringAddCalculator.splitByDefault(input).length).isEqualTo(expected);
     }
 }
