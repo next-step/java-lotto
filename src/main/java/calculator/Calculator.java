93e344699692874eed98b78265ccb0856e0af9d7
kwanguuuu <kwangyu326@gmail.com>
2020-03-26 00:50:09 +0900

    feat(CalculatorTest) : 테스트 통과 ("" 또는 null 입력 시)

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,8 +5,27 @@ public class Calculator {
     private final int result;
 
     public Calculator(String input) {
-        this.result = Integer.parseInt(input);
+        this.result = Integer.parseInt(checkNumber(input));
     }
+
+    private String checkNumber(String input) {
+        return isNull(isEmpty(input));
+    }
+
+    private String isNull(String input) {
+        if (input == null) {
+            return "0";
+        }
+        return input;
+    }
+
+    private String isEmpty(String input) {
+        if (input.isEmpty()) {
+            return "0";
+        }
+        return input;
+    }
+
 
     public int getResult() {
         return result;
