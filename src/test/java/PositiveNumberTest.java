35da83d3fdbf2f63d92fe31332589614fc261c67
LeeSeungHee <palmseung@gmail.com>
2020-03-26 04:18:57 +0900

    test : Add PositiveNumberTest.validateNegativeTest

--- src/test/java/PositiveNumberTest.java
+++ src/test/java/PositiveNumberTest.java
@@ -2,11 +2,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PositiveNumberTest {
     @DisplayName("값은 양의 정수를 생성자의 인자로 받아 생성된 객체는 같다.")
     @Test
     void createTest() {
         assertThat(new PositiveNumber(1)).isEqualTo(new PositiveNumber(1));
+    }
+
+    @DisplayName("음수를 인자로 받아 생성하면 런타임 예외가 발생한다.")
+    @Test
+    void validateNegativeTest() {
+        assertThatThrownBy(() -> {
+            new PositiveNumber(-1);
+        }).isInstanceOf(RuntimeException.class);
     }
 }
