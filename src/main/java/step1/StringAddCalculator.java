444b62d0c9b97efe25f8a7fef8e20d2d36bffdbb
Yeoseon Yun <devyyskr@gmail.com>
2020-03-27 00:42:42 +0900

    refactor(InputParser): 입력받은 input 값을 string 배열 형태로 split 해주는 로직을 클래스로 분리

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -1,30 +1,14 @@
 package step1;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static step1.Numbers.INIT_VALUE;
 
 public class StringAddCalculator {
-    public static int calculate(String input) {
+    public int calculate(String input) {
         if(InputChecker.isNullOrEmpty(input)) {
             return INIT_VALUE;
         }
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-
-        String[] strings;
-
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            strings= m.group(2).split(customDelimiter);
-            // 덧셈 구현
-        }
-        else {
-            strings = input.split(",|:");
-        }
-        Numbers numbers = new Numbers(strings);
-
+        Numbers numbers = new Numbers(InputParser.parseInput(input));
         return numbers.addAll();
     }
 }
