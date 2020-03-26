a26c562290b46a823aff4dec1b00008f04edc3ac
Yeoseon Yun <devyyskr@gmail.com>
2020-03-26 22:00:33 +0900

    feat(StringAddCalculator): null이나 empty값이 입력된 경우 0을 리턴해주도록 구현

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -3,7 +3,9 @@ package step1;
 public class StringAddCalculator {
     public static int splitAndSum(String input) {
         int result = 1;
-
+        if(input == null || input.isEmpty()) {
+            result = 0;
+        }
         return result;
     }
 }
