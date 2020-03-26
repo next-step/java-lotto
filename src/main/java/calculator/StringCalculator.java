4ca1b3258867e8d8e8b44dc2ee0544f49fae4622
ohtaeg <otk1090@naver.com>
2020-03-26 17:19:08 +0900

    feat (StringCalculator) : 분리된 문자열 합하는 메서드 추가 및 빈 문자열 or null 일경우 0을 반환하는 validation 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,12 +2,25 @@ package calculator;
 
 public class StringCalculator {
     private static final int ZERO = 0;
+    private static final String DEFAULT_TOKEN_DELIMITER = ",|:";
 
     public int calculate(final String input) {
-        return ZERO;
+        if (input == null || "".equals(input)) {
+            return ZERO;
+        }
+        String[] splitInput = splitInput(input);
+        return add(splitInput);
     }
 
     public String[] splitInput(final String input) {
-        return input.split(",|:");
+        return input.split(DEFAULT_TOKEN_DELIMITER);
+    }
+
+    private int add(final String[] splitInput) {
+        int sum = ZERO;
+        for (String letter : splitInput) {
+            sum += Integer.parseInt(letter);
+        }
+        return sum;
     }
 }
