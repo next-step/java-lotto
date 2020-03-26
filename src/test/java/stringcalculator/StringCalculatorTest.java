db83572b4169d82f83a4950277e880bf8ce8d646
dongchul <sah3122@naver.com>
2020-03-26 15:09:54 +0900

    test(StringCaculator): add test case

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,20 +1,49 @@
 package stringcalculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
 
+    @DisplayName("문자열 계산기 정상 작동")
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3:6"}, delimiter = ':')
+    @CsvSource(value = {"1,2,3=6", "1,2:3=6"}, delimiter = '=')
     void calculate(String expression, int expected) {
-        StringCalculator stringCalculator = new StringCalculatorTest(expression);
+        StringCalculator stringCalculator = new StringCalculator(expression);
 
         int actual = stringCalculator.calculate();
 
-        assertThat(result).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("빈 문자열 또는 null값 입력 시 0 반환")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void calculateFailByInvalidString(String expression) {
+        StringCalculator stringCalculator = new StringCalculator(expression);
+
+        int actual = stringCalculator.calculate();
+
+        assertThat(actual).isEqualTo(0);
+    }
+
+    @DisplayName("숫자 이외의 값이 전달 될 경우 에러 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,adf,2", "adb,1"})
+    void calculateFailByInvalidString(String expression) {
+        assertThatIllegalArgumentException(new StringCalculator(expression));
+    }
+
+    @DisplayName("음수를 전달하는 경우 에러 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,-1", "-1,2"})
+    void calculateFailByNegative(String expression) {
+        assertThatIllegalArgumentException(new StringCalculator(expression));
     }
 }
