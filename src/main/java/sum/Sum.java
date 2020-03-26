270936fbbb40c476cb7b59ce32694b31e4394db1
teahyuk <jtaeh57@gmail.com>
2020-03-26 21:42:34 +0900

    feat(Sum) : 구분자 기능
    구분자 기능 추가 :
      - 구분자 테스트 추가
      - 구분자 기능 추가

--- src/main/java/sum/Sum.java
+++ src/main/java/sum/Sum.java
@@ -1,10 +1,22 @@
 package sum;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Sum {
 	public static int sum(String sumParam) {
 		if(sumParam == null){
 			return 0;
 		}
+
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(sumParam);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			String[] tokens= m.group(2).split(customDelimiter);
+
+			return new ValueList(tokens).sum();
+		}
+
 		ValueList values = new ValueList(sumParam.split("[,:]"));
 		return values.sum();
 	}
--- src/test/java/sum/SumTest.java
+++ src/test/java/sum/SumTest.java
@@ -1,6 +1,7 @@
 package sum;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
@@ -28,7 +29,7 @@ public class SumTest {
 	@NullSource
 	@EmptySource
 	@DisplayName("빈 값 처리 테스트")
-	void nullOrEmptyTest(String param){
+	void nullOrEmptyTest(String param) {
 		int sum = Sum.sum(param);
 		assertThat(sum)
 				.isEqualTo(0);
@@ -38,7 +39,15 @@ public class SumTest {
 	@DisplayName("유효성 검사 테스트")
 	@ValueSource(strings = {"-5,0", "k:8", "null"})
 	void expectRunTimeException(String invalidParam) {
-		assertThatThrownBy(()->Sum.sum(invalidParam))
+		assertThatThrownBy(() -> Sum.sum(invalidParam))
 				.isInstanceOf(RuntimeException.class);
+	}
+
+	@Test
+	@DisplayName("구분자 기능 테스트")
+	void delimiterTest(){
+		int sum = Sum.sum("//;\n1;2;;3");
+		assertThat(sum)
+				.isEqualTo(6);
 	}
 }
