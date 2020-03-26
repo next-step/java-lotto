80a454d9382cb37382800aeaba9a7ad977346254
jwee0330 <jwee0330@gmail.com>
2020-03-26 23:52:22 +0900

    feat(stringAccumulator): 표현식을 입력 받아 새로운 StringAccumulator 를 생성할 수 있다

---
+++ src/main/java/stringaccumulator/StringAccumulator.java
@@ -0,0 +1,13 @@
+package stringaccumulator;
+
+public class StringAccumulator {
+    private final String expression;
+
+    public StringAccumulator(String expression) {
+        this.expression = expression;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+}
--- src/test/java/stringaccumulator/StringAccumulatorTest.java
+++ src/test/java/stringaccumulator/StringAccumulatorTest.java
@@ -1,4 +1,21 @@
 package stringaccumulator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringAccumulatorTest {
+
+    @DisplayName("표현식을 입력 받아 새로운 StringAccumulator 를 생성할 수 있다")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "1,2", "1,2,3", "1,2:3"})
+    public void newStringAccumInstanceTest(String expression) {
+        StringAccumulator stringAccumulator = new StringAccumulator(expression);
+
+        String takeNewOne = stringAccumulator.getExpression();
+
+        assertThat(takeNewOne).isEqualTo(expression);
+    }
 }
