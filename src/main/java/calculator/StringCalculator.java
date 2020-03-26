0107549face43e1de747151a3b23ec45c9486595
ohtaeg <otk1090@naver.com>
2020-03-26 17:44:43 +0900

    feat (StringCalculator) : 숫자 이외의 값 or 음수일경우 RuntimeException 을 발생하도록 코드 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,9 +3,10 @@ package calculator;
 public class StringCalculator {
     private static final int ZERO = 0;
     private static final String DEFAULT_TOKEN_DELIMITER = ",|:";
+    private static final String EMPTY_STRING = "";
 
     public int calculate(final String input) {
-        if (input == null || "".equals(input)) {
+        if (input == null || EMPTY_STRING.equals(input)) {
             return ZERO;
         }
         String[] splitInput = splitInput(input);
@@ -18,8 +19,16 @@ public class StringCalculator {
 
     private int add(final String[] splitInput) {
         int sum = ZERO;
-        for (String letter : splitInput) {
-            sum += Integer.parseInt(letter);
+        try {
+            for (String letter : splitInput) {
+                int parseNumber = Integer.parseInt(letter);
+                if (parseNumber < 0) {
+                    throw new RuntimeException("음수 입니다.");
+                }
+                sum += Integer.parseInt(letter);
+            }
+        } catch (Exception e) {
+            throw e;
         }
         return sum;
     }
