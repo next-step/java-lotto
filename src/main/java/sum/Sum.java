7251667153e7dd28648863a8022ec8491f2e38be
teahyuk <jtaeh57@gmail.com>
2020-03-26 21:54:10 +0900

    refactor(Sum) : 구분자 기능 리팩토링
    - 메소드 분리 및 Sum 클래스 정리

--- src/main/java/sum/Sum.java
+++ src/main/java/sum/Sum.java
@@ -4,20 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Sum {
+	private static final Pattern delimiterPattern = Pattern.compile("//(.)\n(.*)");
+	private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+
 	public static int sum(String sumParam) {
-		if(sumParam == null){
+		if (sumParam == null) {
 			return 0;
 		}
 
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(sumParam);
+		return new ValueList(parseValueList(sumParam)).sum();
+	}
+
+	private static String[] parseValueList(String sumParam) {
+		Matcher m = delimiterPattern.matcher(sumParam);
 		if (m.find()) {
 			String customDelimiter = m.group(1);
-			String[] tokens= m.group(2).split(customDelimiter);
-
-			return new ValueList(tokens).sum();
+			return m.group(2).split(customDelimiter);
 		}
 
-		ValueList values = new ValueList(sumParam.split("[,:]"));
-		return values.sum();
+		return sumParam.split(DEFAULT_DELIMITER_REGEX);
 	}
 }
