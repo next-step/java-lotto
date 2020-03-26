79ea4b6d296fdc5aec21074ad9a6218a79792e53
ohtaeg <otk1090@naver.com>
2020-03-26 16:53:12 +0900

    test (StringCalculator) : 컴마기준으로 문자열 분리 테스트 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,6 +3,8 @@ package calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,5 +33,16 @@ class StringCalculatorTest {
         int actual = stringCalculator.calculate(null);
 
         assertThat(actual).isZero();
+    }
+
+    @DisplayName("컴마(,) 기준으로 문자열을 분리할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2", "1,2,3", "1,2,3,4"})
+    void splitFromComma(String input) {
+        final String[] expect = input.split(",");
+
+        String[] splitInput = stringCalculator.splitInput(input);
+
+        assertThat(splitInput).isEqualTo(expect);
     }
 }
