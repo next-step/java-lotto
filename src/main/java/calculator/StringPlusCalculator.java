c780086daf56edb54bcc04d04efd3ecc709273f3
5minho <5minhominho@gmail.com>
2020-03-27 07:29:51 +0900

    feature : 숫자 이외의 값 또는 음수를 전달하면 RuntimeException 이 발생한다.

--- src/main/java/calculator/StringPlusCalculator.java
+++ src/main/java/calculator/StringPlusCalculator.java
@@ -2,11 +2,12 @@ package calculator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class StringPlusCalculator {
 
-    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private StringPlusCalculator() {}
+
+    private static final String DEFAULT_DELIMITERS = "[,:]";
     private static final String CUSTOM_DELIMITER_REGEX = "//(.)\\\\n(.*)";
 
     public static int calculate(String input) {
@@ -19,23 +20,15 @@ public class StringPlusCalculator {
 
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
-            return sum(split(matcher.group(2), customDelimiter));
+            String customInput = matcher.group(2);
+
+            return new NatualNumPlusFormula(customInput.split(customDelimiter)).sum();
         }
 
-        return sum(split(input, DEFAULT_DELIMITER_REGEX));
+        return new NatualNumPlusFormula(input.split(DEFAULT_DELIMITERS)).sum();
     }
 
     private static boolean isEmpty(String input) {
         return input == null || input.isEmpty();
-    }
-
-    private static int sum(String[] tokens) {
-        return Stream.of(tokens)
-                .mapToInt(Integer::parseInt)
-                .sum();
-    }
-
-    private static String[] split(String input, String regex) {
-        return input.split(regex);
     }
 }
