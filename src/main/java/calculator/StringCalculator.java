605ce337a6f77509c60d9677a7ab2404289d2662
ohtaeg <otk1090@naver.com>
2020-03-27 02:50:36 +0900

    refactor (StringCalculator) : 숫자 원시 값을 Number 값 객체로 리팩토링

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,43 +2,24 @@ package calculator;
 
 public class StringCalculator {
     private static final String EMPTY_STRING = "";
-    private static final int ZERO = 0;
 
     public int calculate(final String input) {
         if (isNullOrEmpty(input)) {
-            return ZERO;
+            return Number.ZERO;
         }
         String[] splitInput = Delimiter.split(input);
-        return add(splitInput);
+        return add(splitInput).getValue();
     }
 
     private boolean isNullOrEmpty(String input) {
         return input == null || EMPTY_STRING.equals(input.trim());
     }
 
-    private int add(final String[] splitInput) {
-        int sum = ZERO;
+    private Number add(final String[] splitInput) {
+        Number sum = Number.valueOf("0");
         for (String letter : splitInput) {
-            int parseNumber = parse(letter);
-            checkNegative(parseNumber);
-            sum += parseNumber;
+            sum = sum.add(Number.valueOf(letter));
         }
         return sum;
-    }
-
-    private int parse(String letter) {
-        int parseInt;
-        try {
-            parseInt = Integer.parseInt(letter);
-        } catch (NumberFormatException ne) {
-            throw new RuntimeException("숫자로 변환 할 수 없습니다.");
-        }
-        return parseInt;
-    }
-
-    private void checkNegative(int number) {
-        if (number < ZERO) {
-            throw new RuntimeException("음수 입니다.");
-        }
     }
 }
