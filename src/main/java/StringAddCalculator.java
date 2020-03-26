0b11738ffd6b94260844c3bca6151128bcdfb795
feel-coding-republic <feel.coding.republic@gmail.com>
2020-03-26 22:26:21 +0900

    feat:빈값로직추가

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,7 @@
+public class StringAddCalculator {
+    public static int splitAndSum(String inputData) {
+        int result = 0;
+
+        return result;
+    }
+}
---
+++ src/test/java/StringAddCalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+
+    @Test
+    public void splitAndSum_null_빈값테스트() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+}
