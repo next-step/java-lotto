f149b95dd7ce7cf27b3f095cf874bbdf0ff82725
teahyuk <jtaeh57@gmail.com>
2020-03-26 20:11:48 +0900

    feat(Sum) : Step1 Sum Exception 개발
    Exception 체크로직 추가 개발 : 
      - RuntimeExcpetion 체크 TC 추가.
      - TC에 맞게 validationCheck 로직 추가.

--- src/main/java/sum/Sum.java
+++ src/main/java/sum/Sum.java
@@ -1,11 +1,27 @@
 package sum;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Sum {
 	public static int sum(String sumParam) {
-		return Arrays.stream(sumParam.split("[,:]"))
+		List<String> values = Arrays.asList(sumParam.split("[,:]",-1));
+		validationCheck(values);
+		return values.stream()
 				.mapToInt(Integer::parseInt)
 				.sum();
+	}
+
+	private static void validationCheck(List<String> values) {
+		for (String value : values) {
+			if (value == null ||
+					value.isEmpty()) {
+				throw new IllegalArgumentException("value is Empty");
+			}
+
+			if(Integer.parseInt(value) < 0){
+				throw new IllegalArgumentException("value is negative");
+			}
+		}
 	}
 }
--- src/test/java/sum/SumTest.java
+++ src/test/java/sum/SumTest.java
@@ -3,8 +3,9 @@ package sum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class SumTest {
 
@@ -16,5 +17,13 @@ public class SumTest {
 
 		assertThat(sum)
 				.isEqualTo(answer);
+	}
+
+	@ParameterizedTest
+	@DisplayName("유효성 검사 테스트")
+	@ValueSource(strings = {"-5,0", "k:8", "0,,", "null", " : ,2"})
+	void expectRunTimeException(String invalidParam) {
+		assertThatThrownBy(()->Sum.sum(invalidParam))
+				.isInstanceOf(RuntimeException.class);
 	}
 }
