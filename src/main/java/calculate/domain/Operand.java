4542328e5d0593aa1a8ea7fea820cda0d8d6dc17
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-26 23:34:50 +0900

    feat(Operand) : 피연산자 양의 정수 값 테스트
    - 피연산자가 양의 정수 값 가지도록함

--- src/main/java/calculate/domain/Operand.java
+++ src/main/java/calculate/domain/Operand.java
@@ -1,11 +1,16 @@
 package calculate.domain;
 
+import calculate.exception.NotPositiveIntegerException;
+
 import java.util.Objects;
 
 public class Operand {
     private int i;
 
     public Operand(int i) {
+        if (i < 0) {
+            throw new NotPositiveIntegerException("피연산자는 양의 정수만 입력 가능 합니다.");
+        }
         this.i = i;
     }
 
--- src/test/java/calculate/domain/OperandTest.java
+++ src/test/java/calculate/domain/OperandTest.java
@@ -1,11 +1,17 @@
 package calculate.domain;
 
+import calculate.exception.NotPositiveIntegerException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OperandTest {
 
+    @DisplayName("객체 생성 및 비교")
     @Test
     public void operand() throws Exception {
         //given
@@ -14,5 +20,14 @@ public class OperandTest {
 
         //then
         assertThat(operand1.equals(operand2)).isTrue();
+    }
+
+    @DisplayName("피연산자가 양의 정수인지 - fail: 음수 입력")
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -1})
+    public void validateInteger_음수(int num) throws Exception {
+        assertThatThrownBy(
+                () -> new Operand(num)
+        ).isInstanceOf(NotPositiveIntegerException.class);
     }
 }
