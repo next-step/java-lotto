34e87e8b16a2dc1cf2abb1085eb278284101e018
feel-coding-republic <feel.coding.republic@gmail.com>
2020-03-26 22:39:27 +0900

    feat:음수, 커스텀구분자

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,15 +1,38 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String inputData) {
         if (inputData == null || inputData.isEmpty()) {
             return 0;
         }
-
-        String[] splitInputData = inputData.split(",|:");
         int result = 0;
 
-        for (int i = 0; i < splitInputData.length; i++) {
-            result = result + Integer.parseInt(splitInputData[i]);
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputData);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] splitInputData = m.group(2).split(customDelimiter);
+
+            for (int i = 0; i < splitInputData.length; i++) {
+                if(Integer.parseInt(splitInputData[i]) <0){
+                    throw new RuntimeException();
+                }
+
+                result = result + Integer.parseInt(splitInputData[i]);
+            }
+        }else{
+
+            String[] splitInputData = inputData.split(",|:");
+
+            for (int i = 0; i < splitInputData.length; i++) {
+                if(Integer.parseInt(splitInputData[i]) <0){
+                    throw new RuntimeException();
+                }
+                result = result + Integer.parseInt(splitInputData[i]);
+            }
         }
+
         return result;
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -32,5 +32,16 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(6);
     }
 
+    @Test
+    public void splitAndSum_커스텀구분자() {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_음수테스트() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3")).isInstanceOf(RuntimeException.class);
+
+    }
 
 }
