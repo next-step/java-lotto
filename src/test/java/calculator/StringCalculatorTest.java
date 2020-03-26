a9ce8b0315d8105a19f8a644c8aee2532e50897a
ohtaeg <otk1090@naver.com>
2020-03-26 16:12:09 +0900

    test (StringCalculator) : 빈 문자열 입력시 정수 0 반환하는 테스트 코드 추가

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @DisplayName("빈 문자열을 입력할 경우 0을 반환해야 한다.")
+    @Test
+    void returnZero() {
+        final String emptyString = "";
+
+        int actual = stringCalculator.calculate(emptyString);
+
+        assertThat(actual).isZero();
+    }
+}
