81220cec355b8073e5e1229c29544cfd77a23d2f
Yeoseon Yun <devyyskr@gmail.com>
2020-03-26 23:02:57 +0900

    refactor(StringAddCalculator, ValidationUtils): 검증 로직 분리

--- src/main/java/step1/ValidationUtils.java
+++ src/main/java/step1/ValidationUtils.java
@@ -21,4 +21,11 @@ public class ValidationUtils {
         }
         return false;
     }
+
+    public static boolean isOnlyNumber(String input) {
+        if(input.matches("^[0-9]*$")) {
+            return true;
+        }
+        return false;
+    }
 }
