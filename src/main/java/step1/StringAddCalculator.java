343726afeadc9bcb94530af65d2253a448fc4769
Yeoseon Yun <devyyskr@gmail.com>
2020-03-26 21:58:42 +0900

    revert(StringAddCalculator): 요구사항 이해 부족. 테스트 실패 메소드 삭제, 기존 메소드 실패하도록 프로덕션 코드 수정

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -2,7 +2,7 @@ package step1;
 
 public class StringAddCalculator {
     public static int splitAndSum(String input) {
-        int result = 0;
+        int result = 1;
 
         return result;
     }
