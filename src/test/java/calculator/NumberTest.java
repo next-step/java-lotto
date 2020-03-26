0d3335125bcb076d9ddaac1d9ca5b7ef7156fcf4
ohtaeg <otk1090@naver.com>
2020-03-27 02:05:01 +0900

    test (Number) : 정적 팩토리 메서드로 변경 후 테스트 코드 변경

--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -4,16 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class NumberTest {
 
     @DisplayName("숫자 값 객체를 생성한다.")
     @Test
     void create() {
-        Number number = new Number("1");
+        Number number = Number.valueOf("1");
 
-        Number expect = new Number("1");
+        Number expect = Number.valueOf("1");
 
         assertThat(number).isEqualTo(expect);
     }
