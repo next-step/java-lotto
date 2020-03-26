d976d96771f2cf781deccf38052c32417e14e052
Yeoseon Yun <devyyskr@gmail.com>
2020-03-26 21:42:50 +0900

    test(StringAddCalculatorTest): null 또는 빈문자를 받았을 때 0을 리턴해주는 테스트메소드 추가

--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -1,4 +1,16 @@
 package step1;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringAddCalculatorTest {
+    @Test
+    void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
 }
