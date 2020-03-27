ca5f233fecf319327b1f51336b6b66d4c9c03cd9
etff <wish2rich@gmail.com>
2020-03-27 09:39:04 +0900

    feature(calculator): add null or empty test

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,4 +1,10 @@
 package calculator;
 
 public class StringAddCalculator {
+    private final String inputText;
+
+    public StringAddCalculator(String inputText) {
+        this.inputText = inputText;
+    }
+
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -2,13 +2,28 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class StringAddCalculatorTest {
-
     @DisplayName(value = "계산기를 생성할 수 있다")
-    @Test
-    public void canCreate() {
-        new StringAddCalculatorTest();
+    @ParameterizedTest
+    @ValueSource(strings = {"1+2"})
+    public void canCreate(String inputText) {
+        new StringAddCalculator(inputText);
     }
+
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 RuntimeException 예외 처리를 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String inputText) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> new StringAddCalculator(inputText));
+    }
+
+
 
 }
