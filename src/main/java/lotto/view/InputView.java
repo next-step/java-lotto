f37cf33c6fffea7c7d189b798bfea7b692f71765
ohtaeg <otk1090@naver.com>
2020-03-28 01:22:31 +0900

    refactor (InputView) : 구매금액을 콘솔에서 입력받도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int inputPrice() {
-        return 1000;
+        return SCANNER.nextInt();
     }
 }
