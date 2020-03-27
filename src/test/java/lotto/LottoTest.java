7e6712c0dfc1d123f8b7361d8432b41e297c91db
LeeSeungHee <palmseung@gmail.com>
2020-03-28 03:00:01 +0900

    test (lotto) : Add LottoTest.validateSizeTest

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class LottoTest {
+    @DisplayName("6개의 숫자로 구성되지 않은 숫자 리스트를 입력받으면 예외를 반환한다.")
+    @Test
+    void validateSizeTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Lotto(Arrays.asList(1, 2, 3, 4, 5));
+        });
+    }
+}
