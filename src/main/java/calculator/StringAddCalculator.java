9b6491690a03ba68855e439ab2618405563f6f98
etff <wish2rich@gmail.com>
2020-03-27 09:43:33 +0900

    refactor(calclulator): add empty or null check method

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,7 +4,14 @@ public class StringAddCalculator {
     private final String inputText;
 
     public StringAddCalculator(String inputText) {
-        this.inputText = inputText;
+        this.inputText = checkNullOrEmpty(inputText);
+    }
+
+    private String checkNullOrEmpty(String inputText) {
+        if (inputText == null || inputText.trim().isEmpty()) {
+            throw new IllegalArgumentException("공백이나 null이 입력되면 안됩니다.");
+        }
+        return inputText;
     }
 
 }
