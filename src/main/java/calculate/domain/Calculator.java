e6e7712025650e8080ee74f860b112ce1ac95754
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-27 14:39:31 +0900

    refactor(Calculator) : 매개변수 이름 수정
    - 생성자 매개변수 이름 수정

--- src/main/java/calculate/domain/Calculator.java
+++ src/main/java/calculate/domain/Calculator.java
@@ -4,8 +4,8 @@ public class Calculator {
     private Operands operands;
     private Delimiter delimiter;
 
-    public Calculator(String s, String s1) {
-        DelimiterStrategy delimiterStrategy = new DelimiterStrategy(s, s1);
+    public Calculator(String operandString, String delimiter) {
+        DelimiterStrategy delimiterStrategy = new DelimiterStrategy(operandString, delimiter);
         this.operands = delimiterStrategy.getOperands();
         this.delimiter = delimiterStrategy.getDelimiter();
     }
