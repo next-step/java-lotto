6395b35e260fe3eaaf57401b814048866c9d8d4e
LeeSeungHee <palmseung@gmail.com>
2020-03-28 00:57:11 +0900

    feat (stringConverter) : Add StringConverter.validateNonNumber

--- src/main/java/lotto/StringConverter.java
+++ src/main/java/lotto/StringConverter.java
@@ -3,6 +3,14 @@ package lotto;
 public class StringConverter {
 
     public static Money convertStringToMoney(String input) {
-        return new Money(Long.parseLong(input));
+        return new Money(validateNonNumber(input));
+    }
+
+    private static Long validateNonNumber(String input) {
+        try {
+            return Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
     }
 }
