f2852b41582fed2813e81856c64774f45d87c172
ohtaeg <otk1090@naver.com>
2020-03-28 02:47:33 +0900

    feat (OutputView) : 복권 갯수를 출력하는 결과 창 객체 구현 - 현재 static 메서드만 필요하여 객체 생성 방지를 위해 생성자 접근제어자를 private로 설정

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printLotteryCount(int count) {
+        System.out.println(count + "개를 구매했습니다.");
+    }
+}
