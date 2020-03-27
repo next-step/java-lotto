26a50a710ec525bfd2654e1967987627fbf4635f
LeeSeungHee <palmseung@gmail.com>
2020-03-28 03:10:01 +0900

    feat (lotto) : Add Lotto.validateDuplication

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
@@ -10,12 +11,22 @@ public class Lotto {
 
     public Lotto(List<Integer> lottoNumbers) {
         validateSize(lottoNumbers);
+        validateDuplication(lottoNumbers);
         this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
     }
 
     private void validateSize(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또는 6개의 숫자로 구성되어야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> lottoNumbers) {
+        int sizeOfSet = lottoNumbers.stream()
+                .collect(Collectors.toSet())
+                .size();
+        if (lottoNumbers.size() > sizeOfSet) {
+            throw new IllegalArgumentException("로또는 중복되지 않은 숫자로 구성되어야 합니다.");
         }
     }
 }
