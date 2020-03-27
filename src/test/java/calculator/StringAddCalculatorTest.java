6b9110a559d0a664a6f8a02aa1e237748533161d
yohan <yohan.lee@uneedcomms.com>
2020-03-28 03:06:34 +0900

    test: StringAddCalculatorTest

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,6 +1,9 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +16,26 @@ public class StringAddCalculatorTest {
 
         result = StringAddCalculator.calculate(null);
         assertThat(result).isZero();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "5", "22"})
+    void calculate_inputOneNumber_returnOrigin(String input) {
+        int result = StringAddCalculator.calculate(input);
+        assertThat(result).isEqualTo(input);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3/6", "1:5,4/10", "3,5:9/17"}, delimiter = '/')
+    void calculate_inputDelimiters_returnSum(String input, int expected) {
+        int result = StringAddCalculator.calculate(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"//;\\n1;2;3/6, //-\\n4-9-3/16"}, delimiter = '/')
+    void calculate_inputCustomDelimiter_returnSum(String input, int expected) {
+        int result = StringAddCalculator.calculate(input);
+        assertThat(result).isEqualTo(expected);
     }
 }
