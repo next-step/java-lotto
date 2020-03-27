77bfccf54f1b4f94c2a494d492de88e630ff32ab
jinwoo <jwlee@qualson.com>
2020-03-28 01:54:11 +0900

    feat(StringAddCalculator): validate String null or zero function

--- src/main/java/Calculator/StringAddCalculator.java
+++ src/main/java/Calculator/StringAddCalculator.java
@@ -1,4 +1,11 @@
 package Calculator;
 
 public class StringAddCalculator {
+    public static int splitAndSum(final String inputString) {
+        return validateInputString(inputString) ? Integer.parseInt(inputString) : 0;
+    }
+
+    private static boolean validateInputString(final String inputString) {
+        return inputString != null && !inputString.isEmpty();
+    }
 }
--- src/test/java/Calculator/StringAddCalculatorTest.java
+++ src/test/java/Calculator/StringAddCalculatorTest.java
@@ -1,4 +1,18 @@
 package Calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @DisplayName("빈 문자열 혹은 null 을 집어넣었을 때 0을 반환한다.")
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자(final String str) {
+        int result = StringAddCalculator.splitAndSum(str);
+        assertThat(result).isEqualTo(0);
+    }
 }
