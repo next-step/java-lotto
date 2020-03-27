fff09586eebb2e1c215a2dc031bced463eeee662
ohtaeg <otk1090@naver.com>
2020-03-28 04:10:15 +0900

    refactor (Lotto) : 테스트 통과를 위해 로또 복권 객체 임시로 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,8 +2,12 @@ package lotto.domain;
 
 import lotto.dto.Price;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lotto {
     public LotteryTickets buy(final Price price) {
-        return new LotteryTickets(price);
+        List<Lottery> lotteryList = Arrays.asList(new Lottery(), new Lottery());
+        return new LotteryTickets(lotteryList);
     }
 }
