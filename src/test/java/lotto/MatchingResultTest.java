460814b7e73ce038d21d137d7570fc9c4402f352
LeeSeungHee <palmseung@gmail.com>
2020-03-28 05:21:30 +0900

    test (matchingResult) : Add MatchingResultTest

---
+++ src/test/java/lotto/MatchingResultTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MatchingResultTest {
+    @DisplayName("당첨번호와 3개가 같은 로또 갯수를 주면, 받을 수 있는 상금을 Money 객체로 반환한다.")
+    @Test
+    void calculatePrizeMoneyTestWhenThreeMatching() {
+        assertThat(MatchingResult.THREE.calculatePrizeMoney(1))
+                .isEqualTo(new Money(5000l));
+    }
+
+    @DisplayName("당첨번호와 4개가 같은 로또 갯수를 주면, 받을 수 있는 상금을 Money 객체로 반환한다.")
+    @Test
+    void calculatePrizeMoneyTestWhenFourMatching() {
+        assertThat(MatchingResult.FOUR.calculatePrizeMoney(2))
+                .isEqualTo(new Money(100000l));
+    }
+
+    @DisplayName("당첨번호와 5개가 같은 로또 갯수를 주면, 받을 수 있는 상금을 Money 객체로 반환한다.")
+    @Test
+    void calculatePrizeMoneyTestWhenFiveMatching() {
+        assertThat(MatchingResult.FIVE.calculatePrizeMoney(3))
+                .isEqualTo(new Money(4500000l));
+    }
+
+    @DisplayName("당첨번호와 6개가 같은 로또 갯수를 주면, 받을 수 있는 상금을 Money 객체로 반환한다.")
+    @Test
+    void calculatePrizeMoneyTestWhenSixMatching() {
+        assertThat(MatchingResult.SIX.calculatePrizeMoney(5))
+                .isEqualTo(new Money(10000000000l));
+    }
+}
