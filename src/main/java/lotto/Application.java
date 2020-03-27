0d578f90568a56dd0142a39d00cadfc8e19a5bf4
ohtaeg <otk1090@naver.com>
2020-03-28 01:53:12 +0900

    refactor (Application) : 정수 반환을 Price 객체로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,11 @@
 package lotto;
 
+import lotto.dto.Price;
 import lotto.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        int price = inputView.inputPrice();
+        Price price = inputView.inputPrice();
     }
 }
