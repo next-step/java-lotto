d2078bcaad6ec64f2dced1b7db01fb9a200ed40a
parkeeseul <eeseul.p@gmail.com>
2020-03-27 20:37:36 +0900

    refactor(ResultView) : String 상수화

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -9,6 +9,11 @@ public class ResultView {
     private static final String LOTTO_COUNT = "%d개를 구매했습니다.";
     private static final String WINNING_CHANCE = "당첨 통계";
     private static final String LINE = "---------";
+    private static final String RANK1 = "6개 일치(2000000000)원 - %d개";
+    private static final String RANK2 = "5개 일치(1500000)원 - %d개";
+    private static final String RANK3 = "4개 일치(50000)원 - %d개";
+    private static final String RANK4 = "3개 일치(5000)원 - %d개";
+    private static final String PERCENTAGE = "총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
     public static void printLottoCount(int count) {
         System.out.println(String.format(LOTTO_COUNT, count));
@@ -21,10 +26,10 @@ public class ResultView {
     public static void printChanceOfWinning(LottoCalculator lottoCalculator, int investment) {
         System.out.println(WINNING_CHANCE);
         System.out.println(LINE);
-        System.out.println(String.format("3개 일치(5000)원 - %d개", lottoCalculator.getRank4Count()));
-        System.out.println(String.format("4개 일치(50000)원 - %d개", lottoCalculator.getRank3Count()));
-        System.out.println(String.format("5개 일치(1500000)원 - %d개", lottoCalculator.getRank2Count()));
-        System.out.println(String.format("6개 일치(2000000000)원 - %d개", lottoCalculator.getRank1Count()));
-        System.out.println(String.format("총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", lottoCalculator.getWinningPercentage(investment)));
+        System.out.println(String.format(RANK4, lottoCalculator.getRank4Count()));
+        System.out.println(String.format(RANK3, lottoCalculator.getRank3Count()));
+        System.out.println(String.format(RANK2, lottoCalculator.getRank2Count()));
+        System.out.println(String.format(RANK1, lottoCalculator.getRank1Count()));
+        System.out.println(String.format(PERCENTAGE, lottoCalculator.getWinningPercentage(investment)));
     }
 }
