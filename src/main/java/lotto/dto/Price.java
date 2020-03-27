99f529d518d89e583bf23799dce1adcef9e52bcc
ohtaeg <otk1090@naver.com>
2020-03-28 01:49:29 +0900

    feat (Price) : 입력받은 가격을 갯수로도 출력하기 위해 객체 내부에서 가격을 갯수로 반환하기 위해 가격 객체 구현

---
+++ src/main/java/lotto/dto/Price.java
@@ -0,0 +1,24 @@
+package lotto.dto;
+
+import java.util.Objects;
+
+public class Price {
+    private int price;
+
+    public Price(final int i) {
+        this.price = i;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Price price1 = (Price) o;
+        return price == price1.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+}
