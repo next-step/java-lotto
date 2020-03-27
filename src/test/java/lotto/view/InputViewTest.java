d861a41f797f30807802cc3b6d3713e1e8a52003
ohtaeg <otk1090@naver.com>
2020-03-28 01:29:13 +0900

    test (InputView) : 스트림을 받도록 테스트 코드 변경

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,18 +1,28 @@
 package lotto.view;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
 
+    private InputView inputView;
 
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView(new ByteArrayInputStream("1000".getBytes()));
+    }
+
+    @DisplayName("구입 금액을 입력받을 수 있다.")
     @Test
     void insertPrice() {
         final int expect = 1000;
 
-        final int actual = InputView.inputPrice();
+        final int actual = inputView.inputPrice();
 
         assertThat(actual).isEqualTo(expect);
     }
