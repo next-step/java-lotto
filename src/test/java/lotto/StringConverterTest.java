c102ff234158a3087964628645546281d3a8606b
LeeSeungHee <palmseung@gmail.com>
2020-03-28 00:50:05 +0900

    fix (stringConverter) : Delete all tests in StringConverterTest
    Add StringConverterTest.convertStringToMoneyTest

--- src/test/java/lotto/StringConverterTest.java
+++ src/test/java/lotto/StringConverterTest.java
@@ -3,29 +3,15 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringConverterTest {
-    @DisplayName("입력받은 문자열을 인자로 주면, PositiveNumber 타입으로 변환된 객체를 반환한다.")
+    @DisplayName("문자열 입력값을 인자로 주면, long으로 변환된 멤버변수를 통해 생성된 Money객체를 반환한다.")
     @ParameterizedTest
-    @CsvSource(value = {"13000:13000", "1000:1000", "1500:1500"}, delimiter = ':')
-    void convertStringToPositiveNumberTest(String input, int expected) {
-        //when
-        PositiveNumber positiveNumber = StringConverter.convertStringToPositiveNumber(input);
-
-        //then
-        assertThat(positiveNumber).isEqualTo(new PositiveNumber(expected));
-    }
-
-    @DisplayName("입력받은 값이 숫자가 아니면 IllegalArgumentException 을 반환한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"숫자가아닌데?", "Money", "!!!!"})
-    void validateNonNumberTest(String input) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            StringConverter.convertStringToPositiveNumber(input);
-        });
+    @CsvSource(value = {"1000:1000", "5000:5000", "1000000:1000000"}, delimiter = ':')
+    void convertStringToMoneyTest(String input, long expected) {
+        assertThat(StringConverter.convertStringToMoney(input))
+                .isEqualTo(new Money(expected));
     }
 }
