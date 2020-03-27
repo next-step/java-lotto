d7462a6afad05442b78a818380e57772621ca942
LeeSeungHee <palmseung@gmail.com>
2020-03-27 23:49:47 +0900

    feat (positiveNumber) : Add PositiveNumber, PositiveNumbers
    * Add PositiveNumberTest, PositiveNumbersTest

---
+++ src/main/java/lotto/PositiveNumber.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.util.Objects;
+
+public class PositiveNumber {
+    private int number;
+
+    public PositiveNumber(int number) {
+        validateNegative(number);
+        this.number = number;
+    }
+
+    public int add(int number){
+        return this.number + number;
+    }
+
+    private int validateNegative(int number) {
+        if (isNegative(number)) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+        return number;
+    }
+
+    private boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositiveNumber that = (PositiveNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
---
+++ src/main/java/lotto/PositiveNumbers.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import stringaddcalculator.PositiveNumber;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PositiveNumbers {
+    private List<PositiveNumber> positiveNumbers;
+
+    public PositiveNumbers(List<PositiveNumber> positiveNumbers) {
+        this.positiveNumbers = Collections.unmodifiableList(positiveNumbers);
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (PositiveNumber positiveNumber : positiveNumbers) {
+            sum = positiveNumber.add(sum);
+        }
+        return sum;
+    }
+}
