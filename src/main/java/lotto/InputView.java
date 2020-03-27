6d2a743a3411c6fa3c5faf6bdf63fa5cba1fc078
LeeSeungHee <palmseung@gmail.com>
2020-03-27 23:40:49 +0900

    feat (inputView) : Add InputView.validateNullOrEmpty

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,6 +4,13 @@ public class InputView {
     private String input;
 
     public InputView(String input) {
+        validateNullOrEmpty(input);
         this.input = input;
+    }
+
+    private void validateNullOrEmpty(String input){
+        if(input == null || input.isEmpty()){
+            throw new IllegalArgumentException("null 또는 공백은 입력할 수 없습니다.");
+        }
     }
 }
