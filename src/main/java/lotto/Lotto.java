6685454b2187963951a1cbea7347177a4b7cad8f
LeeSeungHee <palmseung@gmail.com>
2020-03-28 03:04:18 +0900

    feat (lotto) : Add Lotto.validateSize

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,9 +4,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+
     private List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        validateSize(lottoNumbers);
         this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
+    }
+
+    private void validateSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또는 6개의 숫자로 구성되어야 합니다.");
+        }
     }
 }
