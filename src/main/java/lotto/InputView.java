2e812d346b1adbe775b094b2b1c79d74fc46ab30
LeeSeungHee <palmseung@gmail.com>
2020-03-27 23:38:47 +0900

    test (inputView) : Add InputViewTest.validateNullOrEmptyTest

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class InputView {
+    private String input;
+
+    public InputView(String input) {
+        this.input = input;
+    }
+}
---
+++ src/test/java/lotto/InputViewTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+public class InputViewTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validateNullOrEmptyTest(String input) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
+            new InputView(input);
+        });
+    }
+}
