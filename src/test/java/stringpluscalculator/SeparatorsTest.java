5702a8a1aa5bb83efd3e8a753eb6655a0c117ca5
isntyet <isntyet@gmail.com>
2020-03-27 21:10:10 +0900

    refactor(step1):  StringPlusCalculatorTest bug
    로직변경으로 인한 StringPlusCalculatorTest 테스트케이스 수정

--- src/test/java/stringpluscalculator/SeparatorsTest.java
+++ src/test/java/stringpluscalculator/SeparatorsTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 public class SeparatorsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"1234567", "//*\n1234567", "1234567"})
+    @ValueSource(strings = {"1234567", "//*\\n1234567", "1234567"})
     @DisplayName("separator 생성 테스트")
     void customSeparatorsTest(String input) {
         assertThat(new Separators(input)).isEqualTo(new Separators(input));
@@ -21,10 +21,10 @@ public class SeparatorsTest {
     @DisplayName("split용 separator 가져오기 테스트")
     void getSplitSeparatorTest() {
         assertAll(
-                () -> assertThat(new Separators("//d\n1234567").getSplitSeparator()).isEqualTo("d"),
-                () -> assertThat(new Separators("//*\n1234567").getSplitSeparator()).isEqualTo("*"),
+                () -> assertThat(new Separators("//d\\n1234567").getSplitSeparator()).isEqualTo("d"),
+                () -> assertThat(new Separators("//*\\n1234567").getSplitSeparator()).isEqualTo("*"),
                 () -> assertThat(new Separators("1234567").getSplitSeparator()).isEqualTo(":|,"),
-                () -> assertThat(new Separators("///\n1234567").getSplitSeparator()).isEqualTo("/")
+                () -> assertThat(new Separators("///\\n1234567").getSplitSeparator()).isEqualTo("/")
         );
     }
 
@@ -32,9 +32,9 @@ public class SeparatorsTest {
     @DisplayName("input에서 custom separator 부분 뺴고 가져오기 테스트")
     void getInputWithoutCustomSeparatorTest() {
         assertAll(
-                () -> assertThat(Separators.getInputWithoutCustomSeparator("//d\n1234567")).isEqualTo("1234567"),
-                () -> assertThat(Separators.getInputWithoutCustomSeparator("//*\n1234567")).isEqualTo("1234567"),
-                () -> assertThat(Separators.getInputWithoutCustomSeparator("//ㅋ\n8ㅋ7ㅋ6ㅋ7ㅋ8")).isEqualTo("8ㅋ7ㅋ6ㅋ7ㅋ8"),
+                () -> assertThat(Separators.getInputWithoutCustomSeparator("//d\\n1234567")).isEqualTo("1234567"),
+                () -> assertThat(Separators.getInputWithoutCustomSeparator("//*\\n1234567")).isEqualTo("1234567"),
+                () -> assertThat(Separators.getInputWithoutCustomSeparator("//ㅋ\\n8ㅋ7ㅋ6ㅋ7ㅋ8")).isEqualTo("8ㅋ7ㅋ6ㅋ7ㅋ8"),
                 () -> assertThat(Separators.getInputWithoutCustomSeparator("8ㅋ7ㅋ6ㅋ7ㅋ8")).isEqualTo("8ㅋ7ㅋ6ㅋ7ㅋ8"),
                 () -> assertThat(Separators.getInputWithoutCustomSeparator("1234567")).isEqualTo("1234567")
         );
