82d56f8552485a0b9efb3be4f3be6c6b5fbbc489
ohtaeg <otk1090@naver.com>
2020-03-28 01:30:34 +0900

    refactor (InputView) : 구입 금액 메시지 출력하도록 상수 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+
     private final Scanner scanner;
 
     public InputView(final InputStream inputStream) {
@@ -11,6 +13,7 @@ public class InputView {
     }
 
     public int inputPrice() {
+        System.out.println(INPUT_PRICE_MESSAGE);
         return scanner.nextInt();
     }
 }
