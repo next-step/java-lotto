e5959fe6907a8e82f9f7b46c11c7f46655b924be
luvram <sahul0804@gmail.com>
2020-03-27 23:17:31 +0900

    feat(addition): 피 연산자의 합을 구한다.

---
+++ src/main/java/study/domain/Addition.java
@@ -0,0 +1,17 @@
+package study.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Addition {
+    public static int sum(Operand ...operands) {
+        return sum(Arrays.asList(operands));
+    }
+    public static int sum(List<Operand> operands) {
+        int result = 0;
+        for(Operand operand: operands) {
+            result += operand.getOperand();
+        }
+        return result;
+    }
+}
--- src/test/java/study/OperandTest.java
+++ src/test/java/study/OperandTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import study.domain.Operand;
 
 import static org.assertj.core.api.Assertions.*;
 
