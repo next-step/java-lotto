0e7213a4e9e9d50552c71811a525dff337f5073b
etff <wish2rich@gmail.com>
2020-03-27 09:25:17 +0900

    feature(calculator): add create test

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,4 +1,14 @@
 package calculator;
 
-public class StringCalculatorTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringAddCalculatorTest {
+
+    @DisplayName(value = "계산기를 생성할 수 있다")
+    @Test
+    public void canCreate() {
+        new StringAddCalculatorTest();
+    }
+
 }
