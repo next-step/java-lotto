6ea58949a06d8c11e25e36fe15498e51600b7d2f
ohtaeg <otk1090@naver.com>
2020-03-28 03:07:07 +0900

    refactor (Lotto) : 로또 구매시 금액의 갯수를 반환하는게 아니라 해당 금액만큼 복권의 갯수를 가진 객체를 반환하도록 리팩토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import lotto.dto.Price;
 
 public class Lotto {
-    public int buy(final Price price) {
-        return price.lotteryCount();
+    public LotteryTickets buy(final Price price) {
+        return new LotteryTickets(price);
     }
 }
