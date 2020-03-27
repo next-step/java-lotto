1d455597ccbc6f1fc4d3c28c0f8ea7199e4048ac
LeeSeungHee <palmseung@gmail.com>
2020-03-28 00:22:49 +0900

    feat (money) : Add Money constructor with 1 argument

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,9 +1,24 @@
 package lotto;
 
+import java.util.Objects;
+
 public class Money {
     private Long money;
 
     public Money(Long money) {
         this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money1 = (Money) o;
+        return Objects.equals(money, money1.money);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
     }
 }
