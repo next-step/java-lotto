4095c586079c64544686eac5e8267992886046eb
ohtaeg <otk1090@naver.com>
2020-03-28 02:47:57 +0900

    refactor (Application) : 복권 갯수를 출력하는 결과 창 객체 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,15 @@
 package lotto;
 
-import lotto.dto.Price;
+import lotto.domain.Lotto;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        Lotto lotto = new Lotto();
         InputView inputView = new InputView();
-        Price price = inputView.inputPrice();
+
+        int lotteryCount = lotto.buy(inputView.inputPrice());
+        OutputView.printLotteryCount(lotteryCount);
     }
 }
