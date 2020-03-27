5642274c4cbd57e950911be084ebc0a469f703e1
LeeSeungHee <palmseung@gmail.com>
2020-03-27 23:56:48 +0900

    feat (stringConverter) : Add StringConverter.convertStringToPositiveNumber

--- src/main/java/lotto/StringConverter.java
+++ src/main/java/lotto/StringConverter.java
@@ -2,6 +2,6 @@ package lotto;
 
 public class StringConverter {
     public static PositiveNumber convertStringToPositiveNumber(String input) {
-        return null;
+        return new PositiveNumber(Integer.parseInt(input));
     }
 }
