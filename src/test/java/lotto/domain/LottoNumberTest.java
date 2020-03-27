f2f47ef45682c7017b3c5e2260c2a67bad7b6b27
ohtaeg <otk1090@naver.com>
2020-03-28 04:57:40 +0900

    test (Lotto Number) : @RepeatedTest를 이용하여 1 ~ 45까지의 숫자를 가지는 객체 생성 테스트 수정

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,19 +1,20 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberTest {
     @DisplayName("1 ~ 45 범위의 숫자를 가진 로또 숫자 객체를 생성할 수 있다.")
-    @Test
-    void create() {
-        final LottoNumber expect = new LottoNumber(1);
+    @RepeatedTest(value = 45, name = "1 ~ 45 중 {currentRepetition} of {totalRepetitions}")
+    void create(RepetitionInfo repetitionInfo) {
+        int number = repetitionInfo.getCurrentRepetition();
+        final LottoNumber expect = new LottoNumber(number);
 
-        final LottoNumber actual = new LottoNumber(1);
+        final LottoNumber actual = new LottoNumber(number);
 
-        assertThat(actual).isNotNull();
         assertThat(actual).isEqualTo(expect);
     }
 }
