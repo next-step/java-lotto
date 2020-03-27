672c3bb61821b939a1d3a3957ef35a8e9c1006bf
LeeSeungHee <palmseung@gmail.com>
2020-03-28 00:35:20 +0900

    test (money) : Delete MoneyTest.validateNonNumberTest
    * Add MoneyTest.validateNegativeTest

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -15,10 +15,10 @@ public class MoneyTest {
         assertThat(new Money(input)).isEqualTo(new Money(input));
     }
 
-    @DisplayName("숫자가 아닌 값을 인자로 받으면 예외를 반환한다.")
+    @DisplayName("음수를 입력받으면 객체는 예외를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1", "@", "Money", "TDD"})
-    void validateNonNumberTest(long input) {
+    @ValueSource(longs = {-1, -10, -10000})
+    void validateNegativeTest(long input) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             new Money(input);
         });
