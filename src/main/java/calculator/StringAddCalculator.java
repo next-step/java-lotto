692db10fd7ada0bfd0657ed5e31e3fda73da53cb
etff <wish2rich@gmail.com>
2020-03-28 01:10:42 +0900

    feature(calculator) : add Integer parser class

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,22 +3,8 @@ package calculator;
 public class StringAddCalculator {
 
     public int calculate(String inputText) {
-        return checkNullOrEmpty(inputText);
-    }
-
-    private int checkNullOrEmpty(String inputText) {
-        if (inputText == null || inputText.trim().isEmpty()) {
-            return 0;
-        }
-        return parseToInt(inputText);
-    }
-
-    private int parseToInt(String inputText) {
-        try {
-            return (Integer.parseInt(inputText));
-        } catch (Exception e) {
-            throw new IllegalArgumentException("숫자가 아닙니다. " + e.getMessage());
-        }
+        IntegerParser integerParser = new IntegerParser(inputText);
+        return integerParser.getNumber();
     }
 
 }
