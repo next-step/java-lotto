76b139e11cc3a6bf5ad7b8e1c0e1679920b7e86d
LeeSeungHee <palmseung@gmail.com>
2020-03-28 03:17:41 +0900

    feat (lotto) : Add Lotto.validateNumberRange

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
     private static final int LOTTO_SIZE = 6;
 
     private List<Integer> lottoNumbers;
@@ -12,6 +14,7 @@ public class Lotto {
     public Lotto(List<Integer> lottoNumbers) {
         validateSize(lottoNumbers);
         validateDuplication(lottoNumbers);
+        validateNumberRange(lottoNumbers);
         this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
     }
 
@@ -27,6 +30,16 @@ public class Lotto {
                 .size();
         if (lottoNumbers.size() > sizeOfSet) {
             throw new IllegalArgumentException("로또는 중복되지 않은 숫자로 구성되어야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> lottoNumbers) {
+        boolean hasNumberOutOfRange = lottoNumbers.stream()
+                .filter(number -> number > LOTTO_NUMBER_MAX || number < LOTTO_NUMBER_MIN)
+                .findAny()
+                .isPresent();
+        if (hasNumberOutOfRange) {
+            throw new IllegalArgumentException("로또는 1부터 45까지의 숫자로만 구성되어야 합니다.");
         }
     }
 }
