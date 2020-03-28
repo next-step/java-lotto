9e5762e1e34f48481731782f9d719144831d7fcf
ohtaeg <otk1090@naver.com>
2020-03-29 02:46:12 +0900

    refactor (OutputView) : 구매한 로또 목록 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.domain.LottoNumbers;
+
+import java.util.List;
+
 public class OutputView {
 
     private OutputView() {
@@ -7,5 +11,11 @@ public class OutputView {
 
     public static void printLotteryCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
+    }
+
+    public static void printLotteries(final List<LottoNumbers> lottoNumbers) {
+        for (LottoNumbers numbers : lottoNumbers) {
+            System.out.println(numbers);
+        }
     }
 }
