ed894eeb6107fb49f722761cfe6cea6263239a4c
Yeoseon Yun <devyyskr@gmail.com>
2020-03-28 21:40:12 +0900

    refactor(Money): money를 검증하는 로직 순서 변경, 매직넘버 상수화, 메소드 기능 분리

--- src/main/java/step2/Money.java
+++ src/main/java/step2/Money.java
@@ -2,22 +2,29 @@ package step2;
 
 public class Money {
     private static final Integer LOTTO_PRICE = 1000;
+    private static final Integer AMOUNT_ZERO = 0;
     public static final String MONEY_NEGATIVE_ERROR = "0보다 큰 금액을 입력해주세요.";
     public static final String MONEY_AMOUNT_ERROR = "로또 금액보다 큰 금액을 입력해주세요.";
 
     private final int money;
 
     public Money(int money) {
+        validateMoney(money);
         this.money = money;
-
-        validateMoney();
     }
 
-    private void validateMoney() {
-        if(money < 0) {
+    private void validateMoney(int money) {
+        checkIsNegativeMoney(money);
+        checkMoneyAmount(money);
+    }
+
+    private void checkIsNegativeMoney(int money) {
+        if(money < AMOUNT_ZERO) {
             throw new IllegalArgumentException(MONEY_NEGATIVE_ERROR);
         }
+    }
 
+    private void checkMoneyAmount(int money) {
         if(money < LOTTO_PRICE) {
             throw new IllegalArgumentException(MONEY_AMOUNT_ERROR);
         }
