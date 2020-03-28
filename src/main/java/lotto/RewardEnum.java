804ac746c06377a588c0d80dbca10623b188cb73
isntyet <isntyet@gmail.com>
2020-03-28 22:26:36 +0900

    feat(step2):  RewardEnum
    상금(Reward) 정보 정의하기
    매치되는 갯수로 상금정보 가져오기 기능 추가
    매치된 갯수로 상금수령 가능한지 체크하는 기능 추가

---
+++ src/main/java/lotto/RewardEnum.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum RewardEnum {
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+    private int matched;
+    private int reward;
+
+    RewardEnum(int matched, int reward) {
+        this.matched = matched;
+        this.reward = reward;
+    }
+
+    public int getMatched() {
+        return this.matched;
+    }
+
+    public int getReward() {
+        return this.matched;
+    }
+
+    public static boolean isWinning(int matched) {
+        return Arrays.stream(RewardEnum.values())
+                .anyMatch(r -> r.getMatched() == matched);
+    }
+
+    public static int getRewardFromMatched(int matched) {
+        return Arrays.stream(RewardEnum.values())
+                .filter(r -> r.getMatched() == matched)
+                .mapToInt(r -> r.getReward())
+                .findFirst()
+                .orElse(0);
+    }
+
+}
