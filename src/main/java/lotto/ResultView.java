b0824bcddad410ea4afec2b46d02aa75ff10e681
crystal <crystal2840@gmail.com>
2020-03-29 00:07:38 +0900

    resultview 안쓰는 코드 제거

--- src/main/java/lotto/ResultView.java
+++ src/main/java/lotto/ResultView.java
@@ -1,20 +1,16 @@
 package lotto;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ResultView {
-    private static final int WINNING_NUMBER_SIZE = 6;
     private static final String SPLIT_TEXT = ",";
 
     private LottoNumber winningNumbers;
 
     public ResultView(String inputText) {
         Set<Integer> numbers = splitWinningNumber(inputText);
-        validateWinningNumbers(numbers);
         winningNumbers = new LottoNumber(numbers);
 
     }
@@ -26,10 +22,5 @@ public class ResultView {
                 .collect(Collectors.toSet());
     }
 
-    private void validateWinningNumbers(Set<Integer> numbers) {
-        if (numbers.size() < WINNING_NUMBER_SIZE) {
-            throw new IllegalArgumentException("다른 숫자를 여섯개 입력해주세요.");
-        }
 
-    }
 }
