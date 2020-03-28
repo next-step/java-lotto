5615530e40a5d8698e1f8d0bb68a1e4980bc5122
ohtaeg <otk1090@naver.com>
2020-03-29 00:20:43 +0900

    refactor (LottoNumberGenerator) : static 메서드로 변경

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -4,15 +4,14 @@ import java.util.*;
 
 public class LottoNumberGenerator {
 
-    private Random random = new Random();
+    private static final Random RANDOM = new Random();
 
-    public List<LottoNumber> generate() {
+    public static List<LottoNumber> generate() {
         Set<LottoNumber> set = new HashSet<>();
-        LottoNumber lottoNumber;
         while (set.size() < 6) {
-            lottoNumber = new LottoNumber(random.nextInt(45) + 1);
-            set.add(lottoNumber);
+            set.add(new LottoNumber(RANDOM.nextInt(45) + 1));
         }
+
         List<LottoNumber> lottoNumbers = new ArrayList<>(set);
         Collections.shuffle(lottoNumbers);
         return lottoNumbers;
