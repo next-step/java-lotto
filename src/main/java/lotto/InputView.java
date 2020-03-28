47f593953ef9a4342eb1a98862ca995b6d284d8a
crystal <crystal2840@gmail.com>
2020-03-28 20:34:50 +0900

    구입금액에서 구매할 수 있는 구매 수량기능 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,9 +3,21 @@ package lotto;
 public class InputView {
     private static final int MIN_PURCHASE_AMOUNT = 1000;
 
+    private int purchaseAmount;
+    private int purchaseCount;
+
+    public InputView(int purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
     public void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < MIN_PURCHASE_AMOUNT) {
             throw new IllegalArgumentException("구매 금액은 최소 천원 이상이어야 합니다");
         }
+    }
+
+    public int getPurchaseCount() {
+        return purchaseAmount / MIN_PURCHASE_AMOUNT;
     }
 }
--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,18 +12,23 @@ class InputViewTest {
 
     private InputView inputView;
 
-    @BeforeEach
-    void setUp() {
-        inputView = new InputView();
-    }
-
     @DisplayName("로또 구입 금액이 최소 금액보다 이하일 경우 예외처리를 해준다")
     @Test
     void validatePurchaseAmount() {
         int purchaseAmount = 500;
-
+        
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            inputView.validatePurchaseAmount(purchaseAmount);
+            inputView = new InputView(purchaseAmount);
         });
+    }
+
+    @DisplayName("구입금액에서 구매할 수 있는 구매 수량을 확인한다.")
+    @Test
+    void getPurchaseCount() {
+        int purchaseAmount = 14000;
+        inputView = new InputView(purchaseAmount);
+        int purchaseCount = inputView.getPurchaseCount();
+
+        assertThat(purchaseCount).isEqualTo(14);
     }
 }
