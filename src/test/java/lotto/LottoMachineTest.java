e70b5e97c697ecfedfeac6efd44cb10e7ffadc7d
yohan <yohan.lee@uneedcomms.com>
2020-03-29 01:20:59 +0900

    test: modify LottoMachineTest method naming

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -12,7 +12,7 @@ public class LottoMachineTest {
     @ParameterizedTest
     @DisplayName("가격에 맞는 개수만큼 로또 티켓 생성")
     @CsvSource(value = {"3500:3", "9800:9", "15920:15"}, delimiter = ':')
-    void test(int money, int expectedLottoTicketCount) {
+    void checkTicketCountByPayMoney(int money, int expectedLottoTicketCount) {
         List<LottoTicket> lottoTickets = LottoMachine.pay(money);
         assertThat(lottoTickets).hasSize(expectedLottoTicketCount);
     }
