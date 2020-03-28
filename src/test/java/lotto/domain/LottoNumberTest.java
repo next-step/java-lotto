373c7b87b7971aa91f1beeacd833c8ba2a12c1ee
teahyuk <jtaeh57@gmail.com>
2020-03-28 13:32:46 +0900

    refactor(LottoNumberTest) : 객체 equal 테스트를 이름을 수정하고 쓸데없는 DisplayName제거

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -9,8 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoNumberTest {
 
 	@Test
-	@DisplayName("객체 테스트")
-	void constructor() {
+	void isEqualTest() {
 		assertThat(new LottoNumber(1, 2, 3, 4, 5, 6))
 				.isEqualTo(new LottoNumber(1, 2, 3, 4, 5, 6));
 	}
