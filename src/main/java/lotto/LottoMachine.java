a752d6bf1f557537a53808c80fc70f3ee343b025
isntyet <isntyet@gmail.com>
2020-03-28 19:56:28 +0900

    feat(step2):  Lotto list
    로또 돈만큼 생성하기

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,16 +1,35 @@
 package lotto;
 
-public class LottoMachine {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
 
     private Money money;
+    private List<Lotto> lottos;
 
-    public LottoMachine(int input) {
-        this.money = new Money(input);
+    public LottoMachine(Money money) {
+        this.money = money;
+        validatePurchasable();
+        this.lottos = generateLottos();
     }
 
-    public void generateLotto() {
-        money.getPurchasableCount(LOTTO_PRICE);
+    public List<Lotto> getLottos() {
+        return this.lottos;
+    }
+
+    private List<Lotto> generateLottos() {
+        List<Lotto> generatedlottos = new ArrayList<>();
+        for (int i = 0; i < money.getPurchasableCount(LOTTO_PRICE); i++) {
+            generatedlottos.add(new Lotto());
+        }
+        return generatedlottos;
+    }
+
+    private void validatePurchasable() {
+        if (money.getPurchasableCount(LOTTO_PRICE) < 1) {
+            throw new RuntimeException("돈이 부족합니다.");
+        }
     }
 }
--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -2,6 +2,8 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,7 +13,16 @@ public class LottoMachineTest {
     @DisplayName("로또기계 생성 테스트")
     void createLotooMachineTest() {
         assertThat(
-                new LottoMachine(1000)
+                new LottoMachine(new Money(1000))
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 9})
+    @DisplayName("로또기계로 돈만큼 로또 생성 테스트")
+    void generateLottosTest(int input) {
+        assertThat(
+                new LottoMachine(new Money(input * 1000)).getLottos().size()
+        ).isEqualTo(input);
     }
 }
