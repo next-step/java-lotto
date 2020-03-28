65bf6eaccbca3a754b02e6af507e5b91eac93554
parkeeseul <eeseul.p@gmail.com>
2020-03-28 19:58:17 +0900

    refactor(Lotto): 상수화

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,17 +8,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int LOTTO_COUNT = 6;
+    private static final String REGEX = ",";
+
     @Getter
     private List<Integer> numbers;
 
     public Lotto(String lottoValue) {
-        this(Arrays.stream(lottoValue.split(","))
+        this(Arrays.stream(lottoValue.split(REGEX))
                 .map(value -> Integer.parseInt(value.trim()))
                 .collect(Collectors.toList()));
     }
 
     Lotto(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_COUNT) {
             throw new RuntimeException("Lotto number must be 6");
         }
 
