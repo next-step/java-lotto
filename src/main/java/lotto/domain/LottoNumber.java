3c4effbacbe349a41707fe5623262bf5dd5c4bad
ohtaeg <otk1090@naver.com>
2020-03-28 22:56:47 +0900

    refactor (Lotto Number) : 1 ~ 45 이외 숫자가 들어올 경우 런타임 예외 발생하도록 validation 메서드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -6,7 +6,14 @@ public class LottoNumber {
     private int number;
 
     public LottoNumber(final int number) {
+        checkAvailableNumber(number);
         this.number = number;
+    }
+
+    private void checkAvailableNumber(final int number) {
+        if (!(0 < number && number < 46)) {
+            throw new RuntimeException("1 ~ 45 사이의 숫자가 아닙니다.");
+        }
     }
 
     @Override
