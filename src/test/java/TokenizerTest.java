ae8dcaa782c4fbda151b211b5030a8689d71bfb0
kks <rudtjs9956@gmail.com>
2020-03-28 22:42:20 +0900

    test(TokenizerTest): add custom delimiter split test

--- src/test/java/TokenizerTest.java
+++ src/test/java/TokenizerTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
@@ -10,8 +10,7 @@ public class TokenizerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3", "1:2:3", "1,2:3", "1:2,3"})
-    public void splitTest() {
-        String message = "1,2:3";
+    public void splitTest(String message) {
         List<Integer> tokens = Tokenizer.split(message);
         assertThat(tokens).hasSize(3);
         assertThat(tokens).contains(1, 2, 3);
@@ -19,16 +18,33 @@ public class TokenizerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1@2,3", "1!2:3", "1!2!3", "1@2@3"})
-    public void withWrongMessageSplitThrowException(String message) {
+    public void wrongMessageSplitThrowException(String message) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             Tokenizer.split(message);
         });
     }
 
-    @Test
-    public void customDelimiterSplitTest() {
-        String message = "1;2;3";
-        String delimiter = ";";
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "1:-2:3", "1,2:-3", "-1:-2,-3"})
+    public void negativeNumberSplitThrowException(String message) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    Tokenizer.split(message);
+                }).withMessage("음수는 계산할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a,b,c", "a:b:c", "A,B,C", "A,b,C"})
+    public void stringMessageSplitThrowException(String stringMessage) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    Tokenizer.split(stringMessage);
+                }).withMessage("문자열은 계산할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1@2@3, @", "1!2!3, !", "1#2#3, #"})
+    public void customDelimiterSplitTest(String message, String delimiter) {
         List<Integer> tokens = Tokenizer.split(message, delimiter);
         assertThat(tokens).hasSize(3);
         assertThat(tokens).contains(1, 2, 3);
@@ -36,12 +52,29 @@ public class TokenizerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"@", "!", "#", ";"})
-    public void withWrongDelimiterSplitThrowException(String delimiter) {
+    public void wrongDelimiterCustomDelimiterSplitThrowException(String delimiter) {
         String message = "1,2:3";
         assertThatIllegalArgumentException().isThrownBy(() -> {
             Tokenizer.split(message, delimiter);
         });
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"-1@2@3, @", "1!-2!3, !", "1#2#-3, #"})
+    public void negativeNumberCustomDelimiterSplitThrowException(String message, String delimiter) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    Tokenizer.split(message, delimiter);
+                }).withMessage("음수는 계산할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a@b@c, @", "X!y!z, !", "D#E#[#]#{#}, #"})
+    public void stringMessageCustomDelimiterSplitThrowException(String message, String delimiter) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    Tokenizer.split(message, delimiter);
+                }).withMessage("문자열은 계산할 수 없습니다.");
+    }
 
 }
