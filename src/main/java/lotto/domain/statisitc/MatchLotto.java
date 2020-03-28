7ce81cc9917e29a8e0be554c6bbe38d92f556486
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-28 12:00:28 +0900

    feat(MatchLotto) : 매칭된 로또 컬렉션
    - 매칭된 번호 갯수 저장
    - 같은 개수가 매칭된 로또를 더해줌

---
+++ src/main/java/lotto/domain/statisitc/MatchLotto.java
@@ -0,0 +1,41 @@
+package lotto.domain.statisitc;
+
+import lotto.domain.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchLotto {
+    private final int matchCount;
+    private final List<Item> matchItem;
+
+    public MatchLotto(int matchCount) {
+        this(matchCount, new ArrayList<>());
+    }
+
+    public MatchLotto(int matchCount, Item matchItem) {
+        this.matchCount = matchCount;
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(matchItem);
+        this.matchItem = items;
+    }
+
+    public MatchLotto(int matchCount, List<Item> matchItem) {
+        this.matchCount = matchCount;
+        ArrayList<Item> items = new ArrayList<>();
+        items.addAll(matchItem);
+        this.matchItem = items;
+    }
+
+    public MatchLotto addItem(Item other) {
+        List<Item> items = new ArrayList<>();
+        items.addAll(this.matchItem);
+
+        return new MatchLotto(this.matchCount, items);
+    }
+
+    public int getItemsCount() {
+        return this.matchItem.size();
+    }
+
+}
---
+++ src/test/java/lotto/domain/statistic/MatchLottoTest.java
@@ -0,0 +1,40 @@
+package lotto.domain.statistic;
+
+import lotto.domain.item.Item;
+import lotto.domain.item.LottoTicket;
+import lotto.domain.statisitc.MatchLotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MatchLottoTest {
+    private List<Item> items;
+    LottoTicket ticket1 = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6));
+    LottoTicket ticket2 = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 10));
+
+    private static Stream<Arguments> provideItems() {
+        return Stream.of(
+                Arguments.of(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                Arguments.of(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 10))),
+                Arguments.of(new LottoTicket(Arrays.asList(1, 2, 3, 4, 10, 11))),
+                Arguments.of(new LottoTicket(Arrays.asList(1, 10, 11, 12, 13, 14, 15))),
+                Arguments.of(new LottoTicket(Arrays.asList(10, 11, 12, 13, 14, 15, 16)))
+        );
+    }
+
+    @DisplayName("생성자 테스트")
+    @Test
+    public void construct() throws Exception {
+        //given
+        MatchLotto matchLotto = new MatchLotto(1, ticket1);
+
+        //then
+        assertThat(matchLotto.getItemsCount()).isEqualTo(1);
+    }
+}
