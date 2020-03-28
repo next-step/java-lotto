722cef88725436b2e1ea4dd90509fb15eb6f34de
kks <rudtjs9956@gmail.com>
2020-03-28 21:24:00 +0900

    feat(Tokenizer): add custom delimiter split

--- src/main/java/Tokenizer.java
+++ src/main/java/Tokenizer.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Tokenizer {
+    private static final String DEFAULT_DELIMITERS = ",|:";
 
     public static List<Integer> split(String message) {
         List<Integer> result = new ArrayList<>();
@@ -11,5 +12,14 @@ public class Tokenizer {
             result.add(Integer.valueOf(stringTokenizer.nextToken()));
         }
         return result;
+    }
+
+    public static List<Integer> split(String message, String delimiter) {
+        List<Integer> result = new ArrayList<>();
+        StringTokenizer stringTokenizer = new StringTokenizer(message, delimiter);
+        while (stringTokenizer.hasMoreTokens()) {
+            result.add(Integer.valueOf(stringTokenizer.nextToken()));
+        }
+        return  result;
     }
 }
