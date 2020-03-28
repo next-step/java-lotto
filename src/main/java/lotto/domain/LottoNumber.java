eaf10466b371a10d5d28209bfd3cbeeb815f3f5e
teahyuk <jtaeh57@gmail.com>
2020-03-28 12:12:45 +0900

    feat(LottoNumber): 로또 번호 VO 추가
    로또 번호 VO 추가 :
      - LottoNumber 생성자 TC 추가.
      - LottoNumber 생성자 유효성 검사 TC 추가.
      - LottoNumber 유효성 검사 기능 추가.

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,11 +1,49 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LottoNumber {
 	private List<Integer> numbers;
 
+	LottoNumber(Integer... numbers) {
+		this(Arrays.asList(numbers));
+	}
+
 	public LottoNumber(List<Integer> numbers) {
+		if (isInvalidLottoNumbers(numbers)) {
+			throw new IllegalArgumentException(String.format("Instantiate LottoNumber failed. " +
+					"numbers must be distinct 6 numbers range 1~45 : numbers=%s", listString(numbers)));
+		}
+
 		this.numbers = numbers;
+	}
+
+	private boolean isInvalidLottoNumbers(List<Integer> numbers) {
+		return numbers.stream()
+				.distinct()
+				.filter(number -> 0 < number && number < 46)
+				.count() != 6;
+	}
+
+	private String listString(List<Integer> numbers) {
+		return numbers.stream()
+				.map(Object::toString)
+				.collect(Collectors.joining(",", "[", "]"));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		LottoNumber that = (LottoNumber) o;
+		return numbers.equals(that.numbers);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(numbers);
 	}
 }
---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTest {
+
+	@Test
+	@DisplayName("객체 테스트")
+	void constructor() {
+		assertThat(new LottoNumber(1, 2, 3, 4, 5, 6))
+				.isEqualTo(new LottoNumber(1, 2, 3, 4, 5, 6));
+	}
+
+	@Test
+	@DisplayName("객체 생성 유효성 테스트")
+	void expectConstructorException(){
+		assertThatThrownBy(()->new LottoNumber(1))
+				.isInstanceOf(RuntimeException.class);
+
+		assertThatThrownBy(()->new LottoNumber(1,2,3,4,5,5))
+				.isInstanceOf(RuntimeException.class);
+	}
+
+}
