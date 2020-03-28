ac74d7f895719b1752febf3b9483e35b761bcdb0
ohtaeg <otk1090@naver.com>
2020-03-29 00:10:13 +0900

    refactor (LottoNumberGenerator) : set을 이용하여 로또 번호 중복 되지 않도록 변경

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,18 +1,20 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class LottoNumberGenerator {
 
     private Random random = new Random();
 
     public List<LottoNumber> generate() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            lottoNumbers.add(new LottoNumber(random.nextInt(45) + 1));
+        Set<LottoNumber> set = new HashSet<>();
+        LottoNumber lottoNumber;
+        while (set.size() < 6) {
+            lottoNumber = new LottoNumber(random.nextInt(45) + 1);
+            set.add(lottoNumber);
         }
+        List<LottoNumber> lottoNumbers = new ArrayList<>(set);
+        Collections.shuffle(lottoNumbers);
         return lottoNumbers;
     }
 }
