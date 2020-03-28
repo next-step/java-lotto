41d676fab4adcbcf06d07d88cb1ea28994feb7f2
yohan <yohan.lee@uneedcomms.com>
2020-03-28 22:00:25 +0900

    fix: modify LottoPriceTest method naming

--- src/test/java/lotto/LottoPriceTest.java
+++ src/test/java/lotto/LottoPriceTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoPriceTest {
     @Test
     @DisplayName("로또 가격 정책 가져오기")
-    void getPrice_fixedPrice_return1000() {
+    void successByGetPrice() {
         int price = LottoPrice.getPrice();
         assertThat(price).isEqualTo(1000);
     }
