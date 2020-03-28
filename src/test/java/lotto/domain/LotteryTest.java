0b43f4e5f7e5dee622aeb12bc62c7c5412d0d9e1
ohtaeg <otk1090@naver.com>
2020-03-28 23:07:23 +0900

    test (Lottery) : 6개의 원시값을 가지는 복권 객체를 LottoNumber 객체가 6개인 리스트를 가지도록 테스트 수정

--- src/test/java/lotto/domain/LotteryTest.java
+++ src/test/java/lotto/domain/LotteryTest.java
@@ -1,18 +1,35 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LotteryTest {
 
+    @BeforeEach
+    void setUp() {
+
+    }
+
     @DisplayName("로또 복권은 6개의 숫자를 가진다.")
     @Test
     void create() {
-        Lottery expect = new Lottery("1", "2", "3", "4", "5", "6");
+        List<LottoNumber> lottoNumbers = Arrays.asList(
+                new LottoNumber(1)
+                , new LottoNumber(2)
+                , new LottoNumber(3)
+                , new LottoNumber(4)
+                , new LottoNumber(5)
+                , new LottoNumber(6)
+        );
+        Lottery expect = new Lottery(lottoNumbers);
 
-        Lottery actual = new Lottery("1", "2", "3", "4", "5", "6");
+        Lottery actual = new Lottery(lottoNumbers);
 
         assertThat(actual).isEqualTo(expect);
     }
