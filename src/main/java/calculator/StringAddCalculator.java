5973cc125883199276af7f7db105d5bd3b289786
yohan <yohan.lee@uneedcomms.com>
2020-03-28 15:42:20 +0900

    refactor: feedback - Pattern create Strategy
    - use static

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,13 +6,14 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
     private static final String DEFAULT_DELIMITERS = "[,:]";
     private static final String CUSTOM_DELIMITER_REGEX = "//(.)#(.*)";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
 
     public static int calculate(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        Matcher custom = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
+        Matcher custom = CUSTOM_PATTERN.matcher(input);
         if (custom.find()) {
             return new CustomNumbers(custom.group(2).split(custom.group(1))).sum();
         }
