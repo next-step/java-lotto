bc703de514dc6efa662368fa967c580273b4a660
jwee0330 <jwee0330@gmail.com>
2020-03-29 03:11:14 +0900

    test(LottoNumber): add test about counting matched numbers

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -27,6 +28,16 @@ class LottoNumberTest {
         LottoNumber another = new LottoNumber(numbers);
 
         assertThat(one).isNotEqualTo(another);
+    }
+
+    @DisplayName("로또 번호끼리 맞은 번호 수 계산 테스트")
+    @Test
+    public void lottoMatchCountTest() {
+        LottoNumber lottoNumber = new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoNumber winningNumber = new LottoNumber(Arrays.asList(1, 2, 3, 10, 11, 12));
+
+        long matchCountInLottoNumber = lottoNumber.getMatchCountInLottoNumber(winningNumber);
+        assertThat(matchCountInLottoNumber).isEqualTo(3);
     }
 
 }
