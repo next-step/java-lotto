3e0c8739d446c3244818aed622585b6c7ecaff73
ohtaeg <otk1090@naver.com>
2020-03-28 23:26:17 +0900

    refactor (Lotto) : 복권을 만드는 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,12 +2,21 @@ package lotto.domain;
 
 import lotto.dto.Price;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     public LotteryTickets buy(final Price price) {
-        List<Lottery> lotteryList = Arrays.asList(new Lottery(), new Lottery());
-        return new LotteryTickets(lotteryList);
+        List<Lottery> lotteries = makeLottery(price);
+        return new LotteryTickets(lotteries);
+    }
+
+    private List<Lottery> makeLottery(final Price price) {
+        List<Lottery> lotteries = new ArrayList<>();
+        final int lotteryCount = price.lotteryCount();
+        for (int i = 0; i < lotteryCount; i++) {
+            lotteries.add(new Lottery());
+        }
+        return lotteries;
     }
 }
