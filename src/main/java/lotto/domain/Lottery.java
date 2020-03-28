2cff4a707fe4426ccf8831edb4505d5859be9362
ohtaeg <otk1090@naver.com>
2020-03-29 03:31:08 +0900

    refactor (Lottery) : 입력받은 문자열로 복권 객체를 생성하도록 리팩토링

--- src/main/java/lotto/domain/Lottery.java
+++ src/main/java/lotto/domain/Lottery.java
@@ -1,16 +1,32 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Lottery {
+    private static final String LOTTO_NUMBER_DELIMITER = ", ";
 
     private LottoNumbers lottoNumbers;
+
+    public Lottery(final String input) {
+        this.lottoNumbers = new LottoNumbers(newInstanceFromInput(input));
+    }
 
     public Lottery(final LottoNumbers lottoNumbers) {
         this.lottoNumbers = lottoNumbers;
     }
 
     LottoNumbers getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    private List<LottoNumber> newInstanceFromInput(final String input) {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (String letter : input.split(LOTTO_NUMBER_DELIMITER)) {
+            LottoNumber lottoNumber = LottoNumber.valueOf(Integer.valueOf(letter));
+            lottoNumbers.add(lottoNumber);
+        }
         return lottoNumbers;
     }
 
