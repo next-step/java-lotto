9411c6c53a550102d3812642050ea060a597dc7c
ohtaeg <otk1090@naver.com>
2020-03-29 02:05:33 +0900

    test (Lottery) : 리스트를 일급컬렉션으로 변경

--- src/test/java/lotto/domain/LotteryTest.java
+++ src/test/java/lotto/domain/LotteryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,14 +12,14 @@ class LotteryTest {
     @DisplayName("로또 복권은 6개의 로또 번호를 가지도록 객체 생성을 할 수 있다.")
     @Test
     void create() {
-        List<LottoNumber> lottoNumbers = Arrays.asList(
+        LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(
                 LottoNumber.valueOf(1)
                 , LottoNumber.valueOf(2)
                 , LottoNumber.valueOf(3)
                 , LottoNumber.valueOf(4)
                 , LottoNumber.valueOf(5)
                 , LottoNumber.valueOf(6)
-        );
+        ));
         Lottery expect = new Lottery(lottoNumbers);
 
         Lottery actual = new Lottery(lottoNumbers);
