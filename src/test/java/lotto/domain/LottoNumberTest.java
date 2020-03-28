28a74cf6b3cfbae7bfdd5cb0d722bd252bb6893e
ohtaeg <otk1090@naver.com>
2020-03-28 22:56:13 +0900

    test (Lotto Number) : 1 ~ 45 이외 숫자가 들어올 경우 런타임 예외 발생 테스트

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -3,8 +3,11 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class LottoNumberTest {
     @DisplayName("1 ~ 45 범위의 숫자를 가진 로또 숫자 객체를 생성할 수 있다.")
@@ -16,5 +19,14 @@ class LottoNumberTest {
         final LottoNumber actual = new LottoNumber(number);
 
         assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("1 ~ 45 범위 외 숫자는 예외를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    void invalidLottoNumber(int number) {
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(
+                () -> new LottoNumber(number)
+        );
     }
 }
