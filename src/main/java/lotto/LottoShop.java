7cc8141fe443f69c94b8fea708f49fe6509bf88d
dongchul <sah3122@naver.com>
2020-03-29 00:07:40 +0900

    refactor(Lotto): add List<Lotto> to LottoBundle

--- src/main/java/lotto/LottoShop.java
+++ src/main/java/lotto/LottoShop.java
@@ -1,19 +1,35 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoBundle;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningType;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoShop {
     private static final int PRICE_PER_PIECE = 1000;
 
-    public List<Lotto> buyAuto(int price) {
-        List<Lotto> lottos = new ArrayList<>();
+    public LottoBundle buyAuto(int price) {
+        validatePrice(price);
         int lottoCount = Math.floorDiv(price, PRICE_PER_PIECE);
+        List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < lottoCount; i++) {
             lottos.add(new Lotto());
         }
+        return new LottoBundle(lottos);
+    }
 
-        return lottos;
+    private void validatePrice(int price) {
+        if (price < PRICE_PER_PIECE) {
+            throw new IllegalArgumentException("로또 가격은 1000원 이상이어야 합니다.");
+        }
+    }
+
+    public LottoResult checkWinning(LottoBundle lottoBundle, String winningNumber) {
+        List<WinningType> winningTypes = lottoBundle.lottery(winningNumber);
+        return new LottoResult(winningTypes, lottoBundle);
     }
 }
