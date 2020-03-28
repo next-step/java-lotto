3397b882c9445176355cb62fbdcdcc4aa25b7a8a
ohtaeg <otk1090@naver.com>
2020-03-29 01:17:53 +0900

    refactor (LottoNumberGenerator) : 메서드 분리

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -7,14 +7,21 @@ public class LottoNumberGenerator {
     private static final Random RANDOM = new Random();
 
     public static List<LottoNumber> generate() {
-        Set<LottoNumber> set = new HashSet<>();
-        while (set.size() < 6) {
-            set.add(LottoNumber.valueOf(RANDOM.nextInt(45) + 1));
-        }
-
-        List<LottoNumber> lottoNumbers = new ArrayList<>(set);
+        List<LottoNumber> lottoNumbers = removeDuplicateNumbers();
         Collections.shuffle(lottoNumbers);
         return lottoNumbers;
+    }
+
+    private static List<LottoNumber> removeDuplicateNumbers() {
+        Set<LottoNumber> set = new HashSet<>();
+        while (set.size() < 6) {
+            set.add(LottoNumber.valueOf(random()));
+        }
+        return new ArrayList<>(set);
+    }
+
+    private static int random() {
+        return RANDOM.nextInt(LottoNumber.MAX) + LottoNumber.MIN;
     }
 }
 
