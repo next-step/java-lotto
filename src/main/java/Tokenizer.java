131676a7a6cb81bf1de6e87b5e504f8cb0b981a5
kks <rudtjs9956@gmail.com>
2020-03-28 22:43:15 +0900

    refactor(Tokenizer): combine split and custom delimiter split

--- src/main/java/Tokenizer.java
+++ src/main/java/Tokenizer.java
@@ -4,22 +4,33 @@ import java.util.StringTokenizer;
 
 public class Tokenizer {
     private static final String DEFAULT_DELIMITERS = ",|:";
+    private static final String NEGATIVE_NUMBER_ERROR = "음수는 계산할 수 없습니다.";
+    private static final String STRING_MESSAGE_ERROR = "문자열은 계산할 수 없습니다.";
 
     public static List<Integer> split(String message) {
-        List<Integer> result = new ArrayList<>();
-        StringTokenizer stringTokenizer = new StringTokenizer(message, ",|:");
-        while (stringTokenizer.hasMoreTokens()) {
-            result.add(Integer.valueOf(stringTokenizer.nextToken()));
-        }
-        return result;
+        return split(message, DEFAULT_DELIMITERS);
     }
 
     public static List<Integer> split(String message, String delimiter) {
         List<Integer> result = new ArrayList<>();
         StringTokenizer stringTokenizer = new StringTokenizer(message, delimiter);
         while (stringTokenizer.hasMoreTokens()) {
-            result.add(Integer.valueOf(stringTokenizer.nextToken()));
+            result.add(validateToken(stringTokenizer.nextToken()));
         }
         return  result;
     }
+
+    private static int validateToken(String token) {
+        int result;
+        try {
+            result = Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(STRING_MESSAGE_ERROR);
+        }
+        if (result <= -1) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR);
+        }
+        return result;
+    }
+
 }
