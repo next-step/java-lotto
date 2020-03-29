10e2c36d0c84a9d3100abdc2a9490d7c1ebe8ef6
ohtaeg <otk1090@naver.com>
2020-03-29 15:08:05 +0900

    feat (LottoResult) : 로또 결과를 가진 객체 구현

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<LottoRank, Integer> chart;
+
+    {
+        chart = new HashMap<>();
+        chart.put(LottoRank.BLANK, 0);
+        chart.put(LottoRank.FOURTH, 0);
+        chart.put(LottoRank.THIRD, 0);
+        chart.put(LottoRank.SECOND, 0);
+        chart.put(LottoRank.FIRST, 0);
+    }
+
+    public void increase(LottoRank lottoRank) {
+        chart.put(lottoRank, chart.get(lottoRank) + 1);
+    }
+
+    public Map<LottoRank, Integer> getChart() {
+        return Collections.unmodifiableMap(chart);
+    }
+}
