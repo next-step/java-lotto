94454c13bfe1324e651c5aa9afe69a450452c1da
yohan <yohan.lee@uneedcomms.com>
2020-03-30 03:37:17 +0900

    refactor(ResultView): 보너스번호 입력 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -11,6 +11,7 @@ public class ResultView {
     private static final String BUY_COUNT_FORMAT = "%d개를 구매했습니다.";
     private static final String LOTTO_TICKET_FORMAT = "[%d, %d, %d, %d, %d, %d]";
     private static final String WINNING_STATISTICS_TITLE = "당첨 통계\n---------";
+    private static final String WINNING_STATISTICS_BONUS_BALL_FORMAT = "%d개 일치, 보너스 볼 일치 (%d원)- %d개";
     private static final String WINNING_STATISTICS_FORMAT = "%d개 일치 (%d원)- %d개";
     private static final String WINNING_STATISTICS_PROFIT_RATE_FORMAT = "총 수익률은 %.2f 입니다.";
     private static final String RESULT_GOOD = "(기준이 1이기 때문에 결과적으로 이득이라는 의미임)";
@@ -35,10 +36,14 @@ public class ResultView {
     }
 
     private static void printWinningStatistics(List<Rank> winningResult, Rank winningValue) {
+        String format = WINNING_STATISTICS_FORMAT;
+        if (winningValue == Rank.SECOND) {
+            format = WINNING_STATISTICS_BONUS_BALL_FORMAT;
+        }
         long count = winningResult.stream()
                 .filter(value -> value == winningValue)
                 .count();
-        System.out.println(String.format(WINNING_STATISTICS_FORMAT,
+        System.out.println(String.format(format,
                 winningValue.getMatchCount(),
                 winningValue.getAmount(),
                 count));
