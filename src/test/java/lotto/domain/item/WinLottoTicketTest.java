9b2bc4b92d179f63dc3fb5b00f2984f84ee55c79
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-30 00:03:40 +0900

    test(WinLottoTicketTest) : 당첨번호 6개와 보너스번호 1개의 중복 테스트

--- src/test/java/lotto/domain/item/WinLottoTicketTest.java
+++ src/test/java/lotto/domain/item/WinLottoTicketTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.item;
 
+import lotto.exception.ValidLottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinLottoTicketTest {
 
@@ -28,10 +30,18 @@ class WinLottoTicketTest {
     @Test
     public void create() throws Exception {
         //given
-        WinLottoTicket winTicket = new WinLottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6), 6);
-        WinLottoTicket winTicket2 = new WinLottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6), 6);
+        WinLottoTicket winTicket = new WinLottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
+        WinLottoTicket winTicket2 = new WinLottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
 
         //then
         assertThat(winTicket.equals(winTicket2)).isTrue();
+    }
+
+    @DisplayName("당첨 로또의 번호 6개와 추가 보너스 번호 1개가 중복되선 안된다")
+    @Test
+    public void validDuplicate() throws Exception {
+        assertThatThrownBy(
+                () -> new WinLottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6), 6)
+        ).isInstanceOf(ValidLottoException.class);
     }
 }
