8d62daa166ee149fa0398da5acc07d8b95679895
teahyuk <jtaeh57@gmail.com>
2020-03-29 15:36:37 +0900

    refactor(LottoCheckTest) : ParameterizeTest 로 수정

--- src/test/java/lotto/domain/LottoCheckerTest.java
+++ src/test/java/lotto/domain/LottoCheckerTest.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,24 +21,27 @@ class LottoCheckerTest {
 				.isEqualTo(new LottoChecker(new LottoNumber(1, 2, 3, 4, 5, 6), 14000));
 	}
 
-	@Test
-	void getWinningResultTest() {
-		LottoChecker lottoChecker = new LottoChecker(new LottoNumber(1, 2, 3, 4, 5, 6), 1000);
+	@ParameterizedTest
+	@MethodSource("provideWinningResultTestArgs")
+	void getWinningResultTest(List<Integer> winningNumbers, List<Integer> drawNumbers, LottoRank exactRank) {
+		LottoChecker lottoChecker = new LottoChecker(new LottoNumber(winningNumbers), 1000);
 
-		assertThat(lottoChecker.getWinningResult(new LottoNumber(1, 2, 3, 4, 5, 6)))
-				.isEqualTo(new LottoResult(Collections.singletonMap(LottoRank.FIRST, 1L), 1000));
-
-		assertThat(lottoChecker.getWinningResult(new LottoNumber(1, 2, 3, 4, 5, 7)))
-				.isEqualTo(new LottoResult(Collections.singletonMap(LottoRank.SECOND, 1L), 1000));
-
-		assertThat(lottoChecker.getWinningResult(new LottoNumber(1, 2, 3, 4, 7, 8)))
-				.isEqualTo(new LottoResult(Collections.singletonMap(LottoRank.THIRD, 1L), 1000));
-
-		assertThat(lottoChecker.getWinningResult(new LottoNumber(1, 2, 3, 7, 8, 9)))
-				.isEqualTo(new LottoResult(Collections.singletonMap(LottoRank.FOURTH, 1L), 1000));
-
-		assertThat(lottoChecker.getWinningResult(new LottoNumber(1, 2, 7, 8, 9, 10)))
-				.isEqualTo(new LottoResult(Collections.EMPTY_MAP, 1000));
+		assertThat(lottoChecker.getWinningResult(new LottoNumber(drawNumbers)))
+				.isEqualTo(new LottoResult(Collections.singletonMap(exactRank, 1L), 1000));
 	}
 
+	private static Stream<Arguments> provideWinningResultTestArgs(){
+		return Stream.of(
+				Arguments.of(makeListFromIntegers(1,2,3,4,5,6),makeListFromIntegers(1,2,3,4,5,6),LottoRank.FIRST),
+				Arguments.of(makeListFromIntegers(1,2,3,4,5,6),makeListFromIntegers(1,2,3,4,5,7),LottoRank.SECOND),
+				Arguments.of(makeListFromIntegers(1,2,3,4,5,6),makeListFromIntegers(1,2,3,4,7,8),LottoRank.THIRD),
+				Arguments.of(makeListFromIntegers(1,2,3,4,5,6),makeListFromIntegers(1,2,3,7,8,9),LottoRank.FOURTH),
+				Arguments.of(makeListFromIntegers(1,2,3,4,5,6),makeListFromIntegers(1,2,7,8,9,10),null)
+		);
+	}
+
+	private static List<Integer> makeListFromIntegers(Integer... ints){
+		return Arrays.stream(ints)
+				.collect(Collectors.toList());
+	}
 }
