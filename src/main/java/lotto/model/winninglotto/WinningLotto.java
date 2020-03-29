fbcfdc29a0000d413b81c50f53aa53fb4cfa3783
LeeSeungHee <palmseung@gmail.com>
2020-03-30 05:40:11 +0900

    docs (README) : Modify README.md

--- src/main/java/lotto/model/winninglotto/WinningLotto.java
+++ src/main/java/lotto/model/winninglotto/WinningLotto.java
@@ -1,5 +1,9 @@
 package lotto.model.winninglotto;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class WinningLotto {
     private WinningLottoNumbers winningLottoNumbers;
     private BonusBall bonusBall;
