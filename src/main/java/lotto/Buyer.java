070e9af9eb39334f5c1f842e0de9acb00ac706f3
yohan <yohan.lee@uneedcomms.com>
2020-03-29 13:03:32 +0900

    feat: Add Buyer

---
+++ src/main/java/lotto/Buyer.java
@@ -0,0 +1,47 @@
+package lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.LottoRule.*;
+
+public class Buyer {
+    private List<LottoTicket> lottoTickets;
+
+    public Buyer() {
+    }
+
+    public Buyer(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public List<LottoTicket> buyLottoTickets(int money) {
+        lottoTickets = LottoMachine.pay(money);
+        return lottoTickets;
+    }
+
+    public BuyerResult getResult(LottoTicket winningTicket) {
+        List<WINNING_VALUE> winningValues = getWinningValues(winningTicket);
+        double profitRate = getProfitRate(winningValues);
+        return new BuyerResult(winningValues, profitRate);
+    }
+
+    private double getProfitRate(List<WINNING_VALUE> winningValues) {
+        long winningAmountSum = winningValues.stream()
+                .mapToLong(WINNING_VALUE::getAmount)
+                .sum();
+        double profitRate = ((double) winningAmountSum) / (lottoTickets.size() * getPrice());
+        return Math.round(profitRate * 100) / 100.0;
+    }
+
+    private List<WINNING_VALUE> getWinningValues(LottoTicket winningTicket) {
+        List<Integer> winningMatchCounts = lottoTickets.stream()
+                .map(lottoTicket -> lottoTicket.compareTo(winningTicket))
+                .filter(matchCount -> matchCount >= getWinningMinCount())
+                .collect(Collectors.toList());
+
+        return winningMatchCounts.stream()
+                .map(WINNING_VALUE::findByMatchCount)
+                .collect(Collectors.toList());
+    }
+}
--- src/test/java/lotto/BuyerTest.java
+++ src/test/java/lotto/BuyerTest.java
@@ -1,6 +1,5 @@
 package lotto;
 
-import lotto.LottoRule.WINNING_VALUE;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
