45cb76a7c62d5aacca7674729a61edd983624a3c
etff <wish2rich@gmail.com>
2020-03-29 22:20:47 +0900

    feature(InputView): add lotto count test

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,10 +2,18 @@ package lotto;
 
 public class InputView {
     private static final int MIN_PRICE = 1000;
+    private static final int LOTTO_PRICE = 1000;
+
+    private final int lottoCount;
     private final int price;
 
     public InputView(int price) {
         this.price = valdatePrice(price);
+        lottoCount = calculateLottoCount(price);
+    }
+
+    public int getLottoCount() {
+        return lottoCount;
     }
 
     private int valdatePrice(int price) {
@@ -14,5 +22,9 @@ public class InputView {
         }
 
         throw new IllegalArgumentException("1000원보다 큰 값을 입력해야합니다.");
+    }
+
+    private int calculateLottoCount(int price) {
+        return price / LOTTO_PRICE;
     }
 }
--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,15 +1,16 @@
 package lotto;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class InputViewTest {
+
+    private
 
     @DisplayName("사용자의 입력을 받을 수 있다.")
     @Test
@@ -23,5 +24,13 @@ public class InputViewTest {
     void testPriceMoreThanThousand(final int price) {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new InputView(price));
+    }
+
+    @DisplayName("사용자가 구매한 로또의 갯수를 구할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {14000})
+    void testLottoCount(final int price) {
+        InputView inputView = new InputView(price);
+        assertThat(inputView.getLottoCount()).isSameAs(14);
     }
 }
