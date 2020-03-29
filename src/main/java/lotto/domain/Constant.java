8873ecebac28d30ee68473323420bd6286d1e2fa
jwee0330 <jwee0330@gmail.com>
2020-03-30 02:47:58 +0900

    refactor(Constant): 게임당 가격을 의미하는 필드의 이름을 직관적으로 수정

--- src/main/java/lotto/domain/Constant.java
+++ src/main/java/lotto/domain/Constant.java
@@ -3,7 +3,7 @@ package lotto.domain;
 public class Constant {
     public static final int ZERO_INDEX = 0;
 
-    public static final int DEFAULT_GAME_PRICE = 1000;
+    public static final int PRICE_PER_GAME = 1_000;
 
     public static final int LOTTO_NUM_MIN = 1;
     public static final int LOTTO_NUM_MAX = 45;
