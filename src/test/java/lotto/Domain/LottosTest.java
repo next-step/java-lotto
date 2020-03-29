926e55e73eb12cae22eee9fe739e508f7bd1e36d
ookhub <damlook@naver.com>
2020-03-30 02:43:13 +0900

    feat(Lottos): add Lottos test

---
+++ src/test/java/lotto/Domain/LottosTest.java
@@ -0,0 +1,51 @@
+package lotto.Domain;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+public class LottosTest {
+
+    @Test
+    void matchTest() {
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 13, 4, 5, 6);
+        Lotto lotto = Lotto.init(lottoNumbers);
+        List<Lotto> lottoList = Arrays.asList(lotto, lotto);
+        Lottos lottos = Lottos.init(lottoList);
+        List<Integer> winningNumber = new ArrayList<>(Arrays.asList(1, 2, 13, 24, 35, 42));
+
+        Map<Integer, Integer> matchCountMap = lottos.match(winningNumber);
+
+        assertThat(matchCountMap)
+                .hasSize(4)
+                .contains(entry(3, 2), entry(4, 0), entry(5, 0), entry(6, 0));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 2 13 4 5 6:5", "13 24 1 5 42 7:50", "1 2 13 35 24 42:2000000"}, delimiter = ':')
+    void statisticsTest(String input, String expected) {
+        List<Integer> lottoNumbers = Arrays.stream(input.split(" "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        Lotto lotto = Lotto.init(lottoNumbers);
+        List<Lotto> lottoList = Arrays.asList(lotto, lotto);
+        Lottos lottos = Lottos.init(lottoList);
+        List<Integer> winningNumber = new ArrayList<>(Arrays.asList(1, 2, 13, 24, 35, 42));
+
+        Map<Integer, Integer> matchCountMap = lottos.match(winningNumber);
+        Double statistics = lottos.statistics(matchCountMap);
+
+        assertThat(statistics).isEqualTo(Double.parseDouble(expected));
+    }
+}
