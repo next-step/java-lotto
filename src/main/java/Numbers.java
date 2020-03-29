f803b55f399f7e715b9de343fdea4c61bc4a8a8d
kks <rudtjs9956@gmail.com>
2020-03-29 21:36:34 +0900

    feat(Numbers): add sum

---
+++ src/main/java/Numbers.java
@@ -0,0 +1,33 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Numbers {
+    private final List<Integer> numbers;
+
+    public Numbers(List<Integer> numbers) {
+        this.numbers = Collections.unmodifiableList(numbers);
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Numbers numbers1 = (Numbers) o;
+        return Objects.equals(numbers, numbers1.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+}
