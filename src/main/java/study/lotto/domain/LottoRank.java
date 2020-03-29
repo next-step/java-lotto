c0b89caadf3d85de28d6180e8945037b9f46493a
luvram <sahul0804@gmail.com>
2020-03-29 17:52:36 +0900

    refactor(lotto rank): 일치하는 숫자와 Rank가 연결되도록 수정

--- src/main/java/study/lotto/domain/LottoRank.java
+++ src/main/java/study/lotto/domain/LottoRank.java
@@ -1,18 +1,45 @@
 package study.lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public enum LottoRank {
-    FIRST(2000000000),
-    SECOND(1500000),
-    THIRD(50000),
-    FOURTH(5000);
+    FIRST(2000000000, 6),
+    SECOND(1500000, 5),
+    THIRD(50000, 4),
+    FOURTH(5000, 3);
+
+    private static Map<Integer, LottoRank> map = new HashMap<>();
 
     private int prize;
+    private int matchCount;
 
-    LottoRank(int prize) {
+    LottoRank(int prize, int matchCount) {
         this.prize = prize;
+        this.matchCount = matchCount;
+    }
+
+    static {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            map.put(lottoRank.matchCount, lottoRank);
+        }
+    }
+
+    public static LottoRank matchCountOf(Integer matchCount) {
+        LottoRank lottoRank = map.get(matchCount);
+        if (Objects.isNull(lottoRank)) {
+            return null;
+        }
+
+        return lottoRank;
     }
 
     public int getPrize() {
         return prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 }
--- src/main/java/study/lotto/domain/LottoRule.java
+++ src/main/java/study/lotto/domain/LottoRule.java
@@ -23,16 +23,6 @@ public class LottoRule {
                                            LottoWinningNumber lottoWinningNumber) {
         List<LottoNumber> matches = lottoWinningNumber.getMatches(lottoTicket);
 
-        if(matches.size() == 3) {
-            return LottoRank.FOURTH;
-        } else if(matches.size() == 4) {
-            return LottoRank.THIRD;
-        } else if(matches.size() == 5) {
-            return LottoRank.SECOND;
-        } else if(matches.size() == 6) {
-            return LottoRank.FIRST;
-        }
-
-        return null;
+        return LottoRank.matchCountOf(matches.size());
     }
 }
