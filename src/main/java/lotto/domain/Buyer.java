5e63e159e883b2b78b112b44bb85a155c7e553eb
yohan <yohan.lee@uneedcomms.com>
2020-03-30 03:01:27 +0900

    feedback(BuyResult): Buyer의 로또 계산 역할 -> BuyerResult 에게 분리
    - Buyer는 결과를 알릴 책임이 있다고 생각하여 설계하였지만 통계와 같은 행위들을
    거쳐야 한다는 것은 Buyer에게는 조금 무거운 책임이지 아니었나 생각이 듭니다!
    리뷰 주신 것처럼 BuyerResult에게 그 역할을 맡겨서 생성한다면 적절한 분리가 될 것 같습니다!
    감사합니다!

--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Buyer {
     private List<LottoTicket> lottoTickets;
@@ -19,27 +18,6 @@ public class Buyer {
     }
 
     public BuyerResult getResult(LottoTicket winningTicket) {
-        List<Rank> winningValues = getWinningValues(winningTicket);
-        double profitRate = getProfitRate(winningValues);
-        return new BuyerResult(winningValues, profitRate);
-    }
-
-    private double getProfitRate(List<Rank> winningValues) {
-        long winningAmountSum = winningValues.stream()
-                .mapToLong(Rank::getAmount)
-                .sum();
-        double profitRate = ((double) winningAmountSum) / (lottoTickets.size() * LottoTicket.PRICE);
-        return Math.round(profitRate * 100) / 100.0;
-    }
-
-    private List<Rank> getWinningValues(LottoTicket winningTicket) {
-        List<Integer> winningMatchCounts = lottoTickets.stream()
-                .map(lottoTicket -> lottoTicket.compareTo(winningTicket))
-                .filter(matchCount -> matchCount >= LottoTicket.WINNING_MIN_COUNT)
-                .collect(Collectors.toList());
-
-        return winningMatchCounts.stream()
-                .map(Rank::findByMatchCount)
-                .collect(Collectors.toList());
+        return new BuyerResult(lottoTickets, winningTicket);
     }
 }
--- src/main/java/lotto/domain/BuyerResult.java
+++ src/main/java/lotto/domain/BuyerResult.java
@@ -1,14 +1,34 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BuyerResult {
-    private List<Rank> winningResult;
-    private double profitRate;
+    private final List<Rank> winningResult;
+    private final double profitRate;
 
-    public BuyerResult(List<Rank> winningResult, double profitRate) {
-        this.winningResult = winningResult;
-        this.profitRate = profitRate;
+    public BuyerResult(List<LottoTicket> lottoTickets, LottoTicket winningTicket) {
+        winningResult = createWinningResult(lottoTickets, winningTicket);
+        profitRate = createProfitRate(lottoTickets);
+    }
+
+    private List<Rank> createWinningResult(List<LottoTicket> lottoTickets, LottoTicket winningTicket) {
+        List<Integer> winningMatchCounts = lottoTickets.stream()
+                .map(lottoTicket -> lottoTicket.compareTo(winningTicket))
+                .filter(matchCount -> matchCount >= LottoTicket.WINNING_MIN_COUNT)
+                .collect(Collectors.toList());
+
+        return winningMatchCounts.stream()
+                .map(Rank::findByMatchCount)
+                .collect(Collectors.toList());
+    }
+
+    private double createProfitRate(List<LottoTicket> lottoTickets) {
+        long winningAmountSum = winningResult.stream()
+                .mapToLong(Rank::getAmount)
+                .sum();
+        double profitRate = ((double) winningAmountSum) / (lottoTickets.size() * LottoTicket.PRICE);
+        return Math.round(profitRate * 100) / 100.0;
     }
 
     public List<Rank> getWinningResult() {
