b07c97c9de07dce58b7ddafed68eb28fc74d1ac1
ohtaeg <otk1090@naver.com>
2020-03-29 17:39:21 +0900

    refactor (LottoResult) : 수익률 계산 로직 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,30 +1,50 @@
 package lotto.domain;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LottoResult {
+    private static final int DEFAULT_MATCH_COUNT = 0;
+    private static final int ONE_HUNDRED_PERCENT = 100;
+
     private final Map<LottoRank, Integer> chart;
 
-    {
-        chart = new HashMap<>();
-        chart.put(LottoRank.BLANK, 0);
-        chart.put(LottoRank.FOURTH, 0);
-        chart.put(LottoRank.THIRD, 0);
-        chart.put(LottoRank.SECOND, 0);
-        chart.put(LottoRank.FIRST, 0);
+    public LottoResult() {
+        this(initChart());
+    }
+
+    public LottoResult(final Map<LottoRank, Integer> chart) {
+        this.chart = chart;
+    }
+
+    private static Map<LottoRank, Integer> initChart() {
+        Map<LottoRank, Integer> chart = new HashMap<>();
+        chart.put(LottoRank.BLANK, DEFAULT_MATCH_COUNT);
+        chart.put(LottoRank.FOURTH, DEFAULT_MATCH_COUNT);
+        chart.put(LottoRank.THIRD, DEFAULT_MATCH_COUNT);
+        chart.put(LottoRank.SECOND, DEFAULT_MATCH_COUNT);
+        chart.put(LottoRank.FIRST, DEFAULT_MATCH_COUNT);
+        return chart;
     }
 
     public void increase(LottoRank lottoRank) {
         chart.put(lottoRank, chart.get(lottoRank) + 1);
     }
 
-    public int getMatchCount(LottoRank lottoRank) {
+    public int wihCount(LottoRank lottoRank) {
         return chart.get(lottoRank);
     }
 
-    public Map<LottoRank, Integer> getChart() {
-        return Collections.unmodifiableMap(chart);
+    public double ratio(Price price) {
+        Price total = new Price(Price.MIN_PRICE);
+        for( Map.Entry<LottoRank, Integer> lottoResult : chart.entrySet()) {
+            int matchCount = lottoResult.getValue();
+            Price rankPrice = lottoResult.getKey()
+                                         .getRankPrice();
+            Price winPrice = rankPrice.calculateWinPrice(matchCount);
+            total = total.sum(winPrice);
+        }
+
+        return Math.floor(total.divide(price) * ONE_HUNDRED_PERCENT) / (double) ONE_HUNDRED_PERCENT;
     }
 }
