593eb83aecbc10954942ab2a3a20ac75eba5e707
jwee0330 <jwee0330@gmail.com>
2020-03-30 02:25:46 +0900

    refactor(AutoLottoNumberStrategy): 자동 번호 생성 전략의 테스트를 리팩토링

--- src/test/java/lotto/infrastructure/AutoLottoNumberStrategyTest.java
+++ src/test/java/lotto/infrastructure/AutoLottoNumberStrategyTest.java
@@ -1,15 +1,13 @@
 package lotto.infrastructure;
 
 import lotto.domain.LottoTicket;
-import lotto.infrastructure.AutoLottoNumberStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static lotto.domain.Constant.*;
+import static lotto.domain.Constant.LOTTO_NUM_COUNT_LIMIT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AutoLottoNumberStrategyTest {
-
     private static final int SIX = LOTTO_NUM_COUNT_LIMIT;
 
     @DisplayName("로또 번호를 자동으로 생성할 수 있다")
@@ -17,9 +15,6 @@ class AutoLottoNumberStrategyTest {
     public void generateLottoNumbersTest() {
         LottoTicket one = new AutoLottoNumberStrategy().get();
         LottoTicket another = new AutoLottoNumberStrategy().get();
-
-        System.out.println(one);
-        System.out.println(another);
 
         assertThat(one.size()).isEqualTo(SIX);
         assertThat(another.size()).isEqualTo(SIX);
