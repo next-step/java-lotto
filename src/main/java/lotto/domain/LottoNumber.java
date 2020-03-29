482f2ad5e1d93dffa24288feaaa0c928f718f3ff
dongchul <sah3122@naver.com>
2020-03-29 18:04:26 +0900

    refactor(Lotto): declare nottonumbers to stream

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,21 +1,15 @@
 package lotto.domain;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumber {
     private static final int ZERO = 0;
     private static final int ONE = 1;
-    private static final List<LottoNumber> lottoNumbers = Arrays.asList(
-            new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5),
-            new LottoNumber(6), new LottoNumber(7), new LottoNumber(8), new LottoNumber(9), new LottoNumber(10),
-            new LottoNumber(11), new LottoNumber(12), new LottoNumber(13), new LottoNumber(14), new LottoNumber(15),
-            new LottoNumber(16), new LottoNumber(17), new LottoNumber(18), new LottoNumber(19), new LottoNumber(20),
-            new LottoNumber(21), new LottoNumber(22), new LottoNumber(23), new LottoNumber(24), new LottoNumber(25),
-            new LottoNumber(26), new LottoNumber(27), new LottoNumber(28), new LottoNumber(29), new LottoNumber(30),
-            new LottoNumber(31), new LottoNumber(32), new LottoNumber(33), new LottoNumber(34), new LottoNumber(35),
-            new LottoNumber(36), new LottoNumber(37), new LottoNumber(38), new LottoNumber(39), new LottoNumber(40),
-            new LottoNumber(41), new LottoNumber(42), new LottoNumber(43), new LottoNumber(44), new LottoNumber(45)
-    );
+    private static final List<LottoNumber> lottoNumbers = IntStream.rangeClosed(1, 45)
+            .mapToObj(LottoNumber::new)
+            .collect(Collectors.toList());
 
     private final int number;
 
