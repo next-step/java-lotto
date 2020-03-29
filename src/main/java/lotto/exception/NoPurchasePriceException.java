578711de5f17b786a2ffaf5b143524a70a9efdab
ohtaeg <otk1090@naver.com>
2020-03-29 16:44:12 +0900

    refactor (NoPurchasePriceException) : 파라미터 제거

--- src/main/java/lotto/exception/NoPurchasePriceException.java
+++ src/main/java/lotto/exception/NoPurchasePriceException.java
@@ -1,9 +1,9 @@
 package lotto.exception;
 
 public class NoPurchasePriceException extends RuntimeException {
-    private static final String EXCEPTION_MESSAGE = "로또 구매 가능한 금액은 천원 이상 입니다. : %d";
+    private static final String EXCEPTION_MESSAGE = "로또 구매 가능한 금액은 천원 이상 입니다.";
 
-    public NoPurchasePriceException(final int causativeValue) {
-        super(String.format(EXCEPTION_MESSAGE, causativeValue));
+    public NoPurchasePriceException() {
+        super(EXCEPTION_MESSAGE);
     }
 }
