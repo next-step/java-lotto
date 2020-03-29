f8da7b2ff6c69628b56ab8f73b166fe4d45a76a5
ohtaeg <otk1090@naver.com>
2020-03-29 16:50:30 +0900

    test (Lotto) : 로또를 구입할 수 없는 경우 커스텀 예외 발생 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import lotto.exception.NoPurchasePriceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class LottoTest {
 
@@ -24,5 +28,14 @@ class LottoTest {
         Lotteries actual = lotto.buy(price);
 
         assertThat(actual.count()).isEqualTo(expect);
+    }
+
+    @DisplayName("구입 금액이 로또 금액보다 작을 경우 NoPurchasePriceException 을 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 999})
+    void noPurchaseException(int price) {
+        assertThatExceptionOfType(NoPurchasePriceException.class).isThrownBy(
+                () -> lotto.buy(new Price(price))
+        );
     }
 }
