7d404dc60883f01b6992026d8424615f0544cdcb
ohtaeg <otk1090@naver.com>
2020-03-29 14:37:03 +0900

    refactor (LottoRank) : 등수 찾는 메서드 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -2,17 +2,26 @@ package lotto.domain;
 
 public enum LottoRank {
     BLANK(0, 0)
-    , FOURTH(4, 5_000)
-    , THIRD(3, 50_000)
-    , SECOND(2, 1_500_000)
-    , FIRST(1, 2_000_000_000)
+    , FOURTH(3, 5_000)
+    , THIRD(4, 50_000)
+    , SECOND(5, 1_500_000)
+    , FIRST(6, 2_000_000_000)
     ;
 
-    private int rank;
+    private int matchCount;
     private int prizeMoney;
 
-    LottoRank(final int rank, final int prizeMoney) {
-        this.rank = rank;
+    LottoRank(final int matchCount, final int prizeMoney) {
+        this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+    }
+
+    public static LottoRank findRank(int matchCount) {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            if (lottoRank.matchCount == matchCount) {
+                return lottoRank;
+            }
+        }
+        return LottoRank.BLANK;
     }
 }
