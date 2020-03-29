7e27042f3c0a0ce3ff3a92c3967388ee9b9a44b5
kesol0328 <kesol0328@gmail.com>
2020-03-29 18:13:44 +0900

    (feat) null 또는 빈문자 체크 로직 구현

---
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -0,0 +1,24 @@
+package stringaddcalculator;
+
+public class StringAddCalculator {
+    private int result;
+    private static final int ZERO = 0;
+
+    public StringAddCalculator() {
+        this.result = ZERO;
+    }
+
+    public int splitAndSum(String inputString) {
+        if (isNullOrEmpty(inputString)) {
+            return ZERO;
+        }
+        return 1;
+    }
+
+    private boolean isNullOrEmpty(String inputString) {
+        if(inputString == null || inputString.equals("")) {
+            return true;
+        }
+        return false;
+    }
+}
