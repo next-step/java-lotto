1205939136051c85e44279188f0465c529389334
feel-coding-republic <feel.coding.republic@gmail.com>
2020-03-30 01:13:50 +0900

    fix:Lotto.java Set적용, 1~45 생성문제는 문의드림

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -1,15 +1,13 @@
 package step2.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private static final int LOTTO_START_NUM = 1;
     private static final int LOTTO_END_NUM = 45;
     private static final int SELECT_NUMBER = 6;
     private int price = 1000;
-    private List<Integer> lottoList;
+    private Set lottoList;
 
     public Lotto() {
         getLottoNumberList();
@@ -19,12 +17,12 @@ public class Lotto {
         return price;
     }
 
-    public List<Integer> getLottoList() {
+    public Set getLottoList() {
         return lottoList;
     }
 
     public void getLottoNumberList() {
-        List<Integer> lottoList = new ArrayList<Integer>();
+        Set lottoList = new HashSet<>();
         for (int i = LOTTO_START_NUM; i <= LOTTO_END_NUM; i++) {
             lottoList.add(i);
         }
@@ -32,6 +30,7 @@ public class Lotto {
     }
 
     public List<Integer> extractLottoNumber() {
+        List lottoList = new ArrayList(this.lottoList);
         List<Integer> lottoNumberList = lottoList;
         Collections.shuffle(lottoNumberList);
         List<Integer> randomNumberList = lottoNumberList.subList(0, SELECT_NUMBER);
