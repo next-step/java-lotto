573aefcf4451f88e4173cd70d285cc01ca270695
ohtaeg <otk1090@naver.com>
2020-03-29 15:40:45 +0900

    refactor (LottoResult) : 수익률을 계산하려다보니 매개변수로 구입금액을 받아야 하기 때문에 당첨 금액도 Price 객체로 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -3,19 +3,19 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    BLANK(0, 0)
-    , FOURTH(3, 5_000)
-    , THIRD(4, 50_000)
-    , SECOND(5, 1_500_000)
-    , FIRST(6, 2_000_000_000)
+    BLANK(0,    new Price(0))
+    , FOURTH(3, new Price(5_000))
+    , THIRD(4,  new Price(50_000))
+    , SECOND(5, new Price(1_500_000))
+    , FIRST(6,  new Price(2_000_000_000))
     ;
 
     private int matchCount;
-    private int prizeMoney;
+    private Price winPrice;
 
-    LottoRank(final int matchCount, final int prizeMoney) {
+    LottoRank(final int matchCount, final Price winPrice) {
         this.matchCount = matchCount;
-        this.prizeMoney = prizeMoney;
+        this.winPrice = winPrice;
     }
 
     public static LottoRank findRank(int matchCount) {
@@ -23,5 +23,9 @@ public enum LottoRank {
                      .filter(lottoRank -> lottoRank.matchCount == matchCount)
                      .findFirst()
                      .orElse(LottoRank.BLANK);
+    }
+
+    public Price getRankPrice() {
+        return winPrice;
     }
 }
