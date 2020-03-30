cb4ce898b52321b0436d2bff8ca8d4d7c427a291
wholemann <wholeman.dev@gmail.com>
2020-03-30 15:32:14 +0900

    refactor(calculator): sum, split을 private메서드로 만든다

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,19 +1,19 @@
 public class StringAddCalculator {
 
-    public static String[] split(String input) {
+    public static int sumAndSplit(String input) {
+        return sum(split(input));
+    }
+
+    private static String[] split(String input) {
         return input.split(",|:");
     }
 
-    public static int sum(String[] strings) {
+    private static int sum(String[] strings) {
         int sum = 0;
         for (String num : strings) {
             sum += Integer.parseInt(num);
         }
 
         return sum;
-    }
-
-    public static int sumAndSplit(String input) {
-        return sum(split(input));
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -6,30 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringAddCalculatorTest {
 
     @Test
-    @DisplayName("입력된 문자열을 콤마를 기준으로 구분하여 나누어진 문자열 배열로 리턴한다")
-    public void splitByCommaReturnsArrayOfStrings() {
-        String[] result = StringAddCalculator.split("1,2");
-
-        assertThat(result).isEqualTo(new String[]{"1", "2"});
-    }
-
-    @Test
-    @DisplayName("입력된 문자열을 콜론을 기준으로 구분하여 나누어진 문자열 배열로 리턴한다")
-    public void splitByColonReturnsArrayOfStrings() {
-        String[] result = StringAddCalculator.split("1:2");
-
-        assertThat(result).isEqualTo(new String[]{"1", "2"});
-    }
-
-    @Test
-    @DisplayName("입력된 문자열 배열에 있는 숫자들의 합을 구한다")
-    public void sumReturnsSumOfNumbers() {
-        int result = StringAddCalculator.sum(new String[]{"1", "2"});
-
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
     @DisplayName("콤마 또는 콜론 구분자를 기준으로 분리한 각 숫자의 합을 리턴한다")
     public void sumAndSplitReturnsSumOfNumbersInString() {
         int result = StringAddCalculator.sumAndSplit("1,2:3");
