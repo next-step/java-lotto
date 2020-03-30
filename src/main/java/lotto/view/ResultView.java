1e0fc0d69ec56cd7f36f0e342108c3399420e757
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-30 14:11:02 +0900

    fix(ResultView) : 3등 맞춘 개수 표기 안하는 버그 수정

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -14,7 +14,7 @@ public class ResultView {
     public static void printResult(LottoDto dto) {
         System.out.println("3개 일치 (5000원)- " + dto.getFifthGameCount() + "개");
         System.out.println("4개 일치 (50000원)- " + dto.getFourthGameCount() + "개");
-        System.out.println("5개 일치 (1500000원)- " + dto.getSecondGameCount() + "개");
+        System.out.println("5개 일치 (1500000원)- " + dto.getThirdGameCount() + "개");
         System.out.println("5개 일치, 보너스 볼 일치 (30000000원)- " + dto.getSecondGameCount() + "개");
         System.out.println("6개 일치 (2000000000원)- " + dto.getFirstGameCount() + "개");
     }
