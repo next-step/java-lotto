1612b6cd18b5c204b71f8e06eef75d00e4dbdca4
ohtaeg <otk1090@naver.com>
2020-03-30 20:15:30 +0900

    test (Lotteries) : 구매한 복권들 중 보너스볼을 갖고있는지 테스트 추가

--- src/test/java/lotto/domain/LotteriesTest.java
+++ src/test/java/lotto/domain/LotteriesTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,13 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LotteriesTest {
+
+    private BonusBall bonusBall;
+
+    @BeforeEach
+    void setUp() {
+        bonusBall = new BonusBall("1");
+    }
 
     @DisplayName("로또 복권은 6개의 로또 번호를 가지도록 객체 생성을 할 수 있다.")
     @Test
@@ -38,7 +46,7 @@ class LotteriesTest {
         LottoNumbers winningLottoNumber = new LottoNumbers("7, 8, 9, 10, 11, 12");
         int expect = 1;
 
-        LottoResult actual = lotteries.analyzeWin(winningLottoNumber);
+        LottoResult actual = lotteries.analyzeWin(winningLottoNumber, bonusBall);
 
         assertThat(actual.wihCount(LottoRank.BLANK)).isEqualTo(expect);
     }
@@ -50,7 +58,7 @@ class LotteriesTest {
         LottoNumbers winningLottoNumber = new LottoNumbers("1, 2, 3, 10, 11, 12");
         int expect = 1;
 
-        LottoResult actual = lotteries.analyzeWin(winningLottoNumber);
+        LottoResult actual = lotteries.analyzeWin(winningLottoNumber, bonusBall);
 
         assertThat(actual.wihCount(LottoRank.FOURTH)).isEqualTo(expect);
     }
@@ -62,7 +70,7 @@ class LotteriesTest {
         LottoNumbers winningLottoNumber = new LottoNumbers("1, 2, 3, 4, 11, 12");
         int expect = 1;
 
-        LottoResult actual = lotteries.analyzeWin(winningLottoNumber);
+        LottoResult actual = lotteries.analyzeWin(winningLottoNumber, bonusBall);
 
         assertThat(actual.wihCount(LottoRank.THIRD)).isEqualTo(expect);
     }
@@ -74,7 +82,7 @@ class LotteriesTest {
         LottoNumbers winningLottoNumber = new LottoNumbers("1, 2, 3, 4, 5, 12");
         int expect = 1;
 
-        LottoResult actual = lotteries.analyzeWin(winningLottoNumber);
+        LottoResult actual = lotteries.analyzeWin(winningLottoNumber, bonusBall);
 
         assertThat(actual.wihCount(LottoRank.SECOND)).isEqualTo(expect);
     }
@@ -86,11 +94,30 @@ class LotteriesTest {
         LottoNumbers winningLottoNumber = new LottoNumbers("1, 2, 3, 4, 5, 6");
         int expect = 1;
 
-        LottoResult actual = lotteries.analyzeWin(winningLottoNumber);
+        LottoResult actual = lotteries.analyzeWin(winningLottoNumber, bonusBall);
 
         assertThat(actual.wihCount(LottoRank.FIRST)).isEqualTo(expect);
     }
 
+    @DisplayName("보너스볼을 갖고 있다.")
+    @Test
+    void hasBonusBall_True() {
+        LottoNumbers lottoNumbers = new LottoNumbers("1, 2, 3, 4, 5, 6");
+
+        boolean hasBonusBall = lottoNumbers.hasBonusBall(bonusBall);
+
+        assertThat(hasBonusBall).isTrue();
+    }
+
+    @DisplayName("보너스볼을 갖고 있지 않다.")
+    @Test
+    void hasBonusBall_False() {
+        LottoNumbers lottoNumbers = new LottoNumbers("2, 3, 4, 5, 6, 7");
+
+        boolean hasBonusBall = lottoNumbers.hasBonusBall(bonusBall);
+
+        assertThat(hasBonusBall).isFalse();
+    }
 
     private List<LottoNumbers> createLottoNumbers() {
         List<LottoNumbers> lottoNumbers = new ArrayList<>();
