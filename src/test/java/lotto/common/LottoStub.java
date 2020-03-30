b1783d7e8735e3ca3bf67ccdb577f63e4439347a
jwee0330 <jwee0330@gmail.com>
2020-03-31 02:21:14 +0900

    refactor(LottoStub): 테스트에 필요한 stub 만 남기고 사용하지 않는 feature 메소드는 삭제한다

--- src/test/java/lotto/common/LottoStub.java
+++ src/test/java/lotto/common/LottoStub.java
@@ -1,9 +1,9 @@
 package lotto.common;
 
-import lotto.domain.*;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoNumbers;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoStub {
@@ -16,30 +16,9 @@ public class LottoStub {
                 new LottoNumber(4), new LottoNumber(5), new LottoNumber(6)));
     }
 
-    public static LottoNumbers getLottoNumbersTwoToSeven() {
-        return new LottoNumbers(Arrays.asList(new LottoNumber(2), new LottoNumber(3), new LottoNumber(4),
-                new LottoNumber(5), new LottoNumber(6), new LottoNumber(7)));
-    }
-
     public static LottoNumbers getLottoNumbers(int... number) {
         return new LottoNumbers(Arrays.stream(number)
                 .mapToObj(LottoNumber::new).collect(Collectors.toList()));
     }
 
-
-    public static LottoTicket getLottoTicket(LottoNumbers lottoNumbers) {
-        return new LottoTicket(lottoNumbers);
-    }
-
-    public static LottoTickets getLottoTickets(LottoTicket... lottoTickets) {
-        return new LottoTickets(Arrays.asList(lottoTickets));
-    }
-
-    public static LottoTickets getLottoTickets(List<LottoTicket> lottoTickets) {
-        return new LottoTickets(lottoTickets);
-    }
-
-    public static WinningTicket getWinningTicket(LottoNumbers lottoNumbers, int bonusNumber) {
-        return new WinningTicket(lottoNumbers, bonusNumber);
-    }
 }
