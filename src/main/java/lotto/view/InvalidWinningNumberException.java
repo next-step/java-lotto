480103bf55d8f55d40403513634ad7e19f64e4bf
jwee0330 <jwee0330@gmail.com>
2020-03-31 03:29:23 +0900

    chore(step3): 리팩토링으로 더이상 사용하지 않는 클래스 파일을 제거한다

--- src/main/java/lotto/view/InvalidWinningNumberException.java
+++
@@ -1,7 +0,0 @@
-package lotto.view;
-
-public class InvalidWinningNumberException extends IllegalArgumentException {
-    public InvalidWinningNumberException(String message) {
-        super("당첨번호가 올바르지 않습니다. 원인: " + message);
-    }
-}
