d3bed95f9ee939ce53797d931c86b33766201556
luvram <sahul0804@gmail.com>
2020-03-31 00:44:32 +0900

    feat(view): 보너스 번호를 입력받는다

--- src/main/java/study/lotto/view/InputView.java
+++ src/main/java/study/lotto/view/InputView.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+
 
     public static int getAmount() {
         Scanner scanner = new Scanner(System.in);
@@ -22,5 +24,11 @@ public class InputView {
         return Arrays.stream(input.split(","))
                 .map(number -> Integer.parseInt(number.trim()))
                 .collect(Collectors.toList());
+    }
+
+    public static int getBonusNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        return scanner.nextInt();
     }
 }
