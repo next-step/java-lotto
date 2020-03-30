9506cfcb26b74c4096b7a2aaa0e578309fdf36e8
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-30 13:31:29 +0900

    refactor(LottoNumberGenerator) : 0~45 숫자 범위를 생성후 공유 한다

--- src/test/java/lotto/domain/stragegy/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/stragegy/LottoNumberGeneratorTest.java
@@ -1,5 +1,6 @@
 package lotto.domain.stragegy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberGeneratorTest {
 
+    @DisplayName("1~45 사이의 랜덤 로또 번호를 생성 한다.")
     @Test
     public void generateLottoNumber_success() throws Exception {
         //when
@@ -15,5 +17,16 @@ class LottoNumberGeneratorTest {
 
         //then
         assertThat(integers.size()).isEqualTo(6);
+    }
+
+    @DisplayName("랜덤번호 2개를 생성후 비교하여 서로 달라야 한다.")
+    @Test
+    public void generateLottoNumber_fail_compareResult() throws Exception {
+        //when
+        List<Integer> compare1 = LottoNumberGenerator.generateLottoNumber();
+        List<Integer> compare2 = LottoNumberGenerator.generateLottoNumber();
+
+        //then
+        assertThat(compare1.equals(compare2)).isFalse();
     }
 }
