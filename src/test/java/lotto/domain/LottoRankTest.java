c1e20a7e274ec6ec8a8fc72e4c2af5b2aa67097c
jwee0330 <jwee0330@gmail.com>
2020-03-31 02:26:27 +0900

    test(LottoRankTest): 로또 랭크는 번호 일치 개수에 따라 로또 당첨에 대한 사전정보를 확인할 수 있다

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -1,5 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoRankTest {
+
+    @DisplayName("로또 랭크는 로또 당첨에 대한 사전 정보를 확인 할 수 있다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 51, 6})
+    public void getLottoRankInfoTest(int matchCount) {
+        LottoRank lottoRank = LottoRank.of(matchCount);
+
+        assertThat(lottoRank.getMatchCount()).isEqualTo(matchCount);
+    }
 
 }
