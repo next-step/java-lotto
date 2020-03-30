ccd4633df4c358a40120e11a17427c600b12265c
kiyeoneee <kyeoneee@gmail.com>
2020-03-31 02:16:30 +0900

    refactor (StringAddCalculator) : input을 구분자로 분리하는 기능 메소드 분리 * 하나의 메소드의 길이는 10라인 미만이도록 한다

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -7,31 +7,36 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
     private static final String REGEX_FORMAT = "[%s]";
     private static final String DEFAULT_DELIMITERS = ",:";
-    private static final String CUSTOM_FORMAT = "\\/\\/(.*)\\n(.*)";
+    private static final String CUSTOM_FORMAT = "//(.)\n(.*)";
 
     public static int splitAndSum(String input) {
-        int result = 0;
 
         if (isEmptyString(input)) {
-            return result;
+            return 0;
         }
 
-        Pattern pattern = Pattern.compile(CUSTOM_FORMAT);
+        return operate(input);
+    }
 
-        Matcher matcher = pattern.matcher(input);
-        String delimiters = DEFAULT_DELIMITERS;
-        if (matcher.find()) {
-            delimiters += matcher.group(1);
-            input = matcher.group(2);
-        }
+    private static int operate(String input) {
+        int result = 0;
 
-        String[] splitInput = input.split(String.format(REGEX_FORMAT, delimiters));
-
-        for (String operand : splitInput) {
+        for (String operand : splitInput(input)) {
             result += Integer.parseInt(operand);
         }
 
         return result;
+    }
+
+    private static String[] splitInput(String input) {
+        Matcher matcher = Pattern.compile(CUSTOM_FORMAT).matcher(input);
+
+        if (matcher.find()) {
+            String delimiter = String.format(REGEX_FORMAT, matcher.group(1).concat(DEFAULT_DELIMITERS));
+            return matcher.group(2).split(delimiter);
+        }
+
+        return input.split(String.format(REGEX_FORMAT, DEFAULT_DELIMITERS));
     }
 
     private static boolean isEmptyString(String string) {
