de80e32b2959beb56e08a0e8d042fad5aa7d6137
gale34 <gales34@gmail.com>
2020-03-30 20:57:48 +0900

    refactor: edit OutputView

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 public class OutputView {
     private static final String RESULT_FORMAT = "%d개 일치 (%d원) - %d개";
+    private static final String RESULT_WITH_BONUS_FORMAT = "%d개 일치, 보너스 볼 일치(%d원) - %d개";
     private static final String PURCHASE_SHOW_FORMAT = "%d개를 구매했습니다.";
     private static final String PROFIT_SHOW_FORMAT = "총 수익률은 %.2f입니다.";
     private static final String PROFIT_WITH_LOSS_SHOW_FORMAT = PROFIT_SHOW_FORMAT + "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
@@ -29,13 +30,22 @@ public class OutputView {
         printResult.keySet()
                 .stream()
                 .filter(lottoResult -> !lottoResult.isBlank())
-                .forEach(lottoResult -> printWinResult(lottoResult, lottoResults.count(lottoResult)));
+                .forEach(lottoResult -> printEachResult(lottoResult, lottoResults.count(lottoResult)));
         printProfitResult(lottoResults.profit());
     }
 
-    private static void printWinResult(final LottoResult lottoResult, final Long resultCount) {
+    private static void printEachResult(final LottoResult lottoResult, final Long resultCount) {
+        if(lottoResult.equals(LottoResult.FIVE_WITH_BONUS)) {
+            printWinResult(lottoResult, resultCount, RESULT_WITH_BONUS_FORMAT);
+            return ;
+        }
+
+        printWinResult(lottoResult, resultCount, RESULT_FORMAT);
+    }
+
+    private static void printWinResult(final LottoResult lottoResult, final Long resultCount, final String resultFormat) {
         System.out.println(String.format(
-                RESULT_FORMAT,
+                resultFormat,
                 lottoResult.getMatchCount(),
                 lottoResult.getPrice(),
                 resultCount));
