cf70f92864abb8bede1e91c6aa5f44175c232f39
jwee0330 <jwee0330@gmail.com>
2020-03-31 03:28:13 +0900

    refactor(LottoGameController): LottoGameController Refactoring

--- src/main/java/lotto/LottoGameController.java
+++ src/main/java/lotto/LottoGameController.java
@@ -1,25 +1,22 @@
 package lotto;
 
-import lotto.domain.LottoGame;
-import lotto.domain.LottoGameResults;
-import lotto.domain.LottoTickets;
-import lotto.domain.WinningNumber;
+import lotto.domain.*;
+import lotto.infrastructure.AutoLottoNumberStrategy;
 import lotto.view.InputView;
 import lotto.view.ResultView;
-
-import java.util.List;
 
 public class LottoGameController {
 
     public static void main(String[] args) {
-        LottoGame lottoGame = new LottoGame();
-        LottoTickets boughtTickets = lottoGame.buy(InputView.requestMoney());
-        boughtTickets.print();
-
-        List<Integer> lottoNumber = InputView.requestWinningNumber();
-        int bonusNumber = InputView.requestBonusNumber();
-        WinningNumber winningNumber = new WinningNumber(lottoNumber, bonusNumber);
-        LottoGameResults results = lottoGame.getResults(boughtTickets, winningNumber);
-        ResultView.print(results);
+        LottoMoney lottoMoney = new LottoMoney(InputView.requestMoney());
+        LottoGame lottoGame = new LottoGame(lottoMoney);
+        LottoTickets lottoTickets = lottoGame.buy(new AutoLottoNumberStrategy());
+        lottoTickets.print();
+        WinningTicket winningTicket = new WinningTicket(
+                InputView.requestWinningNumbers(),
+                InputView.requestBonusNumber()
+        );
+        LottoGameResults lottoGameResults = lottoGame.match(lottoTickets, winningTicket);
+        ResultView.print(lottoGameResults);
     }
 }
