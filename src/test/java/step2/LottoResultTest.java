c324e5ef12476f306cbeb014c244064aa3c6fa45
Yeoseon Yun <devyyskr@gmail.com>
2020-03-31 19:04:38 +0900

    doc(README): 2등 요구사항을 위한 분석 내용 추가

--- src/test/java/step2/LottoResultTest.java
+++ src/test/java/step2/LottoResultTest.java
@@ -23,6 +23,7 @@ public class LottoResultTest {
         resultMap.put(LottoTier.SECOND, 1);
         resultMap.put(LottoTier.THIRD, 1);
         resultMap.put(LottoTier.FOURTH, 1);
+        resultMap.put(LottoTier.FIFTH, 1);
         resultMap.put(LottoTier.NONE, 1);
 
         lottoResult = new LottoResult(resultMap);
@@ -31,7 +32,7 @@ public class LottoResultTest {
     @Test
     @DisplayName("결과에 맞는 수입금액을 도출하는 지 테스트")
     void getTotalPrize() {
-        assertThat(lottoResult.getTotalPrize()).isEqualTo(2000205000);
+        assertThat(lottoResult.getTotalPrize()).isEqualTo(2030205000);
     }
 
     @Test
