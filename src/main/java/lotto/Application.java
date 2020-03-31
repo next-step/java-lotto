10bd734464ba6d25824d002d29c861fe0f198d7c
ohtaeg <otk1090@naver.com>
2020-04-01 01:08:17 +0900

    refactor (Application) : 수동 갯수와, 수동 로또 번호 입력받도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,11 +4,16 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
 
         Price inputPrice = inputView.inputPrice();
+        int manualCount = inputView.inputManualCount();
+
+        List<LottoNumbers> manualLotteries = inputView.inputManualLottoNumbers(manualCount);
         Lotteries lotteries = LottoStore.sell(inputPrice, new LottoPublisher());
 
         OutputView.printLotteryCount(lotteries.count());
