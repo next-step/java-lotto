faae821dce3be8f6033383b2d08d72c4124cd6b5
ohtaeg <otk1090@naver.com>
2020-04-01 01:06:11 +0900

    refactor (InputView) : 수동 구매 갯수와 수동 로또 번호들을 입력받도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,10 +5,14 @@ import lotto.domain.LottoNumbers;
 import lotto.domain.Price;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MANUAL_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
     private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
 
@@ -37,5 +41,22 @@ public class InputView {
         System.out.println();
         System.out.println(INPUT_BONUS_BALL_MESSAGE);
         return LottoNumber.valueOf(Integer.valueOf(scanner.nextLine()));
+    }
+
+    public int inputManualCount() {
+        System.out.println();
+        System.out.println(INPUT_MANUAL_COUNT_MESSAGE);
+        return Integer.valueOf(scanner.nextLine());
+    }
+
+    public List<LottoNumbers> inputManualLottoNumbers(final int manualCount) {
+        System.out.println();
+        System.out.println(INPUT_MANUAL_LOTTO_NUMBERS_MESSAGE);
+
+        List<LottoNumbers> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < manualCount; i++) {
+            lottoNumbers.add(new LottoNumbers(scanner.nextLine()));
+        }
+        return lottoNumbers;
     }
 }
