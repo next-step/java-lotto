2645c4a596ebb9c167ab7930a18f294eaa3b4e67
jbyiop <jbyiop@gmail.com>
2020-03-31 18:21:19 +0900

    refactor : 문자열 덧셈계산리 리팩토링
    문자열 파싱 부분을 Tokenizer 클래스로 분리한다.
    Calculator 의 sum 메서드는 static 으로 변경한다.
    그에 따른 테스트 코드를 모두 수정한다.
    객체를 생성할 수 없도록 private 생성자를 선언한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,35 +1,22 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Calculator {
-    public String[] split(String text) {
-        String delimiter = ",|:";
-        Pattern pattern = Pattern.compile("//(.)\n(.*)");
-        Matcher matcher = pattern.matcher(text);
 
-        if(matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            text = matcher.group(2);
-            delimiter = delimiter + "|" + customDelimiter;
-        }
+    private Calculator() {}
 
-        return text.split(delimiter);
-    }
-
-    public double sum(String text) {
+    public static double sum(String text) {
         if (text == null || "".equals(text.trim())) {
             return 0;
         }
 
-        return Arrays.stream(split(text))
-                .mapToDouble(this::parseDouble)
+        return Arrays.stream(Tokenizer.split(text))
+                .mapToDouble(Calculator::parseDouble)
                 .sum();
     }
 
-    private double parseDouble(String token) {
+    private static double parseDouble(String token) {
         double number = Double.parseDouble(token);
         if(number < 0) {
             throw new RuntimeException("음수를 입력할 수 없습니다.");
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -13,51 +13,34 @@ public class CalculatorTest {
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
     @Test
     void emptyString() {
-        Calculator calculator = new Calculator();
-
-        double sum1 = calculator.sum("");
+        double sum1 = Calculator.sum("");
         assertThat(sum1).isEqualTo(0);
 
-        double sum2 = calculator.sum(null);
+        double sum2 = Calculator.sum(null);
         assertThat(sum2).isEqualTo(0);
     }
 
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
     @Test
     void singleNumber() {
-        Calculator calculator = new Calculator();
-
-        double sum = calculator.sum("23");
+        double sum = Calculator.sum("23");
         assertThat(sum).isEqualTo(23);
-    }
-
-    @DisplayName("문자열 덧셈 계산기는 ','를 구분자로 하여 각 문자열을 분리할 수 있다")
-    @Test
-    void split() {
-        Calculator calculator = new Calculator();
-        String[] tokens = calculator.split("1,2,3");
-
-        assertThat(tokens).containsExactly("1", "2", "3");
     }
 
     @DisplayName("문자열 덧셈 계산기는 ':'를 구분자로 하여 각 문자열을 분리할 수 있다")
     @Test
     void colonDelimiter() {
-        Calculator calculator = new Calculator();
-
-        double sum1 = calculator.sum("10:25:33");
+        double sum1 = Calculator.sum("10:25:33");
         assertThat(sum1).isEqualTo(68);
 
-        double sum2 = calculator.sum("10:25,33");
+        double sum2 = Calculator.sum("10:25,33");
         assertThat(sum2).isEqualTo(68);
     }
 
     @DisplayName("문자열 덧셈 계산기는 ','를 구분자로 하여 각 문자열을 분리한 후 합을 구할 수 있다")
     @Test
     void sumEachTokens() {
-        Calculator calculator = new Calculator();
-        double sum = calculator.sum("1,2,3,4,5");
-
+        double sum = Calculator.sum("1,2,3,4,5");
         assertThat(sum).isEqualTo(15);
     }
 
@@ -65,25 +48,21 @@ public class CalculatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"1,2,a,b,3", "1,2,5,-10,3", "#,#,#,$,!", "-1,-2,-3", "!@,@#,$#", "e,e,e,e,e"})
     void throwRuntimeExceptionsIfNonNumberOrNegativeNumber(String target) {
-        Calculator calculator = new Calculator();
         assertThatThrownBy(() -> {
-            calculator.sum(target);
+            Calculator.sum(target);
         }).isInstanceOf(RuntimeException.class);
     }
 
     @DisplayName("기본 구분자와 함께 문자열 앞부분의 \"//\"와 \"\\n\" 사이에 위치하는 문자를 커스텀 구분자로 사용할 수 있다")
     @Test
     void splitUsingCustomDelimiter() {
-        Calculator calculator = new Calculator();
-
         String expression1 = "//;\n1;2;3";
-        assertThat(calculator.sum(expression1)).isEqualTo(6);
+        assertThat(Calculator.sum(expression1)).isEqualTo(6);
 
         String expression2 = "//;\n1,2;3;4;5";
-        assertThat(calculator.sum(expression2)).isEqualTo(15);
+        assertThat(Calculator.sum(expression2)).isEqualTo(15);
 
         String expression3 = "//:\n3:4:5,6,7";
-        assertThat(calculator.sum(expression3)).isEqualTo(25);
+        assertThat(Calculator.sum(expression3)).isEqualTo(25);
     }
-
 }
