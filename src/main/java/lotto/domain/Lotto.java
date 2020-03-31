db7b70e0ce235bb685f064ba40addde6a256bd34
lucas <lucas@zoyi.co>
2020-04-01 00:51:46 +0900

    feat: implement Lotto

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,14 +1,32 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lotto {
 
-  public Lotto(List<LottoGame> LottoGames) {
+  private List<LottoGame> lottoGames;
 
+  public Lotto(int autoCount) {
+    this(autoCount, new ArrayList<>());
+  }
+
+  public Lotto(List<LottoGame> lottoGames) {
+    this(0, lottoGames);
+  }
+
+  public Lotto(int autoCount, List<LottoGame> lottoGames) {
+    this.lottoGames = Stream.concat(
+        Stream.generate(LottoGame::new).limit(autoCount),
+        lottoGames.stream()
+    ).collect(Collectors.toList());
   }
 
   public List<Rank> getRanks(WinningNumbers winningNumbers) {
-    return null;
+    return lottoGames.stream()
+        .map(winningNumbers::getRank)
+        .collect(Collectors.toList());
   }
 }
