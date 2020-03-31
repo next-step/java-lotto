fc4aa2bbbdea35ab5822d3df5a5db0161b4220ea
kks <rudtjs9956@gmail.com>
2020-03-31 23:02:35 +0900

    feat(Price): add getAvailableBuyTicketCount

---
+++ src/main/java/lotto/domain/Price.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class Price {
+    private static final int LOTTO_TICKET_PRICE = 1000;
+    private int price;
+
+    public Price(int price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    private void validatePrice(int price) {
+        if (price < 1000) {
+            throw new IllegalArgumentException("금액은 1000원 이상 입력해주세요.");
+        }
+        if (price % LOTTO_TICKET_PRICE != 0) {
+            throw new IllegalArgumentException("금액은 1000원 단위로 입력해주세요.");
+        }
+    }
+
+    public int getAvailableBuyTicketCount() {
+        return price / LOTTO_TICKET_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return price == price1.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+}
