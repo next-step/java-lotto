961d70dad2f29cd77b7e32356c1e8d805dc8296d
jbyiop <jbyiop@gmail.com>
2020-03-31 10:04:32 +0900

    refactor : 리팩토링 과정에서 실 기능 구현
    테스트를 통과하기 위한 목적으로 작성된 코드를 실 사용가능한 코드로 변경한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,15 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
-    public String[] split(String s) {
-        return new String[] {"1", "2", "3"};
+    public String[] split(String text) {
+        return text.split(",");
     }
 
-    public double sum(String s) {
-        return 15;
+    public double sum(String text) {
+        return Arrays.stream(split(text))
+                .mapToDouble(Double::parseDouble)
+                .sum();
     }
 }
