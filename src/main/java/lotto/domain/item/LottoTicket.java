5648b3abfa3405b89d30469904fea6474e22d405
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-31 21:25:51 +0900

    refactor (LottoTicket) : Lotto 리팩토링 과정에서 에러 발생 코드 주석 처리

--- src/main/java/lotto/domain/item/LottoTicket.java
+++ src/main/java/lotto/domain/item/LottoTicket.java
@@ -10,32 +10,32 @@ public class LottoTicket extends Lotto {
         super(numbers);
     }
 
-    public int getLuckyNumberMatchCount(Item winTicket) {
-        int matchCount = 0;
-        for (LottoNumber num : numbers) {
-            matchCount = increaseCountIfMatch(winTicket, matchCount, num);
-        }
+//    public int getLuckyNumberMatchCount(Item winTicket) {
+//        int matchCount = 0;
+//        for (LottoNumber num : numbers) {
+//            matchCount = increaseCountIfMatch(winTicket, matchCount, num);
+//        }
+//
+//        return matchCount;
+//    }
+//
+//    private int increaseCountIfMatch(Item winTicket, int matchCount, LottoNumber winNumber) {
+//        for (LottoNumber number : winTicket.getNumbers()) {
+//            if (number.equals(winNumber)) {
+//                matchCount++;
+//            }
+//        }
+//        return matchCount;
+//    }
+//
+//    private boolean isMatchBonus(Item winTicket) {
+//        LottoNumber bonus = ((WinLottoTicket) winTicket).getBonus();
+//        return numbers.contains(bonus);
+//    }
 
-        return matchCount;
-    }
-
-    private int increaseCountIfMatch(Item winTicket, int matchCount, LottoNumber winNumber) {
-        for (LottoNumber number : winTicket.getNumbers()) {
-            if (number.equals(winNumber)) {
-                matchCount++;
-            }
-        }
-        return matchCount;
-    }
-
-    private boolean isMatchBonus(Item winTicket) {
-        LottoNumber bonus = ((WinLottoTicket) winTicket).getBonus();
-        return numbers.contains(bonus);
-    }
-
-    public LottoPrize getRank(Item winLottoTicket) {
-        int matchCount = getLuckyNumberMatchCount(winLottoTicket);
-        boolean matchBonus = isMatchBonus(winLottoTicket);
-        return LottoPrize.findRank(matchCount, matchBonus);
-    }
+//    public LottoPrize getRank(Item winLottoTicket) {
+//        int matchCount = getLuckyNumberMatchCount(winLottoTicket);
+//        boolean matchBonus = isMatchBonus(winLottoTicket);
+//        return LottoPrize.findRank(matchCount, matchBonus);
+//    }
 }
--- src/test/java/lotto/domain/item/LottoTicketTest.java
+++ src/test/java/lotto/domain/item/LottoTicketTest.java
@@ -59,37 +59,37 @@ public class LottoTicketTest {
         assertThat(ticket1.equals(ticket2)).isTrue();
     }
 
-    @DisplayName("당첨 번호와 비교해 로또 번호가 몇개가 맞는지 확인한다.")
-    @ParameterizedTest
-    @MethodSource("provideMatchNumbers")
-    public void getLuckyNumberMatchCount_success(List<LottoNumber> comp, int expect) throws Exception {
-        //given
-        final LottoTicket lotto = new LottoTicket(comp);
-        final WinLottoTicket winLottoTicket = new WinLottoTicket(numbers, new LottoNumber(45));
-
-        //when
-        int match = lotto.getLuckyNumberMatchCount(winLottoTicket);
-
-        //then
-        assertThat(match).isEqualTo(expect);
-    }
-
-    @DisplayName("해당 로또가 몇등인지 판단한다")
-    @Test
-    public void getRank_success() throws Exception {
-        //given
-        final LottoTicket lotto = new LottoTicket(numbers);
-        final WinLottoTicket first = new WinLottoTicket(numbers, new LottoNumber(10));
-        final WinLottoTicket second = new WinLottoTicket(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)), new LottoNumber(6));
-        final WinLottoTicket third = new WinLottoTicket(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)), new LottoNumber(11));
-        final WinLottoTicket miss = new WinLottoTicket(Arrays.asList(new LottoNumber(1), new LottoNumber(10), new LottoNumber(11), new LottoNumber(12), new LottoNumber(13), new LottoNumber(14)), new LottoNumber(20));
-
-        //then
-        assertAll(
-                () -> assertThat(lotto.getRank(first)).isEqualTo(LottoPrize.FIRST)
-                , () -> assertThat(lotto.getRank(second)).isEqualTo(LottoPrize.SECOND)
-                , () -> assertThat(lotto.getRank(third)).isEqualTo(LottoPrize.THIRD)
-                , () -> assertThat(lotto.getRank(miss)).isEqualTo(LottoPrize.MISS)
-        );
-    }
+//    @DisplayName("당첨 번호와 비교해 로또 번호가 몇개가 맞는지 확인한다.")
+//    @ParameterizedTest
+//    @MethodSource("provideMatchNumbers")
+//    public void getLuckyNumberMatchCount_success(List<LottoNumber> comp, int expect) throws Exception {
+//        //given
+//        final LottoTicket lotto = new LottoTicket(comp);
+//        final WinLottoTicket winLottoTicket = new WinLottoTicket(numbers, new LottoNumber(45));
+//
+//        //when
+//        int match = lotto.getLuckyNumberMatchCount(winLottoTicket);
+//
+//        //then
+//        assertThat(match).isEqualTo(expect);
+//    }
+//
+//    @DisplayName("해당 로또가 몇등인지 판단한다")
+//    @Test
+//    public void getRank_success() throws Exception {
+//        //given
+//        final LottoTicket lotto = new LottoTicket(numbers);
+//        final WinLottoTicket first = new WinLottoTicket(numbers, new LottoNumber(10));
+//        final WinLottoTicket second = new WinLottoTicket(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)), new LottoNumber(6));
+//        final WinLottoTicket third = new WinLottoTicket(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)), new LottoNumber(11));
+//        final WinLottoTicket miss = new WinLottoTicket(Arrays.asList(new LottoNumber(1), new LottoNumber(10), new LottoNumber(11), new LottoNumber(12), new LottoNumber(13), new LottoNumber(14)), new LottoNumber(20));
+//
+//        //then
+//        assertAll(
+//                () -> assertThat(lotto.getRank(first)).isEqualTo(LottoPrize.FIRST)
+//                , () -> assertThat(lotto.getRank(second)).isEqualTo(LottoPrize.SECOND)
+//                , () -> assertThat(lotto.getRank(third)).isEqualTo(LottoPrize.THIRD)
+//                , () -> assertThat(lotto.getRank(miss)).isEqualTo(LottoPrize.MISS)
+//        );
+//    }
 }
