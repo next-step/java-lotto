d8d1e5af2358e02f3beb8a9593193a6a4f507a84
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-31 23:43:41 +0900

    refactor (LottoNumberAutoGenerator.java) : 요구사항에 의해 클래스명 수정

--- src/test/java/lotto/domain/stragegy/LottoNumberAutoGeneratorTest.java
+++ src/test/java/lotto/domain/stragegy/LottoNumberAutoGeneratorTest.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LottoNumberGeneratorTest {
+class LottoNumberAutoGeneratorTest {
 
     @DisplayName("1~45 사이의 랜덤 로또 번호를 생성 한다.")
     @Test
     public void generateLottoNumber_success() throws Exception {
         //when
-        List<Integer> integers = LottoNumberGenerator.generateLottoNumber();
+        List<Integer> integers = LottoNumberAutoGenerator.generateLottoNumber();
 
         //then
         assertThat(integers.size()).isEqualTo(6);
@@ -23,8 +23,8 @@ class LottoNumberGeneratorTest {
     @Test
     public void generateLottoNumber_fail_compareResult() throws Exception {
         //when
-        List<Integer> compare1 = LottoNumberGenerator.generateLottoNumber();
-        List<Integer> compare2 = LottoNumberGenerator.generateLottoNumber();
+        List<Integer> compare1 = LottoNumberAutoGenerator.generateLottoNumber();
+        List<Integer> compare2 = LottoNumberAutoGenerator.generateLottoNumber();
 
         //then
         assertThat(compare1.equals(compare2)).isFalse();
