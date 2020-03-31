4eeb33b89b443c37d9619f81f782edbb95d96821
Yeoseon Yun <devyyskr@gmail.com>
2020-03-31 19:23:44 +0900

    feat(LottoTier): 2등 구현을 위해 getTier를 판정하는 로직 내에서 쓸 수 있는 추상 메소드 선언 및 구현

--- src/main/java/step2/domain/LottoTier.java
+++ src/main/java/step2/domain/LottoTier.java
@@ -4,18 +4,32 @@ import java.util.Arrays;
 
 public enum LottoTier {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 150_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, false, 2_000_000_000) {
+        public boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber) {return matchedNumberCount == 6; }
+    },
+    SECOND(5, true, 30_000_000) {
+        public boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber) {return matchedNumberCount == 5 && isMatchBonusNumber == true; }
+    },
+    THIRD(5, false,150_000) {
+        public boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber) {return matchedNumberCount == 5 && isMatchBonusNumber == false; }
+    },
+    FOURTH(4, false, 50_000) {
+        public boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber) {return matchedNumberCount == 4; }
+    },
+    FIFTH(3, false,5_000) {
+        public boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber) {return matchedNumberCount == 3; }
+    },
+    NONE(0, false,0) {
+        public boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber) {return matchedNumberCount < 3; }
+    };
 
     private long matchedNumberCount;
+    private boolean isMatchBonusNumber;
     private int prize;
 
-    LottoTier(long matchedNumberCount, int prize) {
+    LottoTier(long matchedNumberCount, boolean isMatchBonusNumber, int prize) {
         this.matchedNumberCount = matchedNumberCount;
+        this.isMatchBonusNumber = isMatchBonusNumber;
         this.prize = prize;
     }
 
@@ -23,10 +37,12 @@ public enum LottoTier {
         return count * prize;
     }
 
-    public static LottoTier getTier(long matchedNumberCount) {
+    public static LottoTier getTier(long matchedNumberCount, boolean isMatchBonusNumber) {
         return Arrays.stream(values())
-                .filter(tier -> tier.matchedNumberCount == matchedNumberCount)
+                .filter(tier -> tier.isWinning(matchedNumberCount, isMatchBonusNumber) == true)
                 .findFirst()
                 .orElse(NONE);
     }
+
+    abstract boolean isWinning(long matchedNumberCount, boolean isMatchBonusNumber);
 }
