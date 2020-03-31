ee7deb0727ba5a767fc036eb710667253191c66b
wholemann <wholeman.dev@gmail.com>
2020-03-31 23:39:12 +0900

    refactor(lottogametest): 인스턴스로 lottoGame을 생성한다

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,13 +1,13 @@
 package lotto;
 
 public class LottoGame {
-    public static int match(Lotto lotto, Lotto winningLotto) {
+    public int match(Lotto lotto, Lotto winningLotto) {
         return (int) lotto.getNumbers().stream()
                 .filter(num -> winningLotto.getNumbers().contains(num))
                 .count();
     }
 
-    public static Rank getRank(int matchCount) {
+    public Rank getRank(int matchCount) {
         return Rank.of(matchCount);
     }
 }
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,20 +11,28 @@ import static lotto.Rank.THIRD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGameTest {
+
+    private LottoGame lottoGame;
+
+    @BeforeEach
+    public void setUp() {
+        lottoGame = new LottoGame();
+    }
+
     @Test
     @DisplayName("입력된 로또와 당첨 로또의 일치하는 번호 갯수를 리턴한다")
     public void matchLottoNumberReturnsCountOfMatches() {
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 10, 11, 12));
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
 
-        int result = LottoGame.match(lotto, winningLotto);
+        int result = lottoGame.match(lotto, winningLotto);
 
         assertThat(result).isEqualTo(3);
 
         lotto = new Lotto(Arrays.asList(1, 2, 3, 10, 11, 12));
         winningLotto = new Lotto(Arrays.asList(1, 2, 3, 5, 6, 10));
 
-        result = LottoGame.match(lotto, winningLotto);
+        result = lottoGame.match(lotto, winningLotto);
 
         assertThat(result).isEqualTo(4);
     }
@@ -33,17 +42,17 @@ public class LottoGameTest {
     public void getRankReturnsRankOfLotto() {
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 10, 11, 12));
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        int matchCount = LottoGame.match(lotto, winningLotto);
+        int matchCount = lottoGame.match(lotto, winningLotto);
 
-        Rank result = LottoGame.getRank(matchCount);
+        Rank result = lottoGame.getRank(matchCount);
 
         assertThat(result).isEqualTo(FOURTH);
 
         lotto = new Lotto(Arrays.asList(1, 2, 3, 10, 11, 12));
         winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10));
-        matchCount = LottoGame.match(lotto, winningLotto);
+        matchCount = lottoGame.match(lotto, winningLotto);
 
-        result = LottoGame.getRank(matchCount);
+        result = lottoGame.getRank(matchCount);
 
         assertThat(result).isEqualTo(THIRD);
     }
