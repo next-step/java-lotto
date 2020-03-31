73725ef0ef60e9ff5899f5afbcb15afade45bcbc
parkeeseul <eeseul.p@gmail.com>
2020-03-31 21:32:16 +0900

    test: 보너스 볼 번호 중복 검사 테스트 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,13 +1,24 @@
 package lotto.domain;
 
+import lotto.exception.ExceptionType;
+import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningLottoTest {
     private String bonusBall = "7";
     private String winningLottoValue = "1,2,3,4,5,6";
+
+    @Test
+    @DisplayName("보너스 볼 번호 중복 검사")
+    void winningLotto() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto("1,2,3,4,5,6"), "6"))
+                .isInstanceOf(LottoException.class)
+                .hasMessageContaining(ExceptionType.DUPLICATED_LOTTO_BONUS_NUMBER.getMessage());
+    }
 
     @Test
     @DisplayName("당첨 번호와 매칭 되는 로또 번호 수 테스트")
