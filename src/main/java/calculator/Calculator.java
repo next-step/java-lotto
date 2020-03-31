19f65fad0ef648689b9c834dd75ba8444dc87940
jbyiop <jbyiop@gmail.com>
2020-03-31 09:57:36 +0900

    test : 분리된 문자열의 숫자값을 합산을 실패하는 테스트
    분리된 문자열의 값 합산을 실패하는 테스트를 작성한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public String[] split(String s) {
         return new String[] {"1", "2", "3"};
     }
+
+    public double sum(String s) {
+        return 0;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,8 +1,9 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
@@ -12,6 +13,15 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         String[] tokens = calculator.split("1,2,3");
 
-        Assertions.assertThat(tokens).containsExactly("1", "2", "3");
+        assertThat(tokens).containsExactly("1", "2", "3");
+    }
+
+    @DisplayName("문자열 덧셈 계산기는 ','를 구분자로 하여 각 문자열을 분리한 후 합을 구할 수 있다")
+    @Test
+    void sumEachTokens() {
+        Calculator calculator = new Calculator();
+        double sum = calculator.sum("1,2,3,4,5");
+
+        assertThat(sum).isEqualTo(15);
     }
 }
