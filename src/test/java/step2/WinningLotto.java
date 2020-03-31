6998b17fc003e13474236e7111b88cd2b9736d14
Yeoseon Yun <devyyskr@gmail.com>
2020-03-31 19:36:02 +0900

    test(WinningLottoTest): 당첨 로또객체(WinningLotto) 추가 및 당첨 로또 검증 테스트 메소드 추가

---
+++ src/test/java/step2/WinningLotto.java
@@ -0,0 +1,20 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step2.domain.LottoProvider;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class WinningLotto {
+
+    @Test
+    @DisplayName("입력받은 보너스 번호가 로또 번호와 중복되지 않아야 함을 검증한다.")
+    void validateWinningLotto() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            LottoProvider.createWinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 6);
+        })
+    }
+}
