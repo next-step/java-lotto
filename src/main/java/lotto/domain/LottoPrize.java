d809d3ec4d76901bc5584e9a663c3a80a94fba99
kwanguuuu <kwangyu326@gmail.com>
2020-04-01 13:35:55 +0900

    refacotro : 세 자리 숫자마다 _ 표시

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum LottoPrize {
     NONE(0, 0),
-    FOURTH(3, 5000),
-    THIRD(4, 50000),
-    SECOND(5, 1500000),
-    FIRST(6, 2000000000);
+    FOURTH(3, 5_000),
+    THIRD(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     private int matchCount;
     private int price;
