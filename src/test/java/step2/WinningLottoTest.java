fd4a314e3e58bc0b30c28683c716fe8938cead4b
Yeoseon Yun <devyyskr@gmail.com>
2020-04-02 00:38:55 +0900

    refactor(WinningLottoTest): ParameterizedTest를 이용하여 assert 구문을 1번만 쓰도록 수정

--- src/test/java/step2/WinningLottoTest.java
+++ src/test/java/step2/WinningLottoTest.java
@@ -3,12 +3,15 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import step2.domain.LottoProvider;
 import step2.domain.WinningLotto;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -23,17 +26,24 @@ public class WinningLottoTest {
         }).withMessage(WinningLotto.BONUS_NUMBER_DUPLICATION_ERROR);
     }
 
-    @Test
     @DisplayName("당첨 숫자 개수 체크가 제대로 되는지 테스트")
+    @ParameterizedTest
+    @MethodSource("provideLottoNumbers")
     void WinningNumber(List<Integer> input, int expected) {
         WinningLotto lotto = LottoProvider.createWinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 7);
 
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(1, 2, 3, 4, 5, 6)))).isEqualTo(6);
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(1, 2, 3, 4, 5, 7)))).isEqualTo(5);
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(1, 2, 3, 4, 7, 8)))).isEqualTo(4);
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(1, 2, 3, 7, 8, 9)))).isEqualTo(3);
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(1, 2, 7, 8, 9, 10)))).isEqualTo(2);
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(1, 7, 8, 9, 10, 11)))).isEqualTo(1);
-        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(Arrays.asList(7, 8, 9, 10, 11, 12)))).isEqualTo(0);
+        assertThat(lotto.getMatchedNumberCount(LottoProvider.createLotto(input))).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideLottoNumbers() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), 6),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 7), 5),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 7, 8), 4),
+                Arguments.of(Arrays.asList(1, 2, 3, 7, 8, 9), 3),
+                Arguments.of(Arrays.asList(1, 2, 7, 8, 9, 10), 2),
+                Arguments.of(Arrays.asList(1, 7, 8, 9, 10, 11), 1),
+                Arguments.of(Arrays.asList(7, 8, 9, 10, 11, 12), 0)
+        );
     }
 }
