c5802249231add11ea7db30a8b162d8d353b0f34
wholemann <wholeman.dev@gmail.com>
2020-04-01 13:51:33 +0900

    feat(lotto): 중복된 숫자가 들어왔을 때 Exception이 발생한다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_NUMBERS_SIZE = 6;
@@ -10,6 +12,7 @@ public class Lotto {
     public Lotto(List<Integer> lottoNumbers) {
         this.lottoNumbers = lottoNumbers;
         validateSize(lottoNumbers);
+        validateDuplicate(lottoNumbers);
     }
 
     public List<Integer> getNumbers() {
@@ -19,6 +22,13 @@ public class Lotto {
     private void validateSize(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6개만 가능합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<Integer> lottoNumbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(lottoNumbers);
+        if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("로또 번호들은 중복될 수 없습니다.");
         }
     }
 }
