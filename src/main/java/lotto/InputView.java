764192741df4bb06d10015c1351ee20e7075e5cf
wholemann <wholeman.dev@gmail.com>
2020-04-01 13:53:35 +0900

    refactor(inputview): 매직넘버는 명명된 상수로 교체한다

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import static java.util.stream.Collectors.toList;
 
 public class InputView {
+    public static final String NUMBER_DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int getPrice() {
@@ -22,7 +23,7 @@ public class InputView {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String value = scanner.nextLine();
 
-        return Arrays.stream(value.split(","))
+        return Arrays.stream(value.split(NUMBER_DELIMITER))
                 .map(Integer::new)
                 .collect(toList());
     }
