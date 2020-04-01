96b657acc61b1a2ea27abcfedf0a4183db10cff2
wholemann <wholeman.dev@gmail.com>
2020-04-01 13:17:54 +0900

    feat(application): 로또 프로그램 UI 요구사항을 구현한다

--- src/main/java/lotto/LottoGameApplication.java
+++ src/main/java/lotto/LottoGameApplication.java
@@ -19,7 +19,13 @@ public class LottoGameApplication {
         LottoGame lottoGame = new LottoGame();
         int purchaseCount = lottoGame.purchaseLotto(price);
 
+        System.out.println(String.format("%d개를 구매했습니다.", purchaseCount));
+
         List<Lotto> lottos = lottoGame.createLottos(purchaseCount);
+
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
 
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String value = scanner.nextLine();
@@ -32,15 +38,26 @@ public class LottoGameApplication {
 
         List<Rank> results = new ArrayList<>();
 
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+
         for (Lotto lotto : lottos) {
             int matchCount = lottoGame.match(lotto, winningLotto);
             results.add(lottoGame.getRank(matchCount));
         }
 
+        System.out.println("3개 일치 (5000원) - " + results.stream().filter(r -> r == Rank.FOURTH).count());
+        System.out.println("4개 일치 (50000원) - " + results.stream().filter(r -> r == Rank.THIRD).count());
+        System.out.println("5개 일치 (1500000원) - " + results.stream().filter(r -> r == Rank.SECOND).count());
+        System.out.println("6개 일치 (2000000000원) - " + results.stream().filter(r -> r == Rank.FIRST).count());
+
         int totalWinnings = lottoGame.getTotalWinnings(results);
 
         double rateOfRevenue = lottoGame.getRateOfRevenue(price, totalWinnings);
 
-        System.out.println(String.format("총 수익률은 %f입니다", rateOfRevenue));
+        System.out.println(String.format(
+                "총 수익률은 %.2f 입니다. (기준이 1이기 때문에 1보다 작으면 결과적으로 손해라는 의미임)",
+                rateOfRevenue
+        ));
     }
 }
