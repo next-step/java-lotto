975a90d15133716ba8b24470531071a485ec99b0
kiyeon <kyeoneee@gmail.com>
2020-04-01 09:33:50 +0900

    feat (InputView) : step2 - 당첨 번호 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,16 +6,23 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     private static final String PURCHASE_NOTICE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_NOTICE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final int DEFAULT_PRICE = Integer.MIN_VALUE;
 
     public static int getPurchasePrice() {
         System.out.println(PURCHASE_NOTICE);
 
         try {
-            return Integer.parseInt(scanner.next());
+            return Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
             return DEFAULT_PRICE;
         }
+    }
+
+    public static String getWinnerNum() {
+        System.out.println(WINNING_NUMBER_NOTICE);
+
+        return scanner.nextLine();
     }
 
 }
