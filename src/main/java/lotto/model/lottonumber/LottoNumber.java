519d03d2e5ba8c2ff7a102b4fcc47eb3b390a2d7
LeeSeungHee <palmseung@gmail.com>
2020-04-02 03:45:34 +0900

    feat (lottoNumber) : Add LottoNumber.validateNumberRange

--- src/main/java/lotto/model/lottonumber/LottoNumber.java
+++ src/main/java/lotto/model/lottonumber/LottoNumber.java
@@ -1,12 +1,23 @@
 package lotto.model.lottonumber;
 
+import java.util.List;
 import java.util.Objects;
 
 public class LottoNumber {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+
     private int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
+        validateNumberRange(lottoNumber);
         this.lottoNumber = lottoNumber;
+    }
+
+    private void validateNumberRange(int lottoNumber) {
+        if(lottoNumber > LOTTO_NUMBER_MAX || lottoNumber < LOTTO_NUMBER_MIN){
+            throw new IllegalArgumentException("로또는 1부터 45까지의 숫자로만 구성되어야 합니다.");
+        }
     }
 
     @Override
