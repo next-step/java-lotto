4ebe50eea7c223ff57dc7f91d6dc5cf731004c9c
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 20:23:05 +0900

    feat (InputView.java) : 수동 구매 개수 입력, 메시지 상수 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,20 +3,33 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final String BUY_LOTTO_COUNT_INPUT_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String PASSIVITY_BUY_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String LASTWEEK_WIN_NUMBER_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
+
     public static String inputBuyAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
+        System.out.println(BUY_LOTTO_COUNT_INPUT_MESSAGE);
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
 
+    public static String inputBuyPassivityCount() {
+        System.out.println(PASSIVITY_BUY_INPUT_MESSAGE);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+
     public static String inputLuckyNumber() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println(LASTWEEK_WIN_NUMBER_INPUT_MESSAGE);
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
 
     public static String inputBonusLuckyNumber() {
-        System.out.println("보너스 볼을 입력해 주세요.");
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
