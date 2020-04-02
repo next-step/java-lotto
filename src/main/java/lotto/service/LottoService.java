e29de91ed4a3b2cc5c65117a4d89d09cfac83312
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 20:56:52 +0900

    feat (LottoGame.java) : 수동 로또 티켓을 원하는 수만큼 만들어 내도록 수정

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,10 @@
 package lotto.service;
 
 import lotto.domain.item.LottoGame;
+import lotto.domain.item.LottoNumbers;
 import lotto.view.LottoDto;
+
+import java.util.List;
 
 public class LottoService {
 
@@ -10,8 +13,14 @@ public class LottoService {
         lottoGame.createAutoLottoTicket(0);
 
         LottoDto dto = new LottoDto();
-//        dto.setTickets(this.lottoTickets.getTickets());
+        return dto;
+    }
 
+    public LottoDto passivityPlay(List<LottoNumbers> numbers) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.createPassivityLottoTicket(numbers);
+
+        LottoDto dto = new LottoDto();
         return dto;
     }
 }
