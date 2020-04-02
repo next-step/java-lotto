ac8176a86d8e98e162ca47312eb59d7111c0b04a
crystal <crystal2840@gmail.com>
2020-04-02 23:18:41 +0900

    당첨된 로또 카운트 증가 추가

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -1,5 +1,19 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class WinningLottoTest {
 
+    @DisplayName("당첨된 로또의 카운트가 증가 되는지 확인한다")
+    @Test
+    void addCount() {
+        WinningLotto winningLotto = new WinningLotto(Rank.SECOND.getWinningAmount(), 0);
+        winningLotto.addCount();
+
+        assertThat(winningLotto.getCount()).isEqualTo(1);
+    }
 }
