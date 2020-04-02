f4fe10a53dede234604758620b332dd0144a560f
yohan <yohan.lee@uneedcomms.com>
2020-04-02 21:56:30 +0900

    test(LottoNumberTest): DisplayName 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LottoNumberTest {
     @ParameterizedTest
+    @DisplayName("로또 번호 생성")
     @ValueSource(ints = {1, 22, 45})
     void successByCreation(int input) {
         LottoNumber lottoNumber = LottoNumber.of(input);
@@ -17,6 +18,7 @@ public class LottoNumberTest {
     }
 
     @ParameterizedTest
+    @DisplayName("로또 번호 생성 예외처리")
     @ValueSource(ints = {-1, 0, 46})
     void exceptByCreation(int input) {
         assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
@@ -25,6 +27,7 @@ public class LottoNumberTest {
     }
 
     @ParameterizedTest
+    @DisplayName("로또 번호 생성 캐싱")
     @ValueSource(ints = {1, 22, 33})
     void cachingByCreation(int input) {
         assertThat(LottoNumber.of(input) == LottoNumber.of(input));
