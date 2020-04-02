2de38886f04e711b9db17d7d8fa806c2c2e196b9
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 20:51:44 +0900

    feat (LottoGame.java) : 자동 로또 티켓을 원하는 수만큼 만들어 내도록 수정

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,17 @@
 package lotto.service;
 
+import lotto.domain.item.LottoGame;
+import lotto.view.LottoDto;
+
 public class LottoService {
+
+    public LottoDto autoPlay(int gameCount) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.createAutoLottoTicket(0);
+
+        LottoDto dto = new LottoDto();
+//        dto.setTickets(this.lottoTickets.getTickets());
+
+        return dto;
+    }
 }
