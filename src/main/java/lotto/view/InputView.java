3fb96b412894158ba4337b3040d688c4374bb2b5
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 20:31:08 +0900

    feat (InputView.java) : 수동 구매 로또 번호 입력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,6 +10,7 @@ public class InputView {
     private static final String PASSIVITY_BUY_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String LASTWEEK_WIN_NUMBER_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String PASSIVITY_BUY_LOTTO_NUMBER_INPUT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 
     public static String inputBuyAmount() {
         System.out.println(BUY_LOTTO_COUNT_INPUT_MESSAGE);
@@ -19,6 +22,18 @@ public class InputView {
         System.out.println(PASSIVITY_BUY_INPUT_MESSAGE);
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
+    }
+
+    public static List<String> inputBuyPassivityLottoNumber(int count) {
+        System.out.println(PASSIVITY_BUY_LOTTO_NUMBER_INPUT_MESSAGE);
+        Scanner sc = new Scanner(System.in);
+        List<String> input = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            input.add(sc.nextLine());
+        }
+
+        return input;
     }
 
 
