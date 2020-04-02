e47dcc2ad853dffdbd30918d252b3167b7a1a2dd
kks <rudtjs9956@gmail.com>
2020-04-02 21:55:16 +0900

    test(LottoNumberTest): add LottoNumber create test

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class LottoNumberTest {
+    private LottoNumber lottoNumber;
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 23, 45})
+    public void create(int number) {
+        lottoNumber = new LottoNumber(number);
+        assertThat(lottoNumber).isEqualTo(number);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    public void doNotCreate(int number) {
+        assertThatIllegalArgumentException().isThrownBy(() -> lottoNumber = new LottoNumber(number));
+    }
+}
