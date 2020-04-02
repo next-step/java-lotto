c91f598beea83a8f9dbb3636c049442af270536e
yohan <yohan.lee@uneedcomms.com>
2020-04-03 01:39:26 +0900

    test(LottoMachineTest): 가격과 로또 티켓 폼을 제출했을 시 로또 티켓 생성 테스트 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,8 +19,15 @@ public class LottoMachineTest {
         assertThat(lottoTickets).hasSize(expectedLottoTicketCount);
     }
 
-    @Test
-    void test() {
-        LottoTicket lottoTicket = LottoTicket.of(1, 2, 3, 4, 5, 6);
+    @ParameterizedTest
+    @DisplayName("가격과 수동 로또티켓 폼을 제출 시 로또 티켓 생성")
+    @CsvSource(value = {"3500:3", "9800:9", "15920:15"}, delimiter = ':')
+    void manualLottoByCreation(int money, int expectedLottoTicketCount) {
+        LottoTicketForm lottoTicketForm = LottoTicketForm.of(1, 2, 3, 4, 5, 6);
+        LottoTicketForm lottoTicketForm2 = LottoTicketForm.of(11, 12, 13, 14, 15, 16);
+        List<LottoTicket> tickets = LottoMachine.pay(money, Arrays.asList(lottoTicketForm, lottoTicketForm2));
+
+        assertThat(tickets).hasSize(expectedLottoTicketCount);
+        assertThat(tickets).contains(LottoTicket.of(1, 2, 3, 4, 5, 6), LottoTicket.of(11, 12, 13, 14, 15, 16));
     }
 }
