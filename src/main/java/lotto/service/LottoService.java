674e41d70e1c482671affe8477be0f5df5c89912
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 21:04:35 +0900

    feat (LottoService.java) : 뷰에 전달할 DTO에 set

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import lotto.domain.item.LottoGame;
 import lotto.domain.item.LottoNumbers;
+import lotto.domain.item.LottoTickets;
 import lotto.view.LottoDto;
 
 import java.util.List;
@@ -10,17 +11,19 @@ public class LottoService {
 
     public LottoDto autoPlay(int gameCount) {
         LottoGame lottoGame = new LottoGame();
-        lottoGame.createAutoLottoTicket(0);
+        LottoTickets tickets = lottoGame.createAutoLottoTicket(gameCount);
 
         LottoDto dto = new LottoDto();
+        dto.setTickets(tickets);
         return dto;
     }
 
     public LottoDto passivityPlay(List<LottoNumbers> numbers) {
         LottoGame lottoGame = new LottoGame();
-        lottoGame.createPassivityLottoTicket(numbers);
+        LottoTickets tickets = lottoGame.createPassivityLottoTicket(numbers);
 
         LottoDto dto = new LottoDto();
+        dto.setTickets(tickets);
         return dto;
     }
 }
