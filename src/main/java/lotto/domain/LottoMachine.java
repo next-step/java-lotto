29459de2ea8e3183370a5bdad0ce8ba75074c389
yohan <yohan.lee@uneedcomms.com>
2020-04-03 19:23:28 +0900

    refactor: LottoTicketForms 생성 & 가독성을 위한 메소드 명 변경
    - pay -> createLottoTicket

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static lotto.domain.LottoTicket.*;
+import static lotto.domain.LottoTicket.TICKET_PRICE;
 
 public class LottoMachine {
-    public static List<LottoTicket> pay(Money money) {
+    public static List<LottoTicket> createLottoTickets(Money money) {
         List<LottoTicket> lottoTickets = new ArrayList<>();
 
         for (int i = 0; i < getTicketCount(money); i++) {
@@ -24,15 +24,25 @@ public class LottoMachine {
         return money.divide(TICKET_PRICE);
     }
 
-    public static List<LottoTicket> pay(Money money, List<LottoTicketForm> lottoTicketForms) {
-        List<LottoTicket> result = new ArrayList<>();
-        Money leftMoney = money.minus(TICKET_PRICE.multiply(lottoTicketForms.size()));
-        List<LottoTicket> autoLottoTickets = pay(leftMoney);
-        List<LottoTicket> manualLottoTickets = lottoTicketForms.stream()
+    public static List<LottoTicket> createLottoTickets(Money money, LottoTicketForms lottoTicketForms) {
+        Money leftMoney = deductManualPrice(money, lottoTicketForms);
+        List<LottoTicket> manualLottoTickets = createManualLottoTickets(lottoTicketForms);
+        List<LottoTicket> autoLottoTickets = createLottoTickets(leftMoney);
+
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.addAll(manualLottoTickets);
+        lottoTickets.addAll(autoLottoTickets);
+        return lottoTickets;
+    }
+
+    private static List<LottoTicket> createManualLottoTickets(LottoTicketForms lottoTicketForms) {
+        return lottoTicketForms.getLottoTicketForms()
+                .stream()
                 .map(LottoTicket::ofForm)
                 .collect(Collectors.toList());
-        result.addAll(autoLottoTickets);
-        result.addAll(manualLottoTickets);
-        return result;
+    }
+
+    private static Money deductManualPrice(Money money, LottoTicketForms lottoTicketForms) {
+        return money.minus(TICKET_PRICE.multiply(lottoTicketForms.getSize()));
     }
 }
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -14,7 +14,7 @@ public class LottoMachineTest {
     @DisplayName("가격에 맞는 개수만큼 로또 티켓 생성")
     @CsvSource(value = {"3500:3", "9800:9", "15920:15"}, delimiter = ':')
     void checkTicketCountByPayMoney(int money, int expectedLottoTicketCount) {
-        List<LottoTicket> lottoTickets = LottoMachine.pay(Money.of(money));
+        List<LottoTicket> lottoTickets = LottoMachine.createLottoTickets(Money.of(money));
         assertThat(lottoTickets).hasSize(expectedLottoTicketCount);
     }
 
@@ -24,7 +24,8 @@ public class LottoMachineTest {
     void manualLottoByCreation(int money, int expectedLottoTicketCount) {
         LottoTicketForm lottoTicketForm = LottoTicketForm.of(1, 2, 3, 4, 5, 6);
         LottoTicketForm lottoTicketForm2 = LottoTicketForm.of(11, 12, 13, 14, 15, 16);
-        List<LottoTicket> tickets = LottoMachine.pay(Money.of(money), Arrays.asList(lottoTicketForm, lottoTicketForm2));
+        List<LottoTicket> tickets = LottoMachine.createLottoTickets(Money.of(money),
+                LottoTicketForms.of(Arrays.asList(lottoTicketForm, lottoTicketForm2)));
 
         assertThat(tickets).hasSize(expectedLottoTicketCount);
         assertThat(tickets).contains(LottoTicket.of(1, 2, 3, 4, 5, 6), LottoTicket.of(11, 12, 13, 14, 15, 16));
