0e360ed5ff5e9101522234b726d393088ff97ea1
ohtaeg <otk1090@naver.com>
2020-04-04 00:30:04 +0900

    test (LottoResult) : 파라미터 변경으로 인한 테스트 코드 변경

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +8,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoResultTest {
 
+    private WinningLotto winningLotto;
+    private LottoNumbers lottoNumbers;
+    private LottoNumber bonusBall;
+
+    @BeforeEach
+    void setUp() {
+        lottoNumbers = new LottoNumbers("1, 2, 3, 4, 5, 6");
+        bonusBall = LottoNumber.valueOf(7);
+        winningLotto = WinningLotto.of(lottoNumbers, bonusBall);
+    }
+
     @DisplayName("수익률을 계산한다.")
     @Test
     void ratio() {
+        LottoNumbers purchase = new LottoNumbers("1, 2, 3, 7, 8, 9");
         LottoResult lottoResult = new LottoResult();
-        lottoResult.increase(LottoRank.FOURTH);
+        lottoResult.rank(purchase, winningLotto);
 
         final double actual = lottoResult.ratio(new Price(14000));
 
         assertThat(actual).isEqualTo(0.35);
     }
-
 }
