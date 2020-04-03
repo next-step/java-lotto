5f293071dd35434201ff872afaa4c6732ed995cd
LeeSeungHee <palmseung@gmail.com>
2020-04-03 16:07:15 +0900

    Test (WinningLotto) : add test to validate bonusBall not in Lotto

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -3,7 +3,6 @@ package lotto;
 import lotto.model.Lotto;
 import lotto.model.LottoNumber;
 import lotto.model.WinningLotto;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,13 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class WinningLottoTest {
     private List<LottoNumber> lottoNumbers = new ArrayList<>();
     private Lotto lotto;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         for (int i = 1; i <= 6; i++) {
             lottoNumbers.add(new LottoNumber(i));
         }
@@ -27,11 +27,22 @@ public class WinningLottoTest {
 
     @DisplayName("지난 주 당첨번호에 보너스 번호가 포함되지 않으면 정상적으로 객체 생성")
     @Test
-    void createTest(){
+    void createTest() {
         LottoNumber bonusBall = new LottoNumber(7);
 
         assertThatCode(() -> {
             new WinningLotto(lotto, bonusBall);
         }).doesNotThrowAnyException();
+    }
+
+
+    @DisplayName("보너스 번호가 지난 주 당첨번호에 포함된 번호이면 예외 발생")
+    @Test
+    void throwExceptionWhenBonusBallIsInWinningLottoNumbers() {
+        LottoNumber bonusBall = new LottoNumber(6);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new WinningLotto(lotto, bonusBall);
+        });
     }
 }
