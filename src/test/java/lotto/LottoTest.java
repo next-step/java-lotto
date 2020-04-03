87591803d57d1f10a2a747568ce079fa49018479
LeeSeungHee <palmseung@gmail.com>
2020-04-03 15:37:23 +0900

    Test (Lotto) : add test to validate Lotto consist of six LottoNumbers
    * Add LottoTest.throwExceptionWhenNotSixLottoNumbers
    
    * Add LottoTest.throwExceptionWhenNotSixLottoNumbers_2

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -10,20 +10,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
-    private List<LottoNumber> lotto = new ArrayList<>();
-
-    @BeforeEach
-    void setUp(){
-        for(int i=1; i<=6; i++){
-            lotto.add(new LottoNumber(i));
-        }
-    }
+    private List<LottoNumber> lottoNumbers = new ArrayList<>();
 
     @DisplayName("LottoNumber 객체 6개를 주면 정상적으로 Lotto 객체 생성")
     @Test
     void createTest() {
-        assertThatCode(() -> new Lotto(lotto)).doesNotThrowAnyException();
+        for(int i=1; i<=6; i++){
+            lottoNumbers.add(new LottoNumber(i));
+        }
+
+        assertThatCode(() -> new Lotto(lottoNumbers)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("LottoNumber 객체가 6개보다 적으면 예외 발생")
+    @Test
+    void throwExceptionWhenNotSixLottoNumbers(){
+        for(int i=1; i<=5; i++){
+            lottoNumbers.add(new LottoNumber(i));
+        }
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Lotto(lottoNumbers);
+        });
+    }
+
+    @DisplayName("LottoNumber 객체가 6개보다 많으면 예외 발생")
+    @Test
+    void throwExceptionWhenNotSixLottoNumbers_2(){
+        for(int i=1; i<=7; i++){
+            lottoNumbers.add(new LottoNumber(i));
+        }
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Lotto(lottoNumbers);
+        });
     }
 }
