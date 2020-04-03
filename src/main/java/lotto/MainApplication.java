d35c8ba3a5527197353ca7de63922bd57c043e66
LeeSeungHee <palmseung@gmail.com>
2020-04-03 14:22:47 +0900

    Feat (InputView) : add feature to get input for manual lotto numbers

--- src/main/java/lotto/MainApplication.java
+++ src/main/java/lotto/MainApplication.java
@@ -5,6 +5,9 @@ import lotto.view.InputView;
 public class MainApplication {
     public static void main(String[] args) {
         InputView.getInputForMoney();
+
         InputView.getInputForLottoCountManual();
+
+        InputView.getInputForLottoManual();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,15 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String MSG_MONEY_INPUT = "구입금액을 숫자로 입력해 주세요.";
-    private static final String MSG_LOTTO_COUNT = "수동으로 구매할 로또 갯수를 숫자로 입력해 주세요.";
+    private static final String MESSAGE_MONEY_INPUT = "구입금액을 숫자로 입력해 주세요.";
+    private static final String MESSAGE_LOTTO_COUNT = "수동으로 구매할 로또 갯수를 숫자로 입력해 주세요.";
+    private static final String MESSAGE_LOTTO_MANUAL = "수동으로 구매할 로또 번호를 입력하세요. (쉼표로 번호 구분)";
     private static final String WARNING_MONEY_INPUT = "구입금액은 1000원 이상만 입력 가능합니다.";
     private static final String WARNING_LOTTO_COUNT = "로또 금액이 부족합니다. 갯수를 다시 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
@@ -15,19 +20,48 @@ public class InputView {
     private static int lottoCountAuto = lottoCount - lottoCountManual;
 
     public static int getInputForMoney() {
-        System.out.println(MSG_MONEY_INPUT);
-        int money = scanner.nextInt();
+        System.out.println(MESSAGE_MONEY_INPUT);
+        String stringMoney = scanner.nextLine();
+        int money = Integer.parseInt(stringMoney);
         validateGreaterThan1000(isGreaterThan1000(money));
         lottoCount = money / MINIMUM_MONEY_TO_BUY;
         return lottoCount;
     }
 
     public static int getInputForLottoCountManual() {
-        System.out.println(MSG_LOTTO_COUNT);
-        int lottoCount = scanner.nextInt();
+        System.out.println(MESSAGE_LOTTO_COUNT);
+        String stringLottoCount = scanner.nextLine();
+        lottoCount = Integer.parseInt(stringLottoCount);
         validatePurchasable(isPurchasable(lottoCount));
         lottoCountManual = lottoCount;
         return lottoCountManual;
+    }
+
+    public static List<List<Integer>> getInputForLottoManual() {
+        System.out.println(MESSAGE_LOTTO_MANUAL);
+
+        List<List<Integer>> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCountManual; i++) {
+            List<Integer> lotto;
+            String stringLotto = scanner.nextLine();
+            validateNullAndEmpty(isNullOrEmpty(stringLotto));
+            lotto = Arrays.stream(stringLotto.split(","))
+                    .map(it -> Integer.parseInt(it))
+                    .collect(Collectors.toList());
+            validateNullAndEmpty(isNullOrEmpty(stringLotto));
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    private static boolean isNullOrEmpty(String input){
+        return (input.isEmpty() || input == null);
+    }
+
+    private static void validateNullAndEmpty(boolean isNullOrEmpty){
+        if(isNullOrEmpty){
+            throw new IllegalArgumentException("공백 또는 빈문자열은 입력할 수 없습니다.");
+        }
     }
 
     private static boolean isPurchasable(int lottoCountManul) {
