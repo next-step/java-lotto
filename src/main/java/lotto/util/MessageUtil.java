2c7feed1b8963fb12e024c727598f88279f5cb1a
LeeSeungHee <palmseung@gmail.com>
2020-04-03 14:54:31 +0900

    Refactor (InputView) : refactor InputView
    * Add MessageUtil

---
+++ src/main/java/lotto/util/MessageUtil.java
@@ -0,0 +1,11 @@
+package lotto.util;
+
+public class MessageUtil {
+    public static final String MESSAGE_MONEY_INPUT = "구입금액을 숫자로 입력해 주세요.";
+    public static final String MESSAGE_LOTTO_COUNT = "수동으로 구매할 로또 갯수를 숫자로 입력해 주세요.";
+    public static final String MESSAGE_LOTTO_MANUAL = "수동으로 구매할 로또 번호를 입력하세요. (쉼표로 번호 구분)";
+    public static final String MESSAGE_WINNING_LOTTO = "지난 주 당첨번호를 입력하세요. (쉼표로 구분)";
+    public static final String MESSAGE_BONUS_BALL = "지난 주 보너스 번호를 숫자로 입력하세요.";
+    public static final String WARNING_MONEY_INPUT = "구입금액은 1000원 이상만 입력 가능합니다.";
+    public static final String WARNING_LOTTO_COUNT = "로또 금액이 부족합니다. 갯수를 다시 입력해 주세요.";
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,15 +7,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
+import static lotto.util.MessageUtil.*;
 
 public class InputView {
-    private static final String MESSAGE_MONEY_INPUT = "구입금액을 숫자로 입력해 주세요.";
-    private static final String MESSAGE_LOTTO_COUNT = "수동으로 구매할 로또 갯수를 숫자로 입력해 주세요.";
-    private static final String MESSAGE_LOTTO_MANUAL = "수동으로 구매할 로또 번호를 입력하세요. (쉼표로 번호 구분)";
-    private static final String MESSAGE_WINNING_LOTTO = "지난 주 당첨번호를 입력하세요. (쉼표로 구분)";
-    private static final String MESSAGE_BONUS_BALL = "지난 주 보너스 번호를 숫자로 입력하세요.";
-    private static final String WARNING_MONEY_INPUT = "구입금액은 1000원 이상만 입력 가능합니다.";
-    private static final String WARNING_LOTTO_COUNT = "로또 금액이 부족합니다. 갯수를 다시 입력해 주세요.";
     private static final int MINIMUM_MONEY_TO_BUY = 1000;
     private static final String COMMA = ",";
 
