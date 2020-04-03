73b79d859746fd8bc5b85cf642fe0befe359a097
ohtaeg <otk1090@naver.com>
2020-04-03 22:29:37 +0900

    refactor (Application) : 수동 로또 입력시, List가 아닌 Lotteries를 반환하도록 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,17 +4,15 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
 
         Price inputPrice = inputView.inputPrice();
         int manualCount = inputView.inputManualCount();
-        List<LottoNumbers> inputManualLottoNumbers = inputView.inputManualLottoNumbers(manualCount);
 
-        Lotteries lotteries = LottoStore.sell(inputPrice, inputManualLottoNumbers);
+        Lotteries manuals = inputView.inputManualLottoNumbers(manualCount);
+        Lotteries lotteries = LottoStore.sell(inputPrice, manuals);
 
         OutputView.printLotteryCount(manualCount, lotteries.count());
         OutputView.printLotteries(lotteries.getLottoNumbers());
