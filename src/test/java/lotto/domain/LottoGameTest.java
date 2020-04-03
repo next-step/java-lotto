5902d10279744496cf61d16153bd0cc6511fd151
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-03 11:04:37 +0900

    refactor (LottoGame.java) : 불필요 코드 제거

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -25,11 +25,6 @@ class LottoGameTest {
             new LottoTicket(LottoNumbers.createLottoNumbersUseInteger(new ArrayList<Integer>(Arrays.asList(1, 20, 21, 22, 23, 24))))
     ));
 
-    @DisplayName("생성자 테스트")
-    @Test
-    public void constructor_success() throws Exception {
-        LottoGame lottoGame = new LottoGame(new Money(10000));
-    }
 
     @DisplayName("입력금액에서 구매 가능한 로또만큼 구매 되는지 체크")
     @ParameterizedTest
