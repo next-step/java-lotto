264e4996c38692491e437c0f379ddef573b5c036
yohan <yohan.lee@uneedcomms.com>
2020-04-03 21:15:29 +0900

    refactor: LottoTicketFrom 메소드 위치 변경

--- src/main/java/lotto/domain/LottoTicketForm.java
+++ src/main/java/lotto/domain/LottoTicketForm.java
@@ -7,18 +7,19 @@ import java.util.stream.Collectors;
 public class LottoTicketForm {
     private final LottoNumbers lottoNumbers;
 
-    private LottoTicketForm(List<LottoNumber> lottoNumbers) {
-        this.lottoNumbers = LottoNumbers.of(lottoNumbers);
-    }
 
     public static LottoTicketForm of(List<LottoNumber> lottoNumbers) {
-        return new LottoTicketForm(lottoNumbers);
+        return new LottoTicketForm(LottoNumbers.of(lottoNumbers));
     }
 
     public static LottoTicketForm of(int... lottoNumbers) {
-        return new LottoTicketForm(Arrays.stream(lottoNumbers)
+        return new LottoTicketForm(LottoNumbers.of(Arrays.stream(lottoNumbers)
                 .mapToObj(LottoNumber::of)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList())));
+    }
+
+    private LottoTicketForm(LottoNumbers lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
     }
 
     public LottoNumbers getLottoNumbers() {
