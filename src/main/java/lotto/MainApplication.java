8686256d2653cc2b81c998b104a9a9bb43019a9e
LeeSeungHee <palmseung@gmail.com>
2020-04-03 13:27:55 +0900

    Feat (InputView) : add feature to get input for manual lotto count

--- src/main/java/lotto/MainApplication.java
+++ src/main/java/lotto/MainApplication.java
@@ -5,5 +5,6 @@ import lotto.view.InputView;
 public class MainApplication {
     public static void main(String[] args) {
         InputView.getInputForMoney();
+        InputView.getInputForLottoCountManual();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,27 +3,52 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String MSG_MONEY_INPUT = "구입금액을 입력해 주세요. 구입금액은 1000원 이상만 입력 가능합니다.";
+    private static final String MSG_MONEY_INPUT = "구입금액을 숫자로 입력해 주세요.";
+    private static final String MSG_LOTTO_COUNT = "수동으로 구매할 로또 갯수를 숫자로 입력해 주세요.";
+    private static final String WARNING_MONEY_INPUT = "구입금액은 1000원 이상만 입력 가능합니다.";
+    private static final String WARNING_LOTTO_COUNT = "로또 금액이 부족합니다. 갯수를 다시 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
+    private static final int MINIMUM_MONEY_TO_BUY = 1000;
 
-    public static int getInputForMoney(){
-        printQuestion(MSG_MONEY_INPUT);
+    private static int lottoCount;
+    private static int lottoCountManual;
+    private static int lottoCountAuto = lottoCount - lottoCountManual;
+
+    public static int getInputForMoney() {
+        System.out.println(MSG_MONEY_INPUT);
         int money = scanner.nextInt();
         validateGreaterThan1000(isGreaterThan1000(money));
-        return money;
+        lottoCount = money / MINIMUM_MONEY_TO_BUY;
+        return lottoCount;
     }
 
-    private static boolean isGreaterThan1000(int money){
-       return money >= 1000;
+    public static int getInputForLottoCountManual() {
+        System.out.println(MSG_LOTTO_COUNT);
+        int lottoCount = scanner.nextInt();
+        validatePurchasable(isPurchasable(lottoCount));
+        lottoCountManual = lottoCount;
+        return lottoCountManual;
     }
 
-    private static void validateGreaterThan1000(boolean isGreaterThan1000){
-        if(!isGreaterThan1000){
-            getInputForMoney();
+    private static boolean isPurchasable(int lottoCountManul) {
+        return lottoCountManul <= lottoCount;
+    }
+
+    private static void validatePurchasable(boolean isPurchasable) {
+        if (!isPurchasable) {
+            System.out.println(WARNING_LOTTO_COUNT);
+            getInputForLottoCountManual();
         }
     }
 
-    private static void printQuestion(String message){
-        System.out.println(message);
+    private static boolean isGreaterThan1000(int money) {
+        return money >= MINIMUM_MONEY_TO_BUY;
+    }
+
+    private static void validateGreaterThan1000(boolean isGreaterThan1000) {
+        if (!isGreaterThan1000) {
+            System.out.println(WARNING_MONEY_INPUT);
+            getInputForMoney();
+        }
     }
 }
