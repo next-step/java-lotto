e89ccf94123ef5e8eddde3ed5ebbffae8619dfc9
LeeSeungHee <palmseung@gmail.com>
2020-04-03 16:05:51 +0900

    Feat (WinningLotto) : add feature for WinningLotto creation

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,8 +1,11 @@
 package lotto.model;
 
 public class WinningLotto {
+    private Lotto lotto;
+    private LottoNumber bonusBall;
 
     public WinningLotto(Lotto lotto, LottoNumber bonusBall) {
-
+        this.lotto = lotto;
+        this.bonusBall = bonusBall;
     }
 }
