dde859ddc9d48c4d99abdf0e7840b452e4adb691
ohtaeg <otk1090@naver.com>
2020-04-03 23:46:54 +0900

    test (WinningLotto) : 등수 테스트 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -8,22 +8,82 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class WinningLottoTest {
 
+    private WinningLotto winningLotto;
     private LottoNumbers lottoNumbers;
     private LottoNumber bonusBall;
 
     @BeforeEach
     void setUp() {
-        lottoNumbers = new LottoNumbers("1, 2, 3, 4, 5, 7");
+        lottoNumbers = new LottoNumbers("1, 2, 3, 4, 5, 6");
         bonusBall = LottoNumber.valueOf(7);
+        winningLotto = WinningLotto.of(lottoNumbers, bonusBall);
     }
 
     @DisplayName("당첨 번호 객체를 생성할 수 있다.")
     @Test
     void create() {
-        WinningLotto expect = WinningLotto.of(lottoNumbers, bonusBall);
+        WinningLotto actual = WinningLotto.of(new LottoNumbers("1, 2, 3, 4, 5, 6"), LottoNumber.valueOf(7));
 
-        WinningLotto actual = WinningLotto.of(new LottoNumbers("1, 2, 3, 4, 5, 7"), LottoNumber.valueOf(7));
+        assertThat(actual).isEqualTo(winningLotto);
+    }
 
-        assertThat(actual).isEqualTo(expect);
+    @DisplayName("구매한 복권들과 당첨 복권이 일치하지 않는다.")
+    @Test
+    void matchBlank() {
+        LottoNumbers purchase = new LottoNumbers("7, 8, 9, 10, 11, 12");
+
+        LottoRank actual = winningLotto.match(purchase);
+
+        assertThat(actual).isEqualTo(LottoRank.BLANK);
+    }
+
+    @DisplayName("구매한 복권들과 당첨 복권이 3개 일치한다. (4등)")
+    @Test
+    void matchFourth() {
+        LottoNumbers purchase = new LottoNumbers("1, 2, 3, 10, 11, 12");
+
+        LottoRank actual = winningLotto.match(purchase);
+
+        assertThat(actual).isEqualTo(LottoRank.FOURTH);
+    }
+
+    @DisplayName("구매한 복권들과 당첨 복권이 4개 일치한다. (3등)")
+    @Test
+    void matchThird() {
+        LottoNumbers purchase = new LottoNumbers("1, 2, 3, 4, 11, 12");
+
+        LottoRank actual = winningLotto.match(purchase);
+
+        assertThat(actual).isEqualTo(LottoRank.THIRD);
+    }
+
+    @DisplayName("구매한 복권들과 당첨 복권이 5개 일치한다. (2등)")
+    @Test
+    void matchSecond() {
+        LottoNumbers purchase = new LottoNumbers("1, 2, 3, 4, 5, 12");
+
+        LottoRank actual = winningLotto.match(purchase);
+
+        assertThat(actual).isEqualTo(LottoRank.SECOND);
+    }
+
+    @DisplayName("구매한 복권들과 당첨 복권이 5개 + 보너스 볼이 일치한다. (2등 보너스)")
+    @Test
+    void matchSecondBonus() {
+        LottoNumbers purchase = new LottoNumbers("1, 2, 3, 4, 5, 7");
+
+        LottoRank actual = winningLotto.match(purchase);
+
+        assertThat(actual).isEqualTo(LottoRank.SECOND_BONUS);
+    }
+
+    @DisplayName("구매한 복권들과 당첨 복권이 6개 일치한다. (1등)")
+    @Test
+    void analyzeWin_First() {
+        LottoNumbers purchase = new LottoNumbers("1, 2, 3, 4, 5, 6");
+
+        LottoRank actual = winningLotto.match(purchase);
+
+        assertThat(actual).isEqualTo(LottoRank.FIRST);
     }
 }
