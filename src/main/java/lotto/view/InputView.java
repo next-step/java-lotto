72d080b5a55229f7fdeecb610b2085fef0b6ad8c
manhyuk <manhyuk@kw.ac.kr>
2020-04-04 20:58:52 +0900

    feat: view 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -17,6 +17,8 @@ public class InputView {
     }
 
     public Lotto getWinningLotto() {
+        scanner.nextLine();
+        System.out.println("지난 로또 당첨 번호");
         String input = scanner.nextLine();
 
         return new Lotto(Arrays.stream(input.split(DELIMITER))
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import lotto.model.Rank;
+
+import java.util.List;
+
 public class ResultView {
+
+    public void print(List<Rank> gameResults) {
+//        3등
+        System.out.println("3등 " +
+                gameResults.stream().filter(result -> result.getRank() == 3).count());
+//        2등
+        System.out.println("2등 " +
+                gameResults.stream().filter(result -> result.getRank() == 2).count());
+
+//        1등
+        System.out.println("1등 " +
+                gameResults.stream().filter(result -> result.getRank() == 1).count());
+
+    }
 }
