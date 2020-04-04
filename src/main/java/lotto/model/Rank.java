b87a1b6e4e2999ee9ce0b987333dd2868e789a9e
manhyuk <manhyuk@kw.ac.kr>
2020-04-04 20:58:38 +0900

    feat: rank model 추가

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -2,14 +2,30 @@ package lotto.model;
 
 import java.util.List;
 
-public class GameResult {
+public class Rank {
     private int rank;
 
-    public GameResult(final int rank) {
+    public Rank(final int rank) {
         this.rank = rank;
     }
 
     public int getRank() {
         return rank;
     }
+
+    public static int calc(int num) {
+        if (num == 6) {
+            return 1;
+        } else if (num == 5) {
+            return 2;
+        } else if (num == 4) {
+            return 3;
+        } else if (num == 3) {
+            return 4;
+        } else if (num == 2) {
+            return 5;
+        }
+        return 6;
+    }
+
 }
