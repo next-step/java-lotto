3503e1df538fd10fde6fe49e9935671e0cdaed56
manhyuk <manhyuk@kw.ac.kr>
2020-04-04 15:04:23 +0900

    feat: view 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,26 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class InputView {
+    private static final String DELIMITER = ",";
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getMoney() {
+        System.out.println("얼마를 살것인가?");
+        return scanner.nextInt();
+    }
+
+    public Lotto getWinningLotto() {
+        String input = scanner.nextLine();
+
+        return new Lotto(Arrays.stream(input.split(DELIMITER))
+                .map(number -> Integer.parseInt(number))
+                .collect(Collectors.toList()));
+    }
 }
--- src/main/java/lotto/view/PurchaseLottoView.java
+++ src/main/java/lotto/view/PurchaseLottoView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+
+import java.util.List;
+
 public class PurchaseLottoView {
+
+    public void print(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개 를 구매 했다");
+        for (Lotto lotto: lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
 }
