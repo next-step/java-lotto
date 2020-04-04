4b36cc4117b3863fb677c209530e48c5822392e6
manhyuk <manhyuk@kw.ac.kr>
2020-04-04 15:04:32 +0900

    feat: controller 추가

--- src/main/java/lotto/App.java
+++ src/main/java/lotto/App.java
@@ -1,4 +1,23 @@
 package lotto;
 
+import lotto.domain.LottoGame;
+import lotto.model.Lotto;
+import lotto.view.InputView;
+import lotto.view.PurchaseLottoView;
+
+import java.util.List;
+
 public class App {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        PurchaseLottoView purchaseLottoView = new PurchaseLottoView();
+
+        LottoGame lottoGame = new LottoGame();
+        List<Lotto> lottos = lottoGame.initLottos(inputView.getMoney());
+        purchaseLottoView.print(lottos);
+
+
+//        inputView.getWinningLotto();
+    }
 }
