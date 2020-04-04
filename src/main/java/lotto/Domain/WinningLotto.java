b15241bee9121df2a571e6e617be579a7f91c906
ookhub <damlook@naver.com>
2020-04-04 23:30:22 +0900

    faet(winningLotto): add over lotto number exception

--- src/main/java/lotto/Domain/WinningLotto.java
+++ src/main/java/lotto/Domain/WinningLotto.java
@@ -7,12 +7,19 @@ public class WinningLotto {
     int bonus;
 
     public WinningLotto(Lotto lotto, int bonus) {
+        lottoNumberOverException(bonus);
         this.lotto = lotto;
         this.bonus = bonus;
     }
 
     public static WinningLotto init(Lotto lotto, int bonus) {
         return new WinningLotto(lotto, bonus);
+    }
+
+    private void lottoNumberOverException(int bonus) {
+        if(0 >= bonus || bonus > 45) {
+            throw new IllegalArgumentException("로또 번호" + bonus + "가 정합성을 위배하였습니다.");
+        }
     }
 
     public int matchLotto(Lotto targetLotto) {
