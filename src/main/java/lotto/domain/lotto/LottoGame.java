02091579b6e3a59989a97d28417b8b96adea03b8
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-05 23:15:01 +0900

    refactor (LottoGame.java) : 자동 로또 생성 로직 제거

--- src/main/java/lotto/domain/lotto/LottoGame.java
+++ src/main/java/lotto/domain/lotto/LottoGame.java
@@ -1,25 +1,10 @@
 package lotto.domain.lotto;
 
-import lotto.domain.Money;
-import lotto.domain.strategy.LottoNumberAutoGenerator;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGame {
 
     public LottoGame() {
-    }
-
-    public LottoTickets createAutoLottoTicket(int count) {
-        List<LottoTicket> lottos = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            LottoNumbers numbers = LottoNumbers.createLottoNumbersUseInteger(LottoNumberAutoGenerator.generateLottoNumber());
-            lottos.add(new LottoTicket(numbers));
-        }
-
-        return new LottoTickets(lottos);
     }
 
     public LottoTickets createPassivityLottoTicket(List<LottoNumbers> numbers) {
