fa1c76ed663ed3ce61b2b54e531f41bc5b149f6a
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-05 14:41:31 +0900

    refactor (Lotto.java) : validate 로직 메서드로 분리

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -4,6 +4,7 @@ import lotto.exception.ValidLottoException;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Lotto {
 
@@ -11,17 +12,18 @@ public class Lotto {
     protected final LottoNumbers numbers;
 
     public Lotto(LottoNumbers numbers) {
-        if (numbers == null) {
-            throw new ValidLottoException(NULL_PARAM_ERROR_MESSAGE);
-        }
+        validateNumbers(numbers.getValue());
         this.numbers = new LottoNumbers(numbers);
     }
 
     public Lotto(List<LottoNumber> numbers) {
-        if (numbers == null) {
-            throw new ValidLottoException(NULL_PARAM_ERROR_MESSAGE);
-        }
+        validateNumbers(numbers);
         this.numbers = new LottoNumbers(numbers);
+    }
+
+    private void validateNumbers(List<LottoNumber> numbers) {
+        Optional<List<LottoNumber>> numbersOpt = Optional.ofNullable(numbers);
+        numbersOpt.orElseThrow(() -> new ValidLottoException(NULL_PARAM_ERROR_MESSAGE));
     }
 
     public LottoNumbers getNumbers() {
