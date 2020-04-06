7140dd7f2a77e9dff8a1e7565d85d0fb84b981fb
kiyeon <kyeoneee@gmail.com>
2020-04-07 00:10:27 +0900

    refactor (NumberGenerator) : step2 피드백 반영 - 불필요한 iterate 대신 IntStream 사용으로 수정

--- src/main/java/lotto/domain/NumberGenerator.java
+++ src/main/java/lotto/domain/NumberGenerator.java
@@ -4,23 +4,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class NumberGenerator {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
 
-    private static final List<Integer> numberRange = Stream.iterate(MIN_LOTTO_NUMBER, n -> n + 1)
-            .limit(MAX_LOTTO_NUMBER)
-            .collect(Collectors.toList());
+    private static final List<Integer> numberRange = IntStream
+            .rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+            .mapToObj(Integer::new)
+            .collect(toList());
 
     static List<Integer> generateLotto(int numberCount) {
         Collections.shuffle(numberRange, new Random(System.currentTimeMillis()));
 
         List<Integer> oneSet = new ArrayList<>();
         for (int i = 0; i < numberCount; i++) {
-            oneSet.add(numberRange.get(i).intValue());
+            oneSet.add(numberRange.get(i));
         }
         Collections.sort(oneSet);
 
