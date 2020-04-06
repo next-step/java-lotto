ef3b20533c66a64a0fcfce66d57c265384f78810
kwanguuuu <kwangyu326@gmail.com>
2020-04-06 17:42:56 +0900

    refactor(LottoMachineInputView) : step4 수동 구매 뷰 추가

--- src/main/java/lotto/view/LottoMachineInputView.java
+++ src/main/java/lotto/view/LottoMachineInputView.java
@@ -14,9 +14,12 @@ import java.util.stream.Collectors;
 public class LottoMachineInputView {
     private static final String PURCHASE_MESSAGE = "구매금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_INFO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_MESSAGE ="보너스 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_NUMBER_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_NUMBERS_INFO_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 
     private static final String LOTTO_SPLIT_DELIMITER = ",";
+    private static final int MIN_NUMBER = 0;
 
     private static final Scanner sc = new Scanner(System.in);
 
@@ -27,7 +30,33 @@ public class LottoMachineInputView {
 
     public static int lottoPurchaseView() {
         System.out.println(PURCHASE_MESSAGE);
-        return sc.nextLine();
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    public static int lottoManualPurchaseView() {
+        System.out.println(MANUAL_LOTTO_NUMBER_MESSAGE);
+        return Integer.parseInt(sc.nextLine());
+    }
+    public static List<LottoTicket> insertManualNumbersView(int manualCounts) {
+        List<LottoTicket> manualTickets = new ArrayList<>();
+        if (manualCounts > MIN_NUMBER) {
+            System.out.println(MANUAL_LOTTO_NUMBERS_INFO_MESSAGE);
+            makeManualNumber(manualCounts, manualTickets);
+        }
+        return manualTickets;
+    }
+
+    private static void makeManualNumber(int manualCounts, List<LottoTicket> manualTickets) {
+        for (int start = MIN_NUMBER, end = manualCounts; start < end; start++) {
+            String[] inputs = sc.nextLine().split(LOTTO_SPLIT_DELIMITER);
+            manualTickets.add(
+                    new LottoTicket(Arrays.stream(inputs).
+                            map(input -> input.trim())
+                            .mapToInt(Integer::parseInt)
+                            .boxed()
+                            .collect(Collectors.toList()))
+            );
+        }
     }
 
     public static List<Integer> latestWinningLotto() {
@@ -39,7 +68,8 @@ public class LottoMachineInputView {
                 .boxed()
                 .collect(Collectors.toList()));
     }
-    public static int bonusLottoNumber(){
+
+    public static int bonusLottoNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
         return sc.nextInt();
     }
