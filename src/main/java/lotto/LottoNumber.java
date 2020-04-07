71835e7cba676e7a5fecf933b6cca6eea4ffd0ab
sang5c <skah321@gmail.com>
2020-04-07 13:48:46 +0900

    feat(LottoNumber): 로또 번호는 1에서 45까지  생성 가능

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    private void validateNumber(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 번호 범위 벗어남");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class LottoNumberTest {
+
+    @DisplayName("로또 번호 1, 45 정상 생성")
+    @Test
+    void create() {
+        LottoNumber number = new LottoNumber(1);
+        assertThat(new LottoNumber(1)).isEqualTo(number);
+    }
+
+    @DisplayName("로또 번호 범위를 벗어난 경우 에러")
+    @ParameterizedTest
+    @CsvSource({"0", "46"})
+    void outOfRange(int number) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(number));
+    }
+}
