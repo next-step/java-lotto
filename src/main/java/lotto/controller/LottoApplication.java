d6f17225525bfc91b2857ff282f6d46845323f26
isntyet <isntyet@gmail.com>
2020-04-07 22:37:18 +0900

    refactor(step4): LottoApplication
    의미없는 function화 제거

--- src/main/java/lotto/controller/LottoApplication.java
+++ src/main/java/lotto/controller/LottoApplication.java
@@ -4,13 +4,11 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 public class LottoApplication {
     private static final ResultView resultView = new ResultView();
     private static final InputView inputView = new InputView();
     private static LottoStore lottoStore = new LottoStore();
+    private static LottoInspector lottoInspector = new LottoInspector();
 
     public static void main(String[] args) {
         Money myMoney = inputView.inputMoney();
@@ -22,26 +20,8 @@ public class LottoApplication {
 
         WinningLotto winningLotto = inputView.inputWinningNumber();
 
-        InsightResult insightResult = getInsightResult(winningLotto, purchaseResult.getAllLottos());
-        insightYield(myMoney, insightResult);
-    }
-
-    private static InsightResult getInsightResult(WinningLotto winningLotto, List<Lotto> lottos) {
-        LottoInspector lottoInspector = new LottoInspector();
-        InsightResult result = lottoInspector.getResult(winningLotto, lottos);
-
-        resultView.viewInspect(result);
-
-        return result;
-    }
-
-    private static LottoInspector insightYield(Money money, InsightResult insightResult) {
-        LottoInspector lottoInspector = new LottoInspector();
-        int totalRevenue = insightResult.getTotalRevenue();
-        BigDecimal yield = lottoInspector.getYield(money, totalRevenue);
-
-        resultView.viewInsight(yield);
-
-        return lottoInspector;
+        InsightResult insightResult = lottoInspector.getResult(winningLotto, purchaseResult.getAllLottos());
+        resultView.viewInspect(insightResult);
+        resultView.viewInsight(lottoInspector.getYield(myMoney, insightResult.getTotalRevenue()));
     }
 }
