3bd19730b21841976230fadb6832b7d1c8e7daf3
kks <rudtjs9956@gmail.com>
2020-04-08 00:04:22 +0900

    feat(ui): add getWinningLotto, getBonusNo

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -9,4 +9,14 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextLine();
     }
+
+    public static String getWinningLotto() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    public static int getBonusNo() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextInt();
+    }
 }
