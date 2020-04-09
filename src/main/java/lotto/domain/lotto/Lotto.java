c7a5b2d20f00069d97b96c51f1b60e3d4b2114cc
jwee0330 <jwee0330@gmail.com>
2020-04-10 03:27:13 +0900

    refactor(lotto): lotto refactoring

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,67 +1,75 @@
 package lotto.domain.lotto;
 
 import lotto.domain.rank.LottoRank;
-import lotto.domain.rank.WinningLotto;
+import lotto.domain.rank.LottoTier;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
+import static lotto.domain.Constant.LOTTO_NUM_COUNT_LIMIT;
 
 public class Lotto {
-    private static final int MAX_SIZE = 5;
+    private static final int LOTTO_MATCH_FIVE = 5;
+    private final List<LottoNumber> lotto;
 
-    private final List<LottoLine> lottoLines;
-
-    Lotto(List<LottoLine> lottoLines) {
-        validate(lottoLines);
-        this.lottoLines = lottoLines;
+    private Lotto(List<Integer> lottoNumbers) {
+        validate(lottoNumbers);
+        List<LottoNumber> lotto = lottoNumbers.stream()
+                .map(LottoNumber::of)
+                .collect(toList());
+        this.lotto = lotto;
     }
 
-    public List<LottoRank> checkRank(WinningLotto winningLotto) {
-        return lottoLines.stream()
-                .map(lottoTicket -> lottoTicket.checkPrize(winningLotto))
+    public static Lotto of(List<Integer> lottoNumbers) {
+        return new Lotto(lottoNumbers);
+    }
+
+    public LottoRank checkRank(List<Integer> winningNumbers, LottoNumber bonusNumber) {
+        int matchCount = Long.valueOf(lotto.stream()
+                .filter(lottoNumber -> lottoNumber.matches(winningNumbers))
+                .count())
+                .intValue();
+        return getLottoRank(bonusNumber, matchCount);
+    }
+
+    public List<Integer> toIntValues() {
+        return lotto.stream()
+                .map(LottoNumber::intValue)
                 .collect(toList());
     }
 
-    public List<List<Integer>> getLottoSources() {
-        return lottoLines.stream()
-                .map(LottoLine::toIntNumbers)
-                .collect(Collectors.toList());
-    }
-
-    public int getMaxSize() {
-        return lottoLines.size();
-    }
-
-    public List<LottoLine> getLottoLines() {
-        return lottoLines;
-    }
-
-    private void validate(List<LottoLine> lottoLines) {
-        if (lottoLines.size() > MAX_SIZE) {
-            throw new LottoLineMaxSizeException();
+    private LottoRank getLottoRank(LottoNumber bonusNumber, int matchCount) {
+        if (matchCount == LOTTO_MATCH_FIVE) {
+            LottoTier lottoTier = LottoTier.of(matchCount, bonusNumber.matches(toIntValues()));
+            return LottoRank.of(lottoTier);
         }
+        LottoTier lottoTier = LottoTier.of(matchCount, false);
+        return LottoRank.of(lottoTier);
+    }
+
+    private void validate(List<Integer> lottoNumbers) {
+        if (isValidLottoSize(lottoNumbers)) {
+            throw new LottoSizeException();
+        }
+    }
+
+    private boolean isValidLottoSize(List<Integer> lottoNumbers) {
+        return lottoNumbers.stream()
+                .distinct()
+                .count() != LOTTO_NUM_COUNT_LIMIT;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Lotto)) return false;
-        Lotto that = (Lotto) o;
-        return Objects.equals(lottoLines, that.lottoLines);
+        Lotto lotto1 = (Lotto) o;
+        return Objects.equals(lotto, lotto1.lotto);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lottoLines);
-    }
-
-    @Override
-    public String toString() {
-        return "Lotto{" +
-                "lottoLines=" + lottoLines.toString() +
-                '}';
+        return Objects.hash(lotto);
     }
 }
