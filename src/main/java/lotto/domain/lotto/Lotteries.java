8447348ab1fb2bd2aafe81c16de76605447fe7ce
jwee0330 <jwee0330@gmail.com>
2020-04-10 03:27:53 +0900

    refactor(lotteries): lotteries refactoring

--- src/main/java/lotto/domain/lotto/Lotteries.java
+++ src/main/java/lotto/domain/lotto/Lotteries.java
@@ -1,60 +1,23 @@
 package lotto.domain.lotto;
 
-import lotto.application.WinningLottoRequest;
-import lotto.domain.rank.LottoRank;
-import lotto.domain.rank.LottoRanks;
-import lotto.domain.rank.WinningLotto;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Lottery {
-    private final List<Lotto> lottery;
+public class Lotteries {
+    private final List<Lotto> lotteries;
 
-    private Lottery(Lottery lottery) {
-        this.lottery = lottery.getLottery();
+    private Lotteries(List<Lotto> lotteries) {
+        this.lotteries = lotteries;
     }
 
-    public Lottery(List<Lotto> lottery) {
-        this.lottery = lottery;
-    }
-
-    public Lottery(Lottery lotteryByManual, Lottery lotteryByAuto) {
-        List<Lotto> lottery = new ArrayList<>(lotteryByManual.getLottery());
-        lottery.addAll(lotteryByAuto.getLottery());
-        this.lottery = lottery;
-    }
-
-    public static Lottery of(Lottery lottery) {
-        return new Lottery(lottery);
-    }
-
-    public static Lottery of(List<LottoNumbers> lottoNumbers) {
-        return new Lottery(lottoNumbers.stream()
-                .map(Lotto::new)
+    public static Lotteries of(List<List<Integer>> requestManualLottoNumbers) {
+        return new Lotteries(requestManualLottoNumbers.stream()
+                .map(Lotto::of)
                 .collect(Collectors.toList()));
     }
 
-    public List<LottoRank> getRank(WinningLotto winningLotto) {
-        return lottery.stream()
-                .map(lotto -> lotto.checkRank(winningLotto))
-                .collect(Collectors.toList());
+    public List<Lotto> getLotteries() {
+        return lotteries;
     }
 
-    public List<Lotto> getLottery() {
-        return lottery;
-    }
-
-    public LottoRanks matchWinningLotto(WinningLottoRequest winningLottoDto) {
-        List<LottoRank> lottoRanks = lottery.stream()
-                .map(lotto -> lotto.checkRank(winningLottoDto.to()))
-                .collect(Collectors.toList());
-        return new LottoRanks(lottoRanks);
-    }
-
-    public long getSize() {
-        return lottery.stream()
-                .count();
-    }
 }
