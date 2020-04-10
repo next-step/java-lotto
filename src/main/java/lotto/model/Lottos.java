f376f7335d2076ebaf6c96d6f5a5a8e7dc1ae4d8
manhyuk <manhyuk@kw.ac.kr>
2020-04-10 22:01:28 +0900

    test: 일급컬렉션 교환

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,4 @@
+package lotto.model;
+
+public class Lottos {
+}
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,14 +2,13 @@ package lotto;
 
 import lotto.domain.LottoGame;
 import lotto.model.Lotto;
+import lotto.model.Lottos;
 import lotto.model.Result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,13 +22,12 @@ public class LottoTest {
         lottoGame = new LottoGame();
     }
 
-
     @DisplayName("로또 구매 테스트")
     @Test
     void initLottos() {
         int money = 14000;
-        List<Lotto> lottos = lottoGame.initLottos(money);
-        assertThat(lottos.size()).isEqualTo(14);
+        Lottos lottos = lottoGame.initLottos(money);
+        assertThat(lottos.getLottos().size()).isEqualTo(14);
     }
 
     @DisplayName("Match test")
@@ -37,11 +35,11 @@ public class LottoTest {
     void match() {
         int money = 14000;
         Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        List<Lotto> lottos = lottoGame.initLottos(money);
-
+        Lottos lottos = lottoGame.initLottos(money);
+        int bonus = 7;
         List<Result> results = new ArrayList<>();
-        for (Lotto lotto : lottos) {
-            Result rank = lotto.match(winningLotto, lotto);
+        for (Lotto lotto : lottos.getLottos()) {
+            Result rank = lotto.match(winningLotto, bonus, lotto);
             results.add(rank);
         }
         assertThat(results.size()).isEqualTo(14);
@@ -57,7 +55,8 @@ public class LottoTest {
     @DisplayName("lotto 랜덤 값이 1~45 사이인지 테스")
     @Test
     void randomLimit() {
-        int random = lottoGame.getRandom();
+        Lottos lottos = new Lottos();
+        int random = lottos.getRandom();
         assertThat(random).isBetween(1, 45);
     }
 
@@ -65,8 +64,8 @@ public class LottoTest {
     @DisplayName("lotto 생성 테스트")
     @Test
     void createLottos() {
-        List<Lotto> lottos = lottoGame.createLottos(15);
-        assertThat(lottos.size()).isEqualTo(15);
+        Lottos lottos = new Lottos(15);
+        assertThat(lottos.getLottos().size()).isEqualTo(15);
     }
 
 
