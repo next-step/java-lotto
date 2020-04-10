664e6b5dd70e4ae72d04bf0260918df54b47cc00
etff <wish2rich@gmail.com>
2020-04-11 00:22:23 +0900

    refactor: Fixture 삭제

--- src/test/java/lotto/domain/LottoManualGeneratorTest.java
+++ src/test/java/lotto/domain/LottoManualGeneratorTest.java
@@ -1,30 +1,41 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
-import static lotto.domain.Fixture.lottoNumbers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class LottoManualGeneratorTest {
+    private List<LottoNumber> expected;
+
+    @BeforeEach
+    void setUp() {
+        expected = lottoNumbers();
+    }
 
     @DisplayName("로또 입력 생성 테스트")
     @Test
     void testInputCreateLotto() {
         List<String> inputText = Arrays.asList("1", "2", "3", "4", "5", "6");
-        List<LottoNumber> expected = lottoNumbers();
-
         LottoManualGenerator lottoManualGenerator = new LottoManualGenerator(inputText);
+
         List<LottoNumber> actual = lottoManualGenerator.generateNumbers();
 
         assertThat(expected).isEqualTo(actual);
+    }
+
+    private List<LottoNumber> lottoNumbers() {
+        List<Integer> sixNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        return sixNumbers.stream()
+                .map(LottoNumber::of)
+                .collect(Collectors.toList());
     }
 
 }
--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,17 +1,25 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static lotto.domain.Fixture.oneLotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WinningLottoTest {
 
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(new LottoAutoGenerator().getLottoNumbers());
+    }
+
     @DisplayName("당첨 로또의 등수를 구할 수 있다.")
     @Test
     void testLottoNumberMatch() {
-        WinningLotto winningLotto = new WinningLotto(oneLotto());
-        assertThat(Prize.FIRST).isSameAs(winningLotto.match(oneLotto()));
+
+        WinningLotto winningLotto = new WinningLotto(lotto);
+        assertThat(Prize.FIRST).isSameAs(winningLotto.match(lotto));
     }
 }
