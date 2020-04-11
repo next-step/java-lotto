43e8843b572a06e447a5c27e49a4937de7d64f17
isntyet <isntyet@gmail.com>
2020-04-11 12:48:19 +0900

    refactor(step4): 재사용
    재사용 가능한 객체는 한번만 만들자

--- src/main/java/lotto/generator/RandomNumber.java
+++ src/main/java/lotto/generator/RandomNumber.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class RandomNumber implements NumberGenerator {
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_MIN_NUMBER = 1;
+    private final Random random = new Random();
 
     @Override
     public int generate() {
-        return new Random().nextInt(LOTTO_MAX_NUMBER) + LOTTO_MIN_NUMBER;
+        return random.nextInt(LOTTO_MAX_NUMBER) + LOTTO_MIN_NUMBER;
     }
 }
