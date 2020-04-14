df364c9ee5ef8bb481ecdfceba7fcc2589eeea36
manhyuk <manhyuk@kw.ac.kr>
2020-04-14 13:50:14 +0900

    refact: del if-else if

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -27,16 +27,11 @@ public enum Rank {
     }
 
     public static Rank calcRank(int matchCount, boolean isBonus) {
-        if (matchCount == 6) {
-            return Rank.FIRST;
-        } else if (matchCount == 5 && isBonus) {
-            return Rank.SECOND;
-        } else if (matchCount == 5) {
-            return Rank.THIRD;
-        } else if (matchCount == 4) {
-            return Rank.FOURTH;
-        } else if (matchCount == 3) {
-            return Rank.FIFTH;
+
+        for (Rank rank : Rank.values()) {
+            if (rank.matchCount == matchCount) {
+                return rank;
+            }
         }
         return Rank.ZERO;
     }
