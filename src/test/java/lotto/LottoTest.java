e3b04568e34596456d1eb11dbdd91317f1cb83fb
sang5c <skah321@gmail.com>
2020-04-14 21:32:53 +0900

    docs: update tasks list

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTest {
+
+    @DisplayName("로또는 6개의 번호로 구성")
+    @Test
+    public void lotto() {
+//        Lotto lotto = new Lotto();
+    }
+}
