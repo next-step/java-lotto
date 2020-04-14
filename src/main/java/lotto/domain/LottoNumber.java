4b9c4db24ea66b5db61950f8cfc7f23e679bd763
wholemann <wholeman.dev@gmail.com>
2020-04-14 16:37:44 +0900

    feat(lottonumber): 로또의 숫자 범위를 초과하면 예외가 발생한다

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,9 +3,15 @@ package lotto.domain;
 import java.util.Objects;
 
 public class LottoNumber {
+    public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+
     private final int number;
 
     public LottoNumber(int number) {
+        if (number < MINIMUM_LOTTO_NUMBER || number > MAXIMUM_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 숫자는 1~45 범위 안의 숫자만 가능합니다.");
+        }
         this.number = number;
     }
 
