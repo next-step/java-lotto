aafdb60a2ee020da903282d5651325c4c090bb11
sang5c <skah321@gmail.com>
2020-04-15 00:32:35 +0900

    test(LottoSeller): 1천원 미만 금액 에러

--- src/test/java/lotto/LottoSellerTest.java
+++ src/test/java/lotto/LottoSellerTest.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoSellerTest {
 
@@ -16,5 +18,11 @@ class LottoSellerTest {
     public void buyLotto(int money, int expected) {
         List<Lotto> lottos = LottoSeller.buy(money);
         assertThat(lottos.size()).isEqualTo(expected);
+    }
+
+    @DisplayName("1천원 미만 금액 에러")
+    @Test
+    public void buyLottoError() {
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoSeller.buy(900));
     }
 }
