e461ef526fcd11f8505d77417168766308c48d10
sang5c <skah321@gmail.com>
2020-04-16 21:41:43 +0900

    test(LottoSeller): add buy fail code

--- src/test/java/lotto/domain/LottoSellerTest.java
+++ src/test/java/lotto/domain/LottoSellerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoSellerTest {
 
@@ -14,6 +15,12 @@ class LottoSellerTest {
     void buy() {
         List<Object> buy = LottoSeller.buy(5);
         assertThat(buy.size()).isEqualTo(5);
+    }
+
+    @DisplayName("1개 미만 로또 구매시 에러")
+    @Test
+    void buyFail() {
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoSeller.buy(0));
     }
 
     @Test
