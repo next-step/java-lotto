fbd6ec6a4c617140739052a3a5437d21568c5455
sang5c <skah321@gmail.com>
2020-04-16 22:07:19 +0900

    feat(Lotto): 로또 번호 중복시 에러

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,13 @@
 package lotto.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> lottoNumbers;
 
@@ -11,7 +15,15 @@ public class Lotto {
         this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
     }
     public static Object of(List<Integer> lottoNumbers) {
+        checkDuplicatedNumber(lottoNumbers);
         return new Lotto(lottoNumbers);
+    }
+
+    private static void checkDuplicatedNumber(List<Integer> lottoNumbers) {
+        Set<Integer> numberSet = new HashSet<>(lottoNumbers);
+        if (numberSet.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호 중복");
+        }
     }
 
 }
