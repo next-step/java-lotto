ea975ca635e69585e37b2fb7b6a5823d69ea08db
sang5c <skah321@gmail.com>
2020-04-16 22:02:25 +0900

    test(Lotto): 로또 생성 테스트 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,10 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
+    public static Object of(List<Integer> asList) {
+        return null;
+    }
+
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,9 +1,31 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoTest {
+
+    @DisplayName("로또는 6개의 중복되지 않는 번호로 구성")
+    @Test
+    void create() {
+        assertThatCode(() -> Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6))).doesNotThrowAnyException();
+    }
+
+    @DisplayName("로또 번호 중복시 에러")
+    @Test
+    void duplicatedNumber() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 5)));
+    }
+
+    @DisplayName("로또 번호 6개가 아닐시 에러")
+    @Test
+    void invalidSize() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(Arrays.asList(1, 2, 3, 4, 5)));
+    }
 
 }
