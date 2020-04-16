a438d1a85cd6d742b3c310864bae4d1f01aabb2f
sang5c <skah321@gmail.com>
2020-04-16 21:38:59 +0900

    feat(LottoSeller): add buy code

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class LottoSeller {
     public static List<Object> buy(int buyCount) {
-        return null;
+        List<Object> lottos = new ArrayList<>();
+        for (int i = 0; i < buyCount; i++) {
+            lottos.add(new Object());
+        }
+        return lottos;
     }
 
     public static List<Object> match(List<Object> lottos, String winningLottoNumbers) {
