c8eba01efb7cf27cc6ab2c3b27a48d90de53da82
kks <rudtjs9956@gmail.com>
2020-04-18 01:55:30 +0900

    refactor(LottoNumber): remove optional

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
 public class LottoNumber {
     public static final int MIN_VALUE = 1;
@@ -23,8 +22,11 @@ public class LottoNumber {
     }
 
     static LottoNumber of(int number) {
-        return Optional.ofNullable(lottoNumbers.get(number))
-                .orElseThrow(() -> new IllegalArgumentException("로또 번호는 1부터 45까지만 가능합니다."));
+        LottoNumber lottoNumber = lottoNumbers.get(number);
+        if (lottoNumber == null) {
+            new IllegalArgumentException("로또 번호는 1부터 45까지만 가능합니다.");
+        }
+        return lottoNumber;
     }
 
     static LottoNumber of(String value) {
