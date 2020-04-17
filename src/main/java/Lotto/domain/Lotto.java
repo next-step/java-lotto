853791ae495bc10eb74eb7b9d62c6329979c3781
jinwoo <jwlee@qualson.com>
2020-04-17 18:02:35 +0900

    feat(Lotto) : Add function (get winning type with winning numbers)

--- src/main/java/Lotto/domain/Lotto.java
+++ src/main/java/Lotto/domain/Lotto.java
@@ -3,7 +3,11 @@ package Lotto.domain;
 public class Lotto {
     private final LottoNumbers numbers;
 
-    public Lotto(LottoNumbers numbers) {
+    public Lotto(final LottoNumbers numbers) {
         this.numbers = numbers;
+    }
+
+    public WinningType getWinningTypeWithWinningNumbers(final LottoNumbers winningNumbers) {
+        return WinningType.valueOf(numbers.match(winningNumbers));
     }
 }
