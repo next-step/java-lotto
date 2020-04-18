b9e6069eff5596c0a5df77430c460fc748a7389f
jinwoo <jwlee@qualson.com>
2020-04-19 01:59:13 +0900

    feat(LottoNumbers) : Add constructor with inputString And Add toString

--- src/main/java/Lotto/domain/LottoNumbers.java
+++ src/main/java/Lotto/domain/LottoNumbers.java
@@ -1,6 +1,6 @@
 package Lotto.domain;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -8,6 +8,10 @@ import java.util.stream.Collectors;
 
 public class LottoNumbers {
     public static final int LOTTO_SIZE = 6;
+    private static final String COMMA_WITH_SPACE = ", ";
+    private static final String DELIMITER = ",";
+    private static final String PREFIX = "[";
+    private static final String SUFFIX = "]";
 
     private final Set<LottoNumber> lottoNumberSet;
 
@@ -21,7 +25,15 @@ public class LottoNumbers {
     }
 
     public static LottoNumbers of(final List<Integer> numbers) {
-        return new LottoNumbers(numbers.stream().map(LottoNumber::from).collect(Collectors.toSet()));
+        return new LottoNumbers(numbers.stream()
+                .map(LottoNumber::from)
+                .collect(Collectors.toSet()));
+    }
+
+    public static LottoNumbers of(final String inputString) {
+        return of(Arrays.stream(inputString.split(DELIMITER))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
     }
 
     private void validateLottoSize(final Set<LottoNumber> lottoNumberSet) {
@@ -37,5 +49,12 @@ public class LottoNumbers {
 
     public int increment(final LottoNumber number) {
         return this.lottoNumberSet.contains(number) ? 1 : 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.lottoNumberSet.stream()
+                .map(LottoNumber::toString)
+                .collect(Collectors.joining(COMMA_WITH_SPACE, PREFIX, SUFFIX));
     }
 }
