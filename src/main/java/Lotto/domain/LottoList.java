9b4c5fb3416a60505a7a6c2a6e0582e45e85ca15
jinwoo <jwlee@qualson.com>
2020-04-19 02:01:33 +0900

    feat(LottoList) : Add getter and static constructor

--- src/main/java/Lotto/domain/LottoList.java
+++ src/main/java/Lotto/domain/LottoList.java
@@ -5,8 +5,16 @@ import java.util.List;
 public class LottoList {
     private final List<Lotto> lottoList;
 
-    public LottoList(final List<Lotto> lottoList) {
+    private LottoList(final List<Lotto> lottoList) {
         this.lottoList = lottoList;
+    }
+
+    public static LottoList of(final List<Lotto> lottoList) {
+        return new LottoList(lottoList);
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
     }
 
     public LottoResult getResult(final WinningLotto winningLotto) {
