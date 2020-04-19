69bd01d706912c0b348fd9bf4c76b0395c9a88e3
sang5c <skah321@gmail.com>
2020-04-19 19:47:44 +0900

    feat(LottoNumber): 로또 번호는 1이상 45 이하

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,6 +1,11 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     private final int lottoNumber;
 
@@ -9,6 +14,27 @@ public class LottoNumber {
     }
 
     public static LottoNumber of(int lottoNumber) {
+        validateLottoNumber(lottoNumber);
         return new LottoNumber(lottoNumber);
     }
+
+    private static void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber < MIN_LOTTO_NUMBER || lottoNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("lotto number out of range");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
 }
