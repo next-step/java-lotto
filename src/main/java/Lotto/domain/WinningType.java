9ecdb2e1f43cf1989ac4acfd3e39a40bfc0af108
jinwoo <jwlee@qualson.com>
2020-04-21 01:16:57 +0900

    refactor(WinningType) : Add final and change indents

--- src/main/java/Lotto/domain/WinningType.java
+++ src/main/java/Lotto/domain/WinningType.java
@@ -3,12 +3,12 @@ package Lotto.domain;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
-public enum  WinningType {
-    FIRST(6,2_000_000_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 6),
-    SECOND(5,30_000_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 5 && hasBonusNumber),
-    THIRD(5,1_500_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 5 && !hasBonusNumber),
-    FOURTH(4,50_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 4),
-    FIFTH(3,5_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 3),
+public enum WinningType {
+    FIRST(6, 2_000_000_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 6),
+    SECOND(5, 30_000_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 5 && hasBonusNumber),
+    THIRD(5, 1_500_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 5 && !hasBonusNumber),
+    FOURTH(4, 50_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 4),
+    FIFTH(3, 5_000, (countOfMatch, hasBonusNumber) -> countOfMatch == 3),
     MISS(0, 0, (countOfMatch, hasBonusNumber) -> countOfMatch < 3);
 
     private final int countOfMatch;
@@ -17,7 +17,7 @@ public enum  WinningType {
 
     WinningType(final int countOfMatch,
                 final int winningMoney,
-                BiFunction<Integer, Boolean, Boolean> isMatch) {
+                final BiFunction<Integer, Boolean, Boolean> isMatch) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
         this.isMatch = isMatch;
@@ -33,7 +33,7 @@ public enum  WinningType {
 
     public static WinningType valueOf(final int countOfMatch, final boolean hasBonusNumber) {
         return Stream.of(WinningType.values())
-                .filter(value-> value.isMatch.apply(countOfMatch, hasBonusNumber))
+                .filter(value -> value.isMatch.apply(countOfMatch, hasBonusNumber))
                 .findFirst()
                 .orElse(MISS);
     }
