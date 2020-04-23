881aaa55ba9f167a754d38dae3727ff66b9129b1
sang5c <skah321@gmail.com>
2020-04-23 23:39:09 +0900

    fix(LottoSeller): extract magic number

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -3,12 +3,12 @@ package lotto.domain;
 public class LottoSeller {
 
     private static final String NOT_ENOUGH_MONEY = "돈이 모자랍니다";
-
     private static final int LOTTO_PRISE = 1000;
+    private static final int MIN_BUY_COUNT = 1;
 
     public static Lottos buy(int money) {
         int buyCount = money / LOTTO_PRISE;
-        if (buyCount < 1) {
+        if (buyCount < MIN_BUY_COUNT) {
             throw new IllegalArgumentException(NOT_ENOUGH_MONEY);
         }
         return LottoMachine.generate(buyCount);
