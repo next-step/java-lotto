e74bc8732ba7267c86232c32f682a0f05c452fc0
sang5c <skah321@gmail.com>
2020-04-23 22:21:00 +0900

    docs: update step3 tasks list

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -43,7 +43,6 @@ public class ResultView {
         for (Match match : matches) {
             printResultFormat(match, result.getMatchCount(match));
         }
-
         printRateOfReturn(result.rateOfReturn(money));
     }
 
