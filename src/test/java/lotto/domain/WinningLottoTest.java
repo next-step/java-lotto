fd5daf56c43fd38d0a75c070b401e250ed4e1d26
sang5c <skah321@gmail.com>
2020-04-24 00:10:28 +0900

    test(WinningLotto): 당첨번호와 보너스번호 중복 불가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class WinningLottoTest {
 
@@ -13,6 +14,12 @@ class WinningLottoTest {
     @BeforeEach
     void setup() {
         winningLotto = new WinningLotto(Lotto.ofComma("1,2,3,4,5,6"), 7);
+    }
+
+    @DisplayName("당첨번호와 보너스번호 중복 불가")
+    @Test
+    void createError() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new WinningLotto(Lotto.ofComma("1,2,3,4,5,6"), 1));
     }
 
     @DisplayName("2등")
