c0e2e23f17d004f5ff2a7404a0756c147970effa
sang5c <skah321@gmail.com>
2020-04-24 23:40:22 +0900

    feat(Money): 상품 가격 입력하면 구매 가능 횟수 리턴

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,11 +5,14 @@ public class Money {
     private final int money;
 
     public Money(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("0원 미만 금액 불가능");
+        }
         this.money = money;
     }
 
     public int buyCount(int price) {
-        return 0;
+        return money / price;
     }
 
 }
