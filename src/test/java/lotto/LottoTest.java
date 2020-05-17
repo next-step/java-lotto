c7ef5c7559be531aa5d725665fcd0459243b426f
정주영 <jy.jeong@woowahan.com>
2020-05-18 02:10:39 +0900

    refactor 테스트 케이스 리팩토링

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,20 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoTest {
 
+    @DisplayName("로또를 한개 구입할수있다.")
     @Test
     void buy_one_lotto() {
         LottoStore lottoStore = new LottoStore();
@@ -21,6 +23,7 @@ public class LottoTest {
         assertThat(lottos).hasSize(1);
     }
 
+    @DisplayName("로또를 여러개 구입할수있다.")
     @Test
     void buy_many_lotto() {
         LottoStore lottoStore = new LottoStore();
@@ -29,17 +32,26 @@ public class LottoTest {
         assertThat(lottos).hasSize(5);
     }
 
-    @DisplayName("로또는 6개의 숫자를 가지고 있다.")
+    @DisplayName("로또는 6개의 숫자가 아니면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,7,10})
+    void given_not_6_when_lotto_create_then_throw_exception(int numCount) {
+        List<LottoNum> lottoNumList = Stream.generate(() -> new LottoNum(1)).limit(numCount).collect(
+            Collectors.toList());
+
+        assertThatThrownBy(()->new LottoNums(lottoNumList))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또는 6개의 숫자의 숫자를 가진다..")
+    @DisplayName("로또는 6개의 숫자의 숫자를 가진다.")
     @Test
-    void when_lotto_create_then_lotto_has_6_nums() {
+    void given_6_nums_when_lotto_create_then_success() {
         List<LottoNum> lottoNumList = Stream.generate(() -> new LottoNum(1)).limit(6).collect(
             Collectors.toList());
 
-        LottoNums lottoNums = new LottoNums(lottoNumList);
-        Lotto lotto = new Lotto(lottoNums);
+        new LottoNums(lottoNumList);
     }
-
-
 
 
 }
