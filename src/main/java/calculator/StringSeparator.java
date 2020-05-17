0c90e374da62c7b0f0169bbdf7f3b9e02121c8d5
chulman <chlcjfals0122@gmail.com>
2020-05-17 22:01:03 +0900

    feat (StringSeparator): 기본 구현

--- src/main/java/calculator/StringSeparator.java
+++ src/main/java/calculator/StringSeparator.java
@@ -1,12 +1,39 @@
 package calculator;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+
 public class StringSeparator {
 
-    private StringSeparator(){
+    private final static String ZERO = "0";
+
+    private final static String DEFAULT_SEPARATOR = ",|:";
+    private final static String REGEX = "^((\\d+)|(?:[(\\,\\:](\\d+)))+$";
+    private final static Pattern INPUT_PATTERN = Pattern.compile(REGEX);
+
+    private StringSeparator() {
         throw new AssertionError();
     }
 
     public static String[] split(String input) {
-        return null;
+
+        if (StringUtils.isEmpty(input)) {
+            return new String[]{ZERO};
+        }
+
+        validate(input);
+
+        return input.split(DEFAULT_SEPARATOR);
+    }
+
+    private static void validate(String input) {
+        if (!defaultMatches(input)) {
+            throw new IllegalArgumentException("올바르지 않은 구분자 입니다.");
+        }
+    }
+
+    private static boolean defaultMatches(String input) {
+        return INPUT_PATTERN.matcher(input).matches();
     }
 }
