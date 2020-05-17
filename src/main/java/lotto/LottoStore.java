573f6be51205c6165b8634dde67d5cf6b207ddc6
정주영 <jy.jeong@woowahan.com>
2020-05-18 02:18:31 +0900

    refactor lottoStore

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -7,10 +7,22 @@ import java.util.stream.Stream;
 public class LottoStore {
 
     public List<Lotto> sell(long price) {
-        final int lottoCount = (int) (price/1000);
-        return Stream.generate(() -> new Lotto(new LottoNums(Stream.generate(() -> new LottoNum(1)).limit(6).collect(
-            Collectors.toList()))))
-            .limit(lottoCount)
+        return Stream.generate(() -> getLotto())
+            .limit(getLottoCount(price))
             .collect(Collectors.toList());
+    }
+
+    private int getLottoCount(long price) {
+        return (int) (price / 1000);
+    }
+
+    private Lotto getLotto() {
+        return new Lotto(getLottoNums());
+    }
+
+    private LottoNums getLottoNums() {
+        return new LottoNums(Stream.generate(() -> new LottoNum(1))
+            .limit(6)
+            .collect(Collectors.toList()));
     }
 }
