d59e3d656414f6bbaa309917672c2a4951b9b1d3
chulman <chlcjfals0122@gmail.com>
2020-05-17 22:12:59 +0900

    test (StringPlusCalculatorTest): 음수값, 숫자하나, 커스텀 구분자 계산 테스트 추가

--- src/test/java/calculator/StringPlusCalculatorTest.java
+++ src/test/java/calculator/StringPlusCalculatorTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringPlusCalculatorTest {
 
@@ -32,5 +31,29 @@ public class StringPlusCalculatorTest {
     void 문자열_덧셈_다른_구분자_예외_테스트(String input) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> stringPlusCalculator.calculate(input));
+    }
+
+    @CsvSource(value = {"1=1", "2=2", "3=3"}, delimiter = '=')
+    @ParameterizedTest
+    @DisplayName("숫자 하나만 존재하는 input 자열을 계산한다.")
+    void 문자열_덧셈_숫자_하나_테스트(String input, int expected) {
+        int result = stringPlusCalculator.calculate(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @CsvSource(value = {"//;\n1;2;3=6", "//,\n1,2,3=6", "///\n1/2/3=6"}, delimiter = '=')
+    @ParameterizedTest
+    @DisplayName("커스텀 구분자를 계산한다.")
+    void 커스텀_구분_테스트(String input, int expected) {
+        int result = stringPlusCalculator.calculate(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ValueSource(strings = {"//;\n1;2;3", "-1,2", "-2", "2:-2"})
+    @ParameterizedTest
+    @DisplayName("음수 값이 존재할 경우 예외가 발생한다.")
+    void 음수값_구분_예외테스트(String input) {
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(() -> stringPlusCalculator.calculate(input));
     }
 }
