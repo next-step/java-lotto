4a7b28d6e4436b7e1b646920f28d1c266cf277d1
chulman <chlcjfals0122@gmail.com>
2020-05-17 22:26:33 +0900

    test (StringSeparatorTest): 커스텀 구분자 테스트 추가

--- src/test/java/calculator/StringSeparatorTest.java
+++ src/test/java/calculator/StringSeparatorTest.java
@@ -6,12 +6,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringSeparatorTest {
 
-    @CsvSource(value=  {"1:2:3=3", "1=1", "1,2=2"}, delimiter = '=')
+    @CsvSource(value = {"1:2:3=3", "1=1", "1,2=2"}, delimiter = '=')
     @ParameterizedTest
     @DisplayName("기본 구분자(, :) 를 갖는 문자열을 구분한다.")
     void 기본_구분자_테스트(String input, int expected) {
@@ -38,10 +37,34 @@ public class StringSeparatorTest {
 
     @ValueSource(strings = {"1", "2", "3", "4", "5"})
     @ParameterizedTest
-    @DisplayName("숫자 하나를 구분하면 해당 숫자를 반환한다.")
-    void 숫자_하나_구분_테스트(int input) {
-        String[] result = StringSeparator.split(String.valueOf(input));
+    @DisplayName("숫자 하나를 포함한 문자열을 구분하면 해당 숫자를 반환한다.")
+    void 숫자_하나_구분_테스트(String input) {
+        String[] result = StringSeparator.split(input);
         assertThat(result).hasSize(1);
-        assertThat(Integer.parseInt(result[0])).isEqualTo(input);
+        assertThat(Integer.parseInt(result[0])).isEqualTo(Integer.parseInt(input));
+    }
+
+    @ValueSource(strings = {"-1", "-1:2", "2:1,-3"})
+    @ParameterizedTest
+    @DisplayName("음수값을 포함하는 문자열을 구분하면 예외가 발생한다.")
+    void 음수값_구분_테스트(String input) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> StringSeparator.split(input));
+    }
+
+    @CsvSource(value = {"//;\n1;2;3=3", "//,\n1,2,3=3", "///\n1/2=3"}, delimiter = '=')
+    @ParameterizedTest
+    @DisplayName("커스텀 구분자를 포함한 문자열을 구분한다.")
+    void 커스텀_구분_테스트(String input) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> StringSeparator.split(input));
+    }
+
+    @ValueSource(strings = {"//;\n1;2,3", "//,\n1,2!3", "//;\n1=1"})
+    @ParameterizedTest
+    @DisplayName("커스텀 구분자에 포함하지 않은 구분자가 있으면 예외가 발생한다.")
+    void 커스텀_구분_예외_테스트(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringSeparator.split(input));
     }
 }
