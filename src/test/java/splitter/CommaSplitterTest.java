0d790a2c8b1b8b061107711b3dae377437dc2f1a
kwonheejeong <gmlwjd9405@naver.com>
2020-05-18 23:56:50 +0900

    feat: CommaSplitter 에 null 을 입력하면 NullPointerException 예외 발생

--- src/test/java/splitter/CommaSplitterTest.java
+++ src/test/java/splitter/CommaSplitterTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 public class CommaSplitterTest {
 
@@ -22,5 +24,12 @@ public class CommaSplitterTest {
     void failureSplitByEmpty(final String value) {
         assertThat(commaSplitter.split(value))
                 .isEqualTo(new String[0]);
+    }
+
+    @DisplayName("null 을 입력하면 NullPointerException 예외 발생")
+    @ParameterizedTest
+    @NullSource
+    void failureSplitByNull(final String value) {
+        assertThatNullPointerException().isThrownBy(() -> commaSplitter.split(value));
     }
 }
