f8c7b366f29864eff95b435ae559bc48b3fe45bf
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 08:22:27 +0900

    feat: 기본 구분자는 항상 지원 가능

--- src/main/java/splitter/CommaAndColonSplitter.java
+++ src/main/java/splitter/CommaAndColonSplitter.java
@@ -5,6 +5,11 @@ public class CommaAndColonSplitter implements Splitter {
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
 
     @Override
+    public boolean support(final String value) {
+        return true;
+    }
+
+    @Override
     public String[] split(final String value) {
         if (value.isEmpty()) {
             return EMPTY_ARRAY;
