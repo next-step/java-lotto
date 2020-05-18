e67b052ff506ee905e448707de3ad33bb13f0489
kwonheejeong <gmlwjd9405@naver.com>
2020-05-18 23:55:50 +0900

    feat: CommaSplitter 에 빈 문자열을 입력하면 빈 String 배열 반환

--- src/main/java/splitter/CommaSplitter.java
+++ src/main/java/splitter/CommaSplitter.java
@@ -3,8 +3,12 @@ package splitter;
 public class CommaSplitter {
 
     private static final String COMMA = ",";
+    private static final String[] EMPTY_ARRAY = new String[0];
 
     public String[] split(final String value) {
+        if (value.isEmpty()) {
+            return EMPTY_ARRAY;
+        }
         return value.split(COMMA);
     }
 }
