fc78ad6d26dcc6494c7c78955f4491c83e1f82f3
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 00:08:28 +0900

    test: 콜론(:) 또는 콤마(,)를 구분자로 분리한 String 배열을 반환

--- src/test/java/splitter/CommaAndColonSplitterTest.java
+++ src/test/java/splitter/CommaAndColonSplitterTest.java
@@ -3,8 +3,12 @@ package splitter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
@@ -31,5 +35,24 @@ public class CommaAndColonSplitterTest {
     @NullSource
     void failureSplitByNull(final String value) {
         assertThatNullPointerException().isThrownBy(() -> commaAndColonSplitter.split(value));
+    }
+
+    @DisplayName("콜론(:) 또는 콤마(,)를 구분자로 분리한 String 배열을 반환")
+    @ParameterizedTest
+    @MethodSource("splitSuccessCase")
+    void successSplit(final String value, final String[] expected) {
+        assertThat(commaAndColonSplitter.split(value)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> splitSuccessCase() {
+        return Stream.of(
+                Arguments.of("3*4", new String[]{"3*4"}),
+                Arguments.of("//1", new String[]{"//1"}),
+                Arguments.of("\n1", new String[]{"\n1"}),
+                Arguments.of("0", new String[]{"0"}),
+                Arguments.of("10011,2,3", new String[]{"10011", "2", "3"}),
+                Arguments.of("200:22", new String[]{"200", "22"}),
+                Arguments.of("1,2:3", new String[]{"1", "2", "3"})
+        );
     }
 }
