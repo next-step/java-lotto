984cfba3b9115127987548826169bd530b1469b4
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 00:04:22 +0900

    feat: 구분자를 콤마(,) 이외에 콜론(:) 사용 가능

--- src/main/java/splitter/CommaSplitter.java
+++ src/main/java/splitter/CommaSplitter.java
@@ -2,13 +2,13 @@ package splitter;
 
 public class CommaSplitter {
 
-    private static final String COMMA = ",";
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
     private static final String[] EMPTY_ARRAY = new String[0];
 
     public String[] split(final String value) {
         if (value.isEmpty()) {
             return EMPTY_ARRAY;
         }
-        return value.split(COMMA);
+        return value.split(DEFAULT_DELIMITER_REGEX);
     }
 }
