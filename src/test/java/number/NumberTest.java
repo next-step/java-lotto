4ae652ffc88a08bee359e7c2dced8db06c49e9e0
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 09:12:37 +0900

    test: 0 이상의 양의 값 문자가 들어오면 Number 객체 생성 가능

--- src/test/java/number/NumberTest.java
+++ src/test/java/number/NumberTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class NumberTest {
@@ -24,5 +25,13 @@ public class NumberTest {
     void createFailureByNotNumberFormat(final String number) {
         assertThatExceptionOfType(NumberFormatException.class)
                 .isThrownBy(() -> Number.of(number));
+    }
+
+    @DisplayName("0 이상의 양의 값 문자가 들어오면 Number 객체를 생성")
+    @ParameterizedTest
+    @ValueSource(strings = { "0", "1000" })
+    void create(final String number) {
+        assertThatCode(() -> Number.of(number))
+                .doesNotThrowAnyException();
     }
 }
