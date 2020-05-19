a2a2ac8a72f7ab85d364d4b2b5530a1f36d72996
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 09:10:36 +0900

    test: 숫자 이외의 값, null, empty 의 경우 예외 발생

--- src/test/java/number/NumberTest.java
+++ src/test/java/number/NumberTest.java
@@ -2,6 +2,7 @@ package number;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -13,6 +14,15 @@ public class NumberTest {
     @ValueSource(strings = "-1")
     void createFailureByNegativeNumber(final String number) {
         assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> Number.of(number));
+    }
+
+    @DisplayName("생성 실패: 숫자 이외의 값, null, empty 예외 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = "숫자 이외의 값")
+    void createFailureByNotNumberFormat(final String number) {
+        assertThatExceptionOfType(NumberFormatException.class)
                 .isThrownBy(() -> Number.of(number));
     }
 }
