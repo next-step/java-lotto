3ad2461cc22057cbfd75fb2365a5bc5bda07a565
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 20:16:21 +0900

    refactor: 지원하는 구분자가 없는 경우 NoSupportedSplitterException 반환

--- src/main/java/splitter/SplitterManager.java
+++ src/main/java/splitter/SplitterManager.java
@@ -1,5 +1,7 @@
 package splitter;
 
+import exception.NoSupportedSplitterException;
+
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -24,6 +26,6 @@ public class SplitterManager {
         return splitters.stream()
                 .filter(splitter -> splitter.support(value))
                 .findFirst()
-                .orElse(new CommaAndColonSplitter());
+                .orElseThrow(NoSupportedSplitterException::new);
     }
 }
