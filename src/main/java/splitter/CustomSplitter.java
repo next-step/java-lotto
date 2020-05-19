30620a8fd565669b13c506011bfaf7bbceb3016b
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 17:03:22 +0900

    refactor: 정규표현식 메타 문자 리스트를 불변값으로 수정

--- src/main/java/splitter/CustomSplitter.java
+++ src/main/java/splitter/CustomSplitter.java
@@ -1,7 +1,7 @@
 package splitter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,7 +12,7 @@ public class CustomSplitter implements Splitter {
     private static final String EMPTY_OPERAND_REGEX = "//(.)\n";
     private static final int INDEX_OF_DELIMITER = 1;
     private static final int INDEX_OF_VALUE = 2;
-    private static final List<String> REGEX_META_CHAR = new ArrayList<>(
+    private static final List<String> REGEX_META_CHAR = Collections.unmodifiableList(
             Arrays.asList(".", "|", "^", "$", "*", "+", "?", "(", "[", "{", ")")
     );
     private static final Pattern customPattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
