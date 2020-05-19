ffab342ec0a3054be5834eb6beb76a50f05a651b
JuWanHan <baekseongsa@naver.com>
2020-05-19 22:43:04 +0900

    feat(InputView): 지난 주 당첨 번호 입력 구현

--- src/main/java/lotto/exception/InputValueException.java
+++ src/main/java/lotto/exception/InputValueException.java
@@ -2,7 +2,7 @@ package lotto.exception;
 
 public class InputValueException extends RuntimeException {
 
-    public InputValueException() {
-        super("구입금액을 1000원보다 작게 입력할 수 없습니다.");
+    public InputValueException(String message) {
+        super(message);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,28 +2,60 @@ package lotto.view;
 
 import lotto.exception.InputValueException;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public final class InputView {
 
-    private static Scanner scanner = new Scanner(System.in);
-    private static int MIN_PURCHASE_AMOUNT = 1000;
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String DELIMITER_PATTERN = ", ";
+    private static final int MIN_PURCHASE_AMOUNT = 1000;
 
     private InputView() {
     }
 
     public static int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        int purchaseAmount = scanner.nextInt();
+        String purchaseAmount = SCANNER.nextLine();
 
-        validatePurchaseAmount(purchaseAmount);
+        validateBlank(purchaseAmount);
 
-        return purchaseAmount;
+        int amount = convertStringToInt(purchaseAmount);
+        validatePurchaseAmount(amount);
+
+        return amount;
+    }
+
+    public static List<Integer> getLastWeekLottoNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String lottoNumbers = SCANNER.nextLine();
+
+        validateBlank(lottoNumbers);
+
+        return Arrays.stream(lottoNumbers.split(DELIMITER_PATTERN))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     private static void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < MIN_PURCHASE_AMOUNT) {
-            throw new InputValueException();
+            throw new InputValueException("구입금액을 1000원보다 작게 입력할 수 없습니다.");
+        }
+    }
+
+    private static int convertStringToInt(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new InputValueException("구입금액을 숫자로 입력하세요.");
+        }
+    }
+
+    private static void validateBlank(String value) {
+        if (value == null || value.trim().isEmpty()) {
+            throw new InputValueException("데이터를 입력하세요.");
         }
     }
 }
