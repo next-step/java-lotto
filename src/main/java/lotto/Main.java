f68854b3a0a051c7b4924a5d22d2a8baefe9ceec
JuWanHan <baekseongsa@naver.com>
2020-05-20 01:35:07 +0900

    feat(Main): 로또 실행 로직 구현

---
+++ src/main/java/lotto/Main.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import lotto.domain.LottoGame;
+import lotto.domain.LottoMatchResult;
+import lotto.domain.LottoNumbers;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame(InputView.getPurchaseAmount());
+
+        ResultView.printPurchaseCount(lottoGame.getPurchaseCount());
+
+        List<LottoNumbers> lottoNumbers = lottoGame.createLottoNumbers();
+        ResultView.printLottoNumbers(lottoNumbers);
+
+        LottoNumbers lastWeekLottoNumbers = LottoNumbers.newInstance(InputView.getLastWeekLottoNumbers());
+
+        LottoMatchResult lottoMatchResult = LottoMatchResult.newInstance(lastWeekLottoNumbers, lottoNumbers);
+        lottoMatchResult.run();
+
+        ResultView.printMatchResult(lottoMatchResult);
+    }
+}
