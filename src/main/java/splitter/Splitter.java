ccb53df1ad7c1b05e1bb85ae1590d671b344852b
kwonheejeong <gmlwjd9405@naver.com>
2020-05-19 14:06:25 +0900

    refactor: 빈문자열 초기화에서 매직넘버 제거

--- src/main/java/splitter/Splitter.java
+++ src/main/java/splitter/Splitter.java
@@ -2,7 +2,7 @@ package splitter;
 
 public interface Splitter {
 
-    String[] EMPTY_ARRAY = new String[0];
+    String[] EMPTY_ARRAY = new String[] {};
 
     boolean support(String value);
     String[] split(String value);
