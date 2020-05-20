a014bd4d1a54f965e10fa6d14cc309a017cfa331
kwonheejeong <gmlwjd9405@naver.com>
2020-05-20 19:17:28 +0900

    feat: 로또 1장의 가격보다 낮은 금액을 내면 예외 발생

---
+++ src/main/java/lotto/domain/seller/LottoSeller.java
@@ -0,0 +1,33 @@
+package lotto.domain.seller;
+
+import lotto.domain.number.LottoTicket;
+import lotto.domain.strategy.NumberGenerationStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoSeller {
+
+    private static final int ONE_TICKET_PRICE = 1000;
+
+    private final NumberGenerationStrategy generationStrategy;
+
+    private LottoSeller(final NumberGenerationStrategy generationStrategy) {
+        this.generationStrategy = generationStrategy;
+    }
+
+    public static LottoSeller of(final NumberGenerationStrategy generationStrategy) {
+        return new LottoSeller(generationStrategy);
+    }
+
+    public List<LottoTicket> buyTicket(final int money) {
+        validateMoney(money);
+        return new ArrayList<>();
+    }
+
+    private void validateMoney(final int money) {
+        if (money < ONE_TICKET_PRICE) {
+            throw new IllegalArgumentException("최소 금액 " + ONE_TICKET_PRICE + "원이 필요합니다.");
+        }
+    }
+}
