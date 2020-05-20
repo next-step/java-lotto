13b76241eeac013622e3018c0772d4301764bdca
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-20 09:57:29 +0900

    refactor(InputView): static constructor
    - 정적 팩토리 메서드로 정규표현식을 처리하도록 변경

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -4,12 +4,39 @@ import domain.Number;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
+    private String divider;
+    private String targetString;
+
+    private InputView(String divider, String targetString) {
+        this.divider = divider;
+        this.targetString = targetString;
+    }
+
     public List<Number> extractNumbers(String input) {
         return Arrays.stream(input.split("[,:]"))
                 .map(Number::new)
                 .collect(Collectors.toList());
+    }
+
+    public static InputView create(String input) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (matcher.find()) {
+            String divider = "[," + matcher.group(1) + ":]";
+            return new InputView(divider, matcher.group(2));
+        }
+        return new InputView("[,:]", input);
+    }
+
+    public String getDivider() {
+        return divider;
+    }
+
+    public String getTargetString() {
+        return targetString;
     }
 }
--- src/test/java/ui/InputViewTests.java
+++ src/test/java/ui/InputViewTests.java
@@ -1,28 +1,24 @@
 package ui;
 
 import domain.Number;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputViewTests {
-    private InputView inputView;
-
-    @BeforeEach
-    void setup() {
-        inputView = new InputView();
-    }
-
     @DisplayName("입력값 중 기본 구분자(:와 쉼표)를 제외한 숫자만 골라낸 콜렉션 출력")
     @ParameterizedTest
     @ValueSource(strings = {"112:3:5,523:123,1", "112:3:5:523:123:1", "112,3,5,523,123,1"})
     void compositeParser(String input) {
+        InputView inputView = InputView.create(input);
         List<Number> numbers = inputView.extractNumbers(input);
         assertThat(numbers.get(0)).isEqualTo(new Number("112"));
         assertThat(numbers.get(3)).isEqualTo(new Number("523"));
@@ -32,6 +28,21 @@ class InputViewTests {
     @ParameterizedTest
     @ValueSource(strings = {"112:3:5,523:123,-1", "112:3:5,523:123,hello"})
     void parserFailWithInvalidInputs(String input) {
-        assertThatThrownBy(() -> inputView.extractNumbers(input)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> InputView.create(input).extractNumbers(input)).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("커스텀 구분자가 있는 경우 커스텀 구분자를 포함한 regex 반환")
+    @ParameterizedTest
+    @MethodSource("stringIncludedCustomDivider")
+    void getDividerPatternTest(String input, String divider, String target) {
+        InputView inputView = InputView.create(input);
+        assertThat(inputView.getDivider()).isEqualTo(divider);
+        assertThat(inputView.getTargetString()).isEqualTo(target);
+    }
+    public static Stream<Arguments> stringIncludedCustomDivider() {
+        return Stream.of(
+                Arguments.of("//!\n112!3:5,523:123!1", "[,!:]", "112!3:5,523:123!1"),
+                Arguments.of("//;\n112;3:5,523:123;1", "[,;:]", "112;3:5,523:123;1")
+        );
     }
 }
