72b6777d00a50160cdc5c2f0a42f482d56603673
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-21 08:36:14 +0900

    feat(LottoNumber)
    - 로또에서 사용될 번호의 범위를 제한함

---
+++ src/test/java/lotto/domain/lotto/LottoNumberTests.java
@@ -0,0 +1,32 @@
+package lotto.domain.lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTests {
+    @DisplayName("숫자를 입력받아서 로또 번호 객체를 생성할 수 있음")
+    @Test
+    void createLottoNumber() {
+        LottoNumber lottoNumber = LottoNumber.create(1);
+        assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("1 ~ 45 범위의 값을 지난 객체만 생성할 수 있음")
+    @ParameterizedTest
+    @ValueSource(ints = { -100, 0, 46, 100 })
+    void validationCheck(int invalidValue) {
+        assertThatThrownBy(() -> LottoNumber.create(invalidValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("동등성 비교")
+    @Test
+    void equalTest() {
+        assertThat(LottoNumber.create(1)).isEqualTo(LottoNumber.create(1));
+    }
+}
