3faf20fd0d7ba3cc804bdc8ae5df818a7d31e0c3
kwonheejeong <gmlwjd9405@naver.com>
2020-05-20 21:00:06 +0900

    test: 로또 당첨 번호 테스트 코드 추가

---
+++ src/test/java/lotto/domain/ticket/WinningLottoTicketTest.java
@@ -0,0 +1,47 @@
+package lotto.domain.ticket;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class WinningLottoTicketTest {
+
+    @DisplayName("WinningLottoTicket 생성")
+    @Test
+    void create() {
+        assertThatCode(() -> WinningLottoTicket.of("1, 2, 3, 4, 5, 6"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("WinningLottoTicket 생성 실패: 로또 슷자의 개수가 6개가 아닌 경우 예외 발생")
+    @ParameterizedTest
+    @MethodSource("lottoNumbersCase")
+    void notMetNumberCountCondition(final List<Integer> numbers) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoTicket.of(numbers));
+    }
+
+    private static Stream<Arguments> lottoNumbersCase() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5)),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7)),
+                Arguments.of(Arrays.asList(-1, 0, 3, 4, 5, 6))
+        );
+    }
+
+    @DisplayName("WinningLottoTicket 생성 실패: 1 ~ 45 이외의 숫자가 포함된 경우 예외 발생")
+    @Test
+    void outOfRangeLottoNumber() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoTicket.of(Arrays.asList(-1, 0, 3, 4, 5, 6)));
+    }
+}
