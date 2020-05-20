e159cda0431c6511cffbb1a003572f1197b48bf9
JuWanHan <baekseongsa@naver.com>
2020-05-20 23:53:44 +0900

    test(LottoNumbersTest): 테스트 코드 개선

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
@@ -9,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumbersTest {
+
+    private LottoNumbers lottoNumbers = LottoNumbers.newInstance(Arrays.asList(1, 2, 3, 4, 5, 6));
 
     @DisplayName("로또 번호가 없으면 로또 번호를 생성할 수 없다.")
     @Test
@@ -37,21 +41,26 @@ class LottoNumbersTest {
     @DisplayName("로또 번호를 생성할 수 있다.")
     @Test
     void canCreateLottoNumbers() {
-        assertThat(LottoNumbers.newInstance(Arrays.asList(1, 2, 3, 4, 5, 6))).isNotNull();
+        assertThat(lottoNumbers).isNotNull();
     }
 
     @DisplayName("로또 번호를 출력할 수 있다.")
     @Test
     void canPrintLottoNumbersToString() {
-        assertThat(LottoNumbers.newInstance(Arrays.asList(1, 2, 3, 4, 5, 6)).toString())
+        assertThat(lottoNumbers.toString())
                 .isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 
     @DisplayName("다른 로또 번호와 일치하는 번호 개수를 구할 수 있다.")
     @Test
     void canGetMatchCountOtherLottoNumbers() {
-        LottoNumbers lottoNumbers = LottoNumbers.newInstance(Arrays.asList(1, 2, 3, 4, 5, 6));
         assertThat(lottoNumbers.getMatchCount(LottoNumbers.newInstance(Arrays.asList(1, 2, 9, 4, 11, 26))))
                 .isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "1,true", "2,true", "3,true", "7,false" })
+    void canContainsNumber(int number, boolean result) {
+        assertThat(lottoNumbers.isMatchNumber(number)).isEqualTo(result);
     }
 }
