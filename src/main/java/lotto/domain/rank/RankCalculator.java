af8e91a14d41c2acd8bbcce1efcd98af5da3fdec
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-21 08:46:10 +0900

    feat(RankCalculator)
    - 당첨 티켓을 인자로 받아서 객체를 생성할 수 있다.
    - getRank 메서드로 입력받은 로또 티켓과 당첨 티켓을 비교해서 Rank를 알려줄 수 있다.

---
+++ src/main/java/lotto/domain/rank/RankCalculator.java
@@ -0,0 +1,22 @@
+package lotto.domain.rank;
+
+import lotto.domain.lotto.LottoTicket;
+
+public class RankCalculator {
+    private LottoTicket winTicket;
+
+    public RankCalculator(LottoTicket lottoTicket) {
+        this.winTicket = lottoTicket;
+    }
+
+    protected int matchNumberCalculate(LottoTicket lottoTicket) {
+        return (int) lottoTicket.getValues().stream()
+                .filter(lottoNumber -> winTicket.getValues().contains(lottoNumber))
+                .count();
+    }
+
+    public Rank getRank(LottoTicket lottoTicket) {
+        int matchNumber = matchNumberCalculate(lottoTicket);
+        return Rank.create(matchNumber);
+    }
+}
