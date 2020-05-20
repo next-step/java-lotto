578b203ccabdf04b26120b3a28bd021560936119
kwonheejeong <gmlwjd9405@naver.com>
2020-05-20 17:00:11 +0900

    feat: 로또 숫자 개수 검증 및 Set 으로 변경

--- src/main/java/lotto/domain/number/LottoTicket.java
+++ src/main/java/lotto/domain/number/LottoTicket.java
@@ -1,22 +1,31 @@
 package lotto.domain.number;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
 
-    private List<LottoNumber> numbers;
+    private Set<LottoNumber> numbers;
 
     private LottoTicket() {
     }
 
     private LottoTicket(final List<LottoNumber> numbers) {
-        this.numbers = numbers;
+        validateNumbers(numbers);
+        this.numbers = new LinkedHashSet<>(numbers);
     }
 
     public static LottoTicket of(final List<Integer> numbers) {
         return new LottoTicket(numbers.stream()
                 .map(LottoNumber::of)
                 .collect(Collectors.toList()));
+    }
+
+    private void validateNumbers(final List<LottoNumber> numbers) {
+        if (numbers == null || numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
     }
 }
