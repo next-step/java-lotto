a765ee13d8103b4c0a89fc56c2138720d9a9d12b
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-21 12:15:16 +0900

    feat(LottoService): calculateRank
    - 1등 당첨 티켓과 구매한 LottoTickets를 입력받아서 당첨 결과를 계산할 수 있다.

--- src/main/java/lotto/application/LottoService.java
+++ src/main/java/lotto/application/LottoService.java
@@ -1,10 +1,25 @@
 package lotto.application;
 
+import lotto.domain.lotto.LottoTicket;
 import lotto.domain.lotto.LottoTickets;
+import lotto.domain.rank.Rank;
+import lotto.domain.rank.RankCalculator;
+import lotto.domain.rank.Ranks;
 import lotto.domain.shop.Shop;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoService {
     public LottoTickets buyLottoTickets(long money) {
         return Shop.buyLottoTickets(money);
+    }
+
+    public Ranks calculateRank(LottoTicket winTicket, LottoTickets lottoTickets) {
+        RankCalculator rankCalculator = new RankCalculator(winTicket);
+        List<Rank> rankList = lottoTickets.getValues().stream()
+                .map(rankCalculator::getRank)
+                .collect(Collectors.toList());
+        return Ranks.create(rankList);
     }
 }
--- src/test/java/lotto/application/LottoServiceTests.java
+++ src/test/java/lotto/application/LottoServiceTests.java
@@ -1,9 +1,14 @@
 package lotto.application;
 
+import lotto.domain.lotto.LottoGenerator;
+import lotto.domain.lotto.LottoTicket;
 import lotto.domain.lotto.LottoTickets;
+import lotto.domain.rank.Ranks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +25,15 @@ class LottoServiceTests {
     void buyLottoTest() {
         LottoTickets lottoTickets = lottoService.buyLottoTickets(10000);
         assertThat(lottoTickets.size()).isEqualTo(10);
+    }
+
+    @DisplayName("당첨 티켓 번호와 구매한 LottoTickets를 받아서 당첨 결과를 반환할 수 있다.")
+    @Test
+    void rankCalculateTest() {
+        LottoTicket winTicket = LottoGenerator.createManualByIntList(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoTickets lottoTickets = LottoTickets.create(
+                Arrays.asList(LottoGenerator.create(), LottoGenerator.create()));
+
+        assertThat(lottoService.calculateRank(winTicket, lottoTickets)).isInstanceOf(Ranks.class);
     }
 }
