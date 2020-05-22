e8d2dfcbf99fa0b61cc045075463dd40a8fd2869
kwonheejeong <gmlwjd9405@naver.com>
2020-05-22 09:05:26 +0900

    refactor: Map을 이용하여 일치하는 개수에 대한 Prize 반환하도록 수정

--- src/main/java/lotto/domain/prize/Prize.java
+++ src/main/java/lotto/domain/prize/Prize.java
@@ -1,17 +1,23 @@
 package lotto.domain.prize;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public enum Prize {
 
     MISS(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5,  1500000),
-    FIRST(6,  2000000000);
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     private int matchedNumbersCount;
     private int prizeMoney;
+
+    private static final Map<Integer, Prize> PRIZES = Arrays.stream(Prize.values())
+            .collect(Collectors.toMap(Prize::getMatchedNumbersCount, Function.identity()));
 
     Prize(final int matchedNumbersCount, final int prizeMoney) {
         this.matchedNumbersCount = matchedNumbersCount;
@@ -19,10 +25,14 @@ public enum Prize {
     }
 
     public static Prize of(final int matchedNumbersCount) {
-        return Arrays.stream(Prize.values())
-                .filter(prize -> prize.matchedNumbersCount == matchedNumbersCount)
-                .findFirst()
-                .orElse(Prize.MISS);
+        if (!PRIZES.containsKey(matchedNumbersCount)) {
+            return MISS;
+        }
+        return PRIZES.get(matchedNumbersCount);
+    }
+
+    private int getMatchedNumbersCount() {
+        return matchedNumbersCount;
     }
 
     public int getPrizeMoney() {
