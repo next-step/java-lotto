51b021ee6b676e08b0078906cf118bf6feba7b76
kwonheejeong <gmlwjd9405@naver.com>
2020-05-22 12:51:30 +0900

    test: 음수 또는 상금에 포함되지 않는 일치하는 개수인 경우 예외 반환

--- src/test/java/lotto/domain/prize/PrizeTest.java
+++ src/test/java/lotto/domain/prize/PrizeTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PrizeTest {
 
@@ -26,5 +28,12 @@ public class PrizeTest {
                 Arguments.of(5, Prize.THIRD),
                 Arguments.of(6, Prize.FIRST)
         );
+    }
+
+    @DisplayName("음수 또는 상금에 포함되지 않는 일치하는 개수인 경우 예외 반환")
+    @ParameterizedTest
+    @ValueSource(ints = { -1, 7 })
+    void matchedPrizeFailure(final int matchedCount) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Prize.of(matchedCount));
     }
 }
