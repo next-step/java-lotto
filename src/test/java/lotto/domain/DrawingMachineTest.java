25bff57e4f7de37d3a18abdb5964a4e8877dc0ef
Gobukgol <zxczc159@gmail.com>
2020-05-22 15:55:41 +0900

    feat(DrawingMachineTest): DrawingMachine.drawAllLottoTicket  테스트 메소드 구현

--- src/test/java/lotto/domain/DrawingMachineTest.java
+++ src/test/java/lotto/domain/DrawingMachineTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,6 +49,30 @@ class DrawingMachineTest {
         int actualMatchCount = drawingMachine.drawLottoTicket(lottoTicket);
 
         assertThat(actualMatchCount).isEqualTo(matchCount);
+    }
+
+    @DisplayName("모든 로또 티켓과 당첨 번홀르 비교하여 맞은 개수의 리스트를 반환한다.")
+    @Test
+    void drawAllLotto() {
+        DrawingMachine drawingMachine = new DrawingMachine(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        List<Integer> actualMatchCounts = drawingMachine.drawAllLottoTicket(createLottoTickets(3));
+
+        assertThat(actualMatchCounts).contains(6, 5, 4);
+    }
+
+    private List<LottoTicket> createLottoTickets(int numberOfTicket) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+
+        for (int startNumber = 1; startNumber <= numberOfTicket; startNumber++) {
+            List<Integer> numbers = IntStream.rangeClosed(startNumber, startNumber + 5)
+                    .boxed()
+                    .collect(Collectors.toList());
+            LottoTicket lottoTicket = createLottoTicket(numbers);
+            lottoTickets.add(lottoTicket);
+        }
+
+        return lottoTickets;
     }
 
     private LottoTicket createLottoTicket(List<Integer> numbers) {
