39d244f2f3868b7d22d0ae6482252dbd62195a0a
kwonheejeong <gmlwjd9405@naver.com>
2020-05-22 15:15:16 +0900

    feat: 구입 가능한 티켓의 개수 반환

---
+++ src/main/java/lotto/domain/Price.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import lotto.exception.ErrorMessage;
+import lotto.exception.NegativeNumberException;
+
+public class Price {
+
+    static final int MIN_PRICE = 0;
+    public static final int ONE_TICKET_PRICE = 1000;
+
+    private final int price;
+
+    private Price(final int price) {
+        validateNegative(price);
+        this.price = price;
+    }
+
+    public static Price of(final int price) {
+        return new Price(price);
+    }
+
+    private void validateNegative(final int price) {
+        if (price < MIN_PRICE) {
+            throw new NegativeNumberException(price);
+        }
+    }
+
+    public void validateAvailablePrice() {
+        if (price < ONE_TICKET_PRICE) {
+            throw new IllegalArgumentException(String.format(ErrorMessage.REQUIRED_MIN_PRICE, MIN_PRICE));
+        }
+    }
+
+    public int ticketCount() {
+        return price / ONE_TICKET_PRICE;
+    }
+}
