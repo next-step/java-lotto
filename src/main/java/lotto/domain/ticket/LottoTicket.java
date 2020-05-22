cc8f7fd5a11cf24f0ff6edb295125375c9e4c30a
kwonheejeong <gmlwjd9405@naver.com>
2020-05-22 16:59:15 +0900

    feat: LottoTicket 구현

---
+++ src/main/java/lotto/domain/ticket/LottoTicket.java
@@ -0,0 +1,30 @@
+package lotto.domain.ticket;
+
+import lotto.domain.number.LottoNumbers;
+import lotto.domain.prize.Prize;
+import lotto.exception.ErrorMessage;
+
+public class LottoTicket {
+
+    private final LottoNumbers lottoNumbers;
+
+    private LottoTicket(final LottoNumbers lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public static LottoTicket of(final LottoNumbers lottoNumbers) {
+        return new LottoTicket(lottoNumbers);
+    }
+
+    private void validate(final LottoNumbers lottoNumbers) {
+        if (lottoNumbers == null) {
+            throw new IllegalArgumentException(ErrorMessage.NULL_VALUE);
+        }
+    }
+
+    public Prize matchPrize(final WinningLottoTicket winningLottoTicket) {
+        int matchCount = lottoNumbers.matchCount(winningLottoTicket);
+        return Prize.of(matchCount);
+    }
+}
