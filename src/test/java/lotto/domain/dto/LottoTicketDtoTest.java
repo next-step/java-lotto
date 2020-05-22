22f67aae21fbe947fb1769e573994d8255e0e592
kwonheejeong <gmlwjd9405@naver.com>
2020-05-22 19:25:43 +0900

    test: LottoTickets 으로부터 List<LottoTicketDTO> 를 반환

---
+++ src/test/java/lotto/domain/dto/LottoTicketDtoTest.java
@@ -0,0 +1,30 @@
+package lotto.domain.dto;
+
+import lotto.domain.number.LottoNumbers;
+import lotto.domain.ticket.LottoTicket;
+import lotto.domain.ticket.LottoTickets;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketDtoTest {
+
+    @DisplayName("LottoTickets 으로부터 List<LottoTicketDTO> 를 반환")
+    @Test
+    void from() {
+        List<Integer> lottoNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        List<LottoTicket> tickets = new ArrayList<>();
+        tickets.add(LottoTicket.of(LottoNumbers.of("1, 2, 3, 4, 5, 6")));
+        LottoTickets lottoTickets = LottoTickets.of(tickets);
+
+        List<LottoTicketDto> ticketDTOS = LottoTicketDto.from(lottoTickets);
+
+        assertThat(ticketDTOS.get(0).getNumbers()).isEqualTo(lottoNumbers);
+    }
+}
