486ef65a2bb3dbafa552808728d4d9536d4050f7
Sungjun <sungjunpizz@gmail.com>
2020-05-23 22:45:59 +0900

    test: 숫자 아닌 경우 예외 TC 추가

--- src/test/java/calculator/domain/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/domain/calculator/StringCalculatorTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("StringCalculator 클래스 테스트")
 public class StringCalculatorTest {
 
     @DisplayName("문자열 List를 입력받아 Number List를 생성할 수 있다.")
     @Test
-    void number () {
+    void number() {
         String[] texts = {"1", "2", "3"};
 
         StringCalculator stringCalculator = new StringCalculator(texts);
@@ -18,4 +19,12 @@ public class StringCalculatorTest {
         assertThat(stringCalculator).isNotNull();
         assertThat(stringCalculator.getNumbers().get(0).getNumber()).isEqualTo(1);
     }
+
+    @DisplayName("숫자가 아닌 값을 전달하는 경우 RuntiemException 예외를 throw 할 수 있다.")
+    @Test
+    void NonNumericValues() {
+        String[] texts = {"1", "-", "3"};
+        assertThatThrownBy(() -> new StringCalculator(texts)).isInstanceOf(NonNumericValuesException.class);
+    }
 }
+
