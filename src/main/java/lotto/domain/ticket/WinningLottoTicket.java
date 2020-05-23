af7aed5de1f5c7cd67c9c5d98f1dd0e32d85cb2c
kwonheejeong <gmlwjd9405@naver.com>
2020-05-23 12:01:06 +0900

    feat: 보너스 볼은 기존의 당첨번호와 중복 불가

--- src/main/java/lotto/domain/ticket/WinningLottoTicket.java
+++ src/main/java/lotto/domain/ticket/WinningLottoTicket.java
@@ -2,6 +2,7 @@ package lotto.domain.ticket;
 
 import lotto.domain.number.LottoNumber;
 import lotto.domain.number.LottoNumbers;
+import lotto.exception.ErrorMessage;
 
 public class WinningLottoTicket extends LottoTicket {
 
@@ -15,7 +16,14 @@ public class WinningLottoTicket extends LottoTicket {
     public static WinningLottoTicket valueOf(final String winningNumbers, final int bonusNumber) {
         LottoNumbers lottoNumbers = LottoNumbers.of(winningNumbers);
         LottoNumber bonusLottoNumber = LottoNumber.of(bonusNumber);
+        validateDuplication(lottoNumbers, bonusLottoNumber);
 
         return new WinningLottoTicket(lottoNumbers, bonusLottoNumber);
+    }
+
+    private static void validateDuplication(LottoNumbers lottoNumbers, LottoNumber bonusLottoNumber) {
+        if (lottoNumbers.contains(bonusLottoNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_BONUS_NUMBER);
+        }
     }
 }
