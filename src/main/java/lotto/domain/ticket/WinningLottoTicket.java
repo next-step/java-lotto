593e05cc7f0fde71cb61cf3e56ffeb0008ab1fbe
kwonheejeong <gmlwjd9405@naver.com>
2020-05-23 11:54:25 +0900

    feat: 당첨 번호와 보너스 볼로 WinningLottoTicket을 생성

--- src/main/java/lotto/domain/ticket/WinningLottoTicket.java
+++ src/main/java/lotto/domain/ticket/WinningLottoTicket.java
@@ -1,14 +1,21 @@
 package lotto.domain.ticket;
 
+import lotto.domain.number.LottoNumber;
 import lotto.domain.number.LottoNumbers;
 
 public class WinningLottoTicket extends LottoTicket {
 
-    private WinningLottoTicket(final LottoNumbers numbers) {
+    private LottoNumber bonusNumber;
+
+    private WinningLottoTicket(final LottoNumbers numbers, final LottoNumber bonusNumber) {
         super(numbers);
+        this.bonusNumber = bonusNumber;
     }
 
-    public static WinningLottoTicket of(final String winningNumbers) {
-        return new WinningLottoTicket(LottoNumbers.of(winningNumbers));
+    public static WinningLottoTicket valueOf(final String winningNumbers, final int bonusNumber) {
+        LottoNumbers lottoNumbers = LottoNumbers.of(winningNumbers);
+        LottoNumber bonusLottoNumber = LottoNumber.of(bonusNumber);
+
+        return new WinningLottoTicket(lottoNumbers, bonusLottoNumber);
     }
 }
