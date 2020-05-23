e81819fb14a1d3f324ab39c460e146370dc62e67
hy43 <hyde0401@gmail.com>
2020-05-23 23:35:56 +0900

    update StringAddCalculator 커스텀 구분자, 음수 처리, null 또는 빈문자 처리 구현(테스트 전체 통과)

--- src/main/java/domain/StringAddCalculator.java
+++ src/main/java/domain/StringAddCalculator.java
@@ -1,14 +1,33 @@
 package domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-	public static int splitAndSum(String inputString) {
-		String[] inputStringArray = inputString.split(":|,");
+	public static int splitAndSum(String operationTargetString) {
+		if(operationTargetString == null || operationTargetString.isEmpty()) {
+			return 0;
+		}
+		String[] operationTargets = null;
 		int result = 0;
-		for(String inputValue : inputStringArray) {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(operationTargetString);
+		if (m.find()) {
+		    String customDelimiter = m.group(1);
+		    operationTargets= m.group(2).split(customDelimiter);
+		    // 덧셈 구현
+		}
+
+		if (operationTargets == null) {
+			operationTargets = operationTargetString.split(":|,");
+		}
+		
+		for(String inputValue : operationTargets) {
+			if(Integer.parseInt(inputValue) < 0) {
+				throw new RuntimeException();
+			}
 			result += Integer.parseInt(inputValue);
 		}
 		return result;
 	}
-
 }
