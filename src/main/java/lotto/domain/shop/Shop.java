4e2e12b0ece6ffd5872244f094c7c45c5b9c8acc
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-23 11:20:02 +0900

    refactor(Shop): sellLottoTickets
    - 좀 더 응집도 높은 구현을 위해 받은 돈에서 로또 티켓을 얼마나 살 수 있는지 계산하는 책임을 Money 객체로 이관

--- src/main/java/lotto/domain/shop/Shop.java
+++ src/main/java/lotto/domain/shop/Shop.java
@@ -7,12 +7,8 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Shop {
-    protected static int getLottoTicketCount(Money money) {
-        return (int) (money.getValue() / 1000);
-    }
-
     public static LottoTickets sellLottoTickets(Money money) {
-        int lottoTicketCount = getLottoTicketCount(money);
+        int lottoTicketCount = money.howManyLottoTickets();
         return IntStream.range(0, lottoTicketCount)
                 .mapToObj(num -> LottoGenerator.create())
                 .collect(Collectors.collectingAndThen(Collectors.toList(), LottoTickets::create));
--- src/test/java/lotto/domain/shop/ShopTests.java
+++ src/test/java/lotto/domain/shop/ShopTests.java
@@ -3,20 +3,10 @@ package lotto.domain.shop;
 import lotto.domain.lotto.LottoTickets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ShopTests {
-    @DisplayName("돈을 내면 몇 개의 로또 티켓을 살 수 있는지 계산한다.")
-    @ParameterizedTest
-    @CsvSource(value = { "10000:10", "11000:11", "12000:12" }, delimiter = ':')
-    void getLottoTicketCountTest(int moneyValue, int count) {
-        int lottoTicketCount = Shop.getLottoTicketCount(new Money(moneyValue));
-        assertThat(lottoTicketCount).isEqualTo(count);
-    }
-
     @DisplayName("낸 돈으로 최대한 살 수 있는 LottoTicket을 판매한다.")
     @Test
     void sellLottoTickets() {
