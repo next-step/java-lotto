ef233f6dcae2d0e121cb188d6c8a87bd925a109b
Sungjun <sungjunpizz@gmail.com>
2020-05-23 23:52:56 +0900

    refactor: Main 클래스 수정
    - 숫자 아닌 값 예외 체크 추가
    - StringCalculator 호출 및 결과 리턴

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,6 +1,8 @@
 package calculator;
 
-
+import calculator.domain.calculator.Calculator;
+import calculator.domain.calculator.NonNumericValuesException;
+import calculator.domain.calculator.StringCalculator;
 import calculator.domain.splitter.CommaAndColonSplitter;
 import calculator.domain.splitter.CustomSplitter;
 import calculator.domain.splitter.Splitter;
@@ -10,22 +12,37 @@ import java.util.Arrays;
 public class Main {
     private static final int ZERO = 0;
 
-    public int calculate(String input) {
+    public static int calculate(String input) {
 
         if (emptyCheck(input)) {
             return ZERO;
         }
 
         String[] splitInput = split(input);
+        validateNonNumericValues(splitInput);
 
-        return 0;
+        Calculator stringCalculator = new StringCalculator(splitInput);
+
+        return stringCalculator.sum();
     }
 
     private static boolean emptyCheck(String input) {
         return input == null || input.isEmpty();
     }
 
-    private String[] split(String input) {
+    private static void validateNonNumericValues(String[] inputs) {
+        Arrays.stream(inputs).forEach(Main::numericCheck);
+    }
+
+    private static void numericCheck(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NonNumericValuesException();
+        }
+    }
+
+    private static String[] split(String input) {
         Splitter customSplitter = new CustomSplitter();
 
         if (customSplitter.isSupport(input)) {
