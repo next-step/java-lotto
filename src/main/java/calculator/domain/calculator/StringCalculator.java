ea456e98d8b90c97385adefdf74ed226d63b9a88
Sungjun <sungjunpizz@gmail.com>
2020-05-23 22:44:56 +0900

    feat: 숫자 아닌 경우 예외 처리, sum 구현
    - 생성자에서 Number 생성시 숫자가 아니면 Exception throw
    - sum 메소드 구현

--- src/main/java/calculator/domain/calculator/StringCalculator.java
+++ src/main/java/calculator/domain/calculator/StringCalculator.java
@@ -4,19 +4,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StringCalculator implements Calculator{
+public class StringCalculator implements Calculator {
+    private static final Number ZERO = new Number(0);
 
     List<Number> numbers;
 
     public StringCalculator(String[] texts) {
-        this.numbers = Arrays.stream(texts)
-                .map(Number::new)
-                .collect(Collectors.toList());
+        this.numbers = StringToNumber(texts);
+    }
+
+    private List<Number> StringToNumber(String[] texts) {
+        try {
+            return Arrays.stream(texts)
+                    .map(Number::new)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new NonNumericValuesException();
+        }
     }
 
     @Override
     public int sum() {
-        return 0;
+        return numbers.stream().reduce(ZERO, Number::add).getNumber();
     }
 
     public List<Number> getNumbers() {
