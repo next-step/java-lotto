aaf09e39a9b2739b6b7abbda84eaaa11176faddc
Sungjun <sungjunpizz@gmail.com>
2020-05-23 23:02:45 +0900

    test: 문자열 숫자 합 TC 추가

--- src/test/java/calculator/domain/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/domain/calculator/StringCalculatorTest.java
@@ -2,6 +2,11 @@ package calculator.domain.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,5 +31,23 @@ public class StringCalculatorTest {
         String[] texts = {"1", "-", "3"};
         assertThatThrownBy(() -> new StringCalculator(texts)).isInstanceOf(NonNumericValuesException.class);
     }
-}
 
+    @DisplayName("문자열 숫자의 합을 구할 수 있다.")
+    @ParameterizedTest
+    @MethodSource("numbers")
+    void sum(String[] input, int expected) {
+        Calculator calculator = new StringCalculator(input);
+
+        int actual = calculator.sum();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> numbers() {
+        return Stream.of(
+                Arguments.of(new String[]{"1", "2", "3"}, 6),
+                Arguments.of(new String[]{"1", "2", "3", "4"}, 10),
+                Arguments.of(new String[]{"1", "2", "3", "4", "5"}, 15)
+        );
+    }
+}
