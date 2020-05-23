03bd2cac482022b7b641b594a298fe7377276e61
junilHwang <tjsdlf4261@naver.com>
2020-05-23 17:33:25 +0900

    test: [step2] Lotto: 로또 중복 검증 테스트 완료

--- src/test/java/step2/domain/LottoTest.java
+++ src/test/java/step2/domain/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import step2.exception.LottoCountException;
+import step2.exception.LottoReduplicateException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,16 +18,33 @@ public class LottoTest {
 
   @DisplayName("로또 번호가 6개인지 검증하는 테스트")
   @ParameterizedTest
-  @MethodSource("provideLottoNumbers")
+  @MethodSource("provideLottoWrongNumbers")
   void 로또_번호_갯수_검증 (List<Integer> lottoNumbers) {
     assertThatExceptionOfType(LottoCountException.class)
       .isThrownBy(() -> Lotto.of(lottoNumbers));
   }
 
-  private static Stream<Arguments> provideLottoNumbers () {
+  private static Stream<Arguments> provideLottoWrongNumbers () {
     return Stream.of(
       Arguments.of(Arrays.asList("1,2,3,4,5".split(","))),
       Arguments.of(Arrays.asList("1,2,3,4,5,6,7".split(",")))
     );
   }
+
+  @DisplayName("로또 번호의 중복 여부를 검사")
+  @ParameterizedTest
+  @MethodSource("provideReduplicateLottoNumbers")
+  void 로또_번호_중복_검증 (List<Integer> lottoNumbers) {
+    assertThatExceptionOfType(LottoReduplicateException.class)
+      .isThrownBy(() -> Lotto.of(lottoNumbers));
+  }
+
+  private static Stream<Arguments> provideReduplicateLottoNumbers () {
+    return Stream.of(
+      Arguments.of(Arrays.asList("1,2,3,4,5,5".split(","))),
+      Arguments.of(Arrays.asList("1,1,2,2,3,3".split(",")))
+    );
+  }
+
+
 }
