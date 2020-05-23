082d98e4c8f26f45bf1083f5a7643b539ed4e6fc
Sungjun <sungjunpizz@gmail.com>
2020-05-23 21:52:27 +0900

    feat: 정수 Wrapping 클래스 생성

---
+++ src/main/java/calculator/domain/calculator/Number.java
@@ -0,0 +1,21 @@
+package calculator.domain.calculator;
+
+public class Number {
+
+    private final int number;
+
+    public Number(int number) {
+        validateNegative(number);
+        this.number = number;
+    }
+
+    public Number(String input) {
+        this(Integer.parseInt(input));
+    }
+
+    private void validateNegative(int number) {
+        if (number < 0) {
+            throw new NegativeInputException();
+        }
+    }
+}
