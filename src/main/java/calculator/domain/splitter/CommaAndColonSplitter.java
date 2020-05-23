48c28665bf9fc39d7d5b13a386ed9cf25166a807
Sungjun <sungjunpizz@gmail.com>
2020-05-23 18:29:42 +0900

    feat: 콤마, 콜론 Splitter 클래스 생성
    - split 메소드 내부 구현

---
+++ src/main/java/calculator/domain/splitter/CommaAndColonSplitter.java
@@ -0,0 +1,20 @@
+package calculator.domain.splitter;
+
+public class CommaAndColonSplitter implements Splitter{
+    private static final String COMMA_COLON_DELIMITER_REGEX = "[,:]";
+
+    @Override
+    public String[] split(String input) {
+        return input.split(COMMA_COLON_DELIMITER_REGEX);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
