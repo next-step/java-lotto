5734ab0b0495c5395f716d5e8d4ba9facc2c2808
junilHwang <tjsdlf4261@naver.com>
2020-05-23 18:45:41 +0900

    test: [step2] 로또 수익률 테스트 로직 수정

--- src/test/java/step2/domain/LottoGameTest.java
+++ src/test/java/step2/domain/LottoGameTest.java
@@ -25,14 +25,23 @@ public class LottoGameTest {
   @DisplayName("로또 당첨 수를 구하는 메소드 검증")
   @ParameterizedTest
   @MethodSource("provideWinningLottoCount")
-  void 로또_당첨수_테스트 (Lotto winning, Lotto lotto, long expected) {
-    assertThat(expected).isEqualTo(LottoGame.getSames(winning, lotto));
+  void 로또_당첨수_테스트 (LottoGame lottoGame, int same, long expected) {
+    assertThat(expected).isEqualTo(lottoGame.getWinningCount(same));
   }
 
   private static Stream<Arguments> provideWinningLottoCount () {
+    LottoGame lottoGame = LottoGame.of(
+                            Stream.of(
+                              Lotto.of("1,2,3,4,5,7"),
+                              Lotto.of("1,2,3,4,5,8"),
+                              Lotto.of("1,2,3,40,20,10"),
+                              Lotto.of("1,2,3,30,20,10")
+                            ).collect(Collectors.toList())
+                          );
+    lottoGame.setWinningNumber(Lotto.of("1,2,3,4,5,6"));
     return Stream.of(
-      Arguments.of(Lotto.of("1,2,3,4,5,6"), Lotto.of("1,2,3,7,8,9"), 3),
-      Arguments.of(Lotto.of("1,2,3,4,5,6"), Lotto.of("1,2,3,4,5,6"), 6)
+      Arguments.of(lottoGame, 5, 2),
+      Arguments.of(lottoGame, 3, 2)
     );
   }
 
@@ -45,34 +54,19 @@ public class LottoGameTest {
   }
 
   private static Stream<Arguments> provideLottoGameAndWinningPrice () {
+    int payoff = WinningPrice.FIRST + WinningPrice.SECOND + WinningPrice.THIRD + WinningPrice.FORTH;
+    String payoffRatio = String.format("%.2f", payoff / 4000.0);
+    LottoGame lottoGame = LottoGame.of(
+                            Stream.of(
+                              Lotto.of("1,2,3,4,5,6"),
+                              Lotto.of("1,2,3,4,5,10"),
+                              Lotto.of("1,2,3,4,20,10"),
+                              Lotto.of("1,2,3,30,20,10")
+                            ).collect(Collectors.toList())
+                          );
     return Stream.of(
-      Arguments.of(
-        LottoGame.of(
-          Stream.of(
-            Lotto.of("1,2,3,4,5,6"),
-            Lotto.of("1,2,3,4,5,10"),
-            Lotto.of("1,2,3,4,20,10"),
-            Lotto.of("1,2,3,30,20,10")
-          ).collect(Collectors.toList())
-        ),
-        Lotto.of("1,2,3,4,5,6"),
-        String.format(
-          "%.2f",
-          (WinningPrice.FIRST + WinningPrice.SECOND + WinningPrice.THIRD + WinningPrice.FORTH) / 4000.0
-        )
-      ),
-      Arguments.of(
-        LottoGame.of(
-          Stream.of(
-            Lotto.of("1,2,3,4,5,6"),
-            Lotto.of("1,2,3,4,5,10"),
-            Lotto.of("1,2,3,4,20,10"),
-            Lotto.of("1,2,3,30,20,10")
-          ).collect(Collectors.toList())
-        ),
-        Lotto.of("11,12,13,14,15,16"),
-        "0.00"
-      )
+      Arguments.of(lottoGame, Lotto.of("1,2,3,4,5,6"), payoffRatio),
+      Arguments.of(lottoGame, Lotto.of("11,12,13,14,15,16"), "0.00")
     );
   }
 }
