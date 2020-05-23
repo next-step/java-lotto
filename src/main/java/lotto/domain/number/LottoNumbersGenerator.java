cf4e26fd336ca6fe21b152f05a27162c9269dd7f
kwonheejeong <gmlwjd9405@naver.com>
2020-05-23 09:25:45 +0900

    refactor: Integer List를 LottoNumber로 변경

--- src/main/java/lotto/domain/number/LottoNumbersGenerator.java
+++ src/main/java/lotto/domain/number/LottoNumbersGenerator.java
@@ -8,34 +8,29 @@ import java.util.stream.IntStream;
 
 public class LottoNumbersGenerator {
 
-    private static final List<Integer> RANGED_LOTTO_NUMBERS =
+    private static final List<LottoNumber> RANGED_LOTTO_NUMBERS =
             IntStream.rangeClosed(LottoNumber.MIN_VALUE, LottoNumber.MAX_VALUE)
                     .boxed()
+                    .map(LottoNumber::of)
                     .collect(Collectors.toList());
 
     private LottoNumbersGenerator() {}
 
     public static List<LottoNumber> generate() {
-        List<Integer> lottoNumbers = generateRangeNumbers();
+        List<LottoNumber> lottoNumbers = generateRangeNumbers();
         Collections.shuffle(lottoNumbers);
 
-        return convertToLottoNumbers(extractAndSortNumbers(lottoNumbers));
+        return extractAndSortNumbers(lottoNumbers);
     }
 
-    private static List<Integer> generateRangeNumbers() {
+    private static List<LottoNumber> generateRangeNumbers() {
         return new ArrayList<>(RANGED_LOTTO_NUMBERS);
     }
 
-    private static List<Integer> extractAndSortNumbers(final List<Integer> lottoNumbers) {
+    private static List<LottoNumber> extractAndSortNumbers(final List<LottoNumber> lottoNumbers) {
         return lottoNumbers.stream()
                 .limit(LottoNumbers.LOTTO_NUMBER_SIZE)
                 .sorted()
-                .collect(Collectors.toList());
-    }
-
-    private static List<LottoNumber> convertToLottoNumbers(final List<Integer> lottoNumbers) {
-        return lottoNumbers.stream()
-                .map(LottoNumber::of)
                 .collect(Collectors.toList());
     }
 }
