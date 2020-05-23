cddf4beb1cafd11ecbad73858c4e6bd202b5c102
kwonheejeong <gmlwjd9405@naver.com>
2020-05-24 08:30:45 +0900

    test: 수동으로 입력한 로또 티켓이 구매할 수 있는 티켓의 수보다 많으면 예외 반환

--- src/test/java/lotto/domain/seller/LottoSellerTest.java
+++ src/test/java/lotto/domain/seller/LottoSellerTest.java
@@ -1,6 +1,7 @@
 package lotto.domain.seller;
 
 import lotto.domain.price.Price;
+import lotto.domain.ticket.LottoTicket;
 import lotto.domain.ticket.LottoTickets;
 import lotto.util.Generator;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +11,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class LottoSellerTest {
 
@@ -45,5 +47,19 @@ public class LottoSellerTest {
                 Arguments.of(1000, Generator.lottoTickets(), 1),
                 Arguments.of(14000, Generator.lottoTickets(Generator.lottoTicket(1, 2, 3, 4, 5, 6)), 14)
         );
+    }
+
+    @DisplayName("수동으로 입력한 로또 티켓이 구매할 수 있는 티켓의 수보다 많으면 예외 반환")
+    @Test
+    void buyTicketsFailure() {
+        Price price = Price.of(Price.ONE_TICKET_PRICE);
+
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.add(Generator.lottoTicket(1, 2, 3, 4, 5, 6));
+        lottoTickets.add(Generator.lottoTicket(1, 2, 3, 7, 8, 9));
+        LottoTickets manualTickets = LottoTickets.of(lottoTickets);
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> lottoSeller.buyTicket(price, manualTickets));
     }
 }
