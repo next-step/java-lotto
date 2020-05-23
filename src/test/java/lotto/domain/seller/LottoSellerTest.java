9d806fc471a3bf735ec678bb32c6fe85f1ba191c
kwonheejeong <gmlwjd9405@naver.com>
2020-05-24 08:13:43 +0900

    test: 수동 티켓도 구매함에 따른 테스트 코드 수정

--- src/test/java/lotto/domain/seller/LottoSellerTest.java
+++ src/test/java/lotto/domain/seller/LottoSellerTest.java
@@ -1,11 +1,16 @@
 package lotto.domain.seller;
 
 import lotto.domain.price.Price;
+import lotto.domain.ticket.LottoTickets;
+import lotto.util.Generator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -23,15 +28,22 @@ public class LottoSellerTest {
     @Test
     void buyTicketFailureByLackMoney() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> lottoSeller.buyTicket(Price.of(Price.ONE_TICKET_PRICE - 1)));
+                .isThrownBy(() -> lottoSeller.buyTicket(Price.of(Price.ONE_TICKET_PRICE - 1), Generator.lottoTickets()));
     }
 
     @DisplayName("로또 구입 금액에 맞는 로또 티켓을 반환")
     @ParameterizedTest
-    @ValueSource(ints = { 1000, 14000 })
-    void buyTickets(final int money) {
+    @MethodSource
+    void buyTickets(final int money, final LottoTickets manualTickets, final int expected) {
         Price price = Price.of(money);
-        assertThat(lottoSeller.buyTicket(price).getLottoTickets().size())
-                .isEqualTo(price.ticketCount());
+        assertThat(lottoSeller.buyTicket(price, manualTickets).getLottoTickets().size())
+                .isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> buyTickets() {
+        return Stream.of(
+                Arguments.of(1000, Generator.lottoTickets(), 1),
+                Arguments.of(14000, Generator.lottoTickets(Generator.lottoTicket(1, 2, 3, 4, 5, 6)), 14)
+        );
     }
 }
