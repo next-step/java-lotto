65372f330ad06d76f63adf91799bc0847e7dbe4a
jwooss <wodn4131@gmail.com>
2020-05-25 08:56:09 +0900

    refactor: Splitter의 구분자 패턴 상수로 분리

--- src/main/java/step1/Splitter.java
+++ src/main/java/step1/Splitter.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class Splitter {
 
+    private final static String SEPARATE_PATTERN = "[,:]";
+
     public static Integer[] split(String formula) {
-        return Arrays.stream(formula.split("[,:]"))
+        return Arrays.stream(formula.split(SEPARATE_PATTERN))
                 .map(Integer::parseInt)
                 .toArray(Integer[]::new);
     }
--- src/test/java/step1/SplitterTest.java
+++ src/test/java/step1/SplitterTest.java
@@ -3,7 +3,6 @@ package step1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -12,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SplitterTest {
 
-    @DisplayName("콤마(,)를 구분자로 문자열을 분할하여 숫자 배열을 반환한다")
+    @DisplayName("콤마(,)또는 콜론(:) 구분자로 문자열을 분할하여 숫자 배열을 반환한다")
     @MethodSource("provideStringsForSplitter")
     @ParameterizedTest
     void split_TwoNumberWithComma_Tokens(String formula, Integer expected1, Integer expected2) {
@@ -27,7 +26,9 @@ public class SplitterTest {
         return Stream.of(
                 Arguments.of("1,2", 1, 2),
                 Arguments.of("4,5", 4, 5),
-                Arguments.of("3,1", 3, 1)
+                Arguments.of("3,1", 3, 1),
+                Arguments.of("5:5", 5, 5),
+                Arguments.of("2:9", 2, 9)
         );
     }
 }
