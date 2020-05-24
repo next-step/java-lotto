8699df07d7ff80e0a4060c373a1fc9b39fa12f1b
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-25 00:02:57 +0900

    Feat: custom delimiter

--- src/main/java/com/cheolhyeonpark/calculator/StringAddCalculator.java
+++ src/main/java/com/cheolhyeonpark/calculator/StringAddCalculator.java
@@ -1,12 +1,20 @@
 package com.cheolhyeonpark.calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
         if (input == null || input.length() < 1) {
             return 0;
+        }
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] numbers = matcher.group(2).split(customDelimiter);
+            return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
         }
         if (input.contains(",") || input.contains(":")) {
             String[] numbers = input.split("[,|:]");
