3c676787fc56c7830f22b2f107085e06edc87870
chulman <chlcjfals0122@gmail.com>
2020-05-24 15:00:26 +0900

    feat (LottoGameTest) : 로또 게임 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,11 +1,21 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class LottoGame {
 
+    private Map<Integer, Lotto> result = new HashMap<>();
 
     public Lotto add(int round, int[] winningNumber) {
 
-        return null;
+        List<Integer> convertedList = Arrays.stream(winningNumber).boxed()
+                .collect(Collectors.toList());
+
+        return result.put(round, new Lotto(convertedList));
     }
 }
