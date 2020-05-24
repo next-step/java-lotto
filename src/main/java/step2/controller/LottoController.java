604809f55c29e2d8706a8166b895756336b3e481
junilHwang <tjsdlf4261@naver.com>
2020-05-24 23:26:28 +0900

    refactor: [step2] LottoController 역할 최소화

--- src/main/java/step2/controller/LottoController.java
+++ src/main/java/step2/controller/LottoController.java
@@ -2,57 +2,26 @@ package step2.controller;
 
 import step2.domain.Lotto;
 import step2.domain.LottoGame;
+import step2.domain.LottoShop;
 import step2.view.InputView;
 import step2.view.ResultView;
 
 public class LottoController {
-  private final LottoGame lottoGame;
   private final static InputView inputView = InputView.getInstance();
   private final static ResultView resultView = ResultView.getInstance();
 
-  private LottoController (LottoGame lottoGame) {
-    this.lottoGame = lottoGame;
-  }
-
-  private LottoController viewLottoGame () {
-    resultView.printLottoGame(lottoGame);
-    return this;
-  }
-
-  public LottoController inputWinning (Lotto winning) {
-    lottoGame.setWinningNumber(winning);
-    return this;
-  }
-
-  public LottoController inputWinning () {
-    Lotto winning = Lotto.of(inputView.inputWinning());
-    return this.inputWinning(winning);
-  }
-
-  public LottoController viewStat () {
-    resultView.printStat(lottoGame);
-    return this;
-  }
-
-  public void viewPayoff () {
-    resultView.printPayoffRatio(lottoGame.getPayoff());
-  }
-
-  public static LottoController of (LottoGame lottoGame) {
-    return new LottoController(lottoGame);
-  }
-
-  public static LottoController of () {
-    long price = inputView.inputPrice();
-    return of(LottoGame.of(price));
-  }
-
   public static void main(String[] args) {
-    LottoController.of()
-                   .viewLottoGame()
-                   .inputWinning()
-                   .viewStat()
-                   .viewPayoff();
+
+    long price = inputView.inputPrice();
+    LottoShop lottoShop = LottoShop.of(price);
+    resultView.printLottoList(lottoShop);
+
+    Lotto winning = Lotto.of(inputView.inputWinning());
+    LottoGame lottoGame = LottoGame.of(lottoShop, winning);
+
+    resultView.printStat(lottoGame);
+    resultView.printPayoffRatio(lottoGame.getPayoffRatio());
+
   }
 
 }
