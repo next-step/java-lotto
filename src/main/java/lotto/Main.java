bb1715074e504d720e394c8e38b7a3ca8e8e0b11
chulman <chlcjfals0122@gmail.com>
2020-05-24 19:22:45 +0900

    feat (Main) : add main

---
+++ src/main/java/lotto/Main.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
+
+public class Main {
+
+    private static LottoShop lottoShop = new LottoShop();
+    private static LottoGame lottoGame = new LottoGame();
+
+    public static void main(String[] args) {
+
+        int amount = InputView.inputLottoAmount();
+
+        for (int i = 0; i < amount; i++) {
+            lottoShop.buy();
+        }
+        List<LottoTicket> lottoTickets = lottoShop.getLottoTickets();
+        ResultView.printBuyingTickets(lottoTickets);
+
+        int round = 1;
+        int[] winningNumbers = InputView.inputWinningNumbers();
+        lottoGame.add(round, winningNumbers);
+
+        ResultView.printLottoRanks(lottoGame.lottoRanks(round, lottoTickets));
+        ResultView.printRevenueRate(lottoGame.revenueRate(round, lottoTickets));
+
+    }
+}
