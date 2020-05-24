fdb6ae651390bae567ff85c85c4de7855bf11695
jwooss <wodn4131@gmail.com>
2020-05-25 08:32:15 +0900

    feat: 구분자로 문자열을 분할하는 Splitter 구현

---
+++ src/main/java/step1/Splitter.java
@@ -0,0 +1,12 @@
+package step1;
+
+import java.util.Arrays;
+
+public class Splitter {
+
+    public static Integer[] split(String formula) {
+        return Arrays.stream(formula.split(","))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+    }
+}
---
+++ src/test/java/step1/SplitterTest.java
@@ -0,0 +1,33 @@
+package step1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SplitterTest {
+
+    @DisplayName("콤마(,)를 구분자로 문자열을 분할하여 숫자 배열을 반환한다")
+    @MethodSource("provideStringsForSplitter")
+    @ParameterizedTest
+    void split_TwoNumberWithComma_Tokens(String formula, Integer expected1, Integer expected2) {
+        Integer[] tokens = Splitter.split(formula);
+
+        assertThat(tokens).hasSize(2);
+        assertThat(tokens[0]).isEqualTo(expected1);
+        assertThat(tokens[1]).isEqualTo(expected2);
+    }
+
+    private static Stream<Arguments> provideStringsForSplitter() {
+        return Stream.of(
+                Arguments.of("1,2", 1, 2),
+                Arguments.of("4,5", 4, 5),
+                Arguments.of("3,1", 3, 1)
+        );
+    }
+}
