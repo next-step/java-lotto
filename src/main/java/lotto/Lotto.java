ab700017bdb1c4d7792967d8a0aea4caf0c3f01a
chulman <chlcjfals0122@gmail.com>
2020-05-24 12:53:48 +0900

    feat (lotto) : 로또 클래스 기본 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,4 +1,17 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Lotto {
+
+    private List<Integer> lottoNumbers = Collections.EMPTY_LIST;
+
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public List<Integer> getLottoNumbers(){
+        return lottoNumbers;
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,8 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 
@@ -11,7 +13,6 @@ public class LottoTest {
     @Test
     @DisplayName("로또 생성 테스트")
     void 로또_생성_테스트(){
-        assertThatCode(() -> new Lotto()).doesNotThrowAnyException();
+        assertThatCode(() -> new Lotto(new ArrayList<>())).doesNotThrowAnyException();
     }
-
 }
