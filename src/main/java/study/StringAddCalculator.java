1b64c83f6f0f169f7cd1e7cc59dc9bfdbe8c0a62
Zeeyon <zeeyon.c@gmail.com>
2020-05-24 13:22:47 +0900

    refactor: StringAddCalculator static 메서드를 인스턴스 메서드로 변경

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class StringAddCalculator {
 
-    private StringAddCalculator() {}
+    public StringAddCalculator() {}
 
-    public static int splitAndSum(String text) {
+    public int splitAndSum(String text) {
         if (StringUtil.isNullOrEmpty(text)) {
             return 0;
         }
@@ -20,17 +20,17 @@ public class StringAddCalculator {
         return sum(numbers);
     }
 
-    private static List<Integer> getNumbers(String text) {
+    private List<Integer> getNumbers(String text) {
         String[] tokens = StringSplitter.split(text);
         return IntegerUtil.parseArray(tokens);
     }
 
-    private static Integer sum(List<Integer> numbers) {
+    private Integer sum(List<Integer> numbers) {
         return numbers.stream()
                 .reduce(0, Integer::sum);
     }
 
-    private static void checkNegative(List<Integer> numbers) {
+    private void checkNegative(List<Integer> numbers) {
         numbers.stream()
                 .filter(n -> n < 0)
                 .findAny()
--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -10,48 +10,50 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
+    private StringAddCalculator calculator = new StringAddCalculator();
+
     @ParameterizedTest
     @NullAndEmptySource
     public void splitAndSum_null_또는_빈문자(String text) {
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = calculator.splitAndSum(text);
         assertThat(result).isEqualTo(0);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "2", "3"})
     public void splitAndSum_숫자하나(String text) {
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = calculator.splitAndSum(text);
         int expected = Integer.parseInt(text);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = calculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = calculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = calculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> calculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     public void splitAndSum_notNumber() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1, 2, q"))
+        assertThatThrownBy(() -> calculator.splitAndSum("1, 2, q"))
                 .isInstanceOf(NumberFormatException.class);
     }
 }
