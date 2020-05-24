e5c0f344b8f54c59324daca6a638fdc77657da44
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-24 14:22:44 +0900

    refactor (InputView) : 안내 메시지 추가

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class InputView {
 
     private final Scanner scanner;
+    private static final String INPUT_MESSAGE = "계산할 문자열을 입력하세요.";
 
     public InputView() {
         this.scanner = new Scanner(System.in);
     }
 
     public String getUserInput() {
+        System.out.println(INPUT_MESSAGE);
         return scanner.next();
     }
 }
