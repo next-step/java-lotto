fddd381c1cbadf2ed967a5c8e5e405f47ecbb60b
junilHwang <tjsdlf4261@naver.com>
2020-05-24 22:10:35 +0900

    refactor: [step2] ResultView

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -2,45 +2,46 @@ package step2.view;
 
 import step2.domain.Lotto;
 import step2.domain.LottoGame;
-import step2.domain.WinningPrice;
+import step2.domain.Rank;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ResultView {
+
+  private static final String HEAD_FORMAT = "\n당첨 통계\n---------\n%s\n";
+  private static final String BODY_FORMAT = "%d개 일치 (%d원)- %d개";
+  private static final String BUYING_FORMAT = "%d개를 구매했습니다.\n%s\n\n";
+  private static final String PAY_OFF_FORMAT = "총 수익률은 %.2f입니다. 결과적으로 %s입니다\n";
 
   private static ResultView instance;
 
   private ResultView () { }
 
   public void printLottoGame (LottoGame lottoGame) {
-    System.out.printf(
-      "%d개를 구매했습니다.\n%s\n\n",
-      lottoGame.stream().count(),
-      lottoGame.stream()
-               .map(ResultView::lottoToString)
-               .collect(Collectors.joining("\n"))
-    );
+    final long lottoCount = lottoGame.stream().count();
+    final String lottoList = lottoGame.stream()
+                                      .map(ResultView::lottoToString)
+                                      .collect(Collectors.joining("\n"));
+
+    System.out.printf(BUYING_FORMAT, lottoCount, lottoList);
   }
 
   public void printStat (LottoGame lottoGame) {
-    String headFormat = "\n당첨 통계\n---------\n%s\n";
-    String bodyFormat = "%d개 일치 (%d원)- %d개";
-    System.out.printf(
-      headFormat,
-      WinningPrice.stream()
-                  .map(winningPrice -> String.format(
-                      bodyFormat,
-                      winningPrice.getSame(),
-                      winningPrice.getPrice(),
-                      lottoGame.getWinningCount(winningPrice.getSame())))
-                  .collect(Collectors.joining("\n"))
+    System.out.printf(HEAD_FORMAT,
+      Rank.stream()
+          .map(rank -> String.format(BODY_FORMAT,
+                                     rank.getSame(),
+                                     rank.getPrice(),
+                                     lottoGame.getWinningCount(rank)))
+          .collect(Collectors.joining("\n"))
     );
   }
 
+
+
   public void printPayoffRatio (double payoffRatio) {
     System.out.printf(
-      "총 수익률은 %.2f입니다. 결과적으로 %s입니다\n",
+      PAY_OFF_FORMAT,
       payoffRatio,
       getPayoffResult(payoffRatio)
     );
@@ -57,7 +58,9 @@ public class ResultView {
   }
 
   public static String lottoToString (Lotto lotto) {
-    String str = lotto.stream().map(String::valueOf).collect(Collectors.joining(", "));
+    String str = lotto.stream()
+                      .map(String::valueOf)
+                      .collect(Collectors.joining(", "));
     return "[" + str + "]";
   }
 
