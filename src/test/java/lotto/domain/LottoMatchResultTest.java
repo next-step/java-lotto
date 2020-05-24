e211f35adb95e64e92b9d0149824f241af758563
JuWanHan <baekseongsa@naver.com>
2020-05-25 00:06:35 +0900

    test(LottoMatchResultTest): 테스트 코드 개선

--- src/test/java/lotto/domain/LottoMatchResultTest.java
+++ src/test/java/lotto/domain/LottoMatchResultTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,8 +20,15 @@ class LottoMatchResultTest {
 
     @BeforeEach
     void setUp() {
-        LottoGame lottoGame = new LottoGame(14000);
-        lottoGame.createLottoNumbers();
+        LottoGame lottoGame = new LottoGame();
+        List<String> manual = Arrays.asList(
+                "1, 2, 3, 13, 14, 15",
+                "1, 2, 3, 4, 14, 15",
+                "1, 2, 3, 4, 5, 15",
+                "1, 2, 3, 4, 5, 7",
+                "1, 2, 3, 4, 5, 6"
+        );
+        lottoGame.createManualLottoNumbers(manual);
 
         LottoNumbers winLottoNumbers = LottoNumbers.newInstance(Arrays.asList(1, 2, 3, 4, 5, 6));
         this.lottoMatchResult = lottoGame.calculateMatchCount(winLottoNumbers, 7);
@@ -42,6 +50,7 @@ class LottoMatchResultTest {
                 Arguments.of(LottoMatch.THREE),
                 Arguments.of(LottoMatch.FOUR),
                 Arguments.of(LottoMatch.FIVE),
+                Arguments.of(LottoMatch.FIVE_BONUS),
                 Arguments.of(LottoMatch.SIX)
         );
     }
@@ -55,21 +64,41 @@ class LottoMatchResultTest {
 
     @DisplayName("당첨 개수를 0 이상 값을 얻을 수 있다.")
     @ParameterizedTest
-    @MethodSource("generateLottoMatch")
-    void canGetMatchCount(LottoMatch lottoMatch) {
-        assertThat(this.lottoMatchResult.getMatchCount(lottoMatch)).isGreaterThanOrEqualTo(0);
+    @MethodSource("generateMatchCountForLottoMatch")
+    void canGetMatchCount(LottoMatch lottoMatch, int result) {
+        assertThat(this.lottoMatchResult.getMatchCount(lottoMatch)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> generateMatchCountForLottoMatch() {
+        return Stream.of(
+                Arguments.of(LottoMatch.THREE, 1),
+                Arguments.of(LottoMatch.FOUR, 1),
+                Arguments.of(LottoMatch.FIVE, 1),
+                Arguments.of(LottoMatch.FIVE_BONUS, 1),
+                Arguments.of(LottoMatch.SIX, 1)
+        );
     }
 
     @DisplayName("당첨금을 계산할 수 있다.")
     @ParameterizedTest
-    @MethodSource("generateLottoMatch")
-    void canCalculateWinAmount(LottoMatch lottoMatch) {
-        assertThat(this.lottoMatchResult.calculateWinAmount(lottoMatch)).isGreaterThanOrEqualTo(0);
+    @MethodSource("generateWinAmountForLottoMatch")
+    void canCalculateWinAmount(LottoMatch lottoMatch, long result) {
+        assertThat(this.lottoMatchResult.calculateWinAmount(lottoMatch)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> generateWinAmountForLottoMatch() {
+        return Stream.of(
+                Arguments.of(LottoMatch.THREE, 5_000),
+                Arguments.of(LottoMatch.FOUR, 50_000),
+                Arguments.of(LottoMatch.FIVE, 1_500_000),
+                Arguments.of(LottoMatch.FIVE_BONUS, 30_000_000),
+                Arguments.of(LottoMatch.SIX, 2_000_000_000)
+        );
     }
 
     @DisplayName("총 수익률을 계산할 수 있다.")
     @Test
     void canCalculateProfitRate() {
-        assertThat(this.lottoMatchResult.calculateProfitRate(14)).isGreaterThanOrEqualTo(0);
+        assertThat(this.lottoMatchResult.calculateProfitRate(5)).isEqualTo(406_311_000);
     }
 }
