58010a30ab7b8a3ab6425aea6629d9edaee2e15d
chulman <chlcjfals0122@gmail.com>
2020-05-24 15:31:16 +0900

    test (LottoShopTest) : lottoShopTest 추가

---
+++ src/test/java/lotto/LottoShopTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoShopTest {
+
+    @Test
+    @DisplayName("로또를 구입하고 크기를 검증한다.")
+    void 로또_구입_테스트(){
+        LottoShop lottoShop = new LottoShop();
+        lottoShop.buy();
+
+        assertThat(lottoShop.getLottoTickets()).hasSize(1);
+    }
+}
