598f221251ffab38805c50c273850904b0feca0e
JuWanHan <baekseongsa@naver.com>
2020-05-25 00:04:28 +0900

    feat(LottoNumbersFactory): 수동 로또 번호 생성 구현

--- src/main/java/lotto/domain/LottoNumbersFactory.java
+++ src/main/java/lotto/domain/LottoNumbersFactory.java
@@ -7,8 +7,13 @@ public final class LottoNumbersFactory {
     private LottoNumbersFactory() {
     }
 
-    public static LottoNumbers createLottoNumbers() {
+    public static LottoNumbers createAutoLottoNumbers() {
         List<Integer> nonDuplicateNumbers = LottoNumbers.createNonDuplicateNumbers();
+        return LottoNumbers.newInstance(nonDuplicateNumbers);
+    }
+
+    public static LottoNumbers createManualLottoNumbers(String manualNumbers) {
+        List<Integer> nonDuplicateNumbers = LottoNumbers.createNonDuplicateNumbers(manualNumbers);
         return LottoNumbers.newInstance(nonDuplicateNumbers);
     }
 }
