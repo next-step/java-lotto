6ebf950fbb5e9c92cfeef907579ff513c7517adc
bbbnam <bbbnam@naver.com>
2020-05-24 23:56:40 +0900

    refactor : Expression클래스 유틸리티화 및 리팩토링

--- src/main/java/study/step1/Expression.java
+++ src/main/java/study/step1/Expression.java
@@ -6,18 +6,12 @@ import java.util.regex.Pattern;
 
 public class Expression {
     private static final String DEFALUT_EXPRESSION = "0";
-    private static final int INDEX_ONE = 1;
-    private static final int INDEX_TWO = 2;
+    private static final int DELIMITER_LOCATION_NUMBER = 1;
+    private static final int NUMBERPARTS_LOCATION_NUMBER = 2;
     private static final String DELIMITER = ",|:";
     private static final Pattern PATTERN = Pattern.compile("//(.)\\n(.*)");
 
-    private final String expression;
-
-    public Expression(String expression) {
-        this.expression = validateExpression(expression);
-    }
-
-    private String validateExpression(String expression) {
+    public static String validateExpression(String expression) {
         if (isStringNullCheck(expression)){
             return DEFALUT_EXPRESSION;
         }
@@ -25,17 +19,14 @@ public class Expression {
     }
 
     private static boolean isStringNullCheck(String input) {
-        if (Objects.isNull(input) || input.isEmpty()){
-            return true;
-        }
-        return false;
+        return Objects.isNull(input) || input.isEmpty();
     }
 
-    public String[] split() {
+    public static String[] split(String expression) {
         Matcher m = PATTERN.matcher(expression);
         if (m.find()){
-            String customDelimiter = m.group(INDEX_ONE);
-            return m.group(INDEX_TWO).split(customDelimiter);
+            String customDelimiter = m.group(DELIMITER_LOCATION_NUMBER);
+            return m.group(NUMBERPARTS_LOCATION_NUMBER).split(customDelimiter);
         }
         return expression.split(DELIMITER);
     }
