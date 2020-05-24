6f29831bc58973bbc8ccee1cbe94581548135f1a
chulman <chlcjfals0122@gmail.com>
2020-05-24 13:55:45 +0900

    refactor (lottoGenerator) : 로또 생성기 자동 생성 리팩토링

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,25 +1,36 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class LottoGenerator {
 
-    private final Random RANDOM  = new Random();
+    private final Random RANDOM = new Random();
 
     private static final int LOTTO_NUMBER_COUNT = 6;
     private static final int LOTTO_NUMBER_MAX = 45;
 
 
     public Lotto autoGenerate() {
+        return new Lotto(generateLottoNumbers());
+    }
+
+    private List<Integer> generateLottoNumbers() {
         List<Integer> lottoNumbers = new ArrayList<>();
 
-        for (int i = 0; i < LOTTO_NUMBER_COUNT; i++) {
-            lottoNumbers.add(RANDOM.nextInt(LOTTO_NUMBER_MAX) + 1);
+        while (lottoNumbers.size() < LOTTO_NUMBER_COUNT) {
+            add(lottoNumbers, generateNumber());
         }
 
-        return new Lotto(lottoNumbers);
+        return lottoNumbers;
+    }
+
+    private int generateNumber() {
+        return RANDOM.nextInt(LOTTO_NUMBER_MAX) + 1;
+    }
+
+    private void add(List<Integer> lottoNumbers, int number) {
+        lottoNumbers.add(number);
     }
 }
