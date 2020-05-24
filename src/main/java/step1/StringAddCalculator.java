24d4960102645934a271c947ebeea491acffbef5
jwooss <wodn4131@gmail.com>
2020-05-25 08:34:41 +0900

    refactor: 문자열 분할을 위한 Splitter 적용

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -5,12 +5,9 @@ import java.util.Arrays;
 public class StringAddCalculator {
 
     public static int splitAndSum(String formula) {
-        String filteredFormula = StringUtils.blankToZero(formula);
-
-        String[] tokens = filteredFormula.split(",");
+        Integer[] tokens = Splitter.split(StringUtils.blankToZero(formula));
 
         return Arrays.stream(tokens)
-                .map(Integer::parseInt)
                 .reduce(0, Integer::sum);
     }
 }
