6c97c4c9a7a0c4634a83a4126b187e66f34c9d00
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-24 16:17:43 +0900

    feat (StringParser) : String[]을 List<Integer> 변환하면서 예외처리하는 기능 추가

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -4,23 +4,45 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringParser {
 
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_REGULAR_EXPRESSION = "//(.)\n(.*)";
+    private static final String ERROR_INVALID_ARGUMENT = "ERROR : 0 이상의 숫자만 계산할 수 있습니다.";
     private static final int FIRST = 1;
     private static final int SECOND = 2;
+    private static final int MINIMUM_LIMIT = 0;
 
     private StringParser() {
     }
 
-    public static List<String> parseString(String userInput) {
+    public static List<Integer> parseString(String userInput) {
+        String[] stringTokens = splitString(userInput);
+        return Arrays.stream(stringTokens)
+                .map(StringParser::parseIntAndValidate)
+                .collect(Collectors.toList());
+    }
+
+    private static String[] splitString(String userInput) {
         Matcher matcher = Pattern.compile(CUSTOM_REGULAR_EXPRESSION).matcher(userInput);
         if (matcher.find()) {
             String customDelimiter = matcher.group(FIRST);
-            return Arrays.asList(matcher.group(SECOND).split(customDelimiter));
+            return matcher.group(SECOND).split(customDelimiter);
         }
-        return Arrays.asList(userInput.split(DEFAULT_DELIMITER));
+        return userInput.split(DEFAULT_DELIMITER);
+    }
+    
+    private static Integer parseIntAndValidate(String string) {
+        Integer number;
+        try {
+            number = Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(ERROR_INVALID_ARGUMENT);
+        }
+        if (number < MINIMUM_LIMIT)
+            throw new RuntimeException(ERROR_INVALID_ARGUMENT);
+        return number;
     }
 }
