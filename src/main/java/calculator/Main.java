b5151faf2824d80bb78033dfefb1db1fef1f8049
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-24 16:52:09 +0900

    feat (StringAddCalculator) : 합계 기능 구현

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         String userInput = InputView.getUserInput();
+
+        Numbers numbers = new Numbers(StringParser.parseString(userInput));
+        Integer calculationResult = StringAddCalculator.getNumbersSum(numbers);
     }
 }
