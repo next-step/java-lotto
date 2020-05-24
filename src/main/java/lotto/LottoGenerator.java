2486995333c24d90584c42c36651bfc4d70a0dea
chulman <chlcjfals0122@gmail.com>
2020-05-24 21:55:37 +0900

    refactor (LottoGenerator) : 로또 기본 번호 초기에 생성하도록 변경

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -8,20 +8,23 @@ import java.util.stream.IntStream;
 public class LottoGenerator {
 
     private static final int LOTTO_GENERATE_COUNT = 6;
-
     private static final int LOTTO_NUMBER_MIN = 1;
     private static final int LOTTO_NUMBER_MAX = 45;
 
+    private static final List<Integer> DEFAULT_NUMBERS;
+
+    static {
+        DEFAULT_NUMBERS = generateLottoNumbers();
+    }
 
     public LottoTicket autoGenerate() {
-        List<Integer> generateNumbers = generateLottoNumbers();
-        Collections.shuffle(generateNumbers);
-        LottoTicket lottoTicket = LottoTicket.of(generateNumbers.subList(0, LOTTO_GENERATE_COUNT));
+        Collections.shuffle(DEFAULT_NUMBERS);
+        LottoTicket lottoTicket = LottoTicket.of(DEFAULT_NUMBERS.subList(0, LOTTO_GENERATE_COUNT));
         lottoTicket.sort();
         return lottoTicket;
     }
 
-    private List<Integer> generateLottoNumbers() {
+    private static List<Integer> generateLottoNumbers() {
         return IntStream.range(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX + 1)
                 .boxed()
                 .collect(Collectors.toList());
