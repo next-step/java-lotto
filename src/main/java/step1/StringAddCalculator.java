364acfae0afd77a6e3a2302b8fa2fdc4ae1a1661
jwooss <wodn4131@gmail.com>
2020-05-25 00:14:34 +0900

    refactor: Null 또는 빈 문자열을 0으로 반환하는 로직을 StringUtils로 변경

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -3,10 +3,6 @@ package step1;
 public class StringAddCalculator {
 
     public static int splitAndSum(String formula) {
-        if (formula == null || formula.isEmpty()) {
-            return 0;
-        }
-
-        return 0;
+        return Integer.parseInt(StringUtils.blankToZero(formula));
     }
 }
