3dc7202eaecbeabb0dfbe30aecdde4a1a647252b
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-24 23:21:16 +0900

    Feat: Fix code for new test(comma splitter)

--- src/main/java/com/cheolhyeonpark/calculator/StringAddCalculator.java
+++ src/main/java/com/cheolhyeonpark/calculator/StringAddCalculator.java
@@ -1,10 +1,16 @@
 package com.cheolhyeonpark.calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
         if (input == null || input.length() < 1) {
             return 0;
+        }
+        if (input.contains(",")) {
+            String[] numbers = input.split(",");
+            return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
         }
         return Integer.parseInt(input);
     }
