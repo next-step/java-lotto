5a8cfefbfcaa31d9334559934f285e97d30ed0e1
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-24 15:08:15 +0900

    test (NumbersTest) : Numbers 생성 테스트 추가

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -9,12 +9,14 @@ public class InputParser {
 
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_REGULAR_EXPRESSION = "//(.)\n(.*)";
+    private static final int FIRST = 1;
+    private static final int SECOND = 2;
 
     public List<String> parseInput(String userInput) {
         Matcher matcher = Pattern.compile(CUSTOM_REGULAR_EXPRESSION).matcher(userInput);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            return Arrays.asList(matcher.group(2).split(customDelimiter));
+            String customDelimiter = matcher.group(FIRST);
+            return Arrays.asList(matcher.group(SECOND).split(customDelimiter));
         }
         return Arrays.asList(userInput.split(DEFAULT_DELIMITER));
     }
