9ae4f5ed632c4ffa2f11d3f5d58b7716ba90a348
kwonheejeong <gmlwjd9405@naver.com>
2020-05-26 01:29:21 +0900

    refactor: 각 등수가 당첨여부를 스스로 판단하도록 리팩토링

--- src/main/java/lotto/domain/prize/Prize.java
+++ src/main/java/lotto/domain/prize/Prize.java
@@ -2,54 +2,48 @@ package lotto.domain.prize;
 
 import lotto.exception.ErrorMessage;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Prize {
 
-    MISS(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000);
+    MISS(0, 0, (matchedNumbersCount, matchBonus) -> matchedNumbersCount < 3),
+    FIFTH(3, 5_000, (matchedNumbersCount, matchBonus) -> matchedNumbersCount == 3),
+    FOURTH(4, 50_000, (matchedNumbersCount, matchBonus) -> matchedNumbersCount == 4),
+    THIRD(5, 1_500_000, (matchedNumbersCount, matchBonus) -> matchedNumbersCount == 5 && !matchBonus),
+    SECOND(5, 30_000_000, (matchedNumbersCount, matchBonus) -> matchedNumbersCount == 5 && matchBonus),
+    FIRST(6, 2_000_000_000, (matchedNumbersCount, matchBonus) -> matchedNumbersCount == 6);
 
     private final int matchedNumbersCount;
     private final int prizeMoney;
+    private final BiFunction<Integer, Boolean, Boolean> function;
 
     static final int MIN_MATCHED_COUNT = 0;
     static final int MAX_MATCHED_COUNT = 6;
-    private static final Map<Integer, Prize> PRIZES = Collections.unmodifiableMap(
-                Stream.of(Prize.values())
-                        .collect(Collectors.toMap(Prize::getMatchedNumbersCount, Function.identity(), (existing, replacement) -> existing))
-            );
 
-    Prize(final int matchedNumbersCount, final int prizeMoney) {
+    Prize(final int matchedNumbersCount, final int prizeMoney, final BiFunction<Integer, Boolean, Boolean> function) {
         this.matchedNumbersCount = matchedNumbersCount;
         this.prizeMoney = prizeMoney;
+        this.function = function;
     }
 
     public static Prize of(final int matchedNumbersCount, final boolean matchBonus) {
         validateMatchedNumbersCount(matchedNumbersCount);
 
-        Prize prize = PRIZES.getOrDefault(matchedNumbersCount, Prize.MISS);
-        if (prize.isSecondPrize(matchBonus)) {
-            return Prize.SECOND;
-        }
-        return prize;
-    }
-
-    private boolean isSecondPrize(final boolean matchBonus) {
-        return this == Prize.THIRD && matchBonus;
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.operate(matchedNumbersCount, matchBonus))
+                .findFirst()
+                .orElse(MISS);
     }
 
     private static void validateMatchedNumbersCount(final int matchedNumbersCount) {
         if (matchedNumbersCount < MIN_MATCHED_COUNT || matchedNumbersCount > MAX_MATCHED_COUNT) {
             throw new IllegalArgumentException(ErrorMessage.RANGE_OF_PRIZE_COUNT);
         }
+    }
+
+    public boolean operate(final int matchedNumbersCount, final boolean matchBonus) {
+        return function.apply(matchedNumbersCount, matchBonus);
     }
 
     public int getMatchedNumbersCount() {
