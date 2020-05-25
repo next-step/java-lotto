5a901cfc1f3be60035a0e8f74b827e3921c90c2f
yjs <yjs2952@naver.com>
2020-05-25 23:10:44 +0900

    refactor: StringAddCalculator 숫자 1개 입력 시 해당 숫자 리턴하는 로직 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,14 @@
 package calculator;
 
 
-public class Calculator {
+public class StringAddCalculator {
 
     public int plus(String input) {
         if (input == null || input.replaceAll(" ", "").isEmpty()) {
             return 0;
         }
-        return 0;
+
+        int number = Integer.parseInt(input);
+        return number;
     }
 }
