746c4a7f8cb3835867bf367418f823cc8106d6c8
Daeho Han <supreme2705@gmail.com>
2020-05-25 12:28:31 +0900

    feat: 쉼표 및 콜론 구분자를 통한 계산

--- src/main/java/me/daeho/calculator/StringAddCalculator.java
+++ src/main/java/me/daeho/calculator/StringAddCalculator.java
@@ -2,8 +2,11 @@ package me.daeho.calculator;
 
 import me.daeho.utils.StringUtils;
 
+import java.util.Arrays;
+
 
 public class StringAddCalculator {
+    private static final String DEFAULT_REGEX = ",|:";
 
     public static int splitAndSum(String input) {
         if (StringUtils.isEmpty(input))
@@ -12,6 +15,20 @@ public class StringAddCalculator {
         if (StringUtils.isOnlyNumber(input))
             return Integer.parseInt(input);
 
-        return 0;
+        return calculate(input);
+    }
+
+    private static String[] parsingDefault(String data) {
+        return data.split(DEFAULT_REGEX);
+    }
+
+    private static int calculate(String data) {
+        return sum(parsingDefault(data));
+    }
+
+    private static int sum(String[] parsedData) {
+        return Arrays.stream(parsedData)
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
 }
