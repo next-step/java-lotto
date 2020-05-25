812826e48da5405b4d3c8c1ad547b64eeba07279
yjs <yjs2952@naver.com>
2020-05-25 23:34:03 +0900

    refactor: StringAddCalculator 숫자 2개 사이에 , 구분자로 입력 시 두 숫자의 합 리턴하는 로직 추가 및 리팩토링

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,11 +4,24 @@ package calculator;
 public class StringAddCalculator {
 
     public int plus(String input) {
-        if (input == null || input.replaceAll(" ", "").isEmpty()) {
+        if (isNullOrEmpty(input)) {
             return 0;
         }
 
-        int number = Integer.parseInt(input);
-        return number;
+        String[] numbers = input.split(",");
+        return getSum(numbers);
+    }
+
+    private boolean isNullOrEmpty(String input) {
+        return input == null || input.replaceAll(" ", "").isEmpty();
+    }
+
+    private int getSum(String[] numbers) {
+        int sum = 0;
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
     }
 }
