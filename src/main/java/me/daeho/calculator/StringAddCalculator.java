de986966323fa5f682eec05c2ac3b9036ae90d41
Daeho Han <supreme2705@gmail.com>
2020-05-25 12:25:03 +0900

    feat: null 또는 빈문자 입력시 0 반환

---
+++ src/main/java/me/daeho/calculator/StringAddCalculator.java
@@ -0,0 +1,14 @@
+package me.daeho.calculator;
+
+import me.daeho.utils.StringUtils;
+
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String input) {
+        if (StringUtils.isEmpty(input))
+            return 0;
+
+        return 0;
+    }
+}
