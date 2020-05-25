7dcc23ce6eab96252168e54829c465e225b74144
yjs <yjs2952@naver.com>
2020-05-25 23:00:52 +0900

    feat: Calculator 문자열 더하기 메소드 0이 리턴 되도록 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+
+public class Calculator {
+
+    public int plus(String input) {
+        if (input == null || input.replaceAll(" ", "").isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
