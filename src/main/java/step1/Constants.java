51715e2d294dff7b86a4ae19dda21dbcac509859
wangyongchoi <choiwangyong@gmail.com>
2020-05-26 00:22:13 +0900

    feat: “//”와 “\n” 문자 사이에 커스텀 구분자 기능

--- src/main/java/step1/Constants.java
+++ src/main/java/step1/Constants.java
@@ -2,6 +2,6 @@ package step1;
 
 public class Constants {
 
-    public static final String COMMA = ",";
-    public static final String COLON = ":";
+    public static final String COMMA_OR_COLON_REX = ",|:";
+    public static final String SPECIAL_DELIMITER_REX = "//(.)\n(.*)";
 }
--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -1,7 +1,11 @@
 package step1;
 
-import static step1.Constants.COMMA;
-import static step1.Constants.COLON;
+import static step1.Constants.COMMA_OR_COLON_REX;
+import static step1.Constants.SPECIAL_DELIMITER_REX;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
@@ -28,9 +32,22 @@ public class StringAddCalculator {
     }
 
     private static String[] parseNumberString(String numberString) {
-        String tokenPattern = COMMA + "|" + COLON;
-        String[] numbers = numberString.split(tokenPattern);
-        return numbers;
+
+        Matcher specialDelimiterMatcher = Pattern.compile(SPECIAL_DELIMITER_REX)
+            .matcher(numberString);
+
+        return specialDelimiterMatcher.find() ?
+            parseNumbersWithSpecialDelimiter(specialDelimiterMatcher) :
+            parseNumbersWithNormalDelimiter(numberString);
+    }
+
+    private static String[] parseNumbersWithSpecialDelimiter(Matcher specialDelimiterMatcher) {
+        String customDelimiter = specialDelimiterMatcher.group(1);
+        return specialDelimiterMatcher.group(2).split(customDelimiter);
+    }
+
+    private static String[] parseNumbersWithNormalDelimiter(String numberString) {
+        return numberString.split(COMMA_OR_COLON_REX);
     }
 
     private static int addParsedNumberString(String[] numbers) {
