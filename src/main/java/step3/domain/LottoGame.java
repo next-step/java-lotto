3bf665d02698ebd931f05e625f49c7a69c9861f5
junilHwang <tjsdlf4261@naver.com>
2020-05-26 22:18:43 +0900

    refactor: [Step3] WinningLotto class 추가 및 위임에 따른 Lotto Game 리팩토링

--- src/main/java/step3/domain/LottoGame.java
+++ src/main/java/step3/domain/LottoGame.java
@@ -2,23 +2,22 @@ package step3.domain;
 
 public class LottoGame {
 
-  private final Lotto winningLotto;
+  private final WinningLotto winningLotto;
   private final Lottos lottos;
-  private final LottoNumber bonusNumber;
 
-  public LottoGame(Lottos lottos, Lotto winningLotto, LottoNumber bonusNumber) {
+  public LottoGame(Lottos lottos, WinningLotto winningLotto) {
     this.lottos = lottos;
     this.winningLotto = winningLotto;
-    this.bonusNumber = bonusNumber;
   }
 
-  public static LottoGame of (Lottos lottos, Lotto winningLotto, LottoNumber bonusNumber) {
-    return new LottoGame(lottos, winningLotto, bonusNumber);
+  public static LottoGame of (Lottos lottos, WinningLotto winningLotto) {
+    return new LottoGame(lottos, winningLotto);
   }
 
   public long getWinningCount (Rank rank) {
     return lottos.stream()
-                 .filter(lotto -> getRankOfLotto(lotto).equals(rank))
+                 .map(winningLotto::getRankOfLotto)
+                 .filter(rank::equals)
                  .count();
   }
 
@@ -27,11 +26,5 @@ public class LottoGame {
                       .map(rank -> rank.getPrice() * getWinningCount(rank))
                       .reduce(0L, Math::addExact);
     return (double)payoff / lottos.getPrice();
-  }
-
-  public Rank getRankOfLotto (Lotto lotto) {
-    long same = winningLotto.sameCount(lotto);
-    boolean matchBonus = lotto.has(bonusNumber);
-    return Rank.valueOf(same, matchBonus);
   }
 }
