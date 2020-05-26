1b06b5f923d03103b71842f68ed6747093044808
jhseo <jhseo1@iteyes.co.kr>
2020-05-26 13:42:42 +0900

    feat splitAndSum 빈값체크

---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,14 @@
+package step1;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String formula) {
+        if (StringUtils.isBlank(formula)) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+}
