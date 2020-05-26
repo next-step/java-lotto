3d73fd33167410b590ed008fe4207041c19a6d4a
Gobukgol <zxczc159@gmail.com>
2020-05-26 17:10:29 +0900

    refactor(LottoNumberGenerator): LottoNumberGenerator 에 Integer 리스트를 받아 LottoNumber 리스트로 바꿔주는 메소드 추가

--- src/main/java/lotto/domain/number/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/number/LottoNumberGenerator.java
@@ -1,7 +1,9 @@
 package lotto.domain.number;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -34,5 +36,29 @@ public class LottoNumberGenerator {
                 .filter(lottoNumber -> lottoNumber.getNumber() == number)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("1 ~ 45 사이의 값이 아닙니다."));
+    }
+
+    public static List<LottoNumber> findAllByNumbers(List<Integer> numbers) {
+        validateSizeOfNumbers(numbers);
+        validateDuplicateNumber(numbers);
+
+        return numbers.stream()
+                .map(LottoNumberGenerator::findByNumber)
+                .collect(Collectors.toList());
+    }
+
+    private static void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> duplicateNumbers = new HashSet<>(numbers);
+
+        if (duplicateNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("번호는 중복될 수 없습니다.");
+        }
+
+    }
+
+    private static void validateSizeOfNumbers(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("번호의 갯수가 6이 아닙니다.");
+        }
     }
 }
--- src/test/java/lotto/domain/number/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/number/LottoNumberGeneratorTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -38,5 +41,28 @@ class LottoNumberGeneratorTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> LottoNumberGenerator.findByNumber(number))
                 .withMessage("1 ~ 45 사이의 값이 아닙니다.");
+    }
+
+    @DisplayName("입력받은 Integer List가 중복값이 있으면 IllegalArgumentException throw")
+    @Test
+    void duplicatedNumbersThrowException() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumberGenerator.findAllByNumbers(numbers))
+                .withMessage("번호는 중복될 수 없습니다.");
+    }
+
+    @DisplayName("입력받은 Integer List의 크기가 6이 아니면 IllegalArgumentException throw")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 7, 8})
+    void notCorrectNumberSizeThrowException(int size) {
+        List<Integer> numbers = IntStream.rangeClosed(1, size)
+                .boxed()
+                .collect(Collectors.toList());
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumberGenerator.findAllByNumbers(numbers))
+                .withMessage("번호의 갯수가 6이 아닙니다.");
     }
 }
