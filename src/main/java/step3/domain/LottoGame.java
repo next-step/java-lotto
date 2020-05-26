156b5e50ddc4da3c2cb308ae11b47e8d10a4031b
junilHwang <tjsdlf4261@naver.com>
2020-05-26 23:34:05 +0900

    refactor: [Step3] LottoGame 리팩토링 (2)

--- src/main/java/step3/domain/LottoGame.java
+++ src/main/java/step3/domain/LottoGame.java
@@ -9,12 +9,15 @@ public class LottoGame {
   }
 
   public static LottoGame of (long purchaseAmount, Prizes prizes) {
-
     long payoff = prizes.getPayoff();
     double payoffRatio = (double)payoff / purchaseAmount;
-
     return new LottoGame(payoffRatio);
+  }
 
+  public static LottoGame of (Lottos lottos, WinningLotto winningLotto) {
+    long payoff = Prizes.of(lottos, winningLotto).getPayoff();
+    double payoffRatio = (double)payoff / lottos.getPrice();
+    return new LottoGame(payoffRatio);
   }
 
   public double getPayoffRatio () {
