d47b3aca872fc48fae98d53858f9a74b471e95c5
eunji <12@>
2020-05-26 16:08:56 +0900

    feat splitAndSum_null_또는_빈문자() 통과 코드 작성

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -1,7 +1,11 @@
 package stringaddcalculator;
 
 public class StringAddCalculator {
-    public static int splitAndSum(Object o) {
-        return 0;
+    public static int splitAndSum(String input) {
+        if(input == null) return 0;
+        if(input.isEmpty()) return 0;
+
+
+        return -1;
     }
 }
