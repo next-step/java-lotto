e92dce6f9eed0392c2942b31caeeed17e5700ed1
eunji <12@>
2020-05-26 17:40:08 +0900

    refactor 커스텀 구분자 여부 확인 후 연산 진행 / 메서드 분리

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -4,31 +4,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String input) {
-        if (isNullOrEmpty(input)) return 0;
+    private StringAddCalculator() {}
 
-        if (input.length() == 1)
+    public static int splitAndSum(String input) {
+        if (isNullOrEmpty(input))
+            return 0;
+
+        if (isOneNumber(input))
             return Integer.parseInt(input);
 
+        if (isCustomSeparator(input))
+            return findSeparatorAndSum(input);
 
-        String separator = ",|:";
-        Pattern pattern = Pattern.compile("//(.)\n(.*)");
-        Matcher matcher = pattern.matcher(input);
-        if (matcher.find()) {
-            separator = matcher.group(1);
-            input = matcher.group(2);
-        }
-
-        String[] splitData = input.split(separator);
-        int result = 0;
-        for (String data : splitData) {
-            int number = Integer.parseInt(data);
-            if (number < 0)
-                throw new RuntimeException("음수 값이 입력 되었습니다.");
-            result += number;
-        }
-
-        return result;
+        return getBasicSum(",|:", input);
     }
 
     private static boolean isNullOrEmpty(String input) {
@@ -36,4 +24,44 @@ public class StringAddCalculator {
         if (input.isEmpty()) return true;
         return false;
     }
+
+    private static boolean isOneNumber(String input) {
+        if (input.length() == 1)
+            return true;
+        return false;
+    }
+
+    private static boolean isCustomSeparator(String input) {
+        if (input.startsWith("//"))
+            return true;
+        return false;
+    }
+
+    private static int findSeparatorAndSum(String input) {
+        Pattern pattern = Pattern.compile("//(.)\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+        String separator = ",|:";
+        if (matcher.find()) {
+            separator = matcher.group(1);
+            input = matcher.group(2);
+        }
+        return getBasicSum(separator, input);
+    }
+
+    private static int getBasicSum(String separator, String input) {
+        String[] splitValues = input.split(separator);
+        int result = 0;
+        for (String value : splitValues) {
+            int number = Integer.parseInt(value);
+            checkNegative(number);
+            result += number;
+        }
+        return result;
+    }
+
+    private static void checkNegative(int number) {
+        if (number < 0)
+            throw new RuntimeException("음수 값이 입력 되었습니다.");
+    }
+
 }
