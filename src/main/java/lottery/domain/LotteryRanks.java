c0224a5b92df576252f5cecf50459ba8f23de3e2
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-26 23:50:44 +0900

    refactor (LotteryRanks) key 변수 추가

--- src/main/java/lottery/domain/LotteryRanks.java
+++ src/main/java/lottery/domain/LotteryRanks.java
@@ -1,14 +1,20 @@
 package lottery.domain;
 
 public enum LotteryRanks {
-    FIRST_PRIZE(2_000_000_000),
-    SECOND_PRIZE(1_500_000),
-    THIRD_PRIZE(50_000),
-    FOURTH_PRIZE(5_000);
+    FIRST_PRIZE("first", 2_000_000_000),
+    SECOND_PRIZE("second", 1_500_000),
+    THIRD_PRIZE("third", 50_000),
+    FOURTH_PRIZE("fourth",5_000);
 
-    private final int prizeMoney;
+    private final String key;
+    private final long prizeMoney;
 
-    private LotteryRanks(int prizeMoney) {
+    private LotteryRanks(String key, long prizeMoney) {
+        this.key = key;
         this.prizeMoney = prizeMoney;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
     }
 }
