f95f83a784a80daabfecd9e65487416a428ce842
eunji <12@>
2020-05-26 16:10:18 +0900

    feat splitAndSum_숫자하나() 통과 코드 작성

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -2,10 +2,14 @@ package stringaddcalculator;
 
 public class StringAddCalculator {
     public static int splitAndSum(String input) {
-        if(input == null) return 0;
-        if(input.isEmpty()) return 0;
+        if (input == null) return 0;
+        if (input.isEmpty()) return 0;
+
+        if (1 == input.length())
+            return Integer.parseInt(input);
 
 
-        return -1;
+
+            return -1;
     }
 }
