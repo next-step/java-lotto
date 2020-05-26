1312a5f1dacb7d3124313acbdffcd5953293d694
eunji <43842782+eunji12@users.noreply.github.com>
2020-05-26 23:30:00 +0900

    1단계 - 문자열 덧셈 계산기 (#539)
    * feat TestCase 소스코드 추가
    
    * feat splitAndSum_null_또는_빈문자() 통과 코드 작성
    
    * feat splitAndSum_숫자하나() 통과 코드 작성
    
    * feat splitAndSum_쉼표구분자() 통과 코드 작성
    
    * feat splitAndSum_custom_구분자() 통과 코드 작성
    
    * feat splitAndSum_negative() 통과 코드 작성
    
    * refactor 널 체크 메서드 분리
    
    * refactor 커스텀 구분자 여부 확인 후 연산 진행 / 메서드 분리
    
    * refactor if statements 단순 리턴 구조로 변경
    
    Co-authored-by: eunji <들쥐12@>

---
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -0,0 +1,62 @@
+package stringaddcalculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private StringAddCalculator() {}
+
+    public static int splitAndSum(String input) {
+        if (isNullOrEmpty(input))
+            return 0;
+
+        if (isOneNumber(input))
+            return Integer.parseInt(input);
+
+        if (isCustomSeparator(input))
+            return findSeparatorAndSum(input);
+
+        return getBasicSum(",|:", input);
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        if (input == null) return true;
+        return input.isEmpty();
+    }
+
+    private static boolean isOneNumber(String input) {
+        return input.length() == 1;
+    }
+
+    private static boolean isCustomSeparator(String input) {
+        return input.startsWith("//");
+    }
+
+    private static int findSeparatorAndSum(String input) {
+        Pattern pattern = Pattern.compile("//(.)\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+        String separator = ",|:";
+        if (matcher.find()) {
+            separator = matcher.group(1);
+            input = matcher.group(2);
+        }
+        return getBasicSum(separator, input);
+    }
+
+    private static int getBasicSum(String separator, String input) {
+        String[] splitValues = input.split(separator);
+        int result = 0;
+        for (String value : splitValues) {
+            int number = Integer.parseInt(value);
+            checkNegative(number);
+            result += number;
+        }
+        return result;
+    }
+
+    private static void checkNegative(int number) {
+        if (number < 0)
+            throw new RuntimeException("음수 값이 입력 되었습니다.");
+    }
+
+}
