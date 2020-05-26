b90e040830f0dbf5605039dfff4645f7243f51d5
Jay Seo <bluehoped@hanmail.net>
2020-05-26 22:59:57 +0900

    Step1 - 문자열 덧셈 계산기 (#537)
    * test 테스트 코드 붙여넣기
    
    * feat 구분자 클래스 추가
    
    * feat splitAndSum 빈값체크
    
    * feat splitAndSum 숫자하나
    
    * feat splitAndSum 쉼표구분자
    
    * feat splitAndSum 쉼표 또는 콜론 구분자
    
    * feat splitAndSum 커스텀 구분자
    
    * feat splitAndSum negative RuntimeException
    
    * refactor 음수측정 메서드 return String
    
    Co-authored-by: jhseo <jhseo1@iteyes.co.kr>

---
+++ src/main/java/step1/Separator.java
@@ -0,0 +1,6 @@
+package step1;
+
+public class Separator {
+    public static final String DEFAULT_SEPARATOR = ",|:";
+    public static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+}
---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,59 @@
+package step1;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String formula) {
+        if (StringUtils.isBlank(formula)) {
+            return 0;
+        }
+
+        if (formula.length() == 1){
+            return Integer.parseInt(formula);
+        }
+
+        return arraySum(formula);
+    }
+
+    private static int arraySum(String formula) {
+        String[] array = getFormulaSplitArrayBySeparator(formula);
+
+        int sum = 0;
+        for (String number : array) {
+            sum += Integer.parseInt(checkNegative(number));
+        }
+
+        return sum;
+    }
+
+
+    private static String[] getFormulaSplitArrayBySeparator(String formula) {
+        if (formula.contains(",") || formula.contains(":")) {
+            return formula.split(Separator.DEFAULT_SEPARATOR);
+        }
+
+        return getFormulaSplitArrayByCustomSeparator(formula);
+    }
+
+    private static String[] getFormulaSplitArrayByCustomSeparator(String formula) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(formula);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
+        return null;
+    }
+
+    private static String checkNegative(String number) {
+        if (number.contains("-")) {
+            throw new RuntimeException();
+        }
+
+        return number;
+    }
+
+}
