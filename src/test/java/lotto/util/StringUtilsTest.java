7ccdb9c584b895eb0d5b56d46e091a7d8d804c9d
JuWanHan <baekseongsa@naver.com>
2020-05-27 01:25:11 +0900

    test(StringUtilsTest): 공백 유무 테스트 코드 추가

--- src/test/java/lotto/util/StringUtilsTest.java
+++ src/test/java/lotto/util/StringUtilsTest.java
@@ -4,7 +4,11 @@ import lotto.exception.InputValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -25,4 +29,19 @@ class StringUtilsTest {
         assertThat(StringUtils.toInt(stringInt)).isEqualTo(result);
     }
 
+    @DisplayName("공백 유무를 확인할 수 있다.")
+    @ParameterizedTest
+    @MethodSource("provideStringValue")
+    void canIsBlank(String value, boolean result) {
+        assertThat(StringUtils.isBlank(value)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> provideStringValue() {
+        return Stream.of(
+                Arguments.of("value", false),
+                Arguments.of(null, true),
+                Arguments.of("", true),
+                Arguments.of(" ", true)
+        );
+    }
 }
