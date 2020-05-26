ecb6d75f964835c9dc2709dfb7a2bc512f8662f9
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-27 00:19:30 +0900

    refactor: PositiveNumberExtractorTest methods to return List<PositiveNumber>

--- src/test/java/PositiveNumberExtractorTest.java
+++ src/test/java/PositiveNumberExtractorTest.java
@@ -1,12 +1,15 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import stringaddcalculator.PositiveNumber;
 import stringaddcalculator.PositiveNumberExtractor;
+import stringaddcalculator.StringAddCalculator;
 
 public class PositiveNumberExtractorTest {
 
@@ -15,7 +18,7 @@ public class PositiveNumberExtractorTest {
   void 음수인_경우_생성되지_않는다() {
     assertThatExceptionOfType(RuntimeException.class)
         .isThrownBy(() -> {
-          PositiveNumberExtractor.sum("-10");
+          PositiveNumberExtractor.create("-10").get(0);
         });
   }
 
@@ -23,8 +26,8 @@ public class PositiveNumberExtractorTest {
   @NullAndEmptySource
   @ParameterizedTest
   void NULL_빈_문자열이_입력된_경우_0을_뱉는다(String value) {
-    int positiveNumber = PositiveNumberExtractor.sum(value);
-    assertThat(positiveNumber).isEqualTo(0);
+    List<PositiveNumber> positiveNumber = PositiveNumberExtractor.create(value);
+    assertThat(positiveNumber.get(0).getNumber()).isEqualTo(0);
   }
 
   @DisplayName("입력받은 값이 변환 불가능한 경우 RuntimeException을 뱉는다.")
@@ -32,28 +35,30 @@ public class PositiveNumberExtractorTest {
   @ParameterizedTest
   void 정수로_변경_불가하면_생성되지_않는다(String value) {
     assertThatExceptionOfType(NumberFormatException.class)
-        .isThrownBy(() -> PositiveNumberExtractor.sum(value));
+        .isThrownBy(() -> PositiveNumberExtractor.create(value));
   }
 
   @DisplayName("하나만 입력된 자기 자신의 양수의 경우 정상적으로 출력된다.")
   @Test
   void 양수인_경우_생성된다() {
     String testNumber = "5";
-    int positiveNumber = PositiveNumberExtractor.sum(testNumber);
-    assertThat(positiveNumber).isEqualTo(Integer.parseInt(testNumber));
+    List<PositiveNumber> positiveNumber = PositiveNumberExtractor.create(testNumber);
+    assertThat(positiveNumber.get(0).getNumber()).isEqualTo(Integer.parseInt(testNumber));
   }
 
   @DisplayName("구분자가 컴마로 구성된 문자열이 입력될 경우 이를 합한 숫자가 반환된다.")
   @CsvSource(value = {"3,4! 7", "5,6! 11", "5,6,7! 18"}, delimiter = '!')
   @ParameterizedTest
   void 컴마로_구성된_문자열의_합이_리턴된다(String value, Integer result) {
-    assertThat(PositiveNumberExtractor.sum(value)).isEqualTo(result);
+    List<PositiveNumber> positiveNumbers = PositiveNumberExtractor.create(value);
+    assertThat(StringAddCalculator.sum(positiveNumbers)).isEqualTo(result);
   }
 
   @DisplayName("구분자가 콜론으로 구성된 문자열이 입력될 경우 이를 합한 숫자가 반환된다.")
   @CsvSource(value = {"3:4! 7", "5:6! 11", "5:6:7! 18"}, delimiter = '!')
   @ParameterizedTest
   void 콜론으로_구성된_문자열의_합이_리턴된다(String value, Integer result) {
-    assertThat(PositiveNumberExtractor.sum(value)).isEqualTo(result);
+    List<PositiveNumber> positiveNumbers = PositiveNumberExtractor.create(value);
+    assertThat(StringAddCalculator.sum(positiveNumbers)).isEqualTo(result);
   }
 }
