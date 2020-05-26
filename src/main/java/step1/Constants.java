b38a568cf23f2542177192d7bf41b5294f155e45
Wayne <choiwangyong@gmail.com>
2020-05-26 12:04:13 +0900

    Step1 (#535)
    * feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환 기능
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환 기능
    
    * feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환 기능
    
    * feat: 구분자를 컴마(,) 이외에 콜론(:)을 사용 기능
    
    * feat: “//”와 “\n” 문자 사이에 커스텀 구분자 기능
    
    * feat: 음수를 전달할 경우 RuntimeException 예외 발생 기능
    
    * refactor: modify method names
    
    * refactor: add error message class
    
    * refactor: add error message for non digit string

---
+++ src/main/java/step1/Constants.java
@@ -0,0 +1,7 @@
+package step1;
+
+public class Constants {
+
+    public static final String COMMA_OR_COLON_REX = ",|:";
+    public static final String SPECIAL_DELIMITER_REX = "//(.)\n(.*)";
+}
---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,74 @@
+package step1;
+
+import static step1.Constants.COMMA_OR_COLON_REX;
+import static step1.Constants.SPECIAL_DELIMITER_REX;
+import static step1.ErrorMessages.SHOULD_BE_NUMBER;
+import static step1.ErrorMessages.SHOULD_NOT_NEGATIVE_NUMBER;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String numberString) {
+        if (!validateText(numberString)) {
+            return 0;
+        }
+
+        return addParsedNumberString(parseNumberString(numberString));
+    }
+
+    private static Boolean validateText(String numberString) {
+        if (numberString == null) {
+            return false;
+        }
+
+        if (numberString.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static String[] parseNumberString(String numberString) {
+
+        Matcher specialDelimiterMatcher = Pattern.compile(SPECIAL_DELIMITER_REX)
+            .matcher(numberString);
+
+        return specialDelimiterMatcher.find() ?
+            parseNumbersWithSpecialDelimiterMatcher(specialDelimiterMatcher) :
+            parseNumbersWithNormalDelimiterString(numberString);
+    }
+
+    private static String[] parseNumbersWithSpecialDelimiterMatcher(
+        Matcher specialDelimiterMatcher) {
+        String customDelimiter = specialDelimiterMatcher.group(1);
+        return specialDelimiterMatcher.group(2).split(customDelimiter);
+    }
+
+    private static String[] parseNumbersWithNormalDelimiterString(String numberString) {
+        return numberString.split(COMMA_OR_COLON_REX);
+    }
+
+    private static int addParsedNumberString(String[] numbers) {
+        int result = 0;
+        for (String number : numbers) {
+            result += convertStringToInt(number);
+        }
+        return result;
+    }
+
+    private static int convertStringToInt(String number) {
+        int value;
+        try {
+            value = Integer.parseInt(number);
+        } catch (NumberFormatException numberFormatException) {
+            throw new RuntimeException(SHOULD_BE_NUMBER);
+        }
+
+        if (value < 0) {
+            throw new RuntimeException(SHOULD_NOT_NEGATIVE_NUMBER);
+        }
+        return value;
+    }
+}
