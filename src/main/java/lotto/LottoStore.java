fad680c4fb75162be21ed5b495a2b7a39bd8ce7e
jun-pil park <iamfeel2@gmail.com>
2020-05-27 10:25:38 +0900

    feat (LottoStore) : 로또를 구입하는 기능 추가

---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoStore {
+
+    public final static int LOTTO_PRICE_PER_ONE = 1000;
+
+    public static List<Lotto> buy(int price) {
+        validatePrice(price);
+        LottoGenerator lottoGenerator= LottoGenerator.newInstance();
+        int count = (int) Math.floor(price / LOTTO_PRICE_PER_ONE);
+        return lottoGenerator.generator(count);
+    }
+
+    private static void validatePrice(int price) {
+        if(price < LOTTO_PRICE_PER_ONE) {
+            throw new IllegalArgumentException("입력된 금액이 로또구매 최소금액보다 작습니다.");
+        }
+    }
+
+}
