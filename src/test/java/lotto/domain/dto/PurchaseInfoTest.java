ffae65d7d0df6400b1ea262480f7aa137071a321
kwonheejeong <gmlwjd9405@naver.com>
2020-05-27 14:52:44 +0900

    teat: PurchaseInfo 생성 테스트

--- src/test/java/lotto/domain/dto/PurchaseInfoTest.java
+++ src/test/java/lotto/domain/dto/PurchaseInfoTest.java
@@ -1,9 +1,11 @@
 package lotto.domain.dto;
 
+import lotto.domain.price.Price;
 import lotto.util.LottoTicketsGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PurchaseInfoTest {
@@ -13,5 +15,12 @@ public class PurchaseInfoTest {
     void createFailure() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> PurchaseInfo.valueOf(null, LottoTicketsGenerator.newInstance()));
+    }
+
+    @DisplayName("Price 와 수동 로또 번호를 인자로 입력받아 객체를 생성")
+    @Test
+    void create() {
+        assertThatCode(() -> PurchaseInfo.valueOf(Price.of(Price.ONE_TICKET_PRICE), LottoTicketsGenerator.newInstance()))
+                .doesNotThrowAnyException();
     }
 }
