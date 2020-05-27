5bddb1bd208f259302a18f58471da13aba13148a
kwonheejeong <gmlwjd9405@naver.com>
2020-05-27 15:09:08 +0900

    test: 남은 금액으로 구매할 수 있는 autoTickets 개수를 반환

--- src/test/java/lotto/domain/dto/PurchaseInfoTest.java
+++ src/test/java/lotto/domain/dto/PurchaseInfoTest.java
@@ -6,15 +6,31 @@ import lotto.domain.ticket.LottoTickets;
 import lotto.exception.AvailableCountExceedException;
 import lotto.util.LottoTicketGenerator;
 import lotto.util.LottoTicketsGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class PurchaseInfoTest {
+
+    private LottoTickets manualTickets;
+
+    @BeforeEach
+    void setUp() {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.add(LottoTicketGenerator.valueOf(1, 2, 3, 4, 5, 6));
+        lottoTickets.add(LottoTicketGenerator.valueOf(1, 2, 3, 7, 8, 9));
+
+        manualTickets = LottoTickets.of(lottoTickets);
+    }
 
     @DisplayName("Price 가 null 이면 예외 발생")
     @Test
@@ -35,12 +51,27 @@ public class PurchaseInfoTest {
     void exceedOfAvailableCount() {
         Price price = Price.of(Price.ONE_TICKET_PRICE);
 
+        assertThatExceptionOfType(AvailableCountExceedException.class)
+                .isThrownBy(() -> PurchaseInfo.valueOf(price, manualTickets));
+    }
+
+    @DisplayName("남은 금액으로 구매할 수 있는 autoTickets 개수를 반환")
+    @ParameterizedTest
+    @MethodSource
+    void getNumOfAutoTickets(final Price price, final LottoTickets manualTickets, final int expected) {
+        assertThat(PurchaseInfo.valueOf(price, manualTickets).getNumOfAutoTickets()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> getNumOfAutoTickets() {
         List<LottoTicket> lottoTickets = new ArrayList<>();
         lottoTickets.add(LottoTicketGenerator.valueOf(1, 2, 3, 4, 5, 6));
         lottoTickets.add(LottoTicketGenerator.valueOf(1, 2, 3, 7, 8, 9));
-        LottoTickets manualTickets = LottoTickets.of(lottoTickets);
 
-        assertThatExceptionOfType(AvailableCountExceedException.class)
-                .isThrownBy(() -> PurchaseInfo.valueOf(price, manualTickets));
+        return Stream.of(
+                Arguments.of(Price.of(Price.ONE_TICKET_PRICE), LottoTicketsGenerator.valueOf(1, 2, 3, 4, 5, 6), 0),
+                Arguments.of(Price.of(Price.ONE_TICKET_PRICE), LottoTicketsGenerator.newInstance(), 1),
+                Arguments.of(Price.of(Price.ONE_TICKET_PRICE * 4), LottoTickets.of(lottoTickets), 2),
+                Arguments.of(Price.of(Price.ONE_TICKET_PRICE * 10), LottoTickets.of(lottoTickets), 8)
+        );
     }
 }
