bb878ad3685dbcaef005d3bb81e96405c371b36d
jwooss <wodn4131@gmail.com>
2020-05-27 23:46:29 +0900

    feat: 금액을 관리하는 Money 객체

---
+++ src/main/java/step2/Money.java
@@ -0,0 +1,41 @@
+package step2;
+
+import java.util.Objects;
+
+public class Money {
+
+    private int amount;
+
+    private Money(int amount) {
+        this.amount = amount;
+    }
+
+    public static Money valueOf(int amount) {
+        return new Money(amount);
+    }
+
+    public void useAmount(int usedAmount) {
+        if (!this.isAvailableAmount(usedAmount)) {
+            throw new IllegalStateException("보유중인 금액이 부족합니다.");
+        }
+
+        this.amount -= usedAmount;
+    }
+
+    public boolean isAvailableAmount(int usedAmount) {
+        return usedAmount <= this.amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
---
+++ src/test/java/step2/MoneyTest.java
@@ -0,0 +1,63 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MoneyTest {
+
+    @DisplayName("valueOf()는 Money의 Instance를 반환한다")
+    @ValueSource(ints = {1, 2, 3, 4})
+    @ParameterizedTest
+    void valueOf_Amount_MoneyInstance(int amount) {
+        Money money = Money.valueOf(amount);
+        assertThat(money).isInstanceOf(Money.class);
+    }
+
+    @DisplayName("useAmount()는 입력한 금액을 보유한 금액에서 차감한다")
+    @MethodSource("provideMoneyForUseAmount")
+    @ParameterizedTest
+    void useAmount_UsedAmount_SubtractAmount(int totalAmount, int usedAmount) {
+        Money money = Money.valueOf(totalAmount);
+        money.useAmount(usedAmount);
+        assertThat(money).isEqualTo(Money.valueOf(totalAmount - usedAmount));
+    }
+
+    private static Stream<Arguments> provideMoneyForUseAmount() {
+        return Stream.of(
+                Arguments.of(10000, 10000),
+                Arguments.of(10000, 1000),
+                Arguments.of(10000, 100),
+                Arguments.of(10000, 10),
+                Arguments.of(10000, 1)
+        );
+    }
+
+    @DisplayName("useAmount()는 입력한 금액이 보유 금액보다 클 경우 Ru")
+    @Test
+    void useAmount_NotAllowedAmount_ExceptionThrown() {
+        Money money = Money.valueOf(1000);
+
+        assertThatThrownBy(() -> money.useAmount(10000))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("보유중인 금액이 부족합니다.");
+    }
+
+    @DisplayName("isAvailableAmount()는 보유중인 금액과 비교해서 TRUE 또는 FALSE를 반환한다")
+    @Test
+    void isAvailableAmount_UsedAmount_Boolean() {
+        Money money = Money.valueOf(1000);
+
+        assertThat(money.isAvailableAmount(1001)).isFalse();
+        assertThat(money.isAvailableAmount(1000)).isTrue();
+        assertThat(money.isAvailableAmount(999)).isTrue();
+    }
+}
