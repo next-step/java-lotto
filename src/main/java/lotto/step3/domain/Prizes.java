7740f78167111209869b7c1a3f8009615a104565
junilHwang <tjsdlf4261@naver.com>
2020-05-27 19:12:51 +0900

    refactor: [Step3] Prizes에 payoffRatio를 구하는 메소드 추가

--- src/main/java/lotto/step3/domain/Prizes.java
+++ src/main/java/lotto/step3/domain/Prizes.java
@@ -8,11 +8,13 @@ import static java.util.stream.Collectors.toList;
 public class Prizes {
   private final List<Prize> prizes;
   private final WinningLotto winningLotto;
+  private final Lottos lottos;
 
   private Prizes (Lottos lottos, WinningLotto winningLotto) {
     this.winningLotto = winningLotto;
+    this.lottos = lottos;
     this.prizes = Rank.stream()
-                      .map(rank -> Prize.of(rank, getWinningCount(lottos, rank)))
+                      .map(rank -> Prize.of(rank, getWinningCount(rank)))
                       .collect(toList());
   }
 
@@ -20,7 +22,7 @@ public class Prizes {
     return new Prizes(lottos, winningLotto);
   }
 
-  private long getWinningCount (Lottos lottos, Rank rank) {
+  private long getWinningCount (Rank rank) {
     return lottos.stream()
                  .map(winningLotto::getRankOfLotto)
                  .filter(rank::equals)
@@ -33,5 +35,9 @@ public class Prizes {
 
   public long getPayoff () {
     return stream().mapToLong(Prize::getTotalPrize).sum();
+  }
+
+  public double resultLottoGamePayOffRatio () {
+    return (double)getPayoff() / lottos.getPrice();
   }
 }
