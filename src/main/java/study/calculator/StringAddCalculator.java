89af81280a9892a471dc54d7104f80ccb7a3a681
Jaehun Lee <ljhokgo@gmail.com>
2020-05-27 22:35:03 +0900

    feat: 문자열 덧셈 계산
    - 구분자 기준으로 입력값 분리
    - 분리된 값 유효성 검사
    - 분리된 값 더하기

--- src/main/java/study/calculator/StringAddCalculator.java
+++ src/main/java/study/calculator/StringAddCalculator.java
@@ -1,7 +1,39 @@
 package study.calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+    private static final String BASE_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+
     public static int splitAndSum(String input) {
-        return 0;
+        if(input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return new Numbers(split(input)).sum();
     }
+
+    private static String[] split(String input) {
+        if(input.startsWith("//")) {
+            return splitWithCustomDelimiter(input);
+        }
+
+        return input.split(BASE_DELIMITER);
+    }
+
+    private static String[] splitWithCustomDelimiter(String input) {
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+
+            return m.group(2).split(customDelimiter);
+        }
+
+        return null;
+    }
+
+
 }
