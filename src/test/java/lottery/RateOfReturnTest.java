9856a5cb09a18e02fe0e657f2eb097cce00c63fb
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-27 14:40:11 +0900

    refactor (RateOfReturnTest) : 테스트 source 수정

--- src/test/java/lottery/RateOfReturnTest.java
+++ src/test/java/lottery/RateOfReturnTest.java
@@ -11,8 +11,8 @@ public class RateOfReturnTest {
 
     @DisplayName("수익률이 1 이상이면 true, 아니면 false 리턴")
     @ParameterizedTest
-    @CsvSource({"3, true", "1, true", "0.7, false"})
-    public void getBooleanOnRateOfReturn(double rateOfReturn, boolean result) {
+    @CsvSource({"3, true", "1.2, true", "0.7, false"})
+    public void getBooleanOnRateOfReturn(double rateOfReturn, Boolean result) {
         RateOfReturn rateOfReturnObject = new RateOfReturn(rateOfReturn);
 
         assertThat(rateOfReturnObject.isSurplus()).isEqualTo(result);
