59d39ba2f3ceeedf7a246b8a15570cb7ca60603c
junpil park <iamfeel2@gmail.com>
2020-05-27 23:59:21 +0900

    fix (LottoCheckTest) : 꽝가 일때의 당첨번호 중 일치하는 개수를 파악하는 테스트 코드 추

--- src/test/java/lotto/LottoCheckTest.java
+++ src/test/java/lotto/LottoCheckTest.java
@@ -14,6 +14,7 @@ public class LottoCheckTest {
     private static final int INDEX_RANK_2 = 1;
     private static final int INDEX_RANK_3 = 2;
     private static final int INDEX_RANK_4 = 3;
+    private static final int INDEX_BOOM = 4;
 
     @DisplayName("구매한 로또 중에 각 등수에 해당하는 로또 당첨 수 리턴한다.")
     @Test
@@ -22,20 +23,23 @@ public class LottoCheckTest {
         List<Lotto> lottos = Arrays.asList(
                 new Lotto(List.of(1,2,3,4,5,8)),
                 new Lotto(List.of(1,2,3,9,10,8)),
+                new Lotto(List.of(7,8,9,12,10,11)),
                 new Lotto(List.of(1,10,3,9,5,8)));
         LottoCheck lottoCheck = new LottoCheck(winningNumber, lottos);
-        final int[] count = new int[4];
+        final int[] count = new int[5];
 
         count[INDEX_RANK_1] = lottoCheck.getWinningLottoCount(LottoRank.RANK1);
         count[INDEX_RANK_2] = lottoCheck.getWinningLottoCount(LottoRank.RANK2);
         count[INDEX_RANK_3] = lottoCheck.getWinningLottoCount(LottoRank.RANK3);
         count[INDEX_RANK_4] = lottoCheck.getWinningLottoCount(LottoRank.RANK4);
+        count[INDEX_BOOM] = lottoCheck.getWinningLottoCount(LottoRank.BOOM);
 
         assertAll(
             () -> Assertions.assertThat(count[INDEX_RANK_1]).isEqualTo(0),
             () -> Assertions.assertThat(count[INDEX_RANK_2]).isEqualTo(1),
             () -> Assertions.assertThat(count[INDEX_RANK_3]).isEqualTo(0),
-            () -> Assertions.assertThat(count[INDEX_RANK_4]).isEqualTo(2)
+            () -> Assertions.assertThat(count[INDEX_RANK_4]).isEqualTo(2),
+            () -> Assertions.assertThat(count[INDEX_BOOM]).isEqualTo(1)
         );
     }
 
