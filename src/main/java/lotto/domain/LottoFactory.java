62d4db5d9c3af74c972b24a26b769172e8710fc1
yjs <yjs2952@naver.com>
2020-05-27 13:30:46 +0900

    feat: 구입 금액 음수 입력 시 예외 발생 로직 구현 및 테스트

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -8,11 +8,18 @@ public class LottoFactory {
     private static final int PRICE = 1000;
 
     public static List<Lotto> generateLottos(int totalPrice) {
+        validationCheck(totalPrice);
         int lottoCount = totalPrice / PRICE;
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
             lottos.add(new Lotto());
         }
         return lottos;
+    }
+
+    private static void validationCheck(int totalPrice) {
+        if (totalPrice < 0) {
+            throw new IllegalArgumentException("구입금액은 음수를 입력할 수 없습니다.");
+        }
     }
 }
--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -5,12 +5,24 @@ import lotto.domain.LottoFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoFactoryTest {
+
+    @DisplayName("구입 금액이 음수인 경우 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -4})
+    public void whenInputNegativeThenExceptionTest(int price){
+        // then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> LottoFactory.generateLottos(price)
+        );
+    }
 
     @DisplayName("구입 금액을 입력하면 로또 가격으로 나눈 수 만큼의 로또를 발급한다.")
     @ParameterizedTest
