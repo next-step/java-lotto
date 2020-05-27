42a300dbb0263ffd7e60bdba7851c3e23c100ae8
junilHwang <tjsdlf4261@naver.com>
2020-05-27 23:18:51 +0900

    feat: [Step4] 수동구매 로직 추가

--- src/main/java/lotto/step4/view/InputView.java
+++ src/main/java/lotto/step4/view/InputView.java
@@ -1,21 +1,39 @@
 package lotto.step4.view;
 
+import lotto.step4.domain.Lotto;
+import lotto.step4.domain.LottoGenerator;
 import lotto.step4.domain.LottoNumber;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class InputView {
 
   private static InputView instance;
-  private final Scanner scanner;
+  private static final Scanner scanner = new Scanner(System.in);
 
-  private InputView () {
-    this.scanner = new Scanner(System.in);
-  }
+  private InputView () {}
 
   public int inputPrice () {
     System.out.println("구입금액을 입력해 주세요.");
     return Integer.parseInt(scanner.nextLine());
+  }
+
+  public List<Lotto> inputLottoYourOwn () {
+    System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+    int length = Integer.parseInt(scanner.nextLine());
+
+    System.out.println("\n로또 번호를 입력해 주세요.");
+    return Stream.generate(InputView::inputLotto)
+                 .limit(length)
+                 .collect(toList());
+  }
+
+  private static Lotto inputLotto () {
+    return LottoGenerator.generateLotto(scanner.nextLine());
   }
 
   public String inputWinning () {
