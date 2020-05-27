d629669c1dd647f9ddabe7a3fed443e3cdd3b126
kwonheejeong <gmlwjd9405@naver.com>
2020-05-27 14:55:57 +0900

    teat: 구입 금액보다 수동으로 구매한 티켓 수가 더 많으면 예외가 발생

--- src/test/java/lotto/domain/dto/PurchaseInfoTest.java
+++ src/test/java/lotto/domain/dto/PurchaseInfoTest.java
@@ -1,12 +1,18 @@
 package lotto.domain.dto;
 
 import lotto.domain.price.Price;
+import lotto.domain.ticket.LottoTicket;
+import lotto.domain.ticket.LottoTickets;
+import lotto.exception.AvailableCountExceedException;
+import lotto.util.LottoTicketGenerator;
 import lotto.util.LottoTicketsGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PurchaseInfoTest {
 
@@ -22,5 +28,19 @@ public class PurchaseInfoTest {
     void create() {
         assertThatCode(() -> PurchaseInfo.valueOf(Price.of(Price.ONE_TICKET_PRICE), LottoTicketsGenerator.newInstance()))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("수동으로 입력한 로또 티켓이 구매할 수 있는 티켓의 수보다 많으면 예외 반환")
+    @Test
+    void exceedOfAvailableCount() {
+        Price price = Price.of(Price.ONE_TICKET_PRICE);
+
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.add(LottoTicketGenerator.valueOf(1, 2, 3, 4, 5, 6));
+        lottoTickets.add(LottoTicketGenerator.valueOf(1, 2, 3, 7, 8, 9));
+        LottoTickets manualTickets = LottoTickets.of(lottoTickets);
+
+        assertThatExceptionOfType(AvailableCountExceedException.class)
+                .isThrownBy(() -> PurchaseInfo.valueOf(price, manualTickets));
     }
 }
