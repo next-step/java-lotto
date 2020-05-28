22d41477b1412fc23061a9fe115a0db51b19a187
하재권 <hazxz@naver.com>
2020-05-29 06:03:10 +0900

    chore: rename test method

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -14,13 +14,13 @@ class LottoNumberTest {
 
     @DisplayName("1 ~ 45 사이의 수로 LottoNumber 생성하는데 성공")
     @ParameterizedTest
-    @MethodSource("source_create_validParameters_shouldSucceed")
-    public void create_validParameters_shouldSucceed(int param, LottoNumber expected) {
+    @MethodSource("source_generateByManual_validParameters_shouldSucceed")
+    public void generateByManual_validParameters_shouldSucceed(int param, LottoNumber expected) {
         LottoNumber result = LottoNumber.generateByManual(param);
         assertThat(result).isEqualTo(expected);
     }
 
-    private static Stream<Arguments> source_create_validParameters_shouldSucceed() {
+    private static Stream<Arguments> source_generateByManual_validParameters_shouldSucceed() {
         return Stream.of(
                 Arguments.of(1, LottoNumber.generateByManual(1)),
                 Arguments.of(16, LottoNumber.generateByManual(16)),
@@ -29,14 +29,14 @@ class LottoNumberTest {
 
     @DisplayName("1 ~ 45 바깥의 수로 LottoNumber 생성시에 throw IllegalArgumentException")
     @ParameterizedTest
-    @MethodSource("source_create_invalidParameters_shouldFail")
-    public void create_invalidParameters_shouldFail(int param) {
+    @MethodSource("source_generateByManual_invalidParameters_shouldFail")
+    public void generateByManual_invalidParameters_shouldFail(int param) {
         assertThatThrownBy(() -> {
             LottoNumber.generateByManual(param);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
-    private static Stream<Arguments> source_create_invalidParameters_shouldFail() {
+    private static Stream<Arguments> source_generateByManual_invalidParameters_shouldFail() {
         return Stream.of(
                 Arguments.of(-1),
                 Arguments.of(0),
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -16,13 +16,13 @@ class LottoTest {
 
     @DisplayName("중복되지 않은 숫자 6개로 Lotto 생성하는데 성공")
     @ParameterizedTest
-    @MethodSource("source_create_notDuplicatedNumbers_shouldSucceed")
-    public void create_notDuplicatedNumbers_shouldSucceed(List<Integer> param, Lotto expected) {
+    @MethodSource("source_generateByManual_notDuplicatedNumbers_shouldSucceed")
+    public void generateByManual_notDuplicatedNumbers_shouldSucceed(List<Integer> param, Lotto expected) {
         Lotto result = Lotto.generateByManual(param);
         assertThat(result).isEqualTo(expected);
     }
 
-    private static Stream<Arguments> source_create_notDuplicatedNumbers_shouldSucceed() {
+    private static Stream<Arguments> source_generateByManual_notDuplicatedNumbers_shouldSucceed() {
         return Stream.of(
                 Arguments.of(Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6}), Lotto.generateByManual(Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6}))),
                 Arguments.of(Arrays.asList(new Integer[]{40, 41, 42, 43, 44, 45}), Lotto.generateByManual(Arrays.asList(new Integer[]{40, 41, 42, 43, 44, 45}))));
@@ -30,14 +30,14 @@ class LottoTest {
 
     @DisplayName("중복된 숫자 6개로 Lotto 생성 시에 throws IllegalArgumentException")
     @ParameterizedTest
-    @MethodSource("source_create_duplicatedNumbers_shouldFail")
-    public void create_source_create_duplicatedNumbers_shouldFail(List<Integer> param) {
+    @MethodSource("source_generateByManual_duplicatedNumbers_shouldFail")
+    public void create_generateByManual_create_duplicatedNumbers_shouldFail(List<Integer> param) {
         assertThatThrownBy(() -> {
             Lotto.generateByManual(param);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
-    private static Stream<Arguments> source_create_duplicatedNumbers_shouldFail() {
+    private static Stream<Arguments> source_generateByManual_duplicatedNumbers_shouldFail() {
         return Stream.of(
                 Arguments.of(Arrays.asList(new Integer[]{1, 1, 3, 4, 5, 6})),
                 Arguments.of(Arrays.asList(new Integer[]{40, 41, 42, 43, 45, 45})));
