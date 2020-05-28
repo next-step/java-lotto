d5b830a99858df5cd2edb7a395bb304fbb85720f
yongjin-sixshop <yongjin@sixshop.com>
2020-05-28 21:20:17 +0900

    <feat> Lotto Number validation 추가

--- src/main/java/step2/model/LottoNumber.java
+++ src/main/java/step2/model/LottoNumber.java
@@ -4,14 +4,25 @@ import java.util.Objects;
 
 public class LottoNumber implements Comparable<LottoNumber>{
 
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
     private Integer number;
 
     private LottoNumber(Integer number) {
         this.number = number;
     }
 
-    public static LottoNumber newInstance(Integer number) {
+    public static LottoNumber of(Integer number)
+    {
+        validateRange(number);
         return new LottoNumber(number);
+    }
+
+    private static void validateRange(final Integer number) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException("로또의 수 범위는 1 ~ 45 까지의 입니다.");
+        }
     }
 
     @Override
