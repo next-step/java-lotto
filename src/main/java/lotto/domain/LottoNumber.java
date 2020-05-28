1c69483857fa6c788f437a8f07b6c29bd0274f5b
하재권 <hazxz@naver.com>
2020-05-29 04:48:47 +0900

    feat(lotto): 로또 숫자를 관리하는 LottoNumber class 프로덕션 코드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,13 +1,33 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final int number;
 
     public LottoNumber(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(String.format("`number` is must be %d ~ %d", MIN_NUMBER, MAX_NUMBER));
+        }
         this.number = number;
     }
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
