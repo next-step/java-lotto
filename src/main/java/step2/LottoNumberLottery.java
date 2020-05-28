cb69ffd85144b2bb4ef2cdc1b8b05aee86514651
bluehoped <jhseo1@iteyes.co.kr>
2020-05-28 20:22:22 +0900

    feat 로또 번호 추첨기 클래스

---
+++ src/main/java/step2/LottoNumberLottery.java
@@ -0,0 +1,36 @@
+package step2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumberLottery {
+    private static final int MAX_LOTTERY_NUMBER_COUNT = 6;
+
+    public List<LotteryNumbers> getLottoNumbersList(int count) {
+        List<LotteryNumbers> lotteryNumbersList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lotteryNumbersList.add(getLotteryNumbers());
+        }
+
+        return lotteryNumbersList;
+    }
+
+    private LotteryNumbers getLotteryNumbers() {
+        List<Integer> lottoNumbers = LottoNumbers.getLottoNumbers();
+        Collections.shuffle(lottoNumbers);
+
+        LotteryNumbers lotteryNumbers = new LotteryNumbers();
+        for (int i = 0; i < MAX_LOTTERY_NUMBER_COUNT; i ++) {
+            lotteryNumbers.addLottoNumber(lottoNumbers.get(i));
+        }
+
+        sortLotteryNumbers(lotteryNumbers);
+
+        return lotteryNumbers;
+    }
+
+    private void sortLotteryNumbers(LotteryNumbers lotteryNumbers) {
+        Collections.sort(lotteryNumbers.getLottoNumberList());
+    }
+}
