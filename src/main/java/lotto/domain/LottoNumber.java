a1224625aad6973aec392a3d0e90ec86883bc1c1
하재권 <hazxz@naver.com>
2020-05-29 04:46:27 +0900

    test(lotto): 로또 숫자를 관리하는 LottoNumber class 실패하는 테스트 추가

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class LottoNumber {
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
