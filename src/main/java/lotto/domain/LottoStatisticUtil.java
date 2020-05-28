bc9075758aed5ca7644e619c93815869ce33c3b1
하재권 <hazxz@naver.com>
2020-05-29 08:40:15 +0900

    feat(lotto): 당첨번호와 구입한 로또를 입력받으면 당첨에 대한 통계를 출력하는 LottoStatisticUtil 프로덕션 코드 추가

--- src/main/java/lotto/domain/LottoStatisticUtil.java
+++ src/main/java/lotto/domain/LottoStatisticUtil.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class LottoStatisticUtil {
 
     public static int getPrizeCount(Prize prize, Lotto winLotto, List<Lotto> lottoList) {
-        return 0;
+        int count = 0;
+        for (Lotto lotto : lottoList) {
+            count += prize == Prize.award(winLotto, lotto) ? 1 : 0;
+        }
+        return count;
     }
 }
