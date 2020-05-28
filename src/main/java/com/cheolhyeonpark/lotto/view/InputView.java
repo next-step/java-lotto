04bb44f497dd42323c7271f1b3f9bfb12f80a7f1
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-28 21:30:23 +0900

    Feat: input purchase amount and winning numbers

---
+++ src/main/java/com/cheolhyeonpark/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package com.cheolhyeonpark.lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public int inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public String inputWinningNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        scanner.close();
+        super.finalize();
+    }
+}
