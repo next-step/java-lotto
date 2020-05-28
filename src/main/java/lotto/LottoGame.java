e1fbf0c35431628ddbf427e9d6327d1a8a92b8d3
하재권 <hazxz@naver.com>
2020-05-29 06:34:43 +0900

    feat(lotto): InputView class를 통해 구입 금액을 입력 받을 수 있다.

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Price;
+import lotto.ui.InputView;
+
+import java.util.List;
+
+public class LottoGame {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        Price price = inputView.insertPrice();
+
+        LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottoList = lottoMachine.buy(price);
+        inputView.answerPrice(lottoList.size());
+    }
+}
---
+++ src/main/java/lotto/ui/InputView.java
@@ -0,0 +1,20 @@
+package lotto.ui;
+
+import lotto.domain.Price;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String QUESTION_INSERT_PRICE = "구입금액을 입력해 주세요.";
+    private static final String ANSWER_INSERT_PRICE = "%d개를 구매했습니다.";
+
+    public Price insertPrice() {
+        System.out.println(QUESTION_INSERT_PRICE);
+        Scanner scanner = new Scanner(System.in);
+        return Price.of(scanner.nextLine());
+    }
+
+    public void answerPrice(int count) {
+        System.out.println(String.format(ANSWER_INSERT_PRICE, count));
+    }
+}
