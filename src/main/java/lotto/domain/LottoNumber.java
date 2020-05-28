10a5065326190bdf93d947c52215b571f2203ac7
하재권 <hazxz@naver.com>
2020-05-29 04:59:11 +0900

    refactoring(lotto): 유효한(1~45) 로또 숫자를 관리하는 변수 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,14 +1,18 @@
 package lotto.domain;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    private static final List<Integer> LOTTO_NUMBER_LIST = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER).boxed().collect(Collectors.toList());
     private final int number;
 
     public LottoNumber(int number) {
-        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+        if (LOTTO_NUMBER_LIST.contains(number)) {
             throw new IllegalArgumentException(String.format("`number` is must be %d ~ %d", MIN_NUMBER, MAX_NUMBER));
         }
         this.number = number;
