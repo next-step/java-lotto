1239c13fb652a2471eb7369af289276547a77b60
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-29 02:11:33 +0900

    Feat: add price in Rank

--- src/main/java/com/cheolhyeonpark/lotto/domain/Rank.java
+++ src/main/java/com/cheolhyeonpark/lotto/domain/Rank.java
@@ -3,20 +3,26 @@ package com.cheolhyeonpark.lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6),
-    SECOND(5),
-    THIRD(4),
-    FOURTH(3),
-    NONE(0);
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    NONE(0, 0);
 
     private final int count;
+    private final int price;
 
-    Rank(int count) {
+    Rank(int count, int price) {
         this.count = count;
+        this.price = price;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public int getPrice() {
+        return price;
     }
 
     public static Rank findRank(int count) {
