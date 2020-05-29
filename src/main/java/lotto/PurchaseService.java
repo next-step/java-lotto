50c5d7138097af10d3b9a752d77407f3e60de01b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-29 19:27:24 +0900

    refactor: return size of purchased lotto tickets

--- src/main/java/lotto/PurchaseService.java
+++ src/main/java/lotto/PurchaseService.java
@@ -1,8 +1,19 @@
 package lotto;
 
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTickets;
+
 public class PurchaseService {
 
-  public Integer purchase(Integer payable) {
-    return (payable / 1000);
+  public Integer purchase(final Integer payable) {
+    final int DIVIDE_THRESHOLD = 1000;
+    final int NUMBER_OF_PURCHASED_LOTTO = (payable / DIVIDE_THRESHOLD);
+    LottoTickets lottoTickets = new LottoTickets();
+
+    for (int index = 0; index < NUMBER_OF_PURCHASED_LOTTO; index++) {
+      lottoTickets.updateLottoTickets(new LottoTicket());
+    }
+
+    return lottoTickets.numberOfLottoTickets();
   }
 }
