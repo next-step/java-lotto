607f2631a9a47aee33e2878a93ad54312f49aef0
Jaehun Lee <ljhokgo@gmail.com>
2020-05-29 22:43:49 +0900

    feat: 로또 번호 생성 기능 추가
    - 로또 번호 베이스 생성 (1~45 숫자 리스트)
    - 베이스 셔플 후 subList(0, 6) 추출

--- src/main/java/study/lotto/model/Lotto.java
+++ src/main/java/study/lotto/model/Lotto.java
@@ -1,16 +1,34 @@
 package study.lotto.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final List<Integer> LOTTO_NUMBER_BASE = new ArrayList<>();
+
     private List<Integer> lottoNumbers = new ArrayList<>();
 
+    static {
+        for(int i=1; i<46; i++) {
+            LOTTO_NUMBER_BASE.add(i);
+        }
+    }
+
     public static Lotto generate() {
-        return new Lotto();
+        Lotto lotto = new Lotto();
+        lotto.lottoNumbers = lotto.generateLottoNumbers();
+
+        return lotto;
     }
 
     public List<Integer> getLottoNumbers() {
         return lottoNumbers;
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        Collections.shuffle(LOTTO_NUMBER_BASE);
+
+        return LOTTO_NUMBER_BASE.subList(0, 6);
     }
 }
