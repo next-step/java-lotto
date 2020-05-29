2020c4cfcc4adb254d5ac41269d635f68683c883
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-29 20:47:10 +0900

    test: validate lotto number range from 1 to 45

---
+++ src/test/java/LottoNumberTest.java
@@ -0,0 +1,16 @@
+import static lotto.domain.LottoNumber.MAXIMUM_NUMBER_BOUND;
+import static lotto.domain.LottoNumber.MINIMUM_NUMBER_BOUND;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.domain.LottoNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberTest {
+
+  @DisplayName("로또 넘버에 1부터 45 사이의 숫자만 들어갈 수 있다.")
+  @Test
+  void 로또_넘버는_1부터_45_사이이다() {
+    assertThat(new LottoNumber().getNumber()).isBetween(MINIMUM_NUMBER_BOUND, MAXIMUM_NUMBER_BOUND);
+  }
+}
