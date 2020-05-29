d10bb28339c7a76f5db728d955444beba937fc30
Jun <p_illusion@naver.com>
2020-05-30 02:04:25 +0900

    [feat] input, output 생성

---
+++ src/main/java/view/LottoInput.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Scanner;
+
+public class LottoInput {
+    private static final String INPUT_MENT = "숫자오 구분자 문자열을 입력하시오 : ";
+
+    public static String inputGameCoin() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(INPUT_MENT);
+        String scanString = scanner.nextLine();
+
+        return scanString;
+    }
+}
