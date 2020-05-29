78c153363e72f909813f8fca5189995e217fc560
Jun <p_illusion@naver.com>
2020-05-30 02:04:58 +0900

    [feat] 컨트롤러 생성

---
+++ src/main/java/LottoMain.java
@@ -0,0 +1,15 @@
+import modal.StringAddCalculator;
+import view.LottoInput;
+import view.LottoOutput;
+
+public class LottoMain {
+
+    public static void main(String[] args) {
+        StringAddCalculator stringAddCalculator = new StringAddCalculator();
+        String inputString = LottoInput.inputGameCoin();
+
+        int result = stringAddCalculator.splitAndSum(inputString);
+
+        LottoOutput.printResult(result);
+    }
+}
