8cd308c1df5070bb4fc4228a7c2106a7d30b71b3
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-29 14:45:12 +0900

    Test: add GameResultTest

--- src/main/java/com/cheolhyeonpark/lotto/domain/LottoManager.java
+++ src/main/java/com/cheolhyeonpark/lotto/domain/LottoManager.java
@@ -5,26 +5,24 @@ import java.util.stream.IntStream;
 
 public class LottoManager {
 
-    private final LottoInformation lottoInformation;
+    public static final int GAME_PRICE = 1_000;
+
+    private final LottoNumbersList lottoNumbersList;
     private final AutoGenerator generator;
 
-    public LottoManager(LottoInformation lottoInformation, AutoGenerator generator) {
-        this.lottoInformation = lottoInformation;
+    public LottoManager(LottoNumbersList lottoNumbersList, AutoGenerator generator) {
+        this.lottoNumbersList = lottoNumbersList;
         this.generator = generator;
-        createLottoNumbers();
     }
 
-    private void createLottoNumbers() {
-        IntStream.range(0, lottoInformation.getGameCount())
+    public LottoNumbersList createLottoNumbers(int amount) {
+        IntStream.range(0, amount / GAME_PRICE)
                 .mapToObj(i -> generator.generate())
-                .forEach(lottoInformation::addLottoNumbers);
-    }
-
-    public List<LottoNumbers> getLottoNumbersList() {
-        return lottoInformation.getLottoNumbersList();
+                .forEach(lottoNumbersList::addLottoNumbers);
+        return lottoNumbersList;
     }
 
     public GameResult getGameResult(List<Integer> winningNumbers) {
-        return lottoInformation.getGameResult(winningNumbers);
+        return lottoNumbersList.getGameResult(winningNumbers);
     }
 }
--- src/main/java/com/cheolhyeonpark/lotto/domain/LottoNumbersList.java
+++ src/main/java/com/cheolhyeonpark/lotto/domain/LottoNumbersList.java
@@ -3,20 +3,9 @@ package com.cheolhyeonpark.lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoInformation {
-
-    public static final int GAME_PRICE = 1_000;
+public class LottoNumbersList {
 
     private final List<LottoNumbers> lottoNumbersList = new ArrayList<>();
-    private final int amount;
-
-    public LottoInformation(int amount) {
-        this.amount = amount;
-    }
-
-    public int getGameCount() {
-        return amount / GAME_PRICE;
-    }
 
     public void addLottoNumbers(LottoNumbers lottoNumbers) {
         lottoNumbersList.add(lottoNumbers);
