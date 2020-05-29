9d5425297dbc1790cc91d3c25e7174bf0f8bd12d
gwonsungjun <sungjunpizz@gmail.com>
2020-05-29 16:34:40 +0900

    feat: LottoNumbers equals and hashCode 재정의

--- src/main/java/lotto/domain/number/LottoNumbers.java
+++ src/main/java/lotto/domain/number/LottoNumbers.java
@@ -3,6 +3,7 @@ package lotto.domain.number;
 import lotto.domain.generator.NumberGenerator;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoNumbers {
@@ -23,5 +24,18 @@ public class LottoNumbers {
 
     public List<LottoNumber> getNumbers() {
         return numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LottoNumbers)) return false;
+        LottoNumbers that = (LottoNumbers) o;
+        return Objects.equals(numbers, that.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 }
