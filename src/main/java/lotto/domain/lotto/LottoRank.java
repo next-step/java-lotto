8eb49e99703f9421a3c1c7b366499accca0f0c93
Sungjun <sungjunpizz@gmail.com>
2020-05-29 20:54:39 +0900

    feat: LottoRank 클래스 추가

---
+++ src/main/java/lotto/domain/lotto/LottoRank.java
@@ -0,0 +1,30 @@
+package lotto.domain.lotto;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int countOfMatch;
+    private final int winningMoney;
+
+    private LottoRank(int countOfMatch, int winningMoney) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+
+    public static LottoRank valueOf(int countOfMatch, boolean matchBonus) {
+        return null;
+    }
+}
