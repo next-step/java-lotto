e3f4d796f74ceaf76c52123857df9d18e6136d9d
bluehoped <jhseo1@iteyes.co.kr>
2020-05-30 22:04:36 +0900

    fix 디렉터리 위치 변경

---
+++ src/main/java/step2/util/InputScanner.java
@@ -0,0 +1,22 @@
+package step2.util;
+
+import java.util.Scanner;
+
+public class InputScanner {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private InputScanner() {}
+
+    public static int inputNumber() {
+        return sc.nextInt();
+    }
+
+    public static String inputString() {
+        removeNewLine();
+        return sc.nextLine();
+    }
+
+    private static void removeNewLine() {
+        sc.nextLine();
+    }
+}
