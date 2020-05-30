3b22a7919ce43508a440ac837db8ad180d29bdf1
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-30 15:47:58 +0900

    test: validate whether winningNumber is in between 1 and 45.

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class WinningNumberTest {
+
+  @DisplayName("당첨번호는 무조건 1과 45 사이여야 한다.")
+  @CsvSource({"0", "46", "-99", "103", "-1"})
+  @ParameterizedTest
+  void 당첨번호가_1과_45_사이임이_보장된다(Integer number) {
+    assertThatThrownBy(
+        () -> new WinningNumber(number))
+        .isInstanceOf(RuntimeException.class);
+  }
+}
