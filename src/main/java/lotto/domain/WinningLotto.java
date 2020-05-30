951d36d628c4c94a8a7cefaf217a32a585f63202
junpil park <iamfeel2@gmail.com>
2020-05-31 00:23:01 +0900

    feat (WinningLotto) : 당첨번호 Class 의 생성
    보너스번호와 당첨 번호 6자리를 함께 관리하게 위함

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.Objects;
+
+public class WinningLotto {
+
+    private final Lotto lotto;
+    private final int bonus;
+
+    public WinningLotto(String winnigNumbers, int bonus) {
+        this.lotto = Lotto.valueOf(winnigNumbers);
+        this.bonus = bonus;
+        validateContainLotto(lotto, bonus);
+    }
+
+    private void validateContainLotto(Lotto lotto, int bonus) {
+        if(lotto.getContainCount(Collections.singletonList(bonus)) > 0) {
+            throw new IllegalArgumentException("보너스는 당첨번호와 중복이 되면 안됩니다.");
+        }
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public boolean inContainBonus(Lotto lotto) {
+        return lotto.getContainCount(Collections.singletonList(bonus)) > 0;
+    }
+
+    public int getContainCount(Lotto lotto) {
+        return this.lotto.getContainCount(lotto.getNumbers());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WinningLotto that = (WinningLotto) o;
+        return bonus == that.bonus &&
+                lotto.equals(that.lotto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotto, bonus);
+    }
+}
