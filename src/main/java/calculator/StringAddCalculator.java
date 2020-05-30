0b77f501d06c47d7113e4ca8e5773e3cc45fc394
Choi <Choi@choegwangjun-ui-MacBookPro.local>
2020-05-30 19:02:38 +0900

    test 컴마 구분자 활용 두숫자의 합 반환 테스트

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,16 +2,37 @@ package calculator;
 
 public class StringAddCalculator {
 
+    private static String delimeter = ",";
+
     public static int splitAndSum(String calculationText) {
 
         if (validate(calculationText)) {
             return 0;
         }
-        return Integer.parseInt(calculationText);
+        return calculate(calculationText);
     }
 
     private static boolean validate(String calculationText) {
         return calculationText == null || calculationText.isEmpty();
+    }
+
+    private static int calculate(String calculationText) {
+
+        String[] numbers = calculationTextSplit(calculationText);
+        int calculationNumber = 0;
+
+        if (numbers.length == 1) {
+            return Integer.parseInt(numbers[0]);
+        }
+        for(String number : numbers) {
+            calculationNumber += Integer.parseInt(number);
+        }
+
+        return calculationNumber;
+    }
+
+    private static String[] calculationTextSplit(String calculationText) {
+        return calculationText.split(",");
     }
 
 }
