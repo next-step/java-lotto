401e8897729b31a266898a928e747e3a51dc821f
DongHyukki <kderr2791@gmail.com>
2020-05-30 23:32:16 +0900

    refactor : SplitAndSum method Separated Split, Sum Mehtod
    changes
    - Add Split Constant Value
    - spliAndSum Method Refactoring

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -7,13 +7,24 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String text) {
 
-        if(textNullCheck(text)){
+    private static final String TEXT_SPLIT_REGX = ",|:";
+
+    public static int splitAndSum(String text) {
+        if(checkTextNull(text)){
             return 0;
         }
 
-        String[] tokens = text.split(",|:");
+        String tokens[] = splitText(text);
+
+        checkTokens(tokens);
+
+        return sumSplitedText(tokens);
+    }
+
+    private static String[] splitText(String text) {
+
+        String[] tokens = text.split(TEXT_SPLIT_REGX);
 
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
         if (m.find()) {
@@ -21,20 +32,22 @@ public class StringAddCalculator {
             tokens= m.group(2).split(customDelimiter);
         }
 
-        checkTokens(tokens);
+        return tokens;
+    }
 
+    private static int sumSplitedText(String[] tokens) {
         return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
     }
 
     private static void checkTokens(String[] tokens) {
         Arrays.stream(tokens).forEach( token -> {
             if(Integer.parseInt(token) < 0) {
-                throw new RuntimeException("");
+                throw new IllegalArgumentException("");
             }
         });
     }
 
-    private static boolean textNullCheck(String text) {
+    private static boolean checkTextNull(String text) {
         if (text == null || text.equals("")) {
             return true;
         }
