a59283b32efd34cb94af4ba4de31f5cdc0098101
junpil park <iamfeel2@gmail.com>
2020-05-31 00:21:43 +0900

    feat (LottoRank) : 2등 정보 추가, 이로 인해 보너스 숫자를 입력 받게 됨

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,40 +4,41 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum LottoRank {
-    RANK1(6, BigDecimal.valueOf(2000000000)),
-    RANK2(5, BigDecimal.valueOf(1500000)),
-    RANK3(4, BigDecimal.valueOf(50000)),
-    RANK4(3, BigDecimal.valueOf(5000)),
-    BOOM(0, BigDecimal.ZERO);
+    RANK1(6, false, BigDecimal.valueOf(2000000000)),
+    RANK2(5, true, BigDecimal.valueOf(30000000)),
+    RANK3(5, false,  BigDecimal.valueOf(1500000)),
+    RANK4(4, false,  BigDecimal.valueOf(50000)),
+    RANK5(3, false,  BigDecimal.valueOf(5000)),
+    BOOM(0, false,  BigDecimal.ZERO);
 
-    private int containCount;
-    private BigDecimal winningAmount;
+    private final int containCount;
+    private final BigDecimal winningAmount;
+    private final  boolean isBonus;
 
-    private static final Map<Integer, LottoRank> lottoRanks =
-            Collections.unmodifiableMap(
-                    Arrays.stream(values()).collect(Collectors.toMap(LottoRank::getContainCountsSameWinningNumber, Function.identity())));
+    private static final List<LottoRank> lottoRanks =
+            Collections.unmodifiableList(
+                    Arrays.stream(values()).collect(Collectors.toList()));
 
-    LottoRank(int containCount, BigDecimal amount)
+    LottoRank(int containCount, boolean isBonus, BigDecimal amount)
     {
         this.containCount = containCount;
         this.winningAmount = amount;
+        this.isBonus = isBonus;
     }
 
-    public static LottoRank getRank(int containCount) {
-        LottoRank lottoRank = lottoRanks.get(containCount);
-        if(lottoRank == null) {
-            lottoRank = BOOM;
-        }
-        return lottoRank;
+    public static LottoRank getRank(int containCount, boolean isBonus) {
+        return lottoRanks.stream()
+                .filter(lotto -> lotto.isBonus == isBonus
+                        && lotto.getContainCountsSameWinningNumber() == containCount)
+                .findFirst()
+                .orElse(BOOM);
     }
 
     public static List<LottoRank> getWinningLotto() {
-        return List.of(RANK1, RANK2, RANK3, RANK4);
+        return List.of(RANK1, RANK2, RANK3, RANK4, RANK5);
     }
 
     public int getContainCountsSameWinningNumber() {
@@ -47,4 +48,5 @@ public enum LottoRank {
     public BigDecimal getWinningAmount() {
         return winningAmount;
     }
+
 }
--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -13,22 +13,26 @@ class LottoRankTest {
     @DisplayName("당첨번호의 개수가 포함된 개수에 따라 올바른 등수를 리턴한다.")
     @Test
     void getRankTest() {
-        assertThat(LottoRank.getRank(6)).isEqualTo(LottoRank.RANK1);
-        assertThat(LottoRank.getRank(5)).isEqualTo(LottoRank.RANK2);
-        assertThat(LottoRank.getRank(4)).isEqualTo(LottoRank.RANK3);
-        assertThat(LottoRank.getRank(3)).isEqualTo(LottoRank.RANK4);
-        assertThat(LottoRank.getRank(2)).isEqualTo(LottoRank.BOOM);
-        assertThat(LottoRank.getRank(1)).isEqualTo(LottoRank.BOOM);
-        assertThat(LottoRank.getRank(0)).isEqualTo(LottoRank.BOOM);
+        assertThat(LottoRank.getRank(6, false)).isEqualTo(LottoRank.RANK1);
+        assertThat(LottoRank.getRank(5, true)).isEqualTo(LottoRank.RANK2);
+        assertThat(LottoRank.getRank(5, false)).isEqualTo(LottoRank.RANK3);
+        assertThat(LottoRank.getRank(4, false)).isEqualTo(LottoRank.RANK4);
+        assertThat(LottoRank.getRank(3, false)).isEqualTo(LottoRank.RANK5);
+        assertThat(LottoRank.getRank(2, false)).isEqualTo(LottoRank.BOOM);
+        assertThat(LottoRank.getRank(1, false)).isEqualTo(LottoRank.BOOM);
+        assertThat(LottoRank.getRank(0, true)).isEqualTo(LottoRank.BOOM);
+        assertThat(LottoRank.getRank(0, false)).isEqualTo(LottoRank.BOOM);
+        assertThat(LottoRank.getRank(2, true)).isEqualTo(LottoRank.BOOM);
     }
 
     @DisplayName("등수에 따른 금액을 올바르게 리턴한다.")
     @Test
     void getWinningAmountTest() {
         assertThat(LottoRank.RANK1.getWinningAmount()).isEqualTo(BigDecimal.valueOf(2000000000));
-        assertThat(LottoRank.RANK2.getWinningAmount()).isEqualTo(BigDecimal.valueOf(1500000));
-        assertThat(LottoRank.RANK3.getWinningAmount()).isEqualTo(BigDecimal.valueOf(50000));
-        assertThat(LottoRank.RANK4.getWinningAmount()).isEqualTo(BigDecimal.valueOf(5000));
+        assertThat(LottoRank.RANK2.getWinningAmount()).isEqualTo(BigDecimal.valueOf(30000000));
+        assertThat(LottoRank.RANK3.getWinningAmount()).isEqualTo(BigDecimal.valueOf(1500000));
+        assertThat(LottoRank.RANK4.getWinningAmount()).isEqualTo(BigDecimal.valueOf(50000));
+        assertThat(LottoRank.RANK5.getWinningAmount()).isEqualTo(BigDecimal.valueOf(5000));
         assertThat(LottoRank.BOOM.getWinningAmount()).isEqualTo(BigDecimal.ZERO);
     }
 
@@ -37,6 +41,6 @@ class LottoRankTest {
     void getWinningLottoTest() {
         List<LottoRank> lottoRanks = LottoRank.getWinningLotto();
 
-        assertThat(lottoRanks).containsOnly(LottoRank.RANK1, LottoRank.RANK2, LottoRank.RANK3, LottoRank.RANK4);
+        assertThat(lottoRanks).containsOnly(LottoRank.RANK1, LottoRank.RANK2, LottoRank.RANK3, LottoRank.RANK4, LottoRank.RANK5);
     }
 }
