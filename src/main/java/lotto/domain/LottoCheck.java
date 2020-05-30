50d7c29973974a12822195f9bd87822948b9f4b2
junpil park <iamfeel2@gmail.com>
2020-05-31 00:46:03 +0900

    feat (LottoCheck) : 2등 정보 추가로 인한 변경
    1. getWinningLottoCount() 의 로직 변경
      각 로또 리스트에서 당첨번호 개수를 파악한 뒤 각 당첨 개수에 해당하는 랭크를 찾은 뒤 그 개수를 티런하는 방식에서 각 로또 리스트의 랭크를 모두 찾아 놓고, 해당 랭크를 아 그 당첨개수를 리턴하는 방식으로 변경
    
    2. 메소드 명의 변경

--- src/main/java/lotto/domain/LottoCheck.java
+++ src/main/java/lotto/domain/LottoCheck.java
@@ -1,8 +1,8 @@
 package lotto.domain;
 
 import java.math.BigDecimal;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 import static lotto.domain.LottoRank.*;
 
@@ -10,41 +10,35 @@ public class LottoCheck {
 
     private LottoCheck() {}
 
-    public static int getWinningLottoCount(final List<Lotto> lottos, final Lotto winningLotto, final LottoRank rank) {
-        int rankCount = 0;
-        List<Integer> containWinningNumberCounts = getContainCountsSame(lottos, winningLotto.getNumbers());
-        for(Integer containCount : containWinningNumberCounts) {
-            rankCount = addRankCount(rankCount, containCount, rank);
-        }
-        return rankCount;
+    public static int getWinningLottoCount(final List<Lotto> lottos,
+                                           final WinningLotto winningLotto,
+                                           final LottoRank rank) {
+        List<LottoRank> lottoRanks = getLottoRankEachLotto(lottos, winningLotto);
+        return (int) lottoRanks.stream().filter(lottoRank -> lottoRank == rank).count();
     }
 
-    private static List<Integer> getContainCountsSame(final List<Lotto> lottos, final List<Integer> winningNumbers) {
-        List<Integer> counts = new ArrayList<>();
+    private static List<LottoRank> getLottoRankEachLotto(final List<Lotto> lottos,
+                                                          final WinningLotto winningLotto) {
+        List<LottoRank> lottoRanks = new ArrayList<>();
         for(Lotto lotto : lottos) {
-            counts.add(lotto.getContainCount(winningNumbers));
+            LottoRank lottoRank = LottoRank.getRank(winningLotto.getContainCount(lotto),
+                    winningLotto.inContainBonus(lotto));
+            lottoRanks.add(lottoRank);
         }
-        return counts;
+        return lottoRanks;
     }
 
-    private static int addRankCount(final int currentRankCount, final Integer containCount, final LottoRank rank) {
-        int result = currentRankCount;
-        if(containCount.equals(rank.getContainCountsSameWinningNumber())) {
-            result++;
-        }
-        return result;
+    public static BigDecimal getTotalWinningAmount(final List<Lotto> lottos, final WinningLotto winningLotto) {
+        return getTotalWinningAmount(lottos, winningLotto, RANK1)
+                .add(getTotalWinningAmount(lottos, winningLotto, RANK2))
+                .add(getTotalWinningAmount(lottos, winningLotto, RANK3))
+                .add(getTotalWinningAmount(lottos, winningLotto, RANK4))
+                .add(getTotalWinningAmount(lottos, winningLotto, RANK5));
     }
 
-    public static BigDecimal getWinningAmount(final List<Lotto> lottos, final Lotto winningLotto) {
-        return getWinningAmount(lottos, winningLotto, RANK1)
-                .add(getWinningAmount(lottos, winningLotto, RANK2))
-                .add(getWinningAmount(lottos, winningLotto, RANK3))
-                .add(getWinningAmount(lottos, winningLotto, RANK4));
-    }
-
-    private static BigDecimal getWinningAmount(final List<Lotto> lottos,
-                                               final Lotto winningLotto,
-                                               final LottoRank lottoRank) {
+    private static BigDecimal getTotalWinningAmount(final List<Lotto> lottos,
+                                                    final WinningLotto winningLotto,
+                                                    final LottoRank lottoRank) {
         return BigDecimal.valueOf(getWinningLottoCount(lottos, winningLotto, lottoRank))
                 .multiply(lottoRank.getWinningAmount());
     }
--- src/test/java/lotto/domain/LottoCheckTest.java
+++ src/test/java/lotto/domain/LottoCheckTest.java
@@ -16,30 +16,35 @@ public class LottoCheckTest {
     private static final int INDEX_RANK_2 = 1;
     private static final int INDEX_RANK_3 = 2;
     private static final int INDEX_RANK_4 = 3;
-    private static final int INDEX_BOOM = 4;
+    private static final int INDEX_RANK_5 = 4;
+    private static final int INDEX_BOOM = 5;
 
     @DisplayName("구매한 로또 중에 각 등수에 해당하는 로또 당첨 수 리턴한다.")
     @Test
     public void getWinningLottoCountTest() {
-        Lotto winningLotto = Lotto.valueOf("1,2,3,4,5,6");
+        int bonus = 7;
+        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", bonus);
         List<Lotto> lottos = Arrays.asList(
                 new Lotto(List.of(1,2,3,4,5,8)),
                 new Lotto(List.of(1,2,3,9,10,8)),
                 new Lotto(List.of(7,8,9,12,10,11)),
-                new Lotto(List.of(1,10,3,9,5,8)));
-        final int[] count = new int[5];
+                new Lotto(List.of(1,10,3,9,5,8)),
+                new Lotto(List.of(1,2,3,4,5,7)));
+        final int[] count = new int[6];
 
         count[INDEX_RANK_1] = LottoCheck.getWinningLottoCount(lottos, winningLotto, RANK1);
         count[INDEX_RANK_2] = LottoCheck.getWinningLottoCount(lottos, winningLotto, RANK2);
         count[INDEX_RANK_3] = LottoCheck.getWinningLottoCount(lottos, winningLotto, RANK3);
         count[INDEX_RANK_4] = LottoCheck.getWinningLottoCount(lottos, winningLotto, RANK4);
+        count[INDEX_RANK_5] = LottoCheck.getWinningLottoCount(lottos, winningLotto, RANK5);
         count[INDEX_BOOM] = LottoCheck.getWinningLottoCount(lottos, winningLotto, BOOM);
 
         assertAll(
             () -> assertThat(count[INDEX_RANK_1]).isEqualTo(0),
             () -> assertThat(count[INDEX_RANK_2]).isEqualTo(1),
-            () -> assertThat(count[INDEX_RANK_3]).isEqualTo(0),
-            () -> assertThat(count[INDEX_RANK_4]).isEqualTo(2),
+            () -> assertThat(count[INDEX_RANK_3]).isEqualTo(1),
+            () -> assertThat(count[INDEX_RANK_4]).isEqualTo(0),
+            () -> assertThat(count[INDEX_RANK_5]).isEqualTo(2),
             () -> assertThat(count[INDEX_BOOM]).isEqualTo(1)
         );
     }
@@ -47,15 +52,19 @@ public class LottoCheckTest {
     @DisplayName("총 당첨금액을 리턴한다.")
     @Test
     public void getWinningAmountTest() {
-        Lotto winningLotto = Lotto.valueOf("1,2,3,4,5,6");
+        int bonus = 7;
+        WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", bonus);
         List<Lotto> lottos = Arrays.asList(
-                new Lotto(List.of(1,2,3,4,5,8)),
-                new Lotto(List.of(1,2,3,9,10,8)),
-                new Lotto(List.of(7,8,9,12,10,11)),
-                new Lotto(List.of(1,10,3,9,5,8)));
+                new Lotto(List.of(1,2,3,4,5,8)), // 5개 일치 15
+                new Lotto(List.of(1,2,3,9,10,8)), // 3개 5000
+                new Lotto(List.of(7,8,9,12,10,11)), // 0개
+                new Lotto(List.of(1,10,3,9,5,8)), //3
+                new Lotto(List.of(1,2,3,4,5,7))); // 5 개 보너스
 
-        BigDecimal expectedAmount = BigDecimal.valueOf(2 * 5000).add(BigDecimal.valueOf(1500000));
-        BigDecimal actualAmount = LottoCheck.getWinningAmount(lottos, winningLotto);
+        BigDecimal expectedAmount = BigDecimal.valueOf(2 * 5000)
+                .add(BigDecimal.valueOf(1500000))
+                .add(BigDecimal.valueOf(30000000));
+        BigDecimal actualAmount = LottoCheck.getTotalWinningAmount(lottos, winningLotto);
 
         assertThat(actualAmount).isEqualTo(expectedAmount);
     }
