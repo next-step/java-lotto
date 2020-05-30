ca9933f43a85992d9ae9a643531a34050553eb2d
diane.10 <diane.10@kakaocorp.com>
2020-05-30 18:15:35 +0900

    feat(StringAddCalculator)

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,37 @@
+public class StringAddCalculator {
+    public static int splitAndSum(String text) {
+        if (text == null) {
+            return 0;
+        }
+        if (text.isEmpty()) {
+            return 0;
+        }
+        String[] splitText = StringUtils.splitString(text);
+        int[] intArray = convertToIntArray(splitText);
+        return sumIntArray(intArray);
+    }
+
+    private static int[] convertToIntArray(String[] stringArray) {
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = convertUnsignedInt(stringArray[i]);
+        }
+        return intArray;
+    }
+
+    private static int convertUnsignedInt(String text) {
+        int integer = Integer.parseInt(text);
+        if (integer < 0) {
+            throw new RuntimeException("you input negative Integer : " + text);
+        }
+        return integer;
+    }
+
+    private static int sumIntArray(int[] intArray) {
+        int sum = 0;
+        for (int i = 0; i < intArray.length; i++) {
+            sum += intArray[i];
+        }
+        return sum;
+    }
+}
