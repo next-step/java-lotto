21d31ae4715ec033531e57120c980d820351b979
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-30 11:37:39 +0900

    feat: implement LottoResult type that holds Map that contains MatchType, MatchCount

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class LottoResult {
+
+  private final Map<Integer, Integer> lottoStatistics;
+
+  public LottoResult(Map<Integer, Integer> lottoStatistics) {
+    this.lottoStatistics = lottoStatistics;
+  }
+
+  public Map<Integer, Integer> getLottoStatistics() {
+    return lottoStatistics;
+  }
+}
