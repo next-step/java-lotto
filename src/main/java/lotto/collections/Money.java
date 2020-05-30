0cde06ac9e847a63046d3ff1da26fb8da8a7e6ce
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-30 19:13:27 +0900

    feat: create Money value object

---
+++ src/main/java/lotto/collections/Money.java
@@ -0,0 +1,14 @@
+package lotto.collections;
+
+public class Money {
+
+  private int money;
+
+  public Money(int money) {
+    this.money = money;
+  }
+
+  public int getMoney() {
+    return money;
+  }
+}
