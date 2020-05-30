d45c374e974eae5be7811356c6431cd887492242
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-30 19:14:41 +0900

    feat: counting tickets by reward type

--- src/main/java/lotto/collections/LottoResult.java
+++ src/main/java/lotto/collections/LottoResult.java
@@ -1,6 +1,7 @@
 package lotto.collections;
 
 import java.util.Map;
+import java.util.Optional;
 import lotto.util.LottoProfitCalculator;
 
 public class LottoResult {
@@ -14,6 +15,10 @@ public class LottoResult {
   public double getLottoProfitRate(final int lottoTicketSize) {
     double totalProfit = RewardType.calculateProfit(lottoStatistics);
     return new LottoProfitCalculator(totalProfit, lottoTicketSize).getConvertProfit();
+  }
+
+  public int countTicketsByRewardType(RewardType rewardType) {
+    return Optional.ofNullable(lottoStatistics.get(rewardType.getCode())).orElse(0);
   }
 
   public Map<Integer, Integer> getLottoStatistics() {
