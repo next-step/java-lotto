5a1742114010d57fce0ae3aaee1b135bc5750e8b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-30 11:35:37 +0900

    feat: implement LottoTicket with drawing LottoNumbers and its first collection

---
+++ src/main/java/lotto/collections/LottoTickets.java
@@ -0,0 +1,33 @@
+package lotto.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+
+public class LottoTickets {
+
+  private List<LottoTicket> lottoTickets;
+
+  public LottoTickets(List<LottoTicket> lottoTickets) {
+    this.lottoTickets = lottoTickets;
+  }
+
+  protected LottoTickets() {
+  }
+
+  public int numberOfLottoTickets() {
+    return lottoTickets.size();
+  }
+
+  public LottoResult calculateLottoResult(WinningNumbers numbers) {
+    Map<Integer, Integer> lottoStatistics = lottoTickets.stream()
+        .map(ticket -> ticket.getMatchCounts(numbers))
+        .collect(Collectors
+            .groupingBy(Function.identity(), Collectors.reducing(0, e -> 1, Integer::sum)));
+
+    return new LottoResult(lottoStatistics);
+  }
+}
--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,5 +1,45 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import lotto.collections.WinningNumbers;
+
 public class LottoTicket {
 
+  protected List<LottoNumber> lottoNumbers = new ArrayList<>();
+
+  public LottoTicket() {
+    lottoNumbers = drawAllNumbers();
+    sortDescLottoNumbers();
+  }
+
+  private List<LottoNumber> drawAllNumbers() {
+    final int LOTTO_NUMBER_SIZE = 6;
+    for (int size = 0; size < LOTTO_NUMBER_SIZE; size++) {
+      lottoNumbers.add(new LottoNumber());
+    }
+    return lottoNumbers;
+  }
+
+  public void sortDescLottoNumbers() {
+    lottoNumbers.sort(Comparator.comparing(LottoNumber::getNumber));
+  }
+
+  public int getMatchCounts(WinningNumbers winningNumbers) {
+    return Math.toIntExact(lottoNumbers.stream()
+        .filter(number -> number.getMatchCounts(winningNumbers))
+        .count());
+  }
+
+  public List<LottoNumber> getLottoNumbers() {
+    return lottoNumbers;
+  }
+
+  @Override
+  public String toString() {
+    return "LottoTicket{" +
+        "lottoNumbers=" + lottoNumbers +
+        '}';
+  }
 }
