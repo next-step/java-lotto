b612b481ef6ee03136c3d04434287570e035beaf
JuWanHan <baekseongsa@naver.com>
2020-05-30 17:56:48 +0900

    test(LottoGameTest): 테스트 코드 개선

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -16,44 +16,23 @@ class LottoGameTest {
 
     @BeforeEach
     void setUp() {
-        PurchaseAmount purchaseAmount = PurchaseAmount.newInstance(5000);
-
-        List<String> numbers = Arrays.asList(
-                "1, 2, 3, 13, 14, 15",
-                "1, 2, 3, 4, 14, 15",
-                "1, 2, 3, 4, 5, 15",
-                "1, 2, 3, 4, 5, 7",
-                "1, 2, 3, 4, 5, 6"
+        PurchasePrice purchasePrice = PurchasePrice.newInstance(5000);
+        List<LottoTicket> manualTickets = Arrays.asList(
+                LottoTicketFactory.createManualLottoTicket("1, 2, 3, 13, 14, 15"),
+                LottoTicketFactory.createManualLottoTicket("1, 2, 3, 4, 14, 15"),
+                LottoTicketFactory.createManualLottoTicket("1, 2, 3, 4, 5, 15"),
+                LottoTicketFactory.createManualLottoTicket("1, 2, 3, 4, 5, 7"),
+                LottoTicketFactory.createManualLottoTicket("1, 2, 3, 4, 5, 6")
         );
-        ManualNumbers manualNumbers = ManualNumbers.newInstance(numbers);
 
-        this.lottoGame = new LottoGame(purchaseAmount, manualNumbers);
+        this.lottoGame = new LottoGame(PurchaseInfo.newInstance(purchasePrice, manualTickets));
     }
 
-    @DisplayName("구입금액이 존재하지 않으면 생성할 수 없다.")
+    @DisplayName("구입정보가 존재하지 않으면 생성할 수 없다.")
     @Test
-    void canNotCreateLottoGameIfPurchaseAmountIsNull() {
-        assertThatThrownBy(() -> new LottoGame(null, null))
+    void canNotCreateLottoGameIfPurchaseInfoIsNull() {
+        assertThatThrownBy(() -> new LottoGame(null))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("수동 번호가 존재하지 않으면 생성할 수 없다.")
-    @Test
-    void canNotCreateLottoGameIfManualNumbersIsNull() {
-        assertThatThrownBy(() -> new LottoGame(PurchaseAmount.newInstance(1000), null))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("수동 번호 수가 구매 개수보다 많으면 생성할 수 없다.")
-    @Test
-    void canNotCreateLottoGameIfManualNumbersCountGreaterThanPurchaseCount() {
-        assertThatThrownBy(() -> {
-            List<String> manual = Arrays.asList("1, 2, 3, 13, 14, 15",
-                    "1, 2, 3, 4, 14, 15");
-            ManualNumbers manualNumbers = ManualNumbers.newInstance(manual);
-
-            new LottoGame(PurchaseAmount.newInstance(1000), manualNumbers);
-        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("LottoGame을 생성할 수 있다.")
@@ -62,21 +41,21 @@ class LottoGameTest {
         assertThat(this.lottoGame).isInstanceOf(LottoGame.class);
     }
 
-    @DisplayName("로또 번호를 생성할 수 있다.")
+    @DisplayName("자동 로또 번호를 생성할 수 있다.")
     @Test
     void canCreateLottoNumbers() {
-        LottoNumbers lottoNumbers = this.lottoGame.createLottoNumbers();
+        LottoTickets lottoTickets = this.lottoGame.createAutoLottoTickets();
 
-        assertThat(lottoNumbers).isInstanceOf(LottoNumbers.class);
-        assertThat(lottoNumbers.getCountByCreationType(CreationType.MANUAL)).isEqualTo(5);
+        assertThat(lottoTickets).isInstanceOf(LottoTickets.class);
+        assertThat(lottoTickets.getCountByCreationType(CreationType.MANUAL)).isEqualTo(5);
     }
 
     @DisplayName("로또 당첨 결과를 얻을 수 있다.")
     @Test
     void canCreateLottoMatchResult() {
-        LottoNumber winLottoNumber = LottoNumber.newInstance(Arrays.asList(1, 2, 3, 4, 5, 6));
-        BonusNumber bonusNumber = BonusNumber.newInstance(7, winLottoNumber);
-        LottoMatchResult lottoMatchResult = this.lottoGame.calculateMatchCount(winLottoNumber, bonusNumber);
+        LottoTicket winLottoTicket = LottoTicketFactory.createManualLottoTicket("1, 2, 3, 4, 5, 6");
+        LottoNumber bonusNumber = LottoNumber.newBonusNumber(7, winLottoTicket);
+        LottoMatchResult lottoMatchResult = this.lottoGame.calculateMatchCount(winLottoTicket, bonusNumber);
 
         assertThat(lottoMatchResult).isInstanceOf(LottoMatchResult.class);
     }
