bd8d6a56632f715d0e3f1e5bcabfbdec790bb151
junpil park <iamfeel2@gmail.com>
2020-05-31 00:52:03 +0900

    refactor (WinningLottoTest) : import 문 정리

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,15 +1,10 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
     private final static String DEFAULT_WINNING_NUMBER_TEXT = "1,2,3,4,5,6";
