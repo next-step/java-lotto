1021e68ad7aa2f14dc62e8128e3ada20e77ecc0e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-30 15:49:03 +0900

    feat: throw error if winningNumber not have range in between 1 and 45

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -5,7 +5,14 @@ public class WinningNumber {
   private Integer number;
 
   public WinningNumber(Integer number) {
+    validateNumberInRange(number);
     this.number = number;
+  }
+
+  private void validateNumberInRange(Integer number) {
+    if (number < 1 || number > 45) {
+      throw new RuntimeException("winning number should be in between 1 and 45.");
+    }
   }
 
   public Integer getNumber() {
