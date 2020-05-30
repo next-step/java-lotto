18fb51cb6321cd7b0278ea068c1510e0fa51afc8
JuWanHan <baekseongsa@naver.com>
2020-05-30 17:56:25 +0900

    refactor(LottoGame): 생성자 인자 변경 및 기능 개선

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,69 +1,32 @@
 package lotto.domain;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LottoGame {
 
-    private final ManualNumbers manualNumbers;
-    private final PurchaseAmount purchaseAmount;
-    private LottoNumbers lottoNumbers = LottoNumbers.newInstance();
+    private final PurchaseInfo purchaseInfo;
+    private final LottoTickets lottoTickets = LottoTickets.newInstance();
 
-    public LottoGame(PurchaseAmount purchaseAmount, ManualNumbers manualNumbers) {
-        validatePurchaseAmount(purchaseAmount);
-        validateManualNumbers(manualNumbers, purchaseAmount);
+    public LottoGame(PurchaseInfo purchaseInfo) {
+        validatePurchaseInfo(purchaseInfo);
 
-        this.purchaseAmount = purchaseAmount;
-        this.manualNumbers = manualNumbers;
+        this.purchaseInfo = purchaseInfo;
+        this.lottoTickets.addAll(purchaseInfo.getManualTickets());
     }
 
-    private void validatePurchaseAmount(PurchaseAmount purchaseAmount) {
-        if (purchaseAmount == null) {
-            throw new IllegalArgumentException("구입금액이 존재하지 않습니다.");
+    private void validatePurchaseInfo(PurchaseInfo purchaseInfo) {
+        if (purchaseInfo == null) {
+            throw new IllegalArgumentException("구매 정보가 존재하지 않습니다.");
         }
     }
 
-    private void validateManualNumbers(ManualNumbers manualNumbers, PurchaseAmount purchaseAmount) {
-        if (manualNumbers == null) {
-            throw new IllegalArgumentException("수동 로또 번호가 존재하지 않습니다.");
-        }
-
-        if (manualNumbers.getCount() > purchaseAmount.calculatePurchaseCount()) {
-            throw new IllegalArgumentException("수동 로또 수가 총 로또 구매 수보다 많습니다.");
-        }
+    public LottoTickets createAutoLottoTickets() {
+        int autoTicketCount = this.purchaseInfo.getAutoTicketCount();
+        List<LottoTicket> autoLottoTickets = LottoTicketFactory.createAutoLottoTickets(autoTicketCount);
+        return this.lottoTickets.addAll(autoLottoTickets);
     }
 
-    public LottoNumbers createLottoNumbers() {
-        createManualLottoNumbers();
-        createAutoLottoNumbers();
-
-        return this.lottoNumbers.clone();
-    }
-
-    private void createManualLottoNumbers() {
-        List<LottoNumber> manualLottoNumbers = this.manualNumbers.stream()
-                .map(LottoNumberFactory::createManualLottoNumbers)
-                .collect(Collectors.toList());
-
-        this.lottoNumbers.addAll(manualLottoNumbers);
-    }
-
-    private void createAutoLottoNumbers() {
-        int autoLottoCount = getAutoLottoCount();
-
-        List<LottoNumber> autoLottoNumbers = Stream.generate(LottoNumberFactory::createAutoLottoNumbers)
-                .limit(autoLottoCount)
-                .collect(Collectors.toList());
-
-        this.lottoNumbers.addAll(autoLottoNumbers);
-    }
-
-    private int getAutoLottoCount() {
-        return this.purchaseAmount.calculatePurchaseCount() - this.manualNumbers.getCount();
-    }
-
-    public LottoMatchResult calculateMatchCount(LottoNumber lastWinLottoNumber, BonusNumber bonusNumber) {
-        return this.lottoNumbers.calculateMatchCount(lastWinLottoNumber, bonusNumber);
+    public LottoMatchResult calculateMatchCount(LottoTicket lastWinLottoTicket, LottoNumber bonusNumber) {
+        return this.lottoTickets.calculateMatchCount(lastWinLottoTicket, bonusNumber);
     }
 }
