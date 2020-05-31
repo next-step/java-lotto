2581a28a6b32ec41b2991d1119f384a02e5fac76
Jaehun Lee <ljhokgo@gmail.com>
2020-05-31 22:05:04 +0900

    feat: 결과 출력 기능 추가

---
+++ src/main/java/study/lotto/view/ResultView.java
@@ -0,0 +1,29 @@
+package study.lotto.view;
+
+import study.lotto.model.LottoList;
+import study.lotto.model.LottoRank;
+import study.lotto.model.Statistics;
+
+import java.util.Arrays;
+
+public class ResultView {
+
+    public void printLottoList(LottoList lottoList) {
+        lottoList.getLottoList().forEach(System.out::println);
+    }
+
+    public void printStatistics(Statistics statistics) {
+        System.out.println("당첨 통계");
+        System.out.println("----------");
+
+        Arrays.stream(LottoRank.class.getEnumConstants())
+                .filter(LottoRank::isPrizeExists)
+                .forEach(lottoRank ->
+                    System.out.println(
+                            lottoRank + "- "
+                            + statistics.getStatistics().get(lottoRank) + "개")
+                );
+
+        System.out.println("총 수익률은 " + statistics.getEarningRate() + "입니다.");
+    }
+}
