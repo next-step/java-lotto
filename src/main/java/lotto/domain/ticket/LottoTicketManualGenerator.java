61f83d874f756c9f201ded95254bf417c1220561
chulman <chlcjfals0122@gmail.com>
2020-05-31 16:08:45 +0900

    refactor (LottoTicketManualGenerator) : treeSet tp hashSet

--- src/main/java/lotto/domain/ticket/LottoTicketManualGenerator.java
+++ src/main/java/lotto/domain/ticket/LottoTicketManualGenerator.java
@@ -2,12 +2,12 @@ package lotto.domain.ticket;
 
 import lotto.domain.LottoNumber;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.TreeSet;
 
 public class LottoTicketManualGenerator implements LottoTicketGenerator {
 
     public LottoTicket generate(List<LottoNumber> lottoNumbers) {
-        return LottoTicket.of(new TreeSet<>(lottoNumbers));
+        return LottoTicket.of(new HashSet<>(lottoNumbers));
     }
 }
