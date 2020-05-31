7aaa6b3fdffda7732a974e323a2f453384b0cc15
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-31 18:00:07 +0900

    refactor: move constant to global field
    Co-authored-by: Taegyeoung Oh <otk1090@naver.com>

--- src/main/java/lotto/util/LottoProfitCalculator.java
+++ src/main/java/lotto/util/LottoProfitCalculator.java
@@ -1,12 +1,12 @@
 package lotto.util;
 
 public class LottoProfitCalculator {
+    private static final int TICKET_PRICE = 1000;
+    private static final double SECOND_DECIMAL_POINT = 100.0;
+   
+    private double convertProfit;
 
-  private double convertProfit;
-
-  public LottoProfitCalculator(final double convertProfit, final int lottoTicketSize) {
-    final int TICKET_PRICE = 1000;
-    final double SECOND_DECIMAL_POINT = 100.0;
+    public LottoProfitCalculator(final double convertProfit, final int lottoTicketSize) {
     this.convertProfit =
         Math.round(convertProfit / (lottoTicketSize * TICKET_PRICE) * SECOND_DECIMAL_POINT)
             / SECOND_DECIMAL_POINT;
