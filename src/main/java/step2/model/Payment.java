9d84c6263213980bb994dafd297ffbdbd616087c
yongjin-sixshop <yongjin@sixshop.com>
2020-05-31 19:28:49 +0900

    <refactor> 상수 처리 및 validateMoney 메소드 추가

--- src/main/java/step2/model/Payment.java
+++ src/main/java/step2/model/Payment.java
@@ -3,6 +3,7 @@ package step2.model;
 public class Payment {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int ZERO = 0;
 
     private int money;
 
@@ -11,7 +12,15 @@ public class Payment {
     }
 
     public static Payment of(int money) {
+        validateMoney(money);
+
         return new Payment(money);
+    }
+
+    private static void validateMoney(int money) {
+        if (money <= ZERO) {
+            throw new IllegalArgumentException("금액은 0 이상의 값으로 입력해주세요.");
+        }
     }
 
     public int getCountLottoTicket() {
