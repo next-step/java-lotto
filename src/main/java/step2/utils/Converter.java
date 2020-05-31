743b525b350aa831cbfc3bc6f4095810c6387e05
yongjin-sixshop <yongjin@sixshop.com>
2020-05-31 19:25:39 +0900

    <refactor> 역할에 맞는 class 명으로 변경

--- src/main/java/step2/utils/Converter.java
+++ src/main/java/step2/utils/Converter.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public final class LottoUtil {
+public final class Converter {
 
     private static final String DELIMITER = ",";
 
