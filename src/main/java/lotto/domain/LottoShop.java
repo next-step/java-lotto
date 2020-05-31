926028586c174ae3df43d09253b52ea92ac93c38
chulman <chlcjfals0122@gmail.com>
2020-05-31 17:20:02 +0900

    refactor (LottoShop) : lottoShop 자동 및 수동 구매하도록 변경

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,23 +1,41 @@
 package lotto.domain;
 
 import lotto.domain.ticket.LottoTicket;
+import lotto.domain.ticket.LottoTicketAutoGenerator;
 import lotto.domain.ticket.LottoTicketGenerator;
+import lotto.domain.ticket.LottoTicketManualGenerator;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class LottoShop {
 
-    private LottoTicketGenerator lottoTicketGenerator;
+    private final LottoTicketGenerator lottoTicketManualGenerator;
+    private final LottoTicketGenerator lottoTicketAutoGenerator;
 
-    public LottoShop(LottoTicketGenerator lottoTicketGenerator) {
-        this.lottoTicketGenerator = lottoTicketGenerator;
+    public LottoShop(LottoTicketManualGenerator lottoTicketManualGenerator,
+                     LottoTicketAutoGenerator lottoTicketAutoGenerator) {
+        this.lottoTicketManualGenerator = lottoTicketManualGenerator;
+        this.lottoTicketAutoGenerator = lottoTicketAutoGenerator;
+    }
+    public List<LottoTicket> buy(List<LottoNumbers> lottoNumbersList, int autoBuyAmount) {
+        List<LottoTicket> lottoTickets = buyFromManualGenerator(lottoNumbersList);
+        lottoTickets.addAll(buyFromAutoGenerator(autoBuyAmount));
+        return lottoTickets;
     }
 
-    public List<LottoTicket> buy(int amount) {
+    private List<LottoTicket> buyFromAutoGenerator(int amount) {
         return IntStream.range(0, amount)
-                .mapToObj(integer -> lottoTicketGenerator.generate(LottoNumber.getDefaultLottoNumbers()))
-                .collect(Collectors.toList());
+                .mapToObj(integer -> lottoTicketAutoGenerator.generate(LottoNumbers.getDefaultLottoNumbers()))
+                .collect(toList());
+    }
+
+    public List<LottoTicket> buyFromManualGenerator(List<LottoNumbers> lottoNumbersList) {
+        return lottoNumbersList.stream()
+                .map(LottoNumbers::getLottoNumbers)
+                .map(lottoTicketManualGenerator::generate)
+                .collect(toList());
     }
 }
