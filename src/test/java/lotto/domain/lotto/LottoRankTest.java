8e600f2967f95ec4cf1c04a3ed337d8f9672be50
Sungjun <sungjunpizz@gmail.com>
2020-05-31 22:22:10 +0900

    test: LottoRank 2등 추가로 인한 TC 수정

--- src/test/java/lotto/domain/lotto/LottoRankTest.java
+++ src/test/java/lotto/domain/lotto/LottoRankTest.java
@@ -15,20 +15,21 @@ public class LottoRankTest {
     @DisplayName("일치하는 수를 로또 등수로 변경할 수 있다.")
     @ParameterizedTest
     @MethodSource
-    void valueOf(int countOfMatch, LottoRank expected) {
-        LottoRank lottoRank = LottoRank.valueOf(countOfMatch);
+    void valueOf(int countOfMatch, LottoRank expected, int bonusCountOfMatch) {
+        LottoRank lottoRank = LottoRank.valueOf(countOfMatch, bonusCountOfMatch);
         assertThat(lottoRank).isEqualTo(expected);
     }
 
     private static Stream<Arguments> valueOf() {
         return Stream.of(
-                Arguments.of(0, LottoRank.MISS),
-                Arguments.of(1, LottoRank.MISS),
-                Arguments.of(2, LottoRank.MISS),
-                Arguments.of(3, LottoRank.FOURTH),
-                Arguments.of(4, LottoRank.THIRD),
-                Arguments.of(5, LottoRank.SECOND),
-                Arguments.of(6, LottoRank.FIRST)
+                Arguments.of(0, LottoRank.MISS, 0),
+                Arguments.of(1, LottoRank.MISS, 0),
+                Arguments.of(2, LottoRank.MISS, 0),
+                Arguments.of(3, LottoRank.FIFTH, 0),
+                Arguments.of(4, LottoRank.FOURTH, 0),
+                Arguments.of(5, LottoRank.THIRD, 0),
+                Arguments.of(5, LottoRank.SECOND, 1),
+                Arguments.of(6, LottoRank.FIRST, 0)
         );
     }
 }
