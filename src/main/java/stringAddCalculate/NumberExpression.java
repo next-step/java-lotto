2d8248a70b1fc4e6321c571b6e9155ad60ef13f2
Hyeji <khji1210@naver.com>
2020-05-31 20:39:50 +0900

    Step1: 문자열 덧셈  계산기 (#552)
    * feat: isNullOrEmpty, 숫자하나 구현
    
    * feat: 구분자 기능 구현
    
    * refactor: 구분자 상수 선언
    
    * refactor: add부분을 Stream으로 변환
    
    * refactor: Number Class 추가
    
    * refactor: split 분리
    
    * refactor: splitAndSum -> add rename
    
    * refactor: test code 실행

---
+++ src/main/java/stringAddCalculate/NumberExpression.java
@@ -0,0 +1,24 @@
+package stringAddCalculate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberExpression {
+    private final List<Number> numbers;
+
+    public NumberExpression(final String expression) {
+        this.numbers = convertToNumber(expression);
+    }
+
+    private List<Number> convertToNumber(final String expression) {
+        String[] stringNumbers = StringSplit.split(expression);
+        return Arrays.stream(stringNumbers)
+                .map(value -> new Number(Integer.parseInt(value)))
+                .collect(Collectors.toList());
+    }
+
+    public List<Number> getNumbers() {
+        return numbers;
+    }
+}
