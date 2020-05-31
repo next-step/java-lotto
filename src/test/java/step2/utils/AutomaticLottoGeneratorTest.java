0dc5c828dace8cac17a804babb06e8afcf91172f
yongjin-sixshop <yongjin@sixshop.com>
2020-05-31 20:43:45 +0900

    <test> AutomaticLottoGeneratorTest 에 있던 메소드 WinningLottoTicketTest 로 이동

--- src/test/java/step2/utils/AutomaticLottoGeneratorTest.java
+++ src/test/java/step2/utils/AutomaticLottoGeneratorTest.java
@@ -2,19 +2,11 @@ package step2.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import step2.model.LottoNumber;
 import step2.model.PurchasedLottoTicket;
-import step2.model.WinningLottoTicket;
 
 class AutomaticLottoGeneratorTest {
     @DisplayName("로또 생성 테스트")
@@ -30,21 +22,5 @@ class AutomaticLottoGeneratorTest {
         assertThat(purchasedLottoTicket.getNumbers())
             .hasSize(PurchasedLottoTicket.LOTTO_NUMBER_SIZE)
             .allMatch(number -> number.toInt() >= LottoNumber.LOTTO_MIN_NUMBER && number.toInt() <= LottoNumber.LOTTO_MAX_NUMBER);
-    }
-
-    @DisplayName("로또 생성 오류 테스트")
-    @ParameterizedTest
-    @MethodSource("generateLottoExceptionTestCases")
-    public void generateLottoExceptionTest(List<Integer> numbers) {
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> WinningLottoTicket.createBy(numbers))
-            .withMessageContaining("로또의 수 범위는 1 ~ 45 까지의 입니다.");
-    }
-
-    private static Stream<Arguments> generateLottoExceptionTestCases() {
-        return Stream.of(
-            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 70)),
-            Arguments.of(Arrays.asList(-10, 2, 3, 9, 5, 6))
-        );
     }
 }
