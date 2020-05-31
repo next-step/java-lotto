b9ec38de787c7ab19c6c336cc7a248c976f04b89
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-01 02:12:12 +0900

    refactor : negative 또는 NaN 체크 개선

--- src/main/java/stringaddcalculator/PlusCalculator.java
+++ src/main/java/stringaddcalculator/PlusCalculator.java
@@ -1,34 +1,18 @@
 package stringaddcalculator;
 
-import java.util.regex.Pattern;
-
 public class PlusCalculator implements StringArrayCalculator {
-
-  private final static String MINUS = "-";
-  private final static Pattern NOT_NUMBER = Pattern.compile("\\D");
 
   public int calculate(String[] numberArr) {
     int result = 0;
 
     for (String number : numberArr) {
-      checkIsNegative(number);
-      checkIsNumber(number);
-
-      result += Integer.parseInt(number);
+      try {
+        result += Integer.parseInt(number);
+      } catch (NumberFormatException e) {
+        throw new RuntimeException(e);
+      }
     }
 
     return result;
-  }
-
-  private void checkIsNegative(String s) {
-    if (s.startsWith(MINUS)) {
-      throw new RuntimeException();
-    }
-  }
-
-  private void checkIsNumber(String s) {
-    if (NOT_NUMBER.matcher(s).find()) {
-      throw new RuntimeException();
-    }
   }
 }
