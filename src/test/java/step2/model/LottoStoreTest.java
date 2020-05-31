71cfe32e07b7226b6cc2741f699ed89da9787f89
yongjin-sixshop <yongjin@sixshop.com>
2020-05-31 20:30:58 +0900

    <test> LottoStoreTest 추가 > 로또 판매 테스트 추가

---
+++ src/test/java/step2/model/LottoStoreTest.java
@@ -0,0 +1,27 @@
+package step2.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LottoStoreTest {
+
+    @DisplayName("로또 판매 테스트")
+    @ParameterizedTest
+    @MethodSource("sellTestCases")
+    public void sellTest(Payment payment, Integer expectedLottoTicketsCount) {
+        PurchasedLottoTickets purchasedLottoTickets = LottoStore.sell(payment);
+        assertThat(purchasedLottoTickets.size()).isEqualTo(expectedLottoTicketsCount);
+    }
+
+    private static Stream<Arguments> sellTestCases() {
+        return Stream.of(
+            Arguments.of(Payment.of(10000), 10),
+            Arguments.of(Payment.of(4321), 4),
+            Arguments.of(Payment.of(900), 0));
+    }
+}
