09ae5b4c2cd7f88acd78977e35a82dbd6f85b171
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-31 17:42:30 +0900

    refactor: validate the input of user

--- src/main/java/lotto/collections/Money.java
+++ src/main/java/lotto/collections/Money.java
@@ -5,7 +5,14 @@ public final class Money {
   private final int money;
 
   public Money(int money) {
-    this.money = money;
+    this.money = validateMoneyRange(money);
+  }
+
+  private int validateMoneyRange(int money) {
+    if (money < 1000) {
+      throw new RuntimeException("money input is wrong.");
+    }
+    return money;
   }
 
   public int getNumberOfPurchasedLotto() {
