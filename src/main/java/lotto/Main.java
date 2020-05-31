ed2698ee03fd12d3aae6f76c9d3d10d031a8346a
Sungjun <sungjunpizz@gmail.com>
2020-05-31 14:25:58 +0900

    refactor: LottoGame 클래스 제거

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,13 +1,15 @@
 package lotto;
 
 import lotto.domain.generator.LottoNumberGenerator;
-import lotto.domain.lotto.LottoGame;
+import lotto.domain.lotto.LottoRank;
 import lotto.domain.lotto.LottoTicket;
 import lotto.domain.lotto.Price;
 import lotto.domain.winning.WinningNumbers;
 import lotto.domain.winning.WinningStatistics;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,9 +22,8 @@ public class Main {
 
         String winningNumbers = InputView.printRequireWinningNumbers();
 
-        LottoGame lottoGame = new LottoGame(price, lottoTicket);
-        WinningStatistics winningStatistics = lottoGame.execute(new WinningNumbers(winningNumbers));
+        Map<LottoRank, Long> lottoRank = lottoTicket.matchWinningNumber(new WinningNumbers(winningNumbers));
 
-        OutputView.printStatistics(winningStatistics);
+        OutputView.printStatistics(new WinningStatistics(price, lottoRank));
     }
 }
