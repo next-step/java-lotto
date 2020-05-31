e3261d13437ebc752be98ad9d69ba8e2de4f800d
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-31 17:54:45 +0900

    test: change the logic of LottoNumber to have in between 1 and 45

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,17 +1,19 @@
 package lotto.domain;
 
-import static lotto.domain.LottoNumber.MAXIMUM_NUMBER_BOUND;
-import static lotto.domain.LottoNumber.MINIMUM_NUMBER_BOUND;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoNumberTest {
 
-  @DisplayName("로또 넘버에 1부터 45 사이의 숫자만 들어갈 수 있다.")
-  @Test
-  void 로또_넘버는_1부터_45_사이이다() {
-    assertThat(new LottoNumber().getNumber()).isBetween(MINIMUM_NUMBER_BOUND, MAXIMUM_NUMBER_BOUND);
+  @DisplayName("당첨번호는 무조건 1과 45 사이여야 한다.")
+  @CsvSource({"0", "46", "-99", "103", "-1"})
+  @ParameterizedTest
+  void 당첨번호가_1과_45_사이임이_보장된다(Integer number) {
+    assertThatThrownBy(
+        () -> new LottoNumber(number))
+        .isInstanceOf(RuntimeException.class);
   }
 }
