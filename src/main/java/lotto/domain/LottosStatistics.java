28b14c9e3e153acbf8c7becc7074ba165480f63d
jun-pil park <iamfeel2@gmail.com>
2020-06-01 20:04:47 +0900

    refactor (LottosStatistics) : 구입한 로또들의 로또 등수 별 당첨개수, 총 수익률을 관리하는 LottosStatistics 생성
    (cherry picked from commit 2360e043b7c48d83d807b0dc01986750636f4a76)

---
+++ src/main/java/lotto/domain/LottosStatistics.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class LottosStatistics {
+    private final static int EMPTY = 0;
+
+    private final Map<LottoRank, Integer> countByRank = new HashMap<>();
+    private final BigDecimal rateOfReturnAmount;
+
+    public LottosStatistics(List<Lotto> Lottos, BigDecimal buyPrice, WinningLotto winningLotto) {
+        for (Lotto lotto: Lottos) {
+            addLottoRank(lotto.getWinningRank(winningLotto));
+        }
+        this.rateOfReturnAmount = calculateRateOfReturnAmount(buyPrice);
+    }
+
+    private BigDecimal calculateRateOfReturnAmount(BigDecimal buyAmount) {
+        BigDecimal sumAmount = BigDecimal.ZERO;
+        for(LottoRank lottoRank : LottoRank.getWinningLotto(true)) {
+            sumAmount = sumAmount.add(lottoRank.getWinningAmount().multiply(BigDecimal.valueOf(getWinningCount(lottoRank))));
+        }
+        return sumAmount.divide(buyAmount, 2, RoundingMode.HALF_UP);
+    }
+
+    public int getWinningCount(LottoRank rank) {
+        return Optional.ofNullable(countByRank.get(rank)).orElse(EMPTY);
+    }
+
+    private void addLottoRank(LottoRank lottoRank) {
+        Integer count = Optional.ofNullable(countByRank.get(lottoRank)).orElse(EMPTY);
+        countByRank.put(lottoRank, ++count);
+    }
+
+    public String getRateOfReturnAmount() {
+        return rateOfReturnAmount.toString();
+    }
+}
---
+++ src/test/java/lotto/domain/LottosStatisticsTest.java
@@ -0,0 +1,53 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosStatisticsTest {
+    private final WinningLotto winningLotto = new WinningLotto("1,2,3,4,5,6", 7);
+    private final List<Lotto> Lottos = List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 8)), // 5개 일치, 1500000원
+            new Lotto(List.of(1, 2, 3, 9, 10, 8)), // 3개 일치, 5000원
+            new Lotto(List.of(7, 8, 9, 12, 10, 11)), // 0개 일치,
+            new Lotto(List.of(1, 10, 3, 9, 5, 8)), // 3개 일치, 5000원
+            new Lotto(List.of(1, 2, 3, 4, 5, 7))); // 5개 일치 & 보너스 일치, 30000000원
+
+    private final int buyMoney = Lottos.size() * 1000;
+
+    private final LottosStatistics statistics =
+            new LottosStatistics(Lottos, BigDecimal.valueOf(buyMoney), winningLotto);
+
+    @DisplayName("로또들에 대한 각 당첨된 등수별 개수를 가져올 수 있다.")
+    @Test
+    public void getCountTest() {
+        int countRank1 = statistics.getWinningCount(LottoRank.RANK1);
+        int countRank2 = statistics.getWinningCount(LottoRank.RANK2);
+        int countRank3 = statistics.getWinningCount(LottoRank.RANK3);
+        int countRank4 = statistics.getWinningCount(LottoRank.RANK4);
+        int countRank5 = statistics.getWinningCount(LottoRank.RANK5);
+
+        assertThat(countRank1).isEqualTo(0);
+        assertThat(countRank2).isEqualTo(1);
+        assertThat(countRank3).isEqualTo(1);
+        assertThat(countRank4).isEqualTo(0);
+        assertThat(countRank5).isEqualTo(2);
+    }
+
+    @DisplayName("수익률(총당첨금 / 구입금액) 을 구한다.")
+    @Test
+    void getRateOfReturnAmount() {
+        BigDecimal totalAmount =  BigDecimal.ZERO
+                .add(BigDecimal.valueOf(30000000))
+                .add(BigDecimal.valueOf(5000 * 2))
+                .add(BigDecimal.valueOf(1500000));
+
+        assertThat(statistics.getRateOfReturnAmount())
+                .isEqualTo(totalAmount.divide(BigDecimal.valueOf(buyMoney), 2, RoundingMode.HALF_UP).toString());
+    }
+}
