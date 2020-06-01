d2cf3475db81ce983b6c7ba60e501ace69c45766
jun-pil park <iamfeel2@gmail.com>
2020-06-01 20:11:59 +0900

    refactor (Main) : refactor 사항 반영
    (cherry picked from commit 97550b16580cd4e5cd6f8d013f0a3f61017c9604)

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -5,48 +5,21 @@ import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.math.BigDecimal;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         final int buyPriceAmount = InputView.inputBuyAmount();
         List<Lotto> lottosBought = LottoStore.buy(buyPriceAmount);
-        OutputView.printBuyLottoCount(lottosBought.size());
-        OutputView.printLottos(lottosBought);
-        OutputView.printNewLine();
+        OutputView.printBuyLottos(lottosBought);
 
         final String winningNumber = InputView.inputWinningNumber();
         final int bonus = InputView.inputBonusNumber();
+
         final WinningLotto winningLotto = new WinningLotto(winningNumber, bonus);
-        OutputView.printNewLine();
-
-        printWinningLottoResult(lottosBought, winningLotto, buyPriceAmount);
-    }
-
-    private static void printWinningLottoResult(List<Lotto> lottosBought,
-                                                WinningLotto winningLotto,
-                                                long buyPriceAmount) {
-        OutputView.printWinningLottoTitle();
-        OutputView.printDashBar();
-
-        for(LottoRank lottoRank : getLottoRanksSortAscContainWinningNumber()) {
-            OutputView.printSameCountWinningNumber(lottoRank,
-                    LottoCheck.getWinningLottoCount(lottosBought, winningLotto, lottoRank),
-                    lottoRank.isBonus());
-        }
-
-        OutputView.printReturnValue(LottoCheck.getTotalWinningAmount(lottosBought, winningLotto),
-                BigDecimal.valueOf(buyPriceAmount));
-    }
-
-    private static List<LottoRank> getLottoRanksSortAscContainWinningNumber() {
-       return  LottoRank.getWinningLotto().stream().sorted(new Comparator<LottoRank>() {
-            @Override
-            public int compare(LottoRank o1, LottoRank o2) {
-                return o1.getContainCountsSameWinningNumber() - o2.getContainCountsSameWinningNumber();
-            }
-        }).collect(Collectors.toList());
+        LottosStatistics statistics = new LottosStatistics(lottosBought,
+                BigDecimal.valueOf(buyPriceAmount),
+                winningLotto);
+        OutputView.printLottoStatistics(statistics);
     }
 }
