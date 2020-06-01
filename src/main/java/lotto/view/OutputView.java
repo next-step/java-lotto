d701a3f3616ee6a2c8e064301427b6eca083bf4c
jun-pil park <iamfeel2@gmail.com>
2020-06-01 20:09:38 +0900

    refactor (OutputView) : printf 대신 StringBuilder 사용하도록 변경, 당첨 통계를 한 메소드에서 출력하도록 변경
    (cherry picked from commit 773e3ed2d96dc94fc8597f8396504c0b22beaf16)

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,65 +1,62 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottosStatistics;
 import lotto.domain.LottoRank;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 public class OutputView {
     static class TEXT {
-        public static String BOUGHT_LOTTO_COUNT = "%d개를 구매했습니다.%n";
+        public static String BOUGHT_LOTTO_COUNT = "개를 구매했습니다.";
         public static String TITLE_WINNING_LOTTO_RESULT = "당첨 통계";
         public static String DASH_BAR = "---------------";
-        public static String WINNING_LOTTO_PREFIX_RESULT = "%d개 일치";
-        public static String WINNING_LOTTO_BONUS_RESULT = ", 보너스 볼 일치";
-        public static String WINNING_LOTTO_POSTFIX_RESULT = " (%s원) - %d개%n";
+        public static String RETURN_VALUE_PREFIX = "총 수익률은 ";
+        public static String RETURN_VALUE_POSTFIX = "입니다.";
+        public static String DASH = "-";
+        public static String SPACE = " ";
 
-        public static String TOTAL_RETURN_VALUE = "총 수익률은 %s 입니다.%n";
+        public static String STATISTICS_CONTAIN_WINNING_NUMBER = "개 일치";
+        public static String STATISTICS_CONTAIN_BONUS = ", 보너스 볼 일치";
+        public static String PARENTHESIS_LEFT = "(";
+        public static String PARENTHESIS_RIGHT = ")";
     }
 
-    public static void printWinningLottoTitle() {
-        System.out.println(TEXT.TITLE_WINNING_LOTTO_RESULT);
-    }
-
-    public static void printDashBar() {
-        System.out.println(TEXT.DASH_BAR);
-    }
-
-    public static void printLottos(List<Lotto> lottos) {
+    public static void printBuyLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + TEXT.BOUGHT_LOTTO_COUNT);
         for(Lotto lotto : lottos) {
             System.out.println(lotto);
         }
-    }
-
-    public static void printNewLine() {
         System.out.println();
     }
 
-    public static void printSameCountWinningNumber(LottoRank lottoRank, int sameCount, boolean isBonus) {
-        System.out.printf(createSameCountWinningNumberText(isBonus),
-                lottoRank.getContainCountsSameWinningNumber(),
-                lottoRank.getWinningAmount().toString(),
-                sameCount);
+    private static String getStringRateOfReturnAmount(LottosStatistics LottosStatistics) {
+        return new StringBuilder().append(TEXT.RETURN_VALUE_PREFIX)
+                .append(LottosStatistics.getRateOfReturnAmount())
+                .append(TEXT.RETURN_VALUE_POSTFIX).toString();
     }
 
-    private static String createSameCountWinningNumberText(boolean isBonus) {
-        StringBuilder textForPrint = new StringBuilder();
-        textForPrint.append(TEXT.WINNING_LOTTO_PREFIX_RESULT);
-        if(isBonus) {
-           textForPrint.append(TEXT.WINNING_LOTTO_BONUS_RESULT);
+    private static String getStringWinningLottoResult(LottoRank lottoRank, LottosStatistics LottosStatistics) {
+        return new StringBuilder()
+                .append(lottoRank.getContainCountsSameWinningNumber())
+                .append(TEXT.STATISTICS_CONTAIN_WINNING_NUMBER)
+                .append(lottoRank.isBonus() ? TEXT.STATISTICS_CONTAIN_BONUS : TEXT.SPACE)
+                .append(TEXT.PARENTHESIS_LEFT)
+                .append(lottoRank.getWinningAmount())
+                .append(TEXT.PARENTHESIS_RIGHT)
+                .append(TEXT.SPACE).append(TEXT.DASH).append(TEXT.SPACE)
+                .append(LottosStatistics.getWinningCount(lottoRank)).toString();
+    }
+
+    public static void printLottoStatistics(LottosStatistics LottosStatistics) {
+        System.out.println(TEXT.TITLE_WINNING_LOTTO_RESULT);
+        System.out.println(TEXT.DASH_BAR);
+
+        List<LottoRank> lottoRanks = LottoRank.getWinningLotto(false);
+        for (LottoRank lottoRank : lottoRanks) {
+            System.out.println(getStringWinningLottoResult(lottoRank, LottosStatistics));
         }
-        textForPrint.append(TEXT.WINNING_LOTTO_POSTFIX_RESULT);
-        return textForPrint.toString();
-    }
 
-    public static void printBuyLottoCount(int buyCount) {
-        System.out.printf(TEXT.BOUGHT_LOTTO_COUNT, buyCount);
+        System.out.println(getStringRateOfReturnAmount(LottosStatistics));
     }
-
-    public static void printReturnValue(BigDecimal winningAmount, BigDecimal buyAmount) {
-        System.out.printf(TEXT.TOTAL_RETURN_VALUE, winningAmount.divide(buyAmount, 2, RoundingMode.HALF_UP).toString());
-    }
-
 }
