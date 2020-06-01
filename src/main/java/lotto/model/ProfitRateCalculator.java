545ca45fd980e40791f4ce2a91670cdd50459eee
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-02 03:04:40 +0900

    style : 오타수정

--- src/main/java/lotto/model/ProfitRateCalculator.java
+++ src/main/java/lotto/model/ProfitRateCalculator.java
@@ -3,7 +3,7 @@ package lotto.model;
 public class ProfitRateCalculator {
   private final static int DECIMAL_PLACE_TWO = 100;
 
-  public static double calculateByTwoDeicimalPlaces(int income, int expense) {
+  public static double calculateByTwoDecimalPlaces(int income, int expense) {
     return (double)(DECIMAL_PLACE_TWO * income / expense) / DECIMAL_PLACE_TWO;
   }
 }
