0bf81c3b1523dad1df25ce0f3f1a6a0b4b34f04f
gwonsungjun <sungjunpizz@gmail.com>
2020-06-02 20:16:29 +0900

    feat: 문자열 유틸 클래스 추가
    - 중복 사용되는 로직을 뻄

---
+++ src/main/java/lotto/util/StringUtil.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringUtil {
+    public static List<Integer> splitAndToIntegerList(String inputString) {
+        return Arrays.stream(inputString.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
