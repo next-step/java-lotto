97b0d275cb5ab8f09e99564ef9f64f2b2342719b
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-03 03:03:48 +0900

    refactor : 수익률 계산 개선

--- src/main/java/lotto/model/ProfitRateCalculator.java
+++ src/main/java/lotto/model/ProfitRateCalculator.java
@@ -1,9 +1,11 @@
 package lotto.model;
 
 public class ProfitRateCalculator {
+
   private final static int DECIMAL_PLACE_TWO = 100;
 
   public static double calculateByTwoDecimalPlaces(long income, long expense) {
-    return (double)(DECIMAL_PLACE_TWO * income / expense) / DECIMAL_PLACE_TWO;
+    return (double)
+        ((long) (((double) income / (double) expense) * DECIMAL_PLACE_TWO)) / DECIMAL_PLACE_TWO;
   }
 }
