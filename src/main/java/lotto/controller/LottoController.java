61b4b4ca59d15c9dca32a92410b81cbfd176331b
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-03 00:39:12 +0900

    feat : 메인

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import java.util.Scanner;
+import lotto.model.Lotto;
+import lotto.model.LottoFactory;
+import lotto.model.WinningNumbers;
+import lotto.model.WinningStatistics;
+import lotto.view.LottoView;
+
+public class LottoController {
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    LottoView.printPurchaseRequestMsg();
+
+    Lotto lotto = LottoFactory.createLottoByQuickPick(scanner.nextInt());
+    scanner.nextLine();
+
+    LottoView.printPurchaseDoneMsg(lotto.getLottoList().size());
+
+    LottoView.printLottoNumbers(lotto);
+
+    LottoView.printWinningNumberRequestMsg();
+
+    WinningNumbers winningNumbers = WinningNumbers
+        .newInstanceByStrArr(scanner.nextLine().split(","));
+
+    LottoView.printStatisticsMsg();
+
+    WinningStatistics winningStatistics = WinningStatistics
+        .newInstanceFromLottoAndWinningNumbers(lotto, winningNumbers);
+
+    LottoView.printPrizeTierCnt(winningStatistics.getPrizeTierCntMap());
+
+    LottoView.printProfitRate(winningStatistics.getPrifitRate());
+  }
+}
