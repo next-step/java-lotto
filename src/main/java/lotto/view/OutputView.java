48d2559e64869d6801d585aca5304dadb0f8a8c6
gwonsungjun <sungjunpizz@gmail.com>
2020-06-02 21:19:43 +0900

    refactor: 구매 갯수 수동, 자동으로 나눠 출력 하도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,15 +10,20 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private final static String PRINT_LOTTO_COUNT = "개를 구매했습니다.";
+    private final static String PRINT_LOTTO_COUNT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private final static String PRINT_STATISTICS = "당첨 통계\n---------";
     private static final String PRINT_RANK = "%d개 일치 (%d원)- %d개";
     private static final String PRINT_SECOND_RANK = "%d개 일치, 보너스 볼 일치 (%d원)- %d개";
     private static final String PRINT_PROFIT_RATE = "총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    public static void printPurchasedLottoTicket(LottoTicket lottoTicket) {
-        System.out.println(lottoTicket.getLottoNumbersCount() + PRINT_LOTTO_COUNT);
+    public static void printPurchasedLottoCount(int manualLottoCount, LottoTicket lottoTicket) {
+        int totalLottoCount = lottoTicket.getLottoNumbersCount();
+        int autoLottoCount = Math.max(totalLottoCount - manualLottoCount, 0);
 
+        System.out.println(String.format(PRINT_LOTTO_COUNT, manualLottoCount, autoLottoCount));
+    }
+
+    public static void printPurchasedLottoTicket(LottoTicket lottoTicket) {
         lottoTicket.getLottoNumbers()
                 .stream()
                 .map(numbers -> numbers.getNumbers()
