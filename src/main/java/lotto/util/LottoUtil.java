9a8317ea57d578b48d1a8d2faf94a91e688f01a8
eunji <jeeell.922@gmail.com>
2020-06-02 23:00:23 +0900

    feat 당첨번호를 입력받아 문자열을 숫자로 변환하는 로직 추가

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -1,10 +1,24 @@
 package lotto.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoUtil {
     private static final int LOTTO_PRICE = 1000;
-    private LottoUtil() {}
+
+    private LottoUtil() {
+    }
 
     public static int calculateLottoQuantity(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE;
+    }
+
+    public static List<Integer> convertWinningNumber(String input) {
+        String[] winNumbers = input.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String number : winNumbers) {
+            winningNumbers.add(Integer.valueOf(number.trim()));
+        }
+        return winningNumbers;
     }
 }
--- src/test/java/lotto/util/LottoUtilTest.java
+++ src/test/java/lotto/util/LottoUtilTest.java
@@ -1,16 +1,27 @@
 package lotto.util;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoUtilTest {
     @Test
-    void 구매_장수를_계산한다(){
+    void 구매_장수를_계산한다() {
         int 구매금액 = 14000;
         int 구매장수 = LottoUtil.calculateLottoQuantity(구매금액);
         assertThat(구매장수).isEqualTo(14);
+    }
+
+    @Test
+    void 당첨번호_문자를_숫자리스트로_변환한다(){
+        String input = "1, 2, 3, 4, 5, 6";
+        List<Integer> winningNumbers = LottoUtil.convertWinningNumber(input);
+
+        assertThat(winningNumbers).size().isEqualTo(6);
+        assertThat(winningNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+
     }
 }
