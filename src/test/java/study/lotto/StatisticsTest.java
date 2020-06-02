ffc64752b64bca10ff3c62a74e6fd35cd9f6d6e7
Jaehun Lee <ljhokgo@gmail.com>
2020-06-03 00:07:05 +0900

    test:
    - 2등 당첨 테스트 추가
    - bonusNumber 유효성 검사 테스트 추가

--- src/test/java/study/lotto/StatisticsTest.java
+++ src/test/java/study/lotto/StatisticsTest.java
@@ -8,7 +8,7 @@ import study.lotto.model.WinningLotto;
 
 import java.math.BigDecimal;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StatisticsTest {
 
@@ -20,9 +20,21 @@ public class StatisticsTest {
         String[] split = "1, 2, 3, 4, 5, 6".split(", ");
         WinningLotto winningLotto = WinningLotto.of(split);
         Statistics statistics = new Statistics(lottos);
-        statistics.calculateStatistics(winningLotto);
+        statistics.calculateStatistics(winningLotto, 1);
 
         assertThat(statistics.calculateEarningRate(14000))
                 .isGreaterThanOrEqualTo(BigDecimal.ZERO);
+    }
+
+    @DisplayName("당첨금 통계 계산 - 당첨 번호와 보너스 번호가 중복되어 IllegalArgumentException 발생")
+    @Test
+    void calculate_statistics_with_error() {
+        Lottos lottos = Lottos.of(10);
+
+        String[] split = "1, 2, 3, 4, 5, 6".split(", ");
+        WinningLotto winningLotto = WinningLotto.of(split);
+        Statistics statistics = new Statistics(lottos);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> statistics.calculateStatistics(winningLotto, 1));
     }
 }
