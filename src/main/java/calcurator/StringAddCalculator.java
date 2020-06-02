e5f3ac13aba77c3d4102ebd183e91b977f17f10d
impactrudia <impactyoung@gmail.com>
2020-06-02 12:43:30 +0900

    refactor : 매직넘버 없애고 상수화

--- src/main/java/calcurator/StringAddCalculator.java
+++ src/main/java/calcurator/StringAddCalculator.java
@@ -3,7 +3,10 @@ package calcurator;
 public class StringAddCalculator {
     public static int splitAndSum(String input) {
         if (checkNullOrEmpty(input)) return Const.INITIAL_NUMBER_ZERO;
-        return Integer.parseInt(input);
+        if (input.length() == Const.STRING_LENGTH_NUM_ONE) {
+            return Integer.parseInt(input);
+        }
+        return input.split(Const.SYMBOL_COMMA).length;
     }
 
     private static boolean checkNullOrEmpty(String input) {
