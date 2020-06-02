6c51cbfa557de8d8ef686a1be4f9f6d057f65fdf
rlapwl <khji1210@naver.com>
2020-06-02 21:42:51 +0900

    feat: LottoTest

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,11 +2,21 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
+    private static final int LOTTO_COUNT = 3;
 
     @Test
     void drawingLotto() {
+        Lotto lotto = new Lotto(LOTTO_COUNT);
+
+        lotto.drawingLotto();
+        List<LottoNumber> numberList = lotto.getLottoNumbers();
+
+        long count = numberList.stream().filter(lottoNumber -> lottoNumber.getNumbers().size() == 6).count();
+        assertThat(count).isEqualTo(3);
     }
 }
