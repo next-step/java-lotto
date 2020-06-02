14f1f4adcec8d639f3a34c85060ec0c5de559d83
gwonsungjun <sungjunpizz@gmail.com>
2020-06-02 21:19:02 +0900

    feat: InputView 수동 로또 수, 수동 로또 번호 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,43 @@
 package lotto.view;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
     private final static String PRINT_REQUIRE_PRICE = "구입금액을 입력해 주세요.";
     private final static String PRINT_REQUIRE_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private final static String PRINT_REQUIRE_BONUS = "보너스 볼을 입력해 주세요.";
+    private final static String PRINT_REQUIRE_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private final static String PRINT_REQUIRE_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
 
     public static int printRequirePrice() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(PRINT_REQUIRE_PRICE);
 
         return scanner.nextInt();
+    }
+
+    public static int printRequireManualLottoCount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(PRINT_REQUIRE_MANUAL_LOTTO_COUNT);
+
+        return scanner.nextInt();
+    }
+
+    public static List<String> printRequireManualLottoNumbers(int manualCount) {
+        if (manualCount == 0) {
+            return Collections.emptyList();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(PRINT_REQUIRE_MANUAL_LOTTO_NUMBERS);
+
+        return IntStream.range(0, manualCount)
+                .mapToObj(i -> scanner.next())
+                .collect(Collectors.toList());
     }
 
     public static String printRequireWinningNumbers() {
