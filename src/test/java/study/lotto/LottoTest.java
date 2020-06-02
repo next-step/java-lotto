b49ea435b5ccde8f98aff84c0b0bf4f40557ddd0
Jaehun Lee <ljhokgo@gmail.com>
2020-06-02 22:36:57 +0900

    test: LottoNumber 비교 테스트 추가

--- src/test/java/study/lotto/LottoTest.java
+++ src/test/java/study/lotto/LottoTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import study.lotto.model.Lotto;
 import study.lotto.model.Lottos;
@@ -36,6 +37,14 @@ public class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("로또 번호 비교 테스트")
+    @ParameterizedTest
+    @CsvSource(value = { "1:1", "1:2", "10:20" }, delimiterString = ":")
+    void compare_lotto_number(int a, int b) {
+        assertThat(new LottoNumber(a))
+                .isLessThanOrEqualTo(new LottoNumber(b));
+    }
+
     @DisplayName("로또 생성 테스트")
     @Test
     void generate_lotto() {
@@ -54,7 +63,7 @@ public class LottoTest {
     @ValueSource(strings = { "1, 2, 3, 4, 5, 6", "2, 3, 4, 7, 9, 12" })
     void compare_lotto_to_winning_numbers(String input) {
         List<LottoNumber> lottoNumbers = Stream.of(1, 3, 5, 7, 9, 11).map(LottoNumber::new).collect(Collectors.toList());
-        Lotto lotto = new Lotto(lottoNumbers);
+        Lotto lotto = Lotto.of(lottoNumbers);
 
         String[] split = input.split(", ");
         WinningLotto winningLotto = WinningLotto.of(split);
@@ -66,7 +75,7 @@ public class LottoTest {
     @DisplayName("로또 여러개 동시 생성 테스트")
     @Test
     void create_lotto_list() {
-        Lottos lottos = Lottos.create(4);
+        Lottos lottos = Lottos.of(4);
 
         SoftAssertions softly = new SoftAssertions();
         softly.assertThat(lottos.getLottoList().size()).isEqualTo(4);
