6d9a93eddb5b3edf610a14445bb8a0ab6a268090
Jun <p_illusion@naver.com>
2020-06-04 02:16:43 +0900

    [feat] in/out 뷰 생성

---
+++ src/main/java/step2/view/LottoInput.java
@@ -0,0 +1,29 @@
+package step2.view;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LottoInput {
+    private static final String INPUT_MENT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WIN_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+
+    public static int inputGameMoney() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(INPUT_MENT);
+        int inputMoney = scanner.nextInt();
+
+        return inputMoney;
+    }
+
+    public static String inputWinnerNumbers() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(INPUT_WIN_NUMBER);
+        String inputWinnerNumbers = scanner.nextLine();
+
+        return inputWinnerNumbers;
+    }
+}
---
+++ src/main/java/step2/view/LottoOutput.java
@@ -0,0 +1,40 @@
+package step2.view;
+
+import step2.model.Lotto;
+import step2.model.LottoGame;
+import step2.model.WinnerTier;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LottoOutput {
+
+    private static final String LOTTO_NUMBER_DELEVETER = ",";
+
+    public static void printLottoNumbers(LottoGame lottoGame) {
+
+        List<Lotto> lottos = lottoGame.getLottos();
+
+        for (Lotto lotto : lottos) {
+
+            String lottoNumbers = lotto.getLottonumbers().stream()
+                    .sorted()
+                    .map(i -> i.toString())
+                    .collect(Collectors.joining(LOTTO_NUMBER_DELEVETER));
+
+            System.out.printf("[%s]\n", lottoNumbers);
+        }
+    }
+
+    public static void printPurchaseNumbers(int lottoCount) {
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount);
+    }
+
+    public static void printWinningStatistics(Map<WinnerTier, Integer> WinningResult, double Yield) {
+        for (WinnerTier winnerTier : WinningResult.keySet()) {
+            System.out.printf("%d개 일치 (%d원)- %d개\n", winnerTier.getMatchCnt(), winnerTier.getPrize(), WinningResult.get(winnerTier));
+        }
+        System.out.printf("총 수익률은 %.2f입니다.", Yield);
+    }
+}
