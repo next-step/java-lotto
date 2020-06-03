b4daa0ea0c25d2d072c9ca948ae691671724b4eb
jun-pil park <iamfeel2@gmail.com>
2020-06-04 08:59:41 +0900

    feat (LottoNumberPool) : Pool 에서 여러개의 수를 가져오는 기능, Pool의 모든 수를 가져오는 기능 추가

--- src/main/java/lotto/domain/LottoNumberPool.java
+++ src/main/java/lotto/domain/LottoNumberPool.java
@@ -1,9 +1,6 @@
 package lotto.domain;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,13 +8,31 @@ public class LottoNumberPool {
     private final static Map<Integer, LottoNumber> LOTTO_NUMBER_POOL =
             Collections.unmodifiableMap(
                     IntStream.rangeClosed(LottoNumber.LOTTO_START_NUMBER, LottoNumber.LOTTO_LAST_NUMBER).boxed()
-                        .collect(Collectors.toMap(Integer::new, LottoNumber::of)));
+                        .collect(Collectors.toMap(Integer::intValue, LottoNumber::of)));
+
+    private LottoNumberPool() {}
 
     public static LottoNumber get(int number) {
-        return LOTTO_NUMBER_POOL.get(number);
+        return getNumberAfterValidate(number);
     }
 
     public static List<LottoNumber> get(Integer... numbers) {
-        return Arrays.stream(numbers).map(LottoNumber::of).collect(Collectors.toList());
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (Integer key : numbers) {
+            lottoNumbers.add(getNumberAfterValidate(key));
+        }
+        return lottoNumbers;
+    }
+
+    public static List<LottoNumber> getAll() {
+        return new ArrayList<>(LOTTO_NUMBER_POOL.values());
+    }
+
+    private static LottoNumber getNumberAfterValidate(Integer key) {
+        LottoNumber number = LOTTO_NUMBER_POOL.get(key);
+        if(number == null) {
+            throw new IllegalArgumentException("유효하지 않은 로또 번호 입니다.");
+        }
+        return number;
     }
 }
