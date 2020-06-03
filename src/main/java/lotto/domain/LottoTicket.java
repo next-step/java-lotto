8ffb3f4c478abdb441ae300dc884b9684b7b740d
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-03 12:19:11 +0900

    feat: LottoTicket to check each lottoNumbers to have winningNumber each

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -3,20 +3,12 @@ package lotto.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import lotto.collections.WinningNumbers;
-
 public class LottoTicket {
 
 	protected final List<LottoNumber> lottoNumbers;
 
 	public LottoTicket(List<LottoNumber> lottoNumbers) {
 		this.lottoNumbers = lottoNumbers;
-	}
-
-	public int getMatchCounts(final WinningNumbers winningNumbers) {
-		return Math.toIntExact(lottoNumbers.stream()
-			.filter(number -> number.getMatchCounts(winningNumbers))
-			.count());
 	}
 
 	public List<LottoNumber> getLottoNumbers() {
@@ -30,6 +22,12 @@ public class LottoTicket {
 
 	public boolean contains(LottoNumber lottoNumber) {
 		return lottoNumbers.contains(lottoNumber);
+	}
+
+	public int getMatchCount(LottoNumber winningNumber) {
+		return (int) this.lottoNumbers.stream()
+			.filter(number -> winningNumber.getNumber().equals(number.getNumber()))
+			.count();
 	}
 
 	@Override
