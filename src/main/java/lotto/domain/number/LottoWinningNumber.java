58992ceaf3cc417e5d920326995f74c825dce5d8
Gobukgol <zxczc159@gmail.com>
2020-06-03 15:30:57 +0900

    refactor(LottoWiningNumber): LottoWinningNumber 유효성 검증 로직 추가, Integer 리스트를 LottoNumber 리스트로 변환하는 LottoWinningNumber.convertLottoNumbers 메소드 추가

--- src/main/java/lotto/domain/number/LottoWinningNumber.java
+++ src/main/java/lotto/domain/number/LottoWinningNumber.java
@@ -1,17 +1,20 @@
 package lotto.domain.number;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoWinningNumber {
+    private static final int WINNING_NUMBER_SIZE = 6;
     private final List<LottoNumber> winningNumbers;
     private final LottoNumber bonusNumber;
 
     public LottoWinningNumber(List<Integer> winningNumbers, int bonusNumber) {
+        validateWinningNumbers(winningNumbers);
         validateDuplicateBonusNumber(winningNumbers, bonusNumber);
-        this.winningNumbers = winningNumbers.stream()
-                .map(LottoNumber::valueOf)
-                .collect(Collectors.toList());
+
+        this.winningNumbers = convertLottoNumbers(winningNumbers);
         this.bonusNumber = LottoNumber.valueOf(bonusNumber);
     }
 
@@ -23,9 +26,34 @@ public class LottoWinningNumber {
         return bonusNumber;
     }
 
+    private List<LottoNumber> convertLottoNumbers(List<Integer> winningNumbers) {
+        return winningNumbers.stream()
+                .map(LottoNumber::valueOf)
+                .collect(Collectors.toList());
+    }
+
     private void validateDuplicateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복 될 수 없습니다.");
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복 될 수 없습니다. - " + bonusNumber);
+        }
+    }
+
+    private void validateWinningNumbers(List<Integer> winningNumbers) {
+        validateWinningNumberSize(winningNumbers);
+        validateDuplicateWinningNumbers(winningNumbers);
+    }
+
+    private void validateWinningNumberSize(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != WINNING_NUMBER_SIZE) {
+            throw new IllegalArgumentException("당첨 번호의 갯수가 6이 아닙니다. - " + winningNumbers);
+        }
+    }
+
+    private void validateDuplicateWinningNumbers(List<Integer> winningNumbers) {
+        Set<Integer> nonDupulicateNumbers = new HashSet<>(winningNumbers);
+
+        if (nonDupulicateNumbers.size() != WINNING_NUMBER_SIZE) {
+            throw new IllegalArgumentException("중복되는 당첨 번호가 존재합니다. - " + winningNumbers);
         }
     }
 }
--- src/test/java/lotto/domain/number/LottoWinningNumberTest.java
+++ src/test/java/lotto/domain/number/LottoWinningNumberTest.java
@@ -20,6 +20,28 @@ class LottoWinningNumberTest {
 
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new LottoWinningNumber(winningNumbers, bonusNumber))
-                .withMessage("보너스 번호는 당첨 번호와 중복 될 수 없습니다.");
+                .withMessage("보너스 번호는 당첨 번호와 중복 될 수 없습니다. - " + bonusNumber);
+    }
+
+    @DisplayName("당첨 번호의 갯수가 6개가 아니면 IllegalArgumentException throw")
+    @Test
+    void lessOrMoreNumberThrowException() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        int bonusNumber = 8;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoWinningNumber(winningNumbers, bonusNumber))
+                .withMessage("당첨 번호의 갯수가 6이 아닙니다. - " + winningNumbers);
+    }
+
+    @DisplayName("당첨 번호가 중복되는 값이 있다면 IllegalArgumentException throw")
+    @Test
+    void duplicatedWinningNumberThrowException() {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 3, 4, 5);
+        int bonusNumber = 8;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoWinningNumber(winningNumbers, bonusNumber))
+                .withMessage("중복되는 당첨 번호가 존재합니다. - " + winningNumbers);
     }
 }
