40b49c277f93fda3f8801ec1d229bae0c580e07a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-03 12:18:08 +0900

    feat: WinningService to create LottoResult and counting matches logic

--- src/main/java/lotto/service/WinningService.java
+++ src/main/java/lotto/service/WinningService.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import static java.util.stream.Collectors.*;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,7 +11,9 @@ import lotto.collections.LottoTickets;
 import lotto.collections.Money;
 import lotto.collections.RewardType;
 import lotto.collections.WinningNumbers;
+import lotto.collections.WinningTicket;
 import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
 
 public class WinningService {
 
@@ -21,19 +25,26 @@ public class WinningService {
 		return new WinningNumbers(winningNumberBeforeList);
 	}
 
-	public static LottoResult calculateLottoMatches(final LottoTickets lottoTickets,
-		final WinningNumbers winningNumbers) {
-		return lottoTickets.calculateLottoResult(winningNumbers);
-	}
-
 	public static Money calculateProfit(List<RewardType> lottoStatistics) {
-		int value = Arrays.stream(RewardType.values()).filter(lottoStatistics::contains)
+		int value = Arrays.stream(RewardType.values())
+			.filter(lottoStatistics::contains)
 			.map(type -> type.getReward().getValue() * lottoStatistics.stream()
 				.filter(element -> element.equals(type))
 				.mapToInt(i -> 1)
 				.sum())
 			.reduce(0, Integer::sum);
 		return new Money(value);
+	}
+
+	public LottoResult getLottoResult(final LottoTickets lottoTickets, final WinningTicket winningTicket) {
+		return lottoTickets.getLottoTickets().stream()
+			.map(winningTicket::checkLottoNumbers)
+			.collect(collectingAndThen(toList(), LottoResult::new));
+	}
+
+	public int getMatchCount(WinningNumbers winningNumbers, LottoTicket lottoTicket) {
+		return winningNumbers.getWinningNumbers().stream()
+			.mapToInt(lottoTicket::getMatchCount).sum();
 	}
 
 	public static boolean isBonusBall(final LottoTickets lottoTickets, final LottoNumber bonusBall) {
