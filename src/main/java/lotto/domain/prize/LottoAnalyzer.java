cca5de549db37a77e3896cc7fda785b431a490ce
eunji <jeeell.922@gmail.com>
2020-06-03 11:12:17 +0900

    feat 당첨금, 수익율 계산 로직 추가

--- src/main/java/lotto/domain/prize/LottoAnalyzer.java
+++ src/main/java/lotto/domain/prize/LottoAnalyzer.java
@@ -9,12 +9,28 @@ import java.util.Map;
 public class LottoAnalyzer {
     public Map<Integer, Integer> calculateWinningStatistics(List<Lotto> lottoTicket, List<Integer> winningNumbers) {
         WinningChecker winningChecker = new WinningChecker();
-        Map<Integer, Integer> statistics = new HashMap<>();
+        Map<Integer, Integer> matchCountMap = new HashMap<>();
         for (Lotto lotto : lottoTicket) {
             int winCount = winningChecker.checkWinNumber(lotto.getLottoNumber(), winningNumbers);
-            statistics.put(winCount, statistics.getOrDefault(winCount, 0) + 1);
+            matchCountMap.put(winCount, matchCountMap.getOrDefault(winCount, 0) + 1);
 
         }
-        return statistics;
+        return matchCountMap;
     }
+
+    public int calculateWinPrize(Map<Integer, Integer> matchCountMap) {
+        int winPrize = 0;
+        for(Prize prize : Prize.values()){
+            int matchCount = matchCountMap.get(prize.getMatch());
+            winPrize += prize.calculate(matchCount);
+        }
+        return winPrize;
+    }
+
+    public double calculateWinRate(double purchaseAmount, double winPrize) {
+        double winRate = winPrize / purchaseAmount;
+        winRate = Math.floor(winRate * 100) / 100.0;
+        return winRate;
+    }
+
 }
--- src/test/java/lotto/domain/prize/LottoAnalyzerTest.java
+++ src/test/java/lotto/domain/prize/LottoAnalyzerTest.java
@@ -1,20 +1,53 @@
 package lotto.domain.prize;
 
 import lotto.domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoAnalyzerTest {
+    LottoAnalyzer lottoAnalyzer;
+
+    @BeforeEach
+    void setUp() {
+        lottoAnalyzer = new LottoAnalyzer();
+    }
 
     @Test
     void 일치개수별_당첨수를_확인한다() {
-        LottoAnalyzer lottoAnalyzer = new LottoAnalyzer();
+
         List<Integer> winNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
 
         lottoAnalyzer.calculateWinningStatistics(Arrays.asList(Lotto.create()), winNumber);
         // 어떻게 테스트 코드를 짜야할지..
     }
 
+    @Test
+    void 수익금을_계산한다() {
+        Map<Integer, Integer> matchCountMap = new HashMap<>();
+        matchCountMap.put(3, 5);
+        matchCountMap.put(4, 0);
+        matchCountMap.put(5, 1);
+        matchCountMap.put(6, 0);
+
+        int winPrize = lottoAnalyzer.calculateWinPrize(matchCountMap);
+        assertThat(winPrize).isEqualTo(1_525_000);
+
+    }
+
+    @Test
+    void 수익율을_계산한다() {
+        int purchaseAmount = 14000;
+        int winPrize = 5000;
+
+        double winRate = lottoAnalyzer.calculateWinRate(purchaseAmount, winPrize);
+
+        assertThat(winRate).isEqualTo(0.35);
+    }
 }
