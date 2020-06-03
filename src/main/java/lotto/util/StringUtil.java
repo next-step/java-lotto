77708045b3981a741078c078cabd6eb387751f07
Sungjun <sungjunpizz@gmail.com>
2020-06-03 23:22:48 +0900

    refactor: StringUtil private 생성자 추가

--- src/main/java/lotto/util/StringUtil.java
+++ src/main/java/lotto/util/StringUtil.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringUtil {
+    private StringUtil() {
+    }
+
     public static List<Integer> splitAndToIntegerList(String inputString) {
         return Arrays.stream(inputString.split(","))
                 .map(Integer::parseInt)
