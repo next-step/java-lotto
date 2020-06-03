28c39bee9e88e410f6c93dd844ebf3e7110a479f
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-03 12:27:49 +0900

    refactor: change logic of countingTicket by RewardingType as to filter RewardType List

--- src/main/java/lotto/collections/LottoResult.java
+++ src/main/java/lotto/collections/LottoResult.java
@@ -1,41 +1,30 @@
 package lotto.collections;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import lotto.service.WinningService;
 import lotto.util.LottoProfitCalculator;
 
 public final class LottoResult {
 
-	private Map<Integer, Integer> lottoStatistics;
-	private List<RewardType> lottoStatistics2;
+	private List<RewardType> lottoStatistics;
 
-	public LottoResult(Map<Integer, Integer> lottoStatistics) {
+	public LottoResult(List<RewardType> lottoStatistics) {
 		this.lottoStatistics = lottoStatistics;
 	}
 
-	public LottoResult(List<RewardType> lottoStatistics2) {
-		this.lottoStatistics2 = lottoStatistics2;
-	}
-
 	public double getLottoProfitRate(final int lottoTicketSize) {
-		double totalProfit = WinningService.calculateProfit(lottoStatistics2).getValue();
+		double totalProfit = WinningService.calculateProfit(lottoStatistics).getValue();
 		return LottoProfitCalculator.getConvertProfit(totalProfit, lottoTicketSize);
 	}
 
-	public int countTicketsByRewardType(RewardType rewardType) {
-		return Optional.of(
-			Arrays.stream(RewardType.values())
-				.filter(type -> type.equals(rewardType))
-				.mapToInt(i -> 1)
-				.sum())
-			.orElse(0);
+	public int countTicketsByRewardType(final RewardType rewardType) {
+		return Math.toIntExact(lottoStatistics.stream()
+			.filter(type -> type.equals(rewardType))
+			.count());
 	}
 
-	public Map<Integer, Integer> getLottoStatistics() {
+	public List<RewardType> getLottoStatistics() {
 		return lottoStatistics;
 	}
 }
