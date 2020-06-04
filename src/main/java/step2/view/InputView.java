bb67bdafa771272d88da9f75f6ccf239786a1397
yongjin-sixshop <yongjin@sixshop.com>
2020-06-05 07:21:32 +0900

    <new> 보너스볼 입력 inputBonusLottoNumber 추가

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_PURCHASE_PRICE_TEXT = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_LOTTO_NUMBER_TEXT = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_LOTTO_NUMBER_TEXT = "보너스 볼을 입력해 주세요.";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -17,5 +18,10 @@ public class InputView {
     public static String inputWinningLottoNumber() {
         System.out.println(INPUT_WINNING_LOTTO_NUMBER_TEXT);
         return SCANNER.nextLine();
+    }
+
+    public static int inputBonusLottoNumber() {
+        System.out.println(INPUT_BONUS_LOTTO_NUMBER_TEXT);
+        return Integer.parseInt(SCANNER.nextLine());
     }
 }
