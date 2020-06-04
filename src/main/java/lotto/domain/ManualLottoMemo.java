b1e19001562b9fbc54dfda36db06f8c499c00937
junpil park <iamfeel2@gmail.com>
2020-06-05 01:25:53 +0900

    feat (ManualLottoMemo) : input 에서 받은 String형 로또번호들을 ManualLottoMemo 객체로 Wraping

---
+++ src/main/java/lotto/domain/ManualLottoMemo.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import lotto.util.Split;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManualLottoMemo {
+    private final static String COMMA = ",";
+
+    private final List<List<LottoNumber>> lottoMemos = new ArrayList<>();
+
+    private ManualLottoMemo(List<String> manualLottoStrings) {
+        try {
+            addLottoToMemosFrom(manualLottoStrings);
+        }
+        catch (NullPointerException ignore) {}
+    }
+
+    private void addLottoToMemosFrom(List<String> manualLottoStrings) {
+        for (String lottoNumbers : manualLottoStrings) {
+            lottoMemos.add(LottoNumberPool.get(Split.from(lottoNumbers, COMMA)));
+        }
+    }
+
+    public static ManualLottoMemo of(List<String> manualLottoStrings) {
+        return new ManualLottoMemo(manualLottoStrings);
+    }
+
+    public List<List<LottoNumber>> getLottoMemos() {
+        return lottoMemos;
+    }
+
+    public List<LottoNumber> getLottoMemo(int index) {
+        return lottoMemos.get(index);
+    }
+
+    public int size() {
+        return lottoMemos.size();
+    }
+}
---
+++ src/test/java/lotto/domain/ManualLottoMemoTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ManualLottoMemoTest {
+
+    @DisplayName("로또 String 목록에 대한 Memo 객체가 정상적으로 생성된다.")
+    @Test
+    void ofTest() {
+        ManualLottoMemo manualLottoMemo = ManualLottoMemo.of(List.of("1,2,3,4,5,6", "2,3,4,5,6,7"));
+
+        assertThat(manualLottoMemo).isNotNull();
+        assertThat(manualLottoMemo.size()).isEqualTo(2);
+    }
+
+    @DisplayName("Memo의 기록된 로또번호들이 정상적으로 생성된다.")
+    @Test
+    void getLottoMemosTest() {
+        ManualLottoMemo manualLottoMemo = ManualLottoMemo.of(List.of("1,2,3,4,5,6", "2,3,4,5,6,7"));
+        List<List<LottoNumber>> lottoMemo = manualLottoMemo.getLottoMemos();
+
+        assertThat(lottoMemo.size()).isEqualTo(2);
+        assertThat(lottoMemo).contains(LottoNumberPool.get(1,2,3,4,5,6), LottoNumberPool.get(2,3,4,5,6,7));
+    }
+
+    @DisplayName("입력할 로또번호들이 없어도 메모가 정상적으로 생성된다.")
+    @Test
+    void getLottoMemoNullEmptyTest() {
+        ManualLottoMemo manualLottoMemo = ManualLottoMemo.of(null);
+
+        assertThat(manualLottoMemo).isNotNull();
+        assertThat(manualLottoMemo.size()).isEqualTo(0);
+    }
+
+}
