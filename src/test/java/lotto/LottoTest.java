ae98b37b6bdffd8648d5b567bec852f4f2404199
impactrudia <impactyoung@gmail.com>
2020-06-04 09:32:44 +0900

    test : 문자열 null시 예외처리

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,8 +2,11 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
@@ -11,5 +14,13 @@ class LottoTest {
     public void ofComma() {
         Lotto lotto = Lotto.ofComma("1, 2, 3, 4, 5, 6");
         assertThat(lotto).isEqualTo(Lotto.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("문자열 null시 예외처리")
+    public void ofComma_문자열_isNull(String input) {
+        assertThatThrownBy(() -> Lotto.ofComma(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
