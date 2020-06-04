8a6488d504d5b527e9d978ea0f3d861f8a0ee73d
eunji <jeeell.922@gmail.com>
2020-06-04 22:29:30 +0900

    refactor Scanner 상수 선언

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -3,20 +3,18 @@ package lotto.ui;
 import java.util.Scanner;
 
 public class InputView {
-
+    public static final Scanner SCANNER = new Scanner(System.in);
     private InputView() {}
 
     public static int enterPurchaseAmount() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("구입금액을 입력해 주세요.");
-        int amount = scanner.nextInt();
+        int amount = SCANNER.nextInt();
         return amount;
     }
 
     public static String enterWinningNumber() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        String winningNumber = scanner.nextLine();
+        String winningNumber = SCANNER.nextLine();
         return winningNumber;
     }
 
