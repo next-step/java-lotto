a6d5a42ae2fcd9208f44192f6712b2ca6502b61d
diane.10 <diane.10@kakaocorp.com>
2020-06-04 19:36:53 +0900

    test

--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import utils.StringUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
---
+++ src/test/java/lotto/LottoTicketTest.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class LottoTicketTest {
+    @Test
+    void lottoTicketTest() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(3);
+        lottoNumbers.add(5);
+        lottoNumbers.add(8);
+        lottoNumbers.add(11);
+        lottoNumbers.add(14);
+        lottoNumbers.add(40);
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        List<Integer> winningNumber = lottoNumbers;
+        assertThat(lottoTicket.matchWinningNumber(winningNumber)).isEqualTo(6);
+    }
+
+    @Test
+    void negativeAmountTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    List<Integer> lottoNumbers = new ArrayList<>();
+                    lottoNumbers.add(-3);
+                    lottoNumbers.add(5);
+                    lottoNumbers.add(8);
+                    lottoNumbers.add(-11);
+                    lottoNumbers.add(14);
+                    lottoNumbers.add(40);
+                    LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+                    List<Integer> winningNumber = lottoNumbers;
+                    lottoTicket.matchWinningNumber(winningNumber);
+                });
+    }
+}
