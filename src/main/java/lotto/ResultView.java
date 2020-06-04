cb7fdb268ca61767c372a1e598fb56f024add5a2
impactrudia <impactyoung@gmail.com>
2020-06-05 00:34:45 +0900

    refactor : ResultView 안 쓰는 기능 제거

--- src/main/java/lotto/ResultView.java
+++ src/main/java/lotto/ResultView.java
@@ -1,9 +1,8 @@
 package lotto;
 
 public class ResultView {
-    public static final int WIN_STANDARD = 1;
-    public static final String NEWLINE = System.getProperty("line.separator");
-    public static final String LOTTO_REPORT_HEADER = "당첨 통계" + NEWLINE + "----" + NEWLINE;
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private static final String LOTTO_REPORT_HEADER = "당첨 통계" + NEWLINE + "----" + NEWLINE;
 
     public void printPurchaseTicketCount(int count){
         System.out.println(String.format("%s개를 구매했습니다.", count));
@@ -20,10 +19,6 @@ public class ResultView {
         sb.append(result.getProfit());
         sb.append("%입니다.");
         System.out.println(sb.toString());
-    }
-
-    public String perStatistics(int matchedNumber, int winningAmount, int winningNumber) {
-        return String.format("%d개 일치(%d원)- %d개", matchedNumber, winningAmount, winningNumber);
     }
 
     private static void appendByMatch(StringBuilder sb, MatchingResult matchingResult) {
