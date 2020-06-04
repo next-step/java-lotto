24ccbb27e110bd5847b96968308c94909ea82ca9
bbbnam <bbbnam@naver.com>
2020-06-04 20:39:01 +0900

    refactor : 입력, 결과 뷰 화면 기능 구현에 맞게 리팩토링

--- src/main/java/study/step3/view/InputView.java
+++ src/main/java/study/step3/view/InputView.java
@@ -1,11 +1,14 @@
 package study.step3.view;
 
+import study.step3.dto.LottoInputDto;
+
 import java.util.Scanner;
 
 public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
     private final static String INSERT_MONEY = "구입 금액을 입력해 주세요.";
     private final static String INSERT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요. (쉼표로 구분)";
+    private final static String INSERT_BONUS_NUMBERS = "보너스 볼을 입력해 주세요.";
 
     private InputView(){}
 
@@ -14,7 +17,16 @@ public class InputView {
         return scanner.nextLong();
     }
 
-    public static String inputLastWinningNumbers(){
+    public static LottoInputDto inputLastWinningNumbers(){
+        return new LottoInputDto(inputWinningNumbers(), inputBonusBall());
+    }
+
+    private static int inputBonusBall() {
+        System.out.println(INSERT_BONUS_NUMBERS);
+        return scanner.nextInt();
+    }
+
+    private static String inputWinningNumbers() {
         System.out.println(INSERT_WINNING_NUMBERS);
         return scanner.next();
     }
--- src/main/java/study/step3/view/ResultView.java
+++ src/main/java/study/step3/view/ResultView.java
@@ -9,12 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ResultView {
-    private final static String PURCHASED_LOTTOS_COUNTING = "%d개를 구매했습니다.";
-    private final static String LOTTO_RESULTS = "%d개 일치 (%d원)- %d개";
-    private final static String LOTTO_RESULT_EARNINGS_RATE = "총 수익률은 %.2f입니다.";
-    private final static String EARNINGS_RATE = "earningsRate";
-
-    private ResultView(){}
+    private final static String LOTTO_RESULT_EARNINGS_RATE = "earningsRate";
 
     public static void printLottoTickets(LottoTickets lottoTickets) {
         countTotalLottoTickets(lottoTickets);
@@ -25,14 +20,17 @@ public class ResultView {
     }
 
     private static void countTotalLottoTickets(LottoTickets lottoTickets) {
-        System.out.println(String.format(PURCHASED_LOTTOS_COUNTING,lottoTickets.countLottoTickets()));
+        System.out.println(String.format("%d개를 구매했습니다.",lottoTickets.countLottoTickets()));
     }
 
     public static void printLottoResult(LottoResult lottoResult) {
         Map<String, Object> map = lottoResult.getResult();
         for (WinningRank rank : WinningRank.values()){
-            System.out.println(String.format(LOTTO_RESULTS,rank.getMatchingCount(),rank.getPrizeMoney(),lottoResult.countRank(rank)));
+            if (WinningRank.NOTHING.equals(rank)){
+                continue;
+            }
+            System.out.println(String.format("[%s] %d개 일치 (%d원)- %d개",rank.name(),rank.getMatchingCount(),rank.getPrizeMoney(),lottoResult.countRank(rank)));
         }
-        System.out.println(String.format(LOTTO_RESULT_EARNINGS_RATE,map.get(EARNINGS_RATE)));
+        System.out.println(String.format("총 수익률은 %.2f입니다.",map.get(LOTTO_RESULT_EARNINGS_RATE)));
     }
 }
