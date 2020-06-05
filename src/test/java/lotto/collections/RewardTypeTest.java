f055e96dc80b11a6cebe1b19ca49c60733ae1246
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-05 23:42:05 +0900

    test: move the test logic of counting and multiplying RewardType elements from WinningService to RewardTypeTest

--- src/test/java/lotto/collections/RewardTypeTest.java
+++ src/test/java/lotto/collections/RewardTypeTest.java
@@ -2,9 +2,15 @@ package lotto.collections;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class RewardTypeTest {
 
@@ -14,5 +20,24 @@ public class RewardTypeTest {
 	void 매칭된_카운트에_따라_타입이_결정된다(int matchCount, boolean matchBonus, int rewardValue) {
 		RewardType rewardType = RewardType.findTypeByCount(matchCount, matchBonus);
 		assertThat(rewardType.getReward().getValue()).isEqualTo(rewardValue);
+	}
+
+	private static Stream<Arguments> lottoStatisticsMock() { // argument source method
+		List<RewardType> oneLottoStatistics = new ArrayList<>();
+		oneLottoStatistics.add(RewardType.NOTHING);
+		oneLottoStatistics.add(RewardType.THIRD);
+		oneLottoStatistics.add(RewardType.FOURTH);
+		oneLottoStatistics.add(RewardType.FIFTH);
+
+		return Stream.of(
+			Arguments.of(oneLottoStatistics, 1555000)
+		);
+	}
+
+	@DisplayName("WinningService에서 수행하던 통계 로직을 RewardType으로 성공적으로 이관했는 지 검증한다.")
+	@MethodSource("lottoStatisticsMock")
+	@ParameterizedTest
+	void 로또_통계에_따라_최종_수익을_계산한다(List<RewardType> lottoStatistics, int profit) {
+		assertThat(RewardType.calculateProfit(lottoStatistics)).isEqualTo(profit);
 	}
 }
