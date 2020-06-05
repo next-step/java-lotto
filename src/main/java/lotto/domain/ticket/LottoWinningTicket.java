2f51d60910b39b6b4ab6edbe18f64d2b555dc954
Gobukgol <zxczc159@gmail.com>
2020-06-05 13:46:39 +0900

    refactor(LottoWinningTicket): 사용하지 않는 메소드 제거

--- src/main/java/lotto/domain/ticket/LottoWinningTicket.java
+++ src/main/java/lotto/domain/ticket/LottoWinningTicket.java
@@ -17,14 +17,6 @@ public class LottoWinningTicket {
         this.bonusNumber = LottoNumber.valueOf(bonusNumber);
     }
 
-    public LottoTicket getWinningTicket() {
-        return winningTicket;
-    }
-
-    public LottoNumber getBonusNumber() {
-        return bonusNumber;
-    }
-
     private void validateDuplicateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복 될 수 없습니다. - " + bonusNumber);
