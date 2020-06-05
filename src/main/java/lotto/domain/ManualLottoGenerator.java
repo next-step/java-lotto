25c49a10bf2dcafff390f7732de8ca4a69afcd4b
junpil park <iamfeel2@gmail.com>
2020-06-06 06:55:18 +0900

    feat (ManualLotttoGenerator) : 수동 로또번호 메모로 Lotto를 생성하는 Generator 객체 추가

--- src/main/java/lotto/domain/ManualLottoGenerator.java
+++ src/main/java/lotto/domain/ManualLottoGenerator.java
@@ -3,20 +3,27 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManualLottoGenerator implements LottoGenerator{
+public class ManualLottoGenerator implements LottoGenerator {
 
-    private final ManualLottoMemo manualLottoMemo;
+    private ManualLottoMemo manualLottoMemo;
+    private final List<Lotto> lottos = new ArrayList<>();
 
     public ManualLottoGenerator(ManualLottoMemo manualLottoMemo) {
-        this.manualLottoMemo = manualLottoMemo;
+            this.manualLottoMemo = getManualLottoMemoAfterValidateNullEmpty(manualLottoMemo);
     }
 
     @Override
     public List<Lotto> generator() {
-        List<Lotto> lottos = new ArrayList<>();
         for (List<LottoNumber> lottoNumbers : manualLottoMemo.getLottoMemos()) {
             lottos.add(Lotto.of(lottoNumbers));
         }
         return lottos;
+    }
+
+    public ManualLottoMemo getManualLottoMemoAfterValidateNullEmpty(ManualLottoMemo memo) {
+        if(memo == null) {
+            return ManualLottoMemo.empty();
+        }
+        return memo;
     }
 }
---
+++ src/test/java/lotto/domain/ManualLottoGeneratorTest.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import lotto.util.Split;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ManualLottoGeneratorTest {
+
+    @DisplayName("수동번호 메모대로 lotto 가 생성된다,")
+    @Test
+    public void generatorTest() {
+        String lottoMemo1 = "1,2,3,4,5,6";
+        String lottoMemo2 = "1,2,3,7,5,8";
+        ManualLottoMemo memo = ManualLottoMemo.of(List.of(lottoMemo1, lottoMemo2));
+        ManualLottoGenerator manualLottoGenerator = new ManualLottoGenerator(memo);
+
+        List<Lotto> lottos = manualLottoGenerator.generator();
+        assertThat(lottos.size()).isEqualTo(2);
+        assertThat(lottos).contains(
+                Lotto.of(LottoNumberPool.get(Split.from(lottoMemo1, ","))),
+                Lotto.of(LottoNumberPool.get(Split.from(lottoMemo2, ","))));
+    }
+
+    @DisplayName("수동번호를 메모를 전달하지 않을 경우 빈 List가 전달된다.")
+    @Test
+    public void generatorNullTest() {
+        ManualLottoGenerator manualLottoGenerator = new ManualLottoGenerator(null);
+
+        List<Lotto> lottos = manualLottoGenerator.generator();
+
+        assertThat(lottos.size()).isEqualTo(0);
+        assertThat(lottos).isEmpty();
+    }
+
+    @DisplayName("수동번호가 없는 빈 메모를 전달하면 빈 List<Lotto>가 전달된다.")
+    @Test
+    public void generatorEmptyTest() {
+        ManualLottoGenerator manualLottoGenerator = new ManualLottoGenerator(ManualLottoMemo.empty());
+
+        List<Lotto> lottos = manualLottoGenerator.generator();
+
+        assertThat(lottos.size()).isEqualTo(0);
+        assertThat(lottos).isEmpty();
+    }
+
+}
