1dee02c2d4274b3cebcfb76abd3422c6ad1b9ce6
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-07 00:56:25 +0900

    refactor: receive Money as parameter for calculating the number of purchased lotto

--- src/main/java/lotto/service/PurchaseService.java
+++ src/main/java/lotto/service/PurchaseService.java
@@ -3,10 +3,10 @@ package lotto.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import lotto.collections.LottoTicket;
 import lotto.collections.LottoTickets;
-import lotto.collections.Money;
 import lotto.domain.LottoNumber;
-import lotto.domain.LottoTicket;
+import lotto.domain.Money;
 import lotto.util.AutoLottoNumberGenerator;
 import lotto.util.ManualLottoNumberGenerator;
 
@@ -25,13 +25,16 @@ public class PurchaseService {
 		return new LottoTickets(lottoTicketList);
 	}
 
-	protected static LottoTicket drawUserPickedTicket(List<Integer> pickedValues) {
+	public static LottoTicket drawUserPickedTicket(List<Integer> pickedValues) {
 		List<LottoNumber> lottoNumberList = new ManualLottoNumberGenerator().pickList(pickedValues);
 		return new LottoTicket(lottoNumberList);
 	}
 
-	public static LottoTickets purchaseAutoDrawnTickets(int autoDrawnCount, LottoTickets lottoTickets) {
+	public static LottoTickets purchaseAutoDrawnTickets(Money money, LottoTickets lottoTickets) {
+		final int numberOfPurchasedLotto = money.getNumberOfPurchasedLotto();
 		List<LottoTicket> lottoTicketList = new ArrayList<>(lottoTickets.getLottoTickets());
+		final int userManuallyDrawnCount = lottoTicketList.size();
+		final int autoDrawnCount = numberOfPurchasedLotto - userManuallyDrawnCount;
 
 		for (int index = 0; index < autoDrawnCount; index++) {
 			List<LottoNumber> lottoNumberList = new AutoLottoNumberGenerator().pickList();
