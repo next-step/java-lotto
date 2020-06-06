a65e7d6cb7274d8bcc6a009ff2dbdcaaac3d2651
rlapwl <khji1210@naver.com>
2020-06-06 17:17:48 +0900

    refactor: Lotto 메소드 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int NUMBER_COUNT = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,9 +12,13 @@ public class Lotto {
     }
 
     private void validateNumbers(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (!isSatisfied(numbers.size())) {
             throw new IllegalArgumentException("숫자가 부족합니다");
         }
+    }
+
+    private boolean isSatisfied(int count) {
+        return count == NUMBER_COUNT;
     }
 
     public int matches(List<Integer> winningNumbers) {
