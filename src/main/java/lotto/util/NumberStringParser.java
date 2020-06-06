92ff8af1d3f45cec0b8a428842a5731521337350
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-06 22:00:09 +0900

    feat: add NumberStringParser that split string to integer list

---
+++ src/main/java/lotto/util/NumberStringParser.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NumberStringParser {
+
+	public static List<Integer> NumberStringToIntegerList(String numberValueString) {
+		return Arrays.stream(numberValueString.split(","))
+			.map(String::trim)
+			.map(Integer::valueOf)
+			.collect(Collectors.toList());
+	}
+}
