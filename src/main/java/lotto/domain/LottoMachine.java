241ad32f7143d9e3ba88103b01bbf4b6b9088567
rlapwl <khji1210@naver.com>
2020-06-06 17:06:53 +0900

    refactor: LottoMachine 상수 관리

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -6,11 +6,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoMachine {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
     private static final List<Integer> numbers;
 
     static {
         numbers = new ArrayList<>();
-        for (int idx = 1; idx <= 45; idx++) {
+        for (int idx = LOTTO_MIN_NUMBER; idx <= LOTTO_MAX_NUMBER; idx++) {
             numbers.add(idx);
         }
     }
