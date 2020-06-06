0da9700c92a3245400515a49213f347cb415f5c6
rlapwl <khji1210@naver.com>
2020-06-07 01:25:16 +0900

    refactor: enum으로 바꿈

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,20 +1,34 @@
 package lotto.domain;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
-public class LottoPrize {
-    private static final Map<String, Integer> prizeMap;
+public enum LottoPrize {
+    MATCH_THIRD(3, 5000),
+    MATCH_FOURTH(4, 50000),
+    MATCH_FIFTH(5, 1500000),
+    MATCH_SIXTH(6, 2000000000),
+    MISS(0, 0);
 
-    static {
-        prizeMap = new HashMap<>();
-        prizeMap.put("3", 5000);
-        prizeMap.put("4", 50000);
-        prizeMap.put("5", 1500000);
-        prizeMap.put("6", 2000000000);
+    private final int countOfMatch;
+    private final int prize;
+
+    LottoPrize(int countOfMatch, int prize) {
+        this.countOfMatch =countOfMatch;
+        this.prize = prize;
     }
 
-    public static int getPrize(String count) {
-        return prizeMap.get(count) == null ? 0 : prizeMap.get(count);
+    public static LottoPrize valueOf(int countLotto) {
+        return Arrays.stream(LottoPrize.values())
+                .filter(v -> v.getCountOfMatch() == countLotto)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getPrize() {
+        return prize;
     }
 }
--- src/test/java/lotto/domain/LottoPrizeTest.java
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -2,17 +2,29 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoPrizeTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"3,5000", "4,50000"})
-    @DisplayName("상금 금액을 잘 가져오는지 확인")
-    void getPrize(String count, int expected) {
-        int result = LottoPrize.getPrize(count);
-        assertThat(result).isEqualTo(expected);
+    @MethodSource("provideLottoMatchCount")
+    @DisplayName("맞춘 번호 갯수에 대한 정보 잘 가져오는지 체크")
+    void getPrize(int count, LottoPrize expected) {
+        LottoPrize result = LottoPrize.valueOf(count);
+        assertThat(result).isEqualByComparingTo(expected);
+    }
+
+    private static Stream<Arguments> provideLottoMatchCount() {
+        return Stream.of(
+                Arguments.of(3, LottoPrize.MATCH_THIRD),
+                Arguments.of(4, LottoPrize.MATCH_FOURTH),
+                Arguments.of(2, LottoPrize.MISS),
+                Arguments.of(0, LottoPrize.MISS)
+        );
     }
 }
