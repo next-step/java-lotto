6fe57f73854594fc24e9fd79f0428de7cd8c112f
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-06 21:28:04 +0900

    feat: InputView to have user manually input lotto tickets

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import lotto.collections.Money;
 import lotto.collections.WinningNumbers;
@@ -15,6 +18,19 @@ public class InputView {
 		System.out.println("NextStep 로또게임에 오신 것을 환영합니다!");
 		System.out.println("구매금액을 입력해 주세요.");
 		return new Money(scanner.nextInt());
+	}
+
+	public static int numberOfManuallyDrawnTickets() {
+		System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+		return scanner.nextInt();
+	}
+
+	public static List<Integer> inputManuallyDrawnTickets() {
+		String lottoNumberValue = scanner.nextLine();
+		return Arrays.stream(lottoNumberValue.split(","))
+			.map(String::trim)
+			.map(Integer::valueOf)
+			.collect(Collectors.toList());
 	}
 
 	public static LottoNumber inputBonusBall() {
