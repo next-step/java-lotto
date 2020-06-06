368f9dd58a2bece25be0fe006307c7605e456f58
Jun <p_illusion@naver.com>
2020-06-07 04:34:19 +0900

    [refactor] 1. validate 추가 2. indent 분리를 위한 plusContainNumber 메소드 분리 3. String 파라미터를 받는 convertStringtoNumbers 메소드 추가

--- src/main/java/step2/model/Lotto.java
+++ src/main/java/step2/model/Lotto.java
@@ -6,29 +6,89 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Lotto {
+
+    public final static int MIN = 1;
+    public final static int MAX = 45;
+    public final static int SIZE = 6;
+
+    private final static int PLUS_ONE = 1;
+    private final static int PLUS_ZERO= 0;
+
+    private static final String EXCEPTION_NOT_NUMBER = "숫자만 입력해 주세요";
+    private static final String EXCEPTION_OVER_NUMBER = "총 6개의 숫자를 입력해야 합니다.";
+    private static final String EXCEPTION_SIX_NUMBER = "숫자의 범위는 1 ~ 45 입니다.";
+
+    private static final String DELIMETER = ",";
+
     private List<Integer> lottoNumbers;
 
-    public Lotto(List<Integer> inputLottoNumber) {
-        lottoNumbers = inputLottoNumber;
+    public Lotto(List<Integer> inputLottoNumberList) {
+        if (validateNumbers(inputLottoNumberList)) {
+            lottoNumbers = inputLottoNumberList;
+        }
+    }
+
+    public Lotto(String inputWinnerNumbers) {
+        List<Integer> convertLottoNumber = convertStringtoNumbers(inputWinnerNumbers);
+        if (validateNumbers(convertLottoNumber)) {
+            lottoNumbers = convertLottoNumber;
+        }
     }
 
     public List<Integer> getLottonumbers() {
         return Collections.unmodifiableList(lottoNumbers);
     }
 
-    public int checkWinningCount(List<Integer> winningNumbers) {
+    public int checkWinningCount(Lotto winningLotto) {
         int cnt = 0;
-        for (Integer number : winningNumbers) {
-            cnt = plusContainNumber(number);
+        for (Integer number : winningLotto.getLottonumbers()) {
+            cnt += plusContainNumber(number);
         }
         return cnt;
     }
 
-    public int plusContainNumber(Integer number) {
+    public int plusContainNumber(int number) {
         if (lottoNumbers.contains(number)) {
-            number++;
+            return PLUS_ONE;
         }
 
-        return number;
+        return PLUS_ZERO;
+    }
+
+    public boolean validateNumbers(List<Integer> lottoNumbers) {
+        checkNumber(lottoNumbers);
+        checkSize(lottoNumbers);
+
+        return true;
+    }
+
+    private void checkNumber(List<Integer> lottoNumbers) {
+        lottoNumbers.stream()
+            .forEach(number -> {
+                if (number > MAX || number < MIN) {
+                    throw new IllegalArgumentException(EXCEPTION_SIX_NUMBER);
+                }
+            });
+    }
+
+    private void checkSize(List<Integer> lottoNumbers) {
+        int cnt = (int) lottoNumbers.stream()
+                .distinct()
+                .count();
+
+        if (cnt != SIZE) {
+            throw new IllegalArgumentException(EXCEPTION_OVER_NUMBER);
+        }
+    }
+
+    private List<Integer> convertStringtoNumbers(String inputWinnerNumbers) {
+        try {
+            return Stream.of(inputWinnerNumbers.split(DELIMETER))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(EXCEPTION_NOT_NUMBER, e);
+        }
     }
 }
