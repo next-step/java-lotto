82ed4a92dfea3fe1fdf88830a4861e5ef8946eba
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-07 00:58:07 +0900

    refactor: add business logic to get user input and draw ticket manually

--- src/main/java/lotto/NextStepLottoApplication.java
+++ src/main/java/lotto/NextStepLottoApplication.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lotto.collections.LottoResult;
+import lotto.collections.LottoTicket;
 import lotto.collections.LottoTickets;
-import lotto.collections.Money;
 import lotto.collections.WinningNumbers;
 import lotto.collections.WinningTicket;
 import lotto.domain.LottoNumber;
+import lotto.domain.Money;
 import lotto.service.PurchaseService;
 import lotto.service.WinningService;
 import lotto.view.InputView;
@@ -15,14 +19,25 @@ public class NextStepLottoApplication {
 
 	public static void main(String[] args) {
 		Money money = InputView.inputPurchaseMoney();
-		LottoTickets lottoTickets = PurchaseService.purchaseAllAuto(money);
-		OutputView.printNumberOfPurchasedTickets(lottoTickets);
+		LottoTickets userManualTicket = getUserManualTicket();
+		LottoTickets manualAndAutoTickets = PurchaseService.purchaseAutoDrawnTickets(money, userManualTicket);
+		OutputView.printNumberOfPurchasedTickets(userManualTicket, manualAndAutoTickets);
 
 		WinningNumbers winningNumbers = InputView.inputLastWeekWinningNumbers();
 		LottoNumber bonusBall = InputView.inputBonusBall();
 		WinningTicket winningTicket = new WinningTicket(winningNumbers, bonusBall);
 
-		LottoResult lottoResult = WinningService.getLottoResult(lottoTickets, winningTicket);
-		OutputView.printResult(lottoResult, lottoTickets);
+		LottoResult lottoResult = WinningService.getLottoResult(manualAndAutoTickets, winningTicket);
+		OutputView.printResult(lottoResult, manualAndAutoTickets);
+	}
+
+	private static LottoTickets getUserManualTicket() {
+		int numberOfManualTickets = InputView.inputNumberOfManuallyDrawnTickets();
+		List<LottoTicket> lottoTicketList = new ArrayList<>();
+		for (int index = 0; index < numberOfManualTickets; index++) {
+			List<Integer> pickedValues = InputView.inputManuallyDrawnTickets();
+			lottoTicketList.add(PurchaseService.drawUserPickedTicket(pickedValues));
+		}
+		return new LottoTickets(lottoTicketList);
 	}
 }
