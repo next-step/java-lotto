ebc115e2906904fdb406b8280b009ec56975bc91
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-06-06 22:00:34 +0900

    refactor: remove split logic in InputView class

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,6 @@
 package lotto.view;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import lotto.collections.Money;
 import lotto.collections.WinningNumbers;
@@ -20,17 +17,9 @@ public class InputView {
 		return new Money(scanner.nextInt());
 	}
 
-	public static int numberOfManuallyDrawnTickets() {
+	public static int inputManuallyDrawnTickets() {
 		System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
 		return scanner.nextInt();
-	}
-
-	public static List<Integer> inputManuallyDrawnTickets() {
-		String lottoNumberValue = scanner.nextLine();
-		return Arrays.stream(lottoNumberValue.split(","))
-			.map(String::trim)
-			.map(Integer::valueOf)
-			.collect(Collectors.toList());
 	}
 
 	public static LottoNumber inputBonusBall() {
