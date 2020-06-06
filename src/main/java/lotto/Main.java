bdd0a7854648d9e6725d59b18e1bdca610d9571e
junpil park <iamfeel2@gmail.com>
2020-06-06 14:54:07 +0900

    fix (Main) : 원시값을 포장한 객체들을 이용해 재작성

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,25 +1,30 @@
 package lotto;
 
 import lotto.domain.*;
+import lotto.util.Split;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         final int buyPriceAmount = InputView.inputBuyAmount();
-        List<Lotto> lottosBought = LottoStore.buy(buyPriceAmount);
-        OutputView.printBuyLottos(lottosBought);
+        final int countManualLottoCount = InputView.inputManualLottoCount();
+        final List<String> manualLottos = InputView.inputManualLottos(countManualLottoCount);
+
+        LottoStore lottoStore = new LottoStore();
+        ManualLottoMemo manualLottoMemo =ManualLottoMemo.of(manualLottos);
+
+        List<Lotto> lottosBought = lottoStore.buy(PriceLotto.of(buyPriceAmount), manualLottoMemo);
+        OutputView.printBuyLottos(lottosBought, manualLottoMemo);
 
         final String winningNumber = InputView.inputWinningNumber();
         final int bonus = InputView.inputBonusNumber();
-/*
-        final WinningLotto winningLotto = new WinningLotto(winningNumber, bonus);
-        LottosStatistics statistics = new LottosStatistics(lottosBought,
-                BigDecimal.valueOf(buyPriceAmount),
+
+        final WinningLotto winningLotto = new WinningLotto(LottoNumberPool.get(Split.from(winningNumber, ",")), LottoNumberPool.get(bonus));
+        LottosStatistics statistics = new LottosStatistics(lottosBought, PriceLotto.of(buyPriceAmount),
                 winningLotto);
-        OutputView.printLottoStatistics(statistics);*/
+        OutputView.printLottoStatistics(statistics);
     }
 }
