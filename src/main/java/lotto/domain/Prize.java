238d433579dc548b9472df1609f1e27dac7a7fa0
DongHyukki <kderr2791@gmail.com>
2020-06-07 22:49:16 +0900

    refactor
    Added
    - Prize
    - checkPrize method ( return boolean of Proper Prize )

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,22 +1,25 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public enum Prize {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    MISS(0, 0, false);
 
     private final int matchCount;
     private final int winningPrize;
+    private final boolean matchBonus;
 
-    Prize(int matchCount, int winningPrize) {
+    Prize(int matchCount, int winningPrize, Boolean matchBonus) {
         this.matchCount = matchCount;
         this.winningPrize = winningPrize;
+        this.matchBonus = matchBonus;
     }
 
     public int getMatchCount() {
@@ -27,28 +30,23 @@ public enum Prize {
         return winningPrize;
     }
 
-    public static Prize getMatchPrize(int matchCount) {
-        Prize properPrize = Arrays.stream(Prize.values())
-                .filter(prize -> prize.getMatchCount() == matchCount)
-                .findFirst()
-                .orElseThrow();
-
-        return properPrize;
+    public boolean getMatchBonus() {
+        return matchBonus;
     }
 
-    public static Prize valueOf(int rightCount, boolean matchBonus) {
-        Prize properPrize = Arrays.stream(Prize.values())
-                .filter(prize -> prize.getMatchCount() <= rightCount)
-                .findFirst()
-                .orElseThrow();
-
-        return matchRealPrize(properPrize, matchBonus);
-    }
-
-    private static Prize matchRealPrize(Prize properPrize, boolean matchBonus) {
-        if (properPrize.getMatchCount() == SECOND.getMatchCount() && matchBonus) {
-            return Prize.SECOND;
+    public boolean checkPrize(int matchCount, boolean matchBonus) {
+        if (matchCount == Prize.SECOND.getMatchCount()) {
+            return this.getMatchBonus() == matchBonus;
         }
+        return this.getMatchCount() <= matchCount;
+    }
+
+    public static Prize valueOf(int matchCount, boolean matchBonus) {
+        Prize properPrize = Arrays.stream(Prize.values())
+                .filter(prize -> prize.checkPrize(matchCount, matchBonus))
+                .findFirst()
+                .orElseThrow();
+
         return properPrize;
     }
 
