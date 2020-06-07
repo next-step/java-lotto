cb62b39fe24d74bfb29d198d23e6bc925f3db293
eunji <jeeell.922@gmail.com>
2020-06-08 00:12:33 +0900

    refactor stream 적용 - WinningNumbers convertWinningNumber()
    - 정적 팩토리 메서드 추가
    - 생성 테스트 추가

--- src/main/java/lotto/domain/prize/WinningNumbers.java
+++ src/main/java/lotto/domain/prize/WinningNumbers.java
@@ -3,16 +3,17 @@ package lotto.domain.prize;
 import lotto.domain.lotto.LottoNumbers;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class WinningNumbers {
     public static final int LOTTO_NUMBER_SIZE = 6;
-    public static final String SEPARATOR = ",";
-    public static final int MAX_NUMBER_RANGE = 45;
     public static final int MIN_NUMBER_RANGE = 1;
+    public static final int MAX_NUMBER_RANGE = 45;
+    public static final String SEPARATOR = ",";
 
     private List<Integer> winningNumbers;
 
-    public WinningNumbers(String enteredWinNumber) {
+    private WinningNumbers(String enteredWinNumber) {
         List<Integer> winningNumbers = convertWinningNumber(enteredWinNumber);
         checkSize(winningNumbers);
         checkDuplicate(winningNumbers);
@@ -20,13 +21,22 @@ public class WinningNumbers {
         this.winningNumbers = winningNumbers;
     }
 
-    private List<Integer> convertWinningNumber(String enteredWinNumber) {
-        List<Integer> winningNumbers = new ArrayList<>();
-        String[] enteredWinNumbers = enteredWinNumber.split(SEPARATOR);
-        for (String number : enteredWinNumbers) {
-            winningNumbers.add(Integer.valueOf(number.trim()));
+    public static WinningNumbers create(String enteredWinNumber) {
+        return new WinningNumbers(enteredWinNumber);
+    }
+
+    public int getMatchCount(LottoNumbers lottoNumbers) {
+        int matchCount = 0;
+        for (int number : lottoNumbers.getLottoNumbers()) {
+            matchCount += checkMatch(number);
         }
-        return winningNumbers;
+        return matchCount;
+    }
+
+    private List<Integer> convertWinningNumber(String enteredWinNumber) {
+        String[] enteredWinNumbers = enteredWinNumber.split(SEPARATOR);
+        return Arrays.asList(enteredWinNumbers).stream()
+                .map(x -> Integer.parseInt(x.trim())).collect(Collectors.toList());
     }
 
     private void checkSize(List<Integer> winningNumbers) {
@@ -42,7 +52,6 @@ public class WinningNumbers {
         }
     }
 
-
     private void checkInRange(List<Integer> winningNumbers) {
         int max = Collections.max(winningNumbers);
         int min = Collections.min(winningNumbers);
@@ -50,14 +59,6 @@ public class WinningNumbers {
         if (min < MIN_NUMBER_RANGE || max > MAX_NUMBER_RANGE) {
             throw new IllegalArgumentException("당첨 번호는 1 ~ 45 사이의 수 입니다");
         }
-    }
-
-    public int getMatchCount(LottoNumbers lottoNumbers) {
-        int matchCount = 0;
-        for (int number : lottoNumbers.getLottoNumbers()) {
-            matchCount += checkMatch(number);
-        }
-        return matchCount;
     }
 
     private int checkMatch(int number) {
--- src/test/java/lotto/domain/prize/WinningNumbersTest.java
+++ src/test/java/lotto/domain/prize/WinningNumbersTest.java
@@ -7,47 +7,45 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class WinningNumbersTest {
+
+    @Test
+    void 생성_테스트() {
+        String enteredWinNumber = "1, 2, 3, 4, 5, 6";
+        assertThatCode(() -> WinningNumbers.create(enteredWinNumber)).doesNotThrowAnyException();
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
     void 당첨번호_입력수_확인(String enteredWinNumber) {
-        assertThatThrownBy(() -> new WinningNumbers(enteredWinNumber))
+        assertThatThrownBy(() -> WinningNumbers.create(enteredWinNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 당첨번호_중복입력_확인() {
         String enteredWinNumber = "1, 1, 2, 3, 4, 5";
-
-        assertThatThrownBy(() -> new WinningNumbers(enteredWinNumber))
+        assertThatThrownBy(() -> WinningNumbers.create(enteredWinNumber))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("중복된 당첨 번호가 있습니다");
     }
 
     @Test
     void 당첨번호_입력범위_확인() {
         String enteredWinNumber = "0, 1, 32, 37, 45, 49";
-        assertThatThrownBy(() -> new WinningNumbers(enteredWinNumber))
+        assertThatThrownBy(() -> WinningNumbers.create(enteredWinNumber))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("당첨 번호는 1 ~ 45 사이의 수 입니다");
-
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2, 3, 4, 5, 9, 19 | 0", "1, 7, 23, 32, 33, 34 | 4"
             , "1, 7, 23, 34, 37, 44 | 6"}, delimiter = '|')
     void 당첨번호_일치개수를_확인한다(String enteredWinNumber, int expectedCount) {
-        WinningNumbers winningNumbers = new WinningNumbers(enteredWinNumber);
-        LottoNumbers lottoNumbers = new LottoNumbers() {
-            @Override
-            protected List<Integer> createLottoNumbers() {
-                return Arrays.asList(1, 7, 23, 34, 37, 44);
-            }
-        };
+        WinningNumbers winningNumbers = WinningNumbers.create(enteredWinNumber);
+        LottoNumbers lottoNumbers = LottoNumbers.create(Arrays.asList(1, 7, 23, 34, 37, 44));
+
         int matchCount = winningNumbers.getMatchCount(lottoNumbers);
 
         assertThat(matchCount).isEqualTo(expectedCount);
