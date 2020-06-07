f56432103f260df4fea0ead91f5170e89a92d007
eunji <jeeell.922@gmail.com>
2020-06-07 21:03:09 +0900

    refactor 상수 명 변경, 불필요한 코드 정리

--- src/main/java/lotto/domain/lotto/Numbers.java
+++ src/main/java/lotto/domain/lotto/Numbers.java
@@ -1,36 +1,22 @@
 package lotto.domain.lotto;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Numbers {
-    public static final int START_NUMBER = 1;
-    public static final int END_NUMBER = 46;
+    public static final int RANGE_START = 1;
+    public static final int RANGE_CLOSED = 46;
 
     private final List<Integer> numbers;
 
     public Numbers() {
-        List<Integer> numbers = IntStream.range(START_NUMBER, END_NUMBER)
+        List<Integer> numbers = IntStream.range(RANGE_START, RANGE_CLOSED)
                 .boxed().collect(Collectors.toList());
-        checkNumberRange(numbers);
         this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
         return numbers;
-    }
-
-
-    private void checkNumberRange(List<Integer> numbers) {
-        if (isInRange(numbers)) {
-            throw new IllegalArgumentException("유효 범위 내 값이 아닙니다.");
-        }
-    }
-
-    private boolean isInRange(List<Integer> numbers) {
-        return Collections.max(numbers) < START_NUMBER
-                || Collections.max(numbers) >= END_NUMBER;
     }
 }
