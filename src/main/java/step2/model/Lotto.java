f32c6be96a56070855c4ceacf1d9fd2c6247bfdc
Jun <p_illusion@naver.com>
2020-06-08 00:16:41 +0900

    [refactor] 중복제거와 사이즈 체크 기능 메소드 분리

--- src/main/java/step2/model/Lotto.java
+++ src/main/java/step2/model/Lotto.java
@@ -12,10 +12,11 @@ public class Lotto {
     public final static int SIZE = 6;
 
     private final static int PLUS_ONE = 1;
-    private final static int PLUS_ZERO= 0;
+    private final static int PLUS_ZERO = 0;
 
     private static final String EXCEPTION_NOT_NUMBER = "숫자만 입력해 주세요";
     private static final String EXCEPTION_OVER_NUMBER = "총 6개의 숫자를 입력해야 합니다.";
+    private static final String EXCEPTION_DUPLICATION_NUMBER = "중복되지 않은 6개의 숫자를 입력해야 합니다.";
     private static final String EXCEPTION_SIX_NUMBER = "숫자의 범위는 1 ~ 45 입니다.";
 
     private static final String DELIMETER = ",";
@@ -56,24 +57,31 @@ public class Lotto {
     public void validateNumbers(List<Integer> lottoNumbers) {
         checkNumber(lottoNumbers);
         checkSize(lottoNumbers);
+        countWithoutDuplication(lottoNumbers);
     }
 
     private void checkNumber(List<Integer> lottoNumbers) {
         lottoNumbers.stream()
-            .forEach(number -> {
-                if (number > MAX || number < MIN) {
-                    throw new IllegalArgumentException(EXCEPTION_SIX_NUMBER);
-                }
-            });
+                .forEach(number -> {
+                    if (number > MAX || number < MIN) {
+                        throw new IllegalArgumentException(EXCEPTION_SIX_NUMBER);
+                    }
+                });
     }
 
     private void checkSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != SIZE) {
+            throw new IllegalArgumentException(EXCEPTION_OVER_NUMBER);
+        }
+    }
+
+    private void countWithoutDuplication(List<Integer> lottoNumbers) {
         int cnt = (int) lottoNumbers.stream()
                 .distinct()
                 .count();
 
         if (cnt != SIZE) {
-            throw new IllegalArgumentException(EXCEPTION_OVER_NUMBER);
+            throw new IllegalArgumentException(EXCEPTION_DUPLICATION_NUMBER);
         }
     }
 
