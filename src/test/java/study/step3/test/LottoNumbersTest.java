858a5dbcce618a7c8f96258e2407dae97291c82d
bbbnam <bbbnam@naver.com>
2020-06-07 23:25:30 +0900

    test : 로또넘버 콜렉션 클래스 생성을 위한 테스트

---
+++ src/test/java/study/step3/test/LottoNumbersTest.java
@@ -0,0 +1,16 @@
+package study.step3.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import study.step3.domain.LottoNumbers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoNumbersTest {
+
+    @RepeatedTest(20)
+    @DisplayName("로또 티켓은 6개의 번호로 되어 있는지 테스트")
+    public void lottoNumberCountTest(){
+        assertThat(LottoNumbers.getLottoNumbers()).hasSize(6);
+    }
+}
