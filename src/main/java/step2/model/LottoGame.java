8aa0cb566a3c43939c6fe52c42648e8d87d73db7
Jun <p_illusion@naver.com>
2020-06-08 02:00:34 +0900

    [refactor] 원시값 포장을 위한 LottoMoney 객체 생성

--- src/main/java/step2/model/LottoGame.java
+++ src/main/java/step2/model/LottoGame.java
@@ -7,25 +7,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoGame {
-    private static final int LOTTO_COST = 1000;
 
     private List<Lotto> lottos = new ArrayList<>();
 
-    private int lottoCount;
+    private LottoMoney lottoMoney;
 
-    public LottoGame(int lottoMoney) {
-        lottoCount = lottoMoney / LOTTO_COST;
+    public LottoGame(int inputMoney) {
+        lottoMoney = new LottoMoney(inputMoney);
         createRandomLotto();
     }
 
     private void createRandomLotto() {
-        for (int i = 0; i < lottoCount; i++) {
+        for (int i = 0; i < lottoMoney.getLottoCount(); i++) {
             lottos.add(LottoRandomNumbers.getRandomNumber());
         }
     }
 
     public int getLottoCount() {
-        return lottoCount;
+        return lottoMoney.getLottoCount();
     }
 
     public List<Lotto> getLottos() {
---
+++ src/main/java/step2/model/LottoMoney.java
@@ -0,0 +1,16 @@
+package step2.model;
+
+public class LottoMoney {
+
+    private static final int LOTTO_COST = 1000;
+
+    private int lottoMoney;
+
+    public LottoMoney(int inputMoney) {
+        lottoMoney = inputMoney;
+    }
+
+    public int getLottoCount() {
+        return lottoMoney / LOTTO_COST;
+    }
+}
