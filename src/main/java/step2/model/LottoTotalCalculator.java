72bc76bf322cce0aa577bda7aa9d18965342338b
Jun <p_illusion@naver.com>
2020-06-08 00:41:24 +0900

    [refactor] 잘못된 변수병 수정

--- src/main/java/step2/model/LottoTotalCalculator.java
+++ src/main/java/step2/model/LottoTotalCalculator.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoTotalCalculator {
-    private Map<WinnerTier, Integer> WinningResult = new HashMap<>();
+    private Map<WinnerTier, Integer> winningResult = new HashMap<>();
 
     public Map<WinnerTier, Integer> countWinners(LottoGame lottoGame, Lotto WinnerLotto) {
 
@@ -14,17 +14,17 @@ public class LottoTotalCalculator {
                     .filter(lotto -> lotto.checkWinningCount(WinnerLotto) == winnerTier.getMatchCnt())
                     .count();
 
-            WinningResult.put(winnerTier, (int) matchCount);
+            winningResult.put(winnerTier, (int) matchCount);
         }
 
-        return WinningResult;
+        return winningResult;
     }
 
     public double returnYield(int lottoMoney) {
         double sum = 0;
 
         for (WinnerTier winnerTier : WinnerTier.values()) {
-            sum += winnerTier.getPrize() * WinningResult.get(winnerTier);
+            sum += winnerTier.getPrize() * winningResult.get(winnerTier);
         }
 
         return Math.floor(sum / lottoMoney * 100) / 100;
