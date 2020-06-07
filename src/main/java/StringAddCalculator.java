ec61201a50aa1cfd1ee9aab2869eaa1adad8cf04
y00s22 <ryusuy22@gmail.com>
2020-06-07 15:59:23 +0900

    null 또는 빈문자인 경우

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,6 +1,13 @@
 public class StringAddCalculator {
     public static int splitAndSum(String calcStr) {
-        int result = 0;
+        int result = -99;
+        if(calcStr == null) {
+            return 0;
+        }
+
+        if(calcStr.isEmpty() || calcStr.length() == 0 || calcStr.equals("")) {
+            return 0;
+        }
 
         return result;
     }
