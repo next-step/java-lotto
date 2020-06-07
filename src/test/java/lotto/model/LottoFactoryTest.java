a57abed02c7230a18b26fae27163a3497f42b5be
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-06-08 03:25:39 +0900

    feat : 수동 발급 추가

--- src/test/java/lotto/model/LottoFactoryTest.java
+++ src/test/java/lotto/model/LottoFactoryTest.java
@@ -1,10 +1,14 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoFactoryTest {
 
@@ -18,7 +22,8 @@ class LottoFactoryTest {
       "2123, 2"
   })
   void createLottoByQuickPick(int money, int expected) {
-    Lottos lottos = LottoFactory.createLottosByQuickPick(new Money(money));
+    Lottos lottos = LottoFactory
+        .createLottosByQuickPick((int) (new Money(money).getValue() / Lotto.PRICE.getValue()));
 
     assertThat(lottos.getLottoList().size()).isEqualTo(expected);
   }
@@ -26,7 +31,29 @@ class LottoFactoryTest {
   @Test
   void createLottoByQuickPick_0원이하입력() {
     assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-      LottoFactory.createLottosByQuickPick(new Money(-1000));
+      LottoFactory
+          .createLottosByQuickPick((int) (new Money(-1000).getValue() / Lotto.PRICE.getValue()));
     });
+  }
+
+  @ParameterizedTest
+  @MethodSource("quantityWithNumberStrArrProvider")
+  void createLottoByStrArr(int quantity, String[] numberStrArr) {
+    Lottos lottos = LottoFactory.createLottosByManual(numberStrArr);
+
+    assertThat(lottos.getLottoList().size()).isEqualTo(quantity);
+  }
+
+  static Stream<Arguments> quantityWithNumberStrArrProvider() {
+    return Stream.of(
+        arguments(
+            3,
+            new String[]{
+                "1, 2, 3, 4, 5, 6",
+                "1, 2, 3, 4, 5, 7",
+                "1, 2, 3, 4, 5, 8"
+            }
+        )
+    );
   }
 }
