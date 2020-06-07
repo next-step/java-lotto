075fb5d23ab18f4a995398bded0d94ca0fdee8f8
Jun <p_illusion@naver.com>
2020-06-08 00:40:39 +0900

    [refactor] 사용하지 않는 상수 제거

--- src/main/java/step2/model/LottoTotalCalculator.java
+++ src/main/java/step2/model/LottoTotalCalculator.java
@@ -5,13 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoTotalCalculator {
-    private final String EXCEPTION_NOTSIX_NUMBER = "당첨 숫자는 중복되지 않은 6개만 입력 가능합니다.";
-    private final String EXCEPTION_OVER_NUMBER = "당첨 숫자는 1~45만 가능합니다.";
-
-    private final static int MIN = 1;
-    private final static int MAX = 45;
-    private final static int SIZE = 6;
-
     private Map<WinnerTier, Integer> WinningResult = new HashMap<>();
 
     public Map<WinnerTier, Integer> countWinners(LottoGame lottoGame, Lotto WinnerLotto) {
