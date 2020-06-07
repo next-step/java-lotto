cc0ef1b81fccafa9b03f86129e4e88b14c9661aa
y00s22 <ryusuy22@gmail.com>
2020-06-07 17:59:55 +0900

    custom 구분자 프로덕션 코드 리펙토링: 구분자 유무 구하는 메소드 생성, 구분자에 따라 문자열 split()하는 메소드 생성, 빈 문자열에 대한 메소드 작성

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -3,28 +3,60 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     public static int splitAndSum(String calcStr) {
-        int result = 0;
-
-        if (calcStr == null || calcStr.isEmpty()) {
+        if (isBlank(calcStr)) {
             return 0;
         }
 
+        if (!isContainDelimiter(calcStr)) {
+            return Integer.parseInt(calcStr);
+        }
+
+        return splitStr(calcStr);
+    }
+
+    private static int splitStr(String calcStr) {
+        if (isDefaultDelimiter(calcStr)) {
+            String[] numbers = calcStr.split(",|:");
+            return sum(numbers);
+        }
+
+        return splitWithCustomDelimiter(calcStr);
+    }
+
+    private static int splitWithCustomDelimiter(String calcStr) {
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(calcStr);
+
         if (m.find()) {
             String customDelimiter = m.group(1);
-            String[] tokens= m.group(2).split(customDelimiter);
-
-            for (String number: tokens) {
-                result += Integer.parseInt(number);
-            }
-        }
-        else {
-            String[] numbers = calcStr.split(",|:");
-            for (String number: numbers) {
-                result += Integer.parseInt(number);
-            }
+            String[] tokens = m.group(2).split(customDelimiter);
+            return sum(tokens);
         }
 
-        return result;
+        return 0;
+    }
+
+    private static int sum(String[] numbers) {
+        int sum = 0;
+        for (String number: numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
+    }
+
+    private static boolean isContainDelimiter(String calcStr) {
+        return isDefaultDelimiter(calcStr) || isCustomDelimiter(calcStr);
+    }
+
+    private static boolean isDefaultDelimiter(String calcStr) {
+        return calcStr.contains(",") || calcStr.contains(":");
+    }
+
+    private static boolean isCustomDelimiter(String calcStr) {
+        return calcStr.matches("//(.)\n(.*)");
+    }
+
+    private static boolean isBlank(String calcStr) {
+        return calcStr == null || calcStr.isEmpty();
     }
 }
