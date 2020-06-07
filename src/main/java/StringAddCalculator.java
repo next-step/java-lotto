782fe2d6ef492cc143d3f49679aed868bb853c3b
y00s22 <ryusuy22@gmail.com>
2020-06-07 16:12:07 +0900

    쉽표구분자 케이스 리펙토링: 숫자 하나를 입력받는 경우에도 split()을 이용 할 수 있다.

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,22 +1,15 @@
 public class StringAddCalculator {
     public static int splitAndSum(String calcStr) {
-        int result = -99;
+        int result = 0;
 
         if (calcStr == null || calcStr.isEmpty()) {
             return 0;
         }
 
-        if (calcStr.length() == 1) {
-            return Integer.parseInt(calcStr);
-        }
-
         String[] numbers = calcStr.split(",");
-        int sum = 0;
         for (String number: numbers) {
-            sum += Integer.parseInt(number);
+            result += Integer.parseInt(number);
         }
-
-        result = sum;
 
         return result;
     }
