ad2a38ae61079c8d3e6ffdcb65976a766581c792
김다혜 <dahye432111@gmail.com>
2020-06-08 23:08:56 +0900

    feat: 수동 로또 번호를 입력받을 수 있도록 InputView에 메서드 추가

--- src/main/java/dev/dahye/lotto/view/InputView.java
+++ src/main/java/dev/dahye/lotto/view/InputView.java
@@ -1,8 +1,14 @@
 package dev.dahye.lotto.view;
 
+import dev.dahye.lotto.domain.LottoTicket;
+import dev.dahye.lotto.util.ConvertUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final int ZERO_VALUE = 0;
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int doInputMoney() {
@@ -10,6 +16,39 @@ public class InputView {
         int purchaseAmount = Integer.parseInt(scanner.nextLine());
 
         return purchaseAmount;
+    }
+
+    public static int doInputCountOfManualLotto() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int countOfManualLotto = Integer.parseInt(scanner.nextLine());
+
+        return countOfManualLotto;
+    }
+
+    public static void printManualLottoInputHeader() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+    }
+
+    public static List<LottoTicket> doInputManualLotto(int countOfManualLotto) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+
+        if (isExistManualLotto(countOfManualLotto)) {
+            doInputManualLotto(countOfManualLotto, lottoTickets);
+        }
+
+        return lottoTickets;
+    }
+
+    private static void doInputManualLotto(int countOfManualLotto, List<LottoTicket> lottoTickets) {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+
+        for (int i = 0; i < countOfManualLotto; i++) {
+            lottoTickets.add(LottoTicket.manualIssued(ConvertUtils.convertStringToIntegerList(scanner.nextLine())));
+        }
+    }
+
+    private static boolean isExistManualLotto(int countOfManualLotto) {
+        return countOfManualLotto > ZERO_VALUE;
     }
 
     public static String doInputWinningNumbers() {
