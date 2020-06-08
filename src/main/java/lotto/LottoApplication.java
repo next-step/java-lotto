c5961ae379c0518fa8878428df8d4c48b3c6bf9f
eunji <jeeell.922@gmail.com>
2020-06-08 15:55:27 +0900

    refactor 구조 변경에 따른 LottoApplication 수정

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,23 +2,22 @@ package lotto;
 
 import lotto.domain.lotto.LottoTicket;
 import lotto.domain.prize.WinningResult;
-import lotto.service.LottoMachine;
+import lotto.util.LottoUtil;
 import lotto.ui.InputView;
 import lotto.ui.ResultView;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        //로또 금액 입력
         int purchaseAmount = InputView.enterPurchaseAmount();
 
-        //로또 기계에서 로또 구매
-        LottoMachine lottoMachine = LottoMachine.turnOn();
-        LottoTicket lottoTicket = lottoMachine.purchaseLotto(purchaseAmount);
+        int quantity = LottoUtil.calculateLottoQuantity(purchaseAmount);
+        LottoTicket lottoTicket = LottoTicket.create(quantity);
+        ResultView.printLottoTicket(quantity, lottoTicket);
 
         //당첨번호 입력 및 당첨 결과 출력
         String enteredWinNumber = InputView.enterWinningNumber();
-        WinningResult winningResult = WinningResult.create(lottoTicket, enteredWinNumber);
-        winningResult.printWinningResult();
+        WinningResult winningResult = lottoTicket.makeWinningResult(enteredWinNumber);
+        ResultView.printWinningResult(winningResult);
 
         //수익율 출력
         double winningRate = winningResult.calculateWinningRate(purchaseAmount);
