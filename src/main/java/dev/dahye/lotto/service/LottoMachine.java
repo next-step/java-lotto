81de274fcec7e001a5b657690f0a56171b1b124a
김다혜 <dahye432111@gmail.com>
2020-06-09 00:41:30 +0900

    refactor: 메소드 분리

--- src/main/java/dev/dahye/lotto/service/LottoMachine.java
+++ src/main/java/dev/dahye/lotto/service/LottoMachine.java
@@ -12,20 +12,24 @@ public class LottoMachine {
 
     public LottoMachine(LottoMoney lottoMoney, int countOfManualLotto, List<LottoTicket> manualLottoTickets) {
         int countOfLotto = lottoMoney.calculateCountOfLotto();
-        validateManualLotto(countOfLotto, countOfManualLotto, manualLottoTickets);
         this.countOfManualLotto = countOfManualLotto;
+        validateManualLotto(countOfLotto, manualLottoTickets);
         lottoTickets = LottoTickets.multiIssued(countOfLotto, manualLottoTickets);
     }
 
-    private void validateManualLotto(int countOfLotto, int countOfManualLotto, List<LottoTicket> lottoTickets) {
+    private void validateManualLotto(int countOfLotto, List<LottoTicket> lottoTickets) {
+        validateGreaterThan(countOfLotto, countOfManualLotto, "수동 로또 갯수는 전체 갯수를 초과할 수 없습니다.");
+        validateGreaterThan(countOfManualLotto, 0, "수동 로또 갯수는 음수일 수 없습니다.");
+        validateCountOfManualLottoTickets(lottoTickets);
+    }
+
+    private void validateGreaterThan(int countOfLotto, int countOfManualLotto, String s) {
         if (countOfLotto < countOfManualLotto) {
-            throw new IllegalArgumentException("수동 로또 갯수는 전체 갯수를 초과할 수 없습니다.");
+            throw new IllegalArgumentException(s);
         }
+    }
 
-        if (countOfManualLotto < 0) {
-            throw new IllegalArgumentException("수동 로또 갯수는 음수일 수 없습니다.");
-        }
-
+    private void validateCountOfManualLottoTickets(List<LottoTicket> lottoTickets) {
         if (lottoTickets.size() != countOfManualLotto) {
             throw new IllegalArgumentException("수동 로또 갯수 만큼 로또 티켓을 입력해주세요.");
         }
