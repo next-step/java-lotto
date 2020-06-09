0361955b9d2f42f7f78005edc3b22bbcc1478c6a
bluehoped <jhseo1@iteyes.co.kr>
2020-06-10 08:20:34 +0900

    fix 메서드 이름 및 숫자 상수화

--- src/main/java/step1/Separator.java
+++ src/main/java/step1/Separator.java
@@ -4,23 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Separator {
-    public static final String DEFAULT_SEPARATOR = ",|:";
-    public static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    private static final int CUSTOM_SEPARATOR_GROUP_NUMBER= 1;
+    private static final int CUSTOM_SEPARATOR_FORMULA_GROUP_NUMBER = 2;
 
     private Separator(){}
 
     public static String[] split(String formula) {
         Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(formula);
         if (matcher.find()) {
-            String customSeparator = matcher.group(1);
-            String customSeparatorFormula = matcher.group(2);
-            return defaultSplit(customSeparatorFormula, customSeparator);
+            String customSeparator = matcher.group(CUSTOM_SEPARATOR_GROUP_NUMBER);
+            String customSeparatorFormula = matcher.group(CUSTOM_SEPARATOR_FORMULA_GROUP_NUMBER);
+            return splitFormulaBySeparator(customSeparatorFormula, customSeparator);
         }
 
-        return defaultSplit(formula, DEFAULT_SEPARATOR);
+        return splitFormulaBySeparator(formula, DEFAULT_SEPARATOR);
     }
 
-    private static String[] defaultSplit(String formula, String separator) {
+    private static String[] splitFormulaBySeparator(String formula, String separator) {
         return formula.split(separator);
     }
 }
