8b453ee5792fbfef4098d8eb698f19ae7799ee72
bbbnam <bbbnam@naver.com>
2020-06-09 10:12:10 +0900

    refactor : 로또 넘버 cache 처리, 코드 컨벤션 수정

--- src/main/java/study/step3/domain/LottoNumber.java
+++ src/main/java/study/step3/domain/LottoNumber.java
@@ -1,22 +1,24 @@
 package study.step3.domain;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class LottoNumber implements Comparable<LottoNumber>{
-    private final static int LOTTO_MAX_NUMBER = 45;
-    private final static int LOTTO_MIN_NUMBER = 1;
-    private final static String LOTTO_NULL_EXCEPTION = "당첨번호는 빈 값을 입력할 수 없습니다.";
-    private final static String LOTTO_LIMIT_NUMBER_EXCEPTION = "로또 번호는 1보다 작거나 45보다 클 수 없습니다.";
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final String LOTTO_NULL_EXCEPTION = "당첨번호는 빈 값을 입력할 수 없습니다.";
+    private static final String LOTTO_LIMIT_NUMBER_EXCEPTION = "로또 번호는 1보다 작거나 45보다 클 수 없습니다.";
+    private static final String LOTTO_BONUS_BALL_EXCEPTION = "이미 입력한 번호는 보너스로 입력 불가합니다.";
+    private static final LottoNumber [] CACHE = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+                                                        .mapToObj(LottoNumber::new)
+                                                        .toArray(LottoNumber[]::new);
 
     private final int lottoNumber;
 
     public LottoNumber(String lottoNumber) {
         this(convertToInt(validateLottoNumberString(lottoNumber)));
-    }
-
-    public LottoNumber(int lottoNumber) {
-        validateLottoNumberMinMax(lottoNumber);
-        this.lottoNumber = lottoNumber;
     }
 
     private static int convertToInt(String lottoNumber) {
@@ -30,14 +32,38 @@ public class LottoNumber implements Comparable<LottoNumber>{
         return inputLottoNumbers;
     }
 
-    private void validateLottoNumberMinMax(int lottoNumbers) {
-        if (lottoNumbers > LOTTO_MAX_NUMBER || lottoNumbers < LOTTO_MIN_NUMBER){
+    private LottoNumber(int lottoNumber) {
+        lottoLimitValid(lottoNumber);
+        this.lottoNumber = lottoNumber;
+    }
+
+    private static void lottoLimitValid(int lottoNumber) {
+        if (!isRangeClosed(lottoNumber)){
             throw new IllegalArgumentException(LOTTO_LIMIT_NUMBER_EXCEPTION);
         }
     }
 
+    private static boolean isRangeClosed(int lottoNumber) {
+        return lottoNumber <= LOTTO_MAX_NUMBER && lottoNumber >= LOTTO_MIN_NUMBER;
+    }
+
+    public static LottoNumber cacheNumber(int number){
+        lottoLimitValid(number);
+        return CACHE[--number];
+    }
+
     public int getLottoNumber() {
         return lottoNumber;
+    }
+
+    public void validateDuplicationBonusBall(List<LottoNumber> lottoNumbers) {
+        if(lottoNumbers.contains(this)){
+            throw new IllegalArgumentException(LOTTO_BONUS_BALL_EXCEPTION);
+        }
+    }
+
+    public boolean isMatch(Set<LottoNumber> lottoNumbers) {
+        return lottoNumbers.contains(this);
     }
 
     @Override
