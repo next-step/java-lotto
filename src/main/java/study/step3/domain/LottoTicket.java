82b6f25f760ea3d86cc3fd8aabf78e2de826ee35
bbbnam <bbbnam@naver.com>
2020-06-09 10:15:38 +0900

    refactor : 로또 번호 캐싱된 내용으로 가져오도록 수정

--- src/main/java/study/step3/domain/LottoTicket.java
+++ src/main/java/study/step3/domain/LottoTicket.java
@@ -5,29 +5,34 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoTicket {
-    private final static String LOTTO_DUPLICATION_EXCEPTION = "로또 번호는 중복되지 않는 6개의 번호로 입력 가능합니다.";
-    private final static String DELIMITER = ",";
-    private final static int LOTTO_NUMBERS_CONDITION = 6;
+    private static final String LOTTO_DUPLICATION_EXCEPTION = "로또 번호는 중복되지 않는 6개의 번호로 입력 가능합니다.";
+    private static final String DELIMITER = ",";
+    private static final int LOTTO_NUMBERS_CONDITION = 6;
 
     private final Set<LottoNumber> lottoNumbers;
 
     public LottoTicket(String inputLottoNumbers){
-        this(convertToSet(splitNumbers(inputLottoNumbers)));
+        this(convertToSet(parseInt(splitNumbers(inputLottoNumbers))));
+    }
+    private static String [] splitNumbers(String inputLottoNumbers) {
+        return inputLottoNumbers.split(DELIMITER);
+    }
+
+    private static int[] parseInt(String[] splitNumbers) {
+        return Arrays.stream(splitNumbers)
+                     .mapToInt(Integer::parseInt)
+                     .toArray();
+    }
+
+    private static Set<LottoNumber> convertToSet(int [] lottoNumbers) {
+        return Arrays.stream(lottoNumbers)
+                     .mapToObj(LottoNumber::cacheNumber)
+                     .collect(Collectors.toSet());
     }
 
     public LottoTicket(Set<LottoNumber> lottoNumbers) {
         validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
-    }
-
-    private static String [] splitNumbers(String inputLottoNumbers) {
-        return inputLottoNumbers.split(DELIMITER);
-    }
-
-    private static Set<LottoNumber> convertToSet(String [] lottoNumbers) {
-        return Stream.of(lottoNumbers)
-                     .map(LottoNumber::new)
-                     .collect(Collectors.toSet());
     }
 
     private void validateLottoNumbers(Set<LottoNumber> lottoNumbers) {
@@ -54,12 +59,17 @@ public class LottoTicket {
     }
 
     public WinningRank checkWinningRank(WinningLotto winningLottoTicket){
-        return WinningRank.findRanking(getMatchingCount(winningLottoTicket), winningLottoTicket.isMatchBonus(lottoNumbers));
+        return WinningRank.findRanking(getMatchingCount(winningLottoTicket),
+                                        winningLottoTicket.isMatchBonus(lottoNumbers));
     }
 
     private long getMatchingCount(WinningLotto winningLottoTicket) {
-        Set<LottoNumber> matchedLottos = new HashSet<>(this.lottoNumbers);
-        matchedLottos.retainAll(winningLottoTicket.getLottoNumbers());
-        return matchedLottos.size();
+        return winningLottoTicket.matchCount(this);
+    }
+
+    public long matchNumbers(LottoTicket matchedLottos) {
+        Set<LottoNumber> intersection = new HashSet<>(matchedLottos.lottoNumbers);
+        intersection.retainAll(this.lottoNumbers);
+        return intersection.size();
     }
 }
