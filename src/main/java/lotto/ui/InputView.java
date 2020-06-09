236136c035e09bca000d789dc90af4e9b32dba81
eunji <jeeell.922@gmail.com>
2020-06-09 15:18:12 +0900

    feat 보너스볼 입력 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     public static final Scanner SCANNER = new Scanner(System.in);
+
     private InputView() {}
 
     public static int enterPurchaseAmount() {
@@ -20,6 +21,12 @@ public class InputView {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String winningNumber = SCANNER.nextLine();
         return winningNumber;
+    }
+
+    public static int enterBonusBall() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonusBall = SCANNER.nextInt();
+        return bonusBall;
     }
 
 }
