5d71da31c2805350ac56c74c3873bea14c40276a
bluehoped <jhseo1@iteyes.co.kr>
2020-06-09 11:16:34 +0900

    feat 문자열 숫자가 아니면 RuntimeException

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -10,12 +10,21 @@ public class StringAddCalculator {
     }
 
     public static int sum(String[] array) {
+        validateNumbers(array);
         int total = 0;
         for (String number : array) {
             total += Integer.parseInt(number);
         }
 
         return total;
+    }
+
+    private static void validateNumbers(String[] array) {
+        Arrays.stream(array).forEach(number -> {
+            if (Integer.parseInt(number) < 0) {
+                throw new RuntimeException();
+            }
+        });
     }
 
 }
--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("문자열 덧셈 계산기 클래스 테스트")
 public class StringAddCalculatorTest {
@@ -13,5 +14,16 @@ public class StringAddCalculatorTest {
     void 배열_다_더하기() {
         String[] numbers = {"1", "2", "3", "4"};
         assertThat(StringAddCalculator.sum(numbers)).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("음수와 숫자 이외의 값은 RuntimeException")
+    void 숫자아니면_RUNTIMEEXCEPTION() {
+        String[] numbers = {"zz", "dd", "-3", "4"};
+        assertThatThrownBy(() -> {
+            StringAddCalculator.sum(numbers);
+        }).isInstanceOf(RuntimeException.class);
+
+
     }
 }
