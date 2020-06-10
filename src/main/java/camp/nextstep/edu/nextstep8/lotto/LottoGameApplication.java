f48a2744733f72db5c1c21e5e1c55d76aa6e562d
neojjc <neo.jjc@gmail.com>
2020-06-10 09:56:44 +0900

    [feat] 로또 게임 input 작성

---
+++ src/main/java/camp/nextstep/edu/nextstep8/lotto/LottoGameApplication.java
@@ -0,0 +1,8 @@
+package camp.nextstep.edu.nextstep8.lotto;
+
+public class LottoGameApplication {
+    public static void main(String[] args) {
+        int purchaseAmount = LottoGameInput.getPurchaseAmount();
+        String winnerNumbers = LottoGameInput.getWinnerNumberAtLastWeek();
+    }
+}
