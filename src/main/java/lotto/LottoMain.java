2d7ea7d22a8f841c2287402dcfc18ed4a20a3432
diane.10 <diane.10@kakaocorp.com>
2020-06-10 21:19:27 +0900

    refactor

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -3,10 +3,6 @@ package lotto;
 public class LottoMain {
     public static void main(String[] args) {
         LottoController lotto = new LottoController();
-        lotto.makeLottoTicket();
-        lotto.printLottoPurchase();
-        lotto.enterWinningNumbers();
-        lotto.calculateWinningStatistics();
-        lotto.printWinningStatistics();
+        lotto.buyLotto();
     }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,26 +1,26 @@
 package view;
 
-import lotto.LottoController;
 import lotto.LottoTicket;
+import lotto.TicketCollection;
 
 import java.util.List;
 
 public class ResultView {
-    public void printNumberOfLottoTicek(int cnt) {
-        System.out.println(cnt+"개를 구매했습니다.");
+    public static void printNumberOfLottoTicek(int cnt) {
+        System.out.println(cnt + "개를 구매했습니다.");
     }
 
-    public void printTickets(List<LottoTicket> tickets) {
-        for(LottoTicket ticket : tickets) {
+    public static void printTickets(List<LottoTicket> tickets) {
+        for (LottoTicket ticket : tickets) {
             System.out.println(ticket.makeTicketForPrint());
         }
     }
 
-    public void printStatistics(int[] matcher, double percentage) {
+    public static void printStatistics(List<Integer> matcher, double percentage) {
         System.out.println("당첨 통계");
-        for(int i = 3; i < 7; i++) {
-            System.out.println(i+"개 일치 ("+ LottoController.PRIZE[i]+")- "+matcher[i]+"개");
+        for (int i = 3; i < 7; i++) {
+            System.out.println(i + "개 일치 (" + TicketCollection.PRIZE.get(i) + ")- " + matcher.get(i) + "개");
         }
-        System.out.println("총 수익률은 "+percentage+"입니다.");
+        System.out.println("총 수익률은 " + percentage + "입니다.");
     }
 }
