9674abbbfc8821129a7999564ae0cdf5eb6c2c2a
diane.10 <diane.10@kakaocorp.com>
2020-06-10 21:17:19 +0900

    feat(UiManager) : 입출력받는 클래스 따로 분리

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,23 +1,15 @@
 package lotto;
 
-import utils.StringAddCalculator;
-import view.InputView;
-import view.ResultView;
+import view.UiManager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoController {
-    private int investAmount;
-    private int count;
-    private static final int LOTTO_PRICE = 1000;
-    public static final int[] PRIZE = {0, 0, 0, 5000, 50000, 1500000, 2000000000};
-    private List<LottoTicket> tickets = new ArrayList<>();
-    private final InputView inputView = new InputView();
-    private final ResultView resultview = new ResultView();
-    private final RandomPicker randomPicker = new RandomPicker();
+    private UiManager uiManager = new UiManager();
     private List<Integer> winningNumbers = new ArrayList<>();
-    private int[] winningmatcher = new int[7];
+    private List<Integer> winningMatcher = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0));
     private int sumPrize = 0;
     private TicketCollection tickets;
 
@@ -31,42 +23,8 @@ public class LottoController {
         uiManager.printWinningStatistics(winningMatcher, sumPrize);
     }
 
-    public void makeLottoTicket() {
-        for (int i = 0; i < count; i++) {
-            LottoTicket ticket = new LottoTicket(randomPicker.pickRandomLotto());
-            tickets.add(ticket);
-        }
-    }
-
-    public void printLottoPurchase() {
-        resultview.printNumberOfLottoTicek(count);
-        resultview.printTickets(tickets);
-    }
-
-    public void enterWinningNumbers() {
-        numbers = inputView.inputWinningNumber();
-    }
-
     public void calculateWinningStatistics() {
-        String[] num = splitWinningNumber(numbers);
-        winningNumbers = StringAddCalculator.convertToIntArray(num);
-        for (LottoTicket ticket : tickets) {
-            int cnt = ticket.matchWinningNumber(winningNumbers);
-            winningmatcher[cnt]++;
-            sumPrize += PRIZE[cnt];
-        }
-    }
-
-    public void printWinningStatistics() {
-        resultview.printStatistics(winningmatcher, sumPrize / investAmount);
-    }
-
-    private String[] splitWinningNumber(String numbers) {
-        numbers = removeWhiteSpace(numbers);
-        return numbers.split(",");
-    }
-
-    private String removeWhiteSpace(String text) {
-        return text.replace(" ","");
+        winningNumbers = uiManager.takeWinningNumber();
+        sumPrize = tickets.calculateStatics(winningNumbers, winningMatcher);
     }
 }
---
+++ src/main/java/view/UiManager.java
@@ -0,0 +1,41 @@
+package view;
+
+import utils.StringAddCalculator;
+import utils.StringUtils;
+
+import java.util.List;
+
+public class UiManager {
+    private final InputView inputView = new InputView();
+    private int investAmount;
+    private int count;
+    private static final int LOTTO_PRICE = 1000;
+
+
+    public int takeCount() {
+        investAmount = inputView.inputPurchaseAmount();
+        if (investAmount <= 0) {
+            throw new IllegalArgumentException("Only positive numbers can be entered.");
+        }
+        count = investAmount / LOTTO_PRICE;
+
+        return count;
+    }
+
+    public List<Integer> takeWinningNumber() {
+        String numbers = inputView.inputWinningNumber();
+        String[] num = splitWinningNumber(numbers);
+        return StringAddCalculator.convertToIntArray(num);
+    }
+
+    private String[] splitWinningNumber(String numbers) {
+        numbers = StringUtils.removeWhiteSpace(numbers);
+        return numbers.split(",");
+    }
+
+
+    public void printWinningStatistics(List<Integer> winningmatcher, int sumPrize) {
+        ResultView.printStatistics(winningmatcher, sumPrize / investAmount);
+    }
+
+}
