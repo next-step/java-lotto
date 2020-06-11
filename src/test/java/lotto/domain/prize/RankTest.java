117e4ae17685e61589faa65f0749a860ac893f2c
eunji <jeeell.922@gmail.com>
2020-06-11 21:40:41 +0900

    refactor 테스트 코드 변경
    - @ParameterizedTest 사용

--- src/test/java/lotto/domain/prize/RankTest.java
+++ src/test/java/lotto/domain/prize/RankTest.java
@@ -7,15 +7,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RankTest {
-    @Test
-    void 일치하는_수를_로또_등수로_변경한다() {
-        assertThat(Rank.valueOf(6, true)).isEqualTo(Rank.FIRST);
-        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
-
-        assertThat(Rank.valueOf(3, true)).isEqualTo(Rank.FIFTH);
-        assertThat(Rank.valueOf(2, true)).isEqualTo(Rank.MISS);
-        assertThat(Rank.valueOf(1, false)).isEqualTo(Rank.MISS);
+    @ParameterizedTest
+    @CsvSource(value = {"6,true,FIRST", "5, true, SECOND", "5, false, THIRD",
+            "3, true, FIFTH", "2, true, MISS", "1, false, MISS"})
+    void 일치하는_수를_로또_등수로_변경한다(int countOfMatch, boolean matchBonus, Rank rank) {
+        assertThat(Rank.valueOf(countOfMatch, matchBonus)).isEqualTo(rank);
     }
 
 
