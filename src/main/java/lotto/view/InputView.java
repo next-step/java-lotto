0b06cb8d30040d77f7d9c9c335d0b405b032a70b
rlapwl <khji1210@naver.com>
2020-06-11 20:30:49 +0900

    feat(InputView): 필요한 입력값 메소드 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,6 +10,16 @@ public class InputView {
         return scanner.nextInt();
     }
 
+    public static int buySelfLotto() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public static String[] inputSelfNumber() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        return scanner.nextLine().split(", ");
+    }
+
     public static String[] inputWinningNumber() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         scanner.nextLine();
