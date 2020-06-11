ae126a3a67fc9dafb962afe4220d2d3c6261deae
bluehoped <jhseo1@iteyes.co.kr>
2020-06-11 13:32:10 +0900

    fix 통계 출력 및 MathUtils 사용

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -3,32 +3,32 @@ package step2.view;
 import step2.LottoTicket;
 import step2.Statistics;
 import step2.WinningStatistics;
-import step2.Yield;
+import step2.util.MathUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ResultView {
-    private List<LottoTicket> lottoTicketList = new ArrayList<>();
-
+    private static final int LOTTO_PRICE = 1000;
+    private int lottoTicketCount;
     private ResultView() {
     }
 
     public ResultView(List<LottoTicket> lottoTicketList){
-        this.lottoTicketList = lottoTicketList;
-        printMessage(lottoTicketList.size() + "개를 구매했습니다.");
+        this.lottoTicketCount = lottoTicketList.size();
+        printMessage(lottoTicketCount + "개를 구매했습니다.");
         for (LottoTicket lottoTicket : lottoTicketList) {
             printMessage(lottoTicket.getNumbers().toString());
         }
     }
 
-    public void printResult(Statistics result) {
+    public void printResult(Statistics statistics) {
         printMessage("당첨 통계");
         printMessage("----------");
 
-        printStatistics(result);
+        printStatistics(statistics);
 
-        double yield = Yield.calculate(result.getTotalPrice(), lottoTicketList.size() * 1000);
+        double yield = MathUtils.calculateYield(statistics.getTotalPrice(), lottoTicketCount * LOTTO_PRICE);
+        yield = MathUtils.floorDecimal(yield, 2);
         String message = "총 수익률은 " + yield + "입니다.";
         if (yield < 1) {
             message += "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
@@ -36,6 +36,7 @@ public class ResultView {
         printMessage(message);
 
     }
+
     private static void printMessage(String message) {
         System.out.println(message);
     }
@@ -43,7 +44,8 @@ public class ResultView {
     private void printStatistics(Statistics result) {
         result.getStatistics().forEach((key, value) -> {
             WinningStatistics winning = WinningStatistics.valueOfWinningCount(key);
-            winning.print(value);
+            String message = winning.getMatchedNumberCount() + "개 일치 (" + winning.getWinningPrice() + "원)- " + value +"개";
+            printMessage(message);
         });
     }
 }
