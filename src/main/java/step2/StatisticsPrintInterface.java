d5e12d2719a4236e101a62ba02e33f91b43da485
bluehoped <jhseo1@iteyes.co.kr>
2020-06-11 13:30:14 +0900

    fix 출력 펑셔널인터페이스 제거 적용

--- src/main/java/step2/StatisticsPrintInterface.java
+++
@@ -1,6 +0,0 @@
-package step2;
-
-@FunctionalInterface
-public interface StatisticsPrintInterface {
-    void print(int count);
-}
--- src/main/java/step2/WinningStatistics.java
+++ src/main/java/step2/WinningStatistics.java
@@ -3,31 +3,21 @@ package step2;
 import java.util.Arrays;
 
 public enum WinningStatistics {
-    WINING3(3, 5000, (count) -> System.out.println("3개 일치 (5000원)- " + count + "개")),
-    WINING4(4, 50000, (count) -> System.out.println("4개 일치 (50000원)- " + count + "개")),
-    WINING5(5, 1500000, (count) -> System.out.println("5개 일치 (1500000원)- " + count + "개")),
-    WINING6(6, 2000000000, (count) -> System.out.println("6개 일치 (2000000000원)- " + count + "개"));
+    WINING3(3, 5000),
+    WINING4(4, 50000),
+    WINING5(5, 1500000),
+    WINING6(6, 2000000000);
 
     private int matchedNumberCount;
     private int winningPrice;
-    private StatisticsPrintInterface statisticsPrintInterface;
 
-    WinningStatistics(int matchedNumberCount, int winningPrice, StatisticsPrintInterface statisticsPrintInterface) {
+    WinningStatistics(int matchedNumberCount, int winningPrice) {
         this.matchedNumberCount = matchedNumberCount;
         this.winningPrice = winningPrice;
-        this.statisticsPrintInterface = statisticsPrintInterface;
     }
 
     public static WinningStatistics valueOfWinningCount(int winningCount) {
-        return Arrays.stream(values()).filter(statistics -> statistics.compare(statistics.matchedNumberCount, winningCount)).findAny().orElseThrow(IllegalArgumentException::new);
-    }
-
-    private boolean compare(int number1, int number2) {
-        return number1 == number2;
-    }
-
-    public void print(int count) {
-        statisticsPrintInterface.print(count);
+        return Arrays.stream(values()).filter(statistics -> statistics.matchedNumberCount == winningCount).findAny().orElseThrow(IllegalArgumentException::new);
     }
 
     public int getMatchedNumberCount() {
