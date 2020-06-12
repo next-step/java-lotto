57f47fe63c7e24588762d1b01a0c53eac2108ce0
bluehoped <jhseo1@iteyes.co.kr>
2020-06-12 09:11:16 +0900

    refactor(WinningStatistics) : enum 수정
    내용
    - 변수명 변경 winningPrice -> winningMoney
    - valueOfWinningCount static final unmodifiableMap 사용
    - name 및 winningMoney 표현 변경

--- src/main/java/step3/WinningStatistics.java
+++ src/main/java/step3/WinningStatistics.java
@@ -1,30 +1,41 @@
 package step3;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum WinningStatistics {
-    WINING3(3, 5000),
-    WINING4(4, 50000),
-    WINING5(5, 1500000),
-    WINING6(6, 2000000000);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private static final Map<Integer, WinningStatistics> matchedCounts =
+            Collections.unmodifiableMap(Stream.of(values()).
+                    collect(Collectors.toMap(WinningStatistics::getMatchedNumberCount, Function.identity())));
 
     private int matchedNumberCount;
-    private int winningPrice;
+    private int winningMoney;
 
-    WinningStatistics(int matchedNumberCount, int winningPrice) {
+    WinningStatistics(int matchedNumberCount, int winningMoney) {
         this.matchedNumberCount = matchedNumberCount;
-        this.winningPrice = winningPrice;
+        this.winningMoney = winningMoney;
     }
 
     public static WinningStatistics valueOfWinningCount(int winningCount) {
-        return Arrays.stream(values()).filter(statistics -> statistics.matchedNumberCount == winningCount).findAny().orElseThrow(IllegalArgumentException::new);
+        return Optional.ofNullable(matchedCounts.get(winningCount)).orElse(MISS);
     }
 
     public int getMatchedNumberCount() {
         return matchedNumberCount;
     }
 
-    public int getWinningPrice() {
-        return winningPrice;
+    public int getWinningMoney() {
+        return winningMoney;
     }
 }
