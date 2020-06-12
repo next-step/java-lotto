7f2b1d30a237d4c3056998547a225117d012dd61
eunji <jeeell.922@gmail.com>
2020-06-12 21:01:28 +0900

    feat 당첨로또 객체 추가
    - 당첨 번호 생성
    - 로또티켓, 보너스볼과 당첨번호를 가지고 당첨 결과 생성

--- src/test/java/lotto/domain/prize/WinningLottoTest.java
+++ src/test/java/lotto/domain/prize/WinningLottoTest.java
@@ -1,11 +1,9 @@
 package lotto.domain.prize;
 
-import lotto.domain.lotto.Number;
 import lotto.domain.lotto.LottoNumbers;
+import lotto.domain.lotto.LottoTicket;
+import lotto.domain.lotto.LottoNumber;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,47 +11,35 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
-class WinningNumberGeneratorTest {
+class WinningLottoTest {
+    List<LottoNumber> lottoNumber = Arrays.asList(1, 2, 3, 4, 5, 6).stream()
+            .map(x -> LottoNumber.create(x))
+            .collect(Collectors.toList());
+    LottoTicket lottoTicket = LottoTicket.create(1);
+    String enteredWinNumber = "1, 2, 3, 4, 5, 6";
+
 
     @Test
     void 생성_테스트() {
-        String enteredWinNumber = "1, 2, 3, 4, 5, 6";
-        assertThatCode(() -> WinningNumbers.create(enteredWinNumber)).doesNotThrowAnyException();
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
-    void 당첨번호_입력수_확인(String enteredWinNumber) {
-        assertThatThrownBy(() -> WinningNumbers.create(enteredWinNumber))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatCode(() -> WinningLotto.create(lottoTicket, enteredWinNumber, 10)).doesNotThrowAnyException();
     }
 
     @Test
-    void 당첨번호_중복입력_확인() {
-        String enteredWinNumber = "1, 1, 2, 3, 4, 5";
-        assertThatThrownBy(() -> WinningNumbers.create(enteredWinNumber))
-                .isInstanceOf(IllegalArgumentException.class).hasMessage("중복된 당첨 번호가 있습니다");
+    void 당첨번호와_보너스볼_중복확인() {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> WinningLotto.create(lottoTicket, enteredWinNumber, 6)
+        ).withMessageContaining("보너스 볼이 당첨번호와 중복됩니다.");
     }
 
     @Test
-    void 당첨번호_입력범위_확인() {
-        String enteredWinNumber = "0, 1, 32, 37, 45, 49";
-        assertThatThrownBy(() -> WinningNumbers.create(enteredWinNumber))
-                .isInstanceOf(IllegalArgumentException.class).hasMessage("당첨 번호는 1 ~ 45 사이의 수 입니다");
+    void 당첨결과를_생성한다() {
+        LottoTicket lottoTicket = LottoTicket.createOne(Arrays.asList(LottoNumbers.create(lottoNumber)));
+        WinningLotto winningLotto = WinningLotto.create(lottoTicket, enteredWinNumber, 10);
+
+        WinningResult winningResult = winningLotto.makeWinningResult();
+        int winCount = winningResult.tellWinningCount(Rank.FIRST);
+
+        assertThat(winCount).isEqualTo(1);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"2, 3, 4, 5, 9, 19 | 0", "1, 7, 23, 32, 33, 34 | 4"
-            , "1, 7, 23, 34, 37, 44 | 6"}, delimiter = '|')
-    void 당첨번호_일치개수를_확인한다(String enteredWinNumber, int expectedCount) {
-        WinningNumbers winningNumbers = WinningNumbers.create(enteredWinNumber);
-        List<Number> lottoNumber = Arrays.asList(1, 7, 23, 34, 37, 44).stream()
-                .map(k -> Number.create(k)).collect(Collectors.toList());
-
-        LottoNumbers lottoNumbers = LottoNumbers.create(lottoNumber);
-
-        int matchCount = winningNumbers.findMatchCount(lottoNumbers);
-
-        assertThat(matchCount).isEqualTo(expectedCount);
-    }
 }
