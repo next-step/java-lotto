405005fdf1f0bf61ab6f1c30968fdd5a9e2cee97
DominiqN <dmut7691@gmail.com>
2020-06-13 16:59:36 +0900

    fix: make Calculator recieve custom separator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final String SEPARATOR_REGEX = "[,:]";
@@ -8,6 +10,13 @@ public class StringAddCalculator {
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
             return 0;
+        }
+        Matcher a = Pattern.compile("//.+\n").matcher(expression);
+        if (a.find()) {
+            String customSeparator = a.group()
+                    .replace("//", "")
+                    .replace("\n", "");
+            return sum(expression.split("\n")[1].split(customSeparator));
         }
         return sum(expression.split(SEPARATOR_REGEX));
     }
