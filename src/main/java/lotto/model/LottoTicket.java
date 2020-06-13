bfc55f8c5a30d7c2cbee36d69d6bbea824ff60b7
y00s22 <ryusuy22@gmail.com>
2020-06-13 15:24:02 +0900

    로또번호 생성에 대한 테스트와 프로덕션 코드 작성

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -1,5 +1,26 @@
 package lotto.model;
 
-public class LottoTicket {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class LottoTicket {
+    private static final List<Integer> lottoDrawNumbers = new ArrayList(); //{1, 2, 3, 4};
+
+    public LottoTicket() {
+        for(int i = 1; i <= 45; i++) {
+            lottoDrawNumbers.add(i);
+        }
+    }
+
+    public List<Integer> buyTicket() {
+        Collections.shuffle(lottoDrawNumbers);
+        List<Integer> numbers = new ArrayList<>();
+
+        for(int i = 0; i < 6; i++) {
+            numbers.add(lottoDrawNumbers.get(i));
+        }
+
+        return numbers;
+    }
 }
--- src/test/java/lottoTest/LottoTicketTest.java
+++ src/test/java/lottoTest/LottoTicketTest.java
@@ -1,7 +1,24 @@
 package lottoTest;
 
 import lotto.model.LottoTicket;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketTest {
     LottoTicket ticket = new LottoTicket();
+
+    @DisplayName("로또 번호를 자동으로 생성한다 - 중복 없는지 체크")
+    @Test
+    public void isItDuplication() {
+        List<Integer> lotto1 = ticket.buyTicket();
+        List<Integer> lotto2 = ticket.buyTicket();
+
+        assertThat(lotto1.size()).isEqualTo(6);
+        assertThat(lotto2.size()).isEqualTo(6);
+        assertThat(lotto1).isNotEqualTo(lotto2);
+    }
 }
