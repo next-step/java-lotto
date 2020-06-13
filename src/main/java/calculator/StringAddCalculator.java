be2528cdc54d1d627375e7cadffdc6450deb0b54
DominiqN <dmut7691@gmail.com>
2020-06-13 17:05:39 +0900

    refactor: cache custom separator pattern

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final String DEFAULT_SEPARATOR_REGEX = "[,:]";
+    private static final Pattern CUSTOM_SEPARATOR_EXPRESSION_REGEX = Pattern.compile("//.+\n");
 
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
@@ -20,7 +21,7 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String expression) {
-        Matcher a = Pattern.compile("//.+\n").matcher(expression);
+        Matcher a = CUSTOM_SEPARATOR_EXPRESSION_REGEX.matcher(expression);
         if (a.find()) {
             String customSeparator = a.group()
                     .replace("//", "")
