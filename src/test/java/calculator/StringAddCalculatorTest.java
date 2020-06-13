c1e5ce1717a5487af37fb05f117ec65db6d68ce9
DominiqN <dmut7691@gmail.com>
2020-06-13 16:56:57 +0900

    test: add test of Matcher and Pattern

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 public class StringAddCalculatorTest {
     @Test
@@ -34,4 +38,16 @@ public class StringAddCalculatorTest {
         int sum = StringAddCalculator.calculate("//;\n1;2;3");
         assertThat(sum).isEqualTo(6);
     }
+
+    @Test
+    public void patternMatcherTest() {
+        String given = "//ak\nbcd";
+        Matcher matcher = Pattern.compile("//.+\n").matcher(given);
+        assertThat(matcher.find()).isTrue();
+        assertThat(matcher.group()).isEqualTo("//ak\n");
+        assertThatIllegalStateException().isThrownBy(() -> {
+            Pattern.compile("////").matcher(given).group();
+        });
+    }
+
 }
