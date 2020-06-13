a58ba3e54a04b93890c59b260badd42bbc05d3d9
DominiqN <dmut7691@gmail.com>
2020-06-13 16:19:33 +0900

    test: fix sum test that colon also can be a separator

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -22,9 +22,10 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(value);
     }
 
-    @Test
-    public void whenPutTwoIntStringWithComma_thenReturnSumOfThem() {
-        int sum = StringAddCalculator.calculate("1,2");
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2", "1:2"})
+    public void whenPutTwoIntStringWithCommaOrColonSeparator_thenReturnSumOfThem(String expression) {
+        int sum = StringAddCalculator.calculate(expression);
         assertThat(sum).isEqualTo(3);
     }
 }
