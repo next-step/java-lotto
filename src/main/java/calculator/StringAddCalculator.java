5df2f71c83cfad177af3dd89e9a883f32bb7a7f4
DominiqN <dmut7691@gmail.com>
2020-06-13 18:24:27 +0900

    refactor: Name exception message

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -10,6 +10,7 @@ public class StringAddCalculator {
     private static final String CUSTOM_SEPARATOR_WRAPPER_POSTFIX = "\n";
     private static final String CUSTOM_SEPARATOR_INDICATOR = CUSTOM_SEPARATOR_WRAPPER_PREFIX + ".+" + CUSTOM_SEPARATOR_WRAPPER_POSTFIX;
     private static final Pattern CUSTOM_SEPARATOR_INDICATOR_PATTERN = Pattern.compile(CUSTOM_SEPARATOR_INDICATOR);
+    private static final String BAD_EXPRESSION_NON_INTEGER = "The expression must consist only of integers separated by the specific separator.";
 
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
@@ -46,7 +47,7 @@ public class StringAddCalculator {
         try {
             return sumIntString(strings);
         } catch (NumberFormatException e) {
-            throw new RuntimeException("The expression must consist only of integers separated by the specific separator.");
+            throw new RuntimeException(BAD_EXPRESSION_NON_INTEGER);
         }
     }
 
