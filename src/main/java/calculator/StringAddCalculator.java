5f378c3ba043332d2cefb062ca7b8e58fa470c40
DominiqN <dmut7691@gmail.com>
2020-06-13 16:21:41 +0900

    fix: add colon as separator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,7 +3,7 @@ package calculator;
 import java.util.Arrays;
 
 public class StringAddCalculator {
-    private static final String SEPARATOR_REGEX = ",";
+    private static final String SEPARATOR_REGEX = "[,:]";
 
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
