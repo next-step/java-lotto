d608864b50e4eeadf244b8d9e65c5bfe6d8af616
eunji <jeeell.922@gmail.com>
2020-06-13 22:22:05 +0900

    refactor LottoNumber 객체 캐싱 적용

--- src/main/java/lotto/domain/lotto/LottoNumber.java
+++ src/main/java/lotto/domain/lotto/LottoNumber.java
@@ -1,8 +1,19 @@
 package lotto.domain.lotto;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumber implements Comparable<LottoNumber> {
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 45;
+    private static final List<LottoNumber> lottoNumbers =
+            Collections.unmodifiableList(IntStream.rangeClosed(START_NUMBER, END_NUMBER)
+                    .mapToObj(number -> new LottoNumber(number))
+                    .collect(Collectors.toList()));
+
     private final int lottoNumber;
 
     private LottoNumber(int lottoNumber) {
@@ -10,7 +21,14 @@ public class LottoNumber implements Comparable<LottoNumber> {
     }
 
     public static LottoNumber create(int lottoNumber) {
-        return new LottoNumber(lottoNumber);
+        checkInRange(lottoNumber);
+        return lottoNumbers.get(lottoNumber - 1);
+    }
+
+    private static void checkInRange(int lottoNumber) {
+        if(lottoNumber < START_NUMBER || lottoNumber > END_NUMBER){
+            throw new IllegalArgumentException("입력 값이 유효하지 않습니다.");
+        }
     }
 
     @Override
--- src/test/java/lotto/domain/lotto/LottoNumberTest.java
+++ src/test/java/lotto/domain/lotto/LottoNumberTest.java
@@ -1,12 +1,23 @@
 package lotto.domain.lotto;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoNumberTest {
     @Test
     void 생성_테스트() {
-        assertThatCode(() -> LottoNumber.create(5)).doesNotThrowAnyException();
+        assertThatCode(() -> LottoNumber.create(1)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void 생성범위_확인(int lottoNumber) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoNumber.create(lottoNumber))
+                .withMessageContaining("입력 값이 유효하지 않습니다.");
     }
 }
