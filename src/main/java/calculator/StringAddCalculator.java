7d36e8e227b00f3c55540ec878abf69edbc3cf0f
DominiqN <dmut7691@gmail.com>
2020-06-13 17:56:10 +0900

    refactor: refactor extracting custom separator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,7 +6,10 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final String DEFAULT_SEPARATOR_REGEX = "[,:]";
-    private static final Pattern CUSTOM_SEPARATOR_EXPRESSION_REGEX = Pattern.compile("//.+\n");
+    private static final String CUSTOM_SEPARATOR_WRAPPER_PREFIX = "//";
+    private static final String CUSTOM_SEPARATOR_WRAPPER_POSTFIX = "\n";
+    private static final String CUSTOM_SEPARATOR_INDICATOR = CUSTOM_SEPARATOR_WRAPPER_PREFIX + ".+" + CUSTOM_SEPARATOR_WRAPPER_POSTFIX;
+    private static final Pattern CUSTOM_SEPARATOR_INDICATOR_PATTERN = Pattern.compile(CUSTOM_SEPARATOR_INDICATOR);
 
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
@@ -21,14 +24,22 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String expression) {
-        Matcher a = CUSTOM_SEPARATOR_EXPRESSION_REGEX.matcher(expression);
-        if (a.find()) {
-            String customSeparator = a.group()
-                    .replace("//", "")
-                    .replace("\n", "");
-            return expression.split("\n")[1].split(customSeparator);
+        Matcher matcher = CUSTOM_SEPARATOR_INDICATOR_PATTERN.matcher(expression);
+
+        if (matcher.find()) {
+            String expressionWithoutSeparator = expression.replaceAll(CUSTOM_SEPARATOR_INDICATOR, "");
+            String customSeparator = substringCustomSeparator(matcher.group());
+
+            return expressionWithoutSeparator.split(customSeparator);
         }
+
         return expression.split(DEFAULT_SEPARATOR_REGEX);
+    }
+
+    private static String substringCustomSeparator(String source) {
+        int startIndex = source.indexOf(CUSTOM_SEPARATOR_WRAPPER_PREFIX) + CUSTOM_SEPARATOR_WRAPPER_PREFIX.length();
+        int endIndex = source.indexOf(CUSTOM_SEPARATOR_WRAPPER_POSTFIX);
+        return source.substring(startIndex, endIndex);
     }
 
     private static int sum(String[] intStrings) {
