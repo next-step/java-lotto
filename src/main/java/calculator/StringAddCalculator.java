35938e341dfacc957ac5b94ab3d5401913123907
DominiqN <dmut7691@gmail.com>
2020-06-13 16:09:01 +0900

    refactor: extract sum of int string, and name seperator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,20 +3,22 @@ package calculator;
 import java.util.Arrays;
 
 public class StringAddCalculator {
+    private static final String SEPARATOR_REGEX = ",";
+
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
             return 0;
         }
-        try {
-            return Integer.parseInt(expression);
-        } catch (NumberFormatException e) {
-            String[] a = expression.split(",");
-            return Arrays.stream(a).map(Integer::parseInt).reduce(Integer::sum).get();
-        }
-
+        return sum(expression.split(SEPARATOR_REGEX));
     }
 
     private static boolean isNullOrEmpty(String string) {
         return string == null || string.isEmpty();
+    }
+
+    private static int sum(String[] intStrings) {
+        return Arrays.stream(intStrings)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
