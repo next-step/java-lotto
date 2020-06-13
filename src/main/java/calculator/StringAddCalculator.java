c86ead30861a76702aa8c7e959d664b8270f7776
DominiqN <dmut7691@gmail.com>
2020-06-13 16:01:18 +0900

    fix: pass the test

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,11 +1,19 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public static int calculate(String expression) {
         if (isNullOrEmpty(expression)) {
             return 0;
         }
-        return Integer.parseInt(expression);
+        try {
+            return Integer.parseInt(expression);
+        } catch (NumberFormatException e) {
+            String[] a = expression.split(",");
+            return Arrays.stream(a).map(Integer::parseInt).reduce(Integer::sum).get();
+        }
+
     }
 
     private static boolean isNullOrEmpty(String string) {
