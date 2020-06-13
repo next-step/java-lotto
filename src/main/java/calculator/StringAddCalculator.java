de6e74b873fb4267d427736410abbeffe08cf019
DominiqN <dmut7691@gmail.com>
2020-06-13 15:37:54 +0900

    feat: make Calculator return a single int when put a single int string

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,6 +2,12 @@ package calculator;
 
 public class StringAddCalculator {
     public static int calculate(String expression) {
-        return 0;
+        if (expression == null) {
+            return 0;
+        }
+        if (expression == "") {
+            return 0;
+        }
+        return Integer.parseInt(expression);
     }
 }
