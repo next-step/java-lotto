cff9417e30e77fad74ef16064ef7b2ecf9fc5694
neojjc <neo.jjc@gmail.com>
2020-06-14 12:20:37 +0900

    [fix#1] 세미콜론 위치 조정할 것

--- src/main/java/camp/nextstep/edu/nextstep8/lotto/LottoAward.java
+++ src/main/java/camp/nextstep/edu/nextstep8/lotto/LottoAward.java
@@ -6,8 +6,7 @@ public enum LottoAward {
     FIRST(6, 2000000000),
     SECOND(5, 1500000),
     THIRD(4, 50000),
-    FOURTH(3, 5000)
-    ;
+    FOURTH(3, 5000);
 
     private int condition;
     private int prize;
