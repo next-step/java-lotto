0579382cb44352654ffd421ef862e31c4e9527d4
Jun <p_illusion@naver.com>
2020-06-15 01:04:27 +0900

    [feat] 테스트명 변경

--- src/test/java/step2/model/LottoTest.java
+++ src/test/java/step2/model/LottoTest.java
@@ -20,7 +20,7 @@ class LottoTest {
     @ParameterizedTest
     @CsvSource(value={"1,2,10,20,30,40:2", "1,2,3,4,5,6:6", "1,20,2,40,3,44:3"}, delimiter = ':')
     @DisplayName("구입한 로또 대비 당첨된 개수")
-    void checkWinningCount(String input, int result) {
+    void 로또_당첨_개수(String input, int result) {
         List<LottoNumber> testNumbers = new ArrayList<>();
         testNumbers.add(LottoNumber.of(1));
         testNumbers.add(LottoNumber.of(2));
@@ -40,7 +40,7 @@ class LottoTest {
 
     @Test
     @DisplayName("입력한 숫자를 포함하는지 여부")
-    void checkContainNumber() {
+    void 입력한_숫자_포함여부() {
         String inputNumber = "1,2,3,4,5,6";
         assertThat(Lotto.of(LottoMakeNumbers.convertStringToNumbers(inputNumber)).has(LottoNumber.of(1))).isTrue();
         assertThat(Lotto.of(LottoMakeNumbers.convertStringToNumbers(inputNumber)).has(LottoNumber.of(2))).isTrue();
@@ -56,14 +56,14 @@ class LottoTest {
     @ParameterizedTest
     @ValueSource( strings = { "-1,2,3,4,5,6", "0,1,2,3,4,5", "1,2,3,4,5,46" })
     @DisplayName("1~45를 벗어나는 숫자 입력")
-    void checkNumber(String lottoNumbers) {
+    void 범위_벗어난는_숫자(String lottoNumbers) {
         assertThatExceptionOfType(LottoOutOfBoundException.class).isThrownBy(() -> Lotto.of(LottoMakeNumbers.convertStringToNumbers(lottoNumbers)));
     }
 
     @ParameterizedTest
     @ValueSource( strings = { "1,2,3,4,5", "1,2,3,4,5,6,7", "1,2,3,4,5,5" })
-    @DisplayName("중곱된 숫자 및 6개가 아닌 개수 입력")
-    void checkSize(String lottoNumbers) {
+    @DisplayName("중첩된 숫자 및 6개가 아닌 개수 입력")
+    void 중첩_6개_검사(String lottoNumbers) {
         assertThatExceptionOfType(LottoLengthException.class).isThrownBy(() -> Lotto.of(LottoMakeNumbers.convertStringToNumbers(lottoNumbers)));
     }
 }
