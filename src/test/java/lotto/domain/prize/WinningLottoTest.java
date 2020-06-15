f044a7b236e80c142602034f59fe2ea6fdfb46e2
eunji <jeeell.922@gmail.com>
2020-06-15 22:53:27 +0900

    refactor 메서드명 변경에 따른 수정

--- src/test/java/lotto/domain/prize/WinningLottoTest.java
+++ src/test/java/lotto/domain/prize/WinningLottoTest.java
@@ -1,22 +1,12 @@
 package lotto.domain.prize;
 
-import lotto.domain.lotto.LottoNumber;
-import lotto.domain.lotto.LottoNumbers;
-import lotto.domain.lotto.LottoTicket;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class WinningLottoTest {
-    List<LottoNumber> lottoNumber = Arrays.asList(1, 2, 3, 4, 5, 6).stream()
-            .map(x -> LottoNumber.create(x))
-            .collect(Collectors.toList());
     String enteredWinNumber = "1, 2, 3, 4, 5, 6";
-
 
     @Test
     void 생성_테스트() {
@@ -28,17 +18,6 @@ class WinningLottoTest {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> WinningLotto.create(enteredWinNumber, 6)
         ).withMessageContaining("보너스 볼이 당첨번호와 중복됩니다.");
-    }
-
-    @Test
-    void 당첨결과를_생성한다() {
-        LottoTicket lottoTicket = LottoTicket.createOne(Arrays.asList(LottoNumbers.create(lottoNumber)));
-        WinningLotto winningLotto = WinningLotto.create(enteredWinNumber, 10);
-
-        WinningResult winningResult = winningLotto.makeWinningResult(lottoTicket);
-        int winCount = winningResult.tellWinningCount(Rank.FIRST);
-
-        assertThat(winCount).isEqualTo(1);
     }
 
 }
