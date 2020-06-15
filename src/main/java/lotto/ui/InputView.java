0ede3601907fc37c75936ba359b098e7a97cc000
eunji <jeeell.922@gmail.com>
2020-06-15 22:34:43 +0900

    feat View - 수동번호 입력 기능 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,6 +1,9 @@
 package lotto.ui;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
     public static final Scanner SCANNER = new Scanner(System.in);
@@ -8,20 +11,34 @@ public class InputView {
     private InputView() {}
 
     public static int enterPurchaseAmount() {
-        System.out.println("구입 금액을 입력해 주세요.");
+        System.out.println("\n구입 금액을 입력해 주세요.");
         int amount = SCANNER.nextInt();
         SCANNER.nextLine();
         return amount;
     }
 
+    public static int enterManualCount() {
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualCount = SCANNER.nextInt();
+        SCANNER.nextLine();
+        return manualCount;
+    }
+
+    public static List<String> enterManualNumber(int manualCount) {
+        System.out.println("\n수동으로 구매할 번호를 입력해 주세요.");
+        return Stream.generate(() -> SCANNER.nextLine())
+                .limit(manualCount)
+                .collect(Collectors.toList());
+    }
+
     public static String enterWinningNumber() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
         String winningNumber = SCANNER.nextLine();
         return winningNumber;
     }
 
     public static int enterBonusBall() {
-        System.out.println("보너스 볼을 입력해 주세요.");
+        System.out.println("\n보너스 볼을 입력해 주세요.");
         int bonusBall = SCANNER.nextInt();
         return bonusBall;
     }
