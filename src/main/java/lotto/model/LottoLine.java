8b4c4ba7ecef52df8508c439ca5c395155378d32
hanjae <hanjae.jea@gmail.com>
2020-06-15 09:59:32 +0900

    refactor int to LottoNo class

--- src/main/java/lotto/model/LottoLine.java
+++ src/main/java/lotto/model/LottoLine.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -10,35 +11,42 @@ public class LottoLine {
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_END_NUMBER = 45;
     private static final int LOTTO_CONTAINING_NUMBER = 6;
-    private int[] lineNumbers;
+    private List<LottoNo> lineNumbers;
 
     public LottoLine() {
         List<Integer> numbers = IntStream.rangeClosed(LOTTO_START_NUMBER, LOTTO_END_NUMBER)
                 .boxed().collect(Collectors.toList());
         Collections.shuffle(numbers);
-        this.lineNumbers = numbers.subList(0, LOTTO_CONTAINING_NUMBER).stream()
-                .sorted().mapToInt(i -> i).toArray();
+
+        lineNumbers = new ArrayList<>();
+        for (Integer i : numbers.subList(0, LOTTO_CONTAINING_NUMBER)) {
+            lineNumbers.add(new LottoNo(i));
+        }
     }
 
     public LottoLine(int... lineNumbers) {
         if (lineNumbers.length != LOTTO_CONTAINING_NUMBER) {
             throw new IllegalArgumentException("갯수가 잘못되었습니다");
         }
-        this.lineNumbers = lineNumbers;
+
+        this.lineNumbers = new ArrayList<>();
+        for (int i : lineNumbers) {
+            this.lineNumbers.add(new LottoNo(i));
+        }
     }
 
     public int getMatchingNumbers(LottoLine line) {
-        return (int) Arrays.stream(line.lineNumbers).filter(this::containNumber).count();
+        return (int) line.lineNumbers.stream().filter(this::containNumber).count();
     }
 
-    public boolean containNumber(int number) {
-        return Arrays.stream(lineNumbers).boxed().collect(Collectors.toList()).contains(number);
+    public boolean containNumber(LottoNo number) {
+        return lineNumbers.stream().collect(Collectors.toList()).contains(number);
     }
 
     public String getLineContent() {
         StringBuilder builder = new StringBuilder();
         builder.append("[");
-        builder.append(Arrays.stream(lineNumbers).mapToObj(Integer::toString)
+        builder.append(lineNumbers.stream().map(LottoNo::toString)
                 .collect(Collectors.joining(", ")));
         builder.append("]");
         return builder.toString();
