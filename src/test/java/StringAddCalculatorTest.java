f6273b459e623d65d5f9dc1803281dc6985490b2
bo0216 <bo216@naver.com>
2020-06-16 23:50:15 +0900

    test: 빈 문자열 또는 null값을 입력할 경우 테스트 추가

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -4,6 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringAddCalculatorTest {
     @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
