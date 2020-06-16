f1ba399fef7027ce0fb589bed34f65b06060b47b
yjs2952 <yjs2952@naver.com>
2020-06-16 21:48:27 +0900

    refactor: 로또 구입 검증 로직 LottoFactory 로 이동

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -28,14 +28,25 @@ public class LottoFactory {
         List<Lotto> lottos = new ArrayList<>();
 
         for (String numbers : purchaseLottoInput.getManualLottoNumbers()) {
-            lottos.add(new Lotto(StringParser.getParseNumbers(numbers)));
+            lottos.add(Lotto.of(StringParser.getParseNumbers(numbers)));
         }
         return lottos;
     }
 
     public static List<Lotto> createLottos(PurchaseLottoInput purchaseLottoInput){
+        validationCheck(purchaseLottoInput.getPurchasePrice(), purchaseLottoInput.getManualLottoNumbers());
         List<Lotto> autoLottos = createAutoLottos(purchaseLottoInput, new RandomNumberGeneratorStrategy());
         autoLottos.addAll(createManualLottos(purchaseLottoInput));
         return Collections.unmodifiableList(autoLottos);
+    }
+
+    private static void validationCheck(int purchasePrice, List<String> manualLottoNumbers) {
+        if (purchasePrice < LottoFactory.PRICE) {
+            throw new IllegalArgumentException("구입금액은 최소 1000원 이상입니다.");
+        }
+
+        if (manualLottoNumbers == null || manualLottoNumbers.size() > purchasePrice / LottoFactory.PRICE) {
+            throw new IllegalArgumentException("수동으로 구매할 로또 개수가 예산을 초과합니다.");
+        }
     }
 }
--- src/main/java/lotto/view/PurchaseLottoInput.java
+++ src/main/java/lotto/view/PurchaseLottoInput.java
@@ -9,7 +9,6 @@ public class PurchaseLottoInput {
     private final List<String> manualLottoNumbers;
 
     public PurchaseLottoInput(int purchasePrice, List<String> manualLottoNumbers) {
-        validationCheck(purchasePrice, manualLottoNumbers);
         this.purchasePrice = purchasePrice;
         this.manualLottoNumbers = manualLottoNumbers;
     }
@@ -20,16 +19,6 @@ public class PurchaseLottoInput {
 
     public List<String> getManualLottoNumbers() {
         return manualLottoNumbers;
-    }
-
-    private void validationCheck(int purchasePrice, List<String> manualLottoNumbers) {
-        if (purchasePrice < LottoFactory.PRICE) {
-            throw new IllegalArgumentException("구입금액은 최소 1000원 이상입니다.");
-        }
-
-        if (manualLottoNumbers == null || manualLottoNumbers.size() > purchasePrice / LottoFactory.PRICE) {
-            throw new IllegalArgumentException("수동으로 구매할 로또 개수가 예산을 초과합니다.");
-        }
     }
 
     public int getManualSize(){
