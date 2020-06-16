5d39820cc54509b5f2900a6ca4d73ba5518e1104
Jay <bluehoped@hanmail.net>
2020-06-16 23:16:56 +0900

    feat (ResultView) 수정
    내용
    - yield 계산 역할 삭제

--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -5,15 +5,18 @@ import step3.Statistics;
 import step3.WinningStatistics;
 import step3.util.MathUtils;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
     private static final int LOTTO_PRICE = 1000;
-    private int lottoTicketCount;
+    List<LottoTicket> lottoTicketList;
 
     public ResultView(List<LottoTicket> lottoTicketList){
-        this.lottoTicketCount = lottoTicketList.size();
-        printMessage(lottoTicketCount + "개를 구매했습니다.");
+        this.lottoTicketList = lottoTicketList;
+        printMessage(lottoTicketList.size() + "개를 구매했습니다.");
         for (LottoTicket lottoTicket : lottoTicketList) {
             printMessage(lottoTicket.getNumbers().toString());
         }
@@ -25,9 +28,9 @@ public class ResultView {
 
         printStatistics(statistics);
 
-        double yield = MathUtils.calculateYield(statistics.getTotalPrice(), lottoTicketCount * LOTTO_PRICE);
-        yield = MathUtils.floorDecimal(yield, 2);
-        String message = "총 수익률은 " + yield + "입니다.";
+        double yield = statistics.calculateYield(lottoTicketList.size());
+
+        String message = "총 수익률은 " + MathUtils.floorDecimal(yield, 2) + "입니다.";
         if (yield < 1) {
             message += "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
         }
@@ -36,11 +39,14 @@ public class ResultView {
     }
 
     private void printStatistics(Statistics result) {
-        result.getStatistics().forEach((key, value) -> {
-            WinningStatistics winning = WinningStatistics.valueOfWinningCount(key);
-            String message = winning.getMatchedNumberCount() + "개 일치 (" + winning.getWinningMoney() + "원)- " + value +"개";
-            printMessage(message);
-        });
+        Map<WinningStatistics, Integer> resultMap = result.getStatistics();
+        Arrays.stream(WinningStatistics.values()).
+                filter(e -> e.getMatchedNumberCount() > 0).
+                sorted(Comparator.comparingInt(WinningStatistics::getMatchedNumberCount)).
+                forEach(key -> {
+                    String message = key.getMatchedNumberCount() + "개 일치 (" + key.getWinningMoney() + "원)- " + resultMap.get(key) + "개";
+                    printMessage(message);
+                });
     }
 
     private void printMessage(String message) {
