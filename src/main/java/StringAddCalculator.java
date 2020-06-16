af4e18983ca14849f621da30c74c75b6aacb157b
bo0216 <bo216@naver.com>
2020-06-16 23:45:34 +0900

    feat: 쉼표 구분자를 통해 덧셈 기능 구현

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,11 @@
+public class StringAddCalculator {
+    public static int splitAndSum(String text) {
+        int sum = 0;
+        String[] numbers = text.split(",");
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+}
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringAddCalculatorTest {
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
-        assertTaht(result).isEqualTo(6);
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
     }
 }
