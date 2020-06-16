54ef7ba100ddc33635ef01679785b54208c93b1c
bluehoped <bluehoped@hanmail.net>
2020-06-16 23:25:37 +0900

    fix (MathUtilsTest) import 변경

--- src/test/java/step3/MathUtilsTest.java
+++ src/test/java/step3/MathUtilsTest.java
@@ -3,7 +3,7 @@ package step3;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step2.util.MathUtils;
+import step3.util.MathUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
