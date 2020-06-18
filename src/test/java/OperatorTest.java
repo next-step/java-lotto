9fe912b52b23cf332042dee8f544025e163a9fa1
bo0216 <bo216@naver.com>
2020-06-19 00:59:47 +0900

    test: 구분자와 숫자 분리하기 테스트 추가

--- src/test/java/OperatorTest.java
+++ src/test/java/OperatorTest.java
@@ -1,15 +1,22 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OperatorTest {
     @DisplayName("구분자, 숫자 분리하기")
-    @ParameterizedTest
-    @CsvSource(value = {"1,2:3", "//;\\n1;2;3"})
-    public void matchDelimiterTest(String text) {
+    @Test
+    public void matchDelimiterTest() {
+        String value = "1,2:3";
         String[] expected = {"1", "2", "3"};
-        assertThat(Operator.matchDelimiter(text)).isEqualTo(expected);
+        assertThat(Operator.matchDelimiter(value)).isEqualTo(expected);
+    }
+
+    @DisplayName("커스텀 구분자, 숫자 분리하기")
+    @Test
+    public void matchCustomDelimiterTest() {
+        String value = "//;\n1;2;3";
+        String[] expected = {"1", "2", "3"};
+        assertThat(Operator.matchDelimiter(value)).isEqualTo(expected);
     }
 }
