a7cd9fbddbe5efa26946a687914ed10951db9eaf
Jun <p_illusion@naver.com>
2020-06-20 20:51:39 +0900

    [feat] 로또 게임 수동번호 추가

--- src/main/java/step2/model/LottoGame.java
+++ src/main/java/step2/model/LottoGame.java
@@ -1,34 +1,51 @@
 package step2.model;
 
+import step2.exception.LottoOutOfSelfLottoException;
 import step2.util.LottoMakeNumbers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class LottoGame {
-
     private Lottos lottos;
-
+    private Lottos lottosSelf;
     private LottoMoney lottoMoney;
 
-    public LottoGame(LottoMoney lottoMoney) {
+    public LottoGame(LottoMoney lottoMoney, Lottos lottosSelf) {
         this.lottoMoney = lottoMoney;
+        this.lottosSelf = lottosSelf;
+
+        validateLottoGame();
+
         this.lottos = createRandomLotto();
     }
 
     private Lottos createRandomLotto() {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < lottoMoney.getLottoCount(); i++) {
-            lottoList.add(LottoMakeNumbers.getRandomNumber());
-        }
-        return Lottos.of(lottoList);
+        Lottos lottos = Stream.generate(LottoMakeNumbers::getRandomNumber)
+                .limit(getLottoRandomCount())
+                .collect(collectingAndThen(toList(), Lottos::of));
+
+        lottos.addLottos(lottosSelf);
+        return lottos;
     }
 
-    public int getLottoCount() {
-        return lottoMoney.getLottoCount();
+    public int getLottoRandomCount() {
+        return lottoMoney.getLottoCount() - lottosSelf.getCount();
+    }
+
+    public int getLottoSelfCount() {
+        return lottosSelf.getCount();
     }
 
     public Lottos getLottos() {
         return lottos;
+    }
+
+    private void validateLottoGame() {
+        if(lottoMoney.getLottoCount() < lottosSelf.getCount()) {
+            throw new LottoOutOfSelfLottoException();
+        }
     }
 }
