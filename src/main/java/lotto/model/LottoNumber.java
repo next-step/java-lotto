775c97bef970a22d21510d93299c126a1a8c5c85
y00s22 <ryusuy22@gmail.com>
2020-06-20 15:53:25 +0900

    refactoring) LottoNumbers) 번호 중복을 방지하기 위해 번호 지정을 LottoNumber가 아닌 LottoNumbers에서 시행, 사용되지 않는 메소드 삭제

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,15 +1,7 @@
 package lotto.model;
 
-import java.util.Collections;
-
 public class LottoNumber {
     private int number;
-
-    public LottoNumber() {
-        Lotto lotto = new Lotto();
-        Collections.shuffle(lotto.lottoDrawNumbers);
-        number = lotto.lottoDrawNumbers.get(0);
-    }
 
     public LottoNumber(int number) {
         this.number = number;
