e91a5dac636335d51e45b54854b0c616944a356a
sangyun <ksang91@naver.com>
2020-06-20 21:44:16 +0900

    Step1 (#678)
    * style : 함수 이름 변경.
    
    * -feat : 문자열 계산기 구현.
    
    Co-authored-by: sangyounkim <한소위505>

---
+++ src/test/java/StringAddCalculatorTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+public class StringAddCalculatorTest {
+    @Test
+    public void splitAndSum_null_or_empty() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_One_Number() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_toggleByRest() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_toggleByRestOrColon() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_separator() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
