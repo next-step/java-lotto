bcd706653bde73b6f00168c8a35f895dd86887bc
sangz1888 <sangz1888@gmail.com>
2020-06-21 09:26:54 +0900

    <feat>(edu.nextstep.lotto): 로또 여러개 생성, 당첨번호 확인 로직

--- src/main/java/edu/nextstep/lotto/LottoController.java
+++ src/main/java/edu/nextstep/lotto/LottoController.java
@@ -1,4 +1,34 @@
 package edu.nextstep.lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
+    private List lottoList;
+    private Lotto lotto;
+
+    LottoController() {
+        lottoList = new ArrayList();
+        lotto = new Lotto();
+    }
+
+    public void setup() {
+        ResultView.print("구입금앱을 입력해 주세요");
+        int count = ResultView.lottoCount(InputView.inputMoney());
+
+        for (int i = 0; i < count; i++) {
+            lottoList.add(lotto.makeLotto());
+        }
+
+        ResultView.print("지난 주 당첨 번호를 입력해 주세요.");
+        String[] winNumber = InputView.winNumber();
+
+        ResultView.print("당첨 통계");
+        ResultView.print("--------");
+
+        for (int i = 0; i < lottoList.size(); i++) {
+            ((Lotto)lottoList.get(i)).checkLotto(winNumber);
+        }
+
+    }
 }
