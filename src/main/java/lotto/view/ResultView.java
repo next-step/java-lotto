af3872630aac59e03da2f9f60e480bbbecfeb58a
wyh951753 <wyh951753@gmail.com>
2020-06-22 02:41:32 +0900

    Refactor - 로또 자체의 계산로직과 출력의 계산 로직을 분리

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,30 +1,58 @@
 package lotto.view;
 
-import lotto.domain.Prize;
+import lotto.domain.LottoTicket;
+import lotto.domain.Rank;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ResultView {
+public final class ResultView {
+    private static final int MINIMUM_MONEY_TO_PRINT = 0;
 
-    public static void printLottoResult(final List<Prize> prizes) {
-        System.out.println("당첨 통계");
-        System.out.println("--------------------------------------------");
-        System.out.println(getWinningString(Prize.FOURTH, getWinningCount(prizes, Prize.FOURTH)));
-        System.out.println(getWinningString(Prize.THIRD, getWinningCount(prizes, Prize.THIRD)));
-        System.out.println(getWinningString(Prize.SECOND, getWinningCount(prizes, Prize.SECOND)));
-        System.out.println(getWinningString(Prize.FIRST, getWinningCount(prizes, Prize.FIRST)));
+    private ResultView() {}
+
+    public static void printBuyingLotto(final int buyingLotto) {
+        System.out.println(buyingLotto + "개를 구매했습니다.");
     }
 
-    private static String getWinningString(final Prize prize, final int winningCount) {
-        return String.format("%d개 일치 (%d)- %d개", prize.getCountOfMatch(), prize.getWinningMoney(), winningCount);
-    }
-
-    private static int getWinningCount(final List<Prize> prizes, Prize prize) {
-        int count = 0;
-
-        if (prizes.stream().anyMatch((v) -> v.equals(prize))) {
-            count++;
+    public static void printLottoTickets(final List<LottoTicket> lottoTickets) {
+        for (LottoTicket lottoTicket : lottoTickets) {
+            final String lottoTicketResultString = lottoTicket.getLottoNumbers().stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(","));
+            System.out.println(String.format("[%s]", lottoTicketResultString));
         }
-        return count;
     }
+
+    public static void printRankResult(final List<Rank> lottoResults) {
+        System.out.println("당첨 통계");
+        System.out.println("--------------------------------------");
+
+        Arrays.stream(Rank.values())
+                .filter((rank) -> rank.getWinningMoney() > MINIMUM_MONEY_TO_PRINT)
+                .forEach((rank) -> {
+                    final int countOfRank = getCountOfRank(lottoResults, rank);
+                    System.out.println(getRankString(rank, countOfRank));
+                });
+    }
+
+    private static int getCountOfRank(final List<Rank> lottoResult, Rank rank) {
+        return (int) lottoResult.stream()
+                .filter(result -> result == rank)
+                .count();
+    }
+
+    private static String getRankString(final Rank rank, final int countOfRank) {
+        return String.format("%d개 일치 (%d원) - (%d)개", rank.getCountOfMatch(), rank.getWinningMoney(), countOfRank);
+    }
+
+    public static void printTotalEarningRate(final List<Rank> lottoResults, BigDecimal payMoney) {
+        final long winningMoney = lottoResults.stream().mapToLong(Rank::getWinningMoney).sum();
+
+        System.out.println(String.format("총 수익률은 %.2f입니다.",
+                BigDecimal.valueOf(winningMoney).divide(payMoney, 3, BigDecimal.ROUND_UP)));
+    }
+
 }
