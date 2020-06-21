13b94394ae3adea3d279c7e4fcdad030cdd4cd99
sangz1888 <sangz1888@gmail.com>
2020-06-21 09:25:47 +0900

    <feat>(edu.nextstep.lotto): lotto 도메인 구현

--- src/main/java/edu/nextstep/lotto/Lotto.java
+++ src/main/java/edu/nextstep/lotto/Lotto.java
@@ -1,4 +1,36 @@
 package edu.nextstep.lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Lotto {
+    private static final int maxNumber = 50;
+    private List lotto;
+    private int winNumCount;
+
+    Lotto() {
+        lotto = new ArrayList();
+        this.winNumCount = 0;
+    }
+
+    public List makeLotto() {
+        for (int i = 1; i <= maxNumber; i++) {
+            lotto.add(i);
+        }
+
+        Collections.shuffle(lotto);
+        return lotto.subList(0, 5);
+    }
+
+    public int checkLotto(String[] winNumber) {
+        for (int i = 0; i < winNumber.length; i++) {
+            if (lotto.contains(Integer.valueOf(winNumber[0]))) {
+                this.winNumCount++;
+            }
+        }
+
+        return this.winNumCount;
+    }
+
 }
--- src/test/java/edu/nextstep/lotto/LottoTest.java
+++ src/test/java/edu/nextstep/lotto/LottoTest.java
@@ -1,4 +1,25 @@
 package edu.nextstep.lotto;
 
-public class Lotto {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LottoTest {
+    private Lotto lotto;
+
+    @DisplayName("로또 생성 테스트 준비")
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto();
+    }
+
+    @DisplayName("로또 1개 생성")
+    @Test
+    void generatorTest() {
+        List lottery = lotto.makeLotto();
+        ResultView.printLotto(lottery);
+    }
+
 }
