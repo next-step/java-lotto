bce6c52ea81f7b88e587d024512e4347251e7c6b
impactrudia <impactyoung@gmail.com>
2020-06-25 01:20:45 +0900

    refactor : 로또 생성 stream 이용

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,6 +1,5 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -12,11 +11,12 @@ public class LottoGenerator {
     private static final int LOTTERY_SHUFFLE_END_NUMBER = 6;
     private static final int LOTTERY_RANDOM_START_NUM = 1;
     private static final int LOTTERY_RANDOM_END_NUM = 46;
+    private static final int RANGE_START_NUMBER = 0;
 
     public static Lotto generateAuto() {
         List<Integer> seed = createSeed();
         Collections.shuffle(seed);
-        return Lotto.of(new HashSet(seed.subList(LOTTERY_SHUFFLE_START_NUMBER, LOTTERY_SHUFFLE_END_NUMBER)));
+        return Lotto.of(new HashSet<>(seed.subList(LOTTERY_SHUFFLE_START_NUMBER, LOTTERY_SHUFFLE_END_NUMBER)));
     }
 
     private static List<Integer> createSeed() {
@@ -24,11 +24,11 @@ public class LottoGenerator {
     }
 
     public static Lottos generateByMoney(Money money) {
-        List<Lotto> lottos = new ArrayList<>();
-        int moneyCount = money.lotteryCount();
-        for (int i = 0; i < moneyCount; i++) {
-            lottos.add(generateAuto());
-        }
-        return new Lottos(lottos);
+        return new Lottos(
+                IntStream
+                        .range(RANGE_START_NUMBER, money.lotteryCount())
+                        .mapToObj(x -> generateAuto())
+                        .collect(Collectors.toList())
+        );
     }
 }
