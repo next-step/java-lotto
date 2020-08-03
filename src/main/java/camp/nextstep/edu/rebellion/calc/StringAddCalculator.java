02be98d262b4ffafed4c3662c87bc8bf9f99f259
neojjc2 <neo.jjc+2@gmail.com>
2020-08-03 17:20:16 +0900

    [feat][Step1] 테스트 코드 작성 및 불필요한 파일 제거

---
+++ src/main/java/camp/nextstep/edu/rebellion/calc/StringAddCalculator.java
@@ -0,0 +1,9 @@
+package camp.nextstep.edu.rebellion.calc;
+
+public class StringAddCalculator {
+    private StringAddCalculator() {}
+
+    public static int splitAndSum(String expression) {
+        return 0;
+    }
+}
