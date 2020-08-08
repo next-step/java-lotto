c1890304b03fa69e378f8f92ed20524d48356e4d
Sang Uk Ahn <suahnn@gmail.com>
2020-08-09 00:19:31 +0900

    feat: LottoGame, LottoResult 구현

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoResult {
+
+    private final Map<LottoRanking, Integer> rankingMap = new HashMap<>();
+    private int totalPrize = 0;
+
+    public void addRanking(LottoRanking lottoRanking) {
+        rankingMap.put(lottoRanking, rankingMap.getOrDefault(lottoRanking, 0) + 1);
+        totalPrize += lottoRanking.getPrize();
+    }
+
+    public Map<LottoRanking, Integer> getRankingMap() {
+        return Collections.unmodifiableMap(rankingMap);
+    }
+
+    public int getTotalPrize() {
+        return totalPrize;
+    }
+}
