f8c6949501cfbb6581ef0fb632f9bdeffd182161
Sang Uk Ahn <suahnn@gmail.com>
2020-08-09 00:19:13 +0900

    test: LottoGame, LottoResult 테스트클래스 구현

---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoResultTest {
+
+    @Test
+    @DisplayName("당첨결과 매칭 시 전체상금 추가")
+    void getTotalPrize_addMatch_returnSum() {
+        // given
+        LottoResult result = new LottoResult();
+        // when
+        result.addRanking(LottoRanking.THIRD);
+        // then
+        assertThat(result.getTotalPrize()).isEqualTo(LottoRanking.THIRD.getPrize());
+    }
+}
