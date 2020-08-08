8f0d7422a624efbc555823416aea182eecc040cd
Sang Uk Ahn <suahnn@gmail.com>
2020-08-09 00:16:51 +0900

    feat: Ranking 추가

---
+++ src/main/java/lotto/domain/LottoRanking.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public enum LottoRanking {
+
+    FOURTH(3, 5000),
+    THIRD(4, 50000),
+    SECOND(5, 1500000),
+    FIRST(6, 2000000000);
+
+    private final int matchCount;
+    private final int prize;
+
+    LottoRanking(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
