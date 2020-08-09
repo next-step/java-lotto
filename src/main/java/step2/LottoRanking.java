a7056bb9103451a8d36f8f1bb0e15232d841d917
yongdae <7942yongdae@gmail.com>
2020-08-09 18:40:34 +0900

    feat: 메인 클래스 추가

--- src/main/java/step2/LottoRanking.java
+++ src/main/java/step2/LottoRanking.java
@@ -4,10 +4,10 @@ package step2;
  * 당첨 관련된 상수 정의 Enum
  */
 public enum LottoRanking {
-    FIRST(2000000000, 6),
-    SECOND(1500000, 5),
+    FOURTH(5000, 3),
     THIRD(50000, 4),
-    FOURTH(5000, 3);
+    SECOND(1500000, 5),
+    FIRST(2000000000, 6);
 
     // 당첨금
     private int prizeMoney;
@@ -31,4 +31,6 @@ public enum LottoRanking {
     public int getHitCount() {
         return hitCount;
     }
+
+
 }
---
+++ src/main/java/step2/Main.java
@@ -0,0 +1,39 @@
+package step2;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        TicketSellingMachine ticketSellingMachine = new TicketSellingMachine();
+
+        int spendingMoney = InputScanner.getInt("구입금액을 입력해 주세요.");
+
+        List<Ticket> tickets = ticketSellingMachine.buy(spendingMoney);
+        PrintMessage.print("%s개를 구매했습니다.\n", tickets.size());
+        PrintMessage.println();
+
+        tickets.stream()
+                .forEach(ticket -> System.out.println(ticket));
+
+        PrintMessage.println();
+        int[] winningNumber = InputScanner.getInts("지난 주 당첨 번호를 입력해 주세요.");
+
+        PrintMessage.println();
+        PrintMessage.println("당첨 통계");
+        PrintMessage.println("---------");
+
+        for (LottoRanking ranking : LottoRanking.values()) {
+            PrintMessage.print(
+                    "%s개 일치 (%,d원)- %s개\n",
+                    ranking.getHitCount(),
+                    ranking.getPrizeMoney(),
+                    ResultAnalyzer.getHitListCount(winningNumber, tickets, ranking.getHitCount())
+            );
+        }
+
+        PrintMessage.print(
+                "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n",
+                ResultAnalyzer.getRateReturn(winningNumber, tickets, spendingMoney)
+        );
+    }
+}
