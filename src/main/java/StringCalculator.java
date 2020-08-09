96cb7ed059d5f014787bf74233f05478ad97d01d
ohjuntaek <201211734@pukyong.ac.kr>
2020-08-09 16:40:58 +0900

    feat: 커스텀 구분자를 추출한다

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public int calculate(String input, String delimiter) {
@@ -7,5 +10,13 @@ public class StringCalculator {
         return Arrays.stream(split)
                 .map(Integer::parseInt)
                 .reduce(Integer::sum).orElseThrow(RuntimeException::new);
+    }
+
+    public Optional<String> getDelimiter(String input) {
+        Matcher m = Pattern.compile("//(.)\\\\n(.*)").matcher(input);
+        if (m.find()) {
+            return Optional.ofNullable(m.group(1));
+        }
+        return Optional.empty();
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,8 +3,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
@@ -16,7 +21,7 @@ class StringCalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3;6", "2,3,4;9"}, delimiter = ';')
+    @CsvSource(value = {"1,2,3=6", "2,3,4=9"}, delimiter = '=')
     @DisplayName("쉼표가 구분자일 때 덧셈 결과 값을 반환한다.")
     void plusTest1(String input, int expected) {
         int result = calculator.calculate(input, ",");
@@ -25,12 +30,21 @@ class StringCalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1:2:3;6", "2:3:4;9"}, delimiter = ';')
+    @CsvSource(value = {"1:2:3=6", "2:3:4=9"}, delimiter = '=')
     @DisplayName("콜론이 구분자일 때 덧셈 결과 값을 반환한다.")
     void plusTest2(String input, int expected) {
         int result = calculator.calculate(input, ":");
 
         assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"//;\\n1;2;3=;", "//^\\n1^2^3=^"}, delimiter = '=')
+    @DisplayName("커스텀 구분자를 추출한다")
+    void getDelimiter(String input, String expected) {
+        String delimiter = calculator.getDelimiter(input).orElse(",");
+
+        assertThat(delimiter).isEqualTo(expected);
     }
 
 }
