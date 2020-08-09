6b317b4b38e8c46530d99f38f1f1068b88c08276
dhmin5693 <dhmin5693@naver.com>
2020-08-09 16:21:41 +0900

    Feat: 문자열 가산기, 문자열 가산기 테스트 추가

---
+++ src/main/java/adder/StringAdder.java
@@ -0,0 +1,55 @@
+package adder;
+
+import java.util.Arrays;
+
+public class StringAdder {
+
+    public static int splitAndSum(String numberStr) {
+
+        if (isEmptyNumbers(numberStr)) {
+            return 0;
+        }
+
+        return sum(split(numberStr, makeSeparator(numberStr)));
+    }
+
+    private static boolean isEmptyNumbers(String numbers) {
+        return numbers == null || numbers.isEmpty();
+    }
+
+    private static String makeSeparator(String numberStr) {
+
+        StringBuilder separator = new StringBuilder("[,:");
+
+        if (numberStr.startsWith("//")) {
+            separator.append(numberStr, 2, numberStr.indexOf("\n"));
+        }
+
+        return separator.append("]")
+                .toString();
+    }
+
+    private static String[] split(String numberStr, String separator) {
+        if (numberStr.startsWith("//")) {
+            numberStr = numberStr.substring(numberStr.indexOf("\n") + 1);
+        }
+        return numberStr.split(separator);
+    }
+
+    private static int sum(String[] numbers) {
+
+        return Arrays.stream(numbers)
+                .mapToInt(number -> {
+                    try {
+                        int num = Integer.parseInt(number);
+                        if (num < 0) {
+                            throw new RuntimeException("음수를 넣지 마세요");
+                        }
+                        return num;
+                    } catch (NumberFormatException e) {
+                        throw new RuntimeException("숫자만 넣어주세요.");
+                    }
+                })
+                .sum();
+    }
+}
---
+++ src/test/java/adder/StringAdderTest.java
@@ -0,0 +1,48 @@
+package adder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAdderTest {
+
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAdder.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAdder.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAdder.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAdder.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAdder.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAdder.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAdder.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
