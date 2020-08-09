a2af4ac1ebd53920fd50712c56e8e90e6b1abd1f
yongdae <7942yongdae@gmail.com>
2020-08-09 14:18:23 +0900

    feat: 로또 구매 기능 정의 및 추가

---
+++ src/main/java/step2/TicketSellingMachine.java
@@ -0,0 +1,38 @@
+package step2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 로또를 구매 할 수 있는 기능을 정의
+ */
+public class TicketSellingMachine {
+
+    /**
+     * 장당 티켓의 가격
+     */
+    private static final int TICKET_PRICE = 1000;
+
+    /**
+     * 금액을 기준으로 티켓을 발권한다.
+     * 티켓 한장의 가격은 1000원이다.
+     * @param money
+     * @return
+     */
+    public static final List<Ticket> buy(final int money) {
+        return Stream.generate(Ticket::new)
+                .limit(getCount(money))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 금액을 기준으로 발권할 수 있는 티켓의 갯수를 반환한다.
+     *
+     * @param money
+     * @return
+     */
+    private static final int getCount(final int money) {
+        return money / TICKET_PRICE;
+    }
+}
---
+++ src/test/java/step2/TicketSellingMachineTest.java
@@ -0,0 +1,18 @@
+package step2;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TicketSellingMachineTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"900=0", "1000=1", "10000=10"}, delimiter = '=')
+    void buy(int money, int ticketCount) {
+        List<Ticket> tickets = TicketSellingMachine.buy(money);
+        assertThat(tickets.size()).isEqualTo(ticketCount);
+    }
+}
