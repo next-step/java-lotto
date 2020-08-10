15e996045e13b6927ef2334da39342430e6281d8
yongdae <7942yongdae@gmail.com>
2020-08-11 00:37:38 +0900

    refactor: 번호 생성 방식 변경

--- src/main/java/step2/domain/TicketSellingMachine.java
+++ src/main/java/step2/domain/TicketSellingMachine.java
@@ -1,10 +1,9 @@
 package step2.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -12,25 +11,29 @@ import java.util.stream.Stream;
  */
 public class TicketSellingMachine {
 
-    private static final Random DICE = new Random();
-
-    /**
-     * 번호 갯수
-     */
-    private static final int NUMBER_COUNT = 6;
-
     /**
      * 무작위로 정의되는 번호의 범위
      */
     private static final int NUMBER_BOUNDARY = 45;
 
     /**
+     * 번호 갯수
+     */
+    private static final int NUMBER_SIZE = 6;
+
+    /**
+     * 번호 목록
+     */
+    private static final List<Integer> COLLECT = IntStream.rangeClosed(1, NUMBER_BOUNDARY)
+            .boxed()
+            .collect(Collectors.toList());
+
+    /**
      * 장당 티켓의 가격
      */
     private static final int TICKET_PRICE = 1000;
 
-    private TicketSellingMachine() {
-    }
+    private TicketSellingMachine() {}
 
     /**
      * 금액을 기준으로 티켓을 발권한다.
@@ -60,63 +63,13 @@ public class TicketSellingMachine {
      * @return
      */
     private static final int[] createNumbers() {
-        List<Integer> result = new ArrayList<>();
-
-        while (result.size() < NUMBER_COUNT) {
-            result.add(nextNumber(result));
-        }
-
         // 생성된 번호 목록을 섞는다.
-        Collections.shuffle(result);
+        Collections.shuffle(COLLECT);
 
-        return result.stream()
+        return COLLECT.stream()
+                .limit(NUMBER_SIZE)
                 .mapToInt(Integer::intValue)
                 .toArray();
-    }
-
-
-    /**
-     * numbers 목록을 기준으로 중복되지 않는 숫자를 반환한다.
-     * 숫자는 nextInt() 함수를 통해 생성한다. (숫자 범위 1 ~ 45)
-     *
-     * @param numbers
-     * @return
-     */
-    private static final int nextNumber(final List<Integer> numbers) {
-        int result = nextInt(NUMBER_BOUNDARY);
-        boolean duplicated = checkDuplicate(numbers, result);
-
-        while (duplicated) {
-            result = nextInt(NUMBER_BOUNDARY);
-            duplicated = checkDuplicate(numbers, result);
-        }
-
-        return result;
-    }
-
-    /**
-     * 숫자를 반환한다.
-     * 숫자 범위 1 ~ 45
-     *
-     * @param bound
-     * @return
-     */
-    private static final int nextInt(final int bound) {
-        return DICE.nextInt(bound) + 1;
-    }
-
-    /**
-     * 숫자 목록과 숫자를 비교하여 중복된 숫자 여부를 판별한다.
-     *
-     * @param numbers
-     * @param number
-     * @return
-     */
-    private static final boolean checkDuplicate(final List<Integer> numbers, final int number) {
-        return numbers.stream()
-                .filter(value -> value == number)
-                .findFirst()
-                .isPresent();
     }
 
     /**
