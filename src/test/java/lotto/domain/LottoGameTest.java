977faad92886b72c1a2e6d9e551df71baad69f74
monds <suahnn@gmail.com>
2020-08-10 22:09:51 +0900

    test: 보너스볼 테스트케이스 추가

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,18 +13,38 @@ public class LottoGameTest {
 
     @Test
     @DisplayName("특정 개수 매칭 결과 확인")
-    void match() {
+    void match_without_bonus() {
         // given
-        Lotto winningLotto = Lotto.fromString("16,22,33,41,45,46");
+        Lotto winningLotto = Lotto.fromString("16,22,33,41,44,45");
+        LottoNumber bonusBall = LottoNumber.of(17);
         List<Lotto> lottos = new ArrayList<>();
-        lottos.add(Lotto.fromString("16,22,33,40,44,47"));
-        lottos.add(Lotto.fromString("16,22,33,39,44,47"));
+        lottos.add(Lotto.fromString("16,22,33,38,42,45"));
+        lottos.add(Lotto.fromString("16,22,33,39,43,45"));
 
         // when
-        LottoResult lottoResult = LottoGame.match(winningLotto, lottos);
+        LottoResult lottoResult = LottoGame.match(winningLotto, bonusBall, lottos);
+        Map<LottoRanking, Integer> rankingMap = lottoResult.getRankingMap();
 
         // then
-        assertThat(lottoResult.getTotalPrize()).isEqualTo(10000);
-        assertThat(lottoResult.getRankingMap().get(LottoRanking.FOURTH)).isEqualTo(2);
+        assertThat(lottoResult.getTotalPrize()).isEqualTo(100_000);
+        assertThat(rankingMap).containsKey(LottoRanking.THIRD);
+    }
+
+    @Test
+    @DisplayName("특정 개수 매칭 결과 확인 (보너스볼)")
+    void match_with_bonus() {
+        // given
+        Lotto winningLotto = Lotto.fromString("16,22,33,41,44,45");
+        LottoNumber bonusBall = LottoNumber.of(45);
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(Lotto.fromString("16,22,30,41,44,45"));
+        lottos.add(Lotto.fromString("6,7,12,13,14,25"));
+
+        // when
+        LottoResult lottoResult = LottoGame.match(winningLotto, bonusBall, lottos);
+
+        // then
+        assertThat(lottoResult.getTotalPrize()).isEqualTo(LottoRanking.BONUS.prize());
+        assertThat(lottoResult.getRankingMap().get(LottoRanking.BONUS)).isEqualTo(1);
     }
 }
