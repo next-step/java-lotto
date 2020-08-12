51fc3c22bd8f7ac4c34f37caa0bc52543329595f
sefide <sefide@naver.com>
2020-08-12 22:24:24 +0900

    docs: 구현 기능 및 구현할 테스트 목록 작성

---
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calculator = new Calculator();
+        Numbers num
+        calculator
+    }
+}
