d8fbbef9f417fa67b0c249d8cf7918ebdbe2f45a
heesunjeong <jhswit@naver.com>
2020-08-12 23:44:02 +0900

    feat: 문자열 계산기 음수 입력시 exception 발생, 빈 값 입력시 0 리턴

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -8,10 +8,18 @@ public class StringAddCalculator {
     }
 
     public static int calculate(String expression) {
+        if(ValidateUtil.validateEmptyExpression(expression)) {
+            return 0;
+        }
+
         List<Integer> numbers = SplitUtil.splitExpression(expression);
         int result = 0;
 
         for (Integer number : numbers) {
+            if(number < 0) {
+                throw new RuntimeException();
+            }
+
             result += number;
         }
 
