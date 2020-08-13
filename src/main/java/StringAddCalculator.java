3210019778337cf96f0132d7b5b178e45a0492a4
hojun Choi <hojunchoi@hotmail.co.kr>
2020-08-13 17:31:31 +0900

    음수 검증 코드 완성 검증 전용 util 분리 준비

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -6,9 +6,11 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
+        //input 검증
         if(StringUtils.isBlank(input)){
             return 0;
         }
+
         String[] tokens= getTokens(input);
         return add(tokens);
     }
@@ -16,21 +18,33 @@ public class StringAddCalculator {
     private static int add(String[] tokens) {
         int sum = 0;
         for (String token: tokens) {
-            sum += Integer.parseInt(token);
+            sum += convertInt(token);
         }
         return sum;
     }
 
     private static String[] getTokens(String input){
+
+        //input 검증
         Matcher m2 = Pattern.compile("//(.)\n(.*)").matcher(input);
         if (m2.find()) {
             String customDelimiter = m2.group(1);
             return m2.group(2).split(customDelimiter);
         }
 
+        //input 검증
         String pa = "([0-9 \\, \\: ])";
         if (Pattern.compile(pa).matcher(input).find())
             return input.split(",|:");
         return null;
+    }
+
+    //input 검증
+    private static int convertInt(String input){
+        int resunt = Integer.parseInt(input);
+        if (resunt < 0) {
+            throw new RuntimeException("음수는 지원하지 않습니다.");
+        }
+        return resunt;
     }
 }
---
+++ src/test/java/util/ValidatorUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+public class ValidatorUtil {
+
+    @Test
+    public void 커스텀_구분자_검증(){
+    }
+}
