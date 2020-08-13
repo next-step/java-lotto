42807a6c1f85e3ff1b7866d8c5ea1a5962695163
pcspapa <wldn@0605>
2020-08-13 23:26:13 +0900

    feat: 입력 금액에 의한 로또번호 생성
    Closes: #7

--- src/main/java/step2/LottoGenerator.java
+++ src/main/java/step2/LottoGenerator.java
@@ -7,10 +7,27 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoGenerator {
-  public static final List<Integer> NUMBER_POOL = Stream.iterate(1, n -> ++n).limit(45).collect(Collectors.toList());
+  public static final List<Integer> NUMBER_POOL =
+      Stream.iterate(1, n -> ++n).limit(45).collect(Collectors.toList());
 
   public static Lotto generate() {
     Collections.shuffle(NUMBER_POOL);
     return new Lotto(new ArrayList<>(NUMBER_POOL.subList(0, Lotto.FIXED_COUNT)));
+  }
+
+  public static List<Lotto> generate(int money) {
+    validateMoney(money);
+    List<Lotto> lottos = new ArrayList<>();
+    do {
+      lottos.add(generate());
+    } while (money - 1000 >= 1000);
+
+    return lottos;
+  }
+
+  private static void validateMoney(int money) {
+    if (money < 1000) {
+      throw new IllegalArgumentException("1000원 이상입력하세요.");
+    }
   }
 }
--- src/test/java/step2/LottoGeneratorTest.java
+++ src/test/java/step2/LottoGeneratorTest.java
@@ -1,6 +1,7 @@
 package step2;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,17 @@ class LottoGeneratorTest {
   @Test
   void duplicateLotto() {
     assertThat(LottoGenerator.generate()).isNotEqualTo(LottoGenerator.generate());
+  }
+
+  @Test
+  void noneGenerateByMoney() {
+    assertThatThrownBy(() -> LottoGenerator.generate(999).size())
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void generateByMoney() {
+    assertThat(LottoGenerator.generate(1000).size()).isEqualTo(1);
   }
 
 }
