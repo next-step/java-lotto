30539de93aa3324a9b08f605314442745eff84dd
Sang Uk Ahn <suahnn@gmail.com>
2020-08-14 23:36:12 +0900

    feat: 수동 기능 추가

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.text.MessageFormat;
+import java.util.function.Supplier;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -10,15 +11,65 @@ public class LottoShop {
 
     private static final String PRICE_ERROR_MESSAGE = "price must be at least zero";
     private static final String PRICE_UNIT_MESSAGE = "price {0} won unit";
+    private static final String NOT_ENOUGH_MONEY_MESSAGE = "There is not enough money";
+    private static final String COUNT_ERROR_MESSAGE = "count must be at least zero";
 
-    public static Lottos buyAuto(int purchasePrice) {
+    private final int purchasePrice;
+    private final Lottos lottos;
+    private int manualCount = 0;
+
+    public LottoShop(int purchasePrice) {
+        this.purchasePrice = purchasePrice;
+        lottos = new Lottos();
+    }
+
+    public static LottoShop purchasePrice(int purchasePrice) {
         checkArgument(purchasePrice > 0, PRICE_ERROR_MESSAGE);
         checkArgument(purchasePrice % DEFAULT_PRICE_UNIT == 0,
-            MessageFormat.format(PRICE_UNIT_MESSAGE, DEFAULT_PRICE_UNIT));
+                MessageFormat.format(PRICE_UNIT_MESSAGE, DEFAULT_PRICE_UNIT));
+        return new LottoShop(purchasePrice);
+    }
 
-        Lottos lottos = new Lottos();
-        lottos.generateAuto(purchasePrice / DEFAULT_PRICE_UNIT);
+    public LottoShop buyAuto() {
+        if (canBuyLotto()) {
+            lottos.generateAuto(canBuyLottoCount() - manualCount);
+        }
+        return this;
+    }
+
+    public LottoShop buyManual(int count, Supplier<Lotto> lottoSupplier) {
+        checkArgument(count >= 0, COUNT_ERROR_MESSAGE);
+        checkArgument(lottoPrice(count) <= purchasePrice, NOT_ENOUGH_MONEY_MESSAGE);
+        checkArgument(canBuyLotto(), NOT_ENOUGH_MONEY_MESSAGE);
+
+        for (int i = 0; i < count; i++) {
+            lottos.add(lottoSupplier.get());
+        }
+        manualCount = count;
+        return this;
+    }
+
+    public Lottos getLottos() {
         return lottos;
     }
 
+    public int getAutoCount() {
+        return lottos.size() - manualCount;
+    }
+
+    public int getManualCount() {
+        return manualCount;
+    }
+
+    private boolean canBuyLotto() {
+        return lottoPrice(lottos.size()) < purchasePrice;
+    }
+
+    private int lottoPrice(int count) {
+        return count * DEFAULT_PRICE_UNIT;
+    }
+
+    private int canBuyLottoCount() {
+        return purchasePrice / DEFAULT_PRICE_UNIT;
+    }
 }
--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -1,32 +1,64 @@
 package lotto.domain;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static lotto.domain.LottoShop.DEFAULT_PRICE_UNIT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoShopTest {
 
     @Test
-    @DisplayName("구매금액 0 또는 음수")
+    @DisplayName("구입금액 0 또는 음수")
     void buy_negativePrice_exceptThrown() {
-        assertThatThrownBy(() -> LottoShop.buyAuto(0)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> LottoShop.buyAuto(-1000)).isInstanceOf(IllegalArgumentException.class);
+        exceptIllegalArgumentException(() -> LottoShop.purchasePrice(0));
+        exceptIllegalArgumentException(() -> LottoShop.purchasePrice(-1000));
     }
 
     @Test
-    @DisplayName("구매금액 천원 단위가 아닐 경우")
+    @DisplayName("구입금액 천원 단위가 아닐 경우")
     void buy_notDivided1000_exceptThrown() {
-        assertThatThrownBy(() -> LottoShop.buyAuto(150)).isInstanceOf(IllegalArgumentException.class);
+        exceptIllegalArgumentException(() -> LottoShop.purchasePrice(150));
     }
 
     @Test
-    @DisplayName("구매금액에 따라 정확한 개수 구매")
+    @DisplayName("구입금액에 따라 정확한 개수 구매")
     void buy() {
         int price = 14000;
-        assertThat(LottoShop.buyAuto(price)).hasSize(price / 1000);
+        assertThat(LottoShop.purchasePrice(14000)
+                .buyAuto()
+                .getLottos()
+        ).hasSize(price / DEFAULT_PRICE_UNIT);
     }
 
+    @Test
+    @DisplayName("구입금액 이상 수동 구매 시 예외 발생")
+    void buy_overMoney_exceptThrown() {
+        exceptIllegalArgumentException(
+                () -> LottoShop.purchasePrice(1000)
+                        .buyManual(5, Lotto::generateAuto)
+        );
+    }
 
+    @Test
+    @DisplayName("구입금액에 따라 구매한 수동, 자동 개수 일치")
+    void buy_autoAndManual_return() {
+        // given
+        int price = 14000;
+        int manualCount = 5;
+
+        // when
+        LottoShop lottoShop = LottoShop.purchasePrice(14000)
+                .buyManual(manualCount, Lotto::generateAuto)
+                .buyAuto();
+
+        assertThat(lottoShop.getAutoCount()).isEqualTo((price / DEFAULT_PRICE_UNIT) - manualCount);
+        assertThat(lottoShop.getManualCount()).isEqualTo(manualCount);
+    }
+
+    private void exceptIllegalArgumentException(ThrowableAssert.ThrowingCallable throwingCallable) {
+        assertThatThrownBy(throwingCallable).isInstanceOf(IllegalArgumentException.class);
+    }
 }
