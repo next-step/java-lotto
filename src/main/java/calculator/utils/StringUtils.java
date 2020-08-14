84f505ca8cc2e8191b4ea9dc388d1e911f3e3750
sefide <sefide@naver.com>
2020-08-15 02:20:55 +0900

    refactor: 사용하지 않는 isNumber 메소드 제거

--- src/main/java/calculator/utils/StringUtils.java
+++ src/main/java/calculator/utils/StringUtils.java
@@ -1,10 +1,6 @@
 package calculator.utils;
 
 public class StringUtils {
-    public static boolean isNumber(String input) {
-        return false;
-    }
-
     public static boolean isBlank(String input) {
         return input == null || input.trim().equals("");
     }
--- src/test/java/calculator/utils/StringUtilsTest.java
+++ src/test/java/calculator/utils/StringUtilsTest.java
@@ -10,21 +10,6 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringUtilsTest {
 
-    @DisplayName("isNumber 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"3:true", "-1:true", "000:true", "ㅁ:false", "O:false", "0.0:false"}, delimiter = ':')
-    void isNumber_test(String input, boolean result) {
-        assertThat(StringUtils.isNumber(input)).isEqualTo(result);
-    }
-
-    @DisplayName("isNumber - null 입력 시, 예외 발생 테스트")
-    @Test
-    void isNumber_throw_exception_test() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> StringUtils.isNumber(null))
-                .withMessageContaining("입력값이 숫자가 아닙니다.");
-    }
-
     @DisplayName("isBlank - null 입력 테스트")
     @Test
     void isBlank_null_test() {
