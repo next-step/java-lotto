9b9235c366ee095cafa85f0f34bd537e0a0c825b
hanogo <trullo393@gmail.com>
2020-08-14 23:16:26 +0900

    [feat] SplitTextNum 클래스 및 테스트코드 추가

--- src/main/java/calculator/util/ValidUtil.java
+++ src/main/java/calculator/util/ValidUtil.java
@@ -1,4 +1,15 @@
 package calculator.util;
 
+import java.util.List;
+
 public class ValidUtil {
+    private ValidUtil() {
+        throw new AssertionError();
+    }
+
+    public static void validateNumber(List<String> splitNums) {
+        if (splitNums.stream().map(Integer::parseInt).filter(integer -> integer < 0).count() > 0) {
+            throw new RuntimeException();
+        }
+    }
 }
--- src/test/java/calculator/domain/SplitTextNumTest.java
+++ src/test/java/calculator/domain/SplitTextNumTest.java
@@ -1,5 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package calculator.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class SplitTextNumTest {
+    @Test
+    void create() {
+        Assertions.assertThat(new SplitTextNum(new String[]{"10","20","10"})).isEqualToComparingFieldByField(new SplitTextNum(new String[]{"10","20","10"}));
+    }
+
+    @Test
+    void sum() {
+        Assertions.assertThat(new SplitTextNum(new String[]{"10","20","10"}).sum()).isEqualToComparingFieldByField(new SplitTextNum(new String[]{"40"}).sum());
+    }
 
 }
