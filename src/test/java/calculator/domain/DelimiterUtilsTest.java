e80941b6cdfd003e00fb2fa8fd66ba031cac627f
sefide <sefide@naver.com>
2020-08-14 16:58:16 +0900

    test: DelimiterUtilsTest 테스트코드 추가

--- src/test/java/calculator/domain/DelimiterUtilsTest.java
+++ src/test/java/calculator/domain/DelimiterUtilsTest.java
@@ -1,10 +1,8 @@
 package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -13,36 +11,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DelimiterUtilsTest {
 
-    @DisplayName("빈 문자열 입력 확인 테스트")
-    @Test
-    void isEmpty_test() {
-        assertThat(DelimiterUtils.isEmpty(null)).isTrue();
-        assertThat(DelimiterUtils.isEmpty("")).isTrue();
-        assertThat(DelimiterUtils.isEmpty("fullText")).isFalse();
-    }
-
-    @DisplayName("문자열 분리자 체크 필요 여부 확인 테스트")
+    @DisplayName("문자열 구분자 추출 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"3,3:1|false", "1:4:5|false", "//;\\n1;2;3|true"}, delimiter = '|')
-    void needDelimiterFinder_test(String input, boolean result) {
-        DelimiterUtils delimiterUtils = DelimiterUtils.create(input);
-
-        assertThat(delimiterUtils.needDelimiterFinder()).isEqualTo(result);
-    }
-
-    @DisplayName("문자열 분리자 체크 필요 여부 확인 테스트")
-    @ParameterizedTest
-    @MethodSource("makeTestString")
+    @MethodSource("makeInput")
     void findDelimiters_test(String input, String delimiter) {
         assertThat(DelimiterUtils.findDelimiters(input)).isEqualTo(delimiter);
     }
 
-    private static Stream<Arguments> makeTestString() {
+    private static Stream<Arguments> makeInput() {
         return Stream.of(
                 Arguments.of("3,4:5", ",|:"),
-                Arguments.of("//(\n5(5", "("),
-                Arguments.of("//-\n1-2-3-4", "-"),
-                Arguments.of("//*\n7*1*7", "*")
+                Arguments.of("//(\n5(5", "\\("),
+                Arguments.of("//;\n6;1;3", ";"),
+                Arguments.of("//-\n1-2-3-4", "\\-"),
+                Arguments.of("//*\n7*1*7", "\\*")
+        );
+    }
+
+    @DisplayName("커스텀 구분자 필터 테스트")
+    @ParameterizedTest
+    @MethodSource("makeCustomDelimiterInput")
+    void filterCustomDelimiter_test(String input, String filteredInput) {
+        assertThat(DelimiterUtils.filterCustomDelimiter(input)).isEqualTo(filteredInput);
+    }
+
+    private static Stream<Arguments> makeCustomDelimiterInput() {
+        return Stream.of(
+                Arguments.of("//(\n5(5", "5(5"),
+                Arguments.of("//-\n1-2-3-4", "1-2-3-4"),
+                Arguments.of("//*\n7*1*7", "7*1*7")
         );
     }
 
