6f065c5a13ef402a5e0989b0e9e0bd52e998da10
monds <suahnn@gmail.com>
2020-08-14 15:58:28 +0900

    refactor: LottoUtil 삭제

--- src/main/java/lotto/domain/LottoUtil.java
+++
@@ -1,8 +0,0 @@
-package lotto.domain;
-
-public final class LottoUtil {
-
-    public static double calculateSaveOfReturn(int purchasePrice, long prize) {
-        return prize * 1.0 / purchasePrice;
-    }
-}
