0827e536da2d7450d24f75111a2a0164af122579
hanogo <trullo393@gmail.com>
2020-08-14 23:15:04 +0900

    [feat] InputText 클래스 및 테스트 코드 추가

--- src/test/java/calculator/domain/inputTextTest.java
+++ src/test/java/calculator/domain/inputTextTest.java
@@ -1,4 +1,16 @@
 package calculator.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class inputTextTest {
+    @Test
+    void create() {
+        Assertions.assertThat(new InputText("1,2,3")).isEqualToComparingFieldByField(new InputText("1,2,3"));
+    }
+
+    @Test
+    void splitInputTextTest() {
+        Assertions.assertThat(new InputText("1,2:3").splitInputText()).isEqualTo(new InputText("1,2,3").splitInputText());
+    }
 }
