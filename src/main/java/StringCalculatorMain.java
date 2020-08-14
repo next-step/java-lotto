92ddc037417181a5dd27356630206a5e939c4a7a
willing1026 <34908960+willing1026@users.noreply.github.com>
2020-08-15 07:46:43 +0900

    Refactor method 분리

--- src/main/java/StringCalculatorMain.java
+++ src/main/java/StringCalculatorMain.java
@@ -4,38 +4,74 @@ import java.util.regex.Pattern;
 
 public class StringCalculatorMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("문자열 덧셈 수식을 입력해주세요");
-        String param = sc.nextLine();
+        String param = input();
         String delimiter = "[,:]";
 
+        validateExpression(param);
+        delimiter = extractCustomDelimiter(param, delimiter);
+        param = extractExpression(param);
+        String[] expressionValues = splitValues(param, delimiter);
+
+        int result = add(expressionValues);
+
+        System.out.println(result);
+    }
+
+    private static Integer add(String[] expressionValues) {
+        return Arrays.stream(expressionValues).map(value -> Integer.parseInt(value))
+                .reduce((number1, number2) -> number1 + number2)
+                .get();
+    }
+
+    private static String[] splitValues(String param, String delimiter) {
+        String[] split = param.split(delimiter);
+
+        //validation
+        validationNumber(split);
+
+        return split;
+    }
+
+    private static void validationNumber(String[] split) {
+        Arrays.stream(split)
+                .forEach(number -> {
+                    if (!Pattern.matches("[0-9]", number)) {
+                        throw new IllegalArgumentException("숫자가 아닌 값을 입력하셨습니다.");
+                    }
+
+                    if (Integer.parseInt(number) < 0) {
+                        throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
+                    }
+                });
+    }
+
+    private static String extractExpression(String param) {
+        if (param.startsWith("//")) {
+            return param.substring(5);
+        }
+
+        return param;
+    }
+
+    private static String extractCustomDelimiter(String param, String delimiter) {
+        if (param.startsWith("//")) {
+            delimiter = param.substring(2, 3);
+        }
+
+        return delimiter;
+    }
+
+    private static String input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("문자열 덧셈 수식을 입력해주세요");
+        return sc.nextLine();
+    }
+
+    private static boolean validateExpression(String param) {
         if (param == null || param.equals("")) {
             throw new IllegalArgumentException("입력값이 없습니다.");
         }
 
-        param = param.replace("\"", "");
-
-        if (param.startsWith("//")) {
-            delimiter = param.substring(2, 3);
-            param = param.substring(5);
-        }
-        String[] split = param.split(delimiter);
-
-        //validation
-        for (String value : split) {
-            if (!Pattern.matches("[0-9]", value)) {
-                throw new IllegalArgumentException("숫자가 아닌 값을 입력하셨습니다.");
-            }
-
-            if (Integer.parseInt(value) < 0) {
-                throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
-            }
-        }
-
-        int result = Arrays.stream(split).map(value -> Integer.parseInt(value))
-                .reduce((number1, number2) -> number1 + number2)
-                .get();
-
-        System.out.println(result);
+        return true;
     }
 }
