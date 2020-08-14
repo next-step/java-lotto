80b5221ac9048ca2032fe613f1956afdbbfc5487
newton <antksk@gmail.com>
2020-08-14 22:17:28 +0900

    [fix][로또-step4] LottoApplication에 수동 Lotto 번호 등록 기능 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,21 +1,33 @@
 package lotto;
 
+import java.util.Set;
+
+import lotto.domain.LottoManualSalesStoreBuilder;
 import lotto.domain.LottoStore;
 import lotto.domain.core.LottoGenerator;
 import lotto.domain.core.WinningLotto;
+import lotto.ui.LottoUserInput;
 
 import static lotto.domain.core.LottoGenerator.fromIntSet;
 import static lotto.ui.LottoDisplay.display;
 import static lotto.ui.LottoDisplay.newLine;
 import static lotto.ui.LottoUserInput.inputBonusNo;
+import static lotto.ui.LottoUserInput.inputManualLottoCount;
 import static lotto.ui.LottoUserInput.inputPurchaseAmount;
 import static lotto.ui.LottoUserInput.inputWinLotto;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        final LottoStore lottoStore = LottoStore.purchaseAutoLotto(inputPurchaseAmount());
+        int purchaseAmount = inputPurchaseAmount();
+        int manualLottoCount = inputManualLottoCount();
+        LottoStore lottoStore = inputManualLotto(LottoStore.purchaseManualOrAutoLotto(purchaseAmount, manualLottoCount));
         displayLottoList(lottoStore);
         displayLottoStatistics(lottoStore);
+    }
+
+    private static LottoStore inputManualLotto(LottoManualSalesStoreBuilder lottoManualSalesStoreBuilder) {
+        display("수동으로 구매할 번호를 입력해 주세요.");
+        return lottoManualSalesStoreBuilder.createManualLottoStore(LottoUserInput::inputManualLotto);
     }
 
     private static void displayLottoList(LottoStore lottoStore) {
@@ -25,7 +37,8 @@ public class LottoApplication {
     }
 
     private static void displayLottoStatistics(LottoStore lottoStore) {
-        final WinningLotto winningLotto = LottoGenerator.winLotto(fromIntSet(inputWinLotto()), inputBonusNo());
+        Set<Integer> winLottos = inputWinLotto();
+        final WinningLotto winningLotto = LottoGenerator.winLotto(fromIntSet(winLottos), inputBonusNo());
         newLine(1);
         display("당첨 통계");
         display("---------");
