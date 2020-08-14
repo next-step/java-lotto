e9eb1629dc3346c0fc30fe13dd00046874dddbc0
ted.kang <ted.kang@kakaocorp.com>
2020-08-15 01:43:08 +0900

    refactor 4. LottoGames Class 작성하기 (일급 컬렉션)

---
+++ src/main/java/step2/domain/LottoGames.java
@@ -0,0 +1,45 @@
+package step2.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class LottoGames {
+
+	private final List<LottoGame> lottoGames;
+
+	public LottoGames(List<LottoGame> lottoGames) {
+		this.lottoGames = lottoGames;
+	}
+
+	public List<LottoGame> getLottoGames() {
+		return lottoGames;
+	}
+
+	public int getGameSize() {
+		return lottoGames.size();
+	}
+
+	public ConfirmResults confirmPrize(LottoGame lastWeekPrize) {
+		return new ConfirmResults(getConfirmResults(lastWeekPrize));
+	}
+
+	private List<ConfirmResult> getConfirmResults(LottoGame lastWeekPrize) {
+		return lottoGames.stream()
+				.map(lottoGame -> lottoGame.confirmPrize(lastWeekPrize))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		LottoGames that = (LottoGames) o;
+		return lottoGames.equals(that.lottoGames);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(lottoGames);
+	}
+}
---
+++ src/test/java/step2/domain/LottoGamesTest.java
@@ -0,0 +1,25 @@
+package step2.domain;
+
+import com.sun.tools.javac.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import step2.LottoException;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoGamesTest {
+
+	@Test
+	void confirmPrize() {
+	}
+
+
+}
