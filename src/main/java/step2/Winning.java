a1e49e2198037ebfb7b21c8dba4ec67ffe5ea63a
박찬석 <pcspapa@gmail.com>
2020-08-15 23:15:57 +0900

    refactor: Winning 상속과 합성이 공종 합성 제거

--- src/main/java/step2/Winning.java
+++ src/main/java/step2/Winning.java
@@ -6,8 +6,6 @@ import java.util.stream.Collectors;
 
 public class Winning extends Lotto {
 
-  private final Lotto winner;
-
   public Winning(String numbersAsString) {
     this(
         Arrays.stream(numbersAsString.split(","))
@@ -17,18 +15,9 @@ public class Winning extends Lotto {
 
   public Winning(List<Integer> numbers) {
     super(numbers);
-    this.winner = new Lotto(numbers);
-  }
-
-  public boolean isWinner(Lotto lotto) {
-    return winner.equals(lotto);
   }
 
   public int sameNumberCount(Lotto lotto) {
-    if (isWinner(lotto)) {
-      return Lotto.FIXED_COUNT;
-    }
-
     return numbers.stream().filter(number -> lotto.hasNumber(number)).mapToInt(i -> 1).sum();
   }
 }
--- src/test/java/step2/WinningTest.java
+++ src/test/java/step2/WinningTest.java
@@ -4,41 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import step2.LottoStat.Grade;
 
 public class WinningTest {
 
   @Test
-  void isWinner() {
-    Winning winning = new Winning(Arrays.asList(1, 2, 3, 4, 5, 6));
-
-    assertThat(winning.isWinner(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))).isTrue();
-  }
-
-  @Test
-  void isNotWinner() {
-    Winning winning = new Winning(Arrays.asList(1, 2, 3, 4, 5, 7));
-
-    assertThat(winning.isWinner(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))).isFalse();
-  }
-
-  @Test
-  void allSameNumber() {
-    Winning winning = new Winning(Arrays.asList(1, 2, 3, 4, 5, 6));
-
-    assertThat(winning.sameNumberCount(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))).isEqualTo(6);
-  }
-
-  @Test
-  void noneSameNumberIsOne() {
+  void sameNumberIsNone() {
     Winning winning = new Winning(Arrays.asList(40, 41, 42, 43, 44, 45));
 
-    assertThat(winning.sameNumberCount(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))).isEqualTo(0);
+    assertThat(winning.sameNumberCount(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))))
+        .isEqualTo(0);
   }
 
   @Test
   void sameNumberIsOne() {
     Winning winning = new Winning(Arrays.asList(1, 41, 42, 43, 44, 45));
 
-    assertThat(winning.sameNumberCount(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)))).isEqualTo(1);
+    assertThat(winning.sameNumberCount(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))))
+        .isEqualTo(1);
+  }
+
+  @Test
+  void isWinner() {
+    Winning winning = new Winning(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+    assertThat(winning.sameNumberCount(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))))
+        .isEqualTo(Grade.First.getSameCount());
   }
 }
