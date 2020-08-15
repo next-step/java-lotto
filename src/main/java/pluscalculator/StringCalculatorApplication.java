85e7617921c7234abd90ccf6c8a0e0821faeacfe
John <bass0223@naver.com>
2020-08-15 20:22:32 +0900

    application 객체 생성

---
+++ src/main/java/pluscalculator/StringCalculatorApplication.java
@@ -0,0 +1,10 @@
+package pluscalculator;
+
+import pluscalculator.controller.CalculatorController;
+
+public class StringCalculatorApplication {
+
+	public static void main(String[] args) {
+		CalculatorController.execute();
+	}
+}
