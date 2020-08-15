2380b1a6814e777cac388564ce9adef3f9d3204d
samkko <wkvk1234@gmail.com>
2020-08-16 04:55:17 +0900

    [step02][test] LottoGame TC작성

--- src/test/java/com.hskim.lotto/model/LottoGameTest.java
+++ src/test/java/com.hskim.lotto/model/LottoGameTest.java
@@ -3,6 +3,8 @@ package com.hskim.lotto.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGameTest {
@@ -11,7 +13,11 @@ public class LottoGameTest {
     @Test
     void create() {
         // given
+        LottoTickets lottoTickets = new LottoTickets(new PurchasePrice("10000"), new FixedNumberMaker());
+        LottoWinningTicket winningTicket = new LottoWinningTicket(Arrays.asList("1", "8", "9", "10", "17", "25"));
+        LottoGame lottoGame = new LottoGame(lottoTickets, winningTicket);
 
         // when & then
+        assertThat(lottoGame).isEqualTo(new LottoGame(lottoTickets, winningTicket));
     }
 }
