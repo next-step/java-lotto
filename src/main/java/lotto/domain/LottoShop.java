9dfa75880eb49ac5a6d5378dc4d9c7e3b2b4e175
Sang Uk Ahn <suahnn@gmail.com>
2020-08-15 18:24:35 +0900

    refactor: 일부 기능 위치 변경

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import java.text.MessageFormat;
-import java.util.function.Supplier;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -11,65 +10,22 @@ public class LottoShop {
 
     private static final String PRICE_ERROR_MESSAGE = "price must be at least zero";
     private static final String PRICE_UNIT_MESSAGE = "price {0} won unit";
-    private static final String NOT_ENOUGH_MONEY_MESSAGE = "There is not enough money";
     private static final String COUNT_ERROR_MESSAGE = "count must be at least zero";
+    private static final String NOT_ENOUGH_MONEY_MESSAGE = "there is not enough money";
 
-    private final int purchasePrice;
-    private final Lottos lottos;
-    private int manualCount = 0;
-
-    public LottoShop(int purchasePrice) {
-        this.purchasePrice = purchasePrice;
-        lottos = new Lottos();
-    }
-
-    public static LottoShop purchasePrice(int purchasePrice) {
-        checkArgument(purchasePrice > 0, PRICE_ERROR_MESSAGE);
-        checkArgument(purchasePrice % DEFAULT_PRICE_UNIT == 0,
+    public static Lottos purchase(int price, int manualCount) {
+        checkArgument(price > 0, PRICE_ERROR_MESSAGE);
+        checkArgument(price % DEFAULT_PRICE_UNIT == 0,
                 MessageFormat.format(PRICE_UNIT_MESSAGE, DEFAULT_PRICE_UNIT));
-        return new LottoShop(purchasePrice);
+        checkArgument(manualCount >= 0, COUNT_ERROR_MESSAGE);
+
+        int canPurchaseCount = canPurchaseCount(price);
+        checkArgument(canPurchaseCount >= manualCount, NOT_ENOUGH_MONEY_MESSAGE);
+
+        return new Lottos(canPurchaseCount(price));
     }
 
-    public LottoShop buyAuto() {
-        if (canBuyLotto()) {
-            lottos.generateAuto(canBuyLottoCount() - manualCount);
-        }
-        return this;
-    }
-
-    public LottoShop buyManual(int count, Supplier<Lotto> lottoSupplier) {
-        checkArgument(count >= 0, COUNT_ERROR_MESSAGE);
-        checkArgument(lottoPrice(count) <= purchasePrice, NOT_ENOUGH_MONEY_MESSAGE);
-        checkArgument(canBuyLotto(), NOT_ENOUGH_MONEY_MESSAGE);
-
-        for (int i = 0; i < count; i++) {
-            lottos.add(lottoSupplier.get());
-        }
-        manualCount = count;
-        return this;
-    }
-
-    public Lottos getLottos() {
-        return lottos;
-    }
-
-    public int getAutoCount() {
-        return lottos.size() - manualCount;
-    }
-
-    public int getManualCount() {
-        return manualCount;
-    }
-
-    private boolean canBuyLotto() {
-        return lottoPrice(lottos.size()) < purchasePrice;
-    }
-
-    private int lottoPrice(int count) {
-        return count * DEFAULT_PRICE_UNIT;
-    }
-
-    private int canBuyLottoCount() {
-        return purchasePrice / DEFAULT_PRICE_UNIT;
+    public static int canPurchaseCount(int price) {
+        return price / DEFAULT_PRICE_UNIT;
     }
 }
