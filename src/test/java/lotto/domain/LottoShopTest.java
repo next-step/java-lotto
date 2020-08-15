4df70ddeeb5a433d3b3c95adc4b989dc21e08166
Sang Uk Ahn <suahnn@gmail.com>
2020-08-15 18:24:53 +0900

    test: LottoShop 테스트클래스 변경 및 추가

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -13,49 +13,41 @@ public class LottoShopTest {
     @Test
     @DisplayName("구입금액 0 또는 음수")
     void buy_negativePrice_exceptThrown() {
-        exceptIllegalArgumentException(() -> LottoShop.purchasePrice(0));
-        exceptIllegalArgumentException(() -> LottoShop.purchasePrice(-1000));
+        exceptIllegalArgumentException(() -> LottoShop.purchase(0, 0));
+        exceptIllegalArgumentException(() -> LottoShop.purchase(-1000, 0));
     }
 
     @Test
     @DisplayName("구입금액 천원 단위가 아닐 경우")
     void buy_notDivided1000_exceptThrown() {
-        exceptIllegalArgumentException(() -> LottoShop.purchasePrice(150));
+        exceptIllegalArgumentException(() -> LottoShop.purchase(150, 0));
     }
 
     @Test
     @DisplayName("구입금액에 따라 정확한 개수 구매")
     void buy() {
+        // given
         int price = 14000;
-        assertThat(LottoShop.purchasePrice(14000)
-                .buyAuto()
-                .getLottos()
-        ).hasSize(price / DEFAULT_PRICE_UNIT);
+
+        // when
+        Lottos lottos = LottoShop.purchase(price, 0);
+        lottos.fillAuto();
+
+        // then
+        assertThat(lottos).hasSize(LottoShop.canPurchaseCount(price));
     }
 
     @Test
     @DisplayName("구입금액 이상 수동 구매 시 예외 발생")
     void buy_overMoney_exceptThrown() {
-        exceptIllegalArgumentException(
-                () -> LottoShop.purchasePrice(1000)
-                        .buyManual(5, Lotto::generateAuto)
-        );
+        exceptIllegalArgumentException(() -> LottoShop.purchase(1000, 10));
     }
 
     @Test
-    @DisplayName("구입금액에 따라 구매한 수동, 자동 개수 일치")
-    void buy_autoAndManual_return() {
-        // given
+    @DisplayName("구입금액에 따라 구매 가능한 개수")
+    void canPurchaseCount() {
         int price = 14000;
-        int manualCount = 5;
-
-        // when
-        LottoShop lottoShop = LottoShop.purchasePrice(14000)
-                .buyManual(manualCount, Lotto::generateAuto)
-                .buyAuto();
-
-        assertThat(lottoShop.getAutoCount()).isEqualTo((price / DEFAULT_PRICE_UNIT) - manualCount);
-        assertThat(lottoShop.getManualCount()).isEqualTo(manualCount);
+        assertThat(LottoShop.canPurchaseCount(price)).isEqualTo(price / DEFAULT_PRICE_UNIT);
     }
 
     private void exceptIllegalArgumentException(ThrowableAssert.ThrowingCallable throwingCallable) {
