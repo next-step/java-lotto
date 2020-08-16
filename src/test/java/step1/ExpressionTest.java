98862787719c1d32061d6419075b0e498c2b7669
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-16 19:14:49 +0900

    fix(test): ExpressionTest Test annotation not attached.

--- src/test/java/step1/ExpressionTest.java
+++ src/test/java/step1/ExpressionTest.java
@@ -14,6 +14,7 @@ public class ExpressionTest {
                 .isInstanceOf(RuntimeException.class);
     }
 
+    @Test
     public void of() {
         assertThat(Expression.of("3,5:2,3:4,0,2").stream())
                 .containsExactly(3, 5, 2, 3, 4, 0, 2);
