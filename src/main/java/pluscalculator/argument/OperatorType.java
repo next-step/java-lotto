9467926aecde37f42f5a8db1416e62b88bb4a9e7
John <bass0223@naver.com>
2020-08-16 18:30:17 +0900

    checkPattern했던 것을 각각의 인스턴스가 아니라 static 메서드로 빼서 중복 수행 변경

--- src/main/java/pluscalculator/argument/OperatorType.java
+++ src/main/java/pluscalculator/argument/OperatorType.java
@@ -12,23 +12,11 @@ import lombok.Getter;
 public enum OperatorType {
 	ONLY_ONE_NUMBER(Operator.NONE) {
 		@Override
-		public boolean checkPattern(String input) {
-			String[] operandArray = getSplitedInput(input);
-			return isOnlyOneNumber(operandArray);
-		}
-
-		@Override
 		public Operands toOperandsFrom(String input) {
 			return Operands.from(new String[]{input});
 		}
 	},
 	DEFAULT(Operator.PLUS) {
-		@Override
-		public boolean checkPattern(String input) {
-			String[] operandArray = getSplitedInput(input);
-			return isAllDualNumber(operandArray);
-		}
-
 		@Override
 		public Operands toOperandsFrom(String input) {
 			String[] operandArray = getSplitedInput(input);
@@ -37,15 +25,6 @@ public enum OperatorType {
 
 	},
 	CUSTOM(Operator.PLUS) {
-		@Override
-		public boolean checkPattern(String input) {
-			if (!doesFindPattern(input, CUSTOM_PATTERN)) {
-				return false;
-			}
-			String[] operandArray = toOperandsFrom(input).toStringArray();
-			return isAllDualNumber(operandArray);
-		}
-
 		@Override
 		public Operands toOperandsFrom(String input) {
 			Matcher matcher = CUSTOM_PATTERN.matcher(input);
@@ -62,6 +41,7 @@ public enum OperatorType {
 
 	private static final Pattern NUMBER_PATTERN = Pattern.compile("^[-+]?(0|[1-9][0-9]*)(\\\\.[0-9]+)?([eE][-+]?[0-9]+)?$");
 	private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\\\\n(.*)");
+	private static final String DEFAULT_TYPE_DELIMITER = ",|:";
 
 	private static boolean isAllDualNumber(String[] operandArray) {
 		if (operandArray.length <= 1) {
@@ -97,19 +77,26 @@ public enum OperatorType {
 	}
 
 	private static String[] getSplitedInput(String input) {
-		return input.split(",|:");
+		return input.split(DEFAULT_TYPE_DELIMITER);
 	}
 
-	public static OperatorType getOperatorTypeBy(String inputOfUser) {
-		return Stream.of(OperatorType.values())
-					 .filter(operatorType -> operatorType.checkPattern(inputOfUser))
-					 .findFirst()
-					 .orElseThrow(() -> new IllegalArgumentException("연산자를 다시 확인해주세요."));
+	public static OperatorType getOperatorTypeBy(String input) {
+		String[] operandArray = getSplitedInput(input);
+		if (isOnlyOneNumber(operandArray)) {
+			return ONLY_ONE_NUMBER;
+		}
+		if (isAllDualNumber(operandArray)) {
+			return DEFAULT;
+		}
+
+		String[] operandArrayForCustomPattern = CUSTOM.toOperandsFrom(input).toStringArray();
+		if (doesFindPattern(input, CUSTOM_PATTERN) && isAllDualNumber(operandArrayForCustomPattern)) {
+			return CUSTOM;
+		}
+		throw new IllegalArgumentException("연산자를 다시 확인해주세요.");
 	}
 
 	private Operator operator;
-
-	public abstract boolean checkPattern(String input);
 
 	public abstract Operands toOperandsFrom(String input);
 }
