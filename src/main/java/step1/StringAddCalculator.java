a47ead33f06b5f36459e37364aaac7ccb2f8fbbd
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-16 19:10:45 +0900

    refactor: make Regex Pattern constant.

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -6,13 +6,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    public static final String DELIMITER_REGEX = "//(.)\n(.*)";
+    public static final Pattern DELIMITER_PATTERN = Pattern.compile(DELIMITER_REGEX);
 
     public static final int TEXT_GROUP = 2;
     public static final int DELIMITER_GROUP = 1;
 
     private StringAddCalculator() {}
-
-    public static final String DELIMITER_REGEX = "//(.)\n(.*)";
 
     public static int splitAndSum(String target) {
         if (StringUtils.isEmpty(target)) {
@@ -23,7 +23,7 @@ public class StringAddCalculator {
     }
 
     private static Expression createExpression(String target) {
-        Matcher m = Pattern.compile(DELIMITER_REGEX).matcher(target);
+        Matcher m = DELIMITER_PATTERN.matcher(target);
         if (m.find()) {
             return Expression.of(m.group(TEXT_GROUP), m.group(DELIMITER_GROUP));
         }
