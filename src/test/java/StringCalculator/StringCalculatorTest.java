8ea6148c5b3c1f1df9fea0e3e6339a2d8863d1d3
rftengineer <nightpw1@naver.com>
2020-08-17 05:15:29 +0900

    Test(StringCalculator): 단일 숫자 계산에 대한 내용 체크, 음수에 대한 실패하는 테스트 제작

--- src/test/java/StringCalculator/StringCalculatorTest.java
+++ src/test/java/StringCalculator/StringCalculatorTest.java
@@ -1,12 +1,15 @@
 package StringCalculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
+@DisplayName("Lotto Mission - Step1 테스트")
 public class StringCalculatorTest {
 
     @Test
+    @DisplayName("입력 숫자가 두 개일때 진행하는 테스트")
     public void calculateTest_TwoNumbers() {
         String[] value = "1,3".split(",|;");
         MissionCalculator calculator = new MissionCalculator(value);
@@ -14,11 +17,27 @@ public class StringCalculatorTest {
     }
 
     @Test
+    @DisplayName("세 입력숫자에 대한 내용 테스트")
     void calculateTest_ThreeNumbers() {
         String[] value = "1,3;5".split(",|;");
         MissionCalculator calculator = new MissionCalculator(value);
         assertThat(calculator.calculate()).isEqualTo(9);
     }
 
+    @Test
+    @DisplayName("단일 입력에 대한 계산 확인")
+    void calculateTest_OneNumber() {
+        String[] value = "5".split(",|;");
+        MissionCalculator calculator = new MissionCalculator(value);
+        assertThat(calculator.calculate()).isEqualTo(5);
+    }
 
+    @Test
+    @DisplayName("음수 제약조건 확인")
+    void calculateTest_ExceptionForNegativeNumber() {
+        String[] value = "-1,2".split(",|;");
+        MissionCalculator calculator = new MissionCalculator(value);
+        assertThatThrownBy(calculator::calculate)
+                .isInstanceOf(RuntimeException.class);
+    }
 }
