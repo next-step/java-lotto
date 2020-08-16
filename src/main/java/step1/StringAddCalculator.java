704678c3f9ad863768f01316de707ac9eca41e52
skid901 <tlrudejr1993@naver.com>
2020-08-17 04:36:27 +0900

    feat: 문자열 덧셈 계산기 splitAndSum() 메소드 구현
    TestCase 소스 코드를 모두 통과하는 splitAndSum() 메소드 구현

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -1,8 +1,42 @@
 package step1;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String operand) throws RuntimeException {
-        return -1;
+        if (operand == null) {
+            return 0;
+        }
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(operand);
+        if (matcher.find()) {
+            if ("".equals(matcher.group(2))) {
+                return 0;
+            }
+            String customDelimiter = matcher.group(1);
+            Matcher validator = Pattern.compile("[^0-9" + customDelimiter + "]").matcher(matcher.group(2));
+            if (validator.find()) {
+                throw new RuntimeException();
+            }
+            String[] tokens = matcher.group(2).split(customDelimiter);
+            return Arrays.stream(tokens)
+                    .mapToInt(Integer::parseInt)
+                    .reduce(Integer::sum)
+                    .orElseThrow(RuntimeException::new);
+        }
+        if ("".equals(operand)) {
+            return 0;
+        }
+        Matcher validator = Pattern.compile("[^0-9,:]").matcher(operand);
+        if (validator.find()) {
+            throw new RuntimeException();
+        }
+        String[] tokens= operand.split("[,:]");
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .reduce(Integer::sum)
+                .orElseThrow(RuntimeException::new);
     }
 }
