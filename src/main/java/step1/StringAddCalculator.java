23b34a9acd839a4bdc1b2ba0562584eaf274af2e
skid901 <tlrudejr1993@naver.com>
2020-08-17 05:55:12 +0900

    refactor: 문자열 덧셈 계산기 splitAndSum() 메소드 리팩토링
    splitAndSum() 메소드를 프로그래밍 요구사항에 맞게 리팩토링 후, TestCase 소스 코드 통과 확인

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -1,42 +1,55 @@
 package step1;
 
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    public static int splitAndSum(String operand) throws RuntimeException {
-        if (operand == null) {
-            return 0;
-        }
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(operand);
-        if (matcher.find()) {
-            if ("".equals(matcher.group(2))) {
-                return 0;
-            }
-            String customDelimiter = matcher.group(1);
-            Matcher validator = Pattern.compile("[^0-9" + customDelimiter + "]").matcher(matcher.group(2));
-            if (validator.find()) {
-                throw new RuntimeException();
-            }
-            String[] tokens = matcher.group(2).split(customDelimiter);
-            return Arrays.stream(tokens)
-                    .mapToInt(Integer::parseInt)
-                    .reduce(Integer::sum)
-                    .orElseThrow(RuntimeException::new);
-        }
-        if ("".equals(operand)) {
-            return 0;
-        }
-        Matcher validator = Pattern.compile("[^0-9,:]").matcher(operand);
+    private static final String EMPTY_STRING = "";
+    private static final String DEFAULT_DELIMITER = ",:";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int CUSTOM_OPERAND_INDEX = 2;
+    private static final Function<String, String> GET_REGEX_TO_VALIDATE = delimiter -> "[^0-9" + delimiter + "]";
+    private static final Function<String, String> GET_REGEX_TO_SPLIT = delimiter -> "[" + delimiter + "]";
+
+    private static boolean isNull(String operand) {
+        return operand == null;
+    }
+
+    private static boolean isEmpty(String operand) {
+        return EMPTY_STRING.equals(operand);
+    }
+
+    private static void validate(String operand, String delimiter) throws RuntimeException {
+        Matcher validator = Pattern.compile(GET_REGEX_TO_VALIDATE.apply(delimiter)).matcher(operand);
         if (validator.find()) {
             throw new RuntimeException();
         }
-        String[] tokens= operand.split("[,:]");
+    }
+
+    private static int splitAndSum(String operand, String delimiter) throws RuntimeException {
+        if (isEmpty(operand)) {
+            return 0;
+        }
+        validate(operand, delimiter);
+        String[] tokens = operand.split(GET_REGEX_TO_SPLIT.apply(delimiter));
         return Arrays.stream(tokens)
                 .mapToInt(Integer::parseInt)
                 .reduce(Integer::sum)
                 .orElseThrow(RuntimeException::new);
+    }
+
+    public static int splitAndSum(String operand) throws RuntimeException {
+        if (isNull(operand)) {
+            return 0;
+        }
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(operand);
+        if (matcher.find()) {
+            return splitAndSum(matcher.group(CUSTOM_OPERAND_INDEX), matcher.group(CUSTOM_DELIMITER_INDEX));
+        }
+        return splitAndSum(operand, DEFAULT_DELIMITER);
     }
 }
