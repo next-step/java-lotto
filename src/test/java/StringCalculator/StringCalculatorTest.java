50c8b876ceb2677f1caf71bdf064662d51df8517
rftengineer <nightpw1@naver.com>
2020-08-17 05:46:25 +0900

    Refactor: Test 생성자들을 모두 inline처리

--- src/test/java/StringCalculator/StringCalculatorTest.java
+++ src/test/java/StringCalculator/StringCalculatorTest.java
@@ -11,30 +11,26 @@ public class StringCalculatorTest {
     @Test
     @DisplayName("입력 숫자가 두 개일때 진행하는 테스트")
     public void calculateTest_TwoNumbers() {
-        MissionCalculator calculator = new MissionCalculator();
-        assertThat(calculator.splitAndCalculate("1,3")).isEqualTo(4);
+        assertThat(new MissionCalculator().splitAndCalculate("1,3")).isEqualTo(4);
     }
 
     @Test
     @DisplayName("세 입력숫자에 대한 내용 테스트")
     void calculateTest_ThreeNumbers() {
-        MissionCalculator calculator = new MissionCalculator();
-        assertThat(calculator.splitAndCalculate("1,3;5")).isEqualTo(9);
+        assertThat(new MissionCalculator().splitAndCalculate("1,3;5")).isEqualTo(9);
     }
 
     @Test
     @DisplayName("단일 입력에 대한 계산 확인")
     void calculateTest_OneNumber() {
-        MissionCalculator calculator = new MissionCalculator();
-        assertThat(calculator.splitAndCalculate("5")).isEqualTo(5);
+        assertThat(new MissionCalculator().splitAndCalculate("5")).isEqualTo(5);
     }
 
     @Test
     @DisplayName("음수 제약조건 확인")
     void calculateTest_ExceptionForNegativeNumber() {
-        String[] value = "-1,2".split(",|;");
-        MissionCalculator calculator = new MissionCalculator(value);
-        assertThatThrownBy(calculator::calculate)
+        //String[] value = "-1,2".split(",|;");
+        assertThatThrownBy(() -> new MissionCalculator().splitAndCalculate("-1,2"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
