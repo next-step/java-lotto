4eda49c9bd6414c800ef437a6237e0d11b65db93
Dami <fdfdsa123@hanmail.net>
2020-08-16 23:14:20 +0900

    feat ($step1): 커스텀 구분자를 기본 구분자로 설정하는 기능

--- src/main/java/step1/StringUtils.java
+++ src/main/java/step1/StringUtils.java
@@ -1,7 +1,28 @@
 package step1;
 
 public class StringUtils {
-	public static String[] split(String str, String defaultDelimiter) {
-		return str.split(defaultDelimiter);
+	private static final String DEFAULT_DELIMITER = ",|:";
+	private static final String BACK_SLASHES = "\\\\";
+	private static final String START_SIGN = "\\/\\/";
+	private static final String END_SIGN = System.lineSeparator();
+
+	private static final StringBuilder delimiter = new StringBuilder(DEFAULT_DELIMITER);
+
+	public static String[] split(String inputString) {
+		return separateDelimiterAndIntegers(inputString).split(delimiter.toString());
 	}
+
+	public static String separateDelimiterAndIntegers(String inputString) {
+		String[] separatedString = inputString.replaceAll(START_SIGN, BACK_SLASHES).split(END_SIGN);
+		if (separatedString.length > 1) {
+			changeDelimiter(separatedString[0]);
+			return separatedString[1];
+		}
+		return separatedString[0];
+	}
+
+	private static void changeDelimiter(String newDelimiter) {
+		delimiter.replace(0, delimiter.length(), newDelimiter);
+	}
+
 }
