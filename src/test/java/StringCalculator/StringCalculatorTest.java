218441543216234fe6e07f668cf11bc9a47eb4bb
rftengineer <nightpw1@naver.com>
2020-08-17 04:55:51 +0900

    Test(StringCalculator): 실패하는 Test를 포함해 추가

--- src/test/java/StringCalculator/StringCalculatorTest.java
+++ src/test/java/StringCalculator/StringCalculatorTest.java
@@ -7,9 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCalculatorTest {
 
     @Test
-    public void calculateTest() {
+    public void calculateTest_TwoNumbers() {
         String[] value = "1,3".split(",|;");
         MissionCalculator calculator = new MissionCalculator(value);
-        assertThat(calculator.calculate());
+        assertThat(calculator.calculate()).isEqualTo(4);
+    }
+
+    @Test
+    void calculateTest_ThreeNumbers() {
+        String[] value = "1,3;5".split(",|;");
+        MissionCalculator calculator = new MissionCalculator(value);
+        assertThat(calculator.calculate()).isEqualTo(9);
     }
 }
