266579ab411d527d868a3c1a857e84be3f2c2c70
sefide <sefide@naver.com>
2020-08-16 21:57:50 +0900

    test: LottoMeney(구입 금액) 테스트코드 작성

---
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -0,0 +1,7 @@
+package lotto.domain;
+
+public class LottoMoney {
+    public static LottoMoney of(String money) {
+        return null;
+    }
+}
---
+++ src/test/java/lotto/domain/LottoMoneyTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class LottoMoneyTest {
+
+    @DisplayName("로또 구입 금액 정상 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"12000", "1000"})
+    void create(String money) {
+        assertThatCode(() -> LottoMoney.of(money))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("로또 구입 금액 비정상 입력 (1000단위) 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"333", "0", "11001"})
+    void create_invalid_money_unit(String money) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoMoney.of(money))
+                .withMessage("구입 금액은 1000원 단위여야 합니다. (로또 1개 - 1000원)");
+    }
+
+    @DisplayName("로또 구입 금액 비정상 입력 (숫자 아닌 문자 입력) 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"O", "얼마샀더라", "흠"})
+    void create_not_number(String money) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoMoney.of(money))
+                .withMessage("구입 금액은 숫자로 입력해주세요.");
+    }
+
+}
