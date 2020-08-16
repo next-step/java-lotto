d98139ce9c805c72e4ef3ac219c7bafbcf128198
Dami <fdfdsa123@hanmail.net>
2020-08-17 00:57:54 +0900

    feat ($step1): 숫자 이외의 값 또는 음수를 예외처리하는 기능

--- src/main/java/step1/Calculator.java
+++ src/main/java/step1/Calculator.java
@@ -1,18 +1,28 @@
 package step1;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
-	private static final int EMPTY_VALUE = 0;
+	private static final int INIT_VALUE = 0;
+	private static final Pattern POSITIVE_NUMBER = Pattern.compile("^[0-9]+$");
 
 	public static int getIntegerSumOfStrings(String inputString) {
 		if (StringUtils.isEmpty(inputString)) {
-			return EMPTY_VALUE;
+			return INIT_VALUE;
 		}
 
 		String[] strings = StringUtils.split(inputString);
-		int sum = 0;
+		int sum = INIT_VALUE;
 		for (String number : strings) {
-			sum += Integer.parseInt(number);
+			sum += validNumber(number);
 		}
 		return sum;
+	}
+
+	private static int validNumber(String number) {
+		if (POSITIVE_NUMBER.matcher(number).matches()) {
+			return Integer.parseInt(number);
+		}
+		throw new RuntimeException("문자열 덧셈 계산기는 양수만 가능합니다.");
 	}
 }
