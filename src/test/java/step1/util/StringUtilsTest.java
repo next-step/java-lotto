e762c838efa470e42e2585078c7b51af81ee31c0
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-16 18:12:59 +0900

    refactor: remove useless import statement on StringUtilsTest

--- src/test/java/step1/util/StringUtilsTest.java
+++ src/test/java/step1/util/StringUtilsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import step1.util.StringUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
