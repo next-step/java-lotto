28ca49a4f86545b72a69a288aad50b75c6add4ba
choihojun <hojunchoi@hotmail.co.kr>
2020-08-16 10:27:53 +0900

    refactor - getCalculationNumbers fix Optional.ofNullable

--- src/main/java/CalculationNumber.java
+++ src/main/java/CalculationNumber.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import java.util.Optional;
 
 public class CalculationNumber {
     public final List<Integer> innerNumbers;
@@ -9,7 +8,7 @@ public class CalculationNumber {
     }
 
     public static CalculationNumber getCalculationNumbers(String inputCalculationNumbers) {
-        return Optional.ofNullable(ConverterFactory.ofConverter(inputCalculationNumbers))
+        return ConverterFactory.ofConverter(inputCalculationNumbers)
                 .map(Converter::convertInputCalculationNumbers)
                 .orElseThrow(() -> new RuntimeException("의미있는 숫자가 아닙니다."));
     }
