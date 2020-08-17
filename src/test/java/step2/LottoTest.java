e86ef759bf5c954017ddb13784ae44114bb4ee87
Dami <fdfdsa123@hanmail.net>
2020-08-17 15:28:06 +0900

    feat ($step2): 로또 갯수만큼 자동 발급하는 기능

--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -3,6 +3,8 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
@@ -12,5 +14,12 @@ public class LottoTest {
 	void howManyLotto() {
 		int result = Lotto.getLottoCountByPrice(14000);
 		assertThat(result).isEqualTo(14);
+	}
+
+	@DisplayName(value = "입력받은 갯수만큼 로또를 자동 발급 하는 기능")
+	@Test
+	void issueAutoLotto() {
+		List<Integer[]> lottoResult = Lotto.issueAutoLotto(14);
+		assertThat(lottoResult).hasSize(14);
 	}
 }
