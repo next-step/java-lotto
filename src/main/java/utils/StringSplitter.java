a2d46f551ce294e37fef4a297910be3aa83ceb54
pjhsk113 <pjhsk113@naver.com>
2020-08-17 16:24:16 +0900

    [fix] custom delimiter 덧셈 정규식 제거
    - substring으로 덧셈 진행

--- src/main/java/utils/StringSplitter.java
+++ src/main/java/utils/StringSplitter.java
@@ -4,32 +4,45 @@ import exception.StringAdderExceptionHandler;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringSplitter {
     private static final String REGEX_DEFAULT_DELIMITER = "[,:]";
-    private static final Pattern REGEX_CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
-    private static final int CUSTOM_DELIMITER_GROUP = 1;
-    private static final int CUSTOM_NUMBER_GROUP = 2;
+    private static final int CUSTOM_DELIMITER_START_INDEX = 2;
+    private static final int CUSTOM_DELIMITER_END_INDEX = 3;
+    private static final int CUSTOM_NUMBER_START_INDEX = 5;
 
     private StringSplitter() { }
 
     public static List<Integer> splitDelimiter(String input) {
-        Matcher matcher = REGEX_CUSTOM_DELIMITER.matcher(input);
-
-        if (matcher.find()) {
-            String customDelimiter = matcher.group(CUSTOM_DELIMITER_GROUP);
-            return castingToList(matcher.group(CUSTOM_NUMBER_GROUP).split(customDelimiter));
+        if (isCustomDelimiter(input)) {
+            String customDelimiter = subStringToCustomDelimiter(input);
+            String numbers = subStringToCustomNumbers(input);
+            return castingToList(numbers.split(customDelimiter));
         }
+
         return castingToList(input.split(REGEX_DEFAULT_DELIMITER));
     }
 
-    public static List<Integer> castingToList(String[] input) {
+    private static List<Integer> castingToList(String[] input) {
         return Arrays.stream(input)
                 .map(Integer::parseInt)
                 .peek(StringAdderExceptionHandler::validate)
                 .collect(Collectors.toList());
+    }
+
+    private static boolean isCustomDelimiter(String input) {
+        if (input.contains("//")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String subStringToCustomDelimiter(String input) {
+        return input.substring(CUSTOM_DELIMITER_START_INDEX, CUSTOM_DELIMITER_END_INDEX);
+    }
+
+    private static String subStringToCustomNumbers(String input) {
+        return input.substring(CUSTOM_NUMBER_START_INDEX);
     }
 }
