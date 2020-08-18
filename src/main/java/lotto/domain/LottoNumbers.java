d46eccb21979a120755786c67296b2ac1e8a7cdc
LimHanGyeol <dlagksruf19@naver.com>
2020-08-18 22:19:56 +0900

    feat : 로또 복권 기능 구현

---
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LottoNumbers {
+
+    private static final int LOTTO_BASE_START_NUMBER = 1;
+    private static final int LOTTO_BASE_END_NUMBER = 45;
+    public static final String INVALID_LOTTO_NUMBER = "유효하지 않은 번호입니다.";
+
+    private List<Integer> lottoNumbers;
+
+    public LottoNumbers() {
+        this.lottoNumbers = new ArrayList<>();
+        this.lottoNumbers = initLottoNumbers(lottoNumbers);
+        Collections.shuffle(this.lottoNumbers);
+    }
+
+    private List<Integer> initLottoNumbers(List<Integer> lottoNumbers) {
+        for (int i = LOTTO_BASE_START_NUMBER; i <= LOTTO_BASE_END_NUMBER; i++) {
+            lottoNumbers.add(i);
+        }
+        return lottoNumbers;
+    }
+
+    public void lottoNumberRangeException(LottoNumbers lottoNumbers) {
+        for (int number : lottoNumbers.lottoNumbers) {
+            lottoNumberRangeCheck(number);
+        }
+    }
+
+    private void lottoNumberRangeCheck(int number) {
+        if (number < LOTTO_BASE_END_NUMBER || number < LOTTO_BASE_START_NUMBER) {
+            throw new RuntimeException(INVALID_LOTTO_NUMBER);
+        }
+    }
+
+    public Stream<Integer> getLottoNumbers() {
+        return lottoNumbers.stream();
+    }
+
+    public long count() {
+        return lottoNumbers.stream()
+                .count();
+    }
+
+}
---
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class LottoNumbersTest {
+
+    private LottoNumbers lottoNumbers;
+
+    @BeforeEach
+    void setUp() {
+        lottoNumbers = new LottoNumbers();
+    }
+
+    @Test
+    @DisplayName("로또 번호가 총 45개인지 확인")
+    void lottoNumberSizeCheck() {
+        assertThat(lottoNumbers.count()).isEqualTo(45);
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 번호 개수가 맞지 않은 번호 확인")
+    @ValueSource(ints = {1, -1, 100, 50})
+    void invalidLottoNumbersSize(int lottoNumberSize) {
+        assertThat(lottoNumbers.count()).isNotEqualTo(lottoNumberSize);
+    }
+
+    @Test
+    @DisplayName("로또 번호들이 1 ~ 45이내의 숫자인지 확인")
+    void lottoNumberCheck() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> {
+                    lottoNumbers.lottoNumberRangeException(lottoNumbers);
+                }).withMessageMatching(LottoNumbers.INVALID_LOTTO_NUMBER);
+    }
+}
