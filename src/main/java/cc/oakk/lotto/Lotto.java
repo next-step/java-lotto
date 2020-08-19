6f947b596c29aa1c078baaecf1933b719e072621
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-19 20:05:35 +0900

    refactor: lotto to sort and validate number range.

--- src/main/java/cc/oakk/lotto/Lotto.java
+++ src/main/java/cc/oakk/lotto/Lotto.java
@@ -1,18 +1,36 @@
 package cc.oakk.lotto;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Lotto {
+    public static final int NUMBER_COUNT = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
     private final List<Integer> numbers;
 
-    public Lotto(int first, int second, int third, int fourth, int fifth, int sixth) {
-        List<Integer> numbers = Stream.of(first, second, third, fourth, fifth, sixth)
-                .map(Lotto::throwIfBelowZero)
+    public Lotto(List<Integer> numbers) {
+        if (numbers.size() != NUMBER_COUNT) {
+            throw new IllegalArgumentException(String.format("Lotto's number size must be %d!", NUMBER_COUNT));
+        }
+
+        List<Integer> validatedList = numbers.stream()
+                .map(Lotto::throwIfNotValidNumber)
+                .sorted(Comparator.comparingInt(n -> n))
                 .collect(Collectors.toList());
-        this.numbers = Collections.unmodifiableList(numbers);
+
+        this.numbers = Collections.unmodifiableList(validatedList);
+    }
+
+    public static Lotto of(int first, int second, int third, int fourth, int fifth, int sixth) {
+        List<Integer> numbers = Stream.of(first, second, third, fourth, fifth, sixth)
+                .collect(Collectors.toList());
+        return new Lotto(numbers);
     }
 
     public Rank compare(Lotto target) {
@@ -22,9 +40,27 @@ public class Lotto {
         return Rank.getRankByMatchingCount(matchingCount);
     }
 
-    private static int throwIfBelowZero(int target) {
-        if (target <= 0) {
-            throw new IllegalArgumentException(String.format("%d is below zero.", target));
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+    private static int throwIfNotValidNumber(int target) {
+        if (target < MIN_NUMBER || target > MAX_NUMBER) {
+            throw new IllegalArgumentException(String.format("%d is not between 0 and %d.", target, MAX_NUMBER));
         }
         return target;
     }
--- src/test/java/cc/oakk/lotto/LottoTest.java
+++ src/test/java/cc/oakk/lotto/LottoTest.java
@@ -2,26 +2,38 @@ package cc.oakk.lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
     @Test
-    public void constructor_ShouldThrow_OnBelowZero() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(1, 2, 3, 4, 5, -1));
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(1, 2, 3, 4, 5, 0));
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(1, 2, 3, 4, 5, Integer.MIN_VALUE));
+    public void constructor_ShouldThrow_OnInvalidNumber() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(1, 2, 3, 4, 5, Lotto.MIN_NUMBER - 1));
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(1, 2, 3, 4, 5, Lotto.MAX_NUMBER + 1));
+    }
+
+    @Test
+    public void constructor_ShouldThrow_OnInvalidNumberCount() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(IntStream.range(1, Lotto.NUMBER_COUNT).boxed().collect(Collectors.toList())));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(IntStream.range(1, Lotto.NUMBER_COUNT + 2).boxed().collect(Collectors.toList())));
     }
 
     @Test
     public void compare() {
-        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
-        assertThat(lotto.compare(new Lotto(7, 8, 9, 10, 11,12))).isEqualTo(Rank.SEVENTH);
-        assertThat(lotto.compare(new Lotto(6, 7, 8, 9, 10, 11))).isEqualTo(Rank.SIXTH);
-        assertThat(lotto.compare(new Lotto(5, 6, 7, 8, 9, 10))).isEqualTo(Rank.FIFTH);
-        assertThat(lotto.compare(new Lotto(4, 5, 6, 7, 8, 9))).isEqualTo(Rank.FOURTH);
-        assertThat(lotto.compare(new Lotto(3, 4, 5, 6, 7, 8))).isEqualTo(Rank.THIRD);
-        assertThat(lotto.compare(new Lotto(2, 3, 4, 5, 6, 7))).isEqualTo(Rank.SECOND);
-        assertThat(lotto.compare(new Lotto(1, 2, 3, 4, 5, 6))).isEqualTo(Rank.FIRST);
+        Lotto lotto = Lotto.of(1, 2, 3, 4, 5, 6);
+        assertThat(lotto.compare(Lotto.of(7, 8, 9, 10, 11,12))).isEqualTo(Rank.SEVENTH);
+        assertThat(lotto.compare(Lotto.of(6, 7, 8, 9, 10, 11))).isEqualTo(Rank.SIXTH);
+        assertThat(lotto.compare(Lotto.of(5, 6, 7, 8, 9, 10))).isEqualTo(Rank.FIFTH);
+        assertThat(lotto.compare(Lotto.of(4, 5, 6, 7, 8, 9))).isEqualTo(Rank.FOURTH);
+        assertThat(lotto.compare(Lotto.of(3, 4, 5, 6, 7, 8))).isEqualTo(Rank.THIRD);
+        assertThat(lotto.compare(Lotto.of(2, 3, 4, 5, 6, 7))).isEqualTo(Rank.SECOND);
+        assertThat(lotto.compare(Lotto.of(1, 2, 3, 4, 5, 6))).isEqualTo(Rank.FIRST);
     }
 }
