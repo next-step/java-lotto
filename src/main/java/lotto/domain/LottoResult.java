ff2607ec1044d2cb47a2f581c687580ded5bce53
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-20 03:35:27 +0900

    (refactor) 로또 결과 분석 리팩토

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,37 +1,40 @@
 package lotto.domain;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LottoResult {
 
     private static final int RATE = 100;
+
     private Map<WinningPrize, Integer> lottoResult = new LinkedHashMap<>();
 
     public LottoResult() {
         Arrays.stream(WinningPrize.values())
-                .forEach(winningValue -> this.lottoResult.put(winningValue, 0));
+                .forEach(winningPrize -> this.lottoResult.put(winningPrize, 0));
     }
 
     public int calculatePrizeRate(int money) {
         return this.lottoResult.entrySet().stream()
-                .mapToInt(result -> result.getKey().getPrize() * result.getValue()).sum() / money * RATE;
+                .mapToInt(result -> result.getKey().getPrize() * result.getValue()).sum() * RATE / money ;
     }
 
     public void analyzeLottoRank(Set<LottoTicket> lottoTickets, WinningLotto winningLotto) {
         lottoTickets.stream()
                 .map(lottoTicket -> countMatchNumber(lottoTicket, winningLotto))
-                .forEach(this::recordWinningResult);
+                .forEach(winningPrize -> recordWinningResult(winningPrize));
     }
 
-    private WinningPrize countMatchNumber(LottoTicket lotto, WinningLotto winningLotto) {
-        int matchCount = lotto.countMatchNumbers(winningLotto.getWinningLottoNumber());
-        return WinningPrize.valueOf(matchCount);
+    private WinningPrize countMatchNumber(LottoTicket lottoTicket, WinningLotto winningLotto) {
+        int matchCount = lottoTicket.countMatchNumbers(winningLotto.getWinningLottoNumber());
+        WinningPrize winningPrize = WinningPrize.valueOf(matchCount);
+        return winningPrize;
     }
 
     private void recordWinningResult(WinningPrize winningValue) {
         lottoResult.put(winningValue, lottoResult.get(winningValue) + 1);
+    }
+
+    public Map<WinningPrize, Integer> getLottoResult() {
+        return Collections.unmodifiableMap(lottoResult);
     }
 }
