3a8554381460ef5f124cc379b6df118e000f31ae
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-20 03:40:37 +0900

    (feat) 화면 담당 OutputView 객체 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,44 @@
+package lotto.view;
+
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import lotto.domain.WinningPrize;
+
+import java.util.*;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printPurchaseTicketCount(int ticketCount) {
+        System.out.println(String.format("총 %d 장의 로또 티켓을 구매했습니다.", ticketCount));
+    }
+
+    public static void printLottoNumbers(Set<LottoTicket> lottoTickets) {
+        for (LottoTicket lottoTicket : lottoTickets) {
+            List<Integer> sortedLottoNumbers = sortLottoNumber(lottoTicket.getLottoTicketNumbers());
+            System.out.println(sortedLottoNumbers);
+        }
+        System.out.println();
+    }
+
+    private static List<Integer> sortLottoNumber(Set<LottoNumber> lottoNumbers) {
+        List<Integer> sortedLottoNumbers = new ArrayList<>();
+        lottoNumbers.stream().sorted(Comparator.comparing(LottoNumber::getLottoNumber)).forEach(lottoNumber -> sortedLottoNumbers.add(lottoNumber.getLottoNumber()));
+        return sortedLottoNumbers;
+    }
+
+    public static void printLottoResults(Map<WinningPrize, Integer> lottoResult) {
+        System.out.println("[당첨 통계]\n" + "========================");
+        lottoResult.entrySet().forEach(result -> {
+            System.out.print(result.getKey().getMessage());
+            System.out.println(String.format("(%d 원) - %d 개", result.getKey().getPrize(), result.getValue()));
+        });
+        System.out.println();
+    }
+
+    public static void printPrizeRate(int calculatePrizeRate) {
+        System.out.println(String.format("총 수익률은 %d", calculatePrizeRate) + "% 입니다.");
+    }
+}
