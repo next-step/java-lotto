ff716fb5d15bccb1edb6e3eb23dfa1145e94d8ef
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-20 03:36:02 +0900

    (refactor) 로또 가게 리팩토링

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,32 +1,32 @@
 package lotto.domain;
 
-import java.util.Set;
-
 public class LottoShop {
-    private static final int DEFAULT_PRICE_UNIT = 1000;
-    private static final String PRICE_EMPTY_ERROR = "금액은 최소 0이상 이어야 구매 가능합니다.";
-    private static final String PRICE_DIVIDE_ERROR = "지불한 금액이 1000원으로 나누어지지 않습니다.";
+    private static final int DEFAULT_LOTTO_PRICE = 1000;
 
-    public static Set<LottoTicket> buyLotto(int price) {
-        validatePriceIsEmpty(price);
-        validatePriceIsDivide(price);
+    private LottoShop() {
+    }
 
-        int lottoTicketCount = price / DEFAULT_PRICE_UNIT;
+    public static PublishedLottoTicket buyLotto(int money) {
+        validatePriceIsEmpty(money);
+        validatePriceIsDivide(money);
+
+        LottoTicketCount lottoTicketCount = new LottoTicketCount(money);
         LottoMachine.getInstance();
-        PublishedLottoTicket publishedLottoTicket = new PublishedLottoTicket(lottoTicketCount);
-        return publishedLottoTicket.getPublishedLottoTicket();
+        PublishedLottoTicket publishedLottoTicket = new PublishedLottoTicket(lottoTicketCount.getLottoTicketCount());
+
+        return publishedLottoTicket;
     }
 
 
     private static void validatePriceIsEmpty(int price) {
         if(price <= 0) {
-            throw new IllegalArgumentException(PRICE_EMPTY_ERROR);
+            throw new IllegalArgumentException("금액은 최소 0이상 이어야 구매 가능합니다.");
         }
     }
 
     private static void validatePriceIsDivide(int price) {
-        if(price % DEFAULT_PRICE_UNIT != 0) {
-            throw new IllegalArgumentException(PRICE_DIVIDE_ERROR);
+        if(price % DEFAULT_LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("지불한 금액이 1000원으로 나누어지지 않습니다.");
         }
     }
 }
--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -10,21 +10,21 @@ public class LottoShopTest {
 
     @Test
     @DisplayName("구매 금액에 따라 정확한 개수 구매 테스트")
-    void buyLotto() {
+    void buy_lotto() {
         int price = 14000;
-        assertThat(LottoShop.buyLotto(price)).hasSize(price / 1000);
+        assertThat(LottoShop.buyLotto(price).getPublishedLottoTicket()).hasSize(price / 1000);
     }
 
     @Test
     @DisplayName("구매 금액 0 또는 음수 테스트")
-    void zero_or_negative_Price_test() {
+    void zero_or_negative_price() {
         assertThatThrownBy(() -> LottoShop.buyLotto(0)).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> LottoShop.buyLotto(-1000)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("구매 금액이 천원 단위가 아닌 경우 테스트")
-    void price_not_divided_1000_test() {
+    void price_not_divided_1000() {
         assertThatThrownBy(() -> LottoShop.buyLotto(500)).isInstanceOf(IllegalArgumentException.class);
     }
 }
