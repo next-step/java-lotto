98ba3d8616280e18de124cb950e011ae0665117c
hJJANG <dacapolife87@gmail.com>
2020-08-19 22:11:23 +0900

    feat : 로또 메시지 문구상수
    로또의 각 상황별문구 또는 에러 문구를 상수로 관리하였습니다.

--- src/main/java/nextstep/lotto/constant/ExceptionMessage.java
+++ src/main/java/nextstep/lotto/constant/ExceptionMessage.java
@@ -4,4 +4,7 @@ public class ExceptionMessage {
 
     public static final String INVALID_INPUT_ONLY_NUMBER = "입력 문자열은 숫자이외의 값이 될수 없습니다.";
     public static final String INVALID_INPUT_ONLY_POSITIVE_NUMBER = "입력 문자열은 음수 가 될수 없습니다.";
+
+    public static final String INVALID_INPUT_MINIMUM_AMOUNT_FOR_BUY = "최소 구입금액은 1000원 입니다.";
+    public static final String INVALID_INPUT_LOTTO_NUMBER_RANGE = "로또 번호의 범위는 1~45 사이입니다.";
 }
---
+++ src/main/java/nextstep/lotto/constant/PrintMessage.java
@@ -0,0 +1,7 @@
+package nextstep.lotto.constant;
+
+public class PrintMessage {
+    public static final String QUESTION_TEXT_BUY_TOTAL_AMOUNT = "구매금액을 입력해 주세요.";
+    public static final String RESULT_TEXT_BUY_TOTAL_AMOUNT = "개를 구매했습니다.";
+    public static final String QUESTION_TEXT_LOTTO_WINNER_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+}
