5558b1766402776b8cd84e093faa90c309a9aa89
samkko <wkvk1234@gmail.com>
2020-08-20 00:52:39 +0900

    [step04][refactor] BonusNumber class <> LottoNumber class  IS-A -> HAS-A구조로 변경

--- src/main/java/com/hskim/lotto/model/BonusNumber.java
+++ src/main/java/com/hskim/lotto/model/BonusNumber.java
@@ -3,11 +3,14 @@ package com.hskim.lotto.model;
 import com.hskim.lotto.exception.LottoExceptionMessage;
 
 import java.util.List;
+import java.util.Objects;
 
-public class BonusNumber extends LottoNumber {
+public class BonusNumber {
+
+    private LottoNumber bonusNumber;
 
     public BonusNumber(int bonusNumber, List<LottoNumber> winningNumberList) {
-        super(bonusNumber);
+        this.bonusNumber = new LottoNumber(bonusNumber);
         validateBonusNumber(bonusNumber, winningNumberList);
     }
 
@@ -15,5 +18,22 @@ public class BonusNumber extends LottoNumber {
         if (winningNumberList.contains(new LottoNumber(bonusNumber))) {
             throw new IllegalArgumentException(LottoExceptionMessage.DUPLICATED_BONUS_NUMBER.getMessage());
         }
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BonusNumber)) return false;
+        BonusNumber that = (BonusNumber) o;
+        return Objects.equals(bonusNumber, that.bonusNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bonusNumber);
     }
 }
