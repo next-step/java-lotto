24952efeb50e7ab7c7bea568c8ec3a19d9f059ec
박찬석 <pcspapa@gmail.com>
2020-08-19 21:31:58 +0900

    refactor: function 을 이용한 scanner 없애기

--- src/main/java/step2/LottoSalesman.java
+++ src/main/java/step2/LottoSalesman.java
@@ -1,42 +1,32 @@
 package step2;
 
 import java.util.Scanner;
-import java.util.function.Function;
 
 public class LottoSalesman {
 
-  private static final Function<Scanner, Integer> AMOUNT_QUEST = (s) -> s.nextInt();
   private static final String SCRIP_AMOUNT = "구입금액을 입력해 주세요.";
-
-  private static final Function<Scanner, Integer> MANUAL_LOTTO_COUNT_QUEST = (s) -> s.nextInt();
   private static final String SCRIP_MANUAL_LOTTO_COUNT = "\n수동으로 구매할 로또 수를 입력해 주세요.";
-
-  private static final Function<Scanner, String> INPUT_LOTTO_NUMBERS_QUEST = (s) -> s.next();
   private static final String SCRIP_INPUT_LOTTO_NUMBERS = "\n수동으로 구매할 번호를 입력해 주세요.";
-
-  private static final Function<Scanner, String> WINNING_NUMBER_QUEST = (s) -> s.next();
   private static final String SCRIPT_WINNING_NUMBER = "\n지난 주 당첨 번호를 입력해 주세요.";
-
-  private static final Function<Scanner, Integer> BONUS_NUMBER_QUEST = (s) -> s.nextInt();
   private static final String SCRIPT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
-
   private static final String SCRIPT_INFORM_WINNING = "\n당첨 통계";
 
   public static int purchaseAmount(Scanner scanner) {
     System.out.println(SCRIP_AMOUNT);
-    return AMOUNT_QUEST.apply(scanner);
+
+    return scanner.nextInt();
   }
 
   public static int manualLottoCount(Scanner scanner) {
     System.out.println(SCRIP_MANUAL_LOTTO_COUNT);
-    return MANUAL_LOTTO_COUNT_QUEST.apply(scanner);
+    return scanner.nextInt();
   }
 
   public static String[] inputManualLotto(Scanner scanner, int lottoCount) {
     System.out.println(SCRIP_INPUT_LOTTO_NUMBERS);
     String lottoNumbers[] = new String[lottoCount];
     for (int i = 0; i < lottoCount; i++) {
-      lottoNumbers[i] = INPUT_LOTTO_NUMBERS_QUEST.apply(scanner);
+      lottoNumbers[i] = scanner.next();
     }
     return lottoNumbers;
   }
@@ -54,12 +44,12 @@ public class LottoSalesman {
 
   public static String winningNumber(Scanner scanner) {
     System.out.println(SCRIPT_WINNING_NUMBER);
-    return WINNING_NUMBER_QUEST.apply(scanner);
+    return scanner.next();
   }
 
   public static int bonusNumber(Scanner scanner) {
     System.out.println(SCRIPT_BONUS_NUMBER);
-    return BONUS_NUMBER_QUEST.apply(scanner);
+    return scanner.nextInt();
   }
 
   public static void informWinning(Scanner scanner, LottoStats analyze, int payment) {
