bb6ec21ac4eb7de90dd0d299f3478a53466bd0ea
pjhsk113 <pjhsk113@naver.com>
2020-08-20 23:19:19 +0900

    [feat] 구입 금액 입력 및 로또 구매 수량 구하는 기능추가

---
+++ src/main/java/step2/view/InputView.java
@@ -0,0 +1,40 @@
+package step2.view;
+
+import step1.exception.StringAdderExceptionHandler;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final String LOTTO_START_MESSAGE = "구입 금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER= "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String COMMA= ",";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static int inputPurchase() {
+        System.out.println(LOTTO_START_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static List<Integer> inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER);
+        scanner.nextLine();
+        return castingToList(splitNumber(scanner.nextLine()));
+    }
+
+    public static String[] splitNumber(String input) {
+        return input.split(COMMA);
+    }
+
+    public static List<Integer> castingToList(String[] input) {
+        return Arrays.stream(input)
+                .map(Integer::parseInt)
+                .peek(StringAdderExceptionHandler::validate)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/step2/domain/lotto/BuyLottoTest.java
@@ -0,0 +1,36 @@
+package step2.domain.lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import step2.util.LottoNumberGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BuyLottoTest {
+
+    @DisplayName("구입 가격에 맞는 로또 개수를 구매하는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1001 :1",
+            "12780 :12",
+            "1999 :1",
+            "234567 : 234" }, delimiter = ':')
+    public void buy_Lotto_Test(int money, int buyAmount) {
+        BuyLotto buyLotto = new BuyLotto(money);
+        assertThat(buyLotto.getLottoQuantity()).isEqualTo(buyAmount);
+    }
+
+    @DisplayName("구매 수량에 맞는 로또 번호를 생성하는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1001 :1",
+            "12780 :12",
+            "1999 :1",
+            "234567 : 234" }, delimiter = ':')
+    public void lotto_Ticket_Test(int money, int buyAmount) {
+        BuyLotto buyLotto = new BuyLotto(money);
+
+        Lottos lottoTicket = buyLotto.lottoTicket(new LottoNumberGenerator());
+
+        assertThat(lottoTicket.getLottos()).hasSize(buyAmount);
+    }
+}
