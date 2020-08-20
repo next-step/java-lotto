2a0e71535f5a882415a6a20235e302b60c191a1d
great0417 <great0417@hanmail.net>
2020-08-21 00:27:12 +0900

    change - 클래스명 변경

---
+++ src/main/java/step1/exception/CharacterAndMinusCustomException.java
@@ -0,0 +1,7 @@
+package step1.exception;
+
+public class CharacterAndMinusCustomException extends RuntimeException {
+    public CharacterAndMinusCustomException(String message) {
+        super(message);
+    }
+}
--- src/main/java/step1/exception/UserCustomException.java
+++
@@ -1,7 +0,0 @@
-package step1.exception;
-
-public class UserCustomException extends RuntimeException {
-    public UserCustomException(String message) {
-        super(message);
-    }
-}
