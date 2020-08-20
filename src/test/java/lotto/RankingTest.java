5107fa0de0ad48652bec64fb1aa3bb8d9f0317da
John <bass0223@naver.com>
2020-08-20 21:43:32 +0900

    BonusBall 추가에 따른 랭킹 계산하는 로직 변화

--- src/test/java/lotto/RankingTest.java
+++ src/test/java/lotto/RankingTest.java
@@ -13,22 +13,24 @@ public class RankingTest {
 
 	@Test
 	public void getRankingTest() {
-		Lottos lottos = Lottos.of(Arrays.asList(new Lotto(LottoNumbers.of(Arrays.asList(1, 5, 11, 24, 35, 44))),
-												new Lotto(LottoNumbers.of(Arrays.asList(1, 5, 11, 34, 35, 41))),
-												new Lotto(LottoNumbers.of(Arrays.asList(4, 6, 10, 24, 35, 41))),
-												new Lotto(LottoNumbers.of(Arrays.asList(1, 5, 11, 34, 36, 42))),
-												new Lotto(LottoNumbers.of(Arrays.asList(1, 5, 11, 34, 36, 41))),
-												new Lotto(LottoNumbers.of(Arrays.asList(3, 7, 11, 29, 30, 41))),
-												new Lotto(LottoNumbers.of(Arrays.asList(3, 5, 11, 29, 30, 41)))));
+		Lottos lottos = Lottos.of(Arrays.asList(Lotto.of(LottoNumbers.of(Arrays.asList(1, 5, 11, 24, 35, 44))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(1, 5, 11, 34, 35, 41))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(4, 6, 10, 24, 35, 41))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(1, 5, 11, 34, 36, 42))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(1, 5, 11, 34, 36, 41))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(3, 7, 11, 29, 30, 41))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(3, 5, 11, 29, 30, 41))),
+												Lotto.of(LottoNumbers.of(Arrays.asList(1, 5, 10, 11, 34, 41)))));
 
-		WinningBalls winningBalls = WinningBallsFactory.create("1,5,11,34,36,42");
+		WinningBalls winningBalls = WinningBallsFactory.create("1,5,11,34,36,42", "10");
 		List<Ranking> rankings = lottos.getRankings(winningBalls);
-		assertThat(rankings).containsExactly(Ranking.FOURTH,
-											 Ranking.THIRD,
+		assertThat(rankings).containsExactly(Ranking.FIFTH,
+											 Ranking.FOURTH,
 											 Ranking.FAILURE,
 											 Ranking.FIRST,
-											 Ranking.SECOND,
+											 Ranking.THIRD,
 											 Ranking.FAILURE,
-											 Ranking.FAILURE);
+											 Ranking.FAILURE,
+											 Ranking.SECOND);
 	}
 }
