a84277ba8418ff339ed02f9537a706ec49cf8501
김다희 <sefide36@tmon.co.kr>
2020-08-20 20:27:58 +0900

    test: LottoTickets - WinningResult 생성 테스트코드 작성

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -2,7 +2,11 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,5 +21,33 @@ public class LottoTicketsTest {
         LottoTickets lottoTickets = LottoTickets.of(buyCount, LOTTO_TICKET_RANDOM_MAKER);
 
         assertThat(lottoTickets.number()).isEqualTo(expectedBuyCount);
+    }
+
+    @DisplayName("LottoTickets - WinningResult 생성 테스트")
+    @ParameterizedTest
+    @MethodSource("makeForGettingWinningResultData")
+    void getWinningResult(BuyCount buyCount, String selectedLottoNumber,
+                          String winningLottoNumbers, String bonusNumber,
+                          LottoRank rank, long rankCount, double expectedWinningRate) {
+        LottoTickets lottoTickets = LottoTickets.of(buyCount, new LottoTicketSelectMaker(selectedLottoNumber));
+
+        WinningLotto winningLotto = WinningLotto.of(winningLottoNumbers, bonusNumber);
+        WinningResult winningResult = lottoTickets.getWinningResult(winningLotto);
+
+        assertThat(winningResult.getRankCount(rank)).isEqualTo(rankCount);
+        assertThat(winningResult.calculateWinningRate()).isEqualTo(expectedWinningRate);
+    }
+
+    private static Stream<Arguments> makeForGettingWinningResultData() {
+        return Stream.of(
+                Arguments.of(BuyCount.of("3000"), "1,2,3,4,11,12", "1,2,3,4,5,6", "7"
+                        , LottoRank.THIRD, 3, (50_000*3)/3000.0),
+                Arguments.of(BuyCount.of("1000"), "1,2,3,4,11,12", "1,2,3,4,5,6", "7"
+                        , LottoRank.FIRST, 0, 50_000/1000.0),
+                Arguments.of(BuyCount.of("1000"), "1,2,3,4,5,7", "1,2,3,4,5,6", "7"
+                        , LottoRank.SECOND_BONUS, 1, 30_000_000/1000.0),
+                Arguments.of(BuyCount.of("5000"), "1,2,10,11,12,13", "1,2,3,4,5,6", "7"
+                        , LottoRank.DROP, 5, 0/5000.0)
+        );
     }
 }
