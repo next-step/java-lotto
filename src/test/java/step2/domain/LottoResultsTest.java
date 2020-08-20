7f6cf92faa023c3c8f3e65014a238296e0c47d8a
ap-jylee <e200uk@gmail.com>
2020-08-21 00:28:47 +0900

    refactor: 리팩토링
    1. 임포트 정리

--- src/test/java/step2/domain/LottoResultsTest.java
+++ src/test/java/step2/domain/LottoResultsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
