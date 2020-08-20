2e73b66b4d92c00328bfdfdcdea8933e461fd8ee
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-21 00:26:18 +0900

    feat(result): calculate revenue of Lottos

--- src/main/java/cc/oakk/lotto/model/LottoResults.java
+++ src/main/java/cc/oakk/lotto/model/LottoResults.java
@@ -1,5 +1,7 @@
 package cc.oakk.lotto.model;
 
+import cc.oakk.lotto.view.printer.Printable;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,9 +11,11 @@ import static cc.oakk.lotto.util.ValidationAdapters.throwIfNull;
 
 public class LottoResults implements Printable<LottoResults> {
     private final Map<Rank, List<LottoResult>> rankMap;
+    private final List<LottoResult> lottoResultList;
 
     public LottoResults(List<LottoResult> lottoResultList) {
         this.rankMap = new HashMap<>();
+        this.lottoResultList = lottoResultList;
         throwIfNull(lottoResultList).forEach(this::put);
     }
 
@@ -27,6 +31,17 @@ public class LottoResults implements Printable<LottoResults> {
         List<LottoResult> results = new ArrayList<>();
         rankMap.put(rank, results);
         return results;
+    }
+
+    public int size() {
+        return lottoResultList.size();
+    }
+
+    public long calculateRevenue(LottoPrizeProvider<?> provider) {
+        throwIfNull(provider);
+        return lottoResultList.stream()
+                .map(r -> provider.getPrizeByRank(r.getRank()).getValue())
+                .reduce(0L, (acc, cur) -> acc += cur);
     }
 
     public int getRankCount(Rank rank) {
--- src/test/java/cc/oakk/lotto/model/LottoResultsTest.java
+++ src/test/java/cc/oakk/lotto/model/LottoResultsTest.java
@@ -4,6 +4,8 @@ import cc.oakk.lotto.model.Lotto;
 import cc.oakk.lotto.model.LottoResults;
 import cc.oakk.lotto.model.Lottos;
 import cc.oakk.lotto.model.Rank;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,8 +13,10 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoResultsTest {
-    @Test
-    public void getRankCount() {
+    LottoResults results;
+
+    @BeforeEach
+    public void beforeEach() {
         Lotto winningLotto = Lotto.of(1, 3, 5, 7, 9, 11);
         Lottos lottos = new Lottos(Arrays.asList(
                 Lotto.of(1, 2,3, 4, 5, 6),
@@ -22,13 +26,22 @@ public class LottoResultsTest {
                 Lotto.of(1, 4, 8, 12, 16, 20),
                 Lotto.of(1, 14, 18, 22, 26, 30)));
 
-        LottoResults results = lottos.getResults(winningLotto);
+        results = lottos.getResults(winningLotto);
+    }
 
+    @Test
+    public void getRankCount() {
         assertThat(results.getRankCount(Rank.FIRST)).isEqualTo(1);
         assertThat(results.getRankCount(Rank.SECOND)).isEqualTo(2);
         assertThat(results.getRankCount(Rank.THIRD)).isEqualTo(0);
         assertThat(results.getRankCount(Rank.FOURTH)).isEqualTo(1);
         assertThat(results.getRankCount(Rank.FIFTH)).isEqualTo(0);
         assertThat(results.getRankCount(Rank.NONE)).isEqualTo(2);
+    }
+
+    @Test
+    public void calculateRevenue() {
+        LottoPrizeProvider<?> provider = new SimpleLottoPrizeProvider();
+        assertThat(results.calculateRevenue(provider)).isEqualTo(2003005000L);
     }
 }
