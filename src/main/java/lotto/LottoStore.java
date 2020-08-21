9c9d73bf0ccd9d1ba46da6ae8e2bc808e59cbc1c
ted.kang <ted.kang@kakaocorp.com>
2020-08-22 00:49:56 +0900

    feature 5. 예외 처리를 통해 예외가 발생하면 처음 시작으로 돌아가게 유도

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,8 +1,13 @@
 package lotto;
 
-import lotto.domain.*;
+import lotto.domain.ConfirmResults;
+import lotto.domain.GameWinningCondition;
+import lotto.domain.LottoGames;
+import lotto.domain.PurchaseStandBy;
 import lotto.ui.DisplayHere;
 import lotto.ui.OrderHere;
+
+import static lotto.constants.MessageConstant.NEW_LINE;
 
 public class LottoStore {
 
@@ -11,18 +16,24 @@ public class LottoStore {
 	public static void main(String[] args) {
 		OrderHere orderHere = new OrderHere();
 		DisplayHere displayHere = new DisplayHere();
+		boolean result;
+		do {
+			 result = runProcess(orderHere, displayHere);
+		} while (result);
+	}
 
-		PurchaseStandBy purchaseStandBy = orderHere.orderNewPurchasing();
-
-		LottoGames purchasedGames = purchaseStandBy.purchase();
-
-		displayHere.printPurchasedGames(purchasedGames);
-
-		GameWinningCondition lastWeekPrize = orderHere.receiveLastWeekPrize();
-
-		ConfirmResults confirmResults = purchasedGames.confirmPrize(lastWeekPrize);
-
-		displayHere.printPrizeStatistics(purchaseStandBy, confirmResults);
-
+	private static boolean runProcess(OrderHere orderHere, DisplayHere displayHere) {
+		try{
+			PurchaseStandBy purchaseStandBy = orderHere.orderNewPurchasing();
+			LottoGames purchasedGames = purchaseStandBy.purchase();
+			displayHere.printPurchasedGames(purchaseStandBy, purchasedGames);
+			GameWinningCondition lastWeekPrize = orderHere.receiveLastWeekPrize();
+			ConfirmResults confirmResults = purchasedGames.confirmPrize(lastWeekPrize);
+			displayHere.printPrizeStatistics(purchaseStandBy, confirmResults);
+			return false;
+		} catch (LottoException e) {
+			displayHere.printErrorMessage(e.getMessage() + NEW_LINE);
+			return true;
+		}
 	}
 }
