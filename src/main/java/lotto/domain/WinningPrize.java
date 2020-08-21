d5195bbad79abaf14020242aa09c28e695afba83
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-22 02:58:53 +0900

    (refactor) ENUM 숫자 표기 수정

--- src/main/java/lotto/domain/WinningPrize.java
+++ src/main/java/lotto/domain/WinningPrize.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public enum WinningPrize {
 
     ZERO(0, 0, "꽝"),
-    FORTH(3, 5000, "3개 일치"),
-    THIRD(4, 50000, "4개 일치"),
-    SECOND(5, 1500000, "5개 일치"),
-    FIRST(6, 2000000000, "6개 일치");
+    FORTH(3, 5_000, "3개 일치"),
+    THIRD(4, 50_000, "4개 일치"),
+    SECOND(5, 1_500_000, "5개 일치"),
+    FIRST(6, 2_000_000_000, "6개 일치");
 
     private final int matchCount;
     private final int prize;
