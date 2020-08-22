79731481cf0774e3aeb4e964472e1fe3d90c65c1
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-23 01:22:01 +0900

    (refactor) 로또 2등 추가 및 테스트

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -5,12 +5,26 @@ import java.util.Set;
 public class WinningLotto {
 
     private LottoTicket winningLottoNumber;
+    private LottoNumber bonusBall;
 
-    public WinningLotto(Set<LottoNumber> lottoNumbers) {
+    public WinningLotto(Set<LottoNumber> lottoNumbers, int bonusBall) {
         this.winningLottoNumber = new LottoTicket(lottoNumbers);
+        validateBonusBallDuplication(bonusBall);
+        this.bonusBall = new LottoNumber(bonusBall);
+    }
+
+    public void validateBonusBallDuplication(int bonusBall) {
+        if (winningLottoNumber.getLottoTicketNumbers().contains(new LottoNumber(bonusBall))) {
+            throw new IllegalArgumentException("보너스 볼이 당첨 번호에 포함되어 있습니다. 다른 값을 입력하세요.");
+        }
     }
 
     public LottoTicket getWinningLottoNumber() {
         return winningLottoNumber;
+    }
+
+    public boolean isBonusBall(LottoTicket lottoTicket) {
+        return lottoTicket.getLottoTicketNumbers()
+                .contains(this.bonusBall);
     }
 }
--- src/main/java/lotto/domain/WinningPrize.java
+++ src/main/java/lotto/domain/WinningPrize.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public enum WinningPrize {
 
     ZERO(0, 0, "꽝"),
-    FORTH(3, 5_000, "3개 일치"),
-    THIRD(4, 50_000, "4개 일치"),
-    SECOND(5, 1_500_000, "5개 일치"),
+    FIFTH(3, 5_000, "3개 일치"),
+    FORTH(4, 50_000, "4개 일치"),
+    THIRD(5, 1_500_000, "5개 일치"),
+    SECOND(5, 30_000_000, "5개 일치, 보너스볼 일치"),
     FIRST(6, 2_000_000_000, "6개 일치");
 
     private final int matchCount;
@@ -20,7 +21,8 @@ public enum WinningPrize {
         this.message = message;
     }
 
-    static WinningPrize valueOf(int matchCount) {
+    static WinningPrize valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == SECOND.matchCount && matchBonus) return SECOND;
         return Arrays.stream(WinningPrize.values())
                 .filter(winningPrize -> winningPrize.matchCount == matchCount)
                 .findFirst()
