7dbd89277043b6eecb2545ec4ad2e4812999d85a
samkko <wkvk1234@gmail.com>
2020-08-23 19:55:30 +0900

    [step04][test] ManualTicketingNumber class 테스트 에러 수정

--- src/test/java/com.hskim.lotto/model/ManualTicketingNumTest.java
+++ src/test/java/com.hskim.lotto/model/ManualTicketingNumTest.java
@@ -15,10 +15,10 @@ public class ManualTicketingNumTest {
     @Test
     void create() {
         // given
-        ManualTicketingNumber manualTicketingNum = new ManualTicketingNumber(5, new GameNumber(0));
+        ManualTicketingNumber manualTicketingNum = new ManualTicketingNumber(5, new GameNumber(10));
 
         // when & then
-        assertThat(manualTicketingNum).isEqualTo(new ManualTicketingNumber(5, new GameNumber(0)));
+        assertThat(manualTicketingNum).isEqualTo(new ManualTicketingNumber(5, new GameNumber(10)));
     }
 
     @DisplayName("생성 실패 - 음수를 입력한 경우")
@@ -27,7 +27,7 @@ public class ManualTicketingNumTest {
     void create_유효하지않은수_실패(int ticketNum) {
         // when & then
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            new ManualTicketingNumber(ticketNum, new GameNumber(0));
+            new ManualTicketingNumber(ticketNum, new GameNumber(10));
         }).withMessage(LottoTicketingExceptionMessage.NEGATIVE_TICKETING_NUMBER.getMessage());
     }
 }
