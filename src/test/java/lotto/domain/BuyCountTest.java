9c8191b83d4bbe9f761464e36b54a8392b9e304e
sefide <sefide@naver.com>
2020-08-23 22:00:06 +0900

    test: BuyCount 테스트 로직 분리 처리(가독성 높이기 위해)

--- src/test/java/lotto/domain/BuyCountTest.java
+++ src/test/java/lotto/domain/BuyCountTest.java
@@ -50,7 +50,11 @@ public class BuyCountTest {
     @ParameterizedTest
     @CsvSource(value = {"4000,true", "333000,true", "1000,true", "0,false"})
     void getBuyCount(String lottoMoney, boolean canBuy) {
-        assertThat(BuyCount.of(lottoMoney).canBuy()).isEqualTo(canBuy);
+        BuyCount buyCount = BuyCount.of(lottoMoney);
+
+        boolean actual = buyCount.canBuy();
+
+        assertThat(actual).isEqualTo(canBuy);
     }
 
     @DisplayName("로또 개수 빼기 테스트")
@@ -58,6 +62,9 @@ public class BuyCountTest {
     @CsvSource(value = {"4:1:3", "100:-1:101", "320:8:312"}, delimiter = ':')
     void subtract_number(int count, int subtractingCount, int expectedResult) {
         BuyCount buyCount = BuyCount.of(count);
-        assertThat(buyCount.subtract(subtractingCount)).isEqualTo(BuyCount.of(expectedResult));
+
+        BuyCount actualBuyCount = buyCount.subtract(subtractingCount);
+
+        assertThat(actualBuyCount).isEqualTo(BuyCount.of(expectedResult));
     }
 }
