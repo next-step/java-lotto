5ed82a8f9d75ed47ba2d3005174a6466d9975ed1
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-23 15:53:30 +0900

    [refac] toInt

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -2,13 +2,17 @@ package StringAddCalculator;
 
 public class StringAddCalculator {
     public static int splitAndSum(String expression) {
-        int first = Integer.parseInt(splitToExpression(expression)[0]);
+        int first = toInt(splitToExpression(expression)[0]);
         for (int i = 0; i < splitToExpression(expression).length; i += 2){
             String sumOperator = splitToExpression(expression)[i];
-            int second = Integer.parseInt(splitToExpression(expression)[i+1]);
+            int second = toInt(splitToExpression(expression)[i+1]);
             first = calculate(expression);
         }
         return first;
+    }
+
+    private static int toInt(String expression) {
+        return Integer.parseInt(expression);
     }
 
     private static String[] splitToExpression(String expression) {
