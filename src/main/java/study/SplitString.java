4687f9bd8badaaa779bceaa86a5c8231228f48d1
hyeoni90 <hyeona743@gmail.com>
2020-08-23 21:42:29 +0900

    refactor: Modify matcher group index magic number To constant

--- src/main/java/study/SplitString.java
+++ src/main/java/study/SplitString.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SplitString {
+    private static final int INDEX_OF_FIRST_GROUP_MATCHER = 1;
+    private static final int INDEX_OF_SECOND_GROUP_MATCHER = 2;
     private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
     private static final String STRING_DELIMITER = ",|:";
 
@@ -13,7 +15,8 @@ public class SplitString {
     public static String[] splitStringByDelimiter(String str) {
         Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(str);
         if (matcher.find()) {
-            return matcher.group(2).split(matcher.group(1));
+            return matcher.group(INDEX_OF_SECOND_GROUP_MATCHER)
+                    .split(matcher.group(INDEX_OF_FIRST_GROUP_MATCHER));
         }
         return str.split(STRING_DELIMITER);
     }
