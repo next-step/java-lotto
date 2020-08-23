872e54834c75fe3ae1860664b051ecd5f681db6b
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-23 15:50:15 +0900

    [refac] splitToExpression

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -1,13 +1,18 @@
 package StringAddCalculator;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String expressions) {
-        int first = Integer.parseInt(expressions.split(" ")[0]);
-        for (int i = 0; i < expressions.split(" ").length; i += 2){
-            String sumOperator = expressions.split(" ")[i];
-            int second = Integer.parseInt(expressions.split(" ")[i+1]);
-            first = calculate(expressions);
+    public static int splitAndSum(String expression) {
+        int first = Integer.parseInt(splitToExpression(expression)[0]);
+        for (int i = 0; i < splitToExpression(expression).length; i += 2){
+            String sumOperator = splitToExpression(expression)[i];
+            int second = Integer.parseInt(splitToExpression(expression)[i+1]);
+            first = calculate(expression);
         }
         return first;
+    }
+
+    private static String[] splitToExpression(String expression) {
+        String[] expressions = expression.split(" ");
+        return expressions;
     }
 }
