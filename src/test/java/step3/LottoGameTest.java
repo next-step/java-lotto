6cddd63483fb1d41c24fc0142d0bbc484d8c5ac6
Dami <fdfdsa123@hanmail.net>
2020-08-23 23:09:48 +0900

    test ($step3): 테스트 코드 추가

---
+++ src/test/java/step3/LottoGameTest.java
@@ -0,0 +1,28 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoGameTest {
+
+	@DisplayName(value = "로또 구입 금액에 오류가 있을 때 에러 처리 테스트")
+	@ParameterizedTest
+	@ValueSource(ints = {0, 500, 1500, 3256})
+	void lottoPrice(int price) {
+		assertThatIllegalArgumentException()
+				.isThrownBy(() -> new LottoGame().getLottos(price))
+				.withMessage("로또 구입 금액은 1000원 단위만 가능합니다.");
+	}
+
+	@DisplayName(value = "로또 구입 금액만큼 발급한 로또 개수 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"3000:3", "14000:14"}, delimiter = ':')
+	void lottoCount(int price, int expected) {
+		assertThat(new LottoGame().getLottos(price)).hasSize(expected);
+	}
+}
---
+++ src/test/java/step3/domain/LottoStatisticTest.java
@@ -0,0 +1,63 @@
+package step3.domain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoStatisticTest {
+	private static Lotto winningLotto;
+	private static int winningBonus;
+	private static List<Lotto> lottos;
+
+	private static Lotto getLotto(Integer[] nums) {
+		return new Lotto(
+				Arrays.stream(nums)
+						.map(LottoNumber::new)
+						.collect(Collectors.toList())
+		);
+	}
+
+	@BeforeAll
+	static void setWinningInfo() {
+		List<Integer[]> nums = Arrays.asList(
+				new Integer[] {9, 20, 32, 37, 43, 45},
+				new Integer[] {8, 17, 34, 35, 37, 40},
+				new Integer[] {1, 9, 10, 14, 16, 40},       // 4
+				new Integer[] {3, 6, 10, 14, 24, 40},       // 5 BONUS
+				new Integer[] {5, 11, 17, 26, 39, 42},
+				new Integer[] {10, 12, 17, 30, 37, 42},
+				new Integer[] {3, 6, 10, 14, 16, 40},       // 6
+				new Integer[] {2, 6, 9, 15, 32, 36}
+		);
+
+		lottos = new ArrayList<>();
+		for (Integer[] num : nums) {
+			lottos.add(getLotto(num));
+		}
+
+		int[] winning = new int[] {3, 6, 10, 14, 16, 40};
+		List<LottoNumber> winningNumbers = Arrays.stream(winning)
+				.mapToObj(LottoNumber::new)
+				.collect(Collectors.toList());
+		winningLotto = new Lotto(winningNumbers);
+		winningBonus = 24;
+	}
+
+	@DisplayName(value = "로또 당첨 통계 테스트")
+	@Test
+	void calcLottoResult() {
+		Map<LottoWinning, Integer> result = new LottoStatistic(winningLotto, winningBonus)
+				.calcLottoResult(lottos);
+
+		assertThat(result).containsEntry(LottoWinning.BONUS, 1);
+		assertThat(result).containsEntry(LottoWinning.SIX, 1);
+		assertThat(result).containsEntry(LottoWinning.FOUR, 1);
+		assertThat(result).containsEntry(LottoWinning.NOTHING, 5);
+	}
+
+}
