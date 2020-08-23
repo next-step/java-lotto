5e304a7e90a9709611788fec1da3e551e54c0545
John <bass0223@naver.com>
2020-08-23 21:37:21 +0900

    Ranking 테스트 추가

--- src/test/java/lotto/RankingTest.java
+++ src/test/java/lotto/RankingTest.java
@@ -4,8 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import lotto.factory.WinningBallsFactory;
 
@@ -33,4 +37,32 @@ public class RankingTest {
 											 Ranking.FAILURE,
 											 Ranking.SECOND);
 	}
+
+	@ParameterizedTest
+	@MethodSource("rankingParameterProvider")
+	public void getRankingTest2(Ranking ranking, Ranking except) {
+		assertThat(ranking).isEqualTo(except);
+	}
+
+	private static Stream<Arguments> rankingParameterProvider() {
+		WinningBalls winningBalls = WinningBalls.of(Lotto.of(Arrays.asList(9, 13, 19, 26, 40, 45)),
+													BonusBall.of("28"));
+
+		WinningArgument winningArgument = WinningArgument.of(Lotto.of(Arrays.asList(9, 13, 19, 26, 28, 43)),
+															 winningBalls);
+		WinningArgument winningArgument2 = WinningArgument.of(Lotto.of(Arrays.asList(10, 14, 19, 26, 28, 43)),
+															  winningBalls);
+		WinningArgument winningArgument3 = WinningArgument.of(Lotto.of(Arrays.asList(9, 13, 19, 26, 40, 45)),
+															  winningBalls);
+		WinningArgument winningArgument4 = WinningArgument.of(Lotto.of(Arrays.asList(9, 13, 19, 26, 40, 43)),
+															  winningBalls);
+
+		return Stream.of(
+				Arguments.arguments(Ranking.getRakingOf(winningArgument), Ranking.SECOND),
+				Arguments.arguments(Ranking.getRakingOf(winningArgument2), Ranking.FIFTH),
+				Arguments.arguments(Ranking.getRakingOf(winningArgument3), Ranking.FIRST),
+				Arguments.arguments(Ranking.getRakingOf(winningArgument4), Ranking.THIRD)
+						);
+	}
 }
+
