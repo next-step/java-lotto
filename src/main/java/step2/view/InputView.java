9624a3a3ab06fa8e0dd92c6e2a284c86add4e176
pjhsk113 <pjhsk113@naver.com>
2020-08-24 21:16:05 +0900

    [feat] 보너스 볼 입력 추가

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
     private static final String LOTTO_START_MESSAGE = "구입 금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER= "지난 주 당첨 번호를 입력해 주세요.";
     private static final String COMMA= ",";
+    private static final String INPUT_BONUS_NUMBER= "보너스 볼을 입력해주세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -27,11 +28,16 @@ public class InputView {
         return castingToList(splitNumber(scanner.nextLine()));
     }
 
-    public static String[] splitNumber(String input) {
+    public static int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        return scanner.nextInt();
+    }
+
+    private static String[] splitNumber(String input) {
         return input.split(COMMA);
     }
 
-    public static List<Integer> castingToList(String[] input) {
+    private static List<Integer> castingToList(String[] input) {
         return Arrays.stream(input)
                 .map(Integer::parseInt)
                 .peek(StringAdderExceptionHandler::validate)
