68960540e2305d53238a3b7a5871df20b7952c5b
김효린 <hyorin97@naver.com>
2020-08-25 04:00:10 +0900

    feat: 커스텀 문자 지정

--- src/main/java/domain/InputValue.java
+++ src/main/java/domain/InputValue.java
@@ -1,6 +1,8 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputValue {
     public static int inputValueValidate(String inputValue) {
@@ -8,9 +10,21 @@ public class InputValue {
             return 0;
         if(inputValue.isEmpty())
             return 0;
+        if(inputValue.startsWith("//"))
+            return sum(checkCustomDelimiter(inputValue));
 
         String[] numbers = inputValue.split(",|:");
         return sum(numbers);
+    }
+
+    public static String[] checkCustomDelimiter(String inputValue) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputValue);
+        if(m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
+        return new String[0];
     }
 
     private static int sum(String[] numbers) {
--- src/test/java/domain/InputValueTest.java
+++ src/test/java/domain/InputValueTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import static domain.InputValue.checkCustomDelimiter;
 import static domain.InputValue.inputValueValidate;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +27,17 @@ public class InputValueTest {
     @Test
     void 콜론으로_구분() {
         assertThat(inputValueValidate("3:2")).isEqualTo(5);
+        assertThat(inputValueValidate("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    void 커스텀_구분자_지정() {
+        assertThat(inputValueValidate("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test
+    void 커스텀_구분자_체크() {
+        assertThat(checkCustomDelimiter("//;\n1;2;3")).hasSize(3);
+        assertThat(checkCustomDelimiter("//;\n1;2;3")).containsExactly("1","2","3");
     }
 }
