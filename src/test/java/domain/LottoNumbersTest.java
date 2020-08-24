8fc8842d6a9d7c142fdbfa0795c4fa2b36e7231a
ohjuntaek <201211734@pukyong.ac.kr>
2020-08-24 21:38:58 +0900

    refactor: MethodSource 적용

--- src/test/java/domain/LottoNumbersTest.java
+++ src/test/java/domain/LottoNumbersTest.java
@@ -3,16 +3,27 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumbersTest {
+
+    private static Stream<Arguments> provideLottoNumbers() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", "1,2,3,4,5,6"),
+                Arguments.of("2,3,4,5,6,7", "2,3,4,5,6"),
+                Arguments.of("10,24,26,32,27,1", "1")
+        );
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"1,1,2,3,4,5", "1,2,3,4,5,6,7"})
@@ -28,7 +39,7 @@ class LottoNumbersTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3,4,5,6=1,2,3,4,5,6", "2,3,4,5,6,7=2,3,4,5,6", "10,24,26,32,27,1=1"}, delimiter = '=')
+    @MethodSource("provideLottoNumbers")
     @DisplayName("자신이 가진 로또 번호와 제공된 로또 번호에서 일치하는 번호 리스트를 제공한다.")
     void getContainNumbersTest(String input, String expectedInput) {
         List<Number> numbers = makeInputToNumbers(input);
