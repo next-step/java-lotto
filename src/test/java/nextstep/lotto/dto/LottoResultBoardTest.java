61068178e4b1ebe20f180e0ba1990177010dab67
hJJANG <dacapolife87@gmail.com>
2020-08-24 22:46:21 +0900

    test : 로또 결과 테스트 케이스변경
    보너스 번호의 추가로 테스트케이스가 변경되었습니다.

--- src/test/java/nextstep/lotto/dto/LottoResultBoardTest.java
+++ src/test/java/nextstep/lotto/dto/LottoResultBoardTest.java
@@ -13,23 +13,23 @@ class LottoResultBoardTest {
     @Test
     void lottoBoard() {
         LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(6));
-        resultBoard.addLottoResult(LottoRank.of(6));
-        resultBoard.addLottoResult(LottoRank.of(6));
+        resultBoard.addLottoResult(LottoRank.of(6,false));
+        resultBoard.addLottoResult(LottoRank.of(6,false));
+        resultBoard.addLottoResult(LottoRank.of(6,false));
         Map<LottoRank, Integer> lottoResult = resultBoard.getLottoResult();
 
-        assertThat(lottoResult.get(LottoRank.SIX)).isEqualTo(3);
+        assertThat(lottoResult.get(LottoRank.FIRST)).isEqualTo(3);
     }
 
     @Test
     @DisplayName("5천원구입으로 5등수익률")
     void lottoBenefit_5등_Test() {
         LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
         assertThat(resultBoard.getBenefitRate()).isEqualTo(0);
     }
 
@@ -37,11 +37,11 @@ class LottoResultBoardTest {
     @DisplayName("5천원구입으로 4등수익률")
     void lottoBenefit_4등_Test() {
         LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(3));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
+        resultBoard.addLottoResult(LottoRank.of(3,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
         assertThat(resultBoard.getBenefitRate()).isEqualTo(1);
     }
 
@@ -49,11 +49,11 @@ class LottoResultBoardTest {
     @DisplayName("5천원구입으로 3등수익률")
     void lottoBenefit_3등_Test() {
         LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(4));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
+        resultBoard.addLottoResult(LottoRank.of(4,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
         assertThat(resultBoard.getBenefitRate()).isEqualTo(10);
     }
 
@@ -61,23 +61,23 @@ class LottoResultBoardTest {
     @DisplayName("5천원구입으로 2등수익률")
     void lottoBenefit_2등_Test() {
         LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(5));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        assertThat(resultBoard.getBenefitRate()).isEqualTo(300);
+        resultBoard.addLottoResult(LottoRank.of(5,true));
+        resultBoard.addLottoResult(LottoRank.of(0,true));
+        resultBoard.addLottoResult(LottoRank.of(0,true));
+        resultBoard.addLottoResult(LottoRank.of(0,true));
+        resultBoard.addLottoResult(LottoRank.of(0,true));
+        assertThat(resultBoard.getBenefitRate()).isEqualTo(6000);
     }
 
     @Test
     @DisplayName("5천원구입으로 1등수익률")
     void lottoBenefit_1등_Test() {
         LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(6));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
-        resultBoard.addLottoResult(LottoRank.of(0));
+        resultBoard.addLottoResult(LottoRank.of(6,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
+        resultBoard.addLottoResult(LottoRank.of(0,false));
         assertThat(resultBoard.getBenefitRate()).isEqualTo(400000);
     }
 
