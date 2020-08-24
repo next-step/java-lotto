ec704ecd952e7779fe52651319f0c408937a57a8
ap-jylee <e200uk@gmail.com>
2020-08-25 04:19:50 +0900

    refactor: 로또 번호 범위 글로벌 상수로 추출

--- src/main/java/step3/domain/LottoConfig.java
+++ src/main/java/step3/domain/LottoConfig.java
@@ -2,8 +2,8 @@ package step3.domain;
 
 public class LottoConfig {
 
-    public static final int WINNING_COUNT_FROM = 3;
-    public static final int WINNING_COUNT_TO = 6;
+    public static final int FIRST_LOTTO_NUMBER = 1;
+    public static final int LAST_LOTTO_NUMBER = 45;
 
     private LottoConfig() {
 
