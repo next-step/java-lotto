01b62671f3ed0d338bd6ac85441ae9586c6af025
김효린 <hyorin97@naver.com>
2020-08-25 03:30:03 +0900

    숫자 하나를 입력할 경우는 1 반환

--- src/main/java/domain/InputValue.java
+++ src/main/java/domain/InputValue.java
@@ -1,11 +1,12 @@
 package domain;
 
-public class InputValidation {
+public class InputValue {
     public static int inputValueValidate(String inputValue) {
         if(inputValue == null)
             return 0;
         if(inputValue.isEmpty())
             return 0;
-        return 1;
+
+        return Integer.parseInt(inputValue);
     }
 }
--- src/test/java/domain/InputValueTest.java
+++ src/test/java/domain/InputValueTest.java
@@ -2,13 +2,19 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
-import static domain.InputValidation.inputValueValidate;
+import static domain.InputValue.inputValueValidate;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringUtilsTest {
+public class InputValueTest {
     @Test
     void isNull() {
         assertThat(inputValueValidate("")).isEqualTo(0);
         assertThat(inputValueValidate(null)).isEqualTo(0);
+    }
+
+    @Test
+    void isOnly() {
+        assertThat(inputValueValidate("3")).isEqualTo(3);
+        assertThat(inputValueValidate("7")).isEqualTo(7);
     }
 }
