c21e55b7c9ee34dbf5e6bfde919fad6948a10ae2
김효린 <hyorin97@naver.com>
2020-08-25 04:52:27 +0900

    feat: 숫자 이외의 값 예외 처리

--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -10,6 +10,11 @@ public class NumberTest {
         assertThatThrownBy(() -> new Numbers(new String[]{"-1", "1", "2"}))
                 .isInstanceOf(RuntimeException.class);
     }
+    @Test
+    void 숫자_이외의_값이_들어올_때_테스트() {
+        assertThatThrownBy(() -> new Numbers(new String[]{"가", "1", "2"}))
+                .isInstanceOf(RuntimeException.class);
+    }
 
 
 }
