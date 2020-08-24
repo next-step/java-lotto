de8445db0be3237d89f86522fa39e7d6a581f777
jungmisu <jungmisu@tmon.co.kr>
2020-08-24 20:38:07 +0900

    feat: 수동으로 입력받은 로또를 포함하여 로또를 생성하기

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class LottoMachine {
     public static final int LOTTO_PRICE = 1000;
+    private static final String LOTTO_PRICE_INVALID = "구매 금액은 1000원 이상입니다.";
+    private static final String LOTTO_MANUAL_COUNT_EXCEED = "구매 금액보다 입력받은 로또가 더 많습니다.";
     private final LottoNumberExtractor lottoNumberExtractor;
 
     public LottoMachine(LottoNumberExtractor lottoNumberExtractor) {
@@ -13,16 +15,51 @@ public class LottoMachine {
 
     public LottoTickets makeLottoTickets(int price) {
         int count = price / LOTTO_PRICE;
-        List<LottoTicket> lottoTicketList = new ArrayList<>();
+        validateLottoTicketCount(count);
 
-        for (int i = 0; i < count; i++) {
-            lottoTicketList.add(makeAutoLottoTicket());
-        }
+        List<LottoTicket> lottoTicketList = new ArrayList<>();
+        addAutoLottoTickets(lottoTicketList, count);
 
         return new LottoTickets(lottoTicketList, price);
     }
 
+    public LottoTickets makeLottoTickets(int price, List<List<Integer>> manualLottoTicketsNumbers) {
+        int count = price / LOTTO_PRICE;
+        int manualCount = manualLottoTicketsNumbers.size();
+
+        validateLottoTicketCount(count);
+        validateManualLottoTickets(count, manualCount);
+
+        List<LottoTicket> lottoTicketList = new ArrayList<>();
+        manualLottoTicketsNumbers.forEach(numbers -> lottoTicketList.add(makeManualLottoTicket(numbers)));
+        addAutoLottoTickets(lottoTicketList, count - manualCount);
+
+        return new LottoTickets(lottoTicketList, price);
+    }
+
+    private void addAutoLottoTickets(List<LottoTicket> lottoTicketList, int autoCount) {
+        for (int i = 0; i < autoCount; i++) {
+            lottoTicketList.add(makeAutoLottoTicket());
+        }
+    }
+
     public LottoTicket makeAutoLottoTicket() {
         return new LottoTicket(lottoNumberExtractor.getNumbers(LottoTicket.LOTTO_SIZE));
+    }
+
+    public LottoTicket makeManualLottoTicket(List<Integer> manualLottoNumbers) {
+        return LottoTicket.of(manualLottoNumbers);
+    }
+
+    public void validateLottoTicketCount(int totalLottoTicketCount) {
+        if (totalLottoTicketCount < 1) {
+            throw new IllegalArgumentException(LOTTO_PRICE_INVALID);
+        }
+    }
+
+    public void validateManualLottoTickets(int totalLottoTicketCount, int manualLottoTicketCount) {
+        if (totalLottoTicketCount < manualLottoTicketCount) {
+            throw new IllegalArgumentException(LOTTO_MANUAL_COUNT_EXCEED);
+        }
     }
 }
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -1,23 +1,49 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoMachineTest {
+    private LottoMachine lottoMachine;
+
+    @BeforeEach
+    public void init() {
+        LottoNumberExtractor lottoNumberExtractor = new LottoNumberRandomExtractor();
+        lottoMachine = new LottoMachine(lottoNumberExtractor);
+    }
 
     @Test
     @DisplayName("구입 금액만큼 로또를 발급한다.")
     public void makeLottoTickets() {
-        // given
-        LottoNumberExtractor lottoNumberExtractor = new LottoNumberRandomExtractor();
-        LottoMachine lottoMachine = new LottoMachine(lottoNumberExtractor);
-
         // when
         LottoTickets lottoTickets = lottoMachine.makeLottoTickets(3000);
 
         // then
         assertThat(lottoTickets.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("수동, 자동으로 로또를 발급한다.")
+    public void makeLottoTicketsWithManualTicket() {
+        // given
+        List<List<Integer>> manualLottoNumbers = new ArrayList<>();
+        List<Integer> lottoNumbers1 = Arrays.asList(1, 20, 21, 30, 37, 40);
+        List<Integer> lottoNumbers2 = Arrays.asList(11, 15, 23, 29, 38, 43);
+        List<Integer> lottoNumbers3 = Arrays.asList(3, 8, 17, 29, 33, 45);
+        int price = 10000;
+
+        // when
+        LottoTickets lottoTickets = lottoMachine.makeLottoTickets(price, manualLottoNumbers);
+
+        // then
+        assertThat(lottoTickets.size()).isEqualTo(price/LottoMachine.LOTTO_PRICE);
+
     }
 }
