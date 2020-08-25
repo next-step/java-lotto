84b421e485b03f2bc1cdbee2b8a0ecd34c7635ee
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-26 01:03:42 +0900

    refactor: private constructor, use Objects.isNull

--- src/main/java/cc/oakk/lotto/util/ValidationAdapters.java
+++ src/main/java/cc/oakk/lotto/util/ValidationAdapters.java
@@ -1,8 +1,12 @@
 package cc.oakk.lotto.util;
 
+import java.util.Objects;
+
 public class ValidationAdapters {
+    private ValidationAdapters() {}
+
     public static<T> T throwIfNull(T obj) {
-        if (obj == null) {
+        if (Objects.isNull(obj)) {
             throw new IllegalArgumentException("Given object is null.");
         }
         return obj;
