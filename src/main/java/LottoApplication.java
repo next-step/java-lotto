ec37d9922e6114cbd00a138ea3914bc2636f5172
jsoraso <clsrn63@naver.com>
2020-08-25 10:11:53 +0900

    fix : User class 삭제

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,6 +1,5 @@
 import step2.domain.Lotto;
 import step2.domain.LottoMachine;
-import step2.domain.User;
 import step2.domain.WinningLotto;
 import step2.domain.view.InputView;
 import step2.domain.view.OutputView;
@@ -8,14 +7,10 @@ import step2.domain.view.OutputView;
 public class LottoApplication {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-
-        User user = new User(inputView.inputMoney());
-
         LottoMachine lottoMachine = new LottoMachine();
-        Lotto lotto = lottoMachine.makeLotto(user.getMoney());
+        Lotto lotto = lottoMachine.makeLotto(inputView.inputMoney());
 
-        user.setMyLotto(lotto);
+        OutputView outputView = new OutputView();
         outputView.viewLottoNumbers(lotto);
 
         WinningLotto winningLotto = new WinningLotto(inputView.inputWinningNumbers());
