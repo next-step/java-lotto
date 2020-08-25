b1d8f652601d8a038d8e7213cf596de8fbe8e731
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-26 00:57:02 +0900

    feat(range): Add LottoNumberRange

---
+++ src/main/java/cc/oakk/lotto/model/LottoNumberRange.java
@@ -0,0 +1,55 @@
+package cc.oakk.lotto.model;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoNumberRange {
+    private final int from;
+    private final int to;
+
+    public LottoNumberRange(int from, int to) {
+        if (from > to) {
+            throw new IllegalArgumentException("from is greater than to.");
+        }
+        this.from = from;
+        this.to = to;
+    }
+
+    public static LottoNumberRange between(int from, int to) {
+        return new LottoNumberRange(Math.min(from, to), Math.max(from, to));
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public boolean isValidNumber(int number) {
+        return from <= number && number <= to;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumberRange that = (LottoNumberRange) o;
+        return from == that.from &&
+                to == that.to;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(from, to);
+    }
+
+    public List<Integer> createRangeList() {
+        return IntStream.range(from, to)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
