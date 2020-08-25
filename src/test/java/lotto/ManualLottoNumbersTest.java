3437eb235e8a5d8352c9953e6ca9fa197078961a
John <bass0223@naver.com>
2020-08-25 09:49:40 +0900

    테스트 명칭 부여

--- src/test/java/lotto/ManualLottoNumbersTest.java
+++ src/test/java/lotto/ManualLottoNumbersTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class ManualLottoNumbersTest {
 
 	@Test
-	@DisplayName("")
+	@DisplayName("수동으로 생성하는 로또 테스트")
 	public void createManualLottoTest() {
 		LottoPayAmounts lottoPayAmounts = LottoPayAmounts.of("5000");
 		ManualLottoCount manualLottoNumbers = ManualLottoCount.of(lottoPayAmounts, "3");
