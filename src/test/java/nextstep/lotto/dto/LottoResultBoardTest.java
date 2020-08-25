3e1e887892a7a65c1ab4d3d18ebb396753cf8571
HyungjuJang <dacapolife87@gmail.com>
2020-08-26 00:53:45 +0900

    test : 로또 결과의 add넣는 방식과 테스트케이스 수정
    로또결과를 add를 사용하여 당첨자를 넣는 add기능을 제거하였고 이를사용하던 테스트케이스들은 생성자를 이용하여 넣는방식으로 변경하였습니다.

--- src/test/java/nextstep/lotto/dto/LottoResultBoardTest.java
+++ src/test/java/nextstep/lotto/dto/LottoResultBoardTest.java
@@ -1,84 +1,111 @@
 package nextstep.lotto.dto;
 
+import nextstep.lotto.utils.LottoNumberUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static nextstep.lotto.dto.LottoResultBoard.LOTTO_VALUE;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 class LottoResultBoardTest {
 
+    LottoTickets lottoTickets;
+    LottoWinnerNumbers winnerNumbers;
+
+    @BeforeEach
+    void setLottoTicket() {
+        List<LottoTicket> tickets = new ArrayList<>();
+
+        List<String> lottoTicket1 = Arrays.asList("1", "2", "3", "4", "5", "6");
+        List<String> lottoTicket2 = Arrays.asList("7", "8", "9", "10", "11", "12");
+        List<String> lottoTicket3 = Arrays.asList("13", "14", "15", "16", "17", "18");
+
+        tickets.add(LottoTicket.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket1)));
+        tickets.add(LottoTicket.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket2)));
+        tickets.add(LottoTicket.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket3)));
+
+        lottoTickets = LottoTickets.create(tickets);
+        winnerNumbers = LottoWinnerNumbers.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket1), LottoNumber.create(7));
+    }
+
     @Test
     void lottoBoard() {
-        LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(6, false));
-        resultBoard.addLottoResult(LottoRank.of(6, false));
-        resultBoard.addLottoResult(LottoRank.of(6, false));
-        Map<LottoRank, Integer> lottoResult = resultBoard.getLottoResult();
+        LottoResultBoard lottoResultBoard = LottoResultBoard.create(lottoTickets, winnerNumbers);
 
-        assertThat(lottoResult.get(LottoRank.FIRST)).isEqualTo(3);
+        Map<LottoRank, Long> lottoResult = lottoResultBoard.getLottoResult();
+
+        assertThat(lottoResult.get(LottoRank.FIRST)).isEqualTo(1);
     }
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(value = LottoRank.class, names = {"FIFTH"})
     @DisplayName("5천원구입으로 5등수익률")
-    void lottoBenefit_5등_Test() {
-        LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        assertThat(resultBoard.getBenefitRate()).isEqualTo(0);
+    void lottoBenefit_5등_Test(LottoRank rank) {
+        List<String> lottoTicket = Arrays.asList("1", "2", "3", "43", "44", "45");
+        LottoWinnerNumbers winnerNumbers = LottoWinnerNumbers.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket), LottoNumber.create(7));
+        LottoResultBoard lottoResultBoard = LottoResultBoard.create(lottoTickets, winnerNumbers);
+
+        float expectedRate = (float) rank.getReward() / 3000;
+        assertThat(lottoResultBoard.getBenefitRate()).isEqualTo(expectedRate);
     }
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(value = LottoRank.class, names = {"FOURTH"})
     @DisplayName("5천원구입으로 4등수익률")
-    void lottoBenefit_4등_Test() {
-        LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(3, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        assertThat(resultBoard.getBenefitRate()).isEqualTo(1);
+    void lottoBenefit_4등_Test(LottoRank rank) {
+        List<String> lottoTicket = Arrays.asList("1", "2", "3", "4", "44", "45");
+        LottoWinnerNumbers winnerNumbers = LottoWinnerNumbers.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket), LottoNumber.create(7));
+        LottoResultBoard lottoResultBoard = LottoResultBoard.create(lottoTickets, winnerNumbers);
+
+        float expectedRate = (float) rank.getReward() / 3000;
+        assertThat(lottoResultBoard.getBenefitRate()).isEqualTo(expectedRate);
     }
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(value = LottoRank.class, names = {"THIRD"})
     @DisplayName("5천원구입으로 3등수익률")
-    void lottoBenefit_3등_Test() {
-        LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(4, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        assertThat(resultBoard.getBenefitRate()).isEqualTo(10);
+    void lottoBenefit_3등_Test(LottoRank rank) {
+        List<String> lottoTicket = Arrays.asList("1", "2", "3", "4", "5", "45");
+        LottoWinnerNumbers winnerNumbers = LottoWinnerNumbers.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket), LottoNumber.create(7));
+        LottoResultBoard lottoResultBoard = LottoResultBoard.create(lottoTickets, winnerNumbers);
+
+        float expectedRate = (float) rank.getReward() / 3000;
+        assertThat(lottoResultBoard.getBenefitRate()).isEqualTo(expectedRate);
     }
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(value = LottoRank.class, names = {"SECOND"})
     @DisplayName("5천원구입으로 2등수익률")
-    void lottoBenefit_2등_Test() {
-        LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(5, true));
-        resultBoard.addLottoResult(LottoRank.of(0, true));
-        resultBoard.addLottoResult(LottoRank.of(0, true));
-        resultBoard.addLottoResult(LottoRank.of(0, true));
-        resultBoard.addLottoResult(LottoRank.of(0, true));
-        assertThat(resultBoard.getBenefitRate()).isEqualTo(6000);
+    void lottoBenefit_2등_Test(LottoRank rank) {
+        List<String> lottoTicket = Arrays.asList("1", "2", "3", "4", "5", "45");
+        LottoWinnerNumbers winnerNumbers = LottoWinnerNumbers.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket), LottoNumber.create(6));
+        LottoResultBoard lottoResultBoard = LottoResultBoard.create(lottoTickets, winnerNumbers);
+
+        float expectedRate = (float) rank.getReward() / 3000;
+        assertThat(lottoResultBoard.getBenefitRate()).isEqualTo(expectedRate);
     }
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(value = LottoRank.class, names = {"FIRST"})
     @DisplayName("5천원구입으로 1등수익률")
-    void lottoBenefit_1등_Test() {
-        LottoResultBoard resultBoard = new LottoResultBoard();
-        resultBoard.addLottoResult(LottoRank.of(6, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        resultBoard.addLottoResult(LottoRank.of(0, false));
-        assertThat(resultBoard.getBenefitRate()).isEqualTo(400000);
+    void lottoBenefit_1등_Test(LottoRank rank) {
+        List<String> lottoTicket = Arrays.asList("1", "2", "3", "4", "5", "6");
+        LottoWinnerNumbers winnerNumbers = LottoWinnerNumbers.create(LottoNumberUtil.transStringNumberToLottoNumber(lottoTicket), LottoNumber.create(7));
+        LottoResultBoard lottoResultBoard = LottoResultBoard.create(lottoTickets, winnerNumbers);
+
+        float expectedRate = (float) rank.getReward() / 3000;
+        assertThat(lottoResultBoard.getBenefitRate()).isEqualTo(expectedRate);
     }
 
 }
