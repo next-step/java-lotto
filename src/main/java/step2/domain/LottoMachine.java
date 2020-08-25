9497f0298edeefcab24cfe2929322897ac0b20c4
jsoraso <clsrn63@naver.com>
2020-08-25 23:42:00 +0900

    fix : LottoMachine 전체적으로 수정

--- src/main/java/step2/domain/LottoMachine.java
+++ src/main/java/step2/domain/LottoMachine.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    final static int LOTTO_PRICE = 1000;
+    private final static int LOTTO_PRICE = 1_000;
 
     public LottoTicket makeLotto(String money) {
-        int lottoCount = moneyToCount(money);
+        int lottoCount = getCount(money);
         List<LottoNumbers> lottoNumbersList = getLottoNumberListByLottoCount(lottoCount);
         return new LottoTicket(lottoNumbersList);
     }
 
     private List<LottoNumbers> getLottoNumberListByLottoCount(int lottoCount) {
         List<LottoNumbers> lottoNumbersList = new ArrayList<>();
-        for(int i = 0; i < lottoCount; i++) {
+        for (int i = 0; i < lottoCount; i++) {
             LottoNumbers lottoNumbers = new LottoNumbers();
             lottoNumbersList.add(lottoNumbers);
         }
         return lottoNumbersList;
     }
 
-    private int moneyToCount(String money) {
+    private int getCount(String money) {
         int moneyToInt = invalidAndToInt(money);
-        if(moneyToInt % LOTTO_PRICE != 0) {
+        if (moneyToInt % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("천원 단위로 입력해주세요.");
         }
         return moneyToInt / LOTTO_PRICE;
@@ -31,11 +31,11 @@ public class LottoMachine {
 
     private int invalidAndToInt(String money) {
         try {
-            if(money.trim().isEmpty() || money == null) {
+            if (money == null || money.trim().isEmpty()) {
                 throw new IllegalArgumentException("금액을 입력해주세요.");
             }
             return Integer.parseInt(money);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException("숫자가 아닙니다.");
         }
     }
