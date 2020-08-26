6f8fe038c92c381d276afbee4050885ed7a4043d
hanogo <trullo393@gmail.com>
2020-08-27 01:17:19 +0900

    [feat] WinningTableTest 구현 및 테스트

--- src/main/java/lotto/domain/WinningTable.java
+++ src/main/java/lotto/domain/WinningTable.java
@@ -1,4 +1,58 @@
 package lotto.domain;
 
+import lotto.context.Rank;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class WinningTable {
+    private final static Integer ADD_AUTO_INCREMENT = 1;
+    private final static Integer DIVIDE_SCALE = 2;
+    private final static RoundingMode DIVIDE_ROUND_MODE = RoundingMode.DOWN;
+    private final Map<Rank, Integer> matchCountTable;
+
+    public WinningTable() {
+        matchCountTable = new LinkedHashMap<>();
+        setDefaultData();
+    }
+
+    private void setDefaultData() {
+        Arrays.stream(Rank.values())
+                .sorted(Comparator.reverseOrder())
+                .forEach(this::putDefaultData);
+    }
+
+    private void putDefaultData(Rank rank) {
+        if (matchCountTable.containsKey(rank)) {
+            return;
+        }
+        matchCountTable.put(rank, 0);
+    }
+
+    public Map<Rank, Integer> getMatchCountTable() {
+        return matchCountTable;
+    }
+
+    void setAutoIncrementMatchCountResult(Integer matchCount) {
+        matchCountTable.put(Rank.getRank(matchCount), matchCountTable.get(Rank.getRank(matchCount)) + ADD_AUTO_INCREMENT);
+    }
+
+    private Integer getTotalWinningMoney() {
+        return matchCountTable.entrySet()
+                .stream()
+                .map(entry -> entry.getKey().getWinningMoney() * entry.getValue())
+                .reduce(Integer::sum)
+                .get();
+    }
+
+    double calculationEarningsRate(Integer inputMoney) {
+        return BigDecimal.valueOf(getTotalWinningMoney())
+                .divide(BigDecimal.valueOf(inputMoney), DIVIDE_SCALE, DIVIDE_ROUND_MODE)
+                .doubleValue();
+
+    }
 }
--- src/test/java/lotto/domain/WinningTableTest.java
+++ src/test/java/lotto/domain/WinningTableTest.java
@@ -1,5 +1,39 @@
+package lotto.domain;
+
+import lotto.context.Rank;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WinningTableTest {
+    WinningTable winningTable;
 
+    @BeforeEach
+    void create() {
+        winningTable = new WinningTable();
+    }
+
+    @Test
+    void getMatchCountTable() {
+        Assertions.assertThat(winningTable.getMatchCountTable()).containsKeys(Rank.values());
+    }
+
+    @Test
+    void setAutoIncrementMatchCountResult() {
+        winningTable.setAutoIncrementMatchCountResult(3);
+        winningTable.setAutoIncrementMatchCountResult(3);
+        Assertions.assertThat(winningTable.getMatchCountTable().get(Rank.getRank(3))).isEqualTo(2);
+    }
+
+    @Test
+    void calculationEarningsRate() {
+        winningTable.setAutoIncrementMatchCountResult(3);
+        System.out.println(winningTable.calculationEarningsRate(14000));
+        Assertions.assertThat(winningTable.calculationEarningsRate(14000)).isEqualTo(0.35);
+    }
 }
