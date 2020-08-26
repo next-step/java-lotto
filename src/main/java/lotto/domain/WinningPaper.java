fcf385eb59a3bf40e6941096d1b4cd6c88403d59
hanogo <trullo393@gmail.com>
2020-08-27 01:16:44 +0900

    [feat] WinningPaper클래스 구현 및 테스트

--- src/main/java/lotto/domain/WinningPaper.java
+++ src/main/java/lotto/domain/WinningPaper.java
@@ -1,7 +1,22 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class WinningNumber {
-    private List<Integer> winningNumber;
+public class WinningPaper {
+    private final List<Integer> winningNumbers;
+
+    WinningPaper(String winningLottoNumbers) {
+        this.winningNumbers = Arrays.stream(winningLottoNumbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    Integer getMatchCount(List<Integer> lottoNumber) {
+        return (int) lottoNumber.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
 }
--- src/test/java/lotto/domain/WinningPaperTest.java
+++ src/test/java/lotto/domain/WinningPaperTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 class WinningPaperTest {
+    WinningPaper winningPaper;
 
+    @BeforeEach
+    void create() {
+        winningPaper = new WinningPaper("1, 2, 3, 4, 5, 6");
+    }
+
+    @Test
+    void getMatchCount() {
+        Assertions.assertThat(winningPaper.getMatchCount(Arrays.asList(1, 2, 3, 4, 5, 6))).isEqualTo(6);
+    }
 }
