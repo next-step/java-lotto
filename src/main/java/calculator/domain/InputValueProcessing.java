bb9f91933985e5a9ce6284ae783e308f942098d0
김효린 <hyorin97@naver.com>
2020-08-27 17:04:57 +0900

    fix: 상수화 처리

--- src/main/java/calculator/domain/InputValueProcessing.java
+++ src/main/java/calculator/domain/InputValueProcessing.java
@@ -4,28 +4,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputValueProcessing {
-    private static Pattern pattern= Pattern.compile("//(.)\n(.*)");
+    private static final String SPLIT_CRITERIA = ",|:";
+    private static final String PATTERN_CRITERIA = "//(.)\n(.*)";
+    private static final int GROUP_INDEX1 = 1;
+    private static final int GROUP_INDEX2 = 2;
+    private static Pattern pattern= Pattern.compile(PATTERN_CRITERIA);
 
     public static int calculateInputValue(String inputValue) {
-        if (inputValue == null)
-            return 0;
-        if (inputValue.isEmpty())
+        if (inputValue == null || inputValue.isEmpty())
             return 0;
         if (inputValue.startsWith("//")) {
             return new Numbers(checkCustomDelimiter(inputValue)).sum();
         }
 
-        return new Numbers(inputValue.split(",|:")).sum();
+        return new Numbers(inputValue.split(SPLIT_CRITERIA)).sum();
     }
 
     public static String[] checkCustomDelimiter(String inputValue) {
         Matcher m = pattern.matcher(inputValue);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(GROUP_INDEX1);
+            return m.group(GROUP_INDEX2).split(customDelimiter);
         }
         return new String[0];
     }
-
 
 }
