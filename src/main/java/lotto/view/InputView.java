c43d51d0fea73dca4d257a039fa750beac73c214
김효린 <hyorin97@naver.com>
2020-08-29 03:19:28 +0900

    refactor: InputView 클래스 접근제어자 변경 및 상수화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,19 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
+
+    public static final String SPLIT_CRITERIA = ", |,";
+    public static final String GUIDELINE_TEXT_TO_GET_PRICE = "구입금액을 입력해 주세요.";
+    public static final String GUIDELINE_TEXT_TO_GET_WINNING_NUM = "\n지난 주 당첨 번호를 입력해 주세요.";
+
+    private InputView() {
+        throw new AssertionError();
+    }
+
     public static int getLottoPrice() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(GUIDELINE_TEXT_TO_GET_PRICE);
         int paidMoney = Integer.parseInt(scanner.nextLine());
 
         return paidMoney;
@@ -15,8 +24,8 @@ public class InputView {
     public static String[] getWinningNumbers() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
-        String[] winningNumbers = scanner.nextLine().split(", |,");
+        System.out.println(GUIDELINE_TEXT_TO_GET_WINNING_NUM);
+        String[] winningNumbers = scanner.nextLine().split(SPLIT_CRITERIA);
 
         return winningNumbers;
     }
