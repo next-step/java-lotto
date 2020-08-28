90d49b43f3f7fe3ec4f9cad115d91817b6fefb27
김효린 <hyorin97@naver.com>
2020-08-29 03:32:53 +0900

    refactor: ResultView 클래스 접근제어자 변경 및 상수화

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -6,8 +6,16 @@ import java.util.List;
 import java.util.Map;
 
 public class ResultView {
+
+    public static final String LOTTO_COUNT_OUTPUT = "%d개를 구매했습니다.";
+    public static final String GUIDELINE_FOR_WINNING = "\n당첨 통계\n--------";
+
+    private ResultView() {
+        throw new AssertionError();
+    }
+
     public static void printLottoCount(int lottoPrice) {
-        System.out.println(lottoPrice + "개를 구매했습니다.");
+        System.out.println(String.format(LOTTO_COUNT_OUTPUT, lottoPrice));
     }
 
     public static void printLottoNumber(List<Lotto> lottoList) {
@@ -15,15 +23,15 @@ public class ResultView {
     }
 
     public static void printWinningCount(Map<Integer, Long> winningCount, int lottoPrice) {
-        System.out.println("\n당첨 통계\n--------");
+        System.out.println(GUIDELINE_FOR_WINNING);
         int[] winningMoney = {5000, 50000, 1500000, 2000000000};
 
         float sum = 0;
         for (int i = 3; i < 7; i++) {
-            System.out.println(i + "개 일치 (" + winningMoney[i - 3] + "원)- " + winningCount.get(i) + "개");
+            System.out.println(String.format("%d개 일치 (%d원)- %d개", i, winningMoney[i - 3], winningCount.get(i)));
             sum += winningMoney[i - 3] * winningCount.get(i);
         }
 
-        System.out.println("총 수익률은 " + (sum / lottoPrice) + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        System.out.println(String.format("총 수익률은 %f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", sum / lottoPrice));
     }
 }
