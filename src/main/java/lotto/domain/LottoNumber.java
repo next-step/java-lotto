4a40fd86ec3a91569b5801ee04502b94e1bb171d
hyeoni90 <hyeona743@gmail.com>
2020-08-29 17:38:51 +0900

    refactor: LottoNumber 생성자 private

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,13 +3,13 @@ package lotto.domain;
 import java.util.Objects;
 
 public class LottoNumber {
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
     private static final String INVALID_LOTTO_NUMBER = "유효한 로또 번호가 아닙니다.";
 
-    private int number;
+    private final int number;
 
-    public LottoNumber(int number) {
+    private LottoNumber(int number) {
         validate(number);
         this.number = number;
     }
