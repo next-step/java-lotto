f236f5792faf152b1a7770389584b4278cd7b44d
hanogo <trullo393@gmail.com>
2020-08-29 19:04:58 +0900

    [feat] 로또담청번호 확인 기능 추가

--- src/main/java/lotto/context/Error.java
+++ src/main/java/lotto/context/Error.java
@@ -2,7 +2,8 @@ package lotto.context;
 
 public enum Error {
     ERROR_INPUT_TYPE("숫자를 입력해주세요!"),
-    ERROR_INPUT_MIN_MONEY("구입금액은 최소 1000원 이상 입력해야 합니다.");
+    ERROR_INPUT_MIN_MONEY("구입금액은 최소 1000원 이상 입력해야 합니다."),
+    ERROR_WINNING_NUMBER("담청번호는 1부터 45까지 입력이 가능합니다.");
 
     private String msg;
 
--- src/main/java/lotto/domain/WinningPaper.java
+++ src/main/java/lotto/domain/WinningPaper.java
@@ -1,17 +1,35 @@
 package lotto.domain;
 
+import lotto.context.Error;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class WinningPaper {
+    private final Integer MIN_LOTTO_NUMBER = 1;
+    private final Integer MAX_LOTTO_NUMBER = 45;
     private final List<Integer> winningNumbers;
 
     WinningPaper(String winningLottoNumbers) {
-        this.winningNumbers = Arrays.stream(winningLottoNumbers.split(","))
+        List<Integer> numberList = makeNmbersAsList(winningLottoNumbers);
+        vaildList(numberList);
+        this.winningNumbers = numberList;
+    }
+
+    private List makeNmbersAsList(String winningLottoNumbers) {
+        return Arrays.stream(winningLottoNumbers.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private void vaildList(List<Integer> numberList) {
+        if (numberList.stream()
+                .filter(number -> number <  MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER)
+                .count() > 0) {
+            throw new IllegalArgumentException(Error.ERROR_WINNING_NUMBER.getMsg());
+        }
     }
 
     Integer getMatchCount(List<Integer> lottoNumber) {
