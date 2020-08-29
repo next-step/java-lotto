0612e7f7501dd724bf97980cb92730aedb718234
hanogo <trullo393@gmail.com>
2020-08-29 19:02:37 +0900

    [test] InputMoney테스트 추가

--- src/test/java/lotto/domain/InputMoneyTest.java
+++ src/test/java/lotto/domain/InputMoneyTest.java
@@ -1,5 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class InputMoneyTest {
+    @Test
+    void cretateTypeCheck() {
+        Assertions.assertThatExceptionOfType(NumberFormatException.class)
+                .isThrownBy(() -> {
+                    InputMoney inputMoney = new InputMoney("asd");
+                });
+    }
 
+    @Test
+    void cretateMinNumber() {
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    InputMoney inputMoney = new InputMoney("999");
+                });
+    }
 }
