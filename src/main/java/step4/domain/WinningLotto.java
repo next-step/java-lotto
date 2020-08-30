ff6d001d0f36ed096d6bdf5b77ec31842a52c46f
great0417 <great0417@hanmail.net>
2020-08-31 01:28:52 +0900

    refactor - validaion 체크 추가, 생성되기 이전, 체크 먼저 수행

--- src/main/java/step4/domain/WinningLotto.java
+++ src/main/java/step4/domain/WinningLotto.java
@@ -6,11 +6,21 @@ public class WinningLotto {
     private final LottoNumber bonusBall;
 
     public WinningLotto(Lotto winningLottoNumber, LottoNumber bonusBall) {
+        matchBonusValidationCheck(winningLottoNumber, bonusBall);
         this.winningLottoNumber = winningLottoNumber;
         this.bonusBall = bonusBall;
+
+    }
+
+    private void matchBonusValidationCheck(Lotto winningLottoNumber, LottoNumber bonusBall) {
         if (matchBonus(winningLottoNumber)) {
             throw new IllegalArgumentException("보너스번호는 당첨번호에서 제외되어야 합니다.");
         }
+    }
+
+    private boolean matchBonus(Lotto lotto, LottoNumber bonusBall) {
+        return lotto.getLottoNumber().stream()
+                .anyMatch(lottoNumber -> lottoNumber.equals(bonusBall));
     }
 
     public boolean matchBonus(Lotto lotto) {
