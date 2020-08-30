20addbd9d48e73d56e6b9dc657aa161ae1e9c212
hJJANG <dacapolife87@gmail.com>
2020-08-30 15:48:04 +0900

    refactor : Step3 피드백사항업데이트
    matchCount의 파라미터를 List에서 일글컬렉션으로 변경하여 전달값역시 변경하였습니다.

--- src/main/java/nextstep/lotto/dto/LottoTickets.java
+++ src/main/java/nextstep/lotto/dto/LottoTickets.java
@@ -3,8 +3,10 @@ package nextstep.lotto.dto;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoTickets {
 
@@ -18,17 +20,13 @@ public class LottoTickets {
         return new LottoTickets(tickets);
     }
 
-    public int count() {
-        return tickets.size();
-    }
-
     public List<LottoTicket> getAll() {
         return Collections.unmodifiableList(tickets);
     }
 
     public Map<LottoRank, Long> matchCount(final LottoWinnerNumbers winnerTicket) {
         return tickets.stream()
-                .map(lottoTicket -> winnerTicket.matchCount(lottoTicket.getLottoNumber()))
+                .map(lottoTicket -> winnerTicket.matchCount(lottoTicket))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
