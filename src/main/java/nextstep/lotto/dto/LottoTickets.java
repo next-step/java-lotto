25c47bd35f38152abec7cfd7e000f31d1a2c289f
hJJANG <dacapolife87@gmail.com>
2020-08-30 23:29:38 +0900

    refactor : 로또 구입 기능의 이동
    LottoGame에서 로또 티켓을 구입하는 기능을 LottoTickets내부로 이동하였습니다.
    수동의 경우도 같이 이동하려고 하였는데 수동의경우는 inputView객체에서 수동으로 번호를 입력받는 부분이 존재하여 수동부분을 뺀 나머지를 이동하였고 LottoTickets.create호출시 자동으로 구입해야할 갯수도 같이 전달하여 자동으로 구입하는 경우는 LottoTickets내부로 이동하였습니다.

--- src/main/java/nextstep/lotto/dto/LottoTickets.java
+++ src/main/java/nextstep/lotto/dto/LottoTickets.java
@@ -1,9 +1,10 @@
 package nextstep.lotto.dto;
 
+import nextstep.lotto.utils.LottoNumberUtil;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,21 +13,29 @@ public class LottoTickets {
 
     private List<LottoTicket> tickets;
 
-    private LottoTickets(List<LottoTicket> tickets) {
+    private LottoTickets(List<LottoTicket> tickets, int buyAutoAmount) {
         this.tickets = tickets;
+        this.tickets.addAll(buyAutoTicket(buyAutoAmount));
     }
 
-    public static LottoTickets create(List<LottoTicket> tickets) {
-        return new LottoTickets(tickets);
+    public static LottoTickets create(List<LottoTicket> tickets, int buyAutoAmount) {
+        return new LottoTickets(tickets, buyAutoAmount);
     }
 
     public List<LottoTicket> getAll() {
         return Collections.unmodifiableList(tickets);
     }
 
-    public Map<LottoRank, Long> matchCount(final LottoWinnerNumbers winnerTicket) {
+    public Map<LottoRank, Long> resultRank(final LottoWinnerNumbers winnerTicket) {
         return tickets.stream()
                 .map(lottoTicket -> winnerTicket.matchCount(lottoTicket))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private List<LottoTicket> buyAutoTicket(int buyAutoTicketCount) {
+        return Stream.generate((LottoNumberUtil::generator))
+                .limit(buyAutoTicketCount)
+                .map(LottoTicket::create)
+                .collect(Collectors.toList());
     }
 }
