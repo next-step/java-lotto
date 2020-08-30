b27c14e650f9c3c65565aebae219a1727b76f102
hJJANG <dacapolife87@gmail.com>
2020-08-30 15:55:29 +0900

    feat : 수동구입로직추가에따른 InputView기능추가
    수동구입갯수와 수동 구입로또번호의 입력 기능을 추가하였습니다.

--- src/main/java/nextstep/lotto/view/InputView.java
+++ src/main/java/nextstep/lotto/view/InputView.java
@@ -1,17 +1,22 @@
 package nextstep.lotto.view;
 
+import nextstep.lotto.dto.LottoTicket;
+import nextstep.lotto.utils.LottoNumberUtil;
 import nextstep.lotto.utils.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 import static nextstep.lotto.constant.ExceptionMessage.INVALID_INPUT_MINIMUM_AMOUNT_FOR_BUY;
+import static nextstep.lotto.constant.ExceptionMessage.INVALID_INPUT_MINIMUM_MANUAL_AMOUNT_FOR_BUY;
 import static nextstep.lotto.constant.PrintMessage.*;
 
 public class InputView {
 
     private final int MINIMUM_BUY_AMOUNT = 1000;
+    private final int MINIMUM_BUY_MANUAL_AMOUNT = 0;
 
     public int inputTotalAmountForBuy() {
         System.out.println(QUESTION_TEXT_BUY_TOTAL_AMOUNT);
@@ -19,10 +24,37 @@ public class InputView {
         int totalAmount = scanner.nextInt();
 
         if (totalAmount < MINIMUM_BUY_AMOUNT) {
-            throw new RuntimeException(INVALID_INPUT_MINIMUM_AMOUNT_FOR_BUY);
+            throw new IllegalArgumentException(INVALID_INPUT_MINIMUM_AMOUNT_FOR_BUY);
         }
 
         return totalAmount;
+    }
+
+    public int inputManualLottoAmoutForBuy() {
+        System.out.println(QUESTION_TEXT_BUY_MANUAL_AMOUNT);
+        Scanner scanner = new Scanner(System.in);
+        int manualAmount = scanner.nextInt();
+
+        if (manualAmount < MINIMUM_BUY_MANUAL_AMOUNT) {
+            throw new IllegalArgumentException(INVALID_INPUT_MINIMUM_MANUAL_AMOUNT_FOR_BUY);
+        }
+
+        return manualAmount;
+    }
+
+    public List<LottoTicket> inputManualNumber(int manualAmount) {
+        System.out.println(QUESTION_TEXT_LOTTO_MANUAL_NUMBER);
+        Scanner scanner = new Scanner(System.in);
+
+        List<LottoTicket> manualLottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < manualAmount; i++) {
+            String manualNumber = scanner.nextLine();
+            List<String> manualNumbers = Arrays.asList(StringUtils.splitString(manualNumber));
+            manualLottoTickets.add(LottoTicket.create(LottoNumberUtil.transStringNumberToLottoNumber(manualNumbers)));
+        }
+
+        return manualLottoTickets;
     }
 
     public List<String> inputWinnerNumber() {
