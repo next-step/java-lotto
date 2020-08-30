35a2840fcd1583681e4ca239b7861734d799f851
김효린 <hyorin97@naver.com>
2020-08-30 21:49:21 +0900

    refactor: lottoAllNum 변수 static으로 변경

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -6,13 +6,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RandomLottoGenerator implements LottoGeneratorStrategy {
-    public static final int LOTTO_TOTAL_COUNT = 45;
-    public static final int LOTTO_MIN_SIZE = 0;
-    public static final int LOTTO_MAX_SIZE = 6;
+    private static final int LOTTO_TOTAL_COUNT = 45;
+    private static final int LOTTO_MIN_SIZE = 0;
+    private static final int LOTTO_MAX_SIZE = 6;
+    static List<Integer> lottoAllNum = Stream.iterate(1, n -> n + 1).limit(LOTTO_TOTAL_COUNT).collect(Collectors.toList());
+
 
     @Override
     public List<Integer> generateLotto() {
-        List<Integer> lottoAllNum = Stream.iterate(1, n -> n + 1).limit(LOTTO_TOTAL_COUNT).collect(Collectors.toList());
         Collections.shuffle(lottoAllNum);
         List<Integer> lottoNumbers = lottoAllNum.subList(LOTTO_MIN_SIZE, LOTTO_MAX_SIZE);
         Collections.sort(lottoNumbers);
