b97329fcf9d358144cf2c2186e0c12d57b204eb7
김효린 <hyorin97@naver.com>
2020-09-01 00:40:11 +0900

    fix: lottoAllNum static 적용 및 subList 함수 미사용

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -3,19 +3,22 @@ package lotto.domain;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class RandomLottoGenerator implements LottoGeneratorStrategy {
     private static final int LOTTO_TOTAL_COUNT = 45;
     private static final int LOTTO_MIN_SIZE = 0;
     private static final int LOTTO_MAX_SIZE = 6;
+    static List<Integer> lottoAllNum = Stream.iterate(1, n -> n + 1).limit(LOTTO_TOTAL_COUNT).collect(Collectors.toList());
 
     @Override
     public List<Integer> generateLotto() {
-        List<Integer> lottoAllNum = Stream.iterate(1, n -> n + 1).limit(LOTTO_TOTAL_COUNT).collect(Collectors.toList());
-
         Collections.shuffle(lottoAllNum);
-        List<Integer> lottoNumbers = lottoAllNum.subList(LOTTO_MIN_SIZE, LOTTO_MAX_SIZE);
+        List<Integer> lottoNumbers = Stream.iterate(LOTTO_MIN_SIZE, n -> n+1)
+                .limit(LOTTO_MAX_SIZE)
+                .map(n -> lottoAllNum.get(n))
+                .collect(Collectors.toList());
         Collections.sort(lottoNumbers);
 
         return lottoNumbers;
