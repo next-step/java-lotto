99e325ac480b5a168613456a1f73d2fb9442d9eb
hanogo <trullo393@gmail.com>
2020-09-02 01:12:23 +0900

    [refactor] RANK 금액 구분자 추가

--- src/main/java/lotto/context/Rank.java
+++ src/main/java/lotto/context/Rank.java
@@ -3,10 +3,10 @@ package lotto.context;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000);
 
     private int countOfMatch;
     private int winningMoney;
