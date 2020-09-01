af1655a0de1b2cb7a4d26464678a4931b4e292b9
hanogo <trullo393@gmail.com>
2020-09-02 01:09:10 +0900

    [refactor] 메세지 수정

--- src/main/java/lotto/context/Message.java
+++ src/main/java/lotto/context/Message.java
@@ -3,7 +3,7 @@ package lotto.context;
 public enum Message {
     INPUT_MSG("구입금액을 입력해 주세요."),
     BUY_COUNT("%1개를 구매했습니다."),
-    INPUT_WINNING_NUMBER("지난 주 담청 번호를 입력해 주세요."),
+    INPUT_WINNING_NUMBER("지난 주 당첨 번호를 입력해 주세요."),
     RESULT_HEAD("당첨 통계"),
     RESULT_DELIMITER("---------"),
     RESULT_BODY("%1개 일치 (%2원)- %3개"),
