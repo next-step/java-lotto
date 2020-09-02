f0199b77f27c476e0fdb251b00c1d122832886ea
hyeoni90 <hyeona743@gmail.com>
2020-09-02 21:38:57 +0900

    refactor: LottoGame 구입 가격 대신 구매 개수로 수정

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -5,30 +5,29 @@ public class LottoGame {
     private static final Integer INTEGER_ONE = 1;
     private static final String INVALID_LOTTO_PURCHASE_AMOUNT = "로또 구입 금액이 부족합니다.";
 
-    private final int amount;
+    private final int count;
 
-    private LottoGame(int amount) {
-        this.amount = amount;
+    private LottoGame(int count) {
+        this.count = count;
     }
 
     public static LottoGame of(int amount) {
         int count = amount / DEFAULT_LOTTO_PRICE;
         validate(count);
-
-        return new LottoGame(amount);
+        return new LottoGame(count);
     }
 
     public static void validate(int count) {
-        if(count < INTEGER_ONE) {
+        if (count < INTEGER_ONE) {
             throw new IllegalArgumentException(INVALID_LOTTO_PURCHASE_AMOUNT);
         }
     }
 
     public int getCount() {
-        return amount / DEFAULT_LOTTO_PRICE;
+        return count;
     }
 
     public Lottos makeLottos(LottoNumberRandomGenerator lottoNumberRandomGenerator) {
-        return Lottos.of(getCount(), lottoNumberRandomGenerator);
+        return Lottos.of(count, lottoNumberRandomGenerator);
     }
 }
