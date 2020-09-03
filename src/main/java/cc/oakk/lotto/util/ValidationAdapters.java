7d7ae9de8cef2e79092d045a48e95642f66dd664
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-09-04 01:59:32 +0900

    feat(util): add throwIfNegative method to ValidationAdapters.

--- src/main/java/cc/oakk/lotto/util/ValidationAdapters.java
+++ src/main/java/cc/oakk/lotto/util/ValidationAdapters.java
@@ -5,6 +5,17 @@ import java.util.Objects;
 public class ValidationAdapters {
     private ValidationAdapters() {}
 
+    public static int throwIfNegative(int number, String message) {
+        if (number < 0) {
+            throw new IllegalArgumentException(message);
+        }
+        return number;
+    }
+
+    public static int throwIfNegative(int number) {
+        return throwIfNegative(number, "Given number is negative.");
+    }
+
     public static<T> T throwIfNull(T obj) {
         if (Objects.isNull(obj)) {
             throw new IllegalArgumentException("Given object is null.");
--- src/test/java/cc/oakk/lotto/util/ValidationAdaptersTest.java
+++ src/test/java/cc/oakk/lotto/util/ValidationAdaptersTest.java
@@ -2,11 +2,25 @@ package cc.oakk.lotto.util;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ValidationAdaptersTest {
     @Test
     public void throwIfNull() {
         assertThatIllegalArgumentException().isThrownBy(() -> ValidationAdapters.throwIfNull(null));
+    }
+
+    @Test
+    public void throwIfNegative() {
+        assertThatIllegalArgumentException().isThrownBy(() -> ValidationAdapters.throwIfNegative(-1));
+        assertThatIllegalArgumentException().isThrownBy(() -> ValidationAdapters.throwIfNegative(Integer.MIN_VALUE));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ValidationAdapters.throwIfNegative(-1, "ERROR!"))
+                .withMessage("ERROR!");
+
+        assertThat(ValidationAdapters.throwIfNegative(0)).isEqualTo(0);
+        assertThat(ValidationAdapters.throwIfNegative(Integer.MAX_VALUE)).isEqualTo(Integer.MAX_VALUE);
     }
 }
