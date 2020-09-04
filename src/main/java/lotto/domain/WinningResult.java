387f48f33f5b18d35f760b91c75e5662ded889d4
hanogo <trullo393@gmail.com>
2020-09-04 22:56:48 +0900

    [refactor] WinningResult 객체 당첨결과를 반환하는 메소드 추가

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -2,22 +2,38 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class WinningResult {
     private final LottoPaper winningPaper;
+    private LottoNum bonusNum;
 
     public WinningResult(String winningLottoNumbers) {
         winningPaper = new LottoPaper(Arrays.stream(winningLottoNumbers.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
-                .map(integer -> new LottoNum(integer))
+                .map(LottoNum::of)
                 .collect(Collectors.toList()));
     }
 
-    public List<Integer> getWinninLottoNumberToIntegerList() {
+    public List<Integer> getWinningLottoNumberToIntegerList() {
         return winningPaper.getLottoNumbers().stream()
                 .map(LottoNum::getLottoNum)
                 .collect(Collectors.toList());
+    }
+
+    public void setBonusNum(LottoNum lottoNum) {
+        this.bonusNum = lottoNum;
+    }
+
+    public WinningTable makeWinningTable(List<LottoPaper> lottoPapers) {
+        WinningTable winningTable = new WinningTable();
+
+        lottoPapers.forEach(lottoPaper -> {
+            winningTable.setAutoIncrementMatchCountResult(lottoPaper.getMatchCount(getWinningLottoNumberToIntegerList()),
+                    lottoPaper.isContain(Optional.ofNullable(bonusNum.getLottoNum()).get()));
+        });
+        return winningTable;
     }
 }
--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -1,18 +1,11 @@
 package lotto.domain;
 
-import lotto.context.Rank;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class WinningResultTest {
     WinningResult winningResult;
@@ -23,7 +16,15 @@ class WinningResultTest {
 
     @Test
     void getWinninLottoNumberToIntegerList() {
-        Assertions.assertThat(winningResult.getWinninLottoNumberToIntegerList())
+        Assertions.assertThat(winningResult.getWinningLottoNumberToIntegerList())
                 .isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    @DisplayName("WinningTable 생성확인")
+    void makeWinningTable() {
+        winningResult.setBonusNum(LottoNum.of(1));
+        Assertions.assertThat(winningResult.makeWinningTable(new LottoPapers(3).getPapers()).getMatchCountTable())
+                .isEqualTo(new WinningTable().getMatchCountTable());
     }
 }
