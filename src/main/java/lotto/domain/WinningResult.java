f58852ba192af2d4d1c52f099d7ddd4e167e8dfe
hanogo <trullo393@gmail.com>
2020-09-04 23:59:10 +0900

    [fix] WinningResult 객체 null체크 기능 추가

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.context.Error;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -10,11 +12,18 @@ public class WinningResult {
     private LottoNum bonusNum;
 
     public WinningResult(String winningLottoNumbers) {
+        validWinningResult(winningLottoNumbers);
         winningPaper = new LottoPaper(Arrays.stream(winningLottoNumbers.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .map(LottoNum::of)
                 .collect(Collectors.toList()));
+    }
+
+    private void validWinningResult(String winningLottoNumbers) {
+        if (winningLottoNumbers == null || "".equals(winningLottoNumbers)) {
+            throw new IllegalArgumentException(Error.ERROR_INPUT_NULL.getMsg());
+        }
     }
 
     public List<Integer> getWinningLottoNumberToIntegerList() {
