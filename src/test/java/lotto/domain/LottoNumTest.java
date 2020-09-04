4a35a469a50affcc9c465a2358f427d8a2f325bd
hanogo <trullo393@gmail.com>
2020-09-04 22:57:55 +0900

    [test] LottoNum 객체 테스트

--- src/test/java/lotto/domain/LottoNumTest.java
+++ src/test/java/lotto/domain/LottoNumTest.java
@@ -1,17 +1,35 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoNumTest {
+    LottoNum lottoNum;
+
+    @BeforeEach
+    void create() {
+        lottoNum = new LottoNum(1);
+    }
+
     @Test
     void createLottoNum() {
-        Assertions.assertThat(new LottoNum(1)).isEqualToComparingFieldByField(new LottoNum(1));
+        Assertions.assertThat(lottoNum).isEqualToComparingFieldByField(new LottoNum(1));
     }
 
     @Test
     void getLottoNumVaild() {
         Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new LottoNum(46));
+    }
+
+    @Test
+    void Integerof() {
+        Assertions.assertThat(lottoNum).isEqualToComparingFieldByField(LottoNum.of(1));
+    }
+
+    @Test
+    void stringOf() {
+        Assertions.assertThat(lottoNum).isEqualToComparingFieldByField(LottoNum.of(1));
     }
 }
