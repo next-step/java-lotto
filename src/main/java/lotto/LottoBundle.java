5b18f0d73e3c5c5c45702898ac8d1e1da1b7989b
Jay <beginin15@gmail.com>
2020-09-05 22:52:48 +0900

    feat: 등수별 당첨된 로또 개수를 반환하는 메소드 구현

--- src/main/java/lotto/LottoBundle.java
+++ src/main/java/lotto/LottoBundle.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class LottoBundle {
 
@@ -12,5 +16,16 @@ public class LottoBundle {
 
     public int count() {
         return tickets.size();
+    }
+
+    public Map<Prize, Integer> drawing(LottoNumbers winningNumbers) {
+        Map<Prize, Integer> result = new HashMap<>();
+        Map<Prize, List<LottoNumbers>> ticketsPerPrize = tickets.stream()
+                .collect(groupingBy(lottoNumbers -> lottoNumbers.matchNumbers(winningNumbers)));
+
+        for (Prize prize : ticketsPerPrize.keySet()) {
+            result.put(prize, ticketsPerPrize.get(prize).size());
+        }
+        return result;
     }
 }
--- src/test/java/lotto/LottoBundleTest.java
+++ src/test/java/lotto/LottoBundleTest.java
@@ -1,8 +1,13 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +18,32 @@ public class LottoBundleTest {
     @DisplayName("금액으로 구입 가능한 개수만큼 로또 번 생성")
     void buyTickets(int payment, int count) {
         assertThat(LottoFactory.buyTickets(payment).count()).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("등수별 당첨된 로또 장수")
+    void drawing() {
+        // given
+        LottoNumbers winningNumbers = LottoNumbers.of(1, 2, 3, 4, 5, 6);
+        LottoBundle bundle = new LottoBundle(Arrays.asList(
+                LottoNumbers.of(1, 7, 8, 9, 10, 11),
+                LottoNumbers.of(1, 2, 8, 9, 10, 11),
+                LottoNumbers.of(1, 2, 3, 9, 10, 11),
+                LottoNumbers.of(1, 2, 3, 9, 12, 13),
+                LottoNumbers.of(1, 2, 3, 4, 5, 11),
+                LottoNumbers.of(1, 2, 3, 4, 5, 6)
+        ));
+
+        // when
+        Map<Prize, Integer> actual = bundle.drawing(winningNumbers);
+
+        // then
+        Map<Prize, Integer> expected = new HashMap<>();
+        expected.put(Prize.FIRST, 1);
+        expected.put(Prize.SECOND, 1);
+        expected.put(Prize.FOURTH, 2);
+        expected.put(Prize.ETC, 2);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
