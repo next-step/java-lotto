af0b37aefc3e9481b5454b416c72e9f2d52a8e69
Jay <beginin15@gmail.com>
2020-09-06 00:32:19 +0900

    feat: Prize에 당첨액 속성 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Prize {
-    FIRST(6),
-    SECOND(5),
-    THIRD(4),
-    FOURTH(3),
-    ETC(0);
 
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    ETC(0, 0);
+
+    private static final int NOT_WON = 0;
     private static final Map<Long, Prize> BY_MATCH_COUNT = new HashMap<>();
 
     static {
@@ -18,13 +20,19 @@ public enum Prize {
         }
     }
 
-    private long matchCount;
+    private final long matchCount;
+    private final int prize;
 
-    private Prize(long matchCount) {
+    private Prize(long matchCount, int prize) {
         this.matchCount = matchCount;
+        this.prize = prize;
     }
 
     public static Prize valueOfMatchCount(long matchCount) {
-        return BY_MATCH_COUNT.get(matchCount >= Prize.FOURTH.matchCount ? matchCount : 0);
+        return BY_MATCH_COUNT.get(matchCount >= Prize.FOURTH.matchCount ? matchCount : NOT_WON);
+    }
+
+    public int getPrize() {
+        return prize;
     }
 }
