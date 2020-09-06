c152d7a7b66650776c8ceed0522c42fb13fcb7c1
EddyJo <jkjk0600@gamil.com>
2020-09-06 11:04:35 +0900

    feat Lotto Numbers 일급컬랙션

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -1,4 +1,23 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toCollection;
+
 public class LottoNumbers {
+
+    ArrayList<LottoNumber> lottoNumbers;
+
+    public LottoNumbers() { }
+
+    public LottoNumbers(ArrayList<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public static LottoNumbers create(ArrayList<Integer> lottoNums) {
+
+        return new LottoNumbers(lottoNums.stream().map( val -> LottoNumber.of(val))
+                                                  .collect(toCollection(ArrayList::new)));
+    }
 }
--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -1,4 +1,33 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toCollection;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LottoNumbersTest {
+
+    @DisplayName("로또번호 생성 테스트")
+    @Test
+    public void 로또번호_생성_테스트() {
+        ArrayList<Integer> lottoNums = Stream.iterate(1, n -> n + 1 )
+                .limit(6)
+                .collect(toCollection(ArrayList::new));
+
+        assertThat(LottoNumbers.create(lottoNums)).isInstanceOf(LottoNumbers.class);
+    }
+    @DisplayName("로또번호 생성 테스트 isEqaulTo")
+    @Test
+    public void 로또번호_생성_테스트_isEqaulTo() {
+        ArrayList<Integer> lottoNums = Stream.iterate(1, n -> n + 1 )
+                .limit(6)
+                .collect(toCollection(ArrayList::new));
+
+        LottoNumbers expected = new LottoNumbers();
+        assertThat(LottoNumbers.create(lottoNums)).isEqualTo(expected);
+    }
 }
