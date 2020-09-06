ef9787cab04c2dc7fd4a2b5a656a095bb7acf830
hyeoni90 <hyeona743@gmail.com>
2020-09-07 00:07:44 +0900

    feat: Rank 2등 조건 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -6,19 +6,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    MISS(0, 0, false);
 
     private final int countOfMatch;
     private final int winningPrize;
+    private final boolean isMatchBonusBall;
 
-    Rank(int countOfMath, int winningPrize) {
+    Rank(int countOfMath, int winningPrize, boolean isMatchBonusBall) {
         this.countOfMatch = countOfMath;
         this.winningPrize = winningPrize;
+        this.isMatchBonusBall = isMatchBonusBall;
     }
 
     public int getWinningPrize() {
@@ -27,6 +29,10 @@ public enum Rank {
 
     public int getCountOfMatch() {
         return countOfMatch;
+    }
+
+    public boolean isMatchBonusBall() {
+        return isMatchBonusBall;
     }
 
     public static boolean equalsCountOfMatch(Rank rank, int count) {
