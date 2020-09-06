da3105964a73879732e5a9ea9f8a17abb6120ce2
hanogo <trullo393@gmail.com>
2020-09-06 18:10:08 +0900

    [fix] main 수정

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -3,23 +3,34 @@ package lotto.domain;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
 
     public static void start() {
         ResultView.printInputMsg();
-
         InputMoney inputMoney = new InputMoney(InputView.inputText());
-        LottoPapers lottoPapers = new LottoPapers(inputMoney.getPaperCount());
+        ResultView.printManualCount();
+        int manualCount = InputView.inputInt();
 
-        ResultView.showBuyCount(lottoPapers.getPapersSize());
-        ResultView.showLottoNumbersList(lottoPapers.getPapers());
+        ResultView.printManualNumber();
+        List<String> autoText = new ArrayList<>();
+        for (int i = 0; i < manualCount; i++) {
+            autoText.add(InputView.inputText());
+        }
+        LottoStore lottoStore = new LottoStore(autoText, inputMoney.getPaperCount());
+        LottoPapers lottoPapers = lottoStore.makeLottoPapers();
+
+        ResultView.showBuyCount(lottoStore.getManuaCount(), lottoStore.getAutoCount());
+        ResultView.showLottoNumbersList(lottoPapers.getLottoPapers());
         ResultView.showInputWinningNumber();
         String winningLottoNumbers = InputView.inputText();
         ResultView.showInputBonusNumber();
         String bonusNumber = InputView.inputText();
 
         WinningResult winningResult = new WinningResult(winningLottoNumbers, bonusNumber);
-        WinningTable winningTable = winningResult.makeWinningTable(lottoPapers.getPapers());
+        WinningTable winningTable = winningResult.makeWinningTable(lottoPapers.getLottoPapers());
 
         ResultView.showResult(winningTable.getMatchCountTable());
         ResultView.showDelimiter();
