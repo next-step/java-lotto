6598098443dbc72968ed97e49d214ee92f76168a
Jay <beginin15@gmail.com>
2020-09-06 16:34:02 +0900

    test: LottoBundle 생성자 테스트 추가

--- src/test/java/lotto/LottoBundleTest.java
+++ src/test/java/lotto/LottoBundleTest.java
@@ -4,14 +4,46 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LottoBundleTest {
+
+    @Test
+    @DisplayName("생성자 테스트")
+    void create() {
+        assertThat(new LottoBundle(Arrays.asList(
+                LottoNumbers.of(1, 2, 3, 4, 5, 6),
+                LottoNumbers.of(7, 8, 9, 10, 12, 13),
+                LottoNumbers.of(40, 41, 42, 43, 44, 45)
+        ))).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIllegalLottoNumbers")
+    @DisplayName("생성자 예외 발생 테스트")
+    void create_IllegalLottoNumbers(List<Integer> numbers) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            LottoBundle bundle = new LottoBundle(Arrays.asList(new LottoNumbers(numbers)));
+        });
+    }
+
+    private static Stream<List<Integer>> provideIllegalLottoNumbers() {
+        return Stream.of(
+                Arrays.asList(1, 2, 3, 4, 5),
+                Arrays.asList(-7, 8, 9, 10, 12, 13),
+                Arrays.asList(41, 42, 43, 44, 45, 46)
+        );
+    }
 
     @ParameterizedTest
     @CsvSource({"14000, 14", "1000, 1", "0, 0", "-1000, 0"})
