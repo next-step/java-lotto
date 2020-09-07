77c44ca16a6a7b79a6e7a70c5cc0a907ebd65e4a
Jay <beginin15@gmail.com>
2020-09-07 21:24:36 +0900

    feat: 로또 숫자(원시값)를 포장한 LottoNumber 클래스 정의 (피드백 반영)
    - 로또 숫자의 범위 (1~45)에 대한 유효성 검증을 수행

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    private Integer number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber of(int number) {
+        validateRange(number);
+        return new LottoNumber(number);
+    }
+
+    private static void validateRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("1 ~ 45 범위를 벗어나는 숫자를 포함하고 있습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number.equals(that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return number.toString();
+    }
+}
