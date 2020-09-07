00a85c7d0bf3a00b0b7139798b99bc2223fc1946
hyeoni90 <hyeona743@gmail.com>
2020-09-07 22:57:55 +0900

    feat: 당첨 통계에 2등도 출력

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -10,10 +10,10 @@ import static java.math.RoundingMode.FLOOR;
 
 public class ResultView {
     private static final String ANSWER_COUNT_LOTTO_MESSAGE = "개 를 구입 했습니다.";
-    private static final String RESULT_OF_LOTTO_GAME_MESSAGE = "당첨 통계\n---------";
+    private static final String RESULT_OF_LOTTO_GAME_MESSAGE = "\n당첨 통계\n---------";
     private static final String RANK_COUNT_OF_MATCH_MESSAGE = "%s개 일치";
     private static final String BONUS_BALL_COUNT_MESSAGE = ", 보너스 볼 일치";
-    private static final String WINNING_LOTTO_COUNT_OF_MATCH_MESSAGE = "(%s원)- %s개";
+    private static final String WINNING_LOTTO_COUNT_OF_MATCH_MESSAGE = "(%s원)- %s개\n";
     private static final String RESULT_LOTTO_RATE_MESSAGE = "총 수익률은 %s입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n";
     private static final String EMPTY_STRING = " ";
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###.##");
@@ -43,9 +43,10 @@ public class ResultView {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (Rank rank : Rank.ranks()) {
-            stringBuilder.append(String.format(RANK_COUNT_OF_MATCH_MESSAGE, rank.getCountOfMatch(),
-                    rank.isMatchBonusBall() ? BONUS_BALL_COUNT_MESSAGE : EMPTY_STRING,
-                    WINNING_LOTTO_COUNT_OF_MATCH_MESSAGE, rank.getWinningPrize(), lottoResult.countOfMathRanks(rank)));
+            stringBuilder.append(String.format(RANK_COUNT_OF_MATCH_MESSAGE, rank.getCountOfMatch()));
+            stringBuilder.append(rank.isMatchBonusBall() ? BONUS_BALL_COUNT_MESSAGE : EMPTY_STRING);
+            stringBuilder.append(String.format(WINNING_LOTTO_COUNT_OF_MATCH_MESSAGE,
+                    rank.getWinningPrize(), lottoResult.countOfMathRanks(rank)));
         }
         return stringBuilder.toString();
     }
