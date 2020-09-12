032fdd4bdc48ef0f170b9fe4271e24de95ed126b
hyeoni90 <hyeona743@gmail.com>
2020-09-13 01:11:57 +0900

    refactor: LottoController, LottoResult, ResultView 수동 로또 적용 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,15 +4,18 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.List;
+
 public class LottoController {
 
     public static void main(String[] args) {
         int purchaseAmount = InputView.inputPurchaseAmount();
+        List<String> manualLottos = InputView.inputManualLotto();
 
-        LottoGame lottoGame = LottoGame.of(purchaseAmount);
-        ResultView.printPurchaseLottoCount(lottoGame.getCount());
+        LottoGame lottoGame = LottoGame.of(purchaseAmount, manualLottos.size());
+        ResultView.printPurchaseLottoCount(lottoGame.getCount(), manualLottos.size());
 
-        Lottos lottos = lottoGame.makeLottos(new LottoNumberRandomGenerator());
+        Lottos lottos = lottoGame.makeLottos(manualLottos);
         ResultView.printLottos(lottos.toStringByLotto());
 
         WinningLotto winningLotto = WinningLotto.of(InputView.inputWinningNumberOfLastWeek(), InputView.inputBonusNumber());
--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import static lotto.domain.LottoGame.DEFAULT_LOTTO_PRICE;
 
 public class LottoResult {
-    private static final Integer INTEGER_ZERO = 0;
+    public static final int INTEGER_ZERO = 0;
     private final List<Rank> matchRanks;
 
     public LottoResult(List<Rank> ranks) {
@@ -23,10 +23,10 @@ public class LottoResult {
                 .count();
     }
 
-    public int totalPrizeOfMathRanks(){
+    public int totalPrizeOfMathRanks() {
         return matchRanks.stream()
                 .mapToInt(Rank::getWinningPrize)
-                .reduce(INTEGER_ZERO,Integer::sum);
+                .reduce(INTEGER_ZERO, Integer::sum);
     }
 
     public double rateOfTotalPrize() {
