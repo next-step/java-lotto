7a8746d9653a6257800e2bf4766709ca122402f7
mocha121212 <mocha121212@gmail.com>
2020-09-12 21:10:40 +0900

    test : 쉼표구분자

--- src/main/java/step1/util/StringAddCalculator.java
+++ src/main/java/step1/util/StringAddCalculator.java
@@ -1,14 +1,38 @@
 package step1.util;
 
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
 public class StringAddCalculator {
+//    public static final String REGULAR_EXPRESSION = "[-_.~!@#$%^&*()=+{}:?,<>]";
+    public static final String REGULAR_EXPRESSION = "[-_.~!@#$%^&*()=+{}:?<>]";
     public static int splitAndSum(String input) {
+        //1
         if(StringUtils.isNotBlank(input)) {
+            System.out.println("inNotBlank");
             return 0;
         }
+        //2
         if(StringUtils.isNumeric(input)) {
+            System.out.println("isNumeric");
             return Integer.parseInt(input.toString());
         }
-        return Integer.parseInt(input.toString());
+        //3
+        Pattern pattern = Pattern.compile(REGULAR_EXPRESSION);
+        boolean result = pattern.matcher(input).find();
+        if(!result) {
+            return IntStream.of(splitNo(input)).sum();
+        }
 
+        return Integer.parseInt(input.toString());
+    }
+
+    public static int[] splitNo(String input) {
+        String[] tempNo = input.split(",");
+        int[] numbers = new int[tempNo.length];
+        for(int i = 0 ; i < tempNo.length ; i++) {
+            numbers[i] = StringUtils.returnNumber(tempNo[i]);
+        }
+        return numbers;
     }
 }
--- src/main/java/step1/util/StringUtils.java
+++ src/main/java/step1/util/StringUtils.java
@@ -16,4 +16,31 @@ public class StringUtils {
             return false;
         }
     }
+
+    public static int returnNumber(String input) {
+        if(isNotBlank(input)) {
+            return 0;
+        }
+        return Integer.parseInt(input);
+    }
+
+    public static int[] makeNumberList(String input) {
+        String[] string = input.split(",");
+        int[] numbers = new int[string.length];
+        for(int i = 0 ; i < numbers.length ; i++) {
+            numbers[i] = StringUtils.returnNumber(input);
+        }
+        return numbers;
+    }
+//
+//    public static int conversionString(String input) {
+//        return Integer.parseInt(input);
+//    }
+//
+//    public static int beforeConversion(String input) {
+//        if(StringUtils.isNumeric(input)) {
+//            conversionString(input);
+//        }
+//        return 0;
+//    }
 }
