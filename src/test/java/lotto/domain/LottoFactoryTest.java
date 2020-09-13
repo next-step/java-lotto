31d3697d551c13b4a61a534d876d9f6d64b2ea4c
Jay <beginin15@gmail.com>
2020-09-14 01:07:55 +0900

    test: buyTickets 메소드의 테스트 코드 보완

--- src/test/java/lotto/domain/LottoFactoryTest.java
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -5,21 +5,31 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LottoFactoryTest {
 
     @Test
-    @DisplayName("정상적으로 로또 묶음과 번호가 생성되는지 확인")
+    @DisplayName("개수만큼 자동으로 로또가 생성되는지 확인")
     void buyTickets() {
-        assertNotNull(LottoFactory.buyTickets(3000));
+        List<LottoNumbers> numbers = LottoFactory.buyTickets(3);
+        assertEquals(numbers.size(), 3);
     }
 
     @ParameterizedTest
     @CsvSource({"14000, 14", "1000, 1", "0, 0", "-1000, 0"})
-    @DisplayName("금액으로 구입 가능한 개수만큼 로또 번 생성")
-    void buyTickets(int payment, int count) {
-        assertEquals(LottoFactory.buyTickets(payment).size(), count);
+    @DisplayName("개수만큼 수동으로 로또가 생성되는지 확인")
+    void buySlips() {
+        List<LottoNumbers> numbers = LottoFactory.buyTickets(3, () -> Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        assertThat(numbers).containsExactly(
+                LottoNumbers.of(1, 2, 3, 4, 5, 6),
+                LottoNumbers.of(1, 2, 3, 4, 5, 6),
+                LottoNumbers.of(1, 2, 3, 4, 5, 6)
+        );
     }
 }
