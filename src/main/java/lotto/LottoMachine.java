03b3690ff7a69c98d358bf4c85f633c177ad8508
Jay <beginin15@gmail.com>
2020-09-14 02:14:37 +0900

    refactor: 예외 발생 시, 다시 입력받도록 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -9,12 +9,57 @@ import java.util.List;
 public class LottoMachine {
 
     public static void main(String[] args) {
-        List<LottoNumbers> tickets = LottoFactory.buyTickets(InputView.howMuch());
-        ResultView.showTickets(tickets);
+        Payment payment = howMuch();
+        LottoBundle bundle = new LottoBundle();
+
+        int slipCount = buyTickets(payment);
+
+        pickingNumbers(slipCount, bundle);
+        bundle.addTickets(LottoFactory.generateLotto(payment.remaining()));
+
+        ResultView.showTickets(bundle);
 
         WinningLotto winningNumbers = WinningLotto.of(InputView.getWinningNumbers(), InputView.getBonusNumber());
 
-        LottoResult result = winningNumbers.drawing(tickets);
+        LottoResult result = bundle.drawing(winningNumbers);
         ResultView.showDrawingResult(result);
+    }
+
+    public static Payment howMuch() {
+        Payment payment = null;
+        try {
+            payment = new Payment(InputView.howMuch());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력해주세요.");
+            payment = howMuch();
+        } catch (IllegalArgumentException  e) {
+            System.out.println(e.getMessage());
+            payment = howMuch();
+        }
+        return payment;
+    }
+
+    public static int buyTickets(Payment payment) {
+        int slipCount = 0;
+        try {
+            slipCount = InputView.howManySlips();
+            payment.buyTickets(slipCount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            slipCount = buyTickets(payment);
+        }
+        return slipCount;
+    }
+
+    public static void pickingNumbers(int slipCount, LottoBundle bundle) {
+        while (slipCount-- > 0) {
+            List<Integer> pickedNumbers = InputView.getPickedNumbers();
+            try {
+                bundle.addTicket(LottoNumbers.of(pickedNumbers));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                slipCount++;
+            }
+        }
     }
 }
