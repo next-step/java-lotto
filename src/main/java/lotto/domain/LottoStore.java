044d93524146f04d92aa927470672beb8250c274
Taehee9 <dhstoalfh9509@naver.com>
2020-09-19 16:51:29 +0900

    feat: 로또 게임 구현

---
+++ src/main/java/lotto/domain/LottoStore.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Map;
+
+public class LottoStore {
+    private List<Lotto> purchasedLotto;
+    private Map<Rank, Integer> rankInfo = Rank.getInitRankInfo();
+
+    public LottoStore(List<Lotto> lottoList) {
+        this.purchasedLotto = lottoList;
+    }
+
+    public void matchNumber(Lotto winningLotto) {
+        for(Lotto lotto : purchasedLotto) {
+            Rank rank = lotto.matchLotto(winningLotto);
+            if (rankInfo.containsKey(rank)) {
+                rankInfo.replace(rank, rankInfo.get(rank) + 1);
+            }
+        }
+    }
+
+    public Map<Rank, Integer> getRankInfo() {
+        return rankInfo;
+    }
+
+    public int getTotalPrize() {
+        return rankInfo.keySet()
+                .stream()
+                .mapToInt(rank -> rank.getPrize() * rankInfo.get(rank))
+                .reduce((beforePrize, newPrize) -> beforePrize + newPrize)
+                .getAsInt();
+    }
+
+    public String statistics(Money money) {
+        int revenue = getTotalPrize();
+        double yeild = (double) revenue / (double) money.getLottoPrice();
+        return String.format("%.2f", yeild);
+    }
+}
