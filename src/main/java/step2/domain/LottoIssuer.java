badf3e1405741a668e405314873a410d92a60f79
An_Teatreeessential <wdefrg1324@gmail.com>
2020-09-19 16:05:09 +0900

    refactor : 로또번호 캐싱 책임 이전

--- src/main/java/step2/domain/LottoIssuer.java
+++ src/main/java/step2/domain/LottoIssuer.java
@@ -6,26 +6,16 @@ import java.util.List;
 
 public class LottoIssuer {
     private static final int LOTTO_PRICE = 1000;
-    private static final List<LottoNumber> LOTTO_NUM_STORE = new ArrayList<>();
-
-    static {
-        for (int i = 1; i <= 45; i++) {
-            LOTTO_NUM_STORE.add(LottoNumber.of(i));
-        }
-    }
 
     public static List<Lotto> issueLottos(Money money) {
         List<Lotto> LottoList = new ArrayList<>();
 
         for (int i = 0, j = money.getNumberOfPurchases(); i < j; i++) {
-            LottoList.add(Lotto.create(getRandomLottoNumber()));
+            LottoList.add(Lotto.create(LottoNumber.getRandomLottoNumber()));
         }
 
         return LottoList;
     }
-
-    private static List<LottoNumber> getRandomLottoNumber() {
-        Collections.shuffle(LOTTO_NUM_STORE);
-        return LOTTO_NUM_STORE.subList(0, 6);
-    }
 }
+
+
