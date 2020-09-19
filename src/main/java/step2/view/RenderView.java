f069db6825c965a6a144c62b9e95255c53691a2a
An_Teatreeessential <wdefrg1324@gmail.com>
2020-09-19 22:48:28 +0900

    refactor : 당첨 통계 렌더링 수정

--- src/main/java/step2/view/RenderView.java
+++ src/main/java/step2/view/RenderView.java
@@ -29,9 +29,10 @@ public class RenderView {
     public static void showWinningStatic(Map<Rank, Integer> rankInfo) {
         StringBuilder sb = new StringBuilder("당첨 통계\n");
         sb.append("-------\n");
-        sb.append("3개 일치 (5000원) -" + rankInfo.get(Rank.FOURTH) + "개\n");
-        sb.append("4개 일치 (50000원) -" + rankInfo.get(Rank.THIRD) + "개\n");
-        sb.append("5개 일치 (1500000원) -" + rankInfo.get(Rank.SECOND) + "개\n");
+        sb.append("3개 일치 (5000원) -" + rankInfo.get(Rank.FIFTH) + "개\n");
+        sb.append("4개 일치 (50000원) -" + rankInfo.get(Rank.FOURTH) + "개\n");
+        sb.append("5개 일치 (1500000원) -" + rankInfo.get(Rank.THIRD) + "개\n");
+        sb.append("5개 일치, 보너스 볼 일치 (30000000원) -" + rankInfo.get(Rank.SECOND) + "개\n");
         sb.append("6개 일치 (2000000000원) -" + rankInfo.get(Rank.FIRST) + "개\n");
 
         System.out.println(sb.toString());
