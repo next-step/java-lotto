c0af88f9759269f9a5018810e624c9c2feb1f3e7
Taehee9 <dhstoalfh9509@naver.com>
2020-09-19 16:51:16 +0900

    feat: 로또 순위 구현

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NOTHING(0, 0);
+
+    private int matchCount;
+    private int prize;
+
+    private Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public static Rank getRank(int matchCount) {
+        return Arrays.stream(Rank.values())
+                .filter((rank) -> rank.matchCount == matchCount)
+                .findAny()
+                .orElse(NOTHING);
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Map<Rank, Integer> getInitRankInfo() {
+        return Arrays.stream(Rank.values())
+                .collect(Collectors.toMap(rank -> rank, rank -> 0));
+    }
+
+}
