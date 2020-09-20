12f9b95137b6054a52a78b0f8edd0fce2efc7b63
Taehee9 <dhstoalfh9509@naver.com>
2020-09-20 23:07:33 +0900

    feat: 보너스볼 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 public enum Rank {
     FIRST(6, 2_000_000_000),
     SECOND(5, 1_500_000),
+    SECOND_BONUS(5, 30_000_000),
     THIRD(4, 50_000),
     FOURTH(3, 5_000),
     NOTHING(0, 0);
@@ -19,11 +20,19 @@ public enum Rank {
         this.prize = prize;
     }
 
-    public static Rank getRank(int matchCount) {
+    public static Rank getRank(int matchCount, boolean bonus) {
+        if(bonus) {
+            return Rank.SECOND_BONUS;
+        }
+
         return Arrays.stream(Rank.values())
                 .filter((rank) -> rank.matchCount == matchCount)
                 .findAny()
                 .orElse(NOTHING);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
     }
 
     public int getPrize() {
