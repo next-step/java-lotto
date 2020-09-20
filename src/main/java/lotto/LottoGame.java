7f3c1de8176f30bce8d7cc36a8019ff2f75f6d85
Taehee9 <dhstoalfh9509@naver.com>
2020-09-20 23:04:00 +0900

    feat: 보너스볼 추가 및 리팩토링

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,23 +1,24 @@
 package lotto;
 
-import lotto.domain.*;
+import lotto.domain.LottoStore;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.ResultView;
-
-import java.util.List;
 
 public class LottoGame {
     public static void main(String...args){
         Money price = new Money(InputView.howMuchToBuy());
-        List<Lotto> lotto = LottoGenerator.generateLottoList(price.getLottoCount());
+        Lottos lotto = Lottos.generateLottoList(price.getLottoCount());
 
         ResultView.printLottoCount(price.getLottoCount());
-        ResultView.printLotto(lotto);
+        ResultView.printLotto(lotto.getLottoList());
 
-        WinningLotto winningLotto = new WinningLotto(InputView.winningNumber());
+        WinningLotto winningLotto = new WinningLotto(InputView.winningNumber(), InputView.bonusNumber());
 
         LottoStore purchasedLotto = new LottoStore(lotto);
-        purchasedLotto.matchNumber(winningLotto.getWinningLotto());
+        purchasedLotto.matchNumber(winningLotto);
 
         ResultView.printWinningStatistics(purchasedLotto.getRankInfo());
 
