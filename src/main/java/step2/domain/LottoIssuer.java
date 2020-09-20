596e6c333c5760a421e2ae815bba32a0775b1fa0
An_Teatreeessential <wdefrg1324@gmail.com>
2020-09-20 18:53:50 +0900

    feat : 로또 발급기 내 수동번호 생성 기능 추가

--- src/main/java/step2/domain/LottoIssuer.java
+++ src/main/java/step2/domain/LottoIssuer.java
@@ -3,18 +3,29 @@ package step2.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoIssuer {
-    private static final int LOTTO_PRICE = 1000;
 
-    public static List<Lotto> issueLottos(Money money) {
-        List<Lotto> LottoList = new ArrayList<>();
+    public static List<Lotto> issueAutoLottos(int issueCount) {
+        List<Lotto> issuedAutoLottos = new ArrayList<>();
 
-        for (int i = 0, j = money.getNumberOfPurchases(); i < j; i++) {
-            LottoList.add(Lotto.create(LottoNumber.getRandomLottoNumber()));
+        for (int i = 0; i < issueCount; i++) {
+            issuedAutoLottos.add(Lotto.create(LottoNumber.getRandomLottoNumber()));
         }
 
-        return LottoList;
+        return issuedAutoLottos;
+    }
+
+    public static List<Lotto> issueNonAutoLotto(List<String> customLottoNumberList) {
+        List<Lotto> issuedNonAutoLottos = new ArrayList<>();
+
+        for (int i = 0; i < customLottoNumberList.size(); i++) {
+            issuedNonAutoLottos.add(Lotto.create(customLottoNumberList.get(i)));
+        }
+
+        return issuedNonAutoLottos;
     }
 }
 
