52bf48cd5f16d851c1c0dc833a724d29667be287
siyoon <siyoon210@gmail.com>
2020-11-05 08:54:37 +0900

    refactor: stringParser 인스턴스 변수화, 합산과정 메서드 추출

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,21 +4,28 @@ import java.util.List;
 
 public class StringCalculator {
     public static final String NEGATIVE_NUMBER_CAN_NOT_BE_INCLUDED_ERR_MSG = "음수는 포함될 수 없습니다.";
+    private final StringParser stringParser;
+
+    public StringCalculator() {
+        this.stringParser = new StringParser();
+    }
 
     public int sum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        StringParser stringParser = new StringParser();
         List<Integer> numbers = stringParser.getNumbersFrom(input);
 
+        return sumNumbers(numbers);
+    }
+
+    private int sumNumbers(List<Integer> numbers) {
         int result = 0;
         for (Integer number : numbers) {
             validateNumber(number);
             result += number;
         }
-
         return result;
     }
 
