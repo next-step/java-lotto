d511f4a183136dc0bde41c1aaec7b28954edd4cf
siyoon <siyoon210@gmail.com>
2020-11-05 07:45:09 +0900

    feat: 구분자(DELETIMER)에 콜론(:) 추가

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,14 +1,14 @@
 package stringcalculator;
 
 public class StringCalculator {
-    public static final String DELEMITER = ",";
+    public static final String DELEMITERS = "[,:]";
 
     public int sum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        String[] inputSplits = input.split(DELEMITER);
+        String[] inputSplits = input.split(DELEMITERS);
         int result = 0;
         for (String inputSplit : inputSplits) {
             result += Integer.parseInt(inputSplit);
