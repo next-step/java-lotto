c513f88b2b07ab1938452faea0434e0155cb7274
siyoon <siyoon210@gmail.com>
2020-11-05 07:38:19 +0900

    refactor: 구분문자 "," 상수화

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,13 +1,14 @@
 package stringcalculator;
 
 public class StringCalculator {
+    public static final String DELEMITER = ",";
 
     public int sum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        String[] inputSplits = input.split(",");
+        String[] inputSplits = input.split(DELEMITER);
         int result = 0;
         for (String inputSplit : inputSplits) {
             result += Integer.parseInt(inputSplit);
