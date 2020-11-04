7db191c8779a638e96faa48bed984f58ee54fd82
siyoon <siyoon210@gmail.com>
2020-11-05 07:52:40 +0900

    refactor: delimiter들을 List로 가지고, 이를 이용하여 regex를 만드는 메서도 추출

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,19 +1,26 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
-    public static final String DELEMITERS = "[,:]";
+    private static final List<String> DEFAULT_DELIMITERS = Arrays.asList(",", ":");
 
     public int sum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        String[] inputSplits = input.split(DELEMITERS);
+        String[] inputSplits = input.split(getNumberDelimiterRegex());
         int result = 0;
         for (String inputSplit : inputSplits) {
             result += Integer.parseInt(inputSplit);
         }
 
         return result;
+    }
+
+    private String getNumberDelimiterRegex() {
+        return "[" + String.join("", DEFAULT_DELIMITERS) + "]";
     }
 }
