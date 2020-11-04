8d0a14782b2d56d3ad9585b15917c77a7b51fe86
siyoon <siyoon210@gmail.com>
2020-11-05 08:28:37 +0900

    refactor: 커스텀 구분자에서 사용된 index 매직넘버 상수화

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -8,6 +8,8 @@ import java.util.regex.Pattern;
 public class StringCalculator {
     private static final List<String> DEFAULT_DELIMITERS = new ArrayList<>(Arrays.asList(",", ":"));
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//.\\\\n.*");
+    public static final int INDEX_OF_CUSTOM_DELIMITER = 2;
+    public static final int LAST_INDEX_OF_CUSTOM_DELIMITER_PHRASE = 5;
 
     public int sum(String input) {
         if (input == null || input.isEmpty()) {
@@ -16,8 +18,8 @@ public class StringCalculator {
 
         List<String> delimiters = new ArrayList<>(DEFAULT_DELIMITERS);
         if (hasCustomDelimiter(input)) {
-            delimiters.add(String.valueOf(input.charAt(2)));
-            input = input.substring(5);
+            delimiters.add(String.valueOf(input.charAt(INDEX_OF_CUSTOM_DELIMITER)));
+            input = input.substring(LAST_INDEX_OF_CUSTOM_DELIMITER_PHRASE);
         }
 
         String[] inputSplits = input.split(getNumberDelimiterRegex(delimiters));
