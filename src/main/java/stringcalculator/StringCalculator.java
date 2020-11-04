5529dbd72020e1cad87b99e7ee7e8d7b3bba0e87
siyoon <siyoon210@gmail.com>
2020-11-05 07:25:57 +0900

    feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+    public int sum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return -1;
+    }
+}
