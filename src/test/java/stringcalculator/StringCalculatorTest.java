8510d591a6a32858a781e678758ab125d8b5a5ec
siyoon <siyoon210@gmail.com>
2020-11-05 07:27:55 +0900

    test: stringCalculator 인스턴스 변수화

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,17 +1,25 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void init() {
+        stringCalculator = new StringCalculator();
+    }
+
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)")
     void nullAndEmptyTest(String input) {
-        StringCalculator stringCalculator = new StringCalculator();
         assertThat(stringCalculator.sum(input)).isEqualTo(0);
     }
 }
