7e3cfb512e4989f0ce0cb5cc2f0b8c0a77d2a6bd
siyoon <siyoon210@gmail.com>
2020-11-05 08:02:14 +0900

    feat: “//”와 “\n” 문자 사이에 커스텀 구분자를 지정

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,14 +1,20 @@
 package stringcalculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StringCalculator {
-    private static final List<String> DEFAULT_DELIMITERS = Arrays.asList(",", ":");
+    private static final List<String> DEFAULT_DELIMITERS = new ArrayList<>(Arrays.asList(",", ":"));
 
     public int sum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
+        }
+
+        if (hasCustomDelimiter(input)) {
+            addDelimiter();
+            input = input.substring(5);
         }
 
         String[] inputSplits = input.split(getNumberDelimiterRegex());
@@ -18,6 +24,14 @@ public class StringCalculator {
         }
 
         return result;
+    }
+
+    private void addDelimiter() {
+        DEFAULT_DELIMITERS.add(";");
+    }
+
+    private boolean hasCustomDelimiter(String input) {
+        return input.startsWith("//;\\n");
     }
 
     private String getNumberDelimiterRegex() {
