2c1f6a6997e15dc80478611604a575dab1aea5ca
genos.lee <genos.lee@kakaocorp.com>
2020-11-05 20:02:14 +0900

    feature: 빈문자, null 입력 시 0 반환, 숫자 하나 문자열로 입력시 그대로 반환

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class StringAddCalculator {
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String expression) {
+        if (isNullOrEmpty(expression)) {
+            return 0;
+        }
+        return Arrays.stream(expression.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private static boolean isNullOrEmpty(String expression) {
+        return expression == null || expression.isEmpty();
+    }
+}
