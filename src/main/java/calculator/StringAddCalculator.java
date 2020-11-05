75059bcd9c14274e2f305bd19eacadf756c3839b
genos.lee <genos.lee@kakaocorp.com>
2020-11-05 21:12:01 +0900

    refactor: step1 리뷰 반영
    Pattern.compile 한번만 실행되게 분리
    pattern matching된 그룹 id 상수로 분리
    RuntimeException error 메시지 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -7,8 +7,11 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+    private final static int DELIMITER_GROUP_ID = 1;
+    private final static int EXPRESSION_GROUP_ID = 2;
     private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
 
     private StringAddCalculator() {
     }
@@ -21,9 +24,9 @@ public class StringAddCalculator {
     }
 
     private static int getSum(String expression) {
-        Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(expression);
+        Matcher m = pattern.matcher(expression);
         if (m.find()) {
-            return getSum(getTokens(m.group(2), m.group(1)));
+            return getSum(getTokens(m.group(EXPRESSION_GROUP_ID), m.group(DELIMITER_GROUP_ID)));
         }
         return getSum(getTokens(expression, DEFAULT_DELIMITER_REGEX));
     }
@@ -48,7 +51,7 @@ public class StringAddCalculator {
 
     private static void validateUnsignedNumber(int number) {
         if (number < 0) {
-            throw new RuntimeException();
+            throw new RuntimeException("0이상의 숫자가 입력되어야 합니다.");
         }
     }
 
