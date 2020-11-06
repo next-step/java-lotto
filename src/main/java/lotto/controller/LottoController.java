fd3250b92917a0e316e0c875e2e9f96ca3bfc219
genos.lee <genos.lee@kakaocorp.com>
2020-11-07 00:18:27 +0900

    feature: 로또 구입하지 않은 경우 수익률 계산 로직 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,7 +6,6 @@ import lotto.view.InputView;
 import lotto.view.ResultView;
 
 import java.io.PrintWriter;
-import java.util.List;
 import java.util.Scanner;
 
 public class LottoController {
@@ -16,17 +15,21 @@ public class LottoController {
     public static void main(String[] args) {
         PrintWriter output = new PrintWriter(System.out, true);
         InputView inputView = new InputView(new Scanner(System.in), output);
+        ResultView resultView = new ResultView(output);
+
         int money = inputView.getMoney();
         Lottos lottos = Lottos.withMoney(money);
-        ResultView resultView = new ResultView(output);
         resultView.showLottos(lottos);
-        List<Integer> lastLottoNumbers = inputView.getLastLottoNumbers();
-        WinningStatistics winningStatistics = lottos.getWinningStatistics(lastLottoNumbers);
-        int revenue = winningStatistics.calculateRevenue();
-        double yield = revenue / (double) money;
-
-        resultView.showResult(winningStatistics, yield);
+        WinningStatistics winningStatistics = lottos.getWinningStatistics(inputView.getLastLottoNumbers());
+        resultView.showResult(winningStatistics, getYield(money, winningStatistics.calculateRevenue()));
 
         output.close();
+    }
+
+    private static double getYield(int money, int revenue) {
+        if (money == 0) {
+            return 0;
+        }
+        return revenue / (double) money;
     }
 }
