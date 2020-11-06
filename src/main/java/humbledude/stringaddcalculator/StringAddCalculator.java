9635612a69877db75f1f08be9c685e2e7c77e9ac
joshua(박근희) <joshua.park321@kakaocorp.com>
2020-11-06 09:34:58 +0900

    feature(StringAddCalculator): 음수 예외처리
    음수 입력시 예외처리

--- src/main/java/humbledude/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/humbledude/stringaddcalculator/StringAddCalculator.java
@@ -2,7 +2,7 @@ package humbledude.stringaddcalculator;
 
 public class StringAddCalculator {
 
-    private static final String DEAFAULT_SPLIT_TOKEN = ",;";
+    private static final String DEAFAULT_SPLIT_TOKEN = ",|;";
 
     public static int splitAndSum(String input) {
         int sum = 0;
@@ -13,7 +13,7 @@ public class StringAddCalculator {
         String[] strNumbers = input.split(DEAFAULT_SPLIT_TOKEN);
 
         for (String strNumber : strNumbers) {
-            sum += Integer.parseInt(strNumber);
+            sum += parseInt(strNumber);
         }
 
         return sum;
@@ -28,5 +28,15 @@ public class StringAddCalculator {
         }
 
         return true;
+    }
+
+    private static int parseInt(String strNumber) {
+        int number = Integer.parseInt(strNumber);
+
+        if (number < 0) {
+            throw new RuntimeException("음수는 지원하지 않습니다");
+        }
+
+        return number;
     }
 }
