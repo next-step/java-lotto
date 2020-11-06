75740987278e678407ae40abc5206417538725a6
genos.lee <genos.lee@kakaocorp.com>
2020-11-06 20:56:27 +0900

    feature: 지난주 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private final Scanner scanner;
@@ -12,9 +15,16 @@ public class InputView {
         this.output = output;
     }
 
-
     public int getMoney() {
         output.println("구입금액을 입력해 주세요.");
         return scanner.nextInt();
+    }
+
+    public List<Integer> getLastLottoNumbers() {
+        output.println("지난 주 당첨 번호를 입력해 주세요.");
+        return Arrays.stream(scanner.nextLine().split(","))
+                .map(input -> Integer.parseInt(input.trim()))
+                .collect(Collectors.toList());
+
     }
 }
