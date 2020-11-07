d24eac76ed9fdddcc07ef75bb1db400c9076d98e
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-07 23:00:48 +0900

    [feat]: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -2,6 +2,8 @@ package step1;
 
 public class StringAddCalculator {
 
+    private static final String delimiter = ",";
+
     private static boolean checkNull(String str) {
         return str == null;
     }
@@ -14,9 +16,24 @@ public class StringAddCalculator {
         return checkNull(str) || checkEmpty(str);
     }
 
+    private static String[] splitStr(String str) {
+        return str.split(delimiter);
+    }
+
+    private static int parseStr(String str) {
+        return Integer.parseInt(str);
+    }
+
+    private static boolean checkSingle(String str) {
+        return splitStr(str).length < 1;
+    }
+
     public static int splitAndSum(String str) {
         if (checkZero(str)) {
             return 0;
+        }
+        if (checkSingle(str)) {
+            return parseStr(str);
         }
         return 1;
     }
