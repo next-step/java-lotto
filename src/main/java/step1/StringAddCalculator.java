4938818ce90c100faec3efccad8c75b4489597d5
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-07 22:49:22 +0900

    [feat]: 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -2,7 +2,22 @@ package step1;
 
 public class StringAddCalculator {
 
+    private static boolean checkNull(String str) {
+        return str == null;
+    }
+
+    private static boolean checkEmpty(String str) {
+        return str.isEmpty();
+    }
+
+    private static boolean checkZero(String str) {
+        return checkNull(str) || checkEmpty(str);
+    }
+
     public static int splitAndSum(String str) {
+        if (checkZero(str)) {
+            return 0;
+        }
         return 1;
     }
 }
