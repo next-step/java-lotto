b1a70c72c4abc1632970e17ae218a3130a8ff15b
siyoon <siyoon210@gmail.com>
2020-11-07 10:05:55 +0900

    test: 로또 생성시 number size가 유효하지 않으면 throw IllegalStateException

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class LottoTest {
+    @Test
+    @DisplayName("로또 생성시 number size가 유효하지 않으면 throw IllegalStateException")
+    void createTest_invalidNumberSize() {
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(
+                        () -> new Lotto(Arrays.asList(1, 2, 3, 4, 5))
+                );
+    }
+}
