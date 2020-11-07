0d412a53c27aaf10c543d544815e69b33b36d1f4
siyoon <siyoon210@gmail.com>
2020-11-07 10:07:06 +0900

    refactor: 상수 rename

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,13 +5,13 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Lotto {
-    private static final int NUMBERS_SIZE = 6;
+    private static final int VALID_NUMBERS_SIZE = 6;
 
     private final Set<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = new TreeSet<>(numbers);
-        if (this.numbers.size() != NUMBERS_SIZE) {
+        if (this.numbers.size() != VALID_NUMBERS_SIZE) {
             throw new IllegalStateException();
         }
     }
