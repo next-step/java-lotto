ca29693a777f7f96249dd8cd3c35e009f5764856
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-07 23:08:28 +0900

    [feat]: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -28,6 +28,29 @@ public class StringAddCalculator {
         return splitStr(str).length < 1;
     }
 
+    private static int[] parseStrArr(String[] strArr) {
+        int length = strArr.length;
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = parseStr(strArr[i]);
+        }
+        return arr;
+    }
+
+    private static int calcSum(int[] arr) {
+        int sum = 0;
+        for (int num: arr) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static int calcSum(String str) {
+        String[] strArr = splitStr(str);
+        int[] intArr = parseStrArr(strArr);
+        return calcSum(intArr);
+    }
+
     public static int splitAndSum(String str) {
         if (checkZero(str)) {
             return 0;
@@ -35,6 +58,6 @@ public class StringAddCalculator {
         if (checkSingle(str)) {
             return parseStr(str);
         }
-        return 1;
+        return calcSum(str);
     }
 }
