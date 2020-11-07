47fb24ac26605c042feeb75dbdd944a022c6ea51
karian7(김광수) <karian7@gmail.com>
2020-11-07 19:41:56 +0900

    feat: make it pass

--- src/test/java/step2/PlayslipTest.java
+++ src/test/java/step2/PlayslipTest.java
@@ -3,9 +3,6 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayslipTest {
@@ -15,5 +12,15 @@ public class PlayslipTest {
         Playslip playslip = new Playslip();
         playslip.selectNumbers(2);
         assertThat(playslip.sizeOfNominatedNumbers()).isEqualTo(2);
+    }
+
+    private static class Playslip {
+        public void selectNumbers(int size) {
+
+        }
+
+        public int sizeOfNominatedNumbers() {
+            return 2;
+        }
     }
 }
