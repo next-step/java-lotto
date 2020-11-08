d63561e9a69d4e1abc0b9c82bdd69a7b2edb3be6
siyoon <siyoon210@gmail.com>
2020-11-08 11:39:49 +0900

    test: IllegalStateException 메시지 검증 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -14,9 +14,11 @@ public class LottoTest {
     void createTest_invalidNumberSize() {
         assertAll(
                 () -> assertThatExceptionOfType(IllegalStateException.class)
-                        .isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5))),
+                        .isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5)))
+                        .withMessage(Lotto.INVALID_NUMBERS_SIZE_ERR_MSG),
                 () -> assertThatExceptionOfType(IllegalStateException.class)
                         .isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6, 7)))
+                        .withMessage(Lotto.INVALID_NUMBERS_SIZE_ERR_MSG)
         );
     }
 
@@ -24,17 +26,20 @@ public class LottoTest {
     @DisplayName("로또 숫자가 중복되어 number size가 유효하지 않으면 throw IllegalStateException")
     void createTest_duplicatedNumbers() {
         assertThatExceptionOfType(IllegalStateException.class)
-                .isThrownBy(() -> new Lotto(Arrays.asList(1, 1, 1, 1, 1, 1)));
+                .isThrownBy(() -> new Lotto(Arrays.asList(1, 1, 1, 1, 1, 1)))
+                .withMessage(Lotto.INVALID_NUMBERS_SIZE_ERR_MSG);
     }
 
     @Test
-    @DisplayName("로또 숫자는 지정된 범위 만족하지 않으면 throw IllegalStateException (ex 1~45 사이의 숫자만).")
+    @DisplayName("로또 숫자가 지정된 범위 만족하지 않으면 throw IllegalStateException (ex 1~45 사이의 숫자만).")
     void createTest_numberRange() {
         assertAll(
                 () -> assertThatExceptionOfType(IllegalStateException.class)
-                        .isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5, 100))),
+                        .isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5, 46)))
+                        .withMessage(Lotto.INVALID_NUMBER_RANGE_ERR_MSG),
                 () -> assertThatExceptionOfType(IllegalStateException.class)
                         .isThrownBy(() -> new Lotto(Arrays.asList(0, 1, 2, 3, 4, 5)))
+                        .withMessage(Lotto.INVALID_NUMBER_RANGE_ERR_MSG)
         );
     }
 }
