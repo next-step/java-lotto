e4634ae7c3a2a096b758149a66efdc1e6dc34227
siyoon <siyoon210@gmail.com>
2020-11-08 11:28:29 +0900

    feat: 로또 숫자가 지정된 범위 만족하지 않으면 throw IllegalStateException

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Lotto {
+    private static final int VALID_MIN_NUMBER = 1;
+    private static final int VALID_MAX_NUMBER = 45;
     private static final int VALID_NUMBERS_SIZE = 6;
 
     private final Set<Integer> numbers;
@@ -13,6 +15,15 @@ public class Lotto {
         this.numbers = new TreeSet<>(numbers);
         if (this.numbers.size() != VALID_NUMBERS_SIZE) {
             throw new IllegalStateException();
+        }
+        for (Integer number : numbers) {
+            if (number < VALID_MIN_NUMBER) {
+                throw new IllegalStateException();
+            }
+
+            if (number > VALID_MAX_NUMBER) {
+                throw new IllegalStateException();
+            }
         }
     }
 }
