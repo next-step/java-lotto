98dbee39c87a73dcc56501967c3b104650942955
karian7(김광수) <karian7@gmail.com>
2020-11-08 22:35:44 +0900

    test: LotteryNumber 는 6개의 번호를 제공해야 한다

---
+++ src/test/java/step2/LotteryNumberTest.java
@@ -0,0 +1,17 @@
+package step2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class LotteryNumberTest {
+    @DisplayName("6개의 번호를 제공해야 한다")
+    @Test
+    void satisfyConstructor() {
+        Assertions.assertThatThrownBy(() -> new LotteryNumber(new HashSet<>(Arrays.asList(1, 2, 3)))) //
+                .isInstanceOf(NotEnoughNumberException.class);
+    }
+}
