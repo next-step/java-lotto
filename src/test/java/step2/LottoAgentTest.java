f4ae183f03dd7ec6dc58ef0c0a67478ed6d28a77
karian7(김광수) <karian7@gmail.com>
2020-11-08 21:54:39 +0900

    test: 최소 구입금액에 못미친면 예외가 발생한다

--- src/test/java/step2/LottoAgentTest.java
+++ src/test/java/step2/LottoAgentTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoAgentTest {
     @DisplayName("돈을 받고 티켓과 거스름돈을 반환한다.")
@@ -14,6 +15,14 @@ public class LottoAgentTest {
         assertThat(ticketAndChange).isNotNull() //
                 .hasSize(2) //
                 .hasOnlyElementsOfTypes(LottoTickets.class, Integer.class);
+    }
+
+    @DisplayName("돈이 최소 구입금액에 못미치면 예외가 발생한다.")
+    @Test
+    void notEnoughMoney() {
+        LottoAgent lottoAgent = new LottoAgent();
+        assertThatThrownBy(() -> lottoAgent.exchange(500)) //
+                .isInstanceOf(NotEnoughMoneyException.class);
     }
 
     private static class LottoAgent {
