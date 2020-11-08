74a8a5b72493cfcfc34c5215102c7f1ed01d2b86
karian7(김광수) <karian7@gmail.com>
2020-11-08 10:41:07 +0900

    feat: make it pass
    int array 를 List<Integer> 로 변경하였는데, 리스트 핸들링이 더 용이해서 변경하였다.

--- src/test/java/step2/NumberSelectionTest.java
+++ src/test/java/step2/NumberSelectionTest.java
@@ -3,9 +3,7 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,9 +11,9 @@ public class NumberSelectionTest {
     @DisplayName("입력받은 번호 중 6개의 번호를 선택할 수 있다.")
     @Test
     void sixNumberSelection() {
-        int[] selectableNumbers = new int[45];
+        List<Integer> selectableNumbers = new ArrayList<>();
         for (int i = 0; i < 45; i++) {
-            selectableNumbers[i] = i;
+            selectableNumbers.add(i);
         }
 
         NaturalSelection selection = new NaturalSelection();
@@ -23,11 +21,11 @@ public class NumberSelectionTest {
         assertThat(numberSet.size()).isEqualTo(6);
     }
 
-
     private static class NaturalSelection {
 
-        public Set<Integer> select() {
-            return new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        public Set<Integer> select(List<Integer> selectableNumbers) {
+            return new HashSet<>(selectableNumbers.subList(0, 6));
         }
+
     }
 }
