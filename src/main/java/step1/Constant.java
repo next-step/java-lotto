c635de86f34658f829d5eb2bda5f35574ec56dfe
catsbi <killfine198@gmail.com>
2020-11-08 17:22:37 +0900

    [feat] 상수 모음 클래스 생성

---
+++ src/main/java/step1/Constant.java
@@ -0,0 +1,9 @@
+package step1;
+
+public class Constant {
+    public static final String DEFAULT_SPLIT_REGEX = "[,:]";
+    public static final String CUSTOM_SEPARATE_REGEX = "//(.)\n(.*)";
+    public static final char NEGATIVE_OPERATOR = '-';
+
+   private Constant(){}
+}
