040b7fbe23ef587be54c8c2c6dd1f5b78f583639
siyoon <siyoon210@gmail.com>
2020-11-08 11:44:04 +0900

    refactor: validate절차 메서드 추출

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 
 public class Lotto {
     public static final String INVALID_NUMBERS_SIZE_ERR_MSG = "로또 숫자의 개수가 유효하지 않습니다.";
-    public static final String INVALID_NUMBER_RANGE_ERR_MSG = "로또 숫자가 유효범위하지 않습니다.";
+    public static final String INVALID_NUMBER_RANGE_ERR_MSG = "로또 숫자가 유효범위를 벗어났습니다.";
 
     private static final int VALID_MIN_NUMBER = 1;
     private static final int VALID_MAX_NUMBER = 45;
@@ -16,14 +16,23 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         this.numbers = new TreeSet<>(numbers);
-        if (this.numbers.size() != VALID_NUMBERS_SIZE) {
+        validateState(numbers);
+    }
+
+    private void validateState(List<Integer> numbers) {
+        validateNumbersSize();
+        numbers.forEach(this::validateNumberRange);
+    }
+
+    private void validateNumbersSize() {
+        if (numbers.size() != VALID_NUMBERS_SIZE) {
             throw new IllegalStateException(INVALID_NUMBERS_SIZE_ERR_MSG);
         }
+    }
 
-        for (Integer number : numbers) {
-            if (number < VALID_MIN_NUMBER || number > VALID_MAX_NUMBER) {
-                throw new IllegalStateException(INVALID_NUMBER_RANGE_ERR_MSG);
-            }
+    private void validateNumberRange(Integer number) {
+        if (number < VALID_MIN_NUMBER || number > VALID_MAX_NUMBER) {
+            throw new IllegalStateException(INVALID_NUMBER_RANGE_ERR_MSG);
         }
     }
 }
