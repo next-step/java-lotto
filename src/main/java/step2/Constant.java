b3493db0105f33204af2f3e2111596078c0b09e4
catsbi <killfine1988@gmail.com>
2020-11-09 01:00:24 +0900

    [feat] 매직넘버 클래스 생성

---
+++ src/main/java/step2/Constant.java
@@ -0,0 +1,14 @@
+package step2;
+
+public class Constant {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_RANGE_MAX = 45;
+    public static final int LOTTO_MARKING_RANGE = 6;
+    public static final int WINNING_NUMBER_ZERO = 0;
+    public static final int TICKET_INIT_COUNT = 0;
+
+
+    public static final String ERROR_NOT_ENOUGH_MONEY = "돈이 충분하지 않습니다.";
+
+    private Constant(){}
+}
