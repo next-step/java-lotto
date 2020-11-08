d99239d8fd169749aa7e62e2d0ba2b10d4d4f124
karian7(김광수) <karian7@gmail.com>
2020-11-08 22:36:43 +0900

    feat: make it pass

--- src/test/java/step2/LotteryNumberTest.java
+++ src/test/java/step2/LotteryNumberTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class LotteryNumberTest {
     @DisplayName("6개의 번호를 제공해야 한다")
@@ -13,5 +14,16 @@ public class LotteryNumberTest {
     void satisfyConstructor() {
         Assertions.assertThatThrownBy(() -> new LotteryNumber(new HashSet<>(Arrays.asList(1, 2, 3)))) //
                 .isInstanceOf(NotEnoughNumberException.class);
+    }
+
+    private static class LotteryNumber {
+        public LotteryNumber(Set<Integer> numbers) {
+            if (numbers.size() != 6) {
+                throw new NotEnoughNumberException();
+            }
+        }
+    }
+
+    private static class NotEnoughNumberException extends IllegalArgumentException {
     }
 }
