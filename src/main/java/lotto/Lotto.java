ed5bd5b6889ab539d106b7dee7092a376eec6ab3
siyoon <siyoon210@gmail.com>
2020-11-08 11:39:04 +0900

    refactor: IllegalStateExcpetion의 메세지 설정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,9 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Lotto {
+    public static final String INVALID_NUMBERS_SIZE_ERR_MSG = "로또 숫자의 개수가 유효하지 않습니다.";
+    public static final String INVALID_NUMBER_RANGE_ERR_MSG = "로또 숫자가 유효범위하지 않습니다.";
+
     private static final int VALID_MIN_NUMBER = 1;
     private static final int VALID_MAX_NUMBER = 45;
     private static final int VALID_NUMBERS_SIZE = 6;
@@ -14,15 +17,12 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         this.numbers = new TreeSet<>(numbers);
         if (this.numbers.size() != VALID_NUMBERS_SIZE) {
-            throw new IllegalStateException();
+            throw new IllegalStateException(INVALID_NUMBERS_SIZE_ERR_MSG);
         }
-        for (Integer number : numbers) {
-            if (number < VALID_MIN_NUMBER) {
-                throw new IllegalStateException();
-            }
 
-            if (number > VALID_MAX_NUMBER) {
-                throw new IllegalStateException();
+        for (Integer number : numbers) {
+            if (number < VALID_MIN_NUMBER || number > VALID_MAX_NUMBER) {
+                throw new IllegalStateException(INVALID_NUMBER_RANGE_ERR_MSG);
             }
         }
     }
