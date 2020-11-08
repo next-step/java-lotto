2e994e0ff4f3d58324b3fc01114e4eafc2b5e71d
siyoon <siyoon210@gmail.com>
2020-11-08 15:12:38 +0900

    test: 테스트 케이스 추가

--- src/test/java/lotto/WinningCheckerTest.java
+++ src/test/java/lotto/WinningCheckerTest.java
@@ -14,10 +14,6 @@ public class WinningCheckerTest {
     @DisplayName("구매한 로또들과 지난 주 당첨 번호로, 등수별 당첨 여부와 수익이 얼마나 되는지 계산한다.")
     void winningAndStatisticTest() {
         List<Lotto> boughtLottos = new ArrayList<>();
-//        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
-//        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));
-//        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 7, 8)));
-//        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)));
         boughtLottos.add(new Lotto(Arrays.asList(1, 2, 7, 8, 9, 10)));
         List<Integer> winningNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
@@ -25,5 +21,12 @@ public class WinningCheckerTest {
         WinningStatistic winningStatistic = winningChecker.getResult(winningNumbers, boughtLottos);
 
         assertThat(winningStatistic.getEarningsRate()).isEqualTo(0);
+
+        List<Lotto> boughtLottos2 = new ArrayList<>();
+        boughtLottos2.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        WinningStatistic winningStatistic2 = winningChecker.getResult(winningNumbers, boughtLottos2);
+
+        assertThat(winningStatistic2.getEarningsRate()).isEqualTo((double) WinningRank.MATCHES_SIX.winningAmount / LottoFactory.PRICE_OF_ONE_LOTTO);
     }
 }
