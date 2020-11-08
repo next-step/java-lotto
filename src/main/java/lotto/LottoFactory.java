c060c5115b2e31b11865f9792aa4d1ee6de576c9
siyoon <siyoon210@gmail.com>
2020-11-08 11:53:45 +0900

    refactor: 로또 한개 구입가격 상수화

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LottoFactory {
+    private static final int PRICE_OF_ONE_LOTTO = 1_000;
+
     public List<Lotto> buyLotto(int amount) {
         List<Lotto> lottos = new ArrayList<>();
-        for (; amount >= 1000; amount -= 1000) {
+        for (; amount >= PRICE_OF_ONE_LOTTO; amount -= PRICE_OF_ONE_LOTTO) {
             lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
         }
 
