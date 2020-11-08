179bfd9daf0065e468c709418f9b36dfb7cfcd97
siyoon <siyoon210@gmail.com>
2020-11-08 15:37:23 +0900

    refactor: 수익률 계산하기 메서드 추출

--- src/main/java/lotto/WinningChecker.java
+++ src/main/java/lotto/WinningChecker.java
@@ -1,15 +1,29 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningChecker {
     public WinningStatistic getResult(List<Integer> winningNumbers, List<Lotto> boughtLottos) {
-        int totalWinningAmount = 0;
-        for (Lotto boughtLotto : boughtLottos) {
-            WinningRank winningRank = WinningRank.getWinningRank(winningNumbers, boughtLotto);
-            totalWinningAmount += winningRank.winningAmount;
-        }
-        int earningsRate = totalWinningAmount / (LottoFactory.PRICE_OF_ONE_LOTTO * boughtLottos.size());
+        List<WinningRank> winningRanks = getWinningRanks(winningNumbers, boughtLottos);
+        int earningsRate = getEarningsRate(winningRanks, boughtLottos.size());
+
         return new WinningStatistic(null, earningsRate);
+    }
+
+    private List<WinningRank> getWinningRanks(List<Integer> winningNumbers, List<Lotto> boughtLottos) {
+        List<WinningRank> winningRanks = new ArrayList<>();
+        for (Lotto boughtLotto : boughtLottos) {
+            winningRanks.add(WinningRank.getWinningRank(winningNumbers, boughtLotto));
+        }
+        return winningRanks;
+    }
+
+    private int getEarningsRate(List<WinningRank> winningRanks, int boughtLottosSize) {
+        int totalWinningAmount = winningRanks.stream()
+                .mapToInt(r -> r.winningAmount)
+                .sum();
+
+        return totalWinningAmount / (LottoFactory.PRICE_OF_ONE_LOTTO * boughtLottosSize);
     }
 }
