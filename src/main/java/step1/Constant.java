900333993845560cec4aeb7115cc07880275bb76
catsbi <killfine198@gmail.com>
2020-11-08 17:26:28 +0900

    [feat] Separator enum 구현, Constant 매직넘버 추가

--- src/main/java/step1/Constant.java
+++ src/main/java/step1/Constant.java
@@ -5,5 +5,9 @@ public class Constant {
     public static final String CUSTOM_SEPARATE_REGEX = "//(.)\n(.*)";
     public static final char NEGATIVE_OPERATOR = '-';
 
+    public static final int SEPARATOR_GROUP_INDEX = 1;
+    public static final int STRING_GROUP_INDEX = 2;
+
+
    private Constant(){}
 }
