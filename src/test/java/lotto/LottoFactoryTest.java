466871fd8701bc03c45a9bf0eddf48f55174233f
siyoon <siyoon210@gmail.com>
2020-11-08 11:48:30 +0900

    test: LottoFactoryTest 생성

---
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class LottoFactoryTest {
+    private LottoFactory lottoFactory;
+
+    @BeforeEach
+    void init() {
+        LottoFacotry = lottoFactory;
+    }
+}
