278a8afe525ecd802481677547b10e405e473f4c
siyoon <siyoon210@gmail.com>
2020-11-09 08:40:16 +0900

    refactor: ConsoleInputView 구분자 상수화, 유효성검사 예외시에 메시지추가

--- src/main/java/lotto/view/inputview/ConsoleInputView.java
+++ src/main/java/lotto/view/inputview/ConsoleInputView.java
@@ -9,6 +9,8 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class ConsoleInputView implements InputView {
+    public static final String INVALID_NUMBERS_SIZE_ERR_MSG = "입력된 당첨 번호 숫자의 개수가 유효하지 않습니다.";
+    private static final String DELIMITER = ",";
     private final Scanner scanner;
 
     public ConsoleInputView() {
@@ -26,14 +28,18 @@ public class ConsoleInputView implements InputView {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String winningNumbersInput = scanner.next();
 
-        Set<Integer> winningNumbers = Arrays.stream(winningNumbersInput.split(","))
+        Set<Integer> winningNumbers = Arrays.stream(winningNumbersInput.split(DELIMITER))
                 .map(Integer::valueOf)
                 .collect(Collectors.toCollection(TreeSet::new));
 
-        if (winningNumbers.size() != Lotto.VALID_NUMBERS_SIZE) {
-            throw new IllegalArgumentException();
-        }
+        validate(winningNumbers);
 
         return winningNumbers;
+    }
+
+    private void validate(Set<Integer> winningNumbers) {
+        if (winningNumbers.size() != Lotto.VALID_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(INVALID_NUMBERS_SIZE_ERR_MSG);
+        }
     }
 }
