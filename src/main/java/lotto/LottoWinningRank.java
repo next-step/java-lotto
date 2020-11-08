12aaa00a3fb34ef5bfe2a41317eedd624c0d0605
siyoon <siyoon210@gmail.com>
2020-11-08 12:39:37 +0900

    feat: LottoWinningRank.getWinningRank() 생성

---
+++ src/main/java/lotto/LottoWinningRank.java
@@ -0,0 +1,9 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoWinningRank {
+    public static String getWinningRank(List<Integer> winningNumbers, List<Integer> boughtLottoNumbers) {
+        return "1등";
+    }
+}
