74e4b54a5d9f02bf59cec71cba22248e4556db21
karian7(김광수) <karian7@gmail.com>
2020-11-08 21:57:25 +0900

    refactor: 중복제거

--- src/test/java/step2/LottoAgentTest.java
+++ src/test/java/step2/LottoAgentTest.java
@@ -1,5 +1,6 @@
 package step2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoAgentTest {
+
+    private LottoAgent lottoAgent;
+
+    @BeforeEach
+    void setUp() {
+        lottoAgent = new LottoAgent();
+    }
+
     @DisplayName("돈을 받고 티켓과 거스름돈을 반환한다.")
     @Test
     void exchange() {
-        LottoAgent lottoAgent = new LottoAgent();
         Object[] ticketAndChange = lottoAgent.exchange(1500);
         assertThat(ticketAndChange).isNotNull() //
                 .hasSize(2) //
@@ -20,7 +28,6 @@ public class LottoAgentTest {
     @DisplayName("돈이 최소 구입금액에 못미치면 예외가 발생한다.")
     @Test
     void notEnoughMoney() {
-        LottoAgent lottoAgent = new LottoAgent();
         assertThatThrownBy(() -> lottoAgent.exchange(500)) //
                 .isInstanceOf(NotEnoughMoneyException.class);
     }
