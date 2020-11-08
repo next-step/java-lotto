86c745bfc071ce700f1d2cb22251a72f7164f12c
siyoon <siyoon210@gmail.com>
2020-11-08 16:06:12 +0900

    test: WinningRank별 당첨 횟수 계산

--- src/test/java/lotto/WinningCheckerTest.java
+++ src/test/java/lotto/WinningCheckerTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class WinningCheckerTest {
     private WinningChecker winningChecker;
@@ -24,18 +25,21 @@ public class WinningCheckerTest {
     @ParameterizedTest
     @MethodSource("boughtLottos")
     @DisplayName("구매한 로또들과 지난 주 당첨 번호로, 등수별 당첨 여부와 수익이 얼마나 되는지 계산한다.")
-    void winningAndStatisticTest(List<Lotto> boughtLottos, double expectedEarningRate) {
+    void winningAndStatisticTest(List<Lotto> boughtLottos, double expectedEarningRate, int expectedNumberOfMatchesSix) {
         List<Integer> winningNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
         WinningStatistic winningStatistic = winningChecker.getResult(winningNumbers, boughtLottos);
 
-        assertThat(winningStatistic.getEarningsRate()).isEqualTo(expectedEarningRate);
+        assertAll(
+                () -> assertThat(winningStatistic.getEarningsRate()).isEqualTo(expectedEarningRate),
+                () -> assertThat(winningStatistic.getNumberOfWinningRank(WinningRank.MATCHES_SIX)).isEqualTo(expectedNumberOfMatchesSix)
+        );
     }
 
     private static Stream<Arguments> boughtLottos() {
         return Stream.of(
-                Arguments.of(lottosEarningZero(), 0),
-                Arguments.of(lottosEarningMax(), WinningRank.MATCHES_SIX.winningAmount / LottoFactory.PRICE_OF_ONE_LOTTO)
+                Arguments.of(lottosEarningZero(), 0, 0),
+                Arguments.of(lottosEarningMax(), WinningRank.MATCHES_SIX.winningAmount / LottoFactory.PRICE_OF_ONE_LOTTO, 1)
         );
     }
 
