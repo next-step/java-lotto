a059fcaff078fbd7136da231cf4cbcb760840278
siyoon <siyoon210@gmail.com>
2020-11-08 12:05:31 +0900

    test: 테스트케이스 추가

--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LottoFactoryTest {
     private LottoFactory lottoFactory;
@@ -17,9 +16,15 @@ public class LottoFactoryTest {
     }
 
     @Test
-    @DisplayName("구입 금액으로 구매 할 수 있는 만큼 로또를 구입한다.")
+    @DisplayName("주어진 금액으로 구매 할 수 있는 만큼 로또를 구입한다.")
     void buyLottoTest() {
-        List<Lotto> lottos = lottoFactory.buyLottos(10000);
-        assertThat(lottos.size()).isEqualTo(10);
+        assertAll(
+                () -> assertThat(lottoFactory.buyLottos(-1).size()).isEqualTo(0),
+                () -> assertThat(lottoFactory.buyLottos(0).size()).isEqualTo(0),
+                () -> assertThat(lottoFactory.buyLottos(999).size()).isEqualTo(0),
+                () -> assertThat(lottoFactory.buyLottos(1000).size()).isEqualTo(1),
+                () -> assertThat(lottoFactory.buyLottos(10_000).size()).isEqualTo(10),
+                () -> assertThat(lottoFactory.buyLottos(10_100).size()).isEqualTo(10)
+        );
     }
 }
