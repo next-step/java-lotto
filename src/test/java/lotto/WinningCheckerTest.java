26774e3e6ad6cc457468d7460020f7f4080654f1
siyoon <siyoon210@gmail.com>
2020-11-08 15:25:45 +0900

    test: @ParameterizedTest로 변경

--- src/test/java/lotto/WinningCheckerTest.java
+++ src/test/java/lotto/WinningCheckerTest.java
@@ -1,32 +1,53 @@
 package lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningCheckerTest {
-    @Test
+    private WinningChecker winningChecker;
+
+    @BeforeEach
+    void init() {
+        winningChecker = new WinningChecker();
+    }
+
+    @ParameterizedTest
+    @MethodSource("boughtLottos")
     @DisplayName("구매한 로또들과 지난 주 당첨 번호로, 등수별 당첨 여부와 수익이 얼마나 되는지 계산한다.")
-    void winningAndStatisticTest() {
-        List<Lotto> boughtLottos = new ArrayList<>();
-        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 7, 8, 9, 10)));
+    void winningAndStatisticTest(List<Lotto> boughtLottos, double expectedEarningRate) {
         List<Integer> winningNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
-        WinningChecker winningChecker = new WinningChecker();
         WinningStatistic winningStatistic = winningChecker.getResult(winningNumbers, boughtLottos);
 
-        assertThat(winningStatistic.getEarningsRate()).isEqualTo(0);
+        assertThat(winningStatistic.getEarningsRate()).isEqualTo(expectedEarningRate);
+    }
 
-        List<Lotto> boughtLottos2 = new ArrayList<>();
-        boughtLottos2.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    private static Stream<Arguments> boughtLottos() {
+        return Stream.of(
+                Arguments.of(lottosEarningZero(), 0),
+                Arguments.of(lottosEarningMax(), WinningRank.MATCHES_SIX.winningAmount / LottoFactory.PRICE_OF_ONE_LOTTO)
+        );
+    }
 
-        WinningStatistic winningStatistic2 = winningChecker.getResult(winningNumbers, boughtLottos2);
+    private static List<Lotto> lottosEarningZero() {
+        List<Lotto> boughtLottos = new ArrayList<>();
+        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 7, 8, 9, 10)));
+        return boughtLottos;
+    }
 
-        assertThat(winningStatistic2.getEarningsRate()).isEqualTo((double) WinningRank.MATCHES_SIX.winningAmount / LottoFactory.PRICE_OF_ONE_LOTTO);
+    private static List<Lotto> lottosEarningMax() {
+        List<Lotto> boughtLottos = new ArrayList<>();
+        boughtLottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        return boughtLottos;
     }
 }
