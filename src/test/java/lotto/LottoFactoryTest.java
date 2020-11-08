947978722e4a21a4599ecfc6c439a3e7c2951550
siyoon <siyoon210@gmail.com>
2020-11-08 11:50:56 +0900

    test: 구입 금액으로 구매 할 수 있는 만큼 로또를 구입한다.
    ex 로또하나의 금액이 1,000원이고 10,000원이 구매금액이라면 10개의 로또를 구입한다.

--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -1,6 +1,12 @@
 package lotto;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoFactoryTest {
     private LottoFactory lottoFactory;
@@ -8,5 +14,12 @@ public class LottoFactoryTest {
     @BeforeEach
     void init() {
         lottoFactory = new LottoFactory();
+    }
+
+    @Test
+    @DisplayName("구입 금액으로 구매 할 수 있는 만큼 로또를 구입한다.")
+    void buyLottoTest() {
+        List<Lotto> lottos = lottoFactory.buyLotto(10000);
+        assertThat(lottos.size()).isEqualTo(10);
     }
 }
