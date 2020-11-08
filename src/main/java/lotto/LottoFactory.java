3ff5a1ae726df2a856eec0bda1531ca8e29390f2
siyoon <siyoon210@gmail.com>
2020-11-08 11:52:49 +0900

    feat: 주어진 금액으로 구매 할 수 있는 만큼 로또를 구입한다.

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -1,4 +1,16 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoFactory {
+    public List<Lotto> buyLotto(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (; amount >= 1000; amount -= 1000) {
+            lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        }
+
+        return lottos;
+    }
 }
