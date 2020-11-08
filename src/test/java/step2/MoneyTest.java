c5ae389cccb169bf8522de0f8b812d90f3ed1aa8
karian7(김광수) <karian7@gmail.com>
2020-11-08 23:25:09 +0900

    test: Money 테스트

---
+++ src/test/java/step2/MoneyTest.java
@@ -0,0 +1,27 @@
+package step2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoneyTest {
+    @Test
+    void lessThen() {
+        assertThat(Money.of(100).lessThan(Money.of(200))).isTrue();
+    }
+
+    @Test
+    void divide() {
+        assertThat(Money.of(1000).divide(Money.of(100))).isEqualTo(10);
+    }
+
+    @Test
+    void multiply() {
+        assertThat(Money.of(1000).multiply(10)).isEqualTo(Money.of(10000));
+    }
+
+    @Test
+    void subtract() {
+        assertThat(Money.of(1000).subtract(Money.of(100))).isEqualTo(Money.of(900));
+    }
+}
