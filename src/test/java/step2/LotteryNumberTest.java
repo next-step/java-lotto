5486c220e1e176a8f1a51917486fa9cea2ba1160
karian7(김광수) <karian7@gmail.com>
2020-11-09 20:24:42 +0900

    test: 여러 케이스에 대한 테스트 추가

--- src/test/java/step2/LotteryNumberTest.java
+++ src/test/java/step2/LotteryNumberTest.java
@@ -2,11 +2,15 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,9 +24,24 @@ public class LotteryNumberTest {
     }
 
     @DisplayName("주어진 숫자셋과 교집합의 갯수를 구할 수 있다.")
-    @Test
-    void intersection() {
-        assertThat(LotteryNumber.of(1, 2, 3, 4, 5, 6).getIntersection(LotteryNumber.of(1, 2, 3, 4, 5, 6))).isEqualTo(6);
+    @ParameterizedTest
+    @MethodSource("provideIntersectionTest")
+    void intersection(LotteryNumber source, LotteryNumber target, int matched) {
+        assertThat(source.getIntersection(target)).isEqualTo(matched);
+    }
+
+    private static Stream<Arguments> provideIntersectionTest() {
+        //@formatter:off
+        return Stream.of(
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(1, 2, 3, 4, 5, 6),        6),
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(11, 2, 3, 4, 5, 6),       5),
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(11, 12, 3, 4, 5, 6),      4),
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(11, 12, 13, 4, 5, 6),     3),
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(11, 12, 13, 14, 5, 6),    2),
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(11, 12, 13, 14, 15, 6),   1),
+                Arguments.of(LotteryNumber.of(1, 2, 3, 4, 5, 6), LotteryNumber.of(11, 12, 13, 14, 15, 16),  0)
+        );
+        //@formatter:on
     }
 
     static class LotteryNumber {
