5f94db1074f65efa1459e2878d3cf8ff8c824ae0
karian7(김광수) <karian7@gmail.com>
2020-11-09 19:31:35 +0900

    feat: make it pass

--- src/test/java/step2/WinningNumberTest.java
+++ src/test/java/step2/WinningNumberTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
@@ -12,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static step2.LotteryAgentTest.LotteryTickets;
 import static step2.LotteryNumberTest.LotteryNumber;
+import static step2.PlayslipTest.Playslip.NUMBER_POOL;
 
 
 public class WinningNumberTest {
@@ -42,19 +45,42 @@ public class WinningNumberTest {
     }
 
     private static class WinningNumber {
+        private final NumberSelection numberSelection;
+        private LotteryNumber lotteryNumber;
+
         public WinningNumber(NumberSelection numberSelection) {
+            this.numberSelection = numberSelection;
         }
 
-        public void match(LotteryTickets tickets) {
-            throw new IllegalStateException();
+        public LotteryResult match(LotteryTickets tickets) {
+            if (lotteryNumber == null) {
+                throw new IllegalStateException();
+            }
+            return new LotteryResult();
+        }
+
+        public void draw() {
+            lotteryNumber = numberSelection.select(NUMBER_POOL);
+        }
+
+        private static class LotteryResult {
         }
     }
 
     private interface NumberSelection {
+        LotteryNumber select(List<Integer> numberPool);
     }
 
     private static class TestingNumberSelection implements NumberSelection {
-        public TestingNumberSelection(int... numbers) {
+        private final Integer[] numbers;
+
+        public TestingNumberSelection(Integer... numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public LotteryNumber select(List<Integer> numberPool) {
+            return new LotteryNumber(new HashSet<>(Arrays.asList(numbers)));
         }
     }
 }
