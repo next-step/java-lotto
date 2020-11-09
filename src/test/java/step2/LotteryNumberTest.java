941cdee6779ee629238f49c9910592a7ab2efb31
karian7(김광수) <karian7@gmail.com>
2020-11-09 20:17:22 +0900

    feat: make it pass

--- src/test/java/step2/LotteryNumberTest.java
+++ src/test/java/step2/LotteryNumberTest.java
@@ -1,6 +1,5 @@
 package step2;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +8,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LotteryNumberTest {
     @DisplayName("6개의 번호를 제공해야 한다")
@@ -23,7 +22,7 @@ public class LotteryNumberTest {
     @DisplayName("주어진 숫자셋과 교집합의 갯수를 구할 수 있다.")
     @Test
     void intersection() {
-        assertThat(LotteryNumber.of(1, 2, 3, 4, 5, 6).getIntersection(LotteryNumber.of(1, 2, 3, 4, 5, 6))).isEqualTo(0);
+        assertThat(LotteryNumber.of(1, 2, 3, 4, 5, 6).getIntersection(LotteryNumber.of(1, 2, 3, 4, 5, 6))).isEqualTo(6);
     }
 
     static class LotteryNumber {
@@ -42,6 +41,12 @@ public class LotteryNumberTest {
 
         public Set<Integer> getNumbers() {
             return numbers;
+        }
+
+        public int getIntersection(LotteryNumber other) {
+            Set<Integer> intersection = new HashSet<>(numbers);
+            intersection.retainAll(other.numbers);
+            return intersection.size();
         }
     }
 
