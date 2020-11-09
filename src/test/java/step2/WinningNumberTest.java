26516f0322c99bea07cf146256b28911563354e6
karian7(김광수) <karian7@gmail.com>
2020-11-09 19:19:18 +0900

    test: draw 하기 전에 match 할 수 없다

--- src/test/java/step2/WinningNumberTest.java
+++ src/test/java/step2/WinningNumberTest.java
@@ -4,12 +4,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
+
 public class WinningNumberTest {
     @DisplayName("번호 선택방법을 제공받는다.")
     @Test
     void constructWithNumberSelection() {
-        Assertions.assertThatCode(() -> new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6))) //
+        assertThatCode(() -> new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6))) //
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("draw 하기 전에 match 할 수 없다.")
+    @Test
+    void matchWithLotteryTicket() {
+        WinningNumber winningNumber = new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> winningNumber.match(makeLotteryTickets(1, 2, 3, 4, 5, 6))) //
+                .isInstanceOf(IllegalStateException.class)
     }
 
     private static class WinningNumber {
