81700059fd7af30921f5b8a3106bced69350f45c
karian7(김광수) <karian7@gmail.com>
2020-11-09 19:26:41 +0900

    test: match 결과를 받을 수 있다

--- src/test/java/step2/WinningNumberTest.java
+++ src/test/java/step2/WinningNumberTest.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static step2.LotteryAgentTest.LotteryTickets;
 import static step2.LotteryNumberTest.LotteryNumber;
@@ -24,9 +25,16 @@ public class WinningNumberTest {
 
     @DisplayName("draw 하기 전에 match 할 수 없다.")
     @Test
-    void matchWithLotteryTicket() {
+    void cannotMatchWithoutDraw() {
         assertThatThrownBy(() -> winningNumber.match(makeLotteryTickets(1, 2, 3, 4, 5, 6))) //
                 .isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("match 결과를 받을 수 있다.")
+    @Test
+    void matchResult() {
+        winningNumber.draw();
+        assertThat(winningNumber.match(makeLotteryTickets(1, 2, 3, 4, 5, 6))).isNotNull();
     }
 
     private LotteryTickets makeLotteryTickets(Integer... numbers) {
