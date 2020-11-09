8decba96025b01ed05efdf71c8c438bce372a898
karian7(김광수) <karian7@gmail.com>
2020-11-09 19:02:51 +0900

    feat: make it pass

--- src/test/java/step2/WinningNumberTest.java
+++ src/test/java/step2/WinningNumberTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WinningNumberTest {
-    @DisplayName("당첨번호를 제공받는다.")
+    @DisplayName("번호 선택방법을 제공받는다.")
     @Test
     void constructWithNumberSelection() {
-        Assertions.assertThatCode(() -> {
-            new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6));
-        }).doesNotThrowAnyException();
+        Assertions.assertThatCode(() -> new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6))) //
+                .doesNotThrowAnyException();
+    }
+
+    private static class WinningNumber {
+        public WinningNumber(NumberSelection numberSelection) {
+        }
+    }
+
+    private interface NumberSelection {
+    }
+
+    private static class TestingNumberSelection implements NumberSelection {
+        public TestingNumberSelection(int... numbers) {
+        }
     }
 }
