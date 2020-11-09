54a99efe913f0809ffe5801d8dd95cb606046590
홍성민 <honggasm@gmail.com>
2020-11-09 22:07:23 +0900

    feat: StringAddCalculator 의 각 기능별 실패하는 테스트 코드 작성

---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,7 @@
+package step1;
+
+public class StringAddCalculator {
+    public static int splitAndSum(final String expression) {
+        return -1;
+    }
+}
---
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -0,0 +1,73 @@
+package step1;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class StringAddCalculatorTest {
+    @DisplayName("문자열이 null인 경우")
+    @Test
+    public void splitAndSum_null() {
+        final int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("문자열이 blank인 경우")
+    @Test
+    public void splitAndSum_blank() {
+        final int result = StringAddCalculator.splitAndSum("");
+        AssertionsForClassTypes.assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("문자열에 숫자가 하나인 경우")
+    @Test
+    public void splitAndSum_one_number() {
+        final int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("쉼표 구분자")
+    @Test
+    public void splitAndSum_comma_delimiter() {
+        final int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("콜론 구분자")
+    @Test
+    public void splitAndSum_colon_delimiter() {
+        final int result = StringAddCalculator.splitAndSum("1:2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("쉼표, 콜론 구분자")
+    @Test
+    public void splitAndSum_comma_and_colon_delimiter() {
+        final int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("커스텀 구분자")
+    @Test
+    public void splitAndSum_custom_delimiter() {
+        final int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("문자열에 음수 존재")
+    @Test
+    public void splitAndSum_negative() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("문자열에 숫자가 아닌 문자 존재")
+    @Test
+    public void splitAndSum_not_number() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("홍,성,민"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
