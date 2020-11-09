84bb6c2cb6c09661bd20fbb4d6cb190c2b6a6c05
catsbi <hansolsolsol@mosition.co.kr>
2020-11-09 16:56:28 +0900

    [feat] 로또 번호 생성 매직넘버 분리

--- src/main/java/step2/strategy/LottoNumberMakeStrategy.java
+++ src/main/java/step2/strategy/LottoNumberMakeStrategy.java
@@ -8,12 +8,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoNumberMakeStrategy implements NumberMakeStrategy{
+    public static final int START_INCLUSIVE = 1;
+    public static final int LOTTO_START_INDEX = 0;
+    public static final int LOTTO_END_INDEX = 6;
+
     @Override
     public List<Integer> create() {
-        List<Integer> lottoRange = IntStream.range(0, Constant.LOTTO_RANGE_MAX)
+        List<Integer> lottoRange = IntStream.range(START_INCLUSIVE, Constant.LOTTO_RANGE_MAX)
                 .boxed()
                 .collect(Collectors.toList());
         Collections.shuffle(lottoRange);
-        return lottoRange.subList(0, 6);
+        return lottoRange.subList(LOTTO_START_INDEX, LOTTO_END_INDEX);
     }
 }
