1733c208781612e283e624d2ff5b8a555dd96cfc
karian7(김광수) <karian7@gmail.com>
2020-11-09 19:24:23 +0900

    refactor: 불필요한 테스트 제거

--- src/test/java/step2/WinningNumberTest.java
+++ src/test/java/step2/WinningNumberTest.java
@@ -1,5 +1,6 @@
 package step2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,25 +8,23 @@ import java.util.HashSet;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static step2.LotteryAgentTest.LotteryTickets;
 import static step2.LotteryNumberTest.LotteryNumber;
 
 
 public class WinningNumberTest {
-    @DisplayName("번호 선택방법을 제공받는다.")
-    @Test
-    void constructWithNumberSelection() {
-        assertThatCode(() -> new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6))) //
-                .doesNotThrowAnyException();
+
+    private WinningNumber winningNumber;
+
+    @BeforeEach
+    void setUp() {
+        winningNumber = new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6));
     }
 
     @DisplayName("draw 하기 전에 match 할 수 없다.")
     @Test
     void matchWithLotteryTicket() {
-        WinningNumber winningNumber = new WinningNumber(new TestingNumberSelection(1, 2, 3, 4, 5, 6));
-
         assertThatThrownBy(() -> winningNumber.match(makeLotteryTickets(1, 2, 3, 4, 5, 6))) //
                 .isInstanceOf(IllegalStateException.class);
     }
