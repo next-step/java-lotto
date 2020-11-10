d326ce1dcb559f6ca0f573c419f1dc3562e3ca5c
siyoon <siyoon210@gmail.com>
2020-11-10 21:19:11 +0900

    refactor: validate 삭제 (Lotto 객체 생성시에 유효성 검사)

--- src/main/java/lotto/view/inputview/ConsoleInputView.java
+++ src/main/java/lotto/view/inputview/ConsoleInputView.java
@@ -1,8 +1,5 @@
 package lotto.view.inputview;
 
-import lotto.domain.Lotto;
-import lotto.domain.LottoNumber;
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
@@ -30,29 +27,8 @@ public class ConsoleInputView implements InputView {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String winningNumbersInput = scanner.next();
 
-        Set<Integer> winningNumbers = Arrays.stream(winningNumbersInput.split(DELIMITER))
+        return Arrays.stream(winningNumbersInput.split(DELIMITER))
                 .map(Integer::valueOf)
                 .collect(Collectors.toCollection(TreeSet::new));
-
-        validate(winningNumbers);
-
-        return winningNumbers;
-    }
-
-    private void validate(Set<Integer> winningNumbers) {
-        validateNumberSize(winningNumbers);
-        winningNumbers.forEach(this::validateNumberRange);
-    }
-
-    private void validateNumberSize(Set<Integer> winningNumbers) {
-        if (winningNumbers.size() != Lotto.VALID_NUMBERS_SIZE) {
-            throw new IllegalArgumentException(INVALID_NUMBERS_SIZE_ERR_MSG);
-        }
-    }
-
-    private void validateNumberRange(Integer number) {
-        if (number < LottoNumber.VALID_MIN_NUMBER || number > LottoNumber.VALID_MAX_NUMBER) {
-            throw new IllegalStateException(LottoNumber.INVALID_NUMBER_RANGE_ERR_MSG);
-        }
     }
 }
