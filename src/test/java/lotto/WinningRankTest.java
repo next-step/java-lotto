3b75b08607b303167f4a28795fcda3057dcd274e
siyoon <siyoon210@gmail.com>
2020-11-10 21:52:29 +0900

    test: 테스트코드 통합

--- src/test/java/lotto/WinningRankTest.java
+++ src/test/java/lotto/WinningRankTest.java
@@ -2,11 +2,11 @@ package lotto;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
-import lotto.domain.WinningRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static lotto.domain.WinningRank.MATCHES_FIVE;
+import static lotto.domain.WinningRank.MATCHES_FIVE_AND_BONUS_NUMBER;
 import static lotto.domain.WinningRank.MATCHES_FOUR;
 import static lotto.domain.WinningRank.MATCHES_SIX;
 import static lotto.domain.WinningRank.MATCHES_THREE;
@@ -20,29 +20,21 @@ public class WinningRankTest {
     @DisplayName("당첨번호와 구입한 로또번호로 당첨타입을 확인한다.")
     void winningRankTest() {
         Lotto winningLotto = new Lotto(1, 2, 3, 4, 5, 6);
+        LottoNumber bonusNumber = LottoNumber.from(45);
 
         assertAll(
-                () -> assertThat(getWinningRank(winningLotto, new Lotto(1, 2, 3, 4, 5, 6)))
+                () -> assertThat(getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 3, 4, 5, 6)))
                         .isEqualTo(MATCHES_SIX),
-                () -> assertThat(getWinningRank(winningLotto, new Lotto(1, 2, 3, 4, 5, 7)))
+                () -> assertThat(getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 3, 4, 5, 45)))
+                        .isEqualTo(MATCHES_FIVE_AND_BONUS_NUMBER),
+                () -> assertThat(getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 3, 4, 5, 7)))
                         .isEqualTo(MATCHES_FIVE),
-                () -> assertThat(getWinningRank(winningLotto, new Lotto(1, 2, 3, 4, 7, 8)))
+                () -> assertThat(getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 3, 4, 7, 8)))
                         .isEqualTo(MATCHES_FOUR),
-                () -> assertThat(getWinningRank(winningLotto, new Lotto(1, 2, 3, 7, 8, 9)))
+                () -> assertThat(getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 3, 7, 8, 9)))
                         .isEqualTo(MATCHES_THREE),
-                () -> assertThat(getWinningRank(winningLotto, new Lotto(1, 2, 7, 8, 9, 10)))
+                () -> assertThat(getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 7, 8, 9, 10)))
                         .isEqualTo(NONE)
         );
-    }
-
-    @Test
-    @DisplayName("숫자 5개가 일치하고, 보너스 번호를 맞춘경우 당첨타입")
-    void bonusNumberRankTest() {
-        Lotto winningLotto = new Lotto(1, 2, 3, 4, 5, 6);
-        LottoNumber bonusNumber = LottoNumber.from(7);
-
-        WinningRank winningRank = getWinningRank(winningLotto, bonusNumber, new Lotto(1, 2, 3, 4, 5, 6));
-
-        assertThat(winningRank).isEqualTo(MATCHES_FIVE_AND_BONUS_NUMBER);
     }
 }
