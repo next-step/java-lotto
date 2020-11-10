9befb9b867caed2e5cf0929e2e0272f0610f1e76
siyoon <siyoon210@gmail.com>
2020-11-10 22:01:55 +0900

    refactor: 보너스 번호 입력 에러메세지 상수화

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class LottoGame {
     public static final String INVALID_AMOUNT_ERR_MSG = "amount의 값은 음수가 될 수 없습니다.";
+    public static final String INVALID_BONUS_NUMBER_ERR_MSG = "보너스번호는 당첨번호와 같을 수 없습니다.";
 
     private final View view;
     private final LottoService lottoService;
@@ -28,7 +29,7 @@ public class LottoGame {
         Lotto winningLotto = getWinningLotto();
         LottoNumber bonusNumber = LottoNumber.from(view.getBonusNumber());
         if (winningLotto.contains(bonusNumber)) {
-            throw new IllegalStateException("보너스번호는 당첨번호와 같을 수 없습니다.");
+            throw new IllegalStateException(INVALID_BONUS_NUMBER_ERR_MSG);
         }
         printStatistic(winningLotto, bonusNumber, boughtLottos);
     }
