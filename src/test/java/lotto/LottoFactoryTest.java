fc3f57319fa8dab37183cfd7ff5d0e6eee585f29
siyoon <siyoon210@gmail.com>
2020-11-10 09:16:47 +0900

    test: 숫자 표현 통일

--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -15,7 +15,7 @@ public class LottoFactoryTest {
                 () -> assertThat(LottoFactory.buyLottos(-1).size()).isEqualTo(0),
                 () -> assertThat(LottoFactory.buyLottos(0).size()).isEqualTo(0),
                 () -> assertThat(LottoFactory.buyLottos(999).size()).isEqualTo(0),
-                () -> assertThat(LottoFactory.buyLottos(1000).size()).isEqualTo(1),
+                () -> assertThat(LottoFactory.buyLottos(1_000).size()).isEqualTo(1),
                 () -> assertThat(LottoFactory.buyLottos(10_000).size()).isEqualTo(10),
                 () -> assertThat(LottoFactory.buyLottos(10_100).size()).isEqualTo(10)
         );
