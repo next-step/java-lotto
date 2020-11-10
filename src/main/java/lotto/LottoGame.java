772a573953c0e768955ebafc420d565b4ad7758b
siyoon <siyoon210@gmail.com>
2020-11-10 19:22:52 +0900

    feat: amount가 음수인경우 유효성검사 추가 (+ 메서드추출)

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -8,6 +8,8 @@ import lotto.view.View;
 import java.util.List;
 
 public class LottoGame {
+    public static final String INVALID_AMOUNT_ERR_MSG = "amount의 값은 음수가 될 수 없습니다.";
+
     private final View view;
     private final LottoService lottoService;
 
@@ -17,14 +19,38 @@ public class LottoGame {
     }
 
     public void start() {
-        int amount = view.getAmount();
-        List<Lotto> boughtLottos = lottoService.buyLottos(amount);
-        view.printBoughtLottos(boughtLottos);
+        int amount = getAmount();
+        List<Lotto> boughtLottos = buyLottos(amount);
         if (boughtLottos.size() == 0) {
             return;
         }
+        Lotto winningLotto = getWinningLotto();
+        printStatistic(boughtLottos, winningLotto);
+    }
 
-        Lotto winningLotto = new Lotto(view.getWinningNumbers());
+    private int getAmount() {
+        int amount = view.getAmount();
+        validateAmount(amount);
+        return amount;
+    }
+
+    private void validateAmount(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_ERR_MSG);
+        }
+    }
+
+    private List<Lotto> buyLottos(int amount) {
+        List<Lotto> boughtLottos = lottoService.buyLottos(amount);
+        view.printBoughtLottos(boughtLottos);
+        return boughtLottos;
+    }
+
+    private Lotto getWinningLotto() {
+        return new Lotto(view.getWinningNumbers());
+    }
+
+    private void printStatistic(List<Lotto> boughtLottos, Lotto winningLotto) {
         WinningStatistic winningStatistic = lottoService.getResult(winningLotto, boughtLottos);
         view.printWinningStatistic(winningStatistic);
     }
