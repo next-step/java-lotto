364654f74c9d71e60a8f763585cc2d0ced0972cc
oen142 <oen142@naver.com>
2020-11-11 21:25:09 +0900

    refact : 매직넘버 도출

--- src/main/java/step1/domain/StringAddCalculator.java
+++ src/main/java/step1/domain/StringAddCalculator.java
@@ -6,6 +6,9 @@ import java.util.regex.Pattern;
 import static java.lang.Integer.parseInt;
 
 public class StringAddCalculator {
+    private static final int CALCULATOR_OBJECT = 2;
+    private static final int CALCULATOR_DELIMITER = 1;
+    private static final String NO_PATTERN_DELIMITER = "[:,]";
     private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String input) {
@@ -16,13 +19,17 @@ public class StringAddCalculator {
     }
 
     private static String[] getNumbers(String input) {
-        Matcher m = pattern.matcher(input);
+        Matcher m = getMatcher(input);
         if (m.find()) {
-            return m.group(2).split(m.group(1));
+            return m.group(CALCULATOR_OBJECT).split(m.group(CALCULATOR_DELIMITER));
 
         }
-        return input.split("[:,]");
+        return input.split(NO_PATTERN_DELIMITER);
 
+    }
+
+    private static Matcher getMatcher(String input) {
+        return pattern.matcher(input);
     }
 
     private static boolean isEmpty(String input) {
