35c4b5e9081119db13365686888c2753d2dff5f1
oen142 <oen142@naver.com>
2020-11-11 23:15:04 +0900

    test : 로또 1급컬렉션 Lottos 테스트 작성

--- src/test/java/step2/domain/LottoTest.java
+++ src/test/java/step2/domain/LottoTest.java
@@ -1,12 +1,9 @@
 package step2.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
 
