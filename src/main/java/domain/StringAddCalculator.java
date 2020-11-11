d24cce381cbce61df699be43aa28b539eb75559f
oen142 <oen142@naver.com>
2020-11-11 21:07:40 +0900

    refact : 인풋값이 널이거나 비어있을 경우 0 반환

--- src/main/java/domain/StringAddCalculator.java
+++ src/main/java/domain/StringAddCalculator.java
@@ -8,10 +8,7 @@ import static java.lang.Integer.parseInt;
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
-        if (input == null) {
-            return 0;
-        }
-        if (input.isEmpty()) {
+        if (isNull(input)||isEmpty(input)) {
             return 0;
         }
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
@@ -20,30 +17,31 @@ public class StringAddCalculator {
             String group = m.group(1);
             String group2 = m.group(2);
             String[] split = group2.split(group);
-
-            int sum = 0;
-            for (String s : split) {
-                int i = parseInt(s);
-                if (i < 0) {
-                    throw new RuntimeException("익셉션");
-                }
-                sum += i;
-            }
-            return sum;
+            return sumStringArray(split);
         } else {
-
             String[] split = input.split(":|,");
-
-            int sum = 0;
-            for (String s : split) {
-                int i = parseInt(s);
-                if (i < 0) {
-                    throw new RuntimeException("익셉션");
-                }
-                sum += i;
-            }
-            return sum;
+            return sumStringArray(split);
         }
 
+    }
+
+    private static boolean isEmpty(String input) {
+        return input.isEmpty();
+    }
+
+    private static boolean isNull(String input) {
+        return input == null;
+    }
+
+    private static int sumStringArray(String[] split) {
+        int sum = 0;
+        for (String s : split) {
+            int i = parseInt(s);
+            if (i < 0) {
+                throw new RuntimeException("익셉션");
+            }
+            sum += i;
+        }
+        return sum;
     }
 }
