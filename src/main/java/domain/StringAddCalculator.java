50427f5ddaa7f19866346f8a39ea9dc6d71a1d83
oen142 <oen142@naver.com>
2020-11-11 21:03:13 +0900

    feat : 모든 테스트 케이스 통과 완료

--- src/main/java/domain/StringAddCalculator.java
+++ src/main/java/domain/StringAddCalculator.java
@@ -1,22 +1,49 @@
 package domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static java.lang.Integer.parseInt;
 
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
-        if(input == null){
+        if (input == null) {
             return 0;
         }
-        if(input.isEmpty()){
+        if (input.isEmpty()) {
             return 0;
         }
-        String[] split = input.split(",|:");
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        boolean b = m.find();
+        if (b) {
+            String group = m.group(1);
+            String group2 = m.group(2);
+            String[] split = group2.split(group);
 
-        int sum = 0;
-        for (String s : split) {
-            sum += parseInt(s);
+            int sum = 0;
+            for (String s : split) {
+                int i = parseInt(s);
+                if (i < 0) {
+                    throw new RuntimeException("익셉션");
+                }
+                sum += i;
+            }
+            return sum;
+        } else {
+
+            String[] split = input.split(":|,");
+
+            int sum = 0;
+            for (String s : split) {
+                int i = parseInt(s);
+                if (i < 0) {
+                    throw new RuntimeException("익셉션");
+                }
+                sum += i;
+            }
+            return sum;
         }
-        return sum;
+
     }
 }
