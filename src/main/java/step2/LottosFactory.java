c550ec9944dec08eeada5ca7316be530d25fc4cd
홍성민 <honggasm@gmail.com>
2020-11-12 00:16:07 +0900

    feat: 로또 생성 요청시 요청 개수에 음수가 전달된 경우 예외 반환

--- src/main/java/step2/LottosFactory.java
+++ src/main/java/step2/LottosFactory.java
@@ -4,8 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static util.Preconditions.checkArgument;
+
 public class LottosFactory {
+    public static final String COUNT_MUST_NOT_BE_NEGATIVE = "count must not be negative";
+    
     public static Lottos create(final int count) {
+        checkArgument(count >= 0, COUNT_MUST_NOT_BE_NEGATIVE);
+        
+        if (count == 0) {
+            return Lottos.EMPTY;
+        }
+
         final List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             final Lotto lotto = Lotto.of(() -> Arrays.asList(1, 2, 3, 4, 5, 6));
