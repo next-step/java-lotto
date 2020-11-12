dfdb9a41410fa8d28fc05f41ad7fd4e9dc81b20e
karian7(김광수) <karian7@gmail.com>
2020-11-12 18:43:54 +0900

    feat: make it pass

--- src/test/java/step2/LotteryControllerTest.java
+++ src/test/java/step2/LotteryControllerTest.java
@@ -3,19 +3,96 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static step2.LotteryAgentTest.LotteryAgent;
+import static step2.LotteryAgentTest.LotteryTickets;
+import static step2.LotteryNumberTest.LotteryNumber;
+import static step2.LotteryResultTest.LotteryResult;
+import static step2.WinningNumberTest.WinningNumber;
+
 public class LotteryControllerTest {
+    private static final List<String> record = new ArrayList<>();
+
     @DisplayName("컨트롤러는 객체들과 상호작용 순서가 있다")
     @Test
     void interactionSequence() {
-        LotteryController lotteryController = new LotteryController();
+        LotteryController lotteryController = new LotteryController(new InputView(), new ResultView());
 
         lotteryController.request();
 
-        verifyInteractionSequence("requestMoney->" +
-                "responseTicketCount->" +
-                "responseTickets->" +
-                "requestLastWeekLotteryNumber->" +
-                "responseWinningStat->" +
-                "responseRateOfReturn");
+        verifyInteractionSequence();
+
+    }
+
+    private void verifyInteractionSequence() {
+        //@formatter:off
+        assertThat(String.join("->", record))
+                .isEqualTo("requestMoney->" +
+                        "responseTicketCount->" +
+                        "responseTickets->" +
+                        "requestLastWeekLotteryNumber->" +
+                        "responseWinningStat->" +
+                        "responseRateOfReturn");
+        //@formatter:on
+    }
+
+    private static class LotteryController {
+        private final InputView inputView;
+        private final ResultView resultView;
+        private LotteryAgent lotteryAgent;
+
+        public LotteryController(InputView inputView, ResultView resultView) {
+            this.inputView = inputView;
+            this.resultView = resultView;
+            lotteryAgent = new LotteryAgent();
+        }
+
+        public void request() {
+            Money money = inputView.requestMoney();
+            LotteryAgent.ExchangeResult exchangeResult = lotteryAgent.exchange(money);
+            resultView.responseTicketCount(exchangeResult.getLotteryTickets());
+            resultView.responseTickets(exchangeResult.getLotteryTickets());
+            LotteryNumber lastWeekLotteryNumber = inputView.requestLastWeekLotteryNumber();
+            LotteryResult lotteryResult = newWinningNumber(lastWeekLotteryNumber).match(exchangeResult.getLotteryTickets());
+            resultView.responseWinningStat(lotteryResult);
+            resultView.responseRateOfReturn(lotteryResult);
+        }
+
+        private WinningNumber newWinningNumber(LotteryNumber lotteryNumber) {
+            return new WinningNumber(lotteryNumber);
+        }
+    }
+
+    static class InputView {
+        public Money requestMoney() {
+            record.add("requestMoney");
+            return Money.of(1000);
+        }
+
+        public LotteryNumber requestLastWeekLotteryNumber() {
+            record.add("requestLastWeekLotteryNumber");
+            return LotteryNumber.of(1, 2, 3, 4, 5, 6);
+        }
+    }
+
+    static class ResultView {
+        public void responseTicketCount(LotteryTickets lotteryTickets) {
+            record.add("responseTicketCount");
+        }
+
+        public void responseTickets(LotteryTickets lotteryTickets) {
+            record.add("responseTickets");
+        }
+
+        public void responseWinningStat(LotteryResult lotteryResult) {
+            record.add("responseWinningStat");
+        }
+
+        public void responseRateOfReturn(LotteryResult lotteryResult) {
+            record.add("responseRateOfReturn");
+        }
     }
 }
