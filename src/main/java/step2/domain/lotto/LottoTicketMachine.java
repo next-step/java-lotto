70dfb60f3844d11bb1b3c9a57577e6e1a289074e
catsbi <hansolsolsol@mosition.co.kr>
2020-11-12 10:51:05 +0900

    [refactor] LottoTicketMachine 유틸 클래스로 변경

--- src/main/java/step2/domain/lotto/LottoTicketMachine.java
+++ src/main/java/step2/domain/lotto/LottoTicketMachine.java
@@ -7,19 +7,19 @@ import java.util.function.BiFunction;
 
 public class LottoTicketMachine {
 
-    private final BiFunction<Double, Integer, Integer> division = (i, j) -> (int) (i / j);
+    private static final BiFunction<Double, Integer, Integer> division = (i, j) -> (int) (i / j);
 
-    public LottoTickets ticketing(int inputPrice, NumberMakeStrategy strategy) {
+    public static LottoTickets ticketing(int inputPrice, NumberMakeStrategy strategy) {
         int count = countAllowTicket(inputPrice);
         return new LottoTickets(count, strategy);
     }
 
-    public int countAllowTicket(double inputPrice) {
+    public static int countAllowTicket(double inputPrice) {
         isValidPrice(inputPrice);
         return division.apply(inputPrice, Constant.LOTTO_PRICE);
     }
 
-    private void isValidPrice(double inputPrice) {
+    private static void isValidPrice(double inputPrice) {
         if (inputPrice < Constant.LOTTO_PRICE) {
             throw new IllegalArgumentException(Constant.ERROR_NOT_ENOUGH_MONEY);
         }
