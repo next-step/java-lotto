70f9dca5c0dbfaf2f138d13626f2a69b1cd1e6ed
siyoon <siyoon210@gmail.com>
2020-11-12 18:10:41 +0900

    test: 테스트 메서드명 수정, DisplayName 추가

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -4,6 +4,7 @@ import lotto.exception.IllegalInputAmountException;
 import lotto.mock.MockInvalidAmountView;
 import lotto.service.LottoService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -17,7 +18,8 @@ public class LottoGameTest {
     }
 
     @Test
-    void test() {
+    @DisplayName("amount의 음수가 입력된 경우 throw IllegalInputAmountException")
+    void negativeAmountInputTest() {
         assertThatExceptionOfType(IllegalInputAmountException.class)
                 .isThrownBy(() -> lottoGame.start())
                 .withMessage(LottoGame.INVALID_AMOUNT_ERR_MSG);
