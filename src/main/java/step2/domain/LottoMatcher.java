bc07576c7c9ce03c2a39a7f6e30ba4fc83043907
oen142 <oen142@naver.com>
2020-11-13 21:27:33 +0900

    refact : LottoMatcher 메소드 분리

--- src/main/java/step2/domain/LottoMatcher.java
+++ src/main/java/step2/domain/LottoMatcher.java
@@ -1,27 +1,44 @@
 package step2.domain;
 
+import step2.exception.ValidEmptyException;
+import step2.exception.ValidNullException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoMatcher {
+    private static final String DELIMITER_LOTTO = ",";
     private final List<Integer> lottoNumbers;
 
     public LottoMatcher(String input) {
 
-        if(input == null){
-            throw new IllegalArgumentException("널값을 넣을수는 없습니다.");
-        }
-        if(input.trim().isEmpty()){
-            throw new IllegalArgumentException("빈값을 넣을수는 없습니다.");
-        }
-        String[] split = input.split(",");
-        lottoNumbers = Arrays.stream(split)
+        validEmpty(input);
+        validNull(input);
+
+        lottoNumbers = Arrays.stream(separateLottoNumber(input))
                 .map(s -> Integer.parseInt(s.trim()))
                 .collect(Collectors.toList());
+    }
+
+    private String[] separateLottoNumber(String input) {
+        return input.split(DELIMITER_LOTTO);
+    }
+
+    private void validNull(String input) {
+        if (input == null) {
+            throw new ValidNullException();
+        }
+    }
+
+    private void validEmpty(String input) {
+        if (input.trim().isEmpty()) {
+            throw new ValidEmptyException();
+        }
     }
 
     public List<Integer> getLottoNumbers() {
         return lottoNumbers;
     }
+
 }
