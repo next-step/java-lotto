27dcfb2bc2a05968e0bd7ff237ce300be913e54d
oen142 <oen142@naver.com>
2020-11-13 21:53:34 +0900

    feat : 로또 한장을 만들때 6개가 맞지 않으면 익셉션 발생

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -1,5 +1,7 @@
 package step2.domain;
 
+import step2.exception.LottoCountBoundException;
+
 import java.util.List;
 
 public class Lotto {
@@ -7,8 +9,16 @@ public class Lotto {
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+
+        validLottoCount(lottoNumbers);
+
         this.lottoNumbers = lottoNumbers;
 
+    }
+    private void validLottoCount(List<Integer> lottoNumbers){
+        if(lottoNumbers.size() != 6){
+           throw new LottoCountBoundException();
+        }
     }
     public int lottoNumbersSize() {
         return lottoNumbers.size();
