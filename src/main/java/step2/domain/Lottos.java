3f8f476a7b67c987a5950f2896bfc1936d2d8c16
oen142 <oen142@naver.com>
2020-11-14 00:03:05 +0900

    feat : 로또 일급 컬렉션인 Lottos에게 로또 번호매칭을 한다.

--- src/main/java/step2/domain/Lottos.java
+++ src/main/java/step2/domain/Lottos.java
@@ -1,6 +1,7 @@
 package step2.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lottos {
     private final List<Lotto> lottos;
@@ -12,5 +13,21 @@ public class Lottos {
     public boolean isLottoTicketsCount(int size) {
         return lottos.size() == size;
     }
+    public int getLottoCount(){
+        return lottos.size();
+    }
+    public List<String> lottoNumbersToStringList(){
+        return lottos.stream()
+                .map(Lotto::getLottoNumbersToStringList)
+                .collect(Collectors.toList());
+    }
 
+
+    public LottoMatcher matchLastWeekLotto(List<Integer> lastWeekLottoNums) {
+        LottoMatcher lottoMatcher = new LottoMatcher();
+        lottos.stream()
+                    .map(lotto -> lotto.matchLottoNumbers(lastWeekLottoNums))
+                    .forEach(lottoMatcher::increaseMatchLottoCount);
+        return lottoMatcher;
+    }
 }
