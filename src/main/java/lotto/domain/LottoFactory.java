733e65c56fcfc852a33a7e182671308620e03139
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-13 11:34:46 +0900

    [style]: Fix typo

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -8,19 +8,19 @@ import java.util.List;
 public class LottoFactory {
     private LottoFactory() {}
 
-    public static Lotto createLottoFromIntList(List<Integer> lottoNoArr) {
-        LottoValidator.validateLottoNoIntList(lottoNoArr);
+    public static Lotto createLottoFromIntList(List<Integer> lottoNos) {
+        LottoValidator.validateLottoNoIntList(lottoNos);
         Lotto lotto = new Lotto();
-        for (int no : lottoNoArr) {
+        for (int no : lottoNos) {
             lotto.add(no);
         }
         return lotto;
     }
 
-    public static Lotto createLottoFromStrList(List<String> lottoNoArr) {
-        LottoValidator.validateLottoNoStrList(lottoNoArr);
+    public static Lotto createLottoFromStrList(List<String> lottoNos) {
+        LottoValidator.validateLottoNoStrList(lottoNos);
         Lotto lotto = new Lotto();
-        for (String no : lottoNoArr) {
+        for (String no : lottoNos) {
             lotto.add(no);
         }
         return lotto;
