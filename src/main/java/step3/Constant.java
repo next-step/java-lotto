559106ab1a9809baeda294419853b1d834f64ad6
catsbi <killfine1988@gmail.com>
2020-11-13 14:47:34 +0900

    [feat] 상수 추가

---
+++ src/main/java/step3/Constant.java
@@ -0,0 +1,20 @@
+package step3;
+
+public class Constant {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_RANGE_MAX = 46;
+    public static final int START_INCLUSIVE = 1;
+    public static final int LOTTO_START_INDEX = 0;
+    public static final int LOTTO_END_INDEX = 6;
+    public static final int LOTTO_MARKING_RANGE = 6;
+    public static final int WINNING_NUMBER_ZERO = 0;
+    public static final int TICKET_INIT_COUNT = 0;
+    public static final int LOTTO_BONUS_MATCH_NUMBER = 5;
+
+
+    public static final String ERROR_DUPLICATE_NUMBER = "당첨 번호와 중복되는 보너스볼은 입력할 수 없습니다.";
+    public static final String ERROR_OUT_RANGE_NUMBER = "번호는 [1 ~ 45]를 벗어나선 안됩니다.";
+    public static final String ERROR_NOT_ENOUGH_MONEY = "돈이 충분하지 않습니다.";
+
+    private Constant(){}
+}
