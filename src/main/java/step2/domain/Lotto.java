0e4645b45f3c6eedb156140dba6d3efcec210c7a
oen142 <oen142@naver.com>
2020-11-14 00:00:50 +0900

    feat : 맞춘 로또번호를 찾는다.

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -3,6 +3,7 @@ package step2.domain;
 import step2.exception.LottoCountBoundException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
@@ -12,15 +13,31 @@ public class Lotto {
 
         validLottoCount(lottoNumbers);
 
-        this.lottoNumbers = lottoNumbers;
-
+        this.lottoNumbers = lottoNumbers
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
-    private void validLottoCount(List<Integer> lottoNumbers){
-        if(lottoNumbers.size() != 6){
-           throw new LottoCountBoundException();
+
+    private void validLottoCount(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new LottoCountBoundException();
         }
     }
-    public int lottoNumbersSize() {
-        return lottoNumbers.size();
+
+    public boolean lottoNumbersSize(int size) {
+        return lottoNumbers.size() == size;
+    }
+
+    public int matchLottoNumbers(List<Integer> matchLottoNumbers) {
+        return (int) lottoNumbers.stream()
+                .filter(matchLottoNumbers::contains)
+                .count();
+    }
+
+    public String getLottoNumbersToStringList() {
+        return lottoNumbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
     }
 }
--- src/test/java/step2/domain/LottoMatcherTest.java
+++ src/test/java/step2/domain/LottoMatcherTest.java
@@ -14,15 +14,13 @@ class LottoMatcherTest {
     @Test
     @DisplayName("지난주 당첨번호를 입력합니다.")
     void create() {
-        LottoMatcher lottoMatcher = new LottoMatcher("1 ,2 ,3 ,4 ,5 , 6");
-        assertThat(lottoMatcher.getLottoNumbers()).hasSize(6);
-        assertThat(lottoMatcher.getLottoNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+        LastWeekLotto lottoMatcher = new LastWeekLotto("1 ,2 ,3 ,4 ,5 , 6");
     }
 
     @DisplayName("널값을 넣었을때 익셉션 발생")
     @Test
     void validNull() {
-        assertThatThrownBy(() -> new LottoMatcher(null))
+        assertThatThrownBy(() -> new LastWeekLotto(null))
                 .isInstanceOf(ValidNullException.class);
     }
 
@@ -30,7 +28,7 @@ class LottoMatcherTest {
     @DisplayName("빈 문자열을 넣었을때 익셉션 발생")
     @Test
     void validEmpty() {
-        assertThatThrownBy(() -> new LottoMatcher(" "))
+        assertThatThrownBy(() -> new LastWeekLotto(" "))
                 .isInstanceOf(ValidEmptyException.class);
     }
 
