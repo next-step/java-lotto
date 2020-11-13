18a5e6fb3c85e8cc586606aa46c7f92499f6aca8
oen142 <oen142@naver.com>
2020-11-13 20:39:33 +0900

    feat : 로또금액 이하로 샀을때 익셉션

--- src/main/java/step2/domain/LottoFactory.java
+++ src/main/java/step2/domain/LottoFactory.java
@@ -1,19 +1,29 @@
 package step2.domain;
 
+import step2.exception.LottoMoneyException;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class LottoFactory {
 
 
+    private static final int LOTTO_PRICE = 1000;
     private final Lottos lottos;
 
     public LottoFactory(int money) {
-        int lottoTickets = money / 1000;
+        validMoney(money);
+        int lottoTickets = money / LOTTO_PRICE;
 
         lottos = new Lottos(Arrays.stream(new Integer[lottoTickets])
                 .map(integer -> new Lotto(RandomLottoGenerator.generateLottoNumbers()))
                 .collect(Collectors.toList()));
+    }
+
+    private void validMoney(int money) {
+        if(money < 1000){
+            throw new LottoMoneyException();
+        }
     }
 
 
