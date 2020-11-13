5f750d7fea75f171456ca330a4ba30d21db6b22a
oen142 <oen142@naver.com>
2020-11-13 20:46:51 +0900

    test : 로또 랭킹을 테스트하는 코드를 구현합니다.

---
+++ src/test/java/step2/domain/LottoRankingTest.java
@@ -0,0 +1,29 @@
+package step2.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoRankingTest {
+
+    @DisplayName("로또 랭킹을 테스트 합니다.")
+    @ParameterizedTest
+    @MethodSource
+    void createLottoRank(LottoRank lottoRank ){
+        assertThat(lottoRank);
+    }
+
+    private static Stream<Arguments> provideLottoRank(){
+        return Stream.of(
+                Arguments.of()
+        );
+    }
+
+}
