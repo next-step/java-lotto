d4fc63458c897ea53d08c4f5332e9479e889c656
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-13 20:56:33 +0900

    feat:input값이 널이나 empty일 경우 결과로 0을 리턴한다.

--- src/main/java/nextstep/step1/StringSumCalculator.java
+++ src/main/java/nextstep/step1/StringSumCalculator.java
@@ -2,6 +2,9 @@ package nextstep.step1;
 
 public class StringSumCalculator {
 	public int sum(String inputString) {
+		if (inputString == null || inputString.isEmpty()) {
+			return 0;
+		}
 		return 0;
 	}
 
