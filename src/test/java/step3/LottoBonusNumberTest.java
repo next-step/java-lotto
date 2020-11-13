102eda1f9b903e4c6aa750939430c63fdede3a6a
catsbi <killfine1988@gmail.com>
2020-11-13 14:48:09 +0900

    [feat] 보너스 당첨 관련 테스트 케이스 작성

---
+++ src/test/java/step3/LottoBonusNumberTest.java
@@ -0,0 +1,86 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import step3.domain.lotto.LottoNumber;
+import step3.domain.lotto.LottoTicket;
+import step3.domain.lotto.LottoTickets;
+import step3.domain.lotto.WinningNumbers;
+import step3.type.WinningType;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static step3.Constant.ERROR_DUPLICATE_NUMBER;
+import static step3.Constant.ERROR_OUT_RANGE_NUMBER;
+
+public class LottoBonusNumberTest {
+
+
+    @DisplayName("2등 보너스 볼당첨 테스트")
+    @ParameterizedTest
+    @MethodSource("provideLottoTicketAndWinningNumbers")
+    public void matchBonusNumber(LottoTicket lottoTicket, WinningNumbers winningNumbers) {
+        WinningType winningStatistics = winningNumbers.getWinningStatistics(lottoTicket);
+        assertThat(winningStatistics).isEqualTo(WinningType.RANK_TWO_BONUS);
+    }
+
+    private static Stream<Arguments> provideLottoTicketAndWinningNumbers() {
+        return Stream.of(
+                Arguments.of(new LottoTicket(createHashSet(1, 2, 3, 4, 5, 7)), WinningNumbers.of(createHashSet(1, 2, 3, 4, 5, 10), 7))
+        );
+    }
+
+    @DisplayName("2등 보너스 번호 중복 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void bonusNumberDuplicate(int bonusNumber) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> WinningNumbers.of(createHashSet(1, 2, 3, 4, 5, 6), bonusNumber))
+                .withMessage(ERROR_DUPLICATE_NUMBER);
+    }
+
+    @DisplayName("2등 보너스 번호 범위 유효성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void bonusNumberOutRange(int bonusNumber) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> WinningNumbers.of(createHashSet(1, 2, 3, 4, 5, 6), bonusNumber))
+                .withMessage(ERROR_OUT_RANGE_NUMBER);
+    }
+
+    @DisplayName("2등 보너스 번호 당첨 수익률 테스트")
+    @ParameterizedTest
+    @MethodSource("provideWinningTicketAndWinningNumber")
+    void bonusWinningTicketRevenue(LottoTicket ticket, WinningNumbers winningNumbers) {
+        final int BONUS_REVENUE_PERCENT = 30000;
+
+        LottoTickets lottoTickets = new LottoTickets(Collections.singletonList(ticket));
+        double revenueRate = lottoTickets.getRevenueRate(winningNumbers);
+
+        //로또 한장으로 보너스 당첨이 되면 수익율은 30000이다.
+        assertThat(revenueRate).isEqualTo(BONUS_REVENUE_PERCENT);
+
+    }
+
+    private static Stream<Arguments> provideWinningTicketAndWinningNumber() {
+        return Stream.of(
+                Arguments.of(new LottoTicket(createHashSet(1, 2, 3, 4, 5, 6)), WinningNumbers.of("1,2,3,4,5,7", 6))
+        );
+    }
+
+    private static Set<LottoNumber> createHashSet(int... args) {
+        Set<LottoNumber> set = new HashSet<>();
+        for (int arg : args) {
+            set.add(new LottoNumber(arg));
+        }
+        return set;
+    }
+}
