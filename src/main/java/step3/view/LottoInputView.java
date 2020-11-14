defe552c8e2beb3930d4d64c48d586ce3960e315
catsbi <killfine1988@gmail.com>
2020-11-14 16:06:38 +0900

    [refactor] Scanner를 받지 않는 기본 생성자로 변경

--- src/main/java/step3/view/LottoInputView.java
+++ src/main/java/step3/view/LottoInputView.java
@@ -9,9 +9,15 @@ public class LottoInputView implements InputView {
     private static final String QUESTION_LAST_WEEK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String QUESTION_BONUS_BALL = "보너스 볼을 입력해 주세요.";
 
+    private final Scanner scanner;
+
+    public LottoInputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
 
     @Override
-    public int getUseAmount(Scanner scanner) {
+    public int getUseAmount() {
         System.out.println(QUESTION_USE_MONEY);
         int insertedMoney = scanner.nextInt();
         scanner.nextLine();
@@ -19,7 +25,7 @@ public class LottoInputView implements InputView {
     }
 
     @Override
-    public WinningNumbers getWinningNumber(Scanner scanner) {
+    public WinningNumbers getWinningNumber() {
 
         System.out.println(QUESTION_LAST_WEEK_WINNING_NUMBER);
         String insertedWinningNumber = scanner.nextLine();
