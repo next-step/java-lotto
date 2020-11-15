e10878498e7fb4b5d7db49a854d4d2e189e0ad54
oen142 <oen142@naver.com>
2020-11-15 22:36:51 +0900

    fix : 매직 넘버 도출

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String PURCHASE_LOTTO = "구입금액을 입력해 주세요.";
+    private static final String LAST_WEEK_LOTTO = "지난 주 당첨번호를 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
     private InputView(){}
 
     public static int purchaseLotto(){
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(PURCHASE_LOTTO);
         return scanner.nextInt();
     }
     public static String lastWeekLotto(){
-        System.out.println("지난 주 당첨번호를 입력해 주세요.");
+        System.out.println(LAST_WEEK_LOTTO);
         return scanner.next();
     }
 
