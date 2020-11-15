14ee27973a88dc457a9bc6ecef7689ec2fedb1c3
홍성민 <honggasm@gmail.com>
2020-11-15 15:14:31 +0900

    feat: lotto validation 로직 작성

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -3,22 +3,29 @@ package step2.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static util.Preconditions.checkArgument;
+
 public class Lotto {
+    private static final int LOTTO_NUMBERS_LENGTH = 6;
     private static final Money PRICE = Money.of(1000);
     private static final CreateLottoNumbersStrategy DEFAULT_CREATE_NUMBER_STRATEGY = new CreateRandomNumbersStrategy();
+    public static final String LOTTO_NUMBER_MUST_NOT_BE_NULL = "lotto number must not be null";
+    public static final String LOTTO_NUMBER_SIZE_NOT_VALID = "lotto number size must be " + LOTTO_NUMBERS_LENGTH;
+
     private final List<LottoNumber> numbers;
-    private static final int LOTTO_NUMBERS_LENGTH = 6;
-    
+
     private Lotto(final List<LottoNumber> numbers) {
         this.numbers = numbers;
     }
 
-    public static Lotto of(final CreateLottoNumbersStrategy strategy) {
-        return new Lotto(strategy.create());
-    }
-
     public static Lotto of(final List<LottoNumber> numbers) {
+        checkArgument(numbers != null, LOTTO_NUMBER_MUST_NOT_BE_NULL);
+        checkArgument(numbers.size() == LOTTO_NUMBERS_LENGTH, LOTTO_NUMBER_SIZE_NOT_VALID);
         return new Lotto(numbers);
+    }
+    
+    public static Lotto of(final CreateLottoNumbersStrategy strategy) {
+        return of(strategy.create());
     }
 
     public static Lotto of() {
@@ -37,7 +44,7 @@ public class Lotto {
     public static int getLottoNumbersLength() {
         return LOTTO_NUMBERS_LENGTH;
     }
-    
+
     public List<Integer> getLottoNumber() {
         return numbers.stream().map(LottoNumber::getValue).collect(Collectors.toList());
     }
--- src/test/java/step2/domain/LottoTest.java
+++ src/test/java/step2/domain/LottoTest.java
@@ -1,11 +1,17 @@
 package step2.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static step2.domain.Lotto.LOTTO_NUMBER_MUST_NOT_BE_NULL;
+import static step2.domain.Lotto.LOTTO_NUMBER_SIZE_NOT_VALID;
 
 class LottoTest {
     private final List<LottoNumber> lottoNumberOneToSix = Arrays.asList(
@@ -17,25 +23,107 @@ class LottoTest {
             LottoNumber.of(6)
     );
 
-    @Test
-    void of() {
-        // given
-        final CreateLottoNumbersStrategy strategy = () -> lottoNumberOneToSix;
+    @DisplayName("of")
+    @Nested
+    class Of {
+        @DisplayName("로또 번호 생성 전략을 주입")
+        @Test
+        void with_strategy() {
+            // given
+            final CreateLottoNumbersStrategy strategy = () -> lottoNumberOneToSix;
 
-        // when 
-        final Lotto lotto = Lotto.of(strategy);
+            // when 
+            final Lotto lotto = Lotto.of(strategy);
 
-        // then
-        assertThat(lotto).isNotNull();
-    }
+            // then
+            assertThat(lotto).isNotNull();
+        }
 
-    @Test
-    void of_without_strategy() {
-        // when 
-        final Lotto lotto = Lotto.of();
+        @DisplayName("로또 번호 리스트를 직접 전달")
+        @Nested
+        class WithLottoNumberList {
+            @DisplayName("정상 생성")
+            @Test
+            void success() {
+                // when 
+                final Lotto lotto = Lotto.of(lottoNumberOneToSix);
 
-        // then
-        assertThat(lotto).isNotNull();
+                // then
+                assertThat(lotto).isNotNull();
+            }
+
+            @DisplayName("null 리스타가 전달된 경우")
+            @Test
+            void throw_exception_when_receive_null() {
+                // given
+                final List<LottoNumber> nullList = null;
+
+                // when 
+                final Throwable thrown = catchThrowable(() -> {
+                    Lotto.of(nullList);
+                });
+
+                // then
+                Assertions.assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(LOTTO_NUMBER_MUST_NOT_BE_NULL);
+            }
+
+            @DisplayName("로또 번호가 정해진 개수 보다 많은 경우")
+            @Test
+            void throw_exception_when_receive_lotto_number_size_that_bigger_then_standard() {
+                // given
+                final List<LottoNumber> lottoNumberOneToSeven = Arrays.asList(
+                        LottoNumber.of(1),
+                        LottoNumber.of(2),
+                        LottoNumber.of(3),
+                        LottoNumber.of(4),
+                        LottoNumber.of(5),
+                        LottoNumber.of(6),
+                        LottoNumber.of(7)
+                );;
+
+                // when 
+                final Throwable thrown = catchThrowable(() -> {
+                    Lotto.of(lottoNumberOneToSeven);
+                });
+
+                // then
+                Assertions.assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(LOTTO_NUMBER_SIZE_NOT_VALID);
+            }
+
+            @DisplayName("로또 번호가 정해진 개수 보다 많은 경우")
+            @Test
+            void throw_exception_when_receive_lotto_number_size_that_less_then_standard() {
+                // given
+                final List<LottoNumber> lottoNumberOneToFive = Arrays.asList(
+                        LottoNumber.of(1),
+                        LottoNumber.of(2),
+                        LottoNumber.of(3),
+                        LottoNumber.of(4),
+                        LottoNumber.of(5)
+                );;
+
+                // when 
+                final Throwable thrown = catchThrowable(() -> {
+                    Lotto.of(lottoNumberOneToFive);
+                });
+
+                // then
+                Assertions.assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(LOTTO_NUMBER_SIZE_NOT_VALID);
+            }
+        }
+
+        @DisplayName("아무 전달인자가 없는 경우")
+        @Test
+        void without_strategy() {
+            // when 
+            final Lotto lotto = Lotto.of();
+
+            // then
+            assertThat(lotto).isNotNull();
+        }
     }
 
     @Test
