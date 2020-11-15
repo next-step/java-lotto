b302653272269b3f65cec51b559d955ea7db5d39
oen142 <oen142@naver.com>
2020-11-15 22:17:39 +0900

    test fix : 외부에서 랜덤한 값을 받아서 테스트 하기 쉽게 만든다.

--- src/test/java/step2/domain/LottoFactoryTest.java
+++ src/test/java/step2/domain/LottoFactoryTest.java
@@ -4,20 +4,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.exception.LottoMoneyException;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoFactoryTest {
 
-    @Test
-    @DisplayName("로또 가격을 입력하면 갯수를 반환한다.")
-    void create(){
-        LottoFactory lottoTicket = new LottoFactory(14000);
-        assertThat(lottoTicket.isLottoTicketsCount(14)).isTrue();
-    }
     @Test
     @DisplayName("로또의 최소가격 미만으로 지불했을 경우 익셉션이 발생한다.")
     void exception(){
         assertThatThrownBy(() -> new LottoFactory(999))
                 .isInstanceOf(LottoMoneyException.class);
+    }
+
+    @Test
+    @DisplayName("로또 가격을 입력하면 갯수를 반환한다")
+    void name() {
+        LottoFactory lottoFactory = new LottoFactory(14000);
+        assertThat(lottoFactory.getLottoCount()).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("로또 넘버들을 입력하면 String로 반환한다.")
+    void getLottoNumbersListToString() {
+
     }
 }
