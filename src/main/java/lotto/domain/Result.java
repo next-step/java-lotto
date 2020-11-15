c90270e044a4cb930b915fc62ab5da2a571b316e
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-15 22:04:47 +0900

    [Refactor]: HashMap -> Map

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -2,10 +2,11 @@ package lotto.domain;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class Result {
-    private final HashMap<Integer, Integer> result;
+    private final Map<Integer, Integer> result;
 
     public Result(List<Lotto> lottos, Lotto winningLotto) {
         result = new HashMap<>();
