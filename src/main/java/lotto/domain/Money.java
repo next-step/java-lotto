672a4636b19f3000d59a95936e81b10fd700d79d
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-16 00:05:32 +0900

    [fix]: profit 이 0이면 터지는 버그 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,8 +1,7 @@
 package lotto.domain;
 
+import lotto.exception.BadDividerException;
 import lotto.exception.BadMoneyException;
-
-import java.util.Objects;
 
 public class Money {
     private final int money;
@@ -13,35 +12,21 @@ public class Money {
     }
 
     private void validateMoney(int money) {
-        if (money <= 0) {
+        if (money < 0) {
             throw BadMoneyException.getInstance();
         }
 
     }
 
     public double divide(Money divider) {
+        if (divider.money <= 0) {
+            throw BadDividerException.getInstance();
+        }
         return money / divider.money;
     }
 
     @Override
     public String toString() {
         return Integer.toString(money);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Money money1 = (Money) o;
-        return money == money1.money;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(money);
     }
 }
