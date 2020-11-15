1fae01178e3fdf9d36d0a772049cc5cc0697f21b
BlueCitron <sds901234@naver.com>
2020-11-15 22:18:50 +0900

    feat: 로또 구매 금액 클래스(PurchaseAmount) 및 테스트 추가

---
+++ src/test/java/study/lotto/PurchaseAmountTest.java
@@ -0,0 +1,27 @@
+package study.lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PurchaseAmountTest {
+
+    @Test
+    @DisplayName("1000원 미만 구매 실패")
+    void test_create_fail() {
+        assertThrows(IllegalArgumentException.class, () -> new PurchaseAmount(500));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1", "1500,1", "2000,2"})
+    void test_purchase_count(int amount, int numberOfPurchases) {
+        // When
+        PurchaseAmount purchaseAmount = new PurchaseAmount(amount);
+
+        // Then
+        assertEquals(purchaseAmount.numberOfPurchases(), numberOfPurchases);
+    }
+}
