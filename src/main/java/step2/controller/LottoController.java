af3d999cde02c0577c563e59b8b8fadd8a201e37
oen142 <oen142@naver.com>
2020-11-15 22:37:26 +0900

    feat : 게임을 할 수 있는 로또 컨트롤러를 만든다.

--- src/main/java/step2/controller/LottoController.java
+++ src/main/java/step2/controller/LottoController.java
@@ -3,11 +3,9 @@ package step2.controller;
 import step2.domain.LastWeekLotto;
 import step2.domain.LottoFactory;
 import step2.domain.LottoMatcher;
-import step2.domain.LottoRank;
+import step2.domain.RandomLottoGenerator;
 import step2.view.InputView;
 import step2.view.OutputView;
-
-import java.util.Map;
 
 public class LottoController {
 
@@ -16,18 +14,15 @@ public class LottoController {
     public static void runLotto() {
         int lottoPurchaseMoney = InputView.purchaseLotto();
 
-        LottoFactory lottoFactory = new LottoFactory(lottoPurchaseMoney);
+        LottoFactory lottoFactory = new LottoFactory(lottoPurchaseMoney ,RandomLottoGenerator.of());
+
         OutputView.purchaseLotto(lottoFactory.getLottoCount());
         OutputView.printLottoTickets(lottoFactory.getLottoNumbersListToString());
 
         LottoMatcher lottoMatcher = lottoFactory.matchNumbers(LastWeekLotto.separateLottoToList(InputView.lastWeekLotto()));
 
-        Map<LottoRank, Integer> result = lottoMatcher.getResult();
-        System.out.println(result.get(LottoRank.MISS));
-        System.out.println(result.get(LottoRank.FORTH));
-        System.out.println(result.get(LottoRank.THIRD));
-        System.out.println(result.get(LottoRank.SECOND));
-        System.out.println(result.get(LottoRank.FIRST));
+        OutputView.printStatics(lottoMatcher.getResult());
 
+        OutputView.printInvestRate(lottoFactory.getLottoRatio(lottoMatcher, lottoPurchaseMoney));
     }
 }
