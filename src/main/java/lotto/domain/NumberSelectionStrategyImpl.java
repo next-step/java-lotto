6c4f538e8b999bef7a0c18ccc8bec88d560517eb
changgunyee <minyoung403@naver.com>
2020-11-15 17:01:09 +0900

    fix: set lotto numbers size to 6

--- src/main/java/lotto/domain/NumberSelectionStrategyImpl.java
+++ src/main/java/lotto/domain/NumberSelectionStrategyImpl.java
@@ -4,7 +4,7 @@ public class NumberSelectionStrategyImpl implements NumberSelectionStrategy {
     public static final int START_INCLUSIVE = 1;
     public static final int END_EXCLUSIVE = 46;
     public static final int FROM_IDX = 0;
-    public static final int TO_IDX = 5;
+    public static final int TO_IDX = 6;
 
     @Override
     public Numbers collectNumbers() {
