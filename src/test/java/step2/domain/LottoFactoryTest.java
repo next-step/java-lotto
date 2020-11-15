bea60aa3d26d6ada1863a4d90d67eafecfddcf93
oen142 <oen142@naver.com>
2020-11-15 22:30:47 +0900

    test fix : 로또 넘버들을 테스트 한다.

--- src/test/java/step2/domain/LottoFactoryTest.java
+++ src/test/java/step2/domain/LottoFactoryTest.java
@@ -12,20 +12,21 @@ class LottoFactoryTest {
     @Test
     @DisplayName("로또의 최소가격 미만으로 지불했을 경우 익셉션이 발생한다.")
     void exception(){
-        assertThatThrownBy(() -> new LottoFactory(999))
+        assertThatThrownBy(() -> new LottoFactory(999 ,new OneToSixLottoMakeStrategy()))
                 .isInstanceOf(LottoMoneyException.class);
     }
 
     @Test
     @DisplayName("로또 가격을 입력하면 갯수를 반환한다")
     void name() {
-        LottoFactory lottoFactory = new LottoFactory(14000);
+        LottoFactory lottoFactory = new LottoFactory(14000 , new OneToSixLottoMakeStrategy());
         assertThat(lottoFactory.getLottoCount()).isEqualTo(14);
     }
 
     @Test
     @DisplayName("로또 넘버들을 입력하면 String로 반환한다.")
     void getLottoNumbersListToString() {
-
+        LottoFactory lottoFactory = new LottoFactory(1000 , new OneToSixLottoMakeStrategy());
+        assertThat(lottoFactory.getLottoNumbersListToString()).containsExactly("1, 2, 3, 4, 5, 6");
     }
 }
---
+++ src/test/java/step2/domain/OneToSixLottoMakeStrategy.java
@@ -0,0 +1,14 @@
+package step2.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OneToSixLottoMakeStrategy implements LottoMakeStrategy {
+
+
+
+    @Override
+    public List<Integer> generateLottoNumbers() {
+        return Arrays.asList(1,2,3,4,5,6);
+    }
+}
