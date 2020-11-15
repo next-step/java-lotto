e138c64634596cf6b68253e3509c7255fde71374
kyle lee <kyle.lee@balancehero.com>
2020-11-15 15:46:05 +0900

    refactor : Refactor StringAddCalculator

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,42 +1,21 @@
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringAddCalculator {
 
-    private final static String DEFAULT_DELIMITER = "[,:]";
-    private final static String PATTERN_CUSTOM_DELIMITER = "//(.)\n(.*)";
-
-    public static int splitAndSum(String input) {
+    public static int calculate(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
         if (input.length() == 1) {
-            return new Number(input).number();
+            return new PositiveNumber(input).getPositiveNumber();
         }
 
         return result(input);
     }
 
     private static int result(String input) {
-        Matcher matcher = Pattern.compile(PATTERN_CUSTOM_DELIMITER).matcher(input);
-        if (matcher.find()) {
-            return sumByCustomDelimiter(matcher);
+        if (CustomDelimiter.find(input)) {
+            return CustomDelimiter.sum();
         }
 
-        return sum(input.split(DEFAULT_DELIMITER));
-    }
-
-
-    private static int sumByCustomDelimiter(Matcher matcher) {
-        String customDelimiter = matcher.group(1);
-        String[] numbers = matcher.group(2).split(customDelimiter);
-        return sum(numbers);
-    }
-
-    private static int sum(String[] numbers) {
-        return Arrays.stream(numbers)
-                .map(n -> new Number(n).number())
-                .reduce(0, Integer::sum);
+        return DefaultDelimiter.sum(input);
     }
 }
