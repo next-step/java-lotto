a2a8c56bd9f61c2f4e456f97cdd21a4941827a3a
oen142 <oen142@naver.com>
2020-11-15 22:34:51 +0900

    feat : 로또 클래스를 만든다.

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -3,6 +3,7 @@ package step2.domain;
 import step2.exception.LottoCountBoundException;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -39,5 +40,18 @@ public class Lotto {
         return lottoNumbers.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(", "));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
     }
 }
