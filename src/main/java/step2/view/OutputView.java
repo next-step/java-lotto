5e9d7d99568a70c1bc3e53e131c46735ea9df47f
oen142 <oen142@naver.com>
2020-11-15 22:37:09 +0900

    feat : 게임 결과를 확인할수 있는 뷰를 만든다.

--- src/main/java/step2/view/OutputView.java
+++ src/main/java/step2/view/OutputView.java
@@ -1,26 +1,62 @@
 package step2.view;
 
+import step2.domain.LottoRank;
+
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OutputView {
+
+    private static final String LOTTO_PRICE_MESSAGE = "%d개 일치 (%d원)- %d개";
+    private static final String LOTTO_STATICS = "구매 통계";
+    private static final String LOTTO_BLANKET = "[ %s ]";
+    private static final String HYPHEN = "----------";
+    private static final String PURCHASE_AMOUNT = "개를 구매했습니다.";
+    private static final String INVEST_RATE = "총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 %s 라는 의미임)";
 
     private OutputView() {
     }
 
     public static void purchaseLotto(int lottoTicketCount) {
-        System.out.println(lottoTicketCount + "개를 구매했습니다.");
+        println(printPurchaseLotto(lottoTicketCount));
+    }
+
+    private static String printPurchaseLotto(int lottoTicketCount) {
+        return String.format(
+                String.valueOf(lottoTicketCount), PURCHASE_AMOUNT);
+    }
+
+    private static void println(String input) {
+        System.out.println(input);
     }
 
     public static void printLottoTickets(List<String> lottoNumbersListToString) {
-        lottoNumbersListToString.forEach(str -> System.out.println("[ " + str + " ]"));
+        lottoNumbersListToString.forEach(lottoNumbers -> println(String.format(LOTTO_BLANKET, lottoNumbers)));
     }
 
-    public static void printStatics() {
-        System.out.println("당첨 통계");
-        System.out.println("-------");
-
-
+    public static void printStatics(Map<LottoRank, Integer> result) {
+        println(LOTTO_STATICS);
+        println(HYPHEN);
+        println(printLottoPrice(result));
     }
 
+    private static String printLottoPrice(Map<LottoRank, Integer> result) {
+        return Stream.of(LottoRank.FIRST, LottoRank.SECOND, LottoRank.THIRD, LottoRank.FORTH)
+                .map(lottoRank -> String.format(LOTTO_PRICE_MESSAGE, lottoRank.getMatch(), lottoRank.getPrice(), result.get(lottoRank)))
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public static void printInvestRate(double lottoRate){
+
+        println(String.format(INVEST_RATE , lottoRate , quantifyInvestLotto(lottoRate)));
+    }
+    private static String quantifyInvestLotto(double lottoRate){
+        if(lottoRate > 1){
+            return "이득";
+        }
+        return "손해";
+    }
 
 }
