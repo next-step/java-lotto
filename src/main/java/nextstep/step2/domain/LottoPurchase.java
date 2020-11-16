336c2a1799e4b884efe945c30b8216b7a29e1bbd
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-16 23:49:32 +0900

    refactor:로또구매 출력 메소드 변경

--- src/main/java/nextstep/step2/domain/LottoPurchase.java
+++ src/main/java/nextstep/step2/domain/LottoPurchase.java
@@ -2,18 +2,17 @@ package nextstep.step2.domain;
 
 public class LottoPurchase {
 	private static final String LOTTO_PURCHASE_MESSAGE = "%d개를 구매했습니다.";
-	private static int LOTTO_PRICE = 1000;
+	public static int LOTTO_PRICE = 1000;
 	private int money;
 
 	public LottoPurchase(int money) {
 		this.money = money;
 	}
 	public int getLottoCount() {
-		System.out.println(String.format(LOTTO_PURCHASE_MESSAGE, getBuying(money)));
-		return getBuying(money);
+		return money / LOTTO_PRICE;
 	}
 
-	public int getBuying(int money) {
-		return money / LOTTO_PRICE;
+	public void printLottoPurchase() {
+		System.out.println(String.format(LOTTO_PURCHASE_MESSAGE, getLottoCount()));
 	}
 }
