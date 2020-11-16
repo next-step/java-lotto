f2e47aedd31a4e95e8099c3db49f9334558205d7
KyeongMin <kyeongmin.jake.lee@gmail.com>
2020-11-16 10:14:12 +0900

    Step1 - 문자열 덧셈 계산기 (#888)
    * docs - 구현 기능 목록
    
    * feat - 빈 문자열 확인 기능
    
    * feat - 문자열 null 확인 기능
    
    * feat - 빈 문자열 또는 null 값을 입력할 경우 0을 반환하는 기능
    
    * feat - 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는 기능
    
    * feat - 콤마(,) 구분자로 입력할 경우 문자열의 합을 반환하는 기능
    
    * feat - 콤마(,)와 콜론(:)을 섞어서 구분자로 입력할 경우 문자열의 합을 반환하는 기능
    
    * feat - 커스텀 구분자를 지정하는 경우 문자열의 합을 반환하는 기능
    
    * feat - 음수의 값을 입력할 경우 예외처리하는 기능
    
    * feat - 숫자 이외의 값을 입력할 경우 예외처리

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,64 @@
+import util.CalculatorException;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    public int calculate(String text) {
+        if(checkNullString(text) || checkEmptyString(text)) {
+            return 0;
+        }
+
+        CalculatorException.validateMinus(text);
+
+        if(checkOnlyOneText(text)) {
+            CalculatorException.validateNumbers(text);
+            return Integer.parseInt(text);
+        }
+
+        if(checkCustomDelimiter(text)) {
+            return sumCustomDelimiter(text);
+        }
+
+        return sum(text.split(",|:"));
+    }
+
+    private int sumCustomDelimiter(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        String[] tokens = {""};
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            tokens = m.group(2).split(customDelimiter);
+        }
+        return sum(tokens);
+    }
+
+    private boolean checkCustomDelimiter(String text) {
+        return text.startsWith("//");
+    }
+
+    private int sum(String[] texts) {
+        return Arrays.stream(texts)
+                .mapToInt(text -> {
+                    CalculatorException.validateNumbers(text);
+                    return Integer.parseInt(text);
+                })
+                .sum();
+
+    }
+
+    private boolean checkOnlyOneText(String text) {
+        Matcher m = Pattern.compile("^[0-9]+$").matcher(text);
+        return m.find();
+    }
+
+    private boolean checkEmptyString(String text) {
+        return text.isEmpty();
+    }
+
+    private boolean checkNullString(String text) {
+        return text == null;
+    }
+
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    @DisplayName("문자열이 비어있는 경우 0 리턴")
+    void returnZeroEmptyTest() {
+        assertThat(stringCalculator.calculate("")).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("문자열이 Null인 경우 0 리턴")
+    void returnZeroNullTest() {
+        assertThat(stringCalculator.calculate(null)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("문자열에 숫자가 하나인 경우 해당 숫자 리턴")
+    void returnOneNumberTest() {
+        String text = "3";
+        assertThat(stringCalculator.calculate(text)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("문자열에 콤마 구분자가 있을 경우 문자열 합 리턴")
+    void onlyCommaDelimiterTest() {
+        String text = "3,5";
+        assertThat(stringCalculator.calculate(text)).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("문자열에 콤마와 콜론 구분자가 섞여있을 경우 문자열 합 리턴")
+    void commaAndColonDelimiterTest() {
+        String text = "3,5:6";
+        assertThat(stringCalculator.calculate(text)).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자를 지정하는 경우 문자열의 합 리턴")
+    void customDelimiterTest() {
+        String text = "//;\n1;2;3";
+        assertThat(stringCalculator.calculate(text)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("문자열이 음수인 경우 예외 처리")
+    void minusExceptionTest() {
+        String text = "-1";
+        Assertions.assertThrows(RuntimeException.class,
+                () -> stringCalculator.calculate(text));
+    }
+
+    @Test
+    @DisplayName("숫자인 문자열이 아닌 경우 예외 처리")
+    void notNumberExceptionTest() {
+        String text = "1,2,b";
+        Assertions.assertThrows(RuntimeException.class,
+                () -> stringCalculator.calculate(text));
+    }
+
+}
