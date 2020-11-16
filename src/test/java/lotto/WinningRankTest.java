9cd52e25bf3e60d2215dfc7949b2cd1d09cfa847
siyoon <siyoon210@gmail.com>
2020-11-17 07:43:12 +0900

    test: 보너스번호는 당첨번호 5개가 맞은 경우에만 사용한다.

--- src/test/java/lotto/WinningRankTest.java
+++ src/test/java/lotto/WinningRankTest.java
@@ -39,4 +39,27 @@ public class WinningRankTest {
                         .isEqualTo(NONE)
         );
     }
+
+    @Test
+    @DisplayName("보너스번호는 당첨번호 5개가 맞은 경우에만 사용한다.")
+    void winningRankTestBonusNumber() {
+        Lotto winningLotto = Lotto.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = BonusNumber.valueOf(45);
+        WinningNumber winningNumber = new WinningNumber(winningLotto, bonusNumber);
+
+        assertAll(
+                () -> assertThat(getWinningRank(winningNumber, Lotto.of(1, 2, 3, 4, 5, 6)))
+                        .isEqualTo(MATCHES_SIX),
+                () -> assertThat(getWinningRank(winningNumber, Lotto.of(1, 2, 3, 4, 5, 45)))
+                        .isEqualTo(MATCHES_FIVE_AND_BONUS_NUMBER),
+                () -> assertThat(getWinningRank(winningNumber, Lotto.of(1, 2, 3, 4, 5, 7)))
+                        .isEqualTo(MATCHES_FIVE),
+                () -> assertThat(getWinningRank(winningNumber, Lotto.of(1, 2, 3, 4, 7, 45)))
+                        .isEqualTo(MATCHES_FOUR),
+                () -> assertThat(getWinningRank(winningNumber, Lotto.of(1, 2, 3, 7, 8, 45)))
+                        .isEqualTo(MATCHES_THREE),
+                () -> assertThat(getWinningRank(winningNumber, Lotto.of(1, 2, 7, 8, 9, 45)))
+                        .isEqualTo(NONE)
+        );
+    }
 }
