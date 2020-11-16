a9b44d966e7d0b020625e2bda97ef9337943e0b7
changgunyee <minyoung403@naver.com>
2020-11-17 00:38:21 +0900

    chore: remove unused property

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,10 +3,8 @@ package lotto.domain;
 public class Lotto {
 
     private final Numbers numbers;
-    private final NumberSelectionStrategy numberSelectionStrategy;
 
     public Lotto(NumberSelectionStrategy numberSelectionStrategy) {
-        this.numberSelectionStrategy = numberSelectionStrategy;
         this.numbers = numberSelectionStrategy.collectNumbers();
     }
 
