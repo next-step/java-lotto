041e20218e6bc9a29a821ba87011befc16a0c5c8
kaeum <oror1024@gmail.com>
2020-11-16 17:00:10 +0900

    refactor: InputView 클래스의 하드코딩 되어있던 문자열 상수로 관리하도록 변경

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String INSERT_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INSERT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String COMMA = ",";
+    private static final String SKIP_LINE = "[\\r\\n]+";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static Long askMoneyAmount(){
@@ -17,8 +19,8 @@ public class InputView {
 
     public static List<Integer> askWinningNumbers() {
         System.out.println(INSERT_WINNING_NUMBERS);
-        scanner.skip("[\\r\\n]+");
-        return Arrays.stream(scanner.nextLine().split(","))
+        scanner.skip(SKIP_LINE);
+        return Arrays.stream(scanner.nextLine().split(COMMA))
                 .map(s -> s.trim())
                 .mapToInt(Integer::parseInt)
                 .boxed()
