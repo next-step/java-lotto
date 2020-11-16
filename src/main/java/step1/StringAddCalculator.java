6dd363100fcc0302f7f74eecc79b9cb135e0d361
dbeod2 <darung.0211@gmail.com>
2020-11-16 20:53:04 +0900

    refactor : indent 수정

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -5,51 +5,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-
-  public static void main(String[] args) {
-    int result = splitAndSum("-1,2,3");
-    System.out.println(result);
-  }
-
-  public static int splitAndSum(String value) {
-    if (value == null || value.equals("")) {
-      return 0;
+    public int splitAndSum(String value) {
+        if (value == null || value.equals("")) {
+            return 0;
+        }
+        isNegative(value);
+        return addCalculator(value);
     }
 
-    if (value.length() == 1) {
-      return Integer.parseInt(value);
+    private int addCalculator(String inputValue) {
+        String[] values = inputValue.split("[^0-9]");
+        return Arrays.stream(values)
+                .mapToInt(this::add)
+                .sum();
     }
 
-    isNegative(value);
-
-    return addCalculator(value);
-  }
-
-  private static int addCalculator(String inputValue) {
-    String[] values = inputValue.split("[^0-9]");
-    return Arrays.stream(values)
-            .mapToInt(StringAddCalculator::add)
-            .sum();
-  }
-
-  private static int add(String value) {
-    int sum = 0;
-    if (isNumeric(value)) {
-      sum += Integer.parseInt(value);
+    private int add(String value) {
+        int sum = 0;
+        if (isNumeric(value)) {
+            sum += Integer.parseInt(value);
+        }
+        return sum;
     }
-    return sum;
-  }
 
-  private static boolean isNumeric(String value) {
-    return value.matches("[0-9]");
-  }
-
-  public static void isNegative(String value) {
-    Pattern pattern = Pattern.compile("-(?:[0-9])");
-    Matcher matcher = pattern.matcher(value);
-
-    if (matcher.find()) {
-      throw new RuntimeException("입력값이 음수입니다.");
+    private boolean isNumeric(String value) {
+        return value.matches("[0-9]");
     }
-  }
+
+    public void isNegative(String value) {
+        Pattern pattern = Pattern.compile("-(?:[0-9])");
+        Matcher matcher = pattern.matcher(value);
+
+        if (matcher.find()) {
+            throw new RuntimeException("입력값이 음수입니다.");
+        }
+    }
 }
--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -1,5 +1,6 @@
 package step1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,32 +10,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringAddCalculatorTest {
-  @ParameterizedTest
-  @DisplayName("입력값 빈 문자열 또는 null인 경우 0을반환")
-  @ValueSource(strings = {"", "null"})
-  public void 입력값_빈문자열_null인_경우(String value) {
-    assertEquals(StringAddCalculator.splitAndSum(value), 0);
-  }
 
-  @ParameterizedTest
-  @DisplayName("입력값이 하나일때")
-  @ValueSource(ints = {5})
-  public void 입력받은_값이_하나일때(int value) {
-    assertEquals(StringAddCalculator.splitAndSum("5"), value);
-  }
+    private static final StringAddCalculator addCalculator = new StringAddCalculator();
 
-  @ParameterizedTest
-  @DisplayName("문자열 덧셈 계산")
-  @ValueSource(strings = {"//;\\n1;2;3,|:6"})
-  public void 문자열_덧셈(String value) {
-    assertEquals(StringAddCalculator.splitAndSum(value), 12);
-  }
+    @ParameterizedTest
+    @DisplayName("입력값 빈 문자열 또는 null인 경우 0을반환")
+    @ValueSource(strings = {"", "null"})
+    public void 입력값_빈문자열_null인_경우(String value) {
+        assertEquals(addCalculator.splitAndSum(value), 0);
+    }
 
-  @Test
-  @DisplayName("입력값이 음수일때 예외처리")
-  public void 입력값_음수일때_예외처리() {
-    assertThatThrownBy(() -> StringAddCalculator.isNegative("-1,2,3"))
-            .isInstanceOf(RuntimeException.class);
-  }
+    @ParameterizedTest
+    @DisplayName("입력값이 하나일때")
+    @ValueSource(ints = {5})
+    public void 입력받은_값이_하나일때(int value) {
+        assertEquals(addCalculator.splitAndSum("5"), value);
+    }
+
+    @ParameterizedTest
+    @DisplayName("문자열 덧셈 계산")
+    @ValueSource(strings = {"//;\\n1;2;3,|:6"})
+    public void 문자열_덧셈(String value) {
+        assertEquals(addCalculator.splitAndSum(value), 12);
+    }
+
+    @Test
+    @DisplayName("입력값이 음수일때 예외처리")
+    public void 입력값_음수일때_예외처리() {
+        assertThatThrownBy(() -> addCalculator.isNegative("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
 
 }
