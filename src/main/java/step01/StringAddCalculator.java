23ba7de0aa066b1414371ab38f8ae35e5e001a27
eyabc <bey4314@naver.com>
2020-11-16 20:12:01 +0900

    refactor: 메서드 분리해보기

--- src/main/java/step01/StringAddCalculator.java
+++ src/main/java/step01/StringAddCalculator.java
@@ -5,37 +5,64 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    public static String tokens = ",|:";
+    private static String tokens = ",|:";
+    private static String pattern = "//(.)\n(.*)";
+    private static String NEGATIVE_EXCEPTION_MESSAGE = "음수는 계산할 수 없습니다.";
 
-    public static int splitAndSum(String input) {
-        if (input == null || input.trim() == "") {
-            return 0;
-        }
+    private static boolean isBlank(String input) {
+        return input == null || input.trim() == "";
+    }
 
+    private static boolean isANumber(String input) {
         try {
-            return Integer.parseInt(input);
+            Integer.parseInt(input);
+            return true;
         } catch (Exception exception) {
-
+            return false;
         }
+    }
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+    private static Matcher matchPattern(String input) {
+        return Pattern.compile(pattern).matcher(input);
+    }
 
-        String[] numbers;
+    private static String[] tokenize(String input, String delimiter) {
+        return input.split(delimiter);
+    }
 
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            numbers = m.group(2).split(customDelimiter);
-        } else {
-            numbers = input.split(tokens);
+    private static String[] tokenize(String input, Matcher matcher) {
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return tokenize(matcher.group(2), customDelimiter);
         }
+        return tokenize(input, tokens);
+    }
+
+    private static void validateNotNegative(int input) {
+        if (input < 0) {
+            throw new RuntimeException(NEGATIVE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public static int calculate(String[] numbers) {
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
                 .reduce(0, (a, b) -> {
-                    if (a < 0) {
-                        throw new RuntimeException("음수는 계산할 수 없습니다.");
-                    }
+                    validateNotNegative(a);
                     return a + b;
                 });
+    }
+
+    public static int splitAndSum(String input) {
+        if (isBlank(input))
+            return 0;
+
+        if (isANumber(input))
+            return Integer.parseInt(input);
+
+        String[] numbers = tokenize(input, matchPattern(input));
+
+        return calculate(numbers);
 
     }
 }
