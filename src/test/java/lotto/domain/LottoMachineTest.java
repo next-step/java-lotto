62bc428814b0aac4f20b731c8f17079cf733f49a
DongHwan Kim <bkuekany@gmail.com>
2020-11-17 01:11:54 +0900

    test : Add 로또 자동생성 테스트

---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoMachineTest {
+
+    @DisplayName("로또 숫자 랜덤 생성 테스트")
+    @Test
+    public void lottoRandomCreateNumberTest(){
+        //Given
+        LottoMachine lottoMachine = new LottoAutoMachine();
+
+        //When
+        List<Integer> result = lottoMachine.createLottoNumber(new ArrayList<>());
+
+        //Then
+        assertThat(result).hasSize(6);
+    }
+}
