64f6aa726f20fd5e35129d67fc50cb23496c039b
changgunyee <minyoung403@naver.com>
2020-11-17 00:13:29 +0900

    refactor: remove Lotto start and end number from NumberSelectionStrategyImpl

--- src/main/java/lotto/domain/NumberSelectionStrategyImpl.java
+++ src/main/java/lotto/domain/NumberSelectionStrategyImpl.java
@@ -1,15 +1,13 @@
 package lotto.domain;
 
 public class NumberSelectionStrategyImpl implements NumberSelectionStrategy {
-    public static final int START_INCLUSIVE = 1;
-    public static final int END_EXCLUSIVE = 46;
     public static final int FROM_IDX = 0;
     public static final int TO_IDX = 6;
 
     @Override
     public Numbers collectNumbers() {
         return Numbers.builder()
-                .range(START_INCLUSIVE, END_EXCLUSIVE)
+                .range(Numbers.Builder.LOTTO_START_NUM, Numbers.Builder.LOTTO_END_NUM)
                 .shuffle()
                 .subNumbers(FROM_IDX, TO_IDX)
                 .sort()
