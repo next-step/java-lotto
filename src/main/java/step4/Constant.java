36078dc613c56e5bb349b21ff01310a667c42576
catsbi <hansolsolsol@mosition.co.kr>
2020-11-16 17:04:44 +0900

    [feat] 에러 메세지 상수 추가

--- src/main/java/step4/Constant.java
+++ src/main/java/step4/Constant.java
@@ -7,6 +7,9 @@ public class Constant {
     public static final String ERROR_OUT_RANGE_NUMBER = "번호는 [1 ~ 45]를 벗어나선 안됩니다.";
     public static final String ERROR_NOT_ENOUGH_MONEY = "돈이 충분하지 않습니다.";
     public static final String ERROR_INVALID_PARAMETER = "유효하지 않은 숫자입니다.";
+    public static final String ERROR_INVALID_MANUALLY_SIZE = "티켓 수보다 많은 번호를 입력 할 수 없습니다.";
+    public static final String ERROR_INVALID_MARKING_NUMBER = "로또 번호는 6개이며 중복이 없어야 합니다..";
+    public static final String ERROR_REQUIRED_NUMERIC = "숫자만 입력 가능합니다.";
 
     private Constant(){}
 }
