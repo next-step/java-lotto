e3e9712b45588a4e97bfa2dfff20dc1322e7dcf8
eyabc <bey4314@naver.com>
2020-11-16 17:09:46 +0900

    feat: splitAndSum_null_또는_빈문자 테스트 구현

---
+++ src/main/java/step01/StringAddCalculator.java
@@ -0,0 +1,10 @@
+package step01;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if (input == null || input.trim() == "") {
+            return 0;
+        }
+
+    }
+}
