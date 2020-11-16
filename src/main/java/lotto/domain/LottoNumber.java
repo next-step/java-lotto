0ec8f6e29ec9e4c75d4f8877935c312b27230062
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-16 22:12:33 +0900

    [feat] : 로또번호 객체 구현, 테스트 메소드 구현

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class LottoNumber {
+
+    private static final Integer MIN_LOTTO_NUMBER = 1;
+    private static final Integer MAX_LOTTO_NUMBER = 45;
+    private static final Set<LottoNumber> lottoNumbers = new LinkedHashSet<>();
+    private final int lottoNumber;
+
+    static {
+        IntStream.rangeClosed(MIN_LOTTO_NUMBER,MAX_LOTTO_NUMBER)
+                .forEach(i -> lottoNumbers.add(new LottoNumber(i)));
+    }
+
+    private LottoNumber(int lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+
+    public static LottoNumber get(int number) {
+        return lottoNumbers.stream()
+                .filter(num -> num.lottoNumber == number)
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public static Set<LottoNumber> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+}
