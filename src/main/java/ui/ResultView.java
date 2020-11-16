223df26cc226adfb28a14b4cc9e27e80b44ca5af
kaeum <oror1024@gmail.com>
2020-11-16 16:57:52 +0900

    refactor: ResultView 클래스의 printLottoResult() 메서드에 하드코딩되어있던 문자열을 상수로 관리하도록 변경

--- src/main/java/ui/ResultView.java
+++ src/main/java/ui/ResultView.java
@@ -1,11 +1,19 @@
 package ui;
 
+import domain.Lotto;
+import domain.LottoPrize;
 import domain.LottoResult;
 
 import java.util.List;
 
 public class ResultView {
     private static final String BUYING_AMOUNT_IS = "개를 구매했습니다.";
+    private static final String LOTTO_STATISTICS_IS = "당첨 통계\n---------";
+    private static final String FOURTH_PRIZE_IS = "3개 일치 (%d원)- ";
+    private static final String THIRD_PRIZE_IS = "4개 일치 (%d원)- ";
+    private static final String SECOND_PRIZE_IS = "5개 일치 (%d원)- ";
+    private static final String FIRST_PRIZE_IS = "6개 일치 (%d원)- ";
+    private static final String TOTAL_MARGIN_RATES_ARE = "총 수익률은 %.2f입니다.";
 
     public static void printBuyingLottos(List<List<Integer>> lottoNumbers) {
         System.out.print(lottoNumbers.size());
@@ -15,15 +23,15 @@ public class ResultView {
     }
 
     public static void printLottoResult(LottoResult lottoResult) {
-        System.out.println("당첨 통계\n---------");
-        System.out.print("3개 일치 (5000원)- ");
+        System.out.println(LOTTO_STATISTICS_IS);
+        System.out.printf(FOURTH_PRIZE_IS, LottoPrize.FOURTH_PRIZE.getPrizeValue());
         System.out.println(lottoResult.getFourthPrizeCount());
-        System.out.print("4개 일치 (50000원)- ");
+        System.out.printf(THIRD_PRIZE_IS, LottoPrize.THIRD_PRIZE.getPrizeValue());
         System.out.println(lottoResult.getThirdPrizeCount());
-        System.out.print("5개 일치 (1500000원)- ");
+        System.out.printf(SECOND_PRIZE_IS, LottoPrize.SECOND_PRIZE.getPrizeValue());
         System.out.println(lottoResult.getSecondPrizeCount());
-        System.out.print("6개 일치 (2000000000원)- ");
+        System.out.printf(FIRST_PRIZE_IS, LottoPrize.FIRST_PRIZE.getPrizeValue());
         System.out.println(lottoResult.getFirstPrizeCount());
-        System.out.printf("총 수익률은 %f입니다.", lottoResult.getProfitRates());
+        System.out.printf(TOTAL_MARGIN_RATES_ARE, lottoResult.getProfitRates());
     }
 }
