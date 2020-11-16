9f536bf33f646646e3913202c37cad2cd6ffb5ee
eyabc <bey4314@naver.com>
2020-11-17 00:06:02 +0900

    refacter: BlankTest input 변경

--- src/test/java/step01/BlankTest.java
+++ src/test/java/step01/BlankTest.java
@@ -1,6 +1,6 @@
 package step01;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
@@ -8,14 +8,14 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class BlankTest {
 
+    @ParameterizedTest
     @NullSource
-    @Test
     public void test_isNull(String input) {
         assertThat(Blank.isNull(input)).isTrue();
     }
 
+    @ParameterizedTest
     @EmptySource
-    @Test
     public void test_isEmpty(String input) {
         assertThat(Blank.isEmpty(input)).isTrue();
     }
