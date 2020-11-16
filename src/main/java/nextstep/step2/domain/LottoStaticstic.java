14282eea8ce639d490726d992f44056c666a3c45
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-16 23:49:10 +0900

    feat:로또 수익률을 구해서 출력한다.

--- src/main/java/nextstep/step2/domain/LottoStaticstic.java
+++ src/main/java/nextstep/step2/domain/LottoStaticstic.java
@@ -6,9 +6,11 @@ import java.util.stream.Stream;
 
 public class LottoStaticstic {
 	private String winnerNumbers;
+	private LottoPurchase purchase;
 
-	public LottoStaticstic(String winnerNumbers) {
+	public LottoStaticstic(String winnerNumbers, LottoPurchase purchase) {
 		this.winnerNumbers = winnerNumbers;
+		this.purchase = purchase;
 	}
 
 	public Map<LottoReward, List<Lotto>> getLottoRewardMap(List<Lotto> lottoList) {
@@ -19,8 +21,18 @@ public class LottoStaticstic {
 	}
 
 	protected List<Integer> getWinnerNumbers() {
-		return Stream.of(winnerNumbers.replaceAll(" ","").split(","))
+		return Stream.of(winnerNumbers.replaceAll(" ", "").split(","))
 				.map(Integer::parseInt)
 				.collect(Collectors.toList());
+	}
+
+	public float calculateWinningProbability(Map<LottoReward, List<Lotto>> lottoResultMap) {
+		int totalAmount = lottoResultMap.keySet()
+				.stream()
+				.map(reward -> { //
+					int lottoCount = lottoResultMap.get(reward).size();
+					return reward.getPrice() * lottoCount;
+				}).reduce(0, Integer::sum);
+		return (float) totalAmount / (purchase.getLottoCount() * purchase.LOTTO_PRICE);
 	}
 }
--- src/main/java/nextstep/step2/view/LottoResultView.java
+++ src/main/java/nextstep/step2/view/LottoResultView.java
@@ -1,6 +1,7 @@
 package nextstep.step2.view;
 
 import nextstep.step2.domain.Lotto;
+import nextstep.step2.domain.LottoPurchase;
 import nextstep.step2.domain.LottoReward;
 
 import java.util.ArrayList;
@@ -14,12 +15,14 @@ public class LottoResultView {
 
 	public void printLottoStaticsic(Map<LottoReward, List<Lotto>> lottoMap) {
 		System.out.println(LOTTO_STATISTICTS);
-		addLottoEmptyReward(lottoMap);
-		lottoMap.keySet().stream().sorted(Comparator.reverseOrder()).forEach(lottoReward -> {
-			if (lottoReward.getCount() > 0) {
-				System.out.println(String.format("%d개 일치 (%d원)- %d개", lottoReward.getCount(), lottoReward.getPrice(), lottoMap.get(lottoReward).size()));
-			}
-		});
+		addLottoEmptyReward(lottoMap); //로또번호가 3개 이상 맞지 않아도 출력하기 위해서 추가한다.
+		lottoMap.keySet().stream()
+				.sorted(Comparator.reverseOrder())
+				.forEach(lottoReward -> {
+					if (lottoReward.getCount() > 0) { //2개 이하 맞았을 경우는 제외하고 출력하기
+						System.out.println(String.format("%d개 일치 (%d원)- %d개", lottoReward.getCount(), lottoReward.getPrice(), lottoMap.get(lottoReward).size()));
+					}
+				});
 	}
 
 	private void addLottoEmptyReward(Map<LottoReward, List<Lotto>> lottoMap) {
@@ -28,5 +31,9 @@ public class LottoResultView {
 				lottoMap.put(lottoReward, new ArrayList<>());
 			}
 		});
+	}
+
+	public void printWinningProbability(float probability) {
+		System.out.println(String.format("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", probability));
 	}
 }
