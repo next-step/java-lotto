42f0d0f101c1d177383a526431621f601c8e1719
이병무 <am003507@gmail.com>
2020-11-17 21:52:33 +0900

    feat - 구매하는 가격을 입력하는 InputView 추가

---
+++ src/main/java/lotto_auto/Main.java
@@ -0,0 +1,10 @@
+package lotto_auto;
+
+import lotto_auto.view.BuyInputView;
+
+public class Main {
+    public static void main(String[] args) {
+        int money = BuyInputView.enter();
+
+    }
+}
