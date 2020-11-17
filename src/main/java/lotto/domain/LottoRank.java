e128ab93bf92399cc30a244b0da40fcddefbbc2b
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-17 19:12:50 +0900

    코드 리뷰 반영 : 언더스코어를 사용해 숫자 타입 가독성을 높임

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -10,9 +10,9 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
     FOURTH(3, 5000),
     FIFTH(2, 0),
     SIXTH(1, 0),
