6b6ecd350eaa53fc3a6e4a9b210bdb1d6ed6d0b6
sjh <sjh@revhat.com>
2020-11-17 22:55:17 +0900

    [step1] tokenizer, validate 구현

---
+++ src/main/java/Validator.java
@@ -0,0 +1,9 @@
+public class Validator {
+
+    public static String inputValueTest(String inputValue) {
+        if(inputValue.trim().equals("")){
+            return "0";
+        }
+        return inputValue;
+    }
+}
