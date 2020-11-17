9f6de1b4ec5824bafa7ab24536ef608e152aab5b
이병무 <am003507@gmail.com>
2020-11-17 22:57:54 +0900

    test - 로또 티켓 실패하는 테스트 코드 추가

--- src/main/java/lotto_auto/model/LottoTicket.java
+++ src/main/java/lotto_auto/model/LottoTicket.java
@@ -1,4 +1,25 @@
 package lotto_auto.model;
 
+import java.util.List;
+
 public class LottoTicket {
+
+    public LottoTicket() {
+    }
+
+    public LottoTicket(List<Integer> lottoNumber) {
+
+    }
+
+    public int getNumberCount() {
+        return -1;
+    }
+
+    public List<Integer> export() {
+        return null;
+    }
+
+    public int matchNumberCount() {
+        return -1;
+    }
 }
