7e29b26a79669deaf41bf11d43a5a163043168c8
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-17 18:39:11 +0900

    test:리워드를 가지는 로또객체 테스트

---
+++ src/test/java/nextstep/step2/domain/WinningLottoTest.java
@@ -0,0 +1,20 @@
+package nextstep.step2.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningLottoTest {
+	@Test
+	@DisplayName("로또 리워드를 갖는 로또를 생성한다.")
+	public void winningLottoTest() {
+		Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+		List<Integer> lastWinningLotto = Arrays.asList(1,2,3,14,15,16);
+		WinningLotto winningLotto = new WinningLotto(lotto, lastWinningLotto);
+		assertThat(winningLotto.getLottoReward()).isEqualTo(LottoReward.FOURTH);
+	}
+}
