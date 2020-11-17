6a281952a41374ee9e588fe7991120a2241cebac
catsbi <hansolsolsol@mosition.co.kr>
2020-11-17 10:50:37 +0900

    [refactor] 로또의 타입(자동,수동)을 LottoTicket이 가지도록 변경

--- src/main/java/step4/domain/lotto/LottoTicket.java
+++ src/main/java/step4/domain/lotto/LottoTicket.java
@@ -2,6 +2,7 @@ package step4.domain.lotto;
 
 import step4.domain.lotto.firstcollection.LottoNumber;
 import step4.domain.lotto.firstcollection.MarkingNumbers;
+import step4.type.LottoType;
 import step4.strategy.NumberMakeStrategy;
 import step4.type.WinningType;
 
@@ -9,17 +10,20 @@ import java.util.Set;
 
 public class LottoTicket {
     private final MarkingNumbers markingNumbers;
+    private final LottoType lottoType;
 
     public LottoTicket(NumberMakeStrategy strategy) {
-        this(strategy.create());
+        this(strategy.create(), LottoType.AUTO);
     }
 
-    public LottoTicket(Set<LottoNumber> markingNumber) {
+    public LottoTicket(Set<LottoNumber> markingNumber, LottoType lottoType) {
         this.markingNumbers = new MarkingNumbers(markingNumber);
+        this.lottoType = lottoType;
     }
 
     public LottoTicket(String string) {
         this.markingNumbers = MarkingNumbers.of(string);
+        this.lottoType = LottoType.MANUAL;
     }
 
     public boolean isMarked(LottoNumber number) {
@@ -34,5 +38,9 @@ public class LottoTicket {
     public WinningType compareWinningNumber(WinningNumbers winningNumbers) {
         int matchCount = winningNumbers.countEquals(this);
         return WinningType.getType(matchCount, winningNumbers.isMarkedBonusBall(this));
+    }
+
+    public boolean equalsType(LottoType type) {
+        return lottoType.equals(type);
     }
 }
