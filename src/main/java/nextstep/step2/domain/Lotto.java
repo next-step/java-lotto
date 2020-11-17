d8c9c54ba00ae6f65c4cf74a666ad85da8ce6521
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-17 20:01:27 +0900

    refactor:로또가 가지고 있던 리워드 멤버 제거

--- src/main/java/nextstep/step2/domain/Lotto.java
+++ src/main/java/nextstep/step2/domain/Lotto.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Lotto {
 	private List<Integer> numbers;
-	private LottoReward lottoReward;
 
 	public Lotto(List<Integer> numbers) {
 		this.numbers = numbers;
@@ -12,16 +11,5 @@ public class Lotto {
 
 	public List<Integer> getNumbers() {
 		return numbers;
-	}
-
-	public void setLottoReward(List<Integer> winnerNumbers) {
-		long matchCount = getNumbers().stream()
-				.filter(number -> winnerNumbers.contains(number))
-				.count();
-		lottoReward = LottoReward.getReword(Math.toIntExact(matchCount));
-	}
-
-	public LottoReward getLottoReward() {
-		return lottoReward;
 	}
 }
--- src/test/java/nextstep/step2/domain/LottoTest.java
+++ src/test/java/nextstep/step2/domain/LottoTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class LottoTest {
 	@Test
@@ -17,15 +17,5 @@ public class LottoTest {
 		assertThat(lotto.getNumbers()).hasSize(6);
 		assertAll(() -> assertFalse(lotto.getNumbers().contains(0)),
 				() -> assertFalse(lotto.getNumbers().contains(46)));
-	}
-
-	@Test
-	@DisplayName("로또 번호의 리워드를 가질 수 있다.")
-	public void lottoRewardTest() {
-		List<Integer> lastWinnerNumbers = Arrays.asList(1,2,3,4,5,6);
-		Lotto lotto = new Lotto(Arrays.asList(1,2,3,14,15,16));
-		lotto.setLottoReward(lastWinnerNumbers);
-		assertThat(LottoReward.values()).contains(lotto.getLottoReward());
-		assertThat(lotto.getLottoReward()).isEqualTo(LottoReward.FOURTH);
 	}
 }
