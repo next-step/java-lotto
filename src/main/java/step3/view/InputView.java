1d96b9f77a22201399969c87f249089f0ea00b61
oen142 <oen142@naver.com>
2020-11-18 00:44:18 +0900

    feat : 보너스 숫자를 받을수 있는 뷰를 생성

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -6,6 +6,8 @@ public class InputView {
 
     private static final String PURCHASE_LOTTO = "구입금액을 입력해 주세요.";
     private static final String LAST_WEEK_LOTTO = "지난 주 당첨번호를 입력해 주세요.";
+    private static final String BONUS_BALL = "보너스 볼을 입력해 주세요.";
+
     private static final Scanner scanner = new Scanner(System.in);
     private InputView(){}
 
@@ -16,6 +18,10 @@ public class InputView {
     public static String lastWeekLotto(){
         System.out.println(LAST_WEEK_LOTTO);
         return scanner.next();
+    }
+    public static int initBonusBall(){
+        System.out.println(BONUS_BALL);
+        return scanner.nextInt();
     }
 
 }
