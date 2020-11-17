26dd62c5db793c4e7b5a890f58c862735207d3f1
이병무 <am003507@gmail.com>
2020-11-17 18:48:46 +0900

    styles - 같은 작업을 두번 하고 있던것을 제거

--- src/main/java/lotto/Operand.java
+++ src/main/java/lotto/Operand.java
@@ -4,10 +4,11 @@ public class Operand {
     private final Integer value;
 
     public Operand(String value) {
-        if (Integer.parseInt(value) < 0) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
             throw new IllegalArgumentException(ErrorMessage.DO_NOT_CREATE_NEGATIVE_OPERAND);
         }
-        this.value = Integer.valueOf(value);
+        this.value = number;
     }
 
     public Integer getValue() {
