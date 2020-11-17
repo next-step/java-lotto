59aaa802dfdcb14c28809024c3bd53d6278fa910
oen142 <oen142@naver.com>
2020-11-18 00:51:20 +0900

    feat : InputView 줄안바꿈 확인

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -9,17 +9,20 @@ public class InputView {
     private static final String BONUS_BALL = "보너스 볼을 입력해 주세요.";
 
     private static final Scanner scanner = new Scanner(System.in);
-    private InputView(){}
 
-    public static int purchaseLotto(){
+    private InputView() { }
+
+    public static int purchaseLotto() {
         System.out.println(PURCHASE_LOTTO);
         return scanner.nextInt();
     }
-    public static String lastWeekLotto(){
+
+    public static String lastWeekLotto() {
         System.out.println(LAST_WEEK_LOTTO);
         return scanner.next();
     }
-    public static int initBonusBall(){
+
+    public static int initBonusBall() {
         System.out.println(BONUS_BALL);
         return scanner.nextInt();
     }
