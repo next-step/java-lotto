fb4aef8510af238123c56fc1543aa9891590f598
이병무 <am003507@gmail.com>
2020-11-17 22:03:40 +0900

    refactor - 테스트 코드 @BeforeEach 추가

--- src/test/java/lotto_auto/model/LottoStoreTest.java
+++ src/test/java/lotto_auto/model/LottoStoreTest.java
@@ -1,6 +1,7 @@
 package lotto_auto.model;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoStoreTest {
 
+    private LottoStore lottoStore;
+
+    @BeforeEach
+    public void test() {
+        lottoStore = new LottoStore();
+    }
+
     @DisplayName("음수를 입력 받으면 예외 발생 테스트")
     @Test
     public void enterNegativeValueExceptionTest() {
-        LottoStore lottoStore = new LottoStore();
-
         assertThatThrownBy(
                 () -> lottoStore.buy(-1)
         ).isInstanceOf(IllegalArgumentException.class);
@@ -22,7 +28,6 @@ class LottoStoreTest {
     @DisplayName("LottoBundle 생성 테스트")
     @Test
     public void returnLottoBundleTest() {
-        LottoStore lottoStore = new LottoStore();
         LottoBundle lottoBundle = lottoStore.buy(1000);
         assertThat(lottoBundle).isNotNull();
     }
