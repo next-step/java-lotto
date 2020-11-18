3cda0d956740df1892217dddbd59b168d4d673d7
james.star <james.star@kakaocorp.com>
2020-11-18 22:42:38 +0900

    step1 - feat 계산가능한 사전 조건 체크 클래스 추가

---
+++ src/main/java/calculator/domain/StringPreConditionChecker.java
@@ -0,0 +1,17 @@
+package calculator.domain;
+
+public class StringPreConditionChecker {
+
+    public static boolean isRightString(String text) {
+
+        if (text == null) return false;
+
+        if (text.isEmpty()) return false;
+
+        if (text.contains("-")) {
+            throw new RuntimeException();
+        }
+
+        return true;
+    }
+}
