d00394e281ab876c031efc8bb360b2697e965678
nio.d <nio.d@kakaocorp.com>
2020-11-18 14:55:31 +0900

    refactor: step1 피드백반영. validation 로직 분리

--- src/main/java/calculator/MathExpression.java
+++ src/main/java/calculator/MathExpression.java
@@ -13,11 +13,16 @@ public class MathExpression {
      * @param mathExpression custom delimiter 가 포함되지 않은 수식 문자열
      */
     public MathExpression(String mathExpression, String delimiter) {
-         this.operands = Arrays.stream(mathExpression.split(delimiter))
+         List<Integer> operands = Arrays.stream(mathExpression.split(delimiter))
                  .map(Integer::valueOf)
                  .collect(Collectors.toList());
-         if(this.operands.stream().anyMatch(this::lessThanZero))
-             throw new RuntimeException("수식에는 0 또는 양수만 입력 가능합니다.");
+         this.operands = validateAndGet(operands);
+    }
+
+    private List<Integer> validateAndGet(List<Integer> operands) {
+        if(operands.stream().anyMatch(this::lessThanZero))
+            throw new RuntimeException("수식에는 0 또는 양수만 입력 가능합니다.");
+        return operands;
     }
 
     private boolean lessThanZero(Integer value) {
