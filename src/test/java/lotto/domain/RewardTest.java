f09f9f61aa36df24ae547a3cda4d31a9962d884e
DongHwan Kim <bkuekany@gmail.com>
2020-11-18 22:00:02 +0900

    feat : Add 당첨상태를 나타내는 Reward 클래스 추가

---
+++ src/test/java/lotto/domain/RewardTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RewardTest {
+
+
+    @Test
+    public void getRewardResult(){
+        //Given
+        Reward reward = new Reward(Arrays.asList(PrizeInformation.FIVE_MATCH,PrizeInformation.FOUR_MATCH));
+
+        //When
+        Map<PrizeInformation, Integer> result = reward.getRewardResult();
+
+        //Then
+        assertThat(result).isNotNull();
+    }
+
+    @DisplayName("수익률 테스트")
+    @Test
+    public void earningTest(){
+        //Given
+        int totalReward = PrizeInformation.THREE_MATCH.getPrizePrice();
+        int purchasePrice = 14000;
+        float expectRate = (float) totalReward / purchasePrice;
+
+        //When
+        Reward reward = new Reward(Arrays.asList(PrizeInformation.THREE_MATCH));
+        reward.getRewardResult();
+        float rate = reward.earningRate(14000);
+
+        //Then
+        assertThat(rate).isEqualTo(expectRate);
+
+
+    }
+}
