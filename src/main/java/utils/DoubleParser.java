c8056ede80308427b851ad50c561fc2503abedab
eyabc <bey4314@naver.com>
2020-11-18 21:57:27 +0900

    refactor: DoubleParser 생성자 방어

--- src/main/java/utils/DoubleParser.java
+++ src/main/java/utils/DoubleParser.java
@@ -1,6 +1,7 @@
 package utils;
 
 public class DoubleParser {
+    private DoubleParser() {};
     public static double getTwoDecimalPoint(double value) {
         return Math.floor(value * 100) / 100;
     }
