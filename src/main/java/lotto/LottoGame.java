cd5436cf54141436abf51b3be4b877bb03009d1e
DongHwan Kim <bkuekany@gmail.com>
2020-11-18 22:00:20 +0900

    feat : Add LottoGame Controller

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
+
+public class LottoGame {
+
+    public static void main(String[] args) {
+
+        int purchasePrice = InputView.inputPurchasePrice();
+        LottoTicket ticket = LottoShop.purchase(purchasePrice);
+        ResultView.outputPurchaseQuantity(ticket.getTicketQuantity());
+
+        Lottos lottos = LottoShop.exchangeToLotto(ticket, new LottoAutoMachine());
+        ResultView.outputExchangeLottos(lottos);
+
+        List<Integer> prizeNumbers = InputView.lastWeekLottoPrizeNumber();
+        lottos.matchPrizeNumber(prizeNumbers);
+
+        ResultView.outputPrizeStatistics(lottos.getReward());
+        ResultView.outputTotalEarningRate(lottos.getReward(), purchasePrice);
+
+
+    }
+}
