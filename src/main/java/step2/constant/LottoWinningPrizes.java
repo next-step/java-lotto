96dc957fccd50aa89b58bf6ea36cfe20c1877265
원유라 <ura.won@wadiz.kr>
2020-11-19 01:23:57 +0900

    feat : 로또 당첨 관련 enum 클래스 생성

---
+++ src/main/java/step2/constant/LottoWinningPrizes.java
@@ -0,0 +1,16 @@
+package step2.constant;
+
+public enum LottoWinningPrizes {
+    SIX_MATCHES(6, 2000000000),
+    FIVE_MATCHES(5, 1500000),
+    FOUR_MATCHES(4, 50000),
+    THIRD_MATCHES(3, 5000);
+
+    private int match;
+    private long amount;
+
+    LottoWinningPrizes(int match, long amount) {
+        this.match = match;
+        this.amount = amount;
+    }
+}
