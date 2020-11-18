c0ceca6bcdaf3ee954a5da175ced49e0e117506c
rlawls1991 <rlawls1991@gmail.com>
2020-11-18 22:37:27 +0900

    refactor 사용하지않는 view 파일 제거

--- src/main/java/step1/addCalculator/view/OuterResultView.java
+++
@@ -1,8 +0,0 @@
-package step1.addCalculator.view;
-
-public class OuterResultView {
-
-    public static void outerResult(int paramValue){
-        System.out.print("숫자들의 합은 : " + paramValue);
-    }
-}
