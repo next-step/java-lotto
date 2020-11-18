0ee18fa0e7ff02d87783e9b3faf41316eaf82dbb
DongHwan Kim <bkuekany@gmail.com>
2020-11-18 21:43:14 +0900

    style : style 정리

--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -8,10 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
-/**
- * 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
- * (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
- */
 public class StringCalculatorTest {
 
     @DisplayName("쉼표 덧셈 테스트 ")
