dacb7c056319c2eb4053b1f6c42b47ebc474c2d6
eyabc <bey4314@naver.com>
2020-11-18 15:45:40 +0900

    feat: NumberCountCollections 필터링 테스트 및 기능 구현

--- src/main/java/step02/domain/NumberCountCollections.java
+++ src/main/java/step02/domain/NumberCountCollections.java
@@ -2,6 +2,7 @@ package step02.domain;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class NumberCountCollections {
     private final List<NumberCount> numbersCount;
@@ -12,6 +13,12 @@ public class NumberCountCollections {
 
     public static NumberCountCollections of(List<NumberCount> numberCounts) {
         return new NumberCountCollections(numberCounts);
+    }
+
+    public List<NumberCount> filter(int from, int to) {
+        return numbersCount.stream()
+                .filter(numberCount -> numberCount.filter(from, to))
+                .collect(Collectors.toList());
     }
 
     @Override
--- src/test/java/step02/domain/NumberCountCollectionsTest.java
+++ src/test/java/step02/domain/NumberCountCollectionsTest.java
@@ -1,5 +1,6 @@
 package step02.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,16 +10,32 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class NumberCountCollectionsTest {
-    @DisplayName("NumberCountCollections 생성자 테스트")
-    @Test
-    public void test_NumberCountCollections_Constructor() {
-        List<NumberCount> numbersCount = Arrays.asList(
+    List<NumberCount> numbersCount;
+
+    @BeforeEach
+    public void setup() {
+        numbersCount = Arrays.asList(
                 NumberCount.of(0, 7),
                 NumberCount.of(1, 5),
                 NumberCount.of(2, 1),
                 NumberCount.of(3, 1)
         );
+    }
+
+    @DisplayName("NumberCountCollections 생성자 테스트")
+    @Test
+    public void test_NumberCountCollections_Constructor() {
         assertThat(NumberCountCollections.of(numbersCount))
                 .isEqualTo(NumberCountCollections.of(numbersCount));
+    }
+
+    @DisplayName("NumberCountCollections 필터링 테스트")
+    @Test
+    public void test_filter() {
+        NumberCountCollections numberCountCollections = NumberCountCollections.of(numbersCount);
+        assertThat(numberCountCollections.filter(3, 6))
+                .isEqualTo(
+                    Arrays.asList(NumberCount.of(3, 1))
+                );
     }
 }
