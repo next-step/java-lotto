e20060b5803e5d7fe7a065d4ec244291d62e2d17
eyabc <bey4314@naver.com>
2020-11-18 21:34:49 +0900

    refactor: 에러 메시지 수정

--- src/main/java/exception/ExceptionMessage.java
+++ src/main/java/exception/ExceptionMessage.java
@@ -3,5 +3,5 @@ package exception;
 public class ExceptionMessage {
     private ExceptionMessage() {}
 
-    public static final String LESS_THEN_1_EXCEPTION_MESSAGE = "금액이 1원보다 작습니다.";
+    public static final String LESS_THEN_1_EXCEPTION_MESSAGE = "최소 금액은 0원 이상입니다.";
 }
