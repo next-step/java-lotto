6656fcd1f42b72d2187656eadf6c2f52b6c75d14
SungHyeob Baek <baek9652@naver.com>
2020-11-18 15:54:36 +0900

    [feat] LottoTickets에 출력기능 추가

--- src/main/java/lotto/LottoTickets.java
+++ src/main/java/lotto/LottoTickets.java
@@ -10,6 +10,18 @@ public class LottoTickets {
     this.tickets = tickets;
   }
 
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+
+    for (LottoTicket lottoTicket : tickets) {
+      builder.append(lottoTicket);
+      builder.append("\n");
+    }
+
+    return builder.toString();
+  }
+
   public static LottoTickets of(List<LottoTicket> tickets) {
     if (tickets == null) {
       throw new NullPointerException();
--- src/test/java/lotto/LottoTicketsTest.java
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,4 +17,18 @@ class LottoTicketsTest {
         () -> LottoTickets.of(null)
     );
   }
+
+  @Test
+  @DisplayName("티켓 리스트 출력 기능(toString())")
+  void testToString() {
+    LottoTicket sampleTicket = NumberPool.publishTicket();
+    List<LottoTicket> t = new ArrayList<>();
+    for (int i = 0; i < 2; i++) {
+      t.add(sampleTicket);
+    }
+    LottoTickets tickets = LottoTickets.of(t);
+    assertThat(tickets.toString())
+        .isEqualTo(sampleTicket.toString() + '\n' + sampleTicket.toString() + "\n");
+  }
+
 }
