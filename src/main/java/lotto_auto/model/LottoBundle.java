175d365e5f514be4b28b5a508bc16d409c7993dc
이병무 <am003507@gmail.com>
2020-11-18 21:52:13 +0900

    feat - LottoBundle 구현

--- src/main/java/lotto_auto/model/LottoBundle.java
+++ src/main/java/lotto_auto/model/LottoBundle.java
@@ -1,17 +1,34 @@
 package lotto_auto.model;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoBundle {
 
+    private List<LottoTicket> lottoTicketList;
+
     public LottoBundle(int ticketCount) {
+        throwIfNegativeTicketCount(ticketCount);
+        this.lottoTicketList = Collections.unmodifiableList(
+                IntStream.range(0, ticketCount)
+                        .mapToObj(item -> new LottoTicket())
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private void throwIfNegativeTicketCount(int ticketCount) {
+        if (ticketCount <= 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getTicketCount() {
-        return -1;
+        return lottoTicketList.size();
     }
 
     public List<LottoTicket> export() {
-        return null;
+        return lottoTicketList;
     }
 }
