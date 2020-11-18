a097b8fc4f0b92c188ccd8de27c64dc4c65299eb
oen142 <oen142@naver.com>
2020-11-18 23:15:01 +0900

    feat test : LastWeekLottoGenerator 의 변경에 따른 테스트 클래스 변경

---
+++ src/test/java/step3/domain/LastWeekLottoGeneratorTest.java
@@ -0,0 +1,66 @@
+package step3.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import step3.exception.LottoCountBoundException;
+import step3.exception.LottoNumberRangeException;
+import step3.exception.ValidEmptyException;
+import step3.exception.ValidNullException;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LastWeekLottoGeneratorTest {
+
+    @DisplayName("지난주 로또 번호가 잘 생성되는지 확인합니다.")
+    @ParameterizedTest
+    @MethodSource("createStringLotto")
+    void createLastWeekLotto(String input, Lotto expected) {
+        assertThat(LastWeekLottoGenerator.separateLottoToList(input)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> createStringLotto() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                Arguments.of("11,12,13,14,15,16", new Lotto(Arrays.asList(11, 12, 13, 14, 15, 16))),
+                Arguments.of("21,22,23,24,25,26", new Lotto(Arrays.asList(21, 22, 23, 24, 25, 26)))
+        );
+    }
+
+    @Test
+    @DisplayName("로또 번호가 5개 이하일 경우 익셉션이 발생합니다.")
+    void throwLottoCount5() {
+        assertThatThrownBy(() -> LastWeekLottoGenerator.separateLottoToList("1,2,3,4,5"))
+                .isInstanceOf(LottoCountBoundException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 5개 이하일 경우 익셉션이 발생합니다.")
+    void throwLottoCount7() {
+        assertThatThrownBy(() -> LastWeekLottoGenerator.separateLottoToList("1,2,3,4,5,6,7"))
+                .isInstanceOf(LottoCountBoundException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호가 널일경우 익셉션이 발생합니다.")
+    void throwLottoNull() {
+        assertThatThrownBy(() -> LastWeekLottoGenerator.separateLottoToList(null))
+                .isInstanceOf(ValidNullException.class);
+    }
+    @Test
+    @DisplayName("로또 번호가 5개 이하일 경우 익셉션이 발생합니다.")
+    void throwLottoEmpty() {
+        assertThatThrownBy(() -> LastWeekLottoGenerator.separateLottoToList(" "))
+                .isInstanceOf(ValidEmptyException.class);
+    }
+
+
+}
