9c8152693308942babf59a0a362cf1779d8f0268
원유라 <ura.won@wadiz.kr>
2020-11-19 23:16:12 +0900

    refactor : output ui 당첨 통계 메서드 수정

--- src/main/java/step2/view/LottoOuputUi.java
+++ src/main/java/step2/view/LottoOuputUi.java
@@ -1,23 +1,32 @@
 package step2.view;
 
+import step2.constant.LottoWinningPrizes;
+import step2.domain.Lotto;
+
+import java.util.HashMap;
 import java.util.List;
 
 public class LottoOuputUi {
-    public void printLottoCount(String lottoPurchaseQty) {
+
+    public static void printLottos(List<Lotto> lottos) {
+        lottos.stream()
+                .map(Lotto::getLottos)
+                .forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static void printLottoCount(int lottoPurchaseQty) {
         System.out.println(lottoPurchaseQty + "개를 구매했습니다.");
     }
 
-    public void printWinningStatistics(List<Integer> winningCount) {
+    public static void printWinningStatistics(HashMap<LottoWinningPrizes, Integer> lottoWinningPrizesMap) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-
-//        System.out.println("3개 일치 (5000원) - " + winningCount.get(0) + "개");
-//        System.out.println("4개 일치 (50000원) -" + +"개");
-//        System.out.println("5개 일치 (1500000원)-"++"개");
-//        System.out.println("6개 일치 (2000000000원) - "++"개");
+        lottoWinningPrizesMap.forEach((key, value) ->
+                System.out.println(key.getMatch() + "개 일치 (" + key.getAmount() + "원) - " + value +"개"));
     }
 
-    public void printTotalReturn(int yield) {
+    public static void printTotalReturn(double yield) {
         System.out.println("총 수익률은" + yield + "입니다");
     }
 }
