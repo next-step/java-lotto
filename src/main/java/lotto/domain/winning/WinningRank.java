5163c10f919b9c4091f4af80eb264517b7df53b6
mand2 <nayeon.kim1123@gmail.com>
2020-11-20 01:07:04 +0900

    feat : WinningRank enum 객체 생성

---
+++ src/main/java/lotto/domain/winning/WinningRank.java
@@ -0,0 +1,44 @@
+package lotto.domain.winning;
+
+import java.util.Arrays;
+
+/**
+ * Created By mand2 on 2020-11-20.
+ */
+public enum WinningRank {
+
+    FIRST(6, 2000000000, 1),
+    THIRD(5, 1500000, 3),
+    FOURTH(4, 50000, 4),
+    FIFTH(3, 5000, 5),
+    NONE(2, 0, 6);
+
+    private int winningResult;
+    private int prize;
+    private int rank;
+
+    WinningRank(int winningResult, int prize, int rank) {
+        this.winningResult = winningResult;
+        this.prize = prize;
+        this.rank = rank;
+    }
+
+    public static WinningRank getWinningRank(int winningResult) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.winningResult == winningResult)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getWinningResult() {
+        return winningResult;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+}
--- src/test/java/lotto/domain/winning/WinningCheckerTest.java
+++ src/test/java/lotto/domain/winning/WinningCheckerTest.java
@@ -26,9 +26,9 @@ class WinningCheckerTest {
     @BeforeEach
     void setUp() {
         Lotto lotto1 = Lotto.of(LottoNumber.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        Lotto lotto2 = Lotto.of(LottoNumber.of(Arrays.asList(1, 12, 13, 14, 15, 16)));
-        Lotto lotto3 = Lotto.of(LottoNumber.of(Arrays.asList(1, 22, 23, 24, 25, 26)));
-        Lotto lotto4 = Lotto.of(LottoNumber.of(Arrays.asList(1, 32, 33, 34, 35, 36)));
+        Lotto lotto2 = Lotto.of(LottoNumber.of(Arrays.asList(1, 2, 3, 14, 15, 16)));
+        Lotto lotto3 = Lotto.of(LottoNumber.of(Arrays.asList(1, 2, 3, 24, 25, 26)));
+        Lotto lotto4 = Lotto.of(LottoNumber.of(Arrays.asList(1, 2, 3, 34, 35, 36)));
 
         lottos = Lottos.of(Arrays.asList(lotto1, lotto2, lotto3, lotto4));
     }
@@ -36,7 +36,7 @@ class WinningCheckerTest {
     @ParameterizedTest
     @DisplayName("당첨번호와 내가 산 로또번호를 비교한다")
     @CsvSource(value = { "1, 42, 41, 42, 43, 44=1" }, delimiter = '=')
-    void t(String inputWinning, int expected) {
+    void comapre_winningNumber_to_my_lotto(String inputWinning, int expected) {
         // given
         WinningNumber winningNumber = WinningNumber.of(inputWinning);
 
@@ -46,6 +46,24 @@ class WinningCheckerTest {
         // then
         for (Lotto lotto : lottos.list()) {
             assertThat(expected).isEqualTo(lotto.winningResult());
+        }
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("당첨개수로 결과값을 찾는다.")
+    @CsvSource(value = { "1, 2, 3, 42, 43, 44=5", "1, 42, 41, 42, 43, 44=6" }, delimiter = '=')
+    void convert_winningResult_to_winning_rank(String inputWinning, int expectedRank) {
+        // given
+        WinningNumber winningNumber = WinningNumber.of(inputWinning);
+
+        // when
+        lottos.checkWinningResult(winningNumber);
+
+        // then
+        for (Lotto lotto : lottos.list()) {
+            WinningRank winningRank = WinningRank.getWinningRank(lotto.winningResult());
+            assertThat(expectedRank).isEqualTo(winningRank.getRank());
         }
     }
 
