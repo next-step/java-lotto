1ab9c535fbe7f105661092617912b6dd4f446682
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-20 02:47:00 +0900

    Feature : LottoRank 보너스 번호를 포함한 등수를 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+
+import lotto.utils.Pair;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
@@ -9,28 +12,47 @@ import java.util.stream.Collectors;
 
 public enum LottoRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5000),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NONE(0, 0);
 
-    private static final Map<Integer, LottoRank> RANK_MATCH_COUNT_MAP = Collections.unmodifiableMap(
+    private static final Map<Pair<Integer, Boolean>, LottoRank> RANK_MATCH_COUNT_MAP = Collections.unmodifiableMap(
             Arrays.stream(LottoRank.values()).collect(
-                    Collectors.toMap(value -> value.matchCount, Function.identity())));
+                    Collectors.toMap(value -> new Pair<>(value.matchCount, value.matchBonus), Function.identity())));
 
-    private int matchCount;
-    private int reward;
+    private static final int ONLY_INCLUDE_BONUS_MATCH_COUNT = 5;
+
+    private final int matchCount;
+    private final int reward;
+    private final boolean matchBonus;
 
     LottoRank(int matchCount, int reward) {
         this.matchCount = matchCount;
         this.reward = reward;
+        this.matchBonus = false;
     }
 
-    public static LottoRank of(int count) {
-        if (Objects.isNull(RANK_MATCH_COUNT_MAP.get(count))) {
+    LottoRank(int matchCount, int reward, boolean matchBonus) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+        this.matchBonus = matchBonus;
+    }
+
+    public static LottoRank of(int matchCount, boolean matchBonus) {
+        Pair<Integer, Boolean> matchKey = buildMatchKey(matchCount, matchBonus);
+        if (Objects.isNull(RANK_MATCH_COUNT_MAP.get(matchKey))) {
             return LottoRank.NONE;
         }
-        return RANK_MATCH_COUNT_MAP.get(count);
+        return RANK_MATCH_COUNT_MAP.get(matchKey);
+    }
+
+    public static Pair<Integer, Boolean> buildMatchKey(int matchCount, boolean matchBonus) {
+        if (matchCount == ONLY_INCLUDE_BONUS_MATCH_COUNT) {
+            return new Pair<>(matchCount, matchBonus);
+        }
+        return new Pair<>(matchCount, false);
     }
 
     public int getMatchCount() {
--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -11,16 +11,23 @@ public class LottoRankTest {
     @DisplayName("matchCount에 따른 LottoRank 생성 테스트")
     @ParameterizedTest
     @CsvSource(value = {
-            " 0 | NONE ",
-            " 1 | NONE ",
-            " 2 | NONE ",
-            " 3 | FOURTH ",
-            " 4 | THIRD ",
-            " 5 | SECOND ",
-            " 6 | FIRST ",
+            " 0 | false | NONE ",
+            " 0 |  true | NONE ",
+            " 1 | false | NONE ",
+            " 1 |  true | NONE ",
+            " 2 | false | NONE ",
+            " 2 |  true | NONE ",
+            " 3 | false | FIFTH ",
+            " 3 |  true | FIFTH ",
+            " 4 | false | FOURTH ",
+            " 4 |  true | FOURTH ",
+            " 5 | false | THIRD ",
+            " 5 |  true | SECOND ",
+            " 6 | false | FIRST ",
+            " 6 |  true | FIRST ",
     }, delimiter = '|')
-    void of(int matchCount, LottoRank exptectedLottoRank) {
-        LottoRank lottoRank = LottoRank.of(matchCount);
+    void of(int matchCount, boolean matchBonus, LottoRank exptectedLottoRank) {
+        LottoRank lottoRank = LottoRank.of(matchCount, matchBonus);
         assertThat(lottoRank).isEqualTo(exptectedLottoRank);
     }
 }
