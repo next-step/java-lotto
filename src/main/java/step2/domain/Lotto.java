4e627b6ceaea00392e0334a2a599ac0fa603ac90
원유라 <ura.won@wadiz.kr>
2020-11-19 22:49:07 +0900

    feat : 로또 객체 생성

---
+++ src/main/java/step2/domain/Lotto.java
@@ -0,0 +1,36 @@
+package step2.domain;
+
+import java.util.*;
+
+public class Lotto {
+    private static final int QTY = 6;
+    private static final int LOTTO_VALUE = 45;
+    private List<Integer> lottos;
+
+    public void addNumber() {
+        autoLottoNumber();
+        shuffle();
+        sort();
+    }
+
+    private void autoLottoNumber() {
+        Set<Integer> set = new HashSet<>();
+        while (QTY > set.size()) {
+            set.add((int) (Math.random() * LOTTO_VALUE) + 1);
+        }
+        this.lottos = new ArrayList<>(set);
+    }
+
+    public List<Integer> getLottos() {
+        return this.lottos;
+    }
+
+    private void shuffle() {
+        Collections.shuffle(this.lottos, new Random());
+    }
+
+    private void sort() {
+        Collections.sort(this.lottos);
+    }
+
+}
