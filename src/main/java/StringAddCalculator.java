38c5a5664edb73660def6eaf05a2e84125325003
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-19 23:52:49 +0900

    feat : 빈 문자열, null 값을 입력하는 경우 0을 리턴

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,5 +1,12 @@
 public class StringAddCalculator {
     public Integer add(String input) {
+        if (isEmptyValue(input)) {
+            return 0;
+        }
         return null;
+    }
+
+    private boolean isEmptyValue(String input) {
+        return input == null || input.isEmpty();
     }
 }
