2bbad56a7812c9729eabc870d7808fc30d6c62aa
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-20 00:01:36 +0900

    test : 숫자 하나를 입력하는 경우 해당 숫자를 리턴하는 테스트

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,5 +20,12 @@ public class StringAddCalculatorTest {
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0) ")
     void empty_or_null_test(String input) {
         assertThat(calculator.add(input)).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "3:3"}, delimiter = ':')
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)")
+    void single_number_test(String input, int result) {
+        assertThat(calculator.add(input)).isEqualTo(result);
     }
 }
