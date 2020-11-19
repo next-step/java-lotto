8f224efd1446408fececac6481ba3393b9c2b64c
원유라 <ura.won@wadiz.kr>
2020-11-20 00:57:53 +0900

    refactor : lottoWinningPrizes getter 위치 변경

--- src/main/java/step2/constant/LottoWinningPrizes.java
+++ src/main/java/step2/constant/LottoWinningPrizes.java
@@ -6,20 +6,20 @@ public enum LottoWinningPrizes {
     FOUR_MATCHES(4, 50000),
     THIRD_MATCHES(3, 5000);
 
-    public int getMatch() {
-        return match;
-    }
-
-    public long getAmount() {
-        return amount;
-    }
-
     private int match;
     private long amount;
 
     LottoWinningPrizes(int match, long amount) {
         this.match = match;
         this.amount = amount;
+    }
+
+    public int getMatch() {
+        return match;
+    }
+
+    public long getAmount() {
+        return amount;
     }
 
 }
