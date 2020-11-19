b0bca2397c90a77575db9cfc583f30cd4b5d565d
원유라 <ura.won@wadiz.kr>
2020-11-20 01:14:11 +0900

    feat : 로또 util 클래스 추가

---
+++ src/main/java/step2/util/LottoUtil.java
@@ -0,0 +1,25 @@
+package step2.util;
+
+public class LottoUtil {
+
+    public static void purchaseAmountValidate(int amount) {
+        if (amount == 0) {
+            throw new IllegalArgumentException("구입 금액 값이 없습니다.");
+        }
+
+        if (amount < 1000) {
+            throw new IllegalArgumentException("구입 금액은 1000원부터 입력가능합니다.");
+        }
+    }
+
+    public static void lottoNumberSizeValidate(String lastWeekNumber) {
+        if (lastWeekNumber == null) {
+            throw new IllegalArgumentException("번호를 입력해 주세요.");
+        }
+        String[] lottoNumbers = lastWeekNumber.split(",");
+        if(lottoNumbers.length != 6) {
+            throw new IllegalArgumentException("로또 번호는 6보다 클 수 없습니다.");
+        }
+
+    }
+}
