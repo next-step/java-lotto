6dc2757998caa20fb8656ad813cea5928fddb1f1
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-20 00:05:54 +0900

    feat : 숫자 하나를 입력하는 경우 해당 숫자를 리턴

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,9 +1,19 @@
 public class StringAddCalculator {
+
     public Integer add(String input) {
         if (isEmptyValue(input)) {
             return 0;
         }
+
+        if (isSingleNumber(input)) {
+            return Integer.parseInt(input);
+        }
+
         return null;
+    }
+
+    private boolean isSingleNumber(String input) {
+        return input.length() == 1;
     }
 
     private boolean isEmptyValue(String input) {
