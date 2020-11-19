a1632f428e1d46daf93127f6cd397c73c2845602
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-20 01:46:16 +0900

    Feature : 당첨 번호 Winning 도메인 보너스 번호 추가,  당첨 결과에 보너스 번호 추가

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.dto.AnnounceWinning;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,27 +10,35 @@ import java.util.stream.Collectors;
 public class Winning {
 
     private final Set<LottoNumber> winningNumbers;
+    private final LottoNumber bonusNumber;
 
-    private Winning(Set<LottoNumber> winningNumbers) {
+    private Winning(Set<LottoNumber> winningNumbers, LottoNumber bonusNumber) {
         this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public static Winning of(AnnounceWinning announceWinning) {
         return new Winning(announceWinning.getAnnounceWinning().stream()
                                    .map(LottoNumber::of)
-                                   .collect(Collectors.toSet()));
+                                   .collect(Collectors.toSet()),
+                           LottoNumber.of(announceWinning.getBonusNumber()));
     }
 
     public List<LottoRank> scratch(Lottos lottos) {
-        return lottos.getLottos().stream()
-                .map(this::calculateMatchCount)
-                .map(LottoRank::of)
-                .collect(Collectors.toList());
+        List<LottoRank> lottoRanks = new ArrayList<>();
+        for (Lotto lotto : lottos.getLottos()) {
+            lottoRanks.add(LottoRank.of(calculateMatchCount(lotto), isMatchingBonus(lotto)));
+        }
+        return lottoRanks;
     }
 
     private int calculateMatchCount(Lotto lotto) {
         return (int) winningNumbers.stream()
                 .filter(lotto::contains)
                 .count();
+    }
+
+    private boolean isMatchingBonus(Lotto lotto) {
+        return lotto.contains(bonusNumber);
     }
 }
