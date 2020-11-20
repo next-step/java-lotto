dc1a86af78a284a10b1cc01ffa79679a022e0fcf
DongHwan Kim <bkuekany@gmail.com>
2020-11-21 02:02:18 +0900

    fix : change message

--- src/main/java/lotto/domain/exception/ErrorMessage.java
+++ src/main/java/lotto/domain/exception/ErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.domain.exception;
 public enum ErrorMessage {
 
     NOT_VALID_PRICE("원 만큼 더 필요합니다"),
-    NOT_VALID_LOTTO_NUMBER("유효한 로또번호입니다");
+    NOT_VALID_LOTTO_NUMBER("유효하지 않는 로또번호입니다");
 
     private final String message;
 
