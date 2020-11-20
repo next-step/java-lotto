f5978d302e286b844e8ac31500ef36c225a76ce5
dbeod2 <darung.0211@gmail.com>
2020-11-20 18:40:46 +0900

    refactor : 로또 객체 생성시 로또 숫자 메서드 실행으로 변경

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -3,14 +3,18 @@ package step2.domain;
 import java.util.*;
 
 public class Lotto {
+
     private static final int QTY = 6;
     private static final int LOTTO_VALUE = 45;
+
     private List<Integer> lottos = new ArrayList<>();
 
-    public void addNumber() {
+    public Lotto() {
         autoLottoNumber();
-        shuffle();
-        sort();
+    }
+
+    public void lottoSort() {
+        Collections.sort(this.lottos);
     }
 
     private void autoLottoNumber() {
@@ -23,14 +27,6 @@ public class Lotto {
 
     public List<Integer> getLottos() {
         return this.lottos;
-    }
-
-    private void shuffle() {
-        Collections.shuffle(this.lottos, new Random());
-    }
-
-    private void sort() {
-        Collections.sort(this.lottos);
     }
 
 }
