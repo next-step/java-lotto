d2d9ca7e44248e90e0c3a700b3ac11a077dd6222
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-20 19:41:24 +0900

    style

--- src/main/java/step2/lotto/automatic/domain/LottoMatch.java
+++ src/main/java/step2/lotto/automatic/domain/LottoMatch.java
@@ -19,7 +19,7 @@ public class LottoMatch {
     private static String NOT_GOOD_EARNING_RATE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";// 나쁜수익률
 
     private static final int LOTTO_WINNING_PRICE_RANK = 4; //로또 상금 받을 수 있는 등수
-    private static final int MAKEAUTONUMBER = 6; // 로또 맞추야되는 번호 갯수
+    private static final int MAKEAUTONUMBER = 6; // 한게임당 필요한 번호 갯수
     private static final int MAKE_LOTTONUMBER_COUNT = 45; // 로또번호 생성 갯수
     private static final int LOTTO_PRICE = 1000; //로또 가격
     private int lottoPurchaseCount = 0; //로또 구매 갯수
