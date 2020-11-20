5c390800df4d37c5ce648c15d79e5b8c908b8678
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-20 10:39:26 +0900

    [refactor] : 정규식 오류 수정

--- src/main/java/lotto/view/InputViewConfig.java
+++ src/main/java/lotto/view/InputViewConfig.java
@@ -7,7 +7,7 @@ public class InputViewConfig {
     public static final String INPUT_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.\n";
     public static final String INPUT_EMPTY_ERROR_MESSAGE = "오류:: 로또 번호에 빈값을 넣을 수 없습니다.\n";
     public static final String INPUT_ARRAY_SIZE_ERROR_MESSAGE = "오류:: 로또 번호는 숫자 6개를 입력해야 합니다.\n";
-    public static final String NUMBER_REGEX = "^[0-9]";
+    public static final String NUMBER_REGEX = "^[0-9]+";
 
     private InputViewConfig() {}
 }
