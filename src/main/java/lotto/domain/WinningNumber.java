cfd384d7fd961783df0684d63657af912299f424
sehan <sehan.choi@gmail.com>
2020-11-20 13:46:16 +0900

    refactor: LottoNumberParser 에 Exception Translation 적용

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,66 +1,44 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class WinningNumber {
 
-    private final int bonusNumber;
     private List<Integer> numbers;
 
     private LottoNumberParser lottoNumberParser = new LottoNumberParser();
 
-    public WinningNumber(List<Integer> numbers, int bonusNumber) {
+    public WinningNumber(List<Integer> numbers) {
         this.numbers = numbers;
-        this.bonusNumber = getBonusNumberIfValid(bonusNumber);
     }
 
-    public WinningNumber(String winningNumberExp, int bonusNumber) {
-        parse(winningNumberExp);
-        this.bonusNumber = getBonusNumberIfValid(bonusNumber);
-    }
-
-    private int getBonusNumberIfValid(int bonusNumber) {
-        if (numbers.contains(bonusNumber)) throw new InvalidBonusNumberException(bonusNumber);
-        if (bonusNumber < 1 || bonusNumber > 45) throw new InvalidBonusNumberException(bonusNumber);
-        return bonusNumber;
+    public WinningNumber(String expression) {
+        parse(expression);
     }
 
     private void parse(String expression) {
         try {
-            numbers = lottoNumberParser.parse(expression);
-        } catch (RuntimeException e) {
-            throw new InvalidWinningNumberException(expression, e.getCause() != null ? e.getCause() : e);
+            this.numbers = lottoNumberParser.parse(expression);
+        } catch (LottoNumberParseException e) {
+            throw new InvalidWinningNumberException(expression, e);
         }
     }
 
-    public int getMatchedWinningNumberCount(Lotto lotto) {
-        return lotto.getMatchedNumberCount(winningNumberMatcher());
-    }
-
-    public boolean matchBonusNumber(Lotto lotto) {
-        return lotto.getMatchedNumberCount(bonusNumberMatcher()) == 1;
-    }
-
-    private NumberMatcher winningNumberMatcher() {
-        return new ListNumberMatcher(numbers);
-    }
-
-    private NumberMatcher bonusNumberMatcher() {
-        return new ListNumberMatcher(Arrays.asList(bonusNumber));
+    public int getMatchedNumberCount(Lotto lotto) {
+        return lotto.getMatchedNumberCount(new WinningNumberMatcher(numbers));
     }
 }
 
-class ListNumberMatcher implements NumberMatcher {
+class WinningNumberMatcher implements NumberMatcher {
 
     private List<Integer> numbers;
 
-    public ListNumberMatcher(List<Integer> numbers) {
+    public WinningNumberMatcher(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
     @Override
     public boolean match(Integer number) {
-        return numbers.contains(number);
+        return this.numbers.contains(number);
     }
 }
