92326be175d6da46dcd2dd6d70ec6cc8460a2c72
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-20 19:31:04 +0900

    feat 로또 자동생성 Test

---
+++ src/test/java/step2/lotto/automatic/domain/LottoAutoNumbersTest.java
@@ -0,0 +1,24 @@
+package step2.lotto.automatic.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoAutoNumbersTest {
+
+    private static LottoAutoNumbers lottoAutoNumbersTest;
+
+    @Test
+    public void 자동으로_생성된_번호가_6개가_맞는지() {
+        assertThatThrownBy(() ->
+                lottoAutoNumbersTest = new LottoAutoNumbers(7)
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void 로또번호가제대로생성되었는지() {
+        lottoAutoNumbersTest = new LottoAutoNumbers(45);
+        assertThat(lottoAutoNumbersTest.getAutoNumbers(45).size()).isEqualTo(45);
+    }
+}
