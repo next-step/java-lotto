1aa59986955e791b12fc96e022e19f3852080fb7
MJ Choi <1621139@gmail.com>
2020-11-20 15:50:04 +0900

    feature: ADD ErrorMessage

---
+++ src/main/java/step2/util/ErrorMessage.java
@@ -0,0 +1,10 @@
+package step2.util;
+
+public class ErrorMessage {
+    public static final String MORE_THAN_THOUSAND = "로또는 1000원 이상시 구매 가능 합니다.";
+    public static final String INPUT_NUMBER_ERROR = "로또 숫자는 1이상 45이하만 가능 합니다.";
+    public static final String INPUT_SIZE_ERROR = "로또 숫자는 6개만 가능 합니다.";
+
+    private ErrorMessage() {
+    }
+}
