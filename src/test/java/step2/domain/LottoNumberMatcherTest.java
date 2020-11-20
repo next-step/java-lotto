3f4fce94ffb40f2b761e322774e0e8361422f3df
MJ Choi <1621139@gmail.com>
2020-11-21 02:19:30 +0900

    test: ADD double winning lotto test

--- src/test/java/step2/domain/LottoNumberMatcherTest.java
+++ src/test/java/step2/domain/LottoNumberMatcherTest.java
@@ -16,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoNumberMatcherTest {
     List<List<Integer>> lottoNumbers = new ArrayList<>();
     String[] winningLottoNumbers = new String[]{"1", "2", "3", "4", "5", "6"};
+    String[] doubleWinningLottoNumbers = new String[]{"1", "2", "3", "10","11","13"};
     private static final int FOURTH_PRIZE = 5000;
     private static final int THIRD_PRIZE = 50000;
     private static final int SECOND_PRIZE = 1500000;
@@ -24,6 +25,18 @@ public class LottoNumberMatcherTest {
     @BeforeEach
     void setUp() {
         lottoNumbers.add(Arrays.asList(1, 2, 3, 7, 8, 9));
+        lottoNumbers.add(Arrays.asList(10,11,13,15,17,18));
+    }
+
+    @DisplayName("두개의 로또가 당첨되는 경우 테스트")
+    @Test
+    void doubleWinningLottoNumbersTest(){
+        LottoNumberMatcher.getNumberMatch(doubleWinningLottoNumbers, lottoNumbers);
+        HashMap<Integer, Integer> ranks = LottoNumberMatcher.getEachPrizeMoney();
+        assertThat(ranks.get(FOURTH_PRIZE)).isEqualTo(2);
+        assertThat(ranks.get(THIRD_PRIZE)).isEqualTo(0);
+        assertThat(ranks.get(SECOND_PRIZE)).isEqualTo(0);
+        assertThat(ranks.get(FIRST_PRIZE)).isEqualTo(0);
     }
 
     @DisplayName("Rank에 따른 개수 가져오기 테스트")
