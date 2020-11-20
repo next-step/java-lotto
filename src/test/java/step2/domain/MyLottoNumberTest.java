30a75ba70a2fb233172ac20d4d0ccb1e8b8d9cec
MJ Choi <1621139@gmail.com>
2020-11-21 02:23:40 +0900

    test: DELETE didn't use variable about duplicatedNumber

--- src/test/java/step2/domain/MyLottoNumberTest.java
+++ src/test/java/step2/domain/MyLottoNumberTest.java
@@ -9,16 +9,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class MyLottoNumberTest {
     private List<Integer> myNumbers = new ArrayList<>();
-    private List<Integer> duplicatedNumber = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         myNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        duplicatedNumber = Arrays.asList(20, 20, 21, 23, 24, 25);
     }
 
     @DisplayName("내 로또 번호 갯수 유효성")
