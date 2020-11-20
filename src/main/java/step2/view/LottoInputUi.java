890ced7249ee74d1b45d43e39c0b00a58a21837b
dbeod2 <darung.0211@gmail.com>
2020-11-20 18:43:15 +0900

    refactor : 상수 대문자로 변경

--- src/main/java/step2/view/LottoInputUi.java
+++ src/main/java/step2/view/LottoInputUi.java
@@ -3,20 +3,20 @@ package step2.view;
 import java.util.Scanner;
 
 public class LottoInputUi {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
     public static String inputLastWeekNumber() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return scanner.next();
+        return SCANNER.next();
     }
 
     public static void close() {
-        scanner.close();
+        SCANNER.close();
     }
 
 }
