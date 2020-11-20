161a7ec0cc3664ae710652f04366ce70fe08b8c7
DongHwan Kim <bkuekany@gmail.com>
2020-11-21 01:58:22 +0900

    refactor : Refactor 명확한 숫자 표기를 위한 api 변경

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -8,9 +8,10 @@ public abstract class LottoMachine {
 
     protected static final int MAX_BOUND = 6;
     protected static final List<Integer> numbers = new ArrayList<>();
+    private static final int MAX_LOTTO_NUMBER_RANGE = 45;
 
     static {
-        IntStream.range(1, 46).forEach(numbers::add);
+        IntStream.rangeClosed(1, MAX_LOTTO_NUMBER_RANGE).forEach(numbers::add);
     }
 
     abstract List<Integer> createLottoNumber();
