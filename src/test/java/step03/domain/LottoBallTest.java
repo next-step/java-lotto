bb602f6ce69ae5e118976b79b86fec4a0edc7a46
eyabc <bey4314@naver.com>
2020-11-21 15:52:43 +0900

    test: 로또 번호 범위 예외 처리

--- src/test/java/step03/domain/LottoBallTest.java
+++ src/test/java/step03/domain/LottoBallTest.java
@@ -1,9 +1,16 @@
 package step03.domain;
 
+import exception.OutOfLottoNumberRangeException;
+import exception.OutOfLottoNumberRangeExceptionTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LottoBallTest {
 
@@ -11,6 +18,22 @@ public class LottoBallTest {
     @Test
     void test_constructor() {
         assertThat(LottoBall.of(1)).isEqualTo(LottoBall.of(1));
+    }
+
+    private static Stream<Integer> provideOutOfRangeResult() {
+        return Stream.of(
+                -1, 0, 46
+        );
+    }
+
+    @DisplayName("OutOfRange 예외 던짐")
+    @ParameterizedTest
+    @MethodSource("provideOutOfRangeResult")
+    void test_validate(int number) {
+        assertThatExceptionOfType(OutOfLottoNumberRangeException.class)
+                .isThrownBy(() -> {
+                    LottoBall.of(number);
+                });
     }
 
 }
