d17f1d3d0f60c07de748bc2f54a56fe922a2e06b
eyabc <bey4314@naver.com>
2020-11-21 18:28:59 +0900

    feat: Lottos 생성자

---
+++ src/main/java/step03/domain/Lottos.java
@@ -0,0 +1,30 @@
+package step03.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    private Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public static Lottos of(List<Lotto> lottos) {
+        return new Lottos(lottos);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lottos lottos1 = (Lottos) o;
+        return Objects.equals(lottos, lottos1.lottos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos);
+    }
+}
