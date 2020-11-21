a6e6a4e6477c937e63f94e6bc0535fbaf92beca1
kriery <jongchan.lee@lmep.kr>
2020-11-21 23:55:32 +0900

    feat: StringAddCaculator 구현
    - `split`(기본 구분자), `split`(커스텀 구분자), `sum`을 별도로  구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,40 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+    private static final String  defaultDelimiter = ",:";
+    private static final Pattern customDelimiterPattern = Pattern.compile("//(.)\n(.*)");
+
     public static int splitAndSum(String expression) {
-        return 0;
+        if (Objects.isNull(expression)) {
+            return 0;
+        }
+        return sum(split(expression));
+    }
+
+    private static List<String> split(String expression) {
+        Matcher matcher = customDelimiterPattern.matcher(expression);
+        if (matcher.find()) {
+            return split(matcher.group(2), matcher.group(1));
+        }
+        return split(expression, defaultDelimiter);
+    }
+
+    private static List<String> split(String expression, String delimiter) {
+        if (expression.isEmpty()) return Collections.emptyList();
+        return Arrays.asList(expression.split("[" + delimiter + "]"));
+    }
+
+    private static int sum(List<String> numbers) {
+        return numbers.stream()
+                .map(Integer::parseUnsignedInt)  // throws NumberFormatException(is a RuntimeException) when negative integer
+                .reduce(Integer::sum)
+                .orElse(0);
     }
 }
