d748dac833f9864b3adf2101a9bb8ecd9974105c
sehan <sehan.choi@gmail.com>
2020-11-21 21:34:30 +0900

    test: test 코드정리

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,19 +14,19 @@ class LottoNumberTest {
     @DisplayName("NumberMatcher 를 넣어주면 일치하는 번호 갯수를 알려준다")
     @Test
     void matchedNumberCount() {
-        LottoNumber number = new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoNumber number = LottoUtils.lottoNumber("1, 2, 3, 4, 5, 6");
 
-        assertThat(number.getMatchedNumberCount(testMatcher(Arrays.asList(1, 10, 11, 12, 13, 14))))
+        assertThat(number.getMatchedNumberCount(listNumberMatcher("1, 10, 11, 12, 13, 14")))
                 .isEqualTo(1);
-        assertThat(number.getMatchedNumberCount(testMatcher(Arrays.asList(1, 2, 11, 12, 13, 14))))
+        assertThat(number.getMatchedNumberCount(listNumberMatcher("1, 2, 11, 12, 13, 14")))
                 .isEqualTo(2);
-        assertThat(number.getMatchedNumberCount(testMatcher(Arrays.asList(1, 2, 3, 12, 13, 14))))
+        assertThat(number.getMatchedNumberCount(listNumberMatcher("1, 2, 3, 12, 13, 14")))
                 .isEqualTo(3);
-        assertThat(number.getMatchedNumberCount(testMatcher(Arrays.asList(1, 2, 3, 4, 13, 14))))
+        assertThat(number.getMatchedNumberCount(listNumberMatcher("1, 2, 3, 4, 13, 14")))
                 .isEqualTo(4);
-        assertThat(number.getMatchedNumberCount(testMatcher(Arrays.asList(1, 2, 3, 4, 5, 14))))
+        assertThat(number.getMatchedNumberCount(listNumberMatcher("1, 2, 3, 4, 5, 14")))
                 .isEqualTo(5);
-        assertThat(number.getMatchedNumberCount(testMatcher(Arrays.asList(1, 2, 3, 4, 5, 6))))
+        assertThat(number.getMatchedNumberCount(listNumberMatcher("1, 2, 3, 4, 5, 6")))
                 .isEqualTo(6);
     }
 
@@ -46,8 +47,13 @@ class LottoNumberTest {
         assertThat(num.toString()).isEqualTo("1,2,3,4,5,6");
     }
 
-    private NumberMatcher testMatcher(List<Integer> source) {
-        return new ListNumberMatcher(source);
+    private NumberMatcher listNumberMatcher(String sourceNumbers) {
+        List<Integer> inumbers = Arrays.asList(sourceNumbers.split(",")).stream()
+                .map(String::trim)
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+
+        return new ListNumberMatcher(inumbers);
     }
 
 }
