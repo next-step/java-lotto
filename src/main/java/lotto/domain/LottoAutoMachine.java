65839bf7457c05b95c02167fbd5e5634750fd1a7
DongHwan Kim <bkuekany@gmail.com>
2020-11-21 22:44:34 +0900

    refactor : Refactor 추상클래스에서 인터페이스로 변경

--- src/main/java/lotto/domain/LottoAutoMachine.java
+++ src/main/java/lotto/domain/LottoAutoMachine.java
@@ -1,15 +1,23 @@
 package lotto.domain;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class LottoAutoMachine extends LottoMachine {
+public class LottoAutoMachine implements LottoMachine {
+
+    protected static final List<Integer> numbers = new ArrayList<>();
+    private static final int MAX_LOTTO_NUMBER_RANGE = 45;
+
+    static {
+        IntStream.rangeClosed(1, MAX_LOTTO_NUMBER_RANGE).forEach(numbers::add);
+    }
 
     @Override
     public Set<Integer> createLottoNumber(int capacity) {
         Collections.shuffle(numbers);
 
-        Set<Integer> lottoPickNumber = new LinkedHashSet<>();
+        Set<Integer> lottoPickNumber = new TreeSet<>();
         IntStream.range(0, capacity)
                 .forEach(i -> lottoPickNumber.add(numbers.get(i)));
 
--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,18 +1,8 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.IntStream;
 
-public abstract class LottoMachine {
+public interface LottoMachine {
 
-    protected static final List<Integer> numbers = new ArrayList<>();
-    private static final int MAX_LOTTO_NUMBER_RANGE = 45;
-
-    static {
-        IntStream.rangeClosed(1, MAX_LOTTO_NUMBER_RANGE).forEach(numbers::add);
-    }
-
-    abstract public Set<Integer> createLottoNumber(int capacity);
+    Set<Integer> createLottoNumber(int capacity);
 }
