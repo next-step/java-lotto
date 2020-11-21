0ee1de908034c9cd9d907e8ac0594a59686c6221
최종원 <90lastone@gmail.com>
2020-11-21 12:06:02 +0900

    refactor: test 함수 throws Exception 미사용으로 일단 제거

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -15,14 +15,14 @@ public class StringAddCalculatorTest {
 
     // 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    public void splitAndSum_숫자하나() {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     // 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    public void splitAndSum_쉼표구분자() {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
