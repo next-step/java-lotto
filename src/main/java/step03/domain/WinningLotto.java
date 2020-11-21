912c5aaeca21ddcb8ce3d139e2198e9a78c01f43
eyabc <bey4314@naver.com>
2020-11-21 18:27:12 +0900

    feat: 보너스 볼이 당첨로또에 포함되면 예외 던짐

--- src/main/java/step03/domain/WinningLotto.java
+++ src/main/java/step03/domain/WinningLotto.java
@@ -19,7 +19,7 @@ public class WinningLotto {
     }
 
     private static void validateBonusBall(Lotto winningLotto, LottoBall bonusBall) {
-        if (!winningLotto.isContaining(bonusBall)) {
+        if (winningLotto.isContaining(bonusBall)) {
             throw new ExistedInWinningLottoException();
         }
     }
