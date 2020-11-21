ad618c821274999f9dd69eb980c359d862fe0ee7
홍성민 <honggasm@gmail.com>
2020-11-21 17:51:24 +0900

    feat: 예외 발생시 메인에서 출력할 수 있도록 하는 기능 추가

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -4,23 +4,30 @@ import lotto.domain.LottoGame;
 import lotto.dto.LottoStatisticsResult;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import lotto.view.Printer;
 
 import java.util.List;
 
 public class LottoMain {
     public static void main(String[] args) {
-        final int money = InputView.plzEnterMoney();
-        final List<String> lottoNoList = InputView.plzEnterManuallyBuyLottoNo();
+        try {
+            final int money = InputView.plzEnterMoney();
+            final List<String> lottoNoList = InputView.plzEnterManuallyBuyLottoNo();
 
-        final LottoGame lottoGame = LottoGame.of(money);
-        lottoGame.buyLotto(lottoNoList);
+            final LottoGame lottoGame = LottoGame.of(money);
+            lottoGame.buyLotto(lottoNoList);
 
-        OutputView.printLottoCount(lottoNoList.size(), lottoGame.getLottoTicketGroup());
-        OutputView.printAllLotto(lottoGame.getLottoTicketGroup());
+            OutputView.printLottoCount(lottoNoList.size(), lottoGame.getLottoTicketGroup());
+            OutputView.printAllLotto(lottoGame.getLottoTicketGroup());
 
-        final String winningNumberExpression = InputView.plzEnterLastWeekWinningNumber();
-        final int bonusNumber = InputView.plzEnterBonusNumber();
-        final LottoStatisticsResult statisticsResult = lottoGame.getWinLotteryStatistics(winningNumberExpression, bonusNumber);
-        OutputView.printStatistics(statisticsResult);
+            final String winningNumberExpression = InputView.plzEnterLastWeekWinningNumber();
+            final int bonusNumber = InputView.plzEnterBonusNumber();
+            final LottoStatisticsResult statisticsResult = lottoGame.getWinLotteryStatistics(winningNumberExpression, bonusNumber);
+            OutputView.printStatistics(statisticsResult);
+        } catch (IllegalArgumentException e) {
+            Printer.printNewLine();
+            Printer.println("예외 발생!");
+            Printer.print("예외 메시지: " +e.getMessage());
+        }
     }
 }
