9b22fbb28341c2b6578b92ff58b0793addc17e51
최종원 <90lastone@gmail.com>
2020-11-21 11:45:08 +0900

    feat: 1. 입력값 null, 빈문자 검사

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,11 @@
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        // 널, 빈 문자열 체크
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        // 스플릿
+        // 더하기
+        return 1;
+    }
+}
