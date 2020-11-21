b53993e169b58fd1bacd22f4289b9aaa4d21218c
홍성민 <honggasm@gmail.com>
2020-11-21 16:00:01 +0900

    feat: inputview를 통해 수동으로 로또 번호를 입력받는 기능 작성

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -5,15 +5,19 @@ import lotto.dto.LottoStatisticsResult;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoMain {
     public static void main(String[] args) {
         final int money = InputView.plzEnterMoney();
+        final List<String> lottoNoList = InputView.plzEnterManuallyBuyLottoNo();
+
         final LottoGame lottoGame = LottoGame.of(money);
-//        lottoGame.buyLotto();
-        
+        lottoGame.buyLotto(lottoNoList);
+
         OutputView.printLottoCount(lottoGame.getLottoTicketGroup());
         OutputView.printAllLotto(lottoGame.getLottoTicketGroup());
-        
+
         final String winningNumberExpression = InputView.plzEnterLastWeekWinningNumber();
         final int bonusNumber = InputView.plzEnterBonusNumber();
         final LottoStatisticsResult statisticsResult = lottoGame.getWinLotteryStatistics(winningNumberExpression, bonusNumber);
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,12 +11,29 @@ public class InputView {
         return scanner.nextInt();
     }
 
+    private static int plzEnterManuallyBuyLottoCount() {
+        Printer.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        final Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static List<String> plzEnterManuallyBuyLottoNo() {
+        final int manuallyBuyLottoCount = plzEnterManuallyBuyLottoCount();
+        Printer.println("수동으로 구매할 번호를 입력해 주세요.");
+        final Scanner scanner = new Scanner(System.in);
+        final List<String> lottoNoList = new ArrayList<>();
+        for (int i = 0; i < manuallyBuyLottoCount; i++) {
+            lottoNoList.add(scanner.nextLine());
+        }
+        return lottoNoList;
+    }
+
     public static String plzEnterLastWeekWinningNumber() {
         Printer.println("지난 주 당첨 번호를 입력해 주세요.");
         final Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
-    
+
     public static int plzEnterBonusNumber() {
         Printer.println("보너스 볼을 입력해 주세요.");
         final Scanner scanner = new Scanner(System.in);
