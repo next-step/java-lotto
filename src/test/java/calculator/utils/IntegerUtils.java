711aae75a34b0abe8e8fc66c4792729008fd124a
jhoon.bak <wecube.1@gmail.com>
2020-11-22 00:01:26 +0900

    feat(IntegerUtils): 나뉜 문자를 숫자로 바꾼다. - 숫자가 음수이면 Excption을 throw한다.

--- src/test/java/calculator/utils/IntegerUtils.java
+++ src/test/java/calculator/utils/IntegerUtils.java
@@ -1,2 +1,24 @@
-package calculator.utils;public class IntegerUtils {
+package calculator.utils;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class IntegerUtils {
+    private IntegerUtils() {
+    }
+
+    public static List<Integer> parsePositiveInt(List<String> values) {
+        return values.stream()
+                .map(Integer::parseInt)
+                .map(IntegerUtils::validPositive)
+                .collect(Collectors.toList());
+    }
+
+    private static Integer validPositive(Integer value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("not allow negative quantity");
+        }
+        return value;
+    }
 }
--- src/test/java/calculator/utils/IntegerUtilsTest.java
+++ src/test/java/calculator/utils/IntegerUtilsTest.java
@@ -1,5 +1,38 @@
 package calculator.utils;
 
-public class IntegerUtils {
-    
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class IntegerUtilsTest {
+
+    @Test
+    @DisplayName("나뉜 문자를 숫자로 바꾼다")
+    void should_return_integer_type_from_string() {
+        //Given
+        List<String> values = Lists.newArrayList("1", "2", "3");
+
+        //When
+        List<Integer> integers = IntegerUtils.parsePositiveInt(values);
+
+        //Then
+        assertThat(integers).containsAll(Lists.newArrayList(1, 2, 3));
+    }
+
+    @Test
+    @DisplayName("입력한 문자가 음수 이면, Exception을 반환한다.")
+    void should_throw_illegal_argument_exception_when_is_negative_quantity() {
+        //Given
+        List<String> values = Lists.newArrayList("-1", "-2", "3");
+
+        //When & Then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> IntegerUtils.parsePositiveInt(values))
+                .withMessage("not allow negative quantity");
+    }
 }
