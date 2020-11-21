9bc9a20338c766bc9ea9c97750f6010c3020de62
최종원 <90lastone@gmail.com>
2020-11-21 12:04:25 +0900

    test: 3. 쉼표구분자 테스트 추가

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -19,4 +19,11 @@ public class StringAddCalculatorTest {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
+
+    // 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
 }
