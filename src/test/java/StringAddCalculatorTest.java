c4730c0ef2f27e88a05903bc5a12758e9b7b540e
최종원 <90lastone@gmail.com>
2020-11-21 11:45:19 +0900

    test: 1. 입력값 null, 빈문자 검사 테스트

---
+++ src/test/java/StringAddCalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAddCalculatorTest {
+    // 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+}
