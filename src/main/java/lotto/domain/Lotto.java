6cdcaba148a904d704bd4209263dda860ebce518
deyoon91 <kimbyungyoun91@gmail.com>
2020-11-22 16:27:02 +0900

    [refactor] : 로또 객체 당첨결과 리턴 메소드 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -8,9 +7,6 @@ import static lotto.domain.LottoRuleConfig.*;
 
 public class Lotto {
 
-    private static final int BONUS_NUMBER_LIST_INDEX = 6;
-    private static final int START_WINNING_NUMBERS_INDEX = 0;
-    private static final int END_WINNING_NUMBERS_INDEX = 6;
     private final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<LottoNumber> lottoNumbers) {
@@ -37,32 +33,6 @@ public class Lotto {
 
     public List<LottoNumber> getLottoNumbers() {
         return lottoNumbers;
-    }
-
-    public LottoResult lottoResult(List<Integer> winningNumbers) {
-        int winningCount = sameNumberOfCount(winningNumbers);
-        if(isMatchUpToBonusNumber(winningNumbers.get(BONUS_NUMBER_LIST_INDEX))) {
-            return LottoResult.valueOf(winningCount,true);
-        }
-        return LottoResult.valueOf(winningCount,false);
-    }
-
-    private boolean isMatchUpToBonusNumber(int bonusNumber) {
-        return lottoNumbers.stream()
-                .anyMatch(e -> e.getLottoNumber() == bonusNumber);
-    }
-
-    private int sameNumberOfCount(List<Integer> winningNumbers) {
-        List<Integer> ignoreBonusNumber
-                = winningNumbers.subList(START_WINNING_NUMBERS_INDEX,END_WINNING_NUMBERS_INDEX);
-        return (int)lottoNumbers.stream()
-                .mapToInt(i -> numberMatchResult(ignoreBonusNumber,i.getLottoNumber()))
-                .filter(i -> i == 1)
-                .count();
-    }
-
-    private static int numberMatchResult(List<Integer> winningNumbers, int number) {
-        return Collections.frequency(winningNumbers,number);
     }
 
     @Override
