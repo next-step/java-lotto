feff1846003622bf3dabd88794f60c384ab5c9c6
asda004 <asda00400@gmail.com>
2020-11-22 21:17:07 +0900

    step1 (fix) 리뷰반영

--- src/main/java/my/project/constants/Delimiters.java
+++ src/main/java/my/project/constants/Delimiters.java
@@ -6,8 +6,7 @@ package my.project.constants;
  */
 public class Delimiters {
     public static final String DELIMITER = ",|:";
-    public static final String CUSTOM_PREFIX_DELIMITER = "//";
-    public static final String CUSTOM_SUFFIX_DELIMITER = "\n";
+    public static final String CUSTOM_PATTERN_DELIMITER = "//(.)\n(.*)";
 
     private Delimiters() {
     }
--- src/test/java/my/project/cal/StringPlusCalculatorTest.java
+++ src/test/java/my/project/cal/StringPlusCalculatorTest.java
@@ -27,47 +27,47 @@ class StringPlusCalculatorTest {
     @DisplayName("null 값을 입력할 경우 0을 반환해야 한다")
     @ParameterizedTest
     @NullSource
-    public void givenNull_thenReturnZero(String input) {
+    void givenNull_thenReturnZero(String input) {
         assertThat(spc.sumStringByDelimiter(input)).isZero();
     }
 
     @DisplayName("빈 문자열을 입력할 경우 0을 반환해야 한다")
     @ParameterizedTest
     @EmptySource
-    public void givenEmpty_thenReturnZero(String input) {
+    void givenEmpty_thenReturnZero(String input) {
         assertThat(spc.sumStringByDelimiter(input)).isZero();
     }
 
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
     @ParameterizedTest
     @ValueSource(strings = {"1"})
-    public void givenStringOne_thenReturnSelf(String input) {
+    void givenStringOne_thenReturnSelf(String input) {
         assertThat(spc.sumStringByDelimiter(input)).isEqualTo(Integer.parseInt(input));
     }
 
     @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다")
     @ParameterizedTest
     @CsvSource(value = {"1,2|3"}, delimiter = '|')
-    public void givenSingleDelimiter_thenReturnSum(String input, int expected) {
+    void givenSingleDelimiter_thenReturnSum(String input, int expected) {
         assertThat(spc.sumStringByDelimiter(input)).isEqualTo(expected);
     }
 
     @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다")
     @ParameterizedTest
     @CsvSource(value = {"1,2:3|6"}, delimiter = '|')
-    public void givenMultiDelimiter_thenReturnSum(String input, int expected) {
+    void givenMultiDelimiter_thenReturnSum(String input, int expected) {
         assertThat(spc.sumStringByDelimiter(input)).isEqualTo(expected);
     }
 
     @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다")
     @Test
-    public void givenCustomDelimiter_thenReturnSum() {
+    void givenCustomDelimiter_thenReturnSum() {
         assertThat(spc.sumStringByDelimiter("//;\n1;2;3")).isEqualTo(6);
     }
 
     @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다")
     @Test
-    public void givenNegative_thenThrowException() {
+    void givenNegative_thenThrowException() {
         assertThatThrownBy(() -> spc.sumStringByDelimiter("-1,2:3"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("음수가 포함되어 있습니다.")
