61472df7b32e76b0a5a5868f766ac588f23d7bed
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-22 14:24:50 +0900

    refactor 1. 코드라인 줄이기 2. 피요한 문구 추가

--- src/main/java/step2/lotto/automatic/view/InputStep2View.java
+++ src/main/java/step2/lotto/automatic/view/InputStep2View.java
@@ -6,13 +6,17 @@ public class InputStep2View {
 
     private static Scanner sc = new Scanner(System.in);
 
-    public int inputValue() {
+    private InputStep2View(){
+
+    }
+
+    public static int inputValue() {
         int value = 0;
         System.out.println("구매금액을 입력해주세요");
 
         try {
-            value = sc.nextInt();
-            sc.nextLine();
+            value = Integer.parseInt(sc.nextLine());
+
             return value;
         } catch (NumberFormatException e) {
             e.printStackTrace();
@@ -21,8 +25,11 @@ public class InputStep2View {
         return value;
     }
 
-    public String inputWinningLottoNumbers() {
+    public static String inputWinningLottoNumbers() {
+        System.out.println();
+        System.out.println("지난주 당첨 결과를 입력해주세요");
         String returnValue = sc.nextLine();
+
         return returnValue;
     }
 }
