0123bfd77775039946f5fc1ad8457ec6241f0bb1
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-22 14:28:45 +0900

    refactor

--- src/main/java/step2/lotto/automatic/controller/LottoController.java
+++ src/main/java/step2/lotto/automatic/controller/LottoController.java
@@ -2,16 +2,27 @@ package step2.lotto.automatic.controller;
 
 
 import step2.lotto.automatic.domain.LottoMatch;
+import step2.lotto.automatic.domain.LottoStatusEnum;
 import step2.lotto.automatic.view.InputStep2View;
+import step2.lotto.automatic.view.OuterStep2View;
+
+import java.util.Map;
 
 public class LottoController {
 
-    private static InputStep2View inputStep2View = new InputStep2View();
+    private static LottoMatch lottoMatch;
 
     public static void main(String[] args) {
-        int purchaseAmount = inputStep2View.inputValue();
-        LottoMatch lottoMatch = new LottoMatch(purchaseAmount);
+        int purchaseAmount = InputStep2View.inputValue();
+        lottoMatch = new LottoMatch(purchaseAmount);
+        lottoMatch.buyLottoNumber();
 
-        lottoMatch.run();
+        OuterStep2View.printBuyLotto(lottoMatch.getLottoPurchaseCount());
+        OuterStep2View.printAutoLottoNumbers(lottoMatch.getBuyLottoList());
+
+        String inputLottoWinnerNumber = InputStep2View.inputWinningLottoNumbers();
+        Map<Integer, LottoStatusEnum> resunlt = lottoMatch.getLottoResult(inputLottoWinnerNumber);
+
+        OuterStep2View.printLottoResult(resunlt);
     }
 }
