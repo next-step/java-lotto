879dbaf020265a03610efe89cdb6bc3109f7ebe3
sjh <sjh@revhat.com>
2020-11-22 11:51:49 +0900

    [step1] tokenizer test case 추가

--- src/test/java/step1/TokenizerTest.java
+++ src/test/java/step1/TokenizerTest.java
@@ -10,12 +10,22 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class TokenizerTest {
 
     @Test
-    void tokenizerTest() {
+    void tokenizerDefaultTest() {
+        String[] inputValueList = {"1", "2", "3"};
+        String inputValue = inputValueList[0] + "," + inputValueList[1] + ";" + inputValueList[2];
+        Tokenizer tokenizer = new Tokenizer();
+        String[] valueList = tokenizer.splitValue(inputValue);
+        assertThat(valueList.length).isEqualTo(3);
+
+        for (int i = 0; i<inputValueList.length; i++) {
+            assertThat(inputValueList[i].equals(valueList[i])).isEqualTo(true);
+        }
+    }
+
+    @Test
+    void tokenizerChangedTest() {
         String inputValue = "//;\n1;2;3";
         Tokenizer tokenizer = new Tokenizer();
         assertThat(tokenizer.splitValue(inputValue).length).isEqualTo(3);
-
-        inputValue = "1,2,3;4;5";
-        assertThat(tokenizer.splitValue(inputValue).length).isEqualTo(5);
     }
 }
