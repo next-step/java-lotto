5965c8922359585da6731fd1b4c08d0a7d59d0c4
이병무 <am003507@gmail.com>
2020-11-22 18:41:30 +0900

    refactor - 문자를 상수로 추출

--- src/main/java/lotto_auto/view/WinningLottoInputView.java
+++ src/main/java/lotto_auto/view/WinningLottoInputView.java
@@ -9,14 +9,15 @@ import java.util.stream.Collectors;
 
 public class WinningLottoInputView {
 
-    private  static final String MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String COMMA = ",";
 
     public static LottoTicket enter() {
         System.out.println(MESSAGE);
         Scanner scanner = new Scanner(System.in);
         String next = scanner.next();
         return new LottoTicket(
-                Arrays.stream(next.split(","))
+                Arrays.stream(next.split(COMMA))
                         .mapToInt(Integer::parseInt)
                         .boxed()
                         .collect(Collectors.toList())
