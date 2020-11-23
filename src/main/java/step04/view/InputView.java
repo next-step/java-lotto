7593dfecd74da96a2ae9ccf118989ca95ca5cb98
eyabc <bey4314@naver.com>
2020-11-23 13:12:27 +0900

    feat: 수동 로또 입력

--- src/main/java/step04/view/InputView.java
+++ src/main/java/step04/view/InputView.java
@@ -1,5 +1,7 @@
 package step04.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,6 +10,8 @@ public class InputView {
     private static final String  LOTTO_BUY_PAYING_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String  LOTTO_CREATE_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String  PICK_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String  LOTTO_MANUAL_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String  LOTTO_MANUAL_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -25,6 +29,21 @@ public class InputView {
     public static int pickBonusBall() {
         System.out.println(PICK_BONUS_BALL_MESSAGE);
         return scanner.nextInt();
+    }
+
+    public static int readManualCount() {
+        System.out.println(LOTTO_MANUAL_COUNT_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static List<String> readManualLottos(int count) {
+        System.out.println(LOTTO_MANUAL_MESSAGE);
+        List<String> manualLottos = new ArrayList<>();
+
+        for(int i = 0 ; i < count; i++)
+            manualLottos.add(scanner.nextLine());
+
+        return manualLottos;
     }
 
 }
