f71fcc551126dcff5fa8749320deee3b6afef38e
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-23 19:44:27 +0900

    refactor 1. 기능 분리 (총 상금 금액)

--- src/main/java/step2/lotto/automatic/domain/LottoProfitAmount.java
+++ src/main/java/step2/lotto/automatic/domain/LottoProfitAmount.java
@@ -1,15 +1,16 @@
 package step2.lotto.automatic.domain;
 
-import java.util.Map;
-
 public class LottoProfitAmount {
     private static final int MATCH_LOTTE_MAX_COUNT = 4;
     private static final int MATCH_LOTTE_MIN_COUNT = 1;
 
-    public static int runLottoProfitAmount(Map<Integer, LottoStatusEnum> lottoResult) {
+    public static int runLottoProfitAmount() {
         int count = 0;
+        LottoStatusEnum lottoEnum;
+
         for (int i = MATCH_LOTTE_MIN_COUNT; i <= MATCH_LOTTE_MAX_COUNT; i++) {
-            count = count + (lottoResult.get(i).getPrice() * lottoResult.get(i).getWinningCount());
+            lottoEnum = LottoStatusEnum.findByCount(i);
+            count = count + (lottoEnum.getPrice() * lottoEnum.getWinningCount());
         }
 
         return count;
