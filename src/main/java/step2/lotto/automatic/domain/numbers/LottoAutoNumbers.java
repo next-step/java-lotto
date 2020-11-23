5e18ebfe1cf6afe6c8492028214882750793b9ad
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-23 19:36:00 +0900

    refactor 자동로또번호 생성 객체

---
+++ src/main/java/step2/lotto/automatic/domain/numbers/LottoAutoNumbers.java
@@ -0,0 +1,30 @@
+package step2.lotto.automatic.domain.numbers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoAutoNumbers {
+    private static final int MAKE_LOTTO_NUMBERS_COUNT = 45;  // 로또 생성 갯수
+    private List<Integer> basicLottoNumbers = new ArrayList<>();
+
+    LottoAutoNumbers(int makeLottoNumberCount) {
+        initLottoNubmers(makeLottoNumberCount);
+    }
+
+    private void initLottoNubmers(int makeLottoNumberCount) {
+        for (int i = 1; i <= makeLottoNumberCount; i++) {
+            basicLottoNumbers.add(i);
+        }
+    }
+
+    public List<Integer> getAutoNumbers(int getLottoAutoNumberCount) {
+        List<Integer> returnList = new ArrayList<>();
+        Collections.shuffle(basicLottoNumbers);
+
+        for (int i = 0; i < getLottoAutoNumberCount; i++) {
+            returnList.add(basicLottoNumbers.get(i));
+        }
+        return returnList;
+    }
+}
