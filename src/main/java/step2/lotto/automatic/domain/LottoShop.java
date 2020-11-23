7ef9a08986271f8bb012cfbe2890a081e96b6d48
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-23 19:44:53 +0900

    refactor 1. 기능분리   - 로또를 구매할 수 있는 shop 객체 생성

--- src/main/java/step2/lotto/automatic/domain/LottoShop.java
+++ src/main/java/step2/lotto/automatic/domain/LottoShop.java
@@ -1,33 +1,29 @@
 package step2.lotto.automatic.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import step2.lotto.automatic.domain.numbers.LottoMachine;
+
+import java.util.*;
 
 public class LottoShop {
 
     private static final int MAKE_LOTTONUMBER_COUNT = 45; // 로또번호 생성 갯수
     private static final int MAKE_AUTO_NUMBER = 6; // 한게임당 필요한 번호 갯수
-    private static LottoAutoMachine lottoAutoNumbers;
+    private static LottoMachine lottoMachine;
 
     public LottoShop() {
-        initLottoShop();
+        lottoMachine = new LottoMachine(MAKE_LOTTONUMBER_COUNT);
     }
 
-    private void initLottoShop() {
-        lottoAutoNumbers = new LottoAutoMachine(MAKE_LOTTONUMBER_COUNT);
-    }
-
-    public static List<List<Integer>> buyLotto(int lottoBuyCount) {
-        List<List<Integer>> lottoList = new ArrayList<>();
+    public static Set<List<Integer>> buyAutoLotto(int lottoBuyCount) {
+        Set<List<Integer>> returnSet = new HashSet<>();
         List<Integer> lotto = new ArrayList<>();
 
         for (int i = lottoBuyCount; i > 0; i--) {
-            lotto = lottoAutoNumbers.getAutoNumbers(MAKE_AUTO_NUMBER);
+            lotto = lottoMachine.getAutoNumbers(MAKE_AUTO_NUMBER);
             Collections.sort(lotto);
-            lottoList.add(lotto);
+            returnSet.add(lotto);
         }
 
-        return lottoList;
+        return returnSet;
     }
 }
