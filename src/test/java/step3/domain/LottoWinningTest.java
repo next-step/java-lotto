957657646a61e0bb8d192d1695841d03c5afee1c
dbeod2 <darung.0211@gmail.com>
2020-11-24 00:39:54 +0900

    test : 테스트 코드 오류 수정

--- src/test/java/step3/domain/LottoWinningTest.java
+++ src/test/java/step3/domain/LottoWinningTest.java
@@ -1,6 +1,7 @@
 package step3.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import step3.constant.LottoWinningPrizes;
@@ -13,19 +14,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoWinningTest {
 
-    @ParameterizedTest
+    @Test
     @DisplayName("로또 당첨 금액 확인")
-    @ValueSource(strings = "1,2,3,4,5,6")
-    public void 로또_당첨_금액_확인(String lastLottoWinningNumbers) {
+    public void 로또_당첨_금액_확인() {
         List<Lotto> lottos = new ArrayList<>();
+
         Lotto lotto = new Lotto();
+        lotto.getLottos().clear();
         IntStream.of(1, 2, 3, 7, 8, 9)
                 .forEach(i -> lotto.getLottos().add(i));
-        lottos.add(lotto);
 
-        LottoNumber number = new LottoNumber(lastLottoWinningNumbers, "10");
-        LottoWinning lottoWinning = LottoWinning.of(number, lottos);
-        assertThat(lottoWinning.getWinningAmount()).isEqualTo(5000);
+        lottos.add(lotto);
+        LottoNumber number = new LottoNumber("1,2,3,9,12,14", "10");
+
+        LottoWinning lottoWinning = LottoWinning.of(number, new Lottos(lottos));
+        assertThat(lottoWinning.getWinningAmount()).isEqualTo(50000);
     }
 
     @ParameterizedTest
@@ -33,13 +36,16 @@ public class LottoWinningTest {
     @ValueSource(strings = "1,2,3,4,5,6")
     public void 당첨_갯수_확인(String lastLottoWinningNumbers) {
         List<Lotto> lottos = new ArrayList<>();
+
         Lotto lotto = new Lotto();
+        lotto.getLottos().clear();
         IntStream.of(1, 2, 3, 7, 8, 9)
                 .forEach(i -> lotto.getLottos().add(i));
+
         lottos.add(lotto);
 
         LottoNumber number = new LottoNumber(lastLottoWinningNumbers, "10");
-        LottoWinning lottoWinning = LottoWinning.of(number, lottos);
+        LottoWinning lottoWinning = LottoWinning.of(number, new Lottos(lottos));
         assertThat(lottoWinning.getLottoWinningMap().get(LottoWinningPrizes.THIRD_MATCHES)).isEqualTo(1);
     }
 }
