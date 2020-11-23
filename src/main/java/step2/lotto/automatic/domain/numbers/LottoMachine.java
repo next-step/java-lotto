005b5d383cca35a62a921854a7b47fd6861b2557
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-23 19:36:32 +0900

    refactor 로또 생성 관리 객체

--- src/main/java/step2/lotto/automatic/domain/numbers/LottoMachine.java
+++ src/main/java/step2/lotto/automatic/domain/numbers/LottoMachine.java
@@ -1,43 +1,32 @@
-package step2.lotto.automatic.domain;
+package step2.lotto.automatic.domain.numbers;
 
+import step2.lotto.automatic.domain.numbers.LottoAutoNumbers;
 import step2.lotto.automatic.util.LottoStep2ErrorMessage;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * 로또 자동생성
  */
-public class LottoAutoMachine {
+public class LottoMachine {
+
+    private static final int MAKE_LOTTO_NUMBERS_COUNT = 45;  // 로또 생성 갯수
 
     private static List<Integer> basicLottoNumbers = new ArrayList<>();
-    private static final int MAKE_LOTTO_NUMBERS_COUNT = 45;  // 로또 생성 갯수
-    private static LottoAutoMachine lottoAutoNumbers;  // auto 번호 생성
 
-    public LottoAutoMachine(int makeLottoNumberCount) {
+    private static LottoAutoNumbers lottoAutoNumbers;
+
+    public LottoMachine(int makeLottoNumberCount) {
         isNumberNegative(makeLottoNumberCount);
         isMakeCount(makeLottoNumberCount);
-        initLottoNubmers(makeLottoNumberCount);
+        lottoAutoNumbers = new LottoAutoNumbers(makeLottoNumberCount);
     }
 
     public List<Integer> getAutoNumbers(int getLottoAutoNumberCount) {
-        List<Integer> returnList = new ArrayList<>();
         isNumberNegative(getLottoAutoNumberCount);
 
-        Collections.shuffle(basicLottoNumbers);
-
-        for (int i = 0; i < getLottoAutoNumberCount; i++) {
-            returnList.add(basicLottoNumbers.get(i));
-        }
-
-        return returnList;
-    }
-
-    private void initLottoNubmers(int initLottoNubmers) {
-        for (int i = 1; i <= initLottoNubmers; i++) {
-            basicLottoNumbers.add(i);
-        }
+        return lottoAutoNumbers.getAutoNumbers(getLottoAutoNumberCount);
     }
 
     private void isNumberNegative(int paramValue) {
