e426efa2c2e5083a72a8f12d2abcfc33d1454d77
이병무 <am003507@gmail.com>
2020-11-23 18:00:28 +0900

    refactor - 상수 클래스 객체 생성 방지

--- src/main/java/lotto_auto/ErrorMessage.java
+++ src/main/java/lotto_auto/ErrorMessage.java
@@ -2,6 +2,9 @@ package lotto_auto;
 
 public class ErrorMessage {
 
+    private ErrorMessage() {
+    }
+
     public static final String NOT_NULL_WINNING_LOTTO_TICKET = "Not Null Winning Lotto Ticket";
     public static final String INVALID_LOTTO_MATCH_NUM = "Invalid Lotto Match Num";
     public static final String INVALID_LOTTO_RANK = "Invalid Lotto Rank";
@@ -11,4 +14,5 @@ public class ErrorMessage {
     public static final String INVALID_LOTTO_NUMBER = "Invalid Lotto Number";
     public static final String INVALID_LOTTO_NUMBER_COUNT = "Invalid Lotto Number Count";
     public static final String DUPLICATE_LOTTO_NUMBER = "Duplicate Lotto Number";
+
 }
