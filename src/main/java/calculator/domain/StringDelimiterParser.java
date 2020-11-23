02518badf9872965a23ed00a746f9f5ba72469fa
james.star <james.star@kakaocorp.com>
2020-11-23 17:42:20 +0900

    step1.5 - refactor make pattern variable static final

--- src/main/java/calculator/domain/StringDelimiterParser.java
+++ src/main/java/calculator/domain/StringDelimiterParser.java
@@ -11,16 +11,19 @@ public class StringDelimiterParser {
     private static final String CUSTOM_DELIMITER_HEAD = "//";
     private static final String CUSTOM_DELIMITER_TAIL = "\n";
 
+    private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+
     public static String[] parseText(String text) {
 
         if (!doesHaveDelimiter(text)) {
             return new String[]{text};
         }
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
         }
 
         return text.split("[,:]");
