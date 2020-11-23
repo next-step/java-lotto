d38d6c186d1cae4ab15c843cd8730edb3136078f
이병무 <am003507@gmail.com>
2020-11-23 19:40:32 +0900

    test - 등수별 테스트 코드 추가

--- src/test/java/lotto_auto/model/DrawResultTest.java
+++ src/test/java/lotto_auto/model/DrawResultTest.java
@@ -1,11 +1,13 @@
 package lotto_auto.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class DrawResultTest {
 
@@ -77,4 +79,29 @@ class DrawResultTest {
         assertThat(drawResult).isNotNull();
     }
 
+    @DisplayName("각 등수에 대한 당첨 여부 테스트")
+    @Test
+    public void drawResultValidTest(){
+        DrawResult first = DrawResult.valueOfRank(1);
+        DrawResult second = DrawResult.valueOfRank(2);
+        DrawResult third = DrawResult.valueOfRank(3);
+        DrawResult fourth = DrawResult.valueOfRank(4);
+
+        assertAll(
+                ()->assertThat(first.getMoney()).isEqualTo(2000000000L),
+                ()->assertThat(first.isWinning()).isEqualTo(true),
+                ()->assertThat(first.getMatchNum()).isEqualTo(6),
+                ()->assertThat(second.getMoney()).isEqualTo(1500000L),
+                ()->assertThat(second.isWinning()).isEqualTo(true),
+                ()->assertThat(second.getMatchNum()).isEqualTo(5),
+                ()->assertThat(third.getMoney()).isEqualTo(50000L),
+                ()->assertThat(third.isWinning()).isEqualTo(true),
+                ()->assertThat(third.getMatchNum()).isEqualTo(4),
+                ()->assertThat(fourth.getMoney()).isEqualTo(5000L),
+                ()->assertThat(fourth.isWinning()).isEqualTo(true),
+                ()->assertThat(fourth.getMatchNum()).isEqualTo(3)
+        );
+
+
+    }
 }
