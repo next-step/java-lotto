e202258f105f743296956ff1e533c357e0065642
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-23 19:46:26 +0900

    docs refacotr 1. 필요없는 import 삭제 2. enum 객체 가져오기

--- src/main/java/step2/lotto/automatic/view/OuterLottoView.java
+++ src/main/java/step2/lotto/automatic/view/OuterLottoView.java
@@ -2,10 +2,12 @@ package step2.lotto.automatic.view;
 
 import step2.lotto.automatic.domain.LottoStatusEnum;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-public class OuterStep2View {
+public class OuterLottoView {
 
     private static final int MATCH_LOTTE_MAX_COUNT = 4;
     private static final int MATCH_LOTTE_MIN_COUNT = 1;
@@ -13,7 +15,7 @@ public class OuterStep2View {
     private static final String GOOD_PRICE = "(기준이 1이기 때문에 결과적으로 이득라는 의미임)";
     private static final String BAD_PRICE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    private OuterStep2View() {
+    private OuterLottoView() {
 
     }
 
@@ -21,36 +23,45 @@ public class OuterStep2View {
         System.out.println(buyLottoCount + "개를 구매했습니다.");
     }
 
-    public static void printAutoLottoNumbers(List<List<Integer>> paramList) {
-        printDetailAutoNumbers(paramList.get(0));
+    public static void printAutoLottoNumbers(Set<List<Integer>> paramHashSet, int count) {
+        List<List<Integer>> list = new ArrayList<>();
+        list.addAll(paramHashSet);
+        printDetailAutoNumbers(list.get(0));
         System.out.print(System.lineSeparator());
 
-        for (int i = 1; i < paramList.size(); i++) {
-            printDetailAutoNumbers(paramList.get(i));
+        for (int i = 1; i < count; i++) {
+            printDetailAutoNumbers(list.get(i));
             System.out.print(System.lineSeparator());
         }
     }
 
     private static void printDetailAutoNumbers(List<Integer> paramList) {
-        System.out.print("[");
-        System.out.print(paramList.get(0));
+        StringBuffer sb = new StringBuffer();
 
+        sb.append("[");
+        sb.append(paramList.get(0));
         for (int i = 1; i < paramList.size(); i++) {
-            System.out.print(", " + paramList.get(i));
+            sb.append(", " + paramList.get(i));
         }
-
-        System.out.print("]");
+        sb.append("]");
+        System.out.print(sb);
     }
 
-    public static void printLottoResult(Map<Integer, LottoStatusEnum> paramValue) {
-        for (int i = MATCH_LOTTE_MAX_COUNT; i >= MATCH_LOTTE_MIN_COUNT; i--) {
-            System.out.println(paramValue.get(i).getMessage()
-                    + "("
-                    + paramValue.get(i).getPrice()
-                    + ") - "
-                    + paramValue.get(i).getWinningCount());
-        }
+    public static void printLottoResult() {
+        StringBuffer sb = new StringBuffer();
+        LottoStatusEnum lottoEnum;
 
+        for (int i = MATCH_LOTTE_MAX_COUNT; i >= MATCH_LOTTE_MIN_COUNT; i--) {
+            lottoEnum = LottoStatusEnum.findByCount(i);
+            sb.append(lottoEnum.getMessage()
+                    + "("
+                    + lottoEnum.getPrice()
+                    + ") - "
+                    + lottoEnum.getWinningCount()
+            );
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 
     public static void printLottoProfitAmount(double paramValue){
