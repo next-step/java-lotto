201ec18309b7ca22e0d73b7cd0cd8f5393d2620f
kriery <jongchan.lee@lmep.kr>
2020-11-24 15:18:44 +0900

    feat: 게임 관리(`GameManagement`) 추가

---
+++ src/main/java/lottery/GameManagement.java
@@ -0,0 +1,42 @@
+package lottery;
+
+import lottery.domain.Picker;
+import lottery.domain.ShufflePicker;
+import lottery.domain.LotteryNumbers;
+import lottery.view.InputView;
+import lottery.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class GameManagement {
+    private final List<LotteryNumbers> lotteries;
+
+    public GameManagement(int numberGames) {
+        final Picker picker = new ShufflePicker(1, 45);
+        this.lotteries = IntStream.range(0, numberGames)
+                .mapToObj(i -> new LotteryNumbers(picker))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public Map<Integer, Long> getMatches(LotteryNumbers winningNumber) {
+        return lotteries.stream()
+                .map(winningNumber::countMatched)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        int nGames = inputView.getMoneyToBuy() / 1000;
+        GameManagement game = new GameManagement(nGames);
+        outputView.showLotteries(game.lotteries);
+
+        Map<Integer, Long> winnings = game.getMatches(inputView.getWinningNumber());
+        outputView.showWinnings(nGames * 1000, winnings);
+    }
+}
