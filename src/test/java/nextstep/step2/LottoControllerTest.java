a4313ec6b660cfda1c66f7177f116e7da9862c66
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-24 20:28:47 +0900

    test:테스트 보강 및 패키지 변경

---
+++ src/test/java/nextstep/step2/LottoControllerTest.java
@@ -0,0 +1,57 @@
+package nextstep.step2;
+
+import nextstep.step2.domain.*;
+import nextstep.step2.view.LottoResultView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LottoControllerTest {
+	private LottoController underTest = new LottoController();
+
+	@Test
+	@DisplayName("로또를 사고 지난주 당첨번호로 당첨통계를 구한다.")
+	public void startLottoGameTest() {
+		LottoPurchase lottoPurchase = new LottoPurchase(10_000);
+		LottoResultView resultView = new LottoResultView();
+		LottoNumber bonusNumber = LottoNumber.of(7);
+		LottoCount lottoCount = new LottoCount(1, lottoPurchase.getLottoCount());
+		String manualLottos = "1,2,3,13,14,15:20,21,22,23,24,25";
+		List<Lotto> autoLottos = underTest.startLottoGameGetLottos(lottoCount, manualLottos, resultView);
+		underTest.printLottoStaticsic("1,2,3,4,5,6", bonusNumber, lottoPurchase, autoLottos, resultView);
+	}
+
+	@Test
+	@DisplayName("수동로또를 구한다")
+	public void purchaseManualLottosTest() {
+		String manualLottos = "1,2,3,13,14,15:20,21,22,23,24,25:30,31,32,33,34,35";
+		List<Lotto> manualLottoList = underTest.purchaseManualLottos(manualLottos);
+		assertTrue(manualLottoList.get(0).getNumbers().contains(new LottoNumber(1)));
+		assertTrue(manualLottoList.get(1).getNumbers().contains(new LottoNumber(20)));
+		assertTrue(manualLottoList.get(2).getNumbers().contains(new LottoNumber(30)));
+	}
+
+	@Test
+	@DisplayName("자동로또를 구한다.")
+	public void purchaseAutoLottosTest() {
+		List<Lotto> autoLottoList = underTest.purchaseAutoLottos(2);
+		assertThat(autoLottoList).hasSize(2);
+	}
+
+	@Test
+	@DisplayName("수동과 자동으로 구입한 로또를 합친다.")
+	public void mergeLottosTest() {
+		String manualLottos = "1,2,3,13,14,15:20,21,22,23,24,25:30,31,32,33,34,35";
+		List<Lotto> manualLottoList = underTest.purchaseManualLottos(manualLottos);
+		List<Lotto> autoLottoList = underTest.purchaseAutoLottos(3);
+		List<Lotto> lottos = underTest.mergeLottos(manualLottoList, autoLottoList);
+		assertThat(lottos).hasSize(6);
+		assertThat(lottos.get(0).getNumbers()).isEqualTo(MockLotto.mockLotto(Arrays.asList(1,2,3,13,14,15)).getNumbers());
+
+	}
+}
