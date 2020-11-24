db90ddccaa84ca6b17d5922af211675be21bc08b
apalsl <apalsl123@gmail.com>
2020-11-24 23:46:58 +0900

    del step2 code
    코드 초기화 함.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,5 @@
-import step1.StringAddCalculatorMain;
-import step2.LottoMain;
-
 public class Main {
 
     public static void main(String[] args) {
-        LottoMain lottoMain = new LottoMain();
-        lottoMain.run();
     }
 }
