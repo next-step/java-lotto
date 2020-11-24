a7cd1f207bdb5c2cd12ad0930bd4a413d65b2f68
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-24 20:50:39 +0900

    feat 로또 게임 client

--- src/main/java/step3/lotto/client/LottoGameApp.java
+++ src/main/java/step3/lotto/client/LottoGameApp.java
@@ -5,6 +5,7 @@ import step3.lotto.controller.LottoController;
 public class LottoGameApp {
     private static LottoController lottoController = new LottoController();
 
+
     public static void main(String[] args) {
         lottoController.gameStart();
     }
