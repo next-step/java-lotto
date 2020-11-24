02e756bf58416a01ef5bfbe221907e3fedfe4e01
dbeod2 <darung.0211@gmail.com>
2020-11-25 01:37:54 +0900

    feat : 수동 구매 번호 input 메서드 생성

--- src/main/java/step4/view/LottoInputUi.java
+++ src/main/java/step4/view/LottoInputUi.java
@@ -1,6 +1,12 @@
 package step4.view;
 
+import step4.domain.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoInputUi {
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -25,13 +31,25 @@ public class LottoInputUi {
         return SCANNER.nextInt();
     }
 
-    public static String inputManualLottoNumbers() {
+    public static List<Lotto> askManualLottoNumbers(int numberOfManualLottos) {
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
-        return SCANNER.nextLine();
+        SCANNER.skip("[\\r\\n]+");
+
+        return IntStream.range(0, numberOfManualLottos)
+                .mapToObj(i -> setManualLottoNumbers())
+                .collect(Collectors.toList());
+    }
+
+    private static Lotto setManualLottoNumbers() {
+        Lotto lotto = new Lotto();
+        lotto.getLottos().clear();
+
+        Arrays.stream(SCANNER.nextLine().split(","))
+                .forEach(lottoNumber -> lotto.getLottos().add(Integer.parseInt(lottoNumber)));
+        return lotto;
     }
 
     public static void close() {
         SCANNER.close();
     }
-
 }
