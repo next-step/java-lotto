9751edc8b11f07b32a16e03c5fff3bf94e3dcc2e
rlawls1991 <34434550+rlawls1991@users.noreply.github.com>
2020-11-24 20:49:48 +0900

    feat 로또에 관련된 스토어

--- src/main/java/step3/lotto/domain/LottoShop.java
+++ src/main/java/step3/lotto/domain/LottoShop.java
@@ -5,7 +5,6 @@ import step3.lotto.domain.numbers.LottoMachine;
 import java.util.*;
 
 public class LottoShop {
-
     private static final int MAKE_LOTTONUMBER_COUNT = 45; // 로또번호 생성 갯수
     private static final int MAKE_AUTO_NUMBER = 6; // 한게임당 필요한 번호 갯수
     private static LottoMachine lottoMachine;
