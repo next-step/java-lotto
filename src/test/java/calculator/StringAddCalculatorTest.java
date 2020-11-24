7f8c89191e7d18d4066db3725fc5c4388447d27b
naru <hsik0225@gmail.com>
2020-11-24 21:06:34 +0900

    feat : @DisplayName 추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringAddCalculatorTest {
 
     @Test
+    @DisplayName("null과 빈 문자열을 입력시 0을 반환")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.add(null);
         assertThat(result).isEqualTo(0);
@@ -17,31 +19,36 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    @DisplayName("하나의 숫자를 입력하면 그 숫자를 반환")
+    public void splitAndSum_숫자하나() {
         int result = StringAddCalculator.add("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    @DisplayName("쉼표로 구분하여 숫자들을 스플릿 후 합산")
+    public void splitAndSum_쉼표구분자() {
         int result = StringAddCalculator.add("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    @DisplayName("쉼표 또는 콜론으로 구분하여 숫자들을 스플릿 후 합산")
+    public void splitAndSum_쉼표_또는_콜론_구분자() {
         int result = StringAddCalculator.add("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
+    @DisplayName("Custom 구분자로 구분하여 숫자들을 스플릿 후 합산")
+    public void splitAndSum_custom_구분자() {
         int result = StringAddCalculator.add("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_negative() throws Exception {
+    @DisplayName("음수 입력시 Runtime Exception 발생")
+    public void splitAndSum_negative() {
         assertThatThrownBy(() -> StringAddCalculator.add("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
