10c6c237bee368e1b40cc23ecefad356b989e072
naru <hsik0225@gmail.com>
2020-11-24 18:41:28 +0900

    feat : 음수 입력시 예외 발생 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -9,7 +9,7 @@ public class StringAddCalculator {
 
     public static final String DEFAULT_DELIMITER_REGEX = ",|:";
 
-    private static Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public static int add(final String input) {
         if (input == null || input.isEmpty()) {
@@ -28,7 +28,12 @@ public class StringAddCalculator {
         int sum = 0;
         String[] tokens = expression.split(delimiter);
         for (String token : tokens) {
-            sum += Integer.parseInt(token);
+            int number = Integer.parseInt(token);
+            if (number < 0) {
+                throw new RuntimeException();
+            }
+
+            sum += number;
         }
 
         return sum;
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
@@ -37,5 +38,11 @@ public class StringAddCalculatorTest {
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.add("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.add("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
     }
 }
