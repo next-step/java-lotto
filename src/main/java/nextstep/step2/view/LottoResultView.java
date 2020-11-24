5204162072baa5abc52707023307691ce21c4bf4
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-24 20:23:04 +0900

    feat:로또구매 메시지 변경

--- src/main/java/nextstep/step2/view/LottoResultView.java
+++ src/main/java/nextstep/step2/view/LottoResultView.java
@@ -1,6 +1,7 @@
 package nextstep.step2.view;
 
 import nextstep.step2.domain.Lotto;
+import nextstep.step2.domain.LottoCount;
 import nextstep.step2.domain.LottoReward;
 
 import java.util.Comparator;
@@ -8,14 +9,14 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoResultView {
-	private static final String LOTTO_PURCHASE_MESSAGE = "%d개를 구매했습니다.";
+	private static final String LOTTO_PURCHASE_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
 	private static final String LOTTO_STATISTICTS = "당첨통계\n---------";
 	private static final String LOTTO_STATISTICTS_RESULT = "%d개 일치 (%d원)- %d개";
 	private static final String LOTTO_STATISTICTS_BONUS_RESULT = "%d개 일치, 보너스 볼 일치(%d원)- %d개";
 	private static final String LOTTO_PROBABILITY = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-	public void printLottoPurchase(int lottoCount) {
-		System.out.println(String.format(LOTTO_PURCHASE_MESSAGE, lottoCount));
+	public void printLottoPurchase(LottoCount lottoCount) {
+		System.out.println(String.format(LOTTO_PURCHASE_MESSAGE, lottoCount.getManual(), lottoCount.getAuto()));
 	}
 
 	public void printLottoStaticsic(Map<LottoReward, List<Lotto>> lottoRewardListMap) {
