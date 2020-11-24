46dc221148d29c59e298a675f0439a3731590475
Sunghyeob.Baek <baek9652@naver.com>
2020-11-24 22:30:45 +0900

    [test] RankTest 테스트 케이스 강화

--- src/test/java/lotto/RankTest.java
+++ src/test/java/lotto/RankTest.java
@@ -3,32 +3,14 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RankTest {
-
-  private Map<Rank, Integer> sample;
-
-  @BeforeEach
-  void setUp() {
-    this.sample = new HashMap<>();
-    sample.put(Rank.FIRST, 0);
-    sample.put(Rank.SECOND, 1);
-    sample.put(Rank.THIRD, 0);
-    sample.put(Rank.FOURTH, 0);
-    sample.put(Rank.FIFTH, 1);
-    sample.put(Rank.MISS, 0);
-  }
-
-  @Test
-  void testSumExactly() {
-    assertThat(Rank.calculateTotalReward(sample))
-        .isEqualTo(Rank.SECOND.getWinningReward() + Rank.FIFTH.getWinningReward());
-  }
 
   @Test
   @DisplayName("티켓에 보너스 번호를 같이 보냈을 때 정상적으로 2등을 체크 하는지 테스트")
@@ -42,5 +24,27 @@ class RankTest {
   void testScoringWithBonusNumber() {
     assertThat(Rank.getRewardWithBonusBall(5, false))
         .isEqualTo(Rank.THIRD);
+  }
+
+  // TODO: true false 중복 줄일 방법 찾기
+  private static Stream<Arguments> provideEnumRewardMatch() {
+    return Stream.of(
+        Arguments.of(0, Rank.MISS, true),
+        Arguments.of(3, Rank.FIFTH, true),
+        Arguments.of(4, Rank.FOURTH, true),
+        Arguments.of(6, Rank.FIRST, true),
+        Arguments.of(0, Rank.MISS, false),
+        Arguments.of(3, Rank.FIFTH, false),
+        Arguments.of(4, Rank.FOURTH, false),
+        Arguments.of(6, Rank.FIRST, false)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideEnumRewardMatch")
+  @DisplayName("5등 제외하고 나머지")
+  void testOtherRanksWhenMatchedWithBonus(int numHit, Rank expected, boolean bonusBallMatched) {
+    assertThat(Rank.getRewardWithBonusBall(numHit, bonusBallMatched))
+        .isEqualTo(expected);
   }
 }
