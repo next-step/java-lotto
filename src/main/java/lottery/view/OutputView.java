2b992c19a76726f90dd295a25a0583d4418ff22a
kriery <jongchan.lee@lmep.kr>
2020-11-24 22:59:58 +0900

    refactor: `WinningType`을 domain으로 정리, 타입별 조건을 깔끔하게 기술

--- src/main/java/lottery/view/OutputView.java
+++ src/main/java/lottery/view/OutputView.java
@@ -1,6 +1,7 @@
 package lottery.view;
 
 import lottery.domain.LotteryNumbers;
+import lottery.domain.WinningType;
 
 import java.io.PrintStream;
 import java.util.List;
@@ -36,27 +37,12 @@ public class OutputView {
         out.println("당첨 통계");
         out.println("---------");
         for(WinningType winningType : WinningType.values()) {
-            long countMatched = winningResult.getOrDefault(winningType.matches, 0L);
-            earned += winningType.earning * countMatched;
-            out.format("%1$d개 일치 (%2$d)원- %3$d개", winningType.matches, winningType.earning, countMatched);
+            long countMatched = winningResult.getOrDefault(winningType.getMatches(), 0L);
+            earned += winningType.getEarning() * countMatched;
+            out.format("%1$d개 일치 (%2$d)원- %3$d개", winningType.getMatches(), winningType.getEarning(), countMatched);
             out.println();
         }
         out.format("총 수익률은 %1$.2f입니다.", (double)earned / spent);
         out.println();
-    }
-
-    enum WinningType {
-        fifth( 3, 5000),
-        fourth(4, 50000),
-        third( 5, 1500000),
-        first( 6, 2000000000);
-
-        private final int matches;
-        private final int earning;
-
-        WinningType(int matches, int earning) {
-            this.matches = matches;
-            this.earning = earning;
-        }
     }
 }
