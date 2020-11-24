8db08d2aba239e3e16a4f0df97b56806395282dd
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-24 20:28:33 +0900

    feat:로또구매 수동개수를 입력받아 자동,수동로또를 구매하고 확률을 구한다.

--- src/main/java/nextstep/step2/LottoController.java
+++ src/main/java/nextstep/step2/LottoController.java
@@ -5,14 +5,17 @@ import nextstep.step2.view.LottoResultView;
 import nextstep.step2.view.LottoTicketView;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoController {
 
-	public List<Lotto> startLottoGameGetLottos(LottoPurchase purchase, LottoResultView resultView) {
-		resultView.printLottoPurchase(purchase.getLottoCount());
-		return printAutoLottoTicketsAndLottos(purchase);
+	public List<Lotto> startLottoGameGetLottos(LottoCount lottoCount, String manaulLottos, LottoResultView resultView) {
+		resultView.printLottoPurchase(lottoCount);
+		return printLottoListAndReturnLottos(lottoCount, manaulLottos);
 	}
 
 	public void printLottoStaticsic(String winnerNumbers, LottoNumber bonusNumber, LottoPurchase purchase, List<Lotto> lottoList, LottoResultView resultView) {
@@ -22,20 +25,36 @@ public class LottoController {
 		resultView.printWinningProbability(lottoStaticstic.calculateWinningProbability(lottoRewardListMap));
 	}
 
-	private List<Lotto> printAutoLottoTicketsAndLottos(LottoPurchase purchase) {
-		int lottoCount = purchase.getLottoCount();
-		List<Lotto> lottoList = purchaseLottos(lottoCount);
-		LottoTicketView lottoTicketView = new LottoTicketView();
-		lottoTicketView.printLottoTickets(lottoList);
-		return lottoList;
+	private List<Lotto> printLottoListAndReturnLottos(LottoCount lottoCount, String manaulLottos) {
+		List<Lotto> manualList = purchaseManualLottos(manaulLottos);
+		List<Lotto> autoList = purchaseAutoLottos(lottoCount.getAuto());
+		List<Lotto> purchaseLottos = mergeLottos(manualList, autoList);
+		new LottoTicketView().printLottoTickets(purchaseLottos);
+		return purchaseLottos;
 	}
 
-	private List<Lotto> purchaseLottos(int lottoCount) {
-		LottoAutoGenerator lottoAutoGenerator = new LottoAutoGenerator();
-		List<Lotto> lottos = new ArrayList<>(lottoCount);
-		for(int i = 0; i < lottoCount; i++) {
-			lottos.add(lottoAutoGenerator.getAutoNumbers());
+	protected List<Lotto> purchaseManualLottos(String manualLottos) {
+		String[] lottoNumbers = manualLottos.split(":");
+		List<Lotto> lottos = new ArrayList<>(lottoNumbers.length);
+		for(String numbers : lottoNumbers) {
+			lottos.add(new Lotto().makeLottoWithNumbers(numbers));
 		}
 		return lottos;
+	}
+
+	protected List<Lotto> purchaseAutoLottos(int lottoCount) {
+		LottoAutoGenerator lottoGenerator = new LottoAutoGenerator();
+		List<Lotto> lottos = new ArrayList<>(lottoCount);
+		for(int i = 0; i < lottoCount; i++) {
+			lottos.add(lottoGenerator.getAutoNumbers());
+		}
+		return lottos;
+	}
+
+	protected List<Lotto> mergeLottos(List<Lotto> manaual, List<Lotto> auto) {
+		return Stream.of(manaual, auto)
+				.flatMap(Collection::stream)
+				.collect(Collectors.toList());
+
 	}
 }
--- src/main/java/nextstep/step2/LottoGame.java
+++ src/main/java/nextstep/step2/LottoGame.java
@@ -1,5 +1,6 @@
 package nextstep.step2;
 
+import nextstep.step2.domain.LottoCount;
 import nextstep.step2.domain.LottoNumber;
 import nextstep.step2.domain.Lotto;
 import nextstep.step2.domain.LottoPurchase;
@@ -12,17 +13,46 @@ import java.util.Scanner;
 public class LottoGame {
 	public static void main(String[] args) {
 		LottoController controller = new LottoController();
-		InputView inputMoney = new InputView(new Scanner(System.in));
 		LottoResultView resultView = new LottoResultView();
-
-		LottoPurchase purchase = new LottoPurchase(inputMoney.getInputMoney());
-		List<Lotto> autoLottos = controller.startLottoGameGetLottos(purchase, resultView);
-
-		InputView inputWinningNumbers = new InputView(new Scanner(System.in));
-		final String winningNumber = inputWinningNumbers.getInputLottoWinningNumbers();
-
-		InputView inputBonusNumber = new InputView(new Scanner(System.in));
-		final LottoNumber bonusNumber = LottoNumber.of(inputBonusNumber.getInputLottoBonusNumber());
+		int money = getMoney(); //구입금액 입력
+		int manualCount = getManualCount(); //수동 구매수 입력
+		LottoPurchase purchase = new LottoPurchase(money);
+		String manaulLottos = getManualLottos(manualCount); //수동 로또번호 입력
+		//수동, 자동 로또번호 출력
+		List<Lotto> autoLottos = getPurchaseLottos(controller, resultView, manualCount, purchase, manaulLottos);
+		final String winningNumber = getLastWeekLotto(); //지난주 로또번호 입력
+		final LottoNumber bonusNumber = getBonusNumber();  //보너스 번호 입력
+		//당첨통계 출력
 		controller.printLottoStaticsic(winningNumber, bonusNumber, purchase, autoLottos, resultView);
+	}
+
+	private static LottoNumber getBonusNumber() {
+		InputView inputBonusNumber = new InputView(new Scanner(System.in));
+		return LottoNumber.of(inputBonusNumber.getInputLottoBonusNumber());
+	}
+
+	private static String getLastWeekLotto() {
+		InputView inputWinningNumbers = new InputView(new Scanner(System.in));
+		return inputWinningNumbers.getInputLottoWinningNumbers();
+	}
+
+	private static List<Lotto> getPurchaseLottos(LottoController controller, LottoResultView resultView, int manualCount, LottoPurchase purchase, String manaulLottos) {
+		LottoCount lottoCount = new LottoCount(manualCount, purchase.getLottoCount());
+		return controller.startLottoGameGetLottos(lottoCount, manaulLottos, resultView);
+	}
+
+	private static String getManualLottos(int manualCount) {
+		InputView inputManualNumbers = new InputView(new Scanner(System.in));
+		return inputManualNumbers.getInputManualNumbers(manualCount);
+	}
+
+	private static int getManualCount() {
+		InputView inputManualCount = new InputView(new Scanner(System.in));
+		return inputManualCount.getInputManualCount();
+	}
+
+	private static int getMoney() {
+		InputView inputMoney = new InputView(new Scanner(System.in));
+		return inputMoney.getInputMoney();
 	}
 }
