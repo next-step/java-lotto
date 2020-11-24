257572518c3f8fcb7b4eac28131c5b0788b0389e
최종원 <90lastone@gmail.com>
2020-11-24 23:25:41 +0900

    test: 총 수익률 테스트 추가

--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -35,4 +35,22 @@ public class LottoTest {
 
         assertThat(winCounts[0][0]).isEqualTo(1);
     }
+
+    @Test
+    public void 총수익률() {
+        Person person = new Person();
+        List<Integer> lottoNumber = new ArrayList<>();
+        lottoNumber.add(1);
+        lottoNumber.add(2);
+        lottoNumber.add(3);
+        lottoNumber.add(7);
+        lottoNumber.add(8);
+        lottoNumber.add(9);
+        Lotto lotto = new Lotto(lottoNumber);
+        person.addLotto(lotto);
+        int[] winNumber = new int[]{1, 2, 3, 4, 5, 6};
+        int[][] winCounts = person.getWin(winNumber);
+
+        assertThat(person.getTotalRevenue(1000, winCounts)).isEqualTo(500.0);
+    }
 }
