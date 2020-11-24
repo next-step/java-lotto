96720ed1a6b11470418d15a53914e2d620d2e05b
naru <hsik0225@gmail.com>
2020-11-24 18:39:34 +0900

    feat : 커스텀 구분자 덧셈 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,15 +1,32 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 public class StringAddCalculator {
 
-    public static int splitAndSum(String input) {
+    public static final String DEFAULT_DELIMITER_REGEX = ",|:";
+
+    private static Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    public static int add(final String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        int sum = 0;
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            splitAndSum(matcher.group(2), customDelimiter);
+        }
+        return splitAndSum(input, DEFAULT_DELIMITER_REGEX);
+    }
 
-        String[] tokens = input.split(",|:");
+    private static int splitAndSum(final String expression, final String delimiter) {
+        int sum = 0;
+        String[] tokens = expression.split(delimiter);
         for (String token : tokens) {
             sum += Integer.parseInt(token);
         }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -8,22 +8,34 @@ public class StringAddCalculatorTest {
 
     @Test
     public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+        int result = StringAddCalculator.add(null);
         assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
+        result = StringAddCalculator.add("");
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = StringAddCalculator.add("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = StringAddCalculator.add("1,2");
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.add("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.add("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
     }
 }
