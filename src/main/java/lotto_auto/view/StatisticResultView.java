212aa97df7575e681802b3cc4d61ce1ee8d1933f
이병무 <am003507@gmail.com>
2020-11-25 15:55:14 +0900

    refactor - 보너스 문자열 추가

--- src/main/java/lotto_auto/view/StatisticResultView.java
+++ src/main/java/lotto_auto/view/StatisticResultView.java
@@ -5,12 +5,14 @@ import lotto_auto.model.LottoStatistic;
 
 public class StatisticResultView {
 
-    private static final String STATISTIC_MESSAGE = "%s개 일치 (%s원)- %s개";
+    private static final String STATISTIC_MESSAGE = "%s개 일치 %s(%s원)- %s개";
     private static final String STRING_DIVIDER = "---------";
     private static final String WINNING_STATISTIC_MESSAGE = "당첨 통계";
     private static final String TOTAL_YIELD_MESSAGE = "총 수익률은 %.2f 입니다.";
     private static final String BENEFIT_MESSAGE = "기준이 1이기 때문에 결과적으로 이득이라는 의미임";
     private static final String LOSE_MESSAGE = "기준이 1이기 때문에 결과적으로 손해이라는 의미임";
+    private static final String BONUS_NUMBER_MATCH_MESSAGE = "보너스 볼 일치";
+    private static final String EMPTY_STRING = "";
 
     public static void printStatistic(LottoStatistic lottoStatistic) {
         StringBuilder sb = new StringBuilder();
@@ -33,7 +35,14 @@ public class StatisticResultView {
 
     private static String buildStatisticMessage(LottoStatistic lottoStatistic, int rank) {
         DrawResult drawResult = DrawResult.valueOfRank(rank);
-        return String.format(STATISTIC_MESSAGE, drawResult.getMatchNum(), drawResult.getMoney(), lottoStatistic.getRankLottoCount(rank));
+        return String.format(STATISTIC_MESSAGE, drawResult.getMatchNum(), buildBonusMessage(drawResult.isBonus()), drawResult.getMoney(), lottoStatistic.getRankLottoCount(rank));
+    }
+
+    private static String buildBonusMessage(boolean isBonus) {
+        if (isBonus) {
+            return BONUS_NUMBER_MATCH_MESSAGE;
+        }
+        return EMPTY_STRING;
     }
 
     private static String buildYieldDetailMessage(LottoStatistic lottoStatistic) {
