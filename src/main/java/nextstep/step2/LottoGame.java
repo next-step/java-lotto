21a5a21689d6b73b2d7f4ae5f681307ef85626ad
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-25 21:27:21 +0900

    refactor:불필요한 메소드 제거

--- src/main/java/nextstep/step2/LottoGame.java
+++ src/main/java/nextstep/step2/LottoGame.java
@@ -12,17 +12,13 @@ public class LottoGame {
 		final int manualCount = InputView.getInputManualCount(); //수동 구매수 입력
 		String manaulLottos = InputView.getInputManualNumbers(manualCount); //수동 로또번호 입력
 		LottoPurchase purchase = new LottoPurchase(money);
-		Lottos autoLottos = getPurchaseLottos(controller, manualCount, purchase, manaulLottos);
+		LottoCount lottoCount = new LottoCount(manualCount, purchase.getLottoCount());
+		Lottos purchaseLottos = controller.startLottoGameGetLottos(lottoCount, manaulLottos);
 
 		//당첨통계 출력
 		final String winningNumber = InputView.getInputLottoWinningNumbers(); //지난주 로또번호 입력
 		final LottoNumber bonusNumber = LottoNumber.of(InputView.getInputLottoBonusNumber());  //보너스 번호 입력
 		LottoStaticstic lottoStaticstic = new LottoStaticstic(winningNumber, purchase, bonusNumber);
-		controller.printLottoStaticsic(lottoStaticstic, autoLottos);
-	}
-
-	private static Lottos getPurchaseLottos(LottoController controller, int manualCount, LottoPurchase purchase, String manaulLottos) {
-		LottoCount lottoCount = new LottoCount(manualCount, purchase.getLottoCount());
-		return controller.startLottoGameGetLottos(lottoCount, manaulLottos);
+		controller.printLottoStaticsic(lottoStaticstic, purchaseLottos);
 	}
 }
