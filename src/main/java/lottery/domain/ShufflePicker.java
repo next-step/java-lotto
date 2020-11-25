042a1fe03ed84456cfcd80bb34b84673def616ea
kriery <jongchan.lee@lmep.kr>
2020-11-25 10:41:46 +0900

    refactor: change `ShufflePicker` basis list `static`

--- src/main/java/lottery/domain/ShufflePicker.java
+++ src/main/java/lottery/domain/ShufflePicker.java
@@ -6,17 +6,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ShufflePicker implements Picker {
-    private final List<LotteryNumber> basis;
-
-    public ShufflePicker() {
-        basis = IntStream.rangeClosed(LotteryNumber.MIN, LotteryNumber.MAX)
+    private static final List<LotteryNumber> BASIS =
+            IntStream.rangeClosed(LotteryNumber.MIN, LotteryNumber.MAX)
                 .mapToObj(LotteryNumber::valueOf)
                 .collect(Collectors.toList());
-    }
 
     @Override
     public List<LotteryNumber> pick() {
-        Collections.shuffle(basis);
-        return basis.subList(0, Lottery.COUNT_LOTTERY_NUMBER);
+        Collections.shuffle(BASIS);
+        return BASIS.subList(0, Lottery.COUNT_LOTTERY_NUMBER);
     }
 }
