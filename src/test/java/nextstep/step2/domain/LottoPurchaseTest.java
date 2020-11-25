9375c4ca42a0c8acf3cdc4a93e86c092127b310f
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-25 21:27:49 +0900

    refactor:로또구매금액이 부족할때 에러처리

--- src/test/java/nextstep/step2/domain/LottoPurchaseTest.java
+++ src/test/java/nextstep/step2/domain/LottoPurchaseTest.java
@@ -1,5 +1,6 @@
 package nextstep.step2.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,14 @@ public class LottoPurchaseTest {
 	public void getPurchaseMessageTest() {
 		LottoPurchase underTest = new LottoPurchase(14_000);
 		assertThat(underTest.getLottoCount()).isEqualTo(14);
+	}
+
+	@Test
+	@DisplayName("로또 구입 금액이 부족한 테스트 ")
+	public void getPurchaseErrorTest() {
+		LottoPurchase underTest = new LottoPurchase(500);
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> underTest.getLottoCount())
+				.withMessage("로또 구매금액이 부족합니다.");
 	}
 }
