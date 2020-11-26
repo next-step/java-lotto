eb9f104c1da9fa1c2ff5a662d36954211603e49e
kriery <jongchan.lee@lmep.kr>
2020-11-26 23:20:58 +0900

    test: 추가된 Picker 기능에 대한 TC 추가

--- src/test/java/lottery/domain/ShufflePickerTest.java
+++ src/test/java/lottery/domain/ShufflePickerTest.java
@@ -3,10 +3,19 @@ package lottery.domain;
 import lottery.domain.strategy.ShufflePicker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ShufflePickerTest {
+    @DisplayName("shuffle시 count만큼 뽑는지 검사")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+    void pick_count(int count) {
+        assertThat(new ShufflePicker().pick(count).size()).isEqualTo(count);
+    }
+
     @DisplayName("shuffle 값이 범위 내에 있는지 검사")
     @RepeatedTest(20)
     void pick() {
