b7bbffeeb0cf18e43a599212fe5a69d905715962
rlawls1991 <rlawls1991@gmail.com>
2020-11-26 23:23:41 +0900

    refactor 1. LottoTicket 생성으로 인한 코드 수정 2. getRnak 메소드가 길어짐에 따라 코드 분리

--- src/main/java/step3/lotto/domain/numbers/LottoRank.java
+++ src/main/java/step3/lotto/domain/numbers/LottoRank.java
@@ -6,12 +6,12 @@ import java.util.SortedSet;
 
 public class LottoRank {
 
-    private static final Integer DEFAULT_LOTTO_RANK = 7; // 로또 등수구할때 기본으로 세팅되어있는 값
-    private static final Integer MIN_LOTTO_WINNING_RANK = 6;
-    List<Integer> winningNumbers = new ArrayList<>();
+    private static final int DEFAULT_LOTTO_RANK = 7; // 로또 등수구할때 기본으로 세팅되어있는 값
+    private static final int MIN_LOTTO_WINNING_RANK = 6;
+    private LottoTicket winningNumbers;
     private int bonusNumber = 0;
 
-    LottoRank(int bonusNumber, List<Integer> winningNumbers) {
+    LottoRank(int bonusNumber, LottoTicket winningNumbers) {
         this.bonusNumber = bonusNumber;
         this.winningNumbers = winningNumbers;
     }
@@ -23,9 +23,20 @@ public class LottoRank {
             lottoRank = checkSameValue(number, lottoRank);
         }
 
+        lottoRank = findRnak(lottoRank, paramValue);
+
+        return lottoRank;
+    }
+
+    private int findRnak(int lottoRank, SortedSet<Integer> paramValue) {
         //1등이 아닐시 등급 1 추가
         if (lottoRank == 1) {
             return 1;
+        }
+
+        //당첨되지 않았을 시
+        if (lottoRank > MIN_LOTTO_WINNING_RANK) {
+            return 0;
         }
 
         lottoRank++;
@@ -35,25 +46,20 @@ public class LottoRank {
             lottoRank = checkLottoSecond(paramValue, lottoRank);
         }
 
-        //당첨되지 않았을 시
-        if (lottoRank > MIN_LOTTO_WINNING_RANK) {
-            return 0;
-        }
-
         return lottoRank;
     }
 
-    private  Integer checkSameValue(Integer paramValue, int lottoRank) {
+    private int checkSameValue(Integer paramValue, int lottoRank) {
         Integer returnValue = lottoRank;
 
-        if (winningNumbers.contains(paramValue)) {
+        if (winningNumbers.getLottoTicket().contains(paramValue)) {
             returnValue--;
         }
 
         return returnValue;
     }
 
-    private  int checkLottoSecond(SortedSet<Integer> paramValue, int lottoRank) {
+    private int checkLottoSecond(SortedSet<Integer> paramValue, int lottoRank) {
         if (paramValue.contains(bonusNumber)) {
             lottoRank--;
         }
