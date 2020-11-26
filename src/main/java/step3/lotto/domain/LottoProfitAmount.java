08936933a388c903358d88c51333676863351730
rlawls1991 <rlawls1991@gmail.com>
2020-11-26 23:23:11 +0900

    refactor 1. 코드 수정   - 1등부터 4등까지만 나타내게 나옴 -> 5등까지로 수정

--- src/main/java/step3/lotto/domain/LottoProfitAmount.java
+++ src/main/java/step3/lotto/domain/LottoProfitAmount.java
@@ -2,7 +2,7 @@ package step3.lotto.domain;
 
 public class LottoProfitAmount {
 
-    private static final int MATCH_LOTTE_MAX_COUNT = 4;
+    private static final int MATCH_LOTTE_MAX_COUNT = 5;
     private static final int MATCH_LOTTE_MIN_COUNT = 1;
 
     public static double runLottoProfitAmount() {
