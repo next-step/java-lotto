366e23e9a5b30e991c2a3c7534ac86d3cc59028e
MaroKim <dkgell6@gmail.com>
2020-11-27 01:58:36 +0900

    winningStatics를 enum class를 활용한 enumMap으로 변경 그에 따른 로직 수정

--- src/main/java/lotto/model/WinningCheckor.java
+++ src/main/java/lotto/model/WinningCheckor.java
@@ -1,47 +1,39 @@
 package lotto.model;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WinningCheckor {
-    List<Integer> preNumbers;
-    private Map<Integer, Integer> winningStatics;
+    Set<Integer> preNumbers;
+    EnumMap<Rank, Integer> winningStatics;
 
-    public WinningCheckor(List<Integer> preNumbers) {
+    public WinningCheckor(Set<Integer> preNumbers) {
         this.preNumbers = preNumbers;
-        winningStatics = new HashMap<>();
-        winningStatics.put(3, 0);
-        winningStatics.put(4, 0);
-        winningStatics.put(5, 0);
-        winningStatics.put(6, 0);
+        winningStatics = new EnumMap<>(Rank.class);
+        winningStatics.put(Rank.FIRST,0);
+        winningStatics.put(Rank.SECOND,0);
+        winningStatics.put(Rank.THIRD,0);
+        winningStatics.put(Rank.FOURTH,0);
+        winningStatics.put(Rank.NO_MATCH,0);
     }
 
     //당첨금 통계내기
-    public Map<Integer, Integer> makeStatics(LottoTickets lottoTickets) {
+    public EnumMap<Rank, Integer> makeStatics(LottoTickets lottoTickets) {
 
         for (int i = 0; i < lottoTickets.getTicketCount(); i++) {
             int winningCount = checkWinning(lottoTickets.oneOf(i).getTicketNumbers());
-            int count = winningStatics.containsKey(winningCount) ? winningStatics.get(winningCount) : 0;
-            winningStatics.put(winningCount, count + 1);
+            Rank winningRank = Rank.rank(winningCount);
+            winningStatics.put(winningRank, winningStatics.get(winningRank) + winningRank.getMoney());
         }
         return winningStatics;
     }
 
-    //당첨내역 확인하여 Map에 저장하기
+    //당첨개수 확인
     public int checkWinning(List<Integer> lottoTicket) {
         int winningCount = 0;
-        for (Integer preNumber : preNumbers) {
-            winningCount += hasNumber(lottoTicket, preNumber);
+        for (Integer number : lottoTicket) {
+            winningCount += preNumbers.contains(number) ? 1 : 0;
         }
         return winningCount;
     }
 
-    //이전 당첨 번호가 현재 티켓에 있는지 확인하기
-    public int hasNumber(List<Integer> lottoTicket, int number) {
-        if (lottoTicket.contains(number)) {
-            return 1;
-        }
-        return 0;
-    }
 }
