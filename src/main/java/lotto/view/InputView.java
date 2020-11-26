90346e219f362906082cb65b6d4b7e2a4f8517d5
MaroKim <dkgell6@gmail.com>
2020-11-27 01:39:20 +0900

    view단에서 다룰 필요 없는 정보 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,26 +5,20 @@ import lotto.model.LottoTickets;
 import java.util.*;
 
 public class InputView {
-    private Map<String, String> inputPrints;
     Scanner input;
+    private final String inputMoney = "구입금액을 입력해 주세요.";
+    private final String inputPreNumber = "지난 주 당첨 번호를 입력해 주세요.";
 
     public InputView() {
-        inputPrints = new HashMap<>();
-        inputPrints.put("inputMoney", "구입금액을 입력해 주세요.");
-        inputPrints.put("buyTickets", "개를 구매했습니다.");
-        inputPrints.put("inputPreNumber", "지난 주 당첨 번호를 입력해 주세요.");
         input = new Scanner(System.in);
     }
 
-    public int buyLotto(int ticketPrice) {
-        System.out.println(inputPrints.get("inputMoney"));
-        int price = input.nextInt();
+    public int buyLotto() {
+        System.out.println(inputMoney);
+        int money = input.nextInt();
         input.nextLine();
-        int tickets = price / ticketPrice;
-        System.out.println(tickets + inputPrints.get("buyTickets"));
 
-        return tickets;
-
+        return money;
     }
 
     public void printLottoNumbers(LottoTickets lottoTickets) {
@@ -33,11 +27,11 @@ public class InputView {
         }
     }
 
-    public List<Integer> inputPreWinningNumber() {
-        System.out.println(inputPrints.get("inputPreNumber"));
+    public Set<Integer> inputPreWinningNumber() {
+        System.out.println(inputPreNumber);
         String preWinningNumber = input.nextLine();
         String[] numbers = preWinningNumber.split(",");
-        List<Integer> preNumbers = new ArrayList<>();
+        Set<Integer> preNumbers = new HashSet<>();
 
         for (String number : numbers) {
             preNumbers.add(Integer.parseInt(number.trim()));
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,29 +1,26 @@
 package lotto.view;
 
-import java.util.Map;
+import lotto.model.Rank;
+
+import java.util.EnumMap;
 
 public class ResultView {
-    private int ticketCount;
+    private final String results = "당첨 통계\n---------";
+    public EnumMap<Rank, Integer> showWinningResult(EnumMap<Rank, Integer> winningStatics) {
+        System.out.println(results);
+        for(Rank rank : Rank.values()){
+            printExceptNomatch(rank, winningStatics);
+        }
 
-    public ResultView(int ticketCount) {
-        this.ticketCount = ticketCount;
+        return winningStatics;
     }
 
-    public void showWinningResult(Map<Integer, Integer> winningStatic) {
-        System.out.println("당첨 통계");
-        System.out.println("---------");
-        System.out.println("3개 일치 (5000원)- " + winningStatic.get(3) + "개");
-        System.out.println("4개 일치 (50000원)- " + winningStatic.get(4) + "개");
-        System.out.println("5개 일치 (1500000원)- " + winningStatic.get(5) + "개");
-        System.out.println("6개 일치 (2000000000원)- " + winningStatic.get(6) + "개");
-        resultBenefit(winningStatic);
+    public void printExceptNomatch(Rank rank, EnumMap<Rank, Integer> winningStatics){
+        if(rank.getMoney() !=0){
+            int count = winningStatics.get(rank) / rank.getMoney();
+            System.out.println(rank.getWinningCount()+"개 일치 ("+rank.getMoney()+"원)- "+count+"개");
+        }
     }
 
-    public void resultBenefit(Map<Integer, Integer> winningStatic) {
 
-        double price = winningStatic.get(3) * 5000 + winningStatic.get(4) * 50000
-                + winningStatic.get(5) * 1500000 + winningStatic.get(6) * 2000000000;
-        System.out.println("수익금 : " + price);
-        System.out.println("총 수익률은 " + (price / (ticketCount * 1000)) + "입니다.");
-    }
 }
