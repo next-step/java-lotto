7f49851b9af715a8a918f2f10495d15e8661c2e3
rlawls1991 <rlawls1991@gmail.com>
2020-11-28 02:50:36 +0900

    test enum 객체 추가

--- src/test/java/step4/lotto/domain/LottoStatusEnumTest.java
+++ src/test/java/step4/lotto/domain/LottoStatusEnumTest.java
@@ -11,14 +11,14 @@ public class LottoStatusEnumTest {
     private static LottoStatusEnum enumTest;
 
     @Test
-    public void 제대로_COUNT가_증가했는지() {
+    void 제대로_COUNT가_증가했는지() {
         LottoStatusEnum enumTest = LottoStatusEnum.findByCount(4);
         enumTest.addWinningCount();
         assertThat(enumTest.getWinningCount()).isEqualTo(1);
     }
 
     @Test
-    public void 제대로_RANK를_입력했는지() {
+    void 제대로_RANK를_입력했는지() {
         assertThatThrownBy(() ->
                 enumTest = LottoStatusEnum.findByCount(1000)
         ).isInstanceOf(RuntimeException.class);
