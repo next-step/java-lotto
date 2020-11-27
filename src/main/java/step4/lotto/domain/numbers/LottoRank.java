a908bf24ecc6c9f1e5eade6dc688e0537fa0e3d6
rlawls1991 <rlawls1991@gmail.com>
2020-11-28 02:48:20 +0900

    refactor 말씀하신 순위 구하는 부분은 enum으로 처리

--- src/main/java/step4/lotto/domain/numbers/LottoRank.java
+++ src/main/java/step4/lotto/domain/numbers/LottoRank.java
@@ -6,6 +6,7 @@ public class LottoRank {
 
     private static final int DEFAULT_LOTTO_RANK = 7; // 로또 등수구할때 기본으로 세팅되어있는 값
     private static final int MIN_LOTTO_WINNING_RANK = 6;
+    private static final int HIGH_RANK_DIVISION = 5;
     private LottoTicket winningNumbers;
     private int bonusNumber = 0;
 
@@ -14,55 +15,44 @@ public class LottoRank {
         this.winningNumbers = winningNumbers;
     }
 
-    public Integer getRank(SortedSet<Integer> paramValue) {
-        Integer lottoRank = DEFAULT_LOTTO_RANK;
+    public int getRank(SortedSet<Integer> paramSet) {
+        int lottoMatchCount = 0;
+        LottoRankEnum lottoRankEnum = null;
 
-        for (Integer number : paramValue) {
-            lottoRank = checkSameValue(number, lottoRank);
+        for (Integer number : paramSet) {
+            lottoMatchCount = checkSameValue(number, lottoMatchCount);
         }
 
-        lottoRank = findRank(lottoRank, paramValue);
-
-        return lottoRank;
-    }
-
-    private int findRank(int lottoRank, SortedSet<Integer> paramValue) {
-        //1등이 아닐시 등급 1 추가
-        if (lottoRank == 1) {
+        if (lottoMatchCount == 6) {
             return 1;
         }
 
-        //당첨되지 않았을 시
-        if (lottoRank > MIN_LOTTO_WINNING_RANK) {
-            return 0;
+        if (lottoMatchCount >= HIGH_RANK_DIVISION) {
+            lottoMatchCount = checkBounusNumber(bonusNumber, lottoMatchCount, paramSet);
+            lottoRankEnum = LottoRankEnum.findByRank(lottoMatchCount);
         }
 
-        lottoRank++;
-
-        // 3등과 2등 체크
-        if (lottoRank == 3) {
-            lottoRank = checkLottoSecond(paramValue, lottoRank);
-        }
-
-        return lottoRank;
+        return lottoRankEnum.getLottoRank();
     }
 
-    private int checkSameValue(Integer paramValue, int lottoRank) {
-        Integer returnValue = lottoRank;
+    private int checkSameValue(int paramValue, int lottoMatchCount) {
+        int returnValue = lottoMatchCount;
 
         if (winningNumbers.getLottoTicket().contains(paramValue)) {
-            returnValue--;
+            returnValue++;
         }
 
         return returnValue;
     }
 
-    private int checkLottoSecond(SortedSet<Integer> paramValue, int lottoRank) {
-        if (paramValue.contains(bonusNumber)) {
-            lottoRank--;
+    private int checkBounusNumber(int paramNumber, int lottoMatchCount, SortedSet<Integer> paramSet) {
+        int returnValue = lottoMatchCount;
+
+        if (paramSet.contains(paramNumber)) {
+            returnValue++;
         }
 
-        return lottoRank;
+        return returnValue;
     }
 
 }
