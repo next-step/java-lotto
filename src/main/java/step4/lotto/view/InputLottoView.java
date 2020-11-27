ecaa54c1b08ddbef403c3af7b79b48d39d4f1931
rlawls1991 <rlawls1991@gmail.com>
2020-11-28 02:04:37 +0900

    feat 수동 로또 갯수 및 로또번호 입력 기능 추가

--- src/main/java/step4/lotto/view/InputLottoView.java
+++ src/main/java/step4/lotto/view/InputLottoView.java
@@ -1,5 +1,7 @@
 package step4.lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputLottoView {
@@ -7,6 +9,8 @@ public class InputLottoView {
     private static String INPUT_THE_PURCHASE_AMOUNT = "구매금액을 입력해주세요";
     private static String INPUTTHE_LAST_WEEK_WINNING_AMOUNT = "지난주 당첨 결과를 입력해주세요";
     private static String INPUT_THE_BOUNS_NUMBER = "보너스 번호를 입력해주세요";
+    private static String INPUT_THE_MANUAL_BUY_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static String INPUT_THE_MANUAL_BUY_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
 
     private static Scanner sc = new Scanner(System.in);
 
@@ -15,9 +19,27 @@ public class InputLottoView {
 
     }
 
-    public static int inputValue() {
+    public static int inputPrice() {
         System.out.println(INPUT_THE_PURCHASE_AMOUNT);
         return Integer.parseInt(sc.nextLine());
+    }
+
+    public static int inputManualLottoBuyCount() {
+        System.out.println(INPUT_THE_MANUAL_BUY_COUNT);
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    public static List<String> InputManualNumber(int lottoManualBuyCount) {
+        List<String> returnValue = new ArrayList<>();
+        String manualNumber = "";
+        System.out.println(INPUT_THE_MANUAL_BUY_NUMBER);
+
+        for (int i = 0; i < lottoManualBuyCount; i++) {
+            manualNumber = sc.nextLine();
+            returnValue.add(manualNumber);
+        }
+
+        return returnValue;
     }
 
     public static String inputWinningLottoNumbers() {
