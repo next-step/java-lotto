3597c977afa9c82d1736f43647bf1d2e47f76976
rlawls1991 <rlawls1991@gmail.com>
2020-11-28 02:04:57 +0900

    feat 로또 수동번호 입력추가로 인한 기능 추가

--- src/main/java/step4/lotto/domain/numbers/LottoMachine.java
+++ src/main/java/step4/lotto/domain/numbers/LottoMachine.java
@@ -4,22 +4,32 @@ import step4.lotto.util.LottoErrorMessage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 로또 자동생성
  */
 public class LottoMachine {
     private static List<Integer> basicLottoNumbers = new ArrayList<>();
-    private static LottoAutoNumbers lottoAutoNumbers;
+    private static LottoAutoNumber lottoAutoNumbers;
+    private static LottoManualNumber lottoManualNumber;
 
     public LottoMachine() {
-        lottoAutoNumbers = new LottoAutoNumbers();
+        lottoAutoNumbers = new LottoAutoNumber();
     }
 
     public LottoTicket getAutoNumbers(int getLottoAutoNumberCount) {
         isNumberNegative(getLottoAutoNumberCount);
 
         return lottoAutoNumbers.getAutoNumbers(getLottoAutoNumberCount);
+    }
+
+    public Set<LottoTicket> getManualNumbers(List<String> manualNumber, int buyCount) {
+        isNumberNegative(buyCount);
+
+        lottoManualNumber = new LottoManualNumber(manualNumber, buyCount);
+
+        return lottoManualNumber.getManualLottoSet();
     }
 
     private void isNumberNegative(int paramValue) {
