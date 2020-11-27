b73fe088b779b14a07c9118657a3bfb688324723
rlawls1991 <rlawls1991@gmail.com>
2020-11-28 02:02:49 +0900

    feat 로또 수동 구매로 기능 추가

--- src/main/java/step4/lotto/controller/LottoController.java
+++ src/main/java/step4/lotto/controller/LottoController.java
@@ -5,24 +5,44 @@ import step4.lotto.domain.LottoGame;
 import step4.lotto.view.InputLottoView;
 import step4.lotto.view.OuterLottoView;
 
+import java.util.List;
+
 public class LottoController {
 
     public void gameStart() {
         LottoGame lottoGame;
-        int purchaseAmount = InputLottoView.inputValue();
-        lottoGame = new LottoGame(purchaseAmount);
-        lottoGame.buyLotto();
+        List<String> manualLottoNumber = null;
 
-        OuterLottoView.printBuyLotto(lottoGame.getLottoPurchaseCount());
-        OuterLottoView.printAutoLottoNumbers(lottoGame.getBuyLottoHashSet());
+        int purchaseAmount = InputLottoView.inputPrice();
+        int manualLottoBuyCount = InputLottoView.inputManualLottoBuyCount();
+
+        if (manualLottoBuyCount > 0) {
+            manualLottoNumber = InputLottoView.InputManualNumber(manualLottoBuyCount);
+        }
+
+        lottoGame = new LottoGame(purchaseAmount, manualLottoBuyCount);
+        buyLotto(lottoGame, manualLottoNumber);
+        outterLottoNumber(lottoGame);
 
         String inputLottoWinnerNumber = InputLottoView.inputWinningLottoNumbers();
         int bonusNumber = InputLottoView.inputBounsNumber();
 
         lottoGame.getLottoResult(inputLottoWinnerNumber, bonusNumber);
+        printLottoResultView(lottoGame);
+    }
 
+    private void buyLotto(LottoGame lottoGame, List<String> manualLottoNumber) {
+        lottoGame.buyManualLotto(manualLottoNumber);
+        lottoGame.buyAutoLotto();
+    }
+
+    private void outterLottoNumber(LottoGame lottoGame) {
+        OuterLottoView.printLottoHowToKinds(lottoGame.getLottoManualBuyCount(), lottoGame.getLottoAutoBuyCount());
+        OuterLottoView.printAutoLottoNumbers(lottoGame.getBuyLottoHashSet());
+    }
+
+    private void printLottoResultView(LottoGame lottoGame) {
         OuterLottoView.printLottoResult();
         OuterLottoView.printLottoProfitAmount(lottoGame.getLottoProfitAmount());
     }
-
 }
