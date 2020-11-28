9ecc3f403c4ed1b10b5c7d577a6b6a82b0f48a57
dbeod2 <darung.0211@gmail.com>
2020-11-28 17:25:11 +0900

    refactor : LottoNumber <-> Numbers 중복클래스로 인한 merge

--- src/main/java/step4/domain/Numbers.java
+++ src/main/java/step4/domain/Numbers.java
@@ -2,13 +2,13 @@ package step4.domain;
 
 import step4.util.StringUtil;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
-public class Numbers {
-
-    private static final int LOTTO_LIMIT_NUMBER = 6;
+public class Numbers implements Comparable<Numbers> {
+    public static final int START_NUM = 1;
+    public static final int END_NUM = 45;
     private static final String LOTTO_SPLIT_COMMA = ",";
 
     private String lottoNumber;
@@ -26,44 +26,42 @@ public class Numbers {
         return this.lottoNumber;
     }
 
+    private static void numberLimitValidate(String lottoNumber) {
+        String[] lottoNumbers = lottoNumber.split(LOTTO_SPLIT_COMMA);
+        IntStream.range(0, lottoNumbers.length)
+                .filter(i -> Integer.parseInt(lottoNumbers[i]) < START_NUM || Integer.parseInt(lottoNumbers[i]) > END_NUM)
+                .forEach(i -> {
+                    throw new IllegalArgumentException("로또 번호는 1 ~ 45사이의 숫자여야 합니다.");
+                });
+    }
+
     private static void isEmpty(String lottoNumber) {
         if (StringUtil.isEmpty(lottoNumber)) {
             throw new IllegalArgumentException("로또 번호를 입력해 주세요.");
         }
     }
 
-    private static void lottoNumberSizeValidate(String lottoNumber) {
-        String[] lottoNumbers = lottoNumber.split(LOTTO_SPLIT_COMMA);
-        if (lottoNumbers.length != LOTTO_LIMIT_NUMBER) {
-            throw new IllegalArgumentException("로또 번호는 6보다 작거나 클 수 없습니다.");
-        }
-    }
-
-    private static void numberValidate(String lottoNumber) {
-        Pattern positivePattern = Pattern.compile("-(?:[0-9])");
-        Pattern negativePattern = Pattern.compile("([0-9])");
-
-        if (positivePattern.matcher(lottoNumber).find()) {
-            throw new IllegalArgumentException("양수인 숫자만 입력 가능합니다.");
-        }
-
-        if (!negativePattern.matcher(lottoNumber).find()) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
-        }
-    }
-
-    private static void duplicate(String lottoNumber) {
-        String[] lottoNumbers = lottoNumber.split(LOTTO_SPLIT_COMMA);
-        lottoNumbers = new HashSet<>(Arrays.asList(lottoNumbers)).toArray(new String[0]);
-        if (lottoNumbers.length < 6) {
-            throw new IllegalArgumentException("로또 번호는 중복 될 수 없습니다.");
-        }
-    }
-
     private static void validate(String lottoNumber) {
         isEmpty(lottoNumber);
-        lottoNumberSizeValidate(lottoNumber);
-        numberValidate(lottoNumber);
-        duplicate(lottoNumber);
+        numberLimitValidate(lottoNumber);
     }
+
+    @Override
+    public int compareTo(Numbers o) {
+        return Integer.compare(Integer.parseInt(lottoNumber), Integer.parseInt(o.getLottoNumber()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Numbers number1 = (Numbers) o;
+        return lottoNumber.equals(number1.lottoNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
 }
