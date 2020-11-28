bcf455d25f917c1317c072e3502dc2b67856d20b
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-28 19:18:38 +0900

    test : ScannerTest 수정

--- src/test/java/lotto/ScannerTest.java
+++ src/test/java/lotto/ScannerTest.java
@@ -1,22 +1,27 @@
 package lotto;
 
+import lotto.domain.LottoResult;
 import lotto.domain.LottoTicket;
 import lotto.domain.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScannerTest {
 
     @Test
-    @DisplayName("당첨번호와 로또를 비교해서 일치하는 숫자를 로또에 저장한다")
-    public void scan_lotto_ticket() {
+    @DisplayName("로또티켓을 모두 스캔해서 수익률을 계산한다")
+    public void scan_all_lotto_tickets() {
+        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(1, 3, 23, 33, 35, 46));
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.add(lottoTicket);
         Scanner scanner = new Scanner(Arrays.asList(1, 3, 23, 33, 35, 46));
-        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(1, 3, 5, 6, 7, 8));
-        scanner.scan(lottoTicket);
-        assertThat(lottoTicket.getMatchingScore()).isEqualTo(2);
+        LottoResult lottoResult = scanner.scanAll(lottoTickets);
+        assertThat(lottoResult.getProfitRatio()).isEqualTo(2000000000/1000);
     }
 }
