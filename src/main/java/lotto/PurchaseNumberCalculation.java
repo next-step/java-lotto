10686b2f28f587a433f5e8694df9660f7d1b2fd6
monkeyDugi <lbd4946@gmail.com>
2020-11-28 09:57:02 +0900

    refactor : 매직넘버 -> 상수 변경

--- src/main/java/lotto/PurchaseNumberCalculation.java
+++ src/main/java/lotto/PurchaseNumberCalculation.java
@@ -2,15 +2,17 @@ package lotto;
 
 public class PurchaseNumberCalculation {
 
-    public static int calculateNumberPurchase(int purchaseAmount) {
-        if (purchaseAmount < 1000) {
+    private static final int PRICE_PER_TICKET = 1000;
+
+    public static int calculateNumberPurchase(int purchasePrice) {
+        if (purchasePrice < PRICE_PER_TICKET) {
             throw new IllegalArgumentException("1000원 이상을 투입 해주세요");
         }
 
-        if (purchaseAmount % 1000 != 0) {
+        if (purchasePrice % PRICE_PER_TICKET != 0) {
             throw new IllegalArgumentException("1000원 단위로 투입 해주세요");
         }
 
-        return purchaseAmount / 1000;
+        return purchasePrice / PRICE_PER_TICKET;
     }
 }
