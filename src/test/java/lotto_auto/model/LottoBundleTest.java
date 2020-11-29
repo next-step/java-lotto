663df43b6147f3507e980f9a3d580b852beaa45e
이병무 <am003507@gmail.com>
2020-11-29 21:53:37 +0900

    test - LottoBundle 테스트 코드 작성

--- src/test/java/lotto_auto/model/LottoBundleTest.java
+++ src/test/java/lotto_auto/model/LottoBundleTest.java
@@ -1,5 +1,35 @@
 package lotto_auto.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 class LottoBundleTest {
+
+    @DisplayName("로또 번들 티켓 카운트 테스트")
+    @Test
+    public void lottoBundleTicketCountTest() {
+        LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        LottoBundle lottoBundle = new LottoBundle(Arrays.asList(lottoTicket));
+        assertThat(lottoBundle.getTicketCount()).isEqualTo(1);
+    }
+
+    @DisplayName("로또 번들 티켓 카운트 테스트")
+    @Test
+    public void lottoBundleExportTest() {
+        LottoNumbers lottoNumbers = new LottoNumbers("1,2,3,4,5,6");
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        LottoBundle lottoBundle = new LottoBundle(Arrays.asList(lottoTicket));
+        List<LottoTicket> export = lottoBundle.export();
+        assertAll(() -> assertThat(export).isNotNull(),
+                () -> assertThat(export.size()).isEqualTo(1)
+        );
+    }
 
 }
