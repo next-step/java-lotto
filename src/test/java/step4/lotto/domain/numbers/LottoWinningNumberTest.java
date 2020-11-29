30e106dc4857bb1c35f804c0fcf5decd88a4c3c5
rlawls1991 <rlawls1991@gmail.com>
2020-11-30 02:33:08 +0900

    test 1. 에러메세지 확인 2. Lottonumber를 삭제 후 LottoTicket과 기능 합침

--- src/test/java/step4/lotto/domain/numbers/LottoWinningNumberTest.java
+++ src/test/java/step4/lotto/domain/numbers/LottoWinningNumberTest.java
@@ -1,6 +1,7 @@
 package step4.lotto.domain.numbers;
 
 import org.junit.jupiter.api.Test;
+import step4.lotto.util.LottoErrorMessage;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,35 +13,40 @@ public class LottoWinningNumberTest {
     void 숫자_체크_테스트() {
         assertThatThrownBy(() ->
                 winningNumbersTest = new LottoWinningNumber("dfdf,dfdf,123,222,222,aaa", 1)
-        ).isInstanceOf(RuntimeException.class);
+        ).isInstanceOf(RuntimeException.class)
+                .withFailMessage(LottoErrorMessage.getLottoNumberformatexception());
     }
 
     @Test
     void empty체크() {
         assertThatThrownBy(() ->
                 winningNumbersTest = new LottoWinningNumber("", 1)
-        ).isInstanceOf(RuntimeException.class);
+        ).isInstanceOf(RuntimeException.class)
+                .withFailMessage(LottoErrorMessage.getLottoAmountCheck());
     }
 
     @Test
     void null체크() {
         assertThatThrownBy(() ->
                 winningNumbersTest = new LottoWinningNumber(null, 1)
-        ).isInstanceOf(RuntimeException.class);
+        ).isInstanceOf(RuntimeException.class)
+                .withFailMessage(LottoErrorMessage.getLottoAmountCheck());
     }
 
     @Test
     void 범위_테스트() {
         assertThatThrownBy(() ->
                 winningNumbersTest = new LottoWinningNumber("10000,-100,3,4,5,6,7", 1)
-        ).isInstanceOf(RuntimeException.class);
+        ).isInstanceOf(RuntimeException.class)
+                .withFailMessage(LottoErrorMessage.getLottoAmountCheck());
     }
 
     @Test
     void 보너스_번호와_로또번호_중복_테스트() {
         assertThatThrownBy(() ->
                 winningNumbersTest = new LottoWinningNumber("1,2,3,4,5,6", 1)
-        ).isInstanceOf(RuntimeException.class);
+        ).isInstanceOf(RuntimeException.class)
+                .withFailMessage(LottoErrorMessage.getLottoOverlapCheck());
     }
 }
 
