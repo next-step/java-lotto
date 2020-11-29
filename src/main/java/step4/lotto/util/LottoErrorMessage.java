995358ed03b509bfeefa17ac0e260f8453f6ca83
rlawls1991 <rlawls1991@gmail.com>
2020-11-30 02:34:30 +0900

    refactor 필요없는 메세지 삭제

--- src/main/java/step4/lotto/util/LottoErrorMessage.java
+++ src/main/java/step4/lotto/util/LottoErrorMessage.java
@@ -7,12 +7,11 @@ public class LottoErrorMessage {
     private static String LOTTO_INCORRECT_COUNT = "로도번호 갯수가 맞지 않습니다.";
     private static String LOTTO_NEGATIVE_EXCEPTION = "음수가 들어올 수 없습니다.";
     private static String LOTTO_AMOUNT_CHECK = "로또구매 금액은 1000원이상 입력해야됩니다.";
-    private static String LOTTO_RANK_EXCEPTION = "로또 랭크는 1등부터 4등만 입력할 수 있습니다.";
+    private static String LOTTO_RANK_EXCEPTION = "맞는 횟수의 갯수는 3개부터 7개까지 가능합니다.";
     private static String LOTTO_OVERLAP_CHECK = "로또 입력 숫자중에 중복값이 존재합니다.";
-    private static String LOTTO_MAKE_COUNT_CHECK = "로또 번호는 6개만 만들 수 있습니다.";
     private static String LOTTO_BONUS_NUMBER_OVERLAP_CHECK = "로또번호와 보너스번호가 중복되었습니다.";
     private static String LOTTO_MANUAL_BUY_COUNT_CHECK = "수동구입한 로또 가격은 입금한 금액보다 클 수 없습니다.";
-    private static String LOTTO_COMAPRE_MANUAL_COUNT_WITH_INPUT_LOTTO_NUMBER = "입력한 로또번호들의 갯수와 구매한 갯수가 다릅니다.";
+    private static String LOTTO_COMPARE_MANUAL_COUNT_WITH_INPUT_LOTTO_NUMBER = "입력한 로또번호들의 갯수와 구매한 갯수가 다릅니다.";
 
     private LottoErrorMessage() {
     }
@@ -53,8 +52,8 @@ public class LottoErrorMessage {
         return LOTTO_RUNTIME_EXCEPTION;
     }
 
-    public static String getLottoComapreManualCountWithInputLottoNumber() {
-        return LOTTO_COMAPRE_MANUAL_COUNT_WITH_INPUT_LOTTO_NUMBER;
+    public static String getLottoCompareManualCountWithInputLottoNumber() {
+        return LOTTO_COMPARE_MANUAL_COUNT_WITH_INPUT_LOTTO_NUMBER;
     }
 
     public static String getLottoManualBuyCountCheck() {
