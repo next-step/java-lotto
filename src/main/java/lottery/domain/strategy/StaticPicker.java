113d75a79e6f2defe2519b8d9868b54575c9d0f6
kriery <jongchan.lee@lmep.kr>
2020-11-30 10:55:50 +0900

    feature: `StaticPicker` 한번의 input으로 다수번의 `pick`이 가능하도록 변경

--- src/main/java/lottery/domain/strategy/StaticPicker.java
+++ src/main/java/lottery/domain/strategy/StaticPicker.java
@@ -2,19 +2,25 @@ package lottery.domain.strategy;
 
 import lottery.domain.LotteryNumber;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StaticPicker implements Picker {
-    private final String[] numbers;
+    private static final String DELIMITER = " *, *";
+
+    private final Iterator<String> iterator;
 
     public StaticPicker(String input) {
-        numbers = input.split(" *, *");
+        this.iterator = Collections.singletonList(input).iterator();
     }
 
+    public StaticPicker(List<String> inputs) {
+        this.iterator = new ArrayList<>(inputs).iterator();
+    }
+    
     @Override
     public List<LotteryNumber> pick(int count) {
+        String[] numbers = iterator.next().split(DELIMITER);
         return Arrays.stream(numbers)
                 .map(LotteryNumber::valueOf)
                 .collect(Collectors.toList());
