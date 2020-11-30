f461795c19022670ac8544fb6910ea838b5d419c
naru <hsik0225@gmail.com>
2020-11-30 20:59:25 +0900

    feat(output-view): 생성된 로또 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,19 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottoes;
+
 public class OutputView {
 
     public static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
 
     public void showPurchaseCount(int count) {
         System.out.println(count + PURCHASE_MESSAGE);
+    }
+
+    public void showPurchasedLottoes(Lottoes lottoes) {
+        for (Lotto lotto : lottoes.getLottoes()) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 }
