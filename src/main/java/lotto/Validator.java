18375d0ba07ab1d4b01bce343ccf511cb3f2b934
naru <hsik0225@gmail.com>
2020-11-30 19:24:22 +0900

    feat(validator): null 값 혹은 빈 문자열 유효성 검사 기능 구현

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,27 @@
+package lotto;
+
+public class Validator {
+
+    public static final String INPUT_NULL_MESSAGE = "null 값을 입력하였습니다!";
+
+    public static final String INPUT_EMPTY_MESSAGE = "빈 값을 입력하셨습니다!";
+
+    public static final String EMPTY = "";
+
+    protected void validate(String input) {
+        checkNull(input);
+        checkBlank(input);
+    }
+
+    protected void checkNull(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(INPUT_NULL_MESSAGE);
+        }
+    }
+
+    protected void checkBlank(String input) {
+        if (input.trim().equals(EMPTY)) {
+            throw new IllegalArgumentException(INPUT_EMPTY_MESSAGE);
+        }
+    }
+}
