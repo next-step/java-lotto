21cec35e9d32164bf7caf82ffea288b1287ce52e
hanogo <trullo393@gmail.com>
2020-12-02 02:22:58 +0900

    [Feat] LottoUtil Util클래스 추가

--- src/main/java/lotto/util/LottoUtil.java
+++ src/main/java/lotto/util/LottoUtil.java
@@ -1,4 +1,31 @@
 package lotto.util;
 
+import lotto.domain.InputMoney;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoUtil {
+    public static List<Lotto> createAutoLottoList(InputMoney money) {
+        List<Lotto> lottoList = new ArrayList<>();
+        LottoGenerator generator = new RandomLottoGenerator();
+        for (int i = 0; i < money.getAvailableLottoSize(); i++) {
+            lottoList.add(new Lotto(generator));
+        }
+        return lottoList;
+    }
+
+    public static Lotto createMenualLotto(List<Integer> numbers) {
+        LottoGenerator generator = new ManualLottoGenerator(numbers);
+        Lotto lotto = new Lotto(generator);
+        return lotto;
+    }
+
+    public static List<Lotto> createMenualLottoList(List<Integer> numbers) {
+        List<Lotto> lottoList = new ArrayList<>();
+        LottoGenerator generator = new ManualLottoGenerator(numbers);
+        lottoList.add(new Lotto(generator));
+        return lottoList;
+    }
 }
--- src/test/java/lotto/util/LottoUtilTest.java
+++ src/test/java/lotto/util/LottoUtilTest.java
@@ -1,5 +1,31 @@
+package lotto.util;
+
+import lotto.domain.InputMoney;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoUtilTest {
 
+    @Test
+    void createAutoLottoList() {
+        Assertions.assertThat(LottoUtil.createAutoLottoList(new InputMoney(3000)).size())
+                .isEqualTo(3);
+    }
+
+    @Test
+    void createMenualLotto() {
+        Assertions.assertThat(LottoUtil.createMenualLotto(Arrays.asList(1, 2, 3, 4, 5, 6)).toString())
+                .isEqualTo("[1, 2, 3, 4, 5, 6]");
+
+    }
+
+    @Test
+    void createMenualLottoList() {
+        Assertions.assertThat(LottoUtil.createMenualLottoList(Arrays.asList(1, 2, 3, 4, 5, 6)).toString())
+                .isEqualTo("[[1, 2, 3, 4, 5, 6]]");
+    }
 }
