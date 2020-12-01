71174ab019a0a94c66857bea5f6f57718ee2bb95
hanogo <trullo393@gmail.com>
2020-12-02 02:23:29 +0900

    [Feat] WinningResult 도메인 추가

--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -1,5 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import lotto.context.Rank;
+import lotto.util.ManualLottoGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 class WinningResultTest {
 
+    @Test
+    void getCountByRank() {
+        List<Lotto> lottoList = Arrays.asList(new Lotto(new ManualLottoGenerator(Arrays.asList(1,2,3,4,5,6))),
+                new Lotto(new ManualLottoGenerator(Arrays.asList(1,12,13,14,15,16))));
+        WinningResult winningResult = new WinningResult("1,2,3,4,5,6");
+        Assertions.assertThat(winningResult.getCountByRank(lottoList, Rank.FIRST))
+                .isEqualTo(1);
+    }
+
+    @Test
+    void validLottoNumberText() {
+        Assertions.assertThatThrownBy(() -> {
+            new WinningResult("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
