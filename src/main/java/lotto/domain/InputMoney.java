02f1fe0eac71c2569c32db3f739048eea3161fc3
hanogo <trullo393@gmail.com>
2020-12-02 02:19:55 +0900

    [Feat] inputMoney 도메인 추가

--- src/main/java/lotto/domain/InputMoney.java
+++ src/main/java/lotto/domain/InputMoney.java
@@ -1,5 +1,26 @@
 package lotto.domain;
 
-public class InputAmount {
-    private int 
+public class InputMoney {
+    private static final int LOTTO_COST = 1000;
+
+    private final int money;
+
+    public InputMoney(int money) {
+        validMoney(money);
+        this.money = money;
+    }
+
+    private void validMoney(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("최소 구입금액은 1000원 이상이어야 합니다.");
+        }
+    }
+
+    public int getAvailableLottoSize() {
+        return money / LOTTO_COST;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
--- src/test/java/lotto/domain/InputMoneyTest.java
+++ src/test/java/lotto/domain/InputMoneyTest.java
@@ -1,5 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class InputMoneyTest {
+    InputMoney inputMoney;
 
+    @BeforeEach
+    void create() {
+        inputMoney = new InputMoney(10000);
+    }
+
+    @Test
+    void validMoney() {
+        Assertions.assertThatThrownBy(() -> {
+            new InputMoney(0);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getAvailableLottoSize() {
+        Assertions.assertThat(inputMoney.getAvailableLottoSize())
+                .isEqualTo(10);
+    }
+
+    @Test
+    void getMoney() {
+        Assertions.assertThat(inputMoney.getMoney())
+                .isEqualTo(10000);
+    }
 }
