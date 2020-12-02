36bdc0d467bfaea4ed2c55714fbf9f052810bf87
최종원 <90lastone@gmail.com>
2020-12-02 21:16:58 +0900

    refactor: Rank winPrice 천원단위 '_' 구분

--- src/main/java/step2/domain/Rank.java
+++ src/main/java/step2/domain/Rank.java
@@ -2,10 +2,10 @@ package step2.domain;
 
 public enum Rank {
     ZERO(0, 0, "꽝"),
-    FOURTH(3, 5000, "3개 일치"),
-    THIRD(4, 50000, "4개 일치"),
-    SECOND(5, 1500000, "5개 일치"),
-    FIRST(6, 2000000000, "6개 일치");
+    FOURTH(3, 5_000, "3개 일치"),
+    THIRD(4, 50_000, "4개 일치"),
+    SECOND(5, 1_500_000, "5개 일치"),
+    FIRST(6, 2_000_000_000, "6개 일치");
 
     private int matchCount;
     private int winPrice;
