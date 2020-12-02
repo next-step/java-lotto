3530ffaf6b88969a72a1c244aecda18a5ef614d4
최종원 <90lastone@gmail.com>
2020-12-02 23:57:16 +0900

    refactor: LottoNumber 숫자 범위 체크, 정적팩토리 메서드 적용

--- src/main/java/step2/domain/LottoNumber.java
+++ src/main/java/step2/domain/LottoNumber.java
@@ -1,10 +1,32 @@
 package step2.domain;
 
-public class LottoNumber {
-    private final int number;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
-    public LottoNumber(int number) {
+public class LottoNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private static Map<Integer, LottoNumber> lottoNumberCache = new HashMap<>();
+
+    static {
+        IntStream.range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+                .forEach(i -> lottoNumberCache.put(i, new LottoNumber(i)));
+    }
+
+    private int number;
+
+    private LottoNumber(int number) {
         this.number = number;
+    }
+
+    public static LottoNumber of(int number) {
+        LottoNumber lottoNumber = lottoNumberCache.get(number);
+        if (lottoNumber == null) {
+            throw new IllegalArgumentException("로또 번호 범위 벗어남");
+        }
+        return lottoNumber;
     }
 
     public int getNumber() {
