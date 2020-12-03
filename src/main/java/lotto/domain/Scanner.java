ef721950b038f11c6a0309265cae4e9ce8a7e0bd
Jaehee Park <jaeheepark0145@gmail.com>
2020-12-03 23:25:12 +0900

    refactor : 당첨번호 validation 추가

--- src/main/java/lotto/domain/Scanner.java
+++ src/main/java/lotto/domain/Scanner.java
@@ -5,15 +5,24 @@ import java.util.stream.Collectors;
 
 public class Scanner {
 
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
     private List<LottoNumber> winningNumbers;
     private LottoNumber bonusNumber;
 
     public Scanner(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers);
         this.winningNumbers = winningNumbers.stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toList());
         ;
         this.bonusNumber = new LottoNumber(bonusNumber);
+    }
+
+    private void validate(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("당첨 번호 6개를 입력해주세요 :)");
+        }
     }
 
     private void scan(LottoTicket lottoTicket, LottoResult lottoResult) {
@@ -41,6 +50,5 @@ public class Scanner {
         return lottoNumbers.stream()
                 .map(n -> n.toInt())
                 .collect(Collectors.toList());
-
     }
 }
