249154c65b00000ce660a5b7e26bb5a72d5ed6bc
Jaehee Park <jaeheepark0145@gmail.com>
2020-12-03 23:15:11 +0900

    refactor : 당첨번호, 보너스볼을 LottoNumber로 수정

--- src/main/java/lotto/domain/Scanner.java
+++ src/main/java/lotto/domain/Scanner.java
@@ -1,22 +1,32 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Scanner {
 
-    private List<Integer> winningNumbers;
-    private int bonusNumber;
+    private List<LottoNumber> winningNumbers;
+    private LottoNumber bonusNumber;
 
     public Scanner(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
+        this.winningNumbers = winningNumbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+        ;
+        this.bonusNumber = new LottoNumber(bonusNumber);
     }
 
     private void scan(LottoTicket lottoTicket, LottoResult lottoResult) {
-        long matchingScore = lottoTicket.getLottoNumbers().stream()
-                        .filter(n -> winningNumbers.contains(n.toInt())).count();
-        final boolean matchBonus = lottoTicket.getLottoNumbers().stream()
-                .anyMatch(n -> n.toInt() == bonusNumber);
+        long matchingScore = lottoTicket.getLottoNumbers()
+                .stream()
+                .filter(n -> toIntegerList(winningNumbers)
+                        .contains(n.toInt()))
+                .count();
+
+        final boolean matchBonus = lottoTicket.getLottoNumbers()
+                .stream()
+                .anyMatch(n -> n.toInt() == bonusNumber.toInt());
+
 
         lottoResult.saveLottoResult(Long.valueOf(matchingScore).intValue(), matchBonus);
     }
@@ -25,5 +35,12 @@ public class Scanner {
         LottoResult lottoResult = new LottoResult(lottoTickets.size());
         lottoTickets.forEach(lottoTicket -> scan(lottoTicket, lottoResult));
         return lottoResult;
+    }
+
+    private List<Integer> toIntegerList(List<LottoNumber> lottoNumbers) {
+        return lottoNumbers.stream()
+                .map(n -> n.toInt())
+                .collect(Collectors.toList());
+
     }
 }
