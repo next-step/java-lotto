231f23464c400d4dde512ae36fdb87ace0306222
Jaehee Park <jaeheepark0145@gmail.com>
2020-12-03 21:17:25 +0900

    chores : 불필요한 코드 삭제

--- src/main/java/lotto/domain/Generator.java
+++ src/main/java/lotto/domain/Generator.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Generator {
 
     private LottoTicket generateLottoTicket() {
-        LottoNumberMaker lottoNumberMaker = new LottoNumberMaker();
         List<Integer> numbers = LottoNumberMaker.makeNewNumbers();
         return new LottoTicket(numbers);
     }
