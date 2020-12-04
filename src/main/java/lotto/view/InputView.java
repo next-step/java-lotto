bbf307af0f1fbefeb859fdec2fd1352f3b2be0ca
hanogo <trullo393@gmail.com>
2020-12-05 00:21:34 +0900

    [Fix] main 수정 및 View수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -14,7 +16,6 @@ public class InputView {
     }
 
     public static String inputWinningNumber() {
-        sc.nextLine();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return sc.nextLine();
     }
@@ -22,5 +23,17 @@ public class InputView {
     public static int inputBonusBall() {
         System.out.println("보너스 볼을 입력해 주세요.");
         return sc.nextInt();
+    }
+
+    public static List<String> inputManualLotto() {
+        List<String> lottoList = new ArrayList<>();
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualNumber = sc.nextInt();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        sc.nextLine();
+        for (int i = 0; i < manualNumber; i++) {
+            lottoList.add(sc.nextLine());
+        }
+        return lottoList;
     }
 }
