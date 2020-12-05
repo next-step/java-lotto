160bcbdc5594288de45ee1061646b1969134a6f3
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-05 20:43:26 +0900

    test : 테스트 케이스 수정

--- src/test/java/stringsumcalculator/SeparatorsTest.java
+++ src/test/java/stringsumcalculator/SeparatorsTest.java
@@ -4,6 +4,9 @@ import stringsumcalculator.model.Separators;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.assertEquals;
 
 public class SeparatorsTest {
@@ -11,21 +14,18 @@ public class SeparatorsTest {
     @Test
     @DisplayName("구분자 생성이 제대로 되는가")
     public void createSeparator(){
-        Separators separators = new Separators("//;\\n1;2;3");
-        assertEquals(separators.getSeparator(0).getString(),",");
-        assertEquals(separators.getSeparator(1).getString(),":");
-        assertEquals(separators.getSeparator(2).getString(),";");
-    }
+        //Separators separators = new Separators("//;\\n1;2;3");
 
-    @Test
-    @DisplayName("커스텀 구분자를 여러 개 추가할 수도 있는가")
-    public void addManyCustomSeparator(){
-        Separators separators = new Separators("//;\\n//[\\n//]\\n1;2;3");
-        assertEquals(separators.getSeparator(0).getString(),",");
-        assertEquals(separators.getSeparator(1).getString(),":");
-        assertEquals(separators.getSeparator(2).getString(),";");
-        assertEquals(separators.getSeparator(3).getString(),"[");
-        assertEquals(separators.getSeparator(4).getString(),"]");
+        String string = "//[\\n1[2[3";
+        Separators separators = new Separators(string);
+
+        assertEquals(separators.getSeparator(0),"[");
+
+        String string2 = "1,2,3";
+        Separators separators2 = new Separators(string2);
+
+        assertEquals(separators2.getSeparator(0),",");
+        assertEquals(separators2.getSeparator(1),":");
     }
 
 }
