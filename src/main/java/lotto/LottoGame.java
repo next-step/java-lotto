793effb2e3c4456d8221e5075ecfe336838bde7a
DongHwan Kim <bkuekany@gmail.com>
2020-12-05 18:32:55 +0900

    refactor : LottoTicket, Lottos 리팩토링 코드 수정에 따른 변경

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -4,22 +4,25 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
 
     public static void main(String[] args) {
 
         Money money = Money.from(InputView.inputPurchasePrice());
 
-        LottoShop lottoShop = new LottoShop();
+        LottoShop lottoShop = new LottoShop(new LottoAutoMachine());
 
-        LottoTicket ticket = lottoShop.purchaseLottoTicket(money);
-        int manualCount = InputView.inputManualLottoCount();
+        LottoTicket ticket = lottoShop.purchaseTicket(money, InputView.inputManualLottoCount());
 
         InputView.inputManualLottoNumbers();
-        for (int i = 0; i < manualCount; i++) {
-            lottoShop.exchangeManualLottos(ticket, InputView.inputLottoNumbers());
+        List<Lotto> manualLotto = new ArrayList<>();
+        for (int i = 0; i < ticket.getManualQuantity(); i++) {
+            manualLotto.add(Lotto.of(InputView.inputLottoNumbers()));
         }
-        Lottos lottos = lottoShop.exchangeAutoLottos(ticket, new LottoAutoMachine());
+        Lottos lottos = lottoShop.purchaseLottos(ticket, manualLotto);
 
         ResultView.outputPurchaseQuantity(ticket);
         ResultView.outputLottos(lottos);
