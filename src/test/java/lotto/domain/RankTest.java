78ce86d1b69f09e326bbc0ee8c4b8422e5fedaae
naru <hsik0225@gmail.com>
2020-12-06 08:25:12 +0900

    feat(rank): 당첨 번호와 일치 개수와 보너스볼에 따른 랭크 결과 반환하는 기능 구현

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -48,4 +48,64 @@ public class RankTest {
         // then
         assertThat(prize).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("6개 일치 시 1등 반환")
+    public void valueOf_AllMatch_ReturnFirst() {
+
+        // given
+        int matchCount = 6;
+        boolean matchBonus = true;
+
+        // when
+        Rank rank = Rank.valueOf(matchCount, matchBonus);
+
+        //then
+        assertThat(rank).isEqualTo(Rank.FIRST);
+    }
+
+    @Test
+    @DisplayName("5개와 보너스볼 일치 시 2등 반환")
+    public void valueOf_FiveMatchAndMatchBonusBall_ReturnFirst() {
+
+        // given
+        int matchCount = 5;
+        boolean matchBonus = true;
+
+        // when
+        Rank rank = Rank.valueOf(matchCount, matchBonus);
+
+        //then
+        assertThat(rank).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    @DisplayName("5개와 보너스볼 불일치 시 3등 반환")
+    public void valueOf_FiveMatchAndNotMatchBonusBall_ReturnFirst() {
+
+        // given
+        int matchCount = 5;
+        boolean matchBonus = false;
+
+        // when
+        Rank rank = Rank.valueOf(matchCount, matchBonus);
+
+        //then
+        assertThat(rank).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    @DisplayName("1개 일치 시 MISS 반환")
+    public void valueOf_OneMatch_ReturnMiss() {
+
+        // given
+        int matchCount = 1;
+        boolean matchBonus = true;
+
+        // when
+        Rank rank = Rank.valueOf(matchCount, matchBonus);
+
+        //then
+        assertThat(rank).isEqualTo(Rank.MISS);
+    }
 }
