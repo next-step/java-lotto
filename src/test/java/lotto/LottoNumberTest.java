ca4b2a2c6704d21d7d8ecbea81c370fe8e9c8c6b
Jaehee Park <jaeheepark0145@gmail.com>
2020-12-05 11:22:59 +0900

    test : LottoNumberTest 수정

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class LottoNumberTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 46})
+    @ValueSource(ints = {0, 1, 45, 46})
     @DisplayName("로또번호가 1 - 45 사이에서 생성되는지 확인")
     public void new_lotto_number(int input) {
         assertThrows(IllegalArgumentException.class, () -> {
