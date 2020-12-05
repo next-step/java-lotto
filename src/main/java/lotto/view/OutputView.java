c787dcfc13940f64381af060571c702c1f8bdc23
naru <hsik0225@gmail.com>
2020-12-06 08:49:32 +0900

    feat(output-view): 보너스 볼 일치 담청 카운트 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -14,6 +14,10 @@ public final class OutputView {
 
     private static final String EARN_RATE_MESSAGE = "총 수익률은 %.2f입니다.(%s)";
 
+    private static final String DEFAULT_STATISTICS_MESSAGE = "%d개 일치 (%d원)- %d개\n";
+
+    private static final String SECOND_STATISTICS_MESSAGE = "%d개 일치, 보너스 볼 일치(%d원)- %d개\n";
+
     public void showPurchaseCount(final int count) {
         System.out.println(count + PURCHASE_MESSAGE);
     }
@@ -26,16 +30,26 @@ public final class OutputView {
 
     public void showStatistics(final WinningCount winningCount, final int paid) {
         System.out.println(STATISTICS_MESSAGE);
-        showMatchingCount(winningCount);
+        showMatchingCounts(winningCount);
         showEarnRate(winningCount, paid);
     }
 
-    private void showMatchingCount(final WinningCount winningCount) {
+    private void showMatchingCounts(final WinningCount winningCount) {
         for (Rank rank : Rank.values()) {
-            System.out.printf("%d개 일치 (%d원)- %d개\n", rank.getMatchCount()
-                    , rank.getPrize()
-                    , winningCount.getMatchCount(rank));
+            showMatchingCount(winningCount, rank);
         }
+    }
+
+    private void showMatchingCount(final WinningCount winningCount, final Rank rank) {
+        String message = DEFAULT_STATISTICS_MESSAGE;
+
+        if (rank.equals(Rank.SECOND)) {
+            message = SECOND_STATISTICS_MESSAGE;
+        }
+
+        System.out.printf(message, rank.getMatchCount()
+                , rank.getPrize()
+                , winningCount.getMatchCount(rank));
     }
 
     private void showEarnRate(final WinningCount winningCount, final int paid) {
