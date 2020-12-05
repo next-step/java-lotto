c8f53962a822a96a87aa9fc942eed31dc297ae1c
poppo <ccf05017@gmail.com>
2020-12-05 16:35:59 +0900

    refactor(All): remove previous codes
    - 새로운 과정 실습을 위해 이전 과제 코드 모두 제거

--- src/main/java/lotto/domain/lotto/exceptions/OutOfBoundOfLottoNumberException.java
+++
@@ -1,7 +0,0 @@
-package lotto.domain.lotto.exceptions;
-
-public class OutOfBoundOfLottoNumberException extends RuntimeException {
-    public OutOfBoundOfLottoNumberException(String message) {
-        super(message);
-    }
-}
