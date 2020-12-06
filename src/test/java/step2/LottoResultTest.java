9a18c269f7cd2225dd26b202d93a106f1bedc031
최종원 <90lastone@gmail.com>
2020-12-06 14:07:45 +0900

    test: 로또 당첨확인, 총수익률 추가

--- src/test/java/step2/LottoResultTest.java
+++ src/test/java/step2/LottoResultTest.java
@@ -3,55 +3,55 @@ package step2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import step2.domain.Lotto;
+import step2.domain.LottoNumber;
 import step2.domain.LottoResult;
-import step2.domain.WinningLotto;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoResultTest {
-    private List<Integer> lottoNumber;
-    private WinningLotto winningLotto;
+    private List<LottoNumber> lottoNumber;
+    private List<LottoNumber> winningLottoNumber;
+    private Lotto lotto;
+    private Lotto winningLotto;
+    private List<Lotto> buyLotto = new ArrayList<>();
 
     @BeforeEach
     void init() {
         lottoNumber = new ArrayList<>();
-        lottoNumber.add(1);
-        lottoNumber.add(2);
-        lottoNumber.add(3);
-        lottoNumber.add(7);
-        lottoNumber.add(8);
-        lottoNumber.add(9);
+        lottoNumber.add(LottoNumber.of(1));
+        lottoNumber.add(LottoNumber.of(2));
+        lottoNumber.add(LottoNumber.of(3));
+        lottoNumber.add(LottoNumber.of(7));
+        lottoNumber.add(LottoNumber.of(8));
+        lottoNumber.add(LottoNumber.of(9));
+        lotto = new Lotto(lottoNumber);
+        buyLotto.add(lotto);
 
-        List<Integer> winNumber = new ArrayList<>();
-        winNumber.add(1);
-        winNumber.add(2);
-        winNumber.add(3);
-        winNumber.add(4);
-        winNumber.add(5);
-        winNumber.add(6);
-        winningLotto = new WinningLotto(winNumber);
+        winningLottoNumber = new ArrayList<>();
+        winningLottoNumber.add(LottoNumber.of(1));
+        winningLottoNumber.add(LottoNumber.of(2));
+        winningLottoNumber.add(LottoNumber.of(3));
+        winningLottoNumber.add(LottoNumber.of(4));
+        winningLottoNumber.add(LottoNumber.of(5));
+        winningLottoNumber.add(LottoNumber.of(6));
+        winningLotto = new Lotto(winningLottoNumber);
     }
 
     @Test
     public void 당첨확인() {
-        Lotto lotto = new Lotto(lottoNumber);
-        List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(lotto);
-        LottoResult lottoResult = new LottoResult(winningLotto, lottoList);
+        LottoResult lottoResult = new LottoResult(winningLotto, buyLotto);
 
         assertThat(lottoResult.getRanks().size()).isEqualTo(1);
     }
 
     @Test
     public void 총수익률() {
-        Lotto lotto = new Lotto(lottoNumber);
-        List<Lotto> lottoList = new ArrayList<>();
-        lottoList.add(lotto);
-        LottoResult lottoResult = new LottoResult(winningLotto, lottoList);
+        LottoResult lottoResult = new LottoResult(winningLotto, buyLotto);
 
-        assertThat(lottoResult.getTotalRevenue(1000)).isEqualTo(5.0);
+        assertThat(lottoResult.getTotalRevenue(14000)).isEqualTo(BigDecimal.valueOf(0.35));
     }
 }
