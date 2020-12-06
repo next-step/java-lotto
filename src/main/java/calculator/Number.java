66104da450b9fcf9bd296cdc74345ba28cb1ae63
yohan <good_1411@naver.com>
2020-12-06 16:14:06 +0900

    feat: 숫자(Number)에 대한 요구사항을 작성합니다.

---
+++ src/main/java/calculator/Number.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Number {
+
+    private static final int MIN_VALUE = 0;
+
+    private static final Map<Integer, Number> CACHE = new HashMap<>();
+
+    private final int value;
+
+    private Number(final int value) {
+        this.value = value;
+    }
+
+    public static Number valueOf(final int value) {
+        if (value < MIN_VALUE) {
+            throw new RuntimeException();
+        }
+        return CACHE.computeIfAbsent(value, Number::new);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Number)) return false;
+        final Number number = (Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/calculator/NumberTest.java
@@ -0,0 +1,55 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("숫자(Number) 테스트")
+class NumberTest {
+
+    @DisplayName("0과 양수의 값을 허용한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10, 100})
+    void create(int value) {
+        // when
+        Number number = Number.valueOf(value);
+
+        // then
+        assertThat(number).isNotNull();
+    }
+
+    @DisplayName("음수로 생성하려는 경우 예외처리(RuntimeException)를 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -10})
+    void createLess(final int value) {
+        // when / then
+        assertThrows(RuntimeException.class, () -> Number.valueOf(value));
+    }
+
+    @DisplayName("같은 숫자일 경우 동등성을 보장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10, 100})
+    void equals(int value) {
+        // when
+        Number number = Number.valueOf(value);
+        Number other = Number.valueOf(value);
+
+        // then
+        assertThat(number).isEqualTo(other);
+    }
+
+    @DisplayName("캐싱을 통해 같은 객체를 보장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10, 100})
+    void cache(int value) {
+        // when
+        Number number = Number.valueOf(value);
+        Number other = Number.valueOf(value);
+
+        // then
+        assertThat(number).isSameAs(other);
+    }
+}
