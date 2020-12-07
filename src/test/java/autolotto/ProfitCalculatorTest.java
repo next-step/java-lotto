482e968dec07504d1981ad7e58021f704002dfff
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-07 18:30:10 +0900

    test : 수익률 계산기 객체 테스트

--- src/test/java/autolotto/ProfitCalculatorTest.java
+++ src/test/java/autolotto/ProfitCalculatorTest.java
@@ -1,2 +1,22 @@
-package autolotto;public class ProfitCalculator {
+package autolotto;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.HashMap;
+
+public class ProfitCalculatorTest {
+
+    @Test
+    @DisplayName("수익률 계산을 제대로 하는가")
+    public void calculateProfit(){
+        HashMap matchingList = new HashMap<Integer,Integer>();
+
+        matchingList.put(3,1);
+        matchingList.put(4,1);
+        matchingList.put(5,1);
+        matchingList.put(6,1);
+
+        assertEquals(PorfitCalculator.calculate(10000, matchingList),200155.5);
+    }
 }
