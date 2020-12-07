c95c427afab228e2d22876075d2985fc39d50147
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-08 06:54:12 +0900

    test : 수익률 계산 로직 변경

--- src/test/java/autolotto/ProfitCalculatorTest.java
+++ src/test/java/autolotto/ProfitCalculatorTest.java
@@ -1,22 +1,33 @@
 package autolotto;
 
+import autolotto.model.*;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ProfitCalculatorTest {
 
     @Test
     @DisplayName("수익률 계산을 제대로 하는가")
     public void calculateProfit(){
-        HashMap matchingList = new HashMap<Integer,Integer>();
+        Lottos lottos = new Lottos(1000);
+        StringBuilder lottoNumbers = new StringBuilder();
+        Lotto lotto = lottos.getLotto(0);
 
-        matchingList.put(3,1);
-        matchingList.put(4,1);
-        matchingList.put(5,1);
-        matchingList.put(6,1);
+        lottoNumbers.append(lotto.getNumbers(0));
 
-        assertEquals(PorfitCalculator.calculate(10000, matchingList),200155.5);
+        for (int i = 1; i < 6; i++) {
+            lottoNumbers.append(", " + lotto.getNumbers(i));
+        }
+
+        WinningNumbers winningNumbers = new WinningNumbers(lottoNumbers.toString());
+
+        LottosComparer lottosComparer = new LottosComparer(lottos, winningNumbers.getNumbers());
+        System.out.println(lottosComparer.getMatchingCount(6));
+        assertEquals(200000.0, ProfitCalculator.calculate(10000, lottosComparer), 0.0);
     }
 }
