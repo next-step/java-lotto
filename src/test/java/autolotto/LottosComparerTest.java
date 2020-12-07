362a398d04726dadf852d833d7cc7b9e018cf227
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-07 16:33:16 +0900

    test : 로또 번호 비교 객체 테스트

---
+++ src/test/java/autolotto/LottosComparerTest.java
@@ -0,0 +1,24 @@
+package autolotto;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class LottosComparerTest {
+
+    @Test
+    @DisplayName("로또번호가 모두 일치됬을 때 제대로 체크하는가")
+    public void compareLottos(){
+        Lotto lotto = new Lotto();
+        StringBuilder lottoNumbers = lotto.getNumber(0);
+
+        for (int i = 1; i < 6; i++) {
+            lottoNumbers.append(", " + lotto.getNumber(i));
+        }
+
+        WinningNumbers winningNumbers = new WinningNumbers(lottoNumbers);
+
+        LottosComparer lottosComparer = new LottosComparer(lotto, winningNumbers);
+        assertEquals(lottosComparer.getMatchingCount("6") == 1);
+
+    }
+}
