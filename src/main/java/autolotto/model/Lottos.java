879f98c00f0cc0e5e125867d076046b5e38c0e4e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-08 06:46:20 +0900

    feat : 로또 일급 컬렉션 클래스 구현

---
+++ src/main/java/autolotto/model/Lottos.java
@@ -0,0 +1,29 @@
+package autolotto.model;
+
+import autolotto.exception.BeShortOfMoneyException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos(int money){
+        if (money < 1000) {
+            throw new BeShortOfMoneyException("1000원보다 작은 돈으로는 복권을 살 수 없습니다.");
+        }
+
+        for (int i = 0; i < (money / 1000); i++) {
+            lottos.add(new Lotto());
+        }
+    }
+
+    public int getLottosCount(){
+        return lottos.size();
+    }
+
+    public Lotto getLotto(int index) {
+        return lottos.get(index);
+    }
+}
