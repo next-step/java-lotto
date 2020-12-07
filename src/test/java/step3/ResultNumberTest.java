80d85c149b000b7825552d4f3468b8e985066af3
sjh <sjh@revhat.com>
2020-12-08 00:08:13 +0900

    fix ResultNumberTest 구현

---
+++ src/test/java/step3/ResultNumberTest.java
@@ -0,0 +1,42 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step3.VO.Lotto;
+import step3.VO.Lottos;
+import step3.VO.Money;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ResultNumberTest {
+    @Test
+    @DisplayName("Result Number Test")
+    void CheckResultNumberTest() {
+        ResultNumber resultNumber = new ResultNumber();
+
+        resultNumber.settingLottoResultNumber("1, 3, 5, 7, 9, 11");
+
+        Lotto lotto = getLotto();
+        assertThat(resultNumber.checkLottoResult(lotto.numberList)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("Result bonus number test")
+    void CheckBonusNumberTest() {
+        ResultNumber resultNumber = new ResultNumber();
+
+        resultNumber.settingLottoBonusNumber("11");
+
+        Lotto lotto = getLotto();
+        assertThat(resultNumber.checkBonusResult(lotto.numberList)).isTrue();
+
+    }
+
+    public Lotto getLotto() {
+        FixedNumberGenerator fixedNumberGenerator = new FixedNumberGenerator();
+        fixedNumberGenerator.setNumber("1,3,5,7,9,11");
+
+        return new Lotto(fixedNumberGenerator);
+    }
+
+}
