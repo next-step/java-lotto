b1172478c6da16a63729758e02675a2ade14b271
Hwang Jaeseon <willing1026@gmail.com>
2020-12-07 22:17:02 +0900

    refactor: delete old sources (#1026)

--- src/main/java/stringcalculator/view/OutputView.java
+++
@@ -1,9 +0,0 @@
-package stringcalculator.view;
-
-import stringcalculator.expression.ExpressionNumber;
-
-public class OutputView {
-    public static void printResult(ExpressionNumber expressionNumber) {
-        System.out.println(expressionNumber.getNumber());
-    }
-}
