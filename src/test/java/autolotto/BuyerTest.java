874c43bfc365e81945b14250c7cd8ae4949bcf13
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-08 06:52:05 +0900

    test : 구매자 테스트 값 변경

--- src/test/java/autolotto/BuyerTest.java
+++ src/test/java/autolotto/BuyerTest.java
@@ -1,7 +1,10 @@
 package autolotto;
 
+import autolotto.model.Buyer;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertEquals;
 
 public class BuyerTest {
 
@@ -10,6 +13,6 @@ public class BuyerTest {
     public void createBuyer(){
         Buyer buyer = new Buyer(10000);
         buyer.buyLottos();
-        assertEqulas(buyer.getLottosCount(),10);
+        assertEquals(buyer.getLottos().getLottosCount(),10);
     }
 }
