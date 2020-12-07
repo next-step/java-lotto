edaf4b3ec8e52a3eb24b165a662a0d5e44604a54
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-08 06:54:45 +0900

    test : 당첨번호 예외처리 추가

--- src/test/java/autolotto/WinningNumbersTest.java
+++ src/test/java/autolotto/WinningNumbersTest.java
@@ -1,16 +1,20 @@
 package autolotto;
 
+import autolotto.exception.IsNotScopeInTheNumber;
+import autolotto.exception.IsNotSixNumberException;
+import autolotto.model.WinningNumbers;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WinningNumbersTest {
 
     @Test
     @DisplayName("당첨번호 6자리가 아니면 예외처리가 되는가")
     public void isNotSixNumber(){
-        assertThrows(isNotSixNumberException.class, () -> {
+        assertThrows(IsNotSixNumberException.class, () -> {
             new WinningNumbers("1, 2, 3, 4, 5, 6, 7");
         });
     }
@@ -18,8 +22,16 @@ public class WinningNumbersTest {
     @Test
     @DisplayName("숫자가 아니면 예외처리가 되는가")
     public void isNotNumber(){
-        assertThrows(isNotNumberException.class, () -> {
-           new WinningNumbers("1, 2, ㄱ, ㄴ, ㄷ, ㄹ, ㅁ");
+        assertThrows(NumberFormatException.class, () -> {
+           new WinningNumbers("1, 2, ㄱ, ㄴ, ㄷ, ㄹ");
+        });
+    }
+
+    @Test
+    @DisplayName("당첨번호가 1에서 45까지의 숫자가 아니면 예외처리 되는가")
+    public void isNotScopeInTheNumber(){
+        assertThrows(IsNotScopeInTheNumber.class, () -> {
+            new WinningNumbers("1, 2, 99, 5, 3, 4");
         });
     }
 }
