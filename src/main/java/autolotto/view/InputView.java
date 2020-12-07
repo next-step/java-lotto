281d9a2e090dfb9fd8f34e144093ffad5eb85550
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-08 06:47:55 +0900

    feat : 입력 클래스 구현

--- src/main/java/autolotto/view/InputView.java
+++ src/main/java/autolotto/view/InputView.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public int inputMoney(){
+    public static int inputMoney(){
         System.out.println("구입 금액을 입력해주세요.");
         return new Scanner(System.in).nextInt();
+    }
+
+    public static String inputWinningNumbers(){
+        System.out.println("");
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return new Scanner(System.in).nextLine();
     }
 }
