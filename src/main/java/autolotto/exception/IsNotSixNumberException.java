ce7ab40b3b930d473825c2672b821746fe106bc5
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-08 06:50:55 +0900

    feat : 당첨번호가 6개가 아닌 입력에 대한 예외처리 구현

---
+++ src/main/java/autolotto/exception/IsNotSixNumberException.java
@@ -0,0 +1,8 @@
+package autolotto.exception;
+
+public class IsNotSixNumberException extends IllegalArgumentException {
+
+    public IsNotSixNumberException(String message){
+        super(message);
+    }
+}
