f591586eccb60505aa466dde9b45e78abbc6644c
cionman <topspin1278@naver.com>
2020-12-08 20:57:57 +0900

    feat: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환하는 기능 구현

--- src/main/java/com/woowahan/calculator/StringAddCalculator.java
+++ src/main/java/com/woowahan/calculator/StringAddCalculator.java
@@ -1,10 +1,33 @@
 package com.woowahan.calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringAddCalculator {
+
+	private static final String DELIMITER_JOINER = "|";
+	private static final String DELIMITER_COMMA = ",";
+	private static final String DELIMITER_COLON = ":";
 
 	public static int splitAndSum(String input) {
 		if (ValidationUtil.isNullOrEmpty(input))
 			return 0;
-		throw new RuntimeException("문자열 계산기 기능 구현");
+		List<String> delimiters = new ArrayList<>();
+		delimiters.add(DELIMITER_COLON);
+		delimiters.add(DELIMITER_COMMA);
+
+		String[] tokens = split(input, delimiters);
+		return sum(tokens);
+	}
+
+	private static String[] split(String input, List<String> delimiters) {
+		return input.split(String.join(DELIMITER_JOINER, delimiters));
+	}
+
+	private static int sum(String[] tokens) {
+		return Arrays.stream(tokens)
+			.mapToInt(Integer::parseInt)
+			.sum();
 	}
 }
--- src/test/java/com/woowahan/calculator/StringAddCalculatorTest.java
+++ src/test/java/com/woowahan/calculator/StringAddCalculatorTest.java
@@ -2,8 +2,13 @@ package com.woowahan.calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class StringAddCalculatorTest {
@@ -15,8 +20,56 @@ class StringAddCalculatorTest {
 
 		int expected = 0;
 
+		assertSplitAndSumMethod(input, expected);
+	}
+
+	@DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
+	@ParameterizedTest
+	@CsvSource({"1,1", "2,2", "3,3"})
+	void singleNumber(String input, int expected) {
+
+		assertSplitAndSumMethod(input, expected);
+
+	}
+
+	@DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환")
+	@ParameterizedTest
+	@MethodSource("argSplitComma")
+	void splitComma(String input, int expected) {
+
+		assertSplitAndSumMethod(input, expected);
+	}
+
+	public static Stream<Arguments> argSplitComma() {
+		return Stream.of(
+			Arguments.of("0,0", 0),
+			Arguments.of("0,5", 5),
+			Arguments.of("3,5", 8),
+			Arguments.of("10,100", 110)
+		);
+	}
+
+	@DisplayName("구분자를 컴마(,)와 콜론(:)을 함께 사용해도 합이 반환됨")
+	@ParameterizedTest
+	@MethodSource("argSplitCommaAndColon")
+	void splitCommaAndColon(String input, int expected) {
+
+		assertSplitAndSumMethod(input, expected);
+
+	}
+
+	public static Stream<Arguments> argSplitCommaAndColon() {
+		return Stream.of(
+			Arguments.of("0,1:2", 3),
+			Arguments.of("1:2,3", 6),
+			Arguments.of("1:2:3,4", 10),
+			Arguments.of("1:2,3,4", 10),
+			Arguments.of("1,2:3:4", 10)
+		);
+	}
+
+	private void assertSplitAndSumMethod(String input, int expected) {
 		int result = StringAddCalculator.splitAndSum(input);
 		assertThat(result).isEqualTo(expected);
 	}
-
 }
