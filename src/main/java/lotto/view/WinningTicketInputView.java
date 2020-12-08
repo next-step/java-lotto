7997d216ac74447476011a0e083d5ba14b7ba9a0
ccf05017 <ccf05017@gmail.com>
2020-12-08 13:54:08 +0900

    feat(WinningTicketInputView): can parse to LottoTicket
    - 사용자가 입력한 당첨 티켓 문자열을 안전하게 도메인 객체로 변환하기 위한 기능 구현

---
+++ src/main/java/lotto/view/WinningTicketInputView.java
@@ -0,0 +1,47 @@
+package lotto.view;
+
+import lotto.domain.LottoTicket;
+import lotto.domain.StaticCreatePolicy;
+import lotto.view.exceptions.InvalidLottoNumbersException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningTicketInputView {
+    private final List<Integer> lottoNumbers;
+
+    public WinningTicketInputView(final String userValue) {
+        validateValue(userValue);
+        List<Integer> lottoNumbers = parseToNumbers(userValue);
+        validateLottoNumbers(lottoNumbers);
+        this.lottoNumbers = parseToNumbers(userValue);
+    }
+
+    public LottoTicket parseToLottoTicket() {
+        StaticCreatePolicy createPolicy = new StaticCreatePolicy(lottoNumbers);
+        return LottoTicket.of(createPolicy);
+    }
+
+    private void validateValue(final String userValue) {
+        if (userValue.isEmpty()) {
+            throw new InvalidLottoNumbersException("빈 문자열은 입력되지 않습니다.");
+        }
+    }
+
+    private void validateLottoNumbers(final List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new InvalidLottoNumbersException("로또 번호는 총 6개를 입력하셔야 합니다.");
+        }
+    }
+
+    private List<Integer> parseToNumbers(final String lottoNumbers) {
+        try {
+            return Arrays.stream(lottoNumbers.split(", "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new InvalidLottoNumbersException("숫자만 입력 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/lotto/view/WinningTicketInputViewTest.java
@@ -0,0 +1,29 @@
+package lotto.view;
+
+import lotto.view.exceptions.InvalidLottoNumbersException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static lotto.domain.LottoTicketFixtures.LOTTO_TICKET_123456;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinningTicketInputViewTest {
+    @DisplayName("로또에 당첨 결과 확인에 사용할 당첨 티켓을 반환할 수 있다.")
+    @Test
+    void createTest() {
+        WinningTicketInputView inputView = new WinningTicketInputView("1, 2, 3, 4, 5, 6");
+
+        assertThat(inputView.parseToLottoTicket()).isEqualTo(LOTTO_TICKET_123456);
+    }
+
+    @DisplayName("잘못된 문자열을 인자로 받은 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "1, 2, 3", "1,2, 3,4, 5,6"})
+    void createFailTest(String invalidValue) {
+        assertThatThrownBy(() -> new WinningTicketInputView(invalidValue))
+                .isInstanceOf(InvalidLottoNumbersException.class);
+    }
+}
