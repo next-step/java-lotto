550af578156c416862e74ffeb2536037d3f03d82
hyeji <khji1210@naver.com>
2020-12-08 19:02:21 +0900

    feat : StringAddCalculatorTest 코드 추가

--- src/main/java/calculator/domain/StringAddCalculator.java
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -1,2 +1,28 @@
-package calculator.domain;public class StringAddCalculator {
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String expression) {
+        if (expression == null || expression.isEmpty()) {
+            return 0;
+        }
+
+        if (expression.contains(":") || expression.contains(",")) {
+            return Arrays.stream(expression.split(",|:"))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(expression);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return Arrays.stream(m.group(2).split(customDelimiter))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        }
+        return Integer.parseInt(expression);
+    }
 }
