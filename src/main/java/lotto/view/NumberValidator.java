2cda6f5dc5377d9c036dd60d589f77b6ad1dee41
naru <hsik0225@gmail.com>
2020-12-08 21:33:33 +0900

    feat(number-validator): 숫자 유효성 검사를 담당하는 NumberValidator 클래스 정의

---
+++ src/main/java/lotto/view/NumberValidator.java
@@ -0,0 +1,28 @@
+package lotto.view;
+
+public class NumberValidator extends Validator {
+
+    static final String NOT_NUMERIC_MESSAGE = "숫자만 입력해주세요!";
+
+    static final String NOT_POSITIVE_NUMBER = "이 숫자는 양수가 아닙니다!";
+
+    @Override
+    protected void validate(String input) {
+        super.validate(input);
+        checkNumeric(input);
+    }
+
+    protected void checkNumeric(final String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMERIC_MESSAGE);
+        }
+    }
+
+    protected void checkPositive(final String input) {
+        if (Integer.parseInt(input) < 0) {
+            throw new IllegalArgumentException(NOT_POSITIVE_NUMBER);
+        }
+    }
+}
