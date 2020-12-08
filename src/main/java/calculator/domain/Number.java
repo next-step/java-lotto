5d8486ac7e3875d89dea4b7109b745e3ca6776aa
hyeji <khji1210@naver.com>
2020-12-08 19:52:17 +0900

    feat(Number) : 숫자가 아니거나 음수일 경우 예외 발생 기능 구현

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -1,2 +1,25 @@
-package calculator.domain;public class Number {
+package calculator.domain;
+
+public class Number {
+    private final int value;
+
+    public Number(String num) {
+        this.value = convertToValue(num);
+    }
+
+    private int convertToValue(String num) {
+        try{
+            int value = Integer.parseInt(num);
+            validate(value);
+            return value;
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자가 아닙니다.");
+        }
+    }
+
+    private void validate(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("음수입니다.");
+        }
+    }
 }
--- src/test/java/calculator/domain/NumberTest.java
+++ src/test/java/calculator/domain/NumberTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package calculator.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class NumberTest {
-  
+
+    @DisplayName("받은 값이 숫자가 아닐 경우, Exception 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "b"})
+    void value_not_number(String num) {
+        assertThatThrownBy(() -> new Number(num))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("숫자가 아닙니다.");
+    }
+
+    @DisplayName("숫자가 음수일 경우, Exception 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-3"})
+    void value_negative(String num) {
+        assertThatThrownBy(() -> new Number(num))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음수입니다.");
+    }
 }
