efa6a0a027de2371ce0f4a64f892f7a26d674dd7
coderhglee <goodna17@gmail.com>
2020-12-09 21:18:12 +0900

    feat: add negative number exception
    -  음수를 전달하는 경우 RuntimeException throw 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -36,8 +36,16 @@ public class StringAddCalculator {
 
 	private static int parseInteger(String text) {
 		try {
-			return Integer.parseInt(text);
+			int parseInt = Integer.parseInt(text);
+			isNegativeNumber(parseInt);
+			return parseInt;
 		} catch (NumberFormatException numberFormatException) {
+			throw new NumberFormatException();
+		}
+	}
+
+	private static void isNegativeNumber(int parseInt) {
+		if (parseInt < 0) {
 			throw new RuntimeException();
 		}
 	}
