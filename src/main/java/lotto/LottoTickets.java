2dd6e390451d80ad413b72adb554eadab1df5b59
yohan <good_1411@naver.com>
2020-12-09 22:41:54 +0900

    feat: 로또 티켓들(LottoTickets)에 대한 로직을 구현합니다.

---
+++ src/main/java/lotto/LottoTickets.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTickets {
+
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(final List<LottoTicket> lottoTickets) {
+        this.lottoTickets = Collections.unmodifiableList(lottoTickets);
+    }
+
+    public static LottoTickets of(final List<LottoTicket> lottoTickets) {
+        return new LottoTickets(lottoTickets);
+    }
+
+    public List<Rank> match(final LottoTicket winning) {
+        return lottoTickets.stream()
+                .map(lottoTicket -> lottoTicket.match(winning))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("로또 티켓들(LottoTickets) 테스트")
+public class LottoTicketsTest {
+
+    private LottoTickets lottoTickets;
+
+    @BeforeEach
+    void setUp() {
+        lottoTickets = LottoTickets.of(Arrays.asList(
+                LottoTicket.of(1, 2, 3, 4, 5, 6),
+                LottoTicket.of(2, 3, 4, 5, 6, 7),
+                LottoTicket.of(3, 4, 5, 6, 7, 8)
+        ));
+    }
+
+    @DisplayName("당첨 로또와 비교해서 등수 배열을 구할 수 있다.")
+    @Test
+    void match() {
+        // given
+        LottoTicket winning = LottoTicket.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        List<Rank> ranks = lottoTickets.match(winning);
+
+        // then
+        assertThat(ranks).containsExactly(Rank.FIRST, Rank.SECOND, Rank.THIRD);
+    }
+}
