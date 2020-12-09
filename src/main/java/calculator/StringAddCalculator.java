835684dbfc3da5f978930fbfd65ffaf44ab1ad60
coderhglee <goodna17@gmail.com>
2020-12-09 21:10:22 +0900

    feat: add return zero
    - 빈 문자열 또는 null 값을 입력할 경우 0을 반환 기능 추가

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class StringAddCalculator {
+	public static int splitAndSum(String text) {
+		if (isNullOrEmpty(text)) {
+			return 0;
+		}
+		return 0;
+	}
+
+	private static boolean isNullOrEmpty(String text) {
+		return text == null || text.isEmpty();
+	}
+}
