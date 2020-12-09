daefcd2ac09e92ede636eb30bb92ffe2358b53be
Jaehee Park <jaeheepark0145@gmail.com>
2020-12-09 21:44:34 +0900

    feat : LottoMachine, ConsoleView 분리

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -8,9 +8,17 @@ public class LottoApplication {
 
     public static void main(String[] args) {
         ConsoleView consoleView = new ConsoleView();
+        LottoMachine lottoMachine = new LottoMachine();
 
-        LottoMachine lottoMachine = new LottoMachine(consoleView);
-        List<LottoTicket> lottoTickets = lottoMachine.buyLotto();
+        int payment = consoleView.getPayment();
+        int manualLottoCount = consoleView.getManualLottoCount();
+        List<List<Integer>> manualLottoNumbers = consoleView.getManualLottoNumbers(manualLottoCount);
+
+        int autoLottoTicketCount = lottoMachine.buyLotto(payment, manualLottoCount);
+        consoleView.printTickets(manualLottoCount, autoLottoTicketCount);
+
+        List<LottoTicket> lottoTickets = lottoMachine.getLotto(autoLottoTicketCount, manualLottoNumbers);
+        consoleView.printLottoTickets(lottoTickets);
 
         LottoResultReader lottoResultReader = new LottoResultReader(consoleView);
         lottoResultReader.readResult(lottoTickets);
--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -8,25 +8,15 @@ import java.util.List;
 
 public class LottoMachine {
 
-    private final ConsoleView consoleView;
-
-    public LottoMachine(ConsoleView consoleView) {
-        this.consoleView = consoleView;
+    public int buyLotto(int payment, int manualLottoCount) {
+        int lottoTicketCount = Calculator.calculateLottoTicketCount(payment);
+        return Calculator.getAutoLottoCount(lottoTicketCount, manualLottoCount);
     }
 
-    public List<LottoTicket> buyLotto() {
-        int lottoTicketCount = Calculator.calculateLottoTicketCount(consoleView.getPayment());
-        int manualLottoCount = consoleView.getManualLottoCount();
-        int autoLottoTicketCount = Calculator.getAutoLottoCount(lottoTicketCount, manualLottoCount);
-
-        List<List<Integer>> manualLottoNumbers = consoleView.getManualLottoNumbers(manualLottoCount);
+    public List<LottoTicket> getLotto(int autoLottoTicketCount, List<List<Integer>> manualLottoNumbers) {
         List<LottoTicket> manualLottoTickets = Generator.generateManualLottoTicket(manualLottoNumbers);
-
-        consoleView.printTickets(manualLottoCount, autoLottoTicketCount);
         List<LottoTicket> autoLottoTickets = Generator.generateLottoTickets(autoLottoTicketCount);
         autoLottoTickets.addAll(manualLottoTickets);
-        consoleView.printLottoTickets(autoLottoTickets);
-
         return autoLottoTickets;
     }
 }
