cda5a09ea1830405446e58cfefb8637fa4a7faff
yohan <good_1411@naver.com>
2020-12-09 22:32:47 +0900

    feat: 로또 티켓(LottoTicket)에 대한 로직을 구현합니다.

---
+++ src/main/java/lotto/LottoTicket.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTicket {
+
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    private LottoTicket(final List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
+    }
+
+    public static LottoTicket of(final int... lottoNumbers) {
+        return Arrays.stream(lottoNumbers)
+                .mapToObj(LottoNumber::valueOf)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), LottoTicket::of));
+    }
+
+    public static LottoTicket of(final List<LottoNumber> lottoNumbers) {
+        HashSet<LottoNumber> removedDuplication = new HashSet<>(lottoNumbers);
+        int size = removedDuplication.size();
+        if (size != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(
+                    String.format("로또 티켓의 로또 번호는 %d개 입니다. (입력 수: %d", LOTTO_NUMBER_COUNT, lottoNumbers.size()));
+        }
+        return new LottoTicket(lottoNumbers);
+    }
+
+    public boolean contains(final int value) {
+        return contains(LottoNumber.valueOf(value));
+    }
+
+    public boolean contains(final LottoNumber lottoNumber) {
+        return lottoNumbers.contains(lottoNumber);
+    }
+
+    public Rank match(final LottoTicket winning) {
+        int matchCount = (int) lottoNumbers.stream()
+                .filter(winning::contains)
+                .count();
+        return Rank.countOf(matchCount);
+    }
+}
