99f6ac3e1f1678f2eed766f279a248fc919f3637
yohan <good_1411@naver.com>
2020-12-09 21:55:05 +0900

    feat: 금액(Money)에 대한 로직을 구현합니다.

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Money {
+
+    private static final BigDecimal MIN_VALUE = BigDecimal.ZERO;
+
+    private final BigDecimal amount;
+
+    private Money(final BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public static Money valueOf(final long value) {
+        return valueOf(BigDecimal.valueOf(value));
+    }
+
+    public static Money valueOf(final BigDecimal value) {
+        if (value.compareTo(MIN_VALUE) < 0) {
+            throw new IllegalArgumentException(String.format("돈은 %s원 미만일 수 없습니다. (입력: %s)", MIN_VALUE, value));
+        }
+        return new Money(value);
+    }
+
+    public Money plus(final Money other) {
+        return valueOf(amount.add(other.amount));
+    }
+
+    public long divide(final Money other) {
+        return amount.divide(other.amount, RoundingMode.DOWN)
+                .longValue();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money)) return false;
+        final Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    public double profitRate(final Money other) {
+        return this.amount.divide(other.amount, 2, RoundingMode.DOWN)
+                .doubleValue();
+    }
+}
---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,96 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("금액(Money) 테스트")
+public class MoneyTest {
+
+    @DisplayName("0 이상의 숫자를 범위로 한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {0, 1000, 10000})
+    void create(long value) {
+        // when
+        Money money = Money.valueOf(value);
+
+        // then
+        assertThat(money).isNotNull();
+    }
+
+    @DisplayName("음수는 허용하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(longs = {-1, -10})
+    void createNotAllowNegative(long value) {
+        // when / then
+        assertThrows(IllegalArgumentException.class, () -> Money.valueOf(value));
+    }
+
+    @DisplayName("금액을 더할 수 있다")
+    @ParameterizedTest
+    @CsvSource({
+            "1000,2000,3000",
+            "400,100,500"
+    })
+    void plus(long first, long second, long expected) {
+        // when
+        Money money = Money.valueOf(first);
+        Money other = Money.valueOf(second);
+        Money plus = money.plus(other);
+
+        // then
+        assertThat(plus).isEqualTo(Money.valueOf(expected));
+    }
+
+    @DisplayName("금액을 나눌 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "1000,2000,0",
+            "400,100,4",
+            "5500,1000,5"
+    })
+    void divide(long first, long second, long expected) {
+        // when
+        Money money = Money.valueOf(first);
+        Money other = Money.valueOf(second);
+        long divide = money.divide(other);
+
+        // then
+        assertThat(divide).isEqualTo(expected);
+    }
+
+    @DisplayName("수익률을 구할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "1000,2000,0.5",
+            "400,100,4",
+            "5500,1000,5.5"
+    })
+    void profitRate(long first, long second, double expected) {
+        // when
+        Money money = Money.valueOf(first);
+        Money other = Money.valueOf(second);
+        double profitRate = money.profitRate(other);
+
+        // then
+        assertThat(profitRate).isEqualTo(expected);
+    }
+
+    @DisplayName("수익률은 소수점 두번째 자리까지만 구한다.")
+    @ParameterizedTest
+    @CsvSource("100,300,0.33")
+    void profitRateSecondScale(long first, long second, double expected) {
+        // when
+        Money money = Money.valueOf(first);
+        Money other = Money.valueOf(second);
+        double profitRate = money.profitRate(other);
+
+        // then
+        assertThat(profitRate).isEqualTo(expected);
+    }
+}
