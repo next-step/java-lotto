8fa074580c48e08ae0b1847186029f3544e6f95d
yohan <good_1411@naver.com>
2020-12-09 22:19:58 +0900

    feat: 로또 번호(LottoNumber)에 대한 로직을 구현합니다.

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class LottoNumber {
+
+    public static final int MIN_VALUE = 1;
+
+    public static final int MAX_VALUE = 45;
+
+    private static final Map<Integer, LottoNumber> CACHE = new HashMap<>();
+
+    private final int value;
+
+    private LottoNumber(final int value) {
+        this.value = value;
+    }
+
+    public static LottoNumber valueOf(final int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException(
+                    String.format("로또번호는 %d와 %d 사이의 범위만 허용합니다. (입력 값: %d)", MIN_VALUE, MAX_VALUE, value));
+        }
+        return CACHE.computeIfAbsent(value, LottoNumber::new);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LottoNumber)) return false;
+        final LottoNumber that = (LottoNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,55 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("로또 번호(LottoNumber) 테스트")
+public class LottoNumberTest {
+
+    @DisplayName("로또 번호는 1 ~ 45 범위의 숫자 하나를 의미한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 20, 45})
+    void create(int value) {
+        // when
+        LottoNumber lottoNumber = LottoNumber.valueOf(value);
+
+        // then
+        assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("로또 번호가 1 ~ 45 이외일 경우 예외 처리를 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 46})
+    void createOutOfRange(int value) {
+        // when / then
+        assertThrows(IllegalArgumentException.class, () -> LottoNumber.valueOf(value));
+    }
+
+    @DisplayName("로또 번호가 같으면 동일성을 보장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 20, 45})
+    void equals(int value) {
+        // when
+        LottoNumber lottoNumber = LottoNumber.valueOf(value);
+        LottoNumber other = LottoNumber.valueOf(value);
+
+        // then
+        assertThat(lottoNumber).isEqualTo(other);
+    }
+
+    @DisplayName("캐싱을 통해 동일한 객체임을 보장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 20, 45})
+    void cache(int value) {
+        // when
+        LottoNumber lottoNumber = LottoNumber.valueOf(value);
+        LottoNumber other = LottoNumber.valueOf(value);
+
+        // then
+        assertThat(lottoNumber).isSameAs(other);
+    }
+}
