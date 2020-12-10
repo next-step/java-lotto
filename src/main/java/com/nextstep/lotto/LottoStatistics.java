0e3f8c8cf2e0f0daa82cf80e995da19ff0507ea3
lazy-moon <magyeon@naver.com>
2020-12-11 02:30:48 +0900

    feature: 수익률 계산 기능

--- src/main/java/com/nextstep/lotto/LottoStatistics.java
+++ src/main/java/com/nextstep/lotto/LottoStatistics.java
@@ -1,9 +1,12 @@
 package com.nextstep.lotto;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LottoStatistics {
+    private int totalCount = 0;
     private Map<LottoWinning, Integer> result = new HashMap<>();
 
     public void addCount(LottoWinning lottoWinning) {
@@ -12,6 +15,7 @@ public class LottoStatistics {
             count = result.get(lottoWinning);
         }
         result.put(lottoWinning, count + 1);
+        totalCount++;
     }
 
     public int getCount(LottoWinning lottoWinning) {
@@ -19,5 +23,20 @@ public class LottoStatistics {
             return result.get(lottoWinning);
         }
         return 0;
+    }
+
+    public double profitRate() {
+        int totalPrice = totalCount * LottoSeller.LOTTO_PRICE;
+        double totalProfit = getTotalProfit();
+        double profitRate = totalProfit / totalPrice;
+        return new BigDecimal(profitRate).setScale(2, RoundingMode.DOWN).doubleValue();
+    }
+
+    private double getTotalProfit() {
+        return result.entrySet().stream().mapToDouble(entry -> {
+            LottoWinning lottoWinning = entry.getKey();
+            int count = entry.getValue();
+            return (double)lottoWinning.getWinningPrice() * count;
+        }).sum();
     }
 }
--- src/test/java/com/nextstep/lotto/LottoStatisticsTest.java
+++ src/test/java/com/nextstep/lotto/LottoStatisticsTest.java
@@ -1,10 +1,13 @@
 package com.nextstep.lotto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,6 +32,29 @@ public class LottoStatisticsTest {
                 Arguments.of(LottoWinning.WIN_3RD, 2),
                 Arguments.of(LottoWinning.WIN_4TH, 2),
                 Arguments.of(LottoWinning.WIN_5TH, 2)
+        );
+    }
+
+    @DisplayName("수익률 계산")
+    @ParameterizedTest
+    @MethodSource
+    void profitRate(LottoWinning lottoWinning) {
+        LottoStatistics lottoStatistics = new LottoStatistics();
+        for ( int ix = 0 ; ix < 5 ; ix ++ ) {
+            lottoStatistics.addCount(lottoWinning);
+        }
+        double rate = (double)lottoWinning.getWinningPrice() * 5 / 5000;
+        rate = new BigDecimal(rate).setScale(2, RoundingMode.DOWN).doubleValue();
+        System.out.println(rate);
+        assertThat(lottoStatistics.profitRate()).isEqualTo(rate);
+    }
+
+    private static Stream<Arguments> profitRate() {
+        return Stream.of(
+                Arguments.of(LottoWinning.WIN_5TH),
+                Arguments.of(LottoWinning.WIN_4TH),
+                Arguments.of(LottoWinning.WIN_3RD),
+                Arguments.of(LottoWinning.WIN_1ST)
         );
     }
 }
