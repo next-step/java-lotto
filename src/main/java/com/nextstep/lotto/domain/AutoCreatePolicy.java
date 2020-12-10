fa6f3a7717c6a9971d15e714b11e8885cff49f6e
ccf05017 <ccf05017@gmail.com>
2020-12-11 07:46:32 +0900

    refactor(AutoCreatePolicy): use constant
    - 코드 가독성을 높이기 위해 매직넘버를 상수로 추출

--- src/main/java/com/nextstep/lotto/domain/AutoCreatePolicy.java
+++ src/main/java/com/nextstep/lotto/domain/AutoCreatePolicy.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AutoCreatePolicy implements LottoTicketCreatePolicy {
+    private static final int LOTTO_NUMBERS_CUTTER_START = 0;
+    private static final int LOTTO_NUMBERS_CUTTER_END = 6;
+
     private static final List<LottoNumber> lottoNumbers =
             IntStream.rangeClosed(LottoNumber.MIN_SIZE, LottoNumber.MAX_SIZE)
                     .mapToObj(LottoNumber::new)
@@ -15,7 +18,8 @@ public class AutoCreatePolicy implements LottoTicketCreatePolicy {
     public LottoTicket create() {
         Collections.shuffle(lottoNumbers);
 
-        List<LottoNumber> newTicketNumbers = AutoCreatePolicy.lottoNumbers.subList(0, 6);
+        List<LottoNumber> newTicketNumbers = AutoCreatePolicy.lottoNumbers
+                .subList(LOTTO_NUMBERS_CUTTER_START, LOTTO_NUMBERS_CUTTER_END);
 
         return new LottoTicket(newTicketNumbers);
     }
