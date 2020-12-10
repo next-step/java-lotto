d8df812b04b0f39c5c6622f34014ff3aa3c52956
lazy-moon <magyeon@naver.com>
2020-12-11 01:00:43 +0900

    refactor: LottoChecker 제거, 기능을 LottoWinning 으로 이관

--- src/main/java/com/nextstep/lotto/LottoWinning.java
+++ src/main/java/com/nextstep/lotto/LottoWinning.java
@@ -1,13 +1,14 @@
 package com.nextstep.lotto;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public enum LottoWinning {
     RETIRE(0, 0),
     WIN_5TH(3, 5_000),
     WIN_4TH(4, 50_000),
-    WIN_3TH(5, 1_500_000),
-    WIN_1TH(6, 2_000_000_000),
+    WIN_3RD(5, 1_500_000),
+    WIN_1ST(6, 2_000_000_000),
     ;
 
     private int matchedCount;
@@ -26,7 +27,18 @@ public enum LottoWinning {
         return winningPrice;
     }
 
-    public static LottoWinning select(int matchedCount) {
+    public static LottoWinning select(List<Integer> winningNumbers, List<Integer> collectNumbers) {
+        long matchedCount = getMatchedCount(winningNumbers, collectNumbers);
+        return select(matchedCount);
+    }
+
+    private static long getMatchedCount(List<Integer> winningNumbers, List<Integer> collectNumbers) {
+        return collectNumbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+    }
+
+    private static LottoWinning select(long matchedCount) {
         return Stream.of(LottoWinning.values())
                 .filter(lottoWinning -> lottoWinning.matchedCount == matchedCount)
                 .findFirst()
--- src/test/java/com/nextstep/lotto/LottoWinningTest.java
+++ src/test/java/com/nextstep/lotto/LottoWinningTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,19 +17,21 @@ public class LottoWinningTest {
     @DisplayName("맞은 개수에 맞는 등수 나오는지 확인")
     @ParameterizedTest
     @MethodSource
-    void winning(int matchedCount, LottoWinning expected) {
-        LottoWinning winning = LottoWinning.select(matchedCount);
+    void winning(List<Integer> collectNumbers, LottoWinning expected) {
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        LottoWinning winning = LottoWinning.select(winningNumbers, collectNumbers);
         assertThat(winning).isEqualTo(expected);
     }
 
     private static Stream<Arguments> winning() {
         return Stream.of(
-                Arguments.of(0, LottoWinning.RETIRE),
-                Arguments.of(2, LottoWinning.RETIRE),
-                Arguments.of(3, LottoWinning.WIN_5TH),
-                Arguments.of(4, LottoWinning.WIN_4TH),
-                Arguments.of(5, LottoWinning.WIN_3TH),
-                Arguments.of(6, LottoWinning.WIN_1TH)
+                Arguments.of(Arrays.asList(1,2,3,4,5,6), LottoWinning.WIN_1ST),
+                Arguments.of(Arrays.asList(11,2,3,4,5,6), LottoWinning.WIN_3RD),
+                Arguments.of(Arrays.asList(11,12,3,4,5,6), LottoWinning.WIN_4TH),
+                Arguments.of(Arrays.asList(11,12,13,4,5,6), LottoWinning.WIN_5TH),
+                Arguments.of(Arrays.asList(11,12,13,14,5,6), LottoWinning.RETIRE),
+                Arguments.of(Arrays.asList(11,12,13,14,15,6), LottoWinning.RETIRE),
+                Arguments.of(Arrays.asList(11,12,13,14,15,16), LottoWinning.RETIRE)
         );
     }
 }
