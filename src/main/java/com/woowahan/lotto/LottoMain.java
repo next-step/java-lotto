3b90ece97e57eea63ff91ccc49e1ba4ac55d2adb
cionman <topspin1278@naver.com>
2020-12-10 11:13:42 +0900

    refactor : PurchaseInputView와 WinNumberInputView 통합

--- src/main/java/com/woowahan/lotto/LottoMain.java
+++ src/main/java/com/woowahan/lotto/LottoMain.java
@@ -4,18 +4,17 @@ import com.woowahan.lotto.model.LottoResult;
 import com.woowahan.lotto.model.Lottos;
 import com.woowahan.lotto.model.PurchaseInput;
 import com.woowahan.lotto.model.WinNumberInput;
+import com.woowahan.lotto.view.InputView;
 import com.woowahan.lotto.view.LottoReport;
-import com.woowahan.lotto.view.PurchaseInputView;
 import com.woowahan.lotto.view.ResultView;
-import com.woowahan.lotto.view.WinNumberInputView;
 
 public class LottoMain {
 
 	public static void main(String[] args) {
-		PurchaseInput purchaseInput = PurchaseInput.of(PurchaseInputView.getPurchaseAmount());
+		PurchaseInput purchaseInput = PurchaseInput.of(InputView.getPurchaseAmount());
 		Lottos lottos = Lottos.purchase(purchaseInput);
 		ResultView.printResult(LottoReport.reportLottos(lottos));
-		WinNumberInput winNumberInput = WinNumberInput.of(WinNumberInputView.getWinNumber());
+		WinNumberInput winNumberInput = WinNumberInput.of(InputView.getWinNumber());
 		LottoResult result = LottoResult.analyze(winNumberInput, lottos.getLottos());
 		ResultView.printResult(LottoReport.reportLottoResult(result));
 	}
--- src/main/java/com/woowahan/lotto/view/InputView.java
+++ src/main/java/com/woowahan/lotto/view/InputView.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 import com.woowahan.lotto.constant.Message;
 
-public class PurchaseInputView {
+public class InputView {
 
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static String getPurchaseAmount() {
 		System.out.println(Message.MSG_REQUEST_PURCHASE_AMOUNT);
+		return scanner.nextLine();
+	}
+
+	public static String getWinNumber() {
+		System.out.println(Message.MSG_REQUEST_LOTTO_WIN_NUMBER);
 		return scanner.nextLine();
 	}
 
