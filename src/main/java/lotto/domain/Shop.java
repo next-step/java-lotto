b99f00130581521c49775d0509aec03ec8b13d5b
louisJu <rokmctkd6@gmail.com>
2020-12-10 13:35:02 +0900

    fix: 로또 구매 로직 수정 및 test 추가

--- src/main/java/lotto/domain/Shop.java
+++ src/main/java/lotto/domain/Shop.java
@@ -1,16 +1,15 @@
 package lotto.domain;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Shop {
-    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_PRICE = 1_000;
 
     private Shop() {}
 
     public static Lottos buyLottos(int money) {
         int lottoCount = money / LOTTO_PRICE;
-        return (Lottos) Stream.generate(() -> new Lotto()).limit(lottoCount).collect(Collectors.toList());
+        return new Lottos(Stream.generate(() -> new Lotto(Numbers.getNumbers())).limit(lottoCount).collect(Collectors.toList()));
     }
 }
