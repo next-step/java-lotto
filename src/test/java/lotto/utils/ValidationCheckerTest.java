ee6237490c41108dd191dd517f8b04f21cfb0132
louisJu <rokmctkd6@gmail.com>
2020-12-10 17:15:57 +0900

    refactor: public -> private 변경

--- src/test/java/lotto/utils/ValidationCheckerTest.java
+++ src/test/java/lotto/utils/ValidationCheckerTest.java
@@ -12,7 +12,6 @@ public class ValidationCheckerTest {
     @Test
     @DisplayName("널 체크트 테스트")
     void nullTest() {
-
         assertThatThrownBy(() -> ValidationChecker.isEmptyOrNull(null))
                 .isInstanceOf(RuntimeException.class);
     }
@@ -22,30 +21,26 @@ public class ValidationCheckerTest {
     void emptyTest() {
         assertThatThrownBy(() -> ValidationChecker.isEmptyOrNull(""))
                 .isInstanceOf(RuntimeException.class);
-
     }
 
     @Test
     @DisplayName("음수 입력 테스트")
     void minusTest() {
-        assertThatThrownBy(() -> ValidationChecker.checkNegative("-1"))
+        assertThatThrownBy(() -> ValidationChecker.exceptionCheck("-1"))
                 .isInstanceOf(RuntimeException.class);
-
     }
 
     @Test
     @DisplayName("문자 입력 테스트")
     void charTest() {
-        assertThatThrownBy(() -> ValidationChecker.checkChar("e"))
+        assertThatThrownBy(() -> ValidationChecker.exceptionCheck("e"))
                 .isInstanceOf(RuntimeException.class);
-
     }
     @Test
     @DisplayName("범위 초과 테스트 1 미만")
     void underOne() {
         assertThatThrownBy(() -> ValidationChecker.checkValidNumber(Arrays.asList(0,2,3,4,5,6)))
                 .isInstanceOf(RuntimeException.class);
-
     }
 
     @Test
@@ -53,7 +48,6 @@ public class ValidationCheckerTest {
     void overFourtyFive() {
         assertThatThrownBy(() -> ValidationChecker.checkValidNumber(Arrays.asList(0,2,3,48,5,6)))
                 .isInstanceOf(RuntimeException.class);
-
     }
 
 }
