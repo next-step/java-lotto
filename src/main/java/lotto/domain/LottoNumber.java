c54de03b6ee6894ad306955aa06ab4b4cd57d8d2
jaeseon_hwang <jshwang@deleo.co.kr>
2020-12-10 21:24:18 +0900

    feat: lottoNumber객체 (1-45 사이 숫자 보장)

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static Integer MIN_NUMBER = 1;
+    private static Integer MAX_NUMBER = 45;
+
+    private int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number >= MIN_NUMBER && number <= MAX_NUMBER) {
+            return;
+        }
+
+        throw new IllegalArgumentException("1 - 45 사이 숫자를 입력해주세요");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LottoNumberTest {
+
+    @DisplayName("정상생성 : 1-45 사이 숫자")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
+    void create(int number) {
+        //when
+        LottoNumber lottoNumber = new LottoNumber(number);
+
+        //then
+        assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("생성 오류 : 범위를 벗어난 값")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void create_exception(int number) {
+        //when, then
+        assertThatIllegalArgumentException()
+              .isThrownBy(() -> new LottoNumber(number))
+              .withMessage("1 - 45 사이 숫자를 입력해주세요");
+    }
+}
