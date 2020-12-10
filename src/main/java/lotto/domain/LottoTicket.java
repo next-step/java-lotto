a184ff4cd4a9d69cae74c79ea3f826728b9f8192
jaeseon_hwang <jshwang@deleo.co.kr>
2020-12-10 21:26:28 +0900

    feat: lottoTicket객체 ( lottoNumber 6개 보유, 일치개수 확인 )

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LottoTicket {
+    private final Integer LOTTO_NUMBER_COUNT = 6;
+    private final Set<LottoNumber> lottoNumbers;
+
+    public LottoTicket(Set<LottoNumber> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validate(Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers == null || lottoNumbers.isEmpty()) {
+            throwException();
+        }
+
+        if (LOTTO_NUMBER_COUNT != lottoNumbers.size()) {
+            throwException();
+        }
+    }
+
+    private void throwException() {
+        throw new IllegalArgumentException("6개의 로또 번호를 입력해주세요");
+    }
+
+    public int matchCount(LottoTicket lastWinningTicket) {
+        HashSet<LottoNumber> compareLottoTicket = new HashSet<>(this.lottoNumbers);
+        compareLottoTicket.retainAll(lastWinningTicket.lottoNumbers);
+
+        return compareLottoTicket.size();
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -0,0 +1,68 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class LottoTicketTest {
+
+    @DisplayName("로또티켓 발행 - 6개의 로또 번호 입력")
+    @Test
+    void create() {
+        //given
+        Set<LottoNumber> lottoNumbers = IntStream.rangeClosed(1, 6)
+              .mapToObj(LottoNumber::new)
+              .collect(Collectors.toSet());
+
+        //when
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+
+        //then
+        assertThat(lottoTicket).isNotNull();
+    }
+
+    @DisplayName("로또티켓 발행 - 6개의 로또 번호가 아닌 경우")
+    @Test
+    void create_exception() {
+        //given
+        Set<LottoNumber> lottoNumbers = IntStream.rangeClosed(1, 5)
+              .mapToObj(LottoNumber::new)
+              .collect(Collectors.toSet());
+
+        //when
+        assertThatIllegalArgumentException()
+              .isThrownBy(() -> new LottoTicket(lottoNumbers))
+              .withMessage("6개의 로또 번호를 입력해주세요");
+    }
+
+    @DisplayName("일치개수 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"6,11,1", "7,12,0"})
+    void matchCount(int start, int end, int expected) {
+        //given
+        Set<LottoNumber> userLottoNumber = IntStream.rangeClosed(1, 6)
+              .mapToObj(LottoNumber::new)
+              .collect(Collectors.toSet());
+
+        Set<LottoNumber> lastWinningLottoNumber = IntStream.rangeClosed(start, end)
+              .mapToObj(LottoNumber::new)
+              .collect(Collectors.toSet());
+
+        LottoTicket userTicket = new LottoTicket(userLottoNumber);
+        LottoTicket lastWinningTicket = new LottoTicket(lastWinningLottoNumber);
+
+        //when
+        int matchCount = userTicket.matchCount(lastWinningTicket);
+
+        //then
+        assertEquals(matchCount, expected);
+    }
+}
