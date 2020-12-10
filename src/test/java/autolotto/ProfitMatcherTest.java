e4a491c51a6859b2cbb4b308d287dbbcca7a06bb
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-12-10 17:18:16 +0900

    test : ProfitMatcher 객체 테스트 구현

---
+++ src/test/java/autolotto/ProfitMatcherTest.java
@@ -0,0 +1,19 @@
+package autolotto;
+
+import autolotto.model.ProfitMatcher;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ProfitMatcherTest {
+
+    @Test
+    @DisplayName("당첨번호 갯수별 수익 계산을 제대로 하는가")
+    public void matchProfit(){
+        assertTrue(ProfitMatcher.getMatchProfit(3,2) == 10000.0f);
+        assertTrue(ProfitMatcher.getMatchProfit(4,2) == 100000.0f);
+        assertTrue(ProfitMatcher.getMatchProfit(5,2) == 3000000.0f);
+        assertTrue(ProfitMatcher.getMatchProfit(6,2) == 4000000000.0f);
+    }
+}
