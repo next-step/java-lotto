a80c9db449863793c2d79ca858af0017f9957199
louisJu <rokmctkd6@gmail.com>
2020-12-10 17:07:05 +0900

    refactor: 예외처리를 위한 형변환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
 
-    public static long inputMoney() {
+    public static String inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Long.parseLong(sc.nextLine());
+        return sc.nextLine();
     }
     public static String inputWinningLotto() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
