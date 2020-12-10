5282cfb4a1b13b408fafb2fc7f48671ec5e1e7e4
louisJu <rokmctkd6@gmail.com>
2020-12-10 17:08:37 +0900

    feat: 유효성검증 로직 추가

---
+++ src/main/java/lotto/utils/ValidationChecker.java
@@ -0,0 +1,46 @@
+package lotto.utils;
+
+import java.util.List;
+
+public class ValidationChecker {
+    private static final int ZERO = 0;
+    private static final int START_NUM = 1;
+    private static final int END_NUM = 45;
+
+    private ValidationChecker() {
+    }
+
+    public static void isEmptyOrNull(String inputValue) {
+         if(inputValue == null || inputValue.isEmpty()) {
+             throw new RuntimeException("값을 입력해 주세요!");
+         }
+    }
+
+    public static boolean exceptionCheck(String inputValue) {
+        checkChar(inputValue);
+        checkNegative(inputValue);
+        return true;
+    }
+
+    public static void checkNegative(String negative) {
+        if(Integer.parseInt(negative) < ZERO) {
+            throw new RuntimeException("음수가 입력되었습니다.");
+        }
+    }
+
+    public static void checkChar(String character) {
+        try {
+            Integer.parseInt(character);
+        } catch (NumberFormatException nfe) {
+            throw new RuntimeException("문자가 입력되었습니다.");
+        }
+    }
+
+    public static void checkValidNumber(List<Integer> nums) {
+        nums.stream().forEach(num -> {
+            if (num < START_NUM || num > END_NUM) {
+                throw new RuntimeException("로또 범위를 초과한 수가 입력되었습니다.");
+            }
+        });
+    }
+}
---
+++ src/test/java/lotto/utils/ValidationCheckerTest.java
@@ -0,0 +1,59 @@
+package lotto.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidationCheckerTest {
+
+    @Test
+    @DisplayName("널 체크트 테스트")
+    void nullTest() {
+
+        assertThatThrownBy(() -> ValidationChecker.isEmptyOrNull(null))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("공백 체크 테스")
+    void emptyTest() {
+        assertThatThrownBy(() -> ValidationChecker.isEmptyOrNull(""))
+                .isInstanceOf(RuntimeException.class);
+
+    }
+
+    @Test
+    @DisplayName("음수 입력 테스트")
+    void minusTest() {
+        assertThatThrownBy(() -> ValidationChecker.checkNegative("-1"))
+                .isInstanceOf(RuntimeException.class);
+
+    }
+
+    @Test
+    @DisplayName("문자 입력 테스트")
+    void charTest() {
+        assertThatThrownBy(() -> ValidationChecker.checkChar("e"))
+                .isInstanceOf(RuntimeException.class);
+
+    }
+    @Test
+    @DisplayName("범위 초과 테스트 1 미만")
+    void underOne() {
+        assertThatThrownBy(() -> ValidationChecker.checkValidNumber(Arrays.asList(0,2,3,4,5,6)))
+                .isInstanceOf(RuntimeException.class);
+
+    }
+
+    @Test
+    @DisplayName("범위 초과 테스트 45 초과")
+    void overFourtyFive() {
+        assertThatThrownBy(() -> ValidationChecker.checkValidNumber(Arrays.asList(0,2,3,48,5,6)))
+                .isInstanceOf(RuntimeException.class);
+
+    }
+
+}
