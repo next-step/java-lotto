76566167f3d15c714bf43051ce64f3fbe52eb31d
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-12 07:07:44 +0900

    문자열 덧셈 계산기 구현

---
+++ src/main/java/lotto/StringAddCalculator.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+	private static String DEFAULT_DELIMITER = ",|:";
+
+	public static int splitAndSum(String inputText) {
+		inputText = Optional.ofNullable(inputText).map(String::valueOf).orElse("");
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputText);
+		if (m.find()) {
+			return sum(m.group(2).split(m.group(1)));
+		}
+		return sum(inputText.split(DEFAULT_DELIMITER));
+	}
+
+	private static Integer sum(String[] tokens) {
+		Integer result = 0;
+		for (String input : tokens) {
+			result += validateNumber(convertInteger(input));
+		}
+		return result;
+
+	}
+
+	private static Integer convertInteger(String input) {
+		if (input.isEmpty()) {
+			return 0;
+		}
+		return Integer.parseInt(input);
+	}
+
+	private static Integer validateNumber(Integer value) {
+		if (value < 0) {
+			throw new RuntimeException();
+		}
+		return value;
+	}
+
+}
