43748f6e7afa5578cbb9ec5e61376c91d978590d
yeGenieee <cyj95428@gmail.com>
2020-12-11 22:41:22 +0900

    test: 입력받은 구입금액 만큼 로또를 구입하는지 확인하는 테스트 코드 추가

--- src/test/java/step2/LottoGameTest.java
+++ src/test/java/step2/LottoGameTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class LottoGameTest {
 
@@ -19,12 +20,19 @@ class LottoGameTest {
         lottoGame = new LottoGame();
     }
 
-
     @Test
     @DisplayName("입력받은 구입 금액이 1000원 미만인 경우 예외 발생 테스트")
     void input_money_less_than_1000() {
         assertThatThrownBy(() -> lottoGame.buy(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Constants.MONEY_IS_LESS_THAN_1000);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력받은 구입 금액 만큼 로또를 구입하는지 테스트")
+    @ValueSource(ints = {14000})
+    void create_lottery_by_input_money(int money) {
+        List<Lottery> lotteryList = lottoGame.makeLotteryList();
+        assertThat(lotteryList.size()).isEqualTo(14);
     }
 }
