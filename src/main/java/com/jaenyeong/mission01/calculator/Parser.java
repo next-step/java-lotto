a7a25cdd3be66058bf56526bb862043598ae2b05
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-11 18:36:51 +0900

    refactor(Parser): extract parse function to parseInt method

--- src/main/java/com/jaenyeong/mission01/calculator/Parser.java
+++ src/main/java/com/jaenyeong/mission01/calculator/Parser.java
@@ -1,11 +1,14 @@
 package com.jaenyeong.mission01.calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parser {
     private static final int NONE = 0;
     private static final String ONLY_NATURAL_NUMBER_EXP = "^[0-9]$";
+    private static final String NUMBERS_SEPARATORS = ",|:";
     private static final String TEXT_ERR_INVALID_STRING = "[error] : An invalid string was entered.";
 
     public List<Integer> parseToNumbers(String exp) {
@@ -34,11 +37,15 @@ public class Parser {
 
     protected List<Integer> bindingWhenNumberIsNaturalNumber(final String exp, final List<Integer> numbers) {
         if (isNaturalNumber(exp)) {
-            numbers.add(Integer.parseInt(exp));
+            numbers.add(parseInt(exp));
             return numbers;
         }
 
         throw new RuntimeException(TEXT_ERR_INVALID_STRING);
+    }
+
+    private int parseInt(String exp) {
+        return Integer.parseInt(exp);
     }
 
     protected boolean isNaturalNumber(final String exp) {
