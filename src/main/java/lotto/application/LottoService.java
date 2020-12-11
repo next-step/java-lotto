affc870b80d59e9d7d0004369c57fcd0f3217d74
yohan <good_1411@naver.com>
2020-12-12 02:57:07 +0900

    refactor: 애플리케이션과 컨트롤러, 서비스 로직을 추가 구현합니다.

--- src/main/java/lotto/application/LottoService.java
+++ src/main/java/lotto/application/LottoService.java
@@ -2,9 +2,10 @@ package lotto.application;
 
 import lotto.domain.LottoMachine;
 import lotto.domain.LottoReport;
-import lotto.domain.LottoTicket;
 import lotto.domain.LottoTickets;
+import lotto.domain.WinningLotto;
 import lotto.dto.PurchaseResponse;
+import lotto.dto.ReportRequest;
 import lotto.dto.ReportResponse;
 
 public class LottoService {
@@ -20,9 +21,12 @@ public class LottoService {
         return PurchaseResponse.of(lottoTickets);
     }
 
-    public ReportResponse report(LottoTickets lottoTickets, String winningNumbers) {
-        LottoTicket winning = lottoMachine.manual(winningNumbers);
-        LottoReport report = LottoReport.of(lottoTickets.match(winning));
+    public ReportResponse report(final ReportRequest reportRequest) {
+        WinningLotto winningLotto = lottoMachine.winning(
+                reportRequest.getWinningNumbers(),
+                reportRequest.getBonusNumber());
+        LottoTickets lottoTickets = reportRequest.getLottoTickets();
+        LottoReport report = LottoReport.of(lottoTickets.match(winningLotto));
         return ReportResponse.of(report);
     }
 }
