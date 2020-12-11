c99588db94022b6e6b956adece7b02738e1fb0e2
louisJu <rokmctkd6@gmail.com>
2020-12-11 13:11:12 +0900

    test: 피드백 반영 * 결과 계산 로직 test 수정

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -8,48 +8,47 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class LottoAdminTest {
-    private LottoAdmin lottoAdmin = new LottoAdmin(Arrays.asList(1,2,3,4,5,6));
+public class WinningLottoTest {
+    private Lotto winningLotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
 
     @Test
     @DisplayName("1등 당첨 test")
     void firstWinningTest() {
-        assertThat(lottoAdmin.calculateRank(new Lotto(Arrays.asList(6,5,4,3,2,1))))
+        assertThat(winningLotto.calculateRank(new Lotto(Arrays.asList(6,5,4,3,2,1))))
                 .isEqualTo(Rank.FIRST);
     }
 
     @Test
     @DisplayName("3등 당첨 test")
     void thirdWinningTest() {
-        assertThat(lottoAdmin.calculateRank(new Lotto(Arrays.asList(11,5,9,3,8,1))))
+        assertThat(winningLotto.calculateRank(new Lotto(Arrays.asList(11,5,9,3,8,1))))
                 .isEqualTo(Rank.FORTH);
     }
-
     @Test
     @DisplayName("미당첨 test")
     void notWinningTest() {
-        assertThat(lottoAdmin.calculateRank(new Lotto(Arrays.asList(11,43,9,26,8,15))))
+        assertThat(winningLotto.calculateRank(new Lotto(Arrays.asList(11,43,9,26,8,15))))
                 .isEqualTo(Rank.MISS);
     }
 
     @Test
     @DisplayName("입력값 split test")
     void splitLottoStringTest() {
-        LottoAdmin splitLottoAdmin = new LottoAdmin("1, 2, 3, 4, 5, 6");
-        assertThat(lottoAdmin.equals(splitLottoAdmin)).isEqualTo(true);
+        Lotto splitLotto = new Lotto("1, 2, 3, 4, 5, 6");
+        assertThat(winningLotto.equals(splitLotto)).isEqualTo(true);
     }
 
     @Test
     @DisplayName("입력값 유효성검사(문자) test")
     void validationStringTest() {
-        assertThatThrownBy(() -> new LottoAdmin("1, 2, 3, e, 5, 6"))
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, e, 5, 6"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     @DisplayName("입력값 유효성검사(음수) test")
     void validationMinusNumTest() {
-        assertThatThrownBy(() -> new LottoAdmin("1, 2, 3, -4, 5, 6"))
+        assertThatThrownBy(() -> new Lotto("1, 2, 3, -4, 5, 6"))
                 .isInstanceOf(RuntimeException.class);
     }
 
