570d0e0eeb1bc9d9b0ea9973b29754822a54c9f4
louisJu <rokmctkd6@gmail.com>
2020-12-11 10:03:47 +0900

    refactor: main, outputView 수정

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -5,17 +5,19 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
 
         String money = InputView.inputMoney();
-        Lottos lottos = Shop.buyLottos(money);
+        List<Lotto> lottos = Shop.buyLottos(money);
         OutputView.printPurchaseLottos(lottos);
 
         LottoAdmin lottoAdmin = new LottoAdmin(InputView.inputWinningLotto());
         Result result = new Result();
-        for(Lotto lotto : lottos.getLottos()) {
+        for(Lotto lotto : lottos) {
             result.add(lottoAdmin.calculateRank(lotto));
         }
         OutputView.printWinningStatistics(result);
