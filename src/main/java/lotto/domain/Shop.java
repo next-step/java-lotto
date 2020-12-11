ecfff7d6974b5c9dc6b89102cc72c23106a08da8
louisJu <rokmctkd6@gmail.com>
2020-12-11 13:12:55 +0900

    refactor: 피드백 반영 * money 입력값 변경 (String -> long)

--- src/main/java/lotto/domain/Shop.java
+++ src/main/java/lotto/domain/Shop.java
@@ -4,18 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static lotto.utils.ValidationChecker.exceptionCheck;
-import static lotto.utils.ValidationChecker.isEmptyOrNull;
-
 public class Shop {
     private static final int LOTTO_PRICE = 1_000;
 
     private Shop() {}
 
-    public static List<Lotto> buyLottos(String money) {
-        isEmptyOrNull(money);
-        exceptionCheck(money);
-        long lottoCount = Long.parseLong(money) / LOTTO_PRICE;
+    public static List<Lotto> buyLottos(Long money) {
+        long lottoCount = money / LOTTO_PRICE;
         return Stream.generate(() -> new Lotto())
                 .limit(lottoCount)
                 .collect(Collectors.toList());
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
 
-    public static String inputMoney() {
+    public static Long inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return sc.nextLine();
+        return sc.nextLong();
     }
     public static String inputWinningLotto() {
+        sc.nextLine();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return sc.nextLine();
     }
