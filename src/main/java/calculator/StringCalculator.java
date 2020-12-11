29716fe5eb592360861d8772510321ce18664ec9
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-12 04:11:30 +0900

    (feat) 문자열 합 구하기

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import calculator.delimiter.StringDelimiter;
+import calculator.util.StringUtil;
+import calculator.util.ValidationUtil;
+
+import java.util.List;
+
+public class StringCalculator {
+
+    public int add(String text) {
+        if(ValidationUtil.isBlank(text)){
+            return 0;
+        }
+        return sum(StringUtil.textToInt(StringDelimiter.splitByDelimiter(text)));
+    }
+
+    public int sum(List<Integer> numbers) {
+        return numbers.stream()
+                .reduce(0, Integer::sum);
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    public void init() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    @DisplayName("문자열 더하기 테스트")
+    void add_test() {
+        assertAll(
+                () -> assertEquals(0, stringCalculator.add(null)),
+                () -> assertEquals(0, stringCalculator.add("")),
+                () -> assertEquals(1, stringCalculator.add("1")),
+                () -> assertEquals(5, stringCalculator.add("2,3")),
+                () -> assertEquals(15, stringCalculator.add("4,5:6")),
+                () -> assertEquals(24, stringCalculator.add("//;\n7;8;9"))
+        );
+    }
+
+}
