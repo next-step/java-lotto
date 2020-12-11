1eaea6e23a24bef0932643f7f1d133756c936736
yeGenieee <cyj95428@gmail.com>
2020-12-11 22:54:50 +0900

    feat: 입력받은 구입금액으로 구매가능한 로또 개수 테스트 통과하도록 구현

--- src/main/java/step2/LottoGame.java
+++ src/main/java/step2/LottoGame.java
@@ -1,15 +1,48 @@
 package step2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGame {
 
     private static List<Lottery> lotteryList;
 
-    public int buy(int money) {
+    /**
+     * 입력받은 구입 금액만큼 로또를 구입하는 메소드
+     * @param money 입력받은 구입 금액
+     * @return 발급된 로또 용지 묶음
+     */
+    public List<Lottery> buy(int money) {
         if (money < 1000) {
             throw new IllegalArgumentException(Constants.MONEY_IS_LESS_THAN_1000);
         }
-        return 0;
+        return makeLotteryList(money);
+    }
+
+    /**
+     * 로또 용지 묶음을 만드는 메소드
+     * @param money 입력받은 구입 금액
+     * @return 발급된 용지 묶음
+     */
+    public List<Lottery> makeLotteryList(int money) {
+        lotteryList = new ArrayList<>();
+
+        int numberOfLottery = getNumberOfLottery(money);
+
+        for (int i = 0; i < numberOfLottery; i++) {
+            Lottery lottery = new Lottery();
+            lotteryList.add(lottery);
+        }
+
+        return lotteryList;
+    }
+
+    /**
+     * 입력받은 구입 금액으로 살 수 있는 복권 장 수를 얻는 메소드
+     * @param money 입력받은 구입 금액
+     * @return 구입 가능한 복권 장 수
+     */
+    public int getNumberOfLottery(int money) {
+        return money / 1000;
     }
 }
