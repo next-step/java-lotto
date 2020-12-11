12bf425957781c6b7d4aca21217134d5bf1b0ed1
byungkyu-ju <noel1227@gmail.com>
2020-12-11 17:29:14 +0900

    docs: 기능요구사항 작성

---
+++ src/test/java/stringaddcalculator/StringAddCalculatorTest.java
@@ -0,0 +1,12 @@
+package stringaddcalculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : byungkyu
+ * @date : 2020/12/11
+ * @description :
+ **/
+public class StringAddCalculatorTest {
+
+}
