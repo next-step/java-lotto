af6376afacf359ec881fbe91373a5919bc416a4e
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-12 03:55:53 +0900

    (feat) 분리된 문자를 숫자로 변환

---
+++ src/main/java/calculator/StringUtil.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringUtil {
+
+    public static final String NEGATIVE_VALUE_INCLUDE_ERROR = "문자열에 음수값이 포함되어 있습니다.";
+
+    private StringUtil() {
+    }
+
+    public static List<Integer> textToInt(String[] texts) {
+        return Stream.of(texts)
+                .map(text -> validatePositiveNumber(text))
+                .collect(Collectors.toList());
+    }
+
+    public static int validatePositiveNumber(String text) {
+        int number = Integer.parseInt(text);
+        if(number < 0) {
+            throw new RuntimeException(NEGATIVE_VALUE_INCLUDE_ERROR);
+        }
+        return number;
+    }
+
+}
---
+++ src/test/java/calculator/StringUtilTest.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringUtilTest {
+
+    @Test
+    @DisplayName("문자열을 숫자로 변환하는 테스트")
+    void text_To_Int_Test() {
+        String[] texts = {"1", "2", "3"};
+        List<Integer> numbers = StringUtil.textToInt(texts);
+
+        assertThat(numbers.size()).isEqualTo(3);
+        assertEquals(1 , numbers.get(0));
+    }
+
+    @Test
+    @DisplayName("음수가 포함된 문자열 입력시 예외 발생")
+    void negative_number_check_test() {
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+                    String[] texts = {"-1", "2", "3"};
+                    StringUtil.textToInt(texts);
+                }).withMessageMatching("문자열에 음수값이 포함되어 있습니다.");
+    }
+
+}
