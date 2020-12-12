229b2273be7aec32c6fae70ef8f81c5bde37b1a0
hyeji <khji1210@naver.com>
2020-12-13 01:26:00 +0900

    feat : 생성된 로또 숫자들 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,21 @@
 package lotto.view;
 
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class OutputView {
+
+    public static void printLottoTickets(List<LottoTicket> lottoTickets) {
+        System.out.println(lottoTickets.size() + "개를 구매했습니다.");
+        lottoTickets.forEach(lottoTicket -> System.out.println(lottoNumbersToString(lottoTicket.getLottoNumbers())));
+    }
+    
+    private static String lottoNumbersToString(List<LottoNumber> lottoNumbers) {
+        return Arrays.toString(lottoNumbers.stream()
+                .mapToInt(LottoNumber::getValue)
+                .toArray());
+    }
 }
