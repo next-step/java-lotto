257e50beec109a49d63f2b45d0a9a50280cf1013
yeGenieee <cyj95428@gmail.com>
2020-12-12 22:28:51 +0900

    test: 로또 묶음 당첨 여부 테스트 코드 추가

--- src/test/java/step2/LottoGameTest.java
+++ src/test/java/step2/LottoGameTest.java
@@ -2,21 +2,41 @@ package step2;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import step2.domain.Lottery;
 import step2.domain.LottoGame;
+import step2.domain.WinningLottery;
 import step2.util.Constants;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class LottoGameTest {
 
     private LottoGame lottoGame;
+
+    private static Lottery[] lotteries = {
+            new Lottery(Arrays.asList(1, 2, 3, 4, 5, 6)),
+            new Lottery(Arrays.asList(2, 3, 4, 5, 6, 7)),
+            new Lottery(Arrays.asList(3, 4, 5, 6, 7, 8)),
+            new Lottery(Arrays.asList(4, 5, 6, 7, 8, 9)),
+            new Lottery(Arrays.asList(6, 7, 8, 9, 10, 45))
+    };
+
+    static List<Lottery> lotteryList() {
+        return Arrays.asList(lotteries);
+    }
 
     @BeforeEach
     void create_instance() {
@@ -38,5 +58,19 @@ class LottoGameTest {
     void create_lottery_by_input_money(int money, int expected) {
         List<Lottery> lotteryList = lottoGame.buy(money);
         assertThat(lotteryList.size()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("구매한 로또 묶음 당첨 여부 테스트")
+    @MethodSource("lotteryList")
+    void winning_test(List<Lottery> lotteryList) {
+        WinningLottery winningLottery = new WinningLottery("1,2,3,4,5,6".split(","));
+        Map<Lottery, Integer> matchedResult = lottoGame.matchLottery(lotteryList, winningLottery);
+        assertThat(
+                matchedResult.values()
+                                .stream()
+                                .map(Object::toString)
+                                .collect(Collectors.joining(","))
+        ).isEqualTo("6,5,4,3,1");
     }
 }
