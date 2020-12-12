6cba8b88c9b24cfb3801a794edeb45e28fb7f2f9
yeGenieee <cyj95428@gmail.com>
2020-12-13 00:01:23 +0900

    feat: 하나의 복권에 대해 당첨 여부 (일치 갯수) 테스트를 만족하는 코드 구현

--- src/main/java/step2/domain/Lottery.java
+++ src/main/java/step2/domain/Lottery.java
@@ -21,8 +21,51 @@ public class Lottery {
         this.selectedNumbers = winningNumber;
     }
 
+    /**
+     * 생성된 번호를 리턴하는 메소드
+     * @return 생성된 번호
+     */
     public List<Integer> getSelectedNumbers() {
         return this.selectedNumbers;
     }
 
+    /**
+     * 당첨 번호와 현재 복권을 매칭하는 메소드
+     * @param winningLottery 당첨 로또 번호
+     */
+    public void match(WinningLottery winningLottery) {
+        for (int number : winningLottery.getWinningLottery().getSelectedNumbers()) {
+            this.compareNumber(number, this.selectedNumbers);
+        }
+    }
+
+    /**
+     * 당첨 번호와 현재 복권의 번호들과 같은지 비교
+     * @param number 당첨 번호
+     * @param selectedNumbers 현재 복권의 숫자들
+     */
+    private void compareNumber(int number, List<Integer> selectedNumbers) {
+        for (int selected : selectedNumbers) {
+            isEqual(number, selected);
+        }
+    }
+
+    /***
+     * 현재 복권의 숫자와 당첨 번호가 같은지 비교하는 메소드
+     * @param number 당첨 번호
+     * @param selected 현재 복권의 번호
+     */
+    private void isEqual(int number, int selected) {
+        if (number == selected) {
+            this.matched++;
+        }
+    }
+
+    /**
+     * 개별 로또의 일치하는 갯수 반환하는 메소드
+     * @return 일치하는 숫자 갯수
+     */
+    public int getMatched() {
+        return this.matched;
+    }
 }
