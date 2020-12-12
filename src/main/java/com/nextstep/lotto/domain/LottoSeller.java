7ae76380ed73403617e96de9a5581258de3d825c
lazy-moon <magyeon@naver.com>
2020-12-12 23:41:38 +0900

    fix: 피드백 반영, 로또 번호 미리 만들어두도록 변경

--- src/main/java/com/nextstep/lotto/domain/LottoSeller.java
+++ src/main/java/com/nextstep/lotto/domain/LottoSeller.java
@@ -1,16 +1,9 @@
 package com.nextstep.lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LottoSeller {
     public static final int LOTTO_PRICE = 1000;
-    private static final int LOTTO_START_IDX = 0;
-    private static final int LOTTO_NUMBER_COUNT = 6;
-    private static final int LOTTO_MAX_COUNT = 45;
 
     private LottoSeller(){}
 
@@ -24,22 +17,9 @@ public class LottoSeller {
     public static Lottos buy(int count) {
         Lottos lottos = new Lottos();
         for (int i = 0; i < count ; i ++ ) {
-            List<Integer> randomNumbers = getRandomNumbers();
+            List<Integer> randomNumbers = RandomNumberFactory.createRandomNumbers();
             lottos.add(new Lotto(randomNumbers));
         }
         return lottos;
-    }
-
-    private static List<Integer> getRandomNumbers() {
-        List<Integer> totalNumbers = getTotalNumbers();
-        Collections.shuffle(totalNumbers);
-        List<Integer> collectedNumbers = totalNumbers.subList(LOTTO_START_IDX, LOTTO_NUMBER_COUNT);
-        Collections.sort(collectedNumbers);
-        return collectedNumbers;
-    }
-
-    private static List<Integer> getTotalNumbers() {
-        return Stream.iterate(1, i -> i + 1)
-                .limit(LOTTO_MAX_COUNT).collect(Collectors.toList());
     }
 }
