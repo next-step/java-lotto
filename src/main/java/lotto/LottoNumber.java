846ccf22383d32cd6da6252b3bcba27fd6fcf903
byungkyu-ju <noel1227@gmail.com>
2020-12-13 23:46:32 +0900

    feat: 로또 숫자의 범위는 1부터 46까지 이다

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,44 @@
+package lotto;
+
+import java.util.Objects;
+
+/**
+ * @author : byungkyu
+ * @date : 2020/12/13
+ * @description :
+ **/
+public class LottoNumber {
+	private static final int MINIMUM_OF_LOTTO_NUMBER = 1;
+	private static final int MAXIMUM_OF_LOTTO_NUMBER = 46;
+	private final int number;
+
+	public LottoNumber(int number) {
+		validate(number);
+		this.number = number;
+	}
+
+	private void validate(int number) {
+		if(number < MINIMUM_OF_LOTTO_NUMBER || number > MAXIMUM_OF_LOTTO_NUMBER){
+			throw new IllegalArgumentException("로또 숫자의 범위는 1부터 46까지 입니다.");
+		}
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		LottoNumber that = (LottoNumber)o;
+		return number == that.number;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(number);
+	}
+}
