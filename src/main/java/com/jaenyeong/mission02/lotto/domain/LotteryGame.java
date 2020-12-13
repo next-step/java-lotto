b26cca021cbbadf76df7bafb94d4a2c2e2272158
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-14 03:03:56 +0900

    feat(LotteryGame): implement LotteryGame class

---
+++ src/main/java/com/jaenyeong/mission02/lotto/domain/LotteryGame.java
@@ -0,0 +1,41 @@
+package com.jaenyeong.mission02.lotto.domain;
+
+import java.util.List;
+
+public class LotteryGame {
+    protected static final int PRICE = 1_000;
+    private static final int MISS_MATCH = 0;
+    private final LotteryNumbers lotteryNumbers;
+
+    private LotteryGame(final LotteryNumbers lotteryNumbers) {
+        this.lotteryNumbers = lotteryNumbers;
+    }
+
+    public static LotteryGame ofAuto() {
+        return new LotteryGame(LotteryNumbers.ofAuto());
+    }
+
+    public static LotteryGame ofManual(final List<Integer> givenNumbers) {
+        return new LotteryGame(LotteryNumbers.ofManual(givenNumbers));
+    }
+
+    public static int howManyBuyGame(final int money) {
+        return money / PRICE;
+    }
+
+    public int checkWinTheLottery(final LotteryGame otherGame) {
+        if ((otherGame == null) || (otherGame.getLotteryNumbers().size() <= 0)) {
+            return MISS_MATCH;
+        }
+
+        final List<Integer> currentGame = this.lotteryNumbers.getLotteryNumbers();
+
+        currentGame.retainAll(otherGame.getLotteryNumbers());
+
+        return currentGame.size();
+    }
+
+    protected List<Integer> getLotteryNumbers() {
+        return lotteryNumbers.getLotteryNumbers();
+    }
+}
