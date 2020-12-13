450bbf6e58acd0d0beac8f084f9d483a2449848e
BeomJin Jo <beomjinjo@BeomJinui-MacBookPro.local>
2020-12-13 14:47:47 +0900

    refactor(LottoRunner): extract method
    - 코드 가독성을 높이기 위해 지나치게 복잡해진 main 로직을 세분화된 메서드들로 분리

--- src/main/java/com/nextstep/lotto/application/LottoRunner.java
+++ src/main/java/com/nextstep/lotto/application/LottoRunner.java
@@ -9,34 +9,69 @@ import java.util.List;
 
 public class LottoRunner {
     public static void main(String[] args) {
+        Money originalMoney = getMoneyFromUser();
+
+        int numberOfStaticLotto = getNumberOfStaticLottoFromUser();
+
+        LottoTickets staticLottoTickets = getStaticLottoTicketsFromUser(numberOfStaticLotto);
+
+        LottoTickets lottoTickets = buyAutoLottoTicketsByRemainMoney(
+                originalMoney, numberOfStaticLotto, staticLottoTickets
+        );
+
+        WinningTicket winningTicket = lotteryWinningTicket();
+
+        showFinalLottoResult(lottoTickets, winningTicket, originalMoney);
+    }
+
+    private static Money getMoneyFromUser() {
         OutputView.printMoneyInput();
-        Money originalMoney = InputView.getMoney();
+        return InputView.getMoney();
+    }
 
+    private static int getNumberOfStaticLottoFromUser() {
         OutputView.printStaticLottoCountInput();
-        int numberOfStaticLotto = InputView.getNumberOfStaticLotto();
+        return InputView.getNumberOfStaticLotto();
+    }
 
+    private static LottoTickets getStaticLottoTicketsFromUser(final int numberOfStaticLotto) {
         OutputView.printStaticLottoNumberInput();
-        List<LottoTicket> staticLottoTicketInputs = new ArrayList<>();
+        List<LottoTicket> staticLottoTickets = new ArrayList<>();
         for (int i = 0; i < numberOfStaticLotto; i++) {
             LottoTicket lottoTicket = InputView.getLottoTicket();
-            staticLottoTicketInputs.add(lottoTicket);
+            staticLottoTickets.add(lottoTicket);
         }
 
+        return new LottoTickets(staticLottoTickets);
+    }
+
+    private static LottoTickets buyAutoLottoTicketsByRemainMoney(
+            final Money originalMoney, final int numberOfStaticLotto, final LottoTickets staticLottoTickets
+    ) {
         Money remainMoney = originalMoney.minusBoughtLottoTickets(numberOfStaticLotto);
         OutputView.printHowManyLottoTickets(numberOfStaticLotto, remainMoney);
 
-        LottoTickets staticLottoTickets = new LottoTickets(staticLottoTicketInputs);
         LottoTickets autoLottoTickets = LottoTicketsFactory.createAuto(remainMoney);
         LottoTickets lottoTickets = staticLottoTickets.merge(autoLottoTickets);
         OutputView.printBoughtLottoTickets(lottoTickets);
 
+        return lottoTickets;
+    }
+
+    private static WinningTicket lotteryWinningTicket() {
         OutputView.printWinningTicketInput();
         LottoTicket winningTicket = InputView.getLottoTicket();
 
         OutputView.printBonusNumberInput();
         LottoNumber bonusNumber = InputView.getBonusNumber();
 
-        LottoPrize lottoPrize = lottoTickets.checkResult(new WinningTicket(winningTicket, bonusNumber));
+        return new WinningTicket(winningTicket, bonusNumber);
+    }
+
+    private static void showFinalLottoResult(
+            final LottoTickets lottoTickets, final WinningTicket winningTicket, final Money originalMoney
+    ) {
+        LottoPrize lottoPrize = lottoTickets.checkResult(winningTicket);
         OutputView.printStatistics(originalMoney, lottoPrize);
     }
 }
