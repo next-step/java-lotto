dfcac4ef1f22996d61fab81707edc15da60d8f1c
Pilhwan Kim <pilhwankim03@gmail.com>
2020-12-13 19:03:30 +0900

    docs: @DisplayName 을 테스트에 추가

--- src/test/java/stringcalculator/domain/AdderTest.java
+++ src/test/java/stringcalculator/domain/AdderTest.java
@@ -1,5 +1,6 @@
 package stringcalculator.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AdderTest {
 
     @Test
+    @DisplayName("덧셈 연산 테스트")
     void addTest() {
         assertThat(Adder.add(Collections.singletonList(1))).isEqualTo(1);
         assertThat(Adder.add(Arrays.asList(1, 2))).isEqualTo(3);
--- src/test/java/stringcalculator/domain/InputConvertorTest.java
+++ src/test/java/stringcalculator/domain/InputConvertorTest.java
@@ -1,5 +1,6 @@
 package stringcalculator.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +12,7 @@ import java.util.List;
 class InputConvertorTest {
 
     @Test
+    @DisplayName("입력이 Null 이거나 빈 문자열일 경우 변환 테스트")
     void inputNullOrEmptyStringTest() {
         List<Integer> integerList = InputConvertor.convert(null);
         assertThat(integerList).contains(0);
@@ -20,12 +22,14 @@ class InputConvertorTest {
     }
 
     @Test
+    @DisplayName("입력이 정수 1개일 경우 변환 테스트")
     void inputOneNumberTest() {
         List<Integer> integerList = InputConvertor.convert("1");
         assertThat(integerList).contains(1);
     }
 
     @Test
+    @DisplayName("입력이 정수 2개 이상일 경우 변환 테스트")
     void inputTwoMoreNumberTest() {
         List<Integer> integerList = InputConvertor.convert("1,2");
         assertThat(integerList).contains(1, 2);
@@ -35,24 +39,28 @@ class InputConvertorTest {
     }
 
     @Test
+    @DisplayName("구분자가 쉽표 또는 콜론자 포함일 경우 변환 테스트")
     public void input_쉼표_또는_콜론_구분자_포함() throws Exception {
         List<Integer> integerList = InputConvertor.convert("1,2:3");
         assertThat(integerList).contains(1, 2, 3);
     }
 
     @Test
+    @DisplayName("구분자가 커스텀 문자일 경우 변환 테스트")
     void inputCustomSeparatorTest() {
         List<Integer> integerList = InputConvertor.convert("//;\n1;2;3");
         assertThat(integerList).contains(1, 2);
     }
 
     @Test
+    @DisplayName("음수가 입력에 있을시 RuntimeException 발생하는지 테스트")
     public void inputNegativeTest() throws Exception {
         assertThatThrownBy(() -> InputConvertor.convert("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
+    @DisplayName("정수가 아닌 값이 입력에 있을시 RuntimeException 발생하는지 테스트")
     public void inputNonNumberTest() throws Exception {
         assertThatThrownBy(() -> InputConvertor.convert("-,2,3"))
                 .isInstanceOf(RuntimeException.class);
