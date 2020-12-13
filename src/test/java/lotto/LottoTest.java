cbb015da3c53ea1d56a37eb648e467daa1abf178
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-13 18:32:31 +0900

    test : 입력값에 따른 구매 장수 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,8 @@ import calculator.StringAddCalculator;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
@@ -20,5 +22,13 @@ public class LottoTest {
 	public void calculateLottoCount_장당_이천오백원_로또_구매_매수_테스트() {
 		assertThat(lotto.calculateLottoCount(5000,2500)).isEqualTo(2);
 		assertThat(lotto.calculateLottoCount(7800,2500)).isEqualTo(3);
+	}
+
+	@Test
+	public void 구매장수에_맞게_6개의_번호의_로또가_뽑혔는지_테스트() {
+		List<List> result = lotto.randomShuffle(2);
+		assertThat(result.size()).isEqualTo(2);
+		List<Integer> lotto = result.get(0);
+		assertThat(lotto.size()).isEqualTo(6);
 	}
 }
