c8afb544a71f47477c7f2763f5dc45e35626c3e3
BeomJin Jo <beomjinjo@BeomJinui-MacBookPro.local>
2020-12-13 12:41:57 +0900

    refactor(MatchedNumber): extract magic number
    - 코드 가독성을 높이기 위해 매직 넘버를 상수로 추출

--- src/main/java/com/nextstep/lotto/domain/MatchedNumberCount.java
+++ src/main/java/com/nextstep/lotto/domain/MatchedNumberCount.java
@@ -3,6 +3,9 @@ package com.nextstep.lotto.domain;
 import com.nextstep.lotto.domain.exceptions.InvalidMatchedNumberCountException;
 
 public class MatchedNumberCount {
+    private final int MIN_VALUE = 0;
+    private final int MAX_VALUE = 6;
+
     private final int value;
 
     public MatchedNumberCount(int value) {
@@ -11,7 +14,7 @@ public class MatchedNumberCount {
     }
 
     private void validate(final int value) {
-        if (value < 0 || value > 6) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
             throw new InvalidMatchedNumberCountException("일치하는 로또 번호의 수는 1 ~ 6 사이여야만 합니다.");
         }
     }
