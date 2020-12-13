79b4b6069e741f516f141b769890d7706e891f11
orgojy <giveawesome@gmail.com>
2020-12-13 22:18:52 +0900

    feat(Expression): 연산에 필요한 구성을 가진 `Expression` 구현

---
+++ src/main/java/calculator/domain/Expression.java
@@ -0,0 +1,61 @@
+package calculator.domain;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static calculator.domain.DelimiterType.*;
+
+public class Expression {
+    private static final int DELIMITER_INDEX_FOR_GROUP = 1;
+    private static final int EXPRESSION_INDEX_FOR_GROUP = 2;
+
+    private Operands operands;
+
+    public Expression(String expression) {
+        if (isEmpty(expression)) {
+            this.operands = new Operands(new Operand());
+            return;
+        }
+        initiateOperands(expression);
+    }
+
+    private boolean isEmpty(String expression) {
+        return expression == null || expression.isEmpty();
+    }
+
+    private void initiateOperands(String expression) {
+        DelimiterType delimiterType = findByExpression(expression);
+        if (delimiterType == CUSTOM) {
+            initiateCustomOperands(expression);
+            return;
+        }
+        initiateDefaultOperands(expression);
+    }
+
+    private void initiateDefaultOperands(String expression) {
+        this.operands = convertOperands(expression.split(DEFAULT.getRegex()));
+    }
+
+    private void initiateCustomOperands(String expression) {
+        Matcher matcher = Pattern.compile(CUSTOM.getRegex()).matcher(expression);
+        if (matcher.find()) {
+            String delimiterGroup = matcher.group(DELIMITER_INDEX_FOR_GROUP);
+            String[] expressionGroup = matcher.group(EXPRESSION_INDEX_FOR_GROUP).split(delimiterGroup);
+            this.operands = convertOperands(expressionGroup);
+        }
+    }
+
+    private Operands convertOperands(String[] expression) {
+        return new Operands(
+                Stream.of(expression)
+                        .map(Operand::new)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public Operands getOperands() {
+        return operands;
+    }
+}
