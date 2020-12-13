ac9a05a24a17838e359185f8e284c312f3edc54f
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-14 04:17:54 +0900

    feat(Rank): implement Rank enum

---
+++ src/main/java/com/jaenyeong/mission02/lotto/domain/Rank.java
@@ -0,0 +1,44 @@
+package com.jaenyeong.mission02.lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int countOfMatch;
+    private final int winningMoney;
+
+    Rank(final int countOfMatch, final int winningMoney) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+
+    public static Rank valueOf(final int countOfMatch) {
+        return Arrays.stream(values())
+            .filter(rank -> compareCountOfMatch(countOfMatch, rank))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[error] This number is not valid."));
+    }
+
+    private static boolean compareCountOfMatch(final int countOfMatch, final Rank rank) {
+        return countOfMatch == rank.countOfMatch;
+    }
+
+//    public static Rank valueOf(int countOfMatch, boolean matchBonus) {
+//        // TODO 일치하는 수를 로또 등수로 변경한다. enum 값 목록은 "Rank[] ranks = values();"와 같이 가져올 수 있다.
+//        return null;
+//    }
+}
