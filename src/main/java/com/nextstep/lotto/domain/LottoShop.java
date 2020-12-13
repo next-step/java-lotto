8238954c3ce49b9fd76c02bac0604a92ac21105a
BeomJin Jo <beomjinjo@BeomJinui-MacBookPro.local>
2020-12-14 00:28:34 +0900

    feat(LottoShop): buy lottoTickets by auto
    - 사용자가 안전하게 남은 돈으로 자동 LottoTickets를 구매할 수 있도록 기능 구현

--- src/main/java/com/nextstep/lotto/domain/LottoShop.java
+++ src/main/java/com/nextstep/lotto/domain/LottoShop.java
@@ -6,12 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoShop {
-    private final Money money;
+    private final Money originalMoney;
     private final List<LottoTicket> staticBoughtLottoTickets = new ArrayList<>();
 
-    public LottoShop(final Money money) {
-        validate(money);
-        this.money = money;
+    public LottoShop(final Money originalMoney) {
+        validate(originalMoney);
+        this.originalMoney = originalMoney;
+    }
+
+    public void buyStaticLottoTicket(LottoTicket staticBoughtLottoTicket) {
+        validateCanBuy();
+        staticBoughtLottoTickets.add(staticBoughtLottoTicket);
+    }
+
+    public LottoTickets buyAutoLottoTicketsByRemainedMoney() {
+        return LottoTicketsFactory.creatAutoByRemainedMoney(originalMoney, new LottoTickets(staticBoughtLottoTickets));
+    }
+
+    int howManyStaticBoughtLottoTickets() {
+        return staticBoughtLottoTickets.size();
+    }
+
+    private void validateCanBuy() {
+        if (originalMoney.howManyLottoTickets() <= staticBoughtLottoTickets.size() + 1) {
+            throw new InvalidLottoShopException("제시된 금액보다 수동으로 LottoTicket을 더 많이 살 수 없습니다.");
+        }
     }
 
     private void validate(final Money money) {
--- src/test/java/com/nextstep/lotto/domain/LottoShopTest.java
+++ src/test/java/com/nextstep/lotto/domain/LottoShopTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.nextstep.lotto.domain.LottoTicketFixtures.LOTTO_TICKET_123456;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -25,5 +26,40 @@ class LottoShopTest {
         Money money = new Money(notEnoughValue);
 
         assertThatThrownBy(() -> new LottoShop(money)).isInstanceOf(InvalidLottoShopException.class);
+    }
+
+    @DisplayName("정해진 금액 내에서 수동으로 LottoTicket을 구매할 수 있다.")
+    @Test
+    void buyStaticLottoTicketTest() {
+        Money money = new Money(10000L);
+        LottoShop lottoShop = new LottoShop(money);
+        int expectedSize = 1;
+
+        lottoShop.buyStaticLottoTicket(LOTTO_TICKET_123456);
+
+        assertThat(lottoShop.howManyStaticBoughtLottoTickets()).isEqualTo(expectedSize);
+    }
+
+    @DisplayName("정해진 금액을 넘어서 수동으로 LottoTicket 구매 시도 시 예외 발생")
+    @Test
+    void buyStaticLottoTicketFailTest() {
+        Money money = new Money(2000L);
+        LottoShop lottoShop = new LottoShop(money);
+        lottoShop.buyStaticLottoTicket(LOTTO_TICKET_123456);
+
+        assertThatThrownBy(() -> lottoShop.buyStaticLottoTicket(LOTTO_TICKET_123456))
+                .isInstanceOf(InvalidLottoShopException.class);
+    }
+
+    @DisplayName("수동으로 구매하고 남은 LottoTicket을 자동으로 구매한다.")
+    @Test
+    void buyAutoLottoTicketsByRemainedMoneyTest() {
+        Money money = new Money(10000L);
+        LottoShop lottoShop = new LottoShop(money);
+        lottoShop.buyStaticLottoTicket(LOTTO_TICKET_123456);
+
+        LottoTickets lottoTickets = lottoShop.buyAutoLottoTicketsByRemainedMoney();
+
+        assertThat(lottoTickets.size()).isEqualTo(10);
     }
 }
