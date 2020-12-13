4626e62b30459a26215834f156c5a9a288e2f7a2
shk3029 <shk30290@naver.com>
2020-12-14 02:28:11 +0900

    feat: 메인결과 최종결과출력

--- src/main/java/step2/LottoGameMain.java
+++ src/main/java/step2/LottoGameMain.java
@@ -1,16 +1,25 @@
 package step2;
 
 import step2.domain.LottoGame;
+import step2.domain.LottoNumberGenerator;
+import step2.domain.LottoNumbers;
 
 import java.math.BigDecimal;
 
+import static step2.view.InputView.inputMessageLastWeekWinningNumber;
 import static step2.view.InputView.inputMessageLottoAmount;
+import static step2.view.OutputView.outputMessageLottoNumbers;
+import static step2.view.OutputView.outputMessageLottoResult;
 
 public class LottoGameMain {
 
     public static void main(String[] args) {
         int amount = inputMessageLottoAmount();
-        LottoGame lottoGame = new LottoGame(BigDecimal.valueOf(amount));
+        LottoGame lottoGame = new LottoGame(BigDecimal.valueOf(amount), new LottoNumberGenerator());
+        outputMessageLottoNumbers(lottoGame.getLottoNumbers());
+
+        String lastWinningNumber = inputMessageLastWeekWinningNumber();
+        outputMessageLottoResult(amount, lottoGame.lottoResultMap(lastWinningNumber));
     }
 
 
