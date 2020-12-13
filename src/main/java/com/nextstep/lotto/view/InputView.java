3c8565dceba0668de9edeb4e54afffbcbc889f8d
lazy-moon <magyeon@naver.com>
2020-12-13 23:59:05 +0900

    refactor: 피드백 반영, nextInt로 변경

--- src/main/java/com/nextstep/lotto/view/InputView.java
+++ src/main/java/com/nextstep/lotto/view/InputView.java
@@ -7,8 +7,7 @@ public class InputView {
 
     public static int inputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        String line = SCANNER.nextLine();
-        return Integer.parseInt(line);
+        return SCANNER.nextInt();
     }
 
     public static String inputWinnerNumbers() {
