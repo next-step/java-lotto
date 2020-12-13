14fb44914662a57bdf24ef38b9d580afc9f8fdd1
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-14 01:36:16 +0900

    feat(LotteryNumbers): add create LotteryNumbers function when given numbers

--- src/main/java/com/jaenyeong/mission02/lotto/domain/LotteryNumbers.java
+++ src/main/java/com/jaenyeong/mission02/lotto/domain/LotteryNumbers.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class LotteryNumbers {
     private static final int MAX_NUMBERS_OF_LOTTERY = 6;
+    public static final String ERR_TEXT_INVALID_NUMBERS = "[error] This number list is not valid.";
     private final Set<LotteryNumber> lotteryNumbers;
 
     private LotteryNumbers(final Set<LotteryNumber> lotteryNumbers) {
@@ -29,5 +30,20 @@ public class LotteryNumbers {
         return lotteryNumbers.stream()
             .map(LotteryNumber::getLotteryNumber)
             .collect(Collectors.toList());
+    }
+
+    public static LotteryNumbers ofManual(final List<Integer> givenNumbers) {
+        final Set<Integer> uniqueNumbers = new HashSet<>(givenNumbers);
+        if (uniqueNumbers.size() != MAX_NUMBERS_OF_LOTTERY) {
+            throw new IllegalArgumentException(ERR_TEXT_INVALID_NUMBERS);
+        }
+
+        final Set<LotteryNumber> lotteryNumbers = uniqueNumbers.stream()
+            .map(LotteryNumber::ofManual)
+            .collect(Collectors.toCollection(
+                () -> new TreeSet<>(Comparator.comparingInt(LotteryNumber::getLotteryNumber))
+            ));
+
+        return new LotteryNumbers(lotteryNumbers);
     }
 }
--- src/test/java/com/jaenyeong/mission02/lotto/domain/LotteryNumbersTest.java
+++ src/test/java/com/jaenyeong/mission02/lotto/domain/LotteryNumbersTest.java
@@ -2,10 +2,17 @@ package com.jaenyeong.mission02.lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Lotto 게임에 복권 번호 리스트의 래핑 객체(일급 컬렉션) LotteryNumbers 클래스 테스트")
@@ -26,5 +33,53 @@ class LotteryNumbersTest {
 
         lottery.forEach((lotteryNumber) ->
             assertThat(lotteryNumber).isBetween(start, end));
+    }
+
+    @ParameterizedTest
+    @MethodSource("givenValidNumberList")
+    @DisplayName("사용자가 직접 올바른 번호를 지정하는 경우 LottoNumbers 객체 생성 테스트")
+    void createLotteryNumberManuallyWhenGivenValidNumbers(final List<Integer> givenNumbers) {
+        final LotteryNumbers lotteryNumbers = LotteryNumbers.ofManual(givenNumbers);
+
+        final List<Integer> lottery = lotteryNumbers.getLotteryNumbers();
+        givenNumbers.sort(Comparator.naturalOrder());
+
+        final int max = 6;
+        final int start = 1;
+        final int end = 45;
+
+        assertEquals(lottery.size(), max);
+        lottery.forEach((lotteryNumber) -> assertThat(lotteryNumber).isBetween(start, end));
+        assertEquals(lottery, givenNumbers);
+    }
+
+    private static Stream<Arguments> givenValidNumberList() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+            Arguments.of(Arrays.asList(8, 17, 26, 33, 41, 45)),
+            Arguments.of(Arrays.asList(1, 12, 23, 34, 42, 45)),
+            Arguments.of(Arrays.asList(10, 20, 30, 40, 15, 25)),
+            Arguments.of(Arrays.asList(40, 41, 42, 43, 44, 45))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("givenInvalidNumberList")
+    @DisplayName("사용자가 직접 올바르지 않은 번호를 지정하는 경우 LottoNumbers 객체 생성 테스트")
+    void createLotteryNumberManuallyWhenGivenInvalidNumbers(final List<Integer> givenNumbers) {
+        assertThatThrownBy(() -> {
+            final LotteryNumbers lotteryNumbers = LotteryNumbers.ofManual(givenNumbers);
+        })
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> givenInvalidNumberList() {
+        return Stream.of(
+            Arguments.of(Arrays.asList()),
+            Arguments.of(Arrays.asList(-1, 17, 26, 33, 41, 45)),
+            Arguments.of(Arrays.asList(1, 12, 23, 34, 42, 46)),
+            Arguments.of(Arrays.asList(10, 20, 30, 40, 15)),
+            Arguments.of(Arrays.asList(40, 41))
+        );
     }
 }
