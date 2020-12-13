871d996f9ca7df5b14798f45164225359bc30fcc
JeongDaEul <eeeulll@gmail.com>
2020-12-13 10:18:59 +0900

    Step1 문자열 덧셈 계산기 PR (#1055)
    * docs(READ.md): 기능 요구사항 추가
    
    * 문자열 덧셈 계산기 구현
    
    * Update src/main/java/lotto/StringAddCalculator.java
    
    Co-authored-by: 강현구 <hkkang@woowahan.com>
    
    * Update src/main/java/lotto/StringAddCalculator.java
    
    Null 리턴 가능성 제거
    
    Co-authored-by: 강현구 <hkkang@woowahan.com>
    
    * Update src/main/java/lotto/StringAddCalculator.java
    
    Co-authored-by: 강현구 <hkkang@woowahan.com>
    
    * Update src/main/java/lotto/StringAddCalculator.java
    
    기본형으로 변경, Inter일 경우 null 체크 추가
    
    Co-authored-by: 강현구 <hkkang@woowahan.com>
    
    * Update src/main/java/lotto/StringAddCalculator.java
    
    코드 가독성을 위한 수정
    
    Co-authored-by: 강현구 <hkkang@woowahan.com>
    
    * fix : 코멘트 반영
    
    * fix : 코멘트 반영
    
    Co-authored-by: 정다을(Elin) <elin.jeong@goodoc.co.kr>
    Co-authored-by: 강현구 <hkkang@woowahan.com>

---
+++ src/main/java/lotto/StringAddCalculator.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+	private static final String DEFAULT_DELIMITER = ",|:";
+
+	private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+
+	private static Pattern PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+
+	public static int splitAndSum(String inputText) {
+		String text = Optional.ofNullable(inputText).orElse("");
+		Matcher matcher = PATTERN.matcher(text);
+		if (matcher.find()) {
+			String delimiter = matcher.group(1);
+			String expression = matcher.group(2);
+			return sum(expression.split(delimiter));
+		}
+		return sum(text.split(DEFAULT_DELIMITER));
+	}
+
+	private static int sum(String[] tokens) {
+		int result = 0;
+		for (String input : tokens) {
+			result += validatePositiveNumber(convertInteger(input));
+		}
+		return result;
+
+	}
+
+	private static int convertInteger(String input) {
+		if (input == null || input.isEmpty()) {
+			return 0;
+		}
+		return Integer.parseInt(input);
+	}
+
+	private static int validatePositiveNumber(int value) {
+		if (value < 0) {
+			throw new RuntimeException();
+		}
+		return value;
+	}
+
+}
