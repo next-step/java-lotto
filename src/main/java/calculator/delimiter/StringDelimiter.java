73bfd6f02855874ceb6954c7e565cac059ce9feb
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-14 05:31:13 +0900

    (refactor) 매직넘버 상수화

--- src/main/java/calculator/delimiter/StringDelimiter.java
+++ src/main/java/calculator/delimiter/StringDelimiter.java
@@ -7,16 +7,18 @@ public class StringDelimiter {
 
     private static final String DEFAULT_DELIMITER_REGEX = ",|:";
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int PATTERN_MATCH_GROUP_1 = 1;
+    private static final int PATTERN_MATCH_GROUP_2 = 2;
 
     private StringDelimiter() {
     }
 
     public static String[] splitByDelimiter(String text) {
-        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
 
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(PATTERN_MATCH_GROUP_1);
+            return matcher.group(PATTERN_MATCH_GROUP_2).split(customDelimiter);
         }
 
         return text.split(DEFAULT_DELIMITER_REGEX);
