849c1cbd304fab503fe505a4a1d18a67ecee10d6
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-13 21:51:00 +0900

    test : 당첨번호 입력 테스트, 당첨번호와 내가 산 로또 번호 비교 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,14 @@
 package lotto;
 
-import calculator.StringAddCalculator;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
 
@@ -26,9 +28,39 @@ public class LottoTest {
 
 	@Test
 	public void 구매장수에_맞게_6개의_번호의_로또가_뽑혔는지_테스트() {
-		List<List> result = lotto.randomShuffle(2);
+		List<List> result = lotto.generateAutoLottoNumbers(2);
 		assertThat(result.size()).isEqualTo(2);
 		List<Integer> lotto = result.get(0);
 		assertThat(lotto.size()).isEqualTo(6);
+	}
+
+	@Test
+	public void 지난주_당첨번호에서_내가_산_로또번호를_비교해서_결과를_확인한다(){
+		List<Integer> lastPrizeNumber = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+		List<Integer> purchargedLottoNumber = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+		assertThat(lotto.calculatePrizeStatistics(lastPrizeNumber,purchargedLottoNumber)).isEqualTo(6);
+
+		purchargedLottoNumber = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 7, 8));
+		assertThat(lotto.calculatePrizeStatistics(lastPrizeNumber,purchargedLottoNumber)).isEqualTo(4);
+
+	}
+
+	@Test
+	public void 지난주_당첨번호가_잘입력되었는가_테스트(){
+		assertThat(lotto.convertStringToInteger("1, 2, 3, 4, 5, 6").size()).isEqualTo(6);
+
+		assertThat(lotto.convertStringToInteger("1 , 2 , 3 , 4, 5, 6").size()).isEqualTo(6);
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			lotto.convertStringToInteger("1, 2, 3, 4, 5, 6, 7");
+		});
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			lotto.convertStringToInteger("1, 1, 1, 1, 5");
+		});
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			lotto.convertStringToInteger("A, 2, 3, 4, 5, 6, 7");
+		});
+
 	}
 }
