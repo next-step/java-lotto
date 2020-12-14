08f060ee8a9191da42132a8ff78afcb6f79139e2
Ssabae <llsb156@gmail.com>
2020-12-15 02:53:07 +0900

    feat: view 기능 Class 추가 (Input, Output)

---
+++ src/main/java/com/ssabae/nextstep/stringcalculator/view/Printer.java
@@ -0,0 +1,14 @@
+package com.ssabae.nextstep.stringcalculator.view;
+
+/**
+ * @author : leesangbae
+ * @project : java-lotto
+ * @since : 2020-12-14
+ */
+public class Printer {
+
+    public static void printCalculatedResult(int number) {
+        System.out.println("합계 = " + number);
+    }
+
+}
---
+++ src/main/java/com/ssabae/nextstep/stringcalculator/view/UserInputHelper.java
@@ -0,0 +1,19 @@
+package com.ssabae.nextstep.stringcalculator.view;
+
+import java.util.Scanner;
+
+/**
+ * @author : leesangbae
+ * @project : java-lotto
+ * @since : 2020-12-14
+ */
+public class UserInputHelper {
+
+    private static final String INPUT_VALUE = "계산기에 값을 입력해주세요.(ex: 1,2,3 or 1:2:3 or //;\\n1;2;3)";
+
+    public String inputString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(UserInputHelper.INPUT_VALUE);
+        return scanner.nextLine();
+    }
+}
