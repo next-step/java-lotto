d8e6308c50634b2475ec2193339ce384ac250e60
Yeoseon Yun <devyyskr@gmail.com>
2020-12-14 15:24:32 +0900

    feat(View,Main): View 및 Main함수 구현

--- src/main/java/step1/Main.java
+++ src/main/java/step1/Main.java
@@ -3,6 +3,9 @@ package step1;
 public class Main {
 
     public static void main(String[] args) {
+        String input = View.readInput();
+        Numbers numbers = new Numbers(InputSplitter.split(input));
 
+        View.printResult(StringAddCalculator.calculate(numbers));
     }
 }
