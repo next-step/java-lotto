b5481856e604d9a1bd0cb3781841348ba1a0112b
deviscreen <kdy0573@daum.net>
2020-12-15 00:34:15 +0900

    [feed] step1 피드백 반영

--- src/main/java/util/NumberUtil.java
+++ src/main/java/util/NumberUtil.java
@@ -2,10 +2,11 @@ package util;
 
 public class NumberUtil {
 
+    private final static String MINUS_VALUE_ERROR="음수는 입력될 수 없습니다.";
     public static int stringToInt(String number) {
         int parseInt = Integer.parseInt(number);
-        if(parseInt < 0){
-            throw new RuntimeException("음수는 입력될 수 없습니다.");
+        if (parseInt < 0) {
+            throw new RuntimeException(MINUS_VALUE_ERROR);
         }
         return parseInt;
     }
