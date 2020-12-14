ceaf05f7d94963afac5a1f700e288100c6d7e64f
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-15 04:35:14 +0900

    (feat) 당첨 통계 및 수익률 출력

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -9,17 +9,20 @@ public class LottoGame {
 
     public static void main(String[] args) {
         int money = InputView.inputMoney();
-
         LottoTicketCount lottoTicketCount = LottoTicketCount.getInstance(money);
 
         LottoShop lottoShop = LottoShop.getInstance();
         PublishedLottoTicket publishedLottoTicket = lottoShop.buyLotto(money);
 
-        OutputView.printLottoTicketCount(lottoTicketCount.getLottoTicketCount());
-        OutputView.printLottoNumbers(publishedLottoTicket.getPublishedLottoTicket());
+        OutputView.printPublishedLottoResult(lottoTicketCount, publishedLottoTicket);
 
         List<Integer> winningLottoNumbers = InputView.inputWinningLottoNumbers();
+        WinningLotto winningLotto = WinningLotto.ofLottoNumbers(LottoMachine.createManualLottoNumbers(winningLottoNumbers));
 
+        LottoResult lottoResult = LottoResult.getInstance();
+        lottoResult.analyzeLottoRank(publishedLottoTicket.getPublishedLottoTicket(), winningLotto);
+
+        OutputView.printLottoStatics(lottoResult, lottoResult.calculatePrizeRate(money));
     }
 
 }
