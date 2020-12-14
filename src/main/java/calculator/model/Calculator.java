a02062364cdcfe81625afc44e197e40519ab1b0e
charllossdev <charlloss.dev@gmail.com>
2020-12-14 16:27:57 +0900

    test: add 계산기 객체 생성 및 계산 테스트

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,64 +1,19 @@
 package calculator.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Calculator {
 
-	public static final String CALCULATE_SEPARATOR_COMMA = ",";
-	public static final String CALCULATE_SEPARATOR_COLON = ":";
-	private final List<String> expression;
+	private final Expression expression;
 
-	public Calculator(String userInput) {
-		
-		this.expression = getExpression(userInput);
+	public Calculator(Expression expression) {
+
+		this.expression = expression;
 	}
 
-	public List<String> getExpressionList() {
-		return this.expression;
-	}
+	public int getSum() {
 
-
-	public List<String> getExpression(String userInput) {
-
-		return splitExpression(splitUserSeparator(userInput));
-	}
-
-	public List<String> splitExpression(List<String> strings) {
-
-		List<String> result = new ArrayList<>();
-		for (String str : strings) {
-
-			result.addAll(Arrays.asList(str.split("[" + CALCULATE_SEPARATOR_COMMA + CALCULATE_SEPARATOR_COLON + "]")));
-		}
-
-		return result;
-	}
-
-	public int calculate() {
-
-		return expression.stream()
-			.mapToInt(Integer::parseInt)
-			.sum();
-	}
-
-	public List<String> splitUserSeparator(String expression) {
-
-		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(expression);
-		if (m.find()) {
-
-			String customDelimiter = m.group(1);
-			String[] tokens= m.group(2).split(customDelimiter);
-
-			return Arrays.asList(tokens);
-		}
-
-		return Collections.singletonList(expression);
+		return this.expression.sum();
 	}
 
 	@Override
--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -2,79 +2,18 @@ package calculator.model;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
-	private Calculator calculator;
-	private String commaStr;
-	private String colonStr;
-	private String mixedStr;
-	private String anotherSeparator;
-
-	@BeforeEach
-	void init() {
-
-		commaStr = "1,2,3";
-		colonStr = "1:2:3";
-		mixedStr = "1,2:3";
-		anotherSeparator = "//;\n1;2:3;4:5;6,7,8;9";
-	}
-
 	@Test
-	@DisplayName("문자열 계산기: 나눈 문자열 계산 테스트")
-	void calculateTest() {
+	@DisplayName("문자열 계산기: 객체 생성 및 전체 계산 테스트")
+	void calculateAllTest() {
 
-		calculator = new Calculator(commaStr);
-		assertThat(calculator.calculate()).isEqualTo(6);
+		Expression expression = new Expression("//;\n1;2:3;4:5;6,7,8;9");
+		Calculator calculator = new Calculator(expression);
 
-		calculator = new Calculator(colonStr);
-		assertThat(calculator.calculate()).isEqualTo(6);
-
-		calculator = new Calculator(mixedStr);
-		assertThat(calculator.calculate()).isEqualTo(6);
-
-		calculator = new Calculator(anotherSeparator);
-		assertThat(calculator.calculate()).isEqualTo(45);
-	}
-
-	@Test
-	@DisplayName("문자열 계산기: 유저가 추가한 구분자 계산식 생성 및 문자열 나누기 테스트")
-	void findSeparatorTest() {
-
-		calculator = new Calculator(anotherSeparator);
-
-		assertThat(calculator.getExpression(anotherSeparator)).isEqualTo(
-			Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
-
-	}
-
-	@Test
-	@DisplayName("문자열 계산기: 다양한 구분자 계산식 생성 및 문자열 나누기 테스트")
-	void splitTest() {
-
-		String[] inputArray = {"1", "2", "3"};
-
-		calculator = new Calculator(commaStr);
-		assertThat(calculator.getExpressionList()).isEqualTo(Arrays.asList(inputArray));
-
-		calculator = new Calculator(colonStr);
-		assertThat(calculator.getExpressionList()).isEqualTo(Arrays.asList(inputArray));
-
-		calculator = new Calculator(mixedStr);
-		assertThat(calculator.getExpressionList()).isEqualTo(Arrays.asList(inputArray));
-	}
-
-	@Test
-	@DisplayName("문자열 계산기: 계산식을 가지는 객체 생성 테스트")
-	void initCalculator() {
-
-		calculator = new Calculator(mixedStr);
-
-		assertThat(calculator).isEqualTo(new Calculator(mixedStr));
+		assertThat(calculator.getSum()).isEqualTo(45);
 	}
 }
