68d4fd2635b057bed6248774d0734b76e3f7ca6a
tearight <tearightss@gmail.com>
2020-12-14 20:55:15 +0900

    feat(StringCalculator): (,)구분 숫자합 기능 구현
    - 숫자 하나 덧셈 기능 구현
    - 숫자 둘 이상 덧셈 기능 구현

--- src/main/java/string_adder/StringCalculator.java
+++ src/main/java/string_adder/StringCalculator.java
@@ -1,18 +1,27 @@
 package string_adder;
 
+import java.util.stream.Stream;
+
 class StringCalculator {
 
 	private static final String TEXT_EMPTY = "";
+	private static final String SEPARATOR_COMMA = ",";
 
 	int sum(String text) {
 		if (isZeroReturnTarget(text)) {
 			return 0;
 		}
-
-		return -1;
+		String[] numbers = text.split(SEPARATOR_COMMA);
+		return sum(numbers);
 	}
 
 	private boolean isZeroReturnTarget(String text) {
 		return text == null || TEXT_EMPTY.equals(text);
+	}
+
+	private int sum(String[] numbers) {
+		return Stream.of(numbers)
+				.mapToInt(Integer::parseInt)
+				.sum();
 	}
 }
--- src/test/java/string_adder/StringCalculatorTest.java
+++ src/test/java/string_adder/StringCalculatorTest.java
@@ -2,6 +2,8 @@ package string_adder;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,4 +26,23 @@ class StringCalculatorTest {
 	void sum_빈문자열() {
 		assertThat(stringCalculator.sum("")).isEqualTo(0);
 	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0=0", "1=1", "999=999"}, delimiter = '=')
+	void sum_숫자한개(String number, int expected) {
+		assertThat(stringCalculator.sum(number)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0,1=1", "2,999=1001"}, delimiter = '=')
+	void sum_숫자두개(String twoNumbers, int expected) {
+		assertThat(stringCalculator.sum(twoNumbers)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0,1,2=3", "2,1,0,5,1000=1008"}, delimiter = '=')
+	void sum_숫자여러개(String multipleNumbers, int expected) {
+		assertThat(stringCalculator.sum(multipleNumbers)).isEqualTo(expected);
+	}
+
 }
