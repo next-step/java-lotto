d8dbe30eeb1de8aba8492c7b1886610e8a0e618b
shk3029 <shk30290@naver.com>
2020-12-16 00:43:24 +0900

    test: lotto test 수정

--- src/test/java/step3/domain/LottoTest.java
+++ src/test/java/step3/domain/LottoTest.java
@@ -6,15 +6,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import step3.domain.Lotto;
-import step3.domain.LottoNumber;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
-import static step3.domain.LottoNumberGenerator.LOTTO_COUNT;
-import static step3.domain.LottoNumberGenerator.START_NUMBER;
+import static step3.domain.factory.LottoFactory.produceLotto;
 
 class LottoTest {
 
@@ -22,20 +17,20 @@ class LottoTest {
 
     @BeforeEach
     void setUp() {
-        lotto = generateLottoNumber_1to6();
+        lotto = produceLotto("1,2,3,4,5,6");
     }
 
     @ParameterizedTest
     @MethodSource("generateLottoData")
-    @DisplayName("로또번호가 1,2,3,4,5,6일 때 당첨번호들과 일치하는 개수 기능 테스트 ")
-    void equal_lottoNumber_count(String input, int expected) {
-        String[] winningNumbers = input.split(",");
+    @DisplayName("로또번호 1,2,3,4,5,6 일 때, 당첨번호 일치하는 개수 기능 테스트 ")
+    void equal_lottoNumber_count(String lottoNumber, int count) {
+        String[] winningNumbers = lottoNumber.split(",");
 
         for (String winningNumber : winningNumbers) {
-            lotto.equalCheck(new LottoNumber(Integer.parseInt(winningNumber)));
+            lotto.equalCheck(new LottoNumber(Integer.parseInt(winningNumber)), new LottoNumber(1));
         }
 
-        Assertions.assertEquals(lotto.getCount(), expected);
+        Assertions.assertEquals(lotto.getCount(), count);
     }
 
 
@@ -48,11 +43,25 @@ class LottoTest {
         );
     }
 
-    public static Lotto generateLottoNumber_1to6() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int i = START_NUMBER; i <= LOTTO_COUNT; i++) {
-            lottoNumbers.add(new LottoNumber(i));
+
+    @ParameterizedTest
+    @MethodSource("generateLottoBonusData")
+    @DisplayName("로또번호 1,2,3,4,5,6 일 때, 보너스 번호 존재 기능 테스트 ")
+    void equal_lottoNumber_match_bonus(String lottoNumber, int bonus, boolean expected) {
+        String[] winningNumbers = lottoNumber.split(",");
+
+        for (String winningNumber : winningNumbers) {
+            lotto.equalCheck(new LottoNumber(Integer.parseInt(winningNumber)), new LottoNumber(bonus));
         }
-        return new Lotto(lottoNumbers);
+
+        Assertions.assertEquals(lotto.isMatchBonus(), expected);
+    }
+
+
+    static Stream<Arguments> generateLottoBonusData() {
+        return Stream.of(
+                Arguments.of("1,2,3,14,15,16", 3, true),
+                Arguments.of("1,2,3,4,5,16", 17, false)
+        );
     }
 }
