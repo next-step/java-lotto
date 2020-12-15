60263e1d101e640a8054b52fa4ef11d0d2a145c5
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-15 12:30:26 +0900

    refactor(WinningStatistics): extract parse winning prize to String type from rank

--- src/main/java/com/jaenyeong/mission02/lotto/domain/WinningStatistics.java
+++ src/main/java/com/jaenyeong/mission02/lotto/domain/WinningStatistics.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toList;
 
 public class WinningStatistics {
-    private static final String PRINT_FORMAT = "%d개 일치 (%d원) - %d개";
+    private static final String WINNING_PRIZE_PRINT_FORMAT = "%d개 일치 (%d원) - %d개";
     private static final int NONE = 0;
     private static final int ADD = 1;
     private final Map<Rank, Integer> lotteryRanksMap;
@@ -28,11 +28,15 @@ public class WinningStatistics {
 
         printFormatStatistics = this.lotteryRanksMap.keySet().stream()
             .filter(Rank::isNotMissMatch)
-            .map(integer -> String.format(PRINT_FORMAT,
-                integer.getCountOfMatch(),
-                integer.getWinningPrize(),
-                numberOfTheRank(integer)))
+            .map(this::parseWinningPrizePrintFormat)
             .collect(toList());
+    }
+
+    private String parseWinningPrizePrintFormat(final Rank rank) {
+        return String.format(WINNING_PRIZE_PRINT_FORMAT,
+            rank.getCountOfMatch(),
+            rank.getWinningPrize(),
+            numberOfTheRank(rank));
     }
 
     public static WinningStatistics of(final List<Rank> lotteryRanks, final int buyPrice) {
@@ -42,12 +46,12 @@ public class WinningStatistics {
         return new WinningStatistics(lotteryRanksMap, buyPrice);
     }
 
-    public int numberOfGames() {
-        return lotteryRanksMap.values()
-            .stream()
-            .reduce(Integer::sum)
-            .orElse(NONE);
-    }
+//    public int numberOfGames() {
+//        return lotteryRanksMap.values()
+//            .stream()
+//            .reduce(Integer::sum)
+//            .orElse(NONE);
+//    }
 
     public int numberOfTheRank(final Rank rank) {
         return lotteryRanksMap.getOrDefault(rank, NONE);
