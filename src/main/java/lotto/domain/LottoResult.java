46532344083e6c9c7b83ddfa9a93a28b544a7809
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-15 22:35:37 +0900

    add : 당첨 정산기능 추가

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,70 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoResult {
+	public static final int THREE_MATCH_PRIZE_MONEY = 5000;
+
+	public static final int FOUR_MATCH_PRIZE_MONEY = 50000;
+
+	public static final int FIVE_MATCH_PRIZE_MONEY = 1500000;
+
+	public static final int SIX_MATCH_PRIZE_MONEY = 2000000000;
+
+
+	private int threeMatchCount = 0;
+
+	private int fourMatchCount = 0;
+
+	private int fiveMatchCount = 0;
+
+	private int sixMatchCount = 0;
+
+	private int totalPrizeMoney = 0;
+
+	private double earningsRate = 0.0;
+
+	public int getThreeMatchCount() {
+		return threeMatchCount;
+	}
+
+	public int getFourMatchCount() {
+		return fourMatchCount;
+	}
+
+	public int getFiveMatchCount() {
+		return fiveMatchCount;
+	}
+
+	public int getSixMatchCount() {
+		return sixMatchCount;
+	}
+
+	public double getEarningsRate() {
+		return earningsRate;
+	}
+
+	public void arrangePrize(List<Integer> lottoMatchCountResult, int lottoPricePerSheet) {
+		for (Integer matchCount : lottoMatchCountResult) {
+			if (matchCount == 3) {
+				threeMatchCount += 1;
+				totalPrizeMoney += THREE_MATCH_PRIZE_MONEY;
+			}
+			if (matchCount == 4) {
+				fourMatchCount += 1;
+				totalPrizeMoney += FOUR_MATCH_PRIZE_MONEY;
+			}
+			if (matchCount == 5) {
+				fiveMatchCount += 1;
+				totalPrizeMoney += FIVE_MATCH_PRIZE_MONEY;
+			}
+			if (matchCount == 6) {
+				sixMatchCount += 1;
+				totalPrizeMoney += SIX_MATCH_PRIZE_MONEY;
+			}
+		}
+
+		earningsRate = (double) totalPrizeMoney / (lottoMatchCountResult.size() * lottoPricePerSheet);
+	}
+
+}
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,4 +1,24 @@
 package lotto.view;
 
 public class ResultView {
+	public ResultView() {
+	}
+
+	public void printMatchUI(int matchCount, int prizeMoney, int threeMatchCount) {
+		System.out.println(String.format("%d개 일치 (%d원)- %d",matchCount, prizeMoney, threeMatchCount));
+	}
+
+	public void printEarningRateUI(double earningRate) {
+		if (earningRate < 1){
+			System.out.println(String.format("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)", earningRate));
+		}
+		if(earningRate == 1){
+			System.out.println(String.format("총 수익률은 %.2f입니다.(본전)", earningRate));
+		}
+		if(earningRate > 1){
+			System.out.println(String.format("총 수익률은 %.2f입니다.(수익이 생겼습니다)", earningRate));
+		}
+
+
+	}
 }
