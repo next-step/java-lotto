6e70e4efe92b214fadaf8f6a0255ca21bb6c4269
Yeoseon Yun <devyyskr@gmail.com>
2020-12-15 21:58:53 +0900

    feat(LottoMachine): 구매금액 입력받아 살 수 있는 로또 개수 구하는 기능 구현

---
+++ src/main/java/step2/LottoMachine.java
@@ -0,0 +1,11 @@
+package step2;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+
+    private LottoMachine() {}
+
+    public static int calculateLottoCount(int inputMoney) {
+        return inputMoney / LOTTO_PRICE;
+    }
+}
