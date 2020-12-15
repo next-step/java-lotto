6764c12498ae553da7bfd94ddabcfc14a18d0014
Yeoseon Yun <devyyskr@gmail.com>
2020-12-15 22:43:01 +0900

    refactor(Lotto): 변수명 리팩토링

--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -3,9 +3,7 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
