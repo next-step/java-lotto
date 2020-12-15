f77dec9faab1074b07d4adf4730f846530362242
tearight <tearightss@gmail.com>
2020-12-15 20:19:53 +0900

    step1 문자열 계산기 (#1087)
    * Revert "study: java-lotto"
    
    This reverts commit f1f4a8b9
    
    * docs(requirements): 요구사항 정리
    
    * feat(StringCalculator): 더하기 연산시 빈 문자열, null 은 0 을 반환
    
    * refactor(StringCalculator): 0을 리턴하는 조건 리팩토링
    
    * feat(StringCalculator): (,)구분 숫자합 기능 구현
    
    - 숫자 하나 덧셈 기능 구현
    - 숫자 둘 이상 덧셈 기능 구현
    
    * feat(StringCalculator): 음수 계산시 RuntimeException 예외 발생 구현
    
    * feat(StringCalculator): 숫자 구분자 추가(':')
    
    * feat(StringCalculator): 커스텀 구분자 기능 추가

---
+++ src/main/java/string_adder/StringCalculator.java
@@ -0,0 +1,48 @@
+package string_adder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+class StringCalculator {
+
+	private static final String TEXT_EMPTY = "";
+	private static final String DELIMITER_FOR_SPLIT = ":|,";
+	private static final String REGEX_CUSTOM_SEPARATOR = "//(.*)\n(.*)";
+
+	int sum(String text) {
+		if (isZeroReturnTarget(text)) {
+			return 0;
+		}
+
+		String[] numbers = getNumbersBySplit(text);
+ 		return sum(numbers);
+	}
+
+	private String[] getNumbersBySplit(String text) {
+		Matcher m = Pattern.compile(REGEX_CUSTOM_SEPARATOR).matcher(text);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			return m.group(2).split(customDelimiter);
+		}
+		return text.split(DELIMITER_FOR_SPLIT);
+	}
+
+	private boolean isZeroReturnTarget(String text) {
+		return text == null || TEXT_EMPTY.equals(text);
+	}
+
+	private int sum(String[] numbers) {
+		return Stream.of(numbers)
+				.mapToInt(Integer::parseInt)
+				.peek(this::validateNotNegative)
+				.sum();
+	}
+
+	private void validateNotNegative(int number) {
+		if (number < 0) {
+			String msg = String.format("%s is negative !", number);
+			throw new RuntimeException(msg);
+		}
+	}
+}
---
+++ src/test/java/string_adder/StringCalculatorTest.java
@@ -0,0 +1,62 @@
+package string_adder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@SuppressWarnings("NonAsciiCharacters")
+class StringCalculatorTest {
+
+	private StringCalculator stringCalculator;
+
+	@BeforeEach
+	void setUp() {
+		stringCalculator = new StringCalculator();
+	}
+
+	@Test
+	void sum_null() {
+		assertThat(stringCalculator.sum(null)).isEqualTo(0);
+	}
+
+	@Test
+	void sum_빈문자열() {
+		assertThat(stringCalculator.sum("")).isEqualTo(0);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0=0", "1=1", "999=999"}, delimiter = '=')
+	void sum_숫자한개(String number, int expected) {
+		assertThat(stringCalculator.sum(number)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0,1=1", "2:999=1001"}, delimiter = '=')
+	void sum_숫자두개(String twoNumbers, int expected) {
+		assertThat(stringCalculator.sum(twoNumbers)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0,1:2=3", "2:1:0,5,1000=1008"}, delimiter = '=')
+	void sum_숫자여러개(String multipleNumbers, int expected) {
+		assertThat(stringCalculator.sum(multipleNumbers)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"5,-1", "0,-1:-5"})
+	void sum_음수_예외발생(String numbers) {
+		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> stringCalculator.sum(numbers));
+	}
+
+	@Test
+	void sum_커스텀구분자() {
+		assertThat(stringCalculator.sum("//\\+\n1+5")).isEqualTo(6);
+		assertThat(stringCalculator.sum("//;;;\n1;;;5")).isEqualTo(6);
+		assertThat(stringCalculator.sum("//ab\n5ab10")).isEqualTo(15);
+	}
+}
