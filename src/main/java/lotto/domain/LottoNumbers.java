986a85e534ce38ef978b6d76bd59fc89d9889734
wehwan <we_hwan@naver.com>
2020-12-15 10:24:21 +0900

    feat: 원하는 숫자만 세팅 가능하도록 LottoNumbers 생성자 추가

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -7,11 +7,18 @@ import java.util.stream.Collectors;
 
 public class LottoNumbers {
 
-    private static final int LOTTO_NUMBERS_SIZE = 46;
+    private static final int LOTTO_NUMBERS_DEFAULT_SIZE = 46;
+    private static final int DEFAULT_PICK_NUMBER_SIZE = 6;
+
     private final List<LottoNumber> lottoNumbers;
 
     public LottoNumbers() {
         this.lottoNumbers = this.createLottoNumbers();
+    }
+
+    public LottoNumbers(List<Integer> numbers) {
+        this.lottoNumbers = numbers.stream().map(LottoNumber::new)
+                                            .collect(Collectors.toList());
     }
 
     /**
@@ -19,11 +26,19 @@ public class LottoNumbers {
      * @return
      */
     private List<LottoNumber> createLottoNumbers() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>(LottoNumbers.LOTTO_NUMBERS_SIZE);
-        for (int i = 1; i <= LottoNumbers.LOTTO_NUMBERS_SIZE; i++) {
+        List<LottoNumber> lottoNumbers = new ArrayList<>(LottoNumbers.LOTTO_NUMBERS_DEFAULT_SIZE);
+        for (int i = 1; i <= LottoNumbers.LOTTO_NUMBERS_DEFAULT_SIZE; i++) {
             lottoNumbers.add(new LottoNumber(i));
         }
         return lottoNumbers;
+    }
+
+    /**
+     * 기본값으로 설정된 크기만큼의 로또 번호를 뽑습니다.
+     * @return
+     */
+    public List<Integer> pickLottoNumber() {
+        return this.pickLottoNumber(DEFAULT_PICK_NUMBER_SIZE);
     }
 
     /**
@@ -32,11 +47,23 @@ public class LottoNumbers {
      * @return
      */
     public List<Integer> pickLottoNumber(int amount) {
+        if(!this.canPickLottoNumberSize(amount)) {
+            throw new IllegalArgumentException("뽑으려는 번호의 갯수가 미리 세팅된 로또 번호의 크기보다 큽니다.");
+        }
         this.shuffleLottoNumbers();
         return this.lottoNumbers.stream().limit(amount)
                                         .sorted()
                                         .map(LottoNumber::getLottoNumber)
                                         .collect(Collectors.toList());
+    }
+
+    /**
+     * 주어진 수가 미리 세팅 된 로또 번호들의 크기보다 크거나 같아야합니다.
+     * @param amount
+     * @return
+     */
+    private boolean canPickLottoNumberSize(int amount) {
+        return amount <= this.lottoNumbers.size();
     }
 
     /**
--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -3,21 +3,23 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LottoNumbersTest {
 
     @Test
-    @DisplayName("랜덤으로 뽑은 숫자들이 6개이고, 1 ~ 46사이에 있는지 테스트")
-    public void pickLottoNumber() {
+    @DisplayName("46개중에서 랜덤으로 뽑은 숫자들이 6개이고, 1 ~ 46사이에 있는지 테스트")
+    public void pickLottoNumberRandom() {
         // given
         LottoNumbers lottoNumbers = new LottoNumbers();
 
         // when
-        List<Integer> pickLottoNumbers = lottoNumbers.pickLottoNumber(6);
+        List<Integer> pickLottoNumbers = lottoNumbers.pickLottoNumber();
 
         // then
         assertThat(pickLottoNumbers.size()).isEqualTo(6);
@@ -26,5 +28,39 @@ public class LottoNumbersTest {
             assertThat(number).isGreaterThanOrEqualTo(1)
                     .isLessThanOrEqualTo(46);
         });
+    }
+
+    @Test
+    @DisplayName("고정으로 세팅한 숫자들을 6개 뽑았을때, 리스트에 해당 번호가 있는지 테스트")
+    public void pickLottoNumberFixed() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 10, 20, 30, 40, 46);
+        LottoNumbers lottoNumbers = new LottoNumbers(numbers);
+
+        // when
+        List<Integer> pickLottoNumbers = lottoNumbers.pickLottoNumber();
+
+        // then
+        pickLottoNumbers.stream().forEach(number -> {
+            assertThat(number).isIn(numbers);
+        });
+    }
+
+
+    @Test
+    @DisplayName("고정으로 세팅한 숫자들보다 더 큰 크기의 수를 뽑으려고 할 때 오류 발생 테스트")
+    public void pickLottoNumberFixedOccurredException() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 10, 20, 30, 40, 46);
+        LottoNumbers lottoNumbers = new LottoNumbers(numbers);
+
+        // when
+        Throwable throwable = catchThrowable(() -> {
+            lottoNumbers.pickLottoNumber(7);
+        });
+
+        // then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage("뽑으려는 번호의 갯수가 미리 세팅된 로또 번호의 크기보다 큽니다.");
     }
 }
