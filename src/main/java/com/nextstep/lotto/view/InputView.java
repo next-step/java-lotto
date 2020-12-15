6db77b3a09705b6fc8b79dbade5bf2c162af0d72
lazy-moon <magyeon@naver.com>
2020-12-15 14:22:13 +0900

    feature: 입력 중 예외 발생 시 프로그램 종료 안하고 다시 입력받는 기능 추가

--- src/main/java/com/nextstep/lotto/view/InputView.java
+++ src/main/java/com/nextstep/lotto/view/InputView.java
@@ -1,10 +1,12 @@
 package com.nextstep.lotto.view;
 
 import com.nextstep.lotto.domain.Lotto;
+import com.nextstep.lotto.domain.LottoSeller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,25 +14,27 @@ public class InputView {
     private static final String DELIMITER = ",";
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static int inputManualCount() {
+    public static int inputManualCount(int totalCount) {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        return parseInt(SCANNER.nextLine());
+        int manualCount = parseInt(SCANNER.nextLine());
+        if (totalCount < manualCount) {
+            throw new IllegalArgumentException("현재 금액으로는 최대 " + totalCount + "장 구매 가능합니다.");
+        }
+        return manualCount;
     }
 
-    public static List<Lotto> inputManualLottos(int count) {
-        List<Lotto> lottos = new ArrayList<>();
+    public static Lotto inputManualLottos() {
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
-
-        for ( int i = 0 ; i < count ; i ++ ) {
-            Lotto lotto = new Lotto(parseToList(SCANNER.nextLine()));
-            lottos.add(lotto);
-        }
-        return lottos;
+        return new Lotto(parseToList(SCANNER.nextLine()));
     }
 
     public static int inputPrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        return parseInt(SCANNER.nextLine());
+        int price = parseInt(SCANNER.nextLine());
+        if (price < LottoSeller.LOTTO_PRICE) {
+            throw new IllegalArgumentException("최소 구입 금액은 " + LottoSeller.LOTTO_PRICE + "원 입니다.");
+        }
+        return price;
     }
 
     public static List<Integer> inputWinnerNumbers() {
@@ -40,8 +44,11 @@ public class InputView {
 
     private static List<Integer> parseToList(String winningNumberString) {
         String[] numbers = winningNumberString.split(DELIMITER);
+        if ( numbers.length != Lotto.LOTTO_NUMBER_COUNT ) {
+            throw new IllegalArgumentException("숫자는 " + Lotto.LOTTO_NUMBER_COUNT + "개만 입력 가능합니다.");
+        }
         return Stream.of(numbers)
-                .map(Integer::parseInt)
+                .map(InputView::parseInt)
                 .collect(Collectors.toList());
     }
 
@@ -53,6 +60,10 @@ public class InputView {
     }
 
     private static int parseInt(String line) {
-        return Integer.parseInt(line);
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException(line + "은 숫자가 아닙니다.");
+        }
     }
 }
--- src/main/java/com/nextstep/lotto/view/LottoMain.java
+++ src/main/java/com/nextstep/lotto/view/LottoMain.java
@@ -4,7 +4,10 @@ import com.nextstep.lotto.domain.Lotto;
 import com.nextstep.lotto.domain.LottoSeller;
 import com.nextstep.lotto.domain.Lottos;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class LottoMain {
     public static void main(String[] args) {
@@ -17,20 +20,47 @@ public class LottoMain {
     }
 
     private static Lottos buy() {
-        int price = InputView.inputPrice();
+        int price = inputWithoutException(InputView::inputPrice);
         int totalCount = LottoSeller.count(price);
-        int manualCount = InputView.inputManualCount();
+        int manualCount = inputWithoutException(() -> InputView.inputManualCount(totalCount));
         int autoCount = totalCount - manualCount;
 
-        List<Lotto> manualLottos = InputView.inputManualLottos(manualCount);
+        List<Lotto> manualLottos = inputManualLottos(manualCount);
         Lottos lottos = LottoSeller.buy(manualLottos, autoCount);
         ResultView.printBuy(manualCount, autoCount, lottos);
         return lottos;
     }
 
+    private static List<Lotto> inputManualLottos(int manualCount) {
+        List<Lotto> manualLottos = new ArrayList<>();
+        for ( int i = 0 ; i < manualCount ; i ++ ) {
+            Lotto manualLotto = inputWithoutException(InputView::inputManualLottos);
+            manualLottos.add(manualLotto);
+        }
+        return manualLottos;
+    }
+
     private static void draw(Lottos lottos) {
-        List<Integer> winnerNumbers = InputView.inputWinnerNumbers();
-        int bonusNumber = InputView.inputBonusNumber();
+        List<Integer> winnerNumbers = inputWithoutException(InputView::inputWinnerNumbers);
+        int bonusNumber = inputWithoutException(InputView::inputBonusNumber);
         ResultView.printStatistics(lottos.getStatistics(winnerNumbers, bonusNumber));
+    }
+
+    public static <T> T inputWithoutException(Supplier<T> supplier) {
+        Optional<T> result;
+        do {
+            result = handleException(supplier);
+        } while(!result.isPresent());
+        return result.get();
+    }
+
+    private static <T> Optional<T> handleException(Supplier<T> supplier) {
+        Optional<T> result = Optional.empty();
+        try {
+            result = Optional.of(supplier.get());
+        } catch ( Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 }
