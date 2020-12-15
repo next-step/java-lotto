56738b18bd68f15df50170b970f14ce995c3b1f1
shk3029 <shk30290@naver.com>
2020-12-16 00:59:56 +0900

    refactor: 추가된 기능 view 수정

--- src/main/java/step3/view/OutputView.java
+++ src/main/java/step3/view/OutputView.java
@@ -1,35 +1,44 @@
 package step3.view;
 
 import step3.domain.Lotto;
-import step3.domain.LottoNumbers;
+import step3.domain.Lottos;
 import step3.domain.LottoResult;
+import step3.domain.Rank;
+
+import java.util.EnumMap;
+import java.util.Set;
 
 public class OutputView {
 
-    public static void outputMessageLottoNumbers(LottoNumbers lottoNumbers) {
+    public static void outputMessageLottoNumbers(Lottos lottos) {
         StringBuilder builder = new StringBuilder()
-                .append(lottoNumbers.getLottoNumber().size())
+                .append(lottos.getLottoNumber().size())
                 .append("개를 구매했습니다.");
         System.out.println(builder.toString());
 
-        for (Lotto number : lottoNumbers.getLottoNumber()) {
+        for (Lotto number : lottos.getLottoNumber()) {
             System.out.println(number);
         }
     }
 
     public static void outputMessageLottoResult(int amount, LottoResult lottoResult) {
+        EnumMap<Rank, Integer> rankMap = lottoResult.winningResult();
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("--------");
-        System.out.println("3개 일치 (5000원)-" + lottoResult.winningResult(3) + "개");
-        System.out.println("4개 일치 (50000원)-" + lottoResult.winningResult(4) + "개");
-        System.out.println("5개 일치 (1500000원)-" + lottoResult.winningResult(5) + "개");
-        System.out.println("6개 일치 (2000000000원)-" + lottoResult.winningResult(6) + "개");
-
-        outputMessageLottoYield(amount,lottoResult.winnings());
+        System.out.println("3개 일치 (5000원)-" + rankMap.getOrDefault(Rank.FIFTH, 0) + "개");
+        System.out.println("4개 일치 (50000원)-" + rankMap.getOrDefault(Rank.FOURTH, 0) + "개");
+        System.out.println("5개 일치 (1500000원)-" + rankMap.getOrDefault(Rank.THIRD, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30000000원)-" + rankMap.getOrDefault(Rank.SECOND, 0) + "개");
+        System.out.println("6개 일치 (2000000000원)-" + rankMap.getOrDefault(Rank.FIRST,0) + "개");
+        outputMessageLottoYield(amount,rankMap);
     }
 
-    public static void outputMessageLottoYield(int amount, long sum) {
+    public static void outputMessageLottoYield(int amount, EnumMap<Rank, Integer> rankMap) {
+        int sum = 0;
+        for (Rank rank : rankMap.keySet()) {
+            sum += rankMap.get(rank) * rank.getWinningMoney();
+        }
         double yield = (double)sum/(double)amount;
         System.out.print("총 수익률은 ");
         System.out.print(String.format("%.2f", yield));
