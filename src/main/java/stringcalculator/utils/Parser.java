9384248e2c8567946ee9b537bb08bda4424952e7
chh <chh2644@gmail.com>
2020-12-16 23:45:02 +0900

    feat : Parser 객체 validate 변경 구현

--- src/main/java/stringcalculator/utils/Parser.java
+++ src/main/java/stringcalculator/utils/Parser.java
@@ -3,6 +3,22 @@ package stringcalculator.utils;
 public class Parser {
 
     public static int parse(String textNumber) {
-        return Integer.parseInt(textNumber);
+        validate(textNumber);
+
+        int numeric;
+
+        try {
+            numeric = Integer.parseInt(textNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자만 입력이 가능합니다.");
+        }
+
+        return numeric;
+    }
+
+    public static void validate(String textNumber) {
+        if(textNumber == null || "".equals(textNumber)) {
+            throw new NumberFormatException("숫자를 입력해주세요.");
+        }
     }
 }
