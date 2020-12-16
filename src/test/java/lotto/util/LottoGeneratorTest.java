4a974a0801cbc85ea9b64dbd0b0b27f51a4f3b86
orgojy <giveawesome@gmail.com>
2020-12-16 22:37:24 +0900

    feat(LottoGenerator): 로또를 생성하기 위한 유틸 `LottoGenerator` 구현

---
+++ src/test/java/lotto/util/LottoGeneratorTest.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+import lotto.domain.Numbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("로또를 생성하기 위한 유틸 `LottoGenerator`에 대한 테스트")
+class LottoGeneratorTest {
+
+    @DisplayName("`Lotto` 생성을 위한 무작위 번호 6자리 생성")
+    @Test
+    void generateRandomNumbersTest() {
+        // When
+        List<Integer> randomNumbers = LottoGenerator.generateRandomNumbers();
+        // Then
+        assertThat(randomNumbers.size()).isEqualTo(Numbers.TOTAL_NUMBER_SIZE);
+    }
+}
