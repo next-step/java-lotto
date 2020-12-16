f1bc9b87b6d7eeeb8c7f4be4627b68a60b8fa396
charlloss.dev <charlloss.dev@gmail.com>
2020-12-16 22:19:18 +0900

    test: add 로또 금액(money) 객체 생성 테스트

---
+++ src/main/java/lotto/modal/Money.java
@@ -0,0 +1,52 @@
+package lotto.modal;
+
+import java.util.Objects;
+
+import lotto.util.StringValid;
+
+public class Money {
+
+	public static final int LOTTO_MINIMUM_MONEY = 1000;
+
+	private int userMoney;
+
+	private Money() {
+		throw new AssertionError();
+	}
+
+	public Money(String userInputMoney) {
+
+		validationMoney(userInputMoney);
+		this.userMoney = Integer.parseInt(userInputMoney);
+	}
+
+	private void validationMoney(String userInputMoney) throws IllegalArgumentException {
+
+		if (StringValid.isEmptyStr(userInputMoney)) {
+			throw new IllegalArgumentException("돈(금액)이 입력되지 않았습니다.");
+		}
+
+		if (StringValid.isNoNNumber(userInputMoney)) {
+			throw new IllegalArgumentException("돈(금액) 형식을 잘못 입력하였습니다. 숫자만 입력해 주세요.");
+		}
+
+		if (Integer.parseInt(userInputMoney) < LOTTO_MINIMUM_MONEY) {
+			throw new IllegalArgumentException("로또 구매 최고 금액은 " + LOTTO_MINIMUM_MONEY + "원 입니다.");
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Money money = (Money)o;
+		return userMoney == money.userMoney;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userMoney);
+	}
+}
