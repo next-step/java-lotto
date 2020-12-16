f9d3b25ac9e5b78851f79d2fc51782b8d3f145f0
jaeseon_hwang <jshwang@deleo.co.kr>
2020-12-17 00:19:49 +0900

    feat: 예외 처리를 통해 에러가 발생하지 않도록 한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,12 @@ package lotto.view;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputView {
+    private static final Pattern pattern = Pattern.compile("[0-9]+");
     private static final Scanner sc = new Scanner(System.in);
 
     private InputView() {
@@ -14,7 +16,7 @@ public class InputView {
 
     public static int money() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(sc.nextLine());
+        return toIntValue();
     }
 
     public static String lastWinningNumbers() {
@@ -29,7 +31,7 @@ public class InputView {
 
     public static List<String> manualNumbers() {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        int manualCount = Integer.parseInt(sc.nextLine());
+        int manualCount = toIntValue();
 
         if (manualCount == 0) {
             return Collections.emptyList();
@@ -39,5 +41,14 @@ public class InputView {
         return Stream.generate(sc::nextLine)
               .limit(manualCount)
               .collect(Collectors.toList());
+    }
+
+    private static int toIntValue() {
+        String value = sc.nextLine();
+        if (pattern.matcher(value).matches()) {
+            return Integer.parseInt(value);
+        }
+
+        throw new IllegalArgumentException("숫자를 입력해주세요");
     }
 }
