4863f142448d5a88eae8561f831216b64c14eed1
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-16 11:32:57 +0900

    refactor(LotteryShop): refactor buyAutomatically method to use stream

--- src/main/java/com/jaenyeong/mission02/lotto/domain/LotteryShop.java
+++ src/main/java/com/jaenyeong/mission02/lotto/domain/LotteryShop.java
@@ -2,26 +2,24 @@ package com.jaenyeong.mission02.lotto.domain;
 
 import com.jaenyeong.mission02.lotto.domain.lottery.LotteryGame;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class LotteryShop {
     private LotteryShop() {
     }
 
     public static List<LotteryGame> buyAutomatically(final int howManyToBuy) {
-        final List<LotteryGame> games = new ArrayList<>();
-        for (int i = 0; i < howManyToBuy; i++) {
-            games.add(LotteryGame.ofAuto());
-        }
-
-        return games;
+        return Stream.generate(LotteryGame::ofAuto)
+            .limit(howManyToBuy)
+            .collect(toList());
     }
 
     public static List<LotteryGame> buyManually(final List<List<Integer>> givenNumbers) {
         return givenNumbers.stream()
             .map(LotteryGame::ofManual)
-            .collect(Collectors.toList());
+            .collect(toList());
     }
 }
