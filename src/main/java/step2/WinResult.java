cdf1db6087a788e4f19d2073e35a41bef6228ee6
eversong0723 <eversong0723@gmail.com>
2020-12-16 19:53:06 +0900

    feat: WinResult 누락된 부분 커밋

---
+++ src/main/java/step2/WinResult.java
@@ -0,0 +1,37 @@
+package step2;
+
+import java.util.Arrays;
+
+public enum WinResult {
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    MISS(0, 0);
+
+    private int countOfMatch;
+    private int winningMoney;
+
+    private WinResult(int countOfMatch, int winningMoney) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+
+    public static WinResult valueOf(MatchCount matchCount) {
+        if (matchCount.getValue() < FOURTH.countOfMatch) {
+            return MISS;
+        }
+        return Arrays.asList(values()).stream()
+                .filter(item -> matchCount.isSame(item.countOfMatch))
+                .findFirst()
+                .get();
+    }
+}
