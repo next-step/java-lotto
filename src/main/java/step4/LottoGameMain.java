6ee7d4c16da60684b2b35d0d507121ebc7a65f53
shk3029 <shk30290@naver.com>
2020-12-17 02:31:01 +0900

    feat: main 클래스 수동 기능 추가

--- src/main/java/step4/LottoGameMain.java
+++ src/main/java/step4/LottoGameMain.java
@@ -1,8 +1,10 @@
 package step4;
 
+import step4.domain.LottoCount;
 import step4.domain.LottoGame;
 import step4.domain.LottoResult;
 import step4.domain.generator.LottoAutoGenerator;
+import step4.domain.generator.LottoManualGenerator;
 
 import java.math.BigDecimal;
 
@@ -14,8 +16,20 @@ public class LottoGameMain {
 
     public static void main(String[] args) {
         int amount = inputMessageLottoAmount();
-        LottoGame lottoGame = new LottoGame(BigDecimal.valueOf(amount), new LottoAutoGenerator());
-        outputMessageLottoNumbers(lottoGame.getLottoNumbers());
+        int manualCount = inputMessageLottoManualCount();
+
+        LottoGame lottoGame = new LottoGame(BigDecimal.valueOf(amount), manualCount);
+        LottoCount lottoCount = lottoGame.getLottoCount();
+
+        for (int i=0; i<lottoCount.getManualCount(); i++) {
+            lottoGame.buyLotto(new LottoManualGenerator(), inputLottoManual());
+        }
+
+        for (int i=0; i<lottoCount.getAutoCount(); i++) {
+            lottoGame.buyLotto(new LottoAutoGenerator());
+        }
+
+       outputMessageLottoNumbers(lottoGame);
 
         LottoResult lottoResult = lottoGame.lottoResultMap(inputMessageLastWeekWinningNumber(), inputMessageBonusNumber());
         outputMessageLottoResult(amount, lottoResult);
