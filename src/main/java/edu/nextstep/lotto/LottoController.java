16e6dede38f3d9db8b4bce7ee1383e40fe8671a2
Donghwan Noh <namul.bap@gmail.com>
2020-12-17 01:06:09 +0900

    feat(view): 구매한 로또 번호를 모두 화면에 출력함
    LottoController에서 랜덤숫자를 생성하는 Util을 사용
    - getRandomLottoNumbers 메소드: NumberUtil의 generateSortedRandomNumber를 사용해 로또번호 생성
    - getRandomLottoNumbersList 메소드: 파라미터로 받은 size만큼의 로또 번호를 생성하여 List로 리턴
    
    ResultView에 출력 메소드 구현

--- src/main/java/edu/nextstep/lotto/LottoController.java
+++ src/main/java/edu/nextstep/lotto/LottoController.java
@@ -1,12 +1,32 @@
 package edu.nextstep.lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.nextstep.lotto.domain.LottoNumber;
+import edu.nextstep.lotto.domain.LottoNumbers;
 import edu.nextstep.lotto.domain.Money;
+import edu.nextstep.lotto.util.NumberUtil;
 import edu.nextstep.lotto.view.InputView;
 import edu.nextstep.lotto.view.ResultView;
 
 public class LottoController {
 	public void play() {
 		Money money = new Money(InputView.inputMoney());
-		ResultView.printHowManyPurchase(money.howManyPurchase());
+		int numberOfPurchase = money.howManyPurchase();
+		ResultView.printHowManyPurchase(numberOfPurchase);
+		ResultView.printLottos(getRandomLottoNumbersList(numberOfPurchase));
+	}
+
+	protected List<List<Integer>> getRandomLottoNumbersList(int size) {
+		List<List<Integer>> lottoNumbers = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			lottoNumbers.add(getRandomLottoNumbers());
+		}
+		return lottoNumbers;
+	}
+
+	private List<Integer> getRandomLottoNumbers() {
+		return NumberUtil.generateSortedRandomNumber(LottoNumber.START_LIMIT, LottoNumber.END_LIMIT, LottoNumbers.SIZE);
 	}
 }
--- src/main/java/edu/nextstep/lotto/view/ResultView.java
+++ src/main/java/edu/nextstep/lotto/view/ResultView.java
@@ -1,5 +1,7 @@
 package edu.nextstep.lotto.view;
 
+import java.util.List;
+
 public class ResultView {
 
 	private ResultView() {
@@ -7,5 +9,10 @@ public class ResultView {
 
 	public static void printHowManyPurchase(int number) {
 		System.out.println(number + "개를 구매했습니다.");
+	}
+
+	public static void printLottos(List<List<Integer>> lottos) {
+		lottos.forEach(System.out::println);
+		System.out.println();
 	}
 }
