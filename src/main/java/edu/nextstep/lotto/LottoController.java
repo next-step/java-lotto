8f5c99442908c716db33ccc5bd898c5830deaa79
Donghwan Noh <namul.bap@gmail.com>
2020-12-17 00:25:00 +0900

    feat(view): 구매한 로또의 개수를 화면에 출력한다
    ResultView 클래스 생성
    - printHowManyPurchase메소드를 통해 구매 개수 출력
    
    BREAKING CHANGE:
    - LottoController:printHowManyPurchase메소드를 사용해 구매개수 출력

--- src/main/java/edu/nextstep/lotto/LottoController.java
+++ src/main/java/edu/nextstep/lotto/LottoController.java
@@ -1,9 +1,12 @@
 package edu.nextstep.lotto;
 
+import edu.nextstep.lotto.domain.Money;
 import edu.nextstep.lotto.view.InputView;
+import edu.nextstep.lotto.view.ResultView;
 
 public class LottoController {
 	public void play() {
-		int money = InputView.inputMoney();
+		Money money = new Money(InputView.inputMoney());
+		ResultView.printHowManyPurchase(money.howManyPurchase());
 	}
 }
---
+++ src/main/java/edu/nextstep/lotto/view/ResultView.java
@@ -0,0 +1,11 @@
+package edu.nextstep.lotto.view;
+
+public class ResultView {
+
+	private ResultView() {
+	}
+
+	public static void printHowManyPurchase(int number) {
+		System.out.println(number + "개를 구매했습니다.");
+	}
+}
