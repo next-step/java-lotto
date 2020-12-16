038ebb31ed930d256803718a17758a3879ab62bd
jin-jaylin <jin87.an@gmail.com>
2020-12-16 21:09:08 +0900

    feat: 빈값 체크 추가

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -14,26 +14,35 @@ public class InputView {
   private static final String BONUS_NUMBER_STR = "보너스 볼을 입력해 주세요.";
   private static final String MANUAL_LOTTO_COUNT_STR = "수동으로 구매할 로또 수를 입력해 주세요.";
   private static final String MANUAL_LOTTO_NUMBER_STR = "수동으로 구매할 번호를 입력해 주세요.";
+  private static final String EMPTY_VALUE_ERROR_MESSAGE = "입력값이 비었습니다.";
 
 
   public static int getLottoPrice() {
     System.out.println(BUYING_AMOUNT_STR);
-    return Integer.parseInt(scanner.nextLine());
+    String input = scanner.nextLine();
+    emptyOrNullCheck(input);
+    return Integer.parseInt(input);
   }
 
   public static String getWinningNumbers() {
     System.out.println(WINNING_NUMBER_STR);
-    return scanner.nextLine();
+    String input = scanner.nextLine();
+    emptyOrNullCheck(input);
+    return input;
   }
 
   public static String getBonusNumber() {
     System.out.println(BONUS_NUMBER_STR);
-    return scanner.nextLine();
+    String input = scanner.nextLine();
+    emptyOrNullCheck(input);
+    return input;
   }
 
   public static int getManualLottoCount() {
     System.out.println(MANUAL_LOTTO_COUNT_STR);
-    return Integer.parseInt(scanner.nextLine());
+    String input = scanner.nextLine();
+    emptyOrNullCheck(input);
+    return Integer.parseInt(input);
   }
 
   public static List<List<Integer>> getManualLottoNumber(int count) {
@@ -41,8 +50,15 @@ public class InputView {
     System.out.println(MANUAL_LOTTO_NUMBER_STR);
     for (int i = 1; i <= count; i++) {
       String numbers = scanner.nextLine();
+      emptyOrNullCheck(numbers);
       manualList.add(LottoGenerator.getLottoNumberListFromString(numbers));
     }
     return manualList;
+  }
+
+  private static void emptyOrNullCheck(String input) {
+    if (input == null || "".equals(input)) {
+      throw new IllegalArgumentException(EMPTY_VALUE_ERROR_MESSAGE);
+    }
   }
 }
