1c5d6425059c63877066cffff98a315dc6f75fa3
orgojy <giveawesome@gmail.com>
2020-12-17 00:47:27 +0900

    feat(Lotto): 로또 추첨에 필요한 단위 `Lotto` 구현

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("로또 추첨에 필요한 단위 `Lotto`에 대한 테스트")
+class LottoTest {
+
+    @DisplayName("랜덤 `Lotto` 생성")
+    @Test
+    void createRandomLottoTest() {
+        // When
+        Lotto randomLotto = new Lotto();
+        // Then
+        assertThat(randomLotto).isNotNull();
+    }
+
+    @DisplayName("커스텀 `Lotto` 생성")
+    @Test
+    void createCustomLottoTest() {
+        // Given
+        Numbers numbers = new Numbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+        // When
+        Lotto customLotto = new Lotto(numbers);
+        // Then
+        assertThat(customLotto).isNotNull();
+    }
+}
