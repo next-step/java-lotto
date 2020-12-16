98ec65f3611404c5e6fe0c7ffd21d11f6743392c
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-16 14:50:42 +0900

    fix : 코멘트반영, 메소드 분리, 클래스 분리 적용

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -5,7 +5,7 @@ import lotto.view.InputView;
 
 public class Main {
 	public static void main(String[] args) {
-		Lotto lotto = new Lotto();
-		lotto.start();
+		LottoTVShow lottoTVShow = new LottoTVShow();
+		lottoTVShow.broadcast();
 	}
 }
