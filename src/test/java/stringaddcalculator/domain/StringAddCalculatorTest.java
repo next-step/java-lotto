c3ac113716a9e1a878e42d0b2bfc3c6791c3d8b9
byungkyu-ju <noel1227@gmail.com>
2020-12-16 10:16:32 +0900

    refactor: @NullAndEmptySource로 변경

--- src/test/java/stringaddcalculator/domain/StringAddCalculatorTest.java
+++ src/test/java/stringaddcalculator/domain/StringAddCalculatorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,9 +18,7 @@ import stringaddcalculator.domain.StringAddCalculator;
  **/
 public class StringAddCalculatorTest {
 	@ParameterizedTest
-	@NullSource
-	@EmptySource
-	@ValueSource(strings = {""})
+	@NullAndEmptySource
 	public void splitAndSum_null_또는_빈문자(String arg) {
 		int result = StringAddCalculator.splitAndSum(arg);
 		assertThat(result).isEqualTo(0);
