eb56ddcc858aa2d50cde7a32b5b2a35f53ef0286
Mingyu Lee <leemingyu05@gmail.com>
2020-12-17 20:34:35 +0900

    test: 쉼표 구분자 입력시 테스트

--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -23,13 +23,13 @@ public class StringAddCalculatorTest {
 		assertThat(result).isEqualTo(1);
 	}
 
-/*	@Test
+	@Test
 	public void splitAndSum_쉼표구분자() throws Exception {
 		int result = StringAddCalculator.splitAndSum("1,2");
 		assertThat(result).isEqualTo(3);
 	}
 
-	@Test
+/*	@Test
 	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
 		int result = StringAddCalculator.splitAndSum("1,2:3");
 		assertThat(result).isEqualTo(6);
