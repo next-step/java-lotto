54d5f9fc7ab74bb3003c14dd4789c467c3bcad72
tearight <tearightss@gmail.com>
2020-12-17 15:07:55 +0900

    refactor(StringCalculator): 문자열을 쪼개는 책임을 StringSplitter 에 전가

--- src/main/java/string_adder/StringCalculator.java
+++ src/main/java/string_adder/StringCalculator.java
@@ -1,35 +1,21 @@
 package string_adder;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 class StringCalculator {
 
-	private static final String TEXT_EMPTY = "";
 	private static final String DELIMITER_FOR_SPLIT = ":|,";
 	private static final String REGEX_CUSTOM_SEPARATOR = "//(.*)\n(.*)";
 
 	int sum(String text) {
-		if (isZeroReturnTarget(text)) {
+		String[] numbers;
+		try {
+			numbers = StringSplitter.splitBySeparator(text, REGEX_CUSTOM_SEPARATOR, DELIMITER_FOR_SPLIT);
+		} catch (IllegalArgumentException e) {
 			return 0;
 		}
 
-		String[] numbers = getNumbersBySplit(text);
  		return sum(numbers);
-	}
-
-	private String[] getNumbersBySplit(String text) {
-		Matcher m = Pattern.compile(REGEX_CUSTOM_SEPARATOR).matcher(text);
-		if (m.find()) {
-			String customDelimiter = m.group(1);
-			return m.group(2).split(customDelimiter);
-		}
-		return text.split(DELIMITER_FOR_SPLIT);
-	}
-
-	private boolean isZeroReturnTarget(String text) {
-		return text == null || TEXT_EMPTY.equals(text);
 	}
 
 	private int sum(String[] numbers) {
