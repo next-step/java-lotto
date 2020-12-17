3701a67242447357b95cf29465111e7c5413f8c8
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-17 15:08:56 +0900

    refactor(LotteryNumber): rename static lottery numbers pool const

--- src/main/java/com/jaenyeong/mission02/lotto/domain/lottery/LotteryNumber.java
+++ src/main/java/com/jaenyeong/mission02/lotto/domain/lottery/LotteryNumber.java
@@ -6,10 +6,10 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
 
 public class LotteryNumber {
-    private static final Map<Integer, LotteryNumber> lotteryNumberPool = new HashMap<>();
+    private static final Map<Integer, LotteryNumber> LOTTERY_NUMBER_POOL = new HashMap<>();
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = 45;
-    private static final List<LotteryNumber> LOTTERY_NUMBER_POOL = generateLotteryNumbers();
+    private static final List<LotteryNumber> LOTTERY_NUMBER_LIST = generateLotteryNumbers();
     private static final int START_OF_RANGE = 0;
     private static final String ERR_TEXT_INVALID_NUMBER = "[error] This number is not valid.";
 
@@ -26,20 +26,20 @@ public class LotteryNumber {
     }
 
     private static LotteryNumber getFlyweightLotteryNumber(final int givenNumber) {
-        final LotteryNumber lotteryNumber = lotteryNumberPool.get(givenNumber);
+        final LotteryNumber lotteryNumber = LOTTERY_NUMBER_POOL.get(givenNumber);
         if (lotteryNumber != null) {
             return lotteryNumber;
         }
 
         final LotteryNumber newLotteryNumber = new LotteryNumber(givenNumber);
-        lotteryNumberPool.put(givenNumber, newLotteryNumber);
+        LOTTERY_NUMBER_POOL.put(givenNumber, newLotteryNumber);
         return newLotteryNumber;
     }
 
     public static Set<LotteryNumber> ofAutoNumbers(final int endOfRange) {
-        Collections.shuffle(LOTTERY_NUMBER_POOL);
+        Collections.shuffle(LOTTERY_NUMBER_LIST);
         final Set<LotteryNumber> lotteryNumbers = new TreeSet<>(Comparator.comparingInt(LotteryNumber::getLotteryNumber));
-        lotteryNumbers.addAll(LOTTERY_NUMBER_POOL.subList(START_OF_RANGE, endOfRange));
+        lotteryNumbers.addAll(LOTTERY_NUMBER_LIST.subList(START_OF_RANGE, endOfRange));
 
         return lotteryNumbers;
     }
