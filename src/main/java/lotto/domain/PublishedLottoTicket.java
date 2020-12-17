b9a79506921243fb0c06f47fe7f7e3ec8ec46f6e
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-18 00:50:06 +0900

    (feat) 수동 로또 번호 발행 추가

--- src/main/java/lotto/domain/PublishedLottoTicket.java
+++ src/main/java/lotto/domain/PublishedLottoTicket.java
@@ -1,33 +1,56 @@
 package lotto.domain;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import lotto.util.StringSplitter;
+
+import java.util.*;
 
 public class PublishedLottoTicket {
 
     private final Set<LottoTicket> publishedLottoTicket;
 
-    private PublishedLottoTicket(int lottoTicketCount) {
+    private PublishedLottoTicket(int autoLottoTicketCount, List<String> manualLottoNumbers) {
         this.publishedLottoTicket = new HashSet<>();
-        publishLottoTicket(lottoTicketCount);
+        publishAutoLottoTicket(autoLottoTicketCount);
+        publishManualLottoTicket(manualLottoNumbers);
     }
 
-    public static PublishedLottoTicket ofLottoTicketCount(int lottoTicketCount) {
-        return new PublishedLottoTicket(lottoTicketCount);
-    }
-
-    private void publishLottoTicket(int lottoTicketCounts) {
-        LottoMachine lottoMachine = new LottoMachine();
+    public void publishAutoLottoTicket(int lottoTicketCounts) {
+        LottoMachine lottoMachine =  LottoMachine.instance();
         for (int i = 0; i < lottoTicketCounts; i++) {
-            LottoTicket lottoTicket = lottoMachine.generateLottoNumber();
-            this.publishedLottoTicket.add(lottoTicket);
+            LottoTicket autoLottoTicket = lottoMachine.generateAutoLottoNumber();
+            this.publishedLottoTicket.add(autoLottoTicket);
+        }
+    }
+
+    public void publishManualLottoTicket(List<String> manualLottoNumbers) {
+        LottoMachine lottoMachine =  LottoMachine.instance();
+        for (String lottoNumber : manualLottoNumbers) {
+            LottoTicket manualLottoTicket = lottoMachine.generateManualLottoNumbers(StringSplitter.splitText(lottoNumber));
+            this.publishedLottoTicket.add(manualLottoTicket);
         }
     }
 
     public Set<LottoTicket> getPublishedLottoTicket() {
         return Collections.unmodifiableSet(publishedLottoTicket);
+    }
+
+    public static class Builder {
+        int autoLottoTicketCount;
+        List<String> manualLottoNumbers;
+
+        public Builder autoLottoTicketCount(int autoLottoTicketCount) {
+            this.autoLottoTicketCount = autoLottoTicketCount;
+            return this;
+        }
+
+        public Builder manualLottoNumbers(List<String> manualLottoNumbers) {
+            this.manualLottoNumbers = manualLottoNumbers;
+            return this;
+        }
+
+        public PublishedLottoTicket build() {
+            return new PublishedLottoTicket(autoLottoTicketCount, manualLottoNumbers);
+        }
     }
 
     @Override
