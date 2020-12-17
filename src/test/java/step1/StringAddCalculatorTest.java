628c35026222c21a9a0dc5b001b199afbe957e72
Mingyu Lee <leemingyu05@gmail.com>
2020-12-17 20:35:03 +0900

    test: 커스텀 구분자 입력시 테스트

--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -35,13 +35,13 @@ public class StringAddCalculatorTest {
 		assertThat(result).isEqualTo(6);
 	}
 
-/*	@Test
+	@Test
 	public void splitAndSum_custom_구분자() throws Exception {
 		int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
 		assertThat(result).isEqualTo(6);
 	}
 
-	@Test
+/*	@Test
 	public void splitAndSum_negative() throws Exception {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
 			.isInstanceOf(RuntimeException.class);
