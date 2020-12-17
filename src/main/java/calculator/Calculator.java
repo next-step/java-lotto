95b62d5c2822e9a215aefba3ea55e6a88c124a2c
Do Yeon Kim <chikeem90@gmail.com>
2020-12-17 11:27:25 +0900

    refactor: Calculator 클래스 리팩토링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,34 +4,98 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
+	private static final int ZERO = 0;
+	private static final int ONE = 1;
+	private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+	private static final String SPLIT_PATTERN = ",|:";
+	private static final String NUMBER_PATTERN = "^[0-9]+$";
+	private static Matcher matcher;
+
 	public int calculate(String input) {
-		// null 또는 빈문자열
-		if (input == null || input.isEmpty()) {
-			return 0;
-		}
-		String[] numArray;
-		// 커스텀 구분자 체크
-		Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
-		if (matcher.find()) {
-			numArray = matcher.group(2).split(matcher.group(1));
-		} else {
-			// 숫자 하나 문자열
-			numArray = input.split(",|:");
-		}
-		// 음수 포함 시 Exception 발생
-		for (String num : numArray) {
-			if (num.startsWith("-")) {
-				throw new RuntimeException("음수는 입력이 불가합니다.");
-			}
+		if (isNullOrEmpty(input)) {
+			return ZERO;
 		}
 
-		if (numArray.length == 1) {
-			return Integer.parseInt(numArray[0]);
+		String[] numbers = getSplitNumber(input);
+
+		validateNumbers(numbers);
+
+		if (isOnlyOneNumber(numbers)) {
+			return Integer.parseInt(numbers[0]);
 		}
-		// 숫자 두개 콤마 문자열
+
+		return sum(numbers);
+	}
+
+	/**
+	 * 널 또는 빈문자열 체크
+	 */
+	private boolean isNullOrEmpty(String input) {
+		return input == null || input.isEmpty();
+	}
+
+	/**
+	 * 커스텀 구분자 포함여부 체크
+	 */
+	private boolean includeCustomDelimiter(String input) {
+		matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
+		return matcher.find();
+	}
+
+	/**
+	 * 커스텀 구분자로 split
+	 */
+	private String[] splitUsingCustomDelimiter() {
+		return matcher.group(2).split(matcher.group(1));
+	}
+
+	/**
+	 * 커스텀 구분자를 갖고 있는지 아닌지 판단 후 각 유형에 맞게 split 결과 return
+	 */
+	private String[] getSplitNumber(String input) {
+		String[] numbers = input.split(SPLIT_PATTERN);
+		if (includeCustomDelimiter(input)) {
+			numbers = splitUsingCustomDelimiter();
+		}
+		return numbers;
+	}
+
+	/**
+	 * 음수 또는 숫자 외 문자열 입력 체크
+	 */
+	private void validateNumber(String number) {
+		if (number.startsWith("-")) {
+			throw new RuntimeException("음수는 입력이 불가합니다.");
+		}
+
+		if (!number.matches(NUMBER_PATTERN)) {
+			throw new RuntimeException("숫자만 입력 가능합니다.");
+		}
+	}
+
+	/**
+	 * 음수 또는 숫자 외 문자열 입력 체크
+	 */
+	private void validateNumbers(String[] numbers) {
+		for (String number : numbers) {
+			validateNumber(number);
+		}
+	}
+
+	/**
+	 * 하나의 숫자만 입력한 경우 체크
+	 */
+	private boolean isOnlyOneNumber(String[] numbers) {
+		return numbers.length == ONE;
+	}
+
+	/**
+	 * 합계
+	 */
+	private int sum(String[] numbers) {
 		int result = 0;
-		for (String num : numArray) {
-			result += Integer.parseInt(num);
+		for (String number : numbers) {
+			result += Integer.parseInt(number);
 		}
 		return result;
 	}
