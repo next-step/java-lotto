d2ddd869b59d614ebcb38be33bcbbf2fa69e991d
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-18 01:05:58 +0900

    (refactor) LottoController 클래스 생성 및 메소드 분리

---
+++ src/main/java/lotto/LottoGameApp.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import lotto.controller.LottoController;
+
+public class LottoGameApp {
+
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.startLottoGame();
+    }
+}
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,51 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+
+    private LottoTicketCount lottoTicketCount;
+    private PublishedLottoTicket publishedLottoTicket;
+    private LottoResult lottoResult;
+
+    public void startLottoGame() {
+        LottoMoney lottoMoney = new LottoMoney(InputView.inputMoney());
+        buyLottoTicket(lottoMoney);
+
+        OutputView.printPublishedLottoResult(lottoTicketCount, publishedLottoTicket);
+
+        drawLottoResult();
+
+        OutputView.printLottoStatics(this.lottoResult, this.lottoResult.calculatePrizeRate(lottoMoney));
+    }
+
+    private void buyLottoTicket(LottoMoney lottoMoney) {
+        int manualLottoCount = InputView.inputManualLottoCount();
+
+        lottoTicketCount = new LottoTicketCount.Builder()
+                .lottoMoney(lottoMoney)
+                .manualLottoCount(manualLottoCount)
+                .build();
+
+        LottoShop lottoShop = new LottoShop();
+        List<String> manualLottoNumbers = InputView.inputManualLottoNumbers(manualLottoCount);
+        publishedLottoTicket = lottoShop.buyLotto(lottoTicketCount, manualLottoNumbers);
+    }
+
+    private void drawLottoResult() {
+        List<Integer> winningLottoNumbers = InputView.inputWinningLottoNumbers();
+        LottoMachine lottoMachine = LottoMachine.instance();
+        WinningLotto winningLotto = new WinningLotto.Builder()
+                .winningLottoTicket(lottoMachine.generateManualLottoNumbers(winningLottoNumbers))
+                .bonusBall(InputView.inputBonusBall())
+                .build();
+
+        lottoResult = LottoResult.getInstance();
+        lottoResult.analyzeLottoRank(publishedLottoTicket.getPublishedLottoTicket(), winningLotto);
+    }
+
+}
