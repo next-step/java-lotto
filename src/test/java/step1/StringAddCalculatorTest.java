42dbe9e90c0ccbd2904f803831bd6630fe816e65
Mingyu Lee <leemingyu05@gmail.com>
2020-12-17 20:34:48 +0900

    test: 쉼표 또는 콜론 구분자 입력시 테스트

--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -29,13 +29,13 @@ public class StringAddCalculatorTest {
 		assertThat(result).isEqualTo(3);
 	}
 
-/*	@Test
+	@Test
 	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
 		int result = StringAddCalculator.splitAndSum("1,2:3");
 		assertThat(result).isEqualTo(6);
 	}
 
-	@Test
+/*	@Test
 	public void splitAndSum_custom_구분자() throws Exception {
 		int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
 		assertThat(result).isEqualTo(6);
