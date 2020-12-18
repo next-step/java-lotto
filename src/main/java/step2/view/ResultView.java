42011039ed67d1e930fe0cc73a39a5564eb41b94
MJ Choi <1621139@gmail.com>
2020-12-18 22:36:23 +0900

    feature: ADD ResultView for print result

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -1,45 +1,39 @@
 package step2.view;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import step2.domain.LottoResult;
+import step2.domain.LottoTicket;
+import step2.domain.LottoTickets;
+import step2.domain.WinningRank;
 
 public class ResultView {
-    private static final String RESULT_MESSAGE = "당첨통계";
-    private static final String BELOW_LINE = "--------";
-    private static final int MIN_MATCH_NUMBER = 3;
-    private static final String FOURTH_PRIZE_MESSAGE = "3개 일치 (5000원)- ";
-    private static final int FOUR_MATCH_NUMBER = 4;
-    private static final String THIRD_PRIZE_MESSAGE = "4개 일치 (50000원)- ";
-    private static final int FIVE_MATCH_NUMBER = 5;
-    private static final String SECOND_PRIZE_MESSAGE = "5개 일치 (1500000원)- ";
-    private static final int MAX_MATCH_NUMBER = 6;
-    private static final String FIRST_PRIZE_MESSAGE = "6개 일치 (2000000000원)- ";
-    private static final String PIECE = "개";
-    private static final String TOTAL_RETURN_MESSAGE = "총 수익률은 ";
-    private static final String IS = "입니다.";
+    private final static String PURCHASED_LOTTOS_COUNTING = "%d개를 구매했습니다.";
+    private final static String LOTTO_RESULTS = "%d개 일치 (%d원)- %d개";
+    private final static String LOTTO_RESULT_EARNINGS_RATE = "총 수익률은 %.2f입니다.";
+    private final static String EARNINGS_RATE = "earningsRate";
 
-    public static void printNumberOfLotto(int numberOfLotto) {
-        System.out.println(numberOfLotto + "개를 구매했습니다.");
+    private ResultView() {
     }
 
-
-    public static void printWinningResult(HashMap<Integer, Integer> hashMap) {
-        System.out.println(RESULT_MESSAGE);
-        System.out.println(BELOW_LINE);
-        System.out.println(FOURTH_PRIZE_MESSAGE + hashMap.get(MIN_MATCH_NUMBER) + PIECE);
-        System.out.println(THIRD_PRIZE_MESSAGE + hashMap.get(FOUR_MATCH_NUMBER) + PIECE);
-        System.out.println(SECOND_PRIZE_MESSAGE + hashMap.get(FIVE_MATCH_NUMBER) + PIECE);
-        System.out.println(FIRST_PRIZE_MESSAGE + hashMap.get(MAX_MATCH_NUMBER) + PIECE);
-    }
-
-    public static void printLottoRateOfReturn(String lottoRateOfReturn) {
-        System.out.println(TOTAL_RETURN_MESSAGE + lottoRateOfReturn + IS);
-    }
-
-    public static void printMyLottoNumber(List<List<Integer>> lottoNumbers, int ticketOfLotto) {
-        for (int i = 0; i < ticketOfLotto; i++) {
-            System.out.println(lottoNumbers.get(i));
+    public static void printLottoTickets(LottoTickets lottoTickets) {
+        countTotalLottoTickets(lottoTickets);
+        List<LottoTicket> tickets = lottoTickets.getLottoTickets();
+        for (LottoTicket lottoTicket : tickets) {
+            System.out.println(lottoTicket.getLottoNumbers());
         }
+    }
+
+    private static void countTotalLottoTickets(LottoTickets lottoTickets) {
+        System.out.println(String.format(PURCHASED_LOTTOS_COUNTING, lottoTickets.countLottoTickets()));
+    }
+
+    public static void printLottoResult(LottoResult lottoResult) {
+        Map<String, Object> map = lottoResult.getResult();
+        for (WinningRank winningRank : WinningRank.values()) {
+            System.out.println(String.format(LOTTO_RESULTS, winningRank.getMatchingCount(winningRank), winningRank.getPrizeMoney(), lottoResult.countRank(winningRank)));
+        }
+        System.out.println(String.format(LOTTO_RESULT_EARNINGS_RATE, map.get(EARNINGS_RATE)));
     }
 }
