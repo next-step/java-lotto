c2650c180d9a5ec195d89a47b16888ff4dcc13d5
charlloss.dev <charlloss.dev@gmail.com>
2020-12-18 16:41:05 +0900

    fix: update 로또 추첨 보너스 번호 추가 출력 수정

--- src/main/java/lotto/view/UserView.java
+++ src/main/java/lotto/view/UserView.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import lotto.modal.Lotto;
+import lotto.modal.LottoNumber;
 import lotto.modal.Money;
+import lotto.modal.WinnerLotto;
 
 public class UserView {
 
@@ -17,9 +19,14 @@ public class UserView {
 		return new Money(getUserInputSystem());
 	}
 
-	public static Lotto getUserInputLotto() {
-		UserOutput.printUserInputLotto();
-		return Lotto.generateWinnerLotto(getUserInputSystem());
+	public static WinnerLotto getUserInputLotto() {
+		UserOutput.printUserInputWinnerLotto();
+		Lotto winnerLotto = Lotto.generateWinnerLotto(getUserInputSystem());
+
+		UserOutput.printUserInputBonusNumber();
+		LottoNumber bonusNumber = new LottoNumber(getUserInputSystem());
+
+		return new WinnerLotto(winnerLotto, bonusNumber);
 	}
 
 	public static void printLottoPackage(List<Lotto> lottoPackage) {
