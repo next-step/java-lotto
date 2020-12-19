8a4a8ea27b4cd429b01075279a1e74170f730646
Donghwan Noh <namul.bap@gmail.com>
2020-12-20 01:16:19 +0900

    feat(view): 당첨 통계를 화면에 출력할 때 2등도 추가

--- src/main/java/edu/nextstep/lotto/view/ResultView.java
+++ src/main/java/edu/nextstep/lotto/view/ResultView.java
@@ -26,6 +26,7 @@ public class ResultView {
 		printRankStatistics(LottoRank.FIFTH, rankCountingMap);
 		printRankStatistics(LottoRank.FOURTH, rankCountingMap);
 		printRankStatistics(LottoRank.THIRD, rankCountingMap);
+		printRankStatisticsWithBonus(LottoRank.SECOND, rankCountingMap);
 		printRankStatistics(LottoRank.FIRST, rankCountingMap);
 	}
 
@@ -37,9 +38,17 @@ public class ResultView {
 		System.out.println(rank.getMatchCount() + "개 일치 (" + rank.getReward() + "원)- " + count + "개");
 	}
 
+	private static void printRankStatisticsWithBonus(LottoRank rank, Map<LottoRank, Long> rankCountingMap) {
+		Long count = rankCountingMap.get(rank);
+		if (count == null) {
+			count = 0L;
+		}
+		System.out.println(rank.getMatchCount() + "개 일치, 보너스 볼 일치(" + rank.getReward() + "원) - " + count + "개");
+	}
+
 	public static void printProfitRatio(BigDecimal profitRatio) {
 		System.out.print("총 수익률은 " + profitRatio + "입니다.");
-		if(profitRatio.compareTo(BigDecimal.ONE) == -1) {
+		if (profitRatio.compareTo(BigDecimal.ONE) == -1) {
 			System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
 			return;
 		}
