bf3e3071a9cd3fe7b03ce2b6dde69c1a2ad06556
hyeji <khji1210@naver.com>
2020-12-19 10:04:34 +0900

    refactor(WinningLottoTicketTest)

--- src/test/java/lotto/domain/numbers/WinningLottoTicketTest.java
+++ src/test/java/lotto/domain/numbers/WinningLottoTicketTest.java
@@ -1,14 +1,14 @@
 package lotto.domain.numbers;
 
+import lotto.domain.LottoNumberConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,45 +18,39 @@ class WinningLottoTicketTest {
 
     @BeforeEach
     void setUp() {
-        List<LottoNumber> winningNumbers = provideWinningNumbers();
-        winningLottoTicket = new WinningLottoTicket(new LottoTicket(winningNumbers), new LottoNumber(7));
+        List<LottoNumber> winningNumbers = LottoNumberConverter.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+        winningLottoTicket = new WinningLottoTicket(new LottoTicket(winningNumbers), LottoNumber.of(7));
     }
 
     @DisplayName("당첨 번호 6개에 보너스 숫자가 포함되어 있으면, Exception 발생")
     @Test
     void validateBonusNumber() {
-        List<LottoNumber> winningNumbers = provideWinningNumbers();
-        assertThatThrownBy(() -> new WinningLottoTicket(new LottoTicket(winningNumbers), new LottoNumber(4)))
+        List<LottoNumber> winningNumbers = LottoNumberConverter.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> new WinningLottoTicket(new LottoTicket(winningNumbers), LottoNumber.of(4)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("당첨 번호 6개에 보너스 숫자가 포함 되어 있으면 안됩니다.");
     }
 
-    @DisplayName("번호가 당첨번호 리스트(LottoTicket)에 포함되어 있으면 true 반환.")
+    @DisplayName("로또 숫자가 당첨 숫자 리스트에 포함되어 있으면 true 반환.")
     @ParameterizedTest
     @ValueSource(ints = {1, 4, 6})
     void isContain(int number) {
-        boolean result = winningLottoTicket.isContain(new LottoNumber(number));
+        boolean result = winningLottoTicket.isContain(LottoNumber.of(number));
         assertThat(result).isTrue();
     }
 
-    @DisplayName("번호가 보너스 볼과 일치하면 true 반환.")
+    @DisplayName("로또 숫자가 보너스 볼과 일치하면 true 반환.")
     @Test
-    void isMatchBonusNumber1() {
-        boolean result = winningLottoTicket.isMatchBonusNumber(new LottoNumber(7));
+    void isMatchBonusNumber_true() {
+        boolean result = winningLottoTicket.isMatchBonusNumber(LottoNumber.of(7));
         assertThat(result).isTrue();
     }
 
-    @DisplayName("번호가 보너스 볼과 일치하지 않으면 false 반환.")
+    @DisplayName("로또 숫자가 보너스 볼과 일치하지 않으면 false 반환.")
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 8})
-    void isMatchBonusNumber2(int number) {
-        boolean result = winningLottoTicket.isMatchBonusNumber(new LottoNumber(number));
+    void isMatchBonusNumber_false(int number) {
+        boolean result = winningLottoTicket.isMatchBonusNumber(LottoNumber.of(number));
         assertThat(result).isFalse();
-    }
-
-    private List<LottoNumber> provideWinningNumbers() {
-        return Stream.of(1, 2, 3, 4, 5, 6)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
     }
 }
