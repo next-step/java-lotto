a11e90aa2b1a25a3489bde17a60900c3760013f2
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-19 21:03:01 +0900

    fix : 일급 콜렉션 적용 GeneratedLotto 클래스 생성 - 자동으로 뽑힌 6개 당첨 번호 - 지난주 당첨번호 6개

---
+++ src/main/java/lotto/domain/GeneratedLotto.java
@@ -0,0 +1,46 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GeneratedLotto {
+
+	private static final int LOTTO_NUMBER_MAX_COUNT = 6;
+
+	public List<Integer> lottoNumbers = new ArrayList<>();
+
+	public GeneratedLotto(){
+		Collections.shuffle(Lotto.defaultLottoNumbers);
+		for (int i = 0; i < LOTTO_NUMBER_MAX_COUNT; i++) {
+			lottoNumbers.add(Lotto.defaultLottoNumbers.get(i));
+		}
+	}
+
+	public GeneratedLotto(String[] inputNumber){
+		for (int i = 0; i < inputNumber.length; i++) {
+			Integer number = Integer.parseInt(inputNumber[i].trim());
+			lottoNumbers.add(number);
+		}
+		validateMaxCount(lottoNumbers);
+	}
+
+	public static int countMatchNumber(GeneratedLotto lastWeekPrizeNumberList, GeneratedLotto generatedLotto) {
+		Set<Integer> result = lastWeekPrizeNumberList.lottoNumbers.stream()
+				.distinct()
+				.filter(generatedLotto.lottoNumbers::contains)
+				.collect(Collectors.toSet());
+
+		return result.size();
+	}
+
+	private void validateMaxCount(List<Integer> generatedLotto) {
+		Set<Integer> validateSet = generatedLotto.stream().collect(Collectors.toSet());
+		if (validateSet.size() != LOTTO_NUMBER_MAX_COUNT) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
+
--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -11,9 +11,9 @@ public class InputViewTest {
 	@Test
 	public void 지난주_당첨번호가_잘입력되었는가_테스트() {
 		InputView inputView = new InputView();
-		assertThat(inputView.convertStringToInteger("1, 2, 3, 4, 5, 6").size()).isEqualTo(6);
+		assertThat(inputView.convertStringToInteger("1, 2, 3, 4, 5, 6").lottoNumbers.size()).isEqualTo(6);
 
-		assertThat(inputView.convertStringToInteger("1 , 2 , 3 , 4, 5, 6").size()).isEqualTo(6);
+		assertThat(inputView.convertStringToInteger("1 , 2 , 3 , 4, 5, 6").lottoNumbers.size()).isEqualTo(6);
 
 		assertThatIllegalArgumentException().isThrownBy(() -> {
 			inputView.convertStringToInteger("1, 2, 3, 4, 5, 6, 7");
