1884491f783d566715dd3886ba3eacbafc126f41
Pilhwan Kim <pilhwankim03@gmail.com>
2020-12-21 06:42:17 +0900

    refector: WiningCase 를 Enum 으로 변경(Rank)

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -8,7 +8,7 @@ public class LottoResult {
     private static final int INITIAL_COUNT = 0;
     private static final int DEFAULT_WINNING_COUNT = 0;
 
-    private final Map<WinningCase, Integer> winningCounts = new HashMap<>();
+    private final Map<Rank, Integer> rankCounts = new HashMap<>();
     private final int buyAmount;
 
     public LottoResult(List<Lotto> lottoList, Lotto winnerLotto, int buyAmount) {
@@ -16,22 +16,24 @@ public class LottoResult {
 
         lottoList.stream().map(lotto -> {
             int numberCount = lotto.matchNumberCount(winnerLotto);
-            return new WinningCase(numberCount);
-        }).forEach(winningCase -> {
-            winningCounts.putIfAbsent(winningCase, INITIAL_COUNT);
-            winningCounts.computeIfPresent(winningCase,(key, count) -> ++count);
+            return Rank.valueOf(numberCount, false);
+        }).forEach(rank -> {
+            rankCounts.putIfAbsent(rank, INITIAL_COUNT);
+            rankCounts.computeIfPresent(rank,(key, count) -> ++count);
         });
     }
 
     public int getTotalReward() {
-        return winningCounts.entrySet().stream()
-                .map((entry) -> entry.getKey().getReward(entry.getValue()))
+        return rankCounts.entrySet().stream()
+                .map(
+                        (entry) -> entry.getKey().getWinningMoney() * entry.getValue()
+                )
                 .reduce(Integer::sum)
                 .orElse(0);
     }
 
     public int getSameLottoCount(int sameCount) {
-        return winningCounts.getOrDefault(new WinningCase(sameCount), DEFAULT_WINNING_COUNT);
+        return rankCounts.getOrDefault(Rank.valueOf(sameCount, false), DEFAULT_WINNING_COUNT);
     }
 
     public double getRewardRate() {
