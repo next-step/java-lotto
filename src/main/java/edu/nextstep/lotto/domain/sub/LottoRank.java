6be2621b2c9d831402327a989a2beca6da772ba4
Donghwan Noh <namul.bap@gmail.com>
2020-12-20 23:10:33 +0900

    refactor: 리뷰내용 반영 (LottoRank의 valueOf 로직 개선)
    BREAKING CHANGE:
    - LottoRank
      - valueOf: 각 enum들마다의 함수형 인터페이스를 활용해서, 조건의 맞는 Rank를 찾아 반환하도록 로직 개선

--- src/main/java/edu/nextstep/lotto/domain/sub/LottoRank.java
+++ src/main/java/edu/nextstep/lotto/domain/sub/LottoRank.java
@@ -2,37 +2,42 @@ package edu.nextstep.lotto.domain.sub;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.BiPredicate;
 
 public enum LottoRank {
-	FIRST(LottoNumbers.SIZE, 2_000_000_000L),
-	SECOND(LottoNumbers.SIZE - 1, 30_000_000L),
-	THIRD(LottoNumbers.SIZE - 1, 1_500_000L),
-	FOURTH(LottoNumbers.SIZE - 2, 50_000L),
-	FIFTH(LottoNumbers.SIZE - 3, 5_000L),
-	NONE(0, 0L);
 
+	FIRST(LottoNumbers.SIZE, 2_000_000_000L,
+		(matchCount, isMatchBonus) -> matchCount == LottoNumbers.SIZE),
+
+	SECOND(LottoNumbers.SIZE - 1, 30_000_000L,
+		(matchCount, isMatchBonus) -> matchCount == LottoNumbers.SIZE - 1 && isMatchBonus),
+
+	THIRD(LottoNumbers.SIZE - 1, 1_500_000L,
+		(matchCount, isMatchBonus) -> matchCount == LottoNumbers.SIZE - 1 && !isMatchBonus),
+
+	FOURTH(LottoNumbers.SIZE - 2, 50_000L,
+		(matchCount, isMatchBonus) -> matchCount == LottoNumbers.SIZE - 2),
+
+	FIFTH(LottoNumbers.SIZE - 3, 5_000L,
+		(matchCount, isMatchBonus) -> matchCount == LottoNumbers.SIZE - 3),
+
+	NONE(0, 0L,
+		(matchCount, isMatchBonus) -> matchCount >= 0 && matchCount < LottoNumbers.SIZE - 3);
+
+	private final BiPredicate<Integer, Boolean> isMatchRank;
 	private final int matchCount;
 	private final long reward;
 
-	LottoRank(int matchCount, long reward) {
+	LottoRank(int matchCount, long reward, BiPredicate<Integer, Boolean> isMatchRank) {
 		this.matchCount = matchCount;
 		this.reward = reward;
+		this.isMatchRank = isMatchRank;
 	}
 
 	public static Optional<LottoRank> valueOf(int matchCount, boolean isMatchBonus) {
-		if (matchCount < 0 || matchCount > LottoNumbers.SIZE) {
-			return Optional.empty();
-		}
-		if (matchCount < FIFTH.matchCount) {
-			return Optional.of(NONE);
-		}
-		if (SECOND.matchCount == matchCount && isMatchBonus) {
-			return Optional.of(SECOND);
-		}
 		return Arrays.stream(LottoRank.values())
-			.filter(lottoRank -> !lottoRank.equals(LottoRank.SECOND))
-			.filter(lottoRank -> lottoRank.matchCount == matchCount)
-			.findFirst();
+			.filter(lottoRank -> lottoRank.isMatchRank.test(matchCount, isMatchBonus))
+			.findAny();
 	}
 
 	public int getMatchCount() {
