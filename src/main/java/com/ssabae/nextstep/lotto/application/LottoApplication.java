03bfc9136f1d9284f3697fb88caac1e821d114a0
Ssabae <llsb156@gmail.com>
2020-12-20 22:31:15 +0900

    fix: LottoTicketMachine을 Domain Service 객체로 사용

--- src/main/java/com/ssabae/nextstep/lotto/application/LottoApplication.java
+++ src/main/java/com/ssabae/nextstep/lotto/application/LottoApplication.java
@@ -1,6 +1,5 @@
 package com.ssabae.nextstep.lotto.application;
 
-import com.ssabae.nextstep.lotto.domain.LottoTicketsAnalyzer;
 import com.ssabae.nextstep.lotto.domain.Money;
 import com.ssabae.nextstep.lotto.domain.WinningNumber;
 import com.ssabae.nextstep.lotto.domain.lotto.ticketgenerator.AutoLottoTicketGenerator;
@@ -19,13 +18,11 @@ import com.ssabae.nextstep.lotto.view.ResultView;
 public class LottoApplication {
 
     private final LottoTicketMachine lottoTicketMachine;
-    private final LottoTicketsAnalyzer analyzer;
     private final ResultView resultView;
 
     public LottoApplication() {
         LottoTicketGenerator generator = new AutoLottoTicketGenerator();
         this.lottoTicketMachine = new LottoTicketMachine(generator);
-        this.analyzer = new LottoTicketsAnalyzer();
         this.resultView = new ResultView();
     }
 
@@ -36,7 +33,7 @@ public class LottoApplication {
         LottoPrinter.print(lottoTickets);
 
         WinningNumber winningNumber = InputView.winningNumberInput();
-        LottoResultDto lottoResultDto = analyzer.convertToDto(lottoTickets, winningNumber);
+        LottoResultDto lottoResultDto = lottoTicketMachine.calculateYield(lottoTickets, winningNumber);
 
         resultView.report(lottoResultDto);
     }
--- src/main/java/com/ssabae/nextstep/lotto/domain/lotto/LottoTicketMachine.java
+++ src/main/java/com/ssabae/nextstep/lotto/domain/lotto/LottoTicketMachine.java
@@ -2,7 +2,10 @@ package com.ssabae.nextstep.lotto.domain.lotto;
 
 import static com.ssabae.nextstep.lotto.Constant.LOTTO_TICKET_PRICE;
 
+import com.ssabae.nextstep.lotto.application.LottoResultDto;
+import com.ssabae.nextstep.lotto.domain.LottoTicketsAnalyzer;
 import com.ssabae.nextstep.lotto.domain.Money;
+import com.ssabae.nextstep.lotto.domain.WinningNumber;
 import com.ssabae.nextstep.lotto.domain.lotto.ticketgenerator.LottoTicketGenerator;
 import java.util.Collections;
 import java.util.List;
@@ -18,8 +21,11 @@ public class LottoTicketMachine {
 
     private final LottoTicketGenerator generator;
 
+    private final LottoTicketsAnalyzer analyzer;
+
     public LottoTicketMachine(LottoTicketGenerator generator) {
         this.generator = generator;
+        this.analyzer = new LottoTicketsAnalyzer();
     }
 
     public LottoTickets buy(Money amount) {
@@ -29,6 +35,10 @@ public class LottoTicketMachine {
                 .limit(count)
                 .collect(Collectors.toList());
         return new LottoTickets(Collections.unmodifiableList(lottoTicketList));
+    }
+
+    public LottoResultDto calculateYield(LottoTickets lottoTickets, WinningNumber winningNumber) {
+        return this.analyzer.convertToDto(lottoTickets, winningNumber);
     }
 
     private void validate(Money amount) {
