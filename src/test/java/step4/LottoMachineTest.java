8071d6dfe86f7f7a7fa67d784ed7f4ff03f6e45d
최종원 <90lastone@gmail.com>
2020-12-20 23:48:11 +0900

    test: 수동로또, 자동로또 구매처리

--- src/test/java/step4/LottoMachineTest.java
+++ src/test/java/step4/LottoMachineTest.java
@@ -3,12 +3,17 @@ package step4;
 import org.junit.jupiter.api.Test;
 import step4.domain.LottoMachine;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoMachineTest {
     @Test
-    public void 구매갯수계산() {
+    public void buyLotto() {
         LottoMachine lottoMachine = new LottoMachine();
-        assertThat(lottoMachine.buyLotto(14000).size()).isEqualTo(14);
+
+        List<String> ManualLottoNumbers = Arrays.asList("8, 21, 23, 41, 42, 43", "3, 5, 11, 16, 32, 38", "7, 11, 16, 35, 36, 44");
+        assertThat(lottoMachine.buyLotto(14000, ManualLottoNumbers).size()).isEqualTo(14);
     }
 }
