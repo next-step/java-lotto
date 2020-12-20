f5ed37d42e30783447353835d441a28742aa0df7
Donghwan Noh <namul.bap@gmail.com>
2020-12-20 22:24:30 +0900

    refactor: 리뷰내용 반영 (LottoGame에서 lottoGameResult 변수 제거)
    BREAKING CHANGE:
    - LottoGame
      - lottoGameResult: 생성자에서 생성된 후 getter로 반환될 뿐이어 인스턴스 변수에서 제거. getResult 메소드에서 바로 생성하여 반환.

--- src/main/java/edu/nextstep/lotto/domain/LottoGame.java
+++ src/main/java/edu/nextstep/lotto/domain/LottoGame.java
@@ -9,16 +9,14 @@ import edu.nextstep.lotto.domain.sub.WinningLotto;
 public class LottoGame {
 	private final Lottos soldLottos;
 	private final WinningLotto winningLotto;
-	private final LottoGameResult lottoGameResult;
 
 	public LottoGame(List<List<Integer>> soldLottos, List<Integer> firstRankLotto, int bonusNumber) {
 		this.soldLottos = new Lottos(soldLottos);
 		this.winningLotto = new WinningLotto(firstRankLotto, bonusNumber);
-		this.lottoGameResult = new LottoGameResult(this.soldLottos, this.winningLotto);
 	}
 
 	public LottoGameResult getResult() {
-		return lottoGameResult;
+		return new LottoGameResult(this.soldLottos, this.winningLotto);
 	}
 
 }
