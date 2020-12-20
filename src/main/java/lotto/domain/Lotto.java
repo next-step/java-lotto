fdd00fab7087c19713332408cd1403389eef96fc
Sang Uk Ahn <suahnn@gmail.com>
2020-12-20 17:52:13 +0900

    feat: 로또 매치 개수 구하는 로직 구현, 로또 테스트 케이스 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,13 +3,18 @@ package lotto.domain;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class Lotto {
 
     public static final int LOTTO_NUMBER_SIZE = 6;
 
+    private static final String LOTTO_NUMBER_SIZE_MESSAGE = "Count of Lotto numbers is less than %s";
+
     private final Set<LottoNumber> lottoNumbers;
 
     public Lotto(Set<LottoNumber> lottoNumbers) {
+        checkArgument(lottoNumbers.size() == LOTTO_NUMBER_SIZE, LOTTO_NUMBER_SIZE_MESSAGE, LOTTO_NUMBER_SIZE);
         this.lottoNumbers = lottoNumbers;
     }
 
@@ -18,7 +23,7 @@ public class Lotto {
 
         Set<LottoNumber> lottoNumbers = new HashSet<>();
         for (String part : parts) {
-            lottoNumbers.add(LottoNumber.of(part));
+            lottoNumbers.add(LottoNumber.of(part.trim()));
         }
 
         return new Lotto(lottoNumbers);
@@ -28,5 +33,16 @@ public class Lotto {
         return lottoNumbers.stream()
                 .mapToInt(LottoNumber::getValue)
                 .toArray();
+    }
+
+    public int countOfMatch(Lotto lotto) {
+        long count = lottoNumbers.stream()
+                                 .filter(lotto::contains)
+                                 .count();
+        return Math.toIntExact(count);
+    }
+
+    public boolean contains(LottoNumber lottoNumber) {
+        return lottoNumbers.contains(lottoNumber);
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
 
@@ -16,5 +17,29 @@ public class LottoTest {
         Lotto lotto = Lotto.of(input);
         // then
         assertThat(lotto.toArray()).isEqualTo(new int[] {1, 2, 3, 4, 5, 6});
+    }
+
+    @Test
+    @DisplayName("콤마가 아닌 구분자로 로또 생성 시 예외")
+    void testCreateLottoWithOtherDelimiter() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of("1:2:3:4:5:6"));
+    }
+
+    @Test
+    @DisplayName("로또 번호 개수가 6개 미만일 경우 예외")
+    void testCreateLottoLessThanSix() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of("1,2,3"));
+    }
+
+    @Test
+    @DisplayName("매치 개수")
+    void testLottoCountOfMatchAll() {
+        // given
+        Lotto lotto = Lotto.of("1,2,3,4,5,6");
+        Lotto winningLotto = Lotto.of("1,2,3,4,5,6");
+        // when
+        int countOfMatch = lotto.countOfMatch(winningLotto);
+        // then
+        assertThat(countOfMatch).isEqualTo(6);
     }
 }
