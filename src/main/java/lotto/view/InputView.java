f832f1be76bb2b72843b3e9e92b21d3bd5cffa08
Do Yeon Kim <chikeem90@gmail.com>
2020-12-22 01:55:12 +0900

    feat: UI 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,22 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-	private Scanner sc = new Scanner(System.in);
-	public int printBuyMoneyMessageAndReturnInput() {
+	private static final Scanner scanner = new Scanner(System.in);
+	public static int inputBuyMoneyAndReturnInputValue() {
 		System.out.println("구입금액을 입력해 주세요.");
-		return sc.nextInt();
+		return scanner.nextInt();
+	}
+
+	public static List<Integer> inputWinLottoNumbersAndReturnInputValue() {
+		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		scanner.skip("[\r\n]+");
+		String input = scanner.nextLine();
+		String[] inputArray = input.split(",");
+		return Arrays.stream(inputArray).map(i -> i.trim()).map(Integer::parseInt).collect(Collectors.toList());
 	}
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,17 +1,45 @@
 package lotto.view;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoNumbers;
+import lotto.domain.LottoRank;
+import lotto.dto.LottoGameResultDto;
 
 public class ResultView {
-	public void printLottoNumbers(LottoNumbers lottoNumbers) {
+	public static void printLottoNumbers(LottoNumbers lottoNumbers) {
 		for (LottoNumber lottoNumber : lottoNumbers.getLottoNumbers()) {
 			System.out.println(lottoNumber);
 		}
 		System.out.println();
 	}
 
-	public void printLottoGameCount(int lottoGameCount) {
+	public static void printLottoGameCount(int lottoGameCount) {
 		System.out.println(lottoGameCount + "개를 구매했습니다.");
+	}
+
+	public static void printLottoGameStatistic(LottoGameResultDto lottoGameResultDto) {
+		Map<LottoRank, Long> lottoGameResult = lottoGameResultDto.getLottoGameResult();
+		System.out.println("당첨 통계");
+		System.out.println("---------");
+		printLottoRank(LottoRank.FOURTH, lottoGameResult);
+		printLottoRank(LottoRank.THIRD, lottoGameResult);
+		printLottoRank(LottoRank.SECOND, lottoGameResult);
+		printLottoRank(LottoRank.FIRST, lottoGameResult);
+	}
+
+	private static void printLottoRank(LottoRank lottoRank, Map<LottoRank, Long> lottoGameResult) {
+		Long count = lottoGameResult.get(lottoRank);
+		if (count == null) {
+			count = 0L;
+		}
+		System.out.println(lottoRank.getCountOfMatch() + "개 일치 (" + lottoRank.getWinningMoney() + "원)- " + count + "개");
+	}
+
+	public static void printProfitRatio(LottoGameResultDto lottoGameResultDto) {
+		BigDecimal profitRatio = lottoGameResultDto.getProfitRatio();
+		System.out.println("총 수익률은 " + profitRatio + "입니다." + ((profitRatio.compareTo(BigDecimal.ONE) == -1) ? "(기준이 1이기 때문에 결과적으로 손해라는 의미임)" : ""));
 	}
 }
