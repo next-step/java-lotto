ba6433d0fdc3b5cc6d6187169f26f47f5e53c2b8
Do Yeon Kim <chikeem90@gmail.com>
2020-12-22 01:52:49 +0900

    feat: LottoRank 추가

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public enum LottoRank {
+	FIRST(6, 2_000_000_000),
+	SECOND(5, 1_500_000),
+	THIRD(4, 50_000),
+	FOURTH(3, 5_000),
+	MISS(0, 0);
+
+	private final int countOfMatch;
+	private final int winningMoney;
+
+	private LottoRank(int countOfMatch, int winningMoney) {
+		this.countOfMatch = countOfMatch;
+		this.winningMoney = winningMoney;
+	}
+
+	public int getCountOfMatch() {
+		return countOfMatch;
+	}
+
+	public int getWinningMoney() {
+		return winningMoney;
+	}
+}
