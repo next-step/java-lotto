53c08a5cb59a74d207d0a65d95e9c718831cce2b
최종원 <90lastone@gmail.com>
2020-12-21 21:35:12 +0900

    refactor(WinningLotto): Exception 메시지 상수화

--- src/main/java/step4/domain/WinningLotto.java
+++ src/main/java/step4/domain/WinningLotto.java
@@ -3,11 +3,12 @@ package step4.domain;
 public class WinningLotto {
     private Lotto lotto;
     private LottoNumber bonusNumber;
+    private final String ARGUMENT_EXCEPTION_MESSAGE = "보너스 번호는 당첨번호에 포함될 수 없습니다.";
 
     public WinningLotto(Lotto lotto, LottoNumber bonusNumber) {
         this.lotto = lotto;
         if (this.lotto.contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호는 당첨번호에 포함될 수 없습니다.");
+            throw new IllegalArgumentException(ARGUMENT_EXCEPTION_MESSAGE);
         }
         this.bonusNumber = bonusNumber;
     }
