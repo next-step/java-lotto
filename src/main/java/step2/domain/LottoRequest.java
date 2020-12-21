14b71be6d8c92ef6b0eab6b3f625642200167282
bss <baekss0409@gmail.com>
2020-12-22 01:01:16 +0900

    feat: 로또 수동 구매 기능 추가

--- src/main/java/step2/domain/LottoRequest.java
+++ src/main/java/step2/domain/LottoRequest.java
@@ -1,13 +1,36 @@
 package step2.domain;
 
-public class LottoRequest {
-    private int purchaseMoney;
+import step2.domain.lotto.LottoNumbers;
 
-    public LottoRequest(int purchaseMoney) {
+import java.util.List;
+
+import static java.util.Collections.*;
+import static step2.domain.lotto.Lotto.*;
+
+public class LottoRequest {
+    private final int purchaseMoney;
+    private final int totalQuantityForAutomatedPick;
+    private List<LottoNumbers> lottoNumbersByManualPick;
+
+    public LottoRequest(int purchaseMoney, List<LottoNumbers> lottoNumbersByManualPick) {
+        int totalPriceOfManualPick = lottoNumbersByManualPick.size() * LOTTO_PRICE;
+        if (purchaseMoney < totalPriceOfManualPick) {
+            throw new IllegalArgumentException("지불 금액을 초과하여 로또를 구매할 수 없습니다");
+        }
         this.purchaseMoney = purchaseMoney;
+        this.totalQuantityForAutomatedPick = (purchaseMoney - totalPriceOfManualPick) / LOTTO_PRICE;
+        this.lottoNumbersByManualPick = unmodifiableList(lottoNumbersByManualPick);
     }
 
     public int getPurchaseMoney() {
         return purchaseMoney;
+    }
+
+    public int getTotalQuantityForAutomatedPick() {
+        return totalQuantityForAutomatedPick;
+    }
+
+    public List<LottoNumbers> getLottoNumbersByManualPick() {
+        return lottoNumbersByManualPick;
     }
 }
