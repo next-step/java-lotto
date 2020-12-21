50788982c4af2745a38a20239195db6896765eff
Do Yeon Kim <chikeem90@gmail.com>
2020-12-21 23:29:15 +0900

    feat: LottoNumber toString 오버라이드

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoNumber {
 	private List<Integer> lottoNumber;
@@ -11,5 +12,10 @@ public class LottoNumber {
 
 	public void generate(NumberGenerator numberGenerator) {
 		this.lottoNumber = numberGenerator.generateNumbers();
+	}
+
+	@Override
+	public String toString() {
+		return "["+lottoNumber.stream().map(ln -> Integer.toString(ln)).collect(Collectors.joining(", "))+"]";
 	}
 }
