7c86131865200a6ef2438e3eeaeb7393a4842f83
Do Yeon Kim <chikeem90@gmail.com>
2020-12-21 20:44:44 +0900

    feat: 로또 생성 로직 추가

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,10 +1,27 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class LottoNumberGenerator implements NumberGenerator {
+	private final Random random = new Random();
+
 	@Override
 	public List<Integer> generateNumbers() {
-		return null;
+		List<Integer> lottoNumber = new ArrayList<>();
+		while (lottoNumber.size() < 6) {
+			addRandomNumber(lottoNumber);
+		}
+		Collections.shuffle(lottoNumber);
+		return lottoNumber;
+	}
+
+	private void addRandomNumber(List<Integer> lottoNumber) {
+		int randomNumber = random.nextInt(45) + 1;
+		if (!lottoNumber.contains(randomNumber)) {
+			lottoNumber.add(randomNumber);
+		}
 	}
 }
