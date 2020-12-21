7e9e8d2f61704474717a38aee0dbeee0c491ea6b
Do Yeon Kim <chikeem90@gmail.com>
2020-12-22 01:54:08 +0900

    feat: LottoNumber가 일치값 구하는 메서드와 Rank를 갖고오는 메서드를 갖도록 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,10 +1,21 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import javax.swing.text.html.Option;
 
 public class LottoNumber {
 	private List<Integer> lottoNumber;
+
+	public LottoNumber() {
+	}
+
+	public LottoNumber(List<Integer> lottoNumber) {
+		this.lottoNumber = lottoNumber;
+	}
 
 	public List<Integer> getLottoNumber() {
 		return lottoNumber;
@@ -12,6 +23,18 @@ public class LottoNumber {
 
 	public void generate(NumberGenerator numberGenerator) {
 		this.lottoNumber = numberGenerator.generateNumbers();
+	}
+
+	private int countSameNumber(LottoNumber winLottoNumber) {
+		return (int) lottoNumber.stream()
+			.filter(ln -> winLottoNumber.getLottoNumber().contains(ln))
+			.count();
+	}
+
+	public Optional<LottoRank> rankLotto(LottoNumber winLottoNumber) {
+		int sameNumberCount = countSameNumber(winLottoNumber);
+		return Arrays.stream(LottoRank.values())
+			.filter(lottoRank -> lottoRank.getCountOfMatch() == sameNumberCount).findFirst();
 	}
 
 	@Override
