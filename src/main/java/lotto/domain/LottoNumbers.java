63f9081ac51a6376e550d7c781fe99bb412f7f24
Do Yeon Kim <chikeem90@gmail.com>
2020-12-21 23:08:34 +0900

    refactor: LottoNumbers 수정

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -6,13 +6,17 @@ import java.util.List;
 public class LottoNumbers {
 	private List<LottoNumber> lottoNumbers;
 
-	public List<LottoNumber> generateLottoNumbers(int lottoGameCount, NumberGenerator numberGenerator) {
+	public List<LottoNumber> getLottoNumbers() {
+		return lottoNumbers;
+	}
+
+	public LottoNumbers generateLottoNumbers(int lottoGameCount, NumberGenerator numberGenerator) {
 		lottoNumbers = new ArrayList<>();
 		for (int i = 0; i < lottoGameCount; i++) {
 			LottoNumber lottoNumber = new LottoNumber();
 			lottoNumber.generate(numberGenerator);
 			lottoNumbers.add(lottoNumber);
 		}
-		return lottoNumbers;
+		return this;
 	}
 }
