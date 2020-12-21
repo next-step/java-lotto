44f236babd9bec31a06b80a00e61a59271dc22ef
Do Yeon Kim <chikeem90@gmail.com>
2020-12-21 23:09:01 +0900

    feat: generateLottoNumbers 메서드 추가

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,23 +1,17 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-
 public class LottoGame {
 	private static final int LOTTO_PRICE = 1000;
+
+	private int lottoGameCount;
+
 	public int calculateLottoCount(int input) {
-		return input/LOTTO_PRICE;
+		this.lottoGameCount = input/LOTTO_PRICE;
+		return this.lottoGameCount;
 	}
 
-	public List<Integer> generateLottoNumber() {
-		Random random = new Random();
-		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i < 6; i++) {
-			list.add(random.nextInt(45)+1);
-		}
-		Collections.shuffle(list);
-		return list;
+	public LottoNumbers generateLottoNumbers(int lottoGameCount, NumberGenerator numberGenerator) {
+		LottoNumbers lottoNumbers = new LottoNumbers();
+		return lottoNumbers.generateLottoNumbers(lottoGameCount, numberGenerator);
 	}
 }
