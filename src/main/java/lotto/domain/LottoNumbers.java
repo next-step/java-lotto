651cefdb3f2e9f8583ea81666d27d0e0d65572db
Do Yeon Kim <chikeem90@gmail.com>
2020-12-21 20:59:05 +0900

    feat: LottoNumbers 일급컬렉션 추가

---
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumbers {
+	private List<LottoNumber> lottoNumbers;
+
+	public List<LottoNumber> generateLottoNumbers(int lottoGameCount, NumberGenerator numberGenerator) {
+		lottoNumbers = new ArrayList<>();
+		for (int i = 0; i < lottoGameCount; i++) {
+			LottoNumber lottoNumber = new LottoNumber();
+			lottoNumber.generate(numberGenerator);
+			lottoNumbers.add(lottoNumber);
+		}
+		return lottoNumbers;
+	}
+}
