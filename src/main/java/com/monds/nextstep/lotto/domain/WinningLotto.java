56224f51bb9c5c837e1e3ac067652c0a969e3fa0
Sanguk Ahn <suahnn@gmail.com>
2020-12-23 02:55:13 +0900

    Step3 (#1164)
    * docs: 요구사항 추가
    
    * feat: 2등 기능 추가

--- src/main/java/com/monds/nextstep/lotto/domain/WinningLotto.java
+++ src/main/java/com/monds/nextstep/lotto/domain/WinningLotto.java
@@ -3,12 +3,14 @@ package com.monds.nextstep.lotto.domain;
 public class WinningLotto {
 
     private final Lotto winningLotto;
+    private final LottoNumber bonusNumber;
 
-    public WinningLotto(Lotto winningLotto) {
+    public WinningLotto(Lotto winningLotto, LottoNumber bonusNumber) {
         this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
     }
 
     public LottoRanking matching(Lotto lotto) {
-        return LottoRanking.valueOf(winningLotto.countOfMatch(lotto));
+        return LottoRanking.valueOf(winningLotto.countOfMatch(lotto), lotto.contains(bonusNumber));
     }
 }
