6f50c520c8cd1ec92a6685c5cc32e0f126745385
Do Yeon Kim <chikeem90@gmail.com>
2020-12-22 16:36:12 +0900

    test: 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,7 +11,7 @@ public class LottoGameTest {
 
 	@DisplayName("로또 구매 개수 계산 테스트")
 	@ParameterizedTest
-	@CsvSource({"14000,14", "1000,1", "10000,10"})
+	@CsvSource({"14000,14", "1000,1", "10000,10", "0,0", "999,0", "-1000,0"})
 	public void divideByThousandTest(int input, int expected) {
 		int actual = lotto.calculateLottoCount(input);
 		assertThat(expected).isEqualTo(actual);
