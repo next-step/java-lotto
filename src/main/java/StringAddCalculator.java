1f087d48b98e01e814ed1c6e7b30a7f7decf9080
minjikim <carrykim1126@gmail.com>
2020-12-22 15:08:24 +0900

    stringCalculator add seperate by comma, colon delimitor

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public int splitAndSum(String input) {
         int sum = 0;
@@ -10,6 +13,33 @@ public class StringAddCalculator {
         if (input.length() == 1) {
             return sum = Integer.parseInt(input);
         }
+
+        if (input.length() > 1 && input.contains(",")) {
+            if (input.contains(":")) {
+                String[] tokens = input.split(",|:");
+                for (String num : tokens) {
+                    sum += Integer.parseInt(num);
+                }
+                return sum;
+            }
+
+            String[] numbers = input.split(",");
+            for (String num : numbers) {
+                sum += Integer.parseInt(num);
+            }
+            return sum;
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+            for (String num:tokens) {
+                sum += Integer.parseInt(num);
+            }
+            return sum;
+        }
+
         return 0;
     }
 }
