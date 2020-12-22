2b33d26678b622c792f72cf9f3f9c4e92a1d8168
Do Yeon Kim <chikeem90@gmail.com>
2020-12-23 01:10:04 +0900

    test: LottoTest 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,34 +4,39 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+	@DisplayName("로또 match 테스트")
 	@Test
 	public void lottoMatchTest() {
 		int expected = 3;
 		Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-		Lotto winLotto = new Lotto(Arrays.asList(1, 2, 3, 10, 11, 12));
-		int actual = lotto.match(winLotto);
-		assertThat(expected).isEqualTo(actual);
+		WinLotto winLotto = new WinLotto(Arrays.asList(1, 2, 3, 10, 11, 12), 13);
+		MatchResult match = lotto.match(winLotto);
+		assertThat(expected).isEqualTo(match.getCount());
 	}
 
+	@DisplayName("로또 NULL값 테스트")
 	@Test
 	public void lottoNullTest() {
 		assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(null));
 	}
 
+	@DisplayName("로또 1 ~ 45 사이 숫자 테스트")
 	@Test
 	public void lottoOtherNumberTest() {
 		assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(Arrays.asList(-100, 1, 2, 3, 4, 5)));
 		assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(Arrays.asList(1000, 1, 2, 3, 4, 5)));
 	}
 
+	@DisplayName("로또 LottoRank 테스트")
 	@Test
 	public void lottoRankTest() {
-		LottoRank expected = LottoRank.FOURTH;
+		LottoRank expected = LottoRank.SECOND;
 		Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-		Lotto winLotto = new Lotto(Arrays.asList(1, 2, 3, 10, 11, 12));
+		WinLotto winLotto = new WinLotto(Arrays.asList(1, 2, 3, 4, 5, 12), 6);
 		LottoRank actual = lotto.rankLotto(winLotto);
 		assertThat(expected).isEqualTo(actual);
 	}
