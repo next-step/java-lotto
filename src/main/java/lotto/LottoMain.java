6d9bace7451818a0b28dd444b8ca472079024ecc
chh <chh2644@gmail.com>
2020-12-22 13:33:25 +0900

    feat : main 구현

---
+++ src/main/java/lotto/LottoMain.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import lotto.domain.LottoGame;
+
+public class LottoMain {
+
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
+    }
+}
