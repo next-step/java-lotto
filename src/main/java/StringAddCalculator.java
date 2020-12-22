5beac59fefdb74dce8122e7573bb8dfc078f1d7d
minjikim <carrykim1126@gmail.com>
2020-12-22 20:11:46 +0900

    stringCalculator refactoring

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -18,48 +18,50 @@ public class StringAddCalculator {
         if (input.length() > NUM_LENGTH && input.contains(COMMA)) {
             return calculatorColonCommaDelimitor(input, sum);
         }
-        Integer sum1 = calculateCustomDelimitor(input, sum);
-        if (sum1 != null) return sum1;
-
-        return 0;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            return calculateMatcherSum(sum, m);
+        }
+        return sum;
     }
 
     private int calculatorColonCommaDelimitor(String input, int sum) {
         if (input.contains(COLON)) {
             String[] tokens = input.split(",|:");
-            for (String num : tokens) {
-                sum += Integer.parseInt(num);
-            }
-            return sum;
+            return getSum(sum, tokens);
         }
 
-        return extractException(input, sum);
+        return commaDelimitor(input, sum);
     }
 
-    private int extractException(String input, int sum) {
+    private int commaDelimitor(String input, int sum) {
         String[] numbers = input.split(COMMA);
         for (String num : numbers) {
-            if (Integer.parseInt(num) < 0) {
-                throw new RuntimeException("음수가 전달되었습니다.");
-            }
-            if (!Pattern.matches("^[0-9]$", num)) {
-                throw new NumberFormatException("숫자 이외의 값이 전달되었습니다.");
-            }
+            extractException(num);
             sum += Integer.parseInt(num);
         }
         return sum;
     }
 
-    private Integer calculateCustomDelimitor(String input, int sum) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
-            for (String num:tokens) {
-                sum += Integer.parseInt(num);
-            }
-            return sum;
+    private void extractException(String num) {
+        if (Integer.parseInt(num) < 0) {
+            throw new RuntimeException("음수가 전달되었습니다.");
         }
-        return null;
+        if (!Pattern.matches("^[0-9]$", num)) {
+            throw new NumberFormatException("숫자 이외의 값이 전달되었습니다.");
+        }
+    }
+
+    private int calculateMatcherSum(int sum, Matcher m) {
+        String customDelimiter = m.group(1);
+        String[] tokens = m.group(2).split(customDelimiter);
+        return getSum(sum, tokens);
+    }
+
+    private int getSum(int sum, String[] tokens) {
+        for (String num : tokens) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
     }
 }
