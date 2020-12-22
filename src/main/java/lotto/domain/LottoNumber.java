d24d15181ba864d48c659bd3a335188ae8e951de
chh <chh2644@gmail.com>
2020-12-22 13:37:34 +0900

    feat : 로또 번호 객체 정의
    - 로또번호 가져오기 기능 구현
    - 생성된 로또 번호와 당첨 번호 비교 기능 구현

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+public class LottoNumber {
+    private final Integer number;
+
+    public LottoNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+}
