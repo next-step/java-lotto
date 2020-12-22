d4de1ba5c2f44132ae12891e73350c1e806447f7
Do Yeon Kim <chikeem90@gmail.com>
2020-12-23 01:20:57 +0900

    feat: MatchResult로 반환하도록 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,41 +4,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
-	private static final int LOTTO_START_NUMBER = 1;
-	private static final int LOTTO_END_NUMBER = 45;
-
 	private final List<Integer> lotto;
 
 	public Lotto(List<Integer> lotto) {
-		validateLotto(lotto);
 		this.lotto = lotto;
-	}
-
-	private void validateLotto(List<Integer> lotto) {
-		if (lotto == null) {
-			throw new IllegalArgumentException("로또 번호를 입력하지 않았습니다.");
-		}
-
-		boolean isValid = lotto.stream()
-			.anyMatch(number -> number < LOTTO_START_NUMBER || number > LOTTO_END_NUMBER);
-		if (isValid) {
-			throw new IllegalArgumentException("1부터 45사이 번호만 입력 가능합니다.");
-		}
 	}
 
 	public List<Integer> getLotto() {
 		return lotto;
 	}
 
-	protected int match(Lotto winLotto) {
-		return (int) lotto.stream()
-			.filter(winLotto.lotto::contains)
+	protected MatchResult match(WinLotto winLotto) {
+		int count = (int)lotto.stream()
+			.filter(winLotto.getWinLotto()::contains)
 			.count();
+		boolean matchBonus = false;
+		if (count == 5 && lotto.contains(winLotto.getBonus())) {
+			matchBonus = true;
+		}
+		return new MatchResult(count, matchBonus);
 	}
 
-	public LottoRank rankLotto(Lotto winLottoNumber) {
-		int sameNumberCount = match(winLottoNumber);
-		return LottoRank.valueOfCount(sameNumberCount);
+	public LottoRank rankLotto(WinLotto winLotto) {
+		MatchResult matchResult = match(winLotto);
+		return LottoRank.valueOf(matchResult.getCount(), matchResult.isMatchBonus());
 	}
 
 	@Override
