6193276ab90789baf69ce05c719bb94050041992
Yeoseon Yun <devyyskr@gmail.com>
2020-12-23 20:54:22 +0900

    refactor(LottoTierTest): White Space

--- src/test/java/step2/domain/LottoTierTest.java
+++ src/test/java/step2/domain/LottoTierTest.java
@@ -11,16 +11,22 @@ public class LottoTierTest {
     void getTier() {
         assertThat(LottoTier.getTier(6, true)).isEqualTo(LottoTier.FIRST);
         assertThat(LottoTier.getTier(6, false)).isEqualTo(LottoTier.FIRST);
+
         assertThat(LottoTier.getTier(5, true)).isEqualTo(LottoTier.SECOND);
         assertThat(LottoTier.getTier(5, false)).isEqualTo(LottoTier.THIRD);
+
         assertThat(LottoTier.getTier(4, true)).isEqualTo(LottoTier.FOURTH);
         assertThat(LottoTier.getTier(4, false)).isEqualTo(LottoTier.FOURTH);
+
         assertThat(LottoTier.getTier(3, true)).isEqualTo(LottoTier.FIFTH);
         assertThat(LottoTier.getTier(3, false)).isEqualTo(LottoTier.FIFTH);
+
         assertThat(LottoTier.getTier(2, true)).isEqualTo(LottoTier.NONE);
         assertThat(LottoTier.getTier(2, false)).isEqualTo(LottoTier.NONE);
+
         assertThat(LottoTier.getTier(1, true)).isEqualTo(LottoTier.NONE);
         assertThat(LottoTier.getTier(1, false)).isEqualTo(LottoTier.NONE);
+
         assertThat(LottoTier.getTier(0, true)).isEqualTo(LottoTier.NONE);
         assertThat(LottoTier.getTier(0, false)).isEqualTo(LottoTier.NONE);
     }
