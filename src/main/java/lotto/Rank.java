1b9898b958c3ae2cd0521051e688f20438b8138b
monkeyDugi <lbd4946@gmail.com>
2020-12-24 00:21:25 +0900

    refactor : 타입이 추가 되거나 변경 되어도, 로직 변경이 없도록 matchBonus를            상태로 가짐

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -5,30 +5,35 @@ import java.util.stream.Stream;
 
 public enum Rank {
 
-    MISS(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000);
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private static final boolean DEFAULT_MATCH_BONUS = false;
 
     private final int countOfMatch;
     private final int winningMoney;
+    private final boolean matchBonus;
 
     Rank(int countOfMatch, int winningMoney) {
+        this(countOfMatch, winningMoney, DEFAULT_MATCH_BONUS);
+    }
+
+    Rank(int countOfMatch, int winningMoney, boolean matchBonus) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
+        this.matchBonus = matchBonus;
     }
 
     public static Rank valueOf(int countOfMatch, boolean matchBonus) {
         validate(countOfMatch);
 
-        if (matchBonus) {
-            return SECOND;
-        }
-
         Stream<Rank> stream = Arrays.stream(values());
-        return stream.filter(rank -> countOfMatch == rank.getCountOfMatch())
+        return stream.filter(rank -> rank.equalsMatchCount(countOfMatch))
+                     .filter(rank -> rank.isMatchBonus(matchBonus))
                 .findFirst()
                 .orElse(MISS);
     }
@@ -45,6 +50,10 @@ public enum Rank {
 
     public boolean equalsMatchCount(int countOfMatch) {
         return this.countOfMatch == countOfMatch;
+    }
+
+    private boolean isMatchBonus(boolean matchBonus) {
+        return this.matchBonus == matchBonus;
     }
 
     public int getCountOfMatch() {
