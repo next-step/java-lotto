9dc8dde35581aa6f4d38faaed422f93c792d5278
Donghwan Noh <namul.bap@gmail.com>
2020-12-24 03:21:07 +0900

    feat(view): 사용자로부터 수동으로 추첨 번호를 입력받음
    InputView에 메소드 추가
    - inputManualNumbers: 올바른 번호를 입력받을 때까지 사용자로부터 추첨 번호를 입력받음
    - inputManualNumbersList: 파라미터로 받은 숫자만큼 inputManualNumbers를 호출하여 추첨번호 리스트를 만들어 리턴함
    
    LottoController에 isValidLottoNumbers 메소드 추가
    - View에서 Domain에 의존적이지 않게 숫자들의 유효성을 검증할 수 있도록 함.
    
    BREAKING CHANGE:
    - LottoController: InputView의 inputManualNumbersList 메소드에 수동으로 구입할 로또의 장수를 파라미터로 넘겨, 그 개수만큼 수동으로 추첨번호를 입력받을 수 있도록 수정함.

--- src/main/java/edu/nextstep/lotto/LottoController.java
+++ src/main/java/edu/nextstep/lotto/LottoController.java
@@ -5,15 +5,23 @@ import java.util.List;
 import edu.nextstep.lotto.domain.LottoGame;
 import edu.nextstep.lotto.domain.Money;
 import edu.nextstep.lotto.domain.sub.LottoGameResult;
+import edu.nextstep.lotto.domain.sub.LottoNumbers;
 import edu.nextstep.lotto.util.NumberUtil;
 import edu.nextstep.lotto.view.InputView;
 import edu.nextstep.lotto.view.ResultView;
 
 public class LottoController {
+
+	public static boolean isValidLottoNumbers(List<Integer> numbers) {
+		return LottoNumbers.isValid(numbers);
+	}
+
 	public void play() {
 		Money money = new Money(InputView.inputMoney());
 		int numberOfManual = InputView.inputManualPurchaseNumber();
 		money.purchaseLotto(numberOfManual);
+		List<List<Integer>> manualNumbersList = InputView.inputManualNumbersList(numberOfManual);
+
 		int numberOfPurchase = money.howManyPurchase();
 
 		ResultView.printHowManyPurchase(numberOfPurchase);
--- src/main/java/edu/nextstep/lotto/view/InputView.java
+++ src/main/java/edu/nextstep/lotto/view/InputView.java
@@ -1,10 +1,13 @@
 package edu.nextstep.lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import edu.nextstep.lotto.LottoController;
 
 public class InputView {
 	public static Scanner scanner = new Scanner(System.in);
@@ -23,9 +26,14 @@ public class InputView {
 
 	public static List<Integer> inputWinningNumbers() {
 		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		return inputLottoNumbers();
+	}
+
+	public static List<Integer> inputLottoNumbers() {
 		String input = scanner.nextLine();
 		if (!Pattern.matches("\\s*\\d+\\s*(,\\s*\\d+\\s*)*", input)) {
-			return inputWinningNumbers();
+			System.err.println("올바른 로또 번호가 아닙니다. 다시 입력해주세요.");
+			return inputLottoNumbers();
 		}
 		return Arrays.stream(input.split(","))
 			.map(String::trim)
@@ -49,5 +57,22 @@ public class InputView {
 			return inputManualPurchaseNumber();
 		}
 		return Integer.parseInt(input);
+	}
+
+	public static List<List<Integer>> inputManualNumbersList(int numberOfManualPurchase) {
+		System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+		List<List<Integer>> lottoNumbersList = new ArrayList<>();
+		for (int i = 0; i < numberOfManualPurchase; i++) {
+			lottoNumbersList.add(inputManualNumbers());
+		}
+		return lottoNumbersList;
+	}
+
+	private static List<Integer> inputManualNumbers() {
+		List<Integer> lottoNumbers;
+		while (!LottoController.isValidLottoNumbers(lottoNumbers = InputView.inputLottoNumbers())) {
+			System.err.println("올바른 로또 번호가 아닙니다. 다시 입력해주세요.");
+		}
+		return lottoNumbers;
 	}
 }
