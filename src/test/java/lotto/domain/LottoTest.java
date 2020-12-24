7cc5fbb228c689e3a0cc5f39e6417dfdaa3636fc
Do Yeon Kim <chikeem90@gmail.com>
2020-12-25 00:40:23 +0900

    test: LottoNumber 추가 대응 및 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,18 +2,46 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+	WinLotto winLotto = new WinLotto(new Lotto(new ArrayList<>(Arrays.asList(
+		new LottoNumber(1),
+		new LottoNumber(2),
+		new LottoNumber(3),
+		new LottoNumber(4),
+		new LottoNumber(5),
+		new LottoNumber(6)
+	))), new LottoNumber(7));
+
+	@DisplayName("로또 5개만 구성하는 경우 테스트")
+	@Test
+	public void lottoLengthTest() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(new ArrayList<>(Arrays.asList(
+			new LottoNumber(1),
+			new LottoNumber(2),
+			new LottoNumber(3),
+			new LottoNumber(10),
+			new LottoNumber(11)
+		))));
+	}
+
 	@DisplayName("로또 match 테스트")
 	@Test
 	public void lottoMatchTest() {
 		int expected = 3;
-		Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-		WinLotto winLotto = new WinLotto(Arrays.asList(1, 2, 3, 10, 11, 12), 13);
+		Lotto lotto = new Lotto(new ArrayList<>(Arrays.asList(
+			new LottoNumber(1),
+			new LottoNumber(2),
+			new LottoNumber(3),
+			new LottoNumber(8),
+			new LottoNumber(9),
+			new LottoNumber(10)
+		)));
 		MatchResult match = lotto.match(winLotto);
 		assertThat(expected).isEqualTo(match.getCount());
 	}
@@ -22,8 +50,14 @@ class LottoTest {
 	@Test
 	public void lottoRankTest() {
 		LottoRank expected = LottoRank.SECOND;
-		Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-		WinLotto winLotto = new WinLotto(Arrays.asList(1, 2, 3, 4, 5, 12), 6);
+		Lotto lotto = new Lotto(new ArrayList<>(Arrays.asList(
+			new LottoNumber(1),
+			new LottoNumber(2),
+			new LottoNumber(3),
+			new LottoNumber(4),
+			new LottoNumber(5),
+			new LottoNumber(7)
+		)));
 		LottoRank actual = lotto.rankLotto(winLotto);
 		assertThat(expected).isEqualTo(actual);
 	}
