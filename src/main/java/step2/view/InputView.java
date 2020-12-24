851c83af9d54cb42e3efacf9cb264b7e9f5785e4
Mingyu Lee <leemingyu05@gmail.com>
2020-12-24 19:11:56 +0900

    refactor: InputView에서 Integer 파싱 중복코드 제거

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -1,7 +1,8 @@
 package step2.view;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 import step2.domain.LottoNumber;
 import step2.domain.LottoNumbers;
@@ -17,17 +18,12 @@ public class InputView {
 	public int inputPurchasePrice() {
 		final String messageOfInputPurchasePrice = "구입금액을 입력해 주세요.";
 		final String messageOfResultPurchasePrice = "%d개를 구매했습니다.";
-		int purchaseAmount;
 
 		System.out.println(messageOfInputPurchasePrice);
 
-		try {
-			purchaseAmount = Integer.parseInt(scanner.nextLine());
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("숫자만 입력 가능합니다.");
-		}
-
+		int purchaseAmount = parseStringToInteger(scanner.nextLine());
 		validate(purchaseAmount);
+
 		System.out.printf((messageOfResultPurchasePrice) + "%n", purchaseAmount);
 
 		return purchaseAmount;
@@ -42,36 +38,33 @@ public class InputView {
 	public LottoNumbers inputWinNumbers() {
 		final String WIN_NUMBERS_DELIMITER = ", ";
 		final String messageOfInputWinNumbers = "지난 주 당첨 번호를 입력해 주세요";
-		LottoNumbers winNumbers;
+		Set<LottoNumber> winNumbers = new HashSet<>();
 
 		System.out.println(messageOfInputWinNumbers);
 
 		final String input = scanner.nextLine();
 
-		try {
-			winNumbers = new LottoNumbers(Arrays.stream(input.split(WIN_NUMBERS_DELIMITER))
-				.mapToInt(Integer::parseInt)
-				.boxed()
-				.toArray(Integer[]::new));
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+		for (String numberStr : input.split(WIN_NUMBERS_DELIMITER)) {
+			winNumbers.add(new LottoNumber(parseStringToInteger(numberStr)));
 		}
 
 		System.out.println();
 
-		return winNumbers;
+		return new LottoNumbers(winNumbers);
 	}
 
 	public LottoNumber inputBonusNumber() {
 		final String messageOfInputBonusBall = "보너스 볼을 입력해 주세요.";
 		System.out.println(messageOfInputBonusBall);
-		LottoNumber bonusNumber;
+		return new LottoNumber(parseStringToInteger(scanner.nextLine()));
+	}
+
+	private Integer parseStringToInteger(final String input) {
 		try {
-			bonusNumber = new LottoNumber(Integer.parseInt(scanner.nextLine()));
+			return Integer.parseInt(input);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("숫자만 입력 가능합니다.");
 		}
-
-		return bonusNumber;
 	}
+
 }
