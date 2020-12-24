d930281298924d92d3131c70aae183b600aad046
charllossdev <charlloss.dev@gmail.com>
2020-12-24 14:02:01 +0900

    refactor: 로또 구입 금액 money 객체 리펙토링

--- src/main/java/lotto/modal/Money.java
+++ src/main/java/lotto/modal/Money.java
@@ -5,53 +5,23 @@ import java.util.Objects;
 
 import lotto.util.StringValid;
 
-public class GameCondition {
+public class Money {
 
 	private static final int LOTTO_PRICE_MONEY = 1000;
 
 	private final int userMoney;
-	private final int count;
 
-	public GameCondition(String money) {
-		this(money, "0");
-	}
-
-	public GameCondition(String money, String count) {
+	public Money(String money) {
 		this.userMoney = setCheckMoney(money);
-		this.count = setCheckCount(count);
 	}
 
-	public int getCount() {
-		return this.count;
-	}
-
-	public int getRepeatCount() {
-		return this.userMoney / LOTTO_PRICE_MONEY;
-	}
-
-	public int getRandomCount() {
-		return this.getRepeatCount() - this.count;
+	public int getBuyCount() {
+		return userMoney / LOTTO_PRICE_MONEY;
 	}
 
 	public BigDecimal getYield(long totalPrize) {
 		BigDecimal money = new BigDecimal(this.userMoney);
 		return new BigDecimal(totalPrize).divide(money, 2, BigDecimal.ROUND_HALF_UP);
-	}
-
-	private int setCheckCount(String count) {
-		if (StringValid.isEmptyStr(count)) {
-			throw new IllegalArgumentException("수동 반복 횟수가 입력되지 않았습니다.");
-		}
-
-		if (StringValid.isNoNNumber(count)) {
-			throw new IllegalArgumentException("수동 반복 횟수을 잘못 입력하였습니다. 숫자만 입력해 주세요.");
-		}
-		int result = Integer.parseInt(count);
-
-		if (result * LOTTO_PRICE_MONEY > this.userMoney) {
-			throw new IllegalArgumentException("금액이 부족합니다.");
-		}
-		return result;
 	}
 
 	private int setCheckMoney(String userInputMoney) throws IllegalArgumentException {
@@ -76,12 +46,12 @@ public class GameCondition {
 			return true;
 		if (o == null || getClass() != o.getClass())
 			return false;
-		GameCondition that = (GameCondition)o;
-		return userMoney == that.userMoney && count == that.count;
+		Money money = (Money)o;
+		return userMoney == money.userMoney;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(userMoney, count);
+		return Objects.hash(userMoney);
 	}
 }
