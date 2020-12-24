6f4f0e6ea2bb42807f349ff8c4604e58263a359d
charllossdev <charlloss.dev@gmail.com>
2020-12-24 14:18:32 +0900

    fix: 로또구입 갯수(전체갯수, 수동구매갯수) 벨리데이션 및 가독성 수정

--- src/main/java/lotto/modal/Count.java
+++ src/main/java/lotto/modal/Count.java
@@ -1,28 +1,19 @@
 package lotto.modal;
 
-import lotto.util.StringValid;
-
 public class Count {
 
-	private final int count;
+	private final int manualBuyCount;
+	private final int buyCount;
 
-	public Count(String count, Money money) {
-		this.count = setCheckCount(count);
+	public Count(int count, int buyCount) {
+		validateCount(count, buyCount);
+		this.buyCount = buyCount;
+		this.manualBuyCount = count;
 	}
 
-	private int setCheckCount(String count) {
-		if (StringValid.isEmptyStr(count)) {
-			throw new IllegalArgumentException("수동 반복 횟수가 입력되지 않았습니다.");
-		}
-
-		if (StringValid.isNoNNumber(count)) {
-			throw new IllegalArgumentException("수동 반복 횟수을 잘못 입력하였습니다. 숫자만 입력해 주세요.");
-		}
-		int result = Integer.parseInt(count);
-
-		if (result * LOTTO_PRICE_MONEY > this.userMoney) {
+	private void validateCount(int count, int buyCount) {
+		if (count > buyCount) {
 			throw new IllegalArgumentException("금액이 부족합니다.");
 		}
-		return result;
 	}
 }
