7cf7398c95b262c1c2e1c68535924a69b32ceabb
charllossdev <charlloss.dev@gmail.com>
2020-12-24 14:17:31 +0900

    fix: 로또구입 금액 벨리데이션 및 가독성 수정

--- src/main/java/lotto/modal/Money.java
+++ src/main/java/lotto/modal/Money.java
@@ -3,41 +3,30 @@ package lotto.modal;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-import lotto.util.StringValid;
-
 public class Money {
 
 	private static final int LOTTO_PRICE_MONEY = 1000;
 
-	private final int userMoney;
+	private final int inputMoney;
 
-	public Money(String money) {
-		this.userMoney = setCheckMoney(money);
+	public Money(int money) {
+		validateMoney(money);
+		this.inputMoney = money;
+	}
+
+	private void validateMoney(int money) {
+		if (money < LOTTO_PRICE_MONEY) {
+			throw new IllegalArgumentException("로또 구매 최소 금액은 " + LOTTO_PRICE_MONEY + "원 입니다.");
+		}
 	}
 
 	public int getBuyCount() {
-		return userMoney / LOTTO_PRICE_MONEY;
+		return inputMoney / LOTTO_PRICE_MONEY;
 	}
 
 	public BigDecimal getYield(long totalPrize) {
-		BigDecimal money = new BigDecimal(this.userMoney);
+		BigDecimal money = new BigDecimal(this.inputMoney);
 		return new BigDecimal(totalPrize).divide(money, 2, BigDecimal.ROUND_HALF_UP);
-	}
-
-	private int setCheckMoney(String userInputMoney) throws IllegalArgumentException {
-		if (StringValid.isEmptyStr(userInputMoney)) {
-			throw new IllegalArgumentException("돈(금액)이 입력되지 않았습니다.");
-		}
-
-		if (StringValid.isNoNNumber(userInputMoney)) {
-			throw new IllegalArgumentException("돈(금액) 형식을 잘못 입력하였습니다. 숫자만 입력해 주세요.");
-		}
-		int result = Integer.parseInt(userInputMoney);
-
-		if (result < LOTTO_PRICE_MONEY) {
-			throw new IllegalArgumentException("로또 구매 최소 금액은 " + LOTTO_PRICE_MONEY + "원 입니다.");
-		}
-		return result;
 	}
 
 	@Override
@@ -47,11 +36,11 @@ public class Money {
 		if (o == null || getClass() != o.getClass())
 			return false;
 		Money money = (Money)o;
-		return userMoney == money.userMoney;
+		return this.inputMoney == money.inputMoney;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(userMoney);
+		return Objects.hash(inputMoney);
 	}
 }
