073841231aecaa4c20b1659a427e672994eae2fe
charlloss.dev <charlloss.dev@gmail.com>
2020-12-25 01:57:16 +0900

    refactor: winner lotto 벨리데이션 및 리펙토링

--- src/main/java/lotto/modal/WinnerLotto.java
+++ src/main/java/lotto/modal/WinnerLotto.java
@@ -5,22 +5,15 @@ public class WinnerLotto {
 	private final Lotto winner;
 	private final LottoNumber bonusNumber;
 
-	public WinnerLotto(final Lotto winnerLotto, final LottoNumber bonusNumber) {
+	private WinnerLotto(final Lotto winnerLotto, final LottoNumber bonusNumber) {
 
-		validationWinnerLotto(winnerLotto, bonusNumber);
 		this.winner = winnerLotto;
 		this.bonusNumber = bonusNumber;
 	}
 
-	private void validationWinnerLotto(Lotto winnerLotto, LottoNumber bonusNumber) {
-
-		if (winnerLotto == null || bonusNumber == null) {
-			throw new IllegalArgumentException("당첨번호 or 보너스 번호가 잘못 입력되었습니다.");
-		}
-
-		if (winnerLotto.isContainNumber(bonusNumber)) {
-			throw new IllegalArgumentException("당첨번호와 중복된 보너스 번호를 입력햐였습니다.");
-		}
+	public static WinnerLotto generateWinner(Lotto winnerLotto, LottoNumber bonusNumber) {
+		validationWinnerLotto(winnerLotto, bonusNumber);
+		return new WinnerLotto(winnerLotto, bonusNumber);
 	}
 
 	public int getMatchCount(Lotto compareLotto) {
@@ -39,4 +32,16 @@ public class WinnerLotto {
 	public boolean isContainBonus(Lotto lotto) {
 		return lotto.getLotto().contains(bonusNumber);
 	}
+
+	private static void validationWinnerLotto(Lotto winnerLotto, LottoNumber bonusNumber) {
+
+		if (winnerLotto == null || bonusNumber == null) {
+			throw new IllegalArgumentException("당첨번호 or 보너스 번호가 잘못 입력되었습니다.");
+		}
+
+		if (winnerLotto.isContainNumber(bonusNumber)) {
+			throw new IllegalArgumentException("당첨번호와 중복된 보너스 번호를 입력햐였습니다.");
+		}
+	}
+
 }
