698481602025086b2bfabd9dd9be332391c428f2
Do Yeon Kim <chikeem90@gmail.com>
2020-12-26 18:36:19 +0900

    refactor: 입력값 validate 추가

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -7,10 +7,14 @@ public class LottoGame {
 	private static final int LOTTO_PRICE = 1000;
 
 	public int calculateLottoCount(int input) {
-		if (input <= 0) {
-			return 0;
-		}
+		validateInput(input);
 		return input / LOTTO_PRICE;
+	}
+
+	private void validateInput(int input) {
+		if (input < LOTTO_PRICE) {
+			throw new IllegalArgumentException(LOTTO_PRICE + "원 이상 입력 가능합니다.");
+		}
 	}
 
 	public LottoTicket generateLottoNumbers(int lottoGameCount, NumberGenerator numberGenerator) {
@@ -21,5 +25,11 @@ public class LottoGame {
 		return lottoTicket.getLottoTicket().stream()
 			.map(lottoNumber -> lottoNumber.rankLotto(winLotto))
 			.collect(Collectors.groupingBy(lottoRank -> lottoRank, Collectors.counting()));
+	}
+
+	public void validateInput(int lottoTotalCount, int lottoManualCount) {
+		if (lottoManualCount > lottoTotalCount) {
+			throw new IllegalArgumentException("총 로또구매개수보다 많습니다. (총 로또구매개수 : " + lottoTotalCount + ", 수동 로또구매개수 : " + lottoManualCount);
+		}
 	}
 }
