affe6365b6468a14247521c1c0ec365ccdc01c16
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-27 17:29:13 +0900

    fix : 하나의 함수 라인 줄이기

--- src/main/java/lotto/LottoTVShow.java
+++ src/main/java/lotto/LottoTVShow.java
@@ -11,6 +11,12 @@ import lotto.view.ResultView;
 import static lotto.view.InputView.printInputManualLotto;
 
 public class LottoTVShow {
+	private LottoBuyCount lottoTotalBuyCount;
+
+	private LottoBuyCount manualLottoCount;
+
+	private LottoBuyCount autoLottoCount;
+
 	private Lotto lotto = new Lotto();
 
 	private LottoPrizeAnalysisResult lottoPrizeAnalysisResult = new LottoPrizeAnalysisResult();
@@ -18,19 +24,21 @@ public class LottoTVShow {
 	private static final Money LOTTO_PRICE_PER_SHEET = new Money(1000);
 
 	public void start() {
-		LottoBuyCount lottoTotalBuyCount = printAutoLottoNotice();
-		LottoBuyCount manualLottoCount = printManualLottoNotice();
-		LottoBuyCount autoLottoCount = lottoTotalBuyCount.getAutoLottoCount(manualLottoCount);
-		LottoList lottoList = printInputManualLotto(manualLottoCount);
-		InputView.printPurchaseLottoCount(autoLottoCount, manualLottoCount);
-
-		lottoList = lotto.generateAutoLotto(autoLottoCount, lottoList);
+		LottoList lottoList = processUserInput();
+		lottoList = lotto.generateAutoLotto(this.autoLottoCount, lottoList);
 		InputView.printPurchaseLottoNumber(lottoList);
-
 		LottoMatchResultList lottoMatchResultList = processLotto(lottoList);
-
 		this.lottoPrizeAnalysisResult.arrangePrize(lottoMatchResultList, LOTTO_PRICE_PER_SHEET);
 		ResultView.printResult(lottoMatchResultList, lottoPrizeAnalysisResult);
+	}
+
+	private LottoList processUserInput() {
+		this.lottoTotalBuyCount = printAutoLottoNotice();
+		this.manualLottoCount = printManualLottoNotice();
+		this.autoLottoCount = lottoTotalBuyCount.getAutoLottoCount(this.manualLottoCount);
+		InputView.printPurchaseLottoCount(this.autoLottoCount, this.manualLottoCount);
+		LottoList lottoList = printInputManualLotto(this.manualLottoCount);
+		return lottoList;
 	}
 
 	private LottoBuyCount printManualLottoNotice() {
@@ -49,7 +57,7 @@ public class LottoTVShow {
 		return this.compareLastWeekPrizeNumberWithPurchasedLottoNumber(lastWeekPrizeNumbers, purchasedLottos, bonusNumber);
 	}
 
-	public LottoMatchResultList compareLastWeekPrizeNumberWithPurchasedLottoNumber(Lotto lastWeekPrizeNumbers,LottoList purchasedLottos, LottoNumber bonusNumber) {
+	public LottoMatchResultList compareLastWeekPrizeNumberWithPurchasedLottoNumber(Lotto lastWeekPrizeNumbers, LottoList purchasedLottos, LottoNumber bonusNumber) {
 		LottoMatchResultList lottoResultList = new LottoMatchResultList();
 		for (Lotto lotto : purchasedLottos.getLottoList()) {
 			HasBonusNumber isMatchBonus = LottoPrizePolicy.isMatchBonusNumber(lotto, bonusNumber);
