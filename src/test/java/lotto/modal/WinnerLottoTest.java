f2d631b4f0441a9b61f55c3e04a0c6847fd450ab
charlloss.dev <charlloss.dev@gmail.com>
2020-12-28 16:20:03 +0900

    test: 로또 추첨번호 생성 및 당첨 케이스 테스트

--- src/test/java/lotto/modal/WinnerLottoTest.java
+++ src/test/java/lotto/modal/WinnerLottoTest.java
@@ -8,45 +8,50 @@ import org.junit.jupiter.api.Test;
 
 class WinnerLottoTest {
 
-	WinnerLotto winnerLotto;
+	private Lotto lotto;
+	private LottoNumber bonusNumber;
+	private WinnerLotto winnerLotto;
 
 	@BeforeEach
 	void init() {
-		winnerLotto = new WinnerLotto(Lotto.generateLotto("1,2,3,4,5,6"), new LottoNumber(7));
+
+		lotto = Lotto.generateManualLotto("1,2,3,4,5,6");
+		bonusNumber = LottoNumber.generateNumber(7);
+		winnerLotto = WinnerLotto.generateWinner(lotto, bonusNumber);
 	}
 
 	@Test
 	@DisplayName("당첨 로또 객체: 당첨번호 6개 일치 테스트")
 	void compare1ThLottoTest() {
-		Lotto userLott = Lotto.generateLotto("1,2,3,4,5,6");
+		Lotto userLott = Lotto.generateManualLotto("1,2,3,4,5,6");
 		assertThat(winnerLotto.getMatchCount(userLott)).isEqualTo(6);
 	}
 
 	@Test
 	@DisplayName("당첨 로또 객체: 당첨번호 5개 일치 + 보너스 테스트")
 	void compare2ThLottoTest() {
-		Lotto userLott = Lotto.generateLotto("1,2,3,4,5,7");
+		Lotto userLott = Lotto.generateManualLotto("1,2,3,4,5,7");
 		assertThat(winnerLotto.getMatchCount(userLott)).isEqualTo(5);
 	}
 
 	@Test
 	@DisplayName("당첨 로또 객체: 당첨번호 5개 일치 테스트")
 	void compare3ThLottoTest() {
-		Lotto userLott = Lotto.generateLotto("1,2,3,4,5,45");
+		Lotto userLott = Lotto.generateManualLotto("1,2,3,4,5,45");
 		assertThat(winnerLotto.getMatchCount(userLott)).isEqualTo(5);
 	}
 
 	@Test
 	@DisplayName("당첨 로또 객체: 당첨번호 4개 일치 테스트")
 	void compare4ThLottoTest() {
-		Lotto userLott = Lotto.generateLotto("1,2,3,5,37,45");
+		Lotto userLott = Lotto.generateManualLotto("1,2,3,5,37,45");
 		assertThat(winnerLotto.getMatchCount(userLott)).isEqualTo(4);
 	}
 
 	@Test
 	@DisplayName("당첨 로또 객체: 당첨번호 3개 일치 테스트")
 	void compare5ThLottoTest() {
-		Lotto userLott = Lotto.generateLotto("1,2,3,34,37,45");
+		Lotto userLott = Lotto.generateManualLotto("1,2,3,34,37,45");
 		assertThat(winnerLotto.getMatchCount(userLott)).isEqualTo(3);
 	}
 
@@ -57,17 +62,20 @@ class WinnerLottoTest {
 		// 1. 당첨 Lotto 객체가 null 이 아니여야 한다.
 		// 2. 당첨 보너스 번호가 null 이 아니여야 한다.
 		// 3. 당첨 Lotto 객체와 보너스 번호가 동일한 숫자를 가지면 안된다.
-		assertThatIllegalArgumentException().isThrownBy(() -> new WinnerLotto(null, null));
-		assertThatIllegalArgumentException().isThrownBy(() -> new WinnerLotto(new Lotto(), null));
-		assertThatIllegalArgumentException().isThrownBy(() -> new WinnerLotto(null, new LottoNumber(2)));
+		assertThatIllegalArgumentException().isThrownBy(() -> WinnerLotto.generateWinner(null, null));
 		assertThatIllegalArgumentException().isThrownBy(
-			() -> new WinnerLotto(Lotto.generateLotto("1,2,3,4,5,6"), new LottoNumber(2)));
+			() -> WinnerLotto.generateWinner(Lotto.generateRandomLotto(), null));
+		assertThatIllegalArgumentException().isThrownBy(
+			() -> WinnerLotto.generateWinner(null, LottoNumber.generateNumber(2)));
+		assertThatIllegalArgumentException().isThrownBy(
+			() -> WinnerLotto.generateWinner(Lotto.generateManualLotto("1,2,3,4,5,6"), LottoNumber.generateNumber(2)));
 	}
 
 	@Test
 	@DisplayName("당첨 로또 객체: 객체 생성 테스트")
 	void initTest() {
-		WinnerLotto winnerLotto = new WinnerLotto(Lotto.generateLotto("1,2,3,4,5,6"), new LottoNumber(7));
-		assertThat(winnerLotto).isNotNull();
+
+		WinnerLotto winnerLottoTest = WinnerLotto.generateWinner(lotto, bonusNumber);
+		assertThat(winnerLottoTest).isNotNull();
 	}
 }
