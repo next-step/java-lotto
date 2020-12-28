c96eadf1974b8ac0c9ccc73fc91d251f90e2a500
charlloss.dev <charlloss.dev@gmail.com>
2020-12-28 15:56:08 +0900

    test: 로또 생성 테스트

--- src/test/java/lotto/modal/LottoTest.java
+++ src/test/java/lotto/modal/LottoTest.java
@@ -11,15 +11,15 @@ class LottoTest {
 	@DisplayName("로또: 메뉴얼 로또 객체 생성 테스트")
 	void initManualLottoTest() {
 
-		Lotto lotto = Lotto.generateLotto("1,3,23,34,35,36");
+		Lotto lotto = Lotto.generateManualLotto("1,3,23,34,35,36");
 
-		assertThat(lotto).isEqualTo(Lotto.generateLotto("1,3,23,34,35,36"));
+		assertThat(lotto).isEqualTo(Lotto.generateManualLotto("1,3,23,34,35,36"));
 	}
 
 	@Test
 	@DisplayName("로또: 랜덤 로또 객체 생성 테스트")
 	void initRandomLottoTest() {
-		Lotto lotto = new Lotto();
+		Lotto lotto = Lotto.generateRandomLotto();
 		lotto.getLotto()
 			.forEach(System.out::println);
 
