e5faad07a09a729502980b06d0a75a51ad60e03c
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-12-29 13:27:13 +0900

    refactor - 1~45 체크하는 조건문 오류 수정

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -12,7 +12,7 @@ public class Number {
     }
 
     private void validateNumberRange(int number) {
-        if(CommonConstants.LOTTO_NUMBER_MIN >= number || CommonConstants.LOTTO_NUMBER_MAX <= number) {
+        if(CommonConstants.LOTTO_NUMBER_MIN > number || CommonConstants.LOTTO_NUMBER_MAX < number) {
             CommonException.IllegalArgumentException(Message.LOTTO_NUMBER_RANGE_EXCESS);
         }
         this.number = number;
