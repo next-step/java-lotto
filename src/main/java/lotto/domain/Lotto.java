727f2d066578bcdc8d811ccd25f1f1f992a44d3e
chh <chh2644@gmail.com>
2021-01-02 23:05:05 +0900

    feat : 로또 객체 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,35 +1,55 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<LottoNumber> lotto;
+    private final Set<LottoNumber> lotto = new HashSet<>();
+    private static final int LOTTO_LENGTH = 6;
 
     public Lotto(List<Integer> lotto) {
-        this.lotto = changeNumbersToLottoNumber(lotto);
+        for (Integer number : lotto) {
+            this.lotto.add(new LottoNumber(number));
+        }
+
+        if(this.lotto.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("중복없는 6자리의 수를 입력해주세요.");
+        }
     }
 
-    public List<LottoNumber> changeNumbersToLottoNumber(List<Integer> numbers) {
-        return numbers.stream()
-                .map(LottoNumber::new)
+    public long match(Lotto winningLotto) {
+        long count = 0;
+        for (LottoNumber lottoNumber : lotto) {
+            if (winningLotto.contains(lottoNumber)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean contains(LottoNumber lottoNumber) {
+        return this.lotto.contains(lottoNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto1 = (Lotto) o;
+        return Objects.equals(lotto, lotto1.lotto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotto);
+    }
+
+    public List<Integer> getLotto() {
+        return this.lotto.stream().map(LottoNumber::getNumber)
                 .collect(Collectors.toList());
-    }
-
-    public List<Integer> convertLottoNumbers() {
-        return lotto.stream()
-                .map(LottoNumber::getNumber)
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * 생성된 로또 번호와 지난주 당첨 번호 중 같은 번호가 있는지 확인 후 갯수 반환
-     * @param targetNumbers 지난주 당첨 번호
-     * @return long
-     */
-    public long getEqualCount(List<Integer> targetNumbers) {
-        return lotto.stream()
-                .filter(lottoNumber -> targetNumbers.contains(lottoNumber.getNumber()))
-                .count();
     }
 }
