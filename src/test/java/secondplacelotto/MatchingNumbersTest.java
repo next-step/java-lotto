251d80296e5f5e895bbd5d31ba61247c59d95f0a
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2021-01-04 05:10:53 +0900

    로또 매칭 번호 테스트 코드 구현

---
+++ src/test/java/secondplacelotto/MatchingNumbersTest.java
@@ -0,0 +1,32 @@
+package secondplacelotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import secondplacelotto.model.BonusNumber;
+import secondplacelotto.model.MatchingNumbers;
+
+public class MatchingNumbersTest {
+
+    @Test
+    @DisplayName("매칭 기준들의 갯수를 잘 가져오는가")
+    public void getMatchingNumberCount() {
+        assertTrue(MatchingNumbers.getMatchingNumbersCount() == 5);
+    }
+
+    @Test
+    @DisplayName("당첨번호 매칭 카운트가 제대로 되는가")
+    public void matchWinnerNumber() {
+        BonusNumber bonusNumber = new BonusNumber(38);
+
+        MatchingNumbers.addMatchingCount("5", bonusNumber);
+        assertEquals(MatchingNumbers.getMatchingCount(2), 1);
+
+        bonusNumber.match(38,5);
+        MatchingNumbers.addMatchingCount("5", bonusNumber);
+        assertEquals(MatchingNumbers.getMatchingCount(3), 1);
+    }
+}
