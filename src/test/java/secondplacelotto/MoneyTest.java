6fba9a73560b307896ecc958213002a6b6c9ff85
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2021-01-04 05:11:07 +0900

    돈 객체 테스트 코드 구현

--- src/test/java/secondplacelotto/MoneyTest.java
+++ src/test/java/secondplacelotto/MoneyTest.java
@@ -2,14 +2,18 @@ package secondplacelotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import secondplacelotto.exception.isNotEnoughMoneyException;
+import secondplacelotto.model.Money;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MoneyTest {
 
     @Test
-    @DisplayName("구매 가능한 로또를 제대로 가져오는")
-    public void (){
-        Money money = new Money(10000);
+    @DisplayName("구매 가능한 로또를 제대로 가져오는가")
+    public void computeBuyableLotto() {
 
-        assertEquals(money.ComputeBuyableLotto(), 10);
+        assertThrows(isNotEnoughMoneyException.class, () ->
+                new Money(500));
     }
 }
