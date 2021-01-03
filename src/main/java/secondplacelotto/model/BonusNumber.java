b5c2a6631a8eaade5924d2acb09cb997e08f0bf7
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2021-01-04 05:12:56 +0900

    보너스 번호 객체 구현

---
+++ src/main/java/secondplacelotto/model/BonusNumber.java
@@ -0,0 +1,32 @@
+package secondplacelotto.model;
+
+import secondplacelotto.exception.isNotScopeInTheNumberException;
+
+public class BonusNumber {
+    
+    private final int number;
+    private boolean matchingStatus = false;
+    
+    public BonusNumber(int number){
+        if (!(0 < number && number < 46)) {
+           throw new isNotScopeInTheNumberException("보너스번호는 1부터 45까지 가능합니다");
+        }
+
+        this.number = number;
+    }
+
+    public void match(int lottoNumber, int matchingCount) {
+        if (lottoNumber == number && matchingCount == 5) {
+            matchingStatus = true;
+        }
+    }
+
+    public boolean isMatching() {
+        return matchingStatus;
+    }
+
+    public void refreshMatchingStatus() {
+        matchingStatus = false;
+    }
+
+}
