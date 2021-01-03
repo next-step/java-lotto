7b71688d4f8076d10547e5d64d67abf3e235d2d4
mkkim90 <mkzzang0928@gmail.com>
2021-01-03 21:02:01 +0900

    feat : 보너스 랭킹 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -7,15 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RankTest {
     @Test
     void rank() {
-        assertThat(Rank.valueOfRank(6)).isEqualTo(Rank.FIRST);
-        assertThat(Rank.valueOfRank(5)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.valueOfRank(4)).isEqualTo(Rank.THIRD);
-        assertThat(Rank.valueOfRank(3)).isEqualTo(Rank.FOUR);
-        assertThat(Rank.valueOfRank(7)).isEqualTo(Rank.NOTHING);
+        assertThat(Rank.valueOfRank(6,true)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.valueOfRank(5,true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.valueOfRank(5,false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.valueOfRank(4, true)).isEqualTo(Rank.FOUR);
+        assertThat(Rank.valueOfRank(3, true)).isEqualTo(Rank.FIVE);
+        assertThat(Rank.valueOfRank(7, false)).isEqualTo(Rank.NOTHING);
     }
 
     @Test
     void rankMoney() {
-        assertThat(Rank.valueOfRank(6).getMoney()).isEqualTo(2000000000);
+        assertThat(Rank.valueOfRank(6, false).getMoney()).isEqualTo(2000000000);
     }
 }
