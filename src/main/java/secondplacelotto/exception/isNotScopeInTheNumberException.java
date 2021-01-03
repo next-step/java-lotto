638b27fca24b5c5d4d945742d9bb847d6ec79d61
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2021-01-04 05:18:11 +0900

    범위를 벗어난 번호에 대한 커스텀 예외처리 구현

---
+++ src/main/java/secondplacelotto/exception/isNotScopeInTheNumberException.java
@@ -0,0 +1,8 @@
+package secondplacelotto.exception;
+
+public class isNotScopeInTheNumberException extends IllegalArgumentException {
+
+    public isNotScopeInTheNumberException(String message) {
+        super(message);
+    }
+}
