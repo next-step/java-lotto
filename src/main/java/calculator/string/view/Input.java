8074820633198eb665eea5a485a9cb4556720bae
Sunghwan-DS <jeonsung02@gmail.com>
2021-01-26 20:52:59 +0900

    feat: Add expressionInput function
    - 계산식을 입력받기 위한 함수 생성

--- src/main/java/calculator/string/view/Input.java
+++ src/main/java/calculator/string/view/Input.java
@@ -1,4 +1,12 @@
 package calculator.string.view;
 
+import java.util.Scanner;
+
 public class Input {
+    public static String expressionInput() {
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.next();
+
+        return expression;
+    }
 }
