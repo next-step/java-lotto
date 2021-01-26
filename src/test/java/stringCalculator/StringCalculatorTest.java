6782f8a8ead6a560a33b269960df823041c370b1
jeongdaeun98 <uneap@sk.com>
2021-01-26 18:35:25 +0900

    test: add emptyOrNull test

--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -1,6 +1,11 @@
 package stringCalculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import stringCalculator.domain.StringCalculator;
 
 class StringCalculatorTest {
@@ -11,4 +16,10 @@ class StringCalculatorTest {
         calculator = new StringCalculator();
     }
 
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String text) {
+        assertThat(calculator.add(text)).isZero();
+    }
 }
