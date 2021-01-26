97c1a4b13ce2a6e2e542c1e68fd100549dfb89dc
d36choi <yuio1126@gmail.com>
2021-01-26 18:02:17 +0900

    feat: checkMinusParamInput() method 로 음수 입력에 대한 유효성 검증

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -2,7 +2,6 @@ package calculator;
 
 public class InputValidator {
 
-
     public static boolean checkIsNullOrIsEmpty(String text) {
         return isNull(text) || isEmpty(text);
     }
@@ -13,6 +12,10 @@ public class InputValidator {
 
     private static boolean isNull(String text) {
         return text == null;
+    }
+
+    public static boolean checkMinusParamInput(String text) {
+        return Integer.parseInt(text) < 0;
     }
 
 }
