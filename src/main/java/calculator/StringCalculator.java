143faf29666d89ce2d9c8b0e0161ee9a3a3bd08f
d36choi <yuio1126@gmail.com>
2021-01-26 18:17:54 +0900

    feat: StringCalculator 에 split method 2개를 추가함.
    changed by 11014992

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,6 +7,7 @@ public class StringCalculator {
     }
 
     public int add (String text) {
+        int res = 0;
         if (InputValidator.checkIsNullOrIsEmpty(text)) {
             return 0;
         }
@@ -24,6 +25,37 @@ public class StringCalculator {
 
 
         return res;
+    }
+
+    private int splitByDelimiter(String text) {
+        String [] inputs = text.split(",|:");
+        int res = Arrays.stream(inputs)
+                .filter(input -> InputValidator.checkMinusParamInput(input))
+                .map(Integer::parseInt)
+                .reduce(Integer::sum).get();
+//        for (String input : inputs) {
+//            try {
+//                InputValidator.checkMinusParamInput(input);
+//            } catch (RuntimeException e) {
+//                System.out.println("음수는 연산이 불가합니다.");
+//            }
+//            res += Integer.parseInt(input);
+//        }
+        return res;
+    }
+
+    private int splitByCustomDelimiter(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+            int res = 0;
+            for (String input : tokens) {
+                res += Integer.parseInt(input);
+            }
+            return res;
+        }
+        return 0;
     }
 
 }
