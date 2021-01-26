75f938918e6ac33e7f568433edef6cafb3bcc138
jeongdaeun98 <uneap@sk.com>
2021-01-26 18:34:20 +0900

    test: add setUp

--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -1,5 +1,14 @@
 package stringCalculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import stringCalculator.domain.StringCalculator;
+
 class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
 
 }
