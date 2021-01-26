97befb2fcc33d5db4f1eae388c3e0d14fd3f9396
d36choi <yuio1126@gmail.com>
2021-01-26 17:32:08 +0900

    feat: InputValidator 를 추가함

---
+++ src/main/java/calculator/InputValidator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class InputValidator {
+
+
+    public static boolean checkIsNullOrIsEmpty(String text) {
+        return isNull(text) || isEmpty(text);
+    }
+
+    private static boolean isEmpty(String text) {
+        return text.trim().isEmpty();
+    }
+
+    private static boolean isNull(String text) {
+        return text == null;
+    }
+
+}
