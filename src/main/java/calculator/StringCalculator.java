d2025434b471e589a683f420cc1f82b3d1960f5b
koeonyack <covenant.ko@kakao.com>
2021-01-26 17:23:31 +0900

    feat: 커스텀 구분자를 지정할 수 있는 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
     public static final String DEFAULT_DELIMITER = ",|:";
@@ -15,6 +18,12 @@ public class StringCalculator {
     }
 
     private String[] splitText(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+            return tokens;
+        }
         return text.split(",|:");
     }
 
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -43,4 +43,11 @@ public class StringCalculatorTest {
     void colons(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
 }
