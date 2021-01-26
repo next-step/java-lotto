c29c2860f02152275611925a02f3d409e9f73997
jeongdaeun98 <uneap@sk.com>
2021-01-26 18:37:22 +0900

    test: add String test

--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -60,4 +60,11 @@ class StringCalculatorTest {
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> calculator.add("-1"));
     }
+
+    @DisplayName(value = "문자열 계산기에 숫자가 아닌 문자열을 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void stringTest() {
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(() -> calculator.add("a,1,d"));
+    }
 }
