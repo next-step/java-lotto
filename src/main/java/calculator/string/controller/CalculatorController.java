c3d718b92ff084969d2c2cf358c22bb5339b46c6
KimBB <tree9295@gmail.com>
2021-01-27 17:42:23 +0900

    feat: CalculatorController class 생성

---
+++ src/main/java/calculator/string/controller/CalculatorController.java
@@ -0,0 +1,24 @@
+package calculator.string.controller;
+
+import calculator.string.domain.StringCalculator;
+import calculator.string.view.Input;
+import calculator.string.view.Output;
+
+public class CalculatorController {
+
+    public String prepareCalculator() {
+        Output.printInputMessage();
+        String expression = Input.expressionInput();
+        return expression;
+    }
+
+    public int executeCalculator(String expression) {
+        StringCalculator stringCalculator = new StringCalculator();
+        stringCalculator.inputExpression(expression);
+        return stringCalculator.calculation();
+    }
+
+    public void endCalculator(int result){
+        Output.printResultMessage(result);
+    }
+}
