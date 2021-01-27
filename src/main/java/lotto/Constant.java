022bf6bc12e76f7ea1f7b3e90207d945b154fcf3
jeongdaeun98 <uneap@sk.com>
2021-01-27 19:11:37 +0900

    feat: create Constant

---
+++ src/main/java/lotto/Constant.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Constant {
+    public static final String REQUEST_PURCHASE_PRICE = "구입 금액을 입력해 주세요.";
+    public static final String REMIND_LOTTO_COUNT = "개를 구매했습니다.";
+    public static final String REQUEST_WINNING_PRICE = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_BALL = "보너스 볼을 입력해 주세요.";
+    public static final String WINNING_STATISTICS = "\n당첨 통계";
+    public static final String DIVISION_LINE = "---------";
+    public static final String CORRECT_NUM = "개 일치";
+
+}
