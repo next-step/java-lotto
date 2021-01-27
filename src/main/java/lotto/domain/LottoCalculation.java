ba0693d6b24f2fd09d7192c7a6acca7935782f78
koeonyack <covenant.ko@kakao.com>
2021-01-27 18:05:04 +0900

    feat: add isWinningBall
    로또 번호에 포함되는지 검사

--- src/main/java/lotto/domain/LottoCalculation.java
+++ src/main/java/lotto/domain/LottoCalculation.java
@@ -1,8 +1,14 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoCalculation {
 
     public Boolean isBonusBall(int number, int bonusBallNumber) {
         return number == bonusBallNumber;
+    }
+
+    public Boolean isWinningBall(int number, List<Integer> winningBalls) {
+        return winningBalls.contains(number);
     }
 }
--- src/test/java/lotto/domain/LottoCalculationTest.java
+++ src/test/java/lotto/domain/LottoCalculationTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoCalculationTest {
     static LottoCalculation lottoCalculation;
 
@@ -16,6 +20,13 @@ public class LottoCalculationTest {
     void isBonusBallTest () {
         Assertions.assertThat(lottoCalculation.isBonusBall(1, 1)).isTrue();
         Assertions.assertThat(lottoCalculation.isBonusBall(1, 2)).isFalse();
+    }
+
+    @Test
+    void isWinningBallTest() {
+        List<Integer> winningBalls = Arrays.asList(1, 2, 3, 4);
+        Assertions.assertThat(lottoCalculation.isWinningBall(1, winningBalls)).isTrue();
+        Assertions.assertThat(lottoCalculation.isWinningBall(10, winningBalls)).isFalse();
     }
 
 }
