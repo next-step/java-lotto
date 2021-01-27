093829e84bcba16a9049baa3cb234128b139504b
jeongdaeun98 <uneap@sk.com>
2021-01-27 22:24:51 +0900

    feat: create Revenue enum

--- src/main/java/lotto/domain/Revenue.java
+++ src/main/java/lotto/domain/Revenue.java
@@ -1,21 +1,36 @@
 package lotto.domain;
 
-public enum Revenue {
-    THREE_MATCHES("5000원", 3, false),
-    FOUR_MATCHES("50000원", 4, false),
-    FIVE_MATCHES("1500000원", 5, false),
-    FIVE_AND_BONUS_MATCHES("30000000원", 5, true),
-    SIX_MATCHES("2000000000원", 6, false);
+import java.util.Arrays;
+import java.util.List;
 
-    private String revenue;
+public enum Revenue {
+    THREE_MATCHES(5000, 3, false),
+    FOUR_MATCHES(50000, 4, false),
+    FIVE_MATCHES(1500000, 5, false),
+    FIVE_AND_BONUS_MATCHES(30000000, 5, true),
+    SIX_MATCHES(2000000000, 6, false),
+    NOTHING(0, 0, false);
+
+    private long revenue;
     private int matchNum;
     private boolean bonus;
 
-    Revenue(String revenue, int matchNum, boolean bonus) {
+    Revenue(long revenue, int matchNum, boolean bonus) {
         this.revenue = revenue;
         this.matchNum = matchNum;
         this.bonus = bonus;
     }
 
+    public static Revenue checkTheNumberOfMatchingLotto(List<Integer> lottoNumbers,
+        List<Integer> winningNumbers, int bonusNumber) {
+        long matchingCount = winningNumbers.stream()
+            .filter(winningNumber -> lottoNumbers.contains(winningNumber)).count();
 
+        return Arrays.stream(Revenue.values())
+            .filter(revenue -> revenue.matchNum == matchingCount
+                && revenue.bonus == lottoNumbers
+                .contains(bonusNumber))
+            .findFirst()
+            .get();
+    }
 }
