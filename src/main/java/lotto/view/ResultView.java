6f507146e79f9a9ddbb307fc842e0850515edb08
koeonyack <covenant.ko@kakao.com>
2021-01-27 17:33:03 +0900

    feat: ResultView
    add printStatistic method

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -2,9 +2,22 @@ package lotto.view;
 
 public class ResultView {
     private static final String PURCHASE_NUMBER_MESSAGE = "개를 구입했습니다.";
+    private static final String STATISTIC_HEADER_MESSAGE = "당첨 통계\n----------------";
+    private static final String STATISTIC_BODY_MESSAGE = "%d개 일치 (%d원) - %d개";
+    private static final String STATISTIC_BONUS_BODY_MESSAGE = "%d개 일치, 보너스 볼 일치(%d원) - %d개";
+    private static final String STATISTIC_REVENUE_MESSAGE = "총 수익률은 %f 입니다.";
 
     public static void printPurchaseNumber(int number) {
         System.out.println(number + PURCHASE_NUMBER_MESSAGE);
-        return;
+    }
+
+    public static void printStatistic(int matchNumber, Boolean isBonusMatchNumber, int revenue, int lottoNumberMatched, float earningRate) {
+        System.out.println(STATISTIC_HEADER_MESSAGE);
+        if (isBonusMatchNumber) {
+            System.out.println(String.format(STATISTIC_BONUS_BODY_MESSAGE, matchNumber, revenue, lottoNumberMatched));
+        } else {
+            System.out.println(String.format(STATISTIC_BODY_MESSAGE, matchNumber, revenue, lottoNumberMatched));
+        }
+        System.out.println(String.format(STATISTIC_REVENUE_MESSAGE, earningRate));
     }
 }
