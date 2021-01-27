83093664ed8f7cd245f033b93ebb251501c71625
jeongdaeun98 <uneap@sk.com>
2021-01-27 22:24:37 +0900

    feat: create Lotto and LottoMachine

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,20 @@
 package lotto.domain;
 
-public class Lotto {
+import java.util.List;
 
+public class Lotto {
+    private List<List<Integer>> tickets;
+    private static final int BASE_PRICE = 1000;
+
+    public int calculateTickets(int money) {
+        return money / BASE_PRICE;
+    }
+
+    private void buyTickets(int money) {
+        LottoMachine lottoMachine = new LottoMachine();
+        int ticketAmount = calculateTickets(money);
+        for (int i = 0; i < ticketAmount; i++) {
+            tickets.add(lottoMachine.generateTicket());
+        }
+    }
 }
---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoMachine {
+    private final List<Integer> numbers;
+    private final static int MIN_LOTTO = 1;
+    private final static int MAX_LOTTO = 46;
+
+    public LottoMachine() {
+        numbers = IntStream.range(MIN_LOTTO, MAX_LOTTO).boxed().collect(Collectors.toList());
+    }
+
+    public List<Integer> generateTicket() {
+        Collections.shuffle(numbers);
+        List<Integer> ticket = numbers.subList(0, 7);
+        Collections.sort(ticket);
+        return ticket;
+    }
+
+}
