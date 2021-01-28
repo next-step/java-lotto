a469704895c2372fd594b6627938419fc5ee661a
KimBB <tree9295@gmail.com>
2021-01-28 21:34:10 +0900

    feat: Create LottoService class
    - 로또 게임의 서비스를 전달하는 로또 서비스 클래스

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -1,24 +1,51 @@
 package lotto.domain;
 
-import lotto.domain.LottoTicket;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoService {
-    private List<LottoTicket> lottoTickets;
+    private final List<LottoTicket> lottoTickets;
+    private LotteryNumber lotteryNumber;
+    private Map<Rank, Integer> lottoResults;
+    private int profit;
 
     public LottoService() {
         lottoTickets = new ArrayList<>();
     }
 
-    public void buyLottoTickets(int numberOfTickets){
-        for(int i = 0; i < numberOfTickets; ++i){
+    public LottoService(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public void buyLottoTickets(int numberOfTickets) {
+        for (int i = 0; i < numberOfTickets; ++i) {
             lottoTickets.add(new LottoTicket());
         }
     }
 
     public List<LottoTicket> getLottoTickets() {
         return lottoTickets;
+    }
+
+    private void initLottoResults() {
+        lottoResults = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            System.out.println(rank);
+            lottoResults.put(rank, 0);
+        }
+    }
+
+    public void recordLotteryNumber(LotteryNumber lotteryNumber) {
+        this.lotteryNumber = lotteryNumber;
+    }
+
+    public void calculatePrize() {
+        initLottoResults();
+        for (LottoTicket lottoTicket : lottoTickets) {
+            Rank currentRank = lottoTicket.getRank(lotteryNumber);
+            lottoResults.put(currentRank, lottoResults.get(currentRank) + 1);
+        }
     }
 }
