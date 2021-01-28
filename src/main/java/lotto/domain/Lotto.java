a0913c60fe0680367bad9fc055346981d708b21d
d36choi <yuio1126@gmail.com>
2021-01-28 17:29:20 +0900

    feat: Add validation for lottoNumber

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,16 +1,33 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
 
     private final List<Integer> numbers;
     public Lotto(List<Integer> lottoNumbers) {
-
+        checkSizeOfLotto(lottoNumbers);
+        checkDuplicatedNumber(lottoNumbers);
         this.numbers = lottoNumbers;
     }
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    private void checkSizeOfLotto(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개이어야 합니다.");
+        }
+    }
+    private void checkDuplicatedNumber(List<Integer> lottoNumbers) {
+        Set<Integer> lottoNumbersChecker = new HashSet<>(lottoNumbers);
+        if(lottoNumbersChecker.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("중복된 로또번호가 있습니다.");
+        }
+
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,7 +14,7 @@ import java.util.List;
 class LottoTest {
 
 
-    @DisplayName("로또 번호 저장 테스")
+    @DisplayName("로또 번호 저장 테스치")
     @Test
     void 로또_번호_저장() {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
@@ -19,4 +22,26 @@ class LottoTest {
         Assertions.assertThat(lotto.getNumbers()).isEqualTo(numbers);
     }
 
+    //    @ParameterizedTest
+    //    @ValueSource(ints = {1,2,3,4,5})
+    @DisplayName("로또 번호 갯수의 불일치")
+    @Test
+
+    void lottoNumCountNotQualified() {
+
+        Assertions.assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1,3,4,5,6));
+            Lotto lotto = new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @DisplayName("중복된 번호의 포함")
+    @Test
+    void duplicatedLottoNum() {
+        Assertions.assertThatThrownBy(() -> {
+            List<Integer> numbers = new ArrayList<>(Arrays.asList(1,1,1,4,5,6));
+            Lotto lotto = new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
