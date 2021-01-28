e122a83c07e100ca35df3fa7810bf9c4771ebc99
d36choi <yuio1126@gmail.com>
2021-01-28 17:46:58 +0900

    feat: Add InputView, OutputView with simple methods

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,19 @@
 package lotto.view;
 
+import lotto.domain.Prize;
+
+import java.util.List;
+
 public class OutputView {
+
+
+    public static void printNumberOfLottoInfo(int numberOfLotto) {
+        System.out.println(numberOfLotto+"개를 구매하셨습니다.");
+    }
+    public static void printPrizeStatus(List<Prize> myPrizes) {
+        for (Prize p : myPrizes) {
+            System.out.println("wow");
+        }
+    }
 
 }
