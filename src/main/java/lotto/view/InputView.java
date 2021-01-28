e333c3ba7388c60aa65d90e9355f9323a2e60238
munkyung <munkyung0324@gmail.com>
2021-01-28 21:47:08 +0900

    fix: input error bugfix

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,24 +4,22 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static Scanner scanner;
-
-    public InputView() {
-        scanner = new Scanner(System.in);
-    }
+    private static Scanner scanner = new Scanner(System.in);
 
     public static int getMoneyToBuyLotto() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        int moneyAmount = scanner.nextInt();
+        return moneyAmount;
     }
-
 
     public static String getLottoAnswer() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return scanner.nextLine();
+        String winningLottoNumber = scanner.nextLine();
+        return winningLottoNumber;
     }
     public static int getBonusBall() {
-        System.out.println("보너스 볼을 입력해주세.");
-        return scanner.nextInt();
+        System.out.println("보너스 볼을 입력해주세요.");
+        int bonusBall = scanner.nextInt();
+        return bonusBall;
     }
 }
