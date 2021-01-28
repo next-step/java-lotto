d0af3311342bc24789259970df9783dcf8fa653f
d36choi <yuio1126@gmail.com>
2021-01-28 23:35:56 +0900

    doc: TODO 를 LottoMatcher에 추가함.

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class LottoMatcher {
-
+    //TODO: getMatCnt 를 원시타입 포장 클래스인 LottoNumber 와 호환되도록 변경해야 함.
     public int getMatchCnt(List<Integer> lottoNumber, List<Integer> winnerNumbers) {
         return (int) lottoNumber.stream()
             .filter(number -> winnerNumbers.contains(number))
