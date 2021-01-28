9ee5dd403c4d21bc6c2aafdd689e30064f0311fb
koeonyack <covenant.ko@kakao.com>
2021-01-28 17:19:27 +0900

    feat: lotto number checker

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,23 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-
     private final List<Integer> lottoNumbers;
+    private static final int LOTTO_NUMBER_LENGTH = 6;
 
     public Lotto(List<Integer> lottoNumbers) {
+        checkValidLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
+    }
+
+    private void checkValidLottoNumbers(List<Integer> lottoNumbers) {
+        Set<Integer> notDuplicatedNumbers= new HashSet<>(lottoNumbers);
+        if (notDuplicatedNumbers.size() != LOTTO_NUMBER_LENGTH) {
+            throw new IllegalArgumentException("(서로 다른) 로또 번호 6개만 가능합니다.");
+        }
     }
 
     public List<Integer> getLottoNumbers() {
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class LottoMachineTest {
     static LottoMachine lottoMachine;
 
@@ -25,5 +27,12 @@ public class LottoMachineTest {
     void createLottoTest() {
         List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         Assertions.assertThat(lottoMachine.createLotto(lottoNumbers).getLottoNumbers()).isEqualTo(lottoNumbers);
+    }
+
+    @Test
+    void createLottoFailureTest() {
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 3, 5, 6);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> lottoMachine.createLotto(lottoNumbers));
     }
 }
