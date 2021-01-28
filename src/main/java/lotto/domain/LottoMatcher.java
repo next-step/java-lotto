1d7527a66facf92932ff35a7a099a5e0562b7689
munkyung <munkyung0324@gmail.com>
2021-01-28 18:14:42 +0900

    feat: create LottoMatcherTest and add LottoMatcher logic

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -1,4 +1,12 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoMatcher {
+
+    public int getMatchCnt(List<Integer> lottoNumber, List<Integer> winnerNumbers) {
+        return (int) lottoNumber.stream()
+            .filter(number -> winnerNumbers.contains(number))
+            .count();
+    }
 }
