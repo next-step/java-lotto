937da8e910bc764a5fef7aaeee20756045f8febe
d36choi <yuio1126@gmail.com>
2021-01-28 17:13:18 +0900

    feat: Create LottoTest and Lotto class

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
 
+    private final List<Integer> numbers;
+    public Lotto(List<Integer> lottoNumbers) {
+
+        this.numbers = lottoNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,7 +1,22 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class LottoTest {
+
+
+    @DisplayName("로또 번호 저장 테스")
+    @Test
+    void 로또_번호_저장() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        Lotto lotto = new Lotto(numbers);
+        Assertions.assertThat(lotto.getNumbers()).isEqualTo(numbers);
+    }
 
 }
