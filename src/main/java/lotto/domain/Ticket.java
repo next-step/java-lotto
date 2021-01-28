695a82a65277c3eb86c77de71682012e117c8221
jeongdaeun98 <uneap@sk.com>
2021-01-28 23:27:41 +0900

    feat: add Ticket domain that contains lottoNumberStream and matchNumber

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -1,2 +1,28 @@
-package lotto.domain;public class Ticket {
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Ticket {
+
+    private final List<Integer> lottoNumbers;
+
+    public Ticket(List<Integer> tickets) {
+        this.lottoNumbers = tickets;
+    }
+
+    public Stream<Integer> lottoNumberStream() {
+        return lottoNumbers.stream();
+    }
+
+    public long matchNumber(Ticket newTicket) {
+        return newTicket.lottoNumberStream()
+            .filter(lottoNumbers::contains)
+            .count();
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumbers.toString();
+    }
 }
