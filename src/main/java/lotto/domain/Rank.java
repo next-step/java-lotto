3ed41298697fea5e537aea4cffd0a179891b6a51
KimBB <tree9295@gmail.com>
2021-01-28 22:06:10 +0900

    feat: add getRank in Rank
    - 로또 score, 보너스 점수를 계산해 Rank 반환 메서드

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -2,4 +2,13 @@ package lotto.domain;
 
 public enum Rank {
     FIRST, SECOND, THIRD, FOURTH, FIFTH, NOTHING;
+
+    public static Rank getRank(int score, boolean hasBonus) {
+        if (score == 6) { return Rank.FIRST; }
+        if (score == 5 && hasBonus) { return Rank.SECOND; }
+        if (score == 5) { return Rank.THIRD; }
+        if (score == 4) { return Rank.FOURTH; }
+        if (score == 3) { return Rank.FIFTH; }
+        return Rank.NOTHING;
+    }
 }
