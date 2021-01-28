21c88a7a433379f10c458805a6d1365141fde0c0
d36choi <yuio1126@gmail.com>
2021-01-28 18:15:23 +0900

    feat: LottoMain.java

--- src/main/java/lotto/domain/LottoMain.java
+++ src/main/java/lotto/domain/LottoMain.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
+import lotto.view.InputView;
+
 public class LottoMain {
 
     public static void main(String[] args) {
+        try {
+            int money = InputView.getMoneyToBuyLotto();
 
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
