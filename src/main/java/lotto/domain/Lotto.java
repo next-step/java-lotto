603aebd031326cf42eff3416e55d7e9a5b86f020
jeongdaeun98 <uneap@sk.com>
2021-01-28 23:27:10 +0900

    feat: add Lotto that contains statisticsTicket and calculateTotalPrize and earnedMoney

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,34 +1,50 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-public class Ticket {
-    private List<List<Integer>> tickets;
-    private static final int BASE_PRICE = 1000;
-    private int money;
-    private List<Integer> winningNumber;
-    private Integer bonusNumber;
+public class Lotto {
+    private List<Ticket> tickets = new ArrayList<>();
+    private WinningNumber winningNumber;
 
-    public Ticket(int money) {
-        this.money = money;
+    public Lotto(WinningNumber winningNumber) {
+        this.winningNumber = winningNumber;
+    }
+    public Lotto(WinningNumber winningNumber, List<Ticket> tickets){
+        this.winningNumber = winningNumber;
+        this.tickets = tickets;
     }
 
-    public Ticket() { }
+    public Lotto() {}
 
-    public List<List<Integer>> getTickets() {
+    public List<Ticket> getTickets() {
         return tickets;
     }
 
-    public int calculateTickets() {
-        return money / BASE_PRICE;
+    public Map<Revenue, Integer> statisticsTicket() {
+        Map<Revenue, Integer> revenueMap = new HashMap<>();
+        for(Ticket ticket : tickets) {
+            Revenue matchingLotto = Revenue.checkTheNumberOfMatchingLotto(ticket, winningNumber);
+            revenueMap.computeIfPresent(matchingLotto,((revenue, count) -> ++count));
+            revenueMap.putIfAbsent(matchingLotto, 1);
+        }
+        return revenueMap;
     }
 
-    public void buyTickets() {
-        LottoMachine lottoMachine = new LottoMachine();
-        int ticketAmount = calculateTickets();
-        for (int i = 0; i < ticketAmount; i++) {
-            // Ticket
-            tickets.add(lottoMachine.generateTicket());
+    public int calculateTotalPrize(Map<Revenue, Integer> revenueMap) {
+        int totalRevenue = 0;
+        for (Entry<Revenue, Integer> entry : revenueMap.entrySet()) {
+            Revenue revenue = entry.getKey();
+            Integer integer = entry.getValue();
+            totalRevenue += revenue.totalRevenue(integer);
         }
+        return totalRevenue;
+    }
+
+    public boolean earnedMoney(Double yield) {
+        return yield >= 1;
     }
 }
