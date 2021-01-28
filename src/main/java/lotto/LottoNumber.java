32ac92604d185e32afe647b64a9e2ced6d68399e
quadam36 <quadam36@gmail.com>
2021-01-28 18:18:45 +0900

    feat: Add LottoNumber class

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.Objects;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+    private static final int NUM_LEFT_BOUND = 1;
+    private static final int NUM_RIHGT_BOUND = 45;
+
+    private Integer number;
+
+    public LottoNumber(int number) {
+        if (number < NUM_LEFT_BOUND || number > NUM_RIHGT_BOUND) {
+            throw new RuntimeException();
+        }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber other) {
+        return number.compareTo(other.number);
+    }
+}
---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoNumberTest {
+    @DisplayName("범위에 맞지 않는 숫자일 경우 예외를 던진다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void LottoNumberNotValid(int number) {
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(() -> new LottoNumber(number));
+    }
+}
