914cbbb486a34f0876557c8f963e7fb7ddc71a43
munkyung <munkyung0324@gmail.com>
2021-01-28 21:47:39 +0900

    feat: add generating LottoNumber logic

--- src/main/java/lotto/domain/LottoMain.java
+++ src/main/java/lotto/domain/LottoMain.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.view.InputView;
 
 public class LottoMain {
@@ -8,9 +10,18 @@ public class LottoMain {
         try {
             int money = InputView.getMoneyToBuyLotto();
             int numberOfTicket = LottoTickets.countTicketNumberByMoney(money);
-            // List<Lotto> generatedLottoNumbers = LottoGenerator.generateLottoNumbers(numberOfTicket);
-            // LottoTickets purchasedTickets = new LottoTickets(generatedLottoNumbers);
             // 무작위 로또를 생성해 로또티켓 객체에 생성자 주입
+            List<Lotto> generatedLottoNumbers = LottoGenerator.generateLottoNumbers(numberOfTicket);
+            LottoTickets purchasedTickets = new LottoTickets(generatedLottoNumbers);
+            for (int i = 0; i < purchasedTickets.getNumberOfTickets(); i++) {
+                System.out.println(purchasedTickets.getLottoTickets().get(i).toString());
+            }
+            // 지난주 로또 정답을 입력받음
+            String winningLottoNumber = InputView.getLottoAnswer();
+            Arrays.stream(winningLottoNumber.split(", ")).map(Integer::parseInt);
+//            for (int i = 0; i < purchasedTickets.getNumberOfTickets(); i++) {
+//
+//            }
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
