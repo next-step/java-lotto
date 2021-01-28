a9e0b8144743c8d2a25e41d75baddde4e5e79975
jeongdaeun98 <uneap@sk.com>
2021-01-28 23:28:54 +0900

    feat: add Input, OutputHandler view

--- src/main/java/lotto/view/InputHandler.java
+++ src/main/java/lotto/view/InputHandler.java
@@ -5,14 +5,32 @@ import lotto.Constant;
 
 public class InputHandler {
 
-    private Scanner sc = new Scanner(System.in);
+    private final Scanner sc = new Scanner(System.in);
 
-    public String requestPriceFromUser(){
+    public String requestPriceFromUser() {
         printRequestPriceFromUser();
+        return sc.nextLine();
+    }
+
+    public String requestWinningNumber() {
+        printRequestWinningNumber();
+        return sc.nextLine();
+    }
+
+    public String requestBonusNumber() {
+        printRequestBonusNumber();
         return sc.nextLine();
     }
 
     private void printRequestPriceFromUser() {
         System.out.println(Constant.REQUEST_PURCHASE_PRICE);
+    }
+
+    private void printRequestWinningNumber() {
+        System.out.println(Constant.REQUEST_WINNING_PRICE);
+    }
+
+    private void printRequestBonusNumber() {
+        System.out.println(Constant.REQUEST_BONUS_BALL);
     }
 }
