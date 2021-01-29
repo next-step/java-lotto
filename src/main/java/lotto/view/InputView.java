69d2066de48f2ffe92de175f38f11662b9c75f1f
quadam36 <quadam36@gmail.com>
2021-01-29 13:22:41 +0900

    feat: Add view logic methods

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,13 +3,14 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String AMOUNT_MSG = "구입금액을 입력해 주세요.";
+    private static final String TOTAL_COST_MSG = "구입금액을 입력해 주세요.";
     private static final String LOTTO_NUMBERS_MSG = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MSG = "보너스 볼을 입력해 주세요";
+
     private static Scanner scanner = new Scanner(System.in);
 
-    public Integer inputAmount() {
-        System.out.println(AMOUNT_MSG);
+    public Integer inputTotalCost() {
+        System.out.println(TOTAL_COST_MSG);
         return scanner.nextInt();
     }
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,32 +1,46 @@
 package lotto.view;
 
+import lotto.LottoScore;
+import lotto.domain.LottoTicket;
+import lotto.dto.LottoTicketCntDTO;
+import lotto.dto.LottoTicketDTO;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
+    private static final String TICKET_CNT_FMT = "%d개를 구매했습니다.\n";
+    private static final String TICKET_DELIMITER = ", ";
+    private static final String TICKET_FMT = "[%s]\n";
+
     private static final String RESULT_TITLE_MSG = "당첨 통계";
     private static final String RESULT_DELIMITER_MSG = "---------";
+    private static final String RESULT_MATCH_COUNT_DESCRIBE_MSG = "개 일치";
+    private static final String RESULT_BOUNS_DESCRIBE_MSG = ", 보너스 볼 일치";
 
-    private static final int[] PRIZE_REWARDS = {
-        5000, 50_000, 1_500_000, 30_000_000, 2_000_000_000
-    };
-    public void printResult() {
-        System.out.println(RESULT_TITLE_MSG);
-        System.out.println(RESULT_DELIMITER_MSG);
+    private static final String RESULT_SCORE_FMT = "%d%s (%d원) - (%d)개";
+    private static final String RESULT_PROFIT_PERCENT_FMT = "총 수익률은 %.2f 입니다.";
 
+    public void printTicketCnt(LottoTicketCntDTO dto) {
+        System.out.format(TICKET_CNT_FMT, dto.getTicketCnt());
     }
-    public void printResult(List<Integer> result) {
-        for(int i=0; i<result.size(); i++){
-            // TODO : if i == 3, it is match 5 and bonus.
-            if(i==3){
-                System.out.printf("%d개 일치 (%d원) - %d개\n",i+3,PRIZE_REWARDS[i],result.get(i));
-            }
-            else {
-                System.out.printf("%d개 일치, 보너스 볼 일치 (%d원) - %d개\n",i+3,PRIZE_REWARDS[i],result.get(i));
-            }
+
+    public void printTicket(LottoTicketDTO dto) {
+        System.out.format(
+            TICKET_FMT,
+            dto.getLottoNumbers().stream().map(
+                Object::toString
+            ).collect(
+                Collectors.joining(TICKET_DELIMITER)
+            )
+        );
+    }
+
+    private String getDescribeMsg(boolean isBonusMatched) {
+        String msg = RESULT_MATCH_COUNT_DESCRIBE_MSG;
+        if (isBonusMatched) {
+            msg += RESULT_BOUNS_DESCRIBE_MSG;
         }
+        return msg;
     }
-    public void printRevenue(Double resRevenue) {
-        System.out.printf("총 수익률은 %f 입니다.", resRevenue);
-    }
-
 }
