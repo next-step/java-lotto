1134a7a62c38023f685706c95b9a42884668f50c
Sunghwan-DS <jeonsung02@gmail.com>
2021-01-29 21:40:09 +0900

    feat: Create Main.java

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,4 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
 public class Main {
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+
+        lottoController.buyLottoProcess();
+        lottoController.pickLotteryNumber();
+        lottoController.showLottoResult();
+    }
 }
