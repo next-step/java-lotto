45d201493776d18c51f92a969f2c97639fa15e01
quadam36 <quadam36@gmail.com>
2021-01-29 19:48:44 +0900

    feat: Add controller logic

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,10 +1,17 @@
 package lotto;
 
+import lotto.domain.LottoAnswer;
 import lotto.domain.LottoBuyer;
 import lotto.domain.LottoBuyerGenerator;
+import lotto.domain.LottoGame;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
+import lotto.domain.LottoTicketGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoApplication {
     public static void main(String[] args) {
@@ -13,9 +20,26 @@ public class LottoApplication {
 
         LottoBuyer buyer = LottoBuyerGenerator.generateLottoBuyer(input.inputTotalCost());
         output.printTicketCnt(buyer.getLottoTicketCntDTO());
+
+        List<LottoTicket> tickets = new ArrayList<>();
         while (buyer.canBuyTicket()) {
             LottoTicket ticket = buyer.buyTicket();
             output.printTicket(ticket.getLottoTicketDTO());
+            tickets.add(ticket);
         }
+
+        LottoAnswer answer = new LottoAnswer(
+            LottoTicketGenerator.generateManualTicket(
+                input.inputAnswerNumbers()
+            ),
+            new LottoNumber(
+                input.inputBonusNumber()
+            )
+        );
+
+        LottoGame game = new LottoGame(answer, tickets);
+        output.printScoreBoardData(
+            game.calcScoreBoard().getScoreBoardData()
+        );
     }
 }
