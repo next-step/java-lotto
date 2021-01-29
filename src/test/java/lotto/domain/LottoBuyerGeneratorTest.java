34593b6cb177b8a9add82b00aae3b9ceeee762a9
quadam36 <quadam36@gmail.com>
2021-01-29 12:28:20 +0900

    feat: Add LottoBuyerGenerator class

---
+++ src/test/java/lotto/domain/LottoBuyerGeneratorTest.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoBuyerGeneratorTest {
+
+    @DisplayName("입력된 금액에 따라 로또 구매자 객체를 잘 생성하는지 확인")
+    @ParameterizedTest
+    @CsvSource({
+        "300, 0",
+        "3000, 3",
+        "7300, 7",
+    })
+    void generateLottoBuyer(int totalCost, int ticketCnt) {
+        assertEquals(
+            LottoBuyerGenerator.generateLottoBuyer(totalCost),
+            new LottoBuyer(ticketCnt)
+        );
+    }
+}
