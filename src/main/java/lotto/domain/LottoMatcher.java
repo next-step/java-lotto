94a9f3b3cb08cb12a59329e4b7daf39251fd089a
munkyung <munkyung0324@gmail.com>
2021-01-30 14:12:45 +0900

    feat: 보너스 볼 처리에 대한 TODO 구현

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
@@ -22,17 +24,18 @@ public class LottoMatcher {
 
         List<PlayersLotto> playersLottoTickets = lottoTickets.getLottoTickets();
         for (PlayersLotto lotto : playersLottoTickets) {
-            int matchCnt = getMatchCnt(lotto.getNumbers(), winnerNumbers);
-            Prize prize = getPrizeForEachLotto(matchCnt);
+            Prize prize = getPrizeForEachLotto(lotto.getNumbers(), winnerNumbers);
             addPrizeStatus(prize);
         }
     }
 
     public static int getMatchCnt(List<LottoNumber> lottoNumber, WinnerLotto winnerNumbers) {
-        //TODO: 5개 적중 시 보너스 볼을 추가 검증해서 적중수가 5+1지(보너스볼)인지, 6인지를 구분짓는 로직이 필요함.
-        return (int) lottoNumber.stream()
+        // DONE: 5개 적중 시 보너스 볼을 추가 검증해서 적중수가 5+1지(보너스볼)인지, 6인지를 구분짓는 로직이 필요함.
+        // 여기에서는 갯수만 카운팅 하고 보너스 볼에 대한 로직은 getPrizeForEachLotto 에서 검증하는 것이 맞는것 같아 수정해 보았습니다.
+        int matchCnt = (int) lottoNumber.stream()
             .filter(number -> winnerNumbers.getNumbers().contains(number))
             .count();
+        return matchCnt;
     }
 
     public Map<Prize, Integer> getPrizeBoard() {
@@ -43,13 +46,16 @@ public class LottoMatcher {
         PrizeBoard.put(prize,PrizeBoard.get(prize)+1);
     }
 
-    public Prize getPrizeForEachLotto(int matchCount) {
-        //TODO: 5개+보너스볼1개 인 경우의 분기 처리를 어떻게 할지 구현해야 함.
-        for (Prize prize : Prize.values()) {
-            if (prize.getMatchedNumber() == matchCount) {
-                return prize;
-            }
+    public Prize getPrizeForEachLotto(List<LottoNumber> lottoNumber, WinnerLotto winnerNumbers) {
+        int matchCnt = getMatchCnt(lottoNumber, winnerNumbers);
+        if(matchCnt < 3) {
+            throw new IllegalArgumentException("해당 맞춘 수에 대한 상이 없습니다.");
         }
-        throw new IllegalArgumentException("해당 맞춘 수에 대한 상이 없습니다.");
+        Prize returnPrize = Arrays.stream(Prize.values()).filter(prize -> prize.getMatchedNumber() == matchCnt).findAny().get();
+        // DONE : 5개+보너스볼1개 인 경우의 분기 처리 구현
+        if(matchCnt == 5 && lottoNumber.stream().anyMatch(num -> num.getLottoNumber() == winnerNumbers.getBonusBall().getLottoNumber())) {
+            returnPrize = Prize.FIVE_WITH_BONUS;
+        }
+        return returnPrize;
     }
 }
--- src/test/java/lotto/domain/LottoMatcherTest.java
+++ src/test/java/lotto/domain/LottoMatcherTest.java
@@ -53,37 +53,39 @@ class LottoMatcherTest {
         assertThat(lottoMatcher.getPrizeBoard().get(Prize.THREE)).isEqualTo(1);
     }
 
-    @DisplayName("맞춘 개수에 따른 상 enum 반환")
-    @Test
-    void getPrizeForEachLotto() {
-        int matchedCount = 6;
+    //TODO: 이부분에서 개수로만 Enum 매핑을 테스트 하도록 하면 보너스 넘버에 대한 논리 구조가 안맞는것 같다는 생각이 들어요!
+    // 그래서 의견을 듣고자 코멘트로 남겼습니다
+//    @DisplayName("맞춘 개수에 따른 상 enum 반환")
+//    @Test
+//    void getPrizeForEachLotto() {
+//        int matchedCount = 6;
+//
+//        assertThat(lottoMatcher.getPrizeForEachLotto(matchedCount, new LottoNumber(6))).isEqualTo(Prize.SIX);
+//    }
 
-        assertThat(lottoMatcher.getPrizeForEachLotto(matchedCount)).isEqualTo(Prize.SIX);
-
-    }
     @DisplayName("로또 티켓에 대한 등수 매칭 검증. 3개와 6개가 적중한 로또티켓에 대해 테스트.")
     @Test
     void checkAllTickets() {
 
         PlayersLotto playersLotto1 = new PlayersLotto(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6)
+            new LottoNumber(1),
+            new LottoNumber(2),
+            new LottoNumber(3),
+            new LottoNumber(4),
+            new LottoNumber(5),
+            new LottoNumber(6)
         ));
         PlayersLotto playersLotto2 = new PlayersLotto(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(9),
-                new LottoNumber(10),
-                new LottoNumber(11)
+            new LottoNumber(1),
+            new LottoNumber(2),
+            new LottoNumber(3),
+            new LottoNumber(9),
+            new LottoNumber(10),
+            new LottoNumber(11)
         ));
         List<PlayersLotto> playersLottos = new ArrayList<>(Arrays.asList(
-                playersLotto1,
-                playersLotto2
+            playersLotto1,
+            playersLotto2
         ));
         LottoTickets lottoTickets = new LottoTickets(playersLottos);
         List<LottoNumber> winnerNumbers = new ArrayList<>();
@@ -93,9 +95,37 @@ class LottoMatcherTest {
         winnerNumbers.add(new LottoNumber(4));
         winnerNumbers.add(new LottoNumber(5));
         winnerNumbers.add(new LottoNumber(6));
-        WinnerLotto winnerLotto = new WinnerLotto(new LottoNumber(7),winnerNumbers);
-        lottoMatcher.checkAllTickets(lottoTickets,winnerLotto);
+        WinnerLotto winnerLotto = new WinnerLotto(new LottoNumber(7), winnerNumbers);
+        lottoMatcher.checkAllTickets(lottoTickets, winnerLotto);
         assertThat(lottoMatcher.getPrizeBoard().get(Prize.THREE)).isEqualTo(1);
         assertThat(lottoMatcher.getPrizeBoard().get(Prize.SIX)).isEqualTo(1);
+    }
+
+    @DisplayName("로또 티켓에 대한 등수 매칭 검증. 5개의 매칭과 보너스 볼을 적중한 로또티켓 테스트.")
+    @Test
+    void checkFiveWithBonusTicket() {
+
+        PlayersLotto playersLotto1 = new PlayersLotto(Arrays.asList(
+            new LottoNumber(1),
+            new LottoNumber(2),
+            new LottoNumber(3),
+            new LottoNumber(4),
+            new LottoNumber(5),
+            new LottoNumber(6)
+        ));
+        List<PlayersLotto> playersLottos = new ArrayList<>(Arrays.asList(
+            playersLotto1
+        ));
+        LottoTickets lottoTickets = new LottoTickets(playersLottos);
+        List<LottoNumber> winnerNumbers = new ArrayList<>();
+        winnerNumbers.add(new LottoNumber(1));
+        winnerNumbers.add(new LottoNumber(2));
+        winnerNumbers.add(new LottoNumber(3));
+        winnerNumbers.add(new LottoNumber(4));
+        winnerNumbers.add(new LottoNumber(5));
+        winnerNumbers.add(new LottoNumber(7));
+        WinnerLotto winnerLotto = new WinnerLotto(new LottoNumber(6), winnerNumbers);
+        lottoMatcher.checkAllTickets(lottoTickets, winnerLotto);
+        assertThat(lottoMatcher.getPrizeBoard().get(Prize.FIVE_WITH_BONUS)).isEqualTo(1);
     }
 }
