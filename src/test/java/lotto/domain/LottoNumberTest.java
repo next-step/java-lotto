6fca561db55ec8cb122ee276c9c72e45b196f9e0
d36choi <yuio1126@gmail.com>
2021-01-30 23:36:37 +0900

    test: Add LottoNumberTest.java

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoNumberTest {
+
+    @DisplayName("out of lotto range")
+    @Test
+    void checkIsValidTest() {
+        int illegalNumber = 47;
+        Assertions.assertThatThrownBy(()-> {
+            LottoNumber lottoNumber = new LottoNumber(illegalNumber);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("compare test")
+    @Test
+    void compareToTest() {
+        LottoNumber smallNumber = new LottoNumber(1);
+        LottoNumber sameWithSmallNumber = new LottoNumber(1);
+        LottoNumber bigNumber = new LottoNumber(41);
+        assertThat(smallNumber.compareTo(bigNumber)).isEqualTo(-1);
+        assertThat(bigNumber.compareTo(smallNumber)).isEqualTo(1);
+        assertThat(smallNumber.compareTo(sameWithSmallNumber)).isEqualTo(-1);
+    }
+}
