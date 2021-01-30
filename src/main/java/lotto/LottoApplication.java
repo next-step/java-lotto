3c0a72a174ac273f6d49cc4d5ca8c2848182ebb9
Changhwan Lee <chlee1252@gmail.com>
2021-01-30 22:22:05 +0900

    refactor: reduce main function

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -2,6 +2,8 @@ package lotto;
 
 import java.util.List;
 import java.util.Map;
+
+import lotto.controller.LottoController;
 import lotto.controller.Validator;
 import lotto.domain.Lotto;
 import lotto.domain.Price;
@@ -15,41 +17,11 @@ import lotto.view.OutputHandler;
 public class LottoApplication {
 
     public static void main(String[] args) {
-        Validator validator = new Validator();
-        OutputHandler outputHandler = new OutputHandler();
-        InputHandler inputHandler = new InputHandler();
-
-        String money = inputHandler.requestPriceFromUser();
-        if (!validator.isPriceValidate(money)) {
-            outputHandler.printErrorPurchasePrice();
-            System.exit(0);
-        }
-        Price price = new Price(Integer.parseInt(money));
-        outputHandler.printLottoPurchaseCount(price.calculateTickets());
-        List<Ticket> tickets = price.buyTickets();
-        outputHandler.printGeneratedTickets(tickets);
-
-        String winningNumberInput = inputHandler.requestWinningNumber();
-        if (!validator.validateWinningNumber(winningNumberInput)) {
-            outputHandler.printErrorWinningNumber();
-            System.exit(0);
-        }
-
-        String bonusNumber = inputHandler.requestBonusNumber();
-        if (!validator.validateBonusNumber(bonusNumber)) {
-            outputHandler.printErrorBonusBall();
-            System.exit(0);
-        }
-
-        // call winning number
-        WinningNumber winningNumber = new WinningNumber(new Ticket(NumberUtils.convertStringToIntegerList(winningNumberInput)),
-                                                        Integer.parseInt(bonusNumber));
-        Lotto lotto = new Lotto(winningNumber, tickets);
-
-        Map<Revenue, Integer> revenueCluster = lotto.statisticsTicket();
-        outputHandler.printLottoStatics(revenueCluster);
-        int total = lotto.calculateTotalPrize(revenueCluster);
-        Double yield = NumberUtils.calculateYield(Integer.parseInt(money), total);
-        outputHandler.printTotalRevenue(yield);
+        LottoController lottoController = new LottoController();
+        String money = lottoController.getPriceFromUser();
+        List<Ticket> tickets = lottoController.purchaseTicket(money);
+        String winningNumber = lottoController.getWinningNumberFromUser();
+        String bonusNumber = lottoController.getBonusNumberFromUser();
+        lottoController.returnOutput(money, winningNumber, bonusNumber, tickets);
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,63 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.util.NumberUtils;
+import lotto.view.InputHandler;
+import lotto.view.OutputHandler;
+
+import java.util.List;
+import java.util.Map;
+
+public class LottoController {
+
+    private final Validator validator = new Validator();
+    private final OutputHandler outputHandler = new OutputHandler();
+    private final InputHandler inputHandler = new InputHandler();
+
+    public String getPriceFromUser() {
+        String money = inputHandler.requestPriceFromUser();
+        if (!validator.isPriceValidate(money)) {
+            outputHandler.printErrorPurchasePrice();
+            System.exit(0);
+        }
+        return money;
+    }
+
+    public List<Ticket> purchaseTicket(String money) {
+        Price price = new Price(Integer.parseInt(money));
+        outputHandler.printLottoPurchaseCount(price.calculateTickets());
+        List<Ticket> tickets = price.buyTickets();
+        outputHandler.printGeneratedTickets(tickets);
+        return tickets;
+    }
+
+    public String getWinningNumberFromUser() {
+        String winningNumberInput = inputHandler.requestWinningNumber();
+        if (!validator.validateWinningNumber(winningNumberInput)) {
+            outputHandler.printErrorWinningNumber();
+            System.exit(0);
+        }
+        return winningNumberInput;
+    }
+
+    public String getBonusNumberFromUser() {
+        String bonusNumber = inputHandler.requestBonusNumber();
+        if (!validator.validateBonusNumber(bonusNumber)) {
+            outputHandler.printErrorBonusBall();
+            System.exit(0);
+        }
+        return bonusNumber;
+    }
+
+    public void returnOutput(String money, String winningNumberInput, String bonusNumber, List<Ticket> tickets) {
+        WinningNumber winningNumber = new WinningNumber(new Ticket(NumberUtils.convertStringToIntegerList(winningNumberInput)),
+                Integer.parseInt(bonusNumber));
+        Lotto lotto = new Lotto(winningNumber, tickets);
+
+        Map<Revenue, Integer> revenueCluster = lotto.statisticsTicket();
+        outputHandler.printLottoStatics(revenueCluster);
+        int total = lotto.calculateTotalPrize(revenueCluster);
+        Double yield = NumberUtils.calculateYield(Integer.parseInt(money), total);
+        outputHandler.printTotalRevenue(yield);
+    }
 }
