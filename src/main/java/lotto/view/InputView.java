fe412cd052b5d4c8275ba0f7a419ca655eaab28e
KimBB <tree9295@gmail.com>
2021-01-30 10:28:49 +0900

    feat: add validation in InputView class
    - InputView class에 input validation 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.utils.InputValidator;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,29 +10,35 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputPrice() {
-        int price;
-
         System.out.println("구입금액을 입력해 주세요.");
-        price = scanner.nextInt();
+        String input = scanner.next();
+
+        InputValidator.checkNumberValidation(input);
+        Integer price = Integer.valueOf(input);
+        InputValidator.checkPriceValidation(price);
         return price;
     }
 
     public static List<Integer> inputWinningNumbers() {
         List<Integer> winningNumbers = new ArrayList<>();
-
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        String winningNumbersInfo = scanner.next();
-        for (String number : winningNumbersInfo.split(",")) {
-            winningNumbers.add(Integer.valueOf(number.trim()));
+        String winningNumbersInput = scanner.next();
+        for (String input : winningNumbersInput.split(",")) {
+            String winningNumberExceptBlank = input.trim();
+            InputValidator.checkNumberValidation(winningNumberExceptBlank);
+            Integer winningNumber = Integer.valueOf(winningNumberExceptBlank);
+            InputValidator.checkLottoNumberValidation(winningNumber);
+            winningNumbers.add(winningNumber);
         }
+        InputValidator.checkLengthWinningNumbersValidation(winningNumbers.size());
         return winningNumbers;
     }
 
     public static int inputBonusNumber() {
-        int bonusNumber;
-
         System.out.println("보너스 볼을 입력해 주세요.");
-        bonusNumber = scanner.nextInt();
+        String input = scanner.next();
+        InputValidator.checkNumberValidation(input);
+        Integer bonusNumber = Integer.valueOf(input);
         return bonusNumber;
     }
 }
