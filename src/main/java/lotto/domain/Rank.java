8d2e9ca135eb930c5f6d54575253cf846f26df5c
KimDoubleB <tree9295@gmail.com>
2021-01-30 16:38:15 +0900

    refactor: reward final 처리 in Rank

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,7 +4,7 @@ public enum Rank {
     FIRST(2_000_000_000), SECOND(30_000_000), THIRD(1_500_000),
     FOURTH(50_000), FIFTH(5_000), NOTHING(0);
 
-    private int reward;
+    private final int reward;
 
     Rank(int reward){
         this.reward = reward;
