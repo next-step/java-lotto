aa4172455f72b4a6ea9fe4aa302e73d481f2477b
jeongdaeun98 <uneap@sk.com>
2021-01-30 23:11:22 +0900

    feat: add printing error negative num so that user can restart

--- src/main/java/stringCalculator/view/OutputHandler.java
+++ src/main/java/stringCalculator/view/OutputHandler.java
@@ -3,14 +3,21 @@ package stringCalculator.view;
 import stringCalculator.Constant;
 
 public class OutputHandler {
-    public void printResult(int result) {
+    public void printResult(Integer result) {
         System.out.println(createResult(result));
     }
 
-    private StringBuilder createResult(int result) {
+    private StringBuilder createResult(Integer result) {
         StringBuilder stringBuilder = new StringBuilder();
+        if (result == null || result == 0) {
+            return stringBuilder.append(Constant.PRINT_ZERO);
+        }
         return stringBuilder.append(Constant.PRINT_RESULT_FRONT)
             .append(result)
             .append(Constant.PRINT_RESULT_END);
+    }
+
+    public void printErrorNegativeNum() {
+        System.out.println(Constant.ERROR_NEGATIVE_NUM);
     }
 }
