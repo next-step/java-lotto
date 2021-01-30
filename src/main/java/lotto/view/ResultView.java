19e7c5d078cf436b1919e2ca2866166990b96f6d
koeonyack <covenant.ko@kakao.com>
2021-01-30 17:34:50 +0900

    refactor: ResultView 하드코딩 수정

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -9,14 +9,16 @@ import java.util.Map;
 
 public class ResultView {
     private static final String PURCHASE_NUMBER_MESSAGE = "개를 구입했습니다.";
-    private static final String STATISTIC_HEADER_MESSAGE = "당첨 통계\n----------------";
-    private static final String STATISTIC_REVENUE_MESSAGE = "총 수익률은 %f 입니다.";
+    private static final String STATISTIC_HEADER_MESSAGE = "당첨 통계\n---------";
+    private static final String WINNER_COUNT_MESSAGE = "%d개 일치 (%d원)- %d개";
+    private static final String FIVE_BONUS_COUNT_MESSAGE = "%d개 일치, 보너스 볼 일치(%d원)- %d개";
+    private static final String STATISTIC_REVENUE_MESSAGE = "총 수익률은 %.2f입니다.";
 
     public static void printPurchaseNumber(int number) {
         System.out.println(number + PURCHASE_NUMBER_MESSAGE);
     }
 
-    public static void printGenerateLottos(List<Lotto> lottos) {
+    public static void printGeneratedLottos(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getLottoNumbers().toString());
         }
@@ -25,13 +27,24 @@ public class ResultView {
     public static void printResult(Map<WinningType, Integer> matchResult, int money) {
         System.out.println("");
         System.out.println(STATISTIC_HEADER_MESSAGE);
-        System.out.println("3개 일치 (" + WinningType.THREE.getProfit() + "원)- " + matchResult.get(WinningType.THREE) + "개");
-        System.out.println("4개 일치 (" + WinningType.FOUR.getProfit() + "원)- " + matchResult.get(WinningType.FOUR) + "개");
-        System.out.println("5개 일치 (" + WinningType.FIVE.getProfit() + "원)- " + matchResult.get(WinningType.FIVE) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치(" + WinningType.FIVE_BONUS.getProfit() + "원)- " + matchResult.get(WinningType.FIVE_BONUS) + "개");
-        System.out.println("6개 일치 (" + WinningType.SIX.getProfit() + "원)- " + matchResult.get(WinningType.SIX) + "개");
+        for (WinningType winningType : WinningType.values()) {
+            printEachResult(matchResult, winningType);
+        }
 
         float profitRate = LottoCalculation.getProfitRate(matchResult, money);
         System.out.println(String.format(STATISTIC_REVENUE_MESSAGE, profitRate));
+    }
+
+    private static void printEachResult(Map<WinningType, Integer> matchResult, WinningType winningType) {
+        if (winningType.getValue() == 0) {
+            return;
+        }
+        if (winningType == WinningType.FIVE_BONUS) {
+            System.out.println(String.format(FIVE_BONUS_COUNT_MESSAGE,
+                    winningType.getValue(), winningType.getProfit(), matchResult.get(winningType)));
+            return;
+        }
+        System.out.println(String.format(WINNER_COUNT_MESSAGE,
+                winningType.getValue(), winningType.getProfit(), matchResult.get(winningType)));
     }
 }
