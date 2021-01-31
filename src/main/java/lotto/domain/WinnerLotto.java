63ad974b866836e3e3ee223ff5309363d49a555f
munkyung <munkyung0324@gmail.com>
2021-01-31 17:21:33 +0900

    refactor: 멤버 메소드를 Lotto 인터페이스로 이동

--- src/main/java/lotto/domain/WinnerLotto.java
+++ src/main/java/lotto/domain/WinnerLotto.java
@@ -1,18 +1,20 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import lotto.util.Validator;
 
 public class WinnerLotto implements Lotto {
 
-    private final LottoNumber BonusBall;
-    private final List<LottoNumber> numbers;
+    private LottoNumber BonusBall;
+    private List<LottoNumber> numbers;
 
+    public WinnerLotto() {
+    }
 
     public WinnerLotto(LottoNumber bonusBall, List<LottoNumber> numbers) {
         Validator.checkSizeOfLotto(numbers);
         Validator.checkDuplicatedNumber(numbers);
-        BonusBall = bonusBall;
+        this.BonusBall = bonusBall;
         this.numbers = numbers;
     }
 
@@ -22,19 +24,5 @@ public class WinnerLotto implements Lotto {
 
     public LottoNumber getBonusBall() {
         return BonusBall;
-    }
-
-    public static List<LottoNumber> getWinnerLottoWithSplitting(String text) {
-        String [] inputs = text.split(DELIMITER);
-        List<LottoNumber> winnerLotto = new ArrayList<>();
-        for (String input : inputs) {
-            Validator.isInteger(input);
-            Validator.isNegative(input);
-            winnerLotto.add(new LottoNumber(Integer.parseInt(input)));
-        }
-        if (winnerLotto.size() != LOTTO_NUMBER_SIZE) {
-            throw new IllegalArgumentException("로또의 숫자는 6개입니다.");
-        }
-        return winnerLotto;
     }
 }
