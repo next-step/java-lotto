feacf252f6e4911e4e9677fa875d35906db116a7
munkyung <munkyung0324@gmail.com>
2021-01-31 17:16:47 +0900

    refactor: 입력받은 당첨번호를 split 하여 로또번호 객체로 만들어주는 로직을 인터페이스의 디폴트 메소드로 이동 및 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,24 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.util.Validator;
+
 public interface Lotto {
     int LOTTO_NUMBER_SIZE = 6;
     String DELIMITER = "[ ,]+";
+
+    default List<LottoNumber> getWinnerLottoWithSplitting(String winnerLottoInput) {
+        String [] inputs = winnerLottoInput.split(DELIMITER);
+        List<LottoNumber> winnerLotto = new ArrayList<>();
+        for (String input : inputs) {
+            Validator.isInteger(input);
+            Validator.isNegative(input);
+            winnerLotto.add(new LottoNumber(Integer.parseInt(input)));
+        }
+        if (winnerLotto.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException("로또의 숫자는 6개입니다.");
+        }
+        return winnerLotto;
+    }
 }
