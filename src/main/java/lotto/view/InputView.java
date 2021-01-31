79617fb5a32e9277400ec365f0c1500d8e96ab79
quadam36 <quadam36@gmail.com>
2021-01-31 18:30:10 +0900

    feat: Add manualTicketCnt input method to view

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String TOTAL_COST_MSG = "구입금액을 입력해 주세요.";
+    private static final String MANUAL_TICKET_CNT_MSG = "수동으로 구매한 로또 수를 입력해 주세요.";
     private static final String ANSWER_NUMBERS_MSG = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MSG = "보너스 볼을 입력해 주세요";
 
@@ -14,6 +15,13 @@ public class InputView {
         Integer totalCost = scanner.nextInt();
         scanner.nextLine(); // To remove new line character.
         return totalCost;
+    }
+
+    public Integer inputManualTicketCnt() {
+        System.out.println(MANUAL_TICKET_CNT_MSG);
+        Integer manualTicketCnt = scanner.nextInt();
+        scanner.nextLine();
+        return manualTicketCnt;
     }
 
     public String inputAnswerNumbers() {
