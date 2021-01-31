1890489c966a5a662ab1565318a195feb56256a0
hakzzang <mbh930312@naver.com>
2021-01-31 20:04:34 +0900

    test: NumberPicker 테스트 코드 추가

--- src/test/java/lotto/domain/LottoNumbersPickerTest.java
+++ src/test/java/lotto/domain/LottoNumbersPickerTest.java
@@ -9,11 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumbersPickerTest {
 
-    @DisplayName("pick한 LottoNumber가 총 6개")
+    @DisplayName("NumberPicker를 통해 pick을 할 때, 6개의 볼이 나오는 지 확인하는 테스트")
     @Test
     void pickLottoNumbers() {
+        //given
+        NumberPicker numberPicker = new LottoNumbersPicker();
+
         // when
-        List<LottoNumber> lottoNumbers = LottoNumbersPicker.pick();
+        List<LottoNumber> lottoNumbers = numberPicker.pick();
 
         // then
         assertThat(lottoNumbers.size()).isEqualTo(6);
