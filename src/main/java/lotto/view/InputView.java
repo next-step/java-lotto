1ea94895c2204326b63e8cd5800a71b037f9e3d1
quadam36 <quadam36@gmail.com>
2021-01-31 21:06:06 +0900

    feat: Add manual buy related method to input view

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String TOTAL_COST_MSG = "구입금액을 입력해 주세요.";
     private static final String MANUAL_TICKET_CNT_MSG = "수동으로 구매한 로또 수를 입력해 주세요.";
+    private static final String MANUAL_TICKETS_INPUT_MSG = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String ANSWER_NUMBERS_MSG = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MSG = "보너스 볼을 입력해 주세요";
 
@@ -24,8 +25,16 @@ public class InputView {
         return manualTicketCnt;
     }
 
+    public void inputManualTicketsNumbers() {
+        System.out.println(MANUAL_TICKETS_INPUT_MSG);
+    }
+
     public String inputAnswerNumbers() {
         System.out.println(ANSWER_NUMBERS_MSG);
+        return inputTicketsNumbers();
+    }
+
+    public String inputTicketsNumbers() {
         return scanner.nextLine();
     }
 
