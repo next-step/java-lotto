b2d060fe07542e9e4f52f17b17e5c4ce71f8da12
donghee youn <qeadzc9503@naver.com>
2021-02-01 00:08:41 +0900

    refactor : OutputString Getter 삭제

--- src/main/java/lotto/wrapper/OutputString.java
+++ src/main/java/lotto/wrapper/OutputString.java
@@ -5,9 +5,6 @@ public class OutputString  {
     public OutputString(String str) {
         this.str = str;
     }
-    public String getString(){
-        return str;
-    }
     public void print(){
         System.out.println(str);
     }
