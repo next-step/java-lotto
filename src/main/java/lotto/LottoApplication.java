4371f4e797944ced553c1231b65b56b158a7b765
quadam36 <quadam36@gmail.com>
2021-01-31 21:32:02 +0900

    refactor: Extract controller to another class

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,53 +1,19 @@
 package lotto;
 
-import lotto.domain.LottoAnswer;
-import lotto.domain.LottoBuyer;
-import lotto.domain.LottoBuyerGenerator;
-import lotto.domain.LottoGame;
-import lotto.domain.LottoNumber;
-import lotto.domain.LottoTicket;
-import lotto.domain.LottoTicketGenerator;
+import lotto.controller.Controller;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.List;
-
 public class LottoApplication {
     public static void main(String[] args) {
-        InputView input = new InputView();
-        OutputView output = new OutputView();
-
-        // Buy Lotto Tickets
-        LottoBuyer buyer = LottoBuyerGenerator .generateLottoBuyer(
-            input.inputTotalCost(), input.inputManualTicketCnt()
+        Controller controller = new Controller(
+            new InputView(),
+            new OutputView()
         );
 
-        input.inputManualTicketsNumbers();
-        while (buyer.shouldBuyManualTicket()) {
-            buyer.buyManualTicket(
-                input.inputTicketsNumbers()
-            );
-        }
-        buyer.buyAutoTickets();
-
-        List<LottoTicket> tickets = buyer.getBoughtTickets();
-
-        output.printBuyData(buyer.getBuyData());
-
-        // Make Lotto Answer
-        LottoAnswer answer = new LottoAnswer(
-            LottoTicketGenerator.generateManualTicket(
-                input.inputAnswerNumbers()
-            ),
-            new LottoNumber(
-                input.inputBonusNumber()
-            )
-        );
-
-        // Play Lotto Game
-        LottoGame game = new LottoGame(answer, tickets);
-        output.printScoreBoardData(
-            game.calcScoreBoard().getScoreBoardData()
+        controller.playGame(
+            controller.buyTickets(),
+            controller.generateAnswer()
         );
     }
 }
---
+++ src/main/java/lotto/controller/Controller.java
@@ -0,0 +1,68 @@
+package lotto.controller;
+
+import lotto.domain.LottoAnswer;
+import lotto.domain.LottoBuyer;
+import lotto.domain.LottoBuyerGenerator;
+import lotto.domain.LottoGame;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTicketGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class Controller {
+    private InputView input;
+    private OutputView output;
+
+    public Controller(InputView input, OutputView output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public List<LottoTicket> buyTickets() {
+        LottoBuyer buyer = LottoBuyerGenerator.generateLottoBuyer(
+            input.inputTotalCost(), input.inputManualTicketCnt()
+        );
+
+        input.inputManualTicketsNumbers();
+        manualBuy(buyer);
+        autoBuy(buyer);
+        output.printBuyData(
+            buyer.getBuyData()
+        );
+
+        return buyer.getBoughtTickets();
+    }
+
+    private void manualBuy(LottoBuyer buyer) {
+        while (buyer.shouldBuyManualTicket()) {
+            buyer.buyManualTicket(
+                input.inputTicketsNumbers()
+            );
+        }
+    }
+
+    private void autoBuy(LottoBuyer buyer) {
+        buyer.buyAutoTickets();
+    }
+
+    public LottoAnswer generateAnswer() {
+        return new LottoAnswer(
+            LottoTicketGenerator.generateManualTicket(
+                input.inputAnswerNumbers()
+            ),
+            new LottoNumber(
+                input.inputBonusNumber()
+            )
+        );
+    }
+
+    public void playGame(List<LottoTicket> tickets, LottoAnswer answer) {
+        LottoGame game = new LottoGame(answer, tickets);
+        output.printScoreBoardData(
+            game.calcScoreBoard().getScoreBoardData()
+        );
+    }
+}
