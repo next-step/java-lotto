c6311c5be29f73cc4d3f06b66e6fae3b628f6fb4
hakzzang <mbh930312@naver.com>
2021-02-02 18:00:57 +0900

    feat: InputView에 수동으로 구매하는 입력 추

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,18 +4,35 @@ import lotto.domain.LottoNumber;
 import lotto.domain.Money;
 import lotto.resources.StringResources;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-
     public Money getPurchaseAmount() {
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
         System.out.println(StringResources.INPUT_PURCHASE_AMOUNT_HINT);
         return new Money(scanner.nextInt());
     }
 
+    public List<String> getManualPickedNumberLines() {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println(StringResources.INPUT_PURCHASE_MANUAL_LOTTO_COUNT_HINT);
+        final int manualLottoCount = Integer.parseInt(scanner.nextLine());
+
+        final List<String> manualPickedNumberLines = new ArrayList<>();
+        System.out.println(StringResources.INPUT_MANUAL_LOTTO_NUMBERS_HINT);
+        for(int i=0; i<manualLottoCount; i++){
+            String inputManualLottoLine = scanner.nextLine();
+            manualPickedNumberLines.add(inputManualLottoLine);
+        }
+
+        return manualPickedNumberLines;
+    }
+
     public String[] getWinningTicketNumbers() {
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println();
         System.out.println(StringResources.INPUT_LAST_WINNING_LOTTO_NUMBERS_HINT);
         String inputText = scanner.nextLine();
         String[] winningTicketNumbers = inputText.trim().split(",");
@@ -24,9 +41,10 @@ public class InputView {
     }
 
     public LottoNumber getBonusNumber() {
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
         System.out.println(StringResources.INPUT_BONUS_BALL_HINT);
         int bonusNumber = scanner.nextInt();
+
         return new LottoNumber(bonusNumber);
     }
 }
