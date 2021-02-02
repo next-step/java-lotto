71c9c9a3953451f8f3c408179942f4e2047da785
d36choi <yuio1126@gmail.com>
2021-02-02 17:54:10 +0900

    style: 고정적인 출력 문자열에 대해 상수화함.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -10,6 +10,12 @@ import lotto.domain.Prize;
 
 public class OutputView {
 
+    private static final String WINNING_RESULT_TEXT = "\n당첨 통계\n---------";
+    private static final String MATCHED_NUMBER_TEXT = "%d개 일치";
+    private static final String CASH_AND_PRIZE_TEXT = " (%d원) - %d개\n";
+    private static final String ROI_TEXT = "총 수익률은 %d 입니다.\n";
+    private static final String NUMBER_OF_EACH_TICKETS = "\n수동으로 %d장, 자동으로 %d개를 구매했습니다.";
+    private static final String FIVE_WITH_BONUS_CASE_TEXT = ", 보너스 볼 일치";
     public static void printNumberOfLottoInfo(int numberOfLotto) {
         System.out.println(numberOfLotto + "개를 구매하셨습니다.");
     }
@@ -23,28 +29,31 @@ public class OutputView {
     }
 
     public static void printWinningResult(Map<Prize, Integer> prizeBoard) {
-        System.out.println("당첨 통계");
-        System.out.println("---------");
+        System.out.println(WINNING_RESULT_TEXT);
         Arrays.stream(Prize.values()).forEach(value -> {
             if (value.getMatchedNumber() >= Prize.THREE.getMatchedNumber()) {
-                System.out.print(value.getMatchedNumber() + "개 일치");
+                System.out.printf(MATCHED_NUMBER_TEXT,value.getMatchedNumber());
                 printFiveWithBonusBallCase(value);
-                System.out.println(" (" + value.getCash() + "원) - " + prizeBoard.get(value) + "개");
+                System.out.printf(CASH_AND_PRIZE_TEXT, value.getCash(),prizeBoard.get(value));
             }
 
         });
     }
 
     public static void printReturnOnInvestment(double returnOnInvestment) {
-        System.out.println("총 수익률은 " + returnOnInvestment + "입니다.");
+        System.out.printf(ROI_TEXT, returnOnInvestment);
     }
 
     private static void printFiveWithBonusBallCase(Prize prize) {
         if (prize.equals(Prize.FIVE_WITH_BONUS)) {
-            System.out.print(", 보너스 볼 일치");
+            System.out.print(FIVE_WITH_BONUS_CASE_TEXT);
         }
     }
 
+    public static void printNumberOfEachTicket(int numberOfTicket, int numberOfManualLotto) {
+        int numberOfAutoLotto = numberOfTicket - numberOfManualLotto;
+        System.out.printf(NUMBER_OF_EACH_TICKETS,numberOfManualLotto,numberOfAutoLotto);
+    }
 }
 
 //당첨 통계
