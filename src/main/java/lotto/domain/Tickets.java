65ae9ce0f2466df6b2b5def55997bb172fd307f2
hakzzang <mbh930312@naver.com>
2021-02-02 17:59:43 +0900

    feat: Tickets 인터페이스에 구매 정보를 얻는 메소드 추가

--- src/main/java/lotto/domain/Tickets.java
+++ src/main/java/lotto/domain/Tickets.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface Tickets {
     List<LottoTicket> getLottoTickets();
 
+    String getPurchaseInformation();
+
     int getPurchaseAmount();
 
     int getPurchaseCount();
