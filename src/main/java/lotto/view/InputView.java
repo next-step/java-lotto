a31956552cdf609f5e13715c7d8e9866cd08fc6e
hakzzang <mbh930312@naver.com>
2021-02-02 22:23:42 +0900

    feat: 복권을 구매하는 과정에서 각종 에러를 처리하는 로직 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,50 +1,80 @@
 package lotto.view;
 
-import lotto.domain.LottoNumber;
-import lotto.domain.Money;
+import lotto.domain.*;
 import lotto.resources.StringResources;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
     public Money getPurchaseAmount() {
         final Scanner scanner = new Scanner(System.in);
         System.out.println(StringResources.INPUT_PURCHASE_AMOUNT_HINT);
-        return new Money(scanner.nextInt());
+
+        try {
+            return new Money(scanner.nextInt());
+        } catch (Exception exception) {
+            System.out.println(exception.getLocalizedMessage());
+            return getPurchaseAmount();
+        }
     }
 
-    public List<String> getManualPickedNumberLines() {
+    public LottoCount getManualLottoCount(Money money) {
         final Scanner scanner = new Scanner(System.in);
         System.out.println(StringResources.INPUT_PURCHASE_MANUAL_LOTTO_COUNT_HINT);
         final int manualLottoCount = Integer.parseInt(scanner.nextLine());
 
-        final List<String> manualPickedNumberLines = new ArrayList<>();
-        System.out.println(StringResources.INPUT_MANUAL_LOTTO_NUMBERS_HINT);
-        for(int i=0; i<manualLottoCount; i++){
-            String inputManualLottoLine = scanner.nextLine();
-            manualPickedNumberLines.add(inputManualLottoLine);
+        try {
+            return new LottoCount(money, manualLottoCount);
+        } catch (Exception exception) {
+            System.out.println(exception.getLocalizedMessage());
+            return getManualLottoCount(money);
         }
-
-        return manualPickedNumberLines;
     }
 
-    public String[] getWinningTicketNumbers() {
+    public ManualNumbersPicker getManualPickedNumberLines(LottoCount lottoCount) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println(StringResources.INPUT_MANUAL_LOTTO_NUMBERS_HINT);
+
+        final List<String> manualPickedNumberLines = IntStream
+                .range(0, lottoCount.getManualLottoCount())
+                .mapToObj(index -> scanner.nextLine())
+                .collect(Collectors.toList());
+
+        try {
+            return new ManualNumbersPicker(manualPickedNumberLines);
+        } catch (Exception exception) {
+            System.out.println(exception.getLocalizedMessage());
+            return getManualPickedNumberLines(lottoCount);
+        }
+    }
+
+    public LottoTicket getWinningTicketNumbers() {
         final Scanner scanner = new Scanner(System.in);
         System.out.println();
         System.out.println(StringResources.INPUT_LAST_WINNING_LOTTO_NUMBERS_HINT);
         String inputText = scanner.nextLine();
         String[] winningTicketNumbers = inputText.trim().split(",");
 
-        return winningTicketNumbers;
+        try {
+            return new LottoTicket(winningTicketNumbers);
+        } catch (Exception exception){
+            System.out.println(exception.getLocalizedMessage());
+            return getWinningTicketNumbers();
+        }
     }
 
     public LottoNumber getBonusNumber() {
         final Scanner scanner = new Scanner(System.in);
         System.out.println(StringResources.INPUT_BONUS_BALL_HINT);
-        int bonusNumber = scanner.nextInt();
 
-        return new LottoNumber(bonusNumber);
+        try{
+            return new LottoNumber(scanner.nextInt());
+        } catch (Exception exception){
+            System.out.println(exception.getLocalizedMessage());
+            return getBonusNumber();
+        }
     }
 }
