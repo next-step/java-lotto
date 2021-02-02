70eb5d82047cb596c26321d40939d65f125c9eac
jeongdaeun98 <uneap@sk.com>
2021-02-02 22:45:18 +0900

    refactor: modify LottoMachine to create Winning Number

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,21 +8,33 @@ import java.util.stream.IntStream;
 
 public class LottoMachine {
 
-    private final List<Integer> numbers;
+    private List<LottoNumber> lottoNumbers;
     private final static int MIN_LOTTO = 1;
     private final static int MAX_LOTTO = 46;
     private final static int FIRST_LOTTO_INDEX = 0;
     private final static int END_LOTTO_INDEX = 6;
 
     public LottoMachine() {
-        numbers = IntStream.range(MIN_LOTTO, MAX_LOTTO).boxed().collect(Collectors.toList());
+        lottoNumbers = IntStream
+            .range(MIN_LOTTO, MAX_LOTTO)
+            .boxed()
+            .map(LottoNumber::new)
+            .collect(
+                Collectors.toList()
+            );
     }
 
-    public List<Integer> generateTicket() {
-        Collections.shuffle(numbers);
-        List<Integer> ticket = numbers.subList(FIRST_LOTTO_INDEX, END_LOTTO_INDEX);
-        Collections.sort(ticket);
-        return ticket;
+    public Ticket generateTicket() {
+        Collections.shuffle(lottoNumbers);
+        lottoNumbers = lottoNumbers.subList(FIRST_LOTTO_INDEX, END_LOTTO_INDEX);
+        lottoNumbers.sort(LottoNumber::compareTo);
+        return new Ticket(lottoNumbers);
     }
 
+    public WinningNumber generateWinningTicket(String winningNumber) throws IllegalArgumentException {
+        return new WinningNumber(new Ticket(Arrays.stream(winningNumber.split(", "))
+            .map(Integer::parseInt)
+            .map(LottoNumber::new)
+            .collect(Collectors.toList())));
+    }
 }
