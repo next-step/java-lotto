791f784a72f61b4f89c09790149f8fc09f65d3d4
hakzzang <mbh930312@naver.com>
2021-02-02 17:48:35 +0900

    refacotring: LottoPurchase에 ManualNumberPicker 추가에 따른 변경
    - 구매하는 로직 변경
    - 복권을 얼마나 구매했는 지, 보여주는 로직 추가

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -3,28 +3,54 @@ package lotto.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LottoPurchase implements Purchase {
+    private static final String PURCHASE_INFORMATION_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final int LOTTO_TICKET_PRICE = 1_000;
-    private final NumberPicker numberPicker;
-    private final int purchaseAmount;
 
-    public LottoPurchase(final NumberPicker numberPicker, final int purchaseAmount) {
-        this.numberPicker = numberPicker;
-        this.purchaseAmount = purchaseAmount;
+    private final AutoNumbersPicker autoNumbersPicker;
+    private final ManualNumbersPicker manualNumbersPicker;
+
+    private final int purchaseAmount;
+    private int purchaseManualCount = 0;
+    private int purchaseAutoCount = 0;
+
+    public LottoPurchase(final AutoNumbersPicker autoNumbersPicker, final ManualNumbersPicker manualNumbersPicker, final Money money) {
+        this.manualNumbersPicker = manualNumbersPicker;
+        this.autoNumbersPicker = autoNumbersPicker;
+        this.purchaseAmount = money.getAmount();
+        calculateLottoCount();
     }
 
     @Override
     public List<LottoTicket> buy() {
-        int purchaseCount = purchaseAmount / LOTTO_TICKET_PRICE;
-        return IntStream
-                .range(0, purchaseCount)
-                .mapToObj(i -> new LottoTicket(numberPicker.pick()))
+        Stream<LottoTicket> manualNumbersPickerStream = IntStream
+                .range(0, purchaseManualCount)
+                .mapToObj(i -> manualNumbersPicker.pick());
+
+        Stream<LottoTicket> autoNumbersPickerStream = IntStream
+                .range(0, purchaseAutoCount)
+                .mapToObj(i -> autoNumbersPicker.pick());
+
+        return Stream
+                .concat(manualNumbersPickerStream, autoNumbersPickerStream)
                 .collect(Collectors.toList());
     }
 
     @Override
     public int getAmount() {
         return purchaseAmount;
+    }
+
+    private void calculateLottoCount(){
+        int purchaseTotalCount = purchaseAmount / LOTTO_TICKET_PRICE;
+        purchaseManualCount = manualNumbersPicker.getSize();
+        purchaseAutoCount = purchaseTotalCount - purchaseManualCount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(PURCHASE_INFORMATION_MESSAGE, purchaseManualCount, purchaseAutoCount);
     }
 }
