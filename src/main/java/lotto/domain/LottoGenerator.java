0846f4a580b13bee5075949e927145f94120230c
d36choi <yuio1126@gmail.com>
2021-02-02 17:37:26 +0900

    feat: purchaseManualAndAutoLottoTickets method로 수동,자동티켓 발급 로직 작성

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
+import lotto.view.InputView;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
-
+    //TODO: 수동으로 로또 번호 생성
     public static final int LOWER_BOUND_LOTTO = 1;
     public static final int UPPER_BOUND_LOTTO = 46;
     public static final int LOTTO_CAPACITY = 6;
@@ -28,7 +30,7 @@ public class LottoGenerator {
 
     }
 
-    public List<PlayersLotto> generateLottoTickets(int numberOfTicket) {
+    public List<PlayersLotto> generateLottoTicketsAutomatically(int numberOfTicket) {
         // 1로또를 만든다
         // 이 것을 리스트형태로 넘버오브티켓만큼 반복한다
         List<PlayersLotto> playersLottoTickets = new ArrayList<>();
@@ -40,4 +42,23 @@ public class LottoGenerator {
         return playersLottoTickets;
     }
 
+    public List<PlayersLotto> generateLottoTicketsManually(int numberOfTicket) {
+        List<PlayersLotto> ManualPlayersLottoTickets = new ArrayList<>();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for (int i = 0; i < numberOfTicket; i++) {
+            List <LottoNumber> manualLotto = Lotto.getLottoWithSplitting(InputView.getStringOfLotto());
+            ManualPlayersLottoTickets.add(new PlayersLotto(manualLotto));
+        }
+        return ManualPlayersLottoTickets;
+    }
+
+    public List<PlayersLotto> purchaseManualAndAutoLottoTickets(int numberOfTicket,int numberOfManualLotto) {
+        int NumberOfAutoLotto = numberOfTicket-numberOfManualLotto;
+        Validator.isNegative(NumberOfAutoLotto);
+        List<PlayersLotto> playersLottoTickets = generateLottoTicketsManually(numberOfManualLotto);
+        playersLottoTickets.addAll(generateLottoTicketsAutomatically(NumberOfAutoLotto));
+
+        return playersLottoTickets;
+
+    }
 }
