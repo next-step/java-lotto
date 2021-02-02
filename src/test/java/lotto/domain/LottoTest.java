cfb04464fc5532d6950341016a33ed95b7860942
jeongdaeun98 <uneap@sk.com>
2021-02-02 22:20:25 +0900

    test: modify LottoTest

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,9 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import lotto.util.NumberUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,53 +16,54 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class LottoTest {
     private Lotto lotto;
 
-    @DisplayName("가격을 기준으로 티켓 개수 테스트")
-    @ParameterizedTest
-    @MethodSource("provideCalculateTicketsTest")
-    public void calculateTicketsTest(int money, int tickets) {
-        assertEquals(tickets, new Price(money).calculateTickets());
-    }
-
-    private static Stream<Arguments> provideCalculateTicketsTest() {
-        return Stream.of(
-            Arguments.of(24000, 24),
-            Arguments.of(1000, 1)
-        );
-    }
-
     @DisplayName("티켓 별 당첨 통계내기 테스트")
     @ParameterizedTest
     @MethodSource("provideStatisticsTicketTest")
-    public void statisticsTicketTest(WinningNumber winningNumber, List<Ticket> tickets, HashMap<Revenue, Integer> revenueMap) {
-        lotto = new Lotto(winningNumber, tickets);
+    public void statisticsTicketTest(WinningInfo winningInfo, Tickets tickets, HashMap<Revenue, Integer> revenueMap) {
+        lotto = new Lotto(winningInfo, tickets);
         assertEquals(revenueMap, lotto.statisticsTicket());
     }
 
     private static Stream<Arguments> provideStatisticsTicketTest() {
         Map<Revenue, Integer> revenueMap = new HashMap<>();
-        revenueMap.put(Revenue.NOTHING,2);
+        revenueMap.put(Revenue.NOTHING, 2);
         Map<Revenue, Integer> revenueMap1 = new HashMap<>();
-        revenueMap1.put(Revenue.NOTHING,1);
-        revenueMap1.put(Revenue.THREE_MATCHES,1);
+        revenueMap1.put(Revenue.NOTHING, 1);
+        revenueMap1.put(Revenue.THREE, 1);
         Map<Revenue, Integer> revenueMap2 = new HashMap<>();
-        revenueMap2.put(Revenue.THREE_MATCHES,1);
-        revenueMap2.put(Revenue.FIVE_AND_BONUS_MATCHES,1);
+        revenueMap2.put(Revenue.THREE, 1);
+        revenueMap2.put(Revenue.FIVE_AND_BONUS, 1);
         return Stream.of(
-            Arguments.of(new WinningNumber(new Ticket(Arrays.asList(1, 2, 3, 4, 5, 6)),7)
-                ,Arrays.asList(
-                new Ticket(Arrays.asList(8, 21, 23, 41, 42, 43)),
-                new Ticket(Arrays.asList(3, 5, 11, 16, 32, 38)))
-                ,revenueMap),
-            Arguments.of(new WinningNumber(new Ticket(Arrays.asList(41, 42, 43, 4, 5, 7)),7)
-                ,Arrays.asList(
-                    new Ticket(Arrays.asList(8, 21, 23, 41, 42, 43)),
-                    new Ticket(Arrays.asList(3, 5, 11, 16, 32, 38)))
-                ,revenueMap1),
-            Arguments.of(new WinningNumber(new Ticket(Arrays.asList(41, 42, 43, 4, 5, 7)),38)
-                ,Arrays.asList(
-                    new Ticket(Arrays.asList(8, 21, 23, 41, 42, 43)),
-                    new Ticket(Arrays.asList(41, 42, 43, 4, 5, 38)))
-                ,revenueMap2)
+            Arguments.of(new WinningInfo(new WinningNumber(
+                    new Ticket(Stream.of(1, 2, 3, 4, 5, 6).map(LottoNumber::new).collect(
+                        Collectors.toList()))
+                ), new LottoNumber(7))
+                , new Tickets(Arrays.asList(
+                    new Ticket(Stream.of(8, 21, 23, 41, 42, 43).map(LottoNumber::new).collect(
+                        Collectors.toList())),
+                    new Ticket(Stream.of(3, 5, 11, 16, 32, 38).map(LottoNumber::new).collect(
+                        Collectors.toList()))
+                )), revenueMap),
+            Arguments.of(new WinningInfo(new WinningNumber(
+                   new Ticket(Stream.of(41, 42, 43, 4, 5, 7).map(LottoNumber::new).collect(
+                       Collectors.toList()))
+                ), new LottoNumber(7))
+                , new Tickets(Arrays.asList(
+                    new Ticket(Stream.of(8, 21, 23, 41, 42, 43).map(LottoNumber::new).collect(
+                        Collectors.toList())),
+                    new Ticket(Stream.of(3, 5, 11, 16, 32, 38).map(LottoNumber::new).collect(
+                        Collectors.toList())))
+                ), revenueMap1),
+            Arguments.of(new WinningInfo(new WinningNumber(
+                    new Ticket(Stream.of(41, 42, 43, 4, 5, 7).map(LottoNumber::new).collect(
+                        Collectors.toList()))
+                ), new LottoNumber(38))
+                , new Tickets(Arrays.asList(
+                    new Ticket(Stream.of(8, 21, 23, 41, 42, 43).map(LottoNumber::new).collect(
+                        Collectors.toList())),
+                    new Ticket(Stream.of(41, 42, 43, 4, 5, 38).map(LottoNumber::new).collect(
+                        Collectors.toList())))
+                ), revenueMap2)
         );
     }
 
@@ -76,9 +78,9 @@ public class LottoTest {
 
     private static Stream<Arguments> provideCalculateTotalPrizeTest() {
         Map<Revenue, Integer> revenueMap = new HashMap<>();
-        revenueMap.put(Revenue.NOTHING,1);
-        revenueMap.put(Revenue.THREE_MATCHES,1);
-        revenueMap.put(Revenue.FIVE_AND_BONUS_MATCHES, 2);
+        revenueMap.put(Revenue.NOTHING, 1);
+        revenueMap.put(Revenue.THREE, 1);
+        revenueMap.put(Revenue.FIVE_AND_BONUS, 2);
         return Stream.of(
             Arguments.of(revenueMap, 60005000)
         );
