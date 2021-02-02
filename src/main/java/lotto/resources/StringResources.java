ad5d0d9cad7bfe70bc48ab1b0d89ef90ac97eda5
hakzzang <mbh930312@naver.com>
2021-02-02 18:02:08 +0900

    refactoring: 뷰 관련 문자 리소스 추가

--- src/main/java/lotto/resources/StringResources.java
+++ src/main/java/lotto/resources/StringResources.java
@@ -2,6 +2,8 @@ package lotto.resources;
 
 public class StringResources {
     public static final String INPUT_PURCHASE_AMOUNT_HINT = "구입 금액을 입력해주세요.";
+    public static final String INPUT_PURCHASE_MANUAL_LOTTO_COUNT_HINT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    public static final String INPUT_MANUAL_LOTTO_NUMBERS_HINT = "수동으로 구매할 번호를 입력해주세요.";
     public static final String INPUT_LAST_WINNING_LOTTO_NUMBERS_HINT = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_BALL_HINT = "보너스 볼을 입력해 주세요.";
 
