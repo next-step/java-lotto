c20b537c2a45d469eb80546d82b6a063d3de8249
jeongdaeun98 <uneap@sk.com>
2021-02-02 22:44:33 +0900

    refactor: remove NumberUtils to move domain

--- src/main/java/lotto/util/NumberUtils.java
+++ src/main/java/lotto/util/NumberUtils.java
@@ -1,51 +1,25 @@
 package lotto.util;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class NumberUtils {
 
-    private static final int FIRST_RANGE_LOTTO_NUMBER = 0;
-    private static final int END_RANGE_LOTTO_NUMBER = 46;
-
-    public static Integer returnInteger(String string) {
+    public static Integer returnInteger(String string) throws IllegalArgumentException {
         try {
             return Integer.parseInt(string);
         } catch (NumberFormatException e) {
-            return null;
+            throw new IllegalArgumentException();
         }
     }
 
     public static Double calculateYield(int inputPrice, int income) {
         BigDecimal bd = new BigDecimal((double) income / inputPrice);
-        return bd.setScale(2, BigDecimal.ROUND_FLOOR).doubleValue();
+        return bd
+            .setScale(2, BigDecimal.ROUND_FLOOR)
+            .doubleValue();
     }
 
-    public static boolean isNumberUnique(List<String> numbers) {
-        return numbers
-            .stream()
-            .distinct()
-            .count() == numbers.size();
-    }
 
-    public static boolean isNumberInRange(List<String> numbers) {
-        return numbers
-            .stream()
-            .map(Integer::parseInt)
-            .allMatch(NumberUtils::checkLottoNumber);
-    }
-
-    public static boolean checkLottoNumber(int num) {
-        return FIRST_RANGE_LOTTO_NUMBER < num && num < END_RANGE_LOTTO_NUMBER;
-    }
-
-    public static List<Integer> convertStringToIntegerList(String winningNumber) {
-        return Arrays.stream(winningNumber.split(", "))
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
-    }
     public static int convertNullToNumber(Integer value) {
         if (value == null) {
             return 0;
