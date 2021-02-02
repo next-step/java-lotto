c7e0634a60007614c613e973f60e20d0756d9af9
jeongdaeun98 <uneap@sk.com>
2021-02-02 22:44:02 +0900

    feat: create WinningInfo

---
+++ src/main/java/lotto/domain/WinningInfo.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public class WinningInfo {
+
+    private final WinningNumber winningNumber;
+    private final LottoNumber bonusNumber;
+
+    public WinningInfo(WinningNumber winningNumber, LottoNumber bonusNumber) {
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public WinningInfo() {
+        winningNumber = new WinningNumber();
+        bonusNumber = new LottoNumber();
+    }
+
+    public long matchWinning(Ticket ticket) {
+        return ticket.matchWinningNumber(winningNumber);
+    }
+
+    public boolean matchBonus(Ticket ticket) {
+        return ticket.matchBonus(bonusNumber);
+    }
+}
