fce2a2b68825cad4877626cd7a643b74bb5c584a
koeonyack <covenant.ko@kakao.com>
2021-02-03 21:22:34 +0900

    feat: N개의 커스텀 로또 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
 import org.apache.commons.lang3.StringUtils;
+import sun.rmi.server.InactiveGroupException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -11,6 +14,8 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     private static final String INPUT_MESSAGE_LOTTO_MONEY = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MESSAGE_CUSTOM_INPUT_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MESSAGE_CUSTOM_INPUT_LOTTO_NUMBER = "수동으로 구매할 로또 번호를 입력해주세요";
     private static final String INPUT_MESSAGE_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_MESSAGE_BONUS_BALL = "보너스 볼을 입력해 주세요.";
     public static final String REGEX = ", ";
@@ -20,12 +25,29 @@ public class InputView {
         return scanner.nextInt();
     }
 
+    public static int getCustomInputLottoCount() {
+        System.out.println(INPUT_MESSAGE_CUSTOM_INPUT_LOTTO_COUNT);
+        return scanner.nextInt();
+    }
+
+    public static List<Lotto> getCustomLottos(int customCount) {
+        if (customCount == 0) return null;
+
+        System.out.println(INPUT_MESSAGE_CUSTOM_INPUT_LOTTO_NUMBER);
+        scanner.nextLine();
+        String lottoNumber = scanner.nextLine();
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i=0; i < customCount; i++) {
+            lottos.add(new Lotto(getCommonLottoNumbers(lottoNumber)));
+        }
+
+        return lottos;
+    }
+
     public static List<Integer> getWinningNumbers() {
         System.out.println(INPUT_MESSAGE_WINNING_NUMBER);
         scanner.nextLine();
-
         String winningLottoNumber = scanner.nextLine();
-
         return getCommonLottoNumbers(winningLottoNumber);
     }
 
@@ -34,8 +56,8 @@ public class InputView {
         return scanner.nextInt();
     }
 
-    public static List<Integer> getCommonLottoNumbers(String winningLottoNumber) {
-        return Arrays.stream(winningLottoNumber.split(REGEX))
+    public static List<Integer> getCommonLottoNumbers(String lottoNumber) {
+        return Arrays.stream(lottoNumber.split(REGEX))
                 .map(InputView::toInt)
                 .collect(Collectors.toList());
     }
