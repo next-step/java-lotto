24e45e7bc3643b4a89377b190e163ba80045ef3a
jeongdaeun98 <uneap@sk.com>
2021-02-03 23:22:31 +0900

    feat: create RevenueCluster

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,18 +13,13 @@ public class Lotto {
         this.tickets = tickets;
     }
 
-    public Lotto() {
-        tickets = new Tickets();
-        winningInfo = new WinningInfo();
-    }
-
-    public Map<Revenue, Integer> statisticsTicket() {
+    public RevenueCluster statisticsTicket() {
         Map<Revenue, Integer> revenueMap = new HashMap<>();
         tickets.getTicketStream().forEach(ticket -> {
             Revenue matchingLotto = Revenue.checkTheNumberOfMatchingLotto(ticket, winningInfo);
             revenueMap.computeIfPresent(matchingLotto, ((revenue, count) -> ++count));
             revenueMap.putIfAbsent(matchingLotto, 1);
         });
-        return revenueMap;
+        return new RevenueCluster(revenueMap);
     }
 }
