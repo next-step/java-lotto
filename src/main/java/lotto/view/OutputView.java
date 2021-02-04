7b40636fb091fc1c77366e40bb8ea8e15f2b1bee
KimBB <tree9295@gmail.com>
2021-02-05 00:58:16 +0900

    refactor: 결과 출력 코드 간결화
    - Rank.values 사용
    - stream 사용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import lotto.domain.LottoTicket;
 import lotto.domain.Rank;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class OutputView {
@@ -18,11 +19,9 @@ public class OutputView {
     public static void printLottoServiceResult(Map<Rank, Integer> lottoRankingStatus, double interestRate) {
         System.out.println("당첨 통계");
         System.out.println("--------");
-        System.out.printf("3개 일치 (%d원) - %d\n", Rank.FIFTH.getReward(), lottoRankingStatus.get(Rank.FIFTH));
-        System.out.printf("4개 일치 (%d원) - %d\n", Rank.FOURTH.getReward(), lottoRankingStatus.get(Rank.FOURTH));
-        System.out.printf("5개 일치 (%d원) - %d\n", Rank.THIRD.getReward(), lottoRankingStatus.get(Rank.THIRD));
-        System.out.printf("5개 일치, 보너스 볼 일치 (%d원) - %d\n", Rank.SECOND.getReward(), lottoRankingStatus.get(Rank.SECOND));
-        System.out.printf("6개 일치 (%d원) - %d\n", Rank.FIRST.getReward(), lottoRankingStatus.get(Rank.FIRST));
+        Arrays.stream(Rank.values())
+                .filter(rank -> !rank.equals(Rank.NOTHING))
+                .forEach(rank -> System.out.printf("%s 일치 (%d원) - %d\n", rank.getMessage(), rank.getReward(), lottoRankingStatus.get(rank)));
         System.out.printf("총 수익률은 %.1f 입니다.\n", interestRate);
     }
 }
