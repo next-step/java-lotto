2615cfb3a5009811b035d0fe3525936c92b938f4
d36choi <yuio1126@gmail.com>
2021-02-04 21:51:35 +0900

    fix: contains 메소드를 추가하고 getMatchedCount 로직을 간소화시킴
    - 그외에, PlayersLotto 가 Lotto 로 변경됨에 따라 인스턴스 이름을 WinnersLotto 로 변경해, 명칭의 모호함을 수정하고자 했음.

--- src/main/java/lotto/domain/WinnerLotto.java
+++ src/main/java/lotto/domain/WinnerLotto.java
@@ -5,46 +5,42 @@ import java.util.List;
 public class WinnerLotto {
     // extends keyword 제거됨
     private final LottoNumber bonusBall;
-//    private final List<LottoNumber> numbers;
-    private final PlayersLotto playersLotto;
-    public WinnerLotto(LottoNumber bonusBall, PlayersLotto playersLotto) {
+    private final Lotto winnersLotto;
+    public WinnerLotto(LottoNumber bonusBall, Lotto winnersLotto) {
 
-        checkLottoContainsBonusball(bonusBall,playersLotto.getNumbers());
-        this.playersLotto = playersLotto;
+        checkLottoContainsBonusball(bonusBall, winnersLotto.getNumbers());
+        this.winnersLotto = winnersLotto;
         this.bonusBall = bonusBall;
-//        this.numbers = numbers;
     }
 
     public List<LottoNumber> getNumbers() {
-        return playersLotto.getNumbers();
+        return winnersLotto.getNumbers();
     }
 
     public LottoNumber getBonusBall() {
         return bonusBall;
     }
 
-    private void checkLottoContainsBonusball (LottoNumber bonusBall, List<LottoNumber> numbers) {
+    private void checkLottoContainsBonusball (LottoNumber bonusBalla, List<LottoNumber> numbers) {
         if (numbers.contains(bonusBall)) {
             throw new IllegalArgumentException("보너스번호가 로또번호와 중복됩니다.");
         }
     }
 
     public static WinnerLotto of (LottoNumber bonusBall, List<LottoNumber> numbers) {
-        return new WinnerLotto(bonusBall,PlayersLotto.of(numbers));
+        return new WinnerLotto(bonusBall, Lotto.of(numbers));
+    }
+
+    private boolean contains(LottoNumber lottoNumber) {
+        return getNumbers().contains(lottoNumber);
     }
 
     public int getMatchedCount(List<LottoNumber> lottoNumber) {
-        // DONE: 5개 적중 시 보너스 볼을 추가 검증해서 적중수가 5+1지(보너스볼)인지, 6인지를 구분짓는 로직이 필요함.
-        // 여기에서는 갯수만 카운팅 하고 보너스 볼에 대한 로직은 getPrizeForEachLotto 에서 검증하는 것이 맞는것 같아 수정해 보았습니다.
-        int matchedCount = (int) lottoNumber.stream()
-                .filter(this.playersLotto.getNumbers()::contains)
-                .count();
-        return matchedCount;
+        return (int) lottoNumber.stream().filter(this::contains).count();
     }
     public Prize getPrizeForEachLotto(List<LottoNumber> lottoNumber) {
         int matchCnt = getMatchedCount(lottoNumber);
         // DONE : 5개+보너스볼1개 인 경우의 분기 처리 구현
-
         return Prize.getPrizeByMatchedNumber(matchCnt,lottoNumber, bonusBall);
     }
 
