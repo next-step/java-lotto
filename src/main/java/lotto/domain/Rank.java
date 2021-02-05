5873da0d15c6f8be88c6b1132d67202f114643be
gyumin-kim <starkying@gmail.com>
2021-02-05 15:01:31 +0900

    refactor: `Rank`.`matchCount`의 타입을 Integer로 변경
    * NONE인 경우 `matchCount`를 null로 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -15,7 +15,7 @@ public enum Rank {
     THIRD(5, "5개 일치", BigDecimal.valueOf(1_500_000L)),
     FOURTH(4, "4개 일치", BigDecimal.valueOf(50_000L)),
     FIFTH(3, "3개 일치", BigDecimal.valueOf(5_000L)),
-    NONE(0, "꽝", BigDecimal.valueOf(0L)),   // TODO: NONE 없애도 될지 고민
+    NONE(null, "꽝", BigDecimal.valueOf(0L)),
     ;
 
     private static final Map<Integer, Rank> ranks = new HashMap<>();
@@ -26,11 +26,11 @@ public enum Rank {
         }
     }
 
-    private final int matchCount;
+    private final Integer matchCount;
     private final String matchCountMessage;
     private final BigDecimal amount;
 
-    Rank(final int matchCount, final String matchCountMessage, final BigDecimal amount) {
+    Rank(final Integer matchCount, final String matchCountMessage, final BigDecimal amount) {
         this.matchCount = matchCount;
         this.matchCountMessage = matchCountMessage;
         this.amount = amount;
