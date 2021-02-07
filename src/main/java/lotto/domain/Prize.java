710a07882e9b87701424aed2983463468a66a3d3
d36choi <yuio1126@gmail.com>
2021-02-07 15:10:10 +0900

    fix: Remove redundant method in Prize

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -9,14 +9,14 @@ public enum Prize {
     //5개 일치, 보너스 볼 일치(30000000원) - 0개
     //6개 일치 (2000000000원)- 0개
 
-    ZERO(0,0),
-    ONE(1,0),
-    TWO(2,0),
-    THREE(3,5000),
-    FOUR(4,50000),
-    FIVE(5,1500000),
-    FIVE_WITH_BONUS(5,30000000),
-    SIX(6,2000000000);
+    ZERO(0, 0),
+    ONE(1, 0),
+    TWO(2, 0),
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    FIVE_WITH_BONUS(5, 30000000),
+    SIX(6, 2000000000);
 
     private final int matchedNumber;
     private final int cash;
@@ -35,7 +35,6 @@ public enum Prize {
     }
 
 
-
     @Override
     public String toString() {
         return "Prize{" +
@@ -45,7 +44,7 @@ public enum Prize {
     }
 
     private static Prize getPrizeByOnlyMatchedNumber(int matchedNumber) {
-        for(Prize prize : Prize.values()) {
+        for (Prize prize : Prize.values()) {
             if (matchedNumber == prize.matchedNumber) {
                 return prize;
             }
@@ -53,7 +52,7 @@ public enum Prize {
         throw new IllegalArgumentException("no matched prize");
     }
 
-    public static Prize getPrizeByMatchedNumber(final int matchedNumber,boolean isBonus) {
+    public static Prize getPrizeByMatchedNumber(final int matchedNumber, boolean isBonus) {
         Prize returnPrize = getPrizeByOnlyMatchedNumber(matchedNumber);
         if (returnPrize.matchedNumber == Prize.FIVE.matchedNumber && isBonus) {
             return Prize.FIVE_WITH_BONUS;
@@ -61,10 +60,5 @@ public enum Prize {
         return returnPrize;
 
     }
-
-//    public static boolean isFiveWithBonus(final int matchedNumber,Lotto lotto,LottoNumber bonusBall) {
-////        List<LottoNumber> lottoNumbers = lotto.getNumbers();
-//        return matchedNumber == Prize.FIVE.matchedNumber && lotto.contains(bonusBall);
-//    }
 
 }
