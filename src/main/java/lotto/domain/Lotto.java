3c63f6fff8067e3fdbfa34eb623b8286d5d02700
munkyung <munkyung0324@gmail.com>
2021-02-08 02:08:05 +0900

    refactor: 메서드 사용 확장성을 위해 이름 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,17 +9,17 @@ public interface Lotto {
     int LOTTO_NUMBER_SIZE = 6;
     String DELIMITER = "[ ,]+";
 
-    default List<LottoNumber> getLottoNumbersWithSplitting(String winnerLottoInput) {
-        String[] inputs = winnerLottoInput.split(DELIMITER);
-        List<LottoNumber> winnerLotto = new ArrayList<>();
+    default List<LottoNumber> getLottoNumberListWithSplitting(String lottoNumbersInput) {
+        String[] inputs = lottoNumbersInput.split(DELIMITER);
+        List<LottoNumber> lottoTicket = new ArrayList<>();
         for (String input : inputs) {
             Validator.isInteger(input);
             Validator.isNegative(input);
-            winnerLotto.add(new LottoNumber(Integer.parseInt(input)));
+            lottoTicket.add(new LottoNumber(Integer.parseInt(input)));
         }
-        if (winnerLotto.size() != LOTTO_NUMBER_SIZE) {
+        if (lottoTicket.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("로또의 숫자는 6개입니다.");
         }
-        return winnerLotto;
+        return lottoTicket;
     }
 }
