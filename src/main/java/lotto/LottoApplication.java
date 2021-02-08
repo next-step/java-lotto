35fac37ef51681b81d30c6912e6f9d963e6f5753
donghee youn <qeadzc9503@naver.com>
2021-02-09 01:03:46 +0900

    refactor : 외부에 있던 로직들 중 buyer 클래스가 해야 할 일을 buyer 클래스 메서드로 집어넣어 줌

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -13,13 +13,10 @@ import java.util.List;
 public class LottoApplication {
     private static Lotto lotto;
     private static Buyer buyer;
-    private static List<LottoTicket> tickets;
     private static CheckResult checker;
     static Count lottos;
     static Money amount;
     static Count lottosManual;
-    static Money randomAmount;
-    static Count lottosRandom;
     static List<String> lottosManualRaw;
 
     public static void main(String[] args) {
@@ -44,16 +41,15 @@ public class LottoApplication {
         // 수동 구매 수량을 결정.
         OutputView.printManual(); // 수동으로 구매 할 수를 입력하세요.
         lottosManual = InputView.inputAmountManual(); // 수동 입력.
-        randomAmount = Money.subManual(amount, lottosManual);
-        lottosRandom = Count.split(lottos, lottosManual); // lottos에 전체 수량 - 수동 수량 을 해 주는 메서드 생성. 메서드는 new Count(res)를 반환.
-
+        buyer.setLottosManual(lottosManual);
+        buyer.splitManual(amount, lottos, lottosManual);
         OutputView.printSelectManual(); // 로또 수동 입력 하라는 프린트.
-        lottosManualRaw = InputView.selectLottosManually(lottosManual);
-        tickets = LottoGenerator.rawToTickets(lottosManualRaw); // 수동으로 입력하기.
+        lottosManualRaw = InputView.selectLottosManually(buyer.getLottosManual());
+        buyer.buyTicketsManual(lottosManualRaw);
     }
 
-    private static void buyLorroRandom() {
-        tickets.addAll(buyer.buyLotto(randomAmount));// 자동 구매 수량에 따라 티켓 생성.
+    private static void buyLottoRandom() {
+        buyer.buyTicketsRandom();
     }
 
     private static void buyLotto() {
@@ -64,17 +60,17 @@ public class LottoApplication {
         // 구매 가능 수량이 lottos로 담김.
         lottos = buyer.matchPriceAndPayment(amount);
         buyLottoManual();
-        buyLorroRandom();
+        buyLottoRandom();
 
         // 구매
         // 구매 수량 확인
-        OutputView.printBuyerTickets(lottosManual, lottosRandom); // 수동으로 a 개, 자동으로 b 개 구매했습니다.
-        OutputView.printBuyTicketsNumbers(tickets);
+        OutputView.printBuyerTickets(buyer.getLottosManual(), buyer.getLottosRandom()); // 수동으로 a 개, 자동으로 b 개 구매했습니다.
+        OutputView.printBuyTicketsNumbers(buyer.getTickets());
     }
 
     private static void confirmLotto() {
         // 로또 체크 할 인스턴스 생성
-        checker = new CheckResult(Count.sum(lottosManual, lottosRandom));
+        checker = new CheckResult(Count.sum(buyer.getLottosManual(), buyer.getLottosRandom()));
         // 로또 당첨 번호
         OutputView.printLottoMsg();
 
@@ -82,7 +78,7 @@ public class LottoApplication {
         inputBonusLottoNumber();
 
         // Match Lotto
-        checker.countUpMatch(lotto, tickets);
+        checker.countUpMatch(lotto, buyer.getTickets());
     }
 
     private static void inputWinningLottoNumbers() {
@@ -98,12 +94,10 @@ public class LottoApplication {
         lotto.setBonusNumber(bonusLottoNumber);
     }
 
-
     private static void resultLotto() {
         // return result to output view
         List<Count> res = checker.getResult();
-        Double resRevenue = checker.calculateWinningRevenue();
-        // 로또 결과 이전 메시지
+        Double resRevenue = checker.calculateWinningRevenue();        // 로또 결과 이전 메시지
         OutputView.printBeforeResult();
         // 로또 결과 메시지
         OutputView.printResult(res);
--- src/main/java/lotto/domain/Buyer.java
+++ src/main/java/lotto/domain/Buyer.java
@@ -3,20 +3,48 @@ package lotto.domain;
 import lotto.wrapper.Count;
 import lotto.wrapper.Money;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Buyer {
-    public Count matchPriceAndPayment(Money input) {
-        return new Count( input.getMoney() / Lotto.LOTTO_PRICE.getMoney() );
+    private List<LottoTicket> tickets;
+    private Count lottosManual;
+    private Money randomAmount;
+    private Count lottosRandom;
+
+    public Count getLottosManual() {
+        return lottosManual;
     }
 
-    public List<LottoTicket> buyLotto(Money input) {
-        List<LottoTicket> lottos = new ArrayList<>();
-        Count lottoCount = matchPriceAndPayment(input);
-        for (int i = 0; i < lottoCount.getCount(); i++) {
-            lottos.add(LottoGenerator.generateLotto(i));
-        }
-        return lottos;
+    public void setLottosManual(Count lottosManual) {
+        this.lottosManual = lottosManual;
     }
+
+    public Count getLottosRandom() {
+        return lottosRandom;
+    }
+
+    public List<LottoTicket> getTickets() {
+        return tickets;
+    }
+
+    public Count matchPriceAndPayment(Money input) {
+        return new Count(input.getMoney() / Lotto.LOTTO_PRICE.getMoney());
+    }
+
+    public void splitManual(Money amount, Count lottos, Count lottosManual) {
+        randomAmount = Money.subManual(amount, lottosManual);
+        lottosRandom = Count.split(lottos, lottosManual); // lottos에 전체 수량 - 수동 수량 을 해 주는 메서드 생성. 메서드는 new Count(res)를 반환.
+    }
+
+    public void buyTicketsManual(List<String> lottosManualRaw) {
+        tickets = LottoGenerator.rawToTickets(lottosManualRaw); // 수동으로 입력하기.
+    }
+
+    public void buyTicketsRandom() {
+        Count lottoCount = matchPriceAndPayment(randomAmount);
+        for (int i = 0; i < lottoCount.getCount(); i++) {
+            tickets.add(LottoGenerator.generateLotto(i));
+        }
+    }
+
 }
