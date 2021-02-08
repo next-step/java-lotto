7db7423428114938fff1bccb384834daeeb65fe1
koeonyack <covenant.ko@kakao.com>
2021-02-08 23:48:31 +0900

    refactor: lottos를 적용한 컨트롤러 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -15,16 +15,15 @@ public class LottoGame {
 
         LottoMachine lottoMachine = new LottoMachine();
         lottoMachine.initializeLottoCount(money, customLottoCount);
-        List<Lotto> lottos = lottoMachine.generateLottos(InputView.getCustomLottos(customLottoCount));
-
-        ResultView.printPurchaseNumber(lottos.size());             // 개를 구입했습니다.
-        ResultView.printGeneratedLottos(lottos);                   // 생성된 로또 목록
+        Lottos lottos = new Lottos(lottoMachine.generateLottos(InputView.getCustomLottos(customLottoCount)));
+        ResultView.printPurchaseNumber(lottos.getLottos().size());             // 개를 구입했습니다.
+        ResultView.printGeneratedLottos(lottos.getLottos());                   // 생성된 로또 목록
 
         List<Integer> winningLottoNumbers = InputView.getWinningNumbers(); // 당첨 번호를 입력해주세요
         int bonusBall = InputView.getBonusBall();                          // 보너스 볼을 입력해 주세요.
         WinningLotto winningLotto = WinningLotto.generate(new Lotto(winningLottoNumbers), bonusBall);
 
-        Map<WinningType, Integer> matchResult = LottoCalculation.calculateResult(winningLotto, lottos); // 로또 결과 계산
-        ResultView.printResult(matchResult, money);                                                     // 로또 결과 출력
+        Map<WinningType, Integer> matchResult = lottos.calculateResult(winningLotto); // 로또 결과 계산
+        ResultView.printResult(matchResult, money);                                   // 로또 결과 출력
     }
 }
