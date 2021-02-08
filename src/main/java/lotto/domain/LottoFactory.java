b43dc7d06079bed91d2b4f2218bd8893e95cb449
d36choi <yuio1126@gmail.com>
2021-02-08 22:31:12 +0900

    style: remove No Use method in LottoFactory.java

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -7,13 +7,6 @@ import java.util.stream.Collectors;
 
 public class LottoFactory {
 
-    public static List<LottoNumber> createListOfLottoNumber2 (int [] numbers) {
-        List <LottoNumber> lottoNumbers = new ArrayList<>();
-        Arrays.stream(numbers).forEach(number -> {
-            lottoNumbers.add(LottoNumber.of(number));
-        });
-        return lottoNumbers;
-    }
     public static Lotto createLotto(List<Integer> numbers) {
         List <LottoNumber> lottoNumbers = numbers.stream().map(LottoNumber::of).collect(Collectors.toList());
         return Lotto.of(lottoNumbers);
