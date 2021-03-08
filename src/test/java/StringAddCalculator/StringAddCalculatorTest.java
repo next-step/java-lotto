5ee9a4c4e8d521fed832cf04958a504ca9f833ce
JuJinPark <wnwls1216@gmail.com>
2021-03-08 23:14:24 +0900

    [refactor] refactored to comply with convention

--- src/test/java/StringAddCalculator/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculator/StringAddCalculatorTest.java
@@ -13,8 +13,8 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {":0", "'':0", "' ':0"}, delimiter = ':')
     @DisplayName("빈문자열 또는 널은 0으로 취급하는지 테스트")
-    void splitAndSum_null_or_empty_string(String input,int expected) {
-        int result=StringAddCalculator.splitAndSum(input);
+    void splitAndSum_null_or_empty_string(String input, int expected) {
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -22,7 +22,7 @@ public class StringAddCalculatorTest {
     @DisplayName("쉼표구분자로 문자 합산 테스트")
     void splitAndSum_with_comma_delimiter() {
         String input = "1,2";
-        int result=StringAddCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(3);
     }
 
@@ -30,7 +30,7 @@ public class StringAddCalculatorTest {
     @DisplayName("콜론 구분자로 문자 합산 테스트")
     void splitAndSum_with_colon_delimiter() {
         String input = "1:2";
-        int result=StringAddCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(3);
     }
 
@@ -38,7 +38,7 @@ public class StringAddCalculatorTest {
     @DisplayName("콜론과쉼표 복합 구분자로 문자 합산 테스트")
     void splitAndSum_with_comma_and_colon_delimiter() {
         String input = "1:2,4";
-        int result=StringAddCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(7);
     }
 
@@ -46,7 +46,7 @@ public class StringAddCalculatorTest {
     @DisplayName("구분자 없이 문자 합산 테스트")
     void splitAndSum_without_any_delimiter() {
         String input = "1";
-        int result=StringAddCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(1);
     }
 
@@ -54,22 +54,22 @@ public class StringAddCalculatorTest {
     @DisplayName("커스텀 구분자로 문자 합산 테스트")
     void splitAndSum_with_custom_delimiter() {
         String input = "//;\n1;5;3";
-        int result=StringAddCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(9);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"//;\n1;5;-344", "5,-322","422:-23,1"})
+    @ValueSource(strings = {"//;\n1;5;-344", "5,-322", "422:-23,1"})
     @DisplayName("음수 포함시 예외 발생 테스트")
     void splitAndSum_with_negative_number(String input) {
-        assertThatThrownBy(()-> StringAddCalculator.splitAndSum(input));
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"//;\nadd;bdd;-3", "5,asdf","4:gasdfa,1"})
+    @ValueSource(strings = {"//;\nadd;bdd;-3", "5,asdf", "4:gasdfa,1"})
     @DisplayName("숫자가 아닌 문자 포함시 예외 발생 테스트")
     void splitAndSum_with_not_numeric_input(String input) {
-        assertThatThrownBy(()-> StringAddCalculator.splitAndSum(input));
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input));
     }
 
 
