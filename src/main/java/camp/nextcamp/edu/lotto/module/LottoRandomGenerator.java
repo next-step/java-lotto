444919366767cdb6866a5cdafc5758e682ebb756
jeonyeonbin <qtrgor@gmail.com>
2021-03-09 22:36:02 +0900

    refactor: LottoRandomGenerator 변경

--- src/main/java/camp/nextcamp/edu/lotto/module/LottoRandomGenerator.java
+++ src/main/java/camp/nextcamp/edu/lotto/module/LottoRandomGenerator.java
@@ -1,19 +1,20 @@
 package camp.nextcamp.edu.lotto.module;
 
-import java.util.List;
+import static camp.nextcamp.edu.lotto.constants.LottoConstants.*;
+
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class LottoRandomGenerator {
-	private static final int MIN = 1;
-	private static final int MAX = 46;
 	private static final int LOTTO_COUNT = 6;
 
-	public static List<Integer> generate(Random random) {
-		return random.ints(MIN, MAX)
+	public static Set<Integer> generate(Random random) {
+		return random.ints(LOTTO_MIN, LOTTO_MAX)
 			.distinct()
 			.limit(LOTTO_COUNT)
 			.boxed()
-			.collect(Collectors.toList());
+			.collect(Collectors.toCollection(TreeSet::new));
 	}
 }
