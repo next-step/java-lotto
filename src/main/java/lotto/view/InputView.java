32c6ae31d9097f426065ece12b74ca95ff589829
JuJinPark <wnwls1216@gmail.com>
2021-03-10 23:48:44 +0900

    [feat] stub to implement view classes

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import lotto.dto.LotteryGameRequest;
+
+import java.util.List;
+
+public class InputView {
+
+    public static LotteryGameRequest getUserInput() {
+    }
+
+    public static List<Integer> getLastWinningLotteryNumbers() {
+    }
+
+    public static void printWrongInputMessage() {
+    }
+}
---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+import lotto.dto.LotteryStatisticMatchResult;
+import lotto.dto.LotteryTicketDto;
+
+import java.util.List;
+
+public class ResultView {
+
+    public static void printLotteryTickets(List<LotteryTicketDto> lotteryTickets) {
+    }
+
+    public static void printFinalResult(LotteryStatisticMatchResult lotteryStatisticMatchResult) {
+
+    }
+}
