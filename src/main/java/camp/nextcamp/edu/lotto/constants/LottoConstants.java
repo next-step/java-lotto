532422c3b16f387bb7d7af3654652d7bcac6937d
jeonyeonbin <qtrgor@gmail.com>
2021-03-10 21:15:27 +0900

    feat: Constants constructor 방어로직

--- src/main/java/camp/nextcamp/edu/lotto/constants/LottoConstants.java
+++ src/main/java/camp/nextcamp/edu/lotto/constants/LottoConstants.java
@@ -1,6 +1,9 @@
 package camp.nextcamp.edu.lotto.constants;
 
 public class LottoConstants {
+	private LottoConstants() {
+	}
+
 	public static final int LOTTO_MIN = 1;
 	public static final int LOTTO_MAX = 46;
 	public static final int LOTTO_COUNT = 6;
