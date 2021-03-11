7dffe03ea33c047697caf83198fa3a8a6cb4602a
JuJinPark <wnwls1216@gmail.com>
2021-03-11 17:37:23 +0900

    [feat] add view classes

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,56 @@
 package lotto.view;
 
-import lotto.dto.LotteryGameRequest;
+import lotto.dto.LotteryGenerateRequest;
+import lotto.dto.WinningLotteryNumbersRequest;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
-    public static LotteryGameRequest getUserInput() {
+    private static final String LOTTERY_NUMBER_SEPARATOR = ",";
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static LotteryGenerateRequest getLotteryGenerateInput() {
+        return new LotteryGenerateRequest(getAmountOfMoneyToGenerateLottery());
     }
 
-    public static List<Integer> getLastWinningLotteryNumbers() {
+    private static int getAmountOfMoneyToGenerateLottery() {
+        System.out.println(MessageConstant.AMOUNT_INPUT);
+        String amountInString = SCANNER.nextLine();
+        try {
+            return Integer.parseInt(amountInString);
+        } catch (NumberFormatException ex) {
+            printWrongInputMessage();
+            return getAmountOfMoneyToGenerateLottery();
+        }
     }
+
+    public static WinningLotteryNumbersRequest getWinningLotteryNumberInput() {
+        return new WinningLotteryNumbersRequest(getWinningLotteryNumberList());
+
+    }
+
+    private static List<Integer> getWinningLotteryNumberList() {
+        System.out.println(MessageConstant.LAST_WINNING_LOTTERY_NUMBER_INPUT);
+        String[] numberListInString = SCANNER.nextLine().trim().split(LOTTERY_NUMBER_SEPARATOR);
+        try {
+            return Arrays.stream(numberListInString)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toCollection());
+        } catch (NumberFormatException ex) {
+            printWrongInputMessage();
+            return getWinningLotteryNumberList();
+        }
+    }
+
 
     public static void printWrongInputMessage() {
+        System.out.println(MessageConstant.WRONG_INPUT);
     }
+
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -7,7 +7,22 @@ import java.util.List;
 
 public class ResultView {
 
+    private static final String NUMBER_SEPARATOR = ",";
+
     public static void printLotteryTickets(List<LotteryTicketDto> lotteryTickets) {
+        System.out.printf(MessageConstant.PURCHASED_RESULT, lotteryTickets.size());
+
+    }
+
+    private static String extractLotteryNumbersToString(LotteryTicketDto lotteryTicketDto) {
+        StringBuilder b = new StringBuilder();
+        b.append("[");
+        for (int number : lotteryTicketDto.getNumbers()) {
+            b.append(number);
+            b.append(NUMBER_SEPARATOR);
+        }
+        b.append("]");
+        return b.toString().replaceAll(NUMBER_SEPARATOR+"$", "");
     }
 
     public static void printFinalResult(LotteryStatisticMatchResult lotteryStatisticMatchResult) {
