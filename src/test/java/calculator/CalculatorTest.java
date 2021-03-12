8fd3f6af73afb5c71098f760f047fec69ad23b83
Jake <cocahack@naver.com>
2021-03-13 00:09:55 +0900

    test: 기본 구분자를 사용하는 경우에 대한 테스트 작성

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void init() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("기본 구분자 ',' 와 ':' 로 이루어진 문자열에서 숫자를 분리한 다음, 합을 반환한다.")
+    void sumListOfIntegerIfStringHasBasicDelimiters() {
+        assertThat(calculator.sum("1,2:3")).isEqualTo(6);
+    }
+
+}
