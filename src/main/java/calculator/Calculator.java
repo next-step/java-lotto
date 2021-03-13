36ce7f17ad583cccea0b448d5cf1fe42d4138cd2
Jake <cocahack@naver.com>
2021-03-13 16:49:26 +0900

    feat: 빈 문자열, null 입력 시 0을 반환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,6 +9,10 @@ public class Calculator {
     }
 
     public int sum(String expression) {
+        if (expression == null || expression.isEmpty()) {
+            return 0;
+        }
+        
         Sequence sequence = parser.generateSequence(expression);
 
         return sequence.sum();
