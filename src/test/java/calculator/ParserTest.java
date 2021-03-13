aad14675ad4eb678c5bbcd4868ef664e1770c3b6
Jake <cocahack@naver.com>
2021-03-13 13:14:42 +0900

    test: 문자열에서 숫자를 추출하는 테스트 작성

---
+++ src/test/java/calculator/ParserTest.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ParserTest {
+
+    private Parser parser;
+
+    @BeforeEach
+    void init() {
+        parser = new Parser();
+    }
+
+    @ParameterizedTest(name = "(',', ':') 로 구분된 문자열에서 수열을 추출한다.")
+    @ValueSource(strings = {"1,2,3", "1:2:3", "1:2,3"})
+    void makeSequenceFromStringDividedIntoBasicDelimiters(String expression) {
+        assertThat(parser.makeSequence(expression)).containsExactly("1", "2", "3");
+    }
+    
+}
