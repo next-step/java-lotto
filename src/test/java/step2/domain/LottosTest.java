a7a705a7397366a0803b5d7dca5e2411d85c59b2
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-13 19:48:33 +0900

    test : Lottos  당첨번호 매칭 후 당첨 결과갯수 테스트 코드 추가

--- src/test/java/step2/domain/LottosTest.java
+++ src/test/java/step2/domain/LottosTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottosTest {
@@ -23,5 +25,16 @@ class LottosTest {
         Lottos resultLottos = lottos.createLottoList(count);
 
         assertThat(resultLottos.lottoCount()).isEqualTo(4);
+    }
+
+    @DisplayName("로또마다 당첨번호 매칭 후 결과 값은 로또 갯수와 같다")
+    @Test
+    void staticsOfMatch() {
+        List<Integer> winNumber = List.of(1, 2, 3, 4, 5, 6);
+        int count = 4;
+        Lottos lottoList = lottos.createLottoList(count);
+        List<Rank> ranks = lottoList.staticsOfMatch(winNumber);
+
+        assertThat(lottoList.lottoCount()).isEqualTo(ranks.size());
     }
 }
