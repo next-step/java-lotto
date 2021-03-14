09f6965cfa7f39fc50395552662529073f8b74a2
Jake <cocahack@naver.com>
2021-03-14 19:12:32 +0900

    test: 로또 테스트 케이스 작성

---
+++ src/test/java/lottery/domain/LotteryTest.java
@@ -0,0 +1,41 @@
+package lottery.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class LotteryTest {
+
+    private Lottery winningLottery;
+
+    static Stream<Arguments> generateTestCase() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(7, 8, 9, 10, 11, 12), LOSING_TICKET),
+            Arguments.of(Arrays.asList(1, 8, 9, 10, 11, 12), LOSING_TICKET),
+            Arguments.of(Arrays.asList(1, 2, 9, 10, 11, 12), FIFTH),
+            Arguments.of(Arrays.asList(1, 2, 3, 10, 11, 12), FOURTH),
+            Arguments.of(Arrays.asList(1, 2, 3, 5, 10, 12), THIRD),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 12), SECOND),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), FIRST)
+        );
+    }
+
+    @BeforeEach
+    void init() {
+        winningLottery = new Lottery(Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
+
+    @ParameterizedTest(name = "당첨 번호와 비교한 뒤, 로또 등수를 반환한다.")
+    @MethodSource("generateTestCase")
+    void getPrizeAfterComparingLotteries(List<Integer> numbers, Prize expected) {
+        Lottery lottery = new Lottery(numbers);
+
+        assertThat(lottery.compare(winningLottery)).isEqualTo(expected);
+    }
+
+}
