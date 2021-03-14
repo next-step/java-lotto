0fdb30c4f32aa1ddffbba2a33212a55b2b779cae
meme2367 <meme91322367@gmail.com>
2021-03-14 18:01:08 +0900

    feat : LottoList 클래스 작성

---
+++ src/main/java/lotto/domain/LottoList.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoList {
+
+    private List<Lotto> lottoList;
+
+    public LottoList(List<Lotto> lottoList) {
+        this.lottoList = lottoList;
+    }
+
+    @Override
+    public String toString() {
+        String lottoListStr = "";
+        for (Lotto lotto : lottoList) {
+            lottoListStr += "[";
+            lottoListStr += lotto.toString();
+            lottoListStr += "]\n";
+        }
+        return lottoListStr.trim();
+    }
+
+
+    public Map<LottoRank,Integer> makeMatchingCount(List<LottoNumber> lastWeekWinningLotto) {
+        Map<LottoRank,Integer> matchingCountMap = new HashMap<>();
+        for ( Lotto lotto : lottoList) {
+            int matchingCount = lotto.containsCount(lastWeekWinningLotto);
+            LottoRank rank = LottoRank.findByMatchingCount(matchingCount);
+            matchingCountMap.put(rank, matchingCountMap.getOrDefault(rank,1) + 1);
+        }
+
+        return matchingCountMap;
+
+    }
+}
