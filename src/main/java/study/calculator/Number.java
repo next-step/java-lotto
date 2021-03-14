bc25c0e0697c34d34611924dff0aa4c8fe6d628e
seokrae <kslbsh@gmail.com>
2021-03-14 23:05:06 +0900

    refactor: Wrapper 클래스인 Number에서 음수 값 입력 예외처리

---
+++ src/main/java/study/calculator/Number.java
@@ -0,0 +1,41 @@
+package study.calculator;
+
+import study.calculator.exception.CalculatorException;
+
+import java.util.Objects;
+
+public class Number {
+
+    public static final String NEGATIVE_NUMBER_EXCEPTION = "음수는 받을 수 없습니다.";
+
+    private final int number;
+
+    public Number(int number) {
+        this.number = number;
+    }
+
+    public static Number of(String number) {
+        int parseInt = Integer.parseInt(number);
+        if(parseInt < 0) {
+            throw new CalculatorException(NEGATIVE_NUMBER_EXCEPTION);
+        }
+        return new Number(parseInt);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Number number1 = (Number) o;
+        return getNumber() == number1.getNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumber());
+    }
+}
