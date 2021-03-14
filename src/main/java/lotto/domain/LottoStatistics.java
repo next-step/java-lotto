531c614b4ff91f13aa04dd685c7331069371bd4a
meme2367 <meme91322367@gmail.com>
2021-03-14 18:03:05 +0900

    feat : LottoMachine, LottoStatistics 클래스 작성

---
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoStatistics {
+
+    private Map<LottoRank,Integer> lottoStaticResult;
+
+    public LottoStatistics(Map<LottoRank, Integer> matchingCountResult) {
+        this.lottoStaticResult = matchingCountResult;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (LottoRank rank : lottoStaticResult.keySet()) {
+            sb.append(rank.getMatchingCount()+"개 일치 (");
+            sb.append(rank.getMatchingPrice()+"원) - ");
+            sb.append(lottoStaticResult.get(rank)+"개\n");
+        }
+        return sb.toString().trim();
+    }
+
+    private int sum() {
+        int sum = 0;
+        for (LottoRank rank : lottoStaticResult.keySet()) {
+            sum += rank.getMatchingPrice() * lottoStaticResult.get(rank);
+        }
+        return sum;
+    }
+
+    public double calculate(int lottoCount) {
+        int total = sum();
+        return 0;
+    }
+}
