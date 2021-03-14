81282f69f9c9e58476a8653e2501b788fe2973f2
seokrae <kslbsh@gmail.com>
2021-03-14 18:48:31 +0900

    fix: 숫자로 캐스팅이 가능한 문자열 입력시 해당 숫자 반환 성공

--- src/main/java/study/calculator/StringAddCalculator.java
+++ src/main/java/study/calculator/StringAddCalculator.java
@@ -1,6 +1,10 @@
 package study.calculator;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
 
 public class StringAddCalculator {
 
@@ -8,7 +12,31 @@ public class StringAddCalculator {
         if(Objects.isNull(text) || text.isEmpty()) { // null 또는 ""
             return 0;
         }
-        return 0;
+
+        if(text.length() == 1 && isNumeric(text)) {
+            return Integer.parseInt(text);
+        }
+
+        List<Integer> numbers = split(text);
+        return numbers.stream()
+                .reduce(Integer::sum)
+                .get();
     }
 
+    private static List<Integer> split(String text) {
+        String[] split = text.split(",|:");
+        return Arrays.stream(split)
+                .filter(StringAddCalculator::isNumeric)
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
+    private static boolean isNumeric(String text) {
+        try {
+            Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 }
