49d756ce45f566d60b6d87fd63281bd2172fea68
jhLim97 <wnsgur2749@naver.com>
2021-03-14 19:05:59 +0900

    feat: 빈 문자열 또는 null 값에 대한 처리 로직 테스트 구현 및 프로덕션 코드 구현

---
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -0,0 +1,18 @@
+package stringcalculator;
+
+public class StringAddCalculator {
+
+    public int splitAndSum(String input) {
+        int result = 0;
+
+        if (input == null) {
+            result = 0;
+        }
+
+        if (input.isEmpty()) {
+            result = 0;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/stringcalculator/StringAddCalculatorTest.java
@@ -0,0 +1,17 @@
+package stringcalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource({"null, 0", "'', 0"})
+    @DisplayName("빈 문자열 또는 null 값을 입력하는 경우 0을 반환한다.")
+    void splitAndSum_Return0(String input, int result) {
+        int calculationResult = new StringAddCalculator().splitAndSum(input);
+        Assertions.assertThat(calculationResult).isEqualTo(result);
+    }
+}
