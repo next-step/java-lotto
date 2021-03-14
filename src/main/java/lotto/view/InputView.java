7f9e3645cfc736d80fc589f4b56ef965df87b3a1
meme2367 <meme91322367@gmail.com>
2021-03-14 17:59:32 +0900

    feat : view 클래스 작성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import lotto.domain.LottoNumber;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    public int inputPurchasePrice() {
+        System.out.println("구매 금액을 입력해주세요");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public List<LottoNumber> inputWinningLottoNumbers() {
+
+        System.out.println("지난 주 당첨 번호를 입력해 주세요");
+        String winngLottoNumbers = new Scanner(System.in).nextLine();
+
+        return Arrays.stream(winngLottoNumbers.split(","))
+                .map(Integer::valueOf)
+                .map(LottoNumber::of)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import lotto.domain.LottoList;
+import lotto.domain.LottoStatistics;
+
+public class ResultView {
+    public void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void printLottoList(LottoList lottoList) {
+        System.out.println(lottoList.toString());
+    }
+
+    public void printLottoStatics(LottoStatistics lottoStatistics) {
+        System.out.println("당첨 통계");
+        System.out.println("--------");
+        System.out.println(lottoStatistics.toString());
+    }
+
+    public void printLottoEarningRate(double earningRate) {
+        System.out.println("총 수익률은 " + earningRate + "입니다.(기준이 1이기 때문에 결과적으로 손해란 얘기임)");
+    }
+}
