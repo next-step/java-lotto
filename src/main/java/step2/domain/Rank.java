4dff369690e7871fc2acc67f7c0d54cceb861874
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-15 22:26:50 +0900

    feat : rank 2등, 3등 조건문 추가

--- src/main/java/step2/domain/Rank.java
+++ src/main/java/step2/domain/Rank.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     MISS(0, 0);
@@ -18,6 +19,19 @@ public enum Rank {
     }
 
     public static Rank valueOf(int countOfMatch) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.getCountOfMatch() == countOfMatch)
+                .findAny()
+                .orElse(Rank.MISS);
+    }
+
+    public static Rank valueOf(int countOfMatch, boolean matchBonus) {
+        if (countOfMatch == 5 && matchBonus) {
+            return Rank.SECOND;
+        }
+        if (countOfMatch == 5) {
+            return Rank.THIRD;
+        }
         return Arrays.stream(Rank.values())
                 .filter(rank -> rank.getCountOfMatch() == countOfMatch)
                 .findAny()
