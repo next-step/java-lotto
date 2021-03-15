bb0a183e5ddc81a2bc13f4668d13fc0deede60a8
Jake <cocahack@naver.com>
2021-03-15 20:56:22 +0900

    feat: 결과 출력 기능 controller 에 연결

--- src/main/java/lottery/LotteryApplication.java
+++ src/main/java/lottery/LotteryApplication.java
@@ -5,7 +5,7 @@ import lottery.controller.LotteryController;
 public class LotteryApplication {
 
     public static void main(String[] args) {
-        new LotteryController().sellLotteryTicket();
+        new LotteryController().run();
     }
 
 }
--- src/main/java/lottery/controller/LotteryController.java
+++ src/main/java/lottery/controller/LotteryController.java
@@ -1,22 +1,26 @@
 package lottery.controller;
 
-import lottery.domain.LotteryBetting;
-import lottery.dto.ReadonlyLotteryTicket;
+import lottery.domain.LotteryTicket;
+import lottery.domain.LotteryTicketIssuer;
+import lottery.dto.RoundResult;
 import lottery.view.InputView;
 import lottery.view.ResultView;
+
+import java.util.List;
 
 public class LotteryController {
 
     private final InputView inputView = new InputView();
     private final ResultView resultView = new ResultView();
 
-    public void sellLotteryTicket() {
+    public void run() {
         int money = inputView.receivePurchaseAmount();
-
-        LotteryBetting lotteryBetting = new LotteryBetting(money);
-        ReadonlyLotteryTicket lotteryTicket = lotteryBetting.getLotteryTicket();
-
+        LotteryTicket lotteryTicket = LotteryTicketIssuer.issue(money);
         resultView.printLotteryTicket(lotteryTicket);
+
+        List<Integer> winningNumbers = inputView.receiveWinningNumbers();
+        RoundResult roundResult = lotteryTicket.getResult(winningNumbers);
+        resultView.printResult(roundResult);
     }
 
 }
