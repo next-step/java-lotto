398bd9a1df3dfea76efef7724187efe6df6e18ad
cptbong <cptbbong@gmail.com>
2021-03-15 20:09:44 +0900

    refactor - 메서드분리.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -18,14 +18,32 @@ public class Lotto {
         this.lottoPrice = new LottoPriceImpl();
     }
 
+    /**
+     * 로또번호를 제공
+     * @return
+     */
     public List<Integer> getLottoNumbers() {
-        ArrayList<Integer> shuffledNumbers = (ArrayList<Integer>) lottoNumbers.clone();
-        Collections.shuffle(shuffledNumbers);
+        ArrayList<Integer> cloneNumbers = (ArrayList<Integer>) lottoNumbers.clone();
+        Collections.shuffle(cloneNumbers);
+        return getSixNumbers(cloneNumbers);
+    }
+
+    /**
+     * 6개의 번호를 전달
+     * @param shuffledNumbers
+     * @return
+     */
+    private List<Integer> getSixNumbers(ArrayList<Integer> shuffledNumbers) {
         return shuffledNumbers.stream()
                 .limit(6)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * 전달받은 금액으로 로또갯수를 반환
+     * @param receiveMoney
+     * @return
+     */
     public int howManyPass(int receiveMoney) {
         return this.lottoPrice.howManyPass(receiveMoney);
     }
