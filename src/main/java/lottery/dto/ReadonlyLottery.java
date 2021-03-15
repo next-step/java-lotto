1ab5da8ccd5a5910b43463ad79eb77779795d603
Jake <cocahack@naver.com>
2021-03-15 12:55:40 +0900

    feat: 읽기 전용 인터페이스 추가

---
+++ src/main/java/lottery/dto/ReadonlyLottery.java
@@ -0,0 +1,9 @@
+package lottery.dto;
+
+import java.util.List;
+
+public interface ReadonlyLottery {
+
+    List<Integer> getNumbers();
+
+}
---
+++ src/main/java/lottery/dto/ReadonlyLotteryTicket.java
@@ -0,0 +1,9 @@
+package lottery.dto;
+
+import java.util.List;
+
+public interface ReadonlyLotteryTicket {
+
+    List<ReadonlyLottery> getLotteries();
+
+}
