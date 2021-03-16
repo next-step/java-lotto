dade3e701251b814e60274aafdd2b8924b5216b5
seokrae <kslbsh@gmail.com>
2021-03-16 20:49:19 +0900

    test: 금액 입력시 테스트 및 예외처리 테스트

--- src/main/java/study/lotto/view/dto/RequestMoney.java
+++ src/main/java/study/lotto/view/dto/RequestMoney.java
@@ -1,4 +1,33 @@
 package study.lotto.view.dto;
 
+import study.lotto.exception.LottoException;
+
+import java.util.Objects;
+
+import static study.lotto.view.InputView.LOTTO_PRICE;
+
 public class RequestMoney {
+
+    public static final String CANNOT_BUY_LOTTO = "구매가 불가능합니다.";
+    private final int totalLottoCount;
+
+    public RequestMoney(int money) {
+        if(money < LOTTO_PRICE) {
+            throw new LottoException(CANNOT_BUY_LOTTO);
+        }
+        totalLottoCount = money / LOTTO_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RequestMoney)) return false;
+        final RequestMoney money = (RequestMoney) o;
+        return totalLottoCount == money.totalLottoCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(totalLottoCount);
+    }
 }
