29de98670fe06a53b99b73cac19ad5ff4fdee1f5
Dongjoon Park <djoon@users.noreply.github.com>
2021-03-16 18:18:56 +0900

    test: Split expression with custom delimiter

--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -1,14 +1,20 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  *
  * 기능 요구 사항
  * - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열의 경우, 이를 분리하고 리스트로 반환한다.
+ * - (//)와 (\n) 사이 문자를 커스텀 구분자로 하여, 이를 분리하고 리스트로 반환한다.
  */
 
 public class StringAddCalculatorTest {
@@ -30,6 +36,14 @@ public class StringAddCalculatorTest {
     @Test
     public void splitExpressionWithMixed() {
         List<String> result = new InputExpression("1,2:3").split();
+
+        assertThat(result).containsExactly("1", "2", "3");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\\n1;2;3", "//|\\n1|2|3", "//-\\n1-2-3"})
+    public void splitExpressionWithCustomDelimiter(String inputString) {
+        List<String> result = new InputExpression(inputString).split();
 
         assertThat(result).containsExactly("1", "2", "3");
     }
