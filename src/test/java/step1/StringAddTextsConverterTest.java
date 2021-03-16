567b18b241a39a644e05c3c06d99a88ac26e8ad8
abcdefg <choiyurim@naver.com>
2021-03-16 13:18:06 +0900

    test(StringAddTextsConverterTest): add StringAddTextsConverterTest

---
+++ src/test/java/step1/StringAddTextsConverterTest.java
@@ -0,0 +1,56 @@
+package step1;
+
+import org.junit.jupiter.api.Test;
+import step1.controller.StringAddCalculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringAddTextsConverterTest {
+    
+    @Test
+    void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        int expected = 0;
+        assertEquals(result, expected);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void splitAndSum_숫자하나() {
+        int result = StringAddCalculator.splitAndSum("1");
+        int expected = 1;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void splitAndSum_쉼표구분자() {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        int expected = 3;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int expected = 6;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int expected = 6;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThrows(
+                RuntimeException.class,
+                () -> StringAddCalculator.splitAndSum("-1,2,3"));
+    }
+
+}
