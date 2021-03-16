430d3213af82f52703392282c1589e9d0fed6ec5
Dongjoon Park <djoon@users.noreply.github.com>
2021-03-16 17:07:36 +0900

    feat: 구분자에 따라 분리하고, 리스트로 반환.

--- src/main/java/study/InputExpression.java
+++ src/main/java/study/InputExpression.java
@@ -1,13 +1,17 @@
 package study;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InputExpression {
+    private final String inputString;
 
     public InputExpression(String inputString) {
+        this.inputString = inputString;
     }
 
     public List<String> split() {
-        return null;
+        String[] tokens = inputString.split(",|:");
+        return Arrays.asList(tokens);
     }
 }
