1a6b9bd083a4689700dfa28e46359c40bd424878
623nana <6230nana@gmail.com>
2021-03-16 21:54:44 +0900

    feat: 입력 값 검증 기능

---
+++ src/main/java/calculator/InputValidator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class InputValidator {
+
+  public static boolean isEmptyOrBlank(String input) {
+    if (isEmpty(input) || isBlank(input)) {
+      return true;
+    }
+    return false;
+  }
+
+  public static boolean isEmpty(String input) {
+    return "".equals(input);
+  }
+
+  public static boolean isBlank(String input) {
+    return input == null;
+  }
+
+}
---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringAddCalculator {
+
+  public int splitAndSum(String input) {
+    if (InputValidator.isEmptyOrBlank(input)) {
+      return 0;
+    }
+    return 1;
+  }
+}
