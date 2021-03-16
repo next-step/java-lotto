bb889537264cfbf18281d1829eb4232627760296
abcdefg <choiyurim@naver.com>
2021-03-16 17:45:26 +0900

    refactor(TextMatcher): pattern regex 상수로 변경

--- src/main/java/step1/utils/TextMatcher.java
+++ src/main/java/step1/utils/TextMatcher.java
@@ -4,9 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextMatcher {
+    public static final String PATTERN = "//(.)\n(.*)";
+
     private TextMatcher() {}
 
     public static Matcher getMatcher(String text) {
-        return Pattern.compile("//(.)\n(.*)").matcher(text);
+        return Pattern.compile(PATTERN).matcher(text);
     }
 }
