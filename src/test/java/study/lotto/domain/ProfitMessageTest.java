4776234220d6f68648a98af61dd0f743d2344c8c
seokrae <kslbsh@gmail.com>
2021-03-17 17:57:35 +0900

    test: 손익 메시지 테스트

---
+++ src/test/java/study/lotto/domain/ProfitMessageTest.java
@@ -0,0 +1,31 @@
+package study.lotto.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfitMessageTest {
+
+    private static Stream<Arguments> profitEntry() {
+        return Stream.of(
+                Arguments.of(1.5, ProfitMessage.PROFIT),
+                Arguments.of(1.0, ProfitMessage.BREAK_POINT),
+                Arguments.of(0.5, ProfitMessage.LOSS)
+        );
+    }
+
+    @ParameterizedTest(name = "{0} 값 계산 시 결과 -> {1}")
+    @MethodSource(value = "profitEntry")
+    void test(double given, ProfitMessage expected) {
+        // given
+
+        // when
+        ProfitMessage msg = ProfitMessage.of(given);
+        // then
+        assertThat(msg).isEqualTo(expected);
+    }
+}
