1e4fa922bce17ab18a53f58a9ce43bb1a7a0ef57
apdkeb <apdkeb@naver.com>
2021-03-17 12:15:41 +0900

    test : 로또 번호 6개 확인 추가
    2020. 03. 17.
    번호 사이즈 6개 확인
    
    Resolves : loop-study
    See also : yongdae

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,7 +1,12 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class LottoTicketTest {
 
@@ -15,5 +20,18 @@ public class LottoTicketTest {
 
         //then
 
+    }
+
+    @Test
+    @DisplayName("로또 번호 6개인지 확인")
+    public void checkLottoNumberSize() throws Exception {
+        //given
+        LottoTicket lottoTicket = new LottoTicket();
+
+        //when
+        List<Integer> numbers = lottoTicket.getTicketNumber();
+
+        //then
+        assertThat(numbers.size()).isEqualTo(6);
     }
 }
