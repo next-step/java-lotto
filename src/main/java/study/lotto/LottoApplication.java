b5229337ed7e02c5f3d4d05223b64476130a1619
seokrae <kslbsh@gmail.com>
2021-03-17 17:59:34 +0900

    refactor: 메시지 시그니처 의미에 맞게 리펙토링

--- src/main/java/study/lotto/LottoApplication.java
+++ src/main/java/study/lotto/LottoApplication.java
@@ -1,29 +1,32 @@
 package study.lotto;
 
 import study.lotto.controller.LottoGame;
+import study.lotto.domain.LottoResult;
 import study.lotto.service.Lottos;
-import study.lotto.view.dto.RequestMoney;
 import study.lotto.view.InputView;
-import study.lotto.view.dto.RequestWinningNumber;
 import study.lotto.view.ResultView;
+import study.lotto.view.dto.RequestMoney;
+import study.lotto.view.dto.RequestWinningNumber;
 
 public class LottoApplication {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
+        LottoGame lottoGame = new LottoGame();
 
         // 금액 요청
-        RequestMoney requestMoney = inputView.requestMoney();
-
-        LottoGame lottoGame = new LottoGame();
+        RequestMoney requestMoney = InputView.requestMoney();
+        resultView.printBuyCount(requestMoney);
         Lottos lottos = lottoGame.buyLotto(requestMoney);
 
         // 중간 보기
-        resultView.printBuyCount(requestMoney);
         resultView.printBuyLottos(lottos);
 
+        // 당첨 번호 입력
         RequestWinningNumber winningNumber = inputView.requestWinningNumber();
+        LottoResult lottoResult = new LottoResult(requestMoney, winningNumber, lottos);
         // 결과 보기
-        resultView.printStatics(winningNumber, lottos);
+        resultView.printStatics(lottoResult);
+        resultView.printRate(lottoResult);
     }
 }
--- src/main/java/study/lotto/controller/LottoGame.java
+++ src/main/java/study/lotto/controller/LottoGame.java
@@ -1,17 +1,24 @@
 package study.lotto.controller;
 
+import study.lotto.domain.Lotto;
 import study.lotto.service.Lottos;
 import study.lotto.view.dto.RequestMoney;
 
-public class LottoGame {
-    private final Lottos lottos;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    public LottoGame() {
-        this.lottos = new Lottos();
+/**
+ * 유저와 로또 사이 중개자 역할
+ */
+public class LottoGame {
+
+    public Lottos buyLotto(final RequestMoney money) {
+        int totalLottoCount = money.getTotalLottoCount();
+        List<Lotto> collect = Stream.generate(Lotto::new)
+                .limit(totalLottoCount)
+                .collect(Collectors.toList());
+        return new Lottos(collect);
     }
-    // 로또 구매 (로또 한 게임 생성)
-    public Lottos buyLotto(RequestMoney money) {
-        lottos.makeLotto(money);
-        return lottos;
-    }
+
 }
