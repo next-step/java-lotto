f85d60e87de169b47e5906623b8455027be65f18
623nana <6230nana@gmail.com>
2021-03-17 23:30:33 +0900

    refactor: @DisplayName추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
@@ -16,6 +17,7 @@ public class StringAddCalculatorTest {
   }
 
   @Test
+  @DisplayName("[StringAddCalculator] null 또는 빈문자 입력에 대한 테스트")
   public void splitAndSum_null_또는_빈문자() {
     int result = stringAddCalculator.splitAndSum(null);
     assertThat(result).isEqualTo(0);
@@ -25,30 +27,35 @@ public class StringAddCalculatorTest {
   }
 
   @Test
+  @DisplayName("[StringAddCalculator] 숫자 하나를 입력했을 경우에 대한 테스트")
   public void splitAndSum_숫자하나() throws Exception {
     int result = stringAddCalculator.splitAndSum("1");
     assertThat(result).isEqualTo(1);
   }
 
   @Test
+  @DisplayName("[StringAddCalculator] 쉼표 구분자로 숫자 두개를 입력했을 경우에 대한 테스트")
   public void splitAndSum_쉼표구분자() throws Exception {
     int result = stringAddCalculator.splitAndSum("1,2");
     assertThat(result).isEqualTo(3);
   }
 
   @Test
+  @DisplayName("[StringAddCalculator] 쉼표/콜론 구분자를 사용했을 경우에 대한 테스트")
   public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
     int result = stringAddCalculator.splitAndSum("1,2:3");
     assertThat(result).isEqualTo(6);
   }
 
   @Test
+  @DisplayName("[StringAddCalculator] 커스텀 구분자를 사용했을 경우에 대한 테스트")
   public void splitAndSum_custom_구분자() throws Exception {
     int result = stringAddCalculator.splitAndSum("//;\n1;2;3");
     assertThat(result).isEqualTo(6);
   }
 
   @Test
+  @DisplayName("[StringAddCalculator] 음수를 입력했을 경우 예외처리 테스트")
   public void splitAndSum_negative() throws Exception {
     assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
         .isInstanceOf(RuntimeException.class);
