cda27512794cca7dabdd5862e05bc945a51718ad
623nana <6230nana@gmail.com>
2021-03-17 23:27:09 +0900

    refactor: 숫자 Number 클래스화 및 일급 컬렉션 사용

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,40 +1,14 @@
 package calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringAddCalculator {
+
+  private Numbers numbers;
 
   public int splitAndSum(String input) {
     if (InputValidator.isEmptyOrBlank(input)) {
       return 0;
     }
-
-    if (InputValidator.isSingleNumber(input)) {
-      return Integer.parseInt(input);
-    }
-
-    return calculate(input);
-  }
-
-  public int calculate(String input) {
-    String[] numbers = splitString(input);
-    int result = 0;
-
-    for (int i = 0; i < numbers.length; i++) {
-      InputValidator.validateNumber(numbers[i]);
-      result += Integer.parseInt(numbers[i]);
-    }
-
-    return result;
-  }
-
-  public String[] splitString(String text) {
-    Matcher m = Pattern.compile(Constant.CUSTOM_SPLIT_REGEX_PATTERN).matcher(text);
-    if (m.find()) {
-      String customDelimiter = m.group(1);
-      return m.group(2).split(customDelimiter);
-    }
-    return text.split(Constant.BASIC_SPLIT_REGEX);
+    numbers = new Numbers(input);
+    return numbers.sumOfNumbers();
   }
 }
