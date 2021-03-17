93b0c8dd68270dcfd6b36335d53df8aa5ee0c92f
apdkeb <apdkeb@naver.com>
2021-03-17 12:12:25 +0900

    feat : 로또티켓
    2020. 03. 17.
    로또 티켓 발급
    
    Resolves : loop-study
    See also : yongdae

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,7 @@
+package lotto.domain;
+
+public class LottoTicket {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+}
---
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketTest {
+
+    @Test
+    @DisplayName("로또 발급")
+    public void createLottoTicket() throws Exception {
+        //given
+        LottoTicket lottoTicket = new LottoTicket();
+
+        //when
+
+        //then
+
+    }
+}
