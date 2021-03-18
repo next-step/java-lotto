d9c2f35b18e29136eebc8d8942f3cc92e29c22c5
PandaHun <gnsgnsek@gmail.com>
2021-03-18 22:29:11 +0900

    test: Create Money test code

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class Money {
+
+    private final int money;
+
+    public Money(String money) {
+        this.money = 0;
+    }
+
+    public int value() {
+        return -1;
+    }
+}
