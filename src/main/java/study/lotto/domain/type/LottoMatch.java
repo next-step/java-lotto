abce1ae40bade6777b14f44dc7dbc71e449110c2
seokrae <kslbsh@gmail.com>
2021-03-18 18:00:32 +0900

    test: bonus 숫자 체크를 위한 필터 추가 및 테스트

--- src/main/java/study/lotto/domain/type/LottoMatch.java
+++ src/main/java/study/lotto/domain/type/LottoMatch.java
@@ -8,37 +8,56 @@ import java.util.List;
  */
 public enum LottoMatch {
 
-    RANK_FIRST(6, 2_000_000_000),
-    RANK_SECOND(5, 1_500_000),
-    RANK_THIRD(4, 50_000),
-    RANK_FOURTH(3, 5_000),
-    RANK_ETC(0, 0);
+    RANK_FIRST(6, 2_000_000_000, "%s 개 일치 (%s원) - %s개"),
+    RANK_BONUS(5, 30_000_000, "%s 개 일치, 보너스볼 일치(%s원) - %s개"),
+    RANK_SECOND(5, 1_500_000, "%s 개 일치 (%s원) - %s개"),
+    RANK_THIRD(4, 50_000, "%s 개 일치 (%s원) - %s개"),
+    RANK_FOURTH(3, 5_000, "%s 개 일치 (%s원) - %s개"),
+    RANK_ETC(0, 0, "");
 
     private final long matchCount;
     private final long winningReward;
+    private final String guideText;
 
-    LottoMatch(final long matchCount, final long winningReward) {
+    LottoMatch(final long matchCount, final long winningReward, String guideText) {
         this.matchCount = matchCount;
         this.winningReward = winningReward;
+        this.guideText = guideText;
     }
 
     public static List<LottoMatch> all() {
-        return Arrays.asList(RANK_FOURTH, RANK_THIRD, RANK_SECOND, RANK_FIRST);
+        return Arrays.asList(RANK_FOURTH, RANK_THIRD, RANK_SECOND, RANK_BONUS, RANK_FIRST);
+    }
+
+    public static LottoMatch of(final long matchCount, final boolean bonusNumber) {
+        return Arrays.stream(LottoMatch.values())
+                .filter(lottoMatch -> lottoMatch.isMatchCount(matchCount))
+                .filter(lottoMatch -> lottoMatch.isBonusNumber(bonusNumber))
+                .findFirst()
+                .orElse(RANK_ETC);
+    }
+
+    private boolean isBonusNumber(boolean bonusNumber) {
+        return !this.equals(RANK_BONUS) || bonusNumber;
     }
 
     public static LottoMatch of(final long matchCount) {
         return Arrays.stream(LottoMatch.values())
-                .filter(lottoMatch -> isMatchCount(matchCount, lottoMatch))
+                .filter(lottoMatch -> lottoMatch.isMatchCount(matchCount))
                 .findAny()
                 .orElse(RANK_ETC);
     }
 
-    private static boolean isMatchCount(final long matchCount, final LottoMatch lottoMatch) {
-        return lottoMatch.matchCount == matchCount;
+    private boolean isMatchCount(final long matchCount) {
+        return this.matchCount == matchCount;
     }
 
     public long getWinningReward() {
         return winningReward;
+    }
+
+    public String guideLine() {
+        return guideText;
     }
 
     @Override
--- src/test/java/study/lotto/domain/LottoMatchTest.java
+++ src/test/java/study/lotto/domain/LottoMatchTest.java
@@ -14,21 +14,22 @@ class LottoMatchTest {
 
     private static Stream<Arguments> rankRewardEntry() {
         return Stream.of(
-                Arguments.of(6, 2_000_000_000),
-                Arguments.of(5, 1_500_000),
-                Arguments.of(4, 50_000),
-                Arguments.of(3, 5_000),
-                Arguments.of(2, 0),
-                Arguments.of(1, 0),
-                Arguments.of(0, 0)
+                Arguments.of(6, 2_000_000_000, false),
+                Arguments.of(5, 30_000_000, true),
+                Arguments.of(5, 1_500_000, false),
+                Arguments.of(4, 50_000, false),
+                Arguments.of(3, 5_000, false),
+                Arguments.of(2, 0, false),
+                Arguments.of(1, 0, false),
+                Arguments.of(0, 0, false)
         );
     }
 
-    @ParameterizedTest(name = "{0} 개 숫자 맞추는 경우 당첨금 {1} 원 테스트")
+    @ParameterizedTest(name = "{0} 개 숫자 맞추고 보너스 숫자 {2} 경우 당첨금 {1} 원 테스트")
     @MethodSource(value = "rankRewardEntry")
-    void enumOf_숫자_매칭_및_당첨금_확인_테스트(long given, long expected) {
+    void enumOf_숫자_매칭_및_당첨금_확인_테스트(long given, long expected, boolean bonus) {
         // given
-        LottoMatch rank = LottoMatch.of(given);
+        LottoMatch rank = LottoMatch.of(given, bonus);
         // when
         long won = rank.getWinningReward();
         // then
