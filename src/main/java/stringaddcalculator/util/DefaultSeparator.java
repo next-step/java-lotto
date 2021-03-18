c1a40490472edace97fa9066558d14971d89a8b4
PandaHun <gnsgnsek@gmail.com>
2021-03-18 10:04:06 +0900

    feat: Create Separator function
    - 분리기 기능 구현

--- src/main/java/stringaddcalculator/util/DefaultSeparator.java
+++ src/main/java/stringaddcalculator/util/DefaultSeparator.java
@@ -1,8 +1,10 @@
 package stringaddcalculator.util;
 
 public class DefaultSeparator implements Separator {
+    private static final String DEFAULT_DELIMITER = ",|:";
+
     @Override
     public String[] splitExpression(String expression) {
-        return new String[]{""};
+        return expression.split(DEFAULT_DELIMITER);
     }
 }
