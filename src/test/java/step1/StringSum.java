6cd561c2e0c4610c25305ffa99bfa5786e155f16
wkdehdlr <wkdehdlr@gmail.com>
2021-03-19 00:16:37 +0900

    [feat]빈 문자열, null 입력 구현

--- src/test/java/step1/StringSum.java
+++ src/test/java/step1/StringSum.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class StringSum {
 
     public Result sum(String input) {
+        if (input == null || input.isEmpty()) {
+            return new Result(0);
+        }
         String[] split = input.split("[,:]");
         Integer sum = Arrays.stream(split)
             .map(Integer::parseInt)
