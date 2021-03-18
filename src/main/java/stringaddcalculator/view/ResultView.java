207eeb9b4274d3cdce88a39ff33010a7d19ac3a6
PandaHun <gnsgnsek@gmail.com>
2021-03-18 10:03:04 +0900

    feat: Create View Class

---
+++ src/main/java/stringaddcalculator/view/ResultView.java
@@ -0,0 +1,10 @@
+package stringaddcalculator.view;
+
+public class ResultView {
+
+    private ResultView() {}
+
+    public static void showResult(int result) {
+        System.out.println("계산 결과: " + result);
+    }
+}
