c1887e2d9b2f926e5c97618cd19d352311d2eb88
hwangyunho <hwang9yh@gmail.com>
2021-03-18 19:11:33 +0900

    test: null 또는 빈문자열이 들어올 경우 0 반환하는 테스트 추가

---
+++ src/main/java/lotto/step1/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package lotto.step1;
+
+public class StringAddCalculator {
+
+    private StringAddCalculator() {
+    }
+
+    public static int splitAndSum(String input) {
+        return 0;
+    }
+}
---
+++ src/test/java/lotto/step1/StringAddCalculatorTest.java
@@ -0,0 +1,30 @@
+package lotto.step1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringAddCalculatorTest {
+
+    @DisplayName("null 또는 빈문자")
+    @ParameterizedTest
+    @MethodSource("provideNullOREmpty")
+    public void splitAndSum_null_또는_빈문자(String input, int expected) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideNullOREmpty() {
+        return Stream.of(
+                Arguments.of(null, 0),
+                Arguments.of("", 0)
+        );
+    }
+
+
+}
