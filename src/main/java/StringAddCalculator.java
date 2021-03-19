09cf94af8008be9edb41497156a1f865a5ac27b2
이승환 <lufful.induk@gmail.com>
2021-03-20 00:11:00 +0900

    - SEPARATOR_REGEX 추가 - convertStringToIntArray 메소드 분리

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -7,36 +7,42 @@ import java.util.stream.Stream;
 public class StringAddCalculator {
 
     private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final String SEPARATOR_REGEX = "//(.)\n(.*)";
 
     public static int splitAndSum(String value){
         if(value == null || value.isEmpty()){
             return 0;
         }
 
-        int[] list = split(value);
-        return sum(list);
-
+        int[] numbers = split(value);
+        return sum(numbers);
     }
 
     private static int[] split(String value){
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(value);
+        Matcher m = Pattern.compile(SEPARATOR_REGEX).matcher(value);
         if (m.find()) {
             String customDelimiter = m.group(1);
-            return Stream.of(m
-                .group(2)
-                .split(customDelimiter)).mapToInt(Integer::parseInt).toArray();
+            return convertStringToIntArray(m.group(2)
+                .split(customDelimiter));
         }
-        return Stream.of(
-            value
-            .split(DEFAULT_SEPARATOR))
+        return convertStringToIntArray(value.split(DEFAULT_SEPARATOR));
+    }
+
+    private static int[] convertStringToIntArray(String[] list){
+        return Stream
+            .of(list)
             .mapToInt(Integer::parseInt)
             .toArray();
     }
 
     private static int sum(int[] list) {
-        for (int i : list){
-            if(i < 0) throw new NegativeException("음수는 입력 할 수 없습니다.");
-        }
+        validateNegativeNumber(list);
         return Arrays.stream(list).sum();
+    }
+
+    private static void validateNegativeNumber(int[] list) {
+        Arrays.stream(list).filter(i -> i < 0).forEach(i -> {
+            throw new NegativeException("음수는 입력 할 수 없습니다.");
+        });
     }
 }
