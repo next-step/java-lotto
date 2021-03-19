cffe69ac177146415b082e8a87f1d5c56275cd89
ad7549 <ad7549@gmail.com>
2021-03-20 03:25:55 +0900

    refactor: Refactor StringAddCalculator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -5,29 +5,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String text) {
-        if (text == null || text.isEmpty()) {
-            return 0;
+    private static final int DEFAULT_RESULT = 0;
+
+    public static int splitAndSum(final String text) {
+        if (nullOrEmpty(text)) {
+            return DEFAULT_RESULT;
         }
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        final String[] tokens = tokens(text);
+
+        if (containsNegative(tokens)) {
+            throw new RuntimeException();
+        }
+
+        return sum(tokens);
+    }
+
+    private static boolean nullOrEmpty(final String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] tokens(final String text) {
+        final Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
-
-            if (Arrays.stream(tokens).mapToInt(Integer::parseInt).anyMatch(n -> n < 0)) {
-                throw new RuntimeException();
-            }
-
-            return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
-        } else {
-            final String[] tokens = text.split(",|:");
-
-            if (Arrays.stream(tokens).mapToInt(Integer::parseInt).anyMatch(n -> n < 0)) {
-                throw new RuntimeException();
-            }
-
-            return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+            final String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
         }
+
+        return text.split(",|:");
+    }
+
+    private static boolean containsNegative(final String[] tokens) {
+        return Arrays.stream(tokens).mapToInt(Integer::parseInt).anyMatch(StringAddCalculator::negative);
+    }
+
+    private static boolean negative(final int n) {
+        return n < 0;
+    }
+
+    private static int sum(final String[] tokens) {
+        return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
     }
 }
