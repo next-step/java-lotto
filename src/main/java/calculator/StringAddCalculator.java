4f9b20a22c89584c17df3e5010fb68310869df2f
ad7549 <ad7549@gmail.com>
2021-03-20 03:09:56 +0900

    feat: Implement StringAddCalculator

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,33 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-    public static int splitAndSum(Object o) {
-        return -1;
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+
+            if (Arrays.stream(tokens).mapToInt(Integer::parseInt).anyMatch(n -> n < 0)) {
+                throw new RuntimeException();
+            }
+
+            return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+        } else {
+            final String[] tokens = text.split(",|:");
+
+            if (Arrays.stream(tokens).mapToInt(Integer::parseInt).anyMatch(n -> n < 0)) {
+                throw new RuntimeException();
+            }
+
+            return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+        }
     }
 }
