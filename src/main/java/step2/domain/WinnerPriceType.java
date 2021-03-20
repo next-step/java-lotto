c57fd63d94aeca19fe7ccc9eebcc98749424ff62
hwangjeonghyeon <deert101@gmail.com>
2021-03-20 23:37:10 +0900

    [refactor] 로또 당첨금액 enum 추가

--- src/main/java/step2/domain/WinnerPriceType.java
+++ src/main/java/step2/domain/WinnerPriceType.java
@@ -1,2 +1,26 @@
-package step2.domain;public enum WinnerPriceType {
+package step2.domain;
+
+import java.util.Arrays;
+
+public enum WinnerPriceType {
+
+    THREE(3, 5000),
+    FOUR(4, 50000),
+    FIVE(5, 1500000),
+    SIX(6, 2000000000);
+
+    public int matchNumber;
+    public double winningAmount;
+
+    WinnerPriceType(int matchNumber, int winningAmount) {
+        this.matchNumber = matchNumber;
+        this.winningAmount = winningAmount;
+    }
+
+    public static WinnerPriceType of(int matchNumber) {
+        return Arrays.stream(WinnerPriceType.values())
+                .filter(winningTable -> winningTable.matchNumber == matchNumber)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 유형이 존재하지 않습니다."));
+    }
 }
