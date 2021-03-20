1506a07ee1b50ebf19473e2821a69a4af8b2c318
김진원 <jer122@naver.com>
2021-03-20 11:20:40 +0900

    feat : Constant 상수 클래스 추가

---
+++ src/main/java/splitandsum/Constant.java
@@ -0,0 +1,8 @@
+package splitandsum;
+
+public class Constant {
+    public static final int INPUT_ZERO = 0;
+    public static final int FILTER_NUMBER = 1;
+    public static final String DEFAULT_PATTERN = ",|:";
+    public static final String FILTER_PATTERN = "//(.)\n(.*)";
+}
