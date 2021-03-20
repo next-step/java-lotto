9a7f95a9f9a63671564e4eb9e73b08df9ea9f81f
cyr9210 <jipsyer@naver.com>
2021-03-20 17:00:48 +0900

    feat 입력된 금액이 몇장의 로또를 살 수 있는지

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+public class Money {
+
+  private int money;
+
+  public Money(int money) {
+    checkMoney(money);
+    this.money = money;
+  }
+
+  private void checkMoney(int money) {
+    if (money < 0) {
+      throw new IllegalArgumentException("Money should always be greater than 0.");
+    }
+  }
+
+  public int buyLotto() {
+    checkPurchaseLotto(money);
+    return money / 1000;
+  }
+
+  private void checkPurchaseLotto(int money) {
+    if (money % 1000 != 0) {
+      throw new IllegalArgumentException("money is over or less.");
+    }
+  }
+}
