36c68fb8eac4e6bf2e246d9095cb66c4486e0932
Donghui-Lee <ehdgml3206@naver.com>
2021-03-20 18:29:34 +0900

    refactor: StringAddCalculator 클래스에 InputValidator 클래스 적용

--- src/main/java/study/step1/controller/StringAddCalculator.java
+++ src/main/java/study/step1/controller/StringAddCalculator.java
@@ -1,20 +1,14 @@
 package study.step1.controller;
 
 import study.step1.domain.Calculator;
+import study.step1.validator.InputValidator;
 
 public class StringAddCalculator {
     public int splitAndSum(String input) {
-        if(isEmpty(input)) {
+        if(!InputValidator.valid(input)) {
             return 0;
         }
         Calculator calculator = new Calculator(input);
         return calculator.sum();
-    }
-
-    private boolean isEmpty(String input) {
-        if(input == null || input.trim().isEmpty()) {
-            return true;
-        }
-        return false;
     }
 }
--- src/main/java/study/step1/validator/InputValidator.java
+++ src/main/java/study/step1/validator/InputValidator.java
@@ -1,6 +1,12 @@
 package study.step1.validator;
 
 public class InputValidator {
+    public static boolean valid(String input) {
+        if(isEmpty(input) || isBlank(input)) {
+            return false;
+        }
+        return true;
+    }
 
     public static boolean isEmpty(String input) {
         return input == null;
