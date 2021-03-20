0324757cffb17eb7707f3bd781c0ecad630a0f06
Jake <cocahack@naver.com>
2021-03-20 17:16:05 +0900

    test: 로또 클래스 테스트 케이스 추가
    로또 번호를 검증하는 테스트 케이스를 추가하고, 기존 등수를 계산하는 테스트 케이스에
    보너스 볼도 포함하도록 수정

--- src/test/java/lottery/domain/LotteryTest.java
+++ src/test/java/lottery/domain/LotteryTest.java
@@ -1,6 +1,5 @@
 package lottery.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -12,10 +11,9 @@ import java.util.stream.Stream;
 
 import static lottery.domain.Prize.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LotteryTest {
-
-    private List<Integer> winningNumbers;
 
     static Stream<Arguments> generateTestCase() {
         return Stream.of(
@@ -29,9 +27,32 @@ class LotteryTest {
         );
     }
 
-    @BeforeEach
-    void init() {
-        winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    static Stream<Arguments> invalidLotteryTestCases() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 7, 10, 23, 36, 47), 2),
+            Arguments.of(Arrays.asList(1, 7, 10, 23, 36, 42), 52)
+        );
+    }
+
+    static Stream<Arguments> duplicatedNumberLotteries() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 7, 10, 23, 36, 36), 2),
+            Arguments.of(Arrays.asList(1, 7, 10, 23, 36, 42), 23)
+        );
+    }
+
+    @DisplayName("생성할 수 없는 번호가 포함되어 있으면 예외를 던진다.")
+    @ParameterizedTest(name = "로또 번호 - {0}")
+    @MethodSource("invalidLotteryTestCases")
+    void throwExceptionIfInvalidNumberContained(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lottery(numbers)).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("로또 번호 가운데 중복된 것이 있으면 예외를 던진다.")
+    @ParameterizedTest(name = "로또 번호 - {0}")
+    @MethodSource("duplicatedNumberLotteries")
+    void throwExceptionIfNumbersDuplicated(List<Integer> numbers) {
+        assertThatThrownBy(() -> new Lottery(numbers)).isInstanceOf(RuntimeException.class);
     }
 
     @ParameterizedTest(name = "번호: {0}, 예상 등수: {1}")
@@ -40,7 +61,10 @@ class LotteryTest {
     void getPrizeAfterComparingLotteries(List<Integer> numbers, Prize expected) {
         Lottery lottery = new Lottery(numbers);
 
-        assertThat(lottery.getPrize(winningNumbers)).isEqualTo(expected);
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int winningBonusNumber = 7;
+
+        assertThat(lottery.getPrize(winningNumbers, winningBonusNumber)).isEqualTo(expected);
     }
 
 }
