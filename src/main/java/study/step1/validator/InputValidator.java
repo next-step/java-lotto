88efe403c5b5e2fef30491fa7fbfa74a6a7b2f75
Donghui-Lee <ehdgml3206@naver.com>
2021-03-20 18:27:07 +0900

    feat: InputValidator 클래스 추가

---
+++ src/main/java/study/step1/validator/InputValidator.java
@@ -0,0 +1,11 @@
+package study.step1.validator;
+
+public class InputValidator {
+    public static boolean isEmpty(String input) {
+        return false;
+    }
+
+    public static boolean isBlank(String input) {
+        return false;
+    }
+}
