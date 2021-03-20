5a259fa242df0d87d955996bca1bdc85e11e0e54
Donghui-Lee <ehdgml3206@naver.com>
2021-03-20 17:30:30 +0900

    feat: PositiveNumber 클래스 추가

---
+++ src/main/java/study/step1/domain/PositiveNumber.java
@@ -0,0 +1,9 @@
+package study.step1.domain;
+
+public class PositiveNumber {
+    private int number = 0;
+
+    public PositiveNumber(String number) {
+        this.number = Integer.parseInt(number);
+    }
+}
