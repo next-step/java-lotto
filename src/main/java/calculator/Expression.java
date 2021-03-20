37d16df88d7b60a15765402c61529fe1b38a85b3
Beenie93 <seongbeen93@naver.com>
2021-03-20 14:19:10 +0900

    refactor: extract class - expression

---
+++ src/main/java/calculator/Expression.java
@@ -0,0 +1,29 @@
+package calculator;
+
+public class Expression {
+    private static final String NEGATIVE_OPERATOR = "-";
+    private final String expression;
+
+    public Expression(String expression) {
+        this.expression = expression;
+    }
+
+    public boolean isEmptyOrNull() {
+        return expression == null || expression.isEmpty();
+    }
+
+    public boolean isOneLetter() {
+        return expression.length() == 1;
+    }
+
+    public void isNegative() {
+        if (expression.contains(NEGATIVE_OPERATOR)) {
+            throw new RuntimeException("음수가 포함되어 있는지 확인해주세요.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return expression;
+    }
+}
--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -8,33 +8,18 @@ public class StringAddCalculator {
 
     private static final String REGEX = ",| |:";
     private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
-    private static final String NEGATIVE_OPERATOR = "-";
 
-    public static int splitAndSum(String expression) {
-        if (isEmptyOrNull(expression)) {
+    public static int splitAndSum(Expression expression) {
+        if (expression.isEmptyOrNull()) {
             return 0;
         }
 
-        if (isOneLetter(expression)) {
-            return Integer.parseInt(expression);
+        if (expression.isOneLetter()) {
+            return Integer.parseInt(expression.toString());
         }
 
-        isNegative(expression);
-        return sum(split(expression));
-    }
-
-    private static boolean isEmptyOrNull(String expression) {
-        return expression == null || expression.isEmpty();
-    }
-
-    private static boolean isOneLetter(String expression) {
-        return expression.length() == 1;
-    }
-
-    private static void isNegative(String expression) {
-        if (expression.contains(NEGATIVE_OPERATOR)) {
-            throw new RuntimeException("음수가 포함되어 있는지 확인해주세요.");
-        }
+        expression.isNegative();
+        return sum(split(expression.toString()));
     }
 
     private static String[] split(String expression) {
