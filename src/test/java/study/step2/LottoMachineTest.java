d3b08179cf7232968c2afe6ee4425e3b770d9ee9
Donghui-Lee <ehdgml3206@naver.com>
2021-03-21 03:27:38 +0900

    test: 실패하는 테스트 코드 추가

---
+++ src/test/java/study/step2/LottoMachineTest.java
@@ -0,0 +1,21 @@
+package study.step2;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import study.step2.domain.Lotto;
+import study.step2.domain.LottoMachine;
+
+public class LottoMachineTest {
+	@Test
+	@DisplayName("금액에 따른 로또 생성 테스트")
+	void purchase() {
+		LottoMachine lottoMachine = new LottoMachine();
+		List<Lotto> lottos = lottoMachine.purchase(12000);
+		assertThat(lottos.size()).isEqualTo(12);
+	}
+}
