d2d1fc456b50009ebc69255114e472d0b7c11c12
koola97620 <koola976@gmail.com>
2021-03-20 13:55:22 +0900

    feat: 문자열계산기 - 문자열 입력 기능

---
+++ src/main/java/stringCalculator/controller/StringCalculatorMain.java
@@ -0,0 +1,13 @@
+package stringCalculator.controller;
+
+import stringCalculator.view.InputView;
+
+public class StringCalculatorMain {
+
+  public static void main(String[] args) {
+    InputView inputView = new InputView();
+    String input = inputView.input();
+
+  }
+
+}
