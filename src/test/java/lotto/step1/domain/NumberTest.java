87d978dbcbb9a8acb795b5126e03f0179f406a8e
hwangyunho <hwang9yh@gmail.com>
2021-03-21 01:12:43 +0900

    test: Number 도메인 상태값 확인 테스트

--- src/test/java/lotto/step1/domain/NumberTest.java
+++ src/test/java/lotto/step1/domain/NumberTest.java
@@ -3,8 +3,10 @@ package lotto.step1.domain;
 import lotto.step1.exception.StringAddCalculatorException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberTest {
@@ -17,5 +19,13 @@ class NumberTest {
             Number.of(number);
         }).isInstanceOf(StringAddCalculatorException.class)
                 .hasMessage("음수값은 올 수 없습니다.");
+    }
+
+    @DisplayName("of로 값을 생성후, final 상태값 일치하는지 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "5:5", "10:10"}, delimiter = ':')
+    void number_상태값_테스트(int input, int expected) {
+        Number number = Number.of(input);
+        assertThat(number.getNumber()).isEqualTo(expected);
     }
 }
