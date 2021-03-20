7579a16df7071bfff609e8ca0d6be53456082b60
Dion <ksundong@gmail.com>
2021-03-20 15:34:33 +0900

    refactor: 쉼표로 구분된 두 개 숫자의 합을 반환하도록 변경
    메서드가 두 가지 일을 해서 문자열 배열의 합을 구하는 메서드를 추출했습니다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,12 +2,24 @@ package stringcalculator;
 
 public class StringCalculator {
 
+  public static final String DELIMITER = ",";
+
   private StringCalculator() {}
 
   public static int sum(String expression) {
     if (expression == null || expression.isEmpty()) {
       return 0;
     }
-    return Integer.parseInt(expression);
+
+    String[] numbers = expression.split(DELIMITER);
+    return sum(numbers);
+  }
+
+  private static int sum(String[] numbers) {
+    int sum = 0;
+    for (String number : numbers) {
+      sum += Integer.parseInt(number);
+    }
+    return sum;
   }
 }
