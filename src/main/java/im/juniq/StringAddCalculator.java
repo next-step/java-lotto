05771cdf0e0f3e3d1b148158ce54fefb038da7d9
juniq <juniqlim@gmail.com>
2021-03-21 20:48:06 +0900

    repactor: 널, 빈문자 체크 메소드 추출

--- src/main/java/im/juniq/StringAddCalculator.java
+++ src/main/java/im/juniq/StringAddCalculator.java
@@ -2,10 +2,16 @@ package im.juniq;
 
 public class StringAddCalculator {
 	public static int splitAndSum(String input) {
-		if (input == null || input.isEmpty()) {
+		if (checkNullOrEmpty(input)) {
 			return 0;
 		}
-
 		return 1;
+	}
+
+	private static boolean checkNullOrEmpty(String input) {
+		if (input == null || input.isEmpty()) {
+			return true;
+		}
+		return false;
 	}
 }
