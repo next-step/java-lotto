e0efc15019e75ba465d57caf40161bcd3ab92350
ad7549 <ad7549@gmail.com>
2021-03-21 18:04:02 +0900

    test: Add LottoTicketTest

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,7 +2,7 @@ package lotto.domain;
 
 import java.util.Objects;
 
-public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
 
@@ -31,5 +31,10 @@ public class LottoNumber {
     @Override
     public int hashCode() {
         return Objects.hash(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return 0;
     }
 }
---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoTicket {
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
+
+    }
+
+    public List<LottoNumber> ascendingLottoNumbers() {
+        return null;
+    }
+
+    public int matchingCount(List<LottoNumber> lottoNumbers) {
+        return -1;
+    }
+}
