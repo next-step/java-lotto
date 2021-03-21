a90af267fdedec0e20f7f11621c2accad30455bd
Beenie93 <seongbeen93@naver.com>
2021-03-22 01:53:16 +0900

    test: implement a test code for creating lotto ticket

---
+++ src/test/java/lotto/LottoTicketTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import lotto.domain.LottoTicket;
+import lotto.utils.LottoRandomNumberUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketTest {
+
+    @Test
+    @DisplayName("로또 티켓 생성")
+    public void create() throws Exception {
+        List<Integer> lottoNumbers = LottoRandomNumberUtil.lottoNumbers();
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        assertThat(lottoTicket).isEqualTo(new LottoTicket(lottoNumbers));
+    }
+}
