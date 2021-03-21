d00cdeb97aeae9c7605cb10d2ab968cf090c7cba
ad7549 <ad7549@gmail.com>
2021-03-21 17:08:34 +0900

    feat: Implement LottoNumber

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,7 +1,27 @@
 package lotto.domain;
 
-public class LottoNumber {
-    public LottoNumber(int expected) {
+import java.util.Objects;
 
+public class LottoNumber {
+    private final int number;
+
+    public LottoNumber(final int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException();
+        }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
