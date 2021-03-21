a52a792d277f547d4f19b937c0b1cc47f2c542ae
ttokey <ttokey.lee@gmail.com>
2021-03-22 01:23:22 +0900

    feat : Calculator 구현 및 단위테스트 완료

--- src/main/java/step1/Calculator.java
+++ src/main/java/step1/Calculator.java
@@ -1,7 +1,11 @@
 package step1;
 
 public class Calculator {
-    public static int add(Integer[] inputs){
-        return 0;
+    public static int add(Integer[] inputNumbers){
+        int result = 0;
+        for(Integer number : inputNumbers){
+            result += number;
+        }
+        return result;
     }
 }
