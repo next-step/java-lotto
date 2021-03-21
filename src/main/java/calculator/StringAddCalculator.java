b1d8e3d5730d2ffb54f0937bbb2c325c72ab61ac
Beenie93 <seongbeen93@naver.com>
2021-03-21 22:39:27 +0900

    refactor: replace parameter type Expression with String

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,8 +2,8 @@ package calculator;
 
 public class StringAddCalculator {
 
-    public static int splitAndSum(Expression expression) {
-        Splitted splitted = new Splitted(expression);
+    public static int splitAndSum(String expression) {
+        Splitted splitted = new Splitted(new Expression(expression));
         Sum sum = new Sum(splitted);
         return sum.sum();
     }
