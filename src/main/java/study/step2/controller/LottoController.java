129911228c6f3278a8139499ac1a870a5d1723e7
Donghui-Lee <ehdgml3206@naver.com>
2021-03-22 02:43:56 +0900

    feat: LottoController 클래스 추가

---
+++ src/main/java/study/step2/controller/LottoController.java
@@ -0,0 +1,20 @@
+package study.step2.controller;
+
+import study.step2.domain.LottoMachine;
+import study.step2.view.InputView;
+import study.step2.view.ResultView;
+
+public class LottoController {
+
+    private final LottoMachine lottoMachine;
+
+    public LottoController() {
+        lottoMachine = new LottoMachine();
+    }
+
+    public void play() {
+        int purchaseAmount = InputView.inputPurchaseAmount();
+        ResultView.printPurchaseCount(lottoMachine.purchase(purchaseAmount));
+        ResultView.printLottoNumbers(lottoMachine.getLottos());
+    }
+}
