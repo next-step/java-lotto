bc542936364e5b87e55e57ade7bbf7620890d6bd
jhLim97 <wnsgur2749@naver.com>
2021-03-21 18:04:47 +0900

    refactor: 로또 1~45 숫자 초기 생성 LottoPlay에서 처리

--- src/main/java/lotto/domain/LottoPlay.java
+++ src/main/java/lotto/domain/LottoPlay.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import lotto.dto.IssueNumber;
 import lotto.dto.LottoNumber;
 import lotto.dto.LottoNumbers;
-import lotto.util.Lotto;
 
 import java.util.*;
 
@@ -11,7 +10,16 @@ public class LottoPlay {
 
     private static final int LOTTO_PRICE = 1000;
     private static final int LOTTO_LENGTH = 6;
+    public static final int LOTTO_MIN = 1;
+    public static final int LOTTO_MAX = 45;
+    public static final List<LottoNumber> LOTTO_NUMBER = new ArrayList<>();
     private final TotalLottoNumbers totalLottoNumbers = new TotalLottoNumbers();
+
+    static {
+        for (int number = LOTTO_MIN; number <= LOTTO_MAX; number++) {
+            LOTTO_NUMBER.add(new LottoNumber(number));
+        }
+    };
 
     public Map<IssueNumber, LottoNumbers> getLottoNumbers() {
         return totalLottoNumbers.getTotalLottoNumbers();
@@ -28,9 +36,9 @@ public class LottoPlay {
     public LottoNumbers createLottoNumber() {
         List<LottoNumber> lottoNumber = new ArrayList<>();
 
-        Collections.shuffle(Lotto.LOTTO_NUMBER);
+        Collections.shuffle(LOTTO_NUMBER);
         for (int i = 0; i < LOTTO_LENGTH; i++) {
-            lottoNumber.add(new LottoNumber(Lotto.LOTTO_NUMBER.get(i)
+            lottoNumber.add(new LottoNumber(LOTTO_NUMBER.get(i)
                                                         .getLottoNumber()));
         }
         Collections.sort(lottoNumber);
