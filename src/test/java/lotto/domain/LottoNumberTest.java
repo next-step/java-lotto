bf6ad4553e939975359bdfbae3d4f33e5f5d9409
ad7549 <ad7549@gmail.com>
2021-03-21 22:58:57 +0900

    test: Add compareTo test to LottoNumber

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -15,7 +15,18 @@ public class LottoNumberTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
-    public void create_out_of_range(int expected) {
+    public void createOutOfRange(int expected) {
         assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(expected));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10})
+    public void compareTo(int a) {
+        final int b = 5;
+        final int expected = Integer.compare(a, 5);
+
+        final int result = new LottoNumber(a).compareTo(new LottoNumber(b));
+
+        assertThat(result).isEqualTo(expected);
     }
 }
