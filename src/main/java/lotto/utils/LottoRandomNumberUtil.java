912e16db53aeb0ba2171e3a5444530d6eb53a605
Beenie93 <seongbeen93@naver.com>
2021-03-21 23:15:10 +0900

    feat: implement a util to generate lotto random number

---
+++ src/main/java/lotto/utils/LottoRandomNumberUtil.java
@@ -0,0 +1,23 @@
+package lotto.utils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoRandomNumberUtil {
+    private static final int NUMBER_OF_LOTTO_NUMBER = 6;
+    private static final int BOUND_MIN = 1;
+    private static final int BOUND_MAX = 46;
+    private static final List<Integer> numbers = IntStream.range(BOUND_MIN, BOUND_MAX).boxed().collect(Collectors.toList());
+
+    private LottoRandomNumberUtil() {
+    }
+
+    public static List<Integer> lottoNumbers() {
+        Collections.shuffle(numbers);
+        List<Integer> lottoNumbers = numbers.subList(0, NUMBER_OF_LOTTO_NUMBER);
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
+}
