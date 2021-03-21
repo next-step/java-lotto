2487ccfc64f3f33fecbb5cc2a7fec8df567f4afa
Donghui-Lee <ehdgml3206@naver.com>
2021-03-22 02:25:56 +0900

    test: LottoWin 실패하는 테스트 코드 추가

---
+++ src/test/java/study/step2/LottoWinTest.java
@@ -0,0 +1,21 @@
+package study.step2;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import study.step2.domain.LottoWin;
+
+public class LottoWinTest {
+    @ParameterizedTest(name = "합계 테스트")
+    @CsvSource(value = {"3,4,5,6:2001555000", "3,3,3:15000"}, delimiter = ':')
+    public void sum(String inputs, String expected) {
+        LottoWin lottoWin = new LottoWin();
+        String[] hits = inputs.split(",");
+        for(String hit : hits) {
+            lottoWin.hit(Integer.parseInt(hit));
+        }
+        assertThat(lottoWin.sum()).isEqualTo(Integer.parseInt(expected));
+    }
+}
