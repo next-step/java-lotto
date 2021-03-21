cc366488a146c69a64395d998b928c0142914c5f
Beenie93 <seongbeen93@naver.com>
2021-03-22 01:49:52 +0900

    feat: implement a util to check precondition

---
+++ src/main/java/lotto/utils/PreConditionUtil.java
@@ -0,0 +1,15 @@
+package lotto.utils;
+
+public class PreConditionUtil {
+
+    private static final String CHECK_NULL_OR_EMPTY = "null 또는 빈 값인지 확인해주세요";
+
+    private PreConditionUtil() {
+    }
+
+    public static void isNullOrEmpty(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException(CHECK_NULL_OR_EMPTY);
+        }
+    }
+}
