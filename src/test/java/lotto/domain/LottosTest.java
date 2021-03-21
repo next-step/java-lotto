77a6b996ce2b3b189ec42fd2458edee5290d6aaa
PandaHun <gnsgnsek@gmail.com>
2021-03-21 18:16:42 +0900

    test: Create lottos test

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosTest {
+
+    @DisplayName("로또 집합 생성 테스트")
+    @Test
+    void lottosCreatedTest() {
+        List<LottoNumber> lottoNumberList = new ArrayList<>();
+        for (int i  = 1; i <= 6; i++) {
+            lottoNumberList.add(new LottoNumber(i));
+        }
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 1; i <=5 ; i++) {
+            lottos.add(new Lotto(lottoNumberList));
+        }
+        Lottos lottos1 = new Lottos(lottos);
+        assertThat(lottos1.lottoCount()).isEqualTo(5);
+    }
+}
