44b4631bd27cc87f21b538ced78faaf148d8343a
ad7549 <ad7549@gmail.com>
2021-03-22 01:06:53 +0900

    feat: Implement LottoStore

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,10 +1,55 @@
 package lotto.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoStore {
-    public List<LottoTicket> lottoTickets(int price) {
-        return new ArrayList<>();
+    private static final int DEFAULT_PRICE = 1000;
+
+    private final int price;
+
+    public LottoStore() {
+        this(DEFAULT_PRICE);
+    }
+
+    public LottoStore(int price) {
+        this.price = price;
+    }
+
+    public List<LottoTicket> lottoTickets(int payment) {
+        if (payment < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return Stream.generate(this::autoCreatedLottoTicket)
+                .limit(payment / price)
+                .collect(Collectors.toList());
+    }
+
+    private LottoTicket autoCreatedLottoTicket() {
+        return new LottoTicket(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)
+        ));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoStore that = (LottoStore) o;
+        return price == that.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
     }
 }
