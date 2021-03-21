82b580285fc30ed59c4d3eba7c51daf72ffabbad
keke0123 <keke0123@naver.com>
2021-03-21 18:05:20 +0900

    (feat) custom pattern 으로 구분할수 있게 수정

--- src/main/java/step01/code/domain/StringAddCalculator.java
+++ src/main/java/step01/code/domain/StringAddCalculator.java
@@ -2,6 +2,8 @@ package step01.code.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.junit.platform.commons.util.StringUtils;
@@ -9,15 +11,26 @@ import org.junit.platform.commons.util.StringUtils;
 public class StringAddCalculator {
 
   public static int splitAndSum(String str) {
-    // return new Number(str).number();
     if(StringUtils.isBlank(str)) {
       return 0;
+    }
+    Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+    if(m.find()) {
+      String customDelimiter = m.group(1);
+      return sum(split(m.group(2), customDelimiter));
     }
     return sum(split(str));
   }
 
   public static List<Number> split(String str) {
     return Arrays.asList(str.split(",|:"))
+      .stream()
+      .map((num) -> new Number(num))
+      .collect(Collectors.toList()); 
+  }
+
+  public static List<Number> split(String str, String delimiter) {
+    return Arrays.asList(str.split(delimiter))
       .stream()
       .map((num) -> new Number(num))
       .collect(Collectors.toList()); 
