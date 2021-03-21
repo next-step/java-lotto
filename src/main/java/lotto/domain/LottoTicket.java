399a64392043682ea6a466a779dfd73735ce3df7
ad7549 <ad7549@gmail.com>
2021-03-21 23:37:28 +0900

    feat: Implement LottoTicket

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,17 +1,49 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class LottoTicket {
-    public LottoTicket(List<LottoNumber> lottoNumbers) {
+    private final List<LottoNumber> lottoNumbers;
 
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+
+        if (lottoNumbers.stream().distinct().count() != lottoNumbers.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.lottoNumbers = lottoNumbers;
     }
 
     public List<LottoNumber> ascendingLottoNumbers() {
-        return null;
+        final List<LottoNumber> newLottoNumbers = new ArrayList<>(lottoNumbers);
+
+        Collections.sort(newLottoNumbers);
+
+        return newLottoNumbers;
     }
 
     public int matchingCount(List<LottoNumber> lottoNumbers) {
-        return -1;
+        return (int) this.lottoNumbers.stream()
+                .filter(lottoNumbers::contains)
+                .count();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoTicket that = (LottoTicket) o;
+        return Objects.equals(lottoNumbers, that.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
     }
 }
