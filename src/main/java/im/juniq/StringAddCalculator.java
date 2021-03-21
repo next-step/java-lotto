214c93fa045e66477444f6b85a8a9eae1c8184a5
juniq <juniqlim@gmail.com>
2021-03-21 20:40:45 +0900

    feature: 널 또는 빈문자 입력

---
+++ src/main/java/im/juniq/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package im.juniq;
+
+public class StringAddCalculator {
+	public static int splitAndSum(String input) {
+		if (input == null || input.isEmpty()) {
+			return 0;
+		}
+
+		return 1;
+	}
+}
