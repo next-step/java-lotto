7d48dac03541b7dc16d6b053ae2f856b1ab6e987
Donghui-Lee <ehdgml3206@naver.com>
2021-03-21 18:14:08 +0900

    refactor: 테스트 성공 구현

--- src/main/java/study/step2/domain/Lotto.java
+++ src/main/java/study/step2/domain/Lotto.java
@@ -1,11 +1,21 @@
 package study.step2.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> collect) {
-        lottoNumbers = collect;
+        lottoNumbers = this.sorted(collect);
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public List<Integer> sorted(List<Integer> list) {
+        Collections.sort(list);
+        return list;
     }
 }
