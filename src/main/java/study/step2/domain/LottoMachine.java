14ece479a98bd4c66e107418aa7089a95d1d6623
Donghui-Lee <ehdgml3206@naver.com>
2021-03-22 02:50:33 +0900

    refactor: purchase 메서드 수정

--- src/main/java/study/step2/domain/LottoMachine.java
+++ src/main/java/study/step2/domain/LottoMachine.java
@@ -5,20 +5,25 @@ import java.util.List;
 
 public class LottoMachine {
     private static final int LOTTO_PRICE = 1000;
+    private List<Lotto> lottos;
 
     public LottoMachine() {
+        lottos = new ArrayList<>();
     }
 
-    public List<Lotto> purchase(int amount) {
-        List<Lotto> lottos = new ArrayList<>();
-        for(int i=0; i<getLottoCount(amount); i++) {
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public int purchase(int amount) {
+        int count = getLottoCount(amount);
+        for(int i=0; i<count; i++) {
             lottos.add(LottoFactory.createLotto());
         }
-        return lottos;
+        return lottos.size();
     }
 
     private int getLottoCount(int amount) {
         return amount / LOTTO_PRICE;
     }
-
 }
--- src/test/java/study/step2/LottoMachineTest.java
+++ src/test/java/study/step2/LottoMachineTest.java
@@ -2,12 +2,9 @@ package study.step2;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import study.step2.domain.Lotto;
 import study.step2.domain.LottoMachine;
 
 public class LottoMachineTest {
@@ -15,7 +12,6 @@ public class LottoMachineTest {
     @DisplayName("금액에 따른 로또 생성 테스트")
     void purchase() {
         LottoMachine lottoMachine = new LottoMachine();
-        List<Lotto> lottos = lottoMachine.purchase(12000);
-        assertThat(lottos.size()).isEqualTo(12);
+        assertThat(lottoMachine.purchase(12000)).isEqualTo(12);
     }
 }
