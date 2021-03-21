6f9e2804b872b18f0102cff4d8cb78399cb475f2
hwangyunho <hwang9yh@gmail.com>
2021-03-22 00:01:06 +0900

    feat: InputView 로또 구매

---
+++ src/main/java/lotto/step2/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.step2.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static Scanner scanner = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static int enterLottoPurchaseAmount() {
+        System.out.println(LOTTO_PURCHASE_MESSAGE);
+        return scanner.nextInt();
+    }
+}
