abb82a6557c9c94cbd21abc9cc19dc1ebf13d0aa
Donghui-Lee <ehdgml3206@naver.com>
2021-03-22 02:43:39 +0900

    feat: Main 클래스 추가

---
+++ src/main/java/study/step2/LottoMain.java
@@ -0,0 +1,11 @@
+package study.step2;
+
+import study.step2.controller.LottoController;
+
+public class LottoMain {
+
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.play();
+    }
+}
