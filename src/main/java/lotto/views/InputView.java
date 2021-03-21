7cb4a29ad30e9288f812aef238bd4a037724830f
weleek <weleek@gmail.com>
2021-03-21 19:54:44 +0900

    feat: 수동 구매 금액, 로또 번호 입력 기능

--- src/main/java/lotto/views/InputView.java
+++ src/main/java/lotto/views/InputView.java
@@ -8,6 +8,8 @@ public class InputView {
     public final static String AMOUNT = "구입금액을 입력해 주세요.";
     public final static String WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     public final static String BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+    public final static String MANUAL_BUY_QUANTITY = "수동으로 구매할 로또 수를 입력해 주세요.";
+    public final static String MANUAL_BUY_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
 
     private InputView() { }
 
@@ -33,5 +35,13 @@ public class InputView {
 
     public static String bonusNumber() {
         return value(BONUS_NUMBER);
+    }
+
+    public static String manualBuyQuantity() {
+        return value(MANUAL_BUY_QUANTITY);
+    }
+
+    public static String manualBuyNumbers() {
+        return value(MANUAL_BUY_NUMBERS);
     }
 }
