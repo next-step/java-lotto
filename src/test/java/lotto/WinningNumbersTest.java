abfc96e6f3e3b3f3a1f1b442526cbd2b82c581ce
Beenie93 <seongbeen93@naver.com>
2021-03-23 00:55:38 +0900

    refactor: replace List<Integer> with String[]

--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -4,10 +4,6 @@ import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningNumbersTest {
@@ -15,7 +11,7 @@ public class WinningNumbersTest {
     @Test
     @DisplayName("로또 당첨 번호 생성")
     public void create() throws Exception {
-        List<Integer> numbers = IntStream.range(1, 7).boxed().collect(Collectors.toList());
+        String[] numbers = {"1", "2", "3", "4", "5", "6"};
         WinningNumbers winningNumbers = new WinningNumbers(numbers);
         assertThat(winningNumbers).isEqualTo(new WinningNumbers(numbers));
     }
