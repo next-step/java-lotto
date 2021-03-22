be84df35c754a8ae22b7380ade5e3f064cfc4837
Donghui-Lee <ehdgml3206@naver.com>
2021-03-22 12:58:16 +0900

    feat: Amount 클래스 추가

---
+++ src/main/java/study/step2/domain/Amount.java
@@ -0,0 +1,20 @@
+package study.step2.domain;
+
+public class Amount {
+    private final int amount;
+
+    public Amount(int amount) {
+        valid(amount);
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    private void valid(int amount) {
+        if(amount < 0) {
+            throw new IllegalArgumentException("유효하지 않는 금액입니다.");
+        }
+    }
+}
