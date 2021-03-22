4373e2855de14c8a7d6def2ea238aa4576ec969c
Beenie93 <seongbeen93@naver.com>
2021-03-22 22:59:08 +0900

    test: implement a test code to save multiple lotto tickets

---
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTickets;
+import lotto.utils.LottoRandomNumberUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketsTest {
+
+    @Test
+    @DisplayName("다수의 로또 티켓 저장")
+    public void add() throws Exception {
+        LottoTickets lottoTickets = new LottoTickets();
+        lottoTickets.add(new LottoTicket(LottoRandomNumberUtil.lottoNumbers()));
+        lottoTickets.add(new LottoTicket(LottoRandomNumberUtil.lottoNumbers()));
+
+        assertThat(lottoTickets.lottoTickets()).hasSize(2);
+    }
+}
