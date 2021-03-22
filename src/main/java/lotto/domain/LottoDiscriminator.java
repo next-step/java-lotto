17083268421b5dbddf85a2c70142c4f48fa32013
ad7549 <ad7549@gmail.com>
2021-03-22 17:24:11 +0900

    test: Add LottoDiscriminatorTest

---
+++ src/main/java/lotto/domain/LottoDiscriminator.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoDiscriminator {
+    public LottoDiscriminator(List<LottoNumber> lottoNumbers) {
+    }
+
+    public int matchingCount(LottoTicket lottoTickets) {
+        return -1;
+    }
+}
