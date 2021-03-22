71e0482365ca0811a238f44b746c4325b9670135
623nana <6230nana@gmail.com>
2021-03-22 21:49:28 +0900

    feat: 로또 번호 생성 기능

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -3,4 +3,6 @@ package lotto.constant;
 public class Constant {
 
   public static final int UNIT_OF_DIVISION = 1000;
+  public static final int MIN_NUM = 1;
+  public static final int MAX_NUM = 45;
 }
