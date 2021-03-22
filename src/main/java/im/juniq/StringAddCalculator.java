c89efab12112c6b66ba9e7b5d685fbb5d5dfc36b
juniqlim <juniqlim@gmail.com>
2021-03-22 10:40:15 +0900

    [로또 - Step1] 코드리뷰 요청 드립니다! (#1292)
    * test: 테스트코드 작성
    
    문자열 덧셈 계산기 미션의 테스트코드를 가져옴.
    
    * feature: 널 또는 빈문자 입력
    
    * repactor: 널, 빈문자 체크 메소드 추출
    
    * feature: 숫자하나 입력
    
    * feature: 쉼표구분자로 숫자를 구분하여 덧셈
    
    * repactor: 덧셈 메소드 추출
    
    * repactor: if문을 더 단순하게
    
    * repactor: for문을 더 단순하게
    
    * feature: 쉼표 또는 콜론 구분자로 구분하여 덧셈
    
    * feature: 커스텀 구분자로 구분하여 덧셈
    
    * repactor: 메소드명 수정
    
    * feature: 음수를 전달할 경우 RuntimeException 예외가 발생
    
    * repactor: 메소드 추출
    
    * repactor: 메소드 순서변경
    
    * repactor: 메소드 책임 분리
    
    splitedNumbers 에서 checkNegarive 책임을 분리
    
    * repactor: 상수를 static final로

---
+++ src/main/java/im/juniq/StringAddCalculator.java
@@ -0,0 +1,53 @@
+package im.juniq;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+	private static final String BASIC_DELIMITER = ",:";
+	private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+	private static final String NEGATIVE_EXCEPTION_MESSAGE = "숫자는 양수만 입력 가능합니다.";
+
+	public static int splitAndSum(String input) {
+		if (nullableOrEmpty(input)) {
+			return 0;
+		}
+		String[] numbers = splitedNumbers(input);
+		checkNegative(numbers);
+		return sum(numbers);
+	}
+
+	private static boolean nullableOrEmpty(String text) {
+		return text == null || text.isEmpty();
+	}
+
+	private static String[] splitedNumbers(String text) {
+		Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(text);
+		String customDelimiter = "";
+		if (m.find()) {
+			customDelimiter = m.group(1);
+			text = m.group(2);
+		}
+		return text.split("[" + BASIC_DELIMITER + customDelimiter + "]");
+	}
+
+	private static int sum(String[] numbers) {
+		int criterion = 0;
+		for (String number : numbers) {
+			criterion += Integer.parseInt(number);
+		}
+		return criterion;
+	}
+
+	private static void checkNegative(String[] numbers) {
+		for (String number: numbers) {
+			checkNegative(number);
+		}
+	}
+
+	private static void checkNegative(String number) {
+		if (Integer.parseInt(number) < 0) {
+			throw new RuntimeException(NEGATIVE_EXCEPTION_MESSAGE);
+		}
+	}
+}
