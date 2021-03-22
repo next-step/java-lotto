1c1f7fb2b1c97fbafde11ab005193b4ea01166d7
yonghwa <qhdl0224@gmail.com>
2021-03-22 22:42:03 +0900

    [feat] 문자열 null, 빈문자 체크 기능 구현, 숫자 1개, 콤마로 구분 시 문자열 합 테스트, 구현 추가

---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,19 @@
+package step1;
+
+import java.util.Arrays;
+
+public class StringAddCalculator {
+
+    public StringAddCalculator(){
+
+    }
+
+    public int sum(String input) {
+        if(input == null || input.isEmpty()) {
+            return 0;
+        }
+        return Arrays.stream(input.split(","))
+                .mapToInt(i-> Integer.parseInt(i))
+                .sum();
+    }
+}
--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -28,5 +28,24 @@ public class StringAddCalculatorTest {
         int result = stringAddCalculator.sum("");
         assertThat(result).isEqualTo(0);
     }
+    @Test
+    @DisplayName("구분자가 없는 경우에도 split 되는지 테스트")
+    void split_테스트() {
+        int result = stringAddCalculator.sum("");
+        assertThat(result).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는 테스트")
+    void sum_숫자_한개() {
+        int result = stringAddCalculator.sum("3");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 하는 테스트")
+    void sum_콤마_구분자_테스트(){
+        int result = stringAddCalculator.sum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
 
 }
