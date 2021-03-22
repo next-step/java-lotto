407fc7f170ff76ac6a93956af5e0e78f00ea9c0d
Beenie93 <seongbeen93@naver.com>
2021-03-23 01:11:51 +0900

    feat: add a exception case when winning members include character

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,17 +4,37 @@ import java.util.Objects;
 
 public class WinningNumbers {
     private static final int NUMBER_LENGTH = 6;
-    private static final String CHECK_LENGTH = String.format("로또 번호의 길이는 {}(이)여야 합니다.", NUMBER_LENGTH);
+    private static final String CHECK_LENGTH = String.format("당첨 번호의 길이는 {}(이)여야 합니다.", NUMBER_LENGTH);
+    private static final String CHECK_INTEGER = "당첨 번호는 모두 숫자여야 합니다.";
     private final String[] winningNumbers;
 
     public WinningNumbers(String[] winningNumbers) {
-        checkLength(winningNumbers);
+        validate(winningNumbers);
         this.winningNumbers = winningNumbers;
+    }
+
+    private void validate(String[] winningNumbers) {
+        checkLength(winningNumbers);
+        checkInteger(winningNumbers);
     }
 
     private void checkLength(String[] winningNumbers) {
         if (winningNumbers.length != NUMBER_LENGTH) {
             throw new IllegalArgumentException(CHECK_LENGTH);
+        }
+    }
+
+    private void checkInteger(String[] winningNumbers) {
+        for (String winningNumber : winningNumbers) {
+            checkInteger(winningNumber);
+        }
+    }
+
+    private void checkInteger(String winningNumber) {
+        try {
+            Integer.parseInt(winningNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(CHECK_INTEGER);
         }
     }
 
