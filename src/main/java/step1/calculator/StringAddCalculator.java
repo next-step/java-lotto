3298939eabfb000b68fd137d7035b4eeb3613b45
김우재 <kwj1270@naver.com>
2021-03-22 13:40:28 +0900

    feat:StringAddCalculator splitAndSum에 null 또는 빈 문자열을 넣었을 때 0 반환 구현

--- src/main/java/step1/calculator/StringAddCalculator.java
+++ src/main/java/step1/calculator/StringAddCalculator.java
@@ -1,6 +1,8 @@
 package step1.calculator;
 
 public class StringAddCalculator {
-    public static int splitAndSum(Object o) {
+    public static int splitAndSum(String expression) {
+        if(expression == null || expression.isEmpty()) return 0;
+        return 1;
     }
 }
