70055c4bb71051df773127f32e7755d37f407a9b
ad7549 <ad7549@gmail.com>
2021-03-22 22:42:32 +0900

    feat: Implement LottoStatistics

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,12 +1,47 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoStatistics {
-    public LottoStatistics(LottoDiscriminator lottoDiscriminator, List<LottoTicket> lottoTickets) {
+    private final LottoDiscriminator lottoDiscriminator;
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoStatistics(final LottoDiscriminator lottoDiscriminator, final List<LottoTicket> lottoTickets) {
+        this.lottoDiscriminator = lottoDiscriminator;
+        this.lottoTickets = lottoTickets;
+    }
+
+    private int matchingLottoTicketsCount(final int matchingCount) {
+        return (int) lottoTickets.stream().filter(e -> lottoDiscriminator.matchingCount(e) == matchingCount).count();
     }
 
     public int threeMatchingLottoTicketsCount() {
-        return -1;
+        return matchingLottoTicketsCount(3);
+    }
+
+    public int fourMatchingLottoTicketsCount() {
+        return matchingLottoTicketsCount(4);
+    }
+
+    public int fiveMatchingLottoTicketsCount() {
+        return matchingLottoTicketsCount(5);
+    }
+
+    public int sixMatchingLottoTicketsCount() {
+        return matchingLottoTicketsCount(6);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoStatistics that = (LottoStatistics) o;
+        return Objects.equals(lottoDiscriminator, that.lottoDiscriminator) && Objects.equals(lottoTickets, that.lottoTickets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoDiscriminator, lottoTickets);
     }
 }
