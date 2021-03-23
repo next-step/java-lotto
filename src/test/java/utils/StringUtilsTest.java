5d6b3aeee1da7db91a535002d6972165702e327f
김우재 <kwj1270@naver.com>
2021-03-23 12:55:40 +0900

    test:StringUtils 에 대한 테스트 작성

---
+++ src/test/java/utils/StringUtilsTest.java
@@ -0,0 +1,69 @@
+package utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @DisplayName("문자열이 Null을 검증하는 테스트")
+    @Test
+    void isNull_Null값() {
+        // given
+        String inputData = null;
+
+        // when
+        boolean actual = StringUtils.isNull(inputData);
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("문자열이 공백값인지를 검증하는 테스트")
+    @Test
+    void isBlank_공백값들() {
+        // given
+        String emptyData = "";
+        String blankData = "  ";
+
+        // when and then
+        assertAll(
+                () -> assertThat(StringUtils.isBlank(emptyData)).isTrue(),
+                () -> assertThat(StringUtils.isBlank(blankData)).isTrue()
+        );
+
+    }
+
+    @DisplayName("문자열이 공백 또는 널인지를 검증하는 테스트")
+    @Test
+    void isNullOrBlank_널과공백값들() {
+        // given
+        String nullData = null;
+        String emptyData = "";
+        String blankData = "  ";
+
+        // when and then
+        assertAll(
+                () -> assertThat(StringUtils.isNullOrBlank(nullData)).isTrue(),
+                () -> assertThat(StringUtils.isNullOrBlank(emptyData)).isTrue(),
+                () -> assertThat(StringUtils.isNullOrBlank(blankData)).isTrue()
+        );
+    }
+
+    @DisplayName("문자열을 쉼표로 나누었을 때의 String 배열 반환되는지 테스트")
+    @Test
+    void splitByComma_쉼표있는문자열() {
+        // given
+        String inputData = "1,2,3";
+
+        // when
+        String[] data = StringUtils.splitByComma(inputData);
+        int actual = data.length;
+
+        // then
+        assertThat(actual).isEqualTo(3);
+    }
+
+}
