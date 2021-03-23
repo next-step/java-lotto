963757d7aa1d7c30d21fa3449ce91b0724d20f02
Dion <ksundong@gmail.com>
2021-03-23 22:52:22 +0900

    refactor: 로또 번호 생성이 성공하도록 수정

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,10 +1,29 @@
 package lotto;
 
+import java.util.Objects;
+
 public final class LottoNumber {
 
   private final int lottoNumber;
 
   public LottoNumber(int lottoNumber) {
     this.lottoNumber = lottoNumber;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LottoNumber)) {
+      return false;
+    }
+    LottoNumber that = (LottoNumber) o;
+    return lottoNumber == that.lottoNumber;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lottoNumber);
   }
 }
