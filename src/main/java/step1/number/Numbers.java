476928d9e3bda165e5dfbdba2edfb10de60dab31
김우재 <kwj1270@naver.com>
2021-03-23 17:44:00 +0900

    feat:Numbers 인스턴스 생성시 Null값 주입시 예외 처리 발생 기능 구현

--- src/main/java/step1/number/Numbers.java
+++ src/main/java/step1/number/Numbers.java
@@ -7,7 +7,13 @@ public final class Numbers {
     private final List<Number> numbers;
 
     public Numbers(List<Number> numbers) {
+        if(isNull(numbers)) {
+            throw new IllegalArgumentException("null 값을 입력했습니다.");
+        }
         this.numbers = numbers;
     }
 
+    private final boolean isNull(List<Number> numbers){
+        return numbers == null;
+    }
 }
