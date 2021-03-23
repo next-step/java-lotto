d6f82efc698a57d8fada7ac4ba6d0b27393c95cf
김우재 <kwj1270@naver.com>
2021-03-23 17:05:26 +0900

    test:Number 인스턴스 숫자가 아닌 문자열 입력시 예외처리 여부 테스트

--- src/test/java/step1/number/NumberTest.java
+++ src/test/java/step1/number/NumberTest.java
@@ -24,15 +24,30 @@ class NumberTest {
 
     @DisplayName("Number 인스턴스 음수 입력시 예외처리 여부 테스트")
     @Test
-    void 검증() {
+    void 검증_음수() {
         // given
         int inputValue = -1;
 
         // when and then
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             assertThat(new Number(inputValue)).isNotNull();
         }).isInstanceOf(NegativeNumberInputException.class)
-        .hasMessageContaining( "음수가 입력되었습니다.");
+                .hasMessageContaining("음수가 입력되었습니다.");
 
     }
+
+    @DisplayName("Number 인스턴스 숫자가 아닌 문자열 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_숫자아닌문자열() {
+        // given
+        String inputValue = "one";
+
+        // when and then
+        assertThatThrownBy(() -> {
+            assertThat(new Number(inputValue)).isNotNull();
+        }).isInstanceOf(StringNumberFormatException.class)
+                .hasMessageContaining("음수가 입력되었습니다.");
+
+    }
+
 }
