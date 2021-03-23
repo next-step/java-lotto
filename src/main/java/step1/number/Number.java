7327ddaeb6b91bc72fb7ccdca3ad2eb95b8d5b3d
김우재 <kwj1270@naver.com>
2021-03-23 17:34:32 +0900

    feat:Number 인스턴스가 소유한 값을 기준으로 객체 비교하는 기능 구현

--- src/main/java/step1/number/Number.java
+++ src/main/java/step1/number/Number.java
@@ -3,6 +3,8 @@ package step1.number;
 import step1.exception.NegativeNumberInputException;
 import step1.exception.StringNumberFormatException;
 
+import java.util.Objects;
+
 public final class Number {
 
     private final int number;
@@ -35,4 +37,16 @@ public final class Number {
         return number;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
