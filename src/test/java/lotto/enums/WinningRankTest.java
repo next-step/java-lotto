65d4624f34a0ef9582e78a1e58c57364a2587c6b
Beenie93 <seongbeen93@naver.com>
2021-03-24 02:23:40 +0900

    test: implement a test code for winning rank

---
+++ src/test/java/lotto/enums/WinningRankTest.java
@@ -0,0 +1,39 @@
+package lotto.enums;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningRankTest {
+
+    @Test
+    @DisplayName("등수 반환")
+    public void rank() throws Exception {
+        int rank = WinningRank.FIRST_PLACE.rank();
+        assertThat(rank).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("상금 반환")
+    public void prize() throws Exception {
+        int firstPrize = WinningRank.FIRST_PLACE.prize();
+        assertThat(firstPrize).isEqualTo(2_000_000_000);
+    }
+
+    @Test
+    @DisplayName("일치하는 수 반환")
+    public void MatchedCount() throws Exception {
+        int MatchedCount = WinningRank.FIRST_PLACE.matchedCount();
+        assertThat(MatchedCount).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("일치하는 수에 따른 타입 반환")
+    public void findByMacthedCount() throws Exception {
+        WinningRank firstPlace = WinningRank.findByMacthedCount(6);
+        WinningRank etc = WinningRank.findByMacthedCount(2);
+        assertThat(firstPlace).isEqualTo(WinningRank.FIRST_PLACE);
+        assertThat(etc).isEqualTo(WinningRank.ETC);
+    }
+}
