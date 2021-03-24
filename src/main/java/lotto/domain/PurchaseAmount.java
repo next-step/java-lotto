eca3fdf5598e42ab637e432fb200e2dc810419f6
Beenie93 <seongbeen93@naver.com>
2021-03-24 23:00:12 +0900

    refactor: change string formatting

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class PurchaseAmount {
     private final static int PURCHASE_AMOUNT_MIN = 1000;
-    private final static String GREATER_THAN_MIN = String.format("구매금액은 최소 {}원 이상이여야 합니다.", PURCHASE_AMOUNT_MIN);
+    private final static String GREATER_THAN_MIN = String.format("구매금액은 최소 %d원 이상이여야 합니다.", PURCHASE_AMOUNT_MIN);
     private final static String HAS_CHARACTER = "숫자가 아닌 문자가 포함되어 있는지 확인해주세요.";
     private final int purchaseAmount;
 
