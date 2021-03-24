71039fa3937d8983b4caf1b24c74ab34c77497eb
Beenie93 <seongbeen93@naver.com>
2021-03-25 00:36:48 +0900

    feat: add a exception case to check number of delimeter

--- src/main/java/lotto/utils/SplitUtil.java
+++ src/main/java/lotto/utils/SplitUtil.java
@@ -1,20 +1,34 @@
 package lotto.utils;
 
+import java.util.Arrays;
+
 public class SplitUtil {
 
-    public static final String DELIMITER = ",";
+    private static final String DELIMITER = ",";
+    private static final long NUMBER_OF_DELIMITER_STANDARD = 5;
 
     private SplitUtil() {
     }
 
     public static String[] split(String expression) {
         checkDelimiter(expression);
+        checkNumberOfDelimiter(expression.split(""));
         return expression.split(DELIMITER);
     }
 
     private static void checkDelimiter(String expression) {
         if (!expression.contains(DELIMITER)) {
             throw new IllegalArgumentException("구분자가 \",\"인지 확인해주세요.");
+        }
+    }
+
+    private static void checkNumberOfDelimiter(String[] expression) {
+        long count = Arrays.stream(expression)
+                .filter(letter -> letter.equals(DELIMITER))
+                .count();
+
+        if (count != NUMBER_OF_DELIMITER_STANDARD) {
+            throw new IllegalArgumentException("구분자 \",\"의 개수가 5개인지 확인해주세요.");
         }
     }
 }
