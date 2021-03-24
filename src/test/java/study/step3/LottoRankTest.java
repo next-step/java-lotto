2e1f8f44db59c848458e7bd4f811472ab61ad26f
Donghui-Lee <ehdgml3206@naver.com>
2021-03-25 00:47:06 +0900

    test: 보너스볼 추가에 따른 테스트 코드 추가

--- src/test/java/study/step3/LottoRankTest.java
+++ src/test/java/study/step3/LottoRankTest.java
@@ -2,6 +2,8 @@ package study.step3;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,6 +14,14 @@ public class LottoRankTest {
     @ParameterizedTest(name = "통계 일치 갯수에 따른 상금 테스트")
     @CsvSource(value = {"3:5000", "4:50000", "5:1500000", "6:2000000000"}, delimiter = ':')
     public void amountOfHit(int input, int expected) {
-        assertThat(LottoRank.of(input).getAmount()).isEqualTo(expected);
+        assertThat(LottoRank.of(input, false).getAmount()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("보너스 상금 테스트")
+    public void bonus() {
+        int hit = 5;
+        int amount = 30000000;
+        assertThat(LottoRank.of(hit, true).getAmount()).isEqualTo(amount);
     }
 }
--- src/test/java/study/step3/LottoWinTest.java
+++ src/test/java/study/step3/LottoWinTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import study.step3.domain.Amount;
 import study.step3.domain.LottoWin;
 
 public class LottoWinTest {
@@ -17,5 +18,27 @@ public class LottoWinTest {
             lottoWin.hit(Integer.parseInt(hit));
         }
         assertThat(lottoWin.sum()).isEqualTo(Integer.parseInt(expected));
+    }
+
+    @ParameterizedTest(name = "수익 테스트")
+    @CsvSource(value = {"3,4,5:1555", "3,3,3:15"}, delimiter = ':')
+    public void profit(String inputs, String expected) {
+        LottoWin lottoWin = new LottoWin();
+        String[] hits = inputs.split(",");
+        for(String hit : hits) {
+            lottoWin.hit(Integer.parseInt(hit), false);
+        }
+        assertThat(lottoWin.profit(new Amount(1000))).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "보너스 수익 테스트")
+    @CsvSource(value = {"3,4,5:30055", "3,3,5:30010"}, delimiter = ':')
+    public void profitWithBonus(String inputs, String expected) {
+        LottoWin lottoWin = new LottoWin();
+        String[] hits = inputs.split(",");
+        for(String hit : hits) {
+            lottoWin.hit(Integer.parseInt(hit), true);
+        }
+        assertThat(lottoWin.profit(new Amount(1000))).isEqualTo(expected);
     }
 }
