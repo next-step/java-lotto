ec40e2c91902479c161ea4f0ab6651eb0b436769
Donghui-Lee <ehdgml3206@naver.com>
2021-03-25 00:38:36 +0900

    refactor: ResultView bonusBall 적용

--- src/main/java/study/step3/view/ResultView.java
+++ src/main/java/study/step3/view/ResultView.java
@@ -4,12 +4,15 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import study.step3.domain.Lotto;
+import study.step3.domain.LottoNumber;
 import study.step3.domain.LottoRank;
 import study.step3.domain.LottoWin;
 
 public class ResultView {
     private final static String MESSAGE_PURCHASE_COUNT = "개를 구입하셨습니다.";
     private final static String MESSAGE_WIN_RESULT = "당첨 통계\n---------";
+    private final static String MESSAGE_BONUS_BALL = ", 보너스 일치";
+    private static final String MESSAGE_EMPTY = " ";
 
     public static void printPurchaseCount(int count) {
         System.out.println(count + MESSAGE_PURCHASE_COUNT);
@@ -25,8 +28,15 @@ public class ResultView {
     public static void printResult(LottoWin lottoWin) {
         System.out.println(MESSAGE_WIN_RESULT);
         for(LottoRank win : lottoWin.getWin().keySet()) {
-            System.out.println(win.getHit() + "개 일치 (" + win.getAmount() + "원) - " + lottoWin.getWin().get(win) + "개");
+            System.out.println(win.getHit() + "개 일치" + printBonus(win) + "(" + win.getAmount() + "원) - " + lottoWin.getWin().get(win) + "개");
         }
+    }
+
+    private static String printBonus(LottoRank lottoRank) {
+        if (LottoRank.SECOND == lottoRank) {
+            return MESSAGE_BONUS_BALL;
+        }
+        return MESSAGE_EMPTY;
     }
 
     public static void printProfit(BigDecimal profit) {
