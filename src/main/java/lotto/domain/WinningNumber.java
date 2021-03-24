11718ffff841f0afbb257a54604cfebe2bd22879
Younghyun <younghyunjo99@gmail.com>
2021-03-24 10:15:17 +0900

    refact(step3):throw invalid input

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -5,6 +5,13 @@ public class WinningNumber {
     private int bonusNumber;
 
     public WinningNumber(LottoNumber winningNumber, int bonusNumber) {
+        if (winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
+        if (bonusNumber < LottoRule.min() || bonusNumber > LottoRule.max()) {
+            throw new IllegalArgumentException();
+        }
+
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
     }
---
+++ src/test/java/LottoTest/WinningNumberTest.java
@@ -0,0 +1,23 @@
+package LottoTest;
+
+import lotto.domain.LottoNumber;
+import lotto.domain.WinningNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class WinningNumberTest {
+    @ParameterizedTest
+    @CsvSource(value = {"6", "0", "46"}, delimiter = ':')
+    @DisplayName("잘못된 보너스 번호면 Exception")
+    void Given_InvalidBonusNumber_When_New_Then_Exception() {
+        int duplicatedBonusNumber = 6;
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy((() -> new WinningNumber(new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6)), duplicatedBonusNumber)));
+    }
+}
