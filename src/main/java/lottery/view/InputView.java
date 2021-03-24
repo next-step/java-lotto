7a4b9c0ebeeeb8de2d27db62c43972c22bf84fea
Jake <cocahack@naver.com>
2021-03-24 13:20:34 +0900

    feat: 수동 로또 입력 기능 추가

--- src/main/java/lottery/view/InputView.java
+++ src/main/java/lottery/view/InputView.java
@@ -1,15 +1,39 @@
 package lottery.view;
 
+import lottery.dto.LotteryNumbersDto;
+import lottery.dto.LotteryTicketOrderDto;
 import lottery.util.ConsoleInteractiveInputUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final String BASIC_DELIMITER = ",";
 
+    public LotteryTicketOrderDto receiveLotteryTicketOrder() {
+        int money = receivePurchaseAmount();
+        int amount = receiveManualLotteryAmount();
+        List<LotteryNumbersDto> lotteryNumbersDtoList = receiveManualLotteries(amount);
+
+        return new LotteryTicketOrderDto(money, lotteryNumbersDtoList);
+    }
+
     public int receivePurchaseAmount() {
         return ConsoleInteractiveInputUtils.getInt("구입 금액을 입력해주세요.");
+    }
+
+    public int receiveManualLotteryAmount() {
+        return ConsoleInteractiveInputUtils.getInt("수동으로 구매할 로또 수를 입력해 주세요.");
+    }
+
+    private List<LotteryNumbersDto> receiveManualLotteries(int amount) {
+        String message = "수동으로 구매할 번호를 입력해 주세요.";
+
+        return ConsoleInteractiveInputUtils.getIntegerLists(message, BASIC_DELIMITER, amount)
+            .stream()
+            .map(LotteryNumbersDto::new)
+            .collect(Collectors.toList());
     }
 
     public List<Integer> receiveWinningNumbers() {
