05cea0acdeee8a2446fa18d7936c017f50fa2158
Beenie93 <seongbeen93@naver.com>
2021-03-25 00:12:38 +0900

    refactor: make lotto tickets immutable

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoTickets {
-    private final List<LottoTicket> lottoTickets = new ArrayList<>();
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = new ArrayList<>(lottoTickets);
+    }
 
     public void add(LottoTicket lottoTicket) {
         lottoTickets.add(lottoTicket);
--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -4,6 +4,8 @@ import lotto.utils.LottoRandomNumberUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketsTest {
@@ -11,7 +13,7 @@ public class LottoTicketsTest {
     @Test
     @DisplayName("다수의 로또 티켓 저장")
     public void add() throws Exception {
-        LottoTickets lottoTickets = new LottoTickets();
+        LottoTickets lottoTickets = new LottoTickets(new ArrayList<>());
         lottoTickets.add(new LottoTicket(LottoRandomNumberUtil.lottoNumbers()));
         lottoTickets.add(new LottoTicket(LottoRandomNumberUtil.lottoNumbers()));
 
