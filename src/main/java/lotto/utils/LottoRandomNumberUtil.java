7d2dd342ae132d39fb56d2eba262383c6c05a2c3
Beenie93 <seongbeen93@naver.com>
2021-03-25 00:11:25 +0900

    refactor: make lotto numbers immutable

--- src/main/java/lotto/utils/LottoRandomNumberUtil.java
+++ src/main/java/lotto/utils/LottoRandomNumberUtil.java
@@ -1,5 +1,6 @@
 package lotto.utils;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,14 +10,16 @@ public class LottoRandomNumberUtil {
     private static final int NUMBER_OF_LOTTO_NUMBER = 6;
     private static final int BOUND_MIN = 1;
     private static final int BOUND_MAX = 46;
-    private static final List<Integer> numbers = IntStream.range(BOUND_MIN, BOUND_MAX).boxed().collect(Collectors.toList());
+    private static final List<Integer> numbers = IntStream.range(BOUND_MIN, BOUND_MAX)
+            .boxed()
+            .collect(Collectors.toList());
 
     private LottoRandomNumberUtil() {
     }
 
     public static List<Integer> lottoNumbers() {
         Collections.shuffle(numbers);
-        List<Integer> lottoNumbers = numbers.subList(0, NUMBER_OF_LOTTO_NUMBER);
+        List<Integer> lottoNumbers = new ArrayList<>(numbers.subList(0, NUMBER_OF_LOTTO_NUMBER));
         Collections.sort(lottoNumbers);
         return lottoNumbers;
     }
