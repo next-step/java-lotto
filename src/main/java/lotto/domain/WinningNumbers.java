45aea92cc27e70bf51a1ec206ebb2fc805a390a0
Beenie93 <seongbeen93@naver.com>
2021-03-25 01:36:05 +0900

    refactor: replace String[] with List<String>

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,39 +1,39 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class WinningNumbers {
     private static final int NUMBER_LENGTH = 6;
     private static final int BOUND_MAX = 45;
     private static final int BOUND_MIN = 1;
-    private static final String CHECK_LENGTH = String.format("당첨 번호의 길이는 {}(이)여야 합니다.", NUMBER_LENGTH);
+    private static final String CHECK_LENGTH = String.format("당첨 번호의 길이는 %d(이)여야 합니다.", NUMBER_LENGTH);
     private static final String CHECK_INTEGER = "당첨 번호는 모두 숫자여야 합니다.";
-    private static final String CHECK_BOUND = String.format("당첨 번호는 {} ~ {} 범위의 값이어야 합니다.", BOUND_MIN, BOUND_MAX);
-    private final List<Integer> winningNumbers;
+    private static final String CHECK_BOUND = String.format("당첨 번호는 %d ~ %d 범위의 값이어야 합니다.", BOUND_MIN, BOUND_MAX);
+    private final Set<Integer> winningNumbers;
 
-    public WinningNumbers(String[] winningNumbers) {
+    public WinningNumbers(List<String> winningNumbers) {
         validate(winningNumbers);
-        this.winningNumbers = Arrays.stream(winningNumbers)
+        this.winningNumbers = winningNumbers.stream()
                 .mapToInt(Integer::parseInt)
-                .boxed().collect(Collectors.toList());
+                .boxed().collect(Collectors.toSet());
     }
 
-    private void validate(String[] winningNumbers) {
+    private void validate(List<String> winningNumbers) {
         checkLength(winningNumbers);
         checkInteger(winningNumbers);
         checkBound(winningNumbers);
     }
 
-    private void checkLength(String[] winningNumbers) {
-        if (winningNumbers.length != NUMBER_LENGTH) {
+    private void checkLength(List<String> winningNumbers) {
+        if (winningNumbers.size() != NUMBER_LENGTH) {
             throw new IllegalArgumentException(CHECK_LENGTH);
         }
     }
 
-    private void checkInteger(String[] winningNumbers) {
+    private void checkInteger(List<String> winningNumbers) {
         for (String winningNumber : winningNumbers) {
             checkInteger(winningNumber);
         }
@@ -47,7 +47,8 @@ public class WinningNumbers {
         }
     }
 
-    private void checkBound(String[] winningNumbers) {
+    private void checkBound(List<
+            String> winningNumbers) {
         for (String winningNumber : winningNumbers) {
             int number = Integer.parseInt(winningNumber);
             checkBound(number);
@@ -59,6 +60,7 @@ public class WinningNumbers {
             throw new IllegalArgumentException(CHECK_BOUND);
         }
     }
+
 
     @Override
     public boolean equals(Object o) {
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
-import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -12,7 +15,7 @@ public class WinningNumbersTest {
     @Test
     @DisplayName("로또 당첨 번호 생성")
     public void create() throws Exception {
-        String[] numbers = {"1", "2", "3", "4", "5", "6"};
+        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6");
         WinningNumbers winningNumbers = new WinningNumbers(numbers);
         assertThat(winningNumbers).isEqualTo(new WinningNumbers(numbers));
     }
@@ -20,25 +23,32 @@ public class WinningNumbersTest {
     @Test
     @DisplayName("당첨 번호 길이가 6자 이외일 경우 예외")
     public void checkLength() throws Exception {
-        String[] overSix = {"1", "2", "3", "4", "5", "6", "7"};
+        List<String> overSix = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
         assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumbers(overSix));
-        String[] lowerSix = {"1", "2", "3", "4", "5"};
+        List<String> lowerSix = Arrays.asList("1", "2", "3", "4", "5");
         assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumbers(lowerSix));
     }
 
     @Test
     @DisplayName("당청 번호의 요소 모두 정수가 아닐 경우 예외")
     public void checkInteger() throws Exception {
-        String[] numbers = {"1", "2", "3", "4", "5", "X"};
+        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "X");
         assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumbers(numbers));
     }
 
     @Test
     @DisplayName("당첨 번호의 요소 범위가 1 ~ 45가 아닐 경우 예외")
     public void checkBound() throws Exception {
-        String[] overMax = {"1", "2", "3", "4", "5", "46"};
+        List<String> overMax = Arrays.asList("1", "2", "3", "4", "5", "46");
         assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumbers(overMax));
-        String[] lowerMin = {"0", "2", "3", "4", "5", "45"};
+        List<String> lowerMin = Arrays.asList("0", "2", "3", "4", "5", "45");
         assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumbers(lowerMin));
+    }
+
+    @Test
+    @DisplayName("당첨 번호 중 중복되는 숫자가 존재할 경우")
+    public void checkDuplication() throws Exception {
+        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "5");
+        assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumbers(numbers));
     }
 }
