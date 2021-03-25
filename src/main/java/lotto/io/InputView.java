43bdd77c91552a49593cbd5a17ee623e1a85f7e4
Dion <ksundong@gmail.com>
2021-03-25 22:22:58 +0900

    refactor: 문자열 상수로 추출 및 공백 제거 후 구분자로 구분하도록 수정

--- src/main/java/lotto/io/InputView.java
+++ src/main/java/lotto/io/InputView.java
@@ -5,6 +5,12 @@ import lotto.domain.Lotto;
 
 public final class InputView {
 
+  private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+  private static final String INPUT_LAST_WEEK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해주세요.";
+  private static final String SPACE_REGEX = "\\s";
+  private static final String EMPTY_STRING = "";
+  private static final String DELIMITER = ",";
+
   private final Scanner scanner;
 
   public InputView(Scanner scanner) {
@@ -12,14 +18,15 @@ public final class InputView {
   }
 
   public int inputMoney() {
-    System.out.println("구입금액을 입력해 주세요.");
+    System.out.println(INPUT_MONEY);
     return Integer.parseInt(scanner.nextLine());
   }
 
   public Lotto inputLastWinningNumber() {
-    System.out.println("지난 주 당첨 번호를 입력해주세요.");
+    System.out.println(INPUT_LAST_WEEK_WINNING_NUMBER);
     String winningNumberString = scanner.nextLine();
-    String[] winningNumbers = winningNumberString.split(", ");
+
+    String[] winningNumbers = winningNumberString.replaceAll(SPACE_REGEX, EMPTY_STRING).split(DELIMITER);
     System.out.println();
     return new Lotto(winningNumbers);
   }
