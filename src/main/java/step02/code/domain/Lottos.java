ce5e744e8ff321acff4a03b45a675ba768ce29ae
keke0123 <keke0123@naver.com>
2021-03-25 20:19:38 +0900

    (feat) money check 기능 추가

--- src/main/java/step02/code/domain/Lottos.java
+++ src/main/java/step02/code/domain/Lottos.java
@@ -1,5 +1,30 @@
 package step02.code.domain;
 
+import java.util.List;
+
 public class Lottos {
+  private static final int LOTTO_PRICE = 1000;
   
+  private final List<Lotto> lottos;
+
+  // public Lottos(List<Lotto> lottos) {
+  //   this.lottos = lottos;
+  // }
+  public Lottos(int money) { 
+    check(money);
+    this.lottos = null;
+  }
+
+  public static void check(int money) {
+    if(money <= LOTTO_PRICE) {
+      throw new IllegalArgumentException("lotto 구입의 최소 금액보다 작습니다");
+    }
+    if(money % LOTTO_PRICE != 0) {
+      throw new IllegalArgumentException("잔돈이 안남는 금액이 필요합니다");
+    }
+  }
+
+
 }
+
+// 1049913
--- src/test/java/step02/code/domain/LottosTest.java
+++ src/test/java/step02/code/domain/LottosTest.java
@@ -1,5 +1,23 @@
 package step02.code.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LottosTest {
-  
+
+  @Test
+  @DisplayName("lotto 구입금액이 올바르지 않을 경우 예외처리 테스트")
+  public void check() {
+    assertThatThrownBy(() -> {
+      // 로또 최소 구매 금액 이하인경우
+      Lottos.check(500);
+    }).isInstanceOf(IllegalArgumentException.class);
+    assertThatThrownBy(() -> {
+      // 로또 구매후 잔액이 남는 경우
+      Lottos.check(1500);
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
 }
