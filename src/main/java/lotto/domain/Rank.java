d55c96a14e0b54108a5fa99f43dbe67f1a5859c7
ad7549 <ad7549@gmail.com>
2021-03-25 17:37:08 +0900

    feat: Implement Rank

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,9 +1,29 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST;
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    MISS;
 
     public static Rank valueOf(int countOfMatch, boolean matchBonus) {
-        return null;
+        if (countOfMatch == 6) {
+            return FIRST;
+        }
+        if (countOfMatch == 5) {
+            if (matchBonus) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+        if (countOfMatch == 4) {
+            return FOURTH;
+        }
+        if (countOfMatch == 3) {
+            return FIFTH;
+        }
+        return MISS;
     }
 }
