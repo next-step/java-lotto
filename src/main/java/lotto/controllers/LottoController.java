743415d6148979abf43f0ddfeb517e929d537425
ad7549 <ad7549@gmail.com>
2021-03-26 04:44:46 +0900

    feat: Add bonus number input
    InputView에 보너스 번호를 입력받는 기능을 추가하고, controller에도 추가

--- src/main/java/lotto/controllers/LottoController.java
+++ src/main/java/lotto/controllers/LottoController.java
@@ -14,13 +14,16 @@ import java.util.List;
 public class LottoController {
     public void run() {
         final int payment = InputView.payment();
+
         final List<LottoTicket> lottoTickets = new LottoStore().lottoTickets(payment);
 
         ResultView.print(lottoTickets);
 
         final String winnerNumbersInput = InputView.winnerNumbers();
+        final String bonusNumberInput = InputView.bonusNumber();
+
         final LottoDiscriminator lottoDiscriminator = LottoDiscriminatorFactory.from(
-                StringUtil.splitCommas(winnerNumbersInput), "");
+                StringUtil.splitCommas(winnerNumbersInput), bonusNumberInput);
         final LottoStatistics lottoStatistics = new LottoStatistics(lottoDiscriminator, lottoTickets);
 
         ResultView.print(lottoStatistics, payment);
