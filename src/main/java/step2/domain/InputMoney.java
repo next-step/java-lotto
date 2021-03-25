06c32d0eedd8f417ca53ca03e849246c9c31ccee
김우재 <kwj1270@naver.com>
2021-03-25 20:21:56 +0900

    feat:InputMoney 인스턴스 생성 기능 구현

--- src/main/java/step2/domain/InputMoney.java
+++ src/main/java/step2/domain/InputMoney.java
@@ -1,4 +1,15 @@
 package step2.domain;
 
 public class InputMoney {
+
+    private final Integer inputMoney;
+
+    private InputMoney(int inputMoney) {
+        this.inputMoney = Integer.valueOf(inputMoney);
+    }
+
+    public static InputMoney valueOf(int inputValue) {
+        return new InputMoney(inputValue);
+    }
+
 }
