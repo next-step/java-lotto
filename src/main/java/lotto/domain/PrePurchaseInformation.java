7a3e91a47a081743bd73eb7d229081eb68d21a38
weleek <weleek@gmail.com>
2021-03-25 16:00:19 +0900

    feat: 수동 구매 입력 번호에 대한 유효성 검증 추가

--- src/main/java/lotto/domain/PrePurchaseInformation.java
+++ src/main/java/lotto/domain/PrePurchaseInformation.java
@@ -2,19 +2,22 @@ package lotto.domain;
 
 public class PrePurchaseInformation {
     private final Amount amount;
-    private final String values;
+    private final String[] values;
 
     public PrePurchaseInformation(Amount amount, String values) {
         this.amount = amount;
-        this.values = values;
+        this.values = values.split("\n");
+        validation();
     }
 
-    public String value() {
-        return values;
+    public void validation() {
+        if (amount.manualQuantity() != values.length) {
+            throw new IllegalArgumentException("수동 구매 번호를 수동 구매 입력 장수에 맞게 입력해 주세요.");
+        }
     }
 
     public String[] values() {
-        return values.split("\n");
+        return values;
     }
 
     public int autoQuantity() {
