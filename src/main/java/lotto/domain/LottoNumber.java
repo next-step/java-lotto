86161a5af99a8c0eafd1679e6d83f38801d44445
ad7549 <ad7549@gmail.com>
2021-03-26 02:59:28 +0900

    fix: Fix LottoNumber of method error

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,12 +2,18 @@ package lotto.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class LottoNumber implements Comparable<LottoNumber> {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
 
     private static final Map<Integer, LottoNumber> lottoNumbersCache = new HashMap<>();
+
+    static {
+        IntStream.rangeClosed(LottoNumber.MIN_LOTTO_NUMBER, LottoNumber.MAX_LOTTO_NUMBER)
+                .forEach(i -> lottoNumbersCache.put(i, new LottoNumber(i)));
+    }
 
     private final int number;
 
@@ -17,7 +23,7 @@ public class LottoNumber implements Comparable<LottoNumber> {
 
     public static LottoNumber of(final int number) {
         validate(number);
-        return lottoNumbersCache.putIfAbsent(number, new LottoNumber(number));
+        return lottoNumbersCache.get(number);
     }
 
     private static void validate(final int number) {
