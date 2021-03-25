4844f9d07620a5587c2d502a1a0df8864363552a
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-03-25 22:19:38 +0900

    refactor : 생성자 정리, getCount 매서드 추가

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -7,16 +7,17 @@ public class LottoTicket {
     private final List<Lotto> lottoList;
 
     public LottoTicket(int count) {
+        this.lottoList = generateLottoList(count);
+    }
+
+    private List<Lotto> generateLottoList(int count) {
         List<Lotto> lottoList = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             lottoList.add(new Lotto(generateLottoNumbers()));
         }
-        this.lottoList = lottoList;
+        return lottoList;
     }
 
-    public LottoTicket(List<Lotto> lottoList) {
-        this.lottoList = lottoList;
-    }
 
     private int[] generateLottoNumbers() {
         Random random = new Random();
@@ -25,5 +26,13 @@ public class LottoTicket {
             lottoNumberSet.add(random.nextInt(LottoConstant.MAX_LOTTO_NUMBER-1) + LottoConstant.MIN_LOTTO_NUMBER);
         }
         return lottoNumberSet.stream().mapToInt(a->a).toArray();
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+
+    public int getCount() {
+        return this.lottoList.size();
     }
 }
