d39b373467416a74f23aae614528e5b4a0bcbee9
김우재 <kwj1270@naver.com>
2021-03-25 23:33:32 +0900

    test:LottoNumber 인스턴스에 음수 주입시 예외처리 발생 여부 테스트

--- src/test/java/step2/domain/Lotto/LottoNumberTest.java
+++ src/test/java/step2/domain/Lotto/LottoNumberTest.java
@@ -2,8 +2,10 @@ package step2.domain.Lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.exception.InvalidNumberInputException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberTest {
@@ -19,5 +21,19 @@ class LottoNumberTest {
 
         // then
         assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("LottoNumber 인스턴스에 음수 주입시 예외처리 발생 여부 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int inputValue = -1;
+
+        // when and then
+        assertThatThrownBy(() -> {
+            LottoNumber.valueOf(inputValue);
+        }).isInstanceOf(InvalidNumberInputException.class)
+                .hasMessageContaining("범위를 벗어난 값이 입력되었습니다.");
+
     }
 }
