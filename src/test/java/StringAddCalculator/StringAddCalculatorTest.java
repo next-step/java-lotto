ea9f784cfc1f5c54374670e400e5ee7b5c683748
JeongByeongHun <jbh1024@gmail.com>
2021-03-25 22:14:22 +0900

    docs : project initialize & write README.MD

---
+++ src/test/java/StringAddCalculator/StringAddCalculatorTest.java
@@ -0,0 +1,15 @@
+package StringAddCalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class StringAddCalculatorTest {
+
+    private StringAddCalculator stringAddCalculator;
+
+    @BeforeEach
+    void initializeTest(){
+
+    }
+
+
+}
