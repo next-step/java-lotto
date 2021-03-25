d129b699d048c7d8e9bd48a604718998e4470fc7
김우재 <kwj1270@naver.com>
2021-03-25 20:24:58 +0900

    test:InputMoney 인스턴스 음수 주입시 예외처리 여부 테스트

--- src/test/java/step2/domain/InputMoneyTest.java
+++ src/test/java/step2/domain/InputMoneyTest.java
@@ -2,21 +2,37 @@ package step2.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.exception.InvalidNumberInputException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputMoneyTest {
 
-    @DisplayName("InputMoney 인스턴스 생성 여부")
+    @DisplayName("InputMoney 인스턴스 생성 여부 테스트")
     @Test
     void 생성() {
         // given
         int inputValue = 15000;
 
-        // then
+        // when
         InputMoney inputMoney = InputMoney.valueOf(inputValue);
 
         // then
         assertThat(inputMoney).isNotNull();
+    }
+
+    @DisplayName("InputMoney 인스턴스 음수 주입시 예외처리 여부 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int inputValue = -1;
+
+        // when and then
+        assertThatThrownBy(() -> {
+            InputMoney.valueOf(inputValue);
+        }).isInstanceOf(InvalidNumberInputException.class)
+                .hasMessageContaining("범위를 벗어난 값이 입력되었습니다.");
+
     }
 }
