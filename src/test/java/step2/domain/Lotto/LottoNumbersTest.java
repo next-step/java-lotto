f90b00d49a7b1b1180f69d31fc5651b84f56e8c0
김우재 <kwj1270@naver.com>
2021-03-26 01:08:14 +0900

    test:LottoNumbers 인스턴스 생성 여부 테스트 작성

---
+++ src/test/java/step2/domain/Lotto/LottoNumbersTest.java
@@ -0,0 +1,36 @@
+package step2.domain.Lotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoNumbersTest {
+
+    private List<LottoNumber> lottoNumberList;
+
+    @BeforeEach
+    void setUp(){
+        lottoNumberList =  IntStream.range(1, 7)
+                    .mapToObj(LottoNumber::valueOf)
+                    .collect(Collectors.toList());
+    }
+
+    @DisplayName("LottoNumbers 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+
+        // when
+        LottoNumber lottoNumber = new LottoNumber(lottoNumberList);
+
+        // then
+        assertThat(lottoNumber).isNotNull();
+    }
+}
