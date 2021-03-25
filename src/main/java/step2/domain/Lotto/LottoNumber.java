5096e9e6bb3cdd07ded7977e8342b549e3306df5
김우재 <kwj1270@naver.com>
2021-03-26 00:50:46 +0900

    feat:LottoNumber 인스턴스가 같은 소유한 값을 기준으로 같은 주소값을 가지는 기능 구현

--- src/main/java/step2/domain/Lotto/LottoNumber.java
+++ src/main/java/step2/domain/Lotto/LottoNumber.java
@@ -6,27 +6,50 @@ import java.util.Objects;
 
 public final class LottoNumber {
 
-    private static final int ZERO = 0;
+    private static final LottoNumber[] cache;
+
+    private static final int MINIMUM = 1;
+    private static final int MAXIMUM = 45;
+    private static final int ARRAY_LENGTH = MAXIMUM + 1;
 
     private final Integer lottoNumber;
 
-    private LottoNumber(int lottoNumber){
+    static {
+        cache = new LottoNumber[ARRAY_LENGTH];
+    }
+
+    private LottoNumber(int lottoNumber) {
         this(Integer.valueOf(lottoNumber));
     }
 
-    private LottoNumber(Integer lottoNumber){
-        if(isOutOfBounds(lottoNumber)) {
-            throw new InvalidNumberInputException();
-        }
+    private LottoNumber(Integer lottoNumber) {
         this.lottoNumber = lottoNumber;
     }
 
     public static final LottoNumber valueOf(int lottoNumber) {
-        return new LottoNumber(lottoNumber);
+        if (isOutOfBounds(lottoNumber)) {
+            throw new InvalidNumberInputException();
+        }
+        if (isCacheNull(lottoNumber)) {
+            cache[lottoNumber] = new LottoNumber(lottoNumber);
+        }
+        return cache[lottoNumber];
     }
 
-    private final boolean isOutOfBounds(Integer inputMoney) {
-        return inputMoney < ZERO;
+    private static final boolean isCacheNull(Integer index) {
+        return cache[index] == null;
+    }
+
+    private static final boolean isOutOfBounds(Integer lottoNumber) {
+        return (isLessThanMinimum(lottoNumber) || isExceedMaximum(lottoNumber));
+    }
+
+    private static final boolean isLessThanMinimum(Integer lottoNumber) {
+        return lottoNumber < MINIMUM;
+    }
+
+    private static final boolean isExceedMaximum(Integer lottoNumber) {
+        return lottoNumber > MAXIMUM;
     }
 
     @Override
