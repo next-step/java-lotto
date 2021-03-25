9f70eee29ecc818738a4249aa7615359ec03a59e
623nana <6230nana@gmail.com>
2021-03-25 22:52:06 +0900

    test: 로또 당첨 번호 count 로직 클래스 변경에 따른 테스트 수정

--- src/test/java/lotto/HitTest.java
+++ src/test/java/lotto/HitTest.java
@@ -5,26 +5,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import lotto.domain.Hit;
 import lotto.domain.HitNumbers;
-import lotto.domain.Lottery;
-import lotto.domain.LottoNumbers;
 import lotto.domain.LottoNumber;
+import lotto.domain.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LotteryTest {
+public class HitTest {
 
   @Test
-  @DisplayName("[Lottery] 당첨 번호 갯수 count 테스트")
+  @DisplayName("[Hit] 당첨 번호 갯수 count 테스트")
   void countHitTest() {
     List<LottoNumber> numbers = IntStream.rangeClosed(1, 6)
         .mapToObj(LottoNumber::new)
         .collect(Collectors.toList());
     LottoNumbers lottoNumbers = new LottoNumbers(numbers);
     HitNumbers hitNumbers = new HitNumbers("1, 2, 3, 4, 5, 7", 10);
-    Lottery lottery = new Lottery(hitNumbers);
 
-    int hitCount = lottery.countHit(lottoNumbers);
+    int hitCount = Hit.countHit(hitNumbers, lottoNumbers);
 
     assertThat(hitCount).isEqualTo(5);
   }
