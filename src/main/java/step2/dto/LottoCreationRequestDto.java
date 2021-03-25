91c37fbc0df4780020f79cba65da4c6bba9b7516
김우재 <kwj1270@naver.com>
2021-03-25 20:16:56 +0900

    feat:LottoCreationRequestDto 인스턴스 생성 기능 구현

--- src/main/java/step2/dto/LottoCreationRequestDto.java
+++ src/main/java/step2/dto/LottoCreationRequestDto.java
@@ -1,4 +1,15 @@
 package step2.dto;
 
 public class LottoCreationRequestDto {
+
+    private final int inputValue;
+
+    private LottoCreationRequestDto(int inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public static LottoCreationRequestDto newInstance(int inputValue) {
+        return new LottoCreationRequestDto(inputValue);
+    }
+
 }
