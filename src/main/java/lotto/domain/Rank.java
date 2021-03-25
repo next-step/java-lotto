4bf9b3bdaa0c038448a6e86fbf97b06e452e8a0f
Dion <ksundong@gmail.com>
2021-03-26 01:53:16 +0900

    feat: 통계 출력시 사용할 Rank 클래스 선언

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+  NONE(Integer.MIN_VALUE, null, null),
+  THREE(3, new Money(5000), "3개 일치 (5000원)"),
+  FOUR(4, new Money(50_000), "4개 일치 (50000원)"),
+  FIVE(5, new Money(1_500_000), "5개 일치 (1500000원)"),
+  SIX(6, new Money(2_000_000_000), "6개 일치 (2000000000원)");
+
+  int matchCount;
+  Money money;
+  String message;
+
+  Rank(int matchCount, Money money, String message) {
+    this.matchCount = matchCount;
+    this.money = money;
+    this.message = message;
+  }
+
+  public static Rank of(final int matchCount) {
+    return Arrays.stream(Rank.values())
+        .filter(rank -> rank.matchCount == matchCount)
+        .findAny()
+        .orElse(Rank.NONE);
+  }
+}
