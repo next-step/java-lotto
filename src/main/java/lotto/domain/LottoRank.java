05d965c29c37f44f01cd9ba723e7bdcca3190db1
ad7549 <ad7549@gmail.com>
2021-03-26 01:45:56 +0900

    feat: Implement LottoRank.winningPrize()

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,15 +1,21 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    MISS;
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    MISS(0);
+
+    private final int winningPrize;
+
+    LottoRank(int winningPrize) {
+        this.winningPrize = winningPrize;
+    }
 
     public int winningPrize() {
-        return 0;
+        return winningPrize;
     }
 
     public static LottoRank valueOf(int countOfMatch, boolean matchBonus) {
