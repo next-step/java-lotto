484121e46185f25f89ec2354c7e5ff7898c8c3c1
gummybearr <ehrudghl123@naver.com>
2021-03-25 21:07:43 +0900

    refact(step4): extract regex string scan to method

--- src/main/java/step2/View/InputView.java
+++ src/main/java/step2/View/InputView.java
@@ -20,7 +20,7 @@ public class InputView {
 
     public String lastWeekWinningLottoNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요");
-        return scanner.next("[-0-9, ]+");
+        return regexString();
     }
 
     public int lastWeekBonusLottoNumber() {
@@ -32,9 +32,13 @@ public class InputView {
         List<String> manualLottoList = new ArrayList<>();
         System.out.println("수동으로 구매할 번호를 입력해 주세요");
         for (int idx = 0; idx < manualLottoCount; idx++) {
-            String manualLottoNumbers = scanner.next("[-0-9, ]+");
+            String manualLottoNumbers = regexString();
             manualLottoList.add(manualLottoNumbers);
         }
         return manualLottoList;
+    }
+
+    private String regexString(){
+        return scanner.next("[-0-9, ]+");
     }
 }
