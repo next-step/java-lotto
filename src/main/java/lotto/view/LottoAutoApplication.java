6a7967f2f5d9baa12931d410e5d7e712141419da
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-03-26 13:48:06 +0900

    feat : UI 테스트로직 제거, 컨트롤러 연결

--- src/main/java/lotto/view/LottoAutoApplication.java
+++ src/main/java/lotto/view/LottoAutoApplication.java
@@ -2,10 +2,6 @@ package lotto.view;
 
 import lotto.controller.LottoController;
 import lotto.controller.dto.LottoAutoPurchaseResponse;
-import lotto.controller.dto.WinInquiryResponse;
-import lotto.controller.dto.WinStatistic;
-
-import java.util.ArrayList;
 
 public class LottoAutoApplication {
     public static void main(String[] args) {
@@ -15,15 +11,7 @@ public class LottoAutoApplication {
 
         LottoAutoPurchaseResponse lottoAutoPurchaseResponse = lottoController.purchaseLottoAutoTicket(inputView.inputPurchaseAmount());
         resultView.printPurchaseList(lottoAutoPurchaseResponse);
-
         System.out.println();
-
-        // TODO: Controller 호출해 당첨결과 조회
-        inputView.inputWinNumber(lottoAutoPurchaseResponse.getLottoNumberList());
-        WinStatistic winStatistic = new WinStatistic(3, 5000, 1);
-        ArrayList<WinStatistic> winStatisticList = new ArrayList<>();
-        winStatisticList.add(winStatistic);
-        WinInquiryResponse winInquiryResponse = new WinInquiryResponse(0.35, winStatisticList);
-        resultView.printLottoWinStatistic(winInquiryResponse);
+        resultView.printLottoWinStatistic(lottoController.inquiryWin(inputView.inputWinNumber(lottoAutoPurchaseResponse.getLottoNumberList())));
     }
 }
