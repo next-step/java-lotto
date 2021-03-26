493eb34da4bf6a18961b8832b05d43af72d98e4d
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-03-26 15:43:49 +0900

    test : LottoRank enum 조회 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
@@ -51,5 +52,24 @@ class LottoTest {
         for (Lotto lotto : lottoTicket.getLottoList()) {
             assertThat(6).isEqualTo(Arrays.stream(lotto.getLottoNumbers()).distinct().toArray().length);
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "6:2000000000", "5:1500000", "4:50000", "3:5000"}, delimiter = ':')
+    @DisplayName("숫자 matchCount 별 LottoRank Enum 조회")
+    void lottoRank_getEnum(int matchCount, long winAmount) {
+        // given
+        LottoRank lottoRank = LottoRank.getEnum(matchCount);
+
+        // when then
+        assertThat(matchCount).isEqualTo(lottoRank.getMatchCount());
+        assertThat(winAmount).isEqualTo(lottoRank.getWinAmount());
+    }
+
+    @Test
+    @DisplayName("로또 등수 매치카운트에 해당하지 않는 등수정보는 조회불가")
+    void lottoRank_getEnum_IllegalParam() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LottoRank.getEnum(9));
     }
 }
