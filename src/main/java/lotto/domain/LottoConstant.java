9625e584d3710ba9ccd667da1df715b1f8461b24
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-03-26 09:38:33 +0900

    refactor : 로또 가격변수 상수클래스로 이동  - 수익률 계산을 위해 LottoRanks 에서도 가격정보를 사용

--- src/main/java/lotto/domain/LottoConstant.java
+++ src/main/java/lotto/domain/LottoConstant.java
@@ -4,4 +4,5 @@ public class LottoConstant {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int MAX_LOTTO_COUNT = 6;
+    public static final int LOTTO_PRICE = 1000;
 }
--- src/main/java/lotto/service/LottoAutoService.java
+++ src/main/java/lotto/service/LottoAutoService.java
@@ -1,19 +1,18 @@
 package lotto.service;
 
+import lotto.domain.LottoConstant;
 import lotto.domain.LottoTicket;
 
 public class LottoAutoService {
-
-    private static final int LOTTO_PRICE = 1000;
 
     public LottoTicket purchaseLottoTicket(int purchaseAmount) {
         return new LottoTicket(calculateMaxPurchasableCount(purchaseAmount));
     }
 
     private int calculateMaxPurchasableCount(int purchaseAmount) {
-        if(purchaseAmount < LOTTO_PRICE) {
+        if(purchaseAmount < LottoConstant.LOTTO_PRICE) {
             throw new IllegalArgumentException("최소 1000원 이상의 금액을 입력해 주세요.");
         }
-        return purchaseAmount / LOTTO_PRICE;
+        return purchaseAmount / LottoConstant.LOTTO_PRICE;
     }
 }
