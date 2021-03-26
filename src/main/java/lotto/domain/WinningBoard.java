bd2098587fc70e08e4abf31d78ab6cbe74bedb86
dailyworker <workingssu@gmail.com>
2021-03-26 19:14:54 +0900

    refactor : MatchLotto 적용 리팩토링 - 필요없는 상수 제거 - initPrizeResultFromMatchLotto() MatchLotto에 위임 처리 - getTotal() MatchLotto에 위임처리

--- src/main/java/lotto/domain/WinningBoard.java
+++ src/main/java/lotto/domain/WinningBoard.java
@@ -9,19 +9,10 @@ import java.util.Map.Entry;
 public class WinningBoard {
 
   private static final int INIT_VALUE = 0;
-  private static final int FOURTH_CRITERIA = 3;
-  private static final int THIRD_CRITERIA = 4;
-  private static final int SECOND_CRITERIA = 5;
-  private static final int FIRST_CRITERIA = 6;
-  private static final int FOURTH_PRIZE_MONEY = 5000;
-  private static final int THIRD_PRIZE_MONEY = 50000;
-  private static final int SECOND_PRIZE_MONEY = 1500000;
-  private static final int FIRST_PRIZE_MONEY = 2000000000;
-
-  private static final String FOURTH_WINNER_KEY = "fourth";
-  private static final String THIRD_WINNER_KEY = "third";
-  private static final String SECOND_WINNER_KEY = "second";
-  private static final String FIRST_WINNER_KEY = "first";
+  private static final String FOURTH_WINNER_KEY = "FOURTH";
+  private static final String THIRD_WINNER_KEY = "THIRD";
+  private static final String SECOND_WINNER_KEY = "SECOND";
+  private static final String FIRST_WINNER_KEY = "FIRST";
 
   private final Map<String, Integer> winningBoard;
   private final Money money;
@@ -40,77 +31,49 @@ public class WinningBoard {
     Map<String, Integer> resultMap = new HashMap<>();
 
     for (int result : lottoMatchResult) {
-      initPrizeResultFromMatchResult(resultMap, result);
+      initPrizeResultFromMatchLotto(resultMap, result);
     }
 
     return new WinningBoard(resultMap, money);
   }
 
-  private static void initPrizeResultFromMatchResult(Map<String, Integer> resultMap, int result) {
-    if (result == FOURTH_CRITERIA)
-      putLottoMatchResult(resultMap, FOURTH_WINNER_KEY);
-
-    if (result == THIRD_CRITERIA)
-      putLottoMatchResult(resultMap, THIRD_WINNER_KEY);
-
-    if (result == SECOND_CRITERIA)
-      putLottoMatchResult(resultMap, SECOND_WINNER_KEY);
-
-    if (result == FIRST_CRITERIA)
-      putLottoMatchResult(resultMap, FIRST_WINNER_KEY);
+  private static void initPrizeResultFromMatchLotto(Map<String, Integer> resultMap, int result) {
+    MatchLotto matchLotto = MatchLotto.valueOf(result);
+    if(!MatchLotto.isNone(matchLotto)) {
+      putLottoMatchResult(resultMap, matchLotto);
+    }
   }
 
-  private static void putLottoMatchResult(Map<String, Integer> earningBoard, String key) {
-    earningBoard.put(key, earningBoard.getOrDefault(key, 0) + 1);
+  private static void putLottoMatchResult(Map<String, Integer> earningBoard, MatchLotto key) {
+    earningBoard.put(key.getWinnerRank(), earningBoard.getOrDefault(key.getWinnerRank(), 0) + 1);
   }
 
   public String calculateEarningRate() {
     Map<String, Integer> winningBoard = getWinningBoard();
 
-    int total = getTotal(winningBoard);
-    int test = this.money.toInt();
+    int total = getTotalEarning(winningBoard);
+    int boughtMoney = this.money.toInt();
 
-    double earning = (total / (double)test);
+    double earning = (total / (double)boughtMoney);
     return String.valueOf(String.format("%.1f", earning));
   }
 
-  private int getTotal(Map<String, Integer> map) {
+  private int getTotalEarning(Map<String, Integer> map) {
     Iterator<Entry<String, Integer>> entries = map.entrySet().iterator();
 
     int total = INIT_VALUE;
 
     while (entries.hasNext()) {
       Map.Entry<String, Integer> entry = entries.next();
-      total = getTotal(entry);
+      total += getEarning(entry);
     }
 
     return total;
   }
 
-  private int getTotal(Entry<String, Integer> entry) {
-    int total = INIT_VALUE;
-
-    if(isEquals(entry, FOURTH_WINNER_KEY))
-      total += calculateTotalPrizeMoney(entry.getValue(), FOURTH_PRIZE_MONEY);
-
-    if(isEquals(entry, THIRD_WINNER_KEY))
-      total += calculateTotalPrizeMoney(entry.getValue(), THIRD_PRIZE_MONEY);
-
-    if(isEquals(entry, SECOND_WINNER_KEY))
-      total += calculateTotalPrizeMoney(entry.getValue(), SECOND_PRIZE_MONEY);
-
-    if(isEquals(entry, FIRST_WINNER_KEY))
-      total += calculateTotalPrizeMoney(entry.getValue(), FIRST_PRIZE_MONEY);
-
-    return total;
-  }
-
-  private int calculateTotalPrizeMoney(int value, int prizeMoney) {
-    return value * prizeMoney;
-  }
-
-  private boolean isEquals(Entry<String, Integer> entry, String fourthWinnerKey) {
-    return entry.getKey().equals(fourthWinnerKey);
+  private int getEarning(Entry<String, Integer> entry) {
+    MatchLotto prize = MatchLotto.findPrize(entry.getKey());
+    return entry.getValue() * prize.getWinnerMoney();
   }
 
   public String toResultString() {
