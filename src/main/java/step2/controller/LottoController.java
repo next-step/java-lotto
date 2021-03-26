1e104f4914dc85d86eb2010d7c430c1edb418a51
김우재 <kwj1270@naver.com>
2021-03-26 22:09:27 +0900

    test:LottoController 인스턴스의 로또 생성 로직과 LottoExpressionResponseDto 반환 테스트 작성

--- src/main/java/step2/controller/LottoController.java
+++ src/main/java/step2/controller/LottoController.java
@@ -9,5 +9,4 @@ public class LottoController {
         return new LottoController();
     }
 
-
 }
--- src/test/java/step2/controller/LottoControllerTest.java
+++ src/test/java/step2/controller/LottoControllerTest.java
@@ -2,6 +2,7 @@ package step2.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.dto.LottoExpressionResponseDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,17 @@ class LottoControllerTest {
 
     }
 
+    @DisplayName("LottoController 인스턴스의 로또 생성 로직과 LottoExpressionResponseDto 반환 테스트")
+    @Test
+    void 로또생성_및_반환() {
 
+        // given
+        LottoController lottoController = LottoController.newInstance();
+
+        // when
+        LottoExpressionResponseDto actual = lottoController.generateLottoList();
+
+        // then
+        assertThat(actual).isNotNull();
+    }
 }
