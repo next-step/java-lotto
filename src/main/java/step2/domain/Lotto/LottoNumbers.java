5fffda3d9679bce2a6fce9a5a384542baa2bc022
김우재 <kwj1270@naver.com>
2021-03-26 16:47:28 +0900

    refactor:LottoNumbers 불필요한 로직 제거

--- src/main/java/step2/domain/Lotto/LottoNumbers.java
+++ src/main/java/step2/domain/Lotto/LottoNumbers.java
@@ -4,15 +4,9 @@ import step2.exception.ListNullPointerException;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public final class LottoNumbers {
     private final List<LottoNumber> lottoNumbers;
-
-    private LottoNumbers(int start, int end) {
-        this(generateLottoNumbers(start, end));
-    }
 
     private LottoNumbers(List<LottoNumber> lottoNumbers) {
         if (isListNull(lottoNumbers)) {
@@ -21,19 +15,8 @@ public final class LottoNumbers {
         this.lottoNumbers = lottoNumbers;
     }
 
-    private static final List<LottoNumber> generateLottoNumbers(int start, int end) {
-        return IntStream
-                .range(start, end)
-                .mapToObj(LottoNumber::valueOf)
-                .collect(Collectors.toList());
-    }
-
     public final static LottoNumbers newInstance(List<LottoNumber> lottoNumbers) {
         return new LottoNumbers(lottoNumbers);
-    }
-
-    public final static LottoNumbers newInstance(int start, int end) {
-        return new LottoNumbers(start, end);
     }
 
     private final boolean isListNull(List<LottoNumber> lottoNumbers) {
