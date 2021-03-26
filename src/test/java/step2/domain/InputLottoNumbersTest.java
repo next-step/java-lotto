4a32038935b0afc88cae16197b05a06fc9b6d888
김우재 <kwj1270@naver.com>
2021-03-27 04:54:47 +0900

    test:InputLottoNumbers 인스턴스를 문자열로 생성 여부 테스트 작성

--- src/test/java/step2/domain/InputLottoNumbersTest.java
+++ src/test/java/step2/domain/InputLottoNumbersTest.java
@@ -21,7 +21,7 @@ class InputLottoNumbersTest {
     @BeforeEach
     void setUp() {
         testLottoNumbers = IntStream
-                .range(1, 6)
+                .range(1, 7)
                 .mapToObj(LottoNumber::valueOf)
                 .collect(Collectors.toList());
     }
@@ -32,6 +32,17 @@ class InputLottoNumbersTest {
 
         // when
         InputLottoNumbers inputLottoNumbers = InputLottoNumbers.newInstance(testLottoNumbers);
+
+        // then
+        assertThat(inputLottoNumbers).isNotNull();
+    }
+
+    @DisplayName("InputLottoNumbers 인스턴스를 문자열로 생성 여부 테스트")
+    @Test
+    void 생성_문자열() {
+
+        // when
+        InputLottoNumbers inputLottoNumbers = InputLottoNumbers.newInstance("1, 2, 3, 4, 5, 6");
 
         // then
         assertThat(inputLottoNumbers).isNotNull();
