9aeec8556d40ae511ab0ea066230e1f2f16241d4
Sooncheol <ryxhdrnrmf@gmail.com>
2021-03-26 23:00:43 +0900

    feat: LottoStore 구현

---
+++ src/main/java/lotto/step2/domain/LottoStore.java
@@ -0,0 +1,37 @@
+package lotto.step2.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoStore {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    private static List<LottoNumber> allLottoNumbers = new ArrayList<>();
+
+    static {
+        IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+                .forEach(number -> allLottoNumbers.add(new LottoNumber(number)));
+    }
+
+    private static Lotto createLotto() {
+        Collections.shuffle(allLottoNumbers);
+        Lotto lotto = new Lotto(allLottoNumbers.stream()
+                .limit(LOTTO_SIZE)
+                .collect(Collectors.toList()));
+        return lotto;
+    }
+
+    public static Lottos purchase(Money money) {
+        Lottos lottos = new Lottos();
+        long lottoCount = money.LottoCount();
+        for(int i = 0; i < lottoCount; i++) {
+            lottos.addLotto(createLotto());
+        }
+        return lottos;
+    }
+}
