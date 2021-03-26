4e95de16ca492c02a1a52bbfad40528742153f64
juniq <juniqlim@gmail.com>
2021-03-26 23:53:36 +0900

    feat: 로또 구입금액 입력 - 로또 한장 발급

--- src/main/java/im/juniq/lotto/LotteryMachine.java
+++ src/main/java/im/juniq/lotto/LotteryMachine.java
@@ -1,7 +1,6 @@
 package im.juniq.lotto;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LotteryMachine {
@@ -15,7 +14,14 @@ public class LotteryMachine {
 		this.shuffleStrategy = shuffleStrategy;
 	}
 
-	public List<Lotto> lottos(int price) {
-		return Collections.singletonList(new Lotto(shuffleStrategy));
+	public List<Lotto> lottoes(int price) {
+		if (price % 1000 != 0) {
+			throw new RuntimeException("금액은 천원 단위로 입력해주세요.");
+		}
+		List<Lotto> lottoes = new ArrayList<>();
+		for (int i = 0; i < price / 1000; i++) {
+			lottoes.add(new Lotto(shuffleStrategy));
+		}
+		return lottoes;
 	}
 }
