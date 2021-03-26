f1641e9da734f05384f725411cdad3594ac91660
jess <jess.l@kakaocorp.com>
2021-03-26 21:14:04 +0900

    feat(InputView): 구매금액 입력 요청 메시지 출력 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -3,11 +3,14 @@ package lotto;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String REQUEST_PURCHASE_MESSAGE = "구매금액을 입력해 주세요.";
+
     private InputView() {
 
     }
 
     public static String getLottoPurchaseAmount() {
+        System.out.println(REQUEST_PURCHASE_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNext()) {
             return scanner.nextLine();
