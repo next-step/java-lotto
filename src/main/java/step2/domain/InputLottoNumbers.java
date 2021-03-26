f2bd19aa60339d1fe2cbdd210513236b2e1b5243
김우재 <kwj1270@naver.com>
2021-03-27 05:12:01 +0900

    feat:InputLottoNumbers 인스턴스를 문자열로 생성 기능 구현

--- src/main/java/step2/domain/InputLottoNumbers.java
+++ src/main/java/step2/domain/InputLottoNumbers.java
@@ -4,20 +4,38 @@ import step2.domain.Lotto.LottoNumber;
 import step2.exception.ListNullPointerException;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class InputLottoNumbers {
-
-    private final List<LottoNumber> InputLottoNumbers;
+    private static final String COMMA = ",";
+    private final List<LottoNumber> inputLottoNumbers;
 
     private InputLottoNumbers(List<LottoNumber> inputLottoNumbers) {
         if (isListNull(inputLottoNumbers)) {
             throw new ListNullPointerException();
         }
-        this.InputLottoNumbers = inputLottoNumbers;
+        this.inputLottoNumbers = inputLottoNumbers;
+    }
+
+    public InputLottoNumbers(String sentence) {
+        this(toLottoNumberList(sentence));
+    }
+
+    private static List<LottoNumber> toLottoNumberList(String sentence) {
+        return Stream.of(sentence.split(COMMA))
+                .map(String::trim)
+                .map(Integer::valueOf)
+                .map(LottoNumber::valueOf)
+                .collect(Collectors.toList());
     }
 
     public static final InputLottoNumbers newInstance(List<LottoNumber> InputLottoNumbers) {
         return new InputLottoNumbers(InputLottoNumbers);
+    }
+
+    public static final InputLottoNumbers newInstance(String sentence) {
+        return new InputLottoNumbers(sentence);
     }
 
     private final boolean isListNull(List<LottoNumber> inputLottoNumbers) {
