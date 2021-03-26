8029cf1e3a6fa6b1d6928839ac90bd9c7e08f22a
김우재 <kwj1270@naver.com>
2021-03-26 16:30:42 +0900

    feat:LottoNumbers 인스턴스가 소유한 값을 기준으로 비교하는 기능 구현

--- src/main/java/step2/domain/Lotto/LottoNumbers.java
+++ src/main/java/step2/domain/Lotto/LottoNumbers.java
@@ -3,6 +3,7 @@ package step2.domain.Lotto;
 import step2.exception.ListNullPointerException;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -37,5 +38,18 @@ public final class LottoNumbers {
 
     private final boolean isListNull(List<LottoNumber> lottoNumbers) {
         return lottoNumbers == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumbers that = (LottoNumbers) o;
+        return Objects.equals(lottoNumbers, that.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
     }
 }
