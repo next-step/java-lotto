f3a34c42e33dfde8f7ee9318a3c8866aad930286
김우재 <kwj1270@naver.com>
2021-03-26 09:44:50 +0900

    test:LottoNumbers 인스턴스가 부적절한 값 입력받을 시 예외처리 여부 테스트 작성

--- src/test/java/step2/domain/Lotto/LottoNumbersTest.java
+++ src/test/java/step2/domain/Lotto/LottoNumbersTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumbersTest {
@@ -28,9 +28,24 @@ class LottoNumbersTest {
     void 생성() {
 
         // when
-        LottoNumbers lottoNumbers = new LottoNumbers(lottoNumberList);
+        LottoNumbers lottoNumbers = LottoNumbers.newInstance(lottoNumberList);
 
         // then
         assertThat(lottoNumbers).isNotNull();
     }
+
+    @DisplayName("LottoNumbers 인스턴스 부적절한 값 입력시 예외처리 발생 여부 테스트")
+    @Test
+    void 검증_Null() {
+        // given
+        List<LottoNumber> nullLottoNumberList = null;
+
+        // when and then
+        assertThatThrownBy(()-> {
+            LottoNumbers.newInstance(nullLottoNumberList);
+        }).isInstanceOf(ListNullPointerException.class)
+                .hasMessageContaining("null인 리스트가 입력되었습니다.");
+
+    }
+
 }
