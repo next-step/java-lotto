1662058d517314d5fa02fadecd078cce009b0e4d
김우재 <kwj1270@naver.com>
2021-03-27 02:05:25 +0900

    test:InputLottoNumbers 인스턴스에 null 주입시 예외처리 발생 여부 테스트 작성

--- src/test/java/step2/domain/InputLottoNumbersTest.java
+++ src/test/java/step2/domain/InputLottoNumbersTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.domain.Lotto.LottoNumber;
+import step2.exception.ListNullPointerException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputLottoNumbersTest {
@@ -34,4 +36,20 @@ class InputLottoNumbersTest {
         // then
         assertThat(inputLottoNumbers).isNotNull();
     }
+
+    @DisplayName("InputLottoNumbers 인스턴스에 null 주입시 예외처리 발생 여부 테스트")
+    @Test
+    void 검증_Null() {
+        // given
+        List<LottoNumber> nullLottoNumbers = null;
+
+        // when and then
+        assertThatThrownBy(()-> {
+            InputLottoNumbers.newInstance(testLottoNumbers);
+        }).isInstanceOf(ListNullPointerException.class)
+                .hasMessageContaining("null인 리스트가 입력되었습니다.");
+
+    }
+
+
 }
