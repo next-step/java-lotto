77c7835dbe7d9b3289df56a5e8cdb29bddc65eae
김우재 <kwj1270@naver.com>
2021-03-26 17:23:34 +0900

    feat:Lotto 인스턴스 생성 기능 구현

--- src/main/java/step2/domain/Lotto/Lotto.java
+++ src/main/java/step2/domain/Lotto/Lotto.java
@@ -1,4 +1,17 @@
 package step2.domain.Lotto;
 
+import java.util.List;
+
 public class Lotto {
+    private final List<LottoNumber> lotto;
+
+    private Lotto(List<LottoNumber> lotto) {
+        this.lotto = lotto;
+    }
+
+    public static Lotto newInstance(List<LottoNumber> lotto) {
+        return new Lotto(lotto);
+    }
+
+
 }
