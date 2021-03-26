cf8aa29b9317aca50626fa852b5f398e9ef8f3fc
seokrae <kslbsh@gmail.com>
2021-03-27 04:08:13 +0900

    refactor: 상수 및 예외처리 메서드

---
+++ src/main/java/study/lotto/util/Constants.java
@@ -0,0 +1,27 @@
+package study.lotto.util;
+
+public class Constants {
+
+    private Constants() { }
+
+    public static final String GUIDE_LAST_WEEK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String GUIDE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+    public static final String GUIDE_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    public static final String GUIDE_MANUAL_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
+    public static final String GUIDE_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
+
+    public static final String GUIDE_ERR_CANNOT_PARSE_STRING_TO_INTEGER = "숫자로 캐스팅 할 수 없는 값 입니다.";
+    public static final String GUIDE_ERR_NOT_USE_VALUE = "로또 번호로 사용할 수 없는 값 입니다.";
+
+    public static final String GUIDE_ERR_DONT_CONTAINS_BONUS_NUMBER = "보너스 번호는 당첨 번호에 포함될 수 없습니다.";
+    public static final String GUIDE_ERR_GREATER_THAN_LOTTO_COUNT = "구매할 수 있는 로또 갯수보다 많습니다.";
+    public static final String GUIDE_ERR_CANNOT_BUY_LOTTO = "구매가 불가능합니다.";
+    public static final String GUIDE_ERR_NOT_FOUND_MONEY = "금액을 지불하셔야 합니다.";
+    public static final String GUIDE_ERR_LOTTO_SIZE = "로또 번호가 6개가 아닙니다.";
+
+    public static final String SPACIAL_CHARACTER_SPACE = "\\s";
+    public static final String SPLIT_DELIMITER = ",";
+    public static final String WHITE_SPACE = "";
+    public static final int LOTTO_PRICE = 1000;
+
+}
