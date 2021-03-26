01d6c56c2d7426c0fdfb18ab1056530ec37aaf5f
김우재 <kwj1270@naver.com>
2021-03-26 21:55:16 +0900

    test:LottoExpressionResponseDto 인스턴스 null에 주입시 예외처리 발생 여부 테스트 작성

--- src/main/java/step2/dto/LottoExpressionResponseDto.java
+++ src/main/java/step2/dto/LottoExpressionResponseDto.java
@@ -1,4 +1,14 @@
 package step2.dto;
 
+import step2.domain.Lotto.LottoList;
+
 public class LottoExpressionResponseDto {
+    private final LottoList lottoList;
+
+    private LottoExpressionResponseDto(LottoList lottoList) {
+        this.lottoList = lottoList;
+    }
+    public static LottoExpressionResponseDto newInstance(LottoList lottoList) {
+        return new LottoExpressionResponseDto(lottoList);
+    }
 }
--- src/test/java/step2/dto/LottoExpressionResponseDtoTest.java
+++ src/test/java/step2/dto/LottoExpressionResponseDtoTest.java
@@ -2,6 +2,7 @@ package step2.dto;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.domain.Lotto.Lotto;
 import step2.domain.Lotto.LottoList;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoExpressionResponseDtoTest {
@@ -33,15 +35,28 @@ class LottoExpressionResponseDtoTest {
         testLottoList = LottoList.newInstance(testLottos);
     }
 
-    @Disabled("LottoExpressionResponseDto 인스턴스 생성 여부 테스트")
+    @DisplayName("LottoExpressionResponseDto 인스턴스 생성 여부 테스트")
     @Test
     void 생성() {
         // when
-        LottoExpressionResponseDto responseDto = LottoExpressionResponseDto
-                .newInstance(testLottoList);
+        LottoExpressionResponseDto responseDto =
+                LottoExpressionResponseDto.newInstance(testLottoList);
 
         // then
         assertThat(responseDto).isNotNull();
-
     }
+
+    @DisplayName("LottoExpressionResponseDto 인스턴스 null에 주입시 예외처리 발생 여부 테스트")
+    @Test
+    void 검증_Null() {
+        // given
+        LottoList nullLottoList = null;
+
+        // when and then
+        assertThatThrownBy(()-> {
+            LottoExpressionResponseDto.newInstance(nullLottoList);
+        }).isInstanceOf(LottoListNullPointerException.class)
+                .hasMessageContaining("LottoList 인스턴스가 null 입니다.");
+    }
+
 }
