05a865d5df7ddd1773cd441bc23f9767cbf3d832
김우재 <kwj1270@naver.com>
2021-03-26 10:08:02 +0900

    feat:LottoNumbersGenerator 인스턴스 생성 기능 구현

--- src/main/java/step2/domain/Lotto/LottoNumbersGenerator.java
+++ src/main/java/step2/domain/Lotto/LottoNumbersGenerator.java
@@ -1,4 +1,19 @@
 package step2.domain.Lotto;
 
-public class LottoNumbersGenerator {
+public final class LottoNumbersGenerator {
+
+    private static LottoNumbersGenerator instance;
+
+    private LottoNumbersGenerator() {}
+
+    public static final LottoNumbersGenerator getInstance() {
+        if(isInstanceNull()) {
+            instance = new LottoNumbersGenerator();
+        }
+        return instance;
+    }
+
+    private static final boolean isInstanceNull() {
+        return instance == null;
+    }
 }
