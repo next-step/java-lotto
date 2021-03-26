cc426c8fe030aa4b57f64d21a3a458b3efd1f848
dailyworker <workingssu@gmail.com>
2021-03-26 19:12:57 +0900

    refactor : MatchLotto 수정 - valueOf() : 스트림으로 바꾸고, NONE을 추가하여 Null 처리를 위임 - findPrize() : WinningBoard에서의 Key를 통해서 winner를 찾는 역할

--- src/main/java/lotto/domain/MatchLotto.java
+++ src/main/java/lotto/domain/MatchLotto.java
@@ -1,33 +1,47 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum MatchLotto {
-  FOURTH(3, 5000, "4등"),
-  THIRD(4, 50000, "3등"),
-  SECOND(5, 1500000, "2등"),
-  FIRST(6, 2000000000, "1등");
+  NONE(Integer.MIN_VALUE, 0, "NONE"),
+  FOURTH(3, 5000, "FOURTH"),
+  THIRD(4, 50000, "THIRD"),
+  SECOND(5, 1500000, "SECOND"),
+  FIRST(6, 2000000000, "FIRST");
 
-  private int matchResult;
-  private int prize;
-  private String winner;
+  private int matchCount;
+  private Money winnerMoney;
+  private String winnerRank;
 
-  MatchLotto(int matchResult, int prize, String winner) {
-    this.matchResult = matchResult;
-    this.prize = prize;
-    this.winner = winner;
+  MatchLotto(int matchCount, int winnerMoney, String winnerRank) {
+    this.matchCount = matchCount;
+    this.winnerMoney = new Money(winnerMoney);
+    this.winnerRank = winnerRank;
   }
 
-  public MatchLotto valueOf(int matchResult) {
-    MatchLotto matches = null;
-    for (MatchLotto matchLotto : MatchLotto.values()) {
-      matches = matches(matchResult, matchLotto);
-    }
-    return matches;
+  public String getWinnerRank() {
+    return winnerRank;
   }
 
-  private MatchLotto matches(int matchResult, MatchLotto matchLotto) {
-    if (matchLotto.matchResult == matchResult) {
-      return matchLotto;
-    }
-    throw new IllegalArgumentException("허용되지 않은 값이 입력되었습니다.");
+  public int getWinnerMoney() {
+    return winnerMoney.toInt();
+  }
+
+  public static MatchLotto findPrize(String winnerRank) {
+    return Arrays.stream(MatchLotto.values())
+        .filter(matches -> matches.winnerRank.equals(winnerRank))
+        .findAny()
+        .orElseThrow(IllegalArgumentException::new);
+  }
+
+  public static MatchLotto valueOf(int matchCount) {
+    return Arrays.stream(MatchLotto.values())
+        .filter(matches -> matches.matchCount == matchCount)
+        .findAny()
+        .orElse(MatchLotto.NONE);
+  }
+
+  public static boolean isNone(MatchLotto matchLotto) {
+    return matchLotto == MatchLotto.NONE;
   }
 }
