1a164c83f8b01839d912d04c597768722bba3b74
김우재 <kwj1270@naver.com>
2021-03-26 20:57:22 +0900

    feat:LottoGenerateCount 인스턴스 생성 기능 구현

--- src/main/java/step2/domain/Lotto/LottoGenerateCount.java
+++ src/main/java/step2/domain/Lotto/LottoGenerateCount.java
@@ -1,4 +1,24 @@
 package step2.domain.Lotto;
 
-public class LottoGenerateCount {
+import step2.domain.InputMoney;
+
+public final class LottoGenerateCount {
+
+    private final static int DIVIDE_STANDARD = 1000;
+
+    private int count;
+
+    private LottoGenerateCount(int money) {
+        this.count = (money/DIVIDE_STANDARD);
+    }
+
+    public static final LottoGenerateCount newInstance(InputMoney inputMoney) {
+        return newInstance(inputMoney.getInputMoney());
+    }
+
+    public static final LottoGenerateCount newInstance(int money) {
+        return new LottoGenerateCount(money);
+    }
+
+
 }
