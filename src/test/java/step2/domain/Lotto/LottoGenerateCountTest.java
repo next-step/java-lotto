165111e4c2cc205d88074aba3123b554fb145917
김우재 <kwj1270@naver.com>
2021-03-26 20:58:58 +0900

    test:LottoGenerateCount 인스턴스가 다음 시점으로 넘어갈 수 있는지 테스트 작성

--- src/test/java/step2/domain/Lotto/LottoGenerateCountTest.java
+++ src/test/java/step2/domain/Lotto/LottoGenerateCountTest.java
@@ -20,4 +20,18 @@ class LottoGenerateCountTest {
         // then
         assertThat(lottoGenerateCount).isNotNull();
     }
+
+    @DisplayName("LottoGenerateCount 인스턴스가 다음 시점으로 넘어갈 수 있는지 테스트")
+    @Test
+    void 다음() {
+        // given
+        int testMoney = 1000;
+
+        // when
+        LottoGenerateCount lottoGenerateCount = LottoGenerateCount.newInstance(testMoney);
+        boolean actual = lottoGenerateCount.hasNext();
+
+        // then
+        assertThat(actual).isTrue();
+    }
 }
