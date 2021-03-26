6e15e1add303777d5b8316030a6cee4f9eaf1de1
seokrae <kslbsh@gmail.com>
2021-03-27 04:45:02 +0900

    refactor: 파라미터를 wrapper 클래스로 변경 및 Factory 클래스로 변경

--- src/main/java/study/lotto/controller/LottoStore.java
+++ src/main/java/study/lotto/controller/LottoStore.java
@@ -1,47 +1,23 @@
 package study.lotto.controller;
 
 import study.lotto.domain.Lotto;
-import study.lotto.generator.LottoNumberGenerator;
-import study.lotto.generator.NumberGenerator;
+import study.lotto.service.LottoFactory;
 import study.lotto.service.Lottos;
-import study.lotto.view.dto.RequestMoney;
+import study.lotto.view.dto.RequestLottoArgument;
+import study.lotto.view.dto.ManualLottoParser;
 
 import java.util.List;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-import static study.lotto.view.InputView.WHITE_SPACE;
 
 /**
  * 유저와 로또 사이 판매자 역할
  */
 public class LottoStore {
 
-    private final NumberGenerator generator;
-
-    public LottoStore() {
-        this(new LottoNumberGenerator());
+    public Lottos issueLotto(final RequestLottoArgument argument, final ManualLottoParser manualLottoParser) {
+        return new Lottos(getManualAndAuto(argument.autoSize(), manualLottoParser));
     }
 
-    public LottoStore(final NumberGenerator numberGenerator) {
-        this.generator = numberGenerator;
-    }
-
-    private List<Lotto> autoLotto(final Lottos manualLotto, final int totalLottoCount) {
-        List<Lotto> autoLottos = Stream.generate(() -> generator.generate(WHITE_SPACE))
-                .limit(totalLottoCount)
-                .map(Lotto::new)
-                .collect(toList());
-        return manualLotto.addAll(autoLottos);
-    }
-
-    public Lottos lotto(final RequestMoney money, final Lottos manualLotto) {
-        int totalLottoCount = money.autoCount();
-        List<Lotto> sortLottos = autoLotto(manualLotto, totalLottoCount);
-        return new Lottos(sortLottos, money);
-    }
-
-    public Lotto manualLotto(final String manualNumbers) {
-        return new Lotto(generator.generate(manualNumbers));
+    private List<Lotto> getManualAndAuto(final int totalCount, final ManualLottoParser manualLottoParser) {
+        return LottoFactory.createManualAndAuto(totalCount, manualLottoParser);
     }
 }
