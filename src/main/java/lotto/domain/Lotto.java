87c890039debbcde043463059ef9d2135f601c62
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-03-26 20:02:50 +0900

    refactor : lotto Number 배열 검증로직 Lotto 클래스로 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,6 +9,7 @@ public class Lotto {
     private final List<LottoNumber> lottoNumberList;
 
     public Lotto(int[] lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumberList = Arrays.stream(lottoNumbers).mapToObj(LottoNumber::new).collect(Collectors.toList());
     }
 
@@ -22,5 +23,20 @@ public class Lotto {
             matchCount+=Arrays.stream(winNumbers).filter(winNumber -> lottoNumber.getLottoNumber() == winNumber).count();
         }
         return LottoRank.getEnum(matchCount);
+    }
+
+    public static void validateLottoNumbers(int[] winNumbers) {
+        if(winNumbers==null || winNumbers.length==0) {
+            throw new IllegalArgumentException("당첨 숫자를 입력해 주세요.");
+        }
+        if(winNumbers.length != LottoConstant.MAX_LOTTO_COUNT) {
+            throw new IllegalArgumentException("당첨 숫자는 6개의 수 여야 합니다.");
+        }
+        if(Arrays.stream(winNumbers).distinct().count() < LottoConstant.MAX_LOTTO_COUNT) {
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다. 입력값을 확인해주세요.");
+        }
+        for (int winNumber : winNumbers) {
+            LottoNumber.validateLottoNumber(winNumber);
+        }
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoTest {
 
@@ -28,6 +30,46 @@ class LottoTest {
 
         // then
         assertThat(LottoRank.getEnum(matchCount)).isEqualTo(lottoRank);
+    }
+
+    @Test
+    @DisplayName("로또번호 null 입력")
+    void lotto_nullInput() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(null))
+                .withMessageMatching("당첨 숫자를 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("로또번호 개수 오류")
+    void lotto_numberCount() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(new int[]{1, 2, 3, 4, 5, 6, 7}))
+                .withMessageMatching("당첨 숫자는 6개의 수 여야 합니다.");
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(new int[]{1, 2, 3, 4, 5}))
+                .withMessageMatching("당첨 숫자는 6개의 수 여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또번호 중복입력")
+    void lotto_dupInput() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(new int[]{1, 2, 2, 4, 5, 6}))
+                .withMessageMatching("중복된 숫자가 존재합니다. 입력값을 확인해주세요.");
+    }
+
+    @Test
+    @DisplayName("로또 숫자 범위 불일치")
+    void lotto_numberRange() {
+        // given when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(new int[]{1, 48, 2, 3, 5, 6}))
+                .withMessageMatching("로또 숫자는 1과 45사이의 정수 이어야 합니다.");
     }
 
 }
