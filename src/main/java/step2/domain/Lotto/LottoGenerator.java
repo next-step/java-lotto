cd1e7c245227709fa29a8ba8be4944a67ce86dfe
김우재 <kwj1270@naver.com>
2021-03-26 19:16:20 +0900

    feat:LottoNumbersGenerator 인스턴스가 Lotto 인스턴스를 생성하고 반환하는 기능 작성

--- src/main/java/step2/domain/Lotto/LottoGenerator.java
+++ src/main/java/step2/domain/Lotto/LottoGenerator.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public final class LottoGenerator {
+    private static final int START = 1;
+    private static final int LIMIT = 46;
 
     private static LottoGenerator instance;
 
@@ -21,15 +23,20 @@ public final class LottoGenerator {
         return instance == null;
     }
 
+    public final Lotto generateLotto() {
+        LottoNumbers lottoNumbers = generateLottoNumbers();
+        return Lotto.newInstance(lottoNumbers.subLottoNumbers(0, 6));
+    }
 
     private final LottoNumbers generateLottoNumbers() {
         return LottoNumbers.newInstance(generateLottoNumberList());
     }
 
     private final List<LottoNumber> generateLottoNumberList() {
-        return IntStream.range(1, 46)
+        return IntStream.range(START, LIMIT)
                 .mapToObj(LottoNumber::valueOf)
                 .collect(Collectors.toList());
     }
+
 
 }
