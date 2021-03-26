e773236e3ef8676d678c25face172e7f9d3f5dce
juniq <juniqlim@gmail.com>
2021-03-27 00:04:55 +0900

    repactor: 상수 추출, 메소드 추출

--- src/main/java/im/juniq/lotto/LotteryMachine.java
+++ src/main/java/im/juniq/lotto/LotteryMachine.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LotteryMachine {
+	private static final int LOTTO_PRICE = 1000;
+
 	private ShuffleStrategy shuffleStrategy;
 
 	public LotteryMachine() {
@@ -15,13 +17,21 @@ public class LotteryMachine {
 	}
 
 	public List<Lotto> lottoes(int price) {
-		if (price % 1000 != 0) {
-			throw new RuntimeException("금액은 천원 단위로 입력해주세요.");
-		}
+		checkPrice(price);
+		return makeLotto(price / LOTTO_PRICE);
+	}
+
+	private List<Lotto> makeLotto(int numberOfCreated) {
 		List<Lotto> lottoes = new ArrayList<>();
-		for (int i = 0; i < price / 1000; i++) {
+		for (int i = 0; i < numberOfCreated; i++) {
 			lottoes.add(new Lotto(shuffleStrategy));
 		}
 		return lottoes;
+	}
+
+	private void checkPrice(int price) {
+		if (price % LOTTO_PRICE != 0) {
+			throw new RuntimeException("금액은 " + LOTTO_PRICE + "원 단위로 입력해주세요.");
+		}
 	}
 }
