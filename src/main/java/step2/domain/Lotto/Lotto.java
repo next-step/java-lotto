d63327b4ed122ff0ef3261e001516ab983034245
김우재 <kwj1270@naver.com>
2021-03-26 17:37:00 +0900

    feat:Lotto 인스턴스 null 주입시 예외처리 기능 구현

--- src/main/java/step2/domain/Lotto/Lotto.java
+++ src/main/java/step2/domain/Lotto/Lotto.java
@@ -1,16 +1,25 @@
 package step2.domain.Lotto;
 
+import step2.exception.ListNullPointerException;
+
 import java.util.List;
 
 public class Lotto {
     private final List<LottoNumber> lotto;
 
     private Lotto(List<LottoNumber> lotto) {
+        if (isListNull(lotto)) {
+            throw new ListNullPointerException();
+        }
         this.lotto = lotto;
     }
 
-    public static Lotto newInstance(List<LottoNumber> lotto) {
+    public static final Lotto newInstance(List<LottoNumber> lotto) {
         return new Lotto(lotto);
+    }
+
+    private final boolean isListNull(List<LottoNumber> lotto) {
+        return lotto == null;
     }
 
 
