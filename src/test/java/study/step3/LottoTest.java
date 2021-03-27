61500316d67923d8725ae55756b9d98d38cfdf3d
Donghui-Lee <ehdgml3206@naver.com>
2021-03-27 22:24:26 +0900

    refactor: 테스트 코드 추가

--- src/test/java/study/step3/LottoTest.java
+++ src/test/java/study/step3/LottoTest.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
--- src/test/java/study/step3/LottosTest.java
+++ src/test/java/study/step3/LottosTest.java
@@ -2,20 +2,29 @@ package study.step3;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
-import study.step3.domain.Amount;
-import study.step3.domain.LottoFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import study.step3.domain.Lotto;
+import study.step3.domain.LottoNumber;
 import study.step3.domain.Lottos;
 
 public class LottosTest {
-    @Test
-    @DisplayName("금액에 따른 로또 생성 테스트")
-    void purchase() {
-        // given
-        Lottos lottos = LottoFactory.purchase(new Amount(12000));
-        // when & then
-        assertThat(lottos.getLottos().size()).isEqualTo(12);
+    @ParameterizedTest(name = "추가 테스트")
+    @ValueSource(strings = {"1", "45"})
+    public void add(int input) {
+        Lottos lottoList = new Lottos();
+        List<LottoNumber> lottoNumberList = new ArrayList<>();
+        IntStream.range(1, 5).forEach(i -> {
+            lottoNumberList.add(LottoNumber.of(i));
+        });
+        for(int i = 0; i < input; i++) {
+            lottoList.add(new Lotto(lottoNumberList));
+        }
+        assertThat(lottoList.getLottos().size()).isEqualTo(input);
     }
 }
