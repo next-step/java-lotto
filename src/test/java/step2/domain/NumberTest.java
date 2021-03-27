d60d93acee782ae403c28e9ef80c994f5de45c07
shkwon <oper912@naver.com>
2021-03-28 03:24:42 +0900

    test: 범위초과 숫자입력 테스트

--- src/test/java/step2/domain/NumberTest.java
+++ src/test/java/step2/domain/NumberTest.java
@@ -2,8 +2,10 @@ package step2.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.constants.Constants;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberTest {
 
@@ -18,5 +20,15 @@ class NumberTest {
 
         // then
         assertThat(number).isEqualTo(Number.from(text));
+    }
+
+    @Test
+    @DisplayName("로또 범위 초과 숫자 입력시 RuntimeException 발생한다")
+    void range_exceed_expcetion() {
+        assertThatThrownBy(() -> Number.from(Constants.LOTTO_MIN_BOUND - 1))
+                .isInstanceOf(RuntimeException.class);
+
+        assertThatThrownBy(() -> Number.from(Constants.LOTTO_MAX_BOUND + 1))
+                .isInstanceOf(RuntimeException.class);
     }
 }
