e1aca716a15bbe73f52a6da22dd09480720476f9
jess <jess.l@kakaocorp.com>
2021-03-27 16:13:21 +0900

    feat(InputView): 지난주 당첨번호 입력 추가

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String REQUEST_PURCHASE_MESSAGE = "구매금액을 입력해 주세요.";
+    private static final String REQUEST_WINNING_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요";
 
     private InputView() {
 
@@ -11,6 +12,15 @@ public class InputView {
 
     public static String getLottoPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_MESSAGE);
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNext()) {
+            return scanner.nextLine();
+        }
+        throw new IllegalStateException("입력값을 받지 못했습니다.");
+    }
+
+    public static String getLastWeekWinningNumber() {
+        System.out.println(REQUEST_WINNING_LOTTO_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNext()) {
             return scanner.nextLine();
