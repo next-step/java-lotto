db5a49c1486b3f216bb7fb3c3eea3ca53724c4ad
ad7549 <ad7549@gmail.com>
2021-03-27 22:27:49 +0900

    feat: Add error message

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -7,6 +7,8 @@ import java.util.stream.IntStream;
 public class LottoNumber implements Comparable<LottoNumber> {
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
+    private static final String BOUNDARY_ERROR_MESSAGE =
+            String.format("로또 번호의 범위는 %d ~ %d입니다.", MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
 
     private static final Map<Integer, LottoNumber> lottoNumbersCache = new HashMap<>();
 
@@ -22,13 +24,13 @@ public class LottoNumber implements Comparable<LottoNumber> {
     }
 
     public static LottoNumber of(final int number) {
-        validate(number);
+        validateBoundary(number);
         return lottoNumbersCache.get(number);
     }
 
-    private static void validate(final int number) {
+    private static void validateBoundary(final int number) {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(BOUNDARY_ERROR_MESSAGE);
         }
     }
 
