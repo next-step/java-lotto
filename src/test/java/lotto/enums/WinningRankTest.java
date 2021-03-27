25eadd851ee692c52de8fe0cc768bbc969084efd
Beenie93 <seongbeen93@naver.com>
2021-03-27 14:44:44 +0900

    test: implement a test unit to get total prize

--- src/test/java/lotto/enums/WinningRankTest.java
+++ src/test/java/lotto/enums/WinningRankTest.java
@@ -1,7 +1,10 @@
 package lotto.enums;
 
+import lotto.domain.RanksCount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,5 +31,18 @@ public class WinningRankTest {
         WinningRank etc = WinningRank.findByMacthedCount(2);
         assertThat(firstPlace).isEqualTo(WinningRank.FIRST_PLACE);
         assertThat(etc).isEqualTo(WinningRank.ETC);
+    }
+
+    @Test
+    public void totalPrize() throws Exception {
+        //given
+        RanksCount ranksCount = new RanksCount();
+        ranksCount.add(Arrays.asList(WinningRank.FIRST_PLACE, WinningRank.FIRST_PLACE, WinningRank.SECOND_PLACE, WinningRank.THIRD_PLACE));
+
+        //when
+        int totalPrize = WinningRank.totalPrize(ranksCount);
+
+        //then
+        assertThat(totalPrize).isEqualTo(2_000_000_000 + 2_000_000_000 + 1_500_000 + 50_000);
     }
 }
