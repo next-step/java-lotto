498cc085347680ff7721a11a1431878c2d6aefd7
shkwon <oper912@naver.com>
2021-03-28 03:20:16 +0900

    fix: 당첨번호 범위 초과 숫자 입력시 exception 처리

--- src/main/java/step2/domain/Number.java
+++ src/main/java/step2/domain/Number.java
@@ -1,5 +1,7 @@
 package step2.domain;
 
+import step2.constants.Constants;
+
 import java.util.Objects;
 
 public class Number {
@@ -10,6 +12,9 @@ public class Number {
     }
 
     public static Number from(final int number) {
+        if (number < Constants.LOTTO_MIN_BOUND || number > Constants.LOTTO_MAX_BOUND) {
+            throw new RuntimeException("로또에서 가능한 범위를 초과한 숫자입니다.");
+        }
         return new Number(number);
     }
 
