450afc9fdb860ff6d952aea21b81d85f3736fa8d
shkwon <oper912@naver.com>
2021-03-28 01:40:11 +0900

    feat: 구입한 로또 출력

--- src/main/java/step2/controller/LottoController.java
+++ src/main/java/step2/controller/LottoController.java
@@ -16,6 +16,7 @@ public class LottoController {
     }
 
     public void run() {
-
+        ResultView.printPurchaseCount(lottos);
+        ResultView.printAllLotto(lottos);
     }
 }
--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -1,4 +1,33 @@
 package step2.view;
 
+import step2.domain.Lotto;
+import step2.domain.Lottos;
+import step2.domain.Number;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultView {
+
+    public static void printPurchaseCount(Lottos lottos) {
+        System.out.println(String.format("%s개를 구매했습니다.", lottos.getLottos().size()));
+    }
+
+    public static void printAllLotto(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println(getPrintLottoMessage(lotto));
+        }
+    }
+
+    private static String getPrintLottoMessage(Lotto lotto) {
+        return String.format("%s%s%s", "[", String.join(", ", lottoToStringList(lotto)), "]");
+    }
+
+    private static List<String> lottoToStringList(Lotto lotto) {
+        return lotto.getNumbers()
+                .stream()
+                .map(Number::getNumber)
+                .map(i -> String.valueOf(i))
+                .collect(Collectors.toList());
+    }
 }
