c82acca6d0d66698551c306c65d01d1759bb2d84
juniq <juniqlim@gmail.com>
2021-03-27 21:39:41 +0900

    feat: 당첨 번호 입력

--- src/main/java/im/juniq/lotto/view/InputView.java
+++ src/main/java/im/juniq/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package im.juniq.lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,7 +11,16 @@ public class InputView {
 		return Integer.parseInt(scanner.next());
 	}
 
-	public static void main(String[] args) {
-		InputView.purchaseAmount();
+	public static List<Integer> winningNumbers() {
+		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		Scanner scanner = new Scanner(System.in);
+		String inputString = scanner.next();
+		String[] stringNumbers = inputString.split(",");
+
+		List<Integer> numbers = new ArrayList<>();
+		for (String number : stringNumbers) {
+			numbers.add(Integer.parseInt(number));
+		}
+		return numbers;
 	}
 }
