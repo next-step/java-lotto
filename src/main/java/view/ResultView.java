240128e9dd5c88038dd262c96f92e57c6bae1f7a
이승환 <lufful.induk@gmail.com>
2021-03-27 18:37:41 +0900

    <feat> 당첨금액 변경

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -12,9 +12,9 @@ public class ResultView {
 
         List<Rank> ranks = lottoResultResponse.getRanks();
         System.out.println("3개 일치 (5000원)- "+ranks.stream().filter(rank-> rank == Rank.THIRD_PLACE).count());
-        System.out.println("4개 일치 (5000원)- "+ranks.stream().filter(rank-> rank == Rank.FOUR_PLACE).count());
-        System.out.println("5개 일치 (5000원)- "+ranks.stream().filter(rank-> rank == Rank.SECOND_PLACE).count());
-        System.out.println("6개 일치 (5000원)- "+ranks.stream().filter(rank-> rank == Rank.FIRST_PLACE).count());
+        System.out.println("4개 일치 (50000원)- "+ranks.stream().filter(rank-> rank == Rank.FOUR_PLACE).count());
+        System.out.println("5개 일치 (1500000)- "+ranks.stream().filter(rank-> rank == Rank.SECOND_PLACE).count());
+        System.out.println("6개 일치 (2000000000원)- "+ranks.stream().filter(rank-> rank == Rank.FIRST_PLACE).count());
         System.out.println("총 수익률은"+ lottoResultResponse.getYield());
     }
 
