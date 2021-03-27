13e521872a61c9937cedfb6dfade67fc0dcb3194
keke0123 <keke0123@naver.com>
2021-03-27 22:49:09 +0900

    (feat) grade enum 제작

---
+++ src/main/java/step02/code/domain/GradeEnum.java
@@ -0,0 +1,31 @@
+package step02.code.domain;
+
+import java.util.Arrays;
+
+public enum GradeEnum {
+  FIRST(6, 2000000000),
+  SECOND(5, 1500000),
+  THRID(4, 50000),
+  FOURTH(3, 5000),
+  NONE(0, 0);
+
+  private final int grade;
+  private final int prizeMoney;
+
+  private GradeEnum(int grade, int prizeMoney) {
+    this.grade = grade;
+    this.prizeMoney = prizeMoney;
+  }
+
+  public static GradeEnum fromGrade(int grade) {
+    return Arrays.stream(GradeEnum.values())
+      .filter(gradeEnum -> gradeEnum.grade == grade)
+      .findAny()
+      .orElse(GradeEnum.NONE);
+  }
+
+  public int prize() {
+    return prizeMoney;
+  }
+
+}
