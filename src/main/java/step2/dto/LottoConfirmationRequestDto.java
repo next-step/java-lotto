894c0923d7ff6b4065ab84c1ede1788e390061fa
김우재 <kwj1270@naver.com>
2021-03-27 14:51:38 +0900

    feat:LottoConfirmationRequestDto 인스턴스 생성 기능 구현

--- src/main/java/step2/dto/LottoConfirmationRequestDto.java
+++ src/main/java/step2/dto/LottoConfirmationRequestDto.java
@@ -1,4 +1,21 @@
 package step2.dto;
 
+import step2.domain.InputLottoNumbers;
+
 public class LottoConfirmationRequestDto {
+
+    private final InputLottoNumbers inputLottoNumbers;
+
+    private LottoConfirmationRequestDto(InputLottoNumbers inputLottoNumbers) {
+        this.inputLottoNumbers = inputLottoNumbers;
+    }
+
+    private LottoConfirmationRequestDto(String inputLottoNumbers) {
+        this(InputLottoNumbers.newInstance(inputLottoNumbers));
+    }
+
+    public static LottoConfirmationRequestDto newInstance(String inputLottoNumbers) {
+        return new LottoConfirmationRequestDto(inputLottoNumbers);
+    }
+
 }
