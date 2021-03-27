b1467d9320ed849d8e8075fd23a753449c730d93
Beenie93 <seongbeen93@naver.com>
2021-03-27 15:13:30 +0900

    feat: implement ProfitRate Object

---
+++ src/main/java/lotto/domain/ProfitRate.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class ProfitRate {
+    private final double profitRate;
+
+
+    public ProfitRate(double profitRate) {
+        this.profitRate = profitRate;
+    }
+
+    public ProfitRate(int totalPrize, int purchaeAmount) {
+        this.profitRate = (double) totalPrize / purchaeAmount;
+    }
+
+    public double profitRate() {
+        return profitRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProfitRate that = (ProfitRate) o;
+        return Double.compare(that.profitRate, profitRate) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(profitRate);
+    }
+}
