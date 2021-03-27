a4c97ccb4144e94ec3c6713c912ea9b4490d88b1
jess <jess.l@kakaocorp.com>
2021-03-27 16:43:48 +0900

    feat(LottoController): 지난주 당첨번호를 받아 발행한 로또번호의 당첨통계를 출력하는 프로세스 추가

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -7,5 +7,9 @@ public class LottoController {
         String amount = InputView.getLottoPurchaseAmount();
         List<Lotto> lottos = LottoStore.issue(amount);
         ResultView.printLottoPurchased(lottos);
+        String winningNumber = InputView.getLastWeekWinningNumber();
+        Lotto winning = Lotto.of(winningNumber);
+        WinningStatistics statistics = new WinningStatistics(winning, lottos);
+        ResultView.printWinningStatistics(statistics);
     }
 }
