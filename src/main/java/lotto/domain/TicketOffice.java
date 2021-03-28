63d77429674acec514468c0c2dd6aee494fb4cf3
Beenie93 <seongbeen93@naver.com>
2021-03-29 00:24:19 +0900

    feat: implement TicketOffice Object

---
+++ src/main/java/lotto/domain/TicketOffice.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class TicketOffice {
+    private final LottoTicketPrice lottoTicketPrice;
+
+    public TicketOffice(LottoTicketPrice lottoTicketPrice) {
+        this.lottoTicketPrice = lottoTicketPrice;
+    }
+
+    public LottoTickets sale(NumberOfTicket numberOfTicket) {
+        LottoTickets lottoTickets = new LottoTickets(new ArrayList<>());
+
+        for (int i = 0; i < numberOfTicket.count(); i++) {
+            lottoTickets.add(new LottoTicket(new LottoNumbers()));
+        }
+
+        return lottoTickets;
+    }
+
+    public NumberOfTicket numberOfTicket(PurchaseAmount purchaseAmount) {
+        return new NumberOfTicket(purchaseAmount.purchaseAmount() / lottoTicketPrice.price());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TicketOffice that = (TicketOffice) o;
+        return Objects.equals(lottoTicketPrice, that.lottoTicketPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoTicketPrice);
+    }
+}
