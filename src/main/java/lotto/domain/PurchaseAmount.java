aa753c0d64667c24dd6d54300ded45663c00835f
Beenie93 <seongbeen93@naver.com>
2021-03-28 15:27:33 +0900

    refactor: apply main/sub constructor

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -3,20 +3,26 @@ package lotto.domain;
 import java.util.Objects;
 
 public class PurchaseAmount {
-    private final static int PURCHASE_AMOUNT_MIN = 1000;
+    private final static int PURCHASE_AMOUNT_MIN = 1_000;
     private final static String GREATER_THAN_MIN = String.format("구매금액은 최소 %d원 이상이여야 합니다.", PURCHASE_AMOUNT_MIN);
-    private final static String CHECK_CHARACTER = "숫자가 아닌 문자가 포함되어 있는지 확인해주세요.";
+    private final static String CHECK_CHARACTER = "구입 금액에 문자가 포함되어 있는지 확인해주세요.";
     private final int purchaseAmount;
+
+    public PurchaseAmount(String purchaseAmount) {
+        this(convertToInt(purchaseAmount));
+    }
 
     public PurchaseAmount(int purchaseAmount) {
         checkGreaterThanMinimum(purchaseAmount);
         this.purchaseAmount = purchaseAmount;
     }
 
-    public PurchaseAmount(String purchaseAmount) {
-        checkInteger(purchaseAmount);
-        checkGreaterThanMinimum(Integer.parseInt(purchaseAmount));
-        this.purchaseAmount = Integer.parseInt(purchaseAmount);
+    private static int convertToInt(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(CHECK_CHARACTER);
+        }
     }
 
     public NumberOfTicket numberOfTicket(int lottoTicketPrice) {
@@ -25,14 +31,6 @@ public class PurchaseAmount {
 
     public int purchaseAmount() {
         return purchaseAmount;
-    }
-
-    private void checkInteger(String purchaseAmount) {
-        try {
-            Integer.parseInt(purchaseAmount);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(CHECK_CHARACTER);
-        }
     }
 
     private void checkGreaterThanMinimum(int purchaseAmount) {
@@ -53,4 +51,5 @@ public class PurchaseAmount {
     public int hashCode() {
         return Objects.hash(purchaseAmount);
     }
+
 }
--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import lotto.domain.PurchaseAmount;
 import lotto.utils.LottoRandomNumberUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,8 +12,8 @@ public class PurchaseAmountTest {
     @Test
     @DisplayName("구매 금액 객체 생성")
     public void create() throws Exception {
-        PurchaseAmount purchaseAmount = new PurchaseAmount(1000);
-        assertThat(purchaseAmount).isEqualTo(new PurchaseAmount(1000));
+        PurchaseAmount purchaseAmount = new PurchaseAmount(1_000);
+        assertThat(purchaseAmount).isEqualTo(new PurchaseAmount(1_000));
         purchaseAmount = new PurchaseAmount("1000");
         assertThat(purchaseAmount).isEqualTo(new PurchaseAmount("1000"));
     }
@@ -22,7 +21,7 @@ public class PurchaseAmountTest {
     @Test
     @DisplayName("구매한 티켓 장수 구하기")
     public void numberOfTicket() throws Exception {
-        PurchaseAmount purchaseAmount = new PurchaseAmount(1000);
+        PurchaseAmount purchaseAmount = new PurchaseAmount(1_000);
         LottoTicket lottoTicket = new LottoTicket(new LottoNumbers(LottoRandomNumberUtil.lottoNumbers()));
         NumberOfTicket numberOfTicket = purchaseAmount.numberOfTicket(lottoTicket.price());
         assertThat(numberOfTicket.count()).isEqualTo(1);
@@ -33,11 +32,5 @@ public class PurchaseAmountTest {
     public void checkMinimum() throws Exception {
         assertThatIllegalArgumentException().isThrownBy(() -> new PurchaseAmount(999));
         assertThatIllegalArgumentException().isThrownBy(() -> new PurchaseAmount("999"));
-    }
-
-    @Test
-    @DisplayName("숫자가 아닐 경우 예외")
-    public void checkInteger() throws Exception {
-        assertThatIllegalArgumentException().isThrownBy(() -> new PurchaseAmount("1000s"));
     }
 }
