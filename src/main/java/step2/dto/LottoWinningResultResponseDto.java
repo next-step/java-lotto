cec9515d47b12f29884c58d3bf32498313f93b91
김우재 <kwj1270@naver.com>
2021-03-28 23:55:13 +0900

    feat:LottoWinningResultResponseDto 인스턴스 생성 기능 구현

--- src/main/java/step2/dto/LottoWinningResultResponseDto.java
+++ src/main/java/step2/dto/LottoWinningResultResponseDto.java
@@ -1,5 +1,17 @@
 package step2.dto;
 
+import step2.domain.winning.WinningScoreBoard;
+
 public class LottoWinningResultResponseDto {
+
+    private final WinningScoreBoard winningScoreBoard;
+
+    public static LottoWinningResultResponseDto newInstance(WinningScoreBoard winningScoreBoard) {
+        return new LottoWinningResultResponseDto(winningScoreBoard);
+    }
+
+    private LottoWinningResultResponseDto(WinningScoreBoard winningScoreBoard) {
+        this.winningScoreBoard = winningScoreBoard;
+    }
 
 }
