1fcd67bcf22eab4045c73198a73f6b2fd97f84dd
Sooncheol <ryxhdrnrmf@gmail.com>
2021-03-28 19:07:17 +0900

    feat: InputView 수정

--- src/main/java/lotto/step2/controller/LottoController.java
+++ src/main/java/lotto/step2/controller/LottoController.java
@@ -5,10 +5,10 @@ import lotto.step2.view.InputView;
 import lotto.step2.view.ResultView;
 
 public class LottoController {
-    public void run(){
+    public void run() {
         Lottos lottos = LottoStore.purchase(new Money(InputView.enterPurchasingAmount()));
         ResultView.printLottoNumbers(lottos);
-        Lotto winningLotto = new Lotto(InputView.enterWinningNumber());
+        Lotto winningLotto = new Lotto(InputView.enterWinningNumbers());
         ResultView.printResult(new WinningStatistics(lottos, winningLotto));
     }
 }
--- src/main/java/lotto/step2/view/InputView.java
+++ src/main/java/lotto/step2/view/InputView.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class InputView {
     private final static String MESSAGE_PURCHASING_AMOUNT = "구입금액을 입력해 주세요.";
     private final static String MESSAGE_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private final static String MESSAGE_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private final static String DELIMITER_COMMA = ",";
     private final static Scanner scanner = new Scanner(System.in);
-    
+
 
     private InputView() {
     }
@@ -17,9 +18,15 @@ public class InputView {
         return scanner.next();
     }
 
-    public static String[] enterWinningNumber() {
+    public static String[] enterWinningNumbers() {
         System.out.println(MESSAGE_WINNING_NUMBER);
         scanner.nextLine();
         return scanner.nextLine().split(DELIMITER_COMMA);
+    }
+    
+    public static String enterBonusNumber() {
+        System.out.println(MESSAGE_BONUS_NUMBER);
+        scanner.nextLine();
+        return scanner.next();
     }
 }
