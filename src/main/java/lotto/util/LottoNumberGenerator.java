fae042803068605f250481b6028937be1eab64ea
dailyworker <workingssu@gmail.com>
2021-03-28 13:41:29 +0900

    refactor : Strategy 패턴 적용으로 인한 변경사항 수정

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -1,11 +1,15 @@
 package lotto.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Number;
 
 public class LottoNumberGenerator {
 
   private static final int LOTTO_NUMBER_RANGE = 45;
+
   private final List<Integer> lottos;
 
   public LottoNumberGenerator(List<Integer> lottoTotalSet) {
@@ -16,8 +20,8 @@ public class LottoNumberGenerator {
     return lottos;
   }
 
-  public int getOne(int i) {
-    return this.lottos.get(i);
+  public int getOne(int index) {
+    return this.lottos.get(index);
   }
 
   public static LottoNumberGenerator createLottoNumbers() {
@@ -28,4 +32,20 @@ public class LottoNumberGenerator {
     return new LottoNumberGenerator(lottoNumbers);
   }
 
+  public static Lotto createManualLottoNumbers(ManualLottoNumberGenerateStrategy strategy) {
+    return createLotto(strategy.generate());
+  }
+
+  public static Lotto createAutoLottoNumbers(AutoLottoNumberGenerateStrategy strategy) {
+    return createLotto(strategy.generate());
+  }
+
+  private static Lotto createLotto(List<Number> numberList) {
+    return new Lotto(sort(numberList));
+  }
+
+  private static List<Number> sort(List<Number> generatedLottoNumber) {
+    Collections.sort(generatedLottoNumber);
+    return generatedLottoNumber;
+  }
 }
