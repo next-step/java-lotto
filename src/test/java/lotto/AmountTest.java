53342586821be7b256d2c51160d4ba877e465748
623nana <6230nana@gmail.com>
2021-03-28 13:28:42 +0900

    test: 수동 구매 수량 테스트

--- src/test/java/lotto/AmountTest.java
+++ src/test/java/lotto/AmountTest.java
@@ -18,17 +18,34 @@ public class AmountTest {
   }
 
   @Test
-  @DisplayName("[Amount] 복권 수량 생성 테스트")
+  @DisplayName("[Amount] 복권 자동 수량 생성 테스트")
   void createAmountTest() {
-    int quantity = amount.calculateAmount(14000);
+    int quantity = amount.calculateAutoAmount(14000, 2);
 
-    assertThat(quantity).isEqualTo(14);
+    assertThat(quantity).isEqualTo(12);
   }
 
   @Test
   @DisplayName("[Amount] 입력 금액 검증 테스트")
   void validateTest() {
-    assertThatThrownBy(() -> amount.validate(14500))
+    assertThatThrownBy(() -> amount.validate(14500, 0))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("[Amount] 수동 구매 수량 입력 테스트")
+  void manualAmountTest() {
+    amount = new Amount(14000, 2);
+
+    int quantity = amount.getManualAmount();
+
+    assertThat(quantity).isEqualTo(2);
+  }
+
+  @Test
+  @DisplayName("[Amount] 수동 구매 수량 검증 테스트")
+  void validateManualTest() {
+    assertThatThrownBy(() -> amount.validate(14000, 15))
         .isInstanceOf(IllegalArgumentException.class);
   }
 }
