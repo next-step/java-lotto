8914645d8b526a5a44926b363cde6dbdcd412b54
juniq <juniqlim@gmail.com>
2021-03-29 14:55:36 +0900

    test: Price 테스트
    Price 클래스의 테스트 코드가 누락되어 테스트 코드 추가.

---
+++ src/test/java/im/juniq/lotto/PriceTest.java
@@ -0,0 +1,29 @@
+package im.juniq.lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class PriceTest {
+    @Test
+    void create() {
+        assertThatCode(() -> new Price(1000)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void checkPrice() {
+        assertThatThrownBy(() -> new Price(1100)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void numberOfLottoPurchased() {
+        assertThat(new Price(1000).numberOfLottoPurchased()).isEqualTo(1);
+    }
+
+    @Test
+    void yield() {
+        assertThat(new Price(10000).yield(5000D)).isEqualTo(0.5);
+    }
+}
