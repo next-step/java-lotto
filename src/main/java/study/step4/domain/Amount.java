bdcd13f73cad77edffca94e0d0c8e4e8b96b6e56
Donghui-Lee <ehdgml3206@naver.com>
2021-03-29 09:40:07 +0900

    feat: 수동/자동 로또 구매 수, 금액 담당하는 객체 기능 확장

--- src/main/java/study/step4/domain/Amount.java
+++ src/main/java/study/step4/domain/Amount.java
@@ -3,23 +3,47 @@ package study.step4.domain;
 public class Amount {
     private static final int LOTTO_PRICE = 1000;
     private final int amount;
+    private final int manualCount;
 
     public Amount(int amount) {
-        valid(amount);
-        this.amount = amount;
+        this(amount, 0);
+    }
+
+    public Amount(int amount, int manualCount) {
+        this.amount = this.validAmount(amount);
+        this.manualCount = this.validManualCount(manualCount);
+    }
+
+    private int validAmount(int amount) {
+        if ((amount < 0) || (amount % LOTTO_PRICE != 0)) {
+            throw new IllegalArgumentException("로또 구매 최저 금액은 " + LOTTO_PRICE + "원 입니다.");
+        }
+        return amount;
+    }
+
+    private int validManualCount(int manualCount) {
+        if(manualCount < 0) {
+            throw new IllegalArgumentException("구매할 수동 로또 수가 0개 이상이여야 합니다.");
+        }
+        if(totalCount() - manualCount < 0) {
+            throw new IllegalArgumentException("수동으로 구매할 로또 수가 구입금액 보다 큽니다.");
+        }
+        return manualCount;
+    }
+
+    public int totalCount() {
+        return amount / LOTTO_PRICE;
+    }
+
+    public int getAutoCount() {
+        return totalCount() - this.manualCount;
     }
 
     public int getAmount() {
         return amount;
     }
 
-    private void valid(int amount) {
-        if ((amount < 0) || (amount % LOTTO_PRICE != 0)) {
-            throw new IllegalArgumentException("로또 구매 최저 금액은 " + LOTTO_PRICE + "원 입니다.");
-        }
-    }
-
-    public int getPurchaseLottoCount() {
-        return this.amount / LOTTO_PRICE;
+    public int getManualCount() {
+        return manualCount;
     }
 }
--- src/test/java/study/step4/AmountTest.java
+++ src/test/java/study/step4/AmountTest.java
@@ -10,18 +10,36 @@ import org.junit.jupiter.params.provider.ValueSource;
 import study.step4.domain.Amount;
 
 public class AmountTest {
-    @ParameterizedTest(name = "유효성 테스트")
+    @ParameterizedTest(name = "금액 유효성 테스트")
     @ValueSource(strings = {"900", "-1"})
-    public void valid(int input) {
+    public void validAmount(int input) {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Amount(input));
     }
 
-    @ParameterizedTest(name = "복권 구매 갯수 테스트")
+    @ParameterizedTest(name = "수동 복권 수 유효성 테스트")
+    @ValueSource(strings = {"-1", "3"})
+    public void validManual(int input) {
+        // given
+        int amount = 2000;
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Amount(amount, input));
+    }
+
+    @ParameterizedTest(name = "복권 전체 구매 수 테스트")
     @CsvSource(value = {"1000:1", "2000:2", "150000: 150"}, delimiter = ':')
-    public void purchaseLottoCount(int input, int expected) {
+    public void totalCount(int input, int expected) {
         // given
         Amount amount = new Amount(input);
         // when & then
-        assertThat(amount.getPurchaseLottoCount()).isEqualTo(expected);
+        assertThat(amount.totalCount()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "복권 수동 구매 수에 따른 자동 구매 수 테스트")
+    @CsvSource(value = {"1:9", "5:5", "10:0"}, delimiter = ':')
+    void manual(int input, int expected) {
+        // given
+        Amount amount = new Amount(10000, input);
+        // when & then
+        assertThat(amount.getAutoCount()).isEqualTo(expected);
     }
 }
