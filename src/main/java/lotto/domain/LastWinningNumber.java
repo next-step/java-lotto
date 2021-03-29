ddc57d43f16f0e38de4f640dd397d9293016d7d8
Dion <ksundong@gmail.com>
2021-03-29 23:15:33 +0900

    feat: LastWinningNumber 생성테스트가 동작하도록 수정

--- src/main/java/lotto/domain/LastWinningNumber.java
+++ src/main/java/lotto/domain/LastWinningNumber.java
@@ -1,8 +1,32 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public final class LastWinningNumber {
 
-  public LastWinningNumber(Lotto lastWinningLotto, LottoNumber bonusNumber) {
+  private final Lotto lastWinningLotto;
+  private final LottoNumber bonusNumber;
 
+  public LastWinningNumber(Lotto lastWinningLotto, LottoNumber bonusNumber) {
+    this.lastWinningLotto = lastWinningLotto;
+    this.bonusNumber = bonusNumber;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LastWinningNumber)) {
+      return false;
+    }
+    LastWinningNumber that = (LastWinningNumber) o;
+    return Objects.equals(lastWinningLotto, that.lastWinningLotto) && Objects
+        .equals(bonusNumber, that.bonusNumber);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lastWinningLotto, bonusNumber);
   }
 }
