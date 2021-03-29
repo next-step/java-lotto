629481a420854ee355a4de51c5c3d0f18e802584
hwangyunho <hwang9yh@gmail.com>
2021-03-29 22:32:48 +0900

    refactor: 다수 로또 생성 가독성 있게 수정

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -2,24 +2,21 @@ package lotto.domain;
 
 import lotto.domain.enums.LottoMatcher;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoGame {
 
-    private final static int LOTTO_START_INCLUSIVE_VALUE = 0;
+    private final static int LOTTO_INIT_COUNT = 0;
     private final static LottoGenerator lottoGenerator = LottoGenerator.getInstance();
     private final Lottos lottos;
     private LottoBoard lottoBoard;
 
     private LottoGame(final int lottoAmount) {
-        List<Lotto> lottoList = IntStream.range(LOTTO_START_INCLUSIVE_VALUE, lottoAmount)
-                .mapToObj(i ->
-                        Lotto.of(lottoGenerator.generateShuffledLotto())
-                )
-                .collect(Collectors.toList());
-
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int lottoCount = LOTTO_INIT_COUNT; lottoCount < lottoAmount; lottoCount++) {
+            lottoList.add(Lotto.of(lottoGenerator.generateShuffledLotto()));
+        }
         this.lottos = Lottos.of(lottoList);
     }
 
