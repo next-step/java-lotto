eca3a8072741020af229c862386a349f3ffd08a0
Donghui-Lee <ehdgml3206@naver.com>
2021-03-29 14:45:12 +0900

    test: 테스트 코드 추가

--- src/test/java/study/step4/LottoFactoryTest.java
+++ src/test/java/study/step4/LottoFactoryTest.java
@@ -1,7 +1,13 @@
 package study.step4;
 
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,13 +17,24 @@ import study.step4.domain.Lottos;
 
 public class LottoFactoryTest {
 
-    @ParameterizedTest(name = "구입 테스트")
+    @ParameterizedTest(name = "자동 구매 테스트")
     @CsvSource(value = {"2000:2", "3000:3", "12000:12"}, delimiter = ':')
-    public void purchase(int input, int expected) {
+    public void purchaseAutoLotto(int input, int expected) {
         // given
         Lottos lottos = LottoFactory.purchaseAutoLotto(new Amount(input));
         // when & then
         assertThat(lottos.getLottos().size()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("수동 구매 테스트")
+    public void purchaseManualLotto() {
+        // given
+        String[] input = {"1,2,3,4,5,6", "10,11,12,13,14,15"};
+        List<String> test = Arrays.stream(input).collect(toList());
+        Lottos lottos = LottoFactory.purchaseManualLotto(test);
+        // when & then
+        assertThat(lottos.getLottos().size()).isEqualTo(2);
     }
 }
 
