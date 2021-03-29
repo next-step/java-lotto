7c66cfcae6e0ab961214bcc9ec0ed09e028f4fb4
whdudtlr9042 <64422093+whdudtlr9042@users.noreply.github.com>
2021-03-29 21:51:23 +0900

    Step1 문자열 덧셈 계산기 리뷰요청드립니다. (#1352)
    * Step1 문자열 덧셈 계산기 리뷰요청드립니다.
    
    * Step1 fix: 메세지 수정

---
+++ src/main/java/step1/exceptin/StringAddCaculatorException.java
@@ -0,0 +1,8 @@
+package step1.exceptin;
+
+public class StringAddCaculatorException extends RuntimeException{
+
+    public StringAddCaculatorException(String message) {
+        super(message);
+    }
+}
