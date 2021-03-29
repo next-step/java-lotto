a5b1d522a4fe7f59043ccc71ec8232ec57aa6ffc
juniq <juniqlim@gmail.com>
2021-03-29 13:54:02 +0900

    feat: 일치된 숫자에 맞는 당첨결과 찾기

--- src/main/java/im/juniq/lotto/Winning.java
+++ src/main/java/im/juniq/lotto/Winning.java
@@ -1,15 +1,33 @@
 package im.juniq.lotto;
 
-public enum Winning {
-	SIX_NUMBERS_MATCHED(2000000000L),
-	FIVE_NUMBERS_MATCHED(15000000L),
-	FOUR_NUMBERS_MATCHED(50000L),
-	THREE_NUMBERS_MATCHED(5000L);
+import java.util.HashMap;
+import java.util.Map;
 
+public enum Winning {
+	SIX_NUMBERS_MATCHED(6, 2000000000L),
+	FIVE_NUMBERS_MATCHED(5, 15000000L),
+	FOUR_NUMBERS_MATCHED(4, 50000L),
+	THREE_NUMBERS_MATCHED(3, 5000L),
+	LOSING(2, 0L);
+
+	private int matchedCount;
 	private Long amount;
 
-	Winning(Long amount) {
+	Winning(int matchedCount, Long amount) {
+		this.matchedCount = matchedCount;
 		this.amount = amount;
+	}
+
+	public static Winning findByMatchedCount(int numberOfMatchedWinningNumber) {
+		Map<Integer, Winning> map = new HashMap<>();
+		for (Winning winning : Winning.values()) {
+			map.put(winning.matchedCount, winning);
+		}
+		Winning gottenWinning = map.get(numberOfMatchedWinningNumber);
+		if (gottenWinning == null) {
+			return LOSING;
+		}
+		return gottenWinning;
 	}
 
 	public Long amount() {
