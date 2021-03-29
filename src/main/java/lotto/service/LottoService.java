63a81ef29df4ebca898d31b0f4a34fb7598875cc
Dongjoon Park <djoon@users.noreply.github.com>
2021-03-29 12:55:48 +0900

    feat: 로또 구입 서비스 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,11 +1,30 @@
 package lotto.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.domain.machine.LottoGenerator;
+import lotto.domain.shop.LottoShop;
+import lotto.view.dto.LottoDto;
 import lotto.view.dto.LottoOrderDto;
 import lotto.view.dto.LottoOrderResultDto;
 
 public class LottoService {
+    private final LottoGenerator lottoGenerator;
+
+    public LottoService(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
 
     public LottoOrderResultDto purchase(LottoOrderDto lottoOrderDto) {
-        return null;
+        LottoShop lottoShop = new LottoShop(lottoOrderDto.getMoney(), lottoGenerator);
+
+        List<Lotto> lotteries = lottoShop.purchase();
+        List<LottoDto> lottoDtoList = lotteries.stream()
+                .map(Lotto::copyLottoNumbers)
+                .map(LottoDto::new)
+                .collect(Collectors.toList());
+
+        return new LottoOrderResultDto(lottoDtoList);
     }
 }
--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lotto.domain.machine.TestLottoGenerator;
 import lotto.service.LottoService;
 import lotto.view.dto.LottoOrderDto;
 import lotto.view.dto.LottoOrderResultDto;
@@ -13,7 +14,7 @@ public class LottoServiceTest {
     @Test
     @DisplayName("구입 금액에 따라 로또를 구매하는 서비스")
     void lottoOrderingServiceTest() {
-        LottoService lottoService = new LottoService();
+        LottoService lottoService = new LottoService(new TestLottoGenerator());
         LottoOrderDto lottoOrderDto = new LottoOrderDto(14000);
 
         LottoOrderResultDto lottoOrderResultDto = lottoService.purchase(lottoOrderDto);
