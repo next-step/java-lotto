661155b4a943bea72296eed8d8affd5b0b109b4a
w-beom <w_bum@naver.com>
2021-03-31 00:35:00 +0900

    test : 보너스 당첨 테스트구현

--- src/test/java/lotto/domain/StatisticsTest.java
+++ src/test/java/lotto/domain/StatisticsTest.java
@@ -11,16 +11,17 @@ class StatisticsTest {
     @Test
     @DisplayName("matchCount와 맞는 Statistics 객체 반환")
     void getStatisticsByMatchCountTest() {
-        assertThat(Statistics.getStatisticsByMatchCount(3)).isEqualTo(Statistics.THREE);
-        assertThat(Statistics.getStatisticsByMatchCount(4)).isEqualTo(Statistics.FOUR);
-        assertThat(Statistics.getStatisticsByMatchCount(5)).isEqualTo(Statistics.FIVE);
-        assertThat(Statistics.getStatisticsByMatchCount(6)).isEqualTo(Statistics.SIX);
+        assertThat(Statistics.getStatisticsByMatchCount(3,false)).isEqualTo(Statistics.THREE);
+        assertThat(Statistics.getStatisticsByMatchCount(4, false)).isEqualTo(Statistics.FOUR);
+        assertThat(Statistics.getStatisticsByMatchCount(5, false)).isEqualTo(Statistics.FIVE);
+        assertThat(Statistics.getStatisticsByMatchCount(5, true)).isEqualTo(Statistics.BONUS);
+        assertThat(Statistics.getStatisticsByMatchCount(6, false)).isEqualTo(Statistics.SIX);
     }
 
     @Test
     @DisplayName("matchCount와 맞는 객체가 없을경우 Exception 발생")
     void notExistValueTest() {
-        assertThatThrownBy(() -> Statistics.getStatisticsByMatchCount(7))
+        assertThatThrownBy(() -> Statistics.getStatisticsByMatchCount(7, true))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("존재하지 않는 값입니다.");
     }
