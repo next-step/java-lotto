59c89bb32673ecbdaed75e68a93f45f5974ab9be
hwangjeonghyeon <deert101@gmail.com>
2021-03-30 13:58:40 +0900

    [feat] 로또 우승 상수화

--- src/main/java/step4/Rank.java
+++ src/main/java/step4/Rank.java
@@ -1,2 +1,50 @@
-package step4;public class Rank {
+package step4;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Stream;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private int countOfMatch;
+    private int winningMoney;
+
+    private Rank(int countOfMatch, int winningMoney) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+
+    public static Rank rank(int countOfMatch, boolean bonusMatch) {
+        Rank[] rank = values();
+
+        if (Rank.SECOND.getCountOfMatch() == countOfMatch) {
+            return bonus(bonusMatch);
+        }
+
+        return Stream.of(rank)
+                .filter(ranks -> ranks.getCountOfMatch() == countOfMatch)
+                .findFirst()
+                .orElse(Rank.MISS);
+    }
+
+    private static Rank bonus(boolean bonusMatch) {
+        if (bonusMatch) {
+            return SECOND;
+        }
+        return THIRD;
+    }
 }
