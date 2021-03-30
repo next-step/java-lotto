81fccab3789f5852bbfacd5fda2442ab6d87868e
keke0123 <keke0123@naver.com>
2021-03-30 18:14:56 +0900

    (feat) main controller 수정, resultView 수정

--- src/main/java/step02/code/Main.java
+++ src/main/java/step02/code/Main.java
@@ -7,6 +7,7 @@ import step02.code.common.ResultView;
 import step02.code.domain.GradeEnum;
 import step02.code.domain.Lotto;
 import step02.code.domain.Lottos;
+import step02.code.domain.Number;
 import step02.code.domain.Outcome;
 
 public class Main {
@@ -18,23 +19,24 @@ public class Main {
 
     Lotto winningNumber = Lotto.makeLottoByString(InputView.winningNumber());
 
+    Number bonus = Number.numberByString(InputView.bonusNumber());
+
     // 결과
     System.out.println("당첨 통계");
     System.out.println("---------------");
 
-    Outcome outcome = new Outcome(money, lottos, winningNumber);
+    Outcome outcome = new Outcome(money, lottos, winningNumber, bonus);
 
     int total = 0;
-    Map<Integer, Integer> result = outcome.statistic();
-    for(int i = 3; i <= 6; i++) {
-      GradeEnum grade = GradeEnum.fromGrade(i);
-      int matched = result.getOrDefault(i, 0);
-      
-      total += grade.prize() * matched;
+    Map<GradeEnum, Integer> result = outcome.statistic();
 
-      ResultView.result(i, grade.prize(), matched);
+    // GradeEnum.values() 대신 none 만 안나오도록 설정해서 다시
+    for(GradeEnum gradeEnum : GradeEnum.sorted()) {
+      int size = result.getOrDefault(gradeEnum, 0);
+      ResultView.result(gradeEnum, size);
+      total += size * gradeEnum.prize();
     }
-
+    
     ResultView.rate(outcome.profit(total));
     
     InputView.close();
