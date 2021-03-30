4d6354a8a40d226f8266412b456767818dcc813a
yonghwa <qhdl0224@gmail.com>
2021-03-30 22:15:53 +0900

    [refactor] 소스 수정

--- src/main/java/step2/Domain/Profit.java
+++ src/main/java/step2/Domain/Profit.java
@@ -2,26 +2,38 @@ package step2.Domain;
 
 public class Profit {
 
-//    private PurchaseManager manager;
-//    private Rank rank;
-//
-//    public Profit(PurchaseManager manager, Rank rank) {
-//        this.manager = manager;
-//        this.rank = rank;
-//    }
-//
-//    public double profitRate() {
-//        return profitSum() / manager.money().getMoney();
-//    }
-//
-//    //refactoring 필요
+    private Money money;
+    private Rank rank;
+
+    public Profit(Money money, Rank rank) {
+        this.money = money;
+        this.rank = rank;
+    }
+
+    public double profitRate() {
+        return profitSum() / money.getMoney();
+    }
+
+    //refactoring 필요
+    public double profitSum() {
+        return rank.getRank().keySet()
+                .stream()
+                .filter(hitCount -> rank.size(hitCount) > 0)
+                .mapToDouble(hitCount ->
+                        Long.valueOf(LottoPrize.valueOf(hitCount,false).prize()).doubleValue()
+                                * rank.size(hitCount))
+                .sum();
+    }
+
+    //refactoring 필요
 //    public double profitSum() {
-//        return rank.getRank().keySet()
-//                .stream()
-//                .filter(hitCount -> rank.size(hitCount) > 0)
-//                .mapToDouble(hitCount ->
-//                        Long.valueOf(LottoPrize.valueOf(hitCount,false).prize()).doubleValue()
-//                                * rank.size(hitCount))
-//                .sum();
+//        for (int rank = 3; rank <= 6; rank++) {
+//            HitCount hitCount = new HitCount(rank);
+//            System.out.println(String.format("%d개 일치 (%d)원 - %d개",
+//                    rank, LottoPrize.getPrizeInfoByHitCount(hitCount).prize(), ranking.size(hitCount)));
+//        }
+//        Profit profit = new Profit(purchaseManager, ranking);
+//        System.out.println(String.format("총 수익률은 %.2f 입니다.", profit.profitRate()));
 //    }
+
 }
