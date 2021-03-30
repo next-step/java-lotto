d1380d565852a706aea0bfcf20ca2fe965da4837
Dongjoon Park <djoon@users.noreply.github.com>
2021-03-31 01:23:21 +0900

    refactor: controller 삭제

--- src/main/java/lotto/LottoApp.java
+++ src/main/java/lotto/LottoApp.java
@@ -1,6 +1,8 @@
 package lotto;
 
-import lotto.controller.LottoController;
+import lotto.domain.machine.TestLottoGenerator;
+import lotto.service.LottoScoring;
+import lotto.service.LottoShopping;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 import lotto.view.dto.LottoDto;
@@ -10,17 +12,20 @@ import lotto.view.dto.LottoScoringDto;
 import lotto.view.dto.LottoScoringResultDto;
 
 public class LottoApp {
+    private static LottoShopping shoppingService =
+            new LottoShopping(new TestLottoGenerator());
+    private static LottoScoring scoringService = new LottoScoring();
 
     public static void main(String[] args) {
         InputView inputView = new InputView(System.in);
 
         LottoOrderDto lottoOrderDto = new LottoOrderDto(inputView.inputMoney());
-        LottoOrderResultDto lottoOrderResultDto = LottoController.orderLotto(lottoOrderDto);
+        LottoOrderResultDto lottoOrderResultDto = shoppingService.purchase(lottoOrderDto);
         ResultView.printLottoOrderResult(lottoOrderResultDto);
 
         LottoScoringDto lottoScoringDto = new LottoScoringDto(
                 new LottoDto(inputView.inputWinnerNumbers()), lottoOrderResultDto);
-        LottoScoringResultDto lottoScoringResultDto = LottoController.scoreLotto(lottoScoringDto);
+        LottoScoringResultDto lottoScoringResultDto = scoringService.score(lottoScoringDto);
         ResultView.printLottoScoringResult(lottoScoringResultDto);
     }
 }
