c6f014d16ed5dd09b454c998c32394db5f4d2f07
sonAjin <sonajin11@gmail.com>
2021-03-30 23:55:21 +0900

    (refactor) 상수화 할 수 있는 부분 추가

--- src/main/java/step01/Config.java
+++ src/main/java/step01/Config.java
@@ -3,4 +3,7 @@ package step01;
 public class Config {
     static final String ZERO = "0";
     static final String BASIC_SEPARATOR = ",|:";
+    static final int FIRST = 1;
+    static final int SECOND =2;
+    static final String REGEX = "//(.)\n(.*)";
 }
