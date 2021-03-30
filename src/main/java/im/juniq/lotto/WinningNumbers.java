e97a860ca2e3404df49a1270dc5d34b801b2ce80
juniq <juniqlim@gmail.com>
2021-03-30 22:10:22 +0900

    repactor: WinningNumbers 객체 변수에 보너스볼 추가

--- src/main/java/im/juniq/lotto/WinningNumbers.java
+++ src/main/java/im/juniq/lotto/WinningNumbers.java
@@ -6,6 +6,12 @@ import java.util.List;
 
 public class WinningNumbers {
 	private List<Integer> winningNumbers = new ArrayList<>();
+	private int bonusNumber;
+
+	public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+		winningNumbers.addAll(numbers);
+		this.bonusNumber = bonusNumber;
+	}
 
 	public WinningNumbers(String[] numbers) {
 		for (String number : numbers) {
@@ -19,5 +25,9 @@ public class WinningNumbers {
 
 	public boolean existent(int number) {
 		return winningNumbers.contains(number);
+	}
+
+	public boolean matchedBonusNumber(int number) {
+		return bonusNumber == number;
 	}
 }
