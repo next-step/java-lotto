d5eb6bd4de9ac1523753d47445d921a54442191c
김우재 <kwj1270@naver.com>
2021-04-01 01:16:26 +0900

    test:LottoWinningCheckMachine 인스턴스 null 주입시 예외처리 여부 테스트 작성

--- src/test/java/step2/domain/check/LottoWinningCheckMachineTest.java
+++ src/test/java/step2/domain/check/LottoWinningCheckMachineTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import step2.domain.lotto.Lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoWinningCheckMachineTest {
@@ -20,6 +21,19 @@ class LottoWinningCheckMachineTest {
 
         // then
         assertThat(machine).isNotNull();
+    }
+
+    @DisplayName("LottoWinningCheckMachine 인스턴스 null 주입시 예외처리 여부 테스트")
+    @Test
+    void 검증() {
+        // given
+        Lotto winningLotto = null;
+
+        // when and then
+        assertThatThrownBy(() -> LottoWinningCheckMachine.of(winningLotto))
+                .isInstanceOf(LottoNullPointerException.class)
+                .hasMessageContaining("null 인 Lotto가 입력되었습니다.");
+
     }
 
 }
