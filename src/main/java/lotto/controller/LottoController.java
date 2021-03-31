fa94963970c679e6a4a13535215c6ddccc101b48
dailyworker <workingssu@gmail.com>
2021-03-31 20:57:01 +0900

    refactor : 구조변경 및 리팩토링 - LottoController : 보너스볼을 사용자에게 입력할 수 있게끔 변경 및 진입점 startLottoGame() 추가 - InputView : 보너스볼 부분 추가 - OutputView : String.format() 자동 반올림 부분때문에 DecimalFormat으로 해당 값 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import lotto.domain.Lotto;
 import lotto.domain.LottoAgency;
 import lotto.domain.Money;
+import lotto.domain.Number;
 import lotto.ui.InputView;
 import lotto.ui.OutputView;
 
@@ -15,21 +16,42 @@ public class LottoController {
   private static final InputView inputView = new InputView(reader);
   private static final OutputView outputView = new OutputView();
 
-  private LottoAgency agency;
-  private Lotto winNumbers;
+  private final LottoAgency agency;
+  private final Lotto winNumbers;
 
-  public void lottoResult() {
-    OutputView.printResult(agency.getTotalResult(winNumbers));
+  public LottoController() throws IOException {
+    this.agency = createLottoAgency();
+    buyLottoPhase();
+    this.winNumbers = inputWinLottoNumbers();
+    lottoResult();
   }
 
-  public void userInput() throws IOException {
-    Money money = inputView.howManyBuyLottoCoupon();
-    initPhase(money);
-    winNumbers = (inputView.enterWinnerNumbers());
+  public static void startLottoGame() throws IOException {
+    new LottoController();
   }
 
-  private void initPhase(Money money) {
-    agency = new LottoAgency(money);
+  private void lottoResult() throws IOException {
+    Number bonusBall = inputBonusBall();
+    OutputView.printResult(agency.getLottoResult(winNumbers, bonusBall));
+  }
+
+  private LottoAgency createLottoAgency() throws IOException {
+    return new LottoAgency(inputUserHowManyBuyLotto());
+  }
+
+  private Lotto inputWinLottoNumbers() throws IOException {
+    return inputView.enterWinnerNumbers();
+  }
+
+  private Money inputUserHowManyBuyLotto() throws IOException {
+    return inputView.howManyBuyLottoCoupon();
+  }
+
+  private Number inputBonusBall() throws IOException {
+    return inputView.enterBonusBall();
+  }
+
+  private void buyLottoPhase() {
     agency.purchaseLotto();
     outputView.printBoughtLottoCoupons(agency);
   }
--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,21 +1,29 @@
 package lotto.ui;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import lotto.domain.Lotto;
 import lotto.domain.LottoAgency;
 import lotto.domain.LottoCoupon;
-import lotto.domain.WinningBoard;
+import lotto.domain.LottoScoreBoard;
 
 public class OutputView {
 
-  public static void printResult(WinningBoard totalResult) {
+  public static void printResult(LottoScoreBoard totalResult) {
     System.out.println("지난 주 당첨 번호는 " );
     System.out.println(totalResult.toResultString());
-    System.out.println("총 수익률은 " + totalResult.calculateEarningRate() + "입니다.");
+    System.out.println("총 수익률은 " + doubleToStringFormat(totalResult) + "입니다.");
   }
 
   public void printBoughtLottoCoupons(LottoAgency agency) {
     System.out.println(agency.getPurchaseQuantity() + "장을 구매하셨습니다.");
     System.out.println(printLottoCoupon(agency));
+  }
+
+  private static String doubleToStringFormat(LottoScoreBoard totalResult) {
+    DecimalFormat df = new DecimalFormat("#.##");
+    df.setRoundingMode(RoundingMode.DOWN);
+    return df.format(totalResult.getEarningRate());
   }
 
   private String printLottoCoupon(LottoAgency agency) {
