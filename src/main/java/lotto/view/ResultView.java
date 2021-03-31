a37fcc56d09985b62668e17032aeecf867e67cc3
Beenie93 <seongbeen93@naver.com>
2021-04-01 03:34:52 +0900

    feat: add a phrase for a second rank with bonus

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -14,7 +14,9 @@ public class ResultView {
     private static final String DELIMITER = ", ";
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String BOUNDARY = "---------";
-    private static final String MATCH_PRIZE_COUNT = "%d개 일치 (%d원)- %d개";
+    private static final String MATCH_PRIZE_COUNT = "%d개 일치%s(%d원)- %d개";
+    private static final String MATCH_BONUS = ", 보너스 볼 일치";
+    private static final String BLANK = " ";
     private static final String PLUS_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 이득이라는 의미임)";
     private static final String MINUS_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
@@ -34,10 +36,18 @@ public class ResultView {
         System.out.println();
         System.out.println(WINNING_STATISTICS);
         System.out.println(BOUNDARY);
-
         for (WinningRank rank : rankCount.keySet()) {
-            System.out.printf((MATCH_PRIZE_COUNT) + "%n", rank.matchedCount(), rank.prize(), rankCount.get(rank));
+            String bonus = bonus(rank);
+            System.out.printf((MATCH_PRIZE_COUNT) + "%n", rank.matchedCount(), bonus, rank.prize(), rankCount.get(rank));
         }
+    }
+
+    private static String bonus(WinningRank rank) {
+        if (rank.equals(WinningRank.SECOND_PLACE)) {
+            return MATCH_BONUS;
+        }
+
+        return BLANK;
     }
 
     public static void positiveProfitRate(double profitRate) {
