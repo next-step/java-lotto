f4f40c97f87fb03993308466eebc03f8d1e705df
dailyworker <workingssu@gmail.com>
2021-03-31 20:48:13 +0900

    test : MoneyTest message 추가 - 기존의 toInt()를 삭제 한 후 각각의 연산에 맞는 값을 전달하는 메시지로 변경 1. dividePerLotto() : 입력된 금액 / 1000의 연산을 수행 2. calculateEarningRate() : 로또 매치 결과에 따른 수익률 계산 수행 3. calculateWinningMoney() : 상금의 계산을 수행

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -2,8 +2,13 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
   @Test
@@ -14,5 +19,52 @@ class MoneyTest {
     Money money = new Money(1000);
     //then
     assertEquals(money, new Money(1000));
+  }
+
+  @Test
+  @DisplayName("입력 받은 금액을 토대로 로또의 개수만큼 구매할 수 있는가")
+  public void dividePerLotto() throws Exception {
+    //given
+    Money money = new Money(14000);
+    //when
+    int perLotto = money.dividePerLotto();
+    //then
+    assertEquals(perLotto, 14);
+  }
+
+  @ParameterizedTest
+  @CsvSource({"5000, 0.35", "50000, 3.57", "1500000, 107.14", "30000000, 2142.85", "2000000000, 142857.14"})
+  @DisplayName("전체금액과 구매한 금액을 나눠서 수익률을 계산할 수 있는가")
+  public void calculateEarningRate(int prize, String answer) throws Exception {
+    //given
+    Money money = new Money(14000);
+    //when
+    double earning = money.calculateEarningRate(prize);
+    DecimalFormat df = new DecimalFormat("#.##");
+    df.setRoundingMode(RoundingMode.DOWN);
+    //then
+    assertEquals(df.format(earning), answer);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1,2,3,4,5,6})
+  @DisplayName("상금 계산을 수행할 수 있는가")
+  public void calculateWinningMoney(int value) throws Exception {
+    //given
+    LottoRank first = LottoRank.FIRST;
+    LottoRank second = LottoRank.SECOND;
+    LottoRank third = LottoRank.THIRD;
+    LottoRank fourth = LottoRank.FOURTH;
+    LottoRank fifth = LottoRank.FIFTH;
+
+    //when
+    //then
+    assertAll("Test Calculate WinningMoney",
+        () -> assertEquals(first.getWinnerMoney().calculateWinningMoney(value), 2000000000 * value),
+        () -> assertEquals(second.getWinnerMoney().calculateWinningMoney(value), 30000000 * value),
+        () -> assertEquals(third.getWinnerMoney().calculateWinningMoney(value), 1500000 * value),
+        () -> assertEquals(fourth.getWinnerMoney().calculateWinningMoney(value), 50000 * value),
+        () -> assertEquals(fifth.getWinnerMoney().calculateWinningMoney(value), 5000 * value)
+    );
   }
 }
