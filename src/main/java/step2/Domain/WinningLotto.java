93c6d4af9e74306555966cc2029c02a1371ef189
yonghwa <qhdl0224@gmail.com>
2021-03-31 11:22:06 +0900

    [feat] 지난주 당첨번호에 보너스볼 추가하여 구현

--- src/main/java/step2/Domain/WinningLotto.java
+++ src/main/java/step2/Domain/WinningLotto.java
@@ -2,8 +2,18 @@ package step2.Domain;
 
 public class WinningLotto extends Lotto {
 
-    public WinningLotto(String[] lottoList) {
+    private BonusBall bonusBall;
+
+    public WinningLotto(String[] lottoList , BonusBall bonusBall) {
         super(lottoList);
+        if(bonusBallDuplicateCheck(bonusBall)){
+            throw new IllegalArgumentException("지난 주 당첨번호와 보너스 볼이 중복됩니다.");
+        }
+        this.bonusBall = bonusBall;
     }
 
+    private boolean bonusBallDuplicateCheck(BonusBall bonusBall){
+        return this.getLottoNumberList().stream()
+                .anyMatch(lottoNumber -> lottoNumber.getNumber() == bonusBall.getNumber());
+    }
 }
