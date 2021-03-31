67a0eaa8178360f436d57661e9b91ab4b4f061bd
Beenie93 <seongbeen93@naver.com>
2021-03-31 22:59:43 +0900

    feat: implement IllegalStringException

--- src/main/java/lotto/utils/ConvertUtil.java
+++ src/main/java/lotto/utils/ConvertUtil.java
@@ -1,5 +1,7 @@
 package lotto.utils;
 
+import lotto.exception.IllegalStringException;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,24 +9,17 @@ import java.util.stream.Collectors;
 
 public class ConvertUtil {
 
-    private final static String CHECK_CHARACTER = "%s에 문자가 포함되어 있는지 확인해주세요.";
-
     public static int toInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(String.format(CHECK_CHARACTER, input));
+            throw new IllegalStringException(input);
         }
     }
 
     public static List<Integer> toIntegers(List<String> input) {
-        try {
-            return new ArrayList<>(Collections.unmodifiableList(input.stream()
-                    .mapToInt(Integer::parseInt)
-                    .boxed()
-                    .collect(Collectors.toList())));
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(String.format(CHECK_CHARACTER, input));
-        }
+        return new ArrayList<>(Collections.unmodifiableList(input.stream()
+                .map(ConvertUtil::toInt)
+                .collect(Collectors.toList())));
     }
 }
