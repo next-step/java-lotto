3462344ab3a264a60ce4c005d45614061e18261d
dailyworker <workingssu@gmail.com>
2021-03-31 21:06:25 +0900

    test : LottoScoreBoardTest 수정

--- src/test/java/lotto/domain/LottoScoreBoardTest.java
+++ src/test/java/lotto/domain/LottoScoreBoardTest.java
@@ -1,14 +1,16 @@
 package lotto.domain;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 class LottoScoreBoardTest {
 
@@ -18,22 +20,34 @@ class LottoScoreBoardTest {
   public void setUp() {
     lottoRanks.add(LottoRank.FIFTH);
     lottoRanks.add(LottoRank.FOURTH);
-    lottoRanks.add(LottoRank.THIRD);
-    lottoRanks.add(LottoRank.SECOND);
-    lottoRanks.add(LottoRank.FIRST);
   }
 
-  @ParameterizedTest
-  @CsvSource({"3, FIFTH", "4, FOURTH", "5, THIRD", "6, FIRST"})
-  @DisplayName("구매한 로또 쿠폰에 대한 올바른 당첨 결과에 대한 검증을 제대로 하는가")
-  public void validateWinLotto(int winner, String key) throws Exception {
+  @Test
+  @DisplayName("제대로 생성이 되는가")
+  public void create() {
+    //given
+    //when
+    LottoScoreBoard lottoResult = LottoScoreBoard.createLottoResult(lottoRanks, new Money(1000));
+
+    //then
+    assertAll(
+        () -> assertNotNull(lottoResult.getMatchResultMap().get("FIFTH")),
+        () -> assertNotNull(lottoResult.getMatchResultMap().get("FOURTH"))
+    );
+  }
+
+  @Test
+  @DisplayName("매치된 로또의 총 수익률을 검증 할 수 있는가")
+  public void validateTotalEarningRate() throws Exception {
     //given
     LottoScoreBoard lottoResult = LottoScoreBoard.createLottoResult(lottoRanks, new Money(1000));
 
     //when
-    Map<String, Integer> winningBoard = lottoResult.getMatchResultMap();
+    double earningRate = lottoResult.getEarningRate();
+    DecimalFormat df = new DecimalFormat("#.##");
+    df.setRoundingMode(RoundingMode.DOWN);
 
     //then
-    assertEquals((int) winningBoard.get(key), 1);
+    assertEquals(earningRate, 55);
   }
 }
