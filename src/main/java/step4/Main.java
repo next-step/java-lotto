17f953c8409645397b89ec544a3935b6ba4931a6
hwangjeonghyeon <deert101@gmail.com>
2021-04-01 02:26:27 +0900

    [refactor] 명확한 이름 변경

--- src/main/java/step4/Main.java
+++ src/main/java/step4/Main.java
@@ -2,7 +2,7 @@ package step4;
 
 public class Main {
     public static void main(String[] args) {
-        LottoApp app = new LottoApp();
+        LottoController app = new LottoController();
         app.start();
     }
 }
