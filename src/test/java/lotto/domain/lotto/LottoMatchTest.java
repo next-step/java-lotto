98e0fc3635bc9c5aa81755f65d726ccfc0fd2099
Dion <ksundong@gmail.com>
2021-04-01 23:00:00 +0900

    test: LottoMatch 생성 실패 실패하는 테스트 추가

--- src/test/java/lotto/domain/lotto/LottoMatchTest.java
+++ src/test/java/lotto/domain/lotto/LottoMatchTest.java
@@ -1,6 +1,7 @@
 package lotto.domain.lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,9 +11,17 @@ class LottoMatchTest {
   @Test
   @DisplayName("생성 테스트")
   void create() {
+    // given
     int matchCount = 3;
-    boolean isBonusNumberMatch = false;
-    assertThat(new LottoMatch(matchCount, isBonusNumberMatch))
-        .isEqualTo(new LottoMatch(matchCount, isBonusNumberMatch));
+
+    // when
+    // then
+    assertThat(new LottoMatch(matchCount, false)).isEqualTo(new LottoMatch(matchCount, false));
+  }
+
+  @Test
+  @DisplayName("생성 실패 테스트")
+  void createFailure() {
+    assertThatIllegalArgumentException().isThrownBy(() -> new LottoMatch(7, false));
   }
 }
