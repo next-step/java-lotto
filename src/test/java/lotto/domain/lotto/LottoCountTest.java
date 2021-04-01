ecd7edcaf49d0867559bb7f62c56c3bda030ffba
Dion <ksundong@gmail.com>
2021-04-02 00:33:49 +0900

    test: LottoCount 보다 ManualCount를 더 많이 요청하면 실패하는 기능 실패하는 테스트 추가

--- src/test/java/lotto/domain/lotto/LottoCountTest.java
+++ src/test/java/lotto/domain/lotto/LottoCountTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import lotto.exception.MoreManualLottoException;
 import lotto.exception.NegativeLottoCountException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,5 +37,16 @@ class LottoCountTest {
             .isInstanceOf(NegativeLottoCountException.class)
             .hasMessage(NegativeLottoCountException.INVALID_LOTTO_COUNT)
     );
+  }
+
+  @Test
+  @DisplayName("기본 로또 매수보다 많은 수동 로또 생성 시도시 오류발생 테스트")
+  void createMoreManualCount() {
+    // given
+    // when
+    // then
+    assertThatThrownBy(() -> new LottoCount(1, 4))
+        .isInstanceOf(MoreManualLottoException.class)
+        .hasMessage(MoreManualLottoException.INVALID_MANUAL_LOTTO_COUNT);
   }
 }
