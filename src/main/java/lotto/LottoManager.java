967dfbd4461e17165e69ca60c12d7ade8c365e6f
Dion <ksundong@gmail.com>
2021-04-01 20:43:08 +0900

    feat: 수동 입력값 검증 기능 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -19,6 +19,7 @@ public final class LottoManager {
       System.out.println();
 
       int manualCount = inputView.inputManualCount();
+      validateManualCount(lottoCount, manualCount);
       System.out.println();
 
       Lottos userLottos = new Lottos(lottoCount);
@@ -29,6 +30,15 @@ public final class LottoManager {
       WinningStatistics winningStatistics = new WinningStatistics(userLottos, winningLotto);
       Printer.printRank(winningStatistics);
       Printer.printResult(winningStatistics, userMoney);
+    }
+  }
+
+  private static void validateManualCount(long lottoCount, int manualCount) {
+    if (manualCount < 0) {
+      throw new IllegalArgumentException("수동 구매의 장 수는 음수일 수 없습니다.");
+    }
+    if (manualCount > lottoCount) {
+      throw new IllegalArgumentException("구매한 매수보다 수동으로 더 많이 구매할 수 없습니다.");
     }
   }
 }
