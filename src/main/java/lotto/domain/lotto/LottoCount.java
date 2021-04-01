e46d3186438c7d497e6ee3305c2e8292080bafda
Dion <ksundong@gmail.com>
2021-04-02 00:58:01 +0900

    feat: LottoCount 생성 테스트가 성공하도록 수정

--- src/main/java/lotto/domain/lotto/LottoCount.java
+++ src/main/java/lotto/domain/lotto/LottoCount.java
@@ -1,10 +1,29 @@
 package lotto.domain.lotto;
 
+import java.util.Objects;
+
 public final class LottoCount {
 
   private final long lottoCount;
 
   public LottoCount(long lottoCount) {
     this.lottoCount = lottoCount;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LottoCount)) {
+      return false;
+    }
+    LottoCount that = (LottoCount) o;
+    return lottoCount == that.lottoCount;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lottoCount);
   }
 }
