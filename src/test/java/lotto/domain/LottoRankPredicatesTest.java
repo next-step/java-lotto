7bcce33d994554b689e2710be8979245796d411f
dailyworker <workingssu@gmail.com>
2021-04-02 12:46:29 +0900

    test : LottoRank.findRank -> LottoRankPredicates 이동

--- src/test/java/lotto/domain/LottoRankPredicatesTest.java
+++ src/test/java/lotto/domain/LottoRankPredicatesTest.java
@@ -1,37 +1,13 @@
 package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.function.Predicate;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoRankPredicatesTest {
-
-  @Test
-  @DisplayName("LottoRank의 2등과 3등을 getSecondOrThird를 통해 판별 후 가져올 수 있는가")
-  public void getSecondOrThirdTest() throws Exception {
-    //given
-    Predicate<LottoRank> lottoRankPredicate = LottoRankPredicates.getSecondOrThird(false);
-    //when
-    boolean expectedTest = lottoRankPredicate.test(LottoRank.THIRD);
-    //then
-    assertTrue(expectedTest);
-  }
-
-  @Test
-  @DisplayName("LottoRank의 2,3등을 제외한 케이스를 판별 할 수 있는가")
-  public void defaultCaseTest() throws Exception {
-    //given
-    Predicate<LottoRank> lottoRankPredicate = LottoRankPredicates.defaultCase(6);
-    //when
-    boolean test = lottoRankPredicate.test(LottoRank.FIRST);
-    //then
-    assertTrue(test);
-  }
 
   @ParameterizedTest
   @CsvSource({"1, NONE", "2, NONE", "3, FIFTH","4, FOURTH", "6, FIRST"})
@@ -53,5 +29,15 @@ class LottoRankPredicatesTest {
     LottoRank lottoRank = LottoRankPredicates.filterLottoRankIsSecondOrThird(value);
     //then
     assertEquals(lottoRank.name(), name);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"FIFTH", "FOURTH", "THIRD", "SECOND", "FIRST"})
+  public void findRankTest(String value) throws Exception {
+    //given
+    //when
+    LottoRank lottoRank = LottoRankPredicates.filterLottoRankWithString(value);
+    //then
+    assertEquals(lottoRank.name(), value);
   }
 }
