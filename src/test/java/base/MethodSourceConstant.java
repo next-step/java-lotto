7ceab6e357a883e01be75b5d010cec7ddbe008ec
gmoon <gmoon@rsupport.com>
2021-04-02 11:44:37 +0900

    feat 메소드 소스 상수 클래스 추가 - MethodSourceConstant.java

---
+++ src/test/java/base/MethodSourceConstant.java
@@ -0,0 +1,50 @@
+package base;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public final class MethodSourceConstant {
+  private static final String CLASS_PATH = "base.MethodSourceConstant";
+  public static final String CUSTOM_DELIMITER_ARGUMENTS = CLASS_PATH + "#customDelimiterArguments";
+  public static final String IS_POSITIVE_NUMBER_STRING_ARGUMENTS = CLASS_PATH + "#isPositiveNumberStringArguments";
+  public static final String SUM_ARGUMENTS = CLASS_PATH + "#sumArguments";
+
+  private MethodSourceConstant() {
+  }
+
+  static Stream<Arguments> customDelimiterArguments() {
+    return Stream.of(
+            Arguments.of("//;\n1;2;3", ",|:|;", true)
+            , Arguments.of("//*\n1*2*3", ",|:|*", true)
+            , Arguments.of("//\n1;2;3", ",|:", false)
+            , Arguments.of("1,2,3", ",|:", false)
+    );
+  }
+
+  static Stream<Arguments> isPositiveNumberStringArguments() {
+    return Stream.of(
+            Arguments.of("", true)
+            , Arguments.of("123", true)
+            , Arguments.of("+123", true)
+            , Arguments.of("-123", false)
+            , Arguments.of("-123asdf", false)
+            , Arguments.of("123asdf", false)
+    );
+  }
+
+  static Stream<Arguments> sumArguments() {
+    return Stream.of(
+            Arguments.of("", 0)
+            , Arguments.of("  ", 0)
+            , Arguments.of(null, 0)
+            , Arguments.of("1", 1)
+            , Arguments.of("2", 2)
+            , Arguments.of("1,2", 3)
+            , Arguments.of("1,2,3", 6)
+            , Arguments.of("1,2:3", 6)
+            , Arguments.of("//;\n1;2;3", 6)
+            , Arguments.of("//;\n1,2:3;4", 10)
+    );
+  }
+}
