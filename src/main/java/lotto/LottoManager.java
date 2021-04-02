28867f35a9fb94d0d0b3ecad669a5873d50685b6
Dion <ksundong@gmail.com>
2021-04-02 23:55:02 +0900

    refactor: try문 때문에 블록 제한이 걸릴 것 같아, 로직을 메서드로 추출

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -14,28 +14,32 @@ public final class LottoManager {
 
   public static void main(String[] args) {
     try (Scanner scanner = new Scanner(System.in)) {
-      InputView inputView = new InputView(scanner);
-
-      Money userMoney = new Money(inputView.inputMoney());
-      LottoCount lottoCount = new LottoCount(userMoney.divideBy(Lottos.PRICE).toInteger());
-      System.out.println();
-
-      LottoCount manualCount = new LottoCount(inputView.inputManualCount());
-      LottoCounts lottoCounts = new LottoCounts(lottoCount, manualCount);
-      System.out.println();
-
-      Lottos userLottos = new Lottos(lottoCount);
-      userLottos.addLottoList(inputView.inputManualLottoNumber(manualCount));
-      userLottos.addLottoList(lottoCounts.automaticLottos());
-      System.out.println();
-
-      Printer.printLottoCount(userLottos, manualCount);
-      Printer.printLottos(userLottos);
-
-      WinningLotto winningLotto = inputView.inputLastWinningNumber();
-      WinningStatistics winningStatistics = new WinningStatistics(userLottos, winningLotto);
-      Printer.printRank(winningStatistics);
-      Printer.printResult(winningStatistics, userMoney);
+      start(scanner);
     }
+  }
+
+  private static void start(Scanner scanner) {
+    InputView inputView = new InputView(scanner);
+
+    Money userMoney = new Money(inputView.inputMoney());
+    LottoCount lottoCount = new LottoCount(userMoney.divideBy(Lottos.PRICE).toInteger());
+    System.out.println();
+
+    LottoCount manualCount = new LottoCount(inputView.inputManualCount());
+    LottoCounts lottoCounts = new LottoCounts(lottoCount, manualCount);
+    System.out.println();
+
+    Lottos userLottos = new Lottos(lottoCount);
+    userLottos.addLottoList(inputView.inputManualLottoNumber(manualCount));
+    userLottos.addLottoList(lottoCounts.automaticLottos());
+    System.out.println();
+
+    Printer.printLottoCount(userLottos, manualCount);
+    Printer.printLottos(userLottos);
+
+    WinningLotto winningLotto = inputView.inputLastWinningNumber();
+    WinningStatistics winningStatistics = new WinningStatistics(userLottos, winningLotto);
+    Printer.printRank(winningStatistics);
+    Printer.printResult(winningStatistics, userMoney);
   }
 }
