9c0346552f3dcec01009df014be565b2ba4dfa96
Beenie93 <seongbeen93@naver.com>
2021-04-03 12:33:54 +0900

    test: a test unit to divide lotto ticket price into purchase amount

--- src/test/java/lotto/domain/LottoTicketPriceTest.java
+++ src/test/java/lotto/domain/LottoTicketPriceTest.java
@@ -13,4 +13,17 @@ public class LottoTicketPriceTest {
         LottoTicketPrice lottoTicketPrice = new LottoTicketPrice();
         assertThat(lottoTicketPrice.price()).isEqualTo(1_000);
     }
+
+    @Test
+    @DisplayName("구매금액에 따른 구매할 수 있는 로또 티켓 장수 구하기")
+    public void divide() throws Exception {
+        //given
+        LottoTicketPrice lottoTicketPrice = new LottoTicketPrice();
+
+        //when
+        int numberOfTicket = lottoTicketPrice.divide(1000);
+
+        //then
+        assertThat(numberOfTicket).isEqualTo(1);
+    }
 }
