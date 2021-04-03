eb977d16b09f692b9e5c744ed9540a9e16e05015
Beenie93 <seongbeen93@naver.com>
2021-04-03 14:26:48 +0900

    test: add test units to check bound and compare match count

--- src/test/java/lotto/domain/MatchedCountTest.java
+++ src/test/java/lotto/domain/MatchedCountTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
+import lotto.enums.WinningRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class MatchedCountTest {
 
@@ -12,5 +14,19 @@ public class MatchedCountTest {
     public void create() throws Exception {
         MatchedCount matchedCount = new MatchedCount(3);
         assertThat(matchedCount).isEqualTo(new MatchedCount(3));
+    }
+
+    @Test
+    @DisplayName("매칭된 개수가 1 ~ 6 범위가 아닐 경우 예외")
+    public void validateBound() throws Exception {
+        assertThatIllegalArgumentException().isThrownBy(() -> new MatchedCount(0));
+        assertThatIllegalArgumentException().isThrownBy(() -> new MatchedCount(7));
+    }
+
+    @Test
+    @DisplayName("매칭된 개수가 크거나 같은 경우")
+    public void isGreaterOrEqualThan() throws Exception {
+        assertThat(WinningRank.FIRST_PLACE.matchedCount().isGreaterOrEqualThan(5)).isTrue();
+        assertThat(WinningRank.SECOND_PLACE.matchedCount().isGreaterOrEqualThan(6)).isFalse();
     }
 }
