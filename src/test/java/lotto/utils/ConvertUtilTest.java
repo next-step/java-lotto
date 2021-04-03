244cfe3fc58bd117b31ce7be58dc5f4470e35f1c
Beenie93 <seongbeen93@naver.com>
2021-04-03 21:56:38 +0900

    refactor: remove toIntegers() and refactor

--- src/test/java/lotto/utils/ConvertUtilTest.java
+++ src/test/java/lotto/utils/ConvertUtilTest.java
@@ -3,9 +3,6 @@ package lotto.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConvertUtilTest {
@@ -13,13 +10,7 @@ public class ConvertUtilTest {
     @Test
     @DisplayName("정수로 변환")
     public void toInt() throws Exception {
-        assertThat(ConvertUtil.toInt("1")).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("정수 리스트로 변환")
-    public void toIntegers() throws Exception {
-        List<Integer> integers = Arrays.asList(1, 2, 3);
-        assertThat(ConvertUtil.toIntegers(Arrays.asList("1", "2", "3"))).isEqualTo(integers);
+        String input = "1";
+        assertThat(ConvertUtil.toIntOrThrow(input, () -> new IllegalArgumentException(input + "(은)는 정수로 변환할 수 없는 문자입니다."))).isEqualTo(1);
     }
 }
