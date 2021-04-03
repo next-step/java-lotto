71dd9138c9e14ac24d2354b20c2e43bd78bde561
Beenie93 <seongbeen93@naver.com>
2021-04-03 14:28:07 +0900

    feat: implement methods to check bound and compare match count

--- src/main/java/lotto/domain/MatchedCount.java
+++ src/main/java/lotto/domain/MatchedCount.java
@@ -3,18 +3,40 @@ package lotto.domain;
 import java.util.Objects;
 
 public class MatchedCount {
+    private static final int BOUND_MIN = 1;
+    private static final int BOUND_MAX = 6;
+    private static final String CHECK_BOUND = String.format("매칭된 개수는 %d ~ %d의 범위여야 합니다.", BOUND_MIN, BOUND_MAX);
     private final int matchedCount;
+
+    public MatchedCount() {
+        this(BOUND_MIN);
+    }
 
     public MatchedCount(long matchedCount) {
         this((int) matchedCount);
     }
 
     public MatchedCount(int matchedCount) {
+        validateBound(matchedCount);
         this.matchedCount = matchedCount;
+    }
+
+    private void validateBound(int matchedCount) {
+        if (BOUND_MIN > matchedCount || matchedCount > BOUND_MAX) {
+            throw new IllegalArgumentException(CHECK_BOUND);
+        }
     }
 
     public int matchedCount() {
         return matchedCount;
+    }
+
+    public boolean isGreaterOrEqualThan(MatchedCount matchedCount) {
+        return !matchedCount.isGreaterOrEqualThan(this.matchedCount);
+    }
+
+    public boolean isGreaterOrEqualThan(int matchedCount) {
+        return this.matchedCount >= matchedCount;
     }
 
     @Override
