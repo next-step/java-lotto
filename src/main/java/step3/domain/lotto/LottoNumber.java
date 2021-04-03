3cc9c1b1fcb8a1c731a82caa3abfd98708a7a819
김우재 <kwj1270@naver.com>
2021-04-03 21:40:40 +0900

    feat:LottoNumber 인스턴스가 캐싱된 값을 반환하는 기능 구현

--- src/main/java/step3/domain/lotto/LottoNumber.java
+++ src/main/java/step3/domain/lotto/LottoNumber.java
@@ -2,12 +2,23 @@ package step3.domain.lotto;
 
 import step3.exception.LottoNumberOutOfRangeException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class LottoNumber {
 
+    private static final Map<Integer, LottoNumber> CACHE;
     private static final int MINIMUM = 1;
     private static final int MAXIMUM = 45;
 
     private final int lottoNumber;
+
+    static {
+        CACHE = new HashMap<>();
+        for (int i = MINIMUM; i <= MAXIMUM; i++) {
+            CACHE.put(i, valueOf(i));
+        }
+    }
 
     private LottoNumber(int lottoNumber) {
         validateNumber(lottoNumber);
@@ -15,8 +26,12 @@ public final class LottoNumber {
     }
 
     public static final LottoNumber valueOf(int lottoNumber) {
+        if (CACHE.containsKey(lottoNumber)) {
+            return CACHE.get(lottoNumber);
+        }
         return new LottoNumber(lottoNumber);
     }
+
 
     private final void validateNumber(int lottoNumber) {
         if (lottoNumber < MINIMUM || MAXIMUM < lottoNumber) {
