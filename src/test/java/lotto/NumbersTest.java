84a2a538cb7666ed46f6d5ca88685a17fd891d53
jaystar13 <astarisk2@gmail.com>
2021-04-03 16:24:34 +0900

    test: 로또 번호 6개이외 입력 테스트 추가

--- src/test/java/lotto/NumbersTest.java
+++ src/test/java/lotto/NumbersTest.java
@@ -10,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NumbersTest {
+    @DisplayName("당첨된 번호의 총숫자를 반환한다")
     @Test
     void create() {
         Numbers lotto = new Numbers(Arrays.asList(1, 2, 3, 4, 5, 6));
@@ -20,9 +21,10 @@ public class NumbersTest {
 
     @DisplayName("6개 이외의 숫자 입력시 예외를 반환하다.")
     @Test
-    void invalid_5개숫자() {
+    void invalid_6이외숫자() {
         assertThatThrownBy(() -> {
             new Numbers(Arrays.asList(1, 2, 3, 4, 5));
+            new Numbers(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
