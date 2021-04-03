00018854e1937722853f6b96090fb68ca11070d8
dailyworker <workingssu@gmail.com>
2021-04-04 03:02:42 +0900

    refactor : LottoRankPredicates 삭제 - LottoRankFilter를 통한 오버라이딩으로 filter 처리 진행

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,15 +1,25 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
-import lotto.util.LottoRankPredicates;
 
-public enum LottoRank {
+public enum LottoRank implements LottoRankFilter {
 
   NONE(Integer.MIN_VALUE, 0),
   FIFTH(3, 5000),
   FOURTH(4, 50000),
-  THIRD(5, 1500000),
-  SECOND(5, 30000000),
+  THIRD(5, 1500000) {
+    @Override
+    public boolean filter(boolean bonusBall, Money winnerMoney) {
+      return !bonusBall && isCorrectWinnerMoney(winnerMoney);
+    }
+  },
+  SECOND(5, 30000000) {
+    @Override
+    public boolean filter(boolean bonusBall, Money winnerMoney) {
+      return bonusBall && isCorrectWinnerMoney(winnerMoney);
+    }
+  },
   FIRST(6, 2000000000);
 
   private final int matchCount;
@@ -24,16 +34,32 @@ public enum LottoRank {
     return this.matchCount == matchCount;
   }
 
+  public boolean isCorrectWinnerMoney(Money winnerMoney) {
+    return this.winnerMoney == winnerMoney;
+  }
+
   public Money getMatchedLottoWinningMoney(int value) {
     return winnerMoney.multiple(value);
   }
 
+  @Override
+  public boolean filter(boolean bonusBall, Money winnerMoney) {
+    return isCorrectMatchCount(matchCount);
+  }
+
   public static LottoRank findRank(String winnerRank) {
-    return LottoRankPredicates.filterLottoRankByName(winnerRank);
+    return Arrays.stream(LottoRank.values())
+        .filter(rank -> rank.name().equals(winnerRank))
+        .findAny()
+        .orElse(LottoRank.NONE);
   }
 
   public static LottoRank valueOf(int matchCount, boolean bonusBall) {
-    return LottoRankPredicates.filter(matchCount, bonusBall);
+    return Arrays.stream(LottoRank.values())
+        .filter(rank -> rank.isCorrectMatchCount(matchCount))
+        .filter(rank -> rank.filter(bonusBall, rank.winnerMoney))
+        .findAny()
+        .orElse(LottoRank.NONE);
   }
 
   public static LottoRank matches(List<Number> winningNumbers, List<Number> holdingLottoNumbers, Number bonusBall) {
