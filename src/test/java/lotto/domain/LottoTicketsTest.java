d8f0ca9064da1883ecc494d7ca4cc90eed9e87d3
Beenie93 <seongbeen93@naver.com>
2021-04-03 09:42:05 +0900

    test: add a test unit to check if lotto ticket has bonus ball

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,5 +17,14 @@ public class LottoTicketsTest {
         lottoTickets.add(new LottoTicket(new LottoNumbers()));
         lottoTickets.add(new LottoTicket(new LottoNumbers()));
         assertThat(lottoTickets.lottoTickets()).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("로또 티켓이 보너스 볼을 포함하는 지 여부")
+    public void hasBonusNumber() throws Exception {
+        LottoNumbers lottoNumbers = LottoNumbers.from(Arrays.asList(1,2,3,4,5,6));
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        assertThat(lottoTicket.hasBonusNumber(new BonusBall(1))).isTrue();
+        assertThat(lottoTicket.hasBonusNumber(new BonusBall(7))).isFalse();
     }
 }
