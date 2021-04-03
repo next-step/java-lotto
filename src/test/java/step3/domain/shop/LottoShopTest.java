b9922d077426a5adff7029ba34fcb30aa7041e18
김우재 <kwj1270@naver.com>
2021-04-03 23:20:36 +0900

    test:LottoShop 인스턴스 로또 반환 여부 테스트 작성

--- src/test/java/step3/domain/shop/LottoShopTest.java
+++ src/test/java/step3/domain/shop/LottoShopTest.java
@@ -2,8 +2,12 @@ package step3.domain.shop;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step3.domain.lotto.Lotto;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoShopTest {
 
@@ -16,6 +20,23 @@ class LottoShopTest {
 
         // then
         assertThat(lottoShop).isNotNull();
+    }
+
+    @DisplayName("LottoShop 인스턴스 로또 반환 여부 테스트")
+    @Test
+    void 반환_로또() {
+        // given
+        int money = 1000;
+
+        // when
+        LottoShop lottoShop = LottoShop.getInstance();
+        List<Lotto> lottos = lottoShop.getLottos(money);
+
+        // then
+        assertAll(
+                () -> assertThat(lottos).isNotNull(),
+                () -> assertThat(lottos.size()).isEqualTo(1)
+        );
     }
 
 }
