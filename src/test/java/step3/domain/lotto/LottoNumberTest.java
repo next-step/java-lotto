c009a9a2fd0f7db01eeaf86eaa1e71b3ea587326
김우재 <kwj1270@naver.com>
2021-04-03 21:14:38 +0900

    test:LottoNumber 인스턴스에 0이하의 수 입력시 예외처리 여부 테스트

--- src/test/java/step3/domain/lotto/LottoNumberTest.java
+++ src/test/java/step3/domain/lotto/LottoNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumberTest {
 
@@ -18,5 +19,21 @@ class LottoNumberTest {
 
         // then
         assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("LottoNumber 인스턴스에 0이하의 수 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_0이하의수() {
+        // given
+        int fistValue = 0;
+        int secondValue = -1;
+
+        // when
+        assertThatThrownBy(()->{
+            LottoNumber.valueOf(fistValue);
+            LottoNumber.valueOf(secondValue);
+        }).isInstanceOf(LottoNumberOutOfRangeException.class)
+                .hasMessageContaining("로또의 범위가 아닌 숫자가 입력되었습니다.");
+
     }
 }
