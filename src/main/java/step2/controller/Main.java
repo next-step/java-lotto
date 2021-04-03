1212c7caeff845d149fba75d37689cd647f435bc
abcdefg <choiyurim@naver.com>
2021-04-03 23:27:30 +0900

    feat(controller/Main): add controller/Main

--- src/main/java/step2/controller/Main.java
+++ src/main/java/step2/controller/Main.java
@@ -6,26 +6,42 @@ import step2.view.ResultView;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class Main {
+    public static final int LOTTO_NUMBER_RANGE_MIN = 1;
+    public static final int LOTTO_NUMBER_RANGE_MAX = 46;
 
     public static void main(String[] args) {
         int lottoPurchaseAmount = InputView.getLottoPurchaseAmount();
-        Market market = new Market(lottoPurchaseAmount, Collections.EMPTY_LIST);
-        Lottos lottos = market.getLottos();
 
+        List<Integer> lottoAllNumberInRange = getLottoAllNumberInRange();
+        GenerateLotto generateLotto = new GenerateLotto(lottoAllNumberInRange, lottoPurchaseAmount);
+        Lottos lottos = new Lottos(generateLotto.getGeneratedLottos());
         ResultView.printLottos(lottos);
 
         String winnersNumbers = InputView.getWinnersNumber();
-        List<Integer> lottoWinnerNumbers = Arrays.stream(winnersNumbers.split(","))
-                .map(winnersNumber -> Integer.parseInt(winnersNumber))
+        Set<LottoNumber> lottoNumbers = getLottoNumbers(winnersNumbers);
+        Lotto winnerLotto = new Lotto(lottoNumbers);
+        Map<Integer, Long> matchResults = lottos.getMatchResults(winnerLotto);
+        ResultView.printMatchResults(matchResults);
+
+        LottoBenefitCalculator lottoBenefitCalculator =
+                new LottoBenefitCalculator(lottoPurchaseAmount, matchResults);
+        ResultView.printBenefitResult(lottoBenefitCalculator.resultBenefit());
+    }
+
+    private static List<Integer> getLottoAllNumberInRange() {
+        return IntStream.range(LOTTO_NUMBER_RANGE_MIN, LOTTO_NUMBER_RANGE_MAX)
+                .boxed()
                 .collect(Collectors.toList());
+    }
 
-        LottoNumberGenerator lottoNumberGenerator = new LottoFixedLottoNumberGenerator(lottoWinnerNumbers);
-        Map<Integer, Long> matchResults = lottos.getMatchResults(new Lotto(lottoNumberGenerator));
-        LottoMatchResult lottoMatchResult = new LottoMatchResult(lottoPurchaseAmount, matchResults);
-
-        ResultView.printMatchResults(lottoMatchResult);
+    private static Set<LottoNumber> getLottoNumbers(String winnersNumbers) {
+        return Arrays.stream(winnersNumbers.split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
