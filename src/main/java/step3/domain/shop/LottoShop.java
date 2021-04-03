1ca1db498fb49512b4550a4a84daafcc9a25495c
김우재 <kwj1270@naver.com>
2021-04-03 22:53:23 +0900

    test:LottoShop 인스턴스 생성 여부 테스트 작성

---
+++ src/main/java/step3/domain/shop/LottoShop.java
@@ -0,0 +1,4 @@
+package step3.domain.shop;
+
+public class LottoShop {
+}
---
+++ src/test/java/step3/domain/shop/LottoShopTest.java
@@ -0,0 +1,22 @@
+package step3.domain.shop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoShopTest {
+
+    @DisplayName("LottoShop 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+
+        // when
+        LottoShop lottoShop = LottoShop.getInstace();
+
+        // then
+        assertThat(lottoShop).isNotNull();
+    }
+
+}
