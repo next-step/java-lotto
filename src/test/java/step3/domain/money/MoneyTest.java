191e500eb5f3064499c0acd2339e806139b977ec
김우재 <kwj1270@naver.com>
2021-04-03 22:28:20 +0900

    test:Money 인스턴스 음수 입력시 예외처리 여부 테스트 작성

--- src/test/java/step3/domain/money/MoneyTest.java
+++ src/test/java/step3/domain/money/MoneyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
@@ -19,5 +20,18 @@ class MoneyTest {
 
         // then
         assertThat(money).isNotNull();
+    }
+
+    @DisplayName("Money 인스턴스 음수 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int amount = -1;
+
+        // when
+        assertThatThrownBy(() -> Money.valueOf(amount))
+                .isInstanceOf(InputNegativeAmountException.class)
+                .hasMessageContaining("음수값의 돈이 입력되었습니다.");
+
     }
 }
