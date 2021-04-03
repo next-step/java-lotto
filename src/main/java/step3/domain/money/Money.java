c9f6975e3bdf395d039aa9c564244006c3471980
김우재 <kwj1270@naver.com>
2021-04-03 22:25:41 +0900

    feat:Money 인스턴스 생성 기능 구현

--- src/main/java/step3/domain/money/Money.java
+++ src/main/java/step3/domain/money/Money.java
@@ -1,4 +1,15 @@
 package step3.domain.money;
 
-public class Money {
+public final class Money {
+
+    private final int money;
+
+    private Money (int money) {
+        this.money = money;
+    }
+
+    public static Money valueOf(int money) {
+        return new Money(money);
+    }
+
 }
