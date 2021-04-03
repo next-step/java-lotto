acee5b8e664491dab21015479d1a0b0f9045926b
jaystar13 <astarisk2@gmail.com>
2021-04-03 15:57:14 +0900

    refactor: 로또 번호 정적 팩토리 메소드 적용

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -1,15 +1,32 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Number {
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+
+    public static final Map<Integer, Number> lottoNumber = new HashMap<>();
     private final int number;
 
-    public Number(int number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("1부터 45까지 숫자만 입력해 주세요.");
+    static {
+        for (int i = LOTTO_NUMBER_MIN; i <= LOTTO_NUMBER_MAX; i++) {
+            lottoNumber.put(i, new Number(i));
         }
+    }
+
+    private Number(int number) {
         this.number = number;
+    }
+
+    public static Number of(int number) {
+        return Optional.ofNullable(
+                lottoNumber.get(number))
+                .orElseThrow(() -> new IllegalArgumentException(
+                        String.format("%d부터 %d까지 숫자만 입력해 주세요.", LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX)));
     }
 
     @Override
