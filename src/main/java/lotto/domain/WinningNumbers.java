ba57b2c4c2d67bdd30a028377e475cc500afa0ca
Beenie93 <seongbeen93@naver.com>
2021-04-03 19:38:47 +0900

    refactor: remove match()

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -12,8 +12,6 @@ public class WinningNumbers {
     private static final int NUMBER_LENGTH = 6;
     private static final int BOUND_MAX = 45;
     private static final int BOUND_MIN = 1;
-    private static final int PLUS_COUNT = 1;
-    private static final int ZERO_COUNT = 0;
     private static final String CHECK_LENGTH = String.format("당첨 번호의 길이는 %d(이)여야 합니다.", NUMBER_LENGTH);
     private static final String CHECK_BOUND = String.format("당첨 번호는 %d ~ %d 범위의 값이어야 합니다.", BOUND_MIN, BOUND_MAX);
     private static final String CHECK_DUPLICATION = "중복되는 숫자가 포함되어 있는지 확인해주세요.";
@@ -36,11 +34,9 @@ public class WinningNumbers {
     }
 
     public MatchedCount countMatchingNumbers(List<LottoNumber> lottoNumbers) {
-        long matchedCount = 0L;
-
-        for (LottoNumber lottoNumber : lottoNumbers) {
-            matchedCount += match(lottoNumber);
-        }
+        long matchedCount = lottoNumbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
 
         return new MatchedCount(matchedCount);
     }
@@ -49,14 +45,6 @@ public class WinningNumbers {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException(CHECK_DUPLICATION);
         }
-    }
-
-    private int match(LottoNumber lottoNumber) {
-        if (winningNumbers.contains(lottoNumber)) {
-            return PLUS_COUNT;
-        }
-
-        return ZERO_COUNT;
     }
 
     private void validate(List<LottoNumber> winningNumbers) {
