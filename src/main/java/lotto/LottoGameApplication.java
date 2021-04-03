3e854db7868f4eaceba9ea892991f9f587388206
jaystar13 <astarisk2@gmail.com>
2021-04-03 16:02:40 +0900

    feat: 수동번호 입력 추가

--- src/main/java/lotto/LottoGameApplication.java
+++ src/main/java/lotto/LottoGameApplication.java
@@ -8,18 +8,17 @@ import lotto.view.HitResultView;
 import lotto.view.InputView;
 import lotto.view.LottoGamesView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGameApplication {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         inputView.inputPayMoney();
+        List<Numbers> manualNumbers = inputView.inputManualGameNumbers(inputView.inputManualGameCount());
 
         Money payMoney = new Money(inputView.getPayMoney());
         LottoShop lottoShop = new LottoShop();
-        List<Numbers> manualGame = new ArrayList<>();
-        lottoShop.purchase(payMoney, manualGame);
+        lottoShop.purchase(payMoney, manualNumbers);
 
         LottoGamesView lottoGamesView = new LottoGamesView(lottoShop);
         lottoGamesView.printLottoGamesNumber();
