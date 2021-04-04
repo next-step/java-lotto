b82fc8897a6119dc819a6374d0c21c582ef96a1b
Beenie93 <seongbeen93@naver.com>
2021-04-05 01:42:30 +0900

    test: implement a test unit to check duplication

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoNumbersTest {
 
@@ -22,5 +23,11 @@ public class LottoNumbersTest {
         LottoNumbers lottoNumbers = LottoNumbers.from(1, 2, 3, 4, 5, 6);
         assertThat(lottoNumbers.contains(new BonusBall(1))).isTrue();
         assertThat(lottoNumbers.contains(new BonusBall(7))).isFalse();
+    }
+
+    @Test
+    @DisplayName("로또 번호 중복일 시 예외")
+    public void checkDuplication() throws Exception {
+        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumbers.from(1, 2, 3, 4, 5, 5));
     }
 }
