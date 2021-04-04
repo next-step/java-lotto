2aa95ec8a00b0f65031783fdbb1e47be73c39545
abcdefg <choiyurim@naver.com>
2021-04-04 17:01:36 +0900

    refactor(View): git 제대로 커밋안된거 적용

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -2,10 +2,12 @@ package step2.view;
 
 
 import step2.domain.Lotto;
-import step2.domain.LottoMatchResult;
+import step2.domain.LottoMatchResultCount;
 import step2.domain.Lottos;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class ResultView {
 
@@ -20,23 +22,33 @@ public class ResultView {
 
     private static void printLottoNumbers(List<Lotto> lottos) {
         lottos.stream()
-                .map(Lotto::getLottoNumber)
+                .map(Lotto::getLottoNumbers)
                 .forEach(System.out::println);
     }
 
-    public static void printMatchResults(LottoMatchResult matchResults) {
-        matchResults.resultMessages()
-                .forEach(System.out::println);
+    public static void printMatchResults(Map<Integer, Long> matchResults) {
+        System.out.println("\n당첨 통계\n---------");
+        IntStream.range(3, 7)
+                .forEach(index -> {
+                    int cash = getCash(index);
+                    Long amount = matchResults.getOrDefault(index, 0L);
 
-        printBenefitResult(matchResults.resultBenefit());
+                    System.out.println(
+                            index + "개 일치 (" + cash + "원)- " + amount + "개");
+                });
     }
 
-    private static void printBenefitResult(double benefitPercentage) {
+    private static int getCash(int index) {
+        LottoMatchResultCount lottoMatchResultCount = LottoMatchResultCount.of(index);
 
+        return lottoMatchResultCount.getRewardCash(index);
+    }
+
+    public static void printBenefitResult(double benefitPercentage) {
         System.out.println(
                 "총 수익률은 " + benefitPercentage + "입니다.(기준이 1이기 때문에 결과적으로 "
-                + getGainOrLoss(benefitPercentage)
-                + "라는 의미임)");
+                        + getGainOrLoss(benefitPercentage)
+                        + "라는 의미임)");
     }
 
     private static String getGainOrLoss(double benefit) {
