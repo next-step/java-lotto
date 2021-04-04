c8925181eb63261e40c4e17300db1dad68c68806
김우재 <kwj1270@naver.com>
2021-04-04 12:56:46 +0900

    test:WinningCheckMachine 인스턴스가 WinningResult 인스턴스 반환 여부 테스트작성

--- src/test/java/step3/domain/winning/WinningCheckMachineTest.java
+++ src/test/java/step3/domain/winning/WinningCheckMachineTest.java
@@ -17,4 +17,15 @@ class WinningCheckMachineTest {
         assertThat(winningCheckMachine).isNotNull();
     }
 
+    @DisplayName("WinningCheckMachine 인스턴스가 WinningResult 인스턴스 반환 여부 테스트")
+    @Test
+    void 반환() {
+        // when
+        WinningCheckMachine winningCheckMachine = WinningCheckMachine.getInstance();
+        WinningResult winningResult = winningCheckMachine.getWinningResult();
+
+        // then
+        assertThat(winningResult).isNotNull();
+    }
+
 }
