67c61d5874a9b5af97d42083d4f278ebc3b68f35
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-04 21:25:13 +0900

    test : 자동구매, 수동구매 케이스 별 테스트 검증로직 추가  - TODO 삭제

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -13,10 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoControllerTest {
 
     @Test
-    @DisplayName("로또 구입 테스트")
-    void purchaseLotto() {
+    @DisplayName("로또 구입 테스트 자동 Only")
+    void purchaseLotto_AutoOnly() {
         // given
-        LottoTicketPurchaseRequest request = new LottoTicketPurchaseRequest(14000, new ArrayList<>()); // TODO: 수동구매정보 추가입력 테스트
+        LottoTicketPurchaseRequest request = new LottoTicketPurchaseRequest(14000, new ArrayList<>());
         LottoPurchaseController controller = new LottoPurchaseController();
 
         // when
@@ -24,14 +24,50 @@ class LottoControllerTest {
 
         // then
         assertThat(14).isEqualTo(response.getAutoPurchaseCount());
+        assertThat(0).isEqualTo(response.getManualPurchaseCount());
         assertThat(14).isEqualTo(response.getLottoNumberList().size());
+    }
+
+    @Test
+    @DisplayName("로또 구입 테스트 수동 Only")
+    void purchaseLotto_ManualOnly() {
+        // given
+        List<String> manualLottoPurchaseNumberList = Arrays.asList("1, 2, 3, 4, 5, 6", "2, 3, 4, 5, 6, 7", "3, 4, 5, 6, 7, 8");
+        LottoTicketPurchaseRequest request = new LottoTicketPurchaseRequest(3000, manualLottoPurchaseNumberList);
+        LottoPurchaseController controller = new LottoPurchaseController();
+
+        // when
+        LottoTicketPurchaseResponse response = controller.purchaseLottoTicket(request);
+
+        // then
+        assertThat(0).isEqualTo(response.getAutoPurchaseCount());
+        assertThat(3).isEqualTo(response.getManualPurchaseCount());
+        assertThat(3).isEqualTo(response.getLottoNumberList().size());
+    }
+
+    @Test
+    @DisplayName("로또 구입 테스트")
+    void purchaseLotto() {
+        // given
+        List<String> manualLottoPurchaseNumberList = Arrays.asList("1, 2, 3, 4, 5, 6", "2, 3, 4, 5, 6, 7", "3, 4, 5, 6, 7, 8");
+        LottoTicketPurchaseRequest request = new LottoTicketPurchaseRequest(5000, manualLottoPurchaseNumberList);
+        LottoPurchaseController controller = new LottoPurchaseController();
+
+        // when
+        LottoTicketPurchaseResponse response = controller.purchaseLottoTicket(request);
+
+        // then
+        assertThat(2).isEqualTo(response.getAutoPurchaseCount());
+        assertThat(3).isEqualTo(response.getManualPurchaseCount());
+        assertThat(5).isEqualTo(response.getLottoNumberList().size());
     }
 
     @Test
     @DisplayName("로또 구입 시 로또 한 건의 구성 번호는 모두 달라야 한다.")
     void purchaseLotto_lottoNumber_distinct() {
         // given
-        LottoTicketPurchaseRequest request = new LottoTicketPurchaseRequest(14000, new ArrayList<>()); // TODO: 수동구매정보 추가입력 테스트
+        List<String> manualLottoPurchaseNumberList = Arrays.asList("1, 2, 3, 4, 5, 6", "2, 3, 4, 5, 6, 7", "3, 4, 5, 6, 7, 8");
+        LottoTicketPurchaseRequest request = new LottoTicketPurchaseRequest(14000, manualLottoPurchaseNumberList);
         LottoPurchaseController controller = new LottoPurchaseController();
 
         // when
