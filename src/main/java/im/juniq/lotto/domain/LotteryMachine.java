4b69c513e6770f27e60c40a8191bbd57bb92fa42
juniq <juniqlim@gmail.com>
2021-04-04 22:39:25 +0900

    feat: 자동로또 수동로또 함께 구입

--- src/main/java/im/juniq/lotto/domain/LotteryMachine.java
+++ src/main/java/im/juniq/lotto/domain/LotteryMachine.java
@@ -1,5 +1,7 @@
 package im.juniq.lotto.domain;
 
+import java.util.List;
+
 public class LotteryMachine {
 	private final Lottoes lottoes;
 	private final Price price;
@@ -11,6 +13,15 @@ public class LotteryMachine {
 	public LotteryMachine(int price, ShuffleStrategy shuffleStrategy) {
 		this.price = new Price(price);
 		lottoes = new Lottoes(this.price.numberOfLottoPurchased() , shuffleStrategy);
+	}
+
+	public LotteryMachine(int price, List<String> manualLottoes) {
+		this(price, manualLottoes, new NormalShuffleStrategy());
+	}
+
+	public LotteryMachine(int price, List<String> manualLottoes, ShuffleStrategy shuffleStrategy) {
+		this.price = new Price(price);
+		lottoes = new Lottoes(this.price.numberOfLottoPurchased(), shuffleStrategy, manualLottoes);
 	}
 
 	public Lottoes lottoes() {
