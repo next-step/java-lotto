ab5154401917b415ebe511949db29b47da7890c6
김우재 <kwj1270@naver.com>
2021-04-04 09:13:36 +0900

    feat:셔플관련 인터페이스 및 구현 클래스 작성

---
+++ src/main/java/step3/strategy/LottoRandomShuffleStrategy.java
@@ -0,0 +1,27 @@
+package step3.strategy;
+
+import step2.domain.lotto.LottoNumber;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public final class LottoRandomShuffleStrategy implements LottoShuffleStrategy {
+
+    private static LottoRandomShuffleStrategy instance;
+
+    private LottoRandomShuffleStrategy() { }
+
+    public static final LottoRandomShuffleStrategy getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new LottoRandomShuffleStrategy();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void shuffle(List<LottoNumber> lottoNumbers) {
+        Collections.shuffle(lottoNumbers);
+    }
+}
---
+++ src/main/java/step3/strategy/LottoShuffleStrategy.java
@@ -0,0 +1,11 @@
+package step3.strategy;
+
+import step2.domain.lotto.LottoNumber;
+
+import java.util.List;
+
+public interface LottoShuffleStrategy {
+
+    void shuffle(List<LottoNumber> lottoNumbers);
+
+}
