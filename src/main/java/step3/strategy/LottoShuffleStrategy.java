773dd24c88899f4b985ee8b3e70791884a4252d1
김우재 <kwj1270@naver.com>
2021-04-04 21:35:36 +0900

    feat:@FunctionalInterface 추가

--- src/main/java/step3/strategy/LottoShuffleStrategy.java
+++ src/main/java/step3/strategy/LottoShuffleStrategy.java
@@ -1,10 +1,10 @@
 package step3.strategy;
 
-
 import step3.domain.lotto.LottoNumber;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface LottoShuffleStrategy {
 
     void shuffle(List<LottoNumber> lottoNumbers);
