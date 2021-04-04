b88ce58c4954b241bcd843c33cd37287796bcfbb
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-04 20:34:41 +0900

    refactor : LottoManualService 클래스 구조변경  - 수동로또 구매기능만 가지도록 역할 축소

--- src/main/java/lotto/service/LottoManualService.java
+++ src/main/java/lotto/service/LottoManualService.java
@@ -1,20 +1,14 @@
 package lotto.service;
 
-import lotto.domain.LottoConstant;
 import lotto.domain.LottoTicket;
+import lotto.domain.LottoTicketType;
 
 import java.util.List;
 
 public class LottoManualService {
 
-    public LottoTicket purchaseLottoTicket(int purchaseAmount, List<String> manualLottoPurchaseNumberList) {
-        validatePurchasable(purchaseAmount, manualLottoPurchaseNumberList);
-        return new LottoTicket(manualLottoPurchaseNumberList);
+    public LottoTicket purchaseLottoTicket(List<String> manualLottoPurchaseNumberList) {
+        return new LottoTicket(manualLottoPurchaseNumberList, LottoTicketType.MANUAL);
     }
 
-    private void validatePurchasable(int purchaseAmount, List<String> manualLottoPurchaseNumberList) {
-        if (purchaseAmount < manualLottoPurchaseNumberList.size() * LottoConstant.LOTTO_PRICE) {
-            throw new IllegalArgumentException("구입금액이 부족합니다. 금액과 구매장수를 확인해 주세요.");
-        }
-    }
 }
--- src/test/java/lotto/service/LottoManualServiceTest.java
+++ src/test/java/lotto/service/LottoManualServiceTest.java
@@ -3,24 +3,31 @@ package lotto.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoManualServiceTest {
 
     @Test
-    @DisplayName("수동로또 - 구매금액 부족")
-    void purchaseAmount() {
+    @DisplayName("수동로또 - 구매번호 미입력 : null")
+    void nullInput() {
         // given
         LottoManualService lottoManualService = new LottoManualService();
-        int purchaseAmount = 2500;
-        List<String> manualLottoPurchaseNumberList = Arrays.asList("1, 2, 3, 4, 5, 6", "2, 3, 4, 5, 6, 7", "4, 5, 6, 7, 8, 9");
 
         // when then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> lottoManualService.purchaseLottoTicket(purchaseAmount, manualLottoPurchaseNumberList))
-                .withMessageMatching("구입금액이 부족합니다. 금액과 구매장수를 확인해 주세요.");
+                .isThrownBy(() -> lottoManualService.purchaseLottoTicket(null));
+    }
+
+    @Test
+    @DisplayName("수동로또 - 구매번호 미입력 : 빈 리스트")
+    void emptyInput() {
+        // given
+        LottoManualService lottoManualService = new LottoManualService();
+
+        // when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> lottoManualService.purchaseLottoTicket(new ArrayList<>()));
     }
 }
