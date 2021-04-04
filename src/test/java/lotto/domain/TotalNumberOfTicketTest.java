b858570c54f0dfb697cd33bf59c32f1c589a7e62
Beenie93 <seongbeen93@naver.com>
2021-04-05 00:59:26 +0900

    test: implement a test unit to get auto number of ticket

--- src/test/java/lotto/domain/TotalNumberOfTicketTest.java
+++ src/test/java/lotto/domain/TotalNumberOfTicketTest.java
@@ -17,14 +17,7 @@ public class TotalNumberOfTicketTest {
     @Test
     @DisplayName("구매 금액으로 구입할 수 있는 티켓 장수 구하기")
     public void numberOfTicket() throws Exception {
-        //given
-        PurchaseAmount purchaseAmount = new PurchaseAmount(1_000);
-        TicketOffice ticketOffice = new TicketOffice(new LottoTicketPrice());
-
-        //when
-        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(purchaseAmount, ticketOffice.lottoTicketPrice());
-
-        //then
+        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(new PurchaseAmount(1_000), new LottoTicketPrice());
         assertThat(totalNumberOfTicket.numberOfTicket()).isEqualTo(1);
     }
 
@@ -32,14 +25,26 @@ public class TotalNumberOfTicketTest {
     @DisplayName("구매금액에 따른 구매할 수 있는 로또 티켓 장수 구하기")
     public void count() throws Exception {
         //given
-        LottoTicketPrice lottoTicketPrice = new LottoTicketPrice();
-        PurchaseAmount purchaseAmount = new PurchaseAmount(1_000);
-        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(purchaseAmount, lottoTicketPrice);
+        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(new PurchaseAmount(1_000), new LottoTicketPrice());
 
         //when
         int count = totalNumberOfTicket.count();
 
         //then
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("총 티켓 수 - 수동 티켓 수 = 자동 티켓")
+    public void minus() throws Exception {
+        //given
+        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(new PurchaseAmount(5_000), new LottoTicketPrice());
+        ManualNumberOfTicket manualNumberOfTicket = new ManualNumberOfTicket(3, totalNumberOfTicket);
+
+        //when
+        int autoNumberOfTicket = totalNumberOfTicket.minus(manualNumberOfTicket);
+
+        //then
+        assertThat(autoNumberOfTicket).isEqualTo(2);
     }
 }
