60354379b08bb03c73658330aba15d8cf8c34e55
Beenie93 <seongbeen93@naver.com>
2021-04-05 01:56:02 +0900

    faet: implement a method to check positive

--- src/main/java/lotto/domain/ManualNumberOfTicket.java
+++ src/main/java/lotto/domain/ManualNumberOfTicket.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 public class ManualNumberOfTicket {
     private static final String CHECK_MANUAL_NUMBER_OF_TICKET = "입력하신 구매 티켓 수 %s가 숫자인지 확인해주세요.";
     private static final String CHECK_BOUND = "입력하신 구매 티켓 수가 구매 가능한 티켓 수보다 많습니다.";
+    private static final String CHECK_POSITIVE = "입력하신 구매 티켓 수가 음수입니다.";
+    private static final int MIN = 0;
     private final TotalNumberOfTicket totalNumberOfTicket;
     private final int manualNumberOfTicket;
 
@@ -16,8 +18,15 @@ public class ManualNumberOfTicket {
 
     public ManualNumberOfTicket(int manualNumberOfTicket, TotalNumberOfTicket totalNumberOfTicket) {
         validateBound(manualNumberOfTicket, totalNumberOfTicket.count());
+        validatePositive(manualNumberOfTicket);
         this.manualNumberOfTicket = manualNumberOfTicket;
         this.totalNumberOfTicket = totalNumberOfTicket;
+    }
+
+    private void validatePositive(int manualNumberOfTicket) {
+        if (manualNumberOfTicket < MIN) {
+            throw new IllegalArgumentException(CHECK_POSITIVE);
+        }
     }
 
     private void validateBound(int manualNumberOfTicket, int totalNumberOfTicket) {
