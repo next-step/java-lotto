2380b4dc22a2179b587e62068d76d1d7d61e3c0a
김우재 <kwj1270@naver.com>
2021-04-04 14:43:40 +0900

    test:WinningLotto 인스턴스 null 주입시 예외처리 여부 테스트 작성

--- src/main/java/step3/domain/winning/WinningLotto.java
+++ src/main/java/step3/domain/winning/WinningLotto.java
@@ -2,6 +2,9 @@ package step3.domain.winning;
 
 import step3.domain.lotto.Lotto;
 import step3.domain.lotto.LottoNumber;
+import step3.exception.LottoNumberNullPointerException;
+
+import java.util.Objects;
 
 public final class WinningLotto {
 
@@ -9,9 +12,16 @@ public final class WinningLotto {
     private final LottoNumber bonusLotto;
 
 
-    private WinningLotto(Lotto winningLotto, LottoNumber bonusLotto) {
+    private WinningLotto(Lotto winningLotto, LottoNumber bonusLottoNumber) {
+        validateBonusLottoNumberNull(bonusLottoNumber);
         this.winningLotto = winningLotto;
-        this.bonusLotto = bonusLotto;
+        this.bonusLotto = bonusLottoNumber;
+    }
+
+    private final void validateBonusLottoNumberNull(LottoNumber bonusLottoNumber) {
+        if(Objects.isNull(bonusLottoNumber)) {
+            throw new LottoNumberNullPointerException();
+        }
     }
 
     public static final WinningLotto from(Lotto lotto, LottoNumber bonusLottoNumber) {
--- src/test/java/step3/domain/winning/WinningLottoTest.java
+++ src/test/java/step3/domain/winning/WinningLottoTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.domain.lotto.Lotto;
 import step3.domain.lotto.LottoNumber;
+import step3.exception.LottoNumberNullPointerException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
 
@@ -21,6 +23,34 @@ class WinningLottoTest {
 
         // then
         assertThat(winningLotto).isNotNull();
+
+    }
+
+    @DisplayName("WinningLotto 인스턴스에 로또에 null 주입시 예이처리 여부 테스트")
+    @Test
+    void 검증_로또_Null() {
+        // given
+        Lotto lotto = null;
+        LottoNumber bonusLottoNumber = LottoNumber.valueOf(7);
+
+        // when
+        assertThatThrownBy(()-> WinningLotto.from(lotto, bonusLottoNumber))
+                .isInstanceOf(LottoNumberNullPointerException.class)
+                .hasMessageContaining("LottoNumber 인스턴스가 null 입니다.");
+
+    }
+
+    @DisplayName("WinningLotto 인스턴스 보너스 로또 넘버에 null 주입시 예이처리 여부 테스트")
+    @Test
+    void 검증_로또넘버_Null() {
+        // given
+        Lotto lotto = Lotto.of("1, 2, 3, 4, 5, 6");
+        LottoNumber bonusLottoNumber = null;
+
+        // when
+        assertThatThrownBy(()-> WinningLotto.from(lotto, bonusLottoNumber))
+                .isInstanceOf(LottoNumberNullPointerException.class)
+                .hasMessageContaining("LottoNumber 인스턴스가 null 입니다.");
 
     }
 }
