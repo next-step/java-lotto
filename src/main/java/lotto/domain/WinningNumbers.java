c690c55940358bd617d8e77e722b822132cdbbe3
Beenie93 <seongbeen93@naver.com>
2021-04-04 16:01:37 +0900

    refactor: remove a method to check bound

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -7,10 +7,7 @@ import java.util.stream.Collectors;
 
 public class WinningNumbers {
     private static final int NUMBER_LENGTH = 6;
-    private static final int BOUND_MAX = 45;
-    private static final int BOUND_MIN = 1;
     private static final String CHECK_LENGTH = String.format("당첨 번호의 길이는 %d(이)여야 합니다.", NUMBER_LENGTH);
-    private static final String CHECK_BOUND = String.format("당첨 번호는 %d ~ %d 범위의 값이어야 합니다.", BOUND_MIN, BOUND_MAX);
     private static final String CHECK_WINNING_NUMBER = "입력하신 당첨 번호 %s가 숫자인지 확인해주세요.";
     private static final String CHECK_DUPLICATION = "중복되는 숫자가 포함되어 있는지 확인해주세요.";
     private final List<LottoNumber> winningNumbers;
@@ -51,25 +48,12 @@ public class WinningNumbers {
 
     private void validate(List<LottoNumber> winningNumbers) {
         checkLength(winningNumbers);
-        checkBound(winningNumbers);
         checkDuplication(winningNumbers);
     }
 
     private void checkLength(List<LottoNumber> winningNumbers) {
         if (winningNumbers.size() != NUMBER_LENGTH) {
             throw new IllegalArgumentException(CHECK_LENGTH);
-        }
-    }
-
-    private void checkBound(List<LottoNumber> winningNumbers) {
-        for (LottoNumber winningNumber : winningNumbers) {
-            checkBound(winningNumber.lottoNumber());
-        }
-    }
-
-    private void checkBound(Integer number) {
-        if (BOUND_MIN > number || number > BOUND_MAX) {
-            throw new IllegalArgumentException(CHECK_BOUND);
         }
     }
 
