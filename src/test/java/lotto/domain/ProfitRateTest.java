54ecc295042dd1d0b7abea0c13f5698d77ec7081
Beenie93 <seongbeen93@naver.com>
2021-04-05 02:55:40 +0900

    test: add a test unit to check profit rate

--- src/test/java/lotto/domain/ProfitRateTest.java
+++ src/test/java/lotto/domain/ProfitRateTest.java
@@ -10,7 +10,16 @@ public class ProfitRateTest {
     @Test
     @DisplayName("수익률 생성")
     public void create() throws Exception {
-        ProfitRate profitRate = new ProfitRate(5000, 14000);
-        assertThat(profitRate).isEqualTo(new ProfitRate(((double) 5000 / 14000)));
+        ProfitRate profitRate = new ProfitRate(5_000, 14_000);
+        assertThat(profitRate).isEqualTo(new ProfitRate(((double) 5_000 / 14_000)));
+    }
+
+    @Test
+    @DisplayName("수익률 이득 판별")
+    public void isPositive() throws Exception {
+        ProfitRate profitRate = new ProfitRate(14_000, 14_000);
+        assertThat(profitRate.isPositive()).isTrue();
+        profitRate = new ProfitRate(13_000, 14_000);
+        assertThat(profitRate.isPositive()).isFalse();
     }
 }
