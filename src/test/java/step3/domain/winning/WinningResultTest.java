c5ba2a83fd6559fe7a043513a99e8ad43d52d05b
김우재 <kwj1270@naver.com>
2021-04-04 16:14:37 +0900

    test:WinningResult 인스턴스 Null 주입시 예외처리 여부 테스트작성

--- src/test/java/step3/domain/winning/WinningResultTest.java
+++ src/test/java/step3/domain/winning/WinningResultTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.domain.rank.Rank;
 
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WinningResultTest {
@@ -16,12 +18,24 @@ class WinningResultTest {
     @Test
     void 생성() {
         // given
-        Map<Rank, Integer> rankIntegerMap = new TreeMap<>();
+        Map<Rank, Integer> rankIntegerMap = new EnumMap<>(Rank.class);
 
         // when
         WinningResult winningResult = WinningResult.of(rankIntegerMap);
 
         // then
         assertThat(winningResult).isNotNull();
+    }
+
+    @DisplayName("WinningResult 인스턴스 Null 주입시 예외처리 여부 테스트")
+    @Test
+    void 검증_Null() {
+        // given
+        Map<Rank, Integer> rankIntegerMap = null;
+
+        // when
+        assertThatThrownBy(()->WinningResult.of(rankIntegerMap))
+                .isInstanceOf(MapNullPointerException.class)
+                .hasMessageContaining("Map 인스턴스가 Null 입니다.");
     }
 }
