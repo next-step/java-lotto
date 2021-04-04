8c466711f6b501db85f807bc6d9884512dd7731b
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-04 20:51:17 +0900

    refactor : LottoAutoService 구조변경  - 구입갯수 기반 구매 구조로 변경  - 구입금액 계산로직은 LottoPurchaseService 로 이관

--- src/main/java/lotto/service/LottoAutoService.java
+++ src/main/java/lotto/service/LottoAutoService.java
@@ -1,6 +1,5 @@
 package lotto.service;
 
-import lotto.domain.LottoConstant;
 import lotto.domain.LottoRanks;
 import lotto.domain.LottoTicket;
 
@@ -8,15 +7,8 @@ import java.util.List;
 
 public class LottoAutoService {
 
-    public LottoTicket purchaseLottoTicket(int purchaseAmount) {
-        return new LottoTicket(calculateMaxPurchasableCount(purchaseAmount));
-    }
-
-    private int calculateMaxPurchasableCount(int purchaseAmount) {
-        if (purchaseAmount < LottoConstant.LOTTO_PRICE) {
-            throw new IllegalArgumentException("최소 1000원 이상의 금액을 입력해 주세요.");
-        }
-        return purchaseAmount / LottoConstant.LOTTO_PRICE;
+    public LottoTicket purchaseLottoTicket(int purchaseCount) {
+        return new LottoTicket(purchaseCount);
     }
 
     public LottoRanks inquiryWin(List<String> confirmTargetList, int[] winNumbers, int bonusNumber) {
--- src/test/java/lotto/service/LottoAutoServiceTest.java
+++ src/test/java/lotto/service/LottoAutoServiceTest.java
@@ -17,31 +17,30 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 class LottoAutoServiceTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {999, 500, 0, -1})
-    @DisplayName("로또 구매금액은 최소 1000원 이상이어야 한다.")
-    void purchaseAmount(int purchaseAmount) {
+    @ValueSource(ints = {0, -1})
+    @DisplayName("로또 갯수는 1개 이상이어야 한다.")
+    void purchaseLottoTicket(int purchaseCount) {
         // given
         LottoAutoService lottoAutoService = new LottoAutoService();
 
         // when then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> lottoAutoService.purchaseLottoTicket(purchaseAmount))
-                .withMessageMatching("최소 1000원 이상의 금액을 입력해 주세요.");
+                .isThrownBy(() -> lottoAutoService.purchaseLottoTicket(purchaseCount))
+                .withMessageMatching("최소 1개 이상의 구매수량을 입력해 주세요");
     }
 
     @Test
-    @DisplayName("로또 구매결과 구매금액을 1000원 단위로 나눈 수만큼의 티켓이 존재해야 한다.")
+    @DisplayName("로또 구매결과 구매장수만큼의 티켓 카운트가 존재해야 한다.")
     void purchaseCount() {
         // given
-        int purchaseAmount = 14000;
-        int lottoPrice = 1000;
+        int purchaseCount = 14;
         LottoAutoService lottoAutoService = new LottoAutoService();
 
         // when
-        LottoTicket lottoTicket = lottoAutoService.purchaseLottoTicket(purchaseAmount);
+        LottoTicket lottoTicket = lottoAutoService.purchaseLottoTicket(purchaseCount);
 
         // then
-        assertThat(purchaseAmount / lottoPrice).isEqualTo(lottoTicket.getCount());
+        assertThat(purchaseCount).isEqualTo(lottoTicket.getCount());
     }
 
     @Test
