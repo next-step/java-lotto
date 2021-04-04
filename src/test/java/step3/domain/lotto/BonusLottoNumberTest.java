cf889c5c79c4796f978ffefa1c2a78ae65285236
김우재 <kwj1270@naver.com>
2021-04-04 12:43:18 +0900

    test:BonusLottoNumber 인스턴스 null 주입시 예외처리 여부 테스트작성

--- src/test/java/step3/domain/lotto/BonusLottoNumberTest.java
+++ src/test/java/step3/domain/lotto/BonusLottoNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusLottoNumberTest {
@@ -19,5 +20,17 @@ class BonusLottoNumberTest {
 
         // then
         assertThat(bonusLottoNumber).isNotNull();
+    }
+
+    @DisplayName("BonusLottoNumber 인스턴스 null 주입시 예외처리 여부 테스트")
+    @Test
+    void 검증() {
+        // given
+        LottoNumber nullLottoNumber = null;
+
+        // when and then
+        assertThatThrownBy(()-> BonusLottoNumber.valueOf(nullLottoNumber))
+                .isInstanceOf(LottoNumberNullPointerException.class)
+                .hasMessageContaining("LottoNumber 인스턴스가 null 입니다.");
     }
 }
