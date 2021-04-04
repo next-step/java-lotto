4dd3b478ff052d403c5247d8171a58159536fef6
김우재 <kwj1270@naver.com>
2021-04-04 14:34:49 +0900

    test:WinningLotto 인스턴스 생성 기능 구현

--- src/main/java/step3/domain/lotto/WinningLotto.java
+++ src/main/java/step3/domain/lotto/WinningLotto.java
@@ -1,4 +1,17 @@
 package step3.domain.lotto;
 
-public class WinningLotto {
+public final class WinningLotto {
+
+    private final Lotto winningLotto;
+    private final LottoNumber bonusLotto;
+
+
+    private WinningLotto(Lotto winningLotto, LottoNumber bonusLotto) {
+        this.winningLotto = winningLotto;
+        this.bonusLotto = bonusLotto;
+    }
+
+    public static final WinningLotto from(Lotto lotto, LottoNumber bonusLottoNumber) {
+        return new WinningLotto(lotto, bonusLottoNumber);
+    }
 }
