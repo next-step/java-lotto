3b0019e1aea0d51151bde760be081e21a92db022
김우재 <kwj1270@naver.com>
2021-04-04 11:54:48 +0900

    feat:View 클래스 생성

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -1,9 +1,11 @@
 package step3.view;
 
 
+import step3.domain.lotto.BonusLottoNumber;
 import step3.domain.lotto.Lotto;
 import step3.domain.money.Money;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public final class InputView {
@@ -16,14 +18,10 @@ public final class InputView {
     }
 
     public static final InputView getInstance() {
-        if (isInstanceNull()) {
+        if (Objects.isNull(instance)) {
             instance = new InputView();
         }
         return instance;
-    }
-
-    private static final boolean isInstanceNull() {
-        return instance == null;
     }
 
     private final int inputMoneyByClient() {
@@ -36,12 +34,21 @@ public final class InputView {
         return scanner.nextLine();
     }
 
+    private final int inputBonusLottoNumbersByClient() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
     public final Money getMoneyByClient() {
         return Money.valueOf(inputMoneyByClient());
     }
-/*
+
     public final Lotto getWinningLottoByClient() {
         return Lotto.of(inputLottoNumbersByClient());
     }
-*/
+
+    public final BonusLottoNumber getBonusLottoByClient() {
+        return BonusLottoNumber.valueOf(inputBonusLottoNumbersByClient());
+    }
+
 }
