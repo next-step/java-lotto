94d07c277056fa48ac3035a757b888647ecd9787
keke0123 <keke0123@naver.com>
2021-04-04 20:24:02 +0900

    (refactor) 큰 숫자 가독성 높이는 방식으로 수정

--- src/main/java/step02/code/domain/GradeEnum.java
+++ src/main/java/step02/code/domain/GradeEnum.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum GradeEnum {
-  FIRST(6, 2000000000),
-  SECOND(5, 30000000), // 5 + 보너스 숫자 일치
-  THIRD(5, 1500000),
-  FOURTH(4, 50000),
-  FIFTH(3, 5000),
+  FIRST(6, 2_000_000_000),
+  SECOND(5, 30_000_000), // 5 + 보너스 숫자 일치
+  THIRD(5, 1_500_000),
+  FOURTH(4, 50_000),
+  FIFTH(3, 5_000),
   NONE(0, 0);
 
   private static final int SECOND_OR_THIRD_NUMBER = 5;
