122f8d5f02caa21973312a18341eec728b78d3c6
juniq <juniqlim@gmail.com>
2021-04-04 10:32:39 +0900

    repactor: Winning findByMatchedCount 개선
    보너스볼에 대한 빠른 분기처리

--- src/main/java/im/juniq/lotto/domain/Winning.java
+++ src/main/java/im/juniq/lotto/domain/Winning.java
@@ -3,20 +3,18 @@ package im.juniq.lotto.domain;
 import java.util.Arrays;
 
 public enum Winning {
-	FIRST(6, false, 2000000000L),
-	SECOND(5, true, 30000000L),
-	THIRD(5, false, 1500000L),
-	FOURTH(4, false, 50000L),
-	FIFTH(3, false, 5000L),
-	LOSING(2, false, 0L);
+	FIRST(6, 2000000000L),
+	SECOND(5, 30000000L),
+	THIRD(5, 1500000L),
+	FOURTH(4, 50000L),
+	FIFTH(3, 5000L),
+	LOSING(2, 0L);
 
 	private final int matchedCount;
-	private final boolean matchedBonus;
 	private final Long amount;
 
-	Winning(int matchedCount, boolean matchedBonus, Long amount) {
+	Winning(int matchedCount, Long amount) {
 		this.matchedCount = matchedCount;
-		this.matchedBonus = matchedBonus;
 		this.amount = amount;
 	}
 
@@ -29,25 +27,16 @@ public enum Winning {
 	}
 
 	public static Winning findByMatchedCount(int numberOfMatchedWinningNumber, boolean matchedBonus) {
-		return Arrays.stream(Winning.values())
-				.filter(winning -> comparison(numberOfMatchedWinningNumber, matchedBonus, winning)).findFirst()
-				.orElse(LOSING);
-	}
+		if (Winning.THIRD.matchedCount == numberOfMatchedWinningNumber && !matchedBonus) {
+			return Winning.THIRD;
+		}
 
-	private static boolean comparison(int numberOfMatchedWinningNumber, boolean matchedBonus, Winning winning) {
-		return equalMatchedCount(numberOfMatchedWinningNumber, winning) && eligibleBonus(matchedBonus, winning)
-			|| equalMatchedCount(numberOfMatchedWinningNumber, winning) && ineligibleBonus(winning);
+		return Arrays.stream(Winning.values())
+				.filter(winning -> equalMatchedCount(numberOfMatchedWinningNumber, winning)).findFirst()
+				.orElse(LOSING);
 	}
 
 	private static boolean equalMatchedCount(int numberOfMatchedWinningNumber, Winning winning) {
 		return winning.matchedCount == numberOfMatchedWinningNumber;
-	}
-
-	private static boolean eligibleBonus(boolean matchedBonus, Winning winning) {
-		return winning.matchedBonus && matchedBonus;
-	}
-
-	private static boolean ineligibleBonus(Winning winning) {
-		return !winning.matchedBonus;
 	}
 }
