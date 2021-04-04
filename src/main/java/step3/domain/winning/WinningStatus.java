99024baf969f27efa75b681c987decb472055579
김우재 <kwj1270@naver.com>
2021-04-04 14:55:44 +0900

    feat:WinningStatus 인스턴스 생성 기능 구현

--- src/main/java/step3/domain/winning/WinningStatus.java
+++ src/main/java/step3/domain/winning/WinningStatus.java
@@ -1,4 +1,17 @@
 package step3.domain.winning;
 
-public class WinningStatus {
+public final class WinningStatus {
+
+    private final int countOfMatch;
+    private final boolean matchBonus;
+
+    private WinningStatus(int countOfMatch, boolean matchBonus) {
+        this.countOfMatch = countOfMatch;
+        this.matchBonus = matchBonus;
+    }
+
+    public static final WinningStatus from(int countOfMatch, boolean matchBonus) {
+        return new WinningStatus(countOfMatch, matchBonus);
+    }
+
 }
