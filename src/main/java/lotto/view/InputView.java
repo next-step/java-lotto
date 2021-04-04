478034b25b09cb508d47733785e64143c8daf607
Beenie93 <seongbeen93@naver.com>
2021-04-04 15:30:00 +0900

    feat: implement methods to get manual lotto

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MANUAL_LOTTO_TICKET_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_BALL = "보너스 볼을 입력해 주세요.";
 
@@ -15,6 +17,22 @@ public class InputView {
         String purchaseAmount = scanner.nextLine().trim();
         PreConditionUtil.checkNullOrEmpty(purchaseAmount);
         return purchaseAmount;
+    }
+
+    public static String manualLottoTicketCount() {
+        System.out.println();
+        System.out.println(MANUAL_LOTTO_TICKET_COUNT);
+        String manualLottoTicketCount = scanner.nextLine().trim();
+        PreConditionUtil.checkNullOrEmpty(manualLottoTicketCount);
+        return manualLottoTicketCount;
+    }
+
+    public static String manualLottoNumbers() {
+        System.out.println();
+        System.out.println(MANUAL_LOTTO_NUMBERS);
+        String manualLottoNumbers = scanner.nextLine().trim();
+        PreConditionUtil.checkNullOrEmpty(manualLottoNumbers);
+        return manualLottoNumbers;
     }
 
     public static String winningNumbers() {
