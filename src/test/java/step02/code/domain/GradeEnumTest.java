a475f13eb3893511a31d5fd2a2a2a3582649ff04
keke0123 <keke0123@naver.com>
2021-04-04 19:47:12 +0900

    (refactor) 모든 등수 테스트로 수정

--- src/test/java/step02/code/domain/GradeEnumTest.java
+++ src/test/java/step02/code/domain/GradeEnumTest.java
@@ -2,20 +2,34 @@ package step02.code.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class GradeEnumTest {
-  @Test
+  @ParameterizedTest
+  @MethodSource("gradeTestCase")
   @DisplayName("보너스 숫자가 맞을때 2등, 틀릴때 3등 return")
-  public void secondOrThird() {
-    GradeEnum second = GradeEnum.fromGrade(5, true);
-    GradeEnum third = GradeEnum.fromGrade(5, false);
+  public void grade(int matched, boolean isMatchBonus, GradeEnum grade) {
+    GradeEnum gradeEnum = GradeEnum.fromGrade(matched, isMatchBonus);
 
-    assertThat(second).isEqualTo(GradeEnum.SECOND);
-    assertThat(third).isEqualTo(GradeEnum.THIRD);
+    assertThat(gradeEnum).isEqualTo(grade);
+  }
+
+  private static Stream<Arguments> gradeTestCase() {
+    return Stream.of(
+      Arguments.of(6, false, GradeEnum.FIRST),
+      Arguments.of(5, true, GradeEnum.SECOND),
+      Arguments.of(5, false, GradeEnum.THIRD),
+      Arguments.of(4, false, GradeEnum.FOURTH),
+      Arguments.of(3, false, GradeEnum.FIFTH)
+    );
   }
 
   @Test
