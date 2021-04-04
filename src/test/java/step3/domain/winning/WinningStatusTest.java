0b0e526d7c75fefcdce007d2e89ac85ebb513478
김우재 <kwj1270@naver.com>
2021-04-04 14:57:29 +0900

    test:WinningStatus 인스턴스 음수 반환 여부 테스트작성

--- src/test/java/step3/domain/winning/WinningStatusTest.java
+++ src/test/java/step3/domain/winning/WinningStatusTest.java
@@ -2,8 +2,10 @@ package step3.domain.winning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step3.exception.InputNegativeAmountException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WinningStatusTest {
@@ -20,5 +22,18 @@ class WinningStatusTest {
 
         // then
         assertThat(winningStatus).isNotNull();
+    }
+
+    @DisplayName("WinningStatus 인스턴스 음수 반환 여부 테스트")
+    @Test
+    void 검증_반환() {
+        // given
+        int countOfMatch = -1;
+        boolean matchBonus = false;
+
+        // when
+        assertThatThrownBy(()-> WinningStatus.from(countOfMatch, matchBonus))
+                .isInstanceOf(InputNegativeAmountException.class)
+                .hasMessageContaining("음수값의 돈이 입력되었습니다.");
     }
 }
