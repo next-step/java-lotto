1026728677bc8da22ccfbba3aa504af18d4a8ba8
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-04 16:36:52 +0900

    refactor : manualLottoPurchaseCount 필드 제거  - manualLottoPurchaseNumberList size 를 통해 처리

--- src/main/java/lotto/controller/dto/LottoTicketPurchaseRequest.java
+++ src/main/java/lotto/controller/dto/LottoTicketPurchaseRequest.java
@@ -5,21 +5,15 @@ import java.util.List;
 public class LottoTicketPurchaseRequest {
 
     private final int purchaseAmount;
-    private final int manualLottoPurchaseCount;
     private final List<String> manualLottoPurchaseNumberList;
 
     public LottoTicketPurchaseRequest(int purchaseAmount, List<String> manualLottoPurchaseNumberList) {
         this.purchaseAmount = purchaseAmount;
-        this.manualLottoPurchaseCount = manualLottoPurchaseNumberList.size();
         this.manualLottoPurchaseNumberList = manualLottoPurchaseNumberList;
     }
 
     public int getPurchaseAmount() {
         return purchaseAmount;
-    }
-
-    public int getManualLottoPurchaseCount() {
-        return manualLottoPurchaseCount;
     }
 
     public List<String> getManualLottoPurchaseNumberList() {
