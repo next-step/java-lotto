c8977992fe7891282ed1bc1d32869bf068f40d63
김우재 <kwj1270@naver.com>
2021-04-04 12:40:46 +0900

    feat:WinningResult 인스턴스 생성 기능 구현

--- src/main/java/step3/domain/winning/WinningResult.java
+++ src/main/java/step3/domain/winning/WinningResult.java
@@ -1,4 +1,19 @@
 package step3.domain.winning;
 
+import step3.domain.rank.Rank;
+
+import java.util.Map;
+
 public class WinningResult {
+
+    private Map<Rank, Integer> winningResult;
+
+    private WinningResult(Map<Rank, Integer> winningResult) {
+        this.winningResult = winningResult;
+    }
+
+    public static WinningResult of(Map<Rank, Integer> winningResult) {
+        return new WinningResult(winningResult);
+    }
+
 }
