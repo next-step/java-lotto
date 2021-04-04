05108fc9109cf04b66f11c2f91e0be8a66c65447
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-04 21:00:45 +0900

    test : 수동로또 구매 테스트 추가  - 구매숫자 검증  - 티켓유형 검증

--- src/test/java/lotto/service/LottoManualServiceTest.java
+++ src/test/java/lotto/service/LottoManualServiceTest.java
@@ -1,10 +1,14 @@
 package lotto.service;
 
+import lotto.domain.LottoTicket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoManualServiceTest {
@@ -29,5 +33,33 @@ class LottoManualServiceTest {
         // when then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> lottoManualService.purchaseLottoTicket(new ArrayList<>()));
+    }
+
+    @Test
+    @DisplayName("로또 구매결과 구매장수만큼의 티켓 카운트가 존재해야 한다.")
+    void purchaseCount() {
+        // given
+        List<String> manualLottoPurchaseNumberList = Arrays.asList("1, 2, 3, 4, 5, 6", "2, 3, 4, 5, 6, 7", "3, 4, 5, 6, 7, 8");
+        LottoManualService lottoManualService = new LottoManualService();
+
+        // when
+        LottoTicket lottoTicket = lottoManualService.purchaseLottoTicket(manualLottoPurchaseNumberList);
+
+        // then
+        assertThat(3).isEqualTo(lottoTicket.getCount());
+    }
+
+    @Test
+    @DisplayName("로또 구매결과 티켓타입은 수동 티켓이다.")
+    void ticketType_Manual() {
+        // given
+        List<String> manualLottoPurchaseNumberList = Arrays.asList("1, 2, 3, 4, 5, 6", "2, 3, 4, 5, 6, 7", "3, 4, 5, 6, 7, 8");
+        LottoManualService lottoManualService = new LottoManualService();
+
+        // when
+        LottoTicket lottoTicket = lottoManualService.purchaseLottoTicket(manualLottoPurchaseNumberList);
+
+        // then
+        assertThat(lottoTicket.isManualTicket()).isTrue();
     }
 }
