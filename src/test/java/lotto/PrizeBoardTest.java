384ec517b4b1429b07fcf0240015bc1a25823ca9
Dongjoon Park <djoon@users.noreply.github.com>
2021-04-05 01:05:01 +0900

    test: PrizeBoard 테스트 코드 추가

---
+++ src/test/java/lotto/PrizeBoardTest.java
@@ -0,0 +1,42 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.domain.prize.Prize;
+import lotto.domain.stats.PrizeBoard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PrizeBoardTest {
+
+    @Test
+    @DisplayName("Nothing Prize를 기록했을 때, Nothing Prize 승자 인원을 반환한다.")
+    void addNonePrize() {
+        PrizeBoard prizeBoard = new PrizeBoard();
+
+        prizeBoard.record(Prize.NOTHING);
+
+        assertThat(prizeBoard.getWinnerCountByPrize(Prize.NOTHING)).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("1등 Prize를 기록했을 때, 1등 Prize 승자 인원을 반환한다.")
+    void addFirstPrize() {
+        PrizeBoard prizeBoard = new PrizeBoard();
+
+        prizeBoard.record(Prize.FIRST);
+
+        assertThat(prizeBoard.getWinnerCountByPrize(Prize.FIRST)).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("2등 Prize를 2번 기록했을 때, 2등 Prize 승자 인원을 반환한다.")
+    void addSecondPrize() {
+        PrizeBoard prizeBoard = new PrizeBoard();
+
+        prizeBoard.record(Prize.SECOND);
+        prizeBoard.record(Prize.SECOND);
+
+        assertThat(prizeBoard.getWinnerCountByPrize(Prize.SECOND)).isEqualTo(2L);
+    }
+}
