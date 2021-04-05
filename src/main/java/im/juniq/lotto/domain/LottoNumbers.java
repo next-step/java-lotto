aaaadc71ceac3f6b4b2b629f881515a6de306c1f
juniq <juniqlim@gmail.com>
2021-04-05 22:45:07 +0900

    refactor: 매직넘버를 상수로 치환

--- src/main/java/im/juniq/lotto/domain/LottoNumbers.java
+++ src/main/java/im/juniq/lotto/domain/LottoNumbers.java
@@ -4,36 +4,37 @@ import java.util.Iterator;
 import java.util.List;
 
 public class LottoNumbers implements Iterable<Integer> {
-    private final List<Integer> numbers;
+	public static final int LOTTO_NUMBERS_SIZE = 6;
+	private final List<Integer> numbers;
 
-    public LottoNumbers(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 숫자는 6개 이어야 합니다.");
-        }
-        if (numbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException("로또 숫자는 중복 될 수 없습니다.");
-        }
-        this.numbers = numbers;
-    }
+	public LottoNumbers(List<Integer> numbers) {
+		if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+			throw new IllegalArgumentException("로또 숫자는 " + LOTTO_NUMBERS_SIZE + "개 이어야 합니다.");
+		}
+		if (numbers.stream().distinct().count() != LOTTO_NUMBERS_SIZE) {
+			throw new IllegalArgumentException("로또 숫자는 중복 될 수 없습니다.");
+		}
+		this.numbers = numbers;
+	}
 
-    public int matchedCount(LottoNumbers lottoNumbers) {
-        return (int) numbers.stream().filter(lottoNumbers::contain).count();
-    }
+	public int matchedCount(LottoNumbers lottoNumbers) {
+		return (int) numbers.stream().filter(lottoNumbers::contain).count();
+	}
 
-    public boolean contain(int number) {
-        return numbers.contains(number);
-    }
+	public boolean contain(int number) {
+		return numbers.contains(number);
+	}
 
-    public int size() {
-        return numbers.size();
-    }
+	public int size() {
+		return numbers.size();
+	}
 
-    public int get(int index) {
-        return numbers.get(index);
-    }
+	public int get(int index) {
+		return numbers.get(index);
+	}
 
-    @Override
-    public Iterator<Integer> iterator() {
-        return numbers.iterator();
-    }
+	@Override
+	public Iterator<Integer> iterator() {
+		return numbers.iterator();
+	}
 }
