ebf51f5fe46f400ffa0d32c780f98f8fc0f210ca
lee.jaesung <lee.jaesung@joins.com>
2021-04-05 16:35:58 +0900

    test: 로또 구입금액 예외 테스트 케이스 추가

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -4,13 +4,22 @@ import lotto.domain.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class MoneyTest {
     @DisplayName("당첨금액을 생성한다.")
     @Test
     public void hitMoney() {
-        Money hitMoney = new Money(5000);
-        assertThat(hitMoney).isEqualTo(new Money(5000));
+        Money money = new Money(5000);
+        assertThat(money).isEqualTo(new Money(5000));
+    }
+
+    @DisplayName("구입금액이 로또 한장 금액보다 작을 경우 예외를 반환한다.")
+    @Test
+    public void purchase() {
+        assertThatThrownBy(() -> {
+            new Money(500);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
