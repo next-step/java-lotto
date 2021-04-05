a87e628d1e078d28614abed1b7d687d6892c76da
ad7549 <ad7549@gmail.com>
2021-04-05 13:20:32 +0900

    fix: Fix typo

--- src/main/java/lotto/domain/LottoBuyer.java
+++ src/main/java/lotto/domain/LottoBuyer.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class LottoBuyer {
     private static final int MIN_PURCHASABLE_COUNT = 0;
-    private static final String PURCHASABLE_ONE_ERROR_MESSAGE = "구입 금액(payment)는 LottoStore의 가격(price)보다 크거나 같아야 합니다.";
+    private static final String PURCHASABLE_ONE_ERROR_MESSAGE = "구입 금액(payment)은 LottoStore의 가격(price)보다 크거나 같아야 합니다.";
     private static final String MIN_PURCHASABLE_COUNT_ERROR_MESSAGE = String.format("%d개 미만의 개수는 구매할 수 없습니다.", MIN_PURCHASABLE_COUNT);
     private static final String EXCEED_PURCHASABLE_COUNT_ERROR_MESSAGE = "구입 가능한 개수를 초과하였습니다.";
 
