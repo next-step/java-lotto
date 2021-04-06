67d09ad2ec35de8c42d66f7f32878685d6e69276
김우재 <kwj1270@naver.com>
2021-04-06 14:20:28 +0900

    feat:LottoShop 인스턴스 생성 여부 테스트작성

--- src/main/java/step4/domain/shop/LottoShop.java
+++ src/main/java/step4/domain/shop/LottoShop.java
@@ -1,4 +1,15 @@
 package step4.domain.shop;
 
 public class LottoShop {
+
+    private static class LottoShopHolder {
+        private static LottoShop lottoShop = new LottoShop();
+    }
+
+    public LottoShop() { }
+
+    public static final LottoShop getInstance() {
+        return LottoShopHolder.lottoShop;
+    }
+
 }
