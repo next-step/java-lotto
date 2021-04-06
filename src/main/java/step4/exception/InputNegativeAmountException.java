7de3401ac65d2b5d789fbcaf2512e62466f6850d
김우재 <kwj1270@naver.com>
2021-04-06 12:57:59 +0900

    test:Money 인스턴스에 음수 입력시 예외처리 여부 테스트작성

--- src/main/java/step4/exception/InputNegativeAmountException.java
+++ src/main/java/step4/exception/InputNegativeAmountException.java
@@ -2,7 +2,7 @@ package step4.exception;
 
 public final class InputNegativeAmountException extends RuntimeException{
 
-    private final String message = "음수값의 돈이 입력되었습니다.";
+    private final String message = "음수값이 입력되었습니다.";
 
     @Override
     public String getMessage() {
--- src/test/java/step4/domain/money/MoneyTest.java
+++ src/test/java/step4/domain/money/MoneyTest.java
@@ -2,8 +2,10 @@ package step4.domain.money;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.exception.InputNegativeAmountException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
@@ -21,7 +23,17 @@ class MoneyTest {
         assertThat(money).isNotNull();
     }
 
+    @DisplayName("Money 인스턴스에 음수 입력시 예외처리 여부 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int value = -1;
 
+        // when
+        assertThatThrownBy(()-> Money.valueOf(value))
+                .isInstanceOf(InputNegativeAmountException.class)
+                .hasMessageContaining("음수값이 입력되었습니다.");
+    }
 
 
 }
