d717b5bd841de4382f238aee51e3c07b8936bd32
김우재 <kwj1270@naver.com>
2021-04-06 14:56:00 +0900

    feat:LottoShop 인스턴스가 돈에 맞는 로또 반환 기능 구현

--- src/main/java/step4/domain/lotto/Lotto.java
+++ src/main/java/step4/domain/lotto/Lotto.java
@@ -2,17 +2,24 @@ package step4.domain.lotto;
 
 import step4.exception.LottoSizeMissMatchException;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public final class Lotto {
 
     public static final int SIZE = 6;
+    public static final int AMOUNT = 1000;
 
     private final Set<LottoNumber> lotto;
 
     private Lotto(Set<LottoNumber> lotto) {
         validateSize(lotto);
         this.lotto = lotto;
+    }
+
+    public static final Lotto of(List<LottoNumber> lotto) {
+        return of(new HashSet<>(lotto));
     }
 
     public static final Lotto of(Set<LottoNumber> lotto) {
--- src/main/java/step4/domain/shop/LottoShop.java
+++ src/main/java/step4/domain/shop/LottoShop.java
@@ -1,6 +1,18 @@
 package step4.domain.shop;
 
+import step4.domain.lotto.Lotto;
+import step4.domain.lotto.LottoNumber;
+import step4.domain.money.Money;
+import step4.strategy.LottoShuffleStrategy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class LottoShop {
+
+    public static final int FROM_INDEX = 0;
+    public static final int TO_INDEX = 6;
 
     private static class LottoShopHolder {
         private static LottoShop lottoShop = new LottoShop();
@@ -10,6 +22,18 @@ public class LottoShop {
 
     public static final LottoShop getInstance() {
         return LottoShopHolder.lottoShop;
+    }
+
+    public final List<Lotto> purchaseLotto(Money money, LottoShuffleStrategy strategy) {
+        return Stream.generate(() -> generateLotto(strategy))
+                .limit(money.purchaseQuantity(Lotto.AMOUNT))
+                .collect(Collectors.toList());
+    }
+
+    private final Lotto generateLotto(LottoShuffleStrategy strategy) {
+        List<LottoNumber> lottoNumbers = LottoNumber.values();
+        strategy.shuffle(lottoNumbers);
+        return Lotto.of(lottoNumbers.subList(FROM_INDEX, TO_INDEX));
     }
 
 }
