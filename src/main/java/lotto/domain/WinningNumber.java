0a5ce710ef8685f0f7f504f9abb7cf63ba118b4c
dailyworker <workingssu@gmail.com>
2021-04-06 21:52:30 +0900

    refactor : WinningNumber hasBonusBall() 책임 이동

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,8 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class WinningNumber {
 
   private final Lotto winningNumber;
@@ -17,21 +14,16 @@ public class WinningNumber {
     return new WinningNumber(winningNumber, bonusBall);
   }
 
-  public LottoRank match(Lotto holdingNumbers) {
-    return LottoRank.matches(this.winningNumber, holdingNumbers, this.bonusBall);
+  public LottoScoreBoard generateLottoMatchResult(Money money, LottoCoupon coupon,
+      WinningNumber winningNumber) {
+    return LottoScoreBoard.createLottoResult(money, coupon.matches(winningNumber));
   }
 
-  public LottoScoreBoard generateLottoMatchResult(LottoAgency agency) {
-    List<LottoRank> lottoMatchResult = new ArrayList<>();
-    for(int i = 0; i < agency.getPurchaseQuantity(); i++) {
-      lottoMatchResult.add(eachLottoMatchResult(i, agency));
-    }
-    return LottoScoreBoard.createLottoResult(lottoMatchResult, agency.getSeedMoney());
+  public int contains(Lotto lotto) {
+    return lotto.matchCount(winningNumber);
   }
 
-  private LottoRank eachLottoMatchResult(int index, LottoAgency agency) {
-    return match(agency.getCoupon()
-        .getLottoCoupon()
-        .get(index));
+  public boolean hasBonusBall(Lotto lotto) {
+    return lotto.contains(bonusBall);
   }
 }
--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -2,7 +2,9 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,34 +24,39 @@ class WinningNumberTest {
   }
 
   @Test
-  @DisplayName("로또 매칭 결과를 제대로 생성할 수 있는가")
+  @DisplayName("LottoCoupon을 이용하여 매치 로또 결과를 제대로 생성할 수 있는가")
   public void generateLottoResultTest() throws Exception {
     //given
     WinningNumber winningNumber = WinningNumber.createWinningNumbers(
         Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
         new Number(6));
 
-    LottoAgency agency = new LottoAgency(new Money(14000));
+    List<Lotto> lottos = new ArrayList<>();
+    lottos.add(Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+    LottoCoupon coupon = new LottoCoupon(lottos);
 
     //when
-    LottoScoreBoard lottoScoreBoard = winningNumber.generateLottoMatchResult(agency);
+    LottoScoreBoard lottoScoreBoard = winningNumber.generateLottoMatchResult(new Money(2000000000), coupon, winningNumber);
 
     //then
-    assertNotNull(lottoScoreBoard);
+    assertEquals(lottoScoreBoard.totalEarningRate(), 1);
   }
 
   @Test
-  @DisplayName("Lotto를 통해서 당첨 비교를 수행할 수 있는가")
-  public void matchTest() throws Exception {
+  @DisplayName("보너스 볼을 가졌는지 판단할 수 있는가")
+  public void hasBonusBall() throws Exception {
     //given
     WinningNumber winningNumber = WinningNumber.createWinningNumbers(
         Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
         new Number(6));
 
+    Lotto manualLotto = Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+
     //when
-    LottoRank match = winningNumber.match(Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+    boolean hasBonusBall = winningNumber.hasBonusBall(manualLotto);
 
     //then
-    assertEquals(match.name(), "FIRST");
+    assertTrue(hasBonusBall);
   }
 }
