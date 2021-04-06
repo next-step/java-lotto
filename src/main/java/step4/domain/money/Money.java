a05238aefed786e2cc2ee7e22d4ff577c37144f1
김우재 <kwj1270@naver.com>
2021-04-06 13:02:18 +0900

    test:Money 인스턴스에 음수 입력시 예외처리 여부 테스트작성

--- src/main/java/step4/domain/money/Money.java
+++ src/main/java/step4/domain/money/Money.java
@@ -1,11 +1,22 @@
 package step4.domain.money;
 
-public class Money {
+import step4.exception.InputNegativeAmountException;
+
+public final class Money {
+
+    private static final int ZERO = 0;
 
     private final int money;
 
     private Money(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private final void validate(int money) {
+        if (money < ZERO) {
+            throw new InputNegativeAmountException();
+        }
     }
 
     public static final Money valueOf(int money) {
