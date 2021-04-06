274a777c9ed7cfab47f23785f15168756b0a0689
jaystar13 <astarisk2@gmail.com>
2021-04-06 22:04:06 +0900

    test: 반환금액 예외 테스트 추가

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -29,4 +29,13 @@ public class MoneyTest {
         Money payMoney = new Money(10000);
         assertThat(payMoney.remainMoney(3)).isEqualTo(new Money(7000));
     }
+
+    @DisplayName("지불금액보다 큰 금액의 게임을 하면 예외를 반환한다.")
+    @Test
+    public void remain_예외() {
+        Money payMoney = new Money(2000);
+        assertThatThrownBy(() -> {
+            payMoney.remainMoney(3);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
