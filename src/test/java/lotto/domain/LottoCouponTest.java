ef099c61d9a43a43b6cab20fc33ce18aee1f4f73
dailyworker <workingssu@gmail.com>
2021-04-06 21:30:10 +0900

    test : LottoCoupon 추가 메소드로 인한 테스트코드 변경 - LottoCoupon.matches : 여러 로또의 매치 결과를 알려주는 함수 - LottoCoupon.add : 기존 LottoCoupon에 새로운 LottoCoupon을 추가하는 함수 - LottoCoupon.asLottoCoupon : 입력받은 List<Lotto>를 LottoCoupon으로 만들어주는 함수

--- src/test/java/lotto/domain/LottoCouponTest.java
+++ src/test/java/lotto/domain/LottoCouponTest.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +17,69 @@ class LottoCouponTest {
     //when
     LottoCoupon lottoCoupon = LottoCoupon.createLottoCoupon(10);
     //then
-    assertEquals(lottoCoupon.getLottoCoupon().size(), 10);
+    assertEquals(lottoCoupon.size(), 10);
+  }
+
+  @Test
+  @DisplayName("List<Lotto>를 받아서 LottoCoupon으로 생성할 수 있는가")
+  public void lottosToLottoCoupon() throws Exception {
+    //given
+    List<Lotto> lottos = new ArrayList<>();
+    lottos.add(Lotto.createAutoLotto());
+    //when
+    LottoCoupon lottoCoupon = LottoCoupon.asLottoCoupon(lottos);
+    //then
+    assertNotNull(lottoCoupon);
+  }
+
+  @Test
+  @DisplayName("여러 로또의 매치 결과를 얻을 수가 있는가")
+  public void matches() throws Exception {
+    //given
+    List<Lotto> lottos = new ArrayList<>();
+    lottos.add(Lotto.createManualLotto(Arrays.asList(1,2,3,4,5,6)));
+    lottos.add(Lotto.createManualLotto(Arrays.asList(3,4,5,6,7,8)));
+
+    WinningNumber winningNumbers = WinningNumber
+        .createWinningNumbers(Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+            new Number(7));
+
+    //when
+    LottoCoupon lottoCoupon = LottoCoupon.asLottoCoupon(lottos);
+    List<LottoRank> matches = lottoCoupon.matches(winningNumbers);
+
+    //then
+    assertAll(
+        () -> assertEquals(matches.get(0).name(), "FIRST"),
+        () -> assertEquals(matches.get(1).name(), "FOURTH")
+    );
+  }
+
+  @Test
+  @DisplayName("size()를 통해서 LottoCoupon의 size를 알아낼 수 있는가")
+  public void size() throws Exception {
+    //given
+    LottoCoupon lottoCoupon = LottoCoupon.createLottoCoupon(10);
+
+    //when
+    //then
+    assertEquals(lottoCoupon.size(), 10);
+  }
+
+  @Test
+  @DisplayName("기존 LottoCoupon에 새로운 LottoCoupon을 추가할 수 있는가")
+  public void add() throws Exception {
+    //given
+    List<Lotto> lottos = new ArrayList<>();
+    Lotto manualLotto = Lotto.createManualLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+    lottos.add(manualLotto);
+
+    LottoCoupon lottoCoupon = LottoCoupon.createLottoCoupon(10);
+
+    //when
+    LottoCoupon target = LottoCoupon.asLottoCoupon(lottos);
+    lottoCoupon.add(target);
+    //then
+    assertTrue(lottoCoupon.contains(manualLotto));
   }
 }
