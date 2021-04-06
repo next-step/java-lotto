0b90d4cfb764de819cb7fb05dfae3cdd5cf07af3
juniq <juniqlim@gmail.com>
2021-04-06 23:04:02 +0900

    test: 중복제거

--- src/test/java/im/juniq/lotto/domain/WinningNumbersTest.java
+++ src/test/java/im/juniq/lotto/domain/WinningNumbersTest.java
@@ -9,17 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersTest {
+    final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
     @Test
     @DisplayName("당첨번호에 포함되는 숫자인지 확인")
     void existWinningNumber() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertThat(new WinningNumbers(numbers, 7).matchedCount(new LottoNumbers(numbers))).isEqualTo(6);
     }
 
     @Test
     @DisplayName("보너스번호와 일치하는 숫자인지 확인")
     void matchedBonusNumber() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertThat(new WinningNumbers(numbers, 7).matchedBonusNumber(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 7)))).isEqualTo(true);
         assertThat(new WinningNumbers(numbers, 7).matchedBonusNumber(new LottoNumbers(numbers))).isEqualTo(false);
     }
@@ -27,7 +27,6 @@ class WinningNumbersTest {
     @Test
     @DisplayName("로또 숫자는 중복 될 수 없다.")
     void checkDuplicate() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertThatThrownBy(() -> new WinningNumbers(numbers, 6)).isInstanceOf(IllegalArgumentException.class);
     }
 }
