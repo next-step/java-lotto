a49dd0beb7b59ea1775b02411859f5445c17af9b
김우재 <kwj1270@naver.com>
2021-04-06 10:34:37 +0900

    feat:LottoNumber 인스턴스가 캐싱된 값을 반환하는 기능 구현

--- src/main/java/step4/domain/lotto/LottoNumber.java
+++ src/main/java/step4/domain/lotto/LottoNumber.java
@@ -2,12 +2,24 @@ package step4.domain.lotto;
 
 import step4.exception.LottoNumberOutOfRangeException;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public final class LottoNumber {
+
+    private static final Map<Integer, LottoNumber> CACHE;
 
     private static final int MINIMUM = 1;
     private static final int MAXIMUM = 45;
 
     private final int lottoNumber;
+
+    static {
+        CACHE = new TreeMap<>();
+        for (int i = MINIMUM; i <= MAXIMUM; i++) {
+            CACHE.put(i, new LottoNumber(i));
+        }
+    }
 
     private LottoNumber(int lottoNumber) {
         validateRange(lottoNumber);
@@ -15,10 +27,11 @@ public final class LottoNumber {
     }
 
     public final static LottoNumber valueOf(int lottoNumber) {
-        return new LottoNumber(lottoNumber);
+        validateRange(lottoNumber);
+        return CACHE.get(lottoNumber);
     }
 
-    private final void validateRange(int lottoNumber) {
+    private static final void validateRange(int lottoNumber) {
         if (lottoNumber < MINIMUM || lottoNumber > MAXIMUM) {
             throw new LottoNumberOutOfRangeException();
         }
