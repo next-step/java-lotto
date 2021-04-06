15c7b2fdcfd201da908bdc9920469c7ece7f2bb2
김우재 <kwj1270@naver.com>
2021-04-06 10:42:37 +0900

    test:LottoNumber 인스턴스가 캐싱된 모든값을 반환하는지 테스트작성

--- src/test/java/step4/domain/lotto/LottoNumberTest.java
+++ src/test/java/step4/domain/lotto/LottoNumberTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import step4.exception.LottoNumberOutOfRangeException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,20 +29,6 @@ class LottoNumberTest {
         assertThat(lottoNumber).isNotNull();
     }
 
-    @DisplayName("LottoNumber 인스턴스가 캐싱된 값을 반환하는지 테스트")
-    @Test
-    void 비교_캐싱() {
-        // given
-        int inputValue = 1;
-
-        // when
-        LottoNumber expectLottoNumber = LottoNumber.valueOf(inputValue);
-        LottoNumber actualLottoNumber = LottoNumber.valueOf(inputValue);
-
-        // then
-        assertThat(actualLottoNumber).isEqualTo(expectLottoNumber);
-    }
-
     @DisplayName("LottoNumber 인스턴스에 부적절한 값 주입시 예외처리 여부 테스트")
     @ParameterizedTest(name = "{index}번 째 반복, 주입값 : {0}")
     @ValueSource(ints = {-1, 0, 46})
@@ -51,5 +41,33 @@ class LottoNumberTest {
     }
 
 
+    @DisplayName("LottoNumber 인스턴스가 캐싱된 값을 반환하는지 테스트")
+    @Test
+    void 반환_캐싱() {
+        // given
+        int inputValue = 1;
+
+        // when
+        LottoNumber expectedLottoNumber = LottoNumber.valueOf(inputValue);
+        LottoNumber actualLottoNumber = LottoNumber.valueOf(inputValue);
+
+        // then
+        assertThat(actualLottoNumber).isEqualTo(expectedLottoNumber);
+    }
+
+    @DisplayName("LottoNumber 인스턴스가 캐싱된 모든값을 반환하는지 테스트")
+    @Test
+    void 반환_캐싱된_모든값() {
+        // given
+        List<LottoNumber> expectedLottoNumbers = IntStream.range(1, 46)
+                                                .mapToObj(LottoNumber::valueOf)
+                                                .collect(Collectors.toList());
+
+        // when
+        List<LottoNumber> actualLottoNumbers = LottoNumber.values();
+
+        // then
+        assertThat(actualLottoNumbers).isEqualTo(expectedLottoNumbers);
+    }
 
 }
