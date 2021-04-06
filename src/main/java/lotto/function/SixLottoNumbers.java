92e5e215afa3c646102771e2358d129b973ca176
gmoon <gmoon@rsupport.com>
2021-04-06 23:48:39 +0900

    refactoring SixLottoNumbers 메모리 성능 고려하여 개선 - 반복된 6개의 로또 번호 리스트를 static 영역에 선언하여 클래스 생성할 때 한번만 생성되도록 수정

--- src/main/java/lotto/function/SixLottoNumbers.java
+++ src/main/java/lotto/function/SixLottoNumbers.java
@@ -9,14 +9,17 @@ import java.util.stream.IntStream;
 
 public class SixLottoNumbers implements GenerateNumbers {
   private static final int LOTTO_NUMBER_GREATER_THAN_OR_EQUALS = 1;
-  private static final int LOTTO_NUMBER_LESS_THAN = 46;
+  private static final int LOTTO_NUMBER_LESS_THAN = 45;
+  private static List<LottoNumber> sixLottoNumbers;
+
+  static {
+    sixLottoNumbers = IntStream.rangeClosed(LOTTO_NUMBER_GREATER_THAN_OR_EQUALS, LOTTO_NUMBER_LESS_THAN)
+            .mapToObj(LottoNumber::generate)
+            .collect(Collectors.toList());
+  }
 
   @Override
   public List<LottoNumber> get() {
-    List<LottoNumber> sixLottoNumbers = IntStream.range(LOTTO_NUMBER_GREATER_THAN_OR_EQUALS, LOTTO_NUMBER_LESS_THAN)
-            .mapToObj(LottoNumber::generate)
-            .collect(Collectors.toList());
-
     Collections.shuffle(sixLottoNumbers);
     return sixLottoNumbers.subList(0, 6);
   }
