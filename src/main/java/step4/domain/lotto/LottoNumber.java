9f0014961842622ff02f262897079b6bf2128453
김우재 <kwj1270@naver.com>
2021-04-06 10:20:28 +0900

    test:LottoNumber 인스턴스에 부적절한 값 주입시 예외처리 여부 테스트작성

--- src/main/java/step4/domain/lotto/LottoNumber.java
+++ src/main/java/step4/domain/lotto/LottoNumber.java
@@ -1,4 +1,14 @@
 package step4.domain.lotto;
 
 public class LottoNumber {
+    private final int lottoNumber;
+
+    private LottoNumber(int lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+
+    public static LottoNumber valueOf(int lottoNumber) {
+        return new LottoNumber(lottoNumber);
+    }
+
 }
--- src/test/java/step4/domain/lotto/LottoNumberTest.java
+++ src/test/java/step4/domain/lotto/LottoNumberTest.java
@@ -2,8 +2,12 @@ package step4.domain.lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import step3.exception.LottoNumberOutOfRangeException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberTest {
@@ -19,5 +23,16 @@ class LottoNumberTest {
 
         // then
         assertThat(lottoNumber).isNotNull();
+    }
+
+    @DisplayName("LottoNumber 인스턴스에 부적절한 값 주입시 예외처리 여부 테스트")
+    @ParameterizedTest(name = "{index}번 째 반복, 주입값 : {0}")
+    @ValueSource(ints = {-1, 0, 46})
+    void 검증(int inputValue) {
+
+        // then
+        assertThatThrownBy(()-> LottoNumber.valueOf(inputValue))
+                .isInstanceOf(LottoNumberOutOfRangeException.class)
+                .hasMessageContaining("로또의 범위를 벗아난 숫자가 입력되었습니다.");
     }
 }
