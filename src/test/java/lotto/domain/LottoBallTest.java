b59a0f4af400b9e681596872d707582ae968361a
suhyunsim <cnc1106@naver.com>
2021-04-08 01:16:01 +0900

    test: 생성자 관련 테스트 추가

--- src/test/java/lotto/domain/LottoBallTest.java
+++ src/test/java/lotto/domain/LottoBallTest.java
@@ -1,19 +1,32 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoBallTest {
 
-    @ParameterizedTest
-    @DisplayName("로또 번호 생성 테스트")
-    @ValueSource(ints = {1, 45})
-    void create(int ball) {
-        assertThat(LottoBall.valueOf(ball)).isNotNull();
+    @Test
+    @DisplayName("로또 번호 생성 테스트 - 생성자")
+    void create_constructor() {
+        assertAll(
+                () -> assertThat(new LottoBall(1).toStringValue()).isEqualTo("1"),
+                () -> assertThat(new LottoBall(45).toStringValue()).isEqualTo("45")
+        );
+    }
+
+    @Test
+    @DisplayName("로또 번호 생성 테스트 - valueOf")
+    void create_valueOf() {
+        assertAll(
+                () -> assertThat(LottoBall.valueOf(1).toStringValue()).isEqualTo("1"),
+                () -> assertThat(LottoBall.valueOf(45).toStringValue()).isEqualTo("45")
+        );
     }
 
     @ParameterizedTest
