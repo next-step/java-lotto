0722b7ec03abd40140c633bef733eec1320be02e
김우재 <kwj1270@naver.com>
2021-04-07 10:15:42 +0900

    test:Money 인스턴스가 특정 값을 기준으로 구매할 수 있는지 여부 테스트작성

--- src/test/java/step4/domain/money/MoneyTest.java
+++ src/test/java/step4/domain/money/MoneyTest.java
@@ -49,4 +49,19 @@ class MoneyTest {
         assertThat(actual).isEqualTo(10);
     }
 
+    @DisplayName("Money 인스턴스가 특정 값을 기준으로 구매할 수 있는지 여부 테스트")
+    @Test
+    void 검증_구매할_수_있는_갯수인지_확인() {
+        // given
+        int value = 10000;
+        int itemAmount = 1000;
+        int purchaseCount = 10;
+
+        // when
+        Money money = Money.valueOf(value);
+        boolean actual = money.isAvailablePurchase(itemAmount, purchaseCount);
+
+        assertThat(actual).isTrue();
+    }
+
 }
