fee94a1c1db29b6f59ae47a9c88d97f894a4d157
Beenie93 <seongbeen93@naver.com>
2021-04-08 00:56:22 +0900

    refactor: apply lazy evaluation

--- src/main/java/lotto/domain/AutoNumberOfTicket.java
+++ src/main/java/lotto/domain/AutoNumberOfTicket.java
@@ -1,32 +1,15 @@
 package lotto.domain;
 
-import java.util.Objects;
-
 public class AutoNumberOfTicket {
     private final ManualNumberOfTicket manualNumberOfTicket;
     private final TotalNumberOfTicket totalNumberOfTicket;
-    private final int autoNumberOfTicket;
 
     public AutoNumberOfTicket(ManualNumberOfTicket manualNumberOfTicket, TotalNumberOfTicket totalNumberOfTicket) {
         this.manualNumberOfTicket = manualNumberOfTicket;
         this.totalNumberOfTicket = totalNumberOfTicket;
-        this.autoNumberOfTicket = totalNumberOfTicket.minus(manualNumberOfTicket);
     }
 
     public int numberOfTicket() {
-        return autoNumberOfTicket;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AutoNumberOfTicket that = (AutoNumberOfTicket) o;
-        return autoNumberOfTicket == that.autoNumberOfTicket;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(autoNumberOfTicket);
+        return totalNumberOfTicket.minus(manualNumberOfTicket);
     }
 }
--- src/test/java/lotto/domain/AutoNumberOfTicketTest.java
+++ src/test/java/lotto/domain/AutoNumberOfTicketTest.java
@@ -10,9 +10,9 @@ public class AutoNumberOfTicketTest {
     @Test
     @DisplayName("총 티켓 수와 수동 티켓 수를 가지고 자동 로또 티켓 수 생성한다.")
     public void create() throws Exception {
-        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(new PurchaseAmount(5_000), new LottoTicketPrice(1_000));
-        ManualNumberOfTicket manualNumberOfTicket = new ManualNumberOfTicket(3, totalNumberOfTicket);
+        TotalNumberOfTicket totalNumberOfTicket = new TotalNumberOfTicket(new PurchaseAmount(10_000), new LottoTicketPrice(1_000));
+        ManualNumberOfTicket manualNumberOfTicket = new ManualNumberOfTicket(5, totalNumberOfTicket);
         AutoNumberOfTicket autoNumberOfTicket = new AutoNumberOfTicket(manualNumberOfTicket, totalNumberOfTicket);
-        assertThat(autoNumberOfTicket).isEqualTo(new AutoNumberOfTicket(manualNumberOfTicket, totalNumberOfTicket));
+        assertThat(autoNumberOfTicket.numberOfTicket()).isEqualTo(5);
     }
 }
