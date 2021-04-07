c588b0f386560cfe51b6afec4103fb10f1838ad2
suhyunsim <cnc1106@naver.com>
2021-04-07 14:38:38 +0900

    test: Money 테스트 추가

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("생성자 테스트")
+    void create() {
+        assertThat(Money.of(14000)).isEqualTo(Money.of(14000));
+    }
+
+    @Test
+    @DisplayName("생성자 예외 테스트")
+    void validate() {
+        assertThatThrownBy(() -> Money.of(900))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("정상 구매 테스트")
+    void purchase() {
+        assertAll(
+                () -> assertThat(Money.of(1000).purchase()).isEqualTo(1),
+                () -> assertThat(Money.of(14000).purchase()).isEqualTo(14)
+        );
+    }
+
+    @Test
+    @DisplayName("비정상 구매 예외 테스트")
+    void validate_purchase() {
+        assertThatThrownBy(() -> Money.of(900).purchase()).isInstanceOf(IllegalArgumentException.class);
+    }
+}
