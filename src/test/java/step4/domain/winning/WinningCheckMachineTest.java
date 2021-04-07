d774dfe391ea995b0969f8468968c47a56cb4ef7
김우재 <kwj1270@naver.com>
2021-04-07 20:53:32 +0900

    test:WinningCheckMachine 인스턴스 생성 여부 테스트작성

--- src/test/java/step4/domain/winning/WinningCheckMachineTest.java
+++ src/test/java/step4/domain/winning/WinningCheckMachineTest.java
@@ -2,7 +2,6 @@ package step4.domain.winning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step4.domain.winning.WinningCheckMachine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
