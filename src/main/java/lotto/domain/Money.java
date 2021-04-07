5ecf6d41310dd1cb8b0fa78de1bcca20830146e0
suhyunsim <cnc1106@naver.com>
2021-04-07 14:39:29 +0900

    feat: 로또를 구매하는 Money 객체 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class Money {
+    public static final int LOTTO_PRICE = 1000;
+    private final int money;
+
+    private Money(int money) {
+        this.money = money;
+    }
+
+    public static Money of(int payment) {
+        validate(payment);
+        return new Money(payment);
+    }
+
+    private static void validate(int payment) {
+        if (payment < LOTTO_PRICE) {
+            throw new IllegalArgumentException("최소 금액은 1000원 입니다.");
+        }
+    }
+
+    public int purchase() {
+        return money / LOTTO_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money1 = (Money) o;
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+}
