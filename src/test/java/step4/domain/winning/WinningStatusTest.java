00f4e7b50a47347bd19f560c5e7e2268d750cdeb
김우재 <kwj1270@naver.com>
2021-04-07 22:55:44 +0900

    test:WinningStatus 인스턴스 음수 주입시 예외처리 검증 테스트작성

--- src/test/java/step4/domain/winning/WinningStatusTest.java
+++ src/test/java/step4/domain/winning/WinningStatusTest.java
@@ -2,8 +2,10 @@ package step4.domain.winning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.exception.InputNegativeAmountException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WinningStatusTest {
@@ -20,5 +22,19 @@ class WinningStatusTest {
 
         // then
         assertThat(winningStatus).isNotNull();
+    }
+
+    @DisplayName("WinningStatus 인스턴스 음수 주입시 예외처리 검증 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int countOfMatch = -1;
+        boolean matchBonus = false;
+
+        // when
+        assertThatThrownBy(() -> WinningStatus.from(countOfMatch, matchBonus))
+                .isInstanceOf(InputNegativeAmountException.class)
+                .hasMessageContaining("음수값이 입력되었습니다.");
+
     }
 }
