ed41842f021268a6ebea96f96d1e9cfce90af417
김우재 <kwj1270@naver.com>
2021-04-07 10:25:19 +0900

    test:PassiveCount 인스턴스 음수 여부 테스트작성

--- src/test/java/step4/domain/count/PassiveCountTest.java
+++ src/test/java/step4/domain/count/PassiveCountTest.java
@@ -2,8 +2,10 @@ package step4.domain.count;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.exception.InputNegativeAmountException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassiveCountTest {
@@ -19,5 +21,18 @@ class PassiveCountTest {
 
         // then
         assertThat(passiveCount).isNotNull();
+    }
+
+    @DisplayName("PassiveCount 인스턴스 음수 여부 테스트")
+    @Test
+    void 검증_음수() {
+        // given
+        int count = -1;
+
+        // when
+        assertThatThrownBy(()-> PassiveCount.valueOf(count))
+                .isInstanceOf(InputNegativeAmountException.class)
+                .hasMessageContaining("음수값이 입력되었습니다.");
+
     }
 }
