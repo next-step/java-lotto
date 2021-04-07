d6eee2ed78c09edffb11144b415c9b4d4dc45f39
suhyunsim <cnc1106@naver.com>
2021-04-08 01:20:58 +0900

    feat: 6개짜리 로또 객체 관련 로직 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private static final int LOTTO_SIZE = 6;
+
+    private final LottoBalls lottoBalls;
+
+    public Lotto() {
+        lottoBalls = new LottoBalls(this::createAutoLotto);
+    }
+
+    public Lotto(LottoBalls lottoBalls) {
+        this.lottoBalls = lottoBalls;
+    }
+
+    public static Lotto of(Integer... lottoBalls) {
+        return new Lotto(LottoBalls.of(lottoBalls));
+    }
+
+    public static Lotto of(List<Integer> winningBalls) {
+        return new Lotto(LottoBalls.createWinningLottoBalls(winningBalls));
+    }
+
+    public List<LottoBall> createAutoLotto() {
+        List<LottoBall> lottoBallList = LottoBall.getAllBalls();
+        Collections.shuffle(lottoBallList);
+        return lottoBallList.stream()
+                .limit(LOTTO_SIZE)
+                .collect(Collectors.toList());
+    }
+
+    public int countSize() {
+        return lottoBalls.size();
+    }
+
+    public int countMatch(WinningLotto winningLotto) {
+        return lottoBalls.count(winningLotto.getWinningLottoBalls());
+    }
+
+    public LottoBalls getLottoBalls() {
+        return lottoBalls;
+    }
+}
