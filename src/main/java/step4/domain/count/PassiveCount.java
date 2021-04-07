c7029fefc350dfe6775ff9000a0ba7a6949aa1f1
김우재 <kwj1270@naver.com>
2021-04-07 10:23:10 +0900

    feat:PassiveCount 인스턴스 생성 기능 구현

--- src/main/java/step4/domain/count/PassiveCount.java
+++ src/main/java/step4/domain/count/PassiveCount.java
@@ -1,4 +1,14 @@
 package step4.domain.count;
 
-public class PassiveCount {
+public final class PassiveCount {
+
+    private final int count;
+
+    private PassiveCount(int count) {
+        this.count = count;
+    }
+
+    public static final PassiveCount valueOf(int count) {
+        return new PassiveCount(count);
+    }
 }
