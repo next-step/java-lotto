c3ed6921c02123a658b463e883703624b4e195dc
김우재 <kwj1270@naver.com>
2021-04-07 21:10:24 +0900

    feat:WinningResult 인스턴스에 Null 주입시 예외처리 여부 테스트작성

--- src/main/java/step4/domain/winning/WinningResult.java
+++ src/main/java/step4/domain/winning/WinningResult.java
@@ -1,18 +1,28 @@
 package step4.domain.winning;
 
 import step3.domain.rank.Rank;
+import step4.exception.MapNullPointerException;
 
 import java.util.Map;
+import java.util.Objects;
 
 public final class WinningResult {
 
     private final Map<Rank, Integer> winningResult;
 
     private WinningResult(Map<Rank, Integer> winningResult) {
+        validateNull(winningResult);
         this.winningResult = winningResult;
     }
 
-    public static WinningResult of(Map<Rank, Integer> winningResult) {
+    public static final WinningResult of(Map<Rank, Integer> winningResult) {
         return new WinningResult(winningResult);
     }
+
+    private final void validateNull(Map<Rank, Integer> winningResult) {
+        if (Objects.isNull(winningResult)) {
+            throw new MapNullPointerException();
+        }
+    }
+
 }
