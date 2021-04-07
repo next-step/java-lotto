b6b743ecffcad1e33828de1859c8ba23d25534b9
gmoon <gmoon@rsupport.com>
2021-04-08 00:16:58 +0900

    refactoring SixLottoNumbers 로또 번호 정렬

--- src/main/java/lotto/function/SixLottoNumbers.java
+++ src/main/java/lotto/function/SixLottoNumbers.java
@@ -2,6 +2,7 @@ package lotto.function;
 
 import lotto.domain.LottoNumber;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,7 +11,7 @@ import java.util.stream.IntStream;
 public class SixLottoNumbers implements GenerateNumbers {
   private static final int LOTTO_NUMBER_GREATER_THAN_OR_EQUALS = 1;
   private static final int LOTTO_NUMBER_LESS_THAN = 45;
-  private static List<LottoNumber> sixLottoNumbers;
+  private static final List<LottoNumber> sixLottoNumbers;
 
   static {
     sixLottoNumbers = IntStream.rangeClosed(LOTTO_NUMBER_GREATER_THAN_OR_EQUALS, LOTTO_NUMBER_LESS_THAN)
@@ -21,6 +22,8 @@ public class SixLottoNumbers implements GenerateNumbers {
   @Override
   public List<LottoNumber> get() {
     Collections.shuffle(sixLottoNumbers);
-    return sixLottoNumbers.subList(0, 6);
+    List<LottoNumber> lottoNumbers = sixLottoNumbers.subList(0, 6);
+    Collections.sort(lottoNumbers, (lotto1, lotto2) -> lotto1.getLottoNumber() >= lotto2.getLottoNumber() ? 1 : -1);
+    return Collections.unmodifiableList(new ArrayList<>(lottoNumbers));
   }
 }
