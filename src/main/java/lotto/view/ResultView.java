0a278471be2c1639c435a5baa321e0d247f96086
suhyunsim <cnc1106@naver.com>
2021-04-08 12:50:59 +0900

    feat: 뷰에 결과를 출력하도록 ResultView 구현

---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,50 @@
+package lotto.view;
+
+import lotto.domain.*;
+
+import java.util.stream.Collectors;
+
+public class ResultView {
+    private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String RESULT_MESSAGE = "당첨 통계";
+    private static final String RESULT_DELIMITER = "---------";
+    private static final String BALL_DELIMITER = ", ";
+    private static final String PROFIT_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+
+    public static void printPurchase(Lottos lottos) {
+        System.out.println(lottos.size() + PURCHASE_MESSAGE);
+        for (Lotto lotto : lottos.getLottos()) {
+            printLottoNumbers(lotto.getLottoBalls());
+        }
+        System.out.println();
+    }
+
+    public static void printLottoNumbers(LottoBalls lottoBalls) {
+        System.out.println(lottoBalls.getSortedLottoBalls().stream()
+        .map(LottoBall::toStringValue)
+        .collect(Collectors.joining(BALL_DELIMITER, "[", "]")));
+    }
+
+    public static void printWinningResult(LottoResult result) {
+        System.out.println();
+        System.out.println(RESULT_MESSAGE);
+        System.out.println(RESULT_DELIMITER);
+        printPrizeResult(result, Prize.THREE);
+        printPrizeResult(result, Prize.FOUR);
+        printPrizeResult(result, Prize.FIVE);
+        printPrizeResult(result, Prize.SIX);
+    }
+
+    public static void printEarningRate(Money payment, LottoResult result) {
+        int price = payment.getMoney();
+        double earningRate = result.profit() / price;
+        System.out.printf("총 수익률은 %.2f입니다.",earningRate);
+        if (earningRate < 1) {
+            System.out.println(PROFIT_MESSAGE);
+        }
+    }
+
+    private static void printPrizeResult(LottoResult result, Prize prize) {
+        System.out.println(prize.getMessage() + " - " + result.counts(prize) + "개");
+    }
+}
