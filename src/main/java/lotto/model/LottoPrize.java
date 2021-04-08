1ec16af019258bb53f1efae442950c97c367946c
Hoony(JeongByeongHun) <jbh1024@gmail.com>
2021-04-08 20:04:21 +0900

    Update src/main/java/lotto/model/LottoPrize.java
    refactor : improve readability
    
    Co-authored-by: dbeod2 <70149382+dbeod2@users.noreply.github.com>

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum LottoPrize {
 
-    FIRST(6,2000000000),
-    SECOND(5,1500000),
-    THIRD(4,50000),
-    FOURTH(3,5000);
+FIRST(6, 2_000_000_000),
+SECOND(5, 1_500_000),
+THIRD(4, 50_000),
+FOURTH(3, 5_000);
 
     private int matchs;
     private int prize;
