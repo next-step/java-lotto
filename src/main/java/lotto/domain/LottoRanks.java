2666fdef0d606f31849f0e51f01cf94bc10a5aab
dailyworker <workingssu@gmail.com>
2021-04-09 21:35:57 +0900

    feat : List<LottoRank> 일급 콜렉션화

---
+++ src/main/java/lotto/domain/LottoRanks.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoRanks {
+  private final List<LottoRank> lottoRanks;
+
+  public LottoRanks(List<LottoRank> lottoRanks) {
+    this.lottoRanks = lottoRanks;
+  }
+}
