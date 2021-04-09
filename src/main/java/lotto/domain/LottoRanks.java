eefbfeb481f1ff766774ed72b69b78ffd44d8f6a
dailyworker <workingssu@gmail.com>
2021-04-09 23:52:57 +0900

    feat : LottoRanks를 통한 매칭 결과 가져오기 구현 및 테스트코드 추가

--- src/main/java/lotto/domain/LottoRanks.java
+++ src/main/java/lotto/domain/LottoRanks.java
@@ -1,11 +1,21 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class LottoRanks {
+
   private final List<LottoRank> lottoRanks;
 
   public LottoRanks(List<LottoRank> lottoRanks) {
     this.lottoRanks = lottoRanks;
+  }
+
+  public Map<LottoRank, Long> getMatchResult() {
+    return lottoRanks.stream()
+        .filter(LottoRank::isNotNone)
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
   }
 }
---
+++ src/test/java/lotto/domain/LottoRanksTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoRanksTest {
+
+  private static final List<LottoRank> lottoRankList = new ArrayList<>();
+
+  @BeforeAll
+  static void setUp() {
+    lottoRankList.add(LottoRank.FIFTH);
+    lottoRankList.add(LottoRank.FOURTH);
+    lottoRankList.add(LottoRank.THIRD);
+    lottoRankList.add(LottoRank.SECOND);
+    lottoRankList.add(LottoRank.FIRST);
+  }
+
+  @ParameterizedTest
+  @CsvSource({"FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH"})
+  @DisplayName("매칭 결과를 얻을 수 있는가")
+  public void getMatchResult(LottoRank value) throws Exception {
+    //given
+    LottoRanks lottoRanks = new LottoRanks(lottoRankList);
+    //when
+    Map<LottoRank, Long> matchResult = lottoRanks.getMatchResult();
+
+    //then
+    assertTrue(matchResult.containsKey(value));
+  }
+}
