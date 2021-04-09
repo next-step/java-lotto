39a98add0a425b6e4ac8d6fab5412008aafaa08c
yurim022 <yurim2220@gmail.com>
2021-04-10 02:19:05 +0900

    feat(StringCalculator)

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,4 +1,57 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-    public static int splitAndSum(Object o) {
+    public static int splitAndSum(String text) {
+
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        if ( text.length() == 1) {
+            int number = Integer.parseInt(text);
+            validCheck(number);
+            return number;
+        }
+
+        String[] tokens = ifCustomDelimiter(text);
+
+        if (isNotCustomDelimiter(tokens)) {
+            tokens = text.split(",|:");
+        }
+
+        return getSum(tokens);
+    }
+
+    private static boolean isNotCustomDelimiter(String[] tokens) {
+        return tokens == null;
+    }
+
+    private static String[] ifCustomDelimiter(String text) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2)
+                    .split(customDelimiter);
+        }
+        return null;
+    }
+
+    private static int getSum(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .map(num -> {
+                    if (num < 0)
+                        throw new NotValidCalculatorValue("음수는 취급하지 않습니다.");
+                    return num;
+                })
+                .sum();
+    }
+
+    private static void validCheck(Integer number){
+        if (number < 0) {
+            throw new NotValidCalculatorValue("음수는 취급하지 않습니다.");
+        }
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -4,9 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
-
     @Test
-    public void splitAndSum_null_또는_빈문자(){
+    public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
@@ -21,15 +20,26 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_쉽표_또는_콜론_구분자() throws Exception {
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(()-> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
-
 }
