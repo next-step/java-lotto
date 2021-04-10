bbb4777ace01a8853081f1929f0331b66c30fb3a
Beenie93 <seongbeen93@naver.com>
2021-04-10 12:40:37 +0900

    refactor: remove constants for readability

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -9,20 +9,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ResultView {
-    private static final String GOT_TICKETS = "수동으로 %d장, 자동으로 %d장을 구매했습니다";
     private static final String OPEN_BRACKET = "[";
     private static final String CLOSED_BRACKET = "]";
     private static final String DELIMITER = ", ";
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String BOUNDARY = "---------";
-    private static final String MATCH_PRIZE_COUNT = "%d개 일치%s(%d원)- %d개";
     private static final String MATCH_BONUS = ", 보너스 볼 일치";
     private static final String BLANK = " ";
-    private static final String PLUS_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 이득이라는 의미임)";
-    private static final String MINUS_PROFIT_RATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
     public void purchaseTickets(int manualNumberOfTicket, int autoNumberOfTickets) {
-        System.out.printf((GOT_TICKETS) + "%n", manualNumberOfTicket, autoNumberOfTickets);
+        System.out.printf("수동으로 %d장, 자동으로 %d장을 구매했습니다%n", manualNumberOfTicket, autoNumberOfTickets);
     }
 
     public void lottoNumbers(LottoTickets lottotickets) {
@@ -41,7 +37,7 @@ public class ResultView {
         System.out.println(BOUNDARY);
         for (RankCountDto rankCount : ranksCount) {
             String bonus = bonus(rankCount);
-            System.out.printf((MATCH_PRIZE_COUNT) + "%n", rankCount.matchedCountStandard(), bonus, rankCount.prize(), rankCount.matchedCountAfterComparison());
+            System.out.printf("%d개 일치%s(%d원)- %d개%n", rankCount.matchedCountStandard(), bonus, rankCount.prize(), rankCount.matchedCountAfterComparison());
         }
     }
 
@@ -54,10 +50,10 @@ public class ResultView {
     }
 
     public void positiveProfitRate(double profitRate) {
-        System.out.printf((PLUS_PROFIT_RATE) + "%n", profitRate);
+        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 이득이라는 의미임)%n", profitRate);
     }
 
     public void negativeProfitRate(double profitRate) {
-        System.out.printf((MINUS_PROFIT_RATE) + "%n", profitRate);
+        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)%n", profitRate);
     }
 }
