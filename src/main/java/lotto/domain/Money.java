86117ab5f53f4942dafce6218162be1f99456557
HeeSeok Noh <nhs0912@nate.com>
2021-04-10 15:34:21 +0900

    refact: Money class constructor 삭제

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,10 +3,6 @@ package lotto.domain;
 public class Money {
     private int won = 0;
 
-    public Money(final String won) {
-        this(Integer.parseInt(won));
-    }
-
     public Money(final int won) {
         this.won = won;
     }
