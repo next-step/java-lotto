a6b99f5f5f0e9e9808042f01343f225a8bd754fc
gmoon <gmoon@rsupport.com>
2021-04-13 02:41:53 +0900

    feat 로또 보너스 볼 래퍼 클래스 생성 - equals, hashCode, toString 재정의

---
+++ src/main/java/lotto/domain/LottoBonusBall.java
@@ -0,0 +1,41 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoBonusBall {
+  private final LottoNumber value;
+
+  private LottoBonusBall(LottoNumber value) {
+    this.value = value;
+  }
+
+  public static LottoBonusBall create(int number) {
+    return new LottoBonusBall(LottoNumber.generate(number));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+
+    if (o instanceof LottoBonusBall) {
+      LottoBonusBall bonusBall = (LottoBonusBall) o;
+      return Objects.equals(value, bonusBall.value);
+    } else if (o instanceof LottoNumber) {
+      LottoNumber lottoNumber = (LottoNumber) o;
+      return o.equals(lottoNumber);
+    }
+
+    return this.getClass() == o.getClass();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+}
---
+++ src/test/java/lotto/domain/LottoBonusBallTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoBonusBallTest {
+
+  @Test
+  @DisplayName("보너스 볼을 생성한다.")
+  void create() {
+    // given
+    int number = 1;
+
+    // when
+    LottoBonusBall bonusBall = LottoBonusBall.create(number);
+
+    // then
+    assertThat(bonusBall).isEqualTo(LottoNumber.generate(1));
+  }
+}
