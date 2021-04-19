861c4ec4d20c0877266b94f034938ed63624e70e
sonAjin <sonajin11@gmail.com>
2021-04-20 00:48:54 +0900

    (refactor) 수동 당첨 번호 중복 값 Exception 처리

--- src/main/java/step02/model/numberRule/InputNumberRule.java
+++ src/main/java/step02/model/numberRule/InputNumberRule.java
@@ -1,8 +1,10 @@
 package step02.model.numberRule;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static step02.utils.LottoConfig.*;
 
@@ -19,9 +21,15 @@ public class InputNumberRule extends NumberRule {
     }
 
     private List<Integer> splitNumber(String numbers) {
-       String numbersNoBlank = numbers.replaceAll(BLANK, NO_BLANK);
-       return Arrays.stream(numbersNoBlank.trim().split(REGEX))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        String numbersNoBlank = numbers.replaceAll(BLANK, NO_BLANK);
+        Stream<Integer> numberSplit = Arrays.stream(numbersNoBlank.trim().split(REGEX))
+                .map(Integer::parseInt);
+
+        boolean unique = numberSplit.allMatch(new HashSet<>()::add);
+
+        if (unique) {
+            return numberSplit.collect(Collectors.toList());
+        }
+        throw new IllegalArgumentException(ERROR_DUPLICATED_NUMBER);
     }
 }
