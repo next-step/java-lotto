807a0763496b266ce57364e8ca096eb051aa1ddb
gmoon <gmoon@rsupport.com>
2021-04-21 00:06:31 +0900

    refactor LottoTicket 메서드 및 파라미터 네이밍 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoTicket {
-  private static final String ERROR_BOUGHT_LIMIT_FOR_LOTTO_OVER_FORMAT = "구입 금액에 맞는 구매할 로또 총 수는 %d 입니다.";
+  private static final String ERROR_LIMIT_BOUGHT_LOTTO_OVER_FORMAT = "구입 금액에 맞는 구매할 로또 총 수는 %d 입니다.";
   private static final int PRICE = 1_000;
 
   private final List<LottoNumbers> values;
@@ -20,39 +20,40 @@ public class LottoTicket {
     this.values = Collections.unmodifiableList(values);
   }
 
-  public static LottoTicket toBuy(final int money,
+  public static LottoTicket toBuy(final int myMoney,
                                   final GenerateNumbers generateNumbers,
                                   final List<String> manualLottoStrings) {
     final int countOfManualLotto = manualLottoStrings.size();
 
-    List<LottoNumbers> values = new ArrayList<>(countOfTotalLottoNumbers(money));
-    values.addAll(getManualLottoNumbers(money, manualLottoStrings, countOfManualLotto));
-    values.addAll(getAutoLottoNumbers(money, generateNumbers, countOfManualLotto));
+    List<LottoNumbers> values = new ArrayList<>(countOfTotalLottery(myMoney));
+    values.addAll(getManualLottoNumbers(myMoney, manualLottoStrings, countOfManualLotto));
+    values.addAll(getAutoLottoNumbers(myMoney, generateNumbers, countOfManualLotto));
     return new LottoTicket(values);
   }
 
-  private static List<LottoNumbers> getAutoLottoNumbers(int money, GenerateNumbers generateNumbers, int countOfManualLotto) {
-    final int availableMoney = money - ( countOfManualLotto * PRICE );
-    final int countOfMakeAutoLottoNumbers = countOfTotalLottoNumbers(availableMoney);
-    return IntStream.range(0, countOfMakeAutoLottoNumbers)
+  private static List<LottoNumbers> getAutoLottoNumbers(int myMoney, GenerateNumbers generateNumbers, int countOfManualLotto) {
+    final int availableMoney = myMoney - ( countOfManualLotto * PRICE );
+    final int countOfAutoLotto = countOfTotalLottery(availableMoney);
+    checkExceedMaximumBuyToLotto(availableMoney, countOfAutoLotto);
+    return IntStream.range(0, countOfAutoLotto)
             .mapToObj(i -> LottoNumbers.generateSixNumbers(generateNumbers))
             .collect(Collectors.toList());
   }
 
-  private static List<LottoNumbers> getManualLottoNumbers(int money, List<String> manualLottoStrings, int countOfManualLotto) {
-    checkBoughtManualLottoExceedMoney(money, countOfManualLotto);
+  private static List<LottoNumbers> getManualLottoNumbers(int myMoney, List<String> manualLottoStrings, int countOfManualLotto) {
+    checkExceedMaximumBuyToLotto(myMoney, countOfManualLotto);
     return manualLottoStrings.stream()
             .map(LottoNumbers::generateSixNumbersFromStringNumbers)
             .collect(Collectors.toList());
   }
 
-  private static void checkBoughtManualLottoExceedMoney(int money, int countOfManualLotto) {
-    if (countOfTotalLottoNumbers(money) - countOfManualLotto < 0) {
-      throw new IllegalArgumentException(String.format(ERROR_BOUGHT_LIMIT_FOR_LOTTO_OVER_FORMAT, countOfTotalLottoNumbers(money)));
+  private static void checkExceedMaximumBuyToLotto(int myMoney, int countOfBoughtLotto) {
+    if (countOfTotalLottery(myMoney) - countOfBoughtLotto < 0) {
+      throw new IllegalArgumentException(String.format(ERROR_LIMIT_BOUGHT_LOTTO_OVER_FORMAT, countOfTotalLottery(myMoney)));
     }
   }
 
-  private static int countOfTotalLottoNumbers(int money) {
+  private static int countOfTotalLottery(int money) {
     return money / PRICE;
   }
 
