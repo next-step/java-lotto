8d7c93f7d59528690781ebece17d5916865522b0
0hun <whdudgns2654@gmail.com>
2021-04-23 00:24:32 +0900

    refactor : convertNumbers 함수 추가
    convertNumbers 함수 추가
    - 객체를 생성하는 것 대신 타입을 변환하는 경우 단순 함수로 적용

--- src/main/java/study/step1/StringAddCalculator.java
+++ src/main/java/study/step1/StringAddCalculator.java
@@ -1,5 +1,9 @@
 package study.step1;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringAddCalculator {
 
   public StringAddCalculator() {
@@ -12,13 +16,19 @@ public class StringAddCalculator {
 
     TokenDelimiter tokenDelimiter = new TokenDelimiter(text);
 
-    TokenNumbers tokenNumbers = new TokenNumbers(tokenDelimiter.getNumberTokens());
+    List<Integer> convertedNumbers = convertNumbers(tokenDelimiter.getNumberTokens());
 
-    Numbers numbers = new Numbers(tokenNumbers.convertNumbers());
+    Numbers numbers = new Numbers(convertedNumbers);
 
     numbers.validatorNumbers();
 
     return numbers.sum();
+  }
+
+  public List<Integer> convertNumbers(String[] tokenNumber) {
+    return Arrays.stream(tokenNumber)
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
   }
 
 }
