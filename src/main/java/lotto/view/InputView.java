64598299ebfc1e21f0817861b0f916fb85f695d3
suhyunsim <cnc1106@naver.com>
2021-04-23 21:56:29 +0900

    feat: 보너스볼을 InputView에서 입력받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
 
     private static final String INPUT_MESSAGE = "구입금액을 입력해주세요.";
     private static final String LAST_WEEK_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_LOTTO = "보너스 볼을 입력해 주세요.";
     private static final String SPLIT_DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -31,5 +32,10 @@ public class InputView {
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println(BONUS_LOTTO);
+        return scanner.nextInt();
     }
 }
