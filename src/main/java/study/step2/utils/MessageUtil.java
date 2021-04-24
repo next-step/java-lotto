286c3b4d8a9a19f3f87bc74750cadaea364d416c
0hun <whdudgns2654@gmail.com>
2021-04-25 02:44:54 +0900

    feat (feature) : MessageUtil 클래스 추가
    MessageUtil 클래스 추가
    - 메세지 출력을 위해 Util 클래스 추가

---
+++ src/main/java/study/step2/utils/MessageUtil.java
@@ -0,0 +1,13 @@
+package study.step2.utils;
+
+public class MessageUtil {
+
+  private MessageUtil() {}
+
+  public static final String VALIDATOR_MONEY_MESSAGE = "구입금액은 1000원이상으로 입력하세요.";
+  public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+  public static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+  public static final String LAST_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+  public static final String VALIDATOR_NUMBER_MESSAGE = "0보다 큰 값을 입력하세요";
+  public static final String LOTTO_RESULT_MESSAGE = "당첨 통계";
+}
