bd955c4194bd195f972c1a65152ed9c5040a21f4
0hun <whdudgns2654@gmail.com>
2021-04-25 02:29:35 +0900

    feat (feature) : Lotto 클래스 추가
    Lotto 클래스 추가
    - 로또 번호를 담는 Lotto 클래스 추가

---
+++ src/main/java/study/step2/domain/Lotto.java
@@ -0,0 +1,38 @@
+package study.step2.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+
+  private List<Integer> lottoNumbers;
+
+  public Lotto() {
+  }
+
+  public Lotto(List<Integer> lottoNumbers) {
+    this.lottoNumbers = lottoNumbers;
+  }
+
+  public List<Integer> getLottoNumbers() {
+    return lottoNumbers;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Lotto lotto = (Lotto) o;
+    return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lottoNumbers);
+  }
+
+}
