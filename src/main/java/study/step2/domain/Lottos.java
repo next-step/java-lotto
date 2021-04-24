19fe4fb99f112a9a6b6ad1936fb937654d8684a1
0hun <whdudgns2654@gmail.com>
2021-04-25 02:32:57 +0900

    feat (feature) : Lottos 클래스 추가
    Lottos 클래스 추가
    - Lottos 객체를 담는 일급 컬렉션 추가

---
+++ src/main/java/study/step2/domain/Lottos.java
@@ -0,0 +1,37 @@
+package study.step2.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lottos {
+
+  private List<Lotto> lottos;
+
+  public Lottos() {
+  }
+
+  public Lottos(List<Lotto> lottos) {
+    this.lottos = lottos;
+  }
+
+  public List<Lotto> getLottos() {
+    return lottos;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Lottos lottos1 = (Lottos) o;
+    return Objects.equals(lottos, lottos1.lottos);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lottos);
+  }
+}
