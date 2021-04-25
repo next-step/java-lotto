27af48a89222dcd2ea09d76ed7d250f0c6cd68ed
0hun <whdudgns2654@gmail.com>
2021-04-25 21:24:24 +0900

    test (when adding missing tests) : LottoResultTest 추가
     LottoResultTest 추가
     - 로또 결과를 담는 LottoResult를 테스트하는 테스트 클래스 추가

---
+++ src/test/java/study/step2/LottoResultTest.java
@@ -0,0 +1,40 @@
+package study.step2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import study.step2.domain.LottoResult;
+import study.step2.domain.Rank;
+
+public class LottoResultTest {
+
+  @DisplayName("로또 번호 일치하는 갯수 테스트")
+  @Test
+  void lottoResult() {
+    // given
+    LottoResult lottoResult = new LottoResult();
+
+    // when
+    lottoResult.add(Rank.THIRD);
+    lottoResult.add(Rank.THIRD);
+
+    // then
+    assertThat(lottoResult.getValue(Rank.THIRD)).isEqualTo(2);
+  }
+
+  @DisplayName("로또 번호 일치하는 갯수 테스트")
+  @Test
+  void lottoResultFail() {
+    // given
+    LottoResult lottoResult = new LottoResult();
+
+    // when
+    lottoResult.add(Rank.THIRD);
+    lottoResult.add(Rank.THIRD);
+
+    // then
+    assertThat(lottoResult.getValue(Rank.FIRST)).isEqualTo(0);
+  }
+
+}
