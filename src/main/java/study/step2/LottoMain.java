147a62303ad04e3d9637016ab7053cb951548f4b
0hun <whdudgns2654@gmail.com>
2021-04-25 22:19:48 +0900

    feat : LottoMain 추가
    LottoMain 추가
    - 로또 당청을 하기 위한 메인 클래스 추가

---
+++ src/main/java/study/step2/LottoMain.java
@@ -0,0 +1,20 @@
+package study.step2;
+
+import study.step2.controller.LottoController;
+import study.step2.domain.Lotto;
+import study.step2.domain.Lottos;
+import study.step2.view.InputView;
+
+public class LottoMain {
+
+  public static void main(String[] args) {
+    int money = InputView.inputMoney();
+
+    LottoController lottoController = new LottoController();
+    Lottos lottos = lottoController.createLottos(money);
+
+    Lotto winningLotto = new Lotto(InputView.inputLastLottoNumbers());
+    lottoController.findLottoWinning(lottos, winningLotto, money);
+  }
+
+}
