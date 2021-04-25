738bb69ce57116485a51f1b90cdffe7e520a8414
0hun <whdudgns2654@gmail.com>
2021-04-25 22:14:53 +0900

    refactor : makeLottos 수정
    makeLottos 수정
    - 리스트를 생성해서 lotto 객체를 추가하는 것 대신에 Lottos 객체를 생성하여 추가하도록 수정

--- src/main/java/study/step2/domain/LottosFactory.java
+++ src/main/java/study/step2/domain/LottosFactory.java
@@ -1,22 +1,19 @@
 package study.step2.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottosFactory {
 
   public static Lottos makeLottos(int lottoCount) {
-    List<Lotto> lottoList = new ArrayList<>();
+    Lottos lottos = new Lottos();
 
     for (int i = 0; i < lottoCount; i++) {
       List<Integer> pickedLottoNumbers = LottoNumbers.pick();
       Lotto lotto = new Lotto(pickedLottoNumbers);
-      lottoList.add(lotto);
+      lottos.add(lotto);
     }
 
-    return new Lottos(lottoList);
+    return lottos;
   }
-
-
 
 }
