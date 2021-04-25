0159bc4e5622ec8a6e2ac1a12f07fa24153d11fe
0hun <whdudgns2654@gmail.com>
2021-04-25 21:26:24 +0900

    test (when adding missing tests) : ValidatorTest 메소드 추가
     ValidatorTest 메소드 추가
     - 입력한 로또 번호가 비어있을 경우 Exception 테스트
     - 입력한 로또 번호에 음수가 포함되어있을 경우 Exception 테스트
     - 입력한 로또 번호에 중복된 번호가 포함되어있을 경우 Exception 테스트

--- src/test/java/study/step2/ValidatorTest.java
+++ src/test/java/study/step2/ValidatorTest.java
@@ -3,6 +3,8 @@ package study.step2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import study.step2.validator.Validator;
@@ -33,6 +35,51 @@ public class ValidatorTest {
     // when
     Throwable thrown = catchThrowable(() -> {
       Validator.validatorMoney(money);
+    });
+
+    // then
+    assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("입력한 로또 번호가 비어있을 경우 Exception 테스트")
+  @Test
+  void validatorIsEmpty() {
+    // given
+    String text = "";
+
+    // when
+    Throwable thrown = catchThrowable(() -> {
+      Validator.isEmpty(text);
+    });
+
+    // then
+    assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("입력한 로또 번호에 음수가 포함되어있을 경우 Exception 테스트")
+  @Test
+  void validatorPositiveNumbersNumbers() {
+    // given
+    List<Integer> positiveNumbers = Arrays.asList(1, -1, 3, 4, 6, 11);
+
+    // when
+    Throwable thrown = catchThrowable(() -> {
+      Validator.validatorPositiveNumbers(positiveNumbers);
+    });
+
+    // then
+    assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("입력한 로또 번호에 중복된 번호가 포함되어있을 경우 Exception 테스트")
+  @Test
+  void checkDuplication() {
+    // given
+    List<Integer> numbers = Arrays.asList(1, 1, 3, 4, 6, 11);
+
+    // when
+    Throwable thrown = catchThrowable(() -> {
+      Validator.checkDuplication(numbers);
     });
 
     // then
