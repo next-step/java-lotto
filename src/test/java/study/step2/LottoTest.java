ea7a8d0fc645623a4def1cb6fc92afb0047d9426
0hun <whdudgns2654@gmail.com>
2021-04-25 21:25:32 +0900

    test (when adding missing tests) : LottoTest 메소드 추가
     LottoTest 메소드 추가
     - 당첨 번호와 로또 번호가 몇개 맞는지 테스트
     - 당첨 번호와 로또 번호가 맞지 않는지 테스트

--- src/test/java/study/step2/LottoTest.java
+++ src/test/java/study/step2/LottoTest.java
@@ -3,6 +3,7 @@ package study.step2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,34 @@ public class LottoTest {
 
     // then
     assertThat(lotto).isEqualTo(new Lotto(pickedLottoNumbers));
+  }
+
+  @DisplayName("당첨 번호와 로또 번호가 몇개 맞는지 테스트")
+  @Test
+  void lottoNumberMatch() {
+    // given
+    List<Integer> winningLottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    List<Integer> pickedLottoNumbers = Arrays.asList(1, 2, 3, 12, 15, 16);
+
+    // when
+    Lotto lotto = new Lotto(pickedLottoNumbers);
+
+    // then
+    assertThat(lotto.matchCount(winningLottoNumbers)).isEqualTo(3);
+  }
+
+  @DisplayName("당첨 번호와 로또 번호가 맞지 않는지 테스트")
+  @Test
+  void lottoNumberMatchFail() {
+    // given
+    List<Integer> winningLottoNumbers = Arrays.asList(10, 11, 17, 18, 19, 20);
+    List<Integer> pickedLottoNumbers = Arrays.asList(1, 2, 3, 12, 15, 16);
+
+    // when
+    Lotto lotto = new Lotto(pickedLottoNumbers);
+
+    // then
+    assertThat(lotto.matchCount(winningLottoNumbers)).isEqualTo(0);
   }
 
 }
