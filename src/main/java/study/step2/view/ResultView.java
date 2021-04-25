2af2a29e8b858cd477aabb43cf0e0416a24947dd
0hun <whdudgns2654@gmail.com>
2021-04-25 22:18:15 +0900

    refactor : ResultView 수정
    ResultView 수정
    - 로또 통계 결과를 출력하는 메소드 추가
    - 수익률을 출력하는 메소드 추가

--- src/main/java/study/step2/view/ResultView.java
+++ src/main/java/study/step2/view/ResultView.java
@@ -3,8 +3,12 @@ package study.step2.view;
 import static study.step2.utils.MessageUtil.LOTTO_COUNT_MESSAGE;
 import static study.step2.utils.MessageUtil.LOTTO_RESULT_MESSAGE;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import study.step2.domain.LottoResult;
 import study.step2.domain.Lottos;
+import study.step2.domain.Rank;
 
 public class ResultView {
 
@@ -34,9 +38,21 @@ public class ResultView {
     System.out.println("---------");
   }
 
-  public static void printLottoMatchCount() {
-    System.out.println(LOTTO_RESULT_MESSAGE);
-    System.out.println("---------");
+  public static void printLottoResult(LottoResult result) {
+    Map<Rank, Integer> rankMap = result.getRankMap();
+
+    rankMap.keySet().stream().sorted(Comparator.comparingInt(Rank::getMatchCount))
+        .map(rank -> rank.getMatchCount()
+        + "개 일치 ("
+        + rank.getPrizeMoney()
+        + ")- "
+        + rankMap.get(rank)
+        + "개")
+        .forEach(System.out::println);
+  }
+
+  public static void printYield(LottoResult result, int money) {
+    System.out.println(String.format("총 수익률은 %.2f 입니다.", result.calculateYield(money)));
   }
 
 }
