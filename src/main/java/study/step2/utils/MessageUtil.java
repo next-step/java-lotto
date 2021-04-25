b132d5faa45714ae6d8aa2d66d0369273a853b6d
0hun <whdudgns2654@gmail.com>
2021-04-25 22:15:32 +0900

    feat : message 추가
    message 추가

--- src/main/java/study/step2/utils/MessageUtil.java
+++ src/main/java/study/step2/utils/MessageUtil.java
@@ -8,6 +8,7 @@ public class MessageUtil {
   public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
   public static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
   public static final String LAST_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
-  public static final String VALIDATOR_NUMBER_MESSAGE = "0보다 큰 값을 입력하세요";
+  public static final String VALIDATOR_NUMBER_MESSAGE = "번호의 개수는 6개여야 합니다.";
   public static final String LOTTO_RESULT_MESSAGE = "당첨 통계";
+  public static final String LOTTO_NUMBER_DUPLICATED = "중복되는 숫자가 포함되어 있는지 확인해주세요.";
 }
