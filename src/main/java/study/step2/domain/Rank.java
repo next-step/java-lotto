945b917a5fc0ef85202f4e085b24d1c44f3a5b32
0hun <whdudgns2654@gmail.com>
2021-04-26 22:46:28 +0900

    refactor : Rank enum 띄어쓰기 적용
    Rank enum 띄어쓰기 적용
    - enum에서 숫자간 가독성을 위해 띄어쓰기 적용

--- src/main/java/study/step2/domain/Rank.java
+++ src/main/java/study/step2/domain/Rank.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum Rank {
 
-  FIRST(6,2_000_000_000),
-  SECOND(5,15_000_000),
-  THIRD(4,50_000),
-  FOURTH(3,5_000),
-  MISS(0,0);
+  FIRST(6, 2_000_000_000),
+  SECOND(5, 15_000_000),
+  THIRD(4, 50_000),
+  FOURTH(3, 5_000),
+  MISS(0, 0);
 
   private final int matchCount;
   private final int prizeMoney;
@@ -26,7 +26,7 @@ public enum Rank {
     return prizeMoney;
   }
 
-  public static Rank find(long count){
+  public static Rank find(long count) {
     return Arrays.stream(values())
         .filter(rank -> rank.matchCount == count)
         .findFirst()
