268b89f9649765075b3ffdabde3c62f249c424cf
suhyunsim <cnc1106@naver.com>
2021-04-28 03:02:27 +0900

    feat: 보너스 볼 경품을 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -5,31 +5,32 @@ import java.util.List;
 
 public enum Prize {
 
-    ZERO(0, 0, "꽝"),
-    THREE(3, 5000, "3개 일치 (5000원)"),
-    FOUR(4, 50_000, "4개 일치 (50000원)"),
-    FIVE(5, 1_500_000, "5개 일치 (15000000원)"),
-    SIX(6, 2_000_000_000, "6개 일치 (2000000000원)");
+    ZERO(new Hit(0, false), 0, "꽝"),
+    THREE(new Hit(3, false), 5_000, "3개 일치 (5000원)"),
+    FOUR(new Hit(4, false), 50_000, "4개 일치 (50000원)"),
+    FIVE(new Hit(5, false), 1_500_000, "5개 일치 (15000000원)"),
+    BONUS(new Hit(5, true), 30_000_000, "5개 일치, 보너스 볼 일치(30000000원)"),
+    SIX(new Hit(6, false), 2_000_000_000, "6개 일치 (2000000000원)");
 
-    private final int winningCount;
+    private final Hit hit;
     private final int reward;
     private final String message;
 
-    Prize(int winningCount, int reward, String message) {
-        this.winningCount = winningCount;
+    Prize(Hit hit, int reward, String message) {
+        this.hit = hit;
         this.reward = reward;
         this.message = message;
     }
 
-    public static Prize of(int winningCount) {
+    public static Prize of(Hit result) {
         return Arrays.stream(Prize.values())
-                .filter(prize -> prize.winningCount == winningCount)
+                .filter(prize -> prize.hit.equals(result))
                 .findAny()
                 .orElseGet(() -> Prize.ZERO);
     }
 
     public static List<Prize> prizeList() {
-        return Arrays.asList(Prize.THREE, Prize.FOUR, Prize.FIVE, Prize.SIX);
+        return Arrays.asList(Prize.THREE, Prize.FOUR, Prize.FIVE, Prize.BONUS, Prize.SIX);
     }
 
     public String getMessage() {
