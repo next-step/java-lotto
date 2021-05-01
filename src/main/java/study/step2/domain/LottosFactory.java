dd4c3db536a9b788647805394bc2b0f624ef716d
0hun <whdudgns2654@gmail.com>
2021-05-02 00:43:41 +0900

    * refactor : makeLottos 수정
     makeLottos 수정
     - for문 대신 stream을 활용하여 변경

--- src/main/java/study/step2/domain/LottosFactory.java
+++ src/main/java/study/step2/domain/LottosFactory.java
@@ -1,20 +1,16 @@
 package study.step2.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottosFactory {
 
   public static Lottos makeLottos(int lottoCount) {
-    List<Lotto> lottoList = new ArrayList<>();
-
-    for (int i = 0; i < lottoCount; i++) {
-      Set<LottoNumber> pickedLottoNumbers = LottoMachine.pick();
-
-      Lotto lotto = new Lotto(pickedLottoNumbers);
-      lottoList.add(lotto);
-    }
+    List<Lotto> lottoList = IntStream.range(0, lottoCount)
+        .mapToObj(i -> LottoMachine.pick())
+        .map(Lotto::new)
+        .collect(Collectors.toList());
 
     return new Lottos(lottoList);
   }
