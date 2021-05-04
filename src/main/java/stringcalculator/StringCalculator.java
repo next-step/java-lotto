14a355ac6ccef4edc866b5b0b805d9736b7cbb69
h1un <h1unha@naver.com>
2021-05-04 15:36:40 +0900

    feat (LottoAutometic) : lottoList 45개 번호 중 셔플하여 6개 추출 구매 개수만큼 로또번호 출력

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,7 +1,6 @@
 package stringcalculator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
---
+++ src/test/java/LottoAutomaticTest/LottoAutomaticTest.java
@@ -0,0 +1,4 @@
+package LottoAutomaticTest;
+
+public class LottoAutomaticTest {
+}
