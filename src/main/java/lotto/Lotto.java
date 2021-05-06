56ca5f032a215e65ed3cffd75bd3845f54b8f572
Miot2J <ck96117@gmail.com>
2021-05-06 17:40:05 +0900

    feat: 로또 생성시 로또 번호 리스트의 길이는 항상 6인 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,13 +3,22 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private static Integer CORRECT_LOTTO_NUMBERS_SIZE = 6;
+    private static String NOT_CORRECT_LOTTO_NUMBERS_MESSAGE = "로또번호는 총 6개여야 합니다.";
     private List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
     public List<Integer> getLottoNumbers() {
         return lottoNumbers;
+    }
+
+    private void validateLottoNumbers(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != CORRECT_LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(NOT_CORRECT_LOTTO_NUMBERS_MESSAGE);
+        }
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,15 +1,24 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoTest {
 
+    @DisplayName("로또 생성시 로또 번호 리스트의 길이가 6이 아니면 에러.")
+//    @ParameterizedTest
+//    @MethodSource() 추후 리스트 던져주는 함수 구현시 사용
+    @Test
+    void validateLottoListTest() {
+        Integer[] lottoNumbers = {1,2,3,4,5,6,7};
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(()->{
+                    new Lotto(Arrays.asList(lottoNumbers));
+                });
+    }
 }
