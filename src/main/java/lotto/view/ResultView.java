7d646a23697481000935e34b11ae3fe82df5e212
suhyunsim <cnc1106@naver.com>
2021-05-11 15:44:12 +0900

    refactor: 수동, 자동 로또를 구매한 결과를 표시하도록 변경

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -10,14 +10,15 @@ public class ResultView {
 
     private ResultView() {}
 
-    private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+    private static final String PURCHASE_MESSAGE = "수동으로 %d장, 자동으로 %d장을 구매했습니다.";
     private static final String RESULT_MESSAGE = "당첨 통계";
     private static final String RESULT_DELIMITER = "---------";
     private static final String BALL_DELIMITER = ", ";
     private static final String PROFIT_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    public static void printPurchase(Lottos lottos) {
-        System.out.println(lottos.size() + PURCHASE_MESSAGE);
+    public static void printPurchase(Lottos lottos, int inputManualCounts) {
+        System.out.printf(PURCHASE_MESSAGE, inputManualCounts, lottos.size() - inputManualCounts);
+        System.out.println();
         for (Lotto lotto : lottos.getLottos()) {
             printLottoNumbers(lotto.getLottoBalls());
         }
@@ -37,8 +38,8 @@ public class ResultView {
         System.out.println(prizeResult(result));
     }
 
-    public static void printEarningRate(Money payment, LottoResult result) {
-        BigDecimal earningRate = result.earningRate(BigDecimal.valueOf(payment.getMoney()));
+    public static void printEarningRate(Money money, LottoResult result) {
+        BigDecimal earningRate = result.earningRate(BigDecimal.valueOf(money.getPayment()));
         System.out.printf("총 수익률은 %.2f입니다.", earningRate);
         if (earningRate.intValue() < 1) {
             System.out.println(PROFIT_MESSAGE);
