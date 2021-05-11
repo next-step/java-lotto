1d15215818af17ce4264ea0de74e3cbb208b0c9f
suhyunsim <cnc1106@naver.com>
2021-05-11 15:25:39 +0900

    feat: 수동 로또 생성 메서드 Lotto에 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -17,8 +17,8 @@ public class Lotto {
         this.lottoBalls = lottoBalls;
     }
 
-    public static Lotto of(List<Integer> winningBalls) {
-        return new Lotto(LottoBalls.createWinningLottoBalls(winningBalls));
+    public static Lotto createManualLotto(List<Integer> manualBalls) {
+        return new Lotto(LottoBalls.createManualLottoBalls(manualBalls));
     }
 
     public List<LottoBall> createAutoLotto() {
