3f8798f890b3f5db3dd1bfc21a2d7a17a6825fcd
suhyunsim <cnc1106@naver.com>
2021-05-11 15:41:13 +0900

    test: 각각 생성된 수동, 자동 로또들을 Lottos에 추가해서 생성하도록 테스트 작성

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,30 +1,56 @@
 package lotto.domain;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class LottosTest {
 
+    static Stream<Arguments> lottoSource() {
+        return Stream.of(arguments(
+                Lists.list(
+                        new Lotto(LottoBalls.createManualLottoBalls(1, 2, 3, 4, 5, 6)),
+                        new Lotto(LottoBalls.createManualLottoBalls(2, 3, 4, 5, 6, 7))
+                )
+        ));
+    }
+
     @Test
-    @DisplayName("돈을 입력받은만큼 로또로 교환하기")
-    void create() {
-        Money money = Money.of(14000);
+    @DisplayName("돈을 입력받은만큼 자동 로또로 교환하기")
+    void create_auto_lottos() {
+        Money money = Money.of(14000, 0);
         Lottos lottos = Lottos.of(money);
 
         assertThat(lottos.size()).isEqualTo(14);
     }
 
-    @Test
-    @DisplayName("로또 번호 당첨 개수 확인")
-    void check() {
-        Money money = Money.of(14000);
-        Lottos lottos = Lottos.of(money);
-        List<Hit> hitList = lottos.matchHit(WinningLotto.of(1, 2, 3, 4, 5, 6, 7));
+    @ParameterizedTest
+    @DisplayName("수동 로또를 구매한 후 자동 로또를 추가하기")
+    @MethodSource("lottoSource")
+    void create_all_lottos(List<Lotto> lottoList) {
+        Money money = Money.of(14000, 2);
+        Lottos lottos = new Lottos(lottoList);
 
-        assertThat(hitList).isNotNull();
+        lottos.addLottoList(Lottos.of(money).getLottos());
+
+        assertThat(lottos.size()).isEqualTo(14);
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 번호 당첨 개수 확인")
+    @MethodSource("lottoSource")
+    void check(List<Lotto> lottoList) {
+        Lottos lottos = new Lottos(lottoList);
+
+        assertThat(lottos.matchHit(WinningLotto.of(1, 2, 3, 4, 15, 16, 17))).isNotNull();
     }
 }
