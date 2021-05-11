878a04dd62de8f9ea9fc2108d2905a014db2f644
suhyunsim <cnc1106@naver.com>
2021-05-11 15:08:46 +0900

    feat: InputView에서 수동 구매할 로또 수 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +11,9 @@ public class InputView {
     private InputView() {}
 
     private static final String INPUT_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String INPUT_NUMBER_VALIDATION_MESSAGE = "숫자를 입력해주세요.";
+    private static final String INPUT_MANUAL_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요";
     private static final String LAST_WEEK_LOTTO = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_LOTTO = "보너스 볼을 입력해 주세요.";
     private static final String SPLIT_DELIMITER = ",";
@@ -17,7 +21,20 @@ public class InputView {
 
     public static int inputMoney() {
         System.out.println(INPUT_MESSAGE);
-        return Integer.parseInt(scanner.nextLine());
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException(INPUT_NUMBER_VALIDATION_MESSAGE);
+        }
+    }
+
+    public static int inputManualLottoCount() {
+        System.out.println(INPUT_MANUAL_COUNT_MESSAGE);
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException(INPUT_NUMBER_VALIDATION_MESSAGE);
+        }
     }
 
     public static List<Integer> inputWinningNumbers() {
