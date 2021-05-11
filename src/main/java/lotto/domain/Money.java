4b8e231dd6ad215909ecffb13e54b51e43cdc26d
suhyunsim <cnc1106@naver.com>
2021-05-11 15:20:13 +0900

    refactor: 수동 로또 구매 개수를 고려해서 차액 만큼을 자동 로또 구매하도록 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,38 +2,44 @@ package lotto.domain;
 
 import java.util.Objects;
 
-import static lotto.exception.Message.INPUT_MONEY_MESSAGE;
-import static lotto.exception.Message.MINIMUM_MONEY_MESSAGE;
+import static lotto.exception.Message.*;
 
 public class Money {
 
     private static final int LOTTO_PRICE = 1000;
-    private final int money;
+    private final int payment;
+    private final int manualCounts;
+    private final int autoPayment;
 
-    private Money(int money) {
-        validate(money);
-        this.money = money;
+    private Money(int money, int manualCounts) {
+        validate(money, manualCounts);
+        this.payment = money;
+        this.manualCounts = manualCounts;
+        this.autoPayment = money - manualCounts * LOTTO_PRICE;
     }
 
-    public static Money of(int payment) {
-        return new Money(payment);
+    public static Money of(int payment, int manualCounts) {
+        return new Money(payment, manualCounts);
     }
 
-    private void validate(int payment) {
+    private void validate(int payment, int manualCounts) {
         if (payment < LOTTO_PRICE) {
             throw new IllegalArgumentException(MINIMUM_MONEY_MESSAGE);
         }
         if (payment % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(INPUT_MONEY_MESSAGE);
         }
+        if (payment < manualCounts * LOTTO_PRICE) {
+            throw new IllegalArgumentException(MANUAL_MAXIMUM_MESSAGE);
+        }
     }
 
-    public int purchase() {
-        return money / LOTTO_PRICE;
+    public int purchaseAuto() {
+        return autoPayment / LOTTO_PRICE;
     }
 
-    public int getMoney() {
-        return money;
+    public int getPayment() {
+        return payment;
     }
 
     @Override
@@ -41,11 +47,11 @@ public class Money {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Money money1 = (Money) o;
-        return money == money1.money;
+        return payment == money1.payment;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(money);
+        return Objects.hash(payment);
     }
 }
