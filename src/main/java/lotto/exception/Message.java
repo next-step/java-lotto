18ff0313800e2ab04a1e3e14bbb9c48ab8b66e4c
suhyunsim <cnc1106@naver.com>
2021-05-11 15:20:54 +0900

    refactor: 중복 로또 메세지 수정, 수동 로또 관련 에러 메세지 추가

--- src/main/java/lotto/exception/Message.java
+++ src/main/java/lotto/exception/Message.java
@@ -5,6 +5,7 @@ public class Message {
     public static final String SIZE_MESSAGE = "로또 숫자 개수가 6개가 아닙니다.";
     public static final String MINIMUM_MONEY_MESSAGE = "최소 금액은 1000원 입니다.";
     public static final String INPUT_MONEY_MESSAGE = "금액은 1000원 단위 입니다.";
-    public static final String DUPLICATION_MESSAGE = "당첨 로또에 중복된 값이 있습니다.";
+    public static final String MANUAL_MAXIMUM_MESSAGE = "지불 금액 이상으로 수동 로또를 구매할 수 없습니다.";
+    public static final String DUPLICATION_MESSAGE = "로또에 중복된 값이 있습니다.";
     public static final String BONUS_DUPLICATION_MESSAGE = "보너스볼이 당첨 번호에 포함되어 있습니다. 다른 값을 입력하세요.";
 }
