ff60aa35b89ebe46cefda67eec304c1841f47d5f
suhyunsim <cnc1106@naver.com>
2021-05-11 15:44:59 +0900

    refactor: LottoController에 전체 동작 수정사항 반영

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,11 +4,22 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoController {
     public void start() {
-        Money money = Money.of(InputView.inputMoney());
-        Lottos lottos = Lottos.of(money);
-        ResultView.printPurchase(lottos);
+        int inputMoney = InputView.inputMoney();
+        int inputManualCounts = InputView.inputManualLottoCount();
+        Money money = Money.of(inputMoney, inputManualCounts);
+        List<List<Integer>> manualNumbersList = InputView.inputManualLottoNumbers(inputManualCounts);
+
+        Lottos lottos = new Lottos(manualNumbersList.stream()
+                .map(Lotto::createManualLotto)
+                .collect(Collectors.toList()));
+        lottos.addLottoList(Lottos.of(money).getLottos());
+
+        ResultView.printPurchase(lottos, inputManualCounts);
 
         WinningLotto winningLotto = WinningLotto.of(InputView.inputWinningNumbers(), InputView.inputBonusNumber());
         LottoResult result = LottoResult.of(lottos, winningLotto);
