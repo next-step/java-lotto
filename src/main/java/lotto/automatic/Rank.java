994563fc4da6da644271c5d47c8bea4901cd175b
h1un <h1unha@naver.com>
2021-05-14 01:14:17 +0900

    chore : 수정....

--- src/main/java/lotto/automatic/Rank.java
+++ src/main/java/lotto/automatic/Rank.java
@@ -15,39 +15,39 @@ public enum Rank {
     private static final int WINNING_MIN_COUNT = 3;
     private static final String ERROR_VALUE_MSG = "값이 잘못되었습니다. 다시 한번 확인해주세요.";
 
-    private int winningNumber, money;
+    private int winningOfNumber, money;
 
-    Rank(int winningNumber, int money) {
-        this.winningNumber = winningNumber;
+    Rank(int winningOfNumber, int money) {
+        this.winningOfNumber = winningOfNumber;
         this.money = money;
     }
 
-    public int getWinningNumber() {
-        return winningNumber;
+    public int getWinningOfNumber() {
+        return winningOfNumber;
     }
 
     public int getMoney() {
         return money;
     }
 
-    public static Rank getRank(int matchCount, boolean bonus) {
+    public static Rank getRank(int winningOfNumber, boolean bonus) {
         return Arrays.stream(Rank.values())
-                .map(rank -> rank.matchCountToRank(matchCount,bonus))
+                .map(rank -> rank.matchCountToRank(winningOfNumber,bonus))
                 .filter(Objects::nonNull)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ERROR_VALUE_MSG));
     }
 
-    private Rank matchCountToRank(int matchCount, boolean bonus) {
-        for (Rank prizelist : values()) {
-            if (matchCount < WINNING_MIN_COUNT) {
+    private Rank matchCountToRank(int winningOfNumber, boolean bonus) {
+        for (Rank rank : values()) {
+            if (winningOfNumber < WINNING_MIN_COUNT) {
                 return MISS;
             }
-            if (THIRD.getWinningNumber() == matchCount && !bonus) {
+            if (THIRD.getWinningOfNumber() == winningOfNumber && !bonus) {
                 return THIRD;
             }
-            if (prizelist.getWinningNumber() == matchCount) {
-                return prizelist;
+            if (rank.getWinningOfNumber() == winningOfNumber) {
+                return rank;
             }
         }
         throw new IllegalArgumentException(ERROR_VALUE_MSG);
