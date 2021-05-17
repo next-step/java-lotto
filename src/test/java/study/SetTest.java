eab5bb0d20531609132df6f7edccab594d98d916
Jung Insup <jisjis1218@gmail.com>
2021-05-17 22:12:26 +0900

    test (contains test) : Set contains() 기능 테스트
    Set contains() 기능 테스트(true일 때와 true, false 둘다 일때) 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,11 +3,15 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -21,10 +25,26 @@ public class SetTest {
         numbers.add(3);
     }
 
-    @DisplayName("set의 size 확인")
+    @DisplayName("Set size 확인")
     @Test
     void checkSetSize() {
         //then
         assertThat(numbers).hasSize(3);
+    }
+
+    @DisplayName("Set contains 확인(true만 포함)")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsOnlyTrue(int setNumbers) {
+        //then
+        assertThat(numbers.contains(setNumbers)).isTrue();
+    }
+
+    @DisplayName("Set contains 확인(true, false 포함)")
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, true", "3, true", "4, false", "5, false"})
+    void containsBothTrueAndFalse(int num, boolean isContains) {
+        //then
+        assertEquals(numbers.contains(num), isContains);
     }
 }
