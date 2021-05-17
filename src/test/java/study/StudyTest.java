0eadc3f05fb38dc876787f0b5f22da44837b9887
Kiho Cho <40865499+cupeanimus@users.noreply.github.com>
2021-05-18 00:23:28 +0900

    1단계 학습 테스트 실습 (#1461)
    * split test
    
    * 1단계 학습 테스트 요구사항 작업
    -Set Collection에 대한 학습 테스트
    -ParameterizedTest 활용
    -ValueSource, CsvSource 활용

---
+++ src/test/java/study/StudyTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StudyTest {
+
+    @Test
+    void splite(){
+        String data = "1,2";
+        String[] result = data.split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substringTest(){
+        String data = "(1,2)";
+        int startIndex = data.indexOf("(")+1;
+        int endIndex = data.indexOf(")");
+        String result = data.substring(startIndex,endIndex);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("길이 이상 위치의 char값 가져올때 StringIndexOutOfBoundsException 발생 확인")
+    @Test
+    void charAtTest(){
+        String data = "abc";
+        assertThatThrownBy(() -> data.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class);
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> data.charAt(4));
+
+    }
+
+
+}
