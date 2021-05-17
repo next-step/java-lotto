6a504577c37d32153c01725ebb5436d62cccddc9
HyungjuJang <dacapolife87@gmail.com>
2021-05-18 01:56:06 +0900

    test : Set Collection에 대한 학습 테스트 [요구사항3] - CsvSource를 이용하여 구분자로 입력된 조건에 대한 테스트 - true의 경우 이외에도 false인 경우에도 체크가되도록 기능 개선

--- src/test/java/utils/SetTest.java
+++ src/test/java/utils/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -37,5 +38,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     public void containsValueCheckTest(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("개선된 set 에 값이 존재하는지 확인하는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    public void improveContainsValueCheckTest(int input, boolean isContains) {
+        assertThat(numbers.contains(input)).isEqualTo(isContains);
     }
 }
