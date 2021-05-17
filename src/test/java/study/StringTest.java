bd2231e429a0aeaa76e2f92bd67b15ebebf12b2b
chajs226 <chajs226@gmail.com>
2021-05-18 05:24:31 +0900

    split_1 string study

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,8 +6,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void split() {
+    void split_1_2() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void split_1() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
     }
 }
