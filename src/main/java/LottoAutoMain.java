eb64bf72d7abf1dbe9f7b098e193bb6454a46f44
hailey <wjdguswls55@gmail.com>
2021-05-17 17:21:01 +0900

    refactor : 사용하지 않는 클래스 제거

--- src/main/java/LottoAutoMain.java
+++
@@ -1,8 +0,0 @@
-public class LottoAutoMain {
-    public static void main(String[] args) {
-//        LottoAutoView lottoAuto = new LottoAutoView();
-//        lottoAuto.inputPrice();
-//        lottoAuto.inputWinningNumbers();
-
-    }
-}
