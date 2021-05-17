38550e213f91d48a6a52ddab0337f48920355351
hailey <wjdguswls55@gmail.com>
2021-05-17 17:27:27 +0900

    test (LottoRandom) : LottoRandom 클래스에서 요구사항을 맞추기 위한 메소드 분리와 알 수 없는 숫자를 final 변수로 설정하여 알아보기 쉽게 하고자 하였습니다.

--- src/main/java/lottoAuto/service/LottoRandom.java
+++ src/main/java/lottoAuto/service/LottoRandom.java
@@ -6,21 +6,30 @@ import java.util.List;
 import java.util.Random;
 
 public class LottoRandom {
+    private static final int LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_EACH = 6;
+
     public List<List> setLotto(int amount) {
-        Random random = new Random();
+
         List<List> result = new ArrayList<>();
 
         for (int x = 0; x < amount; x++) {
-            List<Integer> num = new ArrayList<>();
-
-            for (int i = 0; i < 6; i++) {
-                num.add(random.nextInt(45) + 1);
-            }
-            Collections.shuffle(num);
-            Collections.sort(num);
+            List<Integer> num = getLottoNumber();
             result.add(num);
         }
 
         return result;
+    }
+
+    private List<Integer> getLottoNumber() {
+        Random random = new Random();
+        List<Integer> num = new ArrayList<>();
+
+        for (int i = 0; i < LOTTO_NUMBER_EACH; i++) {
+            num.add(random.nextInt(LOTTO_NUMBER) + 1);
+        }
+        Collections.shuffle(num);
+        Collections.sort(num);
+        return num;
     }
 }
