f6357426ea73f495d52ecf79c3e95425fc48144e
hailey <wjdguswls55@gmail.com>
2021-05-17 17:25:25 +0900

    feat (statistic, statisticBonus) : 각 메소드에 지켜지지 않았던 요구사항을 수정하여 메소드를 분리하는 작업.

--- src/main/java/lottoAuto/service/LottoAutoService.java
+++ src/main/java/lottoAuto/service/LottoAutoService.java
@@ -1,48 +1,57 @@
 package lottoAuto.service;
 
+import lottoAuto.vo.LottoResultVo;
+
 import java.util.*;
 
 public class LottoAutoService {
 
     private List<Integer> winning_count;
-    private List<Integer> bonus_count;
+    private List<Boolean> bonus_count;
 
     public List<Integer> statistic(List<List> result, List<Integer> winningNum) {
 
         winning_count = new ArrayList<>();
         for (int i = 0; i < result.size(); i++) {
-            int count = 0;
-            for (int j = 0; j < winningNum.size(); j++) {
-                if (result.get(i).contains(winningNum.get(j))) {
-                    count++;
-
-                }
-            }
+            int count = getCount(result, winningNum, i);
             winning_count.add(count);
         }
         return winning_count;
     }
 
-    public List<Integer> statisticBonus(List<List> result, int bonus) {
-        bonus_count = new ArrayList<>();
-
-
-        for (int i = 0; i < result.size(); i++) {
-            int count = 0;
-            if (winning_count.get(i) == 5 && result.get(i).contains(bonus)) {
-                count = 5;
-                bonus_count.add(count);
-            }else {
-                bonus_count.add(count);
+    private int getCount(List<List> result, List<Integer> winningNum, int i) {
+        int count = 0;
+        for (int j = 0; j < winningNum.size(); j++) {
+            if (result.get(i).contains(winningNum.get(j))) {
+                count++;
             }
-
         }
+        return count;
+    }
 
+    public List<Boolean> statisticBonus(List<List> result, int bonus) {
+        bonus_count = new ArrayList<>();
+        for (int i = 0; i < result.size(); i++) {
+            Boolean count = getBonusCount(result, bonus, i);
+            bonus_count.add(count);
+        }
         return bonus_count;
     }
 
-    public double revenue(WinningCount winningCount) {
-//        int revenueNum = (5000* winningCount.three + 50000* winningCount.four + 1500000* winningCount.five + 30000000* winningCount.fiveBonus+2000000000* winningCount.six)/price;
-        return 0.0;
+    private Boolean getBonusCount(List<List> result, int bonus, int i) {
+        Boolean count = false;
+        if (winning_count.get(i) == 5 && result.get(i).contains(bonus)) {
+            count = true;
+        }
+        return count;
+    }
+
+    public List<LottoResultVo> setWinningStatistic(List<Integer> winning_count, List<Boolean> bonus_count) {
+        List<LottoResultVo> lottoResultVoList = new ArrayList<>();
+
+        for (int i = 0; i < winning_count.size(); i++) {
+            lottoResultVoList.add(new LottoResultVo(Rank.findByRank(winning_count.get(i), bonus_count.get(i)), 1));
+        }
+        return lottoResultVoList;
     }
 }
