d7b817c23513ea78f6d5a940baaf6a0f74d7776a
kyle <cupeanimus@naver.com>
2021-05-17 22:53:21 +0900

    1단계 학습 테스트 요구사항 작업 -Set Collection에 대한 학습 테스트 -ParameterizedTest 활용 -ValueSource, CsvSource 활용

--- src/test/java/study/StudyTest.java
+++ src/test/java/study/StudyTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StudyTest {
 
@@ -12,6 +13,24 @@ public class StudyTest {
         String[] result = data.split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substringTest(){
+        String data = "(1,2)";
+        int startIndex = data.indexOf("(")+1;
+        int endIndex = data.indexOf(")");
+        String result = data.substring(startIndex,endIndex);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("길이 이상 위치의 char값 가져올때 StringIndexOutOfBoundsException 발생 확인")
+    @Test
+    void charAtTest(){
+        String data = "abc";
+        assertThatThrownBy(() -> data.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class);
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> data.charAt(4));
+
     }
 
 
