fbea1129ddde662be1abea3256092e03fcbc1b39
bbbnam <bbbnam@naver.com>
2021-05-17 21:07:45 +0900

    feat : 빈 문자열 또는 null 값 입력시 0 반환 구현

---
+++ src/main/java/StringCalculator/StringAddCalculator.java
@@ -0,0 +1,10 @@
+package StringCalculator;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
