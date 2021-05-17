64964234d60d9c94b1a2b96b2c33f3c0e740d7d8
DESKTOP-JH4A8SV\ceo <siryugubin@naver.com>
2021-05-17 15:16:08 +0900

    refactor : rank 수정

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,18 +1,21 @@
 package lotto;
 
 public enum Rank {
-    MISS_MATCH(0, 0),
-    MATCH_3(3, 5000),
-    MATCH_4(4, 50000),
-    MATCH_5(5, 1500000),
-    MATCH_5_BONUS(5, 30000000),
-    MATCH_6(6, 2000000000);
+    MISS_MATCH(0,false,0),
+    MATCH_3(3,false, 5000),
+    MATCH_4(4,false, 50000),
+    MATCH_5(5,false, 1500000),
+    MATCH_5_BONUS(5,true,30000000),
+    MATCH_6(6,false, 2000000000);
 
-    private final int match;
-    private final int money;
+    private final int matchConut;
+    private final boolean matchBonus;
+    private final int prize;
 
-    Rank(int match, int money) {
-        this.match = match;
-        this.money = money;
+    Rank(int matchConut,boolean matchBonus, int prize) {
+        this.matchConut = matchConut;
+        this.matchBonus=matchBonus;
+        this.prize = prize;
+
     }
 }
