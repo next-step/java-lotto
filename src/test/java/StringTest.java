5111295b8cf13f7d7c642f09d8e2acb9ea2ceb84
seungkwon <yevgnenll@gmail.com>
2021-05-17 23:15:22 +0900

    test: 분리 대상 문자열이 없는 경우를 검증한다

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,5 +17,15 @@ public class StringTest {
 		assertThat(result).contains("2");
 		assertThat(result).containsExactly("1", "2");
 	}
+
+	@Test
+	@DisplayName(value = "split 을 실행할 대상이 없는 경우 1개의 element 만 존재한다")
+	void splitOneElement() {
+		String[] result = "1".split(",");
+
+		assertThat(result).contains("1");
+		assertThat(result.length).isEqualTo(1);
+	}
+
 
 }
