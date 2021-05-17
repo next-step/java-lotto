264fccf3372ea22de92e74fc652feb8d37d6e60c
bbbnam <bbbnam@naver.com>
2021-05-17 21:20:45 +0900

    refactor : 메소드 분리

--- src/main/java/StringCalculator/StringAddCalculator.java
+++ src/main/java/StringCalculator/StringAddCalculator.java
@@ -2,13 +2,26 @@ package StringCalculator;
 
 public class StringAddCalculator {
     public static int splitAndSum(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (isNullOrEmpty(input)) {
             return 0;
         }
-        String[] splited = input.split(",");
-        if (splited.length > 1) {
-            return Integer.parseInt(splited[0]) + Integer.parseInt(splited[1]);
+        return split(input);
+    }
+
+    private static int split(String input) {
+        String[] splited = input.split(",|:");
+        int result = 0;
+        for (String number : splited) {
+            result += parseInt(number);
         }
+        return result;
+    }
+
+    private static final int parseInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.trim().isEmpty();
     }
 }
