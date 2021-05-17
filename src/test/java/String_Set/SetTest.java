d18188836c8be2e7ce92693c222ec861cb2bccbe
bbbnam <bbbnam@naver.com>
2021-05-17 13:28:05 +0900

    test : Set 테스트 작성

---
+++ src/test/java/String_Set/SetTest.java
@@ -0,0 +1,46 @@
+package String_Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기(size) 확인 테스트")
+    void testSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains 메소드를 이용해 1,2,3 값이 set에 존재하는지 테스트 - (parameterizedTest 사용)")
+    void testContains1(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("요구사항 2와 달리 입력값에 대한 결과값이 다른 경우에도 검증 가능하도록 테스트")
+    void testContains2(int input, boolean result) {
+        assertThat(numbers.contains(input)).isEqualTo(result);
+    }
+}
