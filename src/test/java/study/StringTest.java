b27f943d47ebfb5fa392b0ed7fcd8f9a7d79c2d8
ttuop <plintcsz@gmail.com>
2021-05-18 07:23:22 +0900

    test: string chartAt test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -18,7 +19,23 @@ public class StringTest {
 
 	@Test
 	void substring() {
-		assertThat("(1,2)".substring(1,4)).isEqualTo("1,2");
+		assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+	}
+
+	@DisplayName("문자열의 특정 위치 문자를 비교하는 테스트")
+	@Test
+	void charAt() {
+		String str = "abc";
+		assertThat(str.charAt(0)).isEqualTo('a');
+		assertThat(str.charAt(1)).isEqualTo('b');
+		assertThat(str.charAt(2)).isEqualTo('c');
+	}
+
+	@DisplayName("문자열의 길이보다 큰 위치의 문자를 가져올 때 발생하는 예외 테스트")
+	@Test
+	void charAtWithStringIndexOutOfBoundsException() {
+		assertThatThrownBy(() -> "abc".charAt(99)).isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessageContaining("9");
 	}
 
 }
