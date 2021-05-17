d0a274d37ab17925723396cbee83b05b3c4766ab
김석진 <7271kim@naver.com>
2021-05-17 23:11:12 +0900

    feat : String 클래스 요구사항 2 구현
    -"(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록
    구현한다.
    
    
    [수정사항]
    - StringTest.java : splitTest3() 메서드 추가
    - README.md : 요구사항 2 추가

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -23,4 +23,12 @@ public class StringTest {
         assertThat(result).containsExactly("1");
     }
 
+    @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환")
+    void splitTest3() {
+        String input = "(1,2)";
+        String result = input.substring(input.indexOf("(") + 1, input.indexOf(")"));
+        assertThat(result).isEqualTo("1,2");
+    }
+
 }
