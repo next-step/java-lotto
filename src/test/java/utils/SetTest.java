cd43cb456e32294d29a183e54a78b24f37a902b2
HyungjuJang <dacapolife87@gmail.com>
2021-05-18 01:50:18 +0900

    test : Set Collection에 대한 학습 테스트 [요구사항2] - set에 값이 존재하는지 contains를 이용한 테스트케이스 작성 - ParameterizedTest와 ValueSource를 이용하여 반복되는 로직 제거

--- src/test/java/utils/SetTest.java
+++ src/test/java/utils/SetTest.java
@@ -3,6 +3,8 @@ package utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,4 +32,10 @@ public class SetTest {
         assertThat(numbers.size()).isEqualTo(size);
     }
 
+    @DisplayName("set 에 값이 존재하는지 확인하는 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void containsValueCheckTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
 }
