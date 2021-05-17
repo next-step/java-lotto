27a99442f4e523c1faefc637f78b9229a3d32b7b
sanggu <tkdrn8578@naver.com>
2021-05-17 21:55:05 +0900

    test: string의 substring, chat test 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,15 +1,19 @@
 package study;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
     @Test
-    @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트")
-    public void split() {
+    @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리된다.")
+    public void split_test() {
         //given
         String input = "1,2";
 
@@ -17,7 +21,51 @@ public class StringTest {
         String[] splitedInput = input.split(",");
 
         //then
-        assertThat(splitedInput[0]).isEqualTo("1");
-        assertThat(splitedInput[1]).isEqualTo("2");
+        assertThat(splitedInput).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환된다.")
+    public void splitOneLength_test() {
+        //given
+        String input = "1";
+
+        //when
+        String[] splitedInput = input.split(",");
+
+        //then
+        assertThat(splitedInput).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("(1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 1,2를 반환한다.")
+    public void substring_test() {
+        //given
+        String input = "(1,2)";
+
+        //when
+        String substringInput = input.substring(1, 4);
+
+        //then
+        assertThat(substringInput).isEqualTo("1,2");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"153, 2, 3", "1, 0, 1", "342, 2, 2"})
+    void chatAt_test(String input, int index, char expectedValue) {
+        //when
+        char value = input.charAt(index);
+
+        //then
+        assertThat(value).isEqualTo(expectedValue);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"153, -1", "1, 2"})
+    void chatAt_fail_test(String input, int index) {
+        //when
+        assertThatThrownBy(
+                () -> input.charAt(index)
+        ).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
