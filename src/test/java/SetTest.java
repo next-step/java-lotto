eadca8bcd2dc04a9358857479029bb357a801c02
Josh <yevgnenll@users.noreply.github.com>
2021-05-18 00:38:42 +0900

    1단계 String 과 Set 코드리뷰 요청합니다 (#1470)

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,38 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName(value = "set 에 element는 중복될 수 없어 4번 add가 발생했지만, size는 3이 된다")
+	void setSize() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"}, delimiter = ',')
+	@DisplayName(value = "1, 2, 3 은 contains true, 4, 5는 false 를 반환한다")
+	void setContains(int element, boolean flag) {
+		assertThat(numbers.contains(element)).isEqualTo(flag);
+	}
+
+}
