ead1dec59a0e3a833ec9f642255895d6249ec231
Jung Insup <jisjis1218@gmail.com>
2021-05-17 21:31:31 +0900

    test (size test) : Set size() 기능 테스트
    Set size() 기능 테스트 코드 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("set의 size 확인")
+    @Test
+    void checkSetSize() {
+        //then
+        assertThat(numbers).hasSize(3);
+    }
+}
