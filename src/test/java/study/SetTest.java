81666b43d08eb6ec9f24d617e873e236d4e335bc
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-18 00:55:44 +0900

    test : SetTest 클래스 신규작성, size 메서드 테스트 작성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 의 크기 확인")
+    void size() {
+        // given when then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
