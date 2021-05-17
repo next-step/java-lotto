871a2b12265b3980fa74b61e40b91ec082496619
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-05-17 20:40:13 +0900

    feat: step1 테스트 추가
    step1 미션 테스트 코드 작성

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,54 @@
+package step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    public void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("중복 제거 후 남은 값 확인")
+    public void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Set 크기 확인")
+    public void setSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("1, 2, 3 값 가지고 있는지 확인")
+    public void containsValue(Integer value){
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1, 2, 3 값은 true, 4, 5 값은 false 반환")
+    public void setContainsWithParameterizedAndCsvSource(Integer number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
