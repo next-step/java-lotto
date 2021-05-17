8d5f9a1504a8e9d3b3164e78421b8424ad5c234a
HyungjuJang <dacapolife87@gmail.com>
2021-05-18 01:40:01 +0900

    test : String 클래스에 대한 학습 테스트 [요구사항3] - charAt을 사용하여 특정위치의 문자가져오기 테스트케이스 작성 - 문자열의 길이를 벗어난 특정위치의 문자를 가져올경우 익셉션 테스트케이스 작성

--- src/test/java/utils/StringUtilsTest.java
+++ src/test/java/utils/StringUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringUtilsTest {
 
@@ -37,5 +38,30 @@ public class StringUtilsTest {
         String removedParenthesis = StringUtils.removeParenthesis(input);
 
         assertThat(removedParenthesis).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt을 사용하여 특정위치의 문자확인")
+    @Test
+    public void charAtTest() {
+        String input = "abc";
+
+        String firstChar = StringUtils.getCharAtIndex(input, 0);
+        assertThat(firstChar).isEqualTo("a");
+
+        String secondChar = StringUtils.getCharAtIndex(input, 1);
+        assertThat(secondChar).isEqualTo("b");
+
+        String lastChar = StringUtils.getCharAtIndex(input, 2);
+        assertThat(lastChar).isEqualTo("c");
+    }
+
+    @DisplayName("문자열의 길이를 벗어난 특정위치의 문자 가져올때 익셉션확인")
+    @Test
+    public void charAtExceptionTest() {
+        String input = "abc";
+        assertThatThrownBy(() ->
+                StringUtils.getCharAtIndex(input, 5)
+        ).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessage("String index out of range: 5");
     }
 }
