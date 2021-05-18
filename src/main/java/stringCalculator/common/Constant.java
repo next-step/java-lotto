f513b9c4acdce3df6bd6ca571599b493157ca544
lkimilhol <lkimilhol@gmail.com>
2021-05-19 01:49:18 +0900

    refactor(*.*): 사용하지 않는 상수 제거, 공백 제거

--- src/main/java/stringCalculator/common/Constant.java
+++ src/main/java/stringCalculator/common/Constant.java
@@ -4,5 +4,4 @@ public class Constant {
     public static final String CUSTOM_PATTERN = "//(.)\n(.*)";
     public static final String DEFAULT_DELIMITER = ",|:";
     public static final String DEFAULT_POSITIVE_VALUE = "0";
-    public static final String NUMERIC_PATTERN = "^[0-9]+$";
 }
