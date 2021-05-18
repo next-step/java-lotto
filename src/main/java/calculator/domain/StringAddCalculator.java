1ff7642ddf78497baebfb48c166ffbcf2f17421f
ohgillwhan <soora_genius@naver.com>
2021-05-18 11:33:43 +0900

    feat : splitAndSum_null_또는_빈문자

---
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package calculator.domain;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String expression) {
+        if (expression == null || expression.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
