8b543125c81e99ff66b005c1e04396bf2670223c
KIM GI TAE <c0433c@gmail.com>
2021-05-18 21:00:45 +0900

    feat(substring): string substring 테스트

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -7,5 +7,13 @@ public class StringTest {
     void split() {
         assertThat("1,2".split(",")).containsExactly("1","2");
         assertThat("1".split(",")).containsExactly("1");
-        }
     }
+
+    @Test
+    void substring() {
+        String data = "(1,2)";
+        assertThat(data.substring(1, data.length()-1)).isEqualTo("1,2");
+    }
+
+    
+}
