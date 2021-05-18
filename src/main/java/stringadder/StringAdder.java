8158bf7b813701d16b231b04834e6cbb10a364d8
ted.kang <ted.kang@kakaocorp.com>
2021-05-19 03:39:53 +0900

    feat: StringAdder 클래스 작성
    StringAdder 클래스 작성 및 테스트코드 작성

---
+++ src/main/java/stringadder/StringAdder.java
@@ -0,0 +1,44 @@
+package stringadder;
+
+import stringadder.domain.Number;
+
+import java.util.regex.Pattern;
+
+import static stringadder.domain.Number.ZERO_NUMBER;
+import static stringadder.domain.OperatorSelector.selectOperator;
+
+public class StringAdder {
+
+  private static final Pattern SPACE_PATTERN = Pattern.compile("\\s");
+  private static final String EMPTY_STRING = "";
+  private static final int SINGLE_STRING_LENGTH = 1;
+
+  private StringAdder() {
+  }
+
+  public static int calculate(String input) {
+    if (isNullOrEmpty(input)) {
+      return ZERO_NUMBER.getValue();
+    }
+    String trimmedInput = removeSpace(input);
+    if (isSingleLengthString(trimmedInput)) {
+      return new Number(trimmedInput).getValue();
+    }
+    return selectOperator(trimmedInput).calculate(trimmedInput)
+                                        .getValue();
+  }
+
+  private static boolean isNullOrEmpty(String input) {
+    return input == null || input.trim()
+                                  .isEmpty();
+  }
+
+  private static boolean isSingleLengthString(String input) {
+    return input.length() == SINGLE_STRING_LENGTH;
+  }
+
+  private static String removeSpace(String input) {
+    return SPACE_PATTERN.matcher(input)
+                        .replaceAll(EMPTY_STRING);
+  }
+}
