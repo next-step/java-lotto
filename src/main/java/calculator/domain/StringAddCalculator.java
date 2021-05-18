77243c9a9d7705d51498ace8a9841b6b704fcc00
ohgillwhan <soora_genius@naver.com>
2021-05-18 11:44:03 +0900

    feat : 쉼표와 콜론으로 구분자를 나누어 값을 더하는 기능 추가 및 음수일 경우 Exception 발생

--- src/main/java/calculator/domain/StringAddCalculator.java
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -1,11 +1,22 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class StringAddCalculator {
+    private static String EXPRESSION_DEFAULT_SEPARATOR = ",|:";
+
     public static int splitAndSum(String expression) {
         if (expression == null || expression.isEmpty()) {
             return 0;
         }
 
-        return 0;
+        String[] split = expression.split(EXPRESSION_DEFAULT_SEPARATOR);
+
+        CalculatorNumbers calculatorNumbers = new CalculatorNumbers(Arrays.stream(split)
+                .map(CalculatorNumber::new)
+                .collect(Collectors.toList()));
+
+        return calculatorNumbers.sum();
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -4,6 +4,7 @@ import calculator.domain.StringAddCalculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
@@ -13,5 +14,29 @@ public class StringAddCalculatorTest {
 
         result = StringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
     }
 }
