d83d97d336a038d83d886a5647064065e03a155c
zuge3927 <zuge3927@naver.com>
2021-05-18 14:14:36 +0900

    feat : 로또 상수 모음 LottoConstant 클래스 생성

---
+++ src/main/java/lotto/LottoConstant.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class LottoConstant {
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int LOTTO_TICKET_SIZE = 6;
+    public static final int LOTTO_PRICE = 1000;
+}
