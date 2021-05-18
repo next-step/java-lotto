8e14c03002020c68160e69a5a41652f643ece071
dohchoi91 <dohchoi91@gmail.com>
2021-05-19 08:54:44 +0900

    feat: 커스텀 구분자 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,20 +1,31 @@
 package calculator;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-	public static final String DELIMITER = ",|:";
 	public static int splitAndSum(String text) {
 		if (Objects.isNull(text) || text.isEmpty()) {
 			return 0;
 		}
 
-		String[] nums = text.split(DELIMITER);
+		String[] tokens = split(text);
 		int sum = 0;
 
-		for (String num : nums) {
+		for (String num : tokens) {
 			sum += Integer.parseInt(num);
 		}
 		return sum;
+	}
+
+	private static String[] split(String text) {
+		String delimiter = ",|:";
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			delimiter = m.group(1);
+			text = m.group(2);
+		}
+		return text.split(delimiter);
 	}
 }
