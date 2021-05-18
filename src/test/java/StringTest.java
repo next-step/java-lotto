402293531b7a3674d467d235b6b0cd6c77bdf721
JisuLim <sanola3@gmail.com>
2021-05-19 00:46:44 +0900

    test: add requirement2

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -17,4 +17,11 @@ public class StringTest {
 		String[] splitString = givenString.split(",");
 		assertThat(splitString).containsExactly("1");
 	}
+
+	@Test
+	public void substringTest() {
+		String givenString = "(1,2)";
+		String actual = givenString.substring(1, 4);
+		assertThat(actual).isEqualTo("1,2");
+	}
 }
