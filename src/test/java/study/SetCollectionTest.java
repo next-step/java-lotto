c219342a0d60efe807239aaf60ec60ea6194a654
mskangg <funnycoding91@gmail.com>
2021-05-19 02:04:11 +0900

    test: Set Collection 요구사항 2 구현
    - Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
    - 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
    - JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -3,10 +3,13 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetCollectionTest {
@@ -34,5 +37,22 @@ public class SetCollectionTest {
 
         //then
         assertEquals(expected, result);
+    }
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.\n" +
+            "구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.\n" +
+            "JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.")
+    /*
+     * @Test
+     * void contains() {
+     *  assertThat(numbers.contains(1)).isTrue();
+     *  assertThat(numbers.contains(2)).isTrue();
+     *  assertThat(numbers.contains(3)).isTrue();
+     * }
+     */
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void parameterized_test(int num) {
+        assertThat(numbers.contains(num)).isTrue();
     }
 }
