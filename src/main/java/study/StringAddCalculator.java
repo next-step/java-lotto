71adef7bf3fd45af86ef2d4ba69615d743b68715
HoonHee Jeong <jhh992000@gmail.com>
2021-05-18 23:13:04 +0900

    feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환하도록 구현

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,6 +2,8 @@ package study;
 
 public class StringAddCalculator {
 
+	public static final String SEPERATOR = ",";
+
 	public static int splitAndSum(String text) {
 		if (text == null || text.trim().isEmpty()) {
 			return 0;
@@ -9,7 +11,23 @@ public class StringAddCalculator {
 		if (isNumeric(text)) {
 			return Integer.parseInt(text);
 		}
-		return -1;
+		return sumNumbersBySeperator(text, SEPERATOR);
+	}
+
+	private static int sumNumbersBySeperator(String text, String seperator) {
+		return sum(split(text, seperator));
+	}
+
+	private static String[] split(String text, String seperator) {
+		return text.split(seperator);
+	}
+
+	private static int sum(String[] numbers) {
+		int result = 0;
+		for (String number : numbers) {
+			result += Integer.parseInt(number);
+		}
+		return result;
 	}
 
 	private static boolean isNumeric(String text) {
