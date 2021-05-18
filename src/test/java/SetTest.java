611c02febe52dd3ec81963b5190b81ca015496bf
Junhee Ko <jko@rainist.com>
2021-05-18 18:24:07 +0900

    test: add test method for containing by ParameterizedTest

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
 
@@ -29,5 +32,11 @@ public class SetTest {
 
     // Then
     assertEquals(expectedSetSize, actualSetSize);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  void givenNumbersInSet_whenCheckIfContaining_thenTrue(Integer number) {
+    assertTrue(numbers.contains(number));
   }
 }
