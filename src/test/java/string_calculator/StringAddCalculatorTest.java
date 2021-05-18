2f7c407307d2aecd9a71672537ef9f430f7bda7b
bbbnam <bbbnam@naver.com>
2021-05-18 12:42:53 +0900

    refactor : 테스트 코드 ParameterizedTest 사용 및 중복제거

--- src/test/java/string_calculator/StringAddCalculatorTest.java
+++ src/test/java/string_calculator/StringAddCalculatorTest.java
@@ -1,47 +1,50 @@
 package string_calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
-    @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+
+    private StringAddCalculator stringAddCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringAddCalculator = new StringAddCalculator();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자일때_0반환(String nullOrEmpty) {
+        int result = stringAddCalculator.splitAndSum(nullOrEmpty);
+
         assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
     }
 
-    @Test
-    public void splitAndSum_숫자하나() {
-        int result = StringAddCalculator.splitAndSum("1");
-        assertThat(result).isEqualTo(1);
+    @ParameterizedTest
+    @CsvSource(value = {"1=1", "1,2=3",  "1,2:3=6"}, delimiter = '=')
+    public void splitAndSum_일반구분자_총합계산_또는_숫자하나반환_정상일때(String input, int expected) {
+        int result = stringAddCalculator.splitAndSum(input);
+
+        assertThat(result).isEqualTo(expected);
     }
 
-    @Test
-    public void splitAndSum_쉼표구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2");
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3", "//@\n2@1@3", "//:\n1:2:3"})
+    public void splitAndSum_custom_구분자_사용시_총합계산(String input) {
+        int result = stringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
-    }
-
-    @Test
-    public void splitAndSum_negative() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+    public void splitAndSum_음수_전달시_예외발생() {
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
