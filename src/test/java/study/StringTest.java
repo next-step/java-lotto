876a55ddbae8e6b1642da9cec36fbb1bde40a0ac
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-18 20:51:44 +0900

    String Test and Exception Test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,15 +1,37 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringTest {
     @Test
+    @DisplayName("문자열 split 테스트")
     void split() {
         String[] result = "1,2".split(",");
-        System.out.println(result[0]);
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("문자열 substring 테스트")
+    void subString() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).contains("1,2");
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열 chatAt Index 예외 테스트")
+    void chatAt() {
+        String str = "abc";
+        assertThatThrownBy(() -> str.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> str.charAt(3))
+                .withMessageMatching("String index out of range: 3");
     }
 }
