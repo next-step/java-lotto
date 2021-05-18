28c63caa7bd9bb43b3cb03bf3efaee9b82f53e97
Junhee Ko <jko@rainist.com>
2021-05-18 18:05:25 +0900

    test: add test method for split numbers by comma

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringTest {
+
+  @Test
+  void givenNumbersWithDelimiter_whenSplitByDelimiter_thenGetSplitNumbers() {
+    // Given
+    String numbersWithDelimiter = "1,2";
+    String delimiter = ",";
+
+    // When
+    String[] splitNumbers = numbersWithDelimiter.split(delimiter);
+
+    // Then
+    assertThat(splitNumbers).containsExactly("1", "2");
+  }
 }
