2ff4e63f6a7651f741b1d61b5a63a1901d15502c
dohchoi91 <dohchoi91@gmail.com>
2021-05-18 14:24:03 +0900

    test: Set의 값 포함 유무 테스트 추가

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Set Collection에 대한 학습 테스트")
 public class SetTest {
@@ -24,5 +26,12 @@ public class SetTest {
 	@DisplayName("요구사항 1 : size() 메소드를 활용해 Set의 크기를 확인")
 	void size() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest(name = "Find : {argumentsWithNames}")
+	@ValueSource(ints = {1, 2, 3} )
+	@DisplayName("요구사항 2 : contains() 메소드를 활용한 값이 존재 유무 확인 ")
+	void contains(int number) {
+		assertThat(numbers.contains(number)).isTrue();
 	}
 }
