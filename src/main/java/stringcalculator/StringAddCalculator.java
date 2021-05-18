b2d6e91a2350e4f2730f98d32fc4e47d01bda4aa
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-05-19 00:17:13 +0900

    feat(StringAddCalculator.java): 커스텀 구분자를 입력받아 숫자를 구분하는 기능 구현
    - 정규표현식을 사용하여 커스텀 구분자가 입력되었는지 확인
    - 커스텀 구분자가 입력된 경우 구분자와 구분자가 포함된 숫자 문자열을 구분하는 기능
    - 커스텀 구분자가 입력된 경우 기본 구분자에 커스텀 구분자를 포함하여 구분자로 사용
    
    test(StringAddCalculatorTest.java): 커스텀 구분자를 입력하고 구분된 숫자의 합을 검증하는 테스트코드 작성
    
    test(RegexTest.java): 정규표현식 관련 클래스 학습 및 테스트 코드 작성

--- src/main/java/stringcalculator/StringAddCalculator.java
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -1,19 +1,47 @@
 package stringcalculator;
 
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     private static final int DEFAULT_RETURN_VALUE = 0;
+    private static final int REGEX_GROUP_INDEX_DELIMITER = 1;
+    private static final int REGEX_GROUP_INDEX_TEXT = 2;
     private static final String SPLIT_DEFAULT_DELIMITERS = ",|:";
+    private static final String JOINER_DELIMITER = "|";
+    private static final String REGEX_TEXT = "//(.)\n(.*)";
 
     public static int splitAndSum(String text) {
         if (validateNullOrEmpty(text)) {
             return DEFAULT_RETURN_VALUE;
         }
-        return getSumStringNumbers(text);
+        String delimiters = getDelimiter(text);
+        String numbers = getTextNumbers(text);
+        return getSumStringNumbers(numbers, delimiters);
     }
 
-    private static int getSumStringNumbers(String numbers) {
+    private static String getDelimiter(String text) {
+        StringJoiner delimiters = new StringJoiner(JOINER_DELIMITER);
+        delimiters.add(SPLIT_DEFAULT_DELIMITERS);
+        Matcher matcher = Pattern.compile(REGEX_TEXT).matcher(text);
+        if (matcher.find()) {
+            delimiters.add(matcher.group(REGEX_GROUP_INDEX_DELIMITER));
+        }
+        return delimiters.toString();
+    }
+
+    private static String getTextNumbers(String text) {
+        Matcher matcher = Pattern.compile(REGEX_TEXT).matcher(text);
+        if (matcher.find()) {
+            text = matcher.group(REGEX_GROUP_INDEX_TEXT);
+        }
+        return text;
+    }
+
+    private static int getSumStringNumbers(String numbers, String delimiters) {
         int sumNumbers = DEFAULT_RETURN_VALUE;
-        for (String number : numbers.split(SPLIT_DEFAULT_DELIMITERS)) {
+        for (String number : numbers.split(delimiters)) {
             sumNumbers += Integer.parseInt(number);
         }
         return sumNumbers;
--- src/test/java/stringcalculator/StringAddCalculatorTest.java
+++ src/test/java/stringcalculator/StringAddCalculatorTest.java
@@ -38,4 +38,13 @@ public class StringAddCalculatorTest {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+
+        int result1 = StringAddCalculator.splitAndSum("//;\n12,2;3");
+        assertThat(result1).isEqualTo(17);
+    }
 }
