04efcc970c4134f994cf8f006518e626f2fe5a97
bbbnam <39575145+bbbnam@users.noreply.github.com>
2021-05-19 01:20:59 +0900

    Step2 - 문자열 덧셈 계산기 (#1483)
    * test : StringAddCalculator 테스트 작성
    
    * feat : 빈 문자열 또는 null 값 입력시 0 반환 구현
    
    * feat : 숫자 하나 문자열로 입력시 해당 숫자 반환 구현
    
    * feat : 숫자 두개를 컴마 구분자로 입력할 경우 두 숫자의 합을 반환 기능 구현
    
    * refactor : 메소드 분리
    
    * feat : 커스텀 구분자 분리 구현
    
    * feat : 음수 올 경우 runtime예외 발생 구현
    
    * refactor : 상수화 리팩토링
    
    * refactor : 자바 패키지명 수정
    
    * refactor : 불필요 throw exception 제거
    
    * refactor : 테스트 코드 ParameterizedTest 사용 및 중복제거
    
    * refactor : 정적 메소드 제거

---
+++ src/main/java/string_calculator/StringAddCalculator.java
@@ -0,0 +1,48 @@
+package string_calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DECIDED_DELIMITER = ",|:";
+    private static final int DEFAULT_NUMBER = 0;
+    private static final int FIRST_MATCH = 1;
+    private static final int SECOND_MATCH = 2;
+
+    public int splitAndSum(String input) {
+        if (isNullOrEmpty(input)) {
+            return DEFAULT_NUMBER;
+        }
+        return add(split(input));
+    }
+
+    private int add(String[] split) {
+        int result = DEFAULT_NUMBER;
+        for (String number : split) {
+            result += parseInt(number);
+        }
+        return result;
+    }
+
+    private String[] split(String input) {
+        Matcher matcher = CUSTOM_PATTERN.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(FIRST_MATCH);
+            return matcher.group(SECOND_MATCH).split(customDelimiter);
+        }
+        return input.split(DECIDED_DELIMITER);
+    }
+
+    private int parseInt(String input) {
+        int number = Integer.parseInt(input);
+        if (number < DEFAULT_NUMBER) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private boolean isNullOrEmpty(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+}
