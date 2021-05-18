b0c5642182aecc28fd9f3a69c031cc450945ed58
mskangg <funnycoding91@gmail.com>
2021-05-19 02:00:08 +0900

    test: String 요구사항 3 구현
    - "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
    - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
    - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringTest {
@@ -52,5 +52,24 @@ public class StringTest {
 
         //then
         assertEquals(input, expected);
+    }
+
+    @Test
+    @DisplayName("abc 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습테스트를 구현한다.\n" +
+            "String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면\n" +
+            "StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.\n" +
+            "JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.")
+    void charAt_StringIndexOutOfBoundsException() {
+        //given
+        String input = "abc";
+
+        //when
+        //then
+        assertThatThrownBy(() -> input.charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> input.charAt(4))
+                .withMessageMatching("String index out of range: \\d+");
     }
 }
