2a93d5fc6cae7b3fedb2e23cf45d43f638fd0eb8
ohgillwhan <soora_genius@naver.com>
2021-05-18 22:48:04 +0900

    feat : LottoNumber의 number를 final로 하고, equals, hashcode구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 import static java.lang.String.format;
 
 public class LottoNumber {
     private static final int MINIMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 45;
 
-    private int number;
+    private final int number;
 
     public LottoNumber(int number) {
         if(number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
@@ -14,5 +16,18 @@ public class LottoNumber {
         }
 
         this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
