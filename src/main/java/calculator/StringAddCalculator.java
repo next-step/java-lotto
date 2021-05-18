479691d610f25594f1ea43cc0d316628574ac4a8
HyungjuJang <dacapolife87@gmail.com>
2021-05-18 13:40:07 +0900

    feat : 입력문자열의 0 또는 null값 체크

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        int sum = 0;
+
+        if (!validationCheck(input)) {
+            return sum;
+        }
+
+        return sum;
+    }
+
+    private static boolean validationCheck(String input) {
+        if (input == null) {
+            return false;
+        }
+
+        if (input.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
