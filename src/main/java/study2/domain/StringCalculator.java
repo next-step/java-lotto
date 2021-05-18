9425ce4083a9aed891dc38c58674d44f6aa690d3
Donghui-Lee <ehdgml3206@naver.com>
2021-05-18 20:41:43 +0900

    refactor: PositiveNumbers Class 적용

--- src/main/java/study2/domain/StringCalculator.java
+++ src/main/java/study2/domain/StringCalculator.java
@@ -1,16 +1,13 @@
 package study2.domain;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private final List<Integer> numbers;
+    private final PositiveNumbers numbers;
 
     public StringCalculator(String input) {
-        String[] stringNumbers = splitInput(parseEmptyToZero(input));
-        numbers = parsePositiveNumbers(stringNumbers);
+        numbers = PositiveNumbers.of(splitInput(parseEmptyToZero(input)));
     }
 
     private String parseEmptyToZero(String stringNumber) {
@@ -30,29 +27,7 @@ public class StringCalculator {
         return input.split(separator);
     }
 
-    private List<Integer> parsePositiveNumbers(String[] stringNumbers) {
-        List<Integer> numbers = new ArrayList<>();
-        for (String stringNumber : stringNumbers) {
-            numbers.add(parsePositiveNumber(stringNumber));
-        }
-        return numbers;
-    }
-
-    private Integer parsePositiveNumber(String stringNumber) {
-        Integer number = Integer.parseInt(stringNumber);
-        validate(number);
-        return number;
-    }
-
-    private void validate(Integer number) {
-        if (number < 0) {
-            throw new RuntimeException("음수가 존재합니다.");
-        }
-    }
-
     public int sum() {
-        return numbers.stream()
-            .mapToInt(Integer::intValue)
-            .sum();
+        return numbers.sum();
     }
 }
