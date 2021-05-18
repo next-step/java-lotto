91974292e4d5f7dbf44c086d01a3bc0b8d2da7a7
hailey <wjdguswls55@gmail.com>
2021-05-18 16:50:08 +0900

    feat (LottoAutoInputService/inputPrice) : 불필요한 VO 클래스를 삭제함
    + VO클래스를 삭제함으로써 inputPrice메소드에는 amount만 계산하여 리턴하는 책임만 가지게 되었습니다.
    + inputPrice 메소드 내에 있던 사용자의 랜덤 로또 번호를 생성하는 메소드도 따로 생성하여 일급컬랙션 클래스에 저장하여 return하게 하였습니다.

--- src/main/java/lottoAuto/service/LottoAutoInputService.java
+++ src/main/java/lottoAuto/service/LottoAutoInputService.java
@@ -1,33 +1,34 @@
 package lottoAuto.service;
 
-import lottoAuto.vo.MyLottoInformationVO;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoAutoInputService {
-    protected MyLottoInformationVO myLottoInformationVO = null;
+
     private LottoRandom lottoRandom = new LottoRandom();
 
     private List<Integer> winningNumber = new ArrayList<>();
     private static final int lotto_price = 1000;
 
-    public void inputPrice(int price) {
+    public int inputPrice(int price) {
         int amount = price / lotto_price;
 
-        List<List> lottoNumber = lottoRandom.setLotto(amount);
-
-        myLottoInformationVO = new MyLottoInformationVO(price, amount, lottoNumber);
-
+        return amount;
     }
 
-    public List<Integer> inputWinningNumber(String winningNumber_string) {
+    public List<Lotto> inputUserLottoNumber(int amount) {
+        List<Lotto> userLotto = lottoRandom.setLotto(amount);
+        return userLotto;
+    }
+
+    public Lotto inputWinningNumber(String winningNumber_string) {
         String[] splitString = winningNumber_string.split(",");
 
         for (int i = 0; i < splitString.length; i++) {
             winningNumber.add(Integer.valueOf(splitString[i]));
         }
-        return winningNumber;
+        Lotto winningLotto = new Lotto(winningNumber);
+        return winningLotto;
 
     }
 }
