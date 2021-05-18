076116ff7a4578f5ddccb7f6bba851c5c8017db7
HoonHee Jeong <jhh992000@gmail.com>
2021-05-18 22:56:38 +0900

    feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하도록 구현

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -6,7 +6,18 @@ public class StringAddCalculator {
 		if (text == null || text.trim().isEmpty()) {
 			return 0;
 		}
+		if (isNumeric(text)) {
+			return Integer.parseInt(text);
+		}
 		return -1;
+	}
+
+	private static boolean isNumeric(String text) {
+		if (text == null || text.trim().isEmpty()) {
+			return false;
+		}
+		String regex = "[0-9]+";
+		return text.matches(regex);
 	}
 
 }
