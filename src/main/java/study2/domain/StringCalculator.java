54eff1ea5e0a52e7791cfccc812f4aa4290538ca
Donghui-Lee <ehdgml3206@naver.com>
2021-05-18 20:46:43 +0900

    refactor: 문자열 상수화

--- src/main/java/study2/domain/StringCalculator.java
+++ src/main/java/study2/domain/StringCalculator.java
@@ -4,6 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private static final String EMPTY_NUMBER = "0";
+    private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+
     private final PositiveNumbers numbers;
 
     public StringCalculator(String input) {
@@ -12,14 +16,14 @@ public class StringCalculator {
 
     private String parseEmptyToZero(String stringNumber) {
         if (stringNumber == null || stringNumber.trim().isEmpty()) {
-            return "0";
+            return EMPTY_NUMBER;
         }
         return stringNumber;
     }
 
     private String[] splitInput(String input) {
-        String separator = ",|:";
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        String separator = DEFAULT_SEPARATOR;
+        Matcher m = Pattern.compile(CUSTOM_SEPARATOR).matcher(input);
         if (m.find()) {
             separator = m.group(1);
             return m.group(2).split(separator);
