18b93469f0e1f715ee8e56a3b1ad17cb31809544
Donghui-Lee <ehdgml3206@naver.com>
2021-05-18 11:00:52 +0900

    test: Set Collection 대한 학습 테스트 - 요구사항 3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -36,5 +37,13 @@ public class SetTest {
     void contains(int input) {
         // given & when & then
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains() 메소드를 활용해 값 존재 유무 확인")
+    void containsByInput(int input, boolean result) {
+        // given & when & then
+        assertThat(numbers.contains(input)).isEqualTo(result);
     }
 }
