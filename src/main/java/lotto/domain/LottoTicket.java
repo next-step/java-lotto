832f5da238d8bd419c9352324f7189fe3955fd0c
ohgillwhan <soora_genius@naver.com>
2021-05-18 22:58:18 +0900

    feat : 로또 티켓은 6개의 숫자를 갖는다.

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.String.format;
+
+public class LottoTicket {
+    private final int LOTTO_NUMBERS_SIZE = 6;
+
+    private final Set<LottoNumber> lottoNumbers;
+
+    public LottoTicket(List<LottoNumber> incrementedLottoNumberBySize) {
+        this.lottoNumbers = new HashSet<>(incrementedLottoNumberBySize);
+
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(format("로또티켓은 %d개의 번호를 가져야 합니다.", LOTTO_NUMBERS_SIZE));
+        }
+    }
+}
---
+++ src/test/java/lotto/LottoTicketTest.java
@@ -0,0 +1,46 @@
+package lotto;
+
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class LottoTicketTest {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1",
+            "1,2",
+            "1,2,3",
+            "1,2,3,4",
+            "1,2,3,4,5",
+            "1,2,3,4,5,5",
+            "1,2,3,4,5,6,7",
+    })
+    @DisplayName("로또티켓은 고유한 6개의 로또번호가 아니면 IllegalArgumentException이 발생한다")
+    public void 로또티켓은_고유한_6개의_로또번호가_아니면_IllegalArgumentException이_발생한다(String fixtureLottoNumber) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoTicket(convertStringToLottoNumberList(fixtureLottoNumber)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "1,2,3,5,8,9", "10,11,12,13,14,15"})
+    @DisplayName("로또티켓은 6개의 로또번호를 가진다")
+    public void 로또티켓은_6개의_로또번호를_가진다(String fixtureLottoNumber) {
+        assertDoesNotThrow(() -> new LottoTicket(convertStringToLottoNumberList(fixtureLottoNumber)));
+    }
+
+    private List<LottoNumber> convertStringToLottoNumberList(String fixtureLottoNumber) {
+        return Arrays.stream(fixtureLottoNumber.split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(toList());
+    }
+}
