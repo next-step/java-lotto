3dde6f0bca42684e0ef99fa3c017b5b1de815d38
dohchoi <dohchoi91@gmail.com>
2021-05-18 16:55:02 +0900

    [Step1] 학습 테스트 실습 (#1491)
    * test: spilit 기능 테스트 추가
    
    * test: substring 기능 테스트 추가
    
    * test: charAt 기능 테스트 추가
    
    * test: Set 크기 확인에 대한 기능 테스트 추가
    
    * test: Set의 값 포함 유무 테스트 추가
    
    * test: Set의 값 포함 결과값 확인 테스트 추가

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,46 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("요구사항 1 : size() 메소드를 활용해 Set의 크기를 확인")
+	void size() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest(name = "Find : {argumentsWithNames}")
+	@ValueSource(ints = {1, 2, 3} )
+	@DisplayName("요구사항 2 : contains() 메소드를 활용한 값이 존재 유무 확인 ")
+	void contains(int number) {
+		assertThat(numbers.contains(number)).isTrue();
+	}
+
+	@ParameterizedTest(name = "Case : {argumentsWithNames}")
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	@DisplayName("요구사항 3 : contains() 메소드를 활용한 값이 포함 결과값 확인")
+	void containsWithResult(String input, String expected) {
+		String result = numbers.contains(Integer.parseInt(input)) ? "true" : "false";
+		assertThat(result).isEqualTo(expected);
+	}
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,36 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스에 대한 학습 테스트")
+public class StringTest {
+	@Test
+	@DisplayName("요구사항1 : split 테스트")
+	void spilt() {
+		assertThat("1,2".split(",")).containsExactly("1", "2");
+		assertThat("1".split(",")).containsExactly("1");
+	}
+	@Test
+	@DisplayName("요구사항2 : substring 테스트")
+	void substring() {
+		String input = "(1,2)";
+		assertThat(input.substring(1, input.length() - 1)).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("요구사항3 : charAt 테스트")
+	void charAtSuccess() {
+		String input = "abc";
+		assertThat(input.charAt(0)).isEqualTo('a');
+		assertThat(input.charAt(1)).isEqualTo('b');
+		assertThat(input.charAt(2)).isEqualTo('c');
+	}
+
+	@Test
+	@DisplayName("요구사항3 : charAt 실패 테스트")
+	void charAtFail() {
+		assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(IndexOutOfBoundsException.class)
+			.hasMessageContaining("String index out of range: 3");
+	}
+}
