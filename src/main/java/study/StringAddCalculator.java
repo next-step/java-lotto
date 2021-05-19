1bebd3e0e04ead2e1fecb4e05818839b574e59a8
ttuop <plintcsz@gmail.com>
2021-05-19 20:10:15 +0900

    refactor: refactoring StringAddCalculator

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,17 +2,14 @@ package study;
 
 public class StringAddCalculator {
 
-	private StringAddCalculator() {
-	}
-
-	public static int splitAndSum(String str) {
+	public int splitAndSum(String str) {
 		StringSplitter splitter = getStringSplitter(str);
 		NonNegativeIntegers integers = new NonNegativeIntegers(splitter.get());
 
 		return integers.sum();
 	}
 
-	private static StringSplitter getStringSplitter(String str) {
+	private StringSplitter getStringSplitter(String str) {
 		NumberPatternExtractor extractor = new NumberPatternExtractor(str);
 		Delimiter delimiter = new Delimiter(extractor.getDelimiterText());
 		String numberText = extractor.getNumberText();
--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -2,42 +2,70 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class StringAddCalculatorTest {
 
+	StringAddCalculator stringAddCalculator = new StringAddCalculator();
+
+	@DisplayName("null 이나 빈 문자열을 전달하면 0을 반환하는 테스트")
 	@ParameterizedTest
 	@NullAndEmptySource
-	public void splitAndSum_null_또는_빈문자(String input) {
-		assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
+	public void splitAndSum_nullOrEmpty(String input) {
+		// when
+		int actual = stringAddCalculator.splitAndSum(input);
+
+		// then
+		assertThat(actual).isEqualTo(0);
 	}
 
+	@DisplayName("숫자 문자열을 전달하면 숫자를 그대로 반환하는 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"0;0", "1;1"}, delimiter = ';')
+	public void splitAndSum_onlyNumber(String input, int expected) {
+		// when
+		int actual = stringAddCalculator.splitAndSum(input);
+
+		// then
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@DisplayName("숫자와 기본 구분자(',', ':')로 구성된 문자열을 전달하면 숫자를 더하는 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"1,2;3", "4,5:6;15", "7:8:9;24"}, delimiter = ';')
+	public void splitAndSum_default_delimiter(String input, int expected) {
+		// when
+		int actual = stringAddCalculator.splitAndSum(input);
+
+		// then
+		assertThat(actual).isEqualTo(expected);
+	}
+
+	@DisplayName("숫자와 지정한 구분자로 구성된 문자열을 전달하면 숫자를 더하는 테스트")
 	@Test
-	public void splitAndSum_숫자하나() {
-		assertThat(StringAddCalculator.splitAndSum("1")).isEqualTo(1);
+	public void splitAndSum_custom_delimiter() {
+		// given
+		String input = "//;\n1;2;3";
+
+		// when
+		int actual = stringAddCalculator.splitAndSum(input);
+
+		// then
+		assertThat(actual).isEqualTo(6);
 	}
 
-	@Test
-	public void splitAndSum_쉼표구분자() {
-		assertThat(StringAddCalculator.splitAndSum("1,2")).isEqualTo(3);
-	}
-
-	@Test
-	public void splitAndSum_쉼표_또는_콜론_구분자() {
-		assertThat(StringAddCalculator.splitAndSum("1,2:3")).isEqualTo(6);
-	}
-
-	@Test
-	public void splitAndSum_custom_구분자() {
-		assertThat(StringAddCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6);
-	}
-
+	@DisplayName("숫자 중에 음수가 있으면 예외가 발생하는 테스트")
 	@Test
 	public void splitAndSum_negative() {
-		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
-			.isInstanceOf(RuntimeException.class);
+		// given
+		String input = "-1,2,3";
+
+		// then
+		assertThatThrownBy(() -> stringAddCalculator.splitAndSum(input)).isInstanceOf(RuntimeException.class);
 	}
 
 }
