5ca2be6d0281bab3fce5346e11cfa94f650c457d
Dohyun Kim <10915235+rurumimic@users.noreply.github.com>
2021-05-20 08:40:17 +0900

    Step1: 학습 테스트 실습 (#1534)

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,48 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@DisplayName("Set 크기는 3이다.")
+	@Test
+	void size() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@DisplayName("Set의 element를 확인한다.")
+	@ParameterizedTest(name = "{0}이 있다.")
+	@ValueSource(ints = {1, 2, 3})
+	void contains(int input) {
+		assertThat(numbers.contains(input)).isTrue();
+	}
+
+	@DisplayName("Set의 element를 확인한다.")
+	@ParameterizedTest(name = "{0}: {1}")
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void removeBrackets(int input, boolean expected) {
+		assertThat(numbers.contains(input)).isEqualTo(expected);
+	}
+
+}
