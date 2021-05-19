e0851ba80163540cf10c75168fb7907989335ca9
youngjun.jin <yzzzzun@gmail.com>
2021-05-19 20:22:57 +0900

    feat: 구분된 문자열의 합을 구하는 로직 구현

--- src/main/java/wootecam/calculator/StringAddCalculator.java
+++ src/main/java/wootecam/calculator/StringAddCalculator.java
@@ -1,7 +1,17 @@
 package wootecam.calculator;
 
+import java.util.List;
+
 public class StringAddCalculator {
-	public static int splitAndSum(Object o) {
-		return 0;
+	public static int splitAndSum(String input) {
+
+		DefaultStringTokenizer dst = new DefaultStringTokenizer();
+
+		List<String> numbers = dst.getNumbers(input);
+
+		return numbers.stream()
+			.map(CalculatorNumber::new)
+			.map(CalculatorNumber::parseNumber)
+			.reduce(0, Integer::sum);
 	}
 }
--- src/test/java/wootecam/calculator/StringAddCaculatorTest.java
+++ src/test/java/wootecam/calculator/StringAddCaculatorTest.java
@@ -1,8 +1,12 @@
 package wootecam.calculator;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class StringAddCaculatorTest {
 	@Test
@@ -12,5 +16,13 @@ public class StringAddCaculatorTest {
 
 		result = StringAddCalculator.splitAndSum("");
 		assertThat(result).isEqualTo(0);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2,3_6", "1:6,3_10", "20:2:3_25"}, delimiter = '_')
+	@DisplayName("기본 구분자로 구성된 입력에대해 문자열 계산기능 테스트")
+	public void splitAndSum_기본구분자사용(String input, int expected) {
+		int result = StringAddCalculator.splitAndSum(input);
+		assertEquals(expected, result);
 	}
 }
