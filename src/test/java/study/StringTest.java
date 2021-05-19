750781b8caf3e880e6ef35bfada89a1b1b18a6a8
ascii92der <ascii92der@gmail.com>
2021-05-19 21:53:28 +0900

    test: String 클래스에 대한 학습 테스트-요구사항3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,19 +1,24 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
     @Test
+    @DisplayName("String split 테스트")
     void splitStringTest() {
         String stringData = "1,2";
         String[] stringArray = stringData.split(",");
         assertThat(stringArray).containsExactly("1", "2");
+
     }
 
     @Test
+    @DisplayName("String split 특정 값 체크 테스트")
     void splitStringContainTest() {
         String stringData = "1,";
         String[] stringArray = stringData.split(",");
@@ -21,8 +26,25 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("subString 테스트")
     void subStringTest() {
         String stringData = "(1,2)";
         assertThat(stringData.substring(1, stringData.length() - 1)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 테스트")
+    void charAtTest() {
+        String stringData = "abc";
+        assertThat(stringData.charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt StringIndexOutOfBoundsException 테스트")
+    void charAtExceptionTest() {
+        String stringData = "abc";
+        assertThatThrownBy(() -> stringData.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: %d", stringData.length());
     }
 }
