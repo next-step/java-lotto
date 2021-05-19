dd0ca4ff225fe4e436a64404a873d17d4ff8ef94
bbbnam <bbbnam@naver.com>
2021-05-19 22:40:18 +0900

    test : 로또번호 사이즈 유효성 검사

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -4,7 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class LottoNumbersTest {
+
+    @Test
+    @DisplayName("로또번호가 중복이거나 입력이 추가/부족 일 경우(6자리가 아닐경우) 예외발생")
+    void checkLottoNumbersSize() {
+        assertThatThrownBy(()-> new LottoNumbers(1,2,3,4,5,5))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("로또 번호는 6자리여야 합니다.");
+    }
 
     @Test
     @DisplayName("숫자값 일치하는 갯수 테스트")
