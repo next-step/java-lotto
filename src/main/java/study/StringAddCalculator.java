a61a281d75c24baf83dd46c3872a9aacc154d850
ttuop <plintcsz@gmail.com>
2021-05-19 17:18:41 +0900

    refactor: refactoring StringAddCalculator
    * extract getStringSplitter method

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -6,13 +6,18 @@ public class StringAddCalculator {
 	}
 
 	public static int splitAndSum(String str) {
-		NumberPatternExtractor extractor = new NumberPatternExtractor(str);
-		Delimiter delimiter = new Delimiter(extractor.getDelimiterText());
-		String numberText = extractor.getNumberText();
-		StringSplitter splitter = new StringSplitter(delimiter, numberText);
+		StringSplitter splitter = getStringSplitter(str);
 		NonNegativeIntegers integers = new NonNegativeIntegers(splitter.get());
 
 		return integers.sum();
+	}
+
+	private static StringSplitter getStringSplitter(String str) {
+		NumberPatternExtractor extractor = new NumberPatternExtractor(str);
+		Delimiter delimiter = new Delimiter(extractor.getDelimiterText());
+		String numberText = extractor.getNumberText();
+
+		return new StringSplitter(delimiter, numberText);
 	}
 
 }
