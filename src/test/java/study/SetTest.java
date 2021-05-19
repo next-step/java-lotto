4c6d577e1f4cb3b95aa383122be5a3a516e6dfa8
jaewanLee <ianlee9180@gmail.com>
2021-05-19 18:32:57 +0900

    test : setTest 요구사항 3 테스트 추가
    CsvSource를 사용하여 delimiter로 값을 분할하여 경우에 따라 서로다른 테스트코드를 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
@@ -34,7 +36,18 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("Set의 contains를 활용해 값의 존재여부를 확인한다")
     void contains(int num) {
-       assertTrue(this.numbers.contains(num));
+        assertTrue(this.numbers.contains(num));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set의 contains를 CsvSource를 사용하여 확인한다.")
+    void containsWithDelimiter(String num,String reference){
+        if(Boolean.valueOf(reference)){
+            assertTrue(this.numbers.contains(Integer.valueOf(num)));
+        }else{
+            assertFalse(this.numbers.contains(Integer.valueOf(num)));
+        }
     }
 }
 
