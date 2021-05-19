4dda017e59e12f8a8816b949d2d843dbfa7dbf10
jimin.joo <wnwlals22@naver.com>
2021-05-19 18:12:23 +0900

    test: 문자열 안 괄호를 찾아 없애는 기능

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -13,11 +13,20 @@ class StringTest {
 			.contains("1", "2");
 	}
 
-
 	@Test
 	@DisplayName("String split method를 이용하여 하나의 문자만 가지는 문자열 자르기 테스트")
 	void splitTestWithSingleText(){
 		assertThat("1".split(","))
 			.containsExactly("1");
 	}
+
+	@Test
+	@DisplayName("String substring을 이용하여 첫번째 문자와 끝 문자 자르기 테스트")
+	void substringTest(){
+		String text = "(1,2)";
+		assertThat(text.substring(1, text.length()-1))
+			.isEqualTo("1,2");
+	}
+
+	
 }
