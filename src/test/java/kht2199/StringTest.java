6d1d67160847bf25cb34290305d8892cf0b6c350
kht2199 <kht2199@gmail.com>
2021-05-20 02:17:22 +0900

    test: String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트 구현

--- src/test/java/kht2199/StringTest.java
+++ src/test/java/kht2199/StringTest.java
@@ -2,6 +2,7 @@ package kht2199;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -31,5 +32,17 @@ public class StringTest {
 	void removeBracketsUsingRegex() {
 		assertThat("(1,2)".replaceAll("[()]", ""))
 			.isEqualTo("1,2");
+	}
+
+	/**
+	 * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+	 * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+	 * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+	 */
+	@Test
+	@DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생")
+	void expectIndexOutOfBoundsExceptionWhenOverIndexSizeOfStringCharAt() {
+		assertThatThrownBy(() -> "abc".charAt(3))
+			.isInstanceOf(IndexOutOfBoundsException.class);
 	}
 }
