e0478118bb383147f2e6b3013524bc2e0fb4f3a7
jimin.joo <wnwlals22@naver.com>
2021-05-19 23:26:02 +0900

    [주지민] 1단계 - 학습 테스트 실습 (#1533)
    * docs: 기능 목록 작성
    
    * docs: 기능 목록 -> 테스트 목록 수정
    
    * test: 원하는 구분자를 이용해 문자열을 자르는 기능
    
    * test: 문자열 안 괄호를 찾아 없애는 기능
    
    * test: 문자열 안 특정 위치의 문자를 반환하는 기능
    
    * docs: 테스트 목록 업데이트
    
    * test: Set Collection, size 확인 테스트
    
    * test: Set Collection, contains 확인 테스트
    
    * test: contains 확인 테스트 리팩토링
    
    * style: 불필요한 파일, import 제거

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringTest {
+
+	@Test
+	@DisplayName("String split method를 이용하여 문자열 자르기 테스트")
+	void splitTest(){
+		assertThat("1,2".split(","))
+			.hasSize(2)
+			.contains("1", "2");
+	}
+
+	@Test
+	@DisplayName("String split method를 이용하여 하나의 문자만 가지는 문자열 자르기 테스트")
+	void splitTestWithSingleText(){
+		assertThat("1".split(","))
+			.containsExactly("1");
+	}
+
+	@Test
+	@DisplayName("String substring을 이용하여 첫번째 문자와 끝 문자 자르기 테스트")
+	void substringTest(){
+		String text = "(1,2)";
+		assertThat(text.substring(1, text.length()-1))
+			.isEqualTo("1,2");
+	}
+
+	@ParameterizedTest(name = "String charAt을 이용한 특정 위치 문자 얻기 테스트. index:{0}, expected:{1}")
+	@CsvSource(value = {
+		"0,a", "1,b", "2,c"
+	})
+	void charAtTest(final int index, final char expected){
+		assertThat("abc".charAt(index))
+			.isEqualTo(expected);
+	}
+
+	@ParameterizedTest(name = "String charAt, 범위값을 넘어가는 특정 위치 참조 테스트. index:{0}")
+	@ValueSource(ints = {
+		-1, 3, 4, 5
+	})
+	void charAtTestWithStringIndexOutOfBoundsException(final int index){
+		assertThatThrownBy(()-> "abc".charAt(index))
+			.isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessageContaining("String index out of range: " + index);
+	}
+}
