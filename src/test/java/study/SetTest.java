24c76ac04ebaf2e1b592b379253046dd2ba557ff
jaewanLee <ianlee9180@gmail.com>
2021-05-19 18:04:51 +0900

    test : SetTest 요구사항 1 테스트코드 추가
    Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -15,5 +19,11 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 Size를 활용해 Set의 크기를 확인한다")
+    void size(){
+        assertThat(this.numbers.size()).isEqualTo(3);
     }
 }
