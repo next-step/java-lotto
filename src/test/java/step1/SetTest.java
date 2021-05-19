14371d3efc712795ad4334732ebc5676de0c413f
wrallee <wrallee@naver.com>
2021-05-19 21:00:50 +0900

    test: Set 요구사항 3 완료

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -34,5 +35,12 @@ public class SetTest {
 	@DisplayName(value = "contains 메서드가 정상 동작한다.")
 	public void checkContainsMethod(int element) throws Exception {
 		assertThat(numbers.contains(element)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	@DisplayName(value = "contains 메서드가 입력값에 따라 true/false를 반환한다.")
+	public void checkContainsMethodDynamic(int element, boolean expected) throws Exception {
+		assertThat(numbers.contains(element)).isEqualTo(expected);
 	}
 }
