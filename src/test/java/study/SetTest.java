3fd35a883f223d53b3dbeaa45e069442a284fb50
ascii92der <ascii92der@gmail.com>
2021-05-19 22:11:49 +0900

    test: Set Collection에 대한 학습 테스트-요구사항2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,11 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -25,6 +28,13 @@ public class SetTest {
     @DisplayName("set size 테스트")
     void setSizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("set contain 테스트")
+    void setContainTest(int input) {
+        assertTrue(numbers.contains(input));
     }
 
 }
