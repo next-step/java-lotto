d132d0e1d750021b55540afcb8c83bc9543b2b57
htkim <kht2199@gmail.com>
2021-05-20 02:12:14 +0900

    feat: implementation String replace step 2 test using regex.

--- src/test/java/kht2199/StringTest.java
+++ src/test/java/kht2199/StringTest.java
@@ -4,23 +4,32 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Spec
- *
- * 1. "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
- * 2. "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
- */
 public class StringTest {
 
+	/**
+	 * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+	 */
 	@Test
 	void splitTest() {
 		assertThat("1,2".split(","))
 			.containsExactly("1", "2");
 	}
 
+	/**
+	 * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+	 */
 	@Test
 	void splitForLengthOneStringTest() {
 		assertThat("1".split(","))
 			.containsExactly("1");
+	}
+
+	/**
+	 * "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+	 */
+	@Test
+	void removeBracketsUsingRegex() {
+		assertThat("(1,2)".replaceAll("[()]", ""))
+			.isEqualTo("1,2");
 	}
 }
