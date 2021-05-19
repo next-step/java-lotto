7570e7d17db355bdf65c8d12024c045df34863c6
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-19 15:00:50 +0900

    feat : 커스텀 구분자가 여러 개인 경우, IllegalArgumentException을 던지는 기능 구현

--- src/main/java/calculator/SplitUtil.java
+++ src/main/java/calculator/SplitUtil.java
@@ -1,7 +1,5 @@
 package calculator;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -9,9 +7,12 @@ import java.util.regex.Pattern;
 
 public class SplitUtil {
     private static final String DEFAULT_DELIMITER = ",|:";
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int ACCEPTED_NUMBER_OF_CUSTOM_DELIMITER = 1;
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
     private static final int GROUP_NUMBER_CUSTOM_DELIMITER = 1;
     private static final int GROUP_NUMBER_OPERANDS = 2;
+
+    private static final String ERROR_INVALID_NUMBER_OF_CUSTOM_DELIMITER = "한 개의 커스텀 구분자만 입력해주세요.";
 
     public static Operands splitByDelimiter(String input) {
         List<Operand> operands = new ArrayList<>();
@@ -33,10 +34,12 @@ public class SplitUtil {
         return byDefaultDelimiter(input, DEFAULT_DELIMITER);
     }
 
-    private static String[] byCustomDelimiter(String operands, String delimiter) {
+    private static String[] byCustomDelimiter(String operands, String delimiter) throws IllegalArgumentException{
+        if (delimiter.length() > ACCEPTED_NUMBER_OF_CUSTOM_DELIMITER){
+            throw new IllegalArgumentException(ERROR_INVALID_NUMBER_OF_CUSTOM_DELIMITER);
+        }
         return operands.split(DEFAULT_DELIMITER+"|"+delimiter);
     }
-
 
     private static String[] byDefaultDelimiter(String operands, String delimiter) {
         return operands.split(delimiter);
