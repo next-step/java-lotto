5429409ed9d2623047bd29b72a538baef98e2c53
JisuLim <sanola3@gmail.com>
2021-05-19 15:31:17 +0900

    refactor: splitAndSum return sum of input string

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class StringAddCalculator {
 	public static int splitAndSum(String inputText) {
 		if (inputText == null || inputText.isEmpty()) {
 			return 0;
 		}
 
-		return Integer.parseInt(inputText);
+		return Arrays.stream(inputText.split(","))
+				.map(Integer::parseInt)
+				.reduce(0, Integer::sum);
 	}
 }
