793900c096750054ee2f0c33bd4182faaa47c26f
yjs2952 <yjs2952@naver.com>
2021-05-19 16:14:29 +0900

    refactor: 매직 넘버 상수처리

--- src/main/java/lotto/domain/RandomNumbersGenerator.java
+++ src/main/java/lotto/domain/RandomNumbersGenerator.java
@@ -5,13 +5,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static lotto.domain.ValidationUtils.*;
+
 public class RandomNumbersGenerator implements LottoNumbersGenerator {
 
-    private static final List<Integer> LOTTO_NUMBERS_CANDIDATES = IntStream.range(1, 45).boxed().collect(Collectors.toList());
+    private static final List<Integer> LOTTO_NUMBERS_CANDIDATES = IntStream.range(MINIMUM_NUMBER, MAXIMUM_NUMBER).boxed().collect(Collectors.toList());
 
     @Override
     public List<Integer> generate() {
         Collections.shuffle(LOTTO_NUMBERS_CANDIDATES);
-        return LOTTO_NUMBERS_CANDIDATES.stream().limit(6).collect(Collectors.toList());
+        return LOTTO_NUMBERS_CANDIDATES.stream().limit(LOTTO_NUMBERS_LENGTH).collect(Collectors.toList());
     }
 }
