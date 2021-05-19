46e56f6eaf94470ee1ddf9702224bb619d03a425
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-19 11:00:12 +0900

    test: Set 콜렉션 contain 메소드 학습 테스트 구현 및 관련 기능 구현 목록 체크

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("Set Collection 학습 테스트")
 public class SetTest {
     private Set<Integer> numbers;
 
@@ -19,7 +23,7 @@ public class SetTest {
     }
 
     @DisplayName("Set.size, numbers 객체의 size 확인")
-    void sizeOfGiven() {
+    void size_numbers() {
         // Given
         int expectedResult = 3;
 
@@ -28,5 +32,13 @@ public class SetTest {
 
         // Then
         assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set.contain, 특정 int 포함 여부 확인")
+    void contain_specificInt(int input) {
+        // Then
+        assertTrue(numbers.contains(input));
     }
 }
