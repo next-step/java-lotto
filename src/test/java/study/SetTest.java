6e3e1a5531767385e438173f957be38f1453286f
jaewanLee <ianlee9180@gmail.com>
2021-05-19 18:19:43 +0900

    test : setTest 요구사항2 테스트 추가
    Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 JUnit의 ParameterizedTest를 활용해 중복 코드를 제거하여 추가함

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,17 +3,20 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
         numbers.add(1);
@@ -23,7 +26,15 @@ public class SetTest {
 
     @Test
     @DisplayName("Set의 Size를 활용해 Set의 크기를 확인한다")
-    void size(){
+    void size() {
         assertThat(this.numbers.size()).isEqualTo(3);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains를 활용해 값의 존재여부를 확인한다")
+    void contains(int num) {
+       assertTrue(this.numbers.contains(num));
+    }
 }
+
