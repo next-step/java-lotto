023c835ec6b2b68104823c20edefc8b7671c3347
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-05-19 21:17:35 +0900

    feat(string calculator): 문자열 계산기 커스텀 구분자 기능 추가
    - 관련 테스트 코드 추가
    - 문자열 계산기의 커스텀 구분자 기능을 추가
        1. input을 커스텀 구분자로 split 하여 더하는 기능
          * 구분자만 추출하는 기능 추가
          * 숫자 영역만 추출하는 기능 추가
          * Escaping이 필요한 문자 처리 기능 추가

--- src/main/java/kr/insup/calculator/StringAddCalculator.java
+++ src/main/java/kr/insup/calculator/StringAddCalculator.java
@@ -1,8 +1,18 @@
 package kr.insup.calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+
+    private static final Pattern pattern = Pattern.compile("^//(.)\n(.*)");
+    private static final String DEFAULT_DELIMITER = "[:,]";
+
     public int calculate(String input) {
-        String[] numbers = input.split("[:,]");
+        String delimiter = getDelimiter(input);
+        String onlyNumbers = getOnlyNumbers(input);
+        String[] numbers = onlyNumbers.split(delimiter);
+
         return calculateNumbers(numbers);
     }
 
@@ -19,5 +29,29 @@ public class StringAddCalculator {
             return 0;
         }
         return Integer.parseInt(number);
+    }
+
+    public String getDelimiter(String input) {
+        Matcher m = pattern.matcher(input);
+        if (m.find()) {
+            String delimiter = m.group(1);
+            return escapeCharacter(delimiter);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    public String getOnlyNumbers(String input) {
+        Matcher m = pattern.matcher(input);
+        if (m.find()) {
+            return m.group(2);
+        }
+        return input;
+    }
+
+    private String escapeCharacter(String delimiter) {
+        if ("\\[](){}*+?|^$.".contains(delimiter)) {
+            return "\\" + delimiter;
+        }
+        return delimiter;
     }
 }
--- src/test/java/kr/insup/calculator/StringAddCalculatorTest.java
+++ src/test/java/kr/insup/calculator/StringAddCalculatorTest.java
@@ -1,9 +1,10 @@
 package kr.insup.calculator;
 
-import kr.insup.calculator.StringAddCalculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +12,7 @@ public class StringAddCalculatorTest {
 
     StringAddCalculator stringAddCalculator = new StringAddCalculator();
 
+    @DisplayName("문자열 받아 합 계산")
     @ParameterizedTest
     @CsvSource(value = {"1,2,3!6", "1:2,4!7", "0,4,2!6"}, delimiter = '!')
     void 문자열_받아_합_계산(String input, int expected) {
@@ -21,6 +23,7 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("문자열 하나일 때 계산")
     @ParameterizedTest
     @CsvSource({"6, 6", "4, 4"})
     void 문자열_하나일_때_계산(String input, int expected) {
@@ -31,6 +34,7 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("문자열 비어있을 때 계산")
     @Test
     void 문자열_비어있을_때_계산() {
         //given
@@ -38,5 +42,38 @@ public class StringAddCalculatorTest {
 
         //then
         assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("구분자 잘 뽑아내는지 테스트")
+    @ParameterizedTest
+    @MethodSource("kr.insup.calculator.TestHelper#provideDelimiterExample")
+    void 구분자_추출_테스트(String input, String expected) {
+        //given
+        String delimiter = stringAddCalculator.getDelimiter(input);
+
+        //then
+        assertThat(delimiter).isEqualTo(expected);
+    }
+
+    @DisplayName("숫자영역만 잘 뽑아내는지 테스트")
+    @ParameterizedTest
+    @MethodSource("kr.insup.calculator.TestHelper#provideNumbersExample")
+    void 숫자영역_추출_테스트(String input, String expected) {
+        //given
+        String onlyNumbers = stringAddCalculator.getOnlyNumbers(input);
+
+        //then
+        assertThat(onlyNumbers).isEqualTo(expected);
+    }
+
+    @DisplayName("커스텀 구분자 있을 때 계산")
+    @ParameterizedTest
+    @MethodSource("kr.insup.calculator.TestHelper#provideCalculateExample")
+    void 커스텀_구분자_있을_때_계산(String input, int expected) {
+        //given
+        int result = stringAddCalculator.calculate(input);
+
+        //then
+        assertThat(result).isEqualTo(expected);
     }
 }
