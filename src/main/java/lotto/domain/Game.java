76c9aa83c13b3c7360aa2e482394902581d17b40
ohgillwhan <soora_genius@naver.com>
2021-05-19 11:34:39 +0900

    feat : 게임을 생성할 때 금액을 입력 받고, 번호생성기도 받을 수 있다. 그리고 기본값은 랜덤적략인 애를 갖고 있다.

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.domain.generator.LottoNumberGenerator;
+import lotto.domain.generator.LottoNumberRandomGenerator;
 
 public class Game {
     private static final Amount TICKET_PRICE = new Amount(1000);
@@ -11,6 +12,10 @@ public class Game {
     public Game(LottoTickets lottoTickets, LottoWon lottoWon) {
         this.lottoTickets = lottoTickets;
         this.lottoWon = lottoWon;
+    }
+
+    public static Game createByMoney(Amount money, LottoWon lottoWon) {
+        return createByMoneyAndGenerator(money, new LottoNumberRandomGenerator(), lottoWon);
     }
 
     public static Game createByMoneyAndGenerator(Amount money, LottoNumberGenerator lottoNumberGenerator, LottoWon lottoWon) {
