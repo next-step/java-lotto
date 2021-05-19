8060f3f22759fafe4415a5ae84e263426fb04256
wrallee <wrallee@naver.com>
2021-05-19 20:53:33 +0900

    test: Set 요구사항 2 완료

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 	private Set<Integer> numbers;
@@ -25,5 +27,12 @@ public class SetTest {
 	@DisplayName(value = "Set의 사이즈를 확인한다.")
 	public void checkSize() throws Exception {
 		assertThat(numbers).hasSize(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName(value = "contains 메서드가 정상 동작한다.")
+	public void checkContainsMethod(int element) throws Exception {
+		assertThat(numbers.contains(element)).isTrue();
 	}
 }
