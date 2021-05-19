4816dae7653b192cba6820d72653b84e070151ef
박소정 <psj6414@naver.com>
2021-05-19 09:24:39 +0900

    test: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달시 구분자를 기준으로 분리한 각 숫자의 합을 반환

---
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -0,0 +1,67 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAddCalculatorTest {
+    private StringAddCalculator stringAddCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringAddCalculator = new StringAddCalculator();
+    }
+
+    @DisplayName("입력받은 문자열 null or empty 검증")
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void validateInput(String input) {
+        boolean result = stringAddCalculator.validateInput(input);
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("input 문자열 쉼표와 콜론 구분하여 문자열 반환 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3", "1:2:3", "1,2:3", "1:2,3"})
+    public void splitInput(String input) {
+        String[] result = stringAddCalculator.splitInput(input);
+        assertThat(result).containsExactly("1","2","3");
+    }
+
+    @DisplayName("배열의 sum 반환 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3=6", "2:2:4=8", "1:2,3,10=16", "10:24,1,1:1=37", "224,111=335"}, delimiter = '=')
+    public void sumNumbers(String input, int expected){
+        String[] inputArray = stringAddCalculator.splitInput(input);
+        int result = stringAddCalculator.sumNumbers(inputArray);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("input 문자열 숫자 하나 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "7:7", "3:3", "4:4"}, delimiter = ':')
+    public void splitAndSum_숫자하나(String input, int expected) {
+        int result = stringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("splitAndSum 함수 쉼표구분자 결과 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3", "2,2,4:8", "1,2,3,10:16", "10,24:34", "224,111:335"}, delimiter = ':')
+    public void splitAndSum_쉼표구분자(String input, int expected) {
+        int result = stringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("splitAndSum 함수 쉼표 또는 콜론 구분자 결과 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3=6", "2:2:4=8", "1:2,3,10=16", "10:24,1,1:1=37", "224,111=335"}, delimiter = '=')
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = stringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+}
