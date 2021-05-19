9699f9342c215d2f006964460064eadbb26343b3
KIM GI TAE <c0433c@gmail.com>
2021-05-19 20:14:22 +0900

    feat(split): 커스텀구분자에 의한 입력 테스트
    - Pattern.compile 활용하여 split 결과 리턴

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,7 +1,18 @@
 package stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    public static String[] split(String target, String separator) {
-        return target.split(separator);
+    private static final String DEFAULT_SEPARATOR =  ",|:";
+
+    public static String[] split(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            String customSeparator = m.group(1);
+            return m.group(2).split(customSeparator);
+        }
+
+        return input.split(DEFAULT_SEPARATOR);
     }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -7,14 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
 
-    @DisplayName("separator를 통한 String split 테스트")
+    @DisplayName("쉼표 또는 콜론 구분자 테스트")
     @Test
-    void split() {
-        String separator = ",|:";
+    void split_쉼표또는콜론구분자() {
+        assertThat(StringCalculator.split("")).containsExactly("");
+        assertThat(StringCalculator.split("1,2")).containsExactly("1", "2");
+        assertThat(StringCalculator.split("1,2,3")).containsExactly("1", "2", "3");
+        assertThat(StringCalculator.split("1,2:3")).containsExactly("1", "2", "3");
+    }
 
-        assertThat(StringCalculator.split("", separator)).containsExactly("");
-        assertThat(StringCalculator.split("1,2", separator)).containsExactly("1", "2");
-        assertThat(StringCalculator.split("1,2,3", separator)).containsExactly("1", "2", "3");
-        assertThat(StringCalculator.split("1,2:3", separator)).containsExactly("1", "2", "3");
+    @DisplayName("커스텀 구분자 테스트")
+    @Test
+    void split_커스텀구분자() {
+        assertThat(StringCalculator.split("//;\n1;2;3")).containsExactly("1", "2", "3");
+        assertThat(StringCalculator.split("//#\n1#2;3")).containsExactly("1", "2;3");
     }
 }
