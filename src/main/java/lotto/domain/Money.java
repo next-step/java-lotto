1dba14b905bf3621d8ed6d9d1a67b6a28045b679
bbbnam <bbbnam@naver.com>
2021-05-19 22:37:05 +0900

    feat : 금액 음수 입력시 예외발생 기능 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,12 +5,21 @@ import java.util.Objects;
 
 public class Money {
     public static final Money NONE = from(0);
+    private static final int MINIMUN_OF_MONEY = 0;
     private static final int SECOND_DECIMAL_PLACES = 2;
+    private static final String NEGATIVE_EXCEPTION_MESSAGE = "입력금액은 음수가 될 수 없습니다.";
 
     private final BigDecimal money;
 
     public Money(BigDecimal money) {
+        validateNegative(money);
         this.money = money;
+    }
+
+    private void validateNegative(BigDecimal money) {
+        if (money.compareTo(BigDecimal.ZERO) < MINIMUN_OF_MONEY) {
+            throw new IllegalArgumentException(NEGATIVE_EXCEPTION_MESSAGE);
+        }
     }
 
     public static Money from(long price) {
