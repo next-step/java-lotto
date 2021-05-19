6f1976afdfb7fc0457cc7300385cc2d6d02f2969
dhmin5693 <dhmin5693@naver.com>
2021-05-20 02:02:37 +0900

    feat: 로또 정의

---
+++ src/main/java/step3/domain/Lotto.java
@@ -0,0 +1,43 @@
+package step3.domain;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+
+        if (numbers == null || numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+        }
+
+        if (isLottoNumberRange(numbers)) {
+            throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+        }
+
+        numbers.sort(Comparator.naturalOrder());
+        this.numbers = Collections.unmodifiableList(numbers);
+
+        if (hasDuplicateNumber()) {
+            throw new IllegalArgumentException("로또 번호는 중복된 수를 포함하면 안됩니다.");
+        }
+    }
+
+    private boolean isLottoNumberRange(List<Integer> numbers) {
+        return numbers.stream()
+                      .anyMatch(number -> number < 1 || number > 45);
+    }
+
+    private boolean hasDuplicateNumber() {
+        return new HashSet<>(numbers).size() != 6;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+}
---
+++ src/test/java/step3/domain/LottoTest.java
@@ -0,0 +1,68 @@
+package step3.domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class LottoTest {
+
+    @DisplayName("로또 번호의 개수는 6개여야 한다.")
+    @MethodSource("lottoNumberSizeTestCase")
+    @ParameterizedTest
+    void lottoNumberSizeTest(List<Integer> numbers) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Lotto(numbers));
+    }
+
+    @SuppressWarnings("unused")
+    private static Stream<Arguments> lottoNumberSizeTestCase() {
+        return Stream.of(
+            Arguments.of(Collections.emptyList()),
+            Arguments.of(Collections.singletonList(1)),
+            Arguments.of(Arrays.asList(1, 2)),
+            Arguments.of(Arrays.asList(1, 2, 3)),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
+        );
+    }
+
+    @DisplayName("로또 번호는 1 ~ 45 사이의 수만 사용할 수 있다.")
+    @MethodSource("lottoNumberRangeTestCase")
+    @ParameterizedTest
+    void lottoNumberRangeTest(List<Integer> numbers) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Lotto(numbers));
+    }
+
+    @SuppressWarnings("unused")
+    private static Stream<Arguments> lottoNumberRangeTestCase() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 46, 2, 3, 4, 5)),
+            Arguments.of(Arrays.asList(0, 45, 2, 3, 4, 5))
+        );
+    }
+
+    @DisplayName("로또 번호는 중복될 수 없다.")
+    @Test
+    void duplicateTest() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Lotto(Arrays.asList(1, 1, 2, 3, 4, 5)));
+    }
+
+    @DisplayName("로또 정상 생성 및 출력")
+    @Test
+    void success() {
+        assertDoesNotThrow(() -> {
+            Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+            System.out.println(lotto);
+        });
+    }
+}
