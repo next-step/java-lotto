6961aed9f61584f35101544593c6cf440753bb6b
dhmin5693 <dhmin5693@naver.com>
2021-05-20 01:01:31 +0900

    refactor: unused import 제거

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -1,7 +1,6 @@
 package step2;
 
 import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
