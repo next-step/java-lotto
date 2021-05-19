d1c12ee7fabd48471c0529394bdb9cd5618b9276
ohgillwhan <soora_genius@naver.com>
2021-05-19 11:49:03 +0900

    feat : 로또 랭킹이 있고, 맞은 갯수에 맞게 로또랭킹을 가져와야 한다

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FORTH(3, 5_000),
+    MISS(0, 0);
+
+    private int countOfMatched;
+    private int amount;
+
+    LottoRank(int countOfMatched, int amount) {
+        this.countOfMatched = countOfMatched;
+        this.amount = amount;
+    }
+
+    public static LottoRank valueOf(int countOfMatched) {
+        return Arrays.stream(values())
+                .filter(item -> item.getCountOfMatched() == countOfMatched)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    public int getCountOfMatched() {
+        return countOfMatched;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
---
+++ src/test/java/lotto/LottoRankTest.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import lotto.domain.LottoRank;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoRankTest {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,MISS",
+            "2,MISS",
+            "3,FORTH",
+            "4,THIRD",
+            "5,SECOND",
+            "6,FIRST"
+    })
+    @DisplayName("맞은 갯수에 맞게 로또랭킹을 가져와야 한다")
+    public void 맞은_갯수에_맞게_로또랭킹을_가져와야_한다(int countOfMatched, LottoRank except) {
+        assertThat(LottoRank.valueOf(countOfMatched))
+                .isEqualTo(except);
+    }
+}
