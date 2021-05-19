f3177dcf1b6fbcc02c407cc0d73a2c553e0cd6a5
김석진 <7271kim@naver.com>
2021-05-20 00:23:55 +0900

    feature : 기능 목록 2 구현
    -  “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용하여 합을 구한다.
    
    [수정사항]
    - Calculator : custom 구분자와, 기본 구분자 분리처리
    - CalculatorTest : addNumberBetweenSeparatorTest2 추가
    - README.md : 기능 목록 수정

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -1,5 +1,8 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     public int addNumberBetweenSeparator(String input) {
@@ -16,15 +19,39 @@ public class Calculator {
         for (String numbesr : numbers) {
             result += parseNumber(numbesr);
         }
+
         return result;
     }
 
     private int parseNumber(String numbesr) {
-        return Integer.parseInt(numbesr);
+        return !checkNull(numbesr) ? Integer.parseInt(numbesr) : 0;
     }
 
     private String[] separateWithSeparator(String input) {
-        return input.split(",|:");
+        String[] result = separateWithCustomSeparator(input);
+        result = separateWithCommaOrColon(result, input);
+
+        return result;
+    }
+
+    private String[] separateWithCommaOrColon(String[] result, String input) {
+        if (result.length == 0) {
+            result = input.split(",|:");
+        }
+
+        return result;
+    }
+
+    private String[] separateWithCustomSeparator(String input) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        String[] result = {};
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            result = matcher.group(2).split(customDelimiter);
+        }
+
+        return result;
     }
 
     private boolean checkNull(String input) {
--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -23,4 +23,11 @@ public class CalculatorTest {
         assertThat(calculator.addNumberBetweenSeparator(input)).isEqualTo(result);
     }
 
+    @ParameterizedTest
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자 기준 split 테스트")
+    @CsvSource(value = {"'//;\n1;2;3':6", "'//ㅋ\n1ㅋ2ㅋ3':6", "'//ㅋ\n':0"}, delimiter = ':')
+    void addNumberBetweenSeparatorTest2(String input, int result) {
+        assertThat(calculator.addNumberBetweenSeparator(input)).isEqualTo(result);
+    }
+
 }
