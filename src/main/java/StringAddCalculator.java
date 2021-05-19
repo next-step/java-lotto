0988fc4473ce9ae1cf0ccb6c70b1522bb4834eca
JisuLim <sanola3@gmail.com>
2021-05-19 15:50:05 +0900

    refactor: splitAndSum split text by custom delimitter

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 	public static int splitAndSum(String inputText) {
@@ -6,8 +8,18 @@ public class StringAddCalculator {
 			return 0;
 		}
 
-		return Arrays.stream(inputText.split("[,:]"))
+		return Arrays.stream(getStringTokens(inputText))
 				.map(Integer::parseInt)
 				.reduce(0, Integer::sum);
+	}
+
+	private static String[] getStringTokens(String text) {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			return m.group(2).split(customDelimiter);
+		}
+
+		return text.split("[,:]");
 	}
 }
