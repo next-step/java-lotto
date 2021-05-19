08bd21d99cf92001354840651c849f4b4d1448a2
bbbnam <bbbnam@naver.com>
2021-05-19 22:36:40 +0900

    test : 금액(돈) 음수값 입력시 예외발생

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -3,10 +3,23 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MoneyTest {
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1, -0.10, -500})
+    @DisplayName("돈에 음수값이 입력되면 예외 발생")
+    void checkNegative(double input) {
+        assertThatThrownBy(() -> new Money(BigDecimal.valueOf(input)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력금액은 음수가 될 수 없습니다.");
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"1400:700:2.00", "3000:1700:1.76", "5000:14000:0.35"}, delimiter = ':')
