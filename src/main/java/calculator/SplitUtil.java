ff60df15c4466aab5420d4393dfaec41284c6efc
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-19 14:17:46 +0900

    feat : 커스텀 구분자로 문자열 분리하는 기능 구현
    + docs : 기능 요구사항 정리
    - 기능 및 제약사항을 자세히 기술

--- src/main/java/calculator/SplitUtil.java
+++ src/main/java/calculator/SplitUtil.java
@@ -1,17 +1,44 @@
 package calculator;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SplitUtil {
-    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int GROUP_NUMBER_CUSTOM_DELIMITER = 1;
+    private static final int GROUP_NUMBER_OPERANDS = 2;
 
     public static Operands splitByDelimiter(String input) {
         List<Operand> operands = new ArrayList<>();
-        String[] splitString = input.split(DEFAULT_DELIMITER);
+
+        String[] splitString = splitAccordingly(input);
         for (String operand : splitString) {
             operands.add(new Operand(operand));
         }
         return new Operands(operands);
+    }
+
+    private static String[] splitAccordingly(String input) {
+        Matcher delimiterMatcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+
+        if (delimiterMatcher.find()) {
+            return byCustomDelimiter(delimiterMatcher.group(GROUP_NUMBER_OPERANDS), delimiterMatcher.group(GROUP_NUMBER_CUSTOM_DELIMITER));
+        }
+
+        return byDefaultDelimiter(input, DEFAULT_DELIMITER);
+    }
+
+    private static String[] byCustomDelimiter(String operands, String delimiter) {
+        return operands.split(DEFAULT_DELIMITER+"|"+delimiter);
+    }
+
+
+    private static String[] byDefaultDelimiter(String operands, String delimiter) {
+        return operands.split(delimiter);
     }
 }
