c8bcead7833dd5a6f8ab09ad8d4e8a2462252e07
jaewanLee <ianlee9180@gmail.com>
2021-05-19 17:27:41 +0900

    test: splitTest 요구사항1-1
    splitTest - "1","2" 값 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,8 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    void split(){
+    @DisplayName("요구사항1-1")
+    void arraySplit(){
         String[] result="1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
         assertThat(result).containsExactly("1","2");
     }
+
 }
