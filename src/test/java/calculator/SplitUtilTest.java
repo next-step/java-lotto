503236ac84045d4162660284e08f3dee3f0c388d
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-19 14:13:07 +0900

    test : SplitUtil 테스트 추가
    - 기본 구분자(쉼표, 콜론)이외에도 커스텀 구분자를 통해서도 분리가 되는지 확인하는 테스트 작성

--- src/test/java/calculator/SplitUtilTest.java
+++ src/test/java/calculator/SplitUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 public class SplitUtilTest {
 
-    @DisplayName("쉼표나 콜론으로 문자열 Split하는지 테스트")
+    @DisplayName("Default 구분자(쉼표,콜론)로 문자열 Split하는지 테스트")
     @Test
     void split_by_default_delimiter(){
         //Given
@@ -17,5 +17,31 @@ public class SplitUtilTest {
 
         //Then
         assertThat(operands.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Custom 구분자로 문자열 Split하는지 테스트")
+    @Test
+    void split_by_custom_delimiter(){
+        //Given
+        String input = "//a\n1a2a3";
+
+        //When
+        Operands operands = SplitUtil.splitByDelimiter(input);
+
+        //Then
+        assertThat(operands.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Default+Custom 구분자로 문자열 Split하는지 테스트")
+    @Test
+    void split_by_default_and_custom_delimiter(){
+        //Given
+        String input = "//a\n1:2a3,4";
+
+        //When
+        Operands operands = SplitUtil.splitByDelimiter(input);
+
+        //Then
+        assertThat(operands.size()).isEqualTo(4);
     }
 }
