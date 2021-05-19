f8d21accca6b08a58e30d4103acea3f1bdf99768
박소정 <psj6414@naver.com>
2021-05-19 19:15:40 +0900

    feat: 금액 입력 및 로또 개수 계산
    금액 입력 및 로또 개수 계산
    - 입력금액 로또개수 계산 함수
    - 입력금액 검증(양의 정수가 아닐경우 예외처리)

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,26 @@
+package lotto;
+
+public class Lotto {
+    private static final int DEFAULT_PRICE = 1000;
+    private static final String MATCH_PRICE = "^[0-9]+$";
+    private static final String EXCEPTION_MESSAGE = "정수만 입력해주세요.";
+
+    private InputView inputView = new InputView();
+
+    public void startPurchase() {
+        inputView.printInputPurchasePrice();
+        int price = calculatePrice(validateInputPrice(inputView.inputPurchasePrice()));
+        inputView.printPurchasePrice(price);
+    }
+
+    protected int validateInputPrice(String price) {
+        if (!price.matches(MATCH_PRICE)) {
+            throw new RuntimeException(EXCEPTION_MESSAGE);
+        }
+        return Integer.parseInt(price);
+    }
+
+    public int calculatePrice(int price) {
+        return price / DEFAULT_PRICE;
+    }
+}
