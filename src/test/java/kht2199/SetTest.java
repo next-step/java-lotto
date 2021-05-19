50579439938a38d0d72b4d7c6ff573b4c36ff7ef
hee-taek <kht2199@gmail.com>
2021-05-20 03:04:28 +0900

    Step1. 학습 테스트 실습 (#1547)
    * feat: implementation step1. split test.
    
    * feat: implementation String replace step 2 test using regex.
    
    * test: String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트 구현
    
    * test: Set Collection에 대한 학습 테스트

---
+++ src/test/java/kht2199/SetTest.java
@@ -0,0 +1,49 @@
+package kht2199;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	// Test Case 구현
+	@Test
+	void sizeExpectThree() {
+		assertThat(numbers.size())
+			.isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void contains(int param) {
+		assertThat(numbers.contains(param)).isTrue();
+	}
+
+	/**
+	 * 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
+	 * 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다
+	 */
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int number, boolean expected) {
+		assertEquals(expected, numbers.contains(number));
+	}
+}
