a993eea344f00b133f45ba92570e051df5ae5a5e
ttuop <plintcsz@gmail.com>
2021-05-19 15:15:19 +0900

    refactor: refactoring StringSeparator
    * rename class StringSeparator -> StringSplitter
    * change access modifier split
    * add get method

--- src/test/java/study/StringSplitterTest.java
+++ src/test/java/study/StringSplitterTest.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
-public class StringSeparatorTest {
+public class StringSplitterTest {
 
 	@ParameterizedTest
 	@NullSource
 	@EmptySource
 	void default_delimiter(String input) {
-		StringSeparator separator = new StringSeparator(new Delimiter(input), "1,2:3");
+		StringSplitter separator = new StringSplitter(new Delimiter(input), "1,2:3");
 
-		assertThat(separator.split()).containsExactly("1", "2", "3");
+		assertThat(separator.get()).containsExactly("1", "2", "3");
 	}
 
 	@Test
 	void custom_delimiter() {
-		StringSeparator separator = new StringSeparator(new Delimiter("!"), "4!5!6");
+		StringSplitter separator = new StringSplitter(new Delimiter("!"), "4!5!6");
 
-		assertThat(separator.split()).containsExactly("4", "5", "6");
+		assertThat(separator.get()).containsExactly("4", "5", "6");
 	}
 }
