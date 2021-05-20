d81ea1080592ab2b5348ed4ee036ff1b7a9d09f9
sj.park <sj.park1@lotte.net>
2021-05-20 16:23:27 +0900

    fix: 당첨 번호 입력 오류 수정
    당첨번호 입력 수정
    - 쉼표 포함해 정상값 입력시 오류 발생하여 정규식 수정

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public class WinningNumbers {
     private static final String SEPARATOR = ",";
-    private static final String SEPARATOR_PATTERN = "^([1-9]{1,2},)+([1-9]{1,2})$";
+    private static final String SEPARATOR_PATTERN = "^(([1-9]{1}|[1-3]{1}[0-9]{1}|[4]{1}[0-5]{1}),)+([1-9]{1}|[1-3]{1}[0-9]{1}|[4]{1}[0-5]{1})$";
     private static final String MATCH_PRICE = "^[0-9]+$";
     private static final String COMMA_MISS_EXCEPTION = "당첨번호는 쉼표(,)로 구분되어야합니다.";
     private static final String INPUT_WINNING_NUMBER_EXCEPTION = "당첨번호를 입력해주세요.";
     private static final String WINNING_NUMBER_DEFAULT_RANGE = "1 ~ 45 사이의 숫자만 입력해주세요.";
-    private static final int MAX_WINNING_NUMBER = 6;
+    private static final int MAX_WINNING_NUMBER_DIGIT = 6;
+    private static final int MIN_LOTTO_RANGE = 0;
+    private static final int MAX_LOTTO_RANGE = 45;
 
     List<Integer> winningNumbers;
 
@@ -37,14 +39,14 @@ public class WinningNumbers {
             return false;
         }
         int number = Integer.parseInt(numberString);
-        if (number < 0 || number > 45) {
+        if (number < MIN_LOTTO_RANGE || number > MAX_LOTTO_RANGE) {
             return false;
         }
         return true;
     }
 
     public boolean checkWinningNumberDigit(int length) {
-        if (length != MAX_WINNING_NUMBER) {
+        if (length != MAX_WINNING_NUMBER_DIGIT) {
             return false;
         }
         return true;
--- src/test/java/lotto/WinningNumbersTest.java
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
