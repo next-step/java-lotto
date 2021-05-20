c7cc00a31ac7b0fae070393ebf20b5e20d966eb7
jess <jess.l@kakaocorp.com>
2021-05-21 00:07:23 +0900

    feat: 기본 구분자, 커스텀 구분자로 스트링 파싱하는 기능 추가

---
+++ src/main/java/calculator/StringParser.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringParser {
+    public final static String DEFAULT_DELIMITER = ",|:";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    public StringParser(String csv) {
+    }
+
+    public static String[] parse(String text) {
+        Matcher matcher = CUSTOM_PATTERN.matcher(text);
+        boolean isCustom = matcher.find();
+        if(isCustom){
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
+    }
+}
+
