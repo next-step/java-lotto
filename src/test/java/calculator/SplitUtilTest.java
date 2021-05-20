12fcba046962d409c32809c350c2235b5482279d
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-20 23:26:43 +0900

    test : 문자열로만 테스트 가능하도록 수정

--- src/test/java/calculator/SplitUtilTest.java
+++ src/test/java/calculator/SplitUtilTest.java
@@ -2,8 +2,6 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,50 +9,47 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class SplitUtilTest {
 
     @DisplayName("Default 구분자(쉼표,콜론)로 문자열 Split하는지 테스트")
-    @ParameterizedTest(name = "{displayName} ==> input : {0}")
-    @CsvSource({"1","2","3"})
-    void split_by_default_delimiter(String input){
+    @Test
+    void split_by_default_delimiter() {
         //Given
         String str = "1:2,3";
 
         //When
-        Operands operands = new Operands(SplitUtil.splitByDelimiter(str));
+        String[] splitStrings = SplitUtil.splitByDelimiter(str);
 
         //Then
-        assertThat(operands.contains(new Operand(input))).isTrue();
+        assertThat(splitStrings).containsExactly("1", "2", "3");
     }
 
     @DisplayName("Custom 구분자로 문자열 Split하는지 테스트")
-    @ParameterizedTest(name = "{displayName} ==> input : {0}")
-    @CsvSource({"1","2","3"})
-    void split_by_custom_delimiter(String input){
+    @Test
+    void split_by_custom_delimiter() {
         //Given
         String str = "//a\n1a2a3";
 
         //When
-        Operands operands = new Operands(SplitUtil.splitByDelimiter(str));
+        String[] splitStrings = SplitUtil.splitByDelimiter(str);
 
         //Then
-        assertThat(operands.contains(new Operand(input))).isTrue();
+        assertThat(splitStrings).containsExactly("1", "2", "3");
     }
 
     @DisplayName("Default+Custom 구분자로 문자열 Split하는지 테스트")
-    @ParameterizedTest(name = "{displayName} ==> input : {0}")
-    @CsvSource({"1","2","3","4"})
-    void split_by_default_and_custom_delimiter(String input){
+    @Test
+    void split_by_default_and_custom_delimiter() {
         //Given
         String str = "//a\n1:2a3,4";
 
         //When
-        Operands operands = new Operands(SplitUtil.splitByDelimiter(str));
+        String[] splitStrings = SplitUtil.splitByDelimiter(str);
 
         //Then
-        assertThat(operands.contains(new Operand(input))).isTrue();
+        assertThat(splitStrings).containsExactly("1", "2", "3", "4");
     }
 
     @DisplayName("Custom 구분자가 여러 개인 경우, IllegalArgumentException을 반환하는지 테스트")
     @Test
-    void numerous_custom_delimiter(){
+    void numerous_custom_delimiter() {
         //Given
         String input = "//a;\n1;2a3";
 
