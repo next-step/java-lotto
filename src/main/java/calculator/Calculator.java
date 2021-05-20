e98147a09a0a5be57d6f80d3701c0fdd4f5a641e
jess <jess.l@kakaocorp.com>
2021-05-20 23:51:41 +0900

    feat(Calculator): 숫자 하나를 문자열로 입력한 경우 해당 숫자를 반환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,9 +3,14 @@ package calculator;
 import java.util.Objects;
 
 public class Calculator {
+    private static final String NUMBER_REGEX = "(-)?[0-9]+";
+
     public static int sum(String text) {
         if (isNullOrEmpty(text)) {
             return 0;
+        }
+        if (isNumber(text)) {
+            return parseNumber(text);
         }
         return 1;
     }
@@ -13,5 +18,15 @@ public class Calculator {
     private static boolean isNullOrEmpty(String text) {
         return Objects.isNull(text) || text.isEmpty();
     }
+
+    private static boolean isNumber(String text) {
+        return text.matches(NUMBER_REGEX);
+    }
+
+    private static int parseNumber(String text) {
+        int value = Integer.parseInt(text);
+        return value;
+    }
+
 }
 
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,4 +12,12 @@ class CalculatorTest {
     void 빈_문자열_또는_null_값을_입력한_경우_0을_반환한다(String text) {
         assertThat(Calculator.sum(text)).isZero();
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"11", "1", "0"})
+    void 숫자_하나를_문자열로_입력한_경우_해당_숫자를_반환한다(String text) {
+        int value = Integer.parseInt(text);
+        assertThat(Calculator.sum(text)).isEqualTo(value);
+    }
+
 }
