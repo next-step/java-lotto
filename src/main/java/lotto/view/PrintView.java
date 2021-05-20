335aae1c6a2225ee9e9f90ead64d9ed40bdb9050
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 01:58:51 +0900

    feat : 로또의 구매 결과를 출력하는 메소드 기능구현

--- src/main/java/lotto/view/PrintView.java
+++ src/main/java/lotto/view/PrintView.java
@@ -1,10 +1,32 @@
 package lotto.view;
 
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoWallet;
+
+import java.util.List;
+import java.util.StringJoiner;
+
 public class PrintView {
 
-    public void showStatusBuyTicket() {
-        System.out.println("개를 구매했습니다.");
-        // 티켓출력
+    public void showStatusBuyTicket(LottoWallet lottoWallet) {
+        List<LottoTicket> lottoTickets = lottoWallet.lottoTickets();
+        System.out.println(lottoWallet.ticketCount() +"개를 구매했습니다.");
+
+        for (LottoTicket lottoTicket : lottoTickets) {
+            printLottoTicket(lottoTicket);
+        }
+    }
+
+    private void printLottoTicket(LottoTicket lottoTicket) {
+        StringJoiner lottoNumberJoiner = new StringJoiner(",", "[", "]");
+
+        for (LottoNumber lottoNumber : lottoTicket.numbers()) {
+            int number = lottoNumber.value();
+            lottoNumberJoiner.add(String.valueOf(number));
+        }
+
+        System.out.println(lottoNumberJoiner.toString());
     }
 
     public void showLottoResult() {
