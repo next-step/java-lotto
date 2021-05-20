5530be78b840d0d3e63b0efb634840bb21b3489f
taemin <taminging@kakao.com>
2021-05-21 00:36:42 +0900

    feat : LottoResult - 미구현

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoResult {
+
+	private final List<LottoRank> lottoRanks;
+
+	public LottoResult() {
+		this.lottoRanks = new ArrayList<>();
+	}
+
+}
---
+++ src/test/java/lotto/LottoResultTest.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+public class LottoResultTest {
+
+	@Test
+	public void getLottoResultTest() {
+		//lottoResult.;
+	}
+}
