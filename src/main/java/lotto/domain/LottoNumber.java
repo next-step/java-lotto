737f7b7b94fd11c0e2695edc30ca3bef53b5d047
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 00:18:42 +0900

    feat : 로또번호 일급컬렉션

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber create(int number) {
+        if(number < 1 || number > 45) {
+            throw new IllegalArgumentException("잘못된 로또 범위 입니다. (1~45사이)");
+        }
+        return new LottoNumber(number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
