999dbfb3ab5c2c2ff5c96249aaf59d1426ab653e
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-21 08:27:01 +0900

    refactor : FormulaDelimiter 클래스 삭제  - enum 클래스를 삭제하고, 비즈니스 로직을 CalculatingNumbers 클래스에서 관리하는 구조로 수정

--- src/main/java/calculator/CalculatingNumbers.java
+++ src/main/java/calculator/CalculatingNumbers.java
@@ -12,6 +12,7 @@ public class CalculatingNumbers {
     private static final int CUSTOM_DELIMITER_INDEX = 1;
     private static final int CALCULATE_TARGET_INDEX = 2;
     private static final Pattern FORMULA_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_DELIMITER = "[,:]";
 
     private final List<CalculatingNumber> calculatingNumbers;
 
@@ -26,7 +27,7 @@ public class CalculatingNumbers {
             return m.group(CALCULATE_TARGET_INDEX).split(customDelimiter);
         }
 
-        return FormulaDelimiter.split(formula);
+        return formula.split(DEFAULT_DELIMITER);
     }
 
     private List<CalculatingNumber> parseCalculatorNumbers(String[] formula) {
