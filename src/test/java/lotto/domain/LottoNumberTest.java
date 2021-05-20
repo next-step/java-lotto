03d4f9cfb6d074e82105e40b09c43a928ea753a2
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 00:18:15 +0900

    test : 로또번호에 대한 테스트케이스 작성 - 정상범위 - 비정상범위

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTest {
+
+    @DisplayName("로또번호의 정상적인 생성확인")
+    @ParameterizedTest
+    @ValueSource(ints = {1,10,45})
+    public void createLottoNumber(int number) {
+        LottoNumber lottoNumber = LottoNumber.create(number);
+
+        assertThat(lottoNumber).isEqualTo(LottoNumber.create(number));
+    }
+
+    @DisplayName("로또번호의 비정상적인 범위에서의 예외확인")
+    @ParameterizedTest
+    @ValueSource(ints = {-1,0,46})
+    public void createInvalidLottoNumber(int number) {
+        assertThatThrownBy(() -> LottoNumber.create(number))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+}
