b936ba711d810a7df463a3a8d53cc625c24186ce
백승열 <beck33333@naver.com>
2021-05-21 00:53:44 +0900

    feat : StringAddCalculator 문자열 덧셈 계산기 기능 추가

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,43 @@
+package step2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) return 0;
+        if (text.length() == 1) return Integer.parseInt(text);
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (m.find()) {
+            return getSumOfTokens(m.group(2), m.group(1));
+        }
+        return getSumOfTokens(text,",|:");
+    }
+
+    private static int getSumOfTokens(String text, String delimiter) {
+        String[] tokens = findNoNegativeTokens(text, delimiter);
+        return calculateTokens(tokens);
+    }
+
+    private static String[] findNoNegativeTokens(String input, String delimiter) {
+        String[] result = input.split(delimiter);
+        for (int i = 0; i < result.length; ++i) {
+            negativeCheck(result[i]);
+        }
+        return result;
+    }
+
+    private static void negativeCheck(String text) {
+        if (Integer.parseInt(text) < 0)
+            throw new RuntimeException("음수 입니다.");
+    }
+
+    private static int calculateTokens(String[] tokens) {
+        int sum = 0;
+        for (int i = 0; i < tokens.length; ++i) sum += Integer.parseInt(tokens[i]);
+        return sum;
+    }
+}
