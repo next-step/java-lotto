c106731c8a0186f0054bc348de2fe4c3a2b95114
chajs226 <chajs226@gmail.com>
2021-05-20 14:46:37 +0900

    [1주차 - Step1] 리뷰 요청드립니다. (#1554)
    * split string study
    
    * split_1 string study
    
    * substing test
    
    * feat: String 클래스 학습테스트
    
    * feat: set collection 학습테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,25 +1,46 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+
+    @DisplayName("요구사항1: 1,2을 ,로 split 확인")
     @Test
     void split_1_2() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
+    @DisplayName("요구사항1: 1을 ,로 split 확인")
     @Test
     void split_1() {
         String[] result = "1".split(",");
         assertThat(result).containsExactly("1");
     }
 
+    @DisplayName("요구사항2: (1,2)를 substring 확인")
     @Test
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("요구사항3: abc를 charAt으로 위치 가져오는 것 확인")
+    @Test
+    void charAt() {
+        String abc = "abc";
+        char rt = abc.charAt(0);
+        assertThat(rt).isEqualTo('a');
+    }
+
+    @DisplayName("요구사항3: StringIndexOutOfBoundsException 확인")
+    @Test
+    public void testCharAtException() {
+        assertThatThrownBy(() -> "abc".charAt(5))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessage(("index를 벗어났습니다."));
     }
 }
