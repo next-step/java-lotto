76b3df8d9ad0f37bd89d253c563e6c4feee96ff8
bbbnam <bbbnam@naver.com>
2021-05-20 22:56:10 +0900

    refactor : 불필요 import 제거

--- src/test/java/lotto/domain/ProfitStatisticsTest.java
+++ src/test/java/lotto/domain/ProfitStatisticsTest.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
