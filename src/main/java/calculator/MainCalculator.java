057229609cc7a27cff6ac310736f187cc81f0bb4
itdar <thesockerrr@gmail.com>
2021-05-21 02:59:33 +0900

    feat: 프로그램 진입점 메인클래스 및 함수 추가

---
+++ src/main/java/calculator/MainCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class MainCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StringCalculator stringCalculator = new StringCalculator();
+        System.out.println(stringCalculator.calculate(scanner.next()));
+    }
+}
