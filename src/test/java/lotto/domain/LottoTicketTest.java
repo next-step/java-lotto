50007ebb42b2fdeb2b2b6451ecef62bbd7b2576e
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 01:38:17 +0900

    test : 로또번호 갯수가 6개가 아닐때 예외처리 테스트케이스 - 예외처리 - LottoNumber클래스 적용

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.utils.LottoNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,17 +8,28 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTicketTest {
 
     @DisplayName("로또 티켓 생성하기")
     @Test
     public void createLottoTicket() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-
-        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+        List<String> lottoNumberText = Arrays.asList("1", "2", "3", "4", "5", "6");
+        List<LottoNumber> lottoNumbers = LottoNumberGenerator.manualGenerator(lottoNumberText);
+        LottoTicket lottoTicket = LottoTicket.create(lottoNumbers);
 
         assertThat(lottoTicket.numbers()).hasSize(6);
+    }
+
+    @DisplayName("로도 번호가 6개가 아닐때 예외")
+    @Test
+    public void invalidLottoNumberCountTest() {
+        List<String> lottoNumberText = Arrays.asList("1", "2", "3", "4", "5");
+        List<LottoNumber> lottoNumbers = LottoNumberGenerator.manualGenerator(lottoNumberText);
+
+        assertThatThrownBy(() -> LottoTicket.create(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
