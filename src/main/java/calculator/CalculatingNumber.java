9ea4a51cd5e665e29e1a91e29f39d44ff5b45f79
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-21 08:19:03 +0900

    refactor : 문자열 파싱과 음수체크로직 분리

--- src/main/java/calculator/CalculatingNumber.java
+++ src/main/java/calculator/CalculatingNumber.java
@@ -5,19 +5,18 @@ public class CalculatingNumber {
     private final int number;
 
     public CalculatingNumber(String number) {
-        this.number = parseInt(number);
-    }
-
-    private int parseInt(String number) {
         try {
-            int parsedNumber = Integer.parseInt(number);
-            if(parsedNumber < 0) {
-                throw new RuntimeException("음수는 입력할 수 없습니다.");
-            }
-            return parsedNumber;
+            this.number = validateNumber(Integer.parseInt(number));
         } catch (NumberFormatException e) {
             throw new RuntimeException("양의 정수가 아닙니다. 입력값을 확인해 주세요.");
         }
+    }
+
+    private int validateNumber(int calculatingNumber) {
+        if(calculatingNumber < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+        return calculatingNumber;
     }
 
     public int getNumber() {
