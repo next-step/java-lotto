573a4d4e25a26da894a11dbf8943c52d1fa8ef3e
dhmin5693 <dhmin5693@naver.com>
2021-05-21 03:15:58 +0900

    feat: 메인 메소드 추가

---
+++ src/main/java/step3/ui/IOManager.java
@@ -0,0 +1,50 @@
+package step3.ui;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class IOManager {
+
+    private static final IOManager STANDARD_IO_MANAGER = new IOManager(new StandardInput(),
+                                                                       new StandardOutput());
+
+    private final Input input;
+    private final Output output;
+
+    public IOManager(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public static IOManager standardIOManager() {
+        return STANDARD_IO_MANAGER;
+    }
+
+    public int inputMoney() {
+        output.printLine("구입금액을 입력해주세요.");
+        return input.nextInt();
+    }
+
+    public List<Integer> inputNumbers() {
+        input.nextLine();
+        String data = input.nextLine();
+        return Arrays.stream(data.split(","))
+                     .map(String::trim)
+                     .map(Integer::parseInt)
+                     .collect(toList());
+    }
+
+    public void printBuyCount(int count) {
+        output.printLine(count + "개를 구매했습니다.");
+    }
+
+    public void printLint(String text) {
+        output.printLine(text);
+    }
+
+    public void printMultiLine(List<String> statistics) {
+        statistics.forEach(output::printLine);
+    }
+}
