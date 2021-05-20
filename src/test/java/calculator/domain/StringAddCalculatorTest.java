e534e83a58a46e11f27ca78ae73e09bc7e43fb63
jordy-torvalds <58139899+jordy-torvalds@users.noreply.github.com>
2021-05-20 16:57:41 +0900

    [1주차 - Step2] 리뷰 요청 드립니다. (#1549)
    * docs: 로또 step2(문자열 덧셈 계산기) 진행에 따른 readme.md 내 내용 수정
    
    * feat: 표현식에서 구분자와 연산식을 분리하는 기능 및 테스트 구현
    
    * docs: 문자열 구분자 기능 구현에 따른 기능 구현 목록 체크
    
    * refactor: 계산기 표현식 팩토리 테스트 내 @DisplayName 추가 및 불필요한 import 문 제거
    
    * style: 불필요한 공백 제거
    
    * feat: 숫자, 숫자 팩토리 도메인 및 테스트 구현
    
    * docs: 숫자, 숫자 팩토리 기능 구현에 따른 기능 구현 목록 체크
    
    * feat: 문자열 계산 도메인 및 테스트 구현
    
    * docs: 문자열 계산 도메인 구현에 따른 관련 기능 구현 목록 체크
    
    * refactor: 테스트 클래스 의 public 선언부 제거, 불필요한 import 문 제거

---
+++ src/test/java/calculator/domain/StringAddCalculatorTest.java
@@ -0,0 +1,48 @@
+package calculator.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("StringAddCalculatorTest 테스트")
+class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @MethodSource("provideSourceToCalculate")
+    @DisplayName("StringAddCalculatorTest calculate, 케이스_별_결과_검증")
+    void calculate_케이스_별_결과_검증(String expression, int expectedResult) {
+        // When
+        int actualResult = StringAddCalculator.calculate(expression);
+
+        // Then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    static Stream<Arguments> provideSourceToCalculate() {
+        return Stream.of(
+                Arguments.of("", 0),
+                Arguments.of(null, 0),
+                Arguments.of("//;\n1,2;3:4", 10),
+                Arguments.of("//@\n4@5@6:3", 18),
+                Arguments.of("2:2:4", 8)
+        );
+    }
+
+    @Test
+    @DisplayName("StringAddCalculatorTest calculate, 런타임 예외")
+    void calculate_예외() {
+        // Given
+        String expression = "-1:0,5";
+
+        // When, Then
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> StringAddCalculator.calculate(expression));
+    }
+}
