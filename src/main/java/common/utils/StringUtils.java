447b2eae7d1efc88372bae127a75770f09524f12
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-21 00:04:19 +0900

    refactor : Util 클래스의 생성자 접근레벨을 private 로 변경

--- src/main/java/common/utils/StringUtils.java
+++ src/main/java/common/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package common.utils;
 
 public class StringUtils {
+
+    private StringUtils() {
+    }
+
     public static boolean isBlank(String args) {
-        return args==null || args.trim().length()==0;
+        return args == null || args.trim().length() == 0;
     }
 }
