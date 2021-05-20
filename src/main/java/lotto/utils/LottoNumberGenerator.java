171264e5d7b648146c12545bc20763493d447f76
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 00:17:02 +0900

    feat : 수동, 자동 로또 생성번호 로직 분리

--- src/main/java/lotto/utils/LottoNumberGenerator.java
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -1,5 +1,6 @@
 package lotto.utils;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,7 +8,33 @@ import java.util.stream.IntStream;
 
 public class LottoNumberGenerator {
 
-    public static List<Integer> randomSixLottoNumbers() {
+    public static List<Integer> manualGenerator(List<String> numbers) {
+        List<Integer> lottoNumbers = transStringNumbersToIntegerNumbers(numbers);
+
+        Collections.sort(lottoNumbers);
+
+        return Collections.unmodifiableList(lottoNumbers);
+    }
+
+    private static List<Integer> transStringNumbersToIntegerNumbers(List<String> numbers) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+
+        for (String number : numbers) {
+            lottoNumbers.add(Integer.valueOf(number));
+        }
+
+        return lottoNumbers;
+    }
+
+    public static List<Integer> autoGenerator() {
+        List<Integer> lottoNumbers = randomSixLottoNumbers();
+
+        Collections.sort(lottoNumbers);
+
+        return Collections.unmodifiableList(lottoNumbers);
+    }
+
+    private static List<Integer> randomSixLottoNumbers() {
         List<Integer> availableLottoNumbers = availableAllLottoNumbers();
         Collections.shuffle(availableLottoNumbers);
 
