b890c49cbc28a17496f2ca4bd26a97722c93c77e
tmkwak <tmkwak@hmm21.com>
2021-05-21 17:29:59 +0900

    feat : LottoResult 미구현 - 생각중..

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -3,12 +3,19 @@ package lotto;
 import java.util.ArrayList;
 import java.util.List;
 
+import lotto.rank.FirstPlace;
+import lotto.rank.LottoRank;
+import lotto.rank.SecondPlace;
+
 public class LottoResult {
 
 	private final List<LottoRank> lottoRanks;
 
-	public LottoResult() {
-		this.lottoRanks = new ArrayList<>();
+	public LottoResult(List<LottoRank> lottoRanks) {
+		this.lottoRanks = lottoRanks;
 	}
 
+	public void printResult() {
+
+	}
 }
