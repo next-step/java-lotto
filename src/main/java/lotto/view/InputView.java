6cd0b6c064c9f4b1b2773286b46621aba0a09f01
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 13:19:22 +0900

    feat : 안내문구 상수추출

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String MESSAGE_QUESTION_HOW_MANY_BUY = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_QUESTION_WINNER_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+
     private Scanner scanner;
 
     public InputView() {
@@ -16,16 +19,14 @@ public class InputView {
     }
 
     public AmountMoney amountMoneyOfBuyTicket() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(MESSAGE_QUESTION_HOW_MANY_BUY);
         int inputAmountMoney = Integer.parseInt(scanner.nextLine());
 
         return AmountMoney.create(inputAmountMoney);
     }
 
-
-
     public List<String> inputWinnerNumber() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println(MESSAGE_QUESTION_WINNER_NUMBER);
         String inputWinnerNumber = scanner.nextLine();
         String[] winnerNumbers = StringUtils.split(inputWinnerNumber);
         return Arrays.asList(winnerNumbers);
