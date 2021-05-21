a78f16bdec830927a150a7fc3873cca674f8f61f
HyungjuJang <dacapolife87@gmail.com>
2021-05-22 02:32:12 +0900

    refactor : 로또번호 생성기능에서 랜덤함수를 외부 다른 클래스로 분리하고 숫자 리스트를 넘겨받아서 생성하도록 변경

--- src/main/java/lotto/utils/LottoNumberGenerator.java
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -5,11 +5,10 @@ import lotto.domain.LottoNumber;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoNumberGenerator {
 
-    public static List<LottoNumber> manualGenerator(List<String> numbers) {
+    public List<LottoNumber> generator(List<String> numbers) {
         List<LottoNumber> lottoNumbers = transStringNumbersToIntegerNumbers(numbers);
 
         Collections.sort(lottoNumbers);
@@ -17,30 +16,11 @@ public class LottoNumberGenerator {
         return Collections.unmodifiableList(lottoNumbers);
     }
 
-    private static List<LottoNumber> transStringNumbersToIntegerNumbers(List<String> numbers) {
+    private List<LottoNumber> transStringNumbersToIntegerNumbers(List<String> numbers) {
         return numbers.stream()
                 .map(number -> Integer.valueOf(number))
                 .map(LottoNumber::create)
                 .collect(Collectors.toList());
     }
 
-    public static List<LottoNumber> autoGenerator() {
-        List<LottoNumber> lottoNumbers = randomSixLottoNumbers();
-
-        Collections.sort(lottoNumbers);
-
-        return Collections.unmodifiableList(lottoNumbers);
-    }
-
-    private static List<LottoNumber> randomSixLottoNumbers() {
-        List<LottoNumber> availableLottoNumbers = availableAllLottoNumbers();
-        Collections.shuffle(availableLottoNumbers);
-
-        return availableLottoNumbers.subList(0, 6);
-    }
-
-    private static List<LottoNumber> availableAllLottoNumbers() {
-        return IntStream.range(1, 45)
-                .mapToObj(LottoNumber::create).collect(Collectors.toList());
-    }
 }
