5c934a55dd9e1205bf7e4c4a2c60f0dbbf2f34fa
jess <jess.l@kakaocorp.com>
2021-05-22 06:36:38 +0900

    test:  로또 픽스쳐 타입 변경

--- src/test/java/lotto/LottoFixture.java
+++ src/test/java/lotto/LottoFixture.java
@@ -5,12 +5,12 @@ import org.assertj.core.util.Lists;
 import java.util.List;
 
 public class LottoFixture {
-    public static final List<Integer> 우승번호 = Lists.newArrayList(1,2,3,4,5,6);
-    public static final List<Integer> 번호_6개_일치 = Lists.newArrayList(1,2,3,4,5,6);
-    public static final List<Integer> 번호_5개_일치 = Lists.newArrayList(45,2,3,4,5,6);
-    public static final List<Integer> 번호_4개_일치 = Lists.newArrayList(45,44,3,4,5,6);
-    public static final  List<Integer> 번호_3개_일치 = Lists.newArrayList(45,44,43,4,5,6);
-    public static final List<Integer> 번호_2개_일치 = Lists.newArrayList(45,44,43,42,5,6);
-    public static final  List<Integer> 번호_1개_일치 = Lists.newArrayList(45,44,43,42,41,6);
-    public static final  List<Integer> 번호_0개_일치 = Lists.newArrayList(45,44,43,42,41,40);
+    public static final Lotto 우승번호 = new Lotto(Lists.newArrayList(1,2,3,4,5,6));
+    public static final Lotto 번호_6개_일치 = new Lotto(Lists.newArrayList(1,2,3,4,5,6));
+    public static final Lotto 번호_5개_일치 = new Lotto(Lists.newArrayList(45,2,3,4,5,6));
+    public static final Lotto 번호_4개_일치 = new Lotto(Lists.newArrayList(45,44,3,4,5,6));
+    public static final  Lotto 번호_3개_일치 = new Lotto(Lists.newArrayList(45,44,43,4,5,6));
+    public static final Lotto 번호_2개_일치 = new Lotto(Lists.newArrayList(45,44,43,42,5,6));
+    public static final  Lotto 번호_1개_일치 = new Lotto(Lists.newArrayList(45,44,43,42,41,6));
+    public static final  Lotto 번호_0개_일치 = new Lotto(Lists.newArrayList(45,44,43,42,41,40));
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -29,21 +29,19 @@ class LottoTest {
 
     @ParameterizedTest
     @MethodSource("provider_로또는_입력받은_로또와_일치하는_번호의_수를_알려준다")
-    void 로또는_입력받은_로또와_일치하는_번호의_수를_알려준다(List<Integer> winners, List<Integer> numbers, int expected) {
-        Lotto winnerLotto = new Lotto(winners);
-        Lotto lotto = new Lotto(numbers);
-        assertThat(lotto.matchCountWith(winnerLotto)).isEqualTo(expected);
+    void 로또는_입력받은_로또와_일치하는_번호의_수를_알려준다(Lotto lotto, int expected) {
+        assertThat(lotto.matchCountWith(우승번호)).isEqualTo(expected);
     }
 
     static Stream<Arguments> provider_로또는_입력받은_로또와_일치하는_번호의_수를_알려준다() {
         return Stream.of(
-                Arguments.of(우승번호, 번호_6개_일치, 6),
-                Arguments.of(우승번호, 번호_5개_일치, 5),
-                Arguments.of(우승번호, 번호_4개_일치, 4),
-                Arguments.of(우승번호, 번호_3개_일치, 3),
-                Arguments.of(우승번호, 번호_2개_일치, 2),
-                Arguments.of(우승번호, 번호_1개_일치, 1),
-                Arguments.of(우승번호, 번호_0개_일치, 0)
+                Arguments.of(번호_6개_일치, 6),
+                Arguments.of(번호_5개_일치, 5),
+                Arguments.of(번호_4개_일치, 4),
+                Arguments.of(번호_3개_일치, 3),
+                Arguments.of(번호_2개_일치, 2),
+                Arguments.of(번호_1개_일치, 1),
+                Arguments.of(번호_0개_일치, 0)
         );
     }
 }
