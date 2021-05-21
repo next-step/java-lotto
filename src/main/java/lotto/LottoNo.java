2d63a9f875c7b50d00769458411979c8fb26b318
dohchoi91 <dohchoi91@gmail.com>
2021-05-21 11:52:45 +0900

    feat: 로또 번호 유효성 검사 추가

--- src/main/java/lotto/LottoNo.java
+++ src/main/java/lotto/LottoNo.java
@@ -3,9 +3,15 @@ package lotto;
 import java.util.Objects;
 
 public class LottoNo {
+	public static final int MIN = 1;
+	public static final int MAX = 45;
+
 	private final int number;
 
-	protected LottoNo(int number) {
+	public LottoNo(int number) {
+		if (number < MIN || number > MAX) {
+			throw new IllegalArgumentException("로또 번호는 1과 45사이의 수여야만 합니다.");
+		}
 		this.number = number;
 	}
 
@@ -22,5 +28,9 @@ public class LottoNo {
 	@Override
 	public int hashCode() {
 		return Objects.hash(number);
+	}
+
+	public int number() {
+		return this.number;
 	}
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,14 +4,34 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoTest {
 
 	@Test
+	@DisplayName("생성 테스트")
 	void create() {
 		assertThat(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6))).isNotNull();
 		assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(Arrays.asList(1, 2, 3, 4, 5)))
 			.withMessage("잘못된 번호 모음입니다.");
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("로또 번호 유효성 성공 테스트")
+	void noValidSuccess(int number) {
+		LottoNo lottoNo = new LottoNo(number);
+		assertThat(lottoNo.number()).isEqualTo(number);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 0, 46})
+	@DisplayName("로또 번호 유효성 실패 테스트")
+	void noValidFail(int number) {
+		assertThatIllegalArgumentException().isThrownBy(() -> new LottoNo(number))
+			.withMessage("로또 번호는 1과 45사이의 수여야만 합니다.");
 	}
 }
