ac8b3f65de774a9c6fe953300c7f9ceb28217f9b
lenkim <joenggyu0@gmail.com>
2021-05-22 00:08:39 +0900

    refactor(Main): 불필요한 사용자 UI 제거

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -6,13 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("문자열 덧셈 계산기 시작");
-		System.out.println("쉼표(,) 또는 콜롬(:) 외 구분자를 지정하고 싶다면 아래 입력해주시기 바랍니다.(Default: ',',':') ");
-		System.out.println("//;\\n1;2;3 구분자 입력하기");
-		String delimiter = sc.next();
-		if (delimiter.length() != 1){
-			throw new RuntimeException("구분자가 유효하지 않습니다.");
-		}
 		System.out.println("더하고자 하는 문자열을 입력해주세요.");
 		String userInput = sc.nextLine();
 		CalculatorModel model = new CalculatorModel(userInput);
