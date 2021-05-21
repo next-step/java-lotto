50cfb098719a128565e8245a44f0ae10518e0bce
itdar <thesockerrr@gmail.com>
2021-05-21 15:47:15 +0900

    feat: 로또 클래스 내부 구현
    - 객체 생성시 자동채번의 경우 바로 번호 생성되어 가짐 (1~ 45, 45개의 번호 중 랜덤으로 6개 뽑음)
    - 구매내역 시각화를 위한 toString() 메서드 오버라이드 (e.g. [1, 2, 3, 4, 5, 6] )

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,40 @@
 package lotto.domain;
 
-public class Lotto {
+import java.util.*;
 
+import static lotto.domain.LottoGameOptions.*;
+
+public class Lotto {
+    private List<Integer> numbers = new ArrayList<>();
+
+    public Lotto() {
+        this(true);
+    }
+
+    public Lotto(boolean auto) {
+        if (auto) {
+            generateNumbers();
+        }
+    }
+
+    private void generateNumbers() {
+        Stack<Integer> lottoNumbers = makeWholeLottoNumbers(LOTTO_NUMBERS_FROM, LOTTO_NUMBERS_TO);
+        Collections.shuffle(lottoNumbers);
+        while (numbers.size() != LOTTO_NUMBER_COUNT) {
+            numbers.add(lottoNumbers.pop());
+        }
+    }
+
+    private Stack<Integer> makeWholeLottoNumbers(int start, int end) {
+        Stack<Integer> numbers = new Stack<>();
+        for (int number = start; number <= end; ++number) {
+            numbers.push(number);
+        }
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
