c3a5671fa339ff04d3534c37ca7afb41c3c2cc0b
itdar <thesockerrr@gmail.com>
2021-05-21 14:54:16 +0900

    test: 입력받는 당첨번호의 포맷에 유효하지 않은 경우 테스트케이스 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -8,6 +8,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class LottoTest {
 
     @Test
+    void 입력된_유효하지않은_당첨번호_Test() {
+        assertThatThrownBy(() -> new LottoWinNumbers("1, 2, 3, 4, 5, b"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new LottoWinNumbers("1, 2, 3, 4, 5"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new LottoWinNumbers("1, 2, 3, 4, 5, 6, 7"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new LottoWinNumbers("1, 2, 59, 4, 5, 5"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new LottoWinNumbers("1, 2, 3, -1, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 구매개수만큼_로또_생성_Test() {
         LottoPurchaseBudget budget = new  LottoPurchaseBudget(5000);
         LottoGenerator generator = new LottoGenerator();
