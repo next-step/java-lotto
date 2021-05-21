267465c91ba1e5e7d764421eab053fbd6d857cf7
lkimilhol <lkimilhol@gmail.com>
2021-05-21 18:50:16 +0900

    feat(Money): Money 클래스 생성
    - 구매 금액을 포장하기 위한 Money 클래스 생성하고 음수 값이 되지 않는 방어 코드 추가

---
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -0,0 +1,5 @@
+package lotto.common;
+
+public class ErrorMessage {
+    public static final String MONEY_CANNOT_BE_NEGATIVE = "돈은 음수값이 될 수 없습니다.";
+}
---
+++ src/test/java/lotto/shop/MoneyTest.java
@@ -0,0 +1,33 @@
+package lotto.shop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import lotto.common.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @Test
+    @DisplayName("돈 생성")
+    void create() {
+        //given
+        Money money = new Money(14000);
+        //when
+        int amount = money.amount();
+        //then
+        assertThat(amount).isEqualTo(14000);
+    }
+
+    @Test
+    @DisplayName("돈이 음수가 되는 경우")
+    void method() {
+        //given
+        //when
+        //then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    Money money = new Money(-10);
+                }).withMessageContaining(ErrorMessage.MONEY_CANNOT_BE_NEGATIVE);
+    }
+}
