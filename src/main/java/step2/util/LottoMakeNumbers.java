661404460e1d91156348734d815abbfac5771ae5
leejun <p_illusion@naver.com>
2021-05-22 04:32:09 +0900

    feat: string -> 로또 숫자로 바꿔주는 툴 구현

---
+++ src/main/java/step2/util/LottoMakeNumbers.java
@@ -0,0 +1,48 @@
+package step2.util;
+
+import step2.exception.LottoNotNumberException;
+import step2.model.Lotto;
+import step2.model.LottoNumber;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+public class LottoMakeNumbers {
+
+    private LottoMakeNumbers() {
+    }
+
+    private static final String DELIMETER = ",";
+
+    private static final List<Integer> numbers = IntStream
+        .rangeClosed(LottoNumber.MIN, LottoNumber.MAX)
+        .boxed()
+        .collect(toList());
+
+    public static Lotto getRandomNumber() {
+        Collections.shuffle(numbers);
+
+        return numbers.stream()
+            .limit(Lotto.SIZE)
+            .map(LottoNumber::of)
+            .collect(collectingAndThen(toList(), Lotto::of));
+    }
+
+    public static List convertStringToNumbers(String inputWinnerNumbers) {
+        try {
+            return Stream.of(inputWinnerNumbers.split(DELIMETER))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNumber::of)
+                .collect(toList());
+
+        } catch (NumberFormatException e) {
+            throw new LottoNotNumberException();
+        }
+    }
+}
