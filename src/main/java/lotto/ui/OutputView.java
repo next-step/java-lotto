6caf3235c76a30e29f4af5b06d5d390fa53c004d
ted.kang <ted.kang@kakaocorp.com>
2021-05-22 02:30:34 +0900

    feat: OutputView 클래스 기능추가
    로또 당첨 결과와 손익률을 출력할 수 있는 기능 추가

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,10 +1,13 @@
 package lotto.ui;
 
+import lotto.dto.AnalysisSheet;
 import lotto.dto.OrderedInfos;
 
 public class OutputView {
 
   private static final String ORDERED_N_GAMES_FORMAT = "%d개를 구매했습니다.";
+  private static final String PRIZE_ANALYSIS = "당첨 통계";
+  private static final String SEPARATE_BAR = "---------";
 
   private OutputView() {}
 
@@ -14,4 +17,11 @@ public class OutputView {
   }
 
 
+  public static void displayAnalysisSheet(AnalysisSheet analysisSheet) {
+    System.out.println(PRIZE_ANALYSIS);
+    System.out.println(SEPARATE_BAR);
+    System.out.println(analysisSheet.toStringPrizeInfos());
+    System.out.println(analysisSheet.toStringYieldAnalysis());
+
+  }
 }
