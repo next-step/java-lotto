2e9e88e926e4fc1fce76b8fa7bbcb62e8a250109
sanggu <tkdrn8578@naver.com>
2021-05-21 09:23:08 +0900

    refactor: test 를 left, right, expectedAnswer를 입력받아 ParameterizedTest로 변경

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,23 +1,21 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"5, 10, 15", "0, 10, 10", "0, 0, 0"})
     @DisplayName("입력받은 숫자들의 합을 구한다.")
-    void sum() {
-        //given
-        int left = 5;
-        int right = 10;
-
+    void sum(int left, int right, int expectedAnswer) {
         //when
         int sum = Calculator.sum(left, right);
 
         //then
-        assertThat(sum).isEqualTo(15);
+        assertThat(sum).isEqualTo(expectedAnswer);
     }
 }
