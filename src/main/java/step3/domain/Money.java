aab8ce3802ae30d85956a23a30154daabad721e8
dhmin5693 <dhmin5693@naver.com>
2021-05-22 02:21:37 +0900

    style: 메소드 순서 변경

--- src/main/java/step3/domain/Money.java
+++ src/main/java/step3/domain/Money.java
@@ -9,17 +9,17 @@ public class Money {
         this.amount = amount;
     }
 
-    private void validateUnit(int amount) {
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("금액은 1,000원 단위로 입력해주세요.");
-        }
-    }
-
     public static Money of(int amount) {
         return new Money(amount);
     }
 
     public int getBuyableLottoSize() {
         return amount / 1000;
+    }
+
+    private void validateUnit(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("금액은 1,000원 단위로 입력해주세요.");
+        }
     }
 }
