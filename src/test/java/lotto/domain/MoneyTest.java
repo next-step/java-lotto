fc75a406e5bba04b419f154f5958daffd64e3768
HyungjuJang <dacapolife87@gmail.com>
2021-05-22 01:23:47 +0900

    test : 클래스의 목적을 변경에 따른 테스트케이스변경 - 입력받은 금액은 문자열이 올수없는 조건을 테스트

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MoneyTest {
+
+    @DisplayName("입력받은돈은 양수만 가능하다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"오백원", "천원"})
+    public void invalidAmountMoneyTest(String money) {
+        assertThatThrownBy(() -> Money.create(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
