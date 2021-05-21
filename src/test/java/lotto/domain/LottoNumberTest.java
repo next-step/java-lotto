85fd4f80b6e050efa4ca910d22da3eff12ff35bb
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 13:34:14 +0900

    test : 로또 번호의 순서비교 테스트

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +27,16 @@ class LottoNumberTest {
         assertThatThrownBy(() -> LottoNumber.create(number))
                 .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @DisplayName("로또 번호의 순서비교 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,-1","2,1,1","1,1,0"}, delimiter = ',')
+    public void compareTest(int number1, int number2, int expectedNumber) {
+        LottoNumber lottoNumber1 = LottoNumber.create(number1);
+        LottoNumber lottoNumber2 = LottoNumber.create(number2);
+
+        assertThat(lottoNumber1.compareTo(lottoNumber2)).isEqualTo(expectedNumber);
     }
 
 }
