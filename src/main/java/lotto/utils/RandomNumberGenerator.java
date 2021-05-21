02ee76d549103c82e6787e0acade54bd33d0f855
HyungjuJang <dacapolife87@gmail.com>
2021-05-22 02:31:04 +0900

    feat : 랜덤숫자 6개를 생성하는 기능 추가

---
+++ src/main/java/lotto/utils/RandomNumberGenerator.java
@@ -0,0 +1,22 @@
+package lotto.utils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomNumberGenerator {
+
+    public static List<String> randomSixNumbers() {
+        List<String> availableNumbers = availableAllNumbers();
+        Collections.shuffle(availableNumbers);
+
+        return availableNumbers.subList(0, 6);
+    }
+
+    private static List<String> availableAllNumbers() {
+        return IntStream.range(1, 45)
+                .mapToObj(number -> String.valueOf(number))
+                .collect(Collectors.toList());
+    }
+}
