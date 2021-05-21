2a3b2883da5ad553bf2155564e92ae078d3ecb29
HoonHee Jeong <jhh992000@gmail.com>
2021-05-21 17:02:33 +0900

    refactor: 로또번호 객체에 equals, hashCode, toString 재정의

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
 	public static final int MIN = 1;
 	public static final int MAX = 45;
@@ -18,5 +20,23 @@ public class LottoNumber {
 
 	public boolean matchNumber(int prizeNumber) {
 		return this.number == prizeNumber;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		LottoNumber that = (LottoNumber) o;
+		return number == that.number;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(number);
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(this.number);
 	}
 }
