46c92010070a136fc8103f7cd2c490607669d006
김석진 <7271kim@naver.com>
2021-05-21 22:28:20 +0900

    feat : 기능 목록 1 구현
    - 숫자는 1부터 45까지이며, 잘못된 값이 들어 올 시 IllegalArgumentException을 발생시킨다.
    
    
    [수정사항]
    - LottoNumber.java : 원시값 포장 객체
    - LottoNumberTest.java : validationTest 추가

---
+++ src/main/java/step3/model/LottoNumber.java
@@ -0,0 +1,26 @@
+package step3.model;
+
+public final class LottoNumber {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final String NOT_IN_RANGE = "1~45까지의 숫자를 입력하세요";
+
+    private int number;
+
+    public LottoNumber(final int number) {
+        validation(number);
+        this.number = number;
+    }
+
+    private void validation(int number) {
+        if (!isAvailableRange(number)) {
+            throw new IllegalArgumentException(NOT_IN_RANGE);
+        }
+    }
+
+    private boolean isAvailableRange(int number) {
+        return (number >= MIN_NUMBER) && (number <= MAX_NUMBER);
+    }
+
+}
