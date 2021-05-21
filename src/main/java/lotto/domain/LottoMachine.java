b3e31b5e3ad31c78e509e0e82d1c813736365962
ted.kang <ted.kang@kakaocorp.com>
2021-05-22 02:28:53 +0900

    feat: LottoMachine 클래스 작성
    * 구매 금액을 받아 로또를 생성하고, 당첨 번호를 받아 만들어진 당첨 내역을 맞춰 볼 수 있는 서비스 클래스
    * 만들어 진 로또에 대해 출력용 DTO를 반환할 수 있다.
    * 당첨 내역과 수익률을 출력하기위한 DTO를 반환할 수 있다.

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,79 @@
+package lotto.domain;
+
+import lotto.dto.AnalysisSheet;
+import lotto.dto.OrderSheet;
+import lotto.dto.OrderedInfos;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+public class LottoMachine {
+
+  private static final int PRICE_PER_GAME = 1000;
+  private static final String INPUT_SHOULD_NOT_LESS_THAN_PRICE_PER_GAME = "최소 구입 금액은 %d원 이상입니다.";
+
+  private final OrderSheet orderSheet;
+  private final LottoGames orderedGames;
+
+  public LottoMachine(OrderSheet orderSheet) {
+    this.orderSheet = orderSheet;
+    this.orderedGames = generateNewOrderedAutoGames(orderSheet.getPurchasingAmount());
+  }
+
+  //For Test Code
+  private LottoMachine(OrderSheet orderSheet, LottoGames lottoGames) {
+    this.orderSheet = orderSheet;
+    this.orderedGames = lottoGames;
+  }
+
+  //For Test Code
+  static LottoMachine staticFactoryMethodForTestCode(long testPurchasingAmount, List<LottoGame> testGames) {
+    return new LottoMachine(new OrderSheet(testPurchasingAmount), new LottoGames(testGames));
+  }
+
+  public OrderedInfos peekOrderedGames() {
+    return orderedGames.peekGameInfos();
+  }
+
+  public AnalysisSheet confirmPrizeOrderedLottos(List<Integer> inputWinningNumbers) {
+    LottoGame winningGame = createLottoGame(inputWinningNumbers);
+    TotalRankings totalRankings = orderedGames.matchRankings(winningGame);
+    return new AnalysisSheet(totalRankings, orderSheet.getPurchasingAmount());
+  }
+
+  private LottoGames generateNewOrderedAutoGames(long purchasingAmount) {
+    List<LottoGame> lottGames = LongStream.range(0, computeNumberOfGames(purchasingAmount))
+        .mapToObj(longValue -> new LottoGame(LottoNumbersFactory.generateNewNumbers()))
+        .collect(Collectors.toList());
+    return new LottoGames(lottGames);
+  }
+
+  private long computeNumberOfGames(long purchasingAmount) {
+    if (purchasingAmount < PRICE_PER_GAME) {
+      throw new IllegalArgumentException(String.format(INPUT_SHOULD_NOT_LESS_THAN_PRICE_PER_GAME, PRICE_PER_GAME));
+    }
+    return purchasingAmount / PRICE_PER_GAME;
+  }
+
+  private LottoGame createLottoGame(List<Integer> inputNumbers) {
+    List<LottoNumber> lottoNumbers = inputNumbers.stream()
+                                    .map(LottoNumber::new)
+                                    .collect(Collectors.toList());
+    return new LottoGame(new LottoNumbers(lottoNumbers));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    LottoMachine that = (LottoMachine) o;
+    return orderSheet.equals(that.orderSheet) && orderedGames.equals(that.orderedGames);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(orderSheet, orderedGames);
+  }
+}
---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,56 @@
+package lotto.domain;
+
+import lotto.dto.AnalysisSheet;
+import lotto.dto.OrderedInfos;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static lotto.util.TestUtil.createLottoGameFromLottoNumbers;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoMachineTest {
+
+  private List<LottoGame> givenGames;
+  private long givenPurchasedAmount = 2000;
+
+  @BeforeEach
+  void setUp() {
+    givenGames = Lists.newArrayList(createLottoGameFromLottoNumbers("1,2,3,4,5,6"), createLottoGameFromLottoNumbers("1,2,3,10,20,30"));
+  }
+
+  @DisplayName("객체 생성 & 동등성 테스트")
+  @Test
+  void constructionAndEqualityTest() {
+    assertThat(LottoMachine.staticFactoryMethodForTestCode(givenPurchasedAmount, givenGames))
+        .isEqualTo(LottoMachine.staticFactoryMethodForTestCode(givenPurchasedAmount, givenGames));
+  }
+
+  @DisplayName("생성된 로또 게임들의 출력을 위한 DTO 객체를 반환 한다.")
+  @Test
+  void peekOrderedGamesTest() {
+    assertThat(LottoMachine.staticFactoryMethodForTestCode(givenPurchasedAmount, givenGames).peekOrderedGames())
+        .isEqualTo(new OrderedInfos(givenGames));
+  }
+
+  @DisplayName("당첨 번호를 맞춰 본 후 당첨 내역과 손익률 출력을 위한 DTO 객체를 반환한다.")
+  @Test
+  void confirmPrizeOrderedLottosTest() {
+    //given
+    List<Integer> givenWinningNumbers = IntStream.range(1, 7)
+        .boxed()
+        .collect(Collectors.toList());
+
+    TotalRankings expectTotalRanking = new TotalRankings(Lists.newArrayList(LottoRanking.FIRST, LottoRanking.FOURTH));
+
+    //when & then
+    assertThat(LottoMachine.staticFactoryMethodForTestCode(givenPurchasedAmount, givenGames).confirmPrizeOrderedLottos(givenWinningNumbers))
+        .isEqualTo(new AnalysisSheet(expectTotalRanking, givenPurchasedAmount));
+  }
+
+}
