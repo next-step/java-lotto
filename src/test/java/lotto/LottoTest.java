a2bc441636e14734f716700bd821efc90e286232
jess <jess.l@kakaocorp.com>
2021-05-22 05:47:13 +0900

    feat(Lotto): 로또는 1-45까지의 숫자 중에서 6개를 골라 발행한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,21 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
 
     @Test
-    void 로또_한_장의_가격은_1000원이다(){
+    void 로또_한_장의_가격은_1000원이다() {
         Lotto lotto = new Lotto();
         assertThat(lotto.PRICE).isEqualTo(1000);
+    }
+
+    @Test
+    void 로또는_1부터_45까지의_숫자_중에서_6개를_골라_발행한다() {
+        Lotto lotto = new Lotto();
+        assertThat(lotto.numbers).allMatch(num -> num <= 45)
+                .allMatch(num -> num >= 1);
     }
 }
