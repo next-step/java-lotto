aa2563311ed58a6224787cd5d0f9f77838ed20bf
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-21 22:40:58 +0900

    feat : validation 로직 작성

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -24,7 +24,21 @@ public class LottoNumbers implements Iterable<LottoNumber> {
     }
 
     public LottoNumbers(List<LottoNumber> lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validateLottoNumbers(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers == null) {
+            throw new IllegalArgumentException("로또번호를 입력해 주세요.");
+        }
+        if (lottoNumbers.size() != MAX_LOTTO_COUNT) {
+            throw new IllegalArgumentException("로또번호는 " +  MAX_LOTTO_COUNT + " 개의 숫자로 이루어져야 합니다.");
+        }
+        int distinctCount = (int) lottoNumbers.stream().distinct().count();
+        if (distinctCount < MAX_LOTTO_COUNT) {
+            throw new IllegalArgumentException("중복된 번호가 존재합니다.");
+        }
     }
 
     @Override
