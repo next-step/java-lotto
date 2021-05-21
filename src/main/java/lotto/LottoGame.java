501f6f9901cdd10019f38264c4c083d61a939369
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 13:16:32 +0900

    feat : 로또게임 전체 비즈니스로직 개발

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,57 @@
+package lotto;
+
+import lotto.domain.*;
+import lotto.utils.LottoLotteryUtils;
+import lotto.utils.LottoNumberGenerator;
+import lotto.view.InputView;
+import lotto.view.PrintView;
+
+import java.util.List;
+
+public class LottoGame {
+
+    private final InputView inputView = new InputView();
+    private final PrintView printView = new PrintView();
+
+    public static void main(String[] args) {
+        new LottoGame().start();
+    }
+
+    private void start() {
+        LottoWallet lottoWallet = buyLotto();
+
+        lottery(lottoWallet.lottoTickets());
+    }
+
+    private LottoWallet buyLotto() {
+        LottoWallet lottoWallet = buyLottoTickets();
+        printView.showStatusBuyTicket(lottoWallet);
+        return lottoWallet;
+    }
+
+    private void lottery(List<LottoTicket> lottoTickets) {
+        WinnerNumbers winnerNumbers = generatorWinnerNumber();
+
+        WinnerStatistics winnerStatistics = LottoLotteryUtils.lotteryThisWeek(lottoTickets, winnerNumbers);
+
+        printView.showLottoResult(winnerStatistics);
+    }
+
+    private WinnerNumbers generatorWinnerNumber() {
+        List<String> inputWinnerNumbers = inputView.inputWinnerNumber();
+
+        List<LottoNumber> lottoNumbers = LottoNumberGenerator.manualGenerator(inputWinnerNumbers);
+
+        WinnerNumbers winnerNumbers = WinnerNumbers.create(lottoNumbers);
+
+        return winnerNumbers;
+    }
+
+    private LottoWallet buyLottoTickets() {
+        AmountMoney amountMoney = inputView.amountMoneyOfBuyTicket();
+
+        LottoWallet lottoWallet = new LottoWallet(amountMoney.couldBuyTicketAmount());
+        lottoWallet.buyLotto();
+        return lottoWallet;
+    }
+}
