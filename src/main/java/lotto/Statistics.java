71f90ccb5c1694b6ce431b5c76c71f9f9e515d3d
dohchoi91 <dohchoi91@gmail.com>
2021-05-21 23:03:27 +0900

    feat: 당첨번호 추가

--- src/main/java/lotto/Statistics.java
+++ src/main/java/lotto/Statistics.java
@@ -4,13 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Statistics {
-	private final List<Lotto> lottos = new ArrayList<>();
 
+	private final List<Lotto> lottos = new ArrayList<>();
+	private final Lotto prizeLotto;
+
+	public Statistics(Lotto prizeLotto) {
+		this.prizeLotto = prizeLotto;
+	}
 	public void addLotto(Lotto lotto) {
 		lottos.add(lotto);
 	}
 
 	public boolean contains(Lotto lotto) {
 		return lottos.contains(lotto);
+	}
+
+	public List<Integer> prizeLottoNumbers() {
+		return  prizeLotto.numbers();
 	}
 }
