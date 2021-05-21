ea983abc989ff9a5dd95a40b60360ec54212911f
HyungjuJang <dacapolife87@gmail.com>
2021-05-22 01:26:01 +0900

    refactor : 금액을 입력받아 구입가능한 로또의 갯수를 판단하여 객체생성으로 변경

--- src/main/java/lotto/domain/LottoWallet.java
+++ src/main/java/lotto/domain/LottoWallet.java
@@ -8,11 +8,28 @@ import java.util.List;
 
 public class LottoWallet {
 
+    private static final int LOTTO_TICKET_PRICE = 1000;
+
     private int countOfLottoTicket;
     private List<LottoTicket> lottoTickets = new ArrayList<>();
 
-    public LottoWallet(int countOfLottoTicket) {
+    private LottoWallet(int countOfLottoTicket) {
         this.countOfLottoTicket = countOfLottoTicket;
+    }
+
+    public static LottoWallet create(Money money) {
+        int userMoney = money.value();
+
+        if (userMoney < LOTTO_TICKET_PRICE) {
+            throw new IllegalArgumentException("최소 구입금액은 1000원입니다.");
+        }
+
+        int couldBuyTicketAmount = calculateCouldBuyTicketAmount(userMoney);
+        return new LottoWallet(couldBuyTicketAmount);
+    }
+
+    private static int calculateCouldBuyTicketAmount(int money) {
+        return money / LOTTO_TICKET_PRICE;
     }
 
     public void buyLotto() {
