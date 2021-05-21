834275ee5f4866787b18d08dd67080a469beacd8
ohgillwhan <soora_genius@naver.com>
2021-05-21 23:27:38 +0900

    test : 2등에 대한 새로운 요구사항 추가로 인한 테스트코드 변경

--- src/test/java/lotto/domain/rank/LottoRankTest.java
+++ src/test/java/lotto/domain/rank/LottoRankTest.java
@@ -9,16 +9,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoRankTest {
     @ParameterizedTest
     @CsvSource(value = {
-            "1,MISS",
-            "2,MISS",
-            "3,FIFTH",
-            "4,FOURTH",
-            "5,THIRD",
-            "6,FIRST"
+            "1,false,MISS",
+            "2,false,MISS",
+            "3,false,FIFTH",
+            "4,false,FOURTH",
+            "5,false,THIRD",
+            "5,true,SECOND",
+            "6,false,FIRST"
     })
     @DisplayName("맞은 갯수에 맞게 로또랭킹을 가져와야 한다")
-    public void 맞은_갯수에_맞게_로또랭킹을_가져와야_한다(int countOfMatched, LottoRank except) {
-        assertThat(LottoRank.valueOf(countOfMatched, false))
+    public void 맞은_갯수에_맞게_로또랭킹을_가져와야_한다(int countOfMatched, boolean bonusMatched, LottoRank except) {
+        assertThat(LottoRank.valueOf(countOfMatched, bonusMatched))
                 .isEqualTo(except);
     }
 }
