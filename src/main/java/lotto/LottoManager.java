7e19d90f24e3f887ce22f58634d10e33e64a20d9
tinyhhj <krwsim@gmail.com>
2021-05-22 00:51:34 +0900

    refactor: 메소드 분리

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -6,63 +6,67 @@ import lotto.domain.LottoMachine;
 import lotto.view.UserView;
 
 public class LottoManager {
-    private static boolean validMoney;
     private static boolean validLottoNumbers;
-    public static void main(String[] args) {
-        LottoMachine machine = new LottoMachine();
-        UserView view = new UserView();
-        LottoGame winGame;
-        int money = -1;
+    private static int money;
+    private static LottoMachine machine = new LottoMachine();
+    private static UserView view = new UserView();
+    private static LottoGame winGame;
 
+    public static void main(String[] args) {
         view.println("구입금을 입력해 주세요.");
-        do {
-            money = parseMoney(view);
-        } while(!validMoney);
-        int lottoGameCount = money / LottoGame.PRICE;
+        parseMoneyUntilOverZero();
+        int lottoGameCount = getLottoGameCount();
         view.println(String.format("%d개를 구매했습니다.",lottoGameCount));
 
-        LottoGames lottoGames = machine.createLottoGames(money);
-
-        for (LottoGame game : lottoGames) {
-            view.println(game);
-        }
+        createAndPrintLottoGame();
 
         view.enter();
         view.println("지난 주 당첨 번호를 입력해 주세요.");
-        do {
-            winGame = parseLottoNumbers(view);
-        } while(!validLottoNumbers);
+        parseLottoNumbersUntilValid();
         view.enter();
 
         view.println(machine.calculateStatics(winGame));
     }
 
+    private static void parseLottoNumbersUntilValid() {
+        do {
+            winGame = parseLottoNumbers(view);
+        } while(winGame == null);
+    }
+
+    private static void parseMoneyUntilOverZero() {
+        do {
+            money = parseMoney(view);
+        } while(money <= 0);
+    }
+
+    private static void createAndPrintLottoGame() {
+        LottoGames lottoGames = machine.createLottoGames(money);
+        for (LottoGame game : lottoGames) {
+            view.println(game);
+        }
+    }
+
+    private static int getLottoGameCount() {
+        return money / LottoGame.PRICE;
+    }
+
     private static LottoGame parseLottoNumbers(UserView view) {
         try {
-            LottoGame winGame = new LottoGame(view.nextIntArray());
-            validLottoNumbers = true;
-            return winGame;
+            return new LottoGame(view.nextIntArray());
         } catch (Exception e) {
             view.println("잘못된 입력입니다. 다시 입력해주세요.");
         }
-        return new LottoGame();
+        return null;
     }
 
     private static int parseMoney(UserView view) {
         try {
-            int money = Integer.parseInt(view.nextLine());
-            setValidMoney(money);
-            return money;
+            return Integer.parseInt(view.nextLine());
         } catch (Exception e) {
             view.println("잘못된 입력입니다. 다시 입력해주세요");
         }
-
         return 0;
     }
 
-    private static void setValidMoney(int money) {
-        if( money > 0) {
-            validMoney = true;
-        }
-    }
 }
