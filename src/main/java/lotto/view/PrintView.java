673ccf6c0446850f6ac2a7eb06018d8e58878a05
HyungjuJang <dacapolife87@gmail.com>
2021-05-21 13:16:01 +0900

    feat : 출력화면 기능구현 - 로또 구입결과 출력 - 당첨통계 출력

--- src/main/java/lotto/view/PrintView.java
+++ src/main/java/lotto/view/PrintView.java
@@ -1,25 +1,34 @@
 package lotto.view;
 
-import lotto.domain.LottoNumber;
-import lotto.domain.LottoTicket;
-import lotto.domain.LottoWallet;
+import lotto.domain.*;
 
 import java.util.List;
 import java.util.StringJoiner;
 
 public class PrintView {
 
+    private static final String MESSAGE_BUY_COUNT = "%s개를 구매했습니다.\n";
+    private static final String MESSAGE_MATCH = "%s개 일치 (%s원)- %s개\n";
+    private static final String MESSAGE_INCOME_RATE = "총 수익률은 %s입니다.";
+    private static final String MESSAGE_LOSS_DESC = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+
+    private static final String MESSAGE_HEAD_LOTTO_STATISTICS = "당첨통계";
+    private static final String MESSAGE_HEAD_LINE_SEPARATOR = "-----------";
+
+    private static final String LOTTO_TICKET_DELIMITER = ",";
+    private static final String LOTTO_TICKET_PREFIX = "[";
+    private static final String LOTTO_TICKET_SUFFIX = "]";
+
     public void showStatusBuyTicket(LottoWallet lottoWallet) {
         List<LottoTicket> lottoTickets = lottoWallet.lottoTickets();
-        System.out.println(lottoWallet.ticketCount() +"개를 구매했습니다.");
-
+        System.out.printf(MESSAGE_BUY_COUNT, lottoWallet.ticketCount());
         for (LottoTicket lottoTicket : lottoTickets) {
             printLottoTicket(lottoTicket);
         }
     }
 
     private void printLottoTicket(LottoTicket lottoTicket) {
-        StringJoiner lottoNumberJoiner = new StringJoiner(",", "[", "]");
+        StringJoiner lottoNumberJoiner = new StringJoiner(LOTTO_TICKET_DELIMITER, LOTTO_TICKET_PREFIX, LOTTO_TICKET_SUFFIX);
 
         for (LottoNumber lottoNumber : lottoTicket.numbers()) {
             int number = lottoNumber.value();
@@ -29,10 +38,37 @@ public class PrintView {
         System.out.println(lottoNumberJoiner.toString());
     }
 
-    public void showLottoResult() {
-        System.out.println("당첨통계");
-        System.out.println("---------------");
-        //결과화면
+    public void showLottoResult(WinnerStatistics winnerStatistics) {
+        showResultHeadLine();
+        showResultBody(winnerStatistics);
+    }
+
+    private void showResultBody(WinnerStatistics winnerStatistics) {
+        printBodyLine(LottoRank.FOURTH, winnerStatistics);
+        printBodyLine(LottoRank.THIRD, winnerStatistics);
+        printBodyLine(LottoRank.SECOND, winnerStatistics);
+        printBodyLine(LottoRank.FIRST, winnerStatistics);
+
+        printRateLine(winnerStatistics);
+    }
+
+    private void printRateLine(WinnerStatistics winnerStatistics) {
+        String incomeRateText = String.format(MESSAGE_INCOME_RATE, winnerStatistics.incomeRate());
+        StringBuilder sb = new StringBuilder(incomeRateText);
+        if(winnerStatistics.isLoss()) {
+            sb.append(MESSAGE_LOSS_DESC);
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private void printBodyLine(LottoRank lottoRank, WinnerStatistics winnerStatistics) {
+        System.out.printf(MESSAGE_MATCH, lottoRank.matchCount(), lottoRank.reward(), winnerStatistics.ticketCountFindByLottoRank(lottoRank));
+    }
+
+    private void showResultHeadLine() {
+        System.out.println(MESSAGE_HEAD_LOTTO_STATISTICS);
+        System.out.println(MESSAGE_HEAD_LINE_SEPARATOR);
     }
 
 }
