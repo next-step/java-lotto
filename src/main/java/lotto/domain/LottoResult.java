542967badc75468621c46eb41600ecad229591a4
itdar <thesockerrr@gmail.com>
2021-05-22 04:22:19 +0900

    refactor: 로또 결과 enum클래스에 수익금 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,27 +1,29 @@
 package lotto.domain;
 
 public enum LottoResult {
-    MATCH_6("1", 6),
-    MATCH_5("2", 5),
-    MATCH_4("3", 4),
-    MATCH_3("4", 3),
-    MATCH_2("5", 2),
-    MATCH_1("0", 1),
-    MATCH_0("0", 0);
+    MATCH_6("1", 6, 2000000000),
+    MATCH_5("2", 5, 150000),
+    MATCH_4("3", 4, 50000),
+    MATCH_3("4", 3, 5000),
+    MATCH_2("0", 2, 0),
+    MATCH_1("0", 1, 0),
+    MATCH_0("0", 0, 0);
 
     private final String grade;
     private final int matchCount;
+    private final int profit;
 
-    LottoResult(String grade, int matchCount) {
+    LottoResult(String grade, int matchCount, int profit) {
         this.grade = grade;
         this.matchCount = matchCount;
-    }
-
-    public String grade() {
-        return grade;
+        this.profit = profit;
     }
 
     public int matchCount() {
         return matchCount;
+    }
+
+    public int profit() {
+        return profit;
     }
 }
