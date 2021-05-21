035de1eccb067ebde6b933930e1bdf80273b5bc4
sanggu <tkdrn8578@naver.com>
2021-05-21 09:33:36 +0900

    feat: 사용자에게 문자열을 입력받아 합을 구하는 테스트를 기존 테스트가 깨지지 않도록 점진적 리펙토링을 적용해 테스트 및 로직 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,32 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
+
+    private final List<Integer> values;
+
+    public Calculator(String input) {
+        String[] split = input.split(",");
+
+        List<Integer> values = new ArrayList<>();
+        for (String value : split) {
+            values.add(Integer.parseInt(value));
+        }
+
+        this.values = values;
+    }
+
+    public int sum2() {
+        int sum = 0;
+
+        for (int value : values) {
+            sum += value;
+        }
+
+        return sum;
+    }
 
     public static int sum(int left, int right) {
         return left + right;
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,11 +12,26 @@ public class CalculatorTest {
     @ParameterizedTest
     @CsvSource({"5, 10, 15", "0, 10, 10", "0, 0, 0"})
     @DisplayName("입력받은 숫자들의 합을 구한다.")
-    void sum(int left, int right, int expectedAnswer) {
+    void sum_test(int left, int right, int expectedAnswer) {
         //when
         int sum = Calculator.sum(left, right);
 
         //then
         assertThat(sum).isEqualTo(expectedAnswer);
     }
+
+    @Test
+    @DisplayName("사용자에게 쉼표이 포함된 숫자를 입력받아 숫자를 더한다.")
+    void inputAndSum_test() {
+        //given
+        String input = "5,10";
+        Calculator calculator = new Calculator(input);
+
+        //when
+        int sum = calculator.sum2();
+
+        //then
+        assertThat(sum).isEqualTo(15);
+    }
+
 }
