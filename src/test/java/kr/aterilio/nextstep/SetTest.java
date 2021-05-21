f46827e53451848cd768f5f3bb5a5865d6c79881
aterilio <lina@aterilio.kr>
2021-05-22 00:27:10 +0900

    feat: step1 - 학습테스트

---
+++ src/test/java/kr/aterilio/nextstep/SetTest.java
@@ -0,0 +1,47 @@
+package kr.aterilio.nextstep;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size numbers -> expected 3")
+    @Test
+    public void testReq1_size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("contain numbers using ValueSource -> expected 1:true, 2:true, 3:true")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
+    public void testReq2_containValueSource(int target) {
+        assertThat(numbers.contains(target)).isTrue();
+    }
+
+    @DisplayName("contain numbers using CsvSource -> expected 1:true, 2:true, 3:true, 4:false, 5:false")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    public void testReq3_containCsvSource(int target, boolean isContain) {
+        assertThat(numbers.contains(target)).isEqualTo(isContain);
+    }
+}
---
+++ src/test/java/kr/aterilio/nextstep/StringTest.java
@@ -0,0 +1,58 @@
+package kr.aterilio.nextstep;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    public static final String SPLIT_TOKEN = ",";
+
+    @DisplayName("split \"1,2\" -> expected {\"1\", \"2\"}")
+    @Test
+    public void testReq1_splitTwoElement() {
+        final String input = "1,2";
+        final String[] result = input.split(SPLIT_TOKEN);
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @DisplayName("split \"1\" -> expected {\"1\"}")
+    @Test
+    public void testReq1_splitOneElement() {
+        final String input = "1";
+        final String[] result = input.split(SPLIT_TOKEN);
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1");
+    }
+
+    @DisplayName("substring \"(1,2)\" -> expected \"1,2\"")
+    @Test
+    public void testReq2_substring() {
+        final String input = "(1,2)";
+        final String result = input.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+
+    }
+
+    @DisplayName("charAt \"abc\" -> expected 0:'a', 1:'b', 2:'c'")
+    @Test
+    public void testReq3_charAt() {
+        final String input = "abc";
+        assertThat(input.charAt(0)).isEqualTo('a');
+        assertThat(input.charAt(1)).isEqualTo('b');
+        assertThat(input.charAt(2)).isEqualTo('c');
+    }
+
+    @DisplayName("charAt \"abc\" -> expected 4:throw IndexOutOfBoundsException")
+    @Test
+    public void testReq3_charAtThrowOutOfBoundException() {
+        final String input = "abc";
+        assertThatThrownBy(() -> {
+            input.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+        .hasMessageContaining("out of range: 4");
+    }
+}
