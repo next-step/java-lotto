702f0a7081834aa1ee7ba1213e8c8731c3c326ec
Donghui-Lee <ehdgml3206@naver.com>
2021-05-22 17:10:10 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Money {
     private static final int LOTTO_PRICE = 1000;
 
     private final int money;
 
     public Money(int money) {
+        validate(money);
         this.money = money;
     }
 
@@ -13,7 +16,28 @@ public class Money {
         this(Integer.parseInt(money));
     }
 
+    private void validate(int money) {
+        if ((money < 0) || (money % LOTTO_PRICE != 0)) {
+            throw new IllegalArgumentException("로또 구매 최저 금액은 " + LOTTO_PRICE + "원 입니다.");
+        }
+    }
+
     public int countOfLotto() {
         return this.money / LOTTO_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Money money1 = (Money)o;
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
     }
 }
