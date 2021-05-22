e202692386883778037511e8710a77adc8e3e979
fDevJc <fDevJc@gmail.com>
2021-05-22 23:53:53 +0900

    test: 테스트케이스 수정

--- src/test/java/lotto/domains/LottosTest.java
+++ src/test/java/lotto/domains/LottosTest.java
@@ -16,20 +16,9 @@ public class LottosTest {
     
     @Test
     void 통계값_3개일치_가져오기_테스트() {
-
         LottoNumbers winningLottoNumbers = new LottoNumbers(Arrays.asList(1,2,3,4,5,6));
-
-        Lotto lotto1 = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,4,5,6)));
-        Lotto lotto2 = new Lotto(new LottoNumbers(Arrays.asList(11,12,13,14,15,16)));
-        Lotto lotto3 = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,14,15,16)));
-
-        Lottos lottos = new Lottos(Arrays.asList(lotto1,lotto2,lotto3));
-
+        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,14,15,16)));
+        Lottos lottos = new Lottos(Arrays.asList(lotto));
         assertThat(lottos.getStatistics(winningLottoNumbers).count(3)).isEqualTo(1);
-    }
-
-    @Test
-    void 당첨금_합_구하기() {
-
     }
 }
