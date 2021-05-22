f38608630ef7d60dad2e78e2655a358147bc4aa0
younghoon <bblnotbb@yahho.com>
2021-05-22 22:06:43 +0900

    refactor : add test code for each class

--- src/main/java/StringAddCalculator/Input.java
+++ src/main/java/StringAddCalculator/Input.java
@@ -1,24 +1,25 @@
 package StringAddCalculator;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Input {
 	private static final String CUSTOM_DELIMITER_REGEX = "//(.*)\n(.*)";
-	private static final String[] ZERO = new String[] {"0"};
+	private static final String ZERO = "0";
 	private static Pattern customDelimiterPattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
 
 	private String input;
 
 	public Input(String input) {
 		this.input = input;
+
+		if (isEmptyOrNull(input)) {
+			this.input = ZERO;
+		}
 	}
 
 	public String[] getStringNumbers() {
-		if (isEmptyOrNull(input)) {
-			return ZERO;
-		}
-
 		Matcher customDelimiterMatcher = getCustomDelimiterMatcher(input);
 
 		if (customDelimiterMatcher.find()) {
@@ -50,5 +51,20 @@ public class Input {
 
 	private Matcher getCustomDelimiterMatcher(String input) {
 		return customDelimiterPattern.matcher(input);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Input input1 = (Input)o;
+		return input.equals(input1.input);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(input);
 	}
 }
