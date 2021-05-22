143c8d8575745b3b948d8be695c06248cdcec406
accidentlywoo <accidentlywoo@gmail.com>
2021-05-22 21:39:24 +0900

    feat : number 도메인 equals override, 기능정의
    exception message 추출
    음수 예외처리
    숫자아닌 값도 예외처리

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -1,9 +1,9 @@
 package domain;
 
-public class Number {
-	private static String NUMBER_FORMAT_EXCEPTION = "숫자를 입력해주세요.";
+import util.ExceptionMessage;
 
-	private Double number;
+public class Number {
+	private Long number;
 
 	public Number () {
 		this("");
@@ -11,18 +11,42 @@ public class Number {
 
 	public Number (String number) {
 		if (number.equals("")) {
-			this.number = 0D;
+			this.number = 0L;
 			return;
 		}
 
 		try {
-			this.number = Double.valueOf(number);
+			checkMinus(number);
+			this.number = Long.valueOf(number);
+
 		} catch (NumberFormatException e) {
-			throw new NumberFormatException(NUMBER_FORMAT_EXCEPTION);
+			throw new NumberFormatException(ExceptionMessage.NUMBER_FORMAT_EXCEPTION);
 		}
+	}
+
+	@Override
+	public boolean equals (Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Number number1 = (Number) o;
+
+		return number != null ? number.equals(number1.number) : number1.number == null;
+	}
+
+	@Override
+	public int hashCode () {
+		return number != null ? number.hashCode() : 0;
 	}
 
 	public Integer number () {
 		return number.intValue();
+	}
+
+	private boolean checkMinus(String number){
+		if (Integer.parseInt(number) < 0) {
+			throw new NumberFormatException();
+		}
+		return true;
 	}
 }
---
+++ src/main/java/util/ExceptionMessage.java
@@ -0,0 +1,5 @@
+package util;
+
+public class ExceptionMessage {
+	public static String NUMBER_FORMAT_EXCEPTION = "숫자를 입력해주세요.";
+}
