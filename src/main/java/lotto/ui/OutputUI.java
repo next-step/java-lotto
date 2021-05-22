9162f862ee6c960b309124c9cf2fa0de6ce20ea4
fDevJc <fDevJc@gmail.com>
2021-05-23 01:48:30 +0900

    feat: 통계프린트 및 수익률프린트 메서드 추가

--- src/main/java/lotto/ui/OutputUI.java
+++ src/main/java/lotto/ui/OutputUI.java
@@ -1,5 +1,7 @@
 package lotto.ui;
 
+import lotto.enums.Payout;
+
 public class OutputUI {
     public static void printInputPurchasingAmount() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -9,5 +11,13 @@ public class OutputUI {
     }
     public static void printInputWinningLottoNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
+    public static void printStatistics(int matchNumber, int count) {
+        System.out.printf("%d개 일치 (%d원)- %d개",matchNumber, Payout.getPayout(matchNumber), count);
+        System.out.println();
+    }
+
+    public static void printRateOfReturn(double rateOfReturn) {
+        System.out.printf("총 수익률은 %s 입니다.", String.format("%.2f",rateOfReturn));
     }
 }
