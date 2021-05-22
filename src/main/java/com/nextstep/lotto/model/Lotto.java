b0dde2cd17efbffab341eb54e3fde925f41cc8ee
KyungSik9870 <rudtlr9870@gmail.com>
2021-05-22 22:11:34 +0900

    feat: 생성한 로또 번호 출력

--- src/main/java/com/nextstep/lotto/model/Lotto.java
+++ src/main/java/com/nextstep/lotto/model/Lotto.java
@@ -1,10 +1,13 @@
 package com.nextstep.lotto.model;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Lotto {
 
 	public static final int LOTTO_SIZE = 6;
+	private static final String STRING_DELIMITER = ", ";
 	private List<LottoNumber> lottoNumbers;
 
 	public Lotto() {
@@ -17,6 +20,15 @@ public class Lotto {
 
 	public int size() {
 		return this.lottoNumbers.size();
+	}
+
+	public void print() {
+		Collections.sort(lottoNumbers);
+		StringJoiner stringJoiner = new StringJoiner(STRING_DELIMITER);
+		for (LottoNumber lottoNumber : lottoNumbers) {
+			stringJoiner.add(String.valueOf(lottoNumber.getNumber()));
+		}
+		System.out.println("[" + stringJoiner + "]");
 	}
 
 	private void generateNumbers() {
--- src/main/java/com/nextstep/lotto/model/LottoNumber.java
+++ src/main/java/com/nextstep/lotto/model/LottoNumber.java
@@ -1,6 +1,6 @@
 package com.nextstep.lotto.model;
 
-public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
 
 	public static final int MIN_NUMBER = 1;
 	public static final int MAX_NUMBER = 45;
@@ -12,5 +12,14 @@ public class LottoNumber {
 			throw new IllegalArgumentException("로또 번호는 1부터 45까지의 숫자여야 합니다.");
 		}
 		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	@Override
+	public int compareTo(LottoNumber lottoNumber) {
+		return number - lottoNumber.number;
 	}
 }
