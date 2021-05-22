922658d79f55e83ab092e6fdc178a00ac050daee
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-22 17:26:03 +0900

    feat : LottoRanks 클래스 신규작성, 수익률 계산로직 구현

---
+++ src/main/java/lotto/domain/LottoRanks.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoRanks {
+
+    private final List<LottoRank> lottoRanks;
+
+    public LottoRanks(List<LottoRank> lottoRanks) {
+        this.lottoRanks = lottoRanks;
+    }
+
+    public double totalReturnRate() {
+        double totalWinAmount = totalWinAmount();
+        double purchaseAmount = lottoRanks.size() * LottoTicket.PRICE;
+        return Math.floor(totalWinAmount / purchaseAmount * 100) / 100;
+    }
+
+    private long totalWinAmount() {
+        return lottoRanks.stream()
+                .mapToLong(LottoRank::winAmount)
+                .sum();
+    }
+}
