51d2e4ec74b991eeafadf519db30b91c3a495bf3
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-05-22 20:52:24 +0900

    docs(README.md): 기능 요구사항 수정 및 추가
    feat(LottoNumber.java): 로또번호 생성
    
    - 1~45사이의 숫자만 가지는 기능 구현
    
    test(LottoNumberTest.java): 로또번호 생성 및 유효성검증 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+/**
+ *  로또번호 생성 테스트
+ */
+public class LottoNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    @DisplayName("로또번호의 값 범위 유효성을 검증한다.")
+    void validate_lotto_number(int number) {
+        assertThatThrownBy(() -> new LottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 45})
+    @DisplayName("생성된 로또번호의 값을 검증한다.")
+    void get_lotto_number(int number) {
+        LottoNumber lottoNumber = new LottoNumber(number);
+        assertThat(lottoNumber.getNumber()).isEqualTo(number);
+    }
+
+    @Test
+    @DisplayName("로또번호를 생성한다.")
+    void create_lotto_number() {
+        int number = 1;
+        LottoNumber lottoNumber = new LottoNumber(number);
+        assertThat(lottoNumber).isNotNull();
+    }
+}
