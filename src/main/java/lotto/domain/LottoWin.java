726416e0f42f7b4f78d591b13f4ea8ee0aff0147
Donghui-Lee <ehdgml3206@naver.com>
2021-05-22 21:11:10 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/lotto/domain/LottoWin.java
+++ src/main/java/lotto/domain/LottoWin.java
@@ -14,10 +14,18 @@ public class LottoWin {
     }
 
     public void hit(int match) {
+        LottoRank lottoWinType = LottoRank.of(match);
+        if(lottoWinType != LottoRank.MISS) {
+            lottoWin.put(lottoWinType, lottoWin.get(lottoWinType) + 1);
+        }
     }
 
     public int sum() {
         int sum = 0;
+        for(LottoRank lottoWinType: lottoWin.keySet()) {
+            int multiple = lottoWinType.amount() * lottoWin.get(lottoWinType);
+            sum += multiple;
+        }
         return sum;
     }
 }
