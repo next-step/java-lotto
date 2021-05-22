e720d7168891de0c2b463e3d3c1f12fbe3d7d525
Junhee Ko <jko@rainist.com>
2021-05-23 01:56:41 +0900

    refactor: Integer count map to Rank count map

--- src/main/java/lotto/domain/MatchNumberCount.java
+++ src/main/java/lotto/domain/MatchNumberCount.java
@@ -1,28 +1,40 @@
 package lotto.domain;
 
+import lotto.enums.Rank;
+
 import java.util.Map;
 import java.util.Objects;
 
+import static lotto.enums.Rank.FIFTH;
+import static lotto.enums.Rank.FIRST;
+import static lotto.enums.Rank.FOURTH;
+import static lotto.enums.Rank.SECOND;
+import static lotto.enums.Rank.THIRD;
+
 public final class MatchNumberCount {
 
-  private final Map<Integer, Integer> matchNumberCount;
+  private final Map<Rank, Integer> matchNumberCount;
 
-  public MatchNumberCount(Map<Integer, Integer> matchNumberCount) {
+  public MatchNumberCount(Map<Rank, Integer> matchNumberCount) {
     this.matchNumberCount = matchNumberCount;
   }
 
   public Integer getRevenue() {
-    return matchNumberCount.get(3) * 5000
-        + matchNumberCount.get(4) * 50000
-        + matchNumberCount.get(5) * 1500000
-        + matchNumberCount.get(6) * 2000000000;
+    int revenue = 0;
+
+    for (Rank rank : Rank.values()) {
+      revenue += matchNumberCount.get(rank) * rank.getWinningMoney();
+    }
+
+    return revenue;
   }
 
   public void print() {
-    System.out.println("3개 일치 (5000원)- " + matchNumberCount.get(3) + "개");
-    System.out.println("4개 일치 (50000원)- " + matchNumberCount.get(4) + "개");
-    System.out.println("5개 일치 (1500000)- " + matchNumberCount.get(5) + "개");
-    System.out.println("6개 일치 (2000000000원)- " + matchNumberCount.get(6) + "개");
+    System.out.println("3개 일치 (5000원)- " + matchNumberCount.get(FIFTH) + "개");
+    System.out.println("4개 일치 (50000원)- " + matchNumberCount.get(FOURTH) + "개");
+    System.out.println("5개 일치 (1500000)- " + matchNumberCount.get(THIRD) + "개");
+    System.out.println("5개 일치, 보너스 볼 일치 (30000000원)- " + matchNumberCount.get(SECOND) + "개");
+    System.out.println("6개 일치 (2000000000원)- " + matchNumberCount.get(FIRST) + "개");
   }
 
   @Override
--- src/test/java/lotto/domain/MatchNumberCountTest.java
+++ src/test/java/lotto/domain/MatchNumberCountTest.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
+import lotto.enums.Rank;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
+import static lotto.enums.Rank.FIFTH;
+import static lotto.enums.Rank.FIRST;
+import static lotto.enums.Rank.FOURTH;
+import static lotto.enums.Rank.MISS;
+import static lotto.enums.Rank.SECOND;
+import static lotto.enums.Rank.THIRD;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchNumberCountTest {
@@ -11,11 +18,13 @@ class MatchNumberCountTest {
   @Test
   void revenue() {
     // Given
-    MatchNumberCount matchNumberCount = new MatchNumberCount(new HashMap<Integer, Integer>() {{
-      put(3, 1);
-      put(4, 0);
-      put(5, 0);
-      put(6, 0);
+    MatchNumberCount matchNumberCount = new MatchNumberCount(new HashMap<Rank, Integer>() {{
+      put(FIRST, 0);
+      put(SECOND, 0);
+      put(THIRD, 0);
+      put(FOURTH, 0);
+      put(FIFTH, 1);
+      put(MISS, 0);
     }});
 
     // When
