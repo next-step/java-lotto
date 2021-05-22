da49c0cde8f1f6871ae04d35286a75f3e79f1c1d
SUNJU\SUNJU <sunju635@naver.com>
2021-05-23 00:05:37 +0900

    test($step3): add LottoTicketTest

---
+++ src/test/java/step3/domain/LottoTicketTest.java
@@ -0,0 +1,42 @@
+package step3.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoTicketTest {
+
+    @Test
+    @DisplayName("로또 티켓 길이 확인")
+    void lottoTicket_shouldBeSix() {
+        // success
+        List<LottoNumber> lottoNumbers1 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)
+        );
+        assertThat(new Lottoticket(lottoNumbers1)).isInstanceOf(LottoTicket.class);
+
+
+        // failed
+        List<LottoNumber> lottoNumbers2 = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4)
+        );
+
+        assertThatThrownBy(()->new Lottoticket(lottoNumbers2)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(""));
+    }
+
+}
