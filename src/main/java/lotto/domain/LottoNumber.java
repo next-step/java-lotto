1b5d8e1a6bae8adb71006dc64340088d4854e331
Donghui-Lee <ehdgml3206@naver.com>
2021-05-22 17:22:29 +0900

    feat: 로또 번호 LottoNumber Class 추가

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class LottoNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private static Map<Integer, LottoNumber> lottoNumberCache = new HashMap<>();
+    private final int number;
+
+    static {
+        IntStream.range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER + 1)
+            .forEach(i -> lottoNumberCache.put(i, new LottoNumber(i)));
+    }
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber of(String number) {
+        return of(Integer.parseInt(number.trim()));
+    }
+
+    public static LottoNumber of(int number) {
+        return lottoNumberCache.get(number);
+    }
+}
