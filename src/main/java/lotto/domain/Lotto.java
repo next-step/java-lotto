2059d0c052bc108d7f697ee16ee087cfcec7df44
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-05-23 00:08:11 +0900

    feat(Lotto.java): 중복된 번호를 가지지 않도록 유효성을 검사하는 기능 구현
    test(LottoTest.java): 중복된 번호 입력시 유효성 검사기능을 검증하는 테스트코드 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,24 +1,34 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private static final int LOTTO_NUMBERS_COUNT = 6;
     private final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<LottoNumber> lottoNumbers) {
-        if (isNotEqualLottoNumbersSize(lottoNumbers)) {
-            throw new IllegalArgumentException("로또번호 개수가 부족하거나 초과하였습니다.");
-        }
-        this.lottoNumbers = lottoNumbers;
+        validateLottoNumbersSize(lottoNumbers);
+        validateDuplicatedNumber(lottoNumbers);
+        this.lottoNumbers = new ArrayList<>(lottoNumbers);
     }
 
     public List<LottoNumber> getLottoNumbers() {
         return new ArrayList<>(this.lottoNumbers);
     }
 
-    private boolean isNotEqualLottoNumbersSize(List<LottoNumber> lottoNumbers) {
-        return lottoNumbers.size() != LOTTO_NUMBERS_COUNT;
+    private void validateLottoNumbersSize(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("로또번호 개수가 부족하거나 초과하였습니다.");
+        }
+    }
+
+    private void validateDuplicatedNumber(List<LottoNumber> lottoNumbers) {
+        Set<LottoNumber> result = new HashSet<>(lottoNumbers);
+        if (result.size() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("로또는 중복되는 번호를 가질 수 없습니다.");
+        }
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -11,27 +11,40 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LottoTest {
+class LottoTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 5, 7, 10})
-    void validate_lotto_numbers_size(int size) {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int i = 1; i <= size; i++) {
-            lottoNumbers.add(new LottoNumber(i));
-        }
+    @ValueSource(strings = {"1,1,2,3,4,5", "1,2,3,3,5,6", "10,11,12,14,14,16"})
+    void isDuplicated_lotto_number(String numbers) {
+        List<LottoNumber> lottoNumbers = toLottoNumbers(numbers);
         assertThatThrownBy(() -> new Lotto(lottoNumbers))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또는 중복되는 번호를 가질 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "1,2,3,4,5", "1,2,3,4,5,6,7", "1,2,3,4,5,6,7,8,9,10"})
+    @DisplayName("로또번호가 6개가 아닐 경우 로또 생성시 유효성 검증")
+    void validate_lotto_numbers_size(String textNumber) {
+        List<LottoNumber> lottoNumbers = toLottoNumbers(textNumber);
+        assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또번호 개수가 부족하거나 초과하였습니다.");
     }
 
     @Test
-    @DisplayName("로또 생성")
+    @DisplayName("6개의 로또번호를 가진 로또 생성")
     void create_lotto() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int i = 1; i <= 6; i++) {
-            lottoNumbers.add(new LottoNumber(i));
-        }
+        List<LottoNumber> lottoNumbers = toLottoNumbers("1,2,3,4,5,6");
         Lotto lotto = new Lotto(lottoNumbers);
         assertThat(lotto.getLottoNumbers().size()).isEqualTo(6);
+    }
+
+    private List<LottoNumber> toLottoNumbers(String textNumber) {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (String number : textNumber.split(",")) {
+            lottoNumbers.add(new LottoNumber(Integer.parseInt(number.trim())));
+        }
+        return lottoNumbers;
     }
 }
