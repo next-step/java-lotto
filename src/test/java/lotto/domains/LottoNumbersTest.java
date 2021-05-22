df4f99ff8d6b0f2f071e26bae6b4e74c863d8fec
fDevJc <fDevJc@gmail.com>
2021-05-22 20:11:12 +0900

    refactor: LottoNumbersTest 테스트케이스 추가

--- src/test/java/lotto/domains/LottoNumbersTest.java
+++ src/test/java/lotto/domains/LottoNumbersTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoNumbersTest {
     @Test
@@ -14,7 +15,21 @@ public class LottoNumbersTest {
     }
 
     @Test
-    void 로또번호_1부터_45이외의_숫자_체크_테스트() {
-        LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(1,2,3,4,5,46));
+    void 로또번호_음수_45이상숫자_에러_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,2,3,4,5,46)));
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(-1,2,3,4,5,45)));
     }
+
+    @Test
+    void 로또번호_중복숫자_에러_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,1,3,4,5,46)));
+    }
+
+    @Test
+    void 로또번호_숫재갯수_에러_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,1,3,4,5)));
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,1,3,4,5,6,7)));
+    }
+
+
 }
