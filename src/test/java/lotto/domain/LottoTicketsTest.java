a98272df5546ad333888b95dd6af9bdb52b67819
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-22 16:41:22 +0900

    test : 수익률계산 테스트 작성

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoTicketsTest {
@@ -16,5 +23,49 @@ class LottoTicketsTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> LottoTickets.autoTickets(purchaseCount))
                 .withMessageMatching("구매 수량을 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("수익률 계산")
+    void totalReturnRate() {
+        // given
+        LottoTicket ticket1 = new LottoTicket(new LottoNumbers(Stream.of(8, 21, 23, 41, 42, 43).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket2 = new LottoTicket(new LottoNumbers(Stream.of(3, 5, 11, 16, 32, 38).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket3 = new LottoTicket(new LottoNumbers(Stream.of(7, 11, 16, 35, 36, 44).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket4 = new LottoTicket(new LottoNumbers(Stream.of(1, 8, 11, 31, 41, 42).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket5 = new LottoTicket(new LottoNumbers(Stream.of(13, 14, 16, 38, 42, 45).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket6 = new LottoTicket(new LottoNumbers(Stream.of(7, 11, 30, 40, 42, 43).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket7 = new LottoTicket(new LottoNumbers(Stream.of(2, 13, 22, 32, 38, 45).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket8 = new LottoTicket(new LottoNumbers(Stream.of(23, 25, 33, 36, 39, 41).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket9 = new LottoTicket(new LottoNumbers(Stream.of(1, 3, 5, 14, 22, 45).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket10 = new LottoTicket(new LottoNumbers(Stream.of(5, 9, 38, 41, 43, 44).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket11 = new LottoTicket(new LottoNumbers(Stream.of(2, 8, 9, 18, 19, 21).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket12 = new LottoTicket(new LottoNumbers(Stream.of(13, 14, 18, 21, 23, 35).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket13 = new LottoTicket(new LottoNumbers(Stream.of(17, 21, 29, 37, 42, 45).map(LottoNumber::of).collect(Collectors.toList())));
+        LottoTicket ticket14 = new LottoTicket(new LottoNumbers(Stream.of(3, 8, 27, 30, 35, 44).map(LottoNumber::of).collect(Collectors.toList())));
+
+        LottoNumbers winNumbers = new LottoNumbers(Stream.of(1, 2, 3, 4, 5, 6).map(LottoNumber::of).collect(Collectors.toList()));
+
+        // when
+        List<LottoTicket> lottoTicketList = new ArrayList<>();
+        lottoTicketList.add(ticket1);
+        lottoTicketList.add(ticket2);
+        lottoTicketList.add(ticket3);
+        lottoTicketList.add(ticket4);
+        lottoTicketList.add(ticket5);
+        lottoTicketList.add(ticket6);
+        lottoTicketList.add(ticket7);
+        lottoTicketList.add(ticket8);
+        lottoTicketList.add(ticket9);
+        lottoTicketList.add(ticket10);
+        lottoTicketList.add(ticket11);
+        lottoTicketList.add(ticket12);
+        lottoTicketList.add(ticket13);
+        lottoTicketList.add(ticket14);
+        LottoTickets lottoTickets = new LottoTickets(lottoTicketList);
+
+        // then
+        assertThat(lottoTickets.totalReturnRate(winNumbers)).isEqualTo(0.35);
+
     }
 }
