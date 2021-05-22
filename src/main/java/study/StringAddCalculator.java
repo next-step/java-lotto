c82b26d74c8692434ed288cdf4b6f0a613ae3ec6
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-23 02:06:55 +0900

    step2 - 피드백 반영

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,56 +1,66 @@
 package study;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     public static final String DEFAULT_DELIMETER_VALUE = ",|:";
     public static final String CUSTOM_DELIMETER_VALUE = "//(.)\n(.*)";
+    public static final String NUMBER_REGEX = "[+-]?[0-9]+$";
+
+    public static final int DEFAULT_RESULT_NUM = 0;
+    public static final int MATCHER_GROUP_1 = 1;
+    public static final int MATCHER_GROUP_2 = 2;
+
+    public static final String ILLEGAL_ARGUMENT_EXECEPTION_MSG = "양수만 입력 가능합니다.";
+    public static final String NUMBER_FORMAT_EXECEPTION_MSG = "숫자만 입력 가능합니다.";
 
     public static int splitAndSum(String text) {
-        if(checkNull(text))
-            return 0;
+        if(checkNullAndEmpty(text))
+            return DEFAULT_RESULT_NUM;
         return sumInteger(seperateText(text));
     }
 
     private static int sumInteger(String[] seperateText) {
         int sum = 0;
-        int absSum = 0;
+        Arrays.asList(seperateText).forEach(checkText -> checkTextValidation(checkText));
         for (String intText : seperateText) {
             sum += Integer.parseInt(intText);
-            absSum += Math.abs(Integer.parseInt(intText));
         }
-        isNegative(sum, absSum);
         return sum;
     }
 
-    private static void isNegative(int sum, int absSum) {
-        if(sum != absSum || sum < 0) {
-            throw new RuntimeException();
+    private static void checkTextValidation(String checkText) {
+        if(!checkText.matches(NUMBER_REGEX)) {
+            throw new NumberFormatException(NUMBER_FORMAT_EXECEPTION_MSG);
+        }
+        if(Integer.parseInt(checkText) < 0 ) {
+            throw new IllegalArgumentException (ILLEGAL_ARGUMENT_EXECEPTION_MSG);
         }
     }
 
     private static String[] seperateText(String text) {
-        String[] seperateText = customDelimiter(text);
+        String[] seperateText = splitCustomDelimiter(text);
         if(seperateText != null)
             return seperateText;
-        return defaultDelimeter(text);
+        return splitDefaultDelimeter(text);
     }
 
-    private static String[] customDelimiter(String text) {
+    private static String[] splitCustomDelimiter(String text) {
         Matcher matcher = Pattern.compile(CUSTOM_DELIMETER_VALUE).matcher(text);
         if(matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            return matcher.group(2).split(customDelimiter);
+            String customDelimiter = matcher.group(MATCHER_GROUP_1);
+            return matcher.group(MATCHER_GROUP_2).split(customDelimiter);
         }
         return null;
     }
 
-    private static String[] defaultDelimeter(String text) {
+    private static String[] splitDefaultDelimeter(String text) {
         return text.split(DEFAULT_DELIMETER_VALUE);
     }
 
-    private static boolean checkNull(String text) {
+    private static boolean checkNullAndEmpty(String text) {
         if(text == null || text.isEmpty())
             return true;
         return false;
