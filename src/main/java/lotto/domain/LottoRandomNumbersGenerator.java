128de5df375ef2425e9a68dc8d1df8c01d80bd2c
HoonHee Jeong <jhh992000@gmail.com>
2021-05-23 04:41:07 +0900

    refactor: 로또번호생성기에서 번호 리스트를 재사용하도록 변경

--- src/main/java/lotto/domain/LottoRandomNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoRandomNumbersGenerator.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public class LottoRandomNumbersGenerator implements RandomNumbersGenerator {
 
+	private final List<Integer> numbers;
+
+	public LottoRandomNumbersGenerator() {
+		this.numbers = new ArrayList<>();
+		for (int number = MIN; number <= MAX; number++) {
+			numbers.add(number);
+		}
+	}
+
 	@Override
 	public List<Integer> generateNumbers() {
 		List<Integer> numbers = this.generateRandomNumbers(LottoNumbers.LENGTH);
@@ -14,12 +23,8 @@ public class LottoRandomNumbersGenerator implements RandomNumbersGenerator {
 	}
 
 	private List<Integer> generateRandomNumbers(int length) {
-		List<Integer> numbers = new ArrayList<>();
-		for (int number = MIN; number <= MAX; number++) {
-			numbers.add(number);
-		}
-		Collections.shuffle(numbers);
-		return new ArrayList<>(numbers.subList(numbers.size() - length, numbers.size()));
+		Collections.shuffle(this.numbers);
+		return new ArrayList<>(this.numbers.subList(this.numbers.size() - length, this.numbers.size()));
 	}
 
 	private void sortAsc(List<Integer> numbers) {
