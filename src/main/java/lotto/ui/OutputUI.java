45104bb860bc5e0e9872e39f4e65bb9bc894c5ca
fDevJc <fDevJc@gmail.com>
2021-05-22 15:42:47 +0900

    feat: ui class 추가

---
+++ src/main/java/lotto/ui/OutputUI.java
@@ -0,0 +1,13 @@
+package lotto.ui;
+
+public class OutputUI {
+    public static void printInputPurchasingAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    public static void printNumberOfPurchases(int number) {
+        System.out.println(number+"개를 구매했습니다.");
+    }
+    public static void printInputWinningLottoNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
+}
