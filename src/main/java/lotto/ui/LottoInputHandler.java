0efbbef3a03badee26978cbd54dcd9a74fb9b045
itdar <thesockerrr@gmail.com>
2021-05-22 17:34:29 +0900

    feat: 로또게임 어플리케이션 메인에 2등 기능 추가
    - Input/Output handler 추가 (2등 내용)
    - 테스트케이스 수정

--- src/main/java/lotto/ui/LottoInputHandler.java
+++ src/main/java/lotto/ui/LottoInputHandler.java
@@ -1,12 +1,10 @@
 package lotto.ui;
 
 import lotto.domain.LottoPurchaseBudget;
-import lotto.domain.LottoWinNumbers;
 
 import java.util.Scanner;
 
 public class LottoInputHandler {
-
     public LottoPurchaseBudget scanLottoPurchaseBudget() {
         System.out.println("구입금액을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
@@ -14,11 +12,17 @@ public class LottoInputHandler {
         return new LottoPurchaseBudget(scanner.nextInt());
     }
 
-    public LottoWinNumbers scanLottoWinNumbers() {
+    public String scanLottoWinNumbersString() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
 
-        return new LottoWinNumbers(scanner.nextLine());
+        return scanner.nextLine();
     }
 
+    public int scanLottoBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextInt();
+    }
 }
