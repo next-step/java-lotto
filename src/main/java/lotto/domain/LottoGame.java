0a464a418f45e761ff7a546610e54389073fb855
Junhee Ko <jko@rainist.com>
2021-05-22 22:02:05 +0900

    add: LottoGame.java

---
+++ src/main/java/lotto/domain/LottoGame.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import lotto.input.InputView;
+import lotto.input.PurchaseAmountQuantity;
+import lotto.input.WinningNumber;
+import lotto.output.ResultView;
+
+public final class LottoGame {
+
+  private final InputView inputView;
+  private final LottoSeller lottoSeller;
+  private final ResultView resultView;
+
+  public LottoGame(InputView inputView, LottoSeller lottoSeller, ResultView resultView) {
+    this.inputView = inputView;
+    this.lottoSeller = lottoSeller;
+    this.resultView = resultView;
+  }
+
+  public void start() {
+    PurchaseAmountQuantity purchaseAmountQuantity = inputView.getPurchaseAmountQuantity();
+
+    LottoBucket lottoBucket = lottoSeller.getLottoBucketBy(purchaseAmountQuantity);
+    WinningNumber winningNumber = inputView.getWinningNumber();
+
+    resultView.printBy(purchaseAmountQuantity, lottoBucket, winningNumber);
+  }
+}
