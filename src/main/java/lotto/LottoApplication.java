8d5f4da19800780341d5e00dfa7b81d0ec490f8c
jess <jess.l@kakaocorp.com>
2021-05-22 10:19:54 +0900

    feat(LottoApplication): LottoApplication 추가 - main 메서드 제공

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import lotto.contoller.LottoAppController;
+
+public class LottoApplication {
+    public static void main(String[] args){
+        LottoAppController app = new LottoAppController();
+        app.run();
+    }
+}
