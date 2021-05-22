bf37389b1c77bf19181fbdf1e8b4539d141c4d6b
HyungjuJang <dacapolife87@gmail.com>
2021-05-23 01:54:10 +0900

    refactor : 랜덤넘버생성부분을 전략패턴으로 변경 - 전략패턴으로 변경함으로서 해당부분이 테스트케이스에 포함된 부분일 경우 테스트용 전략패턴을 적용하여 테스트가 가능해진다.

--- src/main/java/lotto/domain/RandomNumberGeneratorStrategy.java
+++ src/main/java/lotto/domain/RandomNumberGeneratorStrategy.java
@@ -1,23 +1,28 @@
-package lotto.utils;
+package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class RandomNumberGenerator {
+public class RandomNumberGeneratorStrategy implements NumberGeneratorStrategy {
 
-    private RandomNumberGenerator() {
+    public RandomNumberGeneratorStrategy() {
     }
 
-    public static List<String> randomSixNumbers() {
+    @Override
+    public List<String> generator() {
+        return randomSixNumbers();
+    }
+
+    private List<String> randomSixNumbers() {
         List<String> availableNumbers = availableAllNumbers();
         Collections.shuffle(availableNumbers);
 
         return availableNumbers.subList(0, 6);
     }
 
-    private static List<String> availableAllNumbers() {
+    private List<String> availableAllNumbers() {
         return IntStream.range(1, 45)
                 .mapToObj(number -> String.valueOf(number))
                 .collect(Collectors.toList());
