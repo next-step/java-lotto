065c27ec4aa22191789b309a05b03695adf3a07d
mskangg <funnycoding91@gmail.com>
2021-05-23 01:48:07 +0900

    feat: 구매한 모든 로또 결과 확인

--- src/main/java/lotto/domain/Bank.java
+++ src/main/java/lotto/domain/Bank.java
@@ -7,7 +7,15 @@ public final class Bank {
         this.answerLotto = answerLotto;
     }
 
-    public LottoResult matchLotto(Lotto unidentifiedLotto) {
+    public LottoWallet matchLottos(Lottos lottos, Money money) {
+        LottoWallet lottoWallet = new LottoWallet(money);
+        for (Lotto lotto : lottos.getLottos()) {
+            lottoWallet.addLottoResult(matchLotto(lotto));
+        }
+        return lottoWallet;
+    }
+
+    private LottoResult matchLotto(Lotto unidentifiedLotto) {
         return LottoResult.findByMatchCount(answerLotto.matchCount(unidentifiedLotto));
     }
 }
--- src/test/java/lotto/domain/BankTest.java
+++ src/test/java/lotto/domain/BankTest.java
@@ -2,13 +2,20 @@ package lotto.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.platform.commons.util.ReflectionUtils;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class BankTest {
     private Lotto answerLotto;
@@ -21,29 +28,40 @@ public class BankTest {
         ));
     }
 
-    @DisplayName("일치하는 갯수에 맞게 상금이 산정 되는지")
+    @DisplayName("로또번호가 일치하는만큼 LottoWallet에 잘 저장되는지")
     @ParameterizedTest
-    @CsvSource({"1, 2, 3, 4, 5, 6, 6, 2000000000",
-            "1, 2, 3, 4, 5, 45, 5, 1500000",
-            "1, 2, 3, 4, 44, 45, 4, 50000",
-            "1, 2, 3, 43, 44, 45, 3, 5000",
-            "40, 41, 42, 43, 44, 45, 0, 0"
-    })
-    void lotto_result(final int lottoNumber1, final int lottoNumber2, final int lottoNumber3,
-                      final int lottoNumber4, final int lottoNumber5, final int lottoNumber6,
-                      final int expectedMatchCount, final int expectedReward) {
+    @MethodSource("parametersForLottos")
+    void lottos_result(final List<Lotto> lottoList) {
         //given
-        Lotto toCompareLotto = new Lotto(Arrays.asList(
-                new LottoNumber(lottoNumber1), new LottoNumber(lottoNumber2), new LottoNumber(lottoNumber3),
-                new LottoNumber(lottoNumber4), new LottoNumber(lottoNumber5), new LottoNumber(lottoNumber6)
-        ));
+        Lottos lottos = new Lottos(lottoList);
+        Money money = new Money(6000);
+        Bank bank = new Bank(answerLotto);
 
         //when
-        Bank bank = new Bank(answerLotto);
-        LottoResult result = bank.matchLotto(toCompareLotto);
+        LottoWallet lottoWallet = bank.matchLottos(lottos, money);
+        Map<LottoResult, Integer> lottoResultCounts = lottoWallet.getLottoResultCounts();
 
         //then
-        assertThat(result.getMatchCount()).isEqualTo(expectedMatchCount);
-        assertThat(result.getReward()).isEqualTo(expectedReward);
+        assertThat(lottoResultCounts.get(LottoResult.FIRST)).isEqualTo(1);
+        assertThat(lottoResultCounts.get(LottoResult.SECOND)).isEqualTo(1);
+        assertThat(lottoResultCounts.get(LottoResult.THIRD)).isEqualTo(1);
+        assertThat(lottoResultCounts.get(LottoResult.FOURTH)).isEqualTo(2);
+    }
+
+    static Stream<Arguments> parametersForLottos() {
+        return Stream.of(arguments(Arrays.asList(
+                new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))), //LottoResult.FIRST
+                new Lotto(Arrays.asList(new LottoNumber(11), new LottoNumber(2), new LottoNumber(3),
+                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))), //LottoResult.SECOND
+                new Lotto(Arrays.asList(new LottoNumber(11), new LottoNumber(22), new LottoNumber(3),
+                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))), //LottoResult.THIRD
+                new Lotto(Arrays.asList(new LottoNumber(11), new LottoNumber(22), new LottoNumber(33),
+                        new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))), //LottoResult.FOURTH
+                new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(22), new LottoNumber(33),
+                        new LottoNumber(44), new LottoNumber(5), new LottoNumber(6))), //LottoResult.FOURTH
+                new Lotto(Arrays.asList(new LottoNumber(11), new LottoNumber(22), new LottoNumber(33),
+                        new LottoNumber(44), new LottoNumber(5), new LottoNumber(6))) //LottoResult.NONE
+        )));
     }
 }
