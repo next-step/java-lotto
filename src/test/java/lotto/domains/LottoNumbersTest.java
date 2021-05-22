50eeeb2401537f50b3236fc188c12ee9c6e29973
fDevJc <fDevJc@gmail.com>
2021-05-22 23:52:37 +0900

    test: 테스트케이스 추가

--- src/test/java/lotto/domains/LottoNumbersTest.java
+++ src/test/java/lotto/domains/LottoNumbersTest.java
@@ -1,6 +1,8 @@
 package lotto.domains;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
@@ -8,15 +10,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoNumbersTest {
-    @Test
-    void 생성자_테스트() {
-        LottoNumbers lottoNumbers = new LottoNumbers();
-        assertThat(lottoNumbers).isNotNull();
+    @ParameterizedTest
+    @CsvSource(value = {"1,true","6,true","0,false","7,false"}, delimiter = ',')
+    void 특정숫자가_포함되어있는지_테스트(int targetNumber, boolean expected) {
+        LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(1,2,3,4,5,6));
+        assertThat(lottoNumbers.contains(targetNumber)).isEqualTo(expected);
     }
 
     @Test
-    void 로또번호_음수_45이상숫자_에러_테스트() {
+    void 로또번호_0이하_45이상숫자_에러_테스트() {
         assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,2,3,4,5,46)));
+        assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(0,2,3,4,5,45)));
         assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(-1,2,3,4,5,45)));
     }
 
@@ -26,7 +30,7 @@ public class LottoNumbersTest {
     }
 
     @Test
-    void 로또번호_숫재갯수_에러_테스트() {
+    void 로또번호_숫자갯수_에러_테스트() {
         assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,1,3,4,5)));
         assertThrows(IllegalArgumentException.class, () -> new LottoNumbers(Arrays.asList(1,1,3,4,5,6,7)));
     }
