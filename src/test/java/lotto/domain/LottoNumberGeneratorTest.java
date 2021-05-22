62fa15e6d7ef2a5690faee015f36682b9bc37580
HyungjuJang <dacapolife87@gmail.com>
2021-05-23 01:58:43 +0900

    refactor : 패키지변경

--- src/test/java/lotto/domain/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumberGeneratorTest.java
@@ -1,7 +1,5 @@
-package lotto.utils;
+package lotto.domain;
 
-import lotto.domain.LottoNumber;
-import lotto.domain.LottoNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
