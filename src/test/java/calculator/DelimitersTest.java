fb11da12cc833207207490a18bdc554a892824f4
sanggu <tkdrn8578@naver.com>
2021-05-22 14:49:29 +0900

    refactor: ParameterizedTest로 변경하여 다양한 delimiter로 테스트하도록 수정

--- src/test/java/calculator/DelimitersTest.java
+++ src/test/java/calculator/DelimitersTest.java
@@ -1,16 +1,15 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class DelimitersTest {
 
-    @Test
-    void validateExistDelimiter_test() {
-        //given
-        char delimiter = '[';
-
+    @ParameterizedTest
+    @ValueSource(chars = {'[', ']', '|', '<', '>', 'A', '8'})
+    void validateExistDelimiter_test(char delimiter) {
         //when
         assertThatIllegalArgumentException().isThrownBy(() -> Delimiters.validateExistDelimiter(delimiter));
     }
