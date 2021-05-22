91b1e77083b82b6f4089bf174361bde71bb1d7d6
김석진 <7271kim@naver.com>
2021-05-22 11:39:47 +0900

    feat : 기능 목록 9 구현
    - 숫자 배열을 입력 받아 등수별 총 수를 반환한다.
    
    [수정사항]
    - TotalLotto.java : getWinningWithNumbers() 추가
    - TotalLottoTest.java : winningLottoTest() 추가
    - README.md

--- src/main/java/step3/model/TotalLotto.java
+++ src/main/java/step3/model/TotalLotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class TotalLotto {
 
+    private static final int MAX_WINNING = 6;
     private ArrayList<LottoNumbers> totalLotto;
 
     public TotalLotto() {
@@ -27,6 +28,26 @@ public class TotalLotto {
         List<List<Integer>> result = new ArrayList<>();
         for (LottoNumbers lottoNumbers : totalLotto) {
             result.add(lottoNumbers.numbers());
+        }
+        return result;
+    }
+
+    public List<Integer> getWinningWithNumbers(
+            ArrayList<Integer> winningNumber) {
+
+        List<Integer> result = initWithNumbers();
+        for (LottoNumbers lotto : totalLotto) {
+            int countWinning = lotto.countWinning(winningNumber);
+            result.set(countWinning, result.get(countWinning) + 1);
+        }
+
+        return result;
+    }
+
+    private List<Integer> initWithNumbers() {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int index = 0; index < MAX_WINNING + 1; index++) {
+            result.add(0);
         }
         return result;
     }
--- src/test/java/step3/model/TotalLottoTest.java
+++ src/test/java/step3/model/TotalLottoTest.java
@@ -2,6 +2,10 @@ package step3.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,4 +27,32 @@ public class TotalLottoTest {
         assertThat(totalLotto.size()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("당첨 번호를 받아  등수별 총 수를 반환한다.")
+    void winningLottoTest() {
+        TotalLotto totalLotto = new TotalLotto();
+        setLottos(totalLotto);
+
+        List<Integer> winning = totalLotto.getWinningWithNumbers(
+            new ArrayList<>(Arrays.asList(1, 2, 3, 7, 8, 9)));
+
+        assertThat(winning.get(3)).isEqualTo(1);
+        assertThat(winning.get(4)).isEqualTo(2);
+        assertThat(winning.get(5)).isEqualTo(1);
+        assertThat(winning.get(6)).isEqualTo(1);
+    }
+
+    private void setLottos(TotalLotto totalLotto) {
+        addLotto(totalLotto, new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        addLotto(totalLotto,
+            new ArrayList<>(Arrays.asList(1, 2, 3, 7, 10, 11)));
+        addLotto(totalLotto,
+            new ArrayList<>(Arrays.asList(1, 2, 3, 8, 10, 11)));
+        addLotto(totalLotto, new ArrayList<>(Arrays.asList(1, 2, 3, 7, 9, 10)));
+        addLotto(totalLotto, new ArrayList<>(Arrays.asList(1, 2, 3, 7, 8, 9)));
+    }
+
+    private void addLotto(TotalLotto totalLotto, ArrayList<Integer> arrayList) {
+        totalLotto.addLottoOne(new LottoNumbers(arrayList));
+    }
 }
