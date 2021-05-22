0b8dc1d3c10ba86c56fc1fb79b5ff80df33f0238
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-22 15:43:13 +0900

    feat : LottoRank 클래스 신규작성

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+
+    LOSE(0, 0),
+    FIRST(6, 2_000_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
+
+    private final int matchCount;
+    private final int winAmount;
+
+    LottoRank(int matchCount, int winAmount) {
+        this.matchCount = matchCount;
+        this.winAmount = winAmount;
+    }
+
+    public int matchCount() {
+        return matchCount;
+    }
+
+    public int winAmount() {
+        return winAmount;
+    }
+
+    public static LottoRank of(int matchCount) {
+        if (isLose(matchCount)) {
+            return LOSE;
+        }
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static boolean isLose(int matchCount) {
+        return matchCount >= LOSE.matchCount() && matchCount < FIFTH.matchCount();
+    }
+}
