7aa98d9e0b8a0d4e0c9c5d850bc4a89c48d1a877
KyungSik9870 <rudtlr9870@gmail.com>
2021-05-22 18:45:24 +0900

    refactor: Lotto 에서 랜덤숫자 생성하는 로직을 분리
    LottoNumberGenerator 클래스를 만들어 랜덤숫자를 생성하도록 하였음.

--- src/main/java/com/nextstep/lotto/model/Lotto.java
+++ src/main/java/com/nextstep/lotto/model/Lotto.java
@@ -1,24 +1,14 @@
 package com.nextstep.lotto.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
 
-	private static final int LOTTO_SIZE = 6;
+	public static final int LOTTO_SIZE = 6;
 	private List<LottoNumber> lottoNumbers;
 
 	public Lotto() {
 		generateNumbers();
-	}
-
-	private void generateNumbers() {
-		List<Integer> numberList = new ArrayList<>();
-		for (int i = LottoNumber.MIN_NUMBER; i <= LottoNumber.MAX_NUMBER; i++) {
-			numberList.add(i);
-		}
-		lottoNumbers = makeRandomNumber(numberList);
 	}
 
 	public List<LottoNumber> getLottoNumbers() {
@@ -29,12 +19,8 @@ public class Lotto {
 		return this.lottoNumbers.size();
 	}
 
-	private List<LottoNumber> makeRandomNumber(List<Integer> numberList) {
-		Collections.shuffle(numberList);
-		List<LottoNumber> lottoNumbers = new ArrayList<>();
-		for (int i = 0; i < LOTTO_SIZE; i++) {
-			lottoNumbers.add(new LottoNumber(numberList.get(i)));
-		}
-		return lottoNumbers;
+	private void generateNumbers() {
+		LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+		lottoNumbers = lottoNumberGenerator.getRandomNumbers();
 	}
 }
---
+++ src/main/java/com/nextstep/lotto/model/LottoNumberGenerator.java
@@ -0,0 +1,27 @@
+package com.nextstep.lotto.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+	private List<Integer> rangeNumbers;
+
+	public LottoNumberGenerator() {
+		rangeNumbers = new ArrayList<>();
+		for (int i = LottoNumber.MIN_NUMBER; i <= LottoNumber.MAX_NUMBER; i++) {
+			rangeNumbers.add(i);
+		}
+	}
+
+	public List<LottoNumber> getRandomNumbers() {
+		Collections.shuffle(rangeNumbers);
+		List<LottoNumber> lottoNumbers = new ArrayList<>();
+		for (int i = 0; i < Lotto.LOTTO_SIZE; i++) {
+			lottoNumbers.add(new LottoNumber(rangeNumbers.get(i)));
+		}
+		return lottoNumbers;
+	}
+
+}
