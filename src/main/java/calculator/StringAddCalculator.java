1844e0fd44fe23f60874757e6864d597247c5243
Jeong Minkyo <redwonder@naver.com>
2021-05-22 18:43:09 +0900

    feat(StringAddCalculator.java) : 문자열 덧셈 기능 계산기 구현
    - 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
    - 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용
    - 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,36 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+	public static int splitAndSum(String text) {
+		if(text == null) {
+			return 0;
+		}
+
+		if(text.isEmpty()) {
+			return 0;
+		}
+
+		String[] tokens= text.split(",|:");
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			tokens = m.group(2).split(customDelimiter);
+		}
+
+		int sum = 0;
+		for(String input : tokens) {
+			int num = Integer.parseInt(input);
+
+			if (num < 0) {
+				throw new RuntimeException();
+			}
+			sum += num;
+		}
+
+		return sum;
+	}
+}
