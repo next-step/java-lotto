b0607480d43aad0d982cebd30b5ce8cf013e6933
prodo-developer <wjdals300@gmail.com>
2021-05-23 03:17:26 +0900

    feat : 1 ~ 45 의 범위를 가지는 로또 번호를 생성한다.

---
+++ src/main/java/domain/LottoNumber.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private final static String LOTTO_NUMBER_VALIDATION_MESSAGE = "로또 숫자는 1 ~ 45 사이의 숫자만 가능합니다.";
+    private final int lottoNumber;
+
+    public LottoNumber(int number) {
+        validation(number);
+        lottoNumber = number;
+    }
+
+    private void validation(int number) {
+        if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_VALIDATION_MESSAGE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(lottoNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
+    public int ascendingSort(LottoNumber comparisonObject) {
+        return this.lottoNumber - comparisonObject.lottoNumber;
+    }
+}
