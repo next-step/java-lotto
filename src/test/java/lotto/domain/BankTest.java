7976da12806555be73b4cb1382947bcfed3fd441
mskangg <funnycoding91@gmail.com>
2021-05-22 18:49:42 +0900

    test: 반복되는 코드 @CsvSource로 병합

--- src/test/java/lotto/domain/BankTest.java
+++ src/test/java/lotto/domain/BankTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
@@ -19,96 +21,22 @@ public class BankTest {
         ));
     }
 
-    @DisplayName("모든 숫자가 일치하는 1등 당첨되어 상금이 2000000000 인지")
-    @Test
-    void first_lotto() {
-        //given
-        Lotto toCompareLotto = new Lotto(answerLotto);
-        int expectedMatchCount = 6;
-        int expectedReward = 2000000000;
-
-        //when
-        Bank bank = new Bank(answerLotto);
-        LottoResult result = bank.matchLotto(toCompareLotto);
-
-        //then
-        assertThat(result.getMatchCount()).isEqualTo(expectedMatchCount);
-        assertThat(result.getReward()).isEqualTo(expectedReward);
-    }
-
-    @DisplayName("5개의 숫자가 일치하는 2등 당첨되어 상금이 1500000 인지")
-    @Test
-    void second_lotto() {
+    @DisplayName("일치하는 갯수에 맞게 상금이 산정 되는지")
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, 4, 5, 6, 6, 2000000000",
+            "1, 2, 3, 4, 5, 45, 5, 1500000",
+            "1, 2, 3, 4, 44, 45, 4, 50000",
+            "1, 2, 3, 43, 44, 45, 3, 5000",
+            "40, 41, 42, 43, 44, 45, 0, 0"
+    })
+    void lotto_result(final int lottoNumber1, final int lottoNumber2, final int lottoNumber3,
+                      final int lottoNumber4, final int lottoNumber5, final int lottoNumber6,
+                      final int expectedMatchCount, final int expectedReward) {
         //given
         Lotto toCompareLotto = new Lotto(Arrays.asList(
-                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(4), new LottoNumber(5), new LottoNumber(45)
+                new LottoNumber(lottoNumber1), new LottoNumber(lottoNumber2), new LottoNumber(lottoNumber3),
+                new LottoNumber(lottoNumber4), new LottoNumber(lottoNumber5), new LottoNumber(lottoNumber6)
         ));
-        int expectedMatchCount = 5;
-        int expectedReward = 1500000;
-
-        //when
-        Bank bank = new Bank(answerLotto);
-        LottoResult result = bank.matchLotto(toCompareLotto);
-
-        //then
-        assertThat(result.getMatchCount()).isEqualTo(expectedMatchCount);
-        assertThat(result.getReward()).isEqualTo(expectedReward);
-    }
-
-    @DisplayName("4개의 숫자가 일치하는 3등 당첨되어 상금이 50000 인지")
-    @Test
-    void third_lotto() {
-        //given
-        Lotto toCompareLotto = new Lotto(Arrays.asList(
-                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(4), new LottoNumber(44), new LottoNumber(45)
-        ));
-        int expectedMatchCount = 4;
-        int expectedReward = 50000;
-
-
-        //when
-        Bank bank = new Bank(answerLotto);
-        LottoResult result = bank.matchLotto(toCompareLotto);
-
-        //then
-        assertThat(result.getMatchCount()).isEqualTo(expectedMatchCount);
-        assertThat(result.getReward()).isEqualTo(expectedReward);
-    }
-
-    @DisplayName("3개의 숫자가 일치하는 4등 당첨되어 상금이 5000 인지")
-    @Test
-    void fourth_lotto() {
-        //given
-        Lotto toCompareLotto = new Lotto(Arrays.asList(
-                new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(43), new LottoNumber(44), new LottoNumber(45)
-        ));
-        int expectedMatchCount = 3;
-        int expectedReward = 5000;
-
-
-        //when
-        Bank bank = new Bank(answerLotto);
-        LottoResult result = bank.matchLotto(toCompareLotto);
-
-        //then
-        assertThat(result.getMatchCount()).isEqualTo(expectedMatchCount);
-        assertThat(result.getReward()).isEqualTo(expectedReward);
-    }
-
-    @DisplayName("3개 미만의 숫자가 일치하여 상금이 0 인지")
-    @Test
-    void none_lotto() {
-        //given
-        Lotto toCompareLotto = new Lotto(Arrays.asList(
-                new LottoNumber(40), new LottoNumber(41), new LottoNumber(42),
-                new LottoNumber(43), new LottoNumber(44), new LottoNumber(45)
-        ));
-        int expectedMatchCount = 0;
-        int expectedReward = 0;
-
 
         //when
         Bank bank = new Bank(answerLotto);
