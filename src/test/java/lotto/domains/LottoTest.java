026e092f53c0a08cc0dd98753bb3abc575a5d47f
fDevJc <fDevJc@gmail.com>
2021-05-22 23:56:07 +0900

    test: 중복코드 제거 및 테스트케이스 추가

--- src/test/java/lotto/domains/LottoTest.java
+++ src/test/java/lotto/domains/LottoTest.java
@@ -1,25 +1,40 @@
 package lotto.domains;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
-    @Test
-    void 당첨번호_3개_테스트() {
-        List<Integer> winningNumber = Arrays.asList(1,2,3,4,5,6);
-        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,10,11,12)));
-        assertThat(lotto.checkNumber(new LottoNumbers(winningNumber))).isEqualTo(3);
+    private LottoNumbers winningNumber;
+
+    @BeforeEach
+    void setUp() {
+        this.winningNumber = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
     }
 
-    @Test
-    void 당첨번호_6개_테스트() {
-        List<Integer> winningNumber = Arrays.asList(1,2,3,4,5,6);
-        Lotto lotto = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,4,5,6)));
-        assertThat(lotto.checkNumber(new LottoNumbers(winningNumber))).isEqualTo(6);
+    private static Stream<Arguments> provideListAndExpectedNumber() {
+        return Stream.of(
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6))), 6),
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 16))), 5),
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 15, 16))), 4),
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 14, 15, 16))), 3),
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(1, 2, 13, 14, 15, 16))), 2),
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(1, 12, 13, 14, 15, 16))), 1),
+                Arguments.of(new Lotto(new LottoNumbers(Arrays.asList(11, 12, 13, 14, 15, 16))), 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideListAndExpectedNumber")
+    void 당첨번호_테스트(Lotto lotto, int expectedNumber) {
+        assertThat(lotto.matchingNumberCount(winningNumber)).isEqualTo(expectedNumber);
     }
 
     @Test
