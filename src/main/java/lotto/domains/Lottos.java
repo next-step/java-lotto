b02f04bf2bc573893ba40ca21dbceef9ae7efa17
fDevJc <fDevJc@gmail.com>
2021-05-22 22:41:06 +0900

    feat: 통계출력 기능 추가

--- src/main/java/lotto/domains/Lottos.java
+++ src/main/java/lotto/domains/Lottos.java
@@ -13,4 +13,18 @@ public class Lottos {
     public int count() {
         return lottoList.size();
     }
+
+    public void printLottosNumbers() {
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto.lottoNumbers());
+        }
+    }
+
+    public LottoStatistics getStatistics(LottoNumbers winningLottoNumbers) {
+        LottoStatistics lottoStatistics = new LottoStatistics();
+        for (Lotto lotto : lottoList) {
+            lottoStatistics.add(lotto.checkNumber(winningLottoNumbers));
+        }
+        return lottoStatistics;
+    }
 }
--- src/test/java/lotto/domains/LottosTest.java
+++ src/test/java/lotto/domains/LottosTest.java
@@ -7,9 +7,29 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottosTest {
+
     @Test
     void 로또_갯수_테스트() {
         Lottos lottos = new Lottos(Arrays.asList(new Lotto(),new Lotto()));
         assertThat(lottos.count()).isEqualTo(2);
+    }
+    
+    @Test
+    void 통계값_3개일치_가져오기_테스트() {
+
+        LottoNumbers winningLottoNumbers = new LottoNumbers(Arrays.asList(1,2,3,4,5,6));
+
+        Lotto lotto1 = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,4,5,6)));
+        Lotto lotto2 = new Lotto(new LottoNumbers(Arrays.asList(11,12,13,14,15,16)));
+        Lotto lotto3 = new Lotto(new LottoNumbers(Arrays.asList(1,2,3,14,15,16)));
+
+        Lottos lottos = new Lottos(Arrays.asList(lotto1,lotto2,lotto3));
+
+        assertThat(lottos.getStatistics(winningLottoNumbers).count(3)).isEqualTo(1);
+    }
+
+    @Test
+    void 당첨금_합_구하기() {
+
     }
 }
