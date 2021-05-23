ed6b6c7ea9d365850f53fff405a7a1d677a6c60b
wrallee <wrallee@naver.com>
2021-05-23 15:56:41 +0900

    feat(calculator): 쉼표 또는 콜론을 구분자로 가지는 문자열 계산

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,28 @@
 package step2;
 
+import static java.lang.Integer.*;
+
 public class StringAddCalculator {
 	public static int splitAndSum(String input) {
-		return 0;
+		if (input == null || input.isEmpty()) {
+			return 0;
+		}
+
+		if (input.contains(",")) {
+			return getSplitSumResult(input);
+		}
+
+		return parseInt(input);
+	}
+
+	private static int getSplitSumResult(String input) {
+		String[] numbers = input.split(",|:");
+		int sum = 0;
+
+		for (String number : numbers) {
+			sum += parseInt(number);
+		}
+
+		return sum;
 	}
 }
