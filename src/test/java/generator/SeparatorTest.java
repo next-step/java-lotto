a1e00134246846a2d4f482c22fc4490b64c0d1bb
accidentlywoo <accidentlywoo@gmail.com>
2021-05-23 15:46:10 +0900

    teat : 입력값 fixture 분리, 커스텀 구분자 테스트케이스

--- src/test/java/generator/SeparatorTest.java
+++ src/test/java/generator/SeparatorTest.java
@@ -1,6 +1,7 @@
 package generator;
 
 import generate.Separator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.ExceptionMessage;
@@ -13,26 +14,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class SeparatorTest {
+	private List<String> NUMBERS_FIXTURE;
+
+	@BeforeEach
+	public void 인풋값_픽스처(){
+		NUMBERS_FIXTURE = Arrays.asList("1", "2", "3");
+	}
+
 	@Test
 	@DisplayName (",와 :구분자로 문자열을 전달하는 경우 구분자를 기준으로 분리한 숫자의 합을 반환")
 	public void 구분자를_컴마로_콜론으로_숫자분리(){
-		List<String> numbers = Arrays.asList("1", "2", "3");
 
 		List<String> splitComma = Separator.split("1,2,3");
 
-		assertThat(numbers).isEqualTo(splitComma);
+		assertThat(NUMBERS_FIXTURE).isEqualTo(splitComma);
 
 		List<String> splitColon = Separator.split("1:2:3");
 
-		assertThat(numbers).isEqualTo(splitColon);
+		assertThat(NUMBERS_FIXTURE).isEqualTo(splitColon);
 
 		List<String> splitCommaAndColon = Separator.split("1:2,3");
 
-		assertThat(numbers).isEqualTo(splitCommaAndColon);
+		assertThat(NUMBERS_FIXTURE).isEqualTo(splitCommaAndColon);
 
 		List<String> splitCommaAndColon2 = Separator.split("1,2:3");
 
-		assertThat(numbers).isEqualTo(splitCommaAndColon2);
+		assertThat(NUMBERS_FIXTURE).isEqualTo(splitCommaAndColon2);
 	}
 
 	@Test
@@ -50,6 +57,8 @@ class SeparatorTest {
 
 	@Test
 	public void 문자사이에_커스텀구분자를_지정할_수_있다(){
+		List<String> splitCustom = Separator.split("//(.)\\n1.2.3");
 
+		assertThat(NUMBERS_FIXTURE).isEqualTo(splitCustom);
 	}
 }
