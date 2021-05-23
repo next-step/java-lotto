a21ac43a96f60318ae2b601cde9011bac453cb25
wrallee <wrallee@naver.com>
2021-05-23 16:29:51 +0900

    feat(calculator): 커스텀 구분자를 기준으로 더하는 기능

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -2,13 +2,29 @@ package step2;
 
 import static java.lang.Integer.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 	public static int splitAndSum(String text) {
 		if (text == null || text.isEmpty()) {
 			return 0;
 		}
 
-		return sumStringArray(text.split(",|:"));
+		String[] tokens = getStringArray(text);
+
+		return sumStringArray(tokens);
+	}
+
+	private static String[] getStringArray(String text) {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			return m.group(2).split(customDelimiter);
+		}
+
+		return text.split(",|:");
 	}
 
 	private static int sumStringArray(String[] numbers) {
