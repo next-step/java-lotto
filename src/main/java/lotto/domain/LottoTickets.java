c3c7ff8415b9dec1aa05182bf0d77a06abac5d01
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-23 12:07:12 +0900

    refactor : 멤버변수 LottoResult 제거
    https://github.com/next-step/java-lotto/pull/1600#discussion_r637475022

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -5,22 +5,16 @@ import java.util.List;
 
 public class LottoTickets {
 
-    private final String ERROR_AMOUNT_CANNOT_BE_DIVIDED = "로또 가격으로 정확히 나누어떨어지는 금액을 입력해주세요.";
-    private final String ERROR_AMOUNT_OUT_OF_RANGE = "1천원 이상, 10만원 이하의 금액을 입력해주세요.";
-    private final String TOTAL_COUNT_FORMAT = "%d개를 구매했습니다.%n";
-
     private final int PRICE_PER_TICKET = 1000;
     private final int MINIMUM_PURCHASE_AMOUNT = 1000;
     private final int MAXIMUM_PURCHASE_AMOUNT = 100000;
 
     private int purchaseAmount;
     private final List<LottoTicket> lottoTickets;
-    private LottoResult lottoResult;
 
     public LottoTickets(int amount) throws IllegalArgumentException {
         checkValidInput(amount);
         checkDividable(amount);
-        lottoResult = new LottoResult(amount);
         this.purchaseAmount = amount;
         this.lottoTickets = issueTickets(purchaseAmount / PRICE_PER_TICKET);
     }
@@ -35,6 +29,7 @@ public class LottoTickets {
     }
 
     public LottoResult matchingResultWith(WinningNumbers winningNumbers) {
+        LottoResult lottoResult = new LottoResult(purchaseAmount);
         for (LottoTicket ticket : lottoTickets) {
             MatchStatus status = ticket.matchingStatusWith(winningNumbers);
             lottoResult.updateValue(status);
@@ -45,19 +40,22 @@ public class LottoTickets {
     private void checkValidInput(int amount) throws IllegalArgumentException {
         if (amount < MINIMUM_PURCHASE_AMOUNT
                 || amount > MAXIMUM_PURCHASE_AMOUNT) {
-            throw new IllegalArgumentException(ERROR_AMOUNT_OUT_OF_RANGE);
+            throw new IllegalArgumentException();
         }
     }
 
     private void checkDividable(int amount) throws IllegalArgumentException {
         if (amount % PRICE_PER_TICKET != 0) {
-            throw new IllegalArgumentException(ERROR_AMOUNT_CANNOT_BE_DIVIDED);
+            throw new IllegalArgumentException();
         }
+    }
+
+    public int count() {
+        return lottoTickets.size();
     }
 
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(String.format(TOTAL_COUNT_FORMAT, lottoTickets.size()));
         for (LottoTicket ticket : lottoTickets) {
             stringBuilder.append(ticket.toString());
             stringBuilder.append(System.lineSeparator());
