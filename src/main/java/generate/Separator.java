8682b4a1ab95c4574eb5f1748eb1738ab60e86ed
accidentlywoo <accidentlywoo@gmail.com>
2021-05-23 15:20:33 +0900

    feat : 입력값 space trim 메소드 생성 및 분리

--- src/main/java/generate/Separator.java
+++ src/main/java/generate/Separator.java
@@ -1,5 +1,6 @@
 package generate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,8 +8,19 @@ public class Separator {
 	private static final String SPLIT_COMMA_COLON = ",|:";
 
 	public static List<String> split (String input) {
+
 		checkSpecialCharacters(input);
-		return Arrays.asList(input.split(SPLIT_COMMA_COLON));
+
+		return removeSpace(input);
+	}
+	private static List<String> removeSpace (String input) {
+		List<String> numbers = new ArrayList<>();
+
+		for (String number : input.split(SPLIT_COMMA_COLON)) {
+			numbers.add(number.trim());
+		}
+
+		return numbers;
 	}
 
 	private static void checkSpecialCharacters (String input) {
