daa3a98bbd8a167ca8d67385219db4de40d620e8
KIM GI TAE <c0433c@gmail.com>
2021-05-23 23:52:52 +0900

    test(lottoTicket): 테스트케이스 추가
    - 로또번호가 당첨번호와 몇개가 일치하는지 계산하는 기능인 calcHitCount의 테스트 케이스를 추가

--- src/test/java/lottoauto/LottoTicketTest.java
+++ src/test/java/lottoauto/LottoTicketTest.java
@@ -32,18 +32,36 @@ public class LottoTicketTest {
     @DisplayName("티켓의 번호가 당첨번호와 몇개나 맞는지 테스트")
     @Test
     void calcHitCount() {
-        List<LottoNumber> winningNumbersList = Stream.<LottoNumber>builder()
-                .add(new LottoNumber(1))
-                .add(new LottoNumber(2))
-                .add(new LottoNumber(3))
-                .add(new LottoNumber(4))
-                .add(new LottoNumber(5))
-                .add(new LottoNumber(6))
+        LottoNumbers winningNumbers = new LottoNumbers(Stream.<LottoNumber>builder()
+                .add(new LottoNumber(1)).add(new LottoNumber(2)).add(new LottoNumber(3))
+                .add(new LottoNumber(4)).add(new LottoNumber(5)).add(new LottoNumber(6))
                 .build()
-                .collect(Collectors.toList());
-        LottoNumbers winningNumbers = new LottoNumbers(winningNumbersList);
-
+                .collect(Collectors.toList()));
         lottoTicket.calcHitCount(winningNumbers);
         assertThat(lottoTicket.hitCount()).isEqualTo(new HitCount(6));
+
+        winningNumbers = new LottoNumbers(Stream.<LottoNumber>builder()
+                .add(new LottoNumber(1)).add(new LottoNumber(2)).add(new LottoNumber(3))
+                .add(new LottoNumber(4)).add(new LottoNumber(5)).add(new LottoNumber(10))
+                .build()
+                .collect(Collectors.toList()));
+        lottoTicket.calcHitCount(winningNumbers);
+        assertThat(lottoTicket.hitCount()).isEqualTo(new HitCount(5));
+
+        winningNumbers = new LottoNumbers(Stream.<LottoNumber>builder()
+                .add(new LottoNumber(1)).add(new LottoNumber(2)).add(new LottoNumber(3))
+                .add(new LottoNumber(4)).add(new LottoNumber(9)).add(new LottoNumber(10))
+                .build()
+                .collect(Collectors.toList()));
+        lottoTicket.calcHitCount(winningNumbers);
+        assertThat(lottoTicket.hitCount()).isEqualTo(new HitCount(4));
+
+        winningNumbers = new LottoNumbers(Stream.<LottoNumber>builder()
+                .add(new LottoNumber(21)).add(new LottoNumber(22)).add(new LottoNumber(23))
+                .add(new LottoNumber(24)).add(new LottoNumber(25)).add(new LottoNumber(26))
+                .build()
+                .collect(Collectors.toList()));
+        lottoTicket.calcHitCount(winningNumbers);
+        assertThat(lottoTicket.hitCount()).isEqualTo(new HitCount(0));
     }
 }
