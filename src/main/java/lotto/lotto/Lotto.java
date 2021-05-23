7436b962a03b9dc0c3d0db8c1ebf609ecca8484d
lkimilhol <lkimilhol@gmail.com>
2021-05-23 16:46:31 +0900

    refactor(Lotto): 사용되지 않는 equals 메서드 제거

--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -1,11 +1,7 @@
 package lotto.lotto;
 
 import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Lotto {
     private static final int MAX_COUNT = 6;
@@ -27,13 +23,5 @@ public class Lotto {
 
     private boolean isContain(LottoNumber number) {
         return numbers.stream().anyMatch(item -> item.equals(number));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Lotto lotto = (Lotto) o;
-        return Objects.equals(numbers, lotto.numbers);
     }
 }
