392557a7d2889d9fbf33d98f351f56cd769dda8a
ttuop <plintcsz@gmail.com>
2021-05-23 14:20:45 +0900

    refactor: 로또 티켓 생성자 파라미터 타입 변경
    * HashSet -> Set

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,20 +1,20 @@
 package lotto.domain;
 
-import java.util.HashSet;
+import java.util.Set;
 
 class LottoTicket {
 
 	private static final int LOTTO_NUMBER_COUNT = 6;
 	private static final String INVALID_LOTTO_NUMBERS_MESSAGE = "로또 번호가 유효하지 않습니다.";
-	private final HashSet<LottoNumber> lottoNumbers;
+	private final Set<LottoNumber> lottoNumbers;
 
-	LottoTicket(HashSet<LottoNumber> lottoNumbers) {
+	LottoTicket(Set<LottoNumber> lottoNumbers) {
 		validate(lottoNumbers);
 
 		this.lottoNumbers = lottoNumbers;
 	}
 
-	private void validate(HashSet<LottoNumber> lottoNumbers) {
+	private void validate(Set<LottoNumber> lottoNumbers) {
 		if (lottoNumbers == null || lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
 			throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_MESSAGE);
 		}
