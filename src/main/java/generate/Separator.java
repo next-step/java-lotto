908c02672513690301ed5552c905f57ccdf7ba38
accidentlywoo <accidentlywoo@gmail.com>
2021-05-23 15:49:14 +0900

    feat : 커스텀 구분자 로직 생성 및 함수 추출하기
    regexp match 중복 로직 추출

--- src/main/java/generate/Separator.java
+++ src/main/java/generate/Separator.java
@@ -1,33 +1,47 @@
 package generate;
 
+import util.ExceptionMessage;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Separator {
 	private static final String SPLIT_COMMA_COLON = ",|:";
+	private static final String SPLIT_CUSTOM = "//(.)\n(.*)";
 
 	public static List<String> split (String input) {
 
-		checkSpecialCharacters(input);
-
-		return removeSpace(input);
+		return removeSpace(checkSeparator(input));
 	}
-	private static List<String> removeSpace (String input) {
+	private static List<String> removeSpace (String[] input) {
 		List<String> numbers = new ArrayList<>();
 
-		for (String number : input.split(SPLIT_COMMA_COLON)) {
+		for (String number : input) {
 			numbers.add(number.trim());
 		}
 
 		return numbers;
 	}
 
-	private static void checkSpecialCharacters (String input) {
-		checkCustomSeparator(input);
+	private static String[] checkSeparator (String input) {
+		final List<Matcher> matchers = Arrays.asList(
+				Pattern.compile(SPLIT_CUSTOM).matcher(input)
+				, Pattern.compile(SPLIT_COMMA_COLON).matcher(input));
+
+		return matchSeparator(matchers);
 	}
 
-	private static void checkCustomSeparator (String input) {
+	private static String[] matchSeparator (List<Matcher> matchers) {
+		for (Matcher matcher : matchers) {
+			if (matcher.find()) {
+				String separator = matcher.group(1);
 
+				return matcher.group(2).split(separator);
+			}
+		}
+		throw new IllegalArgumentException(ExceptionMessage.SEPARATOR_EXCEPTION);
 	}
 }
