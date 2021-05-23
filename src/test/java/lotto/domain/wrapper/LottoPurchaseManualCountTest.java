cc1a84d12a021ff36570de82679d36b8fc72334f
itdar <thesockerrr@gmail.com>
2021-05-23 22:15:44 +0900

    test: LottoPurchase 클래스에서, 수동구매 개수와 수동구매 개수에 따른 자동구매 개수 테스트케이스 작성

--- src/test/java/lotto/domain/wrapper/LottoPurchaseManualCountTest.java
+++ src/test/java/lotto/domain/wrapper/LottoPurchaseManualCountTest.java
@@ -2,10 +2,20 @@ package lotto.domain.wrapper;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class LottoPurchaseManualCountTest {
 
     @Test
     void 수동구매_개수_Test() {
-
+        LottoPurchase lottoPurchase = new LottoPurchase(5000, 2);
+        assertThat(lottoPurchase.manualCount()).isEqualTo(2);
     }
+
+    @Test
+    void 수동구매_개수에_따른_자동구매_개수_Test() {
+        LottoPurchase lottoPurchase = new LottoPurchase(5000, 2);
+        assertThat(lottoPurchase.countOfAvailableAutoLotto()).isEqualTo(3);
+    }
+
 }
