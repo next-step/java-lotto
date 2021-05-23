b0b7f483b784017ac3bbb3a51ec46dfce2d54756
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-23 14:13:17 +0900

    refactor : 도메인-뷰 분리 작업 3 (InputView)
    https://github.com/next-step/java-lotto/pull/1600#discussion_r637475413

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,10 +1,8 @@
 package lotto.ui;
 
 import calculator.SplitUtil;
-import lotto.ui.OutputView;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,17 +12,7 @@ public class InputView {
     private final Pattern VALID_NUMBER_PATTERN = Pattern.compile("^-?[0-9]+$");
     private final String ERROR_NON_NUMBER = "숫자만 입력해주세요.";
     private final String ERROR_NON_POSITIVE_NUMBER = "0을 초과하는 숫자를 입력해주세요.";
-    private final String ERROR_NUMBER_OUT_OF_RANGE = "1과 45 사이의 숫자를 입력해주세요.";
     private final String ERROR_EMPTY_INPUT = "빈 값이 입력되었습니다. 다시 입력해주세요.";
-    private final String ERROR_NUMBERS_COUNT_INVALID = "중복없이 알맞은 갯수의 번호를 입력해주세요 : ";
-    public final String ERROR_AMOUNT_OUT_OF_RANGE = "1천원 이상, 10만원 이하의 금액을 입력해주세요.";
-
-    private final int MINIMUM_PURCHASE_AMOUNT = 1000;
-    private final int MAXIMUM_PURCHASE_AMOUNT = 100000;
-
-    private final int LOTTO_NUMBER_MIN_VALUE = 1;
-    private final int LOTTO_NUMBER_MAX_VALUE = 45;
-    private final int NUMBER_COUNT = 6;
 
     private Scanner scanner;
     private OutputView outputView;
@@ -47,21 +35,13 @@ public class InputView {
     private boolean checkValidAmount(String input) {
         boolean stopReceivingInput = false;
         try {
-            stopReceivingInput = checkNotNull(input) && checkIsNumber(input)
-                    && checkPositiveNumber(input) && checkAmountRange(input);
+            stopReceivingInput = checkNotNull(input)
+                    && checkIsNumber(input)
+                    && checkPositiveNumber(input);
         } catch (IllegalArgumentException e) {
             outputView.printExceptionMessage(e);
         }
         return stopReceivingInput;
-    }
-
-    private boolean checkAmountRange(String input) {
-        int amount = Integer.parseInt(input);
-        if (amount < MINIMUM_PURCHASE_AMOUNT
-                || amount > MAXIMUM_PURCHASE_AMOUNT) {
-            throw new IllegalArgumentException(ERROR_AMOUNT_OUT_OF_RANGE);
-        }
-        return true;
     }
 
     private boolean checkPositiveNumber(String input) throws IllegalArgumentException {
@@ -119,11 +99,10 @@ public class InputView {
         try {
             stopReceivingInput = false;
             String[] inputs = SplitUtil.splitByDelimiter(input);
-            stopReceivingInput = checkAllValidNumbers(inputs) && checkCountOfNumbers(inputs);
+            stopReceivingInput = checkAllValidNumbers(inputs);
         } catch (IllegalArgumentException e) {
             outputView.printExceptionMessage(e);
         }
-
         return stopReceivingInput;
     }
 
@@ -131,25 +110,6 @@ public class InputView {
         for (String input : inputs) {
             checkIsNumber(input);
             checkPositiveNumber(input);
-            checkNumberRange(input);
-        }
-        return true;
-    }
-
-    private void checkNumberRange(String input) {
-        int number = Integer.parseInt(input);
-        if (number < LOTTO_NUMBER_MIN_VALUE || number > LOTTO_NUMBER_MAX_VALUE) {
-            throw new IllegalArgumentException(ERROR_NUMBER_OUT_OF_RANGE);
-        }
-    }
-
-    private boolean checkCountOfNumbers(String[] inputs) throws IllegalArgumentException {
-        HashSet<Integer> inputToSet = new HashSet<>();
-        for (String input : inputs) {
-            inputToSet.add(Integer.parseInt(input));
-        }
-        if (inputToSet.size() != NUMBER_COUNT) {
-            throw new IllegalArgumentException(ERROR_NUMBERS_COUNT_INVALID + NUMBER_COUNT + "개");
         }
         return true;
     }
