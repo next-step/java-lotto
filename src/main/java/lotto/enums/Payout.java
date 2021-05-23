13f4fc470706b09a936532c53c0fd8929e2fce4a
fDevJc <fDevJc@gmail.com>
2021-05-23 15:57:48 +0900

    refactor: 피드백반영 가독성을 위한 표기방법 변경

--- src/main/java/lotto/enums/Payout.java
+++ src/main/java/lotto/enums/Payout.java
@@ -4,10 +4,10 @@ public enum Payout {
     MATCH_ZERO(0, 0),
     MATCH_ONE(1, 0),
     MATCH_TWO(2, 0),
-    MATCH_THREE(3, 5000),
-    MATCH_FOUR(4, 50000),
-    MATCH_FIVE(5, 1500000),
-    MATCH_SIX(6, 2000000000);
+    MATCH_THREE(3, 5_000),
+    MATCH_FOUR(4, 50_000),
+    MATCH_FIVE(5, 1_500_000),
+    MATCH_SIX(6, 2_000_000_000);
 
     private int matchNumber;
     private int payout;
@@ -25,6 +25,7 @@ public enum Payout {
         return this.matchNumber;
     }
 
+    //TODO 피드백 : 람다식으로 변경
     public static int getPayout(int matchNumber) {
         for (Payout payout : Payout.values()) {
             if (payout.getMatchNumber() == matchNumber) {
