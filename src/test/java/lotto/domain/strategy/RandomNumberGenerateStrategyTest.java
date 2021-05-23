60be7f3002bd64b3c766f2de5cfc6c772cc4b4a8
ted.kang <ted.kang@kakaocorp.com>
2021-05-24 01:51:44 +0900

    test: RandomNumberGenerateStrategy의 테스트용 구현체 변경
    주입된 값으로 생성된 LottoNumbers를 반환하도록 변경

--- src/test/java/lotto/domain/strategy/RandomNumberGenerateStrategyTest.java
+++ src/test/java/lotto/domain/strategy/RandomNumberGenerateStrategyTest.java
@@ -2,16 +2,18 @@ package lotto.domain.strategy;
 
 import lotto.domain.LottoNumbers;
 import lotto.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomNumberGenerateStrategyTest {
 
+  @DisplayName("새로운 로또 숫자들을 반환한다.")
   @Test
   void test() {
     //given
-    RandomNumberGenerateStrategy givenStrategy = new TestRandomNumberGenerateStrategy();
+    RandomNumberGenerateStrategy givenStrategy = new TestRandomNumberGenerateStrategy(TestUtil.getOneToSixLottoNumbers());
     assertThat(givenStrategy.generateNewNumbers()).isEqualTo(new LottoNumbers(TestUtil.getOneToSixLottoNumbers()));
   }
 }
--- src/test/java/lotto/domain/strategy/TestRandomNumberGenerateStrategy.java
+++ src/test/java/lotto/domain/strategy/TestRandomNumberGenerateStrategy.java
@@ -1,12 +1,20 @@
 package lotto.domain.strategy;
 
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoNumbers;
-import lotto.util.TestUtil;
+
+import java.util.List;
 
 public class TestRandomNumberGenerateStrategy implements RandomNumberGenerateStrategy {
 
+  private final List<LottoNumber> testNumbers;
+
+  public TestRandomNumberGenerateStrategy(List<LottoNumber> testNumbers) {
+    this.testNumbers = testNumbers;
+  }
+
   @Override
   public LottoNumbers generateNewNumbers() {
-    return new LottoNumbers(TestUtil.getOneToSixLottoNumbers());
+    return new LottoNumbers(testNumbers);
   }
 }
