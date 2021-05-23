0d03e527f4f6ba8fbc3e60e2708602206b97411a
younghoon <bblnotbb@yahho.com>
2021-05-23 17:40:48 +0900

    feat : add lottos that is 1st collection(list) of lotto instances and add test code of it

---
+++ src/main/java/automaticLotto/Lottos.java
@@ -0,0 +1,20 @@
+package automaticLotto;
+
+import java.util.List;
+
+public class Lottos {
+	public static final int MAXIMUM_LOTTOS_SIZE = 100;
+
+	final List<Lotto> lottos;
+
+	public Lottos(List<Lotto> lottos) {
+		this.lottos = lottos;
+		validateLottosSize();
+	}
+
+	private void validateLottosSize() {
+		if (this.lottos.size() > MAXIMUM_LOTTOS_SIZE) {
+			throw new RuntimeException("the size of lottos can not exceed 100");
+		}
+	}
+}
---
+++ src/test/java/automaticLotto/LottosTest.java
@@ -0,0 +1,26 @@
+package automaticLotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+	@Test
+	@DisplayName("로또 컬렉션의 사이즈는 100을 넘을 수 없음")
+	public void the_size_of_lottos_can_not_exceed_100() {
+		List<Integer> randomNumberList;
+		List<Lotto> lottosExceed100 = new ArrayList<>();
+		for (int i = 0; i < 140; i++) {
+			randomNumberList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+			lottosExceed100.add(new Lotto(randomNumberList));
+		}
+
+		assertThatThrownBy(() -> new Lottos(lottosExceed100))
+			.isInstanceOf(RuntimeException.class);
+	}
+}
