0f48f652ea7c8be4f4491c20d30a16765161a57e
HoonHee Jeong <jhh992000@gmail.com>
2021-05-23 23:52:13 +0900

    refactor: 랜덤로또번호 생성기의 상태값(번호리스트)을 정적변수로 변경

--- src/main/java/lotto/domain/LottoRandomNumbersGenerator.java
+++ src/main/java/lotto/domain/LottoRandomNumbersGenerator.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public class LottoRandomNumbersGenerator implements RandomNumbersGenerator {
 
-	private final List<Integer> numbers;
+	private static final List<Integer> numbers;
 
-	public LottoRandomNumbersGenerator() {
-		this.numbers = new ArrayList<>();
+	static {
+		numbers = new ArrayList<>();
 		for (int number = LottoNumber.MIN; number <= LottoNumber.MAX; number++) {
 			numbers.add(number);
 		}
@@ -17,14 +17,14 @@ public class LottoRandomNumbersGenerator implements RandomNumbersGenerator {
 
 	@Override
 	public List<Integer> generateNumbers() {
-		List<Integer> numbers = this.generateRandomNumbers(Lotto.NUMBER_LENGTH);
-		Collections.sort(numbers);
-		return numbers;
+		List<Integer> randomNumbers = this.generateRandomNumbers(Lotto.NUMBER_LENGTH);
+		Collections.sort(randomNumbers);
+		return randomNumbers;
 	}
 
 	private List<Integer> generateRandomNumbers(int length) {
-		Collections.shuffle(this.numbers);
-		return new ArrayList<>(this.numbers.subList(this.numbers.size() - length, this.numbers.size()));
+		Collections.shuffle(numbers);
+		return new ArrayList<>(numbers.subList(numbers.size() - length, numbers.size()));
 	}
 
 }
