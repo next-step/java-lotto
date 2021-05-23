dcd4104088fdb8b6becee202d0d961bfc4b87d1a
ttuop <plintcsz@gmail.com>
2021-05-23 16:43:58 +0900

    feat: CollectionUtils 클래스 구현
    * Collection element 를 다른 타입으로 변환하는 기능 추가

---
+++ src/main/java/lotto/domain/CollectionUtils.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.Collection;
+import java.util.function.Function;
+
+public class CollectionUtils {
+
+	public static <T, U> void transform(Collection<T> source, Collection<U> target, Function<T, U> function) {
+		source.forEach(arg -> target.add(function.apply(arg)));
+	}
+
+}
