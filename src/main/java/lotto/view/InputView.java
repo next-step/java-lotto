2dc4bb6318a95468d660203c3006e20aa6f0dc8e
SUNJU\SUNJU <sunju635@naver.com>
2021-05-23 20:31:10 +0900

    refactor($step3): refactor InputView

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
 
     public String inputWinningNumber() {
         scanner = new Scanner(System.in);
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
         return scanner.nextLine();
     }
 
