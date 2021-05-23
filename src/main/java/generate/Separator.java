6992d856a65d4e072bdd76c5d223d0fc6ebb311c
accidentlywoo <accidentlywoo@gmail.com>
2021-05-23 13:18:18 +0900

    test : 구분자 단위 테스트 정의

---
+++ src/main/java/generate/Separator.java
@@ -0,0 +1,12 @@
+package generate;
+
+import java.util.List;
+
+public class Separator {
+	private final String SPLIT_COMMA = ",";
+	private final String SPLIT_COLON= ":";
+
+	public static List<String> split (String s) {
+		return null;
+	}
+}
---
+++ src/test/java/generator/SeparatorTest.java
@@ -0,0 +1,39 @@
+package generator;
+
+import generate.Separator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SeparatorTest {
+	@Test
+	@DisplayName (",와 :구분자로 문자열을 전달하는 경우 구분자를 기준으로 분리한 숫자의 합을 반환")
+	public void 구분자를_컴마로_콜론으로_숫자분리(){
+		List<String> numbers = Arrays.asList("1", "2", "3");
+
+		List<String> splitComma = Separator.split("1,2,3");
+
+		assertThat(splitComma).isEqualTo(numbers);
+
+		List<String> splitColon = Separator.split("1:2:3");
+
+		assertThat(splitColon).isEqualTo(numbers);
+
+		List<String> splitCommaAndColon = Separator.split("1:2,3");
+
+		assertThat(splitCommaAndColon).isEqualTo(numbers);
+
+		List<String> splitCommaAndColon2 = Separator.split("1,2:3");
+
+		assertThat(splitCommaAndColon2).isEqualTo(numbers);
+	}
+
+	@Test
+	public void 문자사이에_커스텀구분자를_지정할_수_있다(){
+
+	}
+}
