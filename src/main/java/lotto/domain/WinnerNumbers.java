bd2cb03af4029e30141741978a5eb834bd78a8b9
HyungjuJang <dacapolife87@gmail.com>
2021-05-24 00:43:12 +0900

    feat : 당첨번호에서 보너스번호 관리를 추가

--- src/main/java/lotto/domain/WinnerNumbers.java
+++ src/main/java/lotto/domain/WinnerNumbers.java
@@ -4,25 +4,40 @@ import java.util.List;
 
 public class WinnerNumbers extends LottoTicket {
 
-    private WinnerNumbers(List<LottoNumber> winnerNumbers) {
+    private LottoNumber bonusNumber;
+
+    private WinnerNumbers(List<LottoNumber> winnerNumbers, LottoNumber bonusNumber) {
         super(winnerNumbers);
+        this.bonusNumber = bonusNumber;
     }
 
-    public static WinnerNumbers create(List<LottoNumber> winnerNumbers) {
+    public static WinnerNumbers create(List<LottoNumber> winnerNumbers, LottoNumber bonusNumber) {
         if (winnerNumbers.size() != NORMAL_COUNT_LOTTO_NUMBER) {
             throw new IllegalArgumentException("로또 당첨번호의 갯수는 6개여야합니다.");
         }
-        return new WinnerNumbers(winnerNumbers);
+        if(winnerNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("로또 당첨번호에 보너스번호가 포함되서는 안됩니다.");
+        }
+        return new WinnerNumbers(winnerNumbers, bonusNumber);
     }
 
     public LottoRank checkLottoTicket(LottoTicket lottoTicket) {
         List<LottoNumber> lottoNumbers = lottoTicket.getNumbers();
 
-        int matchCount = (int) this.getNumbers().stream()
+        int matchCount = getMatchCount(lottoNumbers);
+        boolean hasBonusNumber = hasBonusNumber(lottoNumbers);
+
+        return LottoRank.of(matchCount, hasBonusNumber);
+    }
+
+    private int getMatchCount(List<LottoNumber> lottoNumbers) {
+        return (int) this.getNumbers().stream()
                 .filter(ticketNumber -> lottoNumbers
                         .contains(ticketNumber))
                 .count();
+    }
 
-        return LottoRank.of(matchCount);
+    private boolean hasBonusNumber(List<LottoNumber> lottoNumbers) {
+        return lottoNumbers.contains(bonusNumber);
     }
 }
