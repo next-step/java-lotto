30f5e771f152790bdff95b970c19fba4a2a75b72
HoonHee Jeong <jhh992000@gmail.com>
2021-05-23 12:26:57 +0900

    refactor: ResultView 에서 사용하는 메세지 상수화

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -9,6 +9,11 @@ import java.util.Iterator;
 public class ResultView {
 
 	public static final int NONE_MATCH_COUNT = -1;
+	public static final String MESSAGE_WIN_STATISTICS = "당첨 통계";
+	public static final String MESSAGE_LINE_DELIMETER = "---------";
+	public static final String MESSAGE_NUMBER_OF_MATCHS_BY_RANK = "%d개 일치 (%d원)- %d개\n";
+	public static final String MESSAGE_PERCENTAGE_OF_REVENUE = "총 수익률은 %.2f입니다.";
+	public static final String MESSAG_RESULT_IS_LOSS = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
 	public static void printLottoResult(LottoResult lottoResult) {
 		printLottoStatistics(lottoResult);
@@ -17,8 +22,8 @@ public class ResultView {
 
 	private static void printLottoStatistics(LottoResult lottoResult) {
 		System.out.println();
-		System.out.println("당첨 통계");
-		System.out.println("---------");
+		System.out.println(MESSAGE_WIN_STATISTICS);
+		System.out.println(MESSAGE_LINE_DELIMETER);
 
 		HashMap<Rank, Integer> statMap = lottoResult.getStatMap();
 		Iterator<Rank> rankIterator = statMap.keySet().iterator();
@@ -30,7 +35,7 @@ public class ResultView {
 
 	private static void printWinner(Rank rank, HashMap<Rank, Integer> statMap) {
 		if (rank.getMatchCount() > NONE_MATCH_COUNT) {
-			System.out.printf("%d개 일치 (%d원)- %d개\n", rank.getMatchCount(), rank.getWinningMoney(), statMap.get(rank));
+			System.out.printf(MESSAGE_NUMBER_OF_MATCHS_BY_RANK, rank.getMatchCount(), rank.getWinningMoney(), statMap.get(rank));
 		}
 	}
 
@@ -38,9 +43,9 @@ public class ResultView {
 		double profitRate = lottoResult.calculateProfitRate();
 		double percentageOfRevenue = floorProfitRate(profitRate);
 
-		System.out.printf("총 수익률은 %.2f입니다.", percentageOfRevenue);
+		System.out.printf(MESSAGE_PERCENTAGE_OF_REVENUE, percentageOfRevenue);
 		if (profitRate != 0 && profitRate < 1) {
-			System.out.print("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+			System.out.print(MESSAG_RESULT_IS_LOSS);
 		}
 		System.out.println();
 	}
