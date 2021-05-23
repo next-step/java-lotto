4ab5088fa7a070d10de3e23e3fe3ec0ade3b9f02
jaewanLee <ianlee9180@gmail.com>
2021-05-23 17:28:55 +0900

    feat : InputHandler:IsEmptyInput 추가
    IsEmptyInput - string이 null이거나 빈 문자열 여부를 검사한다.

--- src/main/java/stringAddCalculator/InputHandler.java
+++ src/main/java/stringAddCalculator/InputHandler.java
@@ -3,7 +3,13 @@ package stringAddCalculator;
 public class InputHandler {
 
     String INPUT_REGEX = "^[0-9]+$";
-    
+
+    public boolean isEmptyInput(String input) {
+        if (input == null || input.length() == 0)
+            return true;
+        return false;
+    }
+
     public void illegalInputFilter(String input) {
         if (!input.matches(INPUT_REGEX)) {
             throw new RuntimeException("숫자 이외의 값 또는 음수는 사용할 수 없습니다.");
