09c40d02dcebe30590d439efd4d1fc55a391e635
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-05-23 19:49:09 +0900

    feat : 입력 문자열 split 기능
    - 커스텀 구분자로 split
    - 기본 구분자(,:)로 split

---
+++ src/test/java/calculator/SplitUtil.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SplitUtil {
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
+
+    public static String[] splitByDelimiter(String text) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        String delimiter = DEFAULT_DELIMITER;
+        if (m.find()) {
+            delimiter = m.group(1);
+            text = m.group(2);
+        }
+        return text.split(delimiter);
+    }
+
+}
