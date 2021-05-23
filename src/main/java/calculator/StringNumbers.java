fbab5a389b1adb5b1471263801ea7cb277db43d8
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-05-23 20:46:05 +0900

    feat : 입력값 합계 구하기 기능

---
+++ src/main/java/calculator/StringNumbers.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringNumbers {
+
+    private List<StringNumber> numbers;
+
+    public StringNumbers(final String[] input) {
+        numbers = new ArrayList<>();
+        for (String n : input) {
+            numbers.add(new StringNumber(n));
+        }
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (StringNumber n : numbers) {
+            sum += n.getValue();
+        }
+        return sum;
+    }
+
+    public List<StringNumber> getNumbers() {
+        return numbers;
+    }
+
+}
