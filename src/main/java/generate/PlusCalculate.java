d5421a8ffcb90858598cbed02eb5b122ccb25ad6
accidentlywoo <accidentlywoo@gmail.com>
2021-05-23 15:21:06 +0900

    feat : 덧셈 기능 구현

--- src/main/java/generate/PlusCalculate.java
+++ src/main/java/generate/PlusCalculate.java
@@ -1,7 +1,26 @@
 package generate;
 
+import domain.Number;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlusCalculate {
 	public Integer excute (String input) {
-		return null;
+		List<Number> numberList = new ArrayList<>();
+
+		for (String number : Separator.split(input)) {
+			numberList.add(new Number(number));
+		}
+
+		return plus(numberList);
+	}
+
+	private Integer plus(List<Number> numberList) {
+		Integer result = 0;
+		for (Number number : numberList) {
+			result += number.number();
+		}
+		return result;
 	}
 }
