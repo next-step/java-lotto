d6f782d707043ac5adacf01805ad623da01044a9
lenkim <joenggyu0@gmail.com>
2021-05-24 01:27:23 +0900

    refactor: stream api 적용

--- src/main/java/lottery/GenerateTicketMachine.java
+++ src/main/java/lottery/GenerateTicketMachine.java
@@ -4,14 +4,13 @@ import static lottery.util.RandomUtil.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class GenerateTicketMachine {
 
 	public Tickets create(long count) {
 		List<Ticket> tickets = new ArrayList<>();
-		for (int i = 0; i < count; i++) {
-			tickets.add(Ticket.of(generateSixNumbers(1, 45)));
-		}
+		LongStream.range(0, count).forEach(value -> tickets.add(Ticket.of(generateSixNumbers(1, 45))));
 		return Tickets.of(tickets);
 	}
 }
