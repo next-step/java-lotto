e9ec430bb292e09919f6abfbd52af09edd035daf
yjs2952 <yjs2952@naver.com>
2021-05-23 23:48:18 +0900

    feat: 수동 구매 개수와 수동 번호 개수가 다른 경우 예외가 발생한다

--- src/main/java/lotto/domain/ManualLottoNumbersGenerator.java
+++ src/main/java/lotto/domain/ManualLottoNumbersGenerator.java
@@ -4,12 +4,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ManualLottoNumbersGenerator {
+    public static final String MANUAL_COUNT_NOT_MATCH_ERROR_MESSAGE = "수동 구매 개수와 수동 번호 개수가 같아야 합니다";
     private final int manualCount;
     private final List<String> manualNumbers;
 
     public ManualLottoNumbersGenerator(int manualCount, List<String> manualNumbers) {
+        validateManualNumbersLength(manualCount, manualNumbers);
         this.manualCount = manualCount;
         this.manualNumbers = manualNumbers;
+    }
+
+    private void validateManualNumbersLength(int manualCount, List<String> manualNumbers) {
+        if (manualCount > 0 && manualNumbers == null || manualCount != manualNumbers.size()) {
+            throw new IllegalArgumentException(MANUAL_COUNT_NOT_MATCH_ERROR_MESSAGE);
+        }
     }
 
     public List<LottoNumbers> generate() {
--- src/test/java/lotto/domain/ManualLottoNumbersGeneratorTest.java
+++ src/test/java/lotto/domain/ManualLottoNumbersGeneratorTest.java
@@ -3,23 +3,53 @@ package lotto.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ManualLottoNumbersGeneratorTest {
+
+    private List<String> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = Arrays.asList("1,2,3,4,5,6", "2,3,4,5,6,7");
+    }
 
     @Test
     @DisplayName("수동으로 입력한 로또 번호를 발급한다")
     void createManualLottoNumbersTest() {
         int manualLottoCount = 2;
-        List<String> lottos = Arrays.asList("1,2,3,4,5,6", "2,3,4,5,6,7");
-        ManualLottoNumbersGenerator lottoNumbersGenerator = new ManualLottoNumbersGenerator(manualLottoCount, lottos);
+        ManualLottoNumbersGenerator lottoNumbersGenerator = new ManualLottoNumbersGenerator(manualLottoCount, numbers);
 
         List<LottoNumbers> manualLottoNumbers = lottoNumbersGenerator.generate();
         assertThat(manualLottoNumbers).hasSize(manualLottoCount);
         assertThat(manualLottoNumbers).containsExactly(LottoNumbers.of("1,2,3,4,5,6"), LottoNumbers.of("2,3,4,5,6,7"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @MethodSource("provideListForString")
+    @DisplayName("수동 구매 개수와 수동 번호 개수가 다른 경우 예외가 발생한다")
+    void manualCountMatchTest(List<String> numbers) {
+        int manualLottoCount = 3;
+        assertThatThrownBy(() -> new ManualLottoNumbersGenerator(manualLottoCount, numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideListForString() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("1,2,3,4,5,6", "2,3,4,5,6,7")),
+                Arguments.of(Collections.singletonList("1,2,3,4,5,6"))
+        );
     }
 }
