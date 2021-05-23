b7aebf2647561f38b48548d6299790aa001c7032
jaewanLee <ianlee9180@gmail.com>
2021-05-23 17:19:09 +0900

    feat : InputHandler 생성 및 illegalinputFilter 추가
    IllegalInputFilter - 숫자 이외으 값 또는 음수가 들어올경우, RuntimeException을 발생시킨다.

---
+++ src/main/java/stringAddCalculator/InputHandler.java
@@ -0,0 +1,12 @@
+package stringAddCalculator;
+
+public class InputHandler {
+
+    String INPUT_REGEX = "^[0-9]+$";
+    
+    public void illegalInputFilter(String input) {
+        if (!input.matches(INPUT_REGEX)) {
+            throw new RuntimeException("숫자 이외의 값 또는 음수는 사용할 수 없습니다.");
+        }
+    }
+}
