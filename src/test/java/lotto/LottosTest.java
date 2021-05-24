66aa179299e53a984916b466ea7eccda931cff3a
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-24 22:07:25 +0900

    test : 테스트코드로 로또 한장을 구매해 lottos의 당점 번호 갯수 조회 테스트 코드 작성

---
+++ src/test/java/lotto/LottosTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottosTest {
+    @DisplayName("로또 한장을 테스트코드로 구매하여 일치 갯수 조회 테스트")
+    @Test
+    void countLottosCollectNumber_테스트코드로_구매_로또_일치_번호_테스트() {
+        LottoBuy lottoBuy = new LottoBuy();
+        Lottos lottos = lottoBuy.buyOneRandomLotto(() -> new LottoNumber(Arrays.asList(1, 3, 4, 5, 6, 7)));
+        assertThat(lottos.isCount(1)).isTrue();
+
+        int result = lottos.compareWinNumber(Arrays.asList(1, 3, 4, 5, 6, 8));
+        assertThat(result).isEqualTo(5);
+    }
+}
