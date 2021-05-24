c5bf2bd0997a513d601887f08f894950fa150684
yevgnenll <yevgnenll@gmail.com>
2021-05-24 10:50:10 +0900

    refactor: 코드리뷰 반영
    enum 클래스의 일치 회수를 사용해 일치하는 객체를
    직관적으로 찾도록 한다

--- src/main/java/lotto/statistics/Earn.java
+++ src/main/java/lotto/statistics/Earn.java
@@ -1,5 +1,7 @@
 package lotto.statistics;
 
+import java.util.Arrays;
+
 public enum Earn {
 	THREE_MATCHED(3, 5_000),
 	FOUR_MATCHED(4,50_000),
@@ -15,20 +17,12 @@ public enum Earn {
 	private int earn;
 
 	public static Earn match(int matchCount) {
-		if (matchCount == 3) {
-			return THREE_MATCHED;
-		}
-		if (matchCount == 4) {
-			return FOUR_MATCHED;
-		}
-		if (matchCount == 5) {
-			return FIVE_MATCHED;
-		}
-		if (matchCount == 6) {
-			return SIX_MATCHED;
-		}
-		throw new IllegalArgumentException("match count 는 3, 4, 5, 6 만 사용이 가능합니다");
+		return Arrays.stream(values())
+			.filter(value -> value.matchCount == matchCount)
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("match count는 3, 4, 5, 6만 사용이 가능합니다"));
 	}
+
 
 	public int winningPrice() {
 		return earn;
