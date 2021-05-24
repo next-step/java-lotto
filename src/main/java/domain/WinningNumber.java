6661cdfe6968432f05a511280f4f1caa13b0e0ac
prodo-developer <wjdals300@gmail.com>
2021-05-25 01:25:17 +0900

    feat : 당첨 번호 기능 추가

--- src/main/java/domain/WinningNumber.java
+++ src/main/java/domain/WinningNumber.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class WinningNumber {
 
     private final List<LottoNumber> winningNumber;
-    
+
     public WinningNumber(List<LottoNumber> winningNumber) {
         this.winningNumber = winningNumber;
     }
