4cca0f9a5ca8ca898ae231f2de79f9f403949ef2
accidentlywoo <accidentlywoo@gmail.com>
2021-05-24 13:50:33 +0900

    test : 프레젠테이션 계층과 응용 계층 분리
    NumberTest 에서 진행한 validation check 이동
    PlusCalculateTest 에서 진행한 validation check 이동
    CalculatorControllerTest생서으 validation check 단위 테스트 이동

--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -9,23 +9,12 @@ import org.junit.jupiter.params.provider.ValueSource;
 import util.ExceptionMessage;
 
 class NumberTest {
-
-
 	@ParameterizedTest
-	@ValueSource(strings = {"1", "2", "3", "0"})
-	public void 숫자하나를_문자열로_입력할_경우_해당숫자를_반환한다(String number){
+	@ValueSource(ints = {1, 2, 3, 0})
+	public void 숫자하나를_문자열로_입력할_경우_해당숫자를_반환한다(int number){
 		Number singleNumbrt = new Number(number);
 
-		assertThat(Integer.valueOf(number)).isEqualTo(singleNumbrt.number());
+		assertThat(number).isEqualTo(singleNumbrt.number());
 		assertThat(new Number(number)).isEqualTo(singleNumbrt);
-	}
-
-
-	@ParameterizedTest
-	@ValueSource(strings = {"-1", "-2", "-3"})
-	public void 음수를_전달할경우_RuntimeException예외가_발생해야한다(String number){
-		assertThatThrownBy(() -> new Number(number))
-				.isInstanceOf(NumberFormatException.class)
-				.hasMessage(ExceptionMessage.NUMBER_FORMAT_EXCEPTION);
 	}
 }
--- src/test/java/generate/PlusCalculateTest.java
+++ src/test/java/generate/PlusCalculateTest.java
@@ -2,29 +2,13 @@ package generate;
 
 import domain.Number;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import util.ExceptionMessage;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PlusCalculateTest {
 
-	@Test
-	public void 빈문자열_또는_null을_입력할_경우_0을_반환해야한다(){
-		PlusCalculate zeroForNull = new PlusCalculate();
-
-		assertThat(zeroForNull.excute("")).isEqualTo(0);
-
-		assertThat(zeroForNull.excute(null)).isEqualTo(0);
-	}
-	
-	@Test
-	public void 덧셈연산_GREEN(){
-	  PlusCalculate plusCalculate = new PlusCalculate();
-
-    assertThat(plusCalculate.excute("1, 2, 3")).isEqualTo(6);
-
-		assertThat(plusCalculate.excute("5, 2, 3")).isEqualTo(10);
-
-		assertThat(plusCalculate.excute("10: 2: 3")).isEqualTo(15);
-		// 흠... 입력값 형식을 알고있어야 작성할 수 있는 테스트..... 안좋은것같다.
-	}
 }
