fd977a2f3d48c10da047803b729169ae09efdb3d
jess <jess.l@kakaocorp.com>
2021-05-25 06:23:02 +0900

    feat(LottoList): 일급 컬렉션 LottoList 추가

---
+++ src/main/java/lotto/domain/LottoList.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoList {
+    private List<Lotto> lottos;
+
+    public LottoList(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
---
+++ src/test/java/lotto/domain/LottoListTest.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoListTest {
+    @Test
+    void 로또리스트는_로또리스_프로퍼티를_갖는다(){
+        LottoList lottos = new LottoList(Lists.emptyList());
+        assertThat(lottos).hasFieldOrProperty("lottos");
+    }
+}
