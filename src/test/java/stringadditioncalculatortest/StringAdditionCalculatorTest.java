0797695a1a2ff176516ba678f43add76464eeaec
이예림 <dulfla_5@naver.com>
2021-05-24 11:41:03 +0900

    style : 테스트 사이 공백 줄 추가

--- src/test/java/stringadditioncalculatortest/StringAdditionCalculatorTest.java
+++ src/test/java/stringadditioncalculatortest/StringAdditionCalculatorTest.java
@@ -76,6 +76,7 @@ class StringAdditionCalculatorTest {
     void zeroAndColons(final String text) {
         assertThat(calculator.add(text)).isSameAs(3);
     }
+
     @DisplayName(value = "//와 \n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"//;\n1;2;3"})
