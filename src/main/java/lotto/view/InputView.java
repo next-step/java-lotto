22985af5529ab654bd745a7170e414f649907fe8
Donghui-Lee <ehdgml3206@naver.com>
2021-05-24 19:02:06 +0900

    feat: I/O 보너스 상금 관련 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,7 @@ import lotto.utils.StringUtil;
 public class InputView {
     private final static String MESSAGE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private final static String MESSAGE_WIN_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private final static String MESSAGE_BONUS_BALL = "\n보너스 볼을 입력해주세요.";
 
     private final static Scanner scanner = new Scanner(System.in);
 
@@ -18,6 +19,11 @@ public class InputView {
     public static String[] inputWinNumber() {
         System.out.println(MESSAGE_WIN_NUMBER);
         return StringUtil.split(scanner.nextLine());
+    }
+
+    public static String inputBonusBall() {
+        System.out.println(MESSAGE_BONUS_BALL);
+        return scanner.nextLine();
     }
 
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -10,6 +10,8 @@ import lotto.domain.LottoWin;
 public class ResultView {
     private final static String MESSAGE_PURCHASE_COUNT = "개를 구입하셨습니다.";
     private final static String MESSAGE_WIN_RESULT = "\n당첨 통계\n---------";
+    private final static String MESSAGE_BONUS_BALL = ", 보너스 일치";
+    private static final String MESSAGE_EMPTY = " ";
 
     public static void printPurchaseCount(int count) {
         System.out.println(count + MESSAGE_PURCHASE_COUNT);
@@ -24,8 +26,15 @@ public class ResultView {
     public static void printResult(LottoWin lottoWin) {
         System.out.println(MESSAGE_WIN_RESULT);
         for(LottoRank win : lottoWin.win().keySet()) {
-            System.out.println(win.hit() + "개 일치 (" + win.amount() + "원) - " + lottoWin.win().get(win) + "개");
+            System.out.println(win.hit() + "개 일치" + printBonus(win) + "(" + win.amount() + "원) - " + lottoWin.win().get(win) + "개");
         }
+    }
+
+    private static String printBonus(LottoRank lottoRank) {
+        if (LottoRank.SECOND == lottoRank) {
+            return MESSAGE_BONUS_BALL;
+        }
+        return MESSAGE_EMPTY;
     }
 
     public static void printProfit(int payment, LottoWin lottoWin) {
