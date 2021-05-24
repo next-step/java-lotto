76959ae0b2cb36cc044aa4fb3f44b37035f9117c
Junhee Ko <jko@rainist.com>
2021-05-24 16:04:10 +0900

    refactor: print responsibility move to ResultView class

--- src/main/java/lotto/domain/LottoRoi.java
+++ src/main/java/lotto/domain/LottoRoi.java
@@ -8,14 +8,11 @@ public final class LottoRoi {
     this.roi = roi;
   }
 
-  public void print() {
-    System.out.print("총 수익률은 " + roi + "입니다.");
+  public boolean lessThan(int i) {
+    return roi < i;
+  }
 
-    if (roi < 1) {
-      System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
-      return;
-    }
-
-    System.out.println("(기준이 1이기 때문에 결과적으로 이익이라는 의미임)");
+  public double getValue() {
+    return roi;
   }
 }
