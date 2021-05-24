2f5a414da541f0fe8e13fa149b3563785457120c
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-24 22:43:52 +0900

    test : 여러장 테스트 로또 구매 후 랭크 갯수 조회 테스트 코드 작성

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,5 +16,21 @@ public class LottoResultTest {
         Lottos lottos = lottoBuy.buyOneRandomLotto(() -> new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6)));
         LottoResult lottoResult = lottos.compareWinNumber(Arrays.asList(1, 2, 3, 8, 9, 10));
         assertThat(lottoResult.isFourth(1)).isTrue();
+    }
+
+    @DisplayName("테스트 로또를 여러장 구매하여 각 등수 갯수 조회 테스트")
+    @Test
+    void countManyLottoRank_여러장_등수_조회() {
+        Lottos lottos = new Lottos(new ArrayList<Lotto>(Arrays.asList(
+                new Lotto(() -> new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6))),
+                new Lotto(() -> new LottoNumber(Arrays.asList(1, 2, 3, 4, 5, 7))),
+                new Lotto(() -> new LottoNumber(Arrays.asList(1, 2, 3, 4, 7, 8))),
+                new Lotto(() -> new LottoNumber(Arrays.asList(1, 2, 3, 4, 7, 8))))
+        ));
+        LottoResult lottoResult = lottos.compareWinNumber(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lottoResult.isFirst(1)).isTrue();
+        assertThat(lottoResult.isSecond(1)).isTrue();
+        assertThat(lottoResult.isThird(2)).isTrue();
+        assertThat(lottoResult.isFourth(0)).isTrue();
     }
 }
