d7e625139f3fc089706e5184dd8e5e6cb577cab5
accidentlywoo <accidentlywoo@gmail.com>
2021-05-25 01:07:47 +0900

    test : 로또 번호 일치 테스트

--- src/test/java/lotto/NumberMatchTest.java
+++ src/test/java/lotto/NumberMatchTest.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class NumberMatchTest {
+   @Test
+   public void 번호일치테스트_GREEN() {
+       Integer[] winNumber = new Integer[]{1, 2, 3, 4, 5, 6};
+       Integer[] lottoNumber = new Integer[]{1, 2, 3, 4, 5, 6};
 
+       final Integer allMached = NumberMatch.matchCount(winNumber, lottoNumber);
+
+       assertThat(allMached).isEqualTo(6);
+   }
+
+    @Test
+    public void 번호일치테스트_RED() {
+        Integer[] winNumber = new Integer[]{1, 2, 3, 4, 5, 6};
+        Integer[] lottoNumber = new Integer[]{7, 8, 9, 10, 11, 12};
+
+        final Integer nothingMatched = NumberMatch.matchCount(winNumber, lottoNumber);
+
+        assertThat(nothingMatched).isEqualTo(0);
+    }
 }
