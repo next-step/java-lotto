e35e172a50d4f3f6caac18bd8ca8acc9eacaa6e2
moon_ <m3252@naver.com>
2021-05-24 12:20:12 +0900

    feat: 로또 당첨 통계 - 3개의 숫자가 일치하면 당첨금은 5000원이다. - 4개의 숫자가 일치하면 당첨금은 50000원이다. - 5개의 숫자가 일치하면 당첨금은 1500000원이다. - 6개의 숫자가 일치하면 당첨금은 2000000000원이다. - 총 수익률을 알수있다.

---
+++ src/main/java/lotto/Result.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public final class Result {
+
+    private final Lotto winningLotto;
+    private BigDecimal winnings = BigDecimal.ZERO;
+    private BigDecimal profitRate = BigDecimal.ZERO;
+
+    public Result(Number... numbers) {
+        winningLotto = new Lotto(numbers);
+    }
+
+    public void confirm(LottoList lottoList) {
+        for (int i = 0; i < lottoList.size(); i++) {
+            compareWith(lottoList.get(i), winningLotto);
+        }
+        calculateProfitRate(lottoList.size());
+    }
+
+    private void compareWith(Lotto winningLottoNumber, Lotto buyLottoNumber) {
+        int count = 0;
+        for (int i = 0; i < winningLottoNumber.size(); i++) {
+            count = winningLottoNumber.contains(buyLottoNumber.get(i)) ? count + 1 : count;
+        }
+        checkRank(count);
+    }
+
+    private void checkRank(int count){
+        if(count == 3)
+            winnings = winnings.add(LottoWinnings.FOURTH);
+        if(count == 4)
+            winnings = winnings.add(LottoWinnings.THIRD);
+        if(count == 5)
+            winnings = winnings.add(LottoWinnings.SECOND);
+        if(count == 6)
+            winnings = winnings.add(LottoWinnings.FIRST);
+    }
+
+    public BigDecimal winnings() {
+        return winnings;
+    }
+
+    private void calculateProfitRate(int purchasesCount){
+        BigDecimal totalPurchaseAmount = LottoPrice.PRICE.multiply(new BigDecimal(purchasesCount));
+        profitRate = winnings.divide(totalPurchaseAmount, 2);
+    }
+
+    public BigDecimal profitRate(){
+        return profitRate;
+    }
+}
