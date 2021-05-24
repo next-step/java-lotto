8f3b51af0e29b45c48451363bdfadb09141bca22
이예림 <dulfla_5@naver.com>
2021-05-24 11:20:58 +0900

    style : 결과 출력시 공백 추가

--- src/main/java/lotto/view/LottoAutoView.java
+++ src/main/java/lotto/view/LottoAutoView.java
@@ -48,7 +48,7 @@ public class LottoAutoView {
         System.out.println("5개 일치, 보너스 볼 일치(30000000원)- " + WinningResult.getNumberOfSecondPlace() + "개");
         System.out.println("6개 일치 (2000000000원)- " + WinningResult.getNumberOfFirstPlace() + "개");
 
-        System.out.print("총 수익률은" + String.format("%.2f", earningRate) + "입니다.");
+        System.out.print("총 수익률은 " + String.format("%.2f", earningRate) + "입니다.");
         if (earningRate < 1) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
