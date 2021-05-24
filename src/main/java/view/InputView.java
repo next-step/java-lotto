003e4b2bdf0ae0c1bd4c1a7744da7fa679cdfc40
prodo-developer <wjdals300@gmail.com>
2021-05-25 01:25:31 +0900

    feat : 로또 입력 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,17 @@
 package view;
 
+import domain.LottoNumber;
+import domain.WinningNumber;
+
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String DELIMITER = ", ";
     private static final String LOTTO_START_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_AMOUNT_VALID_MESSAGE = "구입금액은 숫자만 입력 가능합니다.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -13,6 +20,12 @@ public class InputView {
         String inputLine = SCANNER.nextLine();
         inputDigitValid(inputLine);
         return Integer.parseInt(inputLine);
+    }
+
+    public static WinningNumber winningNumber(String winningNumberString) {
+        return new WinningNumber(Arrays.stream(winningNumberString.split(DELIMITER))
+                .map(value -> new LottoNumber(Integer.parseInt(value)))
+                .collect(Collectors.toList()));
     }
 
     public static String winningNumber() {
@@ -26,5 +39,10 @@ public class InputView {
                 throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT_VALID_MESSAGE);
             }
         }
+    }
+
+    public static int bonusBall() {
+        System.out.println(LOTTO_BONUS_NUMBER_MESSAGE);
+        return SCANNER.nextInt();
     }
 }
