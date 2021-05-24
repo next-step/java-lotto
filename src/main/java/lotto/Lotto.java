f6681893398f8743dc222211ab57137f107e7299
accidentlywoo <accidentlywoo@gmail.com>
2021-05-24 18:18:33 +0900

    feat : 로또 도메인 구현
    멤버로 로또 번호 세팅, getter 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,4 +1,13 @@
 package lotto;
 
 public class Lotto {
+	private final Integer[] numbers;
+
+	public Lotto (Integer[] numbers) {
+		this.numbers = numbers;
+	}
+
+	public Integer[] numbers () {
+		return numbers;
+	}
 }
