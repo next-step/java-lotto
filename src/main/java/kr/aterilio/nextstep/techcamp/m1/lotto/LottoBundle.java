2f87b4afb53c8330ce43bdcf6af72d912c0e8a95
심정미 <lina@aterilio.kr>
2021-05-24 18:25:48 +0900

    [step3/로또(자동)] feat: 로또 구매 금액 음수 예외 처리

--- src/main/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoBundle.java
+++ src/main/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoBundle.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class LottoBundle {
 
     public static final int PRICE_PER_LOTTO = 1000;
+    public static final String MSG_ERR_PRICE_NEGATIVE = "구입 금액은 0원 이상이어야 합니다.";
 
     private final List<Lotto> lottoBundle = new ArrayList<>();
 
@@ -14,8 +15,15 @@ public class LottoBundle {
     }
 
     private void buy(int money) {
+        validateNegative(money);
         for (int i = money / PRICE_PER_LOTTO; i > 0; --i) {
             lottoBundle.add(new Lotto());
+        }
+    }
+
+    private void validateNegative(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException(MSG_ERR_PRICE_NEGATIVE);
         }
     }
 
--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package kr.aterilio.nextstep.techcamp.m1.lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
 
@@ -14,5 +16,16 @@ public class LottoTest {
     public void buyLottoTest(int money, int count) {
         LottoBundle lottoBundle = new LottoBundle(money);
         assertThat(lottoBundle.count()).isEqualTo(count);
+    }
+
+    @DisplayName("로또 구입 금액이 음수이면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-2000, -5000, -100})
+    public void buyLottoFailedTest(int money) {
+
+        assertThatThrownBy(() -> {
+            new LottoBundle(money);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("0원 이상");
     }
 }
