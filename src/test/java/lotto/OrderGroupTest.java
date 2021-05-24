3d577df7dce374a57e017840314eeb428da93b34
accidentlywoo <accidentlywoo@gmail.com>
2021-05-24 18:30:02 +0900

    test : 주문 그룹 생성 테스트

--- src/test/java/lotto/OrderGroupTest.java
+++ src/test/java/lotto/OrderGroupTest.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class OrderGroupTest {
 
+	@Test
+	public void 주문그룹_생성_테스트(){
+		int size = 3;
+
+		final OrderGroup orderGroup = new OrderGroup(size);
+
+		assertThat(orderGroup.lottos().size()).isEqualTo(size);
+
+	}
 }
