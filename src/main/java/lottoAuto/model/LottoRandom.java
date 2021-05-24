c19d0782d6b2c4fdc49d4429165a3d3c0b7d0d19
hailey <wjdguswls55@gmail.com>
2021-05-24 13:50:23 +0900

    refactor (LottoRandom) : 변수 이름 변경 및 객체 추가
    + setLotto() return 타입 : List<Lotto> -> Lottos
    + lottos -> lotto
    + lottoNumber -> lottoNumbers
    + Random 객체의 오버헤드를 감소시키기 위해 필드에 선언
    + Lotto객체를 선언과 동시에 할당

--- src/main/java/lottoAuto/model/LottoRandom.java
+++ src/main/java/lottoAuto/model/LottoRandom.java
@@ -1,4 +1,4 @@
-package lottoAuto.service;
+package lottoAuto.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -6,24 +6,22 @@ import java.util.List;
 import java.util.Random;
 
 public class LottoRandom {
+    private Random random = new Random();
     private static final int LOTTO_NUMBER = 45;
     private static final int LOTTO_NUMBER_EACH = 6;
 
-    public List<Lotto> setLotto(int amount) {
-
-        List<Lotto> lottos = new ArrayList<>();
+    public Lottos setLotto(int amount) {
+        List<Lotto> lotto = new ArrayList<>();
 
         for (int x = 0; x < amount; x++) {
-            Lotto lottoNumber = getLottoNumber();
-            lottos.add(lottoNumber);
+            Lotto lottoNumbers = getLottoNumber();
+            lotto.add(lottoNumbers);
         }
-
+        Lottos lottos = new Lottos(lotto);
         return lottos;
     }
 
     private Lotto getLottoNumber() {
-        Random random = new Random();
-        Lotto lotto = null;
         List<Integer> num = new ArrayList<>();
 
         for (int i = 0; i < LOTTO_NUMBER_EACH; i++) {
@@ -31,7 +29,7 @@ public class LottoRandom {
         }
         Collections.shuffle(num);
         Collections.sort(num);
-        lotto = new Lotto(num);
+        Lotto lotto = new Lotto(num);
 
         return lotto;
     }
