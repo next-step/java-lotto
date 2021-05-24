d7f5aab6dd3df249a9af508abe905724ac5aa11c
백승열 <beck33333@naver.com>
2021-05-24 22:09:41 +0900

    refactor: FastScanner 제거

--- src/main/java/step3/ui/InputView.java
+++ src/main/java/step3/ui/InputView.java
@@ -1,16 +1,21 @@
 package step3.ui;
 
+import java.util.Scanner;
+
 public class InputView {
 
-    public static FastScanner fs = FastScanner.of();
 
     public static int purchaseAmount() {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("구입금액을 입력해 주세요.");
-        return fs.nextInt();
+        return sc.nextInt();
     }
 
     public static String WinningNumbers() {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return fs.nextLine();
+        return sc.nextLine();
     }
 }
