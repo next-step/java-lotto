8ae9bb6722d69cb3c3069992ea4c02c68272a96d
ohgillwhan <soora_genius@naver.com>
2021-05-24 15:36:00 +0900

    feat : 입출력을 Application에 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import lotto.domain.Game;
 import lotto.domain.LottoWon;
 import lotto.domain.PositiveNumber;
 import lotto.domain.rank.LottoRanks;
+import lotto.domain.ticket.LottoTickets;
 import lotto.view.InfoView;
 import lotto.view.InputView;
 import lotto.view.MatchResultView;
@@ -12,8 +13,9 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         PositiveNumber money = inputView.money();
+        LottoTickets manualLottoTickets = inputView.manualLottoTickets();
 
-        Game game = Game.createByMoney(money);
+        Game game = Game.createWithManualTicketByMoney(manualLottoTickets, money);
         InfoView infoView = new InfoView(game);
 
         infoView.countOfTicket();
