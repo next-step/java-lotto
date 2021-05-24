f4ac09fd3b29b8b0b8df42639dc0b04930258131
HoonHee Jeong <jhh992000@gmail.com>
2021-05-24 21:35:29 +0900

    refactor: 로또 구매 메서드에서 구매한 로또리스트를 생성자로 초기화하여 반환하도록 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,15 +1,10 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
 
 	private final List<Lotto> lottos;
-
-	public Lottos() {
-		this.lottos = new ArrayList<>();
-	}
 
 	public Lottos(List<Lotto> lottos) {
 		this.lottos = lottos;
