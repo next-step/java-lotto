76f2a833a38bcb1168d0d152eaa4b54abd618df4
moon_ <m3252@naver.com>
2021-05-24 23:38:33 +0900

    feat: 로또 진행을 위한 객체 추가

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public final class LottoApplication {
+
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.start();
+    }
+}
