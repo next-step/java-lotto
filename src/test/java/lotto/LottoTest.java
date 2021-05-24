604c665073f53ba85cb9a297acb98a96df871d92
accidentlywoo <accidentlywoo@gmail.com>
2021-05-24 18:17:39 +0900

    test : 로또 도메인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
+	@Test
+	public void 로또번호생성테스트(){
+		Integer[] numbers = new Integer[]{1, 2, 3, 4, 5, 6};
 
+		final Lotto lotto = new Lotto(numbers);
+
+		assertThat(lotto.numbers()).isEqualTo(numbers);
+	}
 }
