c7ccd881732e1854c833542d389c369e5494f1a7
hailey <wjdguswls55@gmail.com>
2021-05-24 14:39:29 +0900

    refactor (LottoAutoView) : 책임 분산
    + 상속 받았던 inputview를 필드로 옮겨주었습니다.

--- src/main/java/lottoAuto/view/LottoAutoResultView.java
+++ src/main/java/lottoAuto/view/LottoAutoResultView.java
@@ -7,10 +7,12 @@ import lottoAuto.vo.LottoResultVo;
 
 import java.util.*;
 
-public class LottoAutoResultView extends LottoAutoInputView {
+public class LottoAutoResultView {
+    private LottoAutoInputView lottoAutoInputView = new LottoAutoInputView();
+
     public int showAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        int amount = inputPrice();
+        int amount = lottoAutoInputView.inputPrice();
         System.out.println(amount + "개를 구매했습니다.");
         return amount;
     }
@@ -23,14 +25,14 @@ public class LottoAutoResultView extends LottoAutoInputView {
 
     public String showWinningNumber() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        String winningLottoString = inputWinningNumber();
+        String winningLottoString = lottoAutoInputView.inputWinningNumber();
 
         return winningLottoString;
     }
 
     public int showBonusBall() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        int bonusBall = inputBonusBall();
+        int bonusBall = lottoAutoInputView.inputBonusBall();
         return bonusBall;
     }
 
