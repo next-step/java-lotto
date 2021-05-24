4f431a88221b8491bc4049eb4c4c16522bfb84b5
accidentlywoo <accidentlywoo@gmail.com>
2021-05-25 00:36:42 +0900

    refactor : 중복제거, 테스트 케이스와 기대값 분리
    빠따 멈춰!

--- src/test/java/generator/SeparatorTest.java
+++ src/test/java/generator/SeparatorTest.java
@@ -5,6 +5,8 @@ import generate.Separator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import util.ExceptionMessage;
 
 import java.util.ArrayList;
@@ -17,25 +19,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 class SeparatorTest {
 	private final List<String> numberList = Arrays.asList("1", "2", "3");
 
-	@Test
+	@ParameterizedTest
+	@ValueSource(strings = {":", ","})
 	@DisplayName (",와 :구분자로 문자열을 전달하는 경우 구분자를 기준으로 분리한 숫자의 합을 반환")
-	public void 구분자를_컴마로_콜론으로_숫자분리(){
+	public void 구분자를_컴마로_콜론으로_숫자분리(String separator){
 
-		List<String> splitComma = Separator.split("1,2,3");
+		List<String> splitNumber = Separator.split("1" + separator + "2" + separator + "3");
 
-		assertThat(numberList).isEqualTo(splitComma);
-
-		List<String> splitColon = Separator.split("1:2:3");
-
-		assertThat(numberList).isEqualTo(splitColon);
-
-		List<String> splitCommaAndColon = Separator.split("1:2,3");
-
-		assertThat(numberList).isEqualTo(splitCommaAndColon);
-
-		List<String> splitCommaAndColon2 = Separator.split("1,2:3");
-
-		assertThat(numberList).isEqualTo(splitCommaAndColon2);
+		assertThat(splitNumber).isEqualTo(numberList);
 	}
 
 	@Test
@@ -53,14 +44,14 @@ class SeparatorTest {
 
 	@Test
 	public void 문자사이에_커스텀구분자를_지정할_수_있다(){
-		assertThat(numberList).isEqualTo(Separator.split("//;\n1;2;3"));
+		assertThat(Separator.split("//;\n1;2;3")).isEqualTo(numberList);
 
-		assertThat(numberList).isEqualTo(Separator.split("// \n1 2 3"));
+		assertThat(Separator.split("// \n1 2 3")).isEqualTo(numberList);
 
 //		assertThat(numberList).isEqualTo(Separator.split("//.\n1.2.3")); ???
 
 //		assertThat(numberList).isEqualTo(Separator.split("//*\n1*2*3")); ??? 머선 129
 
-		assertThat(numberList).isEqualTo(Separator.split("//'\n1'2'3"));
+		assertThat(Separator.split("//'\n1'2'3")).isEqualTo(numberList);
 	}
 }
