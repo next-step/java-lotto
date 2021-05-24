4c2382371c42b14243c29d0bb45a699825c69a5f
wrallee <wrallee@naver.com>
2021-05-25 00:51:12 +0900

    feat(lottos): 금액만큼 로또 여러개가 발급되는 기능

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+	private static final int LOTTO_PRICE = 1000;
+
+	private final List<Lotto> lottos;
+
+	public Lottos(int money) {
+		validateMoney(money);
+
+		lottos = new ArrayList<>();
+
+		int lottoCount = money / LOTTO_PRICE;
+		for (int i = 0; i < lottoCount; i++) {
+			lottos.add(new Lotto(LottoUtils.getRandomNumbers()));
+		}
+	}
+
+	public int count() {
+		return lottos.size();
+	}
+
+	private void validateMoney(int money) {
+		if (money < LOTTO_PRICE) {
+			throw new IllegalArgumentException("Not enough money to buy a lotto.");
+		}
+	}
+}
---
+++ src/test/java/lotto/LottosTest.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+
+	@Test
+	void lottos_발급_테스트() {
+		Lottos lottos1 = new Lottos(10999);
+		Assertions.assertThat(lottos1.count()).isEqualTo(10);
+		Lottos lottos2 = new Lottos(254846);
+		Assertions.assertThat(lottos2.count()).isEqualTo(254);
+	}
+
+	@Test
+	void lottos_발급_실패_테스트() {
+		Assertions.assertThatThrownBy(() -> new Lottos(999))
+			.isInstanceOf(RuntimeException.class);
+	}
+}
