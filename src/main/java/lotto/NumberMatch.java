c8bc31621cb3bc76b581ab5433d10691cdde6fd4
accidentlywoo <accidentlywoo@gmail.com>
2021-05-25 01:08:31 +0900

    feat : 로또 번호 매칭 카운트 로직 구현

--- src/main/java/lotto/NumberMatch.java
+++ src/main/java/lotto/NumberMatch.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class NumberMatch {
+    public static Integer matchCount(Integer[] winNumber, Integer[] lottoNumber) {
+        Integer matchCount = 0;
+        for (Integer number : winNumber) {
+            matchCount = Arrays.asList(lottoNumber).contains(number) ? matchCount+1 : matchCount;
+        }
+        return matchCount;
+    }
 }
