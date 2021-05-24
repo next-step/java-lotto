7a7be9e6c5c87f604ecd1c4d1bce3ecf91119193
백승열 <beck33333@naver.com>
2021-05-24 22:10:49 +0900

    refactor: 같은 로또 구하기 간단하게 개선

--- src/main/java/step3/domain/SameLottoChecker.java
+++ src/main/java/step3/domain/SameLottoChecker.java
@@ -1,40 +1,23 @@
 package step3.domain;
 
-import step3.domain.Lotto;
+import java.util.Set;
 
 public class SameLottoChecker {
 
-
-    private static int left, right;
-    private static int count;
-
     public static int countSameLottoNum(Lotto l1, Lotto l2) {
-        count = 0;
-        left = 0;
-        right = 0;
-        l1.sort();
-        l2.sort();
+        int count = 0;
 
-        while (left < l1.length() && right < l2.length()) {
-            findCount(l1, l2);
+        Set<LottoNumber> anotherLotto = l2.getLottoNumbers();
+        for (LottoNumber another : anotherLotto) {
+            count = calculateCount(l1, count, another);
         }
-
         return count;
     }
 
-    private static void findCount(Lotto l1, Lotto l2) {
-        if (left < 6 && l1.findLottoNumByIdx(left) < l2.findLottoNumByIdx(right)) {
-            left++;
-        }
-
-        if (right < 6 && l1.findLottoNumByIdx(left) > l2.findLottoNumByIdx(right)) {
-            right++;
-        }
-
-        if (left < 6 && right < 6 && l1.findLottoNumByIdx(left).equals(l2.findLottoNumByIdx(right))) {
-            left++;
-            right++;
+    private static int calculateCount(Lotto l1, int count, LottoNumber another) {
+        if (l1.findLottoNum(another)) {
             count++;
         }
+        return count;
     }
 }
--- src/test/java/step3/SameLottoCheckerTest.java
+++ src/test/java/step3/SameLottoCheckerTest.java
@@ -6,6 +6,7 @@ import step3.domain.Lotto;
 import step3.domain.SameLottoChecker;
 
 import java.util.Arrays;
+import java.util.TreeSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,23 +17,19 @@ class SameLottoCheckerTest {
     @Test
     @DisplayName("3개의 일치 하는 로또 번호를 가지는 경우")
     void hasThreeSameLotto() {
+        Lotto given = new Lotto(new TreeSet<>(Arrays.asList(39, 19, 26, 34, 36, 31)));
+        Lotto result = new Lotto(new TreeSet<>(Arrays.asList(1, 3, 39, 19, 26, 2)));
 
-        Lotto given = new Lotto(Arrays.asList(39,19,26,34,36,31));
-
-
-        Lotto result = new Lotto(Arrays.asList(1,3,39,19,26,2));
-
-        assertThat(SameLottoChecker.countSameLottoNum(given,result)).isEqualTo(3);
+        assertThat(SameLottoChecker.countSameLottoNum(given, result)).isEqualTo(3);
     }
 
     @Test
     @DisplayName("6개의 일치 하는 로또 번호를 가지는 경우")
     void hasSixSameLotto() {
+        Lotto given = new Lotto(new TreeSet<>(Arrays.asList(40, 41, 42, 43, 44, 45)));
+        Lotto result = new Lotto(new TreeSet<>(Arrays.asList(45, 42, 43, 40, 44, 41)));
 
-        Lotto given = new Lotto(Arrays.asList(40,41,42,43,44,45));
-        Lotto result = new Lotto(Arrays.asList(45,42,43,40,44,41));
-
-        assertThat(SameLottoChecker.countSameLottoNum(given,result)).isEqualTo(6);
+        assertThat(SameLottoChecker.countSameLottoNum(given, result)).isEqualTo(6);
     }
 
 }
