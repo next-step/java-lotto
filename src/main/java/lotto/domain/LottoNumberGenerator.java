7f69dc16883e390af2b356b6eafbd78820ab9557
lisam <lisam333@gmail.com>
2021-05-24 10:26:40 +0900

    refactor(Lotto): 리팩토링
    - 메서드 레퍼런스로 변경
    - 로또 번호와 사이즈에 대한 밸리데이션 추가

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
 public class LottoNumberGenerator {
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 45;
-    public static final List<Integer> SAMPLE = IntStream.rangeClosed(START_NUMBER, END_NUMBER)
+    private static final List<Integer> SAMPLE = IntStream.rangeClosed(START_NUMBER, END_NUMBER)
             .boxed()
             .collect(Collectors.toList());
 
@@ -19,7 +19,7 @@ public class LottoNumberGenerator {
     public static List<Integer> get() {
         List<Integer> shuffle = new ArrayList<>(SAMPLE);
         Collections.shuffle(shuffle);
-        shuffle = shuffle.subList(0, Lotto.LOTTO_SIZE);
+        shuffle = shuffle.subList(0, Lotto.SIZE);
         shuffle.sort(Integer::compareTo);
         return shuffle;
     }
