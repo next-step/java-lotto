ea6ccdf0b706821f53c016a1189775cd651add45
박소정 <psj6414@naver.com>
2021-05-24 21:04:09 +0900

    refactor: winningNumbers 검증부분 메소드 분리

--- src/main/java/step3/utils/ValidationUtils.java
+++ src/main/java/step3/utils/ValidationUtils.java
@@ -1,7 +1,12 @@
 package step3.utils;
 
+import java.util.List;
+
 public class ValidationUtils {
     private static final String MATCH_PRICE = "^[0-9\\,]*$";
+    private static final String WINNING_NUMBER_COMMA_EXCEPTION = "당첨번호는 쉼표(,)로 구분되어야합니다.";
+    private static final String WINNING_NUMBER_NULL_EXCEPTION = "당첨번호를 입력해주세요.";
+    private static final String WINNING_NUMBER_SIX_DIGIT_EXCEPTION = "당첨번호는 6자리 숫자로 구성되어야합니다.";
 
     private ValidationUtils() {
         throw new AssertionError();
@@ -26,5 +31,19 @@ public class ValidationUtils {
             return false;
         }
         return true;
+    }
+
+    public static  List<Integer> validWinningNumbers(String winningNumbers) {
+        if (!ValidationUtils.isNullCheck(winningNumbers)) {
+            throw new RuntimeException(WINNING_NUMBER_NULL_EXCEPTION);
+        }
+        if (!ValidationUtils.validWinningNumbersComma(winningNumbers)) {
+            throw new RuntimeException(WINNING_NUMBER_COMMA_EXCEPTION);
+        }
+        List<Integer> winningNumberList = StringUtils.processStringNumbers(winningNumbers);
+        if (!ValidationUtils.validWinningNumbersDigit(winningNumberList.size())) {
+            throw new RuntimeException(WINNING_NUMBER_SIX_DIGIT_EXCEPTION);
+        }
+        return winningNumberList;
     }
 }
--- src/main/java/step3/winning/WinningNumbers.java
+++ src/main/java/step3/winning/WinningNumbers.java
@@ -2,29 +2,14 @@ package step3.winning;
 
 import step3.lotto.LottoNumber;
 import step3.lotto.LottoTicket;
-import step3.utils.StringUtils;
 import step3.utils.ValidationUtils;
-
 import java.util.List;
 
 public class WinningNumbers {
-    private static final String WINNING_NUMBER_COMMA_EXCEPTION = "당첨번호는 쉼표(,)로 구분되어야합니다.";
-    private static final String WINNING_NUMBER_NULL_EXCEPTION = "당첨번호를 입력해주세요.";
-    private static final String WINNING_NUMBER_SIX_DIGIT_EXCEPTION = "당첨번호는 6자리 숫자로 구성되어야합니다.";
-
     private LottoTicket winningNumbers;
 
     public WinningNumbers(String inputWinningNumbers) {
-        if (!ValidationUtils.isNullCheck(inputWinningNumbers)) {
-            throw new RuntimeException(WINNING_NUMBER_NULL_EXCEPTION);
-        }
-        if (!ValidationUtils.validWinningNumbersComma(inputWinningNumbers)) {
-            throw new RuntimeException(WINNING_NUMBER_COMMA_EXCEPTION);
-        }
-        List<Integer> winningNumbers = StringUtils.processStringNumbers(inputWinningNumbers);
-        if (!ValidationUtils.validWinningNumbersDigit(winningNumbers.size())) {
-            throw new RuntimeException(WINNING_NUMBER_SIX_DIGIT_EXCEPTION);
-        }
+        List<Integer> winningNumbers = ValidationUtils.validWinningNumbers(inputWinningNumbers);
         this.winningNumbers = new LottoTicket(winningNumbers);
     }
 
