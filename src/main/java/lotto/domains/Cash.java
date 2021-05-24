e50b003feccf5856bbfd5afb7bee6554e1ff745d
fDevJc <fDevJc@gmail.com>
2021-05-24 23:21:34 +0900

    test: 피드백반영 불필요한 Cash 파라미터 삭제

--- src/main/java/lotto/domains/Cash.java
+++ src/main/java/lotto/domains/Cash.java
@@ -1,7 +1,7 @@
 package lotto.domains;
 
 public class Cash {
-    private static final int MINIMUM_AMOUNT = 1000;
+    private static final int LOTTO_PRICE = 1000;
     private final int amount;
 
     public Cash(int amount) {
@@ -9,12 +9,12 @@ public class Cash {
         this.amount = amount;
     }
 
-    public int numberOfPurchasesAvailable(Cash cash) {
-        return this.amount / cash.amount;
+    public int numberOfPurchasesAvailable() {
+        return this.amount / LOTTO_PRICE;
     }
 
     private void isGreaterThanZero(int amount){
-        if (amount < MINIMUM_AMOUNT){
+        if (amount < LOTTO_PRICE){
             throw new IllegalArgumentException("1000 보다 큰숫자를 입력해주세요");
         }
     }
