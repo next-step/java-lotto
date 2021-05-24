c475c41686080eb7df7aadb12d44a58dd3ceb2aa
prodo-developer <wjdals300@gmail.com>
2021-05-25 01:22:50 +0900

    feat : 2등(보너스 볼) 기능 추가

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,8 +1,5 @@
 import controller.LottoGame;
-import domain.LottoMachine;
-import domain.LottoResult;
-import domain.LottoTickets;
-import domain.WinningNumber;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
@@ -15,8 +12,12 @@ public class LottoApplication {
         OutputView.lottoStatus(lottoTickets);
         // 당첨 로또 생성
         WinningNumber winningNumber = lottoGame.generateWinningNumber(InputView.winningNumber(), lottoMachine);
+
+        // 보너스 볼 생성
+        LottoNumber bonusBall = lottoGame.generateBonusBall(InputView.bonusBall(), lottoMachine, winningNumber);
+
         // 로또 게임 결과 생성
-        LottoResult lottoResult = lottoGame.result(lottoTickets, winningNumber);
+        LottoResult lottoResult = lottoGame.result(lottoTickets, winningNumber, bonusBall);
         OutputView.LottoEnd(lottoResult);
     }
 }
