ad105485bf082b2ef633b739e44b941773de13fb
ttuop <plintcsz@gmail.com>
2021-05-25 22:23:39 +0900

    refactor: 로또 서비스 삭제

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,18 +1,23 @@
 package lotto;
 
 import lotto.domain.LottoReport;
+import lotto.domain.LottoStore;
+import lotto.domain.LottoVendor;
+import lotto.domain.Money;
 import lotto.domain.UserLotto;
 
 public class LottoController {
 
-	private final LottoService lottoService = new LottoService();
-
 	public UserLotto buyLotto(String money) {
-		return lottoService.buyLotto(money);
+		LottoStore lottoStore = new LottoStore();
+
+		return lottoStore.buy(new Money(money));
 	}
 
 	public LottoReport report(String winningLottoNumbers, UserLotto userLotto) {
-		return lottoService.report(winningLottoNumbers, userLotto);
+		LottoVendor lottoVendor = new LottoVendor(winningLottoNumbers, "7");
+
+		return lottoVendor.report(userLotto.lottoTickets());
 	}
 
 }
