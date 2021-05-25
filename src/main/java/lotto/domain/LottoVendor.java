2c9d163cf373d0f2e4ba0cb8606abcccc040514d
ttuop <plintcsz@gmail.com>
2021-05-25 22:59:55 +0900

    fix: 로또 사업자에서 2등 로또 포인트 반환되지 않는 버그 수정

--- src/main/java/lotto/domain/LottoVendor.java
+++ src/main/java/lotto/domain/LottoVendor.java
@@ -54,14 +54,14 @@ public class LottoVendor {
 	 */
 	private LottoPoint lottoPoint(LottoTicket lottoTicket, int matchCount) {
 		if (matchCount == SECOND.matchCount()) {
-			new LottoPoint(matchCount, lottoTicket.matchNumber(bonusNumber));
+			return new LottoPoint(matchCount, lottoTicket.matchNumber(bonusNumber));
 		}
 
 		return new LottoPoint(matchCount);
 	}
 
 	private int matchCount(LottoTicket lottoTicket) {
-		return LottoTicket.compare(lottoTicket, winningLottoTicket);
+		return LottoTicket.countDuplicates(lottoTicket, winningLottoTicket);
 	}
 
 }
