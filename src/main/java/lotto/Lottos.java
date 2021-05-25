ccbcd023148fda7e20de01f245d5b9ba7596c85d
wrallee <wrallee@naver.com>
2021-05-25 20:36:33 +0900

    refactor(lottos): values 값을 조작 불가하도록 변경

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static java.util.Collections.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +23,7 @@ public class Lottos {
 	}
 
 	public List<Lotto> values() {
-		return values;
+		return unmodifiableList(values);
 	}
 
 	public int count() {
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class LottosTest {
@@ -8,14 +9,23 @@ public class LottosTest {
 	@Test
 	void lottos_발급_테스트() {
 		Lottos lottos1 = new Lottos(new Money(10999));
-		Assertions.assertThat(lottos1.count()).isEqualTo(10);
+		assertThat(lottos1.count()).isEqualTo(10);
 		Lottos lottos2 = new Lottos(new Money(254846));
-		Assertions.assertThat(lottos2.count()).isEqualTo(254);
+		assertThat(lottos2.count()).isEqualTo(254);
 	}
 
 	@Test
 	void lottos_발급_실패_테스트() {
-		Assertions.assertThatThrownBy(() -> new Lottos(new Money(999)))
+		assertThatThrownBy(() -> new Lottos(new Money(999)))
 			.isInstanceOf(RuntimeException.class);
+	}
+
+	@Test
+	void lottos_밸류값_불변_테스트() {
+		Lottos lottos = new Lottos(new Money(5000));
+		assertThatThrownBy(() -> lottos.values().add(new Lotto()))
+			.isInstanceOf(UnsupportedOperationException.class);
+		assertThatThrownBy(() -> lottos.values().remove(0))
+			.isInstanceOf(UnsupportedOperationException.class);
 	}
 }
