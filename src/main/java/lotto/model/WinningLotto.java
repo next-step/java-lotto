e1d1dbf3366defc524434cff4ddae1eba85a7c1c
Miot2J <ck96117@gmail.com>
2021-05-25 20:10:33 +0900

    refactor: WinningLotto 가 lottoNumbers를 가지는 대신 Lotto객체를 가지도록 변경
    검증된 Lotto를 사용함으로써 불필요한 검증로직을 없앴음

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,31 +1,18 @@
 package lotto.model;
 
-import java.util.Collections;
 import java.util.Set;
 
-import static lotto.model.Lotto.*;
-
 public class WinningLotto {
-    private final Set<LottoNumber> lottoNumbers;
+    private final Lotto winningLotto;
     private final LottoNumber bonusBall;
 
     public WinningLotto(Set<LottoNumber> lottoNumbers, LottoNumber bonusBall) {
-        validate(lottoNumbers, bonusBall);
-        this.lottoNumbers = lottoNumbers;
+        this.winningLotto = new Lotto(lottoNumbers);
         this.bonusBall = bonusBall;
     }
 
-    private void validate(Set<LottoNumber> lottoNumbers, LottoNumber bonusBall) {
-        if (lottoNumbers.size() != CORRECT_LOTTO_NUMBERS_SIZE) {
-            throw new IllegalArgumentException(NOT_CORRECT_LOTTO_NUMBERS_MESSAGE);
-        }
-        if (lottoNumbers.contains(bonusBall)) {
-            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBERS_MESSAGE);
-        }
-    }
-
     public Set<LottoNumber> getLottoNumbers() {
-        return Collections.unmodifiableSet(lottoNumbers);
+        return winningLotto.getLottoNumbers();
     }
 
     public LottoNumber getBonusBall() {
