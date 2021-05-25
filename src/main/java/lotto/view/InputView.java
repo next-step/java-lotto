3d606eae308249a177e3c10fcbab203db0b0f2ac
HyungjuJang <dacapolife87@gmail.com>
2021-05-25 11:37:12 +0900

    feat : 수동로또 갯수와 번호 입력인터페이스 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import lotto.domain.Money;
 import utils.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +11,8 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String MESSAGE_QUESTION_HOW_MANY_BUY = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_QUESTION_HOW_MANY_MANUAL = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MESSAGE_QUESTION_MANUAL_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String MESSAGE_QUESTION_WINNER_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String MESSAGE_QUESTION_BONUS_NUMBER = "보너스번호를 입력해 주세요.";
 
@@ -37,5 +40,30 @@ public class InputView {
         System.out.println(MESSAGE_QUESTION_BONUS_NUMBER);
         int inputBonusNumber = scanner.nextInt();
         return inputBonusNumber;
+    }
+
+    public int inputCountOfManualLottoTicket() {
+        System.out.println(MESSAGE_QUESTION_HOW_MANY_MANUAL);
+        int inputCountOfManualLottoTicket = Integer.parseInt(scanner.nextLine());
+
+        return inputCountOfManualLottoTicket;
+    }
+
+    public List<List<String>> inputManualNumbers(int countOfManualLottoTicket) {
+        List<List<String>> lottoNumbers = new ArrayList<>();
+
+        for (int i = 0; i < countOfManualLottoTicket; i++) {
+            List<String> manualNumber = inputManualNumber();
+            lottoNumbers.add(manualNumber);
+
+        }
+        return lottoNumbers;
+    }
+
+    private List<String> inputManualNumber() {
+        System.out.println(MESSAGE_QUESTION_MANUAL_NUMBER);
+        String inputWinnerNumber = scanner.nextLine();
+        String[] winnerNumbers = StringUtils.split(inputWinnerNumber);
+        return Arrays.asList(winnerNumbers);
     }
 }
