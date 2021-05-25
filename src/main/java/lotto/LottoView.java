dba144ae74cfafb5f6f9d31384ec6cf4e31f2e47
ttuop <plintcsz@gmail.com>
2021-05-25 23:00:39 +0900

    feat: 로또 뷰에 사용자에게 보너스 볼 요청하는 메시지 추가

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -11,6 +11,7 @@ public class LottoView {
 	public static final String EARNING_RESULT_MESSAGE_FORMAT = "총 수익률은 %s입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)";
 	public static final String STATISTIC_HEADER_MESSAGE =
 		LINE_SEPARATOR + "당첨 통계" + LINE_SEPARATOR + "---------" + LINE_SEPARATOR;
+	public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
 	public String inputMoneyView() {
 		return INPUT_MONEY_MESSAGE;
@@ -22,6 +23,10 @@ public class LottoView {
 
 	public String inputWinningLottoNumbers() {
 		return INPUT_WINNING_LOTTO_NUMBERS_MESSAGE;
+	}
+
+	public String inputBonusNumber() {
+		return INPUT_BONUS_NUMBER_MESSAGE;
 	}
 
 	public String lottoReportView(LottoReport lottoReport) {
