728cfb10aa7410e991900910cee6635c8d1c6588
lenkim <joenggyu0@gmail.com>
2021-05-26 03:38:26 +0900

    feat: 2등에게 주어지는 보너스볼 기능 추가

--- src/main/java/lottery/InfoCenter.java
+++ src/main/java/lottery/InfoCenter.java
@@ -1,23 +1,29 @@
 package lottery;
 
+import static java.util.Objects.*;
 import static java.util.stream.Collectors.*;
-
-import java.util.stream.Collectors;
 
 public class InfoCenter {
 
-	private final int MATCH_THREE_NUMBER = 3;
 	private Ticket lastWeekWinningTicket;
+	private int bonus;
 
 	public void setLastWeekWinningTicket(Ticket lastWeekWinningTicket) {
 		this.lastWeekWinningTicket = lastWeekWinningTicket;
 	}
 
+	void setBonusNumber(int number) {
+		this.bonus = number;
+	}
+
 	public Result confirmTicket(Tickets buyerTickets) {
+		requireNonNull(lastWeekWinningTicket, "지난 주 티켓이 없습니다.");
+		requireNonNull(bonus, "보너스 볼이 없습니다.");
+
 		return new Result(LotteryMatchTypeMap.of(buyerTickets.getValues().stream()
-			.map(ticket -> ticket.numbers().matchCountWith(lastWeekWinningTicket.numbers()))
-			.filter(matchCount -> matchCount >= MATCH_THREE_NUMBER)
-			.collect(Collectors.groupingBy(LotteryMatchType::fromInteger, summingInt(a -> 1)))));
+			.map(ticket -> ticket.numbers().getMatchTypeWith(lastWeekWinningTicket.numbers(), bonus))
+			.filter(matchType -> LotteryMatchType.MISS_MATCH != matchType)
+			.collect(groupingBy(a -> a, summingInt(a -> 1)))));
 	}
 
 	public Ticket lastWeekWinningNumbers() {
