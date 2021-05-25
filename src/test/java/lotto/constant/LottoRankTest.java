09478918982f77a9d57d75e7c208ee7fb891c9ac
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-05-25 16:13:29 +0900

    style(LottoRankTest.java): 포맷팅

--- src/test/java/lotto/constant/LottoRankTest.java
+++ src/test/java/lotto/constant/LottoRankTest.java
@@ -1,10 +1,10 @@
 package lotto.constant;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * LottoRank 열거형의 값 확인 및 기능 테스트
