401829ec4a4434dd9450c564095303d33ae80bdf
심정미 <lina@aterilio.kr>
2021-05-25 17:43:28 +0900

    [step3/로또(자동)] refactor: 테스트 함수명 변경

--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
@@ -14,7 +14,7 @@ public class LottoTest {
     @DisplayName("로또 구입 금액을 입력받으면 로또를 구매한다. (1000원당 1장)")
     @ParameterizedTest
     @CsvSource(value = {"3000,3", "14000,14", "8000,8"})
-    public void buyLottoTest(int money, int count) {
+    public void buyLotto(int money, int count) {
         LottoBundle lottoBundle = new LottoBundle(money);
         assertThat(lottoBundle.count()).isEqualTo(count);
     }
@@ -22,7 +22,7 @@ public class LottoTest {
     @DisplayName("로또 구입 금액이 음수이면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {-2000, -5000, -100})
-    public void buyLottoFailedTest(int money) {
+    public void buyLottoFailed(int money) {
         assertThatThrownBy(() -> {
             new LottoBundle(money);
         }).isInstanceOf(IllegalArgumentException.class)
@@ -31,7 +31,7 @@ public class LottoTest {
 
     @DisplayName("로또 생성 시 1-45 범위에 속하지 않는 숫자가 있으면 예외가 발생한다.")
     @Test
-    public void createLottoFailedTest() {
+    public void createLottoFailed_outOfRange() {
         assertThatThrownBy(() -> {
             new Lotto(new Integer[]{-1, 2, 3, 4, 5, 46});
         }).isInstanceOf(IllegalArgumentException.class)
