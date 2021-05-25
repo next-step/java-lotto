9fcd62e575ab88ed5cdb87f3bd6c77ecce4aff31
ascii92der <ascii92der@gmail.com>
2021-05-26 02:26:41 +0900

    refactor: 문자열 추출

--- src/main/java/study/ascii92der/lotto/step3/Lotto.java
+++ src/main/java/study/ascii92der/lotto/step3/Lotto.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Lotto {
     public static final int LOTTO_NUMBER_VALID_COUNT = 6;
+    public static final String ERROR_MASSAGE_WRONG_NUMBER_COUNT = "Lotto Number count is wrong";
+    public static final String ERROR_MASSAGE_DUPLICATE_NUMBER = "This Numbers has duplicate number";
     private List<LottoNumber> lottoNumbers;
 
     public Lotto(String lottoString) {
@@ -19,14 +21,14 @@ public class Lotto {
 
     private void validNumberCount(String[] lottoArray) {
         if(lottoArray.length != LOTTO_NUMBER_VALID_COUNT){
-            throw new IllegalArgumentException("Lotto Number count is wrong");
+            throw new IllegalArgumentException(ERROR_MASSAGE_WRONG_NUMBER_COUNT);
         }
     }
 
     private void validDuplicateNumber(String[] lottoArray) {
         Set<String> lottoNumberSet = new HashSet<>(Arrays.asList(lottoArray));
         if (lottoNumberSet.size() != lottoArray.length) {
-            throw new IllegalArgumentException("This Numbers has duplicate number");
+            throw new IllegalArgumentException(ERROR_MASSAGE_DUPLICATE_NUMBER);
         }
     }
 
--- src/main/java/study/ascii92der/lotto/step3/LottoNumber.java
+++ src/main/java/study/ascii92der/lotto/step3/LottoNumber.java
@@ -4,7 +4,7 @@ public class LottoNumber {
 
     public static final int LOTTO_NUMBER_MINIMUM = 1;
     public static final int LOTTO_NUMBER_MAXIMUM = 45;
-    public static final String OUT_OF_RANGE_LOTTO_NUMBER = "out of range Lotto number";
+    public static final String ERROR_MASSAGE_OUT_OF_RANGE_LOTTO_NUMBER = "out of range Lotto number";
     private final int value;
 
     public LottoNumber(String valueString) {
@@ -12,7 +12,7 @@ public class LottoNumber {
         this.value = Integer.parseInt(valueString);
 
         if (!(value >= LOTTO_NUMBER_MINIMUM && LOTTO_NUMBER_MAXIMUM >= value)) {
-            throw new IllegalArgumentException(OUT_OF_RANGE_LOTTO_NUMBER);
+            throw new IllegalArgumentException(ERROR_MASSAGE_OUT_OF_RANGE_LOTTO_NUMBER);
         }
 
     }
