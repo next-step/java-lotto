0e593d47de2c6f0cdc3a20d626e185f4aadf063b
accidentlywoo <accidentlywoo@gmail.com>
2021-05-25 23:18:48 +0900

    refactor test : 로또번호 랜덤 생성 반복테스트로 변경, 변수명 명시적으로 변경
    @Test -> @RepeatedTest
    numbers -> randomNumbers

--- src/test/java/generate/LottoRandomNumberTest.java
+++ src/test/java/generate/LottoRandomNumberTest.java
@@ -1,25 +1,29 @@
 package generate;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoRandomNumberTest {
-    @Test
+
+    @RepeatedTest(100)
     public void 로또번호자동생성() {
-        Integer[] numbers = LottoRandomNumber.numbers();
+        Integer[] randomNumbers = LottoRandomNumber.numbers();
 
-        assertThat(numbers.length).isEqualTo(6);
+        assertThat(randomNumbers.length).isEqualTo(6);
 
-        assertThat(numbers)
+        assertThat(randomNumbers)
                 .containsOnlyOnce(
-                        numbers[0],
-                        numbers[1],
-                        numbers[2],
-                        numbers[3],
-                        numbers[4],
-                        numbers[5]
+                        randomNumbers[0],
+                        randomNumbers[1],
+                        randomNumbers[2],
+                        randomNumbers[3],
+                        randomNumbers[4],
+                        randomNumbers[5]
                 );
     }
 
