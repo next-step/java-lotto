483e8f9c237cd9f441a0e5531470a7f95381789a
accidentlywoo <accidentlywoo@gmail.com>
2021-05-26 00:43:16 +0900

    feat : 로또 구매 그룹 통계 필드 세팅

--- src/main/java/domain/LottoOrderGroupAnalysis.java
+++ src/main/java/domain/LottoOrderGroupAnalysis.java
@@ -1,4 +1,37 @@
 package domain;
 
+import java.math.BigDecimal;
+
 public class LottoOrderGroupAnalysis {
+	private final Integer[] winnerNumbers;
+	private final LottoOrderGroup lottoOrderGroup;
+	private Integer match3;
+	private Integer match4;
+	private Integer match5;
+	private Integer match6;
+
+	public LottoOrderGroupAnalysis (Integer[] winnerNumbers, LottoOrderGroup lottoOrderGroup) {
+		this.winnerNumbers = winnerNumbers;
+		this.lottoOrderGroup = lottoOrderGroup;
+	}
+
+	public Integer yield () {
+		return new BigDecimal(0).intValue();
+	}
+
+	public Integer match3 () {
+		return match3;
+	}
+
+	public Integer match4 () {
+		return match4;
+	}
+
+	public Integer match5 () {
+		return match5;
+	}
+
+	public Integer match6 () {
+		return match6;
+	}
 }
