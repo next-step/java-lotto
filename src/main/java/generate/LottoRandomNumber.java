e095a60544b8a600c88c152cbadf26a591857ecf
accidentlywoo <accidentlywoo@gmail.com>
2021-05-25 23:19:08 +0900

    feat : 랜덤 로또 숫자 생성 구현

--- src/main/java/generate/LottoRandomNumber.java
+++ src/main/java/generate/LottoRandomNumber.java
@@ -1,7 +1,30 @@
 package generate;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class LottoRandomNumber {
+	private static final Integer LOTTO_NUMBER_LENGTH = 6;
+
 	public static Integer[] numbers () {
-		return null;
+		Integer[] lottoNumbers = new Integer[6];
+
+		for (Integer i = 0; i < LOTTO_NUMBER_LENGTH; i++) {
+			Random random = new Random();
+
+			Integer randomNumber = random.nextInt(44)+1;
+
+			if (checkDuplicate(lottoNumbers, randomNumber)) {
+				lottoNumbers[i] = randomNumber;
+				continue;
+			}
+
+			i--;
+		}
+		return lottoNumbers;
+	}
+
+	private static Boolean checkDuplicate(Integer[] lottoNumbers, int aRandomNumber){
+		return !Arrays.asList(lottoNumbers).contains(aRandomNumber);
 	}
 }
