d81f317092df7ce7a7744d58973fe4077e1c5c74
mskangg <funnycoding91@gmail.com>
2021-05-25 11:50:56 +0900

    feat: 로또 2등 당첨 조건 & 보상 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -7,9 +7,10 @@ import java.util.Arrays;
  */
 public enum LottoResult {
     NONE(0, 0),
-    FOURTH(3, 5000),
-    THIRD(4, 50000),
-    SECOND(5, 1500000),
+    FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
     FIRST(6, 2000000000);
 
     private final int matchCount;
@@ -20,7 +21,12 @@ public enum LottoResult {
         this.reward = reward;
     }
 
-    public static LottoResult findByMatchCount(final int matchCount) {
+    public static LottoResult findByMatchCount(final int matchCount, final boolean matchBonus) {
+        if (matchBonus &&
+                matchCount == LottoResult.SECOND.matchCount) {
+            return LottoResult.SECOND;
+        }
+
         return Arrays.stream(LottoResult.values())
                 .filter(lottoResult -> lottoResult.isMatch(matchCount))
                 .findFirst()
