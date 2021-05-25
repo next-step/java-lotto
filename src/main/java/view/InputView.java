0aff3541cef937de86b23cb39b2de2e99c4e5b65
prodo-developer <wjdals300@gmail.com>
2021-05-26 01:21:56 +0900

    fix : 보너스 볼 책임 부여 및 stream을 이용한 indent 1로 만들기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,6 +13,7 @@ public class InputView {
     private static final String LOTTO_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String LOTTO_PURCHASE_AMOUNT_VALID_MESSAGE = "구입금액은 숫자만 입력 가능합니다.";
+    private static final String BONUS_NUMBER_ERROR_MESSAGE = "보너스 볼은 당첨 번호들이랑 달라야 합니다.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int lottoStart() {
@@ -34,15 +35,21 @@ public class InputView {
     }
 
     private static void inputDigitValid(String inputLine) {
-        for (int i = 0; i < inputLine.length(); i++) {
-            if (!Character.isDigit(inputLine.charAt(i))) {
-                throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT_VALID_MESSAGE);
-            }
+        if(inputLine.chars().anyMatch(ch -> !Character.isDigit(ch))) {
+            throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT_VALID_MESSAGE);
         }
     }
 
     public static int bonusBall() {
         System.out.println(LOTTO_BONUS_NUMBER_MESSAGE);
         return SCANNER.nextInt();
+    }
+
+    public static LottoNumber bonusBall(int inputNumber, WinningNumber winningNumber) {
+        LottoNumber lottoNumber = new LottoNumber(inputNumber);
+        if(winningNumber.contains(lottoNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_ERROR_MESSAGE);
+        }
+        return lottoNumber;
     }
 }
