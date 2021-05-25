d9345947843af4cf637296244fc779d4470d2bf0
HyungjuJang <dacapolife87@gmail.com>
2021-05-25 11:40:13 +0900

    feat : 로또지갑에 수동로또 구입 갯수에따른 자동로또 구입과 수동으로 입력된 로또번호 입력추가

--- src/main/java/lotto/domain/LottoWallet.java
+++ src/main/java/lotto/domain/LottoWallet.java
@@ -8,30 +8,57 @@ public class LottoWallet {
 
     private static final int LOTTO_TICKET_PRICE = 1000;
 
-    private int countOfLottoTicket;
+    private int countOfAutoLottoTicket;
     private List<LottoTicket> lottoTickets = new ArrayList<>();
 
-    private LottoWallet(int countOfLottoTicket) {
-        this.countOfLottoTicket = countOfLottoTicket;
+    private LottoWallet(int countOfAutoLottoTicket) {
+        this.countOfAutoLottoTicket = countOfAutoLottoTicket;
     }
 
-    public static LottoWallet create(Money money) {
+    public static LottoWallet create(Money money, int countOfManualLottoTicket) {
         int userMoney = money.value();
+        validationForMoney(userMoney);
 
+        int couldBuyTicketAmount = calculateCouldBuyTicketAmount(userMoney);
+        validationTicketCount(countOfManualLottoTicket, couldBuyTicketAmount);
+
+        int countOfAutoLottoTicket = couldBuyTicketAmount - countOfManualLottoTicket;
+        return new LottoWallet(countOfAutoLottoTicket);
+    }
+
+    private static void validationTicketCount(int countOfManualLottoTicket, int couldBuyTicketAmount) {
+        if (couldBuyTicketAmount < countOfManualLottoTicket) {
+            throw new IllegalArgumentException("수동구입 갯수가 전체 구입가능갯수를 초과할수 없습니다.");
+        }
+    }
+
+    private static void validationForMoney(int userMoney) {
         if (userMoney < LOTTO_TICKET_PRICE) {
             throw new IllegalArgumentException("최소 구입금액은 1000원입니다.");
         }
-
-        int couldBuyTicketAmount = calculateCouldBuyTicketAmount(userMoney);
-        return new LottoWallet(couldBuyTicketAmount);
     }
 
     private static int calculateCouldBuyTicketAmount(int money) {
         return money / LOTTO_TICKET_PRICE;
     }
 
-    public void buyingLotto(NumberGeneratorStrategy numberGeneratorStrategy) {
-        for (int i = 0; i < countOfLottoTicket; i++) {
+    public void buyingLotto(NumberGeneratorStrategy numberGeneratorStrategy, List<List<String>> manualNumbers) {
+        buyingManualLottoTicket(manualNumbers);
+        buyingAutoLottoTicket(numberGeneratorStrategy);
+    }
+
+    private void buyingManualLottoTicket(List<List<String>> manualNumbers) {
+        for (List<String> lottoNumberText : manualNumbers) {
+            LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+            List<LottoNumber> lottoNumbers = lottoNumberGenerator.generator(lottoNumberText);
+
+            LottoTicket lottoTicket = LottoTicket.create(lottoNumbers);
+            lottoTickets.add(lottoTicket);
+        }
+    }
+
+    private void buyingAutoLottoTicket(NumberGeneratorStrategy numberGeneratorStrategy) {
+        for (int i = 0; i < countOfAutoLottoTicket; i++) {
             LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
             List<LottoNumber> lottoNumbers = lottoNumberGenerator.generator(numberGeneratorStrategy.generator());
             LottoTicket lottoTicket = LottoTicket.create(lottoNumbers);
@@ -40,8 +67,12 @@ public class LottoWallet {
         }
     }
 
-    public int getTicketCount() {
-        return countOfLottoTicket;
+    public int getAutoTicketCount() {
+        return countOfAutoLottoTicket;
+    }
+
+    public int getManualTicketCount() {
+        return lottoTickets.size() - countOfAutoLottoTicket;
     }
 
     public List<LottoTicket> takeOutLottoTickets() {
