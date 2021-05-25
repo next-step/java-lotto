bed42d7925ecda3cc10605b3885b6685da0dd7e8
dohchoi91 <dohchoi91@gmail.com>
2021-05-26 00:19:46 +0900

    refactor: 불필요한 주석 및 메서드 제거

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import java.util.List;
 
-import lotto.Lotto;
 import lotto.Lottos;
 import lotto.Money;
 import lotto.Prize;
@@ -15,44 +14,22 @@ public class Main {
 		InputView inputView = new InputView();
 		ResultView resultView = new ResultView();
 
-		/*
-		 * 사용자 입력 : 구입금액
-		 */
 		long amount = inputView.askAmount();
-		Lottos lottos = buy(amount);
+		Lottos lottos = Store.buy(new Money(amount));
 
-		/*
-		 * 결과 출력 : 구입 로또
-		 */
+		// TODO 이 부분을 어떻게 빼야 할지 모르겠습니다.
 		// for (Lotto lotto : lottos.findAll()) {
 		// 	resultView.printNumbers(lotto.numbers());
 		// }
 
-		/*
-		 * 사용자 입력 : 지난주 당첨 번호
-		 */
 		List<Integer> prizeNumbers = inputView.askPrizeNumbers();
 		Statistics statistics = lottos.statistics(prizeNumbers);
 
-		/*
-		 * 결과 출력 : 통계, 수익률
-		 */
 		resultView.outputIntro();
 		resultView.outputStatistics(Prize.SIX.count(), Prize.SIX.win().amount(), statistics.status(Prize.SIX));
 		resultView.outputStatistics(Prize.FIVE.count(), Prize.FIVE.win().amount(), statistics.status(Prize.FIVE));
 		resultView.outputStatistics(Prize.FOUR.count(), Prize.FOUR.win().amount(), statistics.status(Prize.FOUR));
 		resultView.outputStatistics(Prize.THREE.count(), Prize.THREE.win().amount(), statistics.status(Prize.THREE));
-		//resultView.outputProfit(statistics.profit().rate());
+		resultView.outputProfit(Prize.profit(statistics).rate());
 	}
-
-	private static Lottos buy(long amount) {
-		return Store.buy(new Money(amount));
-	}
-
-	// private static Statistics analyze(List<Integer> prizeNumbers, Lottos lottos) {
-	// 	Statistics statistics = new Statistics(new Lotto(prizeNumbers));
-	// 	statistics.analyze(lottos);
-	// 	return statistics;
-	// }
-
 }
