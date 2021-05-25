ae8f6b0d55656a07292910421e4fa0c7811f23f9
moon_ <m3252@naver.com>
2021-05-25 22:50:30 +0900

    refactor: SCANNER 입력값 유효성 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,7 +11,9 @@ public final class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static BigDecimal receivePurchaseAmount() {
-        return new BigDecimal(SCANNER.nextLine());
+        String input = SCANNER.nextLine();
+        validate(input);
+        return new BigDecimal(input);
     }
 
     public static Lotto receiveWinningNumbers() {
@@ -19,9 +21,30 @@ public final class InputView {
                 .split(",");
         Number[] numbers = new Number[receiveNumbers.length];
         for (int i = 0; i < receiveNumbers.length; i++) {
+            validate(receiveNumbers[i]);
             numbers[i] = new Number(Integer.parseInt(receiveNumbers[i]));
         }
         return new Lotto(numbers);
+    }
+
+    public static void validate(String input) {
+        validateNumber(input);
+        validatePositiveNumber(input);
+    }
+
+
+    private static void validateNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new NumberFormatException("숫자만 입력 가능합니다.");
+        }
+    }
+
+    private static void validatePositiveNumber(String input) {
+        if (Integer.parseInt(input) < 0) {
+            throw new IllegalArgumentException("양수만 입력 가능합니다.");
+        }
     }
 
 }
