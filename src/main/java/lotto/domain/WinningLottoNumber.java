6a8aef8732e75fa40e025c408bb7a6a488dc606d
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-25 23:31:44 +0900

    feat: 당첨 번호 생성자에 유효성 검사 추가 및 관련 테스트 구현

--- src/main/java/lotto/domain/WinningLottoNumber.java
+++ src/main/java/lotto/domain/WinningLottoNumber.java
@@ -2,17 +2,23 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
-import static lotto.domain.LottoNumber.from;
+import static java.lang.String.format;
+import static lotto.domain.LottoGame.LOTTO_NUMBER_COUNT;
+import static lotto.domain.LottoNumber.*;
 
 public class WinningLottoNumber {
     private static final String COMMA = ",";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^\\d+(,\\d+)*$");
 
     private List<LottoNumber> values;
 
     public WinningLottoNumber(String playerInput) {
+        validatePlayerInput(playerInput);
         parseValues(playerInput);
     }
 
@@ -26,6 +32,21 @@ public class WinningLottoNumber {
         }
 
         return lottoResult;
+    }
+
+    private void validatePlayerInput(String playerInput) {
+        Matcher numberArrayCheckResult = NUMBER_PATTERN.matcher(playerInput);
+        if (!numberArrayCheckResult.find()) {
+            throw new IllegalArgumentException("당첨 번호는 양수만 입력할 수 있습니다.");
+        }
+
+        String[] sizeCheckResult = playerInput.split(",");
+        if (sizeCheckResult.length != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(format("당첨 번호는 %d에서 %d사이의 번호 %d자리를 입력 해야 합니다.",
+                    MIN_LOTTO_NUMBER,
+                    MAX_LOTTO_NUMBER,
+                    LOTTO_NUMBER_COUNT));
+        }
     }
 
     private void parseValues(String playerInput) {
--- src/test/java/lotto/domain/WinningLottoNumberTest.java
+++ src/test/java/lotto/domain/WinningLottoNumberTest.java
@@ -2,6 +2,9 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -10,9 +13,33 @@ import java.util.List;
 import static lotto.domain.LottoNumber.from;
 import static lotto.domain.Rank.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("WinningLottoNumber 테스트")
 class WinningLottoNumberTest {
+
+    @DisplayName("new_정상")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,7",
+            "13,22,13,34,25,37",
+            "21,23,37,42,15,37",
+            "31,22,43,44,45,17",})
+    void new_정상(String input) {
+        assertDoesNotThrow(() -> new WinningLottoNumber(input));
+    }
+
+    @DisplayName("new_예외")
+    @ParameterizedTest
+    @CsvSource({"ㄱ,ㄴ,ㄷ",
+            "-1,-3,2,3,5,6",
+            "21,23,37,42,15,-1",
+            "31,22,43,44",})
+    void new_예외(String input) {
+        // When, Then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new WinningLottoNumber(input));
+    }
 
     private List<LottoGame> mockLottoGameList = Arrays.asList(
             new LottoGame(new HashSet(Arrays.asList(from(1), from(2), from(3), from(4), from(5), from(6)))),
