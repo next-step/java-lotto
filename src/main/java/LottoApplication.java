42cc976849bd11f210a8e2fc9be10ea714bb81d9
prodo-developer <wjdals300@gmail.com>
2021-05-26 01:25:01 +0900

    fix : LottoGame 제거 및 각 책임별 클래스 메서드 분리, lottoMachine.generateLottoTickets 메서드 제거 -> 기존 메서드 활용(lottoMachine.generate)

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,23 +1,21 @@
-import controller.LottoGame;
 import domain.*;
 import view.InputView;
 import view.OutputView;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        LottoGame lottoGame = new LottoGame();
         LottoMachine lottoMachine = new LottoMachine();
         // 로또 생성
-        LottoTickets lottoTickets = lottoGame.generateLottoTickets(InputView.lottoStart(), lottoMachine);
+        LottoTickets lottoTickets = lottoMachine.generate(InputView.lottoStart());
         OutputView.lottoStatus(lottoTickets);
         // 당첨 로또 생성
-        WinningNumber winningNumber = lottoGame.generateWinningNumber(InputView.winningNumber(), lottoMachine);
+        WinningNumber winningNumber = WinningNumber.generateWinningNumber(InputView.winningNumber());
 
         // 보너스 볼 생성
-        LottoNumber bonusBall = lottoGame.generateBonusBall(InputView.bonusBall(), lottoMachine, winningNumber);
+        LottoNumber bonusBall = LottoNumber.generateBonusBall(InputView.bonusBall(), winningNumber);
 
         // 로또 게임 결과 생성
-        LottoResult lottoResult = lottoGame.result(lottoTickets, winningNumber, bonusBall);
+        LottoResult lottoResult = LottoResult.result(lottoTickets, winningNumber, bonusBall);
         OutputView.LottoEnd(lottoResult);
     }
 }
