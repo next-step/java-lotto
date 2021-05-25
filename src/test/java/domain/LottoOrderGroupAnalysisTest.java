f2567de830f3962518d92d3d9eba0e4b9c18d458
accidentlywoo <accidentlywoo@gmail.com>
2021-05-26 00:42:23 +0900

    test : 랜덤값... 테스트... ?

--- src/test/java/domain/LottoOrderGroupAnalysisTest.java
+++ src/test/java/domain/LottoOrderGroupAnalysisTest.java
@@ -1,7 +1,22 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoOrderGroupAnalysisTest {
+	@Test
+	public void 로또_숫자_맞춤_계산() {
+		//???
+	}
 
+	@Test
+	public void 수익률_계산() {
+		LottoOrderGroupAnalysis analysis = new LottoOrderGroupAnalysis(
+				new Integer[]{1, 2, 3, 4, 5, 6}
+				, new LottoOrderGroup(10)
+		);
+		//??
+		analysis.yield();
+	}
 }
