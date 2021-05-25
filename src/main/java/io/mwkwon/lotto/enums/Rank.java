d18d97dbce3daf61c966224dbeed905297d753a7
Min Wook Kwon <mwkwon@joonggonara.co.kr>
2021-05-25 21:32:15 +0900

    feat: 당첨 통계 2등 항목 추가
    Rank enum 통계 출력 메시지 추가
    ResultView 통계 출력 기능 수정

--- src/main/java/io/mwkwon/lotto/enums/Rank.java
+++ src/main/java/io/mwkwon/lotto/enums/Rank.java
@@ -8,17 +8,18 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, 2_000_000_000, "%s개 일치 (%s원) - %s개"),
+    SECOND(5, 30_000_000, "%s개 일치, 보너스볼 일치(%s원) - %s개"),
+    THIRD(5, 1_500_000, "%s개 일치 (%s원) - %s개"),
+    FOURTH(4, 50_000, "%s개 일치 (%s원) - %s개"),
+    FIFTH(3, 5_000, "%s개 일치 (%s원) - %s개"),
+    MISS(0, 0, "%s개 이하 일치 (%s원) - %s개");
 
     private static Map<Integer, Rank> ENUM_MAP;
 
     private final int matchCount;
     private final int reward;
+    private final String statisticsMessage;
 
     static {
         ENUM_MAP = Stream.of(Rank.values())
@@ -26,9 +27,10 @@ public enum Rank {
                 .collect(Collectors.toMap(Rank::getMatchCount, Function.identity()));
     }
 
-    Rank(int matchCount, int reward) {
+    Rank(int matchCount, int reward, String statisticsMessage) {
         this.matchCount = matchCount;
         this.reward = reward;
+        this.statisticsMessage = statisticsMessage;
     }
 
     public int getMatchCount() {
@@ -39,6 +41,10 @@ public enum Rank {
         return reward;
     }
 
+    public String getStatisticsMessage() {
+        return statisticsMessage;
+    }
+
     public static Rank valueOf(int matchCount, boolean isMatchBonus) {
         if (matchCount == 5 && isMatchBonus) {
             return Rank.SECOND;
@@ -47,6 +53,6 @@ public enum Rank {
     }
 
     public static List<Rank> createPrintTargetRank() {
-        return Arrays.asList(FOURTH, THIRD, SECOND, FIRST);
+        return Arrays.asList(FIFTH, FOURTH, THIRD, SECOND, FIRST);
     }
 }
--- src/main/java/io/mwkwon/lotto/view/ResultView.java
+++ src/main/java/io/mwkwon/lotto/view/ResultView.java
@@ -9,7 +9,6 @@ public class ResultView {
     private static final String BUY_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String TITLE = "당첨 통계";
     private static final String DELIMITER = "---------";
-    private static final String STATISTICS_MESSAGE = "%s개 일치(%s원)- %s개";
     private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
     public void printBuyLottoCount(LottoPayment lottoPayment) {
@@ -31,7 +30,7 @@ public class ResultView {
         Map<Rank, Integer> rankStatisticsMap = winningRanks.aggregateRank();
         for (Rank rank : Rank.createPrintTargetRank()) {
             int count = rankStatisticsMap.getOrDefault(rank, 0);
-            System.out.println(String.format(STATISTICS_MESSAGE, rank.getMatchCount(), rank.getReward(), count));
+            System.out.println(String.format(rank.getStatisticsMessage(), rank.getMatchCount(), rank.getReward(), count));
         }
     }
 
