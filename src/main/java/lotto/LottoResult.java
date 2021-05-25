46e239c8ec0c568f111d4476105c69efc64e2108
JisuLim <sanola3@gmail.com>
2021-05-25 15:06:58 +0900

    refactor: use enumMap

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -3,34 +3,46 @@ package lotto;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 public class LottoResult {
 	private static final int ROUNDING_OFF_SCALE = 2;
 
-
-	private final List<LottoPrizeType> lottoResults;
+	private final Map<LottoPrizeType, AtomicLong> lottoResults;
 
 	private LottoResult(List<LottoPrizeType> lottoResults) {
-		this.lottoResults = lottoResults;
+		this.lottoResults = enumMapOf(lottoResults);
 	}
 
 	public static LottoResult of(List<LottoPrizeType> lottoResults) {
 		return new LottoResult(lottoResults);
 	}
 
-	public long getNumberOfLottosMatchedWith(int matchedNumberCount) {
-		return lottoResults.stream()
-				.filter(it -> it.getMatchedNumberCount() == matchedNumberCount)
-				.count();
+	private Map<LottoPrizeType, AtomicLong> enumMapOf(List<LottoPrizeType> lottoResults) {
+		Map<LottoPrizeType, AtomicLong> map = new EnumMap<>(LottoPrizeType.class);
+		lottoResults.forEach(it -> map.computeIfAbsent(it, result -> new AtomicLong(0))
+				.getAndIncrement());
+		return map;
+	}
+
+	public Long getNumberOfLottosMatchedWith(int matchedNumberCount) {
+		return lottoResults.get(LottoPrizeType.valueByMatchedNumberCount(matchedNumberCount))
+				.longValue();
 	}
 
 	public float getProfitRate() {
-		return lottoResults.stream()
+		return lottoResults.keySet()
+				.stream()
 				.map(LottoPrizeType::getPrizeMoney)
 				.reduce(BigDecimal.ZERO, BigDecimal::add)
-				.divide(BigDecimal.valueOf(lottoResults.size())
+				.divide(BigDecimal.valueOf(lottoResults.values()
+								.stream()
+								.mapToInt(AtomicLong::intValue)
+								.sum())
 								.multiply(Constants.LOTTO_PRICE),
 						ROUNDING_OFF_SCALE, RoundingMode.FLOOR)
 				.floatValue();
@@ -40,7 +52,7 @@ public class LottoResult {
 	public String toString() {
 		return Arrays.stream(LottoPrizeType.values())
 				.filter(LottoPrizeType::isWinningType)
-				.map(it -> it.getMatchedNumberCount() + "개 일치 (" + it.getPrizeMoney() + "원)- " + getNumberOfLottosMatchedWith(it.getMatchedNumberCount()) + "개")
+				.map(it -> it.getMatchedNumberCount() + "개 일치 (" + it.getPrizeMoney() + "원)- " + lottoResults.getOrDefault(it, new AtomicLong(0)) + "개")
 				.collect(Collectors.joining(Constants.JOINING_DELIMITER));
 	}
 }
