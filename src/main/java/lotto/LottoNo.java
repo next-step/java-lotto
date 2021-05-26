01eb6bf0c9c3b6ff5c45c9e296069955fed054fb
dohchoi91 <dohchoi91@gmail.com>
2021-05-26 23:41:33 +0900

    refactor: 기본 로또번호에 대한 정적맵 추가

--- src/main/java/lotto/LottoNo.java
+++ src/main/java/lotto/LottoNo.java
@@ -1,10 +1,19 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class LottoNo implements Comparable<LottoNo>{
 	public static final int MIN = 1;
 	public static final int MAX = 45;
+
+	private static final Map<Integer, LottoNo> DEFAULT_LOTTO_NO_MAP = new HashMap<>();
+	static {
+		for (int i = MIN; i <= MAX; i++) {
+			DEFAULT_LOTTO_NO_MAP.put(i, new LottoNo(i));
+		}
+	}
 
 	private final int number;
 
@@ -17,7 +26,7 @@ public class LottoNo implements Comparable<LottoNo>{
 
 	public static LottoNo randomNo() {
 		int randomNo = (int)(Math.random() * MAX) + MIN;
-		return new LottoNo(randomNo);
+		return DEFAULT_LOTTO_NO_MAP.get(randomNo);
 	}
 
 	@Override
