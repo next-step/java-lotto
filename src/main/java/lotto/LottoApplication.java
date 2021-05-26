b2bc3f849410a2a8ca0814606802b333a4d62a62
jordy-torvalds <jordy-torvalds@kakao.com>
2021-05-27 05:42:39 +0900

    fix: LottoSeller 삭제에 따른 비즈니스 로직 변경 및 View 클래스의 final 선언

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -7,14 +7,12 @@ import lotto.view.LottoOutputView;
 
 public class LottoApplication {
 
-    private LottoInputView lottoInputView;
-    private LottoOutputView lottoOutputView;
-    private LottoSeller lottoSeller;
+    private final LottoInputView lottoInputView;
+    private final LottoOutputView lottoOutputView;
 
     public LottoApplication() {
         lottoInputView = new LottoInputView();
         lottoOutputView = new LottoOutputView();
-        lottoSeller = new LottoSeller(new LottoGameAutoGenerator());
     }
 
     public static void main(String[] args) {
@@ -24,7 +22,7 @@ public class LottoApplication {
 
     private void run() {
         PurchaseMoney purchaseMoney = lottoInputView.questionPurchaseMoney();
-        LottoGames lottoGames = lottoSeller.sell(purchaseMoney);
+        LottoGames lottoGames = LottoGames.purchase(purchaseMoney);
 
         printPurchaseResult(lottoGames);
 
