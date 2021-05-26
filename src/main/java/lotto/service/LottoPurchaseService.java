9edd9caab2f1e676d9854275ec8fa1edca535355
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-26 13:53:23 +0900

    feat : 구입금액 검증메서드 추가

--- src/main/java/lotto/service/LottoPurchaseService.java
+++ src/main/java/lotto/service/LottoPurchaseService.java
@@ -5,6 +5,12 @@ import lotto.domain.LottoTickets;
 
 public class LottoPurchaseService {
 
+    public void validatePurchasable(LottoMoney havingAmount, LottoMoney purchaseAmount) {
+        if (havingAmount.compareTo(purchaseAmount) < 0) {
+            throw new IllegalArgumentException("금액이 부족합니다. 보유금액과 구입금액을 확인해 주세요.");
+        }
+    }
+
     public LottoTickets purchaseTickets(LottoMoney purchaseAmount) {
         return LottoTickets.autoTickets(purchaseAmount.purchaseCount());
     }
--- src/test/java/lotto/service/LottoPurchaseServiceTest.java
+++ src/test/java/lotto/service/LottoPurchaseServiceTest.java
@@ -3,6 +3,7 @@ package lotto.service;
 import lotto.domain.LottoMoney;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,6 +26,19 @@ class LottoPurchaseServiceTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> service.purchaseTickets(LottoMoney.of(purchaseAmount)))
                 .withMessageMatching("금액이 부족합니다. 최소 1000 원 이상의 금액을 입력해 주세요.");
+    }
+    
+    @Test
+    @DisplayName("구입금액 검증")
+    void validatePurchasable() {
+        // given
+        LottoMoney havingAmount = LottoMoney.of(10000);
+        LottoMoney purchaseAmount = LottoMoney.fromCount(14);
+
+        // when then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> service.validatePurchasable(havingAmount, purchaseAmount))
+                .withMessageMatching("금액이 부족합니다. 보유금액과 구입금액을 확인해 주세요.");
     }
 
 }
