4440589b5b7481d51c0b950e846cde3220c93ef0
ted.kang <ted.kang@kakaocorp.com>
2021-05-27 00:32:11 +0900

    feat: InputView 수동 구매 로또 입력 로직 작성

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -3,17 +3,17 @@ package lotto.ui;
 import lotto.dto.OrderSheet;
 import lotto.dto.WinningNumbersAndBonusNumber;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class InputView {
 
   private static final String PLEASE_INPUT_INTEGER = "자연수로 입력 해 주세요.";
   private static final String PLEASE_INPUT_PURCHASING_PRICE = "구입금액을 입력해 주세요.";
+  private static final String PLEASE_INPUT_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+  private static final String PLEASE_INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
   private static final String PLEASE_INPUT_LAST_WEEK_PRIZE = "지난 주 당첨 번호를 입력해 주세요.";
   private static final String PLEASE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
   private static final Pattern SPACE = Pattern.compile("\\s");
@@ -24,13 +24,25 @@ public class InputView {
   private InputView() {}
 
   public static OrderSheet orderNewGames() {
-    return new OrderSheet(sayQuestionAndGetLong(PLEASE_INPUT_PURCHASING_PRICE));
+    long purchasingAmount = sayQuestionAndGetLong(PLEASE_INPUT_PURCHASING_PRICE);
+    List<List<Integer>> manualLottoNumbers = getManualLottoNumbers(sayQuestionAndGetLong(PLEASE_INPUT_MANUAL_LOTTO_COUNT));
+    return OrderSheet.of(purchasingAmount, manualLottoNumbers);
   }
 
   public static WinningNumbersAndBonusNumber inputWinningNumbersAndBonusNumber() {
-    List<Integer> winningNumbers = sayQuestionAndGetStringArray(PLEASE_INPUT_LAST_WEEK_PRIZE);
+    List<Integer> winningNumbers = sayQuestionAndGetIntegerList(PLEASE_INPUT_LAST_WEEK_PRIZE);
     int bonusNumber = (int) sayQuestionAndGetLong(PLEASE_INPUT_BONUS_NUMBER);
     return new WinningNumbersAndBonusNumber(winningNumbers, bonusNumber);
+  }
+
+  private static List<List<Integer>> getManualLottoNumbers(long sayQuestionAndGetLong) {
+    if(sayQuestionAndGetLong > 0) {
+      System.out.println(PLEASE_INPUT_MANUAL_LOTTO_NUMBERS);
+      return LongStream.range(0, sayQuestionAndGetLong)
+          .mapToObj(index -> getIntegerList())
+          .collect(Collectors.toList());
+    }
+    return Collections.emptyList();
   }
 
   private static long sayQuestionAndGetLong(String question) {
@@ -38,8 +50,12 @@ public class InputView {
     return getLongValue();
   }
 
-  private static List<Integer> sayQuestionAndGetStringArray(String question) {
+  private static List<Integer> sayQuestionAndGetIntegerList(String question) {
     System.out.println(question);
+    return getIntegerList();
+  }
+
+  private static List<Integer> getIntegerList() {
     String input = getStringValue();
     return parsInts(splitByDelimiter(input));
   }
