f515d9a0bfd02a914439896c43df18090d8ab31e
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-27 00:15:09 +0900

    test : LottoTickets 테스트 추가

--- src/test/java/lotto/LottoTicketsTest.java
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -3,33 +3,28 @@ package lotto;
 import lotto.domain.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketsTest {
 
-//    @DisplayName("구매액/티켓가격과 일치하는 티켓장수가 발급되었는지 테스트")
-//    @Test
-//    void check_count_of_tickets() {
-//        //Given
-//        TicketGenerator ticketGenerator = new AutoLottoTicketGenerator();
-//
-//        //When + Then
-//        LottoTickets lottoTickets = new LottoTickets(ticketGenerator.generate(3));
-//
-//        //Then
-//        assertThat(lottoTickets.count()).isEqualTo(purchaseAmount.countOfTickets());
-//    }
+    @DisplayName("입력한 티켓이 추가되는지 테스트")
+    @Test
+    void check_count_of_tickets_after_combine() {
+        //Given
+        List<LottoTicket> tickets = new ArrayList<>();
+        tickets.add(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)));
 
-//    @DisplayName("구매액/티켓가격과 일치하는 티켓장수가 발급되었는지 테스트")
-//    @Test
-//    void check_count_of_tickets() {
-//        //Given
-//        TicketGenerator ticketGenerator = new AutoLottoTicketGenerator();
-//
-//        //When + Then
-//        LottoTickets lottoTickets = new LottoTickets(ticketGenerator.generate(3));
-//
-//        //Then
-//        assertThat(lottoTickets.count()).isEqualTo(purchaseAmount.countOfTickets());
-//    }
+        LottoTickets lottoTickets = new LottoTickets(tickets);
+
+        //When
+        lottoTickets.combineWith(tickets);
+
+        //Then
+        assertThat(lottoTickets.count()).isEqualTo(2);
+    }
 }
