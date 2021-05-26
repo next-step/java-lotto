d096dadfad19c940d1eedecc0eba9a15a5162a88
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-26 10:25:42 +0900

    feat : 구매수량 기반 금액생성 로직 구현

--- src/main/java/lotto/domain/LottoMoney.java
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -13,9 +13,20 @@ public class LottoMoney {
         return new LottoMoney(purchaseAmount);
     }
 
+    public static LottoMoney fromCount(int purchaseCount) {
+        validatePurchaseCount(purchaseCount);
+        return new LottoMoney(purchaseCount * LottoTicket.PRICE);
+    }
+
     private static void validatePurchaseAmount(int purchaseAmount) {
         if (purchaseAmount < LottoTicket.PRICE) {
             throw new IllegalArgumentException("금액이 부족합니다. 최소 " + LottoTicket.PRICE + " 원 이상의 금액을 입력해 주세요.");
+        }
+    }
+
+    private static void validatePurchaseCount(int purchaseCount) {
+        if (purchaseCount <= 0) {
+            throw new IllegalArgumentException("구매 수량을 입력해 주세요.");
         }
     }
 
