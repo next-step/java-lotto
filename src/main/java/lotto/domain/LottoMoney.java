41cfa28e47ad7d3da45b86e55c927e331ad0c923
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-26 15:39:13 +0900

    refactor : 금액부족 검증 예외타입 변경 - Argument -> State

--- src/main/java/lotto/domain/LottoMoney.java
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -32,7 +32,7 @@ public class LottoMoney implements Comparable<LottoMoney> {
 
     public void validatePurchasable() {
         if (amount < LottoTicket.PRICE) {
-            throw new IllegalArgumentException("금액이 부족합니다. 최소 " + LottoTicket.PRICE + " 원 이상의 금액을 입력해 주세요.");
+            throw new IllegalStateException("금액이 부족합니다. 최소 " + LottoTicket.PRICE + " 원 이상의 금액을 입력해 주세요.");
         }
     }
 
