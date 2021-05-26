3f492965c7e51f69ada931ce7169a43f9cdda651
tinyhhj <krwsim@gmail.com>
2021-05-26 22:29:35 +0900

    refactor: indent 1맞추기 Lottos로 변수명 수정

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,25 +3,25 @@ package lotto.domain;
 import java.util.*;
 
 public class Lottos implements Iterable<Lotto>{
-    private List<Lotto> lottoGames = new ArrayList<>();
+    private List<Lotto> lottos = new ArrayList<>();
 
 
     public Lottos(LottoNumberFactory factory, int money) {
         int lottoCount = money / Lotto.PRICE;
         for (int i = 0; i < lottoCount; i++) {
-            lottoGames.add(createLotto(factory,Collections.EMPTY_LIST));
+            lottos.add(createLotto(factory,Collections.emptyList()));
         }
     }
 
-    public Lottos(LottoNumberFactory factory, List<List<Integer>> lottoGames) {
-        for( List<Integer> lotto  : lottoGames) {
-            this.lottoGames.add(createLotto(factory,lotto));
+    public Lottos(LottoNumberFactory factory, List<List<Integer>> lottos) {
+        for( List<Integer> lotto  : lottos) {
+            this.lottos.add(createLotto(factory,lotto));
         }
     }
     public static Lotto createLotto(LottoNumberFactory factory, List<Integer> numbers) {
         Set<LottoNumber> lotto = new HashSet<>();
-        for( int i = 0; i < numbers.size(); i++) {
-            Number lottoNumber = createLottoNumber(factory,numbers.get(i));
+        for( Integer number : numbers) {
+            Number lottoNumber = createLottoNumber(factory,number);
             lotto.add(new LottoNumber(lottoNumber));
         }
 
@@ -37,18 +37,18 @@ public class Lottos implements Iterable<Lotto>{
 
     public LottoStatics calculateStatics(Lotto winGame) {
         List<Rank> ranks = new ArrayList<>();
-        for (Lotto game : lottoGames) {
+        for (Lotto game : lottos) {
             ranks.add(winGame.matchCount(game));
         }
         return new LottoStatics(ranks);
     }
 
     public int size() {
-        return lottoGames.size();
+        return lottos.size();
     }
 
     @Override
     public Iterator<Lotto> iterator() {
-        return lottoGames.iterator();
+        return lottos.iterator();
     }
 }
--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Rank {
     nothing(0,0),
     threeNumbersMatch(3,5_000),
@@ -17,22 +19,30 @@ public enum Rank {
     }
 
     public static int prize(Rank targetRank) {
+        int prize = 0;
         for (Rank rank : values()) {
-            if( rank  == targetRank) {
-                return rank.prize;
-            }
+            prize += prize(targetRank, rank);
+        }
+        return prize;
+    }
+
+    private static int prize(Rank targetRank, Rank rank) {
+        if( rank  == targetRank) {
+            return rank.prize;
         }
         return 0;
     }
 
     public static Rank of(int matchCount) {
-        for (Rank rank : values()) {
-            if (rank.matchCount == matchCount) {
-                return rank;
-            }
-        }
-        return Rank.nothing;
+        return Arrays.asList(
+                Rank.nothing,
+                Rank.nothing,
+                Rank.nothing,
+                Rank.threeNumbersMatch,
+                Rank.fourNumbersMatch,
+                Rank.fiveNumbersMatch,
+                Rank.sixNumbersMatch
+        ).get(matchCount);
     }
-
 
 }
