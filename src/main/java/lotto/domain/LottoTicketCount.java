389f7fbfc7a37e167f60316af81a741c153a5df9
Donghui-Lee <ehdgml3206@naver.com>
2021-05-26 19:00:24 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/lotto/domain/LottoTicketCount.java
+++ src/main/java/lotto/domain/LottoTicketCount.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoTicketCount {
     private final int lottoTicketCount;
 
@@ -8,6 +10,28 @@ public class LottoTicketCount {
     }
 
     public LottoTicketCount(int lottoTicketCount) {
+        validate(lottoTicketCount);
         this.lottoTicketCount = lottoTicketCount;
+    }
+
+    private void validate(int lottoTicketCount) {
+        if (lottoTicketCount < 0) {
+            throw new IllegalArgumentException("0보다 큰 값을 입력하세요");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        LottoTicketCount that = (LottoTicketCount)o;
+        return lottoTicketCount == that.lottoTicketCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoTicketCount);
     }
 }
