03cba16feb36079dba2dc47ec6424abf5747d67c
dohchoi91 <dohchoi91@gmail.com>
2021-05-27 00:48:26 +0900

    refactor: 데이터 전용 객체 추가 및 모델 분리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lotto.Lottos;
@@ -5,31 +7,45 @@ import lotto.Money;
 import lotto.Prize;
 import lotto.Statistics;
 import lotto.Store;
+import lotto.dto.LottoDto;
+import lotto.dto.PrizeDto;
 import lotto.ui.InputView;
 import lotto.ui.ResultView;
 
 public class Main {
+	private static final InputView inputView = new InputView();
+	private static final ResultView resultView = new ResultView();
 
 	public static void main(String[] args) {
-		InputView inputView = new InputView();
-		ResultView resultView = new ResultView();
-
 		long amount = inputView.askAmount();
 		Lottos lottos = Store.buy(new Money(amount));
 
-		// TODO 이 부분을 어떻게 빼야 할지 모르겠습니다.
-		// for (Lotto lotto : lottos.findAll()) {
-		// 	resultView.printNumbers(lotto.numbers());
-		// }
+		List<LottoDto> lottoDtos = createLottoDtos(lottos);
+		resultView.printNumbers(lottoDtos);
 
 		List<Integer> prizeNumbers = inputView.askPrizeNumbers();
 		Statistics statistics = lottos.statistics(prizeNumbers);
 
-		resultView.outputIntro();
-		resultView.outputStatistics(Prize.SIX.count(), Prize.SIX.win().amount(), statistics.status(Prize.SIX));
-		resultView.outputStatistics(Prize.FIVE.count(), Prize.FIVE.win().amount(), statistics.status(Prize.FIVE));
-		resultView.outputStatistics(Prize.FOUR.count(), Prize.FOUR.win().amount(), statistics.status(Prize.FOUR));
-		resultView.outputStatistics(Prize.THREE.count(), Prize.THREE.win().amount(), statistics.status(Prize.THREE));
+		List<PrizeDto> prizeDtos = createPrizeDtos(statistics);
+		resultView.output(prizeDtos);
 		resultView.outputProfit(Prize.profit(statistics).rate());
+	}
+
+	private static List<PrizeDto> createPrizeDtos(Statistics statistics) {
+		List<PrizeDto> prizeDtos = new ArrayList<>();
+		List<Prize> prizes = new ArrayList<>(Arrays.asList(Prize.values()));
+		prizes.remove(Prize.NOTHING);
+		for (Prize value : prizes) {
+			prizeDtos.add(new PrizeDto(value, statistics.status(value)));
+		}
+		return prizeDtos;
+	}
+
+	static List<LottoDto> createLottoDtos(Lottos lottos) {
+		List<LottoDto> lottoDtos = new ArrayList<>();
+		for (List<Integer> numbers : lottos.allNumbers()) {
+			lottoDtos.add(new LottoDto(numbers));
+		}
+		return lottoDtos;
 	}
 }
--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -2,25 +2,30 @@ package lotto.ui;
 
 import java.util.List;
 
+import lotto.dto.LottoDto;
+import lotto.dto.PrizeDto;
+
 public class ResultView {
 	public static final String INFO_INTRO = "당첨 통계\n---------";
 	public static final String FORMAT_PROFIT = "총 수익률은 %.2f 입니다.%n";
 	public static final String FORMAT_STATISTICS = "%d개 일치 (%s원) - %d개%n";
 
-	public void outputIntro() {
+	public void output(List<PrizeDto> prizeDtos) {
 		System.out.println(INFO_INTRO);
-	}
-
-	public void outputStatistics(int index, long amount, int count) {
-		System.out.printf(FORMAT_STATISTICS, index, amount, count);
+		for (PrizeDto prizeDto : prizeDtos) {
+			System.out.printf(FORMAT_STATISTICS, prizeDto.count(), prizeDto.amount(), prizeDto.status());
+		}
 	}
 
 	public void outputProfit(double rate) {
 		System.out.printf(FORMAT_PROFIT, rate);
 	}
 
-	public void printNumbers(List<Integer> numbers) {
-		System.out.println(numbers);
+	public void printNumbers(List<LottoDto> numbers) {
+		for (LottoDto lottoDto : numbers) {
+			System.out.println(lottoDto.lottoNumbers());
+		}
+
 	}
 }
 
