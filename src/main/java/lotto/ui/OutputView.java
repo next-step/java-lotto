3889ea32c1bf2ef6e83d3ff3fa1f973216ea4b0b
ted.kang <ted.kang@kakaocorp.com>
2021-05-27 00:33:19 +0900

    refactor: OutputView가 의존하는 TotalOrderedLottoGameDescription 리팩토링에 따른 변경사항 반영
    파라미터 타입명 변경, 출력 메서드 명 변경

--- src/main/java/lotto/ui/OutputView.java
+++ src/main/java/lotto/ui/OutputView.java
@@ -1,19 +1,19 @@
 package lotto.ui;
 
 import lotto.dto.AnalysisSheet;
-import lotto.dto.TotalOrderedLottoGameNumbers;
+import lotto.dto.TotalOrderedLottoGameDescription;
 
 public class OutputView {
 
-  private static final String ORDERED_N_GAMES_FORMAT = "%d개를 구매했습니다.";
+  private static final String ORDERED_N_GAMES_FORMAT = "%s을 구매했습니다.";
   private static final String PRIZE_ANALYSIS = "당첨 통계";
   private static final String SEPARATE_BAR = "---------";
 
   private OutputView() {}
 
-  public static void displayOrderedGames(TotalOrderedLottoGameNumbers totalOrderedLottoGameNumbers) {
-    System.out.println(String.format(ORDERED_N_GAMES_FORMAT, totalOrderedLottoGameNumbers.getOrderedCount()));
-    System.out.println(totalOrderedLottoGameNumbers.toString());
+  public static void displayOrderedGames(TotalOrderedLottoGameDescription totalOrderedLottoGameDescription) {
+    System.out.println(String.format(ORDERED_N_GAMES_FORMAT, totalOrderedLottoGameDescription.getOrderedCountPerOrderType()));
+    System.out.println(totalOrderedLottoGameDescription.toString());
   }
 
 
