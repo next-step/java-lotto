13ed7ec753d6768db4f21bcd24bfc22cc637a552
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-26 15:52:41 +0900

    test : 구입금액 부족 시 미구입 처리

--- src/test/java/lotto/service/LottoPurchaseServiceTest.java
+++ src/test/java/lotto/service/LottoPurchaseServiceTest.java
@@ -1,14 +1,14 @@
 package lotto.service;
 
 import lotto.domain.LottoMoney;
+import lotto.domain.LottoTickets;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoPurchaseServiceTest {
 
@@ -20,13 +20,14 @@ class LottoPurchaseServiceTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 999})
-    @DisplayName("로또티켓 구입 - 구입금액 부족")
+    @ValueSource(ints = {0, 999})
+    @DisplayName("로또티켓 구입 - 구입금액 부족 시")
     void purchase_lackOfAmount(int purchaseAmount) {
-        // given when then
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> service.purchaseAutoTickets(LottoMoney.of(purchaseAmount)))
-                .withMessageMatching("금액이 부족합니다. 최소 1000 원 이상의 금액을 입력해 주세요.");
+        // given when
+        LottoTickets lottoTickets = service.purchaseAutoTickets(LottoMoney.of(purchaseAmount));
+
+        // then
+        assertThat(lottoTickets.count()).isEqualTo(0);
     }
     
     @Test
