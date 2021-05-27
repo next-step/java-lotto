b9e0756f74415d6ae6a7ac47c878a76f91ac9685
심정미 <lina@aterilio.kr>
2021-05-27 18:01:46 +0900

    [step3/로또(자동)] refactor: 문자열에서 로또로 파싱하는 부분 별도 클래스 선언

--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LuckyNumbers.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LuckyNumbers.java
@@ -1,18 +1,13 @@
 package kr.aterilio.nextstep.techcamp.m1.lotto;
 
-import kr.aterilio.nextstep.techcamp.m1.utils.StringUtil;
+import kr.aterilio.nextstep.techcamp.m1.utils.LottoParser;
 
 public class LuckyNumbers {
 
     private final Lotto luckyNumber;
 
     public LuckyNumbers(String inputLuckyNumbers) {
-        this.luckyNumber = new Lotto(parse(inputLuckyNumbers));
-    }
-
-    private Integer[] parse(String inputLuckyNumbers) {
-        String[] split = inputLuckyNumbers.split(",");
-        return StringUtil.convertToIntegerArray(split);
+        this.luckyNumber = new Lotto(LottoParser.parse(inputLuckyNumbers));
     }
 
     public int count() {
