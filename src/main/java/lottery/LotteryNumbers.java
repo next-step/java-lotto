6ab98cb11a37ab37e9e6b06a9a0fcc432fd72c3d
kimsanggu <tkdrn8578@paymint.co.kr>
2021-05-27 09:59:09 +0900

    refactor: numbers를 arrayList로 변경 및 ParameterizedTest로 수정

--- src/main/java/lottery/LotteryNumbers.java
+++ src/main/java/lottery/LotteryNumbers.java
@@ -1,30 +1,23 @@
 package lottery;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LotteryNumbers {
 
+    private static final int MINIMUM_LOTTERY_NUMBERS_SIZE = 6;
+
     private final List<Integer> numbers;
 
-    public LotteryNumbers(int...numbers) {
-        this.numbers = new ArrayList<>();
-
+    public LotteryNumbers(List<Integer> numbers) {
         validateLotteryNumbersSize(numbers);
 
-        fillNumbers(numbers);
+        this.numbers = numbers;
     }
 
-    private void validateLotteryNumbersSize(int[] numbers) {
-        int sizeOfNumbers = numbers.length;
-        if (sizeOfNumbers != 6) {
+    private void validateLotteryNumbersSize(List<Integer> numbers) {
+        int sizeOfNumbers = numbers.size();
+        if (sizeOfNumbers != MINIMUM_LOTTERY_NUMBERS_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6자리이어야 합니다. : " + sizeOfNumbers);
-        }
-    }
-
-    private void fillNumbers(int[] numbers) {
-        for (int number : numbers) {
-            this.numbers.add(number);
         }
     }
 
--- src/test/java/lottery/LotteryNumbersTest.java
+++ src/test/java/lottery/LotteryNumbersTest.java
@@ -1,7 +1,12 @@
 package lottery;
 
+import config.ArraySource;
+import config.ArraySources;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -11,14 +16,22 @@ public class LotteryNumbersTest {
     @Test
     @DisplayName("6개의 로또번호를 가진다.")
     void lotteryNumber_test() {
-        LotteryNumbers lotteryNumbers = new LotteryNumbers(1, 2, 3, 4, 5, 6);
+        LotteryNumbers lotteryNumbers = new LotteryNumbers(List.of(1, 2, 3, 4, 5, 6));
 
         assertThat(lotteryNumbers.size()).isEqualTo(6);
     }
 
-    @Test
+    @ParameterizedTest
+    @ArraySources(
+            {
+                    @ArraySource({1, 2, 3, 4, 5, 6, 7}),
+                    @ArraySource({1, 2}),
+                    @ArraySource({1}),
+                    @ArraySource({1, 2, 5})
+            }
+    )
     @DisplayName("로또번호의 개수가 6개가 아닌 경우 예외처리한다.")
-    void notSixDigitLotteryNumber_test() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new LotteryNumbers(1, 2, 3, 4, 5, 6, 9));
+    void notSixDigitLotteryNumber_test(List<Integer> numbers) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new LotteryNumbers(numbers));
     }
 }
