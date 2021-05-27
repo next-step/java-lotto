feea11ef6f96adfe35aecf8eca08238ab276a091
Jeff Chung <drifterz303@gmail.com>
2021-05-28 01:18:05 +0900

    feature: 예상 범위를 벗어난 입력 시 예외 처리를 추가한다

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -13,13 +13,13 @@ import java.util.List;
 public class LottoApplication {
     public static void main(String[] args){
 
-        int expense = ViewInput.getExpense();
-        int numLottoTicket = ViewInput.getAvailableNumLottoTicket(expense);
+        int expense = ViewInput.askExpense();
+        int numLottoTicket = ViewInput.askAvailableNumLottoTicket(expense);
         LottoController lottoController = new LottoController();
         List<LottoTicket> userLottoTickets = lottoController.generate(numLottoTicket);
         ConsoleView.printLotto(userLottoTickets);
 
-        List<LottoNumber> winningNumbers = ViewInput.getWinningNumbers();
+        List<LottoNumber> winningNumbers = ViewInput.askWinningNumbers();
         LottoResult lottoResult = lottoController.run(new LottoTicket(winningNumbers),userLottoTickets ,expense);
         ConsoleView.printScoreMap(lottoResult);
         ConsoleView.printResult(lottoResult);
--- src/main/java/lotto/view/ViewInput.java
+++ src/main/java/lotto/view/ViewInput.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.model.LottoNumber;
 
+import java.rmi.server.ExportException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,27 +14,51 @@ public class ViewInput {
     public final static String INPUT_WINNING_LOTTO_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     public final static Integer PRICE_LOTTO_TICKET = 1000;
 
-    public static int getExpense(){
+    public static int askExpense(){
         System.out.println(INPUT_EXPENSE_MESSAGE);
         Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
+        return validateExpense(scan.nextInt());
     }
 
-    public static int getAvailableNumLottoTicket(int expense){
+    public static int askAvailableNumLottoTicket(int expense){
         int numLottoTicket = (expense / PRICE_LOTTO_TICKET);
         System.out.println(String.format(INPUT_NUM_TICKET_MESSAGE, numLottoTicket));
         return numLottoTicket;
     }
 
-    public static List<LottoNumber> getWinningNumbers(){
+    public static List<LottoNumber> askWinningNumbers(){
         System.out.println(INPUT_WINNING_LOTTO_NUMBERS_MESSAGE);
         Scanner scan = new Scanner(System.in);
         String[] tokens = scan.nextLine().split(",");
+        validate(tokens);
         List<LottoNumber> result = new ArrayList<>();
         for (String token : tokens) {
             result.add(new LottoNumber(Integer.parseInt(token)));
         }
         return result;
+    }
 
+    private static int validateExpense(int expense){
+        try {
+            if(expense < PRICE_LOTTO_TICKET){
+                throw new Exception("not enough to buy lotto");
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return expense;
+    }
+
+    private static void validate(String[] tokens) {
+        try {
+            if (tokens.length != 6) {
+                throw new Exception("lottoNumber consists of 6 number");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
