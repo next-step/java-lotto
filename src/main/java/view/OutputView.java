11f51d99d1f4d5c19dc1b5fc0a41db6d3eccb159
prodo-developer <wjdals300@gmail.com>
2021-05-29 01:40:58 +0900

    fix : 수동입력 결과 및 기타 문구 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,20 +5,25 @@ import domain.LottoResult;
 import domain.LottoTickets;
 
 public class OutputView {
-    private static final String LOTTO_PURCHASE_QUANTITY_MESSAGE = "%d개를 구매했습니다.";
+    private static final String LOTTO_PURCHASE_QUANTITY_MESSAGE = "\n수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final String LOTTO_WINNING_MESSAGE = "\n당첨 통계\n" + "---------\n";
     private static final String WINNING_COUNT_MESSAGE = "%d개 일치 (%d원) - %d개\n";
     private static final String WINNING_COUNT_WITH_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개\n";
     private static final String TOTAL_REWARD_MESSAGE = "총 수익률은 %s입니다.";
-    private static String RESULT_MESSAGE = "";
 
-    public static void lottoStatus(LottoTickets lottoTickets) {
-        lottoQuantity(lottoTickets.lottoTicketCount());
+    public static void lottoStatus(LottoTickets lottoTickets, LottoTickets manualLottos) {
+        lottoQuantity(lottoTickets.lottoTicketCount(), manualLottos.lottoTicketCount());
+        manualLottoTickets(manualLottos);
         lottoTickets(lottoTickets);
     }
 
-    private static void lottoQuantity(final int quantity) {
-        System.out.println(String.format(LOTTO_PURCHASE_QUANTITY_MESSAGE, quantity));
+    private static void lottoQuantity(int lottoTicketCount, int manualLottoCount) {
+        System.out.println(String.format(LOTTO_PURCHASE_QUANTITY_MESSAGE, manualLottoCount, lottoTicketCount));
+    }
+
+    private static void manualLottoTickets(LottoTickets manualLottos) {
+        manualLottos.getLottoTickets()
+                .forEach(lottoTicket -> System.out.println(lottoTicket.getLottoTicket()));
     }
 
     private static void lottoTickets(final LottoTickets lottoTickets) {
@@ -29,7 +34,6 @@ public class OutputView {
     public static void LottoEnd(LottoResult lottoResult) {
         winning(lottoResult);
         totalRewardRatio(lottoResult);
-        System.out.println(RESULT_MESSAGE);
     }
 
     private static void winning(final LottoResult lottoResult) {
@@ -42,6 +46,6 @@ public class OutputView {
     }
 
     public static void totalRewardRatio(final LottoResult lottoResult) {
-        RESULT_MESSAGE = String.format(TOTAL_REWARD_MESSAGE, lottoResult.rewardRatio());
+        System.out.println(String.format(TOTAL_REWARD_MESSAGE, lottoResult.rewardRatio()));
     }
 }
