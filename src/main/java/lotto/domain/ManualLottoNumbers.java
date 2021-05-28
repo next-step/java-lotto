92b3dce18ee8773b715206c77e21021a51824427
ttuop <plintcsz@gmail.com>
2021-05-28 18:15:25 +0900

    refactor: ManualLottoNumbers 생성자 유효성 체크 Optional 로 변경

--- src/main/java/lotto/domain/ManualLottoNumbers.java
+++ src/main/java/lotto/domain/ManualLottoNumbers.java
@@ -1,14 +1,21 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class ManualLottoNumbers {
 
 	private final List<LottoNumberText> lottoNumberTextList;
 
 	public ManualLottoNumbers(List<LottoNumberText> lottoNumberTextList) {
-		this.lottoNumberTextList = lottoNumberTextList;
+		this.lottoNumberTextList = validLottoNumberTexts(lottoNumberTextList);
+	}
+
+	private List<LottoNumberText> validLottoNumberTexts(List<LottoNumberText> lottoNumberTextList) {
+		return Optional.ofNullable(lottoNumberTextList)
+			.orElse(new ArrayList<>());
 	}
 
 	public int count() {
