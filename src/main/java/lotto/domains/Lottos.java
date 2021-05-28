eb9443f9fd206db7e2fa744f48dcc2e83ae1d6c7
fDevJc <fDevJc@gmail.com>
2021-05-29 02:16:08 +0900

    refactor: 통계관련 메서드 변경

--- src/main/java/lotto/domains/Lottos.java
+++ src/main/java/lotto/domains/Lottos.java
@@ -1,29 +1,44 @@
 package lotto.domains;
 
+import lotto.enums.MatchingInfo;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Lottos {
 
-    private final List<Lotto> lottoList;
+    private final static int LOTTO_PRICE = 1000;
 
+    private final List<Lotto> lottoList;
+    
     public Lottos(List<Lotto> lottoList) {
-        this.lottoList = lottoList;
+        this.lottoList = new ArrayList<>(lottoList);
     }
 
     public int count() {
         return lottoList.size();
     }
 
-    public LottoStatistics getStatistics(LottoNumbers winningLottoNumbers) {
-        LottoStatistics lottoStatistics = new LottoStatistics();
-        for (Lotto lotto : lottoList) {
-            lottoStatistics.add(lotto.matchingNumberCount(winningLottoNumbers));
-        }
-        return lottoStatistics;
-    }
-
     public List<Lotto> lottos() {
         return Collections.unmodifiableList(this.lottoList);
+    }
+
+    public void matchingWinningNumbersAndBonusNumber(LottoNumbers winningNumbers, int bonusNumber) {
+        for (Lotto lotto : lottoList) {
+            lotto.matching(winningNumbers, bonusNumber);
+        }
+    }
+
+    public int countOfMatchingNumber(MatchingInfo matchingInfo) {
+        return (int) lottoList.stream()
+                .filter(p -> p.matchingInfo() == matchingInfo)
+                .count();
+    }
+
+    public double rateOfReturn() {
+        return lottoList.stream()
+                .mapToDouble(lotto -> lotto.matchingInfo().getPayout())
+                .sum() / (lottoList.size() * LOTTO_PRICE);
     }
 }
