8fa24a35c752d9f20a908aafbd6be58b42099d7c
hailey <wjdguswls55@gmail.com>
2021-05-28 13:36:15 +0900

    feat (LottoSatistic) : 로또 count와 보너스 볼 체크를 하는 함수 변경 및 수익률 계산 로직 추가

--- src/main/java/lottoAuto/model/LottoStatistic.java
+++ src/main/java/lottoAuto/model/LottoStatistic.java
@@ -1,55 +1,49 @@
 package lottoAuto.model;
 
-import lottoAuto.vo.LottoResultVo;
-
 import java.util.*;
 
 public class LottoStatistic {
+    private List<Lotto> userLottos;
+    private Lotto winningLotto;
+    private int bonus;
 
-    private List<Integer> winning_count;
-    private List<Boolean> bonus_count;
-
-    public int getStatistic(List<Lotto> lottos, List<Integer> winningNum) {
-        int count = 0;
-        for (int i = 0; i < lottos.size(); i++) {
-            count = getCount(lottos.get(i).getNumbers(), winningNum, i);
-        }
-        return count;
+    public LottoStatistic(Lottos userLottos, Lotto winningLotto, int bonus) {
+        this.userLottos = userLottos.getLotto();
+        this.winningLotto = winningLotto;
+        this.bonus = bonus;
     }
 
-    private int getCount(List<Integer> lottoNumber, List<Integer> winningNum, int i) {
+    public LottoResults getStatistic() {
+
+        LottoResults lottoResults = new LottoResults();
+
+        for (Lotto userLotto : userLottos) {
+
+            LottoResult lottoResult = new LottoResult(getCount(userLotto.getNumbers(), winningLotto.getNumbers()), hasBonus(userLotto.getNumbers(), bonus));
+            lottoResults.add(lottoResult);
+        }
+        return lottoResults;
+    }
+
+    private int getCount(List<Integer> userLottoNumbers, List<Integer> winningLottoNumbers) {
         int count = 0;
-        for (int j = 0; j < winningNum.size(); j++) {
-            if (lottoNumber.contains(winningNum.get(j))) {
+        for (int j = 0; j < winningLottoNumbers.size(); j++) {
+            if (userLottoNumbers.contains(winningLottoNumbers.get(j))) {
                 count++;
             }
         }
         return count;
     }
 
-    public List<Boolean> getStatisticBonus(List<List> result, int bonus) {
-        bonus_count = new ArrayList<>();
-        for (int i = 0; i < result.size(); i++) {
-            Boolean count = getBonusCount(result, bonus, i);
-            bonus_count.add(count);
-        }
-        return bonus_count;
+    private Boolean hasBonus(List<Integer> userLottoNumbers, int bonus) {
+
+        return userLottoNumbers.contains(bonus);
     }
 
-    private Boolean getBonusCount(List<List> result, int bonus, int i) {
-        Boolean count = false;
-        if (winning_count.get(i) == 5 && result.get(i).contains(bonus)) {
-            count = true;
-        }
-        return count;
+    public double getRevenue(int price, Map<Rank, Long> resultMap) {
+        int sum = resultMap.entrySet().stream().mapToInt(rankLongEntry ->Rank.getPrize(rankLongEntry.getKey(),rankLongEntry.getValue())).sum();
+        double revenue = sum / price;
+        return revenue;
     }
 
-    public List<LottoResultVo> setWinningStatistic(List<Integer> winning_count, List<Boolean> bonus_count) {
-        List<LottoResultVo> lottoResultVoList = new ArrayList<>();
-
-        for (int i = 0; i < winning_count.size(); i++) {
-            lottoResultVoList.add(new LottoResultVo(Rank.findByRank(winning_count.get(i), bonus_count.get(i)), 1));
-        }
-        return lottoResultVoList;
-    }
 }
