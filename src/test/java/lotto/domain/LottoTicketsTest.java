4b269b94202db53394c76bb6a70d6e3de2d96606
SUNJU\SUNJU <sunju635@naver.com>
2021-05-28 12:22:24 +0900

    test($step5): add LottoTicketsTest for merge function

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -4,7 +4,9 @@ import lotto.common.WinningType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -47,5 +49,30 @@ public class LottoTicketsTest {
         ));
         assertThat(secondUserLottoTickets.getPrizeSum(winningLottoTicket, bonusNumber))
                 .isEqualTo(WinningType.THIRD.getPrize() + WinningType.FORTH.getPrize());
+    }
+
+    @Test
+    @DisplayName("로또 티켓 병합 테스트")
+    void mergeLottoTickets() {
+        LottoTicket winningLottoTicket = new LottoTicket("7,8,9,10,11,12");
+        int bonusNumber = 20;
+
+        LottoTickets lottoTickets = new LottoTickets(new ArrayList<LottoTicket>(){{
+            add(new LottoTicket("1,2,3,4,5,6"));
+        }});
+
+        //MISS
+        List<WinningType> winningResult = lottoTickets.getWinningResult(winningLottoTicket,bonusNumber);
+        assertThat(winningResult).containsExactly(WinningType.MISS);
+
+        //1등 로또 티켓 추가
+        List<LottoTicket> newLottoTickets = new ArrayList<LottoTicket>(){{
+            add(new LottoTicket("7,8,9,10,11,12"));
+        }};
+        LottoTickets mergedLottoTickets = lottoTickets.addAll(newLottoTickets);
+
+        //First가 추가되었기 때문에 MISS, First
+        List<WinningType> mergedWinningResult = mergedLottoTickets.getWinningResult(winningLottoTicket,bonusNumber);
+        assertThat(mergedWinningResult).containsExactly(WinningType.MISS, WinningType.FIRST);
     }
 }
