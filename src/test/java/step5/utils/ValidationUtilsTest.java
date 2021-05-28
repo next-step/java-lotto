0a2a36b19902ca96897fe27fc01906fb880e7a83
박소정 <psj6414@naver.com>
2021-05-28 23:09:42 +0900

    test: 당첨번호와 보너스번호 중복 체크

--- src/test/java/step5/utils/ValidationUtilsTest.java
+++ src/test/java/step5/utils/ValidationUtilsTest.java
@@ -2,10 +2,11 @@ package step5.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Set;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 class ValidationUtilsTest {
@@ -13,22 +14,40 @@ class ValidationUtilsTest {
     @ParameterizedTest
     @NullAndEmptySource
     void isNullCheck(String input) {
-        assertThat(ValidationUtils.isNullCheck(input)).isFalse();
+        assertThatThrownBy(() -> {
+            ValidationUtils.isNull(input);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("당첨번호를 입력해주세요.");
     }
 
     @DisplayName("입력받은 문자열이 쉼표로 구분되는지 확인")
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3 4,5.6", "1:2:3:4:5,6", "1 2 3 4 5,8"})
     void validWinningNumbersComma(String input) {
-        boolean result = ValidationUtils.validWinningNumbersComma(input);
-        assertThat(result).isFalse();
+        assertThatThrownBy(() -> {
+            ValidationUtils.validWinningNumbersComma(input);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("당첨번호는 쉼표(,)로 구분되어야합니다.");
     }
 
     @DisplayName("입력받은 size가 6자리인지 확인")
     @ParameterizedTest
     @ValueSource(strings = {"7", "0", "3"})
     void validWinningNumbersDigit(int size) {
-        boolean result = ValidationUtils.validWinningNumbersDigit(size);
-        assertThat(result).isFalse();
+        assertThatThrownBy(() -> {
+            ValidationUtils.validWinningNumbersDigit(size);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("당첨번호는 6자리 숫자로 구성되어야합니다.");
+    }
+
+    @DisplayName("당첨번호와 보너스번호 중복 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:4", "10,15,23,24,25,30:30", "30,35,37,40,41,45:35"}, delimiter = ':')
+    void validDuplicationNumbers(String inputNumbers, int bonusNumber) {
+        Set<Integer> winningNumbers = StringUtils.processStringNumbers(inputNumbers);
+        assertThatThrownBy(() -> {
+            ValidationUtils.validDuplicationNumbers(winningNumbers, bonusNumber);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("보너스볼은 당첨번호와 중복되서는 안됩니다.");
     }
 }
--- src/test/java/step5/winning/WinningNumbersTest.java
+++ src/test/java/step5/winning/WinningNumbersTest.java
@@ -2,6 +2,7 @@ package step5.winning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,7 +14,7 @@ class WinningNumbersTest {
     @NullAndEmptySource
     void setWinningNumbers_null확인(String numbers){
         assertThatThrownBy(() -> {
-            new WinningNumbers(numbers);
+            new WinningNumbers(numbers, 3);
         }).isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("당첨번호를 입력해주세요.");
     }
@@ -23,7 +24,7 @@ class WinningNumbersTest {
     @ValueSource(strings = {"1,2,3,4.5.6", "3,4,5:1:2:6","19 2,3,4,5,1"})
     void winningNumber_쉼표구분(String numbers) {
         assertThatThrownBy(() -> {
-            new WinningNumbers(numbers);
+            new WinningNumbers(numbers, 3);
         }).isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("당첨번호는 쉼표(,)로 구분되어야합니다.");
     }
@@ -33,8 +34,18 @@ class WinningNumbersTest {
     @ValueSource(strings = {"1,2,3,4", "3,4,5", "1", "9,2,3,4,5"})
     void winningNumber_6개미만_예외발생(String numbers) {
         assertThatThrownBy(() -> {
-            new WinningNumbers(numbers);
+            new WinningNumbers(numbers, 3);
         }).isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("당첨번호는 6자리 숫자로 구성되어야합니다.");
+    }
+
+    @DisplayName("당첨번호와 보너스번호 중복 예외 발생")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:6", "10,23,25,30,34,40:25", "11,13,15,28,29,38:29"}, delimiter = ':')
+    void winningNumber_당첨번호_보너스번호_중복(String numbers, int bonusNumber) {
+        assertThatThrownBy(() -> {
+            new WinningNumbers(numbers, bonusNumber);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("보너스볼은 당첨번호와 중복되서는 안됩니다.");
     }
 }
