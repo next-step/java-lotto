5cbab659d82a8520f91f066fcce2715830562534
SUNJU\SUNJU <sunju635@naver.com>
2021-05-28 12:22:50 +0900

    feat($step5): implemet LottoTicketsTest for merge function

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.common.WinningType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,5 +43,10 @@ public class LottoTickets {
         return this.lottoTickets.stream()
                 .map(t -> t.getWinningType(winningLottoTicket, bonusNumber))
                 .collect(Collectors.toList());
+    }
+
+    public LottoTickets addAll(List<LottoTicket> inputTickets) {
+        this.lottoTickets.addAll(inputTickets);
+        return new LottoTickets(this.lottoTickets);
     }
 }
