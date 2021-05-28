6966fa469335d7fa57859c690d5747551217fb61
hailey <wjdguswls55@gmail.com>
2021-05-28 13:37:54 +0900

    feat (ResultView.showAmount/showRevenue) : amount를 return하는 함수와 수익률을 보여주는 함수 작성

--- src/main/java/lottoAuto/view/ResultView.java
+++ src/main/java/lottoAuto/view/ResultView.java
@@ -8,8 +8,10 @@ import java.util.stream.Collectors;
 
 public class ResultView {
     private InputView inputView = new InputView();
+    private static final int LOTTO_PRICE = 1000;
 
-    public int showAmount(int amount) {
+    public int showAmount(int price) {
+        int amount = price / LOTTO_PRICE;
         System.out.println(amount + "개를 구매했습니다.");
         return amount;
     }
@@ -29,4 +31,7 @@ public class ResultView {
         return collect;
     }
 
+    public void showRevenue(double revenue){
+        System.out.println("총 수익률은 "+revenue+"입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+    }
 }
