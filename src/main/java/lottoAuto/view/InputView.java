773aabb8d1936e1f52fa00bbfdf0f70c49bb7ae6
hailey <wjdguswls55@gmail.com>
2021-05-28 12:45:22 +0900

    refactor (InputView) : 입력 책임 분리

--- src/main/java/lottoAuto/view/InputView.java
+++ src/main/java/lottoAuto/view/InputView.java
@@ -2,23 +2,26 @@ package lottoAuto.view;
 
 import java.util.Scanner;
 
-public class LottoAutoInputView {
+public class InputView {
     private Scanner scanner = new Scanner(System.in);
     private static final int LOTTO_PRICE = 1000;
 
     public int inputPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
         int price = scanner.nextInt();
         int amount = price / LOTTO_PRICE;
         return amount;
     }
 
     public String inputWinningNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String winningLottoString = scanner.next();
 
         return winningLottoString;
     }
 
     public int inputBonusBall() {
+        System.out.println("보너스 볼을 입력해 주세요.");
         int bonusBall = scanner.nextInt();
 
         return bonusBall;
