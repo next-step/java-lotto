edefaa9bd5660df83a8621196c248fc50636279b
fDevJc <fDevJc@gmail.com>
2021-05-29 02:15:46 +0900

    refactor: 변수명 변경

--- src/main/java/lotto/domains/LottoSubstituteNumbers.java
+++ src/main/java/lotto/domains/LottoSubstituteNumbers.java
@@ -5,16 +5,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoSubstituteNumbers {
-    private static final List<Integer> lottoSubstituteNumbers = new ArrayList<>();
+    private static final List<Integer> LOTTO_SUBSTITUTE_NUMBERS = new ArrayList<>();
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_NUMBER_FROM_INDEX = 0;
+    private static final int LOTTO_NUMBER_TO_INDEX = 6;
+
     static {
         for (int i = LOTTO_MIN_NUMBER; i <= LOTTO_MAX_NUMBER; i++) {
-            lottoSubstituteNumbers.add(i);
+            LOTTO_SUBSTITUTE_NUMBERS.add(i);
         }
     }
+
     public static List<Integer> sixLottoNumbers() {
-        Collections.shuffle(lottoSubstituteNumbers);
-        return Collections.unmodifiableList(lottoSubstituteNumbers.subList(0,6));
+        Collections.shuffle(LOTTO_SUBSTITUTE_NUMBERS);
+        return Collections.unmodifiableList(LOTTO_SUBSTITUTE_NUMBERS.subList(LOTTO_NUMBER_FROM_INDEX, LOTTO_NUMBER_TO_INDEX));
     }
 }
