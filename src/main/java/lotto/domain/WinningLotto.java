f2839b9abd7728a05bf0c53dd1765266ac4b7b1f
ttuop <plintcsz@gmail.com>
2021-05-28 18:12:35 +0900

    refactor: WinningLotto 생성자 유효성 체크 Optional 로 변경

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,23 +1,29 @@
 package lotto.domain;
 
+import java.util.Optional;
+
 public class WinningLotto {
 
 	private static final String DUPLICATED_LOTTO_NUMBER_MESSAGE = "보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+	public static final String INVALID_BONUS_NUMBER_MESSAGE = "보너스 번호가 유효하지 않습니다.";
 
 	private final LottoTicket winningLottoTicket;
 	private final LottoNumber bonusLottoNumber;
 
 	public WinningLotto(LottoTicket winningLottoTicket, LottoNumber bonusLottoNumber) {
-		validateBonusNumber(winningLottoTicket, bonusLottoNumber);
-
-		this.winningLottoTicket = winningLottoTicket;
-		this.bonusLottoNumber = bonusLottoNumber;
+		this.bonusLottoNumber = validBonusNumber(bonusLottoNumber);
+		this.winningLottoTicket = validLottoTicket(winningLottoTicket, bonusLottoNumber);
 	}
 
-	private void validateBonusNumber(LottoTicket winningLottoTicket, LottoNumber bonusLottoNumber) {
-		if (winningLottoTicket.isMatchingNumber(bonusLottoNumber)) {
-			throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBER_MESSAGE);
-		}
+	private LottoTicket validLottoTicket(LottoTicket winningLottoTicket, LottoNumber bonusLottoNumber) {
+		return Optional.ofNullable(winningLottoTicket)
+			.filter(lottoTicket -> !lottoTicket.isMatchingNumber(bonusLottoNumber))
+			.orElseThrow(() -> new IllegalArgumentException(DUPLICATED_LOTTO_NUMBER_MESSAGE));
+	}
+
+	private LottoNumber validBonusNumber(LottoNumber bonusLottoNumber) {
+		return Optional.ofNullable(bonusLottoNumber)
+			.orElseThrow(() -> new IllegalArgumentException(INVALID_BONUS_NUMBER_MESSAGE));
 	}
 
 	public LottoRank rank(LottoTicket userLottoTicket) {
