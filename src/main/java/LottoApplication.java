9bcbfffedb0f6d1a6b38bf2f19bb8e9b1725336a
prodo-developer <77876817+prodo-developer@users.noreply.github.com>
2021-05-29 23:14:30 +0900

    [step5] 로또(수동) 리뷰 요청드립니다. (#1718)
    * fix : 모키토 라이브러리 추가
    
    * fix : 수동 입력 및 수동 갯수 추가
    
    * fix : 수동 기능 및 자동+수동 기능 추가
    
    * fix : 자동 생성 및 역할별 메서드 분리
    
    * fix : 자동+생성 붙이기 기능
    
    * fix : 수동입력 결과 및 기타 문구 수정
    
    * fix : 불필요한 필드 삭제
    
    * test : 모키토를 이용한 수동처리 테스트
    
    * test : 수동 내역 리스트와 자동 내역 리스트 합친 테스트
    
    * docs : to-do 5단계 추가

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -5,17 +5,26 @@ import view.OutputView;
 public class LottoApplication {
     public static void main(String[] args) {
         LottoMachine lottoMachine = new LottoMachine();
-        // 로또 생성
-        LottoTickets lottoTickets = lottoMachine.generate(InputView.lottoStart());
-        OutputView.lottoStatus(lottoTickets);
+        int price = InputView.purchaseAmount();
+        int manualLottoCount = InputView.manualPurchaseLottoCount();
+
+        LottoTickets manualLottos = InputView.manualLottoNumbers(manualLottoCount);
+
+        LottoTickets autoLottoTickets = lottoMachine.autoGenerate(price, manualLottos);
+
+        OutputView.lottoStatus(autoLottoTickets, manualLottos);
+
         // 당첨 로또 생성
         WinningNumber winningNumber = WinningNumber.generateWinningNumber(InputView.winningNumber());
 
         // 보너스 볼 생성
         LottoNumber bonusBall = LottoNumber.generateBonusBall(InputView.bonusBall(), winningNumber);
+        
+        // 수동 개수 더하기 자동 개수
+        LottoTickets concatTickets = manualLottos.concat(autoLottoTickets);
 
         // 로또 게임 결과 생성
-        LottoResult lottoResult = LottoResult.result(lottoTickets, winningNumber, bonusBall);
+        LottoResult lottoResult = LottoResult.result(concatTickets, winningNumber, bonusBall);
         OutputView.LottoEnd(lottoResult);
     }
 }
