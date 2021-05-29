046215bf4ab1a817412ba208a1ca899487b296bd
ttuop <plintcsz@gmail.com>
2021-05-29 20:58:24 +0900

    refactor: UserLotto 유효성 체크 에러 메시지 수정

--- src/main/java/lotto/domain/UserLotto.java
+++ src/main/java/lotto/domain/UserLotto.java
@@ -1,35 +1,35 @@
 package lotto.domain;
 
+import static lotto.util.CollectionUtils.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static lotto.util.CollectionUtils.*;
-
 public class UserLotto {
 
-	private static final String INVALID_LOTTO_TICKET_LIST_MESSAGE = "로또 티켓이 유효하지 않습니다.";
+    private static final String NO_LOTTO_TICKET_LIST_MESSAGE = "구매한 로또 티켓이 없습니다.";
 
-	private final List<LottoTicket> lottoTickets;
+    private final List<LottoTicket> lottoTickets;
 
-	UserLotto(List<LottoTicket> lottoTicketList) {
-		lottoTickets = Optional.ofNullable(lottoTicketList)
-				.filter(lottoTicket -> lottoTicket.size() > 0)
-				.orElseThrow(() -> new IllegalArgumentException(INVALID_LOTTO_TICKET_LIST_MESSAGE));
-	}
+    UserLotto(List<LottoTicket> lottoTicketList) {
+        lottoTickets = Optional.ofNullable(lottoTicketList)
+            .filter(lottoTicket -> lottoTicket.size() > 0)
+            .orElseThrow(() -> new IllegalArgumentException(NO_LOTTO_TICKET_LIST_MESSAGE));
+    }
 
-	public int count() {
-		return lottoTickets.size();
-	}
+    public int count() {
+        return lottoTickets.size();
+    }
 
-	public List<LottoTicket> lottoTickets() {
-		return Collections.unmodifiableList(lottoTickets);
-	}
+    public List<LottoTicket> lottoTickets() {
+        return Collections.unmodifiableList(lottoTickets);
+    }
 
-	public LottoReport report(WinningLotto winningLotto) {
-		return new LottoReport(Collections.unmodifiableList(
-				transform(lottoTickets, new ArrayList<>(), winningLotto::rank)));
-	}
+    public LottoReport report(WinningLotto winningLotto) {
+        return new LottoReport(Collections.unmodifiableList(
+            transform(lottoTickets, new ArrayList<>(), winningLotto::rank)));
+    }
 
 }
