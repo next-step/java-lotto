558f539bdb6781a4b5621a7cc5caef1838611c82
fDevJc <fDevJc@gmail.com>
2021-05-29 23:33:41 +0900

    test: 자동/수동 로또 카운팅 반환 메서드 테스트 추가

--- src/test/java/lotto/domains/LottosTest.java
+++ src/test/java/lotto/domains/LottosTest.java
@@ -1,6 +1,7 @@
 package lotto.domains;
 
 import lotto.enums.MatchingInfo;
+import lotto.enums.PurchasingWay;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -22,10 +23,23 @@ public class LottosTest {
 
         Lotto lotto1 = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 45)));
         Lotto lotto2 = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 16)));
-        Lottos lottos = new Lottos(Arrays.asList(lotto1,lotto2));
+        Lottos lottos = new Lottos(Arrays.asList(lotto1, lotto2));
 
         lottos.matchingWinningNumbersAndBonusNumber(winningLottoNumbers, bonusNumber);
         assertThat(lottos.countOfMatchingNumber(MatchingInfo.MATCH_FIVE_AND_BONUS)).isEqualTo(1);
         assertThat(lottos.countOfMatchingNumber(MatchingInfo.MATCH_FIVE)).isEqualTo(1);
+    }
+
+    @Test
+    void 구입방식_로또_카운팅_반환_테스트() {
+        Lotto lotto1 = new Lotto();
+        Lotto lotto2 = new Lotto();
+        Lotto lotto3 = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 45)));
+        Lotto lotto4 = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 16)));
+
+        Lottos lottos = new Lottos(Arrays.asList(lotto1, lotto2, lotto3, lotto4));
+
+        assertThat(lottos.countOfPurchasingWay(PurchasingWay.AUTOMATIC)).isEqualTo(2);
+        assertThat(lottos.countOfPurchasingWay(PurchasingWay.MANUAL)).isEqualTo(2);
     }
 }
