96f2f26a3133a08f52eb1e63fad0d7e3e8715073
jaewanLee <ianlee9180@gmail.com>
2021-05-29 17:30:20 +0900

    refactor : WinProfit Enum을 외부에 만들고, match갯수값을 enum 값에 포함시킨다.
    LottoWin - 각 등수별 맞춰야하는 로또 갯수와, 우승상금을 갖고있는 enum
    LottoStatistic - 연속해서 선언되었던 match값들을 Lottowin에 enum 값으로 이동

--- src/main/java/lotto/LottoStatistics.java
+++ src/main/java/lotto/LottoStatistics.java
@@ -1,71 +1,42 @@
 package lotto;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static lotto.Lotto.LOTTO_PRICE;
-
 
 public class LottoStatistics {
     public static int getLottoBuyingCount(int money) {
         return Math.round(money / LOTTO_PRICE);
     }
 
-    enum WinProfit {
-        THREE_MATCH_PROFIT(5000),
-        FOUR_MATCH_PROFIT(50000),
-        FIVE_MATCH_PROFIT(1500000),
-        SIX_MATCH_PROFIT(2000000000);
-
-        private final int value;
-
-        WinProfit(int value) {
-            this.value = value;
+    public static double calculateLottoTicketProfit(LottoTicket lottoTicket) {
+        List<Lotto> lottoList = lottoTicket.lottoList();
+        int totalProfit = 0;
+        for (int i = 0; i < lottoList.size(); i++) {
+            totalProfit += lottoList.get(i).lottoWin().winPrice();
         }
-    }
-
-    private int threeMatch = 0;
-    private int fourMatch = 0;
-    private int fiveMatch = 0;
-    private int sixMatch = 0;
-    private int totalCount = 0;
-
-    public int threeMatch() {
-        return threeMatch;
-    }
-
-    public int fourMatch() {
-        return fourMatch;
-    }
-
-    public int fiveMatch() {
-        return fiveMatch;
-    }
-
-    public int sixMatch() {
-        return sixMatch;
-    }
-
-    public void collectWin(int num) {
-        if (num == 3) {
-            this.threeMatch++;
-        }
-        if (num == 4) {
-            this.fourMatch++;
-        }
-        if (num == 5) {
-            this.fiveMatch++;
-        }
-        if (num == 6) {
-            this.sixMatch++;
-        }
-        totalCount++;
-    }
-
-    public double calculateTotalProfit() {
-        int totalProfit = (threeMatch * WinProfit.THREE_MATCH_PROFIT.value) + (fourMatch * WinProfit.FOUR_MATCH_PROFIT.value) + (fiveMatch * WinProfit.FIVE_MATCH_PROFIT.value) + (sixMatch * WinProfit.SIX_MATCH_PROFIT.value);
-        BigDecimal divisor = new BigDecimal(totalCount * LOTTO_PRICE);
+        BigDecimal divisor = new BigDecimal(lottoList.size() * LOTTO_PRICE);
         BigDecimal totalProfitRatio = new BigDecimal(totalProfit).divide(divisor, 2, BigDecimal.ROUND_HALF_DOWN);
         return totalProfitRatio.doubleValue();
     }
 
+    public static int countLottoWinNumMatch(List<Lotto> lottoList, LottoWin lottoWin) {
+        int count = 0;
+        for (Lotto lotto : lottoList) {
+            count += getLottoWinNumMatchValue(lotto, lottoWin);
+        }
+        return count;
+    }
+
+    private static int getLottoWinNumMatchValue(Lotto lotto, LottoWin lottoWin) {
+        if (isLottoWinNumMatch(lotto, lottoWin)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private static boolean isLottoWinNumMatch(Lotto lotto, LottoWin lottoWin) {
+        return lottoWin == lotto.lottoWin();
+    }
 }
