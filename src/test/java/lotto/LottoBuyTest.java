9a1e64890021add829b1761d0efe8657d2d13eea
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-30 01:50:03 +0900

    feator : 수동 / 자동 로또 구매 메서드 분리 하여 총 카운트 테스트 코드

--- src/test/java/lotto/LottoBuyTest.java
+++ src/test/java/lotto/LottoBuyTest.java
@@ -3,6 +3,14 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +25,25 @@ public class LottoBuyTest {
     @DisplayName("구매 금액 만큼 로또 구매 ( 가격은 1000원에 한장 )")
     @Test
     void countBuyLotto_구매_로또_갯수_검증_테스트() {
-        Lottos lottos = lottoBuy.buyLotto(new Pay(14000));
+        Lottos lottos = lottoBuy.buyAutoLotto(14);
         assertThat(lottos.isCount(14)).isTrue();
+    }
+
+    @DisplayName("수동 구매와 자동 구매 후 로또 갯수 확인")
+    @ParameterizedTest
+    @MethodSource("buyLottoList")
+    void countAutoAndManualLotto(List<String> manualNumbers, int autoNumberCount, int allCount) {
+        LottoBuy lottoBuy = new LottoBuy();
+        for (String manualNumber : manualNumbers) {
+            lottoBuy.buyManualLotto(manualNumber);
+        }
+        Lottos lottos = lottoBuy.buyAutoLotto(autoNumberCount);
+        assertThat(lottos.isCount(allCount)).isTrue();
+    }
+
+    static Stream<Arguments> buyLottoList() {
+        return Stream.of(
+                Arguments.of(Arrays.asList("1,2,3,4,5,6", "1,2,3,4,5,6"), 5, 7)
+        );
     }
 }
