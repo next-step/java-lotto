b293c5550224061d2e7262974a04bf2e0ce22344
fDevJc <fDevJc@gmail.com>
2021-05-31 00:08:16 +0900

    refactor: LottoCount 로또갯수 래핑클래스 파라미터로 수정

--- src/main/java/lotto/ui/InputUI.java
+++ src/main/java/lotto/ui/InputUI.java
@@ -1,5 +1,7 @@
 package lotto.ui;
 
+import lotto.domains.LottoCount;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -23,9 +25,9 @@ public class InputUI {
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public static List<String> inputManualLottosNumbers(int manualLottoAmount) {
+    public static List<String> inputManualLottosNumbers(LottoCount manualLottoCount) {
         List<String> returnStringList = new ArrayList<>();
-        for (int i = 0; i < manualLottoAmount; i++) {
+        for (int i = 0; i < manualLottoCount.count(); i++) {
             returnStringList.add(scanner.nextLine());
         }
         return returnStringList;
