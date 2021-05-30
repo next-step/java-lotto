eae1b1831b996fbfbc00e600afe0ddeaf7cfe605
fDevJc <fDevJc@gmail.com>
2021-05-31 00:07:02 +0900

    refactor: 현재금액으로 n개를 구입가능여부 반환 메서드 추가

--- src/main/java/lotto/domains/Cash.java
+++ src/main/java/lotto/domains/Cash.java
@@ -1,5 +1,4 @@
 package lotto.domains;
-
 public class Cash {
     private static final int LOTTO_PRICE = 1000;
     private final int amount;
@@ -17,5 +16,9 @@ public class Cash {
         if (amount < LOTTO_PRICE) {
             throw new IllegalArgumentException("1000 보다 큰숫자를 입력해주세요");
         }
+    }
+
+    public boolean isPurchasesAvailable(int count) {
+        return numberOfPurchasesAvailable() >= count;
     }
 }
