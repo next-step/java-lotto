5a0bd0538bf0b9656e203d4be711bf98d0ad9ef9
fDevJc <fDevJc@gmail.com>
2021-05-30 17:20:12 +0900

    feat: 구매정보를 통해 수동로또, 오토로또 생성 메서드 추가

--- src/main/java/lotto/domains/LottoMachine.java
+++ src/main/java/lotto/domains/LottoMachine.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
+
+    /*
+    * TODO
+    *  PurchasingInfo(구압가격, 수동로또갯수, 수동로또번호리스트) 를 받아
+    *  수동로또 오토로또 생성
+    */
+
     private LottoMachine() {}
 
     public static Lottos makeLottos(Cash cash) {
@@ -12,5 +19,24 @@ public class LottoMachine {
             lottoList.add(new Lotto());
         }
         return new Lottos(lottoList);
+    }
+
+    public static Lottos makeLottos(PurchasingInfo purchasingInfo) {
+        List<Lotto> lottoList = new ArrayList<>();
+        makeAutomaticLottos(lottoList, purchasingInfo);
+        makeManualLottos(lottoList, purchasingInfo);
+        return new Lottos(lottoList);
+    }
+
+    private static void makeAutomaticLottos(List<Lotto> lottoList, PurchasingInfo purchasingInfo) {
+        for (int i = 0; i < purchasingInfo.autoLottoCount(); i++) {
+            lottoList.add(new Lotto());
+        }
+    }
+
+    private static void makeManualLottos(List<Lotto> lottoList, PurchasingInfo purchasingInfo) {
+        for (String stringLottoNumber : purchasingInfo.manualLottoList()) {
+            lottoList.add(new Lotto(stringLottoNumber));
+        }
     }
 }
