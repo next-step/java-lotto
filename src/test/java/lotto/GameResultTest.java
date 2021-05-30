c149b4164d6f0bbed9ac3c41a8cc7510a5c4102f
wrallee <wrallee@naver.com>
2021-05-31 00:57:32 +0900

    refactor(gameResult): ParameterizedTest에 메소드 소스 적용

--- src/test/java/lotto/GameResultTest.java
+++ src/test/java/lotto/GameResultTest.java
@@ -1,17 +1,21 @@
 package lotto;
 
+import static lotto.LottoGenerator.*;
 import static lotto.Rank.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class GameResultTest {
 
@@ -19,29 +23,19 @@ public class GameResultTest {
 
 	@BeforeEach
 	void setUp() {
-		Lotto lotto1 = LottoGenerator.generate("1,2,3,4,5,6");
-		Lotto lotto2 = LottoGenerator.generate("7,8,9,10,11,12");
-		Lotto lotto3 = LottoGenerator.generate("40,41,42,43,44,45");
-		Lotto lotto4 = LottoGenerator.generate("40,41,42,43,44,45");
-		Lotto lotto5 = LottoGenerator.generate("40,41,42,43,44,45");
+		Lotto lotto1 = generate("1,2,3,4,5,6");
+		Lotto lotto2 = generate("7,8,9,10,11,12");
+		Lotto lotto3 = generate("40,41,42,43,44,45");
+		Lotto lotto4 = generate("40,41,42,43,44,45");
+		Lotto lotto5 = generate("40,41,42,43,44,45");
 		lottos = new Lottos(new ArrayList<>(Arrays.asList(lotto1, lotto2, lotto3, lotto4, lotto5)));
 	}
 
 	@ParameterizedTest
-	@CsvSource(value = {
-		"1,2,3,4,5,6		:7		:0:0:0:0:1",
-		"2,3,4,5,6,7		:1		:0:0:0:1:0",
-		"2,3,4,5,6,7		:45		:0:0:1:0:0",
-		"4,5,6,7,8,9		:45		:2:0:0:0:0",
-		"40,41,42,43,44,45	:1		:0:0:0:0:3"
-	}, delimiter = ':')
+	@MethodSource("winningLottoAndGameResultProvider")
 	@DisplayName("당첨 결과 확인")
-	void 당첨_결과_확인(String lottoNumbers, int bonusNumber,
+	void 당첨_결과_확인(WinningLotto winningLotto,
 			int fifth, int fourth, int third, int second, int first) {
-		Lotto winningNumber = LottoGenerator.generate(lottoNumbers);
-		LottoNumber bonusBall = LottoNumber.valueOf(bonusNumber);
-		WinningLotto winningLotto = new WinningLotto(winningNumber, bonusBall);
-
 		GameResult result = new GameResult(winningLotto, lottos);
 		assertThat(result.dashboard().get(FIFTH)).isEqualTo(fifth);
 		assertThat(result.dashboard().get(FOURTH)).isEqualTo(fourth);
@@ -50,27 +44,38 @@ public class GameResultTest {
 		assertThat(result.dashboard().get(FIRST)).isEqualTo(first);
 	}
 
+	static Stream<Arguments> winningLottoAndGameResultProvider() {
+		return Stream.of(
+			arguments(new WinningLotto(generate("1,2,3,4,5,6"), LottoNumber.valueOf(7)),		0,0,0,0,1),
+			arguments(new WinningLotto(generate("2,3,4,5,6,7"), LottoNumber.valueOf(1)),		0,0,0,1,0),
+			arguments(new WinningLotto(generate("2,3,4,5,6,7"), LottoNumber.valueOf(45)),		0,0,1,0,0),
+			arguments(new WinningLotto(generate("4,5,6,7,8,9"), LottoNumber.valueOf(45)),		2,0,0,0,0),
+			arguments(new WinningLotto(generate("40,41,42,43,44,45"), LottoNumber.valueOf(1)),	0,0,0,0,3)
+		);
+	}
+
 	@ParameterizedTest
-	@CsvSource(value = {
-		"1,2,3,4,5,6		:7		:400000.00",
-		"2,3,4,5,6,7		:1		:6000.00",
-		"2,3,4,5,6,7		:45		:300.00",
-		"4,5,6,7,8,9		:45		:2",
-		"40,41,42,43,44,45	:1		:1200000.00"
-	}, delimiter = ':')
+	@MethodSource("winningLottoAndEarningRateProvider")
 	@DisplayName("당첨 수익률 확인")
-	void 당첨_수익률_확인(String lottoNumbers, int bonusNumber, double expectedRatio) {
-		Lotto winningNumber = LottoGenerator.generate(lottoNumbers);
-		LottoNumber bonusBall = LottoNumber.valueOf(bonusNumber);
-		WinningLotto winningLotto = new WinningLotto(winningNumber, bonusBall);
+	void 당첨_수익률_확인(WinningLotto winningLotto, double expectedRatio) {
 		GameResult result = new GameResult(winningLotto, lottos);
 		assertThat(result.earningRate()).isEqualTo(expectedRatio);
+	}
+
+	static Stream<Arguments> winningLottoAndEarningRateProvider() {
+		return Stream.of(
+			arguments(new WinningLotto(generate("1,2,3,4,5,6"), LottoNumber.valueOf(7)),		400000.00),
+			arguments(new WinningLotto(generate("2,3,4,5,6,7"), LottoNumber.valueOf(1)),		6000.00),
+			arguments(new WinningLotto(generate("2,3,4,5,6,7"), LottoNumber.valueOf(45)),		300.00),
+			arguments(new WinningLotto(generate("4,5,6,7,8,9"), LottoNumber.valueOf(45)),		2),
+			arguments(new WinningLotto(generate("40,41,42,43,44,45"), LottoNumber.valueOf(1)),	1200000.00)
+		);
 	}
 
 	@Test
 	@DisplayName("대시보드 불변 여부 확인")
 	void checkUnmodifiedDashboard() {
-		Lotto winningNumber = LottoGenerator.generate("1,2,3,4,5,6");
+		Lotto winningNumber = generate("1,2,3,4,5,6");
 		LottoNumber bonusBall = LottoNumber.valueOf(7);
 		WinningLotto winningLotto = new WinningLotto(winningNumber, bonusBall);
 		GameResult result = new GameResult(winningLotto, lottos);
