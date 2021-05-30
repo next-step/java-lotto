8ff40a6b6c06c817d8051c1a00c93e7cec36c272
fDevJc <fDevJc@gmail.com>
2021-05-31 00:12:56 +0900

    test: WinningLotto 파라미터 수정

--- src/test/java/lotto/domains/LottosTest.java
+++ src/test/java/lotto/domains/LottosTest.java
@@ -12,14 +12,11 @@ public class LottosTest {
 
     @Test
     void 로또리스트중_n개의숫자가_일치하는_로또카운팅_반환_테스트() {
-        LottoNumbers winningLottoNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-        int bonusNumber = 45;
-
         Lotto lotto1 = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 45)));
         Lotto lotto2 = new Lotto(new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 16)));
         Lottos lottos = new Lottos(Arrays.asList(lotto1, lotto2));
 
-        lottos.matchingWinningNumbersAndBonusNumber(winningLottoNumbers, bonusNumber);
+        lottos.matchingWinningNumbersAndBonusNumber(new WinningLotto("1,2,3,4,5,6", 45));
         assertThat(lottos.countOfMatchingNumber(MatchingInfo.MATCH_FIVE_AND_BONUS)).isEqualTo(1);
         assertThat(lottos.countOfMatchingNumber(MatchingInfo.MATCH_FIVE)).isEqualTo(1);
     }
