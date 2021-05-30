1e89d623b0373dcd24df9e00792637c9c94fff3c
ascii92der <ascii92der@gmail.com>
2021-05-30 15:44:01 +0900

    refactor: 당첨 통계에 2등 추가

--- src/main/java/study/ascii92der/lotto/step3n4/WinResult.java
+++ src/main/java/study/ascii92der/lotto/step3n4/WinResult.java
@@ -5,50 +5,26 @@ import java.util.*;
 public class WinResult {
 
     public static final int LOTTO_PRICE = 1000;
-    private Map<LottoWinner, Integer> result;
     private final int buyLottoCount;
+    private List<LottoResult> lottoResults;
 
-    public WinResult(List<Lotto> lottos, List<String> winnerNumberList) {
-        init();
-        List<LottoResult> lottoResults = new ArrayList<>();
+    public WinResult(List<Lotto> lottos, List<String> winnerNumberList, int bonusNumber) {
+        lottoResults = new ArrayList<>();
         for (Lotto lotto : lottos) {
-            lottoResults.add(new LottoResult(lotto, winnerNumberList));
-        }
-
-        for (LottoResult lottoResult : lottoResults) {
-            saveLottoResult(lottoResult);
+            lottoResults.add(new LottoResult(lotto, winnerNumberList, bonusNumber));
         }
         buyLottoCount = lottoResults.size();
-
-    }
-
-    private void init() {
-        result = new HashMap<>();
-        for (LottoWinner lottoWinner : LottoWinner.values()) {
-            result.put(lottoWinner, 0);
-        }
-    }
-
-    private void saveLottoResult(LottoResult lottoResult) {
-        for (LottoWinner lottoWinner : LottoWinner.values()) {
-            saveWinResult(lottoWinner, lottoResult.matchNumberCount());
-        }
-    }
-
-    private void saveWinResult(LottoWinner lottoWinner, int matchCount) {
-        if (lottoWinner.getMatchNumberCount() == matchCount) {
-            result.put(lottoWinner, result.get(lottoWinner) + 1);
-        }
     }
 
     public int result(LottoWinner lottoWinner) {
-        return result.get(lottoWinner);
+        return (int) lottoResults.stream().filter(
+                lottoResult -> lottoResult.result() == lottoWinner).count();
     }
 
     public double totalEarningRate() {
         double totalEarnWinnerPrice = 0;
-        for (LottoWinner lottoWinner : LottoWinner.values()) {
-            totalEarnWinnerPrice += (lottoWinner.getWinnerPrice() * result.get(lottoWinner));
+        for (LottoResult lottoResult : lottoResults) {
+            totalEarnWinnerPrice += lottoResult.result().getWinnerPrice();
         }
         return totalEarnWinnerPrice / (buyLottoCount * LOTTO_PRICE);
     }
--- src/test/java/study/ascii92der/lotto/step3n4/WinResultTest.java
+++ src/test/java/study/ascii92der/lotto/step3n4/WinResultTest.java
@@ -16,9 +16,10 @@ public class WinResultTest {
     void saveMatchLottoNumberThreeTest() {
         List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,11,22,33"),
                 new Lotto("1,2,3,12,21,44")));
-        WinResult winResult = new WinResult(lottos, new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))));
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), 45);
 
-        assertThat(winResult.result(LottoWinner.FOURTH)).isEqualTo(2);
+        assertThat(winResult.result(LottoWinner.FIFTH)).isEqualTo(2);
     }
 
     @Test
@@ -27,9 +28,10 @@ public class WinResultTest {
         List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,22,33"),
                 new Lotto("1,2,3,11,6,33"),
                 new Lotto("6,2,3,11,5,33")));
-        WinResult winResult = new WinResult(lottos, new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))));
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), 45);
 
-        assertThat(winResult.result(LottoWinner.THIRD)).isEqualTo(1);
+        assertThat(winResult.result(LottoWinner.FOURTH)).isEqualTo(3);
     }
 
     @Test
@@ -37,16 +39,18 @@ public class WinResultTest {
     void saveMatchLottoNumberFiveTest() {
         List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,5,33"),
                 new Lotto("1,2,6,4,5,33")));
-        WinResult winResult = new WinResult(lottos, new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))));
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), 45);
 
-        assertThat(winResult.result(LottoWinner.SECOND)).isEqualTo(2);
+        assertThat(winResult.result(LottoWinner.THIRD)).isEqualTo(2);
     }
 
     @Test
     @DisplayName("6개 일치 값 저장")
     void saveMatchLottoNumberSixTest() {
         List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,5,6")));
-        WinResult winResult = new WinResult(lottos, new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))));
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), 45);
 
         assertThat(winResult.result(LottoWinner.FIRST)).isEqualTo(1);
     }
@@ -55,17 +59,32 @@ public class WinResultTest {
     @DisplayName("일치되는 로또 번호 계산 테스트")
     void calculateMatchedNumberCountTest() {
         List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,5,33")));
-        WinResult winResult = new WinResult(lottos, new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))));
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), 45);
 
-        assertThat(winResult.result(LottoWinner.SECOND)).isEqualTo(1);
+        assertThat(winResult.result(LottoWinner.THIRD)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("총 수익률 계산")
     void calculateTotalEarningRate() {
-        List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,5,33"), new Lotto("1,2,3,16,17,18")));
-        WinResult winResult = new WinResult(lottos, new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))));
+        List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,5,33"),
+                new Lotto("1,2,41,16,17,18")));
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), 45);
 
-        assertThat(winResult.totalEarningRate()).isEqualTo(752.5);
+        assertThat(winResult.totalEarningRate()).isEqualTo(750.0);
+    }
+
+    @Test
+    @DisplayName("보너스 숫자를 포함한 총 수익률 계산")
+    void calculateTotalEarningRateWithBonus() {
+        List<Lotto> lottos = new ArrayList<>(Arrays.asList(new Lotto("1,2,3,4,5,33"),
+                new Lotto("1,2,41,16,17,18")));
+        int bonusNumber = 33;
+        WinResult winResult = new WinResult(lottos,
+                new ArrayList<>(Arrays.asList("1,2,3,4,5,6".split(","))), bonusNumber);
+
+        assertThat(winResult.totalEarningRate()).isEqualTo(15000.0);
     }
 }
