71aac7097c833285ea3787c50b803a4c20959f5a
fDevJc <fDevJc@gmail.com>
2021-05-30 22:41:10 +0900

    refactor: 기존 Cash를 받아 Lottos인스턴스를 생성하던 메서드 삭제

--- src/main/java/lotto/domains/LottoMachine.java
+++ src/main/java/lotto/domains/LottoMachine.java
@@ -5,26 +5,13 @@ import java.util.List;
 
 public class LottoMachine {
 
-    /*
-    * TODO
-    *  PurchasingInfo(구압가격, 수동로또갯수, 수동로또번호리스트) 를 받아
-    *  수동로또 오토로또 생성
-    */
-
-    private LottoMachine() {}
-
-    public static Lottos makeLottos(Cash cash) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < cash.numberOfPurchasesAvailable(); i++) {
-            lottoList.add(new Lotto());
-        }
-        return new Lottos(lottoList);
+    private LottoMachine() {
     }
-
+    //TODO refactor: 로또 객체생성(new Lotto())이 여기있는게 맞을까 ?
     public static Lottos makeLottos(PurchasingInfo purchasingInfo) {
         List<Lotto> lottoList = new ArrayList<>();
-        makeAutomaticLottos(lottoList, purchasingInfo);
         makeManualLottos(lottoList, purchasingInfo);
+        makeAutomaticLottos(lottoList, purchasingInfo);
         return new Lottos(lottoList);
     }
 
