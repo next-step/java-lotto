419d01653f200aae4170339f3c7fdaf91f2e5fa2
accidentlywoo <accidentlywoo@gmail.com>
2021-05-30 12:43:53 +0900

    test : lotto 도메인 랭킹 세팅 테스트

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
+import ui.Rank;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,9 +17,15 @@ class LottoTest {
 
 		assertThat(lotto.numbers()).isEqualTo(numbers);
 	}
-	
 	@Test
-	public void 로또번호_매칭테스트_3개_맞췄을때(){
-	    
+	public void 로또_랭킹세팅_테스트(){
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+		final Lotto lotto = new Lotto(numbers);
+		assertThat(lotto.rank()).isEqualTo(Rank.NONE);
+
+		lotto.rank(Rank.FIFTH);
+
+		assertThat(lotto.rank()).isEqualTo(Rank.FIFTH);
 	}
 }
