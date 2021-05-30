739df6a60735c489c55ded3280dfd034b91c8ebf
fDevJc <fDevJc@gmail.com>
2021-05-31 01:27:30 +0900

    refactor: 수동로또 카운트의 래핑클래스를 생성하며 불필요 코드 삭제

--- src/main/java/lotto/domains/PurchasingInfo.java
+++ src/main/java/lotto/domains/PurchasingInfo.java
@@ -3,35 +3,16 @@ package lotto.domains;
 import java.util.List;
 
 public class PurchasingInfo {
-    /*
-    *TODO
-    * 자동로또 갯수
-    * 수동로또 갯수
-    * 구입금액 밸리데이션체크
-    *
-     */
-    private final Cash cash;
-    private final int manualLottoCount;
-    private final List<String> manualLottoList;
 
-    public PurchasingInfo(Cash cash, int manualLottoCount, List<String> manualLottos) {
+    private final Cash cash;
+    private final List<String> manualLottoList;
+    public PurchasingInfo(Cash cash, List<String> manualLottos) {
         this.cash = cash;
-        validateLottoCount(manualLottoCount);
-        this.manualLottoCount = manualLottoCount;
         this.manualLottoList = manualLottos;
     }
 
-    public PurchasingInfo(int cash, int manualLottoCount, List<String> manualLottos) {
-        this(new Cash(cash),manualLottoCount,manualLottos);
-    }
-
-    private void validateLottoCount(int manualLottoCount) {
-        if(manualLottoCount < 0) {
-            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-        }
-        if (manualLottoCount > cash.numberOfPurchasesAvailable()) {
-            throw new IllegalArgumentException("구입가능금액을 넘었습니다.");
-        }
+    public PurchasingInfo(int cash, List<String> manualLottos) {
+        this(new Cash(cash),manualLottos);
     }
 
     public List<String> manualLottoList() {
@@ -39,10 +20,9 @@ public class PurchasingInfo {
     }
 
     public int manualLottoCount() {
-        return this.manualLottoCount;
+        return this.manualLottoList.size();
     }
 
     public int autoLottoCount() {
-        return cash.numberOfPurchasesAvailable() - manualLottoCount;
-    }
-}
+        return cash.numberOfPurchasesAvailable() - manualLottoList.size();
+    }}
