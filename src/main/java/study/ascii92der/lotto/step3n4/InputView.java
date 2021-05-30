710d27e46e2fdd51f70a3f279224b84ff5e80a6a
ascii92der <ascii92der@gmail.com>
2021-05-30 15:33:09 +0900

    feat: 보너스 볼 입력 추가

--- src/main/java/study/ascii92der/lotto/step3n4/InputView.java
+++ src/main/java/study/ascii92der/lotto/step3n4/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     public static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNER_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
+    public static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 
     public static LottoPrice inputMoney() {
         System.out.println(INPUT_MONEY);
@@ -18,5 +19,9 @@ public class InputView {
     public static List<String> inputWinnerNumbers() {
         System.out.println(INPUT_WINNER_NUMBERS);
         return new ArrayList<>(Arrays.asList(scanner.nextLine().split(",")));
+    }
+    public static int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        return Integer.parseInt(scanner.nextLine());
     }
 }
