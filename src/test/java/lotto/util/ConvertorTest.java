df22788334feb53ff546faabadd774484052c1fc
fDevJc <fDevJc@gmail.com>
2021-05-30 17:16:09 +0900

    test: 테스트 메서드 수정

--- src/test/java/lotto/util/ConvertorTest.java
+++ src/test/java/lotto/util/ConvertorTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ConvertorTest {
     @Test
     void 스트링_리스트_반환_테스트() {
-        assertThat(Convertor.convertStringToList("1,2,3,4,5,6").contains(1)).isTrue();
-        assertThat(Convertor.convertStringToList("1, 2, 3, 4, 5, 6").contains(1)).isTrue();
+        assertThat(Convertor.convertStringToList("1,2,3,4,5,6")).contains(1);
+        assertThat(Convertor.convertStringToList("1, 2, 3, 4, 5, 6")).contains(1);
     }
 }
