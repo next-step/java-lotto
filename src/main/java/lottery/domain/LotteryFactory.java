23317ef6b401e3cf833cac37a829000677bd2127
sanggu <tkdrn8578@naver.com>
2021-06-01 00:05:29 +0900

    refactor: 매직넘버 추출 및 matchAllAndAddCounts 메서드 추출

--- src/main/java/lottery/domain/LotteryFactory.java
+++ src/main/java/lottery/domain/LotteryFactory.java
@@ -1,21 +1,47 @@
 package lottery.domain;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import static lottery.domain.WinnerLottery.DEFAULT_MATCH_COUNT;
+import static lottery.domain.WinnerLottery.MATCH_COUNT;
+
 public class LotteryFactory {
 
-    static Map<Integer, Integer> matchAll(Lotteries lotteries, WinnerLottery winnerLottery) {
-        Map<Integer, Integer> matchCountPairs = new HashMap<>();
-        matchCountPairs.put(3, 0);
-        matchCountPairs.put(4, 0);
-        matchCountPairs.put(5, 0);
-        matchCountPairs.put(6, 0);
+    public static final int THREE_MATCH = 3;
+    public static final int FOUR_MATCH = 4;
+    public static final int FIVE_MATCH = 5;
+    public static final int SIX_MATCH = 6;
+
+    ImmutableMap<Integer, Integer> matchAll(Lotteries lotteries, WinnerLottery winnerLottery) {
+        Map<Integer, Integer> matchCountPairs = matchAllAndAddCounts(lotteries, winnerLottery);
+        return ImmutableMap.copyOf(matchCountPairs);
+    }
+
+    private Map<Integer, Integer> matchAllAndAddCounts(Lotteries lotteries, WinnerLottery winnerLottery) {
+        Map<Integer, Integer> matchCountPairs = initializeMatchCountPairs();
 
         for (Lottery lottery : lotteries.lotteries()) {
             int matchCount = winnerLottery.match(lottery.numbers());
-            matchCountPairs.put(matchCount, matchCountPairs.getOrDefault(matchCount, 0) + 1);
+            matchCountPairs.put(matchCount, addMatchCount(matchCountPairs, matchCount));
         }
+
+        return matchCountPairs;
+    }
+
+    private int addMatchCount(Map<Integer, Integer> matchCountPairs, int matchCount) {
+        return matchCountPairs.getOrDefault(matchCount, DEFAULT_MATCH_COUNT) + MATCH_COUNT;
+    }
+
+    private Map<Integer, Integer> initializeMatchCountPairs() {
+        Map<Integer, Integer> matchCountPairs = new HashMap<>();
+
+        matchCountPairs.put(THREE_MATCH, DEFAULT_MATCH_COUNT);
+        matchCountPairs.put(FOUR_MATCH, DEFAULT_MATCH_COUNT);
+        matchCountPairs.put(FIVE_MATCH, DEFAULT_MATCH_COUNT);
+        matchCountPairs.put(SIX_MATCH, DEFAULT_MATCH_COUNT);
 
         return matchCountPairs;
     }
