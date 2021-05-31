8998e2918f95d91add7a63156d3aef1fc67e2d1e
sanggu <tkdrn8578@naver.com>
2021-05-31 21:06:01 +0900

    feat: 가격이라는 값을 가지는 wrapping class 추가

---
+++ src/main/java/lottery/Price.java
@@ -0,0 +1,25 @@
+package lottery;
+
+import java.util.Objects;
+
+public class Price {
+
+    private final Integer price;
+
+    public Price(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return Objects.equals(price, price1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+}
