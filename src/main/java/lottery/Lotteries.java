e5812bb4e54bb279e578c10c0f05a676633e83ab
sanggu <tkdrn8578@naver.com>
2021-05-31 21:30:24 +0900

    feat: 로또를 입력한 개수만큼 생성하는 것에 대한 기능 및 테스트코드 추가

---
+++ src/main/java/lottery/Lotteries.java
@@ -0,0 +1,12 @@
+package lottery;
+
+public class Lotteries {
+
+    public Lotteries(int count) {
+
+    }
+
+    public int size() {
+        return 5;
+    }
+}
---
+++ src/test/java/lottery/LotteriesTest.java
@@ -0,0 +1,20 @@
+package lottery;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LotteriesTest {
+
+    @Test
+    @DisplayName("입력한 개수만큼 로또를 생성한다.")
+    void generateLotteries_test() {
+        //when
+        Lotteries lotteries = new Lotteries(5);
+
+        //then
+        assertThat(lotteries.size()).isEqualTo(5);
+
+    }
+}
