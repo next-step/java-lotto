3aa469a65dd8bf0b246453f4a06b96b12e515d2c
sanggu <tkdrn8578@naver.com>
2021-05-31 22:04:35 +0900

    refactor: GenerateCount wrapping class 추가에 따른 로직 수정

--- src/main/java/lottery/Lotteries.java
+++ src/main/java/lottery/Lotteries.java
@@ -8,26 +8,17 @@ import java.util.List;
 public class Lotteries {
 
     public static final int START_INDEX = 0;
-    public static final int MINIMUM_GENERATE_COUNT = 1;
 
     private final ImmutableList<Lottery> lotteries;
 
-    public Lotteries(int count, NumberGenerator generator) {
-        validateGenerateCount(count);
-
+    public Lotteries(GenerateCount count, NumberGenerator generator) {
         lotteries = ImmutableList.copyOf(generateLotteries(count, generator));
     }
 
-    private void validateGenerateCount(int count) {
-        if (count < MINIMUM_GENERATE_COUNT) {
-            throw new IllegalArgumentException("로또 생성 개수는 1개 이상이어야 합니다.");
-        }
-    }
-
-    private List<Lottery> generateLotteries(int count, NumberGenerator generator) {
+    private List<Lottery> generateLotteries(GenerateCount generateCount, NumberGenerator generator) {
         List<Lottery> lotteries = new ArrayList<>();
 
-        for (int index = START_INDEX; index < count; index++) {
+        for (int index = START_INDEX; index < generateCount.count(); index++) {
             lotteries.add(new Lottery(new LotteryNumbers(generator.generate())));
         }
 
--- src/test/java/lottery/LotteriesTest.java
+++ src/test/java/lottery/LotteriesTest.java
@@ -2,11 +2,8 @@ package lottery;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LotteriesTest {
 
@@ -14,17 +11,9 @@ public class LotteriesTest {
     @DisplayName("입력한 개수만큼 로또를 생성한다.")
     void generateLotteries_test() {
         //when
-        Lotteries lotteries = new Lotteries(5, new MockNumberGenerator());
+        Lotteries lotteries = new Lotteries(new GenerateCount(5), new MockNumberGenerator());
 
         //then
         assertThat(lotteries.size()).isEqualTo(5);
     }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, -1, -20})
-    @DisplayName("로또 생성 개수가 1개미만인 경우 예외처리한다.")
-    void validateGenerateCount_test(int count) {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Lotteries(count, new MockNumberGenerator()));
-    }
-
 }
