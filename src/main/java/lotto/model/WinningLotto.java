f4e1a8cede732d65f5d06ff7d3d42ff6d82a85b2
Miot2J <ck96117@gmail.com>
2021-06-01 01:32:17 +0900

    refactor: WinningLotto에 보너스볼,로또번호의 중복 유효성 검사 추가

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -3,12 +3,21 @@ package lotto.model;
 import java.util.Set;
 
 public class WinningLotto {
+    private static final String BONUS_BALL_CONTAIN_LOTTO_NUMBER_MESSAGE = "로또넘버와 보너스볼은 중첩 될 수 없습니다.";
+
     private final Lotto winningLotto;
     private final LottoNumber bonusBall;
 
     public WinningLotto(Set<LottoNumber> lottoNumbers, LottoNumber bonusBall) {
+        validateBonusBallNotContainLottoNumbers(lottoNumbers, bonusBall);
         this.winningLotto = new Lotto(lottoNumbers);
         this.bonusBall = bonusBall;
+    }
+
+    private void validateBonusBallNotContainLottoNumbers(Set<LottoNumber> lottoNumbers, LottoNumber bonusBall) {
+        if (lottoNumbers.contains(bonusBall)) {
+            throw new IllegalArgumentException(BONUS_BALL_CONTAIN_LOTTO_NUMBER_MESSAGE);
+        }
     }
 
     public WinningState makeWinningState(Lotto lotto) {
@@ -37,9 +46,5 @@ public class WinningLotto {
 
     public Set<LottoNumber> getLottoNumbers() {
         return winningLotto.getLottoNumbers();
-    }
-
-    public LottoNumber getBonusBall() {
-        return bonusBall;
     }
 }
