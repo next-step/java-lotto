66bdc38d24583b0612abce129101d7eb1b099e5d
kimsanggu <tkdrn8578@paymint.co.kr>
2021-05-31 09:26:39 +0900

    refactor: 로또 번호의 최소 및 최대 값에 대한 상수값 추출 및 Integer로 받도록 수정

--- src/test/java/lottery/LotteryNumber.java
+++ src/test/java/lottery/LotteryNumber.java
@@ -2,8 +2,11 @@ package lottery;
 
 public class LotteryNumber {
 
-    public LotteryNumber(String s) {
-        if (Integer.parseInt(s) > 45 || Integer.parseInt(s) < 1) {
+    private static final int MAXIMUM_LOTTERY_NUMBER = 45;
+    private static final int MINIMUM_LOTTERY_NUMBER = 1;
+
+    LotteryNumber(Integer number) {
+        if (number > MAXIMUM_LOTTERY_NUMBER || number < MINIMUM_LOTTERY_NUMBER) {
             throw new IllegalArgumentException("로또번호는 1 ~ 45 사이의 숫자이어야 합니다.");
         }
     }
--- src/test/java/lottery/LotteryNumberTest.java
+++ src/test/java/lottery/LotteryNumberTest.java
@@ -10,6 +10,6 @@ public class LotteryNumberTest {
     @Test
     @DisplayName("로또번호는 1 ~ 45 사이의 숫자이다.")
     void validateLotteryNumbers_test() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new LotteryNumber("48"));
+        assertThatIllegalArgumentException().isThrownBy(() -> new LotteryNumber(48));
     }
 }
