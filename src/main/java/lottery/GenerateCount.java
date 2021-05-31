9dc7c17737ebee27f6c2d84c41c24576ec6b0c23
sanggu <tkdrn8578@naver.com>
2021-05-31 22:03:52 +0900

    feat: 생성 개수에 대한 wrapping class 추가

---
+++ src/main/java/lottery/GenerateCount.java
@@ -0,0 +1,24 @@
+package lottery;
+
+public class GenerateCount {
+
+    public static final int MINIMUM_GENERATE_COUNT = 1;
+
+    private final int count;
+
+    public GenerateCount(int count) {
+        validateGenerateCount(count);
+
+        this.count = count;
+    }
+
+    private void validateGenerateCount(int count) {
+        if (count < MINIMUM_GENERATE_COUNT) {
+            throw new IllegalArgumentException("로또 생성 개수는 1개 이상이어야 합니다.");
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+}
