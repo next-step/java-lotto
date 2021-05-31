1b489e3211190ba0434353a37eb634b51a220f8f
jaewanLee <ianlee9180@gmail.com>
2021-05-31 20:52:08 +0900

    feat : 보너스 번호 포함하도록 OutPutView 수정
    - private static String getLottoWinCountString(LottoWin lottoWin, int countLottoWinNumMatch) : 당첨 횟수와 순위를 포함하는 포맷으로 String을 return한다.
    - private static String getLottoWinTemplate(LottoTicket lottoTicket) : 당첨 순위와 횟수를 포함한 템플릿을 return 한다.

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -7,6 +7,7 @@ import static java.lang.String.format;
 
 public class OutputView {
     private static final String MATCH_WORDING = "%s개 일치(%s원)- %s개";
+    private static final String MATCH_WORDING_WITH_BONUS = "%s개 일치, 보너스 볼 일치(%s원)- %s개";
     private static final String TOTAL_MATCH_WORDING_LOSS = "총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
     private static final String TOTAL_MATCH_WORDING_PROFIT = "총 수익률은 %s 입니다.(기준이 1이기 때문에 결과적으로 이득라는 의미임)";
 
@@ -32,10 +33,7 @@ public class OutputView {
     }
 
     private static String makeWinWords(LottoTicket lottoTicket) {
-        String output = makeLottoWinCountString(LottoWin.FOURTH_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.FOURTH_PLACE));
-        output += makeLottoWinCountString(LottoWin.THIRD_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.THIRD_PLACE));
-        output += makeLottoWinCountString(LottoWin.SECOND_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.SECOND_PLACE));
-        output += makeLottoWinCountString(LottoWin.FIRST_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.FIRST_PLACE));
+        String output = getLottoWinTemplate(lottoTicket);
         double totalProfitRatio = LottoStatistics.calculateLottoTicketProfit(lottoTicket);
         if (totalProfitRatio >= 1) {
             output += format(TOTAL_MATCH_WORDING_PROFIT, totalProfitRatio);
@@ -45,7 +43,19 @@ public class OutputView {
         return output;
     }
 
-    private static String makeLottoWinCountString(LottoWin lottoWin, int countLottoWinNumMatch) {
+    private static String getLottoWinTemplate(LottoTicket lottoTicket) {
+        String output = getLottoWinCountString(LottoWin.FIFTH_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.FIFTH_PLACE));
+        output += getLottoWinCountString(LottoWin.FOURTH_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.FOURTH_PLACE));
+        output += getLottoWinCountString(LottoWin.THIRD_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.THIRD_PLACE));
+        output += getLottoWinCountString(LottoWin.SECOND_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.SECOND_PLACE));
+        output += getLottoWinCountString(LottoWin.FIRST_PLACE, LottoStatistics.countLottoWinNumMatch(lottoTicket.lottoList(), LottoWin.FIRST_PLACE));
+        return output;
+    }
+
+    private static String getLottoWinCountString(LottoWin lottoWin, int countLottoWinNumMatch) {
+        if (lottoWin == LottoWin.SECOND_PLACE) {
+            return format(MATCH_WORDING_WITH_BONUS, lottoWin.matchNum(), lottoWin.winPrice(), countLottoWinNumMatch) + "\n";
+        }
         return format(MATCH_WORDING, lottoWin.matchNum(), lottoWin.winPrice(), countLottoWinNumMatch) + "\n";
     }
 
