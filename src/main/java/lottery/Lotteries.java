96322c0b43df69fe7dc3404198b52599d490e1fa
sanggu <tkdrn8578@naver.com>
2021-05-31 21:55:37 +0900

    refactor: NumberGenerator를 받아서 Lotteries를 생성하는 로직 추가

--- src/main/java/lottery/Lotteries.java
+++ src/main/java/lottery/Lotteries.java
@@ -1,12 +1,27 @@
 package lottery;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lotteries {
 
-    public Lotteries(int count) {
+    public static final int START_INDEX = 0;
 
+    private final ImmutableList<Lottery> lotteries;
+
+    public Lotteries(int count, NumberGenerator generator) {
+        List<Lottery> lotteries = new ArrayList<>();
+
+        for (int index = START_INDEX; index < count; index++) {
+            lotteries.add(new Lottery(new LotteryNumbers(generator.generate())));
+        }
+
+        this.lotteries = ImmutableList.copyOf(lotteries);
     }
 
     public int size() {
-        return 5;
+        return lotteries.size();
     }
 }
