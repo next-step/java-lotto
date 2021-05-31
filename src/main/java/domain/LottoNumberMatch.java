0af59ac259cf4d798ae0c9d21651d14106d1aebc
accidentlywoo <accidentlywoo@gmail.com>
2021-05-31 23:14:40 +0900

    fix : unneccessary Collection wrapping

--- src/main/java/domain/LottoNumberMatch.java
+++ src/main/java/domain/LottoNumberMatch.java
@@ -2,18 +2,17 @@ package domain;
 
 import ui.Rank;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class LottoNumberMatch {
-    private LottoNumberMatch () { }
+	private LottoNumberMatch () { }
 
-    public static Integer matchCount(List<Integer> winNumber, Lotto lotto) {
-        int matchCount = 0;
-        for (Integer number : winNumber) {
-            matchCount = Arrays.asList(lotto.numbers()).contains(number) ? matchCount+1 : matchCount;
-        }
+	public static Integer matchCount (List<Integer> winNumber, Lotto lotto) {
+		int matchCount = 0;
+		for (Integer number : winNumber) {
+			matchCount = lotto.numbers().contains(number) ? matchCount + 1 : matchCount;
+		}
 
-        return lotto.rank(Rank.valueOf(matchCount)).countOfMatch();
-    }
+		return lotto.rank(Rank.valueOf(matchCount)).countOfMatch();
+	}
 }
