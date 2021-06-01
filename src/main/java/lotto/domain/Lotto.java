209571caafe0ae008504b8d821275e804a143716
rockjoon <rockpago@gmail.com>
2021-06-01 22:07:10 +0900

    feat : 로또 번호 생성 기능
    * 로또 번호는 1부터 45 사이의 숫자 6개
    * 로또 번호는 서로 중복되면 안됨

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+
+    private static List<Integer> LOTTO_NUMBER_POOL = Arrays.asList(
+            1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45
+    );
+    private static int LOTTO_SIZE = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
+        validateLotto();
+    }
+
+    private void validateLotto() {
+        validateNumbers();
+        validateDuplicate();
+    }
+
+    private void validateDuplicate() {
+        Set<Integer> duplicateChecker = new HashSet<>(numbers);
+        if (duplicateChecker.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 " + LOTTO_SIZE + "개를 선택할 수 있습니다.");
+        }
+    }
+
+    private void validateNumbers() {
+        if (!LOTTO_NUMBER_POOL.containsAll(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45까지의 숫자로 구성되어야 합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
