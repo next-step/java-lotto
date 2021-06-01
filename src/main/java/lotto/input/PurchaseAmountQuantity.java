f8e2568a7697f119cf369db9941f6ef16add2394
Junhee Ko <jko@rainist.com>
2021-06-01 18:50:21 +0900

    refactor: PurchaseAmountQuantity class to get manual lotto numbers from user

--- src/main/java/lotto/input/PurchaseAmountQuantity.java
+++ src/main/java/lotto/input/PurchaseAmountQuantity.java
@@ -3,24 +3,41 @@ package lotto.input;
 import lotto.domain.MatchNumberCount;
 import lotto.domain.LottoRoi;
 
+import java.util.List;
+
 public final class PurchaseAmountQuantity {
 
   private final int amount;
   private final int quantity;
+  private final int manualCount;
+  private final int autoCount;
+  private final List<String> manualLottoNumbers;
 
-  public PurchaseAmountQuantity(int amount) {
+  public PurchaseAmountQuantity(int amount, int manualCount, List<String> manualLottoNumbers) {
     this.amount = amount;
     this.quantity = amount / 1000;
-    System.out.println(quantity + "개를 구매했습니다.");
+    this.manualCount = manualCount;
+    this.autoCount = quantity - manualCount;
+    this.manualLottoNumbers = manualLottoNumbers;
+
+    print();
   }
 
-  public int getQuantity() {
-    return quantity;
+  private void print() {
+    System.out.println("\n수동으로 " + this.manualCount + "장, 자동으로 " + this.autoCount + "개를 구매했습니다.");
   }
 
   public LottoRoi getRoiBy(MatchNumberCount matchNumberCount) {
     return new LottoRoi(
         Math.floor((double) matchNumberCount.getRevenue() / this.amount * 100) / 100.0
     );
+  }
+
+  public List<String> getManualLottoNumbers() {
+    return manualLottoNumbers;
+  }
+
+  public int getAutoCount() {
+    return autoCount;
   }
 }
