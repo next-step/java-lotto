737d52e0be59e3dc20e96626ad3418478f1bc27e
dohchoi91 <dohchoi91@gmail.com>
2021-06-01 22:30:20 +0900

    refactor: 특정 당첨(=2등)에 대한 코드 개선

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,35 +1,34 @@
 package lotto;
 
+import java.util.Arrays;
+
 public enum Prize {
-	FIRST(6, new Money(2_000_000_000)),
-	SECOND(5, new Money(30_000_000)),
-	THIRD(5, new Money(1_500_000)),
-	FOURTH(4, new Money(50_000)),
-	FIFTH(3, new Money(5_000)),
-	NOTHING(0, new Money(0));
+	FIRST(6, new Money(2_000_000_000), false),
+	SECOND(5, new Money(30_000_000), true),
+	THIRD(5, new Money(1_500_000), false),
+	FOURTH(4, new Money(50_000), false),
+	FIFTH(3, new Money(5_000), false),
+	NOTHING(0, new Money(0), false);
 
 	private final int count;
 	private final Money win;
+	private final boolean requiredBonusBall;
 
-	Prize(int count, Money winAmount) {
+	Prize(int count, Money win, boolean requiredBonusBall) {
 		this.count = count;
-		this.win = winAmount;
+		this.win = win;
+		this.requiredBonusBall = requiredBonusBall;
 	}
 
-	public static Prize valueOf(int count, boolean matchBonus) {
-		Prize result = NOTHING;
-		for (Prize prize : values()) {
-			result = isSameCount(count, prize) ? prize : result;
-		}
-		return isSecondPrize(result, matchBonus) ? Prize.SECOND : result;
+	public static Prize valueOf(int count, boolean bonusBall) {
+		return Arrays.stream(values())
+			.filter(rank -> rank.isMatched(count, bonusBall))
+			.findAny()
+			.orElse(NOTHING);
 	}
 
-	private static boolean isSecondPrize(Prize prize, boolean matchBonus) {
-		return prize.equals(Prize.THIRD) && matchBonus;
-	}
-
-	private static boolean isSameCount(int count, Prize prize) {
-		return prize.count() == count;
+	private boolean isMatched(int count, boolean matchBonusBall) {
+		return this.count == count && (!this.requiredBonusBall || matchBonusBall);
 	}
 
 	public int count() {
