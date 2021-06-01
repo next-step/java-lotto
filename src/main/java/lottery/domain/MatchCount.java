e8b5006ce2db9fbe60fb1af79c863a271f4745d7
kimsanggu <tkdrn8578@paymint.co.kr>
2021-06-01 18:46:31 +0900

    feat; MatchCount에 대한 개수 정보를 가지는 enum 추가

---
+++ src/main/java/lottery/domain/MatchCount.java
@@ -0,0 +1,24 @@
+package lottery.domain;
+
+import java.util.Arrays;
+
+public enum MatchCount {
+
+    SIX_MATCH(6),
+    FIVE_MATCH(5),
+    FOUR_MATCH(4),
+    THREE_MATCH(3);
+
+    private Integer count;
+
+    MatchCount(Integer count) {
+        this.count = count;
+    }
+
+    public static MatchCount valueOf(int count) {
+        return Arrays.stream(MatchCount.values())
+                .filter(matchCount -> matchCount.count == count)
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
