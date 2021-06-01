f7694ac260519b684c23e520cb78761918295cee
rockjoon <rockpago@gmail.com>
2021-06-01 23:49:49 +0900

    test : 로또 당첨 종류별 결과 확인 테스트

---
+++ src/test/java/lotto/domain/LottoPrizeTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static lotto.domain.LottoPrize.*;
+
+class LottoPrizeTest {
+
+    @DisplayName("종목별 당첨 횟수와 당첨 금액 합계 카운트")
+    @Test
+    void updateStatus() {
+        FIRST.updateWinningResult(FIRST.matchCount);
+        int totalWinningCount = FIRST.getTotalWinningCount();
+        long totalWinningPrize = FIRST.getTotalWinningPrize();
+        assertThat(totalWinningCount).isEqualTo(1);
+        assertThat(totalWinningPrize).isEqualTo(FIRST.matchPrize);
+    }
+
+    @DisplayName("로또 번호 일치 갯수에 따라 당첨 종류 구하기")
+    @Test
+    void findByMatchingCount() {
+        for (LottoPrize prize : values()) {
+            LottoPrize result = findByMatchCount(prize.matchCount);
+            assertThat(result).isEqualTo(prize);
+        }
+    }
+
+}
