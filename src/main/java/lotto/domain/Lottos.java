32ee0dd2d04482bbbeeadc949623f8ce98c43f8e
rockjoon <rockpago@gmail.com>
2021-06-01 22:42:13 +0900

    feat : 티켓 수 만큼 로또 무작위로 생성 기능

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto());
+        }
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+}
