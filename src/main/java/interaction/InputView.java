52605667917184f6882063cc7651a15394868cb3
yevgnenll <yevgnenll@gmail.com>
2021-06-02 00:40:01 +0900

    feat: 수동 구매를 위한 메세지 추가

--- src/main/java/interaction/InputView.java
+++ src/main/java/interaction/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
     private static final String PURCHASE_START_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -31,5 +32,10 @@ public class InputView {
         System.out.println("보너스 볼을 입력해 주세요.");
         int bonusNumber = scanner.nextInt();
         return bonusNumber;
+    }
+
+    public static int manualLottoCount() {
+        System.out.println(MANUAL_LOTTO_COUNT);
+        return scanner.nextInt();
     }
 }
