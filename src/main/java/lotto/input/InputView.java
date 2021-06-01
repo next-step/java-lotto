a55b3bf619f319cdecf09ac6807aba33e54b95ac
Junhee Ko <jko@rainist.com>
2021-06-01 18:48:48 +0900

    feat: manual lotto numbers input in InputView class

--- src/main/java/lotto/input/InputView.java
+++ src/main/java/lotto/input/InputView.java
@@ -1,6 +1,9 @@
 package lotto.input;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class InputView {
 
@@ -10,10 +13,39 @@ public final class InputView {
     this.scanner = scanner;
   }
 
-  public PurchaseAmountQuantity getPurchaseAmountQuantity() {
+  public PurchaseAmountQuantity input() {
+    int amount = getAmount();
+    int manualCount = getManualCount();
+    List<String> manualLottoNumbers = getManualLottoNumbers(manualCount);
+
+    return new PurchaseAmountQuantity(amount, manualCount, manualLottoNumbers);
+  }
+
+  private int getAmount() {
     System.out.println("구매 금액을 입력해주세요.");
 
-    return new PurchaseAmountQuantity(getIntValueFromUser());
+    int amount = scanner.nextInt();
+    clearScanner();
+
+    return amount;
+  }
+
+  private int getManualCount() {
+    System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+
+    int manualCount = scanner.nextInt();
+    clearScanner();
+
+    return manualCount;
+  }
+
+  private List<String> getManualLottoNumbers(int manualCount) {
+    System.out.println("\n수동으로 구매할 번호를 입력해 주세요.");
+
+    return IntStream
+        .range(0, manualCount)
+        .mapToObj(i -> scanner.nextLine())
+        .collect(Collectors.toList());
   }
 
   public WinningNumber getWinningNumber() {
@@ -24,7 +56,7 @@ public final class InputView {
   }
 
   private String[] getWinningNumbers() {
-    System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
 
     return getStringValueFromUser().split(",");
   }
