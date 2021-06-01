84f453eda713aa2c013be2cc3af4085a953da853
Junhee Ko <jko@rainist.com>
2021-06-01 18:49:03 +0900

    feat: add new constructor in Lotto class

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public final class Lotto {
 
@@ -16,6 +17,13 @@ public final class Lotto {
 
   public Lotto(NumberGenerator numberGenerator) {
     numbers = numberGenerator.generateNumbersOf(NUMBER_COUNT);
+    print(numbers);
+  }
+
+  public Lotto(String autoLottoNumber) {
+    numbers = Arrays.stream(autoLottoNumber.trim().replace(" ","").split(","))
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
     print(numbers);
   }
 
