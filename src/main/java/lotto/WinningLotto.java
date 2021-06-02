f90f4740d537ed29a59d2aa8a672ce1b4574e984
jaewanLee <ianlee9180@gmail.com>
2021-06-02 19:18:30 +0900

    refactor :  접근제어자 추가
    접근제어자가 없는 변수에 접근제어자를 추가한다.

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -3,7 +3,7 @@ package lotto;
 import java.util.List;
 
 public class WinningLotto {
-    List<LottoNumber> winningLotto;
+    private List<LottoNumber> winningLotto;
 
     public WinningLotto(List<LottoNumber> winningNumbers) {
         this.winningLotto = winningNumbers;
