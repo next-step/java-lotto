e9a63ba95ec353029aab4d0702aed1b7a627dc46
yevgnenll <yevgnenll@gmail.com>
2021-06-02 23:46:47 +0900

    feat: 수동 구매 개수를 1급 객체로 정의한다

---
+++ src/main/java/lotto/store/ManualCount.java
@@ -0,0 +1,15 @@
+package lotto.store;
+
+public class ManualCount {
+
+    private final int manualCount;
+
+    public ManualCount(int manualCount) {
+        this.manualCount = manualCount;
+    }
+
+    public int get() {
+        return manualCount;
+    }
+
+}
