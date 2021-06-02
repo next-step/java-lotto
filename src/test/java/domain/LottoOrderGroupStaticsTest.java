9b3de8544669869296d485d2a2d4a6bdba2aee3d
accidentlywoo <accidentlywoo@gmail.com>
2021-06-02 23:37:27 +0900

    test : 보너스 2등 매칭 테스트, 통계 테스트 케이스 강화
    소스코드가,.. 왤캐 날라간거같지.. ?

--- src/test/java/domain/LottoOrderGroupStaticsTest.java
+++ src/test/java/domain/LottoOrderGroupStaticsTest.java
@@ -1,24 +1,54 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
+import ui.Rank;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static util.LottoPrice.LOTTO_PRICE;
 
 class LottoOrderGroupStaticsTest {
 	@Test
-	public void 로또_숫자_맞춤_계산() {
-		//???
+	public void 수익률_계산_자연수_1장_1등 () {
+		List<Lotto> lottos = new ArrayList<>();
+		lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+		LottoOrderGroupStatics analysis = new LottoOrderGroupStatics(
+				Arrays.asList(1, 2, 3, 4, 5, 6)
+				, 7
+				, lottos
+		);
+
+		analysis.matchSetting();
+
+		assertThat(Rank.FIRST.money() / LOTTO_PRICE + ".00")
+				.isEqualTo(analysis.yield());
 	}
 
 	@Test
-	public void 수익률_계산() {
+	public void 수익률_계산_자연수_소숫 () {
+		List<Lotto> lottos = new ArrayList<>();
+		lottos.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+		lottos.add(new Lotto(Arrays.asList(11, 12, 13, 14, 15, 16)));
+		lottos.add(new Lotto(Arrays.asList(17, 18, 19, 20, 21, 22)));
+		lottos.add(new Lotto(Arrays.asList(23, 24, 25, 26, 27, 28)));
+		lottos.add(new Lotto(Arrays.asList(29, 30, 31, 32, 33, 34)));
+		lottos.add(new Lotto(Arrays.asList(35, 36, 37, 38, 39, 40)));
+
 		LottoOrderGroupStatics analysis = new LottoOrderGroupStatics(
-				Arrays.asList(1, 2, 3, 4, 5, 6)
-				, new LottoOrderGroup(10)
+				Arrays.asList(1, 2, 3, 8, 9, 10)
+				, 7
+				, lottos
 		);
-		//??
-		analysis.yield();
+
+		analysis.matchSetting();
+
+		System.out.println(analysis.yield());
+
+		assertThat("0.83")
+				.isEqualTo(analysis.yield());
 	}
 }
