46006171619a9aeca2367ab74f3a43e040512457
이예림 <dulfla_5@naver.com>
2021-06-03 20:49:44 +0900

    style : 상수명 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    private static final int numberOfNumbers = 6;
-    private static final int maxNumber = 45;
+    private static final int NUMBER_OF_NUMBERS = 6;
+    private static final int MAX_NUMBER = 45;
 
     private List<Integer> numbers = new ArrayList<>();
 
@@ -32,12 +32,12 @@ public class Lotto {
 
     public Lotto() {
         List<Integer> rangeOfNumbers = new ArrayList<>();
-        for (int j = 0; j < maxNumber; j++) {
+        for (int j = 0; j < MAX_NUMBER; j++) {
             rangeOfNumbers.add(j + 1);
         }
 
         Collections.shuffle(rangeOfNumbers);
-        for (int j = 0; j < numberOfNumbers; j++) {
+        for (int j = 0; j < NUMBER_OF_NUMBERS; j++) {
             addNumber(rangeOfNumbers.get(j));
         }
         Collections.sort(numbers);
@@ -49,7 +49,7 @@ public class Lotto {
 
     public int countWinningNumbers(List<Integer> winningNumbers) {
         int count = 0;
-        for (int i = 0; i < numberOfNumbers; i++) {
+        for (int i = 0; i < NUMBER_OF_NUMBERS; i++) {
             if (numbers.contains(winningNumbers.get(i))) {
                 count++;
             }
