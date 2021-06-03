e9df8d8af140a277b2855db23385f739683fc2f1
이예림 <dulfla_5@naver.com>
2021-06-03 13:27:08 +0900

    refactor : Lottos 로또들 일급 컬렉션

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
