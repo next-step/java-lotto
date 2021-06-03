04de62abe9c5c3f297e12ccb0d5af84cd9597f17
이예림 <dulfla_5@naver.com>
2021-06-03 13:47:37 +0900

    refactor : 사용하지 않는 메서드 제거, 메서드 순서 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -47,36 +47,6 @@ public class Lotto {
         numbers.add(number);
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    public String result(List<Integer> winningNumbers, int bonusNumber) {
-        int numberOfWinningNumbers = countWinningNumbers(winningNumbers);
-        boolean bonus = contains(bonusNumber);
-
-        if (numberOfWinningNumbers == 3) {
-            return "5등";
-        }
-
-        if (numberOfWinningNumbers == 4) {
-            return "4등";
-        }
-
-        if (numberOfWinningNumbers == 5 && !bonus) {
-            return "3등";
-        }
-
-        if (numberOfWinningNumbers == 5 && bonus) {
-            return "2등";
-        }
-
-        if (numberOfWinningNumbers == 6) {
-            return "1등";
-        }
-        return "당첨되지 않았습니다.";
-    }
-
     public int countWinningNumbers(List<Integer> winningNumbers) {
         int count = 0;
         for (int i = 0; i < numberOfNumbers; i++) {
@@ -89,5 +59,9 @@ public class Lotto {
 
     public boolean contains(int bonusNumber) {
         return numbers.contains(bonusNumber);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
