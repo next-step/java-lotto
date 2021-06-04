85df1d3c195c589acded13ea434ac5ed864a4645
이예림 <dulfla_5@naver.com>
2021-06-04 20:14:10 +0900

    refactor : Lotto 클래스는 6개의 로또번호와 당첨여부를 알고 있는 클래스

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,57 +1,20 @@
 package lotto.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    private static final int NUMBER_OF_NUMBERS = 6;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
 
-    private List<Integer> numbers = new ArrayList<>();
+    private List<Integer> lottoNumbers;
 
-    public Lotto(String manualNumbers) {
-        String[] splitManualNumbers = splitBySeparator(manualNumbers);
-        setManualLotto(splitManualNumbers);
-    }
-
-    private String[] splitBySeparator(String manualNumbers) {
-        String separator = ", ";
-        return manualNumbers.split(separator);
-    }
-
-    private void setManualLotto(String[] manualNumbers) {
-        for (int i = 0; i < manualNumbers.length; i++) {
-            addNumber(convertToInt(manualNumbers[i]));
-        }
-    }
-
-    private int convertToInt(String number) {
-        return Integer.parseInt(number);
-    }
-
-    public Lotto() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-        for (int j = MIN_LOTTO_NUMBER; j <= MAX_LOTTO_NUMBER; j++) {
-            lottoNumbers.add(j);
-        }
-
-        Collections.shuffle(lottoNumbers);
-        for (int j = 0; j < NUMBER_OF_NUMBERS; j++) {
-            addNumber(lottoNumbers.get(j));
-        }
-        Collections.sort(numbers);
-    }
-
-    private void addNumber(int number) {
-        numbers.add(number);
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
     }
 
     public int countWinningNumbers(Lotto winningNumbers) {
         int count = 0;
-        for (int i = 0; i < NUMBER_OF_NUMBERS; i++) {
-            if (numbers.contains(winningNumbers.getNumbers().get(i))) {
+        for (int i = 0; i < NUMBER_OF_LOTTO_NUMBERS; i++) {
+            if (lottoNumbers.contains(winningNumbers.getNumbers().get(i))) {
                 count++;
             }
         }
@@ -59,10 +22,10 @@ public class Lotto {
     }
 
     public boolean contains(int bonusNumber) {
-        return numbers.contains(bonusNumber);
+        return lottoNumbers.contains(bonusNumber);
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return lottoNumbers;
     }
 }
