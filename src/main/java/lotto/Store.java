10dc0214afd01c1c46c4a97804cf7758255a45fa
dohchoi91 <dohchoi91@gmail.com>
2021-06-05 00:08:34 +0900

    feat: 상점의 로또 수동 발급 기능 추가

--- src/main/java/lotto/Store.java
+++ src/main/java/lotto/Store.java
@@ -16,4 +16,22 @@ public class Store {
 		}
 		return new Lottos(lottos);
 	}
+
+	public static Lottos buy(Money amount, List<List<Integer>> manualNumbers) {
+		canBuyLottos(amount, manualNumbers.size());
+
+		List<Lotto> lottos = new ArrayList<>();
+		for (List<Integer> manualNumber : manualNumbers) {
+			lottos.add(new Lotto(manualNumber));
+		}
+		return new Lottos(lottos);
+	}
+
+	private static void canBuyLottos(Money amount, int count) {
+		int maxCount = (int)amount.divide(LOTTO_PRICE);
+		if (maxCount < count) {
+			throw new IllegalArgumentException("수동으로 살 수 있는 금액을 넘었습니다.");
+		}
+	}
+
 }
