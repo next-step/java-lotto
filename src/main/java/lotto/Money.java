3ec2530a674aef75a37461f6f72a9761861e6c23
jaewanLee <ianlee9180@gmail.com>
2021-06-06 15:32:09 +0900

    feat : 로또 구매 기능 추가
    - public void buyLotto(int number) : 넘겨받은 갯수만큼 로또를 구매하고 금액을 제외함

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static lotto.Lotto.LOTTO_PRICE;
+
 public class Money {
     private int money;
 
@@ -12,9 +14,20 @@ public class Money {
         return this.money;
     }
 
+    public void buyLotto(int number) {
+        validateBuyingLotto(number);
+        money = money - number * LOTTO_PRICE;
+    }
+
     private void validateMoney(int num) {
         if (num <= 0) {
             throw new RuntimeException("구매 급액은 0보다 커야합니다.");
+        }
+    }
+
+    private void validateBuyingLotto(int num) {
+        if (num * LOTTO_PRICE > money) {
+            throw new RuntimeException("구매 갯수가 금액보다 큽니다.");
         }
     }
 }
