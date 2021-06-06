912cb4b52318f8c2e192efb1ace0a945cfbe128b
jaewanLee <ianlee9180@gmail.com>
2021-06-06 15:56:44 +0900

    test : LottoTicket 생성자, add 테스트 추가

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -2,15 +2,35 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketTest {
     private final int LOTTO_NUM = 14;
-    LottoTicket lottoTicket = new LottoTicket(LOTTO_NUM);
+
 
     @Test
     void lottoList() {
+        LottoTicket lottoTicket = new LottoTicket(LOTTO_NUM);
         assertThat(lottoTicket.lottoList().size()).isEqualTo(LOTTO_NUM);
+    }
+
+    @Test
+    void lottoTicket() {
+        List<Lotto> lottoList = new ArrayList<>();
+        Lotto lotto = new Lotto();
+        lottoList.add(lotto);
+        LottoTicket lottoTicket = new LottoTicket(lottoList);
+        assertThat(lottoTicket.lottoList()).isEqualTo(lottoList);
+    }
+
+    @Test
+    void addLotto(){
+        LottoTicket lottoTicket=new LottoTicket(LOTTO_NUM);
+        lottoTicket.addLotto(1);
+        assertThat(lottoTicket.lottoList().size()).isEqualTo(LOTTO_NUM+1);
     }
 
 }
