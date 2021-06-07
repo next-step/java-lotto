2ebab0a0c319ad1938ef793a322f9bd5acacc4c5
박소연 <sycap01@gmail.com>
2021-06-07 09:43:12 +0900

    refactor : 오류수정

--- src/test/java/domain/lotto/RankTest.java
+++ src/test/java/domain/lotto/RankTest.java
@@ -5,14 +5,6 @@ import org.junit.jupiter.api.Test;
 
 public class RankTest {
     @Test
-    public void equals(){
-        Assertions.assertThat(Rank.FIRST == Rank.FIRST).isEqualTo(true);
-    }
-    @Test
-    public void frist(){
-        Assertions.assertThat(Rank.FIRST == Rank.FIRST).isEqualTo(true);
-    }
-    @Test
     public void plus(){
         final Operator operator = new Operator();
         final int result = operator.operate(3,5);
