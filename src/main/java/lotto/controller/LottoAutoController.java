8d895da72c807db1e1995b4f10a6838c91708cef
이예림 <dulfla_5@naver.com>
2021-06-08 20:46:38 +0900

    refactor : convertWinningNumbersToInt 반환값 변경

--- src/main/java/lotto/controller/LottoAutoController.java
+++ src/main/java/lotto/controller/LottoAutoController.java
@@ -20,7 +20,7 @@ public class LottoAutoController {
         Lottos lottos = new Lottos(autoLotto.createLottos(quantity));
         outputView.printLotto(quantity, lottos.getLottos());
 
-        Lotto winningNumbers = new Lotto(convertWinningNumbersToInt(inputView.inputWinningNumbers()));
+        Lotto winningNumbers = convertWinningNumbersToInt(inputView.inputWinningNumbers());
         int bonusNumber = convertBonusNumberToInt(inputView.inputBonusNumber());
         WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
 
@@ -29,14 +29,14 @@ public class LottoAutoController {
         outputView.outputEarningRate(lottos.calculateEarningRate(winningResult.calculateEarningPrice(), lottoPrice.getPrice()));
     }
 
-    private List<Integer> convertWinningNumbersToInt(String winningStringNumbers) {
+    private Lotto convertWinningNumbersToInt(String winningStringNumbers) {
         List<Integer> winningNumbers = new ArrayList<>();
         String[] splitWinningNumbers = winningStringNumbers.split(", ");
         for (int i = 0; i < splitWinningNumbers.length; i++) {
             int number = Integer.parseInt(splitWinningNumbers[i]);
             winningNumbers.add(number);
         }
-        return winningNumbers;
+        return new Lotto(winningNumbers);
     }
 
     private int convertBonusNumberToInt(String bonusStringNumber) {
