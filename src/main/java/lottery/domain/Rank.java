63b9c1858a124dc90c7f2ff325d76eac8af5e2ed
sanggu <tkdrn8578@naver.com>
2021-06-08 23:41:34 +0900

    refactor: Rank에 matchBonusBall에 대한 값을 포함시켜 조건문을 제거하도록 수정

--- src/main/java/lottery/domain/Rank.java
+++ src/main/java/lottery/domain/Rank.java
@@ -4,38 +4,32 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    FAIL(0, 0),
-    FIFTH(3, 5_000),
-    FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2000_000_000);
+    FAIL(0, 0, false),
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 1_500_000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2000_000_000, false);
 
     private final int matchCount;
     private final int profit;
+    private final boolean matchBonusBall;
 
-    Rank(int matchCount, int profit) {
+    Rank(int matchCount, int profit, boolean matchBonusBall) {
         this.matchCount = matchCount;
         this.profit = profit;
+        this.matchBonusBall = matchBonusBall;
     }
 
     public static Rank valueOf(int matchCount, boolean matchBonus) {
-        if (matchBonus && isSameMatchCount(matchCount, SECOND)) {
-            return SECOND;
-        }
-
-        return valueOf(matchCount);
-    }
-
-    public static Rank valueOf(int matchCount) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> isSameMatchCount(matchCount, rank))
+                .filter(rank -> rank.isSameMatchCount(matchCount) && rank.matchBonusBall == matchBonus)
                 .findFirst()
                 .orElse(FAIL);
     }
 
-    private static boolean isSameMatchCount(int matchCount, Rank rank) {
-        return rank.matchCount == matchCount;
+    private boolean isSameMatchCount(int matchCount) {
+        return this.matchCount == matchCount;
     }
 
     public int profit() {
