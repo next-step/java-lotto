e21034ff5f23a7097036dd8c5ecaa9115bb1d9a0
sanggu <tkdrn8578@naver.com>
2021-06-08 23:38:53 +0900

    test: match에 대한 테스트 코드 추가

--- src/test/java/lottery/domain/BonusBallTest.java
+++ src/test/java/lottery/domain/BonusBallTest.java
@@ -5,15 +5,20 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BonusBallTest {
 
     @Test
-    @DisplayName("보너스볼은 당첨 번호에 포함되면 안된다.")
-    void bonusBall_test() {
-        assertThatIllegalArgumentException().isThrownBy(
-                () -> new BonusBall(new WinnerLottery(new LotteryNumbers(List.of(1, 2, 3, 4, 5, 6))), 3)
-        );
+    @DisplayName("로또 번호들에 보너스볼이 일치하는게 있는지 확인한다.")
+    void match_test() {
+        //given
+        LotteryNumbers lotteryNumbers = new LotteryNumbers(List.of(1, 2, 3, 4, 5, 45));
+        BonusBall bonusBall = new BonusBall(5);
+
+        //when
+        boolean match = bonusBall.match(lotteryNumbers);
+
+        assertThat(match).isEqualTo(true);
     }
 }
