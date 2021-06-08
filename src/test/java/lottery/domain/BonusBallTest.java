a4efbaa5903d0f78cad035a8bcc27341a1c1efb0
sanggu <tkdrn8578@naver.com>
2021-06-08 23:40:21 +0900

    refactor: paramterizedTest로 변겨

--- src/test/java/lottery/domain/BonusBallTest.java
+++ src/test/java/lottery/domain/BonusBallTest.java
@@ -1,7 +1,8 @@
 package lottery.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -9,16 +10,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BonusBallTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"5, true", "45, true", "10, false", "30, false"})
     @DisplayName("로또 번호들에 보너스볼이 일치하는게 있는지 확인한다.")
-    void match_test() {
+    void match_test(int bonusNumber, boolean expectedMatch) {
         //given
         LotteryNumbers lotteryNumbers = new LotteryNumbers(List.of(1, 2, 3, 4, 5, 45));
-        BonusBall bonusBall = new BonusBall(5);
+        BonusBall bonusBall = new BonusBall(bonusNumber);
 
         //when
         boolean match = bonusBall.match(lotteryNumbers);
 
-        assertThat(match).isEqualTo(true);
+        assertThat(match).isEqualTo(expectedMatch);
     }
 }
