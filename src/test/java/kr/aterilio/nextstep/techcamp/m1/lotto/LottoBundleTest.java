b6cce4e4d83ddf74d18febdc5e34ab596c098937
심정미 <lina@aterilio.kr>
2021-06-11 04:52:51 +0900

    [step5/로또(수동)] refactor: 단위 테스트 클래스 분리

---
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoBundleTest.java
@@ -0,0 +1,30 @@
+package kr.aterilio.nextstep.techcamp.m1.lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoBundleTest {
+
+    @DisplayName("로또 구입 금액을 입력받으면 로또를 구매한다. (1000원당 1장)")
+    @ParameterizedTest
+    @CsvSource(value = {"3000,3", "14000,14", "8000,8"})
+    public void buyLotto(int money, int count) {
+        LottoBundle lottoBundle = new LottoBundle(money);
+        assertThat(lottoBundle.count()).isEqualTo(count);
+    }
+
+    @DisplayName("로또 구입 금액이 음수이면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-2000, -5000, -100})
+    public void buyLottoFailed(int money) {
+        assertThatThrownBy(() -> {
+            new LottoBundle(money);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("0원 이상");
+    }
+}
--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
@@ -11,24 +11,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
 
-    @DisplayName("로또 구입 금액을 입력받으면 로또를 구매한다. (1000원당 1장)")
-    @ParameterizedTest
-    @CsvSource(value = {"3000,3", "14000,14", "8000,8"})
-    public void buyLotto(int money, int count) {
-        LottoBundle lottoBundle = new LottoBundle(money);
-        assertThat(lottoBundle.count()).isEqualTo(count);
-    }
-
-    @DisplayName("로또 구입 금액이 음수이면 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(ints = {-2000, -5000, -100})
-    public void buyLottoFailed(int money) {
-        assertThatThrownBy(() -> {
-            new LottoBundle(money);
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("0원 이상");
-    }
-
     @DisplayName("로또 생성 시 1-45 범위에 속하지 않는 숫자가 있으면 예외가 발생한다.")
     @Test
     public void createLottoFailed_outOfRange() {
