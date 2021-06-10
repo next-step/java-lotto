72164473abef4d7b5c1cd553f43d7424da3def73
심정미 <lina@aterilio.kr>
2021-06-11 06:05:03 +0900

    [step5/로또(수동)] refactor: 수동으로 입력하는 로또 숫자 유효성 체크를 위한 리팩토링 (중복)

--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
@@ -2,12 +2,9 @@ package kr.aterilio.nextstep.techcamp.m1.lotto;
 
 import kr.aterilio.nextstep.techcamp.m1.utils.LottoParser;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
@@ -23,10 +20,11 @@ public class LottoTest {
     }
 
     @DisplayName("로또 생성 시 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    public void createLottoFailed_duplicated() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1, 2, 3, 4, 5, 1"})
+    public void createLottoFailed_duplicated(String lottoNumbers) {
         assertThatThrownBy(() -> {
-            new Lotto(new Integer[]{1, 2, 3, 4, 5, 1});
+            new Lotto(LottoParser.parse(lottoNumbers));
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복");
     }
