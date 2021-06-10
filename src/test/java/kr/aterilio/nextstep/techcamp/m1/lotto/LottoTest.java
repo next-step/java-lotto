dc4e31ab9ddf2573d7a41fba2aa66668409a7931
심정미 <lina@aterilio.kr>
2021-06-11 06:09:04 +0900

    [step5/로또(수동)] feat: 수동으로 입력하는 로또 숫자 유효성 체크를 위한 단위 테스트 추가 (6개)

--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
@@ -38,4 +38,14 @@ public class LottoTest {
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자");
     }
+
+    @DisplayName("입력받는 숫자의 갯수가 6개가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"4,3,2,1", "5, 2, 3", "1,2,3,4,5,6,7"})
+    public void createLottoFailed_elementCount(String lottoNumbers) {
+        assertThatThrownBy(() -> {
+            new Lotto(LottoParser.parse(lottoNumbers));
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("6개");
+    }
 }
