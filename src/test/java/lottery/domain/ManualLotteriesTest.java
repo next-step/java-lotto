cdd30562b67f030dc1fa97fc3775e622cf680e06
sanggu <tkdrn8578@naver.com>
2021-06-12 15:45:17 +0900

    refactor: 수동 로또 번호 생성 시 필요한 데이터를 공통으로 사용하기 위해 추추

--- src/test/java/lottery/domain/ManualLotteriesTest.java
+++ src/test/java/lottery/domain/ManualLotteriesTest.java
@@ -9,17 +9,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ManualLotteriesTest {
 
+    public static final List<LotteryNumbers> TEST_LOTTERY_NUMBERS = List.of(
+            new LotteryNumbers(List.of(10, 15, 24, 39, 40, 45)),
+            new LotteryNumbers(List.of(1, 2, 3, 4, 5, 6))
+    );
+
     @Test
     @DisplayName("수동 로또를 입력한 숫자로 생성한다.")
     void generateManualLotteries_test() {
-        //given
-        List<LotteryNumbers> lotteryNumbers = List.of(
-                new LotteryNumbers(List.of(10, 15, 24, 39, 40, 45)),
-                new LotteryNumbers(List.of(1, 2, 3, 4, 5, 6))
-        );
-
         //when
-        ManualLotteries manualLotteries = new ManualLotteries(lotteryNumbers);
+        ManualLotteries manualLotteries = new ManualLotteries(TEST_LOTTERY_NUMBERS);
 
         //then
         assertThat(manualLotteries.size()).isEqualTo(2);
