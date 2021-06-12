32edb23c332df1727526de56b6f4246a8ae70a92
sanggu <tkdrn8578@naver.com>
2021-06-12 14:35:43 +0900

    refactor: manualLotteries의 validate에 대한 메서드를 호출하는 역할만 하도록 수정 및 상수 추출

--- src/main/java/lottery/domain/ManualLotteries.java
+++ src/main/java/lottery/domain/ManualLotteries.java
@@ -2,13 +2,22 @@ package lottery.domain;
 
 public class ManualLotteries {
 
-    public static void validateGenerateCount(int countOfTotalLotteries, int countOfManualLotteries) {
-        if (countOfManualLotteries < 0) {
-            throw new IllegalArgumentException("잘못된 수동 로또개수를 입력하였습니다 : " + countOfManualLotteries);
-        }
+    public static final int MINIMUM_COUNT = 0;
 
+    public static void validateGenerateCount(int countOfTotalLotteries, int countOfManualLotteries) {
+        validateMinimumCount(countOfManualLotteries);
+        validateMaximumCount(countOfTotalLotteries, countOfManualLotteries);
+    }
+
+    private static void validateMaximumCount(int countOfTotalLotteries, int countOfManualLotteries) {
         if (countOfTotalLotteries < countOfManualLotteries) {
             throw new IllegalArgumentException("수동 로또 개수가 모든 로또의 생성 개수보다 클 수 없습니다.");
+        }
+    }
+
+    private static void validateMinimumCount(int countOfManualLotteries) {
+        if (countOfManualLotteries < MINIMUM_COUNT) {
+            throw new IllegalArgumentException("잘못된 수동 로또 개수를 입력하였습니다 : " + countOfManualLotteries);
         }
     }
 }
