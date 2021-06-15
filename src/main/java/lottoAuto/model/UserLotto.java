0f876689de7573f618f45efc61dc0bfc768b4523
hailey <wjdguswls55@gmail.com>
2021-06-15 18:42:30 +0900

    refactor : 유효성 검사 코드 추가

--- src/main/java/lottoAuto/model/UserLotto.java
+++ src/main/java/lottoAuto/model/UserLotto.java
@@ -7,6 +7,7 @@ public class UserLotto {
     private List<Integer> lottoNumbers;
 
     public UserLotto(List<Integer> lottoNumbers) {
+        
         this.lottoNumbers = lottoNumbers;
     }
 
--- src/main/java/lottoAuto/model/WinningLotto.java
+++ src/main/java/lottoAuto/model/WinningLotto.java
@@ -4,12 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class WinningLotto {
+    private static final String CHECK_WINNING_LOTTO_COUNT_MESSAGE = "로또 번호는 6개여야 합니다.";
+    private static final int WINNING_NUMBERS_COUNT = 6;
+
     private List<Integer> winningLottoNumbers;
     private int bonusBall;
 
-    public WinningLotto(List<Integer> winningLottoNumbers,int bonusBall){
+    public WinningLotto(List<Integer> winningLottoNumbers, int bonusBall) {
+        checkWinningLottoNumbersCount(winningLottoNumbers.size());
+
         this.winningLottoNumbers = winningLottoNumbers;
         this.bonusBall = bonusBall;
+    }
+
+    public void checkWinningLottoNumbersCount(int winningLottoNumbersCount) {
+        if (winningLottoNumbersCount < WINNING_NUMBERS_COUNT) {
+            throw new IllegalArgumentException(CHECK_WINNING_LOTTO_COUNT_MESSAGE);
+        }
     }
 
     public List<Integer> getWinningLottoNumbers() {
