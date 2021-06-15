09128a4a2ecb7e3c778a5d11d1a7165039571544
h1un <h1unha@naver.com>
2021-06-16 00:02:12 +0900

    refactor : 숫자 가독성높이기

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(2000000000, (matchCount, isBonusMatch) -> matchCount == 6, "6개 일치"),
-    SECOND(30000000, (matchCount, isBonusMatch) -> matchCount == 5 && isBonusMatch, "5개 일치, 보너스 볼 일치"),
-    THIRD(1500000, (matchCount, isBonusMatch) -> matchCount == 5, "5개 일치"),
-    FOURTH(50000, (matchCount, isBonusMatch) -> matchCount == 4, "4개 일치"),
-    FIFTH(5000, (matchCount, isBonusMatch) -> matchCount == 3, "3개 일치"),
+    FIRST(2_000_000_000, (matchCount, isBonusMatch) -> matchCount == 6, "6개 일치"),
+    SECOND(30_000_000, (matchCount, isBonusMatch) -> matchCount == 5 && isBonusMatch, "5개 일치, 보너스 볼 일치"),
+    THIRD(1_500_000, (matchCount, isBonusMatch) -> matchCount == 5, "5개 일치"),
+    FOURTH(50_000, (matchCount, isBonusMatch) -> matchCount == 4, "4개 일치"),
+    FIFTH(5_000, (matchCount, isBonusMatch) -> matchCount == 3, "3개 일치"),
     MISS(0, (matchCount, isBonusMatch) -> false, "꽝");
 
     private int money;
