6f2f0537af185a53e26d69268558050cea06fc2d
hailey <wjdguswls55@gmail.com>
2021-06-15 18:25:53 +0900

    refactor : test code changed

--- src/test/java/lottoAuto/model/UserLottosTest.java
+++ src/test/java/lottoAuto/model/UserLottosTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UserLottosTest {
 
-    private List<Lotto> lottos = new ArrayList<>();
+    private List<UserLotto> userLottos = new ArrayList<>();
     private List<Integer> lotto = new ArrayList<>();
     private int bonus;
 
@@ -23,48 +23,10 @@ class UserLottosTest {
         lotto = Arrays.asList(1,2,3,4,5,12);
         bonus = 6;
 
-        this.lottos.add(new Lotto(userLotto));
-        this.lottos.add(new Lotto(userLotto2));
+        this.userLottos.add(new UserLotto(userLotto));
+        this.userLottos.add(new UserLotto(userLotto2));
     }
 
-    @Test
-    public void getStatistic(){
-        UserLottos userLottos = new UserLottos(this.lottos);
-        Lotto winningLotto = new Lotto(this.lotto);
-
-        LottoResults lottoResults = getStatistic(userLottos.getUserLottoNumbers(),winningLotto);
-        assertThat(lottoResults.getLottoResult().get(0).getCount()).isEqualTo(5);
-        assertThat(lottoResults.getLottoResult().get(0).getBonus()).isEqualTo(true);
-        assertThat(lottoResults.getLottoResult().get(1).getCount()).isEqualTo(1);
-        assertThat(lottoResults.getLottoResult().get(1).getBonus()).isEqualTo(false);
-    }
-
-    public LottoResults getStatistic(List<Lotto> userLottos, Lotto winningLotto) {
-
-        LottoResults lottoResults = new LottoResults();
-
-        for (Lotto userLotto : userLottos) {
-
-            LottoResult lottoResult = new LottoResult(getCount(userLotto.getLottoNumbers(), winningLotto.getLottoNumbers()),hasBonus(userLotto.getLottoNumbers(),bonus));
-            lottoResults.add(lottoResult);
-        }
-        return lottoResults;
-    }
-
-    private int getCount(List<Integer> userLottoNumbers, List<Integer> winningLottoNumbers) {
-        int count = 0;
-        for (int j = 0; j < winningLottoNumbers.size(); j++) {
-            if (userLottoNumbers.contains(winningLottoNumbers.get(j))) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private Boolean hasBonus(List<Integer> userLottoNumbers, int bonus){
-
-        return userLottoNumbers.contains(bonus);
-    }
     @Test
     public void match() {
         LottoResultsTest lottoResultsTest = new LottoResultsTest();
