c62cd4a258964d1ad7e43c21a1e51ecfe03433a2
hailey <wjdguswls55@gmail.com>
2021-06-15 18:36:19 +0900

    feat : add checkLottoPrice

--- src/main/java/lottoAuto/model/LottoAuto.java
+++ src/main/java/lottoAuto/model/LottoAuto.java
@@ -5,12 +5,19 @@ import java.util.List;
 
 public class LottoAuto {
     private static final int LOTTO_PRICE = 1000;
+    private static final String CHECK_LOTTO_PRICE_MESSAGE = "로또 1장의 가격은 1000원입니다.";
 
     private LottoRandom lottoRandom = new LottoRandom();
 
     public int calculateAmount(int price){
-
+        checkLottoPrice(price);
         return price / LOTTO_PRICE;
+    }
+
+    private void checkLottoPrice(int price){
+        if(price < LOTTO_PRICE){
+            throw new IllegalArgumentException(CHECK_LOTTO_PRICE_MESSAGE);
+        }
     }
 
     public UserLottos inputUserLottoNumber(int amount) {
