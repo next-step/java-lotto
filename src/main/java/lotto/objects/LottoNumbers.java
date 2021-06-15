025792f354eb5cdd5dcdbbf9faf9f4294f152c41
정진우 <jwjung@upchain.kr>
2021-06-15 22:02:43 +0900

    refactor(lotto): LottoNumbers 상수 추가

--- src/main/java/lotto/objects/LottoNumbers.java
+++ src/main/java/lotto/objects/LottoNumbers.java
@@ -5,12 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumbers {
+    private static final int LOTTO_MIN_NUMBER = 0;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
     private List<Integer> allNumbers;
 
     public LottoNumbers() {
         List<Integer> initNumbers = new ArrayList<>();
 
-        for (int i = 0; i < 45; i++) {
+        for (int i = LOTTO_MIN_NUMBER; i < LOTTO_MAX_NUMBER; i++) {
             initNumbers.add(i + 1);
         }
 
