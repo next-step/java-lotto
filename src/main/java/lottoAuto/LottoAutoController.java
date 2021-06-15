36d8c44b38f8ce2be80a554c8c63fb442e7613d0
hailey <wjdguswls55@gmail.com>
2021-06-15 18:24:33 +0900

    refactor : 불필요한 객체 삭제

--- src/main/java/lottoAuto/LottoAutoController.java
+++ src/main/java/lottoAuto/LottoAutoController.java
@@ -4,27 +4,31 @@ import lottoAuto.model.*;
 import lottoAuto.view.InputView;
 import lottoAuto.view.ResultView;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
-public class LottoAutoApplication {
+public class LottoAutoController {
 
     public static void main(String[] args) {
-        int price = InputView.inputPrice();
-        int amount = ResultView.showAmount(price);
-
         LottoAuto lottoAuto = new LottoAuto();
+
+        int price = InputView.inputPrice();
+        int amount = lottoAuto.calculateAmount(price);
+        ResultView.showAmount(amount);
+
         UserLottos userLottos = lottoAuto.inputUserLottoNumber(amount);
         ResultView.showUserLotto(userLottos);
 
         String winningLottoString = InputView.inputWinningNumber();
-        Lotto winningLotto = lottoAuto.inputWinningNumber(winningLottoString);
         int bonusBall = InputView.inputBonusBall();
 
-        LottoStatistic lottoStatistic = new LottoStatistic(userLottos,winningLotto,bonusBall);
-        Map<Rank,Long> resultMap = lottoStatistic.getStatistic();
+        WinningLotto winningLotto = lottoAuto.inputWinningNumber(winningLottoString,bonusBall);
+
+        LottoResults lottoResults = new LottoResults(userLottos,winningLotto,bonusBall);
+        Map<Rank,Long> resultMap = lottoResults.lottoRankCounting();
 
         ResultView.showWinningStatistic(resultMap);
-        double revenue = lottoStatistic.getRevenue(price,resultMap);
+        BigDecimal revenue = lottoResults.getRevenue(price,resultMap);
         ResultView.showRevenue(revenue);
     }
 }
