78d4de4afc3c3b1bc7fdc28244ad26ff5be60e1a
hailey <wjdguswls55@gmail.com>
2021-06-15 18:25:26 +0900

    refactor : moved method

--- src/main/java/lottoAuto/model/LottoResults.java
+++ src/main/java/lottoAuto/model/LottoResults.java
@@ -1,21 +1,64 @@
 package lottoAuto.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LottoResults {
-    private List<LottoResult> lottoResult;
+    private List<LottoResult> lottoResults;
+    private UserLottos userLottos;
+    private WinningLotto winningUserLotto;
+    private int bonusBall;
 
-    public LottoResults() {
-        this.lottoResult = new ArrayList<>();
+    public LottoResults(UserLottos userLottos, WinningLotto winningUserLotto, int bonusBall) {
+        this.lottoResults = new ArrayList<>();
+        this.userLottos = userLottos;
+        this.winningUserLotto = winningUserLotto;
+        this.bonusBall = bonusBall;
+
+        getStatistic();
     }
 
-    public void add(LottoResult lottoResult) {
-        this.lottoResult.add(lottoResult);
+    private void getStatistic() {
+        for (UserLotto userLotto : userLottos.getUserLottoNumbers()) {
+            LottoResult lottoResult = new LottoResult(getCount(userLotto.getLottoNumbers(), winningUserLotto.getWinningLottoNumbers()), hasBonus(userLotto.getLottoNumbers(), bonusBall));
+            lottoResults.add(lottoResult);
+        }
+    }
+
+    private int getCount(List<Integer> userLottoNumbers, List<Integer> winningLottoNumbers) {
+        int count = 0;
+        for (int i = 0; i < winningLottoNumbers.size(); i++) {
+            if (userLottoNumbers.contains(winningLottoNumbers.get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private Boolean hasBonus(List<Integer> userLottoNumbers, int bonus) {
+
+        return userLottoNumbers.contains(bonus);
+    }
+
+    public Map<Rank, Long> lottoRankCounting() {
+        return lottoResults.stream()
+                .collect(Collectors.groupingBy(x -> Rank.findByRank(x), Collectors.counting()));
+    }
+
+    public BigDecimal getRevenue(int price, Map<Rank, Long> resultMap) {
+        int sum = resultMap.entrySet().stream()
+                .mapToInt(rankLongEntry ->Rank.calculatePrize(rankLongEntry.getKey(),rankLongEntry.getValue()))
+                .sum()
+                ;
+
+        return new BigDecimal(String.valueOf(sum)).divide(new BigDecimal(String.valueOf(price)));
     }
 
     public List<LottoResult> getLottoResult() {
-        return lottoResult;
+        return lottoResults;
     }
 
 }
