62328c24e952c20c48f6178bd28b5227c8a6ee6a
hailey <wjdguswls55@gmail.com>
2021-06-15 18:23:53 +0900

    refactor : 계산하는 로직 추가

--- src/main/java/lottoAuto/model/LottoAuto.java
+++ src/main/java/lottoAuto/model/LottoAuto.java
@@ -4,22 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoAuto {
+    private static final int LOTTO_PRICE = 1000;
+
     private LottoRandom lottoRandom = new LottoRandom();
-    private List<Integer> winningNumber = new ArrayList<>();
+
+    public int calculateAmount(int price){
+
+        return price / LOTTO_PRICE;
+    }
 
     public UserLottos inputUserLottoNumber(int amount) {
         UserLottos userLottos = lottoRandom.setLotto(amount);
+
         return userLottos;
     }
 
-    public Lotto inputWinningNumber(String winningNumberString) {
-        String[] splitString = winningNumberString.split(",");
+    public WinningLotto inputWinningNumber(String winningLottoString,int bonusBall) {
+        WinningLotto winningLotto = new WinningLotto(splitWinningNumber(winningLottoString),bonusBall);
 
-        for (int i = 0; i < splitString.length; i++) {
-            winningNumber.add(Integer.valueOf(splitString[i]));
-        }
-        Lotto winningLotto = new Lotto(winningNumber);
         return winningLotto;
+    }
 
+    private List<Integer> splitWinningNumber(String winningLottoString){
+        List<Integer> winningNumbers = new ArrayList<>();
+
+        String[] splitedWinningNumbers = winningLottoString.split(",");
+        for (int i = 0; i < splitedWinningNumbers.length; i++) {
+            winningNumbers.add(Integer.valueOf(splitedWinningNumbers[i]));
+        }
+        return winningNumbers;
     }
 }
