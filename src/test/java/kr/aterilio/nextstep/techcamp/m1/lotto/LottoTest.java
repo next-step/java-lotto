4430bf6f04aef7b452228beee344d0aecd3e0553
심정미 <lina@aterilio.kr>
2021-06-17 02:59:12 +0900

    [step5/로또(수동)] refactor: LottoBall 테스트 분리

--- src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
+++ src/test/java/kr/aterilio/nextstep/techcamp/m1/lotto/LottoTest.java
@@ -9,16 +9,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
 
-    @DisplayName("로또 생성 시 1-45 범위에 속하지 않는 숫자가 있으면 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"-1, 2, 3, 4, 5, 46"})
-    public void createLottoFailed_outOfRange(String lottoNumbers) {
-        assertThatThrownBy(() -> {
-            new Lotto(LottoParser.parse(lottoNumbers));
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("범위");
-    }
-
     @DisplayName("로또 생성 시 중복된 숫자가 있으면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1, 2, 3, 4, 5, 1"})
@@ -27,16 +17,6 @@ public class LottoTest {
             new Lotto(LottoParser.parse(lottoNumbers));
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복");
-    }
-
-    @DisplayName("로또 생성 시 숫자가 아닌 값이 입력되면 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"4,3,t,1", "5, t, 3"})
-    public void createLottoFailed_notNumeric(String lottoNumbers) {
-        assertThatThrownBy(() -> {
-            new Lotto(LottoParser.parse(lottoNumbers));
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자");
     }
 
     @DisplayName("입력받는 숫자의 갯수가 6개가 아니면 예외가 발생한다.")
