19b16852d5c0e24e7472f9df71fc38b1237603a5
h1un <h1unha@naver.com>
2021-06-20 03:03:04 +0900

    Test ( WinningStatisticsTest.java )

---
+++ src/test/java/lotto/domain/WinningStatisticsTest.java
@@ -0,0 +1,70 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningStatisticsTest {
+    public WinningStatistics winningStatistics;
+    List<Rank> lottoRankList;
+    @BeforeEach
+    void set() {
+        lottoRankList = new ArrayList<>();
+    }
+
+    @DisplayName("구매금액")
+    @Test
+    void purchaseAmount() {
+
+        winningStatistics = new WinningStatistics(lottoRankList,10);
+
+        assertThat(winningStatistics.purchaseAmount()).isEqualTo(10000);
+
+    }
+
+    @DisplayName("당첨 금액")
+    @Test
+    void total_5() {
+        lottoRankList.add(Rank.FIFTH);
+
+        winningStatistics = new WinningStatistics(lottoRankList,1);
+
+        assertThat(winningStatistics.total()).isEqualTo(5000);
+
+    }
+
+    @DisplayName("당첨 금액")
+    @Test
+    void total() {
+        lottoRankList.add(Rank.FIFTH);
+        lottoRankList.add(Rank.FIRST);
+
+        winningStatistics = new WinningStatistics(lottoRankList,10);
+
+        assertThat(winningStatistics.total()).isEqualTo(2000005000);
+
+    }
+
+
+    @DisplayName("수익률")
+    @Test
+    void yield() {
+        lottoRankList.add(Rank.FIFTH);
+        lottoRankList.add(Rank.FIRST);
+
+        winningStatistics = new WinningStatistics(lottoRankList,10);
+
+        assertThat(winningStatistics.yield()).isEqualTo("200000.50");
+
+    }
+
+
+}
