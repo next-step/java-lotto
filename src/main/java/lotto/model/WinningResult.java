b77cd056520f4b4062ac7c820fa71e02e6d81d0b
이예림 <dulfla_5@naver.com>
2021-06-28 15:06:43 +0900

    refactor : WinningResult 각 등수별로 당첨번호 갯수(countOfWinningNumber)와 당첨금액(winningAmount)의 정보를 가지는 enum class refactor : WinningResult#addNumberOfWinning 당첨번호 갯수와 보너스번호 존재여부를 입력받으면 당첨결과를 반환 feat : WinningResult#multiplyWinningAmount 당첨횟수를 입력받으면 그만큼의 당첨금액을 반환

--- src/main/java/lotto/model/WinningResult.java
+++ src/main/java/lotto/model/WinningResult.java
@@ -1,69 +1,42 @@
 package lotto.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public enum WinningResult {
-    FIFTH_PRICE(5_000),
-    FOURTH_PRICE(50_000),
-    THIRD_PRICE(1_500_000),
-    SECOND_PRICE(30_000_000),
-    FIRST_PRICE(2_000_000_000);
+    NO_PRICE(0, 0),
+    FIFTH_PRICE(3, 5_000),
+    FOURTH_PRICE(4, 50_000),
+    THIRD_PRICE(5, 1_500_000),
+    SECOND_PRICE(5, 30_000_000),
+    FIRST_PRICE(6, 2_000_000_000);
 
+    private int countOfWinningNumber;
     private int winningAmount;
-    private List<Integer> numberOfWins;
 
-    WinningResult(int winningAmount) {
+    WinningResult(int countOfWinningNumber, int winningAmount) {
+        this.countOfWinningNumber = countOfWinningNumber;
         this.winningAmount = winningAmount;
-        numberOfWins = new ArrayList<>();
     }
 
-    public void addNumberOfWinning(int numberOfWinningNumbers, boolean existenceOfBonusNumber) {
-        int numberOfFifthPlace = 0;
-        int numberOfFourthPlace = 0;
-        int numberOfThirdPlace = 0;
-        int numberOfSecondPlace = 0;
-        int numberOfFirstPlace = 0;
-
-        if (numberOfWinningNumbers == 3) {
-            numberOfFifthPlace++;
+    public static WinningResult addNumberOfWinning(int numberOfWinningNumbers, boolean existenceOfBonusNumber) {
+        if (numberOfWinningNumbers == SECOND_PRICE.countOfWinningNumber && existenceOfBonusNumber) {
+            return SECOND_PRICE;
         }
-
-        if (numberOfWinningNumbers == 4) {
-            numberOfFourthPlace++;
+        for (WinningResult value : values()) {
+            if (numberOfWinningNumbers == value.countOfWinningNumber) {
+                return value;
+            }
         }
-
-        if (numberOfWinningNumbers == 5 && !existenceOfBonusNumber) {
-            numberOfThirdPlace++;
-        }
-
-        if (numberOfWinningNumbers == 5 && existenceOfBonusNumber) {
-            numberOfSecondPlace++;
-        }
-
-        if (numberOfWinningNumbers == 6) {
-            numberOfFirstPlace++;
-        }
-        numberOfWins.add(numberOfFifthPlace);
-        numberOfWins.add(numberOfFourthPlace);
-        numberOfWins.add(numberOfThirdPlace);
-        numberOfWins.add(numberOfSecondPlace);
-        numberOfWins.add(numberOfFirstPlace);
+        return NO_PRICE;
     }
 
-    public int calculateEarningPrice() {
-        return FIFTH_PRICE.getWinningAmount() * numberOfWins.get(0)
-                + FOURTH_PRICE.getWinningAmount() * numberOfWins.get(1)
-                + THIRD_PRICE.getWinningAmount() * numberOfWins.get(2)
-                + SECOND_PRICE.getWinningAmount() * numberOfWins.get(3)
-                + FIRST_PRICE.getWinningAmount() * numberOfWins.get(4);
+    public double multiplyWinningAmount(int winningResultNumber) {
+        return winningAmount * winningResultNumber;
+    }
+
+    public int getCountOfWinningNumber() {
+        return countOfWinningNumber;
     }
 
     public int getWinningAmount() {
         return winningAmount;
-    }
-
-    public List<Integer> getNumberOfWins() {
-        return numberOfWins;
     }
 }
