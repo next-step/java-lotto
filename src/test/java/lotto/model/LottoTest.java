35012a32ca0d1f2e390f2daf1b6fbe3ad34e0fde
이예림 <dulfla_5@naver.com>
2021-06-29 00:33:56 +0900

    refactor : LottoTest#setUp 테스트전 lotto 와 winningLotto 세팅하기 test : LottoTest#contains_lotto_number 로또번호 포함여부 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,7 +1,10 @@
 package lotto.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,28 +13,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class LottoTest {
+    Lotto lotto;
+    Lotto winningLotto;
+
+    @BeforeEach
+    void setUp() {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            lottoNumbers.add(LottoNumber.of(i));
+        }
+        lotto = new Lotto(lottoNumbers);
+
+        List<LottoNumber> winningNumbers = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            winningNumbers.add(LottoNumber.of(i * 2));
+        }
+        winningLotto = new Lotto(winningNumbers);
+    }
+
     @DisplayName(value = "당첨로또번호 갯수 구하기")
     @Test
     void count_winning_numbers() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        lottoNumbers.add(LottoNumber.of(1));
-        lottoNumbers.add(LottoNumber.of(2));
-        lottoNumbers.add(LottoNumber.of(3));
-        lottoNumbers.add(LottoNumber.of(4));
-        lottoNumbers.add(LottoNumber.of(5));
-        lottoNumbers.add(LottoNumber.of(6));
-
-        List<LottoNumber> winningNumbers = new ArrayList<>();
-        winningNumbers.add(LottoNumber.of(2));
-        winningNumbers.add(LottoNumber.of(4));
-        winningNumbers.add(LottoNumber.of(6));
-        winningNumbers.add(LottoNumber.of(8));
-        winningNumbers.add(LottoNumber.of(10));
-        winningNumbers.add(LottoNumber.of(12));
-
-        Lotto lotto = new Lotto(lottoNumbers);
-        Lotto winningLotto = new Lotto(winningNumbers);
-
         assertThat(lotto.countWinningNumbers(winningLotto)).isEqualTo(3);
+    }
+
+    @DisplayName(value = "로또번호 포함여부 구하기")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void contains_lotto_number(final int number) {
+        assertThat(lotto.contains(LottoNumber.of(number))).isTrue();
     }
 }
