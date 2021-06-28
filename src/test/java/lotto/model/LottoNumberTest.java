6f65eb5a7a9400e3051031d78ba75367801802b0
이예림 <dulfla_5@naver.com>
2021-06-29 00:41:33 +0900

    refactor : LottoNumberTest#change_to_string 로또 넘버 문자열로 바꾸기 테스트 추가

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -3,10 +3,18 @@ package lotto.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberTest {
+    @DisplayName(value = "로또넘버 문자열로 바꾸기")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void change_to_string(final int number) {
+        assertThat(LottoNumber.of(number).toString()).isEqualTo(Integer.toString(number));
+    }
 
     @DisplayName(value = "두 로또넘버는 같다.")
     @Test
