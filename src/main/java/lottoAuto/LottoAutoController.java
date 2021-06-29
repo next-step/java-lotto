b60323511f74dd18b9937617ee3d0d600a2323ed
xxhyxxnjxn <hyunjin077@naver.com>
2021-06-30 01:01:49 +0900

    refactor : controller 기능별 메서드 분리

--- src/main/java/lottoAuto/LottoAutoController.java
+++ src/main/java/lottoAuto/LottoAutoController.java
@@ -11,20 +11,27 @@ public class LottoAutoController {
         int amount = LottoCalculator.calculateAmount(price);
         ResultView.showAmount(amount);
 
-        LottoGenerator lottoGenerator = new LottoGenerator(amount);
-        Lottos lottos = new Lottos(lottoGenerator.getLottos());
+        Lottos lottos = makeLottos(amount);
         ResultView.showUserLotto(lottos);
 
-        String winningLottoNumber = InputView.inputWinningNumber();
-        int bonusBall = InputView.inputBonusBall();
-
-        WinningLotto winningLotto = new WinningLotto(StringUtil.makeLotto(winningLottoNumber), bonusBall);
-
+        WinningLotto winningLotto = makeWinningLotto();
         LottoResults lottoResults = lottos.matchWinningLotto(winningLotto);
 
         Revenue revenue = new Revenue(lottoResults.lottoRankCounting(),price);
         ResultView.showWinningStatistic(revenue);
-
         ResultView.showRevenue(revenue.calculateRevenue());
+    }
+
+    private static Lottos makeLottos(int amount){
+        LottoGenerator lottoGenerator = new LottoGenerator(amount);
+
+        return new Lottos(lottoGenerator.getLottos());
+    }
+
+    private static WinningLotto makeWinningLotto(){
+        String winningLottoNumber = InputView.inputWinningNumber();
+        int bonusBall = InputView.inputBonusBall();
+
+        return new WinningLotto(StringUtil.makeLotto(winningLottoNumber), bonusBall);
     }
 }
