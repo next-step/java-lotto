60c7422f621a86fb69dd9fd7c2aff589526a59ae
xxhyxxnjxn <hyunjin077@naver.com>
2021-06-30 02:55:26 +0900

    test : Lotto객체에 대한 테스트 코드 작성

--- src/test/java/lottoAuto/model/LottoTest.java
+++ src/test/java/lottoAuto/model/LottoTest.java
@@ -1,5 +1,6 @@
 package lottoAuto.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoTest {
+    private WinningLotto winningLottoTest;
+
+    @BeforeEach
+    public void set(){
+        Set<LottoNumber> winningNumbers = new HashSet<>();
+        winningNumbers.add(new LottoNumber(1));
+        winningNumbers.add(new LottoNumber(2));
+        winningNumbers.add(new LottoNumber(3));
+        winningNumbers.add(new LottoNumber(4));
+        winningNumbers.add(new LottoNumber(5));
+        winningNumbers.add(new LottoNumber(6));
+        Lotto winningLotto = new Lotto(winningNumbers);
+        winningLottoTest = new WinningLotto(winningLotto,7);
+    }
 
     @DisplayName("로또 번호는 6개이하면 예외를 발생시킨다.")
     @Test
@@ -24,17 +39,32 @@ public class LottoTest {
         assertThrows(IllegalArgumentException.class, () -> new Lotto(lottoNumbers));
     }
 
+    @DisplayName("로또 번호는 6개이상이면 예외를 발생시킨다.")
     @Test
-    public void hasBonusBall(){
-        int bonusBall = 3;
+    public void lottoSizeOverTest() {
         Set<LottoNumber> lottoNumbers = new HashSet<>();
         lottoNumbers.add(new LottoNumber(1));
         lottoNumbers.add(new LottoNumber(2));
         lottoNumbers.add(new LottoNumber(3));
+        lottoNumbers.add(new LottoNumber(4));
+        lottoNumbers.add(new LottoNumber(5));
+        lottoNumbers.add(new LottoNumber(6));
+        lottoNumbers.add(new LottoNumber(7));
 
-        boolean result = lottoNumbers.stream()
-                .anyMatch(lottoNumber -> lottoNumber.hasNumber(bonusBall));
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(lottoNumbers));
+    }
 
-        assertThat(result).isEqualTo(true);
+    @DisplayName("Lotto객체는 LottoResult객체를 반환할 수 있다.")
+    @Test
+    public void makeLottoResult(){
+        Set<LottoNumber> lottoNumbers = new HashSet<>();
+        lottoNumbers.add(new LottoNumber(1));
+        lottoNumbers.add(new LottoNumber(2));
+        lottoNumbers.add(new LottoNumber(3));
+        lottoNumbers.add(new LottoNumber(4));
+        lottoNumbers.add(new LottoNumber(5));
+        lottoNumbers.add(new LottoNumber(6));
+        Lotto lotto = new Lotto(lottoNumbers);
+        assertThat(lotto.makeLottoResult(winningLottoTest)).isInstanceOf(LottoResult.class);
     }
 }
