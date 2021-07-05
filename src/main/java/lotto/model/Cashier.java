c6ccdbb7165074649472f087ad654a6f7c4be055
이예림 <dulfla_5@naver.com>
2021-07-05 16:47:41 +0900

    feat : Cashier 총 로또, 자동로또, 수동로또 갯수 변수 및 getter 추가

--- src/main/java/lotto/model/Cashier.java
+++ src/main/java/lotto/model/Cashier.java
@@ -3,20 +3,33 @@ package lotto.model;
 public class Cashier {
     private static final int LOTTO_PRICE = 1_000;
     private int price;
+    private int lottoQuantity;
+    private int autoQuantity;
+    private int manualQuantity;
 
     public Cashier(int price) {
         this.price = price;
+        lottoQuantity = price / LOTTO_PRICE;
     }
 
-    public int calculateLottoQuantity() {
-        return price / LOTTO_PRICE;
-    }
-
-    public int calculateAutoLottoQuantity(int manualQuantity) {
-        return calculateLottoQuantity() - manualQuantity;
+    public void calculateAutoLottoQuantity(int manualQuantity) {
+        autoQuantity = lottoQuantity - manualQuantity;
+        this.manualQuantity = manualQuantity;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    public int getLottoQuantity() {
+        return lottoQuantity;
+    }
+
+    public int getAutoQuantity() {
+        return autoQuantity;
+    }
+
+    public int getManualQuantity() {
+        return manualQuantity;
     }
 }
