266cb69700ebf6fb810f106b5c6cbbbe00418225
이예림 <dulfla_5@naver.com>
2021-07-05 16:10:10 +0900

    refactor : Lotto#checkLottoSize 메서드명 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,12 +7,12 @@ public class Lotto {
     private List<LottoNumber> lottoNumbers;
 
     public Lotto(List<LottoNumber> lottoNumbers) {
-        checkLottoNumbers(lottoNumbers);
+        checkLottoSize(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
-    private void checkLottoNumbers(List<LottoNumber> lottoNumbers) {
-        if(lottoNumbers.size() != LOTTO_SIZE) {
+    private void checkLottoSize(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또 숫자 갯수는 6개여야 합니다.");
         }
     }
