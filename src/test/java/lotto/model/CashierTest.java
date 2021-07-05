7026b3060c6f1670e486559012876ac32eeb8ee2
이예림 <dulfla_5@naver.com>
2021-07-05 16:51:02 +0900

    refactor : CashierTest 총 로또, 자동로또, 수동로또 갯수 테스트

--- src/test/java/lotto/model/CashierTest.java
+++ src/test/java/lotto/model/CashierTest.java
@@ -17,12 +17,14 @@ public class CashierTest {
     @DisplayName(value = "로또 수량 계산하기")
     @Test
     void calculate_lotto_quantity() {
-        assertThat(cashier.calculateLottoQuantity()).isEqualTo(5);
+        assertThat(cashier.getLottoQuantity()).isEqualTo(5);
     }
 
-    @DisplayName(value = "자동 로또 수량 계산하기")
+    @DisplayName(value = "자동, 수동 로또 수량 계산하기")
     @Test
     void calculate_auto_lotto_quantity() {
-        assertThat(cashier.calculateAutoLottoQuantity(3)).isEqualTo(2);
+        cashier.calculateAutoLottoQuantity(3);
+        assertThat(cashier.getAutoQuantity()).isEqualTo(2);
+        assertThat(cashier.getManualQuantity()).isEqualTo(3);
     }
 }
