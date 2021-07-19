e08690b574b5456773f1f184a6116a5bbf80c874
hyeonjin <hyunjin077@naver.com>
2021-07-19 08:29:44 -0700

    test : 테스트코드 삭제

--- src/test/java/lottoAuto/model/WinningLottoTest.java
+++ src/test/java/lottoAuto/model/WinningLottoTest.java
@@ -22,7 +22,7 @@ public class WinningLottoTest {
         winningNumbers.add(new LottoNumber(5));
         winningNumbers.add(new LottoNumber(6));
         Lotto winningLotto = new Lotto(winningNumbers);
-        winningLottoTest = new WinningLotto(winningLotto, 7);
+        winningLottoTest = new WinningLotto(winningLotto, new LottoNumber(7));
     }
 
     @DisplayName("로또 당첨 번호와 몇개나 일치하는지 구할 수 있다.")
@@ -38,20 +38,5 @@ public class WinningLottoTest {
 
         int count = winningLottoTest.countWinningLottoNumbers(lottoNumbers);
         assertThat(count).isEqualTo(5);
-    }
-
-    @DisplayName("보너스 볼을 포함하고 있는지 알 수 있다.")
-    @Test
-    public void hashBonusBallTest() {
-        Set<LottoNumber> lottoNumbers = new HashSet<>();
-        lottoNumbers.add(new LottoNumber(1));
-        lottoNumbers.add(new LottoNumber(2));
-        lottoNumbers.add(new LottoNumber(3));
-        lottoNumbers.add(new LottoNumber(4));
-        lottoNumbers.add(new LottoNumber(5));
-        lottoNumbers.add(new LottoNumber(7));
-
-        boolean result = winningLottoTest.hasBonusBall(lottoNumbers);
-        assertThat(result).isEqualTo(true);
     }
 }
