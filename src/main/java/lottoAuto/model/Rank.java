f5c69009bffcbf1dcbd2b90aee7602d945119345
hyeonjin <hyunjin077@naver.com>
2021-07-19 08:15:46 -0700

    refactor : 변수 네이밍 변경 numberOfWinnings -> matchCount

--- src/main/java/lottoAuto/model/Rank.java
+++ src/main/java/lottoAuto/model/Rank.java
@@ -11,26 +11,26 @@ public enum Rank {
     FIFTH(3, 5_000),
     NO_MATCH(0, 0);
 
-    private int numberOfWinnings;
+    private int matchCount;
     private int prize;
 
-    Rank(int numberOfWinnings, int prize) {
-        this.numberOfWinnings = numberOfWinnings;
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
         this.prize = prize;
     }
 
     public static Rank findByRank(int countMatchWinningNumbers, boolean hasBonusBall) {
-        if (countMatchWinningNumbers == SECOND.numberOfWinnings && hasBonusBall) {
+        if (countMatchWinningNumbers == SECOND.matchCount && hasBonusBall) {
             return SECOND;
         }
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.numberOfWinnings == countMatchWinningNumbers && rank != SECOND)
+                .filter(rank -> rank.matchCount == countMatchWinningNumbers && rank != SECOND)
                 .findAny()
                 .orElse(NO_MATCH);
     }
 
     public static String makeMessage(Rank rank) {
-        return rank.numberOfWinnings + "개 일치 (" + rank.prize + "원)";
+        return rank.matchCount + "개 일치 (" + rank.prize + "원)";
     }
 
     public static BigDecimal calculatePrize(Rank rank, Long count) {
