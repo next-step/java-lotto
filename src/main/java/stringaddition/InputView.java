e5e09a6bf118fc075343b20487c1554e4f69e27d
hyeonjin <hyunjin077@naver.com>
2021-07-19 08:32:01 -0700

    refactor : 클래스는 상수명 uppercase를 적용시키지 않는다.

--- src/main/java/stringaddition/InputView.java
+++ src/main/java/stringaddition/InputView.java
@@ -3,9 +3,9 @@ package stringaddition;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputText() {
-        return SCANNER.nextLine();
+        return scanner.nextLine();
     }
 }
