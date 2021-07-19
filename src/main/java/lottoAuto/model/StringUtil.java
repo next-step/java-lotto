e8a908eff0cc3412c65a022d30bd33798a94e453
hyeonjin <hyunjin077@naver.com>
2021-07-19 08:17:05 -0700

    refactor : 상수 네이밍 변경 SPLIT_WINNING_LOTTO -> SEPARATOR

--- src/main/java/lottoAuto/model/StringUtil.java
+++ src/main/java/lottoAuto/model/StringUtil.java
@@ -4,12 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class StringUtil {
-    private static final String SPLIT_WINNING_LOTTO = ",";
+    private static final String SEPARATOR = ",";
 
     public static Lotto makeLotto(String winningLottoNumber) {
         Set<LottoNumber> lottoNumbers = new HashSet<>();
 
-        String[] splitedWinningNumbers = winningLottoNumber.split(SPLIT_WINNING_LOTTO);
+        String[] splitedWinningNumbers = winningLottoNumber.split(SEPARATOR);
         for (int i = 0; i < splitedWinningNumbers.length; i++) {
             lottoNumbers.add(new LottoNumber(Integer.valueOf(splitedWinningNumbers[i])));
         }
