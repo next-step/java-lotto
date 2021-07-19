71fccf44a5964f0f51382ac7ee95aec2b40bced9
hyeonjin <hyunjin077@naver.com>
2021-07-19 07:34:20 -0700

    refactor : 불필요한 코드 줄이기

--- src/main/java/lottoAuto/model/Lotto.java
+++ src/main/java/lottoAuto/model/Lotto.java
@@ -5,8 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Lotto {
-    private static final String CHECK_UNDER_LOTTO_COUNT_MESSAGE = "로또 번호의 개수는 6개보다 작으면 안됩니다.";
-    private static final String CHECK_OVER_LOTTO_COUNT_MESSAGE = "로또 번호의 개수는 6개보다 크면 안됩니다.";
+    private static final String CHECK_LOTTO_COUNT_MESSAGE = "로또 번호의 개수는 6개여야 합니다.";
     private static final int LOTTO_NUMBERS_SIZE = 6;
 
     private Set<LottoNumber> lottoNumbers;
@@ -17,11 +16,8 @@ public class Lotto {
     }
 
     public void checkLottoNumbersCount(int lottoNumbersSize) {
-        if (lottoNumbersSize < LOTTO_NUMBERS_SIZE) {
-            throw new IllegalArgumentException(CHECK_UNDER_LOTTO_COUNT_MESSAGE);
-        }
-        if (lottoNumbersSize > LOTTO_NUMBERS_SIZE) {
-            throw new IllegalArgumentException(CHECK_OVER_LOTTO_COUNT_MESSAGE);
+        if (lottoNumbersSize != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(CHECK_LOTTO_COUNT_MESSAGE);
         }
     }
 
