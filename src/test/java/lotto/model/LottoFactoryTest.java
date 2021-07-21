c2f7204de78b977ccb446d5c5b581d9bb1a895ea
이예림 <dulfla_5@naver.com>
2021-07-21 16:24:44 +0900

    refactor : LottoFactoryTest#create_manual_lottos 사용하지 않는 테스트 제거

--- src/test/java/lotto/model/LottoFactoryTest.java
+++ src/test/java/lotto/model/LottoFactoryTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LottoFactoryTest {
     LottoFactory lottoFactory;
+
     @BeforeEach
     void setUp() {
         lottoFactory = new LottoFactory();
@@ -19,29 +19,12 @@ public class LottoFactoryTest {
     void create_auto_lottos() {
         List<Lotto> lottos = lottoFactory.createAutoLottos(3);
         System.out.println("자동 로또 갯수만큼 생성하기");
-        for(Lotto lotto : lottos) {
-            for(LottoNumber lottoNumber : lotto.getNumbers()) {
+        for (Lotto lotto : lottos) {
+            for (LottoNumber lottoNumber : lotto.getNumbers()) {
                 System.out.print(lottoNumber.toString() + " ");
             }
             System.out.println();
         }
         System.out.println();
-    }
-
-    @DisplayName(value = "수동 로또 갯수만큼 생성하기")
-    @Test
-    void create_manual_lottos() {
-        List<String> manualNumbers = new ArrayList<>();
-        manualNumbers.add("1, 2, 3, 4, 5, 6");
-        manualNumbers.add("2, 3, 4, 5, 6, 7");
-        manualNumbers.add("3, 4, 5, 6, 7, 8");
-        List<Lotto> lottos = lottoFactory.createManualLottos(3, manualNumbers);
-        System.out.println("수동 로또 갯수만큼 생성하기");
-        for(Lotto lotto : lottos) {
-            for(LottoNumber lottoNumber : lotto.getNumbers()) {
-                System.out.print(lottoNumber.toString() + " ");
-            }
-            System.out.println();
-        }
     }
 }
