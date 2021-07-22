96ab23336ac9290e1946b0e9357f7a227101594b
nokchax <nokchax@gmail.com>
2021-07-22 21:55:55 +0900

    [R] test: tokenizer split test

---
+++ src/main/java/string_add_calculator/tokenizer/Tokenizer.java
@@ -0,0 +1,7 @@
+package string_add_calculator.tokenizer;
+
+public class Tokenizer {
+    public static String[] split(String inputString) {
+        return new String[0];
+    }
+}
---
+++ src/test/java/string_add_calculator/tokenizer/TokenizerTest.java
@@ -0,0 +1,30 @@
+package string_add_calculator.tokenizer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("토크나이저 클래스 테스트")
+class TokenizerTest {
+    @DisplayName("받아온 문자열을 , 와 : 로 쪼갠후 쪼개진 문자 배열로 반환한다.")
+    @MethodSource
+    @ParameterizedTest
+    void split(String inputString, List<String> expectedTokens) {
+        assertThat(Tokenizer.split(inputString)).containsAll(expectedTokens);
+    }
+
+    private static Stream<Arguments> split() {
+        return Stream.of(
+                Arguments.of("123,45,678", Arrays.asList("123", "45", "678")),
+                Arguments.of("1:2:3", Arrays.asList("1", "2", "3")),
+                Arguments.of("5,10:25", Arrays.asList("5", "10", "25"))
+        );
+    }
+}
