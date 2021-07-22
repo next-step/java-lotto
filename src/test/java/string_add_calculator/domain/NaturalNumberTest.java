1f5681cc33f3634b947d80432fffc15f9ea8a75d
nokchax <nokchax@gmail.com>
2021-07-22 21:46:32 +0900

    [G] test: when input string is not natural number then throw rte

--- src/test/java/string_add_calculator/domain/NaturalNumberTest.java
+++ src/test/java/string_add_calculator/domain/NaturalNumberTest.java
@@ -2,9 +2,11 @@ package string_add_calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("자연수 클래스 테스트")
 public class NaturalNumberTest {
@@ -14,5 +16,13 @@ public class NaturalNumberTest {
     @ParameterizedTest
     void initNaturalNumber(String numberString) {
         assertThat(NaturalNumber.of(numberString).toInt()).isEqualTo(Integer.parseInt(numberString));
+    }
+
+    @DisplayName("음수나 숫자가 아닌 문자열이 혹은 null 이 들어오면 RuntimeException 을 던진다.")
+    @ValueSource(strings = {"-1", "n"})
+    @NullAndEmptySource
+    @ParameterizedTest
+    void throwExceptionWhenNotNaturalNumber(String inputString) {
+        assertThatThrownBy(() -> NaturalNumber.of(inputString)).isInstanceOf(RuntimeException.class);
     }
 }
