4fda101eb452d2344401777ca910dda7097fe565
nokchax <nokchax@gmail.com>
2021-07-22 21:57:02 +0900

    [G] feat: create tokenizer split method

--- src/main/java/string_add_calculator/tokenizer/Tokenizer.java
+++ src/main/java/string_add_calculator/tokenizer/Tokenizer.java
@@ -2,6 +2,6 @@ package string_add_calculator.tokenizer;
 
 public class Tokenizer {
     public static String[] split(String inputString) {
-        return new String[0];
+        return inputString.split("[,|:]");
     }
 }
