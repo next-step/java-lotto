8798e7216877d4625863432cce329e8a85cd5ae3
nokchax <nokchax@gmail.com>
2021-07-22 21:42:30 +0900

    [R] test: isNaturalNumber test

--- src/test/java/util/StringUtilsTest.java
+++ src/test/java/util/StringUtilsTest.java
@@ -2,7 +2,11 @@ package util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,4 +20,21 @@ public class StringUtilsTest {
         assertThat(StringUtils.isEmpty(inputString)).isTrue();
     }
 
+    @DisplayName("문자열이 자연수 일 경우 true 그렇지 않으면 false 를 리턴한다.")
+    @MethodSource
+    @ParameterizedTest
+    void isNaturalNumber(String inputString, boolean expectedValue) {
+        assertThat(StringUtils.isNaturalNumber(inputString)).isEqualTo(expectedValue);
+    }
+
+    private static Stream<Arguments> isNaturalNumber() {
+        return Stream.of(
+                Arguments.of("0", true),
+                Arguments.of("5", true),
+                Arguments.of("10", true),
+                Arguments.of("-1", false),
+                Arguments.of("", false),
+                Arguments.of(null, false)
+        );
+    }
 }
