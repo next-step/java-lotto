7329714e85f6b03ca45fc3bf04d03e6b897069ab
nokchax <nokchax@gmail.com>
2021-07-22 21:03:43 +0900

    [R] test: split expression by , and : then return it's sum

--- src/test/java/string_add_calculator/StringAddCalculatorTest.java
+++ src/test/java/string_add_calculator/StringAddCalculatorTest.java
@@ -2,7 +2,12 @@ package string_add_calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,5 +21,22 @@ public class StringAddCalculatorTest {
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
 
         assertThat(stringAddCalculator.calculate(inputString)).isZero();
+    }
+
+    @DisplayName(",와 :로 분리하여 값들을 모두 더한 후 반환한다.")
+    @MethodSource
+    @ParameterizedTest
+    void splitUsingCommaAndColonAndReturnItsElements(String expressionString, int expectedValue) {
+        StringAddCalculator stringAddCalculator = new StringAddCalculator();
+
+        assertThat(stringAddCalculator.calculate(expressionString)).isEqualTo(expectedValue);
+    }
+
+    private static Stream<Arguments> splitUsingCommaAndColonAndReturnItsElements() {
+        return Stream.of(
+                Arguments.of("1,2,3", 6),
+                Arguments.of("1:2:3", 6),
+                Arguments.of("1,2:3", 6)
+        );
     }
 }
