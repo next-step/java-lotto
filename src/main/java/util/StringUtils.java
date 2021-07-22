31cea769d4b069586be606dbca39c3eefbf74ce5
nokchax <nokchax@gmail.com>
2021-07-22 20:57:14 +0900

    [B] refactor: make private constructor

--- src/main/java/util/StringUtils.java
+++ src/main/java/util/StringUtils.java
@@ -3,6 +3,9 @@ package util;
 import java.util.Objects;
 
 public class StringUtils {
+
+    private StringUtils() {}
+
     public static boolean isEmpty(String inputString) {
         return Objects.isNull(inputString) || inputString.isEmpty();
     }
