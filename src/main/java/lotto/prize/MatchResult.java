efdeb4f96109dc56e67a7fed261a5b1f2d02b52d
nokchax <nokchax@gmail.com>
2021-07-24 00:04:21 +0900

    [G] feat: make MatchResult throw exception when input value is invalid

--- src/main/java/lotto/prize/MatchResult.java
+++ src/main/java/lotto/prize/MatchResult.java
@@ -1,6 +1,7 @@
 package lotto.prize;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class MatchResult {
 
@@ -8,8 +9,18 @@ public class MatchResult {
     private final Map<LottoPrize, Integer> matchResult;
 
     public MatchResult(int payment, Map<LottoPrize, Integer> matchResult) {
+        validate(payment, matchResult);
         this.payment = payment;
         this.matchResult = matchResult;
+    }
+
+    private void validate(int payment, Map<LottoPrize, Integer> matchResult) {
+        if (payment < 0) {
+            throw new IllegalArgumentException("Payment can't be under zero");
+        }
+        if (Objects.isNull(matchResult)) {
+            throw new IllegalArgumentException("MatchResult can't be null");
+        }
     }
 
     public static MatchResult init(int payment, Map<LottoPrize, Integer> matchResult) {
