b55bcd76bed300947ea0d2a951d4c6be1908066a
nokchax <nokchax@gmail.com>
2021-07-23 21:14:34 +0900

    [G] feat: validate LottoTickets input

--- src/main/java/lotto/ticker/LottoTickets.java
+++ src/main/java/lotto/ticker/LottoTickets.java
@@ -1,12 +1,21 @@
 package lotto.ticker;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoTickets {
     private final List<LottoTicket> lottoTickets;
 
     public LottoTickets(List<LottoTicket> lottoTickets) {
+        validate(lottoTickets);
+
         this.lottoTickets = lottoTickets;
+    }
+
+    private void validate(List<LottoTicket> lottoTickets) {
+        if (Objects.isNull(lottoTickets)) {
+            throw new IllegalArgumentException("To init LottoTickets lottoTickets can't be null");
+        }
     }
 
     public static LottoTickets from(List<LottoTicket> lottoTickets) {
