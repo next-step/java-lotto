d6102c1db6920342ba8f3b0d50b8bd850588aca6
nokchax <nokchax@gmail.com>
2021-07-23 17:16:03 +0900

    [R] test: LottoNumber singleton test

--- src/test/java/lotto/domain/number/LottoNumberTest.java
+++ src/test/java/lotto/domain/number/LottoNumberTest.java
@@ -2,7 +2,11 @@ package lotto.domain.number;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,10 +22,25 @@ class LottoNumberTest {
         assertThat(LottoNumber.from(number)).isNotNull();
     }
 
-    @DisplayName("1~45 범위의 숫자가 아니면 초기화 시에 예외를 발생시킨다")
+    @DisplayName("1 ~ 45 범위의 숫자가 아니면 초기화 시에 예외를 발생시킨다")
     @ValueSource(ints = {-1, 0, 46})
     @ParameterizedTest
     void initExceptionWhenOutOfRange(int number) {
         assertThatThrownBy(() -> LottoNumber.from(number)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 항상 같은 객체를 반환한다")
+    @MethodSource
+    @ParameterizedTest
+    void singletonLottoNumber(int number, LottoNumber expectedLottoNumber) {
+        assertThat(LottoNumber.from(number)).isEqualTo(expectedLottoNumber);
+    }
+
+    private static Stream<Arguments> singletonLottoNumber() {
+        return Stream.of(
+                Arguments.of(1, LottoNumber.from(1)),
+                Arguments.of(5, LottoNumber.from(5)),
+                Arguments.of(10, LottoNumber.from(10))
+        );
     }
 }
