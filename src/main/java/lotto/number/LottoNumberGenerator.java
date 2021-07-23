09af005dfc44115aad25e4aaa0e19832c8c6da84
nokchax <nokchax@gmail.com>
2021-07-23 20:41:02 +0900

    [G] feat: implement generate method

--- src/main/java/lotto/number/LottoNumberGenerator.java
+++ src/main/java/lotto/number/LottoNumberGenerator.java
@@ -1,8 +1,30 @@
 package lotto.number;
 
-public class LottoNumberGenerator {
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-    public static boolean generate() {
-        return false;
+import static lotto.number.LottoNumber.MAX_OF_LOTTO_NUMBER;
+import static lotto.number.LottoNumber.MIN_OF_LOTTO_NUMBER;
+import static lotto.number.LottoNumbers.NUMBER_OF_LOTTO_NUMBERS;
+
+public class LottoNumberGenerator {
+    public static final int BASE_INDEX = 0;
+    private static final List<LottoNumber> LOTTO_NUMBER_POOL =
+            IntStream.rangeClosed(MIN_OF_LOTTO_NUMBER, MAX_OF_LOTTO_NUMBER)
+                    .mapToObj(LottoNumber::from)
+                    .collect(Collectors.toList());
+
+    private LottoNumberGenerator() {}
+
+    public static LottoNumbers generate() {
+        Collections.shuffle(LOTTO_NUMBER_POOL);
+
+        return LottoNumbers.from(extractLottoNumber());
+    }
+
+    private static List<LottoNumber> extractLottoNumber() {
+        return LOTTO_NUMBER_POOL.subList(BASE_INDEX, NUMBER_OF_LOTTO_NUMBERS);
     }
 }
