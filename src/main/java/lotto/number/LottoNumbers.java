c32cbe95b2c56fd65ddf046d45743f098ad79eed
nokchax <nokchax@gmail.com>
2021-07-23 20:19:34 +0900

    [G] feat: create LottoNumber static factory method

--- src/main/java/lotto/number/LottoNumbers.java
+++ src/main/java/lotto/number/LottoNumbers.java
@@ -3,7 +3,15 @@ package lotto.number;
 import java.util.List;
 
 public class LottoNumbers {
-    public static boolean from(List<LottoNumber> lottoNumbers) {
-        return false;
+    public static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoNumbers(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public static LottoNumbers from(List<LottoNumber> lottoNumbers) {
+        return new LottoNumbers(lottoNumbers);
     }
 }
--- src/test/java/lotto/number/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/number/LottoNumberGeneratorTest.java
@@ -8,15 +8,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static lotto.number.LottoNumbers.NUMBER_OF_LOTTO_NUMBERS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("로또 번호 생성기 클래스")
 class LottoNumberGeneratorTest {
 
-    @DisplayName("로또 번호 생성기는 자동으로 6개의 로또 번호를 생성해서 반환한다.")
+    @DisplayName("로또 번호 생성기는 자동으로 " + NUMBER_OF_LOTTO_NUMBERS + "개의 로또 번호를 생성해서 반환한다.")
     @Test
     void generateLottoNumbers() {
-        List<LottoNumber> lottoNumbers = IntStream.rangeClosed(1, 6)
+        List<LottoNumber> lottoNumbers = IntStream.rangeClosed(1, NUMBER_OF_LOTTO_NUMBERS)
                 .mapToObj(LottoNumber::from)
                 .collect(Collectors.toList());
 
