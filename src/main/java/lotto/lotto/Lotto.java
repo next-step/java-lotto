77c3ec5e84cc632bb7584cc4966a8b5c74a44a52
nokchax <nokchax@gmail.com>
2021-07-23 23:26:27 +0900

    [G] feat: create valdiate params method

--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -1,12 +1,27 @@
 package lotto.lotto;
 
+import java.util.Objects;
+
 public class Lotto {
+    private static final int MINIMUM_PAYMENT = 0;
+
     private final int payment;
     private final LottoTickets lottoTickets;
 
     public Lotto(int payment, LottoTickets lottoTickets) {
+        validate(payment, lottoTickets);
         this.payment = payment;
         this.lottoTickets = lottoTickets;
+    }
+
+    private void validate(int payment, LottoTickets lottoTickets) {
+        if (payment < MINIMUM_PAYMENT) {
+            throw new IllegalArgumentException("Payment must larger than " + MINIMUM_PAYMENT);
+        }
+
+        if (Objects.isNull(lottoTickets)) {
+            throw new IllegalArgumentException("LottoTickets can't be null");
+        }
     }
 
     public static Lotto init(int payment, LottoTickets lottoTickets) {
