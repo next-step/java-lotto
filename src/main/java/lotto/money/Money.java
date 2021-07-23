fb8b17bc6e3ea97ca3333f62ffda067dd9703ca8
nokchax <nokchax@gmail.com>
2021-07-24 00:12:49 +0900

    [G] feat: implements throw exception when input is under zero

--- src/main/java/lotto/money/Money.java
+++ src/main/java/lotto/money/Money.java
@@ -1,10 +1,19 @@
 package lotto.money;
 
 public class Money {
+    public static final int ZERO = 0;
     private final int money;
 
     public Money(int money) {
+        validate(money);
+
         this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money < ZERO) {
+            throw new IllegalArgumentException("Money value should be equal or greater than " + ZERO);
+        }
     }
 
     public static Money init(int payment) {
