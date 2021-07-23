ad901533ba2c7cfe016db0c9b604705a637f983b
nokchax <nokchax@gmail.com>
2021-07-23 21:10:31 +0900

    [R] test: init exception test

--- src/test/java/lotto/ticker/LottoTicketsTest.java
+++ src/test/java/lotto/ticker/LottoTicketsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static lotto.helper.Generator.generateNumberOfLottoTicket;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("로또 티켓 그룹 클래스 테스트")
 class LottoTicketsTest {
@@ -13,5 +14,11 @@ class LottoTicketsTest {
     @Test
     void initLottoTickets() {
         assertThat(LottoTickets.from(generateNumberOfLottoTicket(5))).isInstanceOf(LottoTickets.class);
+    }
+
+    @DisplayName("들어온 초기화 값이 null 일 경우 예외를 던진다.")
+    @Test
+    void throwExceptionWhenInitValueIsNull() {
+        assertThatThrownBy(() -> LottoTickets.from(null)).isInstanceOf(IllegalArgumentException.class);
     }
 }
