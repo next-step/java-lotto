b451318eb5c35829c27149601862c39450e15415
nokchax <nokchax@gmail.com>
2021-07-24 00:02:36 +0900

    [R] test: init exception test

--- src/test/java/lotto/prize/MatchResultTest.java
+++ src/test/java/lotto/prize/MatchResultTest.java
@@ -3,11 +3,16 @@ package lotto.prize;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("로또 당첨 결과 클래스 테스트")
 class MatchResultTest {
@@ -19,6 +24,21 @@ class MatchResultTest {
         Map<LottoPrize, Integer> matchResult = Collections.emptyMap();
 
         assertThat(MatchResult.init(payment, matchResult)).isInstanceOf(MatchResult.class);
+    }
+
+    @DisplayName("당첨 결과중 지불 금액과 매치 결과는 null 일 수 없다.")
+    @MethodSource
+    @ParameterizedTest
+    void initException(int payment, Map<LottoPrize, Integer> matchResult) {
+        assertThatThrownBy(() -> MatchResult.init(payment, matchResult)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> initException() {
+        return Stream.of(
+                Arguments.of(-1, Collections.emptyMap()),
+                Arguments.of(1, null),
+                Arguments.of(-1, null)
+        );
     }
 
 }
