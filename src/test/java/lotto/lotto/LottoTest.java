23455c46b8a94c16df32f452907579e467aac4e3
nokchax <nokchax@gmail.com>
2021-07-24 02:04:13 +0900

    [R] test: match prize test

--- src/test/java/lotto/lotto/LottoTest.java
+++ src/test/java/lotto/lotto/LottoTest.java
@@ -2,12 +2,16 @@ package lotto.lotto;
 
 import lotto.helper.Generator;
 import lotto.money.Money;
+import lotto.number.WinningNumbers;
+import lotto.prize.LottoPrize;
+import lotto.prize.MatchResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,6 +47,21 @@ public class LottoTest {
     @DisplayName("당첨 번호를 전달 받아 당첨 정보를 반환한다.")
     @Test
     void matchPrize() {
+        WinningNumbers winningNumbers = Generator.winningNumbers(5, 10, 15, 20, 25, 30);
+        LottoTickets lottoTickets = LottoTickets.from(
+                Arrays.asList(
+                        Generator.lottoTicket(5, 10, 15, 20, 25, 30),
+                        Generator.lottoTicket(5, 10, 15, 20, 25, 31),
+                        Generator.lottoTicket(5, 10, 15, 20, 26, 31),
+                        Generator.lottoTicket(5, 10, 15, 21, 26, 31),
+                        Generator.lottoTicket(5, 10, 16, 21, 26, 31)
+                )
+        );
 
+        Lotto lotto = Lotto.init(Money.init(10_000), lottoTickets);
+        MatchResult matchResult = lotto.match(winningNumbers);
+
+        Arrays.stream(LottoPrize.values())
+                .forEach(lottoPrize -> assertThat(matchResult.matchCount(lottoPrize)).isEqualTo(1));
     }
 }
