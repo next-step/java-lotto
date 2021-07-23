5ccc94ee642508afcc614289ac7f8fa8e195c868
nokchax <nokchax@gmail.com>
2021-07-24 00:11:01 +0900

    [R] test: init money exception test

--- src/test/java/lotto/money/MoneyTest.java
+++ src/test/java/lotto/money/MoneyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("돈 클래스 테스트")
 class MoneyTest {
@@ -13,7 +14,15 @@ class MoneyTest {
     @DisplayName("돈은 숫자를 가지고 초기화 한다.")
     @ValueSource(ints = {0, 10, 100})
     @ParameterizedTest
-    void initMoney(int payment) {
-        assertThat(Money.init(payment)).isInstanceOf(Money.class);
+    void initMoney(int monet) {
+        assertThat(Money.init(monet)).isInstanceOf(Money.class);
     }
+
+    @DisplayName("초기화 하는 돈이 0 미만일 경우 예외를 발생시킨다.")
+    @ValueSource(ints = {-1, -5})
+    @ParameterizedTest
+    void initMoneyException(int money) {
+        assertThatThrownBy(() -> Money.init(money)).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
