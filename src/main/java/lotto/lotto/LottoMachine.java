2d85bfb15824f74754026d21516c56a13d838bb9
nokchax <nokchax@gmail.com>
2021-07-24 00:24:45 +0900

    [G] refactor: change payment type to Money class

--- src/main/java/lotto/lotto/LottoMachine.java
+++ src/main/java/lotto/lotto/LottoMachine.java
@@ -1,24 +1,31 @@
 package lotto.lotto;
 
+import lotto.money.Money;
 import lotto.number.LottoNumberGenerator;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoMachine {
-    private static final int PRICE_OF_A_TICKET = 1_000;
+    private static final Money PRICE_OF_A_TICKET = Money.init(1_000);
 
     private LottoMachine() {}
 
     public static Lotto buy(int payment) {
+        LottoTickets lottoTickets = buyAutoTicket(Money.init(payment));
+
+        return Lotto.init(payment, lottoTickets);
+    }
+
+    public static Lotto buy(Money payment) {
         LottoTickets lottoTickets = buyAutoTicket(payment);
 
         return Lotto.init(payment, lottoTickets);
     }
 
-    private static LottoTickets buyAutoTicket(int payment) {
+    private static LottoTickets buyAutoTicket(Money payment) {
         return Stream.generate(LottoMachine::generateAutoTicket)
-                .limit(payment / PRICE_OF_A_TICKET)
+                .limit(payment.getAffordableCount(PRICE_OF_A_TICKET))
                 .collect(Collectors.collectingAndThen(Collectors.toList(), LottoTickets::from));
     }
 
