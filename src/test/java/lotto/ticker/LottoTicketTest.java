722413f41e51c5b9008dda15dd08b13c85ee2e20
nokchax <nokchax@gmail.com>
2021-07-23 20:51:06 +0900

    [R] test: throw exception when LottoNumbers is null

--- src/test/java/lotto/ticker/LottoTicketTest.java
+++ src/test/java/lotto/ticker/LottoTicketTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 @DisplayName("로또 한장애 대한 클래스 테스트")
@@ -14,6 +15,12 @@ class LottoTicketTest {
     @Test
     void initLottoTicket() {
         assertThat(LottoTicket.from(LottoNumberGenerator.generate())).isInstanceOf(LottoTicket.class);
+    }
+
+    @DisplayName("초기화 시에 로또 숫자 그룹이 null 일 경우 예외를 던진다.")
+    @Test
+    void initExceptionWhenLottoTicketIsNull() {
+        assertThatThrownBy(() -> LottoTicket.from(null)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
