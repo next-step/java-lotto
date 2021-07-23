acf389f1252910874d199d5c5a676457cb7b6c23
nokchax <nokchax@gmail.com>
2021-07-23 16:42:09 +0900

    [G] feat: init LottoNumber using static factory method

--- src/main/java/lotto/domain/number/LottoNumber.java
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -1,7 +1,14 @@
 package lotto.domain.number;
 
 public class LottoNumber {
-    public static Object from(int number) {
-        return null;
+
+    private final int number;
+
+    private LottoNumber(int number) {
+        this.number = number;
+    }
+
+    public static LottoNumber from(int number) {
+        return new LottoNumber(number);
     }
 }
