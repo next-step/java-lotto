c33caa6b4f993327434606993c99b73e145a4a97
nokchax <nokchax@gmail.com>
2021-07-23 17:45:58 +0900

    [G] feat: make singleton lotto number

--- src/main/java/lotto/domain/number/LottoNumber.java
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -1,8 +1,18 @@
 package lotto.domain.number;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoNumber {
-    public static final int MAX_OF_LOTTO_NUMBER = 45;
     public static final int MIN_OF_LOTTO_NUMBER = 1;
+    public static final int MAX_OF_LOTTO_NUMBER = 45;
+
+    private static final Map<Integer, LottoNumber> LOTTO_NUMBERS =
+            IntStream.rangeClosed(MIN_OF_LOTTO_NUMBER, MAX_OF_LOTTO_NUMBER)
+                    .mapToObj(LottoNumber::new)
+                    .collect(Collectors.toMap(LottoNumber::toInt, Function.identity()));
 
     private final int number;
 
@@ -14,13 +24,25 @@ public class LottoNumber {
 
     private void validate(int number) {
         if (number < MIN_OF_LOTTO_NUMBER || number > MAX_OF_LOTTO_NUMBER) {
-            throw new IllegalArgumentException(
-                    "Lotto number must be greater than " + MIN_OF_LOTTO_NUMBER + " and less than " + MAX_OF_LOTTO_NUMBER
-            );
+            throwIllegalArgumentException();
         }
     }
 
     public static LottoNumber from(int number) {
-        return new LottoNumber(number);
+        if (!LOTTO_NUMBERS.containsKey(number)) {
+            throwIllegalArgumentException();
+        }
+
+        return LOTTO_NUMBERS.get(number);
+    }
+
+    private static void throwIllegalArgumentException() {
+        throw new IllegalArgumentException(
+                "Lotto number must be greater than " + MIN_OF_LOTTO_NUMBER + " and less than " + MAX_OF_LOTTO_NUMBER
+        );
+    }
+
+    private int toInt() {
+        return number;
     }
 }
