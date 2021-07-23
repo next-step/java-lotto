3a2ffaeea6338132a34892c1bca7232ad37790ad
nokchax <nokchax@gmail.com>
2021-07-23 20:30:58 +0900

    [G] feat: make constructor throw exception when input is invalid

--- src/main/java/lotto/number/LottoNumbers.java
+++ src/main/java/lotto/number/LottoNumbers.java
@@ -1,6 +1,7 @@
 package lotto.number;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoNumbers {
     public static final int NUMBER_OF_LOTTO_NUMBERS = 6;
@@ -8,7 +9,21 @@ public class LottoNumbers {
     private final List<LottoNumber> lottoNumbers;
 
     public LottoNumbers(List<LottoNumber> lottoNumbers) {
+        validate(lottoNumbers);
+
         this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validate(List<LottoNumber> lottoNumbers) {
+        if (Objects.isNull(lottoNumbers)) {
+            throw new IllegalArgumentException("Input lotto numbers are null");
+        }
+
+        if (lottoNumbers.size() != NUMBER_OF_LOTTO_NUMBERS) {
+            throw new IllegalArgumentException(
+                    "Number of lotto numbers is " + lottoNumbers.size() + " number must equal to " + NUMBER_OF_LOTTO_NUMBERS
+            );
+        }
     }
 
     public static LottoNumbers from(List<LottoNumber> lottoNumbers) {
