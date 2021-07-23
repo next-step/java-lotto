d5f54faafa162b981d648a4e0a8d93ac2c1f8da3
nokchax <nokchax@gmail.com>
2021-07-23 17:10:58 +0900

    [G] feat: throw exception when int is over range

--- src/main/java/lotto/domain/number/LottoNumber.java
+++ src/main/java/lotto/domain/number/LottoNumber.java
@@ -1,11 +1,23 @@
 package lotto.domain.number;
 
 public class LottoNumber {
+    public static final int MAX_OF_LOTTO_NUMBER = 45;
+    public static final int MIN_OF_LOTTO_NUMBER = 1;
 
     private final int number;
 
     private LottoNumber(int number) {
+        validate(number);
+
         this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < MIN_OF_LOTTO_NUMBER || number > MAX_OF_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(
+                    "Lotto number must be greater than " + MIN_OF_LOTTO_NUMBER + " and less than " + MAX_OF_LOTTO_NUMBER
+            );
+        }
     }
 
     public static LottoNumber from(int number) {
