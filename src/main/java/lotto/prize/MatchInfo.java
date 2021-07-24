ae76d316d8b8cd371bc26706117586193069db00
nokchax <nokchax@gmail.com>
2021-07-24 20:52:07 +0900

    [G] feat: create match info static factory method

--- src/main/java/lotto/prize/MatchInfo.java
+++ src/main/java/lotto/prize/MatchInfo.java
@@ -1,7 +1,15 @@
 package lotto.prize;
 
 public class MatchInfo {
-    public static boolean of(int i, boolean b) {
-        return false;
+    private final int matchCount;
+    private final boolean bonusMatch;
+
+    private MatchInfo(int matchCount, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public static MatchInfo of(int matchCount, boolean bonusMatch) {
+        return new MatchInfo(matchCount, bonusMatch);
     }
 }
