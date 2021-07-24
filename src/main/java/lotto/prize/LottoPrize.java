f48d9e6a06437dd7af5fcb31b59e5ea6a7bf7036
nokchax <nokchax@gmail.com>
2021-07-24 19:48:52 +0900

    [B] refactor: constructor get int not Money so that remove code duplicate

--- src/main/java/lotto/prize/LottoPrize.java
+++ src/main/java/lotto/prize/LottoPrize.java
@@ -9,11 +9,11 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum LottoPrize {
-    FIRST(6, Money.from(2_000_000_000)),
-    SECOND(5, Money.from(1_500_000)),
-    THIRD(4, Money.from(50_000)),
-    FOURTH(3, Money.from(5_000)),
-    NONE(0, Money.from(0));
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NONE(0, 0);
 
     private static final Map<Integer, LottoPrize> LOTTO_PRIZES = Arrays.stream(LottoPrize.values())
             .collect(Collectors.toMap(LottoPrize::getMatchCount, Function.identity()));
@@ -21,9 +21,9 @@ public enum LottoPrize {
     private final int matchCount;
     private final Money prizeMoney;
 
-    LottoPrize(int matchCount, Money prizeMoney) {
+    LottoPrize(int matchCount, int prizeMoney) {
         this.matchCount = matchCount;
-        this.prizeMoney = prizeMoney;
+        this.prizeMoney = Money.from(prizeMoney);
     }
 
     public static LottoPrize of(int matchCount) {
