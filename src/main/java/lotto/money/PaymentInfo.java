a79e017a531b3f48696be7e14c359e7e8bd6be17
nokchax <nokchax@gmail.com>
2021-07-25 17:22:07 +0900

    [R] test: init payment info test

---
+++ src/main/java/lotto/money/PaymentInfo.java
@@ -0,0 +1,9 @@
+package lotto.money;
+
+import lotto.lotto.LottoTickets;
+
+public class PaymentInfo {
+    public static Object of(Money payment, LottoTickets manualLottoNumbers) {
+        return null;
+    }
+}
---
+++ src/test/java/lotto/money/PaymentInfoTest.java
@@ -0,0 +1,30 @@
+package lotto.money;
+
+import lotto.helper.Generator;
+import lotto.lotto.LottoTickets;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("구매 정보를 담고 있는 클래스 테스트")
+class PaymentInfoTest {
+
+    @DisplayName("구매 정보는, 구매 금액과 수동 로또 티켓을 가지고 초기화 한다.")
+    @MethodSource
+    @ParameterizedTest
+    void init(Money payment, LottoTickets manualLottoNumbers) {
+        assertThat(PaymentInfo.of(payment, manualLottoNumbers)).isNotNull();
+    }
+
+    private static Stream<Arguments> of() {
+        return Stream.of(
+                Arguments.of(Money.from(5000), Generator.autoLottoTickets(5)),
+                Arguments.of(Money.from(5000), Generator.autoLottoTickets(10))
+        );
+    }
+}
