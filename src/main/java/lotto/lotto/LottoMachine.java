8960c6fadc7a70a56032563fc1b4c346bdad68f1
nokchax <nokchax@gmail.com>
2021-07-25 18:18:51 +0900

    [G] feat: implements buyLotto using PaymentInfo and LottoTickets

--- src/main/java/lotto/lotto/LottoMachine.java
+++ src/main/java/lotto/lotto/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto.lotto;
 
 import lotto.money.Money;
+import lotto.money.PaymentInfo;
 import lotto.number.LottoNumberGenerator;
 
 import java.util.Objects;
@@ -20,6 +21,25 @@ public class LottoMachine {
         return Lotto.of(payment, lottoTickets);
     }
 
+    public static Lotto buy(PaymentInfo paymentInfo, LottoTickets lottoTickets) {
+        validate(paymentInfo, lottoTickets);
+
+        LottoTickets autoLottoTickets = buyAutoTicket(paymentInfo);
+        lottoTickets.merge(autoLottoTickets);
+
+        return Lotto.of(paymentInfo, lottoTickets);
+    }
+
+    private static void validate(PaymentInfo paymentInfo, LottoTickets lottoTickets) {
+        if (Objects.isNull(paymentInfo)) {
+            throw new IllegalArgumentException("PaymentInfo can't be null");
+        }
+
+        if (Objects.isNull(lottoTickets)) {
+            throw new IllegalArgumentException("LottoTickets can't be null");
+        }
+    }
+
     private static void validate(Money payment) {
         if (Objects.isNull(payment)) {
             throw new IllegalArgumentException("Payment can't be null");
@@ -29,6 +49,12 @@ public class LottoMachine {
     private static LottoTickets buyAutoTicket(Money payment) {
         return Stream.generate(LottoMachine::generateAutoTicket)
                 .limit(payment.getAffordableCount(PRICE_OF_A_TICKET))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), LottoTickets::from));
+    }
+
+    private static LottoTickets buyAutoTicket(PaymentInfo paymentInfo) {
+        return Stream.generate(LottoMachine::generateAutoTicket)
+                .limit(paymentInfo.getAffordableCount(PRICE_OF_A_TICKET))
                 .collect(Collectors.collectingAndThen(Collectors.toList(), LottoTickets::from));
     }
 
--- src/test/java/lotto/lotto/LottoMachineTest.java
+++ src/test/java/lotto/lotto/LottoMachineTest.java
@@ -1,8 +1,16 @@
 package lotto.lotto;
 
+import lotto.helper.Fixture;
+import lotto.helper.Generator;
 import lotto.money.Money;
+import lotto.money.PaymentInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static lotto.lotto.LottoMachine.PRICE_OF_A_TICKET;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,5 +33,34 @@ class LottoMachineTest {
     @Test
     void buyException() {
         assertThatThrownBy(() -> LottoMachine.buy(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구매 정봐와 수동 로또를 받고 추가적으로 구매 가능한 수의 자동 로또 티켓을 합친 로또를 반환한다.")
+    @Test
+    void buyLotto2() {
+        int paymentValue = 10_000;
+        Money payment = Money.from(10_000);
+        LottoTickets manualLottoTickets = Generator.autoLottoTickets(5);
+        PaymentInfo paymentInfo = PaymentInfo.of(payment, manualLottoTickets);
+
+        Lotto lotto = LottoMachine.buy(paymentInfo, manualLottoTickets);
+
+        assertThat(lotto).isNotNull();
+        assertThat(lotto.getLottoTickets().size()).isEqualTo(paymentValue / PRICE_OF_A_TICKET.toInt());
+    }
+
+    @DisplayName("구매 정보나 수동 로또가 null 일 경우 예외를 발생 시킨다.")
+    @MethodSource
+    @ParameterizedTest
+    void buyException2(PaymentInfo paymentInfo, LottoTickets lottoTickets) {
+        assertThatThrownBy(() -> LottoMachine.buy(paymentInfo, lottoTickets)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> buyException2() {
+        return Stream.of(
+                Arguments.of(null, Generator.autoLottoTickets(1)),
+                Arguments.of(Fixture.paymentInfo(), null),
+                Arguments.of(null, null)
+        );
     }
 }
