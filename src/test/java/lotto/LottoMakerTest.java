d1d4bfecfc56dfc061f46274d93da8f709c52ae8
lsj8367 <lsj8367@naver.com>
2021-07-27 11:23:05 +0900

    test: 금액 예외처리 추가

--- src/test/java/lotto/LottoMakerTest.java
+++ src/test/java/lotto/LottoMakerTest.java
@@ -1,9 +1,8 @@
 package lotto;
 
+import lotto.domain.LottoMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoMakerTest {
 
@@ -11,7 +10,7 @@ class LottoMakerTest {
     @DisplayName("로또 생성 테스트")
     void makeLottoTest() {
         LottoMaker lottoMaker = new LottoMaker();
-        System.out.println(lottoMaker.makeLotto());
+        System.out.println(lottoMaker.run());
     }
 
 }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -14,12 +14,22 @@ class InputViewTest {
     }
 
     @Test
-    @DisplayName("로또 금액 예외")
+    @DisplayName("로또 금액 예외(천원단위가 아닐때)")
     void priceExceptionTest() {
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(() -> inputView.buyLotto(999))
+                                          .isInstanceOf(IllegalArgumentException.class)
+                                          .hasMessageContaining("천원 단위로 입력해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 금액 예외(천원보다 작을때)")
+    void priceExceptionTest2() {
         InputView inputView = new InputView();
 
         assertThatThrownBy(() -> inputView.buyLotto(900))
                                           .isInstanceOf(IllegalArgumentException.class)
-                                          .hasMessageContaining("천원단위");
+                                          .hasMessageContaining("작습니다.");
     }
 }
