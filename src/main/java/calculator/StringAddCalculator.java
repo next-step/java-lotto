cc569f7616e2aee066d6ad6504e9a1d096654ff3
rladowl92 <rladowl92@naver.com>
2021-07-28 01:01:17 +0900

    refactor: 메소드 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -12,22 +12,26 @@ public class StringAddCalculator {
         if(input==null || input.isEmpty()){
             return 0;
         }
+        List<String> tokens = splitTokens(input);
+        checkValidation(tokens);
+        return tokens.stream().mapToInt(Integer::parseInt).sum();
+    }
 
-        List<String> tokens = Arrays.asList(input.split(SPLIT_KEYWORD));
-
+    private static List<String> splitTokens(String input){
         Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
-            tokens = Arrays.asList(matcher.group(2).split(customDelimiter));
+            return Arrays.asList(matcher.group(2).split(customDelimiter));
         }
+        return Arrays.asList(input.split(SPLIT_KEYWORD));
+    }
 
+    private static void checkValidation(List<String> tokens){
         tokens.stream()
                 .filter(i->Integer.parseInt(i) < 0)
                 .findAny()
                 .map(i->{
                     throw new RuntimeException("0 이상의 숫자를 입력해주세요.");
                 });
-
-        return tokens.stream().mapToInt(Integer::parseInt).sum();
     }
 }
