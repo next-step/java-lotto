87b95f7e7eae277018f9629c7387fe8df0f281e2
lsj8367 <lsj8367@naver.com>
2021-07-27 13:08:44 +0900

    feat: 총수익률 구현

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -5,20 +5,67 @@ import lotto.domain.Lotto;
 import java.util.List;
 
 public class ResultView {
-    
+    private static final int FIRST_REWARD = 20000000;
+    private static final int SECOND_REWARD = 1500000;
+    private static final int THIRD_REWARD = 50000;
+    private static final int FORTH_REWARD = 5000;
+
     private int count = 0;
-    
-    public int correctCheck(Lotto lotto, List<Integer> correctNumbers) {
+    private int first = 0;
+    private int second = 0;
+    private int third = 0;
+    private int forth = 0;
+
+    public void correctCheck(Lotto lotto, List<Integer> correctNumbers) {
+        count = 0;
+        List<Integer> list = lotto.getLotto();
         for(int i = 0 ; i < correctNumbers.size(); i++) {
-            getCorrectCount(lotto, correctNumbers, i);
+            getCorrectCount(list, correctNumbers, i);
         }
-        return count;
+        addRank(count);
     }
 
-    private void getCorrectCount(Lotto lotto, List<Integer> correctNumbers, int index) {
-        if (lotto.getLotto().contains(correctNumbers.get(index))) {
+    private void getCorrectCount(List<Integer> list, List<Integer> correctNumbers, int index) {
+        if (list.contains(correctNumbers.get(index))) {
             count++;
         }
     }
 
+    public void lottoList(Lotto lotto) {
+        System.out.println(lotto.getLotto());
+    }
+
+    private void addRank(int checkCount) {
+        switch (checkCount) {
+            case 6:
+                first++;
+                break;
+            case 5:
+                second++;
+                break;
+            case 4:
+                third++;
+                break;
+            case 3:
+                forth++;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void printResult(int price) {
+        System.out.println("당첨 통계");
+
+        System.out.println("-----------");
+
+        System.out.println("1등(" + FIRST_REWARD + "원) : " + first + "개");
+        System.out.println("2등(" + SECOND_REWARD + "원) : " + second + "개");
+        System.out.println("3등(" + THIRD_REWARD + "원) : " + third + "개");
+        System.out.println("4등(" + FORTH_REWARD + "원) : " + forth + "개");
+
+        double result = ((first * FIRST_REWARD) + (second * SECOND_REWARD) + (third * THIRD_REWARD) + (forth * FORTH_REWARD)) / price;
+        System.out.println("총 수익률은 : " + result + "입니다.");
+    }
 }
