5ac07baf26cc41bab2c6d07a526e7c8aae4141d5
rladowl92 <rladowl92@naver.com>
2021-07-28 00:39:28 +0900

    feat: 콜론, 커스텀 구분자 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,11 +2,20 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final String SPLIT_KEYWORD = ",";
+    private static final String SPLIT_KEYWORD = ",|:";
+
     public static int splitAndSum(String input) {
-        List<String> inputs = Arrays.asList(input.split(SPLIT_KEYWORD));
-        return inputs.stream().mapToInt(Integer::parseInt).sum();
+        List<String> tokens = Arrays.asList(input.split(SPLIT_KEYWORD));
+
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            tokens = Arrays.asList(matcher.group(2).split(customDelimiter));
+        }
+        return tokens.stream().mapToInt(Integer::parseInt).sum();
     }
 }
