8786d7c1055739c95c36304f3ff5aeb9bab2e3d6
rkdals213 <56009774+rkdals213@users.noreply.github.com>
2021-07-27 12:31:19 +0900

    1단계 - 문자열 덧셈 계산기 (#1788)
    * feat: 문자열 덧셈 계산기 구현
    
    * feat: 문자열 덧셈 계산기 구현
    
    * docs: readme 작성

---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,47 @@
+package step1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringAddCalculator {
+    static public int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) return 0;
+
+        List<Integer> tokens = split(text);
+        validateTokens(tokens);
+        return sum(tokens);
+    }
+
+    static private List<Integer> split(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return Arrays.stream(m.group(2)
+                .split(customDelimiter))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        }
+
+        return Arrays.stream(text.split("[,:]"))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
+
+    static private int sum(List<Integer> tokens) {
+        return tokens.stream()
+            .reduce(Integer::sum)
+            .get();
+    }
+
+    static private void validateTokens(List<Integer> tokens) {
+        if (tokens.stream().anyMatch(StringAddCalculator::isNegative)) throw new RuntimeException();
+    }
+
+    static private boolean isNegative(int i) {
+        return i < 0;
+    }
+}
---
+++ src/test/java/StringAddCalculatorTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+import step1.StringAddCalculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2,3,4,5,6,7,8,9,10");
+        assertThat(result).isEqualTo(55);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("//;\n-1;2;3"))
+        .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+            .isInstanceOf(RuntimeException.class);
+    }
+}
