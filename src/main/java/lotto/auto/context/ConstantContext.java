ce7f2aa803c05ced5b7b38fa7f85820b5e4bf3f4
shirohoo <hch4102@naver.com>
2021-07-28 18:47:51 +0900

    feat: 전역 상수 저장 클래스를 작성하였다

---
+++ src/main/java/lotto/auto/context/ConstantContext.java
@@ -0,0 +1,8 @@
+package lotto.auto.context;
+
+public class ConstantContext {
+    public static final String MATCH_TABLE_KEY = "matchTable";
+    public static final String EARNINGS_RATE_KEY = "earningsRateKey";
+
+    public static final int GAME_PRICE = 1_000;
+}
