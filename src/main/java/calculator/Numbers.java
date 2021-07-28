60d3c9e0a814aa1ea727557d5efda75603fa2139
seondongpyo <seondongpyo@gmail.com>
2021-07-28 09:46:58 +0900

    refactor(Numbers): Integer를 Number로 변경 및 기존 로직 수정

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -3,21 +3,22 @@ package calculator;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
 public class Numbers {
 
-    private final List<Integer> numbers;
+    private final List<Number> numbers;
 
-    private Numbers(List<Integer> numbers) {
+    private Numbers(List<Number> numbers) {
         this.numbers = numbers;
     }
 
     public static Numbers from(String[] numberArray) {
-        List<Integer> numbers = Arrays.stream(numberArray)
-                                    .map(Integer::parseInt)
+        List<Number> numbers = Arrays.stream(numberArray)
+                                    .map(Number::new)
                                     .collect(collectingAndThen(toList(), Collections::unmodifiableList));
 
         return new Numbers(numbers);
@@ -25,11 +26,13 @@ public class Numbers {
 
     public int sum() {
         return numbers.stream()
-                .mapToInt(number -> number)
+                .mapToInt(Number::getNumber)
                 .sum();
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return numbers.stream()
+                .map(Number::getNumber)
+                .collect(Collectors.toList());
     }
 }
