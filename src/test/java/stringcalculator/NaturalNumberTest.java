ccfe042f4993f8f738586fb6a25190fe1148eda9
Giwan Kim <giwan.kim@gmail.com>
2021-07-28 18:24:26 +0900

    feat(stringcalculator): Add NaturalNumber class
    NaturalNumber includes a factory method to parse String token.

---
+++ src/test/java/stringcalculator/NaturalNumberTest.java
@@ -0,0 +1,37 @@
+package stringcalculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NaturalNumberTest {
+
+    @Test
+    @DisplayName("생성자에 음수를 입력하면 RuntimeException throw")
+    void constructor_negative() {
+        assertThatThrownBy(() -> NaturalNumber.valueOf(-1))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("생성자 작동")
+    void constructor_positive() {
+        NaturalNumber naturalNumber = NaturalNumber.valueOf(0);
+        assertThat(naturalNumber).isEqualTo(NaturalNumber.valueOf(0));
+    }
+
+    @Test
+    @DisplayName("팩토리 메서드에 String 값을 인자로 주었을 때")
+    void valueOf_input_that_can_be_parsed() {
+        NaturalNumber naturalNumber = NaturalNumber.valueOf("3");
+        assertThat(naturalNumber).isEqualTo(NaturalNumber.valueOf(3));
+    }
+
+    @Test
+    @DisplayName("팩토리 메서드에 숫자 외의 String 값을 인자로 주었을 때")
+    void valueOf_non_numerical_input() {
+        assertThatThrownBy(() -> NaturalNumber.valueOf("asdf")).isInstanceOf(
+                RuntimeException.class);
+    }
+}
