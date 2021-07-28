91169ffa14a3cf5eacf78b11b3190efba4f3b124
seondongpyo <seondongpyo@gmail.com>
2021-07-28 11:54:05 +0900

    refactor(Splitter): 커스텀 구분자 패턴을 상수로 추출

--- src/main/java/calculator/Splitter.java
+++ src/main/java/calculator/Splitter.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Splitter {
 
-    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
     private static final String DELIMITER = "[,:]";
     private static final int INDEX_OF_CUSTOM_DELIMITER = 1;
     private static final int INDEX_OF_NUMBERS = 2;
@@ -14,7 +14,7 @@ public class Splitter {
     }
 
     public static String[] split(String inputText) {
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(inputText);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputText);
         if (matcher.find()) {
             String customDelimiter = matcher.group(INDEX_OF_CUSTOM_DELIMITER);
             return matcher.group(INDEX_OF_NUMBERS).split(customDelimiter);
