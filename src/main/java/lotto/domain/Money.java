21c8a8a071a06b35a10a59370277b8a884df4df8
jayce6190 <jayce6190gmail.com>
2021-07-28 19:22:15 +0900

    feature: 입력 금액 Money 클래스로 처리
    (cherry picked from commit de37e38377762211c84eda5590784a382b545877)

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,2 +1,36 @@
-package lotto.domain;public class Money {
+package lotto.domain;
+
+public class Money {
+    private static final int LOWEST_AMOUNT = 1000;
+    private final int amount;
+
+    private Money(int money) {
+        validate(money);
+        this.amount = money;
+    }
+
+    public static Money of(int money) {
+        return new Money(money);
+    }
+
+    private void validate(int money) {
+        if (money < LOWEST_AMOUNT) {
+            throw new IllegalArgumentException(LOWEST_AMOUNT + " 보다 작을 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
+    }
 }
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class MoneyTest {
-  
+
+    @DisplayName("입력 금액이 최저금액인 천원보다 크면 Money객체를 생성한다")
+    @Test
+    public void should_make_object_over_input_value_more_than_lowest_amount() throws Exception {
+        //arrange
+        Money money = Money.of(2000);
+
+        //act, assert
+        assertThat(money).isEqualTo(Money.of(2000));
+    }
+
+    @DisplayName("입력 금액이 최저금액인 천원보다 작으면 IllegalArgumentException을 반환한다")
+    @Test
+    public void should_throw_exception_input_value_under_than_lowest_amount() throws Exception {
+        //arrange, act, assert
+        assertThatIllegalArgumentException().isThrownBy(() -> Money.of(100));
+    }
 }
