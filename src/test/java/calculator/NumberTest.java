aed5c6e27a2bde82aff0deee50083800702dde0d
seondongpyo <seondongpyo@gmail.com>
2021-07-28 12:05:09 +0900

    refactor(NumberTest): Number 생성 케이스를 ValueSource 매개변수 테스트로 수정

--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,10 +10,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class NumberTest {
 
     @DisplayName("숫자 문자열로 Number 객체를 생성한다.")
-    @Test
-    void create() {
-        Number number = new Number("1");
-        assertThat(number.intValue()).isEqualTo(1);
+    @ValueSource(strings = {"1", "10", "100"})
+    @ParameterizedTest
+    void create(String numberString) {
+        Number number = new Number(numberString);
+        assertThat(number.intValue()).isEqualTo(Integer.parseInt(numberString));
     }
 
     @DisplayName("음수 또는 숫자가 아닌 문자가 전달되면 RuntimeException 예외가 발생한다.")
