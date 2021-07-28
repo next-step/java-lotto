6e5b96f1cd86b92c570b50f0dc1722e5d70293ac
YONGJU <odetsuo@gmail.com>
2021-07-28 14:46:56 +0900

    refactor : 숫자와 관련된 검증로직을 포장하여 객체화

---
+++ src/main/java/stringaddition/NationalNumber.java
@@ -0,0 +1,26 @@
+package stringaddition;
+
+public class NationalNumber {
+
+	private static final int ZERO = 0;
+
+	public void isValid(String s) {
+		validateText(s);
+		if (isNegative(s)) {
+			throw new SplitException();
+		}
+	}
+
+	private boolean isNegative(String s) {
+		return Integer.parseInt(s) < ZERO;
+	}
+
+	private void validateText(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			throw new SplitException("숫자가 아닌 문자를 포함하는 문자열은 계산할 수 없습니다.");
+		}
+	}
+
+}
