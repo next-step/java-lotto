5edc30551da80e877d8797fae6362ec896667f6f
darkant99 <darkant99@naver.com>
2021-07-29 01:29:29 +0900

    [FEAT] 테스트 통과

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,23 @@
 package lotto.domain;
 
-import java.util.List;
+import lotto.exception.OutOfSizeException;
+
 import java.util.Set;
 
 public class Lotto {
     private final Set<LottoNumber> values;
 
+    private static final int LOTTO_NUMBER_SIZE = 6;
+
     public Lotto(Set<LottoNumber> numbers) {
         this.values = numbers;
+
+        validValues();
+    }
+
+    private void validValues() {
+        if (values.size() != LOTTO_NUMBER_SIZE) {
+            throw new OutOfSizeException("로또 번호가 6개가 아닙니다.");
+        }
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
+import lotto.exception.OutOfRangeException;
+import lotto.exception.OutOfSizeException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,15 +17,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
-    @CsvSource({
+    @ValueSource(strings = {
             "1,10,20,30,40,45"
     })
     @ParameterizedTest
-    public void lottoTest_생성자(List<Integer> numbers) {
-        Set<LottoNumber> lottoNumbers =
-                numbers.stream()
-                        .map(LottoNumber::new)
-                        .collect(Collectors.toSet());
+    public void lottoTest_생성자(String strNumbers) {
+        Set<LottoNumber> lottoNumbers = Arrays.stream(strNumbers.split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet());
 
         assertThat(
                 new Lotto(lottoNumbers)
@@ -30,16 +33,14 @@ public class LottoTest {
     }
 
     // Lotto의 size 메소드를 테스트 후 제거할것
-    @CsvSource({
-            "0, 2, 3, 4, 5, 45",
-            "1, 2, 3, 4, 5, 46",
-            "1, 2, 3, 4, 5",
-            "1, 2, 3, 4, 5, 6, 7"
+    @ValueSource(strings = {
+            "1,2,3,4,5",
+            "1,2,3,4,5,6,7"
     })
     @ParameterizedTest
-    public void lottoTest_생성자_유효성에러(List<Integer> numbers) {
+    public void lottoTest_생성자_유효성에러_OutOfSizeException(String strNumbers) {
         assertThatThrownBy(() ->
-            lottoTest_생성자(numbers)
-        );
+                lottoTest_생성자(strNumbers)
+        ).isInstanceOf(OutOfSizeException.class);
     }
 }
