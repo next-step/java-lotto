3e6c4888688fe0035060c65a87270d9f7a77f59a
darkant99 <darkant99@naver.com>
2021-07-28 16:32:30 +0900

    [TEST] 테스트 코드 내용 작성

--- src/test/java/stringaddcalculator/AdditionStringTest.java
+++ src/test/java/stringaddcalculator/AdditionStringTest.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class AdditionStringTest {
     @ValueSource(strings = {
             "1,:3,4,2",     // 숫자 이외
@@ -12,35 +18,61 @@ public class AdditionStringTest {
             "1,2,3,-1,4"    // 음수
     })
     @ParameterizedTest
-    public void intValue_숫자이외_음수(String value) {
-
+    public void intValue_숫자이외_음수(String formula) {
+        assertThatThrownBy(() ->
+            new AdditionString(formula).intValue()
+        ).isInstanceOf(RuntimeException.class);
     }
 
     @Test
     public void intValue_빈문자열_Null() {
-
+        String[] formulas = new String[] {
+                " ", "       ", null
+        }
+        for (String iFormula : formulas) {
+            assertThat(
+                    new AdditionString(iFormula).intValue()
+            ).isEqualsTo(0);
+        }
     }
 
-    @ValueSource(strings = {
+    @ValueSource(ints = {
             "1",
             "12",
             "123"
     })
     @ParameterizedTest
-    public void intValue_숫자하나(String value) {
-
+    public void intValue_숫자하나(int number) {
+        assertThat(
+                new AdditionString(
+                        String.valueOf(number)
+                ).intValue()
+        ).isEqualsTo(number);
     }
 
     @CsvSource(value = {
             "1:3,4,2=10"
     }, delimiter = '=')
     @ParameterizedTest
-    public void intValue_구분자_콜론(String value, int result) {
-
+    public void intValue_구분자_콜론(String formula, int result) {
+        assertThat(
+                new AdditionString(formula).intValue()
+        ).isEqualsTo(result);
     }
 
     @Test
     public void intValue_커스텀구분자() {
+        Map<String, Integer> formulaAndResultMap = new HashMap<>();
+        formulaAndResultMap.put("//$\n1$2$3,4:5", 15);
+        formulaAndResultMap.put("//;\\n1;2;3", 6);
 
+        for (Map.Entry<String, Integer> iEntry : formulaAndResultMap.entrySet()) {
+            String iFormula = iEntry.getKey();
+            int iResult = iEntry.getValue();
+
+            assertThat(
+                    new AdditionString(iFormula).intValue()
+            ).isEqualsTo(iResult);
+        }
     }
 }
