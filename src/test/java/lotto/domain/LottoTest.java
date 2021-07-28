0d63cfd1b4c68d21bebb445826ee052ede8406be
darkant99 <darkant99@naver.com>
2021-07-29 01:12:14 +0900

    [TEST] Lotto 생성자 테스트 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoTest {
+    @CsvSource({
+            "1,10,20,30,40,45"
+    })
+    @ParameterizedTest
+    public void lottoTest_생성자(List<Integer> numbers) {
+        assertThat(
+                new Lotto(numbers)
+        ).isNotNull();
+    }
+
+    // Lotto의 size 메소드를 테스트 후 제거할것
+    @CsvSource({
+            "0, 2, 3, 4, 5, 45",
+            "1, 2, 3, 4, 5, 46",
+            "1, 2, 3, 4, 5",
+            "1, 2, 3, 4, 5, 6, 7"
+    })
+    @ParameterizedTest
+    public void lottoTest_생성자_유효성에러(List<Integer> numbers) {
+        assertThatThrownBy(() -> {
+            lottoTest_생성자(numbers);
+        })
+    }
+}
