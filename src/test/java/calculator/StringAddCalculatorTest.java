2d39c97bfbbec48eaabc2099e8a17f6c43e749af
seondongpyo <seondongpyo@gmail.com>
2021-07-28 10:43:44 +0900

    style(StringAddCalculatorTest): 오타 수정 (컴마 -> 콤마)

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -31,7 +31,7 @@ class StringAddCalculatorTest {
 		assertThat(StringAddCalculator.splitAndSum(inputText)).isEqualTo(3);
 	}
 
-	@DisplayName("구분자로 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
+	@DisplayName("구분자로 콤마(,) 이외에 콜론(:)을 사용할 수 있다.")
 	@ValueSource(strings = "1,2:3")
 	@ParameterizedTest
 	void colonAsDelimiter(String inputText) {
