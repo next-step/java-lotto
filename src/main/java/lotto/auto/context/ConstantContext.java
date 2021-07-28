c8893a6cc3aa8792ddbd5ef63063a7afd61d4771
shirohoo <hch4102@naver.com>
2021-07-28 21:50:46 +0900

    fix: 기존 코드 모두 제거

--- src/main/java/lotto/auto/context/ConstantContext.java
+++
@@ -1,8 +0,0 @@
-package lotto.auto.context;
-
-public final class ConstantContext {
-    public static final String MATCH_TABLE_KEY = "matchTable";
-    public static final String EARNINGS_RATE_KEY = "earningsRateKey";
-
-    public static final int GAME_PRICE = 1_000;
-}
--- src/main/java/lotto/auto/strategy/LottoRule.java
+++
@@ -1,7 +0,0 @@
-package lotto.auto.strategy;
-
-import lotto.auto.model.LottoGame;
-
-public interface LottoRule {
-    LottoGame ticketing();
-}
