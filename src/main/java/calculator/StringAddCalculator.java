c8be05cc6a43a230b33a286ffde75473b316c982
seondongpyo <seondongpyo@gmail.com>
2021-07-28 09:49:45 +0900

    refactor(StringAddCalculator): 기존 로직을 Numbers, Splitter 클래스를 이용하여 변경

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,15 +1,8 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringAddCalculator {
 
 	private static final int NULL_OR_EMPTY_RESULT = 0;
-	private static final int SINGLE_NUMBER = 1;
-	private static final int ZERO = 0;
-	private static final String MESSAGE_INVALID_NUMBER = "유효하지 않은 숫자입니다.";
 
 	private StringAddCalculator() {
 	}
@@ -19,38 +12,8 @@ public class StringAddCalculator {
 			return NULL_OR_EMPTY_RESULT;
 		}
 
-		if (hasSingleNumber(inputText)) {
-			return Integer.parseInt(inputText);
-		}
-
-		return sum(Splitter.split(inputText));
-	}
-
-	private static int sum(String[] numbers) {
-		if (hasInvalidNumber(numbers)) {
-			throw new RuntimeException(MESSAGE_INVALID_NUMBER);
-		}
-
-		return Arrays.stream(numbers)
-				.mapToInt(Integer::parseInt)
-				.sum();
-	}
-
-	private static int toInt(String number) {
-		try {
-			return Integer.parseInt(number);
-		} catch (NumberFormatException e) {
-			throw new RuntimeException(MESSAGE_INVALID_NUMBER);
-		}
-	}
-
-	private static boolean hasInvalidNumber(String[] numbers) {
-		return Arrays.stream(numbers)
-				.anyMatch(number -> toInt(number) < ZERO);
-	}
-
-	private static boolean hasSingleNumber(String inputText) {
-		return inputText.length() == SINGLE_NUMBER && toInt(inputText) >= ZERO;
+		Numbers numbers = Numbers.from(Splitter.split(inputText));
+		return numbers.sum();
 	}
 
 	private static boolean isBlank(String inputText) {
