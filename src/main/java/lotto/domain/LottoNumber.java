ed55a6b6089aede69549730c285e56684d2f3de0
darkant99 <darkant99@naver.com>
2021-07-29 01:17:10 +0900

    [FEAT] 프로덕션 클래스 추가

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public class LottoNumber extends Number {
+    private final int value;
+
+    public LottoNumber(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+}
