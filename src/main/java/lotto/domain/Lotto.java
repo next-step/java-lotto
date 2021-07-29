e74c15aa09ee68dc2e70329199396876a5841ff7
jung0930 <wjddlqkqh57@naver.com>
2021-07-29 13:25:07 +0900

    feat : 로또 숫자 범위 검사 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,12 +6,18 @@ import java.util.Set;
 
 public class Lotto {
 
+    private final List<Integer> numbers;
+
     private static final String IS_NULL_OR_NOT_SIX_ERROR_MESSAGE = "6개의 숫자를 등록해야 합니다.";
     private static final String DUPLICATE_NUMBER_ERROR_MESSAGE = "중복된 숫자가 있습니다.";
+    private static final String OUT_OF_BOUNDS_ERROR_MESSAGE = "1부터 45 사이의 숫자를 등록해야 합니다.";
     private static final int LIMIT_SIZE = 6;
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 45;
 
     public Lotto(List<Integer> numbers) {
         validateNumber(numbers);
+        this.numbers = numbers;
     }
 
     private void validateNumber(List<Integer> numbers) {
@@ -20,6 +26,9 @@ public class Lotto {
         }
         if(checkDuplicateNumbers(numbers)) {
             throw new IllegalArgumentException(DUPLICATE_NUMBER_ERROR_MESSAGE);
+        }
+        if(checkNumbersRange(numbers)) {
+            throw new IllegalArgumentException(OUT_OF_BOUNDS_ERROR_MESSAGE);
         }
     }
 
@@ -30,6 +39,13 @@ public class Lotto {
     private boolean checkDuplicateNumbers(List<Integer> numbers) {
         Set<Integer> numbersSet = new HashSet<>(numbers);
         return numbersSet.size() != numbers.size();
+    }
+
+    private boolean checkNumbersRange(List<Integer> numbers) {
+        long count = numbers.stream()
+                .filter(number -> number < MIN_VALUE || number > MAX_VALUE)
+                .count();
+        return count >= MIN_VALUE;
     }
 
 }
