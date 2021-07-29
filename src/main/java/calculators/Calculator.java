eae08623c26b72fcc9faa89d54032bff516b315b
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-07-29 20:41:42 +0900

    feat: 커스텀 구분자 입력받아 합 반환하기

--- src/main/java/calculators/Calculator.java
+++ src/main/java/calculators/Calculator.java
@@ -6,13 +6,25 @@ import java.util.stream.Collectors;
 
 public class Calculator {
 
+    private final String PREFIX = "//";
+    private final String SUFFIX = "\n";
+
     public Integer calculate(String input) {
         List<Integer> numbers = splitNumbers(input);
         return numbers.stream().reduce(0, Integer::sum);
     }
 
     private List<Integer> splitNumbers(String input) {
-        String[] split = input.replaceAll(":", ",").split(",");
+        String separator = ":";
+
+        if (input.startsWith(PREFIX) && input.contains(SUFFIX)) {
+            int start = input.indexOf(PREFIX);
+            int end = input.indexOf(SUFFIX);
+            separator = input.substring(start + 2, end);
+            input = input.substring(end + 1);
+        }
+        input = input.replaceAll(separator, ",");
+        String[] split = input.split(",");
         return Arrays.stream(split).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
--- src/test/java/calculators/CalculatorTest.java
+++ src/test/java/calculators/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculators;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,15 @@ class CalculatorTest {
         int sum = c.calculate(input);
 
         assertEquals(sum, 6);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 입력받아 합 반환하기")
+    void custom_separator() {
+        final String input = "//;\n1;2;3";
+        Calculator c = new Calculator();
+        int sum = c.calculate(input);
+
+        Assertions.assertEquals(sum, 6);
     }
 }
