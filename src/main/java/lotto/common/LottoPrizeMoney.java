6ba533c5d8fee567e522a5f6909631eb8cbf05f8
choibyunghyeon <choi8608@gmail.com>
2021-07-29 21:11:16 +0900

    refactor: LottoPrizeMoney 개선

--- src/main/java/lotto/common/LottoPrizeMoney.java
+++ src/main/java/lotto/common/LottoPrizeMoney.java
@@ -1,24 +1,25 @@
 package lotto.common;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public enum LottoPrizeMoney {
 
-    THREE(3, 5000L),
-    FOUR(4, 50000L),
-    FIVE(5, 1500000L),
-    SIX(6, 2000000000L);
+    THREE(3, 5_000L),
+    FOUR(4, 50_000L),
+    FIVE(5, 1_500_000L),
+    SIX(6, 2000_000_000L);
 
     private final int matchNumberCount;
     private final long prizeMoney;
 
-    private static final Map<Integer, LottoPrizeMoney> map = new HashMap<Integer, LottoPrizeMoney>() {{
-        put(3, THREE);
-        put(4, FOUR);
-        put(5, FIVE);
-        put(6, SIX);
-    }};
+    private static final Map<Integer, LottoPrizeMoney> MAP;
+
+    static {
+        MAP = Arrays.stream(LottoPrizeMoney.values())
+            .collect(Collectors.toMap(e -> e.matchNumberCount, e -> e));
+    }
 
     LottoPrizeMoney(int matchNumberCount, long prizeMoney) {
         this.matchNumberCount = matchNumberCount;
@@ -26,6 +27,6 @@ public enum LottoPrizeMoney {
     }
 
     public static long findByMatchNumberCount(int matchNumberCount) {
-        return map.get(matchNumberCount).prizeMoney;
+        return MAP.get(matchNumberCount).prizeMoney;
     }
 }
