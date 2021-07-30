809011d9594c95a6aef1d2c7552b2584b5a7f11f
YONGJU <odetsuo@gmail.com>
2021-07-30 16:04:06 +0900

    test : 지난주 당첨 번호와 비교

--- src/test/java/lottoautomatic/LottoTest.java
+++ src/test/java/lottoautomatic/LottoTest.java
@@ -3,6 +3,7 @@ package lottoautomatic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +21,23 @@ class LottoTest {
 
 		//then
 		assertThat(numbers).hasSize(6);
+	}
+
+	@Test
+	@DisplayName("지난주 당첨 번호와 비교")
+	void compare_numbers() throws Exception {
+		//given
+		Lotto lotto = new Lotto();
+		List<Integer> lastWeekNumbers = Arrays.asList(1,2,3,4,5,6);
+
+		//when
+		int matchesNumber = lotto.match(lastWeekNumbers);
+
+		//then
+		//테스트가 불가하다!
+		//몇개 맞을지 알 수 없다..
+		//전략패턴이 필요하다.
+
 	}
 
 }
