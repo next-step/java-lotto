0dd68aee88c6ad9c0189249213fd9d59ad6afdff
YONGJU <odetsuo@gmail.com>
2021-07-30 15:43:50 +0900

    refactor : 기본 숫자 배열 메소드화

--- src/main/java/lottoautomatic/LottoNumberGenerator.java
+++ src/main/java/lottoautomatic/LottoNumberGenerator.java
@@ -3,16 +3,22 @@ package lottoautomatic;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoNumberGenerator {
 
-	private static final int LIMIT_SIZE = 6;
+	private static final int NUMBERS_RANGE_START = 0;
+	private static final int NUMBERS_RANGE_END = 6;
 	private static final int MIN_LOTTO_NUMBER = 1;
 	private static final int MAX_LOTTO_NUMBER = 45;
+	private static final List<Integer> defaultNumbers = initDefaultNumbers();
 
-	private static final List<Integer> defaultNumbers = IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER).boxed().collect(Collectors.toList());
+	private static List<Integer> initDefaultNumbers() {
+		List<Integer> defaultNumbers = new ArrayList<>();
+		for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
+			defaultNumbers.add(i);
+		}
+		return defaultNumbers;
+	}
 
 	public List<Integer> generate() {
 		Collections.shuffle(defaultNumbers);
@@ -21,10 +27,11 @@ public class LottoNumberGenerator {
 
 	private List<Integer> selectNumbers() {
 		List<Integer> numbers = new ArrayList<>();
-		for (int i = 0; i < LIMIT_SIZE; i++) {
+		for (int i = NUMBERS_RANGE_START; i < NUMBERS_RANGE_END; i++) {
 			numbers.add(defaultNumbers.get(i));
 		}
 		return numbers;
 	}
+
 
 }
