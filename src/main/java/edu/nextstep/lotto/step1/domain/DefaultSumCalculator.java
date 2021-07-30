2fef5880fe2fe02e266be779d061d638eb851886
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-07-30 14:56:11 +0900

    feat: 문자를 숫자로 변환한 후 합을 구하는 기능

---
+++ src/main/java/edu/nextstep/lotto/step1/domain/DefaultSumCalculator.java
@@ -0,0 +1,29 @@
+package edu.nextstep.lotto.step1.domain;
+
+import java.util.List;
+
+public class DefaultSumCalculator implements SumCalculator {
+
+    private static final String NUMBER_FORMAT_EXCEPTION = "숫자만 입력 가능합니다.";
+
+    @Override
+    public int sum(List<String> tokens) {
+
+        return tokens.stream()
+                .mapToInt(token -> {
+                    int number;
+
+                    try {
+                        number =  Integer.parseInt(token);
+                    } catch (NumberFormatException e) {
+                        throw new RuntimeException(NUMBER_FORMAT_EXCEPTION);
+                    }
+
+                    if (number < 0) {
+                        throw new RuntimeException(NUMBER_FORMAT_EXCEPTION);
+                    }
+
+                    return number;
+                }).sum();
+    }
+}
---
+++ src/main/java/edu/nextstep/lotto/step1/domain/SumCalculator.java
@@ -0,0 +1,8 @@
+package edu.nextstep.lotto.step1.domain;
+
+import java.util.List;
+
+public interface SumCalculator {
+
+    int sum(List<String> tokens);
+}
