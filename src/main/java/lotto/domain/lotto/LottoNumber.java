99fc1ce932f91e2d1a6870d5c91e8016407f566c
choibyunghyeon <choi8608@gmail.com>
2021-07-30 18:05:56 +0900

    feat: 로또 숫자 구현

---
+++ src/main/java/lotto/domain/lotto/LottoNumber.java
@@ -0,0 +1,24 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+import java.util.Random;
+
+public class LottoNumber {
+
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+
+    private final static Random RANDOM = new Random();
+
+    public static boolean isValid(List<Integer> values) {
+        return values.stream().allMatch(LottoNumber::isValid);
+    }
+
+    public static boolean isValid(int value) {
+        return (value >= LOTTO_NUMBER_MIN && value <= LOTTO_NUMBER_MAX);
+    }
+
+    public static int create() {
+        return RANDOM.nextInt(LOTTO_NUMBER_MAX - LOTTO_NUMBER_MIN + 1) + LOTTO_NUMBER_MIN;
+    }
+}
--- src/main/java/lotto/domain/lotto/exception/InvalidLottoNumberException.java
+++ src/main/java/lotto/domain/lotto/exception/InvalidLottoNumberException.java
@@ -2,7 +2,7 @@ package lotto.domain.lotto.exception;
 
 public class InvalidLottoNumberException extends RuntimeException {
 
-    private static final String MESSAGE = "유효하지 않은 로또 번호들 입니다.";
+    private static final String MESSAGE = "유효하지 않은 로또 번호입니다.";
 
     public InvalidLottoNumberException() {
         super(MESSAGE);
