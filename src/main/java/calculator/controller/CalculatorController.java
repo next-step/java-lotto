27706d820dbbb1718748839532ce7e4ba47f9c84
PIH6585 <pih6585@naver.com>
2021-07-30 16:36:12 +0900

    feat : output view add

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,12 +1,19 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
 import calculator.view.CalculatorInputView;
+import calculator.view.CalculatorOutputView;
 
 public class CalculatorController {
 
-	public static void main(String[] args){
-		//CalculatorInputView calculatorInputView = new CalculatorInputView();
-		//String inputValue = calculatorInputView.requestView();
+	public static void main(String[] args) {
+		CalculatorInputView calculatorInputView = new CalculatorInputView();
+		String inputValue = calculatorInputView.requestView();
+
+		Calculator calculator = Calculator.createCalculator(inputValue);
+
+		CalculatorOutputView calculatorOutputView = new CalculatorOutputView();
+		calculatorOutputView.calculateResultView(calculator.calculate());
 
 	}
 }
