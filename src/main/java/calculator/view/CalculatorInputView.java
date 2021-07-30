406f84e8eddfbd57b20b709dbd05977612495c98
PIH6585 <pih6585@naver.com>
2021-07-30 14:26:17 +0900

    feat : input view 추가

---
+++ src/main/java/calculator/view/CalculatorInputView.java
@@ -0,0 +1,18 @@
+package calculator.view;
+
+import java.util.Scanner;
+
+public class CalculatorInputView {
+
+	private static final String REQUEST_MESSAGE = "문자열을 입력해주세요.";
+	private final Scanner scanner;
+
+	public CalculatorInputView() {
+		scanner = new Scanner(System.in);
+	}
+
+	public String requestView(){
+		System.out.println(REQUEST_MESSAGE);
+		return scanner.next();
+	}
+}
