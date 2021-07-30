f8f14011592cf605087943d93af4d9bcaa376ac5
YONGJU <odetsuo@gmail.com>
2021-07-30 15:47:15 +0900

    test : 중복 번호 테스트

--- src/test/java/lottoautomatic/LottoNumberGeneratorTest.java
+++ src/test/java/lottoautomatic/LottoNumberGeneratorTest.java
@@ -3,7 +3,9 @@ package lottoautomatic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +25,18 @@ class LottoNumberGeneratorTest {
 
 	}
 
+	@Test
+	@DisplayName("로또 번호 6개 중복 확인")
+	void lotto_numbers_overlap() throws Exception {
+		//given
+		LottoNumberGenerator generator = new LottoNumberGenerator();
 
+		//when
+		List<Integer> numbers = generator.generate();
+		Set<Integer> overlaps = new HashSet<>(numbers);
+
+		//then
+		assertThat(overlaps).hasSize(numbers.size());
+	}
 
 }
