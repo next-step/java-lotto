60c9787f7f289512802ba386a0fc221f94faca7f
PIH6585 <pih6585@naver.com>
2021-07-30 18:21:53 +0900

    refactor : 일급컬렉션 적용에 따른 로직분리 test : 일급컬렉션 적용에 따른 test

--- src/main/java/calculator/model/Calculation.java
+++ src/main/java/calculator/model/Calculation.java
@@ -1,46 +1,23 @@
 package calculator.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-import calculator.utils.ErrorMessage;
-import calculator.utils.Validation;
 
 public class Calculation {
 
-	public int calculate(String regexp, String calculatorValue) {
-		String[] result = stringToSplit(calculatorValue, regexp);
-		List<String> characters = StringArrayToList(result);
-		return plusCalculate(characters);
+	private final Characters characters;
+
+	public Calculation(String[] inputValue) {
+		characters = new Characters(inputValue);
 	}
 
-	public String[] stringToSplit(String textValue, String regexp) {
-		return textValue.split(regexp);
+	public Calculation(Characters characters) {
+		this.characters = characters;
 	}
 
-	public List<String> StringArrayToList(String[] result) {
-		List<String> characters = new ArrayList<>();
-		Collections.addAll(characters, result);
-		return characters;
+	public int calculate() {
+		return characters.sumCharacters();
 	}
 
-	public int plusCalculate(List<String> characters) {
-		int result = 0;
-		for (String character : characters) {
-			Validation.validNumberTypeCheck(character, ErrorMessage.NUMBER_TYPE_ERROR_MESSAGE);
-			result = calculateReturnValue(result, character);
-		}
-		return result;
-	}
 
-	public int calculateReturnValue(int calculateResult, String character) {
-		return calculateResult + toInt(character);
-	}
-
-	public int toInt(String character) {
-		Validation.validPositiveNumber(Integer.parseInt(character), ErrorMessage.NUMBER_POSITIVE_ERROR_MESSAGE);
-		return Integer.parseInt(character);
-	}
 
 }
