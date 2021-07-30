f266615e02474953ab40ea975d1b2dc470a645cd
syd9538 <syd9538@gmail.com>
2021-07-30 21:35:23 +0900

    feat :null 이나 빈 문자열 시 0 반환 TDD 및 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,5 +2,11 @@ package calculator;
 
 public class StringAddCalculator {
 
-
+    public int calculate(String input) {
+        int result = 0;
+        if (input.isEmpty() || input == null) {
+            return 0;
+        }
+        return result;
+    }
 }
--- src/test/java/calculator/StringAddCalculatorTests.java
+++ src/test/java/calculator/StringAddCalculatorTests.java
@@ -1,6 +1,22 @@
 package calculator;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringAddCalculatorTests {
 
+    @DisplayName("null 이나 빈 문자열 시 0 반환 테스트")
+    @ParameterizedTest
+    @EmptySource
+    void blankOrNullTest(String input){
+        StringAddCalculator stringAddCalculator = new StringAddCalculator();
+
+        int result = stringAddCalculator.calculate(input);
+
+        assertThat(result).isEqualTo(0);
+    }
 }
