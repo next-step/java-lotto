47779bb160c25673a14d606ac4b723d7897c22b1
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-07-30 15:06:54 +0900

    fix: 오류 메시지 수정

--- src/main/java/edu/nextstep/lotto/step1/domain/DefaultSumCalculator.java
+++ src/main/java/edu/nextstep/lotto/step1/domain/DefaultSumCalculator.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class DefaultSumCalculator implements SumCalculator {
 
     private static final String NUMBER_FORMAT_EXCEPTION = "숫자만 입력 가능합니다.";
+    private static final String CAN_NEGATIVE_EXCEPTION = "음수는 사용할 수 없습니다.";
 
     @Override
     public int sum(List<String> tokens) {
@@ -20,7 +21,7 @@ public class DefaultSumCalculator implements SumCalculator {
                     }
 
                     if (number < 0) {
-                        throw new RuntimeException(NUMBER_FORMAT_EXCEPTION);
+                        throw new RuntimeException(CAN_NEGATIVE_EXCEPTION);
                     }
 
                     return number;
