7a122d75b6d08a6ada9baf6917a817b0c2b63cc1
PIH6585 <pih6585@naver.com>
2021-07-30 14:26:32 +0900

    feat : controller init 추가

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,12 @@
+package calculator.controller;
+
+import calculator.view.CalculatorInputView;
+
+public class CalculatorController {
+
+	public static void main(String[] args){
+		//CalculatorInputView calculatorInputView = new CalculatorInputView();
+		//String inputValue = calculatorInputView.requestView();
+
+	}
+}
