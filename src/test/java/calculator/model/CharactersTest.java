d648c58f46a9b0f38488b5fcce916209b23419cb
PIH6585 <pih6585@naver.com>
2021-07-31 16:31:09 +0900

    test : 일급 컬렉션 및 불변객체에 따른 수정에 따른 테스트

--- src/test/java/calculator/model/CharactersTest.java
+++ src/test/java/calculator/model/CharactersTest.java
@@ -1,23 +1,31 @@
 package calculator.model;
 
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CharactersTest {
 
 	@Test
-	@DisplayName("문자열의 집합들에 대하여 합산을 반환한다.")
+	@DisplayName("문자열입력시 정보를 저장후 그 값만 반환한다.")
 	public void sumValues(){
 		Character character1 = new Character(1);
 		Character character2 = new Character(2);
 		Character character3 = new Character(3);
+		List<Character> initCharacters = new ArrayList<>();
+		initCharacters.add(character1);
+		initCharacters.add(character2);
+		initCharacters.add(character3);
 
-		Characters characters = new Characters(character1,character2,character3);
+		Characters characters = new Characters(initCharacters);
 
-		int result = characters.sumCharacters();
+		List<Character> result = characters.getCharacters();
 
-		assertThat(result).isEqualTo(6);
+		assertThat(result).extracting("character").containsExactly(1,2,3);
 	}
 
 }
