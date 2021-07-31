7bfd235957e9b991fef140e12e5dfa8cf58a0666
PIH6585 <pih6585@naver.com>
2021-07-31 16:31:52 +0900

    refactor : calculation class 와 통합 및 일급컬레션에 따른 수정 test : calculation class 와 통합 및 일급컬레션에 따른 테스트

--- src/main/java/calculator/model/Calculator.java
+++ src/main/java/calculator/model/Calculator.java
@@ -1,5 +1,7 @@
 package calculator.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,33 +11,39 @@ import calculator.utils.Validation;
 public class Calculator {
 
 	private static final String PATTERN_REG_EXP = "^//(.)\n(.*)$";
-	private static final String DEFAULT_REG_EXP = ",|:";
+	private static final String DEFAULT_REG_EXP = "[,:]";
 
-	private final Calculation calculation;
+	private final Characters characters;
 
-	private Calculator(String calculatorValue) {
-		calculation = new Calculation(patternResult(PATTERN_REG_EXP,calculatorValue));
+	public Calculator(String calculatorValue) {
+		Validation.validStringEmptyCheck(calculatorValue, ErrorMessage.EMPTY_ERROR_MESSAGE);
+		characters = new Characters(patternResult(PATTERN_REG_EXP, calculatorValue));
 	}
 
-	public String[] patternResult(String regExp, String calculatorValue) {
+	public List<Character> patternResult(String regExp, String calculatorValue) {
 		Matcher matcher = Pattern.compile(regExp).matcher(calculatorValue);
 		if (matcher.find()) {
-			return stringToSplit(matcher.group(1),matcher.group(2));
+			return initCharacters(matcher.group(2).split(matcher.group(1)));
 		}
-		return stringToSplit(DEFAULT_REG_EXP,calculatorValue);
-	}
-
-	public static Calculator createCalculator(String calculatorValue) {
-		Validation.validStringEmptyCheck(calculatorValue, ErrorMessage.EMPTY_ERROR_MESSAGE);
-		return new Calculator(calculatorValue);
+		return initCharacters(calculatorValue.split(DEFAULT_REG_EXP));
 	}
 
 	public int calculate() {
-		return calculation.calculate();
+		return characters.getCharacters().stream().mapToInt(Character::getCharacter).sum();
 	}
 
-	public String[] stringToSplit(String regexp, String textValue) {
-		return textValue.split(regexp);
+	public List<Character> initCharacters(String[] characterArray) {
+		List<Character> initCharacters = new ArrayList<>();
+		for (String text : characterArray) {
+			Character character = new Character(toInt(text));
+			initCharacters.add(character);
+		}
+		return initCharacters;
+	}
+
+	public int toInt(String character) {
+		Validation.validNumberTypeCheck(character, ErrorMessage.NUMBER_TYPE_ERROR_MESSAGE);
+		return Integer.parseInt(character);
 	}
 
 }
--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -3,6 +3,8 @@ package calculator.model;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,43 +15,33 @@ class CalculatorTest {
 	@Test
 	@DisplayName("계산기의 문자열이 빈값이면 예외가 발생된다.")
 	public void validStringEmptyCheck() {
-		assertThrows(IllegalArgumentException.class, () -> Calculator.createCalculator("")
+		assertThrows(IllegalArgumentException.class, () -> new Calculator("")
 		);
 	}
 
 	@Test
 	@DisplayName("문자열을 입력하는 패턴에 따라 분리한다.")
-	public void stringToSplit() {
-		Calculator calculator = Calculator.createCalculator("11");
-
-		String[] result = calculator.stringToSplit(",", "1,2,3");
-
-		assertThat(result).containsExactly("1", "2", "3");
-	}
-
-	@Test
-	@DisplayName("문자열을 입력하는 패턴에 따라 분리한다.")
 	public void patternResult() {
-		Calculator calculator = Calculator.createCalculator("1:2:3");
+		Calculator calculator = new Calculator("1:2:3");
 
-		String[] result = calculator.patternResult(PATTERN_REG_EXP, "1,2,3");
+		List<Character> result = calculator.patternResult(PATTERN_REG_EXP, "1,2,3");
 
-		assertThat(result).containsExactly("1", "2", "3");
+		assertThat(result).extracting("character").containsExactly(1,2,3);
 
-		String[] result2 = calculator.patternResult(PATTERN_REG_EXP, "//,\n1,2,3");
+		List<Character> result2 = calculator.patternResult(PATTERN_REG_EXP, "//,\n1,2,3");
 
-		assertThat(result2).containsExactly("1", "2", "3");
+		assertThat(result2).extracting("character").containsExactly(1,2,3);
 	}
 
 	@Test
 	@DisplayName("계산기의 문자열에 따라 덧셈 계산기에 의하여 계산된 값이 반환된다.")
 	public void plusCalculator() {
-		Calculator calculator = Calculator.createCalculator("//:\n1:2:3:4");
+		Calculator calculator = new Calculator("//:\n1:2:3:4");
 		int result1 = calculator.calculate();
 
 		assertThat(result1).isEqualTo(10);
 
-		Calculator calculator2 = Calculator.createCalculator("1:2:3,4,5");
+		Calculator calculator2 = new Calculator("1:2:3,4,5");
 		int result2 = calculator2.calculate();
 
 		assertThat(result2).isEqualTo(15);
