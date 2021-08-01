2108f15de597a80d6d451ee5ad193929d5d303ec
seondongpyo <seondongpyo@gmail.com>
2021-08-01 22:39:53 +0900

    feat: 수동으로 구매하는 로또 매수만큼 로또 번호를 입력 받는 로직 추가

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 import lotto.domain.LottoMachine;
 import lotto.domain.LottoNumbers;
 import lotto.domain.LottoPrizes;
@@ -13,6 +15,8 @@ public class LottoMain {
 	public static void main(String[] args) {
 		int money = InputView.inputMoney();
 		int manualLottoTicketsCount = InputView.inputManualLottoTicketsCount();
+		List<LottoNumbers> manualLottoTicketNumbers = InputView.inputManualLottoTicketNumbers(manualLottoTicketsCount);
+
 		LottoMachine lottoMachine = new LottoMachine();
 		LottoTickets lottoTickets = lottoMachine.issueLottoTickets(money);
 		ResultView.showLottoTickets(lottoTickets);
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,6 +14,7 @@ public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
 	private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
 	private static final String INPUT_NUMBER_OF_MANUAL_LOTTO_TICKETS = "수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String INPUT_MANUAL_LOTTO_TICKET_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
 	private static final String INPUT_WINNING_NUMBERS = System.lineSeparator() + "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 	private static final String DELIMITER = ",";
@@ -32,6 +33,13 @@ public class InputView {
 	public static int inputManualLottoTicketsCount() {
 		System.out.println(INPUT_NUMBER_OF_MANUAL_LOTTO_TICKETS);
 		return SCANNER.nextInt();
+	}
+
+	public static List<LottoNumbers> inputManualLottoTicketNumbers(int manualLottoTicketsCount) {
+		System.out.println(INPUT_MANUAL_LOTTO_TICKET_NUMBERS);
+		return IntStream.range(INDEX_OF_FIRST_NUMBER, manualLottoTicketsCount)
+				.mapToObj(index -> LottoNumbers.from(lottoTicketNumbersFromUser()))
+				.collect(toList());
 	}
 
 	public static LottoNumbers inputWinningNumbers() {
