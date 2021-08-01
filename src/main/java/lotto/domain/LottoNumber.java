855b523114cc81bec0e706ff4a6bfaf3ca8b214a
syd9538 <syd9538@gmail.com>
2021-08-01 22:45:09 +0900

    style : 상수 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -9,19 +9,23 @@ public class LottoNumber {
     private List<Integer> shuffledNumbers;
     private List<Integer> pickedNumbers;
 
+    private static final int LOTTO_TOP_BOUND = 45;
+    private static final int LOTTO_BOTTOM_BOUND = 1;
+    private static final int COUNT_OF_PICK = 6;
+
     public LottoNumber() {
         shuffled();
         picked();
     }
 
     private void picked() {
-        pickedNumbers = shuffledNumbers.subList(0, 6);
+        pickedNumbers = shuffledNumbers.subList(0, COUNT_OF_PICK);
         Collections.sort(pickedNumbers);
     }
 
     private void shuffled() {
         shuffledNumbers = new ArrayList<>();
-        for (int i = 1; i <= 45; i++) {
+        for (int i = LOTTO_BOTTOM_BOUND; i <= LOTTO_TOP_BOUND; i++) {
             shuffledNumbers.add(i);
         }
 
