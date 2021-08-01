5ccde7e83678690b2c5ab85eefc00289f62674a0
SeungJae Lee <lsj8367@naver.com>
2021-08-01 15:40:05 +0900

    test: 수동 생성 테스트

--- src/test/java/lotto/LottoMakerTest.java
+++ src/test/java/lotto/LottoMakerTest.java
@@ -5,6 +5,10 @@ import lotto.domain.LottoMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoMakerTest {
@@ -13,6 +17,14 @@ class LottoMakerTest {
     @DisplayName("로또 생성 테스트")
     void makeLottoTest() {
         assertThat(LottoMaker.run()).isInstanceOf(Lotto.class);
+    }
+
+    @Test
+    @DisplayName("수동 생성 테스트")
+    void makeManualLottoTest() {
+        Set<Integer> lotto = new HashSet<>(Arrays.asList(1,2,3,4,5,6));
+
+        assertThat(LottoMaker.manual(lotto)).isInstanceOf(Lotto.class);
     }
 
 }
