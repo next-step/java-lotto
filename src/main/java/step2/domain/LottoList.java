902d51ea1048975018c6a471d5ea07797f68dd06
Hae-gun <newchk610@gmail.com>
2021-08-01 15:54:12 +0900

    feat: Create 로또 리스트 테스트 및 객체 생성

--- src/main/java/step2/domain/LottoList.java
+++ src/main/java/step2/domain/LottoList.java
@@ -3,7 +3,25 @@ package step2.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lottos {
-    List<Lotto> lottoList = new ArrayList<>();
+public class LottoList {
+    private List<Lotto> lottoList = new ArrayList<>();
 
+    public static LottoList create() {
+        return new LottoList();
+    }
+
+    public int getSize() {
+        return lottoList.size();
+    }
+
+    public void buyLotto(LottoMachine machine) {
+        this.lottoList.add(Lotto.from(machine.makeLotto()));
+    }
+
+    public Lotto getLotto(int idx) {
+        if(idx > lottoList.size()){
+            throw new IllegalArgumentException(String.format("입력한 인덱스(%d) 가 리스트 사이즈를 초과하엿습니다.",idx));
+        }
+        return this.lottoList.get(idx);
+    }
 }
