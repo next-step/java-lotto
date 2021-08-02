af4db06356b06d12d277748919e37f1751b65e26
shryu <>
2021-08-02 17:43:43 +0900

    refactor : 반보문 stream으로 변환

--- src/main/java/lotto/application/CreateShffledLottoNumbers.java
+++ src/main/java/lotto/application/CreateShffledLottoNumbers.java
@@ -3,22 +3,19 @@ package lotto.application;
 import lotto.util.LottoNumberRange;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreateShffledLottoNumbers implements CreateLottoNumbersStrategy {
     private final int LOTTO_NUMBER_MAX_COUNT = 6;
 
     @Override
     public List<Integer> getNumbersList() {
-        List<Integer> lottoNumbers = new LinkedList<>();
         Collections.shuffle(LottoNumberRange.getLottoNumberRange());
-        int lottoRangeListIndex = 0;
-        while (lottoNumbers.size()<LOTTO_NUMBER_MAX_COUNT) {
-            lottoNumbers.add(LottoNumberRange.getLottoNumberRange().get(lottoRangeListIndex));
-            lottoRangeListIndex++;
-        }
-        Collections.sort(lottoNumbers);
-        return lottoNumbers;
+        return LottoNumberRange.getLottoNumberRange()
+                .stream()
+                .limit(LOTTO_NUMBER_MAX_COUNT)
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
