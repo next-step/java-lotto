8f04d7e0304a13842f41926a3eb026b74b7b57eb
YONGJU <odetsuo@gmail.com>
2021-08-02 23:58:15 +0900

    refactor : 로또 결과 클래스 분리

--- src/main/java/lottoautomatic/domain/Lottos.java
+++ src/main/java/lottoautomatic/domain/Lottos.java
@@ -1,13 +1,11 @@
 package lottoautomatic.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Lottos {
 
 	private static final int AMOUNT_UNIT = 1000;
+
 	private final List<Lotto> lottoList = new ArrayList<>();
 
 	public void buy(int amount, LottoNumberGenerator generator) {
@@ -27,27 +25,12 @@ public class Lottos {
 		return lottoList.size();
 	}
 
-	public Map<Integer, Integer> match(List<Integer> winningNumbers) {
-		Map<Integer, Integer> matchingResult = new HashMap<>();
-		matchingResult.put(3, 0);
-		matchingResult.put(4, 0);
-		matchingResult.put(5, 0);
-		matchingResult.put(6, 0);
-
-		putIntoResult(winningNumbers, matchingResult);
-
-		return matchingResult;
+	public List<Lotto> toList() {
+		return Collections.unmodifiableList(lottoList);
 	}
 
-	private void putIntoResult(List<Integer> winningNumbers, Map<Integer, Integer> matchingResult) {
-		for (Lotto lotto : lottoList) {
-			int matchingKey = lotto.matchingCount(winningNumbers);
-			matchingResult.put(matchingKey, getMatchingCount(matchingResult, matchingKey));
-		}
-	}
-
-	private int getMatchingCount(Map<Integer, Integer> resultMap, int match) {
-		return resultMap.containsKey(match) ? resultMap.get(match) + 1 : 1;
+	public int totalAmount() {
+		return AMOUNT_UNIT * size();
 	}
 
 }
