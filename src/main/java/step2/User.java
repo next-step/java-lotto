ca115bea42d46a876f0908f3f6d3f927c68ce21e
sk1737030 <sk1737030@naver.com>
2021-08-02 20:10:03 +0900

    feat: 사용자가 지갑을 가질 수 있는 기능

--- src/main/java/step2/User.java
+++ src/main/java/step2/User.java
@@ -5,9 +5,11 @@ import java.util.List;
 public class User {
 
     private final String name;
+    private final Wallet wallet;
 
-    public User(String name) {
+    public User(String name, Wallet wallet) {
         this.name = name;
+        this.wallet = wallet;
     }
 
     public List<Lotto> buyLotto(Wallet givenWallet) {
