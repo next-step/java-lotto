92eb9b03e57c5188ab316c156b25cfdcb5fcb800
ckdgus08 <ckdgus0808@naver.com>
2021-08-02 16:01:53 +0900

    feat: 지난 주 당첨번호 지정

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 public class Game {
     private final List<Lotto> lottoList;
+    private Lotto PreviousLotto;
 
     public Game(List<Lotto> lottoList) {
         this.lottoList = lottoList;
+    }
+
+    public void setPreviousLotto(Lotto previousLotto) {
+        this.PreviousLotto = previousLotto;
     }
 
     public List<Lotto> getLottoList() {
--- src/test/java/lotto/domain/GameTest.java
+++ src/test/java/lotto/domain/GameTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import org.assertj.core.util.Sets;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
 
-    @Test
-    @DisplayName("게임 생성")
-    void create() {
+    Game game;
 
+    @BeforeEach
+    void setUp() {
         Set<Ball> ballSet = Sets.newLinkedHashSet(new Ball(1),
                 new Ball(2),
                 new Ball(3),
@@ -24,8 +25,12 @@ public class GameTest {
                 new Ball(6));
 
         List<Lotto> lottoList = Collections.singletonList(new Lotto(ballSet));
-        Game game = new Game(lottoList);
+        game = new Game(lottoList);
+    }
 
+    @Test
+    @DisplayName("게임 생성")
+    void create() {
         assertThat(game.getLottoSize()).isEqualTo(1);
     }
 
@@ -35,5 +40,20 @@ public class GameTest {
         List<Lotto> lottoList = LottoMachine.buyRandomLotto(5000);
         Game game = new Game(lottoList);
         assertThat(game.getLottoSize()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("지난 당첨번호 지정하기")
+    void settingPreviousLotto() {
+        game.setPreviousLotto(new Lotto(
+                Sets.newLinkedHashSet(new Ball(1),
+                        new Ball(2),
+                        new Ball(3),
+                        new Ball(4),
+                        new Ball(5),
+                        new Ball(6))));
+
+        assertThat(game.getLottoSize()).isEqualTo(1);
+        assertThat(game.getLottoList().get(0).toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 }
