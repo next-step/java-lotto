4c3b3b6b8ff21f0920a5873b0f510cf2addfd43d
ckdgus08 <ckdgus0808@naver.com>
2021-08-02 15:18:36 +0900

    feat: 로또 생성, valid

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,34 @@
 package lotto.domain;
 
+import java.util.Objects;
+import java.util.Set;
+
 public class Lotto {
+
+    public static final int NUMBER_SIZE = 6;
+    public static final String VALID_NUMBER_COUNT = "로또 번호 갯수는 " + NUMBER_SIZE + "개가 되어야합니다. (중복된 숫자도 오면 안됩니다.)";
+    private final Set<Ball> balls;
+
+    public Lotto(Set<Ball> balls) {
+        validNumberCountSize(balls);
+        this.balls = balls;
+    }
+
+    private void validNumberCountSize(Set<Ball> balls) {
+        if (balls.size() != NUMBER_SIZE)
+            throw new IllegalArgumentException(VALID_NUMBER_COUNT);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lotto)) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(balls, lotto.balls);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(balls);
+    }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,55 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class LottoTest {
+
+    Set<Ball> ballSet = new HashSet<>();
+
+    @BeforeEach
+    void setUp() {
+        for (int i = 1; i <= 6; i++) {
+            ballSet.add(new Ball(i));
+        }
+    }
+
+    @Test
+    @DisplayName("로또 생성")
+    void create() {
+        Lotto lotto = new Lotto(ballSet);
+        assertThat(lotto).isEqualTo(new Lotto(ballSet));
+    }
+
+    @Test
+    @DisplayName("로또의 숫자 갯수가 6개가 아니면 에러가 발생한다.")
+    void validNumberSize() {
+        ballSet.add(new Ball(7));
+
+        assertThatThrownBy(() -> new Lotto(ballSet))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        ballSet.remove(new Ball(7));
+        ballSet.remove(new Ball(6));
+
+        assertThatThrownBy(() -> new Lotto(ballSet))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또는 중복된 숫자가 포함되면 에러가 발생한다")
+    void validDuplicatedNumber() {
+        ballSet.remove(new Ball(6));
+        ballSet.add(new Ball(5));
+
+        assertThatThrownBy(() -> new Lotto(ballSet))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
