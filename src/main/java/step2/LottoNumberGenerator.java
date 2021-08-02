e0b87d1874706d360cd2e0555378fe7941083976
sk1737030 <sk1737030@naver.com>
2021-08-02 18:49:28 +0900

    refactor: private 생성자 추가

--- src/main/java/step2/LottoNumberGenerator.java
+++ src/main/java/step2/LottoNumberGenerator.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class LottoNumberGenerator {
 
+    private LottoNumberGenerator() {
+    }
+
     private static final List<Integer> lottoNumberList = new ArrayList<>();
 
     public static List<Integer> create() {
