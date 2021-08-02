476076cdc2e3009aaafa79f48f1a39a5bd53f8ff
ckdgus08 <ckdgus0808@naver.com>
2021-08-02 15:00:39 +0900

    feat: 공 생성, valid

--- src/main/java/lotto/domain/Ball.java
+++ src/main/java/lotto/domain/Ball.java
@@ -1,4 +1,31 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Ball {
+
+    private final int number;
+
+    public Ball(int number) {
+        validNumberRange(number);
+        this.number = number;
+    }
+
+    private void validNumberRange(int number) {
+        if (number <= 0 || number >= 46)
+            throw new IllegalArgumentException("공 숫자는 1~45 만 올 수 있습니다.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ball)) return false;
+        Ball ball = (Ball) o;
+        return number == ball.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
