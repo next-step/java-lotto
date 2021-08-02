91f0a6dbdc96e7efd6a26c81561c5bfb3c480fc0
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-08-03 03:28:15 +0900

    test: LottoNumber 테스트 추가

---
+++ src/test/java/edu/nextstep/lotto/step2/vo/LottoNumberTest.java
@@ -0,0 +1,16 @@
+package edu.nextstep.lotto.step2.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoNumberTest {
+
+    @DisplayName("로또 번호 생성 테스트")
+    @Test
+    void create() {
+        LottoNumber lottoNumber = new LottoNumber();
+        assertThat(lottoNumber.getLottoNumbers().size()).isEqualTo(6);
+    }
+}
