2b3cacf4d94e32010730c745cf246f5f7668cb4b
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-08-03 03:29:09 +0900

    feat: 로또 기능 구현

--- src/main/java/edu/nextstep/lotto/step2/App.java
+++ src/main/java/edu/nextstep/lotto/step2/App.java
@@ -1,4 +1,26 @@
 package edu.nextstep.lotto.step2;
 
+import edu.nextstep.lotto.step2.domain.LottoList;
+import edu.nextstep.lotto.step2.domain.WinningList;
+import edu.nextstep.lotto.step2.view.InputView;
+import edu.nextstep.lotto.step2.view.ResultView;
+
 public class App {
+
+    private static final InputView inputView = new InputView();
+    private static final ResultView resultView = new ResultView();
+
+    public static void main(String[] args) {
+        int purchaseAmount = inputView.getPurchaseAmount();
+
+        LottoList lottoList = new LottoList(purchaseAmount);
+
+        resultView.outputNumberOfPurchase(lottoList);
+        resultView.outputLottoList(lottoList);
+
+        WinningList winningList = new WinningList(inputView.getWinningNumber());
+
+        resultView.outputStatistics(lottoList, winningList);
+        resultView.outputRate(lottoList, winningList, purchaseAmount);
+    }
 }
