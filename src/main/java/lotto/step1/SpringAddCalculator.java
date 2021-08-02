3d27b22e82b997004e294d43aaecce217349a617
gunny6026 <gunny6026@naver.com>
2021-08-02 16:58:58 +0900

    feat: 계산기 클래스 생성

---
+++ src/main/java/lotto/step1/SpringAddCalculator.java
@@ -0,0 +1,14 @@
+package lotto.step1;
+
+public class SpringAddCalculator {
+
+    private String value;
+
+    public SpringAddCalculator(String value) {
+        this.value = value;
+    }
+
+    public int splitAndSum(String text) {
+        return 0;
+    }
+}
