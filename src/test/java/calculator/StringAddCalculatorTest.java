84a7c3a5cd4c7b61528c3af523ea806963e6ac4a
0n1dev <all_mind@naver.com>
2021-08-03 00:40:59 +0900

    fix : 테스트 케이스 수정
    - CsvSource 에서 개행처리 안되는 이슈 수정

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -4,26 +4,32 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class StringAddCalculatorTest {
 
+	StringAddCalculator stringAddCalculator;
+
 	@ParameterizedTest
 	@CsvSource(value = {"1,2,3=6", ",=0", "1;2,3=6"}, delimiter = '=')
 	@DisplayName("구분자를 통해 분리하여 덧셈")
 	void splitAndAddStringSuccess(String input, int expected) {
-		int result = StringAddCalculator.splitAndAdd(input);
+		stringAddCalculator = new StringAddCalculator(input);
+
+		int result = stringAddCalculator.splitAndAdd();
 
 		assertThat(result).isEqualTo(expected);
 	}
 
-	@ParameterizedTest
-	@CsvSource(value = {"//+\n1+2,3=6", "//@\n1,2@3=6"}, delimiter = '=')
+	@Test
 	@DisplayName("커스텀 구분자를 추가 후 분리하여 덧셈")
-	void addCustomSeparatorSuccess(String input, int expected) {
-		int result = StringAddCalculator.splitAndAdd(input);
+	void addCustomSeparatorSuccess() {
+		stringAddCalculator = new StringAddCalculator("//+\n1+2,3");
 
-		assertThat(result).isEqualTo(expected);
+		int result = stringAddCalculator.splitAndAdd();
+
+		assertThat(result).isEqualTo(6);
 	}
 }
