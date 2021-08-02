364b75daa7be64eb07fd24727e206bf8312bdeaa
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-08-03 03:24:00 +0900

    feat: InputView, ResultView 기능 구현

--- src/main/java/edu/nextstep/lotto/step2/view/InputView.java
+++ src/main/java/edu/nextstep/lotto/step2/view/InputView.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String QUESTION_PURCHASE_AMOUNT = "구입 금액을 입력해주세요.";
-    private static final String QUESTION_WINNING_NUMBER = "지난 주 당첨 번호를 입력해주세요.";
+    private static final String QUESTION_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해주세요.";
     private final Scanner scanner = new Scanner(System.in);
+
+    public int getPurchaseAmount() {
+        System.out.println(QUESTION_PURCHASE_AMOUNT);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public String getWinningNumber() {
+        System.out.println(QUESTION_WINNING_NUMBERS);
+        return scanner.nextLine();
+    }
 }
--- src/main/java/edu/nextstep/lotto/step2/view/ResultView.java
+++ src/main/java/edu/nextstep/lotto/step2/view/ResultView.java
@@ -1,4 +1,36 @@
 package edu.nextstep.lotto.step2.view;
 
+import edu.nextstep.lotto.step2.domain.LottoList;
+import edu.nextstep.lotto.step2.domain.WinningAmount;
+import edu.nextstep.lotto.step2.domain.WinningList;
+
 public class ResultView {
+
+    private static final String NEW_LINE = "\n";
+
+    public void outputNumberOfPurchase(LottoList lottoList) {
+        System.out.printf("%s 개를 구매했습니다.%n", lottoList.getLottoList().size());
+    }
+
+    public void outputLottoList(LottoList lottoList) {
+        lottoList.getLottoList().forEach(lotto -> System.out.println(lotto.getLottoNumbers()));
+        System.out.print(NEW_LINE);
+    }
+
+    public void outputStatistics(LottoList lottoList, WinningList winningList) {
+        System.out.printf("3개 일치 (5000원)- %s개%n", lottoList.getMatchResult(winningList, WinningAmount.FOURTH.getCount()));
+        System.out.printf("4개 일치 (50000원)- %s개%n", lottoList.getMatchResult(winningList, WinningAmount.THIRD.getCount()));
+        System.out.printf("5개 일치 (1500000원)- %s개%n", lottoList.getMatchResult(winningList, WinningAmount.SECOND.getCount()));
+        System.out.printf("6개 일치 (2000000000원)- %s개%n", lottoList.getMatchResult(winningList, WinningAmount.FIRST.getCount()));
+    }
+
+    public void outputRate(LottoList lottoList, WinningList winningList, int purchaseAmount) {
+        float rate = lottoList.getRate(winningList, purchaseAmount);
+
+        System.out.printf("총 수익률은 %.2f 입니다.", rate);
+
+        if (rate < 1) {
+            System.out.print(" (기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        }
+    }
 }
