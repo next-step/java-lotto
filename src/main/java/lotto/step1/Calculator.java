68bba44aa479081c4123924360443d2f9bde41bb
gunny6026 <gunny6026@naver.com>
2021-08-02 19:40:31 +0900

    refactor: 객체의 역할에 맞는 클래스 이름으로 변경

--- src/main/java/lotto/step1/Calculator.java
+++ src/main/java/lotto/step1/Calculator.java
@@ -1,35 +1,38 @@
 package lotto.step1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     private String text;
-    private Validation validation;
-    private Delimiter delimiter;
-    private static final int NEGATIVE_NUMBER_CONDITION = 0;
-    private static final int PRINT_NUMBER = 0;
+    private static final String DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
     public Calculator(String text) {
         this.text = text;
-        this.delimiter = new Delimiter(text);
-        this.validation = new Validation(text);
     }
 
-    public int calculate() {
-        if (validation.checkEmptyAndNull()) {
-            return PRINT_NUMBER;
+    public boolean checkEmptyAndNull() {
+        if (text.equals("") || text.equals(" ") || text.isEmpty()) {
+            return true;
         }
+        return false;
+    }
 
-        if (validation.checkOnlyNumber()) {
-            return printNumber();
+    public boolean checkNotNegativeNumber() {
+        String[] numbers = text.split(DELIMITER);
+        for (String number : numbers) {
+            isNegativeNumber(number);
         }
-
-        return addNumbers(delimiter.getNumbers());
+        return true;
     }
 
     public void isNegativeNumber(String number) {
-        if (Integer.valueOf(number) < NEGATIVE_NUMBER_CONDITION) {
+        if (Integer.valueOf(number) < 0) {
             throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
         }
+
     }
 
     public int printNumber() {
--- src/test/java/lotto/step1/ValidationTest.java
+++ src/test/java/lotto/step1/ValidationTest.java
@@ -1,46 +1,24 @@
 package lotto.step1;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationTest {
-
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final String DELIMITER = ",|:";
     @Test
-    @DisplayName("빈 문자열 이나 null 을 입력했을 때 boolean 값 확인")
-    void checkEmptyAndNull() {
-        Validation validation = new Validation("");
-        assertThat(validation.checkEmptyAndNull()).isTrue();
-    }
+    void nullCheck() {
+        String text = "//.\n1.2.3";
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(text);
+        System.out.println(matcher.find());
+        String group1 = matcher.group(1);
+        System.out.println(group1);
+        String group2 = matcher.group(2);
+        System.out.println(group2);
 
-    @Test
-    @DisplayName("빈 문자열 이나 null 을 입력했을 때 boolean 값 확인 2")
-    void checkEmptyAndNull2() {
-        Validation validation = new Validation(null);
-        assertThat(validation.checkEmptyAndNull()).isTrue();
-    }
-
-    @Test
-    @DisplayName("문자열로 숫자 하나를 입력 했는지 검증 하는 테스트")
-    void onlyNumberTest() {
-        Validation validation = new Validation("3");
-        assertThat(validation.checkOnlyNumber()).isTrue();
-    }
-
-    @Test
-    @DisplayName("문자열로 숫자 하나를 입력 했는지 검증 하는 테스트2")
-    void onlyNumberTest2() {
-        Validation validation = new Validation("s");
-        assertThat(validation.checkOnlyNumber()).isFalse();
-    }
-
-    @Test
-    @DisplayName("음수를 포함했을 때 예외처리 하는 테스트")
-    void isNegativeNumber() {
-        Calculator calculator = new Calculator("-1,2,4");
-        assertThatThrownBy(() -> calculator.isNegativeNumber("-1")).isInstanceOf(RuntimeException.class);
     }
 }
