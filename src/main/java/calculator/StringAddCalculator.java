79b06001f33d399d073d01f805d55939c19dad65
0n1dev <all_mind@naver.com>
2021-08-02 23:07:49 +0900

    feat : StringAddCalculator 클래스 생성
    - 구분자를 통해 분리하여 덧셈 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,4 +1,26 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
+
+	private StringAddCalculator() {
+
+	}
+
+	public static int splitAndAdd(String input) {
+		String[] splitString = splitString(input);
+
+		return addStringElements(splitString);
+	}
+
+	private static String[] splitString(String input) {
+		return input.split("[,;]");
+	}
+
+	private static int addStringElements(String[] elements) {
+		return Arrays.stream(elements)
+					.map(Integer::parseInt)
+					.reduce(0, Integer::sum);
+	}
 }
