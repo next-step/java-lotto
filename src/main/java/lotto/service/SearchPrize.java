a75be7ab729ed921547f56801b6b5b9147ea3d94
PIH6585 <pih6585@naver.com>
2021-08-03 16:33:14 +0900

    feat : win prize 찾는 service 추가 test : win prize 추가에 따른 test

---
+++ src/main/java/lotto/service/SearchPrize.java
@@ -0,0 +1,38 @@
+package lotto.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lotto.model.LottoGame;
+import lotto.model.LottoGames;
+import lotto.model.Prize;
+
+public class SearchPrize {
+
+	private final Map<Prize, Integer> winPrizes;
+
+	public SearchPrize() {
+		this.winPrizes = setupWinCondition();
+	}
+
+	private Map<Prize, Integer> setupWinCondition() {
+		return Arrays.stream(Prize.values())
+			.collect(Collectors.toMap(winnerResult -> winnerResult, winnerResult -> 0, (a, b) -> b));
+	}
+
+	public Map<Prize, Integer> getWinPrizes(LottoGames lottoGames, List<Integer> lastWinNumbers) {
+		for (LottoGame lottoGame : lottoGames.getLottoGames()) {
+			Prize winnersStatus = Prize.getWinnersStatus(
+				getMatchLottoStatus(lottoGame.getLottoGame(), lastWinNumbers));
+			int count = winPrizes.get(winnersStatus);
+			winPrizes.put(winnersStatus, count + 1);
+		}
+		return winPrizes;
+	}
+
+	public int getMatchLottoStatus(List<Integer> lottoGame, List<Integer> lastWinNumbers) {
+		return (int)lottoGame.stream().filter(lastWinNumbers::contains).count();
+	}
+}
---
+++ src/test/java/lotto/service/SearchPrizeTest.java
@@ -0,0 +1,52 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.model.LottoGame;
+import lotto.model.LottoGames;
+import lotto.model.Prize;
+
+class SearchPrizeTest {
+
+	@Test
+	@DisplayName("로또번호와 당첨번호를 입력하면 일치개수를 나타난다.")
+	public void getMatchLottoCount() {
+		List<Integer> lastWinNumbers = getLottoNumbers(1, 3, 5, 7, 9);
+		List<Integer> lottoGame = getLottoNumbers(1, 3, 5, 10, 12);
+		SearchPrize searchPrize = new SearchPrize();
+		int count = searchPrize.getMatchLottoStatus(lottoGame, lastWinNumbers);
+
+		assertThat(count).isEqualTo(3);
+	}
+
+	@Test
+	@DisplayName("로또게임별 내용에따라 당첨번호를 입력하면 일치개수를 나타난다.")
+	public void getMatchLottoGames() {
+		List<Integer> lastWinNumbers = getLottoNumbers(1, 3, 5, 7, 9);
+		SearchPrize searchPrize = new SearchPrize();
+
+		List<LottoGame> lottoGames = new ArrayList<>();
+		lottoGames.add(new LottoGame(getLottoNumbers(1, 3, 5, 10, 12)));
+		lottoGames.add(new LottoGame(getLottoNumbers(1, 3, 5, 7, 12)));
+		LottoGames lottoGameResult = new LottoGames(lottoGames);
+		Map<Prize, Integer> result = searchPrize.getWinPrizes(lottoGameResult, lastWinNumbers);
+
+		assertThat(result.size()).isEqualTo(5);
+		assertThat(result.get(Prize.FIFTH)).isEqualTo(1);
+		assertThat(result.get(Prize.FOURTH)).isEqualTo(1);
+	}
+
+	private List<Integer> getLottoNumbers(Integer... numbers) {
+		List<Integer> lottoNumbers = new ArrayList<>();
+		Collections.addAll(lottoNumbers, numbers);
+		return lottoNumbers;
+	}
+}
