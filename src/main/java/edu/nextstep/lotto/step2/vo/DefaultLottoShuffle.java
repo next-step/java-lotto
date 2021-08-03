41166e5577529399a7e09f7796e0bf42e804a718
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-08-03 11:57:09 +0900

    refactor: 상수 분리

--- src/main/java/edu/nextstep/lotto/step2/vo/DefaultLottoShuffle.java
+++ src/main/java/edu/nextstep/lotto/step2/vo/DefaultLottoShuffle.java
@@ -8,13 +8,14 @@ import java.util.stream.IntStream;
 public class DefaultLottoShuffle implements LottoShuffle {
 
     private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MAX_COUNT = 6;
 
     @Override
     public List<Integer> shuffle() {
         List<Integer> fullLottoNumbers = IntStream.range(1, LOTTO_MAX_NUMBER).boxed().collect(Collectors.toList());
         Collections.shuffle(fullLottoNumbers);
 
-        List<Integer> lottoNumbers = fullLottoNumbers.subList(0, 6);
+        List<Integer> lottoNumbers = fullLottoNumbers.subList(0, LOTTO_MAX_COUNT);
         Collections.sort(lottoNumbers);
 
         return lottoNumbers;
