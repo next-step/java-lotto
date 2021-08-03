833ff43beefb8021d080a6b18c9f8e0a69157441
PIH6585 <pih6585@naver.com>
2021-08-03 13:12:20 +0900

    feat : error message 상수 추가

---
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -0,0 +1,8 @@
+package lotto.utils;
+
+public class ErrorMessage {
+	public static final String NUMBER_TYPE_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
+	public static final String NUMBER_THOUSAND_UNIT_MESSAGE = "금액은 천단위만 가능합니다.";
+	public static final String EMPTY_ERROR_MESSAGE = "지난주 당첨번호가 빈값 입니다.";
+	public static final String LOTTO_NUMBER_SIZE = "로또 숫자는 총 6개 입니다.";
+}
