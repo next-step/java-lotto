68dbec0a4b70a4a4001d023d9b3fc66ccfd22d08
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-08-03 12:25:35 +0900

    test: 테스트 케이스 추가

--- src/test/java/edu/nextstep/lotto/step2/vo/LottoNumberTest.java
+++ src/test/java/edu/nextstep/lotto/step2/vo/LottoNumberTest.java
@@ -3,6 +3,8 @@ package edu.nextstep.lotto.step2.vo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberTest {
@@ -12,5 +14,12 @@ class LottoNumberTest {
     void create() {
         LottoNumber lottoNumber = new LottoNumber(new DefaultLottoShuffle());
         assertThat(lottoNumber.getLottoNumbers().size()).isEqualTo(6);
+    }
+
+    @DisplayName("로또 번호 생성 테스트 (난수 알고리즘 변경)")
+    @Test
+    void create_again() {
+        LottoNumber lottoNumber = new LottoNumber(() -> Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        assertThat(lottoNumber.getLottoNumbers().size()).isEqualTo(10);
     }
 }
