95ef5a6dffc5813e317607462b7d0d10640ffe95
PIH6585 <pih6585@naver.com>
2021-08-03 10:16:13 +0900

    refactor : calculate 외부주입식으로 method 변경

--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -11,20 +11,22 @@ class CalculatorTest {
 	@Test
 	@DisplayName("계산기의 문자열이 빈값이면 예외가 발생된다.")
 	public void validStringEmptyCheck() {
-		assertThrows(IllegalArgumentException.class, () -> new Calculator("")
+		assertThrows(IllegalArgumentException.class, () -> new Calculator().createCharacters("")
 		);
 	}
 
 	@Test
 	@DisplayName("계산기의 문자열에 따라 덧셈 계산기에 의하여 계산된 값이 반환된다.")
 	public void plusCalculator() {
-		Calculator calculator = new Calculator("//:\n1:2:3:4");
-		int result1 = calculator.calculate();
+		Calculator calculator = new Calculator();
+		Characters characters1 = calculator.createCharacters("//:\n1:2:3:4");
+		int result1 = calculator.calculate(characters1);
 
 		assertThat(result1).isEqualTo(10);
 
-		Calculator calculator2 = new Calculator("1:2:3,4,5");
-		int result2 = calculator2.calculate();
+		Calculator calculator2 = new Calculator();
+		Characters characters2 = calculator.createCharacters("1:2:3,4,5");
+		int result2 = calculator2.calculate(characters2);
 
 		assertThat(result2).isEqualTo(15);
 
