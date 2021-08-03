0bba44a03d613f74cd8bb038d7be16f7a7e0a9ef
ckdgus08 <ckdgus0808@naver.com>
2021-08-04 00:14:37 +0900

    refactor: 언더바(_) 사용하여 숫자 가독성 높히기

--- src/main/java/lotto/domain/Reward.java
+++ src/main/java/lotto/domain/Reward.java
@@ -2,10 +2,10 @@ package lotto.domain;
 
 public enum Reward {
 
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000);
 
     private final int rightNumberCount;
     private final int money;
