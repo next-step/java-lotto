b8204ba4ff73f7b034b8cf2535e737aebc634551
ChangSubKwak <paradise1732@gmail.com>
2021-08-04 07:24:33 +0900

    feat: 쉼표(,)나 콜론(:) 또는 커스텀 구분자로 문자열을 구분 후 이를 더하는 기능(splitAndSum) 추가

--- src/main/java/calculator/StringAdditionCalculator.java
+++ src/main/java/calculator/StringAdditionCalculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,4 +22,31 @@ public class StringAdditionCalculator {
         return splitWithCommaOrColon(expression);
     }
 
+    private static boolean isNaturalNumber(String expression) {
+        Pattern pattern = Pattern.compile("\\d+");
+        return pattern.matcher(expression).matches();
+    }
+
+    private static boolean isNullOrEmpty(String expression) {
+        return Objects.isNull(expression) || "".equals(expression);
+    }
+
+    private static boolean containsNaturalNumber(String[] expressions) {
+        return Arrays.asList(expressions)
+                .stream()
+                .anyMatch(token -> !isNaturalNumber(token));
+    }
+
+    public int splitAndSum(String expression) {
+        if (isNullOrEmpty(expression)) return 0;
+        if (isNaturalNumber(expression)) Integer.parseInt(expression);
+
+        String[] tokens = splitWithCustomDelimiter(expression);
+        if (containsNaturalNumber(tokens)) throw new RuntimeException(NOT_VALID_ARGUMENT_EXIST_STATEMENT);
+
+        return Arrays.asList(tokens)
+                .stream()
+                .map(Integer::parseInt)
+                .reduce(0, (firstOperand, secondOperand) -> firstOperand + secondOperand);
+    }
 }
