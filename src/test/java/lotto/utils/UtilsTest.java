ffd0ef61e6f878deb5503b4be393c81b3650cb51
PIH6585 <pih6585@naver.com>
2021-08-03 17:08:55 +0900

    test : 총 금액 이익관련 계산 테스트

--- src/test/java/lotto/utils/UtilsTest.java
+++ src/test/java/lotto/utils/UtilsTest.java
@@ -10,10 +10,19 @@ public class UtilsTest {
 
 	@ParameterizedTest
 	@DisplayName("숫자를 입력하면 천단위로 몫이 반환된다.")
-	@CsvSource(value = {"2000,2","14000,14","25500,25"})
-	public void thousandUnitDivide(int value, int portion){
+	@CsvSource(value = {"2000,2", "14000,14", "25500,25"})
+	public void returnThousandUnit(int value, int portion) {
 		int result1 = Utils.returnThousandUnit(value);
 
 		assertThat(result1).isEqualTo(portion);
+	}
+
+	@ParameterizedTest
+	@DisplayName("A와 B를 나눈 퍼센트 값이 반환된다.(퍼센트 기준은 1)")
+	@CsvSource(value = {"5000,14000,0.36", "14000,14000,1", "28000,14000,2"})
+	public void returnPercent(int value1, int value2, String percent) {
+		String result = Utils.returnPercent(value1, value2);
+
+		assertThat(result).isEqualTo(percent);
 	}
 }
