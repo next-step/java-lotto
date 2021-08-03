2acf32de5e3cc2d194b2f59beb527d41fb391367
syd9538 <syd9538@gmail.com>
2021-08-03 17:26:55 +0900

    style : split 상수 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,6 +9,9 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
     private static String GET_PURCHASE_AMOUNT_QUESTION = "구입금액을 입력해 주세요.";
     private static String GET_WINNING_NUMBER_QUESTION = "지난 주 당첨 번호를 입력해 주세요.";
+    private static String BLANK = "";
+    private static String SPACE = " ";
+    private static String SPLIT_CUSTOM_REGEX = "\",|, | ,\"";
 
     private InputView() {
 
@@ -23,8 +26,8 @@ public class InputView {
         System.out.println(GET_WINNING_NUMBER_QUESTION);
         scanner.nextLine();
         String winningNumberString = scanner.nextLine();
-        String trimWinningNumberString = winningNumberString.replace(" ", "");
-        return Arrays.stream(trimWinningNumberString.split(",|, | ,"))
+        String trimWinningNumberString = winningNumberString.replace(SPACE, BLANK);
+        return Arrays.stream(trimWinningNumberString.split(SPLIT_CUSTOM_REGEX))
                 .map(number -> LottoNumber.of(Integer.parseInt(number)))
                 .collect(Collectors.toCollection(TreeSet::new));
     }
