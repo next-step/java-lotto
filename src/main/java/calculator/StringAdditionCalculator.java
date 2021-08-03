6db420f480cb99acf66f294cd29378fd92bfda40
ChangSubKwak <paradise1732@gmail.com>
2021-08-04 07:20:53 +0900

    feat: 커스텀 구분자로 문자열을 구분하는 기능(splitWithCustomDelimiter) 기능 추가

--- src/main/java/calculator/StringAdditionCalculator.java
+++ src/main/java/calculator/StringAdditionCalculator.java
@@ -1,9 +1,23 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAdditionCalculator {
     private static final String NOT_VALID_ARGUMENT_EXIST_STATEMENT = "음수 또는 숫자이외 값이 존재합니다";
 
     public String[] splitWithCommaOrColon(String expression) {
         return expression.split(",|:");
     }
+
+    public String[] splitWithCustomDelimiter(String expression) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(expression);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+            return tokens;
+        }
+        return splitWithCommaOrColon(expression);
+    }
+
 }
