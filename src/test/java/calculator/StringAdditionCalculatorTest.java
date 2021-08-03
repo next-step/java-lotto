f5fe77a3043c7771a1fa3a0157efb4bb0d926846
ChangSubKwak <paradise1732@gmail.com>
2021-08-04 07:23:56 +0900

    test: 쉼표(,)나 콜론(:) 또는 커스텀 구분자로 문자열을 구분 후 이를 더하는 기능(splitAndSum) 테스트 케이스 추가

--- src/test/java/calculator/StringAdditionCalculatorTest.java
+++ src/test/java/calculator/StringAdditionCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringAdditionCalculatorTest {
     StringAdditionCalculator stringAdditionCalculator;
@@ -68,4 +70,41 @@ class StringAdditionCalculatorTest {
         );
     }
 
+    @Test
+    void 빈_문자열_또는_null_값을_입력할_경우_0을_반환한다() {
+        assertThat(stringAdditionCalculator.splitAndSum("")).isEqualTo(0);
+        assertThat(stringAdditionCalculator.splitAndSum(null)).isEqualTo(0);
+    }
+
+    @Test
+    void 숫자하나인_문자열를_입력하면_정수형으로_반환한다() {
+        assertThat(stringAdditionCalculator.splitAndSum("1")).isEqualTo(1);
+    }
+
+    @Test
+    void 쉼표구분자가_포함된_숫자문자열를_입력하면_쉼표를_구분자로_하는_분리된_정수들을의_합을_반환한다() {
+        assertThat(stringAdditionCalculator.splitAndSum("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    void 쉼표_또는_콜론_구분자가_포함된_숫자문자열를_입력하면_그_구분자로_분리된_정수들을의_합을_반환한다() {
+        assertThat(stringAdditionCalculator.splitAndSum("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    void 커스텀_구분자가_포함된_숫자문자열를_입력하면_그_구분자로_분리된_정수들을의_합을_반환한다() {
+        assertThat(stringAdditionCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test
+    void 음수가_포함된_숫자문자열를_입력하면_RuntimeException가_발생한다() {
+        assertThatThrownBy(() -> stringAdditionCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 숫자이외의_문자열_입력하면_RuntimeException가_발생한다() {
+        assertThatThrownBy(() -> stringAdditionCalculator.splitAndSum("A,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
 }
