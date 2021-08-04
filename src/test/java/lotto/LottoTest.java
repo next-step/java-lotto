8f15a7ca748acb8a25aee22379e2d3a309e245af
YooInKeun <keun0390@naver.com>
2021-08-04 13:52:24 +0900

    test: 로또 번호, 오름차순 정렬 테스트하는 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,9 +3,9 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("로또 테스트")
@@ -14,21 +14,41 @@ public class LottoTest {
     @DisplayName("로또의 번호 개수는 6개이어여 한다.")
     @Test
     public void lottoNumberCountTest() {
-        // given, when
+        // given
         Lotto lotto = new Lotto();
 
-        // then
+        // when, then
         assertEquals(lotto.getNumbersSize(), 6);
     }
 
-    @DisplayName("로또 번호는 중복되지 않은 번호로 이루어져 있다.")
+    @DisplayName("로또 번호는 중복되지 않은 번호로 이루어져 있어야 한다.")
     @Test
     public void nonDuplicatedLottoNumbersTest() {
-        // given, when
+        // given
         Lotto lotto = new Lotto();
 
-        // then
+        // when
         Set<LottoNumber> lottoNumbers = new HashSet<>(lotto.getNumbers());
+
+        // then
         assertEquals(lotto.getNumbersSize(), lottoNumbers.size());
+    }
+
+    @DisplayName("로또 번호는 오름차순으로 정렬되어 있어야 한다.")
+    @Test
+    public void sortedLottoNumbersTest() {
+        // given
+        Lotto lotto = new Lotto();
+
+        // when
+        List<LottoNumber> sortedLottoNumbers = new ArrayList<>();
+        for (LottoNumber lottoNumber : lotto.getNumbers()) {
+            sortedLottoNumbers.add(lottoNumber);
+        }
+        Collections.sort(sortedLottoNumbers);
+
+        // then
+        assertThat(lotto.getNumbers())
+                .containsExactlyElementsOf(sortedLottoNumbers);
     }
 }
