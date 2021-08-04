b54f2412f9950875a22c7275b6524e9ac845ac25
harris <harrisleesh@gmail.com>
2021-08-04 21:57:04 +0900

    test : Create empty input test and null input test

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringAddCalculatorTest {
+
+    @Test
+    @DisplayName("Input이 empty일 경우 0을 반환한다.")
+    void empty() {
+        int result = StringAddCalculator.calculate("");
+        assertThat(result).isZero();
+    }
+
+    @Test
+    @DisplayName("input이 null일 경우 0을 반환한다.")
+    void nullTest(){
+        int result = StringAddCalculator.calculate(null);
+        assertThat(result).isZero();
+    }
+
+}
