21fe65d52e1e872f5b5e6ea1ade58f7b542dcec3
shirohoo <shirohoo4102@gmail.com>
2021-08-04 15:54:53 +0900

    feat: 요구사항에 맞게 LottoShop 수정

--- src/main/java/lotto/model/LottoShop.java
+++ src/main/java/lotto/model/LottoShop.java
@@ -3,38 +3,38 @@ package lotto.model;
 import lotto.strategy.AutoLottoStrategy;
 import lotto.strategy.LottoRuleStrategy;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 
 public class LottoShop {
     private static final LottoRuleStrategy LOTTO_STRATEGY = AutoLottoStrategy.getInstance();
 
     public static final int LOTTO_PRICE = 1_000;
 
-    private static LottoShop lottoShop;
+    private static LottoShop instance;
 
     private LottoShop() {
     }
 
     public static synchronized LottoShop getInstance() {
-        if (Objects.isNull(lottoShop)) {
-            lottoShop = new LottoShop();
+        if (Objects.isNull(instance)) {
+            instance = new LottoShop();
         }
-        return lottoShop;
+        return instance;
     }
 
     public Lottos buy(final int payment) {
         validate(payment);
-
         int numberOfPurchases = payment / LOTTO_PRICE;
+        return Lottos.from(range(0, numberOfPurchases)
+                                   .mapToObj(this::getLotto)
+                                   .collect(toList()));
+    }
 
-        List<Lotto> lottos = new ArrayList<>();
-        while (0 < numberOfPurchases--) {
-            lottos.add(LOTTO_STRATEGY.ticketing());
-        }
-
-        return Lottos.from(lottos);
+    private Lotto getLotto(final int notUse) {
+        return LOTTO_STRATEGY.ticketing();
     }
 
     private void validate(final int payment) {
