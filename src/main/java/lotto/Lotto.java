91afa6e12fada1b659b102df657aaae695679cc0
YooInKeun <keun0390@naver.com>
2021-08-04 13:20:10 +0900

    refactor: 로또의 숫자 컬렉션 필드, lottoNumbers -> numbers로 리네이밍

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,18 +8,18 @@ import static java.util.stream.Collectors.toList;
 public class Lotto {
     private static final int LOTTO_NUMBER_COUNT = 6;
 
-    private final List<LottoNumber> lottoNumbers;
+    private final List<LottoNumber> numbers;
 
     Lotto() {
         Collections.shuffle(LottoNumber.ALL_LOTTO_NUMBERS);
-        List<LottoNumber> lottoNumbers = LottoNumber.ALL_LOTTO_NUMBERS.stream()
+        List<LottoNumber> numbers = LottoNumber.ALL_LOTTO_NUMBERS.stream()
                 .limit(LOTTO_NUMBER_COUNT)
                 .collect(toList());
-        Collections.sort(lottoNumbers);
-        this.lottoNumbers = lottoNumbers;
+        Collections.sort(numbers);
+        this.numbers = numbers;
     }
 
     int getNumberSize() {
-        return lottoNumbers.size();
+        return numbers.size();
     }
 }
