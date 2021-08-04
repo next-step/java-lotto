ee2b89ed2a32af9b710d8ecd0eb5f4f4cf810c21
YooInKeun <keun0390@naver.com>
2021-08-04 13:04:48 +0900

    feat: 로또 1개 가격보다 구입금액이 적을 때, 예외 발생하는 기능 추가

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -6,15 +6,23 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class Lottos {
-    private static final int LOTTO_PRICE = 1000;
+    private static final int ONE_LOTTO_PRICE = 1000;
 
     private final List<Lotto> lottos;
 
     public Lottos(int purchaseAmount) {
-        int lottoCount = purchaseAmount / LOTTO_PRICE;
+        validatePurchasableAmount(purchaseAmount);
+
+        int lottoCount = purchaseAmount / ONE_LOTTO_PRICE;
         this.lottos = Stream.generate(Lotto::new)
                 .limit(lottoCount)
                 .collect(toList());
+    }
+
+    private void validatePurchasableAmount(int purchaseAmount) {
+        if (purchaseAmount < ONE_LOTTO_PRICE) {
+            throw new IllegalArgumentException(String.format("구입금액은 최소 %d(원) 이상이어야 합니다.", ONE_LOTTO_PRICE));
+        }
     }
 
     public int getSize() {
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("로또 컬렉션 테스트")
@@ -19,5 +20,17 @@ public class LottosTest {
 
         // then
         assertEquals(14, lottos.getSize());
+    }
+
+    @DisplayName("로또 1개 가격보다 구입금액이 적으면 예외가 발생한다.")
+    @Test
+    public void purchaseAmountLessThanOneLottoPriceExceptionTest() {
+        // given
+        int purchaseAmount = 500;
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lottos(purchaseAmount))
+                .withMessage("구입금액은 최소 1000(원) 이상이어야 합니다.");
     }
 }
