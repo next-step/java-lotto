440a3b1af48496076fd08d4cbf0623b903a779fe
sk1737030 <sk1737030@naver.com>
2021-08-04 22:06:51 +0900

    refactor: 테스트 코드 중복 제거

--- src/test/java/step2/domain/LottoStatisticsTest.java
+++ src/test/java/step2/domain/LottoStatisticsTest.java
@@ -1,7 +1,7 @@
 package step2.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -11,16 +11,23 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoStatisticsTest {
 
+    private LottoNumber winOfLottoNumber;
+
+    @BeforeEach
+    void setUp() {
+        winOfLottoNumber = new LottoNumber(Arrays.asList(3, 4, 5, 1, 2, 10));
+    }
+
     @Test
     void 당첨된_통계를_보여준다() {
         // Given
         Map<Integer, Integer> expectedLottoStrategy = new HashMap<>();
-        expectedLottoStrategy.put(6, 3);
+        expectedLottoStrategy.put(3, 1);
 
-        LottoMachine lottoMachine = new LottoMachine(new InputNumberStrategy(Arrays.asList(3, 4, 5, 6, 7, 8)));
+        Integer[] givenNumbers = {3, 4, 5, 6, 7, 8};
 
-        LottoNumber winOfLottoNumber = new LottoNumber(Arrays.asList(3, 4, 5, 1, 2, 10));
-        List<Lotto> lottos = lottoMachine.sell(new Cache(3000));
+        LottoMachine lottoMachine = new LottoMachine(new InputNumberStrategy(Arrays.asList(givenNumbers)));
+        List<Lotto> lottos = lottoMachine.sell(new Cache(1000));
 
         // When
         LottoStatistics lottoStatistics = new LottoStatistics(winOfLottoNumber, lottos);
@@ -31,16 +38,15 @@ class LottoStatisticsTest {
 
     @Test
     void 당청된_수익률을_보여준다() {
-        long expectedLottoProfit = 500L;
+        long expectedLottoProfit = 5;
         Integer[] givenNumbers = {3, 4, 5, 6, 7, 8};
         LottoMachine lottoMachine = new LottoMachine(new InputNumberStrategy(Arrays.asList(givenNumbers)));
-        LottoNumber winOfLottoNumber = new LottoNumber(Arrays.asList(3, 4, 5, 1, 2, 10));
         List<Lotto> lottos = lottoMachine.sell(new Cache(1000));
 
         // When
         LottoStatistics lottoStatistics = new LottoStatistics(winOfLottoNumber, lottos);
 
         // Then
-        assertThat(lottoStatistics.getProfit()).isEqualTo(BigDecimal.valueOf(expectedLottoProfit));
+        assertThat(lottoStatistics.getProfit()).isEqualTo(expectedLottoProfit);
     }
 }
