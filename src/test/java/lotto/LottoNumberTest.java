0d3f000d060340a565f98a5aa0d6800aeafffa99
YooInKeun <keun0390@naver.com>
2021-08-04 14:54:15 +0900

    test: 모든 로또 번호, 1이상 45이하의 번호로 이루어져 있는지 테스트하는 코드 추가

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("로또 번호 테스트")
 public class LottoNumberTest {
@@ -32,6 +32,19 @@ public class LottoNumberTest {
         // then
         for (int i = 0; i < firstAllLottoNumbers.size(); i++) {
             assertSame(firstAllLottoNumbers.get(i), secondAllLottoNumbers.get(i));
+        }
+    }
+
+    @DisplayName("모든 로또 번호는 1이상 45이하의 번호로 이루어져 있어야 한다.")
+    @Test
+    public void getAllLottoNumbers_로또_번호는_1이상_45이하() {
+        // given, when
+        List<LottoNumber> allLottoNumbers = LottoNumber.getAllLottoNumbers();
+
+        // then
+        for (LottoNumber number : allLottoNumbers) {
+            assertThat(number.getNumber())
+                    .isBetween(1, 45);
         }
     }
 }
