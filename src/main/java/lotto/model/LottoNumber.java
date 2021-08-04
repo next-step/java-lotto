718d1e2a4bb59ee6f6b39ab01ae23b777fcec545
shirohoo <shirohoo4102@gmail.com>
2021-08-04 15:52:04 +0900

    feat: 요구사항에 맞게 LottoNumber 수정

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,15 +1,22 @@
 package lotto.model;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class LottoNumber implements Comparable<LottoNumber> {
+    private static final String DELIMITER = ", ";
+
     public static final int MIN_OF_LOTTO_NUMBER = 1;
     public static final int MAX_OF_LOTTO_NUMBER = 45;
+    private static final int NUMBER_OF_LOTTO_COUNT = 6;
 
     private static final Map<Integer, LottoNumber> LOTTO_NUMBERS = rangeClosed(MIN_OF_LOTTO_NUMBER, MAX_OF_LOTTO_NUMBER)
             .mapToObj(LottoNumber::new)
@@ -42,8 +49,41 @@ public class LottoNumber implements Comparable<LottoNumber> {
         return number;
     }
 
+    public static Set<LottoNumber> aggregate(final String winningNumber) {
+        Set<LottoNumber> lottoNumbers = stream(winningNumber.split(DELIMITER))
+                .map(LottoNumber::from)
+                .collect(toSet());
+
+        validateAggregate(lottoNumbers);
+
+        return lottoNumbers;
+    }
+
+    private static void validateAggregate(final Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != NUMBER_OF_LOTTO_COUNT) {
+            throw new IllegalArgumentException("Set<LottoNumber> size must be " + NUMBER_OF_LOTTO_COUNT);
+        }
+    }
+
     @Override
-    public int compareTo(LottoNumber o) {
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public int compareTo(final LottoNumber o) {
         return this.number - o.number;
     }
 }
