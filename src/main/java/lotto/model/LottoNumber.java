47a94c0c40cc077b92f87ac5a8f85a4d373d1fd7
YooInKeun <keun0390@naver.com>
2021-08-04 14:59:30 +0900

    chore: 로또 관련 모델 클래스, model 패키지로 이동

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
