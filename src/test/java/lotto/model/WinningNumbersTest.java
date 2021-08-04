5cc1ebeab707553bc0d2d4b1b46673d85a19bc7e
YooInKeun <keun0390@naver.com>
2021-08-04 15:36:34 +0900

    test; 당첨 번호 컬렉션, 당첨 번호 중복시 예외 발생 테스트 코드 추가

---
+++ src/test/java/lotto/model/WinningNumbersTest.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("당첨 번호 컬렉션 테스트")
+public class WinningNumbersTest {
+
+    @DisplayName("당첨 번호가 중복 되었을 때, Exception이 발생한다.")
+    @Test
+    public void duplicatedWinningNumberExceptionTest() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinningNumbers(numbers))
+                .withMessage("당첨 번호가 중복 되었습니다.");
+    }
+}
