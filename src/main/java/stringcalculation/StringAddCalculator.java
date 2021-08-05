9a951249915fe37f9a2d9d9b23e1bdd0a6fc3e5d
mewookim <40778836+mewookim@users.noreply.github.com>
2021-08-05 18:20:57 +0900

    Step1 (#1843)
    * Step1
    
    * refactor : package name modified
    
    * refactor : 상수화
    
    * test : test code modified
    
    * refactor : method 분리
    
    * refactor : 상수화
    
    * refactor : method modified

---
+++ src/main/java/stringcalculation/StringAddCalculator.java
@@ -0,0 +1,63 @@
+package stringcalculation;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final String CUSTOM_PATTERN = "//(.)\n(.*)";
+    private static final String NUMBER_PATTERN = "[0-9]";
+    private static final String BASE_DELIMITER_PATTERN = "[,:%s]";
+    private static final int DELIMITER_GROUP = 1;
+    private static final int OPERAND_GROUP = 2;
+
+    public static int splitAndSum(String inputText) {
+        if (!validText(inputText)) {
+            return 0;
+        }
+        String[] operandArr = getOperandArr(inputText);
+        if (operandArr.length == 1) {
+            return 1;
+        }
+
+        return sum(operandArr);
+    }
+
+    public static String[] getOperandArr(String text) {
+        Matcher m = Pattern.compile(CUSTOM_PATTERN).matcher(text);
+        String customDelimiter = "";
+        if (m.find()) {
+            customDelimiter = m.group(DELIMITER_GROUP);
+            text = m.group(OPERAND_GROUP);
+        }
+        String regExp = String.format(BASE_DELIMITER_PATTERN, customDelimiter);
+        return text.split(regExp);
+    }
+
+    public static void validNumber(String number) {
+        if (!number.matches(NUMBER_PATTERN)) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void validNegative(int number) {
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static boolean validText(String inputText) {
+        return !Objects.isNull(inputText) && inputText.trim().length() != 0;
+    }
+
+    public static int sum(String[] operandArr) {
+        int result = 0;
+        for (String numberStr : operandArr) {
+            validNumber(numberStr);
+            int number = Integer.parseInt(numberStr);
+            validNegative(number);
+            result += number;
+        }
+        return result;
+    }
+}
