378ea5357f4eee6e7e78a99d669c2461e856b74c
jaeyoungjang2 <lion85488@gmail.com>
2021-08-05 20:15:58 +0900

    feat: LottoMachine domain 생성 3. 사용자가 입력한 당첨 번호 검증.    - ", " 구분자로 나눈 뒤    - 6개의 수로 이루어져 있는지 확인

--- src/main/java/step2/domain/LottoMachine.java
+++ src/main/java/step2/domain/LottoMachine.java
@@ -1,8 +1,23 @@
 package step2.domain;
 
 public class LottoMachine {
+    private String lastLottoNum;
 
-    public void run() {
+    public LottoMachine(String lastLottoNum) {
+        this.lastLottoNum = lastLottoNum;
+        checkValidInput();
+    }
+
+
+    private void checkValidInput() {
+        String[] lastLottoNumArray = stringSpliter();
+        if (lastLottoNumArray.length != 6) {
+            throw new IllegalArgumentException("로또 당첨번호는 6개를 입력하세요. 당첨번호는 ', '로 구분을 하고 있습니다.");
+        }
+    }
+
+    private String[] stringSpliter() {
+        return lastLottoNum.split(", ");
 
     }
 }
