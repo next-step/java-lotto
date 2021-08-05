b36fb4c25b344578bf0e9c8aaf7f9c3b819008ea
gashe-soo <gashe7@gmail.com>
2021-08-06 02:31:52 +0900

    refactor:regex 표현식 상수로 변경

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -9,38 +9,38 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
 
     private static final String DELIMITER = ",|:";
-
+    private static final String CUSTOM_PATTERN = "//(.)\n(.*)";
 
     public static int splitAndSum(String expression) {
         if (StringUtil.isBlank(expression)) {
             return 0;
         }
 
-        Matcher matchResult = Pattern.compile("//(.)\n(.*)").matcher(expression);
+        Matcher matchResult = Pattern.compile(CUSTOM_PATTERN).matcher(expression);
         if (matchResult.find()) {
             String customDelimiter = matchResult.group(1);
-            String[] tokens= matchResult.group(2).split(customDelimiter);
-            return sumSplitExpression(tokens);
+            String[] tokens = matchResult.group(2).split(customDelimiter);
+            return sum(tokens);
         }
 
-        return sumSplitExpression(expression.split(DELIMITER));
+        return sum(expression.split(DELIMITER));
     }
 
-    private static int sumSplitExpression(String[] splitExpression) {
+    private static int sum(String[] splitExpression) {
         return Arrays.stream(splitExpression)
                 .mapToInt(StringAddCalculator::toInt)
                 .reduce(0, (total, number) -> total + number)
                 ;
     }
 
-    private static int toInt(String str){
-        try{
+    private static int toInt(String str) {
+        try {
             int result = Integer.parseInt(str);
-            if(result < 0){
+            if (result < 0) {
                 throw new IllegalArgumentException("0 또는 자연수만 입력하세요.");
             }
             return result;
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("0 또는 자연수만 입력하세요.");
         }
     }
