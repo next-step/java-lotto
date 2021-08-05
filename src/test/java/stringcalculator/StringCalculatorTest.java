37b4874eaf98269b50c0e6e6e5e6cc0ba0bf6ad9
hiro <kiryuinh32@gmail.com>
2021-08-05 21:59:29 +0900

    🚀 1단계 - 문자열 덧셈 계산기 (#1857)

---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,49 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class StringCalculatorTest {
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    public void splitAndSum_null_또는_빈문자(String s) {
+        int result = StringAddCalculator.splitAndSum(s);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
---
+++ src/test/java/stringcalculator/StringParser.java
@@ -0,0 +1,29 @@
+package stringcalculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringParser {
+    private static final String DELIMITER = ",|:";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int SEPARATOR_GROUP_NUMBER = 1;
+    private static final int EXPRESSION_GROUP_NUMBER = 2;
+
+    public static int[] parser(String s) {
+        Matcher matcher = CUSTOM_PATTERN.matcher(s);
+        if (matcher.find()) {
+            return split(matcher.group(EXPRESSION_GROUP_NUMBER), String.join("|", DELIMITER, matcher.group(SEPARATOR_GROUP_NUMBER)));
+        }
+
+        return Arrays.stream(s.split(DELIMITER)).
+                mapToInt(Integer::parseInt).
+                toArray();
+    }
+
+    private static int[] split(String s, String delimiter) {
+        return Arrays.stream(s.split(delimiter))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+    }
+}
