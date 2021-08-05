07a546851f2b24433b69e451a9760c4e1cf4337b
PIH6585 <pih6585@naver.com>
2021-08-05 10:24:34 +0900

    refactor : naming 수정

--- src/test/java/lotto/utils/TicketCalculatorTest.java
+++ src/test/java/lotto/utils/TicketCalculatorTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class TicketCalculationTest {
+class TicketCalculatorTest {
 
 	@ParameterizedTest
 	@DisplayName("숫자를 입력하면 천단위로 몫이 반환된다.")
 	@CsvSource(value = {"2000,2", "14000,14", "25500,25"})
 	public void returnThousandUnit(int value, int portion) {
-		int result1 = TicketCalculation.getLottoTicketNumber(value);
+		int result1 = TicketCalculator.calculatorTicketCount(value);
 
 		assertThat(result1).isEqualTo(portion);
 	}
