a3b71ab2172c67dfde5a12fbcbf8a2577ae71b17
채윤병 <ybchae0801@naver.com>
2021-08-05 23:14:54 +0900

    feat: 커스텀 구분자를 기준으로 분리한 각 숫자의 합을 반환하는 로직 작성

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,19 +1,41 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\\\n(.*)");
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final int NUMBERS_JOINED_BY_DELIMITER_GROUP = 2;
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
 
     public int calculate(String input) {
-        return parse(input).stream()
+        return Arrays.stream(parse(input))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
 
-    private List<String> parse(String input) {
-        return Arrays.asList(input.split(DEFAULT_DELIMITER_REGEX));
+    private String[] parse(String input) {
+        Matcher customDelimiterMatcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (customDelimiterMatcher.find()) {
+            return parseWithCustomDelimiter(customDelimiterMatcher);
+        }
+        return input.split(DEFAULT_DELIMITER_REGEX);
     }
+
+    private String[] parseWithCustomDelimiter(Matcher customDelimiterMatcher) {
+        return getNumbersJoinedByDelimiter(customDelimiterMatcher)
+                    .split(getCustomDelimiter(customDelimiterMatcher));
+    }
+
+    private String getNumbersJoinedByDelimiter(Matcher customDelimiterMatcher) {
+        return customDelimiterMatcher.group(NUMBERS_JOINED_BY_DELIMITER_GROUP);
+    }
+
+    private String getCustomDelimiter(Matcher customDelimiterMatcher) {
+        return Pattern.quote(customDelimiterMatcher.group(CUSTOM_DELIMITER_GROUP));
+    }
+
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -10,8 +10,14 @@ class StringAddCalculatorTest {
     private final StringAddCalculator calculator = new StringAddCalculator();
 
     @ParameterizedTest(name = "쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
-    @CsvSource(value = {"1,2:3=6", "1,2=3", "1:2=3"}, delimiter = '=')
+    @CsvSource(value = {"1,2:3=6", "1,2=3", "1:2=3", "1=1"}, delimiter = '=')
     public void addByDefaultDelimiterTest(String input, int expectedResult) {
+        assertThat(calculator.calculate(input)).isEqualTo(expectedResult);
+    }
+
+    @ParameterizedTest(name = "문자열 앞부분의 “//”와 “\\n” 사이에 위치하는 문자를 커스텀 구분자로 사용 할 수 있다.")
+    @CsvSource(value = {"//;\\n1;2;3=6", "//+\\n1+2=3"}, delimiter = '=')
+    public void addByCustomDelimiterTest(String input, int expectedResult) {
         assertThat(calculator.calculate(input)).isEqualTo(expectedResult);
     }
 
