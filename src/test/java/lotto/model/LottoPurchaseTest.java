9bf8b0c7b2b44f3aebddd1765e8643648855b6f1
PIH6585 <pih6585@naver.com>
2021-08-05 09:14:42 +0900

    refactor : static 변수로 변경에 따라 수정

--- src/test/java/lotto/model/LottoPurchaseTest.java
+++ src/test/java/lotto/model/LottoPurchaseTest.java
@@ -2,25 +2,17 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoPurchaseTest {
 
-	public LottoPurchase lottoPurchase;
-
-	@BeforeEach
-	void setup() {
-		lottoPurchase = new LottoPurchase();
-	}
-
 	@Test
 	@DisplayName("입력된 로또게임 장수 만큼 로또게임이 생성된다.")
 	public void createMoneyByLottoGame() {
-		assertThat(lottoPurchase.lottoTicketPurchase(4000).size()).isEqualTo(4);
+		assertThat(LottoPurchase.lottoTicketPurchase(4000).size()).isEqualTo(4);
 
-		assertThat(lottoPurchase.lottoTicketPurchase(6000).size()).isEqualTo(6);
+		assertThat(LottoPurchase.lottoTicketPurchase(6000).size()).isEqualTo(6);
 	}
 
 }
