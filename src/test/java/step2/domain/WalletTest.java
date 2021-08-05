3ed213fa366680cd29b1bd7f211473ef56231524
sk1737030 <sk1737030@naver.com>
2021-08-06 00:47:11 +0900

    test: 테스트 코드 분리

--- src/test/java/step2/domain/WalletTest.java
+++ src/test/java/step2/domain/WalletTest.java
@@ -10,11 +10,25 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class WalletTest {
 
     @Test
-    void _10000원이_있는_지갑에서_5000원을_인출_할_수_있다() {
+    void _10000원이_있는_지갑에서_5000원을_인출하면_5000원이_나온다() {
         // given
         Wallet expectedWallet = new Wallet(new Cache(5000));
-
         Cache withDrawCache = new Cache(5000);
+        Cache expectedRemainMoney = new Cache(5000);
+
+        // when
+        Money actualMoney = expectedWallet.withDraw(withDrawCache);
+
+        // then
+        assertThat(actualMoney).isEqualTo(expectedRemainMoney);
+    }
+
+    @Test
+    void _10000원이_있는_지갑에서_5000원을_인출하면_지갑에_5000원이_있다() {
+        // given
+        Wallet expectedWallet = new Wallet(new Cache(5000));
+        Cache withDrawCache = new Cache(5000);
+        Cache expectedRemainMoney = new Cache(5000);
 
         // when
         Money actualMoney = expectedWallet.withDraw(withDrawCache);
