107c26bd1a3c334f413bd62d375a4fda1486574a
gashe-soo <gashe7@gmail.com>
2021-08-06 01:52:21 +0900

    feat:커스텀 구분자 기준 숫자합 기능

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -7,14 +7,28 @@ import java.util.Arrays;
 public class StringAddCalculator {
 
     private static final String DELIMITER = ",|:";
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String CUSTOM_SUFFIX = "\n";
 
     public static int splitAndSum(String expression) {
 
-        if(StringUtil.isBlank(expression)){
+        if (StringUtil.isBlank(expression)) {
             return 0;
         }
 
-        return Arrays.stream(expression.split(DELIMITER))
+        if (expression.startsWith(CUSTOM_PREFIX)) {
+            int lastIndex = expression.indexOf(CUSTOM_SUFFIX);
+            String customDelimiter = expression.substring(CUSTOM_PREFIX.length(), lastIndex);
+            String truncatedExpression = expression.substring(lastIndex + CUSTOM_SUFFIX.length());
+
+            return sumSplitExpression(truncatedExpression.split(customDelimiter));
+        }
+
+        return sumSplitExpression(expression.split(DELIMITER));
+    }
+
+    private static int sumSplitExpression(String[] splitExpression) {
+        return Arrays.stream(splitExpression)
                 .mapToInt(Integer::parseInt)
                 .reduce(0, (total, number) -> total + number)
                 ;
