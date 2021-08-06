82ab46ca5a248ce241661cf028dcf30e76cbbc36
gunny6026 <gunny6026@naver.com>
2021-08-06 21:36:41 +0900

    refactor : 역할에 맞는 naming으로 수정 , static 멤버로 전환

--- src/main/java/lotto/step2/LottoMachine.java
+++ src/main/java/lotto/step2/LottoMachine.java
@@ -4,25 +4,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class LottoMachine {
-
-    private ArrayList<Integer> lottoNumbers;
+    private static ArrayList<Integer> lottoNumbers = new ArrayList<>();
     private static final int LOTTO_LAST_NUMBER = 45;
     private static final int LOTTO_LAST_COUNT = 6;
 
-    public LottoMachine() {
-        this.lottoNumbers = new ArrayList<>();
-    }
-
-    public void getLottoNumbers() {
-        for (int i=1; i<=LOTTO_LAST_NUMBER; i++) {
+    static {
+        for (int i = 1; i <= LOTTO_LAST_NUMBER; i++) {
             lottoNumbers.add(i);
         }
     }
 
-    public void pickLotto() {
+    public static void mixLottoNumbers() {
         Collections.shuffle(lottoNumbers);
-        for (int i=1; i<=LOTTO_LAST_COUNT; i++){
-
-        }
     }
 }
