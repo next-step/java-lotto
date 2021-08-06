0f662044a5484a979b8cb7fde418bbb9aa24a8b8
harris <harrisleesh@gmail.com>
2021-08-06 19:18:16 +0900

    refactor : Change null, empty test using @ParameterizedTest and @NullAndEmptySource

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,17 +12,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringAddCalculatorTest {
 
-    @Test
-    @DisplayName("Input이 empty일 경우 0을 반환한다.")
-    void empty() {
-        int result = StringAddCalculator.calculate("");
-        assertThat(result).isZero();
-    }
-
-    @Test
-    @DisplayName("input이 null일 경우 0을 반환한다.")
-    void nullTest(){
-        int result = StringAddCalculator.calculate(null);
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("Input이 empty, null일 경우 0을 반환한다.")
+    void empty(String input) {
+        int result = StringAddCalculator.calculate(input);
         assertThat(result).isZero();
     }
 
