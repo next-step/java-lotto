b9020f2c96896212ed1fe7a3ed147e171b213156
sk1737030 <sk1737030@naver.com>
2021-08-06 20:07:24 +0900

    feat: 로또 숫자에 관한 벨리데이션 추가

--- src/main/java/step2/domain/lotto/Lotto.java
+++ src/main/java/step2/domain/lotto/Lotto.java
@@ -4,24 +4,39 @@ import java.util.List;
 
 public class Lotto {
 
-    private static final String NEGATIVE_NUMBER_EXCEPTION = "로또번호에 음수는 올 수 없습니다.";
+    private static final int MIN_LOTTO_NUM = 1;
+    private static final int MAX_LOTTO_NUM = 45;
+    private static final int LOTTO_SIZE = 6;
+    private static final String LIMIT_NUMBER_EXCEPTION = "로또번호는 1~45까지의 숫자만 올 수 있습니다.";
+    private static final String MIN_SIZE_EXCEPTION = "로또번호는 6개여야 합니다.";
 
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
-        validateLottoNumber(lottoNumbers);
+        validateLottoSize(lottoNumbers);
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validateLottoSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(MIN_SIZE_EXCEPTION);
+        }
     }
 
     public static Lotto create(List<Integer> lottoNumber) {
         return new Lotto(lottoNumber);
     }
 
-    private void validateLottoNumber(List<Integer> numbers) {
+    private void validateLottoNumbers(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number < 0) {
-                throw new IllegalArgumentException(NEGATIVE_NUMBER_EXCEPTION);
-            }
+            validateLottoNumbers(number);
+        }
+    }
+
+    private void validateLottoNumbers(Integer number) {
+        if (number < MIN_LOTTO_NUM || number > MAX_LOTTO_NUM) {
+            throw new IllegalArgumentException(LIMIT_NUMBER_EXCEPTION);
         }
     }
 
@@ -35,8 +50,4 @@ public class Lotto {
         return lottoNumbers;
     }
 
-    @Override
-    public String toString() {
-        return lottoNumbers.toString();
-    }
 }
