6873d5b4608999fff904a9bf74d2a6d1998f5058
shirohoo <shirohoo4102@gmail.com>
2021-08-06 21:50:12 +0900

    refactor: LottoRuleStrategy의 Set을 수정불가능한 Set으로 변경

--- src/main/java/lotto/strategy/LottoRuleStrategy.java
+++ src/main/java/lotto/strategy/LottoRuleStrategy.java
@@ -3,21 +3,18 @@ package lotto.strategy;
 import lotto.model.Lotto;
 import lotto.model.LottoNumber;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
 
+import static java.util.stream.Collectors.toSet;
 import static java.util.stream.IntStream.rangeClosed;
 import static lotto.model.LottoNumber.MAX_OF_LOTTO_NUMBER;
 import static lotto.model.LottoNumber.MIN_OF_LOTTO_NUMBER;
 
 public abstract class LottoRuleStrategy {
-    protected static final Set<LottoNumber> LOTTO_NUMBERS_RANGE = new HashSet<>();
-
-    static {
-        rangeClosed(MIN_OF_LOTTO_NUMBER, MAX_OF_LOTTO_NUMBER)
-                .mapToObj(LottoNumber::from)
-                .forEach(LOTTO_NUMBERS_RANGE::add);
-    }
+    protected static final Set<LottoNumber> LOTTO_NUMBERS_RANGE = Collections.unmodifiableSet(rangeClosed(MIN_OF_LOTTO_NUMBER, MAX_OF_LOTTO_NUMBER)
+                                                                                                      .mapToObj(LottoNumber::from)
+                                                                                                      .collect(toSet()));
 
     abstract public Lotto ticketing();
 }
