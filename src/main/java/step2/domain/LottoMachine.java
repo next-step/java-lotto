1c8e33315194fcc8973f6f9bb12821114d9d9728
jaeyoungjang2 <lion85488@gmail.com>
2021-08-06 22:27:33 +0900

    refactor: indent 2를 넘지 않도록 수정

--- src/main/java/step2/domain/LottoMachine.java
+++ src/main/java/step2/domain/LottoMachine.java
@@ -11,7 +11,7 @@ public class LottoMachine {
     private static ArrayList<String> lottoNumberList = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42"));
     private static final int COUNT_OF_LOTTO = 6;
     private static int numOfLotto;
-    private static int[] matchedLottoRecordArray = new int[COUNT_OF_LOTTO];
+    private static int[] matchedLottoRecordArray = new int[COUNT_OF_LOTTO+1];
     private static ResultView resultView = new ResultView();
 
     public LottoMachine(String lastLottoNum, int numOfLotto) {
@@ -24,34 +24,39 @@ public class LottoMachine {
 
     public void run() {
         for (int i = 0; i < numOfLotto; i++) {
-            compareLottoWithLastLottoNumber(issueLotto());
+            issueLotto();
         }
-
-        System.out.println(Arrays.toString(matchedLottoRecordArray));
+        resultView.printMatchedLottoRecord(matchedLottoRecordArray, numOfLotto);
     }
 
-    public static void compareLottoWithLastLottoNumber(String[] issuedLotto ) {
-        int matchedNumber = 0;
-        for (String userLottoNumber: issuedLotto) {
-            for (String lastLottoNumber: lastLottoNumArray) {
-                if (userLottoNumber.equals(lastLottoNumber)) {
-                    matchedNumber++;
-                }
-            }
+    public static int compareLottoWithLastLottoNumber(String issuedLotto ) {
+        int isMatch = 0;
+        for (String lastLottoNumber: lastLottoNumArray) {
+//            isMatch += isEqual(issuedLotto, lastLottoNumber);
+            isMatch += isEqual(issuedLotto, lastLottoNumber);
         }
-        matchedLottoRecordArray[matchedNumber]++;
-
+        return isMatch;
     }
 
-
+    public static int isEqual(String issuedLottoNumer, String lastLottoNumber) {
+        if (issuedLottoNumer.equals(lastLottoNumber)) {
+            return 1;
+        }
+        return 0;
+    }
 
     public static String[] issueLotto() {
+        int matchedNumber = 0;
         Collections.shuffle(lottoNumberList);
         String[] lotto = new String[COUNT_OF_LOTTO];
         for (int i = 0; i < COUNT_OF_LOTTO; i++) {
-            lotto[i] = lottoNumberList.get(i);
+            String issuedLottoNum = lottoNumberList.get(i);
+            lotto[i] = issuedLottoNum;
+            matchedNumber += compareLottoWithLastLottoNumber(issuedLottoNum);
         }
         resultView.printIssuedLottoNumber(lotto);
+        matchedLottoRecordArray[matchedNumber]++;
+        System.out.println(matchedNumber);
         return lotto;
     }
 
@@ -65,6 +70,5 @@ public class LottoMachine {
 
     private String[] stringSpliter(String lastLottoNum) {
         return lastLottoNum.split(", ");
-
     }
 }
