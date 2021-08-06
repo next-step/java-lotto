2162f9ddb0c17cf51328287984d9dce0615937a0
harris <harrisleesh@gmail.com>
2021-08-06 23:03:24 +0900

    test : Create several delimiter test

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,12 +12,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringAddCalculatorTest {
+    private StringAddCalculator stringAddCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringAddCalculator = new StringAddCalculator();
+    }
 
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("Input이 empty, null일 경우 0을 반환한다.")
     void empty(String input) {
-        int result = StringAddCalculator.calculate(input);
+        int result = stringAddCalculator.calculate(input);
         assertThat(result).isZero();
     }
 
@@ -24,7 +31,7 @@ class StringAddCalculatorTest {
     @ValueSource(ints = {1, 2, 3, 4, 0})
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     void oneNumber(int input){
-        int result = StringAddCalculator.calculate(String.valueOf(input));
+        int result = stringAddCalculator.calculate(String.valueOf(input));
         assertThat(result).isEqualTo(input);
     }
 
@@ -32,14 +39,14 @@ class StringAddCalculatorTest {
     @ValueSource(strings = {"-1", "-1,-2", "1,2,-3"})
     @DisplayName("음수를 입력할 경우 RuntimeException 이 발생해야 한다.")
     void minusInput(String input){
-        assertThatThrownBy(() -> StringAddCalculator.calculate(input)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> stringAddCalculator.calculate(input)).isInstanceOf(RuntimeException.class);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1,2,3;6", "4,5,6;15", "7,8,9;24"}, delimiter = ';')
     @DisplayName("','구분자로 숫자만 열거되어있는 경우 덧셈을 수행한다.")
     void comma(String input, int expected) {
-        int result = StringAddCalculator.calculate(input);
+        int result = stringAddCalculator.calculate(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -49,7 +56,7 @@ class StringAddCalculatorTest {
     )
     @DisplayName("':' 구분자로 숫자만 열거되어있는 경우 덧셈을 수행한다.")
     void colon(String input, int expected) {
-        int result = StringAddCalculator.calculate(input);
+        int result = stringAddCalculator.calculate(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -59,7 +66,7 @@ class StringAddCalculatorTest {
     )
     @DisplayName("',' ':' 구분자가 혼합되어있을 경우 덧셈 결과를 리턴한다.")
     void mixed(String input, int expected) {
-        int result = StringAddCalculator.calculate(input);
+        int result = stringAddCalculator.calculate(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -67,8 +74,15 @@ class StringAddCalculatorTest {
     @DisplayName("';' 구분자로 열거되어있는 경우 덧셈 결과를 리턴한다.")
     void customDelimiter() {
         String input = "//;\n1;2;3";
-        int result = StringAddCalculator.calculate(input);
+        int result = stringAddCalculator.calculate(input);
         assertThat(result).isEqualTo(6);
     }
 
+    @Test
+    @DisplayName("'#', '&' 구분자로 열거되어있는 경우 덧셈 결과를 리턴한다.")
+    void customSeveralDelimiter() {
+        String input = "//#\n//&\n1#2&3,10:11";
+        int result = stringAddCalculator.calculate(input);
+        assertThat(result).isEqualTo(27);
+    }
 }
