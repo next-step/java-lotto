29d5193f47a44c01a25488656be3adf59dac1553
ChangSubKwak <paradise1732@gmail.com>
2021-08-07 07:00:50 +0900

    fix: 필요없는 테스트 삭제, @NullAndEmptySource 이용한 테스트, 커스텀 문자열 여러개 포함할 때 테스트 케이스 작성

--- src/test/java/calculator/StringAdditionCalculatorTest.java
+++ src/test/java/calculator/StringAdditionCalculatorTest.java
@@ -3,8 +3,7 @@ package calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
@@ -20,60 +19,9 @@ class StringAdditionCalculatorTest {
     }
 
     @ParameterizedTest
-    @MethodSource
-    void 쉼표_또는_콜론이_포함된_문자열을_입력받으면_쉼표_또는_콜론을_기준으로_문자열이_분리된다(String input, String[] expected) {
-        String[] result = stringAdditionCalculator.splitWithCommaOrColon(input);
-        assertThat(result).containsExactly(expected);
-    }
-
-    private static Stream<Arguments> 쉼표_또는_콜론이_포함된_문자열을_입력받으면_쉼표_또는_콜론을_기준으로_문자열이_분리된다() {
-        return Stream.of(
-                Arguments.of(
-                        "",
-                        new String[] {""}
-                ),
-                Arguments.of(
-                        "1,2",
-                        new String[] {"1", "2"}
-                ),
-                Arguments.of(
-                        "1,2,3",
-                        new String[] {"1", "2", "3"}
-                ),
-                Arguments.of(
-                        "1,2:3",
-                        new String[] {"1", "2", "3"}
-                )
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource
-    void 커스텀구분자가_포함된_문자열을_입력받으면_그_구분자를_기준으로_문자열이_분리된다(String input, String[] expected) {
-        assertThat(stringAdditionCalculator.splitWithCustomDelimiter(input)).isEqualTo(expected);
-    }
-
-    private static Stream<Arguments> 커스텀구분자가_포함된_문자열을_입력받으면_그_구분자를_기준으로_문자열이_분리된다() {
-        return Stream.of(
-                Arguments.of(
-                        "//;\n1;2;3",
-                        new String[] {"1", "2", "3"}
-                ),
-                Arguments.of(
-                        "//-\n4-5-6",
-                        new String[] {"4", "5", "6"}
-                ),
-                Arguments.of(
-                        "//~\n7~8~9",
-                        new String[] {"7", "8", "9"}
-                )
-        );
-    }
-
-    @Test
-    void 빈_문자열_또는_null_값을_입력할_경우_0을_반환한다() {
-        assertThat(stringAdditionCalculator.splitAndSum("")).isEqualTo(0);
-        assertThat(stringAdditionCalculator.splitAndSum(null)).isEqualTo(0);
+    @NullAndEmptySource
+    void 빈_문자열_또는_null_값을_입력할_경우_0을_반환한다(String input) {
+        assertThat(stringAdditionCalculator.splitAndSum(input)).isEqualTo(0);
     }
 
     @Test
@@ -106,5 +54,18 @@ class StringAdditionCalculatorTest {
     void 숫자이외의_문자열_입력하면_RuntimeException가_발생한다() {
         assertThatThrownBy(() -> stringAdditionCalculator.splitAndSum("A,2,3"))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 커스텀_구분자가_여러개_포함된_숫자문자열를_입력하면_그_구분자로_분리된_정수들을의_합을_반환한다(String actual, int expected) {
+        assertThat(stringAdditionCalculator.splitAndSum(actual)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> 커스텀_구분자가_여러개_포함된_숫자문자열를_입력하면_그_구분자로_분리된_정수들을의_합을_반환한다() {
+        return Stream.of(
+                Arguments.of("//#\n//&\n1#2&3", 6),
+                Arguments.of("//#\n//&\n//,\n//:\n1#2&3,10:11", 27)
+        );
     }
 }
