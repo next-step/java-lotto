99f3ee81521047ab01bbe08f113d78ab264129c7
0n1dev <all_mind@naver.com>
2021-08-06 21:24:20 +0900

    feat : 자동 로또 생성 테스트 코드 작성
    - 자동 로또 생성 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+
+	@Test
+	@DisplayName("로또 자동으로 5개 생성시 예외 발생 없음")
+	void generateAutoLottos() {
+		assertDoesNotThrow(() -> {
+			Lottos.generateAutoLottos(5);
+		})
+	}
+}
