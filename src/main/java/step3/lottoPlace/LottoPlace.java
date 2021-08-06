df12d72cc9e7fd0a7d1d38031b3232a2af4d4f98
rkdals213 <rkdals213@naver.com>
2021-08-06 14:35:19 +0900

    feat: 로또 등수 수정

--- src/main/java/step3/lottoPlace/LottoPlace.java
+++ src/main/java/step3/lottoPlace/LottoPlace.java
@@ -3,22 +3,28 @@ package step3.lottoPlace;
 import java.util.Arrays;
 
 public enum LottoPlace {
-    FIRST(2_000_000_000, 6),
-    SECOND(1_500_000, 5),
-    THIRD(50_000, 4),
-    FORTH(5_000, 3),
-    NONE(0, 0);
+    FIRST(2_000_000_000, 6, false),
+    SECOND(30_000_000, 5, true),
+    THIRD(1_500_000, 5, false),
+    FORTH(50_000, 4, false),
+    FIFTH(5_000, 3, false),
+    NONE(0, 0, false);
 
     private final int price;
     private final int correctNum;
+    private final boolean isBonusNumCorrect;
 
-    LottoPlace(int price, int correctNum) {
+    LottoPlace(int price, int correctNum, boolean isBonusNumCorrect) {
         this.price = price;
         this.correctNum = correctNum;
+        this.isBonusNumCorrect = isBonusNumCorrect;
     }
 
-    public static LottoPlace findPlaceByCorrectNum(int correctNum) {
-        return Arrays.stream(values()).filter(i -> i.correctNum == correctNum).findFirst().orElse(LottoPlace.NONE);
+    public static LottoPlace findPlaceByCorrectNum(int correctNum, boolean isBonusNumCorrect) {
+        return Arrays.stream(values())
+            .filter(i -> i.correctNum == correctNum && (isBonusNumCorrect & i.isBonusNumCorrect) == i.isBonusNumCorrect)
+            .findFirst()
+            .orElse(LottoPlace.NONE);
     }
 
     public int getPrice() {
