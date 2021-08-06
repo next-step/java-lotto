17c0f80df2c7989137055d943a2919bf9ba4707b
iiaii <jhun9409@naver.com>
2021-08-06 18:16:21 +0900

    [step2] refactor: input view static 메서드로 변경

--- src/main/java/lottery/view/PurchasingPriceConsoleInput.java
+++ src/main/java/lottery/view/PurchasingPriceConsoleInput.java
@@ -6,10 +6,10 @@ public class PurchasingPriceConsoleInput {
 
     private static final String PURCHASING_AMOUNT_QUESTION = "구입금액을 입력해 주세요.";
 
-    private final Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public int askPurchasingPrice() {
+    public static String askPurchasingPrice() {
         System.out.println(PURCHASING_AMOUNT_QUESTION);
-        return Integer.parseInt(scanner.nextLine());
+        return scanner.nextLine();
     }
 }
--- src/main/java/lottery/view/WinningLotteryNumberConsoleInput.java
+++ src/main/java/lottery/view/WinningLotteryNumberConsoleInput.java
@@ -10,9 +10,9 @@ public class WinningLotteryNumberConsoleInput {
     private static final String WINNING_LOTTERY_QUESTION = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String DELIMITER = ", ";
 
-    private final Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public List<Integer> askWinningLotteryNumbers() {
+    public static List<Integer> askWinningLotteryNumbers() {
         System.out.println(WINNING_LOTTERY_QUESTION);
         return Arrays.stream(scanner.nextLine().split(DELIMITER))
                 .map(Integer::parseInt)
