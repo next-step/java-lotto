6eb493e9f85abee5dd764d2eab454da115a39f10
rkdals213 <rkdals213@naver.com>
2021-08-06 14:36:10 +0900

    test: 테스트 코드 수정

--- src/test/java/step3/LottoAutoTest.java
+++ src/test/java/step3/LottoAutoTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import step3.domain.Lotto;
 import step3.domain.Lottos;
 import step3.domain.Shop;
+import step3.lottoPlace.LastWeekLotto;
 import step3.lottoPlace.LottoPlace;
 import step3.lottoPlace.LottoPlaceChecker;
 import step3.view.InputView;
@@ -27,19 +28,19 @@ public class LottoAutoTest {
 
     @ParameterizedTest
     @DisplayName("지난주 로또 금액을 제대로 입력}")
-    @CsvSource(value = {"1,2,3,4,5,6", "10,11,12,13,14,15", "45,44,43,42,41,40"}, delimiter = ':')
-    public void successLottoInput(String input) {
+    @CsvSource(value = {"1,2,3,4,5,6:7", "10,11,12,13,14,15:16", "45,44,43,42,41,40:39"}, delimiter = ':')
+    public void successLottoInput(String input, int bonus) {
         //given, when, then
-        InputView.getLastWeekLottoNum(input);
+        InputView.getLastWeekLottoNum(input, bonus);
     }
 
     @ParameterizedTest
     @DisplayName("지난주 로또 금액을 잘못 입력")
-    @CsvSource(value = {"1,2,3,4,5", "-10,11,12,13,14,15", "46,45,44,43,42,41,40,39"}, delimiter = ':')
-    public void failLottoInput(String input) {
+    @CsvSource(value = {"1,2,3,4,5:6", "-10,11,12,13,14,15:16", "46,45,44,43,42,41,40,39:38"}, delimiter = ':')
+    public void failLottoInput(String input, int bonus) {
         //given, when, then
         assertThatThrownBy(() -> {
-            InputView.getLastWeekLottoNum(input);
+            InputView.getLastWeekLottoNum(input, bonus);
         }).isInstanceOf(RuntimeException.class);
     }
 
@@ -62,7 +63,8 @@ public class LottoAutoTest {
 
         //when
         Lotto lotto = Lotto.createLotto();
-        int lottoSize = (int) lotto.getLottoNums().stream()
+        int lottoSize = (int) lotto.getLottoNums()
+            .stream()
             .distinct()
             .count();
 
@@ -72,18 +74,19 @@ public class LottoAutoTest {
 
     @ParameterizedTest
     @DisplayName("일치하는 번호 갯수로 등수를 구한다")
-    @CsvSource(value = {"3:FORTH", "4:THIRD", "5:SECOND", "6:FIRST"}, delimiter = ':')
-    public void getPlaceByDistinctNum(int correctNum, LottoPlace expected) {
+    @CsvSource(value = {"3:false:FIFTH", "4:true:FORTH", "5:false:THIRD", "5:true:SECOND", "6:true:FIRST"}, delimiter = ':')
+    public void getPlaceByDistinctNum(int correctNum, boolean isBonusNumCorrect, LottoPlace expected) {
         //given, when
-        LottoPlace lottoPlace = LottoPlace.findPlaceByCorrectNum(correctNum);
+        LottoPlace lottoPlace = LottoPlace.findPlaceByCorrectNum(correctNum, isBonusNumCorrect);
 
         //then
         assertThat(lottoPlace).isEqualTo(expected);
     }
-//
+
+    //
     @ParameterizedTest
     @DisplayName("등수에 따라 상금을 구한다")
-    @CsvSource(value = {"FORTH:5000", "THIRD:50000", "SECOND:1500000", "FIRST:2000000000"}, delimiter = ':')
+    @CsvSource(value = {"FIFTH:5000", "FORTH:50000", "THIRD:1500000", "SECOND:30000000", "FIRST:2000000000"}, delimiter = ':')
     public void getPriceByPlace(LottoPlace lottoPlace, int expected) {
         //given, when
         int price = lottoPlace.getPrice();
@@ -94,11 +97,15 @@ public class LottoAutoTest {
 
     @ParameterizedTest
     @DisplayName("구매한 로또들의 전체 상금을 구한다")
-    @CsvSource(value = {"FIRST:SECOND:2001500000", "THIRD:FORTH:55000", "FIRST:FORTH:2000005000"}, delimiter = ':')
+    @CsvSource(value = {"FIRST:SECOND:2030000000", "THIRD:FORTH:1550000", "FIRST:FORTH:2000050000"}, delimiter = ':')
     public void getTotalPrice(LottoPlace place1, LottoPlace place2, long expected) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         //given
-        LottoPlaceChecker lottoPlaceChecker = LottoPlaceChecker.of(Arrays.asList(1,2,3,4,5,6));
-        Method calculateTotalPrice = lottoPlaceChecker.getClass().getDeclaredMethod("calculateTotalPrice", List.class);
+        LottoPlaceChecker lottoPlaceChecker = LottoPlaceChecker.of(LastWeekLotto.of(
+            Arrays.asList(1, 2, 3, 4, 5, 6),
+            7
+        ));
+        Method calculateTotalPrice = lottoPlaceChecker.getClass()
+            .getDeclaredMethod("calculateTotalPrice", List.class);
         calculateTotalPrice.setAccessible(true);
 
         //when
@@ -113,7 +120,10 @@ public class LottoAutoTest {
     @MethodSource("getRateOfTotalPriceSource")
     public void getRateOfTotalPrice(List<LottoPlace> lottoPlaces, int cost, BigDecimal expected) {
         //given
-        LottoPlaceChecker lottoPlaceChecker = LottoPlaceChecker.of(Arrays.asList(1,2,3,4,5,6));
+        LottoPlaceChecker lottoPlaceChecker = LottoPlaceChecker.of(LastWeekLotto.of(
+            Arrays.asList(1, 2, 3, 4, 5, 6),
+            7
+        ));
 
         //when
         BigDecimal result = lottoPlaceChecker.calculateWinnerRate(lottoPlaces, cost);
@@ -124,9 +134,9 @@ public class LottoAutoTest {
 
     static Stream<Arguments> getRateOfTotalPriceSource() {
         return Stream.of(
-            Arguments.arguments(Arrays.asList(LottoPlace.FIRST, LottoPlace.SECOND), 2000, BigDecimal.valueOf(1000750)),
-            Arguments.arguments(Arrays.asList(LottoPlace.THIRD, LottoPlace.FORTH), 2000, BigDecimal.valueOf(27.5)),
-            Arguments.arguments(Arrays.asList(LottoPlace.FIRST, LottoPlace.FORTH), 2000, BigDecimal.valueOf(1000002.5))
+            Arguments.arguments(Arrays.asList(LottoPlace.FIRST, LottoPlace.SECOND), 2000, BigDecimal.valueOf(1015000)),
+            Arguments.arguments(Arrays.asList(LottoPlace.THIRD, LottoPlace.FORTH), 2000, BigDecimal.valueOf(775)),
+            Arguments.arguments(Arrays.asList(LottoPlace.FIRST, LottoPlace.FORTH), 2000, BigDecimal.valueOf(1000025))
         );
     }
 }
