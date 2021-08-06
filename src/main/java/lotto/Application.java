61066e49713f16036dcf73b20b64db0cb5423225
hiro <kiryuinh32@gmail.com>
2021-08-06 16:57:00 +0900

    feat: view 테스트를 위한 main메서드 생성

---
+++ src/main/java/lotto/Application.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+public class Application {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int money = inputView.askPurchaseAmount();
+        LottoMachine lottoMachine = new LottoMachine(money);
+        ResultView resultView = new ResultView();
+        resultView.printLotties(lottoMachine);
+    }
+}
