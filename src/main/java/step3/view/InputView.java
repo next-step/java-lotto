d0547292b1b92ee5f354b97c089c90fd2825be84
rkdals213 <rkdals213@naver.com>
2021-08-06 14:39:01 +0900

    test: 보너스 번호 추가 입력 및 validate 로직 추가

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -2,6 +2,7 @@ package step3.view;
 
 import step3.domain.Lottos;
 import step3.domain.Shop;
+import step3.lottoPlace.LastWeekLotto;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,20 +26,27 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public static List<Integer> lastWeekLottoNum() {
+    public static LastWeekLotto lastWeekLottoNum() {
         viewLastWeekLottoNum();
         String lastWeekLottoNum = scanner.next();
-        return getLastWeekLottoNum(lastWeekLottoNum);
+        viewBonusLottoNum();
+        int bonusNum = scanner.nextInt();
+        return getLastWeekLottoNum(lastWeekLottoNum, bonusNum);
     }
 
-    static public void viewLastWeekLottoNum() {
+    public static void viewLastWeekLottoNum() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
     }
 
-    public static List<Integer> getLastWeekLottoNum(String text) {
+    public static void viewBonusLottoNum() {
+        System.out.println("보너스 볼을 입력해 주세요..");
+    }
+
+    public static LastWeekLotto getLastWeekLottoNum(String text, int bonusNum) {
         List<Integer> nums = split(text);
         validateLottoNum(nums);
-        return nums;
+        validateBonusNum(bonusNum);
+        return LastWeekLotto.of(nums, bonusNum);
     }
 
     private static List<Integer> split(String text) {
@@ -46,12 +54,22 @@ public class InputView {
     }
 
     private static void validateLottoNum(List<Integer> nums) {
-        if (nums.stream().anyMatch(i -> i > 45 || i <= 0)) {
+        if (nums.stream().anyMatch(i -> !inRange(i))) {
             throw new RuntimeException("로또 번호는 1 ~ 45 사이의 번호여야 합니다");
         }
 
         if (nums.size() != LOTTO_NUM_SIZE) {
             throw new RuntimeException("로또 번호의 갯수는 6개여야 합니다");
         }
+    }
+
+    private static void validateBonusNum(int bonusNum) {
+        if (!inRange(bonusNum)) {
+            throw new RuntimeException("로또 번호는 1 ~ 45 사이의 번호여야 합니다");
+        }
+    }
+
+    private static boolean inRange(int num) {
+        return num <= 45 && num > 0;
     }
 }
