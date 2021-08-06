4c18865d0d7143b3821b94cb84bf71491a04534f
hiro <kiryuinh32@gmail.com>
2021-08-06 16:54:37 +0900

    feat: 난수 생성을 위한 상수 변수 추가

--- src/main/java/lotto/RandomNumGenerator.java
+++ src/main/java/lotto/RandomNumGenerator.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class RandomNumGenerator implements NumberGenerator {
     private static final Random RANDOM = new Random();
     private static final int LOTTO_NUMBERS_COUNT = 6;
+    private static final int LOTTO_MAX_NUM = 44;
+
 
     private final Set<Integer> randomNumbers;
 
@@ -15,7 +17,7 @@ public class RandomNumGenerator implements NumberGenerator {
     @Override
     public Set<Integer> generateNumber() {
         while (randomNumbers.size() < LOTTO_NUMBERS_COUNT) {
-            randomNumbers.add(RANDOM.nextInt());
+            randomNumbers.add(RANDOM.nextInt(LOTTO_MAX_NUM) + 1);
         }
         return Collections.unmodifiableSet(randomNumbers);
     }
