910ecbb8d2485bbddf6f00748da833d28cd8f5be
0n1dev <all_mind@naver.com>
2021-08-07 01:49:16 +0900

    refactor : 메서드 분리
    - 메서드 분리

--- src/main/java/LottoMain.java
+++ src/main/java/LottoMain.java
@@ -13,34 +13,40 @@ public class LottoMain {
 
 	private static int money;
 
-	private static LottoCount lottoCount;
+	private static Lottos lottos;
 
 	public static void main(String[] args) {
 
-		money = InputView.inputMoney();
-		lottoCount = new LottoCount(money);
+		buyLotto(InputView.inputMoney());
 
-		ResultView.outputLottoCount(lottoCount);
-
-		Lottos lottos = Lottos.generateAutoLottos(lottoCount);
-
-		lottos.getLottos()
-			.forEach(ResultView::outputLottoNumbers);
-
-		String winLottoNumbers = InputView.inputWinLottoNumbers();
-
-		List<Integer> lottoNumbers = Arrays.stream(winLottoNumbers.split(","))
-			.map(String::trim)
-			.mapToInt(Integer::parseInt)
-			.boxed()
-			.collect(Collectors.toList());
-
-		Lotto winLotto = new Lotto(lottoNumbers);
-
+		Lotto winLotto = winLotto(InputView.inputWinLottoNumbers());
 		Records records = lottos.toRevenueRecord().aggregate(winLotto);
 
 		records.getRecords().forEach((key, value) -> System.out.println(key.getMatchNumberCount() + "개 일치 (" + key.getReward() + ") - " + value + "개"));
 
 		System.out.println(records.sumRevenue());
+	}
+
+	private static void buyLotto(int money) {
+		LottoMain.money = money;
+		LottoCount lottoCount = new LottoCount(money);
+
+		ResultView.outputLottoCount(lottoCount.getLottoCount());
+		LottoMain.lottos = Lottos.generateAutoLottos(lottoCount.getLottoCount());
+
+		lottos.getLottos()
+			.forEach(ResultView::outputLottoNumbers);
+	}
+
+	private static Lotto winLotto(String winLottoNumbers) {
+		String[] stringLottoNumbers = winLottoNumbers.split(",");
+
+		List<Integer> lottoNumbers = Arrays.stream(stringLottoNumbers)
+										.map(String::trim)
+										.mapToInt(Integer::parseInt)
+										.boxed()
+										.collect(Collectors.toList());
+
+		return new Lotto(lottoNumbers);
 	}
 }
