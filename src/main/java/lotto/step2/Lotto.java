236f114ae01af0f39725102b1b9f7f69176028e8
gunny6026 <gunny6026@naver.com>
2021-08-06 22:03:17 +0900

    feat : lotto 구입

---
+++ src/main/java/lotto/step2/Lotto.java
@@ -0,0 +1,23 @@
+package lotto.step2;
+
+import java.util.ArrayList;
+
+public class Lotto {
+
+    private ArrayList<ArrayList<Integer>> lotto;
+
+    public Lotto() {
+        this.lotto = new ArrayList<ArrayList<Integer>>();
+    }
+
+    public ArrayList buyLotto(int price) {
+        if (price < 1000) {
+            throw new IllegalArgumentException("돈이 부족합니다. 로또 1장의 가격은 1000원입니다.");
+        }
+        int lottoSize = price / 1000;
+        for (int i=0; i< lottoSize; i++) {
+            lotto.add(LottoMachine.mixLottoNumbers());
+        }
+        return lotto;
+    }
+}
