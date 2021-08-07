326047f56bd1b51d14eb22d2191ebd8d6265195d
shirohoo <shirohoo4102@gmail.com>
2021-08-08 00:53:55 +0900

    refactor: LottoConsoleApplication 코드 정리

--- src/main/java/lotto/LottoConsoleApplication.java
+++ src/main/java/lotto/LottoConsoleApplication.java
@@ -5,45 +5,29 @@ import lotto.model.LottoShop;
 import lotto.model.LottoStatistics;
 import lotto.model.Lottos;
 import lotto.model.WinningLotto;
-import lotto.prize.LottoPrize;
 import lotto.view.InputView;
 import lotto.view.ResultView;
-
-import java.util.List;
 
 import static lotto.model.LottoShop.LOTTO_PRICE;
 import static lotto.model.LottoShop.getInstance;
 
 public class LottoConsoleApplication {
-    public static void main(String[] args) {
-        final LottoShop shop = getInstance();
-        final int payment = InputView.inputPayment();
+    private static final LottoShop LOTTO_SHOP = getInstance();
 
+    public static void main(String[] args) {
+        final int payment = InputView.inputPayment();
         final int manualLottoCount = InputView.inputManualLottoCount();
 
         validate(payment, manualLottoCount);
 
-        final String[] manualNumbers = InputView.inputManualLottoNumbers(manualLottoCount);
-
-        newLine();
-
         final int autoNumberOfPurchases = getAutoNumberOfPurchases(payment, manualLottoCount);
 
-        final Lottos lottos = shop.buy(payment, autoNumberOfPurchases, manualNumbers);
-
-        ResultView.printGameCount(manualLottoCount, autoNumberOfPurchases);
-
-        ResultView.printLottoNumber(lottos);
-
-        final String winningNumbers = InputView.inputWinningNumbers();
-
-        final String bonusNumber = InputView.inputBonusNumber();
-
-        final WinningLotto winningLotto = WinningLotto.from(winningNumbers, bonusNumber);
-        final List<LottoPrize> lottoPrizes = winningLotto.matchResults(lottos);
-        final LottoStatistics lottoStatistics = LottoStatistics.from(payment, lottoPrizes);
-
-        ResultView.printStatistic(lottoStatistics);
+        printGameInformation(payment,
+                             manualLottoCount,
+                             autoNumberOfPurchases,
+                             LOTTO_SHOP.buy(payment,
+                                            autoNumberOfPurchases,
+                                            InputView.inputManualLottoNumbers(manualLottoCount)));
     }
 
     private static void validate(final int payment, final int manualLottoCount) {
@@ -52,13 +36,15 @@ public class LottoConsoleApplication {
         }
     }
 
-    private static void newLine() {
-        System.out.println();
+    private static int getAutoNumberOfPurchases(final int payment, final int manualLottoCount) {
+        return (payment - manualLottoCount * LOTTO_PRICE) / LOTTO_PRICE;
     }
 
-    private static int getAutoNumberOfPurchases(final int payment, final int manualLottoCount) {
-        final int manualLottoPayment = manualLottoCount * LOTTO_PRICE;
-        final int autoNumberOfPurchases = (payment - manualLottoPayment) / LOTTO_PRICE;
-        return autoNumberOfPurchases;
+    private static void printGameInformation(final int payment, final int manualLottoCount, final int autoNumberOfPurchases, final Lottos lottos) {
+        ResultView.printGameCount(manualLottoCount, autoNumberOfPurchases);
+        ResultView.printLottoNumber(lottos);
+        ResultView.printStatistic(LottoStatistics.from(payment,
+                                                       WinningLotto.from(InputView.inputWinningNumbers(),
+                                                                         InputView.inputBonusNumber()).matchResults(lottos)));
     }
 }
