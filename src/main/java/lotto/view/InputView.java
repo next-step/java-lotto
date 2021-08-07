b6a10aaccb91e20ef1e22c277d7d184ad59d8919
hiro <kiryuinh32@gmail.com>
2021-08-07 16:18:31 +0900

    view: 입/출력을 위한 view 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String FIRST_MESSAGE = "구입금액을 입력해 주세요.\n";
+    private static final String ASK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.\n\n";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public int askPurchaseAmount() {
         System.out.printf(FIRST_MESSAGE);
         return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public String getWinningNumber() {
+        System.out.print(ASK_WINNING_NUMBER);
+        return SCANNER.nextLine();
     }
 
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -2,18 +2,31 @@ package lotto.view;
 
 import lotto.Lotto;
 import lotto.LottoMachine;
-
-import java.util.Scanner;
+import lotto.SameNumbersCount;
 
 public class ResultView {
     private static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.\n";
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String WINNING_STATISTICS = "당첨 통계\n";
+    private static final String LINE = "-------\n";
 
     public void printLotties(LottoMachine lottoMachine) {
         System.out.printf(PURCHASE_MESSAGE, lottoMachine.getLottoCount());
+
         for (int i = 0; i < lottoMachine.lottiesCount(); i++) {
             Lotto lotto = lottoMachine.getLotties().getLotties().get(i);
-            System.out.println(lotto.getLottoNums().toString());
+            System.out.println(lotto.getLottoNumber().toString());
+        }
+    }
+
+    public void printSameNumbers() {
+        System.out.print(WINNING_STATISTICS);
+        System.out.print(LINE);
+
+        for (SameNumbersCount s : SameNumbersCount.values()) {
+            System.out.printf("%d개 일치 (%d원)- ", s.getSameNumbersCount(), s.getPrizeMoney());
+            System.out.printf("%d개", s.getCount());
+            System.out.println();
+
         }
     }
 }
