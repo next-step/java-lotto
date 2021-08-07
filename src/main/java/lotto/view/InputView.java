2e3480749d32994dd841996c572f5d2d41fdb28e
harris <harrisleesh@gmail.com>
2021-08-08 00:57:56 +0900

    feat : Create InputView and ResultView

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import lotto.domain.LottoNumbers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int requestPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static LottoNumbers requestWinnerNumbers() {
+        System.out.println();
+        System.out.println("지난 주 당첨 번호를 입력해주세요.");
+        return new LottoNumbers(Arrays.stream(scanner.nextLine().split(",")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList()));
+    }
+}
---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,38 @@
+package lotto.view;
+
+import lotto.domain.LottoGame;
+import lotto.domain.Award;
+import lotto.domain.Lottos;
+
+public class ResultView {
+    public static void printPurchaseLottos(Lottos lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        System.out.println(lottos.toString());
+    }
+
+    public static void printWinners(LottoGame lottoGame) {
+        System.out.println("당첨 통계");
+        System.out.println("----------");
+        printWinnersByAward(Award.FOURTH, lottoGame);
+        printWinnersByAward(Award.THIRD, lottoGame);
+        printWinnersByAward(Award.SECOND, lottoGame);
+        printWinnersByAward(Award.FIRST, lottoGame);
+
+    }
+
+    private static void printWinnersByAward(Award award, LottoGame lottoGame) {
+        System.out.println(award.getMatchNumbers() + "개 일치 (" + award.getAmount() + "원)- " + lottoGame.countWinners(award));
+    }
+
+    public static void printYield(LottoGame lottoGame, int amount) {
+        double yield = getYield(lottoGame, amount);
+        System.out.println("총 수익률은 " + String.format("%.2f", yield) + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+    }
+
+    private static double getYield(LottoGame lottoGame, int amount) {
+        return (double) (Award.FOURTH.getAmount() * lottoGame.countWinners(Award.FOURTH) +
+                Award.THIRD.getAmount() * lottoGame.countWinners(Award.THIRD) +
+                Award.SECOND.getAmount() * lottoGame.countWinners(Award.SECOND) +
+                Award.FIRST.getAmount() * lottoGame.countWinners(Award.FIRST)) / (double) amount;
+    }
+}
