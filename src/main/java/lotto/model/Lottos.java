0813441b12e8fde58c08738650aad494313a6de4
YooInKeun <keun0390@naver.com>
2021-08-07 18:52:12 +0900

    refactor: 로또 가격 상수명 리네이밍

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -6,22 +6,22 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class Lottos {
-    private static final int ONE_LOTTO_PRICE = 1_000;
+    private static final int LOTTO_PRICE = 1_000;
 
     private final List<Lotto> lottos;
 
     public Lottos(int purchaseAmount) {
         validatePurchasableAmount(purchaseAmount);
 
-        int lottoCount = purchaseAmount / ONE_LOTTO_PRICE;
+        int lottoCount = purchaseAmount / LOTTO_PRICE;
         this.lottos = Stream.generate(Lotto::new)
                 .limit(lottoCount)
                 .collect(toList());
     }
 
     private void validatePurchasableAmount(int purchaseAmount) {
-        if (purchaseAmount < ONE_LOTTO_PRICE) {
-            throw new IllegalArgumentException(String.format("구입금액은 최소 %d(원) 이상이어야 합니다.", ONE_LOTTO_PRICE));
+        if (purchaseAmount < LOTTO_PRICE) {
+            throw new IllegalArgumentException(String.format("구입금액은 최소 %d(원) 이상이어야 합니다.", LOTTO_PRICE));
         }
     }
 
