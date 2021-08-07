1c7815835fea23db6bb23c9a5ff513f842f4f953
harris <harrisleesh@gmail.com>
2021-08-07 20:32:25 +0900

    feat : Create Lotto entity with ListSortOrMixStrategy

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import lotto.strategy.ListSortOrMixStrategy;
+import lotto.util.LottoNumberGenerator;
+
+public class Lotto {
+    private LottoNumbers lottoNumbers;
+    private Award award = Award.UNIDENTIFIED;
+
+    public Lotto(ListSortOrMixStrategy listSortOrMixStrategy) {
+        lottoNumbers = LottoNumberGenerator.getLottoNumbers(listSortOrMixStrategy);
+    }
+
+    public void drawLotto(LottoNumbers winnerNumbers) {
+        award = Award.findBy(lottoNumbers.sameAs(winnerNumbers));
+    }
+
+    public boolean isWinner(Award award) {
+        return this.award == award ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumbers.toString();
+    }
+
+    public Award getAward() {
+        return award;
+    }
+
+    public LottoNumbers getNumbers() {
+        return lottoNumbers;
+    }
+}
