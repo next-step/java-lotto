19d70f16f18373cd60b04c13793012aecc54e234
hiro <kiryuinh32@gmail.com>
2021-08-07 16:19:00 +0900

    refactor: 테스트를 위한 main 메서드수 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,8 +7,15 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         int money = inputView.askPurchaseAmount();
+
         LottoMachine lottoMachine = new LottoMachine(money);
+
         ResultView resultView = new ResultView();
         resultView.printLotties(lottoMachine);
+
+        String winningNumber = inputView.getWinningNumber();
+        lottoMachine.confirmLotto(winningNumber);
+
+        resultView.printSameNumbers();
     }
 }
