75121b05e8c1d245a9833d77c1c741561e5633a3
ChangSubKwak <paradise1732@gmail.com>
2021-08-07 10:13:15 +0900

    test: 일급컬렉션 Tokens클래스의 테스트 케이스 추가

---
+++ src/test/java/calculator/TokensTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TokensTest {
+    @Test
+    void 토큰들중에서_음수를_포함하고_있으면_예외를_던진다() {
+        assertThat(1).isEqualTo(1);
+        assertThatThrownBy(() -> new Tokens(new String[]{"1", "2", "-1"}))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 토큰들을_입력으로_받으면_합을_반환한다() {
+        assertThat(new Tokens(new String[] {"1", "3", "5"}).sum()).isEqualTo(9);
+    }
+}
