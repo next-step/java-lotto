1af91948a70bf254143792b7d9bb0e384e0728ab
PIH6585 <pih6585@naver.com>
2021-08-07 17:43:57 +0900

    refactor : 상수화 추가

--- src/main/java/lotto/service/SearchPrize.java
+++ src/main/java/lotto/service/SearchPrize.java
@@ -11,14 +11,17 @@ import lotto.model.WinPrizes;
 
 public class SearchPrize {
 
+	public static final int ADDITION_PRIZE_COUNT = 1;
+	public static final int INITIAL_PRIZE_COUNT = 0;
+
 	public static WinPrizes drawWinPrize(Lottos lottos, Lotto winnerLotto) {
 		Map<Prize, Integer> winPrizes = Prize.getValuesMoneySort()
 			.stream()
-			.collect(Collectors.toMap(prize -> prize, prize -> 0, (a, b) -> b, LinkedHashMap::new));
+			.collect(Collectors.toMap(prize -> prize, prize -> INITIAL_PRIZE_COUNT, (a, b) -> b, LinkedHashMap::new));
 		for (Lotto lotto : lottos.getLottos()) {
 			Prize winPrize = Prize.getWinnersStatus(
 				(int)lotto.getLottoNumbers().stream().filter(winnerLotto.getLottoNumbers()::contains).count());
-			winPrizes.put(winPrize, winPrizes.get(winPrize) + 1);
+			winPrizes.put(winPrize, winPrizes.get(winPrize) + ADDITION_PRIZE_COUNT);
 		}
 		return new WinPrizes(winPrizes);
 	}
