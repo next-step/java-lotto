4faeb47c14442dd38b33606f9e45718f4cb6137e
ChangSubKwak <paradise1732@gmail.com>
2021-08-09 06:53:52 +0900

    fix: Token에서 PositiveNumber으로 클래스명 변경에 따른 수정

--- src/test/java/calculator/PositiveNumbersTest.java
+++ src/test/java/calculator/PositiveNumbersTest.java
@@ -10,7 +10,7 @@ class TokensTest {
     void 토큰들중에서_음수를_포함하고_있으면_예외를_던진다() {
         assertThat(1).isEqualTo(1);
         assertThatThrownBy(() -> new Tokens(new String[]{"1", "2", "-1"}))
-                .isInstanceOf(RuntimeException.class);
+            .isInstanceOf(RuntimeException.class);
     }
 
     @Test
