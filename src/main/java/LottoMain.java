d64eb9f9873334e9b9dff84a1b2bfb0f3c0d4046
0n1dev <all_mind@naver.com>
2021-08-08 15:06:23 +0800

    refactor : LottoController 추가
    - LottoController 추가

--- src/main/java/LottoMain.java
+++ src/main/java/LottoMain.java
@@ -1,56 +1,26 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import lotto.domain.Lotto;
-import lotto.domain.LottoCount;
-import lotto.domain.Lottos;
+import controller.LottoController;
 import lotto.domain.Records;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
 public class LottoMain {
 
-	private static int money;
-
-	private static Lottos lottos;
-
 	public static void main(String[] args) {
-		buyLotto(InputView.inputMoney());
+
+		LottoController lottoController = new LottoController(InputView.inputMoney());
+
+		lottoController.buyLotto();
 
 		ResultView.outputNewLine();
-		Lotto winLotto = winLotto(InputView.inputWinLottoNumbers());
+
+		lottoController.winLotto(InputView.inputWinLottoNumbers());
+
 		ResultView.outputNewLine();
 
-		Records records = lottos.toRevenueRecord().aggregate(winLotto);
-
-		outputStatistics(records);
+		outputStatistics(lottoController.getMoney(), lottoController.getRecords());
 	}
 
-	private static void buyLotto(int money) {
-		LottoMain.money = money;
-		LottoCount lottoCount = new LottoCount(money);
-
-		ResultView.outputLottoCount(lottoCount.getLottoCount());
-		LottoMain.lottos = Lottos.generateAutoLottos(lottoCount.getLottoCount());
-
-		lottos.getLottos()
-			.forEach(ResultView::outputLottoNumbers);
-	}
-
-	private static Lotto winLotto(String winLottoNumbers) {
-		String[] stringLottoNumbers = winLottoNumbers.split(",");
-
-		List<Integer> lottoNumbers = Arrays.stream(stringLottoNumbers)
-										.map(String::trim)
-										.mapToInt(Integer::parseInt)
-										.boxed()
-										.collect(Collectors.toList());
-
-		return new Lotto(lottoNumbers);
-	}
-
-	private static void outputStatistics(Records records) {
+	private static void outputStatistics(int money, Records records) {
 		ResultView.outputStatisticsTitle();
 		records.getRecords().forEach(ResultView::outputLottoNumbers);
 		ResultView.outputYield(money, records.sumRevenue());
---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoCount;
+import lotto.domain.Lottos;
+import lotto.domain.Records;
+import lotto.view.ResultView;
+
+public class LottoController {
+
+	private final int money;
+	private final LottoCount lottoCount;
+	private Lottos lottos;
+	private Lotto winLotto;
+
+	public LottoController(int money) {
+		this.money = money;
+		this.lottoCount = new LottoCount(this.money);
+	}
+
+	public void buyLotto() {
+		ResultView.outputLottoCount(lottoCount.getLottoCount());
+		this.lottos = Lottos.generateAutoLottos(lottoCount.getLottoCount());
+
+		lottos.getLottos()
+			.forEach(ResultView::outputLottoNumbers);
+	}
+
+	public void winLotto(String winLottoNumbers) {
+		String[] stringLottoNumbers = winLottoNumbers.split(",");
+
+		List<Integer> lottoNumbers = Arrays.stream(stringLottoNumbers)
+			.map(String::trim)
+			.mapToInt(Integer::parseInt)
+			.boxed()
+			.collect(Collectors.toList());
+
+		this.winLotto = new Lotto(lottoNumbers);
+	}
+
+	public Records getRecords() {
+		return lottos.toRevenueRecord().aggregate(winLotto);
+	}
+
+	public int getMoney() {
+		return money;
+	}
+}
