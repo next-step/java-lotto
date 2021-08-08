d1491ef147e2f3e8b0e83c70cc565f2e04f229e8
rkdals213 <rkdals213@naver.com>
2021-08-09 00:29:14 +0900

    feat: 수동 로또 구매 메소드 추가 및 검증 로직 분리

--- src/main/java/step4/domain/Shop.java
+++ src/main/java/step4/domain/Shop.java
@@ -1,5 +1,9 @@
 package step4.domain;
 
+import step4.validator.LottoValidator;
+
+import java.util.List;
+
 public class Shop {
     public static final int LOTTO_PRICE = 1000;
 
@@ -7,20 +11,23 @@ public class Shop {
     }
 
     public static Lottos buyLotto(int money) {
-        if (!isAvailableMoney(money)) {
-            throw new RuntimeException("잘못된 금액을 입력하셨습니다.");
-        }
+        LottoValidator.validateMoney(money);
 
         int count = getLottoCount(money);
 
         return Lottos.createLottos(count, LOTTO_PRICE * count);
     }
 
-    private static int getLottoCount(int money) {
-        return money / LOTTO_PRICE;
+    public static Lottos buyLotto(int money, List<List<Integer>> manualLottoNums) {
+        LottoValidator.validateMoney(money);
+
+        int count = getLottoCount(money) - manualLottoNums.size();
+        LottoValidator.validateManualCount(count);
+
+        return Lottos.createLottos(count, manualLottoNums, LOTTO_PRICE * count);
     }
 
-    private static boolean isAvailableMoney(int money) {
-        return money >= 0;
+    private static int getLottoCount(int money) {
+        return money / LOTTO_PRICE;
     }
 }
