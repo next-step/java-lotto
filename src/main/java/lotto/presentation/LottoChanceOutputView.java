47b440e3206228efd26e5371f52f62fcc93342c0
sangnyyy <dltkdals2202@gmail.com>
2021-08-08 11:10:41 +0900

    feat : 구매갯수를 출력하는 View 생성

---
+++ src/main/java/lotto/presentation/LottoChanceOutputView.java
@@ -0,0 +1,10 @@
+package lotto.presentation;
+
+public class LottoChanceOutputView {
+    private static final String LOTTO_CHANCE_OUTPUT_COMMENT = "%d개를 구매했습니다.";
+
+    public void output(int chance){
+        System.out.println(String.format(LOTTO_CHANCE_OUTPUT_COMMENT, chance));
+    }
+
+}
--- src/main/java/lotto/presentation/PurchaseAmountInputView.java
+++ src/main/java/lotto/presentation/PurchaseAmountInputView.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class PurchaseAmountInputView {
 
-    private static final String INPUT_PURCHASE_AMOUNT_COMMENT = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_INPUT_COMMENT = "구입금액을 입력해 주세요.";
 
     public int input(){
-        System.out.println(INPUT_PURCHASE_AMOUNT_COMMENT);
+        System.out.println(PURCHASE_AMOUNT_INPUT_COMMENT);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
