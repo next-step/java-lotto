5886fc9bd2413e9db5941aa50364f75b97a9fd27
sangnyyy <dltkdals2202@gmail.com>
2021-08-08 23:57:43 +0900

    feat : 총 수익률을 보여주는 View 생성

--- src/main/java/lotto/presentation/output/ChanceOutputView.java
+++ src/main/java/lotto/presentation/output/ChanceOutputView.java
@@ -1,10 +1,10 @@
 package lotto.presentation.output;
 
 public class ChanceOutputView {
-    private static final String LOTTO_CHANCE_OUTPUT_COMMENT = "%d개를 구매했습니다.";
+    private static final String LOTTO_CHANCE_OUTPUT_COMMENT = "%d개를 구매했습니다.%n";
 
-    public void output(int chance){
-        System.out.println(String.format(LOTTO_CHANCE_OUTPUT_COMMENT, chance));
+    public void output(int chance) {
+        System.out.printf(LOTTO_CHANCE_OUTPUT_COMMENT, chance);
     }
 
 }
--- src/main/java/lotto/presentation/output/WinningStatisticsOutputView.java
+++ src/main/java/lotto/presentation/output/WinningStatisticsOutputView.java
@@ -4,12 +4,16 @@ import lotto.common.MatchCount;
 
 public class WinningStatisticsOutputView {
     private static final String WINNING_STATISTICS_OUTPUT_COMMENT = "당첨 통계\n---------";
+    private static final String THREE_MATCHES_COMMENT = "3개 일치 (5000원)- %d개%n";
+    private static final String FOUR_MATCHES_COMMENT = "4개 일치 (50000원)- %d개%n";
+    private static final String FIVE_MATCHES_COMMENT = "5개 일치 (1500000원)- %d개%n";
+    private static final String SIX_MATCHES_COMMENT = "6개 일치 (2000000000원)- %d개%n";
 
     public void output(MatchCount matchCount) {
         System.out.println(WINNING_STATISTICS_OUTPUT_COMMENT);
-        System.out.printf("3개 일치 (5000원)- %d개%n", matchCount.getThreeMatches());
-        System.out.printf("4개 일치 (50000원)- %d개%n", matchCount.getFourMatches());
-        System.out.printf("5개 일치 (1500000원)- %d개%n", matchCount.getFiveMatches());
-        System.out.printf("6개 일치 (2000000000원)- %d개%n", matchCount.getSixMatches());
+        System.out.printf(THREE_MATCHES_COMMENT, matchCount.getThreeMatches());
+        System.out.printf(FOUR_MATCHES_COMMENT, matchCount.getFourMatches());
+        System.out.printf(FIVE_MATCHES_COMMENT, matchCount.getFiveMatches());
+        System.out.printf(SIX_MATCHES_COMMENT, matchCount.getSixMatches());
     }
 }
