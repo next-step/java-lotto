94dcf84a8e669e0696098c623cff0a0fb837d02b
minyul <ggomjae@gmail.com>
2021-08-08 23:21:23 +0900

    음수가 들어가있으면 RuntimeException를 던지는 테스크를 통과하다.

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -3,13 +3,26 @@ package domain;
 public class Calculator {
 
 	private static final int INIT_SUM = 0;
+	private static final String NUMBER_REGEXP = "^[0-9]+$";
 
 	public Integer calculate(final String input) {
 		if ("".equals(input) || input == null) {
 			return INIT_SUM;
 		}
+		if (!input.matches(NUMBER_REGEXP)) {
+			throw new RuntimeException("숫자이외의 문자가 들어가있습니다.");
+		}
 		String[] numbers = input.split(",|:");
+
 		return getResult(numbers);
+	}
+
+	private void checkNumbersStatus(final String[] numbers) {
+		for(String number : numbers) {
+			if (Integer.parseInt(number) < 0) {
+				throw new RuntimeException("음수가 들어가있습니다.");
+			}
+		}
 	}
 
 	private Integer getResult(final String[] numbers) {
