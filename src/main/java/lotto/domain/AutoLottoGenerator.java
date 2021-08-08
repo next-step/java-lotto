973517a63f85c06aa6771ec275b6ce7263f66129
shineoov <shineoov88@gmail.com>
2021-08-09 01:15:46 +0900

    fix: 로또(자동) 피드백 반영 - 자동생성 로또 셔플 개선
    - static 변수를 셔플 하지않고 복사해서 셔플

--- src/main/java/lotto/domain/AutoLottoGenerator.java
+++ src/main/java/lotto/domain/AutoLottoGenerator.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,22 +12,24 @@ public class AutoLottoGenerator implements LottoGenerator {
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_NUMBER_COUNT = 6;
 
-    private static final List<LottoNumber> targetNumbers = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+    private static final List<LottoNumber> lottoNumbers = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
             .boxed()
             .map(LottoNumber::new)
             .collect(Collectors.toList());
 
     @Override
     public LottoTicket generate() {
-        shuffleLottoNumber();
-        return pickLottoNumbers();
+        List<LottoNumber> targetNumbers = shuffleLottoNumbers();
+        return pickLottoNumbers(targetNumbers);
     }
 
-    private void shuffleLottoNumber() {
+    private List<LottoNumber> shuffleLottoNumbers() {
+        List<LottoNumber> targetNumbers = new ArrayList<>(lottoNumbers);
         Collections.shuffle(targetNumbers);
+        return targetNumbers;
     }
 
-    private LottoTicket pickLottoNumbers() {
+    private LottoTicket pickLottoNumbers(List<LottoNumber> targetNumbers) {
         List<LottoNumber> lottoNumbers = targetNumbers.stream()
                 .limit(LOTTO_NUMBER_COUNT)
                 .sorted()
