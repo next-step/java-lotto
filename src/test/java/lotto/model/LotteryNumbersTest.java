394f6598fbf7ad0392de66f80590cea1a14090d5
YooInKeun <keun0390@naver.com>
2021-08-08 22:59:28 +0900

    test: 당첨 번호, 보너스 번호 중복 예외 발생 테스트 코드 추가

---
+++ src/test/java/lotto/model/LotteryNumbersTest.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("추첨 번호 테스트")
+public class LotteryNumbersTest {
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 Exception이 발생한다.")
+    @Test
+    public void duplicatedWinningNumbersAndBonusNumberExceptionTest() {
+        // given
+        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoNumber bonusNumber = LottoNumber.valueOf(6);
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LotteryNumbers(winningNumbers, bonusNumber))
+                .withMessage("당첨 번호와 보너스 번호가 중복 되었습니다.");
+    }
+}
