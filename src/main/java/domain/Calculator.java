85e9bfd0ad33a09db3af02d4dde2901e82d8fb91
minyul <ggomjae@gmail.com>
2021-08-08 22:54:18 +0900

    Test에 맞는 Calculator를 생성하고 calculate를 작성한다.

---
+++ src/main/java/domain/Calculator.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Calculator {
+
+	private static final Integer INIT_SUM = 0;
+
+	public Integer calculate(final String input) {
+		String[] numbers = input.split(",|:");
+		return getResult(numbers);
+	}
+
+	private Integer getResult(final String[] numbers) {
+		Integer sum = INIT_SUM;
+		for (String number : numbers) {
+			sum += Integer.parseInt(number);
+		}
+		return sum;
+	}
+}
