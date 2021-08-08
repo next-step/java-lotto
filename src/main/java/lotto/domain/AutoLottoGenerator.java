080b6fa7656ae8f13c2401b4d82db4270372b44d
shineoov <shineoov88@gmail.com>
2021-08-08 13:50:08 +0900

    fix: 로또(자동) 피드백 반영 - 로또 자동 생성 구현체 개선
    - 로또 숫자 범위 매번 생성 하지않고 static 변수로 사용
    
    - static 키워드 누락된 부분 수정

--- src/main/java/lotto/domain/AutoLottoGenerator.java
+++ src/main/java/lotto/domain/AutoLottoGenerator.java
@@ -7,21 +7,13 @@ import java.util.stream.IntStream;
 
 public class AutoLottoGenerator implements LottoGenerator {
 
-    private final int LOTTO_MIN_NUMBER = 1;
-    private final int LOTTO_MAX_NUMBER = 45;
-    private final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
-    private List<Integer> targetNumbers;
-
-    public AutoLottoGenerator() {
-        generateTargetNumbers();
-    }
-
-    private void generateTargetNumbers() {
-        targetNumbers = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
-                .boxed()
-                .collect(Collectors.toList());
-    }
+    private static final List<Integer> targetNumbers = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
 
     @Override
     public LottoTicket generate() {
