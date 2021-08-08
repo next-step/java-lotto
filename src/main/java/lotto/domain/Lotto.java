19f6bf760053388e596ecf42ef00f5419f372c11
0n1dev <all_mind@naver.com>
2021-08-08 15:20:22 +0800

    refactor : 로또 도메인 리팩토링
    - 로또 도메인 리팩토링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,22 +2,25 @@ package lotto.domain;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import lotto.enums.Rank;
+import lotto.exception.LottoValidationException;
 
 public class Lotto {
 
 	private static final int LOTTO_NUMBERS_LENGTH = 6;
 
-	private final List<Integer> lottoNumbers;
+	private final Set<Integer> lottoNumbers;
 
 	public Lotto(List<Integer> lottoNumbers) {
 		if (lottoNumbers.size() != LOTTO_NUMBERS_LENGTH) {
-			throw new IllegalArgumentException("로또 번호는 " + LOTTO_NUMBERS_LENGTH + "개의 숫자여야 합니다.");
+			throw new LottoValidationException("로또 번호는 " + LOTTO_NUMBERS_LENGTH + "개의 숫자여야 합니다.");
 		}
 
-		this.lottoNumbers = lottoNumbers;
+		this.lottoNumbers = new TreeSet<>(lottoNumbers);
 	}
 
 	public Rank figureOutRank(Lotto winningLotto) {
---
+++ src/main/java/lotto/exception/LottoValidationException.java
@@ -0,0 +1,8 @@
+package lotto.exception;
+
+public class LottoValidationException extends RuntimeException {
+
+	public LottoValidationException(String message) {
+		super(message);
+	}
+}
