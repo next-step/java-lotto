3ac4e34479ecfcec6aaf0089b425f450bb23197e
harris <harrisleesh@gmail.com>
2021-08-08 13:13:59 +0900

    feat : Create equals, hashCode and toString methods in LottoNumber

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
     public static final int MAX_NUMBER = 45;
     public static final int MIN_NUMBER = 1;
@@ -14,5 +16,23 @@ public class LottoNumber {
 
     public Integer getLottoNumber() {
         return lottoNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return Objects.equals(lottoNumber, that.lottoNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumber.toString();
     }
 }
