be430653d6a3a120ea376b0c71c028820ef14505
sk1737030 <sk1737030@naver.com>
2021-08-08 12:42:52 +0900

    refactor: 필요없는 minmal제거

--- src/main/java/step3/domain/lotto/LottoStatistics.java
+++ src/main/java/step3/domain/lotto/LottoStatistics.java
@@ -4,18 +4,13 @@ import java.util.List;
 
 public class LottoStatistics {
 
-    private static final Integer MINIMAL_SUCCESS_NUMBER = 3;
-
     public static ResultOfLottos calcLottoOfStatistics(WinOfLotto winOfLottoNumber, List<Lotto> lottos) {
         ResultOfLottos resultOfLottos = new ResultOfLottos();
 
         for (Lotto lotto : lottos) {
             int count = winOfLottoNumber.correctCount(lotto.numbers());
             boolean hasBonusNumber = winOfLottoNumber.hasBonusNumber(lotto);
-
-            if (count >= MINIMAL_SUCCESS_NUMBER) {
-                resultOfLottos.put(LottoRank.find(count, hasBonusNumber));
-            }
+            resultOfLottos.put(LottoRank.find(count, hasBonusNumber));
         }
 
         return resultOfLottos;
