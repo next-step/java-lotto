2fd772d48dc395cb388b019ff6371056fa004bf0
ChangSubKwak <paradise1732@gmail.com>
2021-08-09 07:00:40 +0900

    fix: Token에서 PositiveNumber으로 클래스명 변경에 따른 수정, 정적팩터리 메서드 제거

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -1,26 +1,21 @@
 package calculator;
 
-public class Token {
-    private static final String NOT_VALID_ARGUMENT_EXIST_STATEMENT = "음수 또는 숫자이외 값이 존재합니다";
-    private final String token;
+public class PositiveNumber {
+    private static final String NOT_VALID_ARGUMENT_EXIST_STATEMENT = "음수 또는 0이 존재합니다";
+    private final int positiveNumber;
 
-    public Token(String token) {
-        validateNegativeNumber(token);
-        this.token = token;
+    public PositiveNumber(int positiveNumber) {
+        validate(positiveNumber);
+        this.positiveNumber = positiveNumber;
     }
 
-    private void validateNegativeNumber(String token) {
-        if (Integer.parseInt(token) < 0) {
+    private void validate(int positiveNumber) {
+        if (positiveNumber <= 0) {
             throw new RuntimeException(NOT_VALID_ARGUMENT_EXIST_STATEMENT);
         }
     }
 
-    public static Token create(String token) {
-        return new Token(token);
+    public int number() {
+        return this.positiveNumber;
     }
-
-    public int parseInt() {
-        return Integer.parseInt(this.token);
-    }
-
 }
