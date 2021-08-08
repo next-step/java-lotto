c141050bf6e314e98abe116968ad1aad711e99d9
0n1dev <all_mind@naver.com>
2021-08-08 15:23:48 +0800

    refactor : LottoCount 검증 로직 추가
    - LottoCount 검증 로직 추가

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.exception.MoneyValidationException;
+
 public class LottoCount {
 
 	private static final int LOTTO_PRICE = 1000;
@@ -7,10 +9,14 @@ public class LottoCount {
 	private final int lottoCount;
 
 	public LottoCount(int money) {
+		if (money < LOTTO_PRICE) {
+			throw new MoneyValidationException("로또 구매가 가능한 최소 금액은 " + LOTTO_PRICE + "원 이상입니다.");
+		}
+
 		lottoCount = money / LOTTO_PRICE;
 	}
 
 	public int getLottoCount() {
-		return lottoCount;
+		return lottoCount;	
 	}
 }
---
+++ src/main/java/lotto/exception/MoneyValidationException.java
@@ -0,0 +1,8 @@
+package lotto.exception;
+
+public class MoneyValidationException extends RuntimeException {
+
+	public MoneyValidationException(String message) {
+		super(message);
+	}
+}
