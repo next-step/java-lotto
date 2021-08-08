3dbbf1763ec7e970233e93a64358cc4e9f8e38c8
PIH6585 <pih6585@naver.com>
2021-08-08 10:55:56 +0900

    refactor : stream 적용

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -1,9 +1,9 @@
 package lotto.model;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoMachine {
 
@@ -12,13 +12,9 @@ public class LottoMachine {
 	private static final int LOTTO_START_NUMBER = 1;
 	private static final int LOTTO_END_NUMBER = 46;
 	public static final String LOTTO_MACHINE_CREATE_MESSAGE = "로또기계 생성은 불가능 합니다.";
-	public static final List<Integer> lottoBalls = new ArrayList<>();
-
-	static {
-		for (int i = LOTTO_START_NUMBER; i < LOTTO_END_NUMBER; i++) {
-			lottoBalls.add(i);
-		}
-	}
+	public static final List<Integer> lottoBalls = IntStream.range(LOTTO_START_NUMBER, LOTTO_END_NUMBER)
+		.boxed()
+		.collect(Collectors.toList());
 
 	private LottoMachine() {
 		throw new AssertionError(LOTTO_MACHINE_CREATE_MESSAGE);
