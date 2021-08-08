2574ce78d111e440f05282c72d0f81fa30a23333
rkdals213 <rkdals213@naver.com>
2021-08-09 00:29:47 +0900

    feat: 로또 번호 비교 로직 추가

--- src/main/java/step4/domain/Lotto.java
+++ src/main/java/step4/domain/Lotto.java
@@ -1,5 +1,7 @@
 package step4.domain;
 
+import step4.lottoPlace.LastWeekLotto;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,8 +25,23 @@ public class Lotto {
             .collect(Collectors.toList()));
     }
 
+    public static Lotto createLotto(List<Integer> manualLottoNum) {
+        return new Lotto(manualLottoNum);
+    }
+
     public List<Integer> getLottoNums() {
         return lottoNums;
+    }
+
+    public int countCorrectNums(LastWeekLotto lastWeekLotto) {
+        return (int) lottoNums.stream()
+            .filter(lastWeekLotto::contains)
+            .count();
+    }
+
+    public boolean isLottoNumContainsBonusNum(LastWeekLotto lastWeekLotto) {
+        return lottoNums.stream()
+            .anyMatch(lastWeekLotto::isBonusNumCorrect);
     }
 
     @Override
