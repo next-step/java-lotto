00f7679d7bfd6055e3372ae17e468e98d2355e39
rkdals213 <rkdals213@naver.com>
2021-08-09 00:29:29 +0900

    feat: 수동 로또 구매 메소드 추가

--- src/main/java/step4/domain/Lottos.java
+++ src/main/java/step4/domain/Lottos.java
@@ -8,15 +8,22 @@ public class Lottos {
 
     private final int cost;
 
-    public Lottos(int count, int cost) {
+    public Lottos(int count, List<List<Integer>> manualLottoNums, int cost) {
         this.cost = cost;
         for (int i = 0; i < count; i++) {
             lottos.add(Lotto.createLotto());
         }
+        for (List<Integer> manualLottoNum : manualLottoNums) {
+            lottos.add(Lotto.createLotto(manualLottoNum));
+        }        
     }
 
     public static Lottos createLottos(int count, int cost) {
-        return new Lottos(count, cost);
+        return new Lottos(count, new ArrayList<>(), cost);
+    }
+
+    public static Lottos createLottos(int count, List<List<Integer>> manualLottoNums, int cost) {
+        return new Lottos(count, manualLottoNums, cost);
     }
 
     public int size() {
