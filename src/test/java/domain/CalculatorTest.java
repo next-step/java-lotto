8dc9e33b4decce8ee5b821558f9ce8b95d1b955a
minyul <ggomjae@gmail.com>
2021-08-08 22:52:59 +0900

    Calculator에 대한 Test를 작성한다.

---
+++ src/test/java/domain/CalculatorTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2|3", "1,2:3|6"}, delimiter = '|')
+	void add_calculate(String inputParam, Integer resultParam) {
+		Calculator calculator = new Calculator();
+		Integer result = calculator.calculate(inputParam);
+		assertThat(resultParam).isEqualTo(result);
+	}
+}
