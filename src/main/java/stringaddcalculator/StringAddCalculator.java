da653880c2798332618f020a6d98f5839504c092
sangw0804 <sangw0804@likelion.org>
2021-08-09 00:16:14 +0900

    feat: 테스트 통과

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -1,7 +1,36 @@
 package stringaddcalculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-    public static int splitAndSum(String o) {
-        return 0;
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String regex1 = "//(.+)\n";
+        Pattern pattern = Pattern.compile(regex1);
+        Matcher matcher = pattern.matcher(input);
+
+        StringBuffer sb  = new StringBuffer(",|:");
+
+        if (matcher.find()) {
+            sb.append("|");
+            sb.append(matcher.group(1));
+            input = input.split(regex1)[1];
+        }
+
+        String regex = sb.toString();
+        String[] splits = input.split(regex);
+
+        int sum = Arrays.stream(splits).mapToInt(Integer::parseInt).peek((it) -> {
+            if (it < 0) {
+                throw new RuntimeException("음수는 처리할 수 없습니다.");
+            }
+        }).reduce(0, Integer::sum);
+
+        return sum;
     }
 }
