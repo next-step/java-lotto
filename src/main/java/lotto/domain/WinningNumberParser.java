26af7d476e9aa43c1d2767ea1e5f4fda231f714c
sangnyyy <dltkdals2202@gmail.com>
2021-08-08 21:58:00 +0900

    feat : 지난 주 당첨번호를 파싱할 수 있는 기능 구현

---
+++ src/main/java/lotto/domain/WinningNumberParser.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningNumberParser {
+
+    private static final String DEFAULT_SEPARATOR = ", ";
+
+    public List<Integer> parseToWinningNumbers(String elements) {
+        return Arrays.stream(elements.split(DEFAULT_SEPARATOR))
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/lotto/domain/WinningNumberParserTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningNumberParserTest {
+
+    @Test
+    public void 문자열을_입력받았을_때_파싱을_통해_List로_변환할_수_있다(){
+        //given
+        WinningNumberParser parser = new WinningNumberParser();
+        //when
+        List<Integer> winningNumbers = parser.parseToWinningNumbers("1, 2, 3, 4, 5, 6");
+        //then
+        assertThat(winningNumbers).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+}
