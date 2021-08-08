b862e239c486e1b340bf843432961dc9992edf61
harris <harrisleesh@gmail.com>
2021-08-08 14:02:04 +0900

    refactor : Remove shuffle parameter Random(seed)

--- src/main/java/lotto/strategy/ListMixStrategy.java
+++ src/main/java/lotto/strategy/ListMixStrategy.java
@@ -4,12 +4,10 @@ import lotto.domain.LottoNumber;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class ListMixStrategy implements ListSortOrMixStrategy{
     @Override
     public void sortOrMix(List<LottoNumber> list) {
-        long seed = System.nanoTime();
-        Collections.shuffle(list, new Random(seed));
+        Collections.shuffle(list);
     }
 }
