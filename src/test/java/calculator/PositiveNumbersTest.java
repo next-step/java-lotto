52f8938eb33c006877f9c0bab5daee18ee590700
ChangSubKwak <paradise1732@gmail.com>
2021-08-09 06:57:17 +0900

    fix: Tokens에서 PositiveNumbers으로 클래스명 변경에 따른 수정, 필요없는 테스트 코드 제거

--- src/test/java/calculator/PositiveNumbersTest.java
+++ src/test/java/calculator/PositiveNumbersTest.java
@@ -5,16 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class TokensTest {
+class PositiveNumbersTest {
     @Test
-    void 토큰들중에서_음수를_포함하고_있으면_예외를_던진다() {
-        assertThat(1).isEqualTo(1);
-        assertThatThrownBy(() -> new Tokens(new String[]{"1", "2", "-1"}))
+    void 입력값에서_음수를_포함하고_있으면_예외를_던진다() {
+        assertThatThrownBy(() -> new PositiveNumbers(new int[]{1, 2, -1}))
             .isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    void 토큰들을_입력으로_받으면_합을_반환한다() {
-        assertThat(new Tokens(new String[] {"1", "3", "5"}).sum()).isEqualTo(9);
+    void 양수만을_입력으로_받으면_합을_반환한다() {
+        assertThat(new PositiveNumbers(new int[] {1, 3, 5}).sum()).isEqualTo(9);
     }
 }
