92f68e4b81faad793b8457cd5a039120053d850e
sangw0804 <sangw0804@likelion.org>
2021-08-09 00:31:53 +0900

    refactor: 메소드분리 외 리팩토링

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -5,32 +5,42 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    private static final String CUSTOM_SPLITTER_REGEX = "//(.+)\n";
+    private static final Pattern CUSTOM_SPLITTER_PATTERN = Pattern.compile(CUSTOM_SPLITTER_REGEX);
+    private static final String SPLITTER_SEPARATOR = "|";
+    private static final String[] DEFAULT_SPLITTERS = {",", ":"};
+
     public static int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        String regex1 = "//(.+)\n";
-        Pattern pattern = Pattern.compile(regex1);
-        Matcher matcher = pattern.matcher(input);
+        return sum(split(input));
+    }
 
-        StringBuffer sb  = new StringBuffer(",|:");
+    private static String[] split(String input) {
+        StringBuffer sb  = new StringBuffer(String.join(SPLITTER_SEPARATOR, DEFAULT_SPLITTERS));
 
+        Matcher matcher = CUSTOM_SPLITTER_PATTERN.matcher(input);
         if (matcher.find()) {
-            sb.append("|");
+            sb.append(SPLITTER_SEPARATOR);
             sb.append(matcher.group(1));
-            input = input.split(regex1)[1];
+            input = input.split(CUSTOM_SPLITTER_REGEX)[1];
         }
 
-        String regex = sb.toString();
-        String[] splits = input.split(regex);
+        return input.split(sb.toString());
+    }
 
-        int sum = Arrays.stream(splits).mapToInt(Integer::parseInt).peek((it) -> {
-            if (it < 0) {
-                throw new RuntimeException("음수는 처리할 수 없습니다.");
-            }
-        }).reduce(0, Integer::sum);
+    private static int sum(String[] splits) {
+        return Arrays.stream(splits)
+            .mapToInt(Integer::parseInt)
+            .peek(StringAddCalculator::validateNegative)
+            .reduce(0, Integer::sum);
+    }
 
-        return sum;
+    private static void validateNegative(int inputNumber) {
+        if (inputNumber < 0) {
+            throw new RuntimeException("음수는 처리할 수 없습니다.");
+        }
     }
 }
