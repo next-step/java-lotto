e06fb2c7f88da6e357fe0892c7406c1358c80a87
조원진 <>
2021-08-08 14:27:05 +0900

    feat(LottoNumber) : LottoNumber 규칙을 따르는 클래스 생성

---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class LottoNumberTest {
+
+    @DisplayName("생성")
+    @Test
+    void create() {
+        LottoNumber lottoNumber = new LottoNumber(5);
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(5));
+    }
+
+    @DisplayName("로또 숫자 범위 외 값 입력시 IllegalArgumentException 발생")
+    @ParameterizedTest(name = "{index} {displayName} {arguments}")
+    @ValueSource(ints = {0, 46, -5})
+    void create_ThrowsIllegalArgumentException_IfInputIsOutOfLottoNumberRange(int input) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new LottoNumber(input));
+    }
+}
