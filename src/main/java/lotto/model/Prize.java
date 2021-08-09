fbf08b7e5b3ae0e01982d1310d28db81df689729
PIH6585 <pih6585@naver.com>
2021-08-09 18:32:19 +0900

    feat : 상금에 2등 추가

--- src/main/java/lotto/model/Prize.java
+++ src/main/java/lotto/model/Prize.java
@@ -6,18 +6,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum Prize {
-	FIRST(6, 2_000_000_000),
-	THIRD(5, 1_500_000),
-	FOURTH(4, 50_000),
-	FIFTH(3, 5_000),
-	MISS(0, 0);
+	FIRST(6, 2_000_000_000, false),
+	SECOND(5, 30_000_000, true),
+	THIRD(5, 1_500_000, false),
+	FOURTH(4, 50_000, false),
+	FIFTH(3, 5_000, false),
+	MISS(0, 0, false);
 
 	private final int countOfMatch;
 	private final int winningMoney;
+	private final boolean matchBonus;
 
-	Prize(int countOfMatch, int winningMoney) {
+	Prize(int countOfMatch, int winningMoney, boolean matchBonus) {
 		this.countOfMatch = countOfMatch;
 		this.winningMoney = winningMoney;
+		this.matchBonus = matchBonus;
 	}
 
 	public int getCountOfMatch() {
@@ -28,15 +31,19 @@ public enum Prize {
 		return winningMoney;
 	}
 
+	public boolean getMatchBonus() {
+		return matchBonus;
+	}
+
 	public static List<Prize> getValuesMoneySort() {
 		return Arrays.stream(Prize.values())
 			.sorted(Comparator.comparing(Prize::getWinningMoney))
 			.collect(Collectors.toList());
 	}
 
-	public static Prize getWinnersStatus(int countOfMatch) {
+	public static Prize getWinnersStatus(int countOfMatch, boolean matchBonus) {
 		return Arrays.stream(Prize.values())
-			.filter(r -> r.countOfMatch == countOfMatch)
+			.filter(r -> r.countOfMatch == countOfMatch && (matchBonus & r.matchBonus) == r.matchBonus)
 			.findFirst()
 			.orElse(MISS);
 	}
