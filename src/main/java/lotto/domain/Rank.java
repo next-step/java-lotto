f32b7752afbf177c93a4f1c42a04a5000db73050
ChangSubKwak <paradise1732@gmail.com>
2021-08-10 06:59:03 +0900

    fix: PrizeMoney에서 Rank으로 클래스명 변경에 따른 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,29 +1,25 @@
 package lotto.domain;
 
-import java.util.function.Function;
+public enum Rank {
+    NO_RANK(0, Money.from(0)),
+    FOURTH_RANK(3, Money.from(5000)),
+    THIRD_RANK(4, Money.from(50000)),
+    SECOND_RANK(5, Money.from(1500000)),
+    FIRST_RANK(6, Money.from(2000000000));
 
-public enum PrizeMoney {
-	NO_MATCH(0, Money.from(0)),
-	THREE_MATCH(3, Money.from(5000)),
-	FOUR_MATCH(4, Money.from(50000)),
-	FIVE_MATCH(5, Money.from(1500000)),
-	SIX_MATCH(6, Money.from(2000000000));
+    private final int matchCount;
+    private final Money money;
 
-	private final int matchCount;
-	private final Money money;
-	// private final Function<Integer, Integer> function;
+    Rank(int matchCount, Money money) {
+        this.matchCount = matchCount;
+        this.money = money;
+    }
 
-	//PrizeMoney(int matchCount, Function<Integer, Integer> function) {
-	PrizeMoney(int matchCount, Money money) {
-		this.matchCount = matchCount;
-		this.money = money;
-	}
+    public int getMatchCount() {
+        return matchCount;
+    }
 
-	public int getMatchCount() {
-		return matchCount;
-	}
-
-	public long getPrizeMoney(int lottoCount) {
-		return this.function.apply(lottoCount);
-	}
+    public Money getMoney() {
+        return money;
+    }
 }
