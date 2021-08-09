5eaf38b788b4bbe4318915a54905db37c9fd8c61
harris <harrisleesh@gmail.com>
2021-08-09 23:12:05 +0900

    refactor : Change to primitive type

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -5,16 +5,16 @@ import java.util.Objects;
 public class LottoNumber {
     public static final int MAX_NUMBER = 45;
     public static final int MIN_NUMBER = 1;
-    private Integer lottoNumber;
+    private int lottoNumber;
 
-    public LottoNumber(Integer lottoNumber) {
+    public LottoNumber(int lottoNumber) {
         if (lottoNumber > MAX_NUMBER || lottoNumber < MIN_NUMBER) {
             throw new IllegalArgumentException("로또 넘버는 1~45 사이입니다. 입력 로또 넘버 : " + lottoNumber);
         }
         this.lottoNumber = lottoNumber;
     }
 
-    public Integer getLottoNumber() {
+    public int getLottoNumber() {
         return lottoNumber;
     }
 
