6e1496bbbb26637a191f51a6a289351ec8838f97
YONGJU <odetsuo@gmail.com>
2021-08-09 16:40:12 +0900

    refactor : ui 문자열 상수 치환

--- src/main/java/lottomanual/ui/InputView.java
+++ src/main/java/lottomanual/ui/InputView.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class InputView {
 
 	private static final String HOW_MUCH = "구입금액을 입력해 주세요.";
+	private static final String RE_INPUT_INT_VALUE = "숫자만 입력 가능합니다. 다시 입력해주세요.";
 	private static final String MANUAL_LOTTO_COUNT = "\n수동으로 구매할 로또 수를 입력해 주세요.";
 	private static final String MANUAL_LOTTO_NUMBERS = "\n수동으로 구매할 번호를 입력해 주세요.";
 	private static final String LAST_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
@@ -51,7 +52,7 @@ public class InputView {
 		try {
 			return Integer.parseInt(scanner.nextLine());
 		} catch (NumberFormatException e) {
-			System.out.println("숫자만 입력 가능합니다. 다시 입력해주세요.");
+			System.out.println(RE_INPUT_INT_VALUE);
 			return inputIntValue();
 		}
 	}
