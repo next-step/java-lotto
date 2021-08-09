258c37b8d27ea7c2b5c0a435c78b3d10612c1a05
PIH6585 <pih6585@naver.com>
2021-08-09 18:32:54 +0900

    feat : 2등 추가에 따른 test 수정

--- src/test/java/lotto/model/PrizeTest.java
+++ src/test/java/lotto/model/PrizeTest.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -10,9 +12,9 @@ public class PrizeTest {
 
 	@ParameterizedTest
 	@DisplayName("당첨수에 따라 결과 및 금액을 알수있다.")
-	@CsvSource(value = {"3,5000", "4,50000", "5,1500000", "6,2000000000"})
-	public void returnPrizeMoney(int countOfMatch, int winningMoney) {
-		Prize result = Prize.getWinnersStatus(countOfMatch);
+	@CsvSource(value = {"3,5000,false", "4,50000,false", "5,1500000,false","5,3000000,true", "6,2000000000,false"})
+	public void returnPrizeMoney(int countOfMatch, int winningMoney,boolean matchBonus) {
+		Prize result = Prize.getWinnersStatus(countOfMatch,matchBonus);
 
 		assertThat(result.getWinningMoney()).isEqualTo(winningMoney);
 	}
