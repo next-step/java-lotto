5280cb1c59a6325b3bc62581554a68ca7c114cc9
PIH6585 <pih6585@naver.com>
2021-08-09 18:33:15 +0900

    feat : 2등 추가에 따른 당첨 금액 확인 수정

--- src/main/java/lotto/service/PrizeStatus.java
+++ src/main/java/lotto/service/PrizeStatus.java
@@ -1,29 +1,40 @@
 package lotto.service;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import lotto.model.Lotto;
+import lotto.model.LottoNumber;
 import lotto.model.Lottos;
 import lotto.model.Prize;
 import lotto.model.WinPrizes;
+import lotto.model.WinnerLotto;
 
 public class PrizeStatus {
 
 	public static final int ADDITION_PRIZE_COUNT = 1;
 	public static final int INITIAL_PRIZE_COUNT = 0;
 
-	public static WinPrizes findWinPrize(Lottos lottos, Lotto winnerLotto) {
+	public static WinPrizes findWinPrize(Lottos lottos, WinnerLotto winnerLotto) {
 		Map<Prize, Integer> winPrizes = Prize.getValuesMoneySort()
 			.stream()
 			.collect(Collectors.toMap(prize -> prize, prize -> INITIAL_PRIZE_COUNT, (a, b) -> b, LinkedHashMap::new));
 		for (Lotto lotto : lottos.getLottos()) {
+			boolean matchBonus = findMatchBonusNumber(lotto.getLottoNumbers(), winnerLotto.getBonusNumber());
 			Prize winPrize = Prize.getWinnersStatus(
-				(int)lotto.getLottoNumbers().stream().filter(winnerLotto.getLottoNumbers()::contains).count());
+				(int)lotto.getLottoNumbers()
+					.stream()
+					.filter(winnerLotto.getWinnerLotto().getLottoNumbers()::contains)
+					.count(), matchBonus);
 			winPrizes.computeIfPresent(winPrize, (oldPrize, oldPrizeCount) -> oldPrizeCount + ADDITION_PRIZE_COUNT);
 		}
 		return new WinPrizes(winPrizes);
+	}
+
+	private static boolean findMatchBonusNumber(List<LottoNumber> lottoNumbers, LottoNumber bonusNumber) {
+		return lottoNumbers.contains(bonusNumber);
 	}
 
 }
