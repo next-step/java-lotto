19a2e3bd878adbf4d942886c66e8b81f4c6fca33
yook0328 <yook0328@gmail.com>
2021-08-10 00:42:11 +0900

    feat: 유효성 검증 기능 추가.

--- src/test/java/com/lotto/calculator/NumberSplitterTest.java
+++ src/test/java/com/lotto/calculator/NumberSplitterTest.java
@@ -1,9 +1,11 @@
 package com.lotto.calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class NumberSplitterTest {
@@ -27,5 +29,42 @@ public class NumberSplitterTest {
 
         //then
         assertThat(numbers.length).isEqualTo(expected);
+    }
+
+    @DisplayName("올바르지 않는 값 있을 시 테스트")
+    @ParameterizedTest
+    @CsvSource({"'1:-1:2', -1", "'1:a:2', a", "'//;\n1;-2;3;4', -2", "'//;\n1;a;3;4', a"})
+    public void invalidInput(String input, String expected) {
+        //then
+        assertThatThrownBy(() -> {
+            NumberSplitter.split(input);
+        }).isExactlyInstanceOf(RuntimeException.class)
+                .hasMessage("this input is not valid, value : %s", expected);
+    }
+
+    @DisplayName("null 값 넘길 시 테스트")
+    @Test
+    public void nullInput() {
+        //given
+        String input = null;
+
+        //when
+        int[] result = NumberSplitter.split(input);
+
+        //then
+        assertThat(result.length).isEqualTo(0);
+    }
+
+    @DisplayName("빈값 넘길 시 테스트")
+    @Test
+    public void emptyInput() {
+        //given
+        String input = "";
+
+        //when
+        int[] result = NumberSplitter.split(input);
+
+        //then
+        assertThat(result.length).isEqualTo(0);
     }
 }
