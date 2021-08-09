9e3448637fcf213ce1687267f418ae83472901be
hiro <kiryuinh32@gmail.com>
2021-08-09 13:55:42 +0900

    refactor: LottoMachine 클래스에서 사용되던 멤버 변수와 검증 메서드를 Money 객체로 이동

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,52 +1,15 @@
 package lotto.domain;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static lotto.util.StringUtil.split;
 
 public class LottoMachine {
-    private static final int LOTTO_PRICE = 1000;
-
-    private final Lotties lotties;
-    private final int lottoCount;
-
-    public LottoMachine(final int money) {
-        if (!validMoney(money)) {
-            throw new RuntimeException();
-        }
-        this.lottoCount = money / LOTTO_PRICE;
-        this.lotties = new Lotties(createRandomLotties(money));
-    }
-
-    private List<Lotto> createRandomLotties(final int money) {
+    public Lotties createRandomLotties(Money money) {
         List<Lotto> lotties = new ArrayList<>();
-        for(int i = 0; i < money / LOTTO_PRICE; i++) {
+
+        for(int i = 0; i < money.getLottoCount(); i++) {
             lotties.add(new Lotto(new RandomNumGenerator()));
         }
-        return lotties;
+        return new Lotties(lotties);
     }
 
-    private boolean validMoney(int money) {
-        return money % LOTTO_PRICE == 0;
-    }
-
-    public Lotties getLotties() {
-        return lotties;
-    }
-
-    public int getLottoCount() {
-        return lottoCount;
-    }
-
-    public int lottiesCount() {
-        return lotties.getLottiesSize();
-    }
-
-    public void confirmLotto(String winningNumber) {
-        lotties.sameNumberCount(Arrays.stream(split(winningNumber))
-                .map(Integer::valueOf)
-                .sorted()
-                .collect(Collectors.toList()));
-    }
 }
