e3612c5a849ba7f8acd576a5610ccb209663e9f6
YONGJU <odetsuo@gmail.com>
2021-08-09 15:15:31 +0900

    feat : 수동으로 구매할 로또 수량과 번호를 입력

--- src/main/java/lottomanual/ui/InputView.java
+++ src/main/java/lottomanual/ui/InputView.java
@@ -5,12 +5,30 @@ import java.util.Scanner;
 public class InputView {
 
 	private static final String HOW_MUCH = "구입금액을 입력해 주세요.";
+	private static final String MANUAL_LOTTO_COUNT = "\n수동으로 구매할 로또 수를 입력해 주세요.";
+	private static final String MANUAL_LOTTO_NUMBERS = "\n수동으로 구매할 번호를 입력해 주세요.";
 	private static final String LAST_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
 	private static final String BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static int inputMoney() {
 		System.out.println(HOW_MUCH);
+		return inputIntValue();
+	}
+
+	public static String[] inputManualNumbers() {
+		int manualCount = inputManualCount();
+
+		System.out.println(MANUAL_LOTTO_NUMBERS);
+		String[] manualNumbers = new String[manualCount];
+		for (int i = 0; i < manualCount; i++) {
+			manualNumbers[i] = scanner.nextLine();
+		}
+		return manualNumbers;
+	}
+
+	private static int inputManualCount() {
+		System.out.println(MANUAL_LOTTO_COUNT);
 		return inputIntValue();
 	}
 
