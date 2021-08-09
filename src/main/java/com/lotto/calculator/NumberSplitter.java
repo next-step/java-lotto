845ba8c444aece59efb4f16bb55f0197b150ae1b
yook0328 <yook0328@gmail.com>
2021-08-10 00:41:55 +0900

    test: 유효성 검증 테스트.

--- src/main/java/com/lotto/calculator/NumberSplitter.java
+++ src/main/java/com/lotto/calculator/NumberSplitter.java
@@ -1,6 +1,7 @@
 package com.lotto.calculator;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,13 +10,32 @@ public class NumberSplitter {
     private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("^//(.)\n.*");
     private static final Pattern INPUT_NUMBERS_PATTERN = Pattern.compile("\n(.*)$");
     private static final String EMPTY = "";
+    private static final int[] EMPTY_ARRAY = new int[0];
 
     public static int[] split(String input) {
+        if (Objects.isNull(input) || EMPTY.equals(input)) {
+            return EMPTY_ARRAY;
+        }
+
         String SeparatorRegex = makeSeparatorRegex(input);
         String numberLine = extractNumberLine(input);
 
         return Arrays.stream(numberLine.split(SeparatorRegex))
-                .mapToInt(Integer::valueOf).toArray();
+                .mapToInt(NumberSplitter::extractNumber).toArray();
+    }
+
+    private static int extractNumber(String value) {
+        try {
+            int number = Integer.parseInt(value);
+
+            if (number < 0) {
+                throw new RuntimeException("this input is not valid, value : " + value);
+            }
+
+            return number;
+        } catch (NumberFormatException exception) {
+            throw new RuntimeException("this input is not valid, value : " + value);
+        }
     }
 
     private static String extractNumberLine(String input) {
