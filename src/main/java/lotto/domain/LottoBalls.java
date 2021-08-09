3ecc49db5efb7432083687d139985f630a66d058
ChangSubKwak <paradise1732@gmail.com>
2021-08-10 06:57:43 +0900

    fix: view에 해당하는 부분 제거, indent조정

--- src/main/java/lotto/domain/LottoBalls.java
+++ src/main/java/lotto/domain/LottoBalls.java
@@ -3,61 +3,62 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LottoBalls {
-	private final static int LOTTO_BALLS_MAX_NUM = 6;
+    private final static int LOTTO_BALLS_MAX_NUM = 6;
 
-	private final List<LottoBall> lottoBalls;
+    private final Set<LottoBall> lottoBalls;
 
-	private LottoBalls(int... numbers) {
-		validate(numbers);
-		this.lottoBalls = Arrays.stream(numbers)
-			.mapToObj(LottoBall::from)
-			.collect(Collectors.toList());
-	}
+    private LottoBalls(int... numbers) {
+        validate(numbers);
+        this.lottoBalls = Arrays.stream(numbers)
+            .mapToObj(LottoBall::select)
+            .collect(Collectors.toSet());
+    }
 
-	protected static LottoBalls of(int... numbers) {
-		return new LottoBalls(numbers);
-	}
+    public static LottoBalls of(int... numbers) {
+        return new LottoBalls(numbers);
+    }
 
-	private void validate(int... numbers) {
-		if (numbers.length != LOTTO_BALLS_MAX_NUM) {
-			throw new RuntimeException("로또공의 개수를 만족하지 않습니다.");
-		}
-	}
+    public static LottoBalls createRandomNumber() {
+        List<Integer> randomNumbers = IntStream
+            .rangeClosed(LottoBall.LOTTO_BALL_NUMBER_MIN, LottoBall.LOTTO_BALL_NUMBER_MAX)
+            .boxed()
+            .collect(Collectors.toList());
 
-	public static LottoBalls createRandomNumber() {
-		List<Integer> randomNumbers = IntStream
-			.rangeClosed(LottoBall.LOTTO_BALL_NUMBER_MIN, LottoBall.LOTTO_BALL_NUMBER_MAX)
-			.boxed()
-			.collect(Collectors.toList());
+        Collections.shuffle(randomNumbers);
+        randomNumbers = randomNumbers.subList(0, LOTTO_BALLS_MAX_NUM).stream()
+            .sorted()
+            .collect(Collectors.toList());
 
-		Collections.shuffle(randomNumbers);
-		randomNumbers = randomNumbers.subList(0, LOTTO_BALLS_MAX_NUM).stream()
-			.sorted()
-			.collect(Collectors.toList());
+        return LottoBalls.of(randomNumbers.stream()
+            .mapToInt(Integer::intValue)
+            .toArray());
+    }
 
-		return LottoBalls.of(randomNumbers.stream()
-			.mapToInt(Integer::intValue)
-			.toArray());
-	}
+    private void validate(int... numbers) {
+        if (numbers.length != LOTTO_BALLS_MAX_NUM) {
+            throw new RuntimeException("로또공의 개수를 만족하지 않습니다.");
+        }
+    }
 
-	private boolean contains(int number) {
-		return lottoBalls.stream()
-			.anyMatch(lottoBall -> lottoBall.equals((new LottoBall(number))));
-	}
+    private boolean contains(int number) {
+        return lottoBalls.stream()
+            .anyMatch(lottoBall -> lottoBall.equals((LottoBall.select(number))));
+    }
 
-	public int countMatchNumber(int... winnerNumbers) {
-		return (int) Arrays.stream(winnerNumbers)
-			.filter(this::contains)
-			.count();
-	}
+    public int countMatchNumber(LottoBalls winnerNumbers) {
+        return (int)winnerNumbers.getLottoBallsStream()
+            .filter(winnerNumber -> this.contains(winnerNumber.number()))
+            .count();
+    }
 
-	@Override
-	public String toString() {
-		return "[" + lottoBalls.stream().map(LottoBall::toString)
-			.collect(Collectors.joining(", ")) + "]";
-	}
+    public Stream<LottoBall> getLottoBallsStream() {
+        return lottoBalls.stream();
+    }
 }
+
