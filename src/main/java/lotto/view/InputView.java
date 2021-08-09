996025d4c0cb6b7cf291e42564eb0b167249998e
hiro <kiryuinh32@gmail.com>
2021-08-09 13:40:05 +0900

    refactor: 포멧을 사용하지 않는 부분을 println으로 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String FIRST_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String ASK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.\n";
+    private static final String ASK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
