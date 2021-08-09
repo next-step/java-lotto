04a9013ae667a770fa2ee364f713de53bb3cda0c
beadoer1 <beadoer1@gmail.com>
2021-08-09 23:26:31 +0900

    refactor(StringAddCalculator) : 중복코드 삭제(외부 메서드 처리)

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -5,31 +5,27 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+
     public static int splitAndSum(String inputString) {
         if (inputString == null || inputString.isEmpty()) {
             return 0;
         }
+
         String delimiterByRegex = ",|:";
-        int result = 0;
-        // java.util.regex 패키지의 Matcher, Pattern import
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputString);
         if (m.find()) {
             String customDelimiter = m.group(1);
             delimiterByRegex += ("|" + customDelimiter);
-            String[] numsString= m.group(2).split(delimiterByRegex);
-            System.out.println(delimiterByRegex);
-            // 덧셈 구현
-            for (String numString : numsString) {
-                int num = Integer.parseInt(numString);
-                if (num < 0) {
-                    throw new RuntimeException("음수를 입력할 수 없습니다.");
-                }
-                result += num;
-            }
-            return result;
+            return extracted(m.group(2), delimiterByRegex);
         }
-        String[] numsString= inputString.split(delimiterByRegex);
-        // 덧셈 구현
+
+        return extracted(inputString, delimiterByRegex);
+    }
+
+    private static int extracted(String targetString, String delimiterByRegex) {
+        int result = 0;
+        String[] numsString= targetString.split(delimiterByRegex);
+
         for (String numString : numsString) {
             int num = Integer.parseInt(numString);
             if (num < 0) {
