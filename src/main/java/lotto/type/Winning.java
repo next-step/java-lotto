d29612f3b2c6bfb808db0a4bef9825e0d4427c54
mewookim <k011215@gmail.com>
2021-08-09 22:26:31 +0900

    refactor : space add

--- src/main/java/lotto/type/Winning.java
+++ src/main/java/lotto/type/Winning.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum Winning {
 
-    FIFTH(3, false,5000),
+    FIFTH(3, false, 5000),
     FOURTH(4, false, 50000),
-    THIRD(5, false,1500000),
-    SECOND(5, true,30000000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
     FIRST(6, false, 2000000000);
 
     private final int count;
