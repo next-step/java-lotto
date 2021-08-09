4328ff8d58612ff215fce6cc7221fa4381f1f0d0
ChangSubKwak <paradise1732@gmail.com>
2021-08-10 07:02:19 +0900

    fix: 입력인자를 배열에서 콜렉션으로 변경에 따른 수정, indent조정(탭을 스패이스 변경)

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -6,39 +6,40 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import lotto.domain.LottoBalls;
-import lotto.util.LottoUtil;
+import lotto.domain.LottoTickets;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
 public class LottoController {
-	public List<LottoBalls> createLottoBalls(ResultView resultView, int lottoCount) {
-		List<LottoBalls> lottoBallsList = new ArrayList<>();
-		for (int i = 0; i < lottoCount; i++) {
-			lottoBallsList.add(LottoBalls.createRandomNumber());
-		}
-		resultView.outputLottoLotteries(lottoBallsList);
-		return lottoBallsList;
-	}
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        LottoController lottoController = new LottoController();
 
-	public Map<Integer, Integer> proceedStatistics(InputView inputView, List<LottoBalls> lottoBallsList) {
-		int[] winnerNumbers = Stream.of(inputView.inputInputLastWeekWinnerNumbers().split(","))
-			.mapToInt(s -> Integer.parseInt(s.trim()))
-			.toArray();
-		return LottoUtil.getLottoStatistics(lottoBallsList, winnerNumbers);
-	}
+        int lottoCount = inputView.inputLottoBuyAmount() / 1000;
+        List<LottoBalls> lottoBallsList = lottoController.createLottoBalls(resultView, lottoCount);
+        LottoTickets lottoTickets = new LottoTickets(lottoBallsList);
 
-	public static void main(String[] args) {
-		InputView inputView = new InputView();
-		ResultView resultView = new ResultView();
-		LottoController lottoController = new LottoController();
+        Map<Integer, Integer> winStatistics = lottoController.proceedStatistics(inputView, lottoTickets);
+        resultView.outputStatistics(winStatistics, lottoTickets.getRateOfReturn(lottoCount, winStatistics));
+        inputView.scannerClose();
+    }
 
-		int lottoCount = inputView.inputLottoBuyAmount() / 1000;
-		List<LottoBalls> lottoBallsList = lottoController.createLottoBalls(resultView, lottoCount);
+    public List<LottoBalls> createLottoBalls(ResultView resultView, int lottoCount) {
+        List<LottoBalls> lottoBallsList = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottoBallsList.add(LottoBalls.createRandomNumber());
+        }
+        resultView.outputLottoLotteries(lottoBallsList);
+        return lottoBallsList;
+    }
 
-		Map<Integer, Integer> winStatistics = lottoController.proceedStatistics(inputView, lottoBallsList);
-
-		resultView.outputStatistics(winStatistics, LottoUtil.getRateOfReturn(lottoCount, winStatistics));
-		inputView.scannerClose();
-	}
+    public Map<Integer, Integer> proceedStatistics(InputView inputView, LottoTickets lottoTickets) {
+        LottoBalls lottoBalls =
+            LottoBalls.of(Stream.of(inputView.inputInputLastWeekWinnerNumbers().split(","))
+                .mapToInt(s -> Integer.parseInt(s.trim()))
+                .toArray());
+        return lottoTickets.getLottoStatistics(lottoBalls);
+    }
 
 }
