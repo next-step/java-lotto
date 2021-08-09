ccffdee18dc39af5cf9d38c8da453b9dd724a26e
ChangSubKwak <paradise1732@gmail.com>
2021-08-10 07:04:30 +0900

    fix: 패턴문자열 상수값 변경, 함수순서를 호출순서대로 변경하였음

--- src/main/java/calculator/StringAdditionCalculator.java
+++ src/main/java/calculator/StringAdditionCalculator.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
@@ -9,23 +8,9 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class StringAdditionCalculator {
+    private static final String DEFAULT_DELIMITER_PATTERN = ",|:";
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
-
-    private PositiveNumbers splitWithCustomDelimiter(String expression) {
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(expression);
-
-        List<String> customDelimiterList = new ArrayList<>();
-        expression = extractExpression(expression, matcher, customDelimiterList);
-
-        String delimiters = ",|:";
-        if (customDelimiterList.size() > 0) {
-            delimiters = String.join("|", customDelimiterList);
-        }
-        return new PositiveNumbers(
-            Stream.of(expression.split(delimiters))
-                .mapToInt(Integer::parseInt)
-                .toArray());
-    }
+    private static final Pattern pattern = Pattern.compile("\\d+");
 
     private String extractExpression(String expression, Matcher matcher, final List<String> customDelimiterList) {
         while (matcher.find()) {
@@ -37,13 +22,28 @@ public class StringAdditionCalculator {
         return expression;
     }
 
+    private boolean isNullOrEmpty(String expression) {
+        return Objects.isNull(expression) || "".equals(expression);
+    }
+
     private boolean isNaturalNumber(String expression) {
-        Pattern pattern = Pattern.compile("\\d+");
         return pattern.matcher(expression).matches();
     }
 
-    private boolean isNullOrEmpty(String expression) {
-        return Objects.isNull(expression) || "".equals(expression);
+    private PositiveNumbers splitWithCustomDelimiter(String expression) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(expression);
+
+        List<String> customDelimiterList = new ArrayList<>();
+        expression = extractExpression(expression, matcher, customDelimiterList);
+
+        String delimiters = DEFAULT_DELIMITER_PATTERN;
+        if (customDelimiterList.size() > 0) {
+            delimiters = String.join("|", customDelimiterList);
+        }
+        return new PositiveNumbers(
+            Stream.of(expression.split(delimiters))
+                .mapToInt(Integer::parseInt)
+                .toArray());
     }
 
     public int splitAndSum(String expression) {
