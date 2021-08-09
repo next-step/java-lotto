105ec4ad09351248bf7c7aba837ca642c5c6909f
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2021-08-09 14:08:08 +0900

    feat : 로또(자동)
    추가내용
    * 기존 등급을 연산하는 Rank 클래스에서 2등 항목을 추가하고 이를 판별하기 위한 판별값을 각 등급별로 추가.

--- src/main/java/lotto/service/Rank.java
+++ src/main/java/lotto/service/Rank.java
@@ -3,18 +3,21 @@ package lotto.service;
 import java.util.stream.Stream;
 
 public enum Rank {
-  FIRST(6, 2_000_000_000),
-  THIRD(5, 1_500_000),
-  FOURTH(4, 50_000),
-  FIFTH(3, 5_000),
-  MISS(0, 0);
+  FIRST(6, 2_000_000_000 ,false),
+  SECOND(5, 30_000_000,true),
+  THIRD(5, 1_500_000,false),
+  FOURTH(4, 50_000,false),
+  FIFTH(3, 5_000,false),
+  MISS(0, 0,false);
 
   private final int countOfMatch;
   private final int winningMoney;
+  private final boolean matchBonus;
 
-  Rank(int countOfMatch, int winningMoney) {
+  Rank(int countOfMatch, int winningMoney, Boolean matchBonus) {
     this.countOfMatch = countOfMatch;
     this.winningMoney = winningMoney;
+    this.matchBonus = matchBonus;
   }
 
   public int getCountOfMatch() {
@@ -28,6 +31,14 @@ public enum Rank {
   public static Rank matchRank(int countOfMatch) {
     return Stream.of(values())
         .filter(rank -> (rank.countOfMatch == countOfMatch))
+        .findFirst()
+        .orElse(MISS);
+  }
+
+  public static Rank matchRank(int countOfMatch, boolean matchBonus) {
+    return Stream.of(values())
+        .filter(rank -> (rank.countOfMatch == countOfMatch))
+        .filter(rank -> rank.matchBonus == matchBonus)
         .findFirst()
         .orElse(MISS);
   }
--- src/test/java/lotto/service/RankTest.java
+++ src/test/java/lotto/service/RankTest.java
@@ -23,6 +23,7 @@ class RankTest {
     assertThat(Rank.FIFTH.getCountOfMatch()).isEqualTo(3);
     assertThat(Rank.FOURTH.getCountOfMatch()).isEqualTo(4);
     assertThat(Rank.THIRD.getCountOfMatch()).isEqualTo(5);
+    assertThat(Rank.SECOND.getCountOfMatch()).isEqualTo(5);
     assertThat(Rank.FIRST.getCountOfMatch()).isEqualTo(6);
   }
 
@@ -33,7 +34,18 @@ class RankTest {
     assertThat(Rank.FIFTH.getWinningMoney()).isEqualTo(5000);
     assertThat(Rank.FOURTH.getWinningMoney()).isEqualTo(50000);
     assertThat(Rank.THIRD.getWinningMoney()).isEqualTo(1500000);
+    assertThat(Rank.SECOND.getWinningMoney()).isEqualTo(30000000);
     assertThat(Rank.FIRST.getWinningMoney()).isEqualTo(2000000000);
   }
 
+  @DisplayName("2등판별값과 match값을 주입하면 각 등급이 정상적으로 반환되는지 테스트.")
+  @Test
+  void 판별값포함등급반환테스트() {
+    assertThat(Rank.matchRank(0,false)).isEqualTo(Rank.MISS);
+    assertThat(Rank.matchRank(3,false)).isEqualTo(Rank.FIFTH);
+    assertThat(Rank.matchRank(4,false)).isEqualTo(Rank.FOURTH);
+    assertThat(Rank.matchRank(5,false)).isEqualTo(Rank.THIRD);
+    assertThat(Rank.matchRank(5,true)).isEqualTo(Rank.SECOND);
+    assertThat(Rank.matchRank(6,false)).isEqualTo(Rank.FIRST);
+  }
 }
