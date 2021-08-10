6e8f42cf7cd050cfd832498a51aa9e24148d80de
iiaii <jhun9409@naver.com>
2021-08-10 20:24:06 +0900

    [step3] refactor: LotteryStatisticDto 외부에서 defaultLotteryResults 제공하도록 구조 수정

--- src/main/java/lottery/dto/LotteryStatisticDto.java
+++ src/main/java/lottery/dto/LotteryStatisticDto.java
@@ -6,37 +6,30 @@ import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LotteryStatisticDto {
 
     private final List<LotteryResultDto> lotteryResultDtos;
     private final BigDecimal earningsRate;
 
-    public LotteryStatisticDto(final Map<LotteryResult, Integer> lotteryResultMap, final BigDecimal earningsRate) {
-        this.lotteryResultDtos = toLotteryResultDtoMap(lotteryResultMap);
+    public LotteryStatisticDto(final List<LotteryResult> defaultLotteryResults, final Map<LotteryResult, Integer> lotteryResultMap, final BigDecimal earningsRate) {
+        this.lotteryResultDtos = toLotteryResultDtoMap(defaultLotteryResults, lotteryResultMap);
         this.earningsRate = earningsRate;
     }
 
-    private List<LotteryResultDto> toLotteryResultDtoMap(final Map<LotteryResult, Integer> lotteryResultMap) {
-        Map<LotteryResult, Integer> defaultLotteryResultMap = getDefaultLotteryResultMap();
-        defaultLotteryResultMap.putAll(lotteryResultMap);
-        return defaultLotteryResultMap.entrySet()
+    private List<LotteryResultDto> toLotteryResultDtoMap(final List<LotteryResult> defaultLotteryResults, final Map<LotteryResult, Integer> lotteryResultMap) {
+        setDefaultLotteryResults(defaultLotteryResults, lotteryResultMap);
+        return lotteryResultMap.entrySet()
                 .stream()
                 .map(entry -> entry.getKey().toDto(entry.getValue()))
                 .sorted(Comparator.comparing(LotteryResultDto::getCashPrize))
                 .collect(Collectors.toList());
     }
 
-    private Map<LotteryResult, Integer> getDefaultLotteryResultMap() {
-        return Stream.of(
-                LotteryResult.THREE_MATCHES,
-                LotteryResult.FOUR_MATCHES,
-                LotteryResult.FIVE_MATCHES,
-                LotteryResult.SIX_MATCHES)
-                .collect(Collectors.toMap(Function.identity(), v -> 0));
+    private void setDefaultLotteryResults(final List<LotteryResult> defaultLotteryResults, final Map<LotteryResult, Integer> lotteryResultMap) {
+        defaultLotteryResults
+                .forEach(lotteryResult -> lotteryResultMap.put(lotteryResult, lotteryResultMap.getOrDefault(lotteryResult, 0)));
     }
 
     public List<LotteryResultDto> getLotteryResultDtos() {
