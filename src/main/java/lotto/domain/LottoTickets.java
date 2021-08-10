f978a3a1138199b34678bdee5505a3ed9612b309
shineoov <shineoov88@gmail.com>
2021-08-10 19:14:06 +0900

    refactor: LottoTickets  메소드 분리 및 코드개선

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public final class LottoTickets {
 
+    private static final int BONUS_TARGET_NUMBER_MATCH_COUNT = 5;
+    private static final int SECOND_PLACE_MATCH_COUNT = 6;
+
     private final List<LottoTicket> lottoTickets;
 
     public LottoTickets() {
@@ -44,15 +47,26 @@ public final class LottoTickets {
 
     private LottoRank findLottoRank(final LottoTicket lottoTicket, final List<Integer> winningNumbers, final int bonusNumber) {
         int matchLottoNumberCount = lottoTicket.matchLottoTicket(winningNumbers);
-        if (matchLottoNumberCount == 5) {
-            List<Integer> winningNumbersWithBonusNumber = new ArrayList<>(winningNumbers);
-            winningNumbersWithBonusNumber.add(bonusNumber);
-            int matchBonusCount = lottoTicket.matchLottoTicket(winningNumbersWithBonusNumber);
-            if (matchBonusCount == 6) {
-                return LottoRank.SECOND;
-            }
+        if (matchLottoNumberCount != BONUS_TARGET_NUMBER_MATCH_COUNT) {
+            return LottoRank.of(matchLottoNumberCount, false);
+        }
+
+        List<Integer> winningNumbersAndBonusNumber = cloneWinningNumbersAndBonusNumber(winningNumbers, bonusNumber);
+        if (isSecondPlaceLottoTicket(lottoTicket, winningNumbersAndBonusNumber)) {
+            return LottoRank.SECOND;
         }
         return LottoRank.of(matchLottoNumberCount, false);
+    }
+
+    private List<Integer> cloneWinningNumbersAndBonusNumber(final List<Integer> winningNumbers, final int bonusNumber) {
+        List<Integer> winningNumbersWithBonusNumber = new ArrayList<>(winningNumbers);
+        winningNumbersWithBonusNumber.add(bonusNumber);
+        return winningNumbersWithBonusNumber;
+    }
+
+    private boolean isSecondPlaceLottoTicket(final LottoTicket lottoTicket, final List<Integer> winningNumbersAndBonusNumber) {
+        int matchBonusCount = lottoTicket.matchLottoTicket(winningNumbersAndBonusNumber);
+        return matchBonusCount == SECOND_PLACE_MATCH_COUNT;
     }
 
     public List<LottoTicket> getLottoTickets() {
