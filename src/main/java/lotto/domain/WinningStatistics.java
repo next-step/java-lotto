7ca09e49e3411aebeb4cacfdea02fd8e971b6f9a
hiro <kiryuinh32@gmail.com>
2021-08-10 17:22:29 +0900

    refactor: WinningStatistics addCount() 에서 Map.getOrDefault() 를 사용하도록 수정 (리뷰 반영)
    (comment)[https://github.com/next-step/java-lotto/pull/1881#discussion_r685198391]

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -6,19 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class WinningStatistics {
-    private final Map<Rank, Integer> winningStatistics;
-
-    public WinningStatistics() {
-        winningStatistics = new HashMap<>();
-
-        winningStatistics.put(Rank.ZERO, 0);
-        winningStatistics.put(Rank.ONE, 0);
-        winningStatistics.put(Rank.TWO, 0);
-        winningStatistics.put(Rank.THREE, 0);
-        winningStatistics.put(Rank.FOUR, 0);
-        winningStatistics.put(Rank.FIVE, 0);
-        winningStatistics.put(Rank.SIX, 0);
-    }
+    private final Map<Rank, Integer> winningStatistics = new HashMap<>();
 
     public void setRankCount(List<Rank> rankList) {
         for (Rank rank : rankList) {
@@ -27,8 +15,7 @@ public class WinningStatistics {
     }
 
     private void addCount(Rank rank) {
-        int count = winningStatistics.get(rank);
-        winningStatistics.put(rank, count + 1);
+        winningStatistics.put(rank, winningStatistics.getOrDefault(rank, 0) + 1);
     }
 
     public Map<Rank, Integer> getWinningStatistics() {
