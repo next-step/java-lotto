d840993c286619044cd7e507ccefe67758e8c684
gunny6026 <gunny6026@naver.com>
2021-08-11 14:19:18 +0900

    refactor : 불필요한 인스턴스 변수 삭제

--- src/main/java/lotto/step2/domain/LottoTicket.java
+++ src/main/java/lotto/step2/domain/LottoTicket.java
@@ -1,40 +1,38 @@
 package lotto.step2.domain;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class LottoTicket {
-
-    private List<Integer> lottoNumber;
     private List<LottoNumber> lottoNumbers;
-    private int matchCount;
 
     public LottoTicket(List<LottoNumber> lottoNumbers) {
-        this.lottoNumbers= lottoNumbers;
+        Validation.isCorrectLottoCount(lottoNumbers);
+        Validation.isValidLotto(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
     public int matchWinningNumber(WinningNumber winningNumber) {
-        for (int num : lottoNumber) {
-            isContainNumber(winningNumber, num);
+        int matchCount = 0;
+        for (LottoNumber num : lottoNumbers) {
+            matchCount += isContainNumber(winningNumber, num);
         }
 
         return matchCount;
     }
 
-    private void isContainNumber(WinningNumber winningNumber, int num) {
+    private int isContainNumber(WinningNumber winningNumber, LottoNumber num) {
         if (winningNumber.getWinningNumbers().contains(num)) {
-            matchCount++;
+            return 1;
         }
+        return 0;
     }
 
-    public List<Integer> getLottoNumber() {
-        return lottoNumber;
+    public List<LottoNumber> getLottoNumbers() {
+        return lottoNumbers;
     }
 
     @Override
     public String toString() {
-        return lottoNumber.toString();
+        return lottoNumbers.toString();
     }
 }
