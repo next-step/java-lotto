f046fff462534a587fc9b3080e934bef836cc3b3
조원진 <>
2021-08-11 20:09:42 +0900

    refactor(LottoGame) : 로또 티켓 생성 기능을 LottoNumbersFactory로 이동

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,8 +1,8 @@
 package lotto.domain;
 
 import lotto.generic.Money;
+import lotto.util.LottoNumbersFactory;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -18,28 +18,17 @@ public class LottoGame {
 
     public static LottoTickets buy(Money money) {
         int ticketCounts = calculateTicketCount(money);
-        List<LottoNumbers> lottoTickets = IntStream.range(0, ticketCounts)
-                .mapToObj((index) -> makeLottoNumbers())
-                .collect(toList());
+        List<LottoNumbers> lottoTickets = makeLottoTickets(ticketCounts);
         return new LottoTickets(lottoTickets);
     }
 
     private static int calculateTicketCount(Money money) {
-        return money.divide(LOTTO_PRICE).intValue();
+        return money.divideAndDiscardRemainder(LOTTO_PRICE);
     }
 
-    private static LottoNumbers makeLottoNumbers() {
-        Collections.shuffle(lottoNumbers);
-        List<LottoNumber> lottoNumbers = toLottoNumbers();
-        return new LottoNumbers(lottoNumbers);
-    }
-
-    private static List<LottoNumber> toLottoNumbers() {
-        List<Integer> integers = LottoGame.lottoNumbers.subList(0, LottoNumbers.LOTTO_NUMBERS_SIZE);
-        Collections.sort(integers);
-        return integers
-                .stream()
-                .map(LottoNumber::new)
+    private static List<LottoNumbers> makeLottoTickets(int ticketCounts) {
+        return IntStream.range(0, ticketCounts)
+                .mapToObj((index) -> LottoNumbersFactory.makeAutoLottoNumbers())
                 .collect(toList());
     }
 
