478cd98befd64b0d3602e9c7d88251fa48043d96
SONG22 <ansongi527@gmail.com>
2021-08-11 20:57:46 +0900

    test(step2) : lotto 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
 
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,7 +1,18 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
 
+    @DisplayName("로또 생성, 특정 값이 포함되어 있는지")
+    @Test
+    void create() {
+        assertThat(new Lotto(Arrays.asList(8, 21, 23, 41, 42, 43)).contains(8)).isTrue();
+        assertThat(new Lotto(Arrays.asList(3, 5, 11, 16, 32, 38)).contains(8)).isFalse();
+    }
 }
