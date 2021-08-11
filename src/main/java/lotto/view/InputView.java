7624549faf05a10dc4673fcd668bc323ea7e96d8
조원진 <>
2021-08-11 20:08:04 +0900

    feat(InputView, ResultView) : 로또 결과 출력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -15,9 +15,7 @@ public class InputView {
     }
 
     public static String receiveWinningNumbers() {
-        String input;
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-
         return scanner.nextLine();
     }
 
