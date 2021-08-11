a4701092ce5c905c13c8cafb4e68ea681f129b3c
beadoer1 <beadoer1@gmail.com>
2021-08-11 13:10:34 +0900

    refactor : 중복과정 method 처리

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -8,10 +8,8 @@ public class StringAddCalculator {
     private static String delimitersRegex = ",|:";
 
     public static int splitAndSum(String inputString) {
-        int result = 0;
-
         // 빈 입력값의 경우 0 반환
-        if (inputString == null || inputString.isEmpty() || inputString.trim().isEmpty()) {
+        if (isNullOrEmpty(inputString)) {
             return 0;
         }
 
@@ -20,20 +18,21 @@ public class StringAddCalculator {
         if(matcher.find()){
             delimitersRegex += ("|" + matcher.group(1));
             String[] splitedNumsString = matcher.group(2).split(delimitersRegex);
-            for (String numString : splitedNumsString) {
-                int num = Integer.parseInt(numString);
-                // 음수값 입력 시 예외 발생 구현
-                if (num < 0) {
-                    throw new RuntimeException("숫자는 0 이상이어야 합니다.");
-                }
-                result += num;
-            }
-            return result;
+            return getResult(splitedNumsString);
 
         }
 
         // 기본 구분자를 이용한 분리 및 합계 반환 구현
         String[] splitedNumsString = inputString.split(delimitersRegex);
+        return getResult(splitedNumsString);
+    }
+
+    private static boolean isNullOrEmpty(String inputString){
+        return inputString == null || inputString.isEmpty() || inputString.trim().isEmpty();
+    }
+
+    private static int getResult(String[] splitedNumsString) {
+        int result = 0;
         for (String numString : splitedNumsString) {
             int num = Integer.parseInt(numString);
             if (num < 0) {
@@ -41,7 +40,6 @@ public class StringAddCalculator {
             }
             result += num;
         }
-
         return result;
     }
 }
