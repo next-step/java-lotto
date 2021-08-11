5d6a62070bdebd0091b7b0387b3351bf2b76db42
Gyeongseo Woo <ugs1575@gmail.com>
2021-08-11 14:09:17 +0900

    refactor: 예외 클래스 삭제 inputview 클래스로 옮김

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -1,13 +1,38 @@
 package step2.view;
 
-import step2.model.LottoException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final int LOTTO_PRICE = 1000;
+    private static final int MIN_AMOUNT = 1000;
+    private static final int MIN_CNT = 6;
+
+    private void isDigit(String input) {
+        if (!input.matches("[0-9]*")) {
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
+    }
+
+    private void isBlank(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("빈값입니다.다시 입력해주세요.");
+        }
+    }
+
+    private void isOverMin(String amount) {
+        if (Integer.parseInt(amount) < MIN_AMOUNT) {
+            throw new IllegalArgumentException("로또를 사기에 부족한 금액입니다. 1000원이상 입력해주세요.");
+        }
+    }
+
+    private void checkCnt(String winningNo) {
+        String[] numbers = winningNo.split(",");
+        if (numbers.length < MIN_CNT) {
+            throw new IllegalArgumentException("당첨 번호는 6개 입력해주셔야합니다.");
+        }
+    }
 
     public String ask(String message){
         Scanner scanner = new Scanner(System.in);
@@ -17,13 +42,13 @@ public class InputView {
 
     public int getLottoCnt(String amount) {
         //빈값 검사
-        LottoException.isBlank(amount);
+        isBlank(amount);
 
         //숫자인지 검사
-        LottoException.isDigit(amount);
+        isDigit(amount);
 
         //구매 최소금액 검사
-        LottoException.isOverMin(amount);
+        isOverMin(amount);
 
         int purchaseAmount = getPurchaseAmount(amount);
         int lottoCnt = purchaseAmount/LOTTO_PRICE;
@@ -37,15 +62,15 @@ public class InputView {
 
     public List<Integer> getWinningNo(String winningNo) {
         //빈값 검사
-        LottoException.isBlank(winningNo);
+        isBlank(winningNo);
 
         //6개인지 검사
-        LottoException.checkCnt(winningNo);
+        checkCnt(winningNo);
 
         String[] numbers = winningNo.split(",");
         for (String number : numbers) {
             //숫자인지 검사
-            LottoException.isDigit(number);
+            isDigit(number);
         }
 
         List<Integer> winNoList = new ArrayList<>();
