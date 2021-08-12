38adda3c525c0021024e65d478bf1d0f286db6b0
jaeyoungjang2 <lion85488@gmail.com>
2021-08-12 20:26:19 +0900

    feat: InputView 구현 '구입 금액을 입력해주세요' 메세지 출력 사용자로부터 금액을 입력받는다. '몇개를 구매했습니다.' 메세지 출력

---
+++ src/main/java/step3/LottoApp.java
@@ -0,0 +1,12 @@
+package step3;
+
+import step3.view.InputView;
+
+public class LottoApp {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int numOfLotto = inputView.requestInput();
+
+    }
+
+}
---
+++ src/main/java/step3/view/InputView.java
@@ -0,0 +1,26 @@
+package step3.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    static final String INPUT_MESSAGE = "구입 금액을 입력해주세요.";
+    static final String INPUT_INFO_MESSAGE = "개를 구매했습니다.";
+    static final String INPUT_LAST_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final int chargeOfLotto = 1000;
+    static Scanner sc = new Scanner(System.in);
+
+    public int requestInput() {
+        System.out.println(INPUT_MESSAGE);
+        int charge = sc.nextInt();
+        sc.nextLine();
+        int numOfLotto = charge/chargeOfLotto;
+        System.out.println(numOfLotto+INPUT_INFO_MESSAGE);
+        return numOfLotto;
+    }
+
+    public String requestWinningLottoNumber() {
+        System.out.println(INPUT_LAST_LOTTO_NUMBER);
+        String lastWinningLottoNum = sc.nextLine();
+        return lastWinningLottoNum;
+    }
+}
