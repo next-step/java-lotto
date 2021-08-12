833ee0f7869e0568743c1cd02098e7105f11b562
janghoonseob <arpy1228@gmail.com>
2021-08-13 02:05:05 +0900

    feat: 로또 구매 예외 처리 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,11 +1,9 @@
 package lotto;
 
-import lotto.view.InputView;
-
 public class LottoGame {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,10 +4,27 @@ import java.util.Scanner;
 
 public class InputView {
 
+    public static final int ZERO = 0;
+    public static final int MINIMUM_LOTTO_MONEY = 1000;
+
     public int buyLottoView() {
         System.out.println("구입금액을 입력해주세요.");
         Scanner scanner = new Scanner(System.in);
-        return Integer.parseInt(scanner.nextLine());
+        return checkMoney(scanner.nextLine());
+    }
+
+    public int checkMoney(String inputMoney) {
+        int money = Integer.parseInt(inputMoney);
+
+        if (money < ZERO) {
+            throw new IllegalArgumentException("금액은 1000원 이상이어야 합니다.");
+        }
+
+        if (money % MINIMUM_LOTTO_MONEY != ZERO) {
+            throw new IllegalArgumentException("금액은 1000원 단위어야 합니다.");
+        }
+
+        return money;
     }
 
 }
