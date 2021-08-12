d384a1f54ae05ecadac78d972df92d4d06b7f9f5
ChangSubKwak <paradise1732@gmail.com>
2021-08-12 20:10:36 +0900

    fix: 통계값 테스트 케이스는 WinStatistics로 이동, 보너스 공 추가에 따른 수정

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -19,22 +19,14 @@ class LottoTicketsTest {
         lotteries.add(LottoBalls.of(1, 2, 3, 7, 8, 9));
         lotteries.add(LottoBalls.of(1, 2, 3, 4, 8, 9));
         lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 9));
+        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 10));
         lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 6));
         lottoTickets = LottoTickets.from(lotteries);
     }
 
     @Test
-    void 로또공들의_데이터가_주어질때_각_경우별로_1개씩_당첨되었을때_당첨_통계값이_일치해야한다() {
-        winStatistics = WinStatistics.from(LottoBalls.of(1, 2, 3, 4, 5, 6), lottoTickets);
-        assertThat(winStatistics.countByRank(Rank.FOURTH_RANK)).isEqualTo(1);
-        assertThat(winStatistics.countByRank(Rank.THIRD_RANK)).isEqualTo(1);
-        assertThat(winStatistics.countByRank(Rank.SECOND_RANK)).isEqualTo(1);
-        assertThat(winStatistics.countByRank(Rank.FIRST_RANK)).isEqualTo(1);
-    }
-
-    @Test
     void 당첨_통계값이_14중_4등_당첨된_결과가_주어질_때_수익률이_0_35로_일치해야한다() {
-        winStatistics = WinStatistics.from(LottoBalls.of(7, 8, 9, 41, 42, 43), lottoTickets);
+        winStatistics = WinStatistics.from(LottoBalls.of(7, 8, 9, 41, 42, 43), LottoBall.select(44), lottoTickets);
         assertThat(lottoTickets.getRateOfReturn(14, winStatistics)).isEqualTo(0.35f);
     }
 }
