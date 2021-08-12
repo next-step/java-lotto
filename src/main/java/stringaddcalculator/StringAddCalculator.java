4f3726cd24ff60f435fdd2f0534a889e1c762473
beadoer1 <beadoer1@gmail.com>
2021-08-12 14:36:54 +0900

    refactor(StringAddCalculator) : 정적 메서드, 정적 필드만 있는 클래스로 생성자 호출 방지를 위한 private 처리

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -8,6 +8,11 @@ public class StringAddCalculator {
     private static final String CUSTOM_DELIMITER_INPUT_STRING = "//(.)\n(.*)";
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_INPUT_STRING);
 
+    // 기본 생성자가 만들어지는 것을 방지한다.(인스턴스화 방지용)
+    private StringAddCalculator() {
+        throw new AssertionError();
+    }
+
     public static int splitAndSum(String inputString) {
         if (isNullOrEmpty(inputString)) {
             return 0;
