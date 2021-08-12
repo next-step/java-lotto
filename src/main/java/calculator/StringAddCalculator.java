bcfa7243701ba13dbc9b84a63d34cb3ec8ef1bf0
janghoonseob <arpy1228@gmail.com>
2021-08-12 10:38:16 +0900

    feat: 기본 구분자 더하기 기능 추가 test: 기본 구분자 더하기 테스트 케이스 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -12,6 +12,10 @@ public class StringAddCalculator {
             return 0;
         }
 
+        if (checkOneDigitString(input)) {
+            return checkMinusDigit(input);
+        }
+
         return 0;
     }
 
@@ -22,6 +26,48 @@ public class StringAddCalculator {
         }
 
         return false;
+    }
+
+    public static boolean checkOneDigitString(String input) {
+
+        if (input.length() == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static int checkMinusDigit(String digitString) {
+
+        int result = Integer.parseInt(digitString);
+
+        if (result < 0) {
+            throw new RuntimeException("음수입니다.");
+        }
+
+        return result;
+    }
+
+    public static int delemeterSum(String input) {
+
+        int result = 0;
+
+        String[] numbers = input.split(",|:");
+
+        result = addString(numbers);
+
+        return result;
+    }
+
+    public static int addString(String[] numbers) {
+
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            result += checkMinusDigit(numbers[i]);
+        }
+
+        return result;
     }
 
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -22,4 +22,17 @@ class StringAddCalculatorTest {
         assertThat(result).isEqualTo(1);
     }
 
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.delemeterSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.delemeterSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+
 }
