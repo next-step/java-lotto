e29b6dbc921af10780bd09eceb2413fc4108c869
ChangSubKwak <paradise1732@gmail.com>
2021-08-12 17:04:36 +0900

    fix: 일급콜렉션 WinStatistics추가에 따른 수정, LottoTickets 정적 팩토리 메서드 사용 수정

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -3,9 +3,7 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +11,7 @@ import org.junit.jupiter.api.Test;
 class LottoTicketsTest {
     List<LottoBalls> lotteries;
     LottoTickets lottoTickets;
+    WinStatistics winStatistics;
 
     @BeforeEach
     void setUp() {
@@ -21,23 +20,21 @@ class LottoTicketsTest {
         lotteries.add(LottoBalls.of(1, 2, 3, 4, 8, 9));
         lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 9));
         lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 6));
-        lottoTickets = new LottoTickets(lotteries);
+        lottoTickets = LottoTickets.from(lotteries);
     }
 
     @Test
     void 로또공들의_데이터가_주어질때_각_경우별로_1개씩_당첨되었을때_당첨_통계값이_일치해야한다() {
-        Map<Integer, Integer> winStatistics = lottoTickets.getLottoStatistics(LottoBalls.of(1, 2, 3, 4, 5, 6));
-        assertThat(winStatistics.get(3)).isEqualTo(1);
-        assertThat(winStatistics.get(4)).isEqualTo(1);
-        assertThat(winStatistics.get(5)).isEqualTo(1);
-        assertThat(winStatistics.get(6)).isEqualTo(1);
+        winStatistics = WinStatistics.from(LottoBalls.of(1, 2, 3, 4, 5, 6), lottoTickets);
+        assertThat(winStatistics.countByRank(Rank.FOURTH_RANK)).isEqualTo(1);
+        assertThat(winStatistics.countByRank(Rank.THIRD_RANK)).isEqualTo(1);
+        assertThat(winStatistics.countByRank(Rank.SECOND_RANK)).isEqualTo(1);
+        assertThat(winStatistics.countByRank(Rank.FIRST_RANK)).isEqualTo(1);
     }
 
     @Test
-    void 당첨_통계값이_주어질_때_통계값_일치해야한다() {
-        Map<Integer, Integer> winStatistics = new HashMap<Integer, Integer>() {{
-            put(3, 1);
-        }};
+    void 당첨_통계값이_14중_4등_당첨된_결과가_주어질_때_수익률이_0_35로_일치해야한다() {
+        winStatistics = WinStatistics.from(LottoBalls.of(7, 8, 9, 41, 42, 43), lottoTickets);
         assertThat(lottoTickets.getRateOfReturn(14, winStatistics)).isEqualTo(0.35f);
     }
 }
