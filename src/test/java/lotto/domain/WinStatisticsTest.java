456282f90b720bbc4eb404e0bbfe486e790553da
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:06:46 +0900

    fix: LottoBals -> LottoTicket 클래스명 변경 및 당첨번호 클래스(WinnerNumbers) 추가에 따른 수정

--- src/test/java/lotto/domain/WinStatisticsTest.java
+++ src/test/java/lotto/domain/WinStatisticsTest.java
@@ -1,32 +1,33 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class WinStatisticsTest {
-    List<LottoBalls> lotteries;
+    List<LottoTicket> lotteries;
     LottoTickets lottoTickets;
     WinStatistics winStatistics;
 
     @BeforeEach
     void setUp() {
         lotteries = new ArrayList<>();
-        lotteries.add(LottoBalls.of(1, 2, 3, 7, 8, 9));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 8, 9));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 9));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 10));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 6));
+        lotteries.add(LottoTicket.of(1, 2, 3, 7, 8, 9));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 8, 9));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 5, 9));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 5, 10));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 5, 6));
         lottoTickets = LottoTickets.from(lotteries);
     }
 
     @Test
     void 로또공들의_데이터가_주어질때_각_경우별로_1개씩_당첨되었을때_당첨_통계값이_일치해야한다() {
-        winStatistics = WinStatistics.from(LottoBalls.of(1, 2, 3, 4, 5, 6), LottoBall.select(10), lottoTickets);
+        WinnerNumbers winnerNumbers = WinnerNumbers.from(LottoTicket.of(1, 2, 3, 4, 5, 6), LottoBall.select(10));
+        winStatistics = WinStatistics.from(winnerNumbers, lottoTickets);
         assertThat(winStatistics.countByRank(Rank.FIFTH)).isEqualTo(1);
         assertThat(winStatistics.countByRank(Rank.FOURTH)).isEqualTo(1);
         assertThat(winStatistics.countByRank(Rank.THIRD)).isEqualTo(1);
