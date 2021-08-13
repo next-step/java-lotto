ee6fdaecbe4ac1636fec5695f99cc1daa202c6c7
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:28:18 +0900

    fix: 당첨번호 클래스(WinnerNumbers) 추가에 따른 수정

--- src/main/java/lotto/domain/WinStatistics.java
+++ src/main/java/lotto/domain/WinStatistics.java
@@ -1,22 +1,19 @@
 package lotto.domain;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 public class WinStatistics {
     private final Map<Rank, Integer> winStatistics;
 
-    private WinStatistics(LottoBalls winnerNumbers, LottoBall bonusBall, LottoTickets lottoTickets) {
-        winStatistics = new HashMap<>();
-
-        for (LottoBalls lottoBalls : lottoTickets.toLottoBallsList()) {
-            Rank rank = Rank.valueOf(lottoBalls.countMatchNumber(winnerNumbers), lottoBalls.contains(bonusBall.number()));
-            winStatistics.put(rank, winStatistics.getOrDefault(rank, 0) + 1);
-        }
+    private WinStatistics(WinnerNumbers winnerNumbers, LottoTickets lottoTickets) {
+        winStatistics = Collections.unmodifiableMap(
+            lottoTickets.calculateStatistics(winnerNumbers)
+        );
     }
 
-    public static WinStatistics from(LottoBalls winnerBalls, LottoBall bonusBall, LottoTickets lottoTickets) {
-        return new WinStatistics(winnerBalls, bonusBall, lottoTickets);
+    public static WinStatistics from(WinnerNumbers winnerNumbers, LottoTickets lottoTickets) {
+        return new WinStatistics(winnerNumbers, lottoTickets);
     }
 
     public int countByRank(Rank rank) {
