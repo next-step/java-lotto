f4d299832931791ba17869480e240554e54c31f4
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-13 13:28:48 +0900

    feat : 커스텀 구분자 확인하여 덧셈하도록 구현

--- src/main/java/step1/StringSumCalculator.java
+++ src/main/java/step1/StringSumCalculator.java
@@ -1,5 +1,8 @@
 package step1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringSumCalculator {
 
     public int sum(final String inputText) {
@@ -13,12 +16,23 @@ public class StringSumCalculator {
 
     private int splitAndSum(final String inputText) {
         int result = 0;
-        String[] inputNumbers = inputText.split(",|:");
+        String[] inputNumbers = splitInputText(inputText);
 
         for (String inputNumber : inputNumbers) {
             result += Integer.parseInt(inputNumber);
         }
         return result;
+    }
+
+    private String[] splitInputText(String inputText) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputText);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
+        return inputText.split(",|:");
     }
 
 }
