8cfb2a28e2a2bc0dd9ed338210794bc32f8096dc
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:17:33 +0900

    fix: LottoBals -> LottoTicket 클래스명 변경 및 당첨번호 클래스(WinnerNumbers) 추가에 따른 수정

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -1,32 +1,33 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class LottoTicketsTest {
-    List<LottoBalls> lotteries;
+    List<LottoTicket> lotteries;
     LottoTickets lottoTickets;
     WinStatistics winStatistics;
 
     @BeforeEach
     void setUp() {
         lotteries = new ArrayList<>();
-        lotteries.add(LottoBalls.of(1, 2, 3, 7, 8, 9));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 8, 9));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 9));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 10));
-        lotteries.add(LottoBalls.of(1, 2, 3, 4, 5, 6));
+        lotteries.add(LottoTicket.of(1, 2, 3, 7, 8, 9));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 8, 9));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 5, 9));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 5, 10));
+        lotteries.add(LottoTicket.of(1, 2, 3, 4, 5, 6));
         lottoTickets = LottoTickets.from(lotteries);
     }
 
     @Test
     void 당첨_통계값이_14중_4등_당첨된_결과가_주어질_때_수익률이_0_35로_일치해야한다() {
-        winStatistics = WinStatistics.from(LottoBalls.of(7, 8, 9, 41, 42, 43), LottoBall.select(44), lottoTickets);
+        WinnerNumbers winnerNumbers = WinnerNumbers.from(LottoTicket.of(7, 8, 9, 41, 42, 43), LottoBall.select(44));
+        winStatistics = WinStatistics.from(winnerNumbers, lottoTickets);
         assertThat(lottoTickets.getRateOfReturn(14, winStatistics)).isEqualTo(0.35f);
     }
 }
