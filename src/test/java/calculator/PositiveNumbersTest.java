ee0e39fed0e22b8d43384505a0e504fcbaaf9f8e
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:25:30 +0900

    refactor: import문 정리 및 코드 컨벤션에 따른 정리

--- src/test/java/calculator/PositiveNumbersTest.java
+++ src/test/java/calculator/PositiveNumbersTest.java
@@ -1,19 +1,18 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.Test;
 
 class PositiveNumbersTest {
     @Test
     void 입력값에서_음수를_포함하고_있으면_예외를_던진다() {
-        assertThatThrownBy(() -> new PositiveNumbers(new int[]{1, 2, -1}))
+        assertThatThrownBy(() -> new PositiveNumbers(new int[] {1, 2, -1}))
             .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     void 양수만을_입력으로_받으면_합을_반환한다() {
-        assertThat(new PositiveNumbers(new int[]{1, 3, 5}).sum()).isEqualTo(9);
+        assertThat(new PositiveNumbers(new int[] {1, 3, 5}).sum()).isEqualTo(9);
     }
 }
