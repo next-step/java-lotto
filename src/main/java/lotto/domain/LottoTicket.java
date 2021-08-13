027c227d8d3860650c92860ff043e1f2201c72ee
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:26:49 +0900

    fix: LottoBals -> LottoTicket 클래스명 변경에 따른 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,26 +1,32 @@
 package lotto.domain;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class LottoBalls {
+public class LottoTicket {
     private final static int LOTTO_BALLS_MAX_NUM = 6;
 
     private final Set<LottoBall> lottoBalls;
 
-    private LottoBalls(int... numbers) {
-        this.lottoBalls = Arrays.stream(numbers)
-            .mapToObj(LottoBall::select)
-            .collect(Collectors.toSet());
+    private LottoTicket(int... numbers) {
+        lottoBalls = Collections.unmodifiableSet(
+            Arrays.stream(numbers)
+                .mapToObj(LottoBall::select)
+                .collect(Collectors.toSet())
+        );
         validate();
     }
 
-    public static LottoBalls of(int... numbers) {
-        return new LottoBalls(numbers);
+    public static LottoTicket of(int... numbers) {
+        return new LottoTicket(numbers);
     }
 
-    public static LottoBalls createRandomNumber() {
+    public static LottoTicket createRandomNumber() {
         List<Integer> randomNumbers = IntStream
             .rangeClosed(LottoBall.LOTTO_BALL_NUMBER_MIN, LottoBall.LOTTO_BALL_NUMBER_MAX)
             .boxed()
@@ -29,7 +35,7 @@ public class LottoBalls {
         Collections.shuffle(randomNumbers);
         randomNumbers = new ArrayList<>(randomNumbers.subList(0, LOTTO_BALLS_MAX_NUM));
 
-        return LottoBalls.of(randomNumbers.stream()
+        return LottoTicket.of(randomNumbers.stream()
             .mapToInt(Integer::intValue)
             .toArray());
     }
@@ -45,8 +51,8 @@ public class LottoBalls {
             .anyMatch(lottoBall -> lottoBall.equals((LottoBall.select(number))));
     }
 
-    public int countMatchNumber(LottoBalls winnerNumbers) {
-        return (int) lottoBalls.stream()
+    public int countMatchNumber(LottoTicket winnerNumbers) {
+        return (int)lottoBalls.stream()
             .filter(lottoBall -> winnerNumbers.contains(lottoBall.number()))
             .count();
     }
