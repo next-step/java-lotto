25fda0894e7a708bec815f092a54bfbd2ec08c7b
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 15:35:42 +0900

    test: 로또티켓 클래스와 로또볼 클래스를 합쳐서 만든 당첨번호 클래스의 테스트 케이스 추가

---
+++ src/test/java/lotto/domain/WinnerNumbersTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class WinnerNumbersTest {
+    WinnerNumbers winnerNumbers = WinnerNumbers.from(LottoTicket.of(new int[] {1, 2, 3, 4, 5, 6}), LottoBall.select(7));
+
+    private static Stream<Arguments> 로또티켓을_입력으로_받으면_등급을_알수있다() {
+        return Stream.of(
+            Arguments.of(LottoTicket.of(new int[] {1, 2, 3, 34, 35, 36}), Rank.FIFTH),
+            Arguments.of(LottoTicket.of(new int[] {1, 2, 3, 4, 35, 36}), Rank.FOURTH),
+            Arguments.of(LottoTicket.of(new int[] {1, 2, 3, 4, 5, 36}), Rank.THIRD),
+            Arguments.of(LottoTicket.of(new int[] {1, 2, 3, 4, 5, 7}), Rank.SECOND),
+            Arguments.of(LottoTicket.of(new int[] {1, 2, 3, 4, 5, 6}), Rank.FIRST)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 로또티켓을_입력으로_받으면_등급을_알수있다(LottoTicket lottoTicket, Rank expected) {
+        assertThat(winnerNumbers.decideRank(lottoTicket)).isEqualTo(expected);
+    }
+
+}
