116538d1f8535d12e8a47380765a0afc271bfb2a
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-13 12:49:43 +0900

    test : 입력받은 문자열을 split 한 후 덧셈 결과가 정확하게 나오는지 테스트 작성

--- src/test/java/step1/StringSumCalculatorTest.java
+++ src/test/java/step1/StringSumCalculatorTest.java
@@ -18,4 +18,12 @@ class StringSumCalculatorTest {
         result = stringSumCalculator.sum("");
         assertThat(result).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("쉼표와 콜론으로 split한 숫자를 합해서 반환하는지 확인한다.")
+    void split_colon_comma_test() {
+        StringSumCalculator stringSumCalculator = new StringSumCalculator();
+        int result = stringSumCalculator.sum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
 }
