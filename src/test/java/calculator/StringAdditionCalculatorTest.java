e10250edce59d1789899ff18432b5ab01ee22747
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:24:08 +0900

    refactor: import문 정리

--- src/test/java/calculator/StringAdditionCalculatorTest.java
+++ src/test/java/calculator/StringAdditionCalculatorTest.java
@@ -1,16 +1,15 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringAdditionCalculatorTest {
     StringAdditionCalculator stringAdditionCalculator;
