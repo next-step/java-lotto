b4d69b7e4befdf107a0b4096a7e0cdd5d05a4c8b
ChangSubKwak <paradise1732@gmail.com>
2021-08-13 16:33:03 +0900

    fix: LottoBals -> LottoTicket 클래스명 변경 및 당첨번호 클래스(WinnerNumbers) 추가에 따른 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,16 @@
 package lotto.controller;
 
-import lotto.domain.LottoBall;
-import lotto.domain.LottoBalls;
-import lotto.domain.LottoTickets;
-import lotto.domain.WinStatistics;
-import lotto.view.InputView;
-import lotto.view.ResultView;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+
+import lotto.domain.LottoBall;
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTickets;
+import lotto.domain.WinStatistics;
+import lotto.domain.WinnerNumbers;
+import lotto.view.InputView;
+import lotto.view.ResultView;
 
 public class LottoController {
     public static void main(String[] args) {
@@ -18,28 +19,29 @@ public class LottoController {
         LottoController lottoController = new LottoController();
 
         int lottoCount = inputView.inputLottoBuyAmount() / 1000;
-        List<LottoBalls> lottoBallsList = lottoController.createLottoBalls(resultView, lottoCount);
-        LottoTickets lottoTickets = LottoTickets.from(lottoBallsList);
+        List<LottoTicket> lottoTicketList = lottoController.createLottoBalls(resultView, lottoCount);
+        LottoTickets lottoTickets = LottoTickets.from(lottoTicketList);
 
         WinStatistics winStatistics = lottoController.proceedStatistics(inputView, lottoTickets);
         resultView.outputStatistics(winStatistics, lottoTickets.getRateOfReturn(lottoCount, winStatistics));
         inputView.scannerClose();
     }
 
-    public List<LottoBalls> createLottoBalls(ResultView resultView, int lottoCount) {
-        List<LottoBalls> lottoBallsList = new ArrayList<>();
+    public List<LottoTicket> createLottoBalls(ResultView resultView, int lottoCount) {
+        List<LottoTicket> lottoTicketList = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
-            lottoBallsList.add(LottoBalls.createRandomNumber());
+            lottoTicketList.add(LottoTicket.createRandomNumber());
         }
-        resultView.outputLottoLotteries(lottoBallsList);
-        return lottoBallsList;
+        resultView.outputLottoLotteries(lottoTicketList);
+        return lottoTicketList;
     }
 
     public WinStatistics proceedStatistics(InputView inputView, LottoTickets lottoTickets) {
-        LottoBalls lottoBalls =
-            LottoBalls.of(Stream.of(inputView.inputLastWeekWinnerNumbers().split(","))
+        LottoTicket lottoTicket =
+            LottoTicket.of(Stream.of(inputView.inputLastWeekWinnerNumbers().split(","))
                 .mapToInt(s -> Integer.parseInt(s.trim()))
                 .toArray());
-        return WinStatistics.from(lottoBalls, LottoBall.select(inputView.inputBonusNumber()), lottoTickets);
+        WinnerNumbers winnerNumbers = WinnerNumbers.from(lottoTicket, LottoBall.select(inputView.inputBonusNumber()));
+        return WinStatistics.from(winnerNumbers, lottoTickets);
     }
 }
