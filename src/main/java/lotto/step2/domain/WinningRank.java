b1d33bb19acdda9597ef06f3a0cfe3037258c354
gunny6026 <gunny6026@naver.com>
2021-08-13 10:18:55 +0900

    feat : 3개 미만 일치 필드 생성

--- src/main/java/lotto/step2/domain/WinningRank.java
+++ src/main/java/lotto/step2/domain/WinningRank.java
@@ -2,6 +2,7 @@ package lotto.step2.domain;
 
 public enum WinningRank {
 
+    NONE_PLACE(0, 0),
     FOURTH_PLACE(3, 5000),
     THIRD_PLACE(4, 50_000),
     SECOND_PLACE(5, 1500_000),
@@ -23,6 +24,20 @@ public enum WinningRank {
 
     public int getWinnings() {
         return winnings;
+    }
+
+    static WinningRank findWinningRank(int count) {
+        for (WinningRank rank : WinningRank.values()) {
+            findWinningMatchCount(rank, count);
+        }
+       return WinningRank.NONE_PLACE;
+    }
+
+    static WinningRank findWinningMatchCount(WinningRank rank, int count) {
+        if (rank.matchCount == count) {
+            return rank;
+        }
+        return WinningRank.NONE_PLACE;
     }
 
 
