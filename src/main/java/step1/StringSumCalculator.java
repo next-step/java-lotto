99963e7b64ecbe553c6c1752a93bd0661476de0f
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-13 14:56:34 +0900

    refactor : 매직넘버, 매직 리터럴 상수로 변경, 메소드 분리 진행

--- src/main/java/step1/StringSumCalculator.java
+++ src/main/java/step1/StringSumCalculator.java
@@ -5,40 +5,54 @@ import java.util.regex.Pattern;
 
 public class StringSumCalculator {
 
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
+    private static final int NUMBER_RETURNED_WHEN_EMPTY_OR_NULL = 0;
+    private static final String MINUS_NUMBER_EXCEPTION_MESSAGE = "음수는 입력할 수 없습니다.";
+
     public int sum(final String inputText) {
-
-        if (inputText == null || inputText.trim().isEmpty()) {
-            return 0;
+        if (validationInputText(inputText)) {
+            return NUMBER_RETURNED_WHEN_EMPTY_OR_NULL;
         }
-
         return splitAndSum(inputText);
     }
 
-    private int splitAndSum(final String inputText) {
-        int result = 0;
-        String[] inputNumbers = splitInputText(inputText);
+    private boolean validationInputText(String inputText) {
+        return inputText == null || inputText.trim().isEmpty();
+    }
 
+    private int splitAndSum(final String inputText) {
+
+        String[] inputNumbers = splitInputText(inputText);
+        int result = 0;
         for (String inputNumber : inputNumbers) {
-            result += validationMinusNumber(Integer.parseInt(inputNumber));
+            result += getParseInt(inputNumber);
         }
         return result;
     }
 
     private String[] splitInputText(String inputText) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputText);
 
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputText);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
         }
 
-        return inputText.split(",|:");
+        return inputText.split(DEFAULT_DELIMITER);
     }
 
-    private int validationMinusNumber(int number) {
-        if(number < 0) {
-            throw new RuntimeException("음수는 입력할 수 없습니다.");
-        }
+    private int getParseInt(String inputNumber) {
+        int number = Integer.parseInt(inputNumber);
+        validationMinusNumber(number);
         return number;
+    }
+
+    private void validationMinusNumber(int number) {
+        if (number < 0) {
+            throw new RuntimeException(MINUS_NUMBER_EXCEPTION_MESSAGE);
+        }
     }
 }
