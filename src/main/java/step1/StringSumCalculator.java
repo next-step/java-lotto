6bdeebb78ea012ca0baee54853f286393baa2efa
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-13 12:40:15 +0900

    feat : 문자열 입력받을 때 빈값, null 인지 확인한다.

---
+++ src/main/java/step1/StringSumCalculator.java
@@ -0,0 +1,12 @@
+package step1;
+
+public class StringSumCalculator {
+
+    public int sum(String inputText) {
+        if (inputText == null || inputText.trim().isEmpty()) {
+            return 0;
+        }
+        return 1;
+    }
+
+}
