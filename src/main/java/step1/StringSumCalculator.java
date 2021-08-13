fd1fd9794a1e626475f6d177ec837447f1e13f70
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-13 12:50:17 +0900

    feat : 입력받은 문자열을 split 한 후 덧셈 결과가 정확하게 나오는지 테스트 작

--- src/main/java/step1/StringSumCalculator.java
+++ src/main/java/step1/StringSumCalculator.java
@@ -3,10 +3,18 @@ package step1;
 public class StringSumCalculator {
 
     public int sum(String inputText) {
+
         if (inputText == null || inputText.trim().isEmpty()) {
             return 0;
         }
-        return 1;
+
+        int result = 0;
+        String[] inputNumbers = inputText.split(",|:");
+
+        for (String inputNumber : inputNumbers) {
+            result += Integer.parseInt(inputNumber);
+        }
+        return result;
     }
 
 }
