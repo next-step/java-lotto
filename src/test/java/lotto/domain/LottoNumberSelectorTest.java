f110bea8cdda9b47f435e97a2876002e40a265eb
sixthou <justyuk@naver.com>
2021-08-14 22:15:25 +0900

    test : 자동생성 값 자리수, 범위 체크 테스트

--- src/test/java/lotto/domain/LottoNumberSelectorTest.java
+++ src/test/java/lotto/domain/LottoNumberSelectorTest.java
@@ -2,14 +2,34 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class LottoNumberSelectorTest {
 
-    @DisplayName("랜덥 한 6자리 수 추출 테스트")
+    @DisplayName("랜덤한 6개의 수가 추출 되어여 함.")
     @Test
     void select_number_test() {
         assertThat(LottoNumberSelector.selectNumbers().size()).isEqualTo(6);
+    }
+
+    @DisplayName("값이 1부터 45 사이인지 테스트")
+    @RepeatedTest(100)
+    void name() {
+        List<Integer> selectedNumbers = LottoNumberSelector.selectNumbers();
+        for (Integer number : selectedNumbers) {
+            assertThat(number).isBetween(1, 45);
+        }
+    }
+
+    @DisplayName("6개의 수가 중복되어 나올수가 없음.")
+    @Test
+    void do_not_allow_duplicate_number() {
+        Set<Integer> set = new HashSet<>(LottoNumberSelector.selectNumbers());
+        assertThat(set.size()).isEqualTo(6);
     }
 }
