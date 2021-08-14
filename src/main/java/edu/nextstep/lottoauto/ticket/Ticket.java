4c046d293a470c76b4b3affebe3deaa04d2fa088
beadoer1 <beadoer1@gmail.com>
2021-08-15 03:22:38 +0900

    feat(Ticket) : checkAndUpdateWinningPrize() 구현(들여쓰기 refactoring 필요)

--- src/main/java/edu/nextstep/lottoauto/ticket/Ticket.java
+++ src/main/java/edu/nextstep/lottoauto/ticket/Ticket.java
@@ -13,12 +13,17 @@ import java.util.List;
 
 public class Ticket {
     private final List<Integer> numbers;
-    private final Prize prize;
+    private Prize prize;
 
 
-    public Ticket(List<Integer> numbersTmp) {
-        this.numbers = numbersTmp;
+    private Ticket(List<Integer> numbers) {
+        this.numbers = numbers;
         this.prize = null;
+    }
+
+    private Ticket(List<Integer> numbers, Prize prize) {
+        this.numbers = numbers;
+        this.prize = prize;
     }
 
     public static Ticket create(NumberMaker numberMaker) {
@@ -26,6 +31,21 @@ public class Ticket {
     }
 
     public void checkAndUpdateWinningPrize(WinningNumbers winningNumbers) {
+        int count = 0;
+
+        for (Integer winningNumber : winningNumbers.getWinningNumbers()) {
+            if (numbers.contains(winningNumber)) {
+                count++;
+            }
+        }
+
+        if (count >= 3) {
+            for (Prize p : Prize.values()) {
+                if (p.getValue() == count) {
+                    this.prize = p;
+                }
+            }
+        }
     }
 
     public List<Integer> getNumbers() {
