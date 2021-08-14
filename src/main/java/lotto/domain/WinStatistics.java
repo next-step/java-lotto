ff337bfb70e946bd9bbc1badf6682464e584252d
ChangSubKwak <paradise1732@gmail.com>
2021-08-14 22:11:33 +0900

    fix: WinStatistics 클래스의 본질에 맞는 함수 포함(getRateOfReturn, calculatePrizeMoney), 정적팩터리 메서드 추가

--- src/main/java/lotto/domain/WinStatistics.java
+++ src/main/java/lotto/domain/WinStatistics.java
@@ -1,26 +1,52 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
 public class WinStatistics {
     private final Map<Rank, Integer> winStatistics;
 
+    private WinStatistics(Map<Rank, Integer> winStatistics) {
+        this.winStatistics = winStatistics;
+    }
+
     private WinStatistics(WinnerNumbers winnerNumbers, LottoTickets lottoTickets) {
         winStatistics = Collections.unmodifiableMap(
             lottoTickets.calculateStatistics(winnerNumbers)
+                .result()
         );
+    }
+
+    public static WinStatistics from(Map<Rank, Integer> winStatistics) {
+        return new WinStatistics(winStatistics);
     }
 
     public static WinStatistics from(WinnerNumbers winnerNumbers, LottoTickets lottoTickets) {
         return new WinStatistics(winnerNumbers, lottoTickets);
     }
 
-    public int countByRank(Rank rank) {
-        return winStatistics.getOrDefault(rank, 0);
+    private long calculatePrizeMoney(Rank rank, int lottoCount) {
+        Rank rankResult = Arrays.stream(Rank.values())
+            .filter(e -> e == rank)
+            .findFirst()
+            .orElse(Rank.MISS);
+
+        return rankResult.money()
+            .multiply(lottoCount)
+            .amount();
+    }
+
+    public float getRateOfReturn(int lottoCount) {
+        long prizeMoneySum = winStatistics
+            .entrySet()
+            .stream()
+            .mapToLong(e -> calculatePrizeMoney(e.getKey(), e.getValue()))
+            .sum();
+        return (float)(Math.floor(prizeMoneySum / (lottoCount * 10.0f)) / 100.0f);
     }
 
     public Map<Rank, Integer> result() {
-        return winStatistics;
+        return Collections.unmodifiableMap(winStatistics);
     }
 }
