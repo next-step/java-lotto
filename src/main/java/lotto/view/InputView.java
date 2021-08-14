8c5d99cc662e48e846e457f29e51024495b27a30
ChangSubKwak <paradise1732@gmail.com>
2021-08-14 22:20:15 +0900

    feat: 수동 로또티켓 개수 및 번호 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,17 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import lotto.domain.LottoTicket;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_LOTTO_BUY_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTOTICKET_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String INPUT_MANUAL_LOTTOTICKETS = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String INPUT_LAST_WEEK_WINNER_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 
@@ -13,7 +20,9 @@ public class InputView {
     }
 
     private int inputIntValue() {
-        return scanner.nextInt();
+        int inputValue = scanner.nextInt();
+        scanner.nextLine();
+        return inputValue;
     }
 
     public int inputLottoBuyAmount() {
@@ -25,8 +34,30 @@ public class InputView {
         return amount;
     }
 
+    public int inputManualLottoTicketCount() {
+        System.out.println("\n" + INPUT_MANUAL_LOTTOTICKET_COUNT);
+        return inputIntValue();
+    }
+
+    public List<LottoTicket> inputManualLottoTickets(int lottoTicketCount) {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        if (lottoTicketCount > 0) {
+            System.out.println("\n" + INPUT_MANUAL_LOTTOTICKETS);
+        }
+
+        for (int i = 0; i < lottoTicketCount; i++) {
+            int[] lottoTicketIntArray = getLottoTicketIntArray();
+            lottoTickets.add(LottoTicket.of(lottoTicketIntArray));
+        }
+        return lottoTickets;
+    }
+
+    private int[] getLottoTicketIntArray() {
+        return Arrays.stream(inputStringValue().split(",")).mapToInt(s -> Integer.parseInt(s.trim())).toArray();
+    }
+
     public String inputLastWeekWinnerNumbers() {
-        System.out.println(INPUT_LAST_WEEK_WINNER_NUMBERS);
+        System.out.println("\n" + INPUT_LAST_WEEK_WINNER_NUMBERS);
         return inputStringValue();
     }
 
