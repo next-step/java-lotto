5fbf2930ad1d69b7ee890ada389299d24b3c8b8e
hjjangDev <ju0398@hanmail.net>
2021-08-14 22:47:42 +0900

    test : WinningLottoTest 추가

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -9,30 +9,43 @@ public class LottoResultTest {
     @Test
     @DisplayName("로또 1등(6개 숫자 일치) 테스트")
     void 로또_1등_테스트() {
-        assertThat(LottoResult.getLottoResult(6)).isEqualTo(LottoResult.FIRST);
+        assertThat(LottoResult.getLottoResult(6, false)).isEqualTo(LottoResult.FIRST);
     }
 
     @Test
-    @DisplayName("로또 2등(5개 숫자 일치) 테스트")
+    @DisplayName("로또 2등(5개 숫자, 보너스번호 일치) 테스트")
     void 로또_2등_테스트() {
-        assertThat(LottoResult.getLottoResult(5)).isEqualTo(LottoResult.SECOND);
+        assertThat(LottoResult.getLottoResult(5, true)).isEqualTo(LottoResult.SECOND);
     }
 
     @Test
-    @DisplayName("로또 3등(4개 숫자 일치) 테스트")
+    @DisplayName("로또 3등(5개 숫자 일치) 테스트")
     void 로또_3등_테스트() {
-        assertThat(LottoResult.getLottoResult(4)).isEqualTo(LottoResult.THIRD);
+        assertThat(LottoResult.getLottoResult(5, false)).isEqualTo(LottoResult.THIRD);
     }
 
     @Test
-    @DisplayName("로또 4등(3개 숫자 일치) 테스트")
+    @DisplayName("로또 4등(4개 숫자 일치) 테스트")
     void 로또_4등_테스트() {
-        assertThat(LottoResult.getLottoResult(3)).isEqualTo(LottoResult.FOURTH);
+        assertThat(LottoResult.getLottoResult(4, false)).isEqualTo(LottoResult.FOURTH);
+    }
+
+    @Test
+    @DisplayName("로또 5등(3개 숫자 일치) 테스트")
+    void 로또_5등_테스트() {
+        assertThat(LottoResult.getLottoResult(3, false)).isEqualTo(LottoResult.FIFTH);
     }
 
     @Test
     @DisplayName("로또 0개 숫자 일치 테스트")
-    void 로또_0개일치_테스트() {
-        assertThat(LottoResult.getLottoResult(0)).isEqualTo(LottoResult.FAIL);
+    void 로또_0개_일치_테스트() {
+        assertThat(LottoResult.getLottoResult(0, false)).isEqualTo(LottoResult.FAIL);
     }
+
+    @Test
+    @DisplayName("로또 2개 숫자 일치 테스트")
+    void 로또_2개_일치_테스트() {
+        assertThat(LottoResult.getLottoResult(2, true)).isEqualTo(LottoResult.FAIL);
+    }
+
 }
