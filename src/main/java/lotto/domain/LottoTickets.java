435fa1ae9e3c1ee01d5efcff58359e68c40e4cfe
ChangSubKwak <paradise1732@gmail.com>
2021-08-14 22:18:44 +0900

    fix: WinStatistics 클래스로 함수(getRateOfReturn, calculatePrizeMoney) 이동, calculateStatistics함수의 반환 자료 변경

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -17,33 +16,13 @@ public class LottoTickets {
         return new LottoTickets(lottoTickets);
     }
 
-    private long calculatePrizeMoney(Rank rank, int lottoCount) {
-        Rank rankResult = Arrays.stream(Rank.values())
-            .filter(e -> e == rank)
-            .findFirst()
-            .orElse(Rank.MISS);
-
-        return rankResult.money()
-            .multiply(lottoCount)
-            .amount();
-    }
-
-    public float getRateOfReturn(int lottoCount, WinStatistics winStatistics) {
-        long prizeMoneySum = winStatistics.result()
-            .entrySet()
-            .stream()
-            .mapToLong(e -> calculatePrizeMoney(e.getKey(), e.getValue()))
-            .sum();
-        return (float)(Math.floor(prizeMoneySum / (lottoCount * 10.0f)) / 100.0f);
-    }
-
-    public Map<Rank, Integer> calculateStatistics(WinnerNumbers winnerNumbers) {
+    public WinStatistics calculateStatistics(WinnerNumbers winnerNumbers) {
         Map<Rank, Integer> winStatisticsDTO = new HashMap<>();
         for (LottoTicket lottoTicket : lottoTickets) {
             Rank rank = winnerNumbers.decideRank(lottoTicket);
             winStatisticsDTO.put(rank, winStatisticsDTO.getOrDefault(rank, 0) + 1);
         }
-        return winStatisticsDTO;
+        return WinStatistics.from(winStatisticsDTO);
     }
 
 }
