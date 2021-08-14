d3f7a6d65313746595acc13c681f6fa8ebc4203d
kdh85 <modudamba@naver.com>
2021-08-14 17:51:42 +0900

    refactor : 로또게임(수동)
    내용
    * 테스트소스 코드 정리.

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
 import java.util.Collections;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
