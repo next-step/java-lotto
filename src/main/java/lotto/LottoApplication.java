a9a345e3041c44c32d5e939d72c2347b88fe3b95
sixthou <justyuk@naver.com>
2021-08-14 21:50:12 +0900

    refactor : main클래스 정리

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -7,6 +7,7 @@ import lotto.view.OutputView;
 public class LottoApplication {
 
     public static void main(String[] args) {
+
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
@@ -14,14 +15,15 @@ public class LottoApplication {
         int price = Integer.parseInt(inputView.getNumber());
 
         Lotto lotto = new Lotto(price);
+
         outputView.printGameNum(lotto.getGameNum());
-
         outputView.printTickets(lotto.getLottoGame());
-
         outputView.printLastWinningNumber();
+
         lotto.setWinningNumber(inputView.getNumber());
         lotto.checkResult();
         lotto.setResult();
+
         outputView.printResult(lotto.getResult());
         outputView.printProfitValue(lotto.getProfitRate());
 
