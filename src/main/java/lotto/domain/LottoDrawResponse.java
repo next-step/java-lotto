f3f432b1879ea4187a2c428011901ed28e13afca
harris <harrisleesh@gmail.com>
2021-08-15 23:42:28 +0900

    feat : Create LottoDrawResponse

---
+++ src/main/java/lotto/domain/LottoDrawResponse.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoDrawResponse {
+    private final Map<Award, Integer> lottoResult = new HashMap<>();
+
+    public LottoDrawResponse() {
+        lottoResult.put(Award.FIRST, 0);
+        lottoResult.put(Award.SECOND, 0);
+        lottoResult.put(Award.THIRD, 0);
+        lottoResult.put(Award.FOURTH, 0);
+        lottoResult.put(Award.BANG, 0);
+    }
+
+    public void addOne(Award award) {
+        lottoResult.computeIfPresent(award, (key, value) -> ++value);
+    }
+
+    public Map<Award, Integer> getLottoResult() {
+        return Collections.unmodifiableMap(lottoResult);
+    }
+}
