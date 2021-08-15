4fe8a28d1bbef92db3eb76d3ad157ff06271f8a9
0n1dev <all_mind@naver.com>
2021-08-16 01:10:24 +0900

    feat : 테스트 코드에 따라 기능 구현
    - 테스트 코드에 따라 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -9,6 +9,10 @@ public class Lottos {
 
 	private final List<Lotto> lottos;
 
+	private Lottos() {
+		this.lottos = new ArrayList<>();
+	}
+
 	private Lottos(List<Lotto> lottos) {
 		this.lottos = lottos;
 	}
@@ -23,11 +27,23 @@ public class Lottos {
 		return new Lottos(lottos);
 	}
 
+	public static Lottos generateLottos() {
+		return new Lottos();
+	}
+
 	public RevenueRecord toRevenueRecord() {
 		return new RevenueRecord(lottos);
 	}
 
 	public List<Lotto> getLottos() {
 		return lottos;
+	}
+
+	public void addManualLotto(Lotto lotto) {
+		lottos.add(lotto);
+	}
+
+	public void addAutoLotto() {
+		lottos.add(Lotto.of(AutoLottoNumbersGeneratorUtils.generateRandomNumbers()));
 	}
 }
