1e3bb6bba8d2e245e5d451d47f784f56d6dc74ef
harris <harrisleesh@gmail.com>
2021-08-15 23:45:29 +0900

    refactor : Remove useless test

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -1,61 +1,47 @@
 package lotto.domain;
 
+import lotto.domain.dto.LottoPurchaseResponse;
+import lotto.util.LottoListGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.IntStream;
+import java.util.List;
 
+import static lotto.domain.LottoTest.winnerLotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LottoGameTest {
 
     public static final int TICKET_NUMBER = 5;
-    private LottoGame lottoGame = new LottoGame();
-    private Set<LottoNumber> winnerNumbers = new HashSet<>();
-    private Lotto winnerLotto;
+    private LottoGame lottoGame;
+    private List<Lotto> lottos;
 
     @BeforeEach
     void setUp() {
-        IntStream.rangeClosed(1, 6).forEach(number -> winnerNumbers.add(new LottoNumber(number)));
-        winnerLotto = new Lotto(winnerNumbers);
+        lottoGame = new LottoGame();
+        lottos = LottoListGenerator.creatLottos(TICKET_NUMBER);
     }
 
     @Test
-    @DisplayName("lottoGame의 purchase()함수 호출 시 생성된 Lottos의 수가 ticket의 수와 같은 지 확인한다.")
+    @DisplayName("lottoGame의 purchase함수 호출 시 인자값의 로또리스트와 응답의 로또리스트가 같은 지 확인한다.")
     void purchase() {
-        lottoGame.purchase(TICKET_NUMBER);
-        assertEquals(lottoGame.getLottos().getLottos().size(), TICKET_NUMBER);
+        LottoPurchaseResponse lottoPurchaseResponse = lottoGame.purchase(lottos);
+        assertEquals(lottoPurchaseResponse.getLottoList().size(), TICKET_NUMBER);
+        assertTrue(lottoPurchaseResponse.getLottoList().containsAll(lottos));
     }
 
     @Test
-    @DisplayName("로또게임의 drawLotto 함수 호출 시 각 로또들의 당첨상태(Award)가 변경된다.")
+    @DisplayName("로또게임의 drawLotto 함수 호출 결과 수의 총합은 티켓넘버와 같음을 확인한다.")
     void drawLotto() {
-        lottoGame.purchase(TICKET_NUMBER);
-        for (Lotto lotto : lottoGame.getLottos().getLottos()) {
-            assertEquals(lotto.getAward(), Award.UNIDENTIFIED);
-        }
-
-        lottoGame.drawLotto(winnerLotto);
-        for (Lotto lotto : lottoGame.getLottos().getLottos()) {
-            assertNotEquals(lotto.getAward(), Award.UNIDENTIFIED);
-        }
-
-    }
-
-    @Test
-    @DisplayName("countWinners 함수를 호출했을 때 각각의 합이 ticket의 숫자와 같은 지 확인한다.")
-    void countWinners() {
-        lottoGame.purchase(TICKET_NUMBER);
-        lottoGame.drawLotto(winnerLotto);
-        long countAll = lottoGame.countWinners(Award.FIRST) +
-                lottoGame.countWinners(Award.SECOND) +
-                lottoGame.countWinners(Award.THIRD) +
-                lottoGame.countWinners(Award.FOURTH) +
-                lottoGame.countWinners(Award.BANG);
-        assertEquals(countAll, TICKET_NUMBER);
+        lottoGame.purchase(lottos);
+        LottoDrawResponse lottoResult = lottoGame.drawLotto(winnerLotto);
+        int sum = lottoResult.getLottoResult()
+                .values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        assertEquals(sum, TICKET_NUMBER);
     }
 }
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -6,33 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.IntStream;
 
+import static lotto.domain.LottoTest.winnerLotto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottosTest {
 
     private final int TICKET_NUMBER = 3;
-    private Set<LottoNumber> winnerNumbers = new HashSet<>();
     private Set<LottoNumber> lottoNumberList = new HashSet<>();
-    private Lotto winnerLotto;
     private Lotto lotto;
     private List<Lotto> lottoList = new ArrayList<>();
     private Lottos lottos;
 
     @BeforeEach
     void setUp() {
-        IntStream.rangeClosed(1, 6).forEach(number -> winnerNumbers.add(new LottoNumber(number)));
         IntStream.rangeClosed(1, 6).forEach(number -> lottoNumberList.add(new LottoNumber(number)));
-        winnerLotto = new Lotto(winnerNumbers);
         lotto = new Lotto(lottoNumberList);
-
         IntStream.range(0, TICKET_NUMBER).forEach(index -> lottoList.add(lotto));
-
         lottos = new Lottos(lottoList);
     }
 
@@ -43,24 +35,18 @@ class LottosTest {
     }
 
     @Test
-    @DisplayName("drawLottos 함수를 호출했을때 각 lotto의 award 상태가 변경됨을 확인한다.")
+    @DisplayName("1등로또로 생성한 Lottos의 drawLotto 함수 호출 시 모두 1등임을 확인한다.")
     void drawLottos() {
-        for (Lotto lotto : lottos.getLottos()) {
-            assertEquals(lotto.getAward(),Award.UNIDENTIFIED);
-        }
-
-        lottos.drawLottos(winnerLotto);
-
-        for (Lotto lotto : lottos.getLottos()) {
-            assertEquals(lotto.getAward(),Award.FIRST);
-        }
+        LottoDrawResponse lottoDrawResponse = lottos.drawLottos(winnerLotto);
+        assertEquals(lottoDrawResponse.getLottoResult().get(Award.FIRST), TICKET_NUMBER);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"FIRST:3","SECOND:0"},delimiter = ':')
     @DisplayName("Award 파라미터와 함께 getWinners 함수를 호출하면 당첨 로또의 수를 리턴한다")
     void countWinners(String type, int match) {
-        lottos.drawLottos(winnerLotto);
-        assertEquals(lottos.countWinners(Award.valueOf(type)), match);
+        LottoDrawResponse lottoDrawResponse = lottos.drawLottos(winnerLotto);
+        Map<Award, Integer> lottoResult = lottoDrawResponse.getLottoResult();
+        assertEquals(lottoResult.get(Award.valueOf(type)), match);
     }
 }
