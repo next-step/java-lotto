bab00e094e648f6d090f972b5f5959f9167a646b
0n1dev <all_mind@naver.com>
2021-08-16 01:08:06 +0900

    feat : 수동 구매 + 자동 구매 Lottos 클래스에 등록 테스트 케이스 작성
    - 수동 구매 + 자동 구매 Lottos 클래스에 등록 테스트 케이스 작성

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,9 +1,14 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottosTest {
 
@@ -13,5 +18,17 @@ class LottosTest {
 		assertDoesNotThrow(() -> {
 			Lottos.generateAutoLottos(5);
 		});
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0=false", "1=false", "2=true"}, delimiter = '=')
+	@DisplayName("수동 구매 + 자동 구매 Lottos 클래스에 등록 후 사이즈 비교")
+	void addLottoSuccess(int size, boolean expected) {
+		Lottos lottos = Lottos.generateLottos();
+
+		lottos.addManualLotto(Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6)));
+		lottos.addAutoLotto();
+
+		assertThat((lottos.getLottos().size() == size)).isEqualTo(expected);
 	}
 }
