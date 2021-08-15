8270a0bc9d7a08bf92adb15e8c6fc6342af2d16c
harris <harrisleesh@gmail.com>
2021-08-16 00:48:54 +0900

    feat : Add bonus in LottoWinnersDto

--- src/main/java/lotto/domain/dto/LottoWinnersDto.java
+++ src/main/java/lotto/domain/dto/LottoWinnersDto.java
@@ -5,11 +5,13 @@ import lotto.domain.Award;
 public class LottoWinnersDto {
     private int amount;
     private int matchNumbers;
+    private boolean bonus;
     private long countWinners;
 
     public LottoWinnersDto(Award award, long countWinners) {
         this.amount = award.getAmount();
         this.matchNumbers = award.getMatchNumbers();
+        this.bonus = award.isBonus();
         this.countWinners = countWinners;
     }
 
@@ -19,6 +21,10 @@ public class LottoWinnersDto {
 
     public int getMatchNumbers() {
         return matchNumbers;
+    }
+
+    public boolean isBonus() {
+        return bonus;
     }
 
     public long getCountWinners() {
