ee57f388d9722dfebf2a2710e6f0358cc694c8ee
sangw0804 <sangw0804@likelion.org>
2021-08-15 16:10:46 +0900

    refactor: 클래스 값 위치변경

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -3,10 +3,10 @@ package lotto.model;
 import java.util.Objects;
 
 public class LottoNumber {
-    private final int number;
-
     public static final int MAX_NUMBER = 45;
     public static final int MIN_NUMBER = 1;
+
+    private final int number;
 
     public LottoNumber(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
