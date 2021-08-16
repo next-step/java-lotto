c931183fbba89d1b88476064d12d25dfdd43ea86
gunny6026 <gunny6026@naver.com>
2021-08-16 20:22:15 +0900

    test : 당첨 번호를 로또로 변환하는 테스트

--- src/test/java/lotto/step3/domain/LottoCreatorTest.java
+++ src/test/java/lotto/step3/domain/LottoCreatorTest.java
@@ -11,9 +11,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LottoCreatorTest {
     @Test
-    @DisplayName("생성된 로또의 번호 개수는 6개 입니다.")
+    @DisplayName("생성된 로또가 올바른 로또 인지 검증.")
     void createLotto() {
         Set<LottoNumber> lotto = LottoCreator.createLotto();
         assertThat(lotto.size()).isEqualTo(6);
     }
+
+    @Test
+    @DisplayName("입력한 지난 주 당첨 번호가 올바른 로또 인지 검증 - 6개의 로또 번호")
+    void convertToLotto() {
+        String input = "1, 2, 3, 4, 5, 6";
+        Set<LottoNumber> lotto = LottoCreator.convertToLotto(input);
+        assertThat(lotto.size()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("입력한 지난 주 당첨 번호가 올바른 로또 인지 검증 - 중복된 숫자는 올 수 없다")
+    void convertToLotto2() {
+        String input = "3, 2, 3, 4, 5, 6";
+        Set<LottoNumber> lotto = LottoCreator.convertToLotto(input);
+        assertThatThrownBy(() -> new Lottery(lotto)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력한 지난 주 당첨 번호가 올바른 로또 인지 검증 - 로또 번호는 1~45 숫자만 올수 있다.")
+    void convertToLotto3() {
+        String input = "46, 2, 3, 4, 5, 6";
+        assertThatThrownBy(() -> LottoCreator.convertToLotto(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력한 지난 주 당첨 번호가 올바른 로또 인지 검증 - 로또 번호는 1~45 숫자만 올수 있다.")
+    void convertToLotto4() {
+        String input = "1, 2, 3, 4, 0, 6";
+        assertThatThrownBy(() -> LottoCreator.convertToLotto(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
