7e5516753ead2990596ddbfc271dc1b1fa2686ac
sangw0804 <sangw0804@likelion.org>
2021-08-16 23:30:48 +0900

    feat(InputView): LottoNumbers 입력 validation 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+import lotto.model.LottoNumbers;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -14,15 +18,29 @@ public class InputView {
     }
 
     public static List<Integer> readNumbers() {
-        String raw = null;
-        while (raw == null || raw.isEmpty()) {
-            System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-            raw = scanner.nextLine();
+        List<Integer> numbers = Collections.emptyList();
+        while (numbers.size() != Lotto.LOTTO_NUMBER_SIZE) {
+            String raw = getRawString();
+            numbers = translateNumbers(raw);
         }
 
+        return numbers;
+    }
+
+    private static String getRawString () {
+        String raw = null;
+        while (raw == null || raw.isEmpty()) {
+            System.out.println("지난 주 당첨 번호를 입력해 주세요. (1 ~ 45 사이의 distinct 한 6개 숫자로 입력)");
+            raw = scanner.nextLine();
+        }
+        return raw;
+    }
+
+    private static List<Integer> translateNumbers(String raw) {
         return Arrays.stream(raw.split(","))
-            .map(String::trim)
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
