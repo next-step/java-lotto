0f78bd74d15b207cfeb785f758067b019d6861f4
harris <harrisleesh@gmail.com>
2021-08-16 19:49:32 +0900

    test : Create new test as validateBonusNumber

--- src/test/java/lotto/domain/WinnerLottoTest.java
+++ src/test/java/lotto/domain/WinnerLottoTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,6 +12,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WinnerLottoTest {
@@ -31,6 +33,17 @@ class WinnerLottoTest {
         IntStream.rangeClosed(1, 6).forEach(number -> lottoNumberList.add(new LottoNumber(number)));
         lotto = new Lotto(lottoNumberList);
     }
+
+    @Test
+    @DisplayName("로또 당첨번호와 보너스번호가 중복이면 IllegalArgumentException이 발생한다.")
+    void validateBonusNumber(){
+        Lotto winnerLottoNumbers = createWinnerLottoNumbers();
+        int bonus = 1;
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinnerLotto(winnerLottoNumbers, new LottoNumber(bonus)))
+                .withMessage("로또번호와 보너스 번호가 중복입니다. bonusNumber : " + bonus);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"1,2,3,4,5,6:7:FIRST", "1,2,3,4,5,7:6:SECOND", "1,2,3,4,5,8:9:THIRD", "1,2,3,4,8,9:10:FOURTH", "1,2,3,8,9,10:11:FIFTH", "1,7,8,9,10,11:12:BANG", "7,8,9,10,11,12:13:BANG"},
             delimiter = ':')
