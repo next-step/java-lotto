a4ca176988c3fa7f10ff3223aa1cfe0a6cf5ac59
gunny6026 <gunny6026@naver.com>
2021-08-16 23:12:22 +0900

    test : 당첨 번호와 보너스 볼 테스트 케이스

--- src/main/java/lotto/step3/domain/Winning.java
+++ src/main/java/lotto/step3/domain/Winning.java
@@ -7,7 +7,22 @@ public class Winning {
     private LottoNumber bonusNumber;
 
     public Winning(Set<LottoNumber> winnerNumber, LottoNumber bonusNumber) {
+        if (winnerNumber.size() != Lottery.LOTTO_COUNT){
+            throw new IllegalArgumentException("로또 번호는" + Lottery.LOTTO_COUNT + "개 이어야 합니다.");
+        }
+
+        if (winnerNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 볼은 로또 번호에 없는 숫자 이어야 합니다.");
+        }
         this.winnerNumber = winnerNumber;
         this.bonusNumber = bonusNumber;
+    }
+
+    public Set<LottoNumber> getWinnerNumber() {
+        return winnerNumber;
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
     }
 }
---
+++ src/test/java/lotto/step3/domain/WinningTest.java
@@ -0,0 +1,20 @@
+package lotto.step3.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WinningTest {
+
+    @Test
+    @DisplayName("보너스 볼이 당첨 번호에 포함되면 예외를 발생시킨다.")
+    void isValidWinningLotto() {
+        Set<LottoNumber> winningNumbers = LottoCreator.convertToLotto("1,2,3,4,5,6");
+        assertThatThrownBy(()-> new Winning(winningNumbers , new LottoNumber(3))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
