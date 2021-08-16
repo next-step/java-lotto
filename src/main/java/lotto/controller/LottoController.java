8c2ecf4a4d55e618a8e96fdaa4c2fefa05aa31c5
ChangSubKwak <paradise1732@gmail.com>
2021-08-16 20:06:00 +0900

    fix: 로또티켓생성(LottoTicketGenerator)의 역할을 하는 클래스를 사용함에 따른 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,11 @@
 package lotto.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import lotto.controller.generator.AutoLottoTicketGenerator;
+import lotto.controller.generator.LottoTicketGenerator;
+import lotto.controller.generator.ManualLottoTicketGenerator;
 import lotto.domain.LottoBall;
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoTickets;
@@ -20,43 +21,25 @@ public class LottoController {
         LottoController lottoController = new LottoController();
 
         int lottoCount = inputView.inputLottoBuyAmount() / 1000;
-        List<LottoTicket> lottoTicketList = lottoController.createLottoBalls(inputView, resultView, lottoCount);
-        LottoTickets lottoTickets = LottoTickets.from(lottoTicketList);
-
+        LottoTickets lottoTickets = LottoTickets.from(getLottoTickets(inputView, resultView, lottoCount));
         WinStatistics winStatistics = lottoController.proceedStatistics(inputView, lottoTickets);
-        resultView.outputStatistics(winStatistics, winStatistics.getRateOfReturn(lottoCount));
+
+        resultView.outputStatistics(winStatistics, winStatistics.getRateOfReturn());
         inputView.scannerClose();
     }
 
-    public List<LottoTicket> createLottoBalls(InputView inputView, ResultView resultView, int lottoCount) {
-        int manualLottoTicketCount = inputView.inputManualLottoTicketCount();
-        int autoLottoTicketCount = lottoCount - manualLottoTicketCount;
+    private static List<LottoTicket> getLottoTickets(InputView inputView, ResultView resultView, int lottoCount) {
+        int manualLottoCount = inputView.inputManualLottoTicketCount();
 
-        List<LottoTicket> manualLottoTickets = getManualLottoTickets(inputView, manualLottoTicketCount);
-        List<LottoTicket> autoLottoTickets = getAutoLottoTickets(autoLottoTicketCount);
+        List<LottoTicket> manualLottoTickets = LottoTicketGenerator.createLottoTickets(new ManualLottoTicketGenerator(),
+            manualLottoCount);
+        List<LottoTicket> autoLottoTickets = LottoTicketGenerator.createLottoTickets(new AutoLottoTicketGenerator(),
+            lottoCount - manualLottoCount);
+        manualLottoTickets.addAll(autoLottoTickets);
 
         resultView.outputLottoLotteries(manualLottoTickets, autoLottoTickets);
-        manualLottoTickets.addAll(autoLottoTickets);
-        return manualLottoTickets;
-    }
 
-    private List<LottoTicket> getManualLottoTickets(InputView inputView, int manualLottoTicketCount) {
-        List<LottoTicket> manualLottoTickets = new ArrayList<>();
-        List<String> manualLottoTicketsList = inputView.inputManualLottoTickets(manualLottoTicketCount);
-        for (int i = 0; i < manualLottoTicketCount; i++) {
-            String[] lottoTicketStringArray = manualLottoTicketsList.get(i).split(",");
-            int[] lottoTicketIntArray = Arrays.stream(lottoTicketStringArray).mapToInt(s -> Integer.parseInt(s.trim())).toArray();
-            manualLottoTickets.add(LottoTicket.of(lottoTicketIntArray));
-        }
         return manualLottoTickets;
-    }
-
-    private List<LottoTicket> getAutoLottoTickets(int autoLottoTicketCount) {
-        List<LottoTicket> autoLottoTickets = new ArrayList<>();
-        for (int i = 0; i < autoLottoTicketCount; i++) {
-            autoLottoTickets.add(LottoTicket.createRandomNumber());
-        }
-        return autoLottoTickets;
     }
 
     public WinStatistics proceedStatistics(InputView inputView, LottoTickets lottoTickets) {
@@ -65,6 +48,6 @@ public class LottoController {
                 .mapToInt(s -> Integer.parseInt(s.trim()))
                 .toArray());
         WinnerNumbers winnerNumbers = WinnerNumbers.from(lottoTicket, LottoBall.select(inputView.inputBonusNumber()));
-        return WinStatistics.from(winnerNumbers, lottoTickets);
+        return lottoTickets.calculateStatistics(winnerNumbers);
     }
 }
