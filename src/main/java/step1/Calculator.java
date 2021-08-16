a516b2be85f0bf8bea21a9eb5ad050c1967a99ad
bj-lee90 <littleprince0717@gmail.com>
2021-08-17 08:49:36 +0900

    1단계 - 문자열 덧셈 계산기 (#1957)
    * docs(README.md): 기능 목록 추가
    
    * feat: 게산기 입력 예외 처리
    
    빈 문자열 또는 null 값을 입력할 경우 0을 반환
    
    * feat: 계산기 숫자 1개에 대한 split 및 sum 기능 구현
    
    숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.
    
    * refactor: expression split 기능 Calculator 객체에 위임
    
    * refactor: Calculator 객체 관리하는 일급컬렉션 생성
    
    * refactor: Calculator 컴마, 콜론 계산 기능 추가
    
    - Calculator 객체 구조 변경
    - Operator enum class 추가
    
    * refactor: Calculator 커스텀 구분자 계산 기능 추가
    
    * refactor: Calculator 숫자 이외의 값 또는 음수 입력 시 예외 발생 기능 추가
    
    Co-authored-by: BJ_LEE <bj.lee@navercorp.com>

---
+++ src/main/java/step1/Calculator.java
@@ -0,0 +1,44 @@
+package step1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+  final static char BASIC_SPLITTER_COMMA = ',';
+  final static char BASIC_SPLITTER_COLON = ':';
+  final static char OR_OPERATOR_SYMBOL = '|';
+  private static final String NUMBER_REGEXP = "^[0-9]+$";
+  private static final String CUSTOM_SPLITTER = "//(.)\n(.*)";
+  private static final Pattern CUSTOM_SPLITTER_PATTERN = Pattern.compile(CUSTOM_SPLITTER);
+
+  int calculate(String expr, Operator operator) {
+    int result = 0;
+
+    String[] splittedExpr = split(expr);
+
+    for (String number : splittedExpr) {
+      checkNumberRange(number);
+      result = operator.calculate(result, Integer.parseInt(number));
+    }
+
+    return result;
+  }
+
+  private String[] split(String expr) {
+
+    Matcher m = CUSTOM_SPLITTER_PATTERN.matcher(expr);
+    if (m.find()) {
+      String customDelimiter = m.group(1);
+      return m.group(2).split(customDelimiter);
+    }
+
+    return expr.split("" + BASIC_SPLITTER_COMMA + OR_OPERATOR_SYMBOL + BASIC_SPLITTER_COLON);
+  }
+
+  private void checkNumberRange(String number) {
+    if (!number.matches(NUMBER_REGEXP)) {
+      throw new RuntimeException("숫자 이외의 값 또는 음수가 있습니다.");
+    }
+  }
+}
