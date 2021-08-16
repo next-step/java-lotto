f95d672e5b4de4e6156068f0726ae07d7e1d0937
beadoer1 <beadoer1@gmail.com>
2021-08-16 19:36:35 +0900

    feat(TicketRepository) : TicketRepository test success 메서드(save, findAll) 구현 완료

--- src/main/java/edu/nextstep/lottoauto/repository/TicketRepository.java
+++ src/main/java/edu/nextstep/lottoauto/repository/TicketRepository.java
@@ -2,14 +2,18 @@ package edu.nextstep.lottoauto.repository;
 
 import edu.nextstep.lottoauto.domain.Ticket;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TicketRepository {
 
+    private final List<Ticket> tickets = new ArrayList<>();
+
     public void save(Ticket ticket) {
+        tickets.add(ticket);
     }
 
     public List<Ticket> findAll() {
-        return null;
+        return tickets;
     }
 }
