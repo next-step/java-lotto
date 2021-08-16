8de65fa5a37cd25cb9b4c3cfb7741cd204e41e4b
beadoer1 <beadoer1@gmail.com>
2021-08-16 19:53:19 +0900

    feat(AutoTicketMaker) : AutoTicketMaker test success 메서드(create) 구현 완료

--- src/main/java/edu/nextstep/lottoauto/ticketmaker/AutoTicketMaker.java
+++ src/main/java/edu/nextstep/lottoauto/ticketmaker/AutoTicketMaker.java
@@ -2,11 +2,33 @@ package edu.nextstep.lottoauto.ticketmaker;
 
 import edu.nextstep.lottoauto.domain.Ticket;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AutoTicketMaker implements TicketMaker {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final List<Integer> NUMBERS = new ArrayList<>();
+    private static final int INDEX_FROM_TICKET_NUMBERS = 0;
+    private static final int INDEX_TO_TICKET_NUMBERS = 6;
+
+    static {
+        for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
+            NUMBERS.add(number);
+        }
+    }
 
     @Override
     public Ticket create() {
-        return null;
+        shuffleNumbers();
+        List<Integer> ticketNumbers =
+                new ArrayList<>(NUMBERS.subList(INDEX_FROM_TICKET_NUMBERS, INDEX_TO_TICKET_NUMBERS));
+        return Ticket.of(ticketNumbers);
     }
 
+    protected void shuffleNumbers() {
+        Collections.shuffle(NUMBERS);
+    }
 }
