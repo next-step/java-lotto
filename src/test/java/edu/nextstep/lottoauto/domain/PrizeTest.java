5a4d2d57a2844cb8dc2109fb6a51a521e8322f17
beadoer1 <beadoer1@gmail.com>
2021-08-16 23:51:30 +0900

    feat(PrizeTest) : Test 설명 및 testcase 추가

--- src/test/java/edu/nextstep/lottoauto/domain/PrizeTest.java
+++ src/test/java/edu/nextstep/lottoauto/domain/PrizeTest.java
@@ -1,18 +1,18 @@
 package edu.nextstep.lottoauto.domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PrizeTest {
-    @Test
-    void of() {
-        // given
-        int countOfMatching = 6;
 
+    @ParameterizedTest(name = "Prize enum 정적 팩토리 생성자 Test [{index}] {0}, {1}")
+    @CsvSource({"6, FIRST", "5, SECOND", "4, THIRD", "3, FOURTH", "0, NULL"})
+    void of(int countOfMatching, Prize checkPrize) {
         // when
         Prize prize = Prize.of(countOfMatching);
 
         // then
-        Assertions.assertThat(prize).isEqualTo(Prize.FIRST);
+        Assertions.assertThat(prize).isEqualTo(checkPrize);
     }
 }
