faba5b0cef4ee83825a4ca10850293e4f12c28ea
gunny6026 <gunny6026@naver.com>
2021-08-18 08:36:55 +0900

    refactor : 상수 선언

--- src/main/java/lotto/step3/domain/LottoCreator.java
+++ src/main/java/lotto/step3/domain/LottoCreator.java
@@ -8,20 +8,20 @@ public class LottoCreator {
     private static final int LOTTO_START_COUNT = 1;
     private static final int LOTTO_END_COUNT = 6;
     private static final String DELIMITER = ",";
-    private static List<LottoNumber> lottoNumbers = new ArrayList<>();
+    private static final List<LottoNumber> LOTTO_CREATOR = new ArrayList<>();
 
     static {
         for (int i = LOTTO_MIN_NUMBER; i <= LOTTO_MAX_NUMBER; i++) {
-            lottoNumbers.add(new LottoNumber(i));
+            LOTTO_CREATOR.add(new LottoNumber(i));
         }
     }
 
     public static Set<LottoNumber> createLotto() {
-        Collections.shuffle(lottoNumbers);
+        Collections.shuffle(LOTTO_CREATOR);
         Set<LottoNumber> lotto = new HashSet<>();
 
         for (int i = LOTTO_START_COUNT; i <= LOTTO_END_COUNT; i++) {
-            lotto.add(lottoNumbers.get(i));
+            lotto.add(LOTTO_CREATOR.get(i));
         }
         return lotto;
     }
