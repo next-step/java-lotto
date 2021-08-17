1f83b302a557bdcac18044dc93a837f113e801e3
yook0328 <yook0328@gmail.com>
2021-08-18 00:27:10 +0900

    feat: Lotto 관련 일급 컬렉션 제작 및 로또 생성 구현.

---
+++ src/main/java/com/lotto/model/Lotto.java
@@ -0,0 +1,11 @@
+package com.lotto.model;
+
+import java.util.Set;
+
+public class Lotto {
+    private final Set<Integer> numbers;
+
+    public Lotto(Set<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
