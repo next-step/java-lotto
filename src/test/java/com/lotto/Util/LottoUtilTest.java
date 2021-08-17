86a220dc5553a51f1e26e4c3b790aef50c7e0e36
yook0328 <yook0328@gmail.com>
2021-08-18 00:02:23 +0900

    test: 로또 번호 생성 테스트.

---
+++ src/test/java/com/lotto/Util/LottoUtilTest.java
@@ -0,0 +1,22 @@
+package com.lotto.Util;
+
+import com.lotto.util.LottoUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoUtilTest {
+    @DisplayName("Lotto Number 추출 테스트")
+    @Test
+    public void extractLottoNumberTest() {
+        //when
+        Set<Integer> numbers = LottoUtil.extractRandomLottoNumbers();
+
+        //then
+        assertThat(numbers.size()).isEqualTo(6);
+    }
+
+}
