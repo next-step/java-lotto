6ec34ca76872f0f6e59f0470a76e9da2645304ac
gashe-soo <gashe7@gmail.com>
2021-08-18 19:52:08 +0900

    refactor:상수로 분리

--- src/main/java/lotto/model/LottoTicketOffice.java
+++ src/main/java/lotto/model/LottoTicketOffice.java
@@ -3,18 +3,21 @@ package lotto.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class LottoTicketOffice {
 
     public static final int PRICE = 1000;
+    public static final int LOTTO_NUMBER_COUNT = 6;
     private static final int MAX = 45;
     private static final int MIN = 1;
     private final List<Integer> lottoNumber;
 
     public LottoTicketOffice() {
-        lottoNumber = IntStream.range(MIN, MAX + 1).boxed().collect(Collectors.toList());
+        lottoNumber = IntStream.range(MIN, MAX + 1).boxed().collect(toList());
     }
 
     public List<LottoTicket> buyLotto(int amount) {
@@ -27,6 +30,9 @@ public class LottoTicketOffice {
 
     private LottoTicket publishLottoTicket() {
         Collections.shuffle(lottoNumber);
-        return new LottoTicket(lottoNumber.stream().limit(6).sorted().collect(Collectors.toList()));
+        return lottoNumber.stream()
+                .limit(LOTTO_NUMBER_COUNT)
+                .sorted()
+                .collect(collectingAndThen(toList(), LottoTicket::new));
     }
 }
