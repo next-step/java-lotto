64e9d4266342835961e78fe2df5508af15e96f94
jaehong0721 <newbie302@gmail.com>
2021-08-18 17:23:59 +0900

    refactor: 숫자 문자열을 Numbers 클래스로 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,46 +1,16 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class StringCalculator {
-    public static String[] splitString(String numbers) {
-        if (!isValidNumber(numbers)) return new String[]{"0"};
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbers);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
-        }
-        return numbers.split(",|:");
-    }
-
-    public static int parseInt(String number) {
-        if (!isValidNumber(number)) return 0;
-
-        int parsedInt = Integer.parseInt(number);
-
-        if (parsedInt < 0) {
-            throw new RuntimeException("음수는 포함될 수 없습니다.");
-        }
-        return Integer.parseInt(number);
-    }
-
-    public static int sum(String numbers) {
-        String[] splitted = splitString(numbers);
-        List<Integer> parsed = Arrays.stream(splitted).map(StringCalculator::parseInt).collect(Collectors.toList());
+    public static int sum(String stringNumbers) {
+        List<Integer> numbers = Numbers.toListOfInt(stringNumbers);
         int result = 0;
 
-        for (int i : parsed) {
+        for (int i : numbers) {
             result += i;
         }
         return result;
-    }
-
-    private static boolean isValidNumber(String number) {
-        return number != null && !number.isEmpty();
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -4,61 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringCalculatorTest {
-
-    @Test
-    @DisplayName("쉼표와 콜론을 구분자로 문자열을 분리하는지 테스트")
-    void splitString() {
-        String[] result = StringCalculator.splitString("1,2");
-        assertThat(result).isEqualTo(new String[]{"1", "2"});
-
-        result = StringCalculator.splitString("2:3");
-        assertThat(result).isEqualTo(new String[]{"2", "3"});
-
-        result = StringCalculator.splitString(null);
-        assertThat(result).isEqualTo(new String[]{"0"});
-
-        result = StringCalculator.splitString("");
-        assertThat(result).isEqualTo(new String[]{"0"});
-    }
-
-    @Test
-    @DisplayName("커스텀 구분자로 문자열을 분리하는지 테스트")
-    void splitStringByCustom() {
-        String[] result = StringCalculator.splitString("//%\n1%3%5");
-        assertThat(result).isEqualTo(new String[]{"1", "3", "5"});
-    }
-
-    @Test
-    @DisplayName("숫자 문자를 Int값으로 바꾸는지 테스트")
-    void parseInt() {
-        int result = StringCalculator.parseInt("1");
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("빈 문자열이나 null로 전달될 경우 0으로 반환하는지 테스트")
-    void parseIntToZero() {
-        int result = StringCalculator.parseInt("");
-        assertThat(result).isEqualTo(0);
-
-        result = StringCalculator.parseInt(null);
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("숫자가 아닌 값이나 음수가 전달될 경우 RuntimeException이 나는지 테스트")
-    void parseIntNotPositiveNumber() {
-        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
-            StringCalculator.parseInt("-1");
-        });
-
-        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
-            StringCalculator.parseInt("a");
-        });
-    }
 
     @Test
     @DisplayName("덧셈이 잘 되는지 테스트")
