61fbc1a1f05338d5d6b51b92e239fbe0e9258b29
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-18 12:45:35 +0900

    test: 계산기 클래스의 커스텀 구분자 테스트 추가

--- src/test/java/StringCalculator/CalculatorTest.java
+++ src/test/java/StringCalculator/CalculatorTest.java
@@ -7,10 +7,19 @@ import static org.assertj.core.api.Assertions.*;
 public class CalculatorTest {
 
     @Test
-    @DisplayName("계산기 테스트")
-    void calculatorReturnsRightResult() {
+    @DisplayName("일반 구분자 계산기 테스트")
+    void inputHavingNormalDelimiterTest() {
         Calculator cal = new Calculator();
         WholeNumber actual = cal.calculate("1,2:3");
+        WholeNumber expected = new WholeNumber(6);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 계산기 테스트")
+    void inputHavingCustomizedDelimiterTest() {
+        Calculator cal = new Calculator();
+        WholeNumber actual = cal.calculate("//;\n1;2;3");
         WholeNumber expected = new WholeNumber(6);
         assertThat(actual).isEqualTo(expected);
     }
