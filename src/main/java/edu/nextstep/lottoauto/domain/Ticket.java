c5e0786206c2d112a8bc2834750d0ae4ce56afc3
beadoer1 <beadoer1@gmail.com>
2021-08-19 01:06:49 +0900

    refactor(AutoNumbersMaker, Ticket) : 상수 중복 사용 방지를 위한 Ticket 상수 사용

--- src/main/java/edu/nextstep/lottoauto/domain/Ticket.java
+++ src/main/java/edu/nextstep/lottoauto/domain/Ticket.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class Ticket {
 
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final int LENGTH_OF_NUMBERS = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LENGTH_OF_NUMBERS = 6;
 
     private final List<Integer> numbers;
 
--- src/main/java/edu/nextstep/lottoauto/ticketmaker/AutoNumbersMaker.java
+++ src/main/java/edu/nextstep/lottoauto/ticketmaker/AutoNumbersMaker.java
@@ -1,13 +1,15 @@
 package edu.nextstep.lottoauto.ticketmaker;
 
+import edu.nextstep.lottoauto.domain.Ticket;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class AutoNumbersMaker implements NumbersMaker {
 
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = Ticket.MIN_NUMBER;
+    private static final int MAX_NUMBER = Ticket.MAX_NUMBER;
     private static final List<Integer> NUMBERS = new ArrayList<>();
     private static final int INDEX_FROM_TICKET_NUMBERS = 0;
     private static final int INDEX_TO_TICKET_NUMBERS = 6;
