05a2249605ff524fc492b34c84ee982268a63395
harris <harrisleesh@gmail.com>
2021-08-18 14:14:30 +0900

    feat : Add request manual ticket number in InputView

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,8 @@
 package lotto.view;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
@@ -13,9 +12,28 @@ public class InputView {
         return Integer.parseInt(scanner.nextLine());
     }
 
+    public static int requestManualTicket() {
+        System.out.println();
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Set<Integer>> requestManualNumbers(int manualTicket) {
+        System.out.println();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<Set<Integer>> manualLottoSet = new ArrayList<>();
+        IntStream.range(0, manualTicket)
+                .forEach(i -> manualLottoSet.add(getLottoNumberSet()));
+        return manualLottoSet;
+    }
+
     public static Set<Integer> requestWinnerNumbers() {
         System.out.println();
         System.out.println("지난 주 당첨 번호를 입력해주세요.");
+        return getLottoNumberSet();
+    }
+
+    private static Set<Integer> getLottoNumberSet() {
         return Arrays.stream(scanner.nextLine().split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
