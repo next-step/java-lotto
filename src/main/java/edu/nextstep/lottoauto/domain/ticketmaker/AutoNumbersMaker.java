d08597c1b108fab31d46ce63cef1273a6e56a06c
beadoer1 <beadoer1@gmail.com>
2021-08-20 01:28:42 +0900

    refactor(AutoNumbersMaker) : 별도 클래스 변수 선언을 직접 사용으로 변경

--- src/main/java/edu/nextstep/lottoauto/domain/ticketmaker/AutoNumbersMaker.java
+++ src/main/java/edu/nextstep/lottoauto/domain/ticketmaker/AutoNumbersMaker.java
@@ -8,15 +8,13 @@ import java.util.List;
 
 public class AutoNumbersMaker implements NumbersMaker {
 
-    private static final int MIN_NUMBER = Ticket.MIN_NUMBER;
-    private static final int MAX_NUMBER = Ticket.MAX_NUMBER;
     private static final int INDEX_FROM_TICKET_NUMBERS = 0;
     private static final int INDEX_TO_TICKET_NUMBERS = 6;
 
     private final List<Integer> numbers = new ArrayList<>();
 
     public AutoNumbersMaker() {
-        for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
+        for (int number = Ticket.MIN_NUMBER; number <= Ticket.MAX_NUMBER; number++) {
             numbers.add(number);
         }
     }
