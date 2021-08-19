a8b804094b5f122948a592ddadd17d1c1f98a57c
khcho902 <khcho902@gmail.com>
2021-08-19 14:14:58 +0900

    refactor: split메소드 구현(split, sum 로직 분리)

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -5,19 +5,27 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+  private static final String DEFAULT_SPLIT_DELIMITER_REGEX = ",|:";
+  private static final String CUSTOM_SPLIT_REGEX = "//(.)\n(.*)";
+  private static final int CUSTOM_SPLIT_TARGET_INDEX = 2;
+  private static final int CUSTOM_SPLIT_DELIMITER_INDEX = 1;
+
   public static int splitAndSum(String input) {
     if (input == null || input.equals("")) {
       return 0;
     }
-    Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-    if (m.find()) {
-      return getSumResult(m.group(2), m.group(1));
-    }
-    return getSumResult(input, ",:");
+    return getSumResult(split(input));
   }
 
-  private static int getSumResult(String input, String delemiter) {
-    String[] tokens = input.split("[" + delemiter + "]");
+  private static String[] split(String input) {
+    Matcher m = Pattern.compile(CUSTOM_SPLIT_REGEX).matcher(input);
+    if (m.find()) {
+      return m.group(CUSTOM_SPLIT_TARGET_INDEX).split(m.group(CUSTOM_SPLIT_DELIMITER_INDEX));
+    }
+    return input.split(DEFAULT_SPLIT_DELIMITER_REGEX);
+  }
+
+  private static int getSumResult(String[] tokens) {
     int result = 0;
     for (String token : tokens) {
       int num = Integer.parseInt(token);
