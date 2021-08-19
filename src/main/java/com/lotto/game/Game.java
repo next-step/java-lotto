375eb6e6a0b7694339bc2b7fe8a5c18edfcd876e
yook0328 <yook0328@gmail.com>
2021-08-19 23:42:43 +0900

    feat: Lotto 구매, 당첨번호 생성, 당첨 확인, 수익률 계산 하는 Game 객체 생성.

---
+++ src/main/java/com/lotto/game/Game.java
@@ -0,0 +1,46 @@
+package com.lotto.game;
+
+import com.lotto.model.LottoResult;
+import com.lotto.model.Lottos;
+import com.lotto.model.WinningNumbers;
+import com.lotto.type.RewardType;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Game {
+    private static final int LOTTO_PRICE = 1000;
+
+    //구입하다.
+    public static Lottos purchase(int money) {
+        return new Lottos(money / LOTTO_PRICE);
+    }
+
+    //당첨번호 입력받다.
+    public static WinningNumbers makeWinningNumbers(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>();
+
+        numbers.stream().forEach((number) -> {
+            if (numberSet.contains(number)) {
+                throw new IllegalArgumentException("중복된 숫자가 존재합니다. 숫자들 목록 " + numbers);
+            }
+
+            numberSet.add(number);
+        });
+
+        return new WinningNumbers(numberSet);
+    }
+
+    public static LottoResult check(WinningNumbers winningNumbers, Lottos lottos) {
+        return new LottoResult(winningNumbers, lottos);
+    }
+
+    public static float calculateBenefit(int money, LottoResult result) {
+        int allRewardValue = result.streamCorrectCountMap()
+                .mapToInt((entry) -> RewardType.getRewardValue(entry.getKey()) * entry.getValue())
+                .sum();
+
+        return allRewardValue / (money * 1.f);
+    }
+}
