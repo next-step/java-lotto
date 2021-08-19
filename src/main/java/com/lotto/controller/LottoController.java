747160d7343d8ce23dcae9f40093c9f531114b3a
yook0328 <yook0328@gmail.com>
2021-08-19 23:43:17 +0900

    feat: Lotto game 메인 메서드 컨트롤러 제작.

---
+++ src/main/java/com/lotto/controller/LottoController.java
@@ -0,0 +1,31 @@
+package com.lotto.controller;
+
+import com.lotto.game.Game;
+import com.lotto.model.LottoResult;
+import com.lotto.model.Lottos;
+import com.lotto.model.WinningNumbers;
+import com.lotto.util.InputParser;
+import com.lotto.view.LottoDrawer;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class LottoController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        LottoDrawer.drawMoneyInputMessage();
+        int money = InputParser.convert(scanner.nextLine());
+
+        Lottos lottos = Game.purchase(money);
+        LottoDrawer.drawBuyResult(lottos);
+
+        LottoDrawer.drawWinningNumbers();
+        List<Integer> numbers = InputParser.parseNumbers(scanner.nextLine());
+        WinningNumbers winningNumbers = Game.makeWinningNumbers(numbers);
+
+        LottoResult result = Game.check(winningNumbers, lottos);
+        float benefit = Game.calculateBenefit(money, result);
+        LottoDrawer.drawResult(result, benefit);
+    }
+}
