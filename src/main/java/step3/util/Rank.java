54ea99c093cde4130aec6ca8bdbcfbfcbebefec3
jaeyoungjang2 <lion85488@gmail.com>
2021-08-19 19:36:37 +0900

    refactor: 변수 사이 간격 수정

--- src/main/java/step3/util/Rank.java
+++ src/main/java/step3/util/Rank.java
@@ -3,7 +3,7 @@ package step3.util;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6,2_000_000_000),
+    FIRST(6, 2_000_000_000),
     SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
