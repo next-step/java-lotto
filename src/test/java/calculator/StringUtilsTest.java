9cbe5ed8357a8e44705d64f55fb66f611ac71ab6
khcho902 <khcho902@gmail.com>
2021-08-20 03:22:24 +0900

    test: 테스트 코드 중 DisplayName 빠진 것들 추가

--- src/test/java/calculator/StringUtilsTest.java
+++ src/test/java/calculator/StringUtilsTest.java
@@ -2,11 +2,13 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringUtilsTest {
 
   @Test
+  @DisplayName("null, 공백문자열 판단 테스트")
   void isBlankTest() {
     //given
     String nullStr = null;
