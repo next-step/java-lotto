eb1d7152bad77fdc433ca03ca95b49eaa5743b32
khcho902 <khcho902@gmail.com>
2021-08-19 13:42:39 +0900

    feat: StringAddCalculator클래스 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,8 +1,31 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
   public static int splitAndSum(String input) {
-    return 0;
+    if (input == null || input.equals("")) {
+      return 0;
+    }
+    Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+    if (m.find()) {
+      return getSumResult(m.group(2), m.group(1));
+    }
+    return getSumResult(input, ",:");
+  }
+
+  private static int getSumResult(String input, String delemiter) {
+    String[] tokens = input.split("[" + delemiter + "]");
+    int result = 0;
+    for (String token : tokens) {
+      int num = Integer.parseInt(token);
+      if (num < 0) {
+        throw new RuntimeException("음수 값은 사용될 수 없습니다.");
+      }
+      result += num;
+    }
+    return result;
   }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
+
   @Test
   public void splitAndSum_null_또는_빈문자() {
     int result = StringAddCalculator.splitAndSum(null);
