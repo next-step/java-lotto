1c805a6423ca73d49d6a6c9104f811e010c6363f
jaeyoungjang2 <lion85488@gmail.com>
2021-08-19 19:40:00 +0900

    refactor: 클래스 변수와 인스턴스 변수 사이의 공백 추가

--- src/main/java/step3/domain/ManualLotto.java
+++ src/main/java/step3/domain/ManualLotto.java
@@ -1,7 +1,8 @@
-package step3;
+package step3.domain;
 
 public class ManualLotto {
     private static final String MANUAL_LOTTO_NUM_ERROR = "수동으로 구매할 수 있는 로또의 수는 구입하는 로또의 수 보다 적어야합니다.";
+
     private final int numOfManualPurchaseLotto;
 
     public ManualLotto(int numOfManualPurchaseLotto, int totalNumOfLotto) {
