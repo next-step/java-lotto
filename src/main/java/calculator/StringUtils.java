2773ede3038da707233ba8f073dc100b6bfca9d2
khcho902 <khcho902@gmail.com>
2021-08-20 03:24:01 +0900

    refactor: StringUtils.isBlank메소드 코드 개선

--- src/main/java/calculator/StringUtils.java
+++ src/main/java/calculator/StringUtils.java
@@ -3,6 +3,6 @@ package calculator;
 public class StringUtils {
 
   public static boolean isBlank(String str) {
-    return str == null || str.equals("");
+    return str == null || str.trim().isEmpty();
   }
 }
