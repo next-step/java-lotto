6e4dbc669be310789481674e15d83ae92dfa26df
gunny6026 <gunny6026@naver.com>
2021-08-19 17:59:12 +0900

    test : 수동으로 구매할 로또 수에 대한 테스트 케이스

---
+++ src/test/java/lotto/step4/LottoCountTest.java
@@ -0,0 +1,30 @@
+package lotto.step4;
+
+import lotto.step4.domain.LottoCount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoCountTest {
+
+    @Test
+    @DisplayName("수동으로 구매할 로또 수에 음수가 올 수 없다.")
+    void create() {
+        assertThatThrownBy(() -> new LottoCount(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입 금액보다 많은 로또 수를 입력 했을 때 예외가 발생한다.")
+    void createAutoCount() {
+        LottoCount lottoCount = new LottoCount(5);
+        assertThatThrownBy(()->lottoCount.createAutoCount(3000)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입 굼액에서 수동 로또 수를 제외한 자동 로또 수가 몇개 인지 확인")
+    void createAutoCount2() {
+        LottoCount lottoCount = new LottoCount(5);
+        assertThat(lottoCount.createAutoCount(8000)).isEqualTo(3);
+    }
+}
