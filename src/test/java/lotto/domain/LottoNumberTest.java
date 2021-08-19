defca794594e9a8a22c14a7f6de7f0b7c13ba506
hiro <kiryuinh32@gmail.com>
2021-08-19 20:23:07 +0900

    test: 로또 번호 생성 테스트

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import lotto.exception.LottoNumberSizeException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoNumberTest {
+
+    @ParameterizedTest
+    @DisplayName("로또 번호 생성 예외 테스트")
+    @ValueSource(ints = {-1, 0, 46})
+    void createLottoNumber(int number) {
+        assertThrows(LottoNumberSizeException.class, () -> new LottoNumber(number));
+    }
+
+}
