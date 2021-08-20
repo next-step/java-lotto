66132cd71f9b9c04e329d316d1d6defac1b6ae57
gashe-soo <gashe7@gmail.com>
2021-08-20 20:42:18 +0900

    feat:등수별 당첨 개수와 상금 변경

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    FIFTH(3, 5_000),
+    SECOND(5, 3_000_000),
+    THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
-
-
+    FIFTH(3, 5_000),
     MISS(0, 0);
 
     private final int match;
