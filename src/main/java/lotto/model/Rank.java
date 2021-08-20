a54105ddbcbd3884aa6f998495dd6ed68aeb307e
gashe-soo <gashe7@gmail.com>
2021-08-20 22:45:13 +0900

    feat:보너스볼 포함한 당첨 확인 기능

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -1,21 +1,31 @@
 package lotto.model;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 3_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, 2_000_000_000, (count, isMatch) -> count == 6),
+    SECOND(5, 30_000_000, (count, isMatch) -> count == 5 && isMatch),
+    THIRD(5, 1_500_000, (count, isMatch) -> count == 5 && !isMatch),
+    FOURTH(4, 50_000, (count, isMatch) -> count == 4),
+    FIFTH(3, 5_000, (count, isMatch) -> count == 3),
+    MISS(0, 0, (count, isMatch) -> count < 3);
 
     private final int match;
     private final int winningBonus;
+    private final BiPredicate<Integer, Boolean> matchLogic;
 
-    Rank(int match, int winningBonus) {
+    Rank(int match, int winningBonus, BiPredicate<Integer, Boolean> matchLogic) {
         this.match = match;
         this.winningBonus = winningBonus;
+        this.matchLogic = matchLogic;
+    }
+
+    public static Rank of(int count, boolean isMatch) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchLogic.test(count, isMatch))
+                .findFirst()
+                .get();
     }
 
     public int getMatch() {
@@ -26,12 +36,7 @@ public enum Rank {
         return winningBonus;
     }
 
-    public static Rank of(int count) {
-        if (count < FIFTH.match) {
-            return MISS;
-        }
-        return Arrays.stream(values())
-                .filter(rank -> rank.match == count)
-                .findFirst().get();
+    public BiPredicate<Integer, Boolean> getMatchLogic() {
+        return matchLogic;
     }
 }
