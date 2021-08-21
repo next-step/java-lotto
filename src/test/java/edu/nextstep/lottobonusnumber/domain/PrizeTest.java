254fc9a53dd202824ac56f859727666831837077
beadoer1 <beadoer1@gmail.com>
2021-08-21 16:54:31 +0900

    feat(PrizeTest) : Prize 에 2등(5개 + 보너스 번호 당첨) test fail 추가

--- src/test/java/edu/nextstep/lottobonusnumber/domain/PrizeTest.java
+++ src/test/java/edu/nextstep/lottobonusnumber/domain/PrizeTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class PrizeTest {
 
-    @ParameterizedTest(name = "Prize enum 정적 팩토리 생성자 Test [{index}] {0}, {1}")
-    @CsvSource({"6, FIRST", "5, SECOND", "4, THIRD", "3, FOURTH", "0, LOSE"})
-    void of(int countOfMatching, Prize checkPrize) {
+    @ParameterizedTest(name = "Prize enum 정적 팩토리 생성자 Test [{index}] {0}, {1}, {2}")
+    @CsvSource({"6, false, FIRST", "5, true, SECOND", "5, false, THIRD", "4, false, FOURTH", "3, false, FIFTH", "0, false, LOSE"})
+    void of(int countOfMatching, boolean hasBonusNumber, Prize checkPrize) {
         // when
-        Prize prize = Prize.of(countOfMatching);
+        Prize prize = Prize.of(countOfMatching, hasBonusNumber);
 
         // then
         Assertions.assertThat(prize).isEqualTo(checkPrize);
