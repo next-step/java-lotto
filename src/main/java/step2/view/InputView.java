197d00ee16aabbbb98c8879a0acc4eb96ff3c224
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-22 04:00:33 +0900

    feat : InputView, ResultView 추가 구현

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -6,9 +6,9 @@ public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static String inputPurchasePrice() {
+    public static int inputPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.next();
+        return scanner.nextInt();
     }
 
     public static String inputWinningNumbersLastWeek() {
--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -4,18 +4,22 @@ import step2.domain.LottoTickets;
 import step2.domain.Ticket;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
-    public static void announceTotalLottoTicket(int count) {
+    public static void announceTotalLottoTicketCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
     }
 
     public static void announceTotalLottoNumbers(LottoTickets lottoTickets) {
         List<Ticket> tickets = lottoTickets.getLottoTickets();
 
-        tickets.forEach(ticket ->
-            System.out.println(ticket.toString())
-        );
+        for (Ticket ticket : tickets) {
+            System.out.println(ticket.getLottoNumbers().stream()
+                    .collect(Collectors.toList())
+            );
+        }
+
     }
 }
