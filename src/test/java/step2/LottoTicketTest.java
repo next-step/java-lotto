b2ee1a91943c01db472bc3ec28dae5776c85ebbd
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-21 20:53:13 +0900

    test : 로또 번호를 저장하는 테스트 코드 작성

---
+++ src/test/java/step2/LottoTicketTest.java
@@ -0,0 +1,26 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketTest {
+
+    @Test
+    @DisplayName("로또 번호를 저장한다.")
+    void lottoTicket_save() {
+        LottoTicket lottoTicket = new LottoTicket();
+
+        // 로또 번호 생성
+        List<Integer> lottoNumbers = Arrays.asList(1,2,3,4,5,6);
+
+        lottoTicket.save(lottoNumbers);
+        List<Integer> getLottoNumbers = lottoTicket.getLottoNumbers();
+
+        assertThat(getLottoNumbers).isEqualTo(lottoNumbers);
+    }
+}
