84f4524eefa51b48975ff4393ca8711f2a33aa4a
beadoer1 <beadoer1@gmail.com>
2021-08-21 17:42:22 +0900

    feat(Prize) : 2등(5개 + 보너스 번호) enum 추가 및 정적 팩토리 수정(test 통과)

--- src/main/java/edu/nextstep/lottobonusnumber/domain/Prize.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/Prize.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum Prize {
     FIRST(6, 200_000_000),
-    SECOND(5, 15_000_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 15_000_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     LOSE(0, 0);
-
 
     private final int countOfMatch;
     private final int winningPrize;
@@ -18,9 +18,13 @@ public enum Prize {
         this.winningPrize = winningPrize;
     }
 
-    public static Prize of(int numOfMatch) {
+    public static Prize of(int numOfMatch, boolean hasBonusNumber) {
+        if(numOfMatch == 5 && !hasBonusNumber) {
+            return Prize.THIRD;
+        }
+
         return Arrays.stream(Prize.values())
-                .filter((prize) -> (prize.getCountOfMatch() == numOfMatch))
+                .filter(prize -> prize.countOfMatch == numOfMatch)
                 .findFirst()
                 .orElse(LOSE);
     }
@@ -32,6 +36,4 @@ public enum Prize {
     public int getWinningPrize() {
         return winningPrize;
     }
-
-
 }
