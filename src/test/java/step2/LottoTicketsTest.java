da4b6cd6c97f9412c7cf5cf3de5a0428d4da156e
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-21 21:04:06 +0900

    test : 구매한 로또 티켓들을 저장하는 테스트 코드 작성

---
+++ src/test/java/step2/LottoTicketsTest.java
@@ -0,0 +1,37 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTicketsTest {
+
+    @Test
+    @DisplayName("구매한 로또 티켓 전부를 저장한다.")
+    void lottoTickets_save_test() {
+        LottoTickets lottoTickets = new LottoTickets();
+
+        // 로또 번호 생성
+        LottoTicket lottoNumbers1 = new LottoTicket();
+        lottoNumbers1.saveLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        LottoTicket lottoNumbers2 = new LottoTicket();
+        lottoNumbers1.saveLottoNumbers(Arrays.asList(11, 12, 13, 14, 15, 16));
+
+        LottoTicket lottoNumbers3 = new LottoTicket();
+        lottoNumbers1.saveLottoNumbers(Arrays.asList(21, 22, 23, 24, 25, 26));
+
+        lottoTickets.addLottoTicket(lottoNumbers1);
+        lottoTickets.addLottoTicket(lottoNumbers2);
+        lottoTickets.addLottoTicket(lottoNumbers3);
+
+        assertThat(lottoTickets.getLottoTickets().size()).isEqualTo(3);
+        assertThat(lottoTickets.getLottoTickets().get(0)).isEqualTo(lottoNumbers1);
+        assertThat(lottoTickets.getLottoTickets().get(1)).isEqualTo(lottoNumbers2);
+        assertThat(lottoTickets.getLottoTickets().get(2)).isEqualTo(lottoNumbers3);
+    }
+}
