2947a886aa314cc02428ed4acec21091c46557c1
beadoer1 <beadoer1@gmail.com>
2021-08-21 16:55:17 +0900

    feat(WinningCheckMachineTest) : WinningTicket 을 매개로 한 test 구현, 2등 당첨 확인 test 추가

--- src/test/java/edu/nextstep/lottobonusnumber/domain/WinningCheckMachineTest.java
+++ src/test/java/edu/nextstep/lottobonusnumber/domain/WinningCheckMachineTest.java
@@ -16,18 +16,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WinningCheckMachineTest {
 
     @Test
-    @DisplayName("당첨 결과 확인")
+    @DisplayName("당첨 결과 확인 ")
     void checkWinningResult() {
         // given
         List<Ticket> tickets = TicketMachine.createTickets(1000, () -> createNumbersFromTo(1,6));
+        String winningNumbersString = "4,5,6,7,8,9";
+        int bonusNumber = 10;
+        WinningTicket winningTicket = WinningTicket.of(winningNumbersString, bonusNumber);
 
         // when
         WinningResultForm winningResult =
-                WinningCheckMachine.confirmWinningResult(tickets,"4,5,6,7,8,9");
+                WinningCheckMachine.confirmWinningResult(tickets,winningTicket);
 
         // then
-        assertThat(winningResult.getWinningResult().getOrDefault(Prize.FOURTH,0)).isEqualTo(1);
+        assertThat(winningResult.getWinningResult().getOrDefault(Prize.FIFTH,0)).isEqualTo(1);
         assertThat(winningResult.getRateOfReturn()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("2등 당첨 결과 확인 : 보너스 번호")
+    void checkWinningResult_SECOND() {
+        // given
+        List<Ticket> tickets = TicketMachine.createTickets(1000, () -> createNumbersFromTo(5,10));
+        String winningNumbersString = "4,5,6,7,8,9";
+        int bonusNumber = 10;
+        WinningTicket winningTicket = WinningTicket.of(winningNumbersString, bonusNumber);
+
+        // when
+        WinningResultForm winningResult =
+                WinningCheckMachine.confirmWinningResult(tickets,winningTicket);
+
+        // then
+        assertThat(winningResult.getWinningResult().getOrDefault(Prize.SECOND,0)).isEqualTo(1);
     }
 
     private List<Integer> createNumbersFromTo(int numFrom, int numTo) {
