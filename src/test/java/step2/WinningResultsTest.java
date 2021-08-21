4aa8827aa09f69b4bad09a617144bb2a54879d3c
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-22 02:07:49 +0900

    refactor : 테스트 코드 수정

--- src/test/java/step2/WinningResultsTest.java
+++ src/test/java/step2/WinningResultsTest.java
@@ -30,31 +30,21 @@ public class WinningResultsTest {
     @DisplayName("구매한 티켓의 당첨 결과를 확인한다.")
     void check_winning_results_test() {
 
-
-        // 로또 번호 생성
         Ticket lottoNumbers1 = new Ticket();
         lottoNumbers1.saveLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
 
-        Ticket lottoNumbers2 = new Ticket();
-        lottoNumbers2.saveLottoNumbers(Arrays.asList(11, 12, 13, 14, 15, 16));
-
-        Ticket lottoNumbers3 = new Ticket();
-        lottoNumbers3.saveLottoNumbers(Arrays.asList(21, 22, 23, 24, 25, 26));
-
         LottoTickets lottoTickets = new LottoTickets();
         lottoTickets.addLottoTicket(lottoNumbers1);
-        lottoTickets.addLottoTicket(lottoNumbers2);
-        lottoTickets.addLottoTicket(lottoNumbers3);
 
         WinningResults winningResults = new WinningResults();
         Ticket winningNumbers = new Ticket();
-        winningNumbers.saveLottoNumbers(Arrays.asList(1, 2, 3, 4, 5));
+        winningNumbers.saveLottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
         winningResults.saveWinningLottoNumber(winningNumbers);
 
         winningResults.checkWinning(lottoTickets);
 
         Map<Award, Integer> winningResult = winningResults.getWinningResult();
-        System.out.println(winningResults.getWinningLottoNumber().getLottoNumbers());
-        System.out.println(winningResult);
+
+        assertThat(winningResult.get(Award.FIRST)).isEqualTo(1);
     }
 }
