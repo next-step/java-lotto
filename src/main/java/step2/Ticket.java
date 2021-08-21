cd2d5590cec1cd042819ea206ae00c0d1f345fb2
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-22 02:36:51 +0900

    feat : 번호가 범위 체크 추가

--- src/main/java/step2/Ticket.java
+++ src/main/java/step2/Ticket.java
@@ -3,7 +3,9 @@ package step2;
 import java.util.List;
 
 public class Ticket {
-
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+    private static final int TOTAL_NUMBER_COUNT = 6;
     private List<Integer> lottoNumbers;
 
     public List<Integer> getLottoNumbers() {
@@ -16,8 +18,15 @@ public class Ticket {
     }
 
     private void validateLottoNumbers(List<Integer> lottoNumbers) {
-        if(lottoNumbers.size() != 6) {
+        if (lottoNumbers.size() != TOTAL_NUMBER_COUNT) {
             throw new IllegalArgumentException("입력된 숫자의 갯수가 6개가 아닙니다.");
         }
+
+        lottoNumbers.forEach(number -> {
+            if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
+                throw new IllegalArgumentException("입력된 숫자의 범위를 벗어났습니다.");
+            }
+        });
     }
+
 }
