ac4fa1dabd0b67e7eb484fb38d06128915a7490f
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-21 22:12:08 +0900

    test : 당첨번호 저장 테스트 코드 작성

---
+++ src/test/java/step2/WinningResultsTest.java
@@ -0,0 +1,25 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinningResultsTest {
+
+    @Test
+    @DisplayName("당첨 번호를 저장한다.")
+    void winning_numbers_save_test() {
+        WinningResults winningResults = new WinningResults();
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        winningResults.saveWinningLottoNumber(winningNumbers);
+
+        List<Integer> getWinningNumbers = winningResults.getWinningLottoNumber();
+
+        assertThat(getWinningNumbers).isEqualTo(winningNumbers);
+    }
+}
