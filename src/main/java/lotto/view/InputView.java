b18e03bbedca452bdcfe27438353b2802252c685
hiro <kiryuinh32@gmail.com>
2021-08-21 23:15:25 +0900

    view: 추가 메세지 작성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,29 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static lotto.util.StringUtil.split;
 
 public class InputView {
     private static final String FIRST_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String ASK_BONUS_BALL_NUMBER = "보너스 볼을 입력해 주세요.";
+    private static final String ASK_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String ASK_MANUAL_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public int askPurchaseAmount() {
+    public static int askPurchasePrice() {
         System.out.println(FIRST_MESSAGE);
-        return Integer.parseInt(SCANNER.nextLine());
+        try {
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("구입금액을 잘못 입력했습니다.\n");
+            return askPurchasePrice();
+        }
     }
 
     public String getWinningNumber() {
@@ -22,6 +34,27 @@ public class InputView {
     public String askBonusNumber() {
         System.out.println(ASK_BONUS_BALL_NUMBER);
         return SCANNER.nextLine();
+    }
+
+    public static int askManualLottoCount() {
+        System.out.println(ASK_MANUAL_LOTTO_COUNT);
+        try {
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력 하세요");
+            return askManualLottoCount();
+        }
+    }
+
+    public static List<Integer> getAskManualLottoNumber() {
+        System.out.println(ASK_MANUAL_LOTTO_NUMBER);
+        try {
+            return Arrays.stream(split(SCANNER.nextLine()))
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+        } catch (RuntimeException e) {
+            return getAskManualLottoNumber();
+        }
     }
 
 }
