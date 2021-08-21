e685f83da6decb7f4d94d65c5a0c40212988949a
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-21 23:42:37 +0900

    test : 당첨 결과 확인 로직 작성

--- src/main/java/step2/WinningResults.java
+++ src/main/java/step2/WinningResults.java
@@ -1,7 +1,9 @@
 package step2;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WinningResults {
 
@@ -14,5 +16,35 @@ public class WinningResults {
 
     public Ticket getWinningLottoNumber() {
         return this.winningNumbers;
+    }
+
+    public Map<Integer, Integer> getWinningResult() {
+        return resultMap;
+    }
+
+    public void checkWinning(LottoTickets lottoTickets) {
+        List<Ticket> purchasedTickets = lottoTickets.getLottoTickets();
+
+        for (Ticket purchasedTicket : purchasedTickets) {
+                int countCorrectNumber = matchingWinningNumber(purchasedTicket);
+
+                if(countCorrectNumber < 3) {
+                    return;
+                }
+
+                resultMap.put(countCorrectNumber, resultMap.getOrDefault(countCorrectNumber, 0) + 1);
+        }
+    }
+
+    private int matchingWinningNumber(Ticket purchasedTicket) {
+        return purchasedTicket.getLottoNumbers()
+                .stream()
+                .filter(n -> checkWinningNumber(n))
+                .collect(Collectors.toList())
+                .size();
+    }
+
+    private boolean checkWinningNumber(int number) {
+       return winningNumbers.getLottoNumbers().contains(number);
     }
 }
