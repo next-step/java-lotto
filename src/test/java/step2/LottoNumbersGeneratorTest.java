60c75641adfc14e25381b3863a1b21376925a2f9
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-22 02:59:37 +0900

    test : 로또 번호가 정렬됐는지 테스트 코드 작성

--- src/test/java/step2/LottoNumbersGeneratorTest.java
+++ src/test/java/step2/LottoNumbersGeneratorTest.java
@@ -3,6 +3,10 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoNumbersGeneratorTest {
@@ -16,5 +20,26 @@ public class LottoNumbersGeneratorTest {
 
         assertThat(lottoTicket.getLottoNumbers().size()).isEqualTo(6);
 
+    }
+
+    @Test
+    @DisplayName("로또번호가 정렬됐는지 확인한다.")
+    void lotto_number_sorting_test() {
+        LottoNumbersGenerator lottoNumbersGenerator = new LottoNumbersGenerator();
+
+        List<Integer> lottoNumbers = lottoNumbersGenerator.generateLottoTicket().getLottoNumbers();
+
+        assertThat(checkSort(lottoNumbers)).isEqualTo(true);
+    }
+
+    private boolean checkSort(List<Integer> lottoTicket) {
+        List<Integer> sortedLottoTicket = new ArrayList<>(lottoTicket);
+        Collections.sort(sortedLottoTicket);
+
+        if(lottoTicket.equals(sortedLottoTicket)) {
+            return true;
+        }
+
+        return false;
     }
 }
