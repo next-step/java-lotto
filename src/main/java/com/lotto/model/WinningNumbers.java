db8b169f1f320c18445ccd905d710e2a35af60d6
yook0328 <yook0328@gmail.com>
2021-08-21 19:25:33 +0900

    feat: WinningNumbers에 보너스 숫자를 추가.

--- src/main/java/com/lotto/model/WinningNumbers.java
+++ src/main/java/com/lotto/model/WinningNumbers.java
@@ -1,5 +1,6 @@
 package com.lotto.model;
 
+import java.util.Collections;
 import java.util.Set;
 
 public class WinningNumbers {
@@ -7,24 +8,40 @@ public class WinningNumbers {
 
     private final Set<Integer> numbers;
 
+    private final int bonusNumber;
+
     private static final int MINIMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 45;
 
-    public WinningNumbers(Set<Integer> numbers) {
+    public WinningNumbers(Set<Integer> numbers, int bonusNumber) {
+        validateNumbers(numbers);
+        validateBonusNumber(numbers, bonusNumber);
+
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Set<Integer> numbers, int bonusNumber){
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 숫자가 이미 뽑힌 숫자들에 포함되어 있습니다.");
+        }
+
+        if (isInvalidRange(bonusNumber)) {
+            throw new IllegalArgumentException("잘못된 보너스 숫자가 입력되었습니다. " + numbers);
+        }
+    }
+
+    private void validateNumbers(Set<Integer> numbers) {
         if (numbers.size() != NUMBER_OF_WINNING_NUMBERS) {
             throw new IllegalArgumentException("잘못된 갯수의 숫자가 입력되었습니다. " + numbers);
         }
 
-        if (numbers.stream().anyMatch((number) -> number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER)) {
+        if (numbers.stream().anyMatch((number) -> isInvalidRange(number))) {
             throw new IllegalArgumentException("잘못된 숫자가 입력되었습니다. " + numbers);
         }
-
-        this.numbers = numbers;
     }
 
-    public int checkCorrectCount(Lotto lotto) {
-        return (int) lotto.stream()
-                .filter((number) -> numbers.contains(number))
-                .count();
+    private boolean isInvalidRange(int number) {
+        return number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER;
     }
 }
