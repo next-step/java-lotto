a1e1be0e8abc60a2fca66f39d64559ffa33fa0a7
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-22 02:23:39 +0900

    test : 번호의 갯수가 6개가 아닌경우 exception 발생 테스트 코드 작성

--- src/test/java/step2/TicketTest.java
+++ src/test/java/step2/TicketTest.java
@@ -2,11 +2,17 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TicketTest {
 
@@ -22,5 +28,24 @@ public class TicketTest {
         List<Integer> getLottoNumbers = lottoTicket.getLottoNumbers();
 
         assertThat(getLottoNumbers).isEqualTo(lottoNumbers);
+    }
+
+    @ParameterizedTest
+    @MethodSource("lottoNumbersProvider")
+    @DisplayName("입력 숫자가 6개가 아닌경우 Exception이 발생한다.")
+    void input_count_not_six_exception_test(List<Integer> lottoNumbers) {
+        Ticket ticket = new Ticket();
+
+        assertThatThrownBy(() ->
+                ticket.saveLottoNumbers(lottoNumbers)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    static Stream<Arguments> lottoNumbersProvider() {
+        return Stream.of(
+                arguments(Arrays.asList(1,2,3,4,5,6,7)),
+                arguments(Arrays.asList(1,2,3,4,5))
+        );
     }
 }
