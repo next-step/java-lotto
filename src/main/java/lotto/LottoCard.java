1887aa616a044d1bf79e5df130d10d7ce4f41794
longbeom <wowow1859@naver.com>
2021-08-22 15:45:44 +0900

    feat : 로또 카드 생성

---
+++ src/main/java/lotto/LottoCard.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoCard {
+	private static final int LOTTO_START_NUMBER = 1;
+	private static final int LOTTO_END_NUMBER = 45;
+	public static List<Integer> NUMBERS = new ArrayList<>();
+
+	static {
+		for (int i = LOTTO_START_NUMBER; i <= LOTTO_END_NUMBER; i++) {
+			NUMBERS.add(i);
+		}
+	}
+}
