1d0aebc81cbb1fd09cc88ae90aa8d80b7f4fae1d
jaeyoungjang2 <lion85488@gmail.com>
2021-08-22 23:05:50 +0900

    refactor: 랜덤 코드 변경

--- src/main/java/step3/util/Rank.java
+++ src/main/java/step3/util/Rank.java
@@ -4,19 +4,25 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 public enum Rank {
-    FIRST(2_000_000_000, (countOfMatch, matchBonus) -> countOfMatch == 6),
-    SECOND(30_000_000, (countOfMatch, matchBonus) -> countOfMatch == 5 && matchBonus),
-    THIRD(1_500_000, (countOfMatch, matchBonus) -> countOfMatch == 5 && !matchBonus),
-    FOURTH(50_000, (countOfMatch, matchBonus) -> countOfMatch == 4),
-    FIFTH(5_000, (countOfMatch, matchBonus) -> countOfMatch == 3),
-    MISS(0, (countOfMatch, matchBonus) -> countOfMatch == 0);
+    FIRST(2_000_000_000, 6, (countOfMatch, matchBonus) -> countOfMatch == 6),
+    SECOND(30_000_000, 5, (countOfMatch, matchBonus) -> countOfMatch == 5 && matchBonus),
+    THIRD(1_500_000, 5, (countOfMatch, matchBonus) -> countOfMatch == 5 && !matchBonus),
+    FOURTH(50_000, 4, (countOfMatch, matchBonus) -> countOfMatch == 4),
+    FIFTH(5_000, 3, (countOfMatch, matchBonus) -> countOfMatch == 3),
+    MISS(0, 0, (countOfMatch, matchBonus) -> countOfMatch == 0);
 
     private final int winningMoney;
     private final BiPredicate<Integer, Boolean> condition;
+    private final int countOfMatch;
 
-    Rank(final int winningMoney, final BiPredicate<Integer, Boolean> condition) {
+    Rank(final int winningMoney, int countOfMatch, final BiPredicate<Integer, Boolean> condition) {
         this.winningMoney = winningMoney;
         this.condition = condition;
+        this.countOfMatch = countOfMatch;
+    }
+
+    public int getCountOfMatch() {
+        return countOfMatch;
     }
 
     public static Rank valueOf(int countOfMatch, boolean matchBonus) {
@@ -24,5 +30,9 @@ public enum Rank {
                 .filter(rank -> rank.condition.test(countOfMatch, matchBonus))
                 .findFirst()
                 .orElse(MISS);
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
     }
 }
--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -29,6 +29,15 @@ public class ResultView {
         issuedLottolist.stream()
                 .forEach(issuedLotto -> System.out.println(issuedLotto));
     }
+    public void printMatchedLottoRecord(Map<Rank, Integer> matchedLottoRecordArray) {
+        System.out.println("당첨 통계");
+        System.out.println("-------");
+        System.out.printf("%s개 일치 (%s)- %s개\n",Rank.FIFTH.getCountOfMatch(),Rank.FIFTH.getWinningMoney(),matchedLottoRecordArray.get(Rank.FIFTH));
+        System.out.printf("%s개 일치 (%s)- %s개\n",Rank.FOURTH.getCountOfMatch(),Rank.FOURTH.getWinningMoney(),matchedLottoRecordArray.get(Rank.FOURTH));
+        System.out.printf("%s개 일치 (%s)- %s개\n",Rank.THIRD.getCountOfMatch(),Rank.THIRD.getWinningMoney(),matchedLottoRecordArray.get(Rank.THIRD));
+        System.out.printf("%s개 일치 (%s)- %s개\n",Rank.SECOND.getCountOfMatch(),Rank.SECOND.getWinningMoney(),matchedLottoRecordArray.get(Rank.SECOND));
+        System.out.printf("%s개 일치 (%s)- %s개\n",Rank.FIRST.getCountOfMatch(),Rank.FIRST.getWinningMoney(),matchedLottoRecordArray.get(Rank.FIRST));
+    }
 
     public void printResultPrice(int resultPrice, int numOfLotto) {
         Double totalSpendMoney = numOfLotto * chargeOfLotto;
