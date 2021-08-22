ef7f6930fbb00dcefd2722b993a7d5b9abb0081a
longbeom <wowow1859@naver.com>
2021-08-22 15:44:20 +0900

    feat : 구입 금액의 해당하는 개수만큼 로또를 생성

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+	private final static int PRICE_OF_LOTTO = 1000;
+	private List<Lotto> lottos;
+
+	public Lottos(int amount) {
+		List<Lotto> lottos = new ArrayList<>();
+		int numberOfLottos = amount / PRICE_OF_LOTTO;
+		for (int i = 0; i < numberOfLottos; i++) {
+			lottos.add(new Lotto());
+		}
+
+		this.lottos = lottos;
+	}
+
+	public List<Lotto> getLottos() {
+		return lottos;
+	}
+}
