b114b38af8777399ed3716ece95cd14fd64d76ce
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-22 20:50:10 +0900

    refactor: LottoTicket 테스트 코드에서 RandomNumbers 의존성 삭제

--- src/test/java/Lotto/Ticket/LottoTicketTest.java
+++ src/test/java/Lotto/Ticket/LottoTicketTest.java
@@ -1,16 +1,19 @@
 package Lotto.Ticket;
 
-import Lotto.Number.RandomNumbers;
+import Lotto.Number.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class LottoTicketTest {
 
-    @DisplayName("로또 티켓은 6개의 랜덤 넘버로 초기화된다.")
+    @DisplayName("로또 티켓은 6개의 숫자로 초기화된다.")
     @Test
     void createLottoTicket() {
-        assertDoesNotThrow(() -> new LottoTicket(new RandomNumbers(6, 1, 45).getValue()));
+        assertDoesNotThrow(() -> new LottoTicket(Stream.of(1, 2, 3, 4, 5, 6).map(LottoNumber::new).collect(Collectors.toList())));
     }
 }
