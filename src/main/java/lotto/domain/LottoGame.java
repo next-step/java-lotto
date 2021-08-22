682939fa2cb7300202a459564ed39076a08f8a73
조원진 <>
2021-08-22 21:13:55 +0900

    refactor(LottoGame) : 입력받은 수동 티켓을 포함하여 로또 티켓 생성

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.generic.Money;
 import lotto.util.LottoNumbersFactory;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -11,20 +12,28 @@ import static java.util.stream.Collectors.toList;
 public class LottoGame {
     public static final Money LOTTO_PRICE = Money.wons(1000L);
 
-    public static LottoTickets buy(Money money) {
-        int ticketCounts = calculateTicketCount(money);
-        List<LottoNumbers> lottoTickets = makeLottoTickets(ticketCounts);
-        return new LottoTickets(lottoTickets);
+    public static LottoTickets buy(Money money, String[] manualLottoNumbers) {
+        List<LottoNumbers> manualLottoTickets = makeManualLottoTickets(manualLottoNumbers);
+        int autoTicketCount = calculateAutoTicketCount(money, manualLottoTickets.size());
+        List<LottoNumbers> lottoTickets = makeLottoTickets(manualLottoTickets, autoTicketCount);
+        return new LottoTickets(lottoTickets, autoTicketCount);
     }
 
-    private static int calculateTicketCount(Money money) {
-        return money.divideAndDiscardRemainder(LOTTO_PRICE);
-    }
-
-    private static List<LottoNumbers> makeLottoTickets(int ticketCounts) {
-        return IntStream.range(0, ticketCounts)
-                .mapToObj((index) -> LottoNumbersFactory.makeAutoLottoNumbers())
+    private static List<LottoNumbers> makeManualLottoTickets(String[] manualLottoNumbers) {
+        return Arrays.stream(manualLottoNumbers)
+                .map(LottoNumbersFactory::makeLottoNumbers)
                 .collect(toList());
     }
 
+    private static int calculateAutoTicketCount(Money money, int manualLottoCount) {
+        Money amount = money.minus(LOTTO_PRICE.times(manualLottoCount));
+        return amount.divideAndDiscardRemainder(LOTTO_PRICE);
+    }
+
+    private static List<LottoNumbers> makeLottoTickets(List<LottoNumbers> manualLottoTickets, int autoTicketCount) {
+        manualLottoTickets.addAll(IntStream.range(0, new TicketCount(autoTicketCount).getValue())
+                .mapToObj(index -> LottoNumbersFactory.makeAutoLottoNumbers())
+                .collect(toList()));
+        return manualLottoTickets;
+    }
 }
--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -1,17 +1,48 @@
 package lotto.domain;
 
 import lotto.generic.Money;
+import lotto.util.LottoNumbersFactory;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static lotto.domain.LottoGame.LOTTO_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LottoGameTest {
 
     @DisplayName("로또 티켓 구매")
-    @Test
-    void buy() {
-        LottoTickets lottoTickets = LottoGame.buy(Money.wons(14500));
-        assertThat(lottoTickets.getLottoTickets()).hasSize(14);
+    @ParameterizedTest(name = "{index}. {displayName}, arguments: {arguments}")
+    @MethodSource("parameterProvider")
+    void buy(int purchaseAmount, String[] manualLottoNumbers) {
+        Money amount = Money.wons(purchaseAmount);
+        int ticketCount = amount.divideAndDiscardRemainder(LOTTO_PRICE);
+        LottoTickets lottoTickets = LottoGame.buy(amount, manualLottoNumbers);
+        List<LottoNumbers> manualLottoTickets = Arrays.stream(manualLottoNumbers)
+                .map(LottoNumbersFactory::makeLottoNumbers)
+                .collect(toList());
+
+        assertAll(
+                () -> assertThat(lottoTickets.getLottoTickets()).hasSize(ticketCount),
+                () -> assertThat(lottoTickets.getAutoTicketCount()).isEqualTo(ticketCount - manualLottoNumbers.length),
+                () -> assertThat(lottoTickets.getManualTicketCount()).isEqualTo(manualLottoNumbers.length),
+                () -> assertThat(lottoTickets.getLottoTickets()).containsAll(manualLottoTickets)
+        );
+    }
+
+    //구매금액, 수동 로또 배열
+    static Stream<Arguments> parameterProvider() {
+        return Stream.of(
+                Arguments.of(14500, new String[]{"6, 13, 20, 28, 35, 41", "3, 14, 22, 33, 39, 40", "1, 9, 21, 30, 35, 44"}),
+                Arguments.of(14500, new String[0]),
+                Arguments.of(3000, new String[]{"6, 13, 20, 28, 35, 41", "3, 14, 22, 33, 39, 40", "1, 9, 21, 30, 35, 44"})
+        );
     }
 }
