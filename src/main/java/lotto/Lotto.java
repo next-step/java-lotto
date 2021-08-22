0dd810de397e96fcdfdb51792297a44d1f22dd3a
longbeom <wowow1859@naver.com>
2021-08-22 15:45:59 +0900

    feat : 로또 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Lotto {
+	private List<Integer> numbers;
+
 	public Lotto() {
+		List<Integer> lottoCard = LottoCard.NUMBERS;
+		Collections.shuffle(lottoCard);
+		List<Integer> numbers = new ArrayList<>(lottoCard.subList(0, 6));
+		Collections.sort(numbers);
+		this.numbers = numbers;
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
 	}
 }
