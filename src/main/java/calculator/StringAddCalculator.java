0990c37d169e106b087062f94e95502e89ec8c00
jspark940321 <jspark940321@gmail.com>
2021-08-23 00:16:51 +0900

    feat: 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 구현
    작은따옴표('')로 감싸져있지 않는 테스트 소스 부분 수정 및 테스트 코드 오타 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,9 +1,13 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private String stringDelimiter = ",|:";
+    static private final String STRING_DELIMITER = ",|:";
+    static private final String ERROR_MESSAGE_NOT_NUMBER = "숫자 이외의 값을 입력하였습니다.";
+    static private final String ERROR_MESSAGE_MINUS_NUMBER = "음수는 입력할 수 없습니다.";
 
     public int calculate(String input) {
         if (input == null || input.isEmpty()) {
@@ -11,7 +15,6 @@ public class StringAddCalculator {
         }
 
         String[] splitStrings = splitInput(input);
-
         int[] splitNumbers = convertStringToInteger(splitStrings);
 
         int result = Arrays.stream(splitNumbers)
@@ -21,14 +24,44 @@ public class StringAddCalculator {
     }
 
     private String[] splitInput(String input) {
-        return input.split(stringDelimiter);
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String splitInput = matcher.group(2);
+            return customSplit(customDelimiter, splitInput);
+        }
+        return input.split(STRING_DELIMITER);
+    }
+
+    private String[] customSplit(String customDelimiter, String input) {
+        if (customDelimiter.equals("|")) {
+            customDelimiter = "\\|";
+        }
+        return input.split(customDelimiter);
     }
 
     private int[] convertStringToInteger(String[] splitStrings) {
-        return Arrays.stream(splitStrings)
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] splitNumbers = new int[0];
+
+        try {
+            splitNumbers = Arrays.stream(splitStrings)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(ERROR_MESSAGE_NOT_NUMBER);
+        }
+
+        validateNumber(splitNumbers);
+
+        return splitNumbers;
     }
 
-
+    private void validateNumber(int[] splitNumbers) {
+        for (int number : splitNumbers) {
+            if (number < 0) {
+                throw new RuntimeException(ERROR_MESSAGE_MINUS_NUMBER);
+            }
+        }
+    }
 }
+
