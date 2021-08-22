94b0c1f29b6df0fef386e4d53d727549f5498ffa
longbeom <wowow1859@naver.com>
2021-08-22 15:45:11 +0900

    test : 로또 번호 테스트 (1~45 까지 각각 다른 6개의 번호)

--- src/test/java/lotto/LottoAutoTest.java
+++ src/test/java/lotto/LottoAutoTest.java
@@ -3,12 +3,22 @@ package lotto;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoAutoTest {
+	private Lotto lotto;
+
+	@BeforeEach
+	void init() {
+		lotto = new Lotto();
+	}
+
 	@DisplayName(value = "구입 금액에 해당하는 로또 개수를 발급하는지 테스트")
 	@CsvSource(value = {"14900,14", "10000,10", "5000,5", "0,0"})
 	@ParameterizedTest
@@ -17,5 +27,21 @@ public class LottoAutoTest {
 		List<Lotto> lottoList = lottos.getLottos();
 		int numberOfLotto = lottoList.size();
 		assertThat(numberOfLotto).isEqualTo(expect);
+	}
+
+	@DisplayName(value = "로또 번호가 1부터 45까지 각각 다른 6개의 번호로 구성되는지 테스트")
+	@Test
+	void numbersOfLotto() {
+		List<Integer> numbers = lotto.getNumbers()
+			.stream()
+			.distinct()
+			.collect(Collectors.toList());
+
+		int numberOfDistinctNumbers = numbers.size();
+
+		assertThat(numberOfDistinctNumbers).isEqualTo(6);
+
+		List<Integer> lottoCard = LottoCard.NUMBERS;
+		assertThat(lottoCard).containsAll(numbers);
 	}
 }
