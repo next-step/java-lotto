fc50e456bc7710e86057fe2eb4d72e704494c8c1
devchang202 <devchang202@gmail.com>
2021-08-22 22:46:05 +0900

    feat(StringAddCalculator) : “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있는 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
@@ -10,6 +13,12 @@ public class StringAddCalculator {
     }
 
     static String[] parseOperands(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
         return text.split(",|:");
     }
 
