b11c73f6335a49ae2ef1876dae488f874d8a85f6
yook0328 <yook0328@gmail.com>
2021-08-22 22:01:55 +0900

    refactor: 수동 구매 기능 작성 및 메인 메서드 내 로직들 함수로 추출.

--- src/main/java/com/lotto/controller/LottoController.java
+++ src/main/java/com/lotto/controller/LottoController.java
@@ -1,12 +1,11 @@
 package com.lotto.controller;
 
 import com.lotto.game.Game;
-import com.lotto.model.LottoResult;
-import com.lotto.model.Lottos;
-import com.lotto.model.WinningNumbers;
+import com.lotto.model.*;
 import com.lotto.util.InputParser;
 import com.lotto.view.LottoDrawer;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,25 +14,58 @@ public class LottoController {
         Scanner scanner = new Scanner(System.in);
 
         LottoDrawer.drawMoneyInputMessage();
-        int money = InputParser.convert(scanner.nextLine());
+        LottoMoney lottoMoney = new LottoMoney(InputParser.convert(scanner.nextLine()));
 
-        if (money < Game.LOTTO_PRICE) {
-            LottoDrawer.drawWhenUnderMiminumMoney();
-            return;
-        }
+        LottoDrawer.drawManualLottoCount();
+        int manualCount = InputParser.convert(scanner.nextLine());
+        LottoTicketInfo lottoTicketInfo = new LottoTicketInfo(lottoMoney.getMoney(), manualCount);
 
-        Lottos lottos = Game.purchase(money);
-        LottoDrawer.drawBuyResult(lottos);
 
+        Lottos lottos = proceedLottoPurchasePhase(scanner, lottoTicketInfo);
+        LottoDrawer.drawBuyResult(manualCount, lottos);
+        WinningNumbers winningNumbers = proceedMakeWiningNumberPhase(scanner);
+        proceedResultPhase(lottoMoney.getMoney(), winningNumbers, lottos);
+    }
+
+    private static void proceedResultPhase(int money, WinningNumbers winningNumbers, Lottos lottos) {
+        LottoResult result = Game.check(winningNumbers, lottos);
+        float benefit = result.calculateBenefit(money);
+        LottoDrawer.drawResult(result, benefit);
+    }
+
+    private static WinningNumbers proceedMakeWiningNumberPhase(Scanner scanner) {
         LottoDrawer.drawWinningNumbers();
         List<Integer> numbers = InputParser.parseNumbers(scanner.nextLine());
 
         LottoDrawer.drawBonusNumber();
         int bonusNumber = InputParser.convert(scanner.nextLine());
-        WinningNumbers winningNumbers = Game.makeWinningNumbers(numbers, bonusNumber);
+        return Game.makeWinningNumbers(numbers, bonusNumber);
+    }
 
-        LottoResult result = Game.check(winningNumbers, lottos);
-        float benefit = result.calculateBenefit(money);
-        LottoDrawer.drawResult(result, benefit);
+    private static Lottos proceedLottoPurchasePhase(Scanner scanner, LottoTicketInfo lottoTicketInfo) {
+        LottoDrawer.drawManualLottoNumberInput();
+        List<Lotto> lottoList = new ArrayList<>();
+        lottoList.addAll(proceedManualPhase(scanner, lottoTicketInfo.getManualCount()));
+        lottoList.addAll(proceedAutoPhase(lottoTicketInfo.getAutoCount()));
+        return new Lottos(lottoList);
+    }
+
+    private static List<Lotto> proceedManualPhase(Scanner scanner, int manualCount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int index = 0; index < manualCount; index++) {
+            List<Integer> manualNumbers = InputParser.parseNumbers(scanner.nextLine());
+            lottoList.add(Game.purchaseManual(manualNumbers));
+        }
+
+        return lottoList;
+    }
+
+    private static List<Lotto> proceedAutoPhase(int autoCount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int index = 0; index < autoCount; index++) {
+            lottoList.add(Game.purchaseAuto());
+        }
+
+        return lottoList;
     }
 }
