750b859118c8f956ec5661acb68cdeb1474ca6c0
joo-pe <joohyung1002@naver.com>
2021-08-22 23:04:05 +0900

    복수 구분자 추가

--- src/main/java/com/techmoa/service/StringAddCalculator.java
+++ src/main/java/com/techmoa/service/StringAddCalculator.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class StringAddCalculator {
     public static String targetStr = "";
     public static int result = 0;
-    public static final String REGEXSPLIT =",";
+    public static final String REGEXSPLIT =",|;";
 
     public static int splitAndSum(String inputStr) {
         targetStr = inputStr;
@@ -13,21 +13,14 @@ public class StringAddCalculator {
             checkOneChar();
             checkComa();
         }
-
         return result;
     }
 
     private static void checkComa() {
-        if (checkRegexSplit()) {
-            String paramSums[] = targetStr.split(REGEXSPLIT);
-            result = Arrays.stream(paramSums)
-                        .mapToInt(Integer::parseInt)
-                        .sum();
-        }
-    }
-
-    private static boolean checkRegexSplit() {
-        return targetStr.contains(REGEXSPLIT);
+        String paramSums[] = targetStr.split(REGEXSPLIT);
+        result = Arrays.stream(paramSums)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
     public static boolean checkNullStr() {
@@ -37,8 +30,11 @@ public class StringAddCalculator {
         }
         return true;
     }
+
     public static void checkOneChar() {
         if(targetStr.length() == 1)
             result =  Integer.parseInt(targetStr);
     }
+
+
 }
