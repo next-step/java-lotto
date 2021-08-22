cc4c22bfc941c1accb2fcfeb83cd279467236056
joo-pe <joohyung1002@naver.com>
2021-08-22 17:45:01 +0900

    문자수 확인

--- src/main/java/com/techmoa/service/StringAddCalculator.java
+++ src/main/java/com/techmoa/service/StringAddCalculator.java
@@ -1,10 +1,23 @@
 package com.techmoa.service;
 
 public class StringAddCalculator {
+    public static int result = 0;
     public static int splitAndSum(String inputStr) {
-        if(inputStr == null || inputStr.isEmpty())
-            return 0;
+        if(checkNullStr(inputStr)) {
+            checkOneChar(inputStr);
+        }
 
-        return 0;
+        return result;
+    }
+    public static boolean checkNullStr(String inputStr) {
+        if(inputStr == null || inputStr.isEmpty()) {
+            result = 0;
+            return false;
+        }
+        return true;
+    }
+    public static void checkOneChar(String inputStr) {
+        if(inputStr.length() == 1)
+            result =  Integer.parseInt(inputStr);
     }
 }
