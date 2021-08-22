9b0bae02f021b9b79d8cbb8b613c81bd979d10ef
joo-pe <joohyung1002@naver.com>
2021-08-22 23:20:37 +0900

    커스텀 구분자 추가

--- src/main/java/com/techmoa/service/StringAddCalculator.java
+++ src/main/java/com/techmoa/service/StringAddCalculator.java
@@ -1,26 +1,29 @@
 package com.techmoa.service;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     public static String targetStr = "";
     public static int result = 0;
-    public static final String REGEXSPLIT =",|;";
+    public static final String REGEXSPLIT =",|:";
 
     public static int splitAndSum(String inputStr) {
         targetStr = inputStr;
         if(checkNullStr()) {
             checkOneChar();
-            checkComa();
+            checkCustomSeparator();
         }
         return result;
     }
 
-    private static void checkComa() {
-        String paramSums[] = targetStr.split(REGEXSPLIT);
-        result = Arrays.stream(paramSums)
-                .mapToInt(Integer::parseInt)
-                .sum();
+    private static void checkCustomSeparator() {
+        if(targetStr.contains("//")) {
+            convertCustomPatten();
+            return;
+        }
+        add(targetStr.split(REGEXSPLIT));
     }
 
     public static boolean checkNullStr() {
@@ -34,6 +37,21 @@ public class StringAddCalculator {
     public static void checkOneChar() {
         if(targetStr.length() == 1)
             result =  Integer.parseInt(targetStr);
+    }
+
+    public static void add(String paramSums[]){
+        result = Arrays.stream(paramSums)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    public static void convertCustomPatten() {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(targetStr);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+            add(tokens);
+        }
     }
 
 
