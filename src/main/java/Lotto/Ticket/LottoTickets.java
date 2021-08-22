11a474b001b46b3f8a10028281d0e8d068de8bc2
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-22 22:47:29 +0900

    feature: 로또 티켓 VO 추가

---
+++ src/main/java/Lotto/Ticket/LottoTickets.java
@@ -0,0 +1,30 @@
+package Lotto.Ticket;
+
+import java.util.List;
+import java.util.Objects;
+
+public class LottoTickets {
+
+    private final List<LottoTicket> lottoTickets;
+
+    public LottoTickets(List<LottoTicket> lottoTickets) {
+        this.lottoTickets = lottoTickets;
+    }
+
+    public List<LottoTicket> getValue() {
+        return lottoTickets;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoTickets that = (LottoTickets) o;
+        return Objects.equals(lottoTickets, that.lottoTickets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoTickets);
+    }
+}
