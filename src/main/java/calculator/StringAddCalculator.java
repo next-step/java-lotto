2caf298e5c5a19a43c18c7f0cf85fe16d009f24c
devchang202 <devchang202@gmail.com>
2021-08-22 22:20:47 +0900

    feat(StringAddCalculator) : 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환하는 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,12 +1,25 @@
 package calculator;
 
 public class StringAddCalculator {
-
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
-        return Integer.parseInt(text);
+        return add(parseOperands(text));
+    }
+
+    static String[] parseOperands(String text) {
+        return text.split(",");
+    }
+
+    static int add(String[] operands) {
+        int sum = 0;
+
+        for (int i = 0; i < operands.length; i++) {
+            sum += Integer.parseInt(operands[i]);
+        }
+
+        return sum;
     }
 }
