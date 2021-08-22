1f931c6f342f500c9c3066138d7b6b6428880159
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-22 13:38:43 +0900

    refactor: 랜덤넘버테스트 중복 코드 제거, 기본 값 추가

--- src/test/java/Lotto/Number/RandomNumbersTest.java
+++ src/test/java/Lotto/Number/RandomNumbersTest.java
@@ -12,20 +12,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("랜덤 넘버 리스트는")
 public class RandomNumbersTest {
 
+    public static final int DEFAULT_LOTTO_NUMBER = 0;
+
     @DisplayName("랜덤 숫자를 주어진 갯수만큼 만든다.")
     @ParameterizedTest(name = "갯수: {0}")
     @ValueSource(ints = {5, 6, 7})
     void generateGivenLengthOfNumbers(int length) {
         int actual = new RandomNumbers(length, Limit.MIN.getValue(), Limit.MAX.getValue()).getValue().size();
-        int expected = length;
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(length);
     }
 
     @DisplayName("랜덤 넘버 후보의 최댓값 = 생성자에 명시된 최댓값")
     @Test
     void generateGivenMaxAsMaxCandidateOfRandomNumbers() {
         RandomNumbers randomNumbers = new RandomNumbers(Limit.MAX.getValue(), Limit.MIN.getValue(), Limit.MAX.getValue());
-        LottoNumber actual = randomNumbers.getValue().stream().max(Comparator.comparing(LottoNumber::getValue)).get();
+        LottoNumber actual = randomNumbers.getValue().stream().max(getComparing()).orElse(getDefault());
         LottoNumber expected = new LottoNumber(Limit.MAX.getValue());
         assertThat(actual).isEqualTo(expected);
     }
@@ -34,8 +35,16 @@ public class RandomNumbersTest {
     @Test
     void generateGivenMinAsMinCandidateOfRandomNumbers() {
         RandomNumbers randomNumbers = new RandomNumbers(Limit.MAX.getValue(), Limit.MIN.getValue(), Limit.MAX.getValue());
-        LottoNumber actual = randomNumbers.getValue().stream().min(Comparator.comparing(LottoNumber::getValue)).get();
+        LottoNumber actual = randomNumbers.getValue().stream().min(getComparing()).orElse(getDefault());
         LottoNumber expected = new LottoNumber(Limit.MIN.getValue());
         assertThat(actual).isEqualTo(expected);
+    }
+
+    private Comparator<LottoNumber> getComparing() {
+        return Comparator.comparing(LottoNumber::getValue);
+    }
+
+    private LottoNumber getDefault() {
+        return new LottoNumber(DEFAULT_LOTTO_NUMBER);
     }
 }
