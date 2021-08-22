dc45781f26fbafcf18b18517c5757c6d8a00591f
jspark940321 <jspark940321@gmail.com>
2021-08-22 23:43:34 +0900

    feat: 기본 구분자(쉼표, 콜론)시 숫자합 반환 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,10 +1,34 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
+    private String stringDelimiter = ",|:";
+
     public int calculate(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        return 0;
+
+        String[] splitStrings = splitInput(input);
+
+        int[] splitNumbers = convertStringToInteger(splitStrings);
+
+        int result = Arrays.stream(splitNumbers)
+                .sum();
+
+        return result;
     }
+
+    private String[] splitInput(String input) {
+        return input.split(stringDelimiter);
+    }
+
+    private int[] convertStringToInteger(String[] splitStrings) {
+        return Arrays.stream(splitStrings)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+
 }
