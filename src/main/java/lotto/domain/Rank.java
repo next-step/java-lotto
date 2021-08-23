80be9bab83d981b9bcf623b780fa37c54ce6efb5
SONG22 <ansongi527@gmail.com>
2021-08-23 20:19:17 +0900

    refactor(step3) : 피드백 적용(bonus match)

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -7,26 +7,27 @@ import java.util.stream.Collectors;
 
 public enum Rank {
 
-    NOTHING(0, 0, (match, bonusMatch) -> match < 3),
-    FIFTH(5_000, 3, (match, bonusMatch) -> match == 3),
-    FOURTH(50_000, 4, (match, bonusMatch) -> match == 4),
-    THIRD(1_500_000, 5, (match, bonusMatch) -> match == 5 && !bonusMatch),
-    SECOND(30_000_000, 5, (match, bonusMatch) -> match == 5 && bonusMatch),
-    FIRST(2_000_000_000, 6, (match, bonusMatch) -> match == 6);
+    NOTHING(0, 0, false),
+    FIFTH(5_000, 3, false),
+    FOURTH(50_000, 4, false),
+    THIRD(1_500_000, 5, false),
+    SECOND(30_000_000, 5, true),
+    FIRST(2_000_000_000, 6, false);
 
     private int rewards;
     private int match;
-    private BiPredicate<Integer, Boolean> matchFilter;
+    private boolean matchBonus;
 
-    Rank(int rewards, int match, BiPredicate<Integer, Boolean> matchFilter) {
+    Rank(int rewards, int match, boolean matchBonus) {
         this.rewards = rewards;
         this.match = match;
-        this.matchFilter = matchFilter;
+        this.matchBonus = matchBonus;
     }
 
     static Rank findRank(int countOfMatch, boolean matchBonus) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.getMatchFilter().test(countOfMatch, matchBonus))
+                .filter(rank -> countOfMatch < 3 || rank.match == countOfMatch)
+                .filter(rank -> rank.match != 5 || matchBonus && rank.matchBonus)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("맞는 랭크가 없습니다."));
     }
@@ -43,10 +44,6 @@ public enum Rank {
 
     public int getRewards() {
         return rewards;
-    }
-
-    public BiPredicate<Integer, Boolean> getMatchFilter() {
-        return matchFilter;
     }
 
 }
--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -11,6 +11,7 @@ class RankTest {
     @DisplayName("로또 맞춘 갯수에 따라 등수 찾기 - 보너스 볼 추가")
     @Test
     void findRank() {
+        assertThat(Rank.findRank(2, true)).isEqualTo(Rank.NOTHING);
         assertThat(Rank.findRank(3, false)).isEqualTo(Rank.FIFTH);
         assertThat(Rank.findRank(4, false)).isEqualTo(Rank.FOURTH);
         assertThat(Rank.findRank(5, true)).isEqualTo(Rank.SECOND);
