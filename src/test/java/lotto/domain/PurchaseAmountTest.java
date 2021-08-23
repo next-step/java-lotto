1e1bde18fbf34f3da58bf996aa70165131555152
jspark940321 <jspark940321@gmail.com>
2021-08-24 00:28:56 +0900

    test: 구입금액테스트 작성

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import lotto.LottoConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SuppressWarnings("NonAsciiCharacters")
+class PurchaseAmountTest {
+    @ParameterizedTest
+    @CsvSource({
+            "14000, 14",
+            "500, 0",
+            "5300, 5"
+    })
+    void 구입금액_로또개수_테스트(int input, int expect) {
+        PurchaseAmount purchaseAmount = new PurchaseAmount(input);
+        int actual = purchaseAmount.getLottoCount();
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    void 예외_구입금액_음수_테스트() {
+        assertThatThrownBy(() -> {
+            PurchaseAmount purchaseAmount = new PurchaseAmount(-1);
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LottoConfig.PURCHASE_AMOUNT_MINUS_ERROR_MESSAGE);
+    }
+}
