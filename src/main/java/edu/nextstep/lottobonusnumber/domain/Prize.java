f265a5d6e1a76f2b3368c8800be03a6a7181ca1f
beadoer1 <beadoer1@gmail.com>
2021-08-24 22:10:59 +0900

    feat(Prize) : Prize 생성자(정적 팩토리) 2등 구분 포함 구현

--- src/main/java/edu/nextstep/lottobonusnumber/domain/Prize.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/Prize.java
@@ -1,18 +1,34 @@
 package edu.nextstep.lottobonusnumber.domain;
 
+import java.util.Arrays;
+
 public enum Prize {
     FIRST(6, false, 2_000_000_000),
     SECOND(5, true, 30_000_000),
     THIRD(5, false, 1_500_000),
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),
-    LOSE(0, false, 0),;
+    LOSE(0, false, 0);
+
+    private final int countOfMatch;
+    private final boolean hasBonusNumber;
+    private final int winningPrize;
 
     Prize(int countOfMatch, boolean hasBonusNumber, int winningPrize) {
-
+        this.countOfMatch = countOfMatch;
+        this.hasBonusNumber = hasBonusNumber;
+        this.winningPrize = winningPrize;
     }
 
-    public static Prize of(int countOfMatching, boolean hasBonusNumber) {
-        return null;
+    public static Prize of(int countOfMatch, boolean hasBonusNumber) {
+
+        if (countOfMatch == 5 && !hasBonusNumber) {
+            return THIRD;
+        }
+
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.countOfMatch == countOfMatch)
+                .findFirst()
+                .orElse(LOSE);
     }
 }
--- src/test/java/edu/nextstep/lottobonusnumber/domain/PrizeTest.java
+++ src/test/java/edu/nextstep/lottobonusnumber/domain/PrizeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class PrizeTest {
     @ParameterizedTest(name = "Prize enum 정적 팩토리 생성자 Test [{index}] {0}, {1}")
-    @CsvSource({"6, false, FIRST", "5, true, SECOND", "5, false, THIRD", "4, false, FOURTH", "3, false, FOURTH", "0, false, LOSE"})
+    @CsvSource({"6, false, FIRST", "5, true, SECOND", "5, false, THIRD", "4, false, FOURTH", "3, false, FIFTH", "0, false, LOSE"})
     void of(int countOfMatching, boolean hasBonusNumber, Prize checkPrize) {
         // when
         Prize prize = Prize.of(countOfMatching, hasBonusNumber);
