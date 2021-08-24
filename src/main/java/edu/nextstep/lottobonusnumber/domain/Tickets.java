d87cc66db93089fb222a073bd9604eee199ac77d
beadoer1 <beadoer1@gmail.com>
2021-08-24 22:26:04 +0900

    feat(Tickets) : Tickets 생성자(정적 팩토리) 구현

--- src/main/java/edu/nextstep/lottobonusnumber/domain/Tickets.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/Tickets.java
@@ -1,14 +1,29 @@
 package edu.nextstep.lottobonusnumber.domain;
 
+import edu.nextstep.lottobonusnumber.domain.numbersmaker.NumbersMaker;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class Tickets {
-    private List<Ticket> tickets;
+    private final List<Ticket> tickets;
+
+    public Tickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 
     public static Tickets of(Payment payment, NumbersMaker numbersMaker) {
-        return null;
+        int countOfTickets = payment.countOfTickets();
+
+        List<Ticket> tickets = new ArrayList<>();
+        for (int i = 0; i < countOfTickets; i++) {
+            Ticket ticket = Ticket.madeBy(numbersMaker);
+            tickets.add(ticket);
+        }
+
+        return new Tickets(tickets);
     }
 
     public Stream<Ticket> stream() {
