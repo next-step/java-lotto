d9ce630b1ec6fc68938a04460e64e0c7c8b3b1f6
beadoer1 <beadoer1@gmail.com>
2021-08-24 22:22:21 +0900

    feat(Ticket, NumberMaker) : Ticket 생성자(정적 팩토리) 및 winningTicket 과의 중복 숫자 개수 출력 메서드 구현

--- src/main/java/edu/nextstep/lottobonusnumber/domain/Ticket.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/Ticket.java
@@ -1,20 +1,42 @@
 package edu.nextstep.lottobonusnumber.domain;
 
+import edu.nextstep.lottobonusnumber.exception.NumbersIllegalArgumentException;
 import edu.nextstep.lottobonusnumber.domain.numbersmaker.NumbersMaker;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Ticket {
 
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LENGTH_OF_NUMBERS = 6;
+
     private final List<Integer> numbers;
 
     public Ticket(List<Integer> numbers) {
+        Collections.sort(numbers);
+        validateLengthOfNumbers(numbers);
+        validateOutOfRange(numbers);
         this.numbers = numbers;
     }
 
+    private void validateOutOfRange(List<Integer> numbers) {
+        if (numbers.get(0) < MIN_NUMBER || numbers.get(numbers.size()-1) > MAX_NUMBER) {
+            throw new NumbersIllegalArgumentException("지정 가능 숫자 범위 초과. 범위 : " + MIN_NUMBER + " ~ " + MAX_NUMBER);
+        }
+    }
+
+    private void validateLengthOfNumbers(List<Integer> numbers) {
+        if (numbers.size() != LENGTH_OF_NUMBERS) {
+            throw new NumbersIllegalArgumentException("입력 숫자 개수 미달 or 초과. 지정 개수 : " + LENGTH_OF_NUMBERS);
+        }
+    }
+
     public static Ticket madeBy(NumbersMaker numbersMaker) {
-        return new Ticket(numbersMaker.create());
+        List<Integer> numbers = numbersMaker.create();
+        return new Ticket(numbers);
     }
 
     public Stream<Integer> stream() {
@@ -22,7 +44,9 @@ public class Ticket {
     }
 
     public int countMatchingNumbers(Ticket winningTicket) {
-        return 0;
+        return (int) winningTicket.stream()
+                .filter(numbers::contains)
+                .count();
     }
 
     public boolean contains(int bonusNumber) {
---
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/numbersmaker/CustomNumbersMaker.java
@@ -0,0 +1,11 @@
+package edu.nextstep.lottobonusnumber.domain.numbersmaker;
+
+import java.util.List;
+
+public class CustomNumbersMaker implements NumbersMaker{
+
+    @Override
+    public List<Integer> create() {
+        return null;
+    }
+}
