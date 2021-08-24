6ea633899c7d61c0f3b1cf4d4605b795a987ca40
beadoer1 <beadoer1@gmail.com>
2021-08-25 00:26:49 +0900

    feat(NumbersMaker) : AutoNumbersMaker, CustomNumbersMaker 의 create() 구현

--- src/main/java/edu/nextstep/lottobonusnumber/domain/numbersmaker/AutoNumbersMaker.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/numbersmaker/AutoNumbersMaker.java
@@ -1,11 +1,17 @@
 package edu.nextstep.lottobonusnumber.domain.numbersmaker;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class AutoNumbersMaker implements NumbersMaker{
 
     @Override
     public List<Integer> create() {
-        return null;
+        return new Random().ints(1, 46)
+                .distinct()
+                .limit(6)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/edu/nextstep/lottobonusnumber/domain/numbersmaker/CustomNumbersMaker.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/numbersmaker/CustomNumbersMaker.java
@@ -1,11 +1,22 @@
 package edu.nextstep.lottobonusnumber.domain.numbersmaker;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomNumbersMaker implements NumbersMaker{
 
+    private final List<Integer> numbers;
+
+    public CustomNumbersMaker(String winningNumbers) {
+        this.numbers = Arrays.stream(winningNumbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
     @Override
     public List<Integer> create() {
-        return null;
+        return numbers;
     }
 }
