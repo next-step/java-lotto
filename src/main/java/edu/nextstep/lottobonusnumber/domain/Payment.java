c432b1611b99e8d2b141741b8ee6d01dfdc56caf
beadoer1 <beadoer1@gmail.com>
2021-08-24 21:45:32 +0900

    feat(Payment) : Payment 생성자, 검증 메서드 구현

--- src/main/java/edu/nextstep/lottobonusnumber/domain/Payment.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/Payment.java
@@ -1,11 +1,33 @@
 package edu.nextstep.lottobonusnumber.domain;
 
+import edu.nextstep.lottoauto.exception.PaymentIllegalArgumentException;
+
 public class Payment {
 
+    public static final int TICKET_PRICE = 1_000;
+
+    private final int payment;
+
     public Payment(int payment) {
+        validateUnderAUnitPrice(payment);
+        validateDivideUnitPrice(payment);
+
+        this.payment = payment;
+    }
+
+    private static void validateUnderAUnitPrice(int payment) {
+        if (payment < TICKET_PRICE) {
+            throw new PaymentIllegalArgumentException("최소 입력 가능 금액 미달. 최소 입력 금액 : " + TICKET_PRICE + " 원");
+        }
+    }
+
+    private static void validateDivideUnitPrice(int payment) {
+        if ((payment % TICKET_PRICE) != 0) {
+            throw new PaymentIllegalArgumentException("개 당 금액 " + TICKET_PRICE + " 원으로 해당 단위로 입력 필요.");
+        }
     }
 
     public int countOfTickets() {
-        return 0;
+        return payment / TICKET_PRICE;
     }
 }
--- src/test/java/edu/nextstep/lottobonusnumber/domain/PaymentTest.java
+++ src/test/java/edu/nextstep/lottobonusnumber/domain/PaymentTest.java
@@ -1,7 +1,5 @@
 package edu.nextstep.lottobonusnumber.domain;
 
-import edu.nextstep.lottoauto.domain.TicketMachine;
-import edu.nextstep.lottoauto.domain.ticketmaker.AutoNumbersMaker;
 import edu.nextstep.lottoauto.exception.PaymentIllegalArgumentException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PaymentTest {
 
-    @ParameterizedTest(name = "금액 검증 실패 : " + Ticket.TICKET_PRICE + "원 미만")
-    @ValueSource(ints = {1, Ticket.TICKET_PRICE-1})
+    @ParameterizedTest(name = "금액 검증 실패 : " + Payment.TICKET_PRICE + "원 미만")
+    @ValueSource(ints = {1, Payment.TICKET_PRICE-1})
     void create_fail_under_unit_price(int payment) {
         // when
         assertThatThrownBy(() -> new Payment(payment))
@@ -19,8 +17,8 @@ public class PaymentTest {
                 .hasMessageContaining("최소 입력 가능 금액 미달.");
     }
 
-    @ParameterizedTest(name = "금액 검증 실패 : " + Ticket.TICKET_PRICE + "원 단위 떨어지지 않음")
-    @ValueSource(ints = {Ticket.TICKET_PRICE+1, 2*Ticket.TICKET_PRICE-1})
+    @ParameterizedTest(name = "금액 검증 실패 : " + Payment.TICKET_PRICE + "원 단위 떨어지지 않음")
+    @ValueSource(ints = {Payment.TICKET_PRICE+1, 2*Payment.TICKET_PRICE-1})
     void create_divide_unit_price(int payment) {
         // when, then
         assertThatThrownBy(() -> new Payment(payment))
