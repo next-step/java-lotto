a553cac7168213e4c9440de2cde243af68c6a9e4
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-25 13:54:30 +0900

    fix: 당첨번호 입력 받을 때 ,와 숫자 사이 공백 제거

--- src/main/java/UI/InputView.java
+++ src/main/java/UI/InputView.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class InputView {
-
     public static int askUserToPayForLottoTickets() {
         System.out.println("구입금액을 입력해주세요.");
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +17,7 @@ public class InputView {
     public static List<LottoNumber> getWinningNumbersOfLastWeek() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
-        return Arrays.stream(scanner.nextLine().trim().split(",")).map(Integer::valueOf).map(LottoNumber::new).collect(Collectors.toList());
+        String[] userInputs = scanner.nextLine().replaceAll("\\s+", "").split(",");
+        return Arrays.stream(userInputs).map(Integer::valueOf).map(LottoNumber::new).collect(Collectors.toList());
     }
 }
