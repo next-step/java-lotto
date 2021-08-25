86f9cb0cb1ddf3fe5642728f23c6a349b9cb3223
beadoer1 <beadoer1@gmail.com>
2021-08-26 02:59:27 +0900

    feat(LottoNumber) : 숫자에 대한 중복 최소화를 위한 LottoNumber 객체화. 테스트 및 클래스 생성

---
+++ src/test/java/edu/nextstep/lottobonusnumber/domain/LottoNumberTest.java
@@ -0,0 +1,36 @@
+package edu.nextstep.lottobonusnumber.domain;
+
+import edu.nextstep.lottobonusnumber.exception.NumbersIllegalArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTest {
+
+    @Test
+    @DisplayName("LottoNumber 생성")
+    void create() {
+        // given
+        int number = 1;
+
+        // when
+        LottoNumber lottoNumber = LottoNumber.of(number);
+
+        // then
+        assertThat(lottoNumber).isEqualTo(LottoNumber.of(1));
+    }
+
+    @ParameterizedTest(name = "LottoNumber 생성 실패 : 허용 범위 초과 {0}")
+    @ValueSource(ints = {0, 46}) // given
+    void create_fail_out_of_range(int number) {
+        // when, then
+        assertThatThrownBy(() -> LottoNumber.of(number))
+                .isInstanceOf(NumbersIllegalArgumentException.class)
+                .hasMessageContaining("지정 가능 숫자 범위 초과.");
+    }
+
+}
