1ed714afd3ea577fbddf18b291cb206c5e02a9bf
joo-pe <joohyung1002@naver.com>
2021-08-26 22:01:12 +0900

    (feat) 테스트 확인

--- src/test/java/com/techmoa/lotto/service/LottoServiceTest.java
+++ src/test/java/com/techmoa/lotto/service/LottoServiceTest.java
@@ -2,12 +2,15 @@ package com.techmoa.lotto.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoServiceTest {
 
     @Test
-    public void 복권발행_티켓수확인() {
+    public void 당청번호_생성() {
         int purcharAmount = 5000;
         LottoService lottoService = new LottoService();
         lottoService.createLotto(purcharAmount);
@@ -15,8 +18,24 @@ class LottoServiceTest {
         assertEquals(5, lottoService.getLottoTickets().getLottoTickets().size());
     }
 
-    @Test
-    public void 당청번호_생성() {
 
+    @Test
+    public void 당첨_체크() {
+        LottoService lottoService = new LottoService();
+        String inputString = "1, 3, 4, 5, 6, 7";
+
+        LottoAnswer lottoAnswer = new LottoAnswer(inputString);
+        List<LottoTicket> lottoTicketList = new ArrayList<>();
+        lottoTicketList.add(new LottoTicket().makeLottoNumber());
+        lottoTicketList.add(new LottoTicket().makeLottoNumber());
+        lottoTicketList.add(new LottoTicket().makeLottoNumber());
+        lottoTicketList.add(new LottoTicket().makeLottoNumber());
+        lottoTicketList.add(new LottoTicket().makeLottoNumber());
+        lottoTicketList.add(new LottoTicket().makeLottoNumber());
+
+        LottoTickets lottoTickets = new LottoTickets(lottoTicketList);
+
+        LottoWinResult lottoWinResult = lottoService.checkWinnings(lottoAnswer , lottoTickets);
+        assertEquals(6, lottoWinResult.getResultMap().size());
     }
 }
