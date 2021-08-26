fc6e58134c8ed144140c4202bf6304b90fc6b790
HueWoon Kim <kho0307@gmail.com>
2021-08-26 23:56:04 +0900

    refactor: 숫자 유효범위 테스트 설명 추가

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -2,11 +2,13 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoNumberTest {
 
 	@Test
+	@DisplayName("1~45까지 유효범위 검사")
 	void scope() {
 		assertThatThrownBy(() -> {
 			LottoNumber n1 = new LottoNumber(0);
