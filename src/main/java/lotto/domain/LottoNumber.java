ae0b5b27368e9a4ad05b369650094dd4f57bd34a
khcho902 <khcho902@gmail.com>
2021-08-27 21:07:47 +0900

    feat: LottoNumber클래스 valueOf메소드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -6,18 +6,29 @@ public class LottoNumber implements Comparable<LottoNumber> {
 
   public static final int MIN = 1;
   public static final int MAX = 45;
+
   private final int number;
+  private static final LottoNumber[] lottoNumberCache = new LottoNumber[45];
 
   public LottoNumber(int number) {
     checkNumber(number);
     this.number = number;
   }
 
+  public static LottoNumber valueOf(int number) {
+    checkNumber(number);
+    if (lottoNumberCache[number - 1] != null){
+      return lottoNumberCache[number - 1];
+    }
+    lottoNumberCache[number - 1] = new LottoNumber(number);
+    return lottoNumberCache[number - 1];
+  }
+
   public int number() {
     return number;
   }
 
-  private void checkNumber(int number) {
+  private static void checkNumber(int number) {
     if (number < MIN || number > MAX) {
       throw new IllegalArgumentException("생성 가능한 로또 번호는 " + MIN + "부터 " + MAX + "까지입니다.");
     }
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoNumberTest {
 
-  @ParameterizedTest(name = "로또번호 생성 테스트 : {0}")
+  @ParameterizedTest(name = "생성(new) 테스트 : {0}")
   @ValueSource(ints = {1, 2, 45})
   void createTest(int number) {
     //when
@@ -18,12 +18,32 @@ class LottoNumberTest {
     assertThat(lottoNumber).isEqualTo(new LottoNumber(number));
   }
 
-  @ParameterizedTest(name = "로또번호 생성 에러 테스트 : {0}")
+  @ParameterizedTest(name = "잘못된 생성(new) 테스트 : {0}")
   @ValueSource(ints = {-1, 0, 46})
   void invalidCreateTest(int number) {
     //then
     assertThatIllegalArgumentException().isThrownBy(() -> {
       new LottoNumber(number);
+    });
+  }
+
+  @ParameterizedTest(name = "생성 및 반환(valueOf) 테스트 : {0}")
+  @ValueSource(ints = {1, 2, 45})
+  void valueOfTest(int number) {
+    //when
+    LottoNumber lottoNumber = LottoNumber.valueOf(number);
+
+    //then
+    assertThat(lottoNumber == LottoNumber.valueOf(number)).isTrue();
+    assertThat(lottoNumber).isEqualTo(new LottoNumber(number));
+  }
+
+  @ParameterizedTest(name = "잘못된 생성 및 반환(valueOf)테스트 : {0}")
+  @ValueSource(ints = {-1, 0, 46})
+  void invalidValueOfTest(int number) {
+    //then
+    assertThatIllegalArgumentException().isThrownBy(() -> {
+      LottoNumber.valueOf(number);
     });
   }
 }
