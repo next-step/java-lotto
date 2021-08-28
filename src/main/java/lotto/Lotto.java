b97dfe7350ea74a36acc4ea1d61986b414122e21
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-29 01:37:05 +0900

    refactor: 매직 넘버 > 상수 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class Lotto {
 
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int SAVED_MIN_NUMBER = 1;
+    private static final int SAVED_MAX_NUMBER = 45;
+
+    private static final String LOTTO_NUMBERS_INVALID_SIZE_ERROR_MESSAGE = "로또 번호의 개수는 6개가 들어와야 한다.";
+    private static final String LOTTO_NUMBERS_INVALID_DUPLICATE_ERROR_MESSAGE = "로또의 번호는 중복되어 저장될 수 없다.";
+    private static final String LOTTO_NUMBERS_INVALID_RANGE_ERROR_MESSAGE = "로또 번호는 1이상 45이하의 수만 들어올 수 있다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,20 +23,20 @@ public class Lotto {
     }
 
     private static void checkLottoNumbersSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호의 개수는 6개가 들어와야 한다.");
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_INVALID_SIZE_ERROR_MESSAGE);
         }
     }
 
     private static void checkDuplicatedNumber(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
-            throw new IllegalArgumentException("로또의 번호는 중복되어 저장될 수 없다.");
+            throw new IllegalArgumentException(LOTTO_NUMBERS_INVALID_DUPLICATE_ERROR_MESSAGE);
         }
     }
 
     private static void checkNumberRange(List<Integer> numbers) {
-        if (numbers.stream().filter(num -> num >= 1 && num <= 45).count() != 6) {
-            throw new IllegalArgumentException("로또 번호는 1이상 45이하의 수만 들어올 수 있다.");
+        if (numbers.stream().filter(num -> num >= SAVED_MIN_NUMBER && num <= SAVED_MAX_NUMBER).count() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_INVALID_RANGE_ERROR_MESSAGE);
         }
     }
 
