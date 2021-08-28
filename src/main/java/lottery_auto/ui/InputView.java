724f68e78e5645210625a1c7da01ba5fddab1b87
abcms031 <mjkim031@gmail.com>
2021-08-28 22:43:37 +0900

    refactor : ui 불필요한 인스턴스 사용 수정 및 기능 수정 반영

--- src/main/java/lottery_auto/ui/InputView.java
+++ src/main/java/lottery_auto/ui/InputView.java
@@ -7,14 +7,9 @@ public class InputView {
     private static final String LOTTERY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final Scanner LOTTERY_AMOUNT = new Scanner(System.in);
 
-    private int amount;
-
-    public InputView() {
+    public static int inputAmount() {
         System.out.println(LOTTERY_MESSAGE);
-        this.amount = LOTTERY_AMOUNT.nextInt();
+        return LOTTERY_AMOUNT.nextInt();
     }
 
-    public int getAmount() {
-        return amount;
-    }
 }
--- src/main/java/lottery_auto/ui/ResultView.java
+++ src/main/java/lottery_auto/ui/ResultView.java
@@ -5,7 +5,6 @@ import lottery_auto.domain.WinningResult;
 import lottery_auto.type.ProfitRateMessage;
 import lottery_auto.type.WinningMatch;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -15,13 +14,14 @@ public class ResultView {
     private static final String LOTTERY_TICKET_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---------";
+    private static final Double COMPARISON_CRITERIA = Double.valueOf(1);
 
-    public void print(int ticket, Lotteries lotteries) {
+    public static void print(int ticket, Lotteries lotteries) {
         System.out.println(ticket+LOTTERY_TICKET_MESSAGE);
         System.out.println(lotteries.toString());
     }
 
-    public void print(List<Integer> winningCount) {
+    public static void print(List<Integer> winningCount) {
         System.out.println(WINNING_NUMBER_MESSAGE);
 
         String winningNumber = winningCount.stream()
@@ -32,38 +32,29 @@ public class ResultView {
         System.out.println(winningNumber);
     }
 
-    public void print(WinningResult winningResult) {
+    public static void print(WinningResult winningResult) {
         System.out.println(WINNING_STATISTICS_MESSAGE);
 
-        List<WinningMatch> winningMatches = Arrays.stream(WinningMatch.getValue())
-                .collect(Collectors.toList());
+        List<WinningMatch> winningMatches = WinningMatch.getMeaningValue();
 
         winningMatches.forEach(winningMatch -> {
-            int cnt = winningResult.compareCnt(winningMatch);
-            StringBuilder matchMsg = new StringBuilder(String.valueOf(winningMatch)).append("개 일치 (")
-                                    .append(winningMatch.getWinnings()).append("원)- ")
-                                    .append(cnt).append("개");
+            int cnt = winningResult.compareMatch(winningMatch);
+            StringBuilder matchMessage = new StringBuilder(String.valueOf(winningMatch))
+                                        .append("개 일치 (")
+                                        .append(winningMatch.getWinnings()).append("원)- ")
+                                        .append(cnt).append("개");
 
-            System.out.println(matchMsg);
+            System.out.println(matchMessage);
         });
     }
 
-    public void print(double rate){
-        System.out.println(getMessage(rate));
-    }
-
-    private String getMessage(double rate) {
-        rate = Math.floor(rate * 100) / 100.0;
-        StringBuilder rateMsg = new StringBuilder("총 수익률은 ").append(rate).append("입니다.");
+    public static void print(Double rate){
+        StringBuilder rateMsg = new StringBuilder("\n총 수익률은 ").append(rate).append("입니다.");
         System.out.print(rateMsg);
+        ProfitRateMessage profitRateMessage = ProfitRateMessage.equal(rate.compareTo(COMPARISON_CRITERIA));
 
-        if (rate < 1) {
-            return ProfitRateMessage.PROIFIT_LOSS.getMessage();
-        }
-        if (rate > 1) {
-            return ProfitRateMessage.PROIFIT_BENEFIT.getMessage();
-        }
-        return ProfitRateMessage.PROIFIT_BREAK_EVEN.getMessage();
+        System.out.println(profitRateMessage.getMessage());
     }
+
 
 }
