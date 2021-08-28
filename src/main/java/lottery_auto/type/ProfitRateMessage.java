94c132712f2131018bd3e2548b5d72f03d091bb3
abcms031 <mjkim031@gmail.com>
2021-08-28 22:39:50 +0900

    refactor: ProfitRateMessage에 구분할 변수 추가

--- src/main/java/lottery_auto/type/ProfitRateMessage.java
+++ src/main/java/lottery_auto/type/ProfitRateMessage.java
@@ -1,14 +1,25 @@
 package lottery_auto.type;
 
-public enum ProfitRateMessage {
-    PROIFIT_LOSS("(기준이 1이기 때문에 결과적으로 손해라는 의미임)"),
-    PROIFIT_BENEFIT("(기준이 1이기 때문에 결과적으로 이익이라는 의미임)"),
-    PROIFIT_BREAK_EVEN("(기준이 1이기 때문에 결과적으로 본전이라는 의미임)");
+import java.util.Arrays;
 
+public enum ProfitRateMessage {
+    LOSS(-1, "(기준이 1이기 때문에 결과적으로 손해라는 의미임)"),
+    BENEFIT(1, "(기준이 1이기 때문에 결과적으로 이익이라는 의미임)"),
+    BREAK_EVEN(0, "(기준이 1이기 때문에 결과적으로 본전이라는 의미임)");
+
+    private final int criteria;
     private final String message;
 
-    ProfitRateMessage(String message) {
+    ProfitRateMessage(int criteria, String message) {
+        this.criteria = criteria;
         this.message = message;
+    }
+
+    public static ProfitRateMessage equal(int criteria){
+        return Arrays.asList(ProfitRateMessage.values())
+                .stream()
+                .filter(profitRateMessage -> profitRateMessage.criteria == criteria)
+                .findAny().get();
     }
 
     public String getMessage() {
