92442602c8406bcae5c38e823b03e2a721ca0391
abcms031 <mjkim031@gmail.com>
2021-08-28 22:42:31 +0900

    feat : 수익률 메세지 테스트 코드 추가

---
+++ src/test/java/lottery_auto/ProfitRateMessageTest.java
@@ -0,0 +1,18 @@
+package lottery_auto;
+
+import lottery_auto.type.ProfitRateMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfitRateMessageTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,BENEFIT","0,BREAK_EVEN", "-1,LOSS"})
+    @DisplayName("수익률 평가 메세지 테스트")
+    void equal(int criteria, ProfitRateMessage message) {
+        assertThat(message.equal(criteria)).isEqualTo(message);
+    }
+}
