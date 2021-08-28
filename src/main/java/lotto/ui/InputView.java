603143e530a2e76771f185f057056b712c543aae
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-28 22:56:27 +0900

    refactor: 문자열 상수 추출

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
+    public static final String SPACE_REGEX = "\\s+";
+    public static final String COMMA_REGEX = ",";
+    public static final String EMPTY = "";
 
     public static int askUserToPayForLottoTickets() {
         System.out.println("구입금액을 입력해주세요.");
@@ -16,7 +19,7 @@ public class InputView {
     public static List<Integer> getWinningNumbersOfLastWeek() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
-        String[] userInputs = scanner.nextLine().replaceAll("\\s+", "").split(",");
+        String[] userInputs = scanner.nextLine().replaceAll(SPACE_REGEX, EMPTY).split(COMMA_REGEX);
         return Arrays.stream(userInputs).map(Integer::valueOf).collect(Collectors.toList());
     }
 }
