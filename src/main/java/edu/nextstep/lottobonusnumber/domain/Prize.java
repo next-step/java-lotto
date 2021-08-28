6099099bef9832934eebde5f3741b3fa035d7025
beadoer1 <beadoer1@gmail.com>
2021-08-28 13:47:34 +0900

    refactor(Prize) : step3. 미사용 변수 삭제 처리

--- src/main/java/edu/nextstep/lottobonusnumber/domain/Prize.java
+++ src/main/java/edu/nextstep/lottobonusnumber/domain/Prize.java
@@ -3,20 +3,19 @@ package edu.nextstep.lottobonusnumber.domain;
 import java.util.Arrays;
 
 public enum Prize {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    LOSE(0, false, 0),
-    ;
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    LOSE(0, 0);
 
     private static final int THIRD_PRIZE_COUNT_OF_MATCH = 5;
 
     private final int countOfMatch;
     private final long winningPrize;
 
-    Prize(int countOfMatch, boolean hasBonusNumber, int winningPrize) {
+    Prize(int countOfMatch, int winningPrize) {
         this.countOfMatch = countOfMatch;
         this.winningPrize = winningPrize;
     }
