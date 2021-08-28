666f411ad67a5d5e5cee9b3e200c04dbad203555
abcms031 <mjkim031@gmail.com>
2021-08-28 22:44:03 +0900

    feat : controller 수정

--- src/main/java/lottery_auto/controller/LotteryController.java
+++ src/main/java/lottery_auto/controller/LotteryController.java
@@ -3,36 +3,28 @@ package lottery_auto.controller;
 import lottery_auto.domain.Lotteries;
 import lottery_auto.domain.LotteryMachine;
 import lottery_auto.domain.WinningResult;
-import lottery_auto.strategy.LotteryStrategy;
-import lottery_auto.strategy.LotteryStrategyImpl;
-import lottery_auto.strategy.ProfitRateStrategy;
-import lottery_auto.strategy.ProfitRateStrategyImpl;
 import lottery_auto.ui.InputView;
 import lottery_auto.ui.ResultView;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class LotteryController {
     private static final LotteryMachine LOTTERY_MACHINE = LotteryMachine.getInstance();
-    private static final LotteryStrategy LOTTO_STRATEGY = new LotteryStrategyImpl();
-    private static final ProfitRateStrategy PROFIT_RATE_STRATEGY = new ProfitRateStrategyImpl();
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
-        int amount = inputView.getAmount();
+        BigDecimal amount = new BigDecimal(InputView.inputAmount());
 
-        int ticket = LOTTERY_MACHINE.purchase(amount);
-        Lotteries lotteries = LOTTERY_MACHINE.issue(ticket);
-        resultView.print(ticket, lotteries);
+        int ticketCount = LOTTERY_MACHINE.purchase(amount);
+        Lotteries lotteries = LOTTERY_MACHINE.issue(ticketCount);
+        ResultView.print(ticketCount, lotteries);
 
-        List<Integer> winningNumbers = LOTTO_STRATEGY.generate();
-        resultView.print(winningNumbers);
+        List<Integer> winningNumbers = LOTTERY_MACHINE.generateNumber();
+        ResultView.print(winningNumbers);
 
-        WinningResult winningResult = lotteries.count(winningNumbers);
-        resultView.print(winningResult);
+        WinningResult winningResult = lotteries.countMatchNumber(winningNumbers);
+        ResultView.print(winningResult);
 
-        double rate = (double) winningResult.sum() / amount;
-        resultView.print(PROFIT_RATE_STRATEGY.calculateRate(rate));
+        ResultView.print(winningResult.calculateProfit(amount));
     }
 }
