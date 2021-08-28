e39047c72b0eb2643a9e041e6ae59888c45d6a9f
abcms031 <mjkim031@gmail.com>
2021-08-28 22:40:52 +0900

    refactor :  싱글톤 패턴 단점 중 동기화 문제 보완 및 메소드 이름 수정

--- src/main/java/lottery_auto/domain/LotteryMachine.java
+++ src/main/java/lottery_auto/domain/LotteryMachine.java
@@ -3,41 +3,49 @@ package lottery_auto.domain;
 import lottery_auto.strategy.LotteryStrategy;
 import lottery_auto.strategy.LotteryStrategyImpl;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class LotteryMachine {
 
-    private static final int LOTTO_PRICE = 1000;
+    private static final BigDecimal LOTTO_PRICE = new BigDecimal(1000);
+    private static final int COMPARISON_CRITERIA = 0;
     private static final LotteryStrategy LOTTO_STRATEGY = new LotteryStrategyImpl();
 
     private static LotteryMachine lotteryMachine = null;
 
     private LotteryMachine(){}
 
-    public static synchronized LotteryMachine getInstance(){
-        if(lotteryMachine == null){
-            lotteryMachine = new LotteryMachine();
-        }
-        return lotteryMachine;
+    private static class LazyHolder {
+        public static final LotteryMachine INSTANCE = new LotteryMachine();
+    }
+
+    public static LotteryMachine getInstance(){
+        return LazyHolder.INSTANCE;
     }
 
     public Lotteries issue(final int tickets) {
-        return new Lotteries(Stream.generate(LOTTO_STRATEGY::generate)
+        return new Lotteries(Stream.generate(this::generateNumber)
                              .map(Lottery :: new)
                              .limit(tickets)
                              .collect(Collectors.toList()));
     }
 
-    public int purchase(final int amount) {
+    public List<Integer> generateNumber(){
+        return LOTTO_STRATEGY.generate();
+    }
+
+    public int purchase(final BigDecimal amount) {
         if(!validate(amount)){
             throw new IllegalArgumentException("1000원 이상의 금액을 입력해주세요");
         }
-        return amount / LOTTO_PRICE;
+        return amount.divide(LOTTO_PRICE).intValue();
     }
 
-    public boolean validate(final int amount){
-        return amount >= LOTTO_PRICE;
+    public boolean validate(final BigDecimal amount){
+        return amount.compareTo(LOTTO_PRICE) >= COMPARISON_CRITERIA;
     }
 
 }
