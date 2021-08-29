b7260dc7dec54d37895a103cc10d20de4940ac7b
조원진 <>
2021-08-29 11:34:19 +0900

    refactor(LottoNumbersFactory) : 수동 로또 티켓 생성 기능을 로또 티켓 클래스 안으로 이동

--- src/test/java/lotto/util/LottoNumbersFactoryTest.java
+++ src/test/java/lotto/util/LottoNumbersFactoryTest.java
@@ -1,13 +1,8 @@
 package lotto.util;
 
-import lotto.domain.LottoNumber;
 import lotto.domain.LottoNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,20 +15,4 @@ public class LottoNumbersFactoryTest {
         LottoNumbers lottoNumbers2 = LottoNumbersFactory.makeAutoLottoNumbers();
         assertThat(lottoNumbers1).isNotEqualTo(lottoNumbers2);
     }
-
-
-    @DisplayName("수동 생성")
-    @Test
-    void makeAutoLottoNumbers_ReturnLottoNumbers_IfInputStringHasSixNumbersAndCommas() {
-        String inputLottoNumbers = "1, 5, 7, 13, 27, 38";
-        LottoNumbers lottoNumbers = LottoNumbersFactory.makeLottoNumbers(inputLottoNumbers);
-
-        List<LottoNumber> expected = Arrays.stream(inputLottoNumbers.split(","))
-                .map(lottoNumber -> Integer.parseInt(lottoNumber.trim()))
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
-
-        assertThat(lottoNumbers).isEqualTo(new LottoNumbers(expected));
-    }
-
 }
