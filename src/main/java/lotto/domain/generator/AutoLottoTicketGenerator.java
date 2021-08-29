d917bd52a5d1a13bfc319c2c552452ccc91305aa
ChangSubKwak <paradise1732@gmail.com>
2021-08-29 18:21:22 +0900

    feat: 로또 자동 생성 로직을 AutoLottoTicketGenerator에 추가

--- src/main/java/lotto/domain/generator/AutoLottoTicketGenerator.java
+++ src/main/java/lotto/domain/generator/AutoLottoTicketGenerator.java
@@ -1,13 +1,34 @@
 package lotto.domain.generator;
 
+import lotto.domain.LottoBall;
 import lotto.domain.LottoTicket;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class AutoLottoTicketGenerator implements LottoTicketGenerator {
+    private final static int LOTTO_BALLS_MAX_NUM = 6;
+    private static final List<Integer> LOTTO_BALLS =
+        IntStream.rangeClosed(LottoBall.LOTTO_BALL_NUMBER_MIN, LottoBall.LOTTO_BALL_NUMBER_MAX)
+            .boxed()
+            .collect(Collectors.toList());
+
     private final LottoTicket lottoTicket;
 
     public AutoLottoTicketGenerator() {
-        lottoTicket = LottoTicket.createRandomNumber();
+        lottoTicket = createRandomNumber();
     }
+
+    private LottoTicket createRandomNumber() {
+        Collections.shuffle(LOTTO_BALLS);
+
+        return LottoTicket.of(LOTTO_BALLS.subList(0, LOTTO_BALLS_MAX_NUM).stream()
+            .mapToInt(Integer::intValue)
+            .toArray());
+    }
+
 
     public LottoTicket generate() {
         return lottoTicket;
