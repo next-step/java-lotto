66f04c7eab2f68a73f8798f433a9026cb3d02af3
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-29 13:32:00 +0900

    refactor: Payment의 필드변수명 payments->amount로 변경

--- src/main/java/lotto/game/Payments.java
+++ src/main/java/lotto/game/Payments.java
@@ -5,15 +5,15 @@ import java.util.Objects;
 public class Payments {
 
     public static final int ZERO = 0;
-    private final int payments;
+    private final int amount;
 
-    public Payments(int payments) {
-        isValid(payments);
-        this.payments = payments;
+    public Payments(int amount) {
+        isValid(amount);
+        this.amount = amount;
     }
 
-    private void isValid(int payments) {
-        if (payments <= ZERO) {
+    private void isValid(int amount) {
+        if (amount <= ZERO) {
             throw new IllegalArgumentException("구매 금액은 양수입니다.");
         }
     }
@@ -23,15 +23,15 @@ public class Payments {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Payments payments1 = (Payments) o;
-        return payments == payments1.payments;
+        return amount == payments1.amount;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(payments);
+        return Objects.hash(amount);
     }
 
     public int value() {
-        return this.payments;
+        return this.amount;
     }
 }
--- src/test/java/lotto/game/PaymentsTest.java
+++ src/test/java/lotto/game/PaymentsTest.java
@@ -20,7 +20,7 @@ class PaymentsTest {
     @DisplayName("양의 정수이다.")
     @ParameterizedTest(name = "인풋 {0}에 대해 예외 던짐")
     @ValueSource(ints = {-1000, 0})
-    void isPositiveInteger(int payments) {
-        assertThatThrownBy(() -> new Payments(payments)).isInstanceOf(IllegalArgumentException.class).hasMessageMatching("구매 금액은 양수입니다.");
+    void isPositiveInteger(int amount) {
+        assertThatThrownBy(() -> new Payments(amount)).isInstanceOf(IllegalArgumentException.class).hasMessageMatching("구매 금액은 양수입니다.");
     }
 }
