d2bed7fea647abeaf56914d343c00b501a97cdf2
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-29 15:48:09 +0900

    refactor: 수익률 테스트 코드 추가

--- src/test/java/lotto/gameresult/ProfitRateTest.java
+++ src/test/java/lotto/gameresult/ProfitRateTest.java
@@ -1,6 +1,18 @@
 package lotto.gameresult;
 
-public class ProfitRateTest {
+import lotto.game.Payments;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-    //TODO: profitRate 테스트 코드 작성하기 (GameResult에서 역할 분리 끝난 후 진행 계획)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("수익률은")
+class ProfitRateTest {
+
+    @ParameterizedTest(name = "상금 {0}원, 티켓 구매금 {1}원 일 때: {2}")
+    @CsvSource(value = {"2000:1000:2.00", "2000:2000:1.00", "2000:4000:0.50"}, delimiter = ':')
+    void create(int prizeMoney, int payments, double profitRate) {
+        assertThat(new ProfitRate(prizeMoney, new Payments(payments)).value()).isEqualTo(profitRate);
+    }
 }
