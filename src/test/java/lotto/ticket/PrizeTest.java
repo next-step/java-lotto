839f02296f1eb10e31cda1411f781fc52a403db5
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-29 18:59:10 +0900

    refactor: PrizeTest 내 테스트 코드 통합

--- src/test/java/lotto/ticket/PrizeTest.java
+++ src/test/java/lotto/ticket/PrizeTest.java
@@ -3,24 +3,16 @@ package lotto.ticket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("티켓의 당첨금은")
 class PrizeTest {
 
-    @DisplayName("티켓의 숫자가 당첨 숫자와")
-    @ParameterizedTest(name = "{0}개 일치할 때: {1}원")
-    @CsvSource(value = {"6:2000000000", "5:1500000", "4:50000", "3:5000", "0:0"}, delimiter = ':')
+    @DisplayName("보너스 숫자가 0개이고, 티켓의 숫자가 당첨 숫자와")
+    @ParameterizedTest(name = "{0}개 일치: {1}원")
+    @CsvSource(value = {"6:2000000000", "5:1500000", "4:50000", "3:5000", "2:0", "1:0", "0:0"}, delimiter = ':')
     void prizeIsDecidedByWinningNumberCounts(int winningNumberCounts, int prizeMoney) {
         assertThat(Prize.prize(winningNumberCounts).prizeMoney()).isEqualTo(prizeMoney);
-    }
-
-    @DisplayName("티켓 당첨 번호의 갯수가 1,2면 당첨금은 0원이다.")
-    @ParameterizedTest(name = "당첨번호 갯수 {0}개")
-    @ValueSource(ints = {1, 2})
-    void prizeThrowsException(int winningNumbersCount) {
-        assertThat(Prize.prize(winningNumbersCount).prizeMoney()).isZero();
     }
 }
