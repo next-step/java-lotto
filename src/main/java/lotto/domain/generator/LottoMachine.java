f0dbeb5c858dcaf4776788cf1209560192257b21
ChangSubKwak <paradise1732@gmail.com>
2021-08-29 18:20:19 +0900

    fix: 로또 자동 생성 기능의 반환값을 LottoTickets로 변경, 수동 번호데이터가 주어질 때, 한번에 LottoTickets생성하는 기능 추가

--- src/main/java/lotto/domain/generator/LottoMachine.java
+++ src/main/java/lotto/domain/generator/LottoMachine.java
@@ -1,6 +1,7 @@
 package lotto.domain.generator;
 
 import lotto.domain.LottoTicket;
+import lotto.domain.LottoTickets;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,13 +11,38 @@ import java.util.stream.IntStream;
 public class LottoMachine {
     List<LottoTicketGenerator> generators;
 
-    public List<LottoTicket> autoCreate(int autoLottoCount) {
+    public LottoTickets autoCreate(int autoLottoCount) {
         generators = new ArrayList<>();
         IntStream.range(0, autoLottoCount).forEach(i -> generators.add(new AutoLottoTicketGenerator()));
-        return generators.stream().map(LottoTicketGenerator::generate).collect(Collectors.toList());
+        return LottoTickets.from(
+            generators.stream()
+                .map(LottoTicketGenerator::generate)
+                .collect(Collectors.toList())
+        );
     }
 
     public LottoTicket manualCreate(int... lottoTicketNumbers) {
         return ManualLottoTicketGenerator.of(lottoTicketNumbers).generate();
+    }
+
+    public LottoTicket manualCreate(List<Integer> lottoTicketNumbers) {
+        return ManualLottoTicketGenerator.of(
+            lottoTicketNumbers.stream()
+                .mapToInt(Integer::intValue)
+                .toArray()
+        ).generate();
+    }
+
+    public LottoTickets oneTimeCreate(LottoTickets manualLottoTickets, int autoLottoCount) {
+        List<LottoTicket> tickets = manualLottoTickets.toList();
+
+        generators = new ArrayList<>();
+        IntStream.range(0, autoLottoCount).forEach(i -> generators.add(new AutoLottoTicketGenerator()));
+        tickets.addAll(LottoTickets.from(
+            generators.stream()
+                .map(LottoTicketGenerator::generate)
+                .collect(Collectors.toList())).toList());
+
+        return LottoTickets.from(tickets);
     }
 }
