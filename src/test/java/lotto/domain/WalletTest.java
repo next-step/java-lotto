0fb4c192b1113989ce5b04360b946f9325b1dd08
longbeom <wowow1859@naver.com>
2021-08-29 15:07:32 +0900

    test : 구매 개수에 해당하는 로또를 반환하는 테스트 추가

--- src/test/java/lotto/domain/WalletTest.java
+++ src/test/java/lotto/domain/WalletTest.java
@@ -7,12 +7,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class WalletTest {
-	@DisplayName(value = "구입 금액에 해당하는 로또 개수를 발급하는지 테스트")
+	@DisplayName(value = "구입 금액에 해당하는 로또 개수를 반환")
 	@CsvSource(value = {"14900,14", "10000,10", "5000,5", "0,0"})
 	@ParameterizedTest
-	void buyLottos(int amount, int expect) {
-		Lottos lottos = new Wallet().purchaseLottos(amount);
-		int numberOfLotto = lottos.getNumberOfLottos();
-		assertThat(numberOfLotto).isEqualTo(expect);
+	void countLottoToBuy(int money, int expect) {
+		int count = new Wallet(money).numberOfThingsToBuy();
+		assertThat(count).isEqualTo(expect);
+	}
+
+	@DisplayName(value = "구매 개수에 해당하는 로또를 반환")
+	@CsvSource(value = {"10000,5,5000", "7000,4,3000"})
+	@ParameterizedTest
+	void buyLotto(int money, int numberOfLotto, int change) {
+		Wallet wallet = new Wallet(money);
+		wallet.buyLotto(numberOfLotto);
+
+		assertThat(wallet).isEqualTo(new Wallet(change));
 	}
 }
