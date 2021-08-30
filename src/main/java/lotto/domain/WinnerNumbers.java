8efeb40acb70b2a51de62827853249d4582d00ab
ChangSubKwak <paradise1732@gmail.com>
2021-08-30 20:51:17 +0900

    refactor: 인자명을 보너스볼을 뜻하는 명칭으로 변경

--- src/main/java/lotto/domain/WinnerNumbers.java
+++ src/main/java/lotto/domain/WinnerNumbers.java
@@ -1,12 +1,21 @@
 package lotto.domain;
 
 public class WinnerNumbers {
+    private static final String BONUSBALL_INCLUSION_EXCEPTION_STATEMENT = "보너스 번호가 6개의 숫자에 포함되어 있습니다";
+
     private final LottoTicket winnerNumbers;
     private final LottoBall bonusBall;
 
-    private WinnerNumbers(LottoTicket winnerNumbers, LottoBall lottoBall) {
+    private WinnerNumbers(LottoTicket winnerNumbers, LottoBall bonusBall) {
+        validate(winnerNumbers, bonusBall);
         this.winnerNumbers = winnerNumbers;
-        this.bonusBall = lottoBall;
+        this.bonusBall = bonusBall;
+    }
+
+    private void validate(LottoTicket winnerNumbers, LottoBall bonusBall) {
+        if (winnerNumbers.contains(bonusBall)) {
+            throw new IllegalArgumentException(BONUSBALL_INCLUSION_EXCEPTION_STATEMENT);
+        }
     }
 
     public static WinnerNumbers from(LottoTicket winnerNumbers, LottoBall lottoBall) {
