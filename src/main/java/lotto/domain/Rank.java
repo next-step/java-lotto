dff65c77d46887ddf5176bfd0acd9cee225d26c5
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-01 03:21:50 +0900

    feat: 등수 반환 메소드 추가 및 보너스볼 테스트 상세 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -31,19 +31,30 @@ public enum Rank {
         return this.winCount;
     }
 
-    public static Rank calculateLottoRank(int winCount) {
-        return Arrays.stream(values())
-            .filter(rank -> rank.winCount == winCount)
-            .findFirst()
-            .orElse(Rank.LAST);
-    }
-
     public static Map<Rank, Integer> createRankMap() {
         Map<Rank, Integer> rankMap = new HashMap<>();
         for (Rank rank : values()) {
             rankMap.put(rank, DEFAULT_COUNT);
         }
         return rankMap;
+    }
+
+    public static Rank valueOf(int winCount, boolean matchBonus) {
+        if (winCount == SECOND.winCount && matchBonus) {
+            return SECOND;
+        }
+        if (winCount == THIRD.winCount && !matchBonus) {
+            return THIRD;
+        }
+
+        return calculateLottoRank(winCount);
+    }
+
+    private static Rank calculateLottoRank(int winCount) {
+        return Arrays.stream(values())
+            .filter(rank -> rank.winCount == winCount)
+            .findFirst()
+            .orElse(Rank.LAST);
     }
 
 }
