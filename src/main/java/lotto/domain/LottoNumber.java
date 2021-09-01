fe96d03b55d85b1c46854f3c71d098788a2eea98
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-02 04:07:07 +0900

    feat: LottoNumber 인스턴스 캐싱 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -10,9 +12,17 @@ public class LottoNumber implements Comparable<LottoNumber> {
     public static final int SAVED_MIN_NUMBER = 1;
     public static final int SAVED_MAX_NUMBER = 45;
 
+    private static final Map<Integer, LottoNumber> lottoNumbers = new HashMap<>();
+
     private static final String LOTTO_NUMBERS_INVALID_RANGE_ERROR_MESSAGE = "로또 번호는 1이상 45이하의 수만 들어올 수 있다.";
 
     private final int number;
+
+    static {
+        for (int i = SAVED_MIN_NUMBER; i <= SAVED_MAX_NUMBER; i++) {
+            lottoNumbers.put(i, new LottoNumber(i));
+        }
+    }
 
     public LottoNumber(int number) {
         checkNumberRange(number);
