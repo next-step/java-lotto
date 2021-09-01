d8364bd8c0cdd2cef95346a4d788d86b66a41ec3
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-02 03:22:20 +0900

    feat: InputView 수동 로또 수, 로또 번호 입력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -12,6 +14,20 @@ public class InputView {
     public static int inputPurchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static int inputPurchaseManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<String> inputPurchaseManualLottos(int count) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<String> lottos = new ArrayList<>();
+        for (int i=0; i<count; i++){
+            lottos.add(scanner.nextLine());
+        }
+        return lottos;
     }
 
     public static String inputWinLottoNumbers() {
