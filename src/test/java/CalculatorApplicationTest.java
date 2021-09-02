2543e0bbc48f7033bbac73a62e059eb3c73b2bab
Jade <j2de.dev@gmail.com>
2021-09-03 02:02:08 +0900

    test: delimiter

---
+++ src/test/java/CalculatorApplicationTest.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorApplicationTest {
+    @Test
+    void splitTest() {
+        String[] args = {"1,2:3"};
+        final String inputString = args[0];
+        String basicRegex = ",|:";
+
+        final String[] split = inputString.split(basicRegex);
+        assertThat(split.length).isEqualTo(3);
+    }
+
+    @Test
+    void matchTest() {
+        String[] args = {"//;\n"};
+        final String inputString = args[0];
+        final String customRegex = "//.\\n";
+        assertThat(Pattern.matches(customRegex, inputString)).isTrue();
+    }
+
+    @Test
+    void customRegexMatchTest() {
+        String[] args = {"//;\n1;2;3"};
+        final String inputString = args[0];
+        final String customRegex = "//(.)\\n(.*)";
+        assertThat(Pattern.matches(customRegex, inputString)).isTrue();
+    }
+
+    @Test
+    void getFirstString() {
+        String[] args = {"1,2:3"};
+        String first = CalculatorApplication.findFirstString(args);
+        final String actual = String.valueOf(args[0].charAt(0));
+        assertThat(actual).isEqualTo(first);
+    }
+
+    @Test
+    void isNumberTest() {
+        final String[] args = {"1,2:3"};
+        final String first = CalculatorApplication.findFirstString(args);
+        CalculatorApplication.isNumber(first);
+    }
+
+    @Test
+    void basicRegexSuccessTest() {
+        final String[] args = {"1,2:3"};
+        final String inputString = args[0];
+
+        final String basicRegex = ",|:";
+
+        final String[] stringOfNumber = inputString.split(basicRegex);
+        assertThat(stringOfNumber.length).isEqualTo(3);
+    }
+
+    // todo basicRegexFailTest exception
+
+    @Test
+    void customRegexSuccessTest() {
+        final String[] args = {"//;\n1;2;3"};
+        final String inputString = args[0];
+
+        final String customRegex = "//(.)\\n(.*)";
+        final Matcher matcher = Pattern.compile(customRegex).matcher(inputString);
+        String[] stringOfNumber = null;
+        if (matcher.find()) {
+            final String delimiter = matcher.group(1);
+            final String strings = matcher.group(2);
+            stringOfNumber = strings.split(delimiter);
+
+        }
+        assertThat(stringOfNumber.length).isEqualTo(3);
+    }
+
+    // todo customRegexFailTest exception
+}
