fba740cda19cd5602c28badea3130449fbd76df3
Jade <j2de.dev@gmail.com>
2021-09-03 06:17:08 +0900

    fix: 개행

--- src/main/java/CalculatorApplication.java
+++ src/main/java/CalculatorApplication.java
@@ -3,6 +3,6 @@ public class CalculatorApplication {
     public static void main(String[] args) {
         final String inputString = args[0];
         final Expression expression = Expression.createExpression(inputString);
-        System.out.printf("덧셈 결과 : %s", expression.add());
+        System.out.printf("덧셈 결과 : %s \n", expression.add());
     }
 }
