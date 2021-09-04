071914c0f93ebc8bfedc537f96bea284f9b07ec5
minyul <ggomjae@gmail.com>
2021-09-04 23:08:05 +0900

    feat : 결과를 보여주는 Result를 생성하다

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -5,6 +5,8 @@ public class LottoGame {
 	public static void main(String[] args) {
 
 		Input input = new Input();
+		Result result = new Result();
 		input.setAccount();
+		result.viewResult();
 	}
 }
---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Result {
+
+	public void viewResult() {
+		System.out.println("당첨 통계");
+		System.out.println("----------");
+		System.out.println("3개 일치 (5000원)-");
+		System.out.println("4개 일치 (50000원)-");
+		System.out.println("5개 일치 (1500000원)-");
+		System.out.println("6개 일치 (2000000000원)-");
+		System.out.println("총 수익률은 " + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+	}
+}
