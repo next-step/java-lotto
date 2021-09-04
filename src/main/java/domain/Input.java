aafc654dea55d000e7352320728e1b7a8f3be4e2
minyul <ggomjae@gmail.com>
2021-09-04 22:59:24 +0900

    feat : Input 클래스를 정의하다

---
+++ src/main/java/domain/Input.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.Scanner;
+
+public class Input {
+
+	private static Integer UNIT = 1000;
+
+	private Integer account;
+	private Scanner scanner;
+
+	public Input() {
+		this.scanner = new Scanner(System.in);
+	}
+
+	public void setAccount(Integer account) {
+		System.out.println("구입 금액을 입력해주세요.");
+		this.account = scanner.nextInt();
+		System.out.println(getLottoCount() + "개를 구매했습니다.");
+	}
+
+	private Integer getLottoCount() {
+		return this.account / UNIT;
+	}
+}
