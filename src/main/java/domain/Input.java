e111bf3a64cc44c5e759989eb0d49a6db9d805c0
minyul <ggomjae@gmail.com>
2021-09-04 23:02:14 +0900

    feat : LottoGame 클래스를 정의하다
    Input 클래스의 setAccount 메소드의 매개변수를 제거하다

--- src/main/java/domain/Input.java
+++ src/main/java/domain/Input.java
@@ -13,7 +13,7 @@ public class Input {
 		this.scanner = new Scanner(System.in);
 	}
 
-	public void setAccount(Integer account) {
+	public void setAccount() {
 		System.out.println("구입 금액을 입력해주세요.");
 		this.account = scanner.nextInt();
 		System.out.println(getLottoCount() + "개를 구매했습니다.");
---
+++ src/main/java/domain/LottoGame.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class LottoGame {
+
+	public static void main(String[] args) {
+
+		Input input = new Input();
+		input.setAccount();
+	}
+}
