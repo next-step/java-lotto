f1820aa36659e98110fd8a96cd3d6adc86233751
minyul <ggomjae@gmail.com>
2021-09-06 23:09:38 +0900

    refactor : Rank 값을 이용하여 Result의 상수값을 수정하다

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -5,13 +5,7 @@ import java.util.List;
 public class Result {
 
 	private static final Integer CONTAINER_SIZE = 7;
-	private static final Integer FIRST = 1;
-	private static final Integer SECOND = 2;
-	private static final Integer THIRD = 3;
-	private static final Integer FOURTH = 4;
-	private static final Integer FIFTH = 5;
-
-	private int[] resultContainer;
+	private final int[] resultContainer;
 
 	public Result() {
 		resultContainer = new int[CONTAINER_SIZE];
@@ -20,11 +14,11 @@ public class Result {
 	public void viewResult(List<Rank> container) {
 		firstComment();
 		calculateStatistics(container);
-		System.out.println("3개 일치 (5000원)-" + resultContainer[FIFTH]);
-		System.out.println("4개 일치 (50000원)-" + resultContainer[FOURTH]);
-		System.out.println("5개 일치 (1500000원)-" + resultContainer[THIRD]);
-		System.out.println("5개 일치, 보너스 볼 일치(30000000원)-" + resultContainer[SECOND]);
-		System.out.println("6개 일치 (2000000000원)-" + resultContainer[FIRST]);
+		System.out.println("3개 일치 (5000원)-" + resultContainer[Rank.FIFTH.getRanking()]);
+		System.out.println("4개 일치 (50000원)-" + resultContainer[Rank.FOURTH.getRanking()]);
+		System.out.println("5개 일치 (1500000원)-" + resultContainer[Rank.THIRD.getRanking()]);
+		System.out.println("5개 일치, 보너스 볼 일치(30000000원)-" + resultContainer[Rank.SECOND.getRanking()]);
+		System.out.println("6개 일치 (2000000000원)-" + resultContainer[Rank.FIRST.getRanking()]);
 
 		int sum = 0;
 		System.out.println("총 수익률은 " + sum + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
