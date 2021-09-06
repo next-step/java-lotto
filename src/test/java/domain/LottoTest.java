f314713fc8c823352e1f14dd4e4c4d543cfb6ba9
minyul <ggomjae@gmail.com>
2021-09-06 21:45:30 +0900

    test : 로또에 보너스 점수가 맞는지 확인하는 테스트를 추가하다

--- src/test/java/domain/LottoTest.java
+++ src/test/java/domain/LottoTest.java
@@ -34,4 +34,13 @@ public class LottoTest {
 		Integer matchNumber = Lotto.numberOfMatches(lotto,winningLotto);
 		assertThat(matchNumber).isEqualTo(6);
 	}
+
+	@DisplayName("로또에 보너스 점수가 맞는지 확인하는 테스트")
+	@Test
+	void test_lotto_bonus() {
+		Lotto lotto = new Lotto(Arrays.asList(new Number(1),new Number(2),new Number(3), new Number(4), new Number(5),
+						new Number(6)));
+		boolean bonusStatus = Lotto.bonusNumberOfMatches(lotto, new Number(3));
+		assertThat(bonusStatus).isEqualTo(true);
+	}
 }
