a5c83e804d9597799f0c6b219098adf06eccd3a2
jaehong0721 <newbie302@gmail.com>
2021-09-09 04:21:53 +0900

    feat: 2등 당첨 정보  출력

--- src/main/java/lotto/ResultView.java
+++ src/main/java/lotto/ResultView.java
@@ -15,16 +15,20 @@ public class ResultView {
         System.out.println("당첨 통계");
         System.out.println("----------");
         for (LottoRank rank : LottoRank.ranks()) {
-            System.out.println(
-                    rank.getMatchCount() + "개 일치" +
-                    " (" + rank.getMatchReward() + "원)" +
-                    "- " + lottoResult.winningCount(rank) + "개"
-            );
+            System.out.println(infoString(lottoResult, rank));
         }
         double profitRate = lottoResult.profitRate();
         System.out.println("총 수익률은 " + profitRate + "입니다.");
         if (profitRate < 1) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
+    }
+
+    private static String infoString(LottoResult lottoResult, LottoRank rank) {
+        String countString = rank.getMatchCount() + "개 일치";
+        if (rank == LottoRank.SECOND) countString += ", 보너스 볼 일치";
+        String rewordString = " (" + rank.getMatchReward() + "원)";
+        String winningCountString = "- " + lottoResult.winningCount(rank) + "개";
+        return countString + rewordString + winningCountString;
     }
 }
