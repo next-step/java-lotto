26baba9c41fd119ea6454be244a2f575944319bb
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 08:55:55 +0900

    test: 로또 티켓 테스트 6개의 숫자로 구성

--- src/test/java/step5/domain/LottoTicketTest.java
+++ src/test/java/step5/domain/LottoTicketTest.java
@@ -1,4 +1,35 @@
+package step5.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step3.domain.Lotto;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottoTicketTest {
-  
+    @DisplayName("로또 번호 개수 확인 : 7개의 로또 번호 (에러)")
+    @Test
+    void sevenLotto() {
+        String lottoNumbers = "1, 2, 3, 4, 5, 6, 7";
+        Assertions.assertThatThrownBy(() -> {
+            new LottoTicket(lottoNumbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 개수 확인 : 5개의 로또 번호 (에러)")
+    @Test
+    void fiveLotto() {
+        String lottoNumbers = "1, 2, 3, 4, 5";
+        Assertions.assertThatThrownBy(() -> {
+            new LottoTicket(lottoNumbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 개수 확인 : 7개의 로또 번호 (정상)")
+    @Test
+    void sixLotto() {
+        String lottoNumbers = "1, 2, 3, 4, 5, 6";
+        LottoTicket lottoTicket = new LottoTicket(lottoNumbers);
+    }
 }
