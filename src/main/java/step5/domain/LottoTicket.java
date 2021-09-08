179a4085544ef1517c6e5d855d4374603e6c23f2
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 08:56:20 +0900

    feat: LottoTicket 클래스 생성 하나의 로또 티켓을 의미, 6개의 숫자로 구성

--- src/main/java/step5/domain/LottoTicket.java
+++ src/main/java/step5/domain/LottoTicket.java
@@ -1,2 +1,30 @@
-package step5.domain;public class LottoTicket {
+package step5.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTicket {
+    private static final String LOTTO_NUMBER_ㄷERROR_MESSAGE = "로또 번호는 6개로 구성되어야 합니다.";
+    private static final String DELIMITER = ", ";
+    private final List<LottoNumber> lottoTicket;
+
+    public LottoTicket(String stringlottoNumber) {
+        List<String> lottoNumbers = Arrays.asList(stringlottoNumber.split(DELIMITER));
+        checkValidLottoNumberFormat(stringlottoNumber);
+
+        lottoTicket = lottoNumbers.stream()
+                .map(lottoNumber -> new LottoNumber(lottoNumber))
+                .collect(Collectors.toList());
+    }
+
+    private void checkValidLottoNumberFormat(String stringlottoNumber) {
+        List<String> lottoNumbers = Arrays.asList(stringlottoNumber.split(DELIMITER));
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_ㄷERROR_MESSAGE);
+        }
+    }
+
+
 }
