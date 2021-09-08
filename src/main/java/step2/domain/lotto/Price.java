abf2a3783027e14113768c469bee76a4d1d7c214
Jade <j2de.dev@gmail.com>
2021-09-09 01:19:24 +0900

    feat: price 상수 추가

--- src/main/java/step2/domain/lotto/Price.java
+++ src/main/java/step2/domain/lotto/Price.java
@@ -4,15 +4,17 @@ import step2.domain.statistics.Amount;
 
 public class Price {
     private static final int LOTTO_PRICE = 1000;
+    private static final int ZERO_PRICE = 0;
 
     private final int price;
 
     public Price() {
-        this(0);
+        this(ZERO_PRICE);
     }
 
     public Price(int price) {
-        if (price <= 0) {
+
+        if (price <= ZERO_PRICE) {
             throw new RuntimeException("구매 금액은 0 이하일 수 없습니다.");
         }
         this.price = price;
