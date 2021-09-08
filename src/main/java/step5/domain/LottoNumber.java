314dd0ceaaac8eb9d8931b8db1934c55ad9df8f4
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 08:55:33 +0900

    feat: LottoNumber 클래스 생성

--- src/main/java/step5/domain/LottoNumber.java
+++ src/main/java/step5/domain/LottoNumber.java
@@ -1,2 +1,24 @@
-package step5.domain;public class LottoNumber {
+package step5.domain;
+
+public class LottoNumber {
+    private static final Integer MIN = 1;
+    private static final Integer MAX = 45;
+    private final Integer lottoNumber;
+
+    public LottoNumber(String lottoNumber) {
+        this(Integer.parseInt(lottoNumber));
+    }
+
+    public LottoNumber(Integer lottoNumber) {
+        checkValidRange(lottoNumber);
+        this.lottoNumber = lottoNumber;
+    }
+
+    private void checkValidRange(Integer lottoNumber) {
+        if (lottoNumber < MIN || lottoNumber > MAX) {
+            throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이 값이어야 합니다.");
+        }
+    }
+
+
 }
