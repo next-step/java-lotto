744e202dcc23b106b9c71314b98692c351c5a6a0
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 09:03:32 +0900

    feat: LottoTickets 클래스 생성 LottoTicket 묶음

---
+++ src/main/java/step5/domain/LottoTickets.java
@@ -0,0 +1,20 @@
+package step5.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTickets {
+    private List<LottoTicket> lottoTickets;
+
+    public LottoTickets() {
+        lottoTickets = new ArrayList<>();
+    }
+
+    public void buyLottoAutomatically() {
+
+    }
+
+    public void buyLottoManually() {
+
+    }
+}
