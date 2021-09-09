8e7bd291c794d8fc0229e0be1526c989e5f00437
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 10:03:01 +0900

    feat: 로또 번호 자동 발행

--- src/main/java/step5/domain/AutoLottoTicket.java
+++ src/main/java/step5/domain/AutoLottoTicket.java
@@ -7,18 +7,22 @@ import java.util.stream.Collectors;
 public class AutoLottoTicket implements LottoTicket {
     private static final String LOTTO_NUMBER_ㄷERROR_MESSAGE = "로또 번호는 6개로 구성되어야 합니다.";
     private static final String DELIMITER = ", ";
-//    private final List<LottoNumber> lottoTicket;
+    private static final LottoMachine lottoMachine = new LottoMachine();
+    private final List<LottoNumber> lottoTicket;
 
     public AutoLottoTicket() {
+        List<Integer> autoLottoNumbers = lottoMachine.issue();
+        checkValidLottoNumberFormat(autoLottoNumbers);
 
+        lottoTicket = autoLottoNumbers
+                .stream()
+                .map(autoLottoNumber -> new LottoNumber(autoLottoNumber))
+                .collect(Collectors.toList());
     }
 
-    public void checkValidLottoNumberFormat(String stringlottoNumber) {
-        List<String> lottoNumbers = Arrays.asList(stringlottoNumber.split(DELIMITER));
-        if (lottoNumbers.size() != 6) {
+    public void checkValidLottoNumberFormat(List<Integer> autoLottoNumbers) {
+        if (autoLottoNumbers.size() != 6) {
             throw new IllegalArgumentException(LOTTO_NUMBER_ㄷERROR_MESSAGE);
         }
     }
-
-
 }
