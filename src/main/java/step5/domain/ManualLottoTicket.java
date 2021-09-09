38d72ad644b04eb25e942a93e62854b9b8d57a73
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 10:45:21 +0900

    refactor: 세부사항 조절

--- src/main/java/step5/domain/ManualLottoTicket.java
+++ src/main/java/step5/domain/ManualLottoTicket.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ManualLottoTicket implements LottoTicket {
-    private static final String LOTTO_NUMBER_ㄷERROR_MESSAGE = "로또 번호는 6개로 구성되어야 합니다.";
+    private static final String LOTTO_NUMBER_ERROR_MESSAGE = "로또 번호는 6개로 구성되어야 합니다.";
     private static final String DELIMITER = ", ";
     private final List<LottoNumber> lottoTicket;
 
@@ -21,7 +21,7 @@ public class ManualLottoTicket implements LottoTicket {
     private void checkValidLottoNumberFormat(String stringlottoNumber) {
         List<String> lottoNumbers = Arrays.asList(stringlottoNumber.split(DELIMITER));
         if (lottoNumbers.size() != 6) {
-            throw new IllegalArgumentException(LOTTO_NUMBER_ㄷERROR_MESSAGE);
+            throw new IllegalArgumentException(LOTTO_NUMBER_ERROR_MESSAGE);
         }
     }
 }
--- src/main/java/step5/view/InputView.java
+++ src/main/java/step5/view/InputView.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String NUMBER_OF_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String NUMBER_OF_MANUAL_LOTTO = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String MANUAL_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
     private static final Scanner SC = new Scanner(System.in);
 
     public static Integer requirePurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
-        return SC.nextInt();
+        return Integer.parseInt(SC.nextLine());
     }
 
     public static Integer requireNumberOfManualLotto() {
         System.out.println(NUMBER_OF_MANUAL_LOTTO);
-        return SC.nextInt();
+        return Integer.parseInt(SC.nextLine());
     }
 
     public static String requireManualLottoNumber() {
