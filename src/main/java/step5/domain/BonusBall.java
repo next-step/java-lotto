d11ae3aace6aa8e3e2f5d6e323fbd1bfcc63d72a
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 11:45:36 +0900

    feat: Bonusball 클래스 생성

---
+++ src/main/java/step5/domain/BonusBall.java
@@ -0,0 +1,11 @@
+package step5.domain;
+
+public class BonusBall extends LottoNumber {
+    public BonusBall(String lottoNumber) {
+        super(lottoNumber);
+    }
+
+    public BonusBall(Integer lottoNumber) {
+        super(lottoNumber);
+    }
+}
---
+++ src/main/java/step5/domain/WinningLottoTicket.java
@@ -0,0 +1,31 @@
+package step5.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ManualLottoTicket implements LottoTicket {
+    private static final String LOTTO_NUMBER_ERROR_MESSAGE = "로또 번호는 6개로 구성되어야 합니다.";
+    private static final String DELIMITER = ", ";
+    private final List<LottoNumber> lottoTicket;
+
+    public ManualLottoTicket(String stringlottoNumber) {
+        List<String> lottoNumbers = Arrays.asList(stringlottoNumber.split(DELIMITER));
+        checkValidLottoNumberFormat(stringlottoNumber);
+
+        lottoTicket = lottoNumbers.stream()
+                .map(lottoNumber -> new LottoNumber(lottoNumber))
+                .collect(Collectors.toList());
+    }
+
+    private void checkValidLottoNumberFormat(String stringlottoNumber) {
+        List<String> lottoNumbers = Arrays.asList(stringlottoNumber.split(DELIMITER));
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public List<LottoNumber> ticket() {
+        return lottoTicket;
+    }
+}
