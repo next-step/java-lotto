60c0b9e2199123f701e1ea127b50899b6e4d172b
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 09:54:24 +0900

    feat: LottoMachine 클래스 생성 자동 로또 발행

--- src/main/java/step5/domain/LottoMachine.java
+++ src/main/java/step5/domain/LottoMachine.java
@@ -1,2 +1,26 @@
-package step5.domain;public class LottoMachine {
+package step5.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class LottoMachine {
+    public static final Random RD = new Random();
+    private static final Integer NUMBER_OF_LOTTO_NUMBERS = 6;
+    private static final Integer MIN_OF_LOTTO_NUMBER = 1;
+    private static final Integer MAX_OF_LOTTO_NUMBER = 45;
+
+    private List<Integer> fullLottoNumber = new ArrayList<>();
+
+    public LottoMachine() {
+        for (int i = MIN_OF_LOTTO_NUMBER; i <= MAX_OF_LOTTO_NUMBER; i++) {
+            fullLottoNumber.add(i);
+        }
+    }
+
+    public List<Integer> issue() {
+        Collections.shuffle(fullLottoNumber);
+        return fullLottoNumber.subList(0, 6);
+    }
 }
