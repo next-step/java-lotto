9e72f0efd0159f1447f47844c8619c4de475d573
minyul <ggomjae@gmail.com>
2021-09-10 11:44:16 +0900

    refactor : 결과값을 구할 때, HashMap을 사용하다

--- src/main/java/util/Result.java
+++ src/main/java/util/Result.java
@@ -3,31 +3,24 @@ package util;
 import domain.Lotto;
 import domain.Rank;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Result {
 
-	private static final Integer CONTAINER_SIZE = 7;
 	private static final Integer INIT_NUMBER = 0;
-	private static final Integer FIRST = 1;
-	private static final Integer SECOND = 2;
-	private static final Integer THIRD = 3;
-	private static final Integer FOURTH = 4;
-	private static final Integer FIFTH = 5;
-	private final int[] resultContainer;
-
-	public Result() {
-		resultContainer = new int[CONTAINER_SIZE];
-	}
+	private static final Integer RANK_COUNT_PLUS_UNIT = 1;
+	private static final Map<Rank, Integer> rankMap = new HashMap<>();
 
 	public void viewResult(List<Rank> container) {
 		firstComment();
 		calculateStatistics(container);
-		System.out.println("3개 일치 (5000원)-" + resultContainer[FIFTH]);
-		System.out.println("4개 일치 (50000원)-" + resultContainer[FOURTH]);
-		System.out.println("5개 일치 (1500000원)-" + resultContainer[THIRD]);
-		System.out.println("5개 일치, 보너스 볼 일치(30000000원)-" + resultContainer[SECOND]);
-		System.out.println("6개 일치 (2000000000원)-" + resultContainer[FIRST]);
+		System.out.println("3개 일치 (5000원)-" + (rankMap.get(Rank.FIFTH) == null ? INIT_NUMBER : rankMap.get(Rank.FIFTH)));
+		System.out.println("4개 일치 (50000원)-" + (rankMap.get(Rank.FOURTH) == null ? INIT_NUMBER : rankMap.get(Rank.FOURTH)));
+		System.out.println("5개 일치 (1500000원)-" + (rankMap.get(Rank.THIRD) == null ? INIT_NUMBER : rankMap.get(Rank.THIRD)));
+		System.out.println("5개 일치, 보너스 볼 일치(30000000원)-" + (rankMap.get(Rank.SECOND) == null ? INIT_NUMBER : rankMap.get(Rank.SECOND)));
+		System.out.println("6개 일치 (2000000000원)-" + (rankMap.get(Rank.FIRST) == null ? INIT_NUMBER : rankMap.get(Rank.FIRST)));
 
 		int sum = INIT_NUMBER;
 		System.out.println("총 수익률은 " + sum + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
@@ -40,21 +33,7 @@ public class Result {
 	}
 
 	private void validateRank(Rank rank) {
-		if (Rank.FIRST == rank) {
-			resultContainer[FIRST]++;
-		}
-		if (Rank.FIRST == rank) {
-			resultContainer[SECOND]++;
-		}
-		if (Rank.FIRST == rank) {
-			resultContainer[THIRD]++;
-		}
-		if (Rank.FIRST == rank) {
-			resultContainer[FOURTH]++;
-		}
-		if (Rank.FIRST == rank) {
-			resultContainer[FIFTH]++;
-		}
+		rankMap.merge(rank, RANK_COUNT_PLUS_UNIT , Integer::sum);
 	}
 
 	public void viewLotto(Lotto lotto) {
