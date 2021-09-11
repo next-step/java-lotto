7f5b4955fdbea88dcd575077e5f35605cce613c2
minyul <ggomjae@gmail.com>
2021-09-11 10:44:29 +0900

    test : 수동 로또에 따른 자동 로또 개수가 몇개 인지 테스트하는 부분을 구현하다

--- src/test/java/service/LottoCalculatorTest.java
+++ src/test/java/service/LottoCalculatorTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -37,5 +38,17 @@ public class LottoCalculatorTest {
 		List<Rank> result = calculator.getResult(lottos, winningLotto, bonusNumber);
 		assertThat(result.get(0)).isEqualTo(Rank.THIRD);
 		assertThat(result.get(1)).isEqualTo(Rank.SECOND);
+	}
+
+	@DisplayName("자동 로또 개수를 제대로 반환하는지 확인하는 테스트")
+	@Test
+	void test_auto_size() {
+		Lotto firstLotto = new Lotto(Arrays.asList(new Number(1),new Number(2),new Number(3), new Number(4), new Number(5),
+						new Number(8)));
+		Lotto secondLotto = new Lotto(Arrays.asList(new Number(1),new Number(2),new Number(3), new Number(4), new Number(5),
+						new Number(7)));
+		List<Lotto> manualLottos = new ArrayList<>(Arrays.asList(firstLotto, secondLotto));
+		Integer autoCount = calculator.getAutoLottoCount(new Account(12_500), manualLottos);
+		assertThat(autoCount).isEqualTo(10);
 	}
 }
