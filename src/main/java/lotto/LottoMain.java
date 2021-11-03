3de033fa1bfdd641473ed8c68df12ce75442193f
minhee.son <minhee4735@gmail.com>
2021-11-03 10:04:15 +0900

    feat 로또 구입금액 입력받기 완료

---
+++ src/main/java/lotto/LottoMain.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import lotto.views.LottoInputView;
+
+public class LottoMain {
+
+
+    String money = LottoInputView.takeMoney();
+
+
+
+}
---
+++ src/main/java/lotto/views/LottoInputView.java
@@ -0,0 +1,23 @@
+package lotto.views;
+
+import java.util.Scanner;
+
+public class LottoInputView {
+
+    private static final String QUESTION_PURCHASE_MONEY = "구입금액을 입력해 주세요.";
+
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static String takeMoney() {
+        askQuestion(QUESTION_PURCHASE_MONEY);
+        return getInput();
+    }
+
+    private static void askQuestion(String question) {
+        System.out.println(question);
+    }
+
+    private static String getInput() {
+        return scanner.nextLine();
+    }
+}
