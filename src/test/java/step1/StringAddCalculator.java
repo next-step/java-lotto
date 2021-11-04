d4ea3c61a4391843e2a75dbf9a54621f8f82db58
marty.ni <marty.ni@kakaocorp.com>
2021-11-05 00:28:15 +0900

    Feat : 기본 구분자 , : 기능 추가

--- src/test/java/step1/StringAddCalculator.java
+++ src/test/java/step1/StringAddCalculator.java
@@ -1,11 +1,18 @@
 package step1;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public int splitAndSum(String input) {
         if(isNullOrEmpty(input)) {
             return 0;
         }
-        return 0;
+
+        String[] split = input.split(":|,");
+
+        return Arrays.stream(split)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
     private boolean isNullOrEmpty(String input) {
--- src/test/java/step1/StringAddCalculatorTest.java
+++ src/test/java/step1/StringAddCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -25,5 +26,38 @@ public class StringAddCalculatorTest {
     void addTest(String input) {
         int result = stringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(0);
+    }
+
+
+    @DisplayName("구분자 ':' 더하기 코드는 성공한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:2|3", "1:2:3|6"}, delimiter = '|')
+    void addByColonTest(String input, int expect) {
+        int result = stringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @DisplayName("구분자 ',' 더하기 코드는 성공한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2|3", "1,2,3|6"}, delimiter = '|')
+    void addByCommaTest(String input, int expect) {
+        int result = stringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @DisplayName("구분자 ',' or ':' 더하기 코드는 성공한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2|3", "1,2:3|6", "1:2,4,5:6|18"}, delimiter = '|')
+    void addByCommaOrColonTest(String input, int expect) {
+        int result = stringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @DisplayName("숫자 하나만 있을 경우 그 숫자를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1|1", "1|1"}, delimiter = '|')
+    void oneNumberTest(String input, int expect) {
+        int result = stringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expect);
     }
 }
