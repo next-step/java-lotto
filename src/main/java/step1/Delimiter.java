d6563fc8cf5484a8c81abecb063c4045d412c040
marty.ni <marty.ni@kakaocorp.com>
2021-11-05 13:32:43 +0900

    Refactor : Delimiter 에서 pattern split 책임 제거

--- src/main/java/step1/Delimiter.java
+++ src/main/java/step1/Delimiter.java
@@ -1,47 +1,40 @@
 package step1;
 
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Delimiter {
 
     private static final String DEFAULT_DELIMITER = ":|,";
     private static final String ADD_FORMAT = "%s|%s";
+    private static final String EMPTY_SYMBOL = "";
 
-    private final String delimiter;
+    private final String symbol;
 
-    private Delimiter(String delimiter) {
-        this.delimiter = delimiter;
+    private Delimiter(String symbol) {
+        this.symbol = symbol;
     }
 
     public static Delimiter withDefaultDelimiter() {
         return new Delimiter(DEFAULT_DELIMITER);
     }
 
-    public static Delimiter custom(String delimiter) {
-        return new Delimiter(delimiter);
-    }
-
-    public static Delimiter customByPattern(String input, Pattern custom_pattern) {
-        String delimiter = "";
-        Matcher matcher = custom_pattern.matcher(input);
-        if(matcher.find()) {
-            delimiter = matcher.group(1);
+    public static Delimiter custom(String symbol) {
+        if(symbol == null) {
+            symbol = EMPTY_SYMBOL;
         }
-        return new Delimiter(delimiter);
-    }
-
-    public Delimiter addDelimiter(Delimiter target) {
-        return new Delimiter(String.format(ADD_FORMAT, this.delimiter, target.delimiter));
-    }
-
-    public String getDelimiter() {
-        return this.delimiter;
+        return new Delimiter(symbol);
     }
 
     public boolean isExist() {
-        return this.delimiter != null || !this.delimiter.isEmpty();
+        return this.symbol != null && this.symbol.isEmpty() != true;
+    }
+
+    public Delimiter addDelimiter(Delimiter other) {
+        return new Delimiter(String.format(ADD_FORMAT, this.symbol, other.getSymbol()));
+    }
+
+    public String getSymbol() {
+        return this.symbol;
     }
 
     @Override
@@ -49,11 +42,19 @@ public class Delimiter {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Delimiter delimiter1 = (Delimiter) o;
-        return Objects.equals(delimiter, delimiter1.delimiter);
+        return Objects.equals(symbol, delimiter1.symbol);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(delimiter);
+        return Objects.hash(symbol);
     }
+
+    @Override
+    public String toString() {
+        return "Delimiter{" +
+                "delimiter='" + symbol + '\'' +
+                '}';
+    }
+
 }
--- src/test/java/step1/DelimiterTest.java
+++ src/test/java/step1/DelimiterTest.java
@@ -1,9 +1,10 @@
 package step1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.regex.Pattern;
@@ -12,31 +13,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DelimiterTest {
 
+    private Pattern pattern;
+
+    @BeforeEach
+    void setUp() {
+        String PATTERN_STRING = "//(.)\n(.*)";
+        pattern = Pattern.compile(PATTERN_STRING);
+    }
+
     @DisplayName("isExist() 구분자의 값이 있으면 true를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"//;\n1:3", "//;\n1:2"})
+    @ValueSource(strings = {";", "+"})
     void isExistTrueTest(String input) {
-        String pattern_string = "//(.)\n(.*)";
-        Pattern pattern = Pattern.compile(pattern_string);
-        Delimiter delimiter = Delimiter.customByPattern(input,pattern);
+        Delimiter delimiter = Delimiter.custom(input);
         assertThat(delimiter.isExist()).isTrue();
     }
 
-    @DisplayName("isExist() 커스텀 구분자가 없으면 false를 반환한다.")
+    @DisplayName("isExist() 구분자가 없으면 false를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1:3", "1:2"})
+    @NullAndEmptySource
     void isExistFalseTest(String input) {
-        String pattern_string = "//(.)\n(.*)";
-        Pattern pattern = Pattern.compile(pattern_string);
-        Delimiter delimiter = Delimiter.customByPattern(input, pattern);
+        Delimiter delimiter = Delimiter.custom(input);
         assertThat(delimiter.isExist()).isFalse();
     }
 
     @DisplayName("withDefaultDelimiter() 기본 구분자를 반환한다.")
     @Test
     void withDefaultDelimiterTest() {
-        Delimiter delimiter = Delimiter.withDefaultDelimiter();
-        assertThat(delimiter).isEqualTo(Delimiter.custom(":|,"));
+        Delimiter basicDelimiter = Delimiter.withDefaultDelimiter();
+        assertThat(basicDelimiter).isEqualTo(Delimiter.custom(":|,"));
     }
 
     @DisplayName("addDelimiter()는 delimiter를 합쳐서 반환한다.")
@@ -49,14 +54,5 @@ public class DelimiterTest {
 
         assertThat(result).isEqualTo(Delimiter.custom(":|,|;"));
     }
-
-//    @DisplayName("addDelimiter() 커스텀 구분자가 있으면 입력값과 함께 커스텀 구분자를 반환한다.")
-//    @ParameterizedTest
-//    @ValueSource(strings = {"//;\n1:", "//;\n1:2"})
-//    void getDelimiterTest(String input) {
-//        Delimiter delimiter = Delimiter.create(input);
-//        assertThat(delimiter.delimiter()).isTrue();
-//    }
-
 
 }
