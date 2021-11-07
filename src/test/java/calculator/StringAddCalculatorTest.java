8ff08b1914c9f5ac3445e76e6d263c0d25850210
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-08 02:43:58 +0900

    [test] 빈 문자열 또는 null 값을 입력하는 경우 0을 반환 하는 테스트 코드 추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -2,13 +2,20 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StringAddCalculatorTest {
 
-    @Test
-    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)")
-    void splitAndSum_nullOrEmpty() {
 
+    @ParameterizedTest(name = "[{index}] {0}의 결과는 0 이다.")
+    @NullAndEmptySource
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    void splitAndSum_nullOrEmpty(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(0);
     }
 
     @Test
