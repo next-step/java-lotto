c57f5d08b07e5078f2b5fa7762dfd68a2e955f8e
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-08 02:48:36 +0900

    [feature] 빈 문자열 또는 null을 입력하는 실패 기능 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,13 @@
 package calculator;
 
 public class StringAddCalculator {
+    private static final int ZERO = 0;
+
     public static int splitAndSum(String input) {
-        return 0;
+        if (input == null || input.length() == 0) {
+            return ZERO;
+        }
+
+        return -1;
     }
 }
