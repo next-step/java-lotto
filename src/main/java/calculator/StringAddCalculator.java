94f02fcd636ab553b35b905732d3ac8a8e3c1684
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-08 04:24:23 +0900

    [feature] 커스텀 구분자 지정할 수 있는 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,10 +3,13 @@ package calculator;
 import utils.StringUtils;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     private static final int ZERO = 0;
     private static final String COMMA_AND_COLON = ",|:";
+    private static final Pattern REGEX_CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String input) {
         if (StringUtils.isEmpty(input)) {
@@ -15,6 +18,13 @@ public class StringAddCalculator {
 
         if (StringUtils.isNumeric(input)) {
             return Integer.parseInt(input);
+        }
+
+        Matcher matcher = REGEX_CUSTOM_DELIMITER.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] tokens = matcher.group(2).split(customDelimiter);
+            return sum(tokens);
         }
 
         return sum(input.split(COMMA_AND_COLON));
