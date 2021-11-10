646e76c532192660ee5eac6908a74bd3ee78ee9d
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-10 18:35:15 +0900

    [feedback] static 한 메서드 제거

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -13,8 +13,6 @@ import lotto.ui.ResultView;
 
 import java.util.List;
 
-import static lotto.rule.LottoRule.MINIMUM_PRICE;
-
 public class LottoApplication {
     private static InputView inputView = new InputView();
     private static LottoService lottoService = new LottoService(new LottoTicketRandomMaker(), new LottoResultMaker());
@@ -22,33 +20,14 @@ public class LottoApplication {
     private static ResultView resultView = new ResultView();
 
     public static void main(String[] args) {
-        LottoTickets purchaseLottoTickets = purchaseLottoTickets();
-
-        WinningLottoNumber winningLottoNumber = getWinningLottoNumber();
-
-        checkLottoResult(purchaseLottoTickets, winningLottoNumber);
-    }
-
-    private static LottoTickets purchaseLottoTickets() {
         LottoPrice purchasePrice = inputView.inputPurchasePrice();
         LottoTickets purchaseLottoTickets = lottoController.purchaseLottoTickets(purchasePrice);
-
         resultView.printPurchaseLottoTickets(purchaseLottoTickets);
-        return purchaseLottoTickets;
-    }
 
-    private static WinningLottoNumber getWinningLottoNumber() {
         List<Integer> winningLottoNumbers = inputView.inputWinningLottoNumberOfLastWeeks();
+        WinningLottoNumber winningLottoNumber = lottoController.getWinningLottoNumber(winningLottoNumbers);
 
-        return lottoController.getWinningLottoNumber(winningLottoNumbers);
-    }
-
-    private static void checkLottoResult(LottoTickets purchaseLottoTickets, WinningLottoNumber winningLottoNumber) {
         LottoResult lottoResult = lottoController.checkLottoResult(purchaseLottoTickets, winningLottoNumber);
-        resultView.printLottoResult(lottoResult, getPurchasePrice(purchaseLottoTickets.getCountOfLottoTickets()));
-    }
-
-    private static LottoPrice getPurchasePrice(Integer countOfLottoTickets) {
-        return LottoPrice.from(countOfLottoTickets * MINIMUM_PRICE);
+        resultView.printLottoResult(lottoResult, purchasePrice);
     }
 }
