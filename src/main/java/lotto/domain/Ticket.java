00c37f200b0ebad61d51c28dece18ea1bceaabb7
csh0034 <csh0034@gmail.com>
2021-11-10 23:09:07 +0900

    refactor: Ticket
    - 내부 컬렉션 자료구조 Set 으로 변경
    - stream api 사용부분 변경
    - 매직 리터럴 상수로 변경

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import lotto.exception.TicketRangeException;
 import lotto.exception.TicketSizeException;
@@ -13,50 +15,62 @@ public class Ticket {
 
 	public static final int PRICE = 1000;
 
-	private final List<Integer> values;
+	private static final String DUPLICATE_MESSAGE = "티켓 번호는 중복될 수 없습니다.";
+
+	private final Set<Integer> values;
 
 	Ticket(List<Integer> values) {
+		this(new LinkedHashSet<>(values));
+	}
+
+	Ticket(Set<Integer> values) {
 		validate(values);
 		this.values = values;
 	}
 
-	private void validate(List<Integer> values) {
+	private void validate(Set<Integer> values) {
 		validateSize(values);
 		validateRange(values);
 		validateDuplicate(values);
 	}
 
-	private void validateSize(List<Integer> values) {
+	private void validateSize(Set<Integer> values) {
 		if (values.size() != SIZE_OF_LOTTO_TICKET) {
 			throw new TicketSizeException();
 		}
 	}
 
-	private void validateRange(List<Integer> values) {
+	private void validateRange(Set<Integer> values) {
 		long count = values.stream()
-			.filter(value -> !(value < MIN_OF_LOTTO_NUMBERS))
-			.filter(value -> !(value > MAX_OF_LOTTO_NUMBERS))
+			.filter(this::isPossibleRange)
 			.count();
-
 		if (count != SIZE_OF_LOTTO_TICKET) {
 			throw new TicketRangeException();
 		}
 	}
 
-	private void validateDuplicate(List<Integer> values) {
-		if (values.stream().distinct().count() != SIZE_OF_LOTTO_TICKET) {
-			throw new IllegalArgumentException("티켓 번호는 중복될 수 없습니다.");
+	private boolean isPossibleRange(Integer value) {
+		return value >= MIN_OF_LOTTO_NUMBERS && value <= MAX_OF_LOTTO_NUMBERS;
+	}
+
+	private void validateDuplicate(Set<Integer> values) {
+		if (values.size() != SIZE_OF_LOTTO_TICKET) {
+			throw new IllegalArgumentException(DUPLICATE_MESSAGE);
 		}
 	}
 
 	public static Ticket create(List<Integer> values) {
-		return new Ticket(values);
+		return new Ticket(new LinkedHashSet<>(values));
 	}
 
 	public int getMatchedCount(Ticket winningNumberTicket) {
 		return (int)values.stream()
-			.filter(winningNumberTicket.values::contains)
+			.filter(winningNumberTicket::contains)
 			.count();
+	}
+
+	private boolean contains(Integer number) {
+		return values.contains(number);
 	}
 
 	@Override
