a320435b910215f91a27437927e04ecca0a8c29e
kangyonghyun <hmoon826@gmail.com>
2021-11-10 18:51:01 +0900

    (feat) step1 - 빈 문자열 or null이 들어올 경우 0을 반환

--- src/main/java/lotto/step1/StringCalculator.java
+++ src/main/java/lotto/step1/StringCalculator.java
@@ -1,41 +1,67 @@
 package lotto.step1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
     private static final String BASIC_DELIMITER_PATTERN = "[,:]";
     private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
 
-    private List<String> values;
+    private List<Integer> values = new ArrayList<>();
+    private int result;
 
     public StringCalculator(String input) {
-        String[] array = splitDelimiter(input);
-        this.values = Arrays.asList(array);
+        if (input != null && !input.isEmpty()) {
+            this.values = splitInput(input);
+        }
     }
 
-    public List<String> getValues() {
+    public List<Integer> getValues() {
         return values;
     }
 
-    public int add() {
-        return values.stream()
-                .mapToInt(Integer::valueOf)
+    public int calculateResult() {
+        return result;
+    }
+
+    public void add() {
+        this.result = values.stream()
+                .mapToInt(Integer::intValue)
                 .sum();
     }
 
-    private String[] splitDelimiter(String input) {
+    private List<Integer> splitInput(String input) {
         Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            return splitCustom(input, m);
         }
-        return input.split(BASIC_DELIMITER_PATTERN);
+        return splitBasic(input);
     }
+
+    private List<Integer> splitBasic(String input) {
+        String[] arr = input.split(BASIC_DELIMITER_PATTERN);
+        return toList(arr);
+    }
+
+    private List<Integer> splitCustom(String input, Matcher m) {
+        String customDelimiter = m.group(1);
+        String[] arr = m.group(2).split(customDelimiter);
+        return toList(arr);
+    }
+
+    private List<Integer> toList(String[] stringArr) {
+        return Arrays.stream(stringArr)
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
--- src/test/java/lotto/step1/StringCalculatorTest.java
+++ src/test/java/lotto/step1/StringCalculatorTest.java
@@ -19,22 +19,32 @@ public class StringCalculatorTest {
     @DisplayName("구분자(,:)를 기준으로 분리")
     void splitDelimiter() {
         StringCalculator calculator = new StringCalculator("3,4:5");
-        assertThat(calculator.getValues()).containsExactly("3", "4", "5");
+        assertThat(calculator.getValues()).containsExactly(3, 4, 5);
     }
 
     @Test
     @DisplayName("분리된 숫자 합")
     void add() {
         StringCalculator calculator = new StringCalculator("3,4:5");
-        int result = calculator.add();
-        assertThat(result).isEqualTo(12);
+        calculator.add();
+        assertThat(calculator.calculateResult()).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("문자열 값이 빈문자열 or null -> 0 반환")
+    void nullOrEmpty() {
+        StringCalculator empty = new StringCalculator("");
+        StringCalculator nullValue = new StringCalculator(null);
+        assertThat(empty.calculateResult()).isEqualTo(0);
+        assertThat(nullValue.calculateResult()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("커스텀 구분자 도입")
     void customDelimiter() {
         StringCalculator calculator = new StringCalculator("//;\n3;4;5");
-        int result = calculator.add();
+        calculator.add();
+        int result = calculator.calculateResult();
         assertThat(result).isEqualTo(12);
     }
 
