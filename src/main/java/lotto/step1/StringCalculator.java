583f0b2f9da75d3b32686ad05c09dbdf4674fa14
kangyonghyun <hmoon826@gmail.com>
2021-11-11 14:04:27 +0900

    (feat) step1 - 숫자가 마이너스 or 숫자가 아닌 값 검증추가, 스태틱 메서드로 변경

--- src/main/java/lotto/step1/StringCalculator.java
+++ src/main/java/lotto/step1/StringCalculator.java
@@ -1,79 +1,53 @@
 package lotto.step1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    private static final String BASIC_DELIMITER_PATTERN = "[,:]";
-    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+    private static final Pattern CUSTOM = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern BASIC = Pattern.compile("[,:]");
 
-    private List<Integer> values = new ArrayList<>();
-    private int result;
+    public static int addString(String s) {
+        return checkNullOrEmpty(s) ? 0 : sum(splitValue(s));
+    }
 
-    public StringCalculator(String input) {
-        if (input != null && !input.isEmpty()) {
-            this.values = splitInput(input);
+    public static boolean checkNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    public static String[] splitValue(String input) {
+        return isCustom(input) ? splitCustomPattern(input) : input.split(BASIC.toString());
+    }
+
+    public static int sum(String[] input) {
+        int sum = 0;
+        for (String s : input) {
+            checkMinusOrNotNumber(s);
+            sum += Integer.valueOf(s);
+        }
+        return sum;
+    }
+
+    public static void checkMinusOrNotNumber(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                throw new RuntimeException();
+            }
         }
     }
 
-    public List<Integer> getValues() {
-        return values;
+    static boolean isCustom(String input) {
+        return CUSTOM.matcher(input).matches();
     }
 
-    public int calculateResult() {
-        return result;
-    }
-
-    public void add() {
-        this.result = values.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
-    }
-
-    private List<Integer> splitInput(String input) {
-        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(input);
-        if (m.find()) {
-            return splitCustom(input, m);
+    public static String[] splitCustomPattern(String input) {
+        Matcher matcher = CUSTOM.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
         }
-        return splitBasic(input);
-    }
-
-    private List<Integer> splitBasic(String input) {
-        String[] arr = input.split(BASIC_DELIMITER_PATTERN);
-        return toList(arr);
-    }
-
-    private List<Integer> splitCustom(String input, Matcher m) {
-        String customDelimiter = m.group(1);
-        String[] arr = m.group(2).split(customDelimiter);
-        return toList(arr);
-    }
-
-    private List<Integer> toList(String[] stringArr) {
-        return Arrays.stream(stringArr)
-                .map(Integer::valueOf)
-                .collect(Collectors.toList());
-    }
-
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StringCalculator that = (StringCalculator) o;
-        return Objects.equals(values, that.values);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(values);
+        return null;
     }
 
 }
--- src/test/java/lotto/step1/StringCalculatorTest.java
+++ src/test/java/lotto/step1/StringCalculatorTest.java
@@ -4,48 +4,55 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
     @Test
-    @DisplayName("문자열 계산기 객체 생성")
-    void createStringCalculator() {
-        String input = "3,4:5";
-        StringCalculator calculator = new StringCalculator(input);
-        assertThat(calculator).isEqualTo(new StringCalculator(input));
+    @DisplayName("문자열 값이 빈문자열 or null -> 0 반환")
+    void nullOrEmpty1() {
+        assertThat(StringCalculator.checkNullOrEmpty(null)).isTrue();
+        assertThat(StringCalculator.checkNullOrEmpty("")).isTrue();
     }
 
     @Test
-    @DisplayName("구분자(,:)를 기준으로 분리")
-    void splitDelimiter() {
-        StringCalculator calculator = new StringCalculator("3,4:5");
-        assertThat(calculator.getValues()).containsExactly(3, 4, 5);
+    @DisplayName("기본 구분자로 문자열 쪼개기")
+    void splitBasic() {
+        String[] array = StringCalculator.splitValue("3,4:5");
+        assertThat(array).containsExactly("3", "4", "5");
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 문자열 쪼개기")
+    void splitCustom() {
+        String[] array = StringCalculator.splitValue("//;\n3;4;5");
+        assertThat(array).containsExactly("3", "4", "5");
     }
 
     @Test
     @DisplayName("분리된 숫자 합")
-    void add() {
-        StringCalculator calculator = new StringCalculator("3,4:5");
-        calculator.add();
-        assertThat(calculator.calculateResult()).isEqualTo(12);
+    void addTest() {
+        assertThat(StringCalculator.sum(StringCalculator.splitValue("3,4:5"))).isEqualTo(12);
+        assertThat(StringCalculator.sum(StringCalculator.splitValue("//;\n3;4;5"))).isEqualTo(12);
     }
 
     @Test
-    @DisplayName("문자열 값이 빈문자열 or null -> 0 반환")
-    void nullOrEmpty() {
-        StringCalculator empty = new StringCalculator("");
-        StringCalculator nullValue = new StringCalculator(null);
-        assertThat(empty.calculateResult()).isEqualTo(0);
-        assertThat(nullValue.calculateResult()).isEqualTo(0);
+    @DisplayName("마이너스 값 or 숫자가 아닌 값 에러")
+    void checkMinusOrNotNumber() {
+        assertThatThrownBy(() -> StringCalculator.checkMinusOrNotNumber("3,4:ee"))
+                .isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> StringCalculator.checkMinusOrNotNumber("3,4:-1"))
+                .isInstanceOf(RuntimeException.class);
     }
 
     @Test
-    @DisplayName("커스텀 구분자 도입")
-    void customDelimiter() {
-        StringCalculator calculator = new StringCalculator("//;\n3;4;5");
-        calculator.add();
-        int result = calculator.calculateResult();
-        assertThat(result).isEqualTo(12);
+    @DisplayName("통합")
+    void test() {
+        assertThat(StringCalculator.addString("3,4:5")).isEqualTo(12);
+        assertThat(StringCalculator.addString("//;\n3;4;5")).isEqualTo(12);
+        assertThat(StringCalculator.addString("")).isEqualTo(0);
+        assertThat(StringCalculator.addString(null)).isEqualTo(0);
+
     }
 
 }
