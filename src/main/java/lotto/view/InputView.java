b6a9aa070080bf81bd864d9cdc99cf71afd70b12
csh0034 <csh0034@gmail.com>
2021-11-12 22:43:01 +0900

    refactor: InputView
    - nextInt clearScannerBuffer 추가
    - 개행문자 메서드에서 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,8 +14,8 @@ public final class InputView {
 	private static final Pattern WINNING_NUMBER_PATTERN = Pattern.compile("^(\\d+, ){5}\\d+$");
 	private static final String WINNING_NUMBER_DELIMITER = ", ";
 
-	private static final String INTEGER_INPUT_MISMATCH_MESSAGE = "숫자만 입력 가능합니다.\n";
-	private static final String WINNING_NUMBER_PATTERN_MISMATCH_MESSAGE = "당첨 번호 입력 형식이 올바르지 않습니다.\n";
+	private static final String INTEGER_INPUT_MISMATCH_MESSAGE = "숫자만 입력 가능합니다.";
+	private static final String WINNING_NUMBER_PATTERN_MISMATCH_MESSAGE = "당첨 번호 입력 형식이 올바르지 않습니다.";
 
 	private static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
 	private static final String PURCHASE_COMPLETE_MESSAGE = "%d개를 구매했습니다.\n";
@@ -30,7 +30,7 @@ public final class InputView {
 		try {
 			return innerNumberOfPurchases();
 		} catch (InputMismatchException e) {
-			System.err.println(INTEGER_INPUT_MISMATCH_MESSAGE);
+			System.err.println(INTEGER_INPUT_MISMATCH_MESSAGE + "\n");
 			clearScannerBuffer();
 			return getNumberOfPurchases();
 		}
@@ -56,7 +56,7 @@ public final class InputView {
 		try {
 			return innerWinningTicket();
 		} catch (IllegalArgumentException e) {
-			System.err.println(WINNING_NUMBER_PATTERN_MISMATCH_MESSAGE);
+			System.err.println(e.getMessage() + "\n");
 			return getWinningTicket();
 		}
 	}
@@ -69,6 +69,8 @@ public final class InputView {
 
 		System.out.println(BONUS_BALL_MESSAGE);
 		int bonus = SCANNER.nextInt();
+
+		clearScannerBuffer();
 
 		return WinningTicket.create(winningNumber.split(WINNING_NUMBER_DELIMITER), bonus);
 	}
