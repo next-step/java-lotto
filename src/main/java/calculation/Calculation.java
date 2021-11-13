be296b44b7912def52e54acdc1cd36e8d60416ed
user <kth4878@naver.com>
2021-11-14 02:25:34 +0900

    [Refactor] 사용하지 않는 계산 제거

--- src/main/java/calculation/Calculation.java
+++ src/main/java/calculation/Calculation.java
@@ -4,15 +4,8 @@ import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public enum Calculation {
-    PLUS("+", (a, b) -> a + b),
-    MINUS("-", (a, b) -> a - b),
-    MULTIPLICATION("*", (a, b) -> a * b),
-    DIVIDE("/", (a, b) -> {
-        validToDivideByZero(b);
-        return a / b;
-    });
+    PLUS("+", (a, b) -> a + b);
 
-    private static final String DIVISION_ERROR_MESSAGE = "error : 0으로 나눌수 없습니다.";
     private static final String OPERATOR_ERROR_MESSAGE = "error : 올바른 연산자가 아닙니다.";
     private String operator;
     private BiFunction<Integer, Integer, Integer> calculateValue;
@@ -28,14 +21,9 @@ public enum Calculation {
 
     private static Calculation findValidOperator(String operator){
         return Arrays.stream(values())
-                .filter(i -> i.operator.equals(operator))
+                .filter(formula -> formula.operator.equals(operator))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(OPERATOR_ERROR_MESSAGE));
     }
 
-    private static void validToDivideByZero(int secondNumber){
-        if(secondNumber == 0){
-            throw new IllegalArgumentException(DIVISION_ERROR_MESSAGE);
-        }
-    }
 }
