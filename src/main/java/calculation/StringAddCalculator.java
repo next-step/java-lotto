8deeb8af7badcadbb33abdda04ed50dc630d218b
user <kth4878@naver.com>
2021-11-15 00:32:21 +0900

    [Refactor] 문자열 분리 기능과 덧셈 기능을 분리

--- src/main/java/calculation/StringAddCalculator.java
+++ src/main/java/calculation/StringAddCalculator.java
@@ -18,28 +18,36 @@ public class StringAddCalculator {
             return NULL_AND_EMPTY_ZERO;
         }
 
-        return patternMatch(text).getNumber();
+        int[] numbers = changeToIntegerArrayType(patternMatch(text));
+
+        return calculate(numbers);
     }
 
-    private static Number patternMatch(String text) {
+    private static String[] patternMatch(String text) {
         Matcher matcher = CUSTOM_PATTERN.matcher(text);
-
-        String[] numbers;
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
-            numbers = matcher.group(2).split(customDelimiter);
 
-            return new Number(changeToIntegerArrayType(numbers));
+            return matcher.group(2).split(customDelimiter);
         }
 
-        numbers = text.split(SPLIT_CONDITION);
-        return new Number(changeToIntegerArrayType(numbers));
+        return text.split(SPLIT_CONDITION);
     }
 
     private static int[] changeToIntegerArrayType(String[] numbers) {
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
                 .toArray();
+    }
+
+    private static int calculate(int[] numbers) {
+        Number number = new Number(numbers);
+        int loopNumber = numbers.length - 1;
+        for (int i = 0; i < loopNumber; i++) {
+            number.plus(numbers[i + 1]);
+        }
+
+        return number.getNumber();
     }
 
 }
