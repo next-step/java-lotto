70edd75b5d0e73a5a9d0780e2444098d7e4d7a6d
DongHyo <kdhyo98@gmail.com>
2021-11-15 19:45:58 +0900

    test : 테스트 요구사항 정리

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class CalculatorTest {
+
+    void 디폴트구분자로_덧셈계산하기() {
+
+    }
+
+    void 커스텀구분자로_덧셈계산하기() {
+
+    }
+
+    void 구분자_숫자_외_값있을시_예외처리() {
+
+    }
+
+    void 음수있을시_예외처리() {
+
+    }
+
+}
