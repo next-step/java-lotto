d1398e2b1f66e53f8fcbe97d9e1bb444c63ffc31
jjoy <ekdnlt1995@gmail.com>
2021-11-15 19:20:28 +0900

    feat : 숫자 하나인 문자열이 입력될 경우 해당 숫자를 반환

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,8 +4,11 @@ public class StringAddCalculator {
 
     //쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
     public static int splitAndSum(String input){
-        if(input == null) return 0;
-        if(input.trim().isEmpty()) return 0;
-        return 1;
+        if(isEmptyOrNull(input)) return 0;
+        return Integer.parseInt(input);
+    }
+
+    private static boolean isEmptyOrNull(String input){
+        return input == null || input.trim().isEmpty();
     }
 }
