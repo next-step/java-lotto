f7cb3630553bcec96a8f762bcbff49b3531a38aa
DongHyo <kdhyo98@gmail.com>
2021-11-16 00:46:58 +0900

    test : 숫자 객체 요구사항 정리

---
+++ src/test/java/calculator/NumberTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+
+    @Test
+    void 숫자_더하는_객체() {
+
+    }
+
+    @Test
+    void 숫자_더하는_객체_예외처리() {
+        
+    }
+
+}
