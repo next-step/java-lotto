e8f8900dd3f7931ca53e9bc142467df81edbd383
mj950425 <minjoon1995@naver.com>
2021-11-15 23:31:07 +0900

    [featrue] view (input, output) 생성

--- src/main/java/step1/view/Output.java
+++ src/main/java/step1/view/Output.java
@@ -1,10 +1,13 @@
 package step1.view;
 
-import step1.domain.Numbers;
+import step1.domain.Calculator;
+
+import java.util.List;
 
 public class Output {
 
-    public static void output(Integer input) {
-        System.out.println(input);
+    public static void output(List<Integer> input) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.Add(input).getNum());
     }
 }
