83c3d5f1d3f6857d7d75d6cbf2729645f4eda698
doorisopen <doorwasopen@gmail.com>
2021-11-15 23:31:26 +0900

    feat: step1 수식을 계산한다

--- src/main/java/step1/Formula.java
+++ src/main/java/step1/Formula.java
@@ -1,5 +1,7 @@
 package step1;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +11,9 @@ public class Formula {
     private static final String DEFAULT_REGEX = ",:";
     private static final int CUSTOM_FORMULA_REGEX_INDEX = 1;
     private static final int CUSTOM_FORMULA_INDEX = 2;
+    private static final int NUMBER_ZERO = 0;
+    private static final String CUSTOM_REGEX_PATTERN = "//(.)\n(.*)";
+    private static final String BLANK = "";
 
     private String formula;
 
@@ -21,12 +26,33 @@ public class Formula {
     }
 
     public String[] split() {
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(formula);
-        String customRegex = "";
+        if (isEmpty()) return new String[]{};
+        Matcher matcher = Pattern.compile(CUSTOM_REGEX_PATTERN).matcher(formula);
+        String customRegex = BLANK;
         if (matcher.find()) {
             customRegex = matcher.group(CUSTOM_FORMULA_REGEX_INDEX);
             formula = matcher.group(CUSTOM_FORMULA_INDEX);
         }
         return formula.split(LEFT_SQUARE_BRACKET + DEFAULT_REGEX + customRegex + RIGHT_SQUARE_BRACKET);
+    }
+
+    public int calculate() {
+        String[] splitFormula = split();
+        if (splitFormula.length == NUMBER_ZERO) return NUMBER_ZERO;
+        return Arrays.stream(splitFormula).mapToInt((operand) -> {
+            validateIsDigit(operand);
+            return Integer.parseInt(operand);
+        }).sum();
+    }
+
+    private void validateIsDigit(String operator) {
+        boolean isDigit = operator.chars().allMatch(Character::isDigit);
+        if (!isDigit) {
+            throw new RuntimeException("숫자 이외의 값이 포함되어 있습니다.");
+        }
+    }
+
+    private boolean isEmpty() {
+        return Objects.isNull(formula) || BLANK.equals(formula);
     }
 }
--- src/test/java/step1/FormulaTest.java
+++ src/test/java/step1/FormulaTest.java
@@ -2,9 +2,11 @@ package step1;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FormulaTest {
@@ -31,5 +33,39 @@ public class FormulaTest {
         String[] splitFormula = formula.split();
         //then
         assertThat(splitFormula.length).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3", "1:2:3", "1,2:3",
+            "//a\n1a2a3", "//+\n1+2:3", "//;\n1:2:3"})
+    void 수식의_합을_계산한다(String input) {
+        //given
+        Formula formula = new Formula(input);
+        //when
+        int result = formula.calculate();
+        //then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 수식이_빈값이면_결과는_0_이다(String input) {
+        //given
+        Formula formula = new Formula(input);
+        //when
+        int result = formula.calculate();
+        //then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,a", "1:-2:3", "i1,2:3",
+            "//;\n1;-2;3", "//;\n1;e:3", "//;\n-x:2:3"})
+    void 수식에_음수_혹은_문자가_포함되어_있으면_RuntimeException이_발생한다(String input) {
+        //given
+        Formula formula = new Formula(input);
+        //when & then
+        assertThatThrownBy(formula::calculate)
+                .isInstanceOf(RuntimeException.class);
     }
 }
