9bed7f212db2dd4a5e41255a82aa1753bd6096f2
ydh6226 <ydh6226@naver.com>
2021-11-15 23:24:50 +0900

    feat: Grade 구현

--- README.md
+++ README.md
@@ -8,6 +8,9 @@
 - [x] LottoNumbers (일급컬렉션) 구현 
   - LottoNumber를 정렬된 상태로 반환한다.
   
+- [x] Grade 구현
+  - 등수에 해당하는 당첨개수와 상금을 가지고 있다.
+
 - [ ] LottoTicket 구현
   - 금액에 맞는 LottoNumbers 를 생성한다.
   - 당첨 번호로 당첨 통계를 만들 수 있다.
---
+++ src/main/java/lotto/Grade.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import static java.util.Arrays.stream;
+
+public enum Grade {
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    BANG(0, 0);
+
+    private static final Grade[] CACHED_GRADES = Grade.values();
+
+    private final int matchCount;
+    private final int reward;
+
+    Grade(int matchCount, int reward) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+    }
+
+    public static Grade from(int matchCount) {
+        return stream(CACHED_GRADES)
+                .filter(grade -> grade.matchCount == matchCount)
+                .findAny()
+                .orElse(BANG);
+    }
+}
