a538ec395defe9392124ada943171e763f4f4511
DongHyo <kdhyo98@gmail.com>
2021-11-16 01:37:35 +0900

    feat : 정규식 input 추가

--- src/main/java/calculator/DelimiterRegex.java
+++ src/main/java/calculator/DelimiterRegex.java
@@ -5,21 +5,33 @@ import java.util.regex.Pattern;
 
 public class DelimiterRegex {
 
-    private static final String REGEX = "^\\/\\/(.+)(\\\\n)";
-    private static final Pattern PATTERN = Pattern.compile(REGEX);
+    private static final String REGEX_CUSTOM_DELIMITER = "^\\/\\/(.+)\\\\n";
+    private static final String REGEX_INPUT = "\\\\n(.+)";
+
+    private static final Pattern PATTERN_CUSTOM_DELIMITER = Pattern.compile(REGEX_CUSTOM_DELIMITER);
+    private static final Pattern PATTERN_INPUT = Pattern.compile(REGEX_INPUT);
     private static final Integer DELIMITER_GROUP = 1;
 
     private DelimiterRegex() {
     }
 
-    public static String getDelimiter(String formula) {
-        Matcher matcher = PATTERN.matcher(formula);
+    public static String getCustomDelimiter(String formula) {
+        Matcher matcher = PATTERN_CUSTOM_DELIMITER.matcher(formula);
 
-        if(matcher.find()) {
+        if (matcher.find()) {
             return matcher.group(DELIMITER_GROUP);
         }
 
         return null;
     }
 
+    public static String getInput(String formula) {
+        Matcher matcher = PATTERN_INPUT.matcher(formula);
+
+        if (matcher.find()) {
+            return matcher.group(DELIMITER_GROUP);
+        }
+
+        throw new IllegalArgumentException("커스텀 계산 식이 아닙니다.");
+    }
 }
