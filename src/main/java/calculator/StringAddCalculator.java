6514644ae06dc1290bf16be24c41f00816cad7ce
jjoy <ekdnlt1995@gmail.com>
2021-11-15 19:45:41 +0900

    feat : 구분자로 :도 사용할 수 있다.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -7,7 +7,8 @@ public class StringAddCalculator {
     //쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
     public static int splitAndSum(String input){
         if(isEmptyOrNull(input)) return 0;
-        String[] numbers = input.split(",");
+        String tokens = ",|:";
+        String[] numbers = input.split(tokens);
         return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
     }
 
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -31,6 +32,14 @@ class StringAddCalculatorTest {
     @CsvSource(value = { "1,2;3", "2,3,4;9", "1,3,6;10"}, delimiter = ';')
     @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     void commaSeparatedAddNumbers(String input, int expected){
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "1,2:3;6", "1:3,6;10"}, delimiter = ';')
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
+    void otherCharSeparatedAddNumbers(String input, int expected){
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(expected);
     }
