d8a40c41c7dd9132c207fb3bbe139cb10ac675a3
audgnssweet <audgnssweet@naver.com>
2021-11-17 00:04:54 +0900

    refactor: Expression 원시값을 클래스로 분리

---
+++ src/main/java/step1/Expression.java
@@ -0,0 +1,7 @@
+package step1;
+
+public class Expression {
+
+    public Expression(String s) {
+    }
+}
---
+++ src/test/java/step1/ExpressionTest.java
@@ -0,0 +1,11 @@
+package step1;
+
+import org.junit.jupiter.api.Test;
+
+public class ExpressionTest {
+
+    @Test
+    void expression() {
+        Expression expression = new Expression("1,2:3");
+    }
+}
