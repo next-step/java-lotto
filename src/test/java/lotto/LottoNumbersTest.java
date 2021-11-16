d702895ad644ebb0ba763b322ddaff34a91da6f4
ydh6226 <ydh6226@naver.com>
2021-11-16 21:20:56 +0900

    refactor: 테스트코드에서도 캐시된 LottoNumber을 이용하도록 변경

--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -14,18 +14,11 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class LottoNumbersTest {
 
-    private static final LottoNumber NUM_1 = new LottoNumber(1);
-    private static final LottoNumber NUM_2 = new LottoNumber(2);
-    private static final LottoNumber NUM_3 = new LottoNumber(3);
-    private static final LottoNumber NUM_10 = new LottoNumber(10);
-    private static final LottoNumber NUM_15 = new LottoNumber(15);
-    private static final LottoNumber NUM_20 = new LottoNumber(20);
-
     @DisplayName("LottoNumber를 정렬된 상태로 가지고 있는지")
     @Test
     void lottoNumbers() {
-        LottoNumbers lottoNumbers = new LottoNumbers(asList(NUM_3, NUM_1, NUM_2, NUM_10, NUM_15, NUM_20));
-        assertThat(lottoNumbers.lottoNumbers()).isEqualTo(asList(NUM_1, NUM_2, NUM_3, NUM_10, NUM_15, NUM_20));
+        LottoNumbers lottoNumbers = new LottoNumbers(asList(num(3), num(1), num(2), num(10), num(15), num(20)));
+        assertThat(lottoNumbers.lottoNumbers()).isEqualTo(asList(num(1), num(2), num(3), num(10), num(15), num(20)));
     }
 
     @DisplayName("당첨번호개수에 맞게 등급을 잘 반환하는지")
@@ -37,23 +30,23 @@ class LottoNumbersTest {
     }
 
     static Stream<Arguments> matchArguments() {
-        LottoNumbers lottoNumbers = new LottoNumbers(asList(NUM_3, NUM_1, NUM_2, NUM_10, NUM_20, NUM_15));
+        LottoNumbers lottoNumbers = new LottoNumbers(asList(num(3), num(1), num(2), num(10), num(15), num(20)));
         return Stream.of(
-                Arguments.of(lottoNumbers, new LottoNumbers(asList(NUM_3, NUM_1, NUM_2, NUM_10, NUM_20, NUM_15)), Grade.FIRST),
-                Arguments.of(lottoNumbers, new LottoNumbers(asList(NUM_3, NUM_1, NUM_2, num(43), num(44), num(45))), Grade.FOURTH),
+                Arguments.of(lottoNumbers, new LottoNumbers(asList(num(3), num(1), num(2), num(10), num(20), num(15))), Grade.FIRST),
+                Arguments.of(lottoNumbers, new LottoNumbers(asList(num(3), num(1), num(2), num(43), num(44), num(45))), Grade.FOURTH),
                 Arguments.of(lottoNumbers, new LottoNumbers(asList(num(40), num(41), num(42), num(43), num(44), num(45))), Grade.BANG)
                 );
-    }
-
-    private static LottoNumber num(int num) {
-        return new LottoNumber(num);
     }
 
     @DisplayName("중복된 로또번호를 입력할 경우 예외를 던진다.")
     @Test
     void create_duplicatedLottoNumbers() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoNumbers(asList(NUM_3, NUM_3, NUM_2, NUM_10, NUM_20, NUM_15)))
+                .isThrownBy(() -> new LottoNumbers(asList(num(3), num(3), num(2), num(10), num(20), num(15))))
                 .withMessage(LottoNumbers.DUPLICATION_ERROR_MESSAGE);
+    }
+
+    private static LottoNumber num(int num) {
+        return LottoNumber.from(num);
     }
 }
