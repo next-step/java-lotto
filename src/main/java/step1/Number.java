fd77c843f85e3a6bad2efd55a81135e0c2a6f5b7
audgnssweet <audgnssweet@naver.com>
2021-11-17 00:49:16 +0900

    refactor: Number 클래스 분리 및 테스트코드 작성

---
+++ src/main/java/step1/Number.java
@@ -0,0 +1,34 @@
+package step1;
+
+public class Number {
+
+    private final int number;
+
+    public Number(final String number) {
+        this(Integer.parseInt(number));
+    }
+
+    public Number(int number) {
+        this.number = number;
+        validate();
+    }
+
+    private void validate() {
+        if (this.number < 0) {
+            throw new RuntimeException("입력 숫자는 0보다 작을 수 없습니다");
+        }
+    }
+
+    public Number add(Number other) {
+        return new Number(this.number + other.number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !o.getClass().equals(Number.class)) {
+            return false;
+        }
+        Number other = (Number) o;
+        return this.number == other.number;
+    }
+}
