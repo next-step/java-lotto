a401c958a48d149017b5cff52160db0a217970d5
DongHyo <kdhyo98@gmail.com>
2021-11-17 00:38:09 +0900

    test : 커스텀 구분자 덧셈 테스트코드 작성

--- src/test/java/calculator/AdditionCalculatorTest.java
+++ src/test/java/calculator/AdditionCalculatorTest.java
@@ -9,15 +9,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class AdditionCalculatorTest {
 
     @ParameterizedTest
-    @DisplayName("디폴트구분자로 덧셈계산하기")
+    @DisplayName("디폴트구분자로 덧셈 계산하기")
     @CsvSource(value = {"1:2:3=6", "1,2,3=6", "1,2:3=6"}, delimiter = '=')
-    void 디폴트구분자로_덧셈계산하기(String s, Integer result) {
+    void sumDefaultDelimiter(String s, Integer result) {
         AdditionCalculator calculator = AdditionCalculator.from(s);
         assertThat(calculator.result()).isEqualTo(result);
     }
 
-    void 커스텀구분자로_덧셈계산하기() {
-
+    @ParameterizedTest
+    @DisplayName("커스텀 구분자로 덧셈 계산하기")
+    @CsvSource(value = {"//;\\n1;2;3=6", "//a\\n1a2a3a4=10", "//+\\n1+2+3+5=11"}, delimiter = '=')
+    void sumCustomDelimiter(String s, Integer result) {
+        AdditionCalculator calculator = AdditionCalculator.from(s);
+        assertThat(calculator.result()).isEqualTo(result);
     }
 
     void 구분자_숫자_외_값있을시_예외처리() {
