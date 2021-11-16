64da0c0dfac5955170007321afbd73d8fc95bf33
Han <hankim@tapasmedia.co>
2021-11-16 20:45:24 +0900

    feat : add CUSTOM_DELIMITER_PATTERN
    pass test of splitAndSum_custom_구분자

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,7 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author han
  */
 public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String input) {
         if (nullOrEmpty(input)) {
@@ -25,6 +30,12 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
         return input.split(",|:");
     }
 }
