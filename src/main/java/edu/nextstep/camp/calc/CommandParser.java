f962306ef8d0200e339b00c1ccb81f3fcee66d7b
sid <sid.seo@kakaocorp.com>
2021-11-16 23:33:09 +0900

    feature: 문자열 입력을 받아 숫자를 구분
    지정된 규칙에 의해 입력 문자열 파싱
     - 쉼표나 콜론으로 구분
     - 커스텀 문자열 추출후 같이 구분
     - 구분된 문자열을 숫자로 변환. 음수는 허용하지 않음

---
+++ src/main/java/edu/nextstep/camp/calc/CommandParser.java
@@ -0,0 +1,59 @@
+package edu.nextstep.camp.calc;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class CommandParser {
+    private final static Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
+    private final static Pattern NUMBER_PATTERN = Pattern.compile("[0-9]+");
+    private final static String DELIMITER = ",:";
+    private final static String REGEX_OR_FORMATTER = "[%s]";
+    private final static String DELIMITERS = String.format(REGEX_OR_FORMATTER, DELIMITER);
+    private final static int CUSTOM_DELIMITER_GROUP = 1;
+    private final static int COMMAND_GROUP = 2;
+
+
+    public static Collection<Integer> parse(String command) {
+        if (command == null || command.isEmpty()) {
+            return List.of(0);
+        }
+
+        return Arrays.stream(split(command))
+                .map(String::trim)
+                .map(CommandParser::parseToInt)
+                .collect(Collectors.toList());
+    }
+
+    public static String[] split(String command) {
+        final Matcher matcher = PATTERN.matcher(command);
+        if (!matcher.find()) {
+            return command.split(DELIMITERS);
+        }
+
+        final String customDelimiter = matcher.group(CUSTOM_DELIMITER_GROUP);
+        final String delimiters = String.format(REGEX_OR_FORMATTER, DELIMITER + customDelimiter);
+        return matcher.group(COMMAND_GROUP)
+                .split(delimiters);
+    }
+
+    private static int parseToInt(String numberCandidate) {
+        if (numberCandidate == null || numberCandidate.isEmpty()) {
+            return 0;
+        }
+
+        if (!NUMBER_PATTERN.matcher(numberCandidate).matches()) {
+            throw new IllegalArgumentException("invalid input(given value is not a number):" + numberCandidate);
+        }
+
+        final int number = Integer.parseInt(numberCandidate);
+        if (number < 0) {
+            throw new IllegalArgumentException("invalid input(the number must be positive): " + numberCandidate);
+        }
+
+        return number;
+    }
+}
---
+++ src/main/java/edu/nextstep/camp/calc/Main.java
@@ -0,0 +1,4 @@
+package edu.nextstep.camp.calc;
+
+public class Main {
+}
