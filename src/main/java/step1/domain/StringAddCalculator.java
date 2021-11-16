850a9a3f67fc5aba12861e770b375744cf845519
minjoon_lee <minjoon1995@naver.com>
2021-11-16 14:27:39 +0900

    Create StringAddCalculator.java

---
+++ src/main/java/step1/domain/StringAddCalculator.java
@@ -0,0 +1,10 @@
+package step1.domain;
+
+import step1.util.InputUtil;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String input) {
+        return InputUtil.getNumbers(input).add().getNum();
+    }
+}
