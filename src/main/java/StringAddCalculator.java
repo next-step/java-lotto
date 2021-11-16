df49a687a1d5c6d76d86464c6f1c72d6724856e1
Han <hankim@tapasmedia.co>
2021-11-16 20:30:16 +0900

    feat : add parseInt()
    pass test of splitAndSum_숫자하나

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -7,6 +7,6 @@ public class StringAddCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        return 0;
+        return Integer.parseInt(input);
     }
 }
