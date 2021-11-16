5ae1fa2e11882fbea7d36b33b610a040e39d2222
Han <hankim@tapasmedia.co>
2021-11-16 20:31:45 +0900

    refactor : extract null or empty validation

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -4,9 +4,13 @@
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) {
+        if (nullOrEmpty(input)) {
             return 0;
         }
         return Integer.parseInt(input);
+    }
+
+    private static boolean nullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 }
