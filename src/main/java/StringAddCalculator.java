c8f0f0f6d964e1060ad94e9779fc34b3d0d1c6d1
Han <hankim@tapasmedia.co>
2021-11-16 20:29:34 +0900

    feat : add splitAndSum
    pass test of splitAndSum_null_또는_빈문자

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -3,4 +3,10 @@
  */
 public class StringAddCalculator {
 
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
 }
