ca83c41e0b614b0b7f68819fd4aca3875f8d7930
doorisopen <doorwasopen@gmail.com>
2021-11-17 22:45:37 +0900

    feat: step2 Number 유효성 체크 추가

--- src/main/java/step2/domain/Number.java
+++ src/main/java/step2/domain/Number.java
@@ -3,6 +3,9 @@ package step2.domain;
 import step2.utils.RandomUtils;
 
 public class Number {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
     private int number;
 
     private Number() {
@@ -10,7 +13,9 @@ public class Number {
     }
 
     private Number(String number) {
-        this.number = Integer.parseInt(number);
+        int value = Integer.parseInt(number);
+        validateNumberBound(value);
+        this.number = value;
     }
 
     public static Number create() {
@@ -27,5 +32,11 @@ public class Number {
 
     public String getStringNumber() {
         return String.valueOf(number);
+    }
+
+    private void validateNumberBound(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("유효하지 않은 숫자 범위입니다.");
+        }
     }
 }
--- src/test/java/step2/domain/NumberTest.java
+++ src/test/java/step2/domain/NumberTest.java
@@ -1,8 +1,10 @@
 package step2.domain;
 
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NumberTest {
 
@@ -12,5 +14,11 @@ public class NumberTest {
         Number number = Number.create();
         //when & then
         assertThat(number.getNumber()).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(45);
+    }
+
+    @Test
+    void 범위_1이상_45이하의_숫자가_아니면_IllegalArgumentException_이_발생한다() {
+        assertThatThrownBy(() -> Number.of("46")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Number.of("0")).isInstanceOf(IllegalArgumentException.class);
     }
 }
