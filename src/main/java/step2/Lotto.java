c7e6fc8fa397427d0131025a4208c51416a7b76d
audgnssweet <audgnssweet@naver.com>
2021-11-17 16:48:29 +0900

    feat: Lotto 클래스 생성

--- src/main/java/step2/Lotto.java
+++ src/main/java/step2/Lotto.java
@@ -1,5 +1,21 @@
 package step2;
 
+import java.util.List;
+
 public class Lotto {
 
+    private final LottoNumbers lottoNumbers;
+
+    public Lotto() {
+        lottoNumbers = new LottoNumbers();
+    }
+
+    //랜덤값 테스트를 위한 생성자
+    protected Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = new LottoNumbers(lottoNumbers);
+    }
+
+    public boolean isWinner(Lotto winner) {
+        return lottoNumbers.equals(winner.lottoNumbers);
+    }
 }
--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -1,11 +1,20 @@
 package step2;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class LottoTest {
 
     @Test
-    void lotto() {
-        Lotto lotto = new Lotto();
+    void isWinning() {
+        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5));
+        assertAll(
+            () -> assertTrue(lotto.isWinner(new Lotto(Arrays.asList(1, 2, 3, 4, 5)))),
+            () -> assertFalse(lotto.isWinner(new Lotto(Arrays.asList(2, 3, 4, 5, 6))))
+        );
     }
 }
