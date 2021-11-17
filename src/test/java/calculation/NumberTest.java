56f32c9e70f02405c2c9188d4bd5ba9e23076f76
user <kth4878@naver.com>
2021-11-17 19:46:57 +0900

    [Test] 문자열 계산기 Number Test 추가

--- src/test/java/calculation/NumberTest.java
+++ src/test/java/calculation/NumberTest.java
@@ -1,10 +1,8 @@
 package calculation;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -14,16 +12,17 @@ class NumberTest {
 
     @BeforeEach
     public void setup() {
-        List<Integer> number = new ArrayList<>();
-        number.add(1);
-        number.add(2);
+        List<Number> number = Arrays.asList(
+                new Number(1),
+                new Number(2)
+        );
 
         numbers = new Numbers(number);
     }
 
     @Test
     @DisplayName("합계 계산")
-    public void calculateSum(){
+    public void calculateSum() {
         assertThat(numbers.sum()).isEqualTo(3);
     }
 
