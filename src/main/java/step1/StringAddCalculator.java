462af83cd9f4bc5dc3268bf04c16f26be8b98e81
audgnssweet <audgnssweet@naver.com>
2021-11-17 13:43:16 +0900

    refactor: StringAddCalculator의 매직넘버 삭제

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -2,8 +2,10 @@ package step1;
 
 public class StringAddCalculator {
 
+    private static final int INITIAL_VALUE = 0;
+
     public static Number calculate(Expression expression) {
-        Number ans = new Number(0);
+        Number ans = new Number(INITIAL_VALUE);
 
         while (expression.hasNextNumber()) {
             ans = ans.add(expression.nextNumber());
