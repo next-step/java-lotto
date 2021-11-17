b8c1a86cf105049668079efcff629a8ace117693
user <kth4878@naver.com>
2021-11-17 19:54:47 +0900

    [Feat] 로또 컨트롤러 추가

---
+++ src/main/java/controller/LotteryController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import domain.*;
+import view.InputView;
+import view.OutputView;
+
+public class LotteryController {
+    public static void main(String[] args) {
+        Store store = new Store(InputView.inputPrice());
+        OutputView.purchaseLottery(store);
+
+        LotteryTickets lotteryTickets = new LotteryTickets(store);
+        OutputView.printLotteryNumber(lotteryTickets);
+
+        LastWeekLotteryNumber lastWeekLottery = new LastWeekLotteryNumber(InputView.inputLastWeekNumber());
+
+        RankGroup rankGroup = new RankGroup(lotteryTickets, lastWeekLottery);
+        MoneyPrize moneyPrize = new MoneyPrize(rankGroup);
+
+        OutputView.resultWinningMessage();
+        OutputView.printWinningPrize(rankGroup);
+        OutputView.resultLotteryProfit(moneyPrize.lotteryProfit(store));
+
+    }
+}
