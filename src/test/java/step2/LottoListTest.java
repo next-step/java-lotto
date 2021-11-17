241209d1cfb693dc41c19e40f6867faacfd9a7e4
minjoon_lee <minjoon1995@naver.com>
2021-11-17 19:54:17 +0900

    add : test 코드 추가

--- src/test/java/step2/LottoListTest.java
+++ src/test/java/step2/LottoListTest.java
@@ -4,11 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.domain.Lotto;
 import step2.domain.LottoList;
+import step2.domain.starategy.RandomGetLottoImpl;
 import step2.view.Input;
+
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoListTest {
+    @Test
+    void checkMatchingTest() {
+        List<Integer> winningNumber = Arrays.asList(1, 2, 3, 7, 8, 9);
+        LottoList lottoList = new LottoList(7000, () -> Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lottoList.checkMatching(winningNumber)).isEqualTo(Arrays.asList(0, 0, 0, 7, 0, 0, 0));
+    }
 
 }
