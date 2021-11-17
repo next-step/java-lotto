3df2d8fda53b3a96f44a1424c91049e914ad3d45
user <kth4878@naver.com>
2021-11-17 20:03:26 +0900

    [Test] 당첨 등수별 금액 테스트 추가

---
+++ src/test/java/domain/MoneyPrizeTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MoneyPrizeTest {
+    private MoneyPrize moneyPrize;
+
+    @BeforeEach
+    public void setup() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            numbers.add(i);
+        }
+
+        LotteryTicket lotteryTicket = new LotteryTicket(numbers);
+        List<LotteryTicket> lotteryTickets = Arrays.asList(lotteryTicket);
+        LastWeekLotteryNumber lastWeekLottery = new LastWeekLotteryNumber("1,2,3,4,8,9");
+        moneyPrize = new MoneyPrize(new RankGroup(new LotteryTickets(lotteryTickets), lastWeekLottery));
+    }
+
+    @Test
+    @DisplayName("당첨 등수 상금 확인")
+    public void checkWinningPrize() {
+        assertThat(moneyPrize.value(3)).isEqualTo(50000);
+    }
+
+}
