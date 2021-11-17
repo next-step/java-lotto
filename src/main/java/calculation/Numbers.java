7b5c66649924de23056e7465a9ce5804f1bcc43e
user <kth4878@naver.com>
2021-11-17 19:46:29 +0900

    [Refactor] 문자열 계산기 Number객체 추가로 인한 코드 분할

--- src/main/java/calculation/Numbers.java
+++ src/main/java/calculation/Numbers.java
@@ -6,39 +6,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Numbers {
-    private static final String NEGATIVE_NUMBER_ERROR_MESSAGE = "error : 음수는 사용할수 없습니다.";
-    private static final int NEGATIVE_NUMBER_ZERO = 0;
-    private final List<Integer> number;
+    private final List<Number> number;
 
     public Numbers(String[] numbers) {
         this(changeToIntegerArrayType(numbers));
     }
 
-    public Numbers(List<Integer> numbers) {
-        checkNegativeNumber(numbers);
+    public Numbers(List<Number> numbers) {
         this.number = Collections.unmodifiableList(numbers);
     }
 
-    private static List<Integer> changeToIntegerArrayType(String[] numbers) {
+    private static List<Number> changeToIntegerArrayType(String[] numbers) {
         return Arrays.stream(numbers)
-                .mapToInt(Integer::parseInt)
-                .boxed()
+                .map(Number::new)
                 .collect(Collectors.toList());
-    }
-
-    private void checkNegativeNumber(List<Integer> numbers) {
-        long count = numbers.stream()
-                .filter(number -> number < NEGATIVE_NUMBER_ZERO)
-                .count();
-
-        if (count > NEGATIVE_NUMBER_ZERO) {
-            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR_MESSAGE);
-        }
     }
 
     public int sum() {
         return number.stream()
-                .mapToInt(Integer::intValue)
+                .mapToInt(number -> number.value())
                 .sum();
     }
 }
