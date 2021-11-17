44e38fb9812f30e86106da78de02f840f28870a2
audgnssweet <audgnssweet@naver.com>
2021-11-17 14:11:26 +0900

    test: expression 테스트코드 작성

--- src/main/java/step1/Expression.java
+++ src/main/java/step1/Expression.java
@@ -8,8 +8,8 @@ public class Expression {
     private static final String STANDARD_REGEX = ",|:";
     private static final String CUSTOM_CHECK_REGEX = "//(.)\n(.*)";
     private static final String CUSTOM_REGEX = ",|:|\\%s";
-    private static final int CUSTOM_DELIMITER_INDEX = 1;
-    private static final int CUSTOM_EXPRESSION_INDEX = 2;
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
+    private static final int CUSTOM_EXPRESSION_GROUP = 2;
 
     private Numbers numbers;
 
@@ -29,11 +29,10 @@ public class Expression {
         String[] stringNumbers = input.split(STANDARD_REGEX);
 
         Matcher matcher = Pattern.compile(CUSTOM_CHECK_REGEX).matcher(input);
-
         if (matcher.find()) {
-            String customDelim = matcher.group(CUSTOM_DELIMITER_INDEX);
-            String regex = String.format(CUSTOM_REGEX, customDelim);
-            stringNumbers = matcher.group(CUSTOM_EXPRESSION_INDEX).split(regex);
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_GROUP);
+            String regex = String.format(CUSTOM_REGEX, customDelimiter);
+            stringNumbers = matcher.group(CUSTOM_EXPRESSION_GROUP).split(regex);
         }
 
         return stringNumbers;
--- src/test/java/step1/ExpressionTest.java
+++ src/test/java/step1/ExpressionTest.java
@@ -1,5 +1,7 @@
 package step1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class ExpressionTest {
@@ -7,6 +9,15 @@ public class ExpressionTest {
     @Test
     void expression() {
         Expression expression = new Expression("1,2:3");
+        assertEquals(new Number(6), expression.sum());
     }
 
+    @Test
+    void sum() {
+        Expression expression = new Expression("1,2:3");
+        assertEquals(new Number(6), expression.sum());
+
+        Expression customDelimiterExpression = new Expression("//;\n1;2;3");
+        assertEquals(new Number(6), customDelimiterExpression.sum());
+    }
 }
