270a5fb68ff93eb2d4c093d0ff799ed4d68faeee
ChangmoKang <dkssudckdah@gmail.com>
2021-11-18 22:49:23 +0900

    refactor: 불필요한 객체 생성 제거

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -8,16 +8,19 @@ import java.util.stream.Stream;
 public class LottoGenerator {
     private static final int TOTAL_LOTTO_NUMBER_COUNT = 45;
     private static final int DEFAULT_SELECT_COUNT = 6;
+    private static final List<LottoNumber> LOTTO_NUMBERS;
 
-    public static List<LottoNumber> autoMode() {
-        final List<LottoNumber> allLottoNumbers = Stream.iterate(1, i -> i + 1)
+    static {
+        LOTTO_NUMBERS = Stream.iterate(1, i -> i + 1)
                 .limit(TOTAL_LOTTO_NUMBER_COUNT)
                 .map(LottoNumber::new)
                 .collect(Collectors.toList());
+    }
 
-        Collections.shuffle(allLottoNumbers);
+    public static List<LottoNumber> autoMode() {
+        Collections.shuffle(LOTTO_NUMBERS);
 
-        return allLottoNumbers.stream()
+        return LOTTO_NUMBERS.stream()
                 .limit(DEFAULT_SELECT_COUNT)
                 .sorted()
                 .collect(Collectors.toList());
