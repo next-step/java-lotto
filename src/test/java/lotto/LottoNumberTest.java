5439c0cfe1a147e9500dab59164d3ddf7004d89f
ChangmoKang <dkssudckdah@gmail.com>
2021-11-18 14:18:56 +0900

    refactor: LottoNumber ctor test 수정

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -2,26 +2,33 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberTest {
     @ParameterizedTest
-    @ValueSource(ints = {1, 27, 45})
+    @MethodSource("constructorMethodSource")
     @DisplayName("로또번호는 1이상 45이하의 범위를 갖는다.")
-    void 로또번호생성_성공(int number) {
-        LottoNumber lottoNumber = new LottoNumber(number);
-        assertThat(lottoNumber).isEqualTo(new LottoNumber(number));
+    void constructorMethod(int fail, int success) {
+        LottoNumber lottoNumber = new LottoNumber(success);
+
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(success));
+
+        assertThatThrownBy(() -> {
+            new LottoNumber(fail);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 46})
-    @DisplayName("로또번호는 1이상 45이하의 범위를 갖는다.")
-    void 로또번호생성_실패(int number) {
-        assertThatThrownBy(() -> {
-            new LottoNumber(number);
-        }).isInstanceOf(IllegalArgumentException.class);
+    static Stream<Arguments> constructorMethodSource() {
+        return Stream.of(
+                Arguments.of(-1, 1),
+                Arguments.of(0, 15),
+                Arguments.of(46, 45)
+        );
     }
 }
