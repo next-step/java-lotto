f47b326ed94d9d01c2ae95566b89ed7539e050cd
ChangmoKang <dkssudckdah@gmail.com>
2021-11-18 13:27:51 +0900

    test: of 메소드 테스트 작성

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -2,6 +2,11 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static lotto.Prize.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,5 +30,24 @@ public class PrizeTest {
         assertThat(FOURTH).isGreaterThan(FIFTH);
         assertThat(FIFTH).isGreaterThan(SIXTH);
         assertThat(SIXTH).isGreaterThan(LOSE);
+    }
+
+    @ParameterizedTest
+    @MethodSource("ofSource")
+    @DisplayName("맞은 번호에 따라 상금을 리턴할 수 있는지")
+    void of(int hitCount, Prize result) {
+        assertThat(Prize.of(hitCount)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> ofSource() {
+        return Stream.of(
+                Arguments.of(6, FIRST),
+                Arguments.of(5, SECOND),
+                Arguments.of(4, THIRD),
+                Arguments.of(3, FOURTH),
+                Arguments.of(2, FIFTH),
+                Arguments.of(1, SIXTH),
+                Arguments.of(0, LOSE)
+        );
     }
 }
