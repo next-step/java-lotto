d87643c4996ea12c6f60b74ba22c13074c909e81
marty.ni <marty.ni@kakaocorp.com>
2021-11-19 00:00:59 +0900

    Refactor : main method 분리

--- src/main/java/lotto/main.java
+++ src/main/java/lotto/main.java
@@ -15,31 +15,55 @@ import lotto.vo.WinningHistory;
 public class main {
 
     public static void main(String[] args) {
-
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        Money money = inputView.inputMoney();
 
-        NumberGenerateStrategy strategy = new RandomLottoNumberGenerateStrategy();
-        LottoService lottoService = new LottoService(money, strategy);
+        LottoService lottoService = getLottoService(inputView);
 
-        Lottos lottos = lottoService.buyLotto();
+        buyLotto(outputView, lottoService);
 
-        outputView.renderWithLottos(lottos);
+        WinningHistory winningHistory = startLottoService(inputView, lottoService);
 
-        String winningLottoString = inputView.inputWinningLotto();
+        outputView.renderWithWinningHistory(winningHistory, WinningRank.getReverseRankListWithoutNoRank());
 
-        Lotto winningLotto = Lotto.create(winningLottoString);
+    }
 
-        String bonusBallString = inputView.inputBonus();
+    private static WinningHistory startLottoService(InputView inputView, LottoService lottoService) {
+        Lotto winningLotto = getWinningLotto(inputView);
 
-        LottoNumber lottoNumber = LottoNumber.create(bonusBallString);
+        LottoNumber lottoNumber = getBonusBall(inputView);
 
         lottoService.validBonus(winningLotto, lottoNumber);
 
         WinningHistory winningHistory = lottoService.getWinningHistory(winningLotto, lottoNumber);
+        return winningHistory;
+    }
 
-        outputView.renderWithWinningHistory(winningHistory, WinningRank.getReverseRankListWithoutNoRank());
+    private static LottoNumber getBonusBall(InputView inputView) {
+        String bonusBallString = inputView.inputBonus();
 
+        LottoNumber lottoNumber = LottoNumber.create(bonusBallString);
+        return lottoNumber;
+    }
+
+    private static Lotto getWinningLotto(InputView inputView) {
+        String winningLottoString = inputView.inputWinningLotto();
+
+        Lotto winningLotto = Lotto.create(winningLottoString);
+        return winningLotto;
+    }
+
+    private static void buyLotto(OutputView outputView, LottoService lottoService) {
+        Lottos lottos = lottoService.buyLotto();
+
+        outputView.renderWithLottos(lottos);
+    }
+
+    private static LottoService getLottoService(InputView inputView) {
+        Money money = inputView.inputMoney();
+
+        NumberGenerateStrategy strategy = new RandomLottoNumberGenerateStrategy();
+        LottoService lottoService = new LottoService(money, strategy);
+        return lottoService;
     }
 }
