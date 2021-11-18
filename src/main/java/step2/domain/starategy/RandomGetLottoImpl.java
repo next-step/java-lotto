05295ef6e05e62429c297637545ffab68981e705
mj950425 <minjoon1995@naver.com>
2021-11-19 00:23:44 +0900

    [refactoring] 상수는 대문자로 수정

--- src/main/java/step2/domain/starategy/RandomGetLottoImpl.java
+++ src/main/java/step2/domain/starategy/RandomGetLottoImpl.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static step2.domain.Lotto.NumberList;
+import static step2.domain.Lotto.BOUND_NUMBER;
 import static step2.domain.Lotto.SIZE;
 
 public class RandomGetLottoImpl implements GetLottoNumberStrategy{
 
     @Override
     public List<Integer> getLotto() {
-        Collections.shuffle(NumberList);
-        List<Integer> lottoNumbers = new ArrayList<>(NumberList.subList(0, SIZE));
+        Collections.shuffle(BOUND_NUMBER);
+        List<Integer> lottoNumbers = new ArrayList<>(BOUND_NUMBER.subList(0, SIZE));
         return lottoNumbers;
     }
 }
