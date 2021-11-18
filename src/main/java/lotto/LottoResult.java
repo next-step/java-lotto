5af611121dbf0c2147b3951c7eaef651feb415f1
ChangmoKang <dkssudckdah@gmail.com>
2021-11-18 16:28:19 +0900

    feat & test: LottoResult 테객체 생성

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,2 +1,64 @@
-package lotto;public class LottoResult {
+package lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class LottoResult {
+    private final Map<Prize, Integer> result;
+
+    private static final int ZERO = 0;
+    private static final int DEFAULT_INCREMENT = 1;
+
+    public LottoResult(List<Lotto> lottos, Lotto target) {
+        this.result = getResult(lottos, target);
+    }
+
+    private Map<Prize, Integer> getResult(List<Lotto> lottos, Lotto target) {
+        Map<Prize, Integer> result = new HashMap<>();
+
+        for (Lotto lotto : lottos) {
+            Prize prize = Prize.of(lotto.result(target));
+
+            result.put(prize, incrementValue(result, prize));
+        }
+
+        return result;
+    }
+
+    private int incrementValue(Map<Prize, Integer> result, Prize prize) {
+        return result.containsKey(prize)
+                ? result.get(prize) + DEFAULT_INCREMENT
+                : DEFAULT_INCREMENT;
+    }
+
+    public int result(Prize prize) {
+        if (this.result.containsKey(prize)) {
+            return this.result.get(prize);
+        }
+
+        return ZERO;
+    }
+
+    public double yield() {
+        final double totalPrize = this.result.keySet().stream()
+                .mapToDouble(prize -> prize.money() * this.result.get(prize))
+                .sum();
+
+        return totalPrize / this.result.values().stream().mapToInt(i -> i).sum();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoResult that = (LottoResult) o;
+        return Objects.equals(result, that.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result);
+    }
 }
