a7baa02247832627f4f8ced98cfc33d4a9c3057c
mj950425 <minjoon1995@naver.com>
2021-11-18 23:44:19 +0900

    [refactoring] Lotto exception 추가

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -14,11 +14,31 @@ public class Lotto {
 
     public static final int SIZE = 6;
 
-    public static List<Integer> NumberList = IntStream.range(1, 46).boxed().collect(Collectors.toList());
+    private static Integer BOUND_START = 1;
+
+    private static Integer BOUND_END = 45;
+
+    public static final List<Integer> BOUND_NUMBER = IntStream.rangeClosed(BOUND_START, BOUND_END)
+            .boxed()
+            .collect(Collectors.toList());
 
     public Lotto(GetLottoNumberStrategy getLottoNumberStrategy) {
+        List<Integer> lottoNumbers = getLottoNumberStrategy.getLotto();
+        checkSize(lottoNumbers);
+        checkDistinct(lottoNumbers);
         this.lottoNumbers = getLottoNumberStrategy.getLotto();
+    }
 
+    public void checkSize(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != SIZE) {
+            throw new IllegalArgumentException(SIZE + " 와 길이가 다른 Lotto 는 입력될 수 없습니다.");
+        }
+    }
+
+    public void checkDistinct(List<Integer> lottoNumbers) {
+        if (lottoNumbers.stream().distinct().collect(Collectors.toList()).size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException("로또에 중복된 숫자가 존재합니다.");
+        }
     }
 
     public List<Integer> getLottoNumbers() {
@@ -26,7 +46,9 @@ public class Lotto {
     }
 
     public Integer checkMatching(List<Integer> winningNumbers) {
-        Long count = lottoNumbers.stream().filter(i->winningNumbers.contains(i)).count();
+        Long count = lottoNumbers.stream()
+                .filter(number -> winningNumbers.contains(number))
+                .count();
         return count.intValue();
     }
 }
--- src/test/java/step2/LottoTest.java
+++ src/test/java/step2/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import step2.domain.Lotto;
 
 import static org.assertj.core.api.Assertions.*;
+import static step2.domain.Lotto.SIZE;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,13 +15,46 @@ public class LottoTest {
     @DisplayName("print 로 랜덤 값이 들어간 배열이 잘 생성되는지 확인")
     void checkMatchingTest() {
         List<Integer> testList = Arrays.asList(1, 2, 3, 4, 5, 6);
+
         Lotto lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6));
         assertThat(lotto.checkMatching(testList)).isEqualTo(6);
 
         Lotto lotto2 = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 7));
         assertThat(lotto2.checkMatching(testList)).isEqualTo(5);
 
-        Lotto lotto3 = new Lotto(() -> Arrays.asList(1, 2, 3, 7, 8, 9));
-        assertThat(lotto3.checkMatching(testList)).isEqualTo(3);
+        Lotto lotto3 = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 8, 9));
+        assertThat(lotto3.checkMatching(testList)).isEqualTo(4);
+
+        Lotto lotto4 = new Lotto(() -> Arrays.asList(7, 8, 9, 4, 5, 6));
+        assertThat(lotto4.checkMatching(testList)).isEqualTo(3);
+
+        Lotto lotto5 = new Lotto(() -> Arrays.asList(1, 8, 9, 7, 11, 6));
+        assertThat(lotto5.checkMatching(testList)).isEqualTo(2);
+
+        Lotto lotto6 = new Lotto(() -> Arrays.asList(20, 21, 4, 7, 11, 15));
+        assertThat(lotto6.checkMatching(testList)).isEqualTo(1);
+
+        Lotto lotto7 = new Lotto(() -> Arrays.asList(20, 21, 43, 7, 11, 15));
+        assertThat(lotto7.checkMatching(testList)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("입력된 lotto 길이가 SIZE 보다 클 때 exception 테스트")
+    void sizeCheckTest() {
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining(SIZE + " 와 길이가 다른 Lotto 는 입력될 수 없습니다.");
+
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 5));
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining(SIZE + " 와 길이가 다른 Lotto 는 입력될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("입력된 lotto 에 중복된 숫자가 있을 때 exception 테스트")
+    void distinctCheckTest() {
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(() -> Arrays.asList(1, 2, 3, 4, 6, 6));
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("로또에 중복된 숫자가 존재합니다.");
     }
 }
