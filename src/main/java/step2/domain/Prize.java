c81297abdbfab41154d621cffeaa938c7dbd1ca6
mj950425 <minjoon1995@naver.com>
2021-11-19 12:49:14 +0900

    [refacotring] 가독성좋게 3자리씩 끊어서 정수 표현

--- src/main/java/step2/domain/Prize.java
+++ src/main/java/step2/domain/Prize.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Prize {
-    public static List<Integer> prizeList = Arrays.asList(0,0,0,5000, 50000, 1500000, 2000000000);
 
-    public static double calculatorYield(List<Integer> matchingList,Integer purchaseAmount) {
+    private Prize() {
+    }
+
+    public static List<Integer> prizeList = Arrays.asList(0, 0, 0, 5_000, 50_000, 1_500_000, 2_000_000_000);
+
+    public static double calculatorYield(List<Integer> matchingList, Integer purchaseAmount) {
         double resultPrize = 0L;
         for (int i = 0; i < matchingList.size(); i++) {
             resultPrize += matchingList.get(i) * prizeList.get(i);
