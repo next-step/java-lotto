842656b89091a225d53187f67008f6254a3e144a
mj950425 <minjoon1995@naver.com>
2021-11-19 16:27:58 +0900

    [refactoring] 생활 체조에 의거 메소드 순서 변경 및 변수 네이밍 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,44 +7,43 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Lotto {
-
-    private final List<Integer> lottoNumbers;
-
     public static final int SIZE = 6;
 
     private static Integer BOUND_START = 1;
 
     private static Integer BOUND_END = 45;
 
-    public static final List<Integer> BOUND_NUMBER = IntStream.rangeClosed(BOUND_START, BOUND_END)
+    public static final List<Integer> BOUND_NUMBERS = IntStream.rangeClosed(BOUND_START, BOUND_END)
             .boxed()
             .collect(Collectors.toList());
+
+    private final List<Integer> lottoNumbers;
 
     public Lotto(GetLottoNumberStrategy getLottoNumberStrategy) {
         List<Integer> lottoNumbers = getLottoNumberStrategy.getLotto();
         checkSize(lottoNumbers);
         checkDistinct(lottoNumbers);
-        this.lottoNumbers = getLottoNumberStrategy.getLotto();
+        this.lottoNumbers = lottoNumbers;
     }
 
-    public void checkSize(List<Integer> lottoNumbers) {
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    private void checkSize(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != SIZE) {
             throw new IllegalArgumentException(SIZE + " 와 길이가 다른 Lotto 는 입력될 수 없습니다.");
         }
     }
 
-    public void checkDistinct(List<Integer> lottoNumbers) {
+    private void checkDistinct(List<Integer> lottoNumbers) {
         Integer compareSize = lottoNumbers.stream()
                 .distinct()
                 .collect(Collectors.toList())
                 .size();
-        if ( compareSize != lottoNumbers.size()) {
+        if (compareSize != lottoNumbers.size()) {
             throw new IllegalArgumentException("로또에 중복된 숫자가 존재합니다.");
         }
-    }
-
-    public List<Integer> getLottoNumbers() {
-        return lottoNumbers;
     }
 
     public Integer checkMatching(List<Integer> winningNumbers) {
