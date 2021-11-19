074278fc9e87612e304cebea4959b9bd42fd93bf
mj950425 <minjoon1995@naver.com>
2021-11-19 12:51:53 +0900

    [refactoring] scanner 상수는 대문자로

--- src/main/java/step2/view/Input.java
+++ src/main/java/step2/view/Input.java
@@ -7,21 +7,21 @@ import java.util.stream.Collectors;
 
 public class Input {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner SCANNER = new Scanner(System.in);
 
     private static Integer PRICE_PER_LOTTO = 1000;
 
     public static Integer inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        Integer purchaseAmount = scanner.nextInt();
+        Integer purchaseAmount = SCANNER.nextInt();
         System.out.println(purchaseAmount / PRICE_PER_LOTTO + "개를 구매했습니다.");
         return purchaseAmount;
     }
 
     public static List<Integer> inputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요");
-        scanner.nextLine();
-        return Arrays.asList(scanner.nextLine().split(","))
+        SCANNER.nextLine();
+        return Arrays.asList(SCANNER.nextLine().split(","))
                 .stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
