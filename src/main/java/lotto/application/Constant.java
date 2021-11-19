a1307460d67cf3998b2f54bca8e6e50dd4c2116b
las9897 <las9897@gmail.com>
2021-11-19 17:26:23 +0900

    feat: Lotto 상수 클래스 추가

---
+++ src/main/java/lotto/application/Constant.java
@@ -0,0 +1,13 @@
+package lotto.application;
+
+public class Constant {
+
+    public static final String INPUT_MESSAGE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_MESSAGE_WINNER_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+
+    public static final String OUTPUT_MESSAGE_PURCHASE_LOTTO = "개를 구매했습니다.";
+
+    private Constant() {
+    }
+
+}
