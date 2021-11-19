bfc5ba08cc27c0a778cc21d98ef2a4a3ab7de7a6
ChangmoKang <dkssudckdah@gmail.com>
2021-11-19 15:30:44 +0900

    refactor: 초기화

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,28 +1,2 @@
-package lotto;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-public class LottoGenerator {
-    private static final int TOTAL_LOTTO_NUMBER_COUNT = 45;
-    private static final int DEFAULT_SELECT_COUNT = 6;
-    private static final List<LottoNumber> LOTTO_NUMBERS;
-
-    static {
-        LOTTO_NUMBERS = Stream.iterate(1, i -> i + 1)
-                .limit(TOTAL_LOTTO_NUMBER_COUNT)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
-    }
-
-    public static List<LottoNumber> autoMode() {
-        Collections.shuffle(LOTTO_NUMBERS);
-
-        return LOTTO_NUMBERS.stream()
-                .limit(DEFAULT_SELECT_COUNT)
-                .sorted()
-                .collect(Collectors.toList());
-    }
+package lotto;public class LottoGenerator {
 }
