a20382cd50835ac6c1799bc1b0a8ac36b5258fab
las9897 <las9897@gmail.com>
2021-11-19 17:26:38 +0900

    feat: 공통 Input 클래스 추가

---
+++ src/main/java/common/view/InputView.java
@@ -0,0 +1,22 @@
+package common.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static String nextLine(String message) {
+        OutputView.print(message);
+        return SCANNER.nextLine();
+    }
+
+    public static int nextInt(String message) {
+        OutputView.print(message);
+        return SCANNER.nextInt();
+    }
+
+}
