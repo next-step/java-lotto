e709c73d1b2cb1c4bce8875c072f20944e56cd83
user <kth4878@naver.com>
2021-11-20 18:51:02 +0900

    [Test] 불필요 테스트 제거

--- src/test/java/domain/LotteryTicketsTest.java
+++ src/test/java/domain/LotteryTicketsTest.java
@@ -1,6 +1,5 @@
 package domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,22 +12,13 @@ class LotteryTicketsTest {
     @BeforeEach
     public void setup() {
         Store store = new Store(3000);
-        lotteryTickets = new LotteryTickets(store);
+        lotteryTickets = new LotteryTickets(store.lotteryCount());
     }
 
     @Test
-    @DisplayName("복권 여러개 발행")
+    @DisplayName("복권 여러개 확인")
     public void createIssueLotteryTicket() {
         assertThat(lotteryTickets.size()).isEqualTo(3);
     }
 
-    @Test
-    @DisplayName("발행된 로또 번호가 6개인지 확인")
-    public void checkLotteryNumber() {
-        Assertions.assertAll(
-                () -> assertThat(lotteryTickets.lotteryTicket(0).size()).isEqualTo(6),
-                () -> assertThat(lotteryTickets.lotteryTicket(1).size()).isEqualTo(6),
-                () -> assertThat(lotteryTickets.lotteryTicket(2).size()).isEqualTo(6)
-        );
-    }
 }
