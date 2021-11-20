0088026a9d6d89c5b2b328ff4b0626f98a0abc88
supniverse <kmsup2@gmail.com>
2021-11-21 01:41:31 +0900

    feature: Numbers 추가

---
+++ src/main/java/calculator/Numbers.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import java.util.List;
+
+public class Numbers {
+
+  private List<Number> numberList;
+
+  public Numbers(List<Number> numberList) {
+    this.numberList = numberList;
+  }
+
+  public int continuousSum() {
+    return this.numberList.stream()
+               .reduce((number, number2) -> number.sum(number2))
+               .get().getValue();
+  }
+
+  public boolean isSizeEquals(int size) {
+    return this.numberList.size() == size;
+  }
+
+  public int getValue(int index) {
+    return this.numberList.get(index).getValue();
+  }
+
+}
