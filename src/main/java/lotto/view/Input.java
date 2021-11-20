849d3972d49ef4029b185d3c65bbfad00b98722a
alsqhd <kiryuinh32@gmail.com>
2021-11-20 23:28:43 +0900

    view: InputView 클래스 구현

---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,23 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class Input {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ENTER_NUMBER = "구입금액을 입력해 주세요.";
+
+    private Input() {
+    }
+
+    public static int askPurchaseAmount() {
+        System.out.println(ASK_PURCHASE_AMOUNT);
+        try {
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println(ENTER_NUMBER);
+            return askPurchaseAmount();
+        }
+    }
+
+}
