0eb3bed1771bae168102bfb46722d494412fc0bf
supniverse <kmsup2@gmail.com>
2021-11-21 01:35:54 +0900

    test: tdd를 위한 테스트 코드 개선

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,31 +22,33 @@ public class StringAddCalculatorTest {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = "1")
-  void splitAndSum_숫자하나(String value) throws Exception {
+  @CsvSource(value = {"1=1"}, delimiter = '=')
+  @DisplayName("입력된 숫자가 하나일 경우에는 그 숫자를 그대로 반환한다.")
+  void splitAndSum_숫자하나(String value, int expected) throws Exception {
     int result = StringAddCalculator.splitAndSum(value);
-    assertThat(result).isEqualTo(1);
+    assertThat(result).isEqualTo(expected);
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"1,2"})
-  void splitAndSum_쉼표구분자(String value) throws Exception {
+  @CsvSource(value = {"1,2=3"}, delimiter = '=')
+  @DisplayName("쉼표를 구분자로 사용하여 계산한다. 결과값은 3이다.")
+  void splitAndSum_쉼표구분자(String value, int expected) throws Exception {
     int result = StringAddCalculator.splitAndSum(value);
-    assertThat(result).isEqualTo(3);
+    assertThat(result).isEqualTo(expected);
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"1,2:3"})
-  void splitAndSum_쉼표_또는_콜론_구분자(String value) throws Exception {
+  @CsvSource(value = {"1,2:3=6"}, delimiter = '=')
+  @DisplayName("쉼표 혹은 콜론을 구분자로 넣어준다. 결과값은 6이다.")
+  void splitAndSum_쉼표_또는_콜론_구분자(String value, int expected) throws Exception {
     int result = StringAddCalculator.splitAndSum(value);
-    assertThat(result).isEqualTo(6);
+    assertThat(result).isEqualTo(expected);
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"//;\n1;2;3"})
+  @Test
   @DisplayName("커스텀 구분자로 ;를 사용하여 계산한다. 결과값은 6이다.")
-  void splitAndSum_custom_구분자(String value) throws Exception {
-    int result = StringAddCalculator.splitAndSum(value);
+  void splitAndSum_custom_구분자() throws Exception {
+    int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
     assertThat(result).isEqualTo(6);
   }
 
