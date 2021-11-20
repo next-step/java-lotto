51c442624790c4af884fbcd7911f99a09af50d8e
user <kth4878@naver.com>
2021-11-20 18:37:12 +0900

    [Refactor] UI기능 제거

--- src/main/java/domain/LastWeekLotteryNumber.java
+++ src/main/java/domain/LastWeekLotteryNumber.java
@@ -1,49 +1,23 @@
 package domain;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LastWeekLotteryNumber {
+    private final Set<LotteryNumber> lastWeekLotteryNumber;
+
     private static final String LOTTERY_NUMBER_LACK_ERROR_MESSAGE = "error : 로또 번호는 중복되지 않는 번호 6개 입니다.";
-    private static final String NULL_ERROR_MESSAGE = "error : Null 값은 입력할수 없습니다.";
-    private static final String EMPTY_ERROR_MESSAGE = "error : 공백 은 입력할수 없습니다.";
     private static final int LOTTERY_NUMBER_SIX = 6;
-    private static final String COMMA = ",";
-    private final Set<Number> lastWeekLotteryNumber;
 
-    public LastWeekLotteryNumber(String lastWeekLotteryNumber) {
-        checkTextNull(lastWeekLotteryNumber);
-        checkTextEmpty(lastWeekLotteryNumber);
-        this.lastWeekLotteryNumber = Collections.unmodifiableSet(lastWeekLottery(lastWeekLotteryNumber));
+    public LastWeekLotteryNumber(Set<Integer> numbers) {
+        this.lastWeekLotteryNumber = Collections.unmodifiableSet(lastWeekLottery(numbers));
     }
 
-    private void checkTextNull(String text) {
-        if (Objects.isNull(text)) {
-            throw new IllegalArgumentException(NULL_ERROR_MESSAGE);
-        }
-    }
-
-    private void checkTextEmpty(String text) {
-        if (text.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
-        }
-    }
-
-    public Set<Number> lastWeekLottery(String lastWeekLotteryNumber) {
-        Set<Integer> numbers = removeOverlap(lastWeekLotteryNumber);
-
+    public Set<LotteryNumber> lastWeekLottery(Set<Integer> numbers) {
         validLastWeekLotteryNumber(numbers.size());
 
         return of(numbers);
-    }
-
-    public Set<Integer> removeOverlap(String lastWeekLotteryNumber) {
-        return Arrays.stream(lastWeekLotteryNumber.split(COMMA))
-                .map(number -> Integer.parseInt(number))
-                .collect(Collectors.toSet());
     }
 
     private void validLastWeekLotteryNumber(int count) {
@@ -52,16 +26,16 @@ public class LastWeekLotteryNumber {
         }
     }
 
-    public int lotteryMatch(int number) {
+    public Set<LotteryNumber> of(Set<Integer> numbers) {
+        return numbers.stream()
+                .map(LotteryNumber::new)
+                .collect(Collectors.toSet());
+    }
+
+    public int matchNumber(int number) {
         return (int) lastWeekLotteryNumber.stream()
                 .filter(i -> i.value() == number)
                 .count();
-    }
-
-    public Set<Number> of(Set<Integer> numbers) {
-        return numbers.stream()
-                .map(Number::new)
-                .collect(Collectors.toSet());
     }
 
     public int size() {
