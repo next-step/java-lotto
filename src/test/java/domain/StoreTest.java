7ec49a770bce833305f79ef7098476d59fa0eccf
user <kth4878@naver.com>
2021-11-20 18:51:51 +0900

    [Test] 로또번호 생성 테스트 추가

--- src/test/java/domain/StoreTest.java
+++ src/test/java/domain/StoreTest.java
@@ -4,19 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 class StoreTest {
 
     @Test
-    @DisplayName("로또 구매금액 생성")
-    public void createLotteryPrice() {
+    @DisplayName("로또 구매")
+    public void buyLottery() {
         Store store = new Store(10000);
-        assertThat(store.price()).isEqualTo(10000);
+        assertThat(store.lotteryCount()).isEqualTo(10);
     }
 
     @Test
@@ -30,10 +27,10 @@ class StoreTest {
     }
 
     @Test
-    @DisplayName("나머지 금액 제거")
+    @DisplayName("나머지 금액 제거 확인")
     public void discard() {
         Store store = new Store(1300);
-        assertThat(store.discard()).isEqualTo(1000);
+        assertThat(store.discard(1300)).isEqualTo(1000);
     }
 
     @Test
@@ -44,12 +41,10 @@ class StoreTest {
     }
 
     @Test
-    public void a(){
-        Set<Integer> numbers = new HashSet<>();
-        while (numbers.size() != 6) {
-            numbers.add(RandomNumber.random());
-        }
-
+    @DisplayName("로또 구매후 번호 자동생성")
+    public void lotteryNumbersAutomatically(){
+        LotteryTickets lottery = new Store(1000).lotteryNumbersAutomatically();
+        assertThat(lottery.lotteryTicket(0).size()).isEqualTo(6);
     }
 
 }
