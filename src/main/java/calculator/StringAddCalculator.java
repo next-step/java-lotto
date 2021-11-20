f371c601d20b529dfe235787e4741da6c1f4be36
soo <yunsoo0510@daum.net>
2021-11-21 00:18:23 +0900

    feat : 동작하는 코드 작성

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public int calculate(String text) {
-        return 0;
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        String regex = ",|:";
+        if (text.contains("//") && text.contains("\n")) {
+            int startIdx = text.indexOf("//") + 2;
+            int endIdx = text.indexOf("\n");
+            regex = regex + "|" + text.substring(startIdx, endIdx);
+            text = text.substring(endIdx + 1);
+        }
+
+        String[] split = text.split(regex);
+
+        if (Arrays.stream(split)
+                .mapToInt(Integer::parseInt)
+                .filter(num -> num < 0)
+                .findAny()
+                .isPresent()) {
+            throw new RuntimeException();
+        };
+
+        return Arrays.stream(split).mapToInt(Integer::parseInt).sum();
     }
 }
