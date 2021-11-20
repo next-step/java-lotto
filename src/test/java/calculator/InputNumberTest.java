e9da9a94f9f8677e179b1ec2e7837c1db85aed65
giyeon95 <giyeon15@gmail.com>
2021-11-20 13:40:59 +0900

    test: inputNumberTest 추가 테스트케이스 추가

--- src/test/java/calculator/InputNumberTest.java
+++ src/test/java/calculator/InputNumberTest.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputNumberTest {
 
-    @Test
-    @DisplayName(", :를 기준으로 문자열이 잘 구분되는지 확인한다.")
-    void splitTest() {
-        String input = "1,2:3";
 
+    @ValueSource(strings = "1,2:3")
+    @ParameterizedTest(name = "{argumentsWithNames}, :를 기준으로 문자열이 잘 구분되는지 확인한다.")
+    void splitTest(String input) {
         InputNumber inputNumber = InputNumber.create(input);
         assertThat(inputNumber).isEqualTo(new InputNumber(Arrays.asList(1, 2, 3)));
     }
@@ -28,4 +28,15 @@ public class InputNumberTest {
         assertThat(inputNumber).isEqualTo(new InputNumber());
 
     }
+
+    @Test
+    @DisplayName(value = "숫자 하나를 입력할 경우 해당 숫자를 반환한다.")
+    @SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
+    void splitOneStringTest() {
+        String input = "1";
+        InputNumber inputNumber = InputNumber.create(input);
+
+        assertThat(inputNumber).isEqualTo(new InputNumber(Arrays.asList(1)));
+    }
+
 }
