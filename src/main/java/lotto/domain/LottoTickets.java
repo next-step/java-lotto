fc8ab136c45b8928cdcee2873a3667edad025162
DongHyo <kdhyo98@gmail.com>
2021-11-20 18:02:37 +0900

    feat : 로또일급컬랙션 구현

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LottoTickets {
+
+    private final Set<LottoTicket> tickets;
+
+    public LottoTickets() {
+        this.tickets = new HashSet<>();
+    }
+
+    public LottoTickets autoLottoTicket(Integer ticketCount) {
+        while (tickets.size() != ticketCount) {
+            tickets.add(LottoTicket.of(new HashSet<>(AutoTicket.ticket())));
+        }
+        return this;
+    }
+
+    public Integer size() {
+        return tickets.size();
+    }
+
+    public Set<LottoTicket> getTickets() {
+        return Collections.unmodifiableSet(tickets);
+    }
+
+}
