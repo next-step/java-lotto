e8364969e87e416f394982a0bd166e92b2d0e5e5
user <kth4878@naver.com>
2021-11-20 18:43:19 +0900

    [Fix] 계산오류 수정 int -> double

--- src/main/java/domain/MoneyPrize.java
+++ src/main/java/domain/MoneyPrize.java
@@ -5,10 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MoneyPrize {
-    private static final int RANK_NUMBER_MAX = 4;
-    private static final int RANK_NUMBER_MIN = 1;
-    private static final int LOTTERY_PRICE = 1000;
     private final Map<Integer, Integer> moneyPrize;
+
+    private static final int MATCH_NUMBER_MAX = 6;
+    private static final int MATCH_NUMBER_MIN = 3;
+    private static final double LOTTERY_PRICE = 1000;
 
     public MoneyPrize(RankGroup rankGroup) {
         this.moneyPrize = Collections.unmodifiableMap(moneyPrizeByLotteryRank(rankGroup));
@@ -16,31 +17,27 @@ public class MoneyPrize {
 
     private Map<Integer, Integer> moneyPrizeByLotteryRank(RankGroup rankGroup) {
         Map<Integer, Integer> moneyPrize = new HashMap<>();
-        for (int i = RANK_NUMBER_MIN; i <= RANK_NUMBER_MAX; i++) {
+        for (int i = MATCH_NUMBER_MIN; i <= MATCH_NUMBER_MAX; i++) {
             moneyPrize.put(i, LotteryPrizeCalculation.winningAmount(i, rankGroup.count(i)));
         }
 
         return moneyPrize;
     }
 
-    public double lotteryProfit(Store store) {
+    public double lotteryProfit(int lotteryCount) {
         int money = 0;
         for (int rank : moneyPrize.keySet()) {
             money += moneyPrize.get(rank);
         }
 
-        return Math.round(calculate(money, store.lotteryCount()) * 100) / 100.0;
+        return calculateDivide(money, lotteryCount);
     }
 
-    private int calculate(int money, int count) {
+    private double calculateDivide(int money, int count) {
         return money / (count * LOTTERY_PRICE);
     }
 
-    public int value(int key){
+    public int value(int key) {
         return moneyPrize.get(key);
-    }
-
-    public int size(){
-        return moneyPrize.size();
     }
 }
