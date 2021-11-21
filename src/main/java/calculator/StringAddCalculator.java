650a61f6850c96eb819b021e413a182cf203ad48
JaeHan-Kim <hgal8877@gmail.com>
2021-11-21 22:12:31 +0900

    feat : 분리될 숫자들을 담을 일급컬렉션 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,7 +2,16 @@ package calculator;
 
 public class StringAddCalculator {
 
-    public static int splitAndSum(String text) {
-        return 0;
+    public static final int ZREO = 0;
+
+    public static int splitAndSum(final String text) {
+        if (text == null || text.isEmpty()) {
+            return ZREO;
+        }
+
+        StringDelimiter stringDelimiter = new StringDelimiter();
+        String[] texts = stringDelimiter.parse(text);
+        Numbers numbers = Numbers.from(texts);
+        return numbers.sum();
     }
 }
