c6f118435dcaef33523b9dab7875ec054c0a6485
giyeon95 <giyeon15@gmail.com>
2021-11-22 00:09:25 +0900

    refactor: 코드리뷰 반영
    JAVA Code Convention
     - Constant names 적용
    
     ref: `https://google.github.io/styleguide/javaguide.html#s5.2.4-constant-names`

--- src/main/java/calculator/StringSplitter.java
+++ src/main/java/calculator/StringSplitter.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class StringSplitter implements Splitter {
 
-    private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern CUSTOM_REGEX_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     @SuppressWarnings("RegExpSingleCharAlternation")
     private static final String DEFAULT_REGEX = ",|:";
@@ -13,7 +13,7 @@ public class StringSplitter implements Splitter {
 
     @Override
     public String[] split(String input) {
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = CUSTOM_REGEX_PATTERN.matcher(input);
 
         if (matcher.find()) {
             return addRegexAndSplit(matcher);
