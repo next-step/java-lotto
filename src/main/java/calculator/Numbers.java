f5f223f3183583158ced5aa60c9cb48e5950ecba
JaeHan-Kim <hgal8877@gmail.com>
2021-11-21 22:12:49 +0900

    feat : 분리될 숫자들을 담을 일급컬렉션 추가

---
+++ src/main/java/calculator/Numbers.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Numbers {
+    private final List<Number> numbers;
+
+    public static Numbers from(String[] texts) {
+        List<Number> numbers = Stream.of(texts)
+                                    .map(Number::new)
+                                    .collect(Collectors.toList());
+        return new Numbers(numbers);
+    }
+
+    private Numbers(List<Number> numbers) {
+        this.numbers = numbers;
+    }
+
+    public int sum() {
+        return numbers.stream().mapToInt(Number::getValue).sum();
+    }
+}
