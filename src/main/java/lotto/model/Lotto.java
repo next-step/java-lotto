b63df42ff33909ed7bc4f917872aff180d43b8df
dante(이산하) <dante.93@kakaocorp.com>
2021-11-21 17:09:51 +0900

    feat (create lotto and lottoGenerator)

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Number> pickedNumbers;
+
+    public Lotto(List<Number> pickedNumbers) {
+        this.pickedNumbers = pickedNumbers;
+    }
+}
---
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -0,0 +1,34 @@
+package lotto.utils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class LottoNumberGenerator {
+
+    private static final int FIRST_LOTTO_NUMBER = 1;
+    private static final int LAST_LOTTO_NUMBER_BOUND = 46;
+    private static final int COUNT_TO_PICK = 6;
+
+    private LottoNumberGenerator() {
+    }
+
+    public static List<Number> generate() {
+        return pickNumbers(init());
+    }
+
+    private static List<Integer> init() {
+        return IntStream.range(FIRST_LOTTO_NUMBER, LAST_LOTTO_NUMBER_BOUND)
+                        .boxed()
+                        .collect(Collectors.toList());
+    }
+
+    private static List<Number> pickNumbers(List<Integer> allNumbers) {
+        Collections.shuffle(allNumbers);
+        return allNumbers.stream()
+                         .limit(COUNT_TO_PICK)
+                         .sorted()
+                         .collect(Collectors.toList());
+    }
+}
