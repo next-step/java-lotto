6e49ab4794dc4369b5258a1208582add2a1af107
minjoon_lee <minjoon1995@naver.com>
2021-11-21 13:51:37 +0900

    [feature] number bound exception add

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -3,14 +3,21 @@ package lotto.domain;
 import java.util.Objects;
 
 public class Number {
-    private Integer number;
+    private final Integer number;
 
     public Number(int number) {
+        checkBound(number);
         this.number = number;
     }
 
     public Integer getNumber() {
         return number;
+    }
+
+    private void checkBound(Integer number) {
+        if (number > 45) {
+            throw new IllegalArgumentException("45보다 큰 숫자가 입력될 수 없습니다.");
+        }
     }
 
     @Override
--- src/test/java/lotto/NumberTest.java
+++ src/test/java/lotto/NumberTest.java
@@ -1,10 +1,25 @@
 package lotto;
 
 import lotto.domain.Number;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class NumberTest {
     @Test
+    @DisplayName("45")
+    void checkBoundTest() {
+        int boundNumber = 46;
+        assertThatThrownBy(() -> {
+            new Number(boundNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("45보다 큰 숫자가 입력될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Number 클래스 equal 테스트")
     void numberTest() {
+        assertThat(new Number(10)).isEqualTo(new Number(10));
     }
 }
