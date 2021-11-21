105d14b184fc77cfc9ce784167221559506fb562
nano.son <nano.son@kakaocorp.com>
2021-11-21 21:03:40 +0900

    feat: filterCustomDelimiter

--- src/main/java/lotto/FilterCustomDelimiter.java
+++ src/main/java/lotto/FilterCustomDelimiter.java
@@ -1,10 +1,15 @@
 package lotto;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FilterCustomDelimiter {
-    public Collection<String> filter(String rawFormula) {
+    private final Pattern pattern = Pattern.compile("^//(\\D)\n.+");
+    public String filter(String rawFormula) {
+        Matcher matcher = pattern.matcher(rawFormula);
+        if(matcher.find()) {
+            return matcher.group(1);
+        }
         return null;
     }
 }
--- src/test/java/lotto/FilterCustomDelimiterTest.java
+++ src/test/java/lotto/FilterCustomDelimiterTest.java
@@ -1,37 +1,25 @@
 package lotto;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FilterCustomDelimiterTest {
     private final FilterCustomDelimiter dut = new FilterCustomDelimiter();
     @ParameterizedTest
-    @MethodSource("RawStringToDelimiters")
-    void filter(String rawFormula, Collection<String> expectedDelimeters) {
-        assertThat(dut.filter(rawFormula)).containsExactlyElementsOf(expectedDelimeters);
+    @MethodSource("RawStringToDelimiter")
+    void filter(String rawFormula, String expectedDelimeter) {
+        assertThat(dut.filter(rawFormula)).isEqualTo(expectedDelimeter);
     }
 
-    @Test
-    @NullAndEmptySource
-    void whenRawFormulaIsNullOrEmpty(String rawFormula) {
-        assertThatThrownBy(() -> dut.filter(rawFormula)).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    static Stream<Arguments> RawStringToDelimiters() {
+    static Stream<Arguments> RawStringToDelimiter() {
         return Stream.of(
-                Arguments.of("//;\n1;2;3", Arrays.asList(";")),
-                Arguments.of("1;2;3", Collections.emptyList())
+                Arguments.of("//;\n1;2;3", ";"),
+                Arguments.of("1;2;3", null)
         );
     }
 }
