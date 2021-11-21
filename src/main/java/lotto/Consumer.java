8751959ba26bf045328127ebdad7ae8f58458986
jeje <las9897@gmail.com>
2021-11-21 18:47:38 +0900

    feat: main

---
+++ src/main/java/lotto/Consumer.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import common.view.InputView;
+import common.view.OutputView;
+import lotto.application.Constant;
+import lotto.model.Lotto;
+import lotto.model.LottoStats;
+import lotto.model.LottoStore;
+import lotto.model.LottoWinner;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        int purchaseAmount = InputView.nextInt(Constant.INPUT_MESSAGE_PURCHASE_AMOUNT);
+
+        Lotto lotto = LottoStore.ticket(purchaseAmount);
+        OutputView.print(lotto);
+
+        String winnerNumbers = InputView.nextLine(Constant.INPUT_MESSAGE_WINNER_NUMBERS);
+        LottoWinner lottoWinner = new LottoWinner(winnerNumbers);
+
+        LottoStore.match(lotto, lottoWinner);
+
+        LottoStats lottoStats = new LottoStats(lotto, purchaseAmount);
+
+        OutputView.print(lottoStats);
+    }
+
+}
