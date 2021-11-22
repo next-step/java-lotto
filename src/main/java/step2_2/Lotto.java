883c12d08bd7eb0b848b772b4558e3c975581207
audgnssweet <audgnssweet@naver.com>
2021-11-22 18:36:34 +0900

    feat: LottoNumber의 일급 컬렉션 Lotto 생성

---
+++ src/main/java/step2_2/Lotto.java
@@ -0,0 +1,14 @@
+package step2_2;
+
+import java.util.List;
+
+public class Lotto {
+
+    private List<LottoNumber> lottoNumbers;
+
+    public Lotto(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+
+}
