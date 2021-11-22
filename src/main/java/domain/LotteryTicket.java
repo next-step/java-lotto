df52c2bc0c8912e789b7cede63ace6dee0393d0a
user <kth4878@naver.com>
2021-11-23 02:15:27 +0900

    [Refactor] 생성자 외부 노출 변경 및 match 메서드 등 추가

--- src/main/java/domain/LotteryTicket.java
+++ src/main/java/domain/LotteryTicket.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LotteryTicket {
+    private static final int BONUS_BULL_CHECK_NUMBER = 5;
+    private static final int BONUS_BALL_ZERO = 0;
+
     private final List<LotteryNumber> lotteryTicket;
 
     public LotteryTicket(List<Integer> lotteryTicket) {
@@ -17,21 +20,41 @@ public class LotteryTicket {
                 .collect(Collectors.toList());
     }
 
-    public int getLotteryNumber(int index){
+    public int getLotteryNumber(int index) {
         return lotteryTicket.get(index).value();
     }
 
-    public int matchCount(LastWeekLotteryNumber lastWeekLottery) {
-        int count = 0;
-        for (LotteryNumber number : lotteryTicket) {
-            count += lastWeekLottery.matchNumber(number.value());
+    public int matchCount(LastWeekLottery lastWeekLottery) {
+        return (int) lotteryTicket.stream()
+                .filter(number -> checkMatchCount(number.value(), lastWeekLottery))
+                .count();
+    }
+
+    private boolean checkMatchCount(int number, LastWeekLottery lastWeekLottery) {
+        if (lastWeekLottery.matchNumber(number) > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int matchBonusBall(int rank, int bonusBall) {
+        if (rank != BONUS_BULL_CHECK_NUMBER) {
+            return BONUS_BALL_ZERO;
         }
 
-        return count;
+        return (int) lotteryTicket.stream()
+                .filter(number -> validBonusBall(number.value(), bonusBall))
+                .count();
+    }
+
+    private boolean validBonusBall(int lotteryNumber, int bonusBall) {
+        if (lotteryNumber == bonusBall) {
+            return true;
+        }
+        return false;
     }
 
     public int size() {
         return lotteryTicket.size();
     }
-
 }
--- src/main/java/domain/LotteryTickets.java
+++ src/main/java/domain/LotteryTickets.java
@@ -11,14 +11,14 @@ public class LotteryTickets {
         this(IssueLotteryTicket(lotteryCount));
     }
 
-    public LotteryTickets(List<LotteryTicket> lotteryTicket) {
+    private LotteryTickets(List<LotteryTicket> lotteryTicket) {
         this.lotteryTickets = Collections.unmodifiableList(lotteryTicket);
     }
 
     private static List<LotteryTicket> IssueLotteryTicket(int lotteryCount) {
         List<LotteryTicket> lotteryTicket = new ArrayList<>();
         for (int i = 0; i < lotteryCount; i++) {
-            lotteryTicket.add(new LotteryTicket(RandomNumber.sixRandomNumber()));
+            lotteryTicket.add(new LotteryTicket(RandomNumber.bringNumber()));
         }
 
         return lotteryTicket;
@@ -26,6 +26,14 @@ public class LotteryTickets {
 
     public LotteryTicket lotteryTicket(int index) {
         return lotteryTickets.get(index);
+    }
+
+    public int matchLottery(int index, LastWeekLottery lastWeekLottery) {
+        return lotteryTickets.get(index).matchCount(lastWeekLottery);
+    }
+
+    public int matchBonusBoll(int index, int rank, int bonusBall) {
+        return lotteryTickets.get(index).matchBonusBall(rank, bonusBall);
     }
 
     public int size() {
