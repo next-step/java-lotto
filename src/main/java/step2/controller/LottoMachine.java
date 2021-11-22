50b6502963dee69bbde1204aa2992506b5e53dfd
doorisopen <doorwasopen@gmail.com>
2021-11-22 22:51:56 +0900

    feat: step3 보너스 번호를 입력한다

--- src/main/java/step2/controller/LottoMachine.java
+++ src/main/java/step2/controller/LottoMachine.java
@@ -35,8 +35,8 @@ public class LottoMachine {
         System.out.println();
     }
 
-    public void winningResult(Lottos purchasedLottos, String winningNumbers) {
-        WinningResult winningResult = lottoService.winningResult(purchasedLottos, winningNumbers);
+    public void winningResult(Lottos purchasedLottos, String winningNumbers, int bonusNumber) {
+        WinningResult winningResult = lottoService.winningResult(purchasedLottos, winningNumbers, bonusNumber);
         showWinningStatistics(winningResult);
     }
 
