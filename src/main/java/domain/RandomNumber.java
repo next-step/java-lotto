ac15598995d7ecd68653e090e3312cd3b20c583c
user <kth4878@naver.com>
2021-11-23 02:17:20 +0900

    [Refactor] 랜덤값 -> 로또번로 리스트에서 랜덤으로 추출 하는 방식으로 변경

--- src/main/java/domain/RandomNumber.java
+++ src/main/java/domain/RandomNumber.java
@@ -4,17 +4,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Number {
+public class RandomNumber {
     private static final int LOTTERY_START_SIZE = 0;
     private static final int LOTTERY_END_SIZE = 6;
     private static final int LOTTERY_START_NUMBER = 1;
     private static final int LOTTERY_END_NUMBER = 46;
-    private static final String NUMBER_ERROR_MESSAGE = "error : 유효하지 않는 번호 입니다.";
     private static final List<Integer> lotteryBalls = IntStream.range(LOTTERY_START_NUMBER, LOTTERY_END_NUMBER)
             .boxed()
             .collect(Collectors.toList());
 
-    private Number() {
+    private RandomNumber() {
     }
 
     public static List<Integer> bringNumber() {
@@ -24,18 +23,4 @@ public class Number {
                 .sorted()
                 .collect(Collectors.toList());
     }
-
-    private static boolean checkNumberList(int number) {
-        if (number < LOTTERY_START_NUMBER || number >= LOTTERY_END_NUMBER) {
-            return true;
-        }
-        return false;
-    }
-
-    private static void errorNumber(int count) {
-        if (count > 0) {
-            throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
-        }
-    }
-
 }
