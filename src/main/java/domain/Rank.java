78556122e08bd228d8c60780af9da034069e759f
user <kth4878@naver.com>
2021-11-23 02:19:27 +0900

    [Refactor] 보너스볼 관련 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -2,12 +2,18 @@ package domain;
 
 public class Rank {
     private final int rank;
+    private final int bonusBall;
 
-    public Rank(int rank) {
+    public Rank(int rank, int bonusBall) {
         this.rank = rank;
+        this.bonusBall = bonusBall;
     }
 
-    public int value() {
+    public int rank() {
         return rank;
+    }
+
+    public int bonusBall() {
+        return bonusBall;
     }
 }
