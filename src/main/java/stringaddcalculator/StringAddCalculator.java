243aa30d5b019d507b5a985e96e02119048b1947
michael.10 <michael.10@kakaocorp.com>
2021-11-22 22:53:11 +0900

    Feat returning sum of numbers around comma(,)

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -1,10 +1,17 @@
 package stringaddcalculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringAddCalculator {
     public static int splitAndSum(String value) {
         if (value == null || value.trim().isEmpty()) {
             return 0;
         }
-        return 1;
+        List<Integer> numbers = Arrays.stream(value.split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        return numbers.stream().reduce(0 ,Integer::sum);
     }
 }
+
+
