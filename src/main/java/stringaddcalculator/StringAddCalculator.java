94c3b91e7f7a2f5c73dda614378ba178694d5231
michael.10 <michael.10@kakaocorp.com>
2021-11-22 22:46:06 +0900

    Feat returning 0 when input is either null or blank.

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -2,6 +2,9 @@ package stringaddcalculator;
 
 public class StringAddCalculator {
     public static int splitAndSum(String value) {
-        return 0;
+        if (value == null || value.trim().isEmpty()) {
+            return 0;
+        }
+        return 1;
     }
 }
