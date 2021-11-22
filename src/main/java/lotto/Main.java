55112afa18bf3d39b3f31aa12b1092d955930f6b
Han <hankim@tapasmedia.co>
2021-11-22 22:20:57 +0900

    feat : add main

---
+++ src/main/java/lotto/Main.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import lotto.domain.Credit;
+import lotto.domain.PurchaseMachine;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.Scanner;
+
+/**
+ * @author han
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView(new Scanner(System.in));
+        ResultView resultView = new ResultView();
+
+        Credit credit = inputView.start();
+        PurchaseMachine pm = new PurchaseMachine(credit);
+        PurchaseMachine purchase = pm.purchase();
+        resultView.printPurchaseLottos(purchase);
+        PurchaseMachine purchaseMachineWithWonLotto = inputView.insertLastLotto(purchase);
+    }
+}
