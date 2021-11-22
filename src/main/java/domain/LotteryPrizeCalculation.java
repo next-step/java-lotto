249030f0b49f7cc6d641049d692094916b4f843b
user <kth4878@naver.com>
2021-11-23 02:20:37 +0900

    [Refactor] 보너스볼 추가 및 당첨순위 유효성 검사 추가

--- src/main/java/domain/LotteryPrizeCalculation.java
+++ src/main/java/domain/LotteryPrizeCalculation.java
@@ -1,29 +1,31 @@
 package domain;
 
 import java.util.Arrays;
-import java.util.function.UnaryOperator;
 
 public enum LotteryPrizeCalculation {
-    FIRST_RANK_PRIZE(6, (a) -> a * 2_000_000_000),
-    SECOND_RANK_PRIZE(5, (a) -> a * 1_500_000),
-    THIRD_RANK_PRIZE(4, (a) -> a * 50_000),
-    FOURTH_RANK_PRIZE(3, (a) -> a * 5_000),
-    ZERO_RANK_PRIZE(0, (a) -> 0);
-
-    private int rank;
-    private UnaryOperator<Integer> count;
+    FIRST_RANK_PRIZE(6, 0, 2_000_000_000),
+    SECOND_RANK_PRIZE(5, 1, 30_000_000),
+    THIRD_RANK_PRIZE(5, 0, 1_500_000),
+    FOURTH_RANK_PRIZE(4, 0, 50_000),
+    FIFTH_RANK_PRIZE(3, 0, 5_000),
+    ZERO_RANK_PRIZE(0, 0, 0);
 
     private static final String RANK_ERROR_MESSAGE = "error : 당첨 개수를 확인해주세요.";
     private static final int WINNING_NUMBER_MIN = 3;
     private static final int WINNING_NUMBER_ZERO = 0;
 
-    LotteryPrizeCalculation(int rank, UnaryOperator<Integer> winningCount) {
+    private final int rank;
+    private final int bonusBall;
+    private final int money;
+
+    LotteryPrizeCalculation(int rank, int bonusBall, int money) {
         this.rank = rank;
-        this.count = winningCount;
+        this.bonusBall = bonusBall;
+        this.money = money;
     }
 
-    public static int winningAmount(int rank, int count) {
-        return getRank(validWinningNumber(rank)).count.apply(count);
+    public static int winningAmount(int rank, int bonusBall) {
+        return getRank(validWinningNumber(rank), bonusBall).money;
     }
 
     private static int validWinningNumber(int rank) {
@@ -33,10 +35,17 @@ public enum LotteryPrizeCalculation {
         return rank;
     }
 
-    private static LotteryPrizeCalculation getRank(int rank) {
+    private static LotteryPrizeCalculation getRank(int rank, int bonusBall) {
         return Arrays.stream(values())
-                .filter(lottery -> lottery.rank == rank)
+                .filter(lottery -> validRank(lottery.rank, rank, lottery.bonusBall, bonusBall))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(RANK_ERROR_MESSAGE));
+    }
+
+    private static boolean validRank(int lotteryRank, int rank, int lotteryBonus, int bonus) {
+        if (lotteryRank == rank && lotteryBonus == bonus) {
+            return true;
+        }
+        return false;
     }
 }
