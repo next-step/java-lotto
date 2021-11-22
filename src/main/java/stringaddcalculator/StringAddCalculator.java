64451b7eca0c84525ca44bf536871c5d17e5479d
michael.10 <michael.10@kakaocorp.com>
2021-11-22 23:32:40 +0900

    Feat custom delimeter.

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -2,14 +2,36 @@ package stringaddcalculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringAddCalculator {
+    private static final Pattern DELIMETER_PATTERN = Pattern.compile("//(?<delimeter>.)\n");
+    private static final String DEFAULT_DELIMETER = ",|:";
+
     public static int splitAndSum(String value) {
         if (value == null || value.trim().isEmpty()) {
             return 0;
         }
-        List<Integer> numbers = Arrays.stream(value.split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        Matcher matcher = DELIMETER_PATTERN.matcher(value);
+
+        String delimeter = null;
+        String expression = value;
+
+        while (matcher.find()) {
+            delimeter = matcher.group("delimeter");
+            expression = matcher.replaceFirst("");
+        }
+        if (delimeter == null) {
+            delimeter = DEFAULT_DELIMETER;
+        }
+
+        List<Integer> numbers = Arrays.stream(expression.split(delimeter))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
         return numbers.stream().reduce(0 ,Integer::sum);
     }
 }
