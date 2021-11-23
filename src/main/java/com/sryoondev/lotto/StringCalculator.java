023d09765193f7d37f2620f0436426d49522df18
Sarah Yoon <sarah.yoon@kakaocorp.com>
2021-11-23 21:27:21 +0900

    refactor: 구분자 관련 문자들 상수로 분리

--- src/main/java/com/sryoondev/lotto/StringCalculator.java
+++ src/main/java/com/sryoondev/lotto/StringCalculator.java
@@ -4,6 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER_START_KEYWORD = "//";
+    public static final String CUSTOM_DELIMITER_END_KEYWORD = "\n";
+
     public static int splitAndSum(String text) {
         if (isEmptyString(text)) {
             return 0;
@@ -25,23 +29,23 @@ public class StringCalculator {
     }
 
     public static String[] split(String text) {
-        if (text.startsWith("//")) {
+        if (text.startsWith(CUSTOM_DELIMITER_START_KEYWORD)) {
             return splitByCustomDelimiter(text);
         }
         return splitByDefaultDelimiter(text);
     }
 
     public static String[] splitByDefaultDelimiter(String text) {
-        return text.split(",|:");
+        return text.split(DEFAULT_DELIMITER);
     }
 
     public static String[] splitByCustomDelimiter(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_START_KEYWORD + "(.)" +
+                CUSTOM_DELIMITER_END_KEYWORD + "(.*)").matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             return m.group(2).split(customDelimiter);
         }
         throw new RuntimeException("커스텀 구분자가 입력되지 않았습니다.");
     }
-
 }
