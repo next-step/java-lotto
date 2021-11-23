c390c4627c8925c336fc1597bf524e5391bfb392
alsqhd <kiryuinh32@gmail.com>
2021-11-23 13:55:47 +0900

    test: WinningStatisticsTest 테스트 인자 값 변경

--- src/test/java/lotto/domain/WinningStatisticsTest.java
+++ src/test/java/lotto/domain/WinningStatisticsTest.java
@@ -20,7 +20,7 @@ class WinningStatisticsTest {
     @BeforeEach
     void setUp() {
         /*
-        6개 일치
+        6개 일치 - 1등
          */
         List<LottoNumber> lottoNumbers1 = Arrays.asList(
                 LottoNumber.from(1),
@@ -31,7 +31,7 @@ class WinningStatisticsTest {
                 LottoNumber.from(6)
         );
         /*
-        6개 일치
+        6개 일치 - 1등
          */
         List<LottoNumber> lottoNumbers2 = Arrays.asList(
                 LottoNumber.from(1),
@@ -42,7 +42,7 @@ class WinningStatisticsTest {
                 LottoNumber.from(6)
         );
         /*
-        5개 일치
+        5개 일치 - 2등
          */
         List<LottoNumber> lottoNumbers3 = Arrays.asList(
                 LottoNumber.from(1),
@@ -53,18 +53,18 @@ class WinningStatisticsTest {
                 LottoNumber.from(7)
         );
         /*
-        4개 일치
+        5개 일치 - 3등
          */
         List<LottoNumber> lottoNumbers4 = Arrays.asList(
                 LottoNumber.from(1),
                 LottoNumber.from(2),
                 LottoNumber.from(3),
                 LottoNumber.from(4),
-                LottoNumber.from(7),
+                LottoNumber.from(5),
                 LottoNumber.from(8)
         );
         /*
-        4개 일치
+        4개 일치 - 4등
          */
         List<LottoNumber> lottoNumbers5 = Arrays.asList(
                 LottoNumber.from(1),
@@ -75,7 +75,7 @@ class WinningStatisticsTest {
                 LottoNumber.from(9)
         );
         /*
-        3개 일치
+        3개 일치 - 5등
          */
         List<LottoNumber> lottoNumbers6 = Arrays.asList(
                 LottoNumber.from(1),
@@ -86,7 +86,7 @@ class WinningStatisticsTest {
                 LottoNumber.from(9)
         );
         /*
-        2개 일치
+        2개 일치 - NOTHING
          */
         List<LottoNumber> lottoNumbers7 = Arrays.asList(
                 LottoNumber.from(1),
@@ -97,7 +97,7 @@ class WinningStatisticsTest {
                 LottoNumber.from(10)
         );
         /*
-        1개 일치
+        1개 일치 - NOTHING
          */
         List<LottoNumber> lottoNumbers8 = Arrays.asList(
                 LottoNumber.from(1),
@@ -108,7 +108,7 @@ class WinningStatisticsTest {
                 LottoNumber.from(11)
         );
         /*
-        0개 일치
+        0개 일치 - NOTHING
          */
         List<LottoNumber> lottoNumbers9 = Arrays.asList(
                 LottoNumber.from(11),
@@ -155,7 +155,7 @@ class WinningStatisticsTest {
                         Rank.SECOND, 1
                 ),
                 Arguments.of(
-                        Rank.THIRD, 2
+                        Rank.THIRD, 1
                 ),
                 Arguments.of(
                         Rank.FOURTH, 1
@@ -164,7 +164,7 @@ class WinningStatisticsTest {
                         Rank.FIFTH, 1
                 ),
                 Arguments.of(
-                        Rank.NOTHING, 1
+                        Rank.NOTHING, 3
                 )
         );
     }
@@ -192,110 +192,22 @@ class WinningStatisticsTest {
                 ),
                 Lotto.from(
                         Arrays.asList(
+                                LottoNumber.from(7),
+                                LottoNumber.from(8),
+                                LottoNumber.from(9),
                                 LottoNumber.from(10),
                                 LottoNumber.from(11),
-                                LottoNumber.from(12),
-                                LottoNumber.from(13),
-                                LottoNumber.from(14),
-                                LottoNumber.from(15)
+                                LottoNumber.from(12)
                         )
                 ),
                 Lotto.from(
                         Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
-                        )
-                )
-                ,
-                Lotto.from(
-                        Arrays.asList(
-                                LottoNumber.from(20),
-                                LottoNumber.from(21),
-                                LottoNumber.from(22),
-                                LottoNumber.from(23),
-                                LottoNumber.from(24),
-                                LottoNumber.from(25)
+                                LottoNumber.from(13),
+                                LottoNumber.from(14),
+                                LottoNumber.from(15),
+                                LottoNumber.from(16),
+                                LottoNumber.from(17),
+                                LottoNumber.from(18)
                         )
                 )
         );
@@ -304,16 +216,34 @@ class WinningStatisticsTest {
 
         return Stream.of(
                 Arguments.of(
-                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"1", "2", "3", "14", "15", "16"}, LottoNumber.from(17))),
+                        //3개 로또 중 1등 1개
+                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"1", "2", "3", "4", "5", "6"}, LottoNumber.from(7))),
+                        Math.round(2_000_000_000 / (double) 3000)
+                ),
+                Arguments.of(
+                        //3개 로또 중 2등 1개
+                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"1", "2", "3", "4", "5", "8"}, LottoNumber.from(6))),
+                        Math.round(30_000_000 / (double) 3000)
+                ),
+                Arguments.of(
+                        //3개 로또 중 3등 1개
+                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"1", "2", "3", "4", "5", "16"}, LottoNumber.from(17))),
+                        Math.round(1_500_000 / (double) 3000)
+                ),
+                Arguments.of(
+                        //3개 로또 중 4등 1개
+                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"1", "2", "3", "4", "15", "16"}, LottoNumber.from(17))),
+                        Math.round(50_000 / (double) 3000)
+                ),
+                Arguments.of(
+                        //3개 로또 중 5등 1개
+                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"1", "2", "3", "24", "25", "26"}, LottoNumber.from(17))),
+                        Math.round(5_000 / (double) 3000)
+                ),
+                Arguments.of(
+                        //3개 로또 중 NOTING
+                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"21", "22", "23", "24", "25", "26"}, LottoNumber.from(17))),
                         0.0
-                ),
-                Arguments.of(
-                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"10", "11", "12", "14", "15", "16"}, LottoNumber.from(17))),
-                        136.0
-                ),
-                Arguments.of(
-                    WinningStatistics.from(lottos, WinningLotto.from(new String[] {"10", "11", "12", "20", "22", "23"}, LottoNumber.from(17))),
-                        5.0
                 )
         );
     }
