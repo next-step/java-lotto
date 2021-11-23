78ffcd83ac131414bbae90c09cfe5cf07648f9b4
Sarah Yoon <sarah.yoon@kakaocorp.com>
2021-11-23 21:03:28 +0900

    feature: 콜론 구분자 기능 추가

--- src/main/java/com/sryoondev/lotto/StringCalculator.java
+++ src/main/java/com/sryoondev/lotto/StringCalculator.java
@@ -2,7 +2,7 @@ package com.sryoondev.lotto;
 
 public class StringCalculator {
     public static int splitAndSum(String text) {
-        String[] tokens = text.split(",");
+        String[] tokens = text.split(",|:");
         int result = 0;
         for (int i = 0; i < tokens.length; i++) {
             result += Integer.parseInt(tokens[i]);
--- src/test/java/com/sryoondev/lotto/CalculatorTest.java
+++ src/test/java/com/sryoondev/lotto/CalculatorTest.java
@@ -7,12 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
     @Test
-    @DisplayName("쉼표를 구분자로 갖는 문자열을 전달하는 경우 구분자를 기준으로 분리된 각 숫자의 합을 반환")
-    void testCalculator() {
+    void testCalculator_숫자하나() {
         int result = StringCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
+    }
 
-        result = StringCalculator.splitAndSum("1,2");
+    @Test
+    @DisplayName("쉼표를 구분자로 갖는 문자열을 전달하는 경우 구분자를 기준으로 분리된 각 숫자의 합을 반환")
+    void testCalculator_쉼표구분자() {
+        int result = StringCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("콜론를 구분자로 갖는 문자열을 전달하는 경우 구분자를 기준으로 분리된 각 숫자의 합을 반환")
+    void testCalculator_콜론구분자() {
+        int result = StringCalculator.splitAndSum("1:2");
         assertThat(result).isEqualTo(3);
     }
 }
