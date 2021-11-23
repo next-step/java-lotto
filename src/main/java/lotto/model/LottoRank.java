7746631f5747aa9a5bd69524180830a4ad7d19fb
las9897 <las9897@gmail.com>
2021-11-23 19:09:44 +0900

    feat: LottoRank bonus 2등 추가

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -9,9 +9,10 @@ import java.util.stream.Collectors;
 
 public enum LottoRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     MISS(0, 0);
 
     private final int matchCount;
@@ -27,6 +28,21 @@ public enum LottoRank {
                 .filter(r -> matchCount.equals(new Number(r.matchCount)))
                 .findFirst()
                 .orElse(MISS);
+
+    }
+
+    public static LottoRank valueOf(Number matchCount, boolean isBonus) {
+        if (matchCount.equals(new Number(5))) {
+            return secondOrThird(isBonus);
+        }
+        return valueOf(matchCount);
+    }
+
+    private static LottoRank secondOrThird(boolean isBonus) {
+        if (isBonus) {
+            return SECOND;
+        }
+        return THIRD;
     }
 
     public static List<LottoRank> valuesWithoutMiss() {
@@ -36,6 +52,7 @@ public enum LottoRank {
         Collections.reverse(ranksWithoutMiss);
         return ranksWithoutMiss;
     }
+
 
     public int getMatchCount() {
         return matchCount;
