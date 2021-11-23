55c7e784925142faab6c5b466c8f0fd97187194d
DongHyo <kdhyo98@gmail.com>
2021-11-24 02:01:52 +0900

    test : DisplayName 작성

--- src/test/java/lotto/domain/RateTest.java
+++ src/test/java/lotto/domain/RateTest.java
@@ -2,14 +2,16 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RateTest {
 
     @ParameterizedTest
+    @DisplayName("생성성공")
     @CsvSource(value = {"14000.0:15000.0:1.07", "14000.0:5000.0:0.35", "10000.0:10000.0:1.00"}, delimiter = ':')
-    void 생성성공(double money, double prizeMoney, double result) {
+    void create(double money, double prizeMoney, double result) {
         Rate rate = Rate.of(money, prizeMoney);
         assertThat(rate.getRate()).isEqualTo(result);
     }
