e6909a3b864eb2212c555b4737f80980d326a7e2
Sarah Yoon <sarah.yoon@kakaocorp.com>
2021-11-23 21:10:32 +0900

    feature: 커스텀 구분자 기능 추가

--- src/main/java/com/sryoondev/lotto/StringCalculator.java
+++ src/main/java/com/sryoondev/lotto/StringCalculator.java
@@ -1,8 +1,16 @@
 package com.sryoondev.lotto;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public static int splitAndSum(String text) {
         String[] tokens = text.split(",|:");
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            tokens= m.group(2).split(customDelimiter);
+        }
         int result = 0;
         for (String token : tokens) {
             result += Integer.parseInt(token);
