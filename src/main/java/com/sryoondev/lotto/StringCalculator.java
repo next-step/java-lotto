5f06378216fe69352704813c0eb53754381a84dd
Sarah Yoon <sarah.yoon@kakaocorp.com>
2021-11-23 21:23:33 +0900

    refactor: 기본 구분자와 커스텀 구분자 사용 메소드 분리

--- src/main/java/com/sryoondev/lotto/StringCalculator.java
+++ src/main/java/com/sryoondev/lotto/StringCalculator.java
@@ -8,13 +8,7 @@ public class StringCalculator {
         if (isEmptyString(text)) {
             return 0;
         }
-
-        String[] tokens = text.split(",|:");
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            tokens= m.group(2).split(customDelimiter);
-        }
+        String[] tokens = split(text);
         int result = 0;
         for (String token : tokens) {
             int number = Integer.parseInt(token);
@@ -29,4 +23,25 @@ public class StringCalculator {
     public static boolean isEmptyString(String text) {
         return text == null || text.isEmpty();
     }
+
+    public static String[] split(String text) {
+        if (text.startsWith("//")) {
+            return splitByCustomDelimiter(text);
+        }
+        return splitByDefaultDelimiter(text);
+    }
+
+    public static String[] splitByDefaultDelimiter(String text) {
+        return text.split(",|:");
+    }
+
+    public static String[] splitByCustomDelimiter(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        throw new RuntimeException("커스텀 구분자가 입력되지 않았습니다.");
+    }
+
 }
