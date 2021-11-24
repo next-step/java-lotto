0769f6469d6226a350e6573cb090d805b0a1b459
jjoy <ekdnlt1995@gmail.com>
2021-11-24 21:49:45 +0900

    feat : 결과 저장하는 map 생성하는 factory 클래스 만들기

---
+++ src/main/java/lotto/model/domain/ResultMapManager.java
@@ -0,0 +1,19 @@
+package lotto.model.domain;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ResultMapManager {
+
+    private ResultMapManager() {}
+
+    public static Map<Rank, Integer> init() {
+        Map<Rank, Integer> map = new LinkedHashMap<Rank, Integer>();
+        map.put(Rank.FIFTH, 0);
+        map.put(Rank.FOURTH, 0);
+        map.put(Rank.THIRD, 0);
+        map.put(Rank.SECOND, 0);
+        map.put(Rank.FIRST, 0);
+        return map;
+    }
+}
