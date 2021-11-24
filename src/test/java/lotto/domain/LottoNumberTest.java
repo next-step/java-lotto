50aad5fb8fb35685d8c2f008a97571c6495976e8
supniverse <kmsup2@gmail.com>
2021-11-25 00:45:40 +0900

    fix: Lotto 번호 생성을 위한 테스트 수정

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,14 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoNumberTest {
 
-  @Test
-  void create() {
-    LottoNumber lottoNumber = new LottoNumber(30);
-    assertEquals(30, lottoNumber.getNumber());
+  @ParameterizedTest
+  @ValueSource(ints = {1, 15, 20, 37, 42, 45})
+  @DisplayName("1~45까지의 로또번호가 생성되는지 확인한다.")
+  void create(int input) {
+    LottoNumber lottoNumber = new LottoNumber(input);
+    assertEquals(input, lottoNumber.getNumber());
   }
 
   @ParameterizedTest
   @ValueSource(ints = {46, 50, 100, 200})
+  @DisplayName("45를 초과한 번호를 입력했을때 번호가 생성되지 않고 exception을 던진다.")
   void inValidCreateLottoNumber(int input) {
     assertThrows(IllegalArgumentException.class, () -> new LottoNumber(input));
   }
