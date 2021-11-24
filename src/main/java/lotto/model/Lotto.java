db417c3bb5fa174c037d62e3ad3351c26978e29d
las9897 <las9897@gmail.com>
2021-11-24 10:53:45 +0900

    refactor: Lotto 생성 로직 위치 변경

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.model;
 
+import lotto.factory.LottoNumbersFactory;
+import lotto.generator.LottoNumberGenerator;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -8,6 +12,19 @@ public class Lotto {
 
     public Lotto(List<LottoNumbers> lottoNumbers) {
         this.lotto = lottoNumbers;
+    }
+
+    public Lotto(int count) {
+        this.lotto = autoCreateLotto(count);
+    }
+
+    private List<LottoNumbers> autoCreateLotto(int count) {
+        List<LottoNumbers> lotto = new ArrayList<>();
+        LottoNumberGenerator generator = new LottoNumberGenerator();
+        for (int i = 0; i < count; i++) {
+            lotto.add(LottoNumbersFactory.autoCreateNumbers(generator));
+        }
+        return lotto;
     }
 
     public List<LottoNumbers> getLotto() {
--- src/main/java/lotto/model/LottoStore.java
+++ src/main/java/lotto/model/LottoStore.java
@@ -1,7 +1,5 @@
 package lotto.model;
 
-import lotto.factory.LottoFactory;
-
 public class LottoStore {
 
     private static final int LOTTO_PRICE = 1000;
@@ -11,7 +9,7 @@ public class LottoStore {
 
     public static Lotto ticket(int purchaseAmount) {
         int count = purchaseAmount / LOTTO_PRICE;
-        return LottoFactory.autoCreateLotto(count);
+        return new Lotto(count);
     }
 
     public static LottoReport report(Lotto lotto, LottoWinner winner) {
