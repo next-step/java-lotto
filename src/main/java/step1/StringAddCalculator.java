43c42ff5a39f23e7cf412f6e5d0d75c1ab313c6d
hyunju <guswnt@gmail.com>
2021-11-25 23:57:40 +0900

    feat: step1 문자열 계산기에 빈 문자열이나 null를 전달하면 0을 반환한다.

---
+++ src/main/java/step1/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package step1;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String inputStr) {
+        if (inputStr == null || inputStr.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
