c0910ea9caf7cb761d855e4c2a4e5fe0f567f5b0
dante(이산하) <dante.93@kakaocorp.com>
2021-11-25 22:06:18 +0900

    refactor (improve initNumbers to static)

--- src/main/java/lotto/utils/LottoNumberGenerator.java
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -12,18 +12,22 @@ public final class LottoNumberGenerator {
     private static final int FIRST_LOTTO_NUMBER = 1;
     private static final int LAST_LOTTO_NUMBER_BOUND = 46;
     private static final int COUNT_TO_PICK = 6;
+    private static List<Integer> initNumbers;
+
+    static {
+        initNumbers = IntStream.range(FIRST_LOTTO_NUMBER, LAST_LOTTO_NUMBER_BOUND)
+                               .boxed()
+                               .collect(Collectors.toList());
+    }
 
     private LottoNumberGenerator() {
     }
 
     public static List<Number> generate() {
-        return pickNumbers(init());
-    }
-
-    private static List<Integer> init() {
-        return IntStream.range(FIRST_LOTTO_NUMBER, LAST_LOTTO_NUMBER_BOUND)
-                        .boxed()
-                        .collect(Collectors.toList());
+        List<Integer> sortedInitNumbers = initNumbers.stream()
+                                                     .sorted()
+                                                     .collect(Collectors.toList());
+        return pickNumbers(sortedInitNumbers);
     }
 
     private static List<Number> pickNumbers(List<Integer> allNumbers) {
