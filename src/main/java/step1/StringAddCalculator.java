0fb1d76d5c78a26d37b485decb45fe6eb1e949c5
Lee Dongkyu <nelldklee@gmail.com>
2021-11-26 00:14:32 +0900

    feat(StringAddCalculator): 빈 문자열 또는 null  전달 시 RuntimeException 발생 기능 구현

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -7,19 +7,45 @@ public class StringAddCalculator {
 
 
     public static int splitAndSum(String inputData) {
-        String[] splits = inputData.split(",|:");
-        if (splits.length == 1) {
-            return Integer.parseInt(splits[0]);
+        if (isEmpty(inputData)) {
+            return 0;
         }
+
+        String[] splits = inputData.split(",|:");
         List<Integer> numbers = new ArrayList<>();
         int result = 0;
-        for (String number : splits) {
-            numbers.add(Integer.parseInt(number));
+
+        convertIntegers(splits, numbers);
+
+        if (isSizeOne(numbers)) {
+            return numbers.get(0);
         }
+
         for (Integer number : numbers) {
             result += number;
         }
 
         return result;
+    }
+
+    private static boolean isSizeOne(List<Integer> numbers) {
+        return numbers.size() == 1;
+    }
+
+    private static void convertIntegers(String[] splits, List<Integer> numbers) {
+        for (String split : splits) {
+            int number = getPositive(split);
+            numbers.add(number);
+        }
+    }
+
+    private static int getPositive(String split) {
+        int number = Integer.parseInt(split);
+        if (number < 0) throw new RuntimeException();
+        return number;
+    }
+
+    private static boolean isEmpty(String inputData) {
+        return "".equals(inputData) || inputData == null;
     }
 }
