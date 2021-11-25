3132239f136f362f87da72a66cccf2d09b515014
Lee Dongkyu <nelldklee@gmail.com>
2021-11-25 23:47:19 +0900

    feat(StringAddCalculator): 숫자 하나를 문자열로 입력한 경우 해당 숫자 반환 기능 구현

--- src/main/java/step1/StringAddCalculator.java
+++ src/main/java/step1/StringAddCalculator.java
@@ -8,6 +8,9 @@ public class StringAddCalculator {
 
     public static int splitAndSum(String inputData) {
         String[] splits = inputData.split(",|:");
+        if (splits.length == 1) {
+            return Integer.parseInt(splits[0]);
+        }
         List<Integer> numbers = new ArrayList<>();
         int result = 0;
         for (String number : splits) {
