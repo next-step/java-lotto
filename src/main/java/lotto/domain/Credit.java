9766f97e3963bb2acd0cf940d92096f8dc280e0e
Han <hankim@tapasmedia.co>
2021-11-26 21:24:28 +0900

    Refactor : to use constructor

--- src/main/java/lotto/domain/Credit.java
+++ src/main/java/lotto/domain/Credit.java
@@ -9,8 +9,12 @@ public class Credit {
     private static final int price = 1_000;
     private final PositiveNumber number;
 
+    private Credit(PositiveNumber positiveNumber) {
+        this.number = positiveNumber;
+    }
+
     public Credit(String input) {
-        this.number = new PositiveNumber(Integer.parseInt(input) / price);
+        this(new PositiveNumber(Integer.parseInt(input) / price));
     }
 
     public int getLottoCount() {
