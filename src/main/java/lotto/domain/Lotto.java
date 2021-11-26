d0492a5cf7f6375b37a5d12ae4dbde071bdc114d
Han <hankim@tapasmedia.co>
2021-11-26 21:24:41 +0900

    Refactor : Lotto

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,8 +3,10 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
@@ -12,26 +14,25 @@ import java.util.stream.Collectors;
  * @author han
  */
 public class Lotto {
-    private static final int bound = 46;
-    private static final int count = 6;
+    private static final int BOUND = 46;
+    private static final int COUNT = 6;
     private static final Random r = ThreadLocalRandom.current();
 
     private final List<LottoNumber> numbers;
 
-    private Lotto(List<LottoNumber> numbers) {
+    public Lotto(List<LottoNumber> numbers) {
+        validateNumbers(numbers);
         this.numbers = numbers;
     }
 
     public Lotto(String numbers) {
-        List<LottoNumber> byStrings = createByStrings(numbers);
-        validateNumbers(byStrings);
-        this.numbers = byStrings;
+        this(createByStrings(numbers));
     }
 
     public static Lotto createByAuto() {
-        List<LottoNumber> numbers = r.ints(1, bound)
+        List<LottoNumber> numbers = r.ints(1, BOUND)
             .distinct()
-            .limit(count)
+            .limit(COUNT)
             .mapToObj(LottoNumber::new)
             .collect(Collectors.toList());
 
@@ -43,25 +44,23 @@ public class Lotto {
         return numbers;
     }
 
-    private List<LottoNumber> createByStrings(String numbers) {
-        return Arrays.asList(numbers.split(","))
+    private static List<LottoNumber> createByStrings(String numbers) {
+        List<LottoNumber> byStrings = Arrays.asList(numbers.split(","))
             .stream()
             .map(String::trim)
             .map(LottoNumber::new)
             .collect(Collectors.toList());
+        return byStrings;
     }
 
     private void validateNumbers(List<LottoNumber> numbers) {
-        if (numbers == null || numbers.size() > count) {
+        if (numbers == null || numbers.size() != COUNT) {
             throw new IllegalArgumentException();
         }
 
-        int count = (int) numbers.stream()
-            .map(LottoNumber::getNumber)
-            .distinct()
-            .count();
+        Set<LottoNumber> nonDuplicatedNumber = new HashSet<>(numbers);
 
-        if (numbers.size() != count) {
+        if (nonDuplicatedNumber.size() != COUNT) {
             throw new IllegalArgumentException();
         }
     }
