cec681653bd282ba5c70e63e80a10cbf1515c694
dante(이산하) <dante.93@kakaocorp.com>
2021-11-26 15:31:44 +0900

    refactor (improve LottoNumberGenerator)

--- src/main/java/lotto/utils/LottoNumberGenerator.java
+++ src/main/java/lotto/utils/LottoNumberGenerator.java
@@ -1,6 +1,7 @@
 package lotto.utils;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,11 +13,11 @@ public final class LottoNumberGenerator {
     private static final int FIRST_LOTTO_NUMBER = 1;
     private static final int LAST_LOTTO_NUMBER_BOUND = 46;
     private static final int COUNT_TO_PICK = 6;
-    private static List<Integer> initNumbers;
+    private static List<Number> initNumbers;
 
     static {
         initNumbers = IntStream.range(FIRST_LOTTO_NUMBER, LAST_LOTTO_NUMBER_BOUND)
-                               .boxed()
+                               .mapToObj(Number::new)
                                .collect(Collectors.toList());
     }
 
@@ -24,18 +25,17 @@ public final class LottoNumberGenerator {
     }
 
     public static List<Number> generate() {
-        List<Integer> sortedInitNumbers = initNumbers.stream()
-                                                     .sorted()
-                                                     .collect(Collectors.toList());
+        List<Number> sortedInitNumbers = initNumbers.stream()
+                                                    .sorted(Comparator.comparing(Number::getValue))
+                                                    .collect(Collectors.toList());
         return pickNumbers(sortedInitNumbers);
     }
 
-    private static List<Number> pickNumbers(List<Integer> allNumbers) {
+    private static List<Number> pickNumbers(List<Number> allNumbers) {
         Collections.shuffle(allNumbers);
         return allNumbers.stream()
                          .limit(COUNT_TO_PICK)
-                         .sorted()
-                         .map(Number::new)
+                         .sorted(Comparator.comparing(Number::getValue))
                          .collect(Collectors.toList());
     }
 }
