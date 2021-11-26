026ed9650b4066cb6737439bf8e57677640fed0b
user <kth4878@naver.com>
2021-11-26 16:55:00 +0900

    [Style] 코드 처리방식 변경으로 인한 수정

--- src/main/java/controller/LotteryController.java
+++ src/main/java/controller/LotteryController.java
@@ -4,6 +4,8 @@ import domain.*;
 import view.InputView;
 import view.OutputView;
 
+import java.util.Set;
+
 public class LotteryController {
     public static void main(String[] args) {
         Store store = new Store(InputView.inputPrice());
@@ -12,12 +14,15 @@ public class LotteryController {
         LotteryTickets lotteryTickets = store.createLotteryNumber();
         OutputView.printLotteryNumber(lotteryTickets);
 
-        LastWeekLottery lastWeekLottery = new LastWeekLottery(InputView.inputLastWeekNumber());
-
-        RankGroup rankGroup = new RankGroup(lotteryTickets, lastWeekLottery, InputView.inputBonusBall());
+        Set<Integer> winningNumber = InputView.inputWinningNumber();
+        WinningLottery winningLottery = new WinningLottery(winningNumber, InputView.inputBonusBall());
+        RankGroup rankGroup = new RankGroup(lotteryTickets, winningLottery);
         MoneyPrize moneyPrize = new MoneyPrize(rankGroup);
         OutputView.resultWinningMessage();
         OutputView.printWinningPrize(rankGroup);
         OutputView.resultLotteryProfit(moneyPrize, store);
     }
 }
+
+
+
