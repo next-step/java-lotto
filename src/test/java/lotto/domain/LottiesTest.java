8c99f2bea23961b61de9e4cebf06c36f1d8c1940
JaeHan-Kim <hgal8877@gmail.com>
2021-11-28 00:29:59 +0900

    fix : 객체생성 수정

--- src/test/java/lotto/domain/LottiesTest.java
+++ src/test/java/lotto/domain/LottiesTest.java
@@ -1,8 +1,9 @@
 package lotto.domain;
 
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,9 +13,8 @@ class LottiesTest {
     @DisplayName("여러개의 로또 뭉치 생성 테스트")
     @Test
     void createLotties() {
-        Lotties lotties = new Lotties(List.of(Lotto.from("1, 2, 3, 4, 5, 6")));
+        Lotties lotties = new Lotties(Arrays.asList(Lotto.from("1, 2, 3, 4, 5, 6")));
 
         assertThat(lotties).isNotNull();
     }
-
 }
