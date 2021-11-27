5cb6727ebf7cd35c8f2af29eee82f2cb0945a6ca
supniverse <kmsup2@gmail.com>
2021-11-27 18:57:55 +0900

    refactor: stringbuilder 재활용을 위한 변경

--- src/main/java/lotto/domain/dto/LottoResult.java
+++ src/main/java/lotto/domain/dto/LottoResult.java
@@ -13,18 +13,21 @@ public class LottoResult {
   private static final String RESULT_YIELD_MESSAGE = "총 수익률은 %.2f입니다.";
   private static final String RESULT_YIELD_LESS_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
   private static final String RESULT_YIELD_MORE_MESSAGE = "(기준이 1이기 때문에 결과적으로 이득라는 의미임)";
+  private static final int ZERO = 0;
 
   private final PrizeEntry prizeEntry;
   private final YieldRate yieldRate;
+  private final StringBuilder stringBuilder;
 
   public LottoResult(PrizeEntry prizeEntry, YieldRate yieldRate) {
     this.prizeEntry = prizeEntry;
     this.yieldRate = yieldRate;
+    stringBuilder = new StringBuilder();
   }
 
   @Override
   public String toString() {
-    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.setLength(ZERO);
 
     stringBuilder.append(WINNING_STATS_MESSAGE).append(System.lineSeparator())
                  .append(UNDER_BAR).append(System.lineSeparator());
--- src/main/java/lotto/domain/entity/LottoTicket.java
+++ src/main/java/lotto/domain/entity/LottoTicket.java
@@ -8,17 +8,20 @@ public class LottoTicket {
 
   private static final String INVALID_SIZE_OF_NUMBER = "6개의 숫자 구성이 아닙니다.";
   private static final String DUPLICATED_NUMBER = "중복된 숫자가 있습니다.";
-  private static final int LOTTO_SIZE = 6;
   private static final String LEFT_SQUARE_BRACKET = "[";
-  private static final String RIGHT_SQUARE_BRACKET = "[";
+  private static final String RIGHT_SQUARE_BRACKET = "]";
   private static final String COMMA = ",";
+  private static final int LOTTO_SIZE = 6;
+  private static final int ZERO = 0;
 
   private final List<LottoNumber> numbers;
+  private final StringBuilder stringBuilder;
 
   public LottoTicket(List<LottoNumber> numbers) {
     validSize(numbers);
     validDuplicate(numbers);
     this.numbers = numbers;
+    this.stringBuilder = new StringBuilder();
   }
 
   private void validSize(List<LottoNumber> numbers) {
@@ -34,7 +37,6 @@ public class LottoTicket {
     if (size < LOTTO_SIZE) {
       throw new IllegalArgumentException(DUPLICATED_NUMBER);
     }
-
   }
 
   private boolean contains(int number) {
@@ -57,7 +59,7 @@ public class LottoTicket {
 
   @Override
   public String toString() {
-    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.setLength(ZERO);
     stringBuilder.append(LEFT_SQUARE_BRACKET)
                  .append(getListToString())
                  .append(RIGHT_SQUARE_BRACKET);
