58eed8b9cda81d6e737fadab766748e0f51da46a
hungrytech <xorals9448@gmail.com>
2021-11-27 21:14:17 +0900

    refactor : ticketCount에서 Price로 변경에 의한 변수명 변경

--- src/test/java/lotto/application/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/application/LottoTicketGeneratorTest.java
@@ -13,13 +13,13 @@ public class LottoTicketGeneratorTest {
     @DisplayName("로또 티켓의 숫자개수만큼 티켓을 발행한다.")
     @Test
     void generateTicketTest() {
-        Price ticketCount = Price.of("22000");
+        Price price = Price.of("22000");
         LottoTicketGenerator lottoTicketGenerator = new LottoTicketGenerator();
 
         LottoTickets lottoTickets = lottoTicketGenerator.generateTickets(
-                ticketCount, new AllAutomaticLottoTicketStrategy());
+                price, new AllAutomaticLottoTicketStrategy());
 
-        assertThat(lottoTickets.size()).isEqualTo(ticketCount.getNumberOfTickets());
+        assertThat(lottoTickets.size()).isEqualTo(price.getNumberOfTickets());
     }
 
 }
