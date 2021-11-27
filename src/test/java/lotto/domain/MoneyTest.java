d879f1bfa2ba62fcc5ca57191fd5e405a7f8ba78
JaeHan-Kim <hgal8877@gmail.com>
2021-11-27 21:13:32 +0900

    refactor : Money 테스트 변경

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,20 +1,18 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
 
-    @DisplayName("입력한 금액으로 로또를 몇장 구매할 수 있는지 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"13500:13","5000:5"}, delimiter = ':')
-    void lottoCountTest(int money, int lottoCount) {
-        Money actual = new Money(money);
+    @DisplayName("금액 객체 생성 테스트")
+    @Test
+    void createMoney() {
+        Money money = new Money(1000);
 
-        assertThat(actual.lottoCount()).isEqualTo(lottoCount);
+        assertThat(money).isNotNull();
     }
 }
