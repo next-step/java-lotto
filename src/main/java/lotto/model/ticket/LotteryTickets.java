cc8f806ce00b8740260441b6fc9a79c31a5f7988
jjoy <ekdnlt1995@gmail.com>
2021-11-27 17:32:33 +0900

    feat : 입력 받은 로또 수에 따라서 자동/수동 로또 생성하기

--- src/main/java/lotto/model/ticket/LotteryTickets.java
+++ src/main/java/lotto/model/ticket/LotteryTickets.java
@@ -1,18 +1,34 @@
 package lotto.model.ticket;
 
+import lotto.model.domain.PurchaseInfo;
+import lotto.model.ticket.generator.LotteryTicketGenerator;
+
 import java.util.*;
 
 public class LotteryTickets {
 
     private final List<LotteryTicket> tickets = new ArrayList<LotteryTicket>();
+    private final LotteryTicketGenerator autoTicketGenerator;
+    private final LotteryTicketGenerator manualTicketGenerator;
 
-    public LotteryTickets(int count) {
-        for (int cnt = 0 ; cnt < count; cnt++){
-            tickets.add(LotteryTicketGenerator.generate());
-        }
+    public LotteryTickets(PurchaseInfo purchaseInfo, LotteryTicketGenerator autoTicketGenerator, LotteryTicketGenerator manualTicketGenerator) {
+        this.autoTicketGenerator = autoTicketGenerator;
+        this.manualTicketGenerator = manualTicketGenerator;
+        generate(purchaseInfo.getAutoCount(), purchaseInfo.getManualCount());
     }
 
     public List<LotteryTicket> getTickets(){
         return Collections.unmodifiableList(this.tickets);
+    }
+
+    private void generate(int autoCount, int manualCount) {
+        generateLotteryTickets(manualTicketGenerator, manualCount);
+        generateLotteryTickets(autoTicketGenerator, autoCount);
+    }
+
+    private void generateLotteryTickets(LotteryTicketGenerator generator, int count) {
+        for (int cnt = 0; cnt < count; cnt++){
+            tickets.add(generator.generate());
+        }
     }
 }
--- src/test/java/lotto/model/ticket/LotteryTicketsTest.java
+++ src/test/java/lotto/model/ticket/LotteryTicketsTest.java
@@ -1,6 +1,11 @@
 package lotto.model.ticket;
 
+import lotto.model.domain.PurchaseInfo;
+import lotto.model.ticket.generator.AutoTicketGenerator;
+import lotto.model.ticket.generator.LotteryTicketGenerator;
+import lotto.model.ticket.generator.ManualTicketGenerator;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,7 +19,16 @@ class LotteryTicketsTest {
     @ValueSource(ints = {1, 2, 3, 4})
     @DisplayName("파라미터로 받은 count 와 생성된 복권 티켓 수가 일치하는지 테스트")
     void createdLotteryTicketSizeCheckTest(int count){
-        lotteryTickets = new LotteryTickets(count);
+        PurchaseInfo purchaseInfo = new PurchaseInfo(count * 1000,0);
+        lotteryTickets = new LotteryTickets(purchaseInfo,new AutoTicketGenerator(), new ManualTicketGenerator());
         assertThat(lotteryTickets.getTickets().size()).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("자동생성으로 생성된 로또 숫자가 6개인지 체크")
+    void selectNumberSizeTest(){
+        PurchaseInfo purchaseInfo = new PurchaseInfo(1000,0);
+        lotteryTickets = new LotteryTickets(purchaseInfo, new AutoTicketGenerator(), new ManualTicketGenerator());
+        assertThat(lotteryTickets.getTickets().get(0).getNumbers().size()).isEqualTo(6);
     }
 }
