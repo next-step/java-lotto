9a4f5150ef580965191fc5b4d1a96c8d2695f5a8
JaeHan-Kim <hgal8877@gmail.com>
2021-11-27 21:43:57 +0900

    feat : 로또번호 생성 구현체 구현

---
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoNumberGenerator implements NumberGenerator {
+    private static final int DEFAULT_LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    public Lotto publish() {
+        List<LottoNumber> randomNumbers = shuffleNumbers();
+        return new Lotto(
+                IntStream.range(0, DEFAULT_LOTTO_SIZE)
+                        .boxed()
+                        .map(randomNumbers::get)
+                        .collect(Collectors.toList()));
+    }
+
+    private List<LottoNumber> shuffleNumbers() {
+        List<LottoNumber> randomNumbers = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        Collections.shuffle(randomNumbers);
+        return randomNumbers;
+    }
+}
