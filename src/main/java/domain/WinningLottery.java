98384c8230a7b758a2462b2390bdb5c9934ef0f1
user <kth4878@naver.com>
2021-11-28 03:52:35 +0900

    [Refactor] matchLottery 메소드 contains 사용으로 변경

--- src/main/java/domain/WinningLottery.java
+++ src/main/java/domain/WinningLottery.java
@@ -8,13 +8,14 @@ public class WinningLottery {
     private static final int LOTTERY_SIZE_MAX = 6;
     private static final boolean MATCH_NUMBER_TRUE = true;
     private static final boolean MATCH_NUMBER_FALSE = false;
+    private static final String OVERLAP_ERROR_MESSAGE = "error : 당첨번호 와 보너스볼 은 중복될수 없습니다.";
 
     private final Set<LotteryNumber> winningLottery;
     private final int bonusBall;
 
     public WinningLottery(Set<Integer> numbers, int bonusBall) {
         this.winningLottery = changeToLotteryNumber(numbers);
-        this.bonusBall = bonusBall;
+        this.bonusBall = validBonusBall(bonusBall);
     }
 
     public Set<LotteryNumber> changeToLotteryNumber(Set<Integer> numbers) {
@@ -35,16 +36,21 @@ public class WinningLottery {
                 .collect(Collectors.toSet());
     }
 
-    public boolean matchLottery(int lotteryNumber) {
-        return winningLottery.stream()
-                .anyMatch(number -> validLotteryNumber(number.value(), lotteryNumber));
+    private int validBonusBall(int bonusBall){
+        for(LotteryNumber number : winningLottery){
+            checkOverlap(number.value(), bonusBall);
+        }
+        return bonusBall;
     }
 
-    private boolean validLotteryNumber(int winningNumber, int lotteryNumber) {
-        if (winningNumber == lotteryNumber) {
-            return MATCH_NUMBER_TRUE;
+    private void checkOverlap(int lotteryNumber, int bonusBall){
+        if(lotteryNumber == bonusBall){
+            throw new IllegalArgumentException(OVERLAP_ERROR_MESSAGE);
         }
-        return MATCH_NUMBER_FALSE;
+    }
+
+    public boolean matchLottery(LotteryNumber lotteryNumber) {
+        return winningLottery.contains(lotteryNumber);
     }
 
     public boolean matchBonusBall(int lotteryNumber) {
@@ -57,4 +63,5 @@ public class WinningLottery {
     public int size() {
         return winningLottery.size();
     }
+
 }
