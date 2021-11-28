761721d550aa4747b8531fc3ea5b980cb2964bd2
JaeHan-Kim <hgal8877@gmail.com>
2021-11-28 22:39:10 +0900

    fix : 변경된 generator 및 구현체로 수정

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -7,10 +7,10 @@ public class Store {
     private static int FIRST_GAME = 0;
     public static int LOTTO_ONE_GAME_PRICE = 1000;
 
-    public Lotties purchaseLottiesByMoney(Money money, NumberGenerator numberGenerator) {
+    public Lotties purchaseLottiesByMoney(Money money, LottoGenerator numberGenerator) {
         int gameCount = money.getValue() / LOTTO_ONE_GAME_PRICE;
         return new Lotties(IntStream.range(FIRST_GAME, gameCount)
-                                .mapToObj(n -> numberGenerator.publish())
-                                .collect(Collectors.toList()));
+                .mapToObj(n -> numberGenerator.publish())
+                .collect(Collectors.toList()));
     }
 }
