c481f29e9c2dd7b9c8ae710aba5bb0287f4716b1
JaeHan-Kim <hgal8877@gmail.com>
2021-11-28 23:12:39 +0900

    test : Lotto 피드백 수정 - 자료구조, 메서드 접근제한자

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,31 +1,26 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
 
-    private final List<LottoNumber> numbers;
+    private final Set<LottoNumber> numbers;
 
     public static Lotto from(final String lottoString) {
-        List<LottoNumber> numbers = Arrays.stream(lottoString.split(","))
+        Set<LottoNumber> numbers = Arrays.stream(lottoString.split(","))
                 .map(String::trim)
                 .map(LottoNumber::new)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
         return new Lotto(numbers);
     }
 
-    public Lotto(final List<LottoNumber> numbers) {
-        Collections.sort(numbers);
-        this.numbers = Collections.unmodifiableList(numbers);
-    }
-
-    public long sameLottoNumberCount(Lotto lotto) {
-        return this.numbers.stream()
-                .filter(lotto.numbers::contains)
-                .count();
+    public Lotto(final Set<LottoNumber> numbers) {
+        this.validateLotto(numbers.size());
+        this.numbers = new TreeSet<>(numbers);
     }
 
     public String getNumbers() {
@@ -36,5 +31,17 @@ public class Lotto {
 
     public Rank getRank(Lotto lotto) {
         return Rank.matchNumber(this.sameLottoNumberCount(lotto));
+    }
+
+    private long sameLottoNumberCount(Lotto lotto) {
+        return this.numbers.stream()
+                .filter(lotto.numbers::contains)
+                .count();
+    }
+
+    private void validateLotto(final int count) {
+        if (count == LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 }
