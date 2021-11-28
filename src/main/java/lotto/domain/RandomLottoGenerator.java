4a77f1f8c3692aa123b9b772f28ccd2c6df1f78b
JaeHan-Kim <hgal8877@gmail.com>
2021-11-28 23:06:43 +0900

    test : Lottogenerator 구현체 수정 - 생성자 추가 및 이름 변경

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -10,21 +10,25 @@ public class RandomLottoGenerator implements LottoGenerator {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
 
+    private final List<LottoNumber> randomNumbers;
+
+    public RandomLottoGenerator() {
+        this.randomNumbers = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
     public Lotto publish() {
         List<LottoNumber> randomNumbers = shuffleNumbers();
         return new Lotto(
                 IntStream.range(0, DEFAULT_LOTTO_SIZE)
                         .boxed()
                         .map(randomNumbers::get)
-                        .collect(Collectors.toList()));
+                        .collect(Collectors.toSet()));
     }
 
     private List<LottoNumber> shuffleNumbers() {
-        List<LottoNumber> randomNumbers = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
-                .boxed()
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
-
         Collections.shuffle(randomNumbers);
         return randomNumbers;
     }
