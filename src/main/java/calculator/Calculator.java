bce1a5bf2fcfca62835cda76b395e12e3199a5b4
sojeongw <sojeongw.official@gmail.com>
2021-11-28 21:57:52 +0900

    refactor: 정규식을 이용해 커스텀 구분자를 받도록 한다

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,18 +1,22 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
     public static final String DELIMITER = ",|:";
 
     private final Numbers numbers;
 
     public Calculator(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+
         if (input.contains(",") || input.contains(":")) {
             String[] splitInput = input.split(DELIMITER);
             numbers = new Numbers(splitInput);
-        } else if (input.contains("//") && input.contains("\n")) {
-            char delimiter = input.charAt(2);
-            String substring = input.substring(4);
-            String[] splitInput = substring.split(String.valueOf(delimiter));
+        } else if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] splitInput= m.group(2).split(customDelimiter);
             numbers = new Numbers(splitInput);
         } else {
             numbers = new Numbers(Integer.parseInt(input));
