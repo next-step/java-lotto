39c9166adef7a8375eb6c885ba2c5910efd35a4f
JaeHan-Kim <hgal8877@gmail.com>
2021-11-28 23:52:23 +0900

    fix : 로또 게임 결과를 저장하는 클래스 간소화

--- src/main/java/lotto/domain/LottoGameResult.java
+++ src/main/java/lotto/domain/LottoGameResult.java
@@ -1,34 +1,22 @@
 package lotto.domain;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class LottoGameResult {
-    private final Lotties lotties;
-    private final Lotto winLotto;
-    private final Map<Rank, Long> result;
+    private final ResultRank resultRank;
+    private final int purchaseLottoMoney;
 
     public LottoGameResult(Lotties lotties, Lotto winLotto) {
-        this.lotties = lotties;
-        this.winLotto = winLotto;
-        this.result = getLottiesRank();
+        this.resultRank = new ResultRank(lotties.gameResultRank(winLotto));
+        this.purchaseLottoMoney = Store.LOTTO_ONE_GAME_PRICE * lotties.purchaseLottiesCount();
     }
 
     public double getRate() {
-        int purchaseLottoMoney = Store.LOTTO_ONE_GAME_PRICE * lotties.purchaseLottiesCount();
-        long prizeMoney = result.keySet().stream()
-                                        .map(Rank::getPrizeMoney)
-                                        .reduce(0, Integer::sum);
-        return prizeMoney / purchaseLottoMoney;
+        long prizeMoney = resultRank.getPrizeMoney();
+        return (double) prizeMoney / this.purchaseLottoMoney;
     }
 
     public long getRankCount(Rank rank) {
-        return this.result.getOrDefault(rank, 0L);
+        return this.resultRank.getPrizeCount(rank);
     }
 
-    public Map<Rank, Long> getLottiesRank() {
-        return lotties.getLotties().stream()
-                            .map(winLotto::getRank)
-                            .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
-    }
+
 }
