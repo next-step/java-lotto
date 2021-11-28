30365345e0d985fd2f8ed0f7ef5f2115cad46b37
ssibongee <ssibongee@gmail.com>
2021-11-28 16:39:27 +0900

    feat: 연산식 (Expression) 클래스 구현

---
+++ src/main/java/calculator/domain/Expression.java
@@ -0,0 +1,38 @@
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Expression {
+
+    public static final Pattern EXPRESSION_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    private final Delimiter delimiter;
+
+    private final Operands operands;
+
+    private Expression(Operands operands) {
+        this(Delimiter.DEFAULT_DELIMITER, operands);
+    }
+
+    private Expression(Delimiter delimiter, Operands operands) {
+        this.delimiter = delimiter;
+        this.operands = operands;
+    }
+
+    public static Expression of(String expression) {
+        Matcher matcher = EXPRESSION_PATTERN.matcher(expression);
+        if (matcher.find()) {
+            Delimiter delimiter = Delimiter.from(matcher.group(1));
+            Operands operands = Operands.from(Arrays.asList(delimiter.split(matcher.group(2))));
+            return new Expression(delimiter, operands);
+        }
+
+        return new Expression(Operands.from(Arrays.asList(Delimiter.DEFAULT_DELIMITER.split(expression))));
+    }
+
+    public int calculate() {
+        return operands.sumOf();
+    }
+}
