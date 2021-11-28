4bbd0e881dc41e52067ee5919ce7c42fea7f10a6
sojeongw <sojeongw.official@gmail.com>
2021-11-28 22:12:55 +0900

    feat: 숫자가 아닌 값이나 음수를 전달하면 예외를 던진다

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,15 +1,33 @@
 package calculator;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Number {
     private final int value;
 
     public Number(String input) {
-        this(Integer.parseInt(input));
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+        boolean matches = pattern.matcher(input).matches();
+
+        if(!matches) {
+            throw new RuntimeException("숫자가 아닌 값은 입력할 수 없습니다.");
+        }
+
+        int parsedInput = Integer.parseInt(input);
+
+        if (parsedInput < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+
+        this.value = parsedInput;
     }
 
     public Number(int input) {
+        if (input < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+
         this.value = input;
     }
 
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
 
@@ -27,4 +28,21 @@ class CalculatorTest {
         Calculator calculator = new Calculator("//;\n1;2;3");
         assertThat(calculator.sum()).isEqualTo(6);
     }
+
+    @Test
+    @DisplayName("음수를 전달하면 예외를 던진다")
+    void shouldThrowIfNegative() {
+        assertThatThrownBy(() -> new Calculator("-1,2,3"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("음수는 입력할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 값을 전달하면 예외를 던진다")
+    void shouldThrowIfNotNumber() {
+        assertThatThrownBy(() -> new Calculator("1,%,3"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("숫자가 아닌 값은 입력할 수 없습니다.");
+    }
+
 }
