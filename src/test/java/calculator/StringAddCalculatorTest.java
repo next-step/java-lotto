6ee5a0bba6a96bb99921d47b262e4f91efecbd8c
hyenny <qlwb9191@naver.com>
2021-11-29 07:01:47 +0900

    feat: 테스트 케이스 정리

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,35 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringAddCalculatorTest {
+
+    @Test
+    void splitAndSum_빈문자열_널_0반환() {
+
+    }
+
+    @Test
+    void splitAndSum_숫자하나() {
+
+    }
+
+    @Test
+    void splitAndSum_구분자_컴마또는콜론() {
+
+    }
+
+    @DisplayName("//와 \n문자 사이로 커스텀 구분자를 지정 가능")
+    @Test
+    void splitAndSum_구분자_커스텀() {
+
+    }
+
+    @DisplayName("숫자 이외의 값이나 음수가 오는 경우 RuntimeException")
+    @Test
+    void splitAndSum_숫자이외의값_음수_예외() {
+
+    }
+
+}
