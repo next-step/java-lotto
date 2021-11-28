36acfeb5be6d5b13b464c8091c582a273454e80f
JaeHan-Kim <hgal8877@gmail.com>
2021-11-28 22:28:53 +0900

    test : 로또번호의 동등성과 비교 테스트 케이스 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberTest {
 
@@ -18,7 +18,6 @@ class LottoNumberTest {
         LottoNumber number = new LottoNumber(actual);
 
         assertThat(number).isNotNull();
-        assertThat(number.getNumber()).isEqualTo(actual);
     }
 
     @DisplayName("로또 번호는 0에서 45사이의 값만 가능하다.")
@@ -27,5 +26,24 @@ class LottoNumberTest {
     void createLottoNumberRange(Integer actual) {
         assertThatThrownBy(() -> new LottoNumber(actual))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("로또 번호 비교 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:2:-1", "1:1:0", "2:1:1"}, delimiter = ':')
+    void compareLottoNumber(String x, String y, int compareValue) {
+        LottoNumber numberX = new LottoNumber(x);
+        LottoNumber numberY = new LottoNumber(y);
+
+        assertThat(numberX.compareTo(numberY)).isEqualTo(compareValue);
+    }
+
+    @DisplayName("로또 번호 동등성 테스트")
+    @Test
+    void equalLottoNumber() {
+        LottoNumber x = new LottoNumber(1);
+        LottoNumber y = new LottoNumber(1);
+
+        assertThat(x.equals(y)).isTrue();
     }
 }
