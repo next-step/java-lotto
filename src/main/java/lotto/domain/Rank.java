89b4d0130d021b11fa512af01728004f03bce6e0
LeeJeongGi <apple4rhk@naver.com>
2021-11-29 18:37:42 +0900

    refactor : 2등 구현할때 주석해제

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
+    MISS(0, 0),
     FIFTH(3, 5_000),
-    MISS(0, 0);
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+//    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private final int countOfMatch;
     private final int prizeMoney;
