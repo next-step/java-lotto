a26402c0a9bcb0872e33233d715080acc48768f0
giyeon95 <giyeon15@gmail.com>
2021-11-30 00:02:29 +0900

    feature: 수동 번호 발행 `LottoAssignMachine` 기능 구현

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
@@ -42,5 +43,22 @@ public class LottoTicket {
             .collect(Collectors.toList());
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoTicket that = (LottoTicket) o;
+        return Objects.equals(getNumbers(), that.getNumbers());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumbers());
+    }
 
 }
