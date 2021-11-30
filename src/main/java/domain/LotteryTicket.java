3c8335ff9a610c20d76d351b5f3eb1de8e3a54cb
user <kth4878@naver.com>
2021-11-30 09:43:54 +0900

    [Refactor] 로또번호 6개 유효성 검사 추가 및 상태갑 List -> Set 변경

--- src/main/java/domain/LotteryTicket.java
+++ src/main/java/domain/LotteryTicket.java
@@ -1,16 +1,18 @@
 package domain;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LotteryTicket {
     private static final int BONUS_BULL_CHECK_NUMBER = 5;
     private static final int BONUS_BALL_ZERO = 0;
+    private static final int LOTTERY_END_SIZE = 6;
+    private static final String LOTTERY_SIZE_ERROR_MESSAGE = "error : 로또번호는 6개 입니다.";
 
-    private final List<LotteryNumber> lotteryTicket;
+    private final Set<LotteryNumber> lotteryTicket;
 
-    public LotteryTicket(List<LotteryNumber> lotteryTicket) {
-        this.lotteryTicket = Collections.unmodifiableList(lotteryTicket);
+    public LotteryTicket(Set<LotteryNumber> lotteryTicket) {
+        this.lotteryTicket = Collections.unmodifiableSet(lotteryTicket);
     }
 
     public int matchCount(WinningLottery winningLottery) {
@@ -29,11 +31,42 @@ public class LotteryTicket {
                 .count();
     }
 
-    public int value(int index) {
-        return lotteryTicket.get(index).value();
+    public static LotteryTicket from(Set<LotteryNumber> lottery) {
+        return new LotteryTicket(lottery);
+    }
+
+    public static Set<LotteryNumber> validLotteryNumber(Set<Integer> manualNumber) {
+        validLotterySize(manualNumber.size());
+
+        return Collections.unmodifiableSet(manualNumber.stream()
+                .map(LotteryNumber::new)
+                .collect(Collectors.toSet()));
+    }
+
+    private static void validLotterySize(int size) {
+        if (size != LOTTERY_END_SIZE) {
+            throw new IllegalArgumentException(LOTTERY_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    public Set<LotteryNumber> bringLottery() {
+        return lotteryTicket;
     }
 
     public int size() {
         return lotteryTicket.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LotteryTicket that = (LotteryTicket) o;
+        return Objects.equals(lotteryTicket, that.lotteryTicket);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotteryTicket);
     }
 }
