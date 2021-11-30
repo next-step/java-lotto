ae1da4cb7d9fbe6e68c0af064399fe9883aa4159
dodeon <sojeongw.official@gmail.com>
2021-11-30 21:02:04 +0900

    feat: 합계를 구하기 위한 Numbers를 구현한다

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -1,52 +1,26 @@
-/*
 package calculator;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
-
-import static java.util.Arrays.stream;
-import static java.util.Collections.singletonList;
 
 public class Numbers {
     private final List<Number> numbers;
+    private int sum = 0;
+
+    public Numbers(String[] numbers) {
+        this(Arrays.stream(numbers).map(Number::new).collect(Collectors.toList()));
+    }
 
     public Numbers(List<Number> numbers) {
         this.numbers = numbers;
     }
 
-    public Numbers(int input) {
-        this(singletonList(new Number(input)));
-    }
-
-    public Numbers(String[] input) {
-        this(stream(input)
-                .map(Number::new)
-                .collect(Collectors.toList())
-        );
-    }
-
     public int sum() {
-        int sum = 0;
-
         for (Number number : numbers) {
             sum += number.getValue();
         }
 
         return sum;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Numbers numbers1 = (Numbers) o;
-        return Objects.equals(numbers, numbers1.numbers);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(numbers);
-    }
 }
-*/
--- src/test/java/calculator/NumbersTest.java
+++ src/test/java/calculator/NumbersTest.java
@@ -1,29 +1,20 @@
-/*
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class NumbersTest {
 
     @Test
-    @DisplayName("숫자 하나만 문자열로 입력할 경우 해당 숫자를 저장한다")
-    void shouldReturnIfSingleInput() {
-        Numbers numbers = new Numbers(1);
-        assertThat(numbers).isEqualTo(new Numbers(1));
-    }
+    @DisplayName("숫자들의 합계를 구한다")
+    void shouldSumNumbers() {
+        Numbers numbers = new Numbers(Arrays.asList(new Number("1"), new Number("2")));
+        int result = numbers.sum();
 
-    @Test
-    @DisplayName("숫자 여러 개를 입력할 경우 해당 숫자들을 저장한다")
-    void shouldSplitByComma() {
-        List<Number> input = Arrays.asList(new Number(1), new Number(2));
-        Numbers numbers = new Numbers(input);
-
-        assertThat(numbers).isEqualTo(new Numbers(input));
+        assertThat(result).isEqualTo(3);
     }
-}*/
+}
