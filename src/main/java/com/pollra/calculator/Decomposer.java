cfa00878bb5725a913ccef032940c0c4ed9f3d58
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-12-01 00:03:47 +0900

    feat(Decomposer): 구현중 뭔가 잘못됨을 느낌

--- src/main/java/com/pollra/calculator/Decomposer.java
+++ src/main/java/com/pollra/calculator/Decomposer.java
@@ -1,20 +1,56 @@
 package com.pollra.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @since       2021.11.30
  * @author      pollra
  **********************************************************************************************************************/
 public class Decomposer {
 
+    private static final String COLON = ":";
+    private static final String COMMA = ",";
+
+    private static final String CUSTOM_SEPARATOR_PREFIX = "//";
+    private static final String CUSTOM_SEPARATOR_SUFFIX = "\\\\n";
+
+    private List<Separator> separators;
+
+    public Decomposer() {
+        separators = new ArrayList<>();
+        separators.add(new Separator(COLON));
+        separators.add(new Separator(COMMA));
+    }
+
     public Numbers decompose(String text) {
-        return null;
+        int customSeparatorBoundary = text.indexOf(CUSTOM_SEPARATOR_SUFFIX);
+        if(0 > customSeparatorBoundary) {
+            return decomposeNumbers(text, 0);
+        }
+        if(0 < customSeparatorBoundary) {
+            int textLength = text.length();
+            decomposeSeparators(text.substring(0, customSeparatorBoundary));
+
+            customSeparatorBoundary += CUSTOM_SEPARATOR_SUFFIX.length();
+
+            return decomposeNumbers(text.substring(customSeparatorBoundary, textLength), 0);
+        }
+        throw new RuntimeException("식이 올바르지 않습니다.");
     }
 
     private void decomposeSeparators(String text) {
+        int prefixEndIndex = text.indexOf(CUSTOM_SEPARATOR_PREFIX);
+        prefixEndIndex += CUSTOM_SEPARATOR_PREFIX.length();
 
+        String customSeparatorText = text.substring(prefixEndIndex);
+
+        separators.add(new Separator(customSeparatorText));
     }
 
-    private void decomposeNumbers(String text) {
+    private Numbers decomposeNumbers(String text, int index) {
+        Separator separator = separators.get(index);
 
+        return null;
     }
 }
