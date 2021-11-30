e6ebfb1c45d139be295d7e6c9a3ddcec85249bfb
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-11-30 22:59:24 +0900

    refactor(Number.class): 생성자 순서 정리

--- src/main/java/com/pollra/calculator/Number.java
+++ src/main/java/com/pollra/calculator/Number.java
@@ -16,16 +16,16 @@ public class Number {
         this(DEFAULT_VALUE);
     }
 
+    public Number(int value) {
+        this.value = value;
+    }
+
     public Number(String text) {
         boolean isDigit = text.chars().allMatch(Character::isDigit);
         if( ! isDigit) {
             throw new RuntimeException("음수 및 문자열은 계산할 수 없습니다.");
         }
         value = Integer.parseInt(text);
-    }
-
-    public Number(int value) {
-        this.value = value;
     }
 
     public Number plus(Number otherNumber) {
