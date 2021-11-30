ea7f097283299511ab8fc838b06edb0e7c41a6a2
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-11-30 23:07:29 +0900

    test(Decompose): 실패하는 테스트 작성

---
+++ src/test/java/com/pollra/calculator/DecomposerTest.java
@@ -0,0 +1,24 @@
+package com.pollra.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("분해자 테스트")
+class DecomposerTest {
+
+    @Test
+    @DisplayName("\"//;\\\\n1;2;3\" 입력 시 Numbers 로 변환")
+    public void decompose() {
+        Decomposer decomposer = new Decomposer();
+        Numbers resultNumbers = decomposer.decompose("//;\\\\n1;2;3");
+
+        Numbers expectedNumbers = new Numbers(Numbers.toInstance(
+                Arrays.asList(1, 2, 3)
+        ));
+        assertEquals(expectedNumbers, resultNumbers);
+    }
+}
