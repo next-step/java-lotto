adb484f5814fa6463a9828fbca98663a08152994
user <kth4878@naver.com>
2021-11-30 09:43:04 +0900

    [Refactor] LotteryNumbers 자동 번호생성 변경 및 수동 기능 제거

--- src/main/java/domain/LotteryNumbers.java
+++ src/main/java/domain/LotteryNumbers.java
@@ -9,8 +9,6 @@ public class LotteryNumbers {
     private static final int LOTTERY_END_SIZE = 6;
     private static final int LOTTERY_START_NUMBER = 1;
     private static final int LOTTERY_END_NUMBER = 46;
-    private static final String LOTTERY_SIZE_ERROR_MESSAGE = "error : 로또번호는 6개 입니다.";
-    private static final String LOTTERY_NUMBER_RANGE_ERROR_MESSAGE = "error : 사용가능한 로또번호 범위가 아닙니다.";
 
     private static final List<LotteryNumber> lotteryNumbers = balls();
 
@@ -23,41 +21,12 @@ public class LotteryNumbers {
                 .collect(Collectors.toList());
     }
 
-    public static List<LotteryNumber> bringNumber() {
+    public static Set<Integer> auto() {
         Collections.shuffle(lotteryNumbers);
 
-        return Collections.unmodifiableList(lotteryNumbers.subList(LOTTERY_START_SIZE, LOTTERY_END_SIZE)
-                .stream()
-                .sorted(Comparator.comparing(LotteryNumber::value))
-                .collect(Collectors.toList()));
-    }
-
-    public static List<LotteryNumber> validManualNumber(Set<Integer> manualNumber){
-        validLotterySize(manualNumber.size());
-        validLotteryNumberSize(manualNumber);
-
-        return Collections.unmodifiableList(manualNumber.stream()
-                .map(LotteryNumber::new)
-                .sorted(Comparator.comparing(LotteryNumber::value))
-                .collect(Collectors.toList()));
-    }
-
-    private static void validLotterySize(int size){
-        if(size != LOTTERY_END_SIZE){
-            throw new IllegalArgumentException(LOTTERY_SIZE_ERROR_MESSAGE);
-        }
-    }
-
-    private static void validLotteryNumberSize(Set<Integer> manualNumber){
-        for(int number : manualNumber){
-            validLotteryNumber(number);
-        }
-    }
-
-    private static void validLotteryNumber(int number){
-        if(number < LOTTERY_START_NUMBER || number > LOTTERY_END_NUMBER){
-            throw new IllegalArgumentException(LOTTERY_NUMBER_RANGE_ERROR_MESSAGE);
-        }
+        return IntStream.range(LOTTERY_START_SIZE, LOTTERY_END_SIZE)
+                .mapToObj(i -> lotteryNumbers.get(i).value())
+                .collect(Collectors.toSet());
     }
 
 }
