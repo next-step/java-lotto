c4335f8a6648fed84648ab36f42ee1c5d470bebb
carol.dear <carol.dear@kakaocorp.com>
2021-11-30 17:19:24 +0900

    fix : 대문자에서 소문자로 변경

--- src/main/java/lotto/domain/RandomLottoNumberStrategy.java
+++ src/main/java/lotto/domain/RandomLottoNumberStrategy.java
@@ -7,19 +7,19 @@ import java.util.List;
 public class RandomLottoNumberStrategy implements LottoNumberStrategy {
 
     private static final int LOTTO_NUMBER_SELECT_COUNT = 6;
-    private List<Integer> LOTTO_NUMBER_LIST;
+    private List<Integer> lottoNumList;
     private static final int LOTTO_TOTAL_COUNT = 45;
 
     public RandomLottoNumberStrategy() {
-        LOTTO_NUMBER_LIST = new ArrayList<>(LOTTO_TOTAL_COUNT);
+        lottoNumList = new ArrayList<>(LOTTO_TOTAL_COUNT);
         for (int i = 1; i <= LOTTO_TOTAL_COUNT; i++) {
-            LOTTO_NUMBER_LIST.add(i);
+            lottoNumList.add(i);
         }
     }
 
     @Override
     public List<Integer> createNumber() {
-        Collections.shuffle(LOTTO_NUMBER_LIST);
-        return new ArrayList<>(LOTTO_NUMBER_LIST.subList(0, LOTTO_NUMBER_SELECT_COUNT));
+        Collections.shuffle(lottoNumList);
+        return new ArrayList<>(lottoNumList.subList(0, LOTTO_NUMBER_SELECT_COUNT));
     }
 }
