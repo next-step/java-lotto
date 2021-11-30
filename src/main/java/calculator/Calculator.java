5a985465767397f3470b6190989c4af9b631d9ab
scott.lee <scott.lee@kakaocorp.com>
2021-12-01 02:30:47 +0900

    refactor: split 기능 함수 분리, 상수처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,24 +9,30 @@ public class Calculator {
     private static final String[] DELIMITERS = {",", ":"};
     private static final String DELIMITERS_REGEX = String.join("|", DELIMITERS);
 
+    private static final String CUSTOM_DELIMITER_INPUT_REGEX = "//(.)\n(.*)";
+    private static final int DELIMITER_GROUP_INDEX = 1;
+    private static final int CALCULATE_GROUP_INDEX = 2;
+
+    private static final Pattern inputPattern = Pattern.compile(CUSTOM_DELIMITER_INPUT_REGEX);
+
     public static int compute(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        String[] numbers;
-
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if(matcher.find()) {
-            String delimiter = matcher.group(1);
-            numbers = matcher.group(2).split(delimiter);
-        } else {
-            numbers = input.split(DELIMITERS_REGEX);
-        }
-
+        String[] numbers = splitInput(input);
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    private static String[] splitInput(String input) {
+        Matcher matcher = inputPattern.matcher(input);
+        if(matcher.find()) {
+            String delimiter = matcher.group(DELIMITER_GROUP_INDEX);
+            return matcher.group(CALCULATE_GROUP_INDEX).split(delimiter);
+        }
+        return input.split(DELIMITERS_REGEX);
     }
 
 
