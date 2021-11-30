bc14b6997ebd2d2be2f7f86cef167a5a66bbb3eb
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-11-30 22:09:18 +0900

    feat(testCode): Number 기능 구현

--- src/main/java/com/pollra/calculator/Number.java
+++ src/main/java/com/pollra/calculator/Number.java
@@ -1,5 +1,7 @@
 package com.pollra.calculator;
 
+import java.util.Objects;
+
 /**
  * @since       2021.11.29
  * @author      pollra
@@ -13,6 +15,19 @@ public class Number {
     }
 
     public Number plus(Number otherNumber) {
-        return null;
+        return new Number(value + otherNumber.value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (! (o instanceof Number)) return false;
+        Number number=(Number) o;
+        return value == number.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
