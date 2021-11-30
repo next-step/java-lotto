617ffe5685bf2e5896dbd9f41809ff366983d103
scott.lee <scott.lee@kakaocorp.com>
2021-12-01 02:03:11 +0900

    refactor: 고정 문자열 분리, 변수 네이밍 수정, stream에서 사용하는 메서드 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,11 +3,20 @@ package calculator;
 import java.util.Arrays;
 
 public class Calculator {
-    public static int compute(String s) {
-        if (s == null || s.isEmpty()) {
+
+    private static final String[] DELIMITERS = {",", ":"};
+    private static final String DELIMITERS_REGEX = String.join("|", DELIMITERS);
+
+    public static int compute(String input) {
+        if (input == null || input.isEmpty()) {
             return 0;
         }
-        String[] strings = s.split(",|:");
-        return Arrays.stream(strings).mapToInt(Integer::parseInt).reduce(0, Integer::sum);
+
+        String[] numbers = input.split(DELIMITERS_REGEX);
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
+
+
 }
