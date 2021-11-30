36a0820461aa9fbda69297f0076ad0f8c278d254
dodeon <sojeongw.official@gmail.com>
2021-11-30 10:45:44 +0900

    feat: Calculator 로직을 static으로 변경한다

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,24 +6,37 @@ import java.util.regex.Pattern;
 public class Calculator {
     public static final String DELIMITER = ",|:";
 
-    private final Numbers numbers;
-
-    public Calculator(String input) {
+    public static int calculate(String input) {
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
 
-        if (input.contains(",") || input.contains(":")) {
-            String[] splitInput = input.split(DELIMITER);
-            numbers = new Numbers(splitInput);
-        } else if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] splitInput= m.group(2).split(customDelimiter);
-            numbers = new Numbers(splitInput);
-        } else {
-            numbers = new Numbers(Integer.parseInt(input));
-        }
-    }
+        String[] splitInput;
 
-    public int sum() {
-        return numbers.sum();
+        if(m.find()) {
+            String customDelimiter = m.group(1);
+            splitInput = m.group(2).split(customDelimiter);
+        } else {
+            splitInput = input.split(DELIMITER);
+        }
+
+        int sum = 0;
+
+        for (String s : splitInput) {
+
+            boolean matches = s.matches("[+-]?\\d*(\\.\\d+)?");
+
+            if(!matches) {
+                 throw new RuntimeException("숫자가 아닌 값은 입력할 수 없습니다.");
+            }
+
+            int parsedInt = Integer.parseInt(s);
+
+            if(parsedInt < 0) {
+                throw new RuntimeException("음수는 입력할 수 없습니다.");
+            }
+
+            sum += parsedInt;
+        }
+
+        return sum;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -11,28 +11,28 @@ class CalculatorTest {
     @Test
     @DisplayName("숫자 하나만 문자열로 입력할 경우 해당 숫자를 반환한다")
     void shouldReturnIfSingleInput() {
-        Calculator calculator = new Calculator("1");
-        assertThat(calculator.sum()).isEqualTo(1);
+        int result = Calculator.calculate("1");
+        assertThat(result).isEqualTo(1);
     }
 
     @Test
     @DisplayName("숫자 여러 개를 입력할 경우 쉼표를 구분자로 분리한다")
     void shouldSplitByComma() {
-        Calculator calculator = new Calculator("1,2:3");
-        assertThat(calculator.sum()).isEqualTo(6);
+        int result = Calculator.calculate("1,2:3");
+        assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("커스텀 구분자를 입력할 경우 이를 기준으로 숫자를 분리한다")
     void shouldSplitByCustomDelimiter() {
-        Calculator calculator = new Calculator("//;\n1;2;3");
-        assertThat(calculator.sum()).isEqualTo(6);
+        int result = Calculator.calculate("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("음수를 전달하면 예외를 던진다")
     void shouldThrowIfNegative() {
-        assertThatThrownBy(() -> new Calculator("-1,2,3"))
+        assertThatThrownBy(() -> Calculator.calculate("-1,2,3"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("음수는 입력할 수 없습니다.");
     }
@@ -40,7 +40,7 @@ class CalculatorTest {
     @Test
     @DisplayName("숫자가 아닌 값을 전달하면 예외를 던진다")
     void shouldThrowIfNotNumber() {
-        assertThatThrownBy(() -> new Calculator("1,%,3"))
+        assertThatThrownBy(() -> Calculator.calculate("1,%,3"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("숫자가 아닌 값은 입력할 수 없습니다.");
     }
