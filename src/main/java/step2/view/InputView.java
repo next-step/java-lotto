bb7f6e3f26212fc9bbd6a0a3e7c8c1be819fb4a6
haeuniii <qnfnqnfn9825@gmail.com>
2021-12-01 17:03:55 +0900

    refactor<View>: View class의 데이터 가공 기능 제거
    View class의 데이터 가공 기능을 제거 후 Controller로 옮김

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -8,26 +8,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    public static int start() {
+    public static int getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
 
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt() / 1000;
-        System.out.println(String.format("%d개를 구매했습니다.", input));
+        int input = scanner.nextInt();
+
+        System.out.println(String.format("%d개를 구매했습니다.", input / 1000));
 
         return input;
     }
 
-    public static Lottery inputLotteryNumbers() {
+    public static String inputLotteryNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 
         Scanner scanner = new Scanner(System.in);
-        List<Number> numbers = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) {
-            numbers.add(new Number(scanner.nextInt()));
-        }
-
-        return new Lottery(numbers);
+        return scanner.nextLine();
     }
 }
--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -1,23 +1,34 @@
 package step2.view;
 
-import step1.domain.Number;
 import step2.domain.Lotteries;
 import step2.domain.Lottery;
+import step2.domain.enums.RANKING;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ResultView {
     public static void printLotteries(Lotteries lotteries) {
-        for (Lottery lottery : lotteries.getLotteries()) {
-            String str = lottery.getLottery().stream()
+        for (Lottery lottery : lotteries.getList()) {
+            String str = lottery.getList().stream()
                     .map(n -> String.valueOf(n.getNumber()))
                     .collect(Collectors.joining(", "));
 
             System.out.println(String.format("[%s]", str));
         }
+
+        System.out.println();
     }
 
-    public static void printResult(Number number) {
-        System.out.println(number.getNumber());
+    public static void printResult(Map<RANKING, Integer> result) {
+        System.out.println("\n당첨 통계");
+        
+        for (Map.Entry<RANKING, Integer> entry : result.entrySet()) {
+            System.out.println(String.format("%s개 일치 (%d원) - %d개", entry.getKey().getCorrectCount(), entry.getKey().getPrizeMoney(), entry.getValue()));
+        }
+    }
+
+    public static void printStatisticRate(double profitRate) {
+        System.out.println(String.format("총 수익률은 %.0f%% 입니다.", profitRate));
     }
 }
