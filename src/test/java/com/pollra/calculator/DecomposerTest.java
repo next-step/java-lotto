d7c8ae3ef70b85882a8fa13450cbff6bafa8194f
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-12-01 23:21:59 +0900

    test(Decomposer): 테스트 케이스 추가

--- src/test/java/com/pollra/calculator/DecomposerTest.java
+++ src/test/java/com/pollra/calculator/DecomposerTest.java
@@ -11,26 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class DecomposerTest {
 
     @Test
-    @DisplayName("\"//;\\\\n1;2;3\" 입력 시 Numbers 로 변환")
+    @DisplayName("\"//;\\\\n1;2;3,4:5:6;7,8:9;10\" 입력 시 Numbers 로 변환")
     public void decompose() {
         Decomposer decomposer = new Decomposer();
-        Numbers resultNumbers = decomposer.decompose("//;\\\\n1;2;3");
+        Numbers resultNumbers = decomposer.decompose("//;\\\\n1;2;3,4:5:6;7,8:9;10");
 
         Numbers expectedNumbers = new Numbers(Numbers.toInstance(
-                Arrays.asList(1, 2, 3)
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
         ));
         assertEquals(expectedNumbers, resultNumbers);
     }
 
     @Test
-    @DisplayName("\"1;2;3\" 입력 시 Numbers 로 변환")
+    @DisplayName("\"1:2,3:4:5,6,7,8,9:10\" 입력 시 Numbers 로 변환")
     public void notDecompose() {
         Decomposer decomposer = new Decomposer();
-        Numbers resultNumbers = decomposer.decompose("1;2;3");
+        Numbers resultNumbers = decomposer.decompose("1:2,3:4:5,6,7,8,9:10");
 
         Numbers expectedNumbers = new Numbers(Numbers.toInstance(
-                Arrays.asList(1, 2, 3)
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
         ));
         assertEquals(expectedNumbers, resultNumbers);
+    }
+
+    @Test
+    @DisplayName("\"\"//;\\\\n1:2,3:4:5;6,7,8,9:10;A\" 입력 시 RuntimeException")
+    public void throwableDecompose() {
+        Decomposer decomposer = new Decomposer();
+        assertThrows(RuntimeException.class
+            ,() -> decomposer.decompose("\"//;\\\\n1:2,3:4:5;6,7,8,9:10?A")
+        );
     }
 }
