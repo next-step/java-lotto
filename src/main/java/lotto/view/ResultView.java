ec3562f8ef4aa6fc8808a32485c5ae0cf0af47f0
JaeHan-Kim <hgal8877@gmail.com>
2021-12-02 00:15:18 +0900

    feat : 2등도 출력할 수 있도록 수정

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -6,12 +6,12 @@ import lotto.domain.Rank;
 import lotto.domain.ResultRank;
 
 public class ResultView {
-    private static String PURCHASE_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
-    private static String LOTTO_NUMBER_TEXT_MESSAGE = "[%s]";
-    private static String WIN_MESSAGE = "당첨통계";
-    private static String DASH = "---------";
-    private static String GAME_RANK_MESSAGE = "%d개 일치(%d원)- %d개";
-    private static String GAME_RATE_MESSAGE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+    private static final String PURCHASE_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String LOTTO_NUMBER_TEXT_MESSAGE = "[%s]";
+    private static final String WIN_MESSAGE = "당첨통계";
+    private static final String DASH = "---------";
+    private static final String GAME_RANK_MESSAGE = "%d개 일치%s- %d개";
+    private static final String GAME_RATE_MESSAGE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
 
     public void purchaseLottiesInformation(Lotties lotties) {
         System.out.printf((PURCHASE_LOTTO_COUNT_MESSAGE) + "%n", lotties.purchaseLottiesCount());
@@ -27,11 +27,24 @@ public class ResultView {
         printRank(Rank.FIFTH, result);
         printRank(Rank.FOURTH, result);
         printRank(Rank.THIRD, result);
+        printRank(Rank.SECOND, result);
         printRank(Rank.FIRST, result);
         System.out.println(String.format(GAME_RATE_MESSAGE, result.getRate(purchaseMoney)));
     }
 
     private void printRank(Rank rank, ResultRank result) {
-        System.out.println(String.format(GAME_RANK_MESSAGE, rank.getMatchCount(), rank.getPrizeMoney(), result.getPrizeCount(rank)));
+        System.out.println(String.format(GAME_RANK_MESSAGE, rank.getMatchCount(), this.prizeMoneyText(rank), result.getPrizeCount(rank)));
+    }
+
+    private String prizeMoneyText(Rank rank) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (rank == Rank.SECOND) {
+            stringBuilder.append(", 보너스 볼 일치");
+        }
+        stringBuilder.append("(");
+        stringBuilder.append(rank.getPrizeMoney());
+        stringBuilder.append(")원");
+
+        return stringBuilder.toString();
     }
 }
