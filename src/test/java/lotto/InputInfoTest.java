11b484be540798bd1731dc050ecafc0dca3898a8
jiseok-choi <choi300300@gmail.com>
2021-12-02 10:05:37 +0900

    test: 당첨번호 테스트 코드 추가

--- src/test/java/lotto/InputInfoTest.java
+++ src/test/java/lotto/InputInfoTest.java
@@ -11,4 +11,17 @@ public class InputInfoTest {
         InputInfo inputInfo = new InputInfo("5000");
         Assertions.assertThat(inputInfo.getTicketCount()).isEqualTo(5);
     }
+
+    @Test
+    void 당첨번호_숫자변환_오류() {
+        InputInfo inputInfo = new InputInfo("1000");
+        Assertions.assertThatThrownBy(() -> inputInfo.getAnswer("숫자아닌값")).isInstanceOf(NumberFormatException.class);
+    }
+
+    @Test
+    void 당첨번호_개수_오류() {
+        InputInfo inputInfo = new InputInfo("1000");
+        Assertions.assertThatThrownBy(() -> inputInfo.getAnswer("1,2")).isInstanceOf(NumberFormatException.class);
+    }
+
 }
