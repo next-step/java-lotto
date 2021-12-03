61a1589e1eb319ac417000c431d92cded9dded8d
Han <hankim@tapasmedia.co>
2021-12-03 21:54:18 +0900

    refactor : change exception

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -32,7 +32,7 @@ public class PositiveNumber {
 
     private static void validateNumber(int number) {
         if (number < 0) {
-            throw new RuntimeException();
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
         }
     }
 
