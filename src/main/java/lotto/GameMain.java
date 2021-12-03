4e1f2651363c50631f192355f36f0c5daf7b8621
giyeon95 <giyeon15@gmail.com>
2021-12-04 02:05:22 +0900

    refactor: shop 제거 `shop` 로직이 ResultLottoDTO로 이동

--- src/main/java/lotto/GameMain.java
+++ src/main/java/lotto/GameMain.java
@@ -4,9 +4,9 @@ import lotto.domain.GameResult;
 import lotto.domain.LottoRandomMachine;
 import lotto.domain.LottoTickets;
 import lotto.domain.ResultLotto;
-import lotto.domain.Shop;
 import lotto.domain.Wallet;
 import lotto.dto.AssignBuyPreTicket;
+import lotto.dto.ResultLottoDTO;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -17,17 +17,18 @@ public class GameMain {
         InputView inputView = InputView.create();
         Wallet wallet = inputView.inputMoney();
 
-        AssignBuyPreTicket assignBuyPreTicket = inputView.inputAssignNumberCount(wallet);// 갯수는도 저장해야 하는데?
+        AssignBuyPreTicket assignBuyPreTicket = inputView.inputAssignNumberCount(
+            wallet);// 갯수는도 저장해야 하는데?
         LottoTickets lottoTickets1 = inputView.inputAssignNumber(assignBuyPreTicket);
 
-        Shop shop = new Shop();
-        LottoTickets lottoTickets = shop.buy(wallet, new LottoRandomMachine()); // TODO: assignBuyPreTicket().getWallet() 으로 변경해야 하는가? 2021/12/04 (kiyeon_kim1)
+        ResultLottoDTO resultDto = assignBuyPreTicket.getResultDto(lottoTickets1,
+            new LottoRandomMachine());
 
         OutputView outputView = new OutputView();
-        outputView.printBuyCountMessage(lottoTickets);
+        outputView.printBuyCountMessage(resultDto);
         ResultLotto resultLotto = inputView.inputLottoResultNumber();
 
-        GameResult result = shop.result(lottoTickets, resultLotto);
+        GameResult result = resultDto.result(resultLotto);
         outputView.printByResult(result);
 
     }
