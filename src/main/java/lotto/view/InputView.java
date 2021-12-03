5a6063819acb67dce9bb5f782f0deddffa5dfcbd
Han <hankim@tapasmedia.co>
2021-12-03 21:56:48 +0900

    feat : add insertManualLotto

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,13 @@
 package lotto.view;
 
+import calculator.PositiveNumber;
 import lotto.domain.Credit;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
-import lotto.domain.LottoNumberFactory;
 import lotto.domain.WonLotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,8 +17,8 @@ public class InputView {
     private static final String INSERT_PURCHASE_PRICE = "구입금액을 입력해주세요";
     private static final String INSERT_LAST_WON_LOTTO = "지난 주 당첨 번호를 입력해주세요";
     private static final String INSERT_BONUS_NUMBER = "보너스 볼을 입력해주세요";
-    private static final String INSERT_NON_AUTO_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해주세요.";
-    private static final String INSERT_NON_AUTO_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String INSERT_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해주세요.";
+    private static final String INSERT_MANUAL_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
 
     private final Scanner scanner;
 
@@ -24,12 +26,21 @@ public class InputView {
         this.scanner = scanner;
     }
 
-    public Credit start() {
+    public Credit getCredit() {
         System.out.println(INSERT_PURCHASE_PRICE);
         String line = scanner.nextLine();
-        Credit credit = new Credit(line);
-        System.out.println(String.format(PRINT_CREDIT_COUNT, credit.getLottoCount()));
-        return credit;
+        return new Credit(line);
+    }
+
+    public PositiveNumber getManualLottoCount() {
+        System.out.println(INSERT_MANUAL_LOTTO_COUNT);
+        String line = scanner.nextLine();
+        return new PositiveNumber(line);
+    }
+
+    public List<Lotto> insertManualLotto(PositiveNumber manualLottoCount) {
+        System.out.println(INSERT_MANUAL_LOTTO_NUMBER);
+        return createByString(manualLottoCount.getNumber());
     }
 
     public WonLotto insertWonLotto() {
@@ -39,5 +50,16 @@ public class InputView {
         System.out.println(INSERT_BONUS_NUMBER);
         String bonusNumber = scanner.nextLine();
         return WonLotto.of(new Lotto(wonNumbers), new LottoNumber(bonusNumber));
+    }
+
+    private List<Lotto> createByString(int number) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < number; i++) {
+            String lottoString = scanner.nextLine();
+            lottos.add(new Lotto(lottoString));
+        }
+
+        return lottos;
     }
 }
