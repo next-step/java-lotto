3c2c3ef05915565ee8e3f1516cbcc91548c3b852
hyunju <guswnt@gmail.com>
2021-12-04 09:46:09 +0900

    style: step2 Lotto 포맷팅

--- src/main/java/step2/domain/Lotto.java
+++ src/main/java/step2/domain/Lotto.java
@@ -16,9 +16,9 @@ public class Lotto {
         this.price = price;
     }
 
-    public Map<Integer, Integer> countNumberOfMatching(LottoWinningNumbers lottoWinningNumbers){
+    public Map<Integer, Integer> countNumberOfMatching(LottoWinningNumbers lottoWinningNumbers) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(LottoNumbers lottoNumbers : lottoNumbersList){
+        for (LottoNumbers lottoNumbers : lottoNumbersList) {
             int numberOfMatching = lottoNumbers.numberOfMatching(lottoWinningNumbers);
             int count = map.getOrDefault(numberOfMatching, 1);
             map.put(numberOfMatching, count);
@@ -26,30 +26,37 @@ public class Lotto {
         return map;
     }
 
-    public int prizeMoney(LottoWinningNumbers lottoWinningNumbers){
-        Map<Integer, Integer> map = countNumberOfMatching(lottoWinningNumbers);
-        return map.entrySet().stream().map((m) -> lottoWinningRules.getPrizeOf(m.getKey()) * m.getValue()).reduce(0, Integer::sum);
+    public int prizeMoney(LottoWinningNumbers lottoWinningNumbers) {
+        Map<Integer, Integer> countNumberOfMatching = countNumberOfMatching(lottoWinningNumbers);
+        return prizeMoney(countNumberOfMatching);
     }
 
-    public int prizeMoney(Map<Integer, Integer> countNumberOfMatching){
-        return countNumberOfMatching.entrySet().stream().map((m) -> lottoWinningRules.getPrizeOf(m.getKey()) * m.getValue()).reduce(0, Integer::sum);
+    public int prizeMoney(Map<Integer, Integer> countNumberOfMatching) {
+        return countNumberOfMatching.entrySet().stream()
+                .map(m -> {
+                    int numberOfMatching = m.getKey();
+                    int prizeMoney = lottoWinningRules.getPrizeOf(numberOfMatching);
+                    int count = m.getValue();
+                    return prizeMoney * count;
+                })
+                .reduce(0, Integer::sum);
     }
 
-    public int earningRate(LottoWinningNumbers lottoWinningNumbers){
+    public int earningRate(LottoWinningNumbers lottoWinningNumbers) {
         int prizeMoney = prizeMoney(lottoWinningNumbers);
         return prizeMoney / price;
     }
 
-    public int earningRate(Map<Integer, Integer> countNumberOfMatching){
+    public int earningRate(Map<Integer, Integer> countNumberOfMatching) {
         int prizeMoney = prizeMoney(countNumberOfMatching);
         return prizeMoney / price;
     }
 
-    public int size(){
+    public int size() {
         return lottoNumbersList.size();
     }
 
-    public List<LottoNumbers> getLottoNumbersList(){
+    public List<LottoNumbers> getLottoNumbersList() {
         return Collections.unmodifiableList(lottoNumbersList);
     }
 
