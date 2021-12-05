10c0ba31ef68e8e39d120f3f008b5dd18e5ab33d
JaeHan-Kim <hgal8877@gmail.com>
2021-12-06 00:25:38 +0900

    feat : 수동 금액을 지불 할 수 있는 메서드 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class Money {
     private final int value;
 
@@ -13,5 +15,22 @@ public class Money {
 
     public int getValue() {
         return value;
+    }
+
+    public Money payMoney(int manualLottoCount) {
+        return new Money(this.value - (Store.LOTTO_ONE_GAME_PRICE * manualLottoCount));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
