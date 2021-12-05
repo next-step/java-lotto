b7a15b616e3887a8415ca8bbcb00917caeb07b93
JaeHan-Kim <hgal8877@gmail.com>
2021-12-05 23:59:44 +0900

    test : 로또 수동 구입 추가

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,6 +12,12 @@ public class Store {
         int gameCount = money.getValue() / LOTTO_ONE_GAME_PRICE;
         return new Lotties(IntStream.range(FIRST_GAME, gameCount)
                 .mapToObj(n -> numberGenerator.publish())
+                .collect(Collectors.toList()));
+    }
+
+    public Lotties purchaseManualLotto(List<String> lottoPaper) {
+        return new Lotties(lottoPaper.stream()
+                .map(Lotto::from)
                 .collect(Collectors.toList()));
     }
 }
