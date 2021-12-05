e9127a6a96ce89b4ce7e1d3e8972a62835a870fe
JaeHan-Kim <hgal8877@gmail.com>
2021-12-05 23:05:04 +0900

    test : 게임결과 테스트 케이스 추가

--- src/test/java/lotto/domain/LottiesTest.java
+++ src/test/java/lotto/domain/LottiesTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LottiesTest {
 
@@ -16,5 +15,24 @@ class LottiesTest {
         Lotties lotties = new Lotties(Arrays.asList(Lotto.from("1, 2, 3, 4, 5, 6")));
 
         assertThat(lotties).isNotNull();
+    }
+
+    @DisplayName("게임결과 테스트 - 5등 당첨 한개, 꽝 4개의")
+    @Test
+    void checkGameResult() {
+        Lotto lotto = Lotto.from("2, 9, 13, 33, 39, 45");
+        Lotto lotto2 = Lotto.from("9, 10, 11, 20, 22, 31");
+        Lotto lotto3 = Lotto.from("26, 27, 36, 37, 41, 45");
+        Lotto lotto4 = Lotto.from("2, 16, 28, 31, 38, 45");
+        Lotto lotto5 = Lotto.from("9, 14, 16, 29, 39, 44");
+        Lotties lotties = new Lotties(Arrays.asList(lotto, lotto2, lotto3, lotto4, lotto5));
+        Lotto winLotto = Lotto.from("2, 4, 25, 26, 36, 37");
+        LottoNumber bonusNumber = new LottoNumber("28");
+        WinLotto lastWeekWinLotto = new WinLotto(winLotto, bonusNumber);
+
+        ResultRank resultRank = lotties.gameResultRank(lastWeekWinLotto);
+
+        assertThat(resultRank.getPrizeCount(Rank.FIFTH)).isEqualTo(1);
+        assertThat(resultRank.getPrizeCount(Rank.OUT)).isEqualTo(4);
     }
 }
