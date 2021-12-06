c1f96f0e07d679e2b68c745f8ae951e4108c5871
sojeongw <sojeongw.official@gmail.com>
2021-12-07 08:11:30 +0900

    feat: 로또 한 장을 생성한다

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,47 +1,39 @@
 package lotto;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Lotto {
-    private final LottoNumbers lottoNumbers;
+    private final List<Integer> numbers;
 
-    public Lotto() {
-        this.lottoNumbers = new LottoNumbers();
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
     }
 
-    public Lotto(LottoNumbers lottoNumbers) {
-        this.lottoNumbers = lottoNumbers;
-    }
+    public static Lotto from(List<Integer> numbers) {
+        if (numbers.size() < 6) {
+            throw new IllegalArgumentException("로또 숫자는 6자리여야 합니다.");
+        }
 
-    public LottoNumbers lottoNumbers() {
-        return this.lottoNumbers;
-    }
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("로또 숫자는 1 이상 45 이하여야 합니다.");
+            }
+        }
 
-    public boolean result(LottoNumbers winningNumbers, Condition condition) {
-        return lottoNumbers.result(winningNumbers, condition);
+        return new Lotto(numbers);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Lotto lotto = (Lotto) o;
-        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+        return Objects.equals(numbers, lotto.numbers);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lottoNumbers);
-    }
-
-    @Override
-    public String toString() {
-        return "Lotto{" +
-                "lottoNumbers=" + lottoNumbers +
-                '}';
+        return Objects.hash(numbers);
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,31 +1,35 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoTest {
+public class LottoTest {
 
     @Test
-    @DisplayName("숫자 6개가 담긴 Lotto 객체를 생성한다.")
-    void shouldCreateLotto() {
-        LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-        Lotto lotto = new Lotto(lottoNumbers);
-
-        assertThat(lotto).isEqualTo(new Lotto(lottoNumbers));
+    void shouldCreateSameObject() {
+        Lotto lotto = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertThat(lotto).isEqualTo(Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6)));
     }
 
     @Test
-    @DisplayName("주어진 개수만큼 당첨 번호와 일치하는지 확인한다")
-    void shouldCheckWinningNumbers() {
-        LottoNumbers lottoNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 4, 5, 6));
-        LottoNumbers winningNumbers = new LottoNumbers(Arrays.asList(1, 2, 3, 7, 8, 9));
-        Lotto lotto = new Lotto(lottoNumbers);
+    void shouldThrowWhenInputNotSixNumbers() {
+        assertThatThrownBy(() -> Lotto.from(Arrays.asList(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 숫자는 6자리여야 합니다.");
+    }
 
-        boolean matchedThreeNumbers = lotto.result(winningNumbers, count -> count == 3);
-        assertThat(matchedThreeNumbers).isTrue();
+    @Test
+    void shouldThrowWhenInputNotInRange() {
+        assertThatThrownBy(() -> Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 숫자는 1 이상 45 이하여야 합니다.");
+
+        assertThatThrownBy(() -> Lotto.from(Arrays.asList(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 숫자는 1 이상 45 이하여야 합니다.");
     }
 }
