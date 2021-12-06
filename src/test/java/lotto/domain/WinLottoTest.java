527f05d75743b92aa99555240674f0af0f8b0dab
JaeHan-Kim <hgal8877@gmail.com>
2021-12-07 00:10:35 +0900

    test : Rank를 구하는 테스트 추가

--- src/test/java/lotto/domain/WinLottoTest.java
+++ src/test/java/lotto/domain/WinLottoTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -27,5 +29,33 @@ class WinLottoTest {
         assertThatThrownBy(() -> new WinLotto(winLotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("보너스 번호는 당첨 번호와 종복될 수 없습니다.");
+    }
+
+
+    @DisplayName("로또의 당첨 등수 체크 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"2, 9, 13, 33 ,39, 45:34, 40, 13, 2 ,9, 44:1"}, delimiter = ':')
+    void lottoRankCheck(String buyLottoString, String winLottoString, String bonusString) {
+        Lotto buyLotto = Lotto.from(buyLottoString);
+        Lotto winLotto = Lotto.from(winLottoString);
+        LottoNumber bonusLottoNumber = new LottoNumber(bonusString);
+        WinLotto lastWeekWinLotto = new WinLotto(winLotto, bonusLottoNumber);
+
+        Rank rank = lastWeekWinLotto.getRank(buyLotto);
+
+        assertThat(rank).isEqualTo(Rank.FIFTH);
+    }
+
+    @DisplayName("2등 당첨 테스트")
+    @Test
+    void secondRankLotto() {
+        Lotto buyLotto = Lotto.from("2, 9, 13, 33 ,39, 45");
+        Lotto winLotto = Lotto.from("2, 9, 13, 33 ,39, 44");
+        LottoNumber bonusLottoNumber = new LottoNumber("45");
+        WinLotto lastWeekWinLotto = new WinLotto(winLotto, bonusLottoNumber);
+
+        Rank rank = lastWeekWinLotto.getRank(buyLotto);
+
+        assertThat(rank).isEqualTo(Rank.SECOND);
     }
 }
