ec1dc59b7a71cdf949c4cdb184515d588cc6fe8b
JaeHan-Kim <hgal8877@gmail.com>
2021-12-06 23:43:12 +0900

    fix : 상수형에 final 추가

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Store {
-    private static int FIRST_GAME = 0;
-    public static int LOTTO_ONE_GAME_PRICE = 1000;
+    private static final int FIRST_GAME = 0;
+    public static final int LOTTO_ONE_GAME_PRICE = 1000;
 
     public Lotties purchaseLottiesByMoney(Money money, LottoGenerator numberGenerator) {
         int gameCount = money.getValue() / LOTTO_ONE_GAME_PRICE;
