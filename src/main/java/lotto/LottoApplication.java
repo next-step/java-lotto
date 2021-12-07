8aaf41c62c5864dd3f7c9283af1d2ef7ba54c0f6
sojeongw <sojeongw.official@gmail.com>
2021-12-07 23:49:16 +0900

    feat: 당첨 번호를 입력한다

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,12 +1,14 @@
 package lotto;
 
 import lotto.view.InputView;
+import lotto.view.ResultView;
 
 import java.util.Scanner;
 
 public class LottoApplication {
     private static final Scanner scanner = new Scanner(System.in);
     public static final int LOTTO_PRICE = 1000;
+    public static final String DELIMITER = ", ";
 
     public static void main(String[] args) {
         InputView.printPurchaseRequest();
@@ -14,13 +16,20 @@ public class LottoApplication {
         int purchaseAmount = scanner.nextInt();
         int quantity = quantity(purchaseAmount);
 
-        InputView.printQuantity(quantity);
+        ResultView.printQuantity(quantity);
 
         for (int i = 0; i < quantity; i++) {
             LottoNumbers lottoNumbers = LottoMachine.generateLottoNumber();
             Lotto lotto = Lotto.from(lottoNumbers);
-            InputView.printLottoNumbers(lotto);
+            ResultView.printLottoNumbers(lotto);
         }
+
+        InputView.printWinningNumbersRequest();
+        String[] winningNumbers = winningNumbers();
+    }
+
+    private static String[] winningNumbers() {
+        return scanner.next().split(DELIMITER);
     }
 
     private static int quantity(int purchaseAmount) {
