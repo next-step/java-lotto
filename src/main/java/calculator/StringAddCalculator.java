ffa62e00b74ce7e6b75bd23426e56f522af0131d
minchoi9509 <minchoi9509@gmail.com>
2021-12-07 20:36:34 +0900

    [feat] 콤마(,) 구분 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,11 +2,18 @@ package calculator;
 
 public class StringAddCalculator {
 
+    private static final String TOKEN_DELIMITE = ",";
+
     public static int splitAndSum(String expression) {
         if (expression == null || expression.isEmpty()) {
             return 0;
         }
 
-        return Integer.parseInt(expression);
+        String[] numbers = expression.split(TOKEN_DELIMITE);
+        int result = 0;
+        for (String number : numbers) {
+            result += Integer.parseInt(number);
+        }
+        return result;
     }
 }
