304035557a143aeae27d0767384c831a2b344507
minchoi9509 <minchoi9509@gmail.com>
2021-12-07 20:40:36 +0900

    [style] TOKEN_DELIMIT -> TOKEN_DELIMITER 오타 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,14 +2,14 @@ package calculator;
 
 public class StringAddCalculator {
 
-    private static final String TOKEN_DELIMITE = ",|:";
+    private static final String TOKEN_DELIMITER = ",|:";
 
     public static int splitAndSum(String expression) {
         if (expression == null || expression.isEmpty()) {
             return 0;
         }
 
-        String[] numbers = expression.split(TOKEN_DELIMITE);
+        String[] numbers = expression.split(TOKEN_DELIMITER);
         int result = 0;
         for (String number : numbers) {
             result += Integer.parseInt(number);
