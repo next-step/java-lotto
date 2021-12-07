ef160ad98cc6466e06ed20771bd55e93dc0dd30a
kero <jdy410@naver.com>
2021-12-08 01:32:26 +0900

    feat: equals, hashCode 추가

--- src/main/java/step2/dto/WinningRate.java
+++ src/main/java/step2/dto/WinningRate.java
@@ -1,4 +1,6 @@
-package step2.domain;
+package step2.dto;
+
+import java.util.Objects;
 
 public class WinningRate {
     private final float winningRate;
@@ -9,5 +11,22 @@ public class WinningRate {
 
     public float getWinningRate() {
         return winningRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        WinningRate that = (WinningRate) o;
+        return Float.compare(that.winningRate, winningRate) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winningRate);
     }
 }
