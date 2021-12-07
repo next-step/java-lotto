fcb75a63cf633ceedb63827cedada200f2b503c2
jiseok-choi <choi300300@gmail.com>
2021-12-08 01:10:57 +0900

    test: 리뷰 반영  - 2등 당첨 테스트 코드 추가

--- src/test/java/lotto/ResultTest.java
+++ src/test/java/lotto/ResultTest.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.domain.*;
 import lotto.view.InputInfo;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,10 +13,14 @@ import java.util.Map;
 
 public class ResultTest {
 
-    @Test
-    void 당첨결과_테스트() {
-        List<Lotto> sampleLottos = new ArrayList<>();
-        InputInfo inputInfo = new InputInfo("1000");
+    static List<Lotto> sampleLottos;
+    static InputInfo inputInfo;
+    static Lottos lottos;
+
+    @BeforeAll
+    static void 사전_로또_번호_생성() {
+        sampleLottos = new ArrayList<>();
+        inputInfo = new InputInfo("1000");
 
         sampleLottos.add(Lotto.newInstance(inputInfo.getAnswer("1,2,3,4,5,6")));
         sampleLottos.add(Lotto.newInstance(inputInfo.getAnswer("11,12,13,14,15,16")));
@@ -23,7 +28,12 @@ public class ResultTest {
         sampleLottos.add(Lotto.newInstance(inputInfo.getAnswer("31,32,33,34,35,36")));
         sampleLottos.add(Lotto.newInstance(inputInfo.getAnswer("40,41,42,43,44,45")));
 
-        Lottos lottos = new Lottos(sampleLottos);
+        lottos = new Lottos(sampleLottos);
+    }
+
+
+    @Test
+    void 당첨결과_테스트() {
         Result result = new Result(lottos, Lotto.newInstance(inputInfo.getAnswer("1,2,3,7,8,9")), new Bonus(10));
         Map<Rank, Integer> sampleResult = new HashMap<>();
 
@@ -39,4 +49,23 @@ public class ResultTest {
         // 수익금
         Assertions.assertThat(result.getRateOfReturn()).isEqualTo(1);
     }
+
+    @Test
+    void 당첨결과_2등_테스트() {
+        Result result = new Result(lottos, Lotto.newInstance(inputInfo.getAnswer("1,2,3,7,8,9")), new Bonus(6));
+        Map<Rank, Integer> sampleResult = new HashMap<>();
+
+        sampleResult.put(Rank.MISS, 0);
+        sampleResult.put(Rank.FIFTH, 0);
+        sampleResult.put(Rank.FOURTH, 0);
+        sampleResult.put(Rank.THIRD, 0);
+        sampleResult.put(Rank.SECOND, 1);
+        sampleResult.put(Rank.FIRST, 0);
+
+        // 2등 당첨 결과 내용
+        Assertions.assertThat(result.getResultInfo()).isEqualTo(sampleResult);
+        // 수익금
+        Assertions.assertThat(result.getRateOfReturn()).isEqualTo(6000);
+    }
+
 }
