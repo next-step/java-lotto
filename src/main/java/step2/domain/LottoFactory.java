5947281a86bfc7d69c76e471e1dec32533d89157
hyunju <guswnt@gmail.com>
2021-12-08 03:19:33 +0900

    refactor: step2 문자열 숫자 변환 예외 처리

--- src/main/java/step2/domain/LottoFactory.java
+++ src/main/java/step2/domain/LottoFactory.java
@@ -1,5 +1,7 @@
 package step2.domain;
 
+import step2.exception.LottoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +12,20 @@ public class LottoFactory {
     }
 
     public static Lotto buyWithMoney(String moneyString) {
-        int money = Integer.parseInt(moneyString);
+        int money = toInt(moneyString);
         return buyWithMoney(money);
     }
+
+    private static int toInt(String moneyString) {
+        try{
+            return Integer.parseInt(moneyString);
+        }
+        catch(NumberFormatException e){
+            throw new LottoException("구입할 로또 가격을 숫자로 입력해야합니다.");
+        }
+
+    }
+
 
     public static Lotto buyWithMoney(int money) {
         int numberOfLottoNumbers = money / LOTTO_PRICE;
