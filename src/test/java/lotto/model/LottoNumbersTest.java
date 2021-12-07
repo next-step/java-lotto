a3590e3f98c6ab53fa018c850c2641a44e30ae98
김민혁 <kminheuk@gmail.com>
2021-12-07 23:47:08 +0900

    refactor: Refactoring LottoNumbers test to be simple

--- src/test/java/lotto/model/LottoNumbersTest.java
+++ src/test/java/lotto/model/LottoNumbersTest.java
@@ -37,20 +37,8 @@ class LottoNumbersTest {
     @DisplayName("두 LottoNumbers 중 같은 LottoNumber 의 개수를 반환한다")
     void testCountSameLottoNumber() {
         // Given
-        LottoNumbers lottoNumbersA = new LottoNumbers(Arrays.asList(
-                new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6)));
-        LottoNumbers lottoNumbersB = new LottoNumbers(Arrays.asList(
-                new LottoNumber(11),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(44),
-                new LottoNumber(5),
-                new LottoNumber(6)));
+        LottoNumbers lottoNumbersA = buildLottoNumbers(1, 2, 3, 4, 5, 6);
+        LottoNumbers lottoNumbersB = buildLottoNumbers(1, 2, 3, 4, 35, 36);
 
         // When
         long count = lottoNumbersA.countSameLottoNumber(lottoNumbersB);
