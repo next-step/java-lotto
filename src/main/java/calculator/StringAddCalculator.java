304d992356e1259ab4c791fa8c2437eeac85c0e9
minchoi9509 <minchoi9509@gmail.com>
2021-12-09 00:34:12 +0900

    [feat] 사용자 지정 구분자 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,21 +1,36 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
     private static final String TOKEN_DELIMITER = ",|:";
+    private static final String REG_EXP = "//(.)\n(.*)";
 
     public static int splitAndSum(String expression) {
         if (expression == null || expression.isEmpty()) {
             return 0;
         }
 
-        return sumNumbers(expression.split(TOKEN_DELIMITER));
+        return sumNumbers(splitExpression(expression));
     }
 
     private static int sumNumbers(String[] numbers) {
         return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+    }
+
+    private static String[] splitExpression(String expression) {
+        Matcher matcher = Pattern.compile(REG_EXP).matcher(expression);
+        String delimiter = TOKEN_DELIMITER;
+
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            expression = matcher.group(2);
+        }
+
+        return expression.split(delimiter);
     }
 
 }
