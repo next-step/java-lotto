ce3d1f5feabbbbbe285ef9ea7cc3e51326c3b021
minchoi9509 <minchoi9509@gmail.com>
2021-12-09 00:43:53 +0900

    [refactor] 토큰화 해주는 Tokenization 클래스 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,36 +1,21 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-
-    private static final String TOKEN_DELIMITER = ",|:";
-    private static final String REG_EXP = "//(.)\n(.*)";
 
     public static int splitAndSum(String expression) {
         if (expression == null || expression.isEmpty()) {
             return 0;
         }
 
-        return sumNumbers(splitExpression(expression));
+        Tokenization tokenization = new Tokenization(expression);
+
+        return sumNumbers(tokenization.splitExpression());
     }
 
     private static int sumNumbers(String[] numbers) {
         return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
-    }
-
-    private static String[] splitExpression(String expression) {
-        Matcher matcher = Pattern.compile(REG_EXP).matcher(expression);
-        String delimiter = TOKEN_DELIMITER;
-
-        if (matcher.find()) {
-            delimiter = matcher.group(1);
-            expression = matcher.group(2);
-        }
-
-        return expression.split(delimiter);
     }
 
 }
---
+++ src/main/java/calculator/Tokenization.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Tokenization {
+
+    private static final String TOKEN_DELIMITER = ",|:";
+    private static final String REG_EXP = "//(.)\n(.*)";
+
+    private String expression;
+
+    public Tokenization(String expression) {
+        this.expression = expression;
+    }
+
+    public String[] splitExpression() {
+        Matcher matcher = Pattern.compile(REG_EXP).matcher(expression);
+        String delimiter = TOKEN_DELIMITER;
+
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            this.expression = matcher.group(2);
+        }
+
+        return this.expression.split(delimiter);
+    }
+
+}
