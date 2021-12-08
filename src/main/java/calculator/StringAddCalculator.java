541de9bdf4b6bb6add12e257a239a38dc7d7ecf9
minchoi9509 <minchoi9509@gmail.com>
2021-12-09 00:13:39 +0900

    [refactor] 덧셈 메소드 분리, Stream 형태로 변경

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
     private static final String TOKEN_DELIMITER = ",|:";
@@ -9,11 +11,11 @@ public class StringAddCalculator {
             return 0;
         }
 
-        String[] numbers = expression.split(TOKEN_DELIMITER);
-        int result = 0;
-        for (String number : numbers) {
-            result += Integer.parseInt(number);
-        }
-        return result;
+        return sumNumbers(expression.split(TOKEN_DELIMITER));
     }
+
+    private static int sumNumbers(String[] numbers) {
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+    }
+
 }
