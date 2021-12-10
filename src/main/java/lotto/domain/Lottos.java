80921f0075e93f08bb93823f5cf08e8ed407b88c
orgojy <giveawesome@gmail.com>
2021-12-11 02:02:33 +0900

    feat(Lottos): 구입한 모든 `Lotto`를 가진 `Lottos` 구현

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Lottos {
+    public static final int MIN_LOTTO_PRICE = 1_000;
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        validate(lottos);
+        this.lottos = lottos;
+    }
+
+    public static Lottos from(int size) {
+        return new Lottos(IntStream.range(0, size)
+                .mapToObj(v -> new Lotto())
+                .collect(Collectors.toList()));
+    }
+
+    public LottoResult retrieveStatistics(Lotto winningLotto) {
+        return new LottoResult(findLottoResultType(winningLotto));
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public int size() {
+        return lottos.size();
+    }
+
+    private void validate(List<Lotto> lottos) {
+        if (Objects.isNull(lottos)) {
+            throw new IllegalArgumentException("lottos가 null입니다.");
+        }
+        if (lottos.isEmpty()) {
+            throw new IllegalArgumentException("lottos의 사이즈가 0입니다.");
+        }
+    }
+
+    private List<LottoResultType> findLottoResultType(Lotto winningLottoNumbers) {
+        return lottos.stream()
+                .map(lotto -> lotto.findLottoResultType(winningLottoNumbers.getNumbers()))
+                .collect(Collectors.toList());
+    }
+}
