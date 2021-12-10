ee5ba1f496e151b12f254133594af312f3da9b7f
orgojy <giveawesome@gmail.com>
2021-12-11 02:41:53 +0900

    feat(OutputView): 게임에 필요한 정보를 출력할 `OutputView` 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,53 @@
+package lotto.view;
+
+import lotto.domain.LottoResult;
+import lotto.domain.LottoResultType;
+import lotto.domain.Lottos;
+
+import java.util.Map;
+
+public class OutputView {
+    private static final String LOTTO_SIZE_MESSAGE = "%d개를 구매했습니다.\n";
+    private static final String STATISTICS_TITLE_MESSAGE = "\n당첨 통계\n---------------------";
+    private static final String LOTTO_STATISTICS_MESSAGE = "%,d개 일치 (%,d원)- %,d개%n";
+    private static final String EARNINGS_RATIO_MESSAGE = "총 수익률은 %,.2f입니다.\n";
+
+    private OutputView() {
+        throw new AssertionError();
+    }
+
+    public static void printPurchasedLottoInformation(Lottos lottos) {
+        printPurchasedLottoSize(lottos);
+        printNumbersForLottos(lottos);
+    }
+
+    public static void printStatisticsForLottos(LottoResult lottoResult) {
+        System.out.println(STATISTICS_TITLE_MESSAGE);
+        printStatisticsForLotto(lottoResult);
+        printEarningsRatio(lottoResult.getEarningsRatio());
+    }
+
+    private static void printPurchasedLottoSize(Lottos lottos) {
+        System.out.printf(LOTTO_SIZE_MESSAGE, lottos.size());
+    }
+
+    private static void printNumbersForLottos(Lottos lottos) {
+        lottos.getLottos()
+                .forEach(lotto -> System.out.println(lotto.toString()));
+        System.out.println();
+    }
+
+    private static void printStatisticsForLotto(LottoResult lottoResult) {
+        for (LottoResultType lottoResultType : LottoResultType.WINNING_TYPES) {
+            int matchedCount = lottoResultType.getMatchedCount();
+            int reward = lottoResultType.getReward();
+            Map<LottoResultType, Integer> statistics = lottoResult.getStatistics();
+            int count = statistics.get(lottoResultType);
+            System.out.printf(LOTTO_STATISTICS_MESSAGE, matchedCount, reward, count);
+        }
+    }
+
+    private static void printEarningsRatio(double earningsRatio) {
+        System.out.printf(EARNINGS_RATIO_MESSAGE, earningsRatio);
+    }
+}
