78e9c57d99506c1de797bb56b6d5a77f5949e932
orgojy <giveawesome@gmail.com>
2021-12-13 03:04:15 +0900

    feat(OutputView.java): 로또 게임 통계 결과에서 보너스 결과 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,8 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
-import lotto.domain.LottoResult;
 import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
 
 import java.util.stream.Collectors;
@@ -11,7 +11,10 @@ public class OutputView {
     private static final String LOTTO_SIZE_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String STATISTICS_TITLE_MESSAGE = "\n당첨 통계\n---------------------";
     private static final String LOTTO_STATISTICS_MESSAGE = "%,d개 일치 (%,d원)- %,d개%n";
-    private static final String EARNINGS_RATIO_MESSAGE = "총 수익률은 %,.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n";
+    private static final String LOTTO_BONUS_STATISTICS_MESSAGE = "%,d개 일치, 보너스 볼 일치(%,d원)- %,d개%n";
+    private static final String EARNINGS_RATIO_PROFIT_MESSAGE = "총 수익률은 %,.2f입니다.\n";
+    private static final String EARNINGS_RATIO_LOSS_MESSAGE = "총 수익률은 %,.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n";
+    private static final int BASE_EARNINGS_POINT = 1;
 
     private OutputView() {
         throw new AssertionError();
@@ -47,12 +50,25 @@ public class OutputView {
 
     private static void printStatisticsForLotto(LottoResult lottoResult) {
         for (LottoRank lottoRank : LottoRank.WINNING_TYPES) {
-            System.out.printf(LOTTO_STATISTICS_MESSAGE,
-                    lottoRank.matchedCount(), lottoRank.reward(), lottoResult.countByType(lottoRank));
+            System.out.print(statisticMessage(lottoResult, lottoRank));
         }
     }
 
+    private static String statisticMessage(LottoResult lottoResult, LottoRank lottoRank) {
+        if (lottoRank.notBonus()) {
+            return String.format(LOTTO_STATISTICS_MESSAGE, lottoRank.matchedCount(), lottoRank.reward(), lottoResult.countByType(lottoRank));
+        }
+        return String.format(LOTTO_BONUS_STATISTICS_MESSAGE, lottoRank.matchedCount(), lottoRank.reward(), lottoResult.countByType(lottoRank));
+    }
+
     private static void printEarningsRatio(double earningsRatio) {
-        System.out.printf(EARNINGS_RATIO_MESSAGE, earningsRatio);
+        System.out.print(earningsRatioMessage(earningsRatio));
+    }
+
+    private static String earningsRatioMessage(double earningsRatio) {
+        if (earningsRatio < BASE_EARNINGS_POINT) {
+            return String.format(EARNINGS_RATIO_LOSS_MESSAGE, earningsRatio);
+        }
+        return String.format(EARNINGS_RATIO_PROFIT_MESSAGE, earningsRatio);
     }
 }
