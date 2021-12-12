9be16839d49ca1d16b4e884361886ed602e27027
haeuniii <qnfnqnfn9825@gmail.com>
2021-12-12 10:30:28 +0900

    refactor: 전략을 컨트롤러에서 주입하도록 기능 변경

--- src/main/java/step2/controller/LotteryController.java
+++ src/main/java/step2/controller/LotteryController.java
@@ -2,25 +2,33 @@ package step2.controller;
 
 import step2.domain.Lotteries;
 import step2.domain.Lottery;
+import step2.domain.Number;
 import step2.domain.Statistics;
+import step2.strategy.ManualLotteryStrategy;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class LotteryController {
     private static final int LOTTERY_PURCHASE_PRICE = 1000;
     private static final String LOTTERY_SPLITTER = ",";
 
     public Statistics getStatistics(Lotteries lotteries, String winningNumbers) {
-        return new Statistics(lotteries, getWinningLottery(winningNumbers));
+        return new Statistics(lotteries, getWinningNumbers(winningNumbers));
     }
 
     public Lotteries getLotteries(int purchaseAmount) {
-        return new Lotteries(purchaseAmount / LOTTERY_PURCHASE_PRICE);
+        return Lotteries.createFromCount(new ManualLotteryStrategy(), purchaseAmount / LOTTERY_PURCHASE_PRICE);
     }
 
     public double getProfitRate(Statistics statistics, int purchaseAmount) {
         return statistics.calculateProfitRate(purchaseAmount);
     }
 
-    private Lottery getWinningLottery(String winningNumbers) {
-        return Lottery.createFromArray(winningNumbers.split(LOTTERY_SPLITTER));
+    private Lottery getWinningNumbers(String winningNumbers) {
+        return Lottery.createFromList(Arrays.stream(winningNumbers.split(LOTTERY_SPLITTER))
+                .map(n -> Number.createFromString(n))
+                .collect(Collectors.toList())
+        );
     }
 }
