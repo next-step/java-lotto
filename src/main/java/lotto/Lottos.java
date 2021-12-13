46074d5a02cbe03d679cf569df482e3863dc3fde
dodeon101 <dodeon@101.inc>
2021-12-13 20:55:13 +0900

    feat: 로또 리스트를 일급 컬렉션으로 묶는다

---
+++ src/main/java/lotto/Lottos.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public long match(LottoNumbers winningNumbers, MatchedNumbers matchedNumbers) {
+        return lottos.stream()
+                .filter(lotto -> lotto.match(
+                        winningNumbers,
+                        count -> count == matchedNumbers.value())
+                )
+                .count();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lottos lottos1 = (Lottos) o;
+        return Objects.equals(lottos, lottos1.lottos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos);
+    }
+}
