2737814657be650888d1e9cc401fbf708f57843a
dodeon101 <dodeon@101.inc>
2021-12-13 20:53:33 +0900

    refactor: 일치하는 로또 개수를 나타내는 enum 명을 MatchedNumbers로 수정한다

--- src/main/java/lotto/MatchedNumbers.java
+++ src/main/java/lotto/MatchedNumbers.java
@@ -1,21 +1,25 @@
 package lotto;
 
-public enum MatchingCount {
+public enum MatchedNumbers {
     THREE(3, 5_000),
     FOUR(4, 50_000),
     FIVE(5, 1_500_000),
     SIX(6, 2_000_000_000);
 
-    private final int count;
+    private final int matchedNumber;
     private final int price;
 
-    MatchingCount(int count, int price) {
-        this.count = count;
+    MatchedNumbers(int matchedNumber, int price) {
+        this.matchedNumber = matchedNumber;
         this.price = price;
     }
 
     public int value() {
-        return count;
+        return matchedNumber;
+    }
+
+    public int price() {
+        return price;
     }
 
     public long prize(Long quantity) {
