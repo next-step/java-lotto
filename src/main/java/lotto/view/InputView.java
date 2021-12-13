3f522dc9f54527cdb2652091d359929a2f54369e
dodeon101 <dodeon@101.inc>
2021-12-13 20:56:03 +0900

    refactor: InputView를 리팩터링 한다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,15 +1,23 @@
 package lotto.view;
 
-public class InputView {
+import java.util.Scanner;
 
-    private static final String PURCHASE_REQUEST_MESSAGE = "구입 금액을 입력해주세요.";
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
     private static final String WINNING_NUMBER_REQUEST_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
 
     private InputView() {
     }
 
-    public static void printPurchaseRequest() {
-        System.out.println(PURCHASE_REQUEST_MESSAGE);
+    public static int getPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static String[] getWinningNumbers() {
+        return scanner.next().split(DELIMITER);
     }
 
     public static void printWinningNumbersRequest() {
