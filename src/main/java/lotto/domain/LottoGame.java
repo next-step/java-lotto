36bd2b7bc58269ca3097cd7461badf0f49545d37
javajigi <javajigi@gmail.com>
2021-12-15 15:11:29 +0900

    docs(todo): document validation check list

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class LottoGame {
     public static int match(List<Integer> userLotto, List<Integer> winningLotto, int bonusNo) {
+        // TODO 인자에 대한 유효성 체크
+        // * 로또 한 장은 6개의 번호여야 한다.
+        // * 6개의 각 숫자는 1에서 45 사이의 값이어야 한다.
+        // * 6개의 값은 중복된 값이 있으면 안된다.
+        // * 당첨 번호의 경우는 보너스 번호가 1에서 45 사이의 값이어야 하여, 보너스 번호는 당첨 번호 6개의 값이 아니어야 한다.
+
         int matchCount = match(userLotto, winningLotto);
         if (matchCount == 6) {
             return 1;
