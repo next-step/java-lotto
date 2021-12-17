376fc6b6716b90a5341bf864dac82dc50185c118
gyuyeol <gyuyeol.lee@live.com>
2021-12-18 04:34:49 +0900

    feat(WinningLotteryNumbers): WinningLotteryNumbers 추가

---
+++ src/test/java/lottery/domain/WinningLotteryNumbersTest.java
@@ -0,0 +1,18 @@
+package lottery.domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class WinningLotteryNumbersTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void duplicatedBonusNumber(int bonusNumber) {
+        LotteryTicket lotteryTicket = LotteryTicket.from(Lists.list(1, 2, 3, 4, 5, 6));
+
+        assertThatIllegalArgumentException().isThrownBy(() -> WinningLotteryNumbers.of(lotteryTicket, LotteryNumber.from(bonusNumber)));
+    }
+}
