49c6ab85fd42d64522f3582b18268acc4716a437
haeuniii <qnfnqnfn9825@gmail.com>
2021-12-20 20:00:55 +0900

    refactor<RANKING>: 테스트 클래스 명 변경

--- src/test/java/step2/RankTest.java
+++ src/test/java/step2/RankTest.java
@@ -6,25 +6,26 @@ import step2.domain.enums.Rank;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RankingEnumTest {
+public class RankTest {
     @Test
-    @DisplayName("Lotteries 객체 생성")
+    @DisplayName("Rank 객체 생성")
     void getCorrectCount() {
         assertThat(Rank.FIRST.getCorrectCount()).isEqualTo(6);
-        assertThat(Rank.FOURTH.getCorrectCount()).isEqualTo(3);
+        assertThat(Rank.FOURTH.getCorrectCount()).isEqualTo(4);
     }
 
     @Test
     @DisplayName("당첨 등수에 해당되는 상금 get test")
     void getPrizeMoney() {
-        assertThat(Rank.SECOND.getPrizeMoney()).isEqualTo(1500000);
-        assertThat(Rank.THIRD.getPrizeMoney()).isEqualTo(50000);
+        assertThat(Rank.SECOND.getPrizeMoney()).isEqualTo(3_000_000);
+        assertThat(Rank.THIRD.getPrizeMoney()).isEqualTo(1_500_000);
     }
 
     @Test
     @DisplayName("맞춘 번호 수를 사용하여 RANKING Field get Test")
     void of() {
-        assertThat(Rank.of(3)).isEqualTo(Rank.FOURTH);
-        assertThat(Rank.of(6)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.of(3, false)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.of(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.of(5, true)).isEqualTo(Rank.SECOND);
     }
 }
