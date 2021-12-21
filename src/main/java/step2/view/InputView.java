2fe5f6e9252d38eb571db4c7fe6b1f4151d815f6
haeuniii <qnfnqnfn9825@gmail.com>
2021-12-21 17:01:44 +0900

    feat<InputView>: 수동 로또 구입 개수 및 번호 입력 기능 구현

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -12,18 +12,14 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
 
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-
-        System.out.println(String.format("%d개를 구매했습니다.", input / 1000));
-
-        return input;
+        return scanner.nextInt();
     }
 
     public static String getWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
 
     public static int getBonusBallNumber() {
@@ -31,5 +27,25 @@ public class InputView {
 
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
+    }
+
+    public static int getManualLotteryCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static String[] getManualLotteryNumbers(int manualLotteryCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        String[] numbers = new String[manualLotteryCount];
+
+        for (int idx = 0; idx < manualLotteryCount; idx++) {
+            Scanner scanner = new Scanner(System.in);
+            numbers[idx] = scanner.nextLine().trim();
+        }
+
+        return numbers;
     }
 }
