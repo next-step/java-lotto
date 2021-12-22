9b2006887b6859765a7c844af25d75e8b06073fd
sojeongw <sojeongw.official@gmail.com>
2021-12-22 15:38:48 +0900

    feat: 보너스 볼을 입력받는다

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.lotto.lottonumber.LottoNumber;
 import lotto.lotto.lottonumber.LottoNumbers;
+import lotto.result.WinningNumbers;
 
 import java.util.Scanner;
 
@@ -9,6 +10,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해주세요.";
     private static final String WINNING_NUMBER_REQUEST_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
+    public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해주세요.";
 
     private InputView() {
     }
@@ -18,15 +20,13 @@ public class InputView {
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public static LottoNumbers getWinningNumbers() {
-        return LottoNumbers.from(scanner.nextLine());
-    }
-
-    public static LottoNumber getBonusBall() {
-        return new LottoNumber(Integer.parseInt(scanner.nextLine()));
-    }
-
-    public static void printWinningNumbersRequest() {
+    public static WinningNumbers getWinningNumbers() {
         System.out.println(WINNING_NUMBER_REQUEST_MESSAGE);
+        LottoNumbers winningNumbers = LottoNumbers.from(scanner.nextLine());
+
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        LottoNumber bonusNumber = LottoNumber.from(scanner.nextLine());
+
+        return WinningNumbers.of(winningNumbers, bonusNumber);
     }
 }
