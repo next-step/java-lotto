8e4b82dc1c61b04973071921eb8f977bb00e3eac
sojeongw <sojeongw.official@gmail.com>
2021-12-22 15:44:55 +0900

    feat: 번호 5개와 보너스 볼이 일치하면 2등이 된다

--- src/main/java/lotto/result/Rank.java
+++ src/main/java/lotto/result/Rank.java
@@ -1,14 +1,14 @@
 package lotto.result;
 
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
+    MISS(0, 0),
     FIFTH(3, 5_000),
-    MISS(0, 0);
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000);
 
     private final int matchedNumbersCount;
     private final int prize;
@@ -18,11 +18,17 @@ public enum Rank {
         this.prize = prize;
     }
 
-    public static Rank valueOf(int count) {
-        return Arrays.stream(values())
-                .filter(value -> value.matchedNumbersCount == count)
+    public static Rank valueOf(long count, boolean matchedBonusNumber) {
+        Rank matchedRank = Stream.of(values())
+                .filter(rank -> rank.matchedNumbersCount == count)
                 .findFirst()
                 .orElse(MISS);
+
+        if (isSecond(matchedBonusNumber, matchedRank)) {
+            return SECOND;
+        }
+
+        return matchedRank;
     }
 
     public int matchedNumbersCount() {
@@ -35,5 +41,9 @@ public enum Rank {
 
     public long totalPrize(Long quantity) {
         return prize * quantity;
+    }
+
+    private static boolean isSecond(boolean matchedBonusNumber, Rank matchedRank) {
+        return matchedRank == FOURTH && matchedBonusNumber;
     }
 }
