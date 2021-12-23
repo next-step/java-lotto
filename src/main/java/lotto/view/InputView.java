31729b5f08581fb4befebbbb9dedbec653aea91c
joy <joy.y@kakaocorp.com>
2021-12-23 23:40:09 +0900

    4단계 - 로또(수동)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,25 +1,29 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.WinningLotto;
+import lotto.view.dto.LottosDto;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+import static lotto.domain.Lottos.PRICE_PER_LOTTO;
+
 public class InputView {
     private static final int NUMBERS_SIZE = 6;
     private static final int LAST_NUMBER_INDEX = 5;
     private final Scanner scanner;
-    private final Lottos lottos;
+    private final LottosDto lottos;
+    private final int money;
 
     public InputView() {
         scanner = new Scanner(System.in);
-        System.out.println("구입 금액을 입력해 주세요.");
-        int money = scanner.nextInt();
-        lottos = new Lottos(money);
-        System.out.println(lottos.getSize() + "개를 구매했습니다.");
+        money = getMoney();
+        lottos = new LottosDto(money, new Lottos(getManualLotto(getManualCount())));
+        System.out.println("수동으로 " + lottos.manualSize() + "장, 자동으로 " + lottos.autoSize() + "개를 구매했습니다.");
         printNumbers();
         System.out.println();
     }
@@ -29,7 +33,39 @@ public class InputView {
     }
 
     public Lottos getLottos() {
-        return lottos;
+        return new Lottos(lottos.getLottos());
+    }
+
+    private int getMoney() {
+        System.out.println("구입 금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    private int getManualCount() {
+        while (true) {
+            System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+            int manualCount = scanner.nextInt();
+            if (price(manualCount) <= money) {
+                return manualCount;
+            }
+            System.out.println("구매 금액을 초과하여 입력할 수 없습니다.");
+        }
+    }
+
+    private int price(int count) {
+        return count * PRICE_PER_LOTTO;
+    }
+
+    private List<Lotto> getManualLotto(int manualCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<Lotto> manualLottos = new ArrayList<>();
+        for (int i = 0; i < manualCount; i++) {
+            List<Integer> numbers = new ArrayList<>();
+            IntStream.range(0, NUMBERS_SIZE)
+                    .forEach(j -> numbers.add(parseInteger(j, scanner.next())));
+            manualLottos.add(new Lotto(numbers));
+        }
+        return manualLottos;
     }
 
     public WinningLotto getWinningLotto() {
