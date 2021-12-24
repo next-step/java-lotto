aadd5ae414c9db848dc54fb666be0a68d9807bd6
myeongjooseo <juux_x@naver.com>
2021-12-25 00:35:42 +0900

    test: test case 추가
    test case 추가 및 중복 코드 제거

--- src/test/java/lotto/domain/CalculatePrizeTest.java
+++ src/test/java/lotto/domain/CalculatePrizeTest.java
@@ -3,35 +3,34 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 class CalculatePrizeTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 6})
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("lottoMatchingCountData")
     @DisplayName("1, 3, 4, 5, 낙첨 테스트")
-    void calculatePrizeOfMatchingCount(int num) {
+    void calculatePrizeOfMatchingCount(int num, CalculatePrize expected) {
         CalculatePrize calculatePrize = CalculatePrize.of(num);
-        if (num == 0) {
-            assertThat(calculatePrize).isEqualTo(CalculatePrize.MISS);
-        }
-        if (num == 1) {
-            assertThat(calculatePrize).isEqualTo(CalculatePrize.MISS);
-        }
-        if (num == 2) {
-            assertThat(calculatePrize).isEqualTo(CalculatePrize.MISS);
-        }
-        if (num == 3) {
-            assertThat(calculatePrize).isEqualTo(CalculatePrize.FIFTH);
-        }
-        if (num == 4) {
-            assertThat(calculatePrize).isEqualTo(CalculatePrize.FOURTH);
-        }
-        if (num == 6) {
-            assertThat(calculatePrize).isEqualTo(CalculatePrize.FIRST);
-        }
+        assertThat(calculatePrize).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> lottoMatchingCountData() {
+        return Stream.of(
+            Arguments.of(0, CalculatePrize.MISS),
+            Arguments.of(1, CalculatePrize.MISS),
+            Arguments.of(2, CalculatePrize.MISS),
+            Arguments.of(3, CalculatePrize.FIFTH),
+            Arguments.of(4, CalculatePrize.FOURTH),
+            Arguments.of(6, CalculatePrize.FIRST)
+        );
     }
 
     @ParameterizedTest
--- src/test/java/lotto/domain/MatchedLottoTest.java
+++ src/test/java/lotto/domain/MatchedLottoTest.java
@@ -1,20 +1,50 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 class MatchedLottoTest {
+
+    private LottoNumbers winningNumbers;
+    private LottoNumber bonusNumber;
 
     @BeforeEach
     void setUp(){
-
+        winningNumbers = LottoNumbers.createManualLottoNumber(Arrays.asList(1, 2, 3, 4, 5, 6));
+        bonusNumber = new LottoNumber(7);
     }
 
-    @Test
-    void name() {
-        //LottoNumbers winningNumbers, LottoNumber bonusNumber, LottoNumbers myLottoNumbers
-        //MatchedLotto matchedLotto = new MatchedLotto();
+    @ParameterizedTest(name = "[{index}] 입력값 = {0}, 등수 = {1}")
+    @MethodSource("lottoMissData")
+    @DisplayName("로또 매치 테스트")
+    void matchedLottoTest(List<Integer> lottoNumbers, CalculatePrize resultPrize) {
+        LottoNumbers myLottoNumbers = LottoNumbers.createManualLottoNumber(lottoNumbers);
+        MatchedLotto matchedLotto = new MatchedLotto(winningNumbers, bonusNumber,myLottoNumbers);
+        CalculatePrize calculatePrize = matchedLotto.matchingPrize();
+        assertThat(calculatePrize).isEqualTo(resultPrize);
+    }
+
+    static Stream<Arguments> lottoMissData() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(10, 11, 12, 13, 14, 15), CalculatePrize.MISS),
+            Arguments.of(Arrays.asList(1, 11, 12, 13, 14, 15), CalculatePrize.MISS),
+            Arguments.of(Arrays.asList(1, 2, 12, 13, 14, 15), CalculatePrize.MISS),
+            Arguments.of(Arrays.asList(1, 2, 3, 13, 14, 15), CalculatePrize.FIFTH),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 14, 15), CalculatePrize.FOURTH),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 15), CalculatePrize.THIRD),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 7), CalculatePrize.SECOND),
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), CalculatePrize.FIRST)
+        );
     }
 }
