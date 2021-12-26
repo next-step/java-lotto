bb49e74a8e6026004c75c846faa0ef48aa43f94b
ssibongee <ssibongee@gmail.com>
2021-12-26 23:24:00 +0900

    feat: LottoInputView, LottoOutputView UI 클래스 구현

---
+++ src/main/java/lotto/view/LottoInputView.java
@@ -0,0 +1,43 @@
+package lotto.view;
+
+import lotto.domain.LottoNumber;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LottoInputView {
+
+    private static final LottoInputView INPUT_VIEW = new LottoInputView();
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private LottoInputView() {
+
+    }
+
+    public static LottoInputView getInstance() {
+        return INPUT_VIEW;
+    }
+
+    private int inputInteger() {
+        return SCANNER.nextInt();
+    }
+
+    private String inputString() {
+        return SCANNER.next();
+    }
+
+    public int moneyInputPrompt() {
+        System.out.println("구입금액을 입력해주세요.");
+        return inputInteger();
+    }
+
+    public List<LottoNumber> winningInputPrompt() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return Arrays.stream(inputString().split(","))
+                .map(Integer::parseInt)
+                .map(LottoNumber::from)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -0,0 +1,39 @@
+package lotto.view;
+
+import lotto.domain.LotteryResult;
+import lotto.domain.LottoTickets;
+import lotto.domain.Rank;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LottoOutputView {
+
+    private static final LottoOutputView OUTPUT_VIEW = new LottoOutputView();
+
+    private LottoOutputView() {
+
+    }
+
+    public static LottoOutputView getInstance() {
+        return OUTPUT_VIEW;
+    }
+
+    public void showLottoTickets(LottoTickets tickets) {
+        System.out.println(tickets.count() + "개를 구매했습니다.");
+        System.out.println(tickets);
+    }
+
+    public void showLottoResult(LotteryResult result) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println(printStatistics(result.getStatistics()));
+        System.out.println("총 수익률은 " + result.ratio() + "입니다.");
+    }
+
+    public String printStatistics(Map<Rank, Long> statistics) {
+        return statistics.entrySet().stream()
+                .map(entry -> entry.getKey() + "- " + entry.getValue() + "개")
+                .collect(Collectors.joining("\n"));
+    }
+}
