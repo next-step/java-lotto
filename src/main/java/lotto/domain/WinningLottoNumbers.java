cd66e6fcdb4a15699c6da54814c908bba724bdb4
ssibongee <ssibongee@gmail.com>
2022-01-01 12:48:04 +0900

    feat: WinningLottoNumbers에 보너스볼 추가

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 public class WinningLottoNumbers {
 
-    private LottoNumbers values;
+    private final LottoNumbers winnings;
+    private final LottoNumber bonus;
 
-    private WinningLottoNumbers(LottoNumbers values) {
-        this.values = values;
+    public WinningLottoNumbers(LottoNumbers winnings, LottoNumber bonus) {
+        verify(winnings, bonus);
+        this.winnings = winnings;
+        this.bonus = bonus;
     }
 
     public Rank rank(LottoNumbers numbers) {
-        return Rank.calculate(values.matches(numbers));
+        return Rank.calculate(winnings.matches(numbers), numbers.contains(bonus));
     }
 
-    public static WinningLottoNumbers of(List<LottoNumber> values) {
-        return new WinningLottoNumbers(LottoNumbers.from(values));
+    public static WinningLottoNumbers of(List<LottoNumber> values, LottoNumber bonus) {
+        return new WinningLottoNumbers(LottoNumbers.from(values), bonus);
+    }
+
+    private void verify(LottoNumbers numbers, LottoNumber number) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException("보너스볼의 번호는 당첨번호와 중복된 번호일 수 없습니다.");
+        }
     }
 }
