6f284e233b7f31792115f557195bc40465c73739
ssibongee <ssibongee@gmail.com>
2022-01-01 12:48:42 +0900

    test: 보너스볼 추가에 따른 테스트 코드 리팩토링

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -15,14 +15,15 @@ class LottoTicketsTest {
     @Test
     void calculate() {
         // given
-        WinningLottoNumbers winnings = WinningLottoNumbers.of(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        WinningLottoNumbers winnings = WinningLottoNumbers.of(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 4, 5, 6)), LottoNumber.from(7));
         LottoNumbers first = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 4, 5, 6)));
         LottoNumbers second = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 4, 5, 7)));
-        LottoNumbers third = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 4, 7, 8)));
-        LottoNumbers fourth = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 7, 8, 9)));
+        LottoNumbers third = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 4, 5, 8)));
+        LottoNumbers fourth = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 6, 8, 9)));
+        LottoNumbers fifth = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 3, 7, 8, 9)));
         LottoNumbers none = LottoNumbers.from(LottoNumberFixture.로또_번호_생성(Arrays.asList(1, 2, 7, 8, 9, 10)));
 
-        LottoTickets tickets = LottoTickets.of(Arrays.asList(first, second, third, fourth, none));
+        LottoTickets tickets = LottoTickets.of(Arrays.asList(first, second, third, fourth, fifth, none));
 
         // when
         LotteryResult result = tickets.calculate(winnings);
@@ -33,7 +34,14 @@ class LottoTicketsTest {
                 () -> assertThat(result.getStatistics()).containsKey(Rank.SECOND),
                 () -> assertThat(result.getStatistics()).containsKey(Rank.THIRD),
                 () -> assertThat(result.getStatistics()).containsKey(Rank.FOURTH),
-                () -> assertThat(result.getStatistics()).containsKey(Rank.NONE)
+                () -> assertThat(result.getStatistics()).containsKey(Rank.FIFTH),
+                () -> assertThat(result.getStatistics()).containsKey(Rank.NONE),
+                () -> assertThat(result.getStatistics().get(Rank.FIRST)).isEqualTo(1L),
+                () -> assertThat(result.getStatistics().get(Rank.SECOND)).isEqualTo(1L),
+                () -> assertThat(result.getStatistics().get(Rank.THIRD)).isEqualTo(1L),
+                () -> assertThat(result.getStatistics().get(Rank.FOURTH)).isEqualTo(1L),
+                () -> assertThat(result.getStatistics().get(Rank.FIFTH)).isEqualTo(1L),
+                () -> assertThat(result.getStatistics().get(Rank.NONE)).isEqualTo(1L)
         );
     }
 }
