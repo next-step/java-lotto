4fe7d3c8371117c99847c4a97873356951cfd6a6
yyy96 <dlduswo1247@gmail.com>
2022-01-15 22:35:41 +0900

    로또 input 예외처리

--- src/main/java/domain/LottoNumber.java
+++ src/main/java/domain/LottoNumber.java
@@ -3,6 +3,9 @@ package domain;
 import java.util.List;
 
 public interface LottoNumber {
+    int COUNT_LOTTO_NUMBER = 6;
+    int START_LOTTO_NUMBER = 1;
+    int END_LOTTO_NUMBER = 45;
 
     List<Integer> generateLottoNumber();
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.LottoNumber;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -7,9 +9,11 @@ import java.util.stream.Collectors;
 
 public class InputView {
     Scanner scanner = new Scanner(System.in);
-    private final static String MESSAGE_INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
-    private final static String MESSAGE_INPUT_ANSWER_NUMBER = "지난 주 당첨 번호를 입력해주세요.";
-    private final static String COMMA = ",";
+    private static final String MESSAGE_INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_INPUT_ANSWER_NUMBER = "지난 주 당첨 번호를 입력해주세요.";
+    public static final String MESSAGE_INPUT_ANSWER_NUMBER_COUNT_OVER = "[오류] 숫자를 6개 이상 입력하셨습니다.";
+    public static final String MESSAGE_INPUT_ANSWER_NUMBER_RANGE_OVER = "[오류] 숫자의 범위를 넘어갔습니다.";
+    private static final String COMMA = ",";
 
     public int inputPurchasePrice() {
         System.out.println(MESSAGE_INPUT_PURCHASE_PRICE);
@@ -18,7 +22,23 @@ public class InputView {
 
     public List<Integer> inputAnswerNumber() {
         System.out.println("\n" + MESSAGE_INPUT_ANSWER_NUMBER);
-        return splitAnswerNumber(scanner.next());
+        List<Integer> answerNumbers = splitAnswerNumber(scanner.next());
+        overInputNumberCount(answerNumbers);
+        exceedNumberRange(answerNumbers);
+        return answerNumbers;
+    }
+
+    public void overInputNumberCount(List<Integer> answerNumbers) {
+        if (answerNumbers.size() > LottoNumber.COUNT_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(MESSAGE_INPUT_ANSWER_NUMBER_COUNT_OVER);
+        }
+    }
+
+    public void exceedNumberRange(List<Integer> answerNumbers) {
+        if(answerNumbers.stream()
+                .anyMatch(number -> number > LottoNumber.END_LOTTO_NUMBER || number < LottoNumber.START_LOTTO_NUMBER)){
+            throw new IllegalArgumentException(MESSAGE_INPUT_ANSWER_NUMBER_RANGE_OVER);
+        }
     }
 
     public List<Integer> splitAnswerNumber(String answerNumbers) {
