fa573ce4b0fb419fd103f69164da474880acdf8e
MinChul-Son <smc5236@naver.com>
2022-01-28 15:09:36 +0900

    feat: 쉼표 구분자를 기분으로 입력값을 구분하는 기능 구현

--- src/main/java/Numbers.java
+++ src/main/java/Numbers.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Numbers {
 
@@ -10,6 +12,15 @@ public class Numbers {
         this.numbers = numbers;
     }
 
+    public Numbers(final String expression){
+        this. numbers = splitFromDelimiter(expression);
+    }
+
+    private List<Number> splitFromDelimiter(String expression){
+        return Arrays.stream(expression.split(","))
+            .map(Number::new).collect(Collectors.toList());
+    }
+
     public Number sum() {
         return numbers.stream()
             .reduce(ZERO, Number::add);
@@ -17,5 +28,9 @@ public class Numbers {
 
     public void register(Number number) {
         numbers.add(number);
+    }
+
+    public Number get(int idx){
+        return numbers.get(idx);
     }
 }
