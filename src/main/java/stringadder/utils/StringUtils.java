46387a09e7b9099db6f66cd859c76aa87dfed325
hyeonho8453 <lhh5863@dgu.ac.kr>
2022-01-28 16:28:40 +0900

    refactor(stringAdder): 폴더 변경

--- src/main/java/stringadder/utils/StringUtils.java
+++ src/main/java/stringadder/utils/StringUtils.java
@@ -1,4 +1,4 @@
-package stringadder.utils.exception;
+package stringadder.utils;
 
 import java.util.Arrays;
 import java.util.List;
--- src/test/java/stringadder/domain/AdderTest.java
+++ src/test/java/stringadder/domain/AdderTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import stringadder.utils.exception.StringUtils;
+import stringadder.utils.StringUtils;
 
 public class AdderTest {
 
