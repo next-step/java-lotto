00448f45ff1a4e16dcc6e4053e0007d9acfae114
xxell8 <cell8pepper@gmail.com>
2022-01-28 15:06:45 +0900

    feat(calculator): 커스텀 구분자 사용 가능하도록 기능 추가 구현

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,6 +1,8 @@
 package calculator.domain;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
@@ -23,6 +25,12 @@ public class StringCalculator {
     }
 
     public String[] splitText(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter + '|' + DELIMITER);
+            return tokens;
+        }
         return text.split(DELIMITER);
     }
 
