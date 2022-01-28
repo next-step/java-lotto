b857395b21dd841bb8a2fd5424d2f6d7b70dc7a6
hyeonho8453 <lhh5863@dgu.ac.kr>
2022-01-28 15:41:08 +0900

    test(stringAdder): 커스텀 구분자 기준 문자열 구분 테스트

--- src/test/java/stringadder/domain/AdderTest.java
+++ src/test/java/stringadder/domain/AdderTest.java
@@ -6,15 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import stringadder.utils.exception.StringUtils;
 
 public class AdderTest {
 
     @DisplayName("쉼표_콜론_기준으로_문자열구분")
-    @Test
-    void stringSplitTest() {
-           String input = "1,2:3";
-           List<String> parsingInput = StringUtils.splitStr(input);
+    @ParameterizedTest
+    @ValueSource(strings={"1,2:3","//;\n1;2;3","//-\n1-2-3"})
+    void stringSplitTest(String input) {
+          List<String> parsingInput = StringUtils.splitStr(input);
            assertAll(
                ()->assertThat(parsingInput.get(0)).isEqualTo("1"),
                ()->assertThat(parsingInput.get(1)).isEqualTo("2"),
