35e8c1a59b8055aa97c5be278c69bf5e918037c0
hyeonho8453 <lhh5863@dgu.ac.kr>
2022-01-28 15:41:52 +0900

    feat(stringAdder): 커스텀 구분자 기준 문자열 구분 기능 생성

--- src/main/java/stringadder/utils/exception/StringUtils.java
+++ src/main/java/stringadder/utils/exception/StringUtils.java
@@ -6,9 +6,26 @@ import java.util.stream.Collectors;
 
 public class StringUtils {
 
-    private static final String DELIMITER = ",|:";
+    private static final String STANDARD_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final String SLASH = "//";
+
 
     public static List<String> splitStr(final String input){
-        return Arrays.stream(input.split(DELIMITER)).collect(Collectors.toList());
+        String[] tokens = null;
+        if(input.contains(SLASH)) {
+            tokens = split(input, tokens);
+            return Arrays.stream(tokens).collect(Collectors.toList());
+        }
+        return Arrays.stream(input.split(STANDARD_DELIMITER)).collect(Collectors.toList());
+    }
+
+    private static String[] split(String input, String[] tokens) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            tokens = matcher.group(2).split(customDelimiter);
+        }
+        return tokens;
     }
 }
