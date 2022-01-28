0a9a6898324c7ab8b6adf41adb074837f00c6f51
G1.Jeon <g1.jeon@kakao.com>
2022-01-28 16:24:10 +0900

    test: Calculator 테스트 구현
    - [X] **문자열을 입력**받는다. --> `Calculator#calculate`
    - [X] **식을 검증하다**. --> 검증 : `Calculator#validateExpression`
      - [X] **`,`, `:`은 기본 구분자**이다.
      - [X] 기본 구분자 이외에 `//`와 `\n` 사이에 사용자 임의의 구분자를 설정할 수 있다.
        - [X] 이 때 숫자는 들어올 수 없다.
      - [X] 구분자를 기준으로 *Operand*를 파싱한다.
      - [X] `빈 문자열` 같은 경우에는 `0`이 반환한다.
      - [X] `0, 양수` 이외에는 `RuntimeException`을 throw 한다.
    - [X] **구분자로 나눈다**. --> 분할 : `Tokenizer#splitExpression`
    - [X] 계산 결과를 출력한다.
      - [X] **합**을 계산 --> `Calculator#getSum`
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,87 @@
+package calculator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorTest {
+
+    static Stream<String> validCases() {
+        return Stream.of("", null, "1:2,3", "//;\n1;2;3;4");
+    }
+
+    static Stream<String> invalidCases() {
+        return Stream.of("//0\n1;2;3", "//-\n2,-4;2", "1;3;8:4,0,2");
+    }
+
+    @DisplayName("유효한 케이스")
+    @ParameterizedTest
+    @MethodSource("validCases")
+    void validCases(final String expr) {
+        assertDoesNotThrow(() -> Calculator.calculate(expr));
+    }
+
+    @DisplayName("유효하지 않은 케이스")
+    @ParameterizedTest
+    @MethodSource("invalidCases")
+    void invalidCases(final String expr) {
+        assertThatThrownBy(() -> Calculator.calculate(expr))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String text) {
+        assertThat(Calculator.calculate(text))
+            .isZero();
+    }
+
+    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    void oneNumber(final String text) {
+        assertThat(Calculator.calculate(text))
+            .isSameAs(Integer.parseInt(text));
+    }
+
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    void twoNumbers(final String text) {
+        assertThat(Calculator.calculate(text))
+            .isSameAs(3);
+    }
+
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3"})
+    void colons(final String text) {
+        assertThat(Calculator.calculate(text))
+            .isSameAs(6);
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text) {
+        assertThat(Calculator.calculate(text))
+            .isSameAs(6);
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void negative() {
+        assertThatExceptionOfType(RuntimeException.class)
+            .isThrownBy(() -> Calculator.calculate("-1"));
+    }
+}
