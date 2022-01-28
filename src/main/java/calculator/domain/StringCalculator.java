0a5bbdccdee0c3a8f408ca58981f450b759a9d62
xxell8 <cell8pepper@gmail.com>
2022-01-28 14:28:38 +0900

    feat: 콤마 기준으로 숫자 분리해 더하는 기능 추가

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,11 +1,16 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class StringCalculator {
+    private static final String DELIMITER = ",";
 
     public int add(String text) {
-        return validateText(text);
+        // validateText(text);
+        String[] numbers = splitText(text);
+        return sumNumbers(numbers);
     }
 
     public int validateText(String text) {
@@ -13,5 +18,17 @@ public class StringCalculator {
             return 0;
         }
         return Integer.parseInt(text);
+    }
+
+    public String[] splitText(String text) {
+        return text.split(DELIMITER);
+    }
+
+    public int sumNumbers(String[] numbers) {
+        int result = 0;
+        for (String number : numbers) {
+            result += Integer.parseInt(number);
+        }
+        return result;
     }
 }
