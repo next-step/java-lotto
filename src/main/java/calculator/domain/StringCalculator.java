7f73d9e0dbc40d23e0700b43fa2290bebb9abcb4
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-28 14:45:25 +0900

    feat(calculator): 구분자를 쉼표 이에외 콜론 추가

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -1,23 +1,25 @@
 package calculator.domain;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 public class StringCalculator {
-    private static final String DELIMITER = ",";
+
+    private static final String DELIMITER = ",|:";
 
     public int add(String text) {
-        // validateText(text);
+        try {
+            validateText(text);
+        } catch (IllegalArgumentException e) {
+            return 0;
+        }
         String[] numbers = splitText(text);
         return sumNumbers(numbers);
     }
 
-    public int validateText(String text) {
+    public void validateText(String text) {
         if (Objects.isNull(text) || text.isEmpty()) {
-            return 0;
+            throw new IllegalArgumentException();
         }
-        return Integer.parseInt(text);
     }
 
     public String[] splitText(String text) {
--- src/test/java/calculator/domain/StringCalculatorTest.java
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -18,37 +18,39 @@ class StringCalculatorTest {
         calculator = new StringCalculator();
     }
 
-    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
     @ParameterizedTest
     @NullAndEmptySource
-    void emptyOrNull(final String text) {
+    @Test
+    void 빈_문자열_또는_null_값을_입력할_경우_0을_반환해야_한다(final String text) {
         assertThat(calculator.add(text)).isEqualTo(0);
     }
 
-    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1"})
-    void oneNumber(final String text) {
+    @Test
+    void 숫자_하나를_문자열로_입력할_경우_해당_숫자를_반환한다(final String text) {
         assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
     }
 
-    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2"})
-    void twoNumbers(final String text) {
+    @Test
+    void 숫자_두개를_쉼표구분자로_입력할경우_두숫자의_합을_반환한다(final String text) {
         assertThat(calculator.add(text)).isSameAs(3);
     }
 
-    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @DisplayName(value = "")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:3"})
-    void colons(final String text) {
+    @Test
+    void 구분자를_쉼표_이외에_콜론을_사용할_수_있다(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"//;\n1;2;3"})
+    @Test
     void customDelimiter(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
