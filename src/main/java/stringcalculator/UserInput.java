47b997acfc55b43017e363773322a5fb2e8058d1
hyoenju <l67378810@gmail.com>
2022-01-30 17:50:00 +0900

    feat(input): 사용자 입력값 Class 생성

---
+++ src/main/java/stringcalculator/UserInput.java
@@ -0,0 +1,29 @@
+package stringcalculator;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class UserInput {
+
+    String userInput;
+
+    public UserInput() {
+        userInput = userInputString();
+        if (isNullOrEmpty(userInput)) {
+            userInput = "0";
+        }
+    }
+
+    private String userInputString() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    private boolean isNullOrEmpty(String userInput) {
+        return Objects.isNull(userInput) || userInput.isEmpty();
+    }
+}
