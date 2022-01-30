dbcbe343bd379f9a7fa228496fd613d2ddfa8c4c
Ji In Lee <loveted0225@naver.com>
2022-01-31 00:33:41 +0900

    feat(Parser): 문자열 속 구분자 찾기 기능 구현

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,5 +1,34 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
 
+    private String userInput;
+    private String delimiter;
+
+    public Parser(String userInput) {
+        this.userInput = userInput;
+        this.delimiter = findDelimiter();
+    }
+
+    private String findDelimiter() {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(userInput);
+        if (m.find()) {
+            this.userInput = m.group(2);
+            return m.group(1);
+        }
+        return ",|:";
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public String getDelimiter() {
+        return delimiter;
+    }
 }
