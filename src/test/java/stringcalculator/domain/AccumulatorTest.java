03e802eb82105f635430d3201676086c775b6dc3
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-01-30 18:42:47 +0900

    test: Accumulator 더하기 테스트
    - List<Integer> 를 받아서 더하기 결과 테스트

---
+++ src/test/java/stringcalculator/domain/AccumulatorTest.java
@@ -0,0 +1,18 @@
+package stringcalculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AccumulatorTest {
+
+    @DisplayName("계산 테스트")
+    @Test
+    void 계산_테스트() {
+        int result = Accumulator.calculate(Arrays.asList(1, 2, 3));
+
+        assertThat(result).isEqualTo(6);
+    }
+}
