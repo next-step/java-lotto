07b37981d740057134b6754766207f906aed882c
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-01-30 19:01:07 +0900

    feat: Application 메인 로직 작성

--- src/main/java/stringcalculator/StringCalculatorApplication.java
+++ src/main/java/stringcalculator/StringCalculatorApplication.java
@@ -1,2 +1,22 @@
-package stringcalculator;public class StringCalculatorApplication {
+package stringcalculator;
+
+import java.util.List;
+import stringcalculator.domain.Accumulator;
+import stringcalculator.domain.Delimiters;
+import stringcalculator.domain.ExpressionParser;
+import stringcalculator.view.InputView;
+import stringcalculator.view.ResultView;
+
+public class StringCalculatorApplication {
+
+    public static void main(String[] args) {
+        Delimiters delimiters = InputView.getDelimiters();
+        String expression = InputView.getExpression();
+
+        ExpressionParser expressionParser = new ExpressionParser(delimiters);
+        List<Integer> numbers = expressionParser.parse(expression);
+
+        int result = Accumulator.calculate(numbers);
+        ResultView.print(result);
+    }
 }
