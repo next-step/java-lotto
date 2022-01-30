99b85577ee05688b189bd8faf1d9d9974fd0421c
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-01-30 18:41:22 +0900

    refactor: 필요없는 import 문 제거

--- src/test/java/stringcalculator/domain/DelimiterTest.java
+++ src/test/java/stringcalculator/domain/DelimiterTest.java
@@ -1,9 +1,7 @@
 package stringcalculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.in;
 
-import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -15,6 +13,6 @@ class DelimiterTest {
     @ParameterizedTest
     void delimiter_구분자의_시작과_끝은_숫자(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Delimiter(input));
+            .isThrownBy(() -> new Delimiter(input));
     }
 }
