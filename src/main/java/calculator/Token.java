2fdb6e06e451ceb639a2129f0e0d47a309ba6e7f
Ji In Lee <loveted0225@naver.com>
2022-01-31 00:42:25 +0900

    feat(Token): 분리된 토큰이 숫자가 아닐 때 예외처리

--- src/main/java/calculator/Token.java
+++ src/main/java/calculator/Token.java
@@ -1,5 +1,35 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Token {
+
+    private int operand;
+    private List<Token> tokens;
+    public Token(String parseResult) {
+        this.operand = parseStringToInteger(parseResult);
+    }
+    public Token(String[] splitInput) {
+        tokens = Arrays.stream(splitInput).map(Token::new).collect(Collectors.toList());
+    }
+    public int getOperand() {
+        return operand;
+    }
+
+    private int parseStringToInteger(String parseResult) {
+
+        try {
+            return Integer.parseInt(parseResult);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("피연산자가 숫자가 아닙니다.");
+        }
+
+    }
+
+    public List<Token> getTokens() {
+        return tokens;
+    }
 
 }
