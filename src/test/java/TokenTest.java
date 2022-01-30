aa68b93a39ba1b7cfcb0bc1fbb567f239c1c68a2
Ji In Lee <loveted0225@naver.com>
2022-01-31 00:50:20 +0900

    test(TokenTest): 구분자에 의해 분리된 토큰들의 생성과 예외처리를 위한 테스트코드

---
+++ src/test/java/TokenTest.java
@@ -0,0 +1,13 @@
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import calculator.Token;
+import org.junit.jupiter.api.Test;
+
+public class TokenTest {
+
+    @Test
+    void 숫자가_아닐시_예외() {
+        assertThatExceptionOfType(NumberFormatException.class)
+            .isThrownBy(() -> new Token("*"));
+    }
+}
