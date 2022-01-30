6c33d1d0cbe7d42c9f3dd903320ac453150a7872
Ji In Lee <loveted0225@naver.com>
2022-01-31 00:44:39 +0900

    feat(Application): 문자열 계산기의 Application 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,8 +1,12 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.play();
 
     }
 }
