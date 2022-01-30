6e69012f7f7a9e950870cc49c28930ba81908846
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-01-30 18:44:07 +0900

    feat: Accumulator 더하기 기능 구현
    - List<Integer> 를 받아서 더하는 함수 구현

---
+++ src/main/java/stringcalculator/domain/Accumulator.java
@@ -0,0 +1,11 @@
+package stringcalculator.domain;
+
+import java.util.List;
+
+public class Accumulator {
+
+    public static int calculate(List<Integer> asList) {
+        return asList.stream()
+            .mapToInt(n -> n).sum();
+    }
+}
