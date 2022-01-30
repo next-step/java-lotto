e80d9e14d24f36c2178609c7cc37950b746cc389
kimtaejun97 <kim_taejun@naver.com>
2022-01-30 18:24:33 +0900

    test: ExpressionParser 생성, parse 메서드 구현.
    - ExpressionParser 생성.
    - parse 메서드 구현
    - 구분자를 기준으로 나누어 List<Integer> 반환.
    
    validate:
      - 구분자가 아닌 문자가 들어오면 예외 발생.
      - 2개 이상의 연속된 구분자가 들어오면 예외 발생.

--- src/main/java/stringcalculator/domain/ExpressionParser.java
+++ src/main/java/stringcalculator/domain/ExpressionParser.java
@@ -1,2 +1,46 @@
-package stringcalculator.domain;public class ExpressionParser {
+package stringcalculator.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ExpressionParser {
+
+    private static final String EMPTY_STRING = "";
+    private String delimiters;
+
+    public ExpressionParser(Delimiters delimiters) {
+        this.delimiters = joinDelimiterWithEmptyString(delimiters.getValues());
+    }
+
+    private String joinDelimiterWithEmptyString(List<String> delimiters) {
+        return String.join(EMPTY_STRING, delimiters);
+    }
+
+    public List<Integer> parse(String expression) {
+        String regex = getSplitRegex();
+        validateContainsCharacter(expression);
+
+        String[] split = expression.split(regex);
+
+        return Arrays.stream(split)
+                .map((Integer::parseInt))
+                .collect(Collectors.toList());
+    }
+
+    private String getSplitRegex() {
+        return String.format("[%s]", delimiters);
+    }
+
+    private void validateContainsCharacter(String expression) {
+        String regex = getValidateRegex();
+
+        if (!expression.matches(regex)) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    private String getValidateRegex() {
+        return String.format("[0-9%s]+$", delimiters);
+    }
 }
