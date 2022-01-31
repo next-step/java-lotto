7b223d5f90b37a6c12d2dfe60c5a23c95198967f
yyy96 <dlduswo1247@gmail.com>
2022-01-31 19:18:33 +0900

    Lotto 일급컬렉션 (리팩토링)

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class Lotto {
     private List<Integer> lotto;
-    private int matchCount = 0;
-    private boolean matchBonus = false;
 
     public Lotto() {
         this(new LottoAutoGenerator());
@@ -20,20 +18,14 @@ public class Lotto {
         return Collections.unmodifiableList(lotto);
     }
 
-    public int getMatchCount() {
-        return matchCount;
-    }
-
     public int countMatchCount(List<Integer> answerNumbers) {
-        matchCount = (int) lotto.stream()
+        return (int) lotto.stream()
                 .filter(answerNumbers::contains)
                 .count();
-        return matchCount;
     }
 
     public boolean checkBonusNumber(int bonusNumber) {
-        matchBonus = lotto.stream()
+        return lotto.stream()
                 .anyMatch(number -> number == bonusNumber);
-        return matchBonus;
     }
 }
--- src/test/java/domain/LottoTicketsTest.java
+++ src/test/java/domain/LottoTicketsTest.java
@@ -26,20 +26,4 @@ class LottoTicketsTest {
     }
 
 
-    @Test
-    @DisplayName("로또 총 수익률 계산 테스트")
-    void calculateLottoRatio() {
-        //given
-        LottoTickets lottoTickets = new LottoTickets();
-        lottoTickets.addLottoTicket(new Lotto(() -> new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6))));
-        lottoTickets.addLottoTicket(new Lotto(() -> new ArrayList<>(Arrays.asList(7, 8, 9, 10, 11, 12))));
-        AnswerLotto answerLotto = new AnswerLotto(new ArrayList<>(Arrays.asList(1, 2, 3, 7, 8, 9)), 11);
-
-        //when
-        answerLotto.checkLottoAnswer(lottoTickets.getLottoTickets());
-        lottoTickets.calculateLottoTotalPrize();
-
-        //then
-        assertThat(lottoTickets.calculateLottoRatio(2000)).isEqualTo("5.00");
-    }
 }
