a8ffe8b164ad66e916b6fbec9b77fcf3464da55f
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-02 16:38:30 +0900

    test(lotto): LottoCalculation 테스트 코드 생성

---
+++ src/test/java/lotto/domain/LottoCalculation.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoCalculation {
+
+    private static final int price = 14000;
+    private static final int expectedLotto = 14;
+
+    @DisplayName("로또를_몇개_살수_있는지_계산")
+    @Test
+    void countLottoesTest() {
+        assertThat(LottoCalculation.lottoCalculation(price)).isEqualTo(expectedLotto);
+    }
+}
