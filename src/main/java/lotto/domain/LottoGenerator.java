de2a4078d7b8a9cd49259c01ae3f17a0c2feaf91
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-02 13:36:10 +0900

    feat(LottoGenerator): 6자리의 랜덤 로또번호를 생성하는 객체 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,23 +1,8 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class LottoGenerator {
+public interface LottoGenerator {
 
-    private List<Integer> numbers;
-
-    public LottoGenerator() {
-        numbers = new ArrayList<>();
-
-        setNumber();
-        Collections.shuffle(numbers);
-    }
-
-    public void setNumber() {
-        for (int i = 1; i <= 45; i++) {
-            numbers.add(i);
-        }
-    }
+    List<Integer> get();
 }
