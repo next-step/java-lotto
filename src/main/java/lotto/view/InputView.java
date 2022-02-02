10b98efce5a4fc4ef4ac4df301abe30041a84a13
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-02 13:57:11 +0900

    feat(inputView): 콘솔로 당첨번호를 입력하기 위한 view 객체 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,26 @@
 package lotto.view;
 
+import java.util.Scanner;
+
 public class InputView {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+    private static final String INPUT_IS_EMPTY_ERROR_LOG = "공백은 들어올 수 없습니다.";
+
+    public static String[] writeWinningNumbers() {
+        String input = scanner.nextLine();
+        checkEmptyString(input);
+        return makeStringToArray(input);
+    }
+
+    private static String[] makeStringToArray(String input) {
+        return input.split(DELIMITER);
+    }
+
+    private static void checkEmptyString(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(INPUT_IS_EMPTY_ERROR_LOG);
+        }
+    }
 }
