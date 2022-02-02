5f466f340a7e12963080c6b272825711dd3dc6fa
xxell8 <cell8pepper@gmail.com>
2022-02-02 16:59:34 +0900

    test(ticket): 로또 구매 관련 테스트 추가

--- src/test/java/lotto/domain/TicketTest.java
+++ src/test/java/lotto/domain/TicketTest.java
@@ -1,18 +1,26 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TicketTest {
+    @DisplayName("입력한 금액만큼 로또를 구매")
     @Test
     public void 입력한_구입금액만큼_로또를_구매한다() {
-        //given
-        Ticket ticket = new Ticket(14000);
+        Ticket ticket1 = new Ticket(14000);
+        assertThat(ticket1.getBuyCount()).isEqualTo(14);
 
-        //when
+        Ticket ticket2 = new Ticket(1500);
+        assertThat(ticket2.getBuyCount()).isEqualTo(1);
+    }
 
-        //then
-        assertThat(ticket.getBuyCount()).isEqualTo(14);
+    @DisplayName("1000원 미만의 금액 입력 시 ERROR")
+    @Test
+    void 천원미만_금액_입력() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Ticket(500));
     }
 }
