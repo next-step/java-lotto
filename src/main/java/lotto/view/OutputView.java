ff48c7010070350f28b8dcf5c5810dab45cf1e1b
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-02 16:12:15 +0900

    feat(outputView): 랜덤 로또 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,36 @@
 package lotto.view;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
+
 public class OutputView {
 
-    private static final String MANAGER_LOG = "%d개를 구매했습니다.";
+    private static final String ENTER = "\n";
+    private static final String DELIMITER = ", ";
+    private static final String PURCHASE_AMOUNT_MANAGER_LOG = "%d개를 구매했습니다.";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSED_BRACKET = "]";
 
     public static void printMessage(String message) {
-        System.out.println(message);
+        System.out.print(message);
     }
 
     public static void printPurchaseAmount(int ticketCount) {
-        OutputView.printMessage(String.format(MANAGER_LOG, ticketCount));
+        printMessage(String.format(PURCHASE_AMOUNT_MANAGER_LOG, ticketCount) + ENTER);
+    }
 
+    public static void printPurchaseTicket(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            printMessage(lotto.getNumbers().stream()
+                .sorted(Comparator.comparing(LottoNumber::getNumber))
+                .map(number -> number.getNumber())
+                .map(String::valueOf)
+                .collect(Collectors.joining(DELIMITER, OPEN_BRACKET, CLOSED_BRACKET)) + ENTER);
+        }
+        printMessage(ENTER);
     }
 }
