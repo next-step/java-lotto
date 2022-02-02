5c6225a2808280360be6872a5f2f861eeab596a0
xxell8 <cell8pepper@gmail.com>
2022-02-02 17:00:19 +0900

    feat(ticket): 최소 구매 금액 미만 입력 시, 에러 처리 추가

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -7,6 +7,9 @@ public class Ticket {
     private int buyCount;
 
     public Ticket(int money) {
+        if (money < TICKET_PRICE) {
+            throw new IllegalArgumentException("[ERROR] 최소 구매 금액은 1,000원입니다.");
+        }
         this.buyCount = money / TICKET_PRICE;
     }
 
