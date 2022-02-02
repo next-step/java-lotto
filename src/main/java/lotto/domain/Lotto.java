7d9f49b3c55ea322d117e30ec16cee802a790c08
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-02 17:01:13 +0900

    feat(lotto): Lotto 클래스 생성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> lottoNumber;
+
+    public Lotto(List<Integer> lottoNumber){
+        this.lottoNumber = lottoNumber;
+    }
+}
