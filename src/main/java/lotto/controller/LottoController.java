e2c9a8e63f0878f159b458196e54cfcba7ea33e5
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-02 15:34:43 +0900

    feat(controller): 컨트롤러에서 구입한 티켓 개수의 입출력 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,31 @@
 package lotto.controller;
 
+import lotto.domain.Ticket;
+import lotto.domain.lotto.LottoManager;
+import lotto.domain.machine.RandomLottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class LottoController {
 
+    public LottoController() {
+
+    }
+
+    public void start() {
+        Ticket ticket = buyTicket();
+
+        LottoManager lottoManager = new LottoManager(new RandomLottoGenerator(),
+            ticket.getBuyCount());
+
+        OutputView.printPurchaseAmount(ticket.getBuyCount());
+
+        String[] numbers = InputView.writeWinningNumbers();
+        String bonus = InputView.writeBonusBall();
+    }
+
+    public Ticket buyTicket() {
+        int money = Integer.parseInt(InputView.writePurchaseAmount());
+        return new Ticket(money);
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,14 @@ package lotto.view;
 
 public class OutputView {
 
+    private static final String MANAGER_LOG = "%d개를 구매했습니다.";
+
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printPurchaseAmount(int ticketCount) {
+        OutputView.printMessage(String.format(MANAGER_LOG, ticketCount));
+
     }
 }
