fb4ed720ad30d9da17e9667033937a93d164cac2
hscom96 <hscom96@gmail.com>
2022-02-03 17:02:58 +0900

    feat: 숫자를 음수로 초기화시 예외 발생

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -4,6 +4,7 @@ public class Number {
 
     public Number(final String number) {
         validateNumberFormat(number);
+        validatePositiveNum(number);
         this.number = Integer.parseInt(number);
     }
 
@@ -15,11 +16,17 @@ public class Number {
         return new Number(number + operand.getNumber());
     }
 
-    private void validateNumberFormat(final String number){
+    private void validateNumberFormat(final String number) {
         try {
             Integer.parseInt(number);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("올바른 숫자 포맷이 아닙니다.");
+        }
+    }
+
+    private void validatePositiveNum(final String number) {
+        if (Integer.parseInt(number) < 0) {
+            throw new IllegalArgumentException("숫자가 음수입니다.");
         }
     }
 
--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -1,6 +1,8 @@
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberTest {
 
@@ -8,8 +10,12 @@ public class NumberTest {
     void 숫자_외_문자가_들어온경우_예외발생() {
         // when & then
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> {
-                new Number("cdc");
-            });
+            .isThrownBy(() -> new Number("cdc"));
+    }
+
+    @Test
+    void 숫자가_음수면_예외발생(){
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Number("-1"));
     }
 }
