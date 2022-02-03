eb9b5b66ca0ef29b956547950ce44415410d4dd1
0JUUU <clleo97@naver.com>
2022-02-04 00:23:58 +0900

    feat: 당첨 확인하는 LottoRaffle 클래스 생성

---
+++ src/main/java/lotto/domain/LottoRaffle.java
@@ -0,0 +1,57 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LottoRaffle {
+
+    private final WinningLotto winningLotto;
+    private int match3Number = 0;
+    private int match4Number = 0;
+    private int match5Number = 0;
+    private int matchBonusNumber = 0;
+    private int match6Number = 0;
+
+    public LottoRaffle(WinningLotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+    public void compareLotto(BuyLotto buyLotto) {
+        int count =  buyLotto.getLotto().stream().reduce(0, (cum, val) -> {
+            if(winningLotto.getLotto().contains(val)) {
+                return cum+1;
+            }
+            return cum;
+        });
+
+        if(count == 5 && buyLotto.getLotto().contains(winningLotto.getBonusNumber())) {
+            this.matchBonusNumber++;
+            return;
+        }
+
+        if(count == 3) {
+            this.match3Number++;
+            return;
+        }
+
+        if(count == 4) {
+            this.match4Number++;
+            return;
+        }
+
+        if(count == 5) {
+            this.match5Number++;
+            return;
+        }
+
+        if(count == 6) {
+            this.match6Number++;
+            return;
+        }
+    }
+
+    public List<Integer> getResults() {
+        return Arrays.asList(match3Number, match4Number, match5Number, matchBonusNumber, match6Number);
+    }
+}
