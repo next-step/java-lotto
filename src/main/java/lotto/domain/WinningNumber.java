bf10fec42edcd379f54301e370319bb4129b3453
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-03 19:23:34 +0900

    feat(winning-number): 메서드 분리 및 스트림 적용

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -10,25 +10,26 @@ public class WinningNumber {
     private final List<LottoNumber> lottoNumbers;
     private final LottoNumber bonus;
 
-    public WinningNumber(List<LottoNumber> numbers, LottoNumber bonus) {
-        this.lottoNumbers = numbers;
+    public WinningNumber(List<LottoNumber> lottoNumbers, LottoNumber bonus) {
+        this.lottoNumbers = lottoNumbers;
         this.bonus = bonus;
     }
 
     public Rank compareTo(List<Integer> targetLotto) {
-        int matchCount = 0;
-        boolean bonusBall = false;
-
         List<Integer> winningNumbers = lottoNumbers.stream()
             .map(LottoNumber::getNumber)
             .collect(Collectors.toList());
 
-        for (Integer lottoNumber : targetLotto) {
-            if (isContain(winningNumbers, lottoNumber)) {
-                matchCount++;
-            }
-        }
+        int matchCount = targetLotto.stream()
+            .filter(lottoNumber -> isContain(winningNumbers, lottoNumber))
+            .collect(Collectors.toList())
+            .size();
 
+        return getRank(targetLotto, matchCount);
+    }
+
+    private Rank getRank(List<Integer> targetLotto, int matchCount) {
+        boolean bonusBall = false;
         if (matchCount == 5) {
             bonusBall = isContain(targetLotto, bonus.getNumber());
         }
