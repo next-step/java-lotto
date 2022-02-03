ae1151ea35e4333b49d0dd3fbb3275473ef1f6f7
kimtaejun97 <kim_taejun@naver.com>
2022-02-03 17:49:21 +0900

    test: inputBudget 기능 테스트
    - 입력받은 문자열을 그대로 돌려준다.

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class InputViewTest {
-  
+
+    @DisplayName("구입 금액 입력을 입력받는다.")
+    @ValueSource(strings = {"1450 0", "1500", })
+    @ParameterizedTest
+    void 구입_금액_입력을_입력받는다(String input) {
+        // given
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        // when
+        String expected = InputView.inputBudget();
+
+        // then
+        assertThat(expected).isEqualTo(input);
+    }
 }
