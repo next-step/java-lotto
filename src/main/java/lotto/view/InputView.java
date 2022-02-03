f8609c4cfa0d0c19098e86b4d64dca8138b8a321
kimtaejun97 <kim_taejun@naver.com>
2022-02-03 17:50:22 +0900

    feat: inputBudget 기능 구현.
    - 구매 금액을 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,16 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static String inputBudget() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return input();
+    }
+
+    private static String input() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine().trim();
+    }
 }
