84e0cfac3ef98b873711400130eb0a47ad9a6b51
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-03 20:47:33 +0900

    feat(lotto): 로또 번호 중복 예외처리

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -5,9 +5,12 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private static final String DUPLICATE_NUMBER_MESSAGE = "중복된 번호가 있습니다";
+
     private List<LottoNumber> numbers;
 
     private Lotto(List<LottoNumber> numbers) {
+        validateLotto(numbers);
         this.numbers = numbers;
     }
 
@@ -15,6 +18,17 @@ public class Lotto {
         return new Lotto(numbers.stream()
             .map(LottoNumber::new)
             .collect(Collectors.toList()));
+    }
+
+    private void validateLotto(List<LottoNumber> numbers) {
+        long excludingDuplicatedSize = numbers.stream()
+            .map(LottoNumber::getNumber)
+            .distinct()
+            .count();
+
+        if (numbers.size() != excludingDuplicatedSize) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_MESSAGE);
+        }
     }
 
     public List<LottoNumber> getNumbers() {
