4d7e491e93be51da556a8213f836df66c5a1a10a
kimtaejun97 <kim_taejun@naver.com>
2022-02-03 19:24:47 +0900

    feat: printNumberOfLotto(), printLottoNumber 기능 구현
    - 로또의 구매 개수와 로또 번호 출력.

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -4,6 +4,7 @@ package lotto;
 import lotto.domain.Budget;
 import lotto.domain.Lottos;
 import lotto.view.InputView;
+import lotto.view.PrintView;
 
 public class LottoApplication {
 
@@ -11,5 +12,7 @@ public class LottoApplication {
 
         Budget budget = new Budget(InputView.inputBudget());
         Lottos lottos = Lottos.createAutoLottos(budget);
+        PrintView.printNumberOfLotto(lottos.get().size());
+        PrintView.printLottoNumber(lottos);
     }
 }
---
+++ src/main/java/lotto/view/PrintView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+import lotto.domain.Lottos;
+
+public class PrintView {
+
+    public static void printNumberOfLotto(int size) {
+        System.out.printf("%d개를 구매했습니다.\n", size);
+    }
+
+    public static void printLottoNumber(Lottos lottos) {
+        lottos.get().stream()
+                .forEach(lotto -> System.out.println(lotto.get()));
+    }
+}
