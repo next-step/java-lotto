1d4a42d8747a2ebf1b2d4a8e743a717f431ee275
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-03 18:17:49 +0900

    test(lotto): LottoGeneratorTest 코드 변경

--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -2,9 +2,6 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,10 +10,8 @@ public class LottoGeneratorTest {
     @DisplayName("로또를_생성하는_기능_테스트_6가지_숫자를_선택")
     @Test
     void lottoGenerateTest() {
-        List<Integer> number = Arrays.asList(8, 21, 23, 41, 42, 43);
-        List<LottoNumber> lottoNumber = number.stream().map(LottoNumber::new).collect(Collectors.toList());
-        LottoFullNumber lottoNFullNumber = new LottoFullNumber(lottoNumber);
-        assertThat(lottoNFullNumber.getLottoFullNumber()).isNotNull();
+        Lotto lotto = LottoGenerator.lottoGenerator();
+        assertThat(lotto.getLottoNumber()).isNotNull();
     }
 
 }
