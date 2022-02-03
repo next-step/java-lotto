d455b604f7e7ed8fc53e2ca31524e2ef57d9cb6c
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-03 18:50:39 +0900

    refactor: 불필요 메서드 및 상수 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,14 +6,12 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final int LOTTO_NUMBER_SIZE = 6;
-    private static final int BONUS_BALL_SIZE = 1;
     private static final String PURCHASE_TICKET_MANAGER_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_MANAGER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_BALL_MANAGER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String DELIMITER = ",";
     private static final String INPUT_IS_EMPTY_ERROR_LOG = "공백은 들어올 수 없습니다.";
     private static final String LOTTO_NUMBER_SIZE_ERROR_LOG = "로또는 " + LOTTO_NUMBER_SIZE + "자리여야 합니다.";
-    private static final String BONUS_BALL_SIZE_ERROR_LOG = "보너스 볼은 " + BONUS_BALL_SIZE + "자리여야 합니다.";
 
     public static String writePurchaseAmount() {
         try {
@@ -47,7 +45,6 @@ public class InputView {
             System.out.println(BONUS_BALL_MANAGER_MESSAGE);
             String input = scanner.nextLine();
             checkEmptyString(input);
-            checkOneNumber(input);
             return input;
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
@@ -68,12 +65,6 @@ public class InputView {
     private static void checkSixNumbers(String[] input) {
         if (input.length != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException(LOTTO_NUMBER_SIZE_ERROR_LOG);
-        }
-    }
-
-    private static void checkOneNumber(String input) {
-        if (input.length() != BONUS_BALL_SIZE) {
-            throw new IllegalArgumentException(BONUS_BALL_SIZE_ERROR_LOG);
         }
     }
 }
