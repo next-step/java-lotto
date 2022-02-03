63a7882a9d0c7ecbb31b06089278581efcbd4249
xxell8 <cell8pepper@gmail.com>
2022-02-03 16:40:03 +0900

    feat(rank): 매칭 결과에 따라 등수, 당첨금 계산

---
+++ src/main/java/lotto/domain/lotto/Rank.java
@@ -0,0 +1,36 @@
+package lotto.domain.lotto;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Rank find(int matchCount, boolean matchBonus) {
+        Rank rank = Arrays.stream(Rank.values())
+            .filter(targetRank -> targetRank.matchCount == matchCount)
+            .findFirst().orElse(NONE);
+
+        if (rank == Rank.SECOND && !matchBonus) {
+            return Rank.THIRD;
+        }
+        return rank;
+    }
+}
