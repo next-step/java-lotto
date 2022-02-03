f2f037c6ad0763322dc0492c7fb5762b2ad2ed8a
SeungyeonJung <crowsy01@gmail.com>
2022-02-03 17:02:32 +0900

    test(StringParser): Testing Split Custuom Delimiter
    - 커스텀 구분자 분리 기능 테스트 작성

--- src/test/java/stringcaculator/util/StringParserTest.java
+++ src/test/java/stringcaculator/util/StringParserTest.java
@@ -2,6 +2,7 @@ package stringcaculator.util;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +13,12 @@ class StringParserTest {
     void 기본_구분자_테스트(String numbers, int size) {
         String[] splitNumbers = StringParser.splitDefaultDelimiter(numbers);
         assertThat(splitNumbers.length).isEqualTo(size);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={"//;\n1;2;3", "//!\n1!2!3"})
+    void 커스텀_구분자_테스트(String numbers) {
+        String[] splitNumbers = StringParser.splitCustomDelimiter(numbers);
+        assertThat(splitNumbers.length).isEqualTo(3);
     }
 }
