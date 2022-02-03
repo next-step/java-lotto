7d870f643fb97bb8f607b792b0540576aa6732e5
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-03 16:56:41 +0900

    test: Number Generator
    - interface LottoGenerator
      - AutoGenerator implements above
      (Strategy Pattern)
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/test/java/lotto/domain/generator/AutoGeneratorTest.java
@@ -0,0 +1,49 @@
+package lotto.domain.generator;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import lotto.domain.lotto.Numbers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AutoGeneratorTest {
+
+    private static final int LOTTO_NUMBERS = 6;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+
+    private AutoGenerator autoGenerator;
+    private Numbers numbers;
+    private List<Integer> numberList;
+
+    @BeforeEach
+    void setUp() {
+        autoGenerator = new AutoGenerator();
+        numbers = autoGenerator.generateNumbers();
+        numberList = numbers.get();
+    }
+
+    static boolean isInRange(int number) {
+        return (number >= START_NUMBER && number <= END_NUMBER);
+    }
+
+    @DisplayName("자동 생성 유효성 검사 (개수 일치 여부)")
+    @Test
+    void checkValidRandomNumbers() {
+        assertThat(numberList.size()).isEqualTo(LOTTO_NUMBERS);
+    }
+
+    @DisplayName("자동 생성 유효성 검사 (범위 내 생성)")
+    @Test
+    void checkValidRandomNumbersInRange() {
+        boolean isInRange = numberList.stream()
+            .allMatch(AutoGeneratorTest::isInRange);
+
+        assertTrue(isInRange);
+    }
+
+
+}
