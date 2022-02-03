9457e0be1d4638f0a416c92f35445a044e97e3c5
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-03 17:27:13 +0900

    test(lotto): LottoNumberTest 작성

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -7,6 +7,12 @@ public class LottoNumber implements  Comparable<LottoNumber>{
         this.value = value;
     }
 
+    private void validateRange(int value) {
+        if (value<MIN_RANGE_NUMBER || value > MAX_RANGE_NUMBER){
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+    }
+
     public int getValue() {
         return value;
     }
