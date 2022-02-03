a69126f7d2cab1ebd8337edae370d011bf13f572
Hae-Riri <ehl3288@naver.com>
2022-02-03 18:24:26 +0900

    feat: tokenize 기능 구현
    - 커스텀 구분자 문자열 validation
    - 구분자 기준으로 나뉜 문자열을 int로 파싱

---
+++ src/main/java/calculator/domain/Tokenizer.java
@@ -0,0 +1,45 @@
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Tokenizer {
+
+    private static final String STANDARD_DELIMITER_COMMA = ",";
+    private static final String STANDARD_DELIMITER_COLON = ":";
+
+    private Tokenizer() {
+    }
+
+    public static List<Integer> tokenize(String text) {
+        if (isCustomDelimiter(text)) {
+            final String formattedText = text
+                .replaceAll("[//\\n]", "")
+                .replaceFirst(";", "")
+                .trim();
+
+            final String CUSTOM_DELIMITER = text.substring(text.indexOf("/") + 2,
+                text.indexOf("\n"));
+            return parseInt(formattedText.split(
+                STANDARD_DELIMITER_COMMA + "|" + STANDARD_DELIMITER_COLON + "|"
+                    + CUSTOM_DELIMITER));
+        }
+
+        return parseInt(text.replaceAll("//(.)\n", "(.)")
+            .split(STANDARD_DELIMITER_COMMA + "|" + STANDARD_DELIMITER_COLON));
+    }
+
+    public static List<Integer> parseInt(String[] tokens) {
+        try {
+            return Arrays.stream(tokens).map(Integer::parseInt).collect(Collectors.toList());
+        } catch (NumberFormatException | NullPointerException e) {
+            throw new IllegalArgumentException("[ERROR] 지정된 구분자 외의 문자열이 있습니다.");
+        }
+    }
+
+    public static boolean isCustomDelimiter(String text) {
+        return Pattern.compile("//(.)\n(.*)").matcher(text).find();
+    }
+}
