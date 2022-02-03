239ee5b07cc67813d0a946dccb5c81b95d7952e0
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-03 17:17:12 +0900

    test: Ticket, Tickets
    - 개별 로또 티켓, 티켓 모음 객체
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/test/java/lotto/domain/lotto/TicketsTest.java
@@ -0,0 +1,32 @@
+package lotto.domain.lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.stream.IntStream;
+import lotto.domain.generator.AutoGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TicketsTest {
+
+    private static final int NUM_OF_TICKETS = 5;
+
+    private Tickets tickets;
+
+    @BeforeEach
+    void setUp() {
+        tickets = new Tickets();
+        for (int index = 0; index < NUM_OF_TICKETS; index++) {
+            Ticket ticket = new Ticket(new AutoGenerator().generateNumbers());
+            tickets.addTicket(ticket);
+        }
+    }
+
+    @DisplayName("생성한 로또 수만큼 개수 반환하는지 검증")
+    @Test
+    void testValidCountOfTickets() {
+        assertThat(tickets.get().size())
+            .isEqualTo(NUM_OF_TICKETS);
+    }
+}
