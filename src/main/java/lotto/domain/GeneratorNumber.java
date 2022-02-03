5a17d3063511c9da9dd94a9a1a1801f62a5bff54
0JUUU <clleo97@naver.com>
2022-02-04 01:53:30 +0900

    refactor: 난수 생성 메서드 로직 변경
    - 정렬 추가
    - static으로 변환

--- src/main/java/lotto/domain/GeneratorNumber.java
+++ src/main/java/lotto/domain/GeneratorNumber.java
@@ -1,22 +1,22 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GeneratorNumber {
 
-    private final List<Integer> numbers;
+    private static final int LIMIT_MINIMUM_NUMBER = 1;
+    private static final int LIMIT_MAXIMUM_NUMBER = 45;
+    private static final List<Integer> numbers
+        = IntStream.range(LIMIT_MINIMUM_NUMBER, LIMIT_MAXIMUM_NUMBER + 1)
+        .boxed().collect(Collectors.toList());
 
-    public GeneratorNumber() {
-        numbers = new ArrayList<>();
-        for (int i = 1; i <= 45; i++) {
-            numbers.add(i);
-        }
-    }
-
-    public List<Integer> generateLottoNumbers() {
-        Collections.shuffle(this.numbers);
-        return this.numbers.subList(0, 6);
+    public static List<Integer> generateLottoNumbers() {
+        Collections.shuffle(numbers);
+        final List<Integer> lottoNumbers = numbers.subList(0, 6);
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
     }
 }
