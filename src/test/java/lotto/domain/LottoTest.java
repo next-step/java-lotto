a5c0af1c85548d761b88a1bf24d8efebb8f622f4
kimtaejun97 <kim_taejun@naver.com>
2022-02-03 19:14:08 +0900

    test: Lotto, Lottos 가능 테스트.
    - Lotto 생성
    - Lottos
      - 로또가 구매금액만큼 생성된다.
      - 로또는 발급될 때마다 번호가 섞인다.
      - 로또가 생성된다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoTest {
-  
+
+    @DisplayName("생성 확인")
+    @Test
+    void 생성_확인() {
+        // given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = new Lotto(numbers);
+
+        // then
+        assertThat(lotto.get()).isEqualTo(numbers);
+    }
 }
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,4 +1,56 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.LottoBalls;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class LottosTest {
-  
+
+    private static final int PRICE_OF_LOTTO = 1000;
+
+    @DisplayName("금액만큼 로또가 발급된다.")
+    @ValueSource(strings = {"10000", "14500"})
+    @ParameterizedTest
+    void getNumberOfLottos(String budget) {
+        // given
+        int numberOfLotto = Integer.parseInt(budget) / PRICE_OF_LOTTO;
+
+        // when
+        Lottos autoLottos = Lottos.createAutoLottos(new Budget(budget));
+
+        // then
+        assertThat(autoLottos.get().size()).isEqualTo(numberOfLotto);
+    }
+
+    @DisplayName("로또 번호가 섞인다.")
+    @Test
+    void shuffle() {
+        // given
+        ArrayList<Integer> before = new ArrayList<>(LottoBalls.get());
+
+        // when
+        Lottos.createAutoLottos(new Budget("1000"));
+
+        // then
+        assertThat(LottoBalls.get()).isNotEqualTo(before);
+    }
+
+    @DisplayName("로또가 생성된다.")
+    @Test
+    void createAutoLottos() {
+        // given
+        int numberOfLotto = Integer.parseInt("1000") / PRICE_OF_LOTTO;
+
+        // when
+        List<Lotto> lottos = Lottos.createAutoLottos(new Budget("1000")).get();
+
+        // then
+        assertThat(lottos.get(0).get()).isEqualTo(LottoBalls.get().subList(0, 6));
+    }
 }
