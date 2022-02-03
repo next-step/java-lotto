3adb8644a1e5164857bb498fff7b789d062b6a89
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-03 18:12:38 +0900

    feat: PrizeRatio
    - 수익률 계산 기능 구현
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/main/java/lotto/domain/lotto/PrizeRatio.java
@@ -0,0 +1,29 @@
+package lotto.domain.lotto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
+public class PrizeRatio {
+
+    private static final int ROUND_SCALE = 2;
+
+    private BigDecimal totalPrize;
+
+    public PrizeRatio() {
+        this.totalPrize = BigDecimal.ZERO;
+    }
+
+    public BigDecimal calculateRatio(int purchased, Map<Rank, Integer> matches) {
+        calculatePrize(matches);
+        return totalPrize.divide(new BigDecimal(purchased), ROUND_SCALE, RoundingMode.DOWN);
+    }
+
+    private void calculatePrize(Map<Rank, Integer> matches) {
+        for (Rank rank: Rank.values()) {
+            int prize = rank.getPrize();
+            int count = matches.getOrDefault(rank, 0);
+            totalPrize = totalPrize.add(new BigDecimal(prize * count));
+        }
+    }
+}
