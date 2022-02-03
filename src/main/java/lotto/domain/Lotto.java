8f6f0a5d8699bb1727b1ab46e60febfe45080022
0JUUU <clleo97@naver.com>
2022-02-04 02:16:20 +0900

    refactor: 인스턴스 변수 final 처리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Lotto {
 
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
