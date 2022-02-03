04da87e225ef9f169b67e3e637810474e4a47afb
hyoenju <l67378810@gmail.com>
2022-02-03 21:53:34 +0900

    feat(main): inputView 관련 로직 구현

--- src/main/java/lotto/LottoProcessor.java
+++ src/main/java/lotto/LottoProcessor.java
@@ -1,6 +1,8 @@
 package lotto;
 
-import lotto.domain.UserInput;
+import java.util.List;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoTickets;
 import lotto.view.InputView;
 
 public class LottoProcessor {
@@ -8,7 +10,19 @@ public class LottoProcessor {
     static InputView inputView = new InputView();
 
     public static void main(String[] args) {
-        int price = Integer.parseInt(inputView.getPrice());
-        UserInput userInput = new UserInput(price);
+        int totalPrice = inputView.getTotalPrice();
+        LottoTickets lottoTickets = new LottoTickets(totalPrice);
+
+        int lottoCounts = lottoTickets.getLottoCounts();
+        System.out.printf("%d개를 구매했습니다.%n", lottoCounts);
+
+        List<Integer> winNumbers = inputView.getWinNumbers();
+        int bonusNumber = inputView.getBonusNumber();
+
+        System.out.println(lottoCounts);
+        System.out.println(bonusNumber);
+
+        LottoMachine lottoMachine = new LottoMachine(winNumbers, bonusNumber);
+
     }
 }
