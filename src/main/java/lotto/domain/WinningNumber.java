3a03950b1db49a76754c7174fea59a25bf9bc52a
xxell8 <cell8pepper@gmail.com>
2022-02-03 17:55:35 +0900

    feat(winningNumber): 당첨 번호 클래스 생성

---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.domain.lotto.LottoNumber;
+
+public class WinningNumber {
+    private final List<LottoNumber> numbers;
+    private final LottoNumber bonus;
+
+    public WinningNumber(List<LottoNumber> numbers, LottoNumber bonus) {
+        this.numbers = numbers;
+        this.bonus = bonus;
+    }
+}
