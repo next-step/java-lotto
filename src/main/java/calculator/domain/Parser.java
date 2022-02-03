08c8385ccd106241a7ea35ab157c59b1cd852459
0JUUU <clleo97@naver.com>
2022-02-03 18:01:55 +0900

    feat: Parser 테스트코드 작성

---
+++ src/main/java/calculator/domain/Parser.java
@@ -0,0 +1,7 @@
+package calculator.domain;
+
+public class Parser {
+
+    public Parser() {
+    }
+}
---
+++ src/test/java/calculator/domain/ParserTest.java
@@ -0,0 +1,35 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class ParserTest {
+
+    @Test
+    void 구분자를_기준으로_숫자를_나눈다() {
+        final Parser parser = new Parser();
+        assertThat(parser.splitNumberByDelimiter()).isEqualTo(
+            Arrays.asList(1,2,3)
+        );
+    }
+
+    @Test
+    void 숫자_이외의_값이_들어가는_경우() {
+        final Parser parser = new Parser();
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> parser.splitNumberByDelimiter())
+            .withMessage("");
+    }
+
+    @Test
+    void 음수가_들어가는_경우() {
+        final Parser parser = new Parser();
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> parser.splitNumberByDelimiter())
+            .withMessage("");
+    }
+}
