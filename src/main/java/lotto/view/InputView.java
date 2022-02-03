295319c33d2a0a47709bef6b2a9b9deb2f16026e
hyoenju <l67378810@gmail.com>
2022-02-03 21:48:58 +0900

    feat(input): 사용자 입력 기능 구현
    - getTotalPrice : 로또 구입 금액 입력
    - getWinNumbers : 당첨 번호 입력
    - getBonusNumber : 당첨 보너스 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,30 +1,37 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
-    Scanner scanner = new Scanner(System.in);
+    private static final String PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WIN_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
 
-    final String PRICE_MESSAGE = "구입금액을 입력해 주세요.";
-    final String WIN_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
-    final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private final Scanner scanner = new Scanner(System.in);
 
-    private String getInputString(String message) {
+    private String getInputValue(String message) {
         System.out.println(message);
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
 
-    public final String getPrice() {
-        return getInputString(PRICE_MESSAGE);
+    public final int getTotalPrice() {
+        return Integer.parseInt(getInputValue(PRICE_MESSAGE));
     }
 
-    public final String getWinNumber() {
-        return getInputString(WIN_NUMBER_MESSAGE);
+    public final List<Integer> getWinNumbers() {
+        String[] winNumbers = getInputValue(WIN_NUMBER_MESSAGE).split(",");
+        return Arrays.stream(winNumbers)
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
     }
 
-    public final String getBonusNumber() {
-        return getInputString(BONUS_NUMBER_MESSAGE);
+    public final int getBonusNumber() {
+        return Integer.parseInt(getInputValue(BONUS_NUMBER_MESSAGE));
     }
 
 
