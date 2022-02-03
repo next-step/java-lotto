6a150efd3c099bf55065ea4eada040c4e466fd5a
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-03 19:05:48 +0900

    feat(winning-number): 로또 번호 비교하는 기능을 당첨번호 객체로 변경
    - 변수명 및 함수명 변경
    - 자동로또 번호와 저번주 당첨 번호 비교하는 책임은, 당첨 번호 Class에게 부여하도록 변경

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,19 +1,46 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.lotto.LottoNumber;
+import lotto.domain.lotto.Rank;
 
 public class WinningNumber {
-    private final List<LottoNumber> numbers;
+
+    private final List<LottoNumber> lottoNumbers;
     private final LottoNumber bonus;
 
     public WinningNumber(List<LottoNumber> numbers, LottoNumber bonus) {
-        this.numbers = numbers;
+        this.lottoNumbers = numbers;
         this.bonus = bonus;
     }
 
-    public List<LottoNumber> getNumbers() {
-        return this.numbers;
+    public Rank compareTo(List<Integer> targetLotto) {
+        int matchCount = 0;
+        boolean bonusBall = false;
+
+        List<Integer> winningNumbers = lottoNumbers.stream()
+            .map(LottoNumber::getNumber)
+            .collect(Collectors.toList());
+
+        for (Integer lottoNumber : targetLotto) {
+            if (isContain(winningNumbers, lottoNumber)) {
+                matchCount++;
+            }
+        }
+
+        if (matchCount == 5) {
+            bonusBall = isContain(targetLotto, bonus.getNumber());
+        }
+        return Rank.find(matchCount, bonusBall);
+    }
+
+    public boolean isContain(List<Integer> targetLotto, int lottoNumber) {
+        return targetLotto.contains(lottoNumber);
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return this.lottoNumbers;
     }
 
     public LottoNumber getBonus() {
