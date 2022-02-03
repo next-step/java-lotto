8e658a1de754c4e420106e830d24242491c61573
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-03 17:08:25 +0900

    test(lotto-matcher): 사용자가 입력한 금액과 로또생성기와 일치 로직 구현(red)

---
+++ src/test/java/lotto/domain/LottoMatcherTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoManager;
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.machine.RandomLottoGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LottoMatcherTest {
+    
+    @Test
+    public void 사용자가_입력한_저번주당첨금액과_비교_6개_모두_일치() {
+        //given
+        String[] numbers = {"1", "2", "3", "4", "5", "6"};
+        
+        //when
+        Lotto lotto = Lotto.from(new RandomLottoGenerator().get());
+        List<LottoNumber> lottoNumbers = lotto.getNumbers();
+
+        List<Integer> integers = lottoNumbers.stream()
+            .map(LottoNumber::getNumber)
+            .collect(Collectors.toList());
+
+        //then
+        Assertions.assertThat(LottoMatcher.compare(numbers, integers)).isEqaulTo(6);
+    }
+}
