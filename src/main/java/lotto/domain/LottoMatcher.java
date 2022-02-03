51ba96de5875f4367f72e4adfbe32f3006c9a385
xxell8 <cell8pepper@gmail.com>
2022-02-03 17:23:00 +0900

    feat(LottoMatcher): 로또 숫자 매칭 결과 반환

--- src/main/java/lotto/domain/LottoMatcher.java
+++ src/main/java/lotto/domain/LottoMatcher.java
@@ -1,5 +1,22 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMatcher {
 
+    public static int compare(String[] numbers, List<Integer> lotto) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String number : numbers) {
+            winningNumbers.add(Integer.parseInt(number));
+        }
+
+        int count = 0;
+        for (Integer number : lotto) {
+            if (winningNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
