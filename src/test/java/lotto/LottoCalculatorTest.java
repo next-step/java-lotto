4c8a315daab1ba35b515102a903a5aca601c3859
MinChul-Son <smc5236@naver.com>
2022-02-05 00:15:04 +0900

    🚀 refactor: 리스트의 Integer를 모두 LottoNumber로 변경에 따른 깨진 테스트 복구

--- src/test/java/lotto/LottoCalculatorTest.java
+++ src/test/java/lotto/LottoCalculatorTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.LottoCalculator;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoResult;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +17,13 @@ class LottoCalculatorTest {
     void 당첨_번호와_몇개가_일치하는지_판단할_수_있다() {
         // given
         LottoCalculator calculator = LottoCalculator.getInstance();
-        List<Integer> lotteryNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        List<LottoNumber> lotteryNumbers = Arrays.asList(1, 2, 3, 4, 5, 6).stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
         int bonusNum = 6;
-        List<Integer> userNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 8));
+        List<LottoNumber> userNumbers = Arrays.asList(1, 2, 3, 4, 5, 8).stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
 
         // when
         LottoResult lottoResult = calculator.countLotteryNumber(lotteryNumbers, userNumbers, bonusNum);
