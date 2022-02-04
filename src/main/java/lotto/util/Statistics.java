df32def17be4a234d92fc8ad0ecfa8383c27153b
YangWoo Yu <yuyw0712@naver.com>
2022-02-04 22:43:29 +0900

    feat: 로또 결과 통계 개수를 위한 getCount() 메서드 생성
    Co-authored-by: hochan222<hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/lotto/util/Statistics.java
+++ src/main/java/lotto/util/Statistics.java
@@ -1,16 +1,18 @@
 package lotto.util;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum Statistics {
     FIRST(6, 2000000000),
-    SECOND(5, 30000000), // 이게 보너스볼
+    SECOND(7, 30000000), // 이게 보너스볼
     THIRD(5, 1500000),
     FOURTH(4, 50000),
     FIFTH(3, 5000),
-    NONE1(1,0),
-    NONE2(2,0),
-    NONE(0,0);
+    NONE2(2, 0),
+    NONE1(1, 0),
+    NONE(0, 0);
 
     public int getGrade() {
         return grade;
@@ -33,5 +35,10 @@ public enum Statistics {
             .filter(statistic -> statistic.grade == numberOfMatch)
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public static int getCount(List<Statistics> statistics, int matchCount) {
+        return statistics.stream().filter(x -> x.grade == matchCount).collect(Collectors.toList())
+            .size();
     }
 }
