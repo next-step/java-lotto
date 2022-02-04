a36163e8e4760fd637062226ac2998fa1e45e8d0
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-05 04:34:45 +0900

    refactor(lotto): 매직넘버 적용

--- src/main/java/stringadder/utils/exception/InputValidException.java
+++ src/main/java/stringadder/utils/exception/InputValidException.java
@@ -3,9 +3,10 @@ package stringadder.utils.exception;
 public class InputValidException {
 
     private static final String NEGATIVE_INPUT_ERROR = "음수는 입력할 수 없습니다!";
+    private static final int ZERO_NUMBER = 0;
 
     public static void inputNegativeValidation(int input) {
-        if (input < 0) {
+        if (input < ZERO_NUMBER) {
             throw new RuntimeException(NEGATIVE_INPUT_ERROR);
         }
     }
