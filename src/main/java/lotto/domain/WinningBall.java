ddcfc630d8e2374b8b16bc4599cde97c4ecfb867
Hae-Riri <ehl3288@naver.com>
2022-02-04 17:01:50 +0900

    feat: 당첨번호 클래스 WinningBall 생성

---
+++ src/main/java/lotto/domain/WinningBall.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningBall {
+    private final int number;
+    private final boolean isBonus;
+
+    public WinningBall(int number, boolean isBonus) {
+        this.number = number;
+        this.isBonus = isBonus;
+    }
+
+    public WinningBall(String number) {
+        this(number, false);
+    }
+
+    public WinningBall(String number, boolean isBonus) {
+        this(Integer.parseInt(number), isBonus);
+    }
+
+    public static List<WinningBall> getWinningBalls (String [] numbers, String bonusNumber) {
+        List<WinningBall> balls = Arrays.stream(numbers)
+            .map(WinningBall::new)
+            .collect(Collectors.toList());
+
+        balls.add(new WinningBall(bonusNumber, true));
+        return balls;
+    }
+}
