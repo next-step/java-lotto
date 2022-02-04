12e62dc05ac2a3c2b5b3b1f101ee10e48bab2105
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-02-04 16:29:42 +0900

    feat: JudgeTest 테스트 구현
    - 구매 개수 확인

---
+++ src/test/java/lotto/domain/JudgeTest.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JudgeTest {
+
+    @DisplayName("개수 확인")
+    @Test
+    void checkSecond() {
+
+        // given
+        Lottos lottos = Lottos.createAutoLottos(new Budget("14000"));
+        Judge judge = new Judge(new CorrectNumbers(
+            new WinningNumbers("1,2,3,4,5,6"),
+            new BonusNumber("7")));
+
+        // when
+        int size = judge.getRanks(lottos).get().size();
+
+        // then
+        assertThat(size).isEqualTo(14);
+    }
+}
