3090c941d297fa961f6e7b64ab8209dc17cd262b
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 18:38:13 +0900

    feat(WinningLotto): Create WinnigLotto feature
    - 우승 로또 기능 구현 및 유효성 검증 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,15 +4,28 @@ import java.util.List;
 
 public class WinningLotto {
 
+    private static final int BONUS_IDX = 6;
+    private static final int WINNING_LOTTO_SIZE = 7;
+    private static final String WINNING_LOTTO_SIZE_EXCEPTION_MESSAGE = "[ERROR] 우승 로또 숫자는 보너스 볼 포함 총 7개 입니다";
+
+    private List<LottoNumber> lottoNumbers;
 
     public WinningLotto(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+        validateWinningSize();
     }
 
-    public int bonusBall() {
-        return 0;
+    public LottoNumber bonusBall() {
+        return lottoNumbers.get(BONUS_IDX);
     }
 
     public int count() {
-        return 0;
+        return lottoNumbers.size();
+    }
+
+    private void validateWinningSize() {
+        if (lottoNumbers.size() != WINNING_LOTTO_SIZE) {
+            throw new IllegalArgumentException(WINNING_LOTTO_SIZE_EXCEPTION_MESSAGE);
+        }
     }
 }
