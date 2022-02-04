eb042d2ea5ac14e684983e73094472b0d159b538
parkminsang <minsang0850@naver.com>
2022-02-04 20:58:57 +0900

    feat: 수익률 계산하기, 당첨 통계 출력하기

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -19,4 +21,43 @@ public class OutputView {
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
     }
 
+    public static void printResult(Map<Rank, Integer> ranks, int price) {
+        String loss = "";
+        System.out.println(WINNING_STATISTICS_MESSAGE);
+        double reward = printRanks(ranks);
+        double yield = reward / price;
+        if (yield == ZERO) {
+            loss = LOSS;
+        }
+        yield = (double) ((int) (HUNDRED * yield)) / HUNDRED;
+        System.out.print("총 수익률 " + yield + "입니다." + loss);
+    }
+
+    private static double printRanks(Map<Rank, Integer> ranks) {
+        Iterator<Rank> keys = ranks.keySet().iterator();
+        double money = 0;
+        while (keys.hasNext()) {
+            Rank key = keys.next();
+            money += printRank(ranks, key);
+        }
+        return money;
+    }
+
+    private static double printRank(Map<Rank, Integer> ranks, Rank key) {
+        if (key == Rank.SECOND) {
+            return printSecond(ranks, key);
+        }
+        System.out.println(
+          key.getMatchCount() + "개 일치 (" + key.getReward() + ")- " + ranks.get(key)
+            + "개");
+        return key.getReward() * ranks.get(key);
+    }
+
+    private static double printSecond(Map<Rank, Integer> ranks, Rank key) {
+        System.out.println(
+          key.getMatchCount() + "개 일치" + BONUS_BALL + "(" + key.getReward() + ")- " + ranks.get(key)
+            + "개");
+        return key.getReward() * ranks.get(key);
+    }
 }
+
