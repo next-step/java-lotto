8b830e224b16863d9cc522d7179419a963e52be8
Ji In Lee <loveted0225@naver.com>
2022-02-05 00:31:16 +0900

    refactor: lotto.controller 패키지로 이동

--- src/main/java/lotto/controller/LottoAuto.java
+++ src/main/java/lotto/controller/LottoAuto.java
@@ -1,11 +1,17 @@
-package lotto;
+package lotto.controller;
+
+import lotto.model.LottoRules;
+import lotto.model.User;
+import lotto.model.WinningNumber;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoAuto {
 
     public void play() {
         LottoRules lottoRules = new LottoRules();
-        User user = new User(
-          OutputView.getLottoCount(InputView.getPrice() / lottoRules.getLottoPrice()), lottoRules);
+        User user = new User(OutputView.getLottoCount(InputView.getPrice() / lottoRules.getLottoPrice()),
+            lottoRules);
         OutputView.printLottos(user.getLottos());
         WinningNumber winningNumber = new WinningNumber(InputView.getWinningNumber(), lottoRules);
         winningNumber.setBonusBall(InputView.getBonusBall());
