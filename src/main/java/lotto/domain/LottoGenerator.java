e29d0b2b47ad8e5d4a55478246cfb0b76066cf9c
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 15:33:26 +0900

    feat(LottoGenerator): Create LottoGenreate feature
    - Lotto 난수 생성 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,11 +1,28 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class LottoGenerator {
 
-    public List<Integer> generateLotto() {
-        return new ArrayList<>();
+    private static final int LOTTO_SIZE_BOUND = 6;
+    private static final int LOTTO_MAXIMUM_BOUND = 45;
+    private final Random RANDOM;
+
+    public LottoGenerator() {
+        this.RANDOM = new Random();
+    }
+
+    public Set<Integer> generateLotto() {
+        Set<Integer> lotto = new HashSet<>();
+        while (lotto.size() != LOTTO_SIZE_BOUND) {
+            lotto.add(generate());
+        }
+        return lotto;
+    }
+
+    private Integer generate() {
+        return RANDOM.nextInt(LOTTO_MAXIMUM_BOUND) + 1;
     }
 }
--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -1,25 +1,21 @@
 package lotto.domain;
 
-import java.util.HashSet;
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Set;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoGeneratorTest {
 
     @Test
     void 총_6개의_중복되지_않은_난수를_만든다() {
         LottoGenerator lottoGenerator = new LottoGenerator();
-        List<Integer> lotto = lottoGenerator.generateLotto();
+        Set<Integer> lotto = lottoGenerator.generateLotto();
         int count = (int) lotto.stream()
             .filter(this::inRange)
             .count();
-        Set<Integer> removeDuplicated = new HashSet<>(lotto);
         assertThat(lotto.size()).isEqualTo(6);
         assertThat(count).isEqualTo(6);
-        assertThat(removeDuplicated.size()).isEqualTo(6);
     }
 
     private boolean inRange(Integer i) {
