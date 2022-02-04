c56a68fd0ba600e4fb00d7342c8a92cd969f7e66
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-05 04:31:36 +0900

    refactor(lotto): 컨벤션 수정
    - Adder 클래스 적용

--- src/main/java/stringadder/domain/Adder.java
+++ src/main/java/stringadder/domain/Adder.java
@@ -7,23 +7,40 @@ import stringadder.utils.exception.InputValidException;
 
 public class Adder {
 
-    public static int add(List<String> numbers){
+    private static final String SPACE_DELIMITER = " ";
+    private static final String BLANK_DELIMITER = "";
+    private static final String NULL = null;
 
+    public static int add(List<String> numbers) {
 
-        if(numbers.contains(" ") ||
-            numbers.contains("") ||
-            numbers.contains(null)
-        ) {
+        if (numbers.contains(SPACE_DELIMITER) || numbers.contains(BLANK_DELIMITER)
+            || numbers.contains(NULL)) {
             return 0;
         }
 
+        List<Integer> convertInt = convertIntValue(numbers);
+        checkInvalidValue(convertInt);
 
-        List<Integer> convertInt = numbers.stream().map(Integer::parseInt).collect(Collectors.toList());
-        convertInt.stream().peek(InputValidException::inputNegativeValidation).collect(Collectors.toList());
-
-        return convertInt.stream().mapToInt(Integer::intValue).sum();
+        return makeSum(convertInt);
     }
 
+    private static int makeSum(List<Integer> convertInt) {
+        return convertInt
+            .stream()
+            .mapToInt(Integer::intValue)
+            .sum();
+    }
 
+    private static void checkInvalidValue(List<Integer> convertInt) {
+        convertInt
+            .stream()
+            .peek(InputValidException::inputNegativeValidation)
+            .collect(Collectors.toList());
+    }
 
+    private static List<Integer> convertIntValue(List<String> numbers) {
+        return numbers.stream()
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
 }
