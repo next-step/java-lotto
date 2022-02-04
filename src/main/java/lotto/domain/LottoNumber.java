9172ad074b1471b74138de64034cfd7e357423ac
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 16:11:56 +0900

    feat(LottoNumber): 로또 숫자 기능 구현
    - 로또 숫자 생성 기능 구현
    - 로또 숫자 범위 유효성 검증 기능 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,10 +2,28 @@ package lotto.domain;
 
 public class LottoNumber {
 
-    public LottoNumber(int i) {
+    private static final int LOTTO_MINIMUM_BOUND = 1;
+    private static final int LOTTO_MAXIMUM_BOUND = 45;
+    private static final String INVALID_NUMBER_EXCEPTION_MESSAGE = "[ERROR] 로또 번호는 1 이상 45 이하입니다.";
+
+    private int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        this.lottoNumber = lottoNumber;
+        validateLottoNumber();
     }
 
     public int value() {
-        return 0;
+        return lottoNumber;
+    }
+
+    private void validateLottoNumber() {
+        if (lottoNumberOutRange()) {
+            throw new IllegalArgumentException(INVALID_NUMBER_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean lottoNumberOutRange() {
+        return lottoNumber < LOTTO_MINIMUM_BOUND || lottoNumber > LOTTO_MAXIMUM_BOUND;
     }
 }
