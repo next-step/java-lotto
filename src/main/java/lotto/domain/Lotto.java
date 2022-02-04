832ff055051c0036531f876dce947413652398b6
jihyunhillpark <girl9505@naver.com>
2022-02-04 15:38:43 +0900

    feat: 1~45사이 중복되지 않은 6개의 난수 생성 기능
    - Lotto#generateRandoms()
    - Lotto#generateLotto()

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,23 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
 public class Lotto {
 
+    public LottoNumbers generateLotto() {
+        return new LottoNumbers(generateRandoms());
+    }
+
+    public List<Integer> generateRandoms() {
+        List<Integer> numberCards = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            numberCards.add(i);
+        }
+        Collections.shuffle(numberCards);
+        List<Integer> picked = numberCards.subList(0, 5);
+        Collections.sort(picked);
+        return picked;
+    }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,7 +1,24 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
 
 class LottoTest {
 
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto();
+    }
+
+    @Test
+    void 로또숫자들은_1부터45까지의_중복되지_않은_6개의_수들이다() {
+        List<Integer> lottoNumbers = lotto.generateRandoms();
+        long given = lottoNumbers.stream().distinct().count();
+        assertThat(given).isEqualTo(lottoNumbers.size());
+    }
 }
