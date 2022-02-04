c1267cb2570556511243c7a221bc10bcd3548727
MinChul-Son <smc5236@naver.com>
2022-02-05 01:14:27 +0900

    🚀 feat: 자동 생성된 로또 출력하는 로직 추가

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -8,20 +8,23 @@ import lotto.domain.Lottos;
 
 public class LottoInputView {
 
+    private final StringBuilder stringBuilder = new StringBuilder();
+
     private List<LottoNumber> winningNumbers;
     private LottoNumber bonusBall;
     private Lottos lottos;
+    private int userPrice;
 
     public void prepareLottoGame(final LottoShop lottoShop) {
         System.out.println("구입금액을 입력해 주세요.");
-        final int price = LottoInput.inputPrice();
-        int lottoAmount = lottoShop.countPossibleLottoAmount(price);
+        userPrice = LottoInput.inputPrice();
+        int lottoAmount = lottoShop.countPossibleLottoAmount(userPrice);
         System.out.println(lottoAmount + "개를 구매했습니다.");
 
         lottos = lottoShop.buyLotto();
 
         for (Lotto lotto : lottos.getLottos()) {
-            System.out.println(lotto);
+            printLotto(lotto.getLotto());
         }
 
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
@@ -29,6 +32,22 @@ public class LottoInputView {
 
         System.out.println("보너스 볼을 입력해주세요.");
         bonusBall = new LottoNumber(LottoInput.inputBonusBall());
+    }
+
+    private void printLotto(final List<LottoNumber> lottoNumbers) {
+        stringBuilder.append("[");
+
+        for (LottoNumber lottoNumber : lottoNumbers) {
+            stringBuilder.append(lottoNumber.getNumber())
+                .append(", ");
+        }
+
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(" "));
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(","));
+        stringBuilder.append("]");
+
+        System.out.println(stringBuilder);
+        stringBuilder.setLength(0);
     }
 
     public List<LottoNumber> getWinningNumbers() {
@@ -41,5 +60,9 @@ public class LottoInputView {
 
     public Lottos getLottos() {
         return lottos;
+    }
+
+    public int getPrice() {
+        return this.userPrice;
     }
 }
