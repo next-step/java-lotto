7708bb88de01a0df7edbacfc7e0c497f765a19de
YangWoo Yu <yuyw0712@naver.com>
2022-02-04 16:47:36 +0900

    feat: 사용자 입력에 맞게 로또를 발행하는 Lottos.java 구현
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import static lotto.view.OutputView.ERROR_WRONG_LOTTO_PRICE;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lottos {
+    private static final int LOTTO_PRICE = 1000;
+
+    final List<Lotto> lottoLists;
+    final int lottoCount;
+
+    public Lottos(int totalPrice) {
+        validateLottoPrice(totalPrice);
+
+        this.lottoCount = totalPrice / LOTTO_PRICE;
+        this.lottoLists = generatorLottos();
+    }
+
+    public List<Lotto> getLottoLists() {
+        return Collections.unmodifiableList(lottoLists);
+    }
+
+    private List<Lotto> generatorLottos () {
+        List<Lotto> lottoLists = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottoLists.add(new Lotto());
+        }
+
+        return lottoLists;
+    }
+
+    private void validateLottoPrice(int totalPrice) {
+        if(totalPrice % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ERROR_WRONG_LOTTO_PRICE);
+        }
+    }
+
+}
