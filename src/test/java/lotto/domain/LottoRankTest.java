a2b65b0ac55bdbe7a5fa0c2f64f0969dbd1cab2f
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-02-04 16:17:47 +0900

    test: LottoRank.getRank 기능 테스트
    - 1, 2, 3, 미당첨 확인.

---
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoRankTest {
+
+    @DisplayName("2등 확인")
+    @Test
+    void checkSecond() {
+        // when
+        LottoRank lottoRank = LottoRank.getRank(5, true);
+
+        // then
+        assertThat(lottoRank).isEqualTo(lottoRank.SECOND);
+    }
+
+    @DisplayName("3등 확인")
+    @Test
+    void checkThird() {
+        // when
+        LottoRank lottoRank = LottoRank.getRank(5, false);
+
+        // then
+        assertThat(lottoRank).isEqualTo(LottoRank.THIRD);
+    }
+
+    @DisplayName("1등 확인")
+    @Test
+    void checkFirst() {
+        // when
+        LottoRank lottoRank = LottoRank.getRank(6, false);
+
+        // then
+        assertThat(lottoRank).isEqualTo(lottoRank.FIRST);
+    }
+
+    @DisplayName("미당첨 확인")
+    @ValueSource(ints = {0, 1, 2})
+    @ParameterizedTest
+    void checkFail(int matchCount) {
+        // when
+        LottoRank lottoRank = LottoRank.getRank(matchCount, false);
+
+        // then
+        assertThat(lottoRank).isEqualTo(lottoRank.FAIL);
+    }
+}
