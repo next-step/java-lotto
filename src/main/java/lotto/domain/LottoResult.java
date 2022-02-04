cf95739ec24d6ebf4c0445e794d8666ccacfbea0
0JUUU <clleo97@naver.com>
2022-02-04 23:45:48 +0900

    feat: LottoResult 로직 변경
    - 변수 추가
    - 관련 메서드 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -3,30 +3,35 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoResult {
-    NOMATCH(0, true, 0),
-    MATCH3(3, false, 0),
-    MATCH4(4, false, 0),
-    MATCH5(5, false, 0),
-    MATCHBONUS(5, true, 0),
-    MATCH6(6, false, 0);
+    NOMATCH(0, true, 0, 0),
+    MATCH3(3, false, 0, 5000),
+    MATCH4(4, false, 0, 50000),
+    MATCH5(5, false, 0, 150000),
+    MATCHBONUS(5, true, 0, 30000000),
+    MATCH6(6, false, 0, 2000000000);
 
     private final int matchCount;
     private final boolean isBonusNumber;
     private int count;
+    private int winning;
 
-    LottoResult(int matchCount, boolean isBonusNumber, int count) {
+    LottoResult(int matchCount, boolean isBonusNumber, int count, int winning) {
         this.matchCount = matchCount;
         this.isBonusNumber = isBonusNumber;
         this.count = count;
+        this.winning = winning;
     }
 
-    public static void upCount(int matchCount, boolean isBonusNumber) {
-        Arrays.stream(values())
-            .filter(result -> result.matchCount == matchCount)
-            .filter(result -> result.isBonusNumber == isBonusNumber)
+    public static void increaseMatch(int matchCount, boolean isBonusNumber) {
+        findLottoResult(matchCount, isBonusNumber).plusCount();
+    }
+
+    public static LottoResult findLottoResult(int matchCount, boolean isBonusNumber) {
+        return Arrays.stream(values())
+            .filter(lottoResult -> lottoResult.matchCount == matchCount)
+            .filter(lottoResult -> lottoResult.isBonusNumber == isBonusNumber)
             .findAny()
-            .orElse(NOMATCH)
-            .plusCount();
+            .orElse(NOMATCH);
     }
 
     public void plusCount() {
@@ -34,4 +39,14 @@ public enum LottoResult {
     }
 
     public int getCount() {return this.count;}
+
+    public int getWinning() { return this.winning;}
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusNumber() {
+        return isBonusNumber;
+    }
 }
