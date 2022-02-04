efc49e8c444225cd434aa1fbdb558e9e3dbc79ed
MinChul-Son <smc5236@naver.com>
2022-02-05 00:14:43 +0900

    🚀 refactor: 리스트의 Integer를 모두 LottoNumber로 변경

--- src/main/java/lotto/domain/LottoCalculator.java
+++ src/main/java/lotto/domain/LottoCalculator.java
@@ -1,9 +1,8 @@
-package lotto;
+package lotto.domain;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import lotto.domain.LottoResult;
 
 public class LottoCalculator {
 
@@ -19,21 +18,21 @@ public class LottoCalculator {
         return INSTANCE;
     }
 
-    public LottoResult countLotteryNumber(final List<Integer> lotteryNumbers,
-        List<Integer> userNumbers, int bonusNumber) {
+    public LottoResult countLotteryNumber(final List<LottoNumber> lotteryNumbers,
+        List<LottoNumber> userNumbers, int bonusNumber) {
 
         return new LottoResult(countNormalSuccessNumber(lotteryNumbers, userNumbers),
             countBonusNumber(userNumbers, bonusNumber));
     }
 
-    private int countNormalSuccessNumber(final List<Integer> lotteryNumbers,
-        List<Integer> userNumbers) {
-        Set<Integer> lottoDuplicate = new HashSet<>(lotteryNumbers);
+    private int countNormalSuccessNumber(final List<LottoNumber> lotteryNumbers,
+        List<LottoNumber> userNumbers) {
+        Set<LottoNumber> lottoDuplicate = new HashSet<>(lotteryNumbers);
         lottoDuplicate.addAll(userNumbers);
         return STANDARD_NUMBER - lottoDuplicate.size();
     }
 
-    private int countBonusNumber(List<Integer> userNumbers, int bonusNum) {
+    private int countBonusNumber(List<LottoNumber> userNumbers, int bonusNum) {
         if (userNumbers.contains(bonusNum)) {
             return MATCHED_BONUS_BALL;
         }
