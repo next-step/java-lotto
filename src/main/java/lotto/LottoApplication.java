98dc77e9313cdb64cbdf13c352b0ad9d40d28828
0JUUU <clleo97@naver.com>
2022-02-04 16:30:56 +0900

    feat: 총괄하는 LottoApplication 클래스 구현

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,8 +1,31 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoRaffle;
+import lotto.domain.Lottos;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class LottoApplication {
 
     public static void main(String[] args) {
+        final int money = InputView.inputMoney();
+        final int lottoCount = money / 1000;
+        ResultView.printLottoCount(lottoCount);
 
+        final Lottos lottos = new Lottos(lottoCount);
+        ResultView.printLottos(lottos);
+
+        final List<Integer> winningNumbers = InputView.inputLastWinningNumbers();
+        final Integer bonusNumber = InputView.inputBonusNumber(winningNumbers);
+
+        final LottoRaffle lottoRaffle = new LottoRaffle(new WinningLotto(winningNumbers, bonusNumber));
+        for(Lotto lotto: lottos.getLottos()) {
+            lottoRaffle.compareLotto(lotto);
+        }
+        final List<Integer> results = lottoRaffle.getResults();
+        ResultView.printLottoResult(results, money);
     }
 }
