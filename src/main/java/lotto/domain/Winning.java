ffbffc09ac9b563d6be4dd53bc6f7b4242cc3398
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-04 20:26:52 +0900

    feat(lotto): Winning 클래스 수정
    - winningLottoNumber 메소드에서 생성된 로또 번호가 이전 로또 당첨번호와 몇 개 일치하는지 확인
    - bonusLottoNumber 메소드에서 보너스넘버가 로또 당첨번호에 포함되는지 확인

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -3,10 +3,10 @@ package lotto.domain;
 public class Winning {
 
     private static final String DUPLICATE_ERROR_MESSAGE = "마지막 보너스 볼은 당첨 번호에 포함될 수 없습니다.";
-    private final LottoFullNumber winningLottoNumber;
+    private final Lotto winningLottoNumber;
     private final LottoNumber bonusLottoNumber;
 
-    public Winning(final LottoFullNumber winningLottoNumber, final LottoNumber bonusLottoNumber) {
+    public Winning(final Lotto winningLottoNumber, final LottoNumber bonusLottoNumber) {
         this.winningLottoNumber = winningLottoNumber;
         this.bonusLottoNumber = bonusLottoNumber;
         validationWinningNumber();
@@ -14,8 +14,16 @@ public class Winning {
 
 
     private void validationWinningNumber() {
-        if (winningLottoNumber.getLottoFullNumber().contains(bonusLottoNumber.getValue())) {
+        if (winningLottoNumber.getLottoNumber().contains(bonusLottoNumber.getValue())) {
             throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
+
+    public long winningLottoNumber(Lotto lotto){
+        return winningLottoNumber.countMatch(lotto);
+    }
+    public boolean bonusLottoNumber(Lotto lotto){
+        return lotto.contains(bonusLottoNumber);
+    }
+
 }
