8d63118af5875c7a424784d82a28d257023df84e
jihyunhillpark <girl9505@naver.com>
2022-02-04 16:27:35 +0900

    refactor: 상수 util 분리

---
+++ src/main/java/lotto/util/Constant.java
@@ -0,0 +1,12 @@
+package lotto.util;
+
+public class Constant {
+
+    private Constant() {
+
+    }
+
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+}
