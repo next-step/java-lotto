5ae10c55e462d0f89ae4e42884833a2b8b7b3d4e
MinChul-Son <smc5236@naver.com>
2022-02-05 00:19:25 +0900

    🚀 feat: LottoController 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,39 @@
+package lotto.controller;
+
+
+import lotto.domain.LottoShop;
+import lotto.view.LottoInputView;
+import lotto.view.LottoResultView;
+
+public class LottoController {
+
+    private final LottoInputView lottoInputView;
+    private final LottoResultView lottoResultView;
+    private final LottoShop lottoShop;
+
+    public LottoController(final LottoInputView lottoInputView,
+        final LottoResultView lottoResultView, final LottoShop lottoShop) {
+        this.lottoInputView = lottoInputView;
+        this.lottoResultView = lottoResultView;
+        this.lottoShop = lottoShop;
+    }
+
+    public void run() {
+        setUp();
+        startLotto();
+        analyzeStatistics();
+    }
+
+    private void setUp() {
+        lottoInputView.prepareLottoGame(lottoShop);
+    }
+
+    private void startLotto() {
+        lottoResultView.findWinner(lottoInputView.getLottos(), lottoInputView.getWinningNumbers(),
+            lottoInputView.getBonusBall());
+    }
+
+    private void analyzeStatistics() {
+        lottoResultView.finishGame();
+    }
+}
