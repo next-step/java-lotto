87a725cf39a9aa7a4fa5dce1cbd15c5c2157898e
Hae-Riri <ehl3288@naver.com>
2022-02-05 00:05:01 +0900

    feat: test용 생성자 추가

--- src/main/java/lotto/domain/WinningBall.java
+++ src/main/java/lotto/domain/WinningBall.java
@@ -5,15 +5,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WinningBall {
+
     private final int number;
     private final boolean isBonus;
 
-    public WinningBall(int number, boolean isBonus) {
-        this.number = number;
-        this.isBonus = isBonus;
+    public WinningBall(String number) {
+        this(number, false);
     }
 
-    public WinningBall(String number) {
+    public WinningBall(int number) {
         this(number, false);
     }
 
@@ -21,12 +21,25 @@ public class WinningBall {
         this(Integer.parseInt(number), isBonus);
     }
 
-    public static List<WinningBall> getWinningBalls (String [] numbers, String bonusNumber) {
+    public WinningBall(int number, boolean isBonus) {
+        this.number = number;
+        this.isBonus = isBonus;
+    }
+
+    public static List<WinningBall> getWinningBalls(String[] numbers, String bonusNumber) {
         List<WinningBall> balls = Arrays.stream(numbers)
             .map(WinningBall::new)
             .collect(Collectors.toList());
 
         balls.add(new WinningBall(bonusNumber, true));
         return balls;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean isBonus() {
+        return isBonus;
     }
 }
