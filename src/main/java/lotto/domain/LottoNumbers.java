f79c1c1237ac69eef864bd380d68adff4354abbf
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-05 04:44:14 +0900

    refactor(lotto): LottoNumbers 네이밍 변경

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -7,14 +7,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class LottoFullNumber {
+public class LottoNumbers {
 
     private final static int NUMBER_ZERO = 0;
     private final static int LOTTO_NUMBER_SIZE = 6;
     private final static String ERROR_MESSAGE = "로또 숫자는 중복될 수 없습니다.";
     private final List<LottoNumber> lottoFullNumber;
 
-    public LottoFullNumber(List<LottoNumber> lottoFullNumber) {
+    public LottoNumbers(List<LottoNumber> lottoFullNumber) {
         List<LottoNumber> lottoFullNumberLengthSix = lottoFullNumber.subList(NUMBER_ZERO,
             LOTTO_NUMBER_SIZE);
         Collections.sort(lottoFullNumberLengthSix);
@@ -36,7 +36,7 @@ public class LottoFullNumber {
             .collect(Collectors.toList());
     }
 
-    public long countMatch(LottoFullNumber lottoFullNumber){
+    public long countMatch(LottoNumbers lottoFullNumber){
         return this.lottoFullNumber.stream()
             .filter(lottoFullNumber::contains)
             .count();
