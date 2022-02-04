91a395d78964b3b9726926d864eac231291a7341
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 17:12:23 +0900

    feat(LottoTicket): 로또 티켓 기능 구현
    - 로또 일급 컬렉션 로또 티켓 구현

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -4,10 +4,26 @@ import java.util.List;
 
 public class LottoTicket {
 
+    private static final String NOT_VALID_SIZE_EXCEPTION_MESSAGE = "[ERROR] 로또 티켓은 최소 1개 이상의 로또가 필요합니다.";
+
+    private List<Lotto> lottos;
+
     public LottoTicket(List<Lotto> lottos) {
+        validateLottos(lottos);
+        this.lottos = lottos;
     }
 
     public int lottoCount() {
-        return 0;
+        return lottos.size();
+    }
+
+    private void validateLottos(List<Lotto> lottos) {
+        if (lottoSizeInvalid(lottos)) {
+            throw new IllegalArgumentException(NOT_VALID_SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean lottoSizeInvalid(List<Lotto> lottos) {
+        return lottos == null || lottos.size() == 0;
     }
 }
