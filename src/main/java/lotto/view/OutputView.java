96ba0540d0f7b40c680cafc6b8ca2713f039cf92
YangWoo Yu <yuyw0712@naver.com>
2022-02-04 22:39:36 +0900

    feat: 기능 추가에 따른 Output 메서드 추가
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
     private static final String SYSTEM_LOTTO_COUNT = "개를 구매했습니다.";
     private static final String REQUEST_WINNING_NUMBER_BEFORE = "지난 주 당첨 번호를 입력해주세요.(ex 1, 2, 3, 4, 5)";
     private static final String REQUEST_BONUS_BALL_NUMBER = "보너스 볼을 입력해주세요.";
+    private static final String WINNING_STATS = "당첨 통계\n------------";
 
     public static void printExceptionMessage(String exceptionMessage) {
         System.out.println(ERROR_MESSAGE + exceptionMessage);
@@ -27,5 +28,27 @@ public class OutputView {
 
     public static void printBonusBallNumber() {
         System.out.println(REQUEST_BONUS_BALL_NUMBER);
+    }
+
+    public static void printLottoStatisticTitle() {
+        System.out.println(WINNING_STATS);
+    }
+
+    public static void printLottoStatistic(int grade, int money, int matchCount) {
+        System.out.println(grade + "개 일치(" + money + "원) - " + matchCount + " 개");
+    }
+
+    public static void printLottoBonusStatistic(int grade, int money, int matchCount) {
+        System.out.println(grade + "개 일치, 보너스 볼 일치(" + money + "원) - " + matchCount + " 개");
+    }
+
+    public static void printTotalReturn(Double earningRate) {
+        String profitOrLoss = "손해";
+
+        if(earningRate >= 1) {
+            profitOrLoss = "이득";
+        }
+
+        System.out.println("총 수익률은 " + earningRate + "입니다.(기준이 1이기 때문에 결과적으로 "+ profitOrLoss +"이라는 의미임)");
     }
 }
