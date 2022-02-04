7cc98f140cfd7e6884b7a6506568af0257a19ad6
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-04 15:42:09 +0900

    feat(WinningResult): 당첨 통계를 매핑하는 기능 구현

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -6,13 +6,14 @@ import java.util.Map;
 import lotto.domain.lotto.Rank;
 
 public class WinningResult {
+
     private final Map<Rank, Integer> result;
 
     public WinningResult() {
         this.result = new HashMap<>();
     }
 
-    public void result(List<Rank> ranks) {
+    public void mappingResult(List<Rank> ranks) {
         for (Rank rank : ranks) {
             if (result.containsKey(rank)) {
                 result.put(rank, result.get(rank) + 1);
--- src/test/java/lotto/domain/WinningResultTest.java
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -1,13 +1,10 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.lotto.Lotto;
-import lotto.domain.lotto.LottoManager;
 import lotto.domain.lotto.LottoNumber;
 import lotto.domain.lotto.Rank;
 import org.assertj.core.api.Assertions;
@@ -15,13 +12,22 @@ import org.junit.jupiter.api.Test;
 
 class WinningResultTest {
     @Test
-    public void _1등_1개_2등_1개() {
+    public void 당첨통계_맵핑_테스트() {
         //given
         List<Lotto> lottos = new ArrayList<>();
         Lotto lotto1 = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto lotto2 = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 7));
+        Lotto lotto3 = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 17));
+        Lotto lotto4 = Lotto.from(Arrays.asList(1, 2, 3, 4, 15, 7));
+        Lotto lotto5 = Lotto.from(Arrays.asList(1, 2, 3, 14, 15, 7));
+        Lotto lotto6 = Lotto.from(Arrays.asList(1, 2, 13, 14, 15, 7));
+
         lottos.add(lotto1);
         lottos.add(lotto2);
+        lottos.add(lotto3);
+        lottos.add(lotto4);
+        lottos.add(lotto5);
+        lottos.add(lotto6);
 
         WinningNumber winningNumber = new WinningNumber(Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6)), new LottoNumber(7));
 
@@ -40,10 +46,14 @@ class WinningResultTest {
             ranks.add(rank);
         }
 
-        winningResult.result(ranks);
+        winningResult.mappingResult(ranks);
 
         //then
         Assertions.assertThat(winningResult.getResult().get(Rank.FIRST)).isEqualTo(1);
         Assertions.assertThat(winningResult.getResult().get(Rank.SECOND)).isEqualTo(1);
+        Assertions.assertThat(winningResult.getResult().get(Rank.THIRD)).isEqualTo(1);
+        Assertions.assertThat(winningResult.getResult().get(Rank.FOURTH)).isEqualTo(1);
+        Assertions.assertThat(winningResult.getResult().get(Rank.FIFTH)).isEqualTo(1);
+        Assertions.assertThat(winningResult.getResult().get(Rank.NONE)).isEqualTo(1);
     }
 }
