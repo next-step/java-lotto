93d9b5e243fd00f1219ff92402321399a058ada1
MinChul-Son <smc5236@naver.com>
2022-02-05 03:32:17 +0900

    🚀 feat: ResultView 구현

---
+++ src/main/java/lotto/view/LottoResultView.java
@@ -0,0 +1,89 @@
+package lotto.view;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoCalculator;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
+import lotto.domain.Ranking;
+
+public class LottoResultView {
+
+    private final StringBuilder stringBuilder = new StringBuilder();
+    private final Map<Ranking, Integer> totalResult = new HashMap<>();
+    private int totalWinnerPrice = 0;
+
+    public LottoResultView() {
+        initializeTotalResult();
+    }
+
+    private void initializeTotalResult() {
+        totalResult.put(Ranking.FIRST, 0);
+        totalResult.put(Ranking.SECOND, 0);
+        totalResult.put(Ranking.THIRD, 0);
+        totalResult.put(Ranking.FOURTH, 0);
+        totalResult.put(Ranking.FIFTH, 0);
+    }
+
+    public void findWinner(final List<LottoNumber> winningNumbers, final Lottos userLottos,
+         final LottoNumber bonusNumber) {
+
+        final LottoCalculator lottoCalculator = LottoCalculator.getInstance();
+
+        for (Lotto lotto : userLottos.getLottos()) {
+            LottoResult lottoResult = lottoCalculator.countLotteryNumber(winningNumbers,
+                lotto.getLotto(),
+                bonusNumber.getNumber());
+
+            updateResult(Ranking.judgeRanking(lottoResult));
+        }
+    }
+
+    private void updateResult(final Ranking ranking) {
+        if (totalResult.containsKey(ranking)) {
+            Integer currentCount = totalResult.get(ranking);
+            totalResult.put(ranking, ++currentCount);
+        }
+    }
+
+    public void finishGame(final int userPrice) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+
+        final List<Ranking> rankings = new LinkedList<>(totalResult.keySet());
+        Collections.sort(rankings);
+        Collections.reverse(rankings);
+
+        rankings.forEach(key ->
+            process(key, totalResult.get(key)));
+    }
+
+    private void process(final Ranking ranking, final Integer count) {
+        if (ranking.isFail()) {
+            return;
+        }
+
+        stringBuilder.append(ranking.getNormalSuccessNum())
+            .append("개 일치 ")
+            .append("(")
+            .append(ranking.getWinnerPrice())
+            .append("원) - ")
+            .append(count)
+            .append("개");
+
+        totalWinnerPrice += ranking.multiplyCountAndWinnerPrice(count);
+
+        if (ranking.equals(Ranking.SECOND)) {
+            stringBuilder.insert(5, ", 보너스 볼 일치");
+        }
+
+        System.out.println(stringBuilder);
+        stringBuilder.setLength(0);
+    }
+
+}
