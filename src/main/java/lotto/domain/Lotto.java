45032ce16307b16ae59e92b566b8e80c9817c62d
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 16:41:46 +0900

    feat(Lotto): 로또 생성 및 중복, 갯수 검증 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,30 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
-    public Lotto(List<LottoNumber> lotto) {
+    private static final int LOTTO_SIZE = 6;
+    private static final String NOT_VALIDATE_LOTTO_MESSAGE = "[ERROR] 로또는 중복되지 않은 6개의 숫자가 필요합니다.";
+
+    private Set<LottoNumber> lotto;
+
+    public Lotto(Set<LottoNumber> lotto) {
+        this.lotto = lotto;
+        validateLotto();
     }
 
     public int count() {
-        return 0;
+        return lotto.size();
+    }
+
+    private void validateLotto() {
+        if (lottoSizeNotValid()) {
+            throw new IllegalArgumentException(NOT_VALIDATE_LOTTO_MESSAGE);
+        }
+    }
+
+    private boolean lottoSizeNotValid() {
+        return lotto.size() != LOTTO_SIZE;
     }
 }
