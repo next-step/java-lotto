8a854e19147b4d0ff09ecf7d8d1277fc9c49cc34
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 16:41:08 +0900

    test(Lotto): 로또 테스트 수정
    - 내부 구현 Set이 더 적합하다고 판단되어 변경

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,7 +1,7 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
@@ -12,34 +12,34 @@ public class LottoTest {
 
     @Test
     void 총_6개의_중복되지_않은_로또_숫자를_가진다() {
-        List<LottoNumber> lottoNumbers = Arrays.stream(new int[]{1, 2, 3, 4, 5, 6})
+        Set<LottoNumber> lottoNumbers = Arrays.stream(new int[]{1, 2, 3, 4, 5, 6})
             .boxed()
             .map(LottoNumber::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
         Lotto lotto = new Lotto(lottoNumbers);
         assertThat(lotto.count()).isEqualTo(6);
     }
 
     @Test
     void 중복된_숫자를_가질_수_없다() {
-        List<LottoNumber> lottoNumbers = Arrays.stream(new int[]{1, 1, 2, 3, 4, 5})
+        Set<LottoNumber> lottoNumbers = Arrays.stream(new int[]{1, 1, 2, 3, 4, 5})
             .boxed()
             .map(LottoNumber::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
         assertThatThrownBy(() -> new Lotto(lottoNumbers))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 갯수는_6개_미만_혹은_초과_할_수_없다() {
-        List<LottoNumber> underLottoNumbers = Arrays.stream(new int[]{1, 2, 3, 4, 5})
+        Set<LottoNumber> underLottoNumbers = Arrays.stream(new int[]{1, 2, 3, 4, 5})
             .boxed()
             .map(LottoNumber::new)
-            .collect(Collectors.toList());
-        List<LottoNumber> overLottoNumbers = Arrays.stream(new int[]{1, 2, 3, 4, 5, 6, 7})
+            .collect(Collectors.toSet());
+        Set<LottoNumber> overLottoNumbers = Arrays.stream(new int[]{1, 2, 3, 4, 5, 6, 7})
             .boxed()
             .map(LottoNumber::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
         assertThatThrownBy(() -> new Lotto(underLottoNumbers))
             .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lotto(overLottoNumbers))
