7e8b01529bbd41a81cff1cd4fda9935240b0af22
Hae-Riri <ehl3288@naver.com>
2022-02-04 22:07:17 +0900

    feat: Lotto 클래스 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplication(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if(numbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 넘버는 6개여야 합니다.");
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        if(nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 넘버는 중복이 불가합니다.");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
