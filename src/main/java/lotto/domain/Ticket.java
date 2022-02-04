aeafa0245710a0ee980c18181b181ea27f688225
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-04 17:13:29 +0900

    refactor: String 상수 처리

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -2,13 +2,14 @@ package lotto.domain;
 
 public class Ticket {
 
+    private static final String TICKET_ERROR_MESSAGE = "최소 구매 금액은 1,000원입니다.";
     private static final int TICKET_PRICE = 1000;
 
     private int buyCount;
 
     public Ticket(int money) {
         if (money < TICKET_PRICE) {
-            throw new IllegalArgumentException("[ERROR] 최소 구매 금액은 1,000원입니다.");
+            throw new IllegalArgumentException(TICKET_ERROR_MESSAGE);
         }
         this.buyCount = money / TICKET_PRICE;
     }
