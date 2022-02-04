58b0dc6ebc4237e7e6a6d3a6d4854401984876f0
SeungyeonJung <crowsy01@gmail.com>
2022-02-04 15:24:16 +0900

    testing(LottoGenerator): Testing generating lotto
    - 로또 생성 테스트 작성

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+
+    public List<Integer> generateLotto() {
+        return new ArrayList<>();
+    }
+}
