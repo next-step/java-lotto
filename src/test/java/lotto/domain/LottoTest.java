9d23adab5f0adf58be0a47231407f86d5b43be09
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-02-04 16:07:17 +0900

    test: 당첨번호, 보너스번호 match 기능 테스트.
    - 포함된 당첨 번호 개수 반환.
    - 보너스 번호가 포함되었는지 반환.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -21,4 +21,44 @@ class LottoTest {
         // then
         assertThat(lotto.get()).isEqualTo(numbers);
     }
+
+    @DisplayName("번호 몇개 맞았는지 확인")
+    @Test
+    void matchWinningNumber() {
+        // given
+        Lotto lotto = new Lotto(Arrays.asList("1", "2", "3", "4", "5", "6"));
+        List<String> winnigNumbers = Arrays.asList("1", "2", "3", "4", "5", "7");
+
+        // when
+        int matchNumber = lotto.matchNumber(winnigNumbers);
+
+        // then
+        assertThat(matchNumber).isEqualTo(5);
+    }
+
+    @DisplayName("보너스 번호 맞았는지 확인 - ture")
+    @Test
+    void containsBonusNumber_true() {
+        // given
+        Lotto lotto = new Lotto(Arrays.asList("1", "2", "3", "4", "5", "6"));
+
+        // when
+        boolean isBonus = lotto.containsBonusNumber("5");
+
+        // then
+        assertThat(isBonus).isEqualTo(true);
+    }
+
+    @DisplayName("보너스 번호 맞았는지 확인 - false")
+    @Test
+    void containsBonusNumber_false() {
+        // given
+        Lotto lotto = new Lotto(Arrays.asList("1", "2", "3", "4", "5", "6"));
+
+        // when
+        boolean isBonus = lotto.containsBonusNumber("7");
+
+        // then
+        assertThat(isBonus).isEqualTo(false);
+    }
 }
