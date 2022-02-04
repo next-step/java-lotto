d80c1d04b322bd40d8dc6505d5c524a2f9a47100
YangWoo Yu <yuyw0712@naver.com>
2022-02-04 22:42:14 +0900

    feat: 4개를 맞추고 보너스볼을 맞출 경우 통계를 위한 분기 처리
    Co-authored-by: hochan222<hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,26 +1,26 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import lotto.util.Statistics;
 
 public class LottoStatistics {
 
-    private static final int MATCH_FIVE = 5;
+    private static final int MATCH_FOUR = 4;
     private static final int MIN_WIN_COUNT = 3;
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
     private final List<Lotto> lottoList;
     private final List<Statistics> resultStatistics;
+    private final int lottoPrice;
 
-    public LottoStatistics(List<Integer> winningNumbers, int bonusNumber, List<Lotto> lottoList) {
+    public LottoStatistics(List<Integer> winningNumbers, int bonusNumber, List<Lotto> lottoList, int lottoPrice) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
         this.lottoList = lottoList;
+        this.lottoPrice = lottoPrice;
         this.resultStatistics = new ArrayList<>();
         compareNumber();
     }
@@ -32,13 +32,13 @@ public class LottoStatistics {
     private void compareNumber() {
         for (Lotto lotto : lottoList) {
             int count = matchWinningNumbers(lotto);
-                getRank(count, lotto);
+            getRank(count, lotto);
         }
     }
 
     private void getRank(final int count, final Lotto lotto) {
         if (count < MIN_WIN_COUNT) {
-            return ;
+            return;
         }
 
         if (matchBonusNumber(count, lotto)) {
@@ -48,10 +48,17 @@ public class LottoStatistics {
         }
     }
 
-    private boolean matchBonusNumber(final int count, final Lotto lotto) {
-        return count == MATCH_FIVE && lotto.getLottoList().contains(bonusNumber);
+    public String getLottoEarningRate() {
+        double totalPrice = 0;
+        for (Statistics statistics : resultStatistics) {
+            totalPrice += statistics.getMoney();
+        }
+        return String.format("%.2f",totalPrice / lottoPrice);
     }
 
+    private boolean matchBonusNumber(final int count, final Lotto lotto) {
+        return count == MATCH_FOUR && lotto.getLottoList().contains(bonusNumber);
+    }
 
     private boolean isWinningNumber(final int number) {
         return winningNumbers.contains(number);
