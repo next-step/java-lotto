6e6c02f3acd9a42ba41877abdb49ec24a8d82232
SeungyeonJung <crowsy01@gmail.com>
2022-02-05 16:59:01 +0900

    feat(OutputView): 사용자 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,41 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.controller.ResultGroupDto;
+import lotto.domain.Lotto;
+import lotto.domain.WinningResult;
+
 public class OutputView {
+
+    private static final String LOTTO_COUNT_PRINT_FORMAT = "%d개를 구매했습니다\n";
+    private static final String LOTTO_PRINT_FORMAT = "[%s]\n";
+    private static final String STATICS_RESULT_FORMAT = "%d개 일치 (%d원) - %d개\n";
+    private static final String PROFIT_RESULT_FORMAT = "총 수익률은 %.2f입니다";
+    private static final double PROFIT_THRESHOLD = 1.0;
 
     private OutputView() {
     }
 
     public static void printLottoCount(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+        System.out.printf(LOTTO_COUNT_PRINT_FORMAT, count);
     }
 
+    public static void printLotto(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.printf(LOTTO_PRINT_FORMAT, lotto);
+        }
+    }
+
+    public static void printResult(ResultGroupDto resultGroup) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        for (WinningResult result : WinningResult.winningResults()) {
+            int count = resultGroup.howManyHave(result);
+            System.out.printf(STATICS_RESULT_FORMAT, result.count(), result.prize(), count);
+        }
+        System.out.printf(PROFIT_RESULT_FORMAT, resultGroup.getProfits());
+        if (resultGroup.getProfits() < PROFIT_THRESHOLD) {
+            System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        }
+    }
 }
