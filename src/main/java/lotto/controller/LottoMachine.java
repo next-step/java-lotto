f1e5868ebb40b19e82f8f77fb2d6ad284f96bf48
SeungyeonJung <crowsy01@gmail.com>
2022-02-05 17:12:54 +0900

    feat(LottoMachine): 로또 구매하기 기능 구현

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -1,7 +1,12 @@
 package lotto.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.domain.Lotto;
 import lotto.domain.LottoCount;
 import lotto.domain.LottoGenerator;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
 
 public class LottoMachine {
@@ -12,6 +17,17 @@ public class LottoMachine {
     }
 
     public static LottoTicket purchaseLotto(LottoCount lottoCount) {
-        return null;
+        List<Lotto> lottos = IntStream.range(0, lottoCount.count())
+            .mapToObj((i) -> new Lotto(generateLottoNumber()))
+            .collect(Collectors.toList());
+        return new LottoTicket(lottos);
+    }
+
+    private static List<LottoNumber> generateLottoNumber() {
+        return LOTTO_GENERATOR.generateLotto()
+            .stream()
+            .sorted()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
     }
 }
