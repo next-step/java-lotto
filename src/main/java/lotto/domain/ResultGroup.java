836300cd2f989ea4e0e30fb869a6ce90e962eb40
SeungyeonJung <crowsy01@gmail.com>
2022-02-05 16:36:48 +0900

    feat(ResultGroup): 정답 그룹으로 정답 갯수 확인 기능 구현

--- src/main/java/lotto/domain/ResultGroup.java
+++ src/main/java/lotto/domain/ResultGroup.java
@@ -1,16 +1,24 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResultGroup {
 
-    public ResultGroup() {
+    private Map<WinningResult, Integer> resultGroup;
 
+    public ResultGroup() {
+        this.resultGroup = new HashMap<>();
+        for (WinningResult winningResult : WinningResult.values()) {
+            resultGroup.put(winningResult, 0);
+        }
     }
 
     public void updateResult(WinningResult result) {
-
+        resultGroup.put(result, resultGroup.get(result) + 1);
     }
 
     public int howManyHave(WinningResult result) {
-        return 0;
+        return resultGroup.get(result);
     }
 }
