8f2163c4f1717e42b40404eaa2207f050169c395
Hae-Riri <ehl3288@naver.com>
2022-02-06 17:46:41 +0900

    refactor: 입출력 로직 분리

--- src/main/java/calculator/CalculatorApplication.java
+++ src/main/java/calculator/CalculatorApplication.java
@@ -1,15 +1,16 @@
 package calculator;
 
 import calculator.domain.StringCalculator;
-import java.util.Scanner;
+import calculator.view.InputView;
+import calculator.view.ResultView;
 
 public class CalculatorApplication {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
+        String text = InputView.getText();
 
-        StringCalculator stringCalculator = new StringCalculator();
-        System.out.println(stringCalculator.add(text));
+        StringCalculator stringCalculator = new StringCalculator(text);
+
+        ResultView.printResult(stringCalculator.add());
     }
 }
