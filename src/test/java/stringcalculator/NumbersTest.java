c6c4d166bf494bf8592e4c7651a96f9152686666
hyoenju <l67378810@gmail.com>
2022-02-06 17:22:07 +0900

    test(numbers): calculatorTest 내 테스트 이동

---
+++ src/test/java/stringcalculator/NumbersTest.java
@@ -0,0 +1,27 @@
+package stringcalculator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import stringcalculator.domain.Numbers;
+
+class NumbersTest {
+
+    @Test
+    void 숫자_이외의_값_음수를_전달하면_에러_반환() {
+        List<String> numbers = Arrays.asList("1", "2", "-11as", "a");
+        assertThatThrownBy(() -> new Numbers(numbers))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 숫자리스트_계산_테스트(){
+        Assertions.assertThat(new Numbers(Arrays.asList("1", "2", "3")).sum()).isEqualTo(6);
+    }
+}
+
+
+
