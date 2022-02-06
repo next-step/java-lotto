9363594231920bba94d6348d18ce60f0ea95f3a2
hyoenju <l67378810@gmail.com>
2022-02-06 15:42:48 +0900

    refactor(lotto): Add Variable(Error Message)

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,7 +10,10 @@ public class InputView {
     private static final String PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WIN_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
-    private static final int LOTTO_LENGTH = 6;
+    private static final String ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요";
+
+    private static final String SPLIT = ",";
+
     private final Scanner scanner = new Scanner(System.in);
 
     private String getInputValue(String message) {
@@ -22,33 +25,27 @@ public class InputView {
         try {
             return Integer.parseInt(getInputValue(PRICE_MESSAGE));
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("숫자를 입력해주세요");
+            throw new NumberFormatException(ERROR_MESSAGE);
         }
     }
 
-    public final List<Integer> getWinNumbers() {
-        String[] winNumbers = getInputValue(WIN_NUMBER_MESSAGE).split(",");
-        if (winNumbers.length != LOTTO_LENGTH) {
-            throw new IllegalArgumentException("숫자 6개를 입력해주세요");
-        }
+    public final List<Integer> getWinningNumbers() {
+        String[] winNumbers = getInputValue(WIN_NUMBER_MESSAGE).split(SPLIT);
         try {
             return Arrays.stream(winNumbers)
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("숫자를 입력해주세요");
+            throw new NumberFormatException(ERROR_MESSAGE);
         }
-
     }
 
     public final int getBonusNumber() {
         try {
             return Integer.parseInt(getInputValue(BONUS_NUMBER_MESSAGE));
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("숫자를 입력해주세요");
+            throw new NumberFormatException(ERROR_MESSAGE);
         }
     }
-
-
 }
