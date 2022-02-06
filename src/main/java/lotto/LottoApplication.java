6e47939a44be4351ff9ffbc2377c377ff9d4731a
0JUUU <clleo97@naver.com>
2022-02-07 04:17:01 +0900

    refactor: 직접 input, result, 비즈니스 로직을 실행하는 것이 아닌 controller로 위임

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,35 +1,11 @@
 package lotto;
 
-import java.util.List;
-import java.util.Map;
-import lotto.domain.Lotto;
-import lotto.domain.LottoRaffle;
-import lotto.domain.LottoResult;
-import lotto.domain.Lottos;
-import lotto.domain.WinningLotto;
-import lotto.domain.YieldCalculator;
-import lotto.view.InputView;
-import lotto.view.ResultView;
+import lotto.controller.LottoController;
 
 public class LottoApplication {
 
     public static void main(String[] args) {
-        final int money = InputView.inputMoney();
-        final int lottoCount = money / 1000;
-        ResultView.printLottoCount(lottoCount);
-
-        final Lottos lottos = new Lottos(lottoCount);
-        ResultView.printLottos(lottos);
-
-        final List<Integer> winningNumbers = InputView.inputLastWinningNumbers();
-        final Integer bonusNumber = InputView.inputBonusNumber(winningNumbers);
-
-        final LottoRaffle lottoRaffle = new LottoRaffle(new WinningLotto(winningNumbers, bonusNumber));
-        for (Lotto lotto : lottos.getLottos()) {
-            lottoRaffle.compareLotto(lotto);
-        }
-        final Map<LottoResult, Integer> results = lottoRaffle.getResults();
-        double yield = YieldCalculator.calculateYield(results, money);
-        ResultView.printLottoResults(results, yield);
+        final LottoController lottoController = new LottoController();
+        lottoController.start();
     }
 }
