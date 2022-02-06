101378b28d3e33942a367d9aad2f5acd14988345
hyojeong <qkrgywjd456@naver.com>
2022-02-07 03:23:02 +0900

    refactor(input): 상수 static 추가

--- src/main/java/stringcalculator/domain/UserInput.java
+++ src/main/java/stringcalculator/domain/UserInput.java
@@ -1,11 +1,10 @@
 package stringcalculator.domain;
 
 import java.util.Objects;
-import java.util.Scanner;
 
 public class UserInput {
 
-    private static final String USER_INPUT="0";
+    private static final String USER_INPUT = "0";
     private String userInput;
 
     public UserInput(String userInput) {
