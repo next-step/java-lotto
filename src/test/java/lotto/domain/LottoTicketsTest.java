a030c70d9da99669c873da0e5ae68d1ec2ddebe5
hyoenju <l67378810@gmail.com>
2022-02-06 17:23:42 +0900

    style: Add Line Break

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -1,8 +1,5 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
@@ -12,8 +9,11 @@ class LottoTicketsTest {
 
     @Test
     void 로또_장당_당첨번호_개수_일치여부_테스트() {
-        LottoTickets lottoTickets=new LottoTickets(2000,Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,6)),new Lotto(Arrays.asList(5,6,7,8,9,10))));
-        List<Integer> correctWinNumbers = lottoTickets.countCorrectWinNumber(
+        LottoTickets lottoTickets = new LottoTickets(2000,
+            Arrays.asList(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(5, 6, 7, 8, 9, 10))));
+        List<Integer> correctWinNumbers = lottoTickets.countNumberOfMatches(
             new LottoMachine(Arrays.asList(1, 2, 3, 4, 5, 6), 8));
         Assertions.assertThat(correctWinNumbers.get(0)).isEqualTo(6);
         Assertions.assertThat(correctWinNumbers.get(1)).isEqualTo(2);
@@ -21,7 +21,10 @@ class LottoTicketsTest {
 
     @Test
     void 로또_장당_보너스_개수_일치여부_테스트() {
-        LottoTickets lottoTickets=new LottoTickets(2000,Arrays.asList(new Lotto(Arrays.asList(1,2,3,4,5,6)),new Lotto(Arrays.asList(5,6,7,8,9,10))));
+        LottoTickets lottoTickets = new LottoTickets(2000,
+            Arrays.asList(
+                new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                new Lotto(Arrays.asList(5, 6, 7, 8, 9, 10))));
         List<Integer> checkBonusNumber = lottoTickets.checkBonusNumber(
             new LottoMachine(Arrays.asList(1, 2, 3, 4, 5, 6), 8));
         Assertions.assertThat(checkBonusNumber.get(0)).isEqualTo(0);
