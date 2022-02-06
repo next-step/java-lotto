4e8d1bff8ae28b54e5f5bb2bf05023b8e6f5cf20
kimtaejun97 <kim_taejun@naver.com>
2022-02-06 22:17:36 +0900

    test: 커스텀 구분자, getValuesToString 테스트 추가.
    - 커스텀 구분자 생성.
    - 구분자들을 String 으로 연결하여 반환.

--- src/test/java/stringcalculator/domain/DelimitersTest.java
+++ src/test/java/stringcalculator/domain/DelimitersTest.java
@@ -9,14 +9,36 @@ import org.junit.jupiter.api.Test;
 
 class DelimitersTest {
 
-    @DisplayName("생성 잘 되는지")
+    @DisplayName("공백이 들어오면 기본 구분자로 생성한다.")
     @Test
-    void delimiter_생성_확인() {
+    void delimiter_기본_구분자_생성() {
         final List<String> expected = Arrays.asList(":", ",");
         final Delimiters delimiters = new Delimiters("");
 
         final String actual = delimiters.getValuesToString();
 
         assertThat(actual).isEqualTo(String.join("", expected));
+    }
+
+    @DisplayName("구분자들을 String 으로 연결하여 반환한다")
+    @Test
+    void getValuesToString() {
+        final Delimiters delimiters = new Delimiters("");
+
+        final String actual = delimiters.getValuesToString();
+
+        assertThat(actual).isEqualTo(":,");
+
+    }
+
+    @DisplayName("커스텀 구분자 생성")
+    @Test
+    void delimiter_커스텀_구분자_생성() {
+        String inputCustomDelimiter = "ab";
+        final Delimiters delimiters = new Delimiters(inputCustomDelimiter);
+
+        final String actual = delimiters.getValuesToString();
+
+        assertThat(actual).isEqualTo(inputCustomDelimiter);
     }
 }
