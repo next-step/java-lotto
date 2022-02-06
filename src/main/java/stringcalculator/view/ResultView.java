07bf80cd74a34c00b83d4dcf16f7b72f3660a962
hyoenju <l67378810@gmail.com>
2022-02-06 17:27:29 +0900

    feat(result): 결과 출력 텍스트 수정

--- src/main/java/stringcalculator/view/ResultView.java
+++ src/main/java/stringcalculator/view/ResultView.java
@@ -3,7 +3,6 @@ package stringcalculator.view;
 public class ResultView {
 
     public void printSum(final int sum) {
-        System.out.println(sum);
+        System.out.printf("결과 : %d%n", sum);
     }
-
 }
