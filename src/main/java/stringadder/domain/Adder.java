dee640cf202d95b165869e05edc3e5364af4fc23
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-07 17:14:03 +0900

    refactor(stringAdder): Adder 클래스 수정
    - 매직넘버, 리터럴 적용
    - stream() 정리 ( '.'를 기준으로)
    - validateNumber 메소드생성#숫자 검증

--- src/main/java/stringadder/domain/Adder.java
+++ src/main/java/stringadder/domain/Adder.java
@@ -7,22 +7,38 @@ import stringadder.utils.exception.InputValidException;
 
 public class Adder {
 
+    private static final String BLANK = " ";
+    private static final String EMPTY_SPACE = "";
+    private static final String NULL = null;
+    private static final int VALIDATE_NUMBER_TRUE = 0;
+    private static final int VALIDATE_NUMBER_FALSE = -1;
+
     public static int add(List<String> numbers){
 
-        if(numbers.contains(" ") ||
-            numbers.contains("") ||
-            numbers.contains(null)
-        ) {
-            return 0;
+        int x = validateNumber(numbers);
+        if (x != VALIDATE_NUMBER_FALSE) {
+            return x;
         }
 
+        List<Integer> convertInt = numbers.stream()
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
 
-        List<Integer> convertInt = numbers.stream().map(Integer::parseInt).collect(Collectors.toList());
-        convertInt.stream().peek(InputValidException::inputNegativeValidation).collect(Collectors.toList());
+        convertInt.stream()
+            .peek(InputValidException::inputNegativeValidation)
+            .collect(Collectors.toList());
 
-        return convertInt.stream().mapToInt(Integer::intValue).sum();
+        return convertInt.stream()
+            .mapToInt(Integer::intValue)
+            .sum();
     }
 
-
-
+    private static int validateNumber(List<String> numbers) {
+        if(numbers.contains(BLANK) ||
+            numbers.contains(EMPTY_SPACE) ||
+            numbers.contains(NULL)) {
+            return VALIDATE_NUMBER_TRUE;
+        }
+        return VALIDATE_NUMBER_FALSE;
+    }
 }
