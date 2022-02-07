c671a9eb733b5b3d7540a6c9a8b6e8306f225e53
Hae-Riri <ehl3288@naver.com>
2022-02-07 22:24:04 +0900

    fix: 당첨번호와 보너스번호 간 중복 체크 추가

--- src/main/java/lotto/factory/WinningBallFactory.java
+++ src/main/java/lotto/factory/WinningBallFactory.java
@@ -11,11 +11,18 @@ public class WinningBallFactory {
     }
 
     public static List<WinningBall> of(String[] numbers, String bonusNumber) {
+        validateBonusNumberDuplication(numbers, bonusNumber);
         List<WinningBall> balls = Arrays.stream(numbers)
             .map(WinningBall::new)
             .collect(Collectors.toList());
 
         balls.add(new WinningBall(bonusNumber, true));
         return balls;
+    }
+
+    private static void validateBonusNumberDuplication(String[] numbers, String bonusNumber) {
+        if(Arrays.asList(numbers).contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호와 당첨 번호가 같습니다.");
+        }
     }
 }
