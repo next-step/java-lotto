22ed74cd24a6f828be31a6a4be23d8fbf2a7d104
Ji In Lee <loveted0225@naver.com>
2022-02-07 22:40:28 +0900

    refactor: Adder 생성자 변경에 따라 테스트코드 수정

--- src/test/java/calculator/model/AdderTest.java
+++ src/test/java/calculator/model/AdderTest.java
@@ -2,37 +2,24 @@ package calculator.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import calculator.model.Adder;
-import calculator.model.Token;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class AdderTest {
 
     @Test
     void 더하기() {
-        List<Token> tokens = Arrays.asList(
-            new Token("1"),
-            new Token("2"),
-            new Token("3")
-        );
-
-        Adder adder = new Adder(tokens);
+        String[] parsedInput = {"1", "2", "3"};
+        Adder adder = new Adder(new Token(parsedInput));
         int result = adder.calculate();
 
         assertThat(6).isEqualTo(result);
     }
 
     @Test
-    void 숫자_하나(){
-        List<Token> tokens = Arrays.asList(
-            new Token("1")
-        );
-
-        Adder adder = new Adder(tokens);
+    void 숫자_하나만_들어왔을_경우_결과값은_그_숫자() {
+        String[] parsedInput = {"3"};
+        Adder adder = new Adder(new Token(parsedInput));
         int result = adder.calculate();
-
-        assertThat(1).isEqualTo(result);
+        assertThat(3).isEqualTo(result);
     }
 }
