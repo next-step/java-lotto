9ba10f5937f907d4cb1fe5498cf6dd21a15e0956
0JUUU <clleo97@naver.com>
2022-02-07 17:21:46 +0900

    refactor: 불필요한 Integer value 인스턴스변수 제거

--- src/main/java/lotto/domain/lotto/number/BonusNumber.java
+++ src/main/java/lotto/domain/lotto/number/BonusNumber.java
@@ -1,22 +1,15 @@
 package lotto.domain.lotto.number;
 
 import lotto.util.BonusNumberValidator;
+import lotto.util.Validator;
 
 public class BonusNumber extends Number {
 
-    private final Integer value;
-
     public BonusNumber(String input, Numbers numbers) {
-        this(BonusNumberValidator.parseToInt(input), numbers);
+        this(Validator.parseToInt(input), numbers);
     }
 
     public BonusNumber(Integer value, Numbers numbers) {
-        super(value);
-        this.value = BonusNumberValidator.validateDuplicateBonus(value, numbers.getNumbers());
-    }
-
-    @Override
-    public Integer getValue() {
-        return value;
+        super(BonusNumberValidator.validateDuplicateBonus(value, numbers.getNumbers()));
     }
 }
