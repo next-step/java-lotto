627a52925f6e3dc4b32d5169d99bf7db6fa7dc7d
YangWoo Yu <yuyw0712@naver.com>
2022-02-07 17:55:27 +0900

    refactor: Parser 생성자로 split 1. CUSTOM_DELIMITER 혹은 PARSED_FORMULAR를 getter로 가져올 수 있도록 변경
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/calculator/domain/Parser.java
+++ src/main/java/calculator/domain/Parser.java
@@ -13,29 +13,33 @@ public class Parser {
     public static final String ERROR_NEGATIVE_NUMBER = "입력한 문자열에 음수가 있습니다.";
     private static final int CUSTOM_DELIMITER = 1;
     private static final int PARSED_FORMULAR = 2;
-
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\\n(.*)");
 
-    public List<Integer> splitCustomStringToNumber(String splitedFormular, String delimiter) {
+    private String parsedFormular;
+    private String customDelimiter;
+
+    public Parser(String formula) {
+        splitCustom(formula);
+    }
+
+    public static List<Integer> splitCustomStringToNumber(String splitedFormular,
+        String delimiter) {
         validateFormular(Arrays.asList(splitedFormular.split(delimiter)));
         return Arrays.asList(splitedFormular.split(delimiter)).stream()
             .map(Integer::parseInt).collect(
                 Collectors.toList());
     }
 
-    public ArrayList<String> splitCustom(String formula) {
+    private void splitCustom(String formula) {
         Matcher matcher = CUSTOM_PATTERN.matcher(formula);
         if (matcher.find()) {
-            return new ArrayList<>(
-                Arrays.asList(matcher.group(PARSED_FORMULAR), matcher.group(CUSTOM_DELIMITER)));
+            parsedFormular = matcher.group(PARSED_FORMULAR);
+            customDelimiter = matcher.group(CUSTOM_DELIMITER);
         }
-
-        return new ArrayList<>(Arrays.asList(formula));
     }
 
-    private void validateFormular(List<String> splitedFormular) {
+    private static void validateFormular(List<String> splitedFormular) {
         List<Integer> integeredFormular = stringListToInteger(splitedFormular);
-
         for (int formularNumber : integeredFormular) {
             if (formularNumber < 0) {
                 throw new RuntimeException(OutputView.ERROR + ERROR_NEGATIVE_NUMBER);
@@ -43,7 +47,16 @@ public class Parser {
         }
     }
 
-    private List<Integer> stringListToInteger(List<String> splitedFormular) {
+    private static List<Integer> stringListToInteger(List<String> splitedFormular) {
         return splitedFormular.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
+
+    public String getParsedFormular() {
+        return parsedFormular;
+    }
+
+    public String getCustomDelimiter() {
+        return customDelimiter;
+    }
+
 }
--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -8,7 +8,6 @@ public class StringCalculator {
     private final static int SPLITED_FORMULAR_POSITION = 0;
     private final static int CUSTOM_FORMULAR_SIZE = 2;
     private final static int DELIMITER_POSITON = 1;
-    private int total;
     private String input;
 
     public StringCalculator(String input) {
@@ -16,20 +15,17 @@ public class StringCalculator {
     }
 
     public int add() {
-        Parser parser = new Parser();
-        List<String> customSymbolAndFormula = parser.splitCustom(input);
-        String splitedFormular = customSymbolAndFormula.get(SPLITED_FORMULAR_POSITION);
+        Parser parser = new Parser(input);
+        String splitedFormular = parser.getParsedFormular();
 
-        if (customSymbolAndFormula.size() == CUSTOM_FORMULAR_SIZE) {
-            String delimeter = customSymbolAndFormula.get(DELIMITER_POSITON);
-            total = parser.splitCustomStringToNumber(splitedFormular, delimeter + "|,|:").stream()
-                .mapToInt(Integer::intValue).sum();
-        } else {
-            total = parser.splitCustomStringToNumber(splitedFormular, ",|:").stream()
+        if (!Objects.isNull(parser.getCustomDelimiter())) {
+            String delimeter = parser.getCustomDelimiter();
+            return Parser.splitCustomStringToNumber(splitedFormular, delimeter + "|,|:").stream()
                 .mapToInt(Integer::intValue).sum();
         }
 
-        return total;
+        return Parser.splitCustomStringToNumber(input, ",|:").stream()
+            .mapToInt(Integer::intValue).sum();
     }
 
     private String isEmptyOrNull(String input) {
