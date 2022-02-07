beae1fd177db940c0f0657be423326a7079dbc20
0JUUU <clleo97@naver.com>
2022-02-07 18:35:59 +0900

    feat: Numbers에 관한 생성자 추가 & 생성자 메서드 체이닝 사용
    - 가변인자를 사용한 생성자 추가
    - 하나의 생성자를 활용하도록 생성자 메서드 체이닝 추가

--- src/main/java/lotto/domain/lotto/number/Numbers.java
+++ src/main/java/lotto/domain/lotto/number/Numbers.java
@@ -1,23 +1,29 @@
 package lotto.domain.lotto.number;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import lotto.util.NumbersValidator;
 
 public class Numbers {
 
     private final List<Number> numbers;
 
-    public Numbers(String input) {
-        this.numbers = NumbersValidator.validate(input);
+    public Numbers(final String input) {
+        this(NumbersValidator.validate(input));
     }
 
-    public Numbers(List<Number> numbers) {
+    public Numbers(final int... numbers) {
+        this(Arrays.stream(numbers).mapToObj(Number::new).collect(Collectors.toList()));
+    }
+
+    public Numbers(final Numbers newNumbers) {
+        this(newNumbers.getNumbers());
+    }
+
+    public Numbers(final List<Number> numbers) {
         this.numbers = new ArrayList<>(numbers);
-    }
-
-    public Numbers(Numbers numbers) {
-        this.numbers = numbers.getNumbers();
     }
 
     public List<Number> getNumbers() {
