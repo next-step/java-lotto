863e1c5484d4726922b1521773c9fd894911c8f5
YangWoo Yu <yuyw0712@naver.com>
2022-02-07 22:15:22 +0900

    refactor: 원시자료형 LottoNumber로 래핑
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private final static int MIN_NUMBER = 1;
+    private final static int MAX_NUMBER = 45;
+
+    private int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        if (lottoNumber < MIN_NUMBER || lottoNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45까지입니다");
+        }
+        this.lottoNumber = lottoNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
+    }
+
+    public int getLottoNumber() {
+        return lottoNumber;
+    }
+}
