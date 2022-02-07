30ea5c992d9f902b8604424376a2ebc60af6c06e
MinChul-Son <smc5236@naver.com>
2022-02-07 20:58:11 +0900

    🚀 refactor(lotto): 변경된 코드로 인한 깨진 코드 복구

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.domain.LottoGame;
 import lotto.domain.LottoShop;
 import lotto.view.LottoInputView;
 import lotto.view.LottoResultView;
@@ -9,8 +10,7 @@ public class LottoApplication {
 
     public static void main(String[] args) {
         LottoController lottoController = new LottoController(new LottoInputView(),
-            new LottoResultView(),
-            LottoShop.getInstance());
+            new LottoResultView(), LottoShop.getInstance(), new LottoGame());
         lottoController.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,13 @@
 package lotto.controller;
 
 
+import java.util.List;
+import java.util.Map;
+import lotto.domain.LottoGame;
+import lotto.domain.LottoNumber;
 import lotto.domain.LottoShop;
+import lotto.domain.Lottos;
+import lotto.domain.Ranking;
 import lotto.view.LottoInputView;
 import lotto.view.LottoResultView;
 
@@ -10,18 +16,29 @@ public class LottoController {
     private final LottoInputView lottoInputView;
     private final LottoResultView lottoResultView;
     private final LottoShop lottoShop;
+    private final LottoGame lottoGame;
 
     public LottoController(final LottoInputView lottoInputView,
-        final LottoResultView lottoResultView, final LottoShop lottoShop) {
+        final LottoResultView lottoResultView, final LottoShop lottoShop,
+        final LottoGame lottoGame) {
+
         this.lottoInputView = lottoInputView;
         this.lottoResultView = lottoResultView;
         this.lottoShop = lottoShop;
+        this.lottoGame = lottoGame;
     }
 
     public void run() {
-        lottoInputView.prepareLottoGame(lottoShop);
-        lottoResultView.findWinner(lottoInputView.getWinningNumbers(), lottoInputView.getLottos(),
-            lottoInputView.getBonusBall());
-        lottoResultView.finishGame(lottoInputView.getPrice());
+        final int inputPrice = lottoInputView.inputPrice();
+        final Lottos lottos = lottoGame.startLotto(inputPrice, lottoShop);
+
+        lottoInputView.printLottos(lottos);
+
+        final List<LottoNumber> winningNumber = lottoInputView.inputWinningNumber();
+        final LottoNumber bonusBall = lottoInputView.inputBonusBall();
+
+        final Map<Ranking, Integer> totalResult = lottoGame.findWinner(winningNumber, lottos,
+            bonusBall);
+        lottoResultView.finishGame(totalResult, inputPrice);
     }
 }
