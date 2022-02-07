bf0b9b219af09996d1a009a406de0440b5ade17b
PandaHun <gnsgnsek@gmail.com>
2022-02-07 09:44:53 +0900

    test(StringParser): Add split custom delimiter exception test

--- src/test/java/stringcaculator/util/StringParserTest.java
+++ src/test/java/stringcaculator/util/StringParserTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringParserTest {
 
@@ -20,5 +21,12 @@ class StringParserTest {
     void 커스텀_구분자_테스트(String numbers) {
         String[] splitNumbers = StringParser.splitCustomDelimiter(numbers);
         assertThat(splitNumbers.length).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={"//\n1;2;3", "//1!2!3"})
+    void 커스텀_구분자_예외_테스트(String numbers) {
+        assertThatThrownBy(() -> StringParser.splitCustomDelimiter(numbers))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
