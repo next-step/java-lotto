f64f0974898e9b283f7ca3c7e07f45edbd490d0a
hyoenju <l67378810@gmail.com>
2022-02-07 22:05:14 +0900

    test(rank):  Ranking 수정에 따른 테스트 변경

--- src/test/java/lotto/domain/RankingTest.java
+++ src/test/java/lotto/domain/RankingTest.java
@@ -3,24 +3,25 @@ package lotto.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class WinningPriceTest {
+class RankingTest {
 
     @Test
     void enum_FIVE_반환_테스트() {
         Ranking ranking = Ranking.of(5, 0);
-        Assertions.assertThat(ranking).isEqualTo(Ranking.FIVE);
+        Assertions.assertThat(ranking).isEqualTo(Ranking.THIRD);
     }
 
     @Test
     void enum_FIVE_BONUS_반환_테스트() {
         Ranking ranking = Ranking.of(5, 1);
-        Assertions.assertThat(ranking).isEqualTo(Ranking.FIVEWITHBONUS);
+        Assertions.assertThat(ranking).isEqualTo(Ranking.SECOND);
     }
 
     @Test
     void 로또_당첨_금액_테스트() {
         Ranking ranking = Ranking.of(5, 0);
-        Assertions.assertThat(ranking.operate(1)).isEqualTo(1500000);
+        ranking.addWinningCount();
+        Assertions.assertThat(ranking.operate()).isEqualTo(1500000);
     }
 
 
