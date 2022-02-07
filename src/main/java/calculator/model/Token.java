bebaeb7f7e7e94b6255cd41cbfb93c6a30b5673e
Ji In Lee <loveted0225@naver.com>
2022-02-07 22:37:16 +0900

    feat(Token): equals와 hashCode 오버라이드 메소드 추가

--- src/main/java/calculator/model/Token.java
+++ src/main/java/calculator/model/Token.java
@@ -2,19 +2,23 @@ package calculator.model;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Token {
 
     private int operand;
     private List<Token> tokens;
+
     public Token(String parseResult) {
         this.operand = parseStringToInteger(parseResult);
         checkNegative(operand);
     }
+
     public Token(String[] splitInput) {
         tokens = Arrays.stream(splitInput).map(Token::new).collect(Collectors.toList());
     }
+
     public int getOperand() {
         return operand;
     }
@@ -28,13 +32,31 @@ public class Token {
         }
 
     }
-    private void checkNegative(int operand){
-        if(operand<0){
+
+    private void checkNegative(int operand) {
+        if (operand < 0) {
             throw new RuntimeException("음수입니다");
         }
     }
+
     public List<Token> getTokens() {
         return tokens;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Token token = (Token) o;
+        return operand == token.operand && Objects.equals(tokens, token.tokens);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operand, tokens);
+    }
 }
