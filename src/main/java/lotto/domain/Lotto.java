494e7c5d2d7f7a3ef04b3216898837fb154de6b1
hochan222 <hochan049@gmail.com>
2022-02-08 17:17:27 +0900

    refactor: `.subList()`는 메모리 누수 위험이 있다. `.stream().limit()`을 사용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,8 +3,10 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_MAX_SIZE = 6;
@@ -27,6 +29,8 @@ public class Lotto {
         }
         Collections.shuffle(lottoList);
 
-        return lottoList.subList(0, LOTTO_MAX_SIZE);
+        return lottoList.stream()
+            .limit(LOTTO_MAX_SIZE)
+            .collect(Collectors.toList());
     }
 }
