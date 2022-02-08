9e919c5ba6513be71531abe6b981e47824f596a2
SeungYeonJeong <crowsy@naver.com>
2022-02-08 17:36:14 +0900

    refactor: Remove unnecessary code

--- src/main/java/stringcaculator/util/StringParser.java
+++ src/main/java/stringcaculator/util/StringParser.java
@@ -7,7 +7,6 @@ public class StringParser {
 
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER = "//(.)\\n(.*)";
-    private static final String PATTERN_NOT_FOUND = "[ERROR] 커스텀 문자열을 찾을 수 없습니다.";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
     private static final int DELIMITER_POSITION = 1;
     private static final int REFINE_POSITION = 2;
@@ -20,11 +19,14 @@ public class StringParser {
 
     public static String[] splitCustomDelimiter(String numbers) {
         Matcher matcher = CUSTOM_PATTERN.matcher(numbers);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException(PATTERN_NOT_FOUND);
+        String customDelimiter = DEFAULT_DELIMITER;
+        String refinedExpression = numbers;
+
+        if (matcher.find()) {
+            customDelimiter = matcher.group(DELIMITER_POSITION);
+            refinedExpression = matcher.group(REFINE_POSITION);
         }
-        String customDelimiter = matcher.group(DELIMITER_POSITION);
-        String refinedExpression = matcher.group(REFINE_POSITION);
+
         return refinedExpression.split(customDelimiter);
     }
 }
