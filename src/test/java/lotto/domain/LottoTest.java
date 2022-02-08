290d6b2b1a383ef70b2e4b23ea927f1bc27d0df5
YangWoo Yu <yuyw0712@naver.com>
2022-02-08 21:26:32 +0900

    test: LottoTest.java 테스트 코드 작성
    Co-authored-by: hochan222 hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,37 +1,27 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Arrays;
+import java.util.HashSet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
 
-    Lotto lotto;
-    List<LottoNumber> lottoList;
-
-    @BeforeEach
-    void setUp() {
-//        lotto = new Lotto();
-//        lottoList = lotto.getLottoList();
-    }
-
     @DisplayName(value = "로또가 생성될 때, 로또 하나에는 6개의 숫자가 들어간다.")
     @Test
     void checkValidSize() {
-        assertThat(lottoList.size()).isEqualTo(6);
+        Lotto lotto = new Lotto(new HashSet(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        assertThat(lotto.getLottoNumber().size()).isEqualTo(6);
     }
 
-//    @DisplayName(value = "로또의 범위는 1~45까지이다.")
-//    @Test
-//    void checkValidRange() {
-//       boolean isInRange = lottoList.stream().allMatch(LottoTest::isInRange);
-//       assertThat(isInRange).isTrue();
-//    }
-
-    static boolean isInRange(final int number) {
-        return (number >= 1 && number <= 45);
+    @DisplayName(value = "로또 번호가 6개가 아니면 IllegalArgumentException이 발생한다.")
+    @Test
+    void checkValidRange() {
+        assertThatThrownBy(() -> new Lotto(new HashSet(Arrays.asList(1, 2, 3, 4, 5)))).isInstanceOf(
+            IllegalArgumentException.class);
     }
+
 }
