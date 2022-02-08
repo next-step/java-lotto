1e24c70665807af3a03f302013037021c6fca378
Hoya-kim <1203play@gmail.com>
2022-02-08 18:13:29 +0900

    refactor: Remove useless field("symbol") on Operation, and fix typo

--- src/main/java/stringcalculator/domain/Operation.java
+++ src/main/java/stringcalculator/domain/Operation.java
@@ -4,14 +4,12 @@ import java.util.function.BiFunction;
 
 public enum Operation {
 
-    PLUS("+", (number1, number2) -> number1 + number2),
-    MINUS("-", (number1, number2) -> number1 - number2);
+    PLUS((number1, number2) -> number1 + number2),
+    MINUS((number1, number2) -> number1 - number2);
 
-    private final String symbol;
     private final BiFunction<Integer, Integer, Integer> operator;
 
-    Operation(final String symbol, final BiFunction<Integer, Integer, Integer> operator) {
-        this.symbol = symbol;
+    Operation(final BiFunction<Integer, Integer, Integer> operator) {
         this.operator = operator;
     }
 
--- src/main/java/stringcalculator/view/InputView.java
+++ src/main/java/stringcalculator/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     }
 
     public static String getExpression() {
-        System.out.println(",혹은 :로 구분된 덧셈 식을 입력하세요 (ex. \"1,2\", \"1;2\")");
+        System.out.println(",혹은 :로 구분된 덧셈 식을 입력하세요 (ex. \"1,2\", \"1:2\")");
         return scanner.nextLine().replace("\\n", "\n");
     }
 }
