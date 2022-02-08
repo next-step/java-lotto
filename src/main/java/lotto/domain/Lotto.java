5aa02bfb8d067475b82936f815232c9bee5981cb
hochan222 <hochan049@gmail.com>
2022-02-08 20:18:28 +0900

    refactor: generatorLotto
    - while vs stream 구조 고민
    - IntStream.rangeClosed 고민

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Random;
 
 public class Lotto {
 
@@ -14,23 +14,24 @@ public class Lotto {
     private final List<Integer> lottoList;
 
     public Lotto() {
-        this.lottoList = generatorLotto(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER);
+        this.lottoList = generatorLotto();
     }
 
     public List<Integer> getLottoList() {
         return Collections.unmodifiableList(lottoList);
     }
 
-    private List<Integer> generatorLotto(int minNumber, int maxNumber) {
+    private int generatorNumber(int min, int max) {
+        return new Random().nextInt(max + min) + max;
+    }
+
+    private List<Integer> generatorLotto() {
         List<Integer> lottoList = new ArrayList<>();
 
-        for (int i = minNumber; i <= maxNumber; i++) {
-            lottoList.add(i);
+        while (lottoList.size() < LOTTO_MAX_SIZE) {
+            lottoList.add(generatorNumber(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER));
         }
-        Collections.shuffle(lottoList);
 
-        return lottoList.stream()
-            .limit(LOTTO_MAX_SIZE)
-            .collect(Collectors.toList());
+        return lottoList;
     }
 }
