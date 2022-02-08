bcbe664096f13e9b32575bf1f757e91f471518fc
Hoya-kim <1203play@gmail.com>
2022-02-08 18:08:41 +0900

    refactor: ParameterizedTest내 강화된 검증을 위한 파라미터 추가

--- src/test/java/stringcalculator/domain/StringCalculatorTest.java
+++ src/test/java/stringcalculator/domain/StringCalculatorTest.java
@@ -20,36 +20,37 @@ class StringCalculatorTest {
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1"})
+    @ValueSource(strings = {"1", "2", "3"})
     void oneNumber(final String text) {
         assertThat(StringCalculator.add(text)).isSameAs(Integer.parseInt(text));
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2"})
+    @ValueSource(strings = {"1,2", "2,1", "0,3"})
     void twoNumbers(final String text) {
         assertThat(StringCalculator.add(text)).isSameAs(3);
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2:3"})
+    @ValueSource(strings = {"1,2:3", "1:2,3", "1:2:3"})
     void colons(final String text) {
         assertThat(StringCalculator.add(text)).isSameAs(6);
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"//;\n1;2;3"})
+    @ValueSource(strings = {"//;\n1;2;3", "//-\n1-2-3"})
     void customDelimiter(final String text) {
         assertThat(StringCalculator.add(text)).isSameAs(6);
     }
 
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
-    @Test
-    void negative() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-2"})
+    void negative(final String token) {
         assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> StringCalculator.add("-1"));
+            .isThrownBy(() -> StringCalculator.add(token));
     }
 }
