af5c07b1cbeb15b02d39caedc8fde0a228ea71f7
Hoya-kim <1203play@gmail.com>
2022-02-08 18:11:04 +0900

    refactor: Add test for minus operation

--- src/test/java/stringcalculator/domain/OperationTest.java
+++ src/test/java/stringcalculator/domain/OperationTest.java
@@ -13,4 +13,11 @@ class OperationTest {
         int result = Operation.PLUS.operate(4, 5);
         assertThat(result).isEqualTo(9);
     }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void testMinus() {
+        int result = Operation.MINUS.operate(3, 2);
+        assertThat(result).isEqualTo(1);
+    }
 }
