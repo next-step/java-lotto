fb2d39afce63f0a4216370598519fb052a0d03d6
hochan222 <hochan049@gmail.com>
2022-02-09 00:32:07 +0900

    test: LottoTicket을 LottoTicketGenerator으로 네이밍 변경
    - Lottos를 LottoTicket 과 LottoTicketGenerator로 분리하기

--- src/test/java/lotto/domain/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/domain/LottoTicketGeneratorTest.java
@@ -10,10 +10,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTicketGeneratorTest {
 
-    @DisplayName(value = "정수 자료형인 LottoCount 로 LottoTicket 객체를 생성할때, LottoTicket.size()는 LottoCount와 동일하다.")
+    @DisplayName(value = "정수 자료형인 LottoCount 로 LottoTicketGenerator 객체를 생성할때, LottoTicket.size()는 LottoCount와 동일하다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3, 10, 20, 30})
-    void GivenLottoCountWithIntType_WhenGenerateLottoTicket_ThenLottoTicketSizeEqualsLottoCount(final int lottoCount) {
+    void GivenLottoCountWithIntType_WhenGenerateLottoTicketGenerator_ThenLottoTicketSizeEqualsLottoCount(final int lottoCount) {
         assertThat(LottoTicketGenerator.from(lottoCount).getLottoTicket().size()).isEqualTo(lottoCount);
     }
 }
