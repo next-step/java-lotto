04c653d5c04e29634f26a4b56036e35a7c2aa225
gal2o <hunmin8788@gmail.com>
2022-02-08 23:33:04 +0900

    refactor(Calculator): Parser 클래스 수정 및 매직넘버 상수화

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,33 +1,39 @@
 package calculator;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
 
     private String userInput;
-    private String delimiter;
+    private final String delimiter;
+    private final String MATCH_STRING = "//(.)\n(.*)";
 
     public Parser(String userInput) {
-        this.userInput =checkInputNullOrZero(userInput);
+        this.userInput = checkInputNullOrZero(userInput);
         this.delimiter = findDelimiter();
     }
-    private String checkInputNullOrZero(String userInput){
-        if (userInput==null ||userInput.equals("")){
+
+    private String checkInputNullOrZero(String userInput) {
+        if (Objects.isNull(userInput) || userInput.isEmpty()) {
             return "0";
         }
+
         return userInput;
     }
 
     private String findDelimiter() {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(userInput);
+        Matcher m = Pattern.compile(MATCH_STRING).matcher(userInput);
         if (m.find()) {
             this.userInput = m.group(2);
             return m.group(1);
         }
+
         return ",|:";
     }
+
     public List<Token> parseInput() {
         String[] parseResult = userInput.split(delimiter);
         Token token = new Token(parseResult);
