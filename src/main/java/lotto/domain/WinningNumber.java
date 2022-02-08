512458b255640a7779ea19eedc4d4eaf0e4eca99
YangWoo Yu <yuyw0712@naver.com>
2022-02-08 17:45:02 +0900

    refactor: WinningNumber 중복검사
    Co-authored-by: hochan222 hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -3,12 +3,19 @@ package lotto.domain;
 import java.util.List;
 
 public class WinningNumber {
-
+    private final static String ERROR_DISTINCT = "당첨 번호와 보너스 번호는 중복될 수 없습니다.";
     private List<LottoNumber> winningNumber;
     private LottoNumber bonusNumber;
 
     public WinningNumber(List<LottoNumber> winningNumber, LottoNumber bonusNumber) {
+        validate();
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate() {
+        if(winningNumber.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ERROR_DISTINCT);
+        }
     }
 }
