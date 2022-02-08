087a05737d55050d2b6bb76781391eca17298a14
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-08 22:52:14 +0900

    test(lotto): 매직 상수, 리터럴 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,11 @@ package lotto.domain;
 
 public class Money {
 
+    private static final String LESS_THOUSAND_ERROR_MESSAGE = "구매 금액은 1000원 이하일 수 없습니다.";
+    private static final String NEGATIVE_NUMBER_ERROR_MESSAGE = "구매 금액은 음수일 수 없습니다.";
+    private static final String NON_NUMBER_ERROR_MESSAGE = "구매 금액은 정수만 가능합니다.";
+    private static final int NUMBER_THOUSAND = 1000;
+    private static final int NUMBER_ZERO = 0;
     private final int value;
 
     public Money(final String value) {
@@ -11,7 +16,7 @@ public class Money {
         this.value = Integer.parseInt(value);
     }
 
-    public Money(final int value){
+    public Money(final int value) {
         this.value = value;
     }
 
@@ -21,21 +26,22 @@ public class Money {
 
     private void validateTypeLessThousand(String value) {
         System.out.println(value);
-        if (Integer.parseInt(value) < 1000) {
-            throw new IllegalArgumentException("구매 금액은 1000원 이하일 수 없습니다.");
+        if (Integer.parseInt(value) < NUMBER_THOUSAND) {
+            throw new IllegalArgumentException(LESS_THOUSAND_ERROR_MESSAGE);
         }
     }
 
     private void validateTypeNegativeNumber(String value) {
-        if(Integer.parseInt(value) <0)
-            throw new IllegalArgumentException("구매 금액은 음수일 수 없습니다.");
+        if (Integer.parseInt(value) < NUMBER_ZERO) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR_MESSAGE);
+        }
     }
 
     private int validateTypeInput(String value) {
         try {
             return Integer.parseInt(value);
-        }catch(NumberFormatException exception){
-            throw new IllegalArgumentException("구매 금액은 정수만 가능합니다. ");
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(NON_NUMBER_ERROR_MESSAGE);
         }
     }
 
