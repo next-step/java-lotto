4a20edeb1f676d967f00d7f6dbf8cf137380f487
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-08 22:47:34 +0900

    test(lotto): Lottos 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+
+    @DisplayName("로또의_개수_사이즈_확인_테스트")
+    @Test
+    void countLottoCount() {
+        Lottos lottos = LottoBundle.lottoBundle(5);
+        assertThat(lottos.getLottosSize()).isEqualTo(5);
+    }
+}
