5680cec95c80060e5190f0f4bae85a30ecdcb813
hochan222 <hochan049@gmail.com>
2022-02-09 00:06:14 +0900

    test: LottoTicketGenerator 생성으로 기능 분리
    - Lottos를 LottoTicket 과 LottoTicketGenerator로 분리하기

---
+++ src/test/java/lotto/domain/LottoTicketGeneratorTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoTicketGeneratorTest {
+
+    @DisplayName(value = "정수 자료형인 LottoCount 로 LottoTicket 객체를 생성할때, LottoTicket.size()는 LottoCount와 동일하다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 10, 20, 30})
+    void GivenLottoCountWithIntType_WhenGenerateLottoTicket_ThenLottoTicketSizeEqualsLottoCount(final int lottoCount) {
+        assertThat(LottoTicketGenerator.from(lottoCount).getLottoTicket().size()).isEqualTo(lottoCount);
+    }
+}
