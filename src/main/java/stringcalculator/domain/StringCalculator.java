0942f3bd97757aff734378485c099d70c66b12c4
Hoya-kim <1203play@gmail.com>
2022-02-08 17:24:09 +0900

    refactor: checkNullOrEmpty 메서드 삭제 및 add function내 통합

--- src/main/java/stringcalculator/domain/StringCalculator.java
+++ src/main/java/stringcalculator/domain/StringCalculator.java
@@ -4,26 +4,23 @@ import java.util.List;
 
 public class StringCalculator {
 
-    private static final String EMPTY_VALUE = "0";
+    private static final int EMPTY_VALUE = 0;
 
     public StringCalculator() {
 
     }
 
     public static int add(final String input) {
+        if (input == null || input.isEmpty()) {
+            return EMPTY_VALUE;
+        }
         List<Integer> numbers = preprocess(input);
         return numbers.stream().reduce(0, Operation.PLUS::operate);
     }
 
     private static List<Integer> preprocess(final String input) {
-        String expression = Delimiter.findExpression(checkNullOrEmpty(input));
+        String expression = Delimiter.findExpression(input);
         return Delimiter.extractNumberFromExpression(expression);
     }
 
-    private static String checkNullOrEmpty(String input) {
-        if (input == null || input.isEmpty()) {
-            return EMPTY_VALUE;
-        }
-        return input;
-    }
 }
