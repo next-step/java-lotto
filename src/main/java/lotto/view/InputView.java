c0664f1fe18f240c3874f9bab68530d980c90bd9
0JUUU <clleo97@naver.com>
2022-02-09 17:05:27 +0900

    feat: InputView 로또 수동 구매 개수를 받을 메서드 구현
    - inputManualCount()

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,6 +14,11 @@ public class InputView {
         return SCANNER.nextLine();
     }
 
+    public static String inputManualCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return SCANNER.nextLine();
+    }
+
     public static String inputLastWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return SCANNER.nextLine();
