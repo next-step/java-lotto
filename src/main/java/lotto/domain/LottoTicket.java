5158eca3ff4c57b8003ae967c266100357ff295b
Dami Kim <dami.k.dev@gmail.com>
2022-02-10 01:24:41 +0900

    style: 코드 포맷팅

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 public class LottoTicket {
+
     private static final int LOTTO_NUMBERS_SIZE = 6;
     private final List<Integer> numbers;
 
@@ -15,14 +16,14 @@ public class LottoTicket {
     }
 
     private void validateSize(List<Integer> numbers) {
-        if(numbers.size() != LOTTO_NUMBERS_SIZE) {
+        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 넘버는 6개여야 합니다.");
         }
     }
 
     private void validateDuplication(List<Integer> numbers) {
         Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
-        if(nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
+        if (nonDuplicateNumbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 넘버는 중복이 불가합니다.");
         }
     }
