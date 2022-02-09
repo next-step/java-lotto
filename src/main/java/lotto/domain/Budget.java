9e4da094fc62ab4fe076a02e3724e3c726246e7f
kimtaejun97 <kim_taejun@naver.com>
2022-02-09 18:18:31 +0900

    refactor: 로또의 구매가능 장수를 Budget에서 계산하도록 변경.
    - 변경에 따라 검증 로직 Budget으로 이동.
    - Budget의 getter가 이제 사용되지 않아 제거.

--- src/main/java/lotto/domain/Budget.java
+++ src/main/java/lotto/domain/Budget.java
@@ -3,6 +3,7 @@ package lotto.domain;
 public class Budget {
 
     private final static String NOT_A_NUMBER_EXCEPTION_MESSAGE = "숫자만 입력 가능합니다.";
+    private static final String LACK_OF_MONEY_EXCEPTION_MESSAGE = "로또 한장의 구입 가격보다 투입 금액이 적습니다.";
 
     private final int value;
 
@@ -15,8 +16,15 @@ public class Budget {
         }
     }
 
-    public int getValue() {
-        return value;
+    public int getNumberOfPurchase(int priceOfLotto) {
+        validatePossibleToPurchase(priceOfLotto);
+        return this.value / priceOfLotto;
+    }
+
+    private void validatePossibleToPurchase(final int priceOfLotto) {
+        if (canNotPurchase(priceOfLotto)) {
+            throw new IllegalArgumentException(LACK_OF_MONEY_EXCEPTION_MESSAGE);
+        }
     }
 
     public boolean canNotPurchase(int priceOfLotto) {
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -8,7 +8,6 @@ import lotto.LottoBalls;
 public class Lottos {
 
     private static final int PRICE_OF_LOTTO = 1000;
-    private static final String LACK_OF_MONEY_EXCEPTION_MESSAGE = "로또 한장의 구입 가격보다 투입 금액이 적습니다.";
 
     private final List<Lotto> lottos;
 
@@ -19,23 +18,12 @@ public class Lottos {
     public static Lottos createAutoLottos(final Budget budget) {
         final List<Lotto> lottos = new ArrayList<>();
 
-        final int numberOfLotto = getNumberOfLotto(budget);
+        final int numberOfLotto = budget.getNumberOfPurchase(PRICE_OF_LOTTO);
         for (int i = 0; i < numberOfLotto; i++) {
             LottoBalls.shuffle();
             lottos.add(new Lotto(createRandomNumbers()));
         }
         return new Lottos(lottos);
-    }
-
-    private static int getNumberOfLotto(final Budget budget) {
-        validatePossibleToPurchase(budget);
-        return budget.getValue() / PRICE_OF_LOTTO;
-    }
-
-    private static void validatePossibleToPurchase(Budget budget) {
-        if (budget.canNotPurchase(PRICE_OF_LOTTO)) {
-            throw new IllegalArgumentException(LACK_OF_MONEY_EXCEPTION_MESSAGE);
-        }
     }
 
     private static Set<LottoNumber> createRandomNumbers() {
