7b99894ac76e53f57197f7c2e4f360d97c282dbf
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-09 23:48:06 +0900

    feat: 수동 구매 기능 구현 위한 Controller -> purchase 메서드 변경
    - purchaseManualTickets
    - purchaseAutoTickets 호출 등
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,18 +3,20 @@ package lotto.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.domain.generator.LottoAutoGenerator;
 import lotto.domain.lotto.Answer;
+import lotto.domain.lotto.Numbers;
 import lotto.domain.lotto.PrizeRatio;
 import lotto.domain.lotto.Rank;
 import lotto.domain.lotto.Ticket;
+import lotto.domain.lotto.TicketMachine;
 import lotto.domain.lotto.Tickets;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoController {
-
-    private static final int PRICE_PER_ONE_TICKET = 1000;
 
     private static LottoController lottoController = null;
 
@@ -28,35 +30,53 @@ public class LottoController {
     }
 
     public void run() {
-        Tickets tickets;
-        int amount = InputView.getAmount();
-        int count = amount / PRICE_PER_ONE_TICKET;
+        TicketMachine machine = new TicketMachine(InputView.getAmount(), InputView.getManualLottoTickets());
+        Tickets tickets = purchase(machine.manualTickets(), machine.autoTickets());
 
-        tickets = purchase(count);
         showTickets(tickets);
-        showResult(tickets, amount);
+        showResult(tickets, machine.amount());
     }
 
-    private Tickets purchase(final int count) {
+    private Tickets purchase(final int manualTickets, final int autoTickets) {
+        List<Ticket> manualTicketList = purchaseManualTickets(manualTickets);
+        List<Ticket> autoTicketList = purchaseAutoTickets(autoTickets);
+
+        OutputView.printBuyingTickets(manualTickets, autoTickets);
+
+        return new Tickets(
+            Stream.concat(manualTicketList.stream(), autoTicketList.stream())
+                .collect(Collectors.toList())
+        );
+    }
+
+    private List<Ticket> purchaseManualTickets(final int manualTickets) {
         List<Ticket> tickets = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
+        List<Numbers> numbers = InputView.getManualLottoNumbers(manualTickets);
+
+        for (Numbers lotto: numbers) {
+            tickets.add(new Ticket(lotto));
+        }
+
+        return tickets;
+    }
+
+    private List<Ticket> purchaseAutoTickets(final int autoTickets) {
+        List<Ticket> tickets = new ArrayList<>();
+        for (int i = 0; i < autoTickets; i++) {
             Ticket ticket = new Ticket(new LottoAutoGenerator().generateNumbers());
             tickets.add(ticket);
         }
-        OutputView.printBuyingTickets(count);
 
-        return new Tickets(tickets);
+        return tickets;
     }
 
     private void showTickets(final Tickets tickets) {
-        tickets.get().stream()
-            .map(Ticket::getLotto)
-            .forEach(OutputView::printLottoTicket);
+        tickets.getEachTicketNumbers().forEach(OutputView::printLottoTicket);
     }
 
     private void showResult(final Tickets tickets, final int amount) {
         Answer answer = new Answer(InputView.getComparisonNumbers(), InputView.getBonus());
-        Map<Rank, Integer> prizeMap = answer.compare(tickets);
+        Map<Rank, Integer> prizeMap = answer.getComparisonPrizeMap(tickets);
 
         OutputView.printStatistics(prizeMap);
         OutputView.printResult(new PrizeRatio().calculateRatio(amount, prizeMap));
