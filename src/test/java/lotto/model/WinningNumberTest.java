8e9329e0853c2d3bd99233ddaedf562cdde4a40d
Jiin Lee <loveted0225@naver.com>
2022-02-10 02:54:05 +0900

    refactor(WinningNumberTest): WinningNumber 테스트코드 수정

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -4,30 +4,28 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
 import java.util.List;
-import lotto.model.LottoRules;
-import lotto.model.WinningNumber;
 import org.junit.jupiter.api.Test;
 
 class WinningNumberTest {
 
     @Test
     void 중복된_숫자가_들어오면_예외를_발생하는지_확인() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
+        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "5");
         assertThatExceptionOfType(RuntimeException.class)
-          .isThrownBy(() -> new WinningNumber(numbers, new LottoRules()));
+          .isThrownBy(() -> new WinningNumber(numbers, 9));
     }
 
     @Test
     void 다섯개의_숫자가_들어오면_예외를_발생하는지_확인() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5");
         assertThatExceptionOfType(RuntimeException.class)
-          .isThrownBy(() -> new WinningNumber(numbers, new LottoRules()));
+          .isThrownBy(() -> new WinningNumber(numbers,9));
     }
 
     @Test
     void 일곱개의_숫자가_들어오면_예외를_발생하는지_확인() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6","7");
         assertThatExceptionOfType(RuntimeException.class)
-          .isThrownBy(() -> new WinningNumber(numbers, new LottoRules()));
+          .isThrownBy(() -> new WinningNumber(numbers,9));
     }
 }
