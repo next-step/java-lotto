c569f7287bf8a1e3fda0b4a261126a56d8151588
PandaHun <gnsgnsek@gmail.com>
2022-02-09 22:04:25 +0900

    feat(LottoCount): Add minus with other LottoCount feature
    - 다른 로또 개수와의 차이 구하는 기능 구현

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -5,6 +5,7 @@ public class LottoCount {
     private static final int LOTTO_COUNT_MINIMUM_VALUE = 0;
     private static final String LOTTO_COUNT_NOT_NUMBER_MESSAGE = "[ERROR] 로또 매수는 숫자만 가능합니다.";
     private static final String LOTTO_COUNT_VALIDATE_MESSAGE = "[ERROR] 로또 매수는 0보다 커야 합니다.";
+    private static final String MINUS_EXCEPTION_MESSAGE = "[ERROR] 더 큰 수로 뺄 수 없습니다.";
 
     private final int lottoCount;
 
@@ -22,13 +23,16 @@ public class LottoCount {
         validateLottoCount(parsedLottoCount);
         return new LottoCount(parsedLottoCount);
     }
+    
+    public LottoCount minus(LottoCount otherCount) {
+        if (this.lottoCount < otherCount.lottoCount) {
+            throw new IllegalArgumentException(MINUS_EXCEPTION_MESSAGE);
+        }
+        return new LottoCount(this.lottoCount - otherCount.lottoCount);
+    }
 
     public int count() {
         return lottoCount;
-    }
-
-    public LottoCount minus(LottoCount manualCount) {
-        return null;
     }
 
     private static int parseLottoCount(String lottoCount) {
