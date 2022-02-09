6921722f855ec48b0167588ca5af08c6e9aec049
SeungYeonJeong <crowsy@naver.com>
2022-02-09 18:06:25 +0900

    refactor: Restore code
    - 삭제했던 예외처리 코드 복원

--- src/main/java/stringcaculator/util/StringParser.java
+++ src/main/java/stringcaculator/util/StringParser.java
@@ -7,6 +7,7 @@ public class StringParser {
 
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER = "//(.)\\n(.*)";
+    private static final String PATTERN_NOT_FOUND = "[ERROR] 커스텀 문자열을 찾을 수 없습니다.";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
     private static final int DELIMITER_POSITION = 1;
     private static final int REFINE_POSITION = 2;
@@ -19,14 +20,11 @@ public class StringParser {
 
     public static String[] splitCustomDelimiter(String numbers) {
         Matcher matcher = CUSTOM_PATTERN.matcher(numbers);
-        String customDelimiter = DEFAULT_DELIMITER;
-        String refinedExpression = numbers;
-
-        if (matcher.find()) {
-            customDelimiter = matcher.group(DELIMITER_POSITION);
-            refinedExpression = matcher.group(REFINE_POSITION);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(PATTERN_NOT_FOUND);
         }
-
+        String customDelimiter = matcher.group(DELIMITER_POSITION);
+        String refinedExpression = matcher.group(REFINE_POSITION);
         return refinedExpression.split(customDelimiter);
     }
 }
