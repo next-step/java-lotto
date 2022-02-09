7a5341e4bdb0a59c57067c72b79ba2c5f550c2ed
PandaHun <gnsgnsek@gmail.com>
2022-02-09 21:35:00 +0900

    feat(LottoCount): Add static factory method using string args with validation
    - 문자열 매개변수를 갖는 정적 팩터리 메서드 구현
    - 유효성 검증 작성

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -2,6 +2,10 @@ package lotto.domain;
 
 public class LottoCount {
 
+    private static final int LOTTO_COUNT_MINIMUM_VALUE = 0;
+    private static final String LOTTO_COUNT_NOT_NUMBER_MESSAGE = "[ERROR] 로또 매수는 숫자만 가능합니다.";
+    private static final String LOTTO_COUNT_VALIDATE_MESSAGE = "[ERROR] 로또 매수는 0보다 커야 합니다.";
+
     private final int lottoCount;
 
     private LottoCount(int count) {
@@ -14,10 +18,27 @@ public class LottoCount {
     }
 
     public static LottoCount from(String lottoCount) {
-        return null;
+        int parsedLottoCount = parseLottoCount(lottoCount);
+        validateLottoCount(parsedLottoCount);
+        return new LottoCount(parsedLottoCount);
     }
 
     public int count() {
         return lottoCount;
     }
+
+    private static int parseLottoCount(String lottoCount) {
+        try {
+            return Integer.parseInt(lottoCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LOTTO_COUNT_NOT_NUMBER_MESSAGE);
+        }
+    }
+
+    private static void validateLottoCount(int lottoCount) {
+        if (lottoCount <= LOTTO_COUNT_MINIMUM_VALUE) {
+            throw new IllegalArgumentException(LOTTO_COUNT_VALIDATE_MESSAGE);
+        }
+    }
+
 }
