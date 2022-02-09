cc39741ca06959fb1821b0d54ee1ae63f6e603fd
Dami Kim <dami.k.dev@gmail.com>
2022-02-09 17:12:46 +0900

    refactor: 변수명, package 수정 반영 - lotto.domain.LottoTicket - lotto.domain.WinningNumber - lotto.domain.WinningResult

--- src/main/java/lotto/service/LottoMachine.java
+++ src/main/java/lotto/service/LottoMachine.java
@@ -1,16 +1,17 @@
 package lotto.service;
 
 import java.util.List;
-import lotto.domain.Lotto;
+import lotto.domain.LottoTicket;
 import lotto.domain.LottoResult;
-import lotto.domain.WinningBall;
+import lotto.domain.WinningNumber;
+import lotto.domain.WinningResult;
 
 public class LottoMachine {
 
     private static List<LottoResult> results;
 
-    public static void run(List<Lotto> lottoNumbers, List<WinningBall> winningBalls) {
-        WinningCounter winningCounter = new WinningCounter(lottoNumbers, winningBalls);
+    public static void run(List<LottoTicket> lottoNumbers, List<WinningNumber> winningBalls) {
+        WinningResult winningCounter = new WinningResult(lottoNumbers, winningBalls);
         results = winningCounter.getTotalLottoResult();
     }
 
