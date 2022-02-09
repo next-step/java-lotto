dcc9ee41860119c2ea225fa3e4968510b5799b1e
0JUUU <clleo97@naver.com>
2022-02-09 21:27:16 +0900

    feat: 로또 게임 관련 메서드 추가
    1. generateLottos에 파라미터 추가 : 생성 방식 주입을 위함
    2. makeManualLottos 생성 : 수동 방식으로 로또 생성
    3. generateTotalLottos 생성 : 자동과 수동을 합쳐주는 메서드

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.domain.LottoRaffle;
 import lotto.domain.LottoResult;
 import lotto.domain.lotto.Lotto;
@@ -8,6 +11,8 @@ import lotto.domain.lotto.Lottos;
 import lotto.domain.lotto.WinningLotto;
 import lotto.domain.lotto.count.Count;
 import lotto.domain.lotto.count.ManualCount;
+import lotto.domain.lotto.generator.Generator;
+import lotto.domain.lotto.generator.ManualGenerator;
 import lotto.domain.lotto.number.BonusNumber;
 import lotto.domain.lotto.number.Numbers;
 import lotto.domain.money.Money;
@@ -18,8 +23,8 @@ public class LottoGame {
         return new Count(money.getValue() / 1000);
     }
 
-    public Lottos generateLottos(int lottoCount) {
-        return new Lottos(lottoCount);
+    public Lottos generateLottos(final int lottoCount, final Generator generator) {
+        return new Lottos(lottoCount, generator);
     }
 
     public Map<LottoResult, Integer> getResults(Lottos lottos, Numbers winningNumbers, BonusNumber bonusNumber) {
@@ -32,5 +37,15 @@ public class LottoGame {
 
     public Count calculateAutoCount(Count totalCount, ManualCount manualCount) {
         return new Count(totalCount.getValue() - manualCount.getCountValue());
+    }
+
+    public Lottos makeManualLottos(ManualCount manualCount, List<Numbers> manualNumbers) {
+        return new Lottos(manualCount.getCountValue(), new ManualGenerator(manualNumbers));
+    }
+
+    public Lottos generateTotalLottos(Lottos manualLottos, Lottos autoLottos) {
+        List<Lotto> lottos = Stream.concat(manualLottos.getLottos().stream(), autoLottos.getLottos().stream()).collect(
+            Collectors.toList());
+        return new Lottos(lottos);
     }
 }
