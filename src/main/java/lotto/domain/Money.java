5306d642dfff6f3d29a788006139e128794bfb29
lhh5863 <lhh5863@dgu.ac.kr>
2022-02-09 21:31:20 +0900

    refactor(lotto): Money 객체 유효성 판단 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,9 +2,12 @@ package lotto.domain;
 
 public class Money {
 
+    private final static int MIN_MONEY = 1000;
+    private final static String ERROR_MESSAGE = "구입 금액은 1000원 이상이어야 합니다.";
     private final int value;
 
     public Money(final int value) {
+        validateMoney(value);
         this.value = value;
     }
 
@@ -13,5 +16,11 @@ public class Money {
     }
     public Money multiple(int hit){
         return new Money(value * hit);
+    }
+
+    private void validateMoney(final int value){
+        if (value < MIN_MONEY) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
 }
