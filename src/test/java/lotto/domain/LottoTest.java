fd0568012dc851bd444ac2b0dff7771f4b295357
gal2o <hunmin8788@gmail.com>
2022-02-09 23:09:53 +0900

    refactor(lotto): Lotto 테스트 수정 및 패키지 이동

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,21 +1,18 @@
-package lotto;
+package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 class LottoTest {
 
-    @Test
     @RepeatedTest(100)
     void 랜덤_숫자를_잘_획득하는지_확인() {
-        Lotto lotto = new Lotto(new LottoRules());
-        List<Integer> numbers = lotto.getNumbers();
-        for (Integer number : numbers) {
+        Lotto lotto = new Lotto();
+        lotto.getLottoTicket();
+
+        for (Integer number : lotto.getLotto()) {
             assertThat(number).isBetween(1, 45);
         }
-
     }
 }
