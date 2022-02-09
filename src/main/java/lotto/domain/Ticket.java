f416f33da6b0474711c77d90ae4025d557e873be
xxell-8 <cell8pepper@gmail.com>
2022-02-09 17:00:10 +0900

    refactor: 입력 금액 유효성 검사하는 부분 분리

--- src/main/java/lotto/domain/Ticket.java
+++ src/main/java/lotto/domain/Ticket.java
@@ -2,16 +2,27 @@ package lotto.domain;
 
 public class Ticket {
 
-    private static final String TICKET_ERROR_MESSAGE = "최소 구매 금액은 1,000원입니다.";
+    private static final String ERROR_CANNOT_BUY = "[ERROR] 최소 구매 금액은 1,000원입니다.";
+    private static final String ERROR_WRONG_UNIT = "[ERROR] 로또 구매는 1,000원 단위로 가능합니다.";
     private static final int TICKET_PRICE = 1000;
 
-    private int buyCount;
+    private final int buyCount;
+    private final int money;
 
     public Ticket(int money) {
-        if (money < TICKET_PRICE) {
-            throw new IllegalArgumentException(TICKET_ERROR_MESSAGE);
-        }
+        this.money = money;
+        checkMoney();
         this.buyCount = money / TICKET_PRICE;
+    }
+
+    public void checkMoney() {
+        if (money < TICKET_PRICE) {
+            throw new IllegalArgumentException(ERROR_CANNOT_BUY);
+        }
+
+        if (money % TICKET_PRICE > 0) {
+            throw new IllegalArgumentException(ERROR_WRONG_UNIT);
+        }
     }
 
     public int getBuyCash() {
