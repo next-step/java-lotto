6512acdd17d6018bccc3a1a11def6f4ffe26d469
PandaHun <gnsgnsek@gmail.com>
2022-02-09 23:09:08 +0900

    feat(ManualLottoGenerator): Create manual generate Lotto feature
    - 수동 로또 발행 기능 구현

--- src/main/java/lotto/domain/lottogenerator/ManualLottoGenerator.java
+++ src/main/java/lotto/domain/lottogenerator/ManualLottoGenerator.java
@@ -1,14 +1,24 @@
 package lotto.domain.lottogenerator;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ManualLottoGenerator implements LottoGenerator {
 
+    private static final String DELIMITER = ",";
+
+    private final Set<Integer> lottoNumber;
+
     public ManualLottoGenerator(String manualLotto) {
+        this.lottoNumber = Arrays.stream(manualLotto.split(DELIMITER))
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(Collectors.toSet());
     }
 
     @Override
     public Set<Integer> generateLotto() {
-        return null;
+        return lottoNumber;
     }
 }
