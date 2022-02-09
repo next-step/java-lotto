33c0d5ae44fb31d520044dfa2e25e1eec37ea6c4
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-09 23:46:57 +0900

    refactor: Answer의 compare 로직 Ticket으로 위임 등
    - compare 로직 권한 위임 (Ticket)
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

--- src/main/java/lotto/domain/lotto/Tickets.java
+++ src/main/java/lotto/domain/lotto/Tickets.java
@@ -1,7 +1,9 @@
 package lotto.domain.lotto;
 
-import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Tickets {
 
@@ -11,12 +13,25 @@ public class Tickets {
         this.tickets = tickets;
     }
 
-    public List<Ticket> get() {
-        return Collections.unmodifiableList(tickets);
-    }
-
     public int count() {
         return tickets.size();
     }
 
+    public List<Numbers> getEachTicketNumbers() {
+        return tickets.stream()
+            .map(Ticket::getNumbers)
+            .collect(Collectors.toList());
+    }
+
+    public Map<Rank, Integer> getComparisonPrizeMap(Numbers answerNumbers, int bonus) {
+        Map<Rank, Integer> prizeMap = new LinkedHashMap<>();
+        for (Ticket ticket : tickets) {
+            int matches = ticket.matches(answerNumbers.get());
+            Rank rank = Rank.getRank(matches, ticket.hasBonus(bonus));
+
+            prizeMap.put(rank, prizeMap.getOrDefault(rank, 0) + 1);
+        }
+
+        return prizeMap;
+    }
 }
