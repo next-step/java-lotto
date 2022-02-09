72e42fff1d48af7250739591098c635fb0f1baaa
Jiin Lee <loveted0225@naver.com>
2022-02-10 02:45:56 +0900

    feat(Count): 개수를 관리하는 객체 생성

---
+++ src/main/java/lotto/model/Count.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+public class Count {
+    private final int count;
+
+    public Count(int count){
+        this.count=count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
