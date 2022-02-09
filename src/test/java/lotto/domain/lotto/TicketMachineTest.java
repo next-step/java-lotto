1fb65c0b5d75cccb5a2e188d8f240d4a18bd5951
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-09 23:45:17 +0900

    test: TicketMachineTest
    - 구매금액 단위 유효성
    - 자동/수동 구매장수 유효성
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/test/java/lotto/domain/lotto/TicketMachineTest.java
@@ -0,0 +1,46 @@
+package lotto.domain.lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TicketMachineTest {
+
+    static Stream<Arguments> invalidManualCases() {
+        return Stream.of(
+            Arguments.of(14000, -5),
+            Arguments.of(14000, 15)
+        );
+    }
+
+    @DisplayName("총 구매금액이 주어졌을 때, 1000원 단위가 아닌 유효하지 않은 경우에 예외발생")
+    @Test
+    void invalidAmountCases() {
+        assertThatThrownBy(() -> new TicketMachine(14500, 3))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("총 구매금액과 수동 티켓 수가 유효하게 주어졌을 때, 수동 티켓 수가 음수이거나 총 구매금액을 초과하면 예외발생")
+    @ParameterizedTest
+    @MethodSource("invalidManualCases")
+    void invalidManualTicketCases(final int amount, final int manualTickets) {
+        assertThatThrownBy(() -> new TicketMachine(amount, manualTickets))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("총 구매금액과 수동 티켓 수가 유효하게 주어졌을 때, 수동 티켓 수와 자동 티켓 수가 올바르게 반환")
+    @Test
+    void testEachTicketsValid() {
+        TicketMachine machine = new TicketMachine(14000, 3);
+
+        assertThat(machine.autoTickets()).isEqualTo(11);
+        assertThat(machine.manualTickets()).isEqualTo(3);
+    }
+
+}
