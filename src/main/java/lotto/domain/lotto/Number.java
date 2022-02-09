727a83f4f6c5845b90d9009de6e8267a873baefc
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-09 23:41:55 +0900

    feat: 원시값 포장 (Number)
    - 각 로또 넘버를 Number로 포장
    - 검증 로직 추가
    
    Co-authored-by: fpal95 <snc7303@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/main/java/lotto/domain/lotto/Number.java
@@ -0,0 +1,43 @@
+package lotto.domain.lotto;
+
+import java.util.Objects;
+import lotto.domain.generator.LottoGenerator;
+
+public class Number {
+
+    private static final String EXCEPTION_NOT_IN_RANGE = "숫자가 범위 내에 있지 않습니다.";
+
+    private final int number;
+
+    public Number(int number) {
+        isValid(number);
+        this.number = number;
+    }
+
+    public int value() {
+        return number;
+    }
+
+    private void isValid(int number) {
+        if (number < LottoGenerator.START_NUMBER || number > LottoGenerator.END_NUMBER) {
+            throw new IllegalArgumentException(EXCEPTION_NOT_IN_RANGE);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
