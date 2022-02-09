6a5a4605eb6a13b774f6e0f9869215d16854e97a
PandaHun <gnsgnsek@gmail.com>
2022-02-09 20:55:13 +0900

    test(LottoCount): modify valuesource to csvsoure using input value

--- src/test/java/lotto/domain/LottoCountTest.java
+++ src/test/java/lotto/domain/LottoCountTest.java
@@ -1,18 +1,17 @@
 package lotto.domain;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoCountTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1_000, 14_000, 999_000})
-    void 금액에_따른_로또_개수를_출력한다(int value) {
+    @CsvSource(value = {"1000, 1", "14000, 14", "999000, 999"})
+    void 금액에_따른_로또_개수를_출력한다(int value, int count) {
         Money money = new Money(String.valueOf(value));
         LottoCount lottoCount = LottoCount.from(money);
-        assertThat(lottoCount.count()).isEqualTo(value / 1000);
+        assertThat(lottoCount.count()).isEqualTo(count);
     }
 }
