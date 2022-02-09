1dfbbbde629201f8ae39c0306b2b22329d9880f4
xxell-8 <cell8pepper@gmail.com>
2022-02-09 17:51:53 +0900

    refactor: Lotto 객체들을 가진 Lottos 클래스로 변경

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos(LottoGenerator lottoGenerator, final int buyCount) {
+        for (int i = 0; i < buyCount; i++) {
+            lottos.add(lottoGenerator.get());
+        }
+    }
+
+    public List<Lotto> get() {
+        return this.lottos;
+    }
+}
