130ccd7339690576fc2760f8e41e83224e856967
xxell-8 <cell8pepper@gmail.com>
2022-02-09 17:54:32 +0900

    feat: Getter 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -22,6 +22,10 @@ public class LottoNumber {
         }
     }
 
+    public int get() {
+        return this.number;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
