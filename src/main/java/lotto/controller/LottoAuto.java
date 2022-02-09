c22d0b3f63580ae7c0ead931668edbddd7642d8a
Jiin Lee <loveted0225@naver.com>
2022-02-10 02:54:45 +0900

    refactor(LottoAuto): 모델 객체들의 리팩토링으로 인한 컨트롤러 코드 수정

--- src/main/java/lotto/controller/LottoAuto.java
+++ src/main/java/lotto/controller/LottoAuto.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import lotto.model.LottoRules;
+import lotto.model.Price;
 import lotto.model.User;
 import lotto.model.WinningNumber;
 import lotto.view.InputView;
@@ -18,8 +19,7 @@ public class LottoAuto {
         User user = new User(OutputView.getLottoCount(InputView.getPrice() / lottoRules.getLottoPrice()),
             lottoRules);
         OutputView.printLottos(user.getLottos());
-        WinningNumber winningNumber = new WinningNumber(InputView.getWinningNumber(), lottoRules);
-        winningNumber.setBonusBall(InputView.getBonusBall());
+        WinningNumber winningNumber = new WinningNumber(InputView.inputWinningNumbers(),InputView.inputBonusBall());
         user.setRanks(winningNumber);
         OutputView.printResult(user.getRanks(), user.getCount() * lottoRules.getLottoPrice());
     }
