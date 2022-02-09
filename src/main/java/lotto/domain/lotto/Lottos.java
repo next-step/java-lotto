d4b6605da93a94ff280b392506dddb37d1c398b5
0JUUU <clleo97@naver.com>
2022-02-09 21:24:27 +0900

    feat: 로또 생성 관련 메서드 변경
    - setLottos 내부 로직 변경 (생성 방식 주입)
    - setLotto 제거

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -2,8 +2,7 @@ package lotto.domain.lotto;
 
 import java.util.ArrayList;
 import java.util.List;
-import lotto.domain.GeneratorNumber;
-import lotto.domain.lotto.number.Numbers;
+import lotto.domain.lotto.generator.Generator;
 
 public class Lottos {
 
@@ -17,19 +16,8 @@ public class Lottos {
         this.lottos = setLottos(lottoCount, generator);
     }
 
-    private List<Lotto> setLottos(int lottoCount) {
-        final List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            Lotto lotto = setLotto();
-            lottos.add(lotto);
-        }
-        return lottos;
-    }
-
-    // TODO : 난수를 생성하는 로직과 Lotto 인스턴스를 생성하는 로직이 한 곳에 있으므로 테스트가 어려움
-    private Lotto setLotto() {
-        final Numbers lottoNumbers = GeneratorNumber.generateLottoNumbers();
-        return new Lotto(new Numbers(lottoNumbers));
+    private List<Lotto> setLottos(final int lottoCount, final Generator generator) {
+        return generator.generateLottos(lottoCount);
     }
 
     public List<Lotto> getLottos() {
