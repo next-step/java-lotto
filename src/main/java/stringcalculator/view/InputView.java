4c00e7bfc92b519b71286212ced3594e809ab047
parkhyojeong <qkrgywjd456@naver.com>
2022-02-09 16:51:02 +0900

    feat(inputView):scanner을 통해 값을 입력받아Line 을 생성하도록 변경

--- src/main/java/stringcalculator/view/InputView.java
+++ src/main/java/stringcalculator/view/InputView.java
@@ -1,12 +1,15 @@
 package stringcalculator.view;
 
 import java.util.Scanner;
+import stringcalculator.domain.Line;
 
 public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public String inputCalculateString() {
-        return scanner.nextLine();
+    //TODO 처음에 숫자가 아닐경우 터지게
+    public Line inputCalculateString() {
+
+        return new Line(scanner.nextLine());
     }
 }
