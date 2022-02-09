1a69bd611a18f049ef4059e84283ab7f477235d8
Jiin Lee <loveted0225@naver.com>
2022-02-10 02:53:28 +0900

    refactor(WinningNumber): 당첨번호 유효성 검증 역할 해당 객체에 위임

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,28 +1,41 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumber {
 
-    private final List<Integer> winningNumber;
+    private static final int WINNING_NUMBERS_FIXED_AMOUNT=6;
+    private static final String WINNING_NUMBERS_ERROR_MESSAGE="당첨 번호는 6개입니다.";
+    private static final String DUPLICATE_WINNING_NUMBERS_MESSAGE="중복된 당첨번호입니다.";
+
+    private final List<Integer> winningNumbers;
     private int bonusBall;
 
-    public WinningNumber(List<Integer> winningNumber, LottoRules lottoRules) {
-        WinningNumberValidator winningNumberValidator = new WinningNumberValidator(lottoRules);
-        winningNumberValidator.validateWinningNumber(winningNumber);
-        this.winningNumber = winningNumber;
+    public WinningNumber(List<String> winningNumbersInput, int bonusBall) {
+        winningNumbers= new ArrayList<>();
+        validateWinningNumbers(winningNumbersInput);
+        parseStringToInt(winningNumbersInput);
+        this.bonusBall=bonusBall;
     }
-
+    private void validateWinningNumbers(List<String> winningNumbersInput){
+        if(winningNumbers.size()!=WINNING_NUMBERS_FIXED_AMOUNT){
+            throw new IllegalArgumentException(WINNING_NUMBERS_ERROR_MESSAGE);
+        }
+    }
+    private void checkDuplicateWinningNumber(int winningNumber){
+        if(winningNumbers.contains(winningNumber)){
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBERS_MESSAGE);
+        }
+    }
+    private void parseStringToInt(List<String> winningNumbersInput) {
+        for (int i = 0; i < winningNumbersInput.size(); i++) {
+            checkDuplicateWinningNumber(Integer.parseInt(winningNumbersInput.get(i)));
+            winningNumbers.add(Integer.parseInt(winningNumbersInput.get(i)));
+        }
+    }
     public List<Integer> getWinningNumber() {
-        return winningNumber;
-    }
-
-    public int getBonusBall() {
-        return bonusBall;
-    }
-
-    public void setBonusBall(int bonusBall) {
-        this.bonusBall = bonusBall;
+        return winningNumbers;
     }
 
 }
