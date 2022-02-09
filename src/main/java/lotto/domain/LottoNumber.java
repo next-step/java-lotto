b8ed1f02bcfcf262a4a5cafdbcfb0ae31874b3ce
Dami Kim <dami.k.dev@gmail.com>
2022-02-09 18:02:53 +0900

    refactor: LottoNumber 멤버 변수명 변경(MIN_NUMBER, MAX_NUMBER)

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,9 +2,9 @@ package lotto.domain;
 
 public class LottoNumber {
 
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private int number;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private final int number;
 
     public LottoNumber(int number) {
         validateNumberRange(number);
@@ -12,8 +12,9 @@ public class LottoNumber {
     }
 
     private void validateNumberRange(int number) {
-        if (number > MAX_LOTTO_NUMBER | number < MIN_LOTTO_NUMBER) {
+        if (number > MAX_NUMBER | number < MIN_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 1에서 45까지의 숫자만 사용가능합니다.");
         }
     }
+
 }
