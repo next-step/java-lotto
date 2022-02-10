edfa58e8d119f48747f5982facf5702073bb146a
jihyun park <girl9505@naver.com>
2022-02-11 02:58:44 +0900

    refactor: 통계를 관리하는 클래스 정의
    - Map<LottoRank, Integer>타입의 멤버변수만 가지는 일급 컬렉션

--- src/main/java/lotto/domain/LottoStatistic.java
+++ src/main/java/lotto/domain/LottoStatistic.java
@@ -1,2 +1,46 @@
-package lotto.domain;public class LottoStatistic {
+package lotto.domain;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class LottoStatistic {
+
+    private final Map<LottoRank, Integer> lottoRankStatistic;
+
+    public LottoStatistic(Map<LottoRank, Integer> lottoRankStatistic) {
+        this.lottoRankStatistic = lottoRankStatistic;
+    }
+
+    public BigDecimal getProfit(long principal) {
+        BigDecimal total = BigDecimal.ZERO;
+        for (Map.Entry<LottoRank, Integer> element : lottoRankStatistic.entrySet()) {
+            total = total.add(
+                BigDecimal.valueOf(
+                    element.getKey().getWinningAmount() * (long) element.getValue()));
+        }
+        return total.divide(BigDecimal.valueOf(principal), 2, RoundingMode.DOWN);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        Stream.of(LottoRank.values())
+            .forEach(rank -> {
+                    int rankValue = 0;
+                    if (lottoRankStatistic.containsKey(rank)) {
+                        rankValue = lottoRankStatistic.get(rank);
+                    }
+                    sb.append(rank.getRankString())
+                        .append(" (")
+                        .append(rank.getWinningAmount())
+                        .append(") - ")
+                        .append(rankValue)
+                        .append("개");
+                }
+            );
+        return sb.toString();
+    }
 }
