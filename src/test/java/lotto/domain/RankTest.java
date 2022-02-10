4adafc57fc3fa6c2705dfd839d1a0337e957441c
xxell-8 <cell8pepper@gmail.com>
2022-02-11 01:48:32 +0900

    style: 마지막 줄 엔터 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,7 +1,8 @@
-package lotto.domain.lotto;
+package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lotto.domain.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
