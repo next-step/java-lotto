94d66cffe5f5320ce2b9f348ddf81a53c24d9e93
Jiin Lee <loveted0225@naver.com>
2022-02-10 20:43:40 +0900

    refactor(InputView): 가격 유효성 검증 Price 객체로 위임

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,6 @@ package lotto.view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import lotto.model.Parser;
 
 public class InputView {
 
@@ -11,27 +10,10 @@ public class InputView {
     private static final String PRICE_INPUT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING__NUMBER_INPUT_GUIDE_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_BALL_INPUT_GUIDE_MESSAGE = "보너스 볼을 입력해 주세요.";
-    private static final String NEED_OVER_1000 = "1000원 이상의 금액을 입력해주세요.";
-    private static final String NEED_INTEGER = "정수를 입력해주세요.";
-    private static final int PRICE_OF_A_LOTTO = 1000;
 
     public static int inputPrice() {
         System.out.println(PRICE_INPUT_GUIDE_MESSAGE);
         return SCANNER.nextInt();
-    }
-
-    private static void validatePrice(int price) {
-        if (price < PRICE_OF_A_LOTTO) {
-            throw new RuntimeException(NEED_OVER_1000);
-        }
-    }
-
-    private static void validateIsNumber(String priceString) {
-        try {
-            Integer.parseInt(priceString);
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException(NEED_INTEGER);
-        }
     }
 
     public static List<String> inputWinningNumbers() {
