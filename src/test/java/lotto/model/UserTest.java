f4b502207b5662aafeb9bbf5ea5d574153412bd2
Jiin Lee <loveted0225@naver.com>
2022-02-10 21:11:51 +0900

    test(UserTest): 각 로또가 당첨번호와 일치하는 개수 판단 테스트

--- src/test/java/lotto/model/UserTest.java
+++ src/test/java/lotto/model/UserTest.java
@@ -1,6 +1,5 @@
 package lotto.model;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
@@ -21,5 +20,23 @@ class UserTest {
         User user = new User(lottos);
 
         assertThat(user.getLottos()).hasSize(2);
+    }
+
+    @DisplayName("유저는 자신이 산 복권에서 당첨번호가 몇개인지 판단한다")
+    @Test
+    void matchWinningNumberWithLottos() {
+        List<Lotto> lottos = Arrays.asList(
+            new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+            new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10))
+        );
+
+        User user = new User(lottos);
+        WinningNumber winningNumber = new WinningNumber(
+            Arrays.asList("1", "3", "4", "5", "6", "21"), 7);
+        user.findEachLottoMatchingNumber(winningNumber);
+        Lotto lotto1 = user.getLottos().get(0);
+        Lotto lotto2 = user.getLottos().get(1);
+        assertThat(lotto1.getMatchCount()).isEqualTo(5);
+        assertThat(lotto2.getMatchCount()).isEqualTo(4);
     }
 }
