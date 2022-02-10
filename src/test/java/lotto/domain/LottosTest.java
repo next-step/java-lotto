5fdb23ac9eb87fd2d6e1a0839c3fed7d09be81a4
kimtaejun97 <kim_taejun@naver.com>
2022-02-10 18:45:24 +0900

    test: 수동 로또 발급 기능 추가.
    - 로또 번호 리스트와 예산을 받는다.
    - 리스트의 사이즈 * 로또 가격 만큼 예산 차감.

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,8 +1,11 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -21,6 +24,24 @@ class LottosTest {
         final Lottos autoLottos = Lottos.createAutoLottos(new Budget(budget));
 
         // then
-        assertThat(autoLottos.getNumberOfPurchases()).isEqualTo(numberOfLotto);
+        assertThat(autoLottos.size()).isEqualTo(numberOfLotto);
+    }
+
+    @DisplayName("수동 로또를 생성한다.")
+    @Test
+    void Given_로또_번호_리스트_AND_예산_When_수동로또_생성_Then_로또_번호의_수_만큼_생성() {
+        // given & when
+        Lottos manualLottos = Lottos.createManualLottos(
+                Arrays.asList("1,2,3,4,5,6", "11,12,13,14,15,16"), new Budget("5000"));
+
+        // then
+        assertThat(manualLottos.size()).isEqualTo(2);
+    }
+
+    @DisplayName("수동 로또를 생성할 때 예산이 부족하면 예외가 발생한다.")
+    @Test
+    void Given_로또_번호_리스트_AND_부족한_예산_When_수동로또_생성_Then_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> Lottos.createManualLottos(
+                Arrays.asList("1,2,3,4,5,6"), new Budget("100")));
     }
 }
