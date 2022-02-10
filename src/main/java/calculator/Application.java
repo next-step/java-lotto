3e075c6cbc4cc3d24b4d4c03e7b3912d8834f3ac
xxell-8 <cell8pepper@gmail.com>
2022-02-11 01:12:13 +0900

    refactor: 결과 출력 구문 컨트롤러로 이동

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,7 +8,5 @@ public class Application {
     public static void main(String[] args) {
         CalculatorController controller = new CalculatorController();
         controller.start();
-
-        OutputView.printResult(controller.getResultValue());
     }
 }
