802511faf1b695c6aca8b829ebc994ce5d248950
YangWoo Yu <yuyw0712@naver.com>
2022-02-10 17:44:39 +0900

    refactor: 클린 코드 수직 적용 1. 연관이 있는 메서드의 위치를 비슷하게 정렬
    Co-authored-by: hochan222 hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -12,26 +12,27 @@ public class StringCalculator {
         this.input = validateEmptyOrNull(input);
     }
 
-    public int add() {
-        Parser parser = new Parser(input);
-        String splitedFormular = parser.getParsedFormular();
-
-        if (!Objects.isNull(parser.getCustomDelimiter())) {
-            String delimeter = parser.getCustomDelimiter();
-            return Parser.makeToIntegerList(splitedFormular, delimeter + "|,|:").stream()
-                .mapToInt(Integer::intValue)
-                .sum();
-        }
-
-        return Parser.makeToIntegerList(input, ",|:").stream()
-            .mapToInt(Integer::intValue)
-            .sum();
-    }
-
     private String validateEmptyOrNull(String input) {
         if (Objects.isNull(input) || "".equals(input)) {
             input = RETURN_NUMBER_ZERO;
         }
         return input;
     }
+
+    public int add() {
+        Parser parser = new Parser(input);
+        String splitedFormular = parser.getParsedFormular();
+
+        if (!Objects.isNull(parser.getCustomDelimiter())) {
+            String delimeter = parser.getCustomDelimiter();
+            return parser.makeToIntegerList(splitedFormular, delimeter + "|,|:").stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        }
+
+        return parser.makeToIntegerList(input, ",|:").stream()
+            .mapToInt(Integer::intValue)
+            .sum();
+    }
+
 }
