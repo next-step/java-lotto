951977c2c5dc73b9fa36500761cc94eba7dccb71
Jiin Lee <loveted0225@naver.com>
2022-02-10 20:55:22 +0900

    refactor(User): 불필요한 getter, setter 삭제

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,30 +1,25 @@
 package lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class User {
 
     private final List<Lotto> lottos;
 
     public User(List<Lotto> lottos) {
-        this.lottos = lottos;
+        this.lottos = new ArrayList<>(lottos);
+    }
+
+    public List<Lotto> findEachLottoMatchingNumber(WinningNumber winningNumber) {
+        for (Lotto lotto : lottos) {
+            lotto.matchLottoWithWinningNumber(winningNumber);
+        }
+        return lottos;
     }
 
     public List<Lotto> getLottos() {
         return this.lottos;
-    }
-
-    public Map<Rank, Integer> getRanks() {
-        return Statistics.getRanks();
-    }
-
-    public void setRanks(WinningNumber winningNumber) {
-        Statistics.setRanks(winningNumber, lottos);
-    }
-
-    public int getCount() {
-        return lottos.size();
     }
 
 }
