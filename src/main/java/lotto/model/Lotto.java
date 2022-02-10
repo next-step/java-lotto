84fa0771c1938fb93949f82bdc617ad07b4ae42e
Jiin Lee <loveted0225@naver.com>
2022-02-10 20:49:30 +0900

    refactor(Lotto): 각 로또의 당첨번호 일치 개수 로직 Lotto로 위임

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 public class Lotto {
 
-    private static final int LOTTO_NUMBER_CONDITION = 6;
+    private static final int MAX_LOTTO_LENGTH = 6;
+    private static final int FIVE_MATCHING = 5;
     private static final String LOTTO_NUMBER_VALIDATION_MESSAGE = "로또는 6개의 숫자입니다";
 
     private final List<Integer> lottoNumbers;
+    private int matchCount;
+    private boolean bonusMatch;
 
     public Lotto(List<Integer> lottoNumbers) {
         validateLotto(lottoNumbers);
@@ -17,21 +20,37 @@ public class Lotto {
         this.lottoNumbers = new ArrayList<>(lottoNumbers);
     }
 
+    public Lotto(List<Integer> lottoNumbers, int matchCount, boolean bonusMatch) {
+        this(lottoNumbers);
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+
     private void validateLotto(List<Integer> lottoNumbers) {
-        if (lottoNumbers.size() != LOTTO_NUMBER_CONDITION) {
+        if (lottoNumbers.size() != MAX_LOTTO_LENGTH) {
             throw new IllegalArgumentException(LOTTO_NUMBER_VALIDATION_MESSAGE);
         }
     }
 
-    public List<Integer> getNumbers() {
+    public int matchLottoWithWinningNumber(WinningNumber winningNumber) {
+        matchCount = 0;
+        bonusMatch = false;
+        matchCount = winningNumber.findMatchingCount(lottoNumbers);
+        if (matchCount == FIVE_MATCHING) {
+            bonusMatch = winningNumber.isContainBonusBall(lottoNumbers);
+        }
+        return matchCount;
+    }
+
+    public List<Integer> getLottoNumbers() {
         return lottoNumbers;
     }
 
-    public boolean isContainBonusBall(WinningNumber winningNumber) {
-        return winningNumber.isContainBonusBall(lottoNumbers);
+    public int getMatchCount() {
+        return matchCount;
     }
 
-    public boolean isContainBonusBall(int bonusBall) {
-        return this.lottoNumbers.contains(bonusBall);
+    public boolean getBonusMatch() {
+        return bonusMatch;
     }
 }
