2974176348b6a056a9fdf3a11e1f4f928b326e33
xxell-8 <cell8pepper@gmail.com>
2022-02-11 01:42:28 +0900

    refactor: 당첨 번호와 관련된 로직만 수행하도록 역할 조정
    - Rank 계산 후 반환하는 메서드 삭제
    - 매칭 결과 계산 조건 간소화

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private static final String DUPLICATE_NUMBER_EXCEPTION = "[ERROR] 보너스볼은 중복될 수 없습니다.";
+
+    private final Lotto lotto;
+    private final LottoNumber bonus;
+
+    public WinningLotto(Lotto lotto, LottoNumber bonus) {
+        validateWinningNumber(lotto, bonus);
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+
+    private void validateWinningNumber(Lotto lotto, LottoNumber bonus) {
+        List<LottoNumber> numbers = lotto.getLottoNumbers();
+        if (numbers.contains(bonus)) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_EXCEPTION);
+        }
+    }
+
+    public int getMatchCount(Lotto userLotto) {
+        List<LottoNumber> userNumbers = userLotto.getLottoNumbers();
+        return (int) userNumbers.stream()
+            .filter(userNumber -> isContain(lotto.getLottoNumbers(), userNumber))
+            .count();
+    }
+
+    public boolean getMatchBonus(Lotto userLotto) {
+        List<LottoNumber> userNumbers = userLotto.getLottoNumbers();
+        return isContain(userNumbers, bonus);
+    }
+
+    private boolean isContain(List<LottoNumber> numbers, LottoNumber target) {
+        return numbers.contains(target);
+    }
+}
