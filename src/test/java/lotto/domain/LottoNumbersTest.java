8e84dd0a744a953951f922eaa9674a3868804c96
jihyun park <girl9505@naver.com>
2022-02-11 04:49:00 +0900

    refactor: 불필요한 주석 제거

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -7,12 +7,10 @@ import org.junit.jupiter.api.Test;
 
 class LottoNumbersTest {
 
-    // lottoTicket -> List<Integer> 로또 티켓 -> 난수 생성으로 6자리수 만든
-    // lotto ->  lottoTickets = List<lottoTicket> : 메인 로직 짜면
     @Test
     void 로또티켓은_1에서45사이의_중복되지_않은_6개의_수이다() {
         assertThrows(IllegalArgumentException.class,
-            () -> new LottoTicket(1,2,3,3,5,6));
+            () -> new LottoTicket(1, 2, 3, 3, 5, 6));
     }
 
 }
