48fcedd298a884fc60521486d6da301b4b96edee
xxell-8 <cell8pepper@gmail.com>
2022-02-11 01:17:16 +0900

    test: 구매 금액 입력에 따른 에러 처리 테스트 추가

--- src/test/java/lotto/domain/TicketTest.java
+++ src/test/java/lotto/domain/TicketTest.java
@@ -7,20 +7,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TicketTest {
-    @DisplayName("입력한 금액만큼 로또를 구매")
+    @DisplayName("입력한 금액만큼 로또를 구매한다")
     @ParameterizedTest
-    @CsvSource(value = {"1000,1", "15000,15", "2500,2"})
+    @CsvSource(value = {"1000,1", "15000,15", "2000,2"})
     public void 입력한_구입금액만큼_로또를_구매한다(int money, int expected) {
         Ticket ticket = new Ticket(money);
         assertThat(ticket.getBuyCount()).isEqualTo(expected);
     }
 
-    @DisplayName("1000원 미만의 금액 입력 시 ERROR")
-    @Test
-    void 천원미만_금액_입력() {
+    @DisplayName("1000원 미만의 금액 입력 시 EXCEPTION이 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {50, 100, 500, 789})
+    void 천원미만_금액_입력(int cash) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Ticket(500));
+            .isThrownBy(() -> new Ticket(cash));
+    }
+
+    @DisplayName("1000원 단위의 금액이 아닐 경우 EXCEPTION이 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {1050, 1500, 10200, 1789})
+    void 천원_단위가_아닌_금액_입력(int cash) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Ticket(cash));
     }
 }
