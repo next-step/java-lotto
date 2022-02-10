544425e4b3106414286d36d3859c7abe4bcbb318
kimtaejun97 <kim_taejun@naver.com>
2022-02-10 21:19:55 +0900

    test: 입력 공백 제거 테스트

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -6,29 +6,40 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
-    @DisplayName("구입 금액 입력을 입력받는다.")
-    @ValueSource(strings = {"1450 0", "1500",})
-    @ParameterizedTest
-    void 구입_금액_입력을_입력받는다(String input) {
+    @DisplayName("구입 금액 입력을 입력을 때 사이 공백 등 불필요한 공백을 제거한다..")
+    @Test
+    void When_공백이_있는_구매_금액_입력_Then_공백을_제거하고_반환() {
         // given
-        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        InputStream inputStream = new ByteArrayInputStream("14 0 0".getBytes());
         System.setIn(inputStream);
 
         // when
-        String expected = InputView.inputBudget();
+        String output = InputView.inputBudget();
 
         // then
-        assertThat(expected).isEqualTo(input);
+        assertThat(output).isEqualTo("1400");
+    }
+
+    @DisplayName("보너스 번호 을 입력을 때 사이 공백 등 불필요한 공백을 제거한다..")
+    @Test
+    void When_공백이_있는_보너스_번호_입력_Then_공백을_제거하고_반환() {
+        // given
+        InputStream inputStream = new ByteArrayInputStream("1 4".getBytes());
+        System.setIn(inputStream);
+
+        // when
+        String output = InputView.inputBonusNumber();
+
+        // then
+        assertThat(output).isEqualTo("14");
     }
 
     @DisplayName("숫자 사이 공백이 있는 경우 공백을 제거하고 반환한다.")
     @Test
-    void _When_공백이_있는_입력_Then_공백을_제거하고_반환() {
+    void When_공백이_있는_당첨번호_입력_Then_공백을_제거하고_반환() {
         // given
         InputStream inputStream = new ByteArrayInputStream("1, 2, 3, 4, 5, 6".getBytes());
         System.setIn(inputStream);
