5a151cce5cb39643161e70d880a86f0f459840b2
Jiin Lee <loveted0225@naver.com>
2022-02-10 21:10:58 +0900

    test(Statistics): 랭킹 업데이트 메소드 테스트 추가

--- src/test/java/lotto/model/StatisticsTest.java
+++ src/test/java/lotto/model/StatisticsTest.java
@@ -3,38 +3,21 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
-import java.util.List;
-import lotto.model.Statistics;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class StatisticsTest {
 
     @Test
-    void 매칭되는_숫자_갯수를_잘_찾는지_확인() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        List<Integer> winningNumbers = Arrays.asList(2, 4, 6, 8, 10, 12);
-        assertThat(3).isEqualTo(Statistics.getMatchCount(lottoNumbers, winningNumbers));
+    void 랭킹을_업데이트_한다() {
+        Statistics statistics = new Statistics(2);
+        Lotto lotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6), 5, true);
+        Lotto lotto2 = new Lotto(Arrays.asList(2, 4, 6, 8, 10, 12), 3, false);
+        Map<Rank, Integer> ranks = statistics.updateRanks(
+            Arrays.asList(lotto1, lotto2)
+        );
+        assertThat(ranks.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(ranks.get(Rank.FIFTH)).isEqualTo(1);
+        assertThat(ranks.get(Rank.FIRST)).isEqualTo(0);
     }
-
-    @Test
-    void 매칭되는_숫자_갯수를_잘_찾는지_확인2() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 6, 8);
-        List<Integer> winningNumbers = Arrays.asList(2, 4, 6, 8, 10, 12);
-        assertThat(4).isEqualTo(Statistics.getMatchCount(lottoNumbers, winningNumbers));
-    }
-
-    @Test
-    void 보너스볼과_일치하는지_확인() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 6, 8);
-        int bonusBall = 6;
-        assertThat(Statistics.getBonusScore(lottoNumbers, bonusBall)).isTrue();
-    }
-
-    @Test
-    void 보너스볼과_불일치하는지_확인() {
-        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 6, 8);
-        int bonusBall = 10;
-        assertThat(Statistics.getBonusScore(lottoNumbers, bonusBall)).isFalse();
-    }
-
 }
