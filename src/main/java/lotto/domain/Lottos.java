51e0ab657ac7e42cdad9a85c373fd2f1c7148574
xxell-8 <cell8pepper@gmail.com>
2022-02-11 02:05:02 +0900

    refactor: 생성자를 통한 초기화 방식으로 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -6,9 +6,10 @@ import lotto.domain.lottogenerator.LottoGenerator;
 
 public class Lottos {
 
-    private final List<Lotto> lottos = new ArrayList<>();
+    private final List<Lotto> lottos;
 
     public Lottos(LottoGenerator lottoGenerator, final int buyCount) {
+        lottos = new ArrayList<>();
         for (int i = 0; i < buyCount; i++) {
             lottos.add(lottoGenerator.get());
         }
