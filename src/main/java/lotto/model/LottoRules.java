c4d070ddeaac42724e290b859ffb034c698f0892
Jiin Lee <loveted0225@naver.com>
2022-02-10 20:50:05 +0900

    refactor(LottoRules): 불필요한 getter와 상수 삭제

--- src/main/java/lotto/model/LottoRules.java
+++ src/main/java/lotto/model/LottoRules.java
@@ -8,8 +8,6 @@ public class LottoRules {
 
     private final static int START_NUM = 1;
     private final static int LAST_NUM = 45;
-    private final static int LOTTO_COUNT = 6;
-    private final static int LOTTO_PRICE = 1000;
     private final static int START_INDEX = 0;
     private final static int LAST_INDEX = 6;
 
@@ -27,19 +25,4 @@ public class LottoRules {
         return baseLottoNumbers.subList(START_INDEX, LAST_INDEX);
     }
 
-    public int getLottoPrice() {
-        return LOTTO_PRICE;
-    }
-
-    public int getLottoCount() {
-        return LOTTO_COUNT;
-    }
-
-    public int getStartNum() {
-        return START_NUM;
-    }
-
-    public int getLastNum() {
-        return LAST_NUM;
-    }
 }
