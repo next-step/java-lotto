e15b8e3f177233c8035b68e10257d378b72ba967
MinChul-Son <smc5236@gmail.com>
2022-02-11 14:40:07 +0900

    🚀 test(lotto): Money 테스트 코드 작성

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @Test
+    void 투입한_금액으로_로또를_몇_개_살_수_있는지를_알_수_있다() {
+        Money money = new Money(3000);
+
+        int amount = money.divideByLottoPrice();
+
+        assertThat(amount).isEqualTo(3);
+    }
+
+}
