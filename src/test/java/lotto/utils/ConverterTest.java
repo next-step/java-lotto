0d2a44e8a3155b7c50b89633a37315150af56df7
xxell-8 <cell8pepper@gmail.com>
2022-02-11 15:32:21 +0900

    refactor: 불필요한 구문 삭제 및 비교 방식 변경

--- src/test/java/lotto/utils/ConverterTest.java
+++ src/test/java/lotto/utils/ConverterTest.java
@@ -1,11 +1,10 @@
 package lotto.utils;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 import lotto.domain.Lotto;
-import lotto.domain.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,24 +13,17 @@ class ConverterTest {
     private static final Lotto testLotto;
 
     static {
-        List<LottoNumber> testNumbers = Arrays.asList(
-            LottoNumber.from(1),
-            LottoNumber.from(2),
-            LottoNumber.from(3),
-            LottoNumber.from(4),
-            LottoNumber.from(5),
-            LottoNumber.from(6)
-        );
-        testLotto = Lotto.of(testNumbers);
+        List<Integer> testNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        testLotto = Lotto.from(testNumbers);
     }
 
     @DisplayName("배열을 입력 받아 Lotto 객체를 반환한다")
     @Test
     void 배열을_Lotto로_변환() {
-        String[] givenNumbers = {"1", "2", "3", "4", "5", "6"};
+        String[] givenNumbers = {"6", "5", "4", "3", "2", "1"};
         Lotto actual = Converter.inputToLotto(givenNumbers);
 
-        assertThat(actual).isEqualTo(testLotto);
+        assertThat(actual.getLottoNumbers()).hasSameElementsAs(testLotto.getLottoNumbers());
     }
 
     @DisplayName("Lotto 객체를 String으로 반환한다")
