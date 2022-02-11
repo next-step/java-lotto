6b43b73854db4ec79a16b5af3b3bf3b036698bd3
eohaerim <ehl3288@naver.com>
2022-02-12 02:19:08 +0900

    test: create StringCalculatorTest

--- src/test/java/calculator/domain/StringCalculatorTest.java
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -1,71 +1,38 @@
 package calculator.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalculatorTest {
 
     private StringCalculator calculator;
 
-    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    @ParameterizedTest
-    @NullAndEmptySource
-    void emptyOrNull(final String text) {
-        calculator = new StringCalculator(text);
-        assertThat(calculator.add()).isZero();
+    @DisplayName(value = "숫자 하나로 이루어진 number token을 입력 받으면 해당 숫자를 반환한다.")
+    @Test
+    void oneNumber() {
+        NumberTokens numberTokens = new NumberTokens(1);
+        calculator = new StringCalculator(numberTokens);
+        assertThat(calculator.createResult(numberTokens.getNumbers())).isSameAs(1);
     }
 
-    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"1"})
-    void oneNumber(final String text) {
-        calculator = new StringCalculator(text);
-        assertThat(calculator.add()).isSameAs(Integer.parseInt(text));
-    }
-
-    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2"})
-    void twoNumbers(final String text) {
-        calculator = new StringCalculator(text);
-        assertThat(calculator.add()).isSameAs(3);
-    }
-
-    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"1,2:3"})
-    void colons(final String text) {
-        calculator = new StringCalculator(text);
-        assertThat(calculator.add()).isSameAs(6);
-    }
-
-    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"//o\n1o2o3"})
-    void customDelimiter(final String text) {
-        calculator = new StringCalculator(text);
-        assertThat(calculator.add()).isSameAs(6);
+    @DisplayName(value = "숫자 두 개로 이루어진 number token을 입력 받으면 두 숫자의 합을 반환한다.")
+    @Test
+    void twoNumbers() {
+        NumberTokens numberTokens = new NumberTokens(1,2);
+        calculator = new StringCalculator(numberTokens);
+        assertThat(calculator.createResult(numberTokens.getNumbers())).isSameAs(3);
     }
 
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
     @Test
     void negative() {
-        calculator = new StringCalculator("-1,2,3");
-        assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> calculator.add());
-    }
+        NumberTokens numberTokens = new NumberTokens(-1,2);
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new StringCalculator(numberTokens))
+            .withMessage("[ERROR] 음수는 입력할 수 없습니다.");
 
-    @DisplayName(value = "문자열 계산기에 지정된 구분자 외의 문자를 전달하는 경우 RuntimeException 예외 처리를 한다.")
-    @Test
-    void delimiterValidation() {
-        calculator = new StringCalculator("//;\n-1,2;3?9");
-        assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> calculator.add());
     }
 }
