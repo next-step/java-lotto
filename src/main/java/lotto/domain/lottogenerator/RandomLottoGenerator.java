d678a1fcc7fc6a8314b755db0b3e0063649c78f6
PandaHun <gnsgnsek@gmail.com>
2022-02-11 15:15:59 +0900

    refactor(RandomLottoGenerator): remove unused import

--- src/main/java/lotto/domain/lottogenerator/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/lottogenerator/RandomLottoGenerator.java
@@ -1,9 +1,7 @@
 package lotto.domain.lottogenerator;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.domain.lotto.Lotto;
