1cbb1427d7be564f89b3b526cef1d3775b983a14
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-12 17:29:51 +0900

    refactor: Integer 타입 변환 - Integer.parseInt() 사용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요";
     private static final String WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUSBALL_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String PURCHASE_LOTTO_TICKET = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
 
@@ -28,7 +29,7 @@ public class InputView {
 
     public static LottoNumber inputBbonusLottoNumber() {
         System.out.println(BONUSBALL_MESSAGE);
-        int bonusLottoNumber = Integer.valueOf(SCANNER.nextLine());
+        int bonusLottoNumber = Integer.parseInt(SCANNER.nextLine());
         return new LottoNumber(bonusLottoNumber);
     }
 }
