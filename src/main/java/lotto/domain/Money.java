c22256f1cbba1123e203e3a2c30f3236eb19ee2d
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-12 23:21:36 +0900

    refactor: LottoCalculation 클래스 책임 역할 분리
    - LottoCalculation 클래스의 책임 및 역할 분리
    - Money#lottoCalculation 메소드 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,7 +5,7 @@ public class Money {
     private static final String LESS_THOUSAND_ERROR_MESSAGE = "구매 금액은 1000원 이하일 수 없습니다.";
     private static final String NEGATIVE_NUMBER_ERROR_MESSAGE = "구매 금액은 음수일 수 없습니다.";
     private static final String NON_NUMBER_ERROR_MESSAGE = "구매 금액은 정수만 가능합니다.";
-    private static final int NUMBER_THOUSAND = 1000;
+    private static final int LOTTO_PRICE = 1000;
     private static final int NUMBER_ZERO = 0;
     private final int value;
 
@@ -29,7 +29,7 @@ public class Money {
     }
 
     private void validateTypeLessThousand(int value) {
-        if (value < NUMBER_THOUSAND) {
+        if (value < LOTTO_PRICE) {
             throw new IllegalArgumentException(LESS_THOUSAND_ERROR_MESSAGE);
         }
     }
@@ -46,6 +46,10 @@ public class Money {
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(NON_NUMBER_ERROR_MESSAGE);
         }
+    }
+
+    public int lottoCalculation() {
+        return this.value / LOTTO_PRICE;
     }
 
 }
