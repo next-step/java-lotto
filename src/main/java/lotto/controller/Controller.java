58bf980ca30d5393e12232f7f2219f449a7d2945
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-12 17:07:01 +0900

    refactor: 메소드 네이밍 & 매직 리터럴 변경

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -16,7 +16,7 @@ import lotto.view.UserResult;
 public class Controller {
 
     public static void run() {
-        Money money = UserConsole.inputString();
+        Money money = UserConsole.inputPurchaseAmount();
 
         LottoCalculation lottoCalculation = new LottoCalculation(money);
         int count = lottoCalculation.lottoCalculation();
--- src/main/java/lotto/view/UserConsole.java
+++ src/main/java/lotto/view/UserConsole.java
@@ -6,9 +6,9 @@ import lotto.domain.Money;
 
 public class UserConsole {
 
-    private static final String FIRST_MESSAGE = "구입 금액을 입력해 주세요";
-    private static final String SECOND_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
-    private static final String THIRD_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요";
+    private static final String WINNING_LOTTO_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUSBALL_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
 
@@ -16,18 +16,18 @@ public class UserConsole {
 
     }
 
-    public static Money inputString() {
-        System.out.println(FIRST_MESSAGE);
+    public static Money inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
         return new Money(SCANNER.nextLine());
     }
 
     public static String inputWinningLottoNumber() {
-        System.out.println(SECOND_MESSAGE);
+        System.out.println(WINNING_LOTTO_NUMBER_MESSAGE);
         return SCANNER.nextLine();
     }
 
     public static LottoNumber inputBbonusLottoNumber() {
-        System.out.println(THIRD_MESSAGE);
+        System.out.println(BONUSBALL_MESSAGE);
         int bonusLottoNumber = Integer.valueOf(SCANNER.nextLine());
         return new LottoNumber(bonusLottoNumber);
     }
