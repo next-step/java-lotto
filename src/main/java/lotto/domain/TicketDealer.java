b0330343c28be0b9dbc391b93cedc15f98af3ad7
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-12 14:28:45 +0900

    refactor: 로또를 구매하는 로직 반환값 네이밍 변경
    return LottoRepository -> Lottos

--- src/main/java/lotto/domain/TicketDealer.java
+++ src/main/java/lotto/domain/TicketDealer.java
@@ -5,14 +5,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoNumber;
-import lotto.domain.lotto.LottoRepository;
+import lotto.domain.lotto.Lottos;
 import lotto.domain.machine.LottoGenerator;
 
 public class TicketDealer {
 
     private static final int ONE = 1;
 
-    public static LottoRepository getLottosOf(LottoGenerator lottoGenerator, final int ticketCount) {
+    public static Lottos getLottosOf(LottoGenerator lottoGenerator, final int ticketCount) {
         List<Lotto> lottos = IntStream.rangeClosed(ONE, ticketCount)
             .mapToObj(index -> lottoGenerator.get().stream()
                 .map(LottoNumber::new)
@@ -20,6 +20,6 @@ public class TicketDealer {
             .map(Lotto::new)
             .collect(Collectors.toList());
 
-        return new LottoRepository(lottos);
+        return new Lottos(lottos);
     }
 }
