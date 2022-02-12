b599bd20ff8a5ddd252c2efcd7a7acf2bea2e8c2
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-12 11:05:34 +0900

    refactor: 매직넘버 제거

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String ENTER = "\n";
+    private static final String CUSTOM_DELIMITER_INDEX = "\\";
 
     public static String getUserInput() {
         Scanner scanner = new Scanner(System.in);
@@ -12,7 +13,7 @@ public class InputView {
         StringBuilder sb = new StringBuilder();
 
         String firstLine = scanner.nextLine();
-        if (firstLine.startsWith("//")) {
+        if (firstLine.startsWith(CUSTOM_DELIMITER_INDEX)) {
             String secondLine = scanner.nextLine();
             sb.append(firstLine+ENTER).append(secondLine);
             return sb.toString();
