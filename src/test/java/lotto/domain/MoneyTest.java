75f092836771695cdf63d707439ab9eee477ee51
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-12 17:42:26 +0900

    test: Money 객체 생성 시 테스트 코드 작성

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,17 +1,20 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MoneyTest {
 
     @DisplayName("돈_객체_생성시_주입받은_필드_값에_대한_예외_테스트")
-    @Test
-    void createMoneyTest() {
-        Money money = new Money(3);
-        assertThat(money.getValue()).isEqualTo(3);
+    @ParameterizedTest
+    @ValueSource(strings = {"900", "-2", "abc"})
+    void createMoneyTest(String value) {
+        assertThatIllegalArgumentException().isThrownBy(()->new Money(value));
     }
 
     @DisplayName("로또_당첨_수익률_테스트")
