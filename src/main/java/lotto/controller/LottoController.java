b1b03d356d7aa9896169dd70eb71710d8dc8a8a2
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-12 14:30:51 +0900

    feat: 변경점에 따른 컨트롤러 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,9 +4,8 @@ import lotto.domain.Ticket;
 import lotto.domain.TicketDealer;
 import lotto.domain.WinningNumber;
 import lotto.domain.WinningResult;
-import lotto.domain.lotto.LottoRepository;
+import lotto.domain.lotto.Lottos;
 import lotto.domain.machine.RandomLottoGenerator;
-import lotto.domain.rank.RankRepository;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -16,16 +15,14 @@ public class LottoController {
         OutputView outputView = new OutputView();
 
         Ticket ticket = InputView.buyTicket();
-        LottoRepository lottoRepository = TicketDealer.getLottosOf(new RandomLottoGenerator(), ticket.getBuyCount());
+        Lottos lottoRepository = TicketDealer.getLottosOf(new RandomLottoGenerator(), ticket.getBuyCount());
 
         outputView.printPurchaseAmount(ticket);
         outputView.printPurchaseTicket(lottoRepository);
 
         WinningNumber winningNumber = InputView.makeWinningNumber();
 
-        RankRepository rankRepository = RankRepository.getRanksOf(lottoRepository, winningNumber);
-
-        WinningResult winningResult = new WinningResult(rankRepository);
+        WinningResult winningResult = new WinningResult(lottoRepository, winningNumber);
         winningResult.calculateYield(ticket.getBuyCash());
 
         outputView.printWinningResult(winningResult);
