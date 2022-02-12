b97e3629e559c68d49bd22d4e1c7fefdd4c19bbd
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-13 04:31:21 +0900

    feat: 전달받은 DTO 객체에서 수동, 자동으로 입력된 값을 출력할 수 있도록 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,9 +3,9 @@ package lotto.view;
 import java.util.Arrays;
 import java.util.Comparator;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
 import lotto.domain.Rank;
 import lotto.domain.RankResult;
+import lotto.domain.dto.LottoCalculationDTO;
 
 public class OutputView {
 
@@ -15,14 +15,15 @@ public class OutputView {
     private final static String RESULT_MESSAGE = "%s개 일치 (%d원)- %d개\n";
     private final static String SECOND_RESULT_MESSAGE = "%s개 일치, 보너스 볼 일치 (%d원)- %d개\n";
     private final static String TOTAL_RATIO_MESSAGE = "총 수익률은 %.2f입니다.";
+    private final static String PURCHASED_LOTTO_RESULT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.\n";
 
     private OutputView() {
 
     }
 
-    public static void printCountMessage(Lottos lottos) {
-        System.out.println(lottos.getLottosSize() + LOTTO_COUNT_MESSAGE);
-        for (Lotto lotto : lottos.lottos()) {
+    public static void printCountMessage(LottoCalculationDTO resultDto) {
+        System.out.printf(PURCHASED_LOTTO_RESULT, resultDto.getNumberOfLottoManual(), resultDto.getNumberOfLottoAutomatical());
+        for (Lotto lotto : resultDto.getLottos()) {
             System.out.println(lotto.getLottoNumber());
         }
     }
