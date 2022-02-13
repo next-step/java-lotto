2526f0866a35fca4dbfcf8d3e8bef05ad8e029db
Jiin Lee <loveted0225@naver.com>
2022-02-14 00:47:27 +0900

    feat(MatchInfo): matchCount와 bonusMatch 정보를 갖고 있는 객체 생성

---
+++ src/main/java/lotto/model/MatchInfo.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+public class MatchInfo {
+
+    private int matchCount;
+    private boolean bonusMatch;
+
+    public MatchInfo(int matchCount, boolean bonusMatch){
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+}
