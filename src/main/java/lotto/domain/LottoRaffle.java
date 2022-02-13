62b32dd24bf36bfc1f8dfbefbd3cba0288f6fcca
0JUUU <clleo97@naver.com>
2022-02-14 04:12:28 +0900

    refactor: 인스턴스 변수 제거
    1. MatchResult 인스턴스 변수 제거
    2. compareLotto 제거
    3. compareLottos 추가
    4. getResults 제거

--- src/main/java/lotto/domain/LottoRaffle.java
+++ src/main/java/lotto/domain/LottoRaffle.java
@@ -1,35 +1,32 @@
 package lotto.domain;
 
 import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
 import lotto.domain.lotto.WinningLotto;
 
 public class LottoRaffle {
 
     private final WinningLotto winningLotto;
-    private MatchResult matchResult;
 
     public LottoRaffle(WinningLotto winningLotto) {
         this.winningLotto = winningLotto;
-        matchResult = MatchResult.init();
     }
 
-    public void compareLotto(Lotto lotto) {
-        final int matchCount = calculateMatchCount(lotto);
-        final boolean isBonusNumber = calculateBonusNumber(lotto);
-
-        final LottoResult lottoResult = LottoResult.findLottoResult(matchCount, isBonusNumber);
-        matchResult = matchResult.combine(lottoResult);
-    }
-
-    private boolean calculateBonusNumber(Lotto lotto) {
-        return lotto.hasNumber(winningLotto.getBonusNumber());
+    public MatchResult compareLottos(Lottos lottos) {
+        MatchResult matchResult = MatchResult.init();
+        for (Lotto lotto : lottos.getLottos()) {
+            final int matchCount = calculateMatchCount(lotto);
+            final boolean isBonusNumber = calculateBonusNumber(lotto);
+            matchResult = matchResult.combine(matchCount, isBonusNumber);
+        }
+        return matchResult;
     }
 
     private Integer calculateMatchCount(Lotto lotto) {
         return lotto.getMatchCount(winningLotto);
     }
 
-    public MatchResult getResults() {
-        return matchResult;
+    private boolean calculateBonusNumber(Lotto lotto) {
+        return lotto.hasNumber(winningLotto.getBonusNumber());
     }
 }
