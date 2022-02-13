bbad799af7b07c61dea560bbd01138597f5ed779
hscom96 <hscom96@gmail.com>
2022-02-14 01:58:48 +0900

    refactor: assertAll구문 추가

--- src/test/java/calculator/domain/NumbersTest.java
+++ src/test/java/calculator/domain/NumbersTest.java
@@ -1,6 +1,7 @@
 package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,9 +17,11 @@ class NumbersTest {
         Numbers numbers = new Numbers(expression);
 
         // then
-        assertThat(numbers.get(0).getValue()).isEqualTo(1);
-        assertThat(numbers.get(1).getValue()).isEqualTo(5);
-        assertThat(numbers.get(2).getValue()).isEqualTo(3);
+        assertAll(
+            () -> assertThat(numbers.get(0).getValue()).isEqualTo(1),
+            () -> assertThat(numbers.get(1).getValue()).isEqualTo(5),
+            () -> assertThat(numbers.get(2).getValue()).isEqualTo(3)
+        );
     }
 
     @ParameterizedTest
@@ -28,17 +31,21 @@ class NumbersTest {
         Numbers numbers = new Numbers(expression);
 
         // then
-        assertThat(numbers.getValues().get(0).getValue()).isZero();
-        assertThat(numbers.getValues().size()).isEqualTo(1);
+        assertAll(
+            () -> assertThat(numbers.getValues().get(0).getValue()).isZero(),
+            () -> assertThat(numbers.getValues().size()).isEqualTo(1)
+        );
     }
 
     @Test
-    void 숫자_하나를_문자열로_입력할경우_해당_숫자를_반환한다(){
+    void 숫자_하나를_문자열로_입력할경우_해당_숫자를_반환한다() {
         // when
         Numbers numbers = new Numbers("5");
 
         // then
-        assertThat(numbers.getValues().get(0).getValue()).isEqualTo(5);
-        assertThat(numbers.getValues().size()).isEqualTo(1);
+        assertAll(
+            () -> assertThat(numbers.getValues().get(0).getValue()).isEqualTo(5),
+            () -> assertThat(numbers.getValues().size()).isEqualTo(1)
+        );
     }
 }
