427689da5e77ec5301c19d0a1e5b3bc9e74db147
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-13 15:49:33 +0900

    feat: Controller 로또 생성 수동과 자동 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,11 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.domain.Ticket;
 import lotto.domain.TicketDealer;
 import lotto.domain.WinningNumber;
 import lotto.domain.WinningResult;
+import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.Lottos;
 import lotto.domain.machine.RandomLottoGenerator;
 import lotto.view.InputView;
@@ -15,14 +17,23 @@ public class LottoController {
         OutputView outputView = new OutputView();
 
         Ticket ticket = InputView.buyTicket();
-        Lottos lottoRepository = TicketDealer.getLottosOf(new RandomLottoGenerator(), ticket.getBuyCount());
+
+        List<Lotto> manualLottos = InputView.buyManualLotto();
+        int manualLottosSize = manualLottos.size();
+        Lottos lottos = TicketDealer.getLottosByManual(manualLottos);
+
+        List<Lotto> autoLottos = TicketDealer.getLottosByAuto(lottos, new RandomLottoGenerator(), ticket.getBuyTotalCount() - manualLottos.size());
+        int autoLottosSize = autoLottos.size();
+        lottos.appendLottos(autoLottos);
+
+        ticket.detailCount(manualLottosSize, autoLottosSize);
 
         outputView.printPurchaseAmount(ticket);
-        outputView.printPurchaseTicket(lottoRepository);
+        outputView.printPurchaseTicket(lottos);
 
         WinningNumber winningNumber = InputView.makeWinningNumber();
 
-        WinningResult winningResult = new WinningResult(lottoRepository, winningNumber);
+        WinningResult winningResult = new WinningResult(lottos, winningNumber);
         winningResult.calculateYield(ticket.getBuyCash());
 
         outputView.printWinningResult(winningResult);
