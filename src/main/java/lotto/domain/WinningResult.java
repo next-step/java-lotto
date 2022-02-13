e1e69800f1280180ed226b2dbf6b12b6b527a37c
xxell-8 <cell8pepper@gmail.com>
2022-02-13 17:40:46 +0900

    refactor: WinningResult를 결과 집계용으로 역할 축소
    - 생성자를 통해 설정된 초기값을 바꾸지 못하도록 변경
    - 실제로 계산하는 메서드는 WinningLotto로 이동

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -1,36 +1,14 @@
 package lotto.domain;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class WinningResult {
 
-    private final WinningLotto winningLotto;
     private final Map<Rank, Integer> result;
 
-    public WinningResult(WinningLotto winningLotto) {
-        this.winningLotto = winningLotto;
-        this.result = new LinkedHashMap<>();
-        for (Rank rank : Rank.values()) {
-            if (rank == Rank.NONE) continue;
-            result.put(rank, 0);
-        }
-    }
-
-    public Map<Rank, Integer> mapResult(List<Lotto> userLottos) {
-        for (Lotto lotto : userLottos) {
-            Rank rank = getRank(lotto);
-            if (rank == Rank.NONE) continue;
-            result.put(rank, result.get(rank) + 1);
-        }
-        return new LinkedHashMap<>(result);
-    }
-
-    private Rank getRank(Lotto userLotto) {
-        int matchCount = winningLotto.getMatchCount(userLotto);
-        boolean matchBonus = winningLotto.getMatchBonus(userLotto);
-        return Rank.find(matchCount, matchBonus);
+    public WinningResult(Map<Rank, Integer> result) {
+        this.result = result;
     }
 
     public double calculateProfitRate(int buyCash) {
@@ -44,5 +22,9 @@ public class WinningResult {
             prizeMoney += result.get(rank) * rank.getPrizeMoney();
         }
         return prizeMoney;
+    }
+
+    public Map<Rank, Integer> getResult() {
+        return new LinkedHashMap<>(result);
     }
 }
