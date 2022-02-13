226dba4b8d6306e744958cf3cd1c2ff183eed632
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-13 16:15:37 +0900

    refactor: 미사용 import문 제거

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -1,7 +1,5 @@
 package lotto.domain.lotto;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
