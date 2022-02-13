febfbf831d0916057642eef6e1da5c746fb38e3f
eohaerim <ehl3288@naver.com>
2022-02-13 16:44:18 +0900

    refactor: BigDecimal 적용

--- src/main/java/lotto/factory/UserLottoFactory.java
+++ src/main/java/lotto/factory/UserLottoFactory.java
@@ -1,21 +1,23 @@
 package lotto.factory;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.UserLotto;
 
 public class UserLottoFactory {
 
-    private static final int LOTTO_PRICE = 1000;
+    private static final BigDecimal LOTTO_PRICE = new BigDecimal(1000);
 
     private UserLottoFactory() {
     }
 
     public static int getLottoQuantity(int purchasePrice) {
-        if (purchasePrice < LOTTO_PRICE) {
+        if (purchasePrice < LOTTO_PRICE.intValue()) {
             throw new IllegalArgumentException("[ERROR] 구매 금액이 1000원 이상이어야 합니다.");
         }
-        return purchasePrice / LOTTO_PRICE;
+        return new BigDecimal(purchasePrice).divide(LOTTO_PRICE, RoundingMode.FLOOR).intValue();
     }
 
     public static List<UserLotto> from(int quantity) {
