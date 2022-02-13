e5b78be35c1691410e1128d93eced9900869e783
0JUUU <clleo97@naver.com>
2022-02-14 04:18:28 +0900

    refactor: 변경된 메서드에 맞춰 로직 변경

--- src/main/java/lotto/domain/YieldCalculator.java
+++ src/main/java/lotto/domain/YieldCalculator.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.Map;
 import lotto.domain.money.Money;
 
 public class YieldCalculator {
@@ -10,8 +9,8 @@ public class YieldCalculator {
 
     public static double calculateYield(MatchResult lottoResults, Money money) {
         long profit = 0;
-        for (Map.Entry<LottoResult, Integer> resultEntry : lottoResults.getMatchResult().entrySet()) {
-            profit += (long) resultEntry.getValue() * resultEntry.getKey().getWinning();
+        for (LottoResult lottoResult : lottoResults.getLottoResultSet()) {
+            profit += (long) lottoResults.getCount(lottoResult) * lottoResult.getWinning();
         }
         return (double) profit / (double) money.getValue();
     }
