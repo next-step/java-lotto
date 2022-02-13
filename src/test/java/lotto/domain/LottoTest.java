d3a2e451eddf2203cf9f7b2ab313654a7c6f9cde
parkhyojeong <qkrgywjd456@naver.com>
2022-02-13 20:11:47 +0900

    refactor(lottoTest): 각 로또번호를 객체로 감싸 테스트로직 변경

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -2,23 +2,33 @@ package lotto.domain;
 
 import java.util.Arrays;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
+
+    private  Lotto lotto;
+    private  LottoMachine lottoMachine;
+    @BeforeEach
+    public void init(){
+        List<LottoNumber> lottoNumbers =
+            Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5), new LottoNumber(15));
+        List<LottoNumber> winningLottoNumbers =
+            Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(8), new LottoNumber(9), new LottoNumber(10));
+        this.lotto = new Lotto(lottoNumbers);
+        this.lottoMachine=new LottoMachine(winningLottoNumbers,15);
+    }
     @Test
     public void 로또_한_장당_당첨_번호_일치_개수를_반환함(){
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-
-        LottoMachine lottoMachine = new LottoMachine(Arrays.asList(3, 4, 5, 6, 7, 8), 9);
-        Assertions.assertThat(lotto.compareLottoAndWinnerNumbers(lottoMachine)).isEqualTo(4);
+        Assertions.assertThat(lotto.compareLottoAndWinnerNumbers(lottoMachine)).isEqualTo(3);
     }
 
     @Test
     public void 로또_한_장당_보너스_일치_개수를_반환함(){
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-
-        LottoMachine lottoMachine = new LottoMachine(Arrays.asList(3, 4, 5, 6, 7, 8), 1);
         Assertions.assertThat(lotto.compareLottoAndBonusNumbers(lottoMachine)).isEqualTo(1);
     }
 }
