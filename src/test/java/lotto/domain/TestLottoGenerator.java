ae6905344e0423b3e51c0d4f088383dc82e385da
parkhyojeong <qkrgywjd456@naver.com>
2022-02-13 21:03:31 +0900

    test(testLottoGenerator): test용 lottoGenerator 클래스 생성

---
+++ src/test/java/lotto/domain/TestLottoGenerator.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestLottoGenerator implements LottoGenerator {
+
+    @Override
+    public List<LottoNumber> generateLottoNumber() {
+        return Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+            new LottoNumber(4), new LottoNumber(5), new LottoNumber(6));
+    }
+}
