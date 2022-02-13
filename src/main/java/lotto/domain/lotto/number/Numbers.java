2e6c31582d51c75ed8fc37784dc6319d0679036c
0JUUU <clleo97@naver.com>
2022-02-13 17:08:13 +0900

    refactor: 테스트코드를 위해 만든 생성자 제거
    - List<Number> numbers를 매개변수로 갖는 생성자 사용

--- src/main/java/lotto/domain/lotto/number/Numbers.java
+++ src/main/java/lotto/domain/lotto/number/Numbers.java
@@ -1,9 +1,7 @@
 package lotto.domain.lotto.number;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import lotto.domain.lotto.Lotto;
 import lotto.util.NumbersValidator;
 
@@ -13,10 +11,6 @@ public class Numbers {
 
     public Numbers(final String input) {
         this(NumbersValidator.validate(input));
-    }
-
-    public Numbers(final int... numbers) {
-        this(Arrays.stream(numbers).mapToObj(Number::new).collect(Collectors.toList()));
     }
 
     public Numbers(final Numbers newNumbers) {
