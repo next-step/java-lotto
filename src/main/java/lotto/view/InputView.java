9be8b1e6b6c4cfcf081801e8831969340432cb2b
Jiin Lee <loveted0225@naver.com>
2022-02-15 00:21:31 +0900

    feat: 로또 수동 개수와 번호를 입력하는 메소드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import lotto.model.Lotto;
+import lotto.model.LottoNumber;
 
 public class InputView {
 
@@ -10,20 +13,38 @@ public class InputView {
     private static final String PRICE_INPUT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING__NUMBER_INPUT_GUIDE_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_BALL_INPUT_GUIDE_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String THE_NUMBER_OF_MANUAL_LOTTO_INPUT_MESSAGE="수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_NUMBER_INPUT_MESSAGE ="수동으로 구매할 번호를 입력해 주세요.";
 
     public static int inputPrice() {
         System.out.println(PRICE_INPUT_GUIDE_MESSAGE);
         return SCANNER.nextInt();
     }
 
+    public static int inputAmountOfManualLotto(){
+        System.out.println(THE_NUMBER_OF_MANUAL_LOTTO_INPUT_MESSAGE);
+        return SCANNER.nextInt();
+    }
+
+    public static List<List<String>> inputManualLottoNumber(int amountOfManualLotto){
+        List<List<String>> manualLottos = new ArrayList<>();
+        System.out.println(MANUAL_LOTTO_NUMBER_INPUT_MESSAGE);
+        for(int i=0; i<amountOfManualLotto; i++){
+            String manualLottoNumberInput=SCANNER.nextLine();
+            String [] manualLottoNumber =manualLottoNumberInput.split(" |,");
+            manualLottos.add(Arrays.asList(manualLottoNumber));
+        }
+        return manualLottos;
+    }
     public static List<String> inputWinningNumbers() {
         System.out.println(WINNING__NUMBER_INPUT_GUIDE_MESSAGE);
         String inputWinningNumbers = SCANNER.nextLine();
         return Arrays.asList(inputWinningNumbers.split(" |,"));
     }
 
-    public static int inputBonusBall() {
+    public static LottoNumber inputBonusBall() {
         System.out.println(BONUS_BALL_INPUT_GUIDE_MESSAGE);
-        return SCANNER.nextInt();
+        int inputBonusBall=SCANNER.nextInt();
+        return new LottoNumber(inputBonusBall);
     }
 }
