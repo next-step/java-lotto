e7c9fbf55fe475bb3bf2a51be800ca6e886b85a6
parkhyojeong <qkrgywjd456@naver.com>
2022-02-14 21:15:36 +0900

    refactor(LottoNumberTest): 테스트 메서드명 수정

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 class LottoNumberTest {
 
     @Test
-    public void 로또숫자_범위가_1이상_45이하_테스트_45초과일_때(){
+    public void 로또숫자_범위가_1이상_45이하_테스트_45초과일_때는_에러가_발생(){
         Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(()->new LottoNumber(50));
     }
 
     @Test
-    public void 로또숫자_범위가_1이상_45이하_테스트_1미만일_때(){
+    public void 로또숫자_범위가_1이상_45이하_테스트_1미만일_때는_에러가_발생(){
         Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(()->new LottoNumber(-1));
     }
 
     @Test
-    public void 로또숫자_범위가_1이상_45이하_테스트_범위를_만족할_때(){
+    public void 로또숫자_범위가_1이상_45이하_테스트_범위를_만족할_때는_성공(){
         Assertions.assertThat(new LottoNumber(5)).isEqualTo(new LottoNumber(5));
     }
 
