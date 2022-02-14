4e51154473698f06435296a221fd0f9b19451344
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-15 03:28:14 +0900

    refactor: stream을 활용

--- src/main/java/lotto/domain/RankResult.java
+++ src/main/java/lotto/domain/RankResult.java
@@ -2,15 +2,16 @@ package lotto.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RankResult {
 
-    private static final int INITIAL_VALUE = 0;
     private final Map<Rank, Integer> rankResult;
 
 
     public RankResult(final Lottos lottos, final Winning winning) {
-        this.rankResult = rankCount(lottos, winning);
+        this.rankResult = lottos.lottos().stream().collect(
+            Collectors.groupingBy(arg -> Rank.countMatch(winning, arg),Collectors.summingInt(x -> 1)));
     }
 
     public Map<Rank, Integer> getRankResult() {
@@ -21,18 +22,5 @@ public class RankResult {
         return new Money(rankResult.entrySet().stream()
             .mapToInt(entry -> entry.getKey().getPrize(entry.getValue()).getValue())
             .sum());
-    }
-
-    public HashMap<Rank, Integer> rankCount(final Lottos lottos, final Winning winning) {
-        HashMap<Rank, Integer> rankResult = new HashMap<>();
-        for (Rank rank : Rank.values()) {
-            rankResult.put(rank, INITIAL_VALUE);
-        }
-
-        for (Lotto lotto : lottos.lottos()) {
-            Rank rank = Rank.countMatch(winning, lotto);
-            rankResult.put(rank, rankResult.get(rank) + 1);
-        }
-        return rankResult;
     }
 }
