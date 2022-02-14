6279698c78c194ef87c9c5268acf43e4a8e8f557
parkhyojeong <qkrgywjd456@naver.com>
2022-02-14 21:13:45 +0900

    refactor(LottoNumber): 매직넘버 상수화

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -4,6 +4,9 @@ import java.util.Objects;
 
 public class LottoNumber {
 
+    private static final int LOTTO_START_RANGE=1;
+    private static final int LOTTO_END_RANGE=45;
+    private static final String LOTTO_RANGE_ERROR="1 이상 45 이하의 수를 입력해주세요";
     private final int lottoNumber;
 
     public LottoNumber(final String input) {
@@ -15,8 +18,8 @@ public class LottoNumber {
     }
 
     public int validatorValue(final int value) {
-        if (value < 1 || value > 45) {
-            throw new IllegalArgumentException("1 이상 45 이하의 수를 입력해주세요");
+        if (value < LOTTO_START_RANGE || value > LOTTO_END_RANGE) {
+            throw new IllegalArgumentException(LOTTO_RANGE_ERROR);
         }
         return value;
     }
