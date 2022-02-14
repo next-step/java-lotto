9965250fd83e1b4d227c52506df537dc4e1227d9
hochan222 <hochan049@gmail.com>
2022-02-14 15:07:13 +0900

    test: WinningNumbers 메소드, 변수 네이밍 변경 및 테스트 케이스 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,72 +1,87 @@
-//package lotto.domain;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.assertThatThrownBy;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.stream.Stream;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.MethodSource;
-//
-//class WinningNumbersTest {
-//
-//    static Stream<Arguments> generateValidWinningNumbersInput() {
-//        return Stream.of(
-//            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
-//            Arguments.of(Arrays.asList(10, 11, 12, 13, 14, 15)),
-//            Arguments.of(Arrays.asList(10, 20, 30, 40, 9, 45))
-//        );
-//    }
-//
-//    @DisplayName(value = "정수 리스트의 크기가 6이면, winningNumbers 의 크기가 6")
-//    @ParameterizedTest
-//    @MethodSource("generateValidWinningNumbersInput")
-//    void GivenListWithIntType_WhenListSizeis6_ThenWinningNumbersSize6(
-//        List<Integer> winningNumbersInput) {
-//        final int winningNumbersSize = WinningNumbers.from(winningNumbersInput).getWinningNumbers().size();
-//
-//        assertThat(winningNumbersSize)
-//            .isEqualTo(6);
-//    }
-//
-//    static Stream<Arguments> generateInvalidSizeWinningNumbersInput() {
-//        return Stream.of(
-//            Arguments.of(Arrays.asList()),
-//            Arguments.of(Arrays.asList(1)),
-//            Arguments.of(Arrays.asList(1, 2, 3, 4, 5)),
-//            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6, 7))
-//        );
-//    }
-//
-//    @DisplayName(value = "정수 리스트의 크기가 6이 아니면, IllegalArgumentException.")
-//    @ParameterizedTest
-//    @MethodSource("generateInvalidSizeWinningNumbersInput")
-//    void GivenListWithIntType_WhenListSizeNot6_ThenIllegalArgumentException(
-//        List<Integer> winningNumbersInput) {
-//        assertThatThrownBy(() -> WinningNumbers.from(winningNumbersInput))
-//            .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    static Stream<Arguments> generateInvalidRangeWinningNumbersInput() {
-//        return Stream.of(
-//            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 46)),
-//            Arguments.of(Arrays.asList(0, 2, 3, 4, 5, 6)),
-//            Arguments.of(Arrays.asList(1, 2, 46, 4, 5, 6)),
-//            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 100)),
-//            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 1000))
-//        );
-//    }
-//
-//    @DisplayName(value = "정수 리스트의 범위가 당첨 번호 범위내에 있지 않으면, IllegalArgumentException.")
-//    @ParameterizedTest
-//    @MethodSource("generateInvalidRangeWinningNumbersInput")
-//    void GivenListWithIntType_WhenWinningNumberIsNotInRange_ThenIllegalArgumentException(
-//        List<Integer> winningNumbersInput) {
-//        assertThatThrownBy(() -> WinningNumbers.from(winningNumbersInput))
-//            .isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//}
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class WinningNumbersTest {
+
+    private static final List<Integer> DEFAULT_WINNING_NUMBER_LIST = Arrays.asList(1, 2, 3, 4, 5,
+        6);
+    private static final int DEFAULT_BONUS_NUMBER_45 = 45;
+
+    static WinningNumbers numberListToWinningNumbers(List<Integer> list, int bonusNumber) {
+        List<LottoNumber> lottoNumberList = list.stream()
+            .map(number -> LottoNumber.from(number))
+            .collect(Collectors.toList());
+        Lotto lotto = Lotto.from(lottoNumberList);
+        LottoNumber bonusLottoNumber = LottoNumber.from(bonusNumber);
+
+        return WinningNumbers.from(lotto, bonusLottoNumber);
+    }
+
+    static Stream<Arguments> generateListWithIntTypeIncludeLottoNumber() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+            Arguments.of(Arrays.asList(2, 1, 3, 4, 5, 6)),
+            Arguments.of(Arrays.asList(2, 3, 1, 4, 5, 6)),
+            Arguments.of(Arrays.asList(2, 3, 4, 1, 5, 6)),
+            Arguments.of(Arrays.asList(2, 3, 4, 5, 1, 6)),
+            Arguments.of(Arrays.asList(2, 3, 4, 5, 6, 1))
+        );
+    }
+
+    @DisplayName(value = "winningNumbers에 포함되는 lottoNumber가 주어질 때, isWinningNumbersContain 의 반환값은 참이다.")
+    @ParameterizedTest
+    @MethodSource("generateListWithIntTypeIncludeLottoNumber")
+    void GivenLottoNumberInListWithIntType_WhenIsWinningNumbersContain_ThenTrue(
+        List<Integer> winningNumbersInput) {
+        WinningNumbers winningNumbers = numberListToWinningNumbers(winningNumbersInput,
+            DEFAULT_BONUS_NUMBER_45);
+        LottoNumber lottoNumber = LottoNumber.from(1);
+
+        assertThat(winningNumbers.isWinningNumbersContain(lottoNumber))
+            .isEqualTo(true);
+    }
+
+    static Stream<Arguments> generateListWithIntTypeExcludeLottoNumber() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6)),
+            Arguments.of(Arrays.asList(10, 20, 30, 40, 41, 42))
+        );
+    }
+
+    @DisplayName(value = "winningNumbers에 포함되지 않는 lottoNumber가 주어질 때, isWinningNumbersContain 의 반환값은 거짓이다.")
+    @ParameterizedTest
+    @MethodSource("generateListWithIntTypeExcludeLottoNumber")
+    void GivenLottoNumberNotInListWithIntType_WhenIsWinningNumbersContain_ThenFalse(
+        List<Integer> winningNumbersInput) {
+        WinningNumbers winningNumbers = numberListToWinningNumbers(winningNumbersInput,
+            DEFAULT_BONUS_NUMBER_45);
+        LottoNumber lottoNumber = LottoNumber.from(45);
+
+        assertThat(winningNumbers.isWinningNumbersContain(lottoNumber))
+            .isEqualTo(false);
+    }
+
+    @DisplayName(value = "동일한 두 보너스 넘버가 인자로 주어질 때, isWinningNumbersContain 의 반환값은 참이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 45})
+    void GivenTwoSameBonusLottoNumberWithIntType_WhenIsBonusNumberContain_ThenTrue(
+        int bonusNumber) {
+        WinningNumbers winningNumbers = numberListToWinningNumbers(DEFAULT_WINNING_NUMBER_LIST,
+            bonusNumber);
+        LottoNumber bonusLottoNumber = LottoNumber.from(bonusNumber);
+
+        assertThat(winningNumbers.isBonusNumberEqualTo(bonusLottoNumber))
+            .isEqualTo(true);
+    }
+}
