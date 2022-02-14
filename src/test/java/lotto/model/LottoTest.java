261b5b77213529167341d21134102e56d79a3c3d
Jiin Lee <loveted0225@naver.com>
2022-02-15 00:18:50 +0900

    refactor: LottoNumber 객체로 원시값 포장 -LottoTest

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -10,12 +10,16 @@ class LottoTest {
 
     @Test
     void 로또는_숫자_여섯개() {
-        Lotto lotto = new Lotto(Arrays.asList(2, 4, 8, 41, 15, 20));
+        Lotto lotto = new Lotto(Arrays.asList(
+            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41), new LottoNumber(15),
+            new LottoNumber(20)
+        ));
         assertThat(lotto.getLottoNumbers().size()).isEqualTo(6);
     }
 
     @Test
     void 여섯개가_아닌_숫자가_들어오면_로또생성은_실패한다() {
-        assertThatThrownBy(() -> new Lotto(Arrays.asList(2, 4, 8, 41, 15)));
+        assertThatThrownBy(() -> new Lotto(Arrays.asList( new LottoNumber(2), new LottoNumber(4),
+            new LottoNumber(8), new LottoNumber(41), new LottoNumber(15))));
     }
 }
