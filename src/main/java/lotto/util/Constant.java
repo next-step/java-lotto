34bca6ec72b72e6c6d5faad6508fe6882cf6ed27
jihyun park <girl9505@naver.com>
2022-02-16 04:37:54 +0900

    feat: 소수점 2자리를 표현하기 위한 단위 추가

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -10,6 +10,7 @@ public class Constant {
     public static final int MAX_NUMBER = 45;
     public static final int LOTTO_SIZE = 6;
     public static final int PRICE = 1000;
+    public static final double DIVIDE_UNIT = 100;
 
     public static final String DUPLICATE_ELEMENT = "[ERROR] 중복된 요소가 있습니다!";
     public static final String INVALID_SIZE = "[ERROR] 6개의 숫자로 구성되지 않았습니다!";
