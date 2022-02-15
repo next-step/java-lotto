b54c5c7b20f3bd698213a73b25e727831c0aea31
MinChul-Son <smc5236@gmail.com>
2022-02-15 11:42:51 +0900

    🚀 test(lotto): judgeAllUserLotto 메서드에 대한 테스트 코드 작성

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class LottosTest {
@@ -21,4 +25,29 @@ class LottosTest {
         assertThat(lottos.getLottos().size()).isEqualTo(3);
     }
 
+    @Test
+    void 모든_로또의_순위를_판단할_수_있다() {
+        List<Lotto> lottos = new ArrayList<>();
+
+        final List<LottoNumber> lottoNumbers1 = Arrays.asList(1, 2, 3, 4, 5, 6).stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
+        final List<LottoNumber> lottoNumbers2 = Arrays.asList(1, 2, 3, 4, 5, 7).stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
+
+        lottos.add(new Lotto(lottoNumbers1));
+        lottos.add(new Lotto(lottoNumbers2));
+
+        WinningLotto winningLotto = new WinningLotto(new Lotto(lottoNumbers1), new LottoNumber(8));
+
+        Lottos allLottos = new Lottos(lottos);
+
+        List<Ranking> rankings = allLottos.judgeAllUserLotto(winningLotto);
+
+        assertAll(
+            () -> assertThat(rankings).contains(Ranking.FIRST),
+            () -> assertThat(rankings).contains(Ranking.THIRD)
+        );
+    }
 }
