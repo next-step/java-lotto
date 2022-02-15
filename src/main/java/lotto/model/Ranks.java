52b68b80c610a90a2dbf1ff3e94c0cfda3757745
Jiin Lee <loveted0225@naver.com>
2022-02-15 22:56:31 +0900

    refactor: Statistics 일부 기능을 위임하기 위해 Ranks 객체 생성 -Statistics -Ranks

---
+++ src/main/java/lotto/model/Ranks.java
@@ -0,0 +1,52 @@
+package lotto.model;
+
+import static lotto.model.Rank.getRank;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class Ranks {
+    private static Map<Rank, Integer> ranks;
+
+    public Ranks(){
+        initializeRanks();
+    }
+    private void initializeRanks() {
+        ranks = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            putRank(rank);
+        }
+    }
+
+    public Map<Rank, Integer> updateRanks(List<MatchInfo> matchInfos) {
+        for (MatchInfo matchInfo: matchInfos) {
+            int matchCount = matchInfo.getMatchCount();
+            boolean bonusMatch = matchInfo.getBonusMatch();
+            Rank rank = getRank(matchCount, bonusMatch);
+            incrementRank(rank);
+        }
+        return ranks;
+    }
+
+    private void incrementRank(Rank rank) {
+        if (rank != Rank.MISS) {
+            ranks.put(rank, ranks.get(rank) + 1);
+        }
+    }
+
+    private void putRank(Rank rank) {
+        if (rank != Rank.MISS) {
+            ranks.put(rank, 0);
+        }
+    }
+
+    public int calculateTotalPrize(){
+        int totalPrize=0;
+        for (Rank key : ranks.keySet()) {
+            totalPrize += key.getReward()* ranks.get(key);
+        }
+        return totalPrize;
+    }
+
+}
--- src/main/java/lotto/model/Statistics.java
+++ src/main/java/lotto/model/Statistics.java
@@ -1,64 +1,19 @@
 package lotto.model;
 
-import static lotto.model.Rank.getRank;
-
-import java.util.EnumMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
 public class Statistics {
 
     private static final int PRICE_OF_A_LOTTO = 1000;
     private static final int HUNDRED = 100;
-    private static Map<Rank, Integer> ranks;
-    private double totalPrice;
-    private double totalPrize;
 
-    public Statistics(int lottoAmount) {
-        ranks = new EnumMap<>(Rank.class);
-        initializeRanks();
+    private final double totalPrice;
+    private final double totalPrize;
+
+    public Statistics(int lottoAmount, double totalPrize) {
         this.totalPrice = lottoAmount * PRICE_OF_A_LOTTO;
-        this.totalPrize = 0;
-    }
-
-    private void initializeRanks() {
-        for (Rank rank : Rank.values()) {
-            putRank(rank);
-        }
-    }
-
-    public Map<Rank, Integer> updateRanks(List<MatchInfo> matchInfos) {
-        for (MatchInfo matchInfo: matchInfos) {
-            int matchCount = matchInfo.getMatchCount();
-            boolean bonusMatch = matchInfo.getBonusMatch();
-            Rank rank = getRank(matchCount, bonusMatch);
-            incrementRank(rank);
-        }
-        return ranks;
-    }
-
-    private void incrementRank(Rank rank) {
-        if (rank != Rank.MISS) {
-            ranks.put(rank, ranks.get(rank) + 1);
-        }
-    }
-
-    private void putRank(Rank rank) {
-        if (rank != Rank.MISS) {
-            ranks.put(rank, 0);
-        }
-    }
-
-    private void calculateTotalPrize() {
-        for (Rank key : ranks.keySet()) {
-            totalPrize += ranks.get(key);
-        }
+        this.totalPrize = totalPrize;
     }
 
     public double calculateBenefits() {
-        calculateTotalPrize();
         return Math.floor((totalPrize / totalPrice) * HUNDRED) / HUNDRED;
     }
-
 }
