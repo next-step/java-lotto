422de7de89d7b2c2064634bf56cbd79d6dd163b2
Jiin Lee <loveted0225@naver.com>
2022-02-15 23:01:07 +0900

    feat(OutputView): 수동 로또 개수와 자동 로또개수 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lotto.model.Count;
 import lotto.model.Lotto;
 import lotto.model.LottoNumber;
 import lotto.model.Rank;
@@ -14,8 +15,8 @@ public class OutputView {
     private static final String LOSS_GUIDE_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
     private static final String WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---------";
 
-    public static void printLottoCount(int count) {
-        System.out.println(count + "개를 구매했습니다.");
+    public static void printLottoCount(Count count) {
+        System.out.println("수동으로 "+ count.getManualLottoCount()+ "장 자동으로"+ count.getAutoLottoCount()+ "개를 구매했습니다.");
     }
 
     public static void printLottos(List<Lotto> lottos) {
@@ -24,21 +25,19 @@ public class OutputView {
                 Collectors.toList());
             System.out.println(list);
         }
+        System.out.println();
     }
 
-    public static void printStatistics(Map<Rank, Integer> ranks, double benefits) {
+    public static void printStatistics(Map<Rank, Integer> rankMap, double benefits) {
         String bonusMessage;
         System.out.println(WINNING_STATISTICS_MESSAGE);
-        Iterator<Rank> keys = ranks.keySet().iterator();
-        while (keys.hasNext()) {
-            Rank key = keys.next();
+        for (Rank key : rankMap.keySet()) {
             bonusMessage = addMessageForBonus(key);
             System.out.println(
                 key.getMatchCount() + "개 일치" + bonusMessage + "(" + key.getReward() + ")- "
-                    + ranks.get(key)
+                    + rankMap.get(key)
                     + "개");
         }
-
         printBenefits(benefits);
     }
 
@@ -55,6 +54,5 @@ public class OutputView {
             System.out.println(LOSS_GUIDE_MESSAGE);
         }
     }
-
 }
 
