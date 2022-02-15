496c54e72ac1a66d410c70facf1ce26c25722244
Jiin Lee <loveted0225@naver.com>
2022-02-15 23:02:30 +0900

    test: Statistics 테스트 코드 작성

--- src/test/java/lotto/model/StatisticsTest.java
+++ src/test/java/lotto/model/StatisticsTest.java
@@ -2,22 +2,14 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class StatisticsTest {
 
     @Test
-    void 랭킹을_업데이트_한다() {
-        Statistics statistics = new Statistics(2);
-        MatchInfo matchInfo1 = new MatchInfo(5, true);
-        MatchInfo matchInfo2 = new MatchInfo(3, false);
-        Map<Rank, Integer> ranks = statistics.updateRanks(
-            Arrays.asList(matchInfo1,matchInfo2)
-        );
-        assertThat(ranks.get(Rank.SECOND)).isEqualTo(1);
-        assertThat(ranks.get(Rank.FIFTH)).isEqualTo(1);
-        assertThat(ranks.get(Rank.FIRST)).isEqualTo(0);
+    void 수익률을_계산한다(){
+        Statistics statistics = new Statistics(5,50000);
+        double benefits = statistics.calculateBenefits();
+        assertThat(benefits).isEqualTo(10.0);
     }
 }
