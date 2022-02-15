898c5af8b1b476f18b4e1b2770e3850031864bf7
Jiin Lee <loveted0225@naver.com>
2022-02-15 21:28:31 +0900

    test(WinningNumberTest): 보너스볼이 일치하는지 체크하는 테스트 작성

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
@@ -27,5 +28,14 @@ class WinningNumberTest {
         List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
+    }
+
+    @Test
+    void 보너스볼이_있는지_체크(){
+        WinningNumber winningNumber= new WinningNumber(Arrays.asList("2","4","8","15","20","21"), new LottoNumber(41));
+        boolean bonusBallMatch = winningNumber.isContainBonusBall(Arrays.asList(
+            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41), new LottoNumber(15),
+            new LottoNumber(20)));
+        assertThat(bonusBallMatch).isEqualTo(true);
     }
 }
