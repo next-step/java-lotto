b5fc54ae03b1798027ec0d3ef2b7b9dc67ce2a79
MinChul-Son <smc5236@gmail.com>
2022-02-15 09:50:09 +0900

    🚀 refactor(lotto): 사용하지 않는 import 삭제

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
