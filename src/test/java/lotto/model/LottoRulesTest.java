83338ce813fc6ee4841efd551f38ace4581a116d
Jiin Lee <loveted0225@naver.com>
2022-02-15 23:03:24 +0900

    refactor(LottoRulesTest): 불필요한 출력 제거

--- src/test/java/lotto/model/LottoRulesTest.java
+++ src/test/java/lotto/model/LottoRulesTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class LottoRulesTest {
 
-   // @RepeatedTest(100)
-    @Test
+   @RepeatedTest(100)
     void 여섯개의_숫자를_만든다() {
         LottoRules lottoRules = new LottoRules();
         List<LottoNumber> lottoNumbers = lottoRules.makeLottoNumbers();
@@ -17,9 +16,5 @@ class LottoRulesTest {
         for (LottoNumber lottoNumber : lottoNumbers) {
             assertThat(lottoNumber.getNumber()).isBetween(1, 45);
         }
-        for(LottoNumber lottonumber: lottoNumbers){
-            System.out.println(lottonumber.getNumber());
-        }
-
     }
 }
