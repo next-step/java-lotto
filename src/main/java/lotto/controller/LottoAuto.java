d175a3e4c06337b7b11d1be7e994fd6a7b4aca5e
Jiin Lee <loveted0225@naver.com>
2022-02-15 23:04:19 +0900

    refactor(LottoAuto): 로또 수동 구현 및 일부 객체 리팩토링에 따른 컨트롤러 수정

--- src/main/java/lotto/controller/LottoAuto.java
+++ src/main/java/lotto/controller/LottoAuto.java
@@ -3,12 +3,14 @@ package lotto.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.model.Count;
 import lotto.model.Lotto;
 import lotto.model.LottoNumber;
 import lotto.model.LottoRules;
 import lotto.model.Price;
 import lotto.model.Rank;
+import lotto.model.Ranks;
 import lotto.model.Statistics;
 import lotto.model.User;
 import lotto.model.WinningNumber;
@@ -29,23 +31,40 @@ public class LottoAuto {
     public void play() {
         LottoRules lottoRules = new LottoRules();
         Price price = new Price(InputView.inputPrice());
-        Count count = price.calculateCount();
-        OutputView.printLottoCount(count.getCount());
-        User user = new User(createLottos(lottoRules, count));
+        int manualCount =InputView.inputManualLottoCount();
+        Count count = price.calculateCount(manualCount);
+        User user = new User(saveAllLottos(InputView.inputManualLottoNumber(manualCount),lottoRules, count));
+        OutputView.printLottoCount(count);
         OutputView.printLottos(user.getLottos());
         WinningNumber winningNumber = new WinningNumber(InputView.inputWinningNumbers(),
             InputView.inputBonusBall());
-        Statistics statistics = new Statistics(count.getCount());
-        Map<Rank, Integer> ranks = statistics.updateRanks(
-            user.findEachLottoMatchingNumber(winningNumber));
-        OutputView.printStatistics(ranks, statistics.calculateBenefits());
+        Ranks ranks =new Ranks();
+        Map<Rank, Integer> rankMap=ranks.updateRanks(user.findEachLottoMatchingNumber(winningNumber));
+        Statistics statistics = new Statistics(count.getCount(),ranks.calculateTotalPrize());
+        OutputView.printStatistics(rankMap, statistics.calculateBenefits());
     }
-
-    private List<Lotto> createLottos(LottoRules lottoRules, Count count) {
+    private List<Lotto> saveAllLottos(List<List<Integer>> manualLottoNumberInput,LottoRules lottoRules, Count count){
         List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < count.getCount(); i++) {
+        lottos.addAll(createManualLottos(manualLottoNumberInput));
+        lottos.addAll(creatAutoLottos(lottoRules,count));
+        return lottos;
+    }
+    private List<Lotto> creatAutoLottos(LottoRules lottoRules, Count count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count.getAutoLottoCount(); i++) {
             lottos.add(new Lotto(lottoRules.makeLottoNumbers()));
         }
         return lottos;
+    }
+
+    private List<Lotto> createManualLottos(List<List<Integer>> manualLottoNumberInput){
+        List<LottoNumber> lottoNumbers;
+        List<Lotto> manualLottos = new ArrayList<>();
+        for(List<Integer> list: manualLottoNumberInput){
+            lottoNumbers=list.stream().map(LottoNumber::new).collect(
+                Collectors.toList());
+            manualLottos.add(new Lotto(lottoNumbers));
+        }
+        return manualLottos;
     }
 }
