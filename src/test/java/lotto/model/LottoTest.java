fe97c0e2379907652b10181bfd6d9535b71e8b52
Jiin Lee <loveted0225@naver.com>
2022-02-15 21:33:18 +0900

    test(Lotto): 당첨번호와 보너스볼 일치 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -22,4 +22,28 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(Arrays.asList( new LottoNumber(2), new LottoNumber(4),
             new LottoNumber(8), new LottoNumber(41), new LottoNumber(15))));
     }
+
+    @Test
+    void 당첨번호와_로또번호를_비교한다(){
+        Lotto lotto = new Lotto(Arrays.asList(
+            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41), new LottoNumber(15),
+            new LottoNumber(20)
+        ));
+        WinningNumber winningNumber= new WinningNumber(Arrays.asList("2","4","5","15","20","21"), new LottoNumber(9));
+        MatchInfo matchInfo= lotto.matchLottoWithWinningNumber(winningNumber);
+        assertThat(matchInfo.getMatchCount()).isEqualTo(4);
+    }
+
+    @Test
+    void 다섯개의_당첨번호와_보너스볼이_일치한다(){
+        Lotto lotto = new Lotto(Arrays.asList(
+            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41), new LottoNumber(15),
+            new LottoNumber(20)
+        ));
+        WinningNumber winningNumber= new WinningNumber(Arrays.asList("2","4","8","15","20","21"), new LottoNumber(41));
+        MatchInfo matchInfo= lotto.matchLottoWithWinningNumber(winningNumber);
+        assertThat(matchInfo.getMatchCount()).isEqualTo(5);
+        assertThat(matchInfo.getBonusMatch()).isEqualTo(true);
+    }
+
 }
