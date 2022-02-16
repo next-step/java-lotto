87f0f69f3868b7cb18e3476eab6ce00f4260bcd5
YangWoo Yu <yuyw0712@naver.com>
2022-02-16 11:42:45 +0900

    refactor: LottoStatistics.java 수익률을 double이 아닌 BigDecimal 사용
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/lotto/domain/LottoStatistics.java
+++ src/main/java/lotto/domain/LottoStatistics.java
@@ -1,15 +1,17 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class LottoStatistics {
 
     public String getLottoEarningRate(List<Rank> resultStatistics, LottoPrice lottoPrice) {
-        double totalPrice = 0;
+        int totalPrice = 0;
         for (Rank statistics : resultStatistics) {
             totalPrice += statistics.getMoney();
         }
-        return String.format("%.2f", totalPrice / lottoPrice.getPrice());
+        return String.format("%.2f", BigDecimal.valueOf(totalPrice).divide(
+            BigDecimal.valueOf(lottoPrice.getPrice())));
     }
 
 }
