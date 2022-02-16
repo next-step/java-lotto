4e17eaf4cca73d1e51276c2bb790f8454ab956ae
hscom96 <hscom96@gmail.com>
2022-02-17 03:01:09 +0900

    style: 코드 정렬

--- src/main/java/lotto/domain/Rankings.java
+++ src/main/java/lotto/domain/Rankings.java
@@ -49,6 +49,7 @@ public class Rankings {
     public double countRewardRate(int buyPrice) {
         int winnerPriceSum = countWinnerPriceSum();
         return BigDecimal.valueOf(winnerPriceSum)
-            .divide(BigDecimal.valueOf(buyPrice), REWARD_SCALE, RoundingMode.HALF_EVEN).doubleValue();
+            .divide(BigDecimal.valueOf(buyPrice), REWARD_SCALE, RoundingMode.HALF_EVEN)
+            .doubleValue();
     }
 }
