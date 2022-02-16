73e64d7f8d668f260602b36df3ac48cae47f6bc9
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-16 11:58:36 +0900

    feat: 티켓에 전달할 money를 감싸는 클래스 구현
    - 구입 금액은 음의 정수가 될 수 없음을 검증

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+public class Money {
+
+    private static final String NEGATIVE_ERROR_MESSAGE = "구입금액은 음의 정수가 될 수 없습니다.";
+
+    private final int value;
+
+    public Money(final int value) {
+        validateMoney(value);
+        this.value = value;
+    }
+
+    private void validateMoney(final int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(NEGATIVE_ERROR_MESSAGE);
+        }
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
