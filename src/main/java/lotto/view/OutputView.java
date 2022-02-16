3b283e65cde4527d74e3f245b0681c70ae5bc38e
hochan222 <hochan049@gmail.com>
2022-02-16 11:59:42 +0900

    refactor: `+` 연산자로 문자열을 구성하는 것보다는 `String.format`을 이용해보자.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,52 +5,68 @@ import lotto.domain.PrizeGrade;
 
 public class OutputView {
 
-    public static final String ERROR_MESSAGE = "[ERROR] ";
-    private static final String REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
-    private static final String SYSTEM_LOTTO_COUNT = "개를 구매했습니다.";
-    private static final String REQUEST_WINNING_NUMBER_BEFORE = "\n지난 주 당첨 번호를 입력해주세요.(ex 1, 2, 3, 4, 5, 6)";
-    private static final String REQUEST_BONUS_BALL_NUMBER = "보너스 볼을 입력해주세요.";
-    private static final String WINNING_STATS = "\n당첨 통계\n------------";
+    public static final String PRINT_ERROR_MESSAGE_FORMAT = "[ERROR] %s";
+    private static final String PRINT_REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
+    private static final String PRINT_SYSTEM_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+    private static final String PRINT_REQUEST_WINNING_NUMBER_BEFORE = "\n지난 주 당첨 번호를 입력해주세요.(ex 1, 2, 3, 4, 5, 6)";
+    private static final String PRINT_REQUEST_BONUS_BALL_NUMBER = "보너스 볼을 입력해주세요.";
+    private static final String PRINT_WINNING_STATS = "\n당첨 통계\n------------";
+    private static final String PRINT_PRIZE_GRADE_RESULT_FORMAT = "%d개 일치%s(%d원) - %d개";
+    private static final String PRINT_TOTAL_RETURN_FORMAT = "총 수익률은 %f입니다.(기준이 1이기 때문에 결과적으로 %s이라는 의미임)";
+    private static final String PRINT_LOTTO_NUMBER_FORMAT = "%d ";
+    private static final String BONUS_RESULT = ", 보너스 볼 일치";
 
     public static void printExceptionMessage(String exceptionMessage) {
-        System.out.println(ERROR_MESSAGE + exceptionMessage);
+        System.out.println(String.format(PRINT_ERROR_MESSAGE_FORMAT, exceptionMessage));
     }
 
     public static void printRequestLottoPrice() {
-        System.out.println(REQUEST_LOTTO_PRICE);
+        System.out.println(PRINT_REQUEST_LOTTO_PRICE);
     }
 
     public static void printLottoCount(int count) {
-        System.out.println(count + SYSTEM_LOTTO_COUNT);
+        System.out.println(String.format(PRINT_SYSTEM_LOTTO_COUNT_FORMAT, count));
     }
 
     public static void printWinningNumberBefore() {
-        System.out.println(REQUEST_WINNING_NUMBER_BEFORE);
+        System.out.println(PRINT_REQUEST_WINNING_NUMBER_BEFORE);
     }
 
     public static void printBonusBallNumber() {
-        System.out.println(REQUEST_BONUS_BALL_NUMBER);
+        System.out.println(PRINT_REQUEST_BONUS_BALL_NUMBER);
     }
 
     public static void printLottoStatisticTitle() {
-        System.out.println(WINNING_STATS);
+        System.out.println(PRINT_WINNING_STATS);
     }
 
     public static void printLottoStatistic(PrizeGrade prizeGrade, int prizeGradeCount) {
         if (prizeGrade == PrizeGrade.NONE) {
-            return ;
+            return;
         }
         if (prizeGrade == PrizeGrade.SECOND) {
             System.out.println(
-                (prizeGrade.getMatchCount() + 1) + "개 일치, 보너스 볼 일치(" + prizeGrade.getPrizeMoney()
-                    + "원) - " + prizeGradeCount + " 개");
+                String.format(
+                    PRINT_PRIZE_GRADE_RESULT_FORMAT
+                    , prizeGrade.getMatchCount() + 1
+                    , BONUS_RESULT
+                    , prizeGrade.getPrizeMoney()
+                    , prizeGradeCount
+                )
+            );
 
             return;
         }
 
         System.out.println(
-            prizeGrade.getMatchCount() + "개 일치(" + prizeGrade.getPrizeMoney() + "원) - "
-                + prizeGradeCount + " 개");
+            String.format(
+                PRINT_PRIZE_GRADE_RESULT_FORMAT
+                , prizeGrade.getMatchCount()
+                , ""
+                , prizeGrade.getPrizeMoney()
+                , prizeGradeCount
+            )
+        );
     }
 
     public static void printTotalReturn(Double earningRate) {
@@ -61,12 +77,19 @@ public class OutputView {
         }
 
         System.out.println(
-            "총 수익률은 " + earningRate + "입니다.(기준이 1이기 때문에 결과적으로 " + profitOrLoss + "이라는 의미임)");
+            String.format(
+                PRINT_TOTAL_RETURN_FORMAT,
+                earningRate,
+                profitOrLoss
+            )
+        );
     }
 
     public static void printLottoNumber(Lotto lotto) {
         for (int j = 0; j < lotto.getLotto().size(); j++) {
-            System.out.print(lotto.getLotto().get(j).getLottoNumber() + " ");
+            System.out.print(
+                String.format(PRINT_LOTTO_NUMBER_FORMAT, lotto.getLotto().get(j).getLottoNumber())
+            );
         }
     }
 }
