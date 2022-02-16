d241033c90ed5b0f73ca387f4ca507274a090ea3
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-16 11:59:38 +0900

    feat: 넘버리스트를 받아서 수동로또리스트를 구매하는 기능 구현
    rename TicketDealer -> LottoGameManager

--- src/main/java/lotto/domain/LottoGameManager.java
+++ src/main/java/lotto/domain/LottoGameManager.java
@@ -5,23 +5,22 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoNumber;
-import lotto.domain.lotto.Lottos;
 import lotto.domain.machine.LottoGenerator;
 
-public class TicketDealer {
+public class LottoGameManager {
 
     private static final int ONE = 1;
 
-    public static Lottos getLottosByManual(List<Lotto> lottos) {
-        return new Lottos(lottos);
-    }
-
-    public static List<Lotto> getLottosByAuto(Lottos lottos, LottoGenerator lottoGenerator, final int ticketCount) {
+    public static List<Lotto> getLottosByAuto(LottoGenerator lottoGenerator, final int ticketCount) {
         return IntStream.rangeClosed(ONE, ticketCount)
             .mapToObj(index -> lottoGenerator.get().stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toList()))
             .map(Lotto::new)
             .collect(Collectors.toList());
+    }
+
+    public static List<Lotto> getLottosByManual(List<List<Integer>> lottoNumbers) {
+        return LottoSupplier.makeManualLotto(lottoNumbers);
     }
 }
