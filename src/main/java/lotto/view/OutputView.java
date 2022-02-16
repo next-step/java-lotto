7d8a65712104b7a4dfb17652b80a6b5ed368febc
hochan222 <hochan049@gmail.com>
2022-02-16 12:09:58 +0900

    refactor: `System.out.println(String.format)`을 `System.out.printf`으로 바꿔보자.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,19 +5,19 @@ import lotto.domain.PrizeGrade;
 
 public class OutputView {
 
-    public static final String PRINT_ERROR_MESSAGE_FORMAT = "[ERROR] %s";
+    public static final String PRINT_ERROR_MESSAGE_FORMAT = "[ERROR] %s%n";
     private static final String PRINT_REQUEST_LOTTO_PRICE = "구입금액을 입력해 주세요.";
-    private static final String PRINT_SYSTEM_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+    private static final String PRINT_SYSTEM_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.%n";
     private static final String PRINT_REQUEST_WINNING_NUMBER_BEFORE = "\n지난 주 당첨 번호를 입력해주세요.(ex 1, 2, 3, 4, 5, 6)";
     private static final String PRINT_REQUEST_BONUS_BALL_NUMBER = "보너스 볼을 입력해주세요.";
     private static final String PRINT_WINNING_STATS = "\n당첨 통계\n------------";
-    private static final String PRINT_PRIZE_GRADE_RESULT_FORMAT = "%d개 일치%s(%d원) - %d개";
-    private static final String PRINT_TOTAL_RETURN_FORMAT = "총 수익률은 %f입니다.(기준이 1이기 때문에 결과적으로 %s이라는 의미임)";
+    private static final String PRINT_PRIZE_GRADE_RESULT_FORMAT = "%d개 일치%s(%d원) - %d개%n";
+    private static final String PRINT_TOTAL_RETURN_FORMAT = "총 수익률은 %f입니다.(기준이 1이기 때문에 결과적으로 %s이라는 의미임)%n";
     private static final String PRINT_LOTTO_NUMBER_FORMAT = "%d ";
     private static final String BONUS_RESULT = ", 보너스 볼 일치";
 
     public static void printExceptionMessage(String exceptionMessage) {
-        System.out.println(String.format(PRINT_ERROR_MESSAGE_FORMAT, exceptionMessage));
+        System.out.printf(PRINT_ERROR_MESSAGE_FORMAT, exceptionMessage);
     }
 
     public static void printRequestLottoPrice() {
@@ -25,7 +25,7 @@ public class OutputView {
     }
 
     public static void printLottoCount(int count) {
-        System.out.println(String.format(PRINT_SYSTEM_LOTTO_COUNT_FORMAT, count));
+        System.out.printf(PRINT_SYSTEM_LOTTO_COUNT_FORMAT, count);
     }
 
     public static void printWinningNumberBefore() {
@@ -45,27 +45,23 @@ public class OutputView {
             return;
         }
         if (prizeGrade == PrizeGrade.SECOND) {
-            System.out.println(
-                String.format(
-                    PRINT_PRIZE_GRADE_RESULT_FORMAT
-                    , prizeGrade.getMatchCount() + 1
-                    , BONUS_RESULT
-                    , prizeGrade.getPrizeMoney()
-                    , prizeGradeCount
-                )
+            System.out.printf(
+                PRINT_PRIZE_GRADE_RESULT_FORMAT
+                , prizeGrade.getMatchCount() + 1
+                , BONUS_RESULT
+                , prizeGrade.getPrizeMoney()
+                , prizeGradeCount
             );
 
             return;
         }
 
-        System.out.println(
-            String.format(
-                PRINT_PRIZE_GRADE_RESULT_FORMAT
-                , prizeGrade.getMatchCount()
-                , ""
-                , prizeGrade.getPrizeMoney()
-                , prizeGradeCount
-            )
+        System.out.printf(
+            PRINT_PRIZE_GRADE_RESULT_FORMAT
+            , prizeGrade.getMatchCount()
+            , ""
+            , prizeGrade.getPrizeMoney()
+            , prizeGradeCount
         );
     }
 
@@ -76,20 +72,16 @@ public class OutputView {
             profitOrLoss = "이득";
         }
 
-        System.out.println(
-            String.format(
-                PRINT_TOTAL_RETURN_FORMAT,
-                earningRate,
-                profitOrLoss
-            )
+        System.out.printf(
+            PRINT_TOTAL_RETURN_FORMAT
+            , earningRate
+            , profitOrLoss
         );
     }
 
     public static void printLottoNumber(Lotto lotto) {
         for (int j = 0; j < lotto.getLotto().size(); j++) {
-            System.out.print(
-                String.format(PRINT_LOTTO_NUMBER_FORMAT, lotto.getLotto().get(j).getLottoNumber())
-            );
+            System.out.printf(PRINT_LOTTO_NUMBER_FORMAT, lotto.getLotto().get(j).getLottoNumber());
         }
     }
 }
