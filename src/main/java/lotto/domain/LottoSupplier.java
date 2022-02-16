ffe1b39a1e29be64568b595d5b8080ef333f61ad
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-16 11:56:33 +0900

    feat: 수동넘버리스트를 받아 로또리스트로 변환하는 클래스 구현

---
+++ src/main/java/lotto/domain/LottoSupplier.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.lotto.Lotto;
+
+public class LottoSupplier {
+
+    public static List<Lotto> makeManualLotto(List<List<Integer>> lottoNumbers) {
+        final List<Lotto> manualLottos = new ArrayList<>();
+        lottoNumbers.stream()
+            .forEach(lottoNumber -> manualLottos.add(Lotto.from(lottoNumber)));
+        return manualLottos;
+    }
+}
