313517266d0065f4f5d60a3a9ee25cedc721ea04
Jiin Lee <loveted0225@naver.com>
2022-02-18 02:34:31 +0900

    test(Pricetest): Price의 validation 체크 테스트 작성

--- src/test/java/lotto/model/PriceTest.java
+++ src/test/java/lotto/model/PriceTest.java
@@ -1,16 +1,25 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PriceTest {
 
+    @DisplayName("1000원 이하의 금액은 입력될 수 없다")
     @Test
-    void 수동개수에_따라_자동로또_개수를_계산한다() {
+    void validatePrice(){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
+            () -> new Price(500));
+    }
+
+    @Test
+    void 금액에_따라_구입가능한_모든_로또개수를_계산한다() {
         Price price = new Price(5000);
-        int manualLottoCount = 2;
-        Count autoLottoCount = price.calculateCount(manualLottoCount);
-        assertThat(autoLottoCount.getAutoLottoCount()).isEqualTo(3);
+        int totalLottoCount=price.calculateTotalCount();
+        assertThat(totalLottoCount).isEqualTo(5);
     }
 }
