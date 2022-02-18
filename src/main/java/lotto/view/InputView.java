e0f06152ae630c5269fa8a037b6c7d5a814229a8
minsang0850 <minsang0850@naver.com>
2022-02-19 04:04:38 +0900

    refactor(InputView): add excluding zero case function, divide inputManualLottos

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
     private static final String PRICE_INPUT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String NEED_RIGHT_COUNT = "올바른 로또 수를 입력해 주세요.";
-    private static final String WINNING__NUMBER_INPUT_GUIDE_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String WINNING__NUMBER_INPUT_GUIDE_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_BALL_INPUT_GUIDE_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String NEED_OVER_LOTTO_PRICE = "1000원 이상의 금액을 입력해주세요.";
     private static final String NEED_INTEGER = "정수를 입력해주세요.";
@@ -55,16 +55,22 @@ public class InputView {
     }
     
     public static List<Lotto> inputManualLottos(int manualCount) {
+        if (manualCount == 0) {
+            return new ArrayList<>();
+        }
         System.out.println("수동으로 구매할 번호를 입력해주세요.");
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < manualCount; i++) {
-            List<String> numbersString = Arrays.asList(scanner.nextLine().split(", "));
-            List<Integer> numbers = numbersString.stream().map(string -> Integer.parseInt(string))
-              .collect(Collectors.toList());
-            lottos.add(new Lotto(numbers));
+            lottos.add(needManualLottos(lottos, manualCount));
         }
-        System.out.println();
         return lottos;
+    }
+    
+    private static Lotto needManualLottos(List<Lotto> lottos, int manualCount) {
+        List<String> numbersString = Arrays.asList(scanner.nextLine().split(", "));
+        List<Integer> numbers = numbersString.stream().map(string -> Integer.parseInt(string))
+          .collect(Collectors.toList());
+        return new Lotto(numbers);
     }
     
     public static List<Integer> getWinningNumber() {
