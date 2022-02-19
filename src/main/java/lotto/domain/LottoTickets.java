9186e1c9fec90b60f14f783cd13ad7454975d212
parkhyojeong <qkrgywjd456@naver.com>
2022-02-20 00:46:38 +0900

    feat(lottoTickets): 수동로또 추가로직 구현

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -8,26 +8,55 @@ public class LottoTickets {
 
     private static final int LOTTO_PRICE = 1000;
 
-    private final int lottoCounts;
-    private final List<Lotto> lottoTickets;
-    private final LottoGenerator lottoGenerator;
+    private final int randomCounts;
+    private final int manualCount;
+    private final List<Lotto> lottoTickets = new ArrayList<>();
 
-    public LottoTickets(final int price, final LottoGenerator lottoGenerator) {
-        this.lottoCounts = price / LOTTO_PRICE;
-        this.lottoGenerator = lottoGenerator;
-        this.lottoTickets = makeLottoTickets(lottoCounts);
+    public LottoTickets(final int price, final int manualCount,
+        final List<List<Integer>> manualLine) {
+        final int lottoCounts = price / LOTTO_PRICE;
+        this.randomCounts = lottoCounts - manualCount;
+        this.manualCount = manualCount;
+        this.lottoTickets.addAll(makeManualLottoTickets(manualLine));
+        if (randomCounts > 0) {
+            this.lottoTickets.addAll(makeRandomLottoTickets(randomCounts));
+        }
     }
 
-    private List<Lotto> makeLottoTickets(final int lottoCount) {
+    public LottoTickets(final int price, final int manualCount) {
+        final int lottoCounts = price / LOTTO_PRICE;
+        this.randomCounts = lottoCounts - manualCount;
+        this.manualCount = manualCount;
+        this.lottoTickets.addAll(makeRandomLottoTickets(this.randomCounts));
+    }
+
+    private List<Lotto> makeRandomLottoTickets(final int lottoCount) {
+        final List<Lotto> randomLottos = new ArrayList<>();
+        if (lottoCount > 0) {
+            for (int i = 0; i < lottoCount; i++) {
+                randomLottos.add(new Lotto(new RandomLottoGenerator().getLottoGeneratorNumbers()));
+            }
+        }
+        return randomLottos;
+    }
+
+    private List<Lotto> makeManualLottoTickets(
+        List<List<Integer>> manualLine) {
         final List<Lotto> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            lottoTickets.add(new Lotto(lottoGenerator.getLottoGeneratorNumbers()));
+
+        for (List<Integer> line : manualLine) {
+            lottoTickets.add(
+                new Lotto(new ManualLottoGenerator(line).getLottoGeneratorNumbers()));
         }
         return lottoTickets;
     }
 
-    public int getLottoCounts() {
-        return this.lottoCounts;
+    public int getRandomLottoCounts() {
+        return this.randomCounts;
+    }
+
+    public int getManualCount() {
+        return this.manualCount;
     }
 
     public List<Lotto> getLottoTickets() {
@@ -40,6 +69,4 @@ public class LottoTickets {
             winningResult.compareLottoAndBonusNumbers(lottoMachine))).collect(
             Collectors.toList());
     }
-
-
 }
