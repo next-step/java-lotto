0126bb1930b8e40255669583c227f4fdbe70b6dc
parkhyojeong <qkrgywjd456@naver.com>
2022-02-20 00:48:05 +0900

    feat(main): main 와 컨트롤러 분리

--- src/main/java/lotto/LottoProcessor.java
+++ src/main/java/lotto/LottoProcessor.java
@@ -1,31 +1,16 @@
 package lotto;
 
-import lotto.domain.LottoMachine;
-import lotto.domain.LottoPlay;
-import lotto.domain.RandomLottoGenerator;
+import lotto.controller.LottoController;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
 public class LottoProcessor {
 
-    private static final InputView inputView = new InputView();
-    private static final ResultView resultView = new ResultView();
-
     public static void main(String[] args) {
+        final InputView inputView = new InputView();
+        final ResultView resultView = new ResultView();
+        final LottoController lottoController = new LottoController(inputView, resultView);
 
-        try {
-            final LottoPlay lottoPlay = new LottoPlay(inputView.getTotalPrice(),
-                new RandomLottoGenerator());
-            resultView.printNumberOfLotto(lottoPlay.getLottoCounts());
-            resultView.printLottoTickets(lottoPlay.getLottoTickets());
-
-            lottoPlay.run(new LottoMachine(inputView.getWinNumbers(), inputView.getBonusNumber()));
-
-            resultView.printAnalyzeResults(lottoPlay.getWinningPrices(),
-                lottoPlay.calculateProfitPercent());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
+        lottoController.run();
     }
 }
