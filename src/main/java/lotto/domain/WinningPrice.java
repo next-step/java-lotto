205b29cdaa0aa91068a8c8f30a48ea1789f012e0
parkhyojeong <qkrgywjd456@naver.com>
2022-02-20 00:38:37 +0900

    style: 코드 정렬

--- src/main/java/lotto/domain/WinningPrice.java
+++ src/main/java/lotto/domain/WinningPrice.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-
 import java.util.Arrays;
 import java.util.Objects;
 
--- src/test/java/stringcalculator/SeparatorTest.java
+++ src/test/java/stringcalculator/SeparatorTest.java
@@ -12,7 +12,7 @@ class SeparatorTest {
 
     @DisplayName("사용자 입력으로 들어온 문자열에서 콤마(,),콜론(:)을 제거")
     @ParameterizedTest
-    @ValueSource(strings = {"//.\n123,456:789.10","//@\n123,456:789@10","//$\n123,456:789$10"})
+    @ValueSource(strings = {"//.\n123,456:789.10", "//@\n123,456:789@10", "//$\n123,456:789$10"})
     void 사용자_입력으로_들어온_문자열에서_콤마_콜론_을_기준으로_자름(final String userInput) {
         Separator separator = new Separator(userInput);
         assertThat(separator.split())
