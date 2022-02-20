68c309ef256f2e411926d8dbd080c817a033a282
Jiin Lee <loveted0225@naver.com>
2022-02-20 21:27:04 +0900

    feat: Lotto equals 함수 오버라이딩

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Lotto {
 
@@ -39,6 +40,23 @@ public class Lotto {
 
     public List<LottoNumber> getLottoNumbers() {
         return lottoNumbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
     }
 
 }
