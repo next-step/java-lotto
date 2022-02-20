342bae865adac0443cd36f3fff4d3e75f8c51f69
Jiin Lee <loveted0225@naver.com>
2022-02-20 21:25:49 +0900

    test(ManualLottoTest): ManualLotto 생성 및 저장 메서드 테스트코드 작성

---
+++ src/test/java/lotto/model/ManualLottoTest.java
@@ -0,0 +1,30 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ManualLottoTest {
+
+    @Test
+    void 수동_로또_번호는_중복되선_안된다(){
+        assertThatThrownBy(()->new ManualLotto(
+            Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5),new LottoNumber(5))));
+    }
+
+    @Test
+    void 수동_로또_번호를_저장한다(){
+        ManualLotto manualLotto = new ManualLotto(
+            Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+                new LottoNumber(4), new LottoNumber(5),new LottoNumber(6)));
+        List<Lotto> manualLottos = new ArrayList<>();
+        manualLottos = manualLotto.addManualLotto(manualLottos);
+        assertThat(manualLottos).containsExactly(new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
+            new LottoNumber(4), new LottoNumber(5),new LottoNumber(6))));
+    }
+}
