983a26c0935ec7033f663fd919c5f8d98f3ffbab
Jiin Lee <loveted0225@naver.com>
2022-02-20 21:41:39 +0900

    style(UserTest): 불필요한 line 삭제

--- src/test/java/lotto/model/UserTest.java
+++ src/test/java/lotto/model/UserTest.java
@@ -18,9 +18,7 @@ class UserTest {
             new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
                 new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)))
         );
-
         User user = new User(lottos);
-
         assertThat(user.getLottos()).hasSize(2);
     }
 
