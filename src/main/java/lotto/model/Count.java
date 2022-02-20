8f209fb3bacb882b795d16915d79904be42ca42d
Jiin Lee <loveted0225@naver.com>
2022-02-20 14:24:38 +0900

    feat(Count): 음수값에 대한 validation 체크 구현

--- src/main/java/lotto/model/Count.java
+++ src/main/java/lotto/model/Count.java
@@ -1,16 +1,20 @@
 package lotto.model;
 
 public class Count {
-
+    private static final String PRICE_CANNOT_BE_NEGATIVE ="로또의 개수는 음수가 될 수 없습니다.";
     private final int autoLottoCount;
     private final int manualLottoCount;
 
     public Count(int totalLottoCount, int manualLottoCount) {
-
+        validateCount(manualLottoCount);
         this.autoLottoCount = totalLottoCount-manualLottoCount;
         this.manualLottoCount = manualLottoCount;
     }
-
+    private void validateCount(int manualLottoCount){
+        if(manualLottoCount<0){
+            throw new IllegalArgumentException(PRICE_CANNOT_BE_NEGATIVE);
+        }
+    }
     public int getCount() {
         return autoLottoCount + manualLottoCount;
     }
