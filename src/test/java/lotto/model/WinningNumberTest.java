6a6f859d349f17c765f0a2531515581faea90008
Jiin Lee <loveted0225@naver.com>
2022-02-20 21:29:59 +0900

    refactor(WinningNumberTest): WinningNumber 생성자 및 메소드 파라미터 타입 변경에 따른 테스트코드 변경

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -11,21 +11,22 @@ class WinningNumberTest {
 
     @Test
     void 중복된_숫자가_들어오면_예외를_발생하는지_확인() {
-        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "5");
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,5);
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
     }
 
     @Test
     void 다섯개의_숫자가_들어오면_예외를_발생하는지_확인() {
-        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
     }
 
     @Test
     void 일곱개의_숫자가_들어오면_예외를_발생하는지_확인() {
-        List<String> numbers = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7);
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
     }
@@ -33,7 +34,7 @@ class WinningNumberTest {
     @Test
     void 보너스볼이_있는지_체크() {
         WinningNumber winningNumber = new WinningNumber(
-            Arrays.asList("2", "4", "8", "15", "20", "21"), new LottoNumber(41));
+            Arrays.asList(2, 4, 8, 15, 20, 21), new LottoNumber(41));
         boolean bonusBallMatch = winningNumber.isContainBonusBall(Arrays.asList(
             new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41),
             new LottoNumber(15),
