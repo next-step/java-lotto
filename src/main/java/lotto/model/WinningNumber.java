6dcd134115e3fc16c2f27ec72734a3bb8a2dbfc9
Jiin Lee <loveted0225@naver.com>
2022-02-20 21:28:46 +0900

    refactor: WinningNumber 생성자 및 메소드 파라미터 타입 변경
    - validateWinningNumbers
    - checkDuplicateWinningNumbers

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,41 +1,44 @@
 package lotto.model;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WinningNumber {
 
     private static final int WINNING_NUMBERS_FIXED_AMOUNT = 6;
     private static final String WINNING_NUMBERS_ERROR_MESSAGE = "당첨 번호는 6개입니다.";
     private static final String DUPLICATE_WINNING_NUMBERS_MESSAGE = "중복된 당첨번호입니다.";
+    private static final String BONUS_BALL_EXISTS_IN_WINNING_NUMBERS= "보너스볼이 이미 당첨번호에 있습니다";
 
     private final List<LottoNumber> winningNumbers;
     private final LottoNumber bonusBall;
 
-    public WinningNumber(List<String> winningNumbersInput, LottoNumber bonusBall) {
-        winningNumbers = new ArrayList<>();
-        validateWinningNumbers(winningNumbersInput);
-        parseStringToInt(winningNumbersInput);
+    public WinningNumber(List<Integer> winningNumbers, LottoNumber bonusBall) {
+        validateWinningNumbers(winningNumbers);
+        checkDuplicateWinningNumber(winningNumbers);
+        this.winningNumbers =  winningNumbers.stream().map(LottoNumber::new).collect(Collectors.toList());
+        isBonusBallInWinningNumbers(bonusBall);
         this.bonusBall = bonusBall;
     }
 
-    private void validateWinningNumbers(List<String> winningNumbersInput) {
+    private void validateWinningNumbers(List<Integer> winningNumbersInput) {
         if (winningNumbersInput.size() != WINNING_NUMBERS_FIXED_AMOUNT) {
             throw new IllegalArgumentException(WINNING_NUMBERS_ERROR_MESSAGE);
         }
     }
 
-    private void checkDuplicateWinningNumber(int winningNumber) {
-        if (winningNumbers.contains(new LottoNumber(winningNumber))) {
+    private void checkDuplicateWinningNumber(List<Integer> winningNumbers) {
+        Set<Integer> checkSet = new HashSet<>(winningNumbers);
+        if(checkSet.size()<winningNumbers.size()){
             throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBERS_MESSAGE);
         }
     }
 
-    private void parseStringToInt(List<String> winningNumbersInput) {
-        for (int i = 0; i < winningNumbersInput.size(); i++) {
-            int parsedWinningNumber = Integer.parseInt(winningNumbersInput.get(i));
-            checkDuplicateWinningNumber(parsedWinningNumber);
-            winningNumbers.add(new LottoNumber(parsedWinningNumber));
+    private void isBonusBallInWinningNumbers(LottoNumber bonusBall){
+        if(winningNumbers.contains(bonusBall)){
+            throw new IllegalArgumentException(BONUS_BALL_EXISTS_IN_WINNING_NUMBERS);
         }
     }
 
