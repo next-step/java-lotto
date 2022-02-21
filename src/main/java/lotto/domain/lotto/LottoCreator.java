0e425a83f0779f1b796653ea2ccdb644de39fcad
eohaerim <ehl3288@naver.com>
2022-02-22 02:54:23 +0900

    refactor: create lottoCreator interface, RandomLottoCreator

--- src/main/java/lotto/domain/lotto/LottoCreator.java
+++ src/main/java/lotto/domain/lotto/LottoCreator.java
@@ -1,8 +1,6 @@
 package lotto.domain.lotto;
 
-import java.util.List;
-
 public interface LottoCreator {
 
-    List<Lotto> getLottos();
+    Lotto createLotto();
 }
--- src/main/java/lotto/domain/lotto/RandomLottoCreator.java
+++ src/main/java/lotto/domain/lotto/RandomLottoCreator.java
@@ -6,34 +6,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class RandomLottoCreator implements LottoCreator{
+public class RandomLottoCreator implements LottoCreator {
 
-    private List<Lotto> lottos;
     private final List<LottoNumber> numberData =
         IntStream.rangeClosed(LottoNumber.LOTTO_FIRST_NUMBER, LottoNumber.LOTTO_LAST_NUMBER)
             .boxed()
             .map(LottoNumber::new)
             .collect(Collectors.toList());
 
-    public RandomLottoCreator(int quantity) {
-        this.lottos = createLottos(quantity);
-    }
-
-    private List<Lotto> createLottos(int quantity) {
-        lottos = new ArrayList<>();
-        for(int i = 0; i  < quantity; i++) {
-            lottos.add(createLotto());
-        }
-        return lottos;
-    }
-
-    private Lotto createLotto() {
+    @Override
+    public Lotto createLotto() {
         Collections.shuffle(numberData);
         return new Lotto(new ArrayList<>(numberData.subList(0, Lotto.LOTTO_NUMBERS_SIZE)));
-    }
-
-    @Override
-    public List<Lotto> getLottos() {
-        return null;
     }
 }
