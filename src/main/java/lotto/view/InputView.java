6761e20232a98be05f3a75bf3f582f7fb1eee35a
eohaerim <ehl3288@naver.com>
2022-02-22 03:04:56 +0900

    refactor: delete LottoInitInfo

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,24 +1,23 @@
 package lotto.view;
 
 import java.util.Scanner;
-import lotto.domain.user.LottoInitInfo;
-import lotto.domain.winning.WinningBalls;
+import lotto.domain.winning.WinningNumbers;
 
 public class InputView {
 
     private final static Scanner scanner = new Scanner(System.in);
 
-    public static LottoInitInfo getLottoInitInfo() {
+    public static int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        return new LottoInitInfo(Integer.parseInt(scanner.nextLine()));
+        return Integer.parseInt(scanner.nextLine());
     }
 
-    public static WinningBalls getWinningBalls() {
+    public static WinningNumbers getWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String input = scanner.nextLine();
         String[] numbers = input.split(", ");
 
         System.out.println("보너스 볼을 입력해 주세요.");
-        return new WinningBalls(numbers, scanner.nextLine());
+        return new WinningNumbers(numbers, scanner.nextLine());
     }
 }
