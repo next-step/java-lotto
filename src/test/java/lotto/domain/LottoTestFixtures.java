3be9b2610606d4077bbed7d4ea482c6af356f6e5
eohaerim <ehl3288@naver.com>
2022-02-22 03:32:56 +0900

    test: create LottoMachineTest

--- src/test/java/lotto/domain/LottoTestFixtures.java
+++ src/test/java/lotto/domain/LottoTestFixtures.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoCreator;
 import lotto.domain.lotto.LottoNumber;
 
 public class LottoTestFixtures {
@@ -24,4 +26,20 @@ public class LottoTestFixtures {
         new LottoNumber(4),
         new LottoNumber(4)
     );
+
+    public static final List<LottoNumber> NORMAL_LOTTO_NUMBER = Arrays.asList(
+        new LottoNumber(1),
+        new LottoNumber(2),
+        new LottoNumber(3),
+        new LottoNumber(4),
+        new LottoNumber(5),
+        new LottoNumber(6)
+    );
+
+    public static final LottoCreator FIXED_LOTTO_CREATOR = new LottoCreator() {
+        @Override
+        public Lotto createLotto() {
+            return new Lotto(NORMAL_LOTTO_NUMBER);
+        }
+    };
 }
