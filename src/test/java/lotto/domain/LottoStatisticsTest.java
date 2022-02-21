14a4a552ecd3a75d8348205a56e9f119dbdbc282
YangWoo Yu <yuyw0712@naver.com>
2022-02-21 15:16:14 +0900

    test: LottoStatisticsTest.java 랭킹 정보를 세는 테스트 코드 구현
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/test/java/lotto/domain/LottoStatisticsTest.java
+++ src/test/java/lotto/domain/LottoStatisticsTest.java
@@ -1,10 +1,11 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,18 @@ class LottoStatisticsTest {
         List<Rank> ranks = new ArrayList<>();
         LottoStatistics lottoStatistics = new LottoStatistics(ranks);
         ranks.add(Rank.FIRST);
-        assertThat(lottoStatistics.getLottoEarningRate(new LottoPrice(5000))).isEqualTo("400000.00");
+        assertThat(lottoStatistics.getLottoEarningRate(new LottoPrice(5000))).isEqualTo(
+            "400000.00");
+    }
+
+    @DisplayName(value = "랭킹 정보를 셀 수 있다")
+    @Test
+    void makeRankReportTest() {
+        LottoStatistics lottoStatistics = new LottoStatistics(
+            Arrays.asList(Rank.FIRST, Rank.SECOND, Rank.SECOND));
+        Map<Rank, Integer> rankingReport = lottoStatistics.makeRankReport();
+        assertThat(rankingReport.get(Rank.SECOND)).isEqualTo(2);
+        assertThat(rankingReport.get(Rank.FIRST)).isEqualTo(1);
+        assertThat(rankingReport.get(Rank.THIRD)).isEqualTo(0);
     }
 }
