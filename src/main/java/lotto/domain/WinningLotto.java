48ce8f6f7a3e9bae285ad557c989f651e9a64210
gal2o <hunmin8788@gmail.com>
2022-02-21 20:13:16 +0900

    refactor: WinningLotto는 당첨 로또 번호만을 갖는 클래스로 리팩토링

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,52 +1,20 @@
 package lotto.domain;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class WinningLotto {
 
-    private static Map<Statistics, Integer> winResult;
+    private final List<LottoNumber> winLotto;
 
-    static {
-        winResult = new HashMap<>();
+    public WinningLotto(Lotto lotto) {
+        this.winLotto = lotto.getLotto();
     }
 
-    public WinningLotto(Map<Statistics, Integer> winResult) {
-        WinningLotto.winResult = winResult;
+    public static WinningLotto create(List<LottoNumber> lotto) {
+        return new WinningLotto(new Lotto(lotto));
     }
 
-    public static WinningLotto getMatchWin(Lottos lottos, LottoResult lottoResult) {
-        lottos.getLottos().forEach(
-            lotto -> matchCounting(lottoResult.matchNumbersCount(lotto), lottoResult.isBonusNumberMatch(lotto)));
-
-        return new WinningLotto(winResult);
-    }
-
-    public Map<Statistics, Integer> winResult() {
-        return winResult;
-    }
-
-    public static int getWinProfit() {
-        int totalIncome = 0;
-
-        for (Statistics statistics : Statistics.values()) {
-            totalIncome += (winResult.getOrDefault(statistics, 0) * statistics.getMoney());
-        }
-
-        return totalIncome;
-    }
-
-    public static void matchCounting(int match, boolean bonus) {
-        if (match < Statistics.FIFTH.match) {
-            return;
-        }
-
-        Statistics statistics = Statistics.getReward(match, bonus);
-
-        winResult.put(statistics, findStatistics(statistics) + 1);
-    }
-
-    public static int findStatistics(Statistics statistics) {
-        return winResult.getOrDefault(statistics, 0);
+    public List<LottoNumber> getWinLotto() {
+        return winLotto;
     }
 }
