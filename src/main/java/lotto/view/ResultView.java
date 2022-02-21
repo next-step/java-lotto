90c2493d114ebe1ac8037452bb708c19d2e4b7fb
gal2o <hunmin8788@gmail.com>
2022-02-21 20:17:23 +0900

    refactor: String.format을 사용하여 출력 리팩토링

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,12 +1,16 @@
 package lotto.view;
 
-import java.util.List;
+import java.util.stream.Collectors;
 import lotto.domain.Lotto;
-import lotto.domain.Statistics;
-import lotto.domain.WinningLotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+import lotto.domain.Money;
+import lotto.domain.Rank;
 
 public class ResultView {
 
+    private static final String DELIMETER = ", ";
     private static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String LOTTO_RESULT = "당첨 통계";
     private static final String DIVISION = "================";
@@ -22,31 +26,53 @@ public class ResultView {
         System.out.println(lottoCount + LOTTO_COUNT_MESSAGE);
     }
 
-    public static void printLottos(List<Lotto> lottos) {
-        lottos.forEach(lotto -> System.out.println(lotto.getLotto()));
+    public static void printLottos(LottoTicket lottos) {
+        lottos.getLottoTicket()
+            .forEach(lotto -> System.out.println("[" + lottoNumberToString(lotto) + "]"));
     }
 
-    public static void printWin(WinningLotto winning) {
+    private static String lottoNumberToString(Lotto lotto) {
+        return lotto.getLotto().stream()
+            .map(LottoNumber::getLottoNumber)
+            .sorted()
+            .map(String::valueOf)
+            .collect(Collectors.joining(DELIMETER));
+    }
+
+    public static void printWin(LottoResult lottoResult, Money money) {
         System.out.println(LOTTO_RESULT);
         System.out.println(DIVISION);
 
-        for (Statistics statistics : Statistics.values()) {
-            printResult(statistics, winning);
+        for (Rank rank : Rank.values()) {
+            System.out.print(printResult(rank, lottoResult));
         }
+
+        printProfit(lottoResult, money);
     }
 
-    private static void printResult(Statistics statistics, WinningLotto winning) {
+    private static String printResult(Rank rank, LottoResult lottoResult) {
         String bonusBall = "";
 
-        if (statistics.isSecond()) {
+        if (rank.isNothing()) {
+            return String.format("꽝 입니다 (%d원)- %d개\n", rank.getMoney(),
+                lottoResult.findStatistics(rank));
+        }
+
+        if (rank.isSecond()) {
             bonusBall = BONUS_BALL_MATCH;
         }
 
-        System.out.printf("%d개 일치" + bonusBall + "(%d원)- %d개\n", statistics.getMatch(),
-            statistics.getMoney(), winning.winResult().getOrDefault(statistics, 0));
+        return String.format("%d개 일치 %s (%d원)- %d개\n", rank.getMatch(), bonusBall,
+            rank.getMoney(), lottoResult.findStatistics(rank));
     }
 
-    public static void printProfit(int money) {
-        System.out.printf(TOTAL_INCOME_PRE + "%.2f" + TOTAL_INCOME_POST + "\n", (float) WinningLotto.getWinProfit() / money);
+    private static void printProfit(LottoResult lottoResult, Money money) {
+        String result = String.format("%s %.2f%s\n",
+            TOTAL_INCOME_PRE,
+            (float) lottoResult.getWinProfit() / money.getMoney(),
+            TOTAL_INCOME_POST
+        );
+
+        System.out.printf(result);
     }
 }
