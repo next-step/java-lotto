080a8d57e8d962dc9e2db7573737a19a62ede145
eohaerim <ehl3288@naver.com>
2022-02-22 03:28:10 +0900

    test: change static import of asserThat

--- src/test/java/lotto/domain/number/LottoNumberTest.java
+++ src/test/java/lotto/domain/number/LottoNumberTest.java
@@ -1,7 +1,8 @@
 package lotto.domain.number;
 
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.domain.lotto.LottoNumber;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,8 +11,8 @@ class LottoNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 100})
     void 로또넘버의_범위가_1과_45_사이가_아니라면_예외처리(int number) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new LottoNumber(number))
-            .withMessage("[ERROR] 로또 번호는 1-45 이내의 숫자여야 합니다.");
+        assertThatThrownBy(() -> new LottoNumber(number))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 로또 번호는 1-45 이내의 숫자여야 합니다.");
     }
 }
