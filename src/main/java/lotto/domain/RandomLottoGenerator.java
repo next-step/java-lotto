93dc1cf889bb5ad1cd8b6e4ddbfb17abd625c486
parkhyojeong <qkrgywjd456@naver.com>
2022-02-22 00:45:53 +0900

    refactor(randomLottoGenerator): 로또 길이 유효성 검사 확인

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,12 +21,20 @@ public class RandomLottoGenerator implements LottoGenerator {
         Collections.shuffle(lottoRange);
         List<Integer> subNumbers = lottoRange.stream().limit(COUNT).sorted()
             .collect(Collectors.toList());
+        if (lottoLengthValidation(subNumbers)) {
+            throw new IllegalArgumentException("6개의 로또가 필요합니다");
+        }
         this.lottoNumbers = subNumbers.stream().map(LottoNumber::new).collect(
             Collectors.toList());
     }
 
+    private boolean lottoLengthValidation(List<Integer> line) {
+        return line.size() != COUNT;
+    }
+
     @Override
     public List<LottoNumber> getLottoGeneratorNumbers() {
-        return new ArrayList<>(lottoNumbers);
+        return lottoNumbers;
     }
+
 }
