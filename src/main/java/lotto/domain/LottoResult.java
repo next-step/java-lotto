bab417033fd39451022ac978e8c4522ae104c860
gal2o <hunmin8788@gmail.com>
2022-02-21 20:14:14 +0900

    refactor: LottoResult 클래스는 로또 당첨 결과만을 담당하도록 리팩토링

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,61 +1,29 @@
 package lotto.domain;
 
-import java.util.HashSet;
-import java.util.List;
-import lotto.util.Parser;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LottoResult {
 
-    private final List<Integer> numberInput;
-    private final String bonusInput;
-    private final int START_NUMBER = 1;
-    private final int LAST_NUMBER = 45;
-    private final int LOTTO_LIMIT = 6;
-    private final String WIN_BALL_INVALID = "올바른 당첨 번호 입력이 아닙니다.";
-    private final String BONUS_BALL_INVALID = "올바른 보너스 볼 입력이 아닙니다.";
+    private final Map<Rank, Integer> lottoResult = new HashMap<>();
 
-    public LottoResult(String numberInput, String bonusInput) {
-        this.numberInput = Parser.splitInput(numberInput);
-        this.bonusInput = bonusInput;
-        validateWinNumbers();
-        validateBonusNumber();
-    }
+    public int getWinProfit() {
+        int totalIncome = 0;
 
-    public int matchNumbersCount(Lotto lotto) {
-        return (int) lotto.getLotto().stream().filter(numberInput::contains).count();
-    }
-
-    public boolean isBonusNumberMatch(Lotto lotto) {
-        return lotto.getLotto().contains(Integer.parseInt(bonusInput));
-    }
-
-    private void validateWinNumbers() {
-        HashSet<Integer> duplicate = new HashSet<>(numberInput);
-
-        if (duplicate.size() != LOTTO_LIMIT) {
-            throw new IllegalArgumentException(WIN_BALL_INVALID);
+        for (Rank rank : Rank.values()) {
+            totalIncome += (findStatistics(rank) * rank.getMoney());
         }
 
-        if (numberInput.size() > LOTTO_LIMIT) {
-            throw new IllegalArgumentException(WIN_BALL_INVALID);
-        }
-
-        numberInput.forEach(number -> {
-            if (number < START_NUMBER || number > LAST_NUMBER) {
-                throw new IllegalArgumentException(WIN_BALL_INVALID);
-            }
-        });
+        return totalIncome;
     }
 
-    private void validateBonusNumber() {
-        int bonusNumber = Integer.parseInt(bonusInput);
+    public void matchCounting(int match, boolean bonus) {
+        Rank rank = Rank.getReward(match, bonus);
 
-        if (bonusNumber < START_NUMBER || bonusNumber > LAST_NUMBER) {
-            throw new IllegalArgumentException(BONUS_BALL_INVALID);
-        }
+        lottoResult.put(rank, findStatistics(rank) + 1);
+    }
 
-        if (numberInput.contains(bonusNumber)) {
-            throw new IllegalArgumentException(BONUS_BALL_INVALID);
-        }
+    public int findStatistics(Rank rank) {
+        return lottoResult.getOrDefault(rank, 0);
     }
 }
