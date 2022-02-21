1c2a4684d68907acfc83c868b121762372346336
eohaerim <ehl3288@naver.com>
2022-02-22 02:53:09 +0900

    refactor: create getter in Lotto

--- src/main/java/lotto/domain/lotto/Lotto.java
+++ src/main/java/lotto/domain/lotto/Lotto.java
@@ -1,10 +1,12 @@
 package lotto.domain.lotto;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
+
     public static final int LOTTO_NUMBERS_SIZE = 6;
 
     private final List<LottoNumber> numbers;
@@ -33,5 +35,9 @@ public class Lotto {
         if (set.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException("[ERROR] 로또 넘버에 중복이 있습니다.");
         }
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return new ArrayList<>(numbers);
     }
 }
