0b527c8879094cd3540e17cf498c8460c94b2a42
fpal95 <snc7303@gmail.com>
2022-02-22 22:37:45 +0900

    Refactor(OutputView): String.format적용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,22 +11,20 @@ import lotto.domain.lotto.Rank;
 public class OutputView {
 
     private static final String SPLITTER = ", ";
-    private static final String MESSAGE_BOUGHT_TICKET = "개를 구매했습니다.";
+    private static final String MESSAGE_BOUGHT_TICKET = "%d 개를 구매했습니다.";
     private static final String MESSAGE_TICKET_START = "[";
     private static final String MESSAGE_TICKET_END = "]";
     private static final String MESSAGE_STATISTICS_TITLE = "당첨 통계";
     private static final String MESSAGE_STATISTICS_DIVIDER = "---------";
-    private static final String MESSAGE_RESULT_START = "총 수익률은 ";
-    private static final String MESSAGE_RESULT_END = "입니다.";
-    private static final String MESSAGE_RANK_START = "개 일치 (";
-    private static final String MESSAGE_RANK_BONUS_START = "개 일치, 보너스 볼 일치 (";
-    private static final String MESSAGE_RANK_MIDDLE = "원) - ";
-    private static final String MESSAGE_RANK_BONUS_END = "개";
+    private static final String MESSAGE_RESULT = "총 수익률은 %.2f 입니다.";
+    private static final String MESSAGE_RANK = "%d개 일치 (%d원)- %d개";
+    private static final String MESSAGE_RANK_BONUS = "%d개 일치, 보너스 볼 일치(%d원)- %d개";
+
 
     private OutputView() {}
 
     public static void printBuyingTickets(final int amount) {
-        System.out.println(amount + MESSAGE_BOUGHT_TICKET);
+        System.out.println(String.format(MESSAGE_BOUGHT_TICKET, amount));
     }
 
     public static void printLottoTicket(final LottoNumbers numbers) {
@@ -49,7 +47,7 @@ public class OutputView {
     }
 
     public static void printResult(final BigDecimal calculateRatio) {
-        System.out.println(MESSAGE_RESULT_START + calculateRatio + MESSAGE_RESULT_END);
+        System.out.println(String.format(MESSAGE_RESULT, calculateRatio));
     }
 
     private static void printRank(final Rank rank, final Map<Rank, Integer> prizeMap) {
@@ -62,10 +60,10 @@ public class OutputView {
             return;
         }
 
-        System.out.println(rank.getMatches() + MESSAGE_RANK_START + rank.getPrize() + MESSAGE_RANK_MIDDLE + prizeMap.getOrDefault(rank, 0) + MESSAGE_RANK_BONUS_END);
+        System.out.println(String.format(MESSAGE_RANK, rank.getMatches(),rank.getPrize(),prizeMap.getOrDefault(rank, 0)));
     }
 
     private static void printBonusRank(final Rank rank, final Map<Rank, Integer> prizeMap) {
-        System.out.println(rank.getMatches() + MESSAGE_RANK_BONUS_START + rank.getPrize() + MESSAGE_RANK_MIDDLE + prizeMap.getOrDefault(rank, 0) + MESSAGE_RANK_BONUS_END);
+        System.out.println(String.format(MESSAGE_RANK_BONUS, rank.getMatches(),rank.getPrize(),prizeMap.getOrDefault(rank, 0)));
     }
 }
