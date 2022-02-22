505fd4043f753527fc5a84ba8814f1a3954b48da
parkhyojeong <qkrgywjd456@naver.com>
2022-02-22 18:11:09 +0900

    feat(lotto): 장당 로또 생성시 유효성 검사 확인
    - lottoLengthValidation() 로또 개수 검사
    - lottoDuplication() 중복 로또 검사

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,23 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
 
+    public static final int COUNT = 6;
+
     private final List<LottoNumber> lottoNumbers;
 
     public Lotto(final List<LottoNumber> lottoNumbers) {
+        if (lottoLengthValidation(lottoNumbers)) {
+            throw new IllegalArgumentException("6개의 로또가 필요합니다");
+        }
+        if (lottoDuplication(lottoNumbers)) {
+            throw new RuntimeException("6개의 로또가 필요합니다");
+        }
+
         this.lottoNumbers = lottoNumbers;
     }
 
@@ -23,5 +33,14 @@ public class Lotto {
 
     public List<LottoNumber> getLottoNumbers() {
         return new ArrayList<>(lottoNumbers);
+    }
+
+    private boolean lottoLengthValidation(List<LottoNumber> line) {
+        return line.size() != COUNT;
+    }
+
+    private boolean lottoDuplication(List<LottoNumber> lottoLine) {
+        HashSet<LottoNumber> checkDuplicationValue = new HashSet<>(lottoLine);
+        return checkDuplicationValue.size() != 6;
     }
 }
