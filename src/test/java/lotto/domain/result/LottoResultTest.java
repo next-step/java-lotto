297e815eaa74de03f57d3c274955423c3c77284a
eohaerim <ehl3288@naver.com>
2022-02-22 18:06:02 +0900

    test: create LottoResultTest

--- src/test/java/lotto/domain/result/LottoResultTest.java
+++ src/test/java/lotto/domain/result/LottoResultTest.java
@@ -1,45 +1,49 @@
 package lotto.domain.result;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import lotto.dto.WinningInfo;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoResultTest {
 
     @Test
-    void 당첨결과를_찾지_못할_경우_예외처리() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> LottoResult.of(new WinningInfo(7, false)))
-            .withMessage("[ERROR] 당첨 결과를 찾지 못했습니다.");
-    }
-
-    @Test
-    void 보너스볼과_관련_없는_당첨_확인_3_4_6() {
-
+    void 당첨개수_3개_미만은_보너스와_상관없이_보상이_없다() {
         assertAll(
-            () -> assertThat(LottoResult.of(new WinningInfo(3, true)))
-                .isEqualTo(LottoResult.THREE_MATCHING),
-            () -> assertThat(LottoResult.of(new WinningInfo(4, true)))
-                .isEqualTo(LottoResult.FOUR_MATCHING),
-            () -> assertThat(LottoResult.of(new WinningInfo(6, true)))
-                .isEqualTo(LottoResult.SIX_MATCHING)
+            () -> assertThat(LottoResult.of(1, true)).isEqualTo(LottoResult.NO_REWARD),
+            () -> assertThat(LottoResult.of(2, true)).isEqualTo(LottoResult.NO_REWARD)
         );
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2})
-    void 당첨개수_3_미만일_때_상금_없음(int count) {
-
+    @Test
+    void 당첨개수_3개라면_THREE_MATCHING을_반환한다() {
         assertAll(
-            () -> assertThat(LottoResult.of(new WinningInfo(count, true)))
-                .isEqualTo(LottoResult.NO_REWARD),
-            () -> assertThat(LottoResult.of(new WinningInfo(count, false)))
-                .isEqualTo(LottoResult.NO_REWARD)
+            () -> assertThat(LottoResult.of(3, true)).isEqualTo(LottoResult.THREE_MATCHING),
+            () -> assertThat(LottoResult.of(3, false)).isEqualTo(LottoResult.THREE_MATCHING)
+        );
+    }
+
+    @Test
+    void 당첨개수_4개라면_FOUR_MATCHING_반환() {
+        assertAll(
+            () -> assertThat(LottoResult.of(4, true)).isEqualTo(LottoResult.FOUR_MATCHING),
+            () -> assertThat(LottoResult.of(4, false)).isEqualTo(LottoResult.FOUR_MATCHING)
+        );
+    }
+
+    @Test
+    void 당첨개수_5개라면_보너스여부를_반영해서_FIVE_MATCHING_반환() {
+        assertAll(
+            () -> assertThat(LottoResult.of(5, true)).isEqualTo(LottoResult.FIVE_MATCHING_BONUS),
+            () -> assertThat(LottoResult.of(5, false)).isEqualTo(LottoResult.FIVE_MATCHING)
+        );
+    }
+
+    @Test
+    void 당첨개수_6개라면_SIX_MATCHING_반환() {
+        assertAll(
+            () -> assertThat(LottoResult.of(6, true)).isEqualTo(LottoResult.SIX_MATCHING),
+            () -> assertThat(LottoResult.of(6, false)).isEqualTo(LottoResult.SIX_MATCHING)
         );
     }
 }
