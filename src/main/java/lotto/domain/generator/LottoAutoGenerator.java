a644bf90437ab8093329e63bc8f12b588e44919d
fpal95 <snc7303@gmail.com>
2022-02-22 22:46:00 +0900

    Refactor(LottoAutoGenerator): lottoNumberPool생성

--- src/main/java/lotto/domain/generator/LottoAutoGenerator.java
+++ src/main/java/lotto/domain/generator/LottoAutoGenerator.java
@@ -1,44 +1,28 @@
 package lotto.domain.generator;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import lotto.domain.lotto.Number;
-import lotto.domain.lotto.Numbers;
+import lotto.domain.lotto.LottoNumber;
+import lotto.domain.lotto.LottoNumbers;
 
 public class LottoAutoGenerator implements LottoGenerator {
 
-    private List<Integer> numbers;
+    private List<LottoNumber> lottoNumberPool;
 
     public LottoAutoGenerator() {
-        generate();
-        shuffle();
-    }
-
-    private void shuffle() {
-        Collections.shuffle(numbers);
-    }
-
-    private void generate() {
-        numbers = IntStream.rangeClosed(START_NUMBER, END_NUMBER)
-            .boxed()
+        this.lottoNumberPool = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+            .mapToObj(LottoNumber::new)
             .collect(Collectors.toList());
     }
 
     @Override
-    public Numbers generateNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < LOTTO_NUMBERS; i++) {
-            numbers.add(this.numbers.get(i));
-        }
-
-        return new Numbers(
-            numbers.stream()
-                .sorted()
-                .map(Number::new)
-                .collect(Collectors.toList())
-        );
+    public LottoNumbers generateNumbers() {
+        Collections.shuffle(lottoNumberPool);
+        return new LottoNumbers(lottoNumberPool.stream()
+            .limit(LOTTO_NUMBERS)
+            .collect(Collectors.toList()));
     }
+
 }
