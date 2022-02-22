33fda2732059df739d2ad8e903998c02738f242b
YangWoo Yu <yuyw0712@naver.com>
2022-02-22 17:57:29 +0900

    refactor: eqauls & hashcode 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -31,20 +31,4 @@ public class Lotto {
         return lotto;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Lotto lotto1 = (Lotto) o;
-        return Objects.equals(lotto, lotto1.lotto);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(lotto);
-    }
 }
