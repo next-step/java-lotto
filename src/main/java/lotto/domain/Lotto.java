5f25c9a4410bb42dda3f79c4149d7bb1d4e26567
YangWoo Yu <yuyw0712@naver.com>
2022-02-22 18:07:53 +0900

    refactor: 방어적 복사 Collections.unmodifiableSet 적용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import java.util.Objects;
+import java.util.Collections;
 import java.util.Set;
 
 public class Lotto {
@@ -8,27 +8,27 @@ public class Lotto {
     private static final int LOTTO_NUMBERS_SIZE = 6;
     private static final String ERROR_LOTTO_NUMBER = "로또 번호는 6개여야 합니다.";
 
-    private final Set<LottoNumber> lotto;
+    private final Set<LottoNumber> lottoNumbers;
 
-    public Lotto(Set<LottoNumber> lotto) {
-        validate(lotto);
-        this.lotto = lotto;
+    public Lotto(Set<LottoNumber> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validate(Set<LottoNumber> lottoNumber) {
-        if (lottoNumber.size() != LOTTO_NUMBERS_SIZE) {
+    private void validate(Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBERS_SIZE) {
             throw new IllegalArgumentException(ERROR_LOTTO_NUMBER);
         }
     }
 
     public int matchWinningLotto(WinningLotto winningLotto) {
-        return (int) lotto.stream()
+        return (int) lottoNumbers.stream()
             .filter(winningLotto::hasContainNumber)
             .count();
     }
 
-    public Set<LottoNumber> getLotto() {
-        return lotto;
+    public Set<LottoNumber> getLottoNumbers() {
+        return Collections.unmodifiableSet(lottoNumbers);
     }
 
 }
