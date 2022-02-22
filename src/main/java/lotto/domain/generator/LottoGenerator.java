189187e0e9051f87e6358540feb27a084f199c69
fpal95 <snc7303@gmail.com>
2022-02-22 22:41:41 +0900

    Refactor(LottoGenerator): 상수명 수정

--- src/main/java/lotto/domain/generator/LottoGenerator.java
+++ src/main/java/lotto/domain/generator/LottoGenerator.java
@@ -4,8 +4,8 @@ import lotto.domain.lotto.LottoNumbers;
 
 public interface LottoGenerator {
     int LOTTO_NUMBERS = 6;
-    int START_NUMBER = 1;
-    int END_NUMBER = 45;
+    int LOTTO_MIN_NUMBER = 1;
+    int LOTTO_MAX_NUMBER = 45;
 
     LottoNumbers generateNumbers();
 }
