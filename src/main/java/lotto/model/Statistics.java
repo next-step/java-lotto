4f98b576ef866c7ff7e5f507015f1ad272fdbddf
Jiin Lee <loveted0225@naver.com>
2022-02-23 15:13:59 +0900

    refactor(Statistics): 생성자 파라미터 객체 변경 및 Ranks의 메서드 위임 받음
    -calculateTotalPrize

--- src/main/java/lotto/model/Statistics.java
+++ src/main/java/lotto/model/Statistics.java
@@ -1,19 +1,30 @@
 package lotto.model;
 
+import java.util.Map;
+
 public class Statistics {
 
     private static final int PRICE_OF_A_LOTTO = 1000;
     private static final int HUNDRED = 100;
+    private static final int ZERO = 0;
 
     private final double totalPrice;
     private final double totalPrize;
 
-    public Statistics(int lottoAmount, double totalPrize) {
-        this.totalPrice = lottoAmount * PRICE_OF_A_LOTTO;
-        this.totalPrize = totalPrize;
+    public Statistics(Count count, Map<Rank, Integer> totalRanks) {
+        this.totalPrice = count.getCount() * PRICE_OF_A_LOTTO;
+        this.totalPrize = calculateTotalPrize(totalRanks);
     }
 
     public double calculateBenefits() {
         return Math.floor((totalPrize / totalPrice) * HUNDRED) / HUNDRED;
+    }
+
+    private int calculateTotalPrize(Map<Rank, Integer> totalRanks) {
+        int totalPrize = ZERO;
+        for (Rank key : totalRanks.keySet()) {
+            totalPrize += key.getReward() * totalRanks.get(key);
+        }
+        return totalPrize;
     }
 }
