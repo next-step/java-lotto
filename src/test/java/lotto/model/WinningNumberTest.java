bf08efb10846ba47df1d68dbdd4dd2baba3bd873
Jiin Lee <loveted0225@naver.com>
2022-02-23 15:22:20 +0900

    refactor(WinningNumberTest): WinningNumber 리팩토링애 따른 테스트코드 변경

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -5,36 +5,51 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningNumberTest {
 
+    @DisplayName("중복된_숫자가_들어오면_예외를_발생하는지_확인")
     @Test
-    void 중복된_숫자가_들어오면_예외를_발생하는지_확인() {
+    void when_winning_numbers_duplicates_throw_exception() {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
         assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
+            .isThrownBy(() -> new WinningNumber(numbers, 9));
     }
 
     @Test
-    void 다섯개의_숫자가_들어오면_예외를_발생하는지_확인() {
+    void when_size_of_winning_numbers_less_than_six_throw_exception() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
+            .isThrownBy(() -> new WinningNumber(numbers, 9));
     }
 
     @Test
-    void 일곱개의_숫자가_들어오면_예외를_발생하는지_확인() {
+    void when_size_of_winning_numbers_more_than_six_throw_exception() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
         assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> new WinningNumber(numbers, new LottoNumber(9)));
+            .isThrownBy(() -> new WinningNumber(numbers, 9));
     }
 
+    @DisplayName("로또에서 몇개의 당첨 번호가 일치 하는지 체크한다.")
     @Test
-    void 보너스볼이_있는지_체크() {
+    void findMatchingCount() {
         WinningNumber winningNumber = new WinningNumber(
-            Arrays.asList(2, 4, 8, 15, 20, 21), new LottoNumber(41));
+            Arrays.asList(2, 4, 8, 15, 20, 21), 41);
+        List<LottoNumber> lottoNumbers = Arrays.asList(
+            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41),
+            new LottoNumber(15),
+            new LottoNumber(20));
+        assertThat(winningNumber.findMatchingCount(lottoNumbers)).isEqualTo(5);
+    }
+
+    @DisplayName("로또에 보너스볼이 번호가 있는지 체크")
+    @Test
+    void isContainBonsuBall() {
+        WinningNumber winningNumber = new WinningNumber(
+            Arrays.asList(2, 4, 8, 15, 20, 21), 41);
         boolean bonusBallMatch = winningNumber.isContainBonusBall(Arrays.asList(
             new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41),
             new LottoNumber(15),
