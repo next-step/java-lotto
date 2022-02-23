fe914e9fd4034cbc486f907e036490851a1bedac
Jiin Lee <loveted0225@naver.com>
2022-02-23 14:29:53 +0900

    feat: lottoGenerator를 인터페이스로 선언하여 자동, 수동 로또번호를 생성하는 구현체 생성
    -LottoGenerator
    -AutoLottoGenerator
    -ManualLottoGenerator

--- src/main/java/lotto/model/AutoLottoGenerator.java
+++ src/main/java/lotto/model/AutoLottoGenerator.java
@@ -4,25 +4,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class AutoLottoRules {
+public class AutoLottoGenerator implements LottoGenerator {
 
-    private final static int START_NUM = 1;
-    private final static int LAST_NUM = 45;
     private final static int START_INDEX = 0;
     private final static int LAST_INDEX = 6;
 
     private final List<LottoNumber> initialLottoNumbers;
 
-    public AutoLottoRules() {
+    public AutoLottoGenerator() {
         initialLottoNumbers = new ArrayList<>();
         for (int i = START_NUM; i <= LAST_NUM; i++) {
             initialLottoNumbers.add(new LottoNumber(i));
         }
     }
 
-    public List<LottoNumber> makeLottoNumbers() {
+    @Override
+    public List<LottoNumber> generateLotto() {
         Collections.shuffle(initialLottoNumbers);
-        return new ArrayList<>(initialLottoNumbers.subList(START_INDEX, LAST_INDEX));
+        List<LottoNumber> lottoNumbers = new ArrayList<>(
+            initialLottoNumbers.subList(START_INDEX, LAST_INDEX));
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
     }
-
 }
