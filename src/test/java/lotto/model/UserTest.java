f64793c133cbf41dab57fb514845d32faeca042f
Jiin Lee <loveted0225@naver.com>
2022-02-23 15:23:15 +0900

    refactor(UserTest): setUp 메소드로 중복 코드 삭제

--- src/test/java/lotto/model/UserTest.java
+++ src/test/java/lotto/model/UserTest.java
@@ -4,36 +4,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
 
-    @DisplayName("유저를 잘 생성하는지 테스트")
-    @Test
-    void createUserTest() {
+    private static User user;
+
+    @BeforeAll
+    static void setUp() {
         List<Lotto> lottos = Arrays.asList(
             new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
                 new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))),
             new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
                 new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)))
         );
-        User user = new User(lottos);
+        user = new User(lottos);
+    }
+
+    @DisplayName("유저를 잘 생성하는지 테스트한다")
+    @Test
+    void createUserTest() {
         assertThat(user.getLottos()).hasSize(2);
     }
 
     @DisplayName("유저는 자신이 산 복권에서 당첨번호가 몇개인지 판단한다")
     @Test
     void matchWinningNumberWithLottos() {
-        List<Lotto> lottos = Arrays.asList(
-            new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(4), new LottoNumber(5), new LottoNumber(6))),
-            new Lotto(Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3),
-                new LottoNumber(4), new LottoNumber(5), new LottoNumber(10)))
-        );
-        User user = new User(lottos);
         WinningNumber winningNumber = new WinningNumber(
-            Arrays.asList(1, 3, 4, 5, 6, 21), new LottoNumber(7));
+            Arrays.asList(1, 3, 4, 5, 6, 21), 7);
         List<MatchInfo> matchInfos = user.findEachLottoMatchingNumber(winningNumber);
         assertThat(matchInfos.get(0).getMatchCount()).isEqualTo(5);
         assertThat(matchInfos.get(1).getMatchCount()).isEqualTo(4);
