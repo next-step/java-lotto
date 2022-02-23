e1458877455161cdc96f165bf8b2838f571b43e7
Jiin Lee <loveted0225@naver.com>
2022-02-23 23:46:40 +0900

    refactor(StatisticsTest): Ranks 주입으로 테스트 코드 변경

--- src/test/java/lotto/model/StatisticsTest.java
+++ src/test/java/lotto/model/StatisticsTest.java
@@ -2,29 +2,30 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StatisticsTest {
 
-    private Count count;
-    private Map<Rank, Integer> totalRanks;
     private Statistics statistics;
+    private Ranks ranks;
+    private List<MatchInfo> matchInfos;
 
     @BeforeEach
     void setUp() {
-        count = new Count(5, 1);
-        totalRanks = new HashMap<Rank, Integer>() {{
-            put(Rank.FIFTH, 1);
-            put(Rank.FOURTH, 1);
-            put(Rank.THIRD, 0);
-            put(Rank.SECOND, 0);
-            put(Rank.FIRST, 0);
-        }};
-        statistics = new Statistics(count, totalRanks);
+        ranks = new Ranks();
+        matchInfos = Arrays.asList(
+            new MatchInfo(0, false),
+            new MatchInfo(1, false),
+            new MatchInfo(0, false),
+            new MatchInfo(3, false),
+            new MatchInfo(4, false)
+        );
+        ranks.updateRanks(matchInfos);
+        statistics = new Statistics(ranks);
     }
 
     @DisplayName("수익률을_계산한다")
