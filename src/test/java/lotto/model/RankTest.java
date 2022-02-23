496cfe942582a7fb07da4e85622e3557021fc3a5
Jiin Lee <loveted0225@naver.com>
2022-02-23 15:17:49 +0900

    test(RankTest): 일치하는 당첨번호가 3미만일 경우의 테스트코드 작성

--- src/test/java/lotto/model/RankTest.java
+++ src/test/java/lotto/model/RankTest.java
@@ -2,13 +2,22 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RankTest {
 
+    @DisplayName("당첨된 번호의 개수에 따라 등수를 반환한다")
     @Test
-    void 당첨번호가_일치하는_개수에_따라_순위를_확인() {
+    void getRanks() {
         Rank rank = Rank.getRank(4, true);
         assertThat(rank).isEqualTo(Rank.FOURTH);
+    }
+
+    @DisplayName("일치하는_당첨번호가_3미만일_경우_MISS반환")
+    @Test
+    void when_matchCount_is_less_than_3_then_return_MISS() {
+        Rank rank = Rank.getRank(2, false);
+        assertThat(rank).isEqualTo(Rank.MISS);
     }
 }
