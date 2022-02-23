fe6499cb69cc2108f6052cefa1e34c591e2b5c57
Jiin Lee <loveted0225@naver.com>
2022-02-23 23:48:02 +0900

    refactor(WinningNumberTest): 캐싱된 LottoNumber에 따른 테스트 코드 수정

--- src/test/java/lotto/model/WinningNumberTest.java
+++ src/test/java/lotto/model/WinningNumberTest.java
@@ -13,7 +13,6 @@ class WinningNumberTest {
     @DisplayName("중복된_숫자가_들어오면_예외를_발생하는지_확인")
     @Test
     void when_winning_numbers_duplicates_throw_exception() {
-
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 5);
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> new WinningNumber(numbers, 9));
@@ -33,15 +32,23 @@ class WinningNumberTest {
             .isThrownBy(() -> new WinningNumber(numbers, 9));
     }
 
+    @DisplayName("보너스볼과 당첨번호가 중복되면 생성을 실패한다")
+    @Test
+    void when_bonusball_duplicats_with_winning_number_then_fail() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new WinningNumber(numbers, 6));
+    }
+
     @DisplayName("로또에서 몇개의 당첨 번호가 일치 하는지 체크한다.")
     @Test
     void findMatchingCount() {
         WinningNumber winningNumber = new WinningNumber(
             Arrays.asList(2, 4, 8, 15, 20, 21), 41);
         List<LottoNumber> lottoNumbers = Arrays.asList(
-            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41),
-            new LottoNumber(15),
-            new LottoNumber(20));
+            LottoNumber.valueOf(2), LottoNumber.valueOf(4), LottoNumber.valueOf(8),
+            LottoNumber.valueOf(41),
+            LottoNumber.valueOf(15), LottoNumber.valueOf(20));
         assertThat(winningNumber.findMatchingCount(lottoNumbers)).isEqualTo(5);
     }
 
@@ -51,9 +58,10 @@ class WinningNumberTest {
         WinningNumber winningNumber = new WinningNumber(
             Arrays.asList(2, 4, 8, 15, 20, 21), 41);
         boolean bonusBallMatch = winningNumber.isContainBonusBall(Arrays.asList(
-            new LottoNumber(2), new LottoNumber(4), new LottoNumber(8), new LottoNumber(41),
-            new LottoNumber(15),
-            new LottoNumber(20)));
+            LottoNumber.valueOf(2), LottoNumber.valueOf(4), LottoNumber.valueOf(8),
+            LottoNumber.valueOf(41),
+            LottoNumber.valueOf(15),
+            LottoNumber.valueOf(20)));
         assertThat(bonusBallMatch).isEqualTo(true);
     }
 }
