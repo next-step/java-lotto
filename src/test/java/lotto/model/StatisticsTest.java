0bcf43d0cc3e25143b748f8bc98f8bbac4fb51ea
Jiin Lee <loveted0225@naver.com>
2022-02-23 15:20:36 +0900

    test(StatisticsTest): Statistics 리팩토링애 따른 테스트코드 변경

--- src/test/java/lotto/model/StatisticsTest.java
+++ src/test/java/lotto/model/StatisticsTest.java
@@ -2,14 +2,35 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StatisticsTest {
 
+    private Count count;
+    private Map<Rank, Integer> totalRanks;
+    private Statistics statistics;
+
+    @BeforeEach
+    void setUp() {
+        count = new Count(5, 1);
+        totalRanks = new HashMap<Rank, Integer>() {{
+            put(Rank.FIFTH, 1);
+            put(Rank.FOURTH, 1);
+            put(Rank.THIRD, 0);
+            put(Rank.SECOND, 0);
+            put(Rank.FIRST, 0);
+        }};
+        statistics = new Statistics(count, totalRanks);
+    }
+
+    @DisplayName("수익률을_계산한다")
     @Test
-    void 수익률을_계산한다() {
-        Statistics statistics = new Statistics(5, 50000);
+    void calculateBenefits() {
         double benefits = statistics.calculateBenefits();
-        assertThat(benefits).isEqualTo(10.0);
+        assertThat(benefits).isEqualTo(11.0);
     }
 }
