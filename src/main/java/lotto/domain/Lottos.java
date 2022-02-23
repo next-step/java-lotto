4cd3b0d9de94da7d4f29615f349891aaf217b454
xxell-8 <cell8pepper@gmail.com>
2022-02-23 14:43:49 +0900

    refactor: Lottos 생성 방식 변경
    - 로또 생성 책임 LottoMachine으로 이동

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,26 +3,12 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import lotto.domain.lottogenerator.LottoGenerator;
-
 public class Lottos {
 
     private final List<Lotto> lottos;
 
-    private Lottos(List<Lotto> lottos) {
+    public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
-    }
-
-    public static Lottos withLottoGenerator(LottoGenerator lottoGenerator, int buyCount) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < buyCount; i++) {
-            lottos.add(lottoGenerator.get());
-        }
-        return new Lottos(lottos);
-    }
-
-    public static Lottos withListLotto(List<Lotto> lottos) {
-        return new Lottos(lottos);
     }
 
     public List<Lotto> get() {
