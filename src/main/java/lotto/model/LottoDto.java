80ab00fa588a3b04dcaafee17132a39b8d6bc0a3
Jiin Lee <loveted0225@naver.com>
2022-02-23 14:01:22 +0900

    feat: InputView에서 controller로 로또번호 입력값을 전달하기 위한 Dto 생성
    -LottoDto

---
+++ src/main/java/lotto/model/LottoDto.java
@@ -0,0 +1,16 @@
+package lotto.model;
+
+import java.util.List;
+
+public class LottoDto {
+
+    private final List<Integer> manualLottoNumbers;
+
+    public LottoDto(List<Integer> manualLottoNumbers) {
+        this.manualLottoNumbers = manualLottoNumbers;
+    }
+
+    public List<Integer> getManualLottoNumbers() {
+        return manualLottoNumbers;
+    }
+}
