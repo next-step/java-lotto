35d73eb29167610bf992ace9c1b947ba5ddf00d6
xxell-8 <cell8pepper@gmail.com>
2022-02-23 14:37:41 +0900

    feat: 수동 로또를 위한 입력 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,6 +10,8 @@ public class InputView {
 
     private static final String DELIMITER = ",";
     private static final String PURCHASE_TICKET_MANAGER_LOG = "구입금액을 입력해 주세요.";
+    private static final String MANUAL_COUNT_MANAGER_LOG = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_MANAGER_LOG = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WINNING_NUMBER_MANAGER_LOG = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_BALL_MANAGER_LOG = "보너스 볼을 입력해 주세요.";
     private static final String NOT_BLANK_EXCEPTION = "공백은 들어올 수 없습니다.";
@@ -16,21 +20,45 @@ public class InputView {
     public static int inputBuyCash() {
         try {
             System.out.println(PURCHASE_TICKET_MANAGER_LOG);
-            String input = scanner.nextLine();
-            checkEmpty(input);
-            return Integer.parseInt(input);
+            return getInteger();
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return inputBuyCash();
         }
     }
 
+    public static int inputManualCount() {
+        try {
+            System.out.println(MANUAL_COUNT_MANAGER_LOG);
+            return getInteger();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return inputManualCount();
+        }
+    }
+
+    public static List<String[]> inputManualLotto(int count) {
+        try {
+            if (count == 0) return new ArrayList<>();
+
+            System.out.println(MANUAL_LOTTO_MANAGER_LOG);
+            List<String[]> result = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                result.add(getNumbers());
+            }
+
+            return result;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return inputManualLotto(count);
+        }
+    }
+
+
     public static String[] inputWinningNumbers() {
         try {
             System.out.println(WINNING_NUMBER_MANAGER_LOG);
-            String input = scanner.nextLine();
-            checkEmpty(input);
-            return input.split(DELIMITER);
+            return getNumbers();
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return inputWinningNumbers();
@@ -53,5 +81,17 @@ public class InputView {
         if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException(NOT_BLANK_EXCEPTION);
         }
+    }
+
+    private static int getInteger() {
+        String input = scanner.nextLine();
+        checkEmpty(input);
+        return Integer.parseInt(input);
+    }
+
+    private static String[] getNumbers() {
+        String input = scanner.nextLine();
+        checkEmpty(input);
+        return input.split(DELIMITER);
     }
 }
