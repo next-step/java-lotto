987de3bd531e7d87c738fb6ee647897ebd258153
0JUUU <clleo97@naver.com>
2022-02-27 03:35:31 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/lotto/domain/lotto/Lottos.java
+++ src/main/java/lotto/domain/lotto/Lottos.java
@@ -1,7 +1,6 @@
 package lotto.domain.lotto;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
