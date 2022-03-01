eb61a86fa3d2eb1763765beddcee0d3643661ea6
MinChul-Son <smc5236@gmail.com>
2022-03-01 20:01:07 +0900

    🚀 test(lotto): LottoShop 테스트 코드 추가

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -2,33 +2,34 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LottoShopTest {
 
+    private LottoShop lottoShop = LottoShop.getInstance();
+
     @Test
     void 로또를_사기위한_최소금액은_1000원_이상이다() {
-        final LottoShop lottoShop = LottoShop.getInstance();
-
         assertThatThrownBy(() -> lottoShop.countPossibleLottoAmount(new Money(500)))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("최소 1000원이어야 합니다.");
     }
 
     @Test
-    void 자동_로또를_살_수_있다() {
-        final LottoShop lottoShop = LottoShop.getInstance();
+    void 원하는_만큼_자동_로또를_살_수_있다() {
+        final List<Lotto> lottos = lottoShop.buyAutoLottos(3);
 
-        assertThat(lottoShop.buyAutoLotto()).isInstanceOf(Lotto.class);
+        assertThat(lottos.get(0)).isInstanceOf(Lotto.class);
+        assertThat(lottos.size()).isEqualTo(3);
     }
 
     @Test
     void 수동_로또를_살_수_있다() {
-        final LottoShop lottoShop = LottoShop.getInstance();
         final String[] lottoNumbers = {"1", "2", "3", "4", "5", "6"};
 
-        Lotto handOperatedLotto = lottoShop.buyManualLotto(lottoNumbers);
+        final Lotto handOperatedLotto = lottoShop.buyManualLotto(lottoNumbers);
 
         Assertions.assertAll(
             () ->assertThat(handOperatedLotto.getLottoNumbers().get(0).getNumber()).isEqualTo(1),
@@ -40,4 +41,12 @@ class LottoShopTest {
         );
     }
 
+    @Test
+    void 투입된_돈으로_몇_개의_로또를_살_수_있는지_알_수_있다() {
+        final Money money = new Money(3000);
+
+        final int lottoAmount = lottoShop.countPossibleLottoAmount(money);
+
+        assertThat(lottoAmount).isEqualTo(3);
+    }
 }
