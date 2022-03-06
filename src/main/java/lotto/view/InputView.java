7193272623f4c9af91a75fed0eb28342f1ccc4c8
Hae-Riri <ehl3288@naver.com>
2022-03-06 16:29:27 +0900

    fix: 정보 입력 포맷을 위한 줄바꿈 추가, 문구 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -19,7 +19,7 @@ public class InputView {
     }
 
     public static int getHandLottoQuantity() {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         return Integer.parseInt(scanner.nextLine());
     }
 
@@ -29,6 +29,7 @@ public class InputView {
         for(int i = 0; i < quantity; i++) {
             lottos.add(new Lotto(createLottoNumbers(scanner.nextLine().split(", "))));
         }
+        System.out.println();
         return lottos;
     }
 
@@ -46,6 +47,7 @@ public class InputView {
         String[] numbers = input.split(", ");
 
         System.out.println("보너스 볼을 입력해 주세요.");
+        System.out.println();
         return new WinningNumbers(Arrays.asList(numbers), scanner.nextLine());
     }
 }
