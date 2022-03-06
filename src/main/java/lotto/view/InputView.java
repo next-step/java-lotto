c9770b9f3cec5e3cdfd1fe4b98be52e190da60bc
Hae-Riri <ehl3288@naver.com>
2022-03-06 15:37:25 +0900

    feat: 수동로또 정보 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,7 +2,11 @@ package lotto.view;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
 import lotto.domain.winning.WinningNumbers;
 
 public class InputView {
@@ -12,6 +16,28 @@ public class InputView {
     public static int getMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static int getHandLottoQuantity() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Lotto> getHandLottos(int quantity) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < quantity; i++) {
+            lottos.add(new Lotto(createLottoNumbers(scanner.nextLine().split(", "))));
+        }
+        return lottos;
+    }
+
+    private static List<LottoNumber> createLottoNumbers(String[] numbers) {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for(String number : numbers) {
+            lottoNumbers.add(new LottoNumber(number));
+        }
+        return lottoNumbers;
     }
 
     public static WinningNumbers getWinningNumbers() {
