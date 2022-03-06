37e50d7125bef498426bc40d5a9df70a7b0ec8e7
Hae-Riri <ehl3288@naver.com>
2022-03-06 14:14:59 +0900

    feat: change constructor of LottoMachine
    - change argument: money -> quantity

--- src/main/java/lotto/domain/lotto/LottoMachine.java
+++ src/main/java/lotto/domain/lotto/LottoMachine.java
@@ -2,26 +2,15 @@ package lotto.domain.lotto;
 
 import lotto.domain.lotto.creator.LottoCreator;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
 
-    private static final BigDecimal LOTTO_PRICE = new BigDecimal(1000);
-
     private final List<Lotto> lottos;
 
-    public LottoMachine(int money, LottoCreator lottoCreator) {
-        this.lottos = createLottos(lottoCreator, calculateQuantity(money));
-    }
-
-    private int calculateQuantity(int money) {
-        if (money < LOTTO_PRICE.intValue()) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액이 1000원 이상이어야 합니다.");
-        }
-        return new BigDecimal(money).divide(LOTTO_PRICE, RoundingMode.FLOOR).intValue();
+    public LottoMachine(int quantity, LottoCreator lottoCreator) {
+        this.lottos = createLottos(lottoCreator, quantity);
     }
 
     private List<Lotto> createLottos(LottoCreator lottoCreator, int quantity) {
--- src/test/java/lotto/domain/lotto/LottoMachineTest.java
+++ src/test/java/lotto/domain/lotto/LottoMachineTest.java
@@ -1,23 +1,14 @@
 package lotto.domain.lotto;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import lotto.domain.LottoFixtures;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LottoMachineTest {
-
     @Test
-    void 구매금액이_1000원_미만이면_안된다() {
-        assertThatThrownBy(() -> new LottoMachine(900, LottoFixtures.FIXED_LOTTO_CREATOR))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("[ERROR] 구매 금액이 1000원 이상이어야 합니다.");
-    }
-
-    @Test
-    void 구매금액에_따른_개수만큼_로또를_생성한다() {
-        LottoMachine lottoMachine = new LottoMachine(14000, LottoFixtures.FIXED_LOTTO_CREATOR);
-        assertThat(lottoMachine.getLottos()).hasSize(14);
+    void 개수만큼_로또를_생성한다() {
+        LottoMachine lottoMachine = new LottoMachine(11, LottoFixtures.FIXED_LOTTO_CREATOR);
+        assertThat(lottoMachine.getLottos()).hasSize(11);
     }
 }
