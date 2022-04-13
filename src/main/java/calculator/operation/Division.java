001307becd34eef3063621c246ee1ba77a7a9ad8
DahamLeee <dlek1011@gmail.com>
2022-04-14 00:38:59 +0900

    fix exception handling when divide zero

--- src/main/java/calculator/operation/Division.java
+++ src/main/java/calculator/operation/Division.java
@@ -4,6 +4,9 @@ public class Division implements CustomOperation {
 
     @Override
     public int calculate(int leftPort, int rightPort) {
+        if (rightPort == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수는 없습니다!");
+        }
         return leftPort / rightPort;
     }
 }
