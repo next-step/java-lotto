424c8e8a911e829503ffa5fa3335c3537ba06831
DahamLeee <dlek1011@gmail.com>
2022-04-13 16:26:58 +0900

    feat add

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class StringCalculator {
+
+
+    public int splitAndCalculate(String source) {
+        String[] splits = source.split(" ");
+        int result = Integer.parseInt(splits[0]);
+
+        for (int i = 1; i < splits.length; i++) {
+            if (splits[i].equals("+")) {
+                result += Integer.parseInt(splits[i + 1]);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    @DisplayName("덧셈 테스트")
+    void addition() {
+        String source = "2 + 3";
+
+        int result = calculator.splitAndCalculate(source);
+
+        assertThat(result).isEqualTo(5);
+    }
+
+}
