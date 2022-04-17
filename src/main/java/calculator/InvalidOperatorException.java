952c004361623111a9287ee47d95327c05ba940d
cooper <pbg0205@naver.com>
2022-04-17 21:30:55 +0900

    feat: 연산자 문자와 연산자 객체를 매핑한다

---
+++ src/main/java/calculator/InvalidOperatorException.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class InvalidOperatorException extends RuntimeException {
+
+    private static final String INVALID_OPERATOR_MESSAGE = "올바른 연산자가 아닙니다";
+    public InvalidOperatorException() {
+        super(INVALID_OPERATOR_MESSAGE);
+    }
+}
--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,22 +1,33 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public enum Operator {
 
-    ADD(Number::add),
-    SUBTRACT(Number::subtract),
-    MULTIPLY(Number::multiply),
-    DIVIDE(Number::divide);
+    ADD("+", Number::add),
+    SUBTRACT("-", Number::subtract),
+    MULTIPLY("*", Number::multiply),
+    DIVIDE("/", Number::divide);
+
+    private final String value;
 
     private final BiFunction<Number, Number, Number> calculator;
 
-    Operator(BiFunction<Number, Number, Number> calculator) {
+    Operator(String value, BiFunction<Number, Number, Number> calculator) {
+        this.value = value;
         this.calculator = calculator;
     }
 
     public Number calculate(Number thisNumber, Number otherNumber) {
         return calculator.apply(thisNumber, otherNumber);
+    }
+
+    public static Operator from(String operatorAsString) {
+        return Arrays.stream(values())
+                .filter(operator -> operator.value.equals(operatorAsString))
+                .findFirst()
+                .orElseThrow(InvalidOperatorException::new);
     }
 
 }
