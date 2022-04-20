2a4216d61c21f631590a504ef55204db39696c16
bky4564 <bky4564@hunet.co.kr>
2022-04-20 15:06:17 +0900

    feat: 문자열 계산기 빼기, 곱하기 기능

--- src/main/java/stringcalculator/domain/StringCalculator.java
+++ src/main/java/stringcalculator/domain/StringCalculator.java
@@ -7,9 +7,15 @@ public class StringCalculator {
         }
         String[] split = input.split(" ");
 
-        int result = 0;
-        for (int i = 0; i < split.length; i += 2) {
-            result += Integer.parseInt(split[i]);
+        int result = Integer.parseInt(split[0]);
+        for (int i = 1; i < split.length; i += 2) {
+            if (split[1].equals("+")) {
+                result += Integer.parseInt(split[i + 1]);
+            } else if (split[1].equals("-")) {
+                result -= Integer.parseInt(split[i + 1]);
+            } else if (split[1].equals("*")) {
+                result *= Integer.parseInt(split[i + 1]);
+            }
         }
         return result;
     }
--- src/test/java/stringcalculator/domain/StringCalculatorTest.java
+++ src/test/java/stringcalculator/domain/StringCalculatorTest.java
@@ -25,5 +25,18 @@ class StringCalculatorTest {
         assertThat(StringCalculator.calculate(input)).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"5 - 1 - 1:3", "7 - 1 - 1 - 1:4", "9 - 1 - 1 - 1 - 1:5", "11 - 1 - 1 - 1 - 1 - 1:6"}, delimiter = ':')
+    @DisplayName("입력값을 빼는 경우")
+    void minusTest(String input, Integer expected) {
+        assertThat(StringCalculator.calculate(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4 * 3:12", "5 * 0 * 100:0", "3 * 4 * 5:60"}, delimiter = ':')
+    @DisplayName("입력값을 곱하는 경우")
+    void multiplyTest(String input, Integer expected) {
+        assertThat(StringCalculator.calculate(input)).isEqualTo(expected);
+    }
 
 }
