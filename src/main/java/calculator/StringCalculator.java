d2f807205cd26417fc74f82856db4a383f1a2dc9
younghun <asqwklop12@naver.com>
2022-04-22 00:32:39 +0900

    refactor trim 제거

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,9 +5,9 @@ public class StringCalculator {
   // "1 + 1 + 1 + 1"
   public int calculate(String text) {
     String[] split = text.split(" ");
-    Number number = new Number(Integer.parseInt(split[0].trim()));
+    Number number = new Number(Integer.parseInt(split[0]));
     for (int i = 1; i < split.length; i+=2) {
-      number = number.calculate(split[i], Integer.parseInt(split[i + 1].trim()));
+      number = number.calculate(split[i], Integer.parseInt(split[i + 1]));
     }
     return number.getValue();
   }
