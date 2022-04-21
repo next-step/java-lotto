e8365fe37ceafae093e2a48138508c8c9a8bf159
younghun <asqwklop12@naver.com>
2022-04-22 00:29:43 +0900

    refactor if continue 제거

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,9 +7,6 @@ public class StringCalculator {
     String[] split = text.split(" ");
     Number number = new Number(Integer.parseInt(split[0].trim()));
     for (int i = 1; i < split.length; i+=2) {
-      if (split.length < i + 2) {
-        continue;
-      }
       number = number.calculate(split[i], Integer.parseInt(split[i + 1].trim()));
     }
     return number.getValue();
