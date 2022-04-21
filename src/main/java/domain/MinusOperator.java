00d27e158cbc5fd10272a7f98caad8db527f4ce0
nara1030 <nara1030@naver.com>
2022-04-21 11:03:13 +0900

    style: 띄어쓰기 수정

--- src/main/java/domain/MinusOperator.java
+++ src/main/java/domain/MinusOperator.java
@@ -3,6 +3,6 @@ package domain;
 public class MinusOperator implements Operator {
     @Override
     public int calculate(int num1, int num2) {
-        return num1 -num2;
+        return num1 - num2;
     }
 }
