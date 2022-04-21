47721dbfc12d838a55e412226eb5d5875755ca63
bky4564 <bky4564@hunet.co.kr>
2022-04-21 13:22:42 +0900

    feat: 실행 클래스 생성

---
+++ src/main/java/stringcalculator/domain/StringCalculatorApplication.java
@@ -0,0 +1,11 @@
+package stringcalculator.domain;
+
+import java.util.Scanner;
+
+public class StringCalculatorApplication {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.print("계산할 문자열을 입력하세요 : ");
+        System.out.println("결과 : " + StringCalculator.calculate(scanner.nextLine()));
+    }
+}
