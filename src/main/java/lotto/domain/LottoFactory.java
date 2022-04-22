f776767cb3362778f1b45473678efb0ad5143041
hjeom <nara1030@naver.com>
2022-04-22 17:54:58 +0900

    refactor: 매직넘버 상수 추출

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -8,6 +8,8 @@ public class LottoFactory {
     private static final List<Integer> LOTTO_BASKET;
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_BALL_NUMBER_TO_SELECT = 0;
+    private static final int MAX_BALL_NUMBER_TO_SELECT = 6;
 
     static {
         ArrayList<Integer> lottoNumbers = new ArrayList<>();
@@ -19,7 +21,7 @@ public class LottoFactory {
 
     public static Lotto createLottoAutomatically() {
         Collections.shuffle(LOTTO_BASKET);
-        List<Integer> lottoNumbers = LOTTO_BASKET.subList(0, 6);
+        List<Integer> lottoNumbers = LOTTO_BASKET.subList(MIN_BALL_NUMBER_TO_SELECT, MAX_BALL_NUMBER_TO_SELECT);
         return new Lotto(lottoNumbers);
     }
 }
