3fd98f02e99a85b42491e63897f377a09ab633ef
younghun <asqwklop12@naver.com>
2022-04-23 14:51:26 +0900

    feat 손님이 얼마나 가지고 있는지 테스트한다

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.Objects;
+
+public final class Money {
+
+  private final Integer value;
+
+  public Money() {
+    this(0);
+  }
+
+  public Money(Integer value) {
+    this.value = value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Money money = (Money) o;
+    return Objects.equals(value, money.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+
+  @Override
+  public String toString() {
+    return value.toString();
+  }
+}
