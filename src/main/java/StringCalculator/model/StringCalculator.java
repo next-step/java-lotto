b6d3b59c94e3cba32d1728c11f70952b0697ed43
hwangdongho <ygekl8523@naver.com>
2022-04-23 22:22:44 +0900

    feat: 문자열계산기 작성

--- src/main/java/StringCalculator/model/StringCalculator.java
+++ src/main/java/StringCalculator/model/StringCalculator.java
@@ -1,23 +1,62 @@
 package StringCalculator.model;
 
+import java.util.Objects;
+
 public class StringCalculator {
-    public static int calculate(String problem){
-        return 0;
+    private static final String DELIMITER = " ";
+
+    public static int calculate(String problem) {
+        String[] splitedProblem = problem.split(DELIMITER);
+        int result = getIfNumber(splitedProblem[0]);
+
+        for (int i = 1; i < splitedProblem.length; i += 2) {
+            result = operation(result, splitedProblem[i], splitedProblem[i + 1]);
+        }
+
+        return result;
     }
 
-    private int addtion(){
-        return 0;
+    private static int getIfNumber(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
-    private int subtraction(){
-        return 0;
+    private static int operation(int a, String b, String c) {
+        if (Objects.equals(b, "+")) {
+            return addtion(a, getIfNumber(c));
+        }
+
+        if (Objects.equals(b, "-")) {
+            return subtraction(a, getIfNumber(c));
+        }
+
+        if (Objects.equals(b, "/")) {
+            return division(a, getIfNumber(c));
+        }
+
+        if (Objects.equals(b, "*")) {
+            return multiplication(a, getIfNumber(c));
+        }
+
+        throw new RuntimeException();
     }
 
-    private int division(){
-        return 0;
+    private static int addtion(int a, int b) {
+        return a + b;
     }
 
-    private int multiplication(){
-        return 0;
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    private static int division(int a, int b) {
+        return a / b;
+    }
+
+    private static int multiplication(int a, int b) {
+        return a * b;
     }
 }
