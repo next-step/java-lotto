70faba3f0b8ab19434565d2c95ce034c0c62dbf7
hwangdongho <ygekl8523@naver.com>
2022-04-23 21:21:45 +0900

    feat: 기본 틀 작성

---
+++ src/main/java/StringCalculator/model/StringCalculator.java
@@ -0,0 +1,23 @@
+package StringCalculator.model;
+
+public class StringCalculator {
+    public static int calculate(String problem){
+        return 0;
+    }
+
+    private int addtion(){
+        return 0;
+    }
+
+    private int subtraction(){
+        return 0;
+    }
+
+    private int division(){
+        return 0;
+    }
+
+    private int multiplication(){
+        return 0;
+    }
+}
