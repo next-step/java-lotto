6ab4e55e5a6784ad90eebdefcd38a6872272a49c
DahamLeee <dlek1011@gmail.com>
2022-04-24 14:38:55 +0900

    feat invalid lotto number exception handling

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,10 +4,19 @@ import java.util.Objects;
 
 public class Lotto {
 
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
     private int lottoNumber;
 
     public Lotto(int lottoNumber) {
+        if (invalidLottoNumber(lottoNumber)) {
+            throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이의 숫자여야 합니다.");
+        }
         this.lottoNumber = lottoNumber;
+    }
+
+    private boolean invalidLottoNumber(int lottoNumber) {
+        return lottoNumber < MINIMUM_LOTTO_NUMBER || lottoNumber > MAXIMUM_LOTTO_NUMBER;
     }
 
     @Override
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -7,8 +8,15 @@ import static org.assertj.core.api.Assertions.*;
 class LottoTest {
 
     @Test
+    @DisplayName("로또 번호인 int 를 Lotto Class 로 포장 후 생성")
     void create() {
         assertThat(new Lotto(1)).isEqualTo(new Lotto(1));
     }
 
+    @Test
+    @DisplayName("로또 번호는 1 ~ 45 이외의 숫자가 들어오면 예외를 던집니다.")
+    void invalidLottoNumber() {
+        assertThatThrownBy(() -> new Lotto(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
