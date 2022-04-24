b99c24b2cf293bfb1d8f7c6555b46ed6303fc19a
DahamLeee <dlek1011@gmail.com>
2022-04-24 14:53:57 +0900

    refactor remove all

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -1,51 +1,5 @@
 package lotto.util;
 
-import lotto.Lotteries;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static java.util.Comparator.*;
-import static java.util.stream.Collectors.*;
-
 public class LottoGenerator {
-
-    private static final int PRICE_PER_LOTTO = 1000;
-    private final List<Integer> LOTTO_SET = new ArrayList<>();
-
-    public LottoGenerator() {
-        for (int i = 1; i < 46; i++) {
-            LOTTO_SET.add(i);
-        }
-    }
-
-    public List<Integer> generate() {
-        Collections.shuffle(LOTTO_SET);
-        return LOTTO_SET.subList(0, 6)
-                .stream()
-                .sorted(naturalOrder())
-                .collect(toList());
-    }
-
-    public List<Lotteries> generate(int purchaseAmount) {
-        int lottoCount = calculatePurchasableCount(purchaseAmount);
-        List<Lotteries> lotteries = new ArrayList<>();
-
-        for (int i = 0; i < lottoCount; i++) {
-            Collections.shuffle(LOTTO_SET);
-
-            lotteries.add(LOTTO_SET
-                    .subList(0, 6)
-                    .stream()
-                    .sorted(naturalOrder())
-                    .collect(collectingAndThen(toList(), Lotteries::new)));
-        }
-        return lotteries;
-    }
-
-    private int calculatePurchasableCount(int purchaseAmount) {
-        return purchaseAmount / PRICE_PER_LOTTO;
-    }
 
 }
