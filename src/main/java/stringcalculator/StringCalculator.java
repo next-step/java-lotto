c728d8640869f89705d95750ad5cabdc2b72615c
pangnem <sprnd645@gmail.com>
2022-04-24 19:44:40 +0900

    refactor: 덧셈 메서드 간소화

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,20 +2,45 @@ package stringcalculator;
 
 public class StringCalculator {
     private static final String NOT_ALLOW_NULL_OR_EMPTY_MESSAGE = "비거나 null값은 허용되지 않습니다.";
+    private static final int FIRST_NUMBER_INDEX = 0;
+    private static final int SECOND_NUMBER_INDEX = 2;
+    private static final int ALLOW_STRINGS_LENGTH = 3;
+    private static final String ALLOW_LENGTH_EXCEED_MESSAGE = "허용된 길이를 초과했습니다.";
+    private static final String STRING_SPLIT_DELIMITER = " ";
 
     public int plus(String expression) {
         checkEmpty(expression);
+        checkLengthExceed(expression);
 
-        int result = 0;
-        String[] strings = expression.split(" ");
-        for (int i = 0; i < strings.length; i++) {
-            if ("+".equals(strings[i])) {
-                result += Integer.parseInt(strings[i - 1]);
-            }
+        int firstNumber = getFirstNumber(expression);
+        int secondNumber = getSecondNumber(expression);
+
+        return firstNumber + secondNumber;
+    }
+
+    private void checkLengthExceed(String expression) {
+        String[] strings = getStrings(expression);
+        if (strings.length > ALLOW_STRINGS_LENGTH) {
+            throw new IllegalArgumentException(ALLOW_LENGTH_EXCEED_MESSAGE);
         }
-        result += Integer.parseInt(strings[strings.length - 1]);
+    }
 
-        return result;
+    private int getSecondNumber(String expression) {
+        String[] strings = getStrings(expression);
+        return toInt(strings[SECOND_NUMBER_INDEX]);
+    }
+
+    private int getFirstNumber(String expression) {
+        String[] strings = getStrings(expression);
+        return toInt(strings[FIRST_NUMBER_INDEX]);
+    }
+
+    private int toInt(String string) {
+        return Integer.parseInt(string);
+    }
+
+    private String[] getStrings(String expression) {
+        return expression.split(STRING_SPLIT_DELIMITER);
     }
 
     private void checkEmpty(String expression) {
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
     private StringCalculator stringCalculator;
@@ -21,23 +19,11 @@ public class StringCalculatorTest {
     class plus_메서드는 {
 
         @Nested
-        class null이거나_빈_문자열이_주어질경우 {
-
-            @ParameterizedTest
-            @NullAndEmptySource
-            void IllegalArgumentException을_던진다(String nullOrEmpty) {
-                assertThatIllegalArgumentException()
-                        .isThrownBy(() -> stringCalculator.plus(nullOrEmpty));
-            }
-        }
-
-        @Nested
         class 더하기_기호가_포함된_문자열이_주어질경우 {
 
             @ParameterizedTest
             @CsvSource({
                     "2 + 3,5",
-                    "2 + 3 + 4,9"
             })
             void 더한_값을_리턴한다(String expression, int actual) {
                 assertThat(stringCalculator.plus(expression))
