ad33767cbccb76bcef87af609840c9f2599085a1
syeeuns <syeeuns@gmail.com>
2022-04-25 02:37:31 +0900

    feat: 지난 주 당첨 번호 입력받아 로또 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,5 @@
 package lotto.controller;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.PurchaseAmount;
@@ -11,19 +8,12 @@ import lotto.view.ResultView;
 
 public class LottoController {
 
-  private static final String WINNING_NUMBER_DELIMITER = ", ";
 
   public static void run() {
-    PurchaseAmount purchaseAmount = new PurchaseAmount(InputView.getPurchaseAmount());
+    PurchaseAmount purchaseAmount = InputView.getPurchaseAmount();
     int lottoCount = purchaseAmount.getLottoCount();
     Lottos lottos = Lottos.autoCreate(lottoCount);
     ResultView.printLottos(lottos);
-    Lotto lastWeekWinningLotto = Lotto.create(splitToSet(InputView.getLastWeekWinningNumber()));
-  }
-
-  private static Set<Integer> splitToSet(String value) {
-    return Arrays.stream(value.split(WINNING_NUMBER_DELIMITER))
-        .map(Integer::parseInt)
-        .collect(Collectors.toSet());
+    Lotto lastWeekWinningLotto = InputView.getLastWeekWinningLotto();
   }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,29 +1,31 @@
 package lotto.view;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.domain.PurchaseAmount;
 
 public class InputView {
 
+  private static final String WINNING_NUMBER_DELIMITER = ", ";
   private static Scanner SCANNER = new Scanner(System.in);
 
   private InputView() {
   }
 
-  public static int getPurchaseAmount() {
-    System.out.println("구입금액을 입력해 주세요.");
-    return getIntInput();
+  public static PurchaseAmount getPurchaseAmount() {
+    System.out.println("구매 금액을 입력해주세요.");
+    return new PurchaseAmount(Integer.parseInt(SCANNER.nextLine()));
   }
 
-  public static String getLastWeekWinningNumber() {
+  public static Lotto getLastWeekWinningLotto() {
     System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-    return getStringInput();
-  }
-
-  private static int getIntInput() {
-    return SCANNER.nextInt();
-  }
-
-  private static String getStringInput() {
-    return SCANNER.nextLine();
+    String[] stringNumbers = SCANNER.nextLine()
+        .trim()
+        .split(WINNING_NUMBER_DELIMITER);
+    return Lotto.create(Arrays.stream(stringNumbers)
+        .map(Integer::parseInt)
+        .collect(Collectors.toSet()));
   }
 }
