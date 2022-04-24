ecc663ec30abd558bf59f3068f31860392122da5
rumor1993 <rumor1993@naver.com>
2022-04-24 20:04:06 +0900

    feat: 입력값이 널이거나 공백인 경우 처리

---
+++ src/main/java/InputNumber.java
@@ -0,0 +1,19 @@
+public class InputNumber {
+
+    private String input;
+
+    public InputNumber(String input) {
+        validation(input);
+        this.input = input;
+    }
+
+    private void validation(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("입력 값이 존재하지 않습니다.");
+        }
+
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력 값이 공백 입니다.");
+        }
+    }
+}
