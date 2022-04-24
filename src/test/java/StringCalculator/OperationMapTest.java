e48f711112c90dd1b8e100944322add6271d289a
Roman <roman@channel.io>
2022-04-24 22:58:50 +0900

    Refactor :: OperationMap method 순서 변경

--- src/test/java/StringCalculator/OperationMapTest.java
+++ src/test/java/StringCalculator/OperationMapTest.java
@@ -11,6 +11,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class OperationMapTest {
 
+  @ParameterizedTest(name = "{1} {0} {2} = {3}")
+  @MethodSource("operatorTestArguments")
+  void operatorBy_수행_성공(String operator, int a, int b, int expected) {
+    assertThat(OperationMap.operatorBy(operator, a, b)).isEqualTo(expected);
+  }
+
   private static Stream<Arguments> operatorTestArguments() {
     return Stream.of(
         Arguments.of("+", 1, 2, 3),
@@ -18,12 +24,6 @@ public class OperationMapTest {
         Arguments.of("*", 1, 2, 2),
         Arguments.of("/", 1, 2, 0)
     );
-  }
-
-  @ParameterizedTest(name = "{1} {0} {2} = {3}")
-  @MethodSource("operatorTestArguments")
-  void operatorBy_수행_성공(String operator, int a, int b, int expected) {
-    assertThat(OperationMap.operatorBy(operator, a, b)).isEqualTo(expected);
   }
 
   @Test
