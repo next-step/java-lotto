d17c6210d4077501984007d30b20b285cce614b1
rumor1993 <rumor1993@naver.com>
2022-04-24 20:25:53 +0900

    refactor: sum 함수 파라미터명 변경

--- src/main/java/Addition.java
+++ src/main/java/Addition.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Addition {
-    public int sum(int number1, int nubmer2) {
-        return number1 + nubmer2;
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
     }
 }
