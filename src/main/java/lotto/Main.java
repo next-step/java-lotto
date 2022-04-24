a3b18f231f2ee68e8009d190c8aca8ca32aa6e74
imcool2551 <imcool2551@gmail.com>
2022-04-24 22:47:53 +0900

    feat: Main
    수동 구매 기능을 추가했습니다.
    함수가 길어져서 구매 금액을 입력받는 부분을 다른 함수로 빼냈습니다.

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -5,14 +5,14 @@ import lotto.domain.Lottos;
 import lotto.ui.InputView;
 import lotto.ui.OutputView;
 
+import java.util.List;
+
 import static lotto.domain.Lotto.LOTTO_PRICE;
-import static lotto.domain.Lotto.createLottosOfSize;
 
 public class Main {
 
     public static void main(String[] args) {
         Lottos lottos = createLottos();
-        OutputView.printLottos(lottos);
 
         Lotto winningLotto = InputView.promptWinningLotto();
         int bonusNumber = InputView.promptBonusNumber();
@@ -22,11 +22,29 @@ public class Main {
     }
 
     private static Lottos createLottos() {
+        int purchaseAmount = promptPurchaseAmount();
+
+        int totalLottoSize = purchaseAmount / LOTTO_PRICE;
+        int manualLottoSize = InputView.promptManualLottoSize();
+        int autoLottoSize = totalLottoSize - manualLottoSize;
+        if (manualLottoSize > totalLottoSize) {
+            throw new IllegalArgumentException("수동 구매하려는 로또가 구입 금액보다 많습니다.");
+        }
+
+        List<Lotto> manualLottos = InputView.promptManualLottos(manualLottoSize);
+        List<Lotto> autoLottos = Lotto.createLottosOfSize(autoLottoSize);
+
+        Lottos lottos = Lottos.createLottos(manualLottos, autoLottos);
+        OutputView.printLottos(lottos, manualLottoSize, autoLottoSize);
+        return lottos;
+    }
+
+    private static int promptPurchaseAmount() {
         int purchaseAmount = InputView.promptPurchaseAmount();
         if (purchaseAmount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(LOTTO_PRICE + "원 단위로 구매하실 수 있습니다.");
         }
-        int lottoSize = purchaseAmount / LOTTO_PRICE;
-        return new Lottos(createLottosOfSize(lottoSize));
+        return purchaseAmount;
     }
+
 }
