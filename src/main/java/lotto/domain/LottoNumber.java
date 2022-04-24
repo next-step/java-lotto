08eb21016c18c2b8b2f6e1a8d6c29470539e47d6
syeeuns <syeeuns@gmail.com>
2022-04-24 23:00:25 +0900

    feat: LottoNumber 추가

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+  private static final int MIN_VALUE = 1;
+  private static final int MAX_VALUE = 45;
+  private static final String INVALID_NUMBER_MESSAGE = "1과 45 사이의 자연수만 입력할 수 있습니다.";
+
+  private final int value;
+
+  public LottoNumber(int value) {
+    validate(value);
+    this.value = value;
+  }
+
+  private void validate(int value) {
+    if (value < MIN_VALUE || value > MAX_VALUE) {
+      throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    LottoNumber that = (LottoNumber) o;
+    return value == that.value;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+}
---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoNumberTest {
+
+  @DisplayName("1과 45 사이의 값이 아닌 수로 LottoNumber를 생성하려 하면 예외가 발생한다.")
+  @ParameterizedTest
+  @ValueSource(ints = {0, 46})
+  void create_invalid_number(int input) {
+    assertThatThrownBy(() -> new LottoNumber(input)).isInstanceOf(IllegalArgumentException.class);
+  }
+}
