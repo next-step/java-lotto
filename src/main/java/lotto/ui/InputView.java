f2145aa6aa0c9234677b70ad109fc8a06e0673cf
imcool2551 <imcool2551@gmail.com>
2022-04-24 22:45:33 +0900

    feat: InputView
    수동 구매 입력기능을 추가했습니다.

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -2,6 +2,7 @@ package lotto.ui;
 
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -20,8 +21,26 @@ public class InputView {
         return Integer.parseInt(scanner.nextLine());
     }
 
+    public static int promptManualLottoSize() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Lotto> promptManualLottos(int size) {
+        System.out.println("수동으로 구매할 번호를 예시와 같은 형식으로 한 줄씩 입력해 주세요. ex)1,2,3,4,5,6");
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            lottos.add(createLottoFromUserInput());
+        }
+        return lottos;
+    }
+
     public static Lotto promptWinningLotto() {
         System.out.println("지난 주 당첨 번호를 예시와 같은 형식으로 입력해 주세요. ex)1,2,3,4,5,6");
+        return createLottoFromUserInput();
+    }
+
+    private static Lotto createLottoFromUserInput() {
         String[] stringNumbers = scanner.nextLine()
                 .trim()
                 .split(",");
@@ -32,7 +51,8 @@ public class InputView {
     }
 
     public static int promptBonusNumber() {
-        System.out.println("보너스 볼을 입력해주세요.");
+        System.out.println("보너스 볼을 입력해 주세요.");
         return Integer.parseInt(scanner.nextLine());
     }
+
 }
