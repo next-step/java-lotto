caa38d8a3a0b9ec223a36a163985f9cb9d9ecbc4
syeeuns <syeeuns@gmail.com>
2022-04-25 01:09:23 +0900

    feat: 구입 금액에 대한 일급객체인 PurchaseAmount 생성

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+public class PurchaseAmount {
+
+  private static final int LOTTO_PRICE = 1000;
+  private static final String INVALID_PURCHASE_AMOUNT_MESSAGE = "구입 금액은 1000원 단위로 입력해주세요.";
+
+  private final int value;
+
+  public PurchaseAmount(int value) {
+    validate(value);
+    this.value = value;
+  }
+
+  public int getLottoCount() {
+    return value / 1000;
+  }
+
+  private void validate(int value) {
+    if (value <= 0 || value % LOTTO_PRICE != 0) {
+      throw new IllegalArgumentException(INVALID_PURCHASE_AMOUNT_MESSAGE);
+    }
+  }
+}
