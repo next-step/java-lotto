57bf054721dd72c5a7676fcd946f87668ea6f2bd
Roman <roman@channel.io>
2022-04-26 04:28:00 +0900

    Feat :: LottoApplication 추가

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -3,9 +3,13 @@ import lotto.LottoController;
 public class LottoApplication {
 
   public static void main(String[] args) {
+    try{
     LottoController.init()
-        .insertAmount()
-        .insertWinningNumbers();
+        .purchaseByInput()
+        .insertWinningNumber()
+        .printRevenue();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
-
 }
