613d5b008437a522f0b9b5bfc4d884494ad3e5db
wu2ee <gulb1602@gmail.com>
2022-04-25 18:59:17 +0900

    feat : 문자열 구분 클래스 구현

---
+++ src/main/java/calculator/SplitString.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class SplitString {
+    private static final String BLANK_DELIMITER = " ";
+    private static final List<String> OPERATOR = List.of("+", "-", "*", "/");
+
+    private SplitString() {
+    }
+
+    public static List<String> split(String text) {
+        List<String> elements = split(text, BLANK_DELIMITER);
+        validateElement(elements);
+        return elements;
+    }
+
+    private static List<String> split(String text, String delimiter) {
+        return List.of(text.split(delimiter));
+    }
+
+    private static void validateElement(List<String> elements) {
+        IntStream.range(0, elements.size()).forEach(index -> {
+            if (index % 2 == 0 && !isDigit(elements.get(index))) {
+                throw new IllegalArgumentException("숫자가 아닙니다.");
+            }
+            if (index % 2 == 1 && !isOperator(elements.get(index))) {
+                throw new IllegalArgumentException("연산자가 아닙니다.");
+            }
+        });
+    }
+
+    private static boolean isDigit(String value) {
+        return value.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean isOperator(String value) {
+        return OPERATOR.contains(value);
+    }
+}
---
+++ src/test/java/calculator/SplitStringTest.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class SplitStringTest {
+    @Test
+    void split_공백자로_구분() {
+        List<String> result = SplitString.split("1 + 3 - 2 * 5 / 2");
+        assertThat(result).isEqualTo(List.of("1", "+", "3", "-", "2", "*", "5", "/", "2"));
+    }
+
+    @Test
+    void invalid_입력문자열_숫자로_시작_안하는_경우() {
+        assertThatThrownBy(() -> SplitString.split("+ 3 - 1")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void invalid_입력문자열_사칙연산_기호가_아닌_경우() {
+        assertThatThrownBy(() -> SplitString.split("1 + 3 % 1")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
