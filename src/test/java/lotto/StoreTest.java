df05a51bba488d2b00d46e1d06791929f15846b8
younghun <asqwklop12@naver.com>
2022-04-26 00:52:32 +0900

    style 프로젝트 구조 재 정립

--- src/test/java/lotto/StoreTest.java
+++ src/test/java/lotto/StoreTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import lotto.model.Store;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
