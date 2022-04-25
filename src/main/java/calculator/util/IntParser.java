d82dd23126651654e99642fa6561f83ed501e564
Seongheon Sim <river.water.sea@gmail.com>
2022-04-25 23:48:57 +0900

    fix: IntParser 숫자가 아닌 인자가 들어왔을 때 예외 처리

--- src/main/java/calculator/util/IntParser.java
+++ src/main/java/calculator/util/IntParser.java
@@ -1,15 +1,15 @@
 package calculator.util;
 
-import java.util.Optional;
-
 public class IntParser {
 
     private IntParser() {
     }
 
     public static int parse(String value) {
-        return Optional
-                .of(Integer.parseInt(value))
-                .orElseThrow(() -> new IllegalArgumentException("숫자가 아닌 문자열은 사용할 수 없습니다."));
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 문자열은 사용할 수 없습니다.");
+        }
     }
 }
