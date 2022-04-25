0ee271111977f8bc796e81c39e470509fb1baee6
younghun <asqwklop12@naver.com>
2022-04-25 22:37:45 +0900

    feat 수익률 계산 기능 추가

--- src/test/java/lotto/StatisticsTest.java
+++ src/test/java/lotto/StatisticsTest.java
@@ -13,8 +13,16 @@ class StatisticsTest {
   @DisplayName("n개가 일치했는지 확인한다.")
   void coincide() {
     Statistics statistics = new Statistics();
-    List<List<Integer>> allLotto = Arrays.asList(List.of(1,2,3,4,5,6),List.of(6,7,8,9,11,30));
-    List<Integer> winNumbers = Arrays.asList(21,23,27,29,30,45);
+    List<List<Integer>> allLotto = Arrays.asList(List.of(1, 2, 3, 4, 5, 6),
+        List.of(6, 7, 8, 9, 11, 30));
+    List<Integer> winNumbers = Arrays.asList(21, 23, 27, 29, 30, 45);
     assertThat(statistics.coincideLotto(1, allLotto, winNumbers)).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("수익률 테스트")
+  void yield() {
+    Statistics statistics = new Statistics();
+    assertThat(statistics.yield(5000, 14000)).isEqualTo(0.35);
   }
 }
