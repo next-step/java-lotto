15f03695474e43a703fee3552270b3b2f8d0cc60
bky4564 <bky4564@hunet.co.kr>
2022-04-25 17:05:57 +0900

    refactor: InputView 리팩토링
    - 메시지 상수화
    - extract methods

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.exception.InvalidInputWinningNumberException;
+
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
@@ -8,19 +10,42 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해주세요.";
+    public static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
+    public static final String DELIMITER = ",";
     private final Scanner scanner = new Scanner(System.in);
 
     public BigDecimal readMoney() {
-        System.out.println("구입금액을 입력해주세요.");
+        System.out.println(INPUT_MONEY_MESSAGE);
         return BigDecimal.valueOf(scanner.nextLong());
     }
 
     public List<Integer> readWinningNumber() {
-        System.out.println("지난 주 당첨 번호를 입력해주세요.");
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         String input = scanner.nextLine().replaceAll(" ", "");
-        String[] split = input.split(",");
+        return getWinningNumbers(input);
+    }
+
+    private List<Integer> getWinningNumbers(String input) {
+        String[] split = splitInputByDelimiter(input);
+        List<Integer> winningNumbers = convertNumbers(split);
+        validWinningNumbers(winningNumbers);
+        return winningNumbers;
+    }
+
+    private void validWinningNumbers(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new InvalidInputWinningNumberException();
+        }
+    }
+
+    private List<Integer> convertNumbers(String[] split) {
         return Arrays.stream(split)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private String[] splitInputByDelimiter(String input) {
+        return input.split(DELIMITER);
     }
 }
