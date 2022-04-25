8cf01b09c7cf59d8970eb16f63a0aa875ff8d450
pangnem <sprnd645@gmail.com>
2022-04-25 18:52:41 +0900

    feat: 지난 주 당첨 번호 입력

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -7,5 +7,9 @@ public class LottoMachine {
     public void pull() {
         int price = InputView.inputPrice();
         OutputView.outputLottoNumbers(price);
+        System.out.println();
+
+        String previousWeekWinningNumber = InputView.inputPreviousWeekWinningNumber();
+        System.out.println("previousWeekWinningNumber = " + previousWeekWinningNumber);
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_PREVIOUS_WEEK_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
     public static int inputPrice() {
         System.out.println(INPUT_PRICE_MESSAGE);
         return SCANNER.nextInt();
+    }
+
+    public static String inputPreviousWeekWinningNumber() {
+        System.out.println(INPUT_PREVIOUS_WEEK_WINNING_NUMBER_MESSAGE);
+
+        SCANNER.nextLine();
+        return SCANNER.nextLine();
     }
 }
