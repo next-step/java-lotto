0c7484289502975000776549bfb20f7f6273dc76
guswns1659 <guswns1659@gmail.com>
2022-04-26 07:38:55 +0900

    step2 feat: 6자리 랜덤 숫자를 반환하는 객체 생성

---
+++ src/main/java/autolotto/LottoGenerator.java
@@ -0,0 +1,22 @@
+package autolotto;
+
+import java.util.*;
+
+public class LottoGenerator {
+    private static final Random RANDOM = new Random();
+    private static final int LOTTO_SIZE = 6;
+    private static final int RANDOM_BOUND = 45;
+    private static final int LOTTO_START_NUMBER = 1;
+
+    public Set<Integer> generate() {
+        Set<Integer> lotto = new HashSet<>(LOTTO_SIZE);
+        do {
+            lotto.add(getRandom());
+        } while (lotto.size() < LOTTO_SIZE);
+        return lotto;
+    }
+
+    private int getRandom() {
+        return RANDOM.nextInt(RANDOM_BOUND) + LOTTO_START_NUMBER;
+    }
+}
---
+++ src/test/java/autolotto/LottoGeneratorTest.java
@@ -0,0 +1,16 @@
+package autolotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoGeneratorTest {
+
+    @Test
+    void generateRequestGiven_ReturnLottoNumber() {
+        Set<Integer> randoms = new LottoGenerator().generate();
+        assertThat(randoms.size()).isEqualTo(6);
+    }
+}
