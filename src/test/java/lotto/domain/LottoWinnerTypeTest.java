e3082c51e286f446bed38c8930050bc335b4f861
hjeom <nara1030@naver.com>
2022-04-27 02:29:46 +0900

    test: 로또 당첨 조건으로 보너스볼 추가

--- src/test/java/lotto/domain/LottoWinnerTypeTest.java
+++ src/test/java/lotto/domain/LottoWinnerTypeTest.java
@@ -7,14 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoWinnerTypeTest {
     @ParameterizedTest
-    @CsvSource({"6,FIRST_PLACE", "5,SECOND_PLACE", "4,THIRD_PLACE", "3,FOURTH_PLACE", "-2,NON_WIN"})
-    void 중복개수_일치_당첨타입_반환(int countOfDuplicate, LottoWinnerType winnerType) {
-        assertThat(LottoWinnerType.valueOf(countOfDuplicate)).isEqualTo(winnerType);
+    @CsvSource({"6,false,FIRST_PLACE", "5,true,SECOND_PLACE", "5,false,THIRD_PLACE", "4,false,FOURTH_PLACE", "3,false,FIFTH_PLACE", "-2,false,NON_WIN"})
+    void 중복개수_및_보너스번호_일치_당첨타입_반환(int countOfDuplicate, boolean isBonus, LottoWinnerType winnerType) {
+        assertThat(LottoWinnerType.valueOf(countOfDuplicate, isBonus)).isEqualTo(winnerType);
     }
 
     @ParameterizedTest
-    @CsvSource({"6,2000000000", "5,1500000", "4,50000", "3,5000", "-2,0"})
-    void 중복개수_일치_상금_반환(int countOfDuplicate, int prize) {
-        assertThat(LottoWinnerType.prize(countOfDuplicate)).isEqualTo(prize);
+    @CsvSource({"6,false,2000000000", "5,true,30000000", "5,false,1500000", "4,false,50000", "3,false,5000", "-2,false,0"})
+    void 중복개수_및_보너스번호_일치_상금_반환(int countOfDuplicate, boolean isBonus, int prize) {
+        assertThat(LottoWinnerType.prize(countOfDuplicate, isBonus)).isEqualTo(prize);
     }
 }
