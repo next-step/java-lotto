478681e89cd5ab7941c44936243bb499e0d5c914
imcool2551 <imcool2551@gmail.com>
2022-04-26 19:21:48 +0900

    test: LottoNoTest
    정적 팩터리 메서드를 통한 캐싱을 사용하도록 변경한 뒤 동등성 대신 동일성을 테스트했습니다.

--- src/test/java/lotto/domain/LottoNoTest.java
+++ src/test/java/lotto/domain/LottoNoTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,13 +13,14 @@ class LottoNoTest {
     @ParameterizedTest(name = "로또 번호는 1~45이다.")
     @ValueSource(ints = {0, 46})
     void validation(int number) {
-        assertThatThrownBy(() -> new LottoNo(number))
+        assertThatThrownBy(() -> LottoNo.of(number))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("내부적인 캐싱을 통해 동일성 보장")
     @Test
-    void equals() {
-        assertThat(new LottoNo(1)).isEqualTo(new LottoNo(1));
-        assertThat(new LottoNo(2)).isEqualTo(new LottoNo(2));
+    void of() {
+        assertThat(LottoNo.of(1)).isSameAs(LottoNo.of(1));
+        assertThat(LottoNo.of(45)).isSameAs(LottoNo.of(45));
     }
 }
