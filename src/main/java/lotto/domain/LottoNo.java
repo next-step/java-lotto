e91e7bd6c2436f6aaa2832e80b9e1882efd72d15
imcool2551 <imcool2551@gmail.com>
2022-04-26 18:07:13 +0900

    refactor: LottoNo
    LottoNo를 생성자 대신 정적 팩터리 메서드로 얻게하고 내부적으로 캐싱했습니다.

--- src/main/java/lotto/domain/LottoNo.java
+++ src/main/java/lotto/domain/LottoNo.java
@@ -1,18 +1,35 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class LottoNo {
 
     private static final int MIN_VALID_NUMBER = 1;
     private static final int MAX_VALID_NUMBER = 45;
+    private static final Map<Integer, LottoNo> cache = new HashMap<>();
+
+    static {
+        for (int i = MIN_VALID_NUMBER; i <=MAX_VALID_NUMBER; i++) {
+            cache.put(i, new LottoNo(i));
+        }
+    }
 
     private int number;
-    public LottoNo(int number) {
+
+    private LottoNo(int number) {
         if (number < MIN_VALID_NUMBER || MAX_VALID_NUMBER < number) {
             throw new IllegalArgumentException("invalid lotto number: " + number);
         }
         this.number = number;
+    }
+
+    public static LottoNo of(int number) {
+        if (number < MIN_VALID_NUMBER || MAX_VALID_NUMBER < number) {
+            throw new IllegalArgumentException("invalid lotto number: " + number);
+        }
+        return cache.get(number);
     }
 
     public int getNumber() {
