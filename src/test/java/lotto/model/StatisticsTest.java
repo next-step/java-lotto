a54fe2dd99e192fb6baf9691f03915cd0bc09ad1
younghun <asqwklop12@naver.com>
2022-04-28 01:05:27 +0900

    feat 로또 결과를 확인합니다.

--- src/test/java/lotto/model/StatisticsTest.java
+++ src/test/java/lotto/model/StatisticsTest.java
@@ -14,8 +14,21 @@ class StatisticsTest {
   @Test
   @DisplayName("로또 번호가 맞는지 테스트")
   void awardCount() {
-    List<Product> allLotto = Arrays.asList(new Product(List.of(1, 2, 3, 4, 5, 6)),
+    List<Product> allLotto = Arrays.asList(new Product(List.of(8, 6, 3, 4, 5, 7)),
         new Product(List.of(6, 7, 8, 9, 11, 30)));
-    List<Integer> winNumbers = Arrays.asList(9, 8, 6, 29, 30, 45);
+    Product winNumbers = new Product(Arrays.asList(8, 7, 6, 21, 45, 29));
+    Statistics statistics = new Statistics();
+    assertThat(statistics.result(Grade.THREE, allLotto, winNumbers)).isEqualTo(2);
+  }
+
+  @Test
+  @DisplayName("승리 수당 테스트")
+  void win() {
+    Statistics statistics = new Statistics();
+    statistics = statistics.winReward(Grade.THREE,1);
+    statistics = statistics.winReward(Grade.FOUR,2);
+    statistics = statistics.winReward(Grade.FIVE,1);
+    statistics = statistics.winReward(Grade.SIX,1);
+    assertThat(statistics).isEqualTo(new Statistics(2001605000L));
   }
 }
