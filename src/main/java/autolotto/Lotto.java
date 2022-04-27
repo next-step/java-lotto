a0cd9af6317d1e50d8570270a38ebf468e7f301d
guswns1659 <guswns1659@gmail.com>
2022-04-28 07:30:07 +0900

    step2 feat: Lotto 객체에 당첨번호 몇개인지 확인하는 match() 추가

---
+++ src/main/java/autolotto/Lotto.java
@@ -0,0 +1,17 @@
+package autolotto;
+
+import java.util.Set;
+
+public class Lotto {
+    private Set<Integer> numbers;
+
+    public Lotto(Set<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public int match(WinningLotto winningLotto) {
+        return (int) numbers.stream()
+                .filter(winningLotto::contains)
+                .count();
+    }
+}
---
+++ src/test/java/autolotto/LottoTest.java
@@ -0,0 +1,29 @@
+package autolotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTest {
+
+    private Lotto lotto;
+
+    @BeforeEach
+    void setUp() {
+        lotto = new Lotto(Set.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1, 2, 3, 4 ,5, 6:6",
+            "1, 2, 3, 4, 5, 43:5",
+            "1, 2, 3, 4, 41, 42:4"
+    }, delimiter = ':')
+    void winningLottoGiven_ReturnMatchCount(String number, int matchCount) {
+        assertThat(lotto.match(new WinningLotto(number))).isEqualTo(matchCount);
+    }
+}
