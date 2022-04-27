b647d8c6c861cf32355655843cae56eb2a1ad952
bky4564 <bky4564@hunet.co.kr>
2022-04-27 14:07:45 +0900

    bug: nextLong -> nextLine 으로 변경 및 리팩토링

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,20 +10,36 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해주세요.";
-    public static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
-    public static final String DELIMITER = ",";
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
+    private static final String DELIMITER = ",";
+    private static final String INVALID_READ_MONEY_ERROR_MESSAGE = "구매 금액을 제대로 입력해주세요.";
+    private static final int LOTTO_NUMBER_COUNT = 6;
     private final Scanner scanner = new Scanner(System.in);
 
     public BigDecimal readMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
-        return BigDecimal.valueOf(scanner.nextLong());
+        return BigDecimal.valueOf(parseLong(readInput()));
+    }
+
+    private long parseLong(String input) {
+        long longValue;
+        try {
+            longValue = Long.parseLong(input);
+        } catch (RuntimeException e) {
+            throw new NumberFormatException(INVALID_READ_MONEY_ERROR_MESSAGE);
+        }
+        return longValue;
     }
 
     public List<Integer> readWinningNumber() {
         System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
-        String input = scanner.nextLine().replaceAll(" ", "");
+        String input = readInput().replaceAll(" ", "");
         return getWinningNumbers(input);
+    }
+
+    private String readInput() {
+        return scanner.nextLine();
     }
 
     private List<Integer> getWinningNumbers(String input) {
@@ -34,7 +50,7 @@ public class InputView {
     }
 
     private void validWinningNumbers(List<Integer> winningNumbers) {
-        if (winningNumbers.size() != 6) {
+        if (winningNumbers.size() != LOTTO_NUMBER_COUNT) {
             throw new InvalidInputWinningNumberException();
         }
     }
