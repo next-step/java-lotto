4571bcde571740d3f9ef7b92f189a33460804ebf
guswns1659 <guswns1659@gmail.com>
2022-04-28 18:26:30 +0900

    step2 feat: 여러 Result를 담고 있는 일급콜렉션 생성

---
+++ src/main/java/autolotto/Results.java
@@ -0,0 +1,25 @@
+package autolotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Results {
+    private List<Result> results = new ArrayList<>();
+
+    public void initialize() {
+        results.add(new Result(3, 5000));
+        results.add(new Result(4, 50000));
+        results.add(new Result(5, 1_500_000));
+        results.add(new Result(6, 2_000_000_000));
+    }
+
+    public Optional<Result> find(int name) {
+        for (Result result : results) {
+            if (result.isMatch(name)) {
+                return Optional.of(result);
+            }
+        }
+        return Optional.empty();
+    }
+}
---
+++ src/test/java/autolotto/ResultsTest.java
@@ -0,0 +1,29 @@
+package autolotto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResultsTest {
+
+    private Results results;
+
+    @BeforeEach
+    void setUp() {
+        results = new Results();
+        results.initialize();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "3:3:5000",
+            "4:4:50000"
+    }, delimiter = ':')
+    void matchCountGiven_ReturnResult(int expect, int actual, int prize) {
+        assertThat(results.find(expect)).isEqualTo(Optional.of(new Result(actual, prize)));
+    }
+}
