3cfbc88dfd8329300415a351290836a6e3cd05d3
younghun <asqwklop12@naver.com>
2022-04-29 01:01:50 +0900

    test 로또는 마이너스를 입력할 수 없습니다.

---
+++ src/test/java/lotto/model/LottoNoTest.java
@@ -0,0 +1,17 @@
+package lotto.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoNoTest {
+
+  @Test
+  @DisplayName("로또는 0이하의 숫자를 입력할 수 없습니다.")
+  void lottoNotNegativeNumber() {
+    assertThrows(RuntimeException.class, () -> {
+      new LottoNo(-1);
+    }).printStackTrace();
+  }
+}
