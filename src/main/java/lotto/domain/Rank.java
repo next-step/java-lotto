1fdae31c2f0fbbdfc9fb03e0795017236e9a231c
bky4564 <bky4564@hunet.co.kr>
2022-04-28 14:40:09 +0900

    feat: 등수 enum & test 생성

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    LOSE(0, 0);
+
+    private final int matchNumberCount;
+    private final int prize;
+
+    Rank(int matchNumberCount, int prize) {
+        this.matchNumberCount = matchNumberCount;
+        this.prize = prize;
+    }
+
+    public int getMatchNumberCount() {
+        return matchNumberCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public static Rank getRank(int count) {
+        return Arrays.stream(values())
+                .filter(r -> r.matchNumberCount == count)
+                .findAny()
+                .orElse(LOSE);
+    }
+}
