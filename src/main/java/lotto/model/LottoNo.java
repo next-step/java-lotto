ed863c7ecaab8f68285b3a8e4de8c8e6ee2d21d1
younghun <asqwklop12@naver.com>
2022-04-29 01:01:50 +0900

    test 로또는 마이너스를 입력할 수 없습니다.

--- src/main/java/lotto/model/LottoNo.java
+++ src/main/java/lotto/model/LottoNo.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.Objects;
+import lotto.exception.LottoNegativeException;
 
 public class LottoNo {
   private Integer value;
@@ -11,7 +12,9 @@ public class LottoNo {
   }
 
   private void valid(Integer value) {
-
+    if(value < 0) {
+      throw new LottoNegativeException();
+    }
   }
 
   @Override
