2aff55fcc876118b180fcded32a01127734bd68a
kang-jisu <rkdwltn0425@gmail.com>
2022-04-29 02:50:56 +0900

    refactor: 상수화

--- src/main/java/lotto/view/StatisticsResultView.java
+++ src/main/java/lotto/view/StatisticsResultView.java
@@ -9,6 +9,9 @@ public class StatisticsResultView {
     private static final String OUTPUT_WINNINGS = "%d개 일치 (%d원)- %d개";
     private static final String OUTPUT_YIELD = "총 수익률은 %.2f 입니다.";
     private static final long[] WINNINGS = new long[]{0, 0, 0, 5_000, 50_000, 1_500_000, 2_000_000_000};
+    private static final int DEFAULT_VALUE = 0;
+    private static final int WINNINGS_START = 3;
+    private static final int WINNINGS_END = 6;
 
     private StatisticsResultView() {
         throw new AssertionError();
@@ -24,8 +27,8 @@ public class StatisticsResultView {
 
     private static long printWinningsAndGetSum(Map<Integer, Integer> statistics) {
         long result = 0;
-        for (int i = 3; i <= 6; i++) {
-            int count = statistics.getOrDefault(i, 0);
+        for (int i = WINNINGS_START; i <= WINNINGS_END; i++) {
+            int count = statistics.getOrDefault(i, DEFAULT_VALUE);
             System.out.println(String.format(OUTPUT_WINNINGS, i, WINNINGS[i], count));
             result += count * WINNINGS[i];
         }
