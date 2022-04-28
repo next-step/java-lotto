88d8aeda0d3c25205e9bfd7646a10fd20bd54310
chkim <cndghks0213@gmail.com>
2022-04-28 21:04:40 +0900

    feat: 입력을 split하고 계산하는 StringCalculator 클래스 구현

---
+++ src/main/java/calculator/service/StringCalculator.java
@@ -0,0 +1,29 @@
+package calculator.service;
+
+import calculator.domain.StringNumber;
+import calculator.domain.StringOperator;
+
+public class StringCalculator {
+    private static final String BLANK_INPUT_EXCEPTION = "입력은 빈칸일 수 없습니다.";
+    private static final String DELIMITER = " ";
+
+    public static StringNumber splitStringAndCalculate(String input) {
+        validateInput(input);
+        String[] numberAndOperator = input.split(DELIMITER);
+        return calculate(numberAndOperator);
+    }
+
+    private static void validateInput(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_INPUT_EXCEPTION);
+        }
+    }
+
+    private static StringNumber calculate(String[] s) {
+        StringNumber result = new StringNumber(s[0]);
+        for (int i = 1; i < s.length; i += 2) {
+            result = result.calculate(new StringOperator(s[i]), new StringNumber(s[i + 1]));
+        }
+        return result;
+    }
+}
