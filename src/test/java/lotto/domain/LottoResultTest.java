8e717824e6a8990e0b905fc41d7feeef9cd7cb3e
itdar <thesockerr@gmail.com>
2022-04-28 20:26:25 +0900

    test: 3단계 기능을 위한 테스트 수정

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -12,7 +12,7 @@ class LottoResultTest {
     @Test
     void createLottoResultTest() {
         int fitCount = 1;
-        lottoResult = LottoResult.of(fitCount);
+        lottoResult = LottoResult.of(fitCount, Boolean.TRUE);
 
         assertThat(lottoResult).isNotNull();
     }
@@ -26,13 +26,14 @@ class LottoResultTest {
         int fitCount5 = 5;
         int fitCount6 = 6;
 
-        LottoResult lottoResult0 = LottoResult.of(fitCount0);
-        LottoResult lottoResult1 = LottoResult.of(fitCount0);
-        LottoResult lottoResult2 = LottoResult.of(fitCount0);
-        LottoResult lottoResult3 = LottoResult.of(fitCount3);
-        LottoResult lottoResult4 = LottoResult.of(fitCount4);
-        LottoResult lottoResult5 = LottoResult.of(fitCount5);
-        LottoResult lottoResult6 = LottoResult.of(fitCount6);
+        LottoResult lottoResult0 = LottoResult.of(fitCount0, false);
+        LottoResult lottoResult1 = LottoResult.of(fitCount0, false);
+        LottoResult lottoResult2 = LottoResult.of(fitCount0, false);
+        LottoResult lottoResult3 = LottoResult.of(fitCount3, false);
+        LottoResult lottoResult4 = LottoResult.of(fitCount4, false);
+        LottoResult lottoResult5 = LottoResult.of(fitCount5, false);
+        LottoResult lottoResult55 = LottoResult.of(fitCount5, true);
+        LottoResult lottoResult6 = LottoResult.of(fitCount6, false);
 
         assertThat(lottoResult0.fitCount()).isEqualTo(fitCount0);
         assertThat(lottoResult0.reward()).isEqualTo(Grade.NONE.getReward());
@@ -40,12 +41,19 @@ class LottoResultTest {
         assertThat(lottoResult1.reward()).isEqualTo(Grade.NONE.getReward());
         assertThat(lottoResult2.fitCount()).isEqualTo(fitCount0);
         assertThat(lottoResult2.reward()).isEqualTo(Grade.NONE.getReward());
+
         assertThat(lottoResult3.fitCount()).isEqualTo(fitCount3);
-        assertThat(lottoResult3.reward()).isEqualTo(Grade.FOURTH.getReward());
+        assertThat(lottoResult3.reward()).isEqualTo(Grade.FIFTH.getReward());
+
         assertThat(lottoResult4.fitCount()).isEqualTo(fitCount4);
-        assertThat(lottoResult4.reward()).isEqualTo(Grade.THIRD.getReward());
+        assertThat(lottoResult4.reward()).isEqualTo(Grade.FOURTH.getReward());
+
         assertThat(lottoResult5.fitCount()).isEqualTo(fitCount5);
-        assertThat(lottoResult5.reward()).isEqualTo(Grade.SECOND.getReward());
+        assertThat(lottoResult5.reward()).isEqualTo(Grade.THIRD.getReward());
+
+        assertThat(lottoResult55.fitCount()).isEqualTo(fitCount5);
+        assertThat(lottoResult55.reward()).isEqualTo(Grade.SECOND.getReward());
+
         assertThat(lottoResult6.fitCount()).isEqualTo(fitCount6);
         assertThat(lottoResult6.reward()).isEqualTo(Grade.FIRST.getReward());
     }
--- src/test/java/lotto/domain/LottoResultsTest.java
+++ src/test/java/lotto/domain/LottoResultsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultsTest {
@@ -14,16 +15,16 @@ class LottoResultsTest {
     @BeforeEach
     void setUp() {
         List<LottoResult> lottoResultList = new ArrayList<>();
-        lottoResultList.add(LottoResult.of(3));
-        lottoResultList.add(LottoResult.of(3));
-        lottoResultList.add(LottoResult.of(3));
-        lottoResultList.add(LottoResult.of(3));
-        lottoResultList.add(LottoResult.of(4));
-        lottoResultList.add(LottoResult.of(4));
-        lottoResultList.add(LottoResult.of(5));
-        lottoResultList.add(LottoResult.of(5));
-        lottoResultList.add(LottoResult.of(5));
-        lottoResultList.add(LottoResult.of(6));
+        lottoResultList.add(LottoResult.of(3, false));
+        lottoResultList.add(LottoResult.of(3, true));
+        lottoResultList.add(LottoResult.of(3, false));
+        lottoResultList.add(LottoResult.of(3, true));
+        lottoResultList.add(LottoResult.of(4, false));
+        lottoResultList.add(LottoResult.of(4, true));
+        lottoResultList.add(LottoResult.of(5, false));
+        lottoResultList.add(LottoResult.of(5, false));
+        lottoResultList.add(LottoResult.of(5, true));
+        lottoResultList.add(LottoResult.of(6, true));
 
         lottoResults = new LottoResults(lottoResultList);
     }
@@ -42,8 +43,17 @@ class LottoResultsTest {
         assertThat(count).isEqualTo(2);
     }
 
+    @DisplayName("5개 일치하면서 보너스번호가 일치하지 않는 경우가 있다.")
     @Test
-    void 로또결과에서_5개_일치하는_개수를_찾는다() {
+    void 로또결과에서_5개만_일치하는_개수를_찾는다() {
+        int count = lottoResults.countOf(5);
+
+        assertThat(count).isEqualTo(3);
+    }
+
+    @DisplayName("5개 일치하면서 보너스번호가 일치하지 않는 경우가 있다.")
+    @Test
+    void 로또결과에서_5개와_보너스번호가_일치하는_개수를_찾는다() {
         int count = lottoResults.countOf(5);
 
         assertThat(count).isEqualTo(3);
@@ -60,20 +70,20 @@ class LottoResultsTest {
     void 로또구매비용에_대한_수익률을_반환한다() {
         // 요구사항의 예시
         List<LottoResult> lottoResultList = new ArrayList<>();
-        lottoResultList.add(LottoResult.of(3));
-        lottoResultList.add(LottoResult.of(1));
-        lottoResultList.add(LottoResult.of(2));
-        lottoResultList.add(LottoResult.of(0));
-        lottoResultList.add(LottoResult.of(1));
-        lottoResultList.add(LottoResult.of(2));
-        lottoResultList.add(LottoResult.of(0));
-        lottoResultList.add(LottoResult.of(1));
-        lottoResultList.add(LottoResult.of(2));
-        lottoResultList.add(LottoResult.of(0));
-        lottoResultList.add(LottoResult.of(0));
-        lottoResultList.add(LottoResult.of(0));
-        lottoResultList.add(LottoResult.of(0));
-        lottoResultList.add(LottoResult.of(0));
+        lottoResultList.add(LottoResult.of(3, false));
+        lottoResultList.add(LottoResult.of(1, false));
+        lottoResultList.add(LottoResult.of(2, false));
+        lottoResultList.add(LottoResult.of(0, false));
+        lottoResultList.add(LottoResult.of(1, false));
+        lottoResultList.add(LottoResult.of(2, false));
+        lottoResultList.add(LottoResult.of(0, false));
+        lottoResultList.add(LottoResult.of(1, false));
+        lottoResultList.add(LottoResult.of(2, false));
+        lottoResultList.add(LottoResult.of(0, false));
+        lottoResultList.add(LottoResult.of(0, false));
+        lottoResultList.add(LottoResult.of(0, false));
+        lottoResultList.add(LottoResult.of(0, false));
+        lottoResultList.add(LottoResult.of(0, false));
 
         lottoResults = new LottoResults(lottoResultList);
 
