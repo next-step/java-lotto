4cb312b15147552294128c947f81b39c1e421f80
kang-jisu <rkdwltn0425@gmail.com>
2022-04-29 00:33:06 +0900

    feat: LottoMarket에서 구매한 로또 출력 구현

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,9 +1,14 @@
 package lotto;
 
 import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
 
 public class LottoMain {
     public static void main(String[] args) {
         int buyingMoney = InputView.inputBuyingMoney();
+        List<Lottos> buyingLottos = LottoMarket.buyLottos(buyingMoney, new RandomLottoGenerator());
+        ResultView.printBuyingLottos(buyingLottos);
     }
 }
---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,38 @@
+package lotto.view;
+
+import lotto.Lottos;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResultView {
+
+    private static final String OUTPUT_BUYING_LOTTOS = "%d개를 구매했습니다.";
+    private static final String DELIMITER = ", ";
+    private static final String OUTPUT_OPEN_BRACKET = "[";
+    private static final String OUTPUT_CLOSE_BRACKET = "]";
+
+    private ResultView() {
+        throw new AssertionError();
+    }
+
+    public static void printBuyingLottos(List<Lottos> buyingLottos) {
+        System.out.println(String.format(OUTPUT_BUYING_LOTTOS, buyingLottos.size()));
+
+        for (Lottos lottos : buyingLottos) {
+            printLottos(lottos);
+        }
+        System.lineSeparator();
+    }
+
+    private static void printLottos(Lottos lottos) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(OUTPUT_OPEN_BRACKET);
+        sb.append(lottos.get()
+                .stream()
+                .map(num -> String.valueOf(num))
+                .collect(Collectors.joining(DELIMITER)));
+        sb.append(OUTPUT_CLOSE_BRACKET);
+        System.out.println(sb);
+    }
+}
