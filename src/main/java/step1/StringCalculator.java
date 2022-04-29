27b0714be5e86567e52455f4f42d17026dd441ee
sanghyeon <sanghyeon@finda.co.kr>
2022-04-30 04:37:19 +0900

    test: StringCalculator 테스트

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -13,8 +13,8 @@ public class StringCalculator {
         int result = input.getFirstOperand();
 
         for (int i = 0; i < input.getOperationBundleCount(); i++) {
-            Operator operator = input.getOperator();
-            int operand = input.getOperand();
+            Operator operator = input.getNextOperator();
+            int operand = input.getNextOperand();
             result = operator.operate(result, operand);
         }
 
--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -3,7 +3,8 @@ package step1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,12 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("문자열 계산기")
 class StringCalculatorTest {
 
-    @Test
-    void 사칙연산_테스트() {
-        String input = "- 1 * 2 / 2 - 1";
+    @ParameterizedTest(name = "{displayName} -> [{index}] : {0} = {1}")
+    @CsvSource(value = {"1 * 2 / 2 - 1=0", "1 + 1 * 7 / 2=7", "2 + 3 * 4 / 2=10"}, delimiter = '=')
+    void 사칙연산_테스트(String input, int result) {
 
         StringCalculator stringCalculator = new StringCalculator(input);
 
-        assertThat(stringCalculator.calculate()).isEqualTo(new Number(-2));
+        assertThat(stringCalculator.calculate()).isEqualTo(result);
     }
 }
