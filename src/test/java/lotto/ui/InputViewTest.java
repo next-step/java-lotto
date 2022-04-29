a5839678b8841a15f8d8d3993f527ba22e9ad7f9
hjeom <nara1030@naver.com>
2022-04-29 17:42:41 +0900

    test: 입력(금액 및 수동입력 개수) 테스트 코드 추가

--- src/test/java/lotto/ui/InputViewTest.java
+++ src/test/java/lotto/ui/InputViewTest.java
@@ -1,6 +1,7 @@
 package lotto.ui;
 
-import lotto.exception.InvalidPriceException;
+import lotto.exception.InvalidCountOfManualLotto;
+import lotto.exception.InvalidNegativeNumberException;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -8,7 +9,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class InputViewTest {
     @Test
     void 음수_입력_테스트() {
-        assertThatThrownBy(() -> InputView.validatePrice(-3000))
-                .isInstanceOf(InvalidPriceException.class);
+        assertThatThrownBy(() -> InputView.validateNegativeNumber("금액 혹은 갯수", -3000))
+                .isInstanceOf(InvalidNegativeNumberException.class);
+    }
+
+    @Test
+    void 수동입력개수가_총개수_넘어서는_경우_테스트() {
+        assertThatThrownBy(() -> InputView.validateCountOfManualLotto( 9800, 10))
+                .isInstanceOf(InvalidCountOfManualLotto.class);
     }
 }
