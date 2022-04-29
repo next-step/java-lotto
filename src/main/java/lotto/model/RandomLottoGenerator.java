3a69744b38e1853dc812d7f74eed00096eb07b5b
kang-jisu <rkdwltn0425@gmail.com>
2022-04-30 02:30:30 +0900

    refactor: 자동 로또 생성 로직 변경
    - 1부터 45까지 값이 담겨있는 전역 리스트 생성
    - 전역 리스트 shuffle 후 subList로 6개를 얻어와 새로운 로또 번호 Set 생성

--- src/main/java/lotto/model/RandomLottoGenerator.java
+++ src/main/java/lotto/model/RandomLottoGenerator.java
@@ -2,15 +2,19 @@ package lotto.model;
 
 import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomLottoGenerator implements LottoGenerator {
 
     private static final int LOTTO_SIZE = 6;
-    private static final int LOTTO_NUM_LOWER_BOUND = 1;
-    private static final int LOTTO_NUM_UPPER_BOUND = 45;
+    private static final int LOTTO_NUM_MIN = 1;
+    private static final int LOTTO_NUM_MAX = 45;
+    private static final List<LottoNumber> LOTTO_NUMBERS = IntStream.range(LOTTO_NUM_MIN, LOTTO_NUM_MAX)
+            .mapToObj(LottoNumber::new)
+            .collect(Collectors.toList());
 
     public RandomLottoGenerator() {
     }
@@ -21,21 +25,7 @@ public class RandomLottoGenerator implements LottoGenerator {
     }
 
     private Set<LottoNumber> getLottoNumbers() {
-        List<LottoNumber> numbers = initNumbers();
-
-        Set<LottoNumber> lotto = new LinkedHashSet<>(LOTTO_SIZE);
-        for (int i = 0; i < LOTTO_SIZE; i++) {
-            Collections.shuffle(numbers);
-            lotto.add(numbers.remove(i));
-        }
-        return lotto;
-    }
-
-    private List<LottoNumber> initNumbers() {
-        List<LottoNumber> numbers = new LinkedList<>();
-        for (int i = LOTTO_NUM_LOWER_BOUND; i <= LOTTO_NUM_UPPER_BOUND; i++) {
-            numbers.add(new LottoNumber(i));
-        }
-        return numbers;
+        Collections.shuffle(LOTTO_NUMBERS);
+        return new LinkedHashSet<>(LOTTO_NUMBERS.subList(0, LOTTO_SIZE));
     }
 }
