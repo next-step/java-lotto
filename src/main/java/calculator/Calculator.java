5e99b137280b0739ee88565272892c4aad909d79
jeongjaeeom <jeongjae.eom@gmail.com>
2022-04-29 16:57:09 +0900

    feat($compile): 덧셈 기능 구현
    덧셈 기능 구현
    - Calculator 클래스 생성
    - 연산자(operators)와 피연산자(operands) 구분
    - 더하기 기능 구현
    
    BREAKING CHANGE: 덧셈 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+  public static int calculate(String input) {
+    String[] strings = input.split(" ");
+    List<String> operators = new ArrayList<>();
+    List<Integer> operands = new ArrayList<>();
+    for (int i = 0; i < strings.length; i++) {
+      if (i % 2 == 0) {
+        operands.add(Integer.parseInt(strings[i]));
+      } else {
+        operators.add(strings[i]);
+      }
+    }
+
+    int result = operands.get(0);
+    for (int i = 0; i < operators.size(); i++) {
+      if ("+".equals(operators.get(i))) {
+        result = plus(result, operands.get(i + 1));
+      }
+    }
+    return result;
+  }
+
+  public static int plus(int a, int b) {
+    return a + b;
+  }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+  @Test
+  @DisplayName("덧셈")
+  void plusTest() {
+    assertThat(Calculator.calculate("10 + 20 + 30")).isEqualTo(60);
+  }
+
+
+}
