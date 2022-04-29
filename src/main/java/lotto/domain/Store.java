ae21cc39dbca2e02e445f4bf91dc9e03d4c840d4
hjeom <nara1030@naver.com>
2022-04-30 01:38:12 +0900

    style: 띄어쓰기 등 수정

--- src/main/java/lotto/domain/Store.java
+++ src/main/java/lotto/domain/Store.java
@@ -12,14 +12,13 @@ public class Store {
     public static Lottos pay(List<Lotto> manualLottos, int price) {
         final List<Lotto> lottos = new ArrayList<>(manualLottos);
         int count = countOfLottos(price) - manualLottos.size();
-        while(count > 0) {
+        while (count > 0) {
             lottos.add(LottoFactory.createLottoAutomatically());
             count--;
         }
 
         return Lottos.supplyLottos(lottos);
     }
-
 
     public static int countOfLottos(int price) {
         return price / LOTTO_PRICE_PER_ONE;
