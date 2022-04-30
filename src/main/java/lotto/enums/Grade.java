b6bcdb2aaed3bdda7a314ac94280e49da55ca128
younghun <asqwklop12@naver.com>
2022-04-30 20:13:15 +0900

    style 변수 공백 정리

--- src/main/java/lotto/enums/Grade.java
+++ src/main/java/lotto/enums/Grade.java
@@ -7,17 +7,13 @@ public enum Grade {
   FIVE(5, 1_500_000),
   SIX(6, 2_000_000_000);
 
-  private static final Integer GUESSED = 1;
-  private static final Integer WRONG = 0;
   private final int expectNumber;
   private final int awardPrice;
-
 
   Grade(int expectNumber, int awardPrice) {
     this.expectNumber = expectNumber;
     this.awardPrice = awardPrice;
   }
-
 
   public int winningAmount(int count) {
     return this.awardPrice * count;
