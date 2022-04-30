0bb8ecc17375fedd4c4e5fd1f278a6b58e7dcda7
pangnem <sprnd645@gmail.com>
2022-04-30 13:49:59 +0900

    feat: MatchCount enum 추가

---
+++ src/test/java/lotto/view/MatchCountTest.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MatchCountTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 5000",
+            "4, 50000",
+            "5, 1500000",
+            "6, 2000000000"
+    })
+    void getWinningAmountWith(int matchCount, int winningAmount) {
+        int actual = MatchCount.getWinningAmountWith(matchCount);
+
+        assertThat(actual).isEqualTo(winningAmount);
+    }
+}
