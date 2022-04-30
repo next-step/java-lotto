fff795df3364809a263ae478dcd8555223b86380
pangnem <sprnd645@gmail.com>
2022-05-01 02:07:16 +0900

    test: 맞친 개수별 상금 케이스 추가

--- src/test/java/lotto/view/MatchCountTest.java
+++ src/test/java/lotto/view/MatchCountTest.java
@@ -9,10 +9,14 @@ class MatchCountTest {
 
     @ParameterizedTest
     @CsvSource({
+            "0, 0",
+            "1, 0",
+            "2, 0",
             "3, 5000",
             "4, 50000",
             "5, 1500000",
-            "6, 2000000000"
+            "6, 2000000000",
+            "7, 0"
     })
     void getWinningAmountWith(int matchCount, int winningAmount) {
         int actual = MatchCount.getWinningAmountWith(matchCount);
