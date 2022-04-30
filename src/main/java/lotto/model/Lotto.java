c0fd68026dcce55298c1438845ea612cc9ba9be9
younlll <youn.lee96@gmail.com>
2022-04-30 12:23:11 +0900

    refactor: change class name

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,24 +2,23 @@ package lotto.model;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import lotto.exception.LottoNumberDuplicateException;
 import lotto.strategy.NumberGenerateStrategy;
 
-public class LottoNumbers {
+public class Lotto {
 
   private static final int LOTTO_NUMBER_COUNT = 6;
 
   private final List<LottoNumber> lottoNumbers;
 
-  public LottoNumbers(List<LottoNumber> lottoNumbers) {
+  public Lotto(List<LottoNumber> lottoNumbers) {
     validateDuplicateNumber(lottoNumbers);
     this.lottoNumbers = lottoNumbers;
   }
 
-  public static LottoNumbers create(NumberGenerateStrategy numberGenerateStrategy) {
-    return new LottoNumbers(numberGenerateStrategy.generate());
+  public static Lotto create(NumberGenerateStrategy numberGenerateStrategy) {
+    return new Lotto(numberGenerateStrategy.generate());
   }
 
   public List<LottoNumber> getLottoNumbers() {
--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -3,30 +3,29 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import lotto.exception.LottoNumberDuplicateException;
 import lotto.strategy.DuplicateNumberGenerateStrategy;
 import lotto.strategy.RandomNumberGenerateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LottoNumbersTest {
+public class LottoTest {
 
   @Test
   @DisplayName("생성된 로또번호들의 갯수는 6개이다")
   void checkNumberOfGeneratedLottoNumbers() {
     // given
-    LottoNumbers lottoNumbers =  LottoNumbers.create(new RandomNumberGenerateStrategy());
+    Lotto lotto =  Lotto.create(new RandomNumberGenerateStrategy());
 
     // then
-    assertThat(lottoNumbers.getLottoNumbers()).hasSize(6);
+    assertThat(lotto.getLottoNumbers()).hasSize(6);
   }
   
   @Test
   @DisplayName("중복된 로또 번호가 존재한다면 예외처리를 한다")
   void exceptionDuplicateLottoNumbers() {
     // when & then
-    assertThatThrownBy(() -> LottoNumbers.create(new DuplicateNumberGenerateStrategy()))
+    assertThatThrownBy(() -> Lotto.create(new DuplicateNumberGenerateStrategy()))
         .isInstanceOf(LottoNumberDuplicateException.class);
   }
 }
