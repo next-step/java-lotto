6ac37b3b564684dac5fb15ec6d0972fcc4dac39d
younghun <asqwklop12@naver.com>
2022-05-01 03:34:00 +0900

    style 클래스명 수정

--- src/main/java/lotto/model/Reward.java
+++ src/main/java/lotto/model/Reward.java
@@ -4,24 +4,24 @@ import java.util.List;
 import java.util.Objects;
 import lotto.enums.Grade;
 
-public class Statistics {
+public class Reward {
 
-  private final long reward;
+  private final long value;
 
-  public Statistics() {
+  public Reward() {
     this(0L);
   }
 
-  public Statistics(Long reward) {
-    this.reward = reward;
+  public Reward(Long value) {
+    this.value = value;
   }
 
   public int result(Grade grade, List<Lotto> sellerLotto, Lotto winningLotto) {
     return winningLotto.coincideLotto(grade, sellerLotto);
   }
 
-  public Statistics winReward(Grade grade, int count) {
-    return new Statistics(this.reward + grade.winningAmount(count));
+  public Reward winReward(Grade grade, int count) {
+    return new Reward(this.value + grade.winningAmount(count));
   }
 
   @Override
@@ -32,17 +32,17 @@ public class Statistics {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    Statistics that = (Statistics) o;
-    return Objects.equals(reward, that.reward);
+    Reward that = (Reward) o;
+    return Objects.equals(value, that.value);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(reward);
+    return Objects.hash(value);
   }
 
   @Override
   public String toString() {
-    return String.valueOf(reward);
+    return String.valueOf(value);
   }
 }
