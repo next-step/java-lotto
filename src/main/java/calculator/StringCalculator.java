0457ad7b795889a96f2a0da08a67fd2fbc29115b
wu2ee <gulb1602@gmail.com>
2022-04-30 09:07:00 +0900

    refactor : 매직넘버 -> 클래스 상수로 변경

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,17 +3,20 @@ package calculator;
 import java.util.List;
 
 public class StringCalculator {
-    private static final int FIRST_OPERAND_INDEX = 0;
+    private static final int FIRST_OPERAND = 0;
+    private static final int SECOND_OPERATOR = 1;
+    private static final int TWO_STEP_INDEX = 2;
+    private static final int GET_OPERAND = 1;
 
     private StringCalculator() {
     }
 
     public static int calculate(String text) {
         List<String> elements = SplitString.split(text);
-        Operand resultValue = new Operand(elements.get(FIRST_OPERAND_INDEX));
-        for (int index = 1; index < elements.size(); index += 2) {
+        Operand resultValue = new Operand(elements.get(FIRST_OPERAND));
+        for (int index = SECOND_OPERATOR; index < elements.size(); index += TWO_STEP_INDEX) {
             Operator operator = Operator.valueOfOperatorSymbol(elements.get(index));
-            resultValue = operator.operate(resultValue, new Operand(elements.get(index + 1)));
+            resultValue = operator.operate(resultValue, new Operand(elements.get(index + GET_OPERAND)));
         }
         return resultValue.getNumber();
     }
