9eb7270cb2c28efa63a7d47c5ac220fa56c3434a
pangnem <sprnd645@gmail.com>
2022-04-30 13:52:37 +0900

    refactor: 당첨 통계 출력시 MatchCount enum 사용하도록 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,25 +2,15 @@ package lotto.view;
 
 import lotto.domain.Lottos;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
 
 public class OutputView {
-    private static final Map<Integer, Integer> WINNING_AMOUNT_MAP = new HashMap<>();
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String SEPARATOR = "---------";
     private static final int START_MATCH_COUNT = 3;
     private static final int END_MATCH_COUNT = 6;
-
-    static {
-        WINNING_AMOUNT_MAP.put(3, 5000);
-        WINNING_AMOUNT_MAP.put(4, 50000);
-        WINNING_AMOUNT_MAP.put(5, 1500000);
-        WINNING_AMOUNT_MAP.put(6, 2000000000);
-    }
 
     public static void outputLottoNumbers(Lottos lottos) {
         System.out.printf("%s개를 구매했습니다.\n", lottos.length());
@@ -38,11 +28,10 @@ public class OutputView {
     }
 
     private static IntConsumer outputMatchStatistics(List<Integer> matchLottos) {
-        return i ->
-                System.out.printf("%s개 일치 (%s원)- %s\n",
-                        i,
-                        getWinningAmount(i),
-                        getCount(matchLottos, i));
+        return i -> System.out.printf("%s개 일치 (%s원)- %s\n",
+                i,
+                MatchCount.getWinningAmountWith(i),
+                getCount(matchLottos, i));
     }
 
     private static int getCount(List<Integer> matchLottos, int i) {
@@ -50,8 +39,5 @@ public class OutputView {
                 .filter(matchLotto -> matchLotto == i)
                 .count();
     }
-
-    private static int getWinningAmount(int i) {
-        return WINNING_AMOUNT_MAP.get(i);
-    }
 }
+
