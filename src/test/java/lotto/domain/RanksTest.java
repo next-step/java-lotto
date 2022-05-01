94ac7516c0eda38378a3890306a017225e4038ff
bky4564 <bky4564@hunet.co.kr>
2022-05-02 08:40:06 +0900

    test: 순위 출력 test 추가

--- src/test/java/lotto/domain/RanksTest.java
+++ src/test/java/lotto/domain/RanksTest.java
@@ -1,13 +1,16 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 class RanksTest {
 
@@ -21,6 +24,21 @@ class RanksTest {
         double profitRatio = ranks.getProfitRatio(BigDecimal.valueOf(money));
         // then
         assertThat(profitRatio).isEqualTo(ratio);
+    }
+
+    @Test
+    @DisplayName("순위 개수를 구하는 map 반환")
+    void returnGroupByMap() {
+        // given
+        Ranks ranks = new Ranks(List.of(Rank.THIRD, Rank.THIRD, Rank.FIRST, Rank.SECOND,Rank.SECOND, Rank.THIRD));
+        // when
+        Map<Rank, Integer> rankIntegerMap = ranks.groupBy();
+        System.out.println("rankIntegerMap = " + rankIntegerMap);
+        assertThat(rankIntegerMap).containsExactly(
+                entry(Rank.FIRST, 1),
+                entry(Rank.SECOND, 2),
+                entry(Rank.THIRD, 3),
+                entry(Rank.FOURTH, 0));
     }
 
 }
