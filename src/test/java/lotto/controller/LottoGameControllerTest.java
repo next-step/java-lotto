bec62d4ba400a136400a0247b0a4d9e25e37e4b0
itdar <thesockerr@gmail.com>
2022-05-02 02:09:22 +0900

    test: 로또 게임 시 수동번호 추가 기능 테스트

--- src/test/java/lotto/controller/LottoGameControllerTest.java
+++ src/test/java/lotto/controller/LottoGameControllerTest.java
@@ -1,11 +1,13 @@
 package lotto.controller;
 
+import static lotto.domain.LottoTicket.ONE_TICKET_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 import lotto.domain.LottoResult;
 import lotto.domain.LottoResults;
+import lotto.domain.LottoTicket;
 import lotto.domain.LottoTickets;
 import org.junit.jupiter.api.Test;
 
@@ -14,19 +16,35 @@ class LottoGameControllerTest {
     private LottoGameController lottoGame = new LottoGameController();
 
     @Test
-    void createLottoTicketsTest() {
-        LottoTickets lottoTickets = lottoGame.createLottoTickets(3000);
+    void createLottoTicketsWithManualTest() {
+        final int buyingMoney = 5000;
+        LottoTickets manualLottoTickets = new LottoTickets(Arrays.asList(
+            LottoTicket.create(1,2,3,4,5,6),
+            LottoTicket.create(2,3,4,5,6,7),
+            LottoTicket.create(3,4,5,6,7,8)
+        ));
 
-        assertThat(lottoTickets.count()).isEqualTo(3);
+        LottoTickets lottoTickets = lottoGame.createLottoTicketsWithManual(buyingMoney, manualLottoTickets);
+
+        assertThat(lottoTickets.size()).isEqualTo(buyingMoney / ONE_TICKET_PRICE);
+    }
+
+    @Test
+    void createLottoTicketsTest() {
+        final int buyingMoney = 5000;
+        LottoTickets lottoTickets = lottoGame.createLottoTickets(buyingMoney);
+
+        assertThat(lottoTickets.count()).isEqualTo(buyingMoney / ONE_TICKET_PRICE);
     }
 
     @Test
     void analysisTest() {
-        LottoTickets lottoTickets = lottoGame.createLottoTickets(3000);
+        final int buyingMoney = 5000;
+        LottoTickets lottoTickets = lottoGame.createLottoTickets(buyingMoney);
 
         LottoResults lottoResults = lottoGame.analysisResult(lottoTickets, Arrays.asList(1, 2, 3, 4, 5, 6), 7);
 
-        assertThat(lottoResults.size()).isEqualTo(3);
+        assertThat(lottoResults.size()).isEqualTo(buyingMoney / ONE_TICKET_PRICE);
     }
 
 }
