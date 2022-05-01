703c136b81c2858fbac601460a31a59dac19d5e7
최영훈 <c_yh0327@naver.com>
2022-05-01 20:47:05 +0900

    [feat] LottoTicket
    1. 중복 번호, 로또 번호 갯수 검증 추가

---
+++ src/main/java/Lotto/domain/LottoTicket.java
@@ -0,0 +1,29 @@
+package Lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoTicket {
+    private final int LOTTO_NUMBER_COUNT = 6;
+    private final List<LottoNumber> LottoTicket;
+
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
+        validateLottoSize(lottoNumbers);
+        validateDuplicate(lottoNumbers);
+        this.LottoTicket = lottoNumbers;
+    }
+
+    private void validateLottoSize(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 번호는 6자리여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<LottoNumber> lottoNumbers) {
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoNumbers);
+        if (lottoNumberSet.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 번호는 중복이 될 수 없습니다.");
+        }
+    }
+}
