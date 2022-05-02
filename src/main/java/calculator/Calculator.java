e3554d09da2ae2553253941d30745c59da9c6941
polynomeer <polynomeer@gmail.com>
2022-05-02 21:37:41 +0900

    feat: Implement calculation logic following requirements

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,6 +3,35 @@ package calculator;
 public class Calculator {
 
     public static int calculate(String input) {
-        return 0;
+        if (input == null || input.trim().equals("")) {
+            throw new IllegalArgumentException("Input cannot be null or empty.");
+        }
+
+        String[] tokens = input.split(" ");
+
+        int result = Integer.parseInt(tokens[0]);
+
+        for (int i = 0; i < tokens.length - 2; i += 2) {
+            int operand = Integer.parseInt(tokens[i + 2]);
+            String operator = tokens[i + 1];
+            result = calculate(operator, result, operand);
+        }
+
+        return result;
+    }
+
+    private static int calculate(String operator, int operand1, int operand2) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Input cannot be parsed.");
+        }
     }
 }
