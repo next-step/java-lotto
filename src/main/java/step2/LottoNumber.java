655616a10404e38c57dc8f25a99dfa0d1c7f61b7
sanghyeon <sanghyeon@finda.co.kr>
2022-05-02 22:28:41 +0900

    feat: LottoNumber 구현

---
+++ src/main/java/step2/LottoNumber.java
@@ -0,0 +1,34 @@
+package step2;
+
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private final int value;
+
+    public LottoNumber(String text) {
+        int input = Integer.parseInt(text);
+        validate(input);
+        this.value = input;
+    }
+
+    private void validate(int input) {
+        if (input < 1 || input > 45) {
+            throw new IllegalArgumentException("범위를 벗어나는 번호입니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
