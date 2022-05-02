031a8111e401a575de8aed3972ceb02f2882b8e4
wlroh <wlroh.paul@gmail.com>
2022-05-02 21:53:21 +0900

    refactor: 가독성 위해 언더스코어 적용

--- src/main/java/lotto/constant/MatchResult.java
+++ src/main/java/lotto/constant/MatchResult.java
@@ -4,10 +4,10 @@ import java.util.stream.Stream;
 
 public enum MatchResult {
 
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000);
 
     private final int matchCount;
     private final int winPrice;
