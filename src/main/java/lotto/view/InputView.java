f9e73efedab91e4e5d6e44a73958ec9ab46263a6
younlll <youn.lee96@gmail.com>
2022-05-03 04:16:27 +0900

    refactor: add validation non-numberic

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,15 @@
 package lotto.view;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
+  private static final Pattern WINNING_NUMBER_PATTERN = Pattern.compile("^(\\d+, )\\d+$");
+
   private static final String INPUT_PURCHASED_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
   private static final String INPUT_LAST_WEEK_WINNING_LOTTO_NUMBERS_PROMPT = "지난 주 당첨 번호를 입력해 주세요.";
+  private static final String WINNING_NUMBER_PATTERN_NOT_MATCH_ERROR_MESSAGE = "입력된 우승 로또 번호는 %s으로 숫자 이외의 문자가 입력되었습니다.";
 
   private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -16,6 +20,16 @@ public class InputView {
 
   public static String inputLastWeekWinningLotto() {
     System.out.println(INPUT_LAST_WEEK_WINNING_LOTTO_NUMBERS_PROMPT);
-    return SCANNER.nextLine();
+    String winningLottoNumber = SCANNER.nextLine();
+
+    validate(winningLottoNumber);
+
+    return winningLottoNumber;
+  }
+
+  private static void validate(String winningLottoNumber) {
+    if (!WINNING_NUMBER_PATTERN.matcher(winningLottoNumber).matches()) {
+      throw new IllegalArgumentException(String.format(WINNING_NUMBER_PATTERN_NOT_MATCH_ERROR_MESSAGE, winningLottoNumber));
+    }
   }
 }
