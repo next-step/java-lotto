569787ef86490de6c6873cd7b62257e1bf67be11
rumor1993 <rumor1993@naver.com>
2022-05-03 01:32:22 +0900

    feat: 로또 객체 구현
    로또를 구매하는 기능
    로또를 구매하면 자동으로 번호를 생성
    로또 번호 정렬

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+    private Numbers numbers;
+
+    public Lotto() {
+        this.numbers = new Numbers();
+    }
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = new Numbers(numbers);
+    }
+
+    public int size() {
+        return numbers.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return numbers.equals(lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return "numbers=" + numbers + '}';
+    }
+
+    public List<Integer> contains(List<Integer> prevLottoNumbers) {
+        return numbers.contains(prevLottoNumbers);
+    }
+}
