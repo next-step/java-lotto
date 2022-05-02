6734ef72cd41b0d8fa91a98d3565d946134b6b54
chkim <cndghks0213@gmail.com>
2022-05-02 15:25:21 +0900

    feat: 연산자 클래스 구현

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -1,5 +1,41 @@
 package calculator.domain;
 
-public interface Operator {
-    int operate(int number1, int number2);
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum Operator {
+    PLUS("+", Integer::sum),
+    MINUS("-", (number1, number2) -> number1 - number2),
+    MULTIPLY("*", (number1, number2) -> number1 * number2),
+    DIVIDE("/", (number1, number2) -> {
+        if (number2 == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        }
+        return number1 / number2;
+    });
+
+    private final String symbol;
+    private final BiFunction<Integer, Integer, Integer> operator;
+
+    Operator(String symbol, BiFunction<Integer, Integer, Integer> operator) {
+        this.symbol = symbol;
+        this.operator = operator;
+    }
+
+    public static Operator findOperatorFromSymbol(String symbol) {
+        return Arrays.stream(Operator.values())
+                .filter(operator -> operator.isEqualOperator(symbol))
+                .findAny()
+                .orElseThrow(() -> {
+                    throw new IllegalArgumentException("존재하지 않는 연산자입니다.");
+                });
+    }
+
+    private boolean isEqualOperator(String symbol) {
+        return this.symbol.equals(symbol);
+    }
+
+    public int calculate(int number1, int number2) {
+        return operator.apply(number1, number2);
+    }
 }
--- src/test/java/calculator/domain/OperatorTest.java
+++ src/test/java/calculator/domain/OperatorTest.java
@@ -4,24 +4,39 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 class OperatorTest {
 
-    @DisplayName("사칙연산 테스트")
+    @DisplayName("일치하는 연산자를 반환하는 기능 테스트")
+    @CsvSource(value = {"+:PLUS", "-:MINUS", "*:MULTIPLY", "/:DIVIDE"}, delimiter = ':')
     @ParameterizedTest
-    @CsvSource(value = {"1:2:+:3", "4:5:+:9", "5:2:*:10", "10:2:/:5"}, delimiter = ':')
-    void operateTest(int number1, int number2, String operatorString, int result) {
-        Operator operator = OperatorSelector.findOperator(operatorString);
-        assertThat(operator.operate(number1, number2)).isEqualTo(result);
+    void isEqualOperatorTest(String symbol, Operator operator) {
+        assertThat(Operator.findOperatorFromSymbol(symbol)).isEqualTo(operator);
     }
 
-    @DisplayName("사칙연산 테스트 - 실패 케이스")
+    @DisplayName("일치하는 연산자를 반환하는 기능 테스트 - 실패 케이스")
+    @ValueSource(strings = {"!", "#", "1", "4", "e"})
+    @ParameterizedTest
+    void isEqualOperatorTestFail(String symbol) {
+        assertThatThrownBy(() -> Operator.findOperatorFromSymbol(symbol))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("연산자로 계산하는 기능 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"10:2:+:12", "10:2:-:8", "10:2:*:20", "10:2:/:5", "10:3:/:3"}, delimiter = ':')
+    void calculateTest(int number1, int number2, String symbol, int result) {
+        Operator operator = Operator.findOperatorFromSymbol(symbol);
+        assertThat(operator.calculate(number1, number2)).isEqualTo(result);
+    }
+
+    @DisplayName("연산자로 계산하는 기능 테스트 - 0으로 나눈 경우")
     @Test
-    void operateTestFail() {
-        Operator operator = OperatorSelector.findOperator("/");
-        assertThatIllegalArgumentException().isThrownBy(() -> operator.operate(5, 0));
+    void calculateTestFail() {
+        assertThatThrownBy(() -> Operator.DIVIDE.calculate(10, 0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
