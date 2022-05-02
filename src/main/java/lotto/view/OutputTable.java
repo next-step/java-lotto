2ed5a5bc7fe4c2f8f301c9905c027ea81d66a6bc
younghun <asqwklop12@naver.com>
2022-05-03 00:59:49 +0900

    style 2등 당첨 print추가

--- src/main/java/lotto/view/OutputTable.java
+++ src/main/java/lotto/view/OutputTable.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import java.util.List;
 import lotto.dto.WinningResultDto;
+import lotto.enums.Grade;
 import lotto.model.Lotto;
 
 public final class OutputTable {
@@ -12,7 +13,8 @@ public final class OutputTable {
   private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
   private static final String STATISTICS_MESSAGE = "당첨 통계";
   private static final String UNDER_BAR = "_________";
-  private static final String HISTORY_MESSAGE = "%d개 일치 (%d원)- %d개\n";
+  private static final String HISTORY_MESSAGE = "%d개 일치 (%d원)- %d개";
+  private static final String HISTORY_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원)- %d개";
   private static final String YIELD_MESSAGE = "총 수익률은 %.2f입니다. (기준이 1이기 때문에 결과적으로 %s라는 의미임)";
   private static final String WIN_MESSAGE = "이득";
   private static final String LOSE_MESSAGE = "손해";
@@ -46,9 +48,15 @@ public final class OutputTable {
 
   public static void resultStatistics(List<WinningResultDto> histories) {
     for (WinningResultDto history : histories) {
-      System.out.printf(HISTORY_MESSAGE, history.getGrade().getExpectNumber(),
-          history.getGrade().getAwardPrice(), history.getCount());
+      System.out.println(getMessage(history.getGrade(),history.getCount()));
     }
+  }
+
+  private static String getMessage(Grade grade, int count) {
+    if (Grade.SECOND == grade) {
+      return String.format(HISTORY_BONUS_MESSAGE, grade.getExpectNumber(),grade.getAwardPrice(), count);
+    }
+    return String.format(HISTORY_MESSAGE, grade.getExpectNumber(),grade.getAwardPrice(), count);
   }
 
   public static void printYield(double yield, boolean isStandard) {
