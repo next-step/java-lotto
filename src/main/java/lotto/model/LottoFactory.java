2756d92bab1dae25428503deeefb93966bed0072
younghun <asqwklop12@naver.com>
2022-05-03 01:31:58 +0900

    style 코드 정리

--- src/main/java/lotto/model/LottoFactory.java
+++ src/main/java/lotto/model/LottoFactory.java
@@ -5,11 +5,13 @@ import java.util.HashSet;
 import java.util.List;
 
 public final class LottoFactory {
+
   private static final int LOTTO_MAX_COUNT = 6;
   private static final int LOTTO_START_INDEX = 0;
 
   private static LottoFactory instance;
   private final List<Integer> init;
+
   private LottoFactory(List<Integer> init) {
     this.init = init;
   }
@@ -23,7 +25,7 @@ public final class LottoFactory {
 
   public Lotto apply() {
     Collections.shuffle(init);
-    return new Lotto(new HashSet<>(init.subList(LOTTO_START_INDEX,LOTTO_MAX_COUNT)));
+    return new Lotto(new HashSet<>(init.subList(LOTTO_START_INDEX, LOTTO_MAX_COUNT)));
   }
 
 
