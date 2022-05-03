1f21ba0266da8d7079f2b9e3e9beebd39642a54a
Roman <roman@channel.io>
2022-05-04 03:17:14 +0900

    Refactor :: Controller에서 출력 책임을 LottoOutputView로 이관
    (cherry picked from commit 74e377647b54f2579bc587e39602e651a8b179bb)

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,10 +1,17 @@
 package lotto;
 
+import static util.LottoOutputView.printResultInfo;
+import static util.LottoOutputView.printRevenueRate;
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
 import util.LottoInputView;
 import util.LottoOutputView;
 
 public class LottoController {
+
+  private static final int REVENUE_RATE_SCALE = 2;
 
   public void proceed() {
     PaymentAmount paymentAmount = LottoInputView.insertPaymentAmount();
@@ -15,9 +22,24 @@ public class LottoController {
     LottoOutputView.print(lottoList.toStringForPrinting());
 
     Lotto winningLotto = new Lotto(LottoInputView.insertWinningLotto());
-    BigDecimal revenueRate = lottoList.calculateRevenueWithWinningNumber(winningLotto);
 
-    LottoOutputView.printRevenue(revenueRate);
+    printResultInfo();
+    int totalRevenue = Arrays.stream(LottoPrize.values())
+        .mapToInt(lottoPrize -> {
+          int matchedLottoCount = lottoPrize.getMatchedLottoCount(winningLotto, lottoList);
+          LottoOutputView.printMatchedLottoCount(lottoPrize, matchedLottoCount);
+          return lottoPrize.getRevenue(matchedLottoCount);
+        })
+        .sum();
+
+    BigDecimal revenueRate = BigDecimal.valueOf(totalRevenue)
+        .divide(
+            BigDecimal.valueOf(lottoList.getTotalPurchaseAmount()),
+            REVENUE_RATE_SCALE,
+            RoundingMode.HALF_UP
+        );
+
+    printRevenueRate(revenueRate);
   }
 
   public static LottoController init() {
--- src/main/java/util/LottoOutputView.java
+++ src/main/java/util/LottoOutputView.java
@@ -1,24 +1,36 @@
 package util;
 
 import java.math.BigDecimal;
+import lotto.LottoPrize;
 
 public class LottoOutputView {
 
   private static final String MESSAGE_FOR_PRINTING_PURCHASE_AMOUNT = "%s개를 구매했습니다.";
   private static final String MESSAGE_FOR_PRINTING_RESULT = "총 수익률은 %s 입니다.";
+  private static final String MESSAGE_FOR_RESULT_INFO = "당첨 통계\n--------";
 
-  private LottoOutputView() {}
 
-  public static void printPurchaseAmount(int purchaseAmount) {
-    LottoOutputView.print(buildPurchaseAmountMessage(purchaseAmount));
+  private LottoOutputView() {
   }
 
-  public static void printRevenue(BigDecimal revenueRate) {
-    LottoOutputView.print(String.format(MESSAGE_FOR_PRINTING_RESULT, revenueRate));
+  public static void printPurchaseAmount(int purchaseAmount) {
+    print(buildPurchaseAmountMessage(purchaseAmount));
+  }
+
+  public static void printRevenueRate(BigDecimal revenueRate) {
+    print(String.format(MESSAGE_FOR_PRINTING_RESULT, revenueRate));
+  }
+
+  public static void printMatchedLottoCount(LottoPrize lottoPrize, int matchedLottoCount) {
+    print(lottoPrize.toStringForPrinting(matchedLottoCount));
   }
 
   public static void print(String text) {
     System.out.println(text);
+  }
+
+  public static void printResultInfo() {
+    print(MESSAGE_FOR_RESULT_INFO);
   }
 
   private static String buildPurchaseAmountMessage(int purchaseAmount) {
