9d7fe3ebd6150144b82e01ac2db6130f26431adb
wlroh <wlroh.paul@gmail.com>
2022-05-04 01:07:32 +0900

    test: DisplayName 수정 및 테스트 코드 수정

--- src/test/java/lotto/util/SplitUtilTest.java
+++ src/test/java/lotto/util/SplitUtilTest.java
@@ -8,9 +8,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SplitUtilTest {
 
     @Test
-    @DisplayName("'1, 2, 3, 5' 를 입력하면 {'1', '2', '3', '5'} 문자열 배열을 반환한다.")
+    @DisplayName("지정된 구분자(,) 문자열을 분리한다.")
     void split() {
-        assertThat(SplitUtil.split("1, 2, 3, 5")).isEqualTo(new String[]{"1", "2", "3", "5"});
+        assertThat(SplitUtil.split("1, 2, 3, 5")).containsExactly("1", "2", "3", "5");
     }
 
 }
