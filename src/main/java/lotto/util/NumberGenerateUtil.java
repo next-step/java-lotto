3b3cb1d3de86ace138f6cf276a0fabb416b22f9d
hahoho87 <bbaing4564@gmail.com>
2022-05-04 00:26:23 +0900

    feat: bound validation 추가

--- src/main/java/lotto/util/NumberGenerateUtil.java
+++ src/main/java/lotto/util/NumberGenerateUtil.java
@@ -1,5 +1,7 @@
 package lotto.util;
 
+import lotto.exception.InvalidNumberBoundException;
+
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -7,15 +9,23 @@ import java.util.stream.Collectors;
 public class NumberGenerateUtil {
 
     private static final int MIN_NUMBER_BOUND = 1;
+    private static final Random RANDOM = new Random();
 
     private NumberGenerateUtil() {
     }
 
     public static Set<Integer> generateNumbers(int bound, int size) {
-        return new Random().ints(MIN_NUMBER_BOUND, bound)
+        validBound(bound, size);
+        return RANDOM.ints(MIN_NUMBER_BOUND, bound)
                 .distinct()
                 .limit(size)
                 .boxed()
                 .collect(Collectors.toSet());
+    }
+
+    private static void validBound(int bound, int size) {
+        if (bound <= size) {
+            throw new InvalidNumberBoundException();
+        }
     }
 }
