a98e750f5a08bb8c057cff3c2ed478769f27ac96
양경희 <alicesister1@gmail.com>
2022-05-03 22:52:19 +0900

    refactor: Operator 변경 문자열계산기에 반영

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,47 +6,32 @@ import java.util.Objects;
 public class StringCalculator {
 
   static final String EMPTY_EQUATION_MESSAGE = "계산식을 입력하세요";
-  static final String INVALID_OPERATOR_MESSAGE = "사칙연산 기호가 아닙니다.";
 
-  private static final String SIGN_ADD = "+";
-  private static final String SIGN_SUBTRACT = "-";
-  private static final String SIGN_MULTIPLY = "*";
-  private static final String SIGN_DIVIDE = "/";
+  private static final int FIRST_INDEX = 0;
 
   public static Number calculate(String s) {
-    Objects.requireNonNull(s);
-    if (s.isBlank()) {
-      throw new IllegalArgumentException(EMPTY_EQUATION_MESSAGE);
-    }
+    validateInput(s);
 
     List<String> strings = Splitter.split(s);
-    Number result = Number.createNumber(strings.get(0));
+    Number result = Number.createNumber(strings.get(FIRST_INDEX));
     int size = strings.size();
     for (int i = 1; i < size - 1; i = i + 2) {
-      String operator = strings.get(i);
+      String sign = strings.get(i);
+      Operator operator = Operator.find(sign);
       Number current = Number.createNumber(strings.get(i + 1));
       result = calculate(operator, result, current);
     }
     return result;
   }
 
-  private static Number calculate(String operator, Number result, Number current) {
-    if (Objects.equals(operator, SIGN_ADD)) {
-      return result.add(current);
+  private static void validateInput(String s) {
+    Objects.requireNonNull(s);
+    if (s.isBlank()) {
+      throw new IllegalArgumentException(EMPTY_EQUATION_MESSAGE);
     }
+  }
 
-    if (Objects.equals(operator, SIGN_SUBTRACT)) {
-      return result.subtract(current);
-    }
-
-    if (Objects.equals(operator, SIGN_MULTIPLY)) {
-      return result.multiply(current);
-    }
-
-    if (Objects.equals(operator, SIGN_DIVIDE)) {
-      return result.divide(current);
-    }
-
-    throw new IllegalArgumentException(INVALID_OPERATOR_MESSAGE);
+  private static Number calculate(Operator operator, Number result, Number current) {
+    return Operator.operate(operator, result, current);
   }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -29,6 +29,13 @@ public class StringCalculatorTest {
     assertThatIllegalArgumentException().isThrownBy(() -> StringCalculator.calculate(input))
         .withMessage(StringCalculator.EMPTY_EQUATION_MESSAGE);
   }
+//
+//  @DisplayName("입력받은 문자열을 연산자와 숫자 객체로 분리한다")
+//  @Test
+//  void splitOperatorAndNumbers() {
+//    String input = "1 + 2";
+//    assertThat(StringCalculator.splitOperatorAndNumbers(input)).isEqualTo()
+//  }
 
   @DisplayName("문자열 계산기 덧셈")
   @ParameterizedTest
@@ -77,6 +84,6 @@ public class StringCalculatorTest {
   public void notOperator() {
     assertThatIllegalArgumentException().isThrownBy(
             () -> StringCalculator.calculate("2 ^ 3"))
-        .withMessage(StringCalculator.INVALID_OPERATOR_MESSAGE);
+        .withMessageContaining("지원되지 않는 기호입니다");
   }
 }
