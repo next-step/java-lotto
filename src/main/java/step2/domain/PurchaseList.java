b07635632884088d392f89afc45321ccfa2f72b6
sanghyeon <sanghyeon@finda.co.kr>
2022-05-04 00:03:52 +0900

    feat: PurchaseList 의 각 구매 로또와 지난 당첨 회차 비교

--- src/main/java/step2/domain/PurchaseList.java
+++ src/main/java/step2/domain/PurchaseList.java
@@ -4,6 +4,7 @@ import step2.domain.impl.AutoPurchase;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PurchaseList {
 
@@ -16,13 +17,23 @@ public class PurchaseList {
     }
 
     public PurchaseList(int purchaseCount) {
+        this(purchaseCount, AUTO_PURCHASE);
+    }
+
+    public PurchaseList(int purchaseCount, PurchaseStrategy purchaseStrategy) {
         this.values = new ArrayList<>();
         for (int i = 0; i < purchaseCount; i++) {
-            values.add(new Lotto(AUTO_PURCHASE));
+            values.add(new Lotto(purchaseStrategy));
         }
     }
 
     public List<Lotto> getValues() {
         return this.values;
+    }
+
+    public List<Long> calculateHitCount(Winner winner) {
+        return values.stream()
+                .map(lotto -> lotto.calculateHitCount(winner))
+                .collect(Collectors.toList());
     }
 }
