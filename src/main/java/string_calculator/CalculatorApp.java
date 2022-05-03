a546805adbf1d065549ad076f23daf7a9ad82bf8
devgony <devgony@gmail.com>
2022-05-03 22:57:21 +0900

    refactor: step1: keep the number of members less than 2

--- src/main/java/string_calculator/CalculatorApp.java
+++ src/main/java/string_calculator/CalculatorApp.java
@@ -4,10 +4,8 @@ public class CalculatorApp {
     public static void main(String[] args) {
         try {
             Controller controller = new Controller();
-            controller.scan();
-            controller.parse();
-            controller.computes();
-            controller.print();
+            controller.parse(controller.scan());
+            controller.print(controller.computes());
         }
         catch (Exception exception) {
             System.out.println(exception);
--- src/test/java/string_calculator/ControllerTest.java
+++ src/test/java/string_calculator/ControllerTest.java
@@ -9,8 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ControllerTest {
     @Test
     void parseShouldReturnEqualValuesInBothControllerAndParser() {
-        Controller controller = new Controller("1 + 2");
-        controller.parse();
+        Controller controller = new Controller();
+        controller.parse("1 + 2");
         List<Object> actual = controller.parser.values;
         Parser parser = new Parser();
         parser.parse("1 + 2");
@@ -20,10 +20,9 @@ public class ControllerTest {
 
     @Test
     void computesShouldReturnEqualValuesInBothControllerAndCalculator() {
-        Controller controller = new Controller("1 + 2");
-        controller.parse();
-        controller.computes();
-        Integer actual = controller.result;
+        Controller controller = new Controller();
+        controller.parse("1 + 2");
+        Integer actual = controller.computes();;
         Calculator calculator = new Calculator();
         Integer expected = calculator.computes(controller.parser.values);
         assertThat(actual).isEqualTo(expected);
