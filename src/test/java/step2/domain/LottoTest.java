8d0f3786eb56a157d654dc3b558fcd9192300698
sanghyeon <sanghyeon@finda.co.kr>
2022-05-03 23:49:40 +0900

    test: 로또와 지난 당첨 회차 비교

--- src/test/java/step2/domain/LottoTest.java
+++ src/test/java/step2/domain/LottoTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -30,9 +33,30 @@ class LottoTest {
     }
 
     @Test
-    void 구매전략을이_반환하는_SET의_사이즈가_6개_아닌_경우_예외() {
+    void 구매전략이_반환하는_SET의_사이즈가_6개_아닌_경우_예외() {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> new Lotto(MALFORMED_STRATEGY)
         );
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "1, 2, 3, 4, 5, 6:6",
+                    "11, 2, 3, 4, 5, 6:5",
+                    "11, 12, 3, 4, 5, 6:4",
+                    "11, 12, 13, 4, 5, 6:3",
+                    "11, 12, 13, 14, 5, 6:2",
+                    "11, 12, 13, 14, 15, 6:1"
+            },
+            delimiter = ':'
+    )
+    void 현재_로또와_지난회차_당첨_번호를_비교하여_맞춘_개수를_반환(String input, int hitCount) {
+        PurchaseStrategy purchaseStrategy = (count) -> new HashSet<>(Arrays.asList("1", "2", "3", "4", "5", "6"));
+        Winner winner = new Winner(input);
+
+        Lotto lotto = new Lotto(purchaseStrategy);
+
+        assertThat(lotto.calculateHitCount(winner)).isEqualTo(hitCount);
     }
 }
