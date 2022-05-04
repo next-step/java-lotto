15033ae61bc1d4d076ddb055dbfb86743bda0078
guswns1659 <guswns1659@gmail.com>
2022-05-05 06:34:11 +0900

    step3 refactor: (리뷰) Pattern을 포장한 객체 생성

---
+++ src/test/java/autolotto/domain/LottoNumberPatternTest.java
@@ -0,0 +1,21 @@
+package autolotto.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoNumberPatternTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "45"})
+    void lottoNumberGiven_ReturnTrue(String number) {
+        assertThat(new LottoNumberPattern().match(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46"})
+    void notLottoNumberGiven_ReturnFalse(String number) {
+        assertThat(new LottoNumberPattern().match(number)).isFalse();
+    }
+}
