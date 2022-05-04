3d961d40b3ef3d88c76ad2aa572f68c166d35fbf
slogan-choi <slogan.choi@gmail.com>
2022-05-04 16:12:01 +0900

    feat: step1 2차 피드백 반영

--- src/main/java/study/step1/domain/StringCalculator.java
+++ src/main/java/study/step1/domain/StringCalculator.java
@@ -1,18 +1,12 @@
 package study.step1.domain;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.function.ToIntBiFunction;
 
 public class StringCalculator {
     private static final String SPACE = " ";
     private final Formula formula;
-    private Map<Character, Operator> operators = new HashMap<>();
 
     public StringCalculator(Formula formula) {
-        for (Operator value : Operator.values()) {
-            operators.put(value.operator, value);
-        }
         this.formula = formula;
     }
 
@@ -20,31 +14,41 @@ public class StringCalculator {
         String[] strArr = formula.getString().split(SPACE);
         int result = Integer.parseInt(strArr[0]);
         for (int i = 0; i < strArr.length - 2; i += 2) {
-            result = run(strArr[i + 1].charAt(0), result, Integer.parseInt(strArr[i + 2]));
+            result = run(result, String.valueOf(strArr[i + 1].charAt(0)), Integer.parseInt(strArr[i + 2]));
         }
         return result;
     }
 
-    public int run(char operator, int num1 ,int num2) {
-        return operators.get(operator).calculate(num1, num2);
+    public int run(int num1, String operator, int num2) {
+        int result = 0;
+        for (Operator value : Operator.values()) {
+            if (operator.equals(value.getOperator())) {
+                result = Operator.valueOf(value.name()).calculate(num1, num2);
+            }
+        }
+        return result;
     }
 
     enum Operator {
-        PLUS('+', (n1, n2) -> n1 + n2),
-        MINUS('-', (n1, n2) -> n1 - n2),
-        MULT('*', (n1, n2) -> n1 * n2),
-        DIVIDE('/', (n1, n2) -> n1 / n2);
+        PLUS("+", (n1, n2) -> n1 + n2),
+        MINUS("-", (n1, n2) -> n1 - n2),
+        MULT("*", (n1, n2) -> n1 * n2),
+        DIVIDE("/", (n1, n2) -> n1 / n2);
 
-        private char operator;
-        private BiFunction<Integer, Integer, Integer> expression;
+        private String operator;
+        private ToIntBiFunction<Integer, Integer> expression;
 
-        Operator(char operator, BiFunction<Integer, Integer, Integer> expression) {
+        Operator(String operator, ToIntBiFunction<Integer, Integer> expression) {
             this.operator = operator;
             this.expression = expression;
         }
 
+        public String getOperator() {
+            return operator;
+        }
+
         public int calculate(int num1, int num2) {
-            return this.expression.apply(num1, num2);
+            return expression.applyAsInt(num1, num2);
         }
     }
 }
