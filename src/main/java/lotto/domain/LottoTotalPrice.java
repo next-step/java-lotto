1c13490cfa7031b04b86f7d12e30270f4e0a7786
wu2ee <gulb1602@gmail.com>
2022-05-06 00:44:40 +0900

    refactor : [로또] 로또 총 구매 금액 객체 생성자 파라미터 검증 변경 (LottoNumber 객체와 동일하게 변경)

--- src/main/java/lotto/domain/LottoTotalPrice.java
+++ src/main/java/lotto/domain/LottoTotalPrice.java
@@ -7,14 +7,18 @@ public class LottoTotalPrice {
     private final int totalPrice;
 
     LottoTotalPrice(int totalPrice) {
-        this.totalPrice = validateTotalPriceRange(totalPrice);
+        validateTotalPriceRange(totalPrice);
+        this.totalPrice = totalPrice;
     }
 
-    public LottoTotalPrice(String totalPrice) {
-        if (!isDigit(totalPrice)) {
-            throw new IllegalArgumentException(String.format("%s는 숫자가 아닙니다.", totalPrice));
+    public LottoTotalPrice(String stringTotalPrice) {
+        try {
+            int totalPrice = Integer.parseInt(stringTotalPrice);
+            validateTotalPriceRange(totalPrice);
+            this.totalPrice = totalPrice;
+        } catch (NumberFormatException exception) {
+            throw new NumberFormatException(String.format("%s는 숫자가 아닙니다.", stringTotalPrice));
         }
-        this.totalPrice = validateTotalPriceRange(Integer.parseInt(totalPrice));
     }
 
     public int getTotalPrice() {
@@ -25,15 +29,10 @@ public class LottoTotalPrice {
         return this.totalPrice / UNIT_LOTTO_PRICE;
     }
 
-    private boolean isDigit(String number) {
-        return number.chars().filter(c -> c != '-').allMatch(Character::isDigit);
-    }
-
-    private int validateTotalPriceRange(int totalPrice) {
+    private void validateTotalPriceRange(int totalPrice) {
         if (totalPrice < ZERO) {
             throw new IllegalArgumentException(String.format("구매금액(입력 받은 구매 금액 : %d)은 음수일 수 없습니다.", totalPrice));
         }
-        return totalPrice;
     }
 
     @Override
