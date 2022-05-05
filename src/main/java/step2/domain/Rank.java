9093a4c4af782449344c21d6db740c9bd98669b4
bluewow <dogseen@gmail.com>
2022-05-05 19:40:55 +0900

    refactor: 언더스코어 추가

--- src/main/java/step2/domain/Rank.java
+++ src/main/java/step2/domain/Rank.java
@@ -1,10 +1,10 @@
 package step2.domain;
 
 public enum Rank {
-    POSTION_4(5000, 3),
-    POSTION_3(50000, 4),
-    POSTION_2(1500000, 5),
-    POSTION_1(2000000000, 6);
+    POSTION_4(5_000, 3),
+    POSTION_3(50_000, 4),
+    POSTION_2(1_500_000, 5),
+    POSTION_1(2_000_000_000, 6);
 
     public final int price;
     public final int matchCount;
