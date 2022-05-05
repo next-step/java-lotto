f729b3e4275419d6a4e2dd5842585e66c868e2ab
Roman <roman@channel.io>
2022-05-05 18:17:26 +0900

    Feat :: 보너스숫자 입력 추가
    (cherry picked from commit 20db5a5bd6e16ca258c9b05aae618a68e4148b6f)

--- src/main/java/util/LottoInputView.java
+++ src/main/java/util/LottoInputView.java
@@ -7,20 +7,11 @@ public class LottoInputView {
 
   private static final String MESSAGE_FOR_INSERT_PAYMENT_AMOUNT = "구입 금액을 입력해주세요.";
   private static final String MESSAGE_FOR_INSERT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+  private static final String MESSAGE_FOR_INSERT_BONUS_NUMBERS = "보너스 볼을 입력해 주세요.";
   private static final Scanner SCANNER = new Scanner(System.in);
 
 
   private LottoInputView() {
-  }
-
-  public static Integer scanInt(String description) {
-    printDescription(description);
-    return Integer.parseInt(SCANNER.nextLine());
-  }
-
-  public static String scanString(String description) {
-    printDescription(description);
-    return SCANNER.nextLine();
   }
 
   public static PaymentAmount insertPaymentAmount() {
@@ -31,7 +22,22 @@ public class LottoInputView {
     return scanString(MESSAGE_FOR_INSERT_WINNING_NUMBERS);
   }
 
+  public static int insertBonusNumber() {
+    return scanInt(MESSAGE_FOR_INSERT_BONUS_NUMBERS);
+  }
+
   private static void printDescription(String description) {
     System.out.println(description);
+  }
+
+
+  private static Integer scanInt(String description) {
+    printDescription(description);
+    return Integer.parseInt(SCANNER.nextLine());
+  }
+
+  private static String scanString(String description) {
+    printDescription(description);
+    return SCANNER.nextLine();
   }
 }
