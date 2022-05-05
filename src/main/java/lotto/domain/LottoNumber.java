7e80b92df0d5a34726a90f6125041b8e7668e3eb
wu2ee <gulb1602@gmail.com>
2022-05-05 21:04:27 +0900

    refactor : [로또] 클래스 상수와 인스턴스 변수 개행으로 구분

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -3,6 +3,7 @@ package lotto.domain;
 public class LottoNumber {
     public static final int LOTTO_MIN_NUMBER = 1;
     public static final int LOTTO_MAX_NUMBER = 45;
+
     private final int number;
 
     public LottoNumber(int number) {
