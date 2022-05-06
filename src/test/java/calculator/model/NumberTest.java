4d4179e84067801356794f31fdcaa532a2d46dc8
jshans40 <jshans40@gmail.com>
2022-05-06 13:56:29 +0900

    chore : 불필요 import 제거

--- src/test/java/calculator/model/NumberTest.java
+++ src/test/java/calculator/model/NumberTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NumberTest {
 
