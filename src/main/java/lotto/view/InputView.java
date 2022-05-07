eb3c890919a873c0c24869c8eb16612cfdebbc82
wu2ee <gulb1602@gmail.com>
2022-05-07 16:12:22 +0900

    feat : [로또] 보너스 번호 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     private static final String COMMA_DELIMITER = ",";
     private static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_GUIDE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_BALL_GUIDE = "보너스 볼을 입력해 주세요.";
 
     private InputView() {
     }
@@ -22,5 +23,10 @@ public class InputView {
     public static List<String> scanWinningNumbers() {
         System.out.println(WINNING_NUMBER_GUIDE);
         return SplitStringUtils.split(scanner.nextLine(), COMMA_DELIMITER);
+    }
+
+    public static String scanBonusNumber() {
+        System.out.println(BONUS_BALL_GUIDE);
+        return scanner.nextLine();
     }
 }
