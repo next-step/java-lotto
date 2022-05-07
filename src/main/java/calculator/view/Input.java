131c108c257680e0a9717ef74d5e5a35606eb1ac
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-08 07:05:27 +0900

    refactor: Input, View

--- src/main/java/calculator/view/Input.java
+++ src/main/java/calculator/view/Input.java
@@ -1,14 +1,13 @@
-package view;
+package calculator.view;
 
 import java.util.Scanner;
 
 public class Input {
 
-    private final String INPUT_MSG = "문자열을 입력해주세요";
+    private static final String INPUT_MSG = "문자열을 입력해주세요";
+    private static final Scanner scanner = new Scanner(System.in);
 
-    private final Scanner scanner = new Scanner(System.in);
-
-    public String readExp(){
+    public static String readExp(){
         System.out.println(INPUT_MSG);
         String exp = scanner.nextLine();
         if(exp == null){
@@ -16,5 +15,4 @@ public class Input {
         }
         return exp.replace(" ","");
     }
-
 }
---
+++ src/main/java/calculator/view/Output.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+
+public class Output {
+
+    public static void printResult(int res){
+        System.out.println(res);
+    }
+}
