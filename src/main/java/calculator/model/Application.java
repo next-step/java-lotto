5b0bab229e77bb7ad6a6678495e989fcab100baa
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-08 07:31:09 +0900

    refactor: removeSpace 함수 삭제

--- src/main/java/calculator/model/Application.java
+++ src/main/java/calculator/model/Application.java
@@ -5,14 +5,11 @@ import java.util.List;
 public class Application {
 
     public int calculate(String exp) {
-        exp = removeSpace(exp);
+        exp = exp.replace(" ","");
         List<Number> numbers = Extractor.extractNumbers(exp);
         List<Operator> operators = Extractor.extractOperator(exp);
         Calculator calculator = new Calculator(numbers, operators);
         return calculator.calculate();
     }
 
-    public String removeSpace(String string){
-        return string.replace(" ","");
-    }
 }
