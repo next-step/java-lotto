29aa5c1977cee792a710468325100e29e5bc51fb
Roman <roman@channel.io>
2022-05-07 17:47:17 +0900

    Refactor & Test :: LottoList 리팩토링 및 테스트 수정

--- src/main/java/lotto/LottoList.java
+++ src/main/java/lotto/LottoList.java
@@ -6,12 +6,11 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class LottoList {
 
   private static final String ERROR_MESSAGE_FOR_INVALID_AMOUNT = "Lotto를 생성할 숫자는 0보다 커야합니다.";
-  private static final String LOTTO_DELIMITER = "\n";
   private static final int LOTTO_PRICE = 1000;
   private static final int REVENUE_RATE_SCALE = 2;
 
@@ -21,7 +20,7 @@ public class LottoList {
   LottoList(int lottoAmount) {
     validateAmount(lottoAmount);
     for (int i = 0; i < lottoAmount; i++) {
-      lottoList.add(new Lotto());
+      lottoList.add(Lotto.create());
     }
   }
 
@@ -55,16 +54,14 @@ public class LottoList {
     return getTotalLottoCount() * LOTTO_PRICE;
   }
 
-  public String toStringForPrinting() {
-    StringJoiner joiner = new StringJoiner(LOTTO_DELIMITER);
-    lottoList.forEach(lotto -> joiner.add(lotto.toStringForPrinting()));
-    return joiner.toString();
+  public List<Lotto> getListOfLotto() {
+    return lottoList.stream().collect(Collectors.toUnmodifiableList());
   }
 
   public BigDecimal getRevenueRate(int totalRevenue) {
     return BigDecimal.valueOf(totalRevenue)
         .divide(
-            BigDecimal.valueOf(lottoList.size()),
+            BigDecimal.valueOf(getTotalPurchaseAmount()),
             REVENUE_RATE_SCALE,
             RoundingMode.HALF_UP
         );
