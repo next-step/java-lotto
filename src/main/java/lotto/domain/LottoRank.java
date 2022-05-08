7804d5954cbe7761d12bf080a85b8c2410a4c51c
wu2ee <gulb1602@gmail.com>
2022-05-08 16:32:59 +0900

    refactor : [로또] 불필요한 필드 제거
    - Enum 값 캐싱 대신, 해당 등수를 찾을때마다 조건에 맞는 등수를 반환하도록 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,29 +1,20 @@
 package lotto.domain;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public enum LottoRank {
-    MISS(0, 0, false),
-    FIFTH(3, 5_000, false),
-    FOURTH(4, 50_000, false),
-    THIRD(5, 1_500_000, false),
-    SECOND(5, 30_000_000, true),
-    FIRST(6, 2_000_000_000, false);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
     private static final int MIN_MATCH_COUNT = 0;
-    private static final int FIVE_MATCH_COUNT = 5;
     private static final int MAX_MATCH_COUNT = 6;
-    private static final Map<Integer, LottoRank> BY_LOTTO_RANK = Stream.of(values())
-            .filter(lottoRank -> !lottoRank.matchBonus)
-            .collect(Collectors.toMap(rank -> rank.matchCount, Function.identity()));
 
     private final int matchCount;
     private final int winningMoney;
-    private final boolean matchBonus;
 
     public int matchCount() {
         return this.matchCount;
@@ -33,22 +24,22 @@ public enum LottoRank {
         return this.winningMoney;
     }
 
-    LottoRank(int matchCount, int winningMoney, boolean matchBonus) {
+    LottoRank(int matchCount, int winningMoney) {
         this.matchCount = matchCount;
         this.winningMoney = winningMoney;
-        this.matchBonus = matchBonus;
     }
 
     public static LottoRank findLottoRank(int matchCount, boolean matchBonus) {
         validateMatchCount(matchCount);
-        if (isSecondRank(matchCount, matchBonus)) {
-            return LottoRank.SECOND;
-        }
-        return Optional.ofNullable(BY_LOTTO_RANK.get(matchCount)).orElse(MISS);
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank.isSameMatchCount(matchCount))
+                .filter(lottoRank -> lottoRank != SECOND || matchBonus)
+                .findAny()
+                .orElse(MISS);
     }
 
-    private static boolean isSecondRank(int matchCount, boolean matchBonus) {
-        return matchCount == FIVE_MATCH_COUNT && matchBonus;
+    private boolean isSameMatchCount(int matchCount) {
+        return this.matchCount == matchCount;
     }
 
     private static void validateMatchCount(int matchCount) {
