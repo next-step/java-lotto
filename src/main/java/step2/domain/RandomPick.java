1cb5979559f01b806af989b9e8f615d6fca600bb
bluewow <dogseen@gmail.com>
2022-05-08 12:04:31 +0900

    refactor: randomPick 로직 개선

--- src/main/java/step2/domain/RandomPick.java
+++ src/main/java/step2/domain/RandomPick.java
@@ -7,18 +7,21 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class RandomPick implements LottoPickStrategy {
-    private static final int pickCount = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int START_INDEX = 0;
+    private static final int LAST_INDEX = 6;
+    private static final List<Integer> BASE_NUMBERS = new ArrayList<>();
+
+    static {
+        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+            BASE_NUMBERS.add(i);
+        }
+    }
 
     @Override
     public List<Integer> numberPick() {
-        List<Integer> pick = new ArrayList<>();
-
-        int[] numbers = IntStream.range(1, 46).toArray();
-        Integer[] integers = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-        Collections.shuffle(Arrays.asList(integers));
-        for (int i = 0; i < pickCount; i++) {
-            pick.add(integers[i]);
-        }
-        return pick;
+        Collections.shuffle(BASE_NUMBERS);
+        return List.copyOf(BASE_NUMBERS.subList(START_INDEX, LAST_INDEX));
     }
 }
