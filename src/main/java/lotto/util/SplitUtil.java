f02de1c3a2e7b66226c991b3f215f5df401d6b61
wlroh <wlroh.paul@gmail.com>
2022-05-09 00:54:47 +0900

    refactor: 구분자 동적으로 가능하도록 변경

--- src/main/java/lotto/util/SplitUtil.java
+++ src/main/java/lotto/util/SplitUtil.java
@@ -6,14 +6,18 @@ public class SplitUtil {
 
     private static final String BLANK_REGEX = "\\s";
     private static final String NOT_BLANK = "";
-    private static final String SEPARATOR = ",";
+    private static final String DEFAULT_SEPARATOR = ",";
 
     private SplitUtil() {
         throw new NotSupportInstanceException();
     }
 
     public static String[] split(String value) {
-        return trim(value).split(SEPARATOR);
+        return split(value, DEFAULT_SEPARATOR);
+    }
+
+    public static String[] split(String value, String separator) {
+        return trim(value).split(separator);
     }
 
     private static String trim(String value) {
