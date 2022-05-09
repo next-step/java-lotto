65a5c3f53dd0a4ec9529e425bb1509bd4685d752
dudgns3tp <c_yh0327@naver.com>
2022-05-09 18:37:27 +0900

    [refactor] LottoTicketGroup 매직 상수 처리

--- src/main/java/lotto/domain/LottoTicketGroup.java
+++ src/main/java/lotto/domain/LottoTicketGroup.java
@@ -28,7 +28,7 @@ public class LottoTicketGroup {
             throw new IllegalArgumentException("1000원 이하로는 로또를 구매할 수 없습니다.");
         }
 
-        if (money % 1000 != 0) {
+        if (money % LOTTO_TICKET_PRICE != 0) {
             throw new IllegalArgumentException("로또는 1000원 단위로만 구매할 수 있습니다.");
         }
     }
