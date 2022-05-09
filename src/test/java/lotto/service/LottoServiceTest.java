05a2dd831b4acb538e0d3003cda2f908791874dc
chkim <cndghks0213@gmail.com>
2022-05-09 13:37:41 +0900

    test: 당첨 번호 등록 로직 변경에 따라 테스트도 보너스 번호를 입력받도록 변경

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -3,6 +3,8 @@ package lotto.service;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.RandomNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -10,6 +12,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoServiceTest {
 
@@ -21,5 +24,14 @@ class LottoServiceTest {
         List<Lotto> lottoList = (List< Lotto >) ReflectionTestUtils.getField(lottos, "lottos");
 
         assertThat(lottoList).hasSize(resultCount);
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 안된다.")
+    @Test
+    void registerWinningNumbersTest() {
+        LottoService lottoService = new LottoService();
+        assertThatThrownBy(() -> lottoService.registerWinningNumbers("1,2,3,4,5,6", "3"))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 }
