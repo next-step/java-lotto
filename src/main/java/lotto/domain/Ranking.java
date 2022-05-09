95cc3d3e0aa16b669dbbf5d58d5098866badb9e0
chkim <cndghks0213@gmail.com>
2022-05-09 13:32:39 +0900

    refactor: Ranking field 변수로 bonus count 추가 및 일치하는 순위를 반환하는 로직 변경

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -3,33 +3,42 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Ranking {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FIRST(6, 2000000000, 0),
+    SECOND(5, 30000000, 1),
+    THIRD(5, 1500000, 0),
+    FOURTH(4, 50000, 0),
+    FIFTH(3, 5000, 0),
+    MISS(0, 0, 0);
 
+    private final int requireBonusCount;
     private final int matchCount;
     private final int money;
 
-    Ranking(int matchCount, int money) {
+    Ranking(int matchCount, int money, int requireBonusCount) {
         this.matchCount = matchCount;
         this.money = money;
+        this.requireBonusCount = requireBonusCount;
     }
 
-    public static Ranking findMatchRanking(int matchCount) {
+    public static Ranking findMatchRanking(MatchResult matchResult) {
         return Arrays.stream(Ranking.values())
-                .filter(ranking -> ranking.isEqualRanking(matchCount))
+                .filter(ranking -> ranking.isEqualRanking(matchResult.getMatchCount(), matchResult.getMatchBonus()))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("당첨자가 아닙니다."));
+                .orElse(MISS);
+    }
+
+    private boolean isEqualRanking(int matchCount, int matchBonus) {
+        return this.matchCount == matchCount && this.requireBonusCount <= matchBonus;
+    }
+
+    public boolean isWinner() {
+        return this != MISS;
     }
 
     public int getPriceByCount(int count) {
         return money * count;
     }
 
-    private boolean isEqualRanking(int matchCount) {
-        return this.matchCount == matchCount;
-    }
 
     @Override
     public String toString() {
