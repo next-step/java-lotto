f40e08a4b10f83308a5c839610a38ae3fe64ecf4
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-09 16:23:12 +0900

    feat: LottoNumber 일급객체 LottoTicket

---
+++ src/main/java/lotto/model/LottoTicket.java
@@ -0,0 +1,50 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTicket {
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
+        validate(lottoNumbers);
+        this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
+    }
+
+    private void validate(List<LottoNumber> lottoNumbers) {
+        if(lottoNumbers.size() != 6){
+            throw new IllegalArgumentException("로또는 6개의 번호로 구성되어야합니다.");
+        }
+    }
+
+    public int getCoincidenceCnt(LottoTicket winningNumbers){
+        int cnt = 0;
+        for(LottoNumber lottoNumber: winningNumbers.getLottoNumbers()){
+            if(this.lottoNumbers.contains(lottoNumber)){
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
+    private List<LottoNumber> getLottoNumbers(){
+        return lottoNumbers;
+    }
+
+    @Override
+    public String toString(){
+        int n = lottoNumbers.size();
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i=0; i<n; i++){
+            sb.append(lottoNumbers.get(i));
+            if(i != n-1){
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+}
