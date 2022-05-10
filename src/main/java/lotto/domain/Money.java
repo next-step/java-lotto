70b2ee5361dad30cc4c25b9ee9f4508802aa615c
양경희 <alicesister1@gmail.com>
2022-05-10 21:34:25 +0900

    test: Override 메서드 위치 하단으로 이동

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -18,28 +18,6 @@ public class Money {
     this.amount = amount;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Money money = (Money) o;
-    return Objects.equals(amount, money.amount);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(amount);
-  }
-
-  @Override
-  public String toString() {
-    return String.format(WON_FORMAT, amount);
-  }
-
   public Money multiply(double rate) {
     return new Money((int) (amount * rate));
   }
@@ -61,5 +39,27 @@ public class Money {
 
   public int value() {
     return amount;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Money money = (Money) o;
+    return Objects.equals(amount, money.amount);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(amount);
+  }
+
+  @Override
+  public String toString() {
+    return String.format(WON_FORMAT, amount);
   }
 }
