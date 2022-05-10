e0ed1e08a9e498ef3a956fc669ded2b95e92b1a1
양경희 <alicesister1@gmail.com>
2022-05-10 23:49:19 +0900

    refactor: 당첨 통계 생성자 검증

--- src/main/java/lotto/domain/Statistic.java
+++ src/main/java/lotto/domain/Statistic.java
@@ -1,15 +1,26 @@
 package lotto.domain;
 
+import java.util.Objects;
 import lotto.domain.money.Money;
 
 public class Statistic {
 
+  private static final String NULL_PRIZES = "당첨금은 null일 수 없습니다.";
+  private static final String NEGATIVE_MATCHED_PRICE_COUNT = "당첨 개수는 음수일 수 없습니다.";
   private final Prizes prizes;
   private final int matchedPrizeCount;
 
   public Statistic(int matchedPriceCount, Prizes prizes) {
+    requireNonNegative(matchedPriceCount);
+    Objects.requireNonNull(prizes, NULL_PRIZES);
     this.matchedPrizeCount = matchedPriceCount;
     this.prizes = prizes;
+  }
+
+  private void requireNonNegative(int matchedPriceCount) {
+    if (matchedPriceCount < 0) {
+      throw new IllegalArgumentException(NEGATIVE_MATCHED_PRICE_COUNT);
+    }
   }
 
   public int getMatchedPrizeCount() {
--- src/test/java/lotto/domain/StatisticTest.java
+++ src/test/java/lotto/domain/StatisticTest.java
@@ -1,17 +1,36 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.stream.Stream;
 import lotto.domain.money.Money;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 @DisplayName("당첨 통계 테스트")
 class StatisticTest {
+
+  @DisplayName("당첨금 개수 정보가 음수이면 예외를 던진다")
+  @Test
+  void statistic() {
+    assertThatIllegalArgumentException().isThrownBy(() -> new Statistic(-1, Prizes.NOT_PRIZE))
+        .withMessage("당첨 개수는 음수일 수 없습니다.");
+  }
+
+  @DisplayName("당첨금 정보가 null 이면 예외를 던진다")
+  @ParameterizedTest
+  @NullSource
+  void statistic(Prizes prize) {
+    assertThatNullPointerException().isThrownBy(() -> new Statistic(0, prize))
+        .withMessage("당첨금은 null일 수 없습니다.");
+  }
 
   @DisplayName("Statistic은 당첨금 총액을 반환한다")
   @ParameterizedTest
