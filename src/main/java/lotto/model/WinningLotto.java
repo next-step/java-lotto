465d2cb378121ed3aee09faffb43bd5d82080de3
younlll <youn.lee96@gmail.com>
2022-05-10 23:57:25 +0900

    feat: add validation

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 public class WinningLotto {
 
+  private static final String LAST_WINNIG_NUMBER_NULL_OR_EMPTY_ERROR_MESSAGE = "지난 주 당첨 번호가 null이거나 empty입니다.";
+
   private final Lotto winningLotto;
   private final LottoNumber bonusLottoNumber;
 
@@ -11,7 +13,14 @@ public class WinningLotto {
   }
 
   public static WinningLotto create(String winningLotto, int bonusLottoNumber) {
+    validateNullOrEmpty(winningLotto);
     return new WinningLotto(Lotto.create(winningLotto), LottoNumber.create(bonusLottoNumber));
+  }
+
+  private static void validateNullOrEmpty(String winningLotto) {
+    if(winningLotto == null || winningLotto.isEmpty()) {
+      throw new IllegalArgumentException(LAST_WINNIG_NUMBER_NULL_OR_EMPTY_ERROR_MESSAGE);
+    }
   }
 
   public int matchWinningLottoNumbers(Lotto lotto) {
