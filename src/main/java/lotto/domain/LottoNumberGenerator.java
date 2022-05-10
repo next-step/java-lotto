cab3f9bc40b2adda9d6bc62774a650085df50a7d
양경희 <alicesister1@gmail.com>
2022-05-10 21:08:24 +0900

    refactor: 로또 번호 범위를 명확히 표현

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,6 +1,6 @@
 package lotto.domain;
 
-import static java.util.stream.IntStream.range;
+import static java.util.stream.IntStream.rangeClosed;
 
 import java.util.Collections;
 import java.util.List;
@@ -10,11 +10,11 @@ public class LottoNumberGenerator {
 
   private static final int FROM_INDEX = 0;
   private static final int TO_INDEX = 6;
-  private static final int START_INCLUSIVE = 1;
-  private static final int END_EXCLUSIVE = 46;
+  private static final int LOTTO_START_NUMBER = 1;
+  private static final int LOTTO_END_NUMBER = 45;
 
   public List<Integer> generate() {
-    List<Integer> numbers = range(START_INCLUSIVE, END_EXCLUSIVE).boxed()
+    List<Integer> numbers = rangeClosed(LOTTO_START_NUMBER, LOTTO_END_NUMBER).boxed()
         .collect(Collectors.toList());
     Collections.shuffle(numbers);
     return numbers.subList(FROM_INDEX, TO_INDEX);
--- src/test/java/lotto/domain/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumberGeneratorTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.RepeatedTest;
 public class LottoNumberGeneratorTest {
 
   @DisplayName("중복없는 로또 번호 6개가 반환된다")
-  @RepeatedTest(10)
+  @RepeatedTest(100)
   void generateLottoNumber() {
     LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
     List<Integer> generate = lottoNumberGenerator.generate();
     assertThat(generate).hasSize(6);
     assertThat(generate).doesNotHaveDuplicates();
+    assertThat(generate).hasSizeBetween(1, 45);
   }
 }
