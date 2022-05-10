11757fd162c46637ee410fa4dace598fd83f570c
wlroh <wlroh.paul@gmail.com>
2022-05-11 02:01:21 +0900

    test: 테스트 코드 명칭 변경

--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoGameTest {
 
     @Test
-    @DisplayName("15000원을 입력 시 15개의 로또를 반환한다.")
-    void createLottos() {
+    @DisplayName("15000원으로 로또 게임 생성 시 15개의 로또를 생성한다.")
+    void initialize() {
         LottoGame lottoGame = new LottoGame(15000);
 
         assertThat(lottoGame.lottos().count()).isEqualTo(15);
