049ec17a3d63c84b065523f82bd253741b74e9a9
chkim <cndghks0213@gmail.com>
2022-05-10 15:52:30 +0900

    test: Ranking 테스트 수정

--- src/test/java/lotto/domain/RankingTest.java
+++ src/test/java/lotto/domain/RankingTest.java
@@ -10,9 +10,9 @@ class RankingTest {
 
     @DisplayName("3 ~ 6개를 입력하면 일치하는 당첨 순위를 반환한다.")
     @ParameterizedTest
-    @CsvSource(value = {"1:0:MISS", "3:0:FIFTH", "3:1:FIFTH", "5:0:THIRD", "5:1:SECOND", "6:0:FIRST", "6:1:FIRST"}, delimiter = ':')
-    void findMatchRankingTest(int matchCount, int matchBonus, Ranking ranking) {
-        assertThat(Ranking.findMatchRanking(new MatchResult(matchCount, matchBonus)))
+    @CsvSource(value = {"1:false:MISS", "3:false:FIFTH", "3:true:FIFTH", "5:false:THIRD", "5:true:SECOND", "6:false:FIRST", "6:true:FIRST"}, delimiter = ':')
+    void findMatchRankingTest(int matchCount, boolean matchBonus, Ranking ranking) {
+        assertThat(Ranking.findMatchRanking(matchCount, matchBonus))
                 .isEqualTo(ranking);
     }
 }
