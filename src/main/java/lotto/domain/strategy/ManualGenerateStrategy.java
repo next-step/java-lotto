dd8ac982c8da7a5d223b704d504dfffe9a0d611d
bky4564 <bky4564@hunet.co.kr>
2022-05-10 12:15:28 +0900

    feat: 로또 수동 생성 전략 추가

---
+++ src/main/java/lotto/domain/strategy/ManualGenerateStrategy.java
@@ -0,0 +1,16 @@
+package lotto.domain.strategy;
+
+import lotto.domain.LottoNumber;
+import lotto.util.InputUtil;
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class ManualGenerateStrategy implements LottoNumberGenerateStrategy {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    @Override
+    public Set<LottoNumber> generateLottoNumbers() {
+        return InputUtil.readLottoNumbers(SCANNER.nextLine());
+    }
+}
