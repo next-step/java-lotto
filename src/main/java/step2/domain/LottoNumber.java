45c88e6a834564512978ace96cb9d00d5bac2520
khkim <kihyunkim@daou.co.kr>
2022-05-10 14:10:29 +0900

    refactor: 상수값 재사용

--- src/main/java/step2/domain/LottoNumber.java
+++ src/main/java/step2/domain/LottoNumber.java
@@ -5,8 +5,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class LottoNumber implements Comparable<LottoNumber>{
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
     private static final Map<Integer, LottoNumber> CACHE = new HashMap<>();
     private int value;
 
--- src/main/java/step2/domain/strategy/RandomPick.java
+++ src/main/java/step2/domain/strategy/RandomPick.java
@@ -7,14 +7,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomPick implements LottoPickStrategy {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
     private static final int START_INDEX = 0;
     private static final int LAST_INDEX = 6;
     private static final List<LottoNumber> BASE_NUMBERS = new ArrayList<>();
 
     static {
-        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+        for (int i = LottoNumber.MIN_NUMBER; i <= LottoNumber.MAX_NUMBER; i++) {
             BASE_NUMBERS.add(new LottoNumber(i));
         }
     }
