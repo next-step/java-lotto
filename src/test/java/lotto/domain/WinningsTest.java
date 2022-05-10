6ee2927b050eacb81b09fdce02b21c13c63b399b
jess <official1126@naver.com>
2022-05-11 00:20:57 +0900

    refactor 복수 개의 assertThat -> assertAll로 묶음

--- src/test/java/lotto/domain/WinningsTest.java
+++ src/test/java/lotto/domain/WinningsTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class WinningsTest {
 
@@ -34,11 +35,14 @@ class WinningsTest {
     @Test
     @DisplayName("당첨 개수 확인")
     void check() {
-        assertThat(winnings.recordMatched()[Matched.THREE.index()]).isEqualTo(0);
-        assertThat(winnings.recordMatched()[Matched.FOUR.index()]).isEqualTo(1);
-        assertThat(winnings.recordMatched()[Matched.FIVE.index()]).isEqualTo(1);
-        assertThat(winnings.recordMatched()[Matched.FIVE_AND_BONUS.index()]).isEqualTo(1);
-        assertThat(winnings.recordMatched()[Matched.ALL.index()]).isEqualTo(1);
+        assertAll("당첨 개수 별 로또 현황 확인",
+                () -> assertThat(winnings.recordMatched()[Matched.THREE.index()]).isEqualTo(0),
+                () -> assertThat(winnings.recordMatched()[Matched.FOUR.index()]).isEqualTo(1),
+                () -> assertThat(winnings.recordMatched()[Matched.FIVE.index()]).isEqualTo(1),
+                () -> assertThat(winnings.recordMatched()[Matched.FIVE_AND_BONUS.index()]).isEqualTo(1),
+                () -> assertThat(winnings.recordMatched()[Matched.ALL.index()]).isEqualTo(1)
+        );
+
     }
 
 }
