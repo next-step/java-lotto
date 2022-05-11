de61d0b9d3c4c4806d3bc34855076f03106dfd7a
zieunx <wldms_1217@naver.com>
2022-05-11 22:37:53 +0900

    feat(step2/WinningNumber): 당첨번호 컬렉션을 가지는 WinningNumber 도메인 생성

---
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WinningNumber {
+	public final String DELIMITER = ",";
+	public static final int NUMBER_SIZE = 6;
+	private final List<LottoNumber> numbers;
+
+	public WinningNumber() {
+		numbers = new ArrayList<>();
+	}
+
+	public WinningNumber(String input) {
+		String[] splits = input.split(DELIMITER);
+		validateSize(splits);
+		numbers = Stream.of(splits)
+			.map(LottoNumber::new)
+			.collect(Collectors.toList());
+	}
+
+	private void validateSize(String[] splits) {
+		if(splits.length != NUMBER_SIZE) {
+			throw new IllegalArgumentException("당첨번호는 " + NUMBER_SIZE + "개로 입력해주세요.");
+		}
+	}
+}
---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import exception.StringParsingToIntegerException;
+
+public class WinningNumberTest {
+
+	@DisplayName("입력한 당첨번호가 6개가 아닌경우 에러 발생")
+	@ValueSource(strings = {"1,2,3,4,5,6,7", "1,2,3,4,5"})
+	@ParameterizedTest
+	public void When_GivenWrongSizeInput_Expected_IllegalArgumentException(String input) {
+		assertThatThrownBy(() -> new WinningNumber(input))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("입력한 당첨번호들 중 숫자가 아닌 경우 에러 발생")
+	@ValueSource(strings = {"1,2,3,4,5,!6", "1,2,a3,4,5,6"})
+	@ParameterizedTest
+	public void When_GivenWrongNumberInput_Expected_StringParsingToIntegerException(String input) {
+		assertThatThrownBy(() -> new WinningNumber(input))
+			.isInstanceOf(StringParsingToIntegerException.class);
+	}
+}
