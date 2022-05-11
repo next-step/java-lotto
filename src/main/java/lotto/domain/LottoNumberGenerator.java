bd88370d6d814755da4328203e5fa47ffe1ee4a9
rumor1993 <rumor1993@naver.com>
2022-05-11 12:00:16 +0900

    refactor: 로또 번호 생성 Class명 변경

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class LottoGenerator {
+public class LottoNumberGenerator {
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 45;
     private List<Integer> lottoNumbers = new ArrayList<>();
