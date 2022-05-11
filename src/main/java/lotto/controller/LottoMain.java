d91eed2f5d6b57ac9c037678935890b88d3c39fe
kang-jisu <rkdwltn0425@gmail.com>
2022-05-11 21:35:17 +0900

    feat: 수동 로또 개수, 번호 입력 View 생성

--- src/main/java/lotto/controller/LottoMain.java
+++ src/main/java/lotto/controller/LottoMain.java
@@ -12,9 +12,17 @@ import lotto.model.WinnerLotto;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoMain {
     public static void main(String[] args) {
         Money buyingMoney = new Money(InputView.inputBuyingMoney());
+
+        int manualLottoCount = InputView.inputManualLottoCount();
+        List<String> manualLottoNumbers = InputView.inputManualLottoNumbers(manualLottoCount);
+        Lottos manualLottos = inputManualLottos(manualLottoNumbers);
+
         Lottos buyingLottos = LottoMarket.buyLottos(buyingMoney, new RandomLottoGenerator());
 
         ResultView.printBuyingLottos(buyingLottos);
@@ -31,5 +39,12 @@ public class LottoMain {
         LottoNumber bonusNumber = LottoNumber.valueOf(InputView.inputBonusLottoNumber());
 
         return new WinnerLotto(winnerLotto, bonusNumber);
+    }
+
+    private static Lottos inputManualLottos(List<String> manualLottoNumbers) {
+        return manualLottoNumbers.stream()
+                .map(lottoNumbers -> ExtractLottoNumbers.split(lottoNumbers))
+                .map(Lotto::new)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), Lottos::new));
     }
 }
