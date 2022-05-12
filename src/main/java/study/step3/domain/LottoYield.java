e217a9adef68ec8137c28f9284d1aa86a34d1dbe
slogan-choi <slogan.choi@gmail.com>
2022-05-12 09:43:31 +0900

    feat: step3 1차 피드백 반영 - 정적 유틸리티 클래스가 불필요하게 생성되는 것 방지

--- src/main/java/study/step3/domain/LottoYield.java
+++ src/main/java/study/step3/domain/LottoYield.java
@@ -8,6 +8,8 @@ public class LottoYield {
     private static final int LOTTO_AMOUNT = 1000;
     private static final int DEFAULT_LOTTO_YIELD = 1;
 
+    private LottoYield() {}
+
     public static double operationYield(LottoTickets lottoTickets, LottoWinners lottoWinners) {
         List<LottoTicket> tickets = lottoTickets.readOnlyLottoTicket();
         double lottoPrize = tickets.stream()
