768763b562657124fd60774723e2d75a353868fe
rumor1993 <rumor1993@naver.com>
2022-05-12 22:11:50 +0900

    refactor: 테스트 분리

--- src/test/java/lotto/lottoTest.java
+++ src/test/java/lotto/lottoTest.java
@@ -46,32 +46,6 @@ public class lottoTest {
     }
 
     @Test
-    void _1등_찾기() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 7, 8, 9), 4);
-        assertThat(winningNumbers.findRank(new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9)))).isEqualTo(Rank.ONE);
-    }
-    @Test
-    void _2등_찾기() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 7, 8, 9), 4);
-        assertThat(winningNumbers.findRank(new Lotto(Arrays.asList(1, 2, 3, 7, 8, 4)))).isEqualTo(Rank.TWO);
-    }
-    @Test
-    void _3등_찾기() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 7, 8, 9), 4);
-        assertThat(winningNumbers.findRank(new Lotto(Arrays.asList(1, 2, 3, 7, 8, 0)))).isEqualTo(Rank.THREE);
-    }
-    @Test
-    void _4등_찾기() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 7, 8, 9), 4);
-        assertThat(winningNumbers.findRank(new Lotto(Arrays.asList(1, 2, 3, 7, 0, 0)))).isEqualTo(Rank.FOUR);
-    }
-    @Test
-    void _5등_찾기() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(1, 2, 3, 7, 8, 9), 4);
-        assertThat(winningNumbers.findRank(new Lotto(Arrays.asList(1, 2, 3, 0, 0, 0)))).isEqualTo(Rank.FIVE);
-    }
-
-    @Test
     void 수동로또_구매() {
         Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 7, 8, 9));
         Customer customer = new Customer(10000);
