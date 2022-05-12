a300a8cc324df828e69b9c25f59c27c151f767c5
asqwklop12 <asqwklop12@gmail.com>
2022-05-12 20:16:56 +0900

    feat 줄바꿈 변경

--- src/main/java/lotto/view/OutputTable.java
+++ src/main/java/lotto/view/OutputTable.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public final class OutputTable {
 
-    private static final String BUY_THING_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구입했습니다.\n";
+    private static final String BUY_THING_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구입했습니다.\n\r";
     private static final String STATISTICS_MESSAGE = "당첨 통계";
     private static final String UNDER_BAR = "_________";
     private static final String YIELD_MESSAGE = "총 수익률은 %.2f입니다. (기준이 %d이기 때문에 결과적으로 %s라는 의미임)";
