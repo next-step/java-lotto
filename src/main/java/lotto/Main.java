5c4027fcce6d5681eaf5d89ba4c864f95ed6c1db
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-12 18:20:57 +0900

    refactor: Main.java

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,29 +1,15 @@
 package lotto;
 
 import lotto.model.*;
-import lotto.view.Input;
-import lotto.view.Output;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
             LottoController lottoController = new LottoController();
-
-            Money money = new Money(Input.readMoney());
-            int ticketCnt = money.calculateTicketCnt();
-            Output.printLottoCnt(ticketCnt);
-
-            List<LottoTicket> lottoTicketList = lottoController.generateTickets(ticketCnt, new RandomLottoTicketGenerator());
-            LottoTickets lottoTickets = new LottoTickets(lottoTicketList);
-            Output.printLottoTicketList(lottoTicketList);
-
-            LottoTicket preWinningLottoTicket =  Input.readPreWeekWinningLottoNums();
-            LottoResult lottoResult = lottoController.start(lottoTickets, preWinningLottoTicket);
-            Output.printWinningStatics(money, lottoResult);
+            lottoController.start();
         }catch (Exception exception){
             System.out.println(String.join("\n", "ERROR", exception.getMessage()));
+            exception.printStackTrace();
         }
     }
 }
