64b37ead7391e4e4baee49b89f997ff7e0ca5653
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-12 18:18:20 +0900

    refactor: 잘못된 로또 번호 예외

---
+++ src/main/java/lotto/exception/InvalidLottoNumberException.java
@@ -0,0 +1,8 @@
+package lotto.exception;
+
+public class InvalidLottoNumberException extends RuntimeException{
+    private static final String INVALID_LOTTO_NUMBER_MESSAGE = "로또 번호는 1~54사이의 수 입니다";
+    public InvalidLottoNumberException(){
+        super(INVALID_LOTTO_NUMBER_MESSAGE);
+    }
+}
