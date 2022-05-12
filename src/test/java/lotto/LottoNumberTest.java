7879f61a7e63f77782f7b44e2f786c54e244afa6
eun jung <62321968+dmswjd4117@users.noreply.github.com>
2022-05-12 18:22:15 +0900

    refactor: containsExactlyEntriesOf이용해서 map테스트하기

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.exception.InvalidLottoNumberException;
 import lotto.model.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,14 +10,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class LottoNumberTest {
 
     @Test
-    @DisplayName("LottoNumber 생성 예외 테스트")
+    @DisplayName("1~45 사이의 숫자가 아니면 로또 생성시 예외를 던진다")
     void 로또_생성_예외_테스트() {
         assertThatThrownBy(()->{
             new LottoNumber(46);
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(InvalidLottoNumberException.class);
 
         assertThatThrownBy(()->{
             new LottoNumber(0);
-        }).isInstanceOf(IllegalArgumentException.class);
+        }).isInstanceOf(InvalidLottoNumberException.class);
     }
 }
--- src/test/java/lotto/LottoTicketsTest.java
+++ src/test/java/lotto/LottoTicketsTest.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class LottoTicketsTest {
     LottoTicket winningTicket;
@@ -32,25 +33,19 @@ public class LottoTicketsTest {
     @DisplayName("몇개 일치하는지 테스트")
     void 일치_수_테스트(){
         Integer[] nums = {1, 2, 3, 4, 10, 11}; // 4
-        LottoTicket lottoTicket1 = new LottoTicket(Arrays.stream(nums)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+        LottoTicket lottoTicket1 = new LottoTicket(nums);
 
         Integer[] nums2 = {1, 2, 3, 10, 11, 12}; // 3
-        LottoTicket lottoTicket2 = new LottoTicket(Arrays.stream(nums2)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList()));
+        LottoTicket lottoTicket2 = new LottoTicket(nums2);
 
-        List<LottoTicket> lottoTicketList = new ArrayList<>();
-        lottoTicketList.add(lottoTicket1);
-        lottoTicketList.add(lottoTicket2);
+        LottoTickets lottoTickets = new LottoTickets(List.of(lottoTicket1, lottoTicket2));
 
-        LottoTickets lottoTickets = new LottoTickets(lottoTicketList);
-        Map<Rank, Long> res = lottoTickets.getRankMap(winningTicket);
-        assertThat(res.get(Rank.three)).isEqualTo(1);
-        assertThat(res.get(Rank.four)).isEqualTo(1);
-        assertThat(res.get(Rank.five)).isEqualTo(null);
-        assertThat(res.get(Rank.six)).isEqualTo(null);
+        Map<Rank, Long> rankMap = lottoTickets.getRankMap(winningTicket);
+        Map<Rank, Long> result = Map.ofEntries(
+                entry(Rank.three, 1L),
+                entry(Rank.four, 1L)
+        );
+
+        assertThat(rankMap).containsExactlyEntriesOf(result);
     }
-    
 }
