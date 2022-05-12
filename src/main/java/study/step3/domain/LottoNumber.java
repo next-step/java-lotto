05c9e7d8e3b525629ba580abc81d10eaebf4c5ca
slogan-choi <slogan.choi@gmail.com>
2022-05-12 10:11:13 +0900

    feat: step3 1차 피드백 반영 - 블록 {} 추가

--- src/main/java/study/step3/domain/LottoNumber.java
+++ src/main/java/study/step3/domain/LottoNumber.java
@@ -31,8 +31,12 @@ public class LottoNumber {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         LottoNumber that = (LottoNumber) o;
         return number == that.number;
     }
--- src/main/java/study/step3/domain/LottoNumbers.java
+++ src/main/java/study/step3/domain/LottoNumbers.java
@@ -54,8 +54,12 @@ public class LottoNumbers {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         LottoNumbers that = (LottoNumbers) o;
         return Objects.equals(numbers, that.numbers);
     }
