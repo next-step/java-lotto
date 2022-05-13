5355a24fe66a9dedf8a42094b8d284ae5ccee980
slogan-choi <slogan.choi@gmail.com>
2022-05-13 17:21:26 +0900

    refactor: step3 2차 피드백 반영 - 고정된 우승번호에 대한 셋팅

--- src/test/java/study/step4/domain/LottoWinnersTest.java
+++ src/test/java/study/step4/domain/LottoWinnersTest.java
@@ -1,5 +1,6 @@
 package study.step4.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import study.step3.domain.LottoNumber;
@@ -15,8 +16,11 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 @DisplayName("당첨 번호 세팅")
 class LottoWinnersTest {
 
-    public Set<Integer> createLottoNumber() {
-        return IntStream.range(1,7)
+    private Set<Integer> lottoNumber;
+
+    @BeforeEach
+    public void setUp() {
+        lottoNumber = IntStream.range(1,7)
                 .boxed()
                 .collect(Collectors.toSet());
     }
@@ -24,7 +28,7 @@ class LottoWinnersTest {
     @Test
     @DisplayName("당첨번호와 보너스번호 중복 테스트")
     void 당첨번호와_보너스번호_중복_테스트() {
-        study.step3.domain.LottoNumbers winner = new LottoNumbers(createLottoNumber());
+        study.step3.domain.LottoNumbers winner = new LottoNumbers(lottoNumber);
         study.step3.domain.LottoNumber bonus = new LottoNumber(1);
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
