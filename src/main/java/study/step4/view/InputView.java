fc7dcf2892012f334ff1123f4dd00dd58ebe0c24
slogan-choi <slogan.choi@gmail.com>
2022-05-13 12:52:50 +0900

    refactor: bonus 오타수정

--- src/main/java/study/step4/view/InputView.java
+++ src/main/java/study/step4/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNER_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
-    private static final String INPUT_BOUNS_NUMBERS_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBERS_MESSAGE = "보너스 볼을 입력해 주세요.";
 
     public int inputBuyAmount() {
         System.out.println(INPUT_BUY_AMOUNT_MESSAGE);
@@ -20,7 +20,7 @@ public class InputView {
     }
 
     public int inputBonusNumber() {
-        System.out.println(INPUT_BOUNS_NUMBERS_MESSAGE);
+        System.out.println(INPUT_BONUS_NUMBERS_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
