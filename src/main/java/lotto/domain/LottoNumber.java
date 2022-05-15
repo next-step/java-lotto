88ffca4110cb4adf6a02bdfb67bf39befe70c728
jess <official1126@naver.com>
2022-05-16 01:26:42 +0900

    feat collections.sort를 위한 compare 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,16 +1,24 @@
 package lotto.domain;
 
-public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
 
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     private final int number;
+
+    public LottoNumber(String number) {
+        this(Integer.parseInt(number));
+    }
 
     public LottoNumber(int number) {
         if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("1에서 45사이의 숫자만 입력 가능합니다.");
         }
         this.number = number;
+    }
+
+    public int number() {
+        return number;
     }
 
     @Override
@@ -26,5 +34,10 @@ public class LottoNumber {
     @Override
     public int hashCode() {
         return number;
+    }
+
+    @Override
+    public int compareTo(LottoNumber nextLottoNumber) {
+        return Integer.compare(this.number, nextLottoNumber.number);
     }
 }
