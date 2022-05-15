9c3ebafb3dba9f9887f6074b87007153ade31e24
jess <official1126@naver.com>
2022-05-16 01:20:54 +0900

    refactor 로또 번호 존재 여부 -> 로또에게 전가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -9,20 +9,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class WinningNumbersTest {
 
-    @Test
-    @DisplayName("당첨 번호 중 해당 번호가 포함되어 있는 지 확인")
-    void checkNumber() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(3, 4, 10, 14, 35, 45), 2);
-        assertThat(winningNumbers.checkNumber(3)).isEqualTo(1);
-        assertThat(winningNumbers.checkNumber(33)).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("보너스 번호 당첨 여부 확인")
-    void checkBonus() {
-        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(3, 4, 10, 14, 35, 45), 2);
-        assertThat(winningNumbers.checkBonus(Arrays.asList(2, 5, 10, 24, 40, 44))).isTrue();
-        assertThat(winningNumbers.checkBonus(Arrays.asList(3, 5, 10, 24, 40, 44))).isFalse();
-    }
+//    @Test
+//    @DisplayName("당첨 번호 중 해당 번호가 포함되어 있는 지 확인")
+//    void checkNumber() {
+//        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(3, 4, 10, 14, 35, 45), 2);
+//        assertThat(winningNumbers.checkNumber(new LottoNumber(3))).isEqualTo(1);
+//        assertThat(winningNumbers.checkNumber(new LottoNumber(33))).isEqualTo(0);
+//    }
+//
+//    @Test
+//    @DisplayName("보너스 번호 당첨 여부 확인")
+//    void checkBonus() {
+//        WinningNumbers winningNumbers = new WinningNumbers(Arrays.asList(3, 4, 10, 14, 35, 45), 2);
+//        assertThat(winningNumbers.checkBonus(new Lotto(new int[]{2, 5, 10, 24, 40, 44}))).isTrue();
+//        assertThat(winningNumbers.checkBonus(new Lotto(new int[]{3, 5, 10, 24, 40, 44}))).isFalse();
+//    }
 
 }
