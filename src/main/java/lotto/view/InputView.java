a1afc43236b814ad354a55ade25c1533bd2d7d6f
Seongheon Sim <river.water.sea@gmail.com>
2022-05-16 01:18:54 +0900

    feat: 숫자 데이터 입력 시 예외 처리 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,9 @@ import lotto.model.Lotto;
 import lotto.model.Number;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
@@ -17,6 +19,7 @@ public class InputView {
     private static final String MANUAL_LOTTO_NUMBER_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String BEFORE_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String IS_NOT_NUMBER_MESSAGE = "숫자를 입력해주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static Game createGameByAuto() {
@@ -54,13 +57,20 @@ public class InputView {
     }
 
     public static Number numberValue() {
-        long value = Long.parseLong(stringValue());
+        String stringValue = Optional.ofNullable(stringValue())
+                .filter(isNumber())
+                .orElseThrow(() -> new IllegalArgumentException(IS_NOT_NUMBER_MESSAGE));
+        long value = Long.parseLong(stringValue);
 
         return Number.of(value);
     }
 
     public static String stringValue() {
         return SCANNER.nextLine();
+    }
+
+    private static Predicate<String> isNumber() {
+        return value -> !value.isBlank() && value.chars().boxed().allMatch(c -> '0' <= c && c <= '9');
     }
 
     private static Number getPrice() {
