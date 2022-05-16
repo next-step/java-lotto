2a637bd7e380dbed077ea087092d97166db02c49
jeongjaeeom <jeongjae.eom@gmail.com>
2022-05-16 17:03:26 +0900

    feat($compile): 구매한 한장의 로또 번호와 당첨 번호를 넣으면 당첨 결과를 반환한다.
    구매한 한장의 로또 번호와 당첨 번호를 넣으면 당첨 결과를 반환한다.
    - Rank enum 객체 생성
    - LottoGame match 메서드 구현

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,10 +1,24 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoGame {
 
   public static final int LOTTO_TICKET_PRICE = 1000;
 
   public static long toTicketCount(Money money) {
     return money.divide(LOTTO_TICKET_PRICE).getAmount();
+  }
+
+  public static Rank match(List<Integer> lottoNumbers, List<Integer> winLottoNumbers,
+      Integer bonusNumber) {
+    return Rank.of(getMatchCount(lottoNumbers, winLottoNumbers),
+        lottoNumbers.contains(bonusNumber));
+  }
+
+  private static long getMatchCount(List<Integer> lottoNumbers, List<Integer> winLottoNumbers) {
+    return lottoNumbers.stream()
+        .filter(winLottoNumbers::contains)
+        .count();
   }
 }
--- src/test/java/lotto/domain/LottoGameTest.java
+++ src/test/java/lotto/domain/LottoGameTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,21 @@ class LottoGameTest {
   @DisplayName("로또 구매 금액을 입력하면 구매가능한 로또 장수를 반환한다.")
   void givenMoney_ShouldBeLottoTicketCount() {
     assertThat(LottoGame.toTicketCount(Money.from(9100))).isEqualTo(9);
+  }
+
+  @Test
+  @DisplayName("로또 1등")
+  void givenLotto_shouldBeTopLottoRank() {
+    List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+    List<Integer> winLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+    assertThat(LottoGame.match(lottoNumbers, winLottoNumbers, 7)).isEqualTo(Rank.TOP);
+  }
+
+  @Test
+  @DisplayName("로또 2등")
+  void givenLotto_shouldBeSecondLottoRank() {
+    List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 7);
+    List<Integer> winLottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+    assertThat(LottoGame.match(lottoNumbers, winLottoNumbers, 7)).isEqualTo(Rank.SECOND);
   }
 }
