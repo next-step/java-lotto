e7cea91aa4d96a73e54e5fd866f03d85127f2992
ssamzag <ssamzag@gmail.com>
2022-05-16 16:54:44 +0900

    chore: App.java 경로 변경

--- src/main/java/lotto/App.java
+++ src/main/java/lotto/App.java
@@ -6,14 +6,14 @@ import lotto.ui.ResultView;
 public class App {
     public static void main(String[] args) {
         LottoGame lottoGame = new LottoGame(InputView.getPrice(), InputView.getManualLottoCount());
+        lottoGame.buyLotto(InputView.getManualLottos(lottoGame.getManualBuyCount()));
 
-        UserLottos userLottos = lottoGame.buyLotto(InputView.getManualLottos(lottoGame.getManualBuyCount()));
         ResultView.printBuyQuantityMessage(lottoGame.getMaxPurchasableQuantity(), lottoGame.getManualBuyCount());
-        ResultView.printBuyLottoNumbers(userLottos.getBuyLottos());
+        ResultView.printBuyLottoNumbers(lottoGame.getUserLottos());
 
         WinningLotto winningLottoNumber = new WinningLotto(InputView.getWinningNumber(), InputView.getBonusNumber());
 
-        ResultView.printWinningTitleAndResults(userLottos.getWinningResults(winningLottoNumber));
-        ResultView.printReturnRate(lottoGame.getReturnRate(winningLottoNumber, userLottos));
+        ResultView.printWinningTitleAndResults(lottoGame.getWinningResults(winningLottoNumber));
+        ResultView.printReturnRate(lottoGame.getReturnRate(winningLottoNumber));
     }
 }
