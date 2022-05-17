3981e52dca0c7a2f272758433127f39993445380
slogan-choi <slogan.choi@gmail.com>
2022-05-17 16:31:26 +0900

    fix: step4 1차 피드백 반영 - 싱글톤 패턴 수정

--- src/main/java/study/step4/view/InputView.java
+++ src/main/java/study/step4/view/InputView.java
@@ -16,6 +16,8 @@ public class InputView {
 
     private static final InputView instance = new InputView();
 
+    private InputView() {}
+
     public static InputView getInstance() {
         return instance;
     }
