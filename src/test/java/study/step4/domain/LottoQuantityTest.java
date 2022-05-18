13a3ed522601dab7260fb986fc799351fc738f7e
slogan-choi <slogan.choi@gmail.com>
2022-05-18 14:10:10 +0900

    fix: step4 1차 피드백 반영 - LottoQuantity 클래스의 생성자 변경에 따른 테스트 코드 오류 해결

--- src/test/java/study/step4/domain/LottoQuantityTest.java
+++ src/test/java/study/step4/domain/LottoQuantityTest.java
@@ -15,7 +15,7 @@ class LottoQuantityTest {
     @CsvSource(value = {"11000, 11", "1000,1"})
     @DisplayName("로또 금액 대비 개수 확인")
     void 로또금액_대비_개수확인(int amount, int ticketCount) {
-        LottoQuantity lottoQuantity = new LottoQuantity(amount);
+        LottoQuantity lottoQuantity = new LottoQuantity(amount, 0);
 
         assertThat(lottoQuantity.quantity()).isEqualTo(ticketCount);
     }
@@ -25,7 +25,7 @@ class LottoQuantityTest {
     void 로또_구매금액_천원미만_유효성_확인() {
         int amount = 0;
 
-        assertThatThrownBy(() -> new LottoQuantity(amount))
+        assertThatThrownBy(() -> new LottoQuantity(amount, 0))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
