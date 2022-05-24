8f4aae3d8b52afaed56c5753708e709c5f872acc
chkim <cndghks0213@gmail.com>
2022-05-25 06:23:17 +0900

    refactor: 로또 수동 구매 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,22 +5,33 @@ import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.List;
+
 public class LottoController {
 
-    private static final int EACH_PRICE = 1000;
+    private static final String RESTART_MESSAGE = "\n[INFO] 로또를 재시작합니다.\n";
+
     LottoService lottoService = new LottoService();
 
     public void startLotto() {
-        int purchasePrice = InputView.inputPurchaseAmount();
-        Money purchaseMoney = new Money(purchasePrice - purchasePrice % EACH_PRICE);
+        try {
+            Money totalPurchaseMoney = new Money(InputView.inputPurchaseAmount());
+            int manualLottoCount = InputView.inputManualLottoCount();
+            Money randomPurchaseMoney = totalPurchaseMoney.excludeManualPurchase(manualLottoCount);
+            List<String> manualLottos = InputView.inputManualLottoList(manualLottoCount);
 
-        Lottos registerLottos = lottoService.generateRandomLottos(new RandomNumberGenerator(), purchasePrice, EACH_PRICE);
-        ResultView.printPurchaseMessage(registerLottos, purchasePrice / EACH_PRICE);
+            lottoService.generateManualLottos(manualLottos);
+            Lottos registerLottos = lottoService.generateRandomLottos(new RandomNumberGenerator(), randomPurchaseMoney);
+            ResultView.printPurchaseMessage(registerLottos, manualLottoCount, randomPurchaseMoney.purchaseCount());
 
-        String lastWeekWinningNumber = InputView.inputLastWeekWinningNumber();
-        String bonusNumber = InputView.inputLastWeekBonusNumber();
-        RankingResult rankingResult = lottoService.registerWinningNumbers(lastWeekWinningNumber, bonusNumber);
-        ResultView.printWinningMessage(rankingResult, rankingResult.calculateProfit()
-                .profitRate(purchaseMoney));
+            String lastWeekWinningNumber = InputView.inputLastWeekWinningNumber();
+            String bonusNumber = InputView.inputLastWeekBonusNumber();
+
+            RankingResult rankingResult = lottoService.registerWinningNumbers(lastWeekWinningNumber, bonusNumber);
+            ResultView.printWinningMessage(rankingResult, rankingResult.calculateProfit().profitRate(totalPurchaseMoney));
+        } catch (IllegalArgumentException e) {
+            System.out.println("\n" + e.getMessage() + RESTART_MESSAGE);
+            startLotto();
+        }
     }
 }
