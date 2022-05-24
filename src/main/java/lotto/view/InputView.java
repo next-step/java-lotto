c953730d31e1e8f8f331d06ac5b6e503168e121f
chkim <cndghks0213@gmail.com>
2022-05-25 03:52:35 +0900

    feat: 요구사항 변경에 따라 입력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final String START_MESSAGE = "구매금액을 입력해주세요.";
+    private static final String MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WINNING_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
     private static final String BONUS_LOTTO_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
@@ -14,6 +18,20 @@ public class InputView {
     public static int inputPurchaseAmount() {
         System.out.println(START_MESSAGE);
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static int inputManualLottoCount() {
+        System.out.println(MANUAL_LOTTO_COUNT);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<String> inputManualLottoList(int count) {
+        System.out.println(MANUAL_LOTTO_NUMBER);
+        List<String> lottos = new ArrayList<>();
+        for (int index = 1; index <= count; index++) {
+            lottos.add(scanner.nextLine());
+        }
+        return lottos;
     }
 
     public static String inputLastWeekWinningNumber() {
