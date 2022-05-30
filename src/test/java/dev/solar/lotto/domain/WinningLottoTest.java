164dd7cdf8b9a16941968d424216ed578627e822
blossun <blossunn@gmail.com>
2022-05-31 01:27:25 +0900

    feat: 1등 번호는 보너스 볼이 될 수 없다.

--- src/test/java/dev/solar/lotto/domain/WinningLottoTest.java
+++ src/test/java/dev/solar/lotto/domain/WinningLottoTest.java
@@ -1,11 +1,14 @@
 package dev.solar.lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import dev.solar.lotto.LottoException;
 
 public class WinningLottoTest {
     @DisplayName("당첨 갯수를 확인한다.")
@@ -22,5 +25,13 @@ public class WinningLottoTest {
         final WinningLotto winningLotto = new WinningLotto(Set.of(1, 2, 3, 4, 5, 6), 45);
         Rank result = winningLotto.match(new LottoTicket(Set.of(1, 2, 3, 4, 5, 45)));
         assertThat(result).isEqualTo(Rank.SECOND);
+    }
+
+    @DisplayName("1등 번호는 보너스 볼이 될 수 없다.")
+    @Test
+    void invalid_bonus_ball() {
+        assertThatThrownBy(() -> new WinningLotto(Set.of(1, 2, 3, 4, 5, 6), 1))
+                .isInstanceOf(LottoException.class)
+                .hasMessage("1등 번호는 보너스 볼이 될 수 없습니다.");
     }
 }
