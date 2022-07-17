eacabeacf873652b8a5f86dc9395af78f2f4415f
seo <youngsuseo@naver.com>
2022-07-17 12:23:08 +0900

    FEAT (LOTTO) : Develop Lotto Game (Add Manual Lotto)
    * 로또 게임에 대한 view와 main 함수 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,25 +4,27 @@ import lotto.util.InputUtils;
 
 public class InputView {
     private static final int EACH_PRICE = 1000;
-    private static final int BASE_PRICE = 0;
 
     public static int inputPurchaseAmount() {
-        int purchasePrice = InputUtils.scanNumber("구매금액을 입력해 주세요.");
-
-        if (purchasePrice < EACH_PRICE) {
-            throw new IllegalArgumentException("로또 구매를 위해서는 천원 이상 입력해야 합니다.");
-        }
-
-        if (purchasePrice % EACH_PRICE != BASE_PRICE) {
-            throw new IllegalArgumentException("천원 단위로 입력해주세요.");
-        }
-
-        return printPurchaseAmount(purchasePrice / EACH_PRICE);
+        String purchaseAmount = InputUtils.scan("구매금액을 입력해 주세요.");
+        return Integer.parseInt(purchaseAmount);
     }
 
-    private static int printPurchaseAmount(int amount) {
-        System.out.println(amount + "개를 구매했습니다.");
-        return amount;
+    public static int inputPurchaseManualLotteryAmount() {
+        String purchaseManualLotteryAmount = InputUtils.scan("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(purchaseManualLotteryAmount);
+    }
+
+    public static void inputManualLotteryAmount() {
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+    }
+
+    public static String inputManualLottery() {
+        return InputUtils.scan();
+    }
+
+    public static void printPurchaseAmount(int purchasePrice, int manualLotteryAmount) {
+        System.out.println("수동으로 " + manualLotteryAmount + "장, 자동으로 " + (purchasePrice / EACH_PRICE - manualLotteryAmount) + "개를 구매했습니다.");
     }
 
 
@@ -30,7 +32,7 @@ public class InputView {
         return InputUtils.scan("지난 주 당첨 번호를 입력해 주세요.");
     }
 
-    public static int inputBonusNumber() {
-        return InputUtils.scanNumber("보너스 볼을 입력해 주세요.");
+    public static String inputBonusNumber() {
+        return InputUtils.scan("보너스 볼을 입력해 주세요.");
     }
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,14 +1,30 @@
 package lotto.view;
 
 import lotto.domain.LotteryGames;
+import lotto.domain.LotteryNumber;
 import lotto.domain.Rank;
+import lotto.interfaces.LotteryGame;
 
 import java.util.Map;
 
 public class ResultView {
 
-    public static void print(LotteryGames purchasedLottery) {
-        System.out.println(purchasedLottery);
+    public static void print(LotteryGames purchasedLotteryGames) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (LotteryGame lotteryGame : purchasedLotteryGames.getLotteryGameList()) {
+            appendLotteryNumber(stringBuilder, lotteryGame);
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    private static void appendLotteryNumber(StringBuilder stringBuilder, LotteryGame lotteryGame) {
+        stringBuilder.append("[");
+        for (LotteryNumber lottery : lotteryGame.getLotteries()) {
+            stringBuilder.append(lottery.getNumber()).append(", ");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(","));
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(" "));
+        stringBuilder.append("]\n");
     }
 
     public static void printResult(Map<Rank, Integer> results) {
@@ -18,9 +34,9 @@ public class ResultView {
         for (Rank rank : results.keySet()) {
             if (Rank.SECOND == rank) {
                 System.out.println(rank.getCountOfMatch() + "개 일치, 보너스 볼 일치 (" + rank.getWinningMoney()+ "원)- " + results.get(rank) + "개");
+                continue;
             }
             System.out.println(rank.getCountOfMatch() + "개 일치 (" + rank.getWinningMoney()+ "원)- " + results.get(rank) + "개");
-
         }
     }
 
