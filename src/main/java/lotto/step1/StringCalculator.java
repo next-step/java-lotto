49c2a088a4daf631739748f4559faa0398d1bcfb
JunH <tjdtls690@gmail.com>
2022-09-23 20:53:12 +0900

    :sparkles: feat : 두 숫자의 나눗셈 및 예외 던지기 기능 구현

--- src/main/java/lotto/step1/StringCalculator.java
+++ src/main/java/lotto/step1/StringCalculator.java
@@ -4,12 +4,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StringCalculator {
-    public int calculate(String formula) {
-        String[] split = formula.split(" ");
+    private static final String DIVIDE_NUMBERS_EXCEPTION_MESSAGE = "나눗셈은 정확히 나누어 떨어지는 경우에만 진행할 수 있습니다. 다시 입력해주세요.";
+    
+    public int calculate(String formula) throws UnsupportedOperationException {
+        String[] split = getSplit(formula);
         return rotationCalculate(getNumbers(split), getSymbols(split));
     }
     
-    private int rotationCalculate(int[] numbers, char[] symbols) {
+    private int rotationCalculate(int[] numbers, char[] symbols) throws UnsupportedOperationException {
         int result = 0;
         for (int index = 0; index < symbols.length; index++) {
             result = checkSymbolAndCalculate(numbers, symbols, index);
@@ -17,20 +19,27 @@ public class StringCalculator {
         return result;
     }
     
-    private int checkSymbolAndCalculate(int[] numbers, char[] symbols, int index) {
+    private int checkSymbolAndCalculate(int[] numbers, char[] symbols, int index) throws UnsupportedOperationException {
         if (symbols[index] == '+') {
             return numbers[index] + numbers[index + 1];
         }
-    
+        
         if (symbols[index] == '-') {
             return numbers[index] - numbers[index + 1];
         }
-    
+        
         if (symbols[index] == '*') {
             return numbers[index] * numbers[index + 1];
         }
         
-        return 0;
+        checkCorrectDivideNumbers(numbers[index], numbers[index + 1]);
+        return numbers[index] / numbers[index + 1];
+    }
+    
+    private void checkCorrectDivideNumbers(int firstNumber, int secondNumber) throws UnsupportedOperationException {
+        if (firstNumber / secondNumber != (double) firstNumber / secondNumber) {
+            throw new UnsupportedOperationException(DIVIDE_NUMBERS_EXCEPTION_MESSAGE);
+        }
     }
     
     private char[] getSymbols(String[] split) {
@@ -46,5 +55,9 @@ public class StringCalculator {
                 .filter(index -> index % 2 == 0)
                 .map(index -> Integer.parseInt(split[index]))
                 .toArray();
+    }
+    
+    private String[] getSplit(String formula) {
+        return formula.split(" ");
     }
 }
--- src/test/java/lotto/step1/StringCalculatorTest.java
+++ src/test/java/lotto/step1/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
     @Test
@@ -28,5 +29,22 @@ public class StringCalculatorTest {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.calculate("2 * 5");
         assertThat(result).isEqualTo(10);
+    }
+    
+    @Test
+    @DisplayName("나눗셈")
+    void divide() {
+        StringCalculator stringCalculator = new StringCalculator();
+        int result = stringCalculator.calculate("10 / 5");
+        assertThat(result).isEqualTo(2);
+    }
+    
+    @Test
+    @DisplayName("나눗셈 예외")
+    void divide_exception() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertThatThrownBy(() -> stringCalculator.calculate("9 / 5"))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("나눗셈은 정확히 나누어 떨어지는 경우에만 진행할 수 있습니다. 다시 입력해주세요.");
     }
 }
