69349baf7bf8ed94d32703b449e2ac598bf19055
nooose <hd15807@gmail.com>
2022-09-23 15:39:21 +0900

    refactor: 메서드 분리

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,6 +1,29 @@
 package calculator;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public abstract class Operator {
+    public static final String ADD = "+";
+    public static final String SUBTRACT = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+    private static final Map<String, Operator> CACHE = new ConcurrentHashMap<>();
+
+    static {
+        CACHE.put(ADD, new Add());
+        CACHE.put(SUBTRACT, new Subtract());
+        CACHE.put(MULTIPLY, new Multiply());
+        CACHE.put(DIVIDE, new Divide());
+    }
+
+    public static Operator mapping(String operator) {
+        Operator result = CACHE.get(operator);
+        if (result == null) {
+            throw new IllegalArgumentException(operator + "은(는) 지원하지 않는 연산자입니다.");
+        }
+        return result;
+    }
 
     abstract int operate(int num1, int num2);
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,15 +2,18 @@ package calculator;
 
 import java.util.Queue;
 
+import static calculator.Operator.ADD;
+
 public class StringCalculator {
-    public static final String DEFAULT_OPERATOR = "+";
-    public static final String DEFAULT_DELIMITER = " ";
+    private static final String DEFAULT_OPERATOR_TYPE = ADD;
+    private static final String DEFAULT_DELIMITER = " ";
 
     private final Tokenizer tokenizer = new Tokenizer(DEFAULT_DELIMITER);
 
     public int calculate(String input) {
         Queue<String> tokens = tokenizer.queue(input);
-        return calculate(tokens, DEFAULT_OPERATOR, 0);
+
+        return calculate(tokens, DEFAULT_OPERATOR_TYPE, 0);
     }
 
     private int calculate(Queue<String> tokens, String operator, int result) {
@@ -19,18 +22,22 @@ public class StringCalculator {
         }
 
         String token = tokens.remove();
+        if (isNumber(token)) {
+            result = Operator.mapping(operator)
+                            .operate(result, Integer.parseInt(token));
 
-        try {
-            int number = Integer.parseInt(token);
-            result = operate(operator, result, number);
             return calculate(tokens, operator, result);
-        } catch (NumberFormatException e) {
-            return calculate(tokens, token, result);
         }
+
+        return calculate(tokens, token, result);
     }
 
-    private int operate(String operatorType, int num1, int num2) {
-        Operator operator = OperatorFactory.findOperator(operatorType);
-        return operator.operate(num1, num2);
+    private boolean isNumber(String token) {
+        try {
+            Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
