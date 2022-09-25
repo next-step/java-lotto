2ca226ef57fd2fe6b5b4e9edafc2ea88d471c88d
JunH <tjdtls690@gmail.com>
2022-09-25 21:51:38 +0900

    :art: refactor : 구입 금액 입력 메시지 개선

--- src/main/java/lotto/step2/view/input/InputView.java
+++ src/main/java/lotto/step2/view/input/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
     
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_FORMAT_EXCEPTION_MESSAGE = "올바른 입력 값이 아닙니다. 다시 입력해 주세요.";
-    private static final String LOTTO_PAYMENT_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_PAYMENT_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요. (1000원 단위)";
     private static final String LOTTO_PAYMENT_PRICE_INPUT_FORM = "[1-9][0-9]*000";
     private static final String WINNING_LOTTO_NUMBERS_INPUT_FORM = "(4[0-5]|[1-3][0-9]|[1-9])(,(4[0-5]|[1-3][0-9]|[1-9])){5}";
     private static final String DELIMITER = ",";
