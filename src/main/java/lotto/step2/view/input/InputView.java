e3769fc0b3639a13ac0782fbd761948e1189debd
JunH <tjdtls690@gmail.com>
2022-09-25 21:12:40 +0900

    :art: refactor : 문자열 상수화 리팩토링

--- src/main/java/lotto/step2/view/input/InputView.java
+++ src/main/java/lotto/step2/view/input/InputView.java
@@ -14,9 +14,12 @@ public class InputView {
     
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_FORMAT_EXCEPTION_MESSAGE = "올바른 입력 값이 아닙니다. 다시 입력해 주세요.";
-    private static final String LOTTO_PAYMENT_PRICE_INPUT_FORM = "[1-9][0-9]*000";
     private static final String LOTTO_PAYMENT_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-    public static final String WINNING_LOTTO_NUMBERS_INPUT_FORM = "^[1-9]$|^[1-3][0-9]$|^4[0-5]((,)(^[1-9]$|^[1-3][0-9]$|^)){5}";
+    private static final String LOTTO_PAYMENT_PRICE_INPUT_FORM = "[1-9][0-9]*000";
+    private static final String WINNING_LOTTO_NUMBERS_INPUT_FORM = "^[1-9]$|^[1-3][0-9]$|^4[0-5]((,)(^[1-9]$|^[1-3][0-9]$|^)){5}";
+    private static final String DELIMITER = ",";
+    private static final String SPACE = " ";
+    private static final String EMPTY = "";
     
     public static int lottoPaymentPriceInput() {
         try {
@@ -69,11 +72,11 @@ public class InputView {
     }
     
     private static String removeSpace(String input) {
-        return input.replace(" ", "");
+        return input.replace(SPACE, EMPTY);
     }
     
     private static List<LottoNumber> getLottoNumbers(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(DELIMITER))
                 .mapToInt(Integer::parseInt)
                 .mapToObj(LottoNumber::new)
                 .collect(Collectors.toList());
