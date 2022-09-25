de06d58f7e35c3870631fcd83c3478fd91d6b72e
JunH <tjdtls690@gmail.com>
2022-09-25 20:01:30 +0900

    :memo: docs : null or "" 입력 시 예외 던지기 기능 구현

--- src/main/java/lotto/step2/view/input/InputView.java
+++ src/main/java/lotto/step2/view/input/InputView.java
@@ -1,7 +1,33 @@
 package lotto.step2.view.input;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputView {
-    public static int lottoPaymentInput(String input) {
+    
+    public static final String INPUT_FORMAT_EXCEPTION_MESSAGE = "올바른 입력 값이 아닙니다. 다시 입력해 주세요.";
+    public static final String LOTTO_PAYMENT_INPUT_FORM = "[1-9][0-9]*000";
+    
+    public static int lottoPaymentInput(String input) throws IllegalArgumentException {
+        checkAllLottoPaymentInputException(input);
         return Integer.parseInt(input);
+    }
+    
+    private static void checkAllLottoPaymentInputException(String input) throws IllegalArgumentException {
+        checkNullException(input);
+        checkLottoPaymentInputFormatException(input);
+    }
+    
+    private static void checkLottoPaymentInputFormatException(String input) throws IllegalArgumentException {
+        Matcher matcher = Pattern.compile(LOTTO_PAYMENT_INPUT_FORM).matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INPUT_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+    
+    private static void checkNullException(String input) throws IllegalArgumentException {
+        if (input == null) {
+            throw new IllegalArgumentException(INPUT_FORMAT_EXCEPTION_MESSAGE);
+        }
     }
 }
--- src/test/java/lotto/step2/view/input/InputViewTest.java
+++ src/test/java/lotto/step2/view/input/InputViewTest.java
@@ -2,8 +2,11 @@ package lotto.step2.view.input;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputViewTest {
     @Test
@@ -11,5 +14,14 @@ public class InputViewTest {
     void input() {
         int lottoPaymentInput = InputView.lottoPaymentInput("14000");
         assertThat(lottoPaymentInput).isEqualTo(14000);
+    }
+    
+    @NullAndEmptySource
+    @ParameterizedTest(name = "{displayName} : {0}")
+    @DisplayName("null 또는 \"\" 입력 시 예외 던지기")
+    void null_or_empty_input_exception(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputView.lottoPaymentInput(input))
+                .withMessage("올바른 입력 값이 아닙니다. 다시 입력해 주세요.");
     }
 }
