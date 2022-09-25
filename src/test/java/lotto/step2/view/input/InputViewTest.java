0e19162e0e85849abf1e132120c5e2b17ef1640c
JunH <tjdtls690@gmail.com>
2022-09-25 21:48:10 +0900

    :white_check_mark: test : 0 입력 시 예외 던지기 기능 테스트 완료

--- src/test/java/lotto/step2/view/input/InputViewTest.java
+++ src/test/java/lotto/step2/view/input/InputViewTest.java
@@ -108,4 +108,12 @@ public class InputViewTest {
                 .isThrownBy(() -> InputView.winningLottoNumbersInput("1, 2, 3, 4, -5, 6"))
                 .withMessage("올바른 입력 값이 아닙니다. 다시 입력해 주세요.");
     }
+    
+    @Test
+    @DisplayName("지난 주 당첨 번호 - 0 입력 시 예외 던지기")
+    void winner_lotto_numbers_input_zero_exception() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputView.winningLottoNumbersInput("1, 2, 0, 4, 5, 6"))
+                .withMessage("올바른 입력 값이 아닙니다. 다시 입력해 주세요.");
+    }
 }
