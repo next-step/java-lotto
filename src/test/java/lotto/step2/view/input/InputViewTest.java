92ee7aa6bb0668b91e2c25714649226ad2232686
JunH <tjdtls690@gmail.com>
2022-09-25 21:49:48 +0900

    :white_check_mark: test : 입력 형식에 맞지 않게 입력 시 예외 던지기 기능 테스트 완료

--- src/test/java/lotto/step2/view/input/InputViewTest.java
+++ src/test/java/lotto/step2/view/input/InputViewTest.java
@@ -116,4 +116,13 @@ public class InputViewTest {
                 .isThrownBy(() -> InputView.winningLottoNumbersInput("1, 2, 0, 4, 5, 6"))
                 .withMessage("올바른 입력 값이 아닙니다. 다시 입력해 주세요.");
     }
+    
+    @DisplayName("지난 주 당첨 번호 - 숫자가 아닌 값 입력 시 예외 던지기")
+    @ParameterizedTest(name = "{displayName} : {0}")
+    @ValueSource(strings = {"1, 2, 3 4, 5, 6", "1, 2, 3, 4, 5, ", "1, 2, 3, 4, 5"})
+    void winner_lotto_numbers_input_different_format_exception(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputView.winningLottoNumbersInput(input))
+                .withMessage("올바른 입력 값이 아닙니다. 다시 입력해 주세요.");
+    }
 }
