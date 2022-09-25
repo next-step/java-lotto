ef0c3946e5b414e35dbe068accaaabd3927e6665
nooose <hd15807@gmail.com>
2022-09-25 22:44:41 +0900

    refactor: 테스트 메서드 변경 AssertJ에서 제공하는 Exception 메서드를 사용해봤다.

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -10,10 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
-public class StringCalculatorTest {
+ public class StringCalculatorTest {
 
     StringCalculator calculator;
 
@@ -66,12 +65,12 @@ public class StringCalculatorTest {
     @DisplayName("0으로 나누면 예외 발생")
     @Test
     void divideZero() {
-        assertThatThrownBy(() -> new Divide().operate(10, 0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException().isThrownBy(() -> new Divide().operate(10, 0));
     }
 
     @DisplayName("지원하지 않는 연산자 입력 시 예외 발생")
     @Test
     void calculateWhenInputNotSupportedOperator() {
-        assertThatThrownBy(() -> calculator.calculate("2 % 3")).isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate("2 % 3"));
     }
 }
