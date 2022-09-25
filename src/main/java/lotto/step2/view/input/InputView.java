a51bebf0cdaf7077a5a4863ea0118f419244ac6a
JunH <tjdtls690@gmail.com>
2022-09-25 21:57:08 +0900

    :sparkles: feat : 당첨 번호 입력 기능 구현

--- src/main/java/lotto/step2/view/input/InputView.java
+++ src/main/java/lotto/step2/view/input/InputView.java
@@ -17,6 +17,7 @@ public class InputView {
     private static final String LOTTO_PAYMENT_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요. (1000원 단위)";
     private static final String LOTTO_PAYMENT_PRICE_INPUT_FORM = "[1-9][0-9]*000";
     private static final String WINNING_LOTTO_NUMBERS_INPUT_FORM = "(4[0-5]|[1-3][0-9]|[1-9])(,(4[0-5]|[1-3][0-9]|[1-9])){5}";
+    private static final String WINNING_LOTTO_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요. (구분자는 콤마, 각 숫자 범위는 1 ~ 45)";
     private static final String DELIMITER = ",";
     private static final String SPACE = " ";
     private static final String EMPTY = "";
@@ -51,6 +52,16 @@ public class InputView {
     private static void checkNullException(String input) throws IllegalArgumentException {
         if (input == null) {
             throw new IllegalArgumentException(INPUT_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+    
+    public static WinningLottoNumbers winningLottoNumbersInput() throws IllegalArgumentException {
+        try {
+            System.out.println(WINNING_LOTTO_NUMBERS_INPUT_MESSAGE);
+            return winningLottoNumbersInput(SCANNER.nextLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return winningLottoNumbersInput();
         }
     }
     
