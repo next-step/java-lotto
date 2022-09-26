14ae624b81662da5b78011db4d2e67c2af07dad4
suchan <aud248@naver.com>
2022-09-26 14:43:37 +0900

    feat: Step1 문자열 계산기 뺄셈 기능 추가

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -18,6 +18,20 @@ public class StringCalculator {
                 .reduce(0, Integer::sum);
     }
 
+    public static int minus(String text) {
+        String[] minusParameters = text.split(" ");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < minusParameters.length; i++) {
+            isNumeric(minusParameters[i], numbers);
+        }
+
+        return numbers.stream()
+                .reduce((a, b) -> a - b)
+                .get().intValue();
+    }
+
     private static boolean isNumeric(String addParameter, List<Integer> numbers){
         try {
             int number = Integer.parseInt(addParameter);
--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -11,4 +11,10 @@ public class StringCalculatorTest {
         Integer sum = StringCalculator.add("11 + 4");
         assertThat(sum).isEqualTo(15);
     }
+
+    @Test
+    void minus(){
+        int minus = StringCalculator.minus("11 - 4 - 3");
+        assertThat(minus).isEqualTo(4);
+    }
 }
