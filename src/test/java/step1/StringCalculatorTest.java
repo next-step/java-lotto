e81ab8b63891c72be8f8b1ca4258f724add18dad
suchanmyoung <aud248@naver.com>
2022-09-26 20:47:54 +0900

    test: 예외 테스트 추가

--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -1,9 +1,11 @@
 package step1;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
 
@@ -32,6 +34,7 @@ public class StringCalculatorTest {
     }
 
     @Test
+    @DisplayName("복합 사칙연산을 공백 구분자로 처리한다.")
     void complexCalculate(){
         int calculate = StringCalculator.calculate("11 + 3 - 4 / 5");
         int calculate2 = StringCalculator.calculate("2 + 5 / 7");
@@ -42,5 +45,21 @@ public class StringCalculatorTest {
                 () -> assertThat(calculate2).isEqualTo(1),
                 () -> assertThat(calculate3).isEqualTo(30)
         );
+    }
+
+    @Test
+    @DisplayName("사칙연산 이외의 기호가 들어가면 IllegalArgumentException 을 던진다.")
+    void operator_exception(){
+        assertThatThrownBy(() ->
+            StringCalculator.calculate("11 % 4"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("null 혹은 공백 문자열이 들어가면 IllegalArgumentException 을 던진다.")
+    void operand_exception(){
+        assertThatThrownBy(() ->
+            StringCalculator.calculate(" + 4"))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
