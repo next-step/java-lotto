e562a6f0753f0bdae23357e67f96783e4cfc4d4e
suchanmyoung <aud248@naver.com>
2022-09-26 20:48:43 +0900

    feat: 입출력 / 메인 클래스 추가

---
+++ src/main/java/step1/input/StringCalculatorInput.java
@@ -0,0 +1,11 @@
+package step1.input;
+
+import java.util.Scanner;
+
+public class StringCalculatorInput {
+
+    public static String operation(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
