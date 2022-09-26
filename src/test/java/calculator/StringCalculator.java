71cf6fe23f4061b68ddfb53893abf49259ca81aa
devcs96 <katd6@naver.com>
2022-09-26 22:45:24 +0900

    :recycle: refactor(calcaulator) : 메소드 네이밍 변경

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,18 +1,14 @@
 package calculator;
 
-import java.util.Deque;
-import java.util.Stack;
-
 public class StringCalculator {
 
     private static final String DEFAULT_DELIMITER = " ";
-    private static final int MIN_CALCULABLE_OPERAND_NUMBER = 2;
 
     public static Operand calculate(String input){
         Operands operands = new Operands();
         Operators operators = new Operators();
         for (String param : input.split(DEFAULT_DELIMITER)) {
-            addCollectionByParamType(operands, operators, param);
+            separateParam(operands, operators, param);
             calculate(operands, operators);
         }
         return operands.getOperand();
@@ -25,7 +21,7 @@ public class StringCalculator {
         operands.add(operators.getOperator().operation.apply(operands.getOperand(),operands.getOperand()));
     }
 
-    private static void addCollectionByParamType(Operands operands , Operators operators , String param) {
+    private static void separateParam(Operands operands , Operators operators , String param) {
         if (Operator.isOperator(param)){
             operators.add(Operator.getOperator(param));
             return;
