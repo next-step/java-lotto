6ab00eb32447fedd59cd4ce9d2c2e9a19a9691fb
nooose <hd15807@gmail.com>
2022-09-28 03:21:53 +0900

    refactor: 숫자, 연산자 분리 피드백 반영 숫자와 연산자를 분리하는 클래스를 구현해 불러오도록 처리했고, 기능 구현에 따라 테스트 코드도 작성했다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,54 +2,31 @@ package calculator;
 
 import calculator.type.OperatorType;
 
-import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class StringCalculator {
-    private static final String INVALID_INPUT_MESSAGE = "수식이 올바르지 않습니다.";
-    private static final String DEFAULT_OPERATOR_TYPE = OperatorType.ADD.getOperator();
-    private static final int DEFAULT_VALUE = 0;
-
-    private StringTokenizer tokenizer;
 
     public int calculate(String input) {
-        setTokenizer(input);
-        return operate(DEFAULT_OPERATOR_TYPE, DEFAULT_VALUE);
+        InputTokenizer tokenizer = new InputTokenizer(input);
+
+        Queue<Integer> numbers = tokenizer.numbers();
+        Queue<OperatorType> operators = tokenizer.operators();
+
+        return operate(numbers, operators);
     }
 
-    private void setTokenizer(String input) {
-        try {
-            tokenizer = new StringTokenizer(input);
-        } catch (Exception e) {
-            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+    private int operate(Queue<Integer> numbers, Queue<OperatorType> operators) {
+        int result = numbers.remove();
+
+        while (hasMoreTokens(numbers)) {
+            OperatorType operator = operators.remove();
+            result = operator.operate(result, numbers.remove());
         }
+
+        return result;
     }
 
-    private int operate(String operator, int result) {
-        if (hasNoMoreTokens()) {
-            return result;
-        }
-
-        String token = tokenizer.nextToken();
-        OperatorType supportedOperator = OperatorType.findOperator(operator);
-
-        if (isNumber(token)) {
-            result = supportedOperator.operate(result, Integer.parseInt(token));
-            return operate(operator, result);
-        }
-
-        return operate(token, result);
-    }
-
-    private boolean isNumber(String number) {
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean hasNoMoreTokens() {
-        return !tokenizer.hasMoreTokens();
+    private boolean hasMoreTokens(Queue<Integer> numbers) {
+        return !numbers.isEmpty();
     }
 }
