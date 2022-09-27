3292ffb32234281fd21c1b54641b8fa5c64ff73e
김찬수 <cskim96@crscube.co.kr>
2022-09-27 13:18:51 +0900

    :fire: chore(comment) : 불필요한 주석 삭제

--- src/main/java/lotto/service/impl/RandomNumberPicker.java
+++ src/main/java/lotto/service/impl/RandomNumberPicker.java
@@ -6,15 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/**
- * Created by P-161 at : 2022-09-27
- *
- * 여기에 RandomNumberPicker 클래스에 대한 설명을 기술해주세요
- *
- * @author P-161
- * @version 1.0
- * @since 1.0
- */
+
 public class RandomNumberPicker implements LottoNumberPicker {
 
     private final Random random = new Random();
