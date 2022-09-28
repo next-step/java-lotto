43a7d67cb4451febc2e65e6a44a8ce41cac176cc
devcs96 <katd6@naver.com>
2022-09-29 00:33:19 +0900

    :recycle: refactor : under score 추가

--- src/main/java/lotto/model/MatchNumber.java
+++ src/main/java/lotto/model/MatchNumber.java
@@ -9,10 +9,10 @@ public enum MatchNumber {
     ZERO(0, 0),
     ONE(1, 0),
     TWO(2, 0),
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000);
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1500_000),
+    SIX(6, 2000_000_000);
 
     private final Integer count;
 
