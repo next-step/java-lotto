b38988dff0107b68a096b29aba756ccf91c83734
devcs96 <katd6@naver.com>
2022-09-29 00:45:46 +0900

    :recycle: refactor(ui) : BufferedReader 관리를 InputView 에게 위임

--- src/main/java/lotto/controller/LottoApp.java
+++ src/main/java/lotto/controller/LottoApp.java
@@ -1,61 +1,36 @@
 package lotto.controller;
 
-import lotto.LottoStore;
 import lotto.model.Lotto;
 import lotto.model.Lottos;
 import lotto.service.ProfitStrategy;
-import lotto.service.impl.RandomNumberPicker;
 import lotto.service.impl.RelativeProfitStrategy;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class LottoApp {
 
-    private static final Logger LOGGER = Logger.getLogger( LottoApp.class.getName() );
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            Integer money = getMoneyFromUser(br);
-            Lottos lottos = getLottos(money);
+    private static final Logger LOGGER = Logger.getLogger(LottoApp.class.getName());
 
-            Lotto winningLotto = getWinningLottoFromUser(br);
+    public static void main(String[] args) {
+        try (InputView inputView = new InputView(new BufferedReader(new InputStreamReader(System.in)))) {
+            Integer money = inputView.getMoneyFromUser();
+            Lottos lottos = inputView.getLottos(money);
+
+            Lotto winningLotto = inputView.getWinningLottoFromUser();
+
             OutputView.printSummary(lottos.getMatchNumbers(winningLotto));
 
             ProfitStrategy profitStrategy = getProfitStrategy();
             OutputView.printProfit(profitStrategy.calculate(money, lottos.getWinningMoney(winningLotto)));
         } catch (IllegalArgumentException e) {
             System.out.println("유효하지 않은 입력값입니다.");
-            LOGGER.log(Level.SEVERE , e.getMessage() , e);
-        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static Integer getMoneyFromUser(BufferedReader br) throws IOException {
-        InputView.printMoneyQst();
-        int money = Integer.parseInt(br.readLine().trim());
-        if (money <= 0) {
-            throw new IllegalArgumentException("돈은 0 또는 음수일수 없습니다.");
-        }
-        return money;
-    }
-
-    private static Lottos getLottos(Integer money) {
-        Lottos lottos = new LottoStore(new RandomNumberPicker()).buy(money);
-        InputView.printLottoBuyMsg(lottos.getSize());
-        OutputView.printLottos(lottos);
-        return lottos;
-    }
-
-    private static Lotto getWinningLottoFromUser(BufferedReader br) throws IOException {
-        InputView.printWinningNumberQst();
-        return new Lotto(Arrays.stream(br.readLine().split(InputView.DEFAULT_WINNNING_NUMBER_SEPARATOR)).map((num) -> Integer.valueOf(num.trim()))
-                .collect(Collectors.toList()));
     }
 
     private static ProfitStrategy getProfitStrategy() {
