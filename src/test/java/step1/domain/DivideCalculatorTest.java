0b27baad2ee09e8a55e33743948aefc6e82781ea
KimWanJoo <wjwan0915@gmail.com>
2022-09-29 11:38:31 +0900

    :white_check_mark: test(나누기 의존성검사 추가 (/0) )

--- src/test/java/step1/domain/DivideCalculatorTest.java
+++ src/test/java/step1/domain/DivideCalculatorTest.java
@@ -2,12 +2,10 @@ package step1.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step1.domain.CalculatorPolicy;
-import step1.domain.DivideCalculator;
-import step1.domain.Number;
 
 class DivideCalculatorTest {
 
@@ -28,5 +26,17 @@ class DivideCalculatorTest {
 
         //then
         assertThat(result).isEqualTo(new Number(5));
+    }
+
+    @Test
+    @DisplayName("0으로 나눌 시 예외가 발생한다.")
+    void zero_divide_throw_exception() {
+        //given
+        CalculatorPolicy calculatorPolicy = new DivideCalculator(new Number(5));
+
+        //then
+        assertThatThrownBy(() -> calculatorPolicy.calculate(new Number(0)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("0으로 나눌 수 없습니다.");
     }
 }
