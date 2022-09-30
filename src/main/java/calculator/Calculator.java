728b65636e48db03e80c25765dc963c10af62f04
kingkk31 <kingkk31@naver.com>
2022-09-30 13:14:54 +0900

    refactor: Calculator 연산 메소드 모두 static 변경 및 사칙연산자 static 상수 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,19 +2,24 @@ package calculator;
 
 public class Calculator {
 
-    public int add(int leftNum, int rightNum) {
+    public static final String ADD_OPERATOR = "+";
+    public static final String SUB_OPERATOR = "-";
+    public static final String MULTIPLY_OPERATOR = "*";
+    public static final String DIV_OPERATOR = "/";
+
+    public static int add(int leftNum, int rightNum) {
         return leftNum + rightNum;
     }
 
-    public int sub(int leftNum, int rightNum) {
+    public static int sub(int leftNum, int rightNum) {
         return leftNum - rightNum;
     }
 
-    public int multiply(int leftNum, int rightNum) {
+    public static int multiply(int leftNum, int rightNum) {
         return leftNum * rightNum;
     }
 
-    public int div(int leftNum, int rightNum) {
+    public static int div(int leftNum, int rightNum) {
         return leftNum / rightNum;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -11,40 +11,32 @@ class CalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"2:3:5", "0:4:4", "-1:-2:-3"}, delimiter = ':')
     @DisplayName("덧셈")
-    void add_formula(int leftNum, int rightNum, int expected) {
-        Calculator calculator = new Calculator();
-
-        assertThat(calculator.add(leftNum, rightNum))
+    void add_numbers(int leftNum, int rightNum, int expected) {
+        assertThat(Calculator.add(leftNum, rightNum))
                 .isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"3:2:1", "4:0:4", "-1:-2:1"}, delimiter = ':')
     @DisplayName("뺄셈")
-    void subtract_formula(int leftNum, int rightNum, int expected) {
-        Calculator calculator = new Calculator();
-
-        assertThat(calculator.sub(leftNum, rightNum))
+    void subtract_numbers(int leftNum, int rightNum, int expected) {
+        assertThat(Calculator.sub(leftNum, rightNum))
                 .isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2:3:6", "0:4:0", "-1:-2:2"}, delimiter = ':')
     @DisplayName("곱셈")
-    void multiply_formula(int leftNum, int rightNum, int expected) {
-        Calculator calculator = new Calculator();
-
-        assertThat(calculator.multiply(leftNum, rightNum))
+    void multiply_numbers(int leftNum, int rightNum, int expected) {
+        assertThat(Calculator.multiply(leftNum, rightNum))
                 .isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"4:2:2", "0:4:0", "15:5:3"}, delimiter = ':')
     @DisplayName("나눗셈")
-    void divide_formula(int leftNum, int rightNum, int expected) {
-        Calculator calculator = new Calculator();
-
-        assertThat(calculator.div(leftNum, rightNum))
+    void divide_numbers(int leftNum, int rightNum, int expected) {
+        assertThat(Calculator.div(leftNum, rightNum))
                 .isEqualTo(expected);
     }
 }
