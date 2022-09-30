814434ff748ecf416636c75ba91731241d539fe8
OhSeungCheol <sc.oh131@gmail.com>
2022-09-30 16:57:30 +0900

    [M1] feat: UI 관련 코드 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private String formulaString;
+
+    public InputView(){
+        System.out.println("계산식을 입력하세요(숫자와 연산자는 공백 기준으로 구분) : ");
+        Scanner scanner = new Scanner(System.in);
+        String formulaString = scanner.nextLine();
+        this.formulaString = formulaString;
+    }
+
+    public String getFormulaString() {
+        return formulaString;
+    }
+}
