c32eb9d579b0fbc19b2e1e35a2284e2a15d6c92e
JunH <tjdtls690@gmail.com>
2022-09-30 11:06:22 +0900

    :art: refactor : enum에서 hashmap 제거

--- src/main/java/lotto/step1/Operation.java
+++ src/main/java/lotto/step1/Operation.java
@@ -1,32 +1,27 @@
 package lotto.step1;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public enum Operation {
-    ADD(Integer::sum),
-    SUBTRACT((result, operand) -> result - operand),
-    MULTIPLY((result, operand) -> result * operand),
-    DIVIDE(new DivideStrategy());
-    
-    private static final Map<Character, Operation> operationMapper = new HashMap<>();
+    ADD('+', Integer::sum),
+    SUBTRACT('-', (result, operand) -> result - operand),
+    MULTIPLY('*', (result, operand) -> result * operand),
+    DIVIDE('/', new DivideStrategy());
     
     private final BiFunction<Integer, Integer, Integer> operation;
+    private final char operator;
     
-    static {
-        operationMapper.put('+', ADD);
-        operationMapper.put('-', SUBTRACT);
-        operationMapper.put('*', MULTIPLY);
-        operationMapper.put('/', DIVIDE);
-    }
-    
-    Operation(BiFunction<Integer, Integer, Integer> operation) {
+    Operation(char operator, BiFunction<Integer, Integer, Integer> operation) {
+        this.operator = operator;
         this.operation = operation;
     }
     
     public static Operation mappingOperation(Character operator) {
-        return operationMapper.get(operator);
+        return Arrays.stream(values())
+                .filter(operation -> operation.operator == operator)
+                .findAny()
+                .orElseThrow();
     }
     
     public int calculate(Integer calculateResult, Integer secondOperand) {
