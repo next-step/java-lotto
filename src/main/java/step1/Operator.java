e70e84a56dcf12368c4f6dec7a7a24a9f4548e4b
suchan <aud248@naver.com>
2022-09-30 10:22:05 +0900

    refactor: Operator Enum, Operand Class 로 관리 책임 분배

--- src/main/java/step1/Operator.java
+++ src/main/java/step1/Operator.java
@@ -1,4 +1,26 @@
 package step1;
 
-public class Operator {
+import java.util.Arrays;
+
+public enum Operator {
+
+    PLUS("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+
+    private String operator;
+
+    Operator(String operator) {
+        this.operator = operator;
+    }
+
+    static boolean isOperator(String operator){
+        return Arrays.stream(Operator.values())
+                .anyMatch(operatorType -> operatorType.getOperator().equals(operator));
+    }
+
+    String getOperator() {
+        return operator;
+    }
 }
--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -12,7 +12,7 @@ public class StringCalculator {
 
     public static int calculate(String text) {
         String[] parameters = text.split(OPERATOR_OPERAND_DELIMITER);
-        List<Integer> operands = new ArrayList<>();
+        List<Operand> operands = new ArrayList<>();
         List<String> operators = new ArrayList<>();
 
         for (int i = 0; i < parameters.length; i++) {
@@ -22,22 +22,22 @@ public class StringCalculator {
         return generateResult(operators, operands);
     }
 
-    private static void distinguishOperandAndOperator(String operand, List<Integer> numbers, List<String> operator){
-        try {
-            int number = Integer.parseInt(operand);
-            numbers.add(number);
-        } catch (NumberFormatException numberFormatException) {
+    private static void distinguishOperandAndOperator(String operand, List<Operand> operands, List<String> operator){
+        if (Operator.isOperator(operand)) {
             operator.add(operand);
+            return;
         }
+
+        operands.add(new Operand(operand));
     }
 
-    private static int generateResult(List<String> operators, List<Integer> operand) {
+    private static int generateResult(List<String> operators, List<Operand> operand) {
         int result = 0;
 
         for (int i = 0; i < operators.size(); i++) {
             Calculator calculator = OperatorFactory.createCalculator(operators.get(i));
-            result = calculator.calculate(operand.get(i), operand.get(i+1));
-            operand.set(i+1, result);
+            result = calculator.calculate(operand.get(i).getOperand(), operand.get(i+1).getOperand());
+            operand.set(i+1, new Operand(String.valueOf(result)));
         }
 
         return result;
