ce49b2cfb2ed043611738478dc113e28e53acfb0
hiblue02 <hiblue02@gmail.com>
2022-10-01 18:23:44 +0900

    refactor(LottoSeller): buyLotto를 LottoGame을 반환하게 수정

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -9,12 +8,6 @@ public class LottoSeller {
     private LottoSeller() {
     }
 
-    /**
-     * 가격을 입력하면, 구매 가능할 로또 개수를 반환
-     *
-     * @param money
-     * @return count
-     */
     public static int getLottoCount(int money) {
         int count = money / Lotto.PRICE;
 
@@ -25,15 +18,15 @@ public class LottoSeller {
 
     private static void validateMoney(int money, int count) {
         if (count * Lotto.PRICE != money) {
-            throw new IllegalArgumentException("거스름돈은 반환이 불가능합니다. 로또는 " + Lotto.PRICE + " 입니다.");
+            throw new IllegalArgumentException("거스름돈은 반환이 불가능합니다. 로또는 " + Lotto.PRICE + "원 입니다.");
         }
     }
 
-    public static List<Lotto> buyLotto(int count) {
+    public static LottoGame buyLottoGame(int count) {
 
-        return IntStream.range(0, count)
+        return new LottoGame(IntStream.range(0, count)
                 .mapToObj(a -> LottoAutoMaker.makeLotto())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
 
     }
 }
