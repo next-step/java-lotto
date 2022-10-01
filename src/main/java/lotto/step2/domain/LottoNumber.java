dced771155e10b733e9df881f4747c4c76dcbf10
JunH <tjdtls690@gmail.com>
2022-10-02 00:35:32 +0900

    :sparkles: feat : 로또 번호 범위 초과 시 예외 던지기 기능 추가

--- src/main/java/lotto/step2/domain/LottoNumber.java
+++ src/main/java/lotto/step2/domain/LottoNumber.java
@@ -3,9 +3,17 @@ package lotto.step2.domain;
 import java.util.Objects;
 
 public class LottoNumber implements Comparable<LottoNumber> {
+    private static final String LOTTO_NUMBER_RANGE_EXCEED_EXCEPTION_MESSAGE = "로또번호 범위를 초과하였습니다.";
+    private static final int LOTTO_MIN_NUMBER = 0;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    
     private final int lottoNumber;
     
     public LottoNumber(int lottoNumber) {
+        if (lottoNumber < LOTTO_MIN_NUMBER || lottoNumber > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_EXCEED_EXCEPTION_MESSAGE);
+        }
+        
         this.lottoNumber = lottoNumber;
     }
     
