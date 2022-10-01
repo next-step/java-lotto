99d634faeb47553e62edae9c5f17a4260ac9c1df
robin.son <robin.son@kakaocorp.com>
2022-10-01 16:30:57 +0900

    refactor : 테스트 코드가 성공하도록 Calculator 코드 작성

--- src/main/java/CalculatorImpl.java
+++ src/main/java/CalculatorImpl.java
@@ -3,21 +3,21 @@ import intefaces.Calculator;
 public class CalculatorImpl implements Calculator {
     @Override
     public int addition(int num1, int num2) {
-        return 0;
+        return num1 + num2;
     }
 
     @Override
     public int subtraction(int num1, int num2) {
-        return 0;
+        return num1 - num2;
     }
 
     @Override
     public int multiplication(int num1, int num2) {
-        return 0;
+        return num1 * num2;
     }
 
     @Override
     public int division(int num1, int num2) {
-        return 0;
+        return num1 / num2;
     }
 }
