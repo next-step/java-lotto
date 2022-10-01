b21e528271610afd6905d5003d45fa98fdd1e9b0
JunH <tjdtls690@gmail.com>
2022-10-01 20:36:23 +0900

    :white_check_mark: test : 테스트 코드 개선

--- src/test/java/lotto/step2/domain/LottoTicketTest.java
+++ src/test/java/lotto/step2/domain/LottoTicketTest.java
@@ -20,7 +20,7 @@ class LottoTicketTest {
     @Test
     @DisplayName("로또 번호 6개 일치 확인")
     void confirm_six_number_match() {
-        List<LottoNumber> winningLottoNumbers = Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(6));
+        List<LottoNumber> winningLottoNumbers = Arrays.asList(LottoNumberTest.ONE, LottoNumberTest.TWO, LottoNumberTest.THREE, LottoNumberTest.FOUR, LottoNumberTest.FIVE, LottoNumberTest.SIX);
         LottoRank lottoRank = LottoRank.valueOf(LOTTO_TICKET.confirmNumberMatch(winningLottoNumbers));
         assertThat(lottoRank).isEqualTo(LottoRank.FIRST);
     }
@@ -28,7 +28,7 @@ class LottoTicketTest {
     @Test
     @DisplayName("로또 번호 5개 일치 확인")
     void confirm_five_number_match() {
-        List<LottoNumber> winningLottoNumbers = Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(7), new LottoNumber(5), new LottoNumber(6));
+        List<LottoNumber> winningLottoNumbers = Arrays.asList(LottoNumberTest.ONE, LottoNumberTest.TWO, LottoNumberTest.THREE, LottoNumberTest.FOUR, LottoNumberTest.FIVE, new LottoNumber(7));
         LottoRank lottoRank = LottoRank.valueOf(LOTTO_TICKET.confirmNumberMatch(winningLottoNumbers));
         assertThat(lottoRank).isEqualTo(LottoRank.SECOND);
     }
@@ -36,7 +36,7 @@ class LottoTicketTest {
     @Test
     @DisplayName("로또 번호 4개 일치 확인")
     void confirm_four_number_match() {
-        List<LottoNumber> winningLottoNumbers = Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(7), new LottoNumber(8), new LottoNumber(6));
+        List<LottoNumber> winningLottoNumbers = Arrays.asList(LottoNumberTest.ONE, LottoNumberTest.TWO, LottoNumberTest.THREE, LottoNumberTest.FOUR, new LottoNumber(7), new LottoNumber(8));
         LottoRank lottoRank = LottoRank.valueOf(LOTTO_TICKET.confirmNumberMatch(winningLottoNumbers));
         assertThat(lottoRank).isEqualTo(LottoRank.THIRD);
     }
@@ -44,7 +44,7 @@ class LottoTicketTest {
     @Test
     @DisplayName("로또 번호 3개 일치 확인")
     void confirm_three_number_match() {
-        List<LottoNumber> winningLottoNumbers = Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(7), new LottoNumber(8), new LottoNumber(9));
+        List<LottoNumber> winningLottoNumbers = Arrays.asList(LottoNumberTest.ONE, LottoNumberTest.TWO, LottoNumberTest.THREE, new LottoNumber(7), new LottoNumber(8), new LottoNumber(9));
         LottoRank lottoRank = LottoRank.valueOf(LOTTO_TICKET.confirmNumberMatch(winningLottoNumbers));
         assertThat(lottoRank).isEqualTo(LottoRank.FOURTH);
     }
@@ -52,7 +52,7 @@ class LottoTicketTest {
     @Test
     @DisplayName("로또 번호 2개 일치 확인")
     void confirm_two_number_match() {
-        List<LottoNumber> winningLottoNumbers = Arrays.asList(new LottoNumber(1), new LottoNumber(2), new LottoNumber(7), new LottoNumber(8), new LottoNumber(9), new LottoNumber(10));
+        List<LottoNumber> winningLottoNumbers = Arrays.asList(LottoNumberTest.ONE, LottoNumberTest.TWO, new LottoNumber(7), new LottoNumber(8), new LottoNumber(9), new LottoNumber(10));
         LottoRank lottoRank = LottoRank.valueOf(LOTTO_TICKET.confirmNumberMatch(winningLottoNumbers));
         assertThat(lottoRank).isEqualTo(LottoRank.MISS);
     }
@@ -60,7 +60,7 @@ class LottoTicketTest {
     @Test
     @DisplayName("로또 번호 1개 일치 확인")
     void confirm_one_number_match() {
-        List<LottoNumber> winningLottoNumbers = Arrays.asList(new LottoNumber(1), new LottoNumber(7), new LottoNumber(8), new LottoNumber(9), new LottoNumber(10), new LottoNumber(11));
+        List<LottoNumber> winningLottoNumbers = Arrays.asList(LottoNumberTest.ONE, new LottoNumber(7), new LottoNumber(8), new LottoNumber(9), new LottoNumber(10), new LottoNumber(11));
         LottoRank lottoRank = LottoRank.valueOf(LOTTO_TICKET.confirmNumberMatch(winningLottoNumbers));
         assertThat(lottoRank).isEqualTo(LottoRank.MISS);
     }
--- src/test/java/lotto/step2/domain/PaymentPriceTest.java
+++ src/test/java/lotto/step2/domain/PaymentPriceTest.java
@@ -1,7 +1,6 @@
 package lotto.step2.domain;
 
 import lotto.step2.domain.dto.PaymentPriceDTO;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
