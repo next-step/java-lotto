67797a38f6dec58748e156a933d01b55d7f3aaad
robin.son <robin.son@kakaocorp.com>
2022-10-01 16:49:59 +0900

    feature, test : 문자열을 숫자와 수식으로 분리하는 StringConverterTest 추가 후 코드 작성

---
+++ src/main/java/StringConverter.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StringConverter {
+    private static String REGEX = " ";
+
+    public List<String> convertToList(String mathEquation) {
+        return Arrays.asList(mathEquation.split(REGEX));
+    }
+
+}
---
+++ src/test/java/StringConverterTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class StringConverterTest {
+
+    private static StringConverter stringConverter = new StringConverter();
+    private static String REGEX = " ";
+
+    @Test
+    void 문자열_분해_테스트() {
+        String userInput = "1 + 2 * 3 - 4 / 1";
+        List<String> expected = Arrays.asList(userInput.split(REGEX));
+
+        List<String> result = stringConverter.convertToList(userInput);
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
