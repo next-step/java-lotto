12048f9b4e6b098bb09d524008ccd24ff43e4e7c
OhSeungCheol <sc.oh131@gmail.com>
2022-10-02 16:56:58 +0900

    [M2] feat: lotte 클래스 및 기본 테스트 추가
    로또 번호를 생성하는 기능
    당첨 번호와 구입한 로또 번호의 일치 개수를 계산하는 기능
    
    참조: https://edu.nextstep.camp/s/DVVUSLFe/ls/y5mChmfY

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Lotto {
+    private final static List<Integer> lottoNumberPool = initLottoNumberPool();
+    private final int[] numbers;
+
+    Lotto() {
+        this.numbers = pickLottoNumber();
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public int getMatchCount(List<Integer> collectNumberList){
+        int matchCount = 0;
+        for(int number : this.numbers){
+            if(collectNumberList.contains(number)){
+                   matchCount++;
+            }
+        }
+        return matchCount;
+    }
+
+    private static List<Integer> initLottoNumberPool() {
+        List<Integer> lottoNumberList = new ArrayList<>();
+        IntStream
+                .range(Config.LOTTE_NUMBER_RANGE_TO_MIN, Config.LOTTE_NUMBER_RANGE_TO_MAX)
+                .forEach(lottoNumberList::add);
+        return lottoNumberList;
+    }
+
+    private int[] pickLottoNumber() {
+        int[] numbers = new int[Config.LOTTE_NUMBER_COUNT];
+        Collections.shuffle(lottoNumberPool);
+        for (int index = 0; index < Config.LOTTE_NUMBER_COUNT; index++) {
+            numbers[index] = lottoNumberPool.get(index);
+        }
+        return numbers;
+    }
+
+}
