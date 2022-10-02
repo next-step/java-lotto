02ed73d3dd3197307b4a62662fa346ea9dc4c03f
OhSeungCheol <sc.oh131@gmail.com>
2022-10-02 17:47:27 +0900

    [M2] feat: lottoMachine 클래스 및 기본 테스트 추가
    구입금액에 해당하는 개수대로 로또를 생성 하는 기능
    
    참조: https://edu.nextstep.camp/s/DVVUSLFe/ls/y5mChmfY

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    public List<Lotto> buyLotto(int money){
+        List<Lotto> lottoList = new ArrayList<>();
+        int lottoCount = money / Config.LOTTE_PRICE;
+        for(int index = 0; index < lottoCount; index++){
+            lottoList.add(new Lotto());
+        }
+        return lottoList;
+    }
+}
