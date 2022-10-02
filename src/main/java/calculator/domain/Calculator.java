3c8010838b4dc354eb967bfddd0884d9f0dc941e
swsong <ssw890720@gmail.com>
2022-10-03 00:54:28 +0900

    refactor: 클래스명 변경, 로직 이동

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,48 +1,30 @@
 package calculator.domain;
 
-import calculator.util.StringUtils;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Stack;
 
 /**
  * Created by seungwoo.song on 2022-09-27
  */
 public class Calculator {
-    private final CalculateResult calculateResult;
 
-    public Calculator(CalculateResult calculateResult) {
-        this.calculateResult = calculateResult;
-    }
+    private Stack<Operator> operator = new Stack<>();
+    private int result;
 
-    public int calculate(String text) {
-        validateInput(text);
-
-        List<CalculateInfo> list = convert(text);
-        for (CalculateInfo calculateInfo : list) {
-            calculateInfo.calculate(calculateResult);
-        }
-        return calculateResult.result();
-    }
-
-    private void validateInput(String text) {
-        if (StringUtils.isBlank(text)) {
-            throw new IllegalArgumentException("입력값이 없습니다.");
-        }
-    }
-
-    private List<CalculateInfo> convert(String text) {
-        return Arrays.stream(text.split(" "))
-                .map(token -> parse(token))
-                .collect(Collectors.toList());
-    }
-
-    private CalculateInfo parse(String token) {
-        if (Operator.isOperator(token)) {
-            return new OpercatorValue(Operator.from(token));
+    public void calculate(String value) {
+        if (Operator.isOperator(value)) {
+            operator.push(Operator.from(value));
+            return;
         }
 
-        return new NumberValue(Integer.valueOf(token));
+        if (operator.isEmpty()) {
+            result = Integer.parseInt(value);
+            return;
+        }
+
+        result = operator.pop().operate(result, Integer.parseInt(value));;
+    }
+
+    public int getResult() {
+        return result;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,50 +1,21 @@
 package calculator;
 
-import calculator.domain.CalculateResult;
 import calculator.domain.Calculator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Created by seungwoo.song on 2022-09-27
- */
 public class CalculatorTest {
 
-    private final Calculator calculator = new Calculator(new CalculateResult());
-
     @Test
-    void 더하기() {
-        assertThat(calculator.calculate("3 + 2")).isEqualTo(5);
-    }
+    void 계산() {
+        Calculator calculator = new Calculator();
+        String[] tokens = "1 + 2".split(" ");
 
-    @Test
-    void 빼기() {
-        assertThat(calculator.calculate("3 - 2")).isEqualTo(1);
-    }
+        for (String token : tokens) {
+            calculator.calculate(token);
+        }
 
-    @Test
-    void 곱하기() {
-        assertThat(calculator.calculate("3 * 2")).isEqualTo(6);
-    }
-
-    @Test
-    void 나누기() {
-        assertThat(calculator.calculate("3 / 2")).isEqualTo(1);
-    }
-
-    @Test
-    void 연산() {
-        assertThat(calculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    void 공백입력(String input) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> calculator.calculate(input))
-                .withMessageMatching("입력값이 없습니다.");
+        assertThat(calculator.getResult()).isEqualTo(3);
     }
 }
