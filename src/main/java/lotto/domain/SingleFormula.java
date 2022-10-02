4cd94dcac9fc0cb00fa3cb66c74d05cc5d994338
OhSeungCheol <sc.oh131@gmail.com>
2022-10-02 13:30:13 +0900

    [M1] style: 컨벤션 수정
    if 로직 괄호 추가
    
    https://github.com/next-step/java-lotto/pull/2508

--- src/main/java/lotto/domain/SingleFormula.java
+++ src/main/java/lotto/domain/SingleFormula.java
@@ -34,11 +34,11 @@ public class SingleFormula {
     public int doOperate(int initNumber) {
         int result = initNumber;
 
-        if (this.operator.equals("+")) result += number;
-        if (this.operator.equals("-")) result -= number;
-        if (this.operator.equals("*")) result *= number;
-        if (this.operator.equals("/")) result /= number;
-        if (nextSingleFormula == null) return result;
+        if (this.operator.equals("+")) { result += number; }
+        if (this.operator.equals("-")) { result -= number; }
+        if (this.operator.equals("*")) { result *= number; }
+        if (this.operator.equals("/")) { result /= number; }
+        if (nextSingleFormula == null) { return result; }
 
         return nextSingleFormula.doOperate(result);
     }
