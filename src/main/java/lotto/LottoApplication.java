37c437760833c59c6686a9acfd22e27d3ba9ff08
OhSeungCheol <sc.oh131@gmail.com>
2022-10-02 22:58:09 +0900

    [M2] feat: 메인 함수 추가
    참조: https://edu.nextstep.camp/s/DVVUSLFe/ls/y5mChmfY

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+import java.util.List;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        ResultView resultView= new ResultView();
+        LottoMachine lottoMachine = new LottoMachine();
+
+        // 금액 입력
+        inputView.setPurchaseAmount();
+
+        // 로또 구매
+        List<Lotto> lottoList = lottoMachine.buyLotto(inputView.getPurchaseAmount());
+
+        // 구매한 로또 현황 출력
+        resultView.printLottoNumber(lottoList);
+
+        // 지난 주 당첨 번호 입력
+        inputView.setLastWeeksCollectNumbers();
+
+        // 결과 저장
+        Result result = new Result(lottoList, inputView.getLastWeeksCollectNumbers());
+
+        // 당첨 통계 출력
+        resultView.printResult(result);
+    }
+
+}
