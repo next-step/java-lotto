fb7db1eae7cbb6d4e564fb10dc92d72578728e01
kingkk31 <kingkk31@naver.com>
2022-10-02 15:34:01 +0900

    style: InvaildOperatorException 메세지 오타 수정

--- src/main/java/calculator/exception/InvalidOperatorException.java
+++ src/main/java/calculator/exception/InvalidOperatorException.java
@@ -3,7 +3,7 @@ package calculator.exception;
 public class InvalidOperatorException extends IllegalArgumentException {
 
     public InvalidOperatorException() {
-        super("사칙연산자아 아닌 연산자가 존재합니다.");
+        super("사칙연산자가 아닌 연산자가 존재합니다.");
     }
 
 }
