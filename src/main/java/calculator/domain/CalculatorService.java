171bcf36e4b4523ab117787c81d6c5ed9d7f4191
swsong <ssw890720@gmail.com>
2022-10-03 00:54:53 +0900

    refactor: 클래스명 변경

---
+++ src/main/java/calculator/domain/CalculatorService.java
@@ -0,0 +1,33 @@
+package calculator.domain;
+
+import calculator.util.StringUtils;
+
+/**
+ * Created by seungwoo.song on 2022-09-27
+ */
+public class CalculatorService {
+
+    public static final String SEPARATOR = " ";
+    private final Calculator calculator;
+
+    public CalculatorService(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public int calculate(String text) {
+        validateInput(text);
+        String[] tokens = text.split(SEPARATOR);
+
+        for (String token : tokens) {
+            calculator.calculate(token);
+        }
+
+        return calculator.getResult();
+    }
+
+    private void validateInput(String text) {
+        if (StringUtils.isBlank(text)) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/calculator/CalculatorServiceTest.java
@@ -0,0 +1,50 @@
+package calculator;
+
+import calculator.domain.Calculator;
+import calculator.domain.CalculatorService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Created by seungwoo.song on 2022-09-27
+ */
+public class CalculatorServiceTest {
+
+    private final CalculatorService calculatorService = new CalculatorService(new Calculator());
+
+    @Test
+    void 더하기() {
+        assertThat(calculatorService.calculate("3 + 2")).isEqualTo(5);
+    }
+
+    @Test
+    void 빼기() {
+        assertThat(calculatorService.calculate("3 - 2")).isEqualTo(1);
+    }
+
+    @Test
+    void 곱하기() {
+        assertThat(calculatorService.calculate("3 * 2")).isEqualTo(6);
+    }
+
+    @Test
+    void 나누기() {
+        assertThat(calculatorService.calculate("2 / 2")).isEqualTo(1);
+    }
+
+    @Test
+    void 연산() {
+        assertThat(calculatorService.calculate("2 + 3 * 4 / 2")).isEqualTo(10);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    void 공백입력(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> calculatorService.calculate(input))
+                .withMessageMatching("입력값이 없습니다.");
+    }
+}
