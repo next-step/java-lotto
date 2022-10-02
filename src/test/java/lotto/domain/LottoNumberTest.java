14abed99fde37567689da449281446da24c34240
michol.lee <lovethefeel@gmail.com>
2022-10-02 18:21:14 +0900

    feat(test): 로또 번호(6개) 담당 객체 테스트

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoNumberTest {
+
+    @DisplayName("null 또는 빈 값일 경우 익셉션 처리한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void error_create_lotto(final String input) {
+
+        assertThatThrownBy(() -> new LottoNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력 값이 null 또는 빈 공백 입니다.");
+
+    }
+
+    @DisplayName("입력받은 로또 번호를 생성한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void create_lotto(final int index) {
+
+        final LottoNumber lottoNumber = new LottoNumber("1, 2, 3, 4, 5, 6");
+        final List<Number> result = lottoNumber.getLottoNumber();
+
+        assertThat(result).contains(new Number(index));
+    }
+}
