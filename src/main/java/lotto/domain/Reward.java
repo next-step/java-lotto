435457634067492f8c0f52fa1347c4550ce47faa
OhSeungCheol <sc.oh131@gmail.com>
2022-10-02 18:58:55 +0900

    [M2] feat: 보상에 대한 정보 클래스 추가
    참조: https://edu.nextstep.camp/s/DVVUSLFe/ls/y5mChmfY

---
+++ src/main/java/lotto/domain/Reward.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Reward {
+    private final Map<Integer, Integer> rewardMap = initRewardMap();
+
+    public Map<Integer, Integer> getRewardMap() {
+        return rewardMap;
+    }
+
+    private Map<Integer, Integer> initRewardMap(){
+        // 3개 당첨 : 5000원
+        // 4개 당첨 : 50000원
+        // 5개 당첨 : 1500000원
+        // 6개 당첨 : 2000000000원
+        Map<Integer, Integer> rewardMap = new HashMap<>();
+        rewardMap.put(3, 5000);
+        rewardMap.put(4, 50000);
+        rewardMap.put(5, 1500000);
+        rewardMap.put(6, 2000000000);
+        return rewardMap;
+    }
+}
