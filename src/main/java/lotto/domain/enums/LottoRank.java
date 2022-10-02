4a6697b003a1d043b1ccd4118b6fba1f7f65435f
hiblue02 <hiblue02@gmail.com>
2022-10-02 18:30:01 +0900

    refactor(LottoRank): 로또 순위에 2등과 보너스번호 필요여부 추가

--- src/main/java/lotto/domain/enums/LottoRank.java
+++ src/main/java/lotto/domain/enums/LottoRank.java
@@ -1,22 +1,27 @@
 package lotto.domain.enums;
 
+import java.util.Arrays;
+
 /**
  * 등수별 당첨 숫자 갯수
  */
 public enum LottoRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30000000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    NONE(0, 0, false);
 
 
     private final int matchCount;
     private final int reward;
+    private final boolean needBonus;
 
-    LottoRank(int matchCount, int reward) {
+    LottoRank(int matchCount, int reward, boolean needBonus) {
         this.matchCount = matchCount;
         this.reward = reward;
+        this.needBonus = needBonus;
     }
 
     public int getMatchCount() {
@@ -27,19 +32,19 @@ public enum LottoRank {
         return reward;
     }
 
-    public static LottoRank getLottoRank(int matchCount) {
-        if (matchCount == FIRST.getMatchCount()) {
-            return FIRST;
-        }
-        if (matchCount == SECOND.getMatchCount()) {
+    public static LottoRank getLottoRank(int matchCount, boolean matchBonus) {
+
+        if (isSecond(matchCount, matchBonus)){
             return SECOND;
         }
-        if (matchCount == THIRD.getMatchCount()) {
-            return THIRD;
-        }
-        if (matchCount == FOURTH.getMatchCount()) {
-            return FOURTH;
-        }
-        return NONE;
+
+        return Arrays.stream(values())
+                .filter(lottoRank -> lottoRank.needBonus && lottoRank.matchCount == matchCount)
+                .findFirst().orElse(NONE);
+
+    }
+
+    private static boolean isSecond(int matchCount, boolean matchBonus){
+        return SECOND.matchCount == matchCount && matchBonus;
     }
 }
