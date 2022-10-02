bfac8364a79541781bd2664614142ed9ab8b582f
OhSeungCheol <sc.oh131@gmail.com>
2022-10-02 22:57:59 +0900

    [M2] test: Result 클래스의 변경된 구조에 맞게 테스트 수정
    참조: https://edu.nextstep.camp/s/DVVUSLFe/ls/y5mChmfY

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -3,18 +3,23 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ResultTest {
 
     @Test
-    @DisplayName("로또 당첨 결과가 제대로 저장 및 증가하는지 테스트")
+    @DisplayName("로또 당첨 결과가 제대로 저장, 계산되는지 테스트")
     void saveResultTest2() {
-        int matchCount = 3;
-        Result result = new Result();
-        result.saveResult(matchCount);
-        result.saveResult(matchCount);
-        assertThat(result.getWinCount(matchCount)).isEqualTo(2);
+        Lotto lotto = new Lotto();
+        List<Lotto> lottoList = Arrays.asList(lotto);
+        List<Integer> collectNumberList = Arrays.stream(lotto.getNumbers()).boxed().collect(Collectors.toList());
+        Result result = new Result(lottoList, collectNumberList);
+
+        assertThat(result.getWinCount(Config.LOTTE_NUMBER_COUNT)).isEqualTo(1);
+        assertThat(result.getRateOfIncome()).isEqualTo(2000000);
     }
 
 }
