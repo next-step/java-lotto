99e868fb70a5b675f89bf3182a2992cb8f079116
michol.lee <lovethefeel@gmail.com>
2022-10-03 21:35:14 +0900

    refactor: 메인 실행 어플리케이션 suffix 추가

--- src/main/java/lotto/LottoNumberApplication.java
+++ src/main/java/lotto/LottoNumberApplication.java
@@ -4,21 +4,18 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
-import java.util.List;
-
-public class LottoNumberGame {
+public class LottoNumberApplication {
 
     public static void main(String[] args) {
 
         final Purchase purchase = Purchase.from(InputView.purchasePrint());
         ResultView.availablePurchasePrint(purchase.available());
-        final List<LottoNumber> lottoNumbers = AutoLottoNumbers.autoCreate(purchase.available());
+        LottoNumbers lottoNumbers = new LottoNumbers(AutoLottoNumbers.autoCreate(purchase.available()));
         ResultView.autoLottoNumberPrint(lottoNumbers);
         final WinningResult winningResult = WinningResult.init();
-        winningResult.match(new WinningLottoNumber(InputView.winnerNumberPrint()), lottoNumbers);
-
+        final WinningLottoNumber winningLottoNumber = WinningLottoNumber.from(InputView.winnerNumberPrint());
+        winningResult.collect(lottoNumbers.match(winningLottoNumber));
         ResultView.winningResultPrint(winningResult);
-        purchase.update(winningResult.sum());
-        ResultView.winningPrizeRatePrint(purchase);
+        ResultView.winningPrizeRatePrint(ProfitRate.of(winningResult.sum(), purchase));
     }
 }
