fd9c11f1834b234a5e73aa92ee5026eb6692f828
michol.lee <lovethefeel@gmail.com>
2022-10-03 21:33:59 +0900

    refactor: 코드리뷰 반영 - 정적 팩토리 및 생성자 정리 - 매칭 결과 수행하는 역할 위임

--- src/main/java/lotto/domain/WinningLottoNumber.java
+++ src/main/java/lotto/domain/WinningLottoNumber.java
@@ -7,71 +7,48 @@ import java.util.stream.Collectors;
 public class WinningLottoNumber {
 
     private static final String COMMA_EMPTY_STRING = ", ";
-    public static final int MATCH_ONE = 1;
-    public static final int NOT_MATCH_ZERO = 0;
     public static final int MAX_LOTTO_NUMBER_COUNT = 6;
 
     private final List<Number> winnerLottoNumber;
 
-    public WinningLottoNumber(final String input) {
+    private WinningLottoNumber(final List<Number> winnerLottoNumber) {
 
-        validate(input);
-        this.winnerLottoNumber = convert(input);
+        this.winnerLottoNumber = winnerLottoNumber;
     }
 
-    private void validate(final String input) {
+    public static WinningLottoNumber from(final String input) {
+
+        validate(input);
+        final List<Number> lottoNumbers = split(input);
+        validateSize(lottoNumbers);
+
+        return new WinningLottoNumber(lottoNumbers);
+    }
+
+    private static void validate(final String input) {
 
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException("입력 값이 null 또는 빈 공백 입니다.");
         }
     }
 
-    private List<Number> convert(final String input) {
+    private static List<Number> split(final String input) {
 
-        final List<Number> lottoNumbers = split(input);
-        validateSize(lottoNumbers);
-
-        return lottoNumbers;
-    }
-
-    private List<Number> split(final String input) {
         return Arrays.stream(input.split(COMMA_EMPTY_STRING))
                 .map(number -> new Number(Integer.parseInt(number)))
                 .distinct()
                 .collect(Collectors.toList());
     }
 
-    private void validateSize(final List<Number> lottoNumbers) {
+    private static void validateSize(final List<Number> lottoNumbers) {
 
         if (lottoNumbers.size() != MAX_LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("6개의 숫자를 입력해야 하며 중복 숫자는 입력할 수 없습니다.");
         }
     }
 
-    public int match(final LottoNumber lottoNumber) {
-
-        int result = 0;
-        for (Number number : lottoNumber.getLottoNumber()) {
-            result += match(number);
-        }
-        return result;
-    }
-
-    private int match(final Number number) {
-        if (this.winnerLottoNumber.contains(number)) {
-            return MATCH_ONE;
-        }
-        return NOT_MATCH_ZERO;
-    }
-
     public List<Number> getWinnerLottoNumber() {
 
         return winnerLottoNumber;
-    }
-
-    @Override
-    public String toString() {
-
-        return "" + winnerLottoNumber +"";
     }
 }
