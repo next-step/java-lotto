904f938f77d4f5104ab7e7aca263d092ec9374c5
gumsu <chavi9@naver.com>
2022-10-03 19:54:53 +0900

    feat: 입력 받은 문자열 분리 기능 추가

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     String input;
+    private List<String> operator = new ArrayList<>();
+    private List<Integer> operand = new ArrayList<>();
 
     public Calculator(String input) {
         if (isNullOfEmpty(input)) {
@@ -12,5 +17,24 @@ public class Calculator {
             throw new IllegalArgumentException("입력 값이 null 이나 공백은 불가능합니다.");
         }
         return true;
+    }
+
+    public void split() {
+        String[] strings = input.split(" ");
+        for (String string : strings) {
+            if (string.equals("+") || string.equals("-") || string.equals("*") || string.equals("/")) {
+                operator.add(string);
+            } else {
+                operand.add(Integer.valueOf(string));
+            }
+        }
+    }
+
+    public List<String> getOperator() {
+        return operator;
+    }
+
+    public List<Integer> getOperand() {
+        return operand;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,24 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class CalculatorTest {
 
     @Test
     void 입력값이_null_또는_빈_공백은_에러() {
-        Assertions.assertThatThrownBy(() -> new Calculator(null))
+        assertThatThrownBy(() -> new Calculator(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 연산자와_피연산자로_분리() {
+        Calculator calculator = new Calculator("2 + 3 * 4 / 2");
+        calculator.split();
+        assertThat(calculator.getOperator()).isEqualTo(new ArrayList<String>(Arrays.asList("+","*","/")));
+        assertThat(calculator.getOperand()).isEqualTo(new ArrayList<Integer>(Arrays.asList(2,3,4,2)));
     }
 }
