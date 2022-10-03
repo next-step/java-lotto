b873ebc94685390b64ef56ba286c31d8c6cf253b
hiblue02 <hiblue02@gmail.com>
2022-10-03 18:46:11 +0900

    refactor(InputView): 수동 로또 개수와 수동 로또 번호를 입력받는 메소드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -12,23 +14,46 @@ public class InputView {
     public static int askPurchaseAmount() {
         System.out.println("구매금액을 입력해주세요.");
         int purchaseAmount = SCANNER.nextInt();
-        validatePurchaseAmount(purchaseAmount);
+        validateGreaterThanZero(purchaseAmount);
         return purchaseAmount;
     }
 
-    private static void validatePurchaseAmount(int purchaseAmount) {
-        if (purchaseAmount <= 0){
+    private static void validateGreaterThanZero(int inputNumber) {
+        if (inputNumber <= 0) {
             throw new IllegalArgumentException("0보다 큰 수만 입력할 수 있습니다.");
         }
+    }
+
+    public static int askManualCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualCount = SCANNER.nextInt();
+        validateGreaterThanZero(manualCount);
+        return manualCount;
+    }
+
+    public static List<String[]> askManualNumbers(int count) {
+        ArrayList<String[]> result = new ArrayList<>();
+
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+
+        for (int i = 0; i < count; i++) {
+            result.add(readLottoNumbers());
+        }
+
+        return result;
+    }
+
+    private static String[] readLottoNumbers() {
+        String nextLine = SCANNER.nextLine();
+        return nextLine.split(",");
     }
 
     public static String[] askWinnerNumbers() {
 
         try {
             System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-            String nextLine = SCANNER.next();
-            return nextLine.split(",");
-        }catch (Exception e){
+            return readLottoNumbers();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             askWinnerNumbers();
         }
