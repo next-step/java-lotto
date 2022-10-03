1e71811a80ada5d14d24060d3e4d3a5118b67a10
robin.son <robin.son@kakaocorp.com>
2022-10-03 18:19:43 +0900

    feature : Service로 연산을 수행하는 최종 로직은 CalculatorApplication에 추가, UI를 담당하는 Printer 분리

--- src/main/java/Step1/CalculatorApplication.java
+++ src/main/java/Step1/CalculatorApplication.java
@@ -1,9 +1,16 @@
 package Step1;
 
+import Step1.intefaces.CalculatorService;
+
 public class CalculatorApplication {
 
     public static void main(String[] args) {
+        String mathEquation = Printer.requestInput();
 
+        CalculatorService calculatorService = new CalculatorServiceImpl();
+        int result = calculatorService.calculate(mathEquation);
+
+        Printer.printResult(result);
     }
 
 }
---
+++ src/main/java/Step1/Printer.java
@@ -0,0 +1,14 @@
+package Step1;
+
+public class Printer {
+
+    public static String requestInput() {
+        System.out.println("사칙연산 수식을 입력하세요.");
+        return InputScanner.stringScan();
+    }
+
+    public static void printResult(int result) {
+        System.out.printf("계산 결과 : %d%n", result);
+    }
+
+}
