94ebab2450cca88f752e7bb077990766df3271d4
michol.lee <lovethefeel@gmail.com>
2022-10-03 21:36:43 +0900

    refactor(test): 수익률 분리로 인한 테스트 코드 수정

--- src/test/java/lotto/domain/PurchaseTest.java
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
-import java.math.BigDecimal;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -30,16 +28,5 @@ class PurchaseTest {
         final Purchase amount = Purchase.from(purchase);
 
         assertThat(amount.available()).isEqualTo(count);
-    }
-
-    @DisplayName("로또 구매 금액과 상금의 대한 비율을 구한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"1000,5000,5.00", "14000,5000,0.36", "20000,0,0.00"})
-    void rate(final String purchase, final BigDecimal totalPrice, final String rate) {
-
-        final Purchase amount = Purchase.from(purchase);
-        amount.update(totalPrice);
-
-        assertThat(amount.getRate()).isEqualTo(rate);
     }
 }
