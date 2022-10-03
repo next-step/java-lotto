da86679c066434a39ad1579eacdb59125e4367d6
gumsu <chavi9@naver.com>
2022-10-03 22:00:11 +0900

    feat: 사칙연산 기호가 아닌 경우 기능 추가

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Calculator {
+
+    private static final Pattern EXPRESSION_PATTERN = Pattern.compile("[\\d */+-]+");
     String input;
     private List<String> operator = new ArrayList<>();
     private List<Integer> operand = new ArrayList<>();
 
-    public Calculator(String input) {
-        if (isNullOfEmpty(input)) {
-            this.input = input;
+    public Calculator(String expression) {
+        if (isNullOfEmpty(expression)) {
+            this.input = expression;
+        }
+        if (!EXPRESSION_PATTERN.matcher(expression).matches()) {
+            throw new IllegalArgumentException("입력 값에 올바르지 않은 문자열이 포함되어 있습니다.");
         }
     }
 
-    public boolean isNullOfEmpty(String input) {
+    private boolean isNullOfEmpty(String input) {
         if (input == null) {
             throw new IllegalArgumentException("입력 값이 null 이나 공백은 불가능합니다.");
         }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,3 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
