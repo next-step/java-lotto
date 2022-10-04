09e10aa5285bc9c72a49d62e427b0c6715ba1a0e
JunH <tjdtls690@gmail.com>
2022-10-04 19:30:17 +0900

    :sparkles: feat : 로또 넘버 개수 유효성 검사 로직 추가

--- src/main/java/lotto/step2/domain/LottoTicket.java
+++ src/main/java/lotto/step2/domain/LottoTicket.java
@@ -5,9 +5,14 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
+    private static final String EXCEEDED_NUMBER_OF_LOTTO_NUMBERS_EXCEPTION_MESSAGE = "로또 번호는 6개까지만 가질 수 있습니다.";
+    
     private final List<LottoNumber> lottoTicket;
     
     public LottoTicket(List<LottoNumber> lottoTicket) {
+        if (lottoTicket.size() > 6) {
+            throw new IllegalArgumentException(EXCEEDED_NUMBER_OF_LOTTO_NUMBERS_EXCEPTION_MESSAGE);
+        }
         this.lottoTicket = lottoTicket;
     }
     
