bf1134dd43e76b672f6e5cf6615c14cf919bd61b
Wordbe <seonghojin3@gmail.com>
2022-10-04 22:43:35 +0900

    refactor: 보너스볼과 등수에 따라 분기처리가 없도록 리팩토링

--- src/main/java/lotto/domain/enums/Rank.java
+++ src/main/java/lotto/domain/enums/Rank.java
@@ -2,21 +2,27 @@ package lotto.domain.enums;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public enum Rank {
-    FIRST(2_000_000_000L, 6),
-    SECOND(30_000_000L, 5),
-    THIRD(1_500_000L, 5),
-    FOURTH(50_000L, 4),
-    FIFTH(5_000L, 3),
-    MISS(0L, 0);
+    FIRST(2_000_000_000L, 6, List.of(TRUE, FALSE)),
+    SECOND(30_000_000L, 5, List.of(TRUE)),
+    THIRD(1_500_000L, 5, List.of(FALSE)),
+    FOURTH(50_000L, 4, List.of(TRUE, FALSE)),
+    FIFTH(5_000L, 3, List.of(TRUE, FALSE)),
+    MISS(0L, 0, List.of(TRUE, FALSE));
 
     private final BigDecimal winningAmount;
     private final Integer matchedCount;
+    private final List<Boolean> bonusMatchedStates;
 
-    Rank(Long winningAmount, Integer matchedCount) {
+    Rank(Long winningAmount, Integer matchedCount, List<Boolean> bonusMatchedStates) {
         this.winningAmount = BigDecimal.valueOf(winningAmount);
         this.matchedCount = matchedCount;
+        this.bonusMatchedStates = bonusMatchedStates;
     }
 
     public BigDecimal winningAmount() {
@@ -32,12 +38,8 @@ public enum Rank {
     }
 
     public static Rank of(int matchedCount, boolean bonusMatched) {
-        if (bonusMatched && matchedCount == SECOND.matchedCount) {
-            return SECOND;
-        }
-
         return Arrays.stream(values())
-                .filter(rank -> rank != SECOND && rank.matchedCount == matchedCount)
+                .filter(rank -> rank.matchedCount == matchedCount && rank.bonusMatchedStates.contains(bonusMatched))
                 .findFirst()
                 .orElse(MISS);
     }
