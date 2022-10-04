3f8d33825a4e4b36d87fb70c13dbb153b8100a15
nooose <hd15807@gmail.com>
2022-10-04 23:00:45 +0900

    refactor: 접근제한자 변경 불필요하게 노출 되어있어 변경했다.

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -11,10 +11,10 @@ import java.util.stream.IntStream;
 public class LottoNumber implements Comparable<LottoNumber> {
     public static final int MIN = 1;
     public static final int MAX = 45;
-    public static final Map<Integer, LottoNumber> LOTTO_NUMBERS = new HashMap<>();
     private static final String LOTTO_NUMBER_EXCEPTION_MESSAGE = "로또 번호는 1 이상이어야 합니다.";
     private static final String NOT_FOUND_EXCEPTION_MESSAGE = "캐싱된 데이터를 찾을 수 없습니다.";
     private static final String INPUT_EXCEPTION_MESSAGE = "숫자만 입력해주세요.";
+    private static final Map<Integer, LottoNumber> LOTTO_NUMBERS = new HashMap<>();
 
     private final int number;
 
