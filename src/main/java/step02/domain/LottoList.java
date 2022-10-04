a01968f264947555a6ab584734ae2f9bfff7cab7
jinukix <jinukix@styleshare.kr>
2022-10-04 17:35:44 +0900

    feat: LottoList validate 추가

--- src/main/java/step02/domain/LottoList.java
+++ src/main/java/step02/domain/LottoList.java
@@ -7,6 +7,7 @@ public class LottoList {
     private final List<Lotto> value;
 
     public LottoList(List<Lotto> value) {
+        validateIsEmpty(value);
         this.value = value;
     }
 
@@ -16,5 +17,11 @@ public class LottoList {
 
     public int size() {
         return value.size();
+    }
+
+    private void validateIsEmpty(List<Lotto> value) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("LottoList에는 최소 하나 이상의 로또가 필요합니다.");
+        }
     }
 }
---
+++ src/test/java/step02/domain/LottoListTest.java
@@ -0,0 +1,20 @@
+package step02.domain;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoListTest {
+
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("LottoList를 빈 값으로 생성시 에러 발생 테스트.")
+    void validateIsEmpty(List<Lotto> lotto) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new LottoList(lotto));
+    }
+}
