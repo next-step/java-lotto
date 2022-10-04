85eaea202b48d1720723f78389bde25f08a7d165
kingkk31 <kingkk31@naver.com>
2022-10-04 20:07:48 +0900

    refactor: Operator apply 추상 메소드를 functional interface 로 변경 및 테스트 추가

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -2,44 +2,31 @@ package calculator;
 
 import calculator.exception.InvalidOperatorException;
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Operator {
 
-    ADD("+") {
-        @Override
-        public int apply(int leftOperand, int rightOperand) {
-            return leftOperand + rightOperand;
+    ADD("+", (leftOperand, rightOperand) -> leftOperand + rightOperand),
+    SUB("-", (leftOperand, rightOperand) -> leftOperand - rightOperand),
+    MULTIPLY("*", (leftOperand, rightOperand) -> leftOperand * rightOperand),
+    DIV("/", (leftOperand, rightOperand) -> {
+        if (rightOperand == 0) {
+            throw new DivideByZeroException();
         }
-    },
-    SUB("-") {
-        @Override
-        public int apply(int leftOperand, int rightOperand) {
-            return leftOperand - rightOperand;
-        }
-    },
-    MULTIPLY("*") {
-        @Override
-        public int apply(int leftOperand, int rightOperand) {
-            return leftOperand * rightOperand;
-        }
-    },
-    DIV("/") {
-        @Override
-        public int apply(int leftOperand, int rightOperand) {
-            if (rightOperand == 0) {
-                throw new DivideByZeroException();
-            }
-            return leftOperand / rightOperand;
-        }
-    };
+        return leftOperand / rightOperand;
+    });
 
-    private final String operatorStr;
-
-    Operator(String operatorStr) {
-        this.operatorStr = operatorStr;
+    public int apply(int leftOperand, int rightOperand) {
+        return this.calculateFunc.apply(leftOperand, rightOperand);
     }
 
-    public abstract int apply(int leftOperand, int rightOperand);
+    private final String symbol;
+    private final BiFunction<Integer, Integer, Integer> calculateFunc;
+
+    Operator(String symbol, BiFunction<Integer, Integer, Integer> calculateFunc) {
+        this.symbol = symbol;
+        this.calculateFunc = calculateFunc;
+    }
 
     public static Operator getOperator(String symbol) {
         return Arrays.stream(Operator.values())
