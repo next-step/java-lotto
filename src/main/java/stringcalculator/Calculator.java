ac374b0ea9c8519f666b8dcee5641ec0b28e86cf
owen.qq <owen.qq@kakaocorp.com>
2022-10-05 11:57:16 +0900

    feat: 기본 기능 구현 및 tc추가

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -8,4 +8,16 @@ public class Calculator {
     private static int parseNumber(String number) {
         return Integer.parseInt(number);
     }
+
+    public static int minus(String number1, String number2) {
+        return parseNumber(number1) -  parseNumber(number2);
+    }
+
+    public static int multiply(String number1, String number2) {
+        return parseNumber(number1) * parseNumber(number2);
+    }
+
+    public static int divide(String number1, String number2) {
+        return parseNumber(number1) / parseNumber(number2);
+    }
 }
--- src/test/java/stringcalculator/ATest.java
+++ src/test/java/stringcalculator/ATest.java
@@ -6,12 +6,30 @@ import org.junit.jupiter.api.Test;
 public class ATest {
 
     @Test
-    void adf() {
+    void calculator_두숫자를더한다() {
         String firstNumber = "1";
         String secondNumber = "2";
         int expected = 3;
 
-
         Assertions.assertThat(Calculator.add(firstNumber, secondNumber)).isEqualTo(expected);
+    }
+
+    @Test
+    void calculator_두숫자를뺀다() {
+        String firstNumber = "1";
+        String secondNumber = "2";
+        int expected = -1;
+
+        Assertions.assertThat(Calculator.minus(firstNumber, secondNumber)).isEqualTo(expected);
+    }
+
+    @Test
+    void calculator_두숫자를곱한다() {
+        Assertions.assertThat(Calculator.multiply("2", "3")).isEqualTo(6);
+    }
+
+    @Test
+    void calculator_두숫자를나눈다() {
+        Assertions.assertThat(Calculator.divide("4", "2")).isEqualTo(2);
     }
 }
