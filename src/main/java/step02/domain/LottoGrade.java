35558159cda914907afb4862a261b958ca6e89e5
jinukix <jinukix@styleshare.kr>
2022-10-05 17:33:19 +0900

    refactor: LottoGrade isMustBonus 필드 추가

--- src/main/java/step02/domain/LottoGrade.java
+++ src/main/java/step02/domain/LottoGrade.java
@@ -1,36 +1,53 @@
 package step02.domain;
 
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public enum LottoGrade {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
 
-    THIRD(5, 1_500_000),
+    THIRD(5, false, 1_500_000),
 
-    FOURTH(4, 50_000),
+    FOURTH(4, false, 50_000),
 
-    FIFTH(3, 5_000),
+    FIFTH(3, false, 5_000),
 
-    OTHER_GRADE(0, 0);
-
-    private static final Map<Integer, LottoGrade> lottoGradePool = Map.of(
-        6, FIRST,
-        5, THIRD,
-        4, FOURTH,
-        3, FIFTH,
-        2, OTHER_GRADE,
-        1, OTHER_GRADE,
-        0, OTHER_GRADE
-    );
+    OTHER_GRADE(0, false, 0);
 
     private final int rightCount;
     private final int winnings;
+    private final boolean isMustBonus;
 
-    LottoGrade(int rightCount, int winnings) {
+    LottoGrade(int rightCount, boolean isMustBonus, int winnings) {
         this.rightCount = rightCount;
+        this.isMustBonus = isMustBonus;
         this.winnings = winnings;
+    }
+
+    public static LottoGrade from(int rightCount, boolean bonusMatch) {
+        validateRightCountRange(rightCount);
+
+        return Arrays.stream(values())
+            .sorted(Comparator.comparing(LottoGrade::isMustBonus, Comparator.reverseOrder()))
+            .filter(lottoGrade -> equalsRightCount(lottoGrade, rightCount) && satisfyingBonusMatch(lottoGrade, bonusMatch))
+            .findFirst()
+            .orElse(OTHER_GRADE);
+    }
+
+    private static void validateRightCountRange(int rightCount) {
+        if (rightCount < 0 || rightCount > 6) {
+            throw new IllegalArgumentException("rightCount 값은 0 ~ 6 사이의 값이여야 합니다. rightCount: " + rightCount);
+        }
+    }
+
+    private static boolean equalsRightCount(LottoGrade lottoGrade, int rightCount) {
+        return lottoGrade.getRightCount() == rightCount;
+    }
+
+    private static boolean satisfyingBonusMatch(LottoGrade lottoGrade, boolean bonusMatch) {
+        return !lottoGrade.isMustBonus || bonusMatch;
     }
 
     public int getRightCount() {
@@ -41,15 +58,7 @@ public enum LottoGrade {
         return winnings;
     }
 
-    public static LottoGrade from(int rightCount, boolean bonusMatch) {
-        if (rightCount == 5 && bonusMatch) {
-            return SECOND;
-        }
-
-        LottoGrade lottoGrade = lottoGradePool.get(rightCount);
-        if (lottoGrade == null) {
-            throw new IllegalArgumentException("rightCount 값은 0 ~ 6 사이의 값이여야 합니다. rightCount: " + rightCount);
-        }
-        return lottoGrade;
+    public boolean isMustBonus() {
+        return isMustBonus;
     }
 }
