0bdf667f22761ed4ea51493964945166ba74e362
wisdompark96 <39057317+wisdompark96@users.noreply.github.com>
2022-10-05 20:42:57 +0900

    Step1 (#2512)
    * feature() : 계산기 기능 추가
    
    * fix() : 리뷰 반영
    
    BREAKING POINT
    - line formatting
    - 전체 수식 queue에 담고 검증 하도록 수정
    - renaming
    
    * fix(FormulaConert) : 리뷰 반영
    
    BREAKING POINT
    - private 생성자
    
    * fix(Calculator) : 리뷰 반영
    
    BREAKING POINT
    - private 생성자
    
    * fix(FormulaConvertTest) : 리뷰 반영
    
    BREAKING POINT
    - @NullAndEmptySource 사용하도록 수정

---
+++ src/test/java/FormulaConverterTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Queue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class FormulaConverterTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 = 2", "2,3", "abc"})
+    @DisplayName("연산자가 아니면 예외를 던진다.")
+    public void validate_input_not_operator(String input) {
+        assertThatThrownBy(() -> FormulaConverter.convert(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("연산자가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("입력값이 빈값이면 예외를 던진다.")
+    public void validate_input_is_empty(String input) {
+        assertThatThrownBy(() -> FormulaConverter.convert(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값이 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 + - 12", "2 2", "+ 1 1 +"})
+    @DisplayName("수식이 옳지 않으면 예외를 던진다.")
+    public void validate_formula(String input) {
+        assertThatThrownBy(() -> FormulaConverter.convert(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("수식이 알맞지 않습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 2:3", "1 + 2 + 2:5", "3:1"}, delimiter = ':')
+    @DisplayName("문자열을 나눈다.")
+    public void split_string(String input, int size) {
+        Queue<String> result = FormulaConverter.convert(input);
+        assertThat(result.size()).isEqualTo(size);
+    }
+}
