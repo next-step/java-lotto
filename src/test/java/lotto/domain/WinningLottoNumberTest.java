67fa38fcfb9f94c93476be11d8b132b54954364f
michol.lee <lovethefeel@gmail.com>
2022-10-06 01:20:17 +0900

    refactor(test): 2등 추가로 테스트 코드 수정

--- src/test/java/lotto/domain/WinningLottoNumberTest.java
+++ src/test/java/lotto/domain/WinningLottoNumberTest.java
@@ -1,47 +1,61 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoNumberTest {
 
-    @DisplayName("null 또는 빈 값일 경우 익셉션 처리한다.")
+    @DisplayName("1등 로또 번호가 null 또는 빈 값일 경우 익셉션 처리한다.")
     @ParameterizedTest
     @NullAndEmptySource
-    void error_create_lotto(final String input) {
+    void error_create_winning_lotto(final String winningLotto) {
 
-        assertThatThrownBy(() -> WinningLottoNumber.from(input))
+        assertThatThrownBy(() -> WinningLottoNumber.from(winningLotto, "1"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력 값이 null 또는 빈 공백 입니다.");
-
     }
 
-    @DisplayName("1등 로또 번호를 5개만 입력 또는 7개 이상 입력하거나 중복된 숫자를 입력하면 익셉션 처리한다.")
+    @DisplayName("보너스 번호가 null 또는 빈 값일 경우 익셉션 처리한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1, 2, 2, 4, 5, 6", "1, 2, 3, 4, 5", "1, 2, 3, 4, 5, 6, 7"})
-    void error_create_lotto2(final String input) {
+    @NullAndEmptySource
+    void error_create_bonus_number(final String bonusBall) {
 
-        assertThatThrownBy(() -> WinningLottoNumber.from(input))
+        assertThatThrownBy(() -> WinningLottoNumber.from("1, 2, 3, 4, 5, 6", bonusBall))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("6개의 숫자를 입력해야 하며 중복 숫자는 입력할 수 없습니다.");
+                .hasMessage("입력 값이 null 또는 빈 공백 입니다.");
     }
 
-    @DisplayName("입력받은 값으로 1등 로또 번호를 생성한다.")
+    @DisplayName("입력받은 값으로 1등 로또 번호와 보너스번호를 생성한다.")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     void create_winning_lotto(final int number) {
 
-        final WinningLottoNumber winningLottoNumber = WinningLottoNumber.from("1, 2, 3, 4, 5, 6");
+        final WinningLottoNumber winningLottoNumber = WinningLottoNumber.from("1, 2, 3, 4, 5, 6", "1");
         assertThat(winningLottoNumber.getWinnerLottoNumber()).contains(new Number(number));
+    }
+
+    @DisplayName("보너스 번호는 숫자만 생성가능하다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "?", "%"})
+    void error_bonus_number(final String bonusNumber) {
+
+        assertThatThrownBy(() -> WinningLottoNumber.from("1, 2, 3, 4, 5, 6", bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력해야 합니다.");
+    }
+
+    @DisplayName("보너스 번호는 1 ~ 45 번호 사이에서 생성가능하다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "46"})
+    void error_bonus_number2(final String number) {
+
+        assertThatThrownBy(() -> WinningLottoNumber.from("1, 2, 3, 4, 5, 6", number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 숫자는 1 ~ 45 사이여야 합니다.");
     }
 }
--- src/test/java/lotto/domain/WinningPrizeTest.java
+++ src/test/java/lotto/domain/WinningPrizeTest.java
@@ -13,18 +13,18 @@ class WinningPrizeTest {
 
     @DisplayName("각 등수별 당첨금을 확인한다.")
     @ParameterizedTest
-    @CsvSource(value = {"3,5000", "4,50000", "5,1500000", "6,2000000000"})
-    void winning_price(final int matchNumber, final BigDecimal price) {
+    @CsvSource(value = {"3,false,5000", "4,false,50000", "5,false,1500000", "5,true,30000000", "6,false,2000000000"})
+    void winning_price(final int matchNumber, final boolean matchBonus, final BigDecimal price) {
 
-        assertThat(WinningPrize.from(matchNumber).getPrice()).isEqualTo(price);
+        assertThat(WinningPrize.from(matchNumber, matchBonus).getPrice()).isEqualTo(price);
     }
 
     @DisplayName("일치하는 번호가 2개 이하는 금액을 받지 못한다.")
     @ParameterizedTest
     @CsvSource(value = {"0,0,0", "1,0,0", "2,0,0"})
-    void error_winning_price(final int matchNumber, final int countOfMatch, final BigDecimal price) {
+    void error_winning_price(final int matchNumber, final int countOfMatch) {
 
-        final WinningPrize winningPrize = WinningPrize.from(matchNumber);
+        final WinningPrize winningPrize = WinningPrize.from(matchNumber, false);
 
         assertAll(
                 () -> assertThat(winningPrize.getCountOfMatch()).isEqualTo(countOfMatch),
