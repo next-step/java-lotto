29b37ab41a586e5d9aaea02e13c7c678764a52c7
Wordbe <seonghojin3@gmail.com>
2022-10-05 13:26:11 +0900

    refactor: 보너스볼과 등수에 따라 분기처리가 없도록 리팩토링 (bonusMatchRequired)

--- src/main/java/lotto/domain/enums/Rank.java
+++ src/main/java/lotto/domain/enums/Rank.java
@@ -2,27 +2,23 @@ package lotto.domain.enums;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.List;
-
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 
 public enum Rank {
-    FIRST(2_000_000_000L, 6, List.of(TRUE, FALSE)),
-    SECOND(30_000_000L, 5, List.of(TRUE)),
-    THIRD(1_500_000L, 5, List.of(FALSE)),
-    FOURTH(50_000L, 4, List.of(TRUE, FALSE)),
-    FIFTH(5_000L, 3, List.of(TRUE, FALSE)),
-    MISS(0L, 0, List.of(TRUE, FALSE));
+    FIRST(2_000_000_000L, 6, false),
+    SECOND(30_000_000L, 5, true),
+    THIRD(1_500_000L, 5, false),
+    FOURTH(50_000L, 4, false),
+    FIFTH(5_000L, 3, false),
+    MISS(0L, 0, false);
 
     private final BigDecimal winningAmount;
     private final Integer matchedCount;
-    private final List<Boolean> bonusMatchedStates;
+    private final boolean bonusMatchRequired;
 
-    Rank(Long winningAmount, Integer matchedCount, List<Boolean> bonusMatchedStates) {
+    Rank(Long winningAmount, Integer matchedCount, boolean bonusMatchRequired) {
         this.winningAmount = BigDecimal.valueOf(winningAmount);
         this.matchedCount = matchedCount;
-        this.bonusMatchedStates = bonusMatchedStates;
+        this.bonusMatchRequired = bonusMatchRequired;
     }
 
     public BigDecimal winningAmount() {
@@ -39,8 +35,17 @@ public enum Rank {
 
     public static Rank of(int matchedCount, boolean bonusMatched) {
         return Arrays.stream(values())
-                .filter(rank -> rank.matchedCount == matchedCount && rank.bonusMatchedStates.contains(bonusMatched))
+                .filter(rank -> rank.matchedCount == matchedCount)
+                .filter(rank -> bonusMatchedIfRequires(bonusMatched, rank))
                 .findFirst()
                 .orElse(MISS);
+    }
+
+    private static boolean bonusMatchedIfRequires(boolean bonusMatched, Rank rank) {
+        if (rank.bonusMatchRequired) {
+            return bonusMatched;
+        }
+
+        return true;
     }
 }
