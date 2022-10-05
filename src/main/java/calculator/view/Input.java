670b8b4beb0f011fc55a7ae93c4e37330167f0fb
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-10-05 14:47:20 +0900

    refactor(step1): 입력 화면 정규식 추가

--- src/main/java/calculator/view/Input.java
+++ src/main/java/calculator/view/Input.java
@@ -1,29 +1,48 @@
 package calculator.view;
 
-import util.NumberUtil;
-
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static calculator.validate.InputValidator.*;
 
 public class Input {
     private final static Scanner SCANNER = new Scanner(System.in);
     private final static String SEPARATOR = " ";
 
-    public List<String> inputExpression() {
+    private List<Integer> operandList;
+    private List<String> operatorList;
+
+    public void expression() {
         System.out.println("계산식을 입력해주세요.(숫자와 연산자 사이는 띄어쓰기(space)로 구분)");
         String expression = SCANNER.nextLine();
 
-        if (isValidExpression(expression)) {
-            return Arrays.asList(expression.split(SEPARATOR));
+        if (containSeparator(expression) && isValidOperand(expression) && isValidOperator(expression)) {
+            List<String> elementsOfExpression = Arrays.asList(expression.split(SEPARATOR));
+            parseOperand(elementsOfExpression);
+            parseOperator(elementsOfExpression);
         }
-        throw new InputMismatchException("입력된 계산식이 올바르지 않습니다.");
     }
 
-    private boolean isValidExpression(String expression) {
-        if (!expression.trim().contains(SEPARATOR)) {
-            return false;
-        }
-        return Stream.of(expression.split(SEPARATOR))
-                .allMatch(NumberUtil::isNumeric);
+    private void parseOperand(List<String> elementsOfExpression) {
+        operandList = IntStream.range(0, elementsOfExpression.size())
+                .filter(i -> i % 2 == 0)
+                .mapToObj(i -> Integer.parseInt(elementsOfExpression.get(i)))
+                .collect(Collectors.toList());
+    }
+
+    private void parseOperator(List<String> elementsOfExpression) {
+        operatorList = IntStream.range(0, elementsOfExpression.size())
+                .filter(i -> i % 2 == 1)
+                .mapToObj(i -> elementsOfExpression.get(i))
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getOperandList() {
+        return Collections.unmodifiableList(operandList);
+    }
+
+    public List<String> getOperatorList() {
+        return Collections.unmodifiableList(operatorList);
     }
 }
