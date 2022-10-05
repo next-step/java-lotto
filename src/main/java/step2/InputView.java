7dc6df39a1c965869034fa9bdc16fe05753282ec
celine.kk <celine.kk@kakaocorp.com>
2022-10-05 17:26:37 +0900

    feat. 14개의 랜덤 결과를 출력한다.

--- src/main/java/step2/InputView.java
+++ src/main/java/step2/InputView.java
@@ -1,5 +1,7 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -12,6 +14,12 @@ public class InputView {
     }
 
     public static void printLottoCount(int lottoNum) {
-        System.out.println(lottoNum+"개를 구매했습니다.");
+        System.out.println(lottoNum + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNum(ArrayList<List<Integer>> lottoArray) {
+        for (List<Integer> lotto : lottoArray) {
+            System.out.println(lotto.toString());
+        }
     }
 }
