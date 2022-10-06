d6eb38c094dbf572e8b9295ee4f6497584f32f83
swsong <ssw890720@gmail.com>
2022-10-06 20:51:42 +0900

    feat: 로또결과 도메인추가

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,40 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+/**
+ * Created by seungwoo.song on 2022-10-06
+ */
+public enum LottoResult {
+
+    FIRST(2000000000, 6),
+    SECOND(1500000, 5),
+    THIRD(50000, 4),
+    FORTH(5000, 3),
+    NONE(0,0);
+
+    public static final String 단위 = "원";
+
+    private final int money;
+    private final int matchCount; // TODO 적절한 단어고민
+
+    LottoResult(int money, int matchCount) {
+        this.money = money;
+        this.matchCount = matchCount;
+    }
+
+    public static LottoResult from(int inputMatchCount) {
+        return Arrays.stream(values())
+                .filter(value -> value.matchCount == inputMatchCount)
+                .findAny()
+                .orElse(NONE);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+}
---
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by seungwoo.song on 2022-10-06
+ */
+class LottoResultTest {
+    @Test
+    void 당첨정보() {
+        assertThat(LottoResult.FIRST.getMoney()).isEqualTo(2000000000);
+        assertThat(LottoResult.FIRST.getMatchCount()).isEqualTo(6);
+        assertThat(LottoResult.단위).isEqualTo("원");
+    }
+
+    @Test
+    void 생성() {
+        assertThat(LottoResult.from(3)).isEqualTo(LottoResult.FORTH);
+        assertThat(LottoResult.from(2)).isEqualTo(LottoResult.NONE);
+    }
+}
