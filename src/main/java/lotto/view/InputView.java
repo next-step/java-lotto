ed982e3eaba7a102678ebbd27bdb89f23037e080
OhSeungCheol <sc.oh131@gmail.com>
2022-10-07 02:48:01 +0900

    [M4] feat: 수동으로 구매할 로또 정보를 입력하는 기능 추가
    참조: https://edu.nextstep.camp/s/DVVUSLFe/ls/DwD1LGaT

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,6 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -16,17 +17,38 @@ public class InputView {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         String lastWeeksCollectNumbers = scanner.nextLine();
-        String[] split = lastWeeksCollectNumbers.split(", ");
-        return Arrays
-                .stream(split)
-                .mapToInt(Integer::parseInt)
-                .boxed()
-                .collect(Collectors.toList());
+        return getLottoNumbers(lastWeeksCollectNumbers);
     }
 
     public int inputBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
+    }
+
+    public int inputPassiveLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해주세요.");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public List<List<Integer>> inputPassiveLottoNumbers(int passiveLottoCount) {
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+        List<List<Integer>> numbersCollection = new ArrayList<>();
+        for (int index = 0; index < passiveLottoCount; index++) {
+            Scanner scanner = new Scanner(System.in);
+            String numbersString = scanner.nextLine();
+            numbersCollection.add(getLottoNumbers(numbersString));
+        }
+        return numbersCollection;
+    }
+
+    private List<Integer> getLottoNumbers(String numbersString) {
+        String[] split = numbersString.split(", ");
+        return Arrays
+                .stream(split)
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
