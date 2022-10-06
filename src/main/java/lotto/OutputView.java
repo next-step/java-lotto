fdd1c693ac1759edad86bba15627abe22d1ed67b
Cheeze <cheeze.ball@kakaocorp.com>
2022-10-06 11:10:42 +0900

    refact() : 당첨 금액 확인 로직 분리

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -3,17 +3,18 @@ package lotto;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class OutputView {
 
-    private static final BigDecimal PRICE_PER_LOTTO = BigDecimal.valueOf(1000);
-
     private final Calculator calculator;
+    private final PrizeService prizeService;
 
-    public OutputView(Calculator calculator) {this.calculator = calculator;}
+    public OutputView(Calculator calculator, PrizeService prizeService) {
+        this.calculator = calculator;
+        this.prizeService = prizeService;
+    }
 
     public void printPurchasedLotto(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개 구매했습니다.");
@@ -25,59 +26,23 @@ public class OutputView {
     public void statisticLotto(List<Lotto> lottos, List<Integer> winningNumber) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        Map<Integer, Integer> rankMap = findRank(lottos, winningNumber);
+        Map<Integer, Integer> rankMap = prizeService.confirmRank(lottos, winningNumber);
         printRank(rankMap);
         printYield(rankMap, lottos.size());
     }
 
-    private void printYield(Map<Integer, Integer> rankMap, int size) {
-        BigDecimal purchaseAmount = PRICE_PER_LOTTO.multiply(BigDecimal.valueOf(size));
-        BigDecimal prizeMoney = getPrizeMoney(rankMap);
+    private void printYield(Map<Integer, Integer> rankMap, int purchaseCount) {
+        BigDecimal purchaseAmount = calculator.calculatePurchaseAmount(purchaseCount);
+        BigDecimal prizeMoney = calculator.calculatePrizeMoney(rankMap);
         System.out.println(
                 "총 수익률은 " + String.format("%.3f", calculator.calculateYield(purchaseAmount, prizeMoney)) +
                 "입니다.");
     }
 
-    private BigDecimal getPrizeMoney(Map<Integer, Integer> rankMap) {
-        BigDecimal prizeMoney = BigDecimal.ZERO;
-
-        for (Prize value : Prize.values()) {
-            prizeMoney = prizeMoney.add(
-                    BigDecimal.valueOf(rankMap.get(value.getWinningCount())).multiply(value.getPrizeMoney()));
-        }
-
-        return prizeMoney;
-    }
-
-    private Map<Integer, Integer> findRank(List<Lotto> lottos, List<Integer> winningNumber) {
-        Map<Integer, Integer> rankMap = initRankMap();
-
-        for (Lotto lotto : lottos) {
-            int matchingCount = lotto.findMatchingCount(winningNumber);
-            addRankMap(rankMap, matchingCount);
-        }
-        return rankMap;
-    }
-
     private void printRank(Map<Integer, Integer> rankMap) {
         Arrays.stream(Prize.values()).sorted(Comparator.comparing(Prize::getWinningCount)).forEach(p -> {
-            System.out.println(p.getWinningCount() + "개 일치 (" + p.getWinningCount() + "원) - " +
+            System.out.println(p.getWinningCount() + "개 일치 (" + p.getPrizeMoney() + "원) - " +
                                rankMap.get(p.getWinningCount()) + "개");
         });
-    }
-
-    private Map<Integer, Integer> initRankMap() {
-        Map<Integer, Integer> rankMap = new HashMap<>();
-        for (Prize value : Prize.values()) {
-            rankMap.put(value.getWinningCount(), 0);
-        }
-        return rankMap;
-    }
-
-    private void addRankMap(Map<Integer, Integer> rankMap, int matchingCount) {
-        if (matchingCount < Prize.RANK_4TH.getWinningCount()) {
-            return;
-        }
-        rankMap.put(matchingCount, rankMap.get(matchingCount) + 1);
     }
 }
