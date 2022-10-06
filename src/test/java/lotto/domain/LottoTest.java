624138a2e00b1a7174eb5a1d4d912f5e0e8b78eb
swsong <ssw890720@gmail.com>
2022-10-06 20:52:35 +0900

    feat: 로또 도메인추가

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by seungwoo.song on 2022-10-06
+ */
+class LottoTest {
+
+    @Test
+    void 생성() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(List.of(1,2,3,4,5,6,7)));
+    }
+
+    @Test
+    void 결과() {
+        Lotto lotto = Lotto.of(List.of(1, 2, 3, 4, 5, 6));
+        LottoResult result = lotto.getResult(lotto);
+
+        assertThat(result).isEqualTo(LottoResult.FIRST);
+    }
+}
