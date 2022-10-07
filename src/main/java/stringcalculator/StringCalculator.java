c1a2eff127b0d505edd6db6586e32db7a80eea84
yunhwan <pyh8618@gmail.com>
2022-10-07 19:51:09 +0900

    feat : 문자열에서 Formula를 생성하는 StringCalculator 추가

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,35 @@
+package stringcalculator;
+
+import java.util.List;
+import stringcalculator.util.ConvertUtil;
+
+public class StringCalculator {
+
+    private static final int EVEN_NUMBER = 2;
+
+    private final Formula formula;
+
+    public StringCalculator(List<String> texts) {
+        formula = new Formula();
+        for (int i = 0; i < texts.size(); i++) {
+            addFormula(texts.get(i), formula, i);
+        }
+    }
+
+    public int calculate() {
+        return formula.calculate();
+    }
+
+    private void addFormula(String text, Formula formula, int i) {
+        if (!isOddNumber(i)) {
+            formula.addNumber(ConvertUtil.toInt(text));
+        }
+        if (isOddNumber(i)) {
+            formula.addOperator(text);
+        }
+    }
+
+    private boolean isOddNumber(int i) {
+        return i % EVEN_NUMBER == 1;
+    }
+}
