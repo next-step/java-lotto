e487bceaf4477834a746e119222928868834f54b
moonstal1506 <moonstal1506@gmail.com>
2022-10-07 14:23:53 +0900

    refactor: 입력 값 테스트 @NullAndEmptySource로 변경

--- src/test/java/calculator/model/ExpressionTest.java
+++ src/test/java/calculator/model/ExpressionTest.java
@@ -2,26 +2,20 @@ package calculator.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class ExpressionTest {
 
     @DisplayName("입력 값이 공백이면 에러를 반환한다.")
-    @Test
-    void validateExpressionIsEmpty() {
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validateExpressionIsBlank(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> {
-                    new Expression("");
-                });
-    }
-
-    @DisplayName("입력 값이 null이면 에러를 반환한다.")
-    @Test
-    void validateExpressionIsNull() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> {
-                    new Expression(null);
+                    new Expression(input);
                 });
     }
 
