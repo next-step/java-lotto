07f732360bde219b0e09c681fcc15709eb0a2df5
gumsu <chavi9@naver.com>
2022-10-07 14:39:08 +0900

    refactor: 지난주 당첨 번호 리턴 타입 변경

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,8 +1,12 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
+
     private static final String TOTAL_PRICE = "구매금액을 입력해 주세요.";
     private static final String LAST_LOTTO_RESULT = "지난주 당첨 번호를 입력해 주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -12,8 +16,11 @@ public class InputView {
         return SCANNER.nextInt();
     }
 
-    public static String inputLottoNumberLastResult() {
+    public static List<LottoNumber> inputLottoNumberLastResult() {
         System.out.println(LAST_LOTTO_RESULT);
-        return SCANNER.next();
+        return Arrays.stream(SCANNER.next().split(","))
+            .mapToInt(Integer::parseInt)
+            .mapToObj(LottoNumber::from)
+            .collect(Collectors.toList());
     }
 }
--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,9 +1,12 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoApplication {
 
     public static void main(String[] args) {
         int totalPrice = InputView.inputTotalPrice();
-        String lastResultNumber = InputView.inputLottoNumberLastResult();
+        System.out.println(totalPrice/1000 + "개를 구매했습니다.");
+        List<LottoNumber> lastResultNumber = InputView.inputLottoNumberLastResult();
     }
 }
