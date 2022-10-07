f74f2f985d47aa5828e8f57890073725d57a03a9
gumsu <chavi9@naver.com>
2022-10-07 14:31:43 +0900

    feat: 금액 및 지난주 당첨 번호 입력 기능 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -3,6 +3,7 @@ package lotto;
 public class LottoApplication {
 
     public static void main(String[] args) {
-
+        int totalPrice = InputView.inputTotalPrice();
+        String lastResultNumber = InputView.inputLottoNumberLastResult();
     }
 }
