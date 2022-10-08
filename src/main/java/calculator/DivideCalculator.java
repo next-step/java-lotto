b765e173c72ddf4022d31698f41749279dcebde5
jimbaemon <jimbae@kakao.com>
2022-10-08 19:21:48 +0900

    feat: 나눗셈시 결과 정수가 아닌 경우 에러 발생 구현.

--- src/main/java/calculator/DivideCalculator.java
+++ src/main/java/calculator/DivideCalculator.java
@@ -3,6 +3,9 @@ package calculator;
 public class DivideCalculator implements Calculator {
     @Override
     public int calculate(int a, int b) {
-        return a/b;
+        if (a % b != 0) {
+            throw new IllegalArgumentException("나눗셈의 결과가 정수가 아닙니다.");
+        }
+        return a / b;
     }
 }
