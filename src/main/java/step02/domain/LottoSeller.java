8e77f274b4ccdc5134bc47dcc691660650089ff6
jinukix <jinukix@styleshare.kr>
2022-10-08 16:49:00 +0900

    feat: LottoSeller 수동 로또 구매 기능 추가

--- src/main/java/step02/domain/LottoSeller.java
+++ src/main/java/step02/domain/LottoSeller.java
@@ -1,7 +1,6 @@
 package step02.domain;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.List;
 
 public class LottoSeller {
 
@@ -10,17 +9,28 @@ public class LottoSeller {
     private LottoSeller() {
     }
 
-    public static LottoList sell(int price) {
-        return new LottoList(
-            IntStream.range(0, getLottoCount(price))
-                .mapToObj(i -> Lotto.create())
-                .collect(Collectors.toList())
-        );
+    public static LottoList sell(int price, List<String> manualLottoNumbers) {
+        validateManualLottoNumbers(manualLottoNumbers);
+        int manualLottoCount = manualLottoNumbers.size();
+        if (onlyManualLotto(price, manualLottoCount)) {
+            return LottoMaker.makeManualLotto(manualLottoNumbers);
+        }
+
+        return LottoMaker.makeLotto(getAutoLottoCount(price, manualLottoCount), manualLottoNumbers);
     }
 
-    private static int getLottoCount(int price) {
+    private static boolean onlyManualLotto(int price, int manualLottoCount) {
+        return getAutoLottoCount(price, manualLottoCount) == 0;
+    }
+
+    private static int getAutoLottoCount(int price, int manualLottoCount) {
         validatePrice(price);
-        return price / LOTTO_PRICE;
+        int autoLottoCount = (price / LOTTO_PRICE) - manualLottoCount;
+        if (autoLottoCount < 0) {
+            throw new IllegalArgumentException("자동 로또 발급 개수는 0 미만일 수 없습니다.");
+        }
+
+        return autoLottoCount;
     }
 
     private static void validatePrice(int price) {
@@ -37,6 +47,12 @@ public class LottoSeller {
     private static void validatePriceRange(int price) {
         if (price % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("금액은 " + LOTTO_PRICE + " 단위의 금액을 입력해야합니다. | 입력된 금액: " + price);
+        }
+    }
+
+    private static void validateManualLottoNumbers(List<String> manualLottoNumbers) {
+        if (manualLottoNumbers == null || manualLottoNumbers.isEmpty()) {
+            throw new IllegalArgumentException("수동 로또 넘버는 비어있을 수 없습니다.");
         }
     }
 }
