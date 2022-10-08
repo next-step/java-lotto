874b8bbd7a63cfc940606780c1167e97da7d2e6c
jhwlim <jhwlim25@gmail.com>
2022-10-08 14:06:03 +0900

    feat : 지난주 당첨 번호 입력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,18 +1,32 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.Money;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String DELIMITER = ", ";
 
     private InputView() {}
 
     public static Money inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return new Money(SCANNER.nextInt());
+        return new Money(Integer.parseInt(SCANNER.nextLine()));
+    }
+
+    public static Lotto inputWinningLotto() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        List<LottoNumber> winningLottoNumbers = Arrays.stream(SCANNER.nextLine().split(DELIMITER))
+                .map(numberString -> new LottoNumber(Integer.parseInt(numberString)))
+                .collect(Collectors.toList());
+        return new Lotto(winningLottoNumbers);
     }
 
 }
