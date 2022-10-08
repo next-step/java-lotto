cdfed99673363d2fc300af257280ee7ec96d8af0
모진혁 <gkgk246@naver.com>
2022-10-08 22:46:53 +0900

    refacotr : 기능에따른 메소드 분리와 클래스명 변경

--- src/main/java/Lotto/Controller/Store.java
+++ src/main/java/Lotto/Controller/Store.java
@@ -3,12 +3,9 @@ package Lotto.Controller;
 import Lotto.model.*;
 import Lotto.view.LottoInput;
 import Lotto.view.LottoOutput;
-import calculator.view.Output;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Store {
 
@@ -16,19 +13,32 @@ public class Store {
     private static final int LOTTO_PRICE = 1000;
 
     public void buyLotto() {
-        int price = getPrice();
-        int lottoCount = price / LOTTO_PRICE;
-        Lottos lottos = createLottos(lottoCount);
+        LottoNumberCreateMachine lottoNumberCreateMachine = new LottoNumberCreateMachine();
+        int lottoCount = getLottoCount();
+        Lottos lottos = lottoNumberCreateMachine.createLottos(lottoCount);
         LottoOutput.noticeBuyLotto(lottos);
-        Lotto luckyNumbers = new Lotto(getLuckyNumbers());
+        Lotto luckyNumbers = lottoNumberCreateMachine.getLuckyNumbers(getLuckyNumber());
         Map<Rank, Integer> countingRank = lottos.summaryLottoResult(luckyNumbers);
-        double profit = getProfit(countingRank, price);
+        double profit = getProfit(countingRank, lottoCount);
         LottoOutput.noticeResult(countingRank, profit);
     }
 
-    private double getProfit(Map<Rank, Integer> countingRank, int price) {
+    private int getLottoCount() {
+        int price = LottoInput.getPrice();
+        if (price % LOTTO_PRICE != 0) {
+            new IllegalArgumentException(PRICE_ERROR_MESSAGE);
+        }
+        return price / LOTTO_PRICE;
+    }
+
+    private List<String> getLuckyNumber() {
+        return LottoInput.getLuckyNumbers();
+    }
+
+    private double getProfit(Map<Rank, Integer> countingRank, int lottoCount) {
         double reward = 0;
         int count = 0;
+        double price = lottoCount * LOTTO_PRICE;
         for (Rank rank : countingRank.keySet()) {
             count += countingRank.get(rank);
             reward += rank.getReward() * countingRank.get(rank);
@@ -36,33 +46,6 @@ public class Store {
         if (count == 0) {
             return 0;
         }
-        return reward / (double) price;
-    }
-
-    private List<LottoNumber> getLuckyNumbers() {
-        List<String> numbers = LottoInput.getLuckyNumbers();
-        return numbers.stream()
-                .map(it -> new LottoNumber(Integer.parseInt(it)))
-                .collect(Collectors.toList());
-    }
-
-
-    private int getPrice() {
-        int price = LottoInput.getPrice();
-        if (price % LOTTO_PRICE != 0) {
-            new IllegalArgumentException(PRICE_ERROR_MESSAGE);
-        }
-        return price;
-    }
-
-    private Lottos createLottos(int lottoCount) {
-        LottoNumberPicker lottoNumberPicker = new LottoNumberPicker();
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            Lotto lotto = new Lotto(lottoNumberPicker.pick());
-            lottos.add(lotto);
-        }
-        Lottos result = new Lottos(lottos);
-        return result;
+        return reward / price;
     }
 }
--- src/main/java/Lotto/model/LottoNumberCreateMachine.java
+++ src/main/java/Lotto/model/LottoNumberCreateMachine.java
@@ -7,18 +7,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class LottoNumberPicker {
+public class LottoNumberCreateMachine {
     private static final int PROPER_LOTTONUMBER_COUNT = 6;
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
 
+    public Lottos createLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            Lotto lotto = new Lotto(this.pick());
+            lottos.add(lotto);
+        }
+        Lottos result = new Lottos(lottos);
+        return result;
+    }
 
-    public List<LottoNumber> pick() {
+    public Lotto getLuckyNumbers(List<String> numbers) {
+        return new Lotto(numbers.stream()
+                .map(it -> new LottoNumber(Integer.parseInt(it)))
+                .collect(Collectors.toList()));
+    }
+
+    private List<LottoNumber> pick() {
         final List<LottoNumber> allLottoNumbers = IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER).mapToObj(LottoNumber::new).collect(Collectors.toList());
         Collections.shuffle(allLottoNumbers);
         List<LottoNumber> lotto = new ArrayList<>(allLottoNumbers.subList(0, PROPER_LOTTONUMBER_COUNT));
         Collections.sort(lotto);
         return lotto;
     }
-
 }
