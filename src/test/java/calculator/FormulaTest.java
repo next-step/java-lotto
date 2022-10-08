aa7abe23c0be832c2eabba6de005191ba6dc72b6
jimbaemon <jimbae@kakao.com>
2022-10-08 19:31:01 +0900

    test: 빈스트링, null 값 입력시 에러 발생 테스트.

--- src/test/java/calculator/FormulaTest.java
+++ src/test/java/calculator/FormulaTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class FormulaTest {
 
@@ -18,5 +20,21 @@ public class FormulaTest {
         Formula formula = new Formula("3 + 4 * 2 / 7");
 
         Assertions.assertThat(formula.calculate()).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 입력값이__빈스트링일_경우_에러발생(String blank){
+        Assertions.assertThatThrownBy(() -> new Formula(blank))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("빈 문자열 사용 금지");
+    }
+
+    @Test
+    void 입력값이_NULL일_경우_에러발생(){
+        String nullString = null;
+        Assertions.assertThatThrownBy(() -> new Formula(nullString))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("null 사용 금지");
     }
 }
