0548b896c6568601fba2377d9b2b0e0936c254f5
jimbaemon <jimbae@kakao.com>
2022-10-08 18:36:35 +0900

    test: 계산기를 추상화 하고 계산기 별로 분리 테스트.

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,32 +9,32 @@ public class CalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"1:2:3", "2:3:5", "91:9:100"}, delimiter = ':')
     void 덧셈을_한다(int a, int b, int answer){
-        Calculator calculator = new Calculator();
+        Calculator calculator = new AddCalculator();
 
-        Assertions.assertThat(calculator.add(a, b)).isEqualTo(answer);
+        Assertions.assertThat(calculator.calculate(a, b)).isEqualTo(answer);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"3:2:1", "5:3:2", "91:9:82"}, delimiter = ':')
     void 뺄샘을_한다(int a, int b, int answer){
-        Calculator calculator = new Calculator();
+        Calculator calculator = new SubtractCalculator();
 
-        Assertions.assertThat(calculator.subtract(a, b)).isEqualTo(answer);
+        Assertions.assertThat(calculator.calculate(a, b)).isEqualTo(answer);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"9:2:18", "2:5:10", "6:3:18"}, delimiter = ':')
     void 곱셈을_한다(int a, int b, int answer){
-        Calculator calculator = new Calculator();
+        Calculator calculator = new MultiplyCalculator();
 
-        Assertions.assertThat(calculator.multiply(a, b)).isEqualTo(answer);
+        Assertions.assertThat(calculator.calculate(a, b)).isEqualTo(answer);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"18:2:9", "10:5:2", "6:3:2"}, delimiter = ':')
     void 나눗셈을_한다(int a, int b, int answer){
-        Calculator calculator = new Calculator();
+        Calculator calculator = new DivideCalculator();
 
-        Assertions.assertThat(calculator.divide(a, b)).isEqualTo(answer);
+        Assertions.assertThat(calculator.calculate(a, b)).isEqualTo(answer);
     }
 }
