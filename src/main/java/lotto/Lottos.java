cbf349fb2909fbad5b1fec6f11fb4997bca6b336
o3ozzvb <ou1112@naver.com>
2022-10-08 16:25:15 +0900

    feat : 수익률 구하기

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -12,31 +12,39 @@ public class Lottos {
     public static final int LOTTO_PRICE = 1000;
 
     private List<Lotto> lottoList;
+    private Map<Integer, Integer> result;
+//    private double profitRate;
 
     public Lottos(int amount) {
-        int n = amount/ LOTTO_PRICE;
         this.lottoList = new ArrayList<>();
-        for (int i=0; i<n; i++) {
+        this.result = new HashMap<>();
+
+        int n = amount / LOTTO_PRICE;
+        for (int i = 0; i < n; i++) {
             this.lottoList.add(new Lotto());
         }
     }
 
     public Lottos(List<Lotto> lottoList) {
         this.lottoList = lottoList;
+        this.result = new HashMap<>();
     }
 
     public int lottosSize() {
         return this.lottoList.size();
     }
 
-
     public Map<Integer, Integer> getResult(Lotto winning) {
-        Map<Integer, Integer> result = new HashMap<>();
         for (Lotto lotto : lottoList) {
             int sameNumbers = lotto.getSameNumbers(winning);
             int count = result.getOrDefault(sameNumbers, 0);
-            result.put(sameNumbers, count+1);
+            result.put(sameNumbers, count + 1);
         }
         return result;
     }
+
+    public double getProfitRate() {
+        return (double) result.get(Lotto.MAX_MATCH_NUMBER) / lottosSize();
+    }
+
 }
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottosTest {
 
     @Test
-    public void 로또_리스트_개수_n() {
+    public void 구매금액에_대한_로또_리스트_생성() {
         Lottos lottos = new Lottos(14000);
         assertThat(lottos.lottosSize()).isEqualTo(14);
     }
@@ -34,5 +34,26 @@ public class LottosTest {
         assertThat(result.get(4)).isEqualTo(1);
         assertThat(result.get(5)).isEqualTo(2);
         assertThat(result.get(6)).isEqualTo(1);
+    }
+
+    @Test
+    public void 수익률_구하기() {
+        List<Lotto> lottoList = new ArrayList<>();
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 10)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 20)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 20)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 4, 15, 20)));
+        lottoList.add(new Lotto(Arrays.asList(1, 2, 3, 14, 15, 20)));
+
+        Lottos lottos = new Lottos(lottoList);
+        Lotto winning = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        lottos.getResult(winning);
+
+        assertThat(lottos.getProfitRate()).isEqualTo(0.4);
     }
 }
