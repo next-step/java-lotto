891280898b59042c026e8cd79801d54ceec64e5e
Yunsik-Choi <choiys0311@gmail.com>
2022-10-08 22:13:04 +0900

    feat : Operator valid

---
+++ src/test/java/stringcalculator/OperatorTest.java
@@ -0,0 +1,25 @@
+package stringcalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OperatorTest {
+
+    @Test
+    void create() {
+        Operator operator = new Operator("+");
+
+        assertThat(operator).isEqualTo(new Operator("+"));
+    }
+
+    @ParameterizedTest(name = "Operator는 +,-,*,/ 이외의 문자로 생성할 경우 IllegalArgumentException 에러를 반환한다.")
+    @ValueSource(strings = {"0","&","%","#","(","="})
+    void valid(String text) {
+        assertThatThrownBy(() -> new Operator(text)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
