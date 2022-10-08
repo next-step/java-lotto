fac79dd769a9b2975d9a75452ec0f57d46144467
jimbaemon <jimbae@kakao.com>
2022-10-08 18:08:45 +0900

    test: 문자열 계산식을 문자 배열형 계산식으로 변환 테스트.

---
+++ src/test/java/calculator/FormulaTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FormulaTest {
+
+    @Test
+    void 계산식_문자열을_문자배열로_변경한다() {
+        Formula formula = new Formula("3 + 4 * 2 / 7");
+
+        Assertions.assertThat(formula)
+            .isEqualTo(new Formula(new String[]{"3", "+", "4", "*", "2", "/", "7"}));
+    }
+}
