a256536a564937be1d9408a3e34b428a53fac0ba
moonstal1506 <moonstal1506@gmail.com>
2022-10-09 15:26:46 +0900

    fix: 1000원 미만일 경우 예외 제거

--- src/test/java/lotto/model/MoneyTest.java
+++ src/test/java/lotto/model/MoneyTest.java
@@ -24,15 +24,6 @@ class MoneyTest {
                 }).withMessageMatching("금액은 음수일 수 없습니다.");
     }
 
-    @DisplayName("금액이 부족할 경우 에러를 반환한다.")
-    @Test
-    void validateMoneyNotEnough() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> {
-                    new Money(500);
-                }).withMessageMatching("금액이 부족합니다.");
-    }
-
     @DisplayName("1000원 단위가 아닐 경우 에러를 반환한다.")
     @Test
     void validateMoneyUnit() {
