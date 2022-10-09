f0b62b891efa9e3b3a16f1467e02c003746c216f
wugatama <dnr7410@naver.com>
2022-10-09 15:13:24 +0900

    test: SeparatorTest

--- src/test/java/step1/SeparatorTest.java
+++ src/test/java/step1/SeparatorTest.java
@@ -2,14 +2,16 @@ package step1;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class StringCalculatorTest {
+public class SeparatorTest {
 	@Test
 	@DisplayName("식 받아서 숫자/기호 분리")
 	void separateNumbers() {
-		Calculator calculator = new Calculator("1 + 2 + 3");
-		assertThat(calculator.getFormula()).isEqualTo(new String[] {"1", "+", "2", "+", "3"});
+		Separator separator = new Separator("1 + 2 + 3");
+		assertThat(separator.getFormula()).isEqualTo(Arrays.asList("1", "+", "2", "+", "3"));
 	}
 }
