fbceea3601c07429707bca0573f5bebbff29c95c
nooose <hd15807@gmail.com>
2022-10-09 15:44:50 +0900

    refactor: 리스트를 inline으로 처리

--- src/main/java/lotto/view/LottoResult.java
+++ src/main/java/lotto/view/LottoResult.java
@@ -19,13 +19,10 @@ public class LottoResult {
     }
 
     public List<String> lottoInfos() {
-        List<String> result = new ArrayList<>();
-
-        lottos.values().forEach(lotto -> {
-            result.add(createLottoInfo(lotto));
-        });
-
-        return result;
+        return lottos.values()
+                .stream()
+                .map(this::createLottoInfo)
+                .collect(Collectors.toList());
     }
 
     private String createLottoInfo(Lotto lotto) {
