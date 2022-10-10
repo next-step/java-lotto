2ce3cdcf83c8b68f15949828f486a48774727ff8
Yunsik-Choi <choiys0311@gmail.com>
2022-10-11 01:11:54 +0900

    refactor : Lotto use LottoNumberList

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,16 +1,10 @@
 package lotto.domain;
 
-import java.util.List;
-
 public class Lotto {
 
-    private final List<LottoNumber> lottoNumbers;
-    private static final String LOTTO_SIZE_EXCEPTION = "로또번호는 6개여야 합니다.";
+    private final LottoNumberList lottoNumbers;
 
-    public Lotto(List<LottoNumber> lottoNumbers) {
-        if (lottoNumbers.size() != 6) {
-            throw new IllegalArgumentException(LOTTO_SIZE_EXCEPTION);
-        }
-        this.lottoNumbers = lottoNumbers;
+    public Lotto(LottoNumberList lottoNumberList) {
+        this.lottoNumbers = lottoNumberList;
     }
 }
