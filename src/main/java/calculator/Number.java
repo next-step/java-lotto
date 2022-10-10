ac1709f9e25e874d5e099cd2737cdc18fd7f13b8
bada.kang <kangbada0728@gmail.com>
2022-10-10 11:05:32 +0900

    feat: 숫자가 아닐경우 예외 던지도록 변경

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -12,6 +12,9 @@ public class Number {
         if (number.equals("")) {
             throw new IllegalArgumentException("숫자가 빈값이면 안됩니다.");
         }
+        if (!number.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("숫자로 변환할 수 없습니다.");
+        }
         this.number = Integer.parseInt(number);
     }
 
