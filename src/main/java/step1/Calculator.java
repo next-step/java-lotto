e3dba2ebd4b89644fb1d0128b7bfd6b474fcf475
Greg.Study <greg0917@naver.com>
2022-10-11 04:54:21 +0900

    단순기능구현

---
+++ src/main/java/step1/Calculator.java
@@ -0,0 +1,20 @@
+package step1;
+
+public class Calculator {
+
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
