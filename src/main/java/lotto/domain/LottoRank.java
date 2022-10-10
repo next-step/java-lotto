9e24c4611f4f960349f86124ab4a0fce3526c6f9
yunhwan <pyh8618@gmail.com>
2022-10-11 01:49:43 +0900

    refactor : 출력순서대로 변경, '_' 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THRID(4, 50000),
-    FOURTH(3, 5000),
     NONE(0, 0),
+    FOURTH(3, 5_000),
+    THRID(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000),
     ;
 
     private final int correctNum;
