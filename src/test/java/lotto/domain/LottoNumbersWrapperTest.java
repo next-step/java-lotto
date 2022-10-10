060f92315de736779868d676b60e61cfd6dc33b8
OhSeungCheol <sc.oh131@gmail.com>
2022-10-10 20:43:35 +0900

    [M4] test: getMatchCount, isMatchToBonusNumber 함수 테스트 추사
    참조: https://github.com/next-step/java-lotto/pull/2561

--- src/test/java/lotto/domain/LottoNumbersWrapperTest.java
+++ src/test/java/lotto/domain/LottoNumbersWrapperTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,24 +10,38 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumbersWrapperTest {
+    private static LottoNumbersWrapper testLottoNumbersWrapper;
+    private static final int NOT_LOTTO_NUMBER = -1;
 
-    @Test
-    @DisplayName("로또 번호 일치 개수 계산 기능 테스트")
-    public void lotteMatchCountTest() {
-        LottoNumbersWrapper testLottoNumbersWrapper = getTestLottoNumbersWrapper();
-
-        List<Integer> customCollectNumberList = new ArrayList<>();
-        for (int index = 0; index < Config.LOTTO_NUMBER_COUNT; index++) {
-            customCollectNumberList.add(index + 1);
-            assertThat(testLottoNumbersWrapper.getMatchCount(new LottoNumbersWrapper(customCollectNumberList))).isEqualTo(index + 1);
-        }
-    }
-
-    private LottoNumbersWrapper getTestLottoNumbersWrapper() {
+    @BeforeAll
+    static void getTestLottoNumbersWrapper() {
         List<Integer> lottoNumbers = new ArrayList<>();
         for (int index = 0; index < Config.LOTTO_NUMBER_COUNT; index++) {
             lottoNumbers.add(index + 1);
         }
-        return new LottoNumbersWrapper(lottoNumbers);
+        testLottoNumbersWrapper = new LottoNumbersWrapper(lottoNumbers);
     }
+
+    @Test
+    @DisplayName("로또 번호 일치 개수 계산 기능 테스트")
+    public void getMatchCountTest() {
+        List<Integer> numbers = testLottoNumbersWrapper.getNumbers();
+        List<Integer> customCollectNumberList = new ArrayList<>();
+        for (int index = 0; index < Config.LOTTO_NUMBER_COUNT; index++) {
+            customCollectNumberList.add(numbers.get(index));
+            assertThat(testLottoNumbersWrapper.getMatchCount(new LottoNumbersWrapper(customCollectNumberList))).isEqualTo(index + 1);
+        }
+    }
+
+    @Test
+    @DisplayName("보너스 숫자 일치 판단 기능 테스트")
+    public void isMatchToBonusNumberTest() {
+        int collectBonusNumber = testLottoNumbersWrapper.getNumbers().get(0);
+        assertThat(testLottoNumbersWrapper.isMatchToBonusNumber(collectBonusNumber)).isTrue();
+        assertThat(testLottoNumbersWrapper.isMatchToBonusNumber(NOT_LOTTO_NUMBER)).isFalse();
+    }
+
+
+
+
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,31 +1,59 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
+    private static Lotto lotto;
+    private static final int NOT_LOTTO_NUMBER = -1;
+
+    @BeforeAll
+    static void getTestLottoNumbersWrapper() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        for (int index = 0; index < Config.LOTTO_NUMBER_COUNT; index++) {
+            lottoNumbers.add(index + 1);
+        }
+        lotto = new Lotto(new LottoNumbersWrapper(lottoNumbers));
+    }
+
     @Test
     @DisplayName("로또 번호 개수 테스트")
     public void lotteNumbersSizeTest() {
-        Lotto lotto = new Lotto(NumberGenerator.pickLottoNumber());
-        List<Integer> numbers = lotto.getNumbers();
-
-        assertThat(numbers).hasSize(Config.LOTTO_NUMBER_COUNT);
+        assertThat(lotto.getNumbers()).hasSize(Config.LOTTO_NUMBER_COUNT);
     }
 
     @Test
     @DisplayName("[랜덤 요소 존재] 로또 번호 범위 테스트")
     public void lotteNumbersRangeTest() {
-        Lotto lotto = new Lotto(NumberGenerator.pickLottoNumber());
-        List<Integer> numbers = lotto.getNumbers();
-
-        for (int number : numbers) {
+        for (int number : lotto.getNumbers()) {
             assertThat(number).isBetween(Config.LOTTO_NUMBER_RANGE_TO_MIN, Config.LOTTO_NUMBER_RANGE_TO_MAX);
         }
     }
+
+    @Test
+    @DisplayName("로또 번호 일치 개수 계산 기능 테스트")
+    public void getMatchCountTest() {
+        List<Integer> numbers = lotto.getNumbers();
+        List<Integer> customCollectNumberList = new ArrayList<>();
+        for (int index = 0; index < Config.LOTTO_NUMBER_COUNT; index++) {
+            customCollectNumberList.add(numbers.get(index));
+            assertThat(lotto.getMatchCount(new LottoNumbersWrapper(customCollectNumberList))).isEqualTo(index + 1);
+        }
+    }
+
+    @Test
+    @DisplayName("보너스 숫자 일치 판단 기능 테스트")
+    public void isMatchToBonusNumberTest() {
+        int collectBonusNumber = lotto.getNumbers().get(0);
+        assertThat(lotto.isMatchToBonusNumber(collectBonusNumber)).isTrue();
+        assertThat(lotto.isMatchToBonusNumber(NOT_LOTTO_NUMBER)).isFalse();
+    }
+
 
 }
