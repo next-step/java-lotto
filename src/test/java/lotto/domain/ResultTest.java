ee8238855a4f4e90c193b96cff14b778c3f7082c
OhSeungCheol <sc.oh131@gmail.com>
2022-10-10 21:32:21 +0900

    [M4] test: 테스트 케이스 코드 정리
    참조: https://github.com/next-step/java-lotto/pull/2561

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -1,38 +1,38 @@
 package lotto.domain;
 
 import lotto.enumerate.Rank;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ResultTest {
-    private final int loseNumber = -1;
-    private final int bonusNumber = 0;
+    private static final int NO_MATCH_NUMBER = -1;
+    private static final int BONUS_NUMBER = 0;
+    private static LottoNumbersWrapper collectNumbers = new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6));
+    private static LottoWrapper lottoWrapper;
+
+    @BeforeAll
+    static void setLottoWrapper() {
+        Lotto lotto1 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        Lotto lotto2 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, BONUS_NUMBER)));
+        Lotto lotto3 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, NO_MATCH_NUMBER)));
+        Lotto lotto4 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, NO_MATCH_NUMBER, NO_MATCH_NUMBER)));
+        Lotto lotto5 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, NO_MATCH_NUMBER, NO_MATCH_NUMBER, NO_MATCH_NUMBER)));
+        lottoWrapper = new LottoWrapper(Arrays.asList(lotto1, lotto2, lotto3, lotto4, lotto5));
+    }
 
     @Test
     @DisplayName("로또 당첨 결과가 제대로 저장되는지 테스트")
     void saveResultTest() {
-        LottoNumbersWrapper collectNumberList = new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6));
-        List<Lotto> lottos = getTestLotts();
-
-        Result result = new Result(new LottoWrapper(lottos), collectNumberList, loseNumber);
+        Result result = new Result(lottoWrapper, collectNumbers, BONUS_NUMBER);
 
         for (Rank rank : Rank.values()) {
             assertThat(result.getWinCount(rank)).isEqualTo(1);
         }
-    }
-
-    private List<Lotto> getTestLotts() {
-        Lotto lotto1 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        Lotto lotto2 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, bonusNumber)));
-        Lotto lotto3 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, loseNumber)));
-        Lotto lotto4 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, loseNumber, loseNumber)));
-        Lotto lotto5 = new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, loseNumber, loseNumber, loseNumber)));
-        return Arrays.asList(lotto1, lotto2, lotto3, lotto4, lotto5);
     }
 
 }
