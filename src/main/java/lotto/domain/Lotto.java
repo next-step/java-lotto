e38ef3b6690829f8405c1d5737a959ea27127823
Yunsik-Choi <choiys0311@gmail.com>
2022-10-11 01:00:01 +0900

    feat : Lotto size valid

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,8 +5,12 @@ import java.util.List;
 public class Lotto {
 
     private final List<LottoNumber> lottoNumbers;
+    private static final String LOTTO_SIZE_EXCEPTION = "로또번호는 6개여야 합니다.";
 
     public Lotto(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_SIZE_EXCEPTION);
+        }
         this.lottoNumbers = lottoNumbers;
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,5 +1,39 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class LottoTest {
 
+    @DisplayName("로또번호가 6개가 아니면 에러를 반환한다.")
+    @Test
+    void valid_lotto_size() {
+        List<LottoNumber> lottoSizeUnder = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5)
+        );
+        List<LottoNumber> lottoSizeOver = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6),
+                new LottoNumber(7)
+        );
+
+        Assertions.assertAll(
+                () -> assertThatThrownBy(() -> new Lotto(lottoSizeUnder)).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new Lotto(lottoSizeOver)).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 }
