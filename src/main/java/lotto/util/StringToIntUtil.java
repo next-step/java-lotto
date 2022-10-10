10e5eaa9ebf83e7d93bef169999b7f20b8c2bdf1
yunhwan <pyh8618@gmail.com>
2022-10-11 01:47:12 +0900

    refactor : 오류가 생기기전에 예외 던지기

--- src/main/java/lotto/util/StringToIntUtil.java
+++ src/main/java/lotto/util/StringToIntUtil.java
@@ -1,12 +1,19 @@
 package lotto.util;
 
+import java.util.regex.Pattern;
+
 public class StringToIntUtil {
 
+    private static final String INTEGER_REGEX = "-?\\d+";
+
     public static int apply(String str) {
-        try {
+        if (canInteger(str)) {
             return Integer.parseInt(str);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("정수로 변환할 수 없는 문자열입니다.");
         }
+        throw new IllegalArgumentException("정수로 변환할 수 없는 문자열입니다.");
+    }
+
+    private static boolean canInteger(String str) {
+        return str != null && str.matches(INTEGER_REGEX);
     }
 }
