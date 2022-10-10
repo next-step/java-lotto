77f5b67da2f2f6be3b86adc772d9fffdff906c5f
Yunsik-Choi <choiys0311@gmail.com>
2022-10-11 01:07:45 +0900

    feat : LottoNumberList size valid

--- src/main/java/lotto/domain/LottoNumberList.java
+++ src/main/java/lotto/domain/LottoNumberList.java
@@ -1,12 +1,30 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoNumberList {
 
     private final List<LottoNumber> lottoNumberList;
+    private static final String LOTTO_SIZE_EXCEPTION = "로또 번호는 6개여야 합니다.";
 
     public LottoNumberList(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_SIZE_EXCEPTION);
+        }
         this.lottoNumberList = lottoNumbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumberList that = (LottoNumberList) o;
+        return Objects.equals(lottoNumberList, that.lottoNumberList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumberList);
     }
 }
--- src/test/java/lotto/domain/LottoNumberListTest.java
+++ src/test/java/lotto/domain/LottoNumberListTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,7 +18,8 @@ public class LottoNumberListTest {
                         new LottoNumber(2),
                         new LottoNumber(3),
                         new LottoNumber(4),
-                        new LottoNumber(5)
+                        new LottoNumber(5),
+                        new LottoNumber(6)
                 )
         );
 
@@ -26,8 +29,34 @@ public class LottoNumberListTest {
                         new LottoNumber(2),
                         new LottoNumber(3),
                         new LottoNumber(4),
-                        new LottoNumber(5)
+                        new LottoNumber(5),
+                        new LottoNumber(6)
                 )
         ));
+    }
+
+    @Test
+    void valid_size() {
+        List<LottoNumber> lottoSizeUnder = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5)
+        );
+        List<LottoNumber> lottoSizeOver = Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6),
+                new LottoNumber(7)
+        );
+
+        Assertions.assertAll(
+                () -> assertThatThrownBy(() -> new LottoNumberList(lottoSizeUnder)).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new LottoNumberList(lottoSizeOver)).isInstanceOf(IllegalArgumentException.class)
+        );
     }
 }
