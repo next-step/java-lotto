bb0591f28af90bb0443b524fd5e9458c64887e0c
OhSeungCheol <sc.oh131@gmail.com>
2022-10-10 21:28:15 +0900

    [M4] test: 로또 당첨 Rank 조회 테스트 케이스 추가
    참조: https://github.com/next-step/java-lotto/pull/2561

--- src/test/java/lotto/domain/LottoWrapperTest.java
+++ src/test/java/lotto/domain/LottoWrapperTest.java
@@ -1,42 +1,49 @@
 package lotto.domain;
 
 import lotto.enumerate.Rank;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoWrapperTest {
+    private static final int NO_MATCH_NUMBER = -1;
+    private static final int BONUS_NUMBER = 0;
+    private static LottoNumbersWrapper collectNumbers = new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6));
+    private static Map<Rank, Lotto> lottoEnumMap = new EnumMap(Rank.class);
 
-    @Test
-    @DisplayName("1등 당첨 Rank 조회 테스트")
-    void getResultRanksTest1() {
-        int bonusNumber = 0;
-        LottoNumbersWrapper testLottoNumbersWrapper = new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6));
+    @BeforeAll
+    static void setLottoEnumMap() {
+        lottoEnumMap.put(Rank.FIRST, new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6))));
+        lottoEnumMap.put(Rank.SECOND, new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, BONUS_NUMBER))));
+        lottoEnumMap.put(Rank.THIRD, new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, NO_MATCH_NUMBER))));
+        lottoEnumMap.put(Rank.FOURTH, new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, NO_MATCH_NUMBER, NO_MATCH_NUMBER))));
+        lottoEnumMap.put(Rank.FIFTH, new Lotto(new LottoNumbersWrapper(Arrays.asList(1, 2, 3, NO_MATCH_NUMBER, NO_MATCH_NUMBER, NO_MATCH_NUMBER))));
+    }
 
-        Lotto lotto = new Lotto(testLottoNumbersWrapper);
-        LottoWrapper lottoWrapper = new LottoWrapper(Arrays.asList(lotto));
-        List<Rank> resultRanks = lottoWrapper.getResultRanks(testLottoNumbersWrapper, bonusNumber);
+    @ParameterizedTest
+    @DisplayName("1 ~ 5등 단일 당첨 Rank 조회 테스트")
+    @EnumSource
+    void getResultRanksTest(Rank rank) {
+        LottoWrapper lottoWrapper = new LottoWrapper(Arrays.asList(lottoEnumMap.get(rank)));
+        List<Rank> resultRanks = lottoWrapper.getResultRanks(collectNumbers, BONUS_NUMBER);
 
         assertThat(resultRanks).hasSize(1);
-        assertThat(resultRanks.get(0)).isEqualTo(Rank.FIRST);
+        assertThat(resultRanks.get(0)).isEqualTo(rank);
     }
 
     @Test
-    @DisplayName("2등 당첨 Rank 조회 테스트")
+    @DisplayName("1 ~ 5등 동시 당첨 Rank 조회 테스트")
     void getResultRanksTest2() {
-        int bonusNumber = 0;
-        LottoNumbersWrapper collectNumbersWrapper = new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, 6));
-        LottoNumbersWrapper testLottoNumbersWrapper = new LottoNumbersWrapper(Arrays.asList(1, 2, 3, 4, 5, bonusNumber));
+        List<Lotto> winLottos = new ArrayList<>(lottoEnumMap.values());
+        LottoWrapper lottoWrapper = new LottoWrapper(winLottos);
+        List<Rank> resultRanks = lottoWrapper.getResultRanks(collectNumbers, BONUS_NUMBER);
 
-        Lotto lotto = new Lotto(testLottoNumbersWrapper);
-        LottoWrapper lottoWrapper = new LottoWrapper(Arrays.asList(lotto));
-        List<Rank> resultRanks = lottoWrapper.getResultRanks(collectNumbersWrapper, bonusNumber);
-
-        assertThat(resultRanks).hasSize(1);
-        assertThat(resultRanks.get(0)).isEqualTo(Rank.SECOND);
+        assertThat(resultRanks).hasSize(winLottos.size());
     }
 }
