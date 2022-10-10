935a30357d2d4a0d01ab53295b8fd750af545141
Yunsik-Choi <choiys0311@gmail.com>
2022-10-10 22:34:43 +0900

    refactor : StringControllerMain

--- src/main/java/stringcalculator/StringCalculatorMain.java
+++ src/main/java/stringcalculator/StringCalculatorMain.java
@@ -1,14 +1,8 @@
 package stringcalculator;
 
-import stringcalculator.io.StringCalculatorInput;
-
-import java.util.Scanner;
-
 public class StringCalculatorMain {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println(StringCalculatorController.calculate(StringCalculatorInput.input(" ")));
+        System.out.println(StringCalculatorController.calculate());
     }
 }
