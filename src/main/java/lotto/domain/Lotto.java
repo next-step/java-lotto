a3736238fcb893e65cc9b29df4252a9479d5f8b0
Yunsik-Choi <choiys0311@gmail.com>
2022-10-11 00:13:56 +0900

    feat : Lotto create

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> lottoNumbers;
+
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+}
