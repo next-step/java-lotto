8dcbea4c5f86de9a4981121e54b91a05e4847dbc
caoyu-dev <choyumin01@gmail.com>
2022-10-12 01:13:35 +0900

    [feat] Operator 생성 및 테스트 추가

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,2 +1,23 @@
-package calculator;public enum Operator {
+package calculator;
+
+import java.util.function.BiFunction;
+
+public enum Operator {
+    PLUS("+", (num1, num2) -> num1 + num2),
+    MINUS("-", (num1, num2) -> num1 - num2),
+    MULTIPLY("*", (num1, num2) -> num1 * num2),
+    DIVIDE("/", (num1, num2) -> num1 / num2);
+
+    private final String operator;
+    private final BiFunction<Integer, Integer, Integer> expression;
+
+    Operator(String operator, BiFunction<Integer, Integer, Integer> expression) {
+        this.operator = operator;
+        this.expression = expression;
+    }
+
+    public Integer execute(int num1, int num2) {
+        return expression.apply(num1, num2);
+    }
+
 }
--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class OperatorTest {
-  
+    @Test
+    void plus() {
+        assertThat(Operator.PLUS.execute(1,2)).isEqualTo(3);
+    }
+
+    @Test
+    void minus() {
+        assertThat(Operator.MINUS.execute(3,2)).isEqualTo(1);
+    }
+
+    @Test
+    void multiply() {
+        assertThat(Operator.MULTIPLY.execute(2,3)).isEqualTo(6);
+    }
+
+    @Test
+    void divide() {
+        assertThat(Operator.DIVIDE.execute(8,4)).isEqualTo(2);
+    }
+
 }
