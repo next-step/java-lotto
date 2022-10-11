4aaaa5261e731205e6276757d7751624f1955cbf
wugatama <dnr7410@naver.com>
2022-10-12 01:26:41 +0900

    feat: 로또 결과 일급컬렉션

---
+++ src/main/java/step2/Result.java
@@ -0,0 +1,13 @@
+package step2;
+
+import java.util.List;
+import java.util.Set;
+
+public class Result {
+
+	private final List<Set<Integer>> list;
+
+	public Result(List<Set<Integer>> list) {
+		this.list = list;
+	}
+}
