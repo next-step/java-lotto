a2a7607f4788fda41cbe8e7b1f37f92d0ea65f6b
wugatama <dnr7410@naver.com>
2022-10-12 01:24:03 +0900

    feat: 로또 개당 출력

--- src/main/java/step2/Print.java
+++ src/main/java/step2/Print.java
@@ -1,5 +1,7 @@
 package step2;
 
+import java.util.Set;
+
 public class Print {
 	public static void inputAmount() {
 		System.out.println("구입금액을 입력해 주세요.");
@@ -7,5 +9,9 @@ public class Print {
 
 	public static void amount(int input) {
 		System.out.println(input + "개를 구매했습니다.");
+	}
+
+	public static void result(Set<Integer> result) {
+		System.out.println(result);
 	}
 }
