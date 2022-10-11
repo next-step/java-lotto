614b15d7c276de6295ed63d525e76a8e04b45184
jwoh <jwoh@wemakeprice.com>
2022-10-11 09:25:24 +0900

    refect : enum을 가져올때 optional로 처리

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -2,20 +2,19 @@ package calculator.domain;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public enum Operator
-{
+public enum Operator {
     ADD("+", (num1, num2) -> num1 + num2),
     SUB("-", (num1, num2) -> num1 - num2),
     MUL("*", (num1, num2) -> num1 * num2),
     DIV("/", (num1, num2) -> num1 / num2);
 
-    Operator(String opCode, BiFunction<Double, Double, Double> operator)
-    {
+    Operator(String opCode, BiFunction<Double, Double, Double> operator) {
         this.opCode = opCode;
         this.operator = operator;
     }
@@ -23,13 +22,11 @@ public enum Operator
     private final String opCode;
     private final BiFunction<Double, Double, Double> operator;
 
-    public String getOpCode()
-    {
+    public String getOpCode() {
         return opCode;
     }
 
-    public static Double execute(String opCode, double num1, double num2)
-    {
+    public static Double execute(String opCode, double num1, double num2) {
         return getOperator(opCode).operator.apply(num1, num2);
     }
 
@@ -37,13 +34,7 @@ public enum Operator
         Stream.of(values())
             .collect(Collectors.toMap(Operator::getOpCode, Function.identity())));
 
-    private static Operator getOperator(String opCode)
-    {
-        if (!map.containsKey(opCode))
-        {
-            throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
-        }
-
-        return map.get(opCode);
+    private static Operator getOperator(String opCode) {
+        return Optional.ofNullable(map.get(opCode)).orElseThrow(IllegalArgumentException::new);
     }
 }
