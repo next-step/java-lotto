4e36a507e102801c09b8428fcaebaea74062ddf9
wugatama <dnr7410@naver.com>
2022-10-12 01:03:39 +0900

    test: 1000원 미만 시 예외

--- src/test/java/step2/GameTest.java
+++ src/test/java/step2/GameTest.java
@@ -1,2 +1,15 @@
-package step2;public class GameTest {
+package step2;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+	@Test
+	void name() {
+		assertThatThrownBy(() -> Game.start(999))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("최소 금액은 1000원 입니다");
+	}
 }
