181a85cf93533f7fb47de4460ea373abe24e0e55
gumsu <chavi9@naver.com>
2022-10-11 16:02:06 +0900

    refactor: 중복되는 코드 삭제

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,29 +1,26 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.function.Predicate;
 
 public enum Rank {
 
-
-    FOURTH(3,Money.from(5000), match -> match == 3),
-    THIRD(4, Money.from(50000), match -> match == 4),
-    SECOND(5, Money.from(1500000), match -> match == 5),
-    FIRST(6, Money.from(2000000000), match -> match == 6),
-    NONE(0, Money.from(0), match -> match < 3);
+    FOURTH(3,Money.from(5000)),
+    THIRD(4, Money.from(50000)),
+    SECOND(5, Money.from(1500000)),
+    FIRST(6, Money.from(2000000000)),
+    NONE(0, Money.from(0));
 
     public int match;
     public Money money;
-    Predicate<Integer> isPredicted;
-    Rank(int match, Money money, Predicate<Integer> isPredicted) {
+
+    Rank(int match, Money money) {
         this.match = match;
         this.money = money;
-        this.isPredicted = isPredicted;
     }
 
     public static Rank calculate(int matchCount) {
         return Arrays.stream(values())
-            .filter(rank -> rank.isPredicted.test(matchCount))
+            .filter(rank -> rank.match == matchCount)
             .findFirst()
             .orElse(NONE);
     }
