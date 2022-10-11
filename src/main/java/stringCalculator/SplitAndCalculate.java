264e6b6ae410a8f744d1342de1bf876d28de76cf
nhs219 <heesun_z@naver.com>
2022-10-11 11:29:47 +0900

    refactor : splitAndCalculate 10줄이하로 수정

--- src/main/java/stringCalculator/SplitAndCalculate.java
+++ src/main/java/stringCalculator/SplitAndCalculate.java
@@ -1,5 +1,6 @@
 package stringCalculator;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,18 +10,18 @@ public class SplitAndCalculate {
         Values values = new Values();
         Calculator calculator = new Calculator();
 
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("공백과 null은 허용하지 않습니다.");
-        }
+        checkInputValue(input);
 
-        List<String> strings = splitString(input);
-        for (String string : strings) {
-            values.addValues(string);
-        }
+        splitString(input).forEach(s -> values.addValues(s));
 
         return calculator.calculate(values.pollOperand(), values.pollOperand(), values);
     }
 
+    private void checkInputValue(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("공백과 null은 허용하지 않습니다.");
+        }
+    }
 
     public List<String> splitString(String s) {
         return Arrays.asList(s.split(" "));
