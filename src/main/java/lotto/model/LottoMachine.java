a17ea92e989325d1aaeb6d2575ae69a2df76cc1e
bella.n <bella.n@kakaocorp.com>
2022-10-12 02:31:57 +0900

    refactor: 로또 머신의 로또 번호를 lottoNumber에서 초기화 및 캐싱하도록 변경

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -5,32 +5,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static lotto.model.LottoNumber.initializeLottoNumbers;
+
 public class LottoMachine implements LottoFactory {
 
-    private static final int LOTTO_PRICE = 1000;
-
     private Lotto createLotto() {
-        List<LottoNumber> lottoNumberList = IntStream.rangeClosed(1, 45)
-                .mapToObj(no -> new LottoNumber(no))
-                .collect(Collectors.toList());
-
+        List<LottoNumber> lottoNumberList = initializeLottoNumbers();
         Collections.shuffle(lottoNumberList);
 
         List<LottoNumber> createdLotto = lottoNumberList.subList(0, 6);
-
         Collections.sort(createdLotto);
 
         return new Lotto(createdLotto);
     }
 
     @Override
-    public List<Lotto> create(int amount) {
-        return IntStream.rangeClosed(1, getLottoCount(amount))
+    public List<Lotto> create(int purchasedCount) {
+        return IntStream.rangeClosed(1, purchasedCount)
                 .mapToObj(v -> createLotto())
                 .collect(Collectors.toList());
-    }
-
-    private int getLottoCount(int amount) {
-        return amount / LOTTO_PRICE;
     }
 }
