73756bc4247e4beab448ea7cc800498664a54af2
gmelon <hsh1769@naver.com>
2022-10-11 15:58:11 +0900

    refactor: controller 로직 inline

--- src/main/java/lottery/Controller.java
+++ src/main/java/lottery/Controller.java
@@ -1,22 +1,18 @@
 package lottery;
 
-import java.util.List;
-
+import static lottery.LotteryCompany.createLotteryResult;
 import static lottery.view.InputView.*;
 import static lottery.view.ResultView.*;
 
 public class Controller {
 
     public static void main(String[] args) {
-        int lotteryPurchasePrices = getLotteryPurchasePrices();
-
         Customer customer = new Customer();
-        customer.purchase(lotteryPurchasePrices);
+        customer.purchase(getLotteryPurchasePrices());
 
         printPurchasedLotteryInfos(customer.getLotteryWallet());
 
-        List<Integer> winningLotteryNumbers = getWinningLotteryNumbers();
-        LotteryResult lotteryResult = LotteryCompany.createLotteryResult(winningLotteryNumbers, customer.getLotteryWallet());
+        LotteryResult lotteryResult = createLotteryResult(getWinningLotteryNumbers(), customer.getLotteryWallet());
 
         printLotteryResult(lotteryResult);
     }
