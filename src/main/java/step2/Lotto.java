33534d16f43d40b44d21f55ad330707cf7f24072
wugatama <dnr7410@naver.com>
2022-10-12 01:04:45 +0900

    feat: 로또 생성

--- src/main/java/step2/Lotto.java
+++ src/main/java/step2/Lotto.java
@@ -1,2 +1,34 @@
-package step2;public class Lotto {
+package step2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lotto {
+	private List<Integer> balls = new ArrayList<>();
+
+	public Lotto() {
+		for (int i = 1; i <= 45 ; i++) {
+			this.balls.add(i);
+		}
+	}
+
+	public Set<Integer> getRandomLotto(int randomStart) {
+		Set<Integer> shuffle = new TreeSet<>();
+		Collections.shuffle(balls);
+
+		for (int i = randomStart; i <= randomStart + 5; i++) {
+			shuffle.add(balls.get(i));
+		}
+
+		return shuffle;
+	}
+
+	public List<Integer> getBalls() {
+		return balls;
+	}
 }
