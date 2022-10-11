195647049f863f620de8af6ce82dd7fe68677783
wugatama <dnr7410@naver.com>
2022-10-12 01:27:03 +0900

    feat: 입력금액대로 로또 생성

--- src/main/java/step2/Game.java
+++ src/main/java/step2/Game.java
@@ -1,9 +1,27 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 public class Game {
 	public static void start(int input) {
-		Print.inputAmount();
-		int amount = validAmount(input);
+		int amount = validAmount(input) / 1000;
+		Print.amount(amount);
+
+		startRaffle(amount);
+	}
+
+	public static void startRaffle(int amount) {
+		Lotto lotto = new Lotto();
+		List<Set<Integer>> list = new ArrayList<>();
+		for (int i = 1; i <= amount; i++) {
+			Set<Integer> result = lotto.getRandomLotto(new Random().nextInt(39));
+			Print.result(result);
+			list.add(result);
+		}
+		Result result = new Result(list);
 	}
 
 	private static int validAmount(int inputAmount) {
