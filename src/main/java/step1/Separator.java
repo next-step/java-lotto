3a41d2763f717c6eddc3f0175cd69749f93421a1
wugatama <dnr7410@naver.com>
2022-10-12 00:23:32 +0900

    refactor: Separator 예외처리 역할 분리

--- src/main/java/step1/Separator.java
+++ src/main/java/step1/Separator.java
@@ -1,28 +1,57 @@
 package step1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Separator {
-	private final List<String> formula;
+	private static final String NUMBER_EXCEPTION_MESSAGE = "숫자만 계산할 수 있습니다";
+
+	private final List<Integer> even = new ArrayList<>();
+
+	private final List<String> odd = new ArrayList<>();
 
 	public Separator(String input) {
-		formula = Arrays.asList(input.split(" "));
+		String[] split = input.split(" ");
+		for (int i = 0; i < split.length; i++) {
+			if (checkEven(split, i))
+				continue;
+			odd.add(split[i]);
+		}
 	}
 
-	public List<String> getFormula() {
-		return formula;
+	private boolean checkEven(String[] split, int i) {
+		if (i % 2 == 0) {
+			validateNumber(split[i]);
+			return true;
+		}
+		return false;
+	}
+
+	private void validateNumber(String split) {
+		try {
+			even.add(Integer.parseInt(split));
+		} catch (Exception e) {
+			throw new IllegalArgumentException(NUMBER_EXCEPTION_MESSAGE);
+		}
 	}
 
 	public String getSignTerm(int index) {
-		return formula.get(index);
+		return odd.get(index);
 	}
 
-	public String getTerm() {
+	public int getTerm() {
 		return getTerm(0);
 	}
 
-	public String getTerm(int index) {
-		return formula.get(index);
+	public int getTerm(int index) {
+		return even.get(index);
+	}
+
+	public List<Integer> getTerms() {
+		return this.even;
+	}
+
+	public List<String> getSigns() {
+		return this.odd;
 	}
 }
