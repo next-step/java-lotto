d0ea57ccaa225a869ca1670f43349c285d8dc599
Ye-Ji-Kim <kyeoji0128@naver.com>
2022-10-11 10:58:58 +0900

    feat: 로또 번호 생성 기능 구현

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -7,14 +7,8 @@ public class LottoTicket {
 
     private List<LottoNumber> numbers;
 
-    public LottoTicket(List<Integer> numbers) {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-
-        for (Integer number: numbers) {
-            lottoNumbers.add(new LottoNumber(number));
-        }
-
-        this.numbers = lottoNumbers;
+    public LottoTicket(List<LottoNumber> numbers) {
+        this.numbers = numbers;
     }
 
 }
