e19c91ec1fdde776bfcce2672b6180ee47ab7905
wugatama <dnr7410@naver.com>
2022-10-12 01:05:14 +0900

    feat: 시작, 1000원 미만 예외

--- src/main/java/step2/Game.java
+++ src/main/java/step2/Game.java
@@ -1,2 +1,15 @@
-package step2;public class Game {
+package step2;
+
+public class Game {
+	public static void start(int input) {
+		Print.inputAmount();
+		int amount = validAmount(input);
+	}
+
+	private static int validAmount(int inputAmount) {
+		if (inputAmount < 1000) {
+			throw new IllegalArgumentException("최소 금액은 1000원 입니다");
+		}
+		return inputAmount;
+	}
 }
