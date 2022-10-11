435b0e03ce10738b892bae163727c70fe6f0b3b8
jimbaemon <jimbae@kakao.com>
2022-10-12 00:45:58 +0900

    test: 가진 금액에서 구매 가능 갯수 확인하는 테스트 작성.

---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+    @Test
+    void 가진_금액에서_구매_가능_갯수_확인() {
+        Money money = new Money(14000);
+        Assertions.assertThat(money.calculateQuantity(1000)).isEqualTo(14);
+    }
+}
