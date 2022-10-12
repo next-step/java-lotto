50e594089f1b0c216f8886593dbf7d0a4a659adf
boradol2 <boradol0902@gmail.com>
2022-10-13 01:50:00 +0900

    feature(StringCalculatorService) : 연산수행하는 서비스
    1. input 받아 배열로 저장하는 로직
    2. 반복패턴 찾아 연산 순서대로 수행함.
    3. 각 객체에게 에러처리 맡김.

---
+++ src/main/java/calculator/service/StringCalculatorService.java
@@ -0,0 +1,36 @@
+package calculator.service;
+
+import calculator.domain.CalcNumber;
+import calculator.domain.CalcOperator;
+
+import java.util.List;
+
+public class StringCalculatorService {
+    private static final int FIRST_INPUT_INDEX = 0;
+    private static final String SEPARATOR = " ";
+    private static final String NO_INPUT_MESSAGE = "입력값이 없습니다.";
+
+    private StringCalculatorService() {
+    }
+
+    public static List<String> getOperations(String input) {
+        if (isNullOrBlank(input)) {
+            throw new IllegalArgumentException(NO_INPUT_MESSAGE);
+        }
+        return List.of(input.split(SEPARATOR));
+    }
+
+    public static CalcNumber calculate(List<String> operations) {
+        CalcNumber result = new CalcNumber(operations.get(FIRST_INPUT_INDEX));
+        for (int i = 2; i < operations.size(); i += 2) {
+            CalcOperator operator = new CalcOperator(operations.get(i - 1));
+            CalcNumber nextNum = new CalcNumber(operations.get(i));
+            result = operator.operate().calculator(result, nextNum);
+        }
+        return result;
+    }
+
+    private static boolean isNullOrBlank(String input) {
+        return input == null || input.isBlank();
+    }
+}
