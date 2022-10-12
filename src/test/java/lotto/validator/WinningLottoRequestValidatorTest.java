a04c249e6ab521dcd84fa49e4850718ab493d616
robin.son <robin.son@kakaocorp.com>
2022-10-12 11:38:51 +0900

    test : 당첨 번호와 보너스 번호가 중복되는 경우 Exception 발생 테스트 코드 추가

--- src/test/java/lotto/validator/WinningLottoRequestValidatorTest.java
+++ src/test/java/lotto/validator/WinningLottoRequestValidatorTest.java
@@ -2,7 +2,9 @@ package lotto.validator;
 
 import lotto.models.request.WinningLottoRequest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class WinningLottoRequestValidatorTest {
     private static final String STRING_NUMBER_WRONG_CHARACTER = "문자열로 입력된 로또 번호는 숫자, 공백, 콤마(,)만을 포함해야합니다.";
     private static final String WRONG_BONUS_NUMBER = "로또 번호는 반드시 1~45의 값 입니다.";
+
+    private static final String DUPLICATED_BONUS_NUMBER = "보너스 번호와 로또 번호는 중복되지 않아야합니다.";
 
     @ParameterizedTest
     @DisplayName("당첨 번호가 숫자, 공백, 콤마 외의 기호를 포함할 경우 Exception을 발생시킨다.")
@@ -30,5 +34,14 @@ public class WinningLottoRequestValidatorTest {
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> WinningLottoRequestValidator.validate(winningLottoRequest));
         assertThat(exception.getMessage()).isEqualTo(WRONG_BONUS_NUMBER);
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 먼저 입력된 로또 번호와 중복되면 Exception을 발생시킨다.")
+    void exceptionTest3() {
+        WinningLottoRequest winningLottoRequest = WinningLottoRequest.of("1, 2, 3, 4, 5, 6", 6);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> WinningLottoRequestValidator.validate(winningLottoRequest));
+        assertThat(exception.getMessage()).isEqualTo(DUPLICATED_BONUS_NUMBER);
     }
 }
