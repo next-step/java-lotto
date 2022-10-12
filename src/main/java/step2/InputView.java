e0702e11fb7187e748df629a8c72135ffb33d44f
wugatama <dnr7410@naver.com>
2022-10-12 16:36:13 +0900

    refactor: parseInt 분리

--- src/main/java/step2/InputView.java
+++ src/main/java/step2/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
 	private static final Scanner sc = new Scanner(System.in);
 
-	public static int input() {
-		return Integer.parseInt(sc.nextLine());
+	public static String input() {
+		return sc.nextLine();
 	}
 }
