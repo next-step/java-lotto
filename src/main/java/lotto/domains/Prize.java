6817256f84ccf8dbb436394d414d0ecd28628797
karon-ma <karon.ma@kakaocorp.com>
2022-10-12 19:06:30 +0900

    feat: Add getter to Prize enum

--- src/main/java/lotto/domains/Prize.java
+++ src/main/java/lotto/domains/Prize.java
@@ -3,7 +3,7 @@ package lotto.domains;
 import java.util.Arrays;
 
 public enum Prize {
-    FIRST(6, 2000000000L), SECOND(5, 1500000L), THIRD(4, 50000L), FOURTH(3, 5000L), NONE(0, 0L);
+    FIRST(6, 2000000000L), SECOND(5, 1500000L), THIRD(4, 50000L), FOURTH(3, 5000L), NONE(2, 0L);
 
     private final int correctCount;
     private final long reward;
@@ -13,8 +13,16 @@ public enum Prize {
         this.reward = reward;
     }
 
+    public int getCorrectCount() {
+        return correctCount;
+    }
+
+    public long getReward() {
+        return reward;
+    }
+
     public static Prize find(long count) {
-        if (count <= 2) {
+        if (count <= NONE.correctCount) {
             return NONE;
         }
 
