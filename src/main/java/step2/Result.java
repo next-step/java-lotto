4003f6a24c209ce1be5914f70b785d62c49746ae
wugatama <dnr7410@naver.com>
2022-10-12 16:35:06 +0900

    feat: 로또 결과 분리

--- src/main/java/step2/Result.java
+++ src/main/java/step2/Result.java
@@ -1,13 +1,53 @@
 package step2;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class Result {
 
 	private final List<Set<Integer>> list;
+	private final Map<Operator, Integer> map = new TreeMap<>(){{
+		put(Operator.THREE, 0);
+		put(Operator.FOUR, 0);
+		put(Operator.FIVE, 0);
+		put(Operator.SIX, 0);
+	}};
 
 	public Result(List<Set<Integer>> list) {
 		this.list = list;
+	}
+
+	public List<Set<Integer>> getList() {
+		return list;
+	}
+
+	public Map<Operator, Integer> results(Set<Integer> winningNumbers) {
+		for (Set<Integer> guestLotto : list) {
+			Operator rank = Operator.find(checkRank(guestLotto, winningNumbers));
+			if (rank == null) {
+				continue;
+			}
+			map.put(rank, map.get(rank) + 1);
+		}
+		return map;
+	}
+
+	private int checkRank(Set<Integer> guestLotto, Set<Integer> winningNumbers) {
+		int count = 0;
+		Iterator<Integer> itr = winningNumbers.iterator();
+		while (itr.hasNext()) {
+			count = checkContains(guestLotto, count, itr);
+		}
+		return count;
+	}
+
+	private int checkContains(Set<Integer> guestLotto, int count, Iterator<Integer> itr) {
+		if (guestLotto.contains(itr.next())) {
+			count++;
+		}
+		return count;
 	}
 }
