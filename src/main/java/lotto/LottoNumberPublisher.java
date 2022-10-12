765621e78f71775e35e41489b34c2ac2388737b6
owen.qq <owen.qq@kakaocorp.com>
2022-10-12 11:06:35 +0900

    test: 로또 번호 발행에 대한 tc작성

---
+++ src/main/java/lotto/LottoNumberPublisher.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class LottoNumberPublisher {
+
+    public static int[] publish() {
+        return new int[] {0};
+    }
+}
---
+++ src/test/java/lotto/LottoNumberPublisherTest.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberPublisherTest {
+    @Test
+    void 로또번호를발행한다() {
+        int[] lottoNumbers = LottoNumberPublisher.publish();
+        Assertions.assertThat(lottoNumbers).hasSize(6);
+    }
+}
