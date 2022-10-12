e11f8f757e7e8dbba1e22a4a5a6e66820d431fe0
yeojin822 <xlzt2002@gmail.com>
2022-10-12 21:34:33 +0900

    fix : step1 - 문자열 계산기 피드백 반영

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -21,13 +21,10 @@ public class StringCalculator {
     public static int calculate(String[] data) {
         int result = Integer.parseInt(data[0]);
         for (int i = 1; i < data.length - 1; i += 2) {
-            result = getOperatorExpression(data[i], result, Integer.parseInt(data[i + 1]));
+            Operator operator = Operator.getExpression(data[i]);
+            result = operator.calculate(result, Integer.parseInt(data[i + 1]));
+
         }
         return result;
-    }
-
-    public static int getOperatorExpression(String operator, Integer num1, Integer num2) {
-        return Operator.getExpression(operator)
-                .mapCalculate(num1, num2);
     }
 }
