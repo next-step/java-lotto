516c779741afa667a44357fa1ed3fc6246a67e1d
bella.n <bella.n@kakaocorp.com>
2022-10-12 11:19:36 +0900

    refactor: 보너스 볼 추가에 따른 랭킹 정의 추가 및 상금 변경 (FIFTH, MISS)

--- src/main/java/lotto/model/enumeration/Rank.java
+++ src/main/java/lotto/model/enumeration/Rank.java
@@ -7,23 +7,25 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FIRST(6, 2_000_000_000),
+    SECOND(5.5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
-    private static final Map<Integer, Rank> RANK_MAP = Arrays.stream(Rank.values())
+    private static final Map<Double, Rank> RANK_MAP = Arrays.stream(Rank.values())
             .collect(Collectors.toMap(rank -> rank.countOfMatch, Function.identity()));
 
-    private int countOfMatch;
+    private double countOfMatch;
     private int winningMoney;
 
-    Rank(int countOfMatch, int winningMoney) {
+    Rank(double countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
     }
 
-    public int getCountOfMatch() {
+    public double getCountOfMatch() {
         return countOfMatch;
     }
 
@@ -31,7 +33,7 @@ public enum Rank {
         return winningMoney;
     }
 
-    public static Optional<Rank> valueOf(Integer countOfMatch) {
-        return Optional.ofNullable(RANK_MAP.get(countOfMatch));
+    public static Rank valueOf(double countOfMatch) {
+        return Optional.ofNullable(RANK_MAP.get(countOfMatch)).orElse(MISS);
     }
 }
