655b147f1fddb973c1419d0cc1c0b2bb361573ef
wugatama <dnr7410@naver.com>
2022-10-12 16:33:07 +0900

    test: 최소 금액 테스트, 로또 갯수, 당첨 번호 입력

--- src/test/java/step2/GameTest.java
+++ src/test/java/step2/GameTest.java
@@ -2,14 +2,37 @@ package step2;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class GameTest {
 
 	@Test
-	void name() {
+	@DisplayName("최소 금액 예외")
+	void minAmount() {
 		assertThatThrownBy(() -> Game.start(999))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("최소 금액은 1000원 입니다");
+	}
+
+	@ParameterizedTest
+	@DisplayName("구입금액에 따라 로또 개수 확인")
+	@CsvSource(value = {
+		"10:10",
+		"3:3"
+	}, delimiter = ':')
+	void inputAmountOfLotto(int lottoByMoney, int lotto) {
+		assertThat(Game.startRaffle(lottoByMoney).getList().size()).isEqualTo(lotto);
+	}
+
+	@Test
+	@DisplayName("지난 주 당첨 번호 생성")
+	void makeWinningNumbers() {
+		String str = "1, 2, 3, 4, 5, 6";
+		assertThat(Game.makeWinningResult(str.split(", "))).isEqualTo(Set.of(1, 2, 3, 4, 5, 6));
 	}
 }
