14f71aba164dc2385566bebc1c368b32d7701767
min16 <minji.na@cre.ma>
2022-10-14 00:08:43 +0900

    EDIT - 주석처리된 Operator 테스트 코드 주석 제거

--- src/test/java/Lotto/OperatorTest.java
+++ src/test/java/Lotto/OperatorTest.java
@@ -6,8 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OperatorTest {
     // 안되는 이유?
-//    @Test
-//    void 연산() {
-//        assertThat(Operator.operatorOf("+")).isEqualTo(new Add());
-//    }
+    @Test
+    void 연산() {
+        assertThat(Operator.operatorOf("+")).isEqualTo(new Add());
+    }
 }
