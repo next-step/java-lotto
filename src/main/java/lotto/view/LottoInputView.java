404c7d06c4fc3061bc3e9af4fc1763315be08cd1
yunhwan <pyh8618@gmail.com>
2022-10-13 21:26:52 +0900

    feat : 보너스볼을 입력받는 메서드 추가

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import lotto.domain.LottoBall;
 import lotto.util.StringToIntUtil;
 
 public class LottoInputView {
@@ -24,6 +25,12 @@ public class LottoInputView {
         System.out.println("지난 주 당첨 번호를 입력해주세요.");
         String text = scanner.nextLine();
         return splitText(text);
+    }
+
+    public static LottoBall getBonusBallNumber() {
+        System.out.println("보너스 볼을 입력해주세요.");
+        int number = scanner.nextInt();
+        return new LottoBall(number);
     }
 
     private static List<Integer> splitText(String text) {
