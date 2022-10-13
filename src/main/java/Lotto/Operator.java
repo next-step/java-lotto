9da27b7037c370d7f59a885612491719b1a8a292
min16 <minji.na@cre.ma>
2022-10-14 00:53:59 +0900

    REFACTOR - `Operator` 에서 객체 변수로 갖고 있자

--- src/main/java/Lotto/Operator.java
+++ src/main/java/Lotto/Operator.java
@@ -1,6 +1,14 @@
 package Lotto;
 
 public abstract class Operator {
+    private static final Operator add;
+    private static final Operator subtract;
+
+    static {
+        add = new Add();
+        subtract = new Subtract();
+    }
+
     @Override
     public int hashCode() {
         return super.hashCode();
@@ -19,8 +27,8 @@ public abstract class Operator {
     public abstract int operate(int num1, int num2);
 
     public static Operator operatorOf(String value) {
-        if (value.equals("+")) return new Add();
-        if (value.equals("-")) return new Subtract();
+        if (value.equals("+")) return add;
+        if (value.equals("-")) return subtract;
         return null;
     }
 }
