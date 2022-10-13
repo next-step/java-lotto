d8018f79698e9c242720902dec7c8a7d59407fbf
jimbaemon <jimbae@kakao.com>
2022-10-13 23:50:24 +0900

    test: 로또 테스트 생성, 테스트 만들면서 생각한 설계대로 로또 구입 테스트 수정.

--- src/test/java/lotto/LottoShopTest.java
+++ src/test/java/lotto/LottoShopTest.java
@@ -9,13 +9,14 @@ public class LottoShopTest {
 
     @Test
     void 제공한_금액만큼_로또를_제공() {
-        LottoShop shop = new LottoShop();
+        LottoNumberStrategy lottoNumberStrategy = () -> List.of(16, 15, 14, 13, 12, 11);
+        LottoShop shop = new LottoShop(lottoNumberStrategy);
         Lottos lottos = shop.buy(new Money(2000));
         Assertions.assertThat(lottos).isEqualTo(
             new Lottos(
                 List.of(
-                    new Lotto(),
-                    new Lotto()
+                    new Lotto(List.of(1)),
+                    new Lotto(List.of(1))
                 )
             )
         );
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,17 +3,18 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTest {
 
     @Test
     void 로또를_출력() {
-        LottoNumberStrategy lottoNumberStrategy = new FraudStrategy();
+        LottoNumberStrategy lottoNumberStrategy = () -> new ArrayList<>(List.of(16, 15, 14, 13, 12));
         Lotto lotto = Lotto.print(lottoNumberStrategy);
         Assertions.assertThat(lotto).isEqualTo(
             new Lotto(
-                List.of(12, 13, 14, 15, 16)
+                new ArrayList<>(List.of(12, 13, 14, 15, 16))
             )
         );
     }
