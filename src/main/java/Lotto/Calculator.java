82a3e394235ff74ac3768b633b306bd20125f5a6
min16 <minji.na@cre.ma>
2022-10-14 00:22:47 +0900

    FEATURE - 입력받은 문자열을 계산하는 메소드 구현

--- src/main/java/Lotto/Calculator.java
+++ src/main/java/Lotto/Calculator.java
@@ -1,44 +1,26 @@
 package Lotto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Calculator {
-    static int calculate(String valueText) {
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.calculate("10 + 2 + 9"));
+    }
+
+    public static int calculate(String valueText) {
         String[] values = valueText.split(" ");
-        List<Integer> numbers = toNumbers(values);
-        List<Operator> operators = toOperators(values);
-        int index = 0;
-        int result = numbers.get(0);
-        for (Operator operator : operators) {
-            index++;
-            int num1 = result;
-            int num2 = numbers.get(index);
-            result = operator.operate(num1, num2);
+
+        int result = toNumber(values[0]);
+        for (int i = 1; i < values.length; i += 2) {
+            Operator operator = Operator.operatorOf(values[i]);
+            int number = toNumber(values[i + 1]);
+
+            result = operator.operate(result, number);
         }
 
         return result;
     }
 
-    private static List<Integer> toNumbers(String[] values) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < values.length; i++) {
-            if (i % 2 == 0) {
-                numbers.add(Integer.parseInt(values[i]));
-            }
-        }
-
-        return numbers;
-    }
-
-    private static List<Operator> toOperators(String[] values) {
-        List<Operator> operators = new ArrayList<>();
-        for (int i = 0; i < values.length; i++) {
-            if (i % 2 != 0) {
-                operators.add(Operator.operatorOf(values[i]));
-            }
-        }
-
-        return operators;
+    private static int toNumber(String value) {
+        return Integer.parseInt(value);
     }
 }
