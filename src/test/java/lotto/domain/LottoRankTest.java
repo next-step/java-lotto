8ec60bc127f6386fd4fe9306edc97a57c8557f49
yunhwan <pyh8618@gmail.com>
2022-10-13 21:22:56 +0900

    feat : Rank에 보너스 정보 추가

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -2,14 +2,16 @@ package lotto.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoRankTest {
 
     @ParameterizedTest
-    @EnumSource(LottoRank.class)
-    void 맞춘갯수를_당첨등수로_변환(LottoRank lottoRank) {
-        Assertions.assertThat(LottoRank.findRank(lottoRank.getCorrectNum())).isEqualTo(lottoRank);
+    @CsvSource(value = {"FIRST:false", "SECOND:false", "BONUS:true"}, delimiter = ':')
+    void 맞춘갯수를_당첨등수로_변환(LottoRank lottoRank, boolean matchBonus) {
+        Assertions.assertThat(LottoRank.findRank(lottoRank.getCorrectNum(), matchBonus)).isEqualTo(lottoRank);
     }
 
 }
