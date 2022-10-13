0cf9a693096d0e04fb2e9095c9acfd0b0136de0c
gumsu <chavi9@naver.com>
2022-10-13 19:42:21 +0900

    refactor: 인스턴스 캐싱 적용

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -10,22 +10,27 @@ public class LottoNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     public static final List<LottoNumber> LOTTO_NUMBERS = IntStream
-        .range(MIN_NUMBER, MAX_NUMBER)
-        .mapToObj(LottoNumber::from)
+        .range(MIN_NUMBER, MAX_NUMBER + 1)
+        .mapToObj(LottoNumber::new)
         .collect(Collectors.toList());
 
     private int number;
 
     private LottoNumber(int number) {
-        valid(number);
         this.number = number;
     }
 
     public static LottoNumber from(int number) {
-        return new LottoNumber(number);
+        valid(number);
+        LottoNumber lottoNumber = LOTTO_NUMBERS.get(number-1);
+
+        if (Objects.isNull(lottoNumber)) {
+            lottoNumber = new LottoNumber(number);
+        }
+        return lottoNumber;
     }
 
-    private void valid(int number) {
+    private static void valid(int number) {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException("숫자는 1 ~ 45 만 가능합니다.");
         }
