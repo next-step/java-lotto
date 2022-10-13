9c41fd51b39719e54527a1a828f1f36c873b4c51
sangwon lee <rollo@korda.im>
2022-10-13 09:12:24 +0900

    refactoring(1단계 - 문자열 계산기) : formatter 적용
    intellij formatter 적용 안된 부분 수정

--- src/main/java/calculator/exception/ValidateNumber.java
+++ src/main/java/calculator/exception/ValidateNumber.java
@@ -1,6 +1,6 @@
 package calculator.exception;
 
-public class ValidateNumber extends IllegalArgumentException{
+public class ValidateNumber extends IllegalArgumentException {
     private static final String MESSAGE = "입력 값을 다시 입력해 주세요.";
 
     public ValidateNumber() {
--- src/main/java/calculator/exception/ValidateOperator.java
+++ src/main/java/calculator/exception/ValidateOperator.java
@@ -2,7 +2,7 @@ package calculator.exception;
 
 public class ValidateOperator extends IllegalArgumentException {
 
-    private static final String MESSAGE ="사칙연산 기호를 입력해 주세요";
+    private static final String MESSAGE = "사칙연산 기호를 입력해 주세요";
 
     public ValidateOperator() {
         super(MESSAGE);
