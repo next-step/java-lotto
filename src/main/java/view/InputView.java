8071d614bf57a7410436483a9e5fea3a8b679954
이건회 <rg970604@naver.com>
2022-10-13 22:21:43 +0900

    refactor : InputView.java

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-  public static String question() {
+  public static String expression() {
     Scanner scanner = new Scanner(System.in);
     System.out.println("계산할 식을 입력하세요");
     return scanner.nextLine();
