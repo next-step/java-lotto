103507acbfc90844c47a24285ec0e19738de5025
boradol2 <boradol0902@gmail.com>
2022-10-13 23:40:06 +0900

    perf(SeparatorExpression): Expression 클래스를 만들어서 문자열 입력에 대해 검증 반영
    1. StringCalculatorService 애서 코드 이동
    2. 이 메소드를 실행하던 Application, Service 수정.

--- src/main/java/calculator/service/StringCalculatorService.java
+++ src/main/java/calculator/service/StringCalculatorService.java
@@ -2,25 +2,18 @@ package calculator.service;
 
 import calculator.domain.CalcNumber;
 import calculator.domain.CalcOperator;
+import calculator.domain.SeparatorExpression;
 
 import java.util.List;
 
 public class StringCalculatorService {
     private static final int FIRST_INPUT_INDEX = 0;
-    private static final String SEPARATOR = " ";
-    private static final String NO_INPUT_MESSAGE = "입력값이 없습니다.";
 
     private StringCalculatorService() {
     }
 
-    public static List<String> getOperations(String input) {
-        if (isNullOrBlank(input)) {
-            throw new IllegalArgumentException(NO_INPUT_MESSAGE);
-        }
-        return List.of(input.split(SEPARATOR));
-    }
-
-    public static CalcNumber calculate(List<String> operations) {
+    public static CalcNumber calculate(String input) {
+        List<String> operations = SeparatorExpression.getOperations(input);
         CalcNumber result = new CalcNumber(operations.get(FIRST_INPUT_INDEX));
         for (int i = 2; i < operations.size(); i += 2) {
             CalcOperator operator = new CalcOperator(operations.get(i - 1));
@@ -28,9 +21,5 @@ public class StringCalculatorService {
             result = operator.operate().calculate(result, nextNum);
         }
         return result;
-    }
-
-    private static boolean isNullOrBlank(String input) {
-        return input == null || input.isBlank();
     }
 }
