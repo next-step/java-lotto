59cf8607ab19645c5af9a9ede96d2bac7ecfe9b7
jimbaemon <jimbae@kakao.com>
2022-10-14 01:54:48 +0900

    test: 당첨번호 갯수별 등수 enum 생성 테스트 작성.

---
+++ src/test/java/lotto/DivisionTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisionTest {
+
+    @Test
+    void 일치갯수로_등수_생성(){
+        Assertions.assertThat(Division.valueOf(3)).isEqualTo(Division.FOURTH);
+        Assertions.assertThat(Division.valueOf(4)).isEqualTo(Division.THIRD);
+        Assertions.assertThat(Division.valueOf(5)).isEqualTo(Division.SECOND);
+        Assertions.assertThat(Division.valueOf(6)).isEqualTo(Division.FIRST);
+    }
+
+}
