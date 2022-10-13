8454f7fe1addd9c9f34974df8b181f1cf98e7b43
bella.n <bella.n@kakaocorp.com>
2022-10-13 23:01:29 +0900

    refactor: List<LottoNumber>를 파라미터 인자를 Lotto객체로 변경

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,27 +1,32 @@
 package lotto;
 
 import lotto.model.Lotteries;
-import lotto.model.Lotto;
+import lotto.model.Profit;
+import lotto.model.WinningLotto;
 import lotto.model.enumeration.Rank;
 
 import java.util.Map;
 
 import static lotto.client.InputView.*;
 import static lotto.client.OutputView.*;
-import static lotto.utils.LottoCreator.getLotteries;
+import static lotto.model.LottoCreator.createAutoLotto;
+import static lotto.model.Profit.getReturnRate;
 
 public class Main {
     public static void main(String[] args) {
         int purchaseAmount = scanPurchaseAmount();
+        int manualLottoCount = scanManualLottoCount();
 
-        Lotteries lotteries = getLotteries(purchaseAmount);
+        Lotteries lotteries = new Lotteries(createAutoLotto(purchaseAmount), scanManualLottoNumber(manualLottoCount));
 
         showCountOfLotto(lotteries);
         showCreatedLotteries(lotteries);
 
-        Map<Rank, Long> lotteriesRank = lotteries.getLotteriesRank(new Lotto(scanLastWinLotte()), scanBonusBall());
+        Profit profit = new Profit(new WinningLotto(scanLastWinLotte(), scanBonusBall()));
+
+        Map<Rank, Long> lotteriesRank = profit.getLotteriesRank(lotteries.getAllLotteries());
 
         showResultRank(lotteriesRank);
-        showReturnRate(lotteries, lotteriesRank);
+        showReturnRate(getReturnRate(lotteriesRank, purchaseAmount));
     }
 }
