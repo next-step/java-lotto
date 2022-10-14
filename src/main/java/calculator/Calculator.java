c65d87c3898742dfeebdfb055f564df9b29be84f
caoyu-dev <choyumin01@gmail.com>
2022-10-14 11:58:51 +0900

    [feat] split 추출하여 StringSplit 클래스 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,22 +1,18 @@
 package calculator;
 
-public class Calculator {
-    private static final String DELIMITER = " ";
+import utils.StringSplit;
 
+public class Calculator {
     public Calculator() {
     }
 
     public int calculate(String input) {
-        String[] str = split(input);
+        String[] str = StringSplit.split(input);
         int result = Integer.parseInt(str[0]);
         for (int i = 1; i < str.length - 1; i += 2) {
             Operator operator = Operator.getOperator(str[i]);
             result = operator.execute(result, Integer.parseInt(str[i + 1]));
         }
         return result;
-    }
-
-    public String[] split(String input) {
-        return input.split(DELIMITER);
     }
 }
