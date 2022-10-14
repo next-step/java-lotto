43283eae36de11a1aa3b49d453702158aa4d5d71
min16 <minji.na@cre.ma>
2022-10-14 22:38:17 +0900

    REMOVE - 불필요한 오버라이딩 제거

--- src/main/java/Lotto/Add.java
+++ src/main/java/Lotto/Add.java
@@ -4,19 +4,4 @@ public class Add extends Operator {
     public int operate(int num1, int num2) {
         return num1 + num2;
     }
-
-    @Override
-    public String toString() {
-        return "Add{}";
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 }
