69453a1f6b2cd30a603be5ccdc6a125301f4431c
Yunsik-Choi <choiys0311@gmail.com>
2022-10-14 10:48:36 +0900

    feat : LottoReward rank

--- src/main/java/lotto/domain/LottoReward.java
+++ src/main/java/lotto/domain/LottoReward.java
@@ -2,15 +2,21 @@ package lotto.domain;
 
 public enum LottoReward {
 
-    FIRST(2_000_000_000), THIRD(1_500_000), FOURTH(50_000), FIFTH(5_000);
+    FIRST(2_000_000_000, 1), SECOND(30_000_000, 2), THIRD(1_500_000, 3), FOURTH(50_000, 4), FIFTH(5_000, 5);
 
     private int reward;
+    private int rank;
 
-    LottoReward(int reward) {
+    LottoReward(int reward, int rank) {
         this.reward = reward;
+        this.rank = rank;
     }
 
     public int reward() {
         return reward;
+    }
+
+    public static int reward(int rank) {
+        return values()[rank - 1].reward();
     }
 }
