8afc15003605b5741cd02fdd048fc6254a5537b2
owen.qq <owen.qq@kakaocorp.com>
2022-10-14 11:59:07 +0900

    test: 코드리뷰 반영, commandFactory TC 추가

--- src/test/java/stringcalculator/command/CommandFactoryTest.java
+++ src/test/java/stringcalculator/command/CommandFactoryTest.java
@@ -1,6 +1,7 @@
 package stringcalculator.command;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,27 @@ public class CommandFactoryTest {
     @DisplayName("더하기에 해당하는 연산자를 가져온다")
     @Test
     void get_add_command() {
-        Assertions.assertThat(new CommandFactory().getCommand("+"))
+        assertThat(new CommandFactory().getCommand("+"))
                   .isInstanceOf(AddCommand.class);
     }
 
+    @DisplayName("뺄셈에 해당하는 연산자를 가져온다")
     @Test
     void get_minus_command() {
-
+        assertThat(new CommandFactory().getCommand("-")).isInstanceOf(MinusCommand.class);
     }
+
+    @DisplayName("곱셈에 해당하는 연산자를 가져온다")
+    @Test
+    void get_multiply_command() {
+        assertThat(new CommandFactory().getCommand("*")).isInstanceOf(MultiplyCommand.class);
+    }
+
+    @DisplayName("나눗셈에 해당하는 연산자를 가져온다")
+    @Test
+    void get_divide_command() {
+        assertThat(new CommandFactory().getCommand("/")).isInstanceOf(DivideCommand.class);
+    }
+
 }
 
