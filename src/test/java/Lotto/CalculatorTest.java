c1246952346a4355ef61c5a8e9a7752de4494e66
min16 <minji.na@cre.ma>
2022-10-14 22:33:41 +0900

    REFACTOR - CalculatorTest static 메소드 사용하도록 변경

--- src/test/java/Lotto/CalculatorTest.java
+++ src/test/java/Lotto/CalculatorTest.java
@@ -4,68 +4,58 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CalculatorTest {
     @Test
     void 텍스트_숫자로_변환() {
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate("1");
+        int result = Calculator.calculate("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void 덧셈_계산하기() {
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate("1 + 2 + 3");
-        assertThat(result).isEqualTo(6);
+        int result = Calculator.calculate("1 + 2");
+        assertThat(result).isEqualTo(3);
     }
 
     @Test
     void 뺄셈_계산하기() {
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate("10 - 2 - 3");
+        int result = Calculator.calculate("10 - 2 - 3");
         assertThat(result).isEqualTo(5);
     }
 
     @Test
     void 곱셈_계산하기() {
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate("10 * 2 * 3");
+        int result = Calculator.calculate("10 * 2 * 3");
         assertThat(result).isEqualTo(60);
     }
 
     @Test
     void 나눗셈_계산하기() {
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate("10 / 2 / 3");
+        int result = Calculator.calculate("10 / 2 / 3");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void 여러_연산자_계산하기() {
-        Calculator calculator = new Calculator();
-        int result = calculator.calculate("10 + 2 * 3 / 6 - 1");
+        int result = Calculator.calculate("10 + 2 * 3 / 6 - 1");
         assertThat(result).isEqualTo(5);
     }
 
     @Test
     void 연산자외_다른_기호_들어오면_exception_처리() {
-        Calculator calculator = new Calculator();
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            calculator.calculate("10 ! 10");
+            Calculator.calculate("10 ! 10");
         }).withMessageContaining("사칙연산이 아닙니다.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
     void 공백이_들어오면_exception_처리(String input) {
-        Calculator calculator = new Calculator();
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            calculator.calculate(input);
+            Calculator.calculate(input);
         }).withMessageContaining("유효하지 않은 입력값입니다.");
     }
 }
