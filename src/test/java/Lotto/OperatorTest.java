5bdce4a4c57b436fba45042c867cd4bd43535830
min16 <minji.na@cre.ma>
2022-10-14 22:40:16 +0900

    TEST - OperatorTest 테스트 보강

--- src/test/java/Lotto/OperatorTest.java
+++ src/test/java/Lotto/OperatorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OperatorTest {
-    // 안되는 이유?
     @Test
     void 덧셈() {
         assertThat(Operator.operatorOf("+")).isEqualTo(new Add());
@@ -14,5 +13,15 @@ public class OperatorTest {
     @Test
     void 뺄셈() {
         assertThat(Operator.operatorOf("-")).isEqualTo(new Subtract());
+    }
+
+    @Test
+    void 곱셈() {
+        assertThat(Operator.operatorOf("*")).isEqualTo(new Multiple());
+    }
+
+    @Test
+    void 나눗셈() {
+        assertThat(Operator.operatorOf("/")).isEqualTo(new Divide());
     }
 }
