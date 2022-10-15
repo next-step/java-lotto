d20ab81df52fe7fd3c0c9f5a8a67c74a4709c7d7
sienna <20200803@sungshin.ac.kr>
2022-10-15 23:13:51 +0900

    :sparkles:feat : 공백 예외처리

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import exception.BlankException;
 import model.Calculator;
 import view.InputView;
 import view.OutputView;
@@ -8,12 +9,14 @@ public class CalculatorController {
 
 	private static final OutputView OUTPUT_VIEW = new OutputView();
 	private static final InputView INPUT_VIEW = new InputView();
-
 	private static final Calculator CALCULATOR = new Calculator();
 
 	public void stringCalculator() {
 		String formula = INPUT_VIEW.askFormula();
+
+		if(formula.isEmpty()){
+			throw new BlankException("식을 작성해주세요");
+		}
 		OUTPUT_VIEW.showResult(formula, CALCULATOR.getResult(formula));
 	}
-
 }
---
+++ src/main/java/exception/BlankException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class BlankException extends RuntimeException{
+
+	public BlankException(String message) {
+		super(message);
+	}
+}
