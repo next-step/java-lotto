0e7d648db6b2ebf85afbdc097de4c9b2af000d4d
suchanmyoung <aud248@naver.com>
2022-10-15 14:28:05 +0900

    refactor: Controller 의미 단위로 개행

--- src/main/java/step2/controller/LottoGameController.java
+++ src/main/java/step2/controller/LottoGameController.java
@@ -13,7 +13,6 @@ import step2.view.OutputView;
 public class LottoGameController {
 
     public Ticket prepareLottoTicket() {
-        OutputView.printPriceNotification();
         final Ticket ticket = Ticket.from(InputView.inputPrice());
         OutputView.printPurchaseResult(ticket);
         return ticket;
@@ -23,9 +22,9 @@ public class LottoGameController {
         final LottoGame lottoGame = LottoGame.newInstance();
         final List<LottoNumber> lottoNumbers = lottoGame.playLotto(ticket);
         OutputView.printLottoNumbers(lottoNumbers);
-        OutputView.printWinningNumberNotification();
-        final List<Integer> winningNumbers = InputView.inputWinningNumbers();
-        final LottoResult lottoResult = LottoResult.from(winningNumbers);
+
+        final LottoResult lottoResult = LottoResult.from(InputView.inputWinningNumbers());
+
         final Map<Integer, Integer> drawLottoResult = lottoResult.drawLottoResult(lottoNumbers);
         OutputView.printDrawResult(drawLottoResult);
         final int totalIncome = Prize.calculateTotalIncome(drawLottoResult);
