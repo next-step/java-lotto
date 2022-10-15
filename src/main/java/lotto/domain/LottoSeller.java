188d4f9f35c2601214561065531285e519066c84
yunhwan <pyh8618@gmail.com>
2022-10-16 03:19:02 +0900

    feat : 수동입력으로 인해 변경된 실행 로직

--- src/main/java/lotto/domain/LottoSeller.java
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -1,9 +1,17 @@
 package lotto.domain;
 
 public class LottoSeller {
-    private static final int LOTTO_PRICE = 1000;
 
-    public static int sellLotto(int amount) {
-        return amount / LOTTO_PRICE;
+    public static LottoInput sellLotto(int amount, int manualCount) {
+        validateAmount(amount, manualCount);
+        int totalCount = amount / Lotto.LOTTO_PRICE;
+        int autoCount = totalCount - manualCount;
+        return new LottoInput(autoCount, manualCount);
+    }
+
+    private static void validateAmount(int amount, int manualCount) {
+        if (amount < manualCount * Lotto.LOTTO_PRICE) {
+            throw new IllegalArgumentException("구입금액보다 많이 살 수 없습니다.");
+        }
     }
 }
