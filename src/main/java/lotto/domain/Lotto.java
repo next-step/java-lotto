5a1bfa161e719dc710b6a79dc04141aed2072b61
michol.lee <lovethefeel@gmail.com>
2022-10-16 00:04:27 +0900

    refactor: 로또 팩토리 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,27 +1,14 @@
 package lotto.domain;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Lotto {
 
     private final List<Number> lotto;
 
-    private Lotto(final List<Number> lotto) {
+    public Lotto(final List<Number> lotto) {
 
         this.lotto = lotto;
-    }
-
-    public static Lotto from(final List<Integer> lottoNumber) {
-
-        return new Lotto(convert(lottoNumber));
-    }
-
-    private static List<Number> convert(final List<Integer> numbers) {
-
-        return numbers.stream()
-                .map(Number::new)
-                .collect(Collectors.toList());
     }
 
     public List<Number> getLotto() {
