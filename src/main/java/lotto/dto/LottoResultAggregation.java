ea320844eadf7222efd35bbeeb9578cd5d201471
owen.qq <owen.qq@kakaocorp.com>
2022-10-15 10:49:16 +0900

    refactor: 코드리뷰반영, 사용하지 않는 코드 제거

--- src/main/java/lotto/dto/LottoResultAggregation.java
+++ src/main/java/lotto/dto/LottoResultAggregation.java
@@ -1,7 +1,5 @@
 package lotto.dto;
 
-import static java.util.stream.Collectors.groupingBy;
-
 import java.util.List;
 import java.util.Map;
 
@@ -10,16 +8,6 @@ import lotto.model.LottoReward;
 
 public class LottoResultAggregation {
     private final Map<LottoReward, List<LottoMatchResult>> rewardAndCountMaps;
-
-    public LottoResultAggregation(List<LottoMatchResult> lottoMatchResults) {
-        this.rewardAndCountMaps = lottoMatchResults.stream()
-                                                   .filter(LottoMatchResult::rewardable)
-                                                   .collect(groupingBy(lottoMatchResult -> lottoMatchResult.getLottoReward().get()));
-    }
-
-//    public LottoResultAggregation(LottoMatchResults lottoMatchResults) {
-//        this.rewardAndCountMaps = lottoMatchResults.aggregate();
-//    }
 
     public LottoResultAggregation(Map<LottoReward, List<LottoMatchResult>> rewardAndCountMaps) {
         this.rewardAndCountMaps = rewardAndCountMaps;
