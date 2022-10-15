a21dd992cb2afa8ec8f114c1f93530cd779c6788
caoyu-dev <choyumin01@gmail.com>
2022-10-16 00:08:26 +0900

    [fix] 기타 수정 1. space 한 칸에 해당하는 내용 `.isBlank()` 로 수정 2. 필요없는 import 삭제

--- src/main/java/CalculatorApplication.java
+++ src/main/java/CalculatorApplication.java
@@ -8,7 +8,7 @@ public class CalculatorApplication {
     public static void main(String[] args) {
         System.out.println("원하는 연산을 입력하세요.");
         String str = sc.nextLine();
-        if (str == null | str.equals(" ")) {
+        if (str == null | str.isBlank()) {
             throw new IllegalArgumentException("공백은 올 수 없습니다.");
         }
         int result = Calculator.calculate(str);
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
