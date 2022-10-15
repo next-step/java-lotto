ebb29ee870f58db3b600d5e8662b5fc4870debfd
kws1044 <kws1044@gmail.com>
2022-10-15 16:11:47 +0900

    feat : WinningPriceTest 추가

--- src/test/java/lotto/domain/WinningPriceTest.java
+++ src/test/java/lotto/domain/WinningPriceTest.java
@@ -1,4 +1,22 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class WinningPriceTest {
-  
+
+
+
+    @Test
+    @DisplayName("당첨 등수 테스트")
+    void decide_Grade_Test(){
+        assertThat(WinningPrice.decideGrade(new MatchingResult(5, true))).isEqualTo(WinningPrice.SECOND);
+        assertThat(WinningPrice.decideGrade(new MatchingResult(5, false))).isEqualTo(WinningPrice.THIRD);
+        assertThat(WinningPrice.decideGrade(new MatchingResult(6, true))).isEqualTo(WinningPrice.FIRST);
+        assertThat(WinningPrice.decideGrade(new MatchingResult(6, false))).isEqualTo(WinningPrice.FIRST);
+    }
+
 }
