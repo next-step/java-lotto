ebd8dd954fca7ff9d69ef242bc40c523eccb3437
caoyu-dev <choyumin01@gmail.com>
2022-10-16 01:02:42 +0900

    [chore] input 의 express 메서드에서 checkSpace 메서드 추출

--- src/main/java/view/Input.java
+++ src/main/java/view/Input.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class Input {
     private static final String INPUT_OPERATION_EXPRESSION = "원하는 연산을 입력하세요.";
+    private static final String ERROR_CHECK_SPACE = "공백은 올 수 없습니다.";
     private static final Scanner sc = new Scanner(System.in);
 
     public static String express() {
         System.out.println(INPUT_OPERATION_EXPRESSION);
         String str = sc.nextLine();
-        if (str == null | str.isBlank()) {
-            throw new IllegalArgumentException("공백은 올 수 없습니다.");
-        }
+        checkSpace(str);
         return str;
+    }
+
+    private static void checkSpace(String str) {
+        if (str == null | str.isBlank()) {
+            throw new IllegalArgumentException(ERROR_CHECK_SPACE);
+        }
     }
 }
