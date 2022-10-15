eb41ae62b4bc42ebccf67530ec7d807852509248
michol.lee <lovethefeel@gmail.com>
2022-10-16 00:01:27 +0900

    refactor: 우승 로또에게 책임 위임

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,11 +5,6 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
-    private static final int MATCH_ONE = 1;
-    private static final int NOT_MATCH_ZERO = 0;
-    public static final int MAX_LOTTO_NUMBER_COUNT = 6;
-    private static final String COMMA_EMPTY_STRING = ", ";
-
     private final List<Number> lotto;
 
     private Lotto(final List<Number> lotto) {
@@ -17,41 +12,9 @@ public class Lotto {
         this.lotto = lotto;
     }
 
-    public static Lotto from(final String input) {
-
-        validate(input);
-        final List<Number> lottoNumbers = split(input);
-        validateSize(lottoNumbers);
-
-        return new Lotto(lottoNumbers);
-    }
-
     public static Lotto from(final List<Integer> lottoNumber) {
 
         return new Lotto(convert(lottoNumber));
-    }
-
-    private static void validate(final String input) {
-
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("입력 값이 null 또는 빈 공백 입니다.");
-        }
-    }
-
-    private static void validateSize(final List<Number> lottoNumbers) {
-
-        final Set<Number> numbers = new HashSet<>(lottoNumbers);
-        if (numbers.size() != MAX_LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("6개의 숫자를 입력해야 하며 중복 숫자는 입력할 수 없습니다.");
-        }
-    }
-
-    private static List<Number> split(final String input) {
-
-        return Arrays.stream(input.split(COMMA_EMPTY_STRING))
-                .sorted()
-                .map(number -> new Number(Integer.parseInt(number)))
-                .collect(Collectors.toList());
     }
 
     private static List<Number> convert(final List<Integer> numbers) {
@@ -61,30 +24,23 @@ public class Lotto {
                 .collect(Collectors.toList());
     }
 
-    public int match(final WinningLotto winningLotto) {
-
-        int result = 0;
-        for (Number winningNumber : winningLotto.getWinnerLotto()) {
-            result += compare(winningNumber);
-        }
-        return result;
-    }
-
-    private int compare(final Number winningNumber) {
-
-        if (this.lotto.contains(winningNumber)) {
-            return MATCH_ONE;
-        }
-        return NOT_MATCH_ZERO;
-    }
-
     public List<Number> getLotto() {
 
         return Collections.unmodifiableList(this.lotto);
     }
 
-    public boolean matchBonus(final WinningLotto winningLotto) {
+    @Override
+    public boolean equals(Object o) {
 
-        return this.lotto.contains(winningLotto.getBonusNumber());
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto1 = (Lotto) o;
+        return Objects.equals(lotto, lotto1.lotto);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(lotto);
     }
 }
