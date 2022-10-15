ae0947719870d16b1769e60b4fe868394e58c8a6
shineyuno <shineyuno@gmail.com>
2022-10-16 01:14:31 +0900

    refactor : 리뷰 피드백 반영

--- src/main/java/calculator/CalculatorApplication.java
+++ src/main/java/calculator/CalculatorApplication.java
@@ -3,11 +3,11 @@ package calculator;
 import java.util.Scanner;
 
 public class CalculatorApplication {
-    private static final Scanner scanner= new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("원하시는 연산을 입력하세요.");
-        String text = scanner.nextLine();
+        String text = SCANNER.nextLine();
         int result = StringCalculator.calculate(text);
         System.out.printf("result : %d \r\n", result);
     }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -17,13 +17,13 @@ public class StringCalculatorTest {
         assertThatIllegalArgumentException().isThrownBy(() -> StringCalculator.split(null));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 공백문자 - args : [{arguments}]")
     @ValueSource(strings = {"", "  "})
     public void split_빈공백문자(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> StringCalculator.split(input));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 문자 분리 - args : [{arguments}]")
     @CsvSource(value = {"1:1", "1 3:2", "-1 + 1:3", "2 + 3 * 4 / 2:7"}, delimiter = ':')
     public void split(String text, int expectedSize) {
         List<String> resultList = StringCalculator.split(text);
@@ -32,19 +32,19 @@ public class StringCalculatorTest {
         assertThat(resultList).hasSize(expectedSize);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 유효한 연산자 - args : [{arguments}]")
     @ValueSource(strings = {"+", "-", "*", "/"})
     public void isVaildOperator(String operator) {
         assertThat(StringCalculator.isValidOperator(operator)).isTrue();
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 유효하지않은 연산자 - args : [{arguments}]")
     @ValueSource(strings = {"%", "x", "X", "="})
     public void isVaildOperator_유효하지않은_연산자(String operator) {
         assertThatIllegalArgumentException().isThrownBy(() -> StringCalculator.isValidOperator(operator));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 덧셈 - args : [{arguments}]")
     @CsvSource(value = {"1 + 2:3", "1 + 3 + 5:9", "-1 + 0 + 1 + 2:2"}, delimiter = ':')
     public void sum(String text, int expected) {
         int result = StringCalculator.calculate(text);
@@ -52,7 +52,7 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 뺄셈 - args : [{arguments}]")
     @CsvSource(value = {"1 - 2:-1", "1 - 3 - 5:-7", "10 - 2:8"}, delimiter = ':')
     public void sub(String text, int expected) {
         int result = StringCalculator.calculate(text);
@@ -60,7 +60,7 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 곱셈 - args : [{arguments}]")
     @CsvSource(value = {"1 * 2:2", "1 * 3 * 5:15", "-1 * 0 * 1 * 2:0"}, delimiter = ':')
     public void multi(String text, int expected) {
         int result = StringCalculator.calculate(text);
@@ -68,7 +68,7 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 나눗셈 - args : [{arguments}]")
     @CsvSource(value = {"2 / 1:2", "4 / 2:2", "4 / 2 / 2:1"}, delimiter = ':')
     public void divide(String text, int expected) {
         int result = StringCalculator.calculate(text);
@@ -76,7 +76,7 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index}. 사칙연산 혼합 - args : [{arguments}]")
     @CsvSource(value = {"2 + 3 * 4 / 2:10", "2 + 3 * 4 / 2 - 5:5"}, delimiter = ':')
     public void mixed_operator(String text, int expected) {
         int result = StringCalculator.calculate(text);
