dcc7eb99951b96fdee8087ac319e0993f5fcd202
yunhwan <pyh8618@gmail.com>
2022-10-15 21:07:45 +0900

    refactor : CorrectInfo에서 WinningResult로 당첨결과라는 뜻이 더 잘 드러나도록 변경

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -1,19 +1,22 @@
 package lotto.domain;
 
-import java.util.List;
 import java.util.Objects;
 
-public class CorrectInfo {
-    private int correctCount;
-    boolean bonus;
+public class WinningResult {
+    private final int correctCount;
+    private final boolean bonus;
 
-    public CorrectInfo(int correctNums) {
+    public WinningResult(int correctNums) {
         this(correctNums, false);
     }
 
-    public CorrectInfo(int correctCount, boolean bonus) {
+    public WinningResult(int correctCount, boolean bonus) {
         this.correctCount = correctCount;
         this.bonus = bonus;
+    }
+
+    public boolean hasSameCorrectCount(int count) {
+        return  this.correctCount == count;
     }
 
     public int getCorrectCount() {
@@ -32,7 +35,7 @@ public class CorrectInfo {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        CorrectInfo that = (CorrectInfo) o;
+        WinningResult that = (WinningResult) o;
         return correctCount == that.correctCount && bonus == that.bonus;
     }
 
