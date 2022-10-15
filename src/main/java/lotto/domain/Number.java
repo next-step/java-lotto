d3a7690a4069b743a2141b23ead910861baa7691
michol.lee <lovethefeel@gmail.com>
2022-10-16 00:07:26 +0900

    refactor: number 로직 정리

--- src/main/java/lotto/domain/Number.java
+++ src/main/java/lotto/domain/Number.java
@@ -60,11 +60,4 @@ public class Number {
 
         return Objects.hash(number);
     }
-
-    @Override
-    public String toString() {
-        return "Number{" +
-                "number=" + number +
-                '}';
-    }
 }
