a6b005b3d18973ac1e99b63e6def086c08441e88
michol.lee <lovethefeel@gmail.com>
2022-10-16 00:08:23 +0900

    refactor: 수동 로또 입력 로직 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -14,9 +16,8 @@ public class InputView {
         return SCANNER.next();
     }
 
-    public static String winnerNumberPrint() {
+    public static String winningLottoPrint() {
 
-        SCANNER.nextLine();
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return SCANNER.nextLine();
     }
@@ -25,5 +26,27 @@ public class InputView {
 
         System.out.println("보너스 볼을 입력해 주세요.");
         return SCANNER.next();
+    }
+
+    public static int purchaseManualLottosPrint() {
+
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        try {
+            return Integer.parseInt(SCANNER.next());
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
+    }
+
+    public static List<String> manualLottosPrint(final int countOfManualLotto) {
+
+        SCANNER.nextLine();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<String> lottos = new ArrayList<>();
+        for (int i = 0; i < countOfManualLotto; i++) {
+            String lotto = SCANNER.nextLine();
+            lottos.add(lotto);
+        }
+        return lottos;
     }
 }
