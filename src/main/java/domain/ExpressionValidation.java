0891997415c402623d1757fcae7f95b325e7dfaf
이건회 <rg970604@naver.com>
2022-10-17 05:24:27 +0900

    refactor : ExpressionValidation.java

--- src/main/java/domain/ExpressionValidation.java
+++ src/main/java/domain/ExpressionValidation.java
@@ -5,8 +5,16 @@ import java.util.List;
 
 public class ExpressionValidation {
 
+  private static final String DELIMITER = " ";
+  private static final String[] Operators = {"+", "-", "*", "/"};
+  private static final List<String> OperatorList = Arrays.asList(Operators);
+
+  private ExpressionValidation() {
+    throw new IllegalStateException("유틸리티 클래스를 인스턴스화할 수 없습니다!");
+  }
+
   public static void validateExpression(String expression) {
-    if (expression == null || expression.equals(" ")) {
+    if (expression == null || expression.equals(DELIMITER)) {
       throw new IllegalArgumentException("입력된 값이 없습니다");
     }
   }
@@ -22,9 +30,6 @@ public class ExpressionValidation {
       throw new IllegalArgumentException("존재하지 않는 연산자입니다");
     }
   }
-
-  private static final String[] Operators = {"+", "-", "*", "/"};
-  private static final List<String> OperatorList = Arrays.asList(Operators);
 
   public static boolean isOperator(final String operator) {
     return OperatorList.contains(operator);
