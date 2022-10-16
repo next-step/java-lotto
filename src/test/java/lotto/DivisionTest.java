a349fecb7f447b701f9dbb55e5931613a59eeab9
jimbaemon <jimbae@kakao.com>
2022-10-16 22:53:28 +0900

    test: 총 상금액과 수익률 계산 테스트 생성.

--- src/test/java/lotto/DivisionTest.java
+++ src/test/java/lotto/DivisionTest.java
@@ -3,6 +3,9 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DivisionTest {
@@ -14,6 +17,28 @@ class DivisionTest {
         Assertions.assertThat(Division.valueOf(5)).isEqualTo(Division.SECOND);
         Assertions.assertThat(Division.valueOf(6)).isEqualTo(Division.FIRST);
         Assertions.assertThat(Division.valueOf(0)).isEqualTo(Division.NO_DIVISION);
+    }
+
+    @Test
+    void 등수_목록_총_상금액_계산(){
+        List<Division> divisions = List.of(
+            Division.FOURTH,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION,
+            Division.NO_DIVISION
+        );
+
+        Assertions.assertThat(Division.sumPrize(divisions)).isEqualTo(BigDecimal.valueOf(5000));
     }
 
 }
--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -23,4 +23,28 @@ public class LottosTest {
         Assertions.assertThat(lottos.numberOfThirdDivision(winners)).isEqualTo(1L);
         Assertions.assertThat(lottos.numberOfFourthDivision(winners)).isEqualTo(1L);
     }
+
+    @Test
+    void 수익률_확인_테스트(){
+        Lottos lottos = new Lottos(List.of(
+            Lotto.print(() -> Number.of(8, 21, 23, 41, 42, 43)),
+            Lotto.print(() -> Number.of(3, 5, 11, 16, 32, 38)),
+            Lotto.print(() -> Number.of(7, 11, 16, 35, 36, 44)),
+            Lotto.print(() -> Number.of(1, 8, 11, 31, 41, 42)),
+            Lotto.print(() -> Number.of(13, 14, 16, 38, 42, 45)),
+            Lotto.print(() -> Number.of(7, 11, 30, 40, 42, 43)),
+            Lotto.print(() -> Number.of(2, 13, 22, 32, 38, 45)),
+            Lotto.print(() -> Number.of(23, 25, 33, 36, 39, 41)),
+            Lotto.print(() -> Number.of(1, 3, 5, 14, 22, 45)),
+            Lotto.print(() -> Number.of(5, 9, 38, 41, 43, 44)),
+            Lotto.print(() -> Number.of(2, 8, 9, 18, 19, 21)),
+            Lotto.print(() -> Number.of(13, 14, 18, 21, 23, 35)),
+            Lotto.print(() -> Number.of(17, 21, 29, 37, 42, 45)),
+            Lotto.print(() -> Number.of(3, 8, 27, 30, 35, 44))
+        ));
+
+        List<Number> winners = Number.of(1, 2, 3, 4, 5, 6);
+
+        Assertions.assertThat(lottos.calculateProfitRate(winners)).isEqualTo(0.35);
+    }
 }
