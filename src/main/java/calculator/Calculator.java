d4970f060c0a52e2c41f3321559c5655f7382b7a
Jackson Hong <jacksonhong0111@gmail.com>
2022-10-17 05:49:04 +0900

    [feat] add method 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,36 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculator {
+
+    private static final String ADD_INDICATOR = "+";
+
+    public int calculate(String input) {
+        String[] splitInput = input.split(" ");
+        List<Integer> ints = new ArrayList<>();
+        List<String> operators = new ArrayList<>();
+        Arrays.stream(splitInput).forEach(str -> {
+            if(str.equals(ADD_INDICATOR)) {
+                operators.add(str);
+                return;
+            }
+            ints.add(Integer.parseInt(str));
+        });
+        for (int i = 0; i < ints.size(); i++) {
+            String operator = operators.get(i);
+            if(operator.equals(ADD_INDICATOR)){
+                int result = add(ints.get(0), ints.get(1));
+                ints.set(0, result);
+                ints.remove(1);
+            }
+        }
+        return ints.get(0);
+    }
+
+    private int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @ParameterizedTest
+    @DisplayName("두개의 정수를 가진 문자열에 +가 있는 경우 덧셈 연산하는지 확인")
+    @ValueSource(strings = {"1 + 2"})
+    void testAddWith2Ints(String input){
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("세개의 정수를 가진 문자열에 +가 있는 경우 덧셈 연산하는지 확인")
+    @ValueSource(strings = {"1 + 2 + 3"})
+    void testAddWith3Ints(String input){
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(6);
+    }
+}
