829d2231f74035c2f6f578699cf6df4473af9ea5
jiokim <jiokim222@gmail.com>
2022-10-16 11:01:16 +0900

    feat(Operand): 피연산자

---
+++ src/main/java/StringCalculator/backend/domain/Operand.java
@@ -0,0 +1,41 @@
+package StringCalculator.backend.domain;
+
+import java.util.Objects;
+import java.util.function.BinaryOperator;
+
+public class Operand {
+
+    private int number;
+
+    public Operand(int number) {
+        checkPositive(number);
+        this.number = number;
+    }
+
+    private void checkPositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("음수는 입력될 수 없습니다.");
+        }
+    }
+
+    public int apply(BinaryOperator<Integer> formula, Operand other){
+        return formula.apply(this.number, other.number);
+    }
+
+    public boolean isZero(){
+        return this.number == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operand operand = (Operand) o;
+        return number == operand.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
