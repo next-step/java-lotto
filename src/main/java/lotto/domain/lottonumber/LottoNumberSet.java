a6432b291fbaaecf3daf57a046aca2bf00470388
Yunsik-Choi <choiys0311@gmail.com>
2022-10-16 18:37:10 +0900

    refactor : LottoNumberSet

--- src/main/java/lotto/domain/lottonumber/LottoNumberSet.java
+++ src/main/java/lotto/domain/lottonumber/LottoNumberSet.java
@@ -6,39 +6,41 @@ import java.util.stream.Collectors;
 public class LottoNumberSet {
 
     private final Set<LottoNumber> lottoNumberSet;
-    public static int LOTTO_SIZE = 6;
-    private static final String LOTTO_SIZE_EXCEPTION = "로또 번호는 " + LOTTO_SIZE + "개여야 합니다.";
+    private static final int LOTTO_SET_SIZE = 6;
+    private static final String LOTTONUMBERSET_DELIMITER = ", ";
 
-    public static LottoNumberSet createLottoNumberSet(int... numbers) {
+    private static final String LOTTO_SIZE_EXCEPTION = "로또 번호는 " + LOTTO_SET_SIZE + "개여야 합니다.";
+
+    public static LottoNumberSet createLottoNumberSet(final int... numbers) {
         return new LottoNumberSet(Arrays.stream(numbers)
                 .mapToObj(number -> new LottoNumber(number))
                 .collect(Collectors.toSet()));
     }
 
     public static LottoNumberSet createLottoNumberSet() {
-        return new LottoNumberSet(LottoNumberFactory.getNumbers(LOTTO_SIZE));
+        return new LottoNumberSet(LottoNumberFactory.getNumbers(LOTTO_SET_SIZE));
     }
 
-    public LottoNumberSet(Set<LottoNumber> lottoNumbers) {
-        if (lottoNumbers.size() != LOTTO_SIZE) {
+    public LottoNumberSet(final Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_SET_SIZE) {
             throw new IllegalArgumentException(LOTTO_SIZE_EXCEPTION);
         }
         this.lottoNumberSet = lottoNumbers;
     }
 
-    public int match(LottoNumberSet numbers) {
+    public int match(final LottoNumberSet numbers) {
         return lottoNumberSet.stream()
                 .filter(lottoNumber -> numbers.lottoNumberSet().contains(lottoNumber))
                 .collect(Collectors.toList())
                 .size();
     }
 
-    private Set<LottoNumber> lottoNumberSet() {
-        return Collections.unmodifiableSet(this.lottoNumberSet);
+    public boolean contain(final LottoNumber lottoNumber) {
+        return lottoNumberSet.contains(lottoNumber);
     }
 
-    public boolean contain(LottoNumber lottoNumber) {
-        return lottoNumberSet.contains(lottoNumber);
+    private Set<LottoNumber> lottoNumberSet() {
+        return Collections.unmodifiableSet(this.lottoNumberSet);
     }
 
     @Override
@@ -59,6 +61,6 @@ public class LottoNumberSet {
         return lottoNumberSet.stream()
                 .sorted(Comparator.comparingInt(LottoNumber::number))
                 .map(lottoNumber -> lottoNumber.toString())
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(LOTTONUMBERSET_DELIMITER));
     }
 }
