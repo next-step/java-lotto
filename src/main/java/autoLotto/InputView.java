3858425021dc5b9696520c69adc7558ca27fd681
nhs219 <heesun_z@naver.com>
2022-10-16 20:58:42 +0900

    feat : 결과 출력 및 입력 수정

--- src/main/java/autoLotto/InputView.java
+++ src/main/java/autoLotto/InputView.java
@@ -17,7 +17,7 @@ public class InputView {
     }
 
     public List<Integer> inputNumbers() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
 
         SCANNER.nextLine();
 
--- src/main/java/autoLotto/OutputView.java
+++ src/main/java/autoLotto/OutputView.java
@@ -9,8 +9,21 @@ public class OutputView {
     }
 
     public void outputLottos(Lottos lottos) {
-
         range(0, lottos.getLottosSize())
                  .forEach(i -> System.out.println(lottos.getLotto(i)));
+    }
+
+    public void outputResult(double result, int count) {
+        System.out.println("\n당첨 통계\n" +
+                "---------");
+        System.out.println("3개 일치 (5000원)-"+ count + "개");
+        System.out.println("4개 일치 (50000원)-"+ count + "개");
+        System.out.println("5개 일치 (1500000원)-"+ count + "개");
+        System.out.println("6개 일치 (2000000000원)-"+ count + "개");
+
+        System.out.print("총 수익률은 " + result +"입니다.");
+        if (result < 1) {
+            System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        }
     }
 }
