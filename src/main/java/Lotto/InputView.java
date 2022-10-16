c3580f1fc6ebb7e576f2cd706549b31e76922a2b
min16 <minji.na@cre.ma>
2022-10-16 18:34:30 +0900

    FIX - 누락된 InputView, ResultView 클래스 추가

---
+++ src/main/java/Lotto/InputView.java
@@ -0,0 +1,11 @@
+package Lotto;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputCalculation() {
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/Lotto/ResultView.java
@@ -0,0 +1,7 @@
+package Lotto;
+
+public class ResultView {
+    public static void print(int value) {
+        System.out.println(value);
+    }
+}
