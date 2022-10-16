3ea44bf30b6110311ce7b552b0334bbf0f9a574f
min16 <minji.na@cre.ma>
2022-10-16 19:49:09 +0900

    REFACTOR - `Calculator` 미리 숫자랑 연산자 목록을 분리해서 갖고 있자

--- src/main/java/Lotto/Calculator.java
+++ src/main/java/Lotto/Calculator.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     private static final String INVALID_INPUT = "유효하지 않은 입력값입니다.";
 
@@ -12,19 +15,50 @@ public class Calculator {
         if (valueText == null || valueText.isBlank()) throw new IllegalArgumentException(INVALID_INPUT);
 
         String[] values = valueText.split(" ");
-        int result = toNumber(values[0]);
-        for (int i = 1; i < values.length; i += 2) {
-            Operator operator = Operator.operatorOf(values[i]);
-            int number = toNumber(values[i + 1]);
+        List<Integer> numbers = splitNumbers(values);
+        List<Operator> operators = splitOperators(values);
 
-            result = operator.operate(result, number);
+        int result = numbers.get(0);
+        for (int i = 0; i < operators.size(); i ++) {
+            Operator operator = operators.get(i);
+            result = operator.operate(result, numbers.get(i + 1));
         }
 
         return result;
     }
 
+    private static List<Integer> splitNumbers(String[] values) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < values.length; i++) {
+            if (isOdd(i)) continue;
+            numbers.add(toNumber(values[i]));
+        }
+
+        return numbers;
+    }
+
+    private static List<Operator> splitOperators(String[] values) {
+        List<Operator> operators = new ArrayList<>();
+
+        for (int i = 0; i < values.length; i++) {
+            if (isEven(i)) continue;
+            operators.add(Operator.operatorOf(values[i]));
+        }
+
+        return operators;
+    }
+
     private static int toNumber(String value) {
         if (value.isBlank()) throw new IllegalArgumentException(INVALID_INPUT);
         return Integer.parseInt(value);
+    }
+
+    private static boolean isEven(int value) {
+        return value % 2 == 0;
+    }
+
+    private static boolean isOdd(int value) {
+        return value % 2 != 0;
     }
 }
