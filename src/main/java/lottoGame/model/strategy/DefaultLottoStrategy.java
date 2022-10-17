d62948ac892d6628f189d5fb9d442d06ae1bc27b
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-10-17 15:32:56 +0900

    fix: DefaultLottoStrategy 최소,최대 제한 숫자 상수로 작성.

--- src/main/java/lottoGame/model/strategy/DefaultLottoStrategy.java
+++ src/main/java/lottoGame/model/strategy/DefaultLottoStrategy.java
@@ -5,13 +5,16 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 public class DefaultLottoStrategy implements LottoStrategy {
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+    private static final int LIMIT = 6;
+
     @Override
     public List<Integer> createLottoNum() {
-        return ThreadLocalRandom
-                .current()
-                .ints(1, 45)
+        return ThreadLocalRandom.current()
+                .ints(MIN, MAX)
                 .distinct()
-                .limit(6)
+                .limit(LIMIT)
                 .boxed()
                 .sorted()
                 .collect(Collectors.toList());
