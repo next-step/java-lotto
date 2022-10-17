73de8977d48ed0f29cd3f715df2f351d2a915b71
kureungkureung <terry9611@naver.com>
2022-10-18 00:58:52 +0900

    feat: 연산자 덧셈 기능

---
+++ src/main/java/step1/Operator.java
@@ -0,0 +1,22 @@
+package step1;
+
+import java.util.function.BinaryOperator;
+
+public enum Operator {
+
+    PLUS("+", (a, b) -> a + b)
+    ;
+
+    private final String arithmetic;
+    private final BinaryOperator<Integer> formula;
+
+    Operator(String arithmetic, BinaryOperator<Integer> formula) {
+        this.arithmetic = arithmetic;
+        this.formula = formula;
+    }
+
+    public int executedResult(int operand, int anotherOperand) {
+        return formula.apply(operand, anotherOperand);
+    }
+
+}
