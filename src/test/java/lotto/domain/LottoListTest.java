31f2a855d49725d27aa9b5645ddbcb2a42f0f1c1
jwoh <dhwlsdn741@gmail.com>
2022-10-18 01:41:31 +0900

    test : 도메인 객체 테스트

---
+++ src/test/java/lotto/domain/LottoListTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoListTest {
+
+  private static final int COUNT = 3;
+
+  @Test
+  @DisplayName("지정 갯수만큼 로또가 생성되는지 확인한다.")
+  void createLottoListTest() {
+
+    //given
+    LottoList lottoList = LottoList.of(COUNT);
+
+    //then
+    assertThat(lottoList.getLottoList().size()).isEqualTo(COUNT);
+  }
+}
