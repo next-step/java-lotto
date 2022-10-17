8368ed2948e018bc99349b0366a58d10cebab63d
michol.lee <lovethefeel@gmail.com>
2022-10-17 15:21:45 +0900

    refactor: 로또 메인 로직 수정

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -4,18 +4,40 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.List;
+
 public class LottoApplication {
 
     public static void main(String[] args) {
 
         final Purchase purchase = Purchase.from(InputView.purchasePrint());
         final int countOfManualLotto = InputView.purchaseManualLottosPrint();
-        final ManualLottos manualLottos = ManualLottos.of(countOfManualLotto, InputView.manualLottosPrint(countOfManualLotto));
+        final ManualLottos manualLottos = getManualLottos(countOfManualLotto);
 
-        final Lottos lottos = new Lottos(AutoLottos.autoCreate(purchase.available(), manualLottos.count()));
-        ResultView.autoLottoNumberPrint(lottos);
+        final int countOfAutoLotto = purchase.available() - countOfManualLotto;
+        final Lottos lottos = getLottos(countOfAutoLotto, manualLottos);
+
         final WinningLotto winningLotto = WinningLotto.from(InputView.winningLottoPrint(), InputView.bonusNumberPrint());
+        result(lottos, winningLotto, purchase);
+    }
+
+    private static ManualLottos getManualLottos(final int countOfManualLotto) {
+
+        final List<String> manualLottoList = InputView.manualLottosPrint(countOfManualLotto);
+        ManualLottosValidator.validate(countOfManualLotto, manualLottoList.size());
+        final ManualLottos manualLottos = ManualLottos.from(manualLottoList);
+        return manualLottos;
+    }
+
+    private static Lottos getLottos(final int countOfAutoLotto, final ManualLottos manualLottos) {
+
+        final Lottos lottos = new Lottos(AutoLottos.autoCreate(countOfAutoLotto));
+        ResultView.autoLottoNumberPrint(lottos);
         lottos.add(manualLottos);
+        return lottos;
+    }
+
+    private static void result(final Lottos lottos, final WinningLotto winningLotto, final Purchase purchase) {
 
         final WinningResult winningResult = WinningResult.init();
         winningResult.collect(lottos.match(winningLotto));
