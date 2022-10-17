f58c602101bab4483e1b1a09d3f69d303995ed6b
boradol2 <boradol0902@gmail.com>
2022-10-18 00:13:54 +0900

    test(LottoAmountTest): 로또 가격에 대한 test
    1. 로또 가격을 입력받고 유효한지 판별하는 테스트
    2. 로또 가격에 대한 로또 개수를 반환하는 테스트

---
+++ src/test/java/lotto/domain/LottoAmountTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoAmountTest {
+    @Test
+    @DisplayName("로또 가격을 문자열로 입력받아도 로또가격은 생성 가능하다.")
+    void inputStringLottoAmount_isEqualToIntegerLottoAmount() {
+        assertThat(new LottoAmount("14000")).isEqualTo(new LottoAmount(14000));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"a", "1", "1234", "/"})
+    @DisplayName("로또 가격이 정수가 아닌 유효하지 않은 값으로 입력되었을 때 에러 발생한다.")
+    void inputStringLottoAmount_throwsIllegalArgumentException(String input) {
+        assertThatThrownBy(() -> new LottoAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 가격을 입력 받고 구입 받을 수 있는 로또의 개수를 알 수 있다.")
+    void calcLottoAmount_isEqualToLottoCount() {
+        assertThat(new LottoAmount("14000").calcLottoCount()).isEqualTo(14);
+        assertThat(new LottoAmount(14000).calcLottoCount()).isEqualTo(14);
+    }
+}
