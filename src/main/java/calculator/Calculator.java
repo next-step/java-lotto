871218a8cb5bbd6f1e3f0024e4a22340b4baf677
jackson hong <jacksonhong0111@gmail.com>
2022-10-17 16:19:18 +0900

    [feat] 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,25 +2,35 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
 
 public class Calculator {
 
     private static final String ADD_INDICATOR = "+";
     private static final String MINUS_INDICATOR = "-";
+    private static final String MULTIPLY_INDICATOR = "*";
+    private static final String DIVIDE_INDICATOR = "/";
 
     public int calculate(String input) {
+        if(input == null || input.equals("")) throw new IllegalArgumentException("input cannot be empty");
         String[] splitInput = input.split(" ");
         List<Integer> ints = new ArrayList<>();
         List<String> operators = new ArrayList<>();
         Arrays.stream(splitInput).forEach(str -> {
-            if(str.equals(ADD_INDICATOR) || str.equals(MINUS_INDICATOR)) {
-                operators.add(str);
+            if(str.matches("[0-9]+")){
+                ints.add(Integer.parseInt(str));
                 return;
             }
-            ints.add(Integer.parseInt(str));
+            operators.add(str);
         });
-        for (int i = 0; i < ints.size(); i++) {
+
+        operators.forEach(operator -> {
+            if(!OperatorType.getAllIndicators().contains(operator)) throw new IllegalArgumentException("wrong operator type");
+        });
+
+        for (int i = 0; i < operators.size(); i++) {
             String operator = operators.get(i);
             if(operator.equals(ADD_INDICATOR)){
                 int result = add(ints.get(0), ints.get(1));
@@ -29,6 +39,16 @@ public class Calculator {
             }
             if(operator.equals(MINUS_INDICATOR)){
                 int result = minus(ints.get(0), ints.get(1));
+                ints.set(0, result);
+                ints.remove(1);
+            }
+            if(operator.equals(MULTIPLY_INDICATOR)){
+                int result = multiply(ints.get(0), ints.get(1));
+                ints.set(0, result);
+                ints.remove(1);
+            }
+            if(operator.equals(DIVIDE_INDICATOR)){
+                int result = divide(ints.get(0), ints.get(1));
                 ints.set(0, result);
                 ints.remove(1);
             }
@@ -42,5 +62,13 @@ public class Calculator {
 
     private int add(int num1, int num2){
         return num1 + num2;
+    }
+
+    private int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+    private int divide(int num1, int num2){
+        return num1 / num2;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,10 +1,12 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
 
@@ -40,5 +42,63 @@ public class CalculatorTest {
     void testMinusWith3Ints(String input){
         int result = calculator.calculate(input);
         assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("두개의 정수를 가진 문자열에 *가 있는 경우 곱하기 연산하는지 확인")
+    @ValueSource(strings = {"1 * 2"})
+    void testMultiplyWith2Ints(String input){
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @DisplayName("세개의 정수를 가진 문자열에 *, -가 있는 경우 곱하기 연산하는지 확인")
+    @ValueSource(strings = {"1 * 2 - 3"})
+    void testMultiplyWith3Ints(String input){
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("두개의 정수를 가진 문자열에 /가 있는 경우 나누기 연산하는지 확인")
+    @ValueSource(strings = {"3 / 1"})
+    void testDivideWith2Ints(String input){
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("네개의 정수를 가진 문자열에 *, -, /가 있는 경우 나누기 연산하는지 확인")
+    @ValueSource(strings = {"1 * 2 / 2 - 1"})
+    void testDivideWith3Ints(String input){
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("null 이 입력값인 경우 Exception 확인")
+    void testNullInput(){
+        assertThatThrownBy(() -> calculator.calculate(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("input cannot be empty");
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력값이 공백인 경우 Exception 확인")
+    @ValueSource(strings = {""})
+    void testEmptyInput(String input){
+        assertThatThrownBy(() -> calculator.calculate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("input cannot be empty");
+    }
+
+    @ParameterizedTest
+    @DisplayName("연산자 중 사친연산 기호가 아닌 경우")
+    @ValueSource(strings = {"1 * 2 / 2 ! 1"})
+    void testWrongOperator(String input){
+        assertThatThrownBy(() -> calculator.calculate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("wrong operator type");
     }
 }
