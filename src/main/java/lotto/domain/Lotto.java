70fd39e50e837197477ed73fcb470f7db5673b9f
michol.lee <lovethefeel@gmail.com>
2022-10-17 13:50:47 +0900

    refactor: 코드 리뷰 반영 - 상수명 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,14 +1,54 @@
 package lotto.domain;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    private static final String DELIMITER = ", ";
 
     private final List<Number> lotto;
 
     public Lotto(final List<Number> lotto) {
 
         this.lotto = lotto;
+    }
+
+    public static Lotto from(final String input) {
+
+        final List<Number> lottoNumbers = split(input);
+        validateSize(lottoNumbers);
+
+        return new Lotto(lottoNumbers);
+    }
+
+    public static Lotto from(final List<Integer> lottoNumber) {
+
+        return new Lotto(convert(lottoNumber));
+    }
+
+    private static List<Number> split(final String input) {
+
+        return Arrays.stream(input.split(DELIMITER))
+                .sorted()
+                .map(number -> new Number(Integer.parseInt(number)))
+                .collect(Collectors.toList());
+    }
+
+    private static void validateSize(final List<Number> lottoNumbers) {
+
+        final Set<Number> numbers = new HashSet<>(lottoNumbers);
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("6개의 숫자를 입력해야 하며 중복 숫자는 입력할 수 없습니다.");
+        }
+    }
+
+    private static List<Number> convert(final List<Integer> numbers) {
+
+        return numbers.stream()
+                .map(Number::new)
+                .collect(Collectors.toList());
     }
 
     public List<Number> getLotto() {
