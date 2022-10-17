8ea362cd8b59976e8fcc29e620ca6a17ed835594
nhs219 <heesun_z@naver.com>
2022-10-18 08:29:41 +0900

    refactor : 1000원 상수로 선언

--- src/main/java/autoLotto/Quantity.java
+++ src/main/java/autoLotto/Quantity.java
@@ -1,15 +1,16 @@
 package autoLotto;
 
 public class Quantity {
+    private static final int DIVIDE_1000_WON = 1000;
 
     private final int quantity;
 
     public Quantity(final int amount) {
-        if (amount % 1000 != 0) {
+        if (amount % DIVIDE_1000_WON != 0) {
             throw new IllegalArgumentException("1000원 단위로 입력할 수 있습니다.");
         }
 
-        this.quantity = amount / 1000;
+        this.quantity = amount / DIVIDE_1000_WON;
     }
 
     public int getQuantity() {
