5621d82f5546e8df4b9f80d6315fb01830f7bd98
Yunsik-Choi <choiys0311@gmail.com>
2022-10-17 22:58:04 +0900

    test : ProfitRateTest loss

--- src/test/java/lotto/domain/ProfitRateTest.java
+++ src/test/java/lotto/domain/ProfitRateTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -16,5 +17,13 @@ public class ProfitRateTest {
     @Test
     void valid() {
         assertThatThrownBy(() -> new ProfitRate(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void loss() {
+        Assertions.assertAll(
+                () -> assertThat(new ProfitRate(0.9999).loss()).isTrue(),
+                () -> assertThat(new ProfitRate(1).loss()).isFalse()
+        );
     }
 }
