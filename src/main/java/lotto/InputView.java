1f493caebfebfd67f64aeee361426258a36e7eb1
Ye-Ji-Kim <kyeoji0128@naver.com>
2022-10-18 11:45:24 +0900

    fix: 입력 오류 수정 및 코드 리팩토링

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -10,12 +10,27 @@ public class InputView {
 
     public static long readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return SCANNER.nextLong();
+        return Long.parseLong(SCANNER.nextLine());
     }
 
-    public static List<Integer> readWinningNumbers() {
+    public static List<LottoNumber> readWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return Arrays.stream(SCANNER.nextLine().split(","))
+        String input = SCANNER.nextLine();
+
+        List<Integer> winningNumbers = splitLottoNumbers(input);
+        if (winningNumbers.size() != LottoTicket.LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("당첨 번호의 개수가 부족하거나 초과합니다.");
+        }
+
+        return winningNumbers.stream().map(LottoNumber::new).collect(Collectors.toList());
+    }
+
+    private static List<Integer> splitLottoNumbers(String numberString) {
+        if (numberString.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        return Arrays.stream(numberString.split(","))
             .map(String::trim)
             .map(Integer::parseInt)
             .collect(Collectors.toList());
