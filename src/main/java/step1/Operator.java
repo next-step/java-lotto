a87b25f6a296b5d40840a3a4395fa80abe06c04d
kureungkureung <terry9611@naver.com>
2022-10-18 22:53:27 +0900

    feat: 연산자 나눗셈 기능

--- src/main/java/step1/Operator.java
+++ src/main/java/step1/Operator.java
@@ -6,7 +6,9 @@ public enum Operator {
 
     PLUS("+", (a, b) -> a + b),
     MINUS("-", (a, b) -> a - b),
-    MULTIPLY("*", (a, b) -> a * b);
+    MULTIPLY("*", (a, b) -> a * b),
+    DIVIDE("-", (a, b) -> a / b)
+    ;
 
     private final String arithmetic;
     private final BinaryOperator<Integer> formula;
