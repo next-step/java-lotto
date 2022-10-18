485c7f0466f0780d54bd157abb2361c8c00ccf2b
o3ozzvb <ou1112@naver.com>
2022-10-18 14:25:57 +0900

    feat : 수동 구매 기능 구현

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -6,14 +6,25 @@ import lotto.domain.Lottos;
 import lotto.domain.RankMap;
 import lotto.generator.ManualNumberGenerator;
 
+import java.util.List;
+
 import static lotto.view.InputView.*;
 import static lotto.view.ResultView.*;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        Lottos lottos = new Lottos(inputPurchaseNumber());
-        printPurchaseNumber(lottos.lottosSize());
+        int autoLottoCnt = inputAutoPurchaseNumber();
+        Lottos lottos = new Lottos(autoLottoCnt);
+
+        List<Lotto> lottoList = lottos.getLottoList();
+        int manualLottoCnt = inputManualPurchaseNumber();
+        List<String> manualLottoNumber = inputManualLottoNumber(manualLottoCnt);
+        for (int i = 0; i < manualLottoCnt; i++) {
+            lottoList.add(new Lotto(new ManualNumberGenerator(manualLottoNumber.get(i))));
+        }
+
         printLottoNumbers(lottos);
+        printPurchaseNumber(lottos.lottosSize());
 
         String winningNumber = inputWinningNumber();
         int bonusNumber = inputBonusBall();
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,17 +1,39 @@
 package lotto.view;
 
+import com.sun.nio.sctp.SctpChannel;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    public static final String MSG_INPUT_PURCHASE_NUMBER = "구입금액을 입력해 주세요.";
+    public static final String MSG_INPUT_AUTO_PURCHASE_NUMBER = "구입금액을 입력해 주세요.";
+    public static final String MSG_INPUT_MANUAL_PURCHASE_NUMBER = "수동으로 구매할 로또 수를 입력해 주세요.";
     public static final String MSG_INPUT_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String MSG_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
+    public static final String MSG_INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요.";
 
-    public static int inputPurchaseNumber() {
-        System.out.println(MSG_INPUT_PURCHASE_NUMBER);
+    public static int inputAutoPurchaseNumber() {
+        System.out.println(MSG_INPUT_AUTO_PURCHASE_NUMBER);
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static int inputManualPurchaseNumber() {
+        System.out.println(MSG_INPUT_MANUAL_PURCHASE_NUMBER);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<String> inputManualLottoNumber(int cnt) {
+        List<String> lottoList = new ArrayList<>();
+        System.out.println(MSG_INPUT_MANUAL_LOTTO_NUMBERS);
+        for (int i = 0; i < cnt; i++) {
+            String lotto = scanner.nextLine();
+            lottoList.add(lotto);
+        }
+        return lottoList;
     }
 
     public static String inputWinningNumber() {
