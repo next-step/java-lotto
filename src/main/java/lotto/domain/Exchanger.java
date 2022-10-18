99615500aaa55b7e6550588fed162ed08a867ae0
sangwon lee <rollo@korda.im>
2022-10-18 09:27:19 +0900

    refactoring(2단계 - 로또(자동)) : 금액 교환 수정
    더 명확한 의미를 가지도록 수정

---
+++ src/main/java/lotto/domain/Exchanger.java
@@ -0,0 +1,61 @@
+package lotto.domain;
+
+import lotto.exception.InvalidInputException;
+
+import java.util.Objects;
+
+public class Exchanger {
+
+    private static final Integer LOTTO_PRICE = 1000;
+    private final Integer purchasePrice;
+    private Integer amount;
+
+    public Exchanger(String inputAmount) {
+        this.purchasePrice = convertToInt(validateInput(inputAmount));
+        this.amount = purchase();
+    }
+
+    private String validateInput(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new InvalidInputException();
+        }
+        return input;
+    }
+
+    private Integer convertToInt(String purchaseAmount) {
+        try {
+            return Integer.parseInt(purchaseAmount);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException();
+        }
+    }
+
+    private Integer purchase() {
+        if (this.purchasePrice % LOTTO_PRICE != 0) {
+            throw new InvalidInputException();
+        }
+        this.amount = this.purchasePrice / LOTTO_PRICE;
+        return this.amount;
+    }
+
+    public Integer getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Exchanger)) return false;
+        Exchanger exchanger = (Exchanger) o;
+        return Objects.equals(purchasePrice, exchanger.purchasePrice) && Objects.equals(amount, exchanger.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(purchasePrice, amount);
+    }
+}
