818deb6d3bd7b335ac989087623b27f343b89dca
nhs219 <heesun_z@naver.com>
2022-10-18 13:54:50 +0900

    test : matchQuantity 테스트 추가

--- src/test/java/autoLotto/LottosTest.java
+++ src/test/java/autoLotto/LottosTest.java
@@ -2,6 +2,9 @@ package autoLotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class LottosTest {
@@ -12,5 +15,20 @@ public class LottosTest {
         lottos.addLotto(new Lotto());
 
         assertThat(lottos.getLottosSize()).isEqualTo(1);
+    }
+
+    @Test
+    void matchQuantity() {
+        Lotto lotto = new Lotto(new Integer[]{1, 2, 3, 4, 5, 6});
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(5);
+        numbers.add(45);
+        numbers.add(10);
+        numbers.add(11);
+
+        assertThat(lotto.getMatchQuantity(numbers)).isEqualTo(3);
+
     }
 }
