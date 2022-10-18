4aacfdd26223c7c479eec3735c992e0ee341a90a
JunH <tjdtls690@gmail.com>
2022-10-19 02:02:57 +0900

    :art: refactor : 번호 리스트 캐싱 및 메서드 명 수정

--- src/main/java/lotto/step2/domain/factory/LottoNumbersFactory.java
+++ src/main/java/lotto/step2/domain/factory/LottoNumbersFactory.java
@@ -3,18 +3,17 @@ package lotto.step2.domain.factory;
 import lotto.step2.domain.LottoNumber;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoNumbersFactory {
     private static class LottoNumbersHolder {
-        private static final Map<Integer, LottoNumber> LOTTO_NUMBERS = lottoNumbersCreate1();
+        private static final Map<Integer, LottoNumber> LOTTO_NUMBERS = lottoNumbersCreate();
         private static final int LOTTO_MAX_NUMBER = 45;
         private static final int LOTTO_MIN_NUMBER = 1;
     
-        private static Map<Integer, LottoNumber> lottoNumbersCreate1() {
+        private static Map<Integer, LottoNumber> lottoNumbersCreate() {
             return IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
                     .boxed()
                     .collect(Collectors.toMap(lottoNumber -> lottoNumber, LottoNumber::new));
--- src/main/java/lotto/step2/domain/issuelottostrategy/AutoLottoIssueStrategy.java
+++ src/main/java/lotto/step2/domain/issuelottostrategy/AutoLottoIssueStrategy.java
@@ -12,10 +12,17 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AutoLottoIssueStrategy implements LottoIssueStrategy {
+    private static final List<Integer> NUMBERS = initNumbers();
     private static final int NUMBER_OF_DRAWS = 6;
     private static final int ZERO = 0;
     private static final int LOTTO_MAX_NUMBER = 45;
     private static final int LOTTO_MIN_NUMBER = 1;
+    
+    private static List<Integer> initNumbers() {
+        return IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+    }
     
     @Override
     public LottoTickets issueLottoTickets(PaymentInformation paymentInformation) {
@@ -24,23 +31,13 @@ public class AutoLottoIssueStrategy implements LottoIssueStrategy {
     
     private List<LottoTicket> issueLottoTicket(PaymentInformation paymentInformation) {
         return IntStream.range(ZERO, paymentInformation.countOfAutoLotto())
-                .mapToObj(ticketCount -> new LottoTicket(issueLotto(getShuffledLottoNumber())))
+                .mapToObj(ticketCount -> new LottoTicket(issueLotto(shuffle())))
                 .collect(Collectors.toList());
     }
     
-    private List<Integer> getShuffledLottoNumber() {
-        return shuffle(getInitLottoNumber());
-    }
-    
-    private List<Integer> getInitLottoNumber() {
-        return IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
-                .boxed()
-                .collect(Collectors.toList());
-    }
-    
-    private List<Integer> shuffle(List<Integer> randomLottoNumbers) {
-        Collections.shuffle(randomLottoNumbers);
-        return randomLottoNumbers;
+    private List<Integer> shuffle() {
+        Collections.shuffle(NUMBERS);
+        return NUMBERS;
     }
     
     private List<LottoNumber> issueLotto(List<Integer> shuffleLottoNumbers) {
