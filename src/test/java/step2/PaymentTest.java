9ea1630fbff7898aa2a5a4a92053a08f065f5a88
sienna <20200803@sungshin.ac.kr>
2022-10-18 13:13:27 +0900

    :white_check_mark:test : 1000원당 한장의 로또 티켓이 생긴다

---
+++ src/test/java/step2/PaymentTest.java
@@ -0,0 +1,17 @@
+package step2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PaymentTest {
+
+	@Test
+	@DisplayName("1000원당 한장의 로또 티켓이 생긴다")
+	void 구입금액테스트(){
+		int money = 14000;
+		Payment payment = new Payment();
+		Assertions.assertThat(payment.lottoAmount(money)).isEqualTo(14000/1000);
+	}
+
+}
