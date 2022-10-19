fd5b955ee2c8c9b6f1f5601865f69e313d9b4945
김재훈 <timewarp96@naver.com>
2022-10-20 00:26:35 +0900

    [feat] 로직 수정

--- src/main/java/stringcalculator/App.java
+++ src/main/java/stringcalculator/App.java
@@ -1,14 +1,12 @@
 package stringcalculator;
 
-import static stringcalculator.InputValidator.validateInput;
-
 public class App {
 
     private App() {
     }
 
     public static int calculate(String input) {
-        validateInput(input);
-        return new StringCalculator(input).calculate();
+        var calculator = new StringCalculator(new Input(input));
+        return calculator.calculate();
     }
 }
--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,55 +1,32 @@
 package stringcalculator;
 
 
-import stringcalculator.arithmetic.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static stringcalculator.InputValidator.validateArithmeticSupports;
-import static stringcalculator.InputValidator.validateNumeric;
+import stringcalculator.arithmetic.ArithmeticOperation;
+import stringcalculator.arithmetic.ArithmeticOperationFactory;
 
 public class StringCalculator {
 
-    private static final Map<String, ArithmeticOperation> OPERATION_MAP = new HashMap<>();
-    static {
-        OPERATION_MAP.put("+", new Add());
-        OPERATION_MAP.put("-", new Sub());
-        OPERATION_MAP.put("*", new Multiply());
-        OPERATION_MAP.put("/", new Divide());
-    }
-
+    private final ArithmeticOperationFactory arithmeticOperationFactory;
     private final String[] inputs;
 
-    public StringCalculator(String input) {
-        this.inputs = split("+ " + input);
+    public StringCalculator(Input input) {
+        this.arithmeticOperationFactory = new ArithmeticOperationFactory();
+        this.inputs = input.getValue();
     }
 
     public int calculate() {
-        int result = 0;
+        Number result = new Number(inputs[0]);
         for (int i = 1; i < inputs.length; i += 2) {
-            result = calculate(result, toInt(inputs[i]), getOperation(inputs[i - 1]));
+            result = calculate(result, new Number(inputs[i + 1]), getOperation(inputs[i]));
         }
-        return result;
+        return result.toInt();
     }
 
-    private int calculate(int firstNumber, int secondNumber, ArithmeticOperation operation) {
-        return operation.calculate(firstNumber, secondNumber);
+    private Number calculate(Number firstNumber, Number secondNumber, ArithmeticOperation operation) {
+        return firstNumber.calculate(secondNumber, operation);
     }
 
-    private ArithmeticOperation getOperation(String arithmetic) {
-        ArithmeticOperation operation = OPERATION_MAP.get(arithmetic);
-        validateArithmeticSupports(operation);
-        return operation;
-    }
-
-    private static String[] split(String source) {
-        return source.split(" ");
-    }
-
-
-    private static int toInt(String numberCandidate) {
-        validateNumeric(numberCandidate);
-        return Integer.parseInt(numberCandidate);
+    private ArithmeticOperation getOperation(String operationCandidate) {
+        return arithmeticOperationFactory.getOperation(operationCandidate);
     }
 }
