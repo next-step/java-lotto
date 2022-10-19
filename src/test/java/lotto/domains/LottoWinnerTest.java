2c3431a7ae77e360e6d620cbd620141ddd487d28
karon-ma <karon.ma@kakaocorp.com>
2022-10-19 15:05:29 +0900

    test: Add missing test for LottoWinner

--- src/test/java/lotto/domains/LottoWinnerTest.java
+++ src/test/java/lotto/domains/LottoWinnerTest.java
@@ -1,8 +1,9 @@
 package lotto.domains;
 
-import org.junit.jupiter.api.Test;
-
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
 
 public class LottoWinnerTest {
     @Test
@@ -12,5 +13,14 @@ public class LottoWinnerTest {
 
         assertThatThrownBy(() -> new LottoWinner(lotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testSuccessIsBonusNumber() {
+        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
+        LottoNumber bonusNumber = new LottoNumber(7);
+        LottoWinner winner = new LottoWinner(lotto, bonusNumber);
+
+        assertThat(winner.isBonus(bonusNumber)).isTrue();
     }
 }
