7fe8e79663b40d5a1c8b93b1da5d0b574bb74f4d
caoyu-dev <choyumin01@gmail.com>
2022-10-20 01:02:44 +0900

    [feat] Lotto 및 LottoTest 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,2 +1,31 @@
-package lotto.domain;public class Lotto {
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+
+    private List<Integer> numberList;
+    private Rank rank;
+
+    public List<Integer> getNumberList() {
+        return numberList;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Lotto purchase(List<Integer> numberList) {
+        this.numberList = numberList;
+        return this;
+    }
+
+    public Lotto matchRank(List<Integer> winnerNumberList) {
+        this.rank = Rank.getRank(
+                (int) numberList
+                        .stream()
+                        .filter(winnerNumberList::contains)
+                        .count());
+        return this;
+    }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LottoTest {
-  
+
+    @Test
+    void duplicate_Lotto_Tickets() {
+        Lotto lotto_one = new Lotto().purchase(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto_two = new Lotto().purchase(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto_one.getNumberList()).isEqualTo(lotto_two.getNumberList());
+    }
+
+    @Test
+    void compare_Lotto_Rank() {
+        List<Integer> numberList = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> WinnerNumberList = List.of(1, 2, 3, 4, 5, 7);
+
+        Lotto lotto = new Lotto().purchase(numberList);
+        Lotto rank = lotto.matchRank(WinnerNumberList);
+
+        assertThat(rank.getRank()).isEqualTo(Rank.SECOND);
+    }
+
 }
