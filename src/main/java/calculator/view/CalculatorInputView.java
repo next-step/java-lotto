e19db6907c3a924c7eebf43ed28e8fca2152355a
hasunzo <gktjsdl470@gmail.com>
2022-10-19 20:01:25 +0900

    refactor : view class 인스턴스화 방지

--- src/main/java/calculator/view/CalculatorInputView.java
+++ src/main/java/calculator/view/CalculatorInputView.java
@@ -6,6 +6,10 @@ public class CalculatorInputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    private CalculatorInputView() {
+
+    }
+
     public static String inputArithmeticExpression() {
         System.out.println("연산식을 입력해주세요.");
         return scanner.nextLine();
--- src/main/java/calculator/view/CalculatorResultView.java
+++ src/main/java/calculator/view/CalculatorResultView.java
@@ -2,6 +2,10 @@ package calculator.view;
 
 public class CalculatorResultView {
 
+    private CalculatorResultView() {
+
+    }
+
     public static void printResult(int result) {
         System.out.printf("연산 결과: %d", result);
     }
