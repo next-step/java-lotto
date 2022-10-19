950e2fcf5d0a0a8aecb70817cd7863bcd4ea0124
boradol2 <boradol0902@gmail.com>
2022-10-20 01:51:07 +0900

    test(LottoWinningTest): 로또 당첨 개수와 로또 당첨 금액 test
    1. 로또 당첨 개수 각 일치개수 모두 test
    2. 로또 총 당첨금액 test

---
+++ src/test/java/lotto/domain/LottoWinningTest.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoWinningTest {
+    private LottoWinning result;
+
+    @BeforeEach
+    void setUp() {
+        Lotto lotto1 = new Lotto("1,2,3,4,5,6");
+        Lotto lotto2 = new Lotto("10,11,12,13,14,15");
+        Lotto lotto3 = new Lotto("20,21,22,23,24,25");
+        Lotto lotto4 = new Lotto("1,2,3,23,24,25");
+        List<Lotto> lottos = new ArrayList<>(Arrays.asList(lotto1, lotto2, lotto3, lotto4));
+        Lotto lastWeekTicket = new Lotto("1,2,3,4,5,6");
+        LottoTickets lottoTickets = new LottoTickets(lottos);
+        result = lottoTickets.result(lastWeekTicket);
+    }
+
+    @Test
+    @DisplayName("지난주 당첨 로또랑 비교하여 일치하는 로또 숫의의 개수에 따라 각 로또티켓 개수를 확인한다.")
+    void prizeCountTest() {
+        assertThat(result.count(LottoPrize.SAME_THREE)).isEqualTo(1);
+        assertThat(result.count(LottoPrize.SAME_FOUR)).isEqualTo(0);
+        assertThat(result.count(LottoPrize.SAME_FIVE)).isEqualTo(0);
+        assertThat(result.count(LottoPrize.SAME_SIX)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("지난주 당첨 로또랑 비교하여 당첨된 것이 있어 2000005000 금액을 받음을 확인한다.")
+    void prizeMoneyTest() {
+        assertThat(result.sumPrizeMoney()).isEqualTo(2000005000);
+    }
+}
