65177e387aa0753ec3af1ffe30565cf22611a129
kureungkureung <terry9611@naver.com>
2022-10-21 02:03:08 +0900

    test: 표현식으로부터 연산식들 생성 기능

--- src/test/java/step1/ExpressionAsArithmeticExpressionTest.java
+++ src/test/java/step1/ExpressionAsArithmeticExpressionTest.java
@@ -3,6 +3,8 @@ package step1;
 import org.junit.jupiter.api.Test;
 import step1.numbers.ExpressionAsNumbers;
 import step1.numbers.Numbers;
+import step1.operators.ExpressionAsOperators;
+import step1.operators.Operators;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +16,13 @@ class ExpressionAsArithmeticExpressionTest {
         ExpressionAsArithmeticExpression sut = new ExpressionAsArithmeticExpression(expression);
 
         assertThat(sut.numbers()).isEqualTo(new Numbers(new ExpressionAsNumbers(expression)));
+    }
+
+    @Test
+    void 표현식을_연산식으로_추출할_수있다() {
+        String expression = "1 + 1";
+        ExpressionAsArithmeticExpression sut = new ExpressionAsArithmeticExpression(expression);
+
+        assertThat(sut.operators()).isEqualTo(new Operators(new ExpressionAsOperators(expression)));
     }
 }
