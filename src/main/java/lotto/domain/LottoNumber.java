dd9577e4440e99ea3e3711e9bb0e75c941f35d0b
boradol2 <boradol0902@gmail.com>
2022-10-21 01:27:41 +0900

    refactor(LottoNumber): validation 래퍼클래스에서 관리

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,23 +1,25 @@
 package lotto.domain;
 
-import lotto.expressions.LottoNumberExpression;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class LottoNumber {
-    protected static final List<LottoNumber> CACHE = new ArrayList<>();
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final String INVALID_LOTTO_NUMBER_BOUND_MESSAGE = "로또 번호는 1 ~ 45 의 숫자만 가능합니다.";
+    static final List<LottoNumber> CACHE = new ArrayList<>();
+
     static {
-        IntStream.rangeClosed(LottoNumberExpression.LOTTO_MIN_NUMBER, LottoNumberExpression.LOTTO_MAX_NUMBER)
+        IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
                 .forEach(number -> CACHE.add(new LottoNumber(number)));
     }
 
     private final int number;
 
     public LottoNumber(int number) {
-        LottoNumberExpression.validateNumber(number);
+        validateNumber(number);
         this.number = number;
     }
 
@@ -29,7 +31,26 @@ public class LottoNumber {
         return lottoNumber;
     }
 
+    public void validateNumber(int number) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBER_BOUND_MESSAGE);
+        }
+    }
+
     int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
