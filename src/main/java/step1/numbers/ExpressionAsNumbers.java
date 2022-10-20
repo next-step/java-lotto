443d144edd022917a038fbaf2810547990e619a2
kureungkureung <terry9611@naver.com>
2022-10-21 01:57:48 +0900

    feat: ExpressionAsNumbers 의 동등성 비교

--- src/main/java/step1/numbers/ExpressionAsNumbers.java
+++ src/main/java/step1/numbers/ExpressionAsNumbers.java
@@ -1,6 +1,7 @@
 package step1.numbers;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -29,5 +30,18 @@ public class ExpressionAsNumbers {
         if (expression == null || expression.equals("")) {
             throw new IllegalStateException("올바른 연산식이 아닙니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExpressionAsNumbers that = (ExpressionAsNumbers) o;
+        return Objects.equals(expression, that.expression);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expression);
     }
 }
