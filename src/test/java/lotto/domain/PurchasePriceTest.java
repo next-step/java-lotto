f7e4bab8479d86a67c930946a2a83d7c062fa6a2
kingkk31 <kingkk31@naver.com>
2022-10-20 21:05:11 +0900

    feat: 투입금액 빼기 기능 추가

--- src/test/java/lotto/domain/PurchasePriceTest.java
+++ src/test/java/lotto/domain/PurchasePriceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import lotto.domain.exception.NegativePriceException;
+import lotto.domain.exception.NullPurchasePriceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,30 @@ class PurchasePriceTest {
     void fail_to_init_purchase_price_by_negative_price() {
         assertThatExceptionOfType(NegativePriceException.class)
                 .isThrownBy(() -> new PurchasePrice(-1000));
+    }
+
+    @Test
+    @DisplayName("금액 빼기.")
+    void subtract_price() {
+        PurchasePrice purchasePrice = PurchasePrice.subtractPrice(new PurchasePrice(3000),
+                new PurchasePrice(1000));
+        assertThat(purchasePrice.getPrice())
+                .isEqualTo(2000);
+    }
+
+    @Test
+    @DisplayName("금액 빼기 시 null 이 있으면 예외 발생")
+    void fail_to_subtract_price_by_null_purchase_price() {
+        assertThatExceptionOfType(NullPurchasePriceException.class)
+                .isThrownBy(() -> PurchasePrice.subtractPrice(null, new PurchasePrice(1000)));
+    }
+
+    @Test
+    @DisplayName("금액 빼기 결과로 음수가 나오면 예외 발생")
+    void fail_to_subtract_price_when_result_is_negative_price() {
+        assertThatExceptionOfType(NegativePriceException.class)
+                .isThrownBy(() -> PurchasePrice.subtractPrice(new PurchasePrice(1000),
+                        new PurchasePrice(3000)));
     }
 
 }
