33978619507df541eba3a40b0f0b308bf6b2cc86
kureungkureung <terry9611@naver.com>
2022-10-21 02:25:33 +0900

    feat: 출력기

---
+++ src/main/java/step1/print/ConsoleOutput.java
@@ -0,0 +1,11 @@
+package step1.print;
+
+import step1.arithmeticExpression.ArithmeticExpression;
+
+public class ConsoleOutput {
+
+    public void output(ArithmeticExpression arithmeticExpression) {
+        System.out.println(arithmeticExpression.calculatedResult());
+    }
+
+}
