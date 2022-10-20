0869b92993942a50f826beac3495e002dc597ce8
wugatama <dnr7410@naver.com>
2022-10-20 13:11:16 +0900

    test: 로또 결과

---
+++ src/test/java/step3/ResultTest.java
@@ -0,0 +1,28 @@
+package step3;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+	@Test
+	@DisplayName("로또 결과 확인")
+	void results() {
+		Set<Integer> set1 = Set.of(1, 2, 3, 4, 5, 6);
+		Set<Integer> set2 = Set.of(7, 8, 9, 10, 11, 12);
+		Set<Integer> set3 = Set.of(7, 8, 9, 10, 11, 12);
+		Result result = new Result(List.of(set1, set2, set3));
+		Map<Operator, Integer> map = result.results(Set.of(1, 2, 3, 4, 5, 6));
+
+		assertThat(map.get(Operator.SIX)).isEqualTo(1);
+		assertThat(map.get(Operator.FIVE)).isEqualTo(0);
+	}
+}
