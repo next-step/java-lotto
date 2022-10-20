ceb8f6063a7cdce01d5ed5e8b5081e11317d1c44
kingkk31 <kingkk31@naver.com>
2022-10-20 21:04:30 +0900

    feat: 티켓 박스 합치기 기능 추가

--- src/test/java/lotto/domain/TicketBoxTest.java
+++ src/test/java/lotto/domain/TicketBoxTest.java
@@ -8,8 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import lotto.domain.exception.InvalidTicketsException;
+import lotto.domain.exception.NullTicketBoxException;
 import lotto.domain.number.Ticket;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -34,12 +36,27 @@ class TicketBoxTest {
                 .isThrownBy(() -> new TicketBox(tickets));
     }
 
+    @ParameterizedTest
+    @MethodSource("mergeTicketBoxes")
+    @DisplayName("티켓박스 두개를 하나로 합치기.")
+    void merge_ticket_boxes(TicketBox ticketBox1, TicketBox ticketBox2, int expected) {
+        TicketBox mergeTicketBox = TicketBox.merge(ticketBox1, ticketBox2);
+        assertThat(mergeTicketBox.getSize()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("합칠 티켓박스 중 null 이 있으면 예외 발생.")
+    void fail_to_merge_ticket_boxes_by_null_box() {
+        assertThatExceptionOfType(NullTicketBoxException.class)
+                .isThrownBy(() -> TicketBox.merge(new TicketBox(new ArrayList<>()), null));
+    }
+
     private static Stream<Arguments> getInitTicketBoxTestArguments() {
         return Stream.of(
                 Arguments.arguments(
                         List.of(
-                                new Ticket(numbersToBalls(List.of(1, 2, 3, 4, 5, 6))),
-                                new Ticket(numbersToBalls(List.of(3, 5, 12, 14, 15, 17)))
+                                getTicket(List.of(1, 2, 3, 4, 5, 6)),
+                                getTicket(List.of(3, 5, 12, 14, 15, 17))
                         ),
                         2
                 ),
@@ -51,9 +68,33 @@ class TicketBoxTest {
     }
 
     private static Stream<Arguments> getInvalidTicketList() {
-        Ticket ticket = new Ticket(numbersToBalls(List.of(1, 2, 3, 4, 5, 6)));
+        Ticket ticket = getTicket(List.of(1, 2, 3, 4, 5, 6));
 
         return Stream.of(Arguments.arguments(List.of(ticket, ticket)));
+    }
+
+    private static Stream<Arguments> mergeTicketBoxes() {
+        return Stream.of(
+                Arguments.arguments(
+                        new TicketBox(List.of(getTicket(List.of(1, 2, 3, 4, 5, 6)))),
+                        new TicketBox(List.of(
+                                getTicket(List.of(6, 12, 23, 24, 35, 36)),
+                                getTicket(List.of(5, 7, 22, 34, 39, 45)),
+                                getTicket(List.of(3, 5, 12, 14, 15, 17)))),
+                        4
+                ),
+                Arguments.arguments(
+                        new TicketBox(new ArrayList<>()),
+                        new TicketBox(List.of(
+                                getTicket(List.of(6, 12, 23, 24, 35, 36)),
+                                getTicket(List.of(5, 7, 22, 34, 39, 45)))),
+                        2
+                )
+        );
+    }
+
+    private static Ticket getTicket(List<Integer> numbers) {
+        return new Ticket(numbersToBalls(numbers));
     }
 
 }
