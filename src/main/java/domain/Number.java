06f0ff23306771513ade8c637c849112710b98d5
youngsunwoo <dnaodtjs@gmail.com>
2022-10-20 13:29:52 +0900

    [cc-java-15] lotto - Number 클래스 equal hashcode 작성

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Number {
     int number;
 
@@ -20,5 +22,18 @@ public class Number {
 
     private boolean isIntegerNumber(String value) {
         return (Double.valueOf(value)) % 1 == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
