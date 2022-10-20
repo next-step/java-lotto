4eca4bbfb87843b5519c90f3774741ac7ce8f4ff
Yunsik-Choi <choiys0311@gmail.com>
2022-10-21 00:59:25 +0900

    fix : MoneyInput bufferedReader

--- src/main/java/lotto/view/money/MoneyInput.java
+++ src/main/java/lotto/view/money/MoneyInput.java
@@ -1,14 +1,15 @@
 package lotto.view.money;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class MoneyInput {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static long money() {
         try {
-            return scanner.nextLong();
+            return Long.parseLong(bufferedReader.readLine());
         } catch (Exception e) {
             MoneyOutput.moneyInputError();
         }
