c112dabebcc75e08111c24240110175d84d7bf73
kureungkureung <terry9611@naver.com>
2022-10-21 02:10:19 +0900

    test: 유효한 연산식 검증에 대한 예외처리

--- src/test/java/step1/ExpressionAsArithmeticExpressionTest.java
+++ src/test/java/step1/ExpressionAsArithmeticExpressionTest.java
@@ -1,12 +1,17 @@
 package step1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import step1.numbers.ExpressionAsNumbers;
 import step1.numbers.Numbers;
 import step1.operators.ExpressionAsOperators;
 import step1.operators.Operators;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ExpressionAsArithmeticExpressionTest {
 
@@ -24,5 +29,26 @@ class ExpressionAsArithmeticExpressionTest {
         ExpressionAsArithmeticExpression sut = new ExpressionAsArithmeticExpression(expression);
 
         assertThat(sut.operators()).isEqualTo(new Operators(new ExpressionAsOperators(expression)));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {
+            "1 +",
+            "+",
+            "1+1"
+    })
+    void 올바르지_않는_연산식일_경우_예외가_발생한다(String expression) {
+        ExpressionAsArithmeticExpression sut = new ExpressionAsArithmeticExpression(expression);
+
+        assertAll(
+                () -> assertThatThrownBy(() -> sut.numbers())
+                        .isInstanceOf(IllegalStateException.class)
+                        .hasMessage("유효한 연산식이 아닙니다."),
+
+                () -> assertThatThrownBy(() -> sut.operators())
+                        .isInstanceOf(IllegalStateException.class)
+                        .hasMessage("유효한 연산식이 아닙니다.")
+        );
     }
 }
