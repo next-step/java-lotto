36d2cf040896df7c231656a78a66ebe5d7428770
kingkk31 <kingkk31@naver.com>
2022-10-20 21:08:37 +0900

    feat: 수동 로또도 발급받게 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import java.util.ArrayList;
 import lotto.domain.AutomaticLottoMachine;
 import lotto.domain.LottoMachine;
 import lotto.domain.PurchasePrice;
 import lotto.domain.TicketBox;
+import lotto.domain.number.Ticket;
 import lotto.domain.number.WinningTicket;
 import lotto.domain.winner.WinningReport;
 import lotto.view.LottoInputView;
@@ -14,11 +16,33 @@ public class Lotto {
     public static void main(String[] args) {
         PurchasePrice purchasePrice = new PurchasePrice(LottoInputView.insertPurchasePrice());
 
-        LottoMachine lottoMachine = new AutomaticLottoMachine();
-        TicketBox ticketBox = lottoMachine.issueTickets(purchasePrice);
-        LottoResultView.printTicketCount(ticketBox.getSize());
-        LottoResultView.printTickets(ticketBox);
+        PurchasePrice manualLottoPurchasePrice = new PurchasePrice(Ticket.getPrice() * LottoInputView.insertManualLottoCount());
+        PurchasePrice automaticLottoPurchasePrice = PurchasePrice.subtractPrice(purchasePrice, manualLottoPurchasePrice);
 
+        LottoMachine manualLottoMachine = LottoInputView.insertManualLottoNumbers(manualLottoPurchasePrice);
+        LottoMachine automaticLottoMachine = new AutomaticLottoMachine();
+
+        TicketBox manualTicketBox = getManualTicketBox(manualLottoPurchasePrice, manualLottoMachine);
+        TicketBox automaticTicketBox = getManualTicketBox(automaticLottoPurchasePrice, automaticLottoMachine);
+        TicketBox ticketBox = TicketBox.merge(manualTicketBox, automaticTicketBox);
+
+        printTickets(manualTicketBox, automaticTicketBox, ticketBox);
+        printResult(purchasePrice, ticketBox);
+    }
+
+    private static TicketBox getManualTicketBox(PurchasePrice purchasePrice, LottoMachine lottoMachine) {
+        if (purchasePrice.getPrice() == 0) {
+            return new TicketBox(new ArrayList<>());
+        }
+        return lottoMachine.issueTickets(purchasePrice);
+    }
+
+    private static void printTickets(TicketBox manualTicketBox, TicketBox automaticTicketBox, TicketBox ticketBox) {
+        LottoResultView.printTicketCount(manualTicketBox.getSize(), automaticTicketBox.getSize());
+        LottoResultView.printTickets(ticketBox);
+    }
+
+    private static void printResult(PurchasePrice purchasePrice, TicketBox ticketBox) {
         WinningTicket winningNumbers = LottoInputView.insertWinnerNumbers();
         WinningReport winningReport = new WinningReport();
 
