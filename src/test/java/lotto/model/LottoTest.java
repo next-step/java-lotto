3fa441376b1d534d19d33747e00ecf9f40706fce
bella.n <bella.n@kakaocorp.com>
2022-10-21 00:11:55 +0900

    test: 코드 변경 건에 따른 테스트 코드 이동 및 수정

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import lotto.exception.InvalidLottoSizeException;
+import lotto.model.enumeration.Rank;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -24,9 +25,20 @@ class LottoTest {
 
         WinningLotto winningLotto = new WinningLotto(createLottoNumber(1, 13, 15, 17, 33, 45), new LottoNumber(40));
 
-        double result = winningLotto.matchCount(lottoNumbers);
+        Rank result = winningLotto.match(lottoNumbers);
 
-        assertThat(result).isEqualTo(5);
+        assertThat(result).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    public void 로또_번호_일치_보너스_볼_포함_검증() {
+        Lotto lottoNumbers = new Lotto(createLottoNumber(1, 13, 15, 17, 20, 45));
+
+        WinningLotto winningLotto = new WinningLotto(createLottoNumber(1, 13, 15, 17, 33, 45), new LottoNumber(20));
+
+        Rank result = winningLotto.match(lottoNumbers);
+
+        assertThat(result).isEqualTo(Rank.SECOND);
     }
 
     private List<LottoNumber> createLottoNumber(int... number) {
--- src/test/java/lotto/model/ProfitTest.java
+++ src/test/java/lotto/model/ProfitTest.java
@@ -3,28 +3,67 @@ package lotto.model;
 import lotto.model.enumeration.Rank;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static lotto.model.Profit.getReturnRate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProfitTest {
     @Test
-    public void 로또_당첨금_검증() {
-        Profit profit = new Profit(Map.of(Rank.FIRST, 2L, Rank.THIRD, 1L));
+    public void 로또_수익금_검증() {
+        Map<Rank, Long> lotteriesRank = Map.of(Rank.FIRST, 1L, Rank.THIRD, 1L);
 
-        long result = profit.getTotalWinningMoney();
+        double result = getReturnRate(lotteriesRank, 2001500000);
 
-        assertThat(result).isEqualTo(4001500000L);
+        assertThat(result).isEqualTo(1.0);
     }
 
     @Test
-    public void 로또_당첨금_없는_검증() {
-        Profit profit = new Profit(new HashMap<>());
+    public void 로또_수익금_없는_검증() {
+        Map<Rank, Long> lotteriesRank = new HashMap<>();
 
-        long result = profit.getTotalWinningMoney();
+        double result = getReturnRate(lotteriesRank, 14000);
 
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void 로또_당첨_검증() {
+        Lotteries lotteries = new Lotteries(
+                List.of(new Lotto(createLottoNumber(1, 2, 4, 24, 25, 45)),
+                        new Lotto(createLottoNumber(1, 2, 3, 25, 35, 44)))
+                , new ArrayList<>()
+        );
+
+        WinningLotto winningLotto = new WinningLotto(createLottoNumber(1, 2, 4, 24, 25, 45), new LottoNumber(44));
+        Profit profit = new Profit(winningLotto);
+
+        Map<Rank, Long> result = profit.getLotteriesRank(lotteries.getAllLotteries());
+
+        assertThat(result).isEqualTo(Map.of(Rank.FIRST, 1L, Rank.FIFTH, 1L));
+    }
+
+    @Test
+    public void 로또_당첨_보너스_볼_일치_검증() {
+        Lotteries lotteries = new Lotteries(
+                List.of(new Lotto(createLottoNumber(1, 2, 4, 24, 25, 45)),
+                        new Lotto(createLottoNumber(1, 2, 3, 25, 35, 44)))
+                , List.of(new Lotto(createLottoNumber(3, 4, 7, 22, 26, 43)))
+        );
+
+        WinningLotto winningLotto = new WinningLotto(createLottoNumber(1, 2, 4, 24, 25, 43), new LottoNumber(45));
+        Profit profit = new Profit(winningLotto);
+
+        Map<Rank, Long> result = profit.getLotteriesRank(lotteries.getAllLotteries());
+
+        assertThat(result).isEqualTo(Map.of(Rank.SECOND, 1L, Rank.FIFTH, 1L, Rank.MISS, 1L));
+    }
+
+    private List<LottoNumber> createLottoNumber(int... number) {
+        return Arrays.stream(number)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 
 }
