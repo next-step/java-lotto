d912f308d3c0b81a89fe08875159f09a2e0f96d1
kureungkureung <terry9611@naver.com>
2022-10-21 02:25:18 +0900

    feat: 입력기

---
+++ src/main/java/step1/print/ConsoleInput.java
@@ -0,0 +1,15 @@
+package step1.print;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleInput {
+
+    public String input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        return br.readLine();
+    }
+
+}
