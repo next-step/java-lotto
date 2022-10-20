09fc66a9c3ccc6d673f60f52c626a53d7d920d0a
오은경 <194765@shinsegae.com>
2022-10-20 14:06:52 +0900

    fix : Lotto 비교 메소드 equals()와 hashcode()로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,7 @@ import lotto.generator.NumberGenerator;
 import lotto.generator.RandomNumberGenerator;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -12,15 +13,13 @@ public class Lotto {
     public static final int MAX_MATCH_NUMBER = 6;
 
     private List<LottoNumber> lotto;
-    private NumberGenerator numberGenerator;
 
     public Lotto(NumberGenerator numberGenerator) {
         this.lotto = numberGenerator.generate();
-        this.numberGenerator = numberGenerator;
     }
 
     public int getSameNumberCount(Lotto winning) {
-        if (this.isEqual(winning)) return MAX_MATCH_NUMBER;
+        if (this.equals(winning)) return MAX_MATCH_NUMBER;
 
         List<LottoNumber> winningNumbers = winning.lotto;
 
@@ -34,11 +33,17 @@ public class Lotto {
         return this.lotto;
     }
 
-    public boolean isEqual(Lotto numbers) {
-        return this.lotto.containsAll(numbers.lotto);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto1 = (Lotto) o;
+        return Objects.equals(lotto, lotto1.lotto);
     }
 
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
+    @Override
+    public int hashCode() {
+        return Objects.hash(lotto);
     }
+
 }
