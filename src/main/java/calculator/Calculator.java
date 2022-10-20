bc3bff315f633c32b9ff3cd1dafd8e318b4dc03c
stan.ley <stan.ley@kakaocorp.com>
2022-10-21 00:12:55 +0900

    feat: 계산기 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,32 @@
+package calculator;
+
+public class Calculator {
+    private final String expr;
+    public Calculator(String expr) {
+        if (expr == null || expr.isBlank()) {
+            throw new IllegalArgumentException("null or empty string input");
+        }
+        this.expr = expr;
+    }
+
+    public int calculate() {
+        String[] splits = expr.split(" ");
+        int lhs = Integer.parseInt(splits[0]);
+        for (int i = 1; i < splits.length; i += 2) {
+            String op = splits[i];
+            int rhs = Integer.parseInt(splits[i + 1]);
+
+            if (op.equals("+")) {
+                lhs = lhs + rhs;
+            } else if (op.equals("-")) {
+                lhs = lhs - rhs;
+            } else if (op.equals("*")) {
+                lhs = lhs * rhs;
+            } else if (op.equals("/")) {
+                lhs = lhs / rhs;
+            }
+        }
+
+        return lhs;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class CalculatorTest {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "  "})
+    void test_empty_string(String value) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Calculator(value));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2,2","2 + 3,5", "3 - 2,1", "2 * 3,6", "8 / 3,2", "2 + 3 * 4 / 2,10"})
+    void test_calculate(String expr, int result) {
+        assertThat(new Calculator(expr).calculate()).isEqualTo(result);
+    }
+}
