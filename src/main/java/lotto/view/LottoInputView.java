23656f317b5f999283528fd0f24a6afa6f4ba88d
kingkk31 <kingkk31@naver.com>
2022-10-20 21:07:36 +0900

    feat: input view 에 수동 로또 추가

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -3,9 +3,13 @@ package lotto.view;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import lotto.domain.LottoMachine;
+import lotto.domain.ManualLottoMachine;
+import lotto.domain.PurchasePrice;
 import lotto.domain.exception.IntegerMismatchException;
 import lotto.domain.number.LottoBall;
 import lotto.domain.number.LottoBalls;
+import lotto.domain.number.Ticket;
 import lotto.domain.number.WinningTicket;
 
 public class LottoInputView {
@@ -24,16 +28,40 @@ public class LottoInputView {
 
     public static WinningTicket insertWinnerNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        LottoBalls lottoBalls = new LottoBalls(
-                Arrays.stream(insertData().split(DELIMITER))
-                        .map(LottoInputView::getIntData)
-                        .map(LottoBall::new)
-                        .collect(Collectors.toUnmodifiableList()));
+        LottoBalls lottoBalls = insertLottoBalls();
 
         System.out.println("보너스 볼을 입력해 주세요.");
         LottoBall bonusBall = new LottoBall(getIntData(insertData()));
 
         return new WinningTicket(lottoBalls, bonusBall);
+    }
+
+    public static int insertManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return getIntData(insertData());
+    }
+
+    public static LottoMachine insertManualLottoNumbers(PurchasePrice purchasePrice) {
+        ManualLottoMachine lottoMachine = new ManualLottoMachine();
+        if (purchasePrice.getPrice() == 0) {
+            return lottoMachine;
+        }
+
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        int count = lottoMachine.getTicketCount(purchasePrice, Ticket.getPrice());
+        for (int i = 0; i < count; i++) {
+            lottoMachine.markLottoBalls(insertLottoBalls());
+        }
+
+        return lottoMachine;
+    }
+
+    private static LottoBalls insertLottoBalls() {
+        return new LottoBalls(
+                Arrays.stream(insertData().split(DELIMITER))
+                        .map(LottoInputView::getIntData)
+                        .map(LottoBall::new)
+                        .collect(Collectors.toUnmodifiableList()));
     }
 
     private static int getIntData(String str) {
