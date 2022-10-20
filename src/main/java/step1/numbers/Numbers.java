2b3fd38bae4a24dfef070128b2f0ea492c83a128
kureungkureung <terry9611@naver.com>
2022-10-21 01:57:25 +0900

    feat: Numbers 의 동등성 비교

--- src/main/java/step1/numbers/Numbers.java
+++ src/main/java/step1/numbers/Numbers.java
@@ -3,6 +3,7 @@ package step1.numbers;
 import step1.operators.Operators;
 import step1.operators.operator.Operator;
 
+import java.util.Objects;
 import java.util.Queue;
 
 public class Numbers {
@@ -23,4 +24,16 @@ public class Numbers {
                 .orElse(0);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Numbers numbers = (Numbers) o;
+        return Objects.equals(expressionAsNumbers, numbers.expressionAsNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expressionAsNumbers);
+    }
 }
