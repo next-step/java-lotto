b59e701eff355119e5bdfd52276cbb6220e0a7fa
moonstal1506 <moonstal1506@gmail.com>
2022-10-20 11:01:46 +0900

    feat: 수동으로 구매할 로또 수를 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -21,6 +21,11 @@ public class InputView {
         return new Money(scanner.nextInt());
     }
 
+    public static int inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
     public static Lotto inputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return new Lotto(scanner.next());
