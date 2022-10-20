f8c4b65ba8665f0c0a2265d7e6f8c72a0028352a
sienna <20200803@sungshin.ac.kr>
2022-10-20 16:18:48 +0900

    :white_check_mark:test : 수익룰을 계산해준다 - 셋째자리 반올림

--- src/test/java/step2/PaymentTest.java
+++ src/test/java/step2/PaymentTest.java
@@ -1,6 +1,9 @@
 package step2;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.HashMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.exception.LackOfMoneyException;
@@ -10,18 +13,33 @@ public class PaymentTest {
 
 	@Test
 	@DisplayName("1000원당 한장의 로또 티켓이 생긴다")
-	void 구입금액테스트(){
+	void 구입금액테스트() {
 		int money = 14000;
 		Payment payment = new Payment();
-		Assertions.assertThat(payment.lottoAmount(money)).isEqualTo(14000/1000);
+		assertThat(payment.lottoAmount(money)).isEqualTo(14000 / 1000);
 	}
 
 	@Test
 	@DisplayName("1000원이하를 지불하면 예외 발생")
-	void 금액_부족_테스트(){
+	void 금액_부족_테스트() {
 		int money = 900;
 		Payment payment = new Payment();
-		Assertions.assertThatThrownBy(() -> payment.lottoAmount(money)).isInstanceOf(
+		assertThatThrownBy(() -> payment.lottoAmount(money)).isInstanceOf(
 			LackOfMoneyException.class);
+	}
+
+	@Test
+	@DisplayName("수익률을 계산해준다 - 반올림0")
+	void 수익률() {
+		int money = 14000;
+		Payment payment = new Payment();
+
+		HashMap<Integer, Integer> totalCountMap = new HashMap<>();
+		totalCountMap.put(3, 1);
+		totalCountMap.put(4, 0);
+		totalCountMap.put(5, 0);
+		totalCountMap.put(6, 0);
+
+		assertThat(payment.rateBenefit(money, totalCountMap)).isEqualTo("0.36");
 	}
 }
