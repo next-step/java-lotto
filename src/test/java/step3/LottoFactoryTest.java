e539c7f4e7298f3ebac0e218dad5eb5b762131fc
wugatama <dnr7410@naver.com>
2022-10-20 13:25:17 +0900

    chore: test 클래스 이름변경

--- src/test/java/step3/LottoFactoryTest.java
+++ src/test/java/step3/LottoFactoryTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LottoTest {
+public class LottoFactoryTest {
 	@Test
 	void create() {
 		LottoFactory lotto = new LottoFactory();
