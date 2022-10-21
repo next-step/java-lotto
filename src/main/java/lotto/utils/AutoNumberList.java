9cfe402ff6db3f033cf0595d71ad86c740347fb3
caoyu-dev <choyumin01@gmail.com>
2022-10-21 16:50:50 +0900

    [feat] 로또 번호를 자동 생성하는 유틸 생성

--- src/main/java/lotto/utils/AutoNumberList.java
+++ src/main/java/lotto/utils/AutoNumberList.java
@@ -2,30 +2,37 @@ package lotto.utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class AutoNumList {
+public class AutoNumberList {
 
-    public static List<Integer> makeNumList() {
+    private static final int START_LOTTO_NUMBER = 1;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int START_UNIT = 0;
+    private static final int END_UNIT = 6;
+
+    public static List<Integer> makeNumberList() {
         return sortNumber(getNumberList());
     }
 
     private static List<Integer> sortNumber(List<Integer> numberList) {
-        numberList.sort(Comparator.comparing(Integer::intValue));
-        return numberList;
+        List<Integer> sortNumber = numberList.stream()
+                .sorted(Integer::compareTo)
+                .collect(Collectors.toList());
+        return sortNumber;
     }
 
     private static List<Integer> getNumberList() {
         List<Integer> pickNumber = new ArrayList<>();
 
-        for (int i = 0; i < 45; i++) {
+        for (int i = START_LOTTO_NUMBER; i < END_LOTTO_NUMBER; i++) {
             pickNumber.add(i);
         }
         Collections.shuffle(pickNumber);
-        List<Integer> numberList = pickNumber.subList(0, 6);
+        List<Integer> numberList = pickNumber.subList(START_UNIT, END_UNIT);
+
         return numberList;
     }
-    
-    
+
 }
