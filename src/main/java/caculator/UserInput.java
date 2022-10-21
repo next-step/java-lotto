2b2a943f1c1faf4bcabd0d0e93b456e74f75a31e
gkfgran5037 <gkfgran5037@naver.com>
2022-10-21 17:09:36 +0900

    feat : 수식 체킹 로직

---
+++ src/main/java/caculator/UserInput.java
@@ -0,0 +1,49 @@
+package caculator;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInput {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public void checkBlank(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new IllegalArgumentException("공백은 허용되지 않습니다.");
+        }
+    }
+
+    public boolean checkFormula(List<String> values) {
+        checkValuesLength(values.size());
+        for (int i = 0; i < values.size(); i++) {
+            checkValues(i, values.get(i));
+        }
+        return true;
+    }
+
+    private void checkValuesLength(int valuesLength) {
+        if (valuesLength % 2 == 0) {
+            throw new IllegalArgumentException("수식이 맞는지 확인 바랍니다.");
+        }
+    }
+
+    private static void checkValues(int i, String value) {
+        if (i % 2 == 0 && !StringUtils.isNumeric(value)) {
+            throw new IllegalArgumentException("수식이 맞는지 확인 바랍니다.");
+        }
+        if (i % 2 == 1) {
+            checkSign(value);
+        }
+    }
+
+    private static void checkSign(String value) {
+        if (StringUtils.isNumeric(value)) {
+            throw new IllegalArgumentException("수식이 맞는지 확인 바랍니다.");
+        }
+        if (!ExpressionType.isSign(value)) {
+            throw new IllegalArgumentException("사칙 연산 기호만 입력 가능합니다.");
+        }
+    }
+}
