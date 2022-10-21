c070e10ce52d5f005f43371f66dcf60a1d056272
sienna <20200803@sungshin.ac.kr>
2022-10-22 00:25:12 +0900

    :recycle:refactor : 로또 생성기 리팩토링

--- src/test/java/step2/FixLottoFactory.java
+++ src/test/java/step2/FixLottoFactory.java
@@ -6,7 +6,7 @@ import step2.model.LottoFactory;
 
 public class FixLottoFactory implements LottoFactory {
 
-	public final static List<Integer> FIX_NUM_LIST = Arrays.asList(1,6,22,33,41,45);
+	private final static List<Integer> FIX_NUM_LIST = Arrays.asList(1,6,22,33,41,45);
 
 	@Override
 	public List<Integer> getLottoNum() {
