4dab9af08c7bfaed6a88dfe6e082b8e798476a15
이건회 <rg970604@naver.com>
2022-10-23 01:23:54 +0900

    feat : ResultView.java

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,9 +1,30 @@
 package view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResultView {
 
-  public static void printResult(int result){
-    System.out.println("정답은 " + result + " 입니다");
+  private static final int MIN_INDEX=3;
+  private static final int MAX_INDEX=6;
+
+  static Map<Integer,Integer> prizePerCount = new HashMap<>();
+
+  public static void showPrizeStatistics(Map<Integer,Integer> prizeMap,float revenueRatio){
+    System.out.println("당첨 통계");
+    System.out.println("---------");
+    initPrizePerCount();
+    for(int i=MIN_INDEX;i<=MAX_INDEX;i++){
+      System.out.println(i+"개 일치 ("+prizePerCount.get(i)+"원)-"+prizeMap.get(i)+"개");
+    }
+    System.out.println("총 수익률은"+revenueRatio+"입니다.");
+  }
+
+  private static void initPrizePerCount(){
+    prizePerCount.put(3,5000);
+    prizePerCount.put(4,50000);
+    prizePerCount.put(5,1500000);
+    prizePerCount.put(6,2000000000);
   }
 
 }
