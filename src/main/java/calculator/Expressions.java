1a537ca5b7b9bda60f3b55c767b9de42250309bd
0chil <0@chll.it>
2022-10-22 18:24:19 +0900

    feat(calculator): 특정한 연산기호만 허용한다

--- src/main/java/calculator/Expressions.java
+++ src/main/java/calculator/Expressions.java
@@ -1,12 +1,46 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Expressions {
+
+	public static final List<String> ALLOWED_OPERATORS = List.of("+", "-", "*", "/");
+	private static final Pattern OPERATOR_REGEX = Pattern.compile("\\D");
 
 	public Expressions(List<String> expressions) {
 		validateNotNull(expressions);
 		validateNotContainingBlank(expressions);
+
+		List<String> operators = getOperators(expressions);
+		validateOperators(operators);
+	}
+
+	private List<String> getOperators(List<String> expressions) {
+		List<String> operations = new ArrayList<>();
+		for (String expression : expressions) {
+			if (isOperator(expression)) {
+				operations.add(expression);
+			}
+		}
+		return operations;
+	}
+
+	private boolean isOperator(String expression) {
+		return OPERATOR_REGEX.matcher(expression).matches();
+	}
+
+	private void validateOperators(List<String> operators) {
+		for (String operator : operators) {
+			if (!isAllowedOperator(operator)) {
+				throw new IllegalArgumentException("허용되지 않은 사칙연산 기호입니다");
+			}
+		}
+	}
+
+	private boolean isAllowedOperator(String operator) {
+		return ALLOWED_OPERATORS.contains(operator);
 	}
 
 	private void validateNotNull(List<String> expressions) {
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
+	private static final String SINGLE_SPACE = " ";
+
 	@Test
 	@DisplayName("수식은 null일 수 없다")
 	void expressions_itself_is_not_null() {
@@ -20,5 +22,16 @@ public class CalculatorTest {
 	void expressions_is_not_containing_blank() {
 		List<String> expressions = List.of(" ", "  ");
 		assertThatThrownBy(() -> new Calculator(expressions)).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("사칙연산은 특정 기호만 허용한다")
+	void only_certain_operations_are_allowed() {
+		List<String> expressionsWithAllowedOperations = List.of("1 + 2 / 3 - 1 * 2".split(SINGLE_SPACE));
+		List<String> expressionsWithNotAllowedOperations = List.of("1 + 2 ^ 3 @ 4".split(SINGLE_SPACE));
+
+		assertThatNoException().isThrownBy(() -> new Calculator(expressionsWithAllowedOperations));
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> new Calculator(expressionsWithNotAllowedOperations));
 	}
 }
