772858c469ec3a05a39e946e59f2e4a8ccf3ee52
kureungkureung <terry9611@naver.com>
2022-10-22 15:29:59 +0900

    test: 계산시 연산자 검증 예외처리

--- src/test/java/step1/arithmeticExpression/numbers/NumbersTest.java
+++ src/test/java/step1/arithmeticExpression/numbers/NumbersTest.java
@@ -1,5 +1,6 @@
 package step1.arithmeticExpression.numbers;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import step1.arithmeticExpression.operators.ExpressionAsOperators;
 import step1.arithmeticExpression.operators.Operators;
@@ -15,5 +16,18 @@ class NumbersTest {
 
         Operators operators = new Operators(new ExpressionAsOperators(expression));
         assertThat(sut.calculatedResult(operators)).isEqualTo(4);
+    }
+
+    @Test
+    void 계산을_할_때_올바른_연산자가_아닐_경우_예외가_발생한다() {
+        String expression  = "1 + 2";
+        Numbers sut = new Numbers(new ExpressionAsNumbers(expression));
+
+        String anotherExpression = "1 / 2 + 3";
+        Operators operators = new Operators(new ExpressionAsOperators(anotherExpression));
+
+        Assertions.assertThatThrownBy(() -> sut.calculatedResult(operators))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효한 연산자가 아닙니다.");
     }
 }
