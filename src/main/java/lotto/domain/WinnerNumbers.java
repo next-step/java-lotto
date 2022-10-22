f9d68683a4356f0715407aa18a9a7e9aa934c016
jimbaemon <jimbae@kakao.com>
2022-10-22 12:53:16 +0900

    feat: 당첨번호 갯수 확인.

--- src/main/java/lotto/domain/WinnerNumbers.java
+++ src/main/java/lotto/domain/WinnerNumbers.java
@@ -5,12 +5,21 @@ import java.util.List;
 import java.util.Set;
 
 public class WinnerNumbers {
+    private final int NUMBER_OF_NUMBERS = 6;
+
     private final Set<Number> numbers;
     private final Number bonus;
 
     public WinnerNumbers(Set<Number> numbers, Number bonus) {
+        validate(numbers);
         this.numbers = numbers;
         this.bonus = bonus;
+    }
+
+    private void validate(Set<Number> numbers) {
+        if (numbers.size() != NUMBER_OF_NUMBERS) {
+            throw new IllegalArgumentException("당첨번호의 갯수는 6개 입니다.");
+        }
     }
 
     public int match(Set<Number> lottoNumbers) {
