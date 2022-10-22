364a25ca0deb55a86051fdfd9ca50bf84df2c491
stan.ley <stan.ley@kakaocorp.com>
2022-10-22 16:31:37 +0900

    feat: Main 구현

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,7 +1,11 @@
 package calculator;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        String expr = InputView.readExpression();
+        Calculator calculator = new Calculator(expr);
+        OutputView.printResult(calculator.calculate());
     }
 }
