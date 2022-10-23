1fe760f3f2ccb3fc378e03473fbf55f11ebb768d
boradol2 <boradol0902@gmail.com>
2022-10-24 03:32:11 +0900

    perf(Main): 수정된 로직으로 main 구성

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,25 +1,25 @@
 package lotto;
 
-import lotto.domain.Lotto;
 import lotto.domain.LottoAmount;
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoWinning;
-import lotto.view.InputView;
-import lotto.view.ResultView;
+import lotto.domain.WinningNumbers;
+
+import static lotto.view.InputView.*;
+import static lotto.view.ResultView.*;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        LottoAmount lottoAmount = new LottoAmount(InputView.askAmount());
+        LottoAmount lottoAmount = new LottoAmount(askAmount());
         int lottoCount = lottoAmount.calcLottoCount();
-        ResultView.showLottoCount(lottoCount);
+        showLottoCount(lottoCount);
 
         LottoTicket lottoTicket = new LottoTicket(lottoCount);
-        ResultView.showAutoLottoTicket(lottoTicket);
+        showAutoLottoTicket(lottoTicket);
 
-        Lotto winningLotto = new Lotto(InputView.askWinningLottoNumbers());
-
-        LottoWinning winning = lottoTicket.result(winningLotto);
-        ResultView.printWinning(winning);
-        ResultView.printWinningRate(winning, lottoAmount);
+        WinningNumbers winningNumbers = new WinningNumbers(askWinningLottoNumbers(), askBonusNumber());
+        LottoWinning winning = lottoTicket.result(winningNumbers);
+        printWinning(winning);
+        printWinningRate(winning, lottoAmount);
     }
 }
