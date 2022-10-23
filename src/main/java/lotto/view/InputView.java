92961a2f7bf30b97980c321faddae17f9da3c85e
boradol2 <boradol0902@gmail.com>
2022-10-24 03:14:28 +0900

    feat(InputView): 보너스 번호 입력받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public final class InputView {
     private static final String AMOUNT_QUESTION_MESSAGE = "구입금액을 입력해 주세요. (1장당 1000원 입니다.)";
     private static final String WINNING_NUMBERS_QUESTION_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
@@ -17,6 +18,11 @@ public final class InputView {
 
     public static String askWinningLottoNumbers() {
         System.out.println(WINNING_NUMBERS_QUESTION_MESSAGE);
+        return inputAnswer();
+    }
+
+    public static String askBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
         return inputAnswer();
     }
 
