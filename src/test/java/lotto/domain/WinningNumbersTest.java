c344c57f5206ab637acde732a0c1206774ac56fa
boradol2 <boradol0902@gmail.com>
2022-10-24 03:01:13 +0900

    test(LottoNumbers): 당첨번호  test
    1. 로또 번호와 중복 번호 있으면 에러 발생 test
    2. 보너스 번호 일치 여부 체크하여 일치하지 않으면 false test

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinningNumbersTest {
+    @Test
+    @DisplayName("먼저 뽑힌 당첨 번호 6개와 일치하는 보너스 번호가 뽑히면 에러 발생한다.")
+    void isDuplicatedTest() {
+        assertThatThrownBy(() -> new WinningNumbers("1,2,3,4,5,6", "6"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 중에서 로또 번호와 일치하는 것이 있으면 False 이다.")
+    void hasBonusTest() {
+        Lotto lotto = new Lotto("1, 2, 3, 4, 5, 7");
+        WinningNumbers winningNumbers = new WinningNumbers("1,2,3,4,5,6", "10");
+        assertThat(winningNumbers.hasBonus(lotto)).isFalse();
+    }
+}
