ed09ca50f3e67c7e69a70dce0e615d186914691d
kureungkureung <terry9611@naver.com>
2022-10-23 19:20:17 +0900

    test(LottoTicketTest): 로또 번호 출력 기능

--- src/test/java/step2/lotto/LottoTicketTest.java
+++ src/test/java/step2/lotto/LottoTicketTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTicketTest {
@@ -15,5 +16,16 @@ class LottoTicketTest {
         assertThatThrownBy(() -> LottoTicket.from(numbersGenerator))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 번호는 6개이어야 합니다.");
+    }
+
+    @Test
+    void 로또_번호를_출력할_수_있다() {
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
+
+        LottoTicket lottoTicket = LottoTicket.from(numbersGenerator);
+
+        OutputView<Integer> outputView = new OutputView.Fake<>();
+        assertThatCode(() -> lottoTicket.print(outputView))
+                .doesNotThrowAnyException();
     }
 }
