1683cc6bae60413a99e53dbad2cae49030674175
boradol2 <boradol0902@gmail.com>
2022-10-24 03:31:22 +0900

    feat(ResultView): 보너스 볼 일치 여부 로직 추가와 멘트 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -15,7 +15,9 @@ public class ResultView {
     private static final String NEXT_LINE = System.lineSeparator();
     private static final String COUNT_MESSAGE = "%d개를 구매했습니다." + NEXT_LINE;
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계" + NEXT_LINE + "--------------------";
-    private static final String LOTTO_MATCH_MESSAGE = "%d개 일치 (%d원) - %d개" + NEXT_LINE;
+    private static final String LOTTO_MATCH_MESSAGE = "%d개 일치";
+    private static final String BONUS_MATCH_MESSAGE = ", 보너스 볼 일치";
+    private static final String LOTTO_MATCH_RESULT_MESSAGE = " (%d원) - %d개" + NEXT_LINE;
     private static final String WINNING_RATE_MESSAGE = NEXT_LINE + "총 수익률은 %.2f 입니다. (기준은 1입니다. 1보다 크면 이득, 1보다 작으면 손해)";
 
     private ResultView() {
@@ -38,11 +40,19 @@ public class ResultView {
                 .filter(rank -> rank != LottoRank.MISS)
                 .forEach(rank -> {
                     Integer count = winning.count(rank);
-                    System.out.printf(LOTTO_MATCH_MESSAGE, rank.getCountOfMatch(), rank.getWinningMoney(), count);
+                    printWiningResult(count, rank);
                 });
     }
 
     public static void printWinningRate(LottoWinning winning, LottoAmount lottoAmount) {
         System.out.printf(WINNING_RATE_MESSAGE, lottoAmount.calcWinningRate(winning.sumWinningMoney()));
+    }
+
+    private static void printWiningResult(Integer count, LottoRank rank) {
+        System.out.printf(LOTTO_MATCH_MESSAGE, rank.getCountOfMatch());
+        if (rank == LottoRank.SECOND) {
+            System.out.printf(BONUS_MATCH_MESSAGE);
+        }
+        System.out.printf(LOTTO_MATCH_RESULT_MESSAGE, rank.getWinningMoney(), count);
     }
 }
