15e2577046d3c0c30fa1865fa3411d986318edd9
boradol2 <boradol0902@gmail.com>
2022-10-24 03:26:49 +0900

    test(LottoWinningTest): 로또 테스트 2,3등 로직 들어간 것으로 수정하여 반영.

--- src/test/java/lotto/domain/LottoWinningTest.java
+++ src/test/java/lotto/domain/LottoWinningTest.java
@@ -16,27 +16,29 @@ class LottoWinningTest {
     @BeforeEach
     void setUp() {
         Lotto lotto1 = new Lotto("1,2,3,4,5,6");
-        Lotto lotto2 = new Lotto("10,11,12,13,14,15");
-        Lotto lotto3 = new Lotto("20,21,22,23,24,25");
+        Lotto lotto2 = new Lotto("1,2,3,4,5,7");
+        Lotto lotto3 = new Lotto("1,2,3,4,5,8");
         Lotto lotto4 = new Lotto("1,2,3,23,24,25");
-        List<Lotto> lottos = new ArrayList<>(Arrays.asList(lotto1, lotto2, lotto3, lotto4));
-        Lotto lastWeekTicket = new Lotto("1,2,3,4,5,6");
+        Lotto lotto5 = new Lotto("10,11,12,13,14,15");
+        List<Lotto> lottos = new ArrayList<>(Arrays.asList(lotto1, lotto2, lotto3, lotto4, lotto5));
+        WinningNumbers winningLotto = new WinningNumbers("1,2,3,4,5,6", "7");
         LottoTicket lottoTicket = new LottoTicket(lottos);
-        result = lottoTicket.result(lastWeekTicket);
+        result = lottoTicket.result(winningLotto);
     }
 
     @Test
     @DisplayName("지난주 당첨 로또랑 비교하여 일치하는 로또 숫의의 개수에 따라 각 로또티켓 개수를 확인한다.")
     void prizeCountTest() {
         assertThat(result.count(LottoRank.FIRST)).isEqualTo(1);
-        assertThat(result.count(LottoRank.SECOND)).isEqualTo(0);
-        assertThat(result.count(LottoRank.THIRD)).isEqualTo(0);
-        assertThat(result.count(LottoRank.FOURTH)).isEqualTo(1);
+        assertThat(result.count(LottoRank.SECOND)).isEqualTo(1);
+        assertThat(result.count(LottoRank.THIRD)).isEqualTo(1);
+        assertThat(result.count(LottoRank.FOURTH)).isEqualTo(0);
+        assertThat(result.count(LottoRank.FIFTH)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("지난주 당첨 로또랑 비교하여 당첨된 것이 있어 2000005000 금액을 받음을 확인한다.")
     void prizeMoneyTest() {
-        assertThat(result.sumWinningMoney()).isEqualTo(2000005000);
+        assertThat(result.sumWinningMoney()).isEqualTo(2_031_505_000);
     }
 }
