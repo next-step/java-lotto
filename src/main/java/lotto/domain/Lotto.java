0cf5e1eb7f9eacc4bb6a24f98696e8059611a24d
Myunghwan Kim <samkim2626@gmail.com>
2022-10-23 20:12:36 +0900

    refactor: 로또 번호, 당첨 번호에 대한 새로운 도메인 클래스로 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,15 +5,16 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
-    private static final int LOTTO_NUMBER_COUNT = 6;
     private static final String DELIMITER_COMMA = ", ";
 
-    private final List<Integer> numbers;
+    private final LottoNumbers numbers;
+
+    public Lotto(LottoNumbers numbers) {
+        this.numbers = numbers;
+    }
 
     public Lotto(List<Integer> numbers) {
-        validateCount(numbers);
-        validateDistinct(numbers);
-        this.numbers = numbers;
+        this(new LottoNumbers(numbers));
     }
 
     public static Lotto createFrom(NumberGenerator numberGenerator) {
@@ -21,25 +22,7 @@ public class Lotto {
     }
 
     public List<Integer> getNumbers() {
-        return List.copyOf(this.numbers)
-            .stream()
-            .sorted()
-            .collect(Collectors.toList());
-    }
-
-    private void validateCount(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("로또 번호는 6개입니다.");
-        }
-    }
-
-    private void validateDistinct(List<Integer> numbers) {
-        long count = numbers.stream()
-            .distinct()
-            .count();
-        if (count != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
-        }
+        return this.numbers.getNumbers();
     }
 
     @Override
--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,42 +4,13 @@ import java.util.List;
 
 public class WinningNumbers {
 
-    private static final int WINNING_NUMBER_COUNT = 6;
-    private static final double MIN_NUMBER = 1;
-    private static final double MAX_NUMBER = 45;
+    private final LottoNumbers numbers;
 
-    private final List<Integer> numbers;
-
-    public WinningNumbers(List<Integer> numbers) {
-        validateCount(numbers);
-        validateDistinct(numbers);
-        validateLottoNumber(numbers);
+    public WinningNumbers(LottoNumbers numbers) {
         this.numbers = numbers;
     }
 
-    private void validateCount(List<Integer> numbers) {
-        if (numbers.size() != WINNING_NUMBER_COUNT) {
-            throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
-        }
-    }
-
-    private void validateDistinct(List<Integer> numbers) {
-        long count = numbers.stream()
-            .distinct()
-            .count();
-        if (count != WINNING_NUMBER_COUNT) {
-            throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
-        }
-    }
-
-    private void validateLottoNumber(List<Integer> numbers) {
-        if(containsNotLottoNumber(numbers)) {
-            throw new IllegalArgumentException("당첨 번호는 1~45 사이의 숫자여야 합니다.");
-        }
-    }
-
-    private boolean containsNotLottoNumber(List<Integer> numbers) {
-        return numbers.stream()
-            .anyMatch(number -> number < MIN_NUMBER || number > MAX_NUMBER);
+    public WinningNumbers(List<Integer> numbers) {
+        this(new LottoNumbers(numbers));
     }
 }
