9f93549e711630e6646be3764e6091a559f36073
kureungkureung <terry9611@naver.com>
2022-10-23 18:24:59 +0900

    feat(LottoTicket): 로또 번호 개수 검증

---
+++ src/main/java/step2/lotto/LottoTicket.java
@@ -0,0 +1,25 @@
+package step2.lotto;
+
+import java.util.Set;
+
+public class LottoTicket {
+
+    private final Set<Integer> numbers;
+
+    private LottoTicket(Set<Integer> numbers) {
+        verifyNumbersSize(numbers);
+
+        this.numbers = numbers;
+    }
+
+    private static void verifyNumbersSize(Set<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개이어야 합니다.");
+        }
+    }
+
+    public static LottoTicket from(NumbersGenerator numbersGenerator) {
+        return new LottoTicket(numbersGenerator.numbers());
+    }
+
+}
