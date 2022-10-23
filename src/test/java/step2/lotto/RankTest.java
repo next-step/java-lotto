f1fc11b72374ab827a84d9fb8f6574b4d702b064
kureungkureung <terry9611@naver.com>
2022-10-23 22:39:15 +0900

    test(RankTest): 불필요한 기능 제거

--- src/test/java/step2/lotto/RankTest.java
+++ src/test/java/step2/lotto/RankTest.java
@@ -1,32 +1,14 @@
 package step2.lotto;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RankTest {
-
-    @Test
-    void 일치하는_개수를_입력하면_상금을_알_수_있다() {
-        assertThat(Rank.prizeAmount(6)).isEqualTo(2_000_000_000);
-    }
-
-    @ParameterizedTest
-    @ValueSource(longs = {
-            2,
-            7
-    })
-    void 일치하는_개수가_3개_이상_6개_이하가_아닐경우_예외가_발생한다(long numberOfMatchingNumbers) {
-        assertThatThrownBy(() -> Rank.prizeAmount(numberOfMatchingNumbers))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("일치하는 개수가 3 ~ 6 개기어야 합니다.");
-    }
 
     @Test
     void 일치하는_개수를_통해_등급을_구할_수_있다() {
         assertThat(Rank.rank(6)).isEqualTo(Rank.FIRST);
     }
+
 }
