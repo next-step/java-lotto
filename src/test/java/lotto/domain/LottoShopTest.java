13e8055fff47b077468b531e4afbc9d35e49a7c0
jimbaemon <jimbae@kakao.com>
2022-10-24 20:24:55 +0900

    test: 수동 구입분이 입력가격보다 크면 에러 발생 테스트.

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -21,4 +21,12 @@ public class LottoShopTest {
         LottoShop shop = new LottoShop(lottoNumberStrategy, new Money(14000), 3);
         Assertions.assertThat(shop.numberOfPurchasingAuto()).isEqualTo(11);
     }
+
+    @Test
+    void 수동_구입분이_입력가격보다_크면_에러_발생(){
+        LottoNumberStrategy lottoNumberStrategy = () -> lotto.domain.Number.of(1, 2, 3, 4, 5, 6);
+        Assertions.assertThatThrownBy(() -> new LottoShop(lottoNumberStrategy, new Money(14000), 15))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("수동 구입분이 입력 금액을 초과 했습니다.");
+    }
 }
