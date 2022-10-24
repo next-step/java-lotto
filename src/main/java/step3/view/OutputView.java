931b78d7c3efeee769280e3c59e2f11b4baf7405
SuchanMyoung <aud248@naver.com>
2022-10-24 12:29:53 +0900

    [로또] 3단계 - 로또(2등) (#2655)
    * chore: Step2 준비
    
    * feat: 구입금액 입출력
    
    * feat: 로또 하나 생성 기능
    
    * refactor: 로또 네이밍, 캐싱
    
    * refactor: Lotto -> LottoNumber 로 역할 이동
    
    * feat: 구매 수만큼의 로또 생성 기능
    
    * chore: step3 준비
    
    * refactor: lottonumbers List에서 TreeSet으로 변경
    
    TreeSet은 중복값을 가지지 않고 오름차순 정렬되기 때문에 적절한 자료형으로 판단
    
    * refactor: Prize enum MISS 추가로 LottoResult indent 줄이기
    
    * feat: 2등 추가
    
    * feat: 보너스 번호 입출력 추가
    
    * refactor: 번호 일치, 보너스 볼 일치 파악을 LottoNumber 객체에게 책임 넘기기
    
    * refactor: 번호 일치, 보너스 볼 일치 파악을 LottoNumber 객체에게 책임 넘기기
    
    * test: 당첨 확인 테스트
    
    * refactor: int 로또번호를 LottoNumber 도메인으로 Wrapping
    
    * refactor: isBonusBallMatch 메서드명 변경
    
    * refactor: drawResult.remove(Prize.MISS) 뷰단으로 위치 이동
    
    * refactor: Prize valueOf 추상메서드로 리팩토링
    
    * refactor: LottoNumbers 정렬되도록 comparable 구현
    
    * refactor: Test DisplayName 도메인 언어로 수정
    
    * refactor: Prize BiFunction 으로 리팩토링

---
+++ src/main/java/step3/view/OutputView.java
@@ -0,0 +1,64 @@
+package step3.view;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import step3.domain.LottoNumber;
+import step3.domain.LottoNumbers;
+import step3.domain.Prize;
+import step3.domain.Ticket;
+
+public class OutputView {
+
+    public static void printPriceNotification() {
+        System.out.println("구매금액을 입력해 주세요");
+    }
+
+    public static void printPurchaseResult(final Ticket ticket) {
+        System.out.println(ticket.getTicketCount() + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumbers(final List<LottoNumbers> lottoNumbers) {
+        for (LottoNumbers lottoNumber : lottoNumbers) {
+            lottoNumber.getLottoNumbers()
+                .stream()
+                .forEach(number -> System.out.print(number.getLottoNumber() + " "));
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printWinningNumberNotification() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printDrawResult(final Map<Prize, Integer> drawLottoResult) {
+        System.out.println();
+        drawLottoResult.remove(Prize.MISS);
+        for (Prize prize : drawLottoResult.keySet()) {
+            printEachResult(drawLottoResult, prize);
+        }
+    }
+
+    private static void printEachResult(Map<Prize, Integer> drawLottoResult, Prize prize) {
+        int matchCount = prize.getCountOfMatch();
+        int reward = prize.getReward();
+
+        if (prize.equals(Prize.SECOND)) {
+            System.out.println(matchCount + "개 일치, 보너스 볼 일치(30000000원) -" + drawLottoResult.get(prize));
+            return;
+        }
+
+        System.out.println(matchCount + "개 일치 (" + reward + ")- " + drawLottoResult.get(prize));
+    }
+
+    public static void printGrossReturn(final Ticket ticket, final int totalIncome) {
+        final double purchasePrice = ticket.getPurchasePrice();
+        final double grossReturn = totalIncome / purchasePrice;
+        System.out.println("총 수익률은 " + String.format("%.2f", grossReturn) + "입니다.");
+    }
+
+    public static void printBonusBallNotification() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+    }
+}
