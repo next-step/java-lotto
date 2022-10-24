0834e1d300906c8258b8b75e8a577962f532306d
jimbaemon <jimbae@kakao.com>
2022-10-24 20:46:01 +0900

    fix: 수동 구매시 자동 구매 갯수가 줄어듬으로 테스트 수정.

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -10,7 +10,7 @@ public class LottoShopTest {
     @Test
     void 제공한_금액만큼_로또를_제공() {
         LottoNumberStrategy lottoNumberStrategy = () -> lotto.domain.Number.of(1, 2, 3, 4, 5, 6);
-        LottoShop shop = new LottoShop(lottoNumberStrategy, new Money(2000), 1);
+        LottoShop shop = new LottoShop(lottoNumberStrategy, new Money(2000), 0);
         Lottos lottos = shop.buy();
         Assertions.assertThat(lottos).isEqualTo(new Lottos(List.of(new Lotto(lotto.domain.Number.of(1, 2, 3, 4, 5, 6)), new Lotto(Number.of(1, 2, 3, 4, 5, 6)))));
     }
