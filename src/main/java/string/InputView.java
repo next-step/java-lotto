77eaeb0eba5aaec0f7608a434ecea8abfe2779cb
min16 <minji.na@cre.ma>
2022-10-24 17:21:58 +0900

    REFACTOR - InputView scanner을 상수로 정의

--- src/main/java/string/InputView.java
+++ src/main/java/string/InputView.java
@@ -3,9 +3,9 @@ package string;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String inputCalculation() {
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 }
