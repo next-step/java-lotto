4d83cdf9714792f686cddcdb08b030c459679528
leejihyung <nave369@naver.com>
2022-10-25 00:41:44 +0900

    feat : Parser Domain 작성

--- src/main/java/calculator/domain/Parser.java
+++ src/main/java/calculator/domain/Parser.java
@@ -1,2 +1,21 @@
-package calculator.domain;public class Parser {
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Parser {
+
+    public Queue<String> parseString(String input) {
+
+        if (input == null || input.equals("")) {
+            throw new IllegalArgumentException("공백값이 올 수 없습니다");
+        }
+
+        String[] strings = input.split(" ");
+
+        Queue<String> queue = new LinkedList<>();
+        Arrays.stream(strings).forEach(string -> queue.add(string));
+        return queue;
+    }
 }
