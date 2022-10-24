0c59054dceabb21c78619ad9846901879b8b516c
leejihyung <nave369@naver.com>
2022-10-25 00:40:19 +0900

    feat : Calculator Test 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,2 +1,84 @@
-package calculator;public class CalculatorTest {
+package calculator;
+
+import calculator.domain.Calculator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void 덧셈() {
+        Calculator calculator = new Calculator();
+        Queue<String> stringQueue = new LinkedList<>();
+
+        stringQueue.add("3");
+        stringQueue.add("+");
+        stringQueue.add("5");
+
+        int actualResult = calculator.calculate(stringQueue);
+        assertThat(actualResult).isEqualTo(6);
+    }
+
+    @Test
+    void 뺄셈() {
+        Calculator calculator = new Calculator();
+        Queue<String> stringQueue = new LinkedList<>();
+
+        stringQueue.add("3");
+        stringQueue.add("-");
+        stringQueue.add("5");
+
+        int actualResult = calculator.calculate(stringQueue);
+        assertThat(actualResult).isEqualTo(-2);
+    }
+
+    @Test
+    void 곱셈() {
+        Calculator calculator = new Calculator();
+        Queue<String> stringQueue = new LinkedList<>();
+
+        stringQueue.add("3");
+        stringQueue.add("*");
+        stringQueue.add("5");
+
+        int actualResult = calculator.calculate(stringQueue);
+        assertThat(actualResult).isEqualTo(15);
+    }
+
+    @Test
+    void 나눗셈() {
+        Calculator calculator = new Calculator();
+        Queue<String> stringQueue = new LinkedList<>();
+
+        stringQueue.add("3");
+        stringQueue.add("/");
+        stringQueue.add("5");
+
+        int actualResult = calculator.calculate(stringQueue);
+        assertThat(actualResult).isEqualTo(0);
+    }
+
+    @Test
+    void 복합연산() {
+        Calculator calculator = new Calculator();
+        Queue<String> stringQueue = new LinkedList<>();
+
+        stringQueue.add("2");
+        stringQueue.add("+");
+        stringQueue.add("3");
+        stringQueue.add("*");
+        stringQueue.add("4");
+        stringQueue.add("/");
+        stringQueue.add("2");
+
+        int actualResult = calculator.calculate(stringQueue);
+        assertThat(actualResult).isEqualTo(10);
+    }
+
+
 }
