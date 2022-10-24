057b8a1de0ef223b7472227690a5812e4753a680
jimbaemon <jimbae@kakao.com>
2022-10-24 20:01:23 +0900

    test: 입력금을 한번에 받도록 처리 테스트.

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -12,8 +12,8 @@ public class LottoShopTest {
     @Test
     void 제공한_금액만큼_로또를_제공() {
         LottoNumberStrategy lottoNumberStrategy = () -> lotto.domain.Number.of(1, 2, 3, 4, 5, 6);
-        LottoShop shop = new LottoShop(lottoNumberStrategy);
-        Lottos lottos = shop.buy(new Money(2000));
+        LottoShop shop = new LottoShop(lottoNumberStrategy, new Money(2000));
+        Lottos lottos = shop.buy();
         Assertions.assertThat(lottos).isEqualTo(new Lottos(List.of(new Lotto(lotto.domain.Number.of(1, 2, 3, 4, 5, 6)), new Lotto(Number.of(1, 2, 3, 4, 5, 6)))));
     }
 }
