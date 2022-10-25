eb97b5e9651ce90955431ced2fb65b097b1e51fb
isakchoe <isakchoe@naver.com>
2022-10-25 13:59:07 +0200

    Test: Calculator Test
    - 사칙연산 메서드 테스트 
    - 전체 계산 테스트

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,56 @@
+import domain.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class CalculatorTest {
+
+    @Test
+    @DisplayName("더하기 테스트")
+    void addTest() {
+        Calculator calculator = new Calculator(3);
+        calculator.add(1);
+        int result = calculator.printResult();
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void minusTest() {
+        Calculator calculator = new Calculator(3);
+        calculator.minus(2);
+        int result = calculator.printResult();
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("곱하기 테스트")
+    void multipleTest() {
+        Calculator calculator = new Calculator(3);
+        calculator.multiple(2);
+        int result = calculator.printResult();
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("나누기 테스트")
+    void divideTest() {
+        Calculator calculator = new Calculator(3);
+        calculator.divide(2);
+        int result = calculator.printResult();
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void calculateTest() {
+        String[] input = new String[] {"1", "+", "2", "*", "3"};
+
+        Calculator calculator = new Calculator(input);
+        calculator.calculateAll();
+        int result = calculator.printResult();
+        assertThat(result).isEqualTo(9);
+    }
+
+}
