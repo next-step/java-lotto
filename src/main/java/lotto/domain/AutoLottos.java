42532fc3d1f80d18a993df38eeed33982261fb8c
michol.lee <lovethefeel@gmail.com>
2022-10-26 01:24:16 +0900

    refactor: 코드 리뷰 반영 자동 로또 상수 및 번호 추출 로직 이동

--- src/main/java/lotto/domain/AutoLottos.java
+++ src/main/java/lotto/domain/AutoLottos.java
@@ -3,12 +3,9 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class AutoLottos {
 
-    public static final int LOTTO_INDEX_START = 0;
-    public static final int LOTTO_INDEX_END = 6;
     private static final List<Integer> LOTTO_NUMBERS = Number.range();
 
     private AutoLottos() {}
@@ -18,16 +15,8 @@ public class AutoLottos {
         final List<Lotto> autoLottos = new ArrayList<>();
         for (int i = 0; i < countOfAutoLotto; i++) {
             Collections.shuffle(LOTTO_NUMBERS);
-            autoLottos.add(Lotto.from(pick()));
+            autoLottos.add(Lotto.from(LOTTO_NUMBERS));
         }
         return autoLottos;
-    }
-
-    private static List<Integer> pick() {
-
-        return LOTTO_NUMBERS.subList(LOTTO_INDEX_START, LOTTO_INDEX_END)
-                .stream()
-                .sorted()
-                .collect(Collectors.toList());
     }
 }
