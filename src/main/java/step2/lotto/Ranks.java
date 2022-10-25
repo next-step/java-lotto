cc0d107a8934edd3b7fecf734cf8c43f8786bb6d
kureungkureung <terry9611@naver.com>
2022-10-25 22:29:50 +0900

    feat(Ranks): 로또 순위 통계 기능

--- src/main/java/step2/lotto/Ranks.java
+++ src/main/java/step2/lotto/Ranks.java
@@ -1,6 +1,9 @@
 package step2.lotto;
 
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Ranks {
 
@@ -19,6 +22,26 @@ public class Ranks {
     private double totalPrizeAmount() {
         return rankingAggregation.stream()
                 .reduce(0.0, (prizeAmount, rank) -> rank.addedPrizeAmount(prizeAmount), Double::sum);
+    }
+
+    public Map<Rank, Long> statisticsRanks() {
+        Map<Rank, Long> ranks = countedWinningRanks();
+        countNotWinningRanks(ranks);
+        return Map.copyOf(ranks);
+    }
+
+    private Map<Rank, Long> countedWinningRanks() {
+        return rankingAggregation.stream()
+                .filter(Rank::isNotNothing)
+                .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
+    }
+
+    private void countNotWinningRanks(Map<Rank, Long> ranks) {
+        EnumSet.allOf(Rank.class)
+                .stream()
+                .filter(Rank::isNotNothing)
+                .filter(rank -> !ranks.containsKey(rank))
+                .forEach(rank -> ranks.put(rank, 0L));
     }
 
 }
