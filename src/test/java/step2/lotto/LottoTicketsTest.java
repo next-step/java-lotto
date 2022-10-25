cf8ae8b478e501e053a2864194090716c13e3fd8
kureungkureung <terry9611@naver.com>
2022-10-25 22:38:37 +0900

    test(LottoTicketsTest): 불필요한 테스트 삭제

--- src/test/java/step2/lotto/LottoTicketsTest.java
+++ src/test/java/step2/lotto/LottoTicketsTest.java
@@ -2,7 +2,6 @@ package step2.lotto;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,29 +19,4 @@ class LottoTicketsTest {
         assertThat(lottoTickets.numberOfTickets()).isOne();
     }
 
-    @Test
-    void 로또의_수익률을_구할_수_있다() {
-        int numberOfTickets = 1;
-        int lottoPrice = 1000;
-        int totalPrice = lottoPrice * numberOfTickets;
-        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
-        LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, numbersGenerator).lottoTickets();
-
-        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
-        double firstPrizeAmount = 2_000_000_000;
-        assertThat(lottoTickets.yield(winningNumbers)).isEqualTo(String.format("%.2f", firstPrizeAmount / totalPrice));
-    }
-
-    @Test
-    void 로또의_당첨_통계를_구할_수_있다() {
-        int numberOfTickets = 1;
-        int lottoPrice = 1000;
-        int totalPrice = lottoPrice * numberOfTickets;
-        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
-
-        LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, numbersGenerator).lottoTickets();
-
-        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
-        assertThat(lottoTickets.ranks(winningNumbers)).containsExactly(Map.entry(Rank.FIRST, 1L));
-    }
 }
