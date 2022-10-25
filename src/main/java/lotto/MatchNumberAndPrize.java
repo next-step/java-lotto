24b83a8ea26bb5a7c35098aa999f2c0cb226a917
hasunzo <gktjsdl470@gmail.com>
2022-10-25 20:40:08 +0900

    refactor : prize 언더바 표시

--- src/main/java/lotto/MatchNumberAndPrize.java
+++ src/main/java/lotto/MatchNumberAndPrize.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public enum MatchNumberAndPrize {
     NON(0, 0L),
-    FOURTH(3, 5000L),
-    THIRD(4, 50000L),
-    SECOND(5, 1500000L),
-    FIRST(6, 2000000000L);
+    FOURTH(3, 5_000L),
+    THIRD(4, 50_000L),
+    SECOND(5, 1_500_000L),
+    FIRST(6, 2_000_000_000L);
 
     private final int matchNumber;
     private final Long prize;
