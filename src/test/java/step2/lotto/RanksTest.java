5e1bb502aec9bb2a6fb34040ff9ed2d7af60706f
kureungkureung <terry9611@naver.com>
2022-10-25 22:29:34 +0900

    test(RanksTest): 로또 순위 통계 기능

--- src/test/java/step2/lotto/RanksTest.java
+++ src/test/java/step2/lotto/RanksTest.java
@@ -3,6 +3,7 @@ package step2.lotto;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +17,21 @@ class RanksTest {
 
         int price = 1000;
         assertThat(ranks.yield()).isEqualTo( Rank.FOURTH.prizeAmount() / (rankingAggregation.size() * price));
+    }
+
+    @Test
+    void 통계를_구할_수_있다() {
+        List<Rank> rankingAggregation = List.of(Rank.FOURTH);
+
+        Ranks ranks = new Ranks(rankingAggregation);
+
+        Map<Rank, Long> statisticsRanks = Map.ofEntries(
+                Map.entry(Rank.FIRST, 0L),
+                Map.entry(Rank.SECOND, 0L),
+                Map.entry(Rank.THIRD, 0L),
+                Map.entry(Rank.FOURTH, 1L)
+        );
+        assertThat(ranks.statisticsRanks()).isEqualTo(statisticsRanks);
     }
 
 }
