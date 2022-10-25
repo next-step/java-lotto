345c97622c927b59b9fb4c76c3ca04bfe959fc70
JIWON KIM <kkjim1324@naver.com>
2022-10-26 02:19:57 +0900

    feat : 조건 및 결과 도출 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,26 @@
+import java.util.*;
+import java.util.stream.*;
+
 public class Calculator {
+    private List<String> values;
+
+    public Calculator() {
+        this("");
+    }
+
+    public Calculator(String input) {
+        inputEmptyCheck(input);
+
+        this.values = Stream.of(input.split(" "))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private void inputEmptyCheck(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("입력 문자열이 빈 값 입니다.");
+        }
+    }
+
     public int plus(int left, int right) {
         return left + right;
     }
@@ -12,6 +34,58 @@ public class Calculator {
     }
 
     public int divide(int left, int right) {
+        validateDivision(left, right);
+
         return left / right;
+    }
+
+    private void validateDivision(int left, int right) {
+        checkZero(right);
+        checkInteger(left, right);
+    }
+
+    private void checkZero(int right) {
+        if (right == 0) {
+            throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+        }
+    }
+
+    private void checkInteger(int left, int right) {
+        if (left % right != 0) {
+            throw new IllegalArgumentException("정수로 나누어지지 않습니다.");
+        }
+    }
+
+    public int result() {
+        while (this.values.size() != 1) {
+            initAndCalculate();
+        }
+
+        return Integer.parseInt(this.values.get(0));
+    }
+
+    private void initAndCalculate() {
+        int left = Integer.parseInt(this.values.remove(0));
+        String operator = this.values.remove(0);
+        int right = Integer.parseInt(this.values.remove(0));
+        int result = 0;
+
+        result = calculate(left, operator, right, result);
+
+        this.values.add(0, String.valueOf(result));
+    }
+
+    private int calculate(int left, String operator, int right, int result) {
+        if (operator.equals("+")) {
+            result = plus(left, right);
+        } else if (operator.equals("-")) {
+            result = minus(left, right);
+        } else if (operator.equals("*")) {
+            result = multiply(left, right);
+        } else if (operator.equals("/")) {
+            result = divide(left, right);
+        }
+
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,34 +1,69 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CalculatorTest {
 
     @Test
     void plus() {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator("1 + 2");
         int result = calculator.plus(1, 2);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void minus() {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator("3 - 1");
         int result = calculator.minus(3, 1);
         assertThat(result).isEqualTo(2);
     }
 
     @Test
     void multiply() {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator("3 * 2");
         int result = calculator.multiply(3, 2);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     void divide() {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator("6 / 2");
         int result = calculator.divide(6, 2);
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void divideIntegerCheck() {
+        assertThatThrownBy(() ->{
+            Calculator calculator = new Calculator("3 / 2");
+            int result = calculator.divide(3, 2);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void divideZeroCheck() {
+        assertThatThrownBy(() ->{
+            Calculator calculator = new Calculator("3 / 0");
+            int result = calculator.divide(3, 0);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyInput(String input) {
+        assertThatThrownBy(() ->{
+            Calculator calculator = new Calculator(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void calculate() {
+        Calculator calculator = new Calculator("1 + 2 * 4 / 2");
+        int result = calculator.result();
+        assertThat(result).isEqualTo(6);
     }
 }
