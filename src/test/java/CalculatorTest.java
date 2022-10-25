aa7905b37e4f9c110d8e1a86555785faa809c159
kkywalk2 <kkywalk2@gmail.com>
2022-10-25 23:48:56 +0900

    refactor: 테스트코드에 DisplayName 추가 및 잘못된 convention 변경

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -8,13 +9,15 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource(value = {"1 + 1:2", "2 - 1:1", "4 / 2:2", "3 * 3:9"}, delimiter = ':')
+    @DisplayName("기본 사칙연산을 정상적으로 수행한다.")
     public void calculationTest(String expression, int expected) {
         assertThat(calculateResult(expression)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    public void NullOrEmptyTest(String expression) {
+    @NullAndEmptySource
+    @DisplayName("null 또는 빈 문자열 값을 입력할 경우 예외가 발생한다.")
+    public void nullOrEmptyTest(String expression) {
         assertThatIllegalArgumentException().isThrownBy(() -> calculateResult(expression));
     }
 
