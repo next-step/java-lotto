3dd0c6c6a4653e4997543e791b4f0e072b9e4754
ChaeHyeon <cohys7@gmail.com>
2022-10-26 08:40:37 +0900

    refactor: add _ at number to read easy

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,10 +1,10 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FORTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FORTH(3, 5_000),
     NON_MATCH(0, 0);
 
     private final int count;
