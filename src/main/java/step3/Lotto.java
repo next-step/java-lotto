c8250f20881b8294ef3feb97b9197f8a477d912e
wugatama <dnr7410@naver.com>
2022-10-25 23:51:05 +0900

    refactor: 우승 번호 범위가 1 ~ 45가 아닐때 예외

--- src/main/java/step3/Lotto.java
+++ src/main/java/step3/Lotto.java
@@ -4,6 +4,8 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Lotto {
+	private static final int LOTTO_START = 1;
+	private static final int LOTTO_END = 45;
 
 	private Set<Integer> lotto;
 
@@ -18,9 +20,17 @@ public class Lotto {
 	private static Set<Integer> makeLotto(String[] inputs) {
 		Set<Integer> lotto = new TreeSet<>();
 		for (String input : inputs) {
-			lotto.add(Integer.parseInt(input));
+			lotto.add(checkInt(input));
 		}
 		return lotto;
+	}
+
+	private static Integer checkInt(String input) {
+		int change = Integer.parseInt(input);
+		if (change < LOTTO_START || LOTTO_END < change) {
+			throw new IllegalArgumentException(LOTTO_START + " ~ " + LOTTO_END + " 중복되지 않는 수를 입력해주세요");
+		}
+		return change;
 	}
 
 	public Set<Integer> getLotto() {
