2b03b6d2ff26255702fe5b5639ff66d80d99e628
sienna <20200803@sungshin.ac.kr>
2022-10-26 00:18:39 +0900

    :sparkles:feat : 맞은 개수를 ENUM으로 관리한다

---
+++ src/main/java/step2/model/Rank.java
@@ -0,0 +1,35 @@
+package step2.model;
+
+import java.util.Arrays;
+
+public enum Rank {
+	FIRST(6, 2_000_000_000),
+	SECOND(5, 30_000_000),
+	THIRD(5, 1_500_000),
+	FOURTH(4, 50_000),
+	FIFTH(3, 5_000),
+	MISS(0, 0);
+
+	private int countOfMatch;
+	private int winningMoney;
+
+	Rank(int countOfMatch, int winningMoney) {
+		this.countOfMatch = countOfMatch;
+		this.winningMoney = winningMoney;
+	}
+
+	public int getCountOfMatch() {
+		return countOfMatch;
+	}
+
+	public int getWinningMoney() {
+		return winningMoney;
+	}
+
+	public static Rank of(int countOfMatch) {
+		return Arrays.stream(Rank.values()).filter(
+			rank -> rank.getCountOfMatch() == countOfMatch).findFirst().orElse(MISS);
+
+	}
+
+}
