ba01dece358007d24777f5b2bb9ef4fb03a432df
kureungkureung <terry9611@naver.com>
2022-10-25 22:01:42 +0900

    test(RankTest): 꽝 판별

--- src/test/java/step2/lotto/RankTest.java
+++ src/test/java/step2/lotto/RankTest.java
@@ -3,6 +3,7 @@ package step2.lotto;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RankTest {
 
@@ -11,4 +12,8 @@ class RankTest {
         assertThat(Rank.rank(6)).isEqualTo(Rank.FIRST);
     }
 
+    @Test
+    void 꽝이_아닐_경우_참을_반환한다() {
+        assertTrue(Rank.FIRST.isNotNothing());
+    }
 }
