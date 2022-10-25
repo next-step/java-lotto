df190e8780ae125f3fe7e6c4c34f28e6522395a3
sienna <20200803@sungshin.ac.kr>
2022-10-25 15:32:57 +0900

    :sparkles:feat : 보너스 번호와 일치하는지 구한다

--- src/main/java/step2/model/WinningLotto.java
+++ src/main/java/step2/model/WinningLotto.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 public class WinningLotto {
 
+	private static final int SECOND_MATCH_NUM = 5;
 	private List<Integer> winNumList;
 	private int bonusNumber;
 
-	private WinningLotto(List<Integer> winNumList,int bonusNumber) {
+
+	private WinningLotto(List<Integer> winNumList, int bonusNumber) {
 		this.winNumList = winNumList;
 		this.bonusNumber = bonusNumber;
 	}
 
-	public static WinningLotto of(List<Integer> winNumList,int bonusNumber){
-		return new WinningLotto(winNumList,bonusNumber);
+	public static WinningLotto of(List<Integer> winNumList, int bonusNumber) {
+		return new WinningLotto(winNumList, bonusNumber);
 	}
 
 	public int countOfMatch(List<Integer> lotto) {
@@ -22,9 +24,14 @@ public class WinningLotto {
 			.count();
 	}
 
-	public boolean isBonusNumber(List<Integer> lotto) {
-		long matchOfBonus = lotto.stream().filter(s -> s.equals(bonusNumber)).count();
-		if(matchOfBonus > 0){
+	public boolean isBonus(int countLottoMatch, List<Integer> lotto) {
+		long countOfBonus = 0;
+
+		if (countLottoMatch == SECOND_MATCH_NUM) {
+			countOfBonus = lotto.stream().filter(s -> s.equals(bonusNumber)).count();
+		}
+
+		if (countOfBonus > 0) {
 			return true;
 		}
 		return false;
