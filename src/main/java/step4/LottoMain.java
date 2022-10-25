8d488c5320742dacd69145e99c23d09e7c2f9b3b
yeonsuyang <yys1004u@naver.com>
2022-10-26 00:24:12 +0900

    feat. 수동 로또 구매 추가

--- src/main/java/step4/LottoMain.java
+++ src/main/java/step4/LottoMain.java
@@ -1,11 +1,14 @@
 package step4;
 
+import step3.domian.Lotto;
 import step4.domian.LastWinner;
 import step4.domian.LottoResult;
 import step4.domian.LottoWallet;
 import step4.domian.Revenue;
 import step4.view.InputView;
 import step4.view.ResultView;
+
+import java.util.List;
 
 import static step4.domian.LottoMachine.autoLottos;
 
@@ -14,7 +17,8 @@ public class LottoMain {
 
     public static void main(String[] args) {
         int lottoNum = InputView.buyLotto() / LOTTO_PRICE;
-        ResultView.printLottoCount(lottoNum);
+        int manualNum = InputView.inputManualNum();
+        List<Lotto> manualLottos = InputView.inputManualLottos(manualNum);
 
         LottoWallet lottoWallet = autoLottos(lottoNum);
         ResultView.printLottoNum(lottoWallet);
--- src/main/java/step4/view/InputView.java
+++ src/main/java/step4/view/InputView.java
@@ -1,5 +1,8 @@
 package step4.view;
 
+import step3.domian.Lotto;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,6 +12,10 @@ public class InputView {
     public static final String BUY_TEXT = "구입금액을 입력해 주세요.";
     public static final String LAST_WINNER_TEXT = "지난 주 당첨 번호를 입력해 주세요";
     public static final String LAST_BONUS_TEXT = "보너스 볼을 입력해 주세요.";
+
+    public static final String MANUAL_NUM_TEXT = "수동으로 구매할 로또 수를 입력해 주세요.";
+
+    public static final String MANUAL_LOTTO_TEXT = "수동으로 구매할 번호를 입력해 주세요.";
     public static final Scanner sc = new Scanner(System.in);
 
     public static int buyLotto() {
@@ -16,6 +23,22 @@ public class InputView {
         return sc.nextInt();
     }
 
+    public static int inputManualNum() {
+        sc.nextLine();
+        System.out.println("\n" +MANUAL_NUM_TEXT);
+        return sc.nextInt();
+    }
+
+    public static List<Lotto> inputManualLottos(int num) {
+        sc.nextLine();
+        System.out.println("\n" + MANUAL_LOTTO_TEXT);
+
+        List<Lotto> manualLottos = new ArrayList<>();
+        for(int i=0; i<num;i++){
+            manualLottos.add(new Lotto(Arrays.stream(sc.nextLine().replace(" ", "").split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList())));
+        }
+        return manualLottos;
+    }
     public static List<Integer> inputLastWinner() {
         sc.nextLine();
         System.out.println("\n" + LAST_WINNER_TEXT);
