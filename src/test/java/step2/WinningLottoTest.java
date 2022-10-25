965f1a642cee710b49a1bcb306ac08ca69afec82
sienna <20200803@sungshin.ac.kr>
2022-10-25 13:33:44 +0900

    :white_checK_mark:test : 보너스 번호와 일치하는지 구한다

--- src/test/java/step2/WinningLottoTest.java
+++ src/test/java/step2/WinningLottoTest.java
@@ -15,8 +15,18 @@ public class WinningLottoTest {
 		List<Integer> winNum = Arrays.asList(1, 2, 3, 4, 5, 6);
 		WinningLotto lotteryWin = new WinningLotto(winNum);
 
-		Assertions.assertThat(lotteryWin.countMatch(Arrays.asList(1, 2, 22, 33, 41, 45)))
+		Assertions.assertThat(lotteryWin.countOfMatch(Arrays.asList(1, 2, 22, 33, 41, 45)))
 			.isEqualTo(2);
 	}
 
+	@Test
+	@DisplayName("보너스 번호와 일치하는지 구한다")
+	void match_bonus(){
+		List<Integer> winNum = Arrays.asList(1, 2, 3, 4, 5, 6);
+		int bonusNumber = 7;
+		WinningLotto lotteryWin = new WinningLotto(winNum,bonusNumber);
+
+		Assertions.assertThat(lotteryWin.isBonusNumber(Arrays.asList(1, 2, 22, 33, 41, 45)))
+			.isEqualTo(true);
+	}
 }
