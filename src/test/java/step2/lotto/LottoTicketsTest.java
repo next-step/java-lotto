33b301954c2e989dacde865c9aaf3d5f09ddaea9
kureungkureung <terry9611@naver.com>
2022-10-25 23:42:53 +0900

    test(LottoTicketsTest): 순위들 생성 기능

--- src/test/java/step2/lotto/LottoTicketsTest.java
+++ src/test/java/step2/lotto/LottoTicketsTest.java
@@ -2,6 +2,7 @@ package step2.lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,9 +15,22 @@ class LottoTicketsTest {
         int lottoPrice = 1000;
         int totalPrice = lottoPrice * numberOfTickets;
         NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
+
         LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, numbersGenerator).lottoTickets();
 
         assertThat(lottoTickets.numberOfTickets()).isOne();
     }
 
+    @Test
+    void 순위들을_생성할_수_있다() {
+        int numberOfTickets = 1;
+        int lottoPrice = 1000;
+        int totalPrice = lottoPrice * numberOfTickets;
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
+
+        LottoTickets lottoTickets = new LottoTicketsFactory(totalPrice, numbersGenerator).lottoTickets();
+
+        Set<Integer> winningNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        assertThat(lottoTickets.ranks(winningNumbers)).isEqualTo(new Ranks(List.of(Rank.FIRST)));
+    }
 }
