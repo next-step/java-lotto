1b6c6f3ae75ecd9d0848fb6394000dfa39ef01db
suchan <aud248@naver.com>
2022-10-26 13:41:54 +0900

    refactor: 로또번호 캐시 Integer -> LottoNumber

--- src/main/java/step4/domain/LottoNumberGenerator.java
+++ src/main/java/step4/domain/LottoNumberGenerator.java
@@ -1,7 +1,6 @@
 package step4.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -12,29 +11,28 @@ public class LottoNumberGenerator {
 
     private static final int MIN_LOTTO_RANGE = 1;
     private static final int MAX_LOTTO_RANGE = 45;
-    private static final List<Integer> lottoNumbersCache = new ArrayList<>();
+    private static final List<LottoNumber> lottoNumbersCache = new ArrayList<>();
     private static final int LOTTO_NUMBER_COUNT = 6;
 
     static {
         IntStream.rangeClosed(MIN_LOTTO_RANGE, MAX_LOTTO_RANGE)
-            .forEach(i -> lottoNumbersCache.add(i));
+            .forEach(number -> lottoNumbersCache.add(LottoNumber.from(number)));
     }
 
     private LottoNumberGenerator() {
     }
 
-    public static List<LottoNumber> generateAutoLottoNumber() {
+    public static Set<LottoNumber> generateAutoLottoNumber() {
         Collections.shuffle(lottoNumbersCache);
 
         return lottoNumbersCache.stream()
             .limit(LOTTO_NUMBER_COUNT)
-            .map(number -> LottoNumber.from(number))
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
     }
 
-    public static List<LottoNumber> generateManualLottoNumber(Set<Integer> manualNumbers) {
+    public static Set<LottoNumber> generateManualLottoNumber(Set<Integer> manualNumbers) {
         return manualNumbers.stream()
             .map(number -> LottoNumber.from(number))
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
     }
 }
