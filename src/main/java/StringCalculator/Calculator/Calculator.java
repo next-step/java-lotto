9711946340392c84a7529eb19344a75cf965ad8f
root.directory <root.directory@kakaocorp.com>
2022-10-26 22:29:54 +0900

    feat: Calculator 메서드를 BiFunction으로 변경

--- src/main/java/StringCalculator/Calculator/Calculator.java
+++ src/main/java/StringCalculator/Calculator/Calculator.java
@@ -1,20 +1,40 @@
 package StringCalculator.Calculator;
 
+import java.util.function.BiFunction;
+
 public class Calculator {
+    enum Operator {
+        ADD("+", Calculator.add),
+        SUBTRACT("-", Calculator.subtract),
+        MULTIPLY("*", Calculator.multiply),
+        DIVIDE("/", Calculator.divide)
+        ;
+        private final String sign;
+        private final BiFunction<Float, Float, Float> function;
 
-    public static int add(int number1, int number2) {
-        return number1 + number2;
+        Operator(String sign, BiFunction<Float, Float, Float> function) {
+            this.sign = sign;
+            this.function = function;
+        }
+
+        public String getSign() {
+            return sign;
+        }
+
+        public BiFunction<Float, Float, Float> getFunction() {
+            return function;
+        }
     }
 
-    public static int subtract(int number1, int number2) {
-        return number1 - number2;
-    }
+    public static BiFunction<Float, Float, Float> add =
+            (number1, number2) -> number1 + number2;
 
-    public static int multiply(int number1, int number2) {
-        return number1 * number2;
-    }
+    public static BiFunction<Float, Float, Float> subtract =
+            (number1, number2) -> number1 - number2;
 
-    public static float divide(int number1, int number2) {
-        return (float) number1 / number2;
-    }
+    public static BiFunction<Float, Float, Float> multiply =
+            (number1, number2) -> number1 * number2;
+
+    public static BiFunction<Float, Float, Float> divide =
+            (number1, number2) -> number1 / number2;
 }
--- src/test/java/StringCalculator/Calculator/CalculatorTest.java
+++ src/test/java/StringCalculator/Calculator/CalculatorTest.java
@@ -9,25 +9,25 @@ public class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource(value = {"1;1;2", "3;3;6", "0;4;4"}, delimiter = ';')
-    public void addTest(int num1, int num2, int result) {
-        assertThat(Calculator.add(num1, num2)).isEqualTo(result);
+    public void addTest(float num1, float num2, float result) {
+        assertThat(Calculator.add.apply(num1, num2)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1;1;0", "12;3;9", "0;4;-4"}, delimiter = ';')
-    public void subtractTest(int num1, int num2, int result) {
-        assertThat(Calculator.subtract(num1, num2)).isEqualTo(result);
+    public void subtractTest(float num1, float num2, float result) {
+        assertThat(Calculator.subtract.apply(num1, num2)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1;1;1", "4;3;12", "0;4;0"}, delimiter = ';')
-    public void multiplyTest(int num1, int num2, int result) {
-        assertThat(Calculator.multiply(num1, num2)).isEqualTo(result);
+    public void multiplyTest(float num1, float num2, int result) {
+        assertThat(Calculator.multiply.apply(num1, num2)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1;1;1", "27;3;9", "0;4;0", "1;4;0.25"}, delimiter = ';')
-    public void divideTest(int num1, int num2, float result) {
-        assertThat(Calculator.divide(num1, num2)).isEqualTo(result);
+    public void divideTest(float num1, float num2, float result) {
+        assertThat(Calculator.divide.apply(num1, num2)).isEqualTo(result);
     }
 }
