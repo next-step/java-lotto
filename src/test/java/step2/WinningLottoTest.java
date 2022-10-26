54fa4e9a2d7a8df10671e211ad65fb5d35cc29dd
sienna <20200803@sungshin.ac.kr>
2022-10-26 17:46:17 +0900

    :white_check_mark:test : 보너스 번호와 로또번호는 같을 수 없다

--- src/test/java/step2/WinningLottoTest.java
+++ src/test/java/step2/WinningLottoTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.exception.IllegalNumberException;
 import step2.model.WinningLotto;
 
 public class WinningLottoTest {
@@ -14,7 +15,7 @@ public class WinningLottoTest {
 	void match_num() {
 		List<Integer> winNum = Arrays.asList(1, 2, 3, 4, 5, 6);
 		int bonusNumber = 7;
-		WinningLotto lotteryWin = WinningLotto.of(winNum,bonusNumber);
+		WinningLotto lotteryWin = WinningLotto.of(winNum, bonusNumber);
 
 		Assertions.assertThat(lotteryWin.countOfMatch(Arrays.asList(1, 2, 22, 33, 41, 45)))
 			.isEqualTo(2);
@@ -22,12 +23,21 @@ public class WinningLottoTest {
 
 	@Test
 	@DisplayName("보너스 번호와 일치하는지 구한다")
-	void match_bonus(){
+	void match_bonus() {
 		List<Integer> winNum = Arrays.asList(1, 2, 3, 4, 5, 6);
 		int bonusNumber = 7;
-		WinningLotto lotteryWin = WinningLotto.of(winNum,bonusNumber);
+		WinningLotto lotteryWin = WinningLotto.of(winNum, bonusNumber);
 
-		Assertions.assertThat(lotteryWin.isBonus(5,Arrays.asList(1, 2, 5, bonusNumber, 41, 45)))
+		Assertions.assertThat(lotteryWin.isBonus(5, Arrays.asList(1, 2, 5, bonusNumber, 41, 45)))
 			.isEqualTo(true);
 	}
-}
+
+	@Test
+	@DisplayName("보너스 번호와 당첨 번호는 같을 수 없다")
+	void bonus_valid() {
+		List<Integer> winNum = Arrays.asList(1, 2, 3, 4, 5, 6);
+		int bonusNumber = 6;
+		Assertions.assertThatThrownBy(() -> WinningLotto.of(winNum, bonusNumber))
+			.isInstanceOf(IllegalNumberException.class);
+	}
+	}
