b0679772b63937675f605c6c84234386dbb05558
root.directory <root.directory@kakaocorp.com>
2022-10-26 21:58:35 +0900

    test: String으로 받아 사칙연산 모두 수행

---
+++ src/main/java/StringCalculator/Calculator/ComplexCalculator.java
@@ -0,0 +1,7 @@
+package StringCalculator.Calculator;
+
+public class ComplexCalculator {
+    public static int complexCalculate(String input) {
+        return 0;
+    }
+}
---
+++ src/test/java/StringCalculator/Calculator/ComplexCalculatorTest.java
@@ -0,0 +1,15 @@
+package StringCalculator.Calculator;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComplexCalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 1;2", "3 + 3 / 2 - 3;0", "1 + 12 - 4 / 3;3", "1 + 12 - 4 / 3 * 0;0"}, delimiter = ';')
+    public void complexInput(String input, int result) {
+        assertThat(ComplexCalculator.complexCalculate(input)).isEqualTo(result);
+    }
+}
