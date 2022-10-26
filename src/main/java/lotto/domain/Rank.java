67e6a8e887d0a0b29dd8f9358f0a0a0ffcf9ab4e
caoyu-dev <choyumin01@gmail.com>
2022-10-26 22:16:43 +0900

    [refactor] stream 으로 values() 순회

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, BigDecimal.valueOf(2000000000)),
     SECOND(5, BigDecimal.valueOf(1500000)),
     THIRD(4, BigDecimal.valueOf(50000)),
     FORTH(3, BigDecimal.valueOf(5000)),
+    FIFTH(2, BigDecimal.valueOf(0)),
+    SIXTH(1, BigDecimal.valueOf(0)),
     ZERO(0, BigDecimal.valueOf(0));
 
     private final int count;
@@ -26,18 +29,9 @@ public enum Rank {
     }
 
     public static Rank getRank(int count) {
-        if (FIRST.count == count) {
-            return FIRST;
-        }
-        if (SECOND.count == count) {
-            return SECOND;
-        }
-        if (THIRD.count == count) {
-            return THIRD;
-        }
-        if (FORTH.count == count) {
-            return FORTH;
-        }
-        return ZERO;
+        return Arrays.stream(values())
+                .filter(cnt -> cnt.getCount() == count)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("숫자가 틀립니다."));
     }
 }
