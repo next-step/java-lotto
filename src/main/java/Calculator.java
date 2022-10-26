9bff3ce8cc41cefd5437633b6c27f051e9b6ac08
KIYOUNG KWON <kkywalk2@gmail.com>
2022-10-26 14:01:48 +0900

    🚀 1단계 - 문자열 계산기 (#2718)
    * test: Calculator 기능에 대한 테스트코드 작성
    
    * chore: 불필요한 파일 제거
    
    * feat: 사칙연산 기능구현
    
    * refactor: indent depth 축소
    
    * refactor: 테스트코드에 DisplayName 추가 및 잘못된 convention 변경
    
    * refactor: 사칙연산의 분기를 IntBinaryOperator를 value로 가지는 map으로 수정
    
    * feat: 입력을 받는 class를 분리

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.function.IntBinaryOperator;
+
+public class Calculator {
+
+    private static final Map<String, IntBinaryOperator> OPERATOR_MAP = Map.of("+", (a, b) -> a + b, "-", (a, b) -> a - b, "/", (a, b) -> a / b, "*", (a, b) -> a * b);
+
+    public int calculate(List<String> numbersAndOps) {
+        Deque<Integer> numbers = new LinkedList<>();
+        Deque<String> operators = new LinkedList<>();
+        while (!numbersAndOps.isEmpty()) {
+            String unit = numbersAndOps.remove(0);
+            pushNumberOrOperator(numbers, operators, unit);
+        }
+
+        while (!operators.isEmpty()) {
+            String operator = operators.pollFirst();
+            int right = numbers.pollFirst();
+            int left = numbers.pollFirst();
+            int calculateResult = calculateOperation(left, right, operator);
+            numbers.addFirst(calculateResult);
+        }
+
+        return numbers.pop();
+    }
+
+    private void pushNumberOrOperator(Deque<Integer> numbers, Deque<String> operators, String unit) {
+        if (isOperator(unit)) {
+            operators.push(unit);
+            return;
+        }
+        numbers.push(toInt(unit));
+    }
+
+    private int toInt(String unit) {
+        try {
+            return Integer.parseInt(unit);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("convert failed to number");
+        }
+    }
+
+    private int calculateOperation(int left, int right, String operator) {
+        IntBinaryOperator operateFunction = OPERATOR_MAP.get(operator);
+        return operateFunction.applyAsInt(left, right);
+    }
+
+    private boolean isOperator(String unit) {
+        return OPERATOR_MAP.containsKey(unit);
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 1:2", "2 - 1:1", "4 / 2:2", "3 * 3:9"}, delimiter = ':')
+    @DisplayName("기본 사칙연산을 정상적으로 수행한다.")
+    public void calculationTest(String expression, int expected) {
+        assertThat(calculateResult(expression)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("null 또는 빈 문자열 값을 입력할 경우 예외가 발생한다.")
+    public void nullOrEmptyTest(String expression) {
+        assertThatIllegalArgumentException().isThrownBy(() -> calculateResult(expression));
+    }
+
+    private int calculateResult(String expression) {
+        List<String> numbersAndOps = InputNumbers.inputNumbers(expression);
+        return new Calculator().calculate(numbersAndOps);
+    }
+}
