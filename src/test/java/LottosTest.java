1eb50a8cd64ad3a85d639d97a474f8e2fa817df1
이건회 <rg970604@naver.com>
2022-10-27 05:21:39 +0900

    test : LottosTest.java

--- src/test/java/LottosTest.java
+++ src/test/java/LottosTest.java
@@ -1,14 +1,17 @@
-import domain.Lottos;
+import domain.lotto.Lottos;
+import domain.strategy.GenerateLottoNum;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import strategy.GenerateLottoNumFixed;
 
 public class LottosTest {
 
   @Test
   @DisplayName("로또 5장 구매시 로또의 개수가 5개인지 확인")
   void checkLottosSize() {
-    Lottos lottos = new Lottos(5);
+    GenerateLottoNum generateLottoNum = new GenerateLottoNumFixed();
+    Lottos lottos = new Lottos(5, generateLottoNum);
     Assertions.assertThat(lottos.lottos.size()).isEqualTo(5);
   }
 }
