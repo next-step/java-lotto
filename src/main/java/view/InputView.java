2bfd40d8ba1bf23d2d5a69ab085ceb5bd224dc73
이건회 <rg970604@naver.com>
2022-10-27 05:21:04 +0900

    refactor : InputView.java

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,22 +8,26 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputView {
-  public static int paybill(){
+
+  static final Scanner scanner = new Scanner(System.in);
+
+  private static final String DELIMITER = ", ";
+
+  public static int paybill() {
     System.out.println("구입금액을 입력해 주세요.");
-    Scanner scanner = new Scanner(System.in);
     int payment = scanner.nextInt();
-    int amount = payment / 1000;
-    System.out.println(amount+"개를 구매했습니다");
-    return amount;
+    scanner.nextLine();
+    return payment;
   }
 
-  public static List<Integer> winningNums(){
+  public static List<Integer> winningNums() {
     System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-    Scanner scanner = new Scanner(System.in);
-    String winningnumsString= scanner.nextLine();
-    String [] winningnumsArrayString = winningnumsString.split(", ");
-    int [] winningnumsArray = Stream.of(winningnumsArrayString).mapToInt(Integer::parseInt).toArray();
-    List<Integer> winningNums = Arrays.stream(winningnumsArray).boxed().collect(Collectors.toList());
+    String winningnumsString = scanner.nextLine();
+    String[] winningnumsArrayString = winningnumsString.split(DELIMITER);
+    int[] winningnumsArray = Stream.of(winningnumsArrayString).mapToInt(Integer::parseInt)
+        .toArray();
+    List<Integer> winningNums = Arrays.stream(winningnumsArray).boxed()
+        .collect(Collectors.toList());
     Collections.sort(winningNums);
     return winningNums;
   }
