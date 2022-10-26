c7f137ca5a371b80bdbf103cce797ed7ac4397e8
kureungkureung <terry9611@naver.com>
2022-10-27 00:51:42 +0900

    feat(RankValue): Rank dto 역할

---
+++ src/main/java/step2/dto/RankValue.java
@@ -0,0 +1,37 @@
+package step2.dto;
+
+import step2.lotto.Rank;
+
+import java.util.Map;
+
+public class RankValue {
+
+    private final long numberOfWinnings;
+    private final double prizeAmount;
+    private final long numberOfMatchingNumbers;
+
+    public RankValue(long numberOfWinnings, double prizeAmount, long numberOfMatchingNumbers) {
+        this.numberOfWinnings = numberOfWinnings;
+        this.prizeAmount = prizeAmount;
+        this.numberOfMatchingNumbers = numberOfMatchingNumbers;
+    }
+
+    public static RankValue from(Map.Entry<Rank, Long> entry) {
+        Rank rank = entry.getKey();
+        Long countOfNumber = entry.getValue();
+        return new RankValue(countOfNumber, rank.prizeAmount(), rank.numberOfMatchingNumbers());
+    }
+
+    public long numberOfWinnings() {
+        return numberOfWinnings;
+    }
+
+    public long prizeAmount() {
+        return (long) prizeAmount;
+    }
+
+    public long numberOfMatchingNumbers() {
+        return numberOfMatchingNumbers;
+    }
+
+}
---
+++ src/main/java/step2/dto/RankValues.java
@@ -0,0 +1,24 @@
+package step2.dto;
+
+import step2.lotto.Rank;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RankValues {
+
+    private final Map<Rank, Long> rankingAggregation;
+
+    public RankValues(Map<Rank, Long> rankingAggregation) {
+        this.rankingAggregation = rankingAggregation;
+    }
+
+    public List<RankValue> rankValues() {
+        return rankingAggregation.entrySet()
+                .stream()
+                .map(RankValue::from)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+}
