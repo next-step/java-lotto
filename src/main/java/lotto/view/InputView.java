0a89cdf6894792784d6795aebf93ebbb69090598
boradol2 <boradol0902@gmail.com>
2022-10-28 01:15:51 +0900

    feature(InputView) : 로또 수동개수 입력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public final class InputView {
     private static final String AMOUNT_QUESTION_MESSAGE = "구입금액을 입력해 주세요. (1장당 1000원 입니다.)";
+    private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String WINNING_NUMBERS_QUESTION_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -13,6 +14,11 @@ public final class InputView {
 
     public static String askAmount() {
         System.out.println(AMOUNT_QUESTION_MESSAGE);
+        return inputAnswer();
+    }
+
+    public static String askManualLottoCount() {
+        System.out.println(MANUAL_LOTTO_COUNT_MESSAGE);
         return inputAnswer();
     }
 
