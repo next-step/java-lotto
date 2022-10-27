846ad595d546800e0e6cb39403332e7e78fa8ba6
suchan <aud248@naver.com>
2022-10-27 17:09:26 +0900

    refactor: 미사용 import 제거

--- src/test/java/step4/domain/LottoResultTest.java
+++ src/test/java/step4/domain/LottoResultTest.java
@@ -1,17 +1,16 @@
 package step4.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoResultTest {
 
