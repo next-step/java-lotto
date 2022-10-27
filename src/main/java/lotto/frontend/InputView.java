88dd7dd7fcd4af6d6e0f1b12118b43da48cd77bc
jiokim <jiokim222@gmail.com>
2022-10-28 02:10:58 +0900

    feat(View): 도메인 내의 출력로직을 view로 옮김

--- src/main/java/lotto/frontend/InputView.java
+++ src/main/java/lotto/frontend/InputView.java
@@ -8,12 +8,12 @@ public class InputView {
     private static final String ENTER_MONEY = "구매 금액을 입력해주세요.";
     private static final String ENTER_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
 
-    public static int askEnterMoney() {
+    public int askEnterMoney() {
         System.out.println(ENTER_MONEY);
         return scanner.nextInt();
     }
 
-    public static String askEnterWinningNumbers() {
+    public String askEnterWinningNumbers() {
         System.out.println(ENTER_WINNING_NUMBER);
         return scanner.next();
     }
--- src/main/java/lotto/frontend/ResultView.java
+++ src/main/java/lotto/frontend/ResultView.java
@@ -1,59 +1,94 @@
 package lotto.frontend;
 
-import lotto.backend.domain.LottoStatistics;
-import lotto.backend.domain.LottoTickets;
+import lotto.backend.domain.LottoNumber;
+import lotto.backend.domain.LottoTicket;
+import lotto.backend.dto.LottoResultDto;
+import lotto.backend.dto.LottoTicketsDto;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ResultView {
 
-    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final int MIN_MATCH = 3;
+    private static final int MAX_MATCH = 6;
 
-    public static void printLottoNumber(LottoTickets lottoTickets) {
+    private static final String PREFIX = "[";
+    private static final String SUFFIX = "]";
+    private static final String DELIMITER = ", ";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final StringBuilder STRING_BUILDER = new StringBuilder();
+
+    public void printLottoNumber(LottoTicketsDto lottoTickets) {
         System.out.println(assembleBuyingResult(lottoTickets));
     }
 
-    private static String assembleBuyingResult(LottoTickets LottoTickets) {
-        return new StringBuilder().append(LottoTickets.size())
+    private String assembleBuyingResult(LottoTicketsDto lottoTicketsDto) {
+        return STRING_BUILDER.append(lottoTicketsDto.countOfTicket())
                 .append("개를 구매하였습니다.")
                 .append(LINE_SEPARATOR)
-                .append(LottoTickets.print(LINE_SEPARATOR))
+                .append(assembleLottoTickets(lottoTicketsDto))
+                .append(LINE_SEPARATOR)
                 .toString();
     }
 
-    public static void printLottoResult(LottoStatistics lottoStatistics) {
-        System.out.println(assembleLottoResult(lottoStatistics));
+    private String assembleLottoTickets(LottoTicketsDto lottoTicketsDto) {
+        return IntStream.range(0, lottoTicketsDto.countOfTicket())
+                .mapToObj(i -> assembleLottoTicket(lottoTicketsDto.getValues().get(i)))
+                .collect(Collectors.joining(LINE_SEPARATOR));
+
     }
 
-    private static String assembleLottoResult(LottoStatistics lottoStatistics) {
-        return new StringBuilder()
+    private String assembleLottoTicket(LottoTicket lottoTicket) {
+        return lottoTicket.getValue().stream()
+                .sorted()
+                .map(LottoNumber::toString)
+                .collect(Collectors.joining(DELIMITER, PREFIX, SUFFIX));
+    }
+
+    public void printLottoResult(LottoResultDto lottoResultDto) {
+        System.out.println(assembleLottoResult(lottoResultDto));
+    }
+
+    private String assembleLottoResult(LottoResultDto lottoResultDto) {
+        return STRING_BUILDER
                 .append(LINE_SEPARATOR)
                 .append("당첨 통계")
                 .append(LINE_SEPARATOR)
                 .append("------------------")
                 .append(LINE_SEPARATOR)
-                .append(assembleMatchResult(lottoStatistics, 3))
-                .append(assembleMatchResult(lottoStatistics, 4))
-                .append(assembleMatchResult(lottoStatistics, 5))
-                .append(assembleMatchResult(lottoStatistics, 6))
-                .append(assembleProfit(lottoStatistics))
+                .append(assembleLottoMatchResult(lottoResultDto))
+                .append(assembleProfit(lottoResultDto))
                 .toString();
     }
 
-    private static String assembleMatchResult(LottoStatistics lottoStatistics, int match) {
-        return new StringBuilder()
-                .append(match)
+    private String assembleLottoMatchResult(LottoResultDto lottoResultDto) {
+        return IntStream.range(MIN_MATCH, MAX_MATCH + 1)
+                .mapToObj(i -> assembleMatchResult(i, lottoResultDto.findMatch(i), lottoResultDto.findPrize(i)))
+                .collect(Collectors.joining(LINE_SEPARATOR));
+    }
+
+    private String assembleMatchResult(int rank, int match, int prize) {
+        return STRING_BUILDER
+                .append(rank)
                 .append("개 일치 (")
-                .append(lottoStatistics.printPrize(match))
+                .append(prize)
                 .append("원)- ")
-                .append(lottoStatistics.printMatch(match))
+                .append(match)
                 .append("개")
                 .append(LINE_SEPARATOR)
                 .toString();
     }
 
-    private static String assembleProfit(LottoStatistics lottoStatistics) {
-        return new StringBuilder()
+    private String assembleProfit(LottoResultDto lottoResultDto) {
+        return STRING_BUILDER
                 .append("총 수익률은 ")
-                .append(lottoStatistics.printProfit())
+                .append(lottoResultDto.findProfit())
+                .append(assembleProfitDetail(lottoResultDto))
                 .toString();
+    }
+
+    private String assembleProfitDetail(LottoResultDto lottoResultDto) {
+        return (lottoResultDto.findProfit() < 1) ? "(기준이 1이기 때문에 결과적으로 손해라는 의미임)" : "";
     }
 }
