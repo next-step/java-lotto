ac379f490b1b3c1838fa659669444c17407ab9e7
boradol2 <boradol0902@gmail.com>
2022-10-28 03:49:52 +0900

    feature(InputView) : 수동 로또 개수 입력받기
    1. List<String> 형태로 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,18 @@
 package lotto.view;
 
+import lotto.domain.LottoCount;
+
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class InputView {
+    private static final String NEXT_LINE = System.lineSeparator();
     private static final String AMOUNT_QUESTION_MESSAGE = "구입금액을 입력해 주세요. (1장당 1000원 입니다.)";
-    private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
-    private static final String WINNING_NUMBERS_QUESTION_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_COUNT_MESSAGE = NEXT_LINE + "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTOS_MESSAGE = NEXT_LINE + "수동으로 구매할 번호를 입력해 주세요.";
+    private static final String WINNING_NUMBERS_QUESTION_MESSAGE = NEXT_LINE + "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -20,6 +27,12 @@ public final class InputView {
     public static String askManualLottoCount() {
         System.out.println(MANUAL_LOTTO_COUNT_MESSAGE);
         return inputAnswer();
+    }
+
+    public static List<String> askInputManualLottos(LottoCount lottoCount) {
+        System.out.println(MANUAL_LOTTOS_MESSAGE);
+        return Stream.generate(InputView::inputAnswer)
+                .limit(lottoCount.number()).collect(Collectors.toList());
     }
 
     public static String askWinningLottoNumbers() {
