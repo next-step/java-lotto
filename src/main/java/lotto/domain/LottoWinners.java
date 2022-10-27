9036c151b4a0fc85a9a8d2f35b2cd10fbe42b3e1
jwoh <jwoh@wemakeprice.com>
2022-10-27 18:52:41 +0900

    refect : 불변 결과를 리턴

--- src/main/java/lotto/domain/LottoWinners.java
+++ src/main/java/lotto/domain/LottoWinners.java
@@ -3,9 +3,9 @@ package lotto.domain;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class LottoWinners {
 
@@ -15,16 +15,16 @@ public class LottoWinners {
         this.winners = winners;
     }
 
-    public static LottoWinners of(List<Lotto> lottoList, Lotto weekendLottoNumber) {
+    public static LottoWinners of(List<Lotto> lottoList, Lotto weekendLottoNumber, LottoNumber lottoNumber) {
         return new LottoWinners(lottoList
             .stream()
-            .map(lotto -> match(lotto.getItem(), weekendLottoNumber))
+            .map(lotto -> match(lotto, weekendLottoNumber, lottoNumber))
             .filter(lottoPlace -> lottoPlace != LottoPlace.NONE_PLACE)
             .collect(groupingBy(r -> r, counting())));
     }
 
     public Map<LottoPlace, Long> getWinners() {
-        return winners;
+        return Collections.unmodifiableMap(winners);
     }
 
     public Long getWinnerValue(LottoPlace lottoPlace) {
@@ -37,11 +37,11 @@ public class LottoWinners {
             .sum();
     }
 
-    private static LottoPlace match(Set<LottoNumber> item, Lotto weekendLottoNumber) {
-        int matchCount = weekendLottoNumber.compare(item);
-        boolean bonus = weekendLottoNumber.bonusCompare(item);
+    private static LottoPlace match(Lotto item, Lotto weekendLottoNumber, LottoNumber lottoNumber) {
+        int matchCount = item.compareLottoNumber(weekendLottoNumber.getItem());
+        boolean bonusContain = item.isBonusNumberContains(lottoNumber);
 
-        return LottoPlace.getLottoPlace(matchCount, bonus);
+        return LottoPlace.getLottoPlace(matchCount, bonusContain);
     }
 
 }
