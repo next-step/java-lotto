a45527b76cb3be09a16790f7f1888cc911f65142
gkfgran5037 <gkfgran5037@naver.com>
2022-10-27 16:58:49 +0900

    refactor : 결과 출력 메소드 static으로 변경 및 사용자 입력 추가

--- src/main/java/lotto/conroller/LottoGame.java
+++ src/main/java/lotto/conroller/LottoGame.java
@@ -4,33 +4,35 @@ import lotto.domain.Lotto;
 import lotto.domain.Purchasing;
 import lotto.domain.Statistics;
 import lotto.domain.WinnigType;
-import lotto.strategy.RandomNumberStrategy;
+import lotto.strategy.ManualNumberStrategy;
 import lotto.view.ResultView;
+import lotto.view.UserInput;
 
 import java.util.List;
 import java.util.Map;
 
 public class LottoGame {
 
-    public void buyLotto(int money) {
-        Lotto winningLotto = new Lotto(new RandomNumberStrategy());
+    public void buyLotto() {
+        int money = UserInput.inputMoney();
         Purchasing purchasing = new Purchasing(money);
+        ResultView.printLottoCnt(purchasing.getLottoCnt());
+        ResultView.printLottos(purchasing.getLottoList());
 
-        ResultView resultView = new ResultView();
-        resultView.printLottoCnt(purchasing.getLottoCnt());
-        resultView.printLottos(purchasing.getLottoList());
+        List<Integer> inputNumbers = UserInput.inputWinningLotto();
+        Lotto winningLotto = new Lotto(new ManualNumberStrategy(inputNumbers));
 
         List<Long> matchCntList =  purchasing.getMatchCntList(winningLotto);
         Statistics statistics = new Statistics();
         Map<WinnigType, Integer> statisticsMap = statistics.getStatistics(matchCntList);
 
-        resultView.printResultMent();
-        resultView.printStatistics(statisticsMap);
-        resultView.printRateOfReturn(statistics.getRateOfReturn(money));
+        ResultView.printResultMent();
+        ResultView.printStatistics(statisticsMap);
+        ResultView.printRateOfReturn(statistics.getRateOfReturn(money));
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         LottoGame customer = new LottoGame();
-        customer.buyLotto(31000);
+        customer.buyLotto();
     }
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -9,31 +9,26 @@ import java.util.Map;
 
 public class ResultView {
 
-    private static final String START_LOTTO = "[";
-    private static final String END_LOTTO = "]";
-    private static final String SEPARATOR = ", ";
-    private static final String BOUNDARY = "---------";
-
-    public void printLottoCnt(int cnt) {
+    public static void printLottoCnt(int cnt) {
         System.out.println(cnt + "개를 구매했습니다.");
     }
 
-    public void printLottos(List<Lotto> lottos) {
-        lottos.forEach(lotto -> System.out.println(START_LOTTO + StringUtils.join(lotto.getLotto(), SEPARATOR) + END_LOTTO));
+    public static void printLottos(List<Lotto> lottos) {
+        lottos.forEach(lotto -> System.out.println("[" + StringUtils.join(lotto.getLotto(), ", ") + "]"));
     }
 
-    public void printStatistics(Map<WinnigType, Integer> statisticsMap) {
+    public static void printStatistics(Map<WinnigType, Integer> statisticsMap) {
         statisticsMap.forEach((winnigType, cnt)
                 -> System.out.println(winnigType.getMatchCnt() + "개 일치 (" + winnigType.getWinningAmount() + "원)- " + cnt + "개"));
     }
 
-    public void printRateOfReturn(double rateOfReturn) {
+    public static void printRateOfReturn(double rateOfReturn) {
         System.out.println("총 수익률은 " + rateOfReturn + "입니다.");
     }
 
-    public void printResultMent() {
+    public static void printResultMent() {
         System.out.println();
         System.out.println("당첨 통계");
-        System.out.println(BOUNDARY);
+        System.out.println("---------");
     }
 }
