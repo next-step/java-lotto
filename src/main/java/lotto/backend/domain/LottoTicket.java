3275e85540a7d1d1048b385d0ab305322b7490e7
jiokim <jiokim222@gmail.com>
2022-10-28 02:01:16 +0900

    refactor(LottoTicket): List -> set 자료구조 변경

--- src/main/java/lotto/backend/domain/LottoTicket.java
+++ src/main/java/lotto/backend/domain/LottoTicket.java
@@ -1,21 +1,20 @@
 package lotto.backend.domain;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Set;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 public class LottoTicket {
 
-    private static final int PRICE_PER_LOTTO = 1000;
+    public static final int PRICE_PER_LOTTO = 1000;
     private static final int LOTTO_NUMBER_SIZE = 6;
-    private final List<LottoNumber> value;
+    private final Set<LottoNumber> value;
 
-    private LottoTicket(List<LottoNumber> value) {
+    public LottoTicket(Set<LottoNumber> value) {
         validate(value);
         this.value = value;
     }
@@ -23,19 +22,19 @@ public class LottoTicket {
     public static LottoTicket of(int[] value) {
         return Arrays.stream(value)
                 .mapToObj(LottoNumber::of)
-                .collect(collectingAndThen(toList(), LottoTicket::new));
+                .collect(collectingAndThen(toSet(), LottoTicket::new));
     }
 
-    public static LottoTickets of(int money) {
+    public static LottoTickets of(Money money) {
         return Stream.generate(LottoTicket::create)
-                .limit(money / PRICE_PER_LOTTO)
+                .limit(money.howManyLottoTickets(PRICE_PER_LOTTO))
                 .collect(collectingAndThen(toList(), LottoTickets::new));
     }
 
     private static LottoTicket create() {
         return LottoNumber.createNumbers(LOTTO_NUMBER_SIZE).stream()
                 .sorted()
-                .collect(collectingAndThen(toList(), LottoTicket::new));
+                .collect(collectingAndThen(toSet(), LottoTicket::new));
     }
 
     public int countMatch(LottoTicket other) {
@@ -44,32 +43,18 @@ public class LottoTicket {
                 .count();
     }
 
-    public int price() {
-        return PRICE_PER_LOTTO;
-    }
-
-    private void validate(List<LottoNumber> value) {
+    private void validate(Set<LottoNumber> value) {
         checkSize(value);
-        checkDuplicatedNumber(value);
     }
 
-    private void checkSize(List<LottoNumber> value) {
+    private void checkSize(Set<LottoNumber> value) {
         if (LOTTO_NUMBER_SIZE != value.size()) {
             throw new IllegalArgumentException("로또 번호의 숫자는 6개 입니다.");
         }
     }
 
-    private void checkDuplicatedNumber(List<LottoNumber> value) {
-        boolean duplicated = value.stream().distinct().count() != value.size();
-        if (duplicated) {
-            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
-        }
-    }
-
-    public String print() {
-        return this.value.stream()
-                .map(LottoNumber::print)
-                .collect(Collectors.joining(" ,", "[", "]"));
+    public Set<LottoNumber> getValue() {
+        return Collections.unmodifiableSet(value);
     }
 
     @Override
