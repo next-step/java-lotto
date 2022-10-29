37d65050c1164be6c5cba890149d6254d41d9dcc
heejaeahn <heejahn713@gmail.com>
2022-10-29 17:50:46 +0900

    feat: main 클래스 생성

---
+++ src/main/java/lotto/LottoMain.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import lotto.controller.LottoController;
+
+public class LottoMain {
+
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.buyLotto();
+    }
+}
