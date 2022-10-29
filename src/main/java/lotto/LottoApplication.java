a78630b719a0b55a712a0107d65cb2d3dfe5b57d
yoonjeong <cyj199637@gmail.com>
2022-10-30 00:48:33 +0900

    (refactor) : InputView 수정 - 수동으로 구매할 로또 수 입력 메소드 추가 - 수동으로 구매할 로또 번호 입력 메소드 추가

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import static lotto.view.InputView.inputBonusNumber;
+import static lotto.view.InputView.inputManualLottoQuantity;
+import static lotto.view.InputView.inputManualLottos;
 import static lotto.view.InputView.inputMoney;
 import static lotto.view.InputView.inputWinningLotto;
 import static lotto.view.OutputView.printLottos;
 import static lotto.view.OutputView.printResult;
 
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoStorage;
@@ -17,6 +20,9 @@ public class LottoApplication {
 
     public static void main(String[] args) {
         Money money = new Money(inputMoney());
+        int manualQuantity = inputManualLottoQuantity();
+        List<String> manualLottos = inputManualLottos();
+
         LottoStore store = LottoStore.getInstance();
         LottoStorage storage = store.buyLotto(money, AutoIssueLottoStrategy.getInstance());
         printLottos(storage.getLottos());
--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -9,7 +9,8 @@ public enum ExceptionMessage {
     ERROR_INVALID_LOTTO_NUMBER_COUNT("6개의 숫자를 입력해주세요."),
     ERROR_EMPTY_LOTTO("로또를 구매하지 않았습니다."),
     ERROR_NOT_ENOUGH_MONEY("로또를 구매할 수 없습니다."),
-    ERROR_INVALID_BONUS_NUMBER("로또 당첨 번호 회의 번호를 입력해주세요.");
+    ERROR_INVALID_BONUS_NUMBER("로또 당첨 번호 회의 번호를 입력해주세요."),
+    ERROR_ZERO_OR_NEGATIVE_VALUE("0 이하의 숫자는 허용되지 않습니다.");
 
     private final String message;
 
