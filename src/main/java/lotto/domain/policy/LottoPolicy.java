988734f796f40670a09ba8a30e32e845a76befbe
KimWanJoo <wjwan0915@gmail.com>
2022-10-29 18:50:28 +0900

    :recycle: refactor(Test Code를 위한 Fake객체 위치 수정)

--- src/main/java/lotto/domain/policy/LottoPolicy.java
+++ src/main/java/lotto/domain/policy/LottoPolicy.java
@@ -1,25 +1,8 @@
 package lotto.domain.policy;
 
-import java.util.List;
 import lotto.domain.Lotto;
-import lotto.domain.LottoNumberFactory;
 
 public interface LottoPolicy {
 
     Lotto ball();
-
-    class FakeLottoPolicy implements LottoPolicy {
-
-        @Override
-        public Lotto ball() {
-            return new Lotto(List.of(
-                LottoNumberFactory.getLottoNumber(1),
-                LottoNumberFactory.getLottoNumber(2),
-                LottoNumberFactory.getLottoNumber(3),
-                LottoNumberFactory.getLottoNumber(4),
-                LottoNumberFactory.getLottoNumber(5),
-                LottoNumberFactory.getLottoNumber(6)
-            ));
-        }
-    }
 }
