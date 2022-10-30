3e4f07045af8b73d164603fd9883807577fa9a1f
jiokim <jiokim222@gmail.com>
2022-10-30 23:03:27 +0900

    feat(InputView): 보너스 번호 입력 구현

--- src/main/java/lotto/frontend/InputView.java
+++ src/main/java/lotto/frontend/InputView.java
@@ -1,20 +1,33 @@
 package lotto.frontend;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String COMMA_DELIMITER = ",";
     private static final String ENTER_MONEY = "구매 금액을 입력해주세요.";
     private static final String ENTER_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String ENTER_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
 
     public int askEnterMoney() {
         System.out.println(ENTER_MONEY);
-        return scanner.nextInt();
+        return Integer.parseInt(SCANNER.nextLine());
     }
 
-    public String askEnterWinningNumbers() {
+    public List<Integer> askEnterWinningNumbers() {
         System.out.println(ENTER_WINNING_NUMBER);
-        return scanner.next();
+        return Arrays.stream(SCANNER.nextLine().split(COMMA_DELIMITER))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public int askEnterBonusNumber() {
+        System.out.println(ENTER_BONUS_NUMBER);
+        int bonus = Integer.parseInt(SCANNER.nextLine());
+        return bonus;
     }
 }
