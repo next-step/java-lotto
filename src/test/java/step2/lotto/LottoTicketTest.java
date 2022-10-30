2becbd73ac51c106673a8ab88f854d7527bb87e8
kureungkureung <terry9611@naver.com>
2022-10-30 18:39:12 +0900

    test(LottoTicketTest): 로또 번호 범위 유효성 검증

--- src/test/java/step2/lotto/LottoTicketTest.java
+++ src/test/java/step2/lotto/LottoTicketTest.java
@@ -18,6 +18,15 @@ class LottoTicketTest {
     }
 
     @Test
+    void 로또_번호가_1이상_46이하가_아닐_경우_예외가_발생한다() {
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(0, 1, 2, 3, 4, 5));
+
+        assertThatThrownBy(() -> LottoTicket.from(numbersGenerator))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1 이상 45이하 이어야 합니다.");
+    }
+
+    @Test
     void 로또_번호를_출력할_수_있다() {
         NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
 
@@ -48,4 +57,5 @@ class LottoTicketTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 번호는 6개이어야 합니다.");
     }
+
 }
