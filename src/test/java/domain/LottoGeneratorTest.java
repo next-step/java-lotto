a27f55705829b2a79ef6b6a29d59331d2c7f5291
김건우 <kgw1457@gmail.com>
2022-10-30 16:06:47 +0900

    (feat) 4단계 - 로또(수동)
     - 로또 생성기를 통해 수동 로또를 생성할 수 있다.
     - 입력한 수동개수와 로또의 수가 일치하여야 한다.

--- src/test/java/domain/LottoGeneratorTest.java
+++ src/test/java/domain/LottoGeneratorTest.java
@@ -2,22 +2,26 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoGeneratorTest {
 
+    public static final LottoGenerator LOTTO_GENERATOR = LottoGenerator.getInstance();
+
     @Test
     void 금액에_해당하는_로또를_발급한다() {
-        assertThat(LottoGenerator.getInstance().generateLotto(new Price(6000))).hasSize(6);
+        assertThat(LOTTO_GENERATOR.generateLotto(new Price(6000))).hasSize(6);
     }
 
     @Test
     void 로또생성기는_여섯개의_숫자를_가진_로또를_생성한다() {
-        assertThat(LottoGenerator.getInstance().generateNumbers()).hasSize(6);
+        assertThat(LOTTO_GENERATOR.generateNumbers()).hasSize(6);
     }
 
     @Test
@@ -27,7 +31,7 @@ public class LottoGeneratorTest {
                 .collect(Collectors.toList());
 
 
-        assertThat(LottoGenerator.getInstance().generateLotto(List.of(1, 2, 3, 4, 5, 6))).isEqualTo(new Lotto(lottoNumbers));
+        assertThat(LOTTO_GENERATOR.generateLotto(List.of(1, 2, 3, 4, 5, 6))).isEqualTo(new Lotto(lottoNumbers));
     }
 
     @Test
@@ -37,6 +41,32 @@ public class LottoGeneratorTest {
                 .collect(Collectors.toList());
         int bonus = 7;
 
-        assertThat(LottoGenerator.getInstance().generateWinningLotto(List.of(1,2,3,4,5,6), bonus)).isEqualTo(new WinningLotto(lottoNumbers, new LottoNumber(bonus)));
+        assertThat(LOTTO_GENERATOR.generateWinningLotto(List.of(1,2,3,4,5,6), bonus)).isEqualTo(new WinningLotto(lottoNumbers, new LottoNumber(bonus)));
+    }
+
+    @Test
+    void 수동으로_로또를_생성한다() {
+        int numberOfManualLotto = 3;
+        List<List<Integer>> manualLottoNumbers = new ArrayList<>();
+        List<Integer> lottoNumbers = List.of(1,2,3,4,5,6);
+        manualLottoNumbers.add(lottoNumbers);
+        manualLottoNumbers.add(lottoNumbers);
+        manualLottoNumbers.add(lottoNumbers);
+
+        assertThat(LOTTO_GENERATOR.generateLotto(numberOfManualLotto, manualLottoNumbers)).hasSize(numberOfManualLotto);
+    }
+
+    @Test
+    void 수동으로_로또를_생성할때_개수와_번호의수가_일치하지않으면_예외를뱉는다() {
+        int numberOfManualLotto = 2;
+        List<List<Integer>> manualLottoNumbers = new ArrayList<>();
+        List<Integer> lottoNumbers = List.of(1,2,3,4,5,6);
+        manualLottoNumbers.add(lottoNumbers);
+        manualLottoNumbers.add(lottoNumbers);
+        manualLottoNumbers.add(lottoNumbers);
+
+        assertThatThrownBy(() -> {
+            assertThat(LOTTO_GENERATOR.generateLotto(numberOfManualLotto, manualLottoNumbers)).hasSize(numberOfManualLotto);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
