030bb41249042b4170b0f623a94b53df9aca0bcc
mjin1220 <myeongjin1220@gmail.com>
2022-10-31 10:52:15 +0900

    feat: WinningLottoNumbers 객체 생성 시 유효성 검사

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -5,7 +5,8 @@ public class WinningLottoNumbers {
     private final LottoNumbers lottoNumbers;
     private final LottoNumber bonusLottoNumber;
 
-    public WinningLottoNumbers(LottoNumbers lottoNumbers, LottoNumber bonusLottoNumber) {
+    public WinningLottoNumbers(final LottoNumbers lottoNumbers, final LottoNumber bonusLottoNumber) {
+        validateOrThrow(lottoNumbers, bonusLottoNumber);
         this.lottoNumbers = lottoNumbers;
         this.bonusLottoNumber = bonusLottoNumber;
     }
@@ -16,5 +17,11 @@ public class WinningLottoNumbers {
 
     public LottoNumber getBonusLottoNumber() {
         return this.bonusLottoNumber;
+    }
+
+    private void validateOrThrow(final LottoNumbers lottoNumbers, final LottoNumber bonusLottoNumber) {
+        if (lottoNumbers.contains(bonusLottoNumber)) {
+            throw new IllegalArgumentException("로또 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
+        }
     }
 }
