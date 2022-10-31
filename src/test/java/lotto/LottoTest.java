a1c6d41ebf0dfdfac5eb08a1aff4b04c0c4615c8
heoseungyeon <dia0312@naver.com>
2022-11-01 08:40:47 +0900

    test: 로또 1~45 범주 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -16,4 +16,14 @@ class LottoTest {
 
 		assertThat(lotto.getNumbers().size()).isEqualTo(Lotto.LOTTO_NUMBER_QUANTITY);
 	}
+
+	@DisplayName("생성된 로또의 여섯가지 숫자는 1~45 사이의 숫자이어야 한다.")
+	@Test
+	void Should_Be_Between_1_And_45_When_Create_Lotto() {
+		Lotto lotto = new Lotto();
+
+		assertThat(lotto.getNumbers().stream()
+			.allMatch(number -> number >= 1 && number <= 45))
+			.isTrue();
+	}
 }
