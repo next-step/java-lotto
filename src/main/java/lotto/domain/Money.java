760dac6156f06ffb1851dd99ce7f47a364cc8990
heoseungyeon <dia0312@naver.com>
2022-10-31 23:49:00 +0900

    feat: Money 유효성 검증(음수) 로직 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,11 +1,21 @@
 package lotto.domain;
 
+import lotto.exception.ErrorMessage;
+import lotto.exception.MoneyNegativeException;
+
 public class Money {
 
 	private int value;
 
 	public Money(int value) {
+		if (isNegative(value)) {
+			throw new MoneyNegativeException(ErrorMessage.MONEY_MUST_BE_POSITIVE);
+		}
 		this.value = value;
+	}
+
+	private boolean isNegative(int value) {
+		return value < 0;
 	}
 
 	public int getValue() {
