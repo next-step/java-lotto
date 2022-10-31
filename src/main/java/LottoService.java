add42aa068743c5ab21428594c14cf5b9966c78e
kkywalk2 <kkywalk2@gmail.com>
2022-10-31 14:49:25 +0900

    feat: buyLotto 함수 구현

--- src/main/java/LottoService.java
+++ src/main/java/LottoService.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
 
+    private static final int LOTTO_PRICE = 1000;
     private final IntegerArrayGenerator integerArrayGenerator;
 
     public LottoService(IntegerArrayGenerator integerArrayGenerator) {
         this.integerArrayGenerator = integerArrayGenerator;
     }
 
-    public List<List<Integer>> buyLotto(int price) {
-        throw new RuntimeException("not implemented yet");
+    public List<List<Integer>> buyLotto(int amount) {
+        int count = amount / LOTTO_PRICE;
+        List<List<Integer>> lottoList = new ArrayList<>(amount);
+        for (int i = 0; i < count; i++) {
+            lottoList.add(integerArrayGenerator.getIntegerArray());
+        }
+        return lottoList;
     }
 
     public WinningStatistics calculateWinningStatistics(List<Integer> winningNumbers) {
