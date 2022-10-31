bef88fbb23065e1607ea2d871c9d19c7aa183388
miguel.kim <miguel.kim@kakaocorp.com>
2022-11-01 00:54:49 +0900

    refactor: matchCount를 Prize의 변수로 처리

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,25 +1,29 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public enum Prize {
-    SIX(2000000000),
-    FIVE_BONUS(30000000),
-    FIVE(1500000),
-    FOUR(50000),
-    THREE(5000),
-    NONE(0);
+    SIX(2000000000, 6),
+    FIVE_BONUS(30000000, -1),
+    FIVE(1500000, 5),
+    FOUR(50000, 4),
+    THREE(5000, 3),
+    NONE(0, -1);
 
     private final int value;
+    private final int matchCount;
 
-    Prize(int value) {
+
+    Prize(int value, int matchCount) {
         this.value = value;
+        this.matchCount = matchCount;
     }
 
     public static Prize toPrize(int matchCount) {
-        if (matchCount == 6) return SIX;
-        if (matchCount == 5) return FIVE;
-        if (matchCount == 4) return FOUR;
-        if (matchCount == 3) return THREE;
-        return NONE;
+        return Arrays.stream(Prize.values())
+                .filter(value -> value.matchCount == matchCount)
+                .findFirst()
+                .orElse(NONE);
     }
 
     public int getValue() {
