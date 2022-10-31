80bb1de69fe3b96c10ff554a43d9377ed4df2d8c
heoseungyeon <dia0312@naver.com>
2022-11-01 08:26:11 +0900

    test: 로또 자동 생성 실패하는 테스트 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,14 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
 	public static final int PRICE = 1000;
+	public static final int LOTTO_NUMBER_QUANTITY = 6;
+
+	public List<Integer> numbers;
+
+	public Lotto() {
+
+	}
 }
---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import lotto.domain.Lotto;
+
+class LottoTest {
+
+	@DisplayName("Default Constructor로 로또를 생성하면 여섯개의 숫자를 생성한다.")
+	@Test
+	void Should_Have_Six_Number_When_Create_Lotto_By_Deafult_Constructor() {
+		Lotto lotto = new Lotto();
+
+		assertThat(lotto.numbers.size()).isEqualTo(Lotto.LOTTO_NUMBER_QUANTITY);
+	}
+}
