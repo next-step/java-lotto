f46a901d19adce173fac3a07ce48935e610d7680
kureungkureung <terry9611@naver.com>
2022-11-02 00:32:20 +0900

    refactor(Rank): 당첨 순위 수정

--- src/main/java/step2step3/lotto/Rank.java
+++ src/main/java/step2step3/lotto/Rank.java
@@ -5,9 +5,10 @@ import java.util.EnumSet;
 public enum Rank {
 
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NOTHING(0, 0),
     ;
 
