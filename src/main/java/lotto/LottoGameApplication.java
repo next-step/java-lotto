ef838609c4faa270f2f497c4ce6d7822cb274316
hannah33 <hannah.33@kakaocorp.com>
2022-11-02 00:12:31 +0900

    refactor: 금액이 모자라서 로또를 구입하지 못한 경우 뒷 단계 스킵

--- src/main/java/lotto/LottoGameApplication.java
+++ src/main/java/lotto/LottoGameApplication.java
@@ -13,13 +13,14 @@ public class LottoGameApplication {
 
     public static void main(String[] args) {
         int moneyForTicket = LottoInputView.getMoneyForTicket();
-
         List<LottoTicket> tickets = LottoGame.buy(moneyForTicket);
+        if (tickets.isEmpty()) {
+            return;
+        }
 
         LottoResultView.printTickets(tickets);
 
         LottoTicket lastWeekWinner = LottoInputView.getLastWeekWinnerNumber();
-
         List<LottoPrize> prizes = LottoEarningCalculator.findPrize(tickets, lastWeekWinner);
 
         LottoResultView.printResult(prizes, tickets.size());
