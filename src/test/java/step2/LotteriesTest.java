1dfb854b725e11d4e9cc81e167059f5a1123d734
sienna <20200803@sungshin.ac.kr>
2022-11-01 13:32:06 +0900

    :white_check_mark:test : 수동 로또 + 자동 로또를 보관하는 Lotteries

--- src/test/java/step2/LotteriesTest.java
+++ src/test/java/step2/LotteriesTest.java
@@ -4,26 +4,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperties;
-import step2.controller.LottoController;
 import step2.model.Lotteries;
 import step2.model.Lotto;
 import step2.model.Rank;
 
 public class LotteriesTest {
 
+	List<Integer> winNumber;
+	List<Lotto> handLotto;
+
+	@BeforeEach
+	void before() {
+		winNumber = Arrays.asList(1, 2, 3, 4, 5, 44);
+		handLotto =
+			Arrays.asList(new Lotto(Arrays.asList(14, 32, 23, 41, 25, 26)));
+	}
+
 	@Test
 	@DisplayName("로또(여러장)이 맞은 개수를 보관한다.")
 	void matchMap() {
-		List<Integer> winNum = Arrays.asList(1, 2, 12, 22, 33, 44);
 		int bonusNumber = 9;
-
-		Lotteries lotteries = Lotteries.of(2, new FixLottoFactory());
-
-		assertThat(lotteries.isMatch(winNum, bonusNumber).get(Rank.MISS)).isEqualTo(2);
+		Lotteries lotteries = Lotteries.of(handLotto, 2, new FixLottoFactory());
+		assertThat(lotteries.isMatch(winNumber, bonusNumber)
+			.get(Rank.THIRD))
+			.isEqualTo(2);
 	}
 
 	@Test
@@ -32,7 +39,7 @@ public class LotteriesTest {
 		List<Integer> winNum = Arrays.asList(1, 2, 3, 4, 5, 6);
 		int bonusNumber = 9;
 
-		Lotteries lotteries = Lotteries.of(2, new FixLottoFactory());
+		Lotteries lotteries = Lotteries.of(handLotto, 2, new FixLottoFactory());
 
 		assertThat(lotteries.isMatch(winNum, bonusNumber).get(Rank.FIRST)).isEqualTo(2);
 	}
