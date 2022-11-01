152987f101dbb764813f16ce9db6f9138cd73fd9
shineyuno <shineyuno@gmail.com>
2022-11-01 22:11:29 +0900

    feat: 사용자가 수동으로 추첨 번호를 입력할 수 있도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -16,8 +16,12 @@ public class InputView {
         return NumberUtil.getPositiveInteger(SCANNER.nextLine());
     }
 
-    public static List<Integer> inputWinnigNumber() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    public static int inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return NumberUtil.getPositiveInteger(SCANNER.nextLine());
+    }
+
+    public static List<Integer> inputLottoNumber() {
         String inputText = SCANNER.nextLine();
         String[] tokens = inputText.split(",");
 
@@ -25,6 +29,11 @@ public class InputView {
                 .map(String::trim)
                 .map(NumberUtil::getPositiveInteger)
                 .collect(Collectors.toList());
+    }
+
+    public static List<Integer> inputWinnigNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return inputLottoNumber();
     }
 
     public static int inputBonusNumber() {
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
     @Test
@@ -20,6 +21,77 @@ public class LottoTest {
 
         assertThat(Lotto.convertAmountToTicketCount(amount)).isEqualTo(14);
     }
+
+    @Test
+    void convertAmountToAutoTicketCount() {
+        int amount = 14000;
+
+        assertThat(Lotto.convertAmountToAutoTicketCount(amount, 20, false)).isEqualTo(14);
+    }
+
+    @Test
+    void convertAmountToAutoTicketCount_정합성체크_활성화후_테스트() {
+        int amount = 14000;
+
+        assertThat(Lotto.convertAmountToAutoTicketCount(amount, 13, true)).isEqualTo(1);
+    }
+
+    @Test
+    void convertAmountToAutoTicketCount_정합성체크활성한후_생성할수있는_로또수보다_많은로또수입력시_실패() {
+        int amount = 14000;
+
+        assertThatIllegalArgumentException().isThrownBy(() -> Lotto.convertAmountToAutoTicketCount(amount, 20, true));
+    }
+
+    @Test
+    void lotto_create_with_amount() {
+        int amount = 14000;
+        Lotto lotto = new Lotto(amount);
+
+        assertThat(lotto.getTicketCount()).isEqualTo(14);
+    }
+
+    @Test
+    void lotto_create_with_ticketList() {
+        Ticket ticket1 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Ticket ticket2 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 7));
+
+        Lotto lotto = new Lotto(Arrays.asList(ticket1, ticket2));
+
+        assertThat(lotto.getTicketCount()).isEqualTo(2);
+    }
+
+    @Test
+    void lotto_create_with_amount_n_ticketList() {
+        int amount = 14000;
+        Ticket ticket1 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Ticket ticket2 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 7));
+
+        Lotto lotto = new Lotto(amount, Arrays.asList(ticket1, ticket2));
+
+        assertThat(lotto.getTicketCount()).isEqualTo(14);
+    }
+
+    @Test
+    void lotto_create_with_amount_n_ticketList_구입금액적을시_실패() {
+        int amount = 1000;
+        Ticket ticket1 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Ticket ticket2 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 7));
+
+        assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(amount, Arrays.asList(ticket1, ticket2)));
+    }
+
+    @Test
+    void lotto_create_with_amount_n_ticketList_matchAmountAndTicketList() {
+        int amount = 14000;
+        Ticket ticket1 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Ticket ticket2 = Ticket.of(Arrays.asList(1, 2, 3, 4, 5, 7));
+
+        Lotto lotto = new Lotto(amount, Arrays.asList(ticket1, ticket2), false);
+
+        assertThat(lotto.getTicketCount()).isEqualTo(16);
+    }
+
 
     @Test
     void getTicketCount() {
