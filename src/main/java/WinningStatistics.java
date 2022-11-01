2b77466acd587093f6edf225b207c2350e04c45d
kkywalk2 <kkywalk2@gmail.com>
2022-11-01 23:48:40 +0900

    feat: 수익률 계산 로직 추가

--- src/main/java/WinningStatistics.java
+++ src/main/java/WinningStatistics.java
@@ -3,10 +3,20 @@ import java.util.Map;
 
 public class WinningStatistics {
 
+    private static final double FIRST_REWARD = 2000000;
+    private static final double SECOND_REWARD = 1500;
+    private static final double THIRD_REWARD = 50;
+    private static final double FOURTH_REWARD = 5;
+
     private final Map<Integer, Integer> winningMap;
 
     public double getAdvantageRate() {
-        return 0.0;
+        double count = winningMap.values().stream().mapToInt(Integer::intValue).sum();
+        double firstAmount = winningMap.get(6) * FIRST_REWARD;
+        double secondAmount = winningMap.get(5) * SECOND_REWARD;
+        double thirdAmount = winningMap.get(4) * THIRD_REWARD;
+        double fourthAmount = winningMap.get(3) * FOURTH_REWARD;
+        return (firstAmount + secondAmount + thirdAmount + fourthAmount) / count;
     }
 
     public int getFirst() {
