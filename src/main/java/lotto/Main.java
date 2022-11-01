e6103e1eb9f417efade5be757a0f13fc635a447e
Hamstory <hamcom1212@gmail.com>
2022-11-01 17:56:35 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -6,9 +6,10 @@ import lotto.ui.ResultView;
 import java.util.List;
 
 public class Main {
+    private static final LottoPrice LOTTO_PRICE = new LottoPrice(1000);
+
     public static void main(String[] args) {
-        LottoPrice lottoPrice = getLottoPrice();
-        LottoStore lottoStore = new LottoStore(lottoPrice);
+        LottoStore lottoStore = new LottoStore(LOTTO_PRICE);
         int purchasePrice = InputView.queryPurchasePrice();
         List<LottoTicket> lottoTickets = lottoStore.purchase(purchasePrice);
         LottoRound lottoRound = new LottoRound(lottoTickets);
@@ -19,12 +20,8 @@ public class Main {
         List<LottoResult> results = lottoRound.finishRound(winningTicket);
 
         LottoResultStat resultStat = new LottoResultStat(results);
-        double profitMargin = resultStat.getProfitMargin(lottoPrice);
+        double profitMargin = resultStat.getProfitMargin(LOTTO_PRICE);
         ResultView.printLottoResults(resultStat);
         ResultView.printProfitMargin(profitMargin);
-    }
-
-    private static LottoPrice getLottoPrice() {
-        return new LottoPrice(1000);
     }
 }
