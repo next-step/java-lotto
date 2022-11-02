ff87965483eb9b43d42ba8232c48eb62e3ec54f4
min16 <minji.na@cre.ma>
2022-11-02 20:33:35 +0900

    FEATURE - 당첨 통계에 2등 당첨 출력하기

--- src/main/java/Lotto/ResultView.java
+++ src/main/java/Lotto/ResultView.java
@@ -9,6 +9,8 @@ public class ResultView {
     private static final String BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
     private static final String STATS_RESULT = "당첨 통계";
     private static final String INDICATOR = "----------";
+    private static final String SECOND_RANK_MESSAGE_FORMAT = "%s개 일치, 보너스 볼 일치(%s원)- %s개";
+    private static final String MESSAGE_FORMAT = "%s개 일치, (%s원)- %s개";
 
     public static void inputAmount() { System.out.println(AMOUNT_INPUT); }
 
@@ -37,15 +39,21 @@ public class ResultView {
     public static void result(MyRank myRank) {
         System.out.println(STATS_RESULT);
         System.out.println(INDICATOR);
+
         for (Rank rank : myRank.all()) {
             String msg = String.format(
-                    "%s개 일치 (%s원)- %s개",
+                    messageFormat(rank),
                     rank.getCorrectCount(),
                     rank.getPrize(),
                     myRank.getCount(rank)
             );
             System.out.println(msg);
         }
+    }
+
+    private static String messageFormat(Rank rank) {
+        if (rank == Rank.SECOND) return SECOND_RANK_MESSAGE_FORMAT;
+        return MESSAGE_FORMAT;
     }
 
     public static void printLine() {
