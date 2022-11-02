4af54eacd8cd05964b860f76bbbe8b83984d1b7a
kkywalk2 <kkywalk2@gmail.com>
2022-11-03 01:39:38 +0900

    feat: Lotto 생성에 대한 validation 테스트코드 추가

--- src/test/java/com/nextlevel/kky/lotto/LottoShopTest.java
+++ src/test/java/com/nextlevel/kky/lotto/LottoShopTest.java
@@ -4,6 +4,7 @@ import com.nextlevel.kky.lotto.domain.LotteryCommission;
 import com.nextlevel.kky.lotto.domain.LottoShop;
 import com.nextlevel.kky.lotto.model.Lotto;
 import com.nextlevel.kky.lotto.model.WinningStatistics;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,15 +14,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoShopTest {
 
     @Test
+    @DisplayName("로또를 14000원 어치를 구매하면 14를 살수있다")
     public void buyLottoTest() {
         LottoShop lottoShop = new LottoShop(() -> List.of(1, 2, 3, 4, 5, 6));
         List<Lotto> lottoList = lottoShop.buyLotto(14000);
 
         assertThat(lottoList.size()).isEqualTo(14);
-        assertThat(lottoList.get(0).getNumbers()).contains(1, 2, 3, 4, 5, 6);
     }
 
     @Test
+    @DisplayName("구매한 로또에 대하여 결과를 확인할 수 있다")
     public void calculateWinningStatisticsTest() {
         LottoShop lottoShop = new LottoShop(() -> List.of(1, 2, 3, 4, 5, 6));
         Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
