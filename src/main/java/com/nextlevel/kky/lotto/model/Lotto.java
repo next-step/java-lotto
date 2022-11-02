56b112b93c0cb2aecc0af7643a8980d6e925d921
kkywalk2 <kkywalk2@gmail.com>
2022-11-03 01:30:45 +0900

    feat: treeset을 활용하여 validation

--- src/main/java/com/nextlevel/kky/lotto/model/Lotto.java
+++ src/main/java/com/nextlevel/kky/lotto/model/Lotto.java
@@ -1,22 +1,25 @@
 package com.nextlevel.kky.lotto.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Lotto {
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        checkNumbersValidation(numbers);
-        this.numbers = numbers;
+        TreeSet<Integer> treeSet = new TreeSet<>(numbers);
+        checkNumbersValidation(treeSet);
+        this.numbers = new ArrayList<>(treeSet);
     }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
 
-    private void checkNumbersValidation(List<Integer> numbers) {
-        if (numbers.stream().distinct().count() != 6 || numbers.stream().anyMatch(num -> num > 45 || num < 0)) {
+    private void checkNumbersValidation(TreeSet<Integer> numbers) {
+        if (numbers.size() != 6 || numbers.stream().anyMatch(num -> num > 45 || num < 0)) {
             throw new IllegalArgumentException("잘못된 로또 번호 입니다!");
         }
     }
