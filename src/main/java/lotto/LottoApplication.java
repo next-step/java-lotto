2844a13289172a0fabba99b373c309eff9671ab8
bada.kang <kangbada0728@gmail.com>
2022-11-02 20:03:05 +0900

    refactor: 코드의 명확성을 위한 메서드 분리
    https://github.com/next-step/java-lotto/pull/2760#discussion_r1010774155

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -4,21 +4,28 @@ public class LottoApplication {
     public static void main(String[] args) {
         Money money = Money.of(InputView.inputMoney());
         LottoMachine lottoMachine = new LottoMachine(money);
+
+        Lottos manualLottos = buyManualLottos(lottoMachine);
+        Lottos autoLottos = lottoMachine.buyLottoAuto();
+        OutputView.outputBuyLottoResult(manualLottos, autoLottos);
+
+        Statistic statistic = lottoMachine.calculateStatistic(money, getWinningLotto(), manualLottos, autoLottos);
+        OutputView.outputStatistics(statistic);
+    }
+
+    private static WinningLotto getWinningLotto() {
+        return WinningLotto.builder()
+                .winningNumbers(InputView.inputWinningLottoNumbers())
+                .bonusBall(InputView.inputBonusBall())
+                .build();
+    }
+
+    private static Lottos buyManualLottos(LottoMachine lottoMachine) {
         Lottos manualLottos = new Lottos();
         int willBuyManualLottoCount = InputView.inputWillBuyManualLottoCount();
         if (lottoMachine.canBuyLotto(willBuyManualLottoCount)) {
             manualLottos = lottoMachine.buyLottoManually(InputView.inputManualLottoNumbers(willBuyManualLottoCount));
         }
-        Lottos autoLottos = lottoMachine.buyLottoAuto();
-
-        OutputView.outputBuyLottoResult(manualLottos, autoLottos);
-
-        WinningLotto winningLotto = WinningLotto.builder()
-                .winningNumbers(InputView.inputWinningLottoNumbers())
-                .bonusBall(InputView.inputBonusBall())
-                .build();
-
-        Statistic statistic = lottoMachine.calculateStatistic(money, winningLotto, manualLottos, autoLottos);
-        OutputView.outputStatistics(statistic);
+        return manualLottos;
     }
 }
