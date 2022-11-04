a83e27edb4226574b5e04a052cd76093e1b48fe3
scoopsoup <rnrdud46@nate.com>
2022-11-04 22:47:45 +0900

    feat : 예외처리 구문

---
+++ src/main/java/automaticlotto/automaticexception/ExceptionCommand.java
@@ -0,0 +1,8 @@
+package automaticlotto.automaticexception;
+
+public class ExceptionCommand {
+
+    public static final String PRICE_EXCEPTION_MESSAGE = "구입금액은 0보다 큰 숫자만 입력할 수 있습니다.";
+    public static final String NULL_EXCEPTION_MESSAGE = "입력값은 빈값이 들어올 수 없습니다.";
+    public static final String NUMBER_WINNING_NUMBERS_EXCEPTION_MESSAGE = "당첨번호는 6개입니다.";
+}
