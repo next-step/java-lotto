771aed0079555b3ef3c727b7f733462db6fe359f
scoopsoup <rnrdud46@nate.com>
2022-11-04 22:52:51 +0900

    feat : 로또 구입갯수 구하기

---
+++ src/main/java/automaticlotto/view/OutputView.java
@@ -0,0 +1,24 @@
+package automaticlotto.view;
+
+public class OutputView {
+
+    private static final int LOTTO_PRICE = 1000;
+    private static final String RESULT_TITLE = "당첨 통계\n---------";
+    private static final String[] LOTTO_RESULT_SAME = {"3개 일치 (5000원) - ", "4개 일치 (50000원) - ", "5개 일치 (1500000원) - ", "6개 일치 (2000000000원) - "};
+    private static int totalMoney = 0;
+
+    private OutputView() {
+
+    }
+
+    public static int showLottoNumber(int purchaseAmount) {
+        int buyLottoNumbers = buyLottoNumber(purchaseAmount);
+        System.out.println(buyLottoNumbers + "개를 구매했습니다.");
+        return buyLottoNumbers;
+    }
+
+    private static int buyLottoNumber(int purchaseAmount) {
+        return purchaseAmount / LOTTO_PRICE;
+    }
+
+}
