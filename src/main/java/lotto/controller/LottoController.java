c9e0b54a0aaf53f87265d4f63f17cffbace2099c
jwoh <jwoh@wemakeprice.com>
2022-11-04 09:35:26 +0900

    refect : controller에 out 관련 로직 제거

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -13,26 +13,24 @@ public class LottoController {
     private static LottoController controller;
 
     public static LottoController getLottoController() {
-        if (controller == null)
-        {
+        if (controller == null) {
             controller = new LottoController();
         }
         return controller;
     }
 
     public void run() {
-        System.out.println("구입금액을 입력해 주세요.");
         LottoMoney lottoMoney = LottoMoney.from(InputView.getPrice());
-        System.out.printf("%d개를 구매했습니다.\n", lottoMoney.getLottoCount());
 
-        LottoList lottoList = LottoList.of(lottoMoney.getLottoCount());
-        OutputView.printLottoList(lottoList.getLottoList());
+        LottoList lottoList = LottoList.of(lottoMoney.getLottoCount()
+            , InputView.getManualLottoCount(lottoMoney.getLottoCount()));
 
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        Lotto weekendLotto = Lotto.from(InputView.getWeekendNumbers());
-        System.out.println("보너스 볼을 입력해 주세요.");
-        LottoNumber weekendBonusNumber = LottoNumber.from(InputView.getWeekendBonusNumber());
-        weekendLotto.validBonusNumber(weekendBonusNumber);
+        OutputView.printLottoList(lottoList);
+
+        Lotto weekendLotto = Lotto.from(InputView.getWeekendNumber());
+
+        LottoNumber weekendBonusNumber = weekendLotto
+            .validBonusNumber(InputView.getWeekendBonusNumber());
 
         LottoWinners winners = LottoWinners.of(lottoList.getLottoList(), weekendLotto, weekendBonusNumber);
 
