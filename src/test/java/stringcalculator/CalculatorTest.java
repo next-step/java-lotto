83f68a38aa23c51626c0b3beac16b7f38a1a529e
scoopsoup <rnrdud46@nate.com>
2022-11-04 22:22:04 +0900

    test : enum 테스트 구현

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -12,61 +12,61 @@ public class CalculatorTest {
     private Calculator calculator;
 
     @BeforeEach
-    void Setup(){
+    void setUp() {
         calculator = new Calculator();
     }
 
     @DisplayName("덧셈테스트")
     @Test
     void sumTest() {
-        assertThat(Operate.findOperator("+").calculateNumbers(1, 2)).isEqualTo(3);
+        assertThat(Operator.findOperator("+").calculateNumbers(1, 2)).isEqualTo(3);
     }
 
     @DisplayName("뺄셈테스트")
     @Test
     void subtractionTest() {
-        assertThat(Operate.findOperator("-").calculateNumbers(1, 2)).isEqualTo(-1);
+        assertThat(Operator.findOperator("-").calculateNumbers(1, 2)).isEqualTo(-1);
     }
 
     @DisplayName("곱셈테스트")
     @Test
     void multiplicationTest() {
-        assertThat(Operate.findOperator("*").calculateNumbers(1, 2)).isEqualTo(2);
+        assertThat(Operator.findOperator("*").calculateNumbers(1, 2)).isEqualTo(2);
     }
 
     @DisplayName("나눗셈테스트")
     @Test
     void divisionTest() {
-        assertThat(Operate.findOperator("/").calculateNumbers(1, 2)).isEqualTo(0);
+        assertThat(Operator.findOperator("/").calculateNumbers(1, 2)).isEqualTo(0);
     }
 
     @DisplayName("null값 테스트")
     @Test
     void nullTest() {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.finalCalculate(null));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.calculatorSystem(null));
     }
 
     @DisplayName("빈값 테스트")
     @Test
     void emptyTest() {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.finalCalculate(""));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.calculatorSystem(""));
     }
 
     @DisplayName("사칙연산자 이외 문자 테스트")
     @Test
     void stringTest() {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.finalCalculate("1 ."));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.calculatorSystem("1 ."));
     }
 
     @DisplayName("수식 비정상종료 테스트")
     @Test
     void equationExceptionTest() {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.finalCalculate("1 +"));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> calculator.calculatorSystem("1 +"));
     }
 
     @DisplayName("계산기 테스트")
     @Test
     void calculatorTest() throws IllegalArgumentException {
-        assertThat(calculator.finalCalculate("1 + 2 / 4 + 9 * 4 - 1")).isEqualTo(35);
+        assertThat(calculator.calculatorSystem("1 + 2 / 4 + 9 * 4 - 1")).isEqualTo(35);
     }
 }
