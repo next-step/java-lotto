bc801d354b618ba861ab7ff7d97c8643b1aea882
wugatama <dnr7410@naver.com>
2022-11-06 02:36:24 +0900

    refactor: 줄바꿈 수정, 안쓰는 메소드 삭제

--- src/main/java/step3/Print.java
+++ src/main/java/step3/Print.java
@@ -1,57 +1,72 @@
 package step3;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Print {
-	public static void inputAmount() {
-		System.out.println("구입금액을 입력해 주세요.");
-	}
+    public static void inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
 
-	public static void amount(int input) {
-		System.out.println(input + "개를 구매했습니다.");
-	}
+    public static void result(List<Lotto> result) {
+        for (Lotto lotto : result) {
+            System.out.println(lotto.getLotto());
+        }
+    }
 
-	public static void result(Lotto result) {
-		System.out.println(result.getLotto());
-	}
+    public static void winningNumber() {
+        newLine();
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
 
-	public static void winningNumber() {
-		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-	}
+    public static void newLine() {
+        System.out.println();
+    }
 
-	public static void newLine() {
-		System.out.println();
-	}
+    public static void rateOfWin(int amount, Map<Operator, Integer> results) {
+        System.out.println("당첨 통계");
+        System.out.println("---------------");
+        double rate = 0d;
+        Set<Map.Entry<Operator, Integer>> entries = results.entrySet();
+        for (Map.Entry<Operator, Integer> entry : entries) {
+            rate = printPrize(rate, entry);
+        }
+        System.out.println("총 수익률은 " + String.format("%.2f", (rate / amount)) + "입니다.");
+    }
 
-	public static void rateOfWin(int amount, Map<Operator, Integer> results) {
-		System.out.println("당첨 통계");
-		System.out.println("---------------");
-		double rate = 0d;
-		Set<Map.Entry<Operator, Integer>> entries = results.entrySet();
-		for (Map.Entry<Operator, Integer> entry : entries) {
-			rate = printPrize(rate, entry);
-		}
-		System.out.println("총 수익률은 " + String.format("%.2f", (rate / amount)) + "입니다.");
-	}
+    private static double printPrize(double rate, Map.Entry<Operator, Integer> entry) {
+        if (entry.getKey() != null) {
+            rate += entry.getKey().getWinning() * entry.getValue();
+            System.out.println(checkBonus(entry));
+        }
+        return rate;
+    }
 
-	private static double printPrize(double rate, Map.Entry<Operator, Integer> entry) {
-		if (entry.getKey() != null) {
-			rate += entry.getKey().getWinning() * entry.getValue();
-			System.out.println(checkBonus(entry));
-		}
-		return rate;
-	}
+    private static String checkBonus(Map.Entry<Operator, Integer> entry) {
+        if (entry.getKey().equals(Operator.FIVE_BONUS)) {
+            return
+                    "5개 일치, 보너스 볼 일치 (" + entry.getKey().getWinning() + "원)- " + entry.getValue() + "개";
+        }
+        return entry.getKey().getCount() + "개 일치 (" + entry.getKey().getWinning() + "원)- " + entry.getValue() + "개";
+    }
 
-	private static String checkBonus(Map.Entry<Operator, Integer> entry) {
-		if (entry.getKey().equals(Operator.FIVE_BONUS)) {
-			return
-				"5개 일치, 보너스 볼 일치 (" + entry.getKey().getWinning() + "원)- " + entry.getValue() + "개";
-		}
-		return entry.getKey().getCount() + "개 일치 (" + entry.getKey().getWinning() + "원)- " + entry.getValue() + "개";
-	}
+    public static void bonusBall() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+    }
 
-	public static void bonusBall() {
-		System.out.println("보너스 볼을 입력해 주세요.");
-	}
+    public static void manualLotto() {
+        newLine();
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+    }
+
+    public static void makeManualLotto() {
+        newLine();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+    }
+
+    public static void howManyBuyLotto(int manual, int auto, List<Lotto> combineLotto) {
+        result(combineLotto);
+        System.out.println("수동으로 " + manual + "장, 자동으로 " + auto + "개를 구매했습니다.");
+    }
 }
