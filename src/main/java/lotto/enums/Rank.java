e1033e297014ce626839095a90ddbabe32e6b411
sasha-com <sasha.com@kakaocorp.com>
2022-11-05 21:03:12 +0900

    [step3] refactor: Rank enum으로 로또 당첨숫자갯수, 보상금액정보 분리

---
+++ src/main/java/lotto/enums/Rank.java
@@ -0,0 +1,33 @@
+package lotto.enums;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, BigDecimal.valueOf(2000000000)),
+    SECOND(5, BigDecimal.valueOf(30000000)),
+    THIRD(5, BigDecimal.valueOf(1500000)),
+    FOURTH(4, BigDecimal.valueOf(50000)),
+    FIFTH(3, BigDecimal.valueOf(5000));
+
+
+    int countsOfSameNumbers;
+    BigDecimal rewards;
+
+    Rank(int countsOfSameNumbers, BigDecimal rewards) {
+        this.countsOfSameNumbers = countsOfSameNumbers;
+        this.rewards = rewards;
+    }
+
+    public static Long totalWinningCount() {
+        return Arrays.stream(values()).count();
+    }
+
+    public int getCountsOfSameNumbers() {
+        return this.countsOfSameNumbers;
+    }
+
+    public BigDecimal getRewards() {
+        return this.rewards;
+    }
+}
