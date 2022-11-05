9f0456fb0ddb64999ec0183e2132b99c78faf268
wugatama <dnr7410@naver.com>
2022-11-06 02:35:42 +0900

    feat: 수동 구매 기능

--- src/main/java/step3/Game.java
+++ src/main/java/step3/Game.java
@@ -1,32 +1,59 @@
 package step3;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Game {
-	public static void start(int input) {
-		LottoAmount lottoMoney = new LottoAmount(input);
+    private static final LottoFactory lotto = new LottoFactory();
 
-		Print.amount(lottoMoney.amountOfLottos());
+    public static void start(int input) {
+        LottoAmount lottoMoney = new LottoAmount(input);
+        Print.manualLotto();
 
-		Result guestResult = startRaffle(lottoMoney);
-		Print.winningNumber();
-		Lotto winLotto = new Lotto(InputView.inputWinningNumber());
-		Print.bonusBall();
-		int bonus = Integer.parseInt(InputView.input());
+        int manualLottoAmount = Integer.parseInt(InputView.input());
+        checkManualLottoAmount(lottoMoney, manualLottoAmount);
+        Print.makeManualLotto();
+        List<Lotto> manualLotto = makeManualLotto(manualLottoAmount);
+        List<Lotto> autoLotto = makeAutoLotto(lottoMoney.amountOfLottos() - manualLottoAmount);
+        List<Lotto> combineLotto = Stream.of(manualLotto, autoLotto)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
 
-		Print.rateOfWin(input, guestResult.results(winLotto, bonus));
-	}
+        Print.howManyBuyLotto(manualLotto.size(), autoLotto.size(), combineLotto);
 
-	public static Result startRaffle(LottoAmount lottoMoney) {
-		LottoFactory lotto = new LottoFactory();
-		List<Lotto> list = new ArrayList<>();
-		for (int i = 1; i <= lottoMoney.amountOfLottos(); i++) {
-			Lotto result = new Lotto(lotto.getRandomLotto());
-			Print.result(result);
-			list.add(result);
-		}
-		Print.newLine();
-		return new Result(new Lottos(list));
-	}
+        Print.winningNumber();
+        Lotto winLotto = new Lotto(InputView.inputWinningNumber());
+        Print.bonusBall();
+        int bonus = Integer.parseInt(InputView.input());
+
+        Lottos lottos = new Lottos(combineLotto);
+
+        Print.rateOfWin(input, new Result(lottos).results(winLotto, bonus));
+    }
+
+    private static void checkManualLottoAmount(LottoAmount lottoMoney, int manualLottoAmount) {
+        if (manualLottoAmount > lottoMoney.amountOfLottos()) {
+            throw new IllegalArgumentException(lottoMoney.amountOfLottos() + "개 초과하여 구매할 수 없습니다.");
+        }
+    }
+
+    private static List<Lotto> makeManualLotto(int input) {
+        List<Lotto> list = new ArrayList<>();
+        for (int i = 0; i < input; i++) {
+            list.add(new Lotto(InputView.inputWinningNumber()));
+        }
+        return list;
+    }
+
+    public static List<Lotto> makeAutoLotto(int autoLottoAmount) {
+        List<Lotto> list = new ArrayList<>();
+        for (int i = 1; i <= autoLottoAmount; i++) {
+            Lotto result = new Lotto(lotto.getRandomLotto());
+            list.add(result);
+        }
+        return list;
+    }
 }
