c9a91d55783c2178ccf7f960d87f66439555f062
ChaeHyeon <cohys7@gmail.com>
2022-11-06 23:24:31 +0900

    refactor: to static method

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,7 +16,7 @@ public class OutputView {
         }
     }
 
-    private void printTicket(LotteryTicket ticket) {
+    private static void printTicket(LotteryTicket ticket) {
         List<String> numbers = ticket.getLotteryNumbers()
                 .stream()
                 .map(LotteryNumber::getNumber)
@@ -34,20 +34,20 @@ public class OutputView {
         printRateOfReturn(result.getRateOfReturn());
     }
 
-    private void printRateOfReturn(Double prize) {
+    private static void printRateOfReturn(Double prize) {
         System.out.printf("총 수익률은 %.2f입니다.", prize);
         if (prize < 1) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
         }
     }
 
-    private void printRanks(Ranks ranks) {
+    private static void printRanks(Ranks ranks) {
         for (var rank : ranks.getRanks().entrySet()) {
             printRank(rank.getKey(), rank.getValue());
         }
     }
 
-    private void printRank(Rank rank, Integer count) {
+    private static void printRank(Rank rank, Integer count) {
         if (rank != Rank.NON_MATCH) {
             System.out.printf("%d개 일치 (%d원)- %d개%n", rank.getCount(), rank.getPrize(), count);
         }
