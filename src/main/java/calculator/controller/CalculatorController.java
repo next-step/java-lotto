240ee22f71ec66748e73478010469ae0f64b0a3e
Shin seongsu <tlstjdtn321@naver.com>
2022-11-06 21:21:19 +0900

    fix: service단에서 로직 처리 하도록 수정

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,9 +1,6 @@
 package calculator.controller;
 
-import calculator.domain.Number;
-import calculator.domain.Operator;
 import calculator.service.CalculatorService;
-import java.util.List;
 
 public class CalculatorController {
 
@@ -14,12 +11,7 @@ public class CalculatorController {
     }
 
     public int calculator(String input) {
-        List<String> splitResult = calculatorService.getOperations(input);
-
-        List<Number> numbers = calculatorService.getNumbers(splitResult);           // 숫자들 모음
-        List<Operator> operators = calculatorService.getOperators(splitResult);     // 연산 모음
-
-        return calculatorService.calculate(numbers, operators);
+        return calculatorService.calculate(input);
     }
 
 }
--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -10,7 +10,13 @@ public class CalculatorService {
     private static final String INPUT_ERROR_MESSAGE = "입력값으로 NULL 혹은 빈값은 입력할 수 없습니다,";
     private static final String DELIMITER = " ";
 
-    public List<String> getOperations(String input) {
+    public int calculate(String input) {
+        List<String> splitResult = getOperations(input);
+
+        return operatorCalculate(getNumbers(splitResult), getOperators(splitResult));
+    }
+
+    private List<String> getOperations(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(INPUT_ERROR_MESSAGE);
         }
@@ -18,21 +24,21 @@ public class CalculatorService {
         return List.of(input.split(DELIMITER));
     }
 
-    public List<Number> getNumbers(List<String> splitResult) {
+    private List<Number> getNumbers(List<String> splitResult) {
         return splitResult.stream()
             .filter(Number::isInteger)
             .map(Number::new)
             .collect(Collectors.toList());
     }
 
-    public List<Operator> getOperators(List<String> splitResult) {
+    private List<Operator> getOperators(List<String> splitResult) {
         return splitResult.stream()
             .filter(it -> !Number.isInteger(it))
             .map(Operator::getProperOperator)
             .collect(Collectors.toList());
     }
 
-    public int calculate(List<Number> numbers, List<Operator> operators) {
+    private int operatorCalculate(List<Number> numbers, List<Operator> operators) {
         Number result = numbers.get(0);
 
         int size = operators.size();
