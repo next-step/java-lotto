c7c8ff6863457d3047ff298aaa573aee4d4b2597
jiokim <jiokim222@gmail.com>
2022-11-06 13:30:55 +0900

    test(WinningLottoTest): 당첨번호에 보너스 번호가 포함시 error

--- src/test/java/lotto/backend/domain/WinningLottoTest.java
+++ src/test/java/lotto/backend/domain/WinningLottoTest.java
@@ -1,56 +1,22 @@
 package lotto.backend.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WinningLottoTest {
 
-    @Test
-    @DisplayName("1등 당첨")
-    void rank_1st() {
-        LottoTicket lottoTicket = LottoTicket.of(List.of(1, 3, 4, 36, 39, 40));
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 3, 4, 36, 39, 40), 30);
-        LottoRank rank = winningLotto.analysis(lottoTicket);
-        assertThat(rank).isEqualTo(LottoRank.FIRST);
-    }
-
-    @Test
-    @DisplayName("2등_보너스 당첨")
-    void rank_2nd_with_bonus() {
-        LottoTicket lottoTicket = LottoTicket.of(List.of(1, 3, 4, 36, 39, 40));
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 3, 4, 36, 39, 45), 40);
-        LottoRank rank = winningLotto.analysis(lottoTicket);
-        assertThat(rank).isEqualTo(LottoRank.SECOND);
-    }
-
-    @Test
-    @DisplayName("2등 당첨")
-    void rank_2nd() {
-        LottoTicket lottoTicket = LottoTicket.of(List.of(1, 3, 4, 36, 39, 40));
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 3, 4, 36, 39, 45), 41);
-        LottoRank rank = winningLotto.analysis(lottoTicket);
-        assertThat(rank).isEqualTo(LottoRank.THIRD);
-    }
-
-    @Test
-    @DisplayName("3등 당첨")
-    void rank_3rd() {
-        LottoTicket lottoTicket = LottoTicket.of(List.of(1, 3, 4, 36, 39, 40));
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 3, 4, 36, 37, 45), 40);
-        LottoRank rank = winningLotto.analysis(lottoTicket);
-        assertThat(rank).isEqualTo(LottoRank.FOURTH);
-    }
-
-    @Test
-    @DisplayName("4등 당첨")
-    void rank_4th() {
-        LottoTicket lottoTicket = LottoTicket.of(List.of(1, 3, 4, 36, 39, 40));
-        WinningLotto winningLotto = WinningLotto.of(List.of(1, 3, 4, 20, 37, 45), 40);
-        LottoRank rank = winningLotto.analysis(lottoTicket);
-        assertThat(rank).isEqualTo(LottoRank.FIFTH);
+    @DisplayName("당첨 로또번호에 보너스번호가 포함되있으면 Error")
+    @ParameterizedTest(name = "[{index}} 로또번호: [1,2,3,4,5,6] 보너스번호: {0} => error")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void check_bonus_number(int bonusNum) {
+        LottoTicket lottoTicket = LottoTicket.create(List.of(1, 2, 3, 4, 5, 6));
+        LottoNumber lottoNumber = new LottoNumber(bonusNum);
+        assertThrows(IllegalArgumentException.class
+                , () -> new WinningLotto(lottoTicket, lottoNumber));
     }
 }
