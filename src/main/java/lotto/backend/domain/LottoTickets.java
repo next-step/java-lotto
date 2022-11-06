506a251907f98a64753b45e1f56b38296b7b7655
jiokim <jiokim222@gmail.com>
2022-11-06 13:21:58 +0900

    feat(LottoTickets): 수동번호 & 자동번호 로또 세기 구현

--- src/main/java/lotto/backend/domain/LottoTickets.java
+++ src/main/java/lotto/backend/domain/LottoTickets.java
@@ -8,19 +8,31 @@ public class LottoTickets {
 
     private final List<LottoTicket> values;
 
-    public LottoTickets(final List<LottoTicket> value) {
-        this.values = new ArrayList<>(value);
+    public LottoTickets() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(List<LottoTicket> lottoTickets) {
+        values.addAll(new ArrayList<>(lottoTickets));
+    }
+
+    public int countOfAutoLotto() {
+        return (int) values.stream()
+                .filter(LottoTicket::isAutoType)
+                .count();
+    }
+
+    public int countOfCustomLotto() {
+        return values.size() - countOfAutoLotto();
     }
 
     public int size() {
         return values.size();
     }
 
-    public int orderAmount() {
-        return size() * LottoTicket.PRICE_PER_LOTTO;
-    }
-
     public List<LottoTicket> getValues() {
         return Collections.unmodifiableList(this.values);
     }
+
+
 }
