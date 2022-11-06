8b4b4770ee01a4e47b8291528cdc6c1fa951bc19
jiokim <jiokim222@gmail.com>
2022-11-06 13:34:23 +0900

    refactor(dto): 추가기능에 따른 리팩토링

--- src/main/java/lotto/backend/dto/LottoResultDto.java
+++ src/main/java/lotto/backend/dto/LottoResultDto.java
@@ -3,17 +3,29 @@ package lotto.backend.dto;
 import lotto.backend.domain.LottoRank;
 import lotto.backend.domain.LottoResult;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.Map;
 
 public class LottoResultDto {
 
-    private final Map<LottoRank, Integer> ranking;
+    private final Map<LottoRank, Integer> ranking = new EnumMap<>(LottoRank.class);
     private final double profit;
 
-    public LottoResultDto(Map<LottoRank, Integer> ranking, double profit) {
-        this.ranking = ranking;
+    public LottoResultDto(Map<LottoRank, Integer> rank, double profit) {
+        rankingConvertor(rank);
         this.profit = profit;
+    }
+
+    private void rankingConvertor(Map<LottoRank, Integer> ranks) {
+        Arrays.stream(LottoRank.values())
+                .filter(this::isWinning)
+                .forEach(rank -> ranking.put(rank, ranks.getOrDefault(rank, 0)));
+    }
+
+    private boolean isWinning(LottoRank rank) {
+        return rank != LottoRank.NOTHING;
     }
 
     public static LottoResultDto of(LottoResult lottoResult) {
