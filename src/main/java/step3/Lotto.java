674c04be2f02cb5061b6a90a444f26e1d471049a
wugatama <dnr7410@naver.com>
2022-11-07 00:35:54 +0900

    refactor: checkInt -> checkRange 와 checkSize 로 분리

--- src/main/java/step3/Lotto.java
+++ src/main/java/step3/Lotto.java
@@ -4,53 +4,57 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Lotto {
-	private static final int LOTTO_START = 1;
-	private static final int LOTTO_END = 45;
+    private static final int LOTTO_START = 1;
+    private static final int LOTTO_END = 45;
 
-	private Set<Integer> lotto;
+    private Set<Integer> lotto;
 
-	public Lotto(Set<Integer> lotto) {
-		this.lotto = lotto;
-	}
+    public Lotto(Set<Integer> lotto) {
+        this.lotto = lotto;
+    }
 
-	public Lotto(String[] inputs) {
-		this(makeLotto(inputs));
-	}
+    public Lotto(String[] inputs) {
+        this(makeLotto(inputs));
+    }
 
-	private static Set<Integer> makeLotto(String[] inputs) {
-		Set<Integer> lotto = new TreeSet<>();
-		for (String input : inputs) {
-			lotto.add(checkInt(lotto, input));
-		}
-		return lotto;
-	}
+    private static Set<Integer> makeLotto(String[] inputs) {
+        Set<Integer> lotto = new TreeSet<>();
+        for (String input : inputs) {
+            int ball = Integer.parseInt(input);
+            checkRange(ball);
+            lotto.add(ball);
+        }
+        checkSizeOfLotto(lotto);
+        return lotto;
+    }
 
-	private static Integer checkInt(Set<Integer> lotto, String input) {
-		int change = Integer.parseInt(input);
-		if (change < LOTTO_START || LOTTO_END < change) {
-			throw new IllegalArgumentException(LOTTO_START + " ~ " + LOTTO_END + " 수만 입력해주세요");
-		}
-		if (lotto.contains(change)) {
-			throw new IllegalArgumentException(LOTTO_START + " ~ " + LOTTO_END + " 중복되지 않는 수를 입력해주세요");
-		}
-		return change;
-	}
+    private static void checkRange(int ball) {
+        if (ball < LOTTO_START || ball > LOTTO_END) {
+            throw new IllegalArgumentException(LOTTO_START + " ~ " + LOTTO_END + " 사이의 수만 입력해주세요");
+        }
+    }
 
-	public boolean checkBonus(int bonus) {
-		return lotto.contains(bonus);
-	}
+    private static void checkSizeOfLotto(Set<Integer> lotto) {
+        if (lotto.size() != 6) {
+            throw new IllegalArgumentException(LOTTO_START + " ~ " + LOTTO_END + " 중복되지 않는 수를 입력해주세요");
+        }
+    }
 
-	public int count(Lotto other) {
-		return (int)other.getLotto().stream()
-			.filter(this::isExist)
-			.count();
-	}
+    public boolean checkBonus(int bonus) {
+        return lotto.contains(bonus);
+    }
 
-	public boolean isExist(Integer lottoNumber) {
-		return lotto.contains(lottoNumber);
-	}
+    public int count(Lotto other) {
+        return (int) other.getLotto().stream()
+                .filter(this::isExist)
+                .count();
+    }
 
-	public Set<Integer> getLotto() {
-		return lotto;
-	}
+    public boolean isExist(Integer lottoNumber) {
+        return lotto.contains(lottoNumber);
+    }
+
+    public Set<Integer> getLotto() {
+        return lotto;
+    }
 }
