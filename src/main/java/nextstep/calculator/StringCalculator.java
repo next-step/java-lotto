f93fb70a8dd497af6e77349e5c4491a876d08fde
jayden-17 <jayden.17@kakaocorp.com>
2022-11-06 17:37:41 +0900

    refactor : 긴 메소드 분리 (문자열 파싱 로직 분리)

--- src/main/java/nextstep/calculator/StringCalculator.java
+++ src/main/java/nextstep/calculator/StringCalculator.java
@@ -8,6 +8,19 @@ public class StringCalculator {
 	private static final String DELIMITER = " ";
 	public static Number calculate(final String input) {
 
+		final String[] s = parse(input);
+		final Number number = new Number(s[0]);
+		final List<Operation> operations = new ArrayList<>();
+		for (int i = 1; i < s.length; i = i + 2) {
+			operations.add(new Operation(s[i], s[i + 1]));
+		}
+
+		final Expression expression = new Expression(number, operations);
+		return expression.calculate();
+	}
+
+	private static String[] parse(final String input) {
+
 		if(input == null || input.isBlank()) {
 			throw new IllegalArgumentException("표현식은 공백일 수 없습니다.");
 		}
@@ -18,13 +31,6 @@ public class StringCalculator {
 			throw new IllegalArgumentException("유효하지 않은 포맷입니다.");
 		}
 
-		final Number number = new Number(s[0]);
-		final List<Operation> operations = new ArrayList<>();
-		for (int i = 1; i < s.length; i = i + 2) {
-			operations.add(new Operation(s[i], s[i + 1]));
-		}
-
-		final Expression expression = new Expression(number, operations);
-		return expression.calculate();
+		return s;
 	}
 }
