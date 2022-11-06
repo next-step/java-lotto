fcd1d4a3cdce25eb8e2034abc752889862715666
isakchoe <isakchoe@naver.com>
2022-11-06 16:49:33 +0900

    Test: ExceptionTest
    - 공백, 빈문자열 예외 테스트

---
+++ src/test/java/ExceptionTest.java
@@ -0,0 +1,19 @@
+
+import exception.SplitException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class ExceptionTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "", "   "})
+    @DisplayName("공백, 빈문자열 예외 테스트")
+    public void splitTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                SplitException.split(input));
+    }
+}
