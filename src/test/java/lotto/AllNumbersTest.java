cdd1c4fa53ed054f25de9807d6edf404470cd65f
sasha-com <sasha.com@kakaocorp.com>
2022-11-06 20:08:55 +0900

    [step2] feat: AllNumbersTest, WinningNumbersTest 클래스 추가 및 테스터 검증

---
+++ src/test/java/lotto/AllNumbersTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import lotto.numbers.AllNumbers;
+
+public class AllNumbersTest {
+    @Test
+    public void getNumbers() {
+        List<Integer> allNumbers = AllNumbers.getNumbers();
+        assertThat(allNumbers.isEmpty()).isEqualTo(false);
+        assertThat(allNumbers.size()).isEqualTo(AllNumbers.LAST_NUMBER);
+    }
+}
