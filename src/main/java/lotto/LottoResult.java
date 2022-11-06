a463087e6f09abf854358123d4b965de25113283
Hamstory <hamcom1212@gmail.com>
2022-11-06 23:45:38 +0900

    refactor: LottoResult.of 리팩토링

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,35 +1,31 @@
 package lotto;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum LottoResult {
-    NONE(0, 0),
-    FIFTH(3, 5000),
-    FOURTH(4, 50000),
-    THIRD(5, 1500000),
-    SECOND(5, 30000000),
-    FIRST(6, 2000000000);
+    NONE(0, (matchingCount, bonus) -> matchingCount >= 0 && matchingCount < 3, 0),
+    FIFTH(3, (matchingCount, bonus) -> matchingCount == 3, 5000),
+    FOURTH(4, (matchingCount, bonus) -> matchingCount == 4, 50000),
+    THIRD(5, (matchingCount, bonus) -> matchingCount == 5 && !bonus, 1500000),
+    SECOND(5, (matchingCount, bonus) -> matchingCount == 5 && bonus, 30000000),
+    FIRST(6, (matchingCount, bonus) -> matchingCount == 6, 2000000000);
 
     private final int matchingCount;
+    private final BiFunction<Integer, Boolean, Boolean> matchRule;
     private final int prize;
 
-    LottoResult(int matchingCount, int prize) {
+    LottoResult(int matchingCount, BiFunction<Integer, Boolean, Boolean> matchRule, int prize) {
         this.matchingCount = matchingCount;
+        this.matchRule = matchRule;
         this.prize = prize;
     }
 
     public static LottoResult of(int matchingCount, boolean bonus) {
-        if (matchingCount == 5 && bonus) {
-            return SECOND;
-        }
-        if (matchingCount == 5) {
-            return THIRD;
-        }
-
         return Arrays.stream(values())
-                .filter(result -> result.matchingCount == matchingCount)
+                .filter(result -> result.matchRule.apply(matchingCount, bonus))
                 .findFirst()
-                .orElse(NONE);
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     public int getMatchingCount() {
