52f0ac45cb81dfe6d4fff1315cac2d0bc7007744
sasha-com <sasha.com@kakaocorp.com>
2022-11-06 20:31:18 +0900

    [step2] fix: LottoTest 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,42 +1,53 @@
 package lotto;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import lotto.numbers.AllNumbers;
+import lotto.numbers.Lotto;
 
 public class LottoTest {
     @ParameterizedTest
-    @MethodSource("lottoTestParams")
-    public void constructors(int purchaseAmount, int purchaseCount) {
-        Lotto lotto = new Lotto(purchaseAmount);
+    @ValueSource(ints = {1, 3, 5, 10, 20})
+    public void generateNumbers(int purchaseCount) {
+        Lotto lotto = new Lotto(purchaseCount);
+        lotto.generateNumbers(purchaseCount);
 
-        assertThat(lotto.getPurchaseCount()).isEqualTo(purchaseCount);
-        assertThat(lotto.getNumbers()).isNotNull();
+        assertThat(lotto.getNumbersSize()).isEqualTo(purchaseCount);
+        lotto.getNumbers().forEach(numbers -> assertThat(numbers.size()).isEqualTo(Lotto.TOTAL_NUMBERS_SIZE));
     }
 
     @ParameterizedTest
-    @MethodSource("lottoTestParams")
-    public void generateNumbers(int purchaseAmount, int purchaseCount) {
-        Lotto lotto = new Lotto(purchaseAmount);
-        lotto.generateNumbers();
-
-        assertThat(lotto.getNumbers().size()).isEqualTo(purchaseCount);
-        lotto.getNumbers().forEach(numbers -> {
-            assertThat(numbers.size()).isEqualTo(Lotto.TOTAL_NUMBERS);
-        });
+    @MethodSource("containsNumberParam")
+    public void containsNumber(int index, int number) {
+        Lotto lotto = new Lotto(10);
+        lotto.generateNumbers(10);
+        assertThat(lotto.containsNumber(index, number)).isInstanceOfAny(Boolean.class);
     }
 
-    public static Stream<Arguments> lottoTestParams() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 10, 20})
+    public void getNumbers(int purchaseCount) {
+        Lotto lotto = new Lotto(purchaseCount);
+        lotto.generateNumbers(purchaseCount);
+
+        assertThat(lotto.getNumbers()).isNotEmpty();
+        assertThat(AllNumbers.getNumbers().containsAll(lotto.getNumbers().get(0))).isEqualTo(true);
+    }
+
+    public static Stream<Arguments> containsNumberParam() {
         return Stream.of(
-          Arguments.of(1000, 1000 / Lotto.AMOUNT),
-          Arguments.of(5000, 5000 / Lotto.AMOUNT),
-          Arguments.of(10000, 10000 / Lotto.AMOUNT),
-          Arguments.of(14000, 14000 / Lotto.AMOUNT),
-          Arguments.of(100000, 100000 / Lotto.AMOUNT)
+          Arguments.of(0, 44),
+          Arguments.of(1, 13),
+          Arguments.of(2, 23),
+          Arguments.of(3, 38),
+          Arguments.of(4, 41)
         );
     }
 }
