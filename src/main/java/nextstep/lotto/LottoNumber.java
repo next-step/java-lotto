3d714920c90ce8443c9fbd491b1f3e011106b5a6
jayden-17 <jayden.17@kakaocorp.com>
2022-11-07 22:49:12 +0900

    feat : 로또 숫자 구현

---
+++ src/main/java/nextstep/lotto/LottoNumber.java
@@ -0,0 +1,52 @@
+package nextstep.lotto;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+
+	private static final LottoNumber[] NUMBERS;
+	private static final int MIN = 1;
+	private static final int MAX = 45;
+
+	static {
+		NUMBERS = new LottoNumber[45];
+		for (int i = 0; i < 45; i++) {
+			NUMBERS[i] = new LottoNumber(i+1);
+		}
+	}
+	private final int number;
+
+	private LottoNumber(int number) {
+		this.number = number;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {return true;}
+		if (o == null || getClass() != o.getClass()) {return false;}
+
+		LottoNumber that = (LottoNumber) o;
+
+		return number == that.number;
+	}
+
+	@Override
+	public int hashCode() {
+		return number;
+	}
+
+	public static LottoNumber lottoNumber(final int number) {
+		if(number < MIN || number > MAX) {
+			throw new IllegalArgumentException("로또 번호는 1-45만 가능합니다.");
+		}
+		return NUMBERS[number-1];
+	}
+
+	@Override
+	public String toString() {
+		return number + "";
+	}
+
+	@Override
+	public int compareTo(LottoNumber o) {
+		return Integer.compare(this.number, o.number);
+	}
+}
---
+++ src/test/java/nextstep/lotto/LottoNumberTest.java
@@ -0,0 +1,60 @@
+package nextstep.lotto;
+
+import static nextstep.lotto.LottoNumber.lottoNumber;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LottoNumberTest {
+
+  @DisplayName("1-45 이외의 숫자는 입력할 수 없다.")
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 46})
+  public void spec01(final int invalidNumber) {
+    assertThatIllegalArgumentException().isThrownBy(() -> lottoNumber(invalidNumber));
+  }
+
+  @DisplayName("1-45 의 숫자로 생성 가능하다.")
+  @ParameterizedTest
+  @ValueSource(ints = {1, 4, 45})
+  public void spec02(final int number) {
+    assertThatNoException().isThrownBy(() -> lottoNumber(number));
+  }
+
+  @DisplayName("같은 번호인지 비교할 수 있다.")
+  @ParameterizedTest
+  @ValueSource(ints = {1, 5, 8, 19})
+  public void spec03(final int number) {
+    assertThat(lottoNumber(number)).isEqualTo(lottoNumber(number));
+  }
+
+  @DisplayName("로또 번호는 낮은 번호 순으로 정렬 가능하다.")
+  @Test
+  public void spec04() {
+
+    final List<LottoNumber> lottoNumbers = List.of(
+      lottoNumber(3),
+      lottoNumber(30),
+      lottoNumber(1)
+    );
+
+    final List<LottoNumber> sortedLottoNumbers =
+      lottoNumbers
+        .stream()
+        .sorted()
+        .collect(Collectors.toList());
+
+    assertThat(sortedLottoNumbers).containsExactly(
+      lottoNumber(1),
+      lottoNumber(3),
+      lottoNumber(30)
+    );
+  }
+}
