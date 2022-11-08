7fbf81c682bd6828c4725b53dc1e25a3b26735ca
jwoh <jwoh@wemakeprice.com>
2022-11-08 09:57:20 +0900

    refect : lottoList 일급객체로 변경되어 수동,자동 프린트, 로또 리스트 프린트 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,18 +5,19 @@ import java.util.List;
 import java.util.Map;
 
 import lotto.domain.Lotto;
-import lotto.domain.LottoList;
 import lotto.domain.LottoPlace;
-import lotto.domain.LottoWinners;
 
 public class OutputView {
 
     private OutputView() {
     }
 
-    public static void printLottoList(LottoList lottoList) {
-        printPriceCount(lottoList);
-        printLottoList(lottoList.getLottoList());
+    public static void printPriceCount(int manualCount, int autoCount) {
+        System.out.printf("수동으로 %d장, 자동으로 %d를 구매했습니다.\n", manualCount, autoCount);
+    }
+
+    public static void printLottoList(List<Lotto> lottoList) {
+        lottoList.forEach(System.out::println);
     }
 
     public static void printWinner(Map<LottoPlace, Long> winners) {
@@ -30,12 +31,5 @@ public class OutputView {
         System.out.printf("총 수익률은 %.2f입니다.", yield);
     }
 
-    private static void printPriceCount(LottoList lottoList) {
-        System.out.printf("수동으로 %d장, 자동으로 %d를 구매했습니다.\n", lottoList.getManualListCount()
-            , lottoList.getAutoListCount());
-    }
 
-    public static void printLottoList(List<Lotto> lottoList) {
-        lottoList.forEach(System.out::println);
-    }
 }
