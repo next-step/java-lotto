40abc58220977303168fda97a37596a988c30c65
https://github.com/youngreal <dudwls0505@naver.com>
2022-11-09 02:12:12 +0900

    refactor: value object 수정

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -36,4 +36,17 @@ public class LottoNumber {
     public int getNumber() {
         return number;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
