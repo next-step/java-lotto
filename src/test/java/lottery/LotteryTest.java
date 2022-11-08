cc1dc276dda77bd889e70761501b0eb62132a326
gmelon <hsh1769@naver.com>
2022-11-08 15:43:04 +0900

    test: 로또 번호가 6개가 아닐 경우에 대한 테스트 추가

--- src/test/java/lottery/LotteryTest.java
+++ src/test/java/lottery/LotteryTest.java
@@ -9,12 +9,18 @@ public class LotteryTest {
 
     @Test
     void create_6개미만() {
-        assertThatThrownBy(() -> createLottery(1, 2, 3))
+        assertThatThrownBy(() -> createLottery(1, 2, 3, 4, 5))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void create_중복() {
+    void create_6개초과() {
+        assertThatThrownBy(() -> createLottery(1, 2, 3, 4, 5, 6, 7))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void create_중복값() {
         assertThatThrownBy(() -> createLottery(1, 1, 2, 3, 4, 5))
                 .isInstanceOf(IllegalArgumentException.class);
     }
