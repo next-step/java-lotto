2d3a49c7d727d647cbc4bf219a5fc4329282fedf
wugatama <dnr7410@naver.com>
2022-11-09 22:18:20 +0900

    refactor: 유효성 검증 후 초기화 리뷰반영

--- src/main/java/step3/BonusLotto.java
+++ src/main/java/step3/BonusLotto.java
@@ -5,10 +5,10 @@ public class BonusLotto {
     private final Lotto lotto;
 
     public BonusLotto(Lotto lotto, int bonus) {
-        this.lotto = lotto;
         if (lotto.checkBonus(bonus)) {
             throw new IllegalArgumentException("보너스 번호는 지난 주 당첨 번호와 중복될 수 없습니다.");
         }
+        this.lotto = lotto;
         this.bonus = bonus;
     }
 
