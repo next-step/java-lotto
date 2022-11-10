f30e4cd2dcad31af4764998ca6ad065ff509cdd0
kureungkureung <terry9611@naver.com>
2022-11-10 22:41:41 +0900

    test(RankJudgmentInformationTest): 로또 순위 구하기

--- src/test/java/step2step3/lotto/RankJudgmentInformationTest.java
+++ src/test/java/step2step3/lotto/RankJudgmentInformationTest.java
@@ -1,18 +1,32 @@
 package step2step3.lotto;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RankJudgmentInformationTest {
-    @Test
-    void 보너스_숫자를_통해_2등을_판별할_수_있다() {
+
+    @MethodSource
+    @ParameterizedTest
+    void 순위를_반환한다(Set<Integer> lottoNumbers, Rank rank) {
         LottoTicket lottoTicket = LottoTicket.from(new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6)));
 
         RankJudgmentInformation rankJudgmentInformation = new RankJudgmentInformation(7, lottoTicket);
 
-        assertTrue(rankJudgmentInformation.isSecond(LottoTicket.from(new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 7)))));
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(lottoNumbers);
+        assertThat(rankJudgmentInformation.matchedRank(LottoTicket.from(numbersGenerator))).isEqualTo(rank);
+    }
+
+    private static Stream<Arguments> 순위를_반환한다() {
+        return Stream.of(
+                Arguments.of(Set.of(1, 2, 3, 4, 5, 6), Rank.FIRST),
+                Arguments.of(Set.of(1, 2, 3, 4, 5, 7), Rank.SECOND),
+                Arguments.of(Set.of(1, 2, 3, 4, 5, 8), Rank.THIRD)
+        );
     }
 }
