335fdbd9980cc663c5aed4b5d1d75da44866a03e
scoopsoup <rnrdud46@nate.com>
2022-11-13 02:01:39 +0900

    feat : 로또 결과구하기 & 출력

--- src/main/java/automaticlotto/controller/AutoLottoController.java
+++ src/main/java/automaticlotto/controller/AutoLottoController.java
@@ -1,9 +1,14 @@
 package automaticlotto.controller;
 
 import automaticlotto.domain.Lotto;
+import automaticlotto.domain.LottoWinning;
 import automaticlotto.domain.Lottos;
 import automaticlotto.view.InputView;
 import automaticlotto.view.OutputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static automaticlotto.domain.Lottos.createLottos;
 import static automaticlotto.view.OutputView.showBuyingLottos;
@@ -15,6 +20,13 @@ public class AutoLottoController {
         Lottos lottos = createLottos(buyLottoNumbers);
         showBuyingLottos(lottos);
         Lotto winningLotto = InputView.splitWinningNumbers();
+        List<Integer> lottoResult = Arrays.asList("0", "0", "0", "0", "0", "0", "0").stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        LottoWinning lottoWinning = new LottoWinning();
+        for (Lotto lotto : lottos.getLottos()) {
+            lottoResult = lottoWinning.discriminateLottoNumber(lotto, winningLotto, lottoResult);
+        }
+        OutputView.showWinningNumber(lottoResult);
+        int totalReward = lottoWinning.rankLottoWinner(lottoResult);
+        OutputView.showWinningTotalRate(purchaseAmount, totalReward);
     }
-
 }
