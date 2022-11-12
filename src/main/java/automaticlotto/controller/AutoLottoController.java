5aa4e31acf1178e63eec583bcf422788dc32cba5
scoopsoup <rnrdud46@nate.com>
2022-11-13 01:59:25 +0900

    feat : 로또 구매하기

--- src/main/java/automaticlotto/controller/AutoLottoController.java
+++ src/main/java/automaticlotto/controller/AutoLottoController.java
@@ -1,14 +1,8 @@
 package automaticlotto.controller;
 
-import automaticlotto.domain.Lotto;
-import automaticlotto.domain.LottoWinning;
 import automaticlotto.domain.Lottos;
 import automaticlotto.view.InputView;
 import automaticlotto.view.OutputView;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static automaticlotto.domain.Lottos.createLottos;
 import static automaticlotto.view.OutputView.showBuyingLottos;
