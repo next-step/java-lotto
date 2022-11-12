a063babad3dcb6c691e1a183ad13b5504b72a6d8
kureungkureung <terry9611@naver.com>
2022-11-12 22:18:49 +0900

    test(LottoNumberStorageTest): 로또 번호들 생성 기능

--- src/test/java/step2step3/lotto/LottoNumberStorageTest.java
+++ src/test/java/step2step3/lotto/LottoNumberStorageTest.java
@@ -2,13 +2,18 @@ package step2step3.lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberStorageTest {
     @Test
-    void 저장된_로또를_반환받는다() {
-        LottoNumberStorage lottoNumberStorage = new LottoNumberStorage();
+    void 숫자들을_로또_번호들로_변환시켜준다() {
+        NumbersGenerator numbersGenerator = new NumbersGenerator.Fake(Set.of(1, 2, 3, 4, 5, 6));
+        LottoNumberStorage lottoNumberStorage = new LottoNumberStorage(numbersGenerator);
 
-        assertThat(lottoNumberStorage.lottoNumber(5)).isEqualTo(new LottoNumber(5));
+        List<LottoNumber> lottoNumbers = List.of(new LottoNumber(1), new LottoNumber(2), new LottoNumber(3), new LottoNumber(4), new LottoNumber(5), new LottoNumber(6));
+        assertThat(lottoNumberStorage.lottoNumbers()).isEqualTo(lottoNumbers);
     }
 }
