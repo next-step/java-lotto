e97c83ddae13a6e821329a11f845d10dfda8e22e
lee <wnghkdcjdtns@naver.com>
2022-11-12 14:35:29 +0900

    tes 테스트 케이스 추가

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -1,4 +1,31 @@
 package lotto;
 
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+import lotto.domain.Lottos;
+import lotto.domain.WinningNumbers;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class LottoResultTest {
+
+    @Test
+    @DisplayName("수익률 테스트")
+    public void winningMatch(){
+        Lottos lottos = new Lottos(
+                List.of(
+                  new LottoTicket(List.of(1, 11, 5, 7, 12, 10))
+                , new LottoTicket(List.of(1, 15, 16, 17, 19, 20))
+                , new LottoTicket(List.of(1, 3, 5, 7, 9, 10))
+                ));
+        WinningNumbers winningNumbers = new WinningNumbers("1,3,5,7,9,10");
+        LottoResult lottoResult = new LottoResult(lottos,winningNumbers);
+        lottoResult.matchResult();
+        Assertions.assertThat(lottoResult.lottoYieldCalculate(5000)).isEqualTo(400010);
+    }
+
+
 }
--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -1,4 +1,41 @@
 package lotto;
 
+import lotto.domain.Lottos;
+import lotto.domain.WinningNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class WinningNumberTest {
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {3,5,6,7,8,9})
+    @DisplayName("당첨번호 매치 테스트")
+    public void winningNumbersMatch(int value){
+        WinningNumbers winningNumbers = new WinningNumbers("3,5,6,7,8,9");
+        assertThat(winningNumbers.getWinningNumbers()).contains(value);
+    }
+
+    @Test
+    @DisplayName("당첨번호가 6개 이상이 들어올 경우 테스트")
+    public void winningNumbersSizeException(){
+        assertThatThrownBy(() -> new WinningNumbers("3,5,6,7,8,9,10"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 6개만 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("빈값 테스트")
+    public void winningNumbersNullPointException(){
+        assertThatThrownBy(() -> new Lottos(new ArrayList<>()))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("빈값이 들어왔습니다.");
+    }
 }
