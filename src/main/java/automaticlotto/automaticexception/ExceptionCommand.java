0e5f6ffedd259b55a0e188d69343343b3c84439f
scoopsoup <rnrdud46@nate.com>
2022-11-13 01:25:46 +0900

    feat : 예외처리 문구 추가

--- src/main/java/automaticlotto/automaticexception/ExceptionCommand.java
+++ src/main/java/automaticlotto/automaticexception/ExceptionCommand.java
@@ -5,4 +5,7 @@ public class ExceptionCommand {
     public static final String PRICE_EXCEPTION_MESSAGE = "구입금액은 0보다 큰 숫자만 입력할 수 있습니다.";
     public static final String NULL_EXCEPTION_MESSAGE = "입력값은 빈값이 들어올 수 없습니다.";
     public static final String NUMBER_WINNING_NUMBERS_EXCEPTION_MESSAGE = "당첨번호는 6개입니다.";
+    public static final String LOTTO_NUMBER_RANGE_EXCEPTION_MESSAGE = "로또 번호는 1 ~ 45 사이의 숫자입니다.";
+    public static final String LOTTO_NUMBER_OVERLAP_EXCEPTION_MESSAGE = "로또 번호는 중복이 있을 수 없습니다.";
+    public static final String LOTTOS_EMPTY_EXCEPTION_MESSAGE = "로또는 빈값이 들어올 수 없습니다.";
 }
