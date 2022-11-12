19c4f82f7a6d860033cc0b498e7919045cab48bd
kureungkureung <terry9611@naver.com>
2022-11-12 15:44:13 +0900

    feat(LottoNumber): 로또 번호 동등성 비교

---
+++ src/main/java/step2step3/lotto/LottoNumber.java
@@ -0,0 +1,25 @@
+package step2step3.lotto;
+
+import java.util.Objects;
+
+public class LottoNumber {
+
+    private final int number;
+
+    public LottoNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
