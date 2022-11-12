b05c1fe9aa66fc8a2cec3e8d184677ed7b399ac3
kureungkureung <terry9611@naver.com>
2022-11-12 22:50:12 +0900

    feat(LottoTicket): 로또 번호 저장소를 통한 로또 티켓 생성 로직 추가

--- src/main/java/step2step3/lotto/LottoTicket.java
+++ src/main/java/step2step3/lotto/LottoTicket.java
@@ -13,6 +13,18 @@ public class LottoTicket {
         this.numbers = numbers;
     }
 
+    public static LottoTicket from(LottoNumberStorage lottoNumberStorage) {
+        List<LottoNumber> lottoNumbers = lottoNumberStorage.lottoNumbers();
+        verifyLottoNumbers(lottoNumbers);
+        return new LottoTicket(lottoNumbers);
+    }
+
+    private static void verifyLottoNumbers(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != NUMBER_OF_LOTTO) {
+            throw new IllegalArgumentException("로또 번호는 6개이어야 합니다.");
+        }
+    }
+
     public static LottoTicket from(NumbersGenerator numbersGenerator) {
         Set<Integer> numbers = numbersGenerator.numbers();
         verifyNumbers(numbers);
