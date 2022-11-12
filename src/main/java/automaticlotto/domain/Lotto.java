21a072d73e54f648811318e5023a113f4f486b0e
scoopsoup <rnrdud46@nate.com>
2022-11-13 01:48:05 +0900

    feat : 일급컬렉션(로또하나)

--- src/main/java/automaticlotto/domain/Lotto.java
+++ src/main/java/automaticlotto/domain/Lotto.java
@@ -1,35 +1,31 @@
 package automaticlotto.domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import automaticlotto.automaticexception.ExceptionCommand;
+import automaticlotto.automaticexception.InputValueException;
+
 import java.util.List;
 
 public class Lotto {
 
-    private static final int LOTTO_WINNING_NUMBER = 6;
-    private static final String[] lottoNumbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45"};
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private List<LottoNumber> lottoNumbers;
 
-    public List<String> getWinningLotto() {
-        sortNumber();
-        mixingNumber();
-        return winningNumber();
+    public Lotto(List<LottoNumber> lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void sortNumber() {
-        Collections.sort(Arrays.asList(lottoNumbers));
-    }
-
-    private void mixingNumber() {
-        Collections.shuffle(Arrays.asList(lottoNumbers));
-    }
-
-    private List<String> winningNumber() {
-        List<String> winningNumbers = new ArrayList<>();
-        for (int i = 0; i < LOTTO_WINNING_NUMBER; i++) {
-            winningNumbers.add(lottoNumbers[i]);
+    private void validateLottoNumbers(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new InputValueException(ExceptionCommand.NUMBER_WINNING_NUMBERS_EXCEPTION_MESSAGE);
         }
-        return winningNumbers;
+        if (lottoNumbers.size() != lottoNumbers.stream().map(LottoNumber::getLottoNumber).distinct().count()) {
+            throw new InputValueException(ExceptionCommand.LOTTO_NUMBER_OVERLAP_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return this.lottoNumbers;
     }
 
 }
