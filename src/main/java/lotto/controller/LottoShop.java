68b389038c0f5433c7892a3d0b15788f5568f109
Shin seongsu <tlstjdtn321@naver.com>
2022-11-13 03:12:08 +0900

    feat: lottoShop 에서 view 제어

--- src/main/java/lotto/controller/LottoShop.java
+++ src/main/java/lotto/controller/LottoShop.java
@@ -1,8 +1,14 @@
 package lotto.controller;
 
 import lotto.application.LottoService;
+import lotto.domain.*;
+import lotto.view.InputView;
+import lotto.view.ResultView;
 
 public class LottoShop {
+
+    private static final InputView INPUT_VIEW = new InputView();
+    private static final ResultView RESULT_VIEW = new ResultView();
 
     private final LottoService lottoService;
 
@@ -11,6 +17,15 @@ public class LottoShop {
     }
 
     public void purchase() {
-        lottoService.purchase();
+        PurchaseMoney purchaseMoney = new PurchaseMoney(INPUT_VIEW.inputPurchaseMoney());
+        Lottos lottos = lottoService.purchaseLottos(purchaseMoney);
+        RESULT_VIEW.printLottos(lottos);
+
+        LottoStatistics lottoStatistics = lottoService.statistics(INPUT_VIEW.inputWinningNumbers(), lottos);
+        AccordanceCount accordanceCount = lottoStatistics.collectAccordanceCount();
+        RESULT_VIEW.printWinningStatistics(accordanceCount);
+
+        double profitRatio = lottoStatistics.calculateProfitRatio(accordanceCount, purchaseMoney);
+        RESULT_VIEW.printProfitRatio(profitRatio);
     }
 }
