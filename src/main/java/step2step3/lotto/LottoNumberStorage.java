1ac76f35187bc06795b0a7a89560dae6a763c13f
kureungkureung <terry9611@naver.com>
2022-11-12 22:19:13 +0900

    feat(LottoNumberStorage): 로또 번호들 생성 기능

--- src/main/java/step2step3/lotto/LottoNumberStorage.java
+++ src/main/java/step2step3/lotto/LottoNumberStorage.java
@@ -1,12 +1,31 @@
 package step2step3.lotto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
 
 public class LottoNumberStorage {
+
+    private final NumbersGenerator numbersGenerator;
     private final Map<Integer, LottoNumber> lottoNumbers = new HashMap<>();
 
-    public LottoNumber lottoNumber(int number) {
+    public LottoNumberStorage(NumbersGenerator numbersGenerator) {
+        this.numbersGenerator = numbersGenerator;
+    }
+
+    public List<LottoNumber> lottoNumbers() {
+        Set<Integer> numbers = numbersGenerator.numbers();
+
+        return numbers.stream()
+                .map(this::lottoNumber)
+                .sorted()
+                .collect(toUnmodifiableList());
+    }
+
+    private LottoNumber lottoNumber(int number) {
         if (lottoNumbers.containsKey(number)) {
             return lottoNumbers.get(number);
         }
@@ -15,4 +34,5 @@ public class LottoNumberStorage {
         lottoNumbers.put(number, new LottoNumber(number));
         return lottoNumber;
     }
+
 }
