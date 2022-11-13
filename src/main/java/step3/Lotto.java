106f7d9f193d15150ad81f4d3af9ceed346e668f
wugatama <dnr7410@naver.com>
2022-11-13 16:00:26 +0900

    refactor: 유효성 검증은 해당 객체로 이동

--- src/main/java/step3/Lotto.java
+++ src/main/java/step3/Lotto.java
@@ -3,11 +3,14 @@ package step3;
 import java.util.Set;
 
 public class Lotto {
+    private static final int LOTTO_START = 1;
+    private static final int LOTTO_END = 45;
+    private static final int LOTTO_SIZE = 6;
+
     protected Set<Integer> lotto;
 
     public Lotto(Set<Integer> lotto) {
-        LottoValidator.validateLottoSize(lotto.size());
-        LottoValidator.validateLottoRange(lotto);
+        validate(lotto);
         this.lotto = lotto;
     }
 
@@ -23,6 +26,21 @@ public class Lotto {
 
     public boolean isExist(Integer lottoNumber) {
         return lotto.contains(lottoNumber);
+    }
+
+    private void validate(Set<Integer> lotto) {
+        if (lotto.size() != 6) {
+            throw new IllegalArgumentException("중복되지 않는 숫자 " + LOTTO_SIZE + "개를 입력해주세요.");
+        }
+        for (Integer integer : lotto) {
+            checkLottoRange(integer);
+        }
+    }
+
+    private void checkLottoRange(Integer integer) {
+        if (integer < 1 || integer > 45) {
+            throw new IllegalArgumentException(LOTTO_START + " ~ " + LOTTO_END + " 사이의 수만 입력해주세요");
+        }
     }
 
     @Override
