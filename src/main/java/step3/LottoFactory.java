e8476ebfc3a0149f69915d5ef99b94b28ff0210f
wugatama <dnr7410@naver.com>
2022-11-13 15:59:14 +0900

    refactor: LottoNumber 객체 분리

--- src/main/java/step3/LottoFactory.java
+++ src/main/java/step3/LottoFactory.java
@@ -1,40 +1,25 @@
 package step3;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class LottoFactory {
-    private static final int LOTTO_START = 1;
-    private static final int LOTTO_END = 45;
     private static final int LOTTO_PICK_START = 0;
     private static final int LOTTO_PICK_END = 5;
-    private List<Integer> balls = new ArrayList<>();
-
-    public LottoFactory() {
-        for (int i = LOTTO_START; i <= LOTTO_END; i++) {
-            this.balls.add(i);
-        }
-    }
+    private final LottoNumber lottoNumber = new LottoNumber();
 
     public Set<Integer> getRandomLotto() {
+        List<Integer> shuffleLottoNumber = lottoNumber.shuffle();
         Set<Integer> shuffle = new TreeSet<>();
-        Collections.shuffle(balls);
 
         for (int i = LOTTO_PICK_START; i <= LOTTO_PICK_END; i++) {
-            shuffle.add(balls.get(i));
+            shuffle.add(shuffleLottoNumber.get(i));
         }
-
         return shuffle;
     }
 
-    public Lotto makeWinningLotto(Set<Integer> winningNumber) {
-        return new Lotto(winningNumber);
-    }
-
-    public List<Integer> getBalls() {
-        return balls;
+    public WinningLotto makeWinningLotto(Set<Integer> winningNumber, int bonus) {
+        return new WinningLotto(winningNumber, bonus);
     }
 }
