8ffad3781cb3914fbff92dad7c5a79b214bbfa5a
owen.qq <owen.qq@kakaocorp.com>
2022-11-14 15:04:48 +0900

    refactor: 코드리뷰반영, LottoRewardTest 변경

--- src/test/java/lotto/LottoRewardTest.java
+++ src/test/java/lotto/LottoRewardTest.java
@@ -4,54 +4,50 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import lotto.model.LottoReward;
 
 public class LottoRewardTest {
+    Optional<LottoReward> maybeLottoReward;
 
-    @DisplayName("당첨개수와 보너스볼 매치 여부에 맞는 상금을 조회합니다.")
-    @Test
-    void findByMatchResult_get_secondplace() {
-        Optional<LottoReward> maybeLottoReward = LottoReward.findByMatchResult(5, true);
-
-        Assertions.assertAll(
-                () -> {
-                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward).isPresent();
-                },
-                () -> {
-                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward.get()).isEqualTo(LottoReward.SECOND_PLACE);
-                },
-                () -> {
-                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward.get().getRewardAmount()).isEqualTo(30000000);
-                }
-        );
-
-    }
-
-    @DisplayName("당첨개수와 보너스볼 매치 여부에 맞는 상금을 조회합니다.")
-    @Test
-    void findByMatchResult_get_thirdplace() {
-        Optional<LottoReward> maybeLottoReward = LottoReward.findByMatchResult(5, false);
+    @DisplayName("당첨됩니다")
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "6,true,true,2000000000",
+                    "6,false,true,2000000000",
+                    "5,true,true,30000000",
+            },
+            delimiter = ','
+    )
+    void findByMatchResult_get_secondplace(int matchCount, boolean bonusNumberMatched, boolean isPresent, int rewardAmount) {
+        Optional<LottoReward> maybeLottoReward = LottoReward.findByMatchResult(matchCount, bonusNumberMatched);
 
         Assertions.assertAll(
                 () -> {
-                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward).isPresent();
+                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward.isPresent()).isEqualTo(isPresent);
                 },
                 () -> {
-                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward.get()).isEqualTo(LottoReward.THIRD_PLACE);
-                },
-                () -> {
-                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward.get().getRewardAmount()).isEqualTo(1500000);
+                    org.assertj.core.api.Assertions.assertThat(maybeLottoReward.get().getRewardAmount()).isEqualTo(rewardAmount);
                 }
         );
     }
 
-    @DisplayName("당첨개수와 보너스볼 매치 여부에 맞는 상금을 조회합니다.")
-    @Test
-    void findByMatchResult_get_noplace() {
-        Optional<LottoReward> maybeLottoReward = LottoReward.findByMatchResult(1, false);
-
-        org.assertj.core.api.Assertions.assertThat(maybeLottoReward).isEmpty();
+    @DisplayName("당첨되지 않습니다")
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "1, true,true",
+                    "1, false,true",
+                    "2, true,true",
+                    "2, false,true"
+            },
+            delimiter = ','
+    )
+    void findByMatchResult_get_noplace(int matchCount, boolean bonusNumberMatched, boolean isEmpty) {
+        Optional<LottoReward> maybeLottoReward = LottoReward.findByMatchResult(matchCount, bonusNumberMatched);
+        org.assertj.core.api.Assertions.assertThat(maybeLottoReward.isEmpty()).isEqualTo(isEmpty);
     }
 }
