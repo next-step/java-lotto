29a0397d3aa34ef3567ba708c2bb207d5b009d88
sasha-com <sasha.com@kakaocorp.com>
2022-11-14 11:07:17 +0900

    fix: LottoTest 수정

--- src/test/java/lotto/numbers/LottoTest.java
+++ src/test/java/lotto/numbers/LottoTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import lotto.enums.Ranks;
+
 public class LottoTest {
     @Test
     public void generator() {
@@ -19,10 +21,19 @@ public class LottoTest {
     }
 
     @ParameterizedTest
-    @MethodSource("countSameNumberParam")
-    public void countSameNumbers(List<Integer> numbers) {
+    @MethodSource("getRankOfNumbersFailedParam")
+    public void getRankOfNumbersFailed(List<Integer> numbers, int bonusNumber) {
         Lotto lotto = new Lotto();
-        assertThat(lotto.countSameNumbers(numbers)).isInstanceOf(Integer.class);
+        assertThatThrownBy(() -> {
+            lotto.getRankOfNumbers(numbers, bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getRankOfNumbersSuccessParam")
+    public void getRankOfNumbers(List<Integer> numbers, int bonusNumber) {
+        Lotto lotto = new Lotto();
+        assertThat(lotto.getRankOfNumbers(numbers, bonusNumber)).isInstanceOf(Ranks.class);
     }
 
     @Test
@@ -36,13 +47,23 @@ public class LottoTest {
 
     }
 
-    public static Stream<Arguments> countSameNumberParam() {
+    public static Stream<Arguments> getRankOfNumbersSuccessParam() {
         return Stream.of(
-          Arguments.of(List.of(1, 2, 3, 4, 5, 6)),
-          Arguments.of(List.of(13, 26, 28, 33, 39)),
-          Arguments.of(List.of(2, 23)),
-          Arguments.of(List.of(3, 38, 40, 41)),
-          Arguments.of(List.of(4, 41, 42))
+          Arguments.of(List.of(1, 2, 3, 4, 5, 6), 7),
+          Arguments.of(List.of(13, 26, 28, 33, 39, 44), 43),
+          Arguments.of(List.of(2, 23, 34, 35, 41, 45), 7),
+          Arguments.of(List.of(3, 38, 40, 41, 42, 43), 17),
+          Arguments.of(List.of(4, 9, 16, 19, 26, 33), 40)
+        );
+    }
+
+    public static Stream<Arguments> getRankOfNumbersFailedParam() {
+        return Stream.of(
+                Arguments.of(List.of(0, 2, 3, 4, 5, 6), 7),
+                Arguments.of(List.of(13, 26, 28, 33, 39), 46),
+                Arguments.of(List.of(2, 23), 44),
+                Arguments.of(List.of(3, 38, 40, 41, 44, 46), 39),
+                Arguments.of(List.of(4, 15, 33, 37, 41, 42), 42)
         );
     }
 }
