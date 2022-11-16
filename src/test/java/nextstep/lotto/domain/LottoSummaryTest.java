dd5d8a19410a5a12cbac95d051563217a79f9d4f
jayden-17 <jayden.17@kakaocorp.com>
2022-11-16 23:55:51 +0900

    fix : 당첨 결과 변경(미당첨 순위도 표지)

--- src/test/java/nextstep/lotto/domain/LottoSummaryTest.java
+++ src/test/java/nextstep/lotto/domain/LottoSummaryTest.java
@@ -15,7 +15,8 @@ class LottoSummaryTest {
   public void spec01() {
     final LottoSummary lottoSummary1 = new LottoSummary(1000, List.of(LottoResult.FOURTH_PLACE));
     final LottoSummary lottoSummary2 = new LottoSummary(1000, List.of(
-      LottoResult.FOURTH_PLACE, LottoResult.LOSE, LottoResult.LOSE, LottoResult.LOSE, LottoResult.LOSE)
+      LottoResult.FOURTH_PLACE, LottoResult.LOSE, LottoResult.LOSE, LottoResult.LOSE,
+      LottoResult.LOSE)
     );
     Assertions.assertAll(
       () -> assertThat(lottoSummary1.rateOfReturn()).isEqualTo(new BigDecimal("50.00")),
@@ -26,10 +27,15 @@ class LottoSummaryTest {
   @DisplayName("당첨된 정보를 조회한다.")
   @Test
   public void spec02() {
-    final LottoSummary lottoSummary = new LottoSummary(1000, List.of(LottoResult.FIRST_PLACE, LottoResult.FOURTH_PLACE, LottoResult.FOURTH_PLACE, LottoResult.LOSE, LottoResult.LOSE));
+    final LottoSummary lottoSummary = new LottoSummary(1000,
+      List.of(LottoResult.FIRST_PLACE, LottoResult.FOURTH_PLACE, LottoResult.FOURTH_PLACE,
+        LottoResult.LOSE, LottoResult.LOSE));
 
     assertThat(lottoSummary.winResults()).containsExactly(
+      new LottoWinCount(LottoResult.FIFTH_PLACE, 0),
       new LottoWinCount(LottoResult.FOURTH_PLACE, 2),
+      new LottoWinCount(LottoResult.THIRD_PLACE, 0),
+      new LottoWinCount(LottoResult.SECOND_PLACE, 0),
       new LottoWinCount(LottoResult.FIRST_PLACE, 1)
     );
   }
