e62829445d2c356a225c475999e2ac4f688c9de6
sasha-com <sasha.com@kakaocorp.com>
2022-11-17 20:21:09 +0900

    fix: Lotto.getRankOfNumbers에서 호출되는 validation 메서드 분리

--- src/main/java/lotto/numbers/Lotto.java
+++ src/main/java/lotto/numbers/Lotto.java
@@ -1,7 +1,6 @@
 package lotto.numbers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -21,13 +20,15 @@ public class Lotto {
     }
 
     public Ranks getRankOfNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        validateNumbers(winningNumbers, bonusNumber);
+        validateWinningNumbers(winningNumbers, bonusNumber);
+        validateBonusNumbers(bonusNumber);
+
         List<Integer> pickedNumbers = new ArrayList<>(List.copyOf(numbers));
         pickedNumbers.retainAll(winningNumbers);
         return getRanks(pickedNumbers, bonusNumber);
     }
 
-    private void validateNumbers(List<Integer> winningNumbers, int bonusNumber) {
+    private void validateWinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         if(winningNumbers.isEmpty() || winningNumbers.size() != Lotto.TOTAL_NUMBERS_SIZE) {
             throw new IllegalArgumentException("당첨번호는 6자리여야 합니다.");
         }
@@ -40,12 +41,14 @@ public class Lotto {
             throw new IllegalArgumentException("각 당첨번호의 범위는 1에서 45사이의 숫자입니다.");
         }
 
-        if(bonusNumber <= 0 || bonusNumber > AllNumbers.LAST_NUMBER) {
-            throw new IllegalArgumentException("보너스 번호의 범위는 1에서 45사이의 숫자입니다.");
-        }
-
         if(winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호는 당첨 번호 이외의 숫자여야 합니다.");
+        }
+    }
+
+    private void validateBonusNumbers(int bonusNumber) {
+        if(bonusNumber <= 0 || bonusNumber > AllNumbers.LAST_NUMBER) {
+            throw new IllegalArgumentException("보너스 번호의 범위는 1에서 45사이의 숫자입니다.");
         }
     }
 
