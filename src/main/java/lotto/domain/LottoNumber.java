c7ea36d533017fdf58eee6ec897e2058aedc6f1b
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2022-11-20 01:03:00 +0900

    refactor 로또넘버 메소드 수정

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,15 +9,17 @@ import java.util.stream.IntStream;
 public class LottoNumber {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
-    private static final List<Integer> lottoNumbers;
+    private static final List<Integer> numbers;
 
     static{
-        lottoNumbers = IntStream.range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER + 1)
+        numbers = IntStream.range(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER + 1)
                 .boxed()
                 .collect(Collectors.toList());
     }
 
     public static List<Integer> getLottoNumbers(){
+        Collections.shuffle(numbers);
+        List<Integer> lottoNumbers = new ArrayList<>(numbers.subList(0, 6));
         return lottoNumbers;
     }
 }
