95453d2938009de066d1ca415f518c7c6ab7b347
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2022-11-20 01:13:56 +0900

    refactor println 삭제

--- src/main/java/calculator/CalculatorApplication.java
+++ src/main/java/calculator/CalculatorApplication.java
@@ -8,7 +8,6 @@ import static calculator.view.ResultView.resultCalculator;
 public class CalculatorApplication {
 
     public static void main(String[] args) {
-        System.out.println(Operator.valueOf("-"));
         Calculator calculator = new Calculator(inputNumber());
         resultCalculator(calculator.startCalculate());
     }
