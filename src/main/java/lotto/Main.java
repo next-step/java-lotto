7cfdbf57e598fb5d8971f9f4a58ad8b12d18f2bf
https://github.com/youngreal <dudwls0505@naver.com>
2022-11-19 23:52:23 +0900

    refactor: LottoFactory에서 수량관리, validation 해보기

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -2,21 +2,24 @@ package lotto;
 
 import lotto.model.*;
 
+import static lotto.model.LottoNumber.winningToSet;
 import static lotto.view.InputView.*;
 import static lotto.view.ResultView.*;
 
 public class Main {
     public static void main(String[] args) {
         Price price = new Price(buyPrice());
-        ManualLotto manualQuantity = new ManualLotto(manual(), price);
+        int manualQuantity = manual();
 
-        int restQuantity = manualQuantity.rest(price);
+        LottoFactory lottoFactory = new LottoFactory(manualQuantity, price);
+        int autoQuantity = lottoFactory.rest(price);
 
-        Lottos buyLotto = new Lottos(new LottoFactory().addLotto(restQuantity, manualNumber(manualQuantity)));
-        printQuantity(manualQuantity, restQuantity);
+        Lottos buyLotto = new Lottos(lottoFactory.addLotto(autoQuantity, manualNumber(manualQuantity)));
+        printQuantity(lottoFactory, autoQuantity);
         printBuyLottos(buyLotto);
 
-        RankGroup rankGroup = buyLotto.collectRanks(winningLotto());
+        WinningLotto winningLotto = new WinningLotto(new Lotto(winningToSet(winningLotto())), LottoNumber.cacheLottoNumber(bonusLotto()));
+        RankGroup rankGroup = buyLotto.collectRanks(winningLotto);
 
         Profit profit = new Profit(price.totalQuantity());
         printResult(rankGroup, profit.calculate(rankGroup));
--- src/main/java/lotto/model/LottoFactory.java
+++ src/main/java/lotto/model/LottoFactory.java
@@ -1,13 +1,28 @@
 package lotto.model;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class LottoFactory {
     public static final int LOTTO_MAX_LENGTH = 6;
     private static final int LOTTO_INIT_LENGTH = 0;
+    private final int manualQuantity;
 
-    public List<Lotto> addLotto(int auto, final List<Lotto> manualLotto) {
-        List<Lotto> lottos = new ArrayList<>(manualLotto);
+    public LottoFactory(int manualQuantity, Price price) {
+        validation(manualQuantity, price);
+        this.manualQuantity = manualQuantity;
+    }
+
+    private void validation(int manualQuantity, Price price) {
+        price.manualValidation(manualQuantity);
+    }
+
+    public List<Lotto> addLotto(int auto, final Map<Integer, List<String>> manualLotto) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int count = 0; count < manualQuantity; count++) {
+            lottos.add(new Lotto(createManualLotto(manualLotto, count)));
+        }
 
         for (int i = 0; i < auto; i++) {
             lottos.add(new Lotto(createAutoLotto()));
@@ -16,9 +31,23 @@ public class LottoFactory {
         return lottos;
     }
 
+    private Set<LottoNumber> createManualLotto(final Map<Integer, List<String>> manualLotto, int count) {
+        return manualLotto.get(count).stream()
+                .map(manualLottoNumber -> LottoNumber.cacheLottoNumber(Integer.parseInt(manualLottoNumber)))
+                .collect(Collectors.toSet());
+    }
+
     private Set<LottoNumber> createAutoLotto() {
         List<LottoNumber> list = new ArrayList<>(LottoNumber.cache().values());
         Collections.shuffle(list);
         return new HashSet<>(list.subList(LOTTO_INIT_LENGTH, LOTTO_MAX_LENGTH));
+    }
+
+    public int rest(Price price) {
+        return price.restPrice(manualQuantity);
+    }
+
+    public int getManualQuantity() {
+        return manualQuantity;
     }
 }
