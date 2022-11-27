2d441db9efc21cccf727a9208abb9ab63219d015
youngreal <dudwls0505@naver.com>
2022-11-27 22:02:30 +0900

    refactor: Enum

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -3,23 +3,23 @@ package lotto.model;
 import java.util.Arrays;
 
 public enum Rank {
-    MISS(0, new Price(0)),
-    FIVE(3, new Price(5_000)),
-    FOUR(4, new Price(50_000)),
-    THREE(5, new Price(1_500_000)),
-    TWO(5, new Price(30_000_000)),
-    ONE(6, new Price(2_000_000_000));
+    MISS(0, new Money(0)),
+    FIVE(3, new Money(5_000)),
+    FOUR(4, new Money(50_000)),
+    THREE(5, new Money(1_500_000)),
+    TWO(5, new Money(30_000_000)),
+    ONE(6, new Money(2_000_000_000));
 
     private final int order;
-    private final Price price;
+    private final Money money;
 
-    Rank(int order, Price price) {
+    Rank(int order, Money money) {
         this.order = order;
-        this.price = price;
+        this.money = money;
     }
 
     public int calculatePrice(int value) {
-        return price.total(value);
+        return money.total(value);
     }
 
     public static Rank findRank(int count, boolean isBonus) {
@@ -38,6 +38,6 @@ public enum Rank {
     }
 
     public int getPrice() {
-        return price.getPrice();
+        return money.getPrice();
     }
 }
