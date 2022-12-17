4541ba9b105b5720a91c6737c1b2d9674e49de7a
isakchoe <isakchoe@naver.com>
2022-12-17 23:23:31 +0900

    refactor: InputView
    - inputView: 당첨번호 입력 항목 추가 
    - InputException : 예외값 처리

--- src/main/java/step2/view/InputException.java
+++ src/main/java/step2/view/InputException.java
@@ -3,15 +3,36 @@ package step2.view;
 public class InputException {
 
 
-    public static boolean inputAlpha(String input) {
-        if (isAlpha(input)) {
+    public static boolean isInputMoneyPossible(String input) {
+        if (isAlpha(input) == 1 ) {
             System.out.println("숫자값만 입력해주세요.");
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
-    private static boolean isAlpha(String input) {
-        return input.matches("[a-zA-Z]+");
+    private static int isAlpha(String input) {
+        if (input.matches("[a-zA-Z]+")) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static boolean isInputLottoPossible(String[] strArr) {
+        int errResult = checkStrArr(strArr);
+
+        if (errResult != 0 ) {
+            System.out.println("숫자값만 입력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    private static int checkStrArr(String[] input) {
+        int errNum = 0;
+        for (String str : input) {
+            errNum += isAlpha(str);
+        }
+        return errNum;
     }
 }
--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -2,25 +2,34 @@ package step2.view;
 
 import java.util.Scanner;
 
+
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-
         String inputMoney = scanner.nextLine();
-
-        if (InputException.inputAlpha(inputMoney)) {
+        if (InputException.isInputMoneyPossible(inputMoney)) {
             scanner = new Scanner(System.in);
             return inputMoney();
         }
-
         int money = Integer.parseInt(inputMoney);
         int number = money / 1000;
-
-        System.out.println(number);
+        System.out.print(number);
         System.out.println("개를 구매했습니다.");
-
         return number;
+    }
+
+    public static String[] inputWinningLotto() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+
+        String input = scanner.nextLine();
+        String[] winningLotto = input.split(",");
+
+        if (InputException.isInputLottoPossible(winningLotto)) {
+            scanner = new Scanner(System.in);
+            return inputWinningLotto();
+        }
+        return winningLotto;
     }
 }
