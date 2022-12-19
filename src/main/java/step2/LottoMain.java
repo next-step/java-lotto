ae92d3f5cafc4f211d800f7dfb5b8ff8bcc9d8f5
isakchoe <isakchoe@naver.com>
2022-12-19 21:09:37 +0900

    refactor: LottoMain
    - 메인 함수 구현

--- src/main/java/step2/LottoMain.java
+++ src/main/java/step2/LottoMain.java
@@ -1,10 +1,25 @@
 package step2;
 
+import step2.domain.Lotto;
+import step2.domain.Lottos;
+import step2.domain.WinningLotto;
 import step2.view.InputView;
+import step2.view.ResultView;
 
 public class LottoMain {
 
     public static void main(String[] args) {
-        InputView.inputMoney();
+
+        int lottoCount = InputView.inputMoney();
+        Lottos buyLottos = new Lottos(lottoCount);
+        ResultView.printBuyLottos(buyLottos);
+
+        Integer[] inputWinningLotto = InputView.inputWinningLotto();
+        Lotto winLotto = new Lotto(inputWinningLotto);
+
+        WinningLotto winningLotto = new WinningLotto(buyLottos, winLotto);
+        winningLotto.match();
+
+        ResultView.printResult(winningLotto);
     }
 }
