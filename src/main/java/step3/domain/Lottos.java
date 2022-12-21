6568434d2e8b58ab310a5e1f7d6ba01e7763a8d6
isakchoe <isakchoe@naver.com>
2022-12-21 23:16:23 +0900

    feat - Lottos
     Lotto 를 포장하는 일급 콜렉션

---
+++ src/main/java/step3/domain/Lottos.java
@@ -0,0 +1,28 @@
+package step3.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+
+    private List<Lotto> lottoList = new ArrayList<>();
+
+    public Lottos(int buyCount) {
+        for (int i = 0; i < buyCount; i++) {
+            Lotto lotto = new Lotto();
+            lottoList.add(lotto);
+        }
+    }
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottoList = lottos;
+    }
+
+    public int buyCount() {
+        return lottoList.size();
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
+    }
+}
