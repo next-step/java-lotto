e12457c9a27e85cb2c59b44aa6708c8391eaf18b
isakchoe <isakchoe@naver.com>
2022-12-24 16:30:52 +0900

    refactor - ResultView
    - 2등 당첨 추가

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -2,7 +2,7 @@ package step2.view;
 
 import step2.domain.Lotto;
 import step2.domain.Lottos;
-import step2.domain.WinningLotto;
+import step2.domain.LottoResult;
 
 import java.util.List;
 
@@ -16,21 +16,24 @@ public class ResultView {
         System.out.println();
     }
 
-    public static void printResult(WinningLotto winningLotto) {
+    public static void printResult(LottoResult lottoResult) {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("-----------");
 
-        int fourthCount = winningLotto.getGradeCount(4);
-        int thirdCount = winningLotto.getGradeCount(3);
-        int fifthCount = winningLotto.getGradeCount(5);
-        int firstCount = winningLotto.getGradeCount(1);
-        double revenue = winningLotto.getRevenue();
+        int fifthCount = lottoResult.getGradeCount(5);
+        int fourthCount = lottoResult.getGradeCount(4);
+        int thirdCount = lottoResult.getGradeCount(3);
+        int secondCount = lottoResult.getGradeCount(2);
+        int firstCount = lottoResult.getGradeCount(1);
+        double revenue = lottoResult.getRevenue();
 
         System.out.printf("3개 일치 (5000원)- %d개\n", fifthCount);
         System.out.printf("4개 일치 (50000원)- %d개\n", fourthCount);
         System.out.printf("5개 일치 (1500000원)- %d개\n", thirdCount);
+        System.out.printf("5개 일치, 보너스 볼 일치(3000000원)- %d개\n", secondCount);
         System.out.printf("6개 일치 (2000000000원)- %d개\n", firstCount);
+        System.out.println(revenue);
         System.out.printf("총 수익률은 %.2f입니다.", revenue);
 
         if (revenue < 1) {
