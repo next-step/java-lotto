814e9f54178de7e5f330f6f703849131bae6cd91
awa-intra1 <wnghkdcjdtns@naver.com>
2022-12-25 22:54:49 +0900

    test 로직 변경에 따른 테스트 로직 수정

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -17,7 +17,7 @@ public class WinningNumberTest {
     @Test
     @DisplayName("보너스 번호 매치 테스트")
     public void bonusNumberMatch(){
-        List<LottoNumber> lottoNumbers = LottoTicket.valueOf(List.of(3,5,7,9,13,11));
+        List<LottoNumber> lottoNumbers = LottoTicket.toLottoTickets(3,5,7,9,13,11);
         WinningNumbers winningNumbers = new WinningNumbers(List.of(3,5,7,9,6,10),11);
         assertThat(winningNumbers.bonusNumberMatch(lottoNumbers)).isTrue();
     }
@@ -25,14 +25,14 @@ public class WinningNumberTest {
     @Test
     @DisplayName("보너스 번호 매치 실패 테스트")
     public void bonusNumberFailMatch(){
-        List<LottoNumber> lottoNumbers = LottoTicket.valueOf(List.of(3,5,7,9,13,11));
+        List<LottoNumber> lottoNumbers = LottoTicket.toLottoTickets(3,5,7,9,13,11);
         WinningNumbers winningNumbers = new WinningNumbers(List.of(3,5,7,9,6,10),12);
         assertThat(winningNumbers.bonusNumberMatch(lottoNumbers)).isFalse();
     }
     @Test
     @DisplayName("당첨번호 당첨 갯수 테스트")
     public void winningNumbersMatch(){
-        List<LottoNumber> lottoNumbers = LottoTicket.valueOf(List.of(3,5,7,9,13,11));
+        List<LottoNumber> lottoNumbers = LottoTicket.toLottoTickets(3,5,7,9,13,11);
         WinningNumbers winningNumbers = new WinningNumbers(List.of(3,5,7,9,18,17),15);
         assertThat(winningNumbers.lottoMatchCount(lottoNumbers)).isEqualTo(4);
     }
