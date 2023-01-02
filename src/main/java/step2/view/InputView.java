6485a7210a18dcca97f77747cf2a8cdb4e7fe37b
isakchoe <isakchoe@naver.com>
2023-01-02 22:37:02 +0900

    refactor - InputView
    - 수동 구매 입력 추가

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -1,5 +1,6 @@
 package step2.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -17,23 +18,45 @@ public class InputView {
             return inputMoney();
         }
         int money = Integer.parseInt(inputMoney);
-        int number = money / 1000;
-        System.out.print(number);
-        System.out.println("개를 구매했습니다.");
-        return number;
+        return money;
+    }
+
+    public static int inputManualBuyCount(int money) {
+        System.out.println();
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        String inputCount = scanner.nextLine();
+        if (!InputException.isInputCountPossible(inputCount, money)) {
+            scanner = new Scanner(System.in);
+            return inputManualBuyCount(money);
+        }
+        int count = Integer.parseInt(inputCount);
+        return count;
+    }
+
+    public static List<List<Integer>> inputManualLotto(int count) {
+        System.out.println();
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        List<List<Integer>> manualLottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            manualLottos.add(getLotto());
+        }
+        return manualLottos;
     }
 
     public static List<Integer> inputWinningLotto() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return getLotto();
+    }
 
+    private static List<Integer> getLotto() {
         String input = scanner.nextLine();
-        String[] winningLotto = input.split(", ");
-
-        if (!InputException.isInputLottoPossible(winningLotto) || !InputException.isInputLottoRangePossible(winningLotto)) {
+        String[] lotto = input.split(", ");
+        if (!InputException.isInputLottoPossible(lotto) || !InputException.isInputLottoRangePossible(lotto)) {
             scanner = new Scanner(System.in);
-            return inputWinningLotto();
+            System.out.println("번호를 다시 입력해 주세요.");
+            return getLotto();
         }
-        return Arrays.stream(winningLotto).map(Integer::parseInt).collect(Collectors.toList());
+        return Arrays.stream(lotto).map(Integer::parseInt).collect(Collectors.toList());
     }
 
     public static int inputBonusNumber() {
