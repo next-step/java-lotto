6a86471de63bd999d60878aaf1e1bbfdbbcdbd1f
heoseungyeon <dia0312@naver.com>
2023-01-12 20:31:24 +0900

    feat: LottoNumbers 생성 메서드 구현

---
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -0,0 +1,26 @@
+package lotto.domain;
+
+import java.util.List;
+
+import lotto.exception.ErrorMessage;
+import lotto.exception.InvalidLottoNumberQuantityException;
+
+public class LottoNumbers {
+
+	private final List<Integer> numbers;
+
+	private LottoNumbers(List<Integer> numbers) {
+		if (!isValidNumbersSize(numbers)) {
+			throw new InvalidLottoNumberQuantityException(ErrorMessage.LOTTO_NUMBERS_QUANTITY_MUST_BE_SIX);
+		}
+		this.numbers = numbers;
+	}
+
+	public static LottoNumbers of(List<Integer> numbers) {
+		return new LottoNumbers(numbers);
+	}
+
+	private boolean isValidNumbersSize(List<Integer> numbers) {
+		return numbers.size() == Lotto.LOTTO_NUMBER_QUANTITY;
+	}
+}
