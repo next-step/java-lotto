8b59a471e41f5e262a61a1f5568dd43b198986a7
rock.j <rock.j@kakaopaysec.com>
2023-03-09 10:14:36 +0900

    안쓰는 메소드 정리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
@@ -34,9 +35,7 @@ public class Lotto {
     // 당첨 통계 결과 map으로 추출
     public Map<LottoType ,Long>  winningStatistics( List<LottoObject> lottoObjects, List<Integer> winningNumbers, int bonusNumber ){
         return lottoObjects.stream().map(object -> object.contains(winningNumbers,bonusNumber))
-                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
-                .entrySet()
-                .stream().collect( Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
     }
 
     // 당첨번호 리스트 추출
@@ -45,7 +44,7 @@ public class Lotto {
         return  Arrays.stream(winning).map(Integer::parseInt).collect(Collectors.toList());
     }
 
-    public void lotto(int amount , String winngingText , int bonusNumber){
+    public BigDecimal lotto(int amount , String winngingText , int bonusNumber){
         System.out.println("구입금액을 입력해 주세요");
         System.out.println(amount);
 
@@ -68,69 +67,25 @@ public class Lotto {
 
         Map<LottoType ,Long> results =winningStatistics(lottoObjects ,winningNumbers ,bonusNumber);// 당첨 통계 메소드
 
-        //System.out.println(results.toString());
-
         printWinningList(results); // 당첨 통계  ui 메소드
 
         BigDecimal rate = rateResult(results,amount); // 수익률  계산 메소드
 
         System.out.println("총 수익률은 "+rate+"입니다.");
 
-       // ga.entrySet().stream().re
+       return rate;
     }
 
     public BigDecimal rateResult(Map<LottoType ,Long>  results, int amount){
         AtomicInteger sum = new AtomicInteger();
-        results.forEach((key,value) -> {
-            sum.addAndGet(key.getPriceMultiply(value));
-        });
-        return BigDecimal.valueOf(sum.get()).divide(BigDecimal.valueOf(amount),2,BigDecimal.ROUND_DOWN);
+        results.forEach((key,value) -> sum.addAndGet(key.getPriceMultiply(value)));
+        return BigDecimal.valueOf(sum.get()).divide(BigDecimal.valueOf(amount), 2, RoundingMode.DOWN);
     }
     public void printWinningList( Map<LottoType ,Long>   winningResult){
         winningResult.forEach((key, value) -> System.out.println(key.getPrintString() + "- " + value + "개"));
     }
 
-
-    public static void main(String[] args){
-        System.out.println("구입금액을 입력해 주세요");
-       // Scanner scanner = new Scanner(System.in);
-        int amount =15000;  // 금액
-        int number = amount/LOTTO_PRICE; // 반복 갯수
-        System.out.println(number+"개를 구매했습니다.");
-        List<Integer> result =  new ArrayList<>();
-        IntStream.range(1, 45).forEach(result::add);
-
-        List<LottoObject> lottoObjects = new ArrayList<>();
-
-        IntStream.range(0,number).forEach( i-> {
-                    Collections.shuffle(result);
-                    LottoObject lootObject = new LottoObject();
-                    result.subList(0, LOTTO_NUMBER).forEach(lootObject::add);
-                    lottoObjects.add(lootObject);
-                }
-        );
-        arrayList(lottoObjects);
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        String test = "1, 2, 3, 4, 5, 6";
-        String test2 =test.replace(" ","");
-        System.out.println(test2);
-        String[] test3 = test2.split(",");
-        List<Integer> test4= Arrays.stream(test3).map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println("보너스 볼을 입력해 주세요..");
-        int bonusBall = 7;
-        System.out.println(bonusBall);
-        System.out.println("당첨 통계");
-        Map<LottoType ,Long> ga = lottoObjects.stream().map(object -> object.contains(test4,bonusBall))
-                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
-                .entrySet()
-                .stream().collect( Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-      // System.out.println(ga.toString());
-
-        ga.forEach((key, value) -> System.out.println(key.getPrintString() + "- " + value + "개"));
-    }
-
-    public static   void arrayList(List<LottoObject> list){
+    public void arrayList(List<LottoObject> list){
         for (LottoObject object : list){
             System.out.println(object.toString());
         }
--- src/test/java/LottoTest.java
+++ src/test/java/LottoTest.java
@@ -1,9 +1,11 @@
 import lotto.Lotto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,11 +19,29 @@ public class LottoTest {
     }
 
 
+    @DisplayName("기본 실행테스트 ")
     @ParameterizedTest
     @MethodSource("lottoValues")
-    void emptyOrNull(int number, String string, int bonusNumber) {
+    void test1(int number, String string, int bonusNumber) {
         lotto.lotto(number, string ,bonusNumber );
     }
+
+    @DisplayName("기본 실행테스트2 ")
+    @ParameterizedTest
+    @MethodSource("lottoValues")
+    void test2(int number, String string, int bonusNumber) {
+        assertThat(lotto.lotto(number, string ,bonusNumber )).isGreaterThan(BigDecimal.ONE)
+                .withFailMessage("1보다 작습니다.");
+    }
+
+    @DisplayName("기본 실행테스트3 ")
+    @ParameterizedTest
+    @MethodSource("lottoValues")
+    void test3(int number, String string, int bonusNumber) {
+        assertThat(lotto.lotto(number, string ,bonusNumber )).isLessThan(BigDecimal.ONE)
+                .withFailMessage("1보다 큽니다.");
+    }
+
 
     static Stream<Arguments> lottoValues() {
         return Stream.of(
