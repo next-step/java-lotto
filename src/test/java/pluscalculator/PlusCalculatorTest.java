f3039dfd90b453d1975705a76029f0068ce16dfb
aaaicu <aaaaicu@gmail.com>
2023-03-10 00:49:55 +0900

    test: 쉼표 테스트 케이스 추가 및 green 단계 처리

--- src/test/java/pluscalculator/PlusCalculatorTest.java
+++ src/test/java/pluscalculator/PlusCalculatorTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlusCalculatorTest {
     static class PlusCalculator {
         public static Numbers extractNumbers(final String plusExpression) {
-            return new Numbers(new ArrayList<>());
+            String[] splitNumbers = plusExpression.split(",");
+            return new Numbers(Arrays.stream(splitNumbers).map(Integer::parseInt).collect(Collectors.toList()));
         }
-
     }
 
     static class Numbers {
@@ -24,12 +25,13 @@ public class PlusCalculatorTest {
         }
 
         public int plusAll() {
-            return 100;
+            return numbers.stream().reduce(0, Integer::sum);
         }
     }
 
     @ParameterizedTest
-    @CsvSource("'11,55,44', 100")
+    @CsvSource({"'11,55,44', 110",
+            "'1,2,3', 6"})
     @DisplayName("쉼표를 포함한 문자열을 분리해서 계산한다.")
     void commaSplit(String plusExpression, int expectValue) {
 
