d03150774db309ef697f432f667cc3403968e972
DESKTOP-009CMUM\kakaopaysec <sycha72@gmail.com>
2023-03-11 08:03:18 +0900

    로또 테스트 클래스 작업

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Lotto {
+    private static final int LOTTO_LIMIT = 45;
+    private static final int LOTTO_TICKET_LENGTH = 6;
+    private static final int LOTTO_TICKET_MIN = 0;
+    private final int[] lottoNumbers;
+
+    public Lotto() {
+        lottoNumbers = IntStream.rangeClosed(1, LOTTO_LIMIT).toArray();
+    }
+
+    public int[] getLottoNumber() {
+        return lottoNumbers;
+    }
+
+    public List<Integer> getLottoTicket2() {
+        List<Integer> shuffleNumber =  Arrays.stream(lottoNumbers).boxed().collect(Collectors.toList());
+        Collections.shuffle(Arrays.asList(shuffleNumber));
+        return shuffleNumber.subList(LOTTO_TICKET_MIN, LOTTO_TICKET_LENGTH);
+    }
+}
