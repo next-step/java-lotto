80637908ec2347c29a6f350770527a53c380e0da
DESKTOP-009CMUM\kakaopaysec <sycha72@gmail.com>
2023-03-13 09:22:33 +0900

    로또 테스트 클래스 작업

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,16 +5,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-
+    static final int WIN_PRICE_LEVEL5 = 5000;
+    static final int WIN_PRICE_LEVEL4 = 50000;
+    static final int WIN_PRICE_LEVEL3 = 1500000;
+    static final int WIN_PRICE_LEVEL2 = 30000000;
+    static final int WIN_PRICE_LEVEL1 = 2000000000;
     static LottoGame lottoGame;
     public static void printLottoTicket() {
-        for (int ii = 0; ii < lottoGame.getTickets().size(); ii++ )
-            System.out.println(lottoGame.getTickets().get(ii).getLottoNumber().toString());
+        for (LottoTicket newLotto: lottoGame.getTickets())
+            System.out.println(newLotto.getLottoNumber().toString());
     }
 
-    public static void printWinnumber() {
-        for (int ii = 0; ii < lottoGame.getTickets().size(); ii++ )
-            System.out.println(lottoGame.getTickets().get(ii).getLottoNumber().toString() + lottoGame.getTickets().get(ii).getWinLevel());
+    public static void printWinNumber(int money) {
+        int[] winPrice = {WIN_PRICE_LEVEL1, WIN_PRICE_LEVEL2, WIN_PRICE_LEVEL3, WIN_PRICE_LEVEL4, WIN_PRICE_LEVEL5};
+        int[] winLotto = new int[LottoGame.LOTTO_LEVEL_5];
+        int   winAmount = 0;
+
+        for (LottoTicket newLotto: lottoGame.getTickets()) {
+            if (newLotto.getWinLevel() == 0)
+                continue;
+            winLotto[newLotto.getWinLevel() - 1]++;
+            winAmount += winPrice[newLotto.getWinLevel() - 1];
+        }
+
+        System.out.println("당첨통계");
+        System.out.println("-------");
+        System.out.println("3개 일치 (5000원)-"+winLotto[LottoGame.LOTTO_LEVEL_5-1]+"개");
+        System.out.println("4개 일치 (50000원)-"+winLotto[LottoGame.LOTTO_LEVEL_4-1]+"개");
+        System.out.println("5개 일치 (1500000원)-"+winLotto[LottoGame.LOTTO_LEVEL_3-1]+"개");
+        System.out.println("5개 일치, 보너스 볼 일치(30000000원) -"+winLotto[LottoGame.LOTTO_LEVEL_2-1]+"개");
+        System.out.println("6개 일치 (2000000000원)-"+winLotto[LottoGame.LOTTO_LEVEL_1-1]+"개");
+        System.out.println("총 수익률은"+winAmount/money+"입니다.");
     }
     public static  void main(String[] args) {
 
@@ -32,6 +53,6 @@ public class Application {
         String    bonusNumber = scan.nextLine();
 
         lottoGame.checkWin(winNumbers, bonusNumber);
-        printWinnumber();
+        printWinNumber(lottoMoney);
     }
 }
