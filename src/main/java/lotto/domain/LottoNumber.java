2acb81a41b2cdd3d0b8000583dceb3fe9fd4d456
JEAN-20210074\테스트 <jean.calm@kakaopaysec.com>
2023-03-13 11:34:19 +0900

    refactor: 로또(수동) step3 LottoNumber 객체 매직넘버 상수값 추가
    * LottoNumber 클래스에 숫자 검증 로직 내부의 최소, 최대 비교값을 Magic 넘버 상수값으로 분리

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -4,6 +4,9 @@ import java.util.Objects;
 
 public class LottoNumber {
 
+    private static final int LOTTO_MIN_LIMIT_VALUE = 1;
+    private static final int LOTTO_MAX_LIMIT_VALUE = 45;
+
     private final Integer number;
 
     public LottoNumber(Integer number) {
@@ -12,7 +15,7 @@ public class LottoNumber {
     }
 
     private void validateLottoNumber(Integer number) {
-        if (number <= 0 || number > 45) {
+        if (number < LOTTO_MIN_LIMIT_VALUE || number > LOTTO_MAX_LIMIT_VALUE) {
             throw new IllegalArgumentException("로또 번호는 1 ~ 45까지 숫자만 선택 가능합니다.");
         }
     }
--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -6,6 +6,7 @@ import lotto.ui.LottoHitInfo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
