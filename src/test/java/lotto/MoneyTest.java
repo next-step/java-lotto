d823925e09ebade0376651f85e34a172b865cf0e
aaaicu <aaaaicu@gmail.com>
2023-03-18 12:31:51 +0900

    test: Money클래스 테스트코드 추가

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -12,4 +12,22 @@ public class MoneyTest {
     void moneyVoTest() {
         Assertions.assertThat(new Money(10)).isEqualTo(new Money(10));
     }
+
+    @Test
+    @DisplayName("곱하기")
+    void multiplyTest() {
+        Assertions.assertThat(new Money(1).multiply(10)).isEqualTo(new Money(10));
+    }
+
+    @Test
+    @DisplayName("나누기")
+    void divisionTest() {
+        Assertions.assertThat(new Money(100).division(new Money(10))).isEqualTo(new Money(10));
+    }
+
+    @Test
+    @DisplayName("Money Integer로 변환")
+    void toIntegerTest() {
+        Assertions.assertThat(new Money(100).toInteger()).isEqualTo(100);
+    }
 }
