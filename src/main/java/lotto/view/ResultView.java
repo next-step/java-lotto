844bd17c52491120849052ec71caf74227dbb71d
jade.cyan <cyanjd1226@gmail.com>
2023-03-19 19:49:33 +0900

    결과 출력시 출력순서 정렬

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -2,15 +2,19 @@ package lotto.view;
 
 import lotto.domain.enums.LottoRank;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ResultView {
 
     public void printResult(Map<LottoRank, Integer> result, double totalRate){
 
-        result.forEach((key, value)->{
-            System.out.println( String.format(" %s - %s개", key, value) );
-        });
+        List<LottoRank> keyList = new ArrayList<>(result.keySet());
+        keyList.sort((s1, s2) -> s1.compareTo(s2));
+        for (LottoRank key : keyList) {
+            System.out.println( String.format("%s - %s개", key, result.get(key)) );
+        }
 
         System.out.println ("총 수익률은 " + totalRate + "입니다."); //총 수익률은 x.xx입니다.
 
