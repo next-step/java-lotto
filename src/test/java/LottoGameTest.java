99161b00ed68c91af2ab7cc2a4cbb9640e0240a3
KAITLIN-2019005\kakaopaysec <kaitlin.kpsec@gmail.com>
2023-03-21 22:51:13 +0900

    feat : 테스트 케이스 추가
    로또 번호 6개 반환하는 테스트 추가
    로또번호 1~45 범위로 생성여부 테스트 추가
    중복번호 생성되지 않는지 테스트 추가
    입력 금액에 따라 1장구입금액 미달시 체크 테스트 추가

--- src/test/java/LottoGameTest.java
+++ src/test/java/LottoGameTest.java
@@ -1,6 +1,8 @@
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 import java.io.ByteArrayInputStream;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import lottoGame.InputView;
@@ -17,51 +19,88 @@ public class LottoGameTest {
 
     @DisplayName("구입금액 1000원당 6자리의 로또를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"2000"})
-    void buyLotto(String amt) {
+    @ValueSource(ints = {2000})
+    void buyLotto(int amt) {
         LottoGame lottoGame = new LottoGame();
         lottoGame.buyLotto(amt);
-        assertThat(lottoGame.getLottoListCount()).isEqualTo(Integer.parseInt(amt)/LOTTO_ONE_GAME_AMT);
+        assertThat(lottoGame.getLottoListCount()).isEqualTo(amt / LOTTO_ONE_GAME_AMT);
     }
+
     @DisplayName("일등 당첨 확인")
     @Test
     void getFirstGrade() {
         LottoGame lottoGame = new LottoGame();
-        final var actual = lottoGame.matchingLotto(List.of(1,2,3,4,5,6), Set.of(1,2,3,4,5,6), 7);
+        final var actual = lottoGame.matchingLotto(List.of(1, 2, 3, 4, 5, 6),
+            Set.of(1, 2, 3, 4, 5, 6), 7);
         assertThat(actual).isEqualTo(1);
     }
+
     @DisplayName("이등 당첨 확인")
     @Test
     void getSecondGrade() {
         LottoGame lottoGame = new LottoGame();
-        final var actual = lottoGame.matchingLotto(List.of(1,2,3,4,5,7), Set.of(1,2,3,4,5,6), 7);
+        final var actual = lottoGame.matchingLotto(List.of(1, 2, 3, 4, 5, 7),
+            Set.of(1, 2, 3, 4, 5, 6), 7);
         assertThat(actual).isEqualTo(2);
     }
+
     @DisplayName("삼등 당첨 확인")
     @Test
     void getThirdGrade() {
         LottoGame lottoGame = new LottoGame();
-        final var actual = lottoGame.matchingLotto(List.of(1,2,3,4,5,8), Set.of(1,2,3,4,5,6), 7);
+        final var actual = lottoGame.matchingLotto(List.of(1, 2, 3, 4, 5, 8),
+            Set.of(1, 2, 3, 4, 5, 6), 7);
         assertThat(actual).isEqualTo(3);
     }
+
     @DisplayName("사등 당첨 확인")
     @Test
     void getFourthGrade() {
         LottoGame lottoGame = new LottoGame();
-        final var actual = lottoGame.matchingLotto(List.of(1,2,3,4,8,9), Set.of(1,2,3,4,5,6), 7);
+        final var actual = lottoGame.matchingLotto(List.of(1, 2, 3, 4, 8, 9),
+            Set.of(1, 2, 3, 4, 5, 6), 7);
         assertThat(actual).isEqualTo(4);
     }
+
     @DisplayName("오등 당첨 확인")
     @Test
     void getFifthGrade() {
         LottoGame lottoGame = new LottoGame();
-        final var actual = lottoGame.matchingLotto(List.of(1,2,3,8,9,10), Set.of(1,2,3,4,5,6), 7);
+        final var actual = lottoGame.matchingLotto(List.of(1, 2, 3, 8, 9, 10),
+            Set.of(1, 2, 3, 4, 5, 6), 7);
         assertThat(actual).isEqualTo(5);
     }
+
     @DisplayName("로또 번호 6개를 반환한다.")
     @Test
     void getLottoNumber() {
-        Lotto lotto = new Lotto();
-        System.out.println(lotto.getLottoNumber());
+        for (int i = 0; i < 100; i++) {
+            Lotto lotto = new Lotto();
+
+            List<Integer> lottoNumbers = lotto.getLottoNumber();
+            assertThat(lottoNumbers.size()).isEqualTo(6);
+
+            Set<Integer> checkNumber = new HashSet<>(lottoNumbers);
+            assertThat(checkNumber.size()).isEqualTo(6);
+
+            for (int number : lottoNumbers) {
+                assertThat(number).isBetween(1, 45);
+            }
+
+            System.out.println(lottoNumbers);
+        }
+    }
+
+    @DisplayName("입력값 테스트.")
+    @Test
+    void checkInputData() {
+        LottoGame lottoGame = new LottoGame();
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> lottoGame.buyLotto(500));
+
+        assertThat(lottoGame.buyLotto(1000)).isEqualTo(1);
+
+        assertThat(lottoGame.buyLotto(1500)).isEqualTo(1);
     }
 }
