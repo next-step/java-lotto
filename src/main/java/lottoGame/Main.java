0570e7a1b7542b1f5d095205259356550764af4d
KAITLIN-2019005\kakaopaysec <kaitlin.kpsec@gmail.com>
2023-03-26 16:42:44 +0900

    feat : 메인함수 수동로또 포함하여 로또 구매 추가

--- src/main/java/lottoGame/Main.java
+++ src/main/java/lottoGame/Main.java
@@ -1,5 +1,7 @@
 package lottoGame;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
@@ -9,23 +11,32 @@ public class Main {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
+        // 구입금액 입력
         int inputMoney = inputView.getInputMoney();
 
-        lottoGame.buyLotto(inputMoney);
+        // 수동으로 구매할 로또 수 입력
+        int inputManualLottoCount = inputView.getInputManualLottoCount(inputMoney);
 
+        // 수동으로 구매할 번호 입력
+        List<Lotto> manualLottos = inputView.getInputManualLottoNumber(inputManualLottoCount);
 
-        resultView.printTicket(lottoGame.getLottoTicket());
+        // 로또 구매
+        lottoGame.buyLotto(inputMoney, manualLottos);
 
+        // 구매 결과 프린트
+        resultView.printTicket(lottoGame);
 
-
+        // 지난 주 당첨 번호 입력
         Set<Integer> winningNumbers = inputView.getWinningNumbers();
 
         lottoGame.inputWinningNumbers(winningNumbers);
 
+        // 보너스 볼 입력
         int bonusNumber = inputView.getBonusNumber();
 
         lottoGame.inputBonusNumber(bonusNumber);
 
+        //
         lottoGame.run();
 
         resultView.showResult(lottoGame);
