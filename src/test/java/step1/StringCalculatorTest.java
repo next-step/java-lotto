e0db764e902a3ab6f31a3c8722262a0ffca1e027
dong <dong.22@kakaoenterprise.com>
2023-04-14 16:32:40 +0900

    test : 실패하는 테스트 추가

--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -1,8 +1,72 @@
 package step1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
 
+    @DisplayName("(+) 계산한다")
+    @Test
+    public void plus() {
+        //given
+        //when
+        //then
+        fail();
+    }
 
+    @DisplayName("(-) 계산한다")
+    @Test
+    public void minus() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("(*) 계산한다")
+    @Test
+    public void multiply() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("(/) 계산한다")
+    @Test
+    public void divide() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("입력 값이 null 이거나 공백 문자일 경우 IllegalArgumentException throw 한다")
+    @Test
+    public void emptyInput() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException throw")
+    @Test
+    public void invalidOperator() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("문자열을 \"2 + 3 * 4 / 2\" 입력시 10을 반환한다")
+    @Test
+    public void calculate() {
+        //given
+        //when
+        //then
+        fail();
+    }
 }
