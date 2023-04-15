c8838f31bd0ceef06f36985a5143e37ac95a7651
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-04-16 00:27:36 +0900

    feature - 문자열 입력

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import calculator.view.InputView;
+
+public class Main {
+    public static void main(String[] args) {
+        String expression = InputView.inputExpression();
+    }
+}
---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,17 @@
+package calculator.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputExpression() {
+        System.out.println("계산할 문자열을 입력해주세요.");
+        System.out.println("입력 값은 공백 기준으로 분리되며 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정됩니다.");
+        System.out.println("예시) 2 + 3 * 4 / 2 = 10");
+
+        return scanner.nextLine();
+    }
+
+}
