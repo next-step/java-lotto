05918e21f25710f5fd70e562cf4292090e3e9ac9
JhyunJung <jhyun.rigel@gmail.com>
2023-04-15 13:25:59 +0900

    뺃셈 테스트 및 구현 완료

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public static int add(int a, int b) {
         return a+b;
     }
+
+    public static int subtraction(int a, int b) {
+        return a-b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CalculatorTest {
 
     @Test
@@ -15,6 +17,19 @@ public class CalculatorTest {
         int result = Calculator.add(a,b);
 
         //then
-        Assertions.assertThat(a+b).isEqualTo(14);
+        assertThat(result).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("뺃셈_테스트")
+    public void 뺃셈_테스트(){
+        //given
+        int a = 4;
+        int b = 10;
+        //when
+        int result = Calculator.subtraction(a,b);
+
+        //then
+        assertThat(result).isEqualTo(-6);
     }
 }
