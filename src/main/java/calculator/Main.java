f5ed941d66f3247da68cb9a3723b32cb717915f6
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-04-17 01:42:19 +0900

    feature - 결과값 출력 기능

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -3,12 +3,14 @@ package calculator;
 import calculator.domain.Expression;
 import calculator.domain.StringCalculator;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Main {
     public static void main(String[] args) {
         Expression expression = Expression.createExpression(InputView.inputExpression());
+
         StringCalculator calculator = new StringCalculator(expression);
 
-        calculator.calculate();
+        OutputView.printResult(calculator.calculate());
     }
 }
--- src/test/java/calculator/domain/ExpressionTest.java
+++ src/test/java/calculator/domain/ExpressionTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
