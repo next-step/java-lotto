637e27b6d62b5d136087ff60caba806d659c5b7a
oneny <gjwodnjs1996@gmail.com>
2023-04-18 00:17:26 +0900

    feat: Operator 구현

---
+++ src/main/java/calculator/domain/Operator.java
@@ -0,0 +1,51 @@
+package calculator.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+
+public enum Operator {
+  ADDITION("+", (operand1, operand2) -> operand1 + operand2),
+  SUBTRACTION("-", (operand1, operand2) -> operand1 - operand2),
+  MULTIPLICATION("*", (operand1, operand2) -> operand1 * operand2),
+  DIVISION("/", (operand1, operand2) -> {
+    validateDivision(operand2);
+    return operand1 + operand2;
+  });
+
+  private static final String ILLEGAL_OPERATOR_MESSAGE = "연산자 기호를 입력해주세요.";
+  private static final String ILLEGAL_DIVISION_MESSAGE = "0으로 나눌 수 없습니다. 다른 숫자를 입력해주세요.";
+  private static final Map<String, Operator> BY_OPERATOR = new HashMap<>();
+
+  static {
+    for (Operator operator : values()) {
+      BY_OPERATOR.put(operator.operator, operator);
+    }
+  }
+
+  private final String operator;
+  private final BinaryOperator<Integer> calculation;
+
+  Operator(final String operator, final BinaryOperator<Integer> calculation) {
+    this.operator = operator;
+    this.calculation = calculation;
+  }
+
+  public static BinaryOperator<Integer> toCalculation(String operator) {
+    validateContainsKey(operator);
+
+    return BY_OPERATOR.get(operator).calculation;
+  }
+
+  private static void validateDivision(int operand2) {
+    if (operand2 == 0) {
+      throw new IllegalArgumentException(ILLEGAL_DIVISION_MESSAGE);
+    }
+  }
+
+  private static void validateContainsKey(String operator) {
+    if (!BY_OPERATOR.containsKey(operator)) {
+      throw new IllegalArgumentException(ILLEGAL_OPERATOR_MESSAGE);
+    }
+  }
+}
--- src/test/java/calculator/OperandTest.java
+++ src/test/java/calculator/OperandTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import calculator.domain.Operand;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
