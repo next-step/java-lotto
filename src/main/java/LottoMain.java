629da2fd40b0818d04799126f0ace4c4c3e1686c
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-04-17 20:25:15 +0900

    refactor - 과거 코드 삭제

--- src/main/java/LottoMain.java
+++
@@ -1,8 +0,0 @@
-import controller.LottoController;
-
-public class LottoMain {
-    public static void main(String[] args) {
-        LottoController controller = new LottoController();
-        controller.lottoStart();
-    }
-}
