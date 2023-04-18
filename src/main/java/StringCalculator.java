344422e3be59e4655c46bde47dc30eabe9b11f93
KimJuHyun <rlawngus0910@naver.com>
2023-04-19 00:30:55 +0900

    feat: 문자열을 공백 기준으로 split

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,10 +6,16 @@ public class StringCalculator {
             throw new IllegalArgumentException("문자열을 입력해주세요.");
         }
 
+        String[] token = splitedToken(text);
+
         return 0;
     }
 
     private static boolean isNullOrEmpty(String text) {
         return text == null || text.isEmpty();
+    }
+
+    private static String[] splitedToken(String text) {
+        return text.split(" ");
     }
 }
