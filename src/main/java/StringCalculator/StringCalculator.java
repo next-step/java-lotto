615520127bab177444f5a6e03e28a212344e0431
sangji <jsj9444@gmail.com>
2023-04-18 22:55:33 +0900

    리팩토링

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -3,45 +3,44 @@ package StringCalculator;
 import java.util.List;
 
 public class StringCalculator {
-    private final static String PLUS = "+";
-    private final static String MINUS = "-";
-    private final static String MULTIPLY = "*";
-    private final static String DIVIDE = "/";
+    private final static String SPLIT_PATTERN = " ";
+    private final static String NUMBER_PATTERN = "^[0-9]+$";
+    private final static int INDEX_FIRST = 0;
+    private final static int INDEX_SECOND = 1;
 
     public static int calculate(String input) {
-        int sum = 0;
-        List<String> inputs = List.of(input.split(" "));
-        for(int i=0; i < inputs.size(); i++){
-            System.out.println(inputs.get(i));
-            if(inputs.get(i).matches("^[0-9]+$")){
-                if(i==0){
-                    sum = Integer.valueOf(inputs.get(0));
-                }else {
-                    sum = operating(sum, inputs.get(i - 1), Integer.valueOf(inputs.get(i)));
-                }
-            }
+        if (IsNull(input)) {
+            throw new IllegalArgumentException();
+        }
+
+        return splitAndOperating(input);
+    }
+
+    private static boolean IsNull(String input) {
+        if (input == null || "".equals(input)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int splitAndOperating(String input) {
+        List<String> inputs = List.of(input.split(SPLIT_PATTERN));
+
+        int sum = Integer.valueOf(inputs.get(INDEX_FIRST));
+        for (int index = INDEX_SECOND; index < inputs.size(); index++) {
+            sum = getSum(sum, inputs, index);
         }
 
         return sum;
     }
 
-    private static int operating(int sum, String operation, int value){
-        if(PLUS.equals(operation)){
-            return sum + value;
+    private static int getSum(int sum, List<String> inputs, int index) {
+        if (inputs.get(index).matches(NUMBER_PATTERN)) {
+            return Operation.operating(sum, inputs.get(index - 1), Integer.valueOf(inputs.get(index)));
         }
 
-        if(MINUS.equals(operation)){
-            return sum - value;
-        }
-
-        if(MULTIPLY.equals(operation)){
-            return sum * value;
-        }
-
-        if(DIVIDE.equals(operation)){
-            return sum / value;
-        }
-
-        throw new IllegalArgumentException();
+        return sum;
     }
+
 }
--- src/test/java/StringCalculator/StringCalculatorTest.java
+++ src/test/java/StringCalculator/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package StringCalculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static StringCalculator.StringCalculator.calculate;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,40 +10,28 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class StringCalculatorTest {
-    @Test
-    void 덧셈() {
-        assertThat(calculate("2 + 3")).isEqualTo(5);
-        //assertThat(plus(2,"+",3)).isEqualTo(5);
-    }
 
-    @Test
-    void 뺄셈() {
-        assertThat(calculate("2 - 3")).isEqualTo(-1);
-    }
-
-    @Test
-    void 곱셈() {
-        assertThat(calculate("2 * 3")).isEqualTo(6);
-    }
-
-    @Test
-    void 나눗셈() {
-        assertThat(calculate("2 / 3")).isEqualTo(0);
+    @ParameterizedTest
+    @CsvSource(value = {"2 + 3,5", "2 - 3,-1", "2 * 3,6", "2 / 3,0", "2 / 3 + 1,1"})
+    void 사칙연산(String input, int expected) {
+        assertThat(calculate(input)).isEqualTo(expected);
     }
 
     @Test
     void 입력값이null이거나_빈공백문자일경우_IllegalArgumentException() {
-        assertAll(()-> {
-            assertThatIllegalArgumentException().isThrownBy(() ->calculate("")),
-        });
+        assertAll(
+                () -> {
+                    assertThatIllegalArgumentException().isThrownBy(() -> calculate(""));
+                },
+                () -> {
+                    assertThatIllegalArgumentException().isThrownBy(() -> calculate(null));
+                }
+        );
     }
 
     @Test
     void 사칙연산기호가아닌경우_IllegalArgumentException() {
-    }
-
-    @Test
-    void 사칙연산을_모두포함() {
+        assertThatIllegalArgumentException().isThrownBy(() -> calculate("2 & 3"));
     }
 
 }
