81b1a41ed3218bc9320094063f4cf73d072308c0
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 00:01:19 +0900

    feat: offset 증가 메소드 실패하는 테스트코드 작성

--- src/test/java/domain/OperatorGroupOffsetTest.java
+++ src/test/java/domain/OperatorGroupOffsetTest.java
@@ -1,19 +1,24 @@
 package domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class OperatorGroupOffsetTest {
 
     @Test
     @DisplayName("연산자 그룹 offset은 초기 값으로 0을 반환한다")
-    void initialize(){
+    void initialize() {
 
         assertThat(OperatorGroupOffset.initialize()).isEqualTo(new OperatorGroupOffset(0));
+    }
+
+    @Test
+    @DisplayName("연산자 그룹 offset은 offset증가하는 메소드 호출시 offset이 증가한")
+    void increase(){
+
+        assertThat(OperatorGroupOffset.initialize().increase()).isEqualTo(new OperatorGroupOffset(0));
     }
 
 }
