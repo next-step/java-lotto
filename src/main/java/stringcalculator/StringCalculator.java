4db71c9155bddfa5682dffce24cc285f0e480c36
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-20 00:22:52 +0900

    feature(calculator): Add quotient test, Implement quotient method in StringCalculator class

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -5,7 +5,14 @@ public class StringCalculator {
         return num1 + num2;
     }
 
-    public static int substring(int num1, int num2) {
+    public static int difference(int num1, int num2) {
         return num1 - num2;
+    }
+
+    public static int quotient(int num1, int num2) {
+        if (num1 % num2 != 0) {
+            throw new IllegalArgumentException("나눗셈의 결과가 정수로 떨어지지 않습니다.");
+        }
+        return num1 / num2;
     }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -13,6 +13,18 @@ public class StringCalculatorTest {
 
     @Test
     void 뺄셈() {
-        assertThat(StringCalculator.substring(1, 2)).isEqualTo(-1);
+        assertThat(StringCalculator.difference(1, 2)).isEqualTo(-1);
+    }
+
+    @Test
+    void 나눗셈() {
+        assertThat(StringCalculator.quotient(4, 2)).isEqualTo(2);
+    }
+
+    @Test
+    void 나눗셈의_결과가_정수로_떨어지지_않으면_IllegalArgumentException이_발생한다() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> StringCalculator.quotient(3, 2))
+            .withMessage("나눗셈의 결과가 정수로 떨어지지 않습니다.");
     }
 }
