f53d39f2651b75afd92ad8e89c62e5f91bf89700
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-19 23:22:27 +0900

    feat: operator group offset initialize 성공하는 메소드 작성

--- src/main/java/domain/OperatorGroupOffset.java
+++ src/main/java/domain/OperatorGroupOffset.java
@@ -1,15 +1,30 @@
 package domain;
 
+import java.util.Objects;
+
 public class OperatorGroupOffset {
     private static final int INITIALIZE_NUMBER = 0;
     private final int offset;
 
-    private OperatorGroupOffset(int offset) {
+    public OperatorGroupOffset(int offset) {
         this.offset = offset;
     }
 
     public static OperatorGroupOffset initialize() {
+        return new OperatorGroupOffset(INITIALIZE_NUMBER);
+    }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OperatorGroupOffset that = (OperatorGroupOffset) o;
+        return offset == that.offset;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(offset);
     }
 }
