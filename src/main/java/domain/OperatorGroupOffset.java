7a77bf6eae48dfc2daccb9b4c4867a6a44df4a5a
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-19 23:17:25 +0900

    feat: operator group offset 객체 및 initialize 메소드 생성

---
+++ src/main/java/domain/OperatorGroupOffset.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class OperatorGroupOffset {
+    private static final int INITIALIZE_NUMBER = 0;
+    private final int offset;
+
+    private OperatorGroupOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public static OperatorGroupOffset initialize() {
+
+
+    }
+}
