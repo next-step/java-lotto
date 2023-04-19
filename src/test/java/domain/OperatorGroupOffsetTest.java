a6f5c803883f087bf85bee63e2cea67d9b44b154
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-19 23:21:35 +0900

    feat: operator group offset initialize 실패하는 메소드 작성

--- src/test/java/domain/OperatorGroupOffsetTest.java
+++ src/test/java/domain/OperatorGroupOffsetTest.java
@@ -1,8 +1,10 @@
 package domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperatorGroupOffsetTest {
@@ -10,10 +12,8 @@ class OperatorGroupOffsetTest {
     @Test
     @DisplayName("연산자 그룹 offset은 초기 값으로 0을 반환한다")
     void initialize(){
-        
-        OperatorGroupOffset operatorGroupOffset = OperatorGroupOffset.initialize();
 
-
+        assertThat(OperatorGroupOffset.initialize()).isEqualTo(new OperatorGroupOffset(0));
 
     }
 
