b92885f7429c8d5ac53acaaac622cc422fd1c890
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-19 23:50:16 +0900

    feature(calculator): Add substring test, Implement substring method in StringCalculator class

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,4 +4,8 @@ public class StringCalculator {
     public static int sum(int num1, int num2) {
         return num1 + num2;
     }
+
+    public static int substring(int num1, int num2) {
+        return num1 - num2;
+    }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -2,11 +2,17 @@ package stringcalculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
     @Test
     void 덧셈() {
         assertThat(StringCalculator.sum(1, 2)).isEqualTo(3);
+    }
+
+    @Test
+    void 뺄셈() {
+        assertThat(StringCalculator.substring(1, 2)).isEqualTo(-1);
     }
 }
