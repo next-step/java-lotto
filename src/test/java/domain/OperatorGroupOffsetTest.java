ba8d87447ec01832a183dd3a30f72b4cc37a8486
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-19 23:16:34 +0900

    feat: group offset 테스트 코드 작성

---
+++ src/test/java/domain/OperatorGroupOffsetTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperatorGroupOffsetTest {
+
+    @Test
+    @DisplayName("연산자 그룹 offset은 초기 값으로 0을 반환한다")
+    void initialize(){
+        
+        OperatorGroupOffset operatorGroupOffset = OperatorGroupOffset.initialize();
+
+
+
+    }
+
+}
