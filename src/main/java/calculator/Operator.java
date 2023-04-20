a3c9d906002b1ca91195f6d53e105166aafb0818
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 02:18:41 +0900

    feat: Operator 생성

---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Operator {
+
+    private final String operator;
+
+    public Operator(String operator) {
+        this.operator = operator;
+    }
+
+    public String value() {
+        return operator;
+    }
+}
