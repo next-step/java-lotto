af24c9c5743c4afe77f555cbb565e328f56f46cf
In-wook Hwang <inwook9@gmail.com>
2023-04-20 23:09:50 +0900

    feat: 곱셈기능 추가

--- src/test/java/calculator/OperationTest.java
+++ src/test/java/calculator/OperationTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static calculator.Operator.ADD;
-import static calculator.Operator.SUB;
+import static calculator.Operator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OperationTest {
@@ -34,6 +33,20 @@ class OperationTest {
     @ParameterizedTest(name = "{0} + {1} = {2}")
     void test02(int a, int b, int expected) {
         Operation operation = Operation.findType(SUB);
+        int actual = operation.calculate(a, b);
+        assertThat(actual).isSameAs(expected);
+    }
+
+    @CsvSource(value = {
+            "3,2,6",
+            "5,3,15",
+            "0,2,0",
+            "-1,5,-5",
+    })
+    @DisplayName("입력값을 기준으로 곱셈 연산 후 결과 값을 반환한다.")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    void test03(int a, int b, int expected) {
+        Operation operation = Operation.findType(MUL);
         int actual = operation.calculate(a, b);
         assertThat(actual).isSameAs(expected);
     }
