0aacc3ad6dfa300606b5a12736ba60765cca127a
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 23:05:07 +0900

    feat: 연산자 추출 실패하는 클래스 작성

--- src/test/java/domain/OperatorExtractorTest.java
+++ src/test/java/domain/OperatorExtractorTest.java
@@ -4,9 +4,14 @@ import domain.extractor.Extractor;
 import domain.extractor.OperatorExtractor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OperatorExtractorTest {
 
@@ -17,12 +22,20 @@ class OperatorExtractorTest {
         extractor = new OperatorExtractor();
     }
 
-    @Test
+    @ParameterizedTest(name = "input : {0} , operatorList {1}")
+    @MethodSource("getSuccessPairValue")
     @DisplayName("연산자를 추출하는 테스트")
-    void extract() {
+    void extract(List<String> input,List<Operator> operatorList) {
 
-        List<String> input = List.of("2", "+", "3", "*", "4", "2");
+         assertThat(extractor.extract(input)).isEqualTo(operatorList);
+    }
 
-        extractor.extract(input);
+    static Stream<Arguments> getSuccessPairValue() {
+
+        return Stream.of(
+                Arguments.of(List.of("2", "+", "3", "*", "4", "/", "2"), List.of(Operator.ADDITION, Operator.MULTIPLICATION, Operator.DIVISION)),
+                Arguments.of(List.of("2", "/", "3", "*", "4", "*", "2"), List.of(Operator.DIVISION, Operator.MULTIPLICATION, Operator.MULTIPLICATION)),
+                Arguments.of(List.of("2", "*", "3", "*", "4", "*", "2"), List.of(Operator.MULTIPLICATION, Operator.MULTIPLICATION, Operator.MULTIPLICATION))
+        );
     }
 }
