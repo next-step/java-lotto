9b1b379b30e2c338114508db90a51a1e78d6726e
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 20:30:05 +0900

    refactor: Operator lookup 구분자, 연산자 캐싱 후 가져와 사용

--- src/main/java/domain/Operand.java
+++ src/main/java/domain/Operand.java
@@ -1,6 +1,12 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum Operand {
 
@@ -11,18 +17,20 @@ public enum Operand {
 
     private final String separator;
 
+    private static final Map<String, Operand> separators;
+
+    static {
+        separators = Collections.unmodifiableMap(Stream.of(values())
+                .collect(Collectors.toMap(Operand::getSeparator, Function.identity())));
+    }
+
     Operand(String separator) {
         this.separator = separator;
     }
 
     public static Operand lookUp(String separator) {
-
-        return Arrays.stream(Operand.values())
-                .filter(operand -> operand.separator.equals(separator))
-                .findFirst()
-                .get();
+        return Optional.ofNullable(separators.get(separator)).get();
     }
-
 
     public String getSeparator() {
         return separator;
