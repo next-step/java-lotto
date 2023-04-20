cad700ab74e718cf3614b1408277d58ac40d5da0
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 02:34:22 +0900

    test: 숫자가 아닌 경우 예외처리

--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NumberTest {
 
@@ -10,5 +11,13 @@ public class NumberTest {
     void Number_생성() {
         int num = new Number("1").intValue();
         assertThat(num).isEqualTo(1);
+    }
+
+    @Test
+    void 숫자가_아닌경우_예외처리() {
+        assertThatThrownBy(() -> {
+            new Number("/");
+        }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("숫자가 아닙니다.");
     }
 }
