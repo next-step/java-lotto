e8fe81f28989bf969d00d8a28107c0729f3d259a
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 02:56:41 +0900

    test: split 학습테스트

---
+++ src/test/java/calculator/SplitStudyTest.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SplitStudyTest {
+
+    @Test
+    void split_학습테스트() {
+        //given
+        String s = "1  + 2 * 3 / 4";
+
+        //when
+        String[] arr = s.split(" ");
+
+        //then
+        assertThat(arr[0]).isEqualTo("1");
+        assertThat(arr[1]).isEqualTo("");
+        assertThat(arr[arr.length - 1]).isEqualTo("4");
+    }
+}
