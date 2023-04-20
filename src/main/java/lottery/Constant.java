a9e3ddb7342f4610a5c9116a92b37c865a168ce6
right1203 <right1203@gmail.com>
2023-04-20 21:50:03 +0900

    refactor: 상수만 모아둔 클래스

---
+++ src/main/java/lottery/Constant.java
@@ -0,0 +1,11 @@
+package lottery;
+
+public class Constant {
+    public static final int LOTTERY_MIN_NUMBER = 1;
+    public static final int LOTTERY_MAX_NUMBER = 45;
+    public static final int LOTTERY_TICKET_SIZE = 6;
+    public static final int LOTTERY_TICKET_PRICE = 1_000;
+
+    private Constant() {
+    }
+}
