210a2eecccc71515614bce32d5589d82b90059a8
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 01:36:29 +0900

    refactor: 클래스명 수정

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringCalculator {
+public class StringCalculatorTest {
 
     @Test
     void 덧셈() {
         // given
-        StringCalculator stringCalculator = new StringCalculator()
+        StringCalculator stringCalculator = new StringCalculator();
 
         // when
         int result = stringCalculator.plus(1, 2);
