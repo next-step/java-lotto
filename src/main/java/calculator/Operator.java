80752d4dc7d16bfea9514810b4d670ef3e86e92a
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 02:30:51 +0900

    feat: 사칙연산 아닌경우 예외처리

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -4,8 +4,15 @@ public class Operator {
 
     private final String operator;
 
-    public Operator(String operator) {
-        this.operator = operator;
+    public Operator(String input) {
+        if(isNotOperator(input)) {
+            throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
+        }
+        this.operator = input;
+    }
+
+    private static boolean isNotOperator(String operator) {
+        return !(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/"));
     }
 
     public String value() {
