4ff2dafcaa7423228e6c58ebf0a5fc3bba054d6a
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 20:39:26 +0900

    feat: errorMessage 객체 추카

---
+++ src/main/java/common/error/ErrorMessage.java
@@ -0,0 +1,16 @@
+package common.error;
+
+public enum ErrorMessage {
+
+    NO_SUCH_OPERATOR("연산자를 확인해 주세요:(");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage){
+        this.errorMessage=errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
