e1809ea3519be81d896c1f8160e49a475f6dcdf4
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 20:30:32 +0900

    refactor: sysout제거

--- src/test/java/domain/OperandTest.java
+++ src/test/java/domain/OperandTest.java
@@ -1,7 +1,6 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,7 +16,6 @@ class OperandTest {
     @DisplayName("연산자 조회 테스트")
     void lookUp(String separator, Operand operand) {
 
-        System.out.println(Operand.lookUp(separator));
         assertThat(Operand.lookUp(separator)).isEqualTo(operand);
     }
 
