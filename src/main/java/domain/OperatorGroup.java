384e29cba733cbcab1c29cc9afd678919e9d7558
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 22:57:13 +0900

    feat: 연산자 추출 실패하는 클래스 작성

--- src/main/java/domain/OperatorGroup.java
+++ src/main/java/domain/OperatorGroup.java
@@ -1,15 +1,18 @@
 package domain;
 
+import domain.extractor.Extractor;
+
 import java.util.List;
 
 public class OperatorGroup {
 
     private final List<Operator> operatorList;
     private final OperatorGroupOffset offset;
+    private final Extractor<?> extractor;
 
-
-    private OperatorGroup(List<Operator> operatorList,OperatorGroupOffset offset){
+    private OperatorGroup(List<Operator> operatorList, OperatorGroupOffset offset, Extractor<?> extractor) {
         this.operatorList = operatorList;
         this.offset = offset;
+        this.extractor = extractor;
     }
 }
