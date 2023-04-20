55b0c64ef3bc93eaf9a7fae70665ab30e79e262b
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 21:46:36 +0900

    fix: 클래스 잘못 사용....

--- src/main/java/domain/Operator.java
+++ src/main/java/domain/Operator.java
@@ -1,4 +1,40 @@
 package domain;
 
+import common.error.ErrorMessage;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Operator {
+
+    ADDITION("+"),
+    SUBTRACTION("-"),
+    MULTIPLICATION("*"),
+    DIVISION("/");
+
+    private final String separator;
+
+    private static final Map<String, Operator> separators;
+
+    static {
+        separators = Collections.unmodifiableMap(Stream.of(values())
+                .collect(Collectors.toMap(Operator::getSeparator, Function.identity())));
+    }
+
+    Operator(String separator) {
+        this.separator = separator;
+    }
+
+    public static Operator lookUp(String separator) {
+        return Optional.ofNullable(separators.get(separator))
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NO_SUCH_OPERATOR.getErrorMessage()));
+    }
+
+    private String getSeparator() {
+        return separator;
+    }
 }
