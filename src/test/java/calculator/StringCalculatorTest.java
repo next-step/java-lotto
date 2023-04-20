a510a0a0af065abb60d5b4a8ca6818a6e26a491d
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 01:41:30 +0900

    refactor: @BeforeEach 도입

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,20 +1,22 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void createStringCalculator() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     void 덧셈() {
-        // given
-        StringCalculator stringCalculator = new StringCalculator();
-
-        // when
-        int result = stringCalculator.plus(1, 2);
-
-        // then
-        assertThat(result).isEqualTo(3);
+        assertThat(stringCalculator.plus(1, 2)).isEqualTo(3);
     }
+
 }
