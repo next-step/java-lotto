3f38fb61e5438c011478bde8e628a97743f01c84
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 01:47:39 +0900

    feat: 뺄셈, 곱셈, 나눗셈

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,7 +1,19 @@
 package calculator;
 
 public class StringCalculator {
-    public int plus(int num1, int num2) {
-        return num1 + num2;
+    public int plus(int numBefore, int numAfter) {
+        return numBefore + numAfter;
+    }
+
+    public int minus(int numBefore, int numAfter) {
+        return numBefore - numAfter;
+    }
+
+    public int multiply(int numBefore, int numAfter) {
+        return numBefore * numAfter;
+    }
+
+    public int divide(int numBefore, int numAfter) {
+        return numBefore / numAfter;
     }
 }
