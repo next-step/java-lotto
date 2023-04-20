178b7419de648044dd6a7419581e45ce968f1311
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 02:30:23 +0900

    test: 사칙연산 아닌경우 예외처리

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OperatorTest {
 
@@ -10,5 +11,13 @@ public class OperatorTest {
     void Operator_생성() {
         String operator = new Operator("*").value();
         assertThat(operator).isEqualTo("*");
+    }
+
+    @Test
+    void 사칙연산_아닌경우_예외처리() {
+        assertThatThrownBy(() -> {
+            new Operator("1").value();
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사칙연산 기호가 아닙니다.");
     }
 }
