fb22447b0ec5e565719cba342613580b3435babd
hoyeoon <hoyeon.backend@gmail.com>
2023-04-21 01:35:25 +0900

    test: 덧셈

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,2 +1,20 @@
-package calculator;public class StringCalculator {
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculator {
+
+    @Test
+    void 덧셈() {
+        // given
+        StringCalculator stringCalculator = new StringCalculator()
+
+        // when
+        int result = stringCalculator.plus(1, 2);
+
+        // then
+        assertThat(result).isEqualTo(3);
+    }
 }
