c7fd9b372af76505b3d852d45ab62d86a32857f4
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 23:20:56 +0900

    refactor: 메소드 분리

--- src/main/java/domain/extractor/OperatorExtractor.java
+++ src/main/java/domain/extractor/OperatorExtractor.java
@@ -12,13 +12,21 @@ public class OperatorExtractor implements Extractor<List<Operator>> {
     private static final int INDEX_DIVIDE_NUMBER = 2;
     private static final int INITIAL_INDEX = 0;
 
-
     @Override
-    public List<Operator> extract(List<String> input) {
+    public List<Operator> extract(List<String> inputList) {
 
-        return IntStream.range(INITIAL_INDEX, input.size())
-                .filter(index -> index % INDEX_DIVIDE_NUMBER == ODD_REMAIN_RESULT)
-                .mapToObj(filterIndex -> Operator.lookUp(input.get(filterIndex)))
+        return IntStream.range(INITIAL_INDEX, inputList.size())
+                .filter(this::isOdd)
+                .mapToObj(index-> toOperator(index,inputList))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    private boolean isOdd(int index) {
+        return index % INDEX_DIVIDE_NUMBER == ODD_REMAIN_RESULT;
+    }
+
+    private Operator toOperator(int index, List<String> inputList) {
+        String input = inputList.get(index);
+        return Operator.lookUp(input);
     }
 }
