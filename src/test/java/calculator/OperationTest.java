14d111bf0e9ea447ffc55871a70f5a82185ed8e2
In-wook Hwang <inwook9@gmail.com>
2023-04-20 22:34:56 +0900

    refactor: 테스트 케이스 명 변경

--- src/test/java/calculator/OperationTest.java
+++ src/test/java/calculator/OperationTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static calculator.Operator.ADD;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AdditionTest {
+class OperationTest {
 
     @CsvSource(value = {
             "1,2,3",
@@ -17,7 +18,7 @@ class AdditionTest {
     @DisplayName("입력값을 기준으로 덧셈 연산 후 결과 값을 반환한다.")
     @ParameterizedTest(name = "{0} + {1} = {2}")
     void test01(int a, int b, int expected) {
-        Operation operation = Operation.findType(Operator.ADD);
+        Operation operation = Operation.findType(ADD);
         int actual = operation.calculate(a, b);
         assertThat(actual).isSameAs(expected);
     }
