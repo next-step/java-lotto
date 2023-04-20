7b09fd70b0bd94236b304a3ff9a192a68bc1c210
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 22:45:17 +0900

    feat: 분모를 0으로 나눌시 실패하는 테스트 작성

--- src/main/java/common/error/ErrorMessage.java
+++ src/main/java/common/error/ErrorMessage.java
@@ -2,7 +2,9 @@ package common.error;
 
 public enum ErrorMessage {
 
-    NO_SUCH_OPERATOR("입력하신 연산자를 확인해 주세요:(");
+    NO_SUCH_OPERATOR("입력하신 연산자를 확인해 주세요 :("),
+    NO_DIVIDE_DENOMINATOR_ZERO("분모를 0으로 나눌 수 없어요 :(");
+
 
     private final String errorMessage;
 
