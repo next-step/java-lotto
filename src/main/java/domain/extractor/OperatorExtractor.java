d641bdea6c4f89c0e65934d89cd74d339323539f
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-20 23:16:00 +0900

    feat: 연산자 추출 성공하는 테스트 코드 작성

--- src/main/java/domain/extractor/OperatorExtractor.java
+++ src/main/java/domain/extractor/OperatorExtractor.java
@@ -3,13 +3,22 @@ package domain.extractor;
 import domain.Operator;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class OperatorExtractor implements Extractor<List<Operator>> {
 
     private static final int ODD_REMAIN_RESULT = 1;
+    private static final int INDEX_DIVIDE_NUMBER = 2;
+    private static final int INITIAL_INDEX = 0;
+
 
     @Override
     public List<Operator> extract(List<String> input) {
-        return null;
+
+        return IntStream.range(INITIAL_INDEX, input.size())
+                .filter(index -> index % INDEX_DIVIDE_NUMBER == ODD_REMAIN_RESULT)
+                .mapToObj(filterIndex -> Operator.lookUp(input.get(filterIndex)))
+                .collect(Collectors.toUnmodifiableList());
     }
 }
