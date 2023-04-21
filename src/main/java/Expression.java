1711da5d77d70b293d0807d1b50487b93e02d351
Baek Jiyeon <100.paperkite@gmail.com>
2023-04-21 13:07:26 +0900

    feat: 입력 문자열 분리

---
+++ src/main/java/Expression.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Expression {
+    private static final String DELIMITER = " ";
+
+    private Expression() {
+    }
+
+    public static List<String> parse(String expression) {
+        validate(expression);
+
+        return List.of(expression.split(DELIMITER));
+    }
+
+    private static void validate(String expression) {
+        if (expression == null || expression.isEmpty()) {
+            throw new IllegalArgumentException("input expression should be existed");
+        }
+    }
+}
---
+++ src/test/java/ExpressionTest.java
@@ -0,0 +1,19 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+class ExpressionTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"0=1", "1 + 2=3", "1 + 2 / 3=5"}, delimiter = '=')
+    @DisplayName("공백 기준으로 문자열이 분리가 잘 되어야 한다")
+    public void parseByDelimiter(String expression, int count) {
+        List<String> tokens = Expression.parse(expression);
+
+        Assertions.assertThat(tokens).hasSize(count);
+    }
+
+}
