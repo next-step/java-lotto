307e631d7f5d5098bf8e2c5b99fffdecd6ae1721
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-21 23:33:14 +0900

    feat: 피연산자 추출 구현

--- src/main/java/common/error/ErrorMessage.java
+++ src/main/java/common/error/ErrorMessage.java
@@ -3,13 +3,14 @@ package common.error;
 public enum ErrorMessage {
 
     NO_SUCH_OPERATOR("입력하신 연산자를 확인해 주세요 :("),
-    NO_DIVIDE_DENOMINATOR_ZERO("분모를 0으로 나눌 수 없어요 :(");
+    NO_DIVIDE_DENOMINATOR_ZERO("분모를 0으로 나눌 수 없어요 :("),
+    NO_SUCH_OPERAND("입력하신 피연자 값을 확인해주세요 :(");
 
 
     private final String errorMessage;
 
-    ErrorMessage(String errorMessage){
-        this.errorMessage=errorMessage;
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 
     public String getErrorMessage() {
