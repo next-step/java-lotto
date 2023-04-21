7aff7be57c06c69244a39703bf0a3050899d4095
jaden.1230 <jaden.1230@kakaocorp.com>
2023-04-21 14:00:52 +0900

    refactor: convert initial variables to constants

--- src/main/java/Calculator/CalculatorController.java
+++ src/main/java/Calculator/CalculatorController.java
@@ -9,20 +9,23 @@ import java.util.ArrayList;
 
 public class CalculatorController {
     private static final Integer INDEX_CALCULATE_CURRENT_ELEMENT = 0;
+    private static final String INITIAL_NUMBER_BEFORE_ARGUMENT = "0";
+    private static final String INITIAL_OPERATOR = "+";
+
 
     public void start() {
+
+
         System.out.println("계산기를 시작합니다.");
 
         String[] elements = InputView.getAlgebra();
 
-        String numberBeforeArgument = "0";
-        String operator = "+";
         ArrayList<String> results = new ArrayList<>();
-        results.add(numberBeforeArgument);
-        results.add(operator);
+        results.add(INITIAL_NUMBER_BEFORE_ARGUMENT);
+        results.add(INITIAL_OPERATOR);
 
         Calculator calculator = new Calculator();
-        for (String element: elements) {
+        for (String element : elements) {
             results.add(element);
             results = calculator.calculate(results);
         }
