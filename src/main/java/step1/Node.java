108effb5ba4cf289cca891fbfa16633cd86e8976
dong <dong.22@kakaoenterprise.com>
2023-04-22 10:35:23 +0900

    delete : unused Constructor delete

--- src/main/java/step1/Node.java
+++ src/main/java/step1/Node.java
@@ -2,16 +2,9 @@ package step1;
 
 public class Node {
     private final int value;
-
-    public Node(int value) {
-        this.value = value;
-    }
-
     public Node(String current) {
         value = Integer.parseInt(current);
     }
-
-
     public int getValue() {
         return value;
     }
