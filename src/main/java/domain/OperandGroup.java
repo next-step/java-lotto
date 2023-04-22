0d2d3380e40d0425de31949a71e69c188f0ff654
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-22 23:14:44 +0900

    feat: 피연산자 그룹 및 오프셋

---
+++ src/main/java/domain/OperandGroup.java
@@ -0,0 +1,29 @@
+package domain;
+
+import domain.extractor.Extractor;
+
+import java.util.List;
+
+public class OperandGroup {
+    private final List<Integer> operandList;
+    private final OperandGroupOffset offset;
+
+    private OperandGroup(OperandGroupOffset offset, List<Integer> operandList) {
+        this.operandList = operandList;
+        this.offset = offset;
+    }
+
+    public static OperandGroup of(OperandGroupOffset offset, Extractor<List<Integer>> extractor, List<String> input) {
+        return new OperandGroup(offset, extractor.extract(input));
+    }
+
+    public boolean isContinue() {
+        return offset.isLastOffset(operandList.size() - 1);
+    }
+
+    public int provideOperand() {
+        int currentOffset = offset.increaseAndGet();
+
+        return operandList.get(currentOffset);
+    }
+}
