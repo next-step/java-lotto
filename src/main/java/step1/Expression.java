3cbf04057d681e1d7e00c5ac22f051d5295d5d5e
dong <dong.22@kakaoenterprise.com>
2023-04-22 11:08:18 +0900

    refactor : 포매팅 수정

--- src/main/java/step1/Expression.java
+++ src/main/java/step1/Expression.java
@@ -14,9 +14,9 @@ public class Expression {
 
     public static List<Node> extractNodes(List<String> inputList) {
         List<Node> nodeList = new ArrayList<>();
-        for( int i=0 ; i<inputList.size() ; i++ ) {
+        for (int i = 0; i < inputList.size(); i++) {
             String current = inputList.get(i);
-            if(i%2 == 0) {
+            if (i % 2 == 0) {
                 nodeList.add(new Node(current));
             }
         }
@@ -25,9 +25,9 @@ public class Expression {
 
     public static List<Operator> extractOperators(List<String> inputList) {
         List<Operator> operatorList = new ArrayList<>();
-        for( int i=0 ; i<inputList.size() ; i++ ) {
+        for (int i = 0; i < inputList.size(); i++) {
             String current = inputList.get(i);
-            if(i%2 == 1) {
+            if (i % 2 == 1) {
                 operatorList.add(new Operator(current));
             }
         }
@@ -36,7 +36,7 @@ public class Expression {
 
     public int execute() {
         int sum = nodeList.get(0).getValue();
-        for (int i =0 ; i<operatorList.size() ; i++) {
+        for (int i = 0; i < operatorList.size(); i++) {
             sum = operatorList.get(i).calculate(sum, nodeList.get(i + 1).getValue());
         }
         return sum;
--- src/main/java/step1/Operator.java
+++ src/main/java/step1/Operator.java
@@ -1,7 +1,6 @@
 package step1;
 
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class Operator {
 
@@ -16,15 +15,19 @@ public class Operator {
             this.operator = (i1, i2) -> i1 - i2;
             return;
         }
-        if(current.equals("*")) {
+        if (current.equals("*")) {
             this.operator = (i1, i2) -> i1 * i2;
             return;
         }
-        if(current.equals("/")) {
+        if (current.equals("/")) {
             this.operator = (i1, i2) -> i1 / i2;
             return;
         }
         throw new IllegalArgumentException();
+    }
+
+    public static boolean isOperator(String input) {
+        return input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/");
     }
 
     int calculate(int leftValue, int rightValue) {
