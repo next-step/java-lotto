9979f7e89bb2f1f7f13a68e806a1ccd4e22bfc9a
dong <dong.22@kakaoenterprise.com>
2023-04-22 09:39:15 +0900

    feat : when input blank or null then thorw

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -3,6 +3,12 @@ package step1;
 public class StringCalculator {
 
     public int calculate(String input) {
+        if(input==null || input.isBlank() ) {
+            throw new IllegalArgumentException();
+        }
+
+
         return -1;
+
     }
 }
