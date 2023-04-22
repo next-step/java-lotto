3e517ba3c9e07bf5b68fcf475da8467e5d54f08b
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-22 23:09:07 +0900

    feat: 인풋 , 아웃풋 뷰 , 계산기 구현

---
+++ src/main/java/service/StringCalculator.java
@@ -0,0 +1,30 @@
+package service;
+
+import domain.OperandGroup;
+import domain.Operator;
+import domain.OperatorGroup;
+
+public class StringCalculator {
+    private final OperatorGroup operatorGroup;
+    private final OperandGroup operandGroup;
+
+    public StringCalculator(OperatorGroup operatorGroup, OperandGroup operandGroup) {
+        this.operatorGroup = operatorGroup;
+        this.operandGroup = operandGroup;
+    }
+
+    public int calculate() {
+        int result = getInitializeNumber();
+
+        while (operandGroup.isContinue()) {
+            int rightOperand = operandGroup.provideOperand();
+            Operator operator = operatorGroup.provideOperator();
+            result = operatorGroup.operatorExecute(operator, result, rightOperand);
+        }
+        return result;
+    }
+
+    private int getInitializeNumber() {
+        return operandGroup.provideOperand();
+    }
+}
