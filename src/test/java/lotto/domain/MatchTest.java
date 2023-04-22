a698266f90ab9a72d88d1193262074133bd87585
dong <dong.22@kakaoenterprise.com>
2023-04-23 01:33:58 +0900

    test : 실패하는 테스트 추가

--- src/test/java/lotto/domain/MatchTest.java
+++ src/test/java/lotto/domain/MatchTest.java
@@ -1,7 +1,17 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MatchTest {
-
+    @DisplayName("발급수량만큼 응모권이 발행된다")
+    @Test
+    public void issue() {
+        //given
+        //when
+        //then
+        fail();
+    }
 }
