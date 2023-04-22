6c8cb7f4336371b3f29c0cacbf73399396017248
dong <dong.22@kakaoenterprise.com>
2023-04-22 11:29:03 +0900

    refactor : 반복되는 매직 키워드를 스태틱 변수로 묶어주기

--- src/main/java/step1/Operator.java
+++ src/main/java/step1/Operator.java
@@ -1,37 +1,29 @@
 package step1;
 
+import java.util.Map;
 import java.util.function.BiFunction;
 
 public class Operator {
-
+    private static final Map<String, BiFunction<Integer, Integer, Integer>> operators = Map.of(
+            "+", (i1, i2) -> i1 + i2,
+            "-", (i1, i2) -> i1 - i2,
+            "*", (i1, i2) -> i1 * i2,
+            "/", (i1, i2) -> i1 / i2
+    );
     private final BiFunction<Integer, Integer, Integer> operator;
 
-    public Operator(String current) {
-        if (current.equals("+")) {
-            this.operator = (i1, i2) -> i1 + i2;
-            return;
+    public Operator(String token) {
+        if (!operators.containsKey(token)) {
+            throw new IllegalArgumentException();
         }
-        if (current.equals("-")) {
-            this.operator = (i1, i2) -> i1 - i2;
-            return;
-        }
-        if (current.equals("*")) {
-            this.operator = (i1, i2) -> i1 * i2;
-            return;
-        }
-        if (current.equals("/")) {
-            this.operator = (i1, i2) -> i1 / i2;
-            return;
-        }
-        throw new IllegalArgumentException();
+        operator = operators.get(token);
     }
 
-    public static boolean isOperator(String input) {
-        return input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/");
+    public static boolean isOperator(String token) {
+        return operators.containsKey(token);
     }
 
     int calculate(int leftValue, int rightValue) {
         return operator.apply(leftValue, rightValue);
     }
-
 }
