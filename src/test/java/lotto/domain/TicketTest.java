0d0b05e53b9c8a7211924601e4c752343abec8d1
dong <dong.22@kakaoenterprise.com>
2023-04-23 01:23:06 +0900

    test & docs : 문구수정

--- src/test/java/lotto/domain/TicketTest.java
+++ src/test/java/lotto/domain/TicketTest.java
@@ -1,7 +1,18 @@
 package lotto.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TicketTest {
+    @DisplayName("응모권마다 6개의 번호를 지정한다")
+    @Test
+    public void ticketValidate() {
+        //given
+        //when
+        //then
+        fail();
+    }
 
 }
