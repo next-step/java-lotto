19debad15699add64e5aaf44fa8d29e594870416
dong <dong.22@kakaoenterprise.com>
2023-04-22 10:32:52 +0900

    feat : 문자열 계산기 구현

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -1,14 +1,28 @@
 package step1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public int calculate(String input) {
         if(input==null || input.isBlank() ) {
             throw new IllegalArgumentException();
         }
-
-
-        return -1;
+        String[] inputArray = input.split(" ");
+        List<Node> nodeList = new ArrayList<>();
+        List<Operator> operatorList = new ArrayList<>();
+        for( int i=0 ; i<inputArray.length ; i++ ) {
+            String current = inputArray[i];
+            if(i%2 == 0) {
+                nodeList.add(new Node(current));
+            }
+            if(i%2 == 1) {
+                operatorList.add(new Operator(current));
+            }
+        }
+        Expression expression = new Expression(nodeList, operatorList);
+        return expression.execute();
 
     }
 }
