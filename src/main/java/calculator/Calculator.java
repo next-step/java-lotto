e2127fd07f54a844247de0bc2e4bede8a95e6a7f
juno-junho <ssmm0205@naver.com>
2023-04-22 15:50:49 +0900

    feat(calculator): 사칙연산 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,74 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class Calculator {
+    public int add(String expression) {
+        String[] split = expression.split(" ");
+        return Arrays.stream(split)
+                .filter(i -> !i.equals("+"))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    public int substract(String expression) {
+        String[] split = expression.split(" ");
+        int beforeOperation = 0;
+        boolean substraction = false;
+        for (String s : split) {
+            if (substraction) {
+                beforeOperation = beforeOperation - Integer.parseInt(s);
+                substraction = false;
+                continue;
+            }
+            if (s.equals("-")) {
+                substraction = true;
+                continue;
+            }
+            beforeOperation = Integer.parseInt(s);
+        }
+        return beforeOperation;
+    }
+
+    public int multiply(String expression) {
+        String[] split = expression.split(" ");
+        int beforeOperation = 0;
+        boolean multiplication = false;
+        for (String s : split) {
+            if (multiplication) {
+                beforeOperation *= Integer.parseInt(s);
+                multiplication = false;
+                continue;
+            }
+            if (s.equals("*")) {
+                multiplication = true;
+                continue;
+            }
+            beforeOperation = Integer.parseInt(s);
+        }
+        return beforeOperation;
+    }
+
+    public int divide(String expression) {
+        String[] split = expression.split(" ");
+        int beforeOperation = 0;
+        boolean division = false;
+        for (String s : split) {
+            if (division) {
+                double floatNum = (double) beforeOperation / Integer.parseInt(s);
+                beforeOperation = beforeOperation / Integer.parseInt(s);
+                if (floatNum != beforeOperation) {
+                    throw new ArithmeticException("나누어 떨어지지 않는 값을 입력하셨습니다.");
+                }
+                division = false;
+                continue;
+            }
+            if (s.equals("/")) {
+                division = true;
+                continue;
+            }
+            beforeOperation = Integer.parseInt(s);
+        }
+        return beforeOperation;
+    }
+}
