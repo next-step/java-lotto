637be21ca001005c82c621e1efc8cebc4191e55c
dong <dong.22@kakaoenterprise.com>
2023-04-23 01:34:20 +0900

    test : 랜덤생성의 주체를 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,11 +5,10 @@ import lotto.domain.Ticket;
 import lotto.present.InputPresent;
 import lotto.present.OutputPresent;
 
-import java.util.List;
 import java.util.Random;
 
 public class Lotto {
-    private final Random RANDOM = new Random();
+
 
     public static void main(String[] args) {
         InputPresent inputPresent = new InputPresent();
@@ -19,9 +18,7 @@ public class Lotto {
         int count = inputPresent.purchaseCount();
 
 
-        for(int i=0 ; i<count ; i++) {
-            match.addTicket(new Ticket(purchaseGame()));
-        }
+        match.purchase(count);
         outputPresent.presentTickets(match.renderingTickets());
         Ticket winningTicket = inputPresent.winningNumber();
         match.winningTicket(winningTicket);
@@ -29,7 +26,6 @@ public class Lotto {
         outputPresent.presentReturnRate(match.returnRate());
     }
 
-    private static List<Integer> purchaseGame() {
-        throw new RuntimeException("6개 랜덤 숫자 리스트 만들어서 넘겨주기");
-    }
+
+
 }
--- src/main/java/lotto/domain/Match.java
+++ src/main/java/lotto/domain/Match.java
@@ -2,8 +2,10 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Match {
+    private final Random RANDOM = new Random();
     private final List<Ticket> tickets;
     private Winners winners = null;
 
@@ -11,7 +13,20 @@ public class Match {
         this.tickets = new ArrayList<>();
     }
 
-    public void addTicket(Ticket ticket) {
+    public void purchase(int count) {
+        for(int i = 0; i< count; i++) {
+            addTicket(new Ticket(issuance()));
+        }
+    }
+    private List<Integer> issuance() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i=0 ; i<6 ; i++) {
+            numbers.add(RANDOM.nextInt(46));
+        }
+        return numbers;
+    }
+
+    private void addTicket(Ticket ticket) {
         tickets.add(ticket);
     }
 
