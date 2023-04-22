0e32981d5d22e20899cc1bf9e629b374fe38ff6e
dong <dong.22@kakaoenterprise.com>
2023-04-23 01:20:28 +0900

    test : 테스트클래스 작성

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,7 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LottoTest {
+
+}
---
+++ src/test/java/lotto/domain/TicketTest.java
@@ -0,0 +1,7 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TicketTest {
+
+}
