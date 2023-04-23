0cd065148d3daf587e38e47ce787fae2fcb8f0e8
dong <dong.22@kakaoenterprise.com>
2023-04-23 14:08:24 +0900

    test : 테스트 자동화 레드테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,18 @@
 package lotto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class LottoTest {
-
+    @DisplayName("로또앱을 실행하는 동작테스트를 자동화한다")
+    @Test
+    public void lottoApplicationContextTest() {
+        //given
+        //when
+        Lotto.main(new String[]{});
+        //then
+        fail();
+    }
 }
