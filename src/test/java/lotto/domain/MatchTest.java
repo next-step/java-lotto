6d2dcbbf0e99df620f59b1a17dd8143c009cbb36
dong <dong.22@kakaoenterprise.com>
2023-04-23 10:28:21 +0900

    test : 실패하는 Match 테스트 추가

--- src/test/java/lotto/domain/MatchTest.java
+++ src/test/java/lotto/domain/MatchTest.java
@@ -1,17 +1,27 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MatchTest {
+    private Match matchFixture;
+    @BeforeEach
+    public void beforeEach() {
+        matchFixture = new Match();
+    }
+
     @DisplayName("발급수량만큼 응모권이 발행된다")
     @Test
     public void issue() {
         //given
+        int input = 77;
         //when
+        matchFixture.purchase(input);
         //then
-        fail();
+        assertThat(matchFixture.renderingTickets()).hasSize(input);
     }
 }
