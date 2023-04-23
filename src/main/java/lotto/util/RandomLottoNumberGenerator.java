a18960816e3f0f4bd1898e2bdb2fed1b0673d3d5
dkswnkk <jjoy9810@gmail.com>
2023-04-23 17:08:14 +0900

    feat: 로또 번호 생성 Util 구현

---
+++ src/main/java/lotto/util/RandomLottoNumberGenerator.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomLottoNumberGenerator {
+    public static final int LOTTERY_MIN_NUMBER = 1;
+    public static final int LOTTERY_MAX_NUMBER = 45;
+
+    public static List<Integer> generate() {
+        List<Integer> numbers = IntStream.rangeClosed(LOTTERY_MIN_NUMBER, LOTTERY_MAX_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Collections.shuffle(numbers);
+
+        return numbers.subList(0, 6);
+    }
+
+}
