b52082e75d783d131abe9233059e3f5e7907fe89
dong <dong.22@kakaoenterprise.com>
2023-04-23 14:13:56 +0900

    feat : 구매금액 입력부분 구현

--- src/main/java/lotto/present/InputPresent.java
+++ src/main/java/lotto/present/InputPresent.java
@@ -2,15 +2,20 @@ package lotto.present;
 
 import lotto.domain.Ticket;
 
+import java.util.Scanner;
+
 public class InputPresent {
     private static final String PURCHASE_AMOUNT = "구입금액을 입력해 주세요";
     private static final String PURCHASE_CONFIRM = "%s개를 구매했습니다";
     private static final String WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요";
+    private final Scanner scanner;
     public InputPresent() {
+        scanner = new Scanner(System.in);
     }
 
     public int purchaseCount() {
-        throw new RuntimeException();
+        System.out.println(PURCHASE_AMOUNT);
+        return scanner.nextInt();
     }
 
     public Ticket winningNumber() {
