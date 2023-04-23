e8bb516081c45213dd06c1fb20df731095dd23cb
saerang <lsg10119@gamail.com>
2023-04-24 00:28:15 +0900

    feat: 당첨번호 구현

---
+++ src/main/java/lotto/domain/WinType.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum WinType {
+    FIRST(6,2000000000),
+    SECOND(99, 0),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
+
+    private final long matchedQuantity;
+    private final long winnings;
+
+    private static final Map<Long, WinType> WIN_TYPE_MAP
+            = Collections.unmodifiableMap(Stream.of(values())
+                                                .collect(Collectors.toMap(WinType::getMatchedQuantity, x -> x)));
+
+    WinType(long matchedQuantity, long winnings) {
+        this.matchedQuantity = matchedQuantity;
+        this.winnings = winnings;
+    }
+
+    public long getMatchedQuantity() {
+        return matchedQuantity;
+    }
+
+    public static WinType find(long matchedQuantity) {
+        if (WIN_TYPE_MAP.containsKey(matchedQuantity)) {
+            return WIN_TYPE_MAP.get(matchedQuantity);
+        }
+
+        throw new IllegalArgumentException("WinType 을 찾을 수 없습니다. matchedQuantity: " + matchedQuantity);
+    }
+}
