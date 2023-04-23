8f0b15e0289404ef562afa269bc3e7413437e921
saerang <lsg10119@gamail.com>
2023-04-23 23:07:02 +0900

    refactor: lottoes -> lottos 이름 변경 - o로 끝나서 es를 붙였으나, s를 붙이는게 맞을 듯 - http://ko.wordow.com/english/dictionary/lottos

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -3,23 +3,23 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lottoes {
+public class Lottos {
 
     public static int LOTTO_PRICE = 1000;
-    private final List<Lotto> lottoes = new ArrayList<>();
+    private final List<Lotto> lottos = new ArrayList<>();
 
-    public Lottoes(int price) {
+    public Lottos(int price) {
         if (price < LOTTO_PRICE) {
             throw new IllegalArgumentException("로또는 " + LOTTO_PRICE + "의 금액부터 구매가 가능합니다. 주문금액: " + price);
         }
 
         for (int i = 0; i < price / LOTTO_PRICE; i++) {
-            lottoes.add(new Lotto());
+            lottos.add(new Lotto());
         }
     }
 
 
-    public List<Lotto> lottoes() {
-        return this.lottoes;
+    public List<Lotto> lottos() {
+        return this.lottos;
     }
 }
--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -6,27 +6,27 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LottoesTest {
+class LottosTest {
 
     @Test
-    @DisplayName("Lottoes 를 생성한다.")
+    @DisplayName("Lottos 를 생성한다.")
     void test01() {
-        Lottoes lottoes = new Lottoes(1000);
+        Lottos lottos = new Lottos(1000);
 
-        assertThat(lottoes).isNotNull();
+        assertThat(lottos).isNotNull();
     }
 
     @Test
-    @DisplayName("구입금액으로 Lottoes 를 생성한다.")
+    @DisplayName("구입금액으로 Lottos 를 생성한다.")
     void test02() {
-        Lottoes lottoes = new Lottoes(10100);
+        Lottos lottos = new Lottos(10100);
 
-        assertThat(lottoes.lottoes()).hasSize(10);
+        assertThat(lottos.lottos()).hasSize(10);
     }
 
     @Test
     @DisplayName("구입금액이 천원보다 적으면 에러를 발생한다.")
     void test03() {
-        assertThatThrownBy(() -> new Lottoes(0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lottos(0)).isInstanceOf(IllegalArgumentException.class);
     }
 }
