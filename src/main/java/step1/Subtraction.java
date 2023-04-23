7e4cc25002739645e233837ef2a71d5c0f6b84f7
zuri-pong <zuri.pong@kakaocorp.com>
2023-04-24 03:11:04 +0900

    변수명 x,y 로 통일하게 수정

--- src/main/java/step1/Subtraction.java
+++ src/main/java/step1/Subtraction.java
@@ -2,7 +2,7 @@ package step1;
 
 public class Subtraction implements Operator{
     @Override
-    public int apply(int num1, int num2) {
-        return num1 - num2;
+    public int apply(int x, int y) {
+        return x - y;
     }
 }
