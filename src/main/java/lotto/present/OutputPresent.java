a40b1a366512377c32ca78314f099f2be96cf299
dong <dong.22@kakaoenterprise.com>
2023-04-23 21:03:37 +0900

    chore : 오타수정

--- src/main/java/lotto/present/OutputPresent.java
+++ src/main/java/lotto/present/OutputPresent.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class OutputPresent {
     private static final String STATISTICS_HEAD = "당첨 통계" + System.lineSeparator() + "---------";
     private static final String PRIZE_5TH = "3개 일치 (5000원)- %s개";
-    private static final String PRISE_4TH = "4개 일치 (50000원)- %s개";
+    private static final String PRIZE_4TH = "4개 일치 (50000원)- %s개";
     private static final String PRIZE_3RD = "5개 일치 (1500000원)- %s개";
     private static final String PRIZE_1ST = "6개 일치 (2000000000원)- %s개";
     private static final String RETURN_RATE = "총 수익률은 %f2입니다";
