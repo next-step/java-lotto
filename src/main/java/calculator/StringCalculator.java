04553af0a518ac88c8c10e11abce8a11ebe69721
hoyeoon <hoyeon.backend@gmail.com>
2023-04-23 19:56:00 +0900

    refactor: numbers, operators 리스트 생성 방식 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -12,6 +12,7 @@ public class StringCalculator {
     private static final String SEPARATOR = " ";
     private static final int BEGIN_INDEX = 0;
     private static final int INCREMENT_NEXT_INDEX = 1;
+    public static final int INCREMENT_COUNT = 2;
 
     private Number result;
 
@@ -19,53 +20,43 @@ public class StringCalculator {
 
         String[] arr = input.split(SEPARATOR);
 
-        List<Number> numbers = new ArrayList<>();
-        List<Operator> operators = new ArrayList<>();
-
-        for (int i = BEGIN_INDEX; i < arr.length; i++) {
-            makeNumber(arr, numbers, i);
-            makeOperator(arr, operators, i);
-        }
+        List<Number> numbers = makeNumbers(arr);
+        List<Operator> operators = makeOperators(arr);
 
         initResult(numbers.get(BEGIN_INDEX));
 
-        for (int i = BEGIN_INDEX; i < operators.size(); i++) {
-            calculate(operators, numbers, i);
-        }
+        calculate(numbers, operators);
     }
 
     public int result() {
         return result.intValue();
     }
 
+    private List<Number> makeNumbers(String[] arr) {
+        List<Number> numbers = new ArrayList<>();
+        for (int i = BEGIN_INDEX; i < arr.length; i += INCREMENT_COUNT) {
+            numbers.add(new Number(arr[i]));
+        }
+        return numbers;
+    }
+
+    private List<Operator> makeOperators(String[] arr) {
+        List<Operator> operators = new ArrayList<>();
+        for (int i = BEGIN_INDEX + INCREMENT_NEXT_INDEX; i < arr.length; i += INCREMENT_COUNT) {
+            operators.add(new Operator(arr[i]));
+        }
+        return operators;
+    }
+
     private void initResult(Number number) {
         result = number;
     }
 
-    private void calculate(List<Operator> operators, List<Number> numbers, int i) {
-        OperatorType operator = operators.get(i).value();
-        int calculatedResult = operator.calculate(result, numbers.get(i + INCREMENT_NEXT_INDEX));
-        result = new Number(calculatedResult);
-    }
-
-    private static void makeNumber(String[] arr, List<Number> numbers, int i) {
-        if (isEven(i)) {
-            numbers.add(new Number(arr[i]));
+    private void calculate(List<Number> numbers, List<Operator> operators) {
+        for (int i = BEGIN_INDEX; i < operators.size(); i++) {
+            OperatorType operator = operators.get(i).value();
+            int calculatedResult = operator.calculate(result, numbers.get(i + INCREMENT_NEXT_INDEX));
+            result = new Number(calculatedResult);
         }
     }
-
-    private static void makeOperator(String[] arr, List<Operator> operators, int i) {
-        if (isOdd(i)) {
-            operators.add(new Operator(arr[i]));
-        }
-    }
-
-    private static boolean isEven(int i) {
-        return i % 2 == 0;
-    }
-
-    private static boolean isOdd(int i) {
-        return i % 2 == 1;
-    }
-
 }
