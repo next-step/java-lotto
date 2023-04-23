99b3c789cd158e2d9a1047f8c469753c2ad0e2d5
dong <dong.22@kakaoenterprise.com>
2023-04-23 20:36:28 +0900

    test : 수익비율을 검증하는 테스트코드를 작성한다

--- src/test/java/lotto/domain/WinnersTest.java
+++ src/test/java/lotto/domain/WinnersTest.java
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WinnersTest {
 
     private Winners winnersFixture;
-    private Ticket winTicketFixture;
-    private List<Ticket> purchaseTicketsFixture;
+
 
     @BeforeEach
     public void beforeEach() {
-        winTicketFixture = new Ticket(Set.of(1, 2, 4, 8, 16, 32));
+        Ticket winner = new Ticket(Set.of(1, 2, 4, 8, 16, 32));
 
         Ticket first = new Ticket(Set.of(1, 2, 4, 8, 16, 32));
 
@@ -36,13 +36,13 @@ public class WinnersTest {
         Ticket fifthD = new Ticket(Set.of(1, 2, 4, 25, 36, 27));
         Ticket fifthE = new Ticket(Set.of(1, 2, 4, 35, 26, 17));
 
-        purchaseTicketsFixture = List.of(
+        List<Ticket> purchaseTicketsFixture = List.of(
                 first,
                 thirdA, thirdB, thirdC,
                 fourthA, fourthB, fourthC, fourthD,
                 fifthA, fifthB, fifthC, fifthD, fifthE
         );
-        winnersFixture = new Winners(purchaseTicketsFixture, winTicketFixture);
+        winnersFixture = new Winners(purchaseTicketsFixture, winner);
     }
 
     @DisplayName("1등 담첨통계가 계산된다(6개 번호 일치)")
@@ -90,24 +90,22 @@ public class WinnersTest {
     @Test
     public void income() {
         //given
-        int answer = Prize.FIRST.prizeCalculate(1) +
-                Prize.THIRD.prizeCalculate(3) +
-                Prize.FOURTH.prizeCalculate(4) +
-                Prize.FIFTH.prizeCalculate(5);
+        int answer = 2_004_725_000;
         //when
         int income = winnersFixture.getIncome();
         //then
-        assertEquals(answer, income);
+        assertThat(income).isEqualTo(answer).as("당첨금액 계산식 = Prize.FIRST.prizeCalculate(1) + Prize.THIRD.prizeCalculate(3) + Prize.FOURTH.prizeCalculate(4) + Prize.FIFTH.prizeCalculate(5)");
     }
 
     @DisplayName("투자금 대비 당첨금액의 비율을 사이의 소수점 둘째자리까지 표시한다")
     @Test
     public void returnRatio() {
         //given
-        double answer = 2.00;//계산필요 아직모름
+        int answerX1000 = 154209615;
         //when
-        double ratio = winnersFixture.returnRatio();
+        int ratioX1000 = (int) (winnersFixture.getProfitRatio() * 1000);
         //then
-        assertEquals(answer, ratio);
+        assertThat(ratioX1000).isEqualTo(answerX1000).as("double 형식은 값비교가 힘들어서 1000을 곱한다음 int 형식으로 캐스팅하여 검증했습니다");
+        assertThat(ratioX1000).isEqualTo(answerX1000).as("투자금 13게임[13000₩], 수익금[2004725000₩], 투자대비 수익비율 [154209.615385]");
     }
 }
