90dbc29a45282e8562eade689327c0139754627a
ryan <wsdf25867@gmail.com>
2023-04-23 17:18:16 +0900

    feature : Operator 작성
    * Operator 및 Test 코드 작성

---
+++ src/test/java/stringcalculator/OperatorTest.java
@@ -0,0 +1,27 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class OperatorTest {
+
+    @Test
+    void plus() {
+        assertThat(Operator.of("+")).isEqualTo(Operator.PLUS);
+    }
+
+    @Test
+    void minus() {
+        assertThat(Operator.of("+")).isEqualTo(Operator.MINUS);
+    }
+
+    @Test
+    void multiply() {
+        assertThat(Operator.of("*")).isEqualTo(Operator.MULTIPLY);
+    }
+    @Test
+    void divide() {
+        assertThat(Operator.of("/")).isEqualTo(Operator.DIVIDE);
+    }
+}
