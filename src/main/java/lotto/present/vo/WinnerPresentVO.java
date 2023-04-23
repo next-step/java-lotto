0fd2e19aa49750e11694495ce36113064f08ee92
dong <dong.22@kakaoenterprise.com>
2023-04-23 21:04:56 +0900

    feat : 로또 당첨 결과 출력 구현

--- src/main/java/lotto/present/vo/WinnerPresentVO.java
+++ src/main/java/lotto/present/vo/WinnerPresentVO.java
@@ -5,7 +5,30 @@ import lotto.domain.Winners;
 import java.util.List;
 
 public class WinnerPresentVO {
+    private final int countFifth;
+    private final int countFourth;
+    private final int countThird;
+    private final int countFirst;
     public WinnerPresentVO(Winners winners) {
-        throw new RuntimeException("미구현");
+        this.countFifth = winners.getCountFifth();
+        this.countFourth = winners.getCountFourth();
+        this.countThird = winners.getCountThird();
+        this.countFirst = winners.getCountFirst();
+    }
+
+    public int getCountFifth() {
+        return this.countFifth;
+    }
+
+    public int getCountFourth() {
+        return this.countFourth;
+    }
+
+    public int getCountThird() {
+        return this.countThird;
+    }
+
+    public int getCountFirst() {
+        return this.countFirst;
     }
 }
