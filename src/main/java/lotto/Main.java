178c72f5c759e793477a53af76f1e9d46b37cc8d
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-04-25 01:18:55 +0900

    feature - 이미 입력한 당첨 번호에 포함된 번호면 에러 리턴

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.*;
+import lotto.exception.SameNumberException;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -15,8 +16,15 @@ public class Main {
 
         Lotto winningLotto = InputView.inputWinningNumbers();
         LottoNumber bonusNumber = InputView.inputBonusNumber();
+        checkContainsNumber(winningLotto, bonusNumber);
 
         Winners winners = lottos.findStatistics(winningLotto, bonusNumber);
         OutputView.printResult(winners, lottoAmount);
+    }
+
+    private static void checkContainsNumber(Lotto winningLotto, LottoNumber bonusNumber) {
+        if (winningLotto.hasBonusNumber(bonusNumber)) {
+            throw new IllegalArgumentException("이미 당첨번호에 있습니다.");
+        }
     }
 }
