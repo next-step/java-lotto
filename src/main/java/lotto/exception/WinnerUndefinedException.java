e1f0e76524cdc68f26258a0a96a77b4396e6933b
김동훈 <ultraraptor@naver.com>
2023-04-24 21:08:52 +0900

    Update src/main/java/lotto/exception/WinnerUndefinedException.java
    Co-authored-by: WonChul Heo <heowc1992@gmail.com>

--- src/main/java/lotto/exception/WinnerUndefinedException.java
+++ src/main/java/lotto/exception/WinnerUndefinedException.java
@@ -2,6 +2,6 @@ package lotto.exception;
 
 public class WinnerUndefinedException extends RuntimeException{
     public WinnerUndefinedException() {
-        throw new RuntimeException("당첨티켓이 집계 읺았거나, 당첨번호가 입력되지 않았습니다");
+        throw new RuntimeException("당첨티켓이 집계되지 읺았거나, 당첨번호가 입력되지 않았습니다");
     }
 }
