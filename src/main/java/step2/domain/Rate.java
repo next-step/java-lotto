2598a45767cedc2b38f043078761109b32529d5f
KimJuHyun <rlawngus0910@naver.com>
2023-04-24 23:49:25 +0900

    refactor: 상금 계산 로직을 enum 클래스로 이동

--- src/main/java/step2/domain/Rate.java
+++ src/main/java/step2/domain/Rate.java
@@ -17,20 +17,7 @@ public class Rate {
     }
 
     private static int calculateMoney(int match, int count) {
-        if (match == 3) {
-            return count * 5000;
-        }
-        if (match == 4) {
-            return count * 50000;
-        }
-        if (match == 5) {
-            return count * 1500000;
-        }
-        if (match == 6) {
-            return count * 2000000000;
-        }
-
-        return 0;
+        return count * WinningMoney.winningMoney(match);
     }
 
     private static float roundOffRate(float rate) {
