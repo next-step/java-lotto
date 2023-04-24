79ad657f4b3c7767a8891394b9b1b7fc0081e6cb
hoa0217 <gjwjdghk123@naver.com>
2023-04-25 00:04:47 +0900

    feature: "숫자가 와야할 순서에 숫자가 아닌 문자를 만나면 IllegalArgumentException을 던진다."

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -2,6 +2,8 @@ package stringcalculator;
 
 public class Calculator {
 
+  private static final String NUMBER_REGEX = "-?\\d+";
+
   private Input input;
 
   public Calculator(String input) {
@@ -10,5 +12,24 @@ public class Calculator {
 
   private Calculator(Input input) {
     this.input = input;
+  }
+
+  public int calculate() {
+    String[] inputArr = input.inputSplit();
+
+    int number = getNumber(inputArr[0]);
+    for (int i = 1; i < inputArr.length; i = i + 2) {
+      String operator = inputArr[i];
+      int nextNumber = getNumber(inputArr[i + 1]);
+    }
+
+    return 0;
+  }
+
+  private int getNumber(String input) {
+    if (!input.matches(NUMBER_REGEX)) {
+      throw new IllegalArgumentException("유효하지 않은 입력입니다.");
+    }
+    return Integer.parseInt(input);
   }
 }
