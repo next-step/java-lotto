77f251b271cf505e6841becdae62d93365d722d4
hoa0217 <gjwjdghk123@naver.com>
2023-04-24 23:34:31 +0900

    feature: "입력값을 split(" ") 메서드를 이용해 문자열을 분리한다."

---
+++ src/main/java/stringcalculator/StringSpliter.java
@@ -0,0 +1,10 @@
+package stringcalculator;
+
+public class StringSpliter {
+
+  public static final String DEFAULT_DELIMITER = " ";
+
+  public static String[] split(String input) {
+    return input.split(DEFAULT_DELIMITER);
+  }
+}
