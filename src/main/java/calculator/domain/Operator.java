63f10c9bc6d1a67812ed7d43e1d53d72dc074a52
eohyemin <eohyemin@172.20.nate.com>
2023-04-25 07:55:34 +0900

    feat: 나눗셈 연산 시 검증 추가

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -8,7 +8,11 @@ public enum Operator {
     ADD("+", (num1, num2) -> num1 + num2),
     SUB("-", (num1, num2) -> num1 - num2),
     MULTI("*", (num1, num2) -> num1 * num2),
-    DIVIDE("/", (num1, num2) -> num1 / num2);
+    DIVIDE("/", (num1, num2) -> {
+        validateDivision(num1, num2);
+        return num1 / num2;
+    });
+
 
     private static final Map<String, Operator> OPERATORS = new HashMap<>();
 
@@ -35,5 +39,15 @@ public enum Operator {
 
     public int operate(int num1, int num2) {
         return expression.applyAsInt(num1, num2);
+    }
+
+    private static void validateDivision(int num1, int num2) {
+        if(num2 == 0) {
+            throw new IllegalArgumentException("The divisor cannot be zero");
+        }
+
+        if(num1 % num2 != 0) {
+            throw new IllegalArgumentException("The first number must be divisible by the second number");
+        }
     }
 }
--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -2,10 +2,12 @@ package calculator;
 
 import calculator.domain.Operator;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class OperatorTest {
     @DisplayName("덧셈 연산 테스트")
@@ -42,5 +44,21 @@ public class OperatorTest {
         Operator operator = Operator.from("/");
         int result = operator.operate(x, y);
         assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("나눗셈 연산 시 0으로 나눌 경우 예외 발생")
+    @Test
+    void divideZero() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Operator.from("/").operate(1, 0))
+                .withMessageContaining("The divisor cannot be zero");
+    }
+
+    @DisplayName("나눗셈 연산 시 나누어 떨어지지 않는 경우 예외 발생")
+    @Test
+    void dividable() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Operator.from("/").operate(3, 2))
+                .withMessageContaining("The first number must be divisible by the second number");
     }
 }
