ff5a11f1874bd2866044873915ff924018daedd1
player-geun <player_geun@naver.com>
2023-04-24 19:13:40 +0900

    feat : 입력 유틸 메서드

---
+++ src/main/java/lotto/util/StringSplitter.java
@@ -0,0 +1,16 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringSplitter {
+
+    private StringSplitter() {
+    }
+
+    private static final String DELIMITER = ",";
+
+    public static List<String> split(String input) {
+        return Arrays.asList(input.split(DELIMITER));
+    }
+}
