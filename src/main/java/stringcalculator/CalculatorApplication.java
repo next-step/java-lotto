adcd1168111b8eddc17da2ebaf5e7739e603fed6
hoa0217 <gjwjdghk123@naver.com>
2023-04-24 23:00:17 +0900

    feature: "사용자에게 문자열을 입력받는다."

---
+++ src/main/java/stringcalculator/CalculatorApplication.java
@@ -0,0 +1,11 @@
+package stringcalculator;
+
+import java.util.Scanner;
+
+public class CalculatorApplication {
+
+  public static final Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    String input = sc.nextLine();
+  }
+}
