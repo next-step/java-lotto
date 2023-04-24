d3fb8685732d6d929be130db54e00ba95fdedae7
player-geun <player_geun@naver.com>
2023-04-24 19:19:14 +0900

    refactor : 에러 메세지 이름 변경

--- src/main/java/stringcalculator/util/StringParser.java
+++ src/main/java/stringcalculator/util/StringParser.java
@@ -9,7 +9,7 @@ public class StringParser {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("입력한 값이 정수가 아닙니다.");
+            throw new IllegalArgumentException("입력한 값이 자연수가 아닙니다.");
         }
     }
 }
