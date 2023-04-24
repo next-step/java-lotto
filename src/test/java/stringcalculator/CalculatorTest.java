08b5107d7f34a2dcf73196446892dab23e8b0070
hoa0217 <gjwjdghk123@naver.com>
2023-04-25 00:04:47 +0900

    feature: "숫자가 와야할 순서에 숫자가 아닌 문자를 만나면 IllegalArgumentException을 던진다."

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
 
@@ -13,6 +15,15 @@ public class CalculatorTest {
     assertThatThrownBy(() -> new Calculator(null)).isInstanceOf(IllegalArgumentException.class);
     assertThatThrownBy(() -> new Calculator(" ")).isInstanceOf(IllegalArgumentException.class);
     assertThatThrownBy(() -> new Calculator("")).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("숫자가 와야할 순서에 숫자가 아닌 문자를 만나면 IllegalArgumentException을 던진다.")
+  @ParameterizedTest
+  @ValueSource(strings = {"1 + +", "+ 1 +", "+ + 1"})
+  public void calculate_throwException_outOfOrderNumber(String input) {
+    Calculator calculator = new Calculator(input);
+
+    assertThatThrownBy(() -> calculator.calculate()).isInstanceOf(IllegalArgumentException.class);
   }
 
 }
