dc73635dc5cbeae1f2cb4db221fab8a45d70ef5f
0giri <higiri@naver.com>
2023-04-26 02:59:41 +0900

    feat: 피연산자 구현

---
+++ src/main/java/calculator/domain/Operand.java
@@ -0,0 +1,14 @@
+package calculator.domain;
+
+/**
+ * @author : 0giri
+ * @since : 2023/04/26
+ */
+public class Operand implements ExpressionElement{
+
+    private final int value;
+
+    public Operand(String value) {
+        this.value = Integer.parseInt(value);
+    }
+}
