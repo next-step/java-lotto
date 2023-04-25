e2bf0707206ad8ac27aba94d5800d1ec303f6d3e
In-wook Hwang <inwook9@gmail.com>
2023-04-26 00:57:52 +0900

    feat: 당첨번호 중복, 음수 검증로직 추가(피드백반영)

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.toUnmodifiableList;
 
@@ -18,9 +20,34 @@ public class LottoWinningNumber {
     }
 
     private void validateNumber(List<Integer> lottoNumber) {
+        validateNumberCount(lottoNumber);
+        validateDuplicatedNumbers(lottoNumber);
+        validateNegativeOrZero(lottoNumber);
+    }
+
+    private static void validateNumberCount(List<Integer> lottoNumber) {
         if (lottoNumber.size() != 6) {
             throw new IllegalArgumentException("로또 숫자입력이 올바르지 않습니다.(유효한 개수: 6개)");
         }
+    }
+
+    private static void validateDuplicatedNumbers(List<Integer> lottoNumber) {
+        if (lottoNumber.size() != new HashSet<>(lottoNumber).size()) {
+            throw new IllegalArgumentException("로또에 중복된 숫자가 포함되어있습니다.");
+        }
+    }
+
+    private void validateNegativeOrZero(List<Integer> lottoNumber) {
+        lottoNumber.stream()
+                .filter(isNegativeOrZero())
+                .findAny()
+                .ifPresent(a -> {
+                    throw new IllegalArgumentException("로또에 음수나 0을 입력하실 수 없습니다.");
+                });
+    }
+
+    private Predicate<Integer> isNegativeOrZero() {
+        return n -> Integer.signum(n) < 1;
     }
 
     public static LottoWinningNumber create(String winningNumberAsString) {
