be181c00fde9c01dcc964c980a22bc60f0fd07e7
foameraserblue <foameraserblue@gmail.com>
2023-04-26 00:14:50 +0900

    feature : 수동구입 복권 갯수와 번호를 받는 기능 작성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -8,12 +8,16 @@ import lotto.domain.winning.WinningBall;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
+
 public class LottoGame {
     public static void main(String[] args) {
         final LottoMachine lottoMachine = new LottoMachine();
 
         // 단순히 얼마인지를 가지고있는건 로또게임 당사자의 책임이라 생각해서 로또기계 안으로 생성로직을 넣지 않았습니다.
         final Amount amount = new Amount(InputView.purchaseAmount());
+        final List<String> manualNumbers = InputView.manualLotto();
+
         final Lottos lottos = lottoMachine.makeLottos(amount, new AutomaticStrategy());
 
         OutputView.printLottoCount(lottoMachine.calculateUnitCount(amount));
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -10,7 +12,27 @@ public class InputView {
 
     public static int purchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        final int amount = scanner.nextInt();
+
+        System.out.println();
+        return amount;
+    }
+
+    public static List<String> manualLotto() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        final int manualCount = scanner.nextInt();
+        System.out.println();
+
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        scanner.nextLine();
+
+        final List<String> manualNumbers = new ArrayList<>();
+        for (int i = 0; i < manualCount; i++) {
+            manualNumbers.add(scanner.nextLine());
+        }
+        System.out.println();
+
+        return manualNumbers;
     }
 
     public static String winningNumbers() {
