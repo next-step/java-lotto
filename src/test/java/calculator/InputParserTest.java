0e3cf26cf2c1f460bc760248a13659fbd946ab26
munjeongho <96mjh00@gmail.com>
2023-04-25 13:55:38 +0900

    refactor : 문자열 계산기 테스트케이스명 수정

--- src/test/java/calculator/InputParserTest.java
+++ src/test/java/calculator/InputParserTest.java
@@ -2,13 +2,11 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import calculator.service.InputParser;
 
@@ -23,20 +21,19 @@ public class InputParserTest {
 
 	@DisplayName("올바른 형식이 아닌 숫자")
 	@Test
-	void nullCarNameInput() {
+	void inputIllegalInputError_number() {
 		String input = "숫 + 2";
 		Class expect = IllegalArgumentException.class;
 		String expectedMsg = "올바르지 않은 숫자 입력:";
 
-		assertThatThrownBy(() -> 	inputParser.parseTokens(input))
+		assertThatThrownBy(() -> inputParser.parseTokens(input))
 			.isInstanceOf(expect)
 			.hasMessageContaining(expectedMsg);
 	}
 
-
 	@DisplayName("올바른 형식이 아닌 기호")
 	@Test
-	public void inputIllegalInputError() {
+	public void inputIllegalInputError_symbol() {
 		String input = "1 ++ 2";
 		Class expect = IllegalArgumentException.class;
 		String expectedMsg = "올바르지 않은 기호 입력:";
