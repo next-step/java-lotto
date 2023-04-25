5151c9c9e66e8fa5edf914a09e501a95d3d0c46c
juno-junho <ssmm0205@naver.com>
2023-04-26 01:19:26 +0900

    refactor(calculator): 클래스 분리를 통한 refactoring

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,116 +1,38 @@
 package calculator;
 
-import calculator.operations.Addition;
-import calculator.operations.Division;
-import calculator.operations.Multiplication;
-import calculator.operations.Subtraction;
-
-import java.util.List;
+import static calculator.Operations.*;
 
 public class Calculator {
     private final Expression expression;
-    private int result;
+    private Result result;
 
     public Calculator(String expression) {
         this.expression = new Expression(expression);
     }
 
     public int calculate() {
-        List<String> elements = expression.parseToList();
-        initializeResult(elements);
+        initializeResult();
 
-        for (String element : elements) {
-            checkStatusAndCalculate(element);
-            checkOperationStatus(element);
+        for (int index = 1; index < expression.size(); index += 2) {
+            checkOperation(PLUS, index);
+            checkOperation(MINUS, index);
+            checkOperation(MULTIPLY, index);
+            checkOperation(DIVIDE, index);
         }
-        return result;
+        return result.get();
     }
 
-    private void initializeResult(List<String> elements) {
-        result = Integer.parseInt(elements.remove(0));
+    private void initializeResult() {
+        result = new Result(Integer.parseInt(expression.firstValue()));
     }
 
-    private void checkStatusAndCalculate(String element) {
-        addIfStatusIsOn(element);
-        subtractIfStatusIsOn(element);
-        multiplyIfStatusIsOn(element);
-        divideIfStatusIsOn(element);
-    }
-
-    private void checkOperationStatus(String element) {
-        checkIfAddition(element);
-        checkIfSubtraction(element);
-        checkIfMultiplication(element);
-        checkIfDivision(element);
-    }
-
-    private void addIfStatusIsOn(String element) {
-        if (Addition.status) {
-            result += Integer.parseInt(element);
-            Addition.status = false;
+    private void checkOperation(Operations operation, int index) {
+        if (operation.equals(expression.get(index))) {
+            result.apply(operation, numOfNextIndex(index));
         }
     }
 
-    private void subtractIfStatusIsOn(String element) {
-        if (Subtraction.status) {
-            result -= Integer.parseInt(element);
-            Subtraction.status = false;
-        }
-    }
-
-    private void multiplyIfStatusIsOn(String element) {
-        if (Multiplication.status) {
-            result *= Integer.parseInt(element);
-            Multiplication.status = false;
-        }
-    }
-
-    private void divideIfStatusIsOn(String element) {
-        if (Division.status) {
-            validateIfResultIsInt(element);
-            Division.status = false;
-        }
-    }
-
-    private void validateIfResultIsInt(String element) {
-        int num = Integer.parseInt(element);
-        validateIfElementIsZero(num);
-        double floatNum = (double) result / num;
-        result /= num;
-        if (floatNum != result) {
-            Division.status = false;
-            throw new ArithmeticException("나누어 떨어지지 않는 값을 입력하셨습니다.");
-        }
-    }
-
-    private void validateIfElementIsZero(int num) {
-        if (num == 0) {
-            Division.status = false;
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
-        }
-    }
-
-    private void checkIfAddition(String element) {
-        if (element.equals(Addition.symbol)) {
-            Addition.status = true;
-        }
-    }
-
-    private void checkIfSubtraction(String element) {
-        if (element.equals(Subtraction.symbol)) {
-            Subtraction.status = true;
-        }
-    }
-
-    private void checkIfMultiplication(String element) {
-        if (element.equals(Multiplication.symbol)) {
-            Multiplication.status = true;
-        }
-    }
-
-    private void checkIfDivision(String element) {
-        if (element.equals(Division.symbol)) {
-            Division.status = true;
-        }
+    private int numOfNextIndex(int index) {
+        return Integer.parseInt(expression.get(index + 1));
     }
 }
--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -1,38 +1,13 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Expression {
-
     public static final String CRITERIA = " ";
     public static final String REGEX = "^(-?\\d+|[+\\-*/])$";
-    private final String value;
+    private final String[] value;
 
     public Expression(String value) {
         validateExpression(value);
-        this.value = value;
-    }
-
-    public List<String> parseToList() {
-        List<String> elements = Arrays.stream(value.split(CRITERIA))
-                .collect(Collectors.toList());
-
-        validateElements(elements);
-        return elements;
-    }
-
-    private void validateElements(List<String> elements) {
-        for (String element : elements) {
-            validateEachElement(element);
-        }
-    }
-
-    private void validateEachElement(String element) {
-        if (!element.matches(REGEX)) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
-        }
+        this.value = parsedNumber(value);
     }
 
     private void validateExpression(String value) {
@@ -49,6 +24,36 @@ public class Expression {
     private void validateBlank(String expression) {
         if (expression.isBlank()) {
             throw new IllegalArgumentException("입력값이 공백입니다.");
+        }
+    }
+
+    public int size() {
+        return value.length;
+    }
+
+    public String firstValue() {
+        return value[0];
+    }
+
+    public String get(int index) {
+        return value[index];
+    }
+
+    public String[] parsedNumber(String value) {
+        String[] paredNumber = value.split(CRITERIA);
+        validateNumbers(paredNumber);
+        return paredNumber;
+    }
+
+    private void validateNumbers(String[] paredNumber) {
+        for (String element : paredNumber) {
+            validateEachElement(element);
+        }
+    }
+
+    private void validateEachElement(String element) {
+        if (!element.matches(REGEX)) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         }
     }
 }
