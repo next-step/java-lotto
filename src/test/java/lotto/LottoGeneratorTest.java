7ada6bd74d1b2261537a04849d8a9fceb7c31881
hoyeoon <hoyeon.backend@gmail.com>
2023-04-26 00:33:04 +0900

    test: 로또번호 생성 개수 테스트

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -20,5 +20,15 @@ public class LottoGeneratorTest {
         assertThat(lottoNumbers.stream().allMatch(v -> v >= 1 && v <= 45)).isTrue();
     }
 
+    @Test
+    void 로또번호_생성_개수_테스트() {
+        //given
+        Lotto lotto = LottoGenerator.generateLotto();
 
+        //when
+        List<Integer> lottoNumbers = lotto.numbers;
+
+        //then
+        assertThat(lottoNumbers.size()).isEqualTo(6);
+    }
 }
