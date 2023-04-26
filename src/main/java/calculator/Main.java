4471e1dc2e1f5253ba1bb53dc523d12956c720a4
0giri <higiri@naver.com>
2023-04-27 04:40:26 +0900

    feat: 콘솔로 계산식을 입력받아 계산 결과 출력까지 수행하는 Main 메소드 추가

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import calculator.domain.Calculator;
+import calculator.domain.Expression;
+import calculator.domain.Number;
+import calculator.domain.StringExpressionParser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+/**
+ * @author : 0giri
+ * @since : 2023/04/26
+ */
+public class Main {
+   public static void main(String[] args) {
+      String stringExpression = InputView.askExpression();
+      Expression expression = StringExpressionParser.parse(stringExpression);
+      Number result = Calculator.calculate(expression);
+      OutputView.printResult(result);
+   }
+}
