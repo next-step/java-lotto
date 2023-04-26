1bc4e887a31232ee13806ce85f778e2fa8ac7ed7
didrlgus <rlgusdid@naver.com>
2023-04-26 22:32:15 +0900

    feat: 2단계 - 로또, `당첨 통계 문자열을 출력한다, 구분선 문자열을 출력한다.` 기능 추가

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -1,11 +1,14 @@
 package lotto.ui;
 
+import java.util.List;
+
 import lotto.domain.Lotto;
 import lotto.domain.PurchasedLottos;
+import lotto.domain.PrizeSituation;
 
 public class ResultView {
 
-	public static void printPurchaseCount(int purchaseCount) {
+	public static void printPurchasedCount(int purchaseCount) {
 		System.out.printf("%d개를 구매했습니다.%n", purchaseCount);
 	}
 
@@ -13,5 +16,10 @@ public class ResultView {
 		for (Lotto lotto : purchasedLottos.getLottos()) {
 			System.out.println(lotto.getSelectedNumbers());
 		}
+	}
+
+	public static void printPrizeSituations(List<PrizeSituation> prizeSituations) {
+		System.out.println("당첨 통계");
+		System.out.println("---------");
 	}
 }
