e2f278a1d8b493a2aa165f3c42ee6d4afb5469d8
0giri <higiri@naver.com>
2023-04-27 03:24:17 +0900

    feat: 문자열 주어지면 Operand와 Operator로 이루어진 Expression을 반환하는 StringExpressionParser 구현

---
+++ src/main/java/calculator/domain/StringExpressionParser.java
@@ -0,0 +1,74 @@
+package calculator.domain;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author : 0giri
+ * @since : 2023/04/26
+ */
+public class StringExpressionParser {
+
+    public static Expression parse(String expression) {
+        if (isBlank(expression)) {
+            throw new IllegalArgumentException("null 또는 빈 문자열은 계산할 수 없습니다.");
+        }
+        return new Expression(convertExpression(expression));
+    }
+
+    private static boolean isBlank(String expression) {
+        return expression == null || expression.isBlank();
+    }
+
+    private static List<ExpressionElement> convertExpression(String expression) {
+        String[] elements = parseString(expression);
+        if (isEvenNumber(elements.length)) {
+            throw new IllegalArgumentException("연산 요소의 총 개수는 짝수일 수 없습니다.");
+        }
+        return convertElements(elements);
+    }
+
+    private static String[] parseString(String expression) {
+        return expression.split(" ");
+    }
+
+    private static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+    private static List<ExpressionElement> convertElements(String[] elements) {
+        List<ExpressionElement> expressionElements = new LinkedList<>();
+        for (int i = 0; i < elements.length; i++) {
+            String element = elements[i];
+            convertByIndex(expressionElements, element, i);
+        }
+        return expressionElements;
+    }
+
+    private static void convertByIndex(List<ExpressionElement> expressionElements, String element, int index) {
+        if (isEvenNumber(index)) {
+            Operand operand = convertToOperand(element);
+            addOperand(expressionElements, operand);
+            return;
+        }
+        Operator operator = convertToOperator(element);
+        addOperator(expressionElements, operator);
+    }
+
+    private static Operand convertToOperand(String element) {
+        return new Operand(element);
+    }
+
+    private static void addOperand(List<ExpressionElement> expressionElements, Operand operand) {
+        expressionElements.add(operand);
+    }
+
+    private static Operator convertToOperator(String element) {
+        return Operator.find(element);
+    }
+
+    private static void addOperator(List<ExpressionElement> expressionElements, Operator operator) {
+        expressionElements.add(operator);
+    }
+}
---
+++ src/test/java/calculator/domain/StringExpressionParserTest.java
@@ -0,0 +1,59 @@
+package calculator.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+/**
+ * @author : 0giri
+ * @since : 2023/04/25
+ */
+public class StringExpressionParserTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3 + 3 - 5", "3 / 3", "5 * 5 - 1 + 3"})
+    void name(String value) {
+        Expression expression = StringExpressionParser.parse(value);
+        for (int i = 0; i < expression.size(); i++) {
+            if (i % 2 == 0) {
+                ExpressionElement oddElement = expression.get(i);
+                assertThat(oddElement).isInstanceOf(Operand.class);
+                continue;
+            }
+            ExpressionElement evenElement = expression.get(i);
+            assertThat(evenElement).isInstanceOf(Operator.class);
+        }
+    }
+
+    @Test
+    void null_입력시_IllegalArgumentException() {
+        assertThatIllegalArgumentException().isThrownBy(() -> StringExpressionParser.parse(null));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    void blank_입력시_IllegalArgumentException(String value) {
+        assertThatIllegalArgumentException().isThrownBy(() -> StringExpressionParser.parse(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3 +", "3 + 3 +"})
+    void 연산_요소의_총_개수가_짝수일시_IllegalArgumentException(String value) {
+        assertThatIllegalArgumentException().isThrownBy((() -> StringExpressionParser.parse(value)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3 3", "3 + +"})
+    void 숫자_또는_연산자_연속시_IllegalArgumentException(String value) {
+        assertThatIllegalArgumentException().isThrownBy((() -> StringExpressionParser.parse(value)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"+ 3", "+ 3 +"})
+    void 연산자가_먼저인_경우_IllegalArgumentException(String value) {
+        assertThatIllegalArgumentException().isThrownBy((() -> StringExpressionParser.parse(value)));
+    }
+}
