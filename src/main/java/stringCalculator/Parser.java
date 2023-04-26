24ff9c342779e323bb9a9d5375f9ebf03984b4c7
SeokyungKim <kskyung0624@gmail.com>
2023-04-27 00:13:51 +0900

    feat : Parse 구현

---
+++ src/main/java/stringCalculator/Parser.java
@@ -0,0 +1,49 @@
+package stringCalculator;
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Parser {
+
+  private Parser() {}
+
+  private static class LazyHolder {
+    private static final Parser INSTANCE = new Parser();
+  }
+
+  public static Parser getInstance() {
+    return LazyHolder.INSTANCE;
+  }
+
+  public Expression parse(String input) {
+    List<String> elements = Arrays.asList(input.split(" "));
+    Queue<Operator> operators = new LinkedList<>();
+    Queue<Number> numbers = new LinkedList<>();
+
+    if (isEven(elements.size())) {
+      throw new IllegalArgumentException("연산자는 항상 피연산자 사이에 들어가야 합니다");
+    }
+
+    for (int i = 0; i < elements.size(); i++) {
+      put(operators, numbers, elements.get(i), i);
+    }
+
+    return new Expression(operators, numbers);
+  }
+
+  private void put(Queue<Operator> operators, Queue<Number> numbers, String element, int index) {
+    if (isEven(index)) {
+      numbers.add(new Number(element));
+      return;
+    }
+
+    operators.add(Operator.from(element));
+  }
+
+  private boolean isEven(int target) {
+    return target % 2 == 0;
+  }
+}
