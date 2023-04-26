ae67163a92610be062525d5c4e4ba441f31ef354
jeongAin123 <lovepizza132@gmail.com>
2023-04-26 21:19:39 +0900

    test : calculate 테스트 아직 미완

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,29 +1,37 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Calculator {
     private final List<String> formulaList;
-    private int result;
+    private Double result;
 
     public Calculator(List<String> formulaList) {
         this.formulaList = new ArrayList<>(formulaList);
-        this.result = 0;
+        this.result = 0.0;
     }
 
     public void calculateFormula() {
-        List<Integer> numberList = makeNumberList();
+        List<Double> numberList = makeNumberList();
         List<String> operatorList = makeOperatorList();
+        Queue<String> operatorQueue = new LinkedList<>(operatorList);
+
+        this.result = numberList.stream()
+                .reduce((x, y) -> calculate(x, y, operatorQueue.remove()))
+                .get();
+
 
     }
 
-    public List<Integer> makeNumberList() {
+    public List<Double> makeNumberList() {
         return IntStream.range(0, this.formulaList.size())
                 .filter(index -> index % 2 == 0)
-                .mapToObj(index -> Integer.parseInt(this.formulaList.get(index)))
+                .mapToObj(index -> Double.parseDouble(this.formulaList.get(index)))
                 .collect(Collectors.toList());
     }
 
@@ -34,7 +42,33 @@ public class Calculator {
                 .collect(Collectors.toList());
     }
 
-    public int showResult() {
+    public Double calculate(Double number1, Double number2, String operator) {
+        System.out.println("--------------------------");
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(operator);
+
+        if(operator == "+") {
+            System.out.println(number1 + number2);
+            return number1 + number2;
+        }
+
+        if(operator == "-") {
+            System.out.println(number1 - number2);
+            return number1 - number2;
+        }
+
+        if(operator == "*") {
+            System.out.println(number1 * number2);
+            return number1 * number2;
+        }
+        System.out.println(number1 / number2);
+        return number1 / number2;
+
+
+    }
+
+    public Double showResult() {
         return result;
     }
 }
--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -5,7 +5,9 @@ import lotto.utility.InputChecker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,7 +20,7 @@ public class CalculatorTest {
         List<String> formulaList = inputChecker.formulaToStrList(testInput);
         Calculator calculator = new Calculator(formulaList);
 
-        List<Integer> result = calculator.makeNumberList();
+        List<Double> result = calculator.makeNumberList();
 
         assertThat(result.size()).isEqualTo(4);
         assertThat(result.get(0)).isEqualTo(44);
@@ -41,6 +43,57 @@ public class CalculatorTest {
         assertThat(result.get(0)).isEqualTo("+");
         assertThat(result.get(1)).isEqualTo("-");
         assertThat(result.get(2)).isEqualTo("/");
+    }
+
+    @Test
+    @DisplayName("수식 문자열에서 연산자만 List<String> 형태로 반환")
+    public void operatorQueueTest() {
+        String testInput = " 44 + 55 - 2 / 5 ";
+        InputChecker inputChecker = new InputChecker();
+        List<String> formulaList = inputChecker.formulaToStrList(testInput);
+        Calculator calculator = new Calculator(formulaList);
+
+        List<String> operatorList = calculator.makeOperatorList();
+        Queue<String> result = new LinkedList<>(operatorList);
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.remove()).isEqualTo("+");
+        assertThat(result.remove()).isEqualTo("-");
+        assertThat(result.remove()).isEqualTo("/");
+    }
+
+    @Test
+    @DisplayName("계산")
+    public void calculateTest() {
+        String testInput = " 44 + 55 - 9 / 5 ";
+        InputChecker inputChecker = new InputChecker();
+        List<String> formulaList = inputChecker.formulaToStrList(testInput);
+        Calculator calculator = new Calculator(formulaList);
+
+        Double result = calculator.calculate(4.0, 5.0, "+");
+        Double result2 = calculator.calculate(4.0, 5.0, "-");
+        Double result3 = calculator.calculate(4.0, 5.0, "*");
+        Double result4 = calculator.calculate(3.0, 2.0, "/");
+
+        assertThat(result).isEqualTo(9);
+        assertThat(result2).isEqualTo(-1);
+        assertThat(result3).isEqualTo(20);
+        assertThat(result4).isEqualTo(1.5);
+    }
+
+    @Test
+    @DisplayName("수식 문자열 계산")
+    public void calculateFormulaTest() {
+        String testInput = " 44 + 55 - 9 / 5 ";
+        InputChecker inputChecker = new InputChecker();
+        List<String> formulaList = inputChecker.formulaToStrList(testInput);
+        Calculator calculator = new Calculator(formulaList);
+
+        calculator.calculateFormula();
+
+        Double result = calculator.showResult();
+
+        assertThat(result).isEqualTo(18);
     }
 
 }
