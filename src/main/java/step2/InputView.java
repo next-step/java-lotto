ac141ca15b3207e8d0afc25357cff3974c4fc3ae
seongjo24 <kangsj24@naver.com>
2023-04-26 12:10:18 +0900

    로또금액 검증 추가

--- src/main/java/step2/InputView.java
+++ src/main/java/step2/InputView.java
@@ -4,6 +4,13 @@ public class InputView {
     private static final int LOTTO_PRICE = 1000;
 
     public int getLottoCnt(int amount) {
+        amountValidate(amount);
         return amount / LOTTO_PRICE;
+    }
+
+    public void amountValidate(int amount) {
+        if (amount % LOTTO_PRICE > 0) {
+            throw new IllegalArgumentException("금액은 천원 단위로 입력해주세요");
+        }
     }
 }
