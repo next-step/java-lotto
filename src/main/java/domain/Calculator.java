de744ee901032111f369b12ef69a066530cdc193
soochan <tncksdl05@gmail.com>
2023-04-27 01:23:57 +0900

    refactor : enum 형태로 변경

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -4,37 +4,19 @@ public class Calculator {
 
   private final Numbers numbers;
   private final Operators operators;
-  private final Operations operations;
 
 
   public Calculator(Numbers numbers, Operators operators) {
     this.numbers = numbers;
     this.operators = operators;
-    this.operations = new Operations();
   }
 
 
   public int calculate() {
-    int result = number();
-    while (!isEmpty()) {
-      result = calculation(result, number(), operator());
+    int result = 0;
+    while (!operators.isEmpty()) {
+      result = Operations.calculate(numbers.nextValue(), numbers.nextValue(), operators.nextOperator());
     }
     return result;
-  }
-
-  private boolean isEmpty() {
-    return numbers.isEmpty();
-  }
-
-  private Integer number() {
-    return numbers.poll();
-  }
-
-  private String operator() {
-    return operators.poll();
-  }
-
-  private int calculation(int result, Integer number, String operator) {
-    return operations.calculate(result, number, operator);
   }
 }
--- src/main/java/domain/Operations.java
+++ src/main/java/domain/Operations.java
@@ -1,31 +1,35 @@
 package domain;
 
-import java.util.Map;
 import java.util.function.BiFunction;
 
-public class Operations {
+public enum Operations {
+  ADD("+", (a, b) -> a + b),
+  SUBTRACT("-", (a, b) -> a - b),
+  MULTIPLY("*", (a, b) -> a * b),
+  DIVIDE("/", (a, b) -> a / b);
 
-  private final Map<String, BiFunction<Integer, Integer, Integer>> operations;
+  private final String symbol;
+  private final BiFunction<Integer, Integer, Integer> function;
 
-  public Operations() {
-    this.operations = initOperations();
+  Operations(String symbol, BiFunction<Integer, Integer, Integer> function) {
+    this.symbol = symbol;
+    this.function = function;
   }
 
-  private Map<String, BiFunction<Integer, Integer, Integer>> initOperations() {
-    return Map.of(
-        "+", (a, b) -> a + b,
-        "-", (a, b) -> a - b,
-        "*", (a, b) -> a * b,
-        "/", (a, b) -> a / b
-    );
-  }
-
-  public int calculate(int result, Integer number, String operator) {
-    BiFunction<Integer, Integer, Integer> operation = operations.get(
-        operator);
-    if (operation == null) {
-      throw new IllegalArgumentException("지원하지 않는 연산자가 있습니다.");
+  public static Operations of(String symbol) {
+    for (Operations operations : values()) {
+      if (operations.symbol.equals(symbol)) {
+        return operations;
+      }
     }
-    return operation.apply(result, number);
+    throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
   }
+
+
+  public static int calculate(int number1, int number2, String operator) {
+    Operations operation = Operations.of(operator);
+    return operation.function.apply(number1, number2);
+  }
+
+
 }
