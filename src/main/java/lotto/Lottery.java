57ce783b1a650394ba90da7fb4773355b386a16b
juno-junho <ssmm0205@naver.com>
2023-04-26 23:28:29 +0900

    feat(calculator): 입력 금액 예외 처리

--- src/main/java/lotto/Lottery.java
+++ src/main/java/lotto/Lottery.java
@@ -6,8 +6,29 @@ public class Lottery {
     private final int count;
 
     public Lottery(int money) {
-        this.count = money / PRICE;
+        validateLotteryCount(money);
+        this.count = money / PRICE;;
     }
+
+    private void validateLotteryCount(int money) {
+        checkDivision(money);
+        checkIfUnderPrice(money);
+    }
+
+    private void checkDivision(int money) {
+        double dividedDouble = (double) money / PRICE;
+        int divideToInt = money / PRICE;
+        if (divideToInt !=dividedDouble) {
+            throw new IllegalArgumentException("로또 가격 단위의 구입금액을 입력해 주세요");
+        }
+    }
+
+    private void checkIfUnderPrice(int money) {
+        if (money < PRICE) {
+            throw new IllegalArgumentException("로또 가격 보다 같거나 큰 값을 입력해 주세요.");
+        }
+    }
+
     public int getCount() {
         return count;
     }
--- src/test/java/lotto/LotteryTest.java
+++ src/test/java/lotto/LotteryTest.java
@@ -1,9 +1,11 @@
 package lotto;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LotteryTest {
 
@@ -19,5 +21,27 @@ public class LotteryTest {
         // then
         int lotteryCount = lotto.getCount();
         assertThat(lotteryCount).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("입력 금액이 로또 금액에 떨어지지 않으면 예외를 발생한다.")
+    void when_DoesNotDividedByLotteryPrice_ThrowsException() {
+        // given
+        int inputMoney = 11111;
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lottery(inputMoney));
+    }
+
+    @Test
+    @DisplayName("입력 금액이 로또 금액보다 작으면 예외를 발생시킨다.")
+    void when_InputIsLessThanLotteryPrice_ThrowsException() {
+        // given
+        int inputMoney = 0;
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lottery(inputMoney));
     }
 }
