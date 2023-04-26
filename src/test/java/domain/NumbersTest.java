8f9f0ee03e949900ab1c0b91171d6b439ed0618f
soochan <tncksdl05@gmail.com>
2023-04-27 01:41:45 +0900

    refactor : test code 네이밍 수정

--- src/test/java/domain/NumbersTest.java
+++ src/test/java/domain/NumbersTest.java
@@ -11,16 +11,16 @@ class NumbersTest {
     Numbers numbers = new Numbers("1","2","3");
 
     assertThat(numbers).isNotNull();
-    assertThat(1).isEqualTo(numbers.poll());
-    assertThat(2).isEqualTo(numbers.poll());
-    assertThat(3).isEqualTo(numbers.poll());
+    assertThat(1).isEqualTo(numbers.nextValue());
+    assertThat(2).isEqualTo(numbers.nextValue());
+    assertThat(3).isEqualTo(numbers.nextValue());
   }
 
   @Test
   void 숫자가_아닌_문자가_들어오는_경우_생성자_실패_테스트() {
     assertThatThrownBy(() -> new Numbers("1", "2", "a"))
         .isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("숫자가 아닌 문자가 포함되어 있습니다.");
+        .hasMessageContaining("숫자가 아닌 값이 있습니다.");
   }
 
 }
--- src/test/java/domain/OperationsTest.java
+++ src/test/java/domain/OperationsTest.java
@@ -8,45 +8,36 @@ class OperationsTest {
 
   @Test
   void 더하기_테스트() {
-    Operations operations = new Operations();
-
-    int result = operations.calculate(1, 2, "+");
+    int result = Operations.calculate(1, 2, "+");
 
     assertThat(result).isEqualTo(3);
   }
 
   @Test
   void 빼기_테스트() {
-    Operations operations = new Operations();
-
-    int result = operations.calculate(5, 2, "-");
+    int result = Operations.calculate(5, 2, "-");
 
     assertThat(result).isEqualTo(3);
   }
 
   @Test
   void 곱하기_테스트() {
-    Operations operations = new Operations();
-
-    int result = operations.calculate(2, 3, "*");
+    int result = Operations.calculate(2, 3, "*");
 
     assertThat(result).isEqualTo(6);
   }
 
   @Test
   void 나누기_테스트() {
-    Operations operations = new Operations();
-
-    int result = operations.calculate(6, 2, "/");
+    int result = Operations.calculate(6, 2, "/");
 
     assertThat(result).isEqualTo(3);
   }
 
   @Test
   void 없는_연산자_테스트() {
-    Operations operations = new Operations();
     assertThatIllegalArgumentException()
-        .isThrownBy(() -> operations.calculate(1, 2, "%"))
+        .isThrownBy(() -> Operations.calculate(1, 2, "%"))
         .withMessageContaining("지원하지 않는 연산자입니다.");
   }
 }
