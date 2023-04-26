36000e1399712fd2750fd39814977487089b7cc8
hoyeoon <hoyeon.backend@gmail.com>
2023-04-27 01:50:27 +0900

    feat: 구입금액 1,000 단위 이외의 입력 예외처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final int LOTTO_PRICE = 1000;
     private final Scanner scanner = new Scanner(System.in);
 
     public Long inputPurchasePrice() {
         ResultView.printPurchasePriceInputCommand();
-        return Long.parseLong(nextLine());
+
+        long purchasePrice = Long.parseLong(nextLine());
+        if (isNotInThousands(purchasePrice)) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위여야 합니다.");
+        }
+        return purchasePrice;
+    }
+
+    private boolean isNotInThousands(long purchasePrice) {
+        return purchasePrice % LOTTO_PRICE != 0;
     }
 
     public String inputWinningLottoNumbers() {
         ResultView.printWinningLottoNumberInputCommand();
+
         return nextLine();
     }
 
