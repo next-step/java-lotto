e3368d9a0511aae9835d0ef64bad126a9fabbcf9
didrlgus <rlgusdid@naver.com>
2023-04-27 08:51:42 +0900

    test: 2단계 - 로또, 미흡한 TC 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,9 +20,9 @@ public class LottoMachineTest {
 	}
 
 	@DisplayName("구매한 만큼의 로또를 생성한다. - 예외 케이스")
-	@ValueSource(ints = {800, 1400})
+	@ValueSource(longs = {800, 1400})
 	@ParameterizedTest
-	void test2(int purchaseAmount) {
+	void test2(long purchaseAmount) {
 		assertThatIllegalArgumentException().isThrownBy(() -> new LottoMachine(purchaseAmount));
 	}
 
@@ -55,5 +56,52 @@ public class LottoMachineTest {
 	void test6() {
 		LottoMachine lottoMachine = new LottoMachine();
 		assertThat(lottoMachine.isBenefit(0.99)).isFalse();
+	}
+
+	@DisplayName("당첨 현황을 표현하는 현황판을 작성한다.")
+	@Test
+	void test7() {
+		List<Lotto> lottos = Arrays.asList(
+			new Lotto(0), new Lotto(0),
+			new Lotto(3), new Lotto(3)
+		);
+		LottoMachine lottoMachine = new LottoMachine();
+		Map<Score, Integer> scoreBoard = lottoMachine.makeScoreBoard(lottos);
+
+		assertThat(scoreBoard.get(new Score(0))).isNull();
+		assertThat(scoreBoard.get(new Score(2))).isNull();
+		assertThat(scoreBoard.get(new Score(3))).isEqualTo(2);
+		assertThat(scoreBoard.get(new Score(6))).isEqualTo(0);
+	}
+
+	@DisplayName("상금 현황 목록을 작성한다.")
+	@Test
+	void test8() {
+		List<Lotto> lottos = Arrays.asList(
+			new Lotto(0), new Lotto(0),
+			new Lotto(3), new Lotto(3),
+			new Lotto(6)
+		);
+		LottoMachine lottoMachine = new LottoMachine(lottos);
+		List<PrizeSituation> prizeSituations = lottoMachine.makePrizeSituations();
+
+		assertThat(prizeSituations).containsExactlyInAnyOrder(
+			new PrizeSituation(PrizeType.THREE, 2), new PrizeSituation(PrizeType.FOUR, 0),
+			new PrizeSituation(PrizeType.FIVE, 0), new PrizeSituation(PrizeType.SIX, 1));
+	}
+
+	@DisplayName("상금 현황 목록을 점수순으로 오름차순 정렬한다.")
+	@Test
+	void test9() {
+		List<PrizeSituation> prizeSituations = Arrays.asList(
+			new PrizeSituation(PrizeType.FOUR, 0), new PrizeSituation(PrizeType.SIX, 1),
+			new PrizeSituation(PrizeType.FIVE, 0), new PrizeSituation(PrizeType.THREE, 2)
+		);
+		LottoMachine lottoMachine = new LottoMachine();
+		List<PrizeSituation> sortedPrizeSituations = lottoMachine.sortInOrderScore(prizeSituations);
+		assertThat(sortedPrizeSituations).containsExactly(
+			new PrizeSituation(PrizeType.THREE, 2), new PrizeSituation(PrizeType.FOUR, 0),
+			new PrizeSituation(PrizeType.FIVE, 0), new PrizeSituation(PrizeType.SIX, 1)
+		);
 	}
 }
