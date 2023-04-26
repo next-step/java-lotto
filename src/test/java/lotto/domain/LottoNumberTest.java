c317daed2fa803b7c86e36bfa60cd705ace0199b
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-26 23:30:11 +0900

    refactor: 메소드 시그니처 변경

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -11,13 +11,17 @@ class LottoNumberTest {
     @Test
     @DisplayName("로또 번호 생성 테스트(객체 동등성 테스트)")
     void createLottoNumber(){
-        assertThat(LottoNumber.createLottoNumber(1)).isEqualTo(new LottoNumber(1));
+        assertThat(LottoNumber.provideLottoNumber(1)).isEqualTo(new LottoNumber(1));
     }
 
     @Test
     @DisplayName("로또 번호 자동 생성 테스트(번호 범위 1~45)")
     void autoCreateLottoNumber(){
-        assertThat(LottoNumber.createLottoNumber()).isEqualTo(new LottoNumber(1));
+
+        LottoNumber beginOfLottoNumber = new LottoNumber(1);
+        LottoNumber endOfLottoNumber = new LottoNumber(45);
+
+        assertThat(LottoNumber.provideLottoNumber())
     }
 
 
