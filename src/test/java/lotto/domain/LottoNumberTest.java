1e8bd67b5701c6f5641b14fc4068d78224d8207f
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-26 20:00:17 +0900

    feat: lottoNumber 생성 실패하는 테스트 (객체 동등성)

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -2,49 +2,16 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberTest {
 
     @Test
-    @DisplayName("로또 번호 생성 테스트(객체 동등성,동일성 테스트)")
-    void manualProvideLottoNumber() {
-        assertThat(LottoNumber.provideLottoNumber(1)).isEqualTo(new LottoNumber(1));
+    @DisplayName("로또 번호 획득 테스트")
+    void getLottNumber(){
+         
+        assertThat(LottoNumber.createLottoNumber(1)).isEqualTo(new LottoNumber(1));
     }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 3, 4, 5, 6, 7, 43})
-    @DisplayName("로또 번호 범위 테스트")
-    void autoCreateLottoNumber(int number) {
-
-        LottoNumber lottoNumber = LottoNumber.provideLottoNumber(number);
-
-        assertThat(LottoNumber.isCorrectRange(lottoNumber)).isTrue();
-    }
-
-    @Test
-    @DisplayName("로또 번호 생성 테스트(숫자 범위 1~45)")
-    void autoProvideLottoNumber() {
-        LottoNumber lottoNumber = LottoNumber.provideLottoNumber();
-
-
-        assertThat(LottoNumber.isCorrectRange(lottoNumber)).isTrue();
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {46, 47, 48})
-    @DisplayName("로또 번호 범위(1~45) 를 넘기면 예외를 던진다")
-    void overLottoNumber(int number) {
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> LottoNumber.provideLottoNumber(number))
-                .withMessage("로또 번호 범위가 아니에요 :(");
-
-    }
-
-
 }
