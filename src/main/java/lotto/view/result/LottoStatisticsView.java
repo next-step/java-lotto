36cae1a8c1550c723079de451ce756eb9001ee58
Baek Jiyeon <100.paperkite@gmail.com>
2023-04-26 19:08:11 +0900

    feat(output): 당첨 통계에 2등 관련 정보 추가

--- src/main/java/lotto/view/result/LottoStatisticsView.java
+++ src/main/java/lotto/view/result/LottoStatisticsView.java
@@ -3,32 +3,35 @@ package lotto.view.result;
 import lotto.model.Place;
 import lotto.model.dto.LottoStatisticsDto;
 import lotto.view.View;
+import lotto.view.result.statistic.PlaceView;
+import lotto.view.result.statistic.ProfitView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoStatisticsView implements View {
-    private static final String MATCH_FORMAT = "%d개 일치 (%d원)- %d개\n";
-    private static final String PROFIT_FORMAT = "총 수익률은 %.2f입니다.";
-
-    private final LottoStatisticsDto statistics;
+    private final List<PlaceView> places = new ArrayList<>();
+    private final View comment;
 
     public LottoStatisticsView(LottoStatisticsDto statistics) {
-        this.statistics = statistics;
+        for (Place place : Place.winners()) {
+            int count = statistics.count(place);
+            PlaceView view = new PlaceView(place, count);
+            places.add(view);
+        }
+
+        comment = new ProfitView(statistics.profit());
     }
 
     @Override
     public void render() {
         System.out.println("\n당첨 통계");
         System.out.println("---------");
-        for (Place place : Place.winners()) {
-            System.out.printf(MATCH_FORMAT, place.matches(), place.reward(), statistics.count(place));
+
+        for (View place : places) {
+            place.render();
         }
 
-        float profit = statistics.profit();
-        System.out.println(profitComment(profit));
-    }
-
-
-    private String profitComment(float profit) {
-        String additional = profit < 1 ? "(기준이 1이기 때문에 결과적으로 손해라는 의미임)" : "";
-        return String.format(PROFIT_FORMAT, profit) + additional;
+        comment.render();
     }
 }
