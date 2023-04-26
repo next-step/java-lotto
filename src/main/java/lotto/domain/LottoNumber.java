382b974dd4cce72ed10e99d35560ed4acb3e0490
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-26 23:48:18 +0900

    feat: lotto 번호 자동 생성 테스트

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,9 +1,6 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,16 +8,14 @@ import java.util.stream.IntStream;
 public class LottoNumber {
     private static final int BEGIN_OF_LOTTO_NUMBER = 1;
     private static final int END_OF_LOTTO_NUMBER = 45;
+    private static final int ZERO = 0;
     private static final Map<Integer, LottoNumber> lottoNumberRepository;
-    private static final List<Integer> repositoryKeySet;
     private final int lottoNumber;
 
     static {
         lottoNumberRepository = IntStream.rangeClosed(BEGIN_OF_LOTTO_NUMBER, END_OF_LOTTO_NUMBER)
                 .boxed()
                 .collect(Collectors.toMap(Function.identity(), LottoNumber::new));
-
-        repositoryKeySet = new ArrayList<>(lottoNumberRepository.keySet());
     }
 
     public LottoNumber(int lottoNumber) {
@@ -32,17 +27,21 @@ public class LottoNumber {
     }
 
     public static LottoNumber provideLottoNumber() {
-
-        // lottoNumberRepository
-        return null;
+        List<Integer> numberList = extractNumber();
+        Collections.shuffle(numberList);
+        return provideLottoNumber(numberList.get(ZERO));
     }
 
-    public boolean isCorrectRange(LottoNumber lottoNumber) {
+    private static List<Integer> extractNumber() {
+        return new ArrayList<>(lottoNumberRepository.keySet());
+    }
+
+    public static boolean isCorrectRange(LottoNumber lottoNumber) {
 
         LottoNumber beginOfLottoNumber = provideLottoNumber(BEGIN_OF_LOTTO_NUMBER);
 
         if (lottoNumber.hashCode() < beginOfLottoNumber.hashCode()) {
-            return false;
+            return Boolean.FALSE;
         }
 
         LottoNumber endOfLottoNumber = provideLottoNumber(END_OF_LOTTO_NUMBER);
