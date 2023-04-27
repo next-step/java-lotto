6f5ab135c6baec726fb7fb8899e9a622ba6727c7
ShinYeahChan <syeahc@gmail.com>
2023-04-27 20:06:58 +0900

    refactor : StringCalculator 내 private 메서드 위치 변경 및 private 처리

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class StringCalculator {
 
     private final List<Double> numbers = new ArrayList<>();
-    private final List<String> operatorList = new ArrayList<>();
+    private final List<String> operators = new ArrayList<>();
     private double result;
 
     public StringCalculator(String input) {
@@ -18,26 +18,26 @@ public class StringCalculator {
         }
     }
 
-    private void separateInput(String[] strings, int i) {
-        if(i%2==1){
-            operatorList.add(strings[i]);
-            return;
+    private void validateInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("입력 값이 없습니다.");
         }
-        numbers.add(Double.valueOf(strings[i]));
     }
 
     private String[] splitInput(String input) {
         return input.split(" ");
     }
 
-    private void validateInput(String input) {
-        if(input==null||input.isBlank()){
-            throw new IllegalArgumentException("입력 값이 없습니다.");
+    private void separateInput(String[] strings, int i) {
+        if (i % 2 == 1) {
+            operators.add(strings[i]);
+            return;
         }
+        numbers.add(Double.valueOf(strings[i]));
     }
 
     public String getOperator(int index){
-        return operatorList.get(index);
+        return operators.get(index);
     }
 
     public Double getNumber(int index){
@@ -45,13 +45,13 @@ public class StringCalculator {
     }
 
     public Double calculate() {
-        for (int i = 0; i < operatorList.size(); i++) {
-            calculateEach(operatorList.get(i),numbers.get(i+1));
+        for (int i = 0; i < operators.size(); i++) {
+            calculateEach(operators.get(i),numbers.get(i+1));
         }
         return result;
     }
 
-    public void calculateEach(String symbol, Double number) {
+    private void calculateEach(String symbol, Double number) {
         Operation operation = Operation.find(symbol);
         if(operation == null){
             throw new IllegalArgumentException();
