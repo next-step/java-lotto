bca2fd08f4358d7a894bcdf45685af79d1026cf9
player-geun <player_geun@naver.com>
2023-04-27 16:19:55 +0900

    refactor : 상수 대문자 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,22 +8,22 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
     }
 
     public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
     public static List<Integer> inputLastWinningNumber() {
-        scanner.nextLine();
+        SCANNER.nextLine();
         System.out.println("지난 주 당첨 번호를 압력해주세요.");
         return StringParser.parseIntList(
                 StringSplitter.split(
-                        scanner.nextLine().replace(" ", "")
+                        SCANNER.nextLine().replace(" ", "")
                 ));
     }
 }
