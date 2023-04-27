e82be9b5c4ff7c906bf4963650c910ab11bf10fd
dong <dong.22@kakaoenterprise.com>
2023-04-28 03:06:17 +0900

    refactor : 보너스 볼 일치여부를 저장하는 로직 추가

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,18 +1,20 @@
 package lotto.domain;
 
 public enum Prize {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    FIRST(6, 2_000_000_000,false),
+    SECOND(5, 30_000_000,true),
+    THIRD(5, 1_500_000,false),
+    FOURTH(4, 50_000,false),
+    FIFTH(3, 5_000,false);
 
     private final int matchCount;
     private final int prizeAmount;
+    private final boolean bonusNumberMatch;
 
-    Prize(int matchCount, int prizeAmount) {
+    Prize(int matchCount, int prizeAmount, boolean bonusNumberMatch) {
         this.matchCount = matchCount;
         this.prizeAmount = prizeAmount;
+        this.bonusNumberMatch = bonusNumberMatch;
     }
 
     public boolean isMatch(int matchCount) {
@@ -21,5 +23,9 @@ public enum Prize {
 
     public int calculatePrize(int count) {
         return this.prizeAmount * count;
+    }
+
+    public boolean needBonusNumberMatch() {
+        return bonusNumberMatch;
     }
 }
