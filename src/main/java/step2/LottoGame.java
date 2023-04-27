2333b97d95a01e8088da103a730b13e5ef1cab4b
cohi <tkfka156@gmail.com>
2023-04-27 14:32:59 +0900

    :sparkles: feat: controller, service 영역 구현

---
+++ src/main/java/step2/LottoGame.java
@@ -0,0 +1,52 @@
+package step2;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import step2.domain.LotteryWin;
+import step2.domain.Lotto;
+import step2.domain.LottoFactory;
+import step2.domain.WinningNumbers;
+import step2.service.LottoService;
+import step2.view.InputView;
+import step2.view.LottoView;
+import step2.view.ResultView;
+
+public class LottoGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputView inputView = new InputView();
+        LottoView lottoView = new LottoView();
+        ResultView resultView = new ResultView();
+
+        int money = getMoney(scanner, inputView);
+        inputView.confirmComment(money);
+
+        List<Lotto> lottoList = LottoFactory.of(money);
+
+        lottoView.printLotto(lottoList);
+
+        String winningNumbers = getWinningNumbers(scanner, resultView);
+
+        LottoService lottoService = new LottoService(
+            new LotteryWin(new WinningNumbers(winningNumbers))
+        );
+
+        Map<Integer, Integer> match = lottoService.match(lottoList);
+        resultView.printStatics(match);
+
+        int winningMoney = lottoService.getWinningMoney();
+        resultView.printProfit(money, winningMoney);
+    }
+
+    private static String getWinningNumbers(Scanner scanner, ResultView resultView) {
+        resultView.winningNumberComment();
+        return scanner.next();
+    }
+
+    private static int getMoney(Scanner scanner, InputView inputView) {
+        inputView.inputComment();
+        return scanner.nextInt();
+    }
+}
---
+++ src/main/java/step2/service/LottoService.java
@@ -0,0 +1,24 @@
+package step2.service;
+
+import java.util.List;
+import java.util.Map;
+import step2.domain.LotteryWin;
+import step2.domain.Lotto;
+
+public class LottoService {
+
+    private final LotteryWin lotteryWin;
+
+    public LottoService(LotteryWin lotteryWin) {
+        this.lotteryWin = lotteryWin;
+    }
+
+    public Map<Integer, Integer> match(List<Lotto> lottoList) {
+        lotteryWin.confirm(lottoList);
+        return lotteryWin.getResult();
+    }
+
+    public int getWinningMoney() {
+        return lotteryWin.getWinningAmount();
+    }
+}
