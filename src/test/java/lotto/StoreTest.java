38c4dbb6aeb69807897ba5b5a5f4c9fbbc647c7c
juno-junho <ssmm0205@naver.com>
2023-04-27 18:35:35 +0900

    test(lotto): 분리한 클래스에 맞게 테스트 이동

---
+++ src/test/java/lotto/StoreTest.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class StoreTest {
+
+    @Test
+    @DisplayName("입력받은 구매 금액을 로또 가격으로 나눈 금액 만큼 로또를 생성한다.")
+    void generate_lottery() {
+        // given
+        Price price = new Price(14000);
+
+        // when
+        Store store = new Store(price);
+
+        // then
+        int lotteryCount = store.getLotteryCount();
+        assertThat(lotteryCount).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("입력 금액이 로또 금액에 떨어지지 않으면 예외를 발생한다.")
+    void when_DoesNotDividedByLotteryPrice_ThrowsException() {
+        // given
+        Price price = new Price(11111);
+
+        // when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Store(price));
+    }
+}
