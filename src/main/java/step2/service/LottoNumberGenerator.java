6592a05439e241efb55acae3d3c8d1cfd63200e0
ghkdwlgns612 <ghkdwlgns612@naver.com>
2023-04-27 21:44:47 +0900

    refactor : 기본 로또 번호 발생하는 로직을 LottoNumbers로 이동

--- src/main/java/step2/service/LottoNumberGenerator.java
+++ src/main/java/step2/service/LottoNumberGenerator.java
@@ -1,25 +1,13 @@
 package step2.service;
 
-import step2.vo.LottoNumber;
 import step2.vo.LottoNumbers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LottoNumberGenerator {
-
-    private static final int LOTTO_START_NUMBER = 1;
-
-    private static final int LOTTO_END_NUMBER = 45;
 
     private final LottoNumbers lottoNumbers;
 
     public LottoNumberGenerator() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
-        for (int num = LOTTO_START_NUMBER; num <= LOTTO_END_NUMBER; num++) {
-            lottoNumbers.add(new LottoNumber(num));
-        }
-        this.lottoNumbers = new LottoNumbers(lottoNumbers);
+        this.lottoNumbers = new LottoNumbers();
     }
 
     public Lotto generateLottoNumbers() {
--- src/main/java/step2/vo/LottoNumbers.java
+++ src/main/java/step2/vo/LottoNumbers.java
@@ -8,9 +8,17 @@ import java.util.List;
 
 public class LottoNumbers {
 
+    private static final int LOTTO_START_NUMBER = 1;
+
+    private static final int LOTTO_END_NUMBER = 45;
+
     private final List<LottoNumber> lottoNumbers;
 
-    public LottoNumbers(List<LottoNumber> lottoNumbers) {
+    public LottoNumbers() {
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (int num = LOTTO_START_NUMBER; num <= LOTTO_END_NUMBER; num++) {
+            lottoNumbers.add(new LottoNumber(num));
+        }
         this.lottoNumbers = lottoNumbers;
     }
 
