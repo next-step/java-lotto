d460f868778bb23c8c8956ce751d63dee042b1bd
ain <lovepizza132@gmail.com>
2023-04-28 00:34:37 +0900

    refactor : InputChecker List 변환 및 입력값 검사

--- src/main/java/lotto/utility/InputChecker.java
+++ src/main/java/lotto/utility/InputChecker.java
@@ -6,35 +6,21 @@ import java.util.regex.Pattern;
 
 public class InputChecker {
     private static final Pattern NUMBER_PATTERN_COMPILE = Pattern.compile("^[0-9]{1,}$");
-    private static final Pattern OPERATOR_PATTERN_COMPILE = Pattern.compile("^[+|\\-|*|/]$");
 
     public List<String> formulaToStrList(String formula) {
+        List<String> formulaList = Arrays.asList(formula.trim().split(" "));
+        this.isThrowIllegalArgumentException(formulaList);
 
-        return Arrays.asList(formula.trim().split(" "));
+        return formulaList;
     }
 
-    public void validFormula(List<String> formulaList) {
-        this.isThrowIllegalArgumentException(this.isNumberLastListValue(formulaList));
-
-        for(int i = 0; i<formulaList.size(); i++) {
-            this.isThrowIllegalArgumentException(this.validFormulaIndex(i, formulaList.get(i)));
-        }
-    }
-
-    private boolean validFormulaIndex(int index, String str) {
-        if(index%2 == 0) {
-            return NUMBER_PATTERN_COMPILE.asMatchPredicate().test(str);
-        }
-        return OPERATOR_PATTERN_COMPILE.asMatchPredicate().test(str);
-    }
-
-    private boolean isNumberLastListValue(List<String> formulaList) {
+    private boolean isLastWordNumber(List<String> formulaList) {
         return NUMBER_PATTERN_COMPILE.asMatchPredicate().test(formulaList.get(formulaList.size()-1));
     }
 
-    private void isThrowIllegalArgumentException(boolean flag) {
-        if(!flag) {
-            throw new IllegalArgumentException ("연속으로 숫자 혹은 사칙연산자가 입력되었습니다.");
+    private void isThrowIllegalArgumentException(List<String> formulaList) {
+        if(!this.isLastWordNumber(formulaList)) {
+            throw new IllegalArgumentException ("마지막 입력값이 숫자가 아닙니다.");
         }
     }
 
--- src/test/java/lotto/InputCheckerTest.java
+++ src/test/java/lotto/InputCheckerTest.java
@@ -14,13 +14,15 @@ import java.util.List;
 public class InputCheckerTest {
 
     @Test
-    @DisplayName("입력받은 문자열 앞뒤 공백 제거 후 빈 공백으로 split 하여 짝수 index -> 숫자, 홀수 index -> 연산자 되도록 구분")
+    @DisplayName("입력받은 문자열 앞뒤 공백 제거 후 빈 공백으로 split 리스트 반환")
     public void formulaToStrListTest() {
         String testInput = " 44 + 55 - 2 / 5 ";
 
         InputChecker inputChecker = new InputChecker();
 
         List<String> result = inputChecker.formulaToStrList(testInput);
+
+        assertThat(result.size()).isEqualTo(7);
 
         // 짝수 index -> 숫자
         assertThat(result.get(0)).isEqualTo("44");
@@ -36,89 +38,21 @@ public class InputCheckerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", "  "})
-    @DisplayName("입력받은 문자열이 빈 공백일 때 확인")
-    public void formulaToStrListTest2() {
-        String test = "";
+    @DisplayName("입력받은 문자열이 빈 공백일 때 IllegalArgumentException 예외발생")
+    public void formulaToStrListTest2(String test) {
         InputChecker inputChecker = new InputChecker();
 
-        List<String> result2 = inputChecker.formulaToStrList(test);
-
-        assertThat(result2.get(0)).isEqualTo("");
+        assertThatThrownBy(() -> inputChecker.formulaToStrList(test))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
-
-//    @Test
-//    @DisplayName("입력받은 문자열을 빈 공백으로 split할 경우 index가 짝수이면 숫자, 홀수이면 사칙연산자를 구분")
-//    public void validFormulaIndexTest() {
-//        InputChecker inputChecker = new InputChecker();
-//
-//        boolean result = inputChecker.validFormulaIndex(0, "5");    // 숫자 index: 숫자
-//        boolean result2 = inputChecker.validFormulaIndex(2, "55");    // 숫자 index: 2자리 이상
-//        boolean result3 = inputChecker.validFormulaIndex(0, "");    // 숫자 index: 미입력
-//        boolean result4 = inputChecker.validFormulaIndex(2, "-");    // 숫자 index: 숫자 외 입력
-//        boolean result5 = inputChecker.validFormulaIndex(1, "-");   // 연산자 index: 연산자
-//        boolean result6 = inputChecker.validFormulaIndex(1, "+-");   // 연산자 index: 연산자 여러개
-//        boolean result7 = inputChecker.validFormulaIndex(3, "123");   // 연산자 index: 연산자 외 입력
-//
-//        assertThat(result).isTrue();
-//        assertThat(result2).isTrue();
-//        assertThat(result3).isFalse();
-//        assertThat(result4).isFalse();
-//        assertThat(result5).isTrue();
-//        assertThat(result6).isFalse();
-//        assertThat(result7).isFalse();
-//    }
-
-
-//    @Test
-//    @DisplayName("입력받은 문자열을 빈 공백으로 split할 경우 마지막 index의 value가 숫자면 True")
-//    public void isNumberLastListValueTest() {
-//        String testInput = " 44 + 55 - 2 / 5 ";
-//        String testInput2 = " 44 + 55 - 2 / ";
-//        String testInput3 = "";
-//        InputChecker inputChecker = new InputChecker();
-//
-//        List<String> testInputList = inputChecker.formulaToStrList(testInput);
-//        List<String> testInputList2 = inputChecker.formulaToStrList(testInput2);
-//        List<String> testInputList3 = inputChecker.formulaToStrList(testInput3);
-//        boolean result = inputChecker.isNumberLastListValue(testInputList);
-//        boolean result2 = inputChecker.isNumberLastListValue(testInputList2);
-//        boolean result3 = inputChecker.isNumberLastListValue(testInputList3);
-//
-//        assertThat(result).isTrue();
-//        assertThat(result2).isFalse();
-//        assertThat(result3).isFalse();
-//    }
-
-    @Test
-    @DisplayName("입력받은 문자열이 '숫자 연산자 숫자' 형태가 아닐 경우 예외를 발생시키는 유효성 검사")
-    public void validFormulaTest() {
-        String testInput = " 44 + 55 - 2 // 5";     // 연산자 2개 들어감
-        String testInput2 = " 44 + 55 - 2 / ";      // 마지막이 연산자
-        String testInput3 = " 444 55 55 - 2 / 5";   // 숫자 연속
-        String testInput4 = " 444 -5- - 2 / 5";     // 띄어쓰기 안함
-        String testInput5 = " - 55 - 2 / 5";        // 연산자부터 시작함
-        String testInput6 = " 5t5 - 2 / 5";         // 숫자, 연산자 외 다른 글자 입력
+    @ParameterizedTest
+    @ValueSource(strings = {"5 - 5 -", "5 - 5 -5", "5 - 5 tt"})
+    @DisplayName("입력받은 문자열의 마지막이 숫자가 아닐 경우 IllegalArgumentException 예외발생")
+    public void formulaToStrListTest3(String test) {
         InputChecker inputChecker = new InputChecker();
 
-        List<String> testInputList = inputChecker.formulaToStrList(testInput);
-        List<String> testInputList2 = inputChecker.formulaToStrList(testInput2);
-        List<String> testInputList3 = inputChecker.formulaToStrList(testInput3);
-        List<String> testInputList4 = inputChecker.formulaToStrList(testInput4);
-        List<String> testInputList5 = inputChecker.formulaToStrList(testInput5);
-        List<String> testInputList6 = inputChecker.formulaToStrList(testInput6);
-
-        assertThatThrownBy(() -> inputChecker.validFormula(testInputList))
-                .isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> inputChecker.validFormula(testInputList2))
-                .isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> inputChecker.validFormula(testInputList3))
-                .isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> inputChecker.validFormula(testInputList4))
-                .isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> inputChecker.validFormula(testInputList5))
-                .isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> inputChecker.validFormula(testInputList6))
-                .isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> inputChecker.formulaToStrList(test))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
