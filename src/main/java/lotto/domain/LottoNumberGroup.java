1e0587f0c6b969ada61d3e29df7c320e3e627870
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-27 18:50:17 +0900

    feat: lotto 번호 6개 모으니 실패하는 테스트 작성

---
+++ src/main/java/lotto/domain/LottoNumberGroup.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberGroup {
+
+    private List<LottoNumber> lottoNumberList;
+
+    public List<LottoNumber> aggregateLottoNumber() {
+        return new ArrayList<>();
+    }
+
+}
+
