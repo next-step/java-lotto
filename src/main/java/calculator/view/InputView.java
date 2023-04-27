b08199a82bf5f523f4af2023a49b96e05ad6c58b
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-27 10:25:15 +0900

    feat: InputView 구현

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String START_INPUT_MESSAGE = "계산할 문자열을 입력하세요";
+
+    public static String getUserInput() {
+        System.out.println(START_INPUT_MESSAGE);
+        return SCANNER.nextLine();
+    }
+}
