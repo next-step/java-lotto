6962144af30fef9e097e23c7a643b42886ca9de0
jerry shin <shiney798@gmail.com>
2023-04-28 00:11:45 +0900

    refactor : indent depth 1 적용을 위한 expression 리팩터링

--- src/main/java/stringcalculator/domain/Experssion.java
+++ src/main/java/stringcalculator/domain/Experssion.java
@@ -1,10 +1,11 @@
 package stringcalculator.domain;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Experssion {
     public static final String SPACE = " ";
-    private ArrayList<Integer> numbers = new ArrayList<>();
+    private ArrayList<Integer> operands = new ArrayList<>();
     private ArrayList<String> operators = new ArrayList<>();
 
     public Experssion(String expression) {
@@ -12,35 +13,46 @@ public class Experssion {
             throw new IllegalArgumentException("수식이 입력되지 않았습니다.");
         }
 
-        String[] elements = expression.split(SPACE);
+        String[] terms = expression.split(SPACE);
 
-        if (elements.length % 2 == 0) {
-            throw new IllegalArgumentException("입력한 수식이 형식이 맞지 않거나, 지원하지 않는 연산자 입니다.");
+        if (terms.length % 2 == 0) {
+            throw new IllegalArgumentException("입력한 수식의 개수가 유효하지 않습니다.");
         }
 
-        for (int i = 0; i < elements.length; i++) {
-            if (i % 2 == 0) {
-                if (!elements[i].matches("\\d+")) {
-                    throw new IllegalArgumentException("입력한 수식이 형식이 맞지 않거나, 지원하지 않는 연산자 입니다");
-                }
-                numbers.add(Integer.parseInt(elements[i]));
-            }
+        for (int i = 0; i < terms.length; i++) {
+            addTermToOperandOrOperator(terms[i], i);
+        }
+    }
 
-            if (i % 2 != 0) {
-                if (!elements[i].matches("[+\\-*/]")) {
-                    throw new IllegalArgumentException("입력한 수식이 형식이 맞지 않거나, 지원하지 않는 연산자 입니다");
-                }
+    private void addTermToOperandOrOperator(String term, int i) {
+        if (i % 2 == 0) {
+            validateOperand(term);
+            operands.add(Integer.parseInt(term));
+        }
 
-                operators.add(elements[i]);
-            }
+        if (i % 2 != 0) {
+            validateOperator(term);
+            operators.add(term);
+        }
+    }
+
+    private void validateOperand(String term) {
+        if (!term.matches("\\d+")) {
+            throw new IllegalArgumentException("입력된 피연산자가 숫자가 아닙니다.");
+        }
+    }
+
+    private void validateOperator(String term) {
+        if (!term.matches("[+\\-*/]")) {
+            throw new IllegalArgumentException("입력된 연산자가 사칙연산자가 아닙니다.");
         }
     }
 
     public int reduce() {
-        int result = numbers.get(0).intValue();
+        int result = operands.get(0).intValue();
 
         for (int i = 0; i < operators.size(); i++) {
-            result = calculate(result, numbers.get(i+1).intValue(), operators.get(i));
+            result = calculate(result, operands.get(i + 1).intValue(), operators.get(i));
         }
 
         return result;
--- src/test/java/stringcalculator/ExperssionTest.java
+++ src/test/java/stringcalculator/ExperssionTest.java
@@ -13,7 +13,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ExperssionTest {
 
-    @ParameterizedTest
+    @DisplayName("NULL또는빈값")
+    @ParameterizedTest(name = "{displayName} [{index}] {arguments}")
     @NullAndEmptySource
     void NULL또는빈값(String expression) {
         assertThatThrownBy(() -> {
@@ -21,17 +22,36 @@ public class ExperssionTest {
         }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("수식이 입력되지 않았습니다.");
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"4 4", "+ 3", "4 + 4 5", "3 +", "+ 3 3", "3 + 4 & 5", "3 + 4 4 5"})
-    void 수식오류(String expression) {
+    @DisplayName("수식개수검증")
+    @ParameterizedTest(name = "{displayName} [{index}] {arguments}")
+    @ValueSource(strings = {"4 4", "+ 3", "3 +", "3 + 4 4"})
+    void 수식개수검증(String expression) {
+        assertThatThrownBy(() -> {
+            Experssion experssion = new Experssion("3 + 4 +");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("입력한 수식의 개수가 유효하지 않습니다.");
+    }
+
+    @DisplayName("연산자오류")
+    @ParameterizedTest(name = "{displayName} [{index}] {arguments}")
+    @ValueSource(strings = {"3 + 4 & 5", "3 % 4 + 5", "3 + 4 4 5"})
+    void 연산자오류(String expression) {
         assertThatThrownBy(() -> {
             Experssion experssion = new Experssion(expression);
-        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("입력한 수식이 형식이 맞지 않거나, 지원하지 않는 연산자 입니다");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("입력된 연산자가 사칙연산자가 아닙니다.");
+    }
+
+    @DisplayName("피연산자오류")
+    @ParameterizedTest(name = "{displayName} [{index}] {arguments}")
+    @ValueSource(strings = {"+ 4 - 5 +", "3 + 4 + *", "3 + 4 + A"})
+    void 피연산자오류(String expression) {
+        assertThatThrownBy(() -> {
+            Experssion experssion = new Experssion(expression);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("입력된 피연산자가 숫자가 아닙니다");
     }
 
     @Test
-    @DisplayName("수식계산")
-    void 수식계산() {
+    @DisplayName("사칙연산자계산")
+    void 사칙연산자계산() {
         Experssion experssion = new Experssion("3 + 4 - 2 * 6 / 5");
 
         Assertions.assertThat(experssion.reduce()).isEqualTo(6);
