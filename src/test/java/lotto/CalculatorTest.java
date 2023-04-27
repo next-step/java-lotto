7c5251282b27edfe327f6ab454ccbecc7e4ed2c1
ain <lovepizza132@gmail.com>
2023-04-28 00:50:25 +0900

    refactor : Calculator 수정

--- src/test/java/lotto/CalculatorTest.java
+++ src/test/java/lotto/CalculatorTest.java
@@ -2,10 +2,11 @@ package lotto;
 
 import lotto.domain.Calculator;
 import lotto.domain.InputConverter;
+import lotto.domain.NumberGroup;
+import lotto.domain.OperatorGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -16,9 +17,6 @@ public class CalculatorTest {
     @Test
     @DisplayName("계산")
     public void calculateTest() {
-        String testInput = " 44 + 55 - 9 / 5 ";
-        InputConverter inputConverter = new InputConverter();
-        List<String> formulaList = inputConverter.formulaToStrList(testInput);
         Calculator calculator = new Calculator();
 
         String result = calculator.calculate("4", "5", "+");
@@ -35,16 +33,23 @@ public class CalculatorTest {
     @Test
     @DisplayName("수식 문자열 계산")
     public void calculateFormulaTest() {
-        String testInput = " 44 + 55 - 9 / 5 ";
+        String testInput = " 5 - 5 + 2";
+
         InputConverter inputConverter = new InputConverter();
         List<String> formulaList = inputConverter.formulaToStrList(testInput);
+
+        OperatorGroup operatorGroup = new OperatorGroup(formulaList);
+        Queue<String > operatorQueue = operatorGroup.operatorQueue();
+
+        NumberGroup numberGroup = new NumberGroup(formulaList);
+        List<String> numberList = numberGroup.numberList();
+
         Calculator calculator = new Calculator();
 
-        calculator.calculateFormula(formulaList);
-
+        calculator.calculateFormula(numberList, operatorQueue);
         String result = calculator.showResult();
 
-        assertThat(result).isEqualTo("18");
+        assertThat(result).isEqualTo("2");
     }
 
 
