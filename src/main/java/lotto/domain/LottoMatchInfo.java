70964bc8aa4499227a2abf14e4690129d5344249
dkswnkk <jjoy9810@gmail.com>
2023-04-28 21:30:20 +0900

    feat: countMatchingNumbers 책임을 지는 클래스 추가

---
+++ src/main/java/lotto/domain/LottoMatchInfo.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoMatchInfo {
+    private final int matchCount;
+    private final boolean bonusMatch;
+
+    public LottoMatchInfo(int matchCount, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+
+    public static LottoMatchInfo countMatchingNumbers(LottoNumber lottoNumber, List<Integer> winningNumbers) {
+        List<Integer> numbers = lottoNumber.getNumbers();
+
+        int count = (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+
+        return new LottoMatchInfo(count, false);
+    }
+
+    public static LottoMatchInfo countMatchingNumbers(LottoNumber lottoNumber, List<Integer> winningNumbers, int bonusNumber) {
+        List<Integer> numbers = lottoNumber.getNumbers();
+
+        int count = (int) numbers.stream()
+                .filter(winningNumbers::contains)
+                .count();
+
+        boolean bonusMatch = numbers.contains(bonusNumber);
+
+        return new LottoMatchInfo(count, bonusMatch);
+    }
+
+}
