b489e7fd8dc73ece45c7e6341ab94c6386866505
hoyeoon <hoyeon.backend@gmail.com>
2023-04-29 00:59:55 +0900

    refactor: final 키워드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -7,7 +7,7 @@ public class LottoNumber {
     private static final int LOTTO_NUMBER_MIN = 1;
     private static final int LOTTO_NUMBER_MAX = 45;
 
-    private int number;
+    private final int number;
 
     public LottoNumber(int number) {
         if (isNotValidLottoNumber(number)) {
