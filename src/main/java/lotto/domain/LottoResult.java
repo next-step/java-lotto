5c5d39ad193f995365b106883a1aa1b21a26a2e5
dkswnkk <jjoy9810@gmail.com>
2023-04-28 21:30:42 +0900

    feat: LottoResult에서 보너스 볼까지 고려해서 순위 저장

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,23 +1,41 @@
 package lotto.domain;
 
 import java.util.List;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class LottoResult {
-    private Map<Rank, Long> rankCounts = new HashMap<>();
+    private final static int MATCH_COUNT_FOR_SECOND_RANK = 5;
+    private final Map<Rank, Long> rankCounts;
 
     public LottoResult(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers) {
-        setRankCounts(lottoNumbers, winningNumbers);
+        rankCounts = calculateRankCounts(lottoNumbers, winningNumbers);
     }
 
-    private void setRankCounts(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers) {
-        rankCounts = lottoNumbers.stream()
-                .map(lottoNumber -> lottoNumber.countMatchingNumbers(winningNumbers))
-                .map(Rank::valueOf)
+    public LottoResult(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers, int bonusNumber) {
+        rankCounts = calculateRankCounts(lottoNumbers, winningNumbers, bonusNumber);
+    }
+
+    private Map<Rank, Long> calculateRankCounts(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers) {
+        return lottoNumbers.stream()
+                .map(lottoNumber -> LottoMatchInfo.countMatchingNumbers(lottoNumber, winningNumbers))
+                .map(this::matchInfoToRank)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private Map<Rank, Long> calculateRankCounts(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers, int bonusNumber) {
+        return lottoNumbers.stream()
+                .map(lottoNumber -> LottoMatchInfo.countMatchingNumbers(lottoNumber, winningNumbers, bonusNumber))
+                .map(this::matchInfoToRank)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private Rank matchInfoToRank(LottoMatchInfo matchInfo) {
+        if (matchInfo.getMatchCount() == MATCH_COUNT_FOR_SECOND_RANK && matchInfo.isBonusMatch()) {
+            return Rank.SECOND;
+        }
+        return Rank.valueOf(matchInfo.getMatchCount());
     }
 
     public long findRankCount(int matchCount) {
