922a2ada5ad375a5ee70397847e3e62044f852b3
didrlgus <rlgusdid@naver.com>
2023-04-28 23:49:26 +0900

    refactor: 2단계 - 로또, 코드리뷰 반영
    - PrizeType의 prizeMoney를 가독성 좋게 리팩토링

--- src/main/java/lotto/domain/PrizeType.java
+++ src/main/java/lotto/domain/PrizeType.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum PrizeType {
 
-	THREE(3, 5000),
-	FOUR(4, 50000),
-	FIVE(5, 1500000),
-	SIX(6, 2000000000);
+	THREE(3, 5_000),
+	FOUR(4, 50_000),
+	FIVE(5, 1_500_000),
+	SIX(6, 2_000_000_000);
 
 	public final Score score;
 	public final PrizeMoney prizeMoney;
