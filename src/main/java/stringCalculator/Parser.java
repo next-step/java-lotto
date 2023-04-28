7d167911aeb72054b9b9e8481b505bf4c1129907
SeokyungKim <kskyung0624@gmail.com>
2023-04-28 22:27:25 +0900

    refactor : 부수효과가 발생하는 메서드 put에 대하여 리팩토링한다

--- src/main/java/stringCalculator/Parser.java
+++ src/main/java/stringCalculator/Parser.java
@@ -3,11 +3,11 @@ package stringCalculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Parser {
+
+  private static final int ELEMENT_SEQUENCE = 2;
 
   private Parser() {}
 
@@ -16,27 +16,15 @@ public class Parser {
     List<Operator> operators = new ArrayList<>();
     List<Number> numbers = new ArrayList<>();
 
-    if (isEven(elements.size())) {
-      throw new IllegalArgumentException(String.format("연산자는 항상 피연산자 사이에 들어가야 합니다. 입력받은 값 : %s", input));
+    for (int numberIndex = 0, operatorIndex = 1; numberIndex < elements.size(); numberIndex += ELEMENT_SEQUENCE, operatorIndex += ELEMENT_SEQUENCE) {
+      numbers.add(new Number(elements.get(numberIndex)));
+      operators.add(Operator.from(elements.get(operatorIndex)));
     }
 
-    for (int i = 0; i < elements.size(); i++) {
-      put(operators, numbers, elements.get(i), i);
+    if (numbers.size() <= operators.size()) {
+      throw new IllegalArgumentException(String.format("마지막은 항상 피연산자로 끝나야 합니다. 마지막에 입력된 값 : %s", elements.get(elements.size() - 1)));
     }
 
     return new Expression(operators, numbers);
-  }
-
-  private static void put(List<Operator> operators, List<Number> numbers, String element, int index) {
-    if (isEven(index)) {
-      numbers.add(new Number(element));
-      return;
-    }
-
-    operators.add(Operator.from(element));
-  }
-
-  private static boolean isEven(int target) {
-    return target % 2 == 0;
   }
 }
