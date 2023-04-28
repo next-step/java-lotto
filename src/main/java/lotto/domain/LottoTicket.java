eb35858e1336481d014a69ba1cd44844263ba295
eohyemin <eohyemin@172.20.nate.com>
2023-04-28 12:10:51 +0900

    refactor: 로또티켓 매직넘버 미사용로직 수정

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -37,7 +37,7 @@ public class LottoTicket {
     }
 
     private void validateSize(List<LottoNumber> lottoNumbers) {
-        if (lottoNumbers.size() != 6) {
+        if (lottoNumbers.size() != LOTTO_TICKET_SIZE) {
             throw new IllegalArgumentException("로또 티켓은 6개의 로또 번호로만 구성됩니다.");
         }
     }
