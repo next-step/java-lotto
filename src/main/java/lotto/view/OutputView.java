9f0d9a4bb3bca3af96bb30b28baf8aa2c81b689c
dkswnkk <jjoy9810@gmail.com>
2023-04-28 21:30:59 +0900

    feat: 보너스 볼 추가에 따른 OutputView 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,18 +4,28 @@ import lotto.domain.LottoResult;
 import lotto.domain.LottoTicket;
 import lotto.domain.Rank;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
 
-    private static final Map<Integer, String> MATCH_MESSAGES = new HashMap<>();
+    private static final Map<Integer, List<String>> MATCH_MESSAGES = new HashMap<>();
 
     static {
-        MATCH_MESSAGES.put(3, "3개 일치(5000원)-");
-        MATCH_MESSAGES.put(4, "4개 일치(50000원)-");
-        MATCH_MESSAGES.put(5, "5개 일치(1500000원)-");
-        MATCH_MESSAGES.put(6, "6개 일치(2000000000원)-");
+        MATCH_MESSAGES.put(3, new ArrayList<>());
+        MATCH_MESSAGES.get(3).add("3개 일치(5000원)-");
+
+        MATCH_MESSAGES.put(4, new ArrayList<>());
+        MATCH_MESSAGES.get(4).add("4개 일치(50000원)-");
+
+        MATCH_MESSAGES.put(5, new ArrayList<>());
+        MATCH_MESSAGES.get(5).add("5개 일치(1500000원)-");
+        MATCH_MESSAGES.get(5).add("5개 일치, 보너스 볼 일치(30000000원)-");
+
+        MATCH_MESSAGES.put(6, new ArrayList<>());
+        MATCH_MESSAGES.get(6).add("6개 일치(2000000000원)-");
     }
 
     public static void printLottos(LottoTicket lottoTicket) {
@@ -28,10 +38,18 @@ public class OutputView {
         System.out.println("---------");
 
         for (Integer key : MATCH_MESSAGES.keySet()) {
-            System.out.println(MATCH_MESSAGES.get(key) + lottoResult.findRankCount(key) + "개");
+            printMatchMessage(key, lottoResult);
         }
 
         System.out.println("총 수익률은 " + calculateReturnRate(purchaseAmount, lottoResult) + "입니다.");
+    }
+
+    private static void printMatchMessage(Integer key, LottoResult lottoResult) {
+        List<String> messages = MATCH_MESSAGES.get(key);
+
+        for (String message : messages) {
+            System.out.println(message + lottoResult.findRankCount(key) + "개");
+        }
     }
 
     public static Double calculateReturnRate(int purchaseAmount, LottoResult lottoResult) {
