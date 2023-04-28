6cc3d1e33a502f2dcb3017456c1a9dbf483d2b0d
SeokyungKim <kskyung0624@gmail.com>
2023-04-28 21:46:45 +0900

    refactor : Operator의 calculator 메서드를 추상 메서드가 아닌 인터페이스로 해결한다

--- src/main/java/stringCalculator/Operator.java
+++ src/main/java/stringCalculator/Operator.java
@@ -1,39 +1,20 @@
 package stringCalculator;
 
+import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
 
 public enum Operator {
-  PLUS("+") {
-    @Override
-    public Number calculate(Number number1, Number number2) {
-      return new Number(number1.value() + number2.value());
-    }
-  },
-  MINUS("-") {
-    @Override
-    public Number calculate(Number number1, Number number2) {
-      return new Number(number1.value() - number2.value());
-    }
-  },
-  DIVISION("/") {
-    @Override
-    public Number calculate(Number number1, Number number2) {
-      return new Number(number1.value() / number2.value());
-    }
-  },
-  MULTIPLICATION("*") {
-    @Override
-    public Number calculate(Number number1, Number number2) {
-      return new Number(number1.value() * number2.value());
-    }
-  };
+  PLUS("+",(number1, number2) -> new Number(number1.value() + number2.value())),
+  MINUS("-", (number1, number2) -> new Number(number1.value() - number2.value())),
+  DIVISION("/", (number1, number2) -> new Number(number1.value() / number2.value())),
+  MULTIPLICATION("*", (number1, number2) -> new Number(number1.value() * number2.value()));
 
   private final String operator;
+  private final BinaryOperator<Number> calculate;
 
-  abstract Number calculate(Number number1, Number number2);
-
-  Operator(String operator) {
+  Operator(String operator, BinaryOperator<Number> calculate) {
     this.operator = operator;
+    this.calculate = calculate;
   }
 
   public static Operator from(String operator) {
@@ -41,5 +22,9 @@ public enum Operator {
         .filter(op -> op.operator.equals(operator))
         .findFirst()
         .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 연산자입니다."));
+  }
+
+  public Number calculate(Number number1, Number number2) {
+    return calculate.apply(number1, number2);
   }
 }
