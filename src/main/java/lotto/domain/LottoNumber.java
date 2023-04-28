da4379ff0f5c357093b1d07f1896b0d48412f25a
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-28 23:40:57 +0900

    feat: LottoNumber 메서드 추가  - Set 자료구조 안의 LottoNumber 객체 동등성 비교  - equals(), hashCode() 메서드 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
     private final int number;
 
@@ -16,5 +18,18 @@ public class LottoNumber {
 
     public int findLottoNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LottoNumber)) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
