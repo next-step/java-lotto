4f54cff85c8107f5555c05f97a8a1d02ad66121b
Baek Jiyeon <100.paperkite@gmail.com>
2023-04-28 18:50:29 +0900

    feat: 입력 컨트롤러에서 여러 값을 입력받을 수 있도록 `getValues` 메서드 추가

--- src/main/java/lotto/control/input/InputControl.java
+++ src/main/java/lotto/control/input/InputControl.java
@@ -3,7 +3,11 @@ package lotto.control.input;
 import lotto.view.View;
 import lotto.view.input.DummyView;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public abstract class InputControl<T> {
     protected static final Scanner scanner = new Scanner(System.in);
@@ -20,6 +24,18 @@ public abstract class InputControl<T> {
     public T getValue() {
         view.render();
         return this.input();
+    }
+
+    public List<T> getValues(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("The input value must be greater than 0");
+        }
+
+        view.render();
+
+        return IntStream.range(0, count)
+                .mapToObj(i -> this.input())
+                .collect(toList());
     }
 
     protected abstract T input();
