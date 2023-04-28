5f247bc634f510c1a8ed119d3f89a8605c67df2c
SightStudio <sdm3285@gmail.com>
2023-04-29 04:31:44 +0900

    feature (결과화면 구현)

---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,35 @@
+package lotto.view;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lotto.domain.game.LottoGameStatistics;
+import lotto.domain.game.LottoWinType;
+import lotto.domain.round.LottoRound;
+import lotto.domain.round.LottoRoundResult;
+
+public class ResultView {
+
+  public static void displayStatistics (LottoGameStatistics statistics) {
+    Map<LottoWinType, List<LottoRoundResult>> winTypeMap = statistics.getWinTypeMap();
+
+    System.out.println("\n당첨 통계\n---------");
+    for (LottoWinType value : LottoWinType.values()) {
+      List<LottoRoundResult> lottoRoundResults = winTypeMap.getOrDefault(value, Collections.emptyList());
+      System.out.printf("%d개 일치 (%d원) - %d개\n", value.getMatchingNumberCnt(), value.getPrize(), lottoRoundResults.size());
+    }
+
+    System.out.printf("총 수익률은 %s", statistics.getTotalRateOfReturn());
+  }
+
+  public static void showLottoRounds (List<LottoRound> lottoRounds) {
+    System.out.printf("%d개를 구매했습니다.\n", lottoRounds.size());
+    System.out.println(
+        lottoRounds.stream()
+            .map(LottoRound::getNumbers)
+            .map(Object::toString)
+            .collect(Collectors.joining("\n"))
+    );
+  }
+}
