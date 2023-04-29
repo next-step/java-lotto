84188beddd979ca72991eb435fb17966a4d5b4e3
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-30 06:22:34 +0900

    feat: 당첨 로또 계산을 위한 메서드 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,10 +1,16 @@
 package lotto.domain;
 
 public class WinningLotto {
+
     private final LottoTicket lottoTicket;
 
     public WinningLotto(LottoTicket lottoTicket) {
         this.lottoTicket = lottoTicket;
+    }
+
+    public WinningRank match(LottoTicket ticket) {
+        int matchCount = countOfMatch(ticket);
+        return WinningRank.valueOf(matchCount);
     }
 
     public int countOfMatch(LottoTicket ticket) {
@@ -16,6 +22,16 @@ public class WinningLotto {
 
     public LottoTicket findWinningLotto() {
         return lottoTicket;
+    }
+
+    public WinningStatistics calculateLottoStatistics(LottoTickets lottoTickets) {
+        WinningStatistics winningStatistics = new WinningStatistics();
+
+        for (LottoTicket ticket : lottoTickets.getLottoTickets()) {
+            WinningRank rank = match(ticket);
+            winningStatistics.addMatchedTicket(rank);
+        }
+        return winningStatistics;
     }
 }
 
--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,14 +1,29 @@
 package lotto.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Set;
 import java.util.TreeSet;
 
-@DisplayName("당첨 로또 테스트")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("로또 당첨 테스트")
 class WinningLottoTest {
+
+    private WinningLotto winningLotto;
+
+    @BeforeEach
+    void setUp() {
+        LottoTicket lottoTicket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6)));
+        winningLotto = new WinningLotto(lottoTicket);
+    }
 
     @DisplayName("승리 로또와 일치하는 번호의 카운트를 확인할수 있다")
     @Test
@@ -20,6 +35,7 @@ class WinningLottoTest {
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
+                new LottoNumber(4),
                 new LottoNumber(5),
                 new LottoNumber(7)
         )));
@@ -29,4 +45,94 @@ class WinningLottoTest {
         Assertions.assertThat(countOfMatch).isEqualTo(5);
     }
 
+    @Test
+    @DisplayName("6개의 숫자가 일치할 때 1등이다.")
+    void shouldMatchFirstRankWhenAllNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(6)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.FIRST, rank);
+    }
+
+    @Test
+    @DisplayName("5개의 숫자가 일치할 때 2등이다.")
+    void shouldMatchSecondRankWhenFiveNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(5), new LottoNumber(7)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.SECOND, rank);
+    }
+
+    @Test
+    @DisplayName("4개의 숫자가 일치할 때 3등이다.")
+    void shouldMatchThirdRankWhenFourNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(4),
+                new LottoNumber(10), new LottoNumber(11)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.THIRD, rank);
+    }
+
+    @Test
+    @DisplayName("3개의 숫자가 일치할 때 3등이다.")
+    void shouldMatchFourthRankWhenThreeNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(3), new LottoNumber(11),
+                new LottoNumber(12), new LottoNumber(13)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.FOURTH, rank);
+    }
+
+    @Test
+    @DisplayName("2개의 숫자가 일치할 때 0등이다.")
+    void shouldMatchNoneRankWhenTwoNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(2),
+                new LottoNumber(13), new LottoNumber(14),
+                new LottoNumber(15), new LottoNumber(16)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.NONE, rank);
+    }
+
+    @Test
+    @DisplayName("1개의 숫자가 일치할 때 0등이다.")
+    void shouldMatchNoneRankWhenOneNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(1), new LottoNumber(12),
+                new LottoNumber(13), new LottoNumber(14),
+                new LottoNumber(15), new LottoNumber(16)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.NONE, rank);
+    }
+
+    @DisplayName("0개의 숫자가 일치할 때 0등이다.")
+    @Test
+    void shouldMatchNoneRankWhenNothingNumbersMatch() {
+        LottoTicket ticket = new LottoTicket(Set.of(
+                new LottoNumber(11), new LottoNumber(12),
+                new LottoNumber(13), new LottoNumber(14),
+                new LottoNumber(15), new LottoNumber(16)));
+
+        WinningRank rank = winningLotto.match(ticket);
+
+        assertEquals(WinningRank.NONE, rank);
+    }
 }
