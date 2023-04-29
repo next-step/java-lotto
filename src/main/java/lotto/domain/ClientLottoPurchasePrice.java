19078706b053cf0225e3717a2adc06c357d08d4e
patrickYoo <yhy1045@naver.com>
2023-04-30 00:02:47 +0900

    feat: Client 가격 입력시 에러발생 코드 작성및 테스트코드 작성

--- src/main/java/lotto/domain/ClientLottoPurchasePrice.java
+++ src/main/java/lotto/domain/ClientLottoPurchasePrice.java
@@ -2,9 +2,22 @@ package lotto.domain;
 
 public class ClientLottoPurchasePrice {
 
+    private static final int MIN_PURCHASABLE_PRICE = 1000;
+
+    private static final String INVALID_PRICE_MSG = "로또 최소 구매 금액은 1000원 이상입니다.";
+
     private final int purchasePrice;
 
     public ClientLottoPurchasePrice(int purchasePrice) {
+        if(invalidPrice(purchasePrice)) {
+            throw new IllegalArgumentException(INVALID_PRICE_MSG);
+        }
         this.purchasePrice = purchasePrice;
     }
+
+    private boolean invalidPrice(int purchasePrice) {
+        return purchasePrice < MIN_PURCHASABLE_PRICE;
+    }
+
+
 }
--- src/test/java/lotto/domain/ClientLottoPurchasePriceTest.java
+++ src/test/java/lotto/domain/ClientLottoPurchasePriceTest.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,9 +11,19 @@ public class ClientLottoPurchasePriceTest {
 
     private static final int MIN_PURCHASABLE_PRICE = 1000;
 
+    private static final int LOWER_THAN_MIN_PURCHASABLE_PRICE = 999;
+
     @Test
     @DisplayName("로또 구매 금액 입력시 1000원 이상 입력할 경우 객체 정상 생성")
     void clientLottoPurchasePriceCreate() {
         assertDoesNotThrow(() -> new ClientLottoPurchasePrice(MIN_PURCHASABLE_PRICE));
+    }
+
+    @Test
+    @DisplayName("로또 구매 금액 입력시 1000원 이하 입력할 경우 익셉션 발생")
+    void clientLottoPurchasePriceError() {
+        assertThatThrownBy(() -> new ClientLottoPurchasePrice(LOWER_THAN_MIN_PURCHASABLE_PRICE))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 최소 구매 금액은 1000원 이상입니다.");
     }
 }
