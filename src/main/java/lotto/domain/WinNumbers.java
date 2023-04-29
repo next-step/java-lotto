3a51693cc42f68972448ddc069aaf6d0829624fe
didrlgus <rlgusdid@naver.com>
2023-04-29 11:09:32 +0900

    refactor: 2단계 - 로또, 코드리뷰 반영
    - UI에 추가되어있던 유효성 검증 로직을 도메인으로 이동

--- src/main/java/lotto/domain/WinNumbers.java
+++ src/main/java/lotto/domain/WinNumbers.java
@@ -1,17 +1,64 @@
 package lotto.domain;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WinNumbers {
 
-	private final List<Integer> winNumbers;
+	public static String SIZE_EXCEPTION_TEXT = String.format("당첨 넘버의 크기는 %d입니다.", Lotto.SIZE);
+	public static String TYPE_EXCEPTION_TEXT = "당첨번호는 숫자만 입력 가능합니다.";
+	public static String RANGE_EXCEPTION_TEXT = String.format("당첨 넘버는 %d~%d까지의 숫자만 입력 가능합니다.", Lotto.MINIMUM_BOUND, Lotto.MAXIMUM_BOUND);
+	public static String DUPLICATE_EXCEPTION_TEXT = "당첨 넘버는 중복될 수 없습니다.";
 
-	public WinNumbers(List<Integer> winNumbers) {
-		this.winNumbers = winNumbers;
+	private final Set<Integer> winNumbers = new HashSet<>();
+
+	public WinNumbers(String input) {
+		String[] split = this.split(input);
+		this.checkSize(split);
+		this.addWinNumbers(split);
+		this.checkDuplicate(split.length);
+	}
+
+	private String[] split(String input) {
+		return input.split(", ");
+	}
+
+	private void checkSize(String[] split) {
+		if (split.length != Lotto.SIZE) {
+			throw new IllegalArgumentException(WinNumbers.SIZE_EXCEPTION_TEXT);
+		}
+	}
+
+	private void addWinNumbers(String[] split) {
+		for (String winNumberString : split) {
+			int winNumber = this.toInt(winNumberString);
+			this.checkRange(winNumber);
+			winNumbers.add(winNumber);
+		}
+	}
+
+	private int toInt(String winNumbersString) {
+		try {
+			return Integer.parseInt(winNumbersString);
+		} catch (Exception e) {
+			throw new IllegalArgumentException(WinNumbers.TYPE_EXCEPTION_TEXT);
+		}
+	}
+
+	private void checkRange(int winNumber) {
+		if (winNumber < Lotto.MINIMUM_BOUND || winNumber > Lotto.MAXIMUM_BOUND) {
+			throw new IllegalArgumentException(WinNumbers.RANGE_EXCEPTION_TEXT);
+		}
+	}
+
+	private void checkDuplicate(int splitLength) {
+		if (this.winNumbers.size() != splitLength) {
+			throw new IllegalArgumentException(WinNumbers.DUPLICATE_EXCEPTION_TEXT);
+		}
 	}
 
 	public Score plusScore(LottoNumbers lottoNumbers, Score score) {
-		for (Integer winNumber : winNumbers) {
+		for (Integer winNumber : this.winNumbers) {
 			this.plus(lottoNumbers, score, winNumber);
 		}
 		return score;
--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,10 +1,6 @@
 package lotto.ui;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
-import lotto.domain.Lotto;
 
 public class InputView {
 
@@ -23,44 +19,8 @@ public class InputView {
 		return purchaseAmount;
 	}
 
-	public static List<Integer> inputWinNumbers() {
+	public static String inputWinNumbers() {
 		System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
-		String[] split = scanner.nextLine().split(", ");
-
-		checkSize(split.length);
-
-		List<Integer> winNumbers = new ArrayList<>();
-		for (String winNumberString : split) {
-			winNumbers.add(correctWinNumber(winNumberString));
-		}
-
-		return winNumbers;
-	}
-
-	private static void checkSize(int length) {
-		if (length != Lotto.SIZE) {
-			throw new IllegalArgumentException(String.format("로또의 크기인 %d만큼 입력 가능합니다.", Lotto.SIZE));
-		}
-	}
-
-	private static int correctWinNumber(String winNumbersString) {
-		return checkRange(toInt(winNumbersString));
-	}
-
-	private static int toInt(String winNumbersString) {
-		int winNumber;
-		try {
-			winNumber = Integer.parseInt(winNumbersString);
-		} catch (Exception e) {
-			throw new IllegalArgumentException("당첨번호는 숫자만 입력 가능합니다.");
-		}
-		return winNumber;
-	}
-
-	private static int checkRange(int winNumber) {
-		if (winNumber < Lotto.MINIMUM_BOUND || winNumber > Lotto.MAXIMUM_BOUND) {
-			throw new IllegalArgumentException(String.format("당첨 넘버는 %d~%d까지의 숫자만 입력 가능합니다.", Lotto.MINIMUM_BOUND, Lotto.MAXIMUM_BOUND));
-		}
-		return winNumber;
+		return scanner.nextLine();
 	}
 }
