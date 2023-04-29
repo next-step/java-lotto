5bf051994387109fb858dd43bd0e8e0bec7fa957
dong <dong.22@kakaoenterprise.com>
2023-04-29 12:17:34 +0900

    refactor : 티켓 중간인터페이스 생성

--- src/main/java/lotto/domain/TicketInf.java
+++ src/main/java/lotto/domain/TicketInf.java
@@ -3,12 +3,12 @@ package lotto.domain;
 import java.util.List;
 import java.util.Set;
 
-public interface Ticket {
+public interface TicketInf {
     Set<Integer> parseToNumbers(String stringNumbers);
     void validateTicket();
-    int countMatchNumbers(Ticket ticket);
+    int countMatchNumbers(TicketInf ticketInf);
 
     //이건 winner 전용
     boolean includeNumber(int number); //사실상 인클루드 보너스임
-    int countWinners(List<Ticket> challengeTickets, Prize prize);
+    int countWinners(List<TicketInf> challengeTicketInfs, Prize prize);
 }
