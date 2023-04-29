1acfb86b821ff95f9db43ce9f79c90b21245b7ff
신형기 <tkaqkeldk@hanatour.com>
2023-04-29 15:12:06 +0900

    feat: lotto Test 작성

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoTest {
+
+    @Test
+    @DisplayName("로또 번호는 6개여야 한다")
+    void lotto_must_be_size_SIX() {
+        Lotto lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+        assertEquals(6, lotto.getLottoNumbers().size());
+    }
+
+    @Test
+    @DisplayName("로또 번호는 1부터 45사이여야 한다")
+    void lotto_number_must_be_between_1_and_45() {
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(Arrays.asList(1,2,3,4,5,46)));
+    }
+
+    @Test
+    @DisplayName("로또 번호는 중복될 수 없다")
+    void lotto_number_must_not_be_duplicated() {
+        assertThrows(IllegalArgumentException.class, () -> new Lotto(Arrays.asList(1,2,3,4,5,5)));
+    }
+}
