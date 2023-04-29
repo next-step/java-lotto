7d8ecf651fbf53eea135c140c565e93471724e71
Sim Jung <tlawjd99gma@gmail.com>
2023-04-29 22:15:28 +0900

    feat : 조건에 맞는 로또 객체를 생성할 수 있다 - 로또 숫자의 범위는 1~45 이다. - 로또 숫자는 6개이다. - 로또 숫자는 중복되지 않는다. - 로또 숫자는 오름차순된다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,2 +1,56 @@
-package lotto;public class Lotto {
+package lotto;
+
+import java.util.Arrays;
+
+public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
+    private static final String INVALID_LOTTO_NUMBERS_MESSAGE = "적절한 로또 입력이 아닙니다.";
+
+    private final int[] lottoNumbers;
+
+    public Lotto(int[] lottoNumbers) {
+        if (!isValidLottoNumbers(lottoNumbers)) {
+            throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_MESSAGE);
+        }
+
+        this.lottoNumbers = Arrays.stream(lottoNumbers)
+                .sorted()
+                .toArray();
+    }
+
+    public int[] getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    private boolean isValidLottoNumbers(int[] lottoNumbers) {
+        if (lottoNumbers.length != LOTTO_NUMBER_SIZE) {
+            return false;
+        }
+
+        if (!isBetweenMinAndMax(lottoNumbers)) {
+            return false;
+        }
+
+        if (!hasUniqueNumbers(lottoNumbers)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isBetweenMinAndMax(int[] lottoNumbers) {
+        return Arrays.stream(lottoNumbers)
+                .noneMatch(it ->
+                        it < MINIMUM_LOTTO_NUMBER ||
+                                it > MAXIMUM_LOTTO_NUMBER
+                );
+    }
+
+    private boolean hasUniqueNumbers(int[] lottoNumbers) {
+        return Arrays.stream(lottoNumbers)
+                .distinct()
+                .count() == LOTTO_NUMBER_SIZE;
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,2 +1,45 @@
-package lotto;public class LottoTest {
+package lotto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class LottoTest {
+    @Test
+    void 로또_숫자의_개수가_6개가_아니면_오류가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(new int[]{}))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "{0}은 로또 숫자의 범위를 벗어나서 오류가 발생한다")
+    @MethodSource("getInputFor_로또_숫자의_범위가_1에서_45_사이가_아니면_오류가_발생한다")
+    void 로또_숫자의_범위가_1에서_45_사이가_아니면_오류가_발생한다(int[] input) {
+        assertThatThrownBy(() -> new Lotto(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또는_만들어지면_오름차순으로_정렬된다() {
+        assertThat(new Lotto(new int[]{4, 2, 5, 3, 6, 1}).getLottoNumbers())
+                .containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 로또에_중복된_숫자가_있으면_오류가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(new int[]{1, 2, 3, 4, 5, 5}))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> getInputFor_로또_숫자의_범위가_1에서_45_사이가_아니면_오류가_발생한다() {
+        return Stream.of(
+                Arguments.arguments(new int[]{0, 2, 3, 4, 5, 6}),
+                Arguments.arguments(new int[]{1, 2, 3, 4, 5, 46})
+        );
+    }
 }
