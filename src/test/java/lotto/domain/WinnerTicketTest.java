429de2335c20595472ff8f0171ffeb02f98e3c5e
dong <dong.22@kakaoenterprise.com>
2023-04-29 15:42:02 +0900

    test : 테스트코드 추가

--- src/test/java/lotto/domain/WinnerTicketTest.java
+++ src/test/java/lotto/domain/WinnerTicketTest.java
@@ -4,26 +4,67 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class WinnerTicketTest {
 
-    private WinnerTicket winnerTicket;
+    private WinnerTicket winnerTicketFixture;
+    private List<Ticket> ticketsFixture;
 
     @BeforeEach
     public void beforeEach() {
-        winnerTicket = new WinnerTicket(new Ticket(Set.of(1, 2, 4, 8, 16, 32)), 33);
+        Ticket winner = new Ticket(Set.of(1, 2, 4, 8, 16, 32));
+        winnerTicketFixture = winner.winnerTicket(33);
+
+        Ticket first = new Ticket(Set.of(1, 2, 4, 8, 16, 32));
+        Ticket secondA = new Ticket(Set.of(1, 2, 4, 8, 16, 33));
+        Ticket secondB = new Ticket(Set.of(1, 2, 4, 8, 32, 33));
+
+        Ticket thirdA = new Ticket(Set.of(1, 2, 4, 8, 16, 40));
+        Ticket thirdB = new Ticket(Set.of(1, 2, 4, 8, 16, 41));
+        Ticket thirdC = new Ticket(Set.of(1, 2, 4, 8, 16, 42));
+
+        Ticket fourthA = new Ticket(Set.of(1, 2, 33, 4, 35, 8));
+        Ticket fourthB = new Ticket(Set.of(1, 2, 33, 4, 35, 8));
+        Ticket fourthC = new Ticket(Set.of(1, 2, 33, 4, 35, 8));
+        Ticket fourthD = new Ticket(Set.of(1, 2, 33, 4, 35, 8));
+
+        Ticket fifthA = new Ticket(Set.of(1, 2, 4, 15, 26, 37));
+        Ticket fifthB = new Ticket(Set.of(1, 2, 4, 25, 26, 17));
+        Ticket fifthC = new Ticket(Set.of(1, 2, 4, 35, 36, 37));
+        Ticket fifthD = new Ticket(Set.of(1, 2, 4, 25, 36, 27));
+        Ticket fifthE = new Ticket(Set.of(1, 2, 4, 35, 26, 17));
+        ticketsFixture = List.of(
+                first,
+                secondA, secondB,
+                thirdA, thirdB, thirdC,
+                fourthA, fourthB, fourthC, fourthD,
+                fifthA, fifthB, fifthC, fifthD, fifthE
+        );
     }
 
     @DisplayName("보너스 숫자 여부를 판별한다")
     @Test
     public void includeBonus() {
         //given
+        Ticket ticketA = new Ticket(Set.of(1, 2, 4, 8, 16, 33));
+        Ticket ticketB = new Ticket(Set.of(1, 2, 4, 8, 32, 33));
+        Ticket ticketC = new Ticket(Set.of(1, 2, 4, 8, 16, 40));
         //when
+        Boolean actualA = winnerTicketFixture.includeBonus(ticketA);
+        Boolean actualB = winnerTicketFixture.includeBonus(ticketB);
+        Boolean actualC = winnerTicketFixture.includeBonus(ticketC);
         //then
-        fail();
+        assertAll("",
+                () -> assertThat(actualA).as("보너스 숫자를 포함하고 있어야한다").isEqualTo(true),
+                () -> assertThat(actualB).as("보너스 숫자를 포함하고 있어야한다").isEqualTo(true),
+                () -> assertThat(actualC).as("보너스 숫자를 포함하지 않는다").isEqualTo(false)
+        );
     }
 
     @DisplayName("당첨자의 숫자를 구하는 기능을 검증한다")
