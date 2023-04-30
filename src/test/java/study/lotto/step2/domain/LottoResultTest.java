8aa09a7e7b3cc914b4f12d4734ae53c232435bd8
seriouskang <scdean110@gmail.com>
2023-04-30 10:49:34 +0900

    feat(LottoResult): 로또 결과 enum 신규

---
+++ src/test/java/study/lotto/step2/domain/LottoResultTest.java
@@ -0,0 +1,43 @@
+package study.lotto.step2.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class LottoResultTest {
+    @Test
+    @DisplayName("로또 당첨 번호 갯수가 1 이상 6 이하가 아닐 경우, IllegalArgumentException 예외 발생")
+    void invalid_numbers_of_match() {
+        // given
+        int invalidNumbersOfMatch = 7;
+
+        // when, then
+        assertThatThrownBy(() -> LottoResult.of(invalidNumbersOfMatch))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 당첨 번호 갯수는 1 이상 6 이하의 정수: " + invalidNumbersOfMatch);
+    }
+
+    @ParameterizedTest(name = "[{index}/6] {displayName}")
+    @MethodSource("numbersOfMatchAndLottoResult")
+    @DisplayName("당첨 번호 갯수로 부터 LottoResult enum 반환")
+    void lotto_result(int numbersOfMatch, LottoResult expectedLottoResult) {
+        assertThat(LottoResult.of(numbersOfMatch)).isEqualTo(expectedLottoResult);
+    }
+
+    private static Stream<Arguments> numbersOfMatchAndLottoResult() {
+        return Stream.of(
+                Arguments.of(1, LottoResult.MATCH_ONE_NUMBER),
+                Arguments.of(2, LottoResult.MATCH_TWO_NUMBERS),
+                Arguments.of(3, LottoResult.MATCH_THREE_NUMBERS),
+                Arguments.of(4, LottoResult.MATCH_FOUR_NUMBERS),
+                Arguments.of(5, LottoResult.MATCH_FIVE_NUMBERS),
+                Arguments.of(6, LottoResult.MATCH_SIX_NUMBERS)
+        );
+    }
+}
