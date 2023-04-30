11ae0738d78cde29ec0f8815daadbadf56a52227
seriouskang <scdean110@gmail.com>
2023-04-30 17:00:32 +0900

    refactor(LottoSeller): 로또 금액 및 수량 타입 변경(int -> long)

--- src/main/java/study/lotto/step2/application/LottoSeller.java
+++ src/main/java/study/lotto/step2/application/LottoSeller.java
@@ -6,33 +6,33 @@ import study.lotto.step2.domain.SoldLottos;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class LottoSeller {
-    private static final int LOTTO_PRICE = 1_000;
+    private static final long LOTTO_PRICE = 1_000L;
     private final LottoFactory lottoFactory;
 
     public LottoSeller(LottoFactory lottoFactory) {
         this.lottoFactory = lottoFactory;
     }
 
-    public SoldLottos sell(int amount) {
+    public SoldLottos sell(long amount) {
         return new SoldLottos(lottos(numberOfSelling(amount)));
     }
 
-    private int numberOfSelling(int amount) {
+    private long numberOfSelling(long amount) {
         validateAmount(amount);
         return amount / LOTTO_PRICE;
     }
 
-    private void validateAmount(int amount) {
+    private void validateAmount(long amount) {
         if(amount < LOTTO_PRICE) {
             throw new IllegalArgumentException("최소 구입 금액은 " + LOTTO_PRICE + "원입니다: " + amount);
         }
     }
 
-    private List<Lotto> lottos(int numberOfLotto) {
-        return IntStream.rangeClosed(1, numberOfLotto)
+    private List<Lotto> lottos(long numberOfLotto) {
+        return LongStream.rangeClosed(1, numberOfLotto)
                 .mapToObj(index -> lottoFactory.lotto())
                 .collect(Collectors.toList());
     }
