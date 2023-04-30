d0efd7bdb35dee91d20a05b1bdf8eea53a46d373
seriouskang <scdean110@gmail.com>
2023-04-30 22:31:14 +0900

    feat(ConsoleInputUtil): 콘솔 입력 유틸 신규

---
+++ src/main/java/study/lotto/step2/presentation/ConsoleInputUtil.java
@@ -0,0 +1,22 @@
+package study.lotto.step2.presentation;
+
+import java.util.Scanner;
+
+public class ConsoleInputUtil {
+    private static final String INSERT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INSERT_LAST_WEEK_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private ConsoleInputUtil() {
+    }
+
+    public static String askPurchaseAmount() {
+        System.out.println(INSERT_PURCHASE_AMOUNT);
+        return SCANNER.nextLine();
+    }
+
+    public static String askLastWeekWinningNumbers() {
+        System.out.println(INSERT_LAST_WEEK_WINNING_NUMBERS);
+        return SCANNER.nextLine();
+    }
+}
