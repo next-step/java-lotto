f126225ecbf3f51cd3546cf46e13067b7905b858
신형기 <tkaqkeldk@hanatour.com>
2023-05-01 01:02:38 +0900

    refactor: ResultView indent 2depth 개선

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -28,14 +28,18 @@ public class ResultView {
         sb.append("당첨 통계\n------\n");
 
         int[] matchCounts = lottoStatistics.getMatchCounts();
+        printMatchCount(sb, matchCounts);
+
+        double yield = lottoStatistics.calculateYield();
+        sb.append(String.format("총 수익률은 %.2f%s 입니다. ", yield, (yield > 1 ? "" : " (기준이 1이기 때문에 결과적으로 손해라는 의미임)")));
+        return sb.toString();
+    }
+
+    private static void printMatchCount(final StringBuilder sb, final int[] matchCounts) {
         for (int i = 0; i < matchCounts.length; i++) {
             int matches = i + 3;
             LottoPrize prize = LottoPrize.valueOf(matches);
             sb.append(String.format("%d 개 일치 (%s) 원 - %d 개\n", matches, prize.getPrizeMoneyFormat(), matchCounts[i]));
         }
-
-        double yield = lottoStatistics.calculateYield();
-        sb.append(String.format("총 수익률은 %.2f%s 입니다. ", yield, (yield > 1 ? "" : " (기준이 1이기 때문에 결과적으로 손해라는 의미임)")));
-        return sb.toString();
     }
 }
