6b683a1ce2dfacfcc66fd249374f3a07c71b521d
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-01 00:15:10 +0900

    feat: money 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 public class Money {
+    public static final Integer LOTTO_UNIT_PRICE = 1_000;
 
     private final long amount;
 
@@ -9,8 +10,16 @@ public class Money {
     }
 
     public static Money wons(long amount) {
+        if (isNegativeNumberOrZero(amount)) {
+            throw new IllegalArgumentException("1000원 이상 입력해주세 :(");
+        }
         return new Money(amount);
     }
+
+    private static boolean isNegativeNumberOrZero(long amount) {
+        return amount <= LOTTO_UNIT_PRICE;
+    }
+
 
     public Money minus(Money amount) {
         return new Money(this.amount - amount.amount);
