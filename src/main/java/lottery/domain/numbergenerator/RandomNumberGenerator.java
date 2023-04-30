7bc464d8d03c7fa177429ed6e7415438f3fea1b4
patrickYoo <yhy1045@naver.com>
2023-05-01 02:32:20 +0900

    chor: 임의의 숫자 발생기 1부터 시작하는 매직넘버 상수화

--- src/main/java/lottery/domain/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/lottery/domain/numbergenerator/RandomNumberGenerator.java
@@ -5,10 +5,13 @@ import static lottery.domain.constant.LotteryNumberRule.MAX_LOTTERY_NUMBER;
 import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
+
     private static final Random RANDOM = new Random();
+
+    private static final int START_FROM_ONE = 1;
 
     @Override
     public int generatedNumber() {
-        return RANDOM.nextInt(MAX_LOTTERY_NUMBER.getRuleNumber()) + 1;
+        return RANDOM.nextInt(MAX_LOTTERY_NUMBER.getRuleNumber()) + START_FROM_ONE;
     }
 }
