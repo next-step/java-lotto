ed3da71c6b95dcd1816085ad8715412a71c570da
eohyemin <eohyemin@eohyemin-ui-MacBookPro.local>
2023-04-30 15:34:08 +0900

    refactor(LottoGenerator): 로또 생성 static 변경

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -12,21 +12,20 @@ public class LottoGenerator {
     private static final int LOTTO_NUMBER_MAXIMUM = 45;
     private static final int LOTTO_TICKET_SIZE = 6;
 
-    private final List<LottoNumber> lottoNumbers;
-
-    public LottoGenerator() {
-        this.lottoNumbers = IntStream.rangeClosed(LOTTO_NUMBER_MINIMUM, LOTTO_NUMBER_MAXIMUM)
-                .mapToObj(LottoNumber::new)
+    private static final List<LottoNumber> lottoNumbers = IntStream.rangeClosed(LOTTO_NUMBER_MINIMUM, LOTTO_NUMBER_MAXIMUM)
+            .mapToObj(LottoNumber::new)
                 .collect(Collectors.toList());
+
+    private LottoGenerator() {
     }
 
-    public LottoTickets generate(int count) {
+    public static LottoTickets generate(int count) {
         return new LottoTickets(IntStream.range(0, count)
                 .mapToObj(i -> generateLottoNumbers())
                 .collect(Collectors.toList()));
     }
 
-    private LottoTicket generateLottoNumbers() {
+    private static LottoTicket generateLottoNumbers() {
         Collections.shuffle(lottoNumbers, new Random(System.currentTimeMillis()));
         List<LottoNumber> randomLottoNumbers = new ArrayList<>(lottoNumbers.subList(0, LOTTO_TICKET_SIZE));
         return new LottoTicket(randomLottoNumbers);
