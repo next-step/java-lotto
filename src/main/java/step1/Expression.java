d9c71981171e0d17d31a3822e1695e3c1a7b412e
seongjo24 <kangsj24@naver.com>
2023-04-30 18:52:41 +0900

    Operator, OperatorTest 추가

--- src/main/java/step1/Expression.java
+++ src/main/java/step1/Expression.java
@@ -9,9 +9,6 @@ public class Expression {
     private List<String> numbers;
 
     public Expression(String[] expression) {
-        if (expression.length < 3) {
-            throw new IllegalArgumentException("형식에 맞춰 식을 입력하세요");
-        }
         operators = new ArrayList<>();
         numbers = new ArrayList<>();
         for (String exp : expression) {
@@ -22,50 +19,14 @@ public class Expression {
     public int calculate() {
         int result = Integer.parseInt(numbers.get(0));
         for (int i = 0; i < operators.size(); i++) {
-            String operator = operators.get(i);
-            int number = Integer.parseInt(numbers.get(i + 1));
-            result = operate(result, operator, number);
+            Operator operator = Operator.of(operators.get(i));
+            result = operator.calculate(result, Integer.parseInt(numbers.get(i + 1)));
         }
         return result;
     }
 
-    public int operate(int result, String operator, int number) {
-        result = plus(result, operator, number);
-        result = minus(result, operator, number);
-        result = multiply(result, operator, number);
-        result = divide(result, operator, number);
-        return result;
-    }
 
-    public Integer divide(int result, String operator, int number) {
-        if ("/".equals(operator)) {
-            return result / number;
-        }
-        return result;
-    }
-
-    public Integer multiply(int result, String operator, int number) {
-        if ("*".equals(operator)) {
-            return result * number;
-        }
-        return result;
-    }
-
-    public Integer minus(int result, String operator, int number) {
-        if ("-".equals(operator)) {
-            return result - number;
-        }
-        return result;
-    }
-
-    public Integer plus(int result, String operator, int number) {
-        if ("+".equals(operator)) {
-            return result + number;
-        }
-        return result;
-    }
-
-    public void seperateNumberAndOperater(String exp) {
+    private void seperateNumberAndOperater(String exp) {
         if (exp.matches(REGEXP)) {
             this.numbers.add(exp);
             return;
--- src/test/java/step1/ExpressionTest.java
+++ src/test/java/step1/ExpressionTest.java
@@ -12,11 +12,5 @@ public class ExpressionTest {
         assertThat(expression.calculate()).isEqualTo(9);
     }
 
-    @Test
-    void 사칙연산() {
-        assertThat(expression.operate(2, "+", 3)).isEqualTo(5);
-        assertThat(expression.operate(5, "*", 4)).isEqualTo(20);
-        assertThat(expression.operate(20, "/", 2)).isEqualTo(10);
-        assertThat(expression.operate(10, "-", 1)).isEqualTo(9);
-    }
+
 }
