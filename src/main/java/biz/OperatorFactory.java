47d3e9c0e25623b6fc7c34c895292ce8691e22e1
henry.5 <henry.5@kakaoenterprise.com>
2023-04-30 15:20:15 +0900

    step1 - operator Enum

--- src/main/java/biz/OperatorFactory.java
+++ src/main/java/biz/OperatorFactory.java
@@ -1,5 +1,6 @@
 package biz;
 
+import biz.enums.OperatorEnum;
 import biz.function.*;
 
 import java.util.HashMap;
@@ -10,10 +11,10 @@ public class OperatorFactory {
     private static Map<String, Operator> factory = new HashMap<String, Operator>();
 
     static {
-        factory.put("*", new MultipleOperator());
-        factory.put("/", new DivideOperator());
-        factory.put("-", new MinusOperator());
-        factory.put("+", new PlusOperator());
+        factory.put(OperatorEnum.MULTIPLY.getValue(), new MultipleOperator());
+        factory.put(OperatorEnum.DIVIDE.getValue(), new DivideOperator());
+        factory.put(OperatorEnum.MINUS.getValue(), new MinusOperator());
+        factory.put(OperatorEnum.PLUS.getValue(), new PlusOperator());
     }
 
     public OperatorFactory() {
---
+++ src/main/java/biz/enums/OperatorEnum.java
@@ -0,0 +1,20 @@
+package biz.enums;
+
+public enum OperatorEnum {
+
+    PLUS("+"),
+    MINUS("-"),
+    DIVIDE("/"),
+    MULTIPLY("*");
+
+    private String value;
+
+    OperatorEnum(String value) {
+        this.value = value;
+    }
+
+
+    public String getValue() {
+        return value;
+    }
+}
