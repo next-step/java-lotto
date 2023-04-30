eafbc621b0a64fcb01a6f458c46235f31a8c59c7
soochan <tncksdl05@gmail.com>
2023-04-30 17:54:38 +0900

    feat : 랜덤한 넘버를 갖는 로또 티켓 객체 추가

---
+++ src/main/java/domain/LottoTicket.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.List;
+
+public class LottoTicket {
+
+  private List<Integer> numbers;
+
+  public LottoTicket(List<Integer> numbers) {
+    this.numbers = numbers;
+  }
+
+}
