fcaba4cba0d9632ace85e889997612147f3b6313
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-01 00:09:31 +0900

    feat: 통계 구현

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -10,30 +9,24 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class Statistics {
     private final WinnerLotto winnerLotto;
-    private final List<Lotto> lottoList;
-    private final Map<Prize, Long> statisticsWinnerMap;
 
-    public Statistics(WinnerLotto winnerLotto, List<Lotto> lottoList) {
+
+    public Statistics(WinnerLotto winnerLotto) {
         this.winnerLotto = winnerLotto;
-        this.lottoList = lottoList;
-        this.statisticsWinnerMap = Collections.unmodifiableMap(doStatistic());
     }
 
-    public Map<Prize, Long> doStatistic() {
+    public Map<Prize, Long> statisticsWinner(List<Lotto> lottoList) {
+
         return lottoList.stream()
                 .map(winnerLotto::findMatchingCount)
                 .map(Prize::valueOf)
-                .filter(prize -> Prize.OUT_OF_PLACE != prize)
                 .collect(groupingBy(Function.identity(), Collectors.counting()));
     }
 
-    public Map<Prize, Long> statisticsWinner() {
-        return statisticsWinnerMap;
-    }
+    public double getProfit(Money money, List<Lotto> lottoList) {
+        Map<Prize, Long> winnerMap = statisticsWinner(lottoList);
 
-    public double getProfit(Money money) {
-
-        int sum = getSumProfit(statisticsWinnerMap);
+        int sum = getSumProfit(winnerMap);
 
         return Money.wons(sum).division(money);
     }
@@ -41,10 +34,7 @@ public class Statistics {
     private int getSumProfit(Map<Prize, Long> winnerMap) {
         return winnerMap.entrySet()
                 .stream()
-                .mapToInt(prize -> {
-                    Prize profitPrize = prize.getKey();
-                    return profitPrize.calculatePriceMoney(profitPrize, Math.toIntExact(prize.getValue()));
-                })
+                .mapToInt(prize -> Prize.calculatePriceMoney(prize.getKey(), Math.toIntExact(prize.getValue())))
                 .sum();
     }
 }
