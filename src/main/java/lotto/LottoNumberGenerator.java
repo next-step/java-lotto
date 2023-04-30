bb1e34d8c0bbdeeb74329b8abf2ddd1e69bd5109
SimYeJu <simyeju@naver.com>
2023-04-30 17:49:33 +0900

    [step2] feat 로또는 1장당 6개의 번호를 랜덤으로 생성한다. 로또 번호는 1부터 45사이의 숫자여야한다.

---
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LottoNumberGenerator {
+
+  private final List<Integer> lottoNumber = new LinkedList<>();
+
+  public void init() {
+    for (int i = 1; i <= 45; i++) {
+      lottoNumber.add(i);
+    }
+  }
+
+  public List<Integer> create() {
+    Collections.shuffle(lottoNumber);
+    return lottoNumber.subList(0, 6);
+  }
+
+}
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,12 +1,32 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LottoTest {
+
+  @Test
+  void 로또는1장당6개의번호를랜덤으로생성한다() {
+    LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+    lottoNumberGenerator.init();
+
+    List<Integer> lottoNumber = lottoNumberGenerator.create();
+
+    assertThat(lottoNumber).hasSize(6);
+  }
+
+  @Test
+  void 로또번호는1부터45사이의숫자여야한다() {
+    LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+    lottoNumberGenerator.init();
+
+    List<Integer> lottoNumber = lottoNumberGenerator.create();
+
+    assertThat(lottoNumber).allMatch(number -> number < 46 && number > 0);
+  }
 
   @Test
   void 로또구입금액의백원단위는무시하고구매갯수를정한다() {
