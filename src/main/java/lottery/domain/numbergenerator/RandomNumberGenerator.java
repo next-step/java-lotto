3533c311801ee3655b75a1ed21e07cb0a9fe23a5
patrickYoo <yhy1045@naver.com>
2023-04-30 14:20:54 +0900

    feat: Random Number 0~45 숫자 생성되던 부분 1 ~ 45 로 정상 생성되도록 수정

--- src/main/java/lottery/domain/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/lottery/domain/numbergenerator/RandomNumberGenerator.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
-    private static final int RANDOM_BOUND = 46;
+    private static final int RANDOM_BOUND = 45;
     private static final Random RANDOM = new Random();
 
     @Override
     public int generatedNumber() {
-        return RANDOM.nextInt(RANDOM_BOUND);
+        return RANDOM.nextInt(RANDOM_BOUND) + 1;
     }
 }
