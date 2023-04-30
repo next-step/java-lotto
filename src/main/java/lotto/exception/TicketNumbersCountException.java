1337d950179e94e130ecfcec0c8c1f60b2bdbca0
dong <dong.22@kakaoenterprise.com>
2023-04-30 12:33:57 +0900

    refactor : 익셉션 문구는 팩트만 담백하게

--- src/main/java/lotto/exception/TicketNumbersCountException.java
+++ src/main/java/lotto/exception/TicketNumbersCountException.java
@@ -2,6 +2,6 @@ package lotto.exception;
 
 public class TicketNumbersCountException extends IllegalArgumentException {
     public TicketNumbersCountException() {
-        throw new IllegalArgumentException("로또는 항상 6개의 숫자를 가져야만 합니다");
+        throw new IllegalArgumentException("Ticket 객체는 항상 6개의 숫자를 가져야만 한다");
     }
 }
