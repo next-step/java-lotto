4701546e11132f5a72bc56816856436b4f7e7409
So-JunHyeok <sojunhyeok@naver.com>
2023-05-01 20:03:21 +0900

    문자열계산기 입력

---
+++ src/test/java/stringCalculator/InputViewTest.java
@@ -0,0 +1,14 @@
+package stringCalculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import stringCalculator.view.InputView;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class InputViewTest {
+
+}
