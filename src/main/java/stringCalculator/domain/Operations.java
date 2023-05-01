178194955002edd492f4f1010ec5d7f6698e2321
PiHyoJeong <shson4007@gmail.com>
2023-05-02 00:12:05 +0900

    Test: 사칙연산 테스트코드 작성

---
+++ src/main/java/stringCalculator/domain/Operations.java
@@ -0,0 +1,21 @@
+package stringCalculator.domain;
+
+import java.util.Arrays;
+
+public class Operations {
+    public int plus(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int minus(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
