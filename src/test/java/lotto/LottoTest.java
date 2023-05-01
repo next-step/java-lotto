4083b294fc1eb29622b8f117303cf817370a81cc
dong <dong.22@kakaoenterprise.com>
2023-05-01 21:06:27 +0900

    test : 테스트코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,7 +23,16 @@ public class LottoTest {
         //given
         consoleInput(
                 "99000" + System.lineSeparator() +
+                        //수동 구매 수
+                        "4"+ System.lineSeparator() +
+                        // 수동구매 번호
                         "1, 2, 3, 4, 5, 6" + System.lineSeparator() +
+                        "8, 21, 23, 41, 42, 43" +System.lineSeparator() +
+                        "3, 5, 11, 16, 32, 38" +System.lineSeparator() +
+                        "7, 11, 16, 35, 36, 44"+System.lineSeparator() +
+                        //지난주 당첨번호
+                        "7, 11, 16, 35, 36, 44"+System.lineSeparator() +
+                        //보너스볼
                         "7"
         );
         //when
