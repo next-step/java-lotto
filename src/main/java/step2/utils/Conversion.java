abc761166bdd101b622598dacf9d32a8a04dfe3b
cohi <tkfka156@gmail.com>
2023-05-02 05:08:54 +0900

    :recycle: refactor(Conversion): 번호 검증 로직 추가

--- src/main/java/step2/utils/Conversion.java
+++ src/main/java/step2/utils/Conversion.java
@@ -5,7 +5,15 @@ public class Conversion {
     private Conversion() {
     }
 
-    public static int stringToInt(String str) {
-        return Integer.parseInt(str);
+    public static int stringToInt(String str){
+        return getStringToNumber(str);
+    }
+
+    private static int getStringToNumber(String str) {
+        try {
+            return Integer.parseInt(str);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
