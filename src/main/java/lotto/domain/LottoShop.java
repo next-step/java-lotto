5214a974fd616b53972a43671fd7e524b8899676
SimJung <tlawjd99gma@gmail.com>
2023-05-01 22:27:17 +0900

    feat : LottoShop private constructor 추가

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -6,6 +6,9 @@ public class LottoShop {
     public static final int LOTTO_PRICE = 1_000;
     private static final int MIN_MONEY_BOUND = 0;
 
+    private LottoShop() {
+    }
+
     public static Lottos sellLotto(int money) {
         if (money < MIN_MONEY_BOUND) {
             throw new InvalidLottoMoneyException("입력한 금액 : ", String.valueOf(money));
