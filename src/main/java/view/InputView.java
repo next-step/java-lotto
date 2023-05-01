91875b6af4a427b6b65f637528a66f398c1994d4
soochan <tncksdl05@gmail.com>
2023-05-01 09:28:55 +0900

    refactor : 투자한 금액에 대한 유효성 검사 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import domain.LottoTicket;
 import java.util.List;
 import java.util.Scanner;
 import util.Converter;
@@ -10,7 +11,26 @@ public class InputView {
 
   public static int scanMoney() {
     System.out.println("구입금액을 입력해 주세요.");
-    return scanner.nextInt();
+    int investMoney = scanner.nextInt();
+    validateInvestMoney(investMoney);
+    return investMoney;
+  }
+
+  private static void validateInvestMoney(int investMoney) {
+    if (isUnderTicketPrice(investMoney)) {
+      throw new IllegalArgumentException("로또 구입 금액은 1000원 이상이어야 합니다.");
+    }
+    if (isNotThousands(investMoney)) {
+      throw new IllegalArgumentException("로또 구입 금액은 1000원 단위여야 합니다.");
+    }
+  }
+
+  private static boolean isNotThousands(int investMoney) {
+    return investMoney % LottoTicket.PRICE != 0;
+  }
+
+  private static boolean isUnderTicketPrice(int investMoney) {
+    return investMoney < LottoTicket.PRICE;
   }
 
   public static List<Integer> scanWinningNumbers() {
