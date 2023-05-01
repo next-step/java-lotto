857fb7e09a7e5be7a3dc2dadd088a0d72f87e029
player-geun <player_geun@naver.com>
2023-05-01 22:49:52 +0900

    refactor : 입력 순서와 로또 번호 클래스로 입력받는 것으로 변경한다.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,23 +1,27 @@
 package lotto.controller;
 
 import lotto.domain.LottoManager;
+import lotto.domain.LottoNumber;
 import lotto.domain.WinningTicket;
 import lotto.dto.LottoNumbersDto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
 
     public static void main(String[] args) {
         LottoManager lottoManager = new LottoManager();
         int purchaseAmount = InputView.inputPurchaseAmount();
-        int bonusBallNumber = InputView.inputBonusBallNumber();
         lottoManager.createLottoNumbers(purchaseAmount);
         OutputView.outputLottoCount(lottoManager.getLottoCount());
         OutputView.outputLottoNumbers(LottoNumbersDto.of(lottoManager.getLottoNumbers()));
 
+        List<Integer> winningNumbers = InputView.inputLastWinningNumber();
+        LottoNumber bonusBallNumber = new LottoNumber(InputView.inputBonusBallNumber());
         WinningTicket winningTicket
-                = new WinningTicket(InputView.inputLastWinningNumber(), bonusBallNumber);
+                = new WinningTicket(winningNumbers, bonusBallNumber);
         OutputView.outputLottoStatistics(lottoManager.calculateLottoStatistics(winningTicket));
     }
 }
