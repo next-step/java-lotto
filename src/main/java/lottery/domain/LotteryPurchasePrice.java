ec993ea789241ba11d45e2109414ce4fb744d827
patrickYoo <yhy1045@naver.com>
2023-05-02 00:54:50 +0900

    chor: 클래스 변수 메시지 동적으로 변경되도록 수정

--- src/main/java/lottery/domain/LotteryPurchasePrice.java
+++ src/main/java/lottery/domain/LotteryPurchasePrice.java
@@ -4,12 +4,13 @@ import static lottery.domain.constant.LotteryNumberRule.LOTTERY_PRICE;
 
 public class LotteryPurchasePrice {
 
-    private static final String INVALID_PRICE_MSG = "로또 최소 구매 금액은 1000원 이상입니다.";
+    private static final String INVALID_PRICE_MSG = String.format("로또 최소 구매 금액은 %d원 이상입니다.",
+            LOTTERY_PRICE.getRuleNumber());
 
     private final int purchasedPrice;
 
     public LotteryPurchasePrice(int purchasedPrice) {
-        if(invalidPrice(purchasedPrice)) {
+        if (invalidPrice(purchasedPrice)) {
             throw new IllegalArgumentException(INVALID_PRICE_MSG);
         }
         this.purchasedPrice = purchasedPrice;
