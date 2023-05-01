ca898516cc7d02106131af1332e40caf94f15f48
soochan <tncksdl05@gmail.com>
2023-05-01 15:30:41 +0900

    refactor : 로또 티켓을 발급하는 기능 추가

---
+++ src/main/java/lotto2/domain/LottoTicket.java
@@ -0,0 +1,16 @@
+package lotto2.domain;
+
+import java.util.List;
+
+public class LottoTicket {
+
+  public static final int TICKET_PRICE = 1000;
+  private final List<Integer> numbers;
+
+  public LottoTicket(List<Integer> numbers) {
+    this.numbers = numbers;
+  }
+
+
+
+}
