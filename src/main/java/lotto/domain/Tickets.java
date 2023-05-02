81fb836d868490665fdd63e5ebf23b5b1c454890
dong <dong.22@kakaoenterprise.com>
2023-05-02 21:58:54 +0900

    feat : 일급콜렉션 래핑 클래스 작성

---
+++ src/main/java/lotto/domain/Tickets.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Tickets {
+    private final List<Ticket> tickets;
+
+    public Tickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+}
