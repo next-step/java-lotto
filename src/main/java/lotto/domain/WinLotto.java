76beebbf72db966683b41c71dd557990e0bcfe99
didrlgus <rlgusdid@naver.com>
2023-05-02 16:59:58 +0900

    refactor: 3단계 - 로또(2등), 코드리뷰 반영
    - 보너스 볼과 당첨 번호가 중복되는 경우 유효성 체크 로직을 생성자로 위임

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -6,14 +6,14 @@ public class WinLotto {
 	private final LottoNumber bonusNumber;
 
 	public WinLotto(WinNumbers winNumbers, LottoNumber bonusNumber) {
+		if (winNumbers.contains(bonusNumber)) {
+			throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
+		}
 		this.winNumbers = winNumbers;
 		this.bonusNumber = bonusNumber;
 	}
 
 	public void calculateScore(PurchasedLottos purchasedLottos) {
-		if (this.winNumbers.contains(this.bonusNumber)) {
-			throw new IllegalArgumentException("보너스 볼은 당첨 번호와 중복될 수 없습니다.");
-		}
 		purchasedLottos.calculateScore(this.winNumbers, this.bonusNumber);
 	}
 }
--- src/test/java/lotto/domain/WinLottoTest.java
+++ src/test/java/lotto/domain/WinLottoTest.java
@@ -10,7 +10,6 @@ public class WinLottoTest {
 	@DisplayName("당첨 번호 목록에 보너스 번호가 포함되는 경우 - 예외 케이스")
 	@Test
 	void test1() {
-		WinLotto winLotto = new WinLotto(new WinNumbers("1, 2, 3, 4, 5, 6"), new LottoNumber(1));
-		assertThatIllegalArgumentException().isThrownBy(() -> winLotto.calculateScore(new PurchasedLottos()));
+		assertThatIllegalArgumentException().isThrownBy(() -> new WinLotto(new WinNumbers("1, 2, 3, 4, 5, 6"), new LottoNumber(1)));
 	}
 }
