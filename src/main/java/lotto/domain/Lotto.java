2750210c30b9f2f92aff5d7f826c9ee3fea85e79
ryan <wsdf25867@gmail.com>
2023-05-03 00:20:39 +0900

    feat : Lotto 작성
    * Lotto 및 테스트 작성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(long... lottoNumbers) {
+        this.lottoNumbers = Arrays.stream(lottoNumbers)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
+    public Lotto(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = new ArrayList<>(lottoNumbers);
+    }
+
+    @Override
+    public String toString() {
+        return lottoNumbers.toString();
+    }
+
+    public Match countMatching(Lotto winningLotto) {
+        return Match.of(winningLotto.lottoNumbers.stream()
+                .filter(lottoNumbers::contains)
+                .count());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoTest {
+
+    @Test
+    void countMatching() {
+        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
+        Lotto match3 = new Lotto(1, 2, 3, 40, 41, 42);
+
+        assertThat(lotto.countMatching(match3)).isEqualTo(Match.THREE);
+    }
+}
