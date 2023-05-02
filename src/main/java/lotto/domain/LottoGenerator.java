c8caf99843ad1663d543070832df50ac78beed1f
ryan <wsdf25867@gmail.com>
2023-05-03 00:21:36 +0900

    feat : LottoGenerator 작성
    * LottoGenerator 작성

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+
+    private static final List<LottoNumber> LOTTO_NUMBERS_CACHE = new ArrayList<>();
+
+    static {
+        for (int i = LottoNumber.MINIMUM_LOTTO_NUMBER; i <= LottoNumber.MAXIMUM_LOTTO_NUMBER; i++) {
+            LOTTO_NUMBERS_CACHE.add(new LottoNumber(i));
+        }
+    }
+
+    public Lotto generate() {
+        Collections.shuffle(LOTTO_NUMBERS_CACHE);
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (int i = 0; i < Lotto.LOTTO_NUMBERS_SIZE; i++) {
+            lottoNumbers.add(LOTTO_NUMBERS_CACHE.get(i));
+        }
+        Collections.sort(lottoNumbers);
+        return new Lotto(lottoNumbers);
+    }
+
+    public Lotto generate(String numbers) {
+        String[] splitNumbers = numbers.split(", ");
+        List<LottoNumber> lottoNumbers = new ArrayList<>();
+        for (String number : splitNumbers) {
+            lottoNumbers.add(new LottoNumber(Integer.parseInt(number)));
+        }
+        return new Lotto(lottoNumbers);
+    }
+
+    public Lottos generate(long count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(generate());
+        }
+        return new Lottos(lottos);
+    }
+}
