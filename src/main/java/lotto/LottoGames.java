e95f3af2acc53a86ef1300c933faf498fb9e1069
이재현 <wincrane2@flow.team>
2023-05-02 19:59:13 +0900

    feat: 당첨금액별 카운트 기능 추가

--- src/main/java/lotto/LottoGames.java
+++ src/main/java/lotto/LottoGames.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class LottoGames {
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_SIZE = 6;
     private final List<Lotto> lottoGameList = new ArrayList<>();
     private Lotto firstLotto;
+    private final int[] result = new int[LOTTO_SIZE];
 
     public LottoGames(List<String> lottoNumberList, String firstLottoNumber) {
         lottoNumberList.forEach(number -> lottoGameList.add(new Lotto(number)));
@@ -34,4 +36,11 @@ public class LottoGames {
         return sum() / (double)(LOTTO_PRICE * lottoGameList.size());
     }
 
+    public void calculatePrizeCount() {
+        lottoGameList.forEach(lotto -> result[lotto.findMatchCount(firstLotto)]++);
+    }
+
+    protected int[] getResult() {
+        return result;
+    }
 }
--- src/test/java/lotto/LottoGamesTest.java
+++ src/test/java/lotto/LottoGamesTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -21,6 +22,14 @@ public class LottoGamesTest {
     public void gameMakeTest() {
         LottoGames lottoGames = new LottoGames(14);
         assertThat(lottoGames.size()).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("상금별 당첨이 몇개가 있는지 확인")
+    public void calculatePrizeCountTest() {
+        LottoGames lottoGames = new LottoGames(Arrays.asList("1, 2, 3, 41, 42, 43"), "1, 2, 3, 4, 5, 6");
+        lottoGames.calculatePrizeCount();
+        assertThat(lottoGames.getResult()).containsExactly(0, 0, 0, 1, 0, 0);
     }
 
 
