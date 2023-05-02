45395efd719ccc310f5e8d0710f4bd86ecf2be07
ShinYeahChan <syeahc@gmail.com>
2023-05-02 20:28:20 +0900

    refactor(test) : PurchaseTest 설명 추가, gitkeep 제거

--- src/test/java/lotto/domain/PurchaseTest.java
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -9,14 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PurchaseTest {
 
-    @ParameterizedTest(name = "[{index}] {0}원 투입")
+    @ParameterizedTest(name = "[{index}] {0}원 투입 (정상금액 투입 확인)")
     @ValueSource(ints = {1000, 5000, 18000, 330000})
     void 구입금액입력(int inputMoney) {
         Purchase purchase = new Purchase(inputMoney);
         assertThat(purchase.money()).isEqualTo(inputMoney);
     }
 
-    @ParameterizedTest(name = "[{index}] {0}원 투입")
+    @ParameterizedTest(name = "[{index}] {0}원 투입 (구입금액이 음수이므로 예외처리)")
     @ValueSource(ints = {-1, -500, -3000, -15000})
     void 구입금액0미만입력(int inputMoney) {
         assertThatThrownBy(() -> new Purchase(inputMoney)).isInstanceOf(IllegalArgumentException.class);
