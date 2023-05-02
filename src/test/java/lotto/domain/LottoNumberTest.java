9c2d9054bbd5a04750935b9ea26f80fb8b9206e5
dong <dong.22@kakaoenterprise.com>
2023-05-02 21:34:53 +0900

    test : 테스트코드 구현

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
+import lotto.exception.LottoNumberOutOfRangeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LottoNumberTest {
@@ -10,18 +14,28 @@ public class LottoNumberTest {
     @Test
     public void rangeOutException() {
         //given
+        int input = 46;
         //when
         //then
-        fail();
+        assertThatThrownBy(() -> {
+            new LottoNumber(input);
+        }).isInstanceOf(LottoNumberOutOfRangeException.class)
+                .hasMessageContaining("LottoNumber 숫자는 1~45 사이만 허용된다");
     }
 
-    @DisplayName("범위 내 숫자 입력시 예외가 발생한다")
+    @DisplayName("범위 내 숫자 입력시 인스턴스가 생성된다")
     @Test
     public void rangeInGood() {
         //given
+        int input = 45;
         //when
+        LottoNumber lottoNumber = new LottoNumber(input);
         //then
-        fail();
+        assertAll("",
+                () -> assertThat(lottoNumber).as("인스턴스 생성에 성공했다").isNotNull(),
+                () -> assertThat(lottoNumber.getLottoNumber()).as("인스턴스 생성에 성공했다").isEqualTo(input)
+        );
+
     }
 
     @DisplayName("동등성 검증 - 숫자가 같으면 같은 인스턴스라고 인식한다")
@@ -29,7 +43,9 @@ public class LottoNumberTest {
     public void equals() {
         //given
         //when
+        LottoNumber lottoNumberA = new LottoNumber(1);
+        LottoNumber lottoNumberB = new LottoNumber(1);
         //then
-        fail();
+        assertEquals(lottoNumberA, lottoNumberB);
     }
 }
