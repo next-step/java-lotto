dbdc38d31100cffdd1a47a11aa74bf1ff40adf3d
june-spring <june.spring@kakaocorp.com>
2023-05-03 04:32:17 +0900

    test: 사칙연산 각각에 대한 테스트

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -11,6 +11,34 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringCalculatorTest {
 
     @Test
+    void 더하기() {
+        List<String> tokens = List.of("2", "+", "3");
+        int result = StringCalculator.calculate(tokens);
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    void 빼기() {
+        List<String> tokens = List.of("2", "-", "7");
+        int result = StringCalculator.calculate(tokens);
+        assertThat(result).isEqualTo(-5);
+    }
+
+    @Test
+    void 곱하기() {
+        List<String> tokens = List.of("0", "*", "7");
+        int result = StringCalculator.calculate(tokens);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void 나누기() {
+        List<String> tokens = List.of("9", "/", "2");
+        int result = StringCalculator.calculate(tokens);
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
     void 분리된_문자를_입력순으로_계산() {
         List<String> tokens = List.of("2", "+", "3", "*", "4", "/", "2");
         int result = StringCalculator.calculate(tokens);
