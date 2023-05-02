251bdf414e62fec85a8c566a9db69837282c3eb4
dong <dong.22@kakaoenterprise.com>
2023-05-02 21:26:09 +0900

    test : 테스트코드 추가

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LottoNumberTest {
+    @DisplayName("범위를 벗어난 숫자 입력시 예외가 발생한다")
+    @Test
+    public void rangeOutException() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("범위 내 숫자 입력시 예외가 발생한다")
+    @Test
+    public void rangeInGood() {
+        //given
+        //when
+        //then
+        fail();
+    }
+
+    @DisplayName("동등성 검증 - 숫자가 같으면 같은 인스턴스라고 인식한다")
+    @Test
+    public void equals() {
+        //given
+        //when
+        //then
+        fail();
+    }
+}
