b00577c0b7199ac3c4aabb19189b63b0c66e1d35
ghkdwlgns612 <ghkdwlgns612@naver.com>
2023-05-02 23:09:20 +0900

    refactor : Test 제거 작업.

--- src/test/java/step2/service/LottoTest.java
+++ src/test/java/step2/service/LottoTest.java
@@ -8,9 +8,9 @@ import step2.vo.LottoNumber;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static step2.service.LottoFixture.*;
+import static step2.service.LottoFixture.lottoNumbers_3개_번호;
+import static step2.service.LottoFixture.lottoNumbers_5개_번호;
 
 public class LottoTest {
 
@@ -26,24 +26,6 @@ public class LottoTest {
         return Stream.of(
                 Arguments.arguments(lottoNumbers_3개_번호()),
                 Arguments.arguments(lottoNumbers_5개_번호())
-        );
-    }
-
-    @ParameterizedTest(name = "로또 번호와 승자 번호 동일한 숫자갯수 확인하는 테스트")
-    @MethodSource("provideLottoNumbersAndWinNumbers")
-    void countWinNumTest(List<LottoNumber> lottoNumbers, List<LottoNumber> winNumbers, LottoNumber bonusNumber, LottoRank expected) {
-        Lotto lotto = new Lotto(lottoNumbers);
-        LottoRank result = lotto.countWinNumber(winNumbers, bonusNumber);
-        assertThat(result).isEqualTo(expected);
-    }
-
-    static Stream<Arguments> provideLottoNumbersAndWinNumbers() {
-        return Stream.of(
-                Arguments.arguments(createDefaultLottoNumber(), lottoNumbers_3개일치(), new LottoNumber(40), LottoRank.FIFTH),
-                Arguments.arguments(createDefaultLottoNumber(), lottoNumbers_4개일치(), new LottoNumber(40), LottoRank.FOURTH),
-                Arguments.arguments(createDefaultLottoNumber(), lottoNumbers_5개일치(), new LottoNumber(40), LottoRank.THIRD),
-                Arguments.arguments(createDefaultLottoNumber(), lottoNumbers_5개일치_보너스(), new LottoNumber(6), LottoRank.SECOND),
-                Arguments.arguments(createDefaultLottoNumber(), lottoNumbers_6개일치(), new LottoNumber(40), LottoRank.FIRST)
         );
     }
 }
