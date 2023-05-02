43264288027d70499f093233c0458d4183fa1aea
june-spring <june.spring@kakaocorp.com>
2023-05-03 03:48:27 +0900

    feat: 분리된 문자를 계산

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -8,4 +8,34 @@ public class StringCalculator {
     public static List<String> devide(String input) {
         return Arrays.stream(input.trim().split(" ")).collect(Collectors.toList());
     }
+
+    public static int calculate(List<String> tokens) {
+        int result = Integer.parseInt(tokens.get(0));
+        String prev = "";
+        tokens = tokens.subList(1, tokens.size());
+        for (String token: tokens) {
+            try {
+                int number = Integer.parseInt(token);
+                switch (prev) {
+                    case "+":
+                        result += number;
+                        break;
+                    case "*":
+                        result *= number;
+                        break;
+                    case "-":
+                        result -= number;
+                        break;
+                    case "/":
+                        result /= number;
+                        break;
+                    default:
+                        throw new RuntimeException("잘못된 입력입니다.");
+                }
+            } catch (NumberFormatException e) {
+                prev = token;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -15,4 +15,11 @@ public class StringCalculatorTest {
         List<String> tokens = StringCalculator.devide(input);
         assertThat(tokens).hasSameElementsAs(List.of("2", "+", "3", "*", "4", "/", "2"));
     }
+
+    @Test
+    void 분리된_문자를_입력순으로_계산() {
+        List<String> tokens = List.of("2", "+", "3", "*", "4", "/", "2");
+        int result = StringCalculator.calculate(tokens);
+        assertThat(result).isEqualTo(10);
+    }
 }
