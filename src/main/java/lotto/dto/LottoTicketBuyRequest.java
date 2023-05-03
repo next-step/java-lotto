ecd68e38b8868864191d73372d73c3969f1c6a84
seongjo24 <kangsj24@naver.com>
2023-05-04 01:43:53 +0900

    로또구매 request 수정 manualNumbers,buyAutomaticCount로 복권 구매정보 입력

--- src/main/java/lotto/dto/LottoTicketBuyRequest.java
+++ src/main/java/lotto/dto/LottoTicketBuyRequest.java
@@ -1,16 +1,21 @@
 package lotto.dto;
 
-public class LottoTicketBuyRequest {
-    private int amount;
-    private int lottoCount;
+import java.util.List;
 
-    public LottoTicketBuyRequest(int amount, int lottoCount) {
-        this.amount = amount;
-        this.lottoCount = lottoCount;
+public class LottoTicketBuyRequest {
+    private List<List<Integer>> manualNumbers;
+    private int buyAutomaticCount;
+
+    public LottoTicketBuyRequest(List<List<Integer>> manualNumbers, int buyAutomaticCount) {
+        this.manualNumbers = manualNumbers;
+        this.buyAutomaticCount = buyAutomaticCount;
     }
 
-  
-    public int getLottoCount() {
-        return lottoCount;
+    public List<List<Integer>> getManualNumbers() {
+        return manualNumbers;
+    }
+
+    public int getBuyAutomaticCount() {
+        return buyAutomaticCount;
     }
 }
