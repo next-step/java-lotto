3f189f9a35686bcc50cb8479b10f8ba0a299584c
seriouskang <scdean110@gmail.com>
2023-05-03 22:20:02 +0900

    feat(LottoNumber): 로또 번호 반환 메소드 추가

--- src/main/java/study/lotto/step2/domain/LottoNumber.java
+++ src/main/java/study/lotto/step2/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package study.lotto.step2.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
     private static final int MINIMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 45;
@@ -8,6 +10,10 @@ public class LottoNumber {
     public LottoNumber(int number) {
         validateNumber(number);
         this.number = number;
+    }
+
+    public int number() {
+        return number;
     }
 
     private void validateNumber(int number) {
@@ -19,5 +25,18 @@ public class LottoNumber {
 
     private boolean isOutOfRange(int number) {
         return number < MINIMUM_NUMBER || MAXIMUM_NUMBER < number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LottoNumber)) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
