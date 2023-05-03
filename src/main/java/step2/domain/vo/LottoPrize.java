23260decd9926f628aac7148b19940b46c3d5969
juhyun <zzang9ha@naver.com>
2023-05-03 22:10:34 +0900

    refactor: (리뷰 반영) 미사용 필드 제거

--- src/main/java/step2/domain/vo/LottoPrize.java
+++ src/main/java/step2/domain/vo/LottoPrize.java
@@ -4,18 +4,16 @@ import java.util.Arrays;
 
 public enum LottoPrize {
 
-    FIRST("1등", 2_000_000_000, 6),
-    SECOND("2등", 1_500_000, 5),
-    THIRD("3등", 50000, 4),
-    FOURTH("4등", 5000, 3),
-    NONE("낙첨", 0, 0);
+    FIRST(2_000_000_000, 6),
+    SECOND(1_500_000, 5),
+    THIRD(50000, 4),
+    FOURTH(5000, 3),
+    NONE(0, 0);
 
-    private final String rank;
     private final int prize;
     private final int prizeCount;
 
-    LottoPrize(String rank, int prize, int prizeCount) {
-        this.rank = rank;
+    LottoPrize(int prize, int prizeCount) {
         this.prize = prize;
         this.prizeCount = prizeCount;
     }
