a27e4edeee469e0949556215115352ca4d99d27d
soochan <tncksdl05@gmail.com>
2023-05-03 18:29:31 +0900

    test : LottoResults 에 대한 테스트 코드 추가

--- src/test/java/lotto3/domain/LottoResultsTest.java
+++ src/test/java/lotto3/domain/LottoResultsTest.java
@@ -24,15 +24,27 @@ class LottoResultsTest {
   }
 
   @Test
-  void 천원으로_로또_구매하면_상금이_5000원인_5등에_당첨되는_검증() {
+  void 천원으로_로또_구매해서_상금_5000원인_5등에_당첨되다면_수익율_계산_검증() {
     Map<Prize, Long> prizeCounts = new HashMap<>();
     prizeCounts.put(Prize.FIFTH, 1L);
 
     LottoResults lottoResults = new LottoResults(prizeCounts);
 
+    double actualProfitRate = 5;
     Money investMoney = new Money(1000);
-    double profitRate = lottoResults.calculateProfitRate(investMoney);
+    double expectedProfitRate = lottoResults.calculateProfitRate(investMoney);
 
-    assertThat(profitRate).isEqualTo(5);
+    assertThat(expectedProfitRate).isEqualTo(actualProfitRate);
+  }
+
+  @Test
+  void 로또결과에서_1등당첨_없는_경우_결과_0개반환() {
+    Map<Prize, Long> prizeCounts = new HashMap<>();
+    prizeCounts.put(Prize.SECOND, 1L);
+    prizeCounts.put(Prize.THIRD, 1L);
+
+    LottoResults lottoResults = new LottoResults(prizeCounts);
+
+    assertThat(lottoResults.getMatchedCount(Prize.FIRST)).isEqualTo(0);
   }
 }
