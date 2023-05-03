7bcd7261dc85ff346bb94c2a498d4c3c72f4f98a
yes <yes.chicken@kakaocorp.com>
2023-05-03 13:41:18 +0900

    feat: 구입금액 검증 로직 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -10,6 +10,9 @@ public class Lottos {
     private final List<Lotto> lottos;
 
     public Lottos(int money, List<Lotto> manualLottos) {
+        if (isInvalidMoney(money)) {
+            throw new IllegalArgumentException("구입 금액은 1000의 배수인 양수여야 합니다.");
+        }
         List<Lotto> lottos = new ArrayList<>(manualLottos);
         int AutoLottoCount = money / LOTTO_PRICE - manualLottos.size();
         for (int i = 0; i < AutoLottoCount; i++) {
@@ -24,6 +27,11 @@ public class Lottos {
 
     public Lottos(List<Lotto> manualLottos) {
         this(LOTTO_PRICE * manualLottos.size(), manualLottos);
+    }
+
+    private boolean isInvalidMoney(int money) {
+        System.out.println(money % 1000);
+        return money <= 0 || money % 1000 != 0;
     }
 
     public Map<Rank, Long> getRankCount(WinNumbers winNumbers) {
