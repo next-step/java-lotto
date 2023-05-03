f00aa909c1dcfd0c42b9f1d055bd0cd5e19aee91
soochan <tncksdl05@gmail.com>
2023-05-03 18:41:51 +0900

    test : Prize에 대한 테스트 코드 추가

--- src/test/java/lotto3/domain/PrizeTest.java
+++ src/test/java/lotto3/domain/PrizeTest.java
@@ -7,15 +7,6 @@ import org.junit.jupiter.api.Test;
 class PrizeTest {
 
   @Test
-  void 당첨번호에_따른_적절한_객체_반환_검증() {
-    assertThat(Prize.valueOfMatchCount(0)).isEqualTo(Prize.NONE);
-    assertThat(Prize.valueOfMatchCount(3)).isEqualTo(Prize.FIFTH);
-    assertThat(Prize.valueOfMatchCount(4)).isEqualTo(Prize.FOURTH);
-    assertThat(Prize.valueOfMatchCount(5)).isEqualTo(Prize.THIRD);
-    assertThat(Prize.valueOfMatchCount(6)).isEqualTo(Prize.FIRST);
-  }
-
-  @Test
   void 당첨번호와_보너스번호에_따른_적절한_객체_반환_검증() {
     assertThat(Prize.valueOfMatchCountAndBonusNumber(5, true)).isEqualTo(Prize.SECOND);
     assertThat(Prize.valueOfMatchCountAndBonusNumber(5, false)).isEqualTo(Prize.THIRD);
