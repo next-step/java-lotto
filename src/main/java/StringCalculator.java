235e6eca5d5493f4db16ec3fd8c072999d8c2f20
choitaehoon <hugebigdream@gmail.com>
2023-05-04 01:13:57 +0900

    refactor : 메소드 분리 작업

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,53 +1,82 @@
 import java.util.Stack;
 
 public class StringCalculator {
+    private static final String BLANK = " ";
+    public static final String GIVEN_STRING_MUST_NOT_BE_NULL = "입력값은 null 일 수 없습니다.";
+    public static final String GIVEN_STRING_MUST_NOT_BE_BLANK = "입력값은 공백 일 수 없습니다.";
+    public static final String PLUS_OPERATOR = "+";
+    public static final String MINUS_OPERATOR = "-";
+    public static final String MULTIPLE_OPERATOR = "*";
+    public static final String DIVIDE_OPERATOR = "/";
+    public static final String OPERATOR_MUST_BE_FOUR_ARITHMETIC_OPERATION = "연산자는 사칙연산만 가능합니다.";
 
-    public static int calculate(String givenValue) {
+    public static int calculateByGivenString(String givenValue) {
+        checkIsNullOrBlank(givenValue);
         Stack<String> stack = new Stack<>();
-        String[] strings = givenValue.split(" ");
+        String[] strings = givenValue.split(BLANK);
         for (String string : strings) {
-            if (string == null) {
-                throw new IllegalArgumentException("입력값은 null 일 수 없습니다.");
-            }
-            if (string.isBlank()) {
-                throw new IllegalArgumentException("입력값은 공백 일 수 없습니다.");
-            }
-
             stack.add(string);
-            if (stack.size() == 3) {
-                calculate(stack);
-            }
+            calculateIsAbleToCalculate(stack);
         }
         return Integer.parseInt(stack.pop());
     }
 
-    private static void calculate(Stack<String> stack) {
+    private static void checkIsNullOrBlank(String givenValue) {
+        if (givenValue == null) {
+            throw new IllegalArgumentException(GIVEN_STRING_MUST_NOT_BE_NULL);
+        }
+        if (givenValue.isBlank()) {
+            throw new IllegalArgumentException(GIVEN_STRING_MUST_NOT_BE_BLANK);
+        }
+    }
+
+    private static void calculateIsAbleToCalculate(Stack<String> stack) {
+        if (stack.size() == 3) {
+            doCalculate(stack);
+        }
+    }
+
+    private static void doCalculate(Stack<String> stack) {
         int lastNumber = Integer.parseInt(stack.pop());
         String operator = stack.pop();
+        checkIsNotFourArithmeticOperations(operator);
         int firstNumber = Integer.parseInt(stack.pop());
-        if (isNotFourArithmeticOperations(operator)) {
-            throw new IllegalArgumentException("연산자는 사칙연산만 가능합니다.");
+
+        if (operator.equals(PLUS_OPERATOR)) {
+            stack.add(String.valueOf(plus(lastNumber, firstNumber)));
         }
-        if (operator.equals("+")) {
-            stack.add(String.valueOf(firstNumber + lastNumber));
+        if (operator.equals(MINUS_OPERATOR)) {
+            stack.add(String.valueOf(minus(lastNumber, firstNumber)));
         }
-        if (operator.equals("-")) {
-            stack.add(String.valueOf(firstNumber - lastNumber));
+        if (operator.equals(DIVIDE_OPERATOR)) {
+            stack.add(String.valueOf(divide(lastNumber, firstNumber)));
         }
-        if (operator.equals("/")) {
-            stack.add(String.valueOf(firstNumber / lastNumber));
-        }
-        if (operator.equals("*")) {
-            stack.add(String.valueOf(firstNumber * lastNumber));
+        if (operator.equals(MULTIPLE_OPERATOR)) {
+            stack.add(String.valueOf(multiple(lastNumber, firstNumber)));
         }
     }
 
-    private static boolean isNotFourArithmeticOperations(String operator) {
-        return !operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/");
+    private static void checkIsNotFourArithmeticOperations(String operator) {
+        if (!operator.equals(PLUS_OPERATOR) && !operator.equals(MINUS_OPERATOR)
+                && !operator.equals(MULTIPLE_OPERATOR) && !operator.equals(DIVIDE_OPERATOR)) {
+            throw new IllegalArgumentException(OPERATOR_MUST_BE_FOUR_ARITHMETIC_OPERATION);
+        }
     }
 
-    public static void main(String[] args) {
-        int value = calculate("2 + 3 * 4 / 2");
-        System.out.println("value = " + value);
+
+    private static int plus(int lastNumber, int firstNumber) {
+        return firstNumber + lastNumber;
+    }
+
+    private static int minus(int lastNumber, int firstNumber) {
+        return firstNumber - lastNumber;
+    }
+
+    private static int divide(int lastNumber, int firstNumber) {
+        return firstNumber / lastNumber;
+    }
+
+    private static int multiple(int lastNumber, int firstNumber) {
+        return firstNumber * lastNumber;
     }
 }
