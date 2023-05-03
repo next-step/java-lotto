e4481469c09c31f13ac991f797647606444c6ada
soochan <tncksdl05@gmail.com>
2023-05-03 18:44:02 +0900

    test : WinningNumbers에 대한 테스트 코드 추가

--- src/test/java/lotto3/domain/WinningNumbersTest.java
+++ src/test/java/lotto3/domain/WinningNumbersTest.java
@@ -9,18 +9,26 @@ class WinningNumbersTest {
 
 
   @Test
-  void 당첨번호가_1번보다_작은경우_실패_테스트() {
+  void 당첨번호가_1번보다_작다면_예외_던지는_테스트() {
     List<Integer> numbers = List.of(0, 1, 2, 3, 4, 5);
-    WinningNumbers winningNumbers = new WinningNumbers(numbers);
-
-    assertThat(winningNumbers.isBetweenOneAndFortyFive()).isFalse();
+    assertThat(catchThrowable(() -> new WinningNumbers(numbers)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("지난 주 당첨 번호는 1~45 사이의 숫자여야 합니다.");
   }
 
   @Test
-  void 당첨번호가_45번보다_큰경우_실패_테스트() {
+  void 당첨번호가_45번보다_큰다면_예외_던지는_테스트() {
     List<Integer> numbers = List.of(1, 2, 3, 4, 5, 46);
-    WinningNumbers winningNumbers = new WinningNumbers(numbers);
+    assertThat(catchThrowable(() -> new WinningNumbers(numbers)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("지난 주 당첨 번호는 1~45 사이의 숫자여야 합니다.");
+  }
 
-    assertThat(winningNumbers.isBetweenOneAndFortyFive()).isFalse();
+  @Test
+  void 당첨번호_개수가_6개를_넘어가면_예외_던지는_검증() {
+    List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+    assertThat(catchThrowable(() -> new WinningNumbers(numbers)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("지난 주 당첨 번호는 6개를 입력해 주세요.");
   }
 }
