cca559dacd6994fa453876bb744fae0fa6aea955
juhyun <zzang9ha@naver.com>
2023-05-03 23:47:51 +0900

    refactor: 테스트 코드 수정

--- src/test/java/step2/domain/entity/LottoTicketsTest.java
+++ src/test/java/step2/domain/entity/LottoTicketsTest.java
@@ -3,24 +3,18 @@ package step2.domain.entity;
 import org.junit.jupiter.api.Test;
 import step2.domain.vo.LottoPrize;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketsTest {
 
     @Test
-    void 발급받은_티켓에_3개가_일치하는_로또가_2장이_존재한다() {
-        final var tickets = createTestTickets();
-
-        final var actual = tickets.getLottoRankCount(LottoPrize.FOURTH);
-
-        assertThat(actual).isEqualTo(2);
-    }
-
-    @Test
     void 로또_당첨_수익률을_계산한다() {
         final var tickets = createTestTickets();
+        final var lottoPrizes = List.of(LottoPrize.FOURTH, LottoPrize.FOURTH);
 
-        final var actual = tickets.getTotalRate();
+        final var actual = tickets.getTotalRate(lottoPrizes);
 
         assertThat(actual).isEqualTo(5.0);
     }
@@ -28,10 +22,10 @@ class LottoTicketsTest {
     private LottoTickets createTestTickets() {
         final var lotto1 = Lotto.winner("1, 2, 3, 4, 5, 6");
         final var lotto2 = Lotto.winner("1, 2, 3, 4, 5, 6");
-        final var ticket1 = new LottoTicket(lotto1, LottoPrize.FOURTH);
-        final var ticket2 = new LottoTicket(lotto2, LottoPrize.FOURTH);
+        final var ticket1 = new LottoTicket(lotto1);
+        final var ticket2 = new LottoTicket(lotto2);
 
-        return new LottoTickets(ticket1, ticket2);
+        return new LottoTickets(List.of(ticket1, ticket2));
     }
 
 }
