4b7018d343d4ccc0ad7a3ab52a170ee208e7e176
soochan <tncksdl05@gmail.com>
2023-05-03 13:17:25 +0900

    refactor : 투자 금액을 받고, 로또 티켓들을 발급하는 로직을 stream api 적극 활용

--- src/main/java/lotto3/util/LottoFactory.java
+++ src/main/java/lotto3/util/LottoFactory.java
@@ -4,6 +4,8 @@ import static lotto3.domain.LottoTicket.TICKET_PRICE;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lotto3.domain.LottoTicket;
 import lotto3.domain.LottoTickets;
 import lotto3.domain.Money;
@@ -12,11 +14,9 @@ public class LottoFactory {
 
   public static LottoTickets createLottoTickets(Money investMoney) {
     int numberOfTickets = investMoney.numberOfTickets();
-    List<LottoTicket> lottoTickets = new ArrayList<>();
-    for (int i = 0; i < numberOfTickets; i++) {
-      List<Integer> randomNumbers = RandomNumberGenerator.generate();
-      lottoTickets.add(new LottoTicket(randomNumbers));
-    }
+    List<LottoTicket> lottoTickets = IntStream.range(0, numberOfTickets)
+        .mapToObj(i -> new LottoTicket(RandomNumberGenerator.generate()))
+        .collect(Collectors.toList());
     return new LottoTickets(lottoTickets);
   }
 
