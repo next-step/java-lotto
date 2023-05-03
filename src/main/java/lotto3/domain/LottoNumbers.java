4e2670c2581402346b6b2e3a4df1daee31e96172
soochan <tncksdl05@gmail.com>
2023-05-03 18:15:07 +0900

    refactor : LottoNumbers에 대한 유효성 검사 로직 추가

--- src/main/java/lotto3/domain/LottoNumbers.java
+++ src/main/java/lotto3/domain/LottoNumbers.java
@@ -4,10 +4,37 @@ import java.util.List;
 
 public class LottoNumbers {
 
+  private static final int LOTTO_NUMBERS_SIZE = 6;
+  private static final int LOTTO_NUMBER_START = 1;
+  private static final int LOTTO_NUMBER_END = 45;
   private final List<Integer> numbers;
 
   public LottoNumbers(List<Integer> numbers) {
     this.numbers = numbers;
+    validateHasSixNumbers();
+    validateIsBetweenOneAndFortyFive();
+    validateHasNoDuplicate();
+  }
+
+  private void validateHasNoDuplicate() {
+    if (numbers.stream().distinct().count() != LOTTO_NUMBERS_SIZE) {
+      throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+    }
+  }
+
+  private void validateIsBetweenOneAndFortyFive() {
+    this.numbers.stream()
+        .filter(number -> number < LOTTO_NUMBER_START || number > LOTTO_NUMBER_END)
+        .findAny()
+        .ifPresent(number -> {
+          throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자여야 합니다.");
+        });
+  }
+
+  private void validateHasSixNumbers() {
+    if (numbers.size() != 6) {
+      throw new IllegalArgumentException("로또 번호는 6개만 가질 수 있습니다.");
+    }
   }
 
   public List<Integer> getNumbers() {
