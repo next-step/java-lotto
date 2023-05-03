64f6241408a725f7b389b5b8c29468e859780cf0
nayoon-kim <belloyv@gmail.com>
2023-05-03 21:58:45 +0900

    로또 출력 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,5 +1,10 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+
+import javax.xml.transform.Result;
+
 public class ResultView {
     public static void printCostOfLotto() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -11,6 +16,14 @@ public class ResultView {
 
     public static void printWinningNumber() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printLottos(Lottos lottos) {
+        lottos.getLottos().stream().forEach(lotto -> ResultView.printLotto(lotto));
+    }
+
+    public static void printLotto(Lotto lotto) {
+        System.out.println(lotto);
     }
 
     public static void printStatistics() {
