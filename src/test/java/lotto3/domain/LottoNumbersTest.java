12f674a00fb5be210abfd058de9bc7854bcd8caf
soochan <tncksdl05@gmail.com>
2023-05-03 18:22:47 +0900

    test : LottoNumbers 에 대한 테스트 추가

--- src/test/java/lotto3/domain/LottoNumbersTest.java
+++ src/test/java/lotto3/domain/LottoNumbersTest.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test;
 class LottoNumbersTest {
 
   @Test
-  void 당첨번호_중에서_몇개를_맞춘경우_검증() {
+  void 당첨번호_중에서_3개를_맞춘경우_검증() {
     LottoNumbers lottoNumbers = new LottoNumbers(List.of(1, 2, 3, 4, 5, 6));
 
-    WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 43, 45, 46));
+    WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 43, 44, 45));
 
     assertThat(lottoNumbers.countMatch(winningNumbers)).isEqualTo(3);
   }
@@ -20,10 +20,29 @@ class LottoNumbersTest {
   void 당첨번호_중에서_하나도_맞히지_않은_경우_검증() {
     LottoNumbers lottoNumbers = new LottoNumbers(List.of(1, 2, 3, 4, 5, 6));
 
-    WinningNumbers winningNumbers = new WinningNumbers(List.of(11, 12, 13, 43, 45, 46));
+    WinningNumbers winningNumbers = new WinningNumbers(List.of(11, 12, 13, 43, 44, 45));
 
     assertThat(lottoNumbers.countMatch(winningNumbers)).isEqualTo(0);
   }
 
+  @Test
+  void 로또번호_생성될때_6자리_숫자로_이루어지지_않다면_예외_발생() {
+    assertThatThrownBy(() -> new LottoNumbers(List.of(1, 2, 3, 4, 5)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("로또 번호는 6개만 가질 수 있습니다.");
+  }
 
+  @Test
+  void 로또번호_생성될때_중복된_숫자가_있으면_예외_발생() {
+    assertThatThrownBy(() -> new LottoNumbers(List.of(1, 2, 3, 4, 5, 5)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("로또 번호는 중복될 수 없습니다.");
+  }
+
+  @Test
+  void 로또번호_생성될때_숫자가_1과_45_사이가_아니면_예외_발생() {
+    assertThatThrownBy(() -> new LottoNumbers(List.of(1, 2, 3, 4, 5, 46)))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("로또 번호는 1~45 사이의 숫자여야 합니다.");
+  }
 }
