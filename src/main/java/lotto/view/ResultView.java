65a8952247051b3bee657bdc34865d8d6544e3ee
ShinYeahChan <syeahc@gmail.com>
2023-05-03 22:11:23 +0900

    refactor : 로또 번호 출력 형태 ResultView에서 직접 지정

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -5,22 +5,32 @@ import lotto.domain.*;
 public class ResultView {
     public static void showMyLottos(MyLottos myLottos) {
         for (int i = 0; i < myLottos.count(); i++) {
-            System.out.println(myLottos.find(i));
+            printNumber(myLottos.find(i).numbers());
         }
         System.out.println();
     }
 
-    public static void showWinLotto(WinLotto winLotto) {
-        System.out.printf("지난 주 당첨 번호 : %s\n\n", winLotto);
+    private static void printNumber(Numbers numbers) {
+        System.out.print("["+numbers.find(0));
+        for (int i = 1; i < numbers.count(); i++) {
+            System.out.print(", "+numbers.find(i));
+        }
+        System.out.println("]");
+    }
+
+    public static void showWinLotto(Numbers numbers) {
+        System.out.print("지난 주 당첨 번호 : ");
+        printNumber(numbers);
+        System.out.println();
     }
 
     public static void showLottoResult(Result result) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.printf("3개 일치 (%d원) - %d개\n", LottoRule.MATCHED_3_NUMBERS_PRIZE, result.getMatched3NumbersCount());
-        System.out.printf("4개 일치 (%d원) - %d개\n", LottoRule.MATCHED_4_NUMBERS_PRIZE, result.getMatched4NumbersCount());
-        System.out.printf("5개 일치 (%d원) - %d개\n", LottoRule.MATCHED_5_NUMBERS_PRIZE, result.getMatched5NumbersCount());
-        System.out.printf("6개 일치 (%d원) - %d개\n", LottoRule.MATCHED_6_NUMBERS_PRIZE, result.getMatched6NumbersCount());
+        System.out.printf("3개 일치 (%d원) - %d개\n", LottoRule.RANK_FOURTH_PRIZE, result.rankFourthCount());
+        System.out.printf("4개 일치 (%d원) - %d개\n", LottoRule.RANK_THIRD_PRIZE, result.rankThirdCount());
+        System.out.printf("5개 일치 (%d원) - %d개\n", LottoRule.RANK_SECOND_PRIZE, result.rankSecondCount());
+        System.out.printf("6개 일치 (%d원) - %d개\n", LottoRule.RANK_FIRST_PRIZE, result.rankFirstCount());
     }
 
     public static void showProfit(double profit) {
