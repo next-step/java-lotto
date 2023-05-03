ed1ca6d12b6dd4d200ac4e4b380b1f9f8bec3787
juhyun <zzang9ha@naver.com>
2023-05-03 23:46:29 +0900

    refactor: Lotto 도메인 테스트 추가

--- src/test/java/step2/domain/entity/LottoTest.java
+++ src/test/java/step2/domain/entity/LottoTest.java
@@ -22,4 +22,14 @@ class LottoTest {
                 () -> Lotto.winner("1, 2, 3, 4, 5")
         );
     }
+
+    @Test
+    void 당첨번호와_로또의_일치하는_번호_갯수를_구한다() {
+        final var winner = Lotto.winner("1, 2, 3, 4, 5, 6");
+        final var compare = new Lotto(1, 2, 3, 4, 7, 8);
+
+        final var actual = winner.countSameNumber(compare);
+
+        assertThat(actual).isEqualTo(4);
+    }
 }
