c91776b2d8fac43d3b44a93a7488a23ce425f46d
EthanKim <hey0507@gmail.com>
2023-05-04 00:36:26 +0900

    Feat : UserInput 문자열 기능 개발 - Blank 를 기준으로 문자열 분리 기능 개발 및 테스트코드 작성

--- src/main/java/domain/UserInput.java
+++ src/main/java/domain/UserInput.java
@@ -1,11 +1,14 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class UserInput {
 
     private static final String[] INVALID_INPUTS = {"\t", "\n", ""};
     static final String EMPTY_ERROR_MESSAGE = "빈 값을 입력하시면 안됩니다.";
+    static final String BLANK_DELIMITER = " ";
 
     private String userInput;
 
@@ -20,4 +23,10 @@ public class UserInput {
         }
         return userInput;
     }
+
+    public List<String> separateInputWithBlank() {
+        return Arrays.
+                asList(this.userInput.split(BLANK_DELIMITER));
+    }
+
 }
--- src/test/java/domain/UserInputTest.java
+++ src/test/java/domain/UserInputTest.java
@@ -1,9 +1,11 @@
 package domain;
 
 import static domain.UserInput.EMPTY_ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class UserInputTest {
@@ -16,4 +18,13 @@ class UserInputTest {
                 .hasMessageContaining(EMPTY_ERROR_MESSAGE);
     }
 
+
+    @ParameterizedTest(name = "공백에 맞춰 문자열을 분리한다.")
+    @CsvSource(value = "안 녕 하 세 요:5", delimiter = ':')
+    void separateTest(String userInput, int expected) {
+        assertThat(new UserInput(userInput)
+                .separateInputWithBlank()
+                .size())
+                .isEqualTo(expected);
+    }
 }
