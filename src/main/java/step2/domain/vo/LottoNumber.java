26fad917746627699a752e7069bf37e8ea5b4188
juhyun <zzang9ha@naver.com>
2023-05-03 22:08:14 +0900

    refactor: (리뷰 반영) Wrapper -> Primitive 타입으로 수정

--- src/main/java/step2/domain/vo/LottoNumber.java
+++ src/main/java/step2/domain/vo/LottoNumber.java
@@ -7,7 +7,7 @@ public class LottoNumber {
     public static final int LOTTO_START_NUMBER = 1;
     public static final int LOTTO_END_NUMBER = 45;
 
-    private final Integer number;
+    private final int number;
 
     public LottoNumber(int number) {
         validate(number);
