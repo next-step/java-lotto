ca3f4b8f110bcc31ac05816c95d591d83fd8a77f
lehdqlsl <lehdqlsl@naver.com>
2023-05-04 12:09:36 +0900

    fix: mvc 패턴 적용

---
+++ src/main/java/controller/LottoController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import lotto.LottoGame;
+import lotto.LottoNumber;
+import lotto.LottoScore;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+import java.util.List;
+
+public class LottoController {
+    private final InputView inputView;
+    private final ResultView resultView;
+    private final LottoGame lottoGame;
+
+    public LottoController(InputView inputView, ResultView resultView, LottoGame lottoGame) {
+        this.inputView = inputView;
+        this.resultView = resultView;
+        this.lottoGame = lottoGame;
+    }
+
+    public void buy() {
+        lottoGame.buyLotto(inputView.price());
+        inputView.buy(lottoGame);
+        resultView.view(lottoGame.lottos());
+    }
+
+    public void result() {
+        LottoScore lottoScore = lottoGame.result(inputView.result());
+        resultView.result(lottoScore);
+    }
+}
--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import controller.LottoController;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
@@ -9,21 +10,16 @@ import java.util.Scanner;
 
 public class LottoMain {
     public static void main(String[] args) {
-        InputView inputView = new InputView(new Scanner(System.in));
-        ResultView resultView = new ResultView();
-        LottoGame lottoGame = new LottoGame(
-                new LottoNumberGenerator(),
-                new ArrayList<>()
+        LottoController lottoController = new LottoController(
+                new InputView(new Scanner(System.in)),
+                new ResultView(),
+                new LottoGame(
+                        new LottoNumberGenerator(),
+                        new ArrayList<>())
         );
 
-        lottoGame.buyLotto(inputView.price());
-        inputView.buy(lottoGame);
-        resultView.view(lottoGame.lottos());
-
-        List<LottoNumber> winNumber = inputView.result();
-        LottoScore lottoScore = lottoGame.result(winNumber);
-
-        resultView.result(lottoScore);
+        lottoController.buy();
+        lottoController.result();
     }
 
 
