ad7aac527cba1ad0a2294db83eb25bc93dff2ef4
lehdqlsl <lehdqlsl@naver.com>
2023-05-04 12:05:23 +0900

    fix: 호출 변경

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -6,30 +6,25 @@ import lotto.view.ResultView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class LottoMain {
     public static void main(String[] args) {
         InputView inputView = new InputView(new Scanner(System.in));
         ResultView resultView = new ResultView();
-        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        LottoGame lottoGame = new LottoGame(
+                new LottoNumberGenerator(),
+                new ArrayList<>()
+        );
 
-        int lottoCount = inputView.price();
-
-        List<Lotto> lottos = new ArrayList<>();
-
-        for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(lottoNumberGenerator.lotto()));
-            resultView.view(lottos);
-        }
+        lottoGame.buyLotto(inputView.price());
+        inputView.buy(lottoGame);
+        resultView.view(lottoGame.lottos());
 
         List<LottoNumber> winNumber = inputView.result();
+        LottoScore lottoScore = lottoGame.result(winNumber);
 
-        List<LottoMatcher> collect = lottos.stream()
-                .map(lotto -> lotto.match(winNumber))
-                .collect(Collectors.toList());
-
-        LottoResult lottoResult = new LottoResult(collect);
-        lottoResult.result();
+        resultView.result(lottoScore);
     }
+
+
 }
