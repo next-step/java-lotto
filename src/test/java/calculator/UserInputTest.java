a1b0758f8bd292a9931984f8cfec23f000bacadf
ian.ja <ian.ja@kakaocorp.com>
2023-05-04 20:46:23 +0900

    step1

---
+++ src/test/java/calculator/UserInputTest.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class UserInputTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    void nullInputValidation(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new UserInput(input));
+    }
+
+    @Test
+    void NonArithmeticOperatorInputValidation() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new UserInput("1 + 2 $ 5"));
+        assertThatIllegalArgumentException().isThrownBy(() -> new UserInput("1 * 3 ^ 9"));
+        assertThat(new UserInput("1 + 2 / 3")).isInstanceOf(UserInput.class);
+        assertThat(new UserInput("1 * 2 / 3 - 4")).isInstanceOf(UserInput.class);
+    }
+}
