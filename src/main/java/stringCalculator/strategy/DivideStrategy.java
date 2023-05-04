dc667e87a6aad2e2e6af84f50fd274ee6328a494
jaewon0913 <jaewon9937@gmail.com>
2023-05-04 10:38:07 +0900

    Feat(Lotto) : 문자열 계산기 개발(1단계_피드백)
    - 0 으로 나누기 수행 시 Exception 전달
    - 테스트 코드 추가

--- src/main/java/stringCalculator/strategy/DivideStrategy.java
+++ src/main/java/stringCalculator/strategy/DivideStrategy.java
@@ -4,6 +4,11 @@ public class DivideStrategy implements CalculatorStrategy {
 
     @Override
     public int applyAsInt(int left, int right) {
+
+        if(right == 0) {
+            throw new ArithmeticException("0 으로는 나눌 수 없습니다.");
+        }
+
         return left / right;
     }
 }
--- src/test/java/stringCalculator/strategy/StrategyTest.java
+++ src/test/java/stringCalculator/strategy/StrategyTest.java
@@ -3,8 +3,10 @@ package stringCalculator.strategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import stringCalculator.domain.Operand;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StrategyTest {
 
@@ -46,5 +48,16 @@ public class StrategyTest {
         DivideStrategy strategy = new DivideStrategy();
 
         assertThat(strategy.applyAsInt(prevNum, nextNum)).isEqualTo(prevNum / nextNum);
+    }
+
+    @DisplayName("나누는 숫자가 0인 경우 에러를 전달한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "2:0"}, delimiter = ':')
+    public void DivideStrategyTest_0으로_나눌시_에러전달(int prevNum, int nextNum) {
+
+        DivideStrategy strategy = new DivideStrategy();
+
+        assertThatThrownBy(() -> strategy.applyAsInt(prevNum, nextNum))
+                .isInstanceOf(ArithmeticException.class);
     }
 }
