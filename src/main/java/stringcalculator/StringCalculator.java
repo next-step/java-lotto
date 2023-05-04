52c53edf463542bb8f715d7c7384aa600d37ba72
issac.park <issac.park@kakaocorp.com>
2023-05-04 21:43:35 +0900

    feat: StringCalculator 및 테스트 코드 뼈대 메서드

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,28 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+    public StringCalculator() {
+
+    }
+
+    public Long calculate(String input) {
+        return 0L;
+    }
+
+    Long add(String a, String b) {
+        return 0L;
+    }
+
+    Long subtract(String a, String b) {
+        return 0L;
+    }
+
+    Long multiply(String a, String b) {
+        return 0L;
+    }
+
+    Long divide(String a, String b) {
+        return 0L;
+    }
+}
---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,34 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    @Test
+    void Given_string_When_calculate_Then_succeed() {
+
+    }
+
+    @Test
+    void Test_add() {
+
+    }
+
+    @Test
+    void Test_subtract() {
+
+    }
+
+    @Test
+    void Test_multiply() {
+
+    }
+
+    @Test
+    void Test_divide() {
+
+    }
+
+}
