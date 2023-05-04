29d25a8098c7df39e1a3b6c63074fe30c2afa9ea
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2023-05-04 21:26:47 +0900

    feat RandomLotteryStrategy 구현

--- src/main/java/lottery/domain/RandomLotteryStrategy.java
+++ src/main/java/lottery/domain/RandomLotteryStrategy.java
@@ -1,11 +1,23 @@
 package lottery.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class RandomLotteryStrategy implements LotteryStrategy {
+    Random random;
     public RandomLotteryStrategy() {
+        random = new Random();
     }
 
     @Override
     public Lottery generate() {
-        return null;
+        List<Integer> lotteryNumbers = new ArrayList<>();
+        for(int i = 0; i < Lotteries.LOTTERY_LENGTH; i++) {
+            lotteryNumbers.add(Lotteries.MIN_LOTTERY_NUMBER + random.nextInt(Lotteries.MAX_LOTTERY_NUMBER - Lotteries.MIN_LOTTERY_NUMBER));
+        }
+        Collections.sort(lotteryNumbers);
+        return Lottery.createLottery(lotteryNumbers);
     }
 }
