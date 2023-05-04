1f38bdbabeeb6c12d9502792deb4301bc0c9770d
KangHo-dev <tiger452003@gmail.com>
2023-05-04 23:28:17 +0900

    feat : UI, main 클래스

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,2 +1,11 @@
-package calculator;public class StringCalculator {
+package calculator;
+
+public class StringCalculator {
+    public static void main(String[] args) {
+        String inputExpression = InputView.inputExpression();
+        Expression expression = ExpressionParser.parse(inputExpression);
+        Number result = expression.calculate();
+        ResultView.printResult(result);
+    }
+
 }
