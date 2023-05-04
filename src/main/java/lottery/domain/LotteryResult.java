e0e85d808944553139fc83257d8b919da5410afa
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2023-05-04 21:00:04 +0900

    feat LotteryResult 구현

--- src/main/java/lottery/domain/LotteryResult.java
+++ src/main/java/lottery/domain/LotteryResult.java
@@ -1,11 +1,25 @@
 package lottery.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LotteryResult {
     private final List<Integer> winNumbers;
 
     public LotteryResult(List<Integer> winNumbers) {
         this.winNumbers = winNumbers;
+    }
+
+    @Override
+    public String toString() {
+        return winNumbers.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LotteryResult that = (LotteryResult) o;
+        return Objects.equals(winNumbers, that.winNumbers);
     }
 }
