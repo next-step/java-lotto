8b39de9cfc76d1a1b361b49231961c03acddec9c
EthanKim <hey0507@gmail.com>
2023-05-05 19:29:07 +0900

    Test : 1개의 문자를 list로 변경하는 부분 singletonList 로 변경

--- src/test/java/domain/OperatorTest.java
+++ src/test/java/domain/OperatorTest.java
@@ -3,7 +3,7 @@ package domain;
 import static domain.Operator.IS_NOT_SYMBOL_ERROR_MESSAGE;
 import static domain.Operator.validateOperatorSymbol;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,8 +13,8 @@ class OperatorTest {
 
     @ParameterizedTest(name = "사칙연산 부호가 아닐경우 exception 발생, {0} 의 경우")
     @ValueSource(strings = {"_", "!", "@", "#"})
-    void OperatorTest(String input) {
-        List<String> stringList = Arrays.asList(input);
+    void operatorTest(String input) {
+        List<String> stringList = Collections.singletonList(input);
         Assertions.assertThatThrownBy(() -> validateOperatorSymbol(stringList))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(IS_NOT_SYMBOL_ERROR_MESSAGE);
