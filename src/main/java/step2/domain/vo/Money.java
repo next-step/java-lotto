fc81ad6453cff5b1326f41c0342befb8d85c77fc
juhyun <zzang9ha@naver.com>
2023-05-05 13:57:14 +0900

    refactor: 매직 넘버 제거

--- src/main/java/step2/domain/vo/Money.java
+++ src/main/java/step2/domain/vo/Money.java
@@ -2,6 +2,8 @@ package step2.domain.vo;
 
 public class Money {
 
+    private static final int MINIMUM_AMOUNT = 0;
+
     private final int money;
 
     public Money(int money) {
@@ -10,7 +12,7 @@ public class Money {
     }
 
     private void validate(int money) {
-        if (money < 0) {
+        if (money < MINIMUM_AMOUNT) {
             throw new IllegalStateException("구입금액은 0원보다 커야 합니다. 현재 구입금액: " + money);
         }
     }
