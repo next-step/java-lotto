4e05a1de617aa1fad40c7e13ee72599723ce8dc3
juhyun <zzang9ha@naver.com>
2023-05-05 14:29:49 +0900

    feat: 2등 보너스 볼 추첨 로직 추가, 메시지 전달 로직 추가

--- src/main/java/step2/main/Application.java
+++ src/main/java/step2/main/Application.java
@@ -2,7 +2,6 @@ package step2.main;
 
 import step2.domain.vo.LottoPrize;
 import step2.domain.vo.Money;
-import step2.service.LottoPrizeChecker;
 import step2.service.LottoTicketIssuance;
 import step2.view.InputView;
 import step2.view.OutputView;
@@ -13,13 +12,13 @@ public class Application {
 
     public static void main(String[] args) {
         final var money = InputView.inputMoney();
-        final var lottoTickets
-                = LottoTicketIssuance.issues(new Money(money));
+        final var lottoTickets = LottoTicketIssuance.issues(new Money(money));
 
         OutputView.printTickets(lottoTickets);
 
         final var lastLottoNumbers = InputView.inputLastLottoNumbers();
-        List<LottoPrize> lottoPrizes = LottoPrizeChecker.checker(lottoTickets.getLottoTickets(), lastLottoNumbers);
+        final var bonusBall = InputView.inputBonusLottoNumber();
+        List<LottoPrize> lottoPrizes = lottoTickets.checkLottoPrize(lastLottoNumbers, bonusBall);
 
         OutputView.printWinnerStatistics(lottoTickets, lottoPrizes);
     }
