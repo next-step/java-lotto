cd0e62832de5e81f231daa25ee4ce8b6cbda6120
juhyun <zzang9ha@naver.com>
2023-05-05 15:51:46 +0900

    refactor: 클래스 네이밍 변경

--- src/test/java/step2/service/LottosIssuanceTest.java
+++ src/test/java/step2/service/LottosIssuanceTest.java
@@ -8,23 +8,23 @@ import step2.domain.vo.Money;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class LottoTicketIssuanceTest {
+public class LottosIssuanceTest {
 
     @ParameterizedTest
     @CsvSource(value = {"14000:14", "5500:5", "12000:12"}, delimiter = ':')
     void 로또_티켓은_구입금액에서_로또금액을_나눈만큼_발급된다(int money, int quantity) {
-        final var tickets = LottoTicketIssuance.issues(new Money(money));
+        final var lottos = LottosIssuance.issues(new Money(money));
 
-        final var lottoTickets = tickets.getLottoTickets();
+        final var lottoList = lottos.getLottos();
 
-        assertThat(lottoTickets).hasSize(quantity);
+        assertThat(lottoList).hasSize(quantity);
     }
 
     @Test
     void 로또_티켓_발급시_구입금액이_로또금액보다_작을경우_예외를_던진다() {
         assertThrows(
                 IllegalStateException.class,
-                () -> LottoTicketIssuance.issues(new Money(900))
+                () -> LottosIssuance.issues(new Money(900))
         );
     }
 }
