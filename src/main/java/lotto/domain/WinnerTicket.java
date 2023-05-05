6cb07a4389a30a6b525fb97a6900d9609489431a
dong <dong.22@kakaoenterprise.com>
2023-05-05 20:11:38 +0900

    chore : 불필요한 method 제거

--- src/main/java/lotto/domain/WinnerTicket.java
+++ src/main/java/lotto/domain/WinnerTicket.java
@@ -17,32 +17,6 @@ public class WinnerTicket {
         return ticket.includeNumber(this.bonusNumber);
     }
 
-    public int countWinner(Tickets challengeTickets, Prize prize) {
-        int count = 0;
-        for (Ticket ticket : challengeTickets.allTickets()) {
-            count = count + (isWinner(ticket, prize) ? 1 : 0);
-        }
-        return count;
-    }
-
-    private boolean isWinner(Ticket challengerTicket, Prize prize) {
-        return matchCount(challengerTicket, prize) && checkBonusCondition(challengerTicket, prize);
-    }
-
-    private boolean checkBonusCondition(Ticket ticket, Prize prize) {
-        if (prize == Prize.SECOND) {
-            return this.includeBonus(ticket);
-        }
-        if (prize == Prize.THIRD) {
-            return !this.includeBonus(ticket);
-        }
-        return true;
-    }
-
-    private boolean matchCount(Ticket ticket, Prize prize) {
-        return prize.isMatch(this.ticket.countMatchNumbers(ticket));
-    }
-
     public Prize checkLucky(Ticket thatTicket) {
         int matchCount = this.ticket.countMatchNumbers(thatTicket);
         boolean containsBonus = thatTicket.includeNumber(bonusNumber);
