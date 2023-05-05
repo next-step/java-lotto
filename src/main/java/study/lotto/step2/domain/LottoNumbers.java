f408e7aee58b8e2c1ac15e6d9eb4b7e82b1fa174
seriouskang <scdean110@gmail.com>
2023-05-05 09:39:17 +0900

    feat(LottoNumbers): 로또 번호 반환 시, 정렬하여 반환

--- src/main/java/study/lotto/step2/domain/LottoNumbers.java
+++ src/main/java/study/lotto/step2/domain/LottoNumbers.java
@@ -1,11 +1,18 @@
 package study.lotto.step2.domain;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class LottoNumbers {
     private static final int NUMBER_OF_SELECT = 6;
     private final Set<LottoNumber> lottoNumbers;
+
+    public LottoNumbers(Integer... lottoNumbers) {
+        this(of(lottoNumbers));
+    }
 
     public LottoNumbers(Set<LottoNumber> lottoNumbers) {
         validateLottoNumbers(lottoNumbers);
@@ -15,12 +22,18 @@ public class LottoNumbers {
     public Set<Integer> numbers() {
         return lottoNumbers.stream()
                 .map(LottoNumber::number)
-                .collect(Collectors.toUnmodifiableSet());
+                .collect(Collectors.collectingAndThen(Collectors.toCollection(TreeSet::new), Collections::unmodifiableSet));
     }
 
     private void validateLottoNumbers(Set<LottoNumber> lottoNumbers) {
         if(lottoNumbers.size() != NUMBER_OF_SELECT) {
             throw new IllegalArgumentException("로또 번호 선택 갯수는 " + NUMBER_OF_SELECT + "개입니다: " + lottoNumbers.size());
         }
+    }
+
+    private static Set<LottoNumber> of(Integer... lottoNumbers) {
+        return Arrays.stream(lottoNumbers)
+                .map(LottoNumber::of)
+                .collect(Collectors.toUnmodifiableSet());
     }
 }
--- src/test/java/study/lotto/step2/domain/LottoNumbersTest.java
+++ src/test/java/study/lotto/step2/domain/LottoNumbersTest.java
@@ -3,10 +3,6 @@ package study.lotto.step2.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,36 +11,21 @@ class LottoNumbersTest {
     @DisplayName("로또 번호가 6개가 아닐 경우, IllegalArgumentException 예외 발생")
     void invalid_lotto_number_size_the_throw_IllegalArgumentException() {
         // given
-        Set<LottoNumber> illegalSizeLottoNumbers = sequenceLottoNumberSet(1, 7);
+        Integer[] invalidLottoNumbers = {1, 2, 3, 4, 5, 6, 7};
 
         // when, then
-        assertThatThrownBy(() -> new LottoNumbers(illegalSizeLottoNumbers))
+        assertThatThrownBy(() -> new LottoNumbers(invalidLottoNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호 선택 갯수는 6개입니다: " + illegalSizeLottoNumbers.size());
+                .hasMessage("로또 번호 선택 갯수는 6개입니다: " + invalidLottoNumbers.length);
     }
 
     @Test
-    @DisplayName("로또 번호 Set 반환")
+    @DisplayName("정렬된 로또 번호 Set 반환")
     void numbers() {
         // given
-        int startNumber = 1;
-        int endNumber = 6;
-        LottoNumbers lottoNumbers = new LottoNumbers(sequenceLottoNumberSet(startNumber, endNumber));
+        LottoNumbers lottoNumbers = new LottoNumbers(1, 6, 2, 5, 3, 4);
 
         // when, then
-        assertThat(lottoNumbers.numbers()).isEqualTo(unmodifiableSequenceIntegerSet(startNumber, endNumber));
-    }
-
-    private Set<Integer> unmodifiableSequenceIntegerSet(int start, int end) {
-        return IntStream.rangeClosed(start, end)
-                .boxed()
-                .collect(Collectors.toUnmodifiableSet());
-    }
-
-    private Set<LottoNumber> sequenceLottoNumberSet(int start, int end) {
-        return IntStream.rangeClosed(start, end)
-                .boxed()
-                .map(LottoNumber::of)
-                .collect(Collectors.toSet());
+        assertThat(lottoNumbers.numbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 }
