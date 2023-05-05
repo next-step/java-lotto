b44381364a56a5353c40966744a8b8448700d6d0
dong <dong.22@kakaoenterprise.com>
2023-05-05 14:59:03 +0900

    refactor : 등수 계산 로직 수정

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -5,7 +5,9 @@ public enum Prize {
     SECOND(5, 30_000_000, true),
     THIRD(5, 1_500_000, false),
     FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false);
+    FIFTH(3, 5_000, false),
+    BOOM(0, 0, false);
+
 
     private final int matchCount;
     private final int prizeAmount;
@@ -15,6 +17,28 @@ public enum Prize {
         this.matchCount = matchCount;
         this.prizeAmount = prizeAmount;
         this.bonusNumberMatch = bonusNumberMatch;
+    }
+
+    public static Prize calculatePrize(int matchCount, boolean containsBonuns) {
+        if(matchCount < FIFTH.matchCount) {
+            return BOOM;
+        }
+        if(matchCount == FIFTH.matchCount) {
+            return FIFTH;
+        }
+        if(matchCount == FOURTH.matchCount) {
+            return FOURTH;
+        }
+        if((matchCount == THIRD.matchCount) && (containsBonuns == THIRD.bonusNumberMatch )){
+            return THIRD;
+        }
+        if((matchCount == SECOND.matchCount) && (containsBonuns == SECOND.bonusNumberMatch )){
+            return SECOND;
+        }
+        if(matchCount == FIRST.matchCount) {
+            return FIRST;
+        }
+        throw new RuntimeException("알수없는 계산 에러..");
     }
 
     public boolean isMatch(int matchCount) {
--- src/main/java/lotto/domain/Statics.java
+++ src/main/java/lotto/domain/Statics.java
@@ -1,72 +1,63 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Statics {
     private static final String PROFIT_MESSAGE = "(손익분기점이 1이기 때문에 결과적으로 이득이라는 의미임)";
     private static final String LOSS_MESSAGE = "(손익분기점이 1이기 때문에 결과적으로 손해라는 의미임)";
 
-    private final int countFifth;
-    private final int countFourth;
-    private final int countThird;
-    private final int countSecond;
-    private final int countFirst;
-    private final int income;
-    private final double profitRatio;
+    private final Map<Prize, Integer> prizeIntegerMap;
+    private final int totalTryTicketCount;
+    //private final int income;
+    //private final double profitRatio;
 
     public Statics(Tickets tickets, WinnerTicket winnerTicket) {
-        this.countFirst = aggregate(tickets, winnerTicket, Prize.FIRST);
-        this.countSecond = aggregate(tickets, winnerTicket, Prize.SECOND);
-        this.countThird = aggregate(tickets, winnerTicket, Prize.THIRD);
-        this.countFourth = aggregate(tickets, winnerTicket, Prize.FOURTH);
-        this.countFifth = aggregate(tickets, winnerTicket, Prize.FIFTH);
-        this.income = aggregateIncome();
-        this.profitRatio = aggregateProfitRatio(tickets.count(), this.income);
+        totalTryTicketCount = tickets.count();
+        prizeIntegerMap = aggregateBetter(tickets, winnerTicket);
+//        this.countFirst = aggregate(tickets, winnerTicket, Prize.FIRST);
+//        this.countSecond = aggregate(tickets, winnerTicket, Prize.SECOND);
+//        this.countThird = aggregate(tickets, winnerTicket, Prize.THIRD);
+//        this.countFourth = aggregate(tickets, winnerTicket, Prize.FOURTH);
+//        this.countFifth = aggregate(tickets, winnerTicket, Prize.FIFTH);
+        //this.income = aggregateIncome();
+        //this.profitRatio = aggregateProfitRatio(tickets.count(), this.income);
+    }
+
+    private Map<Prize, Integer> aggregateBetter(Tickets tickets, WinnerTicket winnerTicket) {
+        Map<Prize, Integer> prizeIntegerMap = new HashMap<>();
+        for( Ticket ticket : tickets.getTickets()) {
+            Prize prize = winnerTicket.checkLucky(ticket);
+            prizeIntegerMap.put(prize, prizeIntegerMap.getOrDefault(prize, 0) + 1);
+        }
+        return prizeIntegerMap;
     }
 
     private double aggregateProfitRatio(int ticketCount, int income) {
         return (double) income / (double) (ticketCount * 1000);
     }
 
-    private int aggregateIncome() {
-        return Prize.FIRST.calculatePrize(this.countFirst) +
-                Prize.SECOND.calculatePrize(this.countSecond) +
-                Prize.THIRD.calculatePrize(this.countThird) +
-                Prize.FOURTH.calculatePrize(this.countFourth) +
-                Prize.FIFTH.calculatePrize(this.countFifth);
-    }
-
     public int getIncome() {
-        return income;
+        return Prize.FIRST.calculatePrize(prizeIntegerMap.get(Prize.FIRST)) +
+                Prize.SECOND.calculatePrize(prizeIntegerMap.get(Prize.SECOND)) +
+                Prize.THIRD.calculatePrize(prizeIntegerMap.get(Prize.THIRD)) +
+                Prize.FOURTH.calculatePrize(prizeIntegerMap.get(Prize.FOURTH)) +
+                Prize.FIFTH.calculatePrize(prizeIntegerMap.get(Prize.FIFTH));
     }
 
-    private int aggregate(Tickets challengerTickets, WinnerTicket winnerTicket, Prize prize) {
-        return winnerTicket.countWinner(challengerTickets, prize);
+//    private int aggregate(Tickets challengerTickets, WinnerTicket winnerTicket, Prize prize) {
+//        return winnerTicket.countWinner(challengerTickets, prize);
+//    }
+
+    public int countPrize(Prize prize) {
+        return this.prizeIntegerMap.get(prize);
     }
 
-    public int getCountFifth() {
-        return countFifth;
-    }
-
-    public int getCountFourth() {
-        return countFourth;
-    }
-
-    public int getCountThird() {
-        return countThird;
-    }
-
-    public int getCountFirst() {
-        return countFirst;
-    }
-
-    public double getProfitRatio() {
-        return profitRatio;
+    public double profitRatio() {
+        return aggregateProfitRatio(this.totalTryTicketCount, this.getIncome());
     }
 
     public String breakEvenMessage() {
-        return this.profitRatio >= 1 ? PROFIT_MESSAGE : LOSS_MESSAGE;
-    }
-
-    public int getCountSecond() {
-        return this.countSecond;
+        return this.profitRatio() >= 1 ? PROFIT_MESSAGE : LOSS_MESSAGE;
     }
 }
