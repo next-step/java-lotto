8f7a4d3790aad4df43be3755e18823cf126a1087
dong <dong.22@kakaoenterprise.com>
2023-05-05 20:20:24 +0900

    refactor : 잘못된 티켓 사용 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -9,21 +9,18 @@ import lotto.present.dto.IncomePresentDTO;
 import lotto.present.dto.WinnerPresentDTO;
 
 public class Lotto {
-
-
     public static void main(String[] args) {
         InputPresent inputPresent = new InputPresent();
         OutputPresent outputPresent = new OutputPresent();
-        Tickets tickets = new Tickets();
-        //구매수량
-        int totalPurchaseCount = inputPresent.purchaseCount();
 
+        int totalPurchaseCount = inputPresent.purchaseCount();
         Tickets manualTickets = inputPresent.manualPurchases();
-        Tickets autoTickets = Tickets.automaticIssue(totalPurchaseCount - tickets.count());
+        Tickets autoTickets = Tickets.automaticIssue(totalPurchaseCount - manualTickets.count());
         outputPresent.presentTickets(manualTickets, autoTickets);
+        autoTickets.add(manualTickets);
 
         WinnerTicket winnerTicket = new WinnerTicket(inputPresent.winningNumbers(), inputPresent.bonusNumber());
-        Statics statics = new Statics(tickets, winnerTicket);
+        Statics statics = new Statics(autoTickets, winnerTicket);
         outputPresent.presentWinners(new WinnerPresentDTO(statics));
         outputPresent.presentIncome(new IncomePresentDTO(statics));
     }
