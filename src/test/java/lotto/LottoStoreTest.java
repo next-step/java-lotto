0008f9ad9e8728a8c79c39ff75baebf6b01bac9e
KimWooHun <dngjs1@naver.com>
2023-05-05 18:01:54 +0900

    test : 로또_step2 구입금액입력 및 금액 1000원단위 검사 테스트

---
+++ src/test/java/lotto/LottoStoreTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import lotto.domain.LottoStore;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LottoStoreTest {
+    @Test
+    @DisplayName("입력금액만큼 로또가 생성되는지 테스트(1000원에 로또 1장)")
+    void buyLottoTest() {
+        int money = 5000;
+        assertThat(LottoStore.buyLotto(money)).isEqualTo(money/1000);
+    }
+}
