70b37c531a7ba6c05732a80762df23db2327ff86
seriouskang <scdean110@gmail.com>
2023-05-05 09:44:02 +0900

    refactor(Lotto): Lotto 클래스 인스턴스 변수 타입 변경(Set<LottoNumber> -> LottoNumbers)

--- src/main/java/study/lotto/step2/domain/Lotto.java
+++ src/main/java/study/lotto/step2/domain/Lotto.java
@@ -1,30 +1,21 @@
 package study.lotto.step2.domain;
 
-import java.util.Collections;
 import java.util.Objects;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class Lotto {
-    private static final int NUMBER_OF_SELECT = 6;
-    private final Set<LottoNumber> lottoNumbers;
+    private final LottoNumbers lottoNumbers;
 
-    public Lotto(Set<LottoNumber> lottoNumbers) {
-        validateLottoNumbers(lottoNumbers);
+    public Lotto(Integer... lottoNumbers) {
+        this(new LottoNumbers(lottoNumbers));
+    }
+
+    public Lotto(LottoNumbers lottoNumbers) {
         this.lottoNumbers = lottoNumbers;
     }
 
     public Set<Integer> numbers() {
-        return lottoNumbers.stream()
-                .map(LottoNumber::number)
-                .collect(Collectors.collectingAndThen(Collectors.toCollection(TreeSet::new), Collections::unmodifiableSet));
-    }
-
-    private void validateLottoNumbers(Set<LottoNumber> lottoNumbers) {
-        if(lottoNumbers.size() != NUMBER_OF_SELECT) {
-            throw new IllegalArgumentException("로또 번호 선택 갯수는 " + NUMBER_OF_SELECT + "개입니다: " + lottoNumbers.size());
-        }
+        return lottoNumbers.numbers();
     }
 
     @Override
--- src/test/java/study/lotto/step2/domain/LottoTest.java
+++ src/test/java/study/lotto/step2/domain/LottoTest.java
@@ -3,45 +3,16 @@ package study.lotto.step2.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
     @Test
-    @DisplayName("로또 번호가 6개가 아닐 경우, IllegalArgumentException 예외 발생")
-    void invalid_lotto_number_size_the_throw_IllegalArgumentException() {
-        // given
-        Set<LottoNumber> illegalSizeLottoNumbers = sequenceLottoNumberSet(1, 7);
-
-        // when, then
-        assertThatThrownBy(() -> new Lotto(illegalSizeLottoNumbers))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호 선택 갯수는 6개입니다: " + illegalSizeLottoNumbers.size());
-    }
-
-    @Test
-    @DisplayName("선택한 로또 번호 오름차순 정렬 후 반환")
+    @DisplayName("정렬된 로또 번호 반환")
     void numbers() {
         // given
-        Lotto lotto = new Lotto(sequenceLottoNumberSet(1, 6));
+        Lotto lotto = new Lotto(1, 6, 2, 5, 3, 4);
 
-        // when
-        Set<Integer> lottoNumbers = lotto.numbers();
-
-        // then
-        Set<Integer> expectedLottoNumbers = Collections.unmodifiableSortedSet(new TreeSet<>(Set.of(1, 2, 3, 4, 5, 6)));
-        assertThat(lottoNumbers).isEqualTo(expectedLottoNumbers);
-    }
-
-    private Set<LottoNumber> sequenceLottoNumberSet(int start, int end) {
-        return IntStream.rangeClosed(start, end)
-                .boxed()
-                .map(LottoNumber::new)
-                .collect(Collectors.toSet());
+        // when, then
+        assertThat(lotto.numbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 }
