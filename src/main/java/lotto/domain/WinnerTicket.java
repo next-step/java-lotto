0d264eba0e1e2eabc290bf29c427021aeaf5ea1c
dong <dong.22@kakaoenterprise.com>
2023-05-06 00:30:05 +0900

    chore : 스타일 수정

--- src/main/java/lotto/domain/WinnerTicket.java
+++ src/main/java/lotto/domain/WinnerTicket.java
@@ -1,12 +1,21 @@
 package lotto.domain;
 
+import lotto.exception.LottoNumberDuplicatedException;
+
 public class WinnerTicket {
     private final Ticket ticket;
     private final LottoNumber bonusNumber;
 
     public WinnerTicket(Ticket ticket, LottoNumber bonusNumber) {
         this.ticket = ticket;
+        validateBonus(ticket, bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonus(Ticket ticket, LottoNumber bonusNumber) {
+        if (ticket.includeNumber(bonusNumber)) {
+            throw new LottoNumberDuplicatedException();
+        }
     }
 
     public Ticket ticket() {
