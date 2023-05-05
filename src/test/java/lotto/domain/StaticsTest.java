c6c557391b26c007e297d897ecd1c1dad62f8079
dong <dong.22@kakaoenterprise.com>
2023-05-05 14:59:13 +0900

    test : 등수 계산 로직 수정

--- src/test/java/lotto/domain/StaticsTest.java
+++ src/test/java/lotto/domain/StaticsTest.java
@@ -54,7 +54,7 @@ public class StaticsTest {
         //given
         int answer = 1;
         //when
-        int countFirst = staticsFixture.getCountFirst();
+        int countFirst = staticsFixture.countPrize(Prize.FIRST);
         //then
         assertEquals(answer, countFirst);
     }
@@ -65,7 +65,7 @@ public class StaticsTest {
         //given
         int expect = 2;
         //when
-        int actual = staticsFixture.getCountSecond();
+        int actual = staticsFixture.countPrize(Prize.SECOND);
         //then
         assertEquals(expect, actual);
     }
@@ -76,7 +76,7 @@ public class StaticsTest {
         //given
         int answer = 3;
         //when
-        int countFirst = staticsFixture.getCountThird();
+        int countFirst = staticsFixture.countPrize(Prize.THIRD);
         //then
         assertEquals(answer, countFirst);
     }
@@ -87,7 +87,7 @@ public class StaticsTest {
         //given
         int answer = 4;
         //when
-        int countFirst = staticsFixture.getCountFourth();
+        int countFirst = staticsFixture.countPrize(Prize.FOURTH);
         //then
         assertEquals(answer, countFirst);
     }
@@ -98,7 +98,7 @@ public class StaticsTest {
         //given
         int answer = 5;
         //when
-        int countFirst = staticsFixture.getCountFifth();
+        int countFirst = staticsFixture.countPrize(Prize.FIFTH);
         //then
         assertEquals(answer, countFirst);
     }
@@ -112,11 +112,11 @@ public class StaticsTest {
         int actual = staticsFixture.getIncome();
         //then
         assertAll("당첨금액 = Prize.FIRST.prizeCalculate(1) + Prize.SECOND.prizeCalculate(2) + Prize.THIRD.prizeCalculate(3) + Prize.FOURTH.prizeCalculate(4) + Prize.FIFTH.prizeCalculate(5)",
-                () -> assertEquals(2_000_000_000, Prize.FIRST.calculatePrize(staticsFixture.getCountFirst())),
-                () -> assertEquals(30_000_000 * 2, Prize.SECOND.calculatePrize(staticsFixture.getCountSecond())),
-                () -> assertEquals(1_500_000 * 3, Prize.THIRD.calculatePrize(staticsFixture.getCountThird())),
-                () -> assertEquals(50_000 * 4, Prize.FOURTH.calculatePrize(staticsFixture.getCountFourth())),
-                () -> assertEquals(5_000 * 5, Prize.FIFTH.calculatePrize(staticsFixture.getCountFifth())),
+                () -> assertEquals(2_000_000_000, Prize.FIRST.calculatePrize(staticsFixture.countPrize(Prize.FIRST))),
+                () -> assertEquals(30_000_000 * 2, Prize.SECOND.calculatePrize(staticsFixture.countPrize(Prize.SECOND))),
+                () -> assertEquals(1_500_000 * 3, Prize.THIRD.calculatePrize(staticsFixture.countPrize(Prize.THIRD))),
+                () -> assertEquals(50_000 * 4, Prize.FOURTH.calculatePrize(staticsFixture.countPrize(Prize.FOURTH))),
+                () -> assertEquals(5_000 * 5, Prize.FIFTH.calculatePrize(staticsFixture.countPrize(Prize.FIFTH))),
                 () -> assertEquals(expected, actual)
         );
     }
@@ -127,7 +127,7 @@ public class StaticsTest {
         //given
         int answerX1000 = 137648333;
         //when
-        int ratioX1000 = (int) (staticsFixture.getProfitRatio() * 1000);
+        int ratioX1000 = (int) (staticsFixture.profitRatio() * 1000);
         //then
         assertThat(ratioX1000).as("double type 은 equal 평가가 곤란해서 1000을 곱한다음 int 형식으로 캐스팅하여 검증했습니다").isEqualTo(answerX1000);
         assertThat(ratioX1000).as("투자금 13게임[13000₩], 수익금[2004725000₩], 투자대비 수익비율 [154209.615385]").isEqualTo(answerX1000);
