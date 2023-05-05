0a24aaeb40ec7f0e1f4fbdd28ebaaa7a92866733
dong <dong.22@kakaoenterprise.com>
2023-05-05 20:11:49 +0900

    test : 테스트코드 수정

--- src/test/java/lotto/domain/WinnerTicketTest.java
+++ src/test/java/lotto/domain/WinnerTicketTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,38 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 public class WinnerTicketTest {
 
     private WinnerTicket winnerTicketFixture;
-    private Tickets ticketsFixture;
+    private Ticket ticketFixture;
 
     @BeforeEach
     public void beforeEach() {
-        Ticket winner = Ticket.of(Set.of(1, 2, 4, 8, 16, 32));
-        winnerTicketFixture = new WinnerTicket(winner, (LottoNumber.of(33)));
-
-        Ticket first = Ticket.of(Set.of(1, 2, 4, 8, 16, 32));
-        Ticket secondA = Ticket.of(Set.of(1, 2, 4, 8, 16, 33));
-        Ticket secondB = Ticket.of(Set.of(1, 2, 4, 8, 32, 33));
-
-        Ticket thirdA = Ticket.of(Set.of(1, 2, 4, 8, 16, 40));
-        Ticket thirdB = Ticket.of(Set.of(1, 2, 4, 8, 16, 41));
-        Ticket thirdC = Ticket.of(Set.of(1, 2, 4, 8, 16, 42));
-
-        Ticket fourthA = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
-        Ticket fourthB = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
-        Ticket fourthC = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
-        Ticket fourthD = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
-
-        Ticket fifthA = Ticket.of(Set.of(1, 2, 4, 15, 26, 37));
-        Ticket fifthB = Ticket.of(Set.of(1, 2, 4, 25, 26, 17));
-        Ticket fifthC = Ticket.of(Set.of(1, 2, 4, 35, 36, 37));
-        Ticket fifthD = Ticket.of(Set.of(1, 2, 4, 25, 36, 27));
-        Ticket fifthE = Ticket.of(Set.of(1, 2, 4, 35, 26, 17));
-        ticketsFixture = new Tickets(List.of(
-                first,
-                secondA, secondB,
-                thirdA, thirdB, thirdC,
-                fourthA, fourthB, fourthC, fourthD,
-                fifthA, fifthB, fifthC, fifthD, fifthE
-        ));
+        ticketFixture = Ticket.of(Set.of(1, 2, 4, 8, 16, 32));
+        winnerTicketFixture = new WinnerTicket(ticketFixture, (LottoNumber.of(33)));
     }
 
     @DisplayName("보너스 숫자 여부를 판별한다")
@@ -66,23 +39,51 @@ public class WinnerTicketTest {
         );
     }
 
-    @DisplayName("당첨자의 숫자를 구하는 기능을 검증한다")
+    @DisplayName("당첨 티켓 입력시 Prize 를 반환한다")
     @Test
-    public void countWinner() {
+    public void checkLucky() {
         //given
+        Ticket first = Ticket.of(Set.of(1, 2, 4, 8, 16, 32));
+
+        Ticket secondA = Ticket.of(Set.of(1, 2, 4, 8, 16, 33));
+        Ticket secondB = Ticket.of(Set.of(1, 2, 4, 8, 32, 33));
+
+        Ticket thirdA = Ticket.of(Set.of(1, 2, 4, 8, 16, 40));
+        Ticket thirdB = Ticket.of(Set.of(1, 2, 4, 8, 16, 41));
+        Ticket thirdC = Ticket.of(Set.of(1, 2, 4, 8, 16, 42));
+
+        Ticket fourthA = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
+        Ticket fourthB = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
+        Ticket fourthC = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
+        Ticket fourthD = Ticket.of(Set.of(1, 2, 33, 4, 35, 8));
+
+        Ticket fifthA = Ticket.of(Set.of(1, 2, 4, 15, 26, 37));
+        Ticket fifthB = Ticket.of(Set.of(1, 2, 4, 25, 26, 17));
+        Ticket fifthC = Ticket.of(Set.of(1, 2, 4, 35, 36, 37));
+        Ticket fifthD = Ticket.of(Set.of(1, 2, 4, 25, 36, 27));
+        Ticket fifthE = Ticket.of(Set.of(1, 2, 4, 35, 26, 17));
         //when
-        int actualFirst = winnerTicketFixture.countWinner(ticketsFixture, Prize.FIRST);
-        int actualSecond = winnerTicketFixture.countWinner(ticketsFixture, Prize.SECOND);
-        int actualThird = winnerTicketFixture.countWinner(ticketsFixture, Prize.THIRD);
-        int actualFourth = winnerTicketFixture.countWinner(ticketsFixture, Prize.FOURTH);
-        int actualFifth = winnerTicketFixture.countWinner(ticketsFixture, Prize.FIFTH);
         //then
-        assertAll("",
-                () -> assertThat(actualFirst).as("").isEqualTo(1),
-                () -> assertThat(actualSecond).as("").isEqualTo(2),
-                () -> assertThat(actualThird).as("").isEqualTo(3),
-                () -> assertThat(actualFourth).as("").isEqualTo(4),
-                () -> assertThat(actualFifth).as("").isEqualTo(5)
+        assertAll("당첨 티켓 입력시 Prize 를 반환해야 한다. 등수별로 해당 로직을 검증한다",
+                () -> assertThat(winnerTicketFixture.checkLucky(first)).as("1등 티켓을 검증한다").isEqualTo(Prize.FIRST),
+
+                () -> assertThat(winnerTicketFixture.checkLucky(secondA)).as("2등 티켓을 검증한다").isEqualTo(Prize.SECOND),
+                () -> assertThat(winnerTicketFixture.checkLucky(secondB)).as("2등 티켓을 검증한다").isEqualTo(Prize.SECOND),
+
+                () -> assertThat(winnerTicketFixture.checkLucky(thirdA)).as("3등 티켓을 검증한다").isEqualTo(Prize.THIRD),
+                () -> assertThat(winnerTicketFixture.checkLucky(thirdB)).as("3등 티켓을 검증한다").isEqualTo(Prize.THIRD),
+                () -> assertThat(winnerTicketFixture.checkLucky(thirdC)).as("3등 티켓을 검증한다").isEqualTo(Prize.THIRD),
+
+                () -> assertThat(winnerTicketFixture.checkLucky(fourthA)).as("4등 티켓을 검증한다").isEqualTo(Prize.FOURTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fourthB)).as("4등 티켓을 검증한다").isEqualTo(Prize.FOURTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fourthC)).as("4등 티켓을 검증한다").isEqualTo(Prize.FOURTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fourthD)).as("4등 티켓을 검증한다").isEqualTo(Prize.FOURTH),
+
+                () -> assertThat(winnerTicketFixture.checkLucky(fifthA)).as("5등 티켓을 검증한다").isEqualTo(Prize.FIFTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fifthB)).as("5등 티켓을 검증한다").isEqualTo(Prize.FIFTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fifthC)).as("5등 티켓을 검증한다").isEqualTo(Prize.FIFTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fifthD)).as("5등 티켓을 검증한다").isEqualTo(Prize.FIFTH),
+                () -> assertThat(winnerTicketFixture.checkLucky(fifthE)).as("5등 티켓을 검증한다").isEqualTo(Prize.FIFTH)
         );
     }
 }
