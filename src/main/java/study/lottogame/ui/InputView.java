6f8cc3ecea5f70a5c158eabc04e7eb4c9a86fc89
hoa0217 <gjwjdghk123@naver.com>
2023-05-05 21:45:35 +0900

    refactor: "inputView 변수명 수정"

--- src/main/java/study/lottogame/ui/InputView.java
+++ src/main/java/study/lottogame/ui/InputView.java
@@ -13,20 +13,20 @@ public class InputView {
 
   public static Money inputPurchaseMoney() {
     System.out.println("구입 금액을 입력해 주세요.");
-    String input = getInput();
-    return new Money(Integer.parseInt(input));
+    int money = Integer.parseInt(getInput().trim());
+    return new Money(money);
   }
 
   public static Lottery inputPrizeLottery() {
     System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-    String[] splitArr = getInput().split(",");
-    return LotteryFactory.create(splitArr);
+    String[] numbers = getInput().split(",");
+    return LotteryFactory.create(numbers);
   }
 
-  public static LottoNumber inputBonusLottoNumber(){
+  public static LottoNumber inputBonusLottoNumber() {
     System.out.println("보너스 볼을 입력해 주세요.");
-    int number = Integer.parseInt(getInput().trim());
-    return LottoNumber.valueOf(number);
+    int lottoNumber = Integer.parseInt(getInput().trim());
+    return LottoNumber.valueOf(lottoNumber);
   }
 
   private static String getInput() {
