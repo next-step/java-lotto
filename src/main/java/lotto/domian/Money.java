ec3d40be7a6c690828fcdb79411434263b188c9f
highjune <highjune37@gmail.com>
2023-05-05 16:23:19 +0900

    feat(Money): 돈의 제약
    - 돈이 천원 단위가 아니면 에러가 발생한다

---
+++ src/main/java/lotto/domian/Money.java
@@ -0,0 +1,18 @@
+package lotto.domian;
+
+public class Money {
+
+    private final int money;
+
+    public Money(int money) {
+        checkUnit(money);
+        this.money = money;
+    }
+
+    private void checkUnit(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("돈은 1000원 단위여야 합니다.");
+        }
+    }
+
+}
