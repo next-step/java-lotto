f8d3139f81de933e8c96481dc0c61dcc60e98aca
dong <dong.22@kakaoenterprise.com>
2023-05-05 15:55:59 +0900

    refactor : 불필요한 클래스 제거

--- src/main/java/lotto/domain/Issuer.java
+++ src/main/java/lotto/domain/Issuer.java
@@ -5,38 +5,3 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class Issuer {
-    private final Tickets tickets;
-
-    public Issuer() {
-        this.tickets = new Tickets(new ArrayList<>());
-    }
-
-    public void automaticIssue(int ticketCount) {
-        for (int i = 0; i < ticketCount; i++) {
-            tickets.add(new Ticket(issueNumbers()));
-        }
-    }
-
-
-    private Set<LottoNumber> issueNumbers() {
-        Set<LottoNumber> numbers = new HashSet<>();
-        while (numbers.size() < 6) {
-            numbers.add(LottoNumber.any());
-        }
-        return numbers;
-    }
-
-    public Tickets issuedTickets() {
-        return tickets;
-    }
-
-    public Statics issuedTicketStatistics(WinnerTicket winnerTicket) {
-        return new Statics(tickets, winnerTicket);
-    }
-
-
-    public void manualIssue(Tickets manualPurchases) {
-        tickets.add(manualPurchases);
-    }
-}
