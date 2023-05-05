49a154524b44de4bac2a34a9a0936c5628a2003e
Hwang Sangwook <goodbyeyo@naver.com>
2023-05-05 13:51:48 +0900

    feat: 보너스 번호 전달 기능  - 로또 게임 객체 생성시 보너스 번호 전달  - 당첨 로또 등수 확인 메서드에 보너스 번호 전달

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -1,10 +1,13 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("로또 티켓 집합 클래스 테스트")
 class LottoTicketsTest {
@@ -12,7 +15,7 @@ class LottoTicketsTest {
     @DisplayName("로또 티켓 구입은 1000원 보다 적으면 실패한다")
     @Test
     void lottoTicketsCreate_fail() {
-        Assertions.assertThatIllegalArgumentException()
+        assertThatIllegalArgumentException()
                 .isThrownBy(() -> LottoTickets.create(900))
                 .withMessage("티켓 구입 금액은 최소 1000원 이상 이여야 합니다");
     }
@@ -22,6 +25,31 @@ class LottoTicketsTest {
     void lottoTicketsCreate_success(int lottoTicketCount, int expected) {
         LottoTickets lottoTickets = LottoTickets.create(lottoTicketCount);
         int ticketCount = lottoTickets.findLottoTicketCount();
-        Assertions.assertThat(ticketCount).isEqualTo(expected);
+        assertThat(ticketCount).isEqualTo(expected);
     }
+
+    @DisplayName("보너스번호와 당첨로또를 전달하면 당첨통계결과를 얻을수 있다")
+    @Test
+    void calculateLottoStatistics() {
+        List<LottoTicket> lottoTickets = new ArrayList<>();
+        lottoTickets.add(LottoTicket.from(Set.of(1, 2, 3, 4, 5, 6)));
+        lottoTickets.add(LottoTicket.from(Set.of(2, 3, 4, 5, 6, 7)));
+        lottoTickets.add(LottoTicket.from(Set.of(2, 3, 4, 5, 6, 8)));
+        lottoTickets.add(LottoTicket.from(Set.of(3, 4, 5, 6, 7, 8)));
+        LottoTickets tickets = new LottoTickets(lottoTickets);
+
+        Set<Integer> winningNumbers = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoTicket winningLottoTicket = LottoTicket.from(winningNumbers);
+        WinningLotto winningLotto = new WinningLotto(winningLottoTicket);
+
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        WinningStatistics winningStatistics = tickets.calculateLottoStatistics(winningLotto, bonusNumber);
+
+        assertThat(winningStatistics.getCount(WinningRank.FIRST)).isEqualTo(1);
+        assertThat(winningStatistics.getCount(WinningRank.SECOND)).isEqualTo(1);
+        assertThat(winningStatistics.getCount(WinningRank.THIRD)).isEqualTo(1);
+        assertThat(winningStatistics.getCount(WinningRank.FOURTH)).isEqualTo(1);
+    }
+
 }
