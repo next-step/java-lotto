04c6f1ab734fd6b148b507da13872c7c60c04df4
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-05 23:22:54 +0900

    refactor : lottoNumber 일급컬렉션 및 리팩토링

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -1,83 +1,33 @@
 package lotto.domain;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoNumbers {
-    private static final int LOTTO_NUMBER_GROUP_LENGTH = 6;
-    private List<LottoNumber> lottoNumberGroup = new ArrayList<>();
+    private final List<LottoNumber> lottoNumbers;
 
-    public LottoNumbers() {
-    }
-
-    public LottoNumbers(List<LottoNumber> lottoNumberGroup) {
-        this.lottoNumberGroup = lottoNumberGroup;
-    }
-
-    public LottoNumbers aggregateNonDuplicateLottoNumber() {
-        Set<LottoNumber> lottoNumberList = new HashSet<>();
-
-        while (lottoNumberList.size() < LOTTO_NUMBER_GROUP_LENGTH) {
-            lottoNumberList.add(LottoNumber.provideLottoNumber());
-        }
-
-        return new LottoNumbers(toList(lottoNumberList));
-    }
-
-    private List<LottoNumber> toList(Set<LottoNumber> lottoNumberList) {
-        return new ArrayList<>(lottoNumberList).stream()
-                .sorted(LottoNumber::compareTo)
-                .collect(Collectors.toUnmodifiableList());
-    }
-
-    public LottoNumbers initializedManualLottoNumber(List<Integer> requestNumberList) {
-        validateLottoNumberLength(requestNumberList);
-        validateSameNumber(requestNumberList);
-
-
-        List<LottoNumber> lottoNumberList = requestNumberList.stream()
-                .map(LottoNumber::provideLottoNumber)
-                .sorted(LottoNumber::compareTo)
-                .collect(Collectors.toUnmodifiableList());
-
-        return new LottoNumbers(lottoNumberList);
+    public LottoNumbers(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
     }
 
     public int countMatchingLottoNumber(LottoNumbers lottoNumbers) {
         return findMatchingLottoNumber(lottoNumbers).size();
     }
 
-    private void validateLottoNumberLength(List<Integer> lottoNumberList) {
-
-        if (lottoNumberList.size() != LOTTO_NUMBER_GROUP_LENGTH) {
-            throw new IllegalArgumentException("로또 번호는 6개 입력 해주셔야해요 :(");
-        }
-    }
-
-    private void validateSameNumber(List<Integer> lottoNumberList) {
-        Set<Integer> removeDuplicateLottoNumber = new HashSet<>(lottoNumberList);
-
-        if (removeDuplicateLottoNumber.size() != LOTTO_NUMBER_GROUP_LENGTH) {
-            throw new IllegalArgumentException("중복된 로또번호가 존재해요 :(");
-        }
-    }
-
     private List<LottoNumber> filterMatchingLottoNumber(List<LottoNumber> lottoNumberList) {
-        return lottoNumberList.stream().filter(this::isMatchingLottoNumber)
+        return lottoNumberList.stream()
+                .filter(this::isMatchingLottoNumber)
                 .collect(Collectors.toUnmodifiableList());
     }
 
     private List<LottoNumber> findMatchingLottoNumber(LottoNumbers targetLottoNumbers) {
-        return targetLottoNumbers.filterMatchingLottoNumber(this.lottoNumberGroup);
+        return targetLottoNumbers.filterMatchingLottoNumber(this.lottoNumbers);
     }
 
-    private boolean isMatchingLottoNumber(LottoNumber lottoNumber) {
-        return lottoNumberGroup.stream()
+    public boolean isMatchingLottoNumber(LottoNumber lottoNumber) {
+        return lottoNumbers.stream()
                 .anyMatch(lottoNumber::isSameLottoNumber);
-    }
-
-    public int getLottoNumberGroupSize() {
-        return lottoNumberGroup.size();
     }
 
     @Override
@@ -85,17 +35,17 @@ public class LottoNumbers {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         LottoNumbers that = (LottoNumbers) o;
-        return lottoNumberGroup.equals(that.lottoNumberGroup);
+        return lottoNumbers.equals(that.lottoNumbers);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(lottoNumberGroup);
+        return Objects.hash(lottoNumbers);
     }
 
     @Override
     public String toString() {
-        return lottoNumberGroup.toString();
+        return lottoNumbers.toString();
     }
 }
 
