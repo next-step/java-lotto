2a04e0427f20b59b40810e11ddc9adb123d90411
dong <dong.22@kakaoenterprise.com>
2023-05-06 13:35:38 +0900

    test : 예외발생 테스트 추가

--- src/test/java/lotto/domain/NumberStrategyRandomTest.java
+++ src/test/java/lotto/domain/NumberStrategyRandomTest.java
@@ -1,22 +1,48 @@
 package lotto.domain;
 
+import lotto.exception.NumberLimitRangeRuleViolateException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NumberStrategyRandomTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(NumberStrategySequentialTest.class);
-    int lowLimit = 7;
-    int highLimit = 45;
+    private final int lowLimit = 7;
+    private final int highLimit = 45;
+
+    private NumberStrategy numberStrategy;
+
+    @BeforeEach
+    public void beforeEach() {
+        numberStrategy = NumberStrategyRandom.of();
+    }
 
     @DisplayName("입력한 범위 사이의 값만 출력되는지를 검증한다")
     @RepeatedTest(1000)
     public void validateRange() {
-        int generatedNumber = NumberStrategyRandom.of().generate(lowLimit, highLimit);
+        //given
+        //when
+        int generatedNumber = numberStrategy.generate(lowLimit, highLimit);
         LOGGER.info(() -> String.format("생성된 숫자는 %d 입니다", generatedNumber));
+        //then
         assertThat(generatedNumber).isBetween(lowLimit, highLimit);
+    }
+
+    @DisplayName("highLimit 가 lowLimit 보다 작은경우 예외가 발생한다")
+    @Test
+    public void exception() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            numberStrategy.generate(highLimit, lowLimit);
+        }).isInstanceOf(NumberLimitRangeRuleViolateException.class)
+                .hasMessageContaining("LimitRange 가 잘못 입력되었습니다");
     }
 }
