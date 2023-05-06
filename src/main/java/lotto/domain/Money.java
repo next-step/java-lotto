c692588479ee1fa547b9a55ceb522a91e0193b68
0giri <higiri@naver.com>
2023-05-07 03:23:01 +0900

    feat: Money 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public class Money {
+
+    private final int amount;
+
+    public Money(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("음수는 사용할 수 없습니다.");
+        }
+        this.amount = amount;
+    }
+
+    public int buyCount(int price) {
+        return this.amount / price;
+    }
+}
---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MoneyTest {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    @Test
+    void 자연수_또는_0으로_생성시_성공() {
+        assertThatNoException().isThrownBy(() -> new Money(0));
+        assertThatNoException().isThrownBy(() -> new Money(1));
+    }
+
+    @Test
+    void 음수로_생성시_IllegalArgumentException() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Money(-1));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "9999,9", "10000,10", "10001,10"})
+    void 구매가능_횟수_조회(int amount, int expect) {
+        Money money = new Money(amount);
+        int actual = money.buyCount(LOTTO_PRICE);
+        assertThat(actual).isEqualTo(expect);
+    }
+}
