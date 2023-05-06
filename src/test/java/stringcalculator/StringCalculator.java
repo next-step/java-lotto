de9e23405893e714643f674004cb73d912862291
MoonHKLee <moonhyuk.lee@lotte.net>
2023-05-06 14:29:32 +0900

    feat(stringcalculator) : 이상한 기호 validate 기능 추가.

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -8,11 +8,10 @@ import java.util.regex.Pattern;
 public class StringCalculator {
     private static final String OPERATION_REGEX = "[+\\-*/]+";
     private static final String NUMBER_REGEX = "\\d+";
+    private static final String NOT_VALID_REGEX = "[^\\d+\\-*/^\\s]";
 
     public static int calculate(String input) {
-        if (input == null || input.equals("")) {
-            throw new IllegalArgumentException("빈 값을 입력할 수 없습니다.");
-        }
+        containsOnlyValidCharacters(input);
         List<Integer> numbers = toInts(getCharacters(input, NUMBER_REGEX));
         List<String> operations = getCharacters(input, OPERATION_REGEX);
         int result = numbers.get(0);
@@ -52,5 +51,15 @@ public class StringCalculator {
             ints.add(Integer.parseInt(string));
         }
         return ints;
+    }
+
+    private static void containsOnlyValidCharacters(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("빈 값을 입력할 수 없습니다.");
+        }
+
+        if (Pattern.compile(NOT_VALID_REGEX).matcher(input).find()) {
+            throw new IllegalArgumentException("사칙연산 기호를 입력해주세요.");
+        }
     }
 }
