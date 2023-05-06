076c1af614f8b87f87156ed6f748a73a7c8acd4e
MoonHKLee <moonhyuk.lee@lotte.net>
2023-05-06 12:41:52 +0900

    test(stringcalculator) : 테스트 작성 - 뺄셈 관련 테스트 작성.

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class StringCalculatorTest {
 
     @Test
+    @DisplayName("덧셈 테스트")
     void testPlus() {
         //arrange
         String input = "1 + 4 + 7";
@@ -16,5 +18,18 @@ public class StringCalculatorTest {
 
         //assert
         assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("뺄셈 테스트")
+    void testMinus() {
+        //arrange
+        String input = "1 - 4 - 7";
+
+        //act
+        int result = StringCalculator.calculate(input);
+
+        //assert
+        assertThat(result).isEqualTo(-10);
     }
 }
