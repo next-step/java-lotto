531d1886846053134a4152f0b4e03f61c8d982a1
dkswnkk <jjoy9810@gmail.com>
2023-05-06 22:06:15 +0900

    feat: 사용하지 않는 로직 제거

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -5,15 +5,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoResult {
-    private static final int NO_BONUS_NUMBER = 0;
-
     private final Map<Rank, Long> rankCounts;
-
-    public LottoResult(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers) {
-        rankCounts = lottoNumbers.stream()
-                .map(lotto -> createRank(lotto, winningNumbers, NO_BONUS_NUMBER))
-                .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
-    }
 
     public LottoResult(List<LottoNumber> lottoNumbers, List<Integer> winningNumbers, int bonusNumber) {
         rankCounts = lottoNumbers.stream()
@@ -21,16 +13,16 @@ public class LottoResult {
                 .collect(Collectors.groupingBy(rank -> rank, Collectors.counting()));
     }
 
-    private Rank createRank(LottoNumber lotto, List<Integer> winningNumbers, int bonusNumber) {
-        LottoMatchInfo lottoMatchInfo = LottoMatchInfo.countMatchingNumbers(lotto, winningNumbers, bonusNumber);
-
-        return Rank.valueOf(lottoMatchInfo.getMatchCount(), lottoMatchInfo.isBonusMatch());
-    }
-
     public long findRankCount(int matchCount, boolean bonusMatch) {
         Rank rank = Rank.valueOf(matchCount, bonusMatch);
 
         return rankCounts.getOrDefault(rank, 0L);
+    }
+
+    private Rank createRank(LottoNumber lotto, List<Integer> winningNumbers, int bonusNumber) {
+        LottoMatchInfo lottoMatchInfo = LottoMatchInfo.countMatchingNumbers(lotto, winningNumbers, bonusNumber);
+
+        return Rank.valueOf(lottoMatchInfo.getMatchCount(), lottoMatchInfo.isBonusMatch());
     }
 
 }
