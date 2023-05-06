22dd98a04067efd0745af2a4019ff815b852dc08
dong <dong.22@kakaoenterprise.com>
2023-05-06 14:05:05 +0900

    refactor : 컨트롤러 로직이 너무 길어 가독성이 떨어지므로 이를 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,16 +14,19 @@ public class Lotto {
         InputPresent inputPresent = new InputPresent();
         OutputPresent outputPresent = new OutputPresent();
 
-        int totalPurchaseCount = inputPresent.purchaseCount();
+        Tickets issueTickets = issueTickets(inputPresent, outputPresent, inputPresent.purchaseCount());
+        WinnerTicket winnerTicket = new WinnerTicket(inputPresent.winningNumbers(), inputPresent.bonusNumber());
+        Statics statics = new Statics(issueTickets, winnerTicket);
+
+        outputPresent.presentWinners(new WinnerPresentDTO(statics));
+        outputPresent.presentIncome(new IncomePresentDTO(statics));
+    }
+
+    private static Tickets issueTickets(InputPresent inputPresent, OutputPresent outputPresent, int totalPurchaseCount) {
         Tickets manualTickets = inputPresent.manualPurchases();
         Auditor.manualPurchaseValidation(totalPurchaseCount, manualTickets.count());
         Tickets autoTickets = Tickets.automaticIssue(totalPurchaseCount - manualTickets.count());
         outputPresent.presentTickets(manualTickets, autoTickets);
-        autoTickets.add(manualTickets);
-
-        WinnerTicket winnerTicket = new WinnerTicket(inputPresent.winningNumbers(), inputPresent.bonusNumber());
-        Statics statics = new Statics(autoTickets, winnerTicket);
-        outputPresent.presentWinners(new WinnerPresentDTO(statics));
-        outputPresent.presentIncome(new IncomePresentDTO(statics));
+        return Tickets.composite(manualTickets, autoTickets);
     }
 }
