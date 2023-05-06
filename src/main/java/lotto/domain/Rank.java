ab115028e62709f5f1dec7fbdf25d01f2d6867d5
nayoon-kim <belloyv@gmail.com>
2023-05-06 13:48:08 +0900

    Rank 추가

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Rank {
+
+    FOURTH(3, 5_000),
+    THIRD(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
+
+    private final int count;
+    private final int reward;
+
+    private static final Map<Integer, Integer> rank = Stream.of(values()).collect(Collectors.toMap(Rank::count, Rank::reward));
+
+    Rank(int count, int reward) {
+        this.count = count;
+        this.reward = reward;
+    }
+
+    int count() {
+        return count;
+    }
+
+    int reward() {
+        return reward;
+    }
+
+    public static int of(int count) {
+        return rank.get(count);
+    }
+
+    public static List<Integer> getCountList() {
+        return Stream.of(values()).map(Rank::count).collect(Collectors.toList());
+    }
+}
