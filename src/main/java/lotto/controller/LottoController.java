c27e880cc66f1ef9f9a526abc565aeeb0ffbc042
Hwang Sangwook <goodbyeyo@naver.com>
2023-05-06 21:18:16 +0900

    feat: LottoController 모듈 조립  - 수동 로또 수, 수동 로또 번호 문자열 -> 도메인 클래스 전달

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,12 +4,17 @@ import lotto.domain.*;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import java.util.List;
+
 public class LottoController {
 
     public void run() {
         int purchaseAmount = InputView.inputPurchaseAmount();
-        LottoTickets lottoTickets = LottoTickets.create(purchaseAmount);
-        ResultView.printLottoTickets(lottoTickets);
+        int manualLottoCount = InputView.inputManualLottoCount();
+        List<String> manualLottoNumber = InputView.inputManualLottoNumber(manualLottoCount);
+
+        LottoTickets lottoTickets = LottoTickets.create(purchaseAmount, manualLottoCount, manualLottoNumber);
+        ResultView.printLottoTickets(lottoTickets, manualLottoCount);
 
         String winningNumbersString = InputView.inputWinningNumbers();
         int bonusNumber = InputView.inputBonusNumber();
