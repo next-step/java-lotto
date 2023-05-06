69a1c15325235ee2bb858f05960f3d4030ce0f2a
wattt3 <chleodnr3@gmail.com>
2023-05-06 17:08:28 +0900

    Fix: LottoRank - Rename WinLotto -> LottoRank - MISS 추가

--- src/main/java/lotto/domain/constant/LottoRank.java
+++ src/main/java/lotto/domain/constant/LottoRank.java
@@ -2,26 +2,27 @@ package lotto.domain.constant;
 
 import java.util.Arrays;
 
-public enum WinLotto {
+public enum LottoRank {
 
     FIRST(6, 2000000000),
     SECOND(5, 1500000),
     THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FOURTH(3, 5000),
+    MISS(0, 0);
 
     private final Integer hitNumber;
     private final Integer winningAmount;
 
-    WinLotto(Integer hitNumber, Integer winningAmount) {
+    LottoRank(Integer hitNumber, Integer winningAmount) {
         this.hitNumber = hitNumber;
         this.winningAmount = winningAmount;
     }
 
-    public static WinLotto toWinLotto(Integer hitNumber) {
+    public static LottoRank toWinLotto(Integer hitNumber) {
         return Arrays.stream(values())
             .filter((winLotto) -> winLotto.hitNumber.equals(hitNumber))
             .findAny()
-            .orElseThrow(IllegalArgumentException::new);
+            .orElse(MISS);
     }
 
     public Integer getWinningAmount() {
--- src/test/java/lotto/domain/constant/LottoRankTest.java
+++ src/test/java/lotto/domain/constant/LottoRankTest.java
@@ -1,30 +1,28 @@
 package lotto.domain.constant;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class WinLottoTest {
+class LottoRankTest {
 
     @ParameterizedTest
     @CsvSource({"3,FOURTH", "4,THIRD", "5,SECOND", "6,FIRST"})
-    void toWinLottoTest(Integer hitNumber, WinLotto winLotto) {
-        assertThat(WinLotto.toWinLotto(hitNumber)).isEqualTo(winLotto);
+    void toWinLottoTest(Integer hitNumber, LottoRank winLotto) {
+        assertThat(LottoRank.toWinLotto(hitNumber)).isEqualTo(winLotto);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 7})
+    @ValueSource(ints = {0, 1, 2})
     void toWinLottoExceptionTest(Integer hitNumber) {
-        assertThatThrownBy(() -> WinLotto.toWinLotto(hitNumber))
-            .isInstanceOf(IllegalArgumentException.class);
+        assertThat(LottoRank.toWinLotto(hitNumber)).isEqualTo(LottoRank.MISS);
     }
 
     @ParameterizedTest
     @CsvSource({"FIRST,2000000000", "SECOND,1500000", "THIRD,50000", "FOURTH,5000"})
-    void getWinningAmountTest(WinLotto winLotto, Integer winningAmount) {
+    void getWinningAmountTest(LottoRank winLotto, Integer winningAmount) {
         assertThat(winLotto.getWinningAmount()).isEqualTo(winningAmount);
     }
 }
