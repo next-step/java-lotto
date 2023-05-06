f0faf40c4b65ae2968265bf6782ee839d3f8f469
hoyeoon <hoyeon.backend@gmail.com>
2023-05-06 09:49:50 +0900

    refactor: 코드컨벤션 수정

--- src/main/java/lotto/utils/LottoIssuer.java
+++ src/main/java/lotto/utils/LottoIssuer.java
@@ -15,10 +15,10 @@ import static lotto.domain.LottoNumber.LOTTO_NUMBER_MIN;
 public class LottoIssuer {
 
     private static final int BEGIN_INDEX = 0;
-    private static List<Integer> allNumbers = IntStream.rangeClosed(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX)
+    private static final List<Integer> ALL_LOTTO_NUMBERS = IntStream.rangeClosed(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX)
             .boxed()
             .collect(Collectors.toList());
-    
+
     public static List<Lotto> issueLottos(long lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = BEGIN_INDEX; i < lottoCount; i++) {
@@ -28,8 +28,8 @@ public class LottoIssuer {
     }
 
     private static Lotto issueLotto() {
-        Collections.shuffle(allNumbers);
-        List<Integer> numbers = allNumbers.subList(BEGIN_INDEX, LOTTO_SIZE);
+        Collections.shuffle(ALL_LOTTO_NUMBERS);
+        List<Integer> numbers = ALL_LOTTO_NUMBERS.subList(BEGIN_INDEX, LOTTO_SIZE);
         return new Lotto(numbers);
     }
 }
