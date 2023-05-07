0d774efa573fc486550f7b39d8f5f5ebbc77b161
ain <lovepizza132@gmail.com>
2023-05-07 20:32:36 +0900

    chore : 접근자 수정'

--- src/main/java/lotto/utility/NumberMaker.java
+++ src/main/java/lotto/utility/NumberMaker.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class NumberMaker {
-  private static final List<Integer> NUMBER_UNDER_FORTY_FIVE = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
+  public static final List<Integer> NUMBER_UNDER_FORTY_FIVE = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
 
   public static List<Integer> makeSixLottoNumber() {
     return sortNumber(shuffleNumber(NUMBER_UNDER_FORTY_FIVE).subList(0, 6));
