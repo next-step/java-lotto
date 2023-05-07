e2cd4bf81f8610c80ef703955a0a1d8933594dfc
진우 <nice7677@naver.com>
2023-05-07 16:14:43 +0900

    fix: 스캐너 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner;
 
     private static final String BUY_AMOUNT_INPUT_TEXT = "구입금액을 입력해 주세요.";
     private static final String LAST_WEEK_WINNING_NUMBER_INPUT_TEXT = "지난 주 당첨 번호를 입력해 주세요.";
@@ -15,13 +15,17 @@ public class InputView {
 
 
     public static int inputBuyAmount() {
+        scanner = new Scanner(System.in);
         System.out.println(BUY_AMOUNT_INPUT_TEXT);
         return scanner.nextInt();
     }
 
     public static List<Integer> inputLastWeekWinningNumber() {
         System.out.println(LAST_WEEK_WINNING_NUMBER_INPUT_TEXT);
-        return Arrays.stream(scanner.nextLine().split(LAST_WEEK_WINNING_NUMBER_REX_PATTERN))
+        scanner = new Scanner(System.in);
+        String lastWeekWinningNumber = scanner.nextLine();
+        System.out.println(lastWeekWinningNumber);
+        return Arrays.stream(lastWeekWinningNumber.split(LAST_WEEK_WINNING_NUMBER_REX_PATTERN))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
