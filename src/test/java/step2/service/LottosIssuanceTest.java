3300c84b5ca1521c1c529cdb50147e40a99331f4
juhyun <zzang9ha@naver.com>
2023-05-07 22:59:54 +0900

    test: 불필요한 테스트 케이스 제거, 인자 추가

--- src/test/java/step2/service/LottosIssuanceTest.java
+++ src/test/java/step2/service/LottosIssuanceTest.java
@@ -1,30 +1,22 @@
 package step2.service;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import step2.domain.vo.Money;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottosIssuanceTest {
 
     @ParameterizedTest
     @CsvSource(value = {"14000:14", "5500:5", "12000:12"}, delimiter = ':')
-    void 로또_티켓은_구입금액에서_로또금액을_나눈만큼_발급된다(int money, int quantity) {
-        final var lottos = LottosIssuance.issues(new Money(money));
+    void 로또_발급(int money, int quantity) {
+        final var lottos = LottosIssuance.issues(new ArrayList<>(), new Money(money));
 
         final var lottoList = lottos.getLottos();
 
         assertThat(lottoList).hasSize(quantity);
-    }
-
-    @Test
-    void 로또_티켓_발급시_구입금액이_로또금액보다_작을경우_예외를_던진다() {
-        assertThrows(
-                IllegalStateException.class,
-                () -> LottosIssuance.issues(new Money(900))
-        );
     }
 }
