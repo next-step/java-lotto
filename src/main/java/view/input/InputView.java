9f3a9246dbca9300600a0cb75bedbab33686654c
yoonjaej3 <yoonjaej3@gmail.com>
2023-05-08 01:08:47 +0900

    refact : 유틸 클래스 변환

--- src/main/java/view/input/InputView.java
+++ src/main/java/view/input/InputView.java
@@ -1,23 +1,24 @@
-package View.Input;
+package view.input;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     static Scanner scanner = new Scanner(System.in);
 
-    public int getInputBuyAmount() {
+    public static int inputBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
 
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public ArrayList<Integer> inputWinnerLottoNum() {
+    public static List<Integer> inputWinnerLottoNum() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 
         String[] nums = scanner.nextLine().split(",");
 
-        ArrayList<Integer> numArrayList = new ArrayList<>();
+        List<Integer> numArrayList = new ArrayList<>();
 
         for (String num : nums) {
             numArrayList.add(Integer.parseInt(num.trim()));
@@ -25,6 +26,12 @@ public class InputView {
 
         return numArrayList;
 
+    }
+
+    public static int inputBonusBall() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+
+        return Integer.parseInt(scanner.next());
     }
 
 }
