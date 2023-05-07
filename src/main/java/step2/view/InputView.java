271146ea63c18d18dae27a91db0fc4dc4dc7f4ae
juhyun <zzang9ha@naver.com>
2023-05-07 22:57:12 +0900

    feat: 수동 로또 구매 수량 입력 추가, 구입 금액 반환형 변경

--- src/main/java/step2/view/InputView.java
+++ src/main/java/step2/view/InputView.java
@@ -1,8 +1,13 @@
 package step2.view;
 
 import step2.domain.entity.Lotto;
+import step2.domain.entity.LottoFactory;
 import step2.domain.vo.LottoNumber;
+import step2.domain.vo.LottoType;
+import step2.domain.vo.Money;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -12,24 +17,57 @@ public class InputView {
     private InputView() {
     }
 
-    public static int inputMoney() {
+    public static Money inputMoney() {
         System.out.println("구입 금액을 입력해 주세요.");
 
-        return SCANNER.nextInt();
+        final var money = SCANNER.nextInt();
+
+        return new Money(money);
     }
 
-    public static Lotto inputLastLottoNumbers() {
+    public static int inputManualLottoQuantity(Money input) {
+        System.out.println(System.lineSeparator() + "수동으로 구매할 로또 수를 입력해 주세요.");
+
+        final var money = input.getMoney();
+        final var quantity = SCANNER.nextInt();
+        final var needMoney = Lotto.PRICE * quantity;
+
+        if (needMoney > money) {
+            final var message = String.format(
+                    "금액이 부족하여 로또를 구매할 수 없습니다. 구매 가능한 로또 수량: %d, 필요금액: %d, 보유금액: %d",
+                    money / Lotto.PRICE,
+                    needMoney,
+                    money
+            );
+            throw new IllegalStateException(message);
+        }
+
+        return quantity;
+    }
+
+    public static List<Lotto> inputManualLotto(int quantity) {
         SCANNER.nextLine();
 
+        System.out.println(System.lineSeparator() + "수동으로 구매할 번호를 입력해 주세요.");
+
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < quantity; i++) {
+            final var numbers = SCANNER.nextLine();
+            lottos.add(LottoFactory.of(LottoType.MANUAL, numbers));
+        }
+
+        return lottos;
+    }
+
+    public static String inputLastLottoNumbers() {
         System.out.println(System.lineSeparator() + "지난 주 당첨 번호를 입력해 주세요.");
 
-        final var number = SCANNER.nextLine();
-
-        return Lotto.winner(number);
+        return SCANNER.nextLine();
     }
 
     public static LottoNumber inputBonusLottoNumber() {
-        System.out.println(System.lineSeparator() + "보너스 볼을 입력해 주세요.");
+        System.out.println(System.lineSeparator() + "보너스 번호를 입력해 주세요.");
 
         final var number = SCANNER.nextLine();
 
