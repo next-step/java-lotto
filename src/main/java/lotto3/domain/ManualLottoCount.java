8ccf2d2c21962056681164b69d96381e7ea653aa
soochan <tncksdl05@gmail.com>
2023-05-07 19:57:44 +0900

    refactor : ManualLottoCount 생성자에서 유효성 검사 순서 변경

--- src/main/java/lotto3/domain/ManualLottoCount.java
+++ src/main/java/lotto3/domain/ManualLottoCount.java
@@ -5,11 +5,11 @@ public class ManualLottoCount {
   private final int count;
 
   public ManualLottoCount(int count) {
+    validateIsPositive(count);
     this.count = count;
-    validateIsPositive();
   }
 
-  private void validateIsPositive() {
+  private void validateIsPositive(int count) {
     if (count < 0) {
       throw new IllegalArgumentException("수동으로 구매할 로또 수는 0보다 커야합니다.");
     }
