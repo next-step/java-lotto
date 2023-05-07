7e1167cd6111a9133f3714eded8cb2cba3fe967b
dkswnkk <jjoy9810@gmail.com>
2023-05-07 21:38:41 +0900

    test: 테스트 코드 작성

--- src/test/java/LottoNumberTest.java
+++ src/test/java/LottoNumberTest.java
@@ -1,3 +1,4 @@
+import lotto.domain.LottoAvailableNumber;
 import lotto.domain.LottoMatchInfo;
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoResult;
@@ -18,22 +19,30 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("로또 테스트")
 public class LottoNumberTest {
-
-    private static final int NO_BONUS_NUMBER = -1;
+    private static final LottoAvailableNumber BONUS_NUMBER_45 = new LottoAvailableNumber(45);
     private static final List<LottoNumber> NO_MANUAL_LOTTO_NUMBERS = new ArrayList<>();
     private static final RandomLottoNumberGenerateStrategy RANDOM_LOTTO_NUMBER_GENERATE_STRATEGY = new RandomLottoNumberGenerateStrategy();
+    private static final List<LottoAvailableNumber> NUMBERS_1_TO_6 = IntStream.rangeClosed(1, 6)
+            .mapToObj(LottoAvailableNumber::new)
+            .collect(Collectors.toList());
+    private static final List<LottoAvailableNumber> NUMBERS_7_TO_12 = IntStream.rangeClosed(7, 12)
+            .mapToObj(LottoAvailableNumber::new)
+            .collect(Collectors.toList());
+
 
     @Test
     @DisplayName("6개의 번호를 가진 랜덤 로또를 생성할 수 있다.")
     void generateSixLottoNumbers_test() {
         LottoNumber lottoNumber = new LottoNumber(RANDOM_LOTTO_NUMBER_GENERATE_STRATEGY);
 
-        Assertions.assertThat(lottoNumber.getNumbers()).hasSize(6);
+        Assertions.assertThat(lottoNumber.getLottoNumbers()).hasSize(6);
     }
 
     @RepeatedTest(10)
@@ -49,7 +58,7 @@ public class LottoNumberTest {
     @DisplayName("로또 번호가 정해진 사이값의 숫자가 아니라면 예외를 던진다.")
     @ValueSource(ints = {-1, 46})
     void validateLottoNumber_test(int lottoNumber) {
-        assertThatThrownBy(() -> InputView.validateLottoNumberInRange(lottoNumber))
+        assertThatThrownBy(() -> new LottoAvailableNumber(lottoNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -81,11 +90,19 @@ public class LottoNumberTest {
     @DisplayName("당첨 번호에서 숫자만 분리할 수 있다.")
     void extractWinningNumbers_test() {
         String input = "1, 2, 3, 4, 5, 6";
-        Integer[] numbers = {1, 2, 3, 4, 5, 6};
 
-        List<Integer> extractNumbers = InputView.extractNumbers(input);
+        List<LottoAvailableNumber> extractNumbers = InputView.extractNumbers(input);
 
-        Assertions.assertThat(extractNumbers).contains(numbers);
+        Assertions.assertThat(extractNumbers)
+                .containsExactlyInAnyOrderElementsOf(NUMBERS_1_TO_6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 500})
+    @DisplayName("금액이 부족하면 예외를 던진다.")
+    void validatePurchaseAmount_test(int purchaseAmount) {
+        assertThatThrownBy(() -> new LottoTicket(3, NO_MANUAL_LOTTO_NUMBERS, purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
@@ -101,10 +118,8 @@ public class LottoNumberTest {
     @DisplayName("당첨 번호가 0~6개 안의 숫자에서 확인되는 지 확인할 수 있다.")
     void checkMatchingNumbers_test() {
         LottoNumber lottoNumber = new LottoNumber(RANDOM_LOTTO_NUMBER_GENERATE_STRATEGY);
-        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-
-        LottoMatchInfo matchInfo = LottoMatchInfo.countMatchingNumbers(lottoNumber, winningNumbers, NO_BONUS_NUMBER);
+        LottoMatchInfo matchInfo = LottoMatchInfo.countMatchingNumbers(lottoNumber, NUMBERS_1_TO_6, BONUS_NUMBER_45);
 
         Assertions.assertThat(matchInfo.getMatchCount()).isBetween(0, 6);
     }
@@ -113,10 +128,9 @@ public class LottoNumberTest {
     @DisplayName("보너스 번호가 없을 때 수익률 계산이 동작하는지 확인할 수 있다.")
     void calculateReturnRate_test() {
         int purchaseAmount = 10_000;
-        LottoTicket lottoTicket = new LottoTicket(10, NO_MANUAL_LOTTO_NUMBERS);
-        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        LottoTicket lottoTicket = new LottoTicket(10, NO_MANUAL_LOTTO_NUMBERS, purchaseAmount);
 
-        LottoResult lottoResult = lottoTicket.calculateResult(winningNumbers, NO_BONUS_NUMBER);
+        LottoResult lottoResult = lottoTicket.calculateResult(NUMBERS_1_TO_6, BONUS_NUMBER_45);
         double returnRate = OutputView.calculateReturnRate(purchaseAmount, lottoResult);
 
         Assertions.assertThat(returnRate).isGreaterThanOrEqualTo(0);
@@ -125,10 +139,9 @@ public class LottoNumberTest {
     @Test
     @DisplayName("보너스 번호가 당첨번호에 포함되어 있을 때 예외를 던진다.")
     void validateBonusNumber_test() {
-        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int bonusNumber = 6;
 
-        assertThatThrownBy(() -> InputView.validateBonusNumber(winningNumbers, bonusNumber))
+        assertThatThrownBy(() -> InputView.validateBonusNumber(NUMBERS_1_TO_6, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -136,11 +149,10 @@ public class LottoNumberTest {
     @DisplayName("보너스 번호가 있을 때 수익률 계산이 동작하는지 확인할 수 있다.")
     void calculateWithBonusNumberReturnRate_test() {
         int purchaseAmount = 10_000;
-        LottoTicket lottoTicket = new LottoTicket(10, NO_MANUAL_LOTTO_NUMBERS);
-        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
+        LottoTicket lottoTicket = new LottoTicket(10, NO_MANUAL_LOTTO_NUMBERS, purchaseAmount);
+        LottoAvailableNumber bonusNumber = new LottoAvailableNumber(7);
 
-        LottoResult lottoResult = lottoTicket.calculateResult(winningNumbers, bonusNumber);
+        LottoResult lottoResult = lottoTicket.calculateResult(NUMBERS_1_TO_6, bonusNumber);
         double returnRate = OutputView.calculateReturnRate(purchaseAmount, lottoResult);
 
         Assertions.assertThat(returnRate).isGreaterThanOrEqualTo(0);
@@ -150,10 +162,9 @@ public class LottoNumberTest {
     @DisplayName("당첨 번호와 일치하는 숫자와 보너스 번호 일치 여부를 확인할 수 있다.")
     void checkMatchingNumbersAndBonus_test() {
         LottoNumber lottoNumber = new LottoNumber(RANDOM_LOTTO_NUMBER_GENERATE_STRATEGY);
-        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        int bonusNumber = 7;
+        LottoAvailableNumber bonusNumber = new LottoAvailableNumber(7);
 
-        LottoMatchInfo lottoMatchInfo = LottoMatchInfo.countMatchingNumbers(lottoNumber, winningNumbers, bonusNumber);
+        LottoMatchInfo lottoMatchInfo = LottoMatchInfo.countMatchingNumbers(lottoNumber, NUMBERS_1_TO_6, bonusNumber);
 
         Assertions.assertThat(lottoMatchInfo.getMatchCount()).isBetween(0, 6);
         Assertions.assertThat(lottoMatchInfo.isBonusMatch()).isIn(true, false);
@@ -162,11 +173,13 @@ public class LottoNumberTest {
     @Test
     @DisplayName("로또를 수동으로 구매할 수 있다.")
     void purchaseManualLottoTicket_test() {
-        List<LottoNumber> manualLottoNumbers = new ArrayList<>();
-        manualLottoNumbers.add(new LottoNumber(new ManualLottoNumberGenerateStrategy(Arrays.asList(1, 2, 3, 4, 5, 6))));
-        manualLottoNumbers.add(new LottoNumber(new ManualLottoNumberGenerateStrategy(Arrays.asList(7, 8, 9, 10, 11, 12))));
+        int purchaseAmount = 2_000;
+        List<LottoNumber> manualLottoNumbers = new ArrayList<>(Arrays.asList(
+                new LottoNumber(new ManualLottoNumberGenerateStrategy(NUMBERS_1_TO_6)),
+                new LottoNumber(new ManualLottoNumberGenerateStrategy(NUMBERS_7_TO_12))
+        ));
 
-        LottoTicket manualLottoTicket = new LottoTicket(0, manualLottoNumbers);
+        LottoTicket manualLottoTicket = new LottoTicket(0, manualLottoNumbers, purchaseAmount);
 
         Assertions.assertThat(manualLottoTicket.getTotalTicketCount())
                 .isEqualTo(2);
@@ -175,22 +188,24 @@ public class LottoNumberTest {
     @Test
     @DisplayName("수동으로 생성된 로또 번호가 올바른지 확인할 수 있다.")
     void checkManualLottoNumber_test() {
-        List<Integer> manualNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<LottoAvailableNumber> manualNumbers = new ArrayList<>();
         LottoNumber manualLottoNumber = new LottoNumber(new ManualLottoNumberGenerateStrategy(manualNumbers));
 
-        Assertions.assertThat(manualLottoNumber.getNumbers())
+        Assertions.assertThat(manualLottoNumber.getLottoNumbers())
                 .containsExactlyInAnyOrderElementsOf(manualNumbers);
     }
 
     @Test
     @DisplayName("수동과 자동 로또를 함께 구매할 수 있다.")
     void purchaseManualAndAutoLottoTickets_test() {
-        List<LottoNumber> manualLottoNumbers = new ArrayList<>();
-        manualLottoNumbers.add(new LottoNumber(new ManualLottoNumberGenerateStrategy(Arrays.asList(1, 2, 3, 4, 5, 6))));
-        manualLottoNumbers.add(new LottoNumber(new ManualLottoNumberGenerateStrategy(Arrays.asList(7, 8, 9, 10, 11, 12))));
+        int purchaseAmount = 3_000;
+        List<LottoNumber> manualLottoNumbers = new ArrayList<>(Arrays.asList(
+                new LottoNumber(new ManualLottoNumberGenerateStrategy(NUMBERS_1_TO_6)),
+                new LottoNumber(new ManualLottoNumberGenerateStrategy(NUMBERS_7_TO_12))
+        ));
 
-        int autoTicketCount = 3;
-        LottoTicket mixedLottoTicket = new LottoTicket(autoTicketCount, manualLottoNumbers);
+        int autoTicketCount = 1;
+        LottoTicket mixedLottoTicket = new LottoTicket(autoTicketCount, manualLottoNumbers, purchaseAmount);
 
         Assertions.assertThat(mixedLottoTicket.getTotalTicketCount())
                 .isEqualTo(autoTicketCount + manualLottoNumbers.size());
