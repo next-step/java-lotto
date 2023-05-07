aee017fb58d6c1497f6078dd21054187237c0308
nayoon-kim <belloyv@gmail.com>
2023-05-07 16:30:53 +0900

    로또 번호 체크 메소드 변경

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -9,6 +8,8 @@ import java.util.stream.IntStream;
 public class Lottos {
     private static final int LOTTO_REWARD_LIMIT = 3;
     private static final int BONUS_BALL_CHECK = 5;
+
+    private static final int MIN_NUM_OF_LOTTO = 1;
 
     private List<Lotto> lottos;
 
@@ -21,7 +22,7 @@ public class Lottos {
     }
 
     private List<Lotto> createLottos(int numberOfLotto) {
-        return IntStream.range(1, numberOfLotto).boxed().map(lotto -> create()).collect(Collectors.toList());
+        return IntStream.range(MIN_NUM_OF_LOTTO, numberOfLotto).boxed().map(lotto -> create()).collect(Collectors.toList());
     }
 
     private Lotto create() {
@@ -29,19 +30,18 @@ public class Lottos {
     }
 
     public List<WinningCount> matchesLottos(Lotto winningLotto, LottoNumber bonusBall) {
-        List<WinningCount> matchLottoList = new ArrayList<>();
-        for(Lotto lotto: lottos) {
-            int count = lotto.getMatchingNumberCount(winningLotto);
-            if (count < LOTTO_REWARD_LIMIT) {
-                continue;
-            }
-            if (count == BONUS_BALL_CHECK) {
-                matchLottoList.add(WinningCount.of(count, lotto.contains(bonusBall)));
-                continue;
-            }
-            matchLottoList.add(WinningCount.of(count));
+        return lottos.stream()
+                .filter(lotto -> lotto.getMatchingNumberCount(winningLotto) >= LOTTO_REWARD_LIMIT)
+                .map(lotto -> WinningCount.of(lotto.getMatchingNumberCount(winningLotto)
+                        , checkBonusBall(lotto, bonusBall, lotto.getMatchingNumberCount(winningLotto))))
+                .collect(Collectors.toList());
+    }
+
+    private boolean checkBonusBall(Lotto lotto, LottoNumber bonusBall, int count) {
+        if (count != BONUS_BALL_CHECK) {
+            return false;
         }
-        return matchLottoList;
+        return lotto.contains(bonusBall);
     }
 
     public List<Lotto> getLottos() {
