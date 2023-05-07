358a84afd1dd0bfe2d99a4cbc37eed3a6572c09b
진우 <nice7677@naver.com>
2023-05-07 14:48:14 +0900

    feat: 지난 주 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,16 +1,29 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
     private static final String BUY_AMOUNT_INPUT_TEXT = "구입금액을 입력해 주세요.";
+    private static final String LAST_WEEK_WINNING_NUMBER_INPUT_TEXT = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String LAST_WEEK_WINNING_NUMBER_REX_PATTERN = ", ";
+
 
     public static int inputBuyAmount() {
         System.out.println(BUY_AMOUNT_INPUT_TEXT);
         return scanner.nextInt();
+    }
+
+    public static List<Integer> inputLastWeekWinningNumber() {
+        System.out.println(LAST_WEEK_WINNING_NUMBER_INPUT_TEXT);
+        return Arrays.stream(scanner.nextLine().split(LAST_WEEK_WINNING_NUMBER_REX_PATTERN))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
 }
