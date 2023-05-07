fa51b1ec2b346eb2b66fde66001207ddbb0fc8e3
dhkimneer <kimds989@naver.com>
2023-05-07 15:06:29 +0900

    feature: Add ForceLottoCreationStrategy class for testing

---
+++ src/main/java/lotto/domain/ForceLottoCreationStrategy.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ForceLottoCreationStrategy implements LottoCreationStrategy {
+
+    private static final int LOTTO_LENGTH_UPPER_BOUND = 6;
+    private static final int START_NUMBER = 1;
+    private static final int LIMIT_NUMBER = 46;
+    private static final List<Integer> candidateNumbers = IntStream.range(START_NUMBER, LIMIT_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
+
+    @Override
+    public List<Lotto> createLottos(int purchaseCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < purchaseCount; count++) {
+            lottos.add(createLotto());
+        }
+        return lottos;
+    }
+
+    private Lotto createLotto() {
+        return new Lotto(findSixLengthAndSortedLotto());
+    }
+
+    private List<Integer> findSixLengthAndSortedLotto() {
+        return candidateNumbers.stream().limit(LOTTO_LENGTH_UPPER_BOUND).sorted().collect(Collectors.toList());
+    }
+}
