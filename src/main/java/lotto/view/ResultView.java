2c625849ab17f981bb0981cefc0713134f629ed7
진우 <nice7677@naver.com>
2023-05-07 14:43:09 +0900

    feat: 로또구매목록 출력

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,11 +1,19 @@
 package lotto.view;
 
+import java.util.List;
+
 public class ResultView {
 
     private static final String BUY_COMPLETED_INPUT_TEXT = "%s개를 구매했습니다.";
 
-    public void printBuyCompleted(String count) {
-        System.out.format(BUY_COMPLETED_INPUT_TEXT, count);
+    public static void printBuyCompleted(String count) {
+        System.out.println(String.format(BUY_COMPLETED_INPUT_TEXT, count));
+    }
+
+    public static void printLottoList(List<List<Integer>> lottoList) {
+        for (List<Integer> lotto : lottoList) {
+            System.out.println(lotto);
+        }
     }
 
 }
