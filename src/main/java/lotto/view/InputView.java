8b9dae364ea9036592b597e50dfeb4815a9a233b
진우 <nice7677@naver.com>
2023-05-08 12:29:45 +0900

    refactor: 코멘트 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,22 +7,19 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    private static Scanner scanner;
-
+    private static final Scanner scanner = new Scanner(System.in);
     private static final String BUY_AMOUNT_INPUT_TEXT = "구입금액을 입력해 주세요.";
     private static final String LAST_WEEK_WINNING_NUMBER_INPUT_TEXT = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String LAST_WEEK_WINNING_NUMBER_REX_PATTERN = ", ";
 
 
     public static int inputBuyAmount() {
-        scanner = new Scanner(System.in);
         System.out.println(BUY_AMOUNT_INPUT_TEXT);
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static List<Integer> inputLastWeekWinningNumber() {
         System.out.println(LAST_WEEK_WINNING_NUMBER_INPUT_TEXT);
-        scanner = new Scanner(System.in);
         String lastWeekWinningNumber = scanner.nextLine();
         System.out.println(lastWeekWinningNumber);
         return Arrays.stream(lastWeekWinningNumber.split(LAST_WEEK_WINNING_NUMBER_REX_PATTERN))
