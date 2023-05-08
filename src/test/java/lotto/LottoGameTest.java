56d8ca47270e3713099ca9a6cbbf184f61e693ce
lehdqlsl <lehdqlsl@naver.com>
2023-05-08 19:26:04 +0900

    fix(피드백적용): 로또구매 경계값 테스트 적용.

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -5,10 +5,13 @@ import lotto.domain.LottoNumber;
 import lotto.domain.LottoNumberGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LottoGameTest {
@@ -22,11 +25,19 @@ public class LottoGameTest {
         );
     }
 
-    @Test
-    void 로또를_구매할수없음() {
-        assertThatThrownBy(() -> lottoGame.buyLotto(1))
+    @ParameterizedTest
+    @ValueSource(ints = {1,500,999})
+    void _1000원_미만시_로또를_구매할수없음(int input) {
+        assertThatThrownBy(() -> lottoGame.buyLotto(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 구매 비용이 부족합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1000,2000})
+    void _1000원_이상시_로또를_구매할수있음(int input) {
+        assertThatCode(() -> lottoGame.buyLotto(input))
+                .doesNotThrowAnyException();;
     }
 
     @Test
