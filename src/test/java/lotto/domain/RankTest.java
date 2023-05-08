21ee0e5a4e2a83f75bb2d294ed7a92775f5589c0
highjune <highjune37@gmail.com>
2023-05-09 01:29:19 +0900

    refactor(RankTest): 중복코드 삭제
    - of 함수로 중복코드 삭제

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import lotto.domian.Lotto;
-import lotto.domian.LottoNumber;
 import lotto.domian.Rank;
 import lotto.domian.WinNumber;
 import org.assertj.core.api.Assertions;
@@ -9,30 +8,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class RankTest {
 
     private WinNumber winNumber;
-    private Set<LottoNumber> lottoNumberSet = new HashSet<>();
 
     @BeforeEach
     public void setUp() {
         winNumber = new WinNumber(Lotto.of(1,2,3,4,5,6));
-
-        lottoNumberSet.add(new LottoNumber(1));
-        lottoNumberSet.add(new LottoNumber(2));
     }
 
     @DisplayName("1등을 가릴 수 있다.")
     @Test
     public void rank_DependsOnCount_ChooseFirst() {
-        lottoNumberSet.add(new LottoNumber(3));
-        lottoNumberSet.add(new LottoNumber(4));
-        lottoNumberSet.add(new LottoNumber(5));
-        lottoNumberSet.add(new LottoNumber(6));
-        Lotto lotto = new Lotto(lottoNumberSet);
+        Lotto lotto = Lotto.of(1,2,3,4,5,6);
 
         Rank rank = Rank.find(winNumber.distinguish(lotto));
 
@@ -42,11 +30,7 @@ public class RankTest {
     @DisplayName("2등을 가릴 수 있다.")
     @Test
     public void rank_DependsOnCount_ChooseSecond() {
-        lottoNumberSet.add(new LottoNumber(3));
-        lottoNumberSet.add(new LottoNumber(4));
-        lottoNumberSet.add(new LottoNumber(5));
-        lottoNumberSet.add(new LottoNumber(7));
-        Lotto lotto = new Lotto(lottoNumberSet);
+        Lotto lotto = Lotto.of(1,2,3,4,5,7);
 
         Rank rank = Rank.find(winNumber.distinguish(lotto));
 
@@ -56,11 +40,7 @@ public class RankTest {
     @DisplayName("3등을 가릴 수 있다.")
     @Test
     public void rank_DependsOnCount_ChooseThird() {
-        lottoNumberSet.add(new LottoNumber(3));
-        lottoNumberSet.add(new LottoNumber(4));
-        lottoNumberSet.add(new LottoNumber(7));
-        lottoNumberSet.add(new LottoNumber(8));
-        Lotto lotto = new Lotto(lottoNumberSet);
+        Lotto lotto = Lotto.of(1,2,3,4,7,8);
 
         Rank rank = Rank.find(winNumber.distinguish(lotto));
 
@@ -70,11 +50,7 @@ public class RankTest {
     @DisplayName("4등을 가릴 수 있다.")
     @Test
     public void rank_DependsOnCount_ChooseFourth() {
-        lottoNumberSet.add(new LottoNumber(3));
-        lottoNumberSet.add(new LottoNumber(7));
-        lottoNumberSet.add(new LottoNumber(8));
-        lottoNumberSet.add(new LottoNumber(9));
-        Lotto lotto = new Lotto(lottoNumberSet);
+        Lotto lotto = Lotto.of(1,2,3,7,8,9);
 
         Rank rank = Rank.find(winNumber.distinguish(lotto));
 
@@ -84,11 +60,7 @@ public class RankTest {
     @DisplayName("2개 이하를 맞힐 경우 예외가 발생한다.")
     @Test
     public void rank_DependsOnUnderCount_ThrowException() {
-        lottoNumberSet.add(new LottoNumber(7));
-        lottoNumberSet.add(new LottoNumber(8));
-        lottoNumberSet.add(new LottoNumber(9));
-        lottoNumberSet.add(new LottoNumber(10));
-        Lotto lotto = new Lotto(lottoNumberSet);
+        Lotto lotto = Lotto.of(1,2,7,8,9,10);
 
         Rank rank = Rank.find(winNumber.distinguish(lotto));
 
