b68469c5faa686dd36e0e3eddb772982509cb7d9
biblekimm <biblekimm931@gmail.com>
2023-05-09 21:28:12 +0900

    refactor: step2-피드백 반영

--- src/test/java/lotto/domain/PrizeTypeTest.java
+++ src/test/java/lotto/domain/PrizeTypeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class PrizeTypeTest {
-
+ 
     @Test
     void prizeTypeTest_findPrizeType() {
         assertThat(PrizeType.findPrizeType(3)).isEqualTo(PrizeType.THREE);
