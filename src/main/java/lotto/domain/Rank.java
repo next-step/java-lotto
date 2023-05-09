e1e7f01706bce6dc49e42f691f1da3efb21de17d
dhkimneer <kimds989@naver.com>
2023-05-09 23:06:05 +0900

    chore: Add winningTitle to Rank enum and move Rank.java to domain folder

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    FIRST_PLACE(6, 2_000_000_000),
-    SECOND_PLACE(5, 1_500_000),
-    THIRD_PLACE(4, 50_000),
-    FOURTH_PLACE(3, 5_000),
-    LOST(0, 0);
+    FIRST_PLACE("FIRST_PLACE", 6, 2_000_000_000),
+    SECOND_PLACE("SECOND_PLACE", 5, 1_500_000),
+    THIRD_PLACE("THIRD_PLACE", 4, 50_000),
+    FOURTH_PLACE("FOURTH_PLACE", 3, 5_000),
+    LOST("LOST", 0, 0);
 
+    private final String winningTitle;
     private final int winningCount;
     private final int winnings;
 
-    Rank(int winningCount, int winnings) {
+    Rank(String winningTitle, int winningCount, int winnings) {
+        this.winningTitle = winningTitle;
         this.winningCount = winningCount;
         this.winnings = winnings;
     }
@@ -27,6 +29,10 @@ public enum Rank {
 
     private static boolean isWinningCount(int winningCount, Rank rank) {
         return rank.winningCount == winningCount;
+    }
+
+    public String getWinningTitle() {
+        return winningTitle;
     }
 
     public int getWinningCount() {
