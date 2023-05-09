f832d541e591265191f2eb5c7b1f8b6a7de38a1e
0giri <higiri@naver.com>
2023-05-10 00:43:30 +0900

    feat: LottoInputView 구현

---
+++ src/main/java/lotto/view/LottoInputView.java
@@ -0,0 +1,18 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class LottoInputView {
+
+    public static int askBuyAmount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public static String askLastWinnerNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.nextLine();
+    }
+}
