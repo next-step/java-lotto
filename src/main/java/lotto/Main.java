cd0a7f4b7c52176d8c8537419358a97d74e3761e
0giri <higiri@naver.com>
2023-05-10 00:43:44 +0900

    feat: Lotto Main 실행 메소드 구현

---
+++ src/main/java/lotto/Main.java
@@ -0,0 +1,54 @@
+package lotto;
+
+import lotto.domain.*;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public static void main(String[] args) {
+        // 로또 생성 횟수 추출
+        int amount = LottoInputView.askBuyAmount();
+        Money money = new Money(amount);
+        int buyCount = money.buyCount(LOTTO_PRICE);
+        LottoOutputView.printBuyCount(buyCount);
+
+        // 로또 생성
+        List<Lotto> lottoList = buyManyOfLotto(buyCount);
+
+        // 지난주 당첨 번호 생성
+        String stringNumbers = LottoInputView.askLastWinnerNumbers();
+        WinnerNumbers winnerNumbers = makeWinnerNumbers(stringNumbers);
+
+        // 로또 당첨 결과 확인
+        LottoResults lottoResults = makeLottoResults(winnerNumbers, lottoList, money);
+        LottoOutputView.printLottoResults(lottoResults);
+    }
+
+    private static List<Lotto> buyManyOfLotto(int buyCount) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < buyCount; i++) {
+            Lotto lotto = LottoGenerator.generate();
+            lottoList.add(lotto);
+            LottoOutputView.printLotto(lotto);
+        }
+        return lottoList;
+    }
+
+    private static WinnerNumbers makeWinnerNumbers(String stringNumbers) {
+        Set<LottoNumber> lottoNumbers = LottoNumbersParser.parseToSet(stringNumbers);
+        return new WinnerNumbers(lottoNumbers);
+    }
+
+    private static LottoResults makeLottoResults(WinnerNumbers winnerNumbers, List<Lotto> lottoList, Money money) {
+        Map<Rank, Integer> winningStats = LottoMatcher.match(winnerNumbers, lottoList);
+        return new LottoResults(money, winningStats);
+    }
+}
