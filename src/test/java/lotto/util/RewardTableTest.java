07a53388a513e329d99b005b79286055a7e660ad
jaewon0913 <jaewon9937@gmail.com>
2023-05-09 16:17:22 +0900

    Feat(Lotto) : Step3 로또(2등) 개발
    - RewardTable 에 2등 관련 Enum 추가
    - 5개 당첨자 중 2등 인지 확인 로직 추가
    - 테스트 코드 수정

--- src/test/java/lotto/util/RewardTableTest.java
+++ src/test/java/lotto/util/RewardTableTest.java
@@ -5,32 +5,31 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.InputMismatchException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RewardTableTest {
 
-    @DisplayName("로또 게임 맞춘 갯수를 전달하면 등수를 응답한다.")
+    @DisplayName("로또 게임 맞춘 갯수와 보너스를 전달하면 등수를 응답한다.")
     @ParameterizedTest
-    @CsvSource(value = {"3:FOURTH_RANK", "4:THIRD_RANK", "5:SECOND_RANK", "6:FIRST_RANK"}, delimiterString = ":")
-    public void rewardTableTest_등수_확인_테스트(int matchRank, RewardTable rewardTableEnum) {
-        assertThat(RewardTable.of(matchRank)).isEqualTo(rewardTableEnum);
+    @CsvSource(value = {"3:FIFTH_RANK:", "4:FOURTH_RANK:","5:THIRD_RANK:false", "5:SECOND_RANK:true", "6:FIRST_RANK:"}, delimiterString = ":")
+    public void rewardTableTest_등수_확인_테스트(int matchRank, RewardTable rewardTableEnum, Boolean isBonus) {
+
+        assertThat(RewardTable.of(matchRank, isBonus)).isEqualTo(rewardTableEnum);
     }
 
     @DisplayName("로또 게임 맞춘 갯수가 등수에 없다면 에러를 응답한다.")
     @ParameterizedTest
     @ValueSource(ints = {1, 2})
     public void rewardTableTest_없는_등수_테스트(int matchRank) {
-        assertThatThrownBy(() -> RewardTable.of(matchRank))
+        assertThatThrownBy(() -> RewardTable.of(matchRank, null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 게임 맞춘 갯수와 게임 수를 전달하면 당첨 금액을 응답한다.")
     @ParameterizedTest
-    @CsvSource(value = {"3:5:25000", "4:2:100000", "5:1:150000", "6:1:2000000000"}, delimiterString = ":")
-    public void rewardTableTest_당첨금액_확인_테스트(int matchRank, int matchGameNum, int rewardNum) {
-        assertThat(RewardTable.of(matchRank).matchReward(matchGameNum)).isEqualTo(rewardNum);
+    @CsvSource(value = {"3:5:25000:", "4:2:100000:", "5:1:1500000:false", "5:1:30000000:true", "6:1:2000000000:"}, delimiterString = ":")
+    public void rewardTableTest_당첨금액_확인_테스트(int matchRank, int matchGameNum, int rewardNum, Boolean isBonus) {
+        assertThat(RewardTable.of(matchRank, isBonus).matchReward(matchGameNum)).isEqualTo(rewardNum);
     }
 }
