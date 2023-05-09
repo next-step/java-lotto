68ac887e74b08224346198ce0de074932b9621fc
강지우 <ito.mbs3@kt.com>
2023-05-09 13:35:41 +0900

    chore : LottoApplication 변경
    - 앱실행전 세팅 변경

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import lotto.domain.LottoBuyer;
-import lotto.domain.LottoGenerator;
 import lotto.domain.LottoStore;
+import lotto.domain.Store;
 
 public class LottoApplication {
     public static void main(String[] args) {
-        new LottoStore(new LottoGenerator()).open(new LottoBuyer());
+        Store store = new LottoStore();
+        LottoBuyer lottoBuyer = new LottoBuyer(store);
+        LottoGame lottoGame = new LottoGame(lottoBuyer);
+        lottoGame.start();
     }
 }
