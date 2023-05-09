7f1cd945cc299cf41cb8858b70200a42ea7f73a5
juhyun <zzang9ha@naver.com>
2023-05-10 00:23:36 +0900

    feat: 당첨 번호, 보너스 번호 중복 체크

--- src/main/java/step2/domain/entity/WinnerLotto.java
+++ src/main/java/step2/domain/entity/WinnerLotto.java
@@ -14,6 +14,7 @@ public class WinnerLotto {
 
     public WinnerLotto(LottoNumber bonusNumber, List<LottoNumber> winnerLottoNumbers) {
         validate(winnerLottoNumbers);
+        validateDuplicateNumber(winnerLottoNumbers, bonusNumber);
 
         this.bonusNumber = bonusNumber;
         this.winnerLottoNumbers = winnerLottoNumbers;
@@ -22,6 +23,14 @@ public class WinnerLotto {
     private void validate(List<LottoNumber> numbers) {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6자리여야 합니다.");
+        }
+    }
+
+    private void validateDuplicateNumber(List<LottoNumber> winnerLottoNumbers, LottoNumber bonusNumber) {
+        if (winnerLottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(
+                    "당첨 번호에 보너스 번호가 존재합니다. 당첨 번호: " + winnerLottoNumbers + ", 보너스 번호: " + bonusNumber
+            );
         }
     }
 
