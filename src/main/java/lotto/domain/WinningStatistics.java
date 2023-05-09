529520e17968cdff5371674e915196758d224a8d
jaewon0913 <jaewon9937@gmail.com>
2023-05-09 09:57:52 +0900

    Feat(Lotto) : Step2 로또(자동) 피드백
    - 각 객체 역할에 대한 명확한 역할을 지니도록 수정

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class WinningStatistics {
     private static final int MINIMUM_MATCH_COUNT = 3;
+    private static final int MAXIMUM_MATCH_COUNT = 6;
 
     private List<Integer> lottoResultNumbers;
     private Map<Integer, Integer> resultGameStatistics;
@@ -19,29 +20,25 @@ public class WinningStatistics {
     public Map<Integer, Integer> resultLottoGame(List<LottoGame> lottoGames) {
 
         for(LottoGame lottoGame : lottoGames) {
-            this.resultGameStatistics.put(lottoGame.matchLottoNumberCount(this.lottoResultNumbers)
-                    , this.resultGameStatistics.getOrDefault(lottoGame.matchLottoNumberCount(this.lottoResultNumbers), 0) + 1);
+            addWinningStatistics(lottoGame.matchLottoNumberCount(this.lottoResultNumbers));
         }
 
-        removeMinMatchCount();
-
         return this.resultGameStatistics;
+    }
+
+    private void addWinningStatistics(int matchCount) {
+        if (matchCount >= MINIMUM_MATCH_COUNT) {
+            this.resultGameStatistics.put(matchCount, this.resultGameStatistics.getOrDefault(matchCount, 0) + 1);
+        }
     }
 
     private void makeResultMap() {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i <= 6; i++) {
+        for (int i = MINIMUM_MATCH_COUNT; i <= MAXIMUM_MATCH_COUNT; i++) {
             map.put(i, 0);
         }
 
         this.resultGameStatistics = map;
-    }
-
-    private void removeMinMatchCount() {
-
-        for(int key = 0 ; key < MINIMUM_MATCH_COUNT; key++) {
-            this.resultGameStatistics.remove(key);
-        }
     }
 }
