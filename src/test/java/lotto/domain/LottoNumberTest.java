e728e51814ef006acf3d26736a7494bdda2a1e2c
dhkimneer <kimds989@naver.com>
2023-05-09 23:09:37 +0900

    test: Add LottoNumberTest.java to domain folder

---
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoNumberTest {
+
+    @Test
+    @DisplayName("[요구사항 1] 로또 번호가 1 ~ 45 사이의 숫자가 아닐 경우, IllegalArgumentException을 던진다.")
+    void 요구사항_1() {
+        // given
+        int givenNumber = 46;
+
+        // then
+        assertThatThrownBy(() -> new LottoNumber(givenNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호는 1과 45 사이의 숫자여야만 합니다.");
+    }
+}
