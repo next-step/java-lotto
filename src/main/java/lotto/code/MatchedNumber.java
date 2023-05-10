b2a78e0dea65f48d3033c1bb251bf5604eeab760
진우 <nice7677@naver.com>
2023-05-10 23:27:22 +0900

    refactor: matchedNumber 변경

--- src/main/java/lotto/code/MatchedNumber.java
+++ src/main/java/lotto/code/MatchedNumber.java
@@ -4,10 +4,11 @@ import java.util.EnumSet;
 
 public enum MatchedNumber {
 
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000),
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    FIVE_AND_BONUS(5, 30_000_000),
+    SIX(6, 2_000_000_000),
     ;
 
     private final int number;
@@ -26,12 +27,17 @@ public enum MatchedNumber {
         return amount;
     }
 
-    public static MatchedNumber findByNumber(int number) {
+    public static MatchedNumber findByNumberWithMatchedBonus(int number, boolean isMatchedBonus) {
+        if (number == 5 && isMatchedBonus) return FIVE_AND_BONUS;
         return EnumSet.allOf(MatchedNumber.class)
                 .stream()
                 .filter(matchedNumber -> number == matchedNumber.getNumber())
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("없는 일치 번호 입니다."));
+    }
+
+    public static boolean isMatchedNumberFiveAndBonus(MatchedNumber matchedNumber) {
+        return FIVE_AND_BONUS == matchedNumber;
     }
 
 }
---
+++ src/test/java/lotto/code/MatchedNumberTest.java
@@ -0,0 +1,21 @@
+package lotto.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class MatchedNumberTest {
+
+    @Test
+    void findByNumberWithMatchedBonus() {
+        assertThat(MatchedNumber.findByNumberWithMatchedBonus(3, false)).isEqualTo(MatchedNumber.THREE);
+        assertThat(MatchedNumber.findByNumberWithMatchedBonus(4, false)).isEqualTo(MatchedNumber.FOUR);
+        assertThat(MatchedNumber.findByNumberWithMatchedBonus(5, false)).isEqualTo(MatchedNumber.FIVE);
+        assertThat(MatchedNumber.findByNumberWithMatchedBonus(5, true)).isEqualTo(MatchedNumber.FIVE_AND_BONUS);
+        assertThat(MatchedNumber.findByNumberWithMatchedBonus(6, false)).isEqualTo(MatchedNumber.SIX);
+        assertThatIllegalArgumentException().isThrownBy(() -> MatchedNumber.findByNumberWithMatchedBonus(7,false))
+                .withMessageMatching("없는 일치 번호 입니다.");
+    }
+
+}
