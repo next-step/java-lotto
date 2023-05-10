99eac1c4fbd270ce05aa55c2613f2fb20e27b625
patrickYoo <yhy1045@naver.com>
2023-05-11 02:00:46 +0900

    feat: 가격 입력뷰, 수동입력 로또 입력 가능하도록 수정

--- src/main/java/lottery/view/PriceInputView.java
+++ src/main/java/lottery/view/PriceInputView.java
@@ -1,20 +1,35 @@
 package lottery.view;
 
+import static lottery.view.BlankPrintView.printBlank;
+
 import java.util.Scanner;
+import lottery.domain.LotteryPurchasedPrice;
+import lottery.domain.ManualQuantity;
 import lottery.domain.PurchasedLottery;
 
 public class PriceInputView {
 
     private static final String PRICE_INPUT_MSG = "구매금액을 입력해 주세요";
 
-    private static final String LOTTERIES_NUMBER_GUID = "개를 구매했습니다.";
+    private static final String MANUAL_QUANTITY_MSG = "수동으로 구매할 로또 수를 입력해 주세요.";
+
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static PurchasedLottery buyLotteries() {
         printSource(PRICE_INPUT_MSG);
-        PurchasedLottery purchasedLottery
-                = new PurchasedLottery(new Scanner(System.in).nextInt());
-        printSource(purchasedLottery.numberOfPlays()+LOTTERIES_NUMBER_GUID);
-        return purchasedLottery;
+
+        final LotteryPurchasedPrice lotteryPurchasedPrice = new LotteryPurchasedPrice(
+                SCANNER.nextInt());
+
+        printBlank();
+
+        printSource(MANUAL_QUANTITY_MSG);
+
+        final ManualQuantity manualQuantity = new ManualQuantity(SCANNER.nextInt());
+
+        printBlank();
+
+        return new PurchasedLottery(lotteryPurchasedPrice, manualQuantity);
     }
 
     private static void printSource(String source) {
