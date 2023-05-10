ade91bd1989f8fb0ec42a807c8427ac2acb5cb35
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-10 18:25:03 +0900

    fix : [코드리뷰 반영] 매칭볼 카운트 버그 수정

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -9,7 +9,7 @@ public enum Prize {
     FIRST_PLACE("6개 일치 (2000000000원)", 6, 2000000000),
     SECOND_PLACE("5개 일치, 보너스 볼 일치(30000000원) ", 5, 1500000),
     THIRD_PLACE("5개 일치 (1500000원)", 5, 50000),
-    FOURTH_PLACE("4개 일치 (50000원)", 3, 5000),
+    FOURTH_PLACE("4개 일치 (50000원)", 4, 5000),
     FIFTH_PLACE("3개 일치 (5000원)", 3, 5000),
     OUT_OF_PLACE("", 0, 0);
 
