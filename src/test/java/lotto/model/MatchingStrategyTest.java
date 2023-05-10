b10896e421265ec4ecaa1091927a48deebcb1ff4
henry.5 <henry.5@kakaoenterprise.com>
2023-05-10 19:58:59 +0900

    step4 - step3 피드백 반영

--- src/test/java/lotto/model/MatchingStrategyTest.java
+++ src/test/java/lotto/model/MatchingStrategyTest.java
@@ -3,10 +3,13 @@ package lotto.model;
 import lotto.model.enums.MatchingStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RankingTest {
+public class MatchingStrategyTest {
 
     @Test
     @DisplayName("2등 당첨자 보너스 당첨일 경우 추출 테스트")
@@ -20,12 +23,10 @@ public class RankingTest {
         assertThat(MatchingStrategy.find(5, false)).isEqualTo(MatchingStrategy.FIVE);
     }
 
-    @Test
     @DisplayName("당첨자 추출 테스트")
-    void check_get_winner(){
-        assertThat(MatchingStrategy.find(3, false)).isEqualTo(MatchingStrategy.THREE);
-        assertThat(MatchingStrategy.find(4, false)).isEqualTo(MatchingStrategy.FOUR);
-        assertThat(MatchingStrategy.find(5, false)).isEqualTo(MatchingStrategy.FIVE);
-        assertThat(MatchingStrategy.find(6, false)).isEqualTo(MatchingStrategy.SIX);
+    @ParameterizedTest
+    @CsvSource(value = {"3:THREE","4:FOUR","5:FIVE","6:SIX"}, delimiter = ':')
+    void check_get_winner(int element,String matchingStrategy ){
+        assertThat(MatchingStrategy.find(element, false)).isEqualTo(MatchingStrategy.valueOf(matchingStrategy));
     }
 }
