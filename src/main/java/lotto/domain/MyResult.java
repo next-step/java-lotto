11db93e6c71e83b809a035dfa77a2b9025ee2ca7
ShinYeahChan <syeahc@gmail.com>
2023-05-11 19:35:45 +0900

    refactor : prizeAmount 필드 삭제 / 수익률 계산시 rankCountMap을 통해서 계산

--- src/main/java/lotto/domain/MyResult.java
+++ src/main/java/lotto/domain/MyResult.java
@@ -7,33 +7,29 @@ import java.util.Map;
 public class MyResult {
     private final MyPurchase myPurchase;
     private Map<Rank, Integer> rankCountMap;
-    private int prizeAmount;
 
     public MyResult(MyPurchase myPurchase) {
         this.myPurchase = myPurchase;
     }
 
     public void update(List<Rank> ranks) {
-        prizeAmount = updatePrizeAmount(ranks);
         rankCountMap = updateRanksCount(ranks);
     }
 
-    private int updatePrizeAmount(List<Rank> ranks) {
-        return ranks.stream()
-                .mapToInt(Rank::getPrize)
-                .sum();
-    }
-
     private Map<Rank, Integer> updateRanksCount(List<Rank> ranks) {
-        Map<Rank,Integer> updatedRanksCount = new HashMap<>();
-        for(Rank rank : ranks){
+        Map<Rank, Integer> updatedRanksCount = new HashMap<>();
+        for (Rank rank : ranks) {
             updatedRanksCount.put(rank, updatedRanksCount.getOrDefault(rank, 0) + 1);
         }
         return updatedRanksCount;
     }
 
     public double profit() {
-        return (prizeAmount + 0.0) / (myPurchase.money());
+        double totalPrizeAmount = 0;
+        for (Rank rank : Rank.values()) {
+            totalPrizeAmount += countRank(rank) * rank.getPrize();
+        }
+        return totalPrizeAmount / (myPurchase.money());
     }
 
     public int countRank(Rank rank) {
