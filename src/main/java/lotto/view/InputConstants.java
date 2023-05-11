f9204ca1a4626652b35338fcc42af739000c25e1
henry.5 <henry.5@kakaoenterprise.com>
2023-05-11 18:28:57 +0900

    step4 - 입력 로직 리펙트룅

---
+++ src/main/java/lotto/view/InputConstants.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class InputConstants {
+    public static final String PURCHASE_QUESTION = "구입금액을 입력해 주세요.";
+    public static final String MANUAL_LOTTO_COUNT_QUESTION = "수동으로 구매할 로또 수를 입력해 주세요.";
+    public static final String MANUAL_LOTTO_NUMBERS_QUESTION = "수동으로 구매할 번호를 입력해 주세요.";
+    public static final String WINNING_NUMBERS_QUESTION = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_QUESTION = "보너스 볼을 입력해 주세요.";
+}
