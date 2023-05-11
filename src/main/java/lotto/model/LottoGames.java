96ecba291b89b6dbb1623752e159c8bc672a3d10
henry.5 <henry.5@kakaoenterprise.com>
2023-05-11 18:31:13 +0900

    step4 - 통계로직 버그 수정

--- src/main/java/lotto/model/LottoGames.java
+++ src/main/java/lotto/model/LottoGames.java
@@ -6,11 +6,11 @@ import lotto.util.RandomUtil;
 import java.util.*;
 
 public class LottoGames {
-    public static final int NUMBER_SIZE = 6;
-    public static final int NUMBER_RANGE = 45;
     private List<Lotto> games;
     private Set<Integer> winningNumbers;
     private Integer bonusNumber;
+    private Integer manualLottoCount;
+    private Integer purchaseValue;
     private Map<MatchingStrategy, List<Lotto>> aggregatedGames = new LinkedHashMap<>();
 
     public LottoGames() {
@@ -18,28 +18,22 @@ public class LottoGames {
         this.initResult();
     }
 
-    public LottoGames(int count) {
-        this();
-        this.initGames(count);
-    }
-
-    private void initResult() {
-//        aggregatedGames.put(MatchingStrategy.SIX, new ArrayList<>());
-//        aggregatedGames.put(MatchingStrategy.FIVE_WITH_BONUS, new ArrayList<>());
-//        aggregatedGames.put(MatchingStrategy.FIVE, new ArrayList<>());
-//        aggregatedGames.put(MatchingStrategy.FOUR, new ArrayList<>());
-//        aggregatedGames.put(MatchingStrategy.THREE, new ArrayList<>());
-    }
-
-    private void initGames(int count) {
+    public void initGames(int count) {
         for (int i = 0; i < count; i++) {
             addLotto(initLotto());
         }
     }
 
+    private void initResult() {
+        aggregatedGames.put(MatchingStrategy.THREE, new ArrayList<>());
+        aggregatedGames.put(MatchingStrategy.FOUR, new ArrayList<>());
+        aggregatedGames.put(MatchingStrategy.FIVE, new ArrayList<>());
+        aggregatedGames.put(MatchingStrategy.FIVE_WITH_BONUS, new ArrayList<>());
+        aggregatedGames.put(MatchingStrategy.SIX, new ArrayList<>());
+    }
 
     private Lotto initLotto() {
-        return new Lotto(RandomUtil.generateRandomList(NUMBER_SIZE,NUMBER_RANGE));
+        return new Lotto(RandomUtil.generateRandomList(Lotto.NUMBER_SIZE,Lotto.NUMBER_RANGE));
     }
 
     public void addLotto(Lotto lotto) {
@@ -56,6 +50,7 @@ public class LottoGames {
 
     public List<Lotto> getGames(){ return games; }
 
+
     public void aggregate() {
         games.forEach(this::calculateEqualCount);
     }
@@ -70,7 +65,8 @@ public class LottoGames {
 
         Optional.ofNullable(MatchingStrategy.find(count, isContained))
                 .ifPresent((result)->  {
-                        List<Lotto> elements = aggregatedGames.getOrDefault(result, List.of(ticket));
+                        List<Lotto> elements = aggregatedGames.get(result);
+                        elements.add(ticket);
                         aggregatedGames.put(result, elements);
                     }
                 );
@@ -78,6 +74,26 @@ public class LottoGames {
 
     public Map<MatchingStrategy, List<Lotto>> getStatistic(){
         return Collections.unmodifiableMap(aggregatedGames);
+    }
+
+    public void setManualLottoCount(int count) {
+        this.manualLottoCount = count;
+    }
+
+    public int getManualLottoCount() {
+        return this.manualLottoCount;
+    }
+
+    public int getAutoLottoCount() {
+        return (this.purchaseValue / Lotto.LOTTO_PRICE) - this.getManualLottoCount();
+    }
+
+    public void setPurchaseValue(int purchaseValue) {
+        this.purchaseValue = purchaseValue;
+    }
+
+    public int getPurchaseValue() {
+        return this.purchaseValue;
     }
 
     public void setBonusNumber(int input) {
