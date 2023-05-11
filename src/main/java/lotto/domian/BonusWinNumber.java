7589731bfd09a292ecd2eb379031457c46724520
highjune <highjune37@gmail.com>
2023-05-11 11:04:59 +0900

    feat(BonusWinNumber): 보너스 볼의 범위
    - 보너스볼 단일 멤버변수 객체 생성
    - 테스트 코드 작성

---
+++ src/main/java/lotto/domian/BonusWinNumber.java
@@ -0,0 +1,10 @@
+package lotto.domian;
+
+public class BonusWinNumber {
+
+    private final LottoNumber bonusBall;
+
+    public BonusWinNumber(LottoNumber bonusBall) {
+        this.bonusBall = bonusBall;
+    }
+}
