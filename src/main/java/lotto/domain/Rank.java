7c053421986141ad8984f3302f43267c30313ff8
ryan <wsdf25867@gmail.com>
2023-05-11 22:30:12 +0900

    refactor : Rank 수정
    - Rank 에서 view 로직 제거

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,43 +1,47 @@
 package lotto.domain;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public enum Rank {
-    FIRST(6, new Money(2_000_000_000L), "6개 일치 (2000000000원)- %d개"),
-    SECOND(5, new Money(30_000_000L), "5개 일치, 보너스 볼 일치(30000000원)- %d개"),
-    THIRD(5, new Money(1_500_000L), "5개 일치 (1500000원)- %d개"),
-    FOURTH(4, new Money(50_000L), "4개 일치 (50000원)- %d개"),
-    FIFTH(3, new Money(5_000L), "3개 일치 (5000원)- %d개"),
-    MISS(0, new Money(0L), "");
+    FIRST(6, new Money(2_000_000_000L)),
+    SECOND(5, new Money(30_000_000L)),
+    THIRD(5, new Money(1_500_000L)),
+    FOURTH(4, new Money(50_000L)),
+    FIFTH(3, new Money(5_000L)),
+    MISS(0, new Money(0L));
 
     private final int count;
     private final Money money;
-    private final String message;
+    private static final Map<Long, Rank> RANK_CACHE;
 
-    Rank(int count, Money money, String message) {
+    static {
+        RANK_CACHE = new HashMap<>();
+        RANK_CACHE.put(6L, Rank.FIRST);
+        RANK_CACHE.put(5L, Rank.THIRD);
+        RANK_CACHE.put(4L, Rank.FOURTH);
+        RANK_CACHE.put(3L, Rank.FIFTH);
+        RANK_CACHE.put(0L, Rank.MISS);
+    }
+
+    Rank(int count, Money money) {
         this.count = count;
         this.money = money;
-        this.message = message;
     }
 
     public static Rank of(long count, boolean hasBonus) {
-        if (count == 5 && hasBonus) {
+        if (count == SECOND.count && hasBonus) {
             return SECOND;
         }
-        if (count == 5) {
-            return THIRD;
-        }
-        return Arrays.stream(values())
-                .filter(match -> match.count == count)
-                .findAny()
-                .orElse(MISS);
+        return Optional.ofNullable(RANK_CACHE.get(count)).orElse(Rank.MISS);
     }
 
     public Money getMoney() {
         return money;
     }
 
-    public String getMessage() {
-        return message;
+    public int getCount() {
+        return count;
     }
 }
