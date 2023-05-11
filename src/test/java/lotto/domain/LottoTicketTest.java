8ee68c56d27787a5899b873c2bfe25b9f9c1d469
SeokyungKim <kskyung0624@gmail.com>
2023-05-12 00:03:01 +0900

    test : 불필요한 import를 제거한다

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -2,8 +2,6 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import lotto.domain.LottoTicket;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
