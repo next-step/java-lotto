28c3baae4260c17e0e11ab06fc3e192fe8d00126
woony-kim <woony.kim@balancehero.com>
2023-05-12 21:35:42 +0900

    refactor methodSource 적용

--- src/test/java/domain/WinningAnalyzerTest.java
+++ src/test/java/domain/WinningAnalyzerTest.java
@@ -3,45 +3,22 @@ package domain;
 import static domain.WinningStatistics.WINNING_PRIZES;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class WinningAnalyzerTest {
 
-    @Test
-    public void 지난주_당첨번호를_입력하면_당첨통계를_제공한다() throws Exception {
-        //given
-        List<int[]> lottoList = new ArrayList<>();
-        int[] firstRound = { 2, 4, 6, 7, 10, 12 };
-        int[] secondRound = { 1, 4, 6, 7, 10, 12 };
-        int[] thirdRound = { 1, 2, 3, 4, 5, 6 };
-        int[] fourthRound = { 1, 4, 5, 6, 15, 17 };
-        int[] fifthRound = { 1, 3, 15, 17, 18, 19 };
-        int[] sixthRound = { 1, 3, 15, 17, 29, 30 };
-        int[] seventhRound = { 1, 3, 15, 17, 29, 31 };
-        lottoList.add(firstRound);
-        lottoList.add(secondRound);
-        lottoList.add(thirdRound);
-        lottoList.add(fourthRound);
-        lottoList.add(fifthRound);
-        lottoList.add(sixthRound);
-        lottoList.add(seventhRound);
-        LottoResults lottoResults = new LottoResults(lottoList);
 
-        List<Integer> winningNumbers = new ArrayList<>();
-        winningNumbers.add(1);
-        winningNumbers.add(3);
-        winningNumbers.add(15);
-        winningNumbers.add(17);
-        winningNumbers.add(29);
-        winningNumbers.add(31);
+    @MethodSource("provideLottoList")
+    @ParameterizedTest
+    public void 지난주_당첨번호를_입력하면_당첨통계를_제공한다(List<int[]> lottoList, List<Integer> winningNumbers) throws Exception {
+        //given
+        LottoResults lottoResults = new LottoResults(lottoList);
         WinningAnalyzer winningAnalyzer = new WinningAnalyzer(lottoResults, winningNumbers);
         //when
         WinningStatistics winningStatistics = winningAnalyzer.calculateWinningStatistics();
--- src/test/java/domain/WinningStatisticsTest.java
+++ src/test/java/domain/WinningStatisticsTest.java
@@ -8,28 +8,29 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class WinningStatisticsTest {
+
     @Test
     public void 로또_결과를_입력받으면_매칭_결과를_반환한다() {
         //given
-        List<Integer> lastWeekWinningResults = new ArrayList<>();
-        lastWeekWinningResults.add(1);
-        lastWeekWinningResults.add(3);
-        lastWeekWinningResults.add(15);
-        lastWeekWinningResults.add(17);
-        lastWeekWinningResults.add(21);
-        lastWeekWinningResults.add(35);
-        WinningStatistics winningStatistics = new WinningStatistics(lastWeekWinningResults);
+        List<Integer> winningNumbers = new ArrayList<>();
+        winningNumbers.add(1);
+        winningNumbers.add(3);
+        winningNumbers.add(15);
+        winningNumbers.add(17);
+        winningNumbers.add(21);
+        winningNumbers.add(35);
+        WinningStatistics winningStatistics = new WinningStatistics(winningNumbers);
 
-        int[] lottoResultWithFourthPrize = { 1, 3, 15, 7, 9, 11 };
-        int[] lottoResultWithThirdPrize = { 1, 3, 15, 17, 9, 11 };
-        int[] lottoResultWithSecondPrize = { 1, 3, 15, 17, 9, 21 };
-        int[] lottoResultWithFirstPrize = { 1, 3, 15, 17, 21, 35 };
+        int[] fourthPrize = { 1, 3, 15, 7, 9, 11 };
+        int[] thirdPrize = { 1, 3, 15, 17, 9, 11 };
+        int[] secondPrize = { 1, 3, 15, 17, 9, 21 };
+        int[] firstPrize = { 1, 3, 15, 17, 21, 35 };
 
         //when
-        winningStatistics.matchCount(lottoResultWithFourthPrize);
-        winningStatistics.matchCount(lottoResultWithThirdPrize);
-        winningStatistics.matchCount(lottoResultWithSecondPrize);
-        winningStatistics.matchCount(lottoResultWithFirstPrize);
+        winningStatistics.matchCount(fourthPrize);
+        winningStatistics.matchCount(thirdPrize);
+        winningStatistics.matchCount(secondPrize);
+        winningStatistics.matchCount(firstPrize);
 
         //then
         assertThat(winningStatistics.getWinningResults()[0]).isEqualTo(1);
