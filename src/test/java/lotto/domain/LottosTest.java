b5cd6c52552522c232259c6395ad9cc999e7c6b7
ryan <wsdf25867@gmail.com>
2023-05-12 21:54:42 +0900

    feat : 수동 로또와 자동로또 합치는 기능 추가
    - 수동 로또와 자동로또 합치는 기능 추가 및 테스트

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -15,14 +15,38 @@ class LottosTest {
         Lottos lottos = getLottos();
         Lotto winningLotto = new Lotto(1, 2, 3, 4, 5, 6);
 
-        Map<Rank, Long> rankResult = lottos.countMatching(new WinningLotto(winningLotto, new LottoNumber(7)));
-        assertThat(rankResult).containsValues(1L, 1L, 1L, 1L, 1L);
+        RankResult rankResult = lottos.countMatching(new WinningLotto(winningLotto, new LottoNumber(7)));
+        assertThat(rankResult).isEqualTo(RankResultTest.getRankResult());
     }
 
     @Test
     void size() {
         Lottos lottos = getLottos();
         assertThat(lottos.size()).isEqualTo(5);
+    }
+
+    @Test
+    void addAll() {
+        Lottos lottos1 = getLottos();
+        Lottos lottos2 = getLottos();
+        Lottos actual = lottos1.addAll(lottos2);
+        Lottos expect = getAddAllLottos();
+
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    private static Lottos getAddAllLottos() {
+        return new Lottos(List.of(
+                new Lotto(1, 2, 3, 4, 5, 6),
+                new Lotto(2, 3, 4, 5, 6, 7),
+                new Lotto(2, 3, 4, 5, 6, 9),
+                new Lotto(3, 4, 5, 6, 7, 8),
+                new Lotto(4, 5, 6, 7, 8, 9),
+                new Lotto(1, 2, 3, 4, 5, 6),
+                new Lotto(2, 3, 4, 5, 6, 7),
+                new Lotto(2, 3, 4, 5, 6, 9),
+                new Lotto(3, 4, 5, 6, 7, 8),
+                new Lotto(4, 5, 6, 7, 8, 9)));
     }
 
     public static Lottos getLottos() {
