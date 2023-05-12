17505c0624f8bcd12b0c5faa8a23924809c47ea3
김성경 <102819554+biblekimm@users.noreply.github.com>
2023-05-12 17:30:06 +0900

    feat: step3-로또(2등) (#3179)

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,10 +1,32 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.Lottos;
+import lotto.domain.Statistics;
 import lotto.view.InputView;
 
 public class LottoMain {
 
     public static void main(String[] args) {
-        InputView.executeLotto();
+        executeLotto();
+    }
+
+    private static void executeLotto() {
+        String money = InputView.inputMoney();
+        Lottos lottos = buyLotto(money);
+
+        generateStatistics(lottos, money);
+    }
+
+    private static Lottos buyLotto(String money) {
+        return LottoController.generateLotto(money);
+    }
+
+    private static void generateStatistics(Lottos lottos, String money) {
+        String winnerNumber = InputView.inputWinnerNumber();
+        int bonusNumber = InputView.inputBonusNumber();
+
+        Statistics statistics = LottoController.generateStatistics(lottos, winnerNumber, bonusNumber);
+        LottoController.findRateOfReturn(money, statistics);
     }
 }
