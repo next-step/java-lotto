a6f693a930dd90b1e6c98424b3cffb5c9d0976b2
ryan <wsdf25867@gmail.com>
2023-05-12 21:51:19 +0900

    feat : 수동 입력 기능 추가
    - 수동 입력 개수 및 번호 받는 기능 추가
    - 수동 입력 개수 및 번호 출력 기능 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -4,7 +4,6 @@ import lotto.domain.Rank;
 import lotto.dto.LottoDto;
 import lotto.dto.LottosDto;
 import lotto.dto.RankResultDto;
-import stringcalculator.Calculator;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -12,14 +11,16 @@ import java.util.List;
 import java.util.Map;
 
 public class ResultView {
-    public static void printResultBuyCount(long purchaseCount) {
-        System.out.printf("%d개를 구매했습니다.\n", purchaseCount);
+    public static void printResultBuyCount(long manualLottosCount, long autoLottosCount) {
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.\n", manualLottosCount, autoLottosCount);
     }
 
-    public static void printLottos(LottosDto lottoDtos) {
-        List<LottoDto> lottos = lottoDtos.getLottos();
-        printResultBuyCount(lottos.size());
-        lottos.forEach(ResultView::printLotto);
+    public static void printLottos(LottosDto manualLottosDto, LottosDto autoLottosDto) {
+        List<LottoDto> manualLottos = manualLottosDto.getLottos();
+        List<LottoDto> autoLottos = autoLottosDto.getLottos();
+        printResultBuyCount(manualLottos.size(), autoLottos.size());
+        manualLottos.forEach(ResultView::printLotto);
+        autoLottos.forEach(ResultView::printLotto);
     }
     private static void printLotto(LottoDto lotto) {
         System.out.println(lotto.getLotto());
