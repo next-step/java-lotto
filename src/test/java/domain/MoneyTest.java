efc0e7c98d22ddc69a7521170dd9688fd32db5dd
woony-kim <woony.kim@balancehero.com>
2023-05-12 21:39:44 +0900

    refactor moneyTest 메소드명 수정

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
     @Test
-    public void 금액이_1000원_미만으로_들어오면_예외처리한다() {
+    public void 금액이_1000원_미만으로_들어오면_예외를_던진다() {
         //given
         int input = 999;
 
