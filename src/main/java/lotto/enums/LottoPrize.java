1f378b3fd57fc05d997f9bf6bde2e97d56818c46
신형기 <tkaqkeldk@hanatour.com>
2023-05-13 13:48:28 +0900

    fix: LottoPrize Test 실패케이스 수정

--- src/main/java/lotto/enums/LottoPrize.java
+++ src/main/java/lotto/enums/LottoPrize.java
@@ -3,27 +3,23 @@ package lotto.enums;
 import java.util.Arrays;
 
 public enum LottoPrize {
-    FIRST(6, 2_000_000_000),
+    FIRST(6, false, 2_000_000_000),
 
     SECOND(5, true, 30_000_000),
 
     THIRD(5, false, 1_500_000),
 
-    FOURTH(4, 50_000),
+    FOURTH(4, false,50_000),
 
-    FIFTH(3, 5_000),
+    FIFTH(3,false, 5_000),
 
-    MISS(0, 0);
+    MISS(0, false, 0);
 
     private final int matchCount;
 
     private final boolean hasBonusNumber;
 
     private final int prizeMoney;
-
-    LottoPrize(int matchCount, int prizeMoney) {
-        this(matchCount, false, prizeMoney);
-    }
 
     LottoPrize(int matchCount, boolean hasBonusNumber, int prizeMoney) {
         this.matchCount = matchCount;
@@ -37,7 +33,7 @@ public enum LottoPrize {
         }
 
         return Arrays.stream(values())
-                .filter(prize -> prize.matchCount == matchCount)
+                .filter(prize -> prize.matchCount == matchCount && prize.hasBonusNumber == hasBonusNumber)
                 .findFirst()
                 .orElse(MISS);
     }
