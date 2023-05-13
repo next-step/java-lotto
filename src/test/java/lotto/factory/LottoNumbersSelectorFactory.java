fb3fae1c38640e7fe86d8e68f3bf2e10a2167855
SeokyungKim <kskyung0624@gmail.com>
2023-05-13 16:51:46 +0900

    test : mutable하도록 List.of에서 ArrayList로 변경한다

--- src/test/java/lotto/factory/LottoNumbersSelectorFactory.java
+++ src/test/java/lotto/factory/LottoNumbersSelectorFactory.java
@@ -2,18 +2,19 @@ package lotto.factory;
 
 import static lotto.domain.LottoNumbers.pick;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.LottoNumbersSelector;
 
 public class LottoNumbersSelectorFactory {
 
   public static LottoNumbersSelector oneToSix() {
-    return number -> List.of(pick("1"), pick("2"), pick("3"),
-        pick("4"), pick("5"), pick("6"));
+    return number -> new ArrayList<>(List.of(pick("1"), pick("2"), pick("3"),
+        pick("4"), pick("5"), pick("6")));
   }
 
   public static LottoNumbersSelector oneToFiveAndSeven() {
-    return number -> List.of(pick("1"), pick("2"), pick("3"),
-        pick("4"), pick("5"), pick("7"));
+    return number -> new ArrayList<>(List.of(pick("1"), pick("2"), pick("3"),
+        pick("4"), pick("5"), pick("7")));
   }
 }
