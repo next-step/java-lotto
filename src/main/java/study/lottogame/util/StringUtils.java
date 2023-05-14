fac40f348d3d4ae22b02bd2b171d22c3fd1dbf0d
hoa0217 <gjwjdghk123@naver.com>
2023-05-14 16:31:44 +0900

    refactor: 유틸성 클래스 생성자 private로 제한

--- src/main/java/study/lottogame/util/StringUtils.java
+++ src/main/java/study/lottogame/util/StringUtils.java
@@ -2,8 +2,11 @@ package study.lottogame.util;
 
 public class StringUtils {
 
+  private StringUtils() {
+  }
+
   public static void checkNullOrBlank(String number) {
-    if(number == null || number.isBlank()){
+    if (number == null || number.isBlank()) {
       throw new IllegalArgumentException("유효하지 않은 입력입니다.");
     }
   }
