29b88479f72ab4dd67d0ecf49141ce0ebcb151f7
dhkimneer <kimds989@naver.com>
2023-05-14 21:40:33 +0900

    test: Update LottoResultTest.java because of bonus number

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -10,18 +10,18 @@ import static org.assertj.core.api.Assertions.*;
 
 class LottoResultTest {
 
-    private static final List<Rank> ranks = Arrays.asList(Rank.SECOND_PLACE, Rank.SECOND_PLACE, Rank.THIRD_PLACE, Rank.FOURTH_PLACE, Rank.LOST);
+    private static final List<Rank> ranks = Arrays.asList(Rank.THIRD_PLACE, Rank.THIRD_PLACE, Rank.FOURTH_PLACE, Rank.FIFTH_PLACE, Rank.LOST);
     private static final LottoResult lottoResult = new LottoResult(ranks);
 
     @Test
-    @DisplayName("[요구사항 1] 주어진 복권 결과들 중 2등은 2개이다.")
+    @DisplayName("[요구사항 1] 주어진 복권 결과들 중 3등은 2개이다.")
     void 요구사항_1() {
         // given
-        int secondPlaceCount = 2;
-        Rank rank = Rank.SECOND_PLACE;
+        int thirdPlaceCount = 2;
+        Rank rank = Rank.THIRD_PLACE;
 
         // then
-        assertThat(lottoResult.findWinningCount(rank)).isEqualTo(secondPlaceCount);
+        assertThat(lottoResult.findWinningCount(rank)).isEqualTo(thirdPlaceCount);
     }
 
     @Test
