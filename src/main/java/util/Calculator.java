9edd2f845dabcb1868adb9dfbcbad8d244baece8
letitgobaby <gnu@gnuui-MacBookPro.local>
2023-05-14 14:14:14 +0900

    feat : Calculator 유틸 객체 생성

--- src/main/java/util/Calculator.java
+++ src/main/java/util/Calculator.java
@@ -2,4 +2,19 @@ package util;
 
 public class Calculator {
     
+    public static int plus(int leftNum, int rightNum) {
+        return leftNum + rightNum;
+    }
+
+    public static int minus(int leftNum, int rightNum) {
+        return leftNum - rightNum;
+    }
+
+    public static int multiply(int leftNum, int rightNum) {
+        return leftNum * rightNum;
+    }
+
+    public static int division(int leftNum, int rightNum) {
+        return leftNum / rightNum;
+    }
 }
