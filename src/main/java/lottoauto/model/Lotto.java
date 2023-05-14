4b1747869f05337b4eb0ca2106096da5d7f8a3a2
kyoungil.yoon <dev.atypical@gmail.com>
2023-05-14 19:27:38 +0900

    refactor: magic number

--- src/main/java/lottoauto/model/Lotto.java
+++ src/main/java/lottoauto/model/Lotto.java
@@ -10,6 +10,7 @@ import java.util.stream.IntStream;
 public class Lotto {
 
     public static final List<Integer> LOTTO_NUMBER_RANGE = IntStream.range(1, 45).boxed().collect(Collectors.toList());
+    private static final int LOTTO_LIMIT_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto() {
@@ -21,6 +22,16 @@ public class Lotto {
 
         this.numbers = lottoNumbers;
     }
+    private void lottoValidation(List<Integer> lottoNumber) {
+        if (lottoNumber.size() != LOTTO_LIMIT_SIZE) {
+            throw new IllegalStateException("로또의 숫자가 6개가 아닙니다.");
+        }
+
+        HashSet<Integer> distinctNumbers = new HashSet<>(lottoNumber);
+        if (distinctNumbers.size() != LOTTO_LIMIT_SIZE) {
+            throw new IllegalStateException("중복되는 숫자가 있습니다.");
+        }
+    }
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
@@ -30,15 +41,8 @@ public class Lotto {
         return this.numbers;
     }
 
-    private void lottoValidation(List<Integer> lotto) {
-        if (lotto.size() != 6) {
-            throw new IllegalStateException("로또의 숫자가 6개가 아닙니다.");
-        }
-
-        HashSet<Integer> distinctNumbers = new HashSet<>(lotto);
-        if (distinctNumbers.size() != 6) {
-            throw new IllegalStateException("중복되는 숫자가 있습니다.");
-        }
+    public boolean contains(int bonusNumber) {
+        return numbers.contains(bonusNumber);
     }
 
 }
--- src/main/java/lottoauto/model/WinningLotto.java
+++ src/main/java/lottoauto/model/WinningLotto.java
@@ -1,11 +1,13 @@
 package lottoauto.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class WinningLotto {
 
+    private final int LOTTO_MATCH_COUNT_5 = 5;
     private final List<Integer> winningLotto;
     private final int bonusNumber;
 
@@ -18,18 +20,26 @@ public class WinningLotto {
 
     public WinningReward compare(Lotto lotto) {
 
-        boolean existBonus = lotto.getNumbers().contains(bonusNumber);
+        boolean existBonus = lotto.contains(bonusNumber);
         List<Integer> matchNumbers = lotto.getNumbers().stream()
                 .filter(number -> winningLotto.stream()
                         .anyMatch(Predicate.isEqual(number)))
                 .collect(Collectors.toList());
 
-        if (matchNumbers.size() == 5) {
+        if (matchNumbers.size() == WinningReward.MATCH_5.getMatchCount()) {
             return calculateBonus(existBonus);
         }
 
-        return WinningReward.findRewardByMatches(matchNumbers.size());
+        return WinningReward.findRewardByMatchCount(matchNumbers.size());
 
+    }
+
+    public LottoResult result(Lottos lottos) {
+        List<WinningReward> matchCount = new ArrayList<>();
+        for (Lotto lotto : lottos.getLottos()) {
+            matchCount.add(compare(lotto));
+        }
+        return new LottoResult(matchCount);
     }
 
     private WinningReward calculateBonus(boolean existBonus) {
@@ -48,7 +58,6 @@ public class WinningLotto {
         if (winningNumber.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 숫자가 당첨 숫자에 포함되어 있습니다.");
         }
-
 
         if (!Lotto.LOTTO_NUMBER_RANGE.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 숫자는 1~45까지의 숫자를 입력해야 합니다.");
