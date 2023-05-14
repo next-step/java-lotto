6b368a6c642da53ee1e8bed88ccc9c0781bb850e
hoa0217 <gjwjdghk123@naver.com>
2023-05-14 16:06:06 +0900

    refactor: Rank 생성자 private 추가

--- src/main/java/study/lottogame/domain/Rank.java
+++ src/main/java/study/lottogame/domain/Rank.java
@@ -13,7 +13,7 @@ public enum Rank {
   private int countOfMatch;
   private int winningMoney;
 
-  Rank(int countOfMatch, int winningMoney) {
+  private Rank(int countOfMatch, int winningMoney) {
     this.countOfMatch = countOfMatch;
     this.winningMoney = winningMoney;
   }
