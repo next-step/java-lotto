de689f041f635e4b92f781fe5590a2fe5448ac22
dhkimneer <kimds989@naver.com>
2023-05-14 21:36:11 +0900

    chore: Update InputView.java because of requesting bonus number

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 
     private static final String REQUEST_PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBERS_INPUT = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String REQUEST_BONUS_NUMBER_INPUT = "보너스 볼을 입력해 주세요.";
     private static final String SPLITTER = ", ";
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -20,6 +21,11 @@ public class InputView {
     public List<Integer> requestWinningNumbers() {
         System.out.println(REQUEST_WINNING_NUMBERS_INPUT);
         return Arrays.stream(SCANNER.nextLine().split(SPLITTER)).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public int requestBonusNumber() {
+        System.out.println(REQUEST_BONUS_NUMBER_INPUT);
+        return Integer.parseInt(SCANNER.nextLine());
     }
 }
 
