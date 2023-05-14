01b9c5b424a08bfc951191317b6814db498c24e2
woonys <wodns1324@naver.com>
2023-05-14 23:59:50 +0900

    refactor LottoNumber 일급 컬렉션 추가

--- src/main/java/domain/LottoNumGenerator.java
+++ src/main/java/domain/LottoNumGenerator.java
@@ -1,9 +1,6 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class LottoNumGenerator {
     private static final int LOTTO_NUM_LIMIT = 45;
@@ -11,15 +8,32 @@ public class LottoNumGenerator {
 
     private static final List<Integer> LOTTO_BALLS = new ArrayList<>();
 
+    private static final Map<Integer, LottoNumber> LOTTO_NUMBER_MAP = new HashMap<>();
+
     static {
         for (int i = 1; i <= LOTTO_NUM_LIMIT; i++) {
             LOTTO_BALLS.add(i);
         }
 
+        for (int i = 1; i <= LOTTO_NUM_LIMIT; i++) {
+            LOTTO_NUMBER_MAP.put(i, new LottoNumber(i));
+        }
     }
 
     public static int[] generateNumbers() {
         int[] generatedNumbers = new int[LOTTO_SIZE];
+        Collections.shuffle(LOTTO_BALLS);
+        Collections.shuffle();
+        for (int i = 0; i < LOTTO_SIZE; i++) {
+            generatedNumbers[i] = LOTTO_BALLS.get(i);
+        }
+        Arrays.sort(generatedNumbers);
+
+        return generatedNumbers;
+    }
+
+    public static List<LottoNumber> generateNumbers2() {
+        List<LottoNumber> generatedNumbers = new ArrayList<>();
         Collections.shuffle(LOTTO_BALLS);
         for (int i = 0; i < LOTTO_SIZE; i++) {
             generatedNumbers[i] = LOTTO_BALLS.get(i);
---
+++ src/main/java/domain/LottoNumber.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class LottoNumber {
+
+    private final int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+}
