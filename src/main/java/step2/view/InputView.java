cf7638ac70aeba2133bdc62e32759dd2bed94e3b
std-freejia <std.freejia@gmail.com>
2023-05-14 23:22:09 +0900

    test: 구매 금액 입력받기

---
+++ src/main/java/step2/view/InputView.java
@@ -0,0 +1,14 @@
+package step2.view;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int inputCost() {
+        System.out.println("구매할 금액을 입력해주세요.");
+        return scanner.nextInt();
+    }
+}
