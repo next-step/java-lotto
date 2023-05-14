884a7b10e38f91e3c1be7796798dccf6bb126754
dhkimneer <kimds989@naver.com>
2023-05-14 21:31:17 +0900

    chore: Move validation logic to LottoNumbersValidationUtils.java

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,23 +2,20 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.utils.LottoNumbersValidationUtils.validateLottoNumbers;
+
 public class Lotto {
 
     public static final int LOTTO_PRICE = 1_000;
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
     public static int findPurchaseCount(int purchaseAmount) {
         return (int) purchaseAmount / LOTTO_PRICE;
-    }
-
-    public int getMatchNumbers(List<Integer> winningNumbers) {
-        return (int) this.getLottoNumbers().stream()
-                .filter(winningNumbers::contains)
-                .count();
     }
 
     public List<Integer> getLottoNumbers() {
