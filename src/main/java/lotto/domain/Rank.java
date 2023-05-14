46397a391b12ae67b9775bdd4645c8ce034413fa
dhkimneer <kimds989@naver.com>
2023-05-14 21:33:49 +0900

    chore: Update Second place and move calculation logic to WinningLotto class

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,13 +1,12 @@
 package lotto.domain;
 
-import java.util.Arrays;
-
 public enum Rank {
 
     FIRST_PLACE("FIRST_PLACE", 6, 2_000_000_000),
-    SECOND_PLACE("SECOND_PLACE", 5, 1_500_000),
-    THIRD_PLACE("THIRD_PLACE", 4, 50_000),
-    FOURTH_PLACE("FOURTH_PLACE", 3, 5_000),
+    SECOND_PLACE("SECOND_PLACE", 5, 30_000_000),
+    THIRD_PLACE("THIRD_PLACE", 5, 1_500_000),
+    FOURTH_PLACE("FOURTH_PLACE", 4, 50_000),
+    FIFTH_PLACE("FIFTH_PLACE", 3, 5_000),
     LOST("LOST", 0, 0);
 
     private final String winningTitle;
@@ -18,17 +17,6 @@ public enum Rank {
         this.winningTitle = winningTitle;
         this.winningCount = winningCount;
         this.winnings = winnings;
-    }
-
-    public static Rank findRank(int winningCount) {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> isWinningCount(winningCount, rank))
-                .findFirst()
-                .orElse(Rank.LOST);
-    }
-
-    private static boolean isWinningCount(int winningCount, Rank rank) {
-        return rank.winningCount == winningCount;
     }
 
     public String getWinningTitle() {
