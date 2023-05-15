ab41bf6712af768760629d51e27a8a89f8e7f2c4
woony-kim <woony.kim@balancehero.com>
2023-05-15 23:03:18 +0900

    refactor methodSource 추가

--- src/test/java/domain/WinningStatisticsTest.java
+++ src/test/java/domain/WinningStatisticsTest.java
@@ -3,33 +3,27 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class WinningStatisticsTest {
 
-    @Test
-    public void 로또_결과를_입력받으면_매칭_결과를_반환한다() {
+    @MethodSource("provideWinningNumbers")
+    @ParameterizedTest
+    public void 로또_결과를_입력받으면_매칭_결과를_반환한다(List<Integer> winningNumbers, List<int[]> prizes) {
         //given
-        List<Integer> winningNumbers = new ArrayList<>();
-        winningNumbers.add(1);
-        winningNumbers.add(3);
-        winningNumbers.add(15);
-        winningNumbers.add(17);
-        winningNumbers.add(21);
-        winningNumbers.add(35);
         WinningStatistics winningStatistics = new WinningStatistics(winningNumbers);
 
-        int[] fourthPrize = { 1, 3, 15, 7, 9, 11 };
-        int[] thirdPrize = { 1, 3, 15, 17, 9, 11 };
-        int[] secondPrize = { 1, 3, 15, 17, 9, 21 };
-        int[] firstPrize = { 1, 3, 15, 17, 21, 35 };
-
-        LottoNumber[] fourthPrizes = LottoNumber.fromIntegers(fourthPrize);
-        LottoNumber[] thirdPrizes = LottoNumber.fromIntegers(thirdPrize);
-        LottoNumber[] secondPrizes = LottoNumber.fromIntegers(secondPrize);
-        LottoNumber[] firstPrizes = LottoNumber.fromIntegers(firstPrize);
+        LottoNumber[] fourthPrizes = LottoNumber.fromIntegers(prizes.get(0));
+        LottoNumber[] thirdPrizes = LottoNumber.fromIntegers(prizes.get(1));
+        LottoNumber[] secondPrizes = LottoNumber.fromIntegers(prizes.get(2));
+        LottoNumber[] firstPrizes = LottoNumber.fromIntegers(prizes.get(3));
 
         //when
         winningStatistics.matchCount(fourthPrizes);
@@ -44,5 +38,19 @@ public class WinningStatisticsTest {
         assertThat(winningStatistics.getWinningResults()[3]).isEqualTo(1);
         assertThat(winningStatistics.getTotalWinnings()).isEqualTo(2001555000);
 
+    }
+
+    private static Stream<Arguments> provideWinningNumbers() {
+        return Stream.of(
+            Arguments.of(
+                Arrays.asList(1, 3, 15, 17, 21, 35),
+                Arrays.asList(
+                    new int[]{ 1, 3, 15, 7, 9, 11 },
+                    new int[]{ 1, 3, 15, 17, 9, 11 },
+                    new int[]{ 1, 3, 15, 17, 9, 21 },
+                    new int[]{ 1, 3, 15, 17, 21, 35 }
+                )
+            )
+        );
     }
 }
