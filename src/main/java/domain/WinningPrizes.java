02b9e10fb306f2896933ce30f844d12a855e639f
woony-kim <woony.kim@balancehero.com>
2023-05-15 22:58:34 +0900

    feat 보너스 볼 객체 구현

--- src/main/java/domain/WinningPrizes.java
+++ src/main/java/domain/WinningPrizes.java
@@ -2,9 +2,10 @@ package domain;
 
 public enum WinningPrizes {
     FIRST_PRIZE(2000000000),
-    SECOND_PRIZE(1500000),
-    THIRD_PRIZE(50000),
-    FOURTH_PRIZE(5000);
+    SECOND_PRIZE(3000000),
+    THIRD_PRIZE(1500000),
+    FOURTH_PRIZE(50000),
+    FIFTH_PRIZE(5000);
 
     private int prizeMoney;
 
