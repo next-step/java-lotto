3831c765871907538f9944375dba7471dd16d3cd
seominah <seominah95@gmail.com>
2023-05-16 01:31:51 +0900

    Feat: Money 생성 및 테스트 코드 추가

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class Money {
+    private int money;
+
+    public Money(String money) {
+        int intMoney = toIntMoney(money);
+        initialize(intMoney);
+    }
+
+    public Money(int money) {
+        initialize(money);
+    }
+
+    private int toIntMoney(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘몬된 값을 입력하였습니다.");
+        }
+    }
+
+    private void initialize(int money) {
+        if (money < 1) {
+            throw new IllegalArgumentException("음수 또는 0이 아닌 숫자를 입력해주세요.");
+        }
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money1 = (Money) o;
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+}
