020d37e0e4f1882c61c6ff14a6c7313ec8999bc3
yes <yes.chicken@kakaocorp.com>
2023-05-15 18:42:32 +0900

    test: 테스트 분리

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -48,8 +48,19 @@ public class LottosTest {
         Lotto lotto4 = new Lotto(Set.of(1, 2, 3, 4, 5, 8));
         Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3, lotto4));
         WinNumbers winNumbers = new WinNumbers(Set.of(1, 2, 3, 4, 5, 6), 7);
+        assertThat(lottos.getEarningRate(winNumbers)).isEqualTo(388.75);
+    }
+
+    @Test
+    @DisplayName("이득인지 손해인지 확인한다")
+    public void gainOrLoss() {
+        Lotto lotto1 = new Lotto(Set.of(1, 3, 5, 14, 22, 45));
+        Lotto lotto2 = new Lotto(Set.of(3, 5, 11, 16, 32, 38));
+        Lotto lotto3 = new Lotto(Set.of(1, 2, 3, 4, 7, 8));
+        Lotto lotto4 = new Lotto(Set.of(1, 2, 3, 4, 5, 8));
+        Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3, lotto4));
+        WinNumbers winNumbers = new WinNumbers(Set.of(1, 2, 3, 4, 5, 6), 7);
         assertAll(
-                () -> assertThat(lottos.getEarningRate(winNumbers)).isEqualTo(388.75),
                 () -> assertThat(lottos.isGain(winNumbers)).isTrue(),
                 () -> assertThat(lottos.isLoss(winNumbers)).isFalse()
         );
