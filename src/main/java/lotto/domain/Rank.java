e86cd4879d67e5437cd07d5cf13af67c03aec5c7
yes <yes.chicken@kakaocorp.com>
2023-05-15 18:39:44 +0900

    refactor: 언더바를 이용해 상금 가독성 높이기

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     LOSE(2, 0);
 
     private final int matchCount;
