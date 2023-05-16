d75792dee5ab966cdd3e0caaf01b0560a2bf8c4a
KangHo-dev <tiger452003@gmail.com>
2023-05-17 04:54:12 +0900

    feat : 보너스 번호포함 5개 일치할 경우의 등수 알아내기 기능.

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -7,13 +7,14 @@ import java.util.stream.Collectors;
 
 public enum Rank {
 
-    FIRST(6, new Money(2_000_000_000)),
-    SECOND(5, new Money(1_500_000)),
-    THIRD(4, new Money(1_500_000)),
+    NONE(0, new Money(0)),
     FOURTH(3, new Money(5_000)),
-    NONE(0, new Money(0));
-    private static final Map<Integer, Rank> lotto_rank = Arrays.stream(values())
-            .collect(Collectors.toMap(Rank::count, Function.identity()));
+    THIRD(4, new Money(1_500_000)),
+    SECOND(5, new Money(1_500_000)),
+    BONUS_RANK(5, new Money(30_000_000)),
+    FIRST(6, new Money(2_000_000_000));
+
+    public static final int BONUS_RANK_COUNT = 5;
 
     private final int count;
     private final Money prize;
@@ -23,16 +24,26 @@ public enum Rank {
         this.prize = prize;
     }
 
-    public static Rank of(int count, boolean isWin) {
-        if (isWin) {
-            return lotto_rank.getOrDefault(count, NONE);
+    public static Rank findByCount(int count, boolean hasBonusBall) {
+        if (count == BONUS_RANK_COUNT && hasBonusBall) {
+            return BONUS_RANK;
         }
 
-        return NONE;
+        return Arrays.stream(values())
+                .filter(rank -> rank.isMatch(count))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private boolean isMatch(int count) {
+        return this.count == count;
     }
 
     public static Rank of(int count) {
-        return lotto_rank.getOrDefault(count, NONE);
+        return Arrays.stream(values())
+                .filter(rank -> rank.count == count)
+                .findFirst()
+                .orElse(NONE);
     }
 
     public static Map<Rank, Integer> initLottoResult() {
