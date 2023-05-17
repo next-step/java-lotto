ff72581366e53ef4e2cddc28dbb579962137811d
bahar-j <joonie811@naver.com>
2023-05-17 22:49:49 +0900

    refactor: 보너스 번호 validation 추가

--- src/test/java/lotto/model/LotteryTest.java
+++ src/test/java/lotto/model/LotteryTest.java
@@ -187,4 +187,13 @@ public class LotteryTest {
         assertThat(autoLotteryTickets.size()).isEqualTo(11);
     }
 
+    @Test
+    void 보너스번호는_당첨번호와_겹칠수_없음() {
+        Set<Integer> winNumbers = Set.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 1;
+        assertThatThrownBy(() -> new WinNumbers(winNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호는 당첨 번호와 겹칠 수 없습니다.");
+    }
+
 }
