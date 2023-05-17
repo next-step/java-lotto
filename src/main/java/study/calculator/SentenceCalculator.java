5ddeb77b340434ce23465c98cb75fcbca3ca400e
letitgobaby <48303144+letitgobaby@users.noreply.github.com>
2023-05-17 13:46:01 +0900

    Step1 문자열 계산기 (#3208)
    * doc : step1 요구사항 추가
    
    * feat : Calculator 클래스 생성
    
    * feat : gitignore 수정
    
    * feat : Calculator 유틸 객체 생성
    
    * refactor : Calculator 유틸 클래스 삭제
    
    * feat : SentenceCalculator 사칙연산 클래스 추가
    
    * doc : README.md 요구사항 체크
    
    * feat : Operation Enum 클래스 추가
    
    * refactor : SentenceCalculator 객체 내에서 사칙연산 로직을 enum으로 교체
    
    * refactor : SentenceCalculator 사용하지 않는 상태 값 정리
    
    ---------
    
    Co-authored-by: letitgobaby <gnu@gnuui-MacBookPro.local>

---
+++ src/main/java/study/calculator/SentenceCalculator.java
@@ -0,0 +1,63 @@
+package study.calculator;
+
+import study.enums.Operation;
+
+public class SentenceCalculator {
+
+    String separation = " ";
+
+    int opreationResult = 0;
+    String inputSentence = null;
+    
+
+    public SentenceCalculator(String sentence) {
+        isValidSentence(sentence);
+        this.inputSentence = sentence;
+    }
+
+
+    public int doCalculate() {
+        String[] values = inputSentence.split(separation);
+
+        opreationResult = toNumber(values[0]);
+
+        for (int i = 1; i < values.length; i++) {
+            opreationResult = addOperation(values[i], values[++i]);
+        }
+
+
+        return opreationResult;
+    }
+
+
+    private int addOperation(String operationText, String rightValue) {
+        int rightNum = toNumber(rightValue);
+
+        Operation operation = Operation.findBy(operationText);
+        return operation.calculate(opreationResult, rightNum);
+    }
+
+
+    private void isValidSentence(String sentence) {
+        if (sentence == null || sentence.trim().isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 연산입니다");
+        }
+
+        if (!sentence.contains(separation)) {
+            throw new IllegalArgumentException("연산은 반드시 빈 공백 문자열을 포함해야 합니다");
+        }
+
+        if (sentence.split(separation).length < 3) {
+            throw new IllegalArgumentException("완전하지 않은 연산입니다");
+        }
+    }
+
+
+    private int toNumber(String textNum) {
+        if (textNum == null || textNum.trim() == "") {
+            throw new IllegalArgumentException("입력한 값이 유효하지 않습니다");
+        }
+        return Integer.parseInt(textNum);
+    }
+
+}
