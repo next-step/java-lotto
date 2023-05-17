49d9a144175f53a00e5ca2058c39aa2e547505d3
highjune <highjune37@gmail.com>
2023-05-17 23:54:27 +0900

    refactor(Store): static 메서드로 변경

--- src/main/java/lotto/domian/Store.java
+++ src/main/java/lotto/domian/Store.java
@@ -2,30 +2,30 @@ package lotto.domian;
 
 public class Store {
 
-    public LottoBundle order(Money money) {
+    public static LottoBundle order(Money money) {
         return LottoCompany.makeLottoBundle(decideCount(money));
     }
 
-    public Money orderManual(Money money, int manualOrderCount) {
+    public static Money orderManual(Money money, int manualOrderCount) {
         validateCount(manualOrderCount);
         validateMoney(money.amount(), manualOrderCount);
         return new Money(money.amount() - manualOrderCount * Lotto.PURCHASE_UNIT);
     }
 
-    private void validateCount(int manualOrderCount) {
+    private static void validateCount(int manualOrderCount) {
         if (manualOrderCount < 0) {
             throw new IllegalArgumentException("수동로또 구매숫자는 음수가 될 수 없습니다.");
         }
     }
 
-    private void validateMoney(int amount, int manualOrderCount) {
+    private static void validateMoney(int amount, int manualOrderCount) {
         if (amount / Lotto.PURCHASE_UNIT < manualOrderCount) {
             throw new IllegalArgumentException("로또를 구매하기에 돈이 부족합니다.");
         }
     }
 
 
-    private int decideCount(Money money) {
+    private static int decideCount(Money money) {
         return money.amount() / Lotto.PURCHASE_UNIT;
     }
 
--- src/test/java/lotto/domain/StoreTest.java
+++ src/test/java/lotto/domain/StoreTest.java
@@ -17,45 +17,39 @@ public class StoreTest {
         Money money = new Money(4000);
         int count = money.amount() / 1000;
 
-        Store store = new Store();
-
-        LottoBundle bundle = store.order(money);
+        LottoBundle bundle = Store.order(money);
         Assertions.assertThat(bundle.unfoldLottoBundle().size()).isEqualTo(count);
     }
 
     @DisplayName("음수만큼 구매하려고 할 경우 예외를 던진다.")
     @Test
     public void orderManual_NegativeNumber_ThrowException() {
-        Store store = new Store();
-        Assertions.assertThatThrownBy(() -> store.orderManual(new Money(1000), -1))
+        Assertions.assertThatThrownBy(() -> Store.orderManual(new Money(1000), -1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("금액보다 더 많이 구매하려고 하는 경우 예외를 던진다.")
     @Test
     public void orderManual_OrderMoreThanMoney_ThrowException() {
-        Store store = new Store();
-        Assertions.assertThatThrownBy(() -> store.orderManual(new Money(2000), 4))
+        Assertions.assertThatThrownBy(() -> Store.orderManual(new Money(2000), 4))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("금액만큼 수동로또를 산 후 나머지 금액을 반환한다.")
     @Test
     public void orderManual_ReturnChangeAfterManualLotto_ChangeMoneyCorrect() {
-        Store store = new Store();
-        Assertions.assertThat(store.orderManual(new Money(5000), 3)).isEqualTo(new Money(2000));
+        Assertions.assertThat(Store.orderManual(new Money(5000), 3)).isEqualTo(new Money(2000));
     }
 
-    @DisplayName("구입금액에서 수동로또 갯수만큼 차감한 수만큼 자동로또를 생성한다.")
+    @DisplayName("구입금액에서 수동로또 갯수 차감한 수만큼 자동로또를 생성한다.")
     @Test
     public void order_Calculate_CountSame() {
         int purchaseMoney = 14000;
         int manualOrderCount = 3;
         int autoLottoSize = (14000 - 3 * Lotto.PURCHASE_UNIT) / Lotto.PURCHASE_UNIT;
 
-        Store store = new Store();
-        Money change = store.orderManual(new Money(purchaseMoney), manualOrderCount);
-        LottoBundle autoLottoBundle = store.order(change);
+        Money change = Store.orderManual(new Money(purchaseMoney), manualOrderCount);
+        LottoBundle autoLottoBundle = Store.order(change);
 
         Assertions.assertThat(autoLottoBundle.unfoldLottoBundle().size()).isEqualTo(autoLottoSize);
 
