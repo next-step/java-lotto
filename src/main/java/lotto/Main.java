d8bd1652431ba830e5bd7e080b049710dfd46030
JhyunJung <jhyun.rigel@gmail.com>
2023-05-17 21:59:25 +0900

    fix: 코드 리뷰 반영
    Input의 Scanner 등 수정

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -8,24 +8,22 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useDelimiter(",\\s*|\r?\n");
-        int money = InputView.inputPay(scanner);
+        int money = InputView.inputPay();
         Customer customer = new Customer(money);
-        int manualCount = InputView.inputCount(scanner);
+        int manualCount = InputView.inputCount();
         List<Set<Integer>> manualNumbers = new ArrayList<>();
         if (manualCount > 0) {
-            manualNumbers = InputView.inputManualNumbers(manualCount, scanner);
+            manualNumbers = InputView.inputManualNumbers(manualCount);
         }
 
         customer.buyLotto(manualCount, manualNumbers);
 
         OutputView.showLottos(customer.getManualLotto(), customer.getAutoLotto());
-        Set<Integer> lastNumbers = InputView.lastWinNumbers(scanner);
-        int bonusNumber = InputView.inputBonusNumber(scanner);
+        Set<Integer> lastNumbers = InputView.lastWinNumbers();
+        int bonusNumber = InputView.inputBonusNumber();
         WinNumber winNumber = new WinNumber(lastNumbers, bonusNumber);
 
         Map<KLottoRank, Integer> lottoResult = customer.checkLottoWin(winNumber);
         OutputView.LottoResult(lottoResult, customer);
-        scanner.close();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,63 +4,61 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class InputView {
-    public static int inputPay(Scanner scanner) {
+
+    public static int inputPay() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("구입 금액을 입력하세요.");
         return scanner.nextInt();
     }
 
-    public static Set<Integer> lastWinNumbers(Scanner scanner) {
+    public static Set<Integer> lastWinNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-
-        Set<Integer> numbers = new HashSet<>();
-        inputLastNumbers(numbers, scanner);
+        Set<Integer> numbers = inputLastNumbers();
         return numbers;
     }
 
-    public static int inputBonusNumber(Scanner scanner) {
+    public static int inputBonusNumber() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("보너스 볼을 입력해 주세요.");
         return scanner.nextInt();
     }
 
-    private static void inputLastNumbers(Set<Integer> numbers, Scanner scanner) {
-        int count = 0;
-        while (count < 6 && scanner.hasNextInt()) {
-            int number = scanner.nextInt();
-            numbers.add(number);
-            count++;
+    private static Set<Integer> inputLastNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        String[] lottoNumbers = scanner.nextLine().split("[\\s,]+");
+        if (lottoNumbers.length > 6) {
+            throw new IllegalStateException("로또 번호는 6개까지 입력 됩니다.");
         }
-        if (count == 6 && scanner.hasNext()) {
-            System.out.println("입력값이 6개를 초과하여 추가 입력은 무시됩니다.");
-        }
+        return Arrays.stream(lottoNumbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
     }
 
-    public static int inputCount(Scanner scanner) {
+    public static int inputCount() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         return scanner.nextInt();
     }
 
-    public static List<Set<Integer>> inputManualNumbers(int manualCount, Scanner scanner) {
+    public static List<Set<Integer>> inputManualNumbers(int manualCount) {
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
         List<Set<Integer>> numberList = new ArrayList<>();
         for (int i = 0; i < manualCount; i++) {
-            Set<Integer> lottoNumbers = inputLottoNumbers(scanner);
+            Set<Integer> lottoNumbers = inputLottoNumbers();
             numberList.add(lottoNumbers);
         }
 
         return numberList;
     }
 
-    private static Set<Integer> inputLottoNumbers(Scanner scanner) {
-        Set<Integer> lottoNumbers = new HashSet<>();
-        int count = 0;
-        while (count < 6 && scanner.hasNextInt()) {
-            int number = scanner.nextInt();
-            lottoNumbers.add(number);
-            count++;
+    private static Set<Integer> inputLottoNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        String[] lottoNumbers = scanner.nextLine().split("[\\s,]+");
+        if (lottoNumbers.length > 6) {
+            throw new IllegalStateException("로또 번호는 6개까지 입력 됩니다.");
         }
-        if (count == 6 && scanner.hasNext()) {
-            System.out.println("입력값이 6개를 초과하여 추가 입력은 무시됩니다.");
-        }
-        return lottoNumbers;
+        return Arrays.stream(lottoNumbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
     }
 }
