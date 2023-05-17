c70749ea51dc7df53b1d783ed8064ae237265414
prolkh <prolkh@gmail.com>
2023-05-18 02:18:58 +0900

    refactor : 메소드 길이를 줄이고 indent 단계를 줄이다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,11 +1,29 @@
 package stringcalculator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StringCalculator {
-    public static Integer calculate(String inputString) {
+    public int calculate(String inputString) {
+        // 유효성 검사
+        validationCheck(inputString);
 
+        String[] tokens = inputString.split(" ");
+
+        String operator = "";
+        int result = 0;
+
+        for (String token : tokens) {
+            // token이 연산자일때 처리
+            if (isOperator(token)) {
+                operator = token;
+                continue;
+            }
+            // token이 숫자일때 연산
+            result = getCalculatedResult(operator, result, token);
+        }
+
+        return result;
+    }
+
+    private void validationCheck(String inputString) {
         if (inputString == null || inputString.length() == 0) {
             throw new IllegalArgumentException("null을 입력하였습니다.");
         }
@@ -14,42 +32,34 @@ public class StringCalculator {
         if (!inputString.matches(exp)) {
             throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
         }
+    }
 
-        String[] tokens = inputString.split(" ");
+    private boolean isOperator(String token) {
+        return "+".equals(token)
+                || "-".equals(token)
+                || "*".equals(token)
+                || "/".equals(token);
+    }
 
-        String operator = "";
-        int result = 0;
+    private int getCalculatedResult(String operator, int result, String token) {
+        int num = Integer.parseInt(token);
 
-        for (String token : tokens) {
-            // 연산자 처리
-            if ("+".equals(token)
-                    || "-".equals(token)
-                    || "*".equals(token)
-                    || "/".equals(token)) {
-                operator = token;
-                continue;
-            }
-
-            // 숫자 처리
-            int num = Integer.parseInt(token);
-
-            if ("".equals(operator)) {
-                result = num;
-            }
-            if ("+".equals(operator)) {
-                result += num;
-            }
-            if ("-".equals(operator)) {
-                result -= num;
-            }
-            if ("*".equals(operator)) {
-                result *= num;
-            }
-            if ("/".equals(operator)) {
-                result /= num;
-            }
+        if ("".equals(operator)) {
+            return num;
+        }
+        if ("+".equals(operator)) {
+            return result + num;
+        }
+        if ("-".equals(operator)) {
+            return result - num;
+        }
+        if ("*".equals(operator)) {
+            return result * num;
+        }
+        if ("/".equals(operator)) {
+            return result / num;
         }
 
-        return result;
+        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
     }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -7,28 +7,35 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringCalculatorTest {
 
-    private StringCalculator stringCalculator;
+    private StringCalculator stringCalculator = new StringCalculator();
+
+    @Test
+    void 모든_사칙연산() {
+        Integer result = stringCalculator.calculate("5 + 4 - 3 * 2 / 2");
+
+        assertThat(result).isEqualTo(6);
+    }
 
     @Test
     void 덧셈() {
-        Integer result1 = StringCalculator.calculate("2 + 3");
+        Integer result1 = stringCalculator.calculate("2 + 3");
 
         assertThat(result1).isEqualTo(5);
 
 
-        Integer result2 = StringCalculator.calculate("4 + 3 + 2 + 1");
+        Integer result2 = stringCalculator.calculate("4 + 3 + 2 + 1");
 
         assertThat(result2).isEqualTo(10);
     }
 
     @Test
     void 뺄셈() {
-        Integer result1 = StringCalculator.calculate("2 - 3");
+        Integer result1 = stringCalculator.calculate("2 - 3");
 
         assertThat(result1).isEqualTo(-1);
 
 
-        Integer result2 = StringCalculator.calculate("4 + 3 - 2 - 1");
+        Integer result2 = stringCalculator.calculate("4 + 3 - 2 - 1");
 
         assertThat(result2).isEqualTo(4);
     }
@@ -36,12 +43,12 @@ public class StringCalculatorTest {
 
     @Test
     void 곱셈() {
-        Integer result1 = StringCalculator.calculate("2 * 3");
+        Integer result1 = stringCalculator.calculate("2 * 3");
 
         assertThat(result1).isEqualTo(6);
 
 
-        Integer result2 = StringCalculator.calculate("4 * 3 - 2 + 1");
+        Integer result2 = stringCalculator.calculate("4 * 3 - 2 + 1");
 
         assertThat(result2).isEqualTo(11);
     }
@@ -49,12 +56,12 @@ public class StringCalculatorTest {
 
     @Test
     void 나눗셈() {
-        Integer result1 = StringCalculator.calculate("3 / 2");
+        Integer result1 = stringCalculator.calculate("3 / 2");
 
         assertThat(result1).isEqualTo(1);
 
 
-        Integer result2 = StringCalculator.calculate("4 / 3 - 2 + 1");
+        Integer result2 = stringCalculator.calculate("4 / 3 - 2 + 1");
 
         assertThat(result2).isEqualTo(0);
     }
@@ -62,18 +69,18 @@ public class StringCalculatorTest {
     @Test
     void 입력값이_null() {
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            StringCalculator.calculate(null);
+            stringCalculator.calculate(null);
         }).withMessage("null을 입력하였습니다.");
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            StringCalculator.calculate("");
+            stringCalculator.calculate("");
         }).withMessage("null을 입력하였습니다.");
     }
 
     @Test
     void 사칙연산_기호가_아닌_경우() {
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            StringCalculator.calculate("2 & 3");
+            stringCalculator.calculate("2 & 3");
         }).withMessage("사칙연산 기호가 아닙니다.");
     }
 }
