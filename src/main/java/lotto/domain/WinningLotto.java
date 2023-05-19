dbd9059ca9ebdfd0883b53ebe9020cdb42cd543f
dhkimneer <kimds989@naver.com>
2023-05-19 21:50:14 +0900

    chore: Update WinningLotto.java

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,54 +1,19 @@
 package lotto.domain;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static lotto.utils.LottoNumberValidationUtils.*;
-import static lotto.utils.LottoNumbersValidationUtils.validateLottoNumbers;
+import java.util.Set;
 
 public class WinningLotto {
 
-    private final List<Integer> winningNumbers;
-    private final int bonusLottoNumber;
+    private final Set<LottoNumber> winningNumbers;
+    private final LottoNumber bonusLottoNumber;
 
-    public WinningLotto(List<Integer> winningNumbers, int bonusLottoNumber) {
-        validateLottoNumbers(winningNumbers);
-        isLottoCandidateNumber(bonusLottoNumber);
+    public WinningLotto(Set<LottoNumber> winningNumbers, LottoNumber bonusLottoNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusLottoNumber = bonusLottoNumber;
     }
 
     public Rank calculateRank(Lotto lotto) {
-        return findRank(this.getMatchNumbers(lotto), hasBonusNumber(lotto));
-    }
-
-    public Rank findRank(int winningCount, boolean hasBonusNumber) {
-        if (isSecondPlace(winningCount, hasBonusNumber)) {
-            return Rank.SECOND_PLACE;
-        }
-        if (isThirdPlace(winningCount, hasBonusNumber)) {
-            return Rank.THIRD_PLACE;
-        }
-        return findCertainPlace(winningCount);
-    }
-
-    private Rank findCertainPlace(int winningCount) {
-        return Arrays.stream(Rank.values())
-                .filter(rank -> isWinningCount(winningCount, rank))
-                .findFirst()
-                .orElse(Rank.LOST);
-    }
-
-    private static boolean isThirdPlace(int winningCount, boolean hasBonusNumber) {
-        return winningCount == Rank.THIRD_PLACE.getWinningCount() && !hasBonusNumber;
-    }
-
-    private static boolean isSecondPlace(int winningCount, boolean hasBonusNumber) {
-        return winningCount == Rank.SECOND_PLACE.getWinningCount() && hasBonusNumber;
-    }
-
-    public boolean isWinningCount(int winningCount, Rank rank) {
-        return rank.getWinningCount() == winningCount;
+        return Rank.findRank(getMatchNumbers(lotto), hasBonusNumber(lotto));
     }
 
     public int getMatchNumbers(Lotto lotto) {
@@ -57,7 +22,7 @@ public class WinningLotto {
                 .count();
     }
 
-    public boolean hasBonusNumber(Lotto lotto) {
+    private boolean hasBonusNumber(Lotto lotto) {
         return lotto.getLottoNumbers().contains(bonusLottoNumber);
     }
 }
