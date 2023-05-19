5667ca5ae33c45ce35c56381b1e9979242f6d480
dhkimneer <kimds989@naver.com>
2023-05-19 21:51:44 +0900

    test: Update LottosTest.java

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
-import lotto.test.ForceLottoCreationStrategy;
+import lotto.domain.test.ForceLottoCreationStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +18,7 @@ public class LottosTest {
     @DisplayName("[요구사항 1] 당첨 여부에 따라 올바른 당첨 개수가 산출되고 당첨금이 지급된다. (2등)")
     void 요구사항_1() {
         // given: 당첨 번호 생성 / shuffle하지 않고 PURCHASE_COUNT만큼 복권 만드는 전략 생성
-        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 7), 6);
+        WinningLotto winningLotto = new WinningLotto(Set.of(1, 2, 3, 4, 5, 7).stream().map(LottoNumber::new).collect(Collectors.toSet()), new LottoNumber(6));
         ForceLottoCreationStrategy forceLottoCreationStrategy = new ForceLottoCreationStrategy();
 
         // when: 로또 생성
@@ -36,7 +37,7 @@ public class LottosTest {
     @DisplayName("[요구사항 2] 당첨 여부에 따라 올바른 당첨 개수가 산출되고 당첨금이 지급된다. (3등)")
     void 요구사항_2() {
         // given: 당첨 번호 생성 / shuffle하지 않고 PURCHASE_COUNT만큼 복권 만드는 전략 생성
-        WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 7), 9);
+        WinningLotto winningLotto = new WinningLotto(Set.of(1, 2, 3, 4, 5, 7).stream().map(LottoNumber::new).collect(Collectors.toSet()), new LottoNumber(9));
         ForceLottoCreationStrategy forceLottoCreationStrategy = new ForceLottoCreationStrategy();
 
         // when: 로또 생성
