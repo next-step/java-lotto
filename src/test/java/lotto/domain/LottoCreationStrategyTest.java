6c5eff36201ca442905a48dedfae9455d4a7ece6
dhkimneer <kimds989@naver.com>
2023-05-19 21:52:16 +0900

    test: Update LottoCreationStrategyTest.java

--- src/test/java/lotto/domain/LottoCreationStrategyTest.java
+++ src/test/java/lotto/domain/LottoCreationStrategyTest.java
@@ -1,11 +1,12 @@
 package lotto.domain;
 
-import lotto.test.ForceLottoCreationStrategy;
+import lotto.domain.test.ForceLottoCreationStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,7 +14,7 @@ public class LottoCreationStrategyTest {
 
     private static final int PURCHASE_COUNT = 3;
     private static final int EXPECTED_WINNING_COUNT = 6;
-    private static final WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 9);
+    private static final WinningLotto winningLotto = new WinningLotto(Set.of(1, 2, 3, 4, 5, 6).stream().map(LottoNumber::new).collect(Collectors.toSet()), new LottoNumber(9));
 
     @Test
     @DisplayName("[요구사항 1] shuffle하지 않고 PURCHASE_COUNT만큼 복권을 만들면 winningLotto가 PURCHASE_COUNT만큼 생성되어야 한다.")
