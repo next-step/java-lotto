1ae046685816bab2c9d3f90862275eeb2bded319
dhkimneer <kimds989@naver.com>
2023-05-19 21:51:30 +0900

    test: Update LottoTest.java

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,7 +1,11 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,5 +20,17 @@ public class LottoTest {
 
         // then
         assertThat(Lotto.findPurchaseCount(purchaseAmount)).isEqualTo(expectedResult);
+    }
+
+    @Test
+    @DisplayName("[요구사항 2] 로또 번호 개수가 6개가 아닐 경우 IllegalArgumentException throw")
+    void 요구사항_2() {
+        // given
+        Set<LottoNumber> lottoNumbers = Set.of(1, 2, 3, 4, 5).stream().map(LottoNumber::new).collect(Collectors.toSet());
+
+        // then
+        Assertions.assertThatThrownBy(() -> new Lotto(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호는 6개의 숫자로 구성되어야 합니다.");
     }
 }
