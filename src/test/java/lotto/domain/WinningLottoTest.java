f8ce53fcc9a13aaa4298cfa5d5ff39d8f4713779
dhkimneer <kimds989@naver.com>
2023-05-19 21:52:30 +0900

    test: Update WinningLottoTest.java

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,7 +15,7 @@ public class WinningLottoTest {
 
     @BeforeEach
     void setUp() {
-        winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6), 9);
+        winningLotto = new WinningLotto(Set.of(1, 2, 3, 4, 5, 6).stream().map(LottoNumber::new).collect(Collectors.toSet()), new LottoNumber(9));
     }
 
     @Test
@@ -24,64 +25,20 @@ public class WinningLottoTest {
         Rank expectedRank = Rank.FIRST_PLACE;
 
         // when: 로또 구입
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(Set.of(1, 2, 3, 4, 5, 6).stream().map(LottoNumber::new).collect(Collectors.toSet()));
 
         // then
         assertThat(winningLotto.calculateRank(lotto)).isEqualTo(expectedRank);
     }
 
     @Test
-    @DisplayName("[요구사항 2] 로또 번호가 2개 일치하는 경우에는 보너스 숫자가 맞아도 LOST, 즉 꽝이다.")
+    @DisplayName("[요구사항 2] 당첨 번호와 동일한 번호들을 가진 로또를 구입했을 때 일치되는 숫자는 6개이다.")
     void 요구사항_2() {
-        // given
-        int winningCount = 2;
-        boolean hasBonusNumber = true;
-
-        // then
-        assertThat(winningLotto.findRank(winningCount, hasBonusNumber)).isEqualTo(Rank.LOST);
-    }
-
-    @Test
-    @DisplayName("[요구사항 3] 로또 번호가 5개 일치하고 보너스 숫자가 맞으면 2등이다.")
-    void 요구사항_3() {
-        // given
-        int winningCount = 5;
-        boolean hasBonusNumber = true;
-
-        // then
-        assertThat(winningLotto.findRank(winningCount, hasBonusNumber)).isEqualTo(Rank.SECOND_PLACE);
-    }
-
-    @Test
-    @DisplayName("[요구사항 4] 로또 번호가 5개 일치하지만 보너스 숫자가 틀리면 3등이다.")
-    void 요구사항_4() {
-        // given
-        int winningCount = 5;
-        boolean hasBonusNumber = false;
-
-        // then
-        assertThat(winningLotto.findRank(winningCount, hasBonusNumber)).isEqualTo(Rank.THIRD_PLACE);
-    }
-
-    @Test
-    @DisplayName("[요구사항 5] 5등을 하려면 일치되는 개수가 3개여야 한다.")
-    void 요구사항_5() {
-        // given
-        int winningCount = 3;
-        Rank rank = Rank.FIFTH_PLACE;
-
-        // then
-        assertThat(winningLotto.isWinningCount(winningCount, rank)).isTrue();
-    }
-
-    @Test
-    @DisplayName("[요구사항 6] 당첨 번호와 동일한 번호들을 가진 로또를 구입했을 때 일치되는 숫자는 6개이다.")
-    void 요구사항_6() {
         // given: 기대 되는 결과
         int expectedMatchCount = 6;
 
         // when: 복권 당첨 번호와 같은 번호들을 가진 로또 구입
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(Set.of(1, 2, 3, 4, 5, 6).stream().map(LottoNumber::new).collect(Collectors.toSet()));
 
         // then
         assertThat(winningLotto.getMatchNumbers(lotto)).isEqualTo(expectedMatchCount);
