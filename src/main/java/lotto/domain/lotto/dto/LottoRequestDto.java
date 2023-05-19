e62c4a2539dac5babfbf9257d3ec5c34cccd2540
highjune <highjune37@gmail.com>
2023-05-19 14:28:20 +0900

    refactor(dto, service 패키지 밑): 패키지, 객체 추가
    - dto, service 패키지 추가
    - LottoRequestDto, LottoService 클래스 추가

---
+++ src/main/java/lotto/domain/lotto/dto/LottoRequestDto.java
@@ -0,0 +1,6 @@
+package lotto.domain.lotto.dto;
+
+public class LottoRequestDto {
+
+
+}
--- src/main/java/lotto/domain/lotto/service/LottoService.java
+++ src/main/java/lotto/domain/lotto/service/LottoService.java
@@ -1,6 +1,8 @@
 package lotto.domain.lotto.service;
 
-import lotto.domain.lotto.Money;
+import lotto.domain.lotto.*;
+
+import java.util.List;
 
 public class LottoService {
 
