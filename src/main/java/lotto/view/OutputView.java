382e5636df1b3746bf152f133cb114a821368c8f
Lee Byoung Seong <dev.bslee@gmail.com>
2023-05-20 18:37:36 +0900

    feat : 로또 구매 장수 출력 기능(수동/자동)

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.Money;
 import lotto.domain.Prize;
 import lotto.domain.Statistics;
+import lotto.model.LottoInformation;
 
 import java.util.List;
 
@@ -10,8 +12,10 @@ public class OutputView {
 
     private static final double PROFIT_PRINCIPAL_CONDITION = 1.0;
 
-    public static void printLottoQuantity(Lotto lotto) {
-        System.out.println(lotto.getLottoNumbersSize() + "개를 구매했습니다.");
+    public static void printLottoQuantity(Money manualPurchaseAmount, Money autoPurchaseAmount) {
+
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.%n",
+                calculateLottoAmountBy(manualPurchaseAmount), calculateLottoAmountBy(autoPurchaseAmount));
     }
 
     public static void printLottoList(Lotto lotto) {
@@ -66,5 +70,9 @@ public class OutputView {
         if (profit < PROFIT_PRINCIPAL_CONDITION) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임 :( )");
         }
+    }
+
+    private static long calculateLottoAmountBy(Money money) {
+        return Math.floorDiv(money.getAmount(), LottoInformation.LOTTO_UNIT_PRICE);
     }
 }
