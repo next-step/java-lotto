0f89f1e2c6b8b5fea6dd13fb111c5cc9e8a02b9b
ian.ja <ian.ja@kakaocorp.com>
2023-05-20 22:55:16 +0900

    refactor: 불필요한 PrizeType의 필드 제거

--- src/main/java/lotto/domain/PrizeType.java
+++ src/main/java/lotto/domain/PrizeType.java
@@ -4,25 +4,23 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public enum PrizeType {
-    FIRST_PRIZE(6, false, 2_000_000_000),
+    FIRST_PRIZE(6,  2_000_000_000),
 
-    SECOND_PRIZE(5, true, 30_000_000),
+    SECOND_PRIZE(5,  30_000_000),
 
-    THIRD_PRIZE(5, false, 1_500_000),
+    THIRD_PRIZE(5,  1_500_000),
 
-    FOURTH_PRIZE(4, false, 50_000),
+    FOURTH_PRIZE(4,  50_000),
 
-    FIFTH_PRIZE(3, false, 5_000),
+    FIFTH_PRIZE(3,  5_000),
 
-    NOT_MATCHING(0, false, 0);
+    NOT_MATCHING(0,  0);
 
     private final int numberOfMatching;
-    private final boolean bonusMatching;
     private final long prize;
 
-    PrizeType(int numberOfMatching, boolean bonusMatching, long prize) {
+    PrizeType(int numberOfMatching,  long prize) {
         this.numberOfMatching = numberOfMatching;
-        this.bonusMatching = bonusMatching;
         this.prize = prize;
     }
 
