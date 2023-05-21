248b349b2eee7c8dd1c1235e969028d4a8d3f36e
dhkimneer <kimds989@naver.com>
2023-05-21 20:17:04 +0900

    chore: Update InputView.java for step4

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,14 +3,15 @@ package lotto.view;
 import lotto.domain.Lotto;
 import lotto.domain.LottoNumber;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InputView {
 
     private static final String REQUEST_PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
+    private static final String REQUEST_MANUALLY_PURCHASE_COUNT_INPUT = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String REQUEST_MANUALLY_PURCHASE_NUMBERS_INPUT = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String REQUEST_WINNING_NUMBERS_INPUT = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String REQUEST_BONUS_NUMBER_INPUT = "보너스 볼을 입력해 주세요.";
     private static final String SPLITTER = ", ";
@@ -21,13 +22,31 @@ public class InputView {
         return getIntNumber();
     }
 
+    public int requestManuallyPurchaseCount() {
+        System.out.println();
+        System.out.println(REQUEST_MANUALLY_PURCHASE_COUNT_INPUT);
+        return getIntNumber();
+    }
+
+    public List<Lotto> requestManuallyPurchaseNumbers(int requestManuallyPurchaseCount) {
+        System.out.println();
+        System.out.println(REQUEST_MANUALLY_PURCHASE_NUMBERS_INPUT);
+        return IntStream.range(0, requestManuallyPurchaseCount)
+                        .mapToObj(i -> createLotto())
+                        .collect(Collectors.toList());
+    }
+
     public Lotto requestWinningNumbers() {
         System.out.println(REQUEST_WINNING_NUMBERS_INPUT);
-        Set<LottoNumber> requestedWinningNumbers = Arrays.stream(SCANNER.nextLine().split(SPLITTER))
+        return createLotto();
+    }
+
+    private Lotto createLotto() {
+        Set<LottoNumber> requestedNumbers = Arrays.stream(SCANNER.nextLine().split(SPLITTER))
                 .map(Integer::parseInt)
                 .map(LottoNumber::new)
                 .collect(Collectors.toSet());
-        return new Lotto(requestedWinningNumbers);
+        return new Lotto(requestedNumbers);
     }
 
     public LottoNumber requestBonusNumber() {
