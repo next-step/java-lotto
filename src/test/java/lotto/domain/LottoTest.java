056b89dfdd573800ac3f2b5c111d8ae805084fe4
dhkimneer <kimds989@naver.com>
2023-05-21 20:18:56 +0900

    test: Add LottoPurchaseCountTest.java and move Lotto's calculation logic test to it

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -7,24 +7,11 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.*;
-
 public class LottoTest {
 
     @Test
-    @DisplayName("[요구사항 1] 로또 구입 개수 산출 테스트로, 로또를 '정수'개 사야 한다.")
+    @DisplayName("[요구사항 1] 로또 번호 개수가 6개가 아닐 경우 IllegalArgumentException throw")
     void 요구사항_1() {
-        // given: purchaseAmount(구입 금액) 및 기대 결과 제시
-        int purchaseAmount = 13700;
-        int expectedResult = 13;
-
-        // then
-        assertThat(Lotto.findPurchaseCount(purchaseAmount)).isEqualTo(expectedResult);
-    }
-
-    @Test
-    @DisplayName("[요구사항 2] 로또 번호 개수가 6개가 아닐 경우 IllegalArgumentException throw")
-    void 요구사항_2() {
         // given
         Set<LottoNumber> lottoNumbers = Set.of(1, 2, 3, 4, 5).stream().map(LottoNumber::new).collect(Collectors.toSet());
 
