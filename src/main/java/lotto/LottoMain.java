ee5da9664afa5bc89a39007056c084e63e8bba8a
dhkimneer <kimds989@naver.com>
2023-05-21 20:17:41 +0900

    chore: Update LottoMain.java for step4

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -12,11 +12,15 @@ public class LottoMain {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
-        int purchaseCount = Lotto.findPurchaseCount(inputView.requestPurchaseAmount()); // 구입 금액으로부터 구입 개수 산출
-        resultView.showPurchaseCount(purchaseCount);
+        int requestPurchaseAmount = inputView.requestPurchaseAmount();
+        int requestManuallyPurchaseCount = inputView.requestManuallyPurchaseCount();
+        LottoPurchaseCount lottoPurchaseCount = new LottoPurchaseCount(requestPurchaseAmount, requestManuallyPurchaseCount);
 
-        RandomLottoCreationStrategy randomLottoCreationStrategy = new RandomLottoCreationStrategy(); // 전략 설정
-        Lottos lottos = new Lottos(purchaseCount, randomLottoCreationStrategy);
+        ManuallyCreatedLottos manuallyCreatedLottos = new ManuallyCreatedLottos(inputView.requestManuallyPurchaseNumbers(requestManuallyPurchaseCount));
+        resultView.showPurchaseCount(lottoPurchaseCount.getRequestManuallyPurchaseCount(), lottoPurchaseCount.getAutomaticallyPurchaseCount());
+
+        RandomLottoCreationStrategy randomLottoCreationStrategy = new RandomLottoCreationStrategy(); // 자동 전략 설정
+        Lottos lottos = new Lottos(lottoPurchaseCount.getAutomaticallyPurchaseCount(), randomLottoCreationStrategy, manuallyCreatedLottos);
         List<Lotto> createdLottos = lottos.getCreatedLottos(); // 로또 리스트 만들기
 
         resultView.showLottos(createdLottos); // 만들어진 로또 산출
