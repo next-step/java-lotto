35b634634d97e0436253d9bf744c462dd016c485
신형기 <tkaqkeldk@hanatour.com>
2023-05-22 23:33:09 +0900

    refactor: 불필요 code 제거

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -64,28 +64,6 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-//    public List<Numbers> getManualLottoNumbers(final int manualLottoCount) {
-//        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
-//
-//        return IntStream.range(0, manualLottoCount)
-//                .mapToObj(i -> {
-//                    try {
-//                        return reader.readLine().trim();
-//                    } catch (IOException e) {
-//                        throw new UncheckedIOException(e);
-//                    }
-//                })
-//                .map(this::parseNumbers)
-//                .map(Numbers::new)
-//                .collect(Collectors.toList());
-//    }
-
-//    private List<Integer> parseNumbers(String input) {
-//        return Arrays.stream(input.split(DELIMITER))
-//                .map(Integer::parseInt)
-//                .collect(Collectors.toList());
-//    }
-
     public List<List<Integer>> getManualLottoNumbers(int count) {
         List<List<Integer>> manualLottos = new ArrayList<>();
 
