1279a74f0a321da400fa294eb99daf764a0c1c33
0giri <higiri@naver.com>
2023-05-24 01:00:03 +0900

    refactor: indent 초과된 것 수정

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -8,27 +8,25 @@ public class LottoInputView {
 
     public static int askBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-
+        
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            int amount = scanner.nextInt();
-            if (Lotto.PRICE.isMultiple(amount)) {
-                return amount;
-            }
+        int amount = scanner.nextInt();
+        while (!Lotto.PRICE.isMultiple(amount)) {
             System.out.printf("%d원 단위로 입력해주세요.%n", Lotto.PRICE.amount());
+            amount = scanner.nextInt();
         }
+        return amount;
     }
 
     public static String askLastWinnerNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String lottoNumbers = scanner.nextLine();
-            if (!lottoNumbers.isBlank()) {
-                return lottoNumbers;
-            }
+        String lottoNumbers = scanner.nextLine();
+        while (lottoNumbers.isBlank()) {
             System.out.println("빈 값은 허용하지 않습니다.");
+            lottoNumbers = scanner.nextLine();
         }
+        return lottoNumbers;
     }
 }
