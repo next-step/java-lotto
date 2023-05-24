e4ed06e6a6a15812fbc8de5989cc8bd26a3b3cf4
SeungYoung Oh <seung-00@naver.com>
2023-05-25 03:21:04 +0900

    feat: Application 추가

---
+++ src/main/java/step2/lotto/Application.java
@@ -0,0 +1,51 @@
+package step2.lotto;
+
+import java.util.List;
+
+import step2.lotto.domain.LottoTicket;
+import step2.lotto.domain.LottoTickets;
+import step2.lotto.domain.LottoWinningStatistic;
+import step2.lotto.domain.LottoWinningStatisticDto;
+
+public class Application {
+
+	public static void main(String[] args) {
+		start();
+	}
+
+	public static void start() {
+		int lottoCount = ready();
+
+		LottoWinningStatisticDto lottoWinningStatisticDto = run(lottoCount);
+
+		terminate(lottoWinningStatisticDto);
+	}
+
+	private static LottoWinningStatisticDto run(int lottoCount) {
+
+		LottoTickets lottoTickets = LottoTickets.create();
+
+		for (int i = 0; i < lottoCount; i++) {
+			lottoTickets.add();
+		}
+
+		LottoUI.printLottoTickets(lottoTickets);
+
+		List<Integer> numbers = LottoUI.uiForWinningNumbers();
+
+		LottoWinningStatistic lottoWinningStatistic = LottoWinningStatistic.from(lottoTickets);
+		LottoWinningStatisticDto lottoWinningStatisticDto = lottoWinningStatistic.calculateStatistic(LottoTicket.from(numbers));
+
+		return lottoWinningStatisticDto;
+	}
+
+	private static int ready() {
+		int lottoCount = LottoUI.uiForLottoPurchase();
+
+		return lottoCount;
+	}
+
+	private static void terminate(LottoWinningStatisticDto lottoWinningStatisticDto) {
+		LottoUI.printLottoWinningStatistic(lottoWinningStatisticDto);
+	}
+}
