f25ac2c401e977fa2ecb92901c576e8fbb2316c4
yoonjaej3 <yoonjaej3@gmail.com>
2023-05-28 14:25:55 +0900

    refact : SCANNER 상수관리 및 파라미터 수정

---
+++ src/main/java/view/input/InputView.java
@@ -0,0 +1,63 @@
+package view.input;
+
+import model.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+
+    }
+
+    public static int inputBuyAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static int inputPassiveCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static List<Lotto> inputPassiveLotto(int count) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        List<Lotto> result = new ArrayList<>();
+
+        while (count-- > 0) {
+            String[] nums = SCANNER.nextLine().split(",");
+            Lotto lotto = new Lotto(Stream.of(nums)
+                    .map(number -> Integer.parseInt(number.trim()))
+                    .collect(Collectors.toList()));
+
+            result.add(lotto);
+        }
+
+        return result;
+
+    }
+
+    public static List<Integer> inputWinnerLottoNum() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+
+        String[] nums = SCANNER.nextLine().split(",");
+        return Stream.of(nums)
+                .map(number -> Integer.parseInt(number.trim()))
+                .collect(Collectors.toList());
+    }
+
+    public static int inputBonusBall() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+
+        return Integer.parseInt(SCANNER.next());
+    }
+
+}
