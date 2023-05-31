2a44b799b78bc2ae700c6cedcb7e3000835429d5
dhkimneer <kimds989@naver.com>
2023-05-31 22:43:25 +0900

    tests: Update two tests because of normal situation

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberTest {
@@ -17,5 +18,18 @@ public class LottoNumberTest {
         assertThatThrownBy(() -> new LottoNumber(givenNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1과 45 사이의 숫자여야만 합니다.");
+    }
+
+    @Test
+    @DisplayName("[요구사항 2] 로또 번호가 1 ~ 45 사이의 숫자일 경우, 정상적으로 로또 번호가 생성된다.")
+    void 요구사항_2() {
+        // given
+        int givenNumber = 45;
+
+        // when
+        LottoNumber lottoNumber = new LottoNumber(givenNumber);
+
+        // then
+        assertThat(lottoNumber.getNumber()).isEqualTo(givenNumber);
     }
 }
