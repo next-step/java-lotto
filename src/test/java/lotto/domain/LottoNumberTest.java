0a7476326bb053336a24f33ece9a3064f13257cb
0giri <higiri@naver.com>
2023-06-01 03:15:40 +0900

    test: isSameAs로 메모리 주소 비교하도록 변경

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -30,9 +30,7 @@ public class LottoNumberTest {
     @Test
     void 로또_번호_숫자가_같으면_같은_인스턴스_재사용() {
         LottoNumber lottoNumber1 = LottoNumber.valueOf(1);
-        int actual = System.identityHashCode(lottoNumber1);
         LottoNumber lottoNumber2 = LottoNumber.valueOf(1);
-        int expected = System.identityHashCode(lottoNumber2);
-        assertThat(actual).isEqualTo(expected);
+        assertThat(lottoNumber1).isSameAs(lottoNumber2);
     }
 }
