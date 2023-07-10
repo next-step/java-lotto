589bd7141010974e2dbcc4c79e1995480bd3a1de
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 17:30:57 +0900

    refactor: 당첨 번호와 보너스 번호 중복 검증 로직 메서드 분리

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -8,10 +8,14 @@ public class WinningNumbers {
     private final LottoNumber bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        validateDuplicatedBonusNumber(winningNumbers, bonusNumber);
+        this.winningNumbers = new Lotto(winningNumbers);
+        this.bonusNumber = new LottoNumber(bonusNumber);
+    }
+
+    private void validateDuplicatedBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호와 당첨 번호가 중복 됩니다.");
         }
-        this.winningNumbers = new Lotto(winningNumbers);
-        this.bonusNumber = new LottoNumber(bonusNumber);
     }
 }
