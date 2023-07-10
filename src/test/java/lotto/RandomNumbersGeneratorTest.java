0ac235980ffe55fa443a094c237fd02ff7a53571
dahyen0o <seeunchoi99@gmail.com>
2023-07-10 20:07:42 +0900

    test: '숫자들의 범위가 1이상 45이하 성공' 테스트
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/test/java/lotto/RandomNumbersGeneratorTest.java
+++ src/test/java/lotto/RandomNumbersGeneratorTest.java
@@ -2,6 +2,9 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RandomNumbersGeneratorTest {
@@ -10,5 +13,25 @@ class RandomNumbersGeneratorTest {
     void 싱글톤객체인_RandomNumberGenerator객체_반환_성공() {
         // given, when, then
         assertDoesNotThrow(RandomNumbersGenerator::getInstance);
+    }
+
+    // 1. 랜덤(1~45)
+    // 2, 숫자 6개
+    // 3. 중복 안 됨
+    // 4. 정렬
+    @Test
+    void 숫자들의_범위가_1이상_45이하_성공() {
+        // given
+        RandomNumbersGenerator randomNumbersGenerator = RandomNumbersGenerator.getInstance();
+
+        // when
+        List<Integer> numbers = randomNumbersGenerator.generate();
+
+        // then
+        assertThat(numbers).allSatisfy(number ->
+                assertThat(number)
+                        .isGreaterThanOrEqualTo(1)
+                        .isLessThanOrEqualTo(45)
+        );
     }
 }
