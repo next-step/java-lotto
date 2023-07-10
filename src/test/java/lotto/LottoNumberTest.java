806a3e05ef1e217f492c946e6543e08328a8cf76
dahyen0o <seeunchoi99@gmail.com>
2023-07-10 17:47:58 +0900

    test: '넘버의 값을 1미만으로 생성하려고 할시 예외발생' 테스트
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class LottoNumberTest {
@@ -11,6 +12,12 @@ class LottoNumberTest {
     void 넘버_객체_생성_성공() {
         // given, when, then
         assertDoesNotThrow(() -> LottoNumber.of(1));
+    }
+
+    @Test
+    void 넘버의_값을_1미만으로_생성하려고_할시_예외발생() {
+        // given, when, then
+        assertThrows(IllegalArgumentException.class, () -> LottoNumber.of(0));
     }
 
 
