0c89f9fdf6a8356f82708fa85f7f469638311380
hwihwi <yjh200523@naver.com>
2023-07-10 17:32:48 +0900

    :hammer: refactor: 매직 넘버 제거 및 예외 메시지 추가

--- src/main/java/lotto/domain/Ball.java
+++ src/main/java/lotto/domain/Ball.java
@@ -2,19 +2,22 @@ package lotto.domain;
 
 public class Ball {
 
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+    public static final String NUMBER_RANGE_EXCEPTION_MESSAGE =
+            "로또 숫자는 " + MIN_NUMBER + "에서 " + MAX_NUMBER + "의 범위를 가질 수 있습니다.";
     private final int value;
 
     public Ball(final String value) {
         validate(value);
-
         this.value = Integer.parseInt(value);
     }
 
     private void validate(final String value) {
         int number = Integer.parseInt(value);
 
-        if (number < 1 || 45 < number) {
-            throw new IllegalArgumentException();
+        if (number < MIN_NUMBER || MAX_NUMBER < number) {
+            throw new IllegalArgumentException(NUMBER_RANGE_EXCEPTION_MESSAGE);
         }
     }
 }
