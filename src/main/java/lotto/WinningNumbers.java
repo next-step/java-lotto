770c95fb6feaf111839a3ac9a6c2ba3ad5a031b3
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 17:24:50 +0900

    feat: 당첨 번호 생성 로직 구현

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 public class WinningNumbers {
 
+    private final Lotto winningNumbers;
+    private final LottoNumber bonusNumber;
+
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
-        throw new IllegalArgumentException();
+        this.winningNumbers = new Lotto(winningNumbers);
+        this.bonusNumber = new LottoNumber(bonusNumber);
     }
 }
