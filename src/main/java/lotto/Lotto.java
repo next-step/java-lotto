fc553dcc25cad7fed1a3cefccbc68c6893626e84
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 17:00:52 +0900

    feat: 로또 번호 개수가 6개가 아닌 경우 예외 처리 로직 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,6 +8,9 @@ public class Lotto {
     private List<LottoNumber> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+        }
         this.lottoNumbers = lottoNumbers.stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toList());
