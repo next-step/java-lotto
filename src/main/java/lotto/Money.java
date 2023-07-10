d8cf623bfa4150ef21edbbfcc6c52db3eabc8f74
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 18:03:53 +0900

    feat: 음수 값 입력시 머니 생성 예외 처리 로직 구현

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -5,6 +5,13 @@ public class Money {
     private final int money;
 
     public Money(int money) {
+        validateMoney(money);
         this.money = money;
+    }
+
+    private void validateMoney(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("돈이 음수일 수 없습니다.");
+        }
     }
 }
