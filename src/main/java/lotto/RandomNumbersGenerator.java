8a8edb0a7f04f23f6b52dea57cf3bd444a6cd172
dahyen0o <seeunchoi99@gmail.com>
2023-07-10 20:12:11 +0900

    feat: '숫자들의 범위가 1이상 45이하 성공' 기능 구현
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/main/java/lotto/RandomNumbersGenerator.java
+++ src/main/java/lotto/RandomNumbersGenerator.java
@@ -1,10 +1,18 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class RandomNumbersGenerator implements NumbersGenerator {
 
     private static final RandomNumbersGenerator INSTANCE = new RandomNumbersGenerator();
+
+    private static final List<Integer> baseNumbers = IntStream.rangeClosed(1, 45)
+            .boxed()
+            .collect(Collectors.toUnmodifiableList());
 
     private RandomNumbersGenerator() {
     }
@@ -15,6 +23,6 @@ public final class RandomNumbersGenerator implements NumbersGenerator {
 
     @Override
     public List<Integer> generate() {
-        return null;
+        return baseNumbers;
     }
 }
