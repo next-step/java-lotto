ba8d35d4518370ba31278d2dab99049d66e72338
hwihwi <yjh200523@naver.com>
2023-07-10 17:29:21 +0900

    :sparkles: feat: 로또 숫자 범위 초과 입력 검사 추가

--- src/main/java/lotto/domain/Ball.java
+++ src/main/java/lotto/domain/Ball.java
@@ -5,6 +5,16 @@ public class Ball {
     private final int value;
 
     public Ball(final String value) {
+        validate(value);
+
         this.value = Integer.parseInt(value);
+    }
+
+    private void validate(final String value) {
+        int number = Integer.parseInt(value);
+
+        if (number < 1 || 45 < number) {
+            throw new IllegalArgumentException();
+        }
     }
 }
