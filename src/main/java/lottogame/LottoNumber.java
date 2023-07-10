24aa53894aa9a587ae8515c1fdb8f51c8ca2ae97
jjungk <jsk33a@gmail.com>
2023-07-10 20:25:28 +0900

    refactor : LottoNumber 리팩토링

--- src/main/java/lottogame/LottoNumber.java
+++ src/main/java/lottogame/LottoNumber.java
@@ -1,33 +1,49 @@
 package lottogame;
 
+import java.text.MessageFormat;
+import java.util.Objects;
+
 public final class LottoNumber {
 
+    public static final int LOWER_BOUND = 1;
+    public static final int UPPER_BOUND = 45;
     private final int lottoNumber;
 
     public LottoNumber(final String value) {
-        validateNull(value);
         validateBlank(value);
         int number = Integer.parseInt(value.trim());
-        validate(number);
+        validateRange(number);
         this.lottoNumber = number;
-
     }
 
     private void validateBlank(String value) {
-        if (value.isBlank()) {
+        if (value == null || value.isBlank()) {
             throw new IllegalArgumentException("숫자를 입력해야 합니다");
         }
     }
 
-    private void validateNull(String value) {
-        if (value == null) {
-            throw new IllegalArgumentException("숫자를 입력해야 합니다");
+    private void validateRange(int number) {
+        if (number < LOWER_BOUND || number > UPPER_BOUND) {
+            throw new IllegalArgumentException(
+                MessageFormat.format("{0}이상 {1}이하의 숫자여야합니다", LOWER_BOUND, UPPER_BOUND)
+            );
         }
     }
 
-    private void validate(int number) {
-        if (number < 1 || number > 45) {
-            throw new IllegalArgumentException("1이상 45이하의 숫자여야합니다");
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
         }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumber);
     }
 }
