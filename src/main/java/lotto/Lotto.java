b4e2e6142a6c3638b51938b13c463f2bff380c2a
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 16:49:52 +0900

    feat: 로또 객체 정상 입력시 로또 객체 정상 생성 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,15 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
+    private List<LottoNumber> lottoNumbers;
+
     public Lotto(List<Integer> lottoNumbers) {
-        throw new IllegalArgumentException();
+        this.lottoNumbers = lottoNumbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 }
