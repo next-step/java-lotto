9fd3c7750239655912f48e7209b107d17272204b
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 17:11:15 +0900

    test: 참조하지 않는 패키지 제거

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,6 @@ package lotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
