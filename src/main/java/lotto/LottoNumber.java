f48a198582270d616bf4db7363dc3ac558f845c2
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 16:41:23 +0900

    refactor: 로또 번호 검증 로직 메서드 분리 및 매직 넘버 상수화

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -2,12 +2,18 @@ package lotto;
 
 public class LottoNumber {
 
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
-        if (lottoNumber < 1 || lottoNumber > 45) {
+        validateNumber(lottoNumber);
+        this.lottoNumber = lottoNumber;
+    }
+
+    private void validateNumber(int lottoNumber) {
+        if (lottoNumber < MIN_NUMBER || lottoNumber > MAX_NUMBER) {
             throw new IllegalArgumentException("로또 번호는 1부터 45 이내의 숫자로 구성되어야 합니다.");
         }
-        this.lottoNumber = lottoNumber;
     }
 }
