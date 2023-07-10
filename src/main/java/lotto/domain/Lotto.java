f3b5ba155b4951253bddc731c6084a29dd3ff0c5
hwihwi <yjh200523@naver.com>
2023-07-10 18:06:36 +0900

    :sparkles: feat: 로또 생성자 작성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+
+    private List<Ball> balls;
+
+    public Lotto(List<Ball> balls) {
+        this.balls = balls;
+    }
+}
