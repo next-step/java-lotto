13891390bc0edb95b5ba75c2005f97a1a844d281
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 16:36:10 +0900

    feat: 로또 번호 생성 예외 검증 로직 구현

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -5,6 +5,9 @@ public class LottoNumber {
     private final int lottoNumber;
 
     public LottoNumber(int lottoNumber) {
+        if (lottoNumber < 1 || lottoNumber > 45) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 이내의 숫자로 구성되어야 합니다.");
+        }
         this.lottoNumber = lottoNumber;
     }
 }
