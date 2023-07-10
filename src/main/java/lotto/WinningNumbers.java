1afa90fa393a19db95cb4de2fbcc05f3381a8000
jiwon choi <jjiwoning12@gmail.com>
2023-07-10 17:28:36 +0900

    feat: 당첨 번호와 보너스 번호 중복 검증 로직 작성

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -8,6 +8,9 @@ public class WinningNumbers {
     private final LottoNumber bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호와 당첨 번호가 중복 됩니다.");
+        }
         this.winningNumbers = new Lotto(winningNumbers);
         this.bonusNumber = new LottoNumber(bonusNumber);
     }
