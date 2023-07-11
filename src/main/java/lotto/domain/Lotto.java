4e23f99391c99cc921d1c4ff51ffd2d9314da4bb
rimrim990 <lim990718@naver.com>
2023-07-11 15:32:17 +0900

    fix: 모든 랜덤이 동일하게 생성되는 버그 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +25,7 @@ public class Lotto {
 
     public static Lotto createRandomLotto() {
         Collections.shuffle(values);
-        return new Lotto(values.subList(0, LOTTO_SIZE));
+        return new Lotto(new ArrayList<>(values.subList(0, LOTTO_SIZE)));
     }
 
     public static Lotto createSpecificLotto(final List<Integer> numbers) {
