88e4168cd70ac2c21c2c7caa2f864862b8da5666
impati <yongs170@naver.com>
2023-07-11 11:37:56 +0900

    Feat : 로또머니 생성 Co-authored-by: worldii <bbc2788@naver.com>

---
+++ src/main/java/lotto/model/LottoMoney.java
@@ -0,0 +1,22 @@
+package lotto.model;
+
+public class LottoMoney {
+    public static final int LOTTO_UNIT = 1000;
+    public static final int ZERO = 0;
+    private final int money;
+
+    public LottoMoney(final int money) {
+        validateMoney(money);
+        this.money = money;
+    }
+
+    private void validateMoney(int money) {
+        if (money < ZERO) {
+            throw new IllegalStateException("로또 머니가 양수여야 합니다.");
+        }
+    }
+
+    public int getLottoCount() {
+        return money / LOTTO_UNIT;
+    }
+}
