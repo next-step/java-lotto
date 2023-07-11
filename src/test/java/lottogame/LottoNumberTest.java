2ca5e4bd6049d413a76230ccea5b167a477d5825
jjungk <jsk33a@gmail.com>
2023-07-11 14:16:19 +0900

    test : LottoNumber 일치 불일치 테스트 코드 추가

--- src/test/java/lottogame/LottoNumberTest.java
+++ src/test/java/lottogame/LottoNumberTest.java
@@ -1,9 +1,11 @@
 package lottogame;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -24,6 +26,24 @@ public class LottoNumberTest {
     void 로또_번호_검증_성공(int value) {
         assertThatNoException()
             .isThrownBy(() -> new LottoNumber(value));
+    }
+
+    @DisplayName("자신과 같은 숫자라면 true를 반환한다")
+    @Test
+    void 숫자_비교_일치() {
+        LottoNumber lottoNumber = new LottoNumber(1);
+        LottoNumber another = new LottoNumber(1);
+
+        assertThat(lottoNumber.isSame(another)).isTrue();
+    }
+
+    @DisplayName("자신과 다른 숫자라면 false를 반환한다")
+    @Test
+    void 숫자_비교_불일치() {
+        LottoNumber lottoNumber = new LottoNumber(1);
+        LottoNumber another = new LottoNumber(2);
+
+        assertThat(lottoNumber.isSame(another)).isFalse();
     }
 
 }
