df0a865f6012d35e9ab49be9408d8d441a952e1e
devxb <dlwnsdud205@naver.com>
2023-07-11 18:35:24 +0900

    feat: ConsolePurchase(Inputer,Viwer), ConsoleResultCheckViewer 구현

--- src/main/java/lottogame/console/purchase/ConsolePurchaseInputer.java
+++ src/main/java/lottogame/console/purchase/ConsolePurchaseInputer.java
@@ -1,11 +1,14 @@
 package lottogame.console.purchase;
 
+import java.util.Scanner;
 import lottogame.controller.purchase.spi.PurchaseInputer;
 
 public class ConsolePurchaseInputer implements PurchaseInputer {
 
     @Override
     public int inputMoney() {
-        return 1000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
     }
 }
--- src/main/java/lottogame/console/resultcheck/ConsoleResultCheckInputer.java
+++ src/main/java/lottogame/console/resultcheck/ConsoleResultCheckInputer.java
@@ -1,17 +1,27 @@
 package lottogame.console.resultcheck;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lottogame.controller.resultcheck.spi.ResultCheckInputer;
 
 public class ConsoleResultCheckInputer implements ResultCheckInputer {
 
+    private final Scanner scanner = new Scanner(System.in);
+
     @Override
     public Set<Integer> inputWinningLottoNumbers() {
-        return null;
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+
+        return Arrays.stream(scanner.next().split(", "))
+            .map(Integer::valueOf)
+            .collect(Collectors.toSet());
     }
 
     @Override
     public Integer inputBonusLottoNumber() {
-        return null;
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextInt();
     }
 }
