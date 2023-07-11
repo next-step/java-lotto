1c8801adad1e676340bab003e89948346ead0e48
hwihwi <yjh200523@naver.com>
2023-07-11 19:29:06 +0900

    :hammer: refactor: ranks 관련 로직 메서드 추출, 매직 넘버 제거

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -6,18 +6,27 @@ import java.util.Map;
 
 public class WinningStatistics {
 
+    private static final int DEFAULT_COUNT = 0;
+
     private final Map<WinningCriteria, Integer> ranks;
 
     public WinningStatistics(final List<LottoResult> lottoResults) {
-        this.ranks = new HashMap<>();
+        this.ranks = initRanks(lottoResults);
+    }
+
+    private Map<WinningCriteria, Integer> initRanks(final List<LottoResult> lottoResults) {
+        Map<WinningCriteria, Integer> ranks = new HashMap<>();
+
         for (WinningCriteria value : WinningCriteria.values()) {
-            ranks.put(value, 0);
+            ranks.put(value, DEFAULT_COUNT);
         }
 
         for (LottoResult lottoResult : lottoResults) {
             WinningCriteria winningCriteria = lottoResult.getWinningCriteria();
-            ranks.put(winningCriteria, ranks.getOrDefault(winningCriteria, 0) + 1);
+            ranks.put(winningCriteria, ranks.getOrDefault(winningCriteria, DEFAULT_COUNT) + 1);
         }
+
+        return ranks;
     }
 
     public int getRank(WinningCriteria criteria) {
