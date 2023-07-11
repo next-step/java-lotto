3b4a302fea6e43b4d602bcfaceafe0738b816411
hwihwi <yjh200523@naver.com>
2023-07-11 12:08:56 +0900

    :sparkles: feat: 구입 금액 범위 검증 작성

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -5,6 +5,15 @@ public class Money {
     private final int value;
 
     public Money(final String value) {
+        validate(value);
         this.value = Integer.parseInt(value);
+    }
+
+    private void validate(final String value) {
+        int money = Integer.parseInt(value);
+
+        if (money < 1_000 || 100_000 < money) {
+            throw new IllegalArgumentException();
+        }
     }
 }
