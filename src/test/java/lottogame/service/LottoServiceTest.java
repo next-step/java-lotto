c36b74698d5db09e5740d4c31a760d1068797ba7
devxb <dlwnsdud205@naver.com>
2023-07-11 16:32:44 +0900

    refactor: LottoService 메소드 추출

--- src/test/java/lottogame/service/LottoServiceTest.java
+++ src/test/java/lottogame/service/LottoServiceTest.java
@@ -13,12 +13,14 @@ import org.junit.jupiter.api.Test;
 class LottoServiceTest {
 
     @Nested
-    @DisplayName("buy 메소드는")
+    @DisplayName("purchase 메소드는")
     class Describe_Buy {
 
         @Nested
         @DisplayName("1000원으로 나누어 떨어지지 않으면,")
         class Context_Not_Divided_By_1000 {
+
+            private static final int NOT_DIVIDED_MONEY = 1001;
 
             private final NumberGenerator stubNumberGenerator = (count) -> Set.of(1, 2, 3, 4, 5, 6);
             private final LottoService lottoService = new LottoService(stubNumberGenerator);
@@ -26,11 +28,9 @@ class LottoServiceTest {
             @Test
             @DisplayName("IllegalArgumentException 을 던진다")
             void It_Throw_IllegalArgumentException() {
-                assertThatThrownBy(() -> lottoService.buy(1001)).isInstanceOf(IllegalArgumentException.class);
+                assertThatThrownBy(() -> lottoService.purchase(NOT_DIVIDED_MONEY)).isInstanceOf(
+                    IllegalArgumentException.class);
             }
-
         }
-
     }
-
 }
