a014a91e2bf5ef254b1967bd76f817b81afa7e6b
dahyen0o <seeunchoi99@gmail.com>
2023-07-11 12:05:44 +0900

    feat: '로또 가격 이하의 돈 객체 생성시 예외 발생' 기능 구현
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/main/java/lotto/LottoMoney.java
+++ src/main/java/lotto/LottoMoney.java
@@ -5,7 +5,14 @@ public class LottoMoney {
     private final int value;
 
     public LottoMoney(final int value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(final int value) {
+        if (value < 1_000) {
+            throw new IllegalArgumentException("로또를 구입하기에 돈이 부족합니다. 로또는 한 장에 1000원 입니다. 현재 구입 금액은 " + value + "원 입니다.");
+        }
     }
 
     public int getValue() {
