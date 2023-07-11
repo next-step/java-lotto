1ae94e2f572252fe2fecc8d1acda1a42733e2ad7
hwihwi <yjh200523@naver.com>
2023-07-11 18:49:41 +0900

    :hammer: refactor: static import 추가 및 사용하지 않는 import 제거

--- src/test/java/lotto/domain/LottoResultsTest.java
+++ src/test/java/lotto/domain/LottoResultsTest.java
@@ -1,7 +1,8 @@
 package lotto.domain;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +12,7 @@ public class LottoResultsTest {
     @DisplayName("로또 결과 생성 테스트")
     void lottoResults_create() {
         /* given & when & then */
-        Assertions.assertDoesNotThrow(() -> new LottoResults(List.of(
+        assertDoesNotThrow(() -> new LottoResults(List.of(
                 new LottoResult(1, false),
                 new LottoResult(6, true)))
         );
