89a9243642a2a9a1a160c5608c1dcfe1d2a34bba
dahyen0o <seeunchoi99@gmail.com>
2023-07-11 12:02:54 +0900

    test: '로또 가격 이하의 돈 객체 생성시 예외 발생' 테스트
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/test/java/lotto/LottoMoneyTest.java
+++ src/test/java/lotto/LottoMoneyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LottoMoneyTest {
 
@@ -18,6 +19,12 @@ class LottoMoneyTest {
     void 돈_객체_값_반환_성공() {
         // given, when, then
         assertThat(new LottoMoney(1000).getValue()).isEqualTo(1000);
+    }
+
+    @Test
+    void 로또_가격_이하의_돈_객체_생성시_예외_발생() {
+        // given, when, then
+        assertThrows(IllegalArgumentException.class, () -> new LottoMoney(999));
     }
 
 }
