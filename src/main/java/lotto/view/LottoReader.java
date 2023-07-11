65afb1fff29b3490b2f9f774199b4005f5f89aef
hwihwi <yjh200523@naver.com>
2023-07-11 20:56:22 +0900

    :hammer: refactor: 당첨 번호 입력 시 공백 제거

--- src/main/java/lotto/view/LottoReader.java
+++ src/main/java/lotto/view/LottoReader.java
@@ -11,12 +11,14 @@ public class LottoReader {
     private static final String DELIMITER = ",";
     private static final String INPUT_WINNING_LOTTO_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
     private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String SPACE = " ";
+    private static final String EMPTY_STRING = "";
 
     public Lotto readWinningLotto() {
         System.out.println(INPUT_WINNING_LOTTO_NUMBERS_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         List<Ball> winningLottoBalls = new ArrayList<>();
-        String winningLottoNumbers = scanner.nextLine();
+        String winningLottoNumbers = scanner.nextLine().replace(SPACE, EMPTY_STRING);
         for (String winningLottoNumber : winningLottoNumbers.split(DELIMITER)) {
             winningLottoBalls.add(new Ball(winningLottoNumber));
         }
