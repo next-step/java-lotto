293ff3e6f447d83f74f107d30dfc854402ba716f
hwihwi <yjh200523@naver.com>
2023-07-11 16:21:23 +0900

    :sparkles: feat: 구입한 로또 일급 컬렉션 생성

---
+++ src/main/java/lotto/domain/BoughtLottos.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class BoughtLottos {
+
+    private final int count;
+    private final List<Lotto> lottos;
+
+    public BoughtLottos(List<Lotto> lottos) {
+        this.count = lottos.size();
+        this.lottos = lottos;
+    }
+}
