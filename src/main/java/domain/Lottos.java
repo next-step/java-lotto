4844b21545df278e434bdb19729cb6feca4b9b5e
whatasame <whatasame@gmail.com>
2023-07-11 16:11:06 +0900

    feat: Lotto의 일급 컬렉션 구현
    Co-authored-by: gunner6603 <gunner6603@snu.ac.kr>

---
+++ src/main/java/domain/Lottos.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(final List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Rank> checkAllLottoResult(final WinningLotto winningLotto) {
+        return lottos.stream()
+                .map(winningLotto::checkLottoResult)
+                .collect(Collectors.toList());
+    }
+}
