5fe0a1a7d05efb18043fc29cf56c4b176ed90da4
dahyen0o <seeunchoi99@gmail.com>
2023-07-11 16:42:52 +0900

    test: '로또 당첨결과가 올바르게 저장 성공' 테스트
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/test/java/lotto/LottoResultTest.java
+++ src/test/java/lotto/LottoResultTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoResultTest {
@@ -13,5 +14,16 @@ class LottoResultTest {
     void 로또_당첨결과_객체_생성_성공() {
         // given, when, then
         assertDoesNotThrow(() -> new LottoResult(List.of()));
+    }
+
+    @Test
+    void 로또_당첨결과가_올바르게_저장_성공() {
+        // given
+        LottoResult lottoResult = new LottoResult(List.of(Rank.SECOND, Rank.FOURTH, Rank.SECOND));
+
+        // when & then
+        assertThat(lottoResult.getCount(Rank.SECOND)).isEqualTo(2);
+        assertThat(lottoResult.getCount(Rank.FOURTH)).isEqualTo(1);
+        assertThat(lottoResult.getCount(Rank.FIRST)).isZero();
     }
 }
