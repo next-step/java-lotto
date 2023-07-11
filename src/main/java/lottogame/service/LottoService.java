8682330aa125de71c4b11bfa489f279145278b79
devxb <dlwnsdud205@naver.com>
2023-07-11 16:32:44 +0900

    refactor: LottoService 메소드 추출

--- src/main/java/lottogame/service/LottoService.java
+++ src/main/java/lottogame/service/LottoService.java
@@ -10,23 +10,31 @@ import lottogame.service.reponse.LottoPurchaseResponse.LottoNumbersResponse;
 
 public class LottoService {
 
+    private static final int PURCHASABLE_UNIT = 1000;
     private final NumberGenerator numberGenerator;
 
     public LottoService(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
 
-    public LottoPurchaseResponse buy(int money) {
-        if (money % 1000 != 0) {
-            throw new IllegalArgumentException(String.format("money는 1000원으로 나누어 떨어져야 합니다 money: \"%d\"", money));
-        }
+    public LottoPurchaseResponse purchase(int money) {
+        assertMoney(money);
+        return toLottoPurchaseResponse(createLottoNumbers(money));
+    }
 
+    private List<LottoNumbers> createLottoNumbers(int money) {
         List<LottoNumbers> lottoNumbersList = new ArrayList<>();
-        int count = money % 1000;
-        for (int i = 0; i < count; i++) {
+        for (int count = 0; count < money % PURCHASABLE_UNIT; count++) {
             lottoNumbersList.add(new LottoNumbers(numberGenerator));
         }
-        return toLottoPurchaseResponse(lottoNumbersList);
+        return lottoNumbersList;
+    }
+
+    private void assertMoney(int money) {
+        if (money % PURCHASABLE_UNIT != 0) {
+            throw new IllegalArgumentException(
+                String.format("money는 \"%d\"원으로 나누어 떨어져야 합니다 money: \"%d\"", PURCHASABLE_UNIT, money));
+        }
     }
 
     private LottoPurchaseResponse toLottoPurchaseResponse(List<LottoNumbers> lottoNumbersList) {
@@ -36,5 +44,4 @@ public class LottoService {
                 .collect(Collectors.toList())
         );
     }
-
 }
