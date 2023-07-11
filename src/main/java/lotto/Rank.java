f88d367de07d92193359f99aff792476ae58bcf2
dahyen0o <seeunchoi99@gmail.com>
2023-07-11 15:11:40 +0900

    feat: '당첨번호 개수와 보너스볼 보유여부로 랭크계산 성공' 기능 구현
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public enum Rank {
+
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean hasBonusBall;
+    private final int prize;
+
+    Rank(final int matchCount, final boolean hasBonusBall, final int prize) {
+        this.matchCount = matchCount;
+        this.hasBonusBall = hasBonusBall;
+        this.prize = prize;
+    }
+
+    public static Rank valueOf(final int matchCount, final boolean hasBonusBall) {
+        // TODO refactor
+        if (matchCount == 5 && hasBonusBall) {
+            return SECOND;
+        }
+        if (matchCount == 5) {
+            return THIRD;
+        }
+
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(NONE);
+    }
+}
