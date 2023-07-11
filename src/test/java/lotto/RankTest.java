9bdf9788f47fc996d4907da7f3fac3b77b5fc020
dahyen0o <seeunchoi99@gmail.com>
2023-07-11 15:11:25 +0900

    test: '당첨번호 개수와 보너스볼 보유여부로 랭크계산 성공' 테스트
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

---
+++ src/test/java/lotto/RankTest.java
@@ -0,0 +1,34 @@
+package lotto;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RankTest {
+
+    @ParameterizedTest
+    @MethodSource("matchCountAndBounsBallProvider")
+    void 당첨번호_개수와_보너스볼_보유여부로_랭크계산_성공(int matchCount, boolean hasBonusBall, Rank result) {
+        // given, when, then
+        assertThat(Rank.valueOf(matchCount, hasBonusBall)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> matchCountAndBounsBallProvider() {
+        return Stream.of(
+                Arguments.of(6, false, Rank.FIRST),
+                Arguments.of(5, true, Rank.SECOND),
+                Arguments.of(5, false, Rank.THIRD),
+                Arguments.of(4, false, Rank.FOURTH),
+                Arguments.of(3, false, Rank.FIFTH),
+                Arguments.of(2, false, Rank.NONE),
+                Arguments.of(1, true, Rank.NONE),
+                Arguments.of(0, false, Rank.NONE)
+        );
+    }
+}
