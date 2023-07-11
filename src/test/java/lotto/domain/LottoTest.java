479507303feb1464e30559601705dac00a9e07da
hwihwi <yjh200523@naver.com>
2023-07-11 11:42:36 +0900

    :umbrella: test: 로또 번호 오름차순 정렬 테스트 작성
    - List.of()를 사용하지 않도록 수정

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,10 +1,11 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.ArrayList;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,46 +15,43 @@ public class LottoTest {
     @DisplayName("로또 정상 생성 테스트")
     void lotto_create() {
         /* given & when & then */
-        assertDoesNotThrow(() -> new Lotto(List.of(
-                new Ball("1"),
-                new Ball("2"),
-                new Ball("3"),
-                new Ball("4"),
-                new Ball("5"),
-                new Ball("6")))
-        );
+        assertDoesNotThrow(() -> new Lotto(getBalls("1", "2", "3", "4", "5", "6")));
     }
 
     @Test
     @DisplayName("로또 개수 검증 테스트")
     void lotto_invalidBallsSize() {
         /* given & when & then */
-        assertThatThrownBy(() -> new Lotto(List.of(
-                new Ball("1")
-        ))).isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(getBalls("1", "2", "3", "4", "5"))).isExactlyInstanceOf(
+                IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new Lotto(List.of(
-                new Ball("1"),
-                new Ball("2"),
-                new Ball("3"),
-                new Ball("4"),
-                new Ball("5"),
-                new Ball("6"),
-                new Ball("7")
-        ))).isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(getBalls("1", "2", "3", "4", "5", "6", "7"))).isExactlyInstanceOf(
+                IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("로또 번호 중복 테스트")
     void lotto_duplicateBalls() {
         /* given & when & then */
-        assertThatThrownBy(() -> new Lotto(List.of(
-                new Ball("1"),
-                new Ball("2"),
-                new Ball("3"),
-                new Ball("4"),
-                new Ball("4"),
-                new Ball("6")
-        ))).isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(getBalls("1", "2", "3", "4", "4", "6")))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호 오름차순 정렬 테스트")
+    void lotto_sortBallsByAscending() {
+        /* given */
+        final Lotto lotto = new Lotto(getBalls("6", "5", "4", "3", "2", "1"));
+
+        /* when & then */
+        assertThat(lotto).isEqualTo(new Lotto(getBalls("1", "2", "3", "4", "5", "6")));
+    }
+
+    private List<Ball> getBalls(final String... args) {
+        final List<Ball> balls = new ArrayList<>();
+        for (String arg : args) {
+            balls.add(new Ball(arg));
+        }
+        return balls;
     }
 }
