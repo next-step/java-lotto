07f370cd7eb41225ab0c75ffce9421b783541666
jiwon choi <jjiwoning12@gmail.com>
2023-07-11 11:24:56 +0900

    feat: RandomGenerator generateLotto 메서드 구현

--- src/main/java/lotto/RandomGenerator.java
+++ src/main/java/lotto/RandomGenerator.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class RandomGenerator {
+
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 46;
+    private static final int START_INDEX = 0;
+    private static final int LOTTO_SIZE = 6;
+    private static final List<Integer> lottoNumbers = IntStream.range(START_NUMBER, END_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
+
+    private RandomGenerator() {
+    }
+
     public static Lotto generateLotto() {
-        throw new IllegalArgumentException();
+        Collections.shuffle(lottoNumbers);
+        return new Lotto(lottoNumbers.subList(START_INDEX, LOTTO_SIZE));
     }
 }
