df1cdb2da7e3cfec6829aef6744f4fbe539ba9f1
jiwon choi <jjiwoning12@gmail.com>
2023-07-11 11:00:51 +0900

    refactor: Money 클래스 int 타입 long 타입으로 변경

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -2,14 +2,14 @@ package lotto;
 
 public class Money {
 
-    private final int money;
+    private final long money;
 
-    public Money(int money) {
+    public Money(long money) {
         validateMoney(money);
         this.money = money;
     }
 
-    private void validateMoney(int money) {
+    private void validateMoney(long money) {
         if (money < 0) {
             throw new IllegalArgumentException("돈이 음수일 수 없습니다.");
         }
