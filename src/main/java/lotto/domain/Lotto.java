f198e7df307205b3d7b637e157aa03d0265a63bd
hwihwi <yjh200523@naver.com>
2023-07-11 11:05:08 +0900

    :hammer: refactor: final 키워드 및 예외 메시지 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,7 +6,8 @@ public class Lotto {
 
     private static final int SIZE = 6;
     private static final String LOTTO_SIZE_EXCEPTION_MESSAGE = "로또 번호는 6개여야 합니다.";
-    private List<Ball> balls;
+    public static final String DUPLICATE_BALLS_EXCEPTION_MESSAGE = "로또 번호가 중복되었습니다.";
+    private final List<Ball> balls;
 
     public Lotto(final List<Ball> balls) {
         validate(balls);
@@ -14,8 +15,8 @@ public class Lotto {
     }
 
     private void validate(final List<Ball> balls) {
-        if (balls.stream().distinct().count() != balls.size()) {
-            throw new IllegalArgumentException();
+        if (balls.stream().distinct().count() != SIZE) {
+            throw new IllegalArgumentException(DUPLICATE_BALLS_EXCEPTION_MESSAGE);
         }
         if (balls.size() != SIZE) {
             throw new IllegalArgumentException(LOTTO_SIZE_EXCEPTION_MESSAGE);
