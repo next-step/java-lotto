5907dc0a9d478187648a0a91692931f0877a020b
hwihwi <yjh200523@naver.com>
2023-07-11 12:13:23 +0900

    :hammer: refactor: 매직 넘버 제거 및 예외 메세지 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,6 +2,10 @@ package lotto.domain;
 
 public class Money {
 
+    private static final int MIN_PURCHASE_MONEY = 1_000;
+    private static final int MAX_PURCHASE_MONEY = 100_000;
+    private static final String MONEY_RANGE_EXCEPTION_MESSAGE =
+            "로또는 " + MIN_PURCHASE_MONEY + "원 이상 " + MAX_PURCHASE_MONEY + "원 이하만 구매할 수 있습니다";
     private final int value;
 
     public Money(final String value) {
@@ -12,8 +16,8 @@ public class Money {
     private void validate(final String value) {
         int money = Integer.parseInt(value);
 
-        if (money < 1_000 || 100_000 < money) {
-            throw new IllegalArgumentException();
+        if (money < MIN_PURCHASE_MONEY || MAX_PURCHASE_MONEY < money) {
+            throw new IllegalArgumentException(MONEY_RANGE_EXCEPTION_MESSAGE);
         }
     }
 }
