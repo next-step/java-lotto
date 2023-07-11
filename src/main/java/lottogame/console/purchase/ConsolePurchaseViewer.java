ac9bd9e54819390d20a92a196c7ecf918cf02e3a
devxb <dlwnsdud205@naver.com>
2023-07-11 18:35:24 +0900

    feat: ConsolePurchase(Inputer,Viwer), ConsoleResultCheckViewer 구현

--- src/main/java/lottogame/console/purchase/ConsolePurchaseViewer.java
+++ src/main/java/lottogame/console/purchase/ConsolePurchaseViewer.java
@@ -1,6 +1,8 @@
 package lottogame.console.purchase;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lottogame.controller.purchase.spi.PurchaseViewer;
 import lottogame.domain.LottoTicket;
 
@@ -8,5 +10,24 @@ public class ConsolePurchaseViewer implements PurchaseViewer {
 
     @Override
     public void draw(List<LottoTicket> lottoTickets) {
+        System.out.printf("%d개를 구매했습니다.%n", lottoTickets.size());
+        System.out.println(parsePrintString(lottoTickets));
     }
+
+    private String parsePrintString(List<LottoTicket> lottoTickets) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        lottoTickets.forEach(lottoTicket -> parseLottoTicket(lottoTicket.getLottoNumbers(), stringBuilder));
+
+        return stringBuilder.toString();
+    }
+
+    private void parseLottoTicket(Set<Integer> lottoTickets, StringBuilder stringBuilder) {
+        stringBuilder.append("[");
+        stringBuilder.append(lottoTickets.stream()
+            .map(lottoTicket -> Integer.toString(lottoTicket))
+            .collect(Collectors.joining(", ")));
+        stringBuilder.append("]\n");
+    }
+
 }
