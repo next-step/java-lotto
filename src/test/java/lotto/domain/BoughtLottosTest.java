70c04d478665a849c539f44b4a11c8dbd303cb25
hwihwi <yjh200523@naver.com>
2023-07-11 16:26:21 +0900

    :umbrella: test: 구입한 로또의 개수 테스트 작성

--- src/test/java/lotto/domain/BoughtLottosTest.java
+++ src/test/java/lotto/domain/BoughtLottosTest.java
@@ -1,9 +1,11 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +19,20 @@ public class BoughtLottosTest {
 
         /* when & then */
         assertDoesNotThrow(() -> new BoughtLottos(lottos));
+    }
+
+    @Test
+    @DisplayName("구입한 로또의 개수 테스트")
+    void boughtLottos_count() {
+        /* given */
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(LottoTest.getBalls("1", "2", "3", "4", "5", "6")));
+        lottos.add(new Lotto(LottoTest.getBalls("1", "2", "3", "4", "5", "6")));
+
+        /* when */
+        BoughtLottos boughtLottos = new BoughtLottos(lottos);
+
+        /* then */
+        assertThat(boughtLottos.getCount()).isEqualTo(lottos.size());
     }
 }
