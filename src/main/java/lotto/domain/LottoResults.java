c5f73d8d6f9bded4b3d4c833d9302dddc8e4a441
hwihwi <yjh200523@naver.com>
2023-07-11 18:48:14 +0900

    :sparkles: feat: 로또 결과 일급 컬렉션 구현

---
+++ src/main/java/lotto/domain/LottoResults.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoResults {
+
+    private final List<LottoResult> lottoResults;
+
+    public LottoResults(final List<LottoResult> lottoResults) {
+        this.lottoResults = lottoResults;
+    }
+}
