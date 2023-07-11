57cf67e8c64e1bf811cd36566a68d368d4d70587
jiwon choi <jjiwoning12@gmail.com>
2023-07-11 16:45:26 +0900

    feat: LottoApplication 구현

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import lotto.dto.LottoResultResponseDto;
+import lotto.dto.LottoStatusResponseDto;
+import lotto.dto.MoneyRequestDto;
+import lotto.dto.WinningNumbersRequestDto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoController lottoController = new LottoController(new LottoService());
+
+        MoneyRequestDto moneyRequestDto = inputView.inputMoney();
+        LottoStatusResponseDto lottoStatusResponseDto = lottoController.buyLotto(moneyRequestDto);
+        outputView.printBuyStatus(lottoStatusResponseDto);
+
+        WinningNumbersRequestDto winningNumbersRequestDto = inputView.inputWinningNumbers();
+        LottoResultResponseDto lottoResultResponseDto = lottoController.drawWinningLotto(winningNumbersRequestDto);
+        outputView.printLottoResult(lottoResultResponseDto);
+    }
+}
