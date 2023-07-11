f37a33452ea308177c1e263c6a92f3281ecfd659
hwihwi <yjh200523@naver.com>
2023-07-11 10:46:04 +0900

    :umbrella: test: 로또 개수 검증 테스트 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +24,22 @@ public class LottoTest {
         );
     }
 
+    @Test
+    @DisplayName("로또 개수 검증 테스트")
+    void lotto_invalidBallsSize() {
+        /* given & when & then */
+        assertThatThrownBy(() -> new Lotto(List.of(
+                new Ball("1")
+        ))).isExactlyInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Lotto(List.of(
+                new Ball("1"),
+                new Ball("2"),
+                new Ball("3"),
+                new Ball("4"),
+                new Ball("5"),
+                new Ball("6"),
+                new Ball("7")
+        ))).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
 }
