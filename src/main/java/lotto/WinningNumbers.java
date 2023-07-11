a0410dc344078e84d40c95d8d2f330608b23857b
dahyen0o <seeunchoi99@gmail.com>
2023-07-11 14:17:29 +0900

    feat: '당첨번호 객체 생성 성공' 기능 구현
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/main/java/lotto/WinningNumbers.java
+++ src/main/java/lotto/WinningNumbers.java
@@ -1,9 +1,17 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class WinningNumbers {
+
+    private final List<LottoNumber> winningNumbers;
+    private final LottoNumber bonusBall;
+
     public WinningNumbers(final List<Integer> winningNumbers, final int bonusBall) {
-        throw new UnsupportedOperationException();
+        this.winningNumbers = winningNumbers.stream()
+                .map(LottoNumber::of)
+                .collect(Collectors.toUnmodifiableList());
+        this.bonusBall = LottoNumber.of(bonusBall);
     }
 }
