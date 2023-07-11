6dcff9e8cb1cd944b57990799daa36c3ed217854
whatasame <whatasame@gmail.com>
2023-07-11 19:04:04 +0900

    feat: 등수 상금 계산 구현
    Co-authored-by: gunner6603 <gunner6603@snu.ac.kr>

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,29 +1,42 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public enum Rank {
 
-    FIRST(6, false),
-    SECOND(5, true),
-    THIRD(5, false),
-    FOURTH(4, false),
-    FIFTH(3, false),
-    NONE(0, false);
+    FIRST(6, false, 2_000_000_000L),
+    SECOND(5, true, 30_000_000L),
+    THIRD(5, false, 1_500_000L),
+    FOURTH(4, false, 50_000L),
+    FIFTH(3, false, 5_000L),
+    NONE(0, false, 0L);
 
     private final long matchLottoNumber;
     private final boolean hasToCheckBonus;
+    private final long prize;
 
-    Rank(final long matchLottoNumber, final boolean hasToCheckBonus) {
+    Rank(final long matchLottoNumber, final boolean hasToCheckBonus, final long prize) {
         this.matchLottoNumber = matchLottoNumber;
         this.hasToCheckBonus = hasToCheckBonus;
+        this.prize = prize;
     }
 
     public static Rank resolveRank(final long matchLottoNumber, final boolean containsBonus) {
         return Arrays.stream(Rank.values())
                 .filter(rank -> rank.match(matchLottoNumber, containsBonus))
                 .findAny()
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(() -> new UnsupportedOperationException("지원하지 않는 등수입니다."));
+    }
+
+    public static long calculatePrize(final Map<Rank, Long> statistic) {
+        return statistic.entrySet().stream()
+                .mapToLong(Rank::calculatePrize)
+                .sum();
+    }
+
+    private static long calculatePrize(final Map.Entry<Rank, Long> entry) {
+        return entry.getKey().prize * entry.getValue();
     }
 
     private boolean match(final long matchLottoNumber, final boolean containsBonus) {
