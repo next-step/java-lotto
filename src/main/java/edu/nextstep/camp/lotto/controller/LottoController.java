79ec369ed93c6cc8f57f6ada425374c8483007ec
rkd5656meta <rkd8307@naver.com>
2023-07-12 17:05:42 +0900

    feat: LottoController 추가

---
+++ src/main/java/edu/nextstep/camp/lotto/controller/LottoController.java
@@ -0,0 +1,46 @@
+package edu.nextstep.camp.lotto.controller;
+
+import edu.nextstep.camp.lotto.domain.LottoFactory;
+import edu.nextstep.camp.lotto.domain.LottoGame;
+import edu.nextstep.camp.lotto.domain.Lottos;
+import edu.nextstep.camp.lotto.view.LottoOutputView;
+
+import java.util.Arrays;
+
+public class LottoController {
+
+    private final LottoGame lottoGame;
+    private final LottoOutputView lottoOutputView;
+
+    public LottoController(LottoFactory lottoFactory, LottoOutputView lottoOutputView) {
+        this.lottoGame = new LottoGame(lottoFactory);
+        this.lottoOutputView = lottoOutputView;
+    }
+
+    public Lottos buyLottos(String amount) {
+        return this.buyLottos(Integer.valueOf(amount));
+    }
+
+    public Lottos buyLottos(int amount) {
+        Lottos lottos = lottoGame.buyLotto(amount);
+
+        lottoOutputView.buyLottoView(amount, lottos);
+
+        return lottos;
+    }
+
+    public void analysisLotto(Lottos lottos, String... lottoNumbers) {
+        this.analysisLotto(lottos, Arrays.stream(lottoNumbers)
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new)
+        );
+    }
+
+    public void analysisLotto(Lottos lottos, Integer... lottoNumbers) {
+        if (lottos == null) {
+            new IllegalArgumentException("로또가 null 입니다.");
+        }
+
+        lottoOutputView.lottoAnalysisView(lottos.analysis(lottoNumbers));
+    }
+}
