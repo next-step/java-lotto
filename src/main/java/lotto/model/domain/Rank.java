1901739f3e9310b27fe0767910f52337e95d81b4
dahyen0o <seeunchoi99@gmail.com>
2023-07-12 15:13:06 +0900

    refactor: Rank.valueOf() 리팩토링
    Co-authored-by: dahyen0o <seeunchoi99@gmail.com>
    Co-authored-by: grolarkim <audwnswmf@naver.com>

--- src/main/java/lotto/model/domain/Rank.java
+++ src/main/java/lotto/model/domain/Rank.java
@@ -5,8 +5,18 @@ import java.util.Arrays;
 public enum Rank {
 
     FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000) {
+        @Override
+        boolean match(final int matchCount, final boolean hasBonusBall) {
+            return matchCount == SECOND.matchCount && hasBonusBall == SECOND.hasBonusBall;
+        }
+    },
+    THIRD(5, false, 1_500_000) {
+        @Override
+        boolean match(final int matchCount, final boolean hasBonusBall) {
+            return matchCount == THIRD.matchCount && hasBonusBall == THIRD.hasBonusBall;
+        }
+    },
     FOURTH(4, false, 50_000),
     FIFTH(3, false, 5_000),
     NONE(0, false, 0);
@@ -22,18 +32,14 @@ public enum Rank {
     }
 
     public static Rank valueOf(final int matchCount, final boolean hasBonusBall) {
-        // TODO refactor
-        if (matchCount == 5 && hasBonusBall) {
-            return SECOND;
-        }
-        if (matchCount == 5) {
-            return THIRD;
-        }
-
         return Arrays.stream(values())
-                .filter(rank -> rank.matchCount == matchCount)
+                .filter(rank -> rank.match(matchCount, hasBonusBall))
                 .findFirst()
                 .orElse(NONE);
+    }
+
+    boolean match(final int matchCount, final boolean hasBonusBall) {
+        return this.matchCount == matchCount;
     }
 
     public static long getTotalPrize(Rank rank, int count) {
