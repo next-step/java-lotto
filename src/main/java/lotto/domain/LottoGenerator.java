60eece4239932287f31dad6c0b4ff9cbd272b878
ProtoSeo <proto_seo@naver.com>
2023-07-12 16:48:25 +0900

    :hammer: refactor: for문을 stream을 활용하도록 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,7 +1,8 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoGenerator {
 
@@ -14,19 +15,16 @@ public class LottoGenerator {
 
     public LottoGenerator(final LottoGenerateStrategy lottoGenerateStrategy) {
         this.lottoGenerateStrategy = lottoGenerateStrategy;
-        this.balls = new ArrayList<>();
-        for (int i = Ball.MIN_NUMBER; i <= Ball.MAX_NUMBER; i++) {
-            this.balls.add(new Ball(i));
-        }
+        this.balls = IntStream.rangeClosed(Ball.MIN_NUMBER, Ball.MAX_NUMBER)
+                .mapToObj(Ball::new)
+                .collect(Collectors.toList());
     }
 
     public BoughtLottos generate(final Money money) {
-        final int count = money.getValue() / Lotto.PRICE;
-        final List<Lotto> boughtLottos = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            boughtLottos.add(lottoGenerateStrategy.generate(balls));
-        }
-
+        final int buyCount = money.getValue() / Lotto.PRICE;
+        final List<Lotto> boughtLottos = IntStream.range(0, buyCount)
+                .mapToObj(i -> lottoGenerateStrategy.generate(balls))
+                .collect(Collectors.toList());
         return new BoughtLottos(boughtLottos);
     }
 }
