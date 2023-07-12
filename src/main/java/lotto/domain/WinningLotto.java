bed2f24e06679d986f290e8089a3acdc0b3b94b5
ProtoSeo <proto_seo@naver.com>
2023-07-12 17:27:15 +0900

    :hammer: refactor: 예외 관련 메세지 작성

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class WinningLotto {
 
+    private static final String DUPLICATE_BONUS_WITH_LOTTO_BALLS_EXCEPTION_MESSAGE
+            = "보너스 볼은 지난주 당첨 로또 번호와 중복될 수 없습니다.";
     private final Lotto lotto;
     private final Ball bonus;
 
@@ -13,7 +15,7 @@ public class WinningLotto {
 
     private void validate(final Lotto lotto, final Ball bonus) {
         if (lotto.isContains(bonus)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATE_BONUS_WITH_LOTTO_BALLS_EXCEPTION_MESSAGE);
         }
     }
 }
