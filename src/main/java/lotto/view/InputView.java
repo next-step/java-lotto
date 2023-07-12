8db2dfb38508b339ea53c0b2f01da8e8f7c38247
dongjoo0-0 <kdj486347@gmail.com>
2023-07-12 15:29:09 +0900

    fix: 당첨 번호 입력시 오류 해결

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,10 +3,10 @@ package lotto.view;
 import lotto.dto.MoneyRequestDto;
 import lotto.dto.WinningNumbersRequestDto;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
 
@@ -21,7 +21,8 @@ public class InputView {
     public WinningNumbersRequestDto inputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String numbers = scanner.nextLine();
-        List<Integer> lottoNumbers = Arrays.stream(numbers.split("[, ]"))
+        List<Integer> lottoNumbers = Stream.of(numbers.split(","))
+                .map(String::trim)
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
