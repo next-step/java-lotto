4bad56400574ba85549177561d6f7aebd0047eb4
rkd5656meta <rkd8307@naver.com>
2023-07-14 00:54:34 +0900

    feat: 지난 당첨 번호 정보를 가지고 있는 객체 추가
    - 보너스 번호와 로또 번호 리스트를 받아 객체 생성
    - 기존 로또를 기반으로 LottoRank를 확인할 수 있는 기능

---
+++ src/main/java/edu/nextstep/camp/lotto/domain/LastWinLotto.java
@@ -0,0 +1,44 @@
+package edu.nextstep.camp.lotto.domain;
+
+import java.util.Arrays;
+
+public class LastWinLotto {
+
+    private final Lotto lastWinLotto;
+
+    private final LottoNumber bonusNumber;
+
+    private LastWinLotto(Lotto lastWinLotto, LottoNumber bonusNumber) {
+        this.lastWinLotto = lastWinLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static LastWinLotto of(String bonusNumber, String[] lottoNumbers) {
+        return of(Integer.parseInt(bonusNumber), Arrays.stream(lottoNumbers)
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new)
+        );
+    }
+
+    public LottoRank checkRank(Lotto lotto) {
+        return LottoRank.valueOf(lotto.matchNumberCount(lastWinLotto), lotto.isMatchBonus(bonusNumber));
+    }
+
+    public static LastWinLotto of(Integer bonusNumber, Integer[] lottoNumbers) {
+        duplicateBonusNumberCheck(bonusNumber, lottoNumbers);
+
+        return new LastWinLotto(new Lotto(lottoNumbers), new LottoNumber(bonusNumber));
+    }
+
+    private static void duplicateBonusNumberCheck(Integer bonusNumber, Integer[] beforeLottoNums) {
+        for (Integer beforeLottoNum : beforeLottoNums) {
+            numberEqualsCheck(bonusNumber, beforeLottoNum);
+        }
+    }
+
+    private static void numberEqualsCheck(Integer bonusNumber, Integer beforeLottoNum) {
+        if (beforeLottoNum.equals(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 지난주 로또 번호와 겹칠 수 없습니다.");
+        }
+    }
+}
