91ac191dfdcd217d6bb80ffb7b10d87e10f1da29
rimrim990 <lim990718@naver.com>
2023-07-13 14:14:20 +0900

    refactor: LottoNumber 캐시를 미리 생성해놓지 않고 추후 할당하도록 수정

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,29 +1,23 @@
 package lotto.domain;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LottoNumber {
 
     static final int LOTTO_START = 1;
     static final int LOTTO_END = 45;
-    private static final List<LottoNumber> lottoNumberCache = IntStream.rangeClosed(
-            0,
-            LottoNumber.LOTTO_END
-        )
-        .boxed()
-        .map(LottoNumber::new)
-        .collect(Collectors.toList());
+    private static final Map<Integer, LottoNumber> lottoNumberCache = new HashMap<>();
+
     private final Integer number;
 
     private LottoNumber(final Integer number) {
         this.number = number;
     }
 
-    public static LottoNumber of(int number) {
+    public static LottoNumber of(final int number) {
         verify(number);
-        return lottoNumberCache.get(number);
+        return lottoNumberCache.putIfAbsent(number, new LottoNumber(number));
     }
 
     private static void verify(final Integer number) {
