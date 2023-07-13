ab063cae97170fbc7b295ea610ceaa246fe7231c
김명준 <audwnswmf@naver.com>
2023-07-13 15:22:01 +0900

    chore: Rank의 prize를 long으로 명시

--- src/main/java/lotto/model/domain/Rank.java
+++ src/main/java/lotto/model/domain/Rank.java
@@ -4,28 +4,28 @@ import java.util.Arrays;
 
 public enum Rank {
 
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000) {
+    FIRST(6, false, 2_000_000_000L),
+    SECOND(5, true, 30_000_000L) {
         @Override
         boolean match(final int matchCount, final boolean hasBonusBall) {
             return matchCount == SECOND.matchCount && hasBonusBall == SECOND.hasBonusBall;
         }
     },
-    THIRD(5, false, 1_500_000) {
+    THIRD(5, false, 1_500_000L) {
         @Override
         boolean match(final int matchCount, final boolean hasBonusBall) {
             return matchCount == THIRD.matchCount && hasBonusBall == THIRD.hasBonusBall;
         }
     },
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FOURTH(4, false, 50_000L),
+    FIFTH(3, false, 5_000L),
+    NONE(0, false, 0L);
 
     private final int matchCount;
     private final boolean hasBonusBall;
     private final long prize;
 
-    Rank(final int matchCount, final boolean hasBonusBall, final int prize) {
+    Rank(final int matchCount, final boolean hasBonusBall, final long prize) {
         this.matchCount = matchCount;
         this.hasBonusBall = hasBonusBall;
         this.prize = prize;
