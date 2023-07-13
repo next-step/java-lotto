189f8384e126e4d0f411c7231ac4b6aa93af4467
rkd5656meta <rkd8307@naver.com>
2023-07-14 00:55:21 +0900

    refactor: 기존 analysis 관련 메소드 미사용 처리 및 LastWinLotto 기반으로 analysis 기능 사용하게 변경

--- src/main/java/edu/nextstep/camp/lotto/controller/LottoController.java
+++ src/main/java/edu/nextstep/camp/lotto/controller/LottoController.java
@@ -1,8 +1,6 @@
 package edu.nextstep.camp.lotto.controller;
 
-import edu.nextstep.camp.lotto.domain.LottoFactory;
-import edu.nextstep.camp.lotto.domain.LottoGame;
-import edu.nextstep.camp.lotto.domain.Lottos;
+import edu.nextstep.camp.lotto.domain.*;
 import edu.nextstep.camp.lotto.view.LottoOutputView;
 
 import java.util.Arrays;
@@ -29,6 +27,10 @@ public class LottoController {
         return lottos;
     }
 
+    /**
+     * @Deprecated 보너스 번호 추가로 미사용
+     */
+    @Deprecated
     public void analysisLotto(Lottos lottos, String... lottoNumbers) {
         this.analysisLotto(lottos, Arrays.stream(lottoNumbers)
                 .map(Integer::parseInt)
@@ -36,11 +38,22 @@ public class LottoController {
         );
     }
 
+    /**
+     * @Deprecated 보너스 번호 추가로 미사용
+     */
     public void analysisLotto(Lottos lottos, Integer... lottoNumbers) {
+        this.analysisLotto(lottos, lottos.analysis(lottoNumbers));
+    }
+
+    public void analysisLotto(Lottos lottos, LastWinLotto lastWinLotto) {
+        this.analysisLotto(lottos, lottos.analysis(lastWinLotto));
+    }
+
+    public void analysisLotto(Lottos lottos, LottoAnalysisResult analysisResult) {
         if (lottos == null) {
             throw new IllegalArgumentException("로또가 null 입니다.");
         }
 
-        lottoOutputView.lottoAnalysisView(lottos.analysis(lottoNumbers));
+        lottoOutputView.lottoAnalysisView(analysisResult);
     }
 }
