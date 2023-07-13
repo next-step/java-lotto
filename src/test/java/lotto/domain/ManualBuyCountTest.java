7d54b4f850f85c6b91007cb2140785f389561923
ProtoSeo <proto_seo@naver.com>
2023-07-14 08:45:17 +0900

    :hammer: refactor: static import 로 분리

--- src/test/java/lotto/domain/ManualBuyCountTest.java
+++ src/test/java/lotto/domain/ManualBuyCountTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,6 @@ public class ManualBuyCountTest {
     @Test
     @DisplayName("수동으로 구매할 로또 수 생성 테스트")
     void manualBuyCount_create() {
-        Assertions.assertDoesNotThrow(() -> new ManualBuyCount(10, new Money("10000")));
+        assertDoesNotThrow(() -> new ManualBuyCount(10, new Money("10000")));
     }
 }
