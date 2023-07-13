0552018201f3cabe76732fc82b0fa91f14cd753f
gunner6603 <gunner6603@snu.ac.kr>
2023-07-14 00:16:58 +0900

    feat: LottoApplication 실행 로직 변경

--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,8 +1,13 @@
-import domain.*;
+import domain.Lotto;
+import domain.LottoGenerator;
+import domain.LottoNumber;
+import domain.LottoResult;
+import domain.Lottos;
+import domain.WinningLotto;
+import java.util.ArrayList;
+import java.util.List;
 import view.InputView;
 import view.OutputView;
-
-import java.util.List;
 
 public class LottoApplication {
 
@@ -20,16 +25,29 @@ public class LottoApplication {
         // 가격 입력
         final long money = InputView.readMoney();
 
+        //수동으로 구매할 로또 수 입력
+        final long manualPurchaseCount = InputView.readManualPurchaseCount();
+
+        //수동으로 구매할 번호 입력
+        List<List<Integer>> numberBundles = new ArrayList<>();
+        InputView.printPromptMessageForManualPurchaseLottoNumbers();
+
+        for (int count = 0; count < manualPurchaseCount; count++) {
+            numberBundles.add(InputView.readLottoNumbers());
+        }
+
         // 로또 만들고
-        final List<Lotto> lottos = LottoGenerator.generateLottosAutomatically(money);
+        final List<Lotto> lottos = LottoGenerator.generateLottosManuallyAndThenAutomatically(money,
+            manualPurchaseCount, numberBundles);
 
         // 구매 결과 출력
-        OutputView.printLottosSize(lottos);
+        OutputView.printLottosSize(lottos, manualPurchaseCount);
 
         // 로또 출력한다.
         OutputView.printLottos(lottos);
 
         // 당첨 번호 입력
+        InputView.printPromptMessageForWinningLottoNumbers();
         final List<Integer> lottoNumbers = InputView.readLottoNumbers();
         final int lottoNumber = InputView.readLottoNumber();
 
@@ -40,9 +58,11 @@ public class LottoApplication {
         OutputView.printStatistic(lottoResult, money);
     }
 
-    private static LottoResult play(final List<Lotto> target, final List<Integer> lottoNumbers, final int lottoNumber) {
+    private static LottoResult play(final List<Lotto> target, final List<Integer> lottoNumbers,
+        final int lottoNumber) {
         final Lottos lottos = new Lottos(target);
-        final WinningLotto winningLotto = new WinningLotto(new Lotto(lottoNumbers), new LottoNumber(lottoNumber));
+        final WinningLotto winningLotto = new WinningLotto(new Lotto(lottoNumbers),
+            new LottoNumber(lottoNumber));
 
         return new LottoResult(lottos.checkAllLottoResult(winningLotto));
     }
