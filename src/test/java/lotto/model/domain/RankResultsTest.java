30c282dbe9021023547e4609c3bf84496eca2917
김명준 <audwnswmf@naver.com>
2023-07-13 17:34:32 +0900

    test: RankResults 생성자 추가 테스트 및 통계 반환 테스트

--- src/test/java/lotto/model/domain/RankResultsTest.java
+++ src/test/java/lotto/model/domain/RankResultsTest.java
@@ -2,8 +2,10 @@ package lotto.model.domain;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class RankResultsTest {
@@ -13,5 +15,34 @@ class RankResultsTest {
         // given, when, then
         assertThat(new RankResults(List.of(Rank.FIRST, Rank.SECOND, Rank.NONE)).getRanks())
                 .isEqualTo(List.of(Rank.FIRST, Rank.SECOND, Rank.NONE));
+    }
+
+    @Test
+    void 로또_당첨결과_객체_합쳐서_생성_및_값_확인_성공() {
+        // given
+        RankResults rankResults = new RankResults(
+                new RankResults(List.of(Rank.FIRST, Rank.SECOND, Rank.NONE)),
+                new RankResults(List.of(Rank.FIRST, Rank.THIRD, Rank.NONE)));
+
+        // when, then
+        assertThat(rankResults.getRanks()).isEqualTo(
+                List.of(Rank.FIRST, Rank.SECOND, Rank.NONE, Rank.FIRST, Rank.THIRD, Rank.NONE));
+    }
+
+    @Test
+    void 로또_당첨결과_객체_통계_반환_성공() {
+        // given
+        RankResults rankResults = new RankResults(List.of(Rank.FIRST, Rank.SECOND, Rank.NONE));
+
+        // when
+        Map<Rank, Integer> counts = rankResults.getRanksCounts();
+
+        // then
+        assertThat(counts).contains(
+                entry(Rank.FIRST, 1),
+                entry(Rank.SECOND, 1),
+                entry(Rank.NONE, 1)
+        );
+
     }
 }
