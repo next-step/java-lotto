d0dd8f3c75a432753c334f00f87dbcef9445aa2a
rimrim990 <lim990718@naver.com>
2023-07-13 14:36:25 +0900

    refactor: LottoNumber 에 equals 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class LottoNumber {
 
@@ -29,5 +30,22 @@ public class LottoNumber {
 
     public Integer getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
