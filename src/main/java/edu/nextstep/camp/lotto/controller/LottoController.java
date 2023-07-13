f9eccca03945f2fb2ba0a80f120a761a5f2ce5f7
rkd5656meta <rkd8307@naver.com>
2023-07-14 00:59:27 +0900

    refactor: 기본 analysisLotto 삭제

--- src/main/java/edu/nextstep/camp/lotto/controller/LottoController.java
+++ src/main/java/edu/nextstep/camp/lotto/controller/LottoController.java
@@ -3,8 +3,6 @@ package edu.nextstep.camp.lotto.controller;
 import edu.nextstep.camp.lotto.domain.*;
 import edu.nextstep.camp.lotto.view.LottoOutputView;
 
-import java.util.Arrays;
-
 public class LottoController {
 
     private final LottoGame lottoGame;
@@ -27,33 +25,11 @@ public class LottoController {
         return lottos;
     }
 
-    /**
-     * @Deprecated 보너스 번호 추가로 미사용
-     */
-    @Deprecated
-    public void analysisLotto(Lottos lottos, String... lottoNumbers) {
-        this.analysisLotto(lottos, Arrays.stream(lottoNumbers)
-                .map(Integer::parseInt)
-                .toArray(Integer[]::new)
-        );
-    }
-
-    /**
-     * @Deprecated 보너스 번호 추가로 미사용
-     */
-    public void analysisLotto(Lottos lottos, Integer... lottoNumbers) {
-        this.analysisLotto(lottos, lottos.analysis(lottoNumbers));
-    }
-
     public void analysisLotto(Lottos lottos, LastWinLotto lastWinLotto) {
-        this.analysisLotto(lottos, lottos.analysis(lastWinLotto));
-    }
-
-    public void analysisLotto(Lottos lottos, LottoAnalysisResult analysisResult) {
         if (lottos == null) {
             throw new IllegalArgumentException("로또가 null 입니다.");
         }
 
-        lottoOutputView.lottoAnalysisView(analysisResult);
+        lottoOutputView.lottoAnalysisView(lottos.analysis(lastWinLotto));
     }
 }
