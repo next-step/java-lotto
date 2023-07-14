8075dc92cca5ae0eabc4523604a454e3f9408761
ProtoSeo <proto_seo@naver.com>
2023-07-14 16:51:50 +0900

    :sparkles: feat: 해당 금액으로 원하는 로또 개수만큼 구매할 수 있는지 검사하도록 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -8,8 +8,13 @@ public class Money {
             "로또는 " + MIN_PURCHASE_MONEY + "원 이상 " + MAX_PURCHASE_MONEY + "원 이하만 구매할 수 있습니다";
     private static final String NUMBER_PATTERN = "\\d+";
     private static final String NOT_NUMERIC_EXCEPTION_MESSAGE = "숫자만 입력할 수 있습니다.";
+    private static final String CAN_NOT_BUY_EXCEPTION_MESSAGE = "주어진 돈으로 해당 개수 만큼 로또를 구매할 수 없습니다.";
 
     private final int value;
+
+    Money(final int value) {
+        this.value = value;
+    }
 
     public Money(final String value) {
         validate(value);
@@ -30,6 +35,17 @@ public class Money {
     private void validateRangeOfMoney(final int money) {
         if (money < MIN_PURCHASE_MONEY || MAX_PURCHASE_MONEY < money) {
             throw new IllegalArgumentException(MONEY_RANGE_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public Money receiveAfterPurchaseLotto(LottoBuyCount lottoBuyCount) {
+        validateCanPurchaseLottoWantedCount(lottoBuyCount);
+        return new Money(this.value - Lotto.calculatePurchaseMoney(lottoBuyCount).getValue());
+    }
+
+    public void validateCanPurchaseLottoWantedCount(LottoBuyCount lottoBuyCount) {
+        if (this.value < Lotto.calculatePurchaseMoney(lottoBuyCount).getValue()) {
+            throw new IllegalArgumentException(CAN_NOT_BUY_EXCEPTION_MESSAGE);
         }
     }
 
