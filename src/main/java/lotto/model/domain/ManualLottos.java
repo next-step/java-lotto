a6c3b9875af56f77e2213aa45731fa342a81940d
김명준 <audwnswmf@naver.com>
2023-07-14 14:24:11 +0900

    feat: 수동 로또 컬렉션 추가 및 테스트 추가

---
+++ src/main/java/lotto/model/domain/ManualLottos.java
@@ -0,0 +1,10 @@
+package lotto.model.domain;
+
+import java.util.List;
+
+public final class ManualLottos extends Lottos {
+
+    public ManualLottos(List<Lotto> lottos) {
+        super(lottos);
+    }
+}
