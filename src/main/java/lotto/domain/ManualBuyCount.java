754aeec9e9767e714ee040172ede76d47f4d3d9a
ProtoSeo <proto_seo@naver.com>
2023-07-14 13:44:42 +0900

    :sparkles: feat: 주어진 돈으로 수동 로또를 살 수 없는 경우 검증 추가

--- src/main/java/lotto/domain/ManualBuyCount.java
+++ src/main/java/lotto/domain/ManualBuyCount.java
@@ -5,6 +5,13 @@ public class ManualBuyCount {
     final int value;
 
     public ManualBuyCount(final int value, final Money money) {
+        validate(value, money);
         this.value = value;
+    }
+
+    private void validate(final int value, final Money money) {
+        if (money.getValue() < value * Lotto.PRICE) {
+            throw new IllegalArgumentException("주어진 돈으로 해당 개수 만큼 로또를 구매할 수 없습니다.");
+        }
     }
 }
