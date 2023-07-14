dcd5155c53ba61040155150897b46ada448b8e88
worldii <bbc2788@naver.com>
2023-07-14 15:05:04 +0900

    Test : shuffle 자동 로또 테스트

--- src/test/java/lotto/model/AutoLottoGeneratorTest.java
+++ src/test/java/lotto/model/AutoLottoGeneratorTest.java
@@ -1,22 +1,29 @@
 package lotto.model;
 
-import lotto.model.util.AutoLottoGenerator;
-import lotto.model.util.LottoGenerator;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AutoLottoGeneratorTest {
 
     @Test
-    @DisplayName("로또 자동 생성기는 올바른 로또를 생성한다.")
-    void 자동_로또_생성() {
-
+    @DisplayName("로또 자동 생성기는 로또를 6자리 생성한다.")
+    void 자동_로또_생성_6자리() {
+        // given
         LottoGenerator autoLottoGenerator = new AutoLottoGenerator();
+        // when
+        List<LottoNumber> generateLotto = autoLottoGenerator.generate();
+        // then
+        assertThat(generateLotto.size()).isEqualTo(6);
+    }
 
-        assertThatCode(autoLottoGenerator::generate)
-            .doesNotThrowAnyException();
+    @Test
+    @DisplayName("로또 자동 생성기는 매번 실행 시,새로운 로또를 생성한다.")
+    void 매번_자동_로또_생성() {
+        LottoGenerator lottoGenerator = new AutoLottoGenerator();
+        assertThat(lottoGenerator.generate()).isNotEqualTo(lottoGenerator.generate());
     }
 
 }
