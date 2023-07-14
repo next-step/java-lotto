3a407875c6735c47b823b1f02f3e2d156e0f3800
ProtoSeo <proto_seo@naver.com>
2023-07-14 14:17:43 +0900

    :sparkles: feat: 로또 구매 개수 구입 및 WinningLotto를 반환해주는 메서드로 수정

--- src/main/java/lotto/view/LottoReader.java
+++ src/main/java/lotto/view/LottoReader.java
@@ -5,25 +5,42 @@ import java.util.List;
 import java.util.Scanner;
 import lotto.domain.Ball;
 import lotto.domain.Lotto;
+import lotto.domain.ManualLottoBuyCount;
+import lotto.domain.Money;
+import lotto.domain.WinningLotto;
 
 public class LottoReader {
 
     private static final String DELIMITER = ",";
+    private static final String INPUT_MANUAL_LOTTO_BUY_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String INPUT_WINNING_LOTTO_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해주세요.";
     private static final String INPUT_BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요.";
 
-    public Lotto readWinningLotto() {
-        System.out.println(INPUT_WINNING_LOTTO_NUMBERS_MESSAGE);
+    public ManualLottoBuyCount readManualLottoBuyCount(Money money) {
+        System.out.println(INPUT_MANUAL_LOTTO_BUY_COUNT_MESSAGE);
         Scanner scanner = new Scanner(System.in);
-        List<Ball> winningLottoBalls = new ArrayList<>();
-        String winningLottoNumbers = scanner.nextLine();
-        for (String winningLottoNumber : winningLottoNumbers.split(DELIMITER)) {
-            winningLottoBalls.add(new Ball(winningLottoNumber.trim()));
-        }
-        return new Lotto(winningLottoBalls);
+        return new ManualLottoBuyCount(Integer.parseInt(scanner.nextLine()), money);
     }
 
-    public Ball readBonusBall() {
+    public WinningLotto readWinningLotto() {
+        return new WinningLotto(
+                readLotto(INPUT_WINNING_LOTTO_NUMBERS_MESSAGE),
+                readBonusBall()
+        );
+    }
+
+    private Lotto readLotto(final String inputManualLottoNumbersMessage) {
+        System.out.println(inputManualLottoNumbersMessage);
+        Scanner scanner = new Scanner(System.in);
+        List<Ball> lottoBalls = new ArrayList<>();
+        final String lottoNumbers = scanner.nextLine();
+        for (String lottoNumber : lottoNumbers.split(DELIMITER)) {
+            lottoBalls.add(new Ball(lottoNumber.trim()));
+        }
+        return new Lotto(lottoBalls);
+    }
+
+    private Ball readBonusBall() {
         System.out.println(INPUT_BONUS_BALL_MESSAGE);
         Scanner scanner = new Scanner(System.in);
         return new Ball(scanner.nextLine());
