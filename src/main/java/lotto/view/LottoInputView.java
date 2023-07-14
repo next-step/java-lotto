606d6f406829663a359f35ee8344679085ffa3e4
dahyen0o <seeunchoi99@gmail.com>
2023-07-14 14:49:30 +0900

    feat: 수동 로또의 개수를 입력받는 기능 구현

--- src/main/java/lotto/view/LottoInputView.java
+++ src/main/java/lotto/view/LottoInputView.java
@@ -22,6 +22,11 @@ public final class LottoInputView {
         return Long.parseLong(SCANNER.nextLine());
     }
 
+    public long inputManualLottosCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Long.parseLong(SCANNER.nextLine());
+    }
+
     public List<Integer> inputWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         return Arrays.stream(SCANNER.nextLine().split(","))
