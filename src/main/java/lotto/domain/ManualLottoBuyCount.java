ff2dd43709f289743a84d4c0cd3453dd2df29a6f
ProtoSeo <proto_seo@naver.com>
2023-07-14 13:50:06 +0900

    :hammer: refactor: 클래스 네임 수정

--- src/main/java/lotto/domain/ManualLottoBuyCount.java
+++ src/main/java/lotto/domain/ManualLottoBuyCount.java
@@ -1,11 +1,11 @@
 package lotto.domain;
 
-public class ManualBuyCount {
+public class ManualLottoBuyCount {
 
     private static final String CAN_NOT_BUY_EXCEPTION_MESSAGE = "주어진 돈으로 해당 개수 만큼 로또를 구매할 수 없습니다.";
     private final int value;
 
-    public ManualBuyCount(final int value, final Money money) {
+    public ManualLottoBuyCount(final int value, final Money money) {
         validate(value, money);
         this.value = value;
     }
--- src/test/java/lotto/domain/ManualBuyCountTest.java
+++ src/test/java/lotto/domain/ManualBuyCountTest.java
@@ -10,14 +10,14 @@ public class ManualBuyCountTest {
     @Test
     @DisplayName("수동으로 구매할 로또 수 생성 테스트")
     void manualBuyCount_create() {
-        assertDoesNotThrow(() -> new ManualBuyCount(10, new Money("10000")));
+        assertDoesNotThrow(() -> new ManualLottoBuyCount(10, new Money("10000")));
     }
 
     @Test
     @DisplayName("입력 금액보다 더 많이 로또를 구매하려는 경우 테스트")
     void manualBuyCount_outOfRange() {
         assertThrows(IllegalArgumentException.class,
-                () -> new ManualBuyCount(10, new Money("9999"))
+                () -> new ManualLottoBuyCount(10, new Money("9999"))
         );
     }
 }
