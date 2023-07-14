70036e04a5ea14b71c702b1252e542c3419f9cd7
ProtoSeo <proto_seo@naver.com>
2023-07-14 13:46:54 +0900

    :hammer: refactor: 예외 메세지 작성

--- src/main/java/lotto/domain/ManualBuyCount.java
+++ src/main/java/lotto/domain/ManualBuyCount.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 public class ManualBuyCount {
 
+    private static final String CAN_NOT_BUY_EXCEPTION_MESSAGE = "주어진 돈으로 해당 개수 만큼 로또를 구매할 수 없습니다.";
     final int value;
 
     public ManualBuyCount(final int value, final Money money) {
@@ -11,7 +12,7 @@ public class ManualBuyCount {
 
     private void validate(final int value, final Money money) {
         if (money.getValue() < value * Lotto.PRICE) {
-            throw new IllegalArgumentException("주어진 돈으로 해당 개수 만큼 로또를 구매할 수 없습니다.");
+            throw new IllegalArgumentException(CAN_NOT_BUY_EXCEPTION_MESSAGE);
         }
     }
 }
