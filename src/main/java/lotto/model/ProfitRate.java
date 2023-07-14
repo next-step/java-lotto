16e91ffebfd074b3c98fb4af6863733805ea7346
김명준 <audwnswmf@naver.com>
2023-07-14 15:57:02 +0900

    feat: 로또 수익률 객체 총 상금 및 지불한돈 음수일시 예외 발생

--- src/main/java/lotto/model/ProfitRate.java
+++ src/main/java/lotto/model/ProfitRate.java
@@ -1,11 +1,29 @@
 package lotto.model;
 
-public class ProfitRate {
+public final class ProfitRate {
 
     private final double profitRate;
 
-    public ProfitRate(long totalPrize, double spentMoney) {
+    public ProfitRate(final long totalPrize, final double spentMoney) {
+        validate(totalPrize, spentMoney);
         this.profitRate = totalPrize / spentMoney;
+    }
+
+    private void validate(long totalPrize, double spentMoney) {
+        validateTotalPrize(totalPrize);
+        validateSpentMoney(spentMoney);
+    }
+
+    private static void validateSpentMoney(double spentMoney) {
+        if (spentMoney < 0.0) {
+            throw new IllegalArgumentException("지불한 돈은 0 이상입니다.");
+        }
+    }
+
+    private static void validateTotalPrize(long totalPrize) {
+        if (totalPrize < 0L) {
+            throw new IllegalArgumentException("총 상금은 0 이상입니다.");
+        }
     }
 
     public double getProfitRate() {
--- src/test/java/lotto/model/ProfitRateTest.java
+++ src/test/java/lotto/model/ProfitRateTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,17 @@ class ProfitRateTest {
         // given, when, then
         assertThat(new ProfitRate(10000, 1000).getProfitRate()).isCloseTo(10.0,
                 Percentage.withPercentage(99.9));
+    }
+
+    @Test
+    void 로또_수익률_총상금_음수일시_예외발생() {
+        // given, when, then
+        assertThrows(IllegalArgumentException.class, () -> new ProfitRate(-1L, 1000));
+    }
+
+    @Test
+    void 로또_수익률_지불한돈_음수일시_예외발생() {
+        // given, when, then
+        assertThrows(IllegalArgumentException.class, () -> new ProfitRate(0, -1));
     }
 }
