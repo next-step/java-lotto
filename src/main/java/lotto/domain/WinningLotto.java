39a825b7f9b1720dd1bc44154b9cc39903abe8d6
hwihwi <yjh200523@naver.com>
2023-07-14 16:34:33 +0900

    :hammer: refactor: 예외 메시지를 상수로 변경

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 public class WinningLotto {
 
+    private static final String DUPLICATE_BONUS_BALL_EXCEPTION_MESSAGE = "당첨 번호와 보너스 볼이 중복됩니다.";
+
     private final Lotto winningLotto;
     private final Ball bonusBall;
 
@@ -13,7 +15,7 @@ public class WinningLotto {
 
     private void validate(final Lotto lotto, final Ball bonusBall) {
         if (lotto.containsBall(bonusBall)) {
-            throw new IllegalArgumentException("당첨 번호와 보너스 볼이 중복됩니다.");
+            throw new IllegalArgumentException(DUPLICATE_BONUS_BALL_EXCEPTION_MESSAGE);
         }
     }
 
