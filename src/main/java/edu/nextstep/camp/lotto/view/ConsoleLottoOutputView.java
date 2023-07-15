cc68238e2b452ca3142a44633e031a009b6480b8
rkd5656meta <rkd8307@naver.com>
2023-07-15 16:32:16 +0900

    refactor: 로또 분석 콘솔 출력 시 2등 필터 로직 추가

--- src/main/java/edu/nextstep/camp/lotto/view/ConsoleLottoOutputView.java
+++ src/main/java/edu/nextstep/camp/lotto/view/ConsoleLottoOutputView.java
@@ -20,11 +20,20 @@ public class ConsoleLottoOutputView implements LottoOutputView {
         print("---------");
 
         for (int i = 3; i <= 6; i++) {
-            LottoRank lottoRank = LottoRank.withMatchNumberCount(i);
-            print(i + "개 일치 (" + lottoRank.getWinnings() + ")-" + analysisResult.countByLottoRank(lottoRank) + "개");
+            printLotto(analysisResult, i);
         }
 
         print("총 수익률은 " + analysisResult.earningRate() + "입니다.");
+    }
+
+    private void printLotto(LottoAnalysisResult analysisResult, int matchCount) {
+        LottoRank lottoRank = LottoRank.valueOf(matchCount, false);
+        print(matchCount + "개 일치 (" + lottoRank.getWinnings() + ")-" + analysisResult.countByLottoRank(lottoRank) + "개");
+
+        if (matchCount == 5){
+            lottoRank = LottoRank.SECOND;
+            print(matchCount + "개, 보너스 볼 일치 일치 (" + lottoRank.getWinnings() + ")-" + analysisResult.countByLottoRank(lottoRank) + "개");
+        }
     }
 
     private void print(String msg) {
