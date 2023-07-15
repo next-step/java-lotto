49b07e6ea7423c26f35ee9a75d5634b6d7f77546
pushedrumex <pushedrumex@naver.com>
2023-07-16 00:43:36 +0900

    test : LOTTO_PRICE 상수화

--- src/test/java/lotto/PaymentTest.java
+++ src/test/java/lotto/PaymentTest.java
@@ -9,13 +9,15 @@ import org.junit.jupiter.api.Test;
 
 public class PaymentTest {
 
+    final static int LOTTO_PRICE = 1000;
+
     @DisplayName("투입한 금액이 1,000원 미만이면 예외 발생한다")
     @Test()
     void 최소_투입_금액_검증() {
         int value = 999;
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Payment(value))
+            .isThrownBy(() -> new Payment(value, LOTTO_PRICE))
             .withMessage("1,000원 이상 투입해 주세요");
     }
 
@@ -25,11 +27,11 @@ public class PaymentTest {
         int value1 = 12000;
         int value2 = 1000;
 
-        Payment payment1 = new Payment(value1);
-        Payment payment2 = new Payment(value2);
+        Payment payment1 = new Payment(value1, LOTTO_PRICE);
+        Payment payment2 = new Payment(value2, LOTTO_PRICE);
 
-        assertThat(payment1.dividedWith(1000)).isEqualTo(12);
-        assertThat(payment2.dividedWith(1000)).isEqualTo(1);
+        assertThat(payment1.dividedWith(LOTTO_PRICE)).isEqualTo(12);
+        assertThat(payment2.dividedWith(LOTTO_PRICE)).isEqualTo(1);
     }
 
 }
