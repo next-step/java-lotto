83d5db1af1f00448efca5eaae05ecda3e8f960e8
jjungk <jsk33a@gmail.com>
2023-07-15 21:37:58 +0900

    fix(NumberGenerator) : shuffle 마다 새로운 숫자 리스트 생성 -> multithread 환경 대응

--- src/main/java/lotto/domain/game/NumberGenerator.java
+++ src/main/java/lotto/domain/game/NumberGenerator.java
@@ -10,12 +10,6 @@ public final class NumberGenerator {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
 
-    private final List<Integer> numbers;
-
-    public NumberGenerator() {
-        numbers = generateNumbers();
-    }
-
     private List<Integer> generateNumbers() {
         return IntStream.rangeClosed(MIN_NUMBER,
                 MAX_NUMBER)
@@ -24,6 +18,7 @@ public final class NumberGenerator {
     }
 
     public List<Integer> shuffleNumbers() {
+        List<Integer> numbers = generateNumbers();
         Collections.shuffle(numbers);
         return numbers;
     }
