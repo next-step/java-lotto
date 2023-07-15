cebc2cfe1b26df1da1b683a1e2faf182270a66ce
rkd5656meta <rkd8307@naver.com>
2023-07-15 16:36:09 +0900

    refactor: 기존 Input 관련 로직을 InputView로 분리함으로서 코드 간소화

--- src/main/java/edu/nextstep/camp/lotto/Main.java
+++ src/main/java/edu/nextstep/camp/lotto/Main.java
@@ -2,26 +2,19 @@ package edu.nextstep.camp.lotto;
 
 import edu.nextstep.camp.lotto.controller.LottoController;
 import edu.nextstep.camp.lotto.domain.AutoLottoFactory;
-import edu.nextstep.camp.lotto.domain.LastWinLotto;
 import edu.nextstep.camp.lotto.domain.Lottos;
+import edu.nextstep.camp.lotto.view.ConsoleLottoInputView;
 import edu.nextstep.camp.lotto.view.ConsoleLottoOutputView;
-import edu.nextstep.camp.lotto.view.InputView;
-import edu.nextstep.camp.lotto.view.OutputView;
+import edu.nextstep.camp.lotto.view.LottoView;
 
 public class Main {
     public static void main(String[] args) {
-        LottoController controller = new LottoController(new AutoLottoFactory(), new ConsoleLottoOutputView());
+        LottoController controller = new LottoController(new AutoLottoFactory(), new LottoView(new ConsoleLottoInputView(), new ConsoleLottoOutputView()));
 
         try {
-            OutputView.print("구입금액을 입력해 주세요.");
-            Lottos lottos = controller.buyLottos(InputView.input());
-
-            OutputView.print("지난 주 당첨 번호를 입력해 주세요.");
-            String[] lastWinLotto = InputView.input().split(",");
-
-            OutputView.print("보너스 번호를 입력해 주세요");
-            controller.analysisLotto(lottos, LastWinLotto.of(InputView.input(), lastWinLotto));
-        } catch (Exception e){
+            Lottos lottos = controller.buyLottos();
+            controller.analysisLotto(lottos);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
