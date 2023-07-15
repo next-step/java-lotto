a6fd73e54ca84bb30d6d00f7b4ebffd3b4b46baa
jjungk <jsk33a@gmail.com>
2023-07-16 01:49:10 +0900

    test(LottoStatisticsTest) : 테스트 코드 추가

--- src/test/java/lotto/LottoStatisticsTest.java
+++ src/test/java/lotto/LottoStatisticsTest.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+
+import java.util.List;
+import lotto.domain.game.LottoCount;
+import lotto.domain.game.Lottos;
+import lotto.domain.game.NumberGenerator;
 import lotto.domain.game.Payment;
 import lotto.domain.game.WinningNumber;
+import lotto.domain.statistics.LottoStatistics;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,13 +18,56 @@ public class LottoStatisticsTest {
     @DisplayName("로또 결과 통계 객체 생성 성공")
     @Test
     void create() {
-//        LottoService lottoService = new LottoService(new Payment(2000));
+        int inputMoney = 3000;
+        Payment payment = new Payment(inputMoney);
+
+        int lottoManualCount = 2;
+        List<String> lottosManual = List.of(
+            "1,2,3,4,5,6",
+            "7,8,9,10,11,12"
+        );
+        LottoCount lottoCount = new LottoCount(payment, lottoManualCount);
+        NumberGenerator numberGenerator = new NumberGenerator(){
+            @Override
+            public List<Integer> shuffleNumbers() {
+                return List.of(1,2,3,4,5,6);
+            }
+        };
+        Lottos lottos = new Lottos(lottoCount, lottosManual, numberGenerator);
+
         String lottoValue = "1,2,3,4,5,6";
         int bonusValue = 7;
         WinningNumber winningNumber = new WinningNumber(lottoValue, bonusValue);
-        Payment payment = new Payment(3000);
 
-//        assertThatNoException().isThrownBy(() -> new LottoStatistics(lottoService.getLottos(), winningNumber, payment));
+        assertThatNoException().isThrownBy(() -> new LottoStatistics(lottos, winningNumber, payment));
+    }
+
+    @DisplayName("로또 수익률 반환")
+    @Test
+    void getProfitRate() {
+        int inputMoney = 3000;
+        Payment payment = new Payment(inputMoney);
+
+        int lottoManualCount = 2;
+        List<String> lottosManual = List.of(
+            "1,2,3,4,5,6",
+            "7,8,9,10,11,12"
+        );
+        LottoCount lottoCount = new LottoCount(payment, lottoManualCount);
+        NumberGenerator numberGenerator = new NumberGenerator(){
+            @Override
+            public List<Integer> shuffleNumbers() {
+                return List.of(40,41,42,43,44,45);
+            }
+        };
+        Lottos lottos = new Lottos(lottoCount, lottosManual, numberGenerator);
+
+        String lottoValue = "1,2,3,4,5,8";
+        int bonusValue = 7;
+        WinningNumber winningNumber = new WinningNumber(lottoValue, bonusValue);
+
+        LottoStatistics lottoStatistics = new LottoStatistics(lottos, winningNumber, payment);
+        assertThat(lottoStatistics.getProfitRate()).isEqualTo(500.00);
     }
 
 }
