57d9e2faf4296e0bae7e6883c83a908c749c881b
dahyen0o <seeunchoi99@gmail.com>
2023-07-16 13:14:18 +0900

    feat: '총 로또 개수가 0 이하면 실패' 기능 구현

--- src/main/java/lotto/model/domain/LottosSize.java
+++ src/main/java/lotto/model/domain/LottosSize.java
@@ -1,18 +1,26 @@
 package lotto.model.domain;
 
-public class LottosSize {
+public final class LottosSize {
 
     private final long totalSize;
     private final long manualSize;
     private final long automaticSize;
 
     private LottosSize(final long totalSize, final long manualSize) {
+        validateTotalSize(totalSize);
+
         this.totalSize = totalSize;
         this.manualSize = manualSize;
         this.automaticSize = totalSize - manualSize;
     }
 
     public static LottosSize of(final LottoMoney totalMoney, final long manualSize) {
-        return new LottosSize(totalMoney.size(Lotto.SIZE), manualSize);
+        return new LottosSize(totalMoney.size(Lotto.COST), manualSize);
+    }
+
+    private void validateTotalSize(final long totalSize) {
+        if (totalSize <= 0) {
+            throw new IllegalArgumentException("로또를 구입할 수 없습니다. 로또는 한 장에 " + Lotto.COST + "원 입니다.");
+        }
     }
 }
