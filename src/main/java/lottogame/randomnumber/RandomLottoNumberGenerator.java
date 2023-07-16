84acdf544ac97e0b32650dd17c160c98e46f8e41
chanwooDev <pove2019@gmail.com>
2023-07-16 11:44:00 +0900

    refactor: RandomLottoNumberGenerator의 for->Intstream으로 코드 수정

--- src/main/java/lottogame/randomnumber/RandomLottoNumberGenerator.java
+++ src/main/java/lottogame/randomnumber/RandomLottoNumberGenerator.java
@@ -1,12 +1,13 @@
 package lottogame.randomnumber;
 
-import java.util.ArrayList;
+import lottogame.domain.LottoNumber;
+import lottogame.domain.spi.NumberGenerator;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import lottogame.domain.LottoNumber;
-import lottogame.domain.spi.NumberGenerator;
+import java.util.stream.IntStream;
 
 public class RandomLottoNumberGenerator implements NumberGenerator {
 
@@ -18,13 +19,9 @@ public class RandomLottoNumberGenerator implements NumberGenerator {
     }
 
     private List<Integer> createAllLottoNumbers() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-        for (int lottoNumber = LottoNumber.MIN_LOTTO_NUMBER; lottoNumber <= LottoNumber.MAX_LOTTO_NUMBER;
-            lottoNumber++) {
-
-            lottoNumbers.add(lottoNumber);
-        }
-        return lottoNumbers;
+        return IntStream.rangeClosed(LottoNumber.MIN_LOTTO_NUMBER, LottoNumber.MAX_LOTTO_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
     }
 
     private Set<Integer> createSubSetFrom(List<Integer> nonShuffledValues, int count) {
