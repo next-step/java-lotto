b29d3784af5a6a28225622d68ab304a22d46f0c9
dahyen0o <seeunchoi99@gmail.com>
2023-07-16 13:22:31 +0900

    feat: '수동 로또 개수가 0 미만이거나 총 로또 개수 초과면 실패' 기능 구현

--- src/main/java/lotto/model/domain/LottosSize.java
+++ src/main/java/lotto/model/domain/LottosSize.java
@@ -8,6 +8,7 @@ public final class LottosSize {
 
     private LottosSize(final long totalSize, final long manualSize) {
         validateTotalSize(totalSize);
+        validateManualSize(manualSize, totalSize);
 
         this.totalSize = totalSize;
         this.manualSize = manualSize;
@@ -21,6 +22,16 @@ public final class LottosSize {
     private void validateTotalSize(final long totalSize) {
         if (totalSize <= 0) {
             throw new IllegalArgumentException("로또를 구입할 수 없습니다. 로또는 한 장에 " + Lotto.COST + "원 입니다.");
+        }
+    }
+
+    private void validateManualSize(final long manualSize, final long totalSize) {
+        if (manualSize < 0) {
+            throw new IllegalArgumentException("로또 개수는 음수가 될 수 없습니다. 입력한 수동 로또 개수: " + manualSize);
+        }
+
+        if (manualSize > totalSize) {
+            throw new IllegalArgumentException("전체 로또 개수(" + totalSize + "개)보다 수동 로또 개수(" + manualSize + "개)가 더 많습니다.");
         }
     }
 }
