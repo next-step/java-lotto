d0b78d4e48f38dd0becd81a6707f572d0614ec09
dahyen0o <seeunchoi99@gmail.com>
2023-07-16 13:05:19 +0900

    feat: '로또 개수 생성 성공' 기능 구현

--- src/main/java/lotto/model/domain/LottosSize.java
+++ src/main/java/lotto/model/domain/LottosSize.java
@@ -2,7 +2,17 @@ package lotto.model.domain;
 
 public class LottosSize {
 
+    private final long totalSize;
+    private final long manualSize;
+    private final long automaticSize;
+
+    private LottosSize(final long totalSize, final long manualSize) {
+        this.totalSize = totalSize;
+        this.manualSize = manualSize;
+        this.automaticSize = totalSize - manualSize;
+    }
+
     public static LottosSize of(final LottoMoney totalMoney, final long manualSize) {
-        return null;
+        return new LottosSize(totalMoney.size(Lotto.SIZE), manualSize);
     }
 }
