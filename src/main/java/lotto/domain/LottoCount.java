e8842743e58112c1e6578e65992c409ad06afe89
whatasame <whatasame@gmail.com>
2023-07-16 20:00:45 +0900

    fix: equals(), hashcode(), toString() 정의

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import java.util.Objects;
+import java.util.StringJoiner;
+
 import static lotto.domain.Lotto.LOTTO_PRICE;
 
 public class LottoCount {
@@ -23,5 +26,25 @@ public class LottoCount {
 
     public long getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final LottoCount that = (LottoCount) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", LottoCount.class.getSimpleName() + "[", "]")
+                .add("value=" + value)
+                .toString();
     }
 }
