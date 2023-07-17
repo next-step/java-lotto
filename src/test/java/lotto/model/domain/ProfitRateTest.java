85c44fd722cd58235595d61b14eff62d8fe530f6
dahyen0o <seeunchoi99@gmail.com>
2023-07-17 20:46:34 +0900

    test: '수익률이 음수면 에러 발생' 테스트

--- src/test/java/lotto/model/domain/ProfitRateTest.java
+++ src/test/java/lotto/model/domain/ProfitRateTest.java
@@ -10,4 +10,9 @@ class ProfitRateTest {
     void 수익률_객체_생성_성공() {
         assertThatNoException().isThrownBy(() -> ProfitRate.valueOf(0.1));
     }
+
+    @Test
+    void 수익률이_음수면_에러_발생() {
+        assertThatIllegalArgumentException().isThrownBy(() -> ProfitRate.valueOf(-0.1));
+    }
 }
