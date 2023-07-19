439e5d4d3601a8f6ba33522d7084025a561a3a5c
hwihwi <yjh200523@naver.com>
2023-07-19 20:52:59 +0900

    :sparkles: feat: 구매한 로또 병합 테스트 코드 및 프로덕션 코드 작성

--- src/main/java/lotto/domain/BoughtLottos.java
+++ src/main/java/lotto/domain/BoughtLottos.java
@@ -1,16 +1,15 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BoughtLottos {
 
-    private final int count;
     private final List<Lotto> lottos;
 
     public BoughtLottos(final List<Lotto> lottos) {
-        this.count = lottos.size();
-        this.lottos = lottos;
+        this.lottos = new ArrayList<>(lottos);
     }
 
     public LottoResults winningResults(final WinningLotto winningLotto) {
@@ -19,8 +18,16 @@ public class BoughtLottos {
                 .collect(Collectors.toList()));
     }
 
+    public static BoughtLottos merge(final BoughtLottos b1, final BoughtLottos b2) {
+        final BoughtLottos merged = new BoughtLottos(b1.lottos);
+
+        merged.lottos.addAll(b2.lottos);
+
+        return merged;
+    }
+
     public int getCount() {
-        return this.count;
+        return this.lottos.size();
     }
 
     public List<Lotto> getLottos() {
--- src/test/java/lotto/domain/BoughtLottosTest.java
+++ src/test/java/lotto/domain/BoughtLottosTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BoughtLottosTest {
+class BoughtLottosTest {
 
     @Test
     @DisplayName("구입한 로또 정상 생성 입력 테스트")
@@ -32,6 +32,26 @@ public class BoughtLottosTest {
 
         /* when & then */
         assertThat(boughtLottos.getCount()).isEqualTo(lottos.size());
+    }
+
+    @Test
+    @DisplayName("로또 병합 테스트")
+    void boughtLottos_merge() {
+        /* given */
+        List<Lotto> lottos1 = new ArrayList<>();
+        lottos1.add(new Lotto(LottoTest.getBalls("1", "2", "3", "4", "5", "6")));
+        lottos1.add(new Lotto(LottoTest.getBalls("1", "2", "3", "4", "5", "6")));
+        BoughtLottos boughtLottos1 = new BoughtLottos(lottos1);
+
+        List<Lotto> lottos2 = new ArrayList<>();
+        lottos2.add(new Lotto(LottoTest.getBalls("1", "2", "3", "4", "5", "6")));
+        lottos2.add(new Lotto(LottoTest.getBalls("1", "2", "3", "4", "5", "6")));
+        BoughtLottos boughtLottos2 = new BoughtLottos(lottos2);
+
+        BoughtLottos merged = BoughtLottos.merge(boughtLottos1, boughtLottos2);
+
+        /* when & then */
+        assertThat(merged.getCount()).isEqualTo(boughtLottos1.getCount() + boughtLottos2.getCount());
     }
 
     @Test
