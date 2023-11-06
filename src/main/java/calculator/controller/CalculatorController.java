b8492a24750486d71d8daf4ea91d6cd689cc0bea
g0rani <pjw0405@naver.com>
2023-11-06 22:04:34 +0900

    feat: view, controller 작성
    - 입력값과 결과에 대한 view 작성

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,9 +1,30 @@
 package calculator.controller;
 
+import calculator.domain.Operation;
+import calculator.domain.OperationValidator;
+import calculator.domain.StringSplitter;
+import calculator.view.InputView;
+import calculator.view.ResultView;
+import java.util.List;
+import java.util.Scanner;
+
 public class CalculatorController {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = InputView.input(scanner);
 
+        OperationValidator.valid(input);
+        List<String> operations = StringSplitter.splitOperation(input);
+        List<Integer> numbers = StringSplitter.splitNumbers(input);
+
+        int basicNumber = numbers.get(0);
+        for(int i = 0; i < operations.size(); i++) {
+            Operation operation = Operation.searchBy(operations.get(i));
+            basicNumber = operation.calculate(basicNumber, numbers.get(i + 1));
+        }
+        // todo. 결과 수정
+        ResultView.report(basicNumber);
     }
 
 }
---
+++ src/main/java/calculator/view/ResultView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+public class ResultView {
+
+    private ResultView() {
+    }
+
+    public static void report(int input) {
+        System.out.println("계산 결과 : " + input);
+    }
+}
