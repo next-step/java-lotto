057894016be1026a2869943e83868b200fa6cdd8
g0rani <pjw0405@naver.com>
2023-11-06 20:22:33 +0900

    feat: 문자열 입력한 문자와 숫자를 분리 구현
    - Pattern 클레스를 이용하여 정규표현식으로 문자와 숫자 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,22 +1,46 @@
 package calculator;
 
+import java.util.List;
+
 public class StringCalculator {
 
+    private final List<Integer> numbers;
+    private final List<Operation> operations;
+
     public StringCalculator(String input) {
+        validBlank(input);
+        numbers = createNumbers(input);
+
+        validOperation(input);
+        operations = createOperations(input);
+    }
+
+    private void validBlank(String input) {
         if (isBlank(input)) {
             throw new IllegalArgumentException();
         }
+    }
 
-        if (isNotOperation(input)) {
+    private static boolean isBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    private List<Integer> createNumbers(String input) {
+        return null;
+    }
+
+    private void validOperation(String input) {
+        if (!Operation.isOperation(input)) {
             throw new IllegalArgumentException();
         }
     }
 
-    private boolean isNotOperation(String input) {
-        return !Operation.isOperation(input);
+    private List<Operation> createOperations(String input) {
+        return null;
     }
 
-    private boolean isBlank(String input) {
-        return input == null || input.isBlank();
+    public List<Integer> numbers() {
+        return null;
     }
+
 }
