04c1ac6ce7962a823ec67bb7414fd3d1dc676e71
kimgwanghoon <khkimykk@naver.com>
2023-11-07 13:17:58 +0900

    test: 입력 유효성검증 테스트

---
+++ src/test/java/calculator/InputValidationTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputValidationTest {
+
+    private InputValidation inputValidation;
+
+    @BeforeEach
+    public void setUp() {
+        inputValidation = new InputValidation();
+    }
+
+    @DisplayName("입력값 검증 테스트")
+    @Test
+    public void inputValidation() {
+        //given
+        List<String> input = List.of("2", "+", "3", "*", "4", "/", "2");
+        //when
+        boolean result = inputValidation.validation(input);
+        //then
+        assertThat(result).isTrue();
+    }
+
+
+}
