230fed903f8e302d6684c11b01dc21f563f0b833
kimgwanghoon <khkimykk@naver.com>
2023-11-07 15:20:57 +0900

    test: 입력 유효성검증 예외처리 테스트코드

--- src/test/java/calculator/InputValidationTest.java
+++ src/test/java/calculator/InputValidationTest.java
@@ -3,11 +3,16 @@ package calculator;
 import calculator.validation.InputValidation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidationTest {
 
@@ -29,5 +34,50 @@ public class InputValidationTest {
         assertThat(result).isTrue();
     }
 
+    @Nested
+    @DisplayName("예외처리 테스트")
+    class ValidationFaild {
+        @DisplayName("숫자 외 값이 처음 입력되었을 때 예외처리 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"a", "-"})
+        public void 처음_입력값_숫자가_아닌_경우(String value) {
+            //given
+            List<String> input = List.of(value, "+", "3", "*", "4", "/", "2");
+            //when
+            //then
+            assertThatThrownBy(() -> inputValidation.validation(input)).isInstanceOf(IllegalArgumentException.class);
+        }
 
+        @DisplayName("숫자가 아닌 값이 입력되었을 때 예외처리 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"a", "-"})
+        public void 마지막_입력값_숫자가_아닌_경우(String value) {
+            //given
+            List<String> input = List.of("2", "+", "3", "*", "4", "/", value);
+            //when
+            //then
+            assertThatThrownBy(() -> inputValidation.validation(input)).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("연산자가 연속으로 입력되었을 때 예외처리 테스트")
+        @Test
+        public void 연산자가_연속으로_입력된_경우() {
+            //given
+            List<String> input = List.of("2", "++", "3", "*", "4", "/", "2");
+            //when
+            //then
+            assertThatThrownBy(() -> inputValidation.validation(input)).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("null, 빈값이 입력되었을 때 예외처리 테스트")
+        @ParameterizedTest
+        @NullAndEmptySource
+        public void null_빈값이_입력된_경우(String value) {
+            //given
+            List<String> input = List.of(value);
+            //when
+            //then
+            assertThatThrownBy(() -> inputValidation.validation(input)).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
 }
