23085dbbb59edbecdfb3a412151433f2ca04088a
kimgwanghoon <khkimykk@naver.com>
2023-11-07 11:03:42 +0900

    test: 덧셈 구현

---
+++ src/main/java/calculator/Addition.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class Addition {
+
+    public int calculate(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+}
--- src/test/java/calculator/AdditionTest.java
+++ src/test/java/calculator/AdditionTest.java
@@ -1,11 +1,19 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AdditionTest {
+
+    private Addition addition;
+
+    @BeforeEach
+    public void setUp() {
+        addition = new Addition();
+    }
 
     @DisplayName("덧셈 테스트")
     @Test
@@ -14,7 +22,7 @@ public class AdditionTest {
         int firstNumber = 1;
         int secondNumber = 2;
         //when
-        int result = Addition.calculate(firstNumber, secondNumber);
+        int result = addition.calculate(firstNumber, secondNumber);
         //then
         assertThat(result).isEqualTo(3);
     }
