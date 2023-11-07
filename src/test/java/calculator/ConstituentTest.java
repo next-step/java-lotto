46bedede5089e6d3d479dbbb7b1bd75a5b92ad84
KimGwangHoon <khkimykk@naver.com>
2023-11-08 01:30:42 +0900

    test: 입력값 List로 저장하는 도메인 Test작성

---
+++ src/test/java/calculator/ConstituentTest.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConstituentTest {
+
+    @DisplayName("입력값 셋팅")
+    @Test
+    public void inputSetting() {
+        //given
+        List<String> input = List.of("2", "+", "3");
+        //when
+        Constituent constituent = new Constituent(input);
+        //then
+        assertThat(constituent.getNumbers()).isEqualTo(List.of(2, 3));
+        assertThat(constituent.getAccounts()).isEqualTo(List.of("+"));
+    }
+}
