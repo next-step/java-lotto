660539ed83d14bb8a97035a2a04b45755c8553be
kimgwanghoon <khkimykk@naver.com>
2023-11-07 11:01:10 +0900

    test: 덧셈 테스트케이스

--- src/test/java/calculator/AdditionTest.java
+++ src/test/java/calculator/AdditionTest.java
@@ -1,4 +1,21 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AdditionTest {
+
+    @DisplayName("덧셈 테스트")
+    @Test
+    public void add() {
+        //given
+        int firstNumber = 1;
+        int secondNumber = 2;
+        //when
+        int result = Addition.calculate(firstNumber, secondNumber);
+        //then
+        assertThat(result).isEqualTo(3);
+    }
 }
