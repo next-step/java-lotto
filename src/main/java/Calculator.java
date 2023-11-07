9e8d674a9bc779e74d31f286172a3c10eeb83382
sunggyupaik <melon9751@naver.com>
2023-11-08 08:15:48 +0900

    [feat] 사칙연산 계산을 추가하라
    덧셈, 나눗셈, 곱셈, 나눗셈을 하는 기능을 추가합니다

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,21 @@
 public class Calculator {
-	public int add(int left, int right) {
-		return left + right;
+	public int calculation(int left, char c, int right) {
+		if(c == '+') {
+			return left + right;
+		}
+
+		if(c == '-') {
+			return left - right;
+		}
+
+		if(c == '*') {
+			return left * right;
+		}
+
+		if(c == '/') {
+			return left / right;
+		}
+
+		throw new IllegalArgumentException("사칙연산 기호를 입력하세요.");
 	}
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,46 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
 	@Test
 	void add_oneAndTwo_three() {
 		Calculator c = new Calculator();
-		int result = c.add(1,2);
+		int result = c.calculation(1, '+',2);
 
 		assertThat(result).isEqualTo(3);
+	}
+
+	@Test
+	void subtract_towAndOne_one() {
+		Calculator c = new Calculator();
+		int result = c.calculation(2, '-', 1);
+
+		assertThat(result).isEqualTo(1);
+	}
+
+	@Test
+	void multiply_towAndThree_six() {
+		Calculator c = new Calculator();
+		int result = c.calculation(2, '*', 3);
+
+		assertThat(result).isEqualTo(6);
+	}
+
+	@Test
+	void divide_sixAndTwo_Three() {
+		Calculator c = new Calculator();
+		int result = c.calculation(6, '/', 2);
+
+		assertThat(result).isEqualTo(3);
+	}
+
+	@Test
+	void given_leftBracket_throwsException() {
+		Calculator c = new Calculator();
+		assertThatThrownBy(
+				() -> c.calculation(6, '(', 2)
+		).isInstanceOf(IllegalArgumentException.class);
 	}
 }
