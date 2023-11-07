ded53ba9c522d966f4c6b34f1ff2f83240483dc2
KimGwangHoon <khkimykk@naver.com>
2023-11-08 02:05:50 +0900

    test: 입력값 계산하여 결과 반환하는 함수 test 추가

--- src/test/java/calculator/ConstituentTest.java
+++ src/test/java/calculator/ConstituentTest.java
@@ -21,4 +21,16 @@ public class ConstituentTest {
         assertThat(constituent.getNumbers()).isEqualTo(List.of(2, 3));
         assertThat(constituent.getAccounts()).isEqualTo(List.of("+"));
     }
+
+    @DisplayName("입력값 계산")
+    @Test
+    public void 입력값_계산() {
+        //given
+        List<String> input = List.of("2", "+", "3");
+        //when
+        Constituent constituent = new Constituent(input);
+        int result = constituent.StartCounter();
+        //then
+        assertThat(result).isEqualTo(5);
+    }
 }
