c1bdfc40657e0e1f518943e4f7dd066fa0f33c70
jikimee64 <jikimee64@gmail.com>
2023-11-09 00:09:42 +0900

    feat: 사칙연산 기호가 아닌 경우 예외 발생

--- src/main/java/calculator/domain/Input.java
+++ src/main/java/calculator/domain/Input.java
@@ -1,17 +1,45 @@
 package calculator.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Input {
+
+    private static final String REGEX_NUMBER = "[0-9]";
+    private static final String ARITHMETIC = "[+\\-*/]";
+    private static final String BLANK = " ";
+    private static final Pattern ARITHMETIC_PATTERN = Pattern.compile(ARITHMETIC);
 
     private final String text;
 
     public Input(String text) {
         validateEmptyOrNull(text);
+        validateArithmetic(text);
         this.text = text;
     }
 
     public void validateEmptyOrNull(String text) {
-        if(this.text == null || this.text.isBlank()){
+        if (text == null || text.isBlank()) {
             throw new IllegalArgumentException("입력 값이 null이거나 빈 공백 문자일 수 없습니다.");
+        }
+    }
+
+    public void validateArithmetic(String text) {
+        String operators = extractedOperator(text);
+        for (char operator : operators.toCharArray()) {
+            validateOperator(Character.toString(operator));
+        }
+    }
+
+    private static String extractedOperator(String text) {
+        return text.replaceAll(REGEX_NUMBER, "")
+                .replaceAll(BLANK, "");
+    }
+
+    private void validateOperator(String operator) {
+        Matcher matcher = ARITHMETIC_PATTERN.matcher(String.valueOf(operator));
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("사칙연산 기호만 사용 가능합니다.");
         }
     }
 
--- src/test/java/domain/InputTest.java
+++ src/test/java/domain/InputTest.java
@@ -7,19 +7,45 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
 
+    private static final String REGEX_NOT_NUMBER = "[0-9]";
+
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("실패 - 입력 값이 null이거나 빈 공백 문자일 경우 예외가 발생한다.")
-    void fail_input_null_or_empty(String text){
+    void fail_input_null_or_empty(String text) {
         // given & when & then
         Assertions.assertThatThrownBy(() -> new Input(text))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력 값이 null이거나 빈 공백 문자일 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("성공 - 사칙연산 기호만 있는 경우 예외가 발생하지 않는다.")
+    void success_not_contain_or_empty() {
+        // given
+        String text = "2 + 3 * 4 / 2";
+
+        // when & then
+        assertThat(new Input(text)).as("사칙 연산 기호가 모두 존재하면 예외가 발생하지 않는다.");
+    }
+
+    @Test
+    @DisplayName("실패 - 사칙연산 기호가 아닌 경우 예외가 발생한다.")
+    void fail_not_contain_or_empty() {
+        // given
+        String text = "2 + 3 * 4 ? 2";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Input(text))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사칙연산 기호만 사용 가능합니다.");
     }
 
 }
