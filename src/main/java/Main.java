88e2731af148e3c37def540858dc52f371288d65
sunggyupaik <melon9751@naver.com>
2023-11-09 00:36:59 +0900

    [feat] 입력값에 순서에 따른 계산을 구현하라
    사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서를 결정하도록
    구현합니다

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("계산식을 입력하세요.");
 		String inputExpression = scanner.nextLine();
 		if(inputExpression.isBlank()) {
 			throw new IllegalArgumentException("유효한 계산식을 입력해주세요");
 		}
 
 		String[] inputs = inputExpression.split(" ");
+		int result = Integer.parseInt(inputs[0]);
+		Calculator calculator = new Calculator();
+		for (int i = 0; i < inputs.length - 1; i += 2) {
+			result = calculator.calculation(result, inputs[i+1].charAt(0), Integer.parseInt(inputs[i+2]));
+		}
+
+		System.out.printf("계산 결과값은 %d 입니다.", result);
 	}
 }
