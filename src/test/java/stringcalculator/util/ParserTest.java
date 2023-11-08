a77f817f40b024a9e4e38c1a69bdf4ee414dac6c
엄태권 <akfls221@gmail.com>
2023-11-09 04:51:41 +0900

    feat : Parser 추가 및 테스트코드 작성

---
+++ src/test/java/stringcalculator/util/ParserTest.java
@@ -0,0 +1,29 @@
+package stringcalculator.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class ParserTest {
+
+    @Test
+    @DisplayName("문자열 정수를 입력할 경우 해당하는 정수타입을 반환한다.")
+    void parseInteger() {
+        String inputString = "1";
+
+        int result = Parser.parseInteger(inputString);
+
+        assertThat(result).isOne();
+    }
+
+    @Test
+    @DisplayName("문자열이 정수가 아닐경우 정수타입 파싱에 실패한다.")
+    void parseInteger_문자() {
+        String inputString = "가";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> Parser.parseInteger(inputString))
+                .withMessageMatching("숫자 형식이 아닙니다.");
+    }
+}
