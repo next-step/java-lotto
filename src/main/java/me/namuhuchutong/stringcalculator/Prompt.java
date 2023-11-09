26fe4c9415a3e7d6e3e7cb652e1d588b2413328e
namuhuchutong <namuhuchutong@gmail.com>
2023-11-10 02:26:58 +0900

    feat: 사칙연산자, 숫자 이외 필터링 기능 구현

--- src/main/java/me/namuhuchutong/stringcalculator/Prompt.java
+++ src/main/java/me/namuhuchutong/stringcalculator/Prompt.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Prompt {
 
+    private static final String NUMBER_AND_OPERATORS_REGEX = "[0-9/*\\-+\\s]";
+
     private final Scanner scanner = new Scanner(System.in);
 
     public void printMessage(String message) {
@@ -14,12 +16,20 @@ public class Prompt {
         printMessage(message);
         String input = scanner.nextLine();
         validateBlank(input);
+        validateCorrectExpression(input);
         return input.replaceAll(" ", "");
     }
 
     private void validateBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("계산식은 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateCorrectExpression(String input) {
+        String[] split = input.split(NUMBER_AND_OPERATORS_REGEX);
+        if (split.length != 0) {
+            throw new IllegalArgumentException("사칙연산자와 숫자만 입력할 수 있습니다.");
         }
     }
 
--- src/test/java/me/namuhuchutong/stringcalculator/PromptTest.java
+++ src/test/java/me/namuhuchutong/stringcalculator/PromptTest.java
@@ -54,8 +54,6 @@ class PromptTest {
     @ValueSource(strings = {
             "1 2 44 32 2 1",
             "1 * 2 - 3 / 5 + 2",
-            "asdfas adsfas ",
-            "@#$#@ sd 234"
     })
     void given_string_should_remove_white_space(String given) {
         //given
@@ -68,5 +66,23 @@ class PromptTest {
 
         //then
         assertThat(result).isFalse();
+    }
+    
+    @DisplayName("숫자와 사칙연산자 외의 값이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1 ! 2 @ 3 # 4 $ 5 % 6",
+            "1 + 7 - 9 * 1 / 10 @ 3",
+            "a + b, c - d * dwad ( 8",
+            "a + 7 - 9 * 1 / 10 - 3",
+    })
+    void throw_exception_when_expression_is_not_correct(String given) {
+        //given
+        setIn(given);
+        Prompt prompt = new Prompt();
+
+        //when, then
+        assertThatThrownBy(() -> prompt.showStringPrompt(TEST))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
