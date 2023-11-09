419e714385cc39eedfd22b54b7f83980fa74431c
KimGwangHoon <khkimykk@naver.com>
2023-11-09 22:36:17 +0900

    docs: 로또 기능 요구 상세 작성

--- src/main/java/calculator/calcMain.java
+++ src/main/java/calculator/calcMain.java
@@ -1,9 +1,7 @@
 package calculator;
 
 import calculator.domain.Constituent;
-import calculator.factory.AccountFactory;
 import calculator.support.SplitSupport;
-import calculator.validation.InputValidation;
 import calculator.view.InputView;
 import calculator.view.ResultView;
 
@@ -13,7 +11,7 @@ public class calcMain {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        Constituent constituent = new Constituent(new SplitSupport().split(inputView.inputCalculateString()));
+        Constituent constituent = new Constituent(SplitSupport.split(inputView.inputCalculateString()));
 
         ResultView resultView = new ResultView();
         resultView.printResult(constituent.StartCounter());
