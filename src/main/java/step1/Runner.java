0deeabc22dd7865e47be73f9f9ebcbb0a7bf1562
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-09 20:09:48 +0900

    feat: 사칙연산 결과값을 출력할 수 있다

--- src/main/java/step1/Runner.java
+++ src/main/java/step1/Runner.java
@@ -1,21 +1,16 @@
 package step1;
 
 import step1.domain.Calculator;
-import step1.domain.Operator;
 import step1.domain.Seperator;
 import step1.view.InputView;
-
-import java.util.LinkedList;
+import step1.view.ResultView;
 
 public class Runner {
 
     public static void main(String[] args) {
         Seperator seperator = new Seperator(InputView.inputText());
-
-        LinkedList<Integer> numbers = seperator.numbers();
-        LinkedList<Operator> operators = seperator.operators();
-
-        int result = Calculator.calculation(numbers, operators);
+        int result = Calculator.calculation(seperator.numbers(), seperator.operators());
+        ResultView.printResult(result);
     }
 
 }
