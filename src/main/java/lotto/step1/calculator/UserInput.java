aae6f67f4bb6184890bda6c94bbfa52c39c593ed
HyunggiPark <phg9587@naver.com>
2023-11-10 02:00:52 +0900

    refact: 매개변수 명 변경 및 메서드를 더 작게 쪼갠 후, 각 메서드에 맞게 테스트 코드 분리

--- src/main/java/lotto/step1/calculator/UserInput.java
+++ src/main/java/lotto/step1/calculator/UserInput.java
@@ -1,21 +1,36 @@
 package lotto.step1.calculator;
 
 public class UserInput {
-    private final String userInputString;
+    private final String userInput;
 
-    public UserInput(final String userInputString) {
-        verifyUserInputString(userInputString);
+    public UserInput(final String input) {
+        validateUserInputString(input);
 
-        this.userInputString = userInputString;
+        this.userInput = input;
     }
 
     public String[] split() {
-        return userInputString.split(" ");
+        return userInput.split(" ");
     }
 
-    private void verifyUserInputString(final String userInputString) {
-        if (userInputString == null || userInputString.isEmpty()) {
-            throw new IllegalArgumentException("입력은 null 혹은 빈 공백일 수 없습니다.");
+    private void validateUserInputString(final String input) {
+        validateNullAndEmpty(input);
+    }
+
+    private void validateNullAndEmpty(final String input) {
+        validateNull(input);
+        validateEmpty(input);
+    }
+
+    private void validateEmpty(final String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력은 빈 공백이 될 수 없습니다.");
+        }
+    }
+
+    private void validateNull(final String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("입력은 null이 될 수 없습니다.");
         }
     }
 }
--- src/test/java/lotto/step1/calculator/UserInputTest.java
+++ src/test/java/lotto/step1/calculator/UserInputTest.java
@@ -2,9 +2,7 @@ package lotto.step1.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
@@ -15,23 +13,33 @@ class UserInputTest {
     @ParameterizedTest
     @MethodSource("userInputStringWithSpace")
     @DisplayName("숫자와 사칙 연산 사이를 빈 공백으로 구분한 문자열이 입력되면, 해당 문자열을 숫자와 사칙 연산으로 분리한다.")
-    void splitUserInputStringWithSpace(String userInputString, String[] expectedStrings) {
+    void splitUserInputStringWithSpace(String input, String[] expectedStrings) {
         //given
         //when
-        UserInput userInput = new UserInput(userInputString);
+        UserInput userInput = new UserInput(input);
 
         //then
         assertThat(userInput.split()).isEqualTo(expectedStrings);
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
-    @DisplayName("입력 값이 null이거나 빈 공백 문자일 경우, IllegalArgumentException 예외가 발생한다.")
-    void splitUserInputStringWithNullAndEmptyString(String userInputString) {
+    @NullSource
+    @DisplayName("입력 값이 null일 경우, IllegalArgumentException 예외가 발생한다.")
+    void splitUserInputStringWithNull(String input) {
         //given, when, then
-        assertThatThrownBy(() -> new UserInput(userInputString))
+        assertThatThrownBy(() -> new UserInput(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력은 null 혹은 빈 공백일 수 없습니다.");
+                .hasMessage("입력은 null이 될 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @DisplayName("입력 값이 빈 공백 문자일 경우, IllegalArgumentException 예외가 발생한다.")
+    void splitUserInputStringWithEmptyString(String input) {
+        //given, when, then
+        assertThatThrownBy(() -> new UserInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력은 빈 공백이 될 수 없습니다.");
     }
 
     public static Stream<Arguments> userInputStringWithSpace() {
