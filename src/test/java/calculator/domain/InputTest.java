c7ffe3579433912c7bc645461174ba69e33e298c
jikimee64 <jikimee64@gmail.com>
2023-11-09 22:23:56 +0900

    chore: 패키지 이동

--- src/test/java/calculator/domain/InputTest.java
+++ src/test/java/calculator/domain/InputTest.java
@@ -1,20 +1,14 @@
-package domain;
+package calculator.domain;
 
-import calculator.domain.Input;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
-
-    private static final String REGEX_NOT_NUMBER = "[0-9]";
 
     @ParameterizedTest
     @NullAndEmptySource
