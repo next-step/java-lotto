1c777cc879f0d2531ffd109c6f92e36328bee575
KimGwangHoon <khkimykk@naver.com>
2023-11-10 02:09:15 +0900

    test: 결제금액 테스트

---
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -0,0 +1,54 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PurchaseTest {
+
+    private Purchase purchase;
+
+    void setUp(int amount) {
+        purchase = new Purchase(amount);
+    }
+
+    @Test
+    void 로또_구매_금액_유효성_검사_성공() {
+        // given
+        setUp(1000);
+        // when
+        int amount = purchase.amount();
+        // then
+        assertThat(amount).isEqualTo(1000);
+    }
+
+    @DisplayName("로또 구입 실패 금액 오류")
+    @Nested
+    class 구입_실패 {
+
+        @DisplayName("1000원 미만일 경우")
+        @Test
+        void 금액이_작은_경우() {
+            // given
+            // when
+            // then
+            assertThatIllegalArgumentException().isThrownBy(() -> {
+                setUp(999);
+            }).withMessageMatching("로또 구입 금액은 1000원 이상 입력해야 합니다.");
+        }
+
+        @DisplayName("1000원 단위가 아닌경우")
+        @Test
+        void 단위가_1000원_아닌경우() {
+            // given
+            // when
+            // then
+            assertThatIllegalArgumentException().isThrownBy(() -> {
+                setUp(1001);
+            }).withMessageMatching("로또 구입 금액은 1000원 단위로 가능합니다.");
+        }
+    }
+}
