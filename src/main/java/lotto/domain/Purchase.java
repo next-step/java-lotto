d443f1dff1282d416aa7af0ca4b9bb95e2994a5c
KimGwangHoon <khkimykk@naver.com>
2023-11-10 02:09:35 +0900

    feat: 결제금액 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,2 +1,17 @@
-package lotto.domain;public class Purchase {
+package lotto.domain;
+
+import static lotto.validate.InputValidation.inputValidate;
+
+public class Purchase {
+    private final int amount;
+
+    public Purchase(int amount) {
+        inputValidate(amount);
+        this.amount = amount;
+    }
+
+    public int amount() {
+        return amount;
+    }
+
 }
