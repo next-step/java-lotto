fbd7dfcdd7f32122f5fa85d097b74916477e758a
Jinhong Kim <deepred.kim@gmail.com>
2023-11-10 09:25:19 +0900

    feat: 하나의 덧셈 식을 계산한다

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -2,11 +2,21 @@ package camp.nextstep.edu.calculator;
 
 public class StringCalculator {
 
-    public static int calc(String input) {
-        if (input == null || input.isBlank()) {
+    public static int calculate(String strExpression) {
+        if (isEmpty(strExpression)) {
             throw new IllegalArgumentException("공백이나 null이 올 수 없습니다");
         }
+        String[] expression = strExpression.split(" ");
+        return calculate(expression);
+    }
 
-        return 0;
+    private static int calculate(String[] oneExpression) {
+        int left = Integer.parseInt(oneExpression[0]);
+        int right = Integer.parseInt(oneExpression[2]);
+        return left + right;
+    }
+
+    private static boolean isEmpty(String input) {
+        return input == null || input.isBlank();
     }
 }
--- src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
+++ src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
@@ -1,8 +1,11 @@
 package camp.nextstep.edu.calculator;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -11,7 +14,14 @@ public class StringCalculatorTest {
     @NullAndEmptySource
     @ParameterizedTest
     void nullOrEmpty(String input) {
-        assertThatThrownBy(() -> StringCalculator.calc(input))
+        assertThatThrownBy(() -> StringCalculator.calculate(input))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("하나의 덧셈 식을 계산한다")
+    void onePlus() {
+        int result = StringCalculator.calculate("1 + 2");
+        assertThat(result).isEqualTo(3);
     }
 }
