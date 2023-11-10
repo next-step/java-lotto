f85818e988b4e46ec1b4b85d648739e8644a7afd
HyunggiPark <phg9587@naver.com>
2023-11-10 11:46:30 +0900

    refact: Test의 메소드명을 조금 더 이해하기 쉽도록 수정

--- src/test/java/lotto/step1/calculator/UserInputTest.java
+++ src/test/java/lotto/step1/calculator/UserInputTest.java
@@ -11,9 +11,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UserInputTest {
     @ParameterizedTest
-    @MethodSource("userInputStringWithSpace")
+    @MethodSource("userInputWithWhiteSpace")
     @DisplayName("숫자와 사칙 연산 사이를 빈 공백으로 구분한 문자열이 입력되면, 해당 문자열을 숫자와 사칙 연산으로 분리한다.")
-    void splitUserInputStringWithSpace(String input, String[] expectedStrings) {
+    void splitUserInputWithWhiteSpace(String input, String[] expectedStrings) {
         //given
         //when
         UserInput userInput = new UserInput(input);
@@ -22,7 +22,7 @@ class UserInputTest {
         assertThat(userInput.getInputs()).isEqualTo(expectedStrings);
     }
 
-    public static Stream<Arguments> userInputStringWithSpace() {
+    public static Stream<Arguments> userInputWithWhiteSpace() {
         return Stream.of(
                 Arguments.of("1 + 2", new String[]{"1", "+", "2"}),
                 Arguments.of("2 - 4", new String[]{"2", "-", "4"}),
