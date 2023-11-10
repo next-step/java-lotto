88792278785b836af7c1a0226cf714845aea1d8d
beyondorder <youofsea@gmail.com>
2023-11-10 17:59:20 +0900

    숫자와 사칙연산 기호 이외의 문자가 포함된 경우 IllegalException 예외 발생

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -1,17 +1,57 @@
 package step1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
-        private String input;
+    private final String input;
+
+    private List<Integer> numbers;
+    private List<String> operators;
+
+    private static final String NUMBER_REGEX = "\\d+";
+    private static final String OPERATOR_REGEX = "[+-/*]";
+    private static final String EXCEPT_NUMBER_OPERATOR_WHITESPACE_REGEX = "[^\\d-+*/\\s]";
 
     public StringCalculator(String input) {
         this.input = input;
         inputCheck(input);
+        this.numbers = numbers(input);
+        this.operators = operators(input);
+    }
+
+    private List<String> operators(String input) {
+        Pattern pattern = Pattern.compile(OPERATOR_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        List<String> operators = new ArrayList<>();
+        while(matcher.find()) {
+            operators.add(matcher.group());
+        }
+        return operators;
+    }
+
+    private List<Integer> numbers(String input) {
+        Pattern pattern = Pattern.compile(NUMBER_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        List<Integer> result = new ArrayList<>();
+        while(matcher.find()) {
+            result.add(Integer.parseInt(matcher.group()));
+        }
+        return result;
     }
 
     private void inputCheck(String input) {
         if(nullOrWhiteSpaceInput(input)){
             throw new IllegalArgumentException("입력 문자열은 null 또는 빈 문자열이어서는 안됩니다.");
+        }
+
+        Pattern pattern = Pattern.compile(EXCEPT_NUMBER_OPERATOR_WHITESPACE_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.find()) {
+            throw new IllegalArgumentException("입력 문자열에 숫자와 사칙연산 기호 이외의 문자가 포함되어 있습니다.");
         }
     }
 
--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -2,12 +2,15 @@ package step1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
+    private final String COMMON_INPUT = "2 + 3 * 4 / 2";
     @Test
     @DisplayName("문자열 계산기 생성")
     void create() {
@@ -15,38 +18,50 @@ public class StringCalculatorTest {
         assertThat(stringCalculator).isInstanceOf(StringCalculator.class);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 IllegalException 예외 발생")
-    void add() {
-        assertThatThrownBy(() -> new StringCalculator(""))
+    void add(String input) {
+        assertThatThrownBy(() -> new StringCalculator(null))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new StringCalculator(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자와 사칙연산 기호 이외의 문자가 포함된 경우 IllegalException 예외 발생")
+    void inputCheck() {
+        assertThatThrownBy(() -> new StringCalculator("13,2,3,4,5,6,7,8,9,0"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("덧셈")
     void add2() {
-        StringCalculator stringCalculator = new StringCalculator("1,2");
+        StringCalculator stringCalculator = new StringCalculator(COMMON_INPUT);
         assertThat(stringCalculator.add(1, 2)).isEqualTo(3);
     }
 
     @Test
     @DisplayName("뺄셈")
     void subtract() {
-        StringCalculator stringCalculator = new StringCalculator("1,2");
+        StringCalculator stringCalculator = new StringCalculator(COMMON_INPUT);
         assertThat(stringCalculator.subtract(1, 2)).isEqualTo(-1);
     }
 
     @Test
     @DisplayName("곱셈")
     void multiply() {
-        StringCalculator stringCalculator = new StringCalculator("1,2");
+        StringCalculator stringCalculator = new StringCalculator(COMMON_INPUT);
         assertThat(stringCalculator.multiply(1, 2)).isEqualTo(2);
     }
 
     @Test
     @DisplayName("나눗셈")
     void divide() {
-        StringCalculator stringCalculator = new StringCalculator("1,2");
+        StringCalculator stringCalculator = new StringCalculator(COMMON_INPUT);
         assertThat(stringCalculator.divide(1, 2)).isEqualTo(0);
     }
+
 }
