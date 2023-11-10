e1b213ced058ecc2b0e0684b804c124ad36376ad
g0rani <pjw0405@naver.com>
2023-11-10 20:20:53 +0900

    test: 로또 구입 금액에 대한 테스트 작성
    - 나눗셈 테스트 작성
    - 구매 가능한 로또 수량 테스트 작성

--- src/test/java/lotto/domain/BuyingAmountTest.java
+++ src/test/java/lotto/domain/BuyingAmountTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,6 +16,26 @@ class BuyingAmountTest {
     @Test
     void 로또번호를_1000원_단위로_살_수_없다() {
         assertThrows(IllegalArgumentException.class, () -> new BuyingAmount(14500));
+    }
+
+    @Test
+    void 나눗셈이_가능하다() {
+        BuyingAmount buyingAmount = new BuyingAmount(1000);
+
+        double actual = buyingAmount.divide(14000);
+        double expected = 14;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 구매_가능한_로또_수를_알_수_있다() {
+        BuyingAmount buyingAmount = new BuyingAmount(14000);
+
+        int actual = buyingAmount.units();
+        int expected = 14;
+
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
