02111968de91147e33705883c8c805b1a1831314
Jinhong Kim <deepred.kim@gmail.com>
2023-11-10 09:49:42 +0900

    refactor: 리터럴 숫자 상수로 선언

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -2,6 +2,12 @@ package camp.nextstep.edu.calculator;
 
 public class StringCalculator {
 
+    private static final int FIRST_LEFT_IDX = 0;
+    private static final int FIRST_OPERATOR_IDX = 1;
+    private static final int FIRST_RIGHT_IDX = 2;
+    private static final int SECOND_OPERATOR_IDX = 3;
+    private  static final int NEXT_OPERATOR_GAP = 2;
+
     public static int calculate(String strExpressions) {
         if (isEmpty(strExpressions)) {
             throw new IllegalArgumentException("공백이나 null이 올 수 없습니다");
@@ -11,8 +17,12 @@ public class StringCalculator {
     }
 
     private static int calculateAll(String[] expressions) {
-        int result = calculate(expressions[0], expressions[1], expressions[2]);
-        for (int i = 3; i < expressions.length; i += 2) {
+        int result = calculate(expressions[FIRST_LEFT_IDX],
+            expressions[FIRST_OPERATOR_IDX],
+            expressions[FIRST_RIGHT_IDX]
+        );
+
+        for (int i = SECOND_OPERATOR_IDX; i < expressions.length; i += NEXT_OPERATOR_GAP) {
             result = calculate(String.valueOf(result), expressions[i], expressions[i+1]);
         }
         return result;
@@ -46,7 +56,7 @@ public class StringCalculator {
         }
         return left / right;
     }
-    
+
     private static boolean isEmpty(String input) {
         return input == null || input.isBlank();
     }
