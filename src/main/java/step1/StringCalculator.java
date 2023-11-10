aabe2a1f1577cba6dc5f92076d5f7753c2e654b1
beyondorder <youofsea@gmail.com>
2023-11-10 17:22:47 +0900

    곱셈, 나눗셈

--- src/main/java/step1/StringCalculator.java
+++ src/main/java/step1/StringCalculator.java
@@ -26,4 +26,12 @@ public class StringCalculator {
     public int subtract(int first, int second) {
         return first - second;
     }
+
+    public int multiply(int first, int second) {
+        return first * second;
+    }
+
+    public int divide(int first, int second) {
+        return first / second;
+    }
 }
--- src/test/java/step1/StringCalculatorTest.java
+++ src/test/java/step1/StringCalculatorTest.java
@@ -18,7 +18,7 @@ public class StringCalculatorTest {
     @Test
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 IllegalException 예외 발생")
     void add() {
-        assertThatThrownBy(()-> new StringCalculator(""))
+        assertThatThrownBy(() -> new StringCalculator(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -26,14 +26,27 @@ public class StringCalculatorTest {
     @DisplayName("덧셈")
     void add2() {
         StringCalculator stringCalculator = new StringCalculator("1,2");
-        assertThat(stringCalculator.add(1,2)).isEqualTo(3);
+        assertThat(stringCalculator.add(1, 2)).isEqualTo(3);
     }
 
     @Test
     @DisplayName("뺄셈")
     void subtract() {
         StringCalculator stringCalculator = new StringCalculator("1,2");
-        assertThat(stringCalculator.subtract(1,2)).isEqualTo(-1);
+        assertThat(stringCalculator.subtract(1, 2)).isEqualTo(-1);
     }
 
+    @Test
+    @DisplayName("곱셈")
+    void multiply() {
+        StringCalculator stringCalculator = new StringCalculator("1,2");
+        assertThat(stringCalculator.multiply(1, 2)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("나눗셈")
+    void divide() {
+        StringCalculator stringCalculator = new StringCalculator("1,2");
+        assertThat(stringCalculator.divide(1, 2)).isEqualTo(0);
+    }
 }
