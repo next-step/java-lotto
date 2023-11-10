324ffb86dcc9229774f6fc9c2180f8a4d411a86b
kimgwanghoon <khkimykk@naver.com>
2023-11-10 10:33:15 +0900

    feat: enum 당첨 갯수,금액 구현

--- src/main/java/lotto/domain/RankLotto.java
+++ src/main/java/lotto/domain/RankLotto.java
@@ -1,2 +1,40 @@
-package lotto.domain;public class RankLotto {
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum RankLotto {
+
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    MISS(0, 0);
+
+    private final int matchCount;
+    private final int winningMoney;
+
+    RankLotto(int matchCount, int winningMoney) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+    }
+
+    public static RankLotto findRank(int matchCount) {
+        return Arrays.stream(RankLotto.values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    int matchCount() {
+        return matchCount;
+    }
+
+    int winningMoney() {
+        return winningMoney;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d개 일치 (%d원)", matchCount, winningMoney);
+    }
 }
