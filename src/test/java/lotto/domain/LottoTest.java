a5abd41c2a2b83df52b7c4ff970113b2ba625a13
kimgwanghoon <khkimykk@naver.com>
2023-11-10 16:13:04 +0900

    test: 로또 번호 비교

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,6 +5,8 @@ import lotto.strategy.NumberStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
@@ -29,5 +31,25 @@ public class LottoTest {
         Lotto lotto = new Lotto(numberStrategy.create(1).get(0).lottoNumbers());
         // then
         assertThat(lotto.toString()).isEqualTo("[2, 3, 5, 6, 10, 40]");
+    }
+
+    @DisplayName("로또 번호 비교")
+    @Test
+    void 로또_번호_비교() {
+        // given
+        NumberStrategy numberStrategy = new FixtureNumberStrategy();
+        Lotto lotto = new Lotto(numberStrategy.create(1).get(0).lottoNumbers());
+        Lotto lottoNumber = new Lotto(List.of(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)
+        ));
+        // when
+        int result = lotto.match(lottoNumber);
+        // then
+        assertThat(result).isEqualTo(4);
     }
 }
