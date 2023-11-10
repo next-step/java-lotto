ecabfbb5c701cd482014b777c2952989a7829a25
kimgwanghoon <khkimykk@naver.com>
2023-11-10 15:50:32 +0900

    refactor: 금액 만큼 로또 생성 구현

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,22 +1,37 @@
 package lotto.domain;
 
+import lotto.strategy.NumberStrategy;
+import lotto.strategy.RandomNumberStrategy;
+
+import java.util.List;
+
 import static lotto.validate.InputValidation.inputValidate;
 
 public class Purchase {
 
     private static final int LOTTO_AMOUNT = 1000;
     private final int amount;
+    private final List<Lotto> lottos;
 
     public Purchase(int amount) {
+        this(amount, new RandomNumberStrategy());
+    }
+
+    public Purchase(int amount, NumberStrategy numberStrategy) {
         inputValidate(amount);
         this.amount = amount;
+        this.lottos = numberStrategy.create(lottoCount());
     }
 
     public int amount() {
         return amount;
     }
 
-    public int lottoCount() {
+    public List<Lotto> lottos() {
+        return lottos;
+    }
+
+    private int lottoCount() {
         return amount / LOTTO_AMOUNT;
     }
 
