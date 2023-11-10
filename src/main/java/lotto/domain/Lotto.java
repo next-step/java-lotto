6be6d3b92cbc5fb9ae0a8ca7f50b1c687afce653
g0rani <pjw0405@naver.com>
2023-11-10 20:55:07 +0900

    refactor: 로또 자료구조 set으로 변경
    - 중복된 번호 허용을 막기 위한 자료 구조 변경
    - 자료구조에 따라 검증 클래스 이동과 삭제

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,16 +1,29 @@
 package lotto.domain;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<LottoNumber> lottoNumbers;
+    public static final int DEFAULT_QUANTITY = 6;
+    private final Set<LottoNumber> lottoNumbers;
 
     public Lotto(List<Integer> numbers) {
-        this.lottoNumbers = new ArrayList<>();
+        this.lottoNumbers = new HashSet<>();
         for(int number : numbers) {
             lottoNumbers.add(new LottoNumber(number));
         }
+        validateSize(lottoNumbers);
+    }
+
+    private static void validateSize(Set<LottoNumber> numbers) {
+        if (isSizeNotEqualsSix(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isSizeNotEqualsSix(Set<LottoNumber> numbers) {
+        return numbers == null || numbers.size() != DEFAULT_QUANTITY;
     }
 
     public boolean contains(LottoNumber lottoNumber) {
