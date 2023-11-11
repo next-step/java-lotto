0f1c3892cda0e97203ea7e7f78031b9bc2450ddb
g0rani <pjw0405@naver.com>
2023-11-11 13:24:04 +0900

    refacotr: 통화 단위 타입 변경
    - 소숫점 계산표기를 위한 BicDecimal 타입으로 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
+import java.math.BigDecimal;
+
 public enum LottoRank {
 
-    NOT_MATCHED("", 0, new LottoPrize(0, Currency.KRW)),
-    FOURTH("3개 일치", 3, new LottoPrize(5000, Currency.KRW)),
-    THIRD("4개 일치", 4, new LottoPrize(50000, Currency.KRW)),
-    SECOND("5개 일치", 5, new LottoPrize(1500000, Currency.KRW)),
-    FIRST("6개 일치", 6, new LottoPrize(2000000000, Currency.KRW));
+    NOT_MATCHED("", 0, new LottoPrize(BigDecimal.ZERO, Currency.KRW)),
+    FOURTH("3개 일치", 3, new LottoPrize(BigDecimal.valueOf(5000), Currency.KRW)),
+    THIRD("4개 일치", 4, new LottoPrize(BigDecimal.valueOf(50000), Currency.KRW)),
+    SECOND("5개 일치", 5, new LottoPrize(BigDecimal.valueOf(1500000), Currency.KRW)),
+    FIRST("6개 일치", 6, new LottoPrize(BigDecimal.valueOf(2000000000), Currency.KRW));
 
     private final String description;
     private final int matchedCount;
