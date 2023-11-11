17cf5a2fb780892fe1227139b0d0c5f385e7bb11
엄태권 <akfls221@gmail.com>
2023-11-12 04:42:53 +0900

    feat : Lotto 생성 및 테스트코드 작성

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,63 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+
+        this.lottoNumbers = numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
+    public int matchCount(Lotto otherLotto) {
+        return (int) getIntegerLottoNumbers().stream()
+                .filter(otherLotto::isMatchNumber)
+                .count();
+    }
+
+    private boolean isMatchNumber(int number) {
+        return getIntegerLottoNumbers().contains(number);
+    }
+
+    private List<Integer> getIntegerLottoNumbers() {
+        return this.lottoNumbers.stream()
+                .map(LottoNumber::getLottoNumber)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+
+        if (uniqueNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또의 숫자는 중복없이 6개여야 합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getIntegerLottoNumbers().toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(lottoNumbers, lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoNumbers);
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,61 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoTest {
+
+    @Test
+    @DisplayName("허용범위의 숫자(중복이 없고 / 1 ~ 45 사이 / 6자리)로 Lotto를 생성시 정상 생성된다.")
+    void createLotto() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(new Lotto(numbers)).isEqualTo(new Lotto(numbers));
+    }
+
+    @Test
+    @DisplayName("Lotto 객체를 다른 객체와 비교하여 동일한 숫자가 존재하는 개수 만큼 숫자를 반환한다.")
+    void matchCount() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Lotto baseLotto = new Lotto(numbers);
+
+        int matchCount = baseLotto.matchCount(new Lotto(numbers));
+
+        assertThat(matchCount).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("중복된 숫자로 Lotto를 생성시 오류가 발생한다.")
+    void createLotto_중복숫자() {
+        List<Integer> numbers = Arrays.asList(1, 1, 3, 4, 5, 6);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(numbers))
+                .withMessage("로또의 숫자는 중복없이 6개여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("6자리 미만의 숫자를 입력하여 Lotto를 생성시 오류가 발생한다.")
+    void createLotto_6자리미만() {
+        List<Integer> numbers = Arrays.asList(1, 1, 3, 4, 5);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(numbers))
+                .withMessage("로또의 숫자는 중복없이 6개여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("1 ~ 45 자리 외의 숫자로 Lotto를 생성시 오류가 발생한다.")
+    void createLotto_범위초과() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 66);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(numbers))
+                .withMessage("로또의 번호 범위를 벗어났습니다.");
+    }
+}
