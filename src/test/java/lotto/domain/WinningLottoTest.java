4097532a71216b7193fc27131b0d86f5872d3acf
g0rani <pjw0405@naver.com>
2023-11-11 23:44:31 +0900

    test: 로또 등수 구하기 test 추가 작성
    - 모든 경우에 대한 작성

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,21 +1,40 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WinningLottoTest {
 
-    @Test
-    void 당첨로또번호로_구입로또_등수를_알_수_있다() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+    static Stream<Arguments> 로또_매개변수() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), LottoRank.FIRST),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 45)), LottoRank.SECOND),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 5, 12)), LottoRank.THIRD),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 4, 11, 12)), LottoRank.FOURTH),
+                Arguments.of(new Lotto(List.of(1, 2, 3, 9, 11, 12)), LottoRank.FIFTH),
+                Arguments.of(new Lotto(List.of(1, 2, 9, 10, 11, 12)), LottoRank.NOT_MATCHED),
+                Arguments.of(new Lotto(List.of(1, 8, 9, 10, 11, 12)), LottoRank.NOT_MATCHED),
+                Arguments.of(new Lotto(List.of(7, 8, 9, 10, 11, 12)), LottoRank.NOT_MATCHED)
+                // 다른 테스트 케이스도 추가할 수 있습니다.
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("로또_매개변수")
+    void 당첨로또번호로_구입로또_등수를_알_수_있다(Lotto lotto,
+                                 LottoRank lottoRank) {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 45);
 
         List<LottoRank> actual = winningLotto.winningRank(List.of(lotto));
-        List<LottoRank> expected = List.of(LottoRank.FIRST);
+        List<LottoRank> expected = List.of(lottoRank);
 
         assertThat(actual).isEqualTo(expected);
     }
