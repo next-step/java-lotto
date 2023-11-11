2e5e7daef13155d96bbb5e2bb17d57853cbe491e
solchan98 <dev.sol0427@gmail.com>
2023-11-11 13:53:08 +0900

    feat: 문자열 사칙연산 수행 기능 구현 및 테스트 성공

---
+++ src/main/java/src/Operator.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public enum Operator {
+    PLUS("+", Integer::sum),
+    MINUS("-", (num1, num2) -> num1 - num2),
+    DIVIDE("/", (num1, num2) -> num1 / num2),
+    MULTIPLE("*", (num1, num2) -> num1 * num2);
+
+    private final String operation;
+    private final BinaryOperator<Integer> expression;
+
+    Operator(String operation, BinaryOperator<Integer> expression) {
+        this.operation = operation;
+        this.expression = expression;
+    }
+
+    public static Operator valueByOperation(String operation) {
+        return Arrays.stream(values()).filter(value -> value.operation.equals(operation))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public int operate(int num1, int num2) {
+        return expression.apply(num1, num2);
+    }
+}
--- src/main/java/src/StringCalculator.java
+++ src/main/java/src/StringCalculator.java
@@ -1,8 +1,20 @@
 package src;
 
+import java.util.*;
+
 public class StringCalculator {
 
     public int calculate(String value) {
-        return 0;
+        Deque<String> chars = new LinkedList<>(Arrays.asList(value.split(" ")));
+
+        int result = Integer.parseInt(chars.pop());
+
+        while(!chars.isEmpty()) {
+            Operator operator = Operator.valueByOperation(chars.pop());
+
+            result = operator.operate(result, Integer.parseInt(chars.pop()));
+        }
+
+        return result;
     }
 }
