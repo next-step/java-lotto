8f746406330a52abe3c04782edb9950c9f85195c
HyunggiPark <phg9587@naver.com>
2023-11-11 16:20:32 +0900

    feat: 프로그램을 실행시킬 Main 클래스와 Lotto프로그램을 전반적으로 관리할 LottoProgram 클래스 생성

---
+++ src/main/java/lotto/step2/util/LottoProgram.java
@@ -0,0 +1,10 @@
+package lotto.step2.util;
+
+import lotto.step2.input.InputView;
+import lotto.step2.input.UserInput;
+
+public class LottoProgram {
+    public static void start() {
+        UserInput input = InputView.input();
+    }
+}
