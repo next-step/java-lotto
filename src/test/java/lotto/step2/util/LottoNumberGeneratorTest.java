8fb7cf8670e9669fdf8ecdc9593acf48deaec7dd
HyunggiPark <phg9587@naver.com>
2023-11-11 21:52:45 +0900

    refact: 테스트명을 이해하기 쉽게 수정 및 테스트에 반복을 추가

--- src/test/java/lotto/step2/util/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/step2/util/LottoNumberGeneratorTest.java
@@ -2,16 +2,17 @@ package lotto.step2.util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoNumberGeneratorTest {
-    @Test
+    @RepeatedTest(3)
     @DisplayName("로또 번호를 만들면, 1이상 45이하의 중복되지 않은 6개의 수를 반환한다.")
-    void generateLottoNumbers() {
+    void testLottoHasUniqueSixNumbersWhichAre1to45() {
         //given
         //when
         Set<Integer> lottoNumbers = LottoNumberGenerator.generateLottoNumbers();
