11ddc54e97b2b6f3b40c68af9464222558bfff5f
KimGwangHoon <khkimykk@naver.com>
2023-11-12 21:56:58 +0900

    style: 통계 출력문구 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -8,5 +8,8 @@ public class ResultView {
 
     public static void resultPrint(double rateOfReturn) {
         print(String.format("총 수익률은 %.2f 입니다.", rateOfReturn));
+        if(rateOfReturn < 1 ) {
+            print("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        }
     }
 }
