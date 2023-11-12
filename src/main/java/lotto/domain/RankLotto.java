5f8bf36c9c65e0e88e07b621ee2b6b895ee34dbe
KimGwangHoon <khkimykk@naver.com>
2023-11-12 21:58:38 +0900

    fix: 2등 당첨금 수정

--- src/main/java/lotto/domain/RankLotto.java
+++ src/main/java/lotto/domain/RankLotto.java
@@ -6,7 +6,7 @@ import java.util.function.BiFunction;
 public enum RankLotto {
 
     FIRST(6, 2_000_000_000, (matchCount, matchBonus) -> matchCount == 6),
-    SECOND(5, 1_500_000 , (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
+    SECOND(5, 30_000_000 , (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
     THIRD(5, 1_500_000 , (matchCount, matchBonus) -> matchCount == 5 && !matchBonus),
     FOURTH(4, 50_000, (matchCount, matchBonus) -> matchCount == 4),
     FIFTH(3, 5_000, (matchCount, matchBonus) -> matchCount == 3),
@@ -39,6 +39,9 @@ public enum RankLotto {
 
     @Override
     public String toString() {
-        return String.format("%d개 일치 (%d원)", matchCount, winningMoney);
+        if (this != SECOND) {
+            return String.format("%d개 일치 (%d원)", matchCount, winningMoney);
+        }
+        return String.format("%d개 일치, 보너스 볼 일치 (%d원)", matchCount, winningMoney);
     }
 }
