ddd731e332f90efc0d3d4715370a6cd6122884f7
yumble <hnjn1101@gmail.com>
2023-11-12 23:32:29 +0900

    feat: InputParser 함수 추가
    - parseFromTokenToNumber : 사용자입력을 분리한 리스트를 받아
      숫자리스트로 변환하는 함수
    - parseFromTokenToOperator : 사용자입력을 분리한 리스트를 받아
      Operator로 변환하는 함수

--- src/main/java/step1/InputParser.java
+++ src/main/java/step1/InputParser.java
@@ -1,10 +1,15 @@
 package step1;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputParser {
 
     private static final String DELIMITER = " ";
+    private static final int NUMBER_START_INDEX = 0;
+    private static final int OPERATOR_START_INDEX = 1;
+    private static final int INDEX_INCREMENT = 2;
 
     public List<String> splitUserInput(String userInput) {
         validateUserInput(userInput);
@@ -16,4 +21,21 @@ public class InputParser {
             throw new IllegalArgumentException("입력 값이 null이거나 빈 공백 문자열입니다.");
         }
     }
+
+    public List<Integer> parseFromTokenToNumber(List<String> tokens) {
+        List<Integer> numbers = new LinkedList<>();
+        for (int i = NUMBER_START_INDEX; i < tokens.size(); i = i + INDEX_INCREMENT) {
+            numbers.add(Integer.parseInt(tokens.get(i)));
+        }
+        return numbers;
+    }
+
+    public List<Operator> parseFromTokenToOperator(List<String> tokens) {
+        List<Operator> operators = new LinkedList<>();
+        for (int i = OPERATOR_START_INDEX; i < tokens.size(); i = i + INDEX_INCREMENT) {
+            operators.add(Operator.findOperatorByUserInput(tokens.get(i).charAt(0))); //Operator의 필드를 String으로 바꿀지, 기존로직을 이용할지 고민
+        }
+        return operators;
+    }
+
 }
--- src/test/java/step1/InputParserTest.java
+++ src/test/java/step1/InputParserTest.java
@@ -1,7 +1,10 @@
 package step1;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,5 +26,45 @@ public class InputParserTest {
     public void 빈_문자열_입력_시_에러_반환_테스트(String userInput) {
         assertThatThrownBy(() -> inputParser.splitUserInput(userInput))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 숫자_리스트_반환_테스트() {
+        List<String> tokens = makeTokens();
+
+        List<Integer> numbers = inputParser.parseFromTokenToNumber(tokens);
+
+        assertThat(numbers).hasSize(3);
+        assertThat(numbers).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    public void 연산기호_리스트_정상_반환_테스트() {
+        List<String> tokens = makeTokens();
+
+        List<Operator> operators = inputParser.parseFromTokenToOperator(tokens);
+
+        assertThat(operators).hasSize(2);
+        assertThat(operators).containsExactly(Operator.PLUS, Operator.MULTIPLICATION);
+    }
+
+    @Test
+    public void 연산기호가_아닌_다른_문자_입력_시_에러_반환_테스트() {
+        List<String> tokens = makeTokens();
+        tokens.add("a");
+        tokens.add("3");
+
+        assertThatThrownBy(() -> inputParser.parseFromTokenToOperator(tokens))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static List<String> makeTokens() {
+        List<String> tokens = new ArrayList<>();
+        tokens.add("1");
+        tokens.add("+");
+        tokens.add("2");
+        tokens.add("*");
+        tokens.add("3");
+        return tokens;
     }
 }
