86796a73e0ee186cee1e52442b3e766795d71245
KimGwangHoon <khkimykk@naver.com>
2023-11-12 19:42:11 +0900

    feat: 당첨순위enum 보너스번호2등 추가

--- src/main/java/lotto/domain/RankLotto.java
+++ src/main/java/lotto/domain/RankLotto.java
@@ -1,26 +1,30 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum RankLotto {
 
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, 2_000_000_000, (matchCount, matchBonus) -> matchCount == 6),
+    SECOND(5, 1_500_000 , (matchCount, matchBonus) -> matchCount == 5 && matchBonus),
+    THIRD(5, 1_500_000 , (matchCount, matchBonus) -> matchCount == 5 && !matchBonus),
+    FOURTH(4, 50_000, (matchCount, matchBonus) -> matchCount == 4),
+    FIFTH(3, 5_000, (matchCount, matchBonus) -> matchCount == 3),
+    MISS(0, 0, (matchCount, matchBonus) -> matchCount <= 2);
 
     private final int matchCount;
     private final int winningMoney;
+    private final BiFunction<Integer, Boolean, Boolean> condition;
 
-    RankLotto(int matchCount, int winningMoney) {
+    RankLotto(int matchCount, int winningMoney, BiFunction<Integer, Boolean, Boolean> condition) {
         this.matchCount = matchCount;
         this.winningMoney = winningMoney;
+        this.condition = condition;
     }
 
-    public static RankLotto findRank(int matchCount) {
+    public static RankLotto findRank(int matchCount, boolean bonusCount) {
         return Arrays.stream(RankLotto.values())
-                .filter(rank -> rank.matchCount == matchCount)
+                .filter(rank -> rank.condition.apply(matchCount, bonusCount))
                 .findFirst()
                 .orElse(MISS);
     }
