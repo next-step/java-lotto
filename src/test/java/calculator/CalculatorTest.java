83f2b3917bac0b1ea76b8b2627b45684eee99435
이승준 <skp.seungjun@sk.com>
2023-11-12 13:37:04 +0900

    단순 곱셈, 뺄셈, 나눗셈 구현

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -13,4 +13,25 @@ public class CalculatorTest {
         assertThat(StringCalculator.cal("3 + 6"))
                 .isEqualTo(9);
     }
+
+    @Test
+    @DisplayName("[StringCalculator.cal] 피연산자 2개의 뺄셈 계산식 -> 뺄셈 결과 반환")
+    public void simpleSubTest() {
+        assertThat(StringCalculator.cal("3 - 6"))
+                .isEqualTo(-3);
+    }
+
+    @Test
+    @DisplayName("[StringCalculator.cal] 피연산자 2개의 곱셈 계산식 -> 곱셈 결과 반환")
+    public void simpleMultiTest() {
+        assertThat(StringCalculator.cal("3 * 6"))
+                .isEqualTo(18);
+    }
+
+    @Test
+    @DisplayName("[StringCalculator.cal] 피연산자 2개의 나눗셈 계산식 -> 나눗셈 결과 반환")
+    public void simpleDivTest() {
+        assertThat(StringCalculator.cal("12 / 6"))
+                .isEqualTo(2);
+    }
 }
