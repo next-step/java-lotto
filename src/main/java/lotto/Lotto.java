cddd30fff44204950be3615f7219328f905956cf
이창섭 <ventulus95@gmail.com>
2023-11-12 22:34:25 +0900

    feat: 로또게임 구현
    - 모짝미 중 커밋 진행 없이 해버려서 일단 단일 타겟으로 넘깁니다.

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,73 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Lotto {
+
+    private List<LottoNumber> numbers;
+
+    public Lotto() {}
+
+    public Lotto(List<Integer> numbers) {
+        mapLottoNumbers(numbers);
+        sortNumbers();
+    }
+
+    private void mapLottoNumbers(List<Integer> numbers) {
+        this.numbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            this.numbers.add(new LottoNumber(number));
+        }
+    }
+
+    public List<LottoNumber> numbers() {
+        return numbers;
+    }
+
+    public int size() {
+        return numbers.size();
+    }
+
+    private void sortNumbers() {
+        Collections.sort(numbers);
+    }
+
+    public void makeNumbers() {
+        this.numbers = new ArrayList<>();
+        sortNumbers();
+//        for (int i = 0; i <6; i++) {
+//            numbers.add(i);
+//        }
+    }
+
+    public int countMatchingWinningLotto(Lotto lotto) {
+        int count = 0;
+        for (LottoNumber number : lotto.numbers()) {
+            count += containsWinningNumber(number);
+        }
+        return count;
+    }
+
+    private int containsWinningNumber(LottoNumber lottoNumber) {
+        if (numbers.contains(lottoNumber)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lotto lotto = (Lotto) o;
+        return Objects.equals(numbers, lotto.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+}
---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+
+    public static final int LOTTO_PRICE = 1000;
+    private List<Lotto> lottos = new ArrayList<>();
+    private Lotto winningLotto;
+
+    public void buyLotto(int price) {
+        int gameCount = getGameCount(price);
+        while(countLotto() < gameCount) {
+            Lotto lotto = new Lotto();
+            lotto.makeNumbers();
+            lottos.add(lotto);
+        }
+    }
+
+    private int getGameCount(int price) {
+        if (isValidBuyLottoPrice(price)) {
+            throw new IllegalArgumentException();
+        }
+        return price /1000;
+    }
+
+    private  boolean isValidBuyLottoPrice(int price) {
+        return price % LOTTO_PRICE != 0;
+    }
+
+    public int countLotto() {
+        return lottos.size();
+    }
+
+    public void registerWinningLotto(List<Integer> numbers) {
+        this.winningLotto = new Lotto(numbers);
+    }
+
+    public Lotto winningLotto() {
+        return winningLotto;
+    }
+}
