2dba2b39acaa185d91d74842857ad6e8a95f6493
이창섭 <ventulus95@gmail.com>
2023-11-12 22:40:26 +0900

    feat: 로또 통계 클래스 추가
    - 로또 통계 클래스 추가

---
+++ src/test/java/lotto/LottoStaticsTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LottoStaticsTest {
+
+    @Test
+    void 당첨번호와_일치하는_갯수별로_로또갯수를_저장한다() {
+        LottoStatics lottoStatics = new LottoStatics();
+        List<Lotto> lottos = List.of();
+        Lotto winningLotto = new Lotto();
+//        lottoStatics.classifyRankLotto(lottos, winningLotto);
+
+    }
+}
