a25044f042a1255fa44a6119c829eaf983c4cd20
namuhuchutong <namuhuchutong@gmail.com>
2023-11-12 14:19:43 +0900

    refactor: 메서드 이름 수정

--- src/test/java/me/namuhuchutong/lotto/LottoSellerTest.java
+++ src/test/java/me/namuhuchutong/lotto/LottoSellerTest.java
@@ -9,7 +9,7 @@ class LottoSellerTest {
     
     @DisplayName("로또를 구입할 수 없는 금액일 경우 예외가 발생한다.")
     @Test
-    void throw_exception_when_can_buy_lotto() {
+    void throw_exception_when_can_not_buy_lotto() {
         //given
         int givenAmount = 999;
         LottoSeller lottoSeller = new LottoSeller(() -> null);
