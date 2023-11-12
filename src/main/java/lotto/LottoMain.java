2997371057f32e31e6c0bfd48a840b2752900fb1
KimGwangHoon <khkimykk@naver.com>
2023-11-12 17:08:54 +0900

    fix: intput변경 static method만을 가지는 utility class는 private 생성자를 가지도록 구현한다.

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -5,19 +5,19 @@ import lotto.domain.WinnerNumber;
 import lotto.domain.WinnerResult;
 import lotto.strategy.NumberStrategy;
 import lotto.strategy.RandomNumberStrategy;
-import lotto.view.InputView;
 
+import static lotto.view.InputView.inputMoney;
+import static lotto.view.InputView.inputWinNumber;
 import static lotto.view.ResultView.*;
 
 public class LottoMain {
 
-    private static final InputView INPUT_VIEW = new InputView();
     private static final NumberStrategy NUMBER_STRATEGY = new RandomNumberStrategy();
 
     public static void main(String[] args) {
-        Purchase purchase = new Purchase(INPUT_VIEW.inputMoney(), NUMBER_STRATEGY);
+        Purchase purchase = new Purchase(inputMoney(), NUMBER_STRATEGY);
         print(purchase.toString());
-        WinnerNumber winnerNumber = new WinnerNumber(INPUT_VIEW.inputWinNumber());
+        WinnerNumber winnerNumber = new WinnerNumber(inputWinNumber());
         WinnerResult winnerResult = new WinnerResult(winnerNumber.resultLotto(), purchase.lottos());
         print(winnerResult.toString());
         resultPrint(winnerResult.rateOfReturn(purchase.amount()));
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,9 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    private InputView() {
+    }
+
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return SCANNER.nextInt();
