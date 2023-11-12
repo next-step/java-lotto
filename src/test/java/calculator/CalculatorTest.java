422a07d1e411835c1f04197ff5dadcf77ce29619
이창섭 <ventulus95@gmail.com>
2023-11-12 22:56:33 +0900

    refact: 테스트 코드 리펙토링

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -6,30 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calculator =  new Calculator();
+
     @Test
     void 더하기_기능() {
-        Calculator calculator = new Calculator();
         int result = calculator.plus(1,1);
         assertEquals(2, result);
     }
 
     @Test
     void 빼기_기능() {
-        Calculator calculator = new Calculator();
         int result = calculator.minus(1, 1);
         assertEquals(0, result);
     }
 
     @Test
     void 곱하기_기능() {
-        Calculator calculator = new Calculator();
         int result = calculator.multiply(3,4);
         assertEquals(12, result);
     }
 
     @Test
     void 나누기_기능() {
-        Calculator calculator = new Calculator();
         int result = calculator.divide(4, 2);
         assertEquals(2, result);
     }
