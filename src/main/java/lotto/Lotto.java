ab5ad71c9bf58be67dc1e791da1a81e94c7e02d8
sunggyupaik <melon9751@naver.com>
2023-11-12 23:41:26 +0900

    [feat] 로또 숫자  자동 생성 및 정렬을 구현하라
    정렬된 랜덤 숫자 6개인 로또 숫자 자동 생성을 구현합니다

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,49 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+	private final static int NUMBER_COUNT = 6;
+	private final List<Integer> lottoNumbers;
+
+	public Lotto() {
+		this.lottoNumbers = randomLottoNumbers();
+	}
+
+	public Lotto(List<Integer> lottoNumbers) {
+		this.lottoNumbers = lottoNumbers;
+	}
+
+	private List<Integer> randomLottoNumbers() {
+		List<Integer> allNumbers = allNumbers();
+		List<Integer> randomLottoNumbers = sortedRandomNumbers(allNumbers);
+
+		return Collections.unmodifiableList(randomLottoNumbers);
+	}
+
+	private List<Integer> sortedRandomNumbers(List<Integer> numbers) {
+		Collections.shuffle(numbers);
+		List<Integer> randomLottoNumbers = new ArrayList<>();
+		for(int i = 0; i < Lotto.NUMBER_COUNT; i++) {
+			randomLottoNumbers.add(numbers.get(i));
+		}
+		Collections.sort(randomLottoNumbers);
+
+		return randomLottoNumbers;
+	}
+
+	private List<Integer> allNumbers() {
+		List<Integer> numbers = new ArrayList<>();
+		for (int i = 1; i <= 45; i++) {
+			numbers.add(i);
+		}
+
+		return numbers;
+	}
+
+	public List<Integer> lottoNumbers() {
+		return this.lottoNumbers;
+	}
+}
