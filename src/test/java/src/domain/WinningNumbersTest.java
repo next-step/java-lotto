dc24492013ae32d7ce0bbbe537cd04664d2c974f
solchan98 <dev.sol0427@gmail.com>
2023-11-13 18:48:49 +0900

    test: 로또를 매치하여 순위를 확인할 수 있는 기능 테스트 작성

---
+++ src/test/java/src/domain/WinningNumbersTest.java
@@ -0,0 +1,28 @@
+package src.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinningNumbersTest {
+
+    @Test
+    void 로또와_매치하여_순위를_확인할_수_있다() {
+        // given
+        WinningNumbers winningNumbers = new WinningNumbers(
+                List.of(GameNumber.of(1), GameNumber.of(2), GameNumber.of(3),
+                        GameNumber.of(4), GameNumber.of(5), GameNumber.of(6))
+        );
+
+        List<GameNumber> gameNumbers = List.of(GameNumber.of(4), GameNumber.of(5), GameNumber.of(6),
+                GameNumber.of(7), GameNumber.of(8), GameNumber.of(9));
+
+        // when
+        Place place = winningNumbers.match(gameNumbers);
+
+        // then
+        assertThat(place).isEqualTo(Place.FOURTH_PLACE);
+    }
+}
