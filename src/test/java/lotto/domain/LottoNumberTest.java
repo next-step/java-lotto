a70be55774e57dd06b96144e67e62d97a635ae52
엄태권 <akfls221@gmail.com>
2023-11-14 01:19:26 +0900

    fix : 0, 46 엣지케이스 테스트

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -14,11 +16,11 @@ class LottoNumberTest {
         assertThat(new LottoNumber(1)).isEqualTo(new LottoNumber(1));
     }
 
-    @Test
-    @DisplayName("허용범위(1~45) 외의 숫자로 LottoNumber을 생성시 오류가 발생한다.")
-    void createLottoNumber_초과범위() {
+    @ParameterizedTest(name = "허용범위(1~45) 외의 숫자 {0}으로 LottoNumber을 생성시 오류가 발생한다.")
+    @ValueSource(ints = {0, 46})
+    void createLottoNumber_초과범위(int wrongValue) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoNumber(99))
+                .isThrownBy(() -> new LottoNumber(wrongValue))
                 .withMessage("로또의 번호 범위를 벗어났습니다.");
     }
 }
