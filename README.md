066c57ac60eaea1cf8d4f7aef8194d8ddf34d813
HyunggiPark <phg9587@naver.com>
2023-11-13 23:59:11 +0900

    refact: 여러 검증을 사용하는 경우, assertAll을 사용해보자 [`SoftAssertions.assertSoftly`]

--- README.md
+++ README.md
@@ -126,4 +126,16 @@
 - [X] ~~2등을 위해 추가 번호를 하나 더 추첨한다.~~
 - [X] ~~당첨 통계에도 2등을 추가한다.~~
 
+### 추가 피드백 사항
+
+- 23.11.13
+  - [ ] throwExceptionIfValueIsNotValid의 parameter 확인
+  - [ ] LottoRank가 가지는 인스턴스 변수가 많아졌다. 축소가 가능한지 확인
+  - [ ] LottoRank 생성자에서 final을 붙이는 parameter 확인
+  - [ ] matchLottos() 메서드의 역할을 로또 객체에게 시키는 것이 가능한지 확인
+  - [ ] validateSize가 중복해서 수행된다. 어떻게 처리하면 좋을지 고민
+  - [ ] Set<LottoNumber>는 결국 Lotto 객체인 것 같다. Lotto 객체를 관리하도록 변경
+  - [ ] 사용하지 않는 NumberValidatorTest 삭제
+  - [X] ~~여러 검증을 사용하는 경우, assertAll을 사용해보자 [`SoftAssertions.assertSoftly`]~~
+
 ---
--- src/test/java/lotto/step2/domain/LottoRankTest.java
+++ src/test/java/lotto/step2/domain/LottoRankTest.java
@@ -1,30 +1,35 @@
 package lotto.step2.domain;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoRankTest {
     @Test
     @DisplayName("findByCount의 입력으로 일치하는 개수를 넣으면, 해당하는 LottoRank 객체를 반환한다.")
     void testFindByCount() {
-        assertThat(LottoRank.findByCountAndBonusMatching(3, false)).isEqualTo(LottoRank.FIFTH);
-        assertThat(LottoRank.findByCountAndBonusMatching(4, false)).isEqualTo(LottoRank.FOURTH);
-        assertThat(LottoRank.findByCountAndBonusMatching(5, false)).isEqualTo(LottoRank.THIRD);
-        assertThat(LottoRank.findByCountAndBonusMatching(5, true)).isEqualTo(LottoRank.SECOND);
-        assertThat(LottoRank.findByCountAndBonusMatching(6, false)).isEqualTo(LottoRank.FIRST);
+        SoftAssertions.assertSoftly(softly -> {
+                    softly.assertThat(LottoRank.findByCountAndBonusMatching(3, false)).isEqualTo(LottoRank.FIFTH);
+                    softly.assertThat(LottoRank.findByCountAndBonusMatching(4, false)).isEqualTo(LottoRank.FOURTH);
+                    softly.assertThat(LottoRank.findByCountAndBonusMatching(5, false)).isEqualTo(LottoRank.THIRD);
+                    softly.assertThat(LottoRank.findByCountAndBonusMatching(5, true)).isEqualTo(LottoRank.SECOND);
+                    softly.assertThat(LottoRank.findByCountAndBonusMatching(6, false)).isEqualTo(LottoRank.FIRST);
+                }
+        );
     }
 
     @Test
     @DisplayName("hasMatchingCount를 사용하면, 각 LottoRank의 matchingCount가 0 초과인지 확인한다.")
     void testHasMatchingCount() {
-        assertThat(LottoRank.OTHER.hasMatchingCount()).isFalse();
+        SoftAssertions.assertSoftly(softly -> {
+                    softly.assertThat(LottoRank.OTHER.hasMatchingCount()).isFalse();
 
-        assertThat(LottoRank.FIFTH.hasMatchingCount()).isTrue();
-        assertThat(LottoRank.FOURTH.hasMatchingCount()).isTrue();
-        assertThat(LottoRank.THIRD.hasMatchingCount()).isTrue();
-        assertThat(LottoRank.SECOND.hasMatchingCount()).isTrue();
-        assertThat(LottoRank.FIRST.hasMatchingCount()).isTrue();
+                    softly.assertThat(LottoRank.FIFTH.hasMatchingCount()).isTrue();
+                    softly.assertThat(LottoRank.FOURTH.hasMatchingCount()).isTrue();
+                    softly.assertThat(LottoRank.THIRD.hasMatchingCount()).isTrue();
+                    softly.assertThat(LottoRank.SECOND.hasMatchingCount()).isTrue();
+                    softly.assertThat(LottoRank.FIRST.hasMatchingCount()).isTrue();
+                }
+        );
     }
 }
