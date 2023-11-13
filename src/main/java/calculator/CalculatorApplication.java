cfdb43d7a5e7f50ccc31383c49883540d5dcb5ab
hoon25 <cchoon95@hanmail.net>
2023-11-14 02:39:13 +0900

    feat(output): OutputView 구현

--- src/main/java/calculator/CalculatorApplication.java
+++ src/main/java/calculator/CalculatorApplication.java
@@ -1,6 +1,8 @@
 package calculator;
 
+import calculator.domain.Calculator;
 import calculator.ui.InputView;
+import calculator.ui.OutputView;
 
 import java.util.List;
 
@@ -8,6 +10,8 @@ public class CalculatorApplication {
     public static void main(String[] args) {
         List<String> strings = InputView.input();
 
+        Calculator calculator = new Calculator(strings);
 
+        OutputView.output(calculator.calculate());
     }
 }
