157682a386c0d6e3bf7c20f7528bf3f6bb968640
엄태권 <akfls221@gmail.com>
2023-11-13 22:12:20 +0900

    feat : LottoMachine 불필요한 equals 제거

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,7 +1,5 @@
 package lotto.domain;
 
-import java.util.Objects;
-
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1_000;
@@ -20,18 +18,5 @@ public class LottoMachine {
 
     public Lottos getLottoTickets() {
         return this.lottos;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LottoMachine that = (LottoMachine) o;
-        return Objects.equals(money, that.money) && Objects.equals(lottos, that.lottos);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(money, lottos);
     }
 }
