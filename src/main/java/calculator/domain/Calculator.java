d08e18fc3e4650b8efb3dcf86b845da767233fc8
hoon25 <cchoon95@hanmail.net>
2023-11-14 02:51:14 +0900

    refactor(calculator): 메서드 리팩토링

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -10,20 +10,31 @@ public class Calculator {
     }
 
     public int calculate() {
-        int result = Integer.parseInt(inputString.get(0));
-        int i = 1;
-        while (i < inputString.size()) {
-            result = partialCalculate(result, inputString.get(i), inputString.get(i + 1));
-            i += 2;
-            if ((i + 1) == inputString.size()) {
-                throw new IllegalArgumentException("비정상적인 연산입니다.");
-            }
+        int result = Digit.of(inputString.get(0)).getNum();
+        int point = 1;
+        while (point < inputString.size()) {
+            validateCalculate(point);
+            result = partialCalculate(result, getNextOperator(point), getNextDigit(point));
+            point += 2;
         }
         return result;
     }
 
-    private int partialCalculate(int num1, String operator, String num2) {
-        return Operator.of(operator)
-                .operate(num1, Digit.of(num2).getNum());
+    private Operator getNextOperator(int i) {
+        return Operator.of(inputString.get(i));
+    }
+
+    private Digit getNextDigit(int i) {
+        return Digit.of(inputString.get(i + 1));
+    }
+
+    private int partialCalculate(int num1, Operator operator, Digit num2) {
+        return operator.operate(num1, num2.getNum());
+    }
+
+    private void validateCalculate(int i) {
+        if ((i + 1) == inputString.size()) {
+            throw new IllegalArgumentException("비정상적인 연산입니다.");
+        }
     }
 }
