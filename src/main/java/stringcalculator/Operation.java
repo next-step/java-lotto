8617831433cc482e3d01e7ddbf926945283acad7
kimbro97 <rlagudwog@gmail.com>
2023-11-13 23:17:47 +0900

    feat: 두 수를 더하는 기능 구현

---
+++ src/main/java/stringcalculator/Operation.java
@@ -0,0 +1,7 @@
+package stringcalculator;
+
+public class Operation {
+    public static Number addition(int number1, int number2) {
+        return number1 + number2;
+    }
+}
---
+++ src/test/java/stringcalculator/OperationTest.java
@@ -0,0 +1,14 @@
+package stringcalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OperationTest {
+    @Test
+    @DisplayName("두 수를 더 할 수 있다.")
+    public void addition() {
+        Number result = Operation.addition(4, 5);
+        Assertions.assertThat(result).isEqualTo(9);
+    }
+}
