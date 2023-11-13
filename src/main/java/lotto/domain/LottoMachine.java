c500b1a2e6f148e5aa9ff1458c52a04f4f7c60ed
엄태권 <akfls221@gmail.com>
2023-11-13 22:56:28 +0900

    fix : Money를 매게변수로 받도록 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,17 +3,15 @@ package lotto.domain;
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1_000;
-    private final Money money;
     private final Lottos lottos;
 
 
     public LottoMachine(Money money) {
-        this.money = money;
-        this.lottos = Lottos.of(calcPossibleLottoCount());
+        this.lottos = Lottos.of(calcPossibleLottoCount(money));
     }
 
-    private int calcPossibleLottoCount() {
-        return this.money.getMoney() / LOTTO_PRICE;
+    private int calcPossibleLottoCount(Money money) {
+        return money.getMoney() / LOTTO_PRICE;
     }
 
     public Lottos getLottoTickets() {
