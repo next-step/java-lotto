65011b33ac686c888d462b1f7692c5fe7fe86aab
beyondorder <youofsea@gmail.com>
2023-11-14 00:16:31 +0900

    feat: 로또번호 생성 가능한 숫자는 1~45이다.

--- src/main/java/step3/domain/LottoNumber.java
+++ src/main/java/step3/domain/LottoNumber.java
@@ -5,7 +5,14 @@ public class LottoNumber {
     private final int number;
 
     private LottoNumber(int number) {
+        inputValidation(number);
         this.number = number;
+    }
+
+    private void inputValidation(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자만 가능합니다.");
+        }
     }
 
     public static LottoNumber of(int number) {
--- src/test/java/step3/domain/LottoNumberTest.java
+++ src/test/java/step3/domain/LottoNumberTest.java
@@ -3,6 +3,8 @@ package step3.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,4 +16,13 @@ public class LottoNumberTest {
         LottoNumber lottoNumber = LottoNumber.of(1);
         assertThat(lottoNumber.number()).isEqualTo(1);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    @DisplayName("로또번호 생성 가능한 숫자는 1~45이다. 예외 테스트")
+    void createException(int input) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.of(input));
+    }
+
+
 }
