e3f7bc8a0780a9f5a140a490f55eca169cff5742
jeonghuntech <jeonghun.tech@gmail.com>
2023-11-13 12:04:16 +0900

    feat: 문자열 파싱

---
+++ src/main/java/parse/Parser.java
@@ -0,0 +1,20 @@
+package parse;
+
+import token.Tokens;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Parser {
+    public static final Tokens parse(String text) {
+        validateEmpty(text);
+        List<String> splits = Splitter.split(text, Splitter.BLANK);
+        return Tokens.from(splits);
+    }
+
+    private static void validateEmpty(String text) {
+        if (text == null || text.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
