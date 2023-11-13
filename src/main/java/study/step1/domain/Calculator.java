5f396de4925f83d629681d06fa0e1bf0c66b8965
gilbertlim <9ilbert.app@gmail.com>
2023-11-13 14:48:15 +0900

    refactor: Enum 요소에 계산함수 다형성 적용

--- src/main/java/study/step1/domain/Calculator.java
+++ src/main/java/study/step1/domain/Calculator.java
@@ -9,17 +9,10 @@ import study.step1.domain.type.Operator;
 public class Calculator {
 
     private final Input input;
-    private final Calculation calculator;
-
     private int latestValue = 0;
 
     public Calculator(String[] input) {
-        this(input, new DefaultCalculation());
-    }
-
-    public Calculator(String[] input, Calculation calculator) {
         this.input = new Input(input);
-        this.calculator = calculator;
     }
 
     public int calculate() {
@@ -28,40 +21,12 @@ public class Calculator {
         latestValue = Objects.requireNonNull(numbers.poll());
         int size = numbers.size();
         for (int i = 0; i < size; i++) {
-            process(operators.poll(), numbers.poll());
+            latestValue = process(operators.poll(), numbers.poll());
         }
         return latestValue;
     }
 
-    private void process(Operator operator, int number) {
-        isPlus(operator, number);
-        isMinus(operator, number);
-        isTimes(operator, number);
-        isDivide(operator, number);
+    private int process(Operator operator, int number) {
+        return operator.calculate(latestValue, number);
     }
-
-    private void isPlus(Operator operator, int number) {
-        if (operator.isPlus()) {
-            latestValue = calculator.plus(latestValue, number);
-        }
-    }
-
-    private void isMinus(Operator operator, int number) {
-        if (operator.isMinus()) {
-            latestValue = calculator.minus(latestValue, number);
-        }
-    }
-
-    private void isTimes(Operator operator, int number) {
-        if (operator.isTimes()) {
-            latestValue = calculator.times(latestValue, number);
-        }
-    }
-
-    private void isDivide(Operator operator, int number) {
-        if (operator.isDivide()) {
-            latestValue = calculator.divide(latestValue, number);
-        }
-    }
-
 }
--- src/main/java/study/step1/domain/type/Operator.java
+++ src/main/java/study/step1/domain/type/Operator.java
@@ -1,20 +1,41 @@
 package study.step1.domain.type;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
+
+import study.step1.domain.DefaultOperation;
 
 public enum Operator {
 
-    PLUS("+", "더하기"),
-    MINUS("-", "빼기"),
-    TIMES("*", "곱하기"),
-    DIVIDE("/", "나누기");
+    PLUS("+", "더하기", (x, y) -> {
+        DefaultOperation operation = new DefaultOperation();
+        return operation.plus(x, y);
+    }),
+    MINUS("-", "빼기", (x, y) -> {
+        DefaultOperation operation = new DefaultOperation();
+        return operation.minus(x, y);
+    }),
+    TIMES("*", "곱하기", (x, y) -> {
+        DefaultOperation operation = new DefaultOperation();
+        return operation.times(x, y);
+    }),
+    DIVIDE("/", "나누기", (x, y) -> {
+        DefaultOperation operation = new DefaultOperation();
+        return operation.divide(x, y);
+    });
 
     private final String code;
     private final String message;
+    public final BiFunction<Integer, Integer, Integer> expression;
 
-    Operator(String code, String message) {
+    public int calculate(int x, int y) {
+        return expression.apply(x, y);
+    }
+
+    Operator(String code, String message, BiFunction<Integer, Integer, Integer> expression) {
         this.code = code;
         this.message = message;
+        this.expression = expression;
     }
 
     public static Operator valueOfCode(String code) {
@@ -26,21 +47,5 @@ public enum Operator {
 
     public String getCode() {
         return code;
-    }
-
-    public boolean isPlus() {
-        return this == PLUS;
-    }
-
-    public boolean isMinus() {
-        return this == MINUS;
-    }
-
-    public boolean isTimes() {
-        return this == TIMES;
-    }
-
-    public boolean isDivide() {
-        return this == DIVIDE;
     }
 }
