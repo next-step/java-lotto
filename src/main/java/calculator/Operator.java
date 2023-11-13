fb6253ec36b2380262f0f5154c08744986ebe594
yoohojeong <yh132@naver.com>
2023-11-14 05:01:08 +0900

    refactor : #3337 step1 피드백 반영 - readme 요구사항 상세 작성 - Operator enum 선언으로 반복 패턴 제거 - '입력 값이 null 또는 빈 공백일 경우' 테스트 시 NullAndEmptySource 활용

---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,55 @@
+package calculator;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Operator {
+
+        ADDITION("+", "더하기", (valueX, valueY) -> valueX + valueY),
+        SUBTRACTION("-", "빼기", (valueX, valueY) -> valueX - valueY),
+        MULTIPLICATION("*", "곱하기", (valueX, valueY) -> valueX * valueY),
+        DIVISION("/", "나누기", (valueX, valueY) -> valueX / valueY);
+
+        private final String code;
+        private final String description;
+        private final BiFunction<Integer, Integer, Integer> expression;
+
+        Operator(String code, String description, BiFunction<Integer, Integer, Integer> expression) {
+                this.code = code;
+                this.description = description;
+                this.expression = expression;
+        }
+
+        public String code() {
+                return code;
+        }
+
+        public boolean isSameCode(String code) {
+                return code.equals(this.code);
+        }
+
+        public static Operator valueOfCode(String code) {
+                if (!CODE_MAP.containsKey(code)) {
+                        throw new IllegalArgumentException("잘못된 사칙연산 기호를 입력하였습니다.");
+                }
+                return CODE_MAP.get(code);
+        }
+
+        public int apply(int valueX, int valueY) {
+                if (this.isSameCode(DIVISION.code)) {
+                        validateDivisionDenominator(valueY);
+                }
+                return expression.apply(valueX, valueY);
+        }
+
+        private void validateDivisionDenominator(int valueY) {
+                if (valueY == 0) {
+                        throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
+        }
+
+        private static final Map<String, Operator> CODE_MAP =
+            Stream.of(values()).collect(Collectors.toMap(Operator::code, operator -> operator));
+}
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,42 +4,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class CalculatorTest {
 
         @Test
         void 더하기() {
+                Calculator calculator = new Calculator();
                 int valueX = 1;
                 int valueY = 10;
-                assertThat(Addition.add(valueX, valueY)).isEqualTo(11);
+                assertThat(Operator.ADDITION.apply(valueX, valueY)).isEqualTo(11);
         }
 
         @Test
         void 빼기() {
                 int valueX = 1;
                 int valueY = 10;
-                assertThat(Subtraction.subtract(valueX, valueY)).isEqualTo(-9);
+                assertThat(Operator.SUBTRACTION.apply(valueX, valueY)).isEqualTo(-9);
         }
 
         @Test
         void 곱하기() {
                 int valueX = 1;
                 int valueY = 10;
-                assertThat(Multiplication.multiple(valueX, valueY)).isEqualTo(10);
+                assertThat(Operator.MULTIPLICATION.apply(valueX, valueY)).isEqualTo(10);
         }
 
         @Test
         void 나누기() {
                 int valueX = 1;
                 int valueY = 10;
-                assertThat(Division.divide(valueX, valueY)).isEqualTo(0);
+                assertThat(Operator.DIVISION.apply(valueX, valueY)).isEqualTo(0);
         }
 
-        @Test
-        void 입력_값이_null_또는_빈_공백이면_에러() {
+        @ParameterizedTest
+        @NullAndEmptySource
+        void 입력_값이_null_또는_빈_공백이면_에러(String input) {
                 Calculator calculator = new Calculator();
-                assertThatThrownBy(() -> calculator.calculate("")).isInstanceOf(IllegalArgumentException.class);
-                assertThatThrownBy(() -> calculator.calculate(null)).isInstanceOf(IllegalArgumentException.class);
+                assertThatThrownBy(() -> calculator.calculate(input)).isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
