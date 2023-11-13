b7c3b4db40d0332c5f0e509a577df7b6cb1ca391
qwe5507 <apvmffkdls@gmail.com>
2023-11-13 23:51:20 +0900

    refactor: [step2피드백 반영] 숫자 표기 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, new BigDecimal("2_000_000_000")),
-    SECOND(5, new BigDecimal("30_000_000")),
-    THIRD(5, new BigDecimal("1_500_000")),
-    FOURTH(4, new BigDecimal("50_000")),
-    FIFTH(3, new BigDecimal("5_000")),
+    FIRST(6, new BigDecimal(2_000_000_000)),
+    SECOND(5, new BigDecimal(30_000_000)),
+    THIRD(5, new BigDecimal(1_500_000)),
+    FOURTH(4, new BigDecimal(50_000)),
+    FIFTH(3, new BigDecimal(5_000)),
     MISS(0, BigDecimal.ZERO);
 
     public static final String INVALID_COUNT = "올바른 갯수가 아닙니다.";
