b2fdf48656831eb62b225d475f87a9eac04b5ce2
parkje0927 <parkje09270@gmail.com>
2023-11-13 20:35:05 +0900

    refactor: 문자열 계산기를 실행하는 Main 클래스에서는 input 을 Calculator 클래스에 전달하고 output 을 호출하는 역할만 수행하도록 변경

--- src/main/java/step1/CalculatorMain.java
+++ src/main/java/step1/CalculatorMain.java
@@ -1,22 +1,14 @@
 package step1;
 
 import step1.domain.calculator.Calculator;
-import step1.view.InputView;
-import step1.view.OutputView;
+
+import static step1.view.InputView.input;
+import static step1.view.OutputView.output;
 
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        String[] inputValues = inputView.input();
-
         Calculator calculator = new Calculator();
-        int result = Integer.valueOf(inputValues[0]);
-        for (int i = 1; i < inputValues.length; i += 2) {
-            calculator.updateOperator(inputValues[i]);
-            result = calculator.calculate(result, Integer.valueOf(inputValues[i + 1]));
-        }
-        outputView.output(result);
+        output(calculator.calculate(input()));
     }
 }
