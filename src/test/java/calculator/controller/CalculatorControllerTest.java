f0eea3773e43d9da2711c5a15b994d964b612273
kyunghyun-park <qqqq_o@naver.com>
2023-11-13 23:49:01 +0900

    test: 입력값 패턴에 대한 통과 테스트 추가

--- src/test/java/calculator/controller/CalculatorControllerTest.java
+++ src/test/java/calculator/controller/CalculatorControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorControllerTest {
@@ -14,7 +15,7 @@ class CalculatorControllerTest {
     void validate_null() {
         assertThatThrownBy(() -> new CalculatorController(" "))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력값이 형식에 맞지 않습니다");
+                .hasMessageContaining("연산식이 입력되지 않았습니다.");
     }
 
     @ParameterizedTest
@@ -24,10 +25,21 @@ class CalculatorControllerTest {
             , "2  +3 * 4 / 2"
             , "+ 3 / 2"})
     @DisplayName("연산식의 형식이 맞지 않을 경우 IllegalArgumentException를 반환한다.")
-    void validate_pattern(String expression) {
+    void validate_invalid_pattern(String expression) {
         assertThatThrownBy(() -> new CalculatorController(expression))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력값이 형식에 맞지 않습니다");
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "2 + 3 * 4 / 2"
+            , "2 + 3"
+            , "4 / 2"})
+    @DisplayName("연산식의 형식이 맞을 경우 예외를 반환하지 않는다.")
+    void validate_valid_pattern(String expression) {
+        assertThatCode(() -> new CalculatorController(expression))
+                .doesNotThrowAnyException();
     }
 }
