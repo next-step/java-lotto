f500ced0e4271313bccb639f4d71e0731fa4713b
parkje0927 <parkje09270@gmail.com>
2023-11-13 20:33:33 +0900

    feat: view 패키지 내의 InputView, OutputView 클래스의 메소드를 static 메소드로 변경

--- src/main/java/step1/view/InputView.java
+++ src/main/java/step1/view/InputView.java
@@ -8,13 +8,13 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String SPLIT_SEPARATOR = "\\s";
 
-    public String[] input() {
+    public static String[] input() {
         System.out.println("수행할 수식을 입력해주세요.");
         String inputValue = SCANNER.nextLine();
         return getExpression(inputValue);
     }
 
-    public String[] getExpression(String inputValue) {
+    public static String[] getExpression(String inputValue) {
         if (Objects.isNull(inputValue) || inputValue.isBlank()) {
             throw new IllegalArgumentException("입력 값은 null 이거나 빈 공백 문자일 수 없습니다.");
         }
--- src/main/java/step1/view/OutputView.java
+++ src/main/java/step1/view/OutputView.java
@@ -2,7 +2,7 @@ package step1.view;
 
 public class OutputView {
 
-    public void output(int result) {
+    public static void output(int result) {
         System.out.println("계산 결과는 " + result + " 입니다.");
     }
 }
