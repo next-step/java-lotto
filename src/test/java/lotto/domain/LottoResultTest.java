a712a30ce0dc3e7f9cb3bdbbaf22f46b9612debd
엄태권 <akfls221@gmail.com>
2023-11-14 01:16:10 +0900

    fix : BeforeEach 수정

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,13 +1,11 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,29 +13,15 @@ class LottoResultTest {
 
     private LottoResult lottoResult;
 
-    @BeforeEach
-    void init() {
-        Lotto lotto = new Lotto((Arrays.asList(1, 2, 3, 4, 5, 6)));
-        Lotto winnigLotto = new Lotto((Arrays.asList(1, 2, 3, 10, 11, 12)));
-        WinningLotto winningLotto = new WinningLotto(winnigLotto);
-        Lottos lottos = new Lottos(Arrays.asList(lotto));
-
-        lottoResult = new LottoResult(winningLotto, lottos);
-    }
-
-
     @Test
-    @DisplayName("1개의 4등 당첨 로또가 있기때문에 총 당첨금액은 5,000원이다. ")
-    void calcTotalWinningPrice() {
-        Money returnRate = lottoResult.calcTotalWinningPrice(lottoResult.findWinningRanks());
-        assertThat(returnRate.getMoney()).isEqualTo(5000);
-    }
-
-    @Test
-    @DisplayName("1개의 4등 당첨 로또가 있기 때문에, 1개의 4등 당첨결과를 가지고있다.")
+    @DisplayName("1개의 4등 당첨 로또가 있기때문에 findWinningRanks를 할 경우 4등짜리 1개가 나온다.")
     void findWinningRanks() {
-        List<Rank> winningRanks = lottoResult.findWinningRanks();
+        List<Lotto> lottos = Arrays.asList(new Lotto((Arrays.asList(1, 2, 3, 4, 5, 6))));
+        Lotto winnigLotto = new Lotto((Arrays.asList(1, 2, 3, 10, 11, 12)));
+        this.lottoResult = new LottoResult(new WinningLotto(winnigLotto), new Lottos(lottos));
 
-        assertThat(winningRanks).hasSize(1).contains(Rank.FOURTH);
+        Map<Rank, Long> winningRanks = this.lottoResult.findWinningRanks();
+
+        assertThat(winningRanks).containsEntry(Rank.FOURTH, 1L);
     }
 }
