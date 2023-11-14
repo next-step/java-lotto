29def5da173d3bb70acd38cfc51f109935f8293c
kyunghyun-park <qqqq_o@naver.com>
2023-11-15 00:39:35 +0900

    test: 사칙연산에 대한 테스트 추가

--- src/test/java/calculator/domain/OperatorTest.java
+++ src/test/java/calculator/domain/OperatorTest.java
@@ -2,10 +2,36 @@ package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OperatorTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,3,5", "50,4,54", "35,10,45"})
+    void plus(int x, int y, int expected) {
+        int actual = Operator.PLUS.apply(x, y);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,3,-1", "50,4,46", "35,10,25"})
+    void minus(int x, int y, int expected) {
+        int actual = Operator.MINUS.apply(x, y);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,3,6", "50,4,200", "3,10,30"})
+    void multiply(int x, int y, int expected) {
+        int actual = Operator.MULTIPLY.apply(x, y);
+
+        assertThat(actual).isEqualTo(expected);
+    }
 
     @Test
     @DisplayName("나눗셈 연산 시 결과 값이 정수로 떨어진다.")
