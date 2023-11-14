4017bbbec4008788f2f339170b69b51a2e7a39a2
choomi1217 <whdudal1217@naver.com>
2023-11-15 01:00:48 +0900

    feat: Calculator 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Calculator {
+import java.util.Stack;
+
+public class Calculator {
+
+    public static CalculatorExpression calculate(String postfix) {
+        Stack<CalculatorExpression> stack = new Stack<>();
+        for (char c : postfix.toCharArray()) {
+            switch (c) {
+                case '+':
+                    stack.push(new AddExpression(stack.pop(), stack.pop()));
+                    break;
+                case '-':
+                    CalculatorExpression right = stack.pop();
+                    CalculatorExpression left = stack.pop();
+                    stack.push(new MinusExpression(left, right));
+                    break;
+                case '*':
+                    stack.push(new MultiplicationExpression(stack.pop(), stack.pop()));
+                    break;
+                case '/':
+                    CalculatorExpression dividend = stack.pop();
+                    CalculatorExpression divisor = stack.pop();
+                    stack.push(new DivideExpression(divisor, dividend));
+                    break;
+                default:
+                    stack.push(new VariableExpression(c));
+            }
+        }
+        return stack.pop();
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Calculator {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.beans.Expression;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.internal.bytebuddy.asm.Advice.Argument;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CalculatorTest {
+
+    private static final VariableExpression VARIABLE_ONE = new VariableExpression('1');
+    private static final VariableExpression VARIABLE_TWO = new VariableExpression('2');
+    @DisplayName("기호를 만나면 알맞은 Expression을 반환한다.")
+    @ParameterizedTest(name = " \"{0}\"은 \"{1}\"타입")
+    @MethodSource
+    void someMarkMustReturnExpressionType(String postfixString, CalculatorExpression expectedType){
+        CalculatorExpression expression = Calculator.calculate(postfixString);
+        assertThat(expression).isInstanceOf(expectedType.getClass());
+        assertThat(expression.calculate()).isEqualTo(expectedType.calculate());
+    }
+
+    static Stream<Arguments> someMarkMustReturnExpressionType(){
+        return Stream.of(
+            Arguments.arguments("12+", new AddExpression(VARIABLE_ONE,VARIABLE_TWO)),
+            Arguments.arguments("12-", new MinusExpression(VARIABLE_ONE,VARIABLE_TWO)),
+            Arguments.arguments("12*", new MultiplicationExpression(VARIABLE_ONE,VARIABLE_TWO)),
+            Arguments.arguments("12/", new DivideExpression(VARIABLE_ONE,VARIABLE_TWO))
+        );
+    }
+
 }
