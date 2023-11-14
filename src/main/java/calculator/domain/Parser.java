f674c4a15140e2f5495827c764d594a9dcdf9143
pilming44 <pilming44@naver.com>
2023-11-15 01:11:43 +0900

    refactor, test: 리팩토링 및 테스트코드 수정

---
+++ src/main/java/calculator/domain/Parser.java
@@ -0,0 +1,58 @@
+package calculator.domain;
+
+import calculator.operatorenum.OperatorEnum;
+
+import java.util.*;
+
+public class Parser {
+    public static List<String> split(String text) {
+        String[] splitText = text.split(" ");
+        return Arrays.asList(splitText);
+    }
+
+    public static Deque<Integer> numberParsing(List<String> texts) {
+        Deque<Integer> numbers = new ArrayDeque<>();
+
+        for(int evenIndex = 0; evenIndex < texts.size(); evenIndex += 2) {
+            int number = parseNumberFormat(texts.get(evenIndex));
+            numbers.addLast(number);
+        }
+
+        return numbers;
+    }
+
+    public static Deque<String> operatorParsing(List<String> texts) {
+        Deque<String> operators = new ArrayDeque<>();
+        for(int oddIndex = 1; oddIndex < texts.size(); oddIndex += 2) {
+            operatorCheck(texts.get(oddIndex));
+            operators.addLast(texts.get(oddIndex));
+        }
+
+        return operators;
+    }
+    private static int parseNumberFormat(String numberString) {
+        try {
+            int result = Integer.parseInt(numberString);
+            return result;
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자가 아닙니다", e);
+        }
+    }
+
+    private static void operatorCheck(String operator) {
+        if (!OperatorEnum.contains(operator)) {
+            throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
+        }
+    }
+
+    public static boolean emptyValidate(String text) {
+        if (empty(text)) {
+            throw new IllegalArgumentException("입력 값이 없습니다.");
+        }
+        return true;
+    }
+
+    private static boolean empty(String text) {
+        return text == null || text.isBlank();
+    }
+}
--- src/test/java/calculator/domain/ParserTest.java
+++ src/test/java/calculator/domain/ParserTest.java
@@ -1,15 +1,15 @@
-package calculator;
+package calculator.domain;
 
+import calculator.domain.Parser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ParserTest {
 
@@ -40,42 +40,34 @@ class ParserTest {
     @DisplayName("숫자 순서에 숫자가 아닌경우 예외 발생")
     void 숫자_아님(){
         List<String> expression = Arrays.asList("4", "+", "*", "-", "5");
-        assertThatThrownBy(() -> Parser.numberReverseParsing(expression))
+        assertThatThrownBy(() -> Parser.numberParsing(expression))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("숫자 순서에 모두 숫자일경우 역순으로 적재된 스택 리턴")
+    @DisplayName("숫자 순서에 모두 숫자일경우 숫자가 담긴 deque 리턴")
     void 숫자파싱(){
         List<String> expression = Arrays.asList("4", "+", "3", "-", "5");
-        Stack<Integer> parsedNumberStack = Parser.numberReverseParsing(expression);
-        Stack<Integer> expectedStack = new Stack<>();
+        Deque<Integer> parsedNumberDeque = Parser.numberParsing(expression);
+        Deque<Integer> expectedDeque = new ArrayDeque<>();
 
-        expectedStack.push(5);
-        expectedStack.push(3);
-        expectedStack.push(4);
+        expectedDeque.addLast(4);
+        expectedDeque.addLast(3);
+        expectedDeque.addLast(5);
 
-        assertThat(parsedNumberStack.size()).isEqualTo(expectedStack.size());
-
-        for (int i = 0; i < expectedStack.size(); i++) {
-            assertThat(parsedNumberStack.get(i)).isEqualTo(expectedStack.get(i));
-        }
+        assertThat(parsedNumberDeque).containsExactlyElementsOf(expectedDeque);
     }
 
     @Test
-    @DisplayName("연산자 순서에 모두 연산자일경우 역순으로 적재된 스택 리턴")
+    @DisplayName("연산자 순서에 모두 연산자일경우 연산자가 담긴 deque 리턴")
     void 연산자파싱(){
         List<String> expression = Arrays.asList("4", "+", "3", "-", "5");
-        Stack<String> parsedOperatorStack = Parser.operatorParsing(expression);
-        Stack<String> expectedStack = new Stack<>();
+        Deque<String> parsedOperatorDeque = Parser.operatorParsing(expression);
+        Deque<String> expectedDeque = new ArrayDeque<>();
 
-        expectedStack.push("-");
-        expectedStack.push("+");
+        expectedDeque.addLast("+");
+        expectedDeque.addLast("-");
 
-        assertThat(parsedOperatorStack.size()).isEqualTo(expectedStack.size());
-
-        for (int i = 0; i < expectedStack.size(); i++) {
-            assertThat(parsedOperatorStack.get(i)).isEqualTo(expectedStack.get(i));
-        }
+        assertThat(parsedOperatorDeque).containsExactlyElementsOf(expectedDeque);
     }
 }
