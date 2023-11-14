329bfb9a0fdd9986c8a237a35a0d97702f4ee580
jonghunbaek <qor6028@naver.com>
2023-11-14 11:59:43 +0900

    feat : 복권 등수를 표현하는 enum추가
    - 일치하는 번호 개수, 상금을 필드로 가진다.

---
+++ src/main/java/lotto/domain/LotteryRank.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+public enum LotteryRank {
+
+    FIRST(6, 2_000_000_000L),
+    SECOND(5, 1_500_000L),
+    THIRD(4, 50_000L),
+    FOURTH(3, 5_000L);
+
+    private int matchingCount;
+    private long prize;
+
+    LotteryRank(int matchingCount, long prize) {
+        this.matchingCount = matchingCount;
+        this.prize = prize;
+    }
+
+    public int matchingCount() {
+        return this.matchingCount;
+    }
+}
