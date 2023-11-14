2a4289fe33f9b85fe0d92610788d21ad619791c0
HyunggiPark <phg9587@naver.com>
2023-11-14 21:46:56 +0900

    test: findByCountAndBonusMatching 테스트 코드 분리
    - SoftAssertions 사용시 테스트 시간이 길어져서, 사용하는 것 대신에 테스트 코드를 분리

--- src/test/java/lotto/step2/domain/LottoRankTest.java
+++ src/test/java/lotto/step2/domain/LottoRankTest.java
@@ -3,21 +3,12 @@ package lotto.step2.domain;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoRankTest {
-    @Test
-    @DisplayName("findByCount의 입력으로 일치하는 개수를 넣으면, 해당하는 LottoRank 객체를 반환한다.")
-    void testFindByCount() {
-        SoftAssertions.assertSoftly(softly -> {
-                    softly.assertThat(LottoRank.findByCountAndBonusMatching(3, false)).isEqualTo(LottoRank.FIFTH);
-                    softly.assertThat(LottoRank.findByCountAndBonusMatching(4, false)).isEqualTo(LottoRank.FOURTH);
-                    softly.assertThat(LottoRank.findByCountAndBonusMatching(5, false)).isEqualTo(LottoRank.THIRD);
-                    softly.assertThat(LottoRank.findByCountAndBonusMatching(5, true)).isEqualTo(LottoRank.SECOND);
-                    softly.assertThat(LottoRank.findByCountAndBonusMatching(6, false)).isEqualTo(LottoRank.FIRST);
-                }
-        );
-    }
-
     @Test
     @DisplayName("hasMatchingCount를 사용하면, 각 LottoRank의 matchingCount가 0 초과인지 확인한다.")
     void testHasMatchingCount() {
@@ -45,5 +36,80 @@ class LottoRankTest {
                     softly.assertThat(LottoRank.FIRST.getDescription()).isEqualTo("1등");
                 }
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    @DisplayName("findByCountAndBonusMatching의 입력으로 5등에 해당하는 숫자 일치 개수를 넣으면, 보너스 일치 여부와 관계없이 5등에 해당하는 LottoRank 객체를 반환한다.")
+    void testFindByCountAndBonusMatchingForFifthRank(boolean isBonusMatch) {
+        // given
+        int matchingCount = LottoRank.FIFTH.getMatchingCount();
+        LottoRank expectedRank = LottoRank.FIFTH;
+
+        // when
+        LottoRank resultRank = LottoRank.findByCountAndBonusMatching(matchingCount, isBonusMatch);
+
+        // then
+        assertThat(resultRank).isEqualTo(expectedRank);
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    @DisplayName("findByCountAndBonusMatching의 입력으로 4등에 해당하는 숫자 일치 개수를 넣으면, 보너스 일치 여부와 관계없이 4등에 해당하는 LottoRank 객체를 반환한다.")
+    void testFindByCountAndBonusMatchingForFourthRank(boolean isBonusMatch) {
+        // given
+        int matchingCount = LottoRank.FOURTH.getMatchingCount();
+        LottoRank expectedRank = LottoRank.FOURTH;
+
+        // when
+        LottoRank resultRank = LottoRank.findByCountAndBonusMatching(matchingCount, isBonusMatch);
+
+        // then
+        assertThat(resultRank).isEqualTo(expectedRank);
+    }
+
+    @Test
+    @DisplayName("findByCountAndBonusMatching의 입력으로 3등에 해당하는 숫자 일치 개수를 넣고 보너스가 일치하지 않으면, 3등에 해당하는 LottoRank 객체를 반환한다.")
+    void testFindByCountAndBonusMatchingForThirdRank() {
+        // given
+        int matchingCount = LottoRank.THIRD.getMatchingCount();
+        boolean isBonusMatch = false;
+        LottoRank expectedRank = LottoRank.THIRD;
+
+        // when
+        LottoRank resultRank = LottoRank.findByCountAndBonusMatching(matchingCount, isBonusMatch);
+
+        // then
+        assertThat(resultRank).isEqualTo(expectedRank);
+    }
+
+    @Test
+    @DisplayName("findByCountAndBonusMatching의 입력으로 2등에 해당하는 숫자 일치 개수를 넣고 보너스가 일치하면, 2등에 해당하는 LottoRank 객체를 반환한다.")
+    void testFindByCountAndBonusMatchingForSecondRank() {
+        // given
+        int matchingCount = LottoRank.SECOND.getMatchingCount();
+        boolean isBonusMatch = true;
+        LottoRank expectedRank = LottoRank.SECOND;
+
+        // when
+        LottoRank resultRank = LottoRank.findByCountAndBonusMatching(matchingCount, isBonusMatch);
+
+        // then
+        assertThat(resultRank).isEqualTo(expectedRank);
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    @DisplayName("findByCountAndBonusMatching의 입력으로 1등에 해당하는 숫자 일치 개수를 넣으면, 보너스 일치 여부와 관계없이 1등에 해당하는 LottoRank 객체를 반환한다.")
+    void testFindByCountAndBonusMatchingForFirstRank(boolean isBonusMatch) {
+        // given
+        int matchingCount = LottoRank.FIRST.getMatchingCount();
+        LottoRank expectedRank = LottoRank.FIRST;
+
+        // when
+        LottoRank resultRank = LottoRank.findByCountAndBonusMatching(matchingCount, isBonusMatch);
+
+        // then
+        assertThat(resultRank).isEqualTo(expectedRank);
     }
 }
