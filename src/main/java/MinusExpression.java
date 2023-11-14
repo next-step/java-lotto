7d8b88e55008646f13b6fa47f901bda051726c20
choomi1217 <whdudal1217@naver.com>
2023-11-14 22:54:02 +0900

    feat:뺄셈 기능 구현

---
+++ src/main/java/MinusExpression.java
@@ -0,0 +1,15 @@
+public class MinusExpression {
+
+    private final int left;
+    private final int right;
+
+    public MinusExpression(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+
+    public int calculate() {
+        return left - right;
+    }
+}
---
+++ src/test/java/MinusExpressionTest.java
@@ -0,0 +1,14 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MinusExpressionTest {
+
+    @DisplayName("뺄셈을 합니다.")
+    @Test
+    void plusCalculate(){
+        MinusExpression minusExpression = new MinusExpression(5, 2);
+        int result = minusExpression.calculate();
+        Assertions.assertThat(result).isEqualTo(3);
+    }
+}
