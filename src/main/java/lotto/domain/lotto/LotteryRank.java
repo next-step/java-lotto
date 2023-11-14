67e94ef4ffac3358c662c2ffe4eb8b57632296b9
jonghunbaek <qor6028@naver.com>
2023-11-14 19:38:07 +0900

    refactor : 등수 순서 변경

--- src/main/java/lotto/domain/lotto/LotteryRank.java
+++ src/main/java/lotto/domain/lotto/LotteryRank.java
@@ -2,10 +2,10 @@ package lotto.domain.lotto;
 
 public enum LotteryRank {
 
-    FIRST(6, 2_000_000_000L),
-    SECOND(5, 1_500_000L),
+    FOURTH(3, 5_000L),
     THIRD(4, 50_000L),
-    FOURTH(3, 5_000L);
+    SECOND(5, 1_500_000L),
+    FIRST(6, 2_000_000_000L);
 
     private int matchingCount;
     private long prize;
