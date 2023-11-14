ccca2fbcb1cd54cf24a92b487a6c0a50f217052b
pilming44 <pilming44@naver.com>
2023-11-15 01:09:26 +0900

    feat, test: 계산을 담당하는 OperatorEnum 생성

---
+++ src/main/java/calculator/operatorenum/OperatorEnum.java
@@ -0,0 +1,32 @@
+package calculator.operatorenum;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum OperatorEnum {
+    PLUS("+", (firstNumber, secondNumber) -> firstNumber + secondNumber),
+    MINUS("-", (firstNumber, secondNumber) -> firstNumber - secondNumber),
+    MULTIPLY("*", (firstNumber, secondNumber) -> firstNumber * secondNumber),
+    DIVIDE("/", (firstNumber, secondNumber) -> firstNumber / secondNumber);
+
+    private String operator;
+    private BiFunction<Integer, Integer, Integer> function;
+    OperatorEnum(String operator, BiFunction<Integer, Integer, Integer> function) {
+        this.operator = operator;
+        this.function = function;
+    }
+
+    public static int calculate(String operator, int firstNumber, int secondNumber) {
+        return Arrays.stream(values())
+                .filter(enumValue -> enumValue.operator.equals(operator))
+                .findFirst()
+                .map(enumValue -> enumValue.function.apply(firstNumber, secondNumber))
+                .orElseThrow(() -> new IllegalArgumentException("Unsupported operator: " + operator));
+    }
+
+    public static boolean contains(String value) {
+        return Arrays.stream(values())
+                .anyMatch(operatorEnum -> operatorEnum.operator.equals(value));
+    }
+
+}
---
+++ src/test/java/calculator/operatorenum/OperatorEnumTest.java
@@ -0,0 +1,44 @@
+package calculator.operatorenum;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class OperatorEnumTest {
+    @DisplayName("2개의 값을 더한다.")
+    @ParameterizedTest(name = "2개의 값을 더한다.")
+    @CsvSource(value = {"4:2:+:6", "3:1:+:4", "7:2:+:9"}, delimiter = ':')
+    void add(int firstInput, int secondInput, String operator, int excepted) {
+        assertThat(OperatorEnum.calculate(operator, firstInput, secondInput)).isEqualTo(excepted);
+    }
+
+    @DisplayName("두개의 값을 뺀다.")
+    @ParameterizedTest(name = "두개의 값을 뺀다.")
+    @CsvSource(value = {"4:2:-:2", "3:1:-:2", "7:2:-:5"}, delimiter = ':')
+    void minus(int firstInput, int secondInput, String operator, int excepted) {
+        assertThat(OperatorEnum.calculate(operator, firstInput, secondInput)).isEqualTo(excepted);
+    }
+
+    @DisplayName("두개의 값을 곱한다.")
+    @ParameterizedTest(name = "두개의 값을 곱한다.")
+    @CsvSource(value = {"4:2:*:8", "3:1:*:3", "7:2:*:14"}, delimiter = ':')
+    void multiply(int firstInput, int secondInput, String operator, int excepted) {
+        assertThat(OperatorEnum.calculate(operator, firstInput, secondInput)).isEqualTo(excepted);
+    }
+
+    @DisplayName("두개의 값을 나눈다")
+    @ParameterizedTest
+    @CsvSource(value = {"4:2:/:2", "3:1:/:3", "8:2:/:4"}, delimiter = ':')
+    void divide(int firstInput, int secondInput, String operator, int excepted) {
+        assertThat(OperatorEnum.calculate(operator, firstInput, secondInput)).isEqualTo(excepted);
+    }
+
+    @DisplayName("연산자에 해당하는지 확인한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"+:true", "-:true", "*:true", "/:true", "&:false", "4:false"}, delimiter = ':')
+    void 연산자_확인(String operator, boolean expectedBoolean) {
+        assertThat(OperatorEnum.contains(operator)).isEqualTo(expectedBoolean);
+    }
+}
