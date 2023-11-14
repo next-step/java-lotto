23151901f324bc2133a6c86e8f5a5825cdee1c69
choomi1217 <whdudal1217@naver.com>
2023-11-14 22:57:41 +0900

    test: 테스트 메소드명 변경'

--- src/test/java/MultiplicationExpressionTest.java
+++ src/test/java/MultiplicationExpressionTest.java
@@ -2,11 +2,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class MinusExpressionTest {
+public class MultiplicateExpressionTest {
 
     @DisplayName("뺄셈을 합니다.")
     @Test
-    void minusCalculate(){
+    void plusCalculate(){
         MinusExpression minusExpression = new MinusExpression(5, 2);
         int result = minusExpression.calculate();
         Assertions.assertThat(result).isEqualTo(3);
