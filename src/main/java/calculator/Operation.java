83e3b8595ac63adb511ad0085df91c9a2b0b41ea
박상권 <79737870+pawoo0211@users.noreply.github.com>
2023-11-14 12:00:49 +0900

    1단계 - 문자열 계산기 (#3353)
    * [step1] test: 문자열 계산기 테스트 코드 작성
    
    * [step1] feat: 문자열 계산기 기능 구현
    
    * [step1] test: 접근제어자 public 제거
    
    * [step1] refactor: Pattern 객체 상수화
    
    * [step1] refactor: 불필요한 변수 제거
    
    * [step1] refactor: else문 제거
    
    * [step1] refactor: 계산기 패키지 생성

---
+++ src/main/java/calculator/Operation.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Operation {
+    ADD("+") {
+        public int apply(int number1, int number2) {
+            return number1 + number2;
+        }
+    },
+    SUBTRACT("-") {
+        public int apply(int number1, int number2) {
+            return number1 - number2;
+        }
+    },
+    MULTIPLY("*") {
+        public int apply(int number1, int number2) {
+            return number1 * number2;
+        }
+    },
+    DIVIDE("/") {
+        public int apply(int number1, int number2) {
+            if (number2 == 0) {
+                throw new IllegalArgumentException("Cannot divide by zero");
+            }
+            return number1 / number2;
+        }
+    };
+
+    private static Map<String, Operation> OPERATION_MAP = new HashMap<>();
+    private String operation;
+
+    Operation(String operation) {
+        this.operation = operation;
+    }
+
+    static {
+        for (Operation operation : values()) {
+            OPERATION_MAP.put(operation.operation, operation);
+        }
+    }
+
+    public static Operation getOperationByElement(String element) {
+        return OPERATION_MAP.get(element);
+    }
+
+    public abstract int apply(int number1, int number2);
+}
---
+++ src/main/java/calculator/Operations.java
@@ -0,0 +1,36 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.List;
+
+public class Operations {
+    private static final Pattern OPERATION_PATTERN = Pattern.compile("[+\\-*/]");
+
+    private List<Operation> operationList = new ArrayList<>();
+
+    public void addIfOperation(String element) {
+        if (isOperation(element)) {
+            operationList.add(Operation.getOperationByElement(element));
+        }
+    }
+
+    public boolean isOperation(String element) {
+        Matcher operationMatcher = OPERATION_PATTERN.matcher(element);
+        if (operationMatcher.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    public final List<Operation> findAllOperation() {
+        return Collections.unmodifiableList(operationList);
+    }
+
+    public int getLengthOfOperations() {
+        return operationList.size();
+    }
+}
