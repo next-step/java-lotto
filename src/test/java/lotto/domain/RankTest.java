61bae87bf0b351eba46b738fc2b811be62bdd9f8
엄태권 <akfls221@gmail.com>
2023-11-15 04:13:29 +0900

    fix : Rank 변경에 따른 Rank 변경 테스트 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -8,10 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RankTest {
 
-    @ParameterizedTest(name = "[{index}] 맞춘 숫자가 {0}개면, Rank는 {1}")
-    @CsvSource({"6, FIRST", "5, SECOND", "4, THIRD", "3, FOURTH"})
-    void rank_맞춘숫자_결과(int matchCount, Rank expected) {
-        Rank findRank = Rank.findRank(matchCount);
+    @ParameterizedTest(name = "[{index}] 맞춘 숫자가 {0}개 보너스 여부가 {1}면, Rank는 {2} 이다")
+    @CsvSource({
+            "6, false, FIRST",
+            "5, true, SECOND",
+            "5, false, THIRD",
+            "4, false, FOURTH",
+            "3, false, FIFTH"})
+    void rank_맞춘숫자_결과(int matchCount, boolean isBonus, Rank expected) {
+        Rank findRank = Rank.findRank(matchCount, isBonus);
+
+        assertThat(findRank).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "[{index}] 맞춘 숫자가 3이하의 숫자({0}개) 보너스 여부가 {1}면, Rank는 {2} 이다")
+    @CsvSource({"0, false, NONE", "1, false, NONE", "2, false, NONE",})
+    void rank_등수에_없는결과(int matchCount, boolean isBonus, Rank expected) {
+        Rank findRank = Rank.findRank(matchCount, isBonus);
 
         assertThat(findRank).isEqualTo(expected);
     }
