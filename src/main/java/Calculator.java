946e413ed282caa8dfd1424e572b0858e12c7eeb
tgHan <hantaegune@mf.seegene.com>
2023-11-14 13:58:27 +0900

    if, for 등 공백 컨벤션

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,11 +14,11 @@ public class Calculator {
         ArrayList<Integer> valueList = filterValue(strList);
         ArrayList<String> opsList = filterOps(strList);
 
-        if(valueList.size()==0){
+        if (valueList.size() == 0){
             throw new IllegalArgumentException("연산 할 값이 없습니다.");
         }
 
-        if(valueList.size()!=opsList.size()+1){
+        if (valueList.size() != opsList.size()+1){
             throw new IllegalArgumentException("계산식이 잘못 되였습니다.");
         }
 
@@ -28,7 +28,7 @@ public class Calculator {
         /**
          * 계산 공식 돌리기
          */
-        for(int i = 0 ; i < opsList.size() ; i++){
+        for (int i = 0 ; i < opsList.size() ; i++){
             resultValue = getResultValue(valueList.get(i +1), opsList.get(i), resultValue);
         }
 
@@ -53,16 +53,16 @@ public class Calculator {
         /**
          * indent(들여쓰기) depth를 2단계에서 1단계로
          */
-        if("+".equals(ops)){
+        if ("+".equals(ops)){
             resultValue = add(resultValue, value);
         }
-        if("-".equals(ops)){
+        if ("-".equals(ops)){
             resultValue = subtract(resultValue, value);
         }
-        if("*".equals(ops)){
+        if ("*".equals(ops)){
             resultValue = multiply(resultValue, value);
         }
-        if("/".equals(ops)){
+        if ("/".equals(ops)){
             resultValue = divide(resultValue, value);
         }
 
@@ -73,7 +73,7 @@ public class Calculator {
         ArrayList<String> opsList = new ArrayList<String>();
 
         IntStream.range(0, inputList.length).forEach(idx -> {
-            if(idx%2!=0 && checkOps(inputList[idx])){
+            if (idx%2 !=0 && checkOps(inputList[idx])){
                 opsList.add(inputList[idx]);
             }
         });
@@ -95,7 +95,7 @@ public class Calculator {
         ArrayList<Integer> valueList = new ArrayList<Integer>();
 
         IntStream.range(0, inputList.length).forEach(idx -> {
-            if(idx%2==0){
+            if (idx%2 == 0){
                 valueList.add(convertNum(inputList[idx]));
             }
         });
@@ -130,14 +130,14 @@ public class Calculator {
 
     public static int divide(int number1, int number2){
 
-        if(!checkDivideValue(number1,number2)){
+        if (!checkDivideValue(number1,number2)){
             throw new IllegalArgumentException("나눗셈 결과 값이 정수로 떨어지지 않습니다.");
         }
         return number1 / number2;
     }
 
     public static boolean checkDivideValue(int number1, int number2){
-        if(number1 % number2!=0){
+        if (number1 % number2 != 0){
             return false;
         }
         return true;
