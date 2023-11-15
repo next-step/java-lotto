7936bce651dbeba82be2295c46a3accec2b9a156
jonghunbaek <qor6028@naver.com>
2023-11-15 22:58:15 +0900

    feat : LottoNumbers 일급 컬렉션 생성 시 번호 개수 확인하는 검증 추가

--- src/main/java/lotto/domain/lotto/wrapper/LottoNumbers.java
+++ src/main/java/lotto/domain/lotto/wrapper/LottoNumbers.java
@@ -11,8 +11,22 @@ public class LottoNumbers {
     public LottoNumbers() {
     }
 
-    public LottoNumbers(List<Integer> inputs) {
-        inputs.forEach(input -> lottoNumberses.add(new LottoNumber(input)));
+    public LottoNumbers(List<Integer> numbers) {
+        validateAmountOfNumbers(numbers);
+
+        numbers.forEach(number -> lottoNumberses.add(new LottoNumber(number)));
+    }
+
+    private void validateAmountOfNumbers(List<Integer> numbers) {
+        if (removeDuplicate(numbers).size() != 6) {
+            throw new IllegalArgumentException("로또 당첨 번호는 6개가 입력되어야 합니다.");
+        }
+    }
+
+    private List<Integer> removeDuplicate(List<Integer> inputs) {
+        return inputs.stream()
+            .distinct()
+            .collect(Collectors.toUnmodifiableList());
     }
 
     public List<Integer> numbers() {
