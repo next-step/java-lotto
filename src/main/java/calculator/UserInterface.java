dc71119ec815e6e9f34781e7527264971f87639c
pilming44 <pilming44@naver.com>
2023-11-15 23:36:02 +0900

    style: UserInterface 오타 수정

--- src/main/java/calculator/UserInterface.java
+++ src/main/java/calculator/UserInterface.java
@@ -4,7 +4,7 @@ import calculator.domain.Calculator;
 import calculator.view.InputView;
 import calculator.view.ResultView;
 
-public class UserInteface {
+public class UserInterface {
     public static void main(String[] args) {
         String expression = InputView.expression();
         Calculator calculator = new Calculator(expression);
