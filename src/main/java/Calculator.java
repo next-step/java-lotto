7cc7be923084498c7e1482ec662fc51018b59dd1
sunga <eert2260@naver.com>
2023-11-16 01:21:28 +0900

    feat : 문자열 계산 결과 반환 및 사칙 연산 테스트 변경

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,46 @@
+import java.util.List;
+
 public class Calculator {
-    public static int sum(int number1, int number2){
+    private static final String SUM_DELIMITER = "+";
+    private static final String SUBTRACTION_DELIMITER = "-";
+    private static final String MULTIPLICATION_DELIMITER = "*";
+    private static final String DIVISION_DELIMITER = "/";
+
+    public static int stringCalculate(String inputString) {
+        List<Integer> numberList = SplitString.splitNumber(inputString);
+        List<String> operatorList = SplitString.splitOperator(inputString);
+
+        int result = numberList.get(0);
+
+        for(int i=0; i<operatorList.size(); i++){
+            result = operate(operatorList.get(i), result, numberList.get(i +1));
+        }
+
+        return result;
+    }
+
+    public static int operate(String operator, int result, int operand) {
+        if(operator.equals(SUM_DELIMITER)) return sum(result, operand);
+        if(operator.equals(SUBTRACTION_DELIMITER)) return subtraction(result, operand);
+        if(operator.equals(MULTIPLICATION_DELIMITER)) return multiplication(result, operand);
+        if (operator.equals(DIVISION_DELIMITER)) return division(result, operand);
+
+        return 0;
+    }
+
+    private static int sum(int number1, int number2){
         return number1 + number2;
     }
 
-    public static int subtraction(int number1, int number2){
+    private static int subtraction(int number1, int number2){
         return number1 - number2;
     }
 
-    public static int multiplication(int number1, int number2) {
+    private static int multiplication(int number1, int number2) {
         return number1 * number2;
     }
 
-    public static int division(int number1, int number2) {
+    private static int division(int number1, int number2) {
         return number1 / number2;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,10 +2,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CalculatorTest {
 
-    @Test
+    /*@Test
     @DisplayName("두 숫자를 입력하면 덧셈 연산을 수행한다.")
     void 덧셈연산(){
         assertThat(Calculator.sum(2, 3)).isEqualTo(5);
@@ -27,5 +28,24 @@ public class CalculatorTest {
     @DisplayName("두 숫자를 입력하면 나눗셈 연산을 수행한다.")
     void 나눗셈연산(){
         assertThat(Calculator.division(7, 3)).isEqualTo(2);
+    }*/
+
+    @Test
+    @DisplayName("연산자와 두 숫자를 입력하면 연산을 수행한다.")
+    void 사칙연산_수행(){
+        assertAll(
+                () -> assertThat(Calculator.operate("+",2, 3)).isEqualTo(5),
+                () -> assertThat(Calculator.operate("-",7, 3)).isEqualTo(4),
+                () -> assertThat(Calculator.operate("*",2, 3)).isEqualTo(6),
+                () -> assertThat(Calculator.operate("/",7, 3)).isEqualTo(2)
+        );
+    }
+
+    @Test
+    @DisplayName("")
+    void 문자열_계산결과_반환(){
+        String inputString = "2 + 3 * 4 / 2";
+
+        assertThat(Calculator.stringCalculate(inputString)).isEqualTo(10);
     }
 }
