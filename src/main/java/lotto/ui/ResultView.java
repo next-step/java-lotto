f49646d4fd33a71142a603c44d2176d15f9470a3
pawoo0211 <pawoo0211@naver.com>
2023-11-15 20:57:27 +0900

    [step2] feature: 총 수익률 UI 구현

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -9,13 +9,21 @@ public class ResultView {
 
     public void showResult(LottoResponse response) {
         System.out.println("당첨 통계");
-        System.out.println("3개 일치 (5000원)- 1개");
         HashMap<WinningAmount, Integer> countAllWinning = response.getCountAllWinning();
         for (WinningAmount winningAmount : countAllWinning.keySet()) {
-            int matchCount = winningAmount.getMatchCount();
-            int amount = winningAmount.getAmount();
-            int count = countAllWinning.get(winningAmount);
-            System.out.println(matchCount + "개 일치(" + amount + ")-" + count + "개");
+            printWinningResult(countAllWinning, winningAmount);
         }
+        printRateOfReturn(response.getRateOfReturn());
+    }
+
+    private void printWinningResult(HashMap<WinningAmount, Integer> countAllWinning, WinningAmount winningAmount) {
+        int matchCount = winningAmount.getMatchCount();
+        int amount = winningAmount.getAmount();
+        int count = countAllWinning.get(winningAmount);
+        System.out.println(matchCount + "개 일치(" + amount + ")-" + count + "개");
+    }
+
+    private void printRateOfReturn(int rateOfReturn) {
+        System.out.println("총 수익률 : " + rateOfReturn);
     }
 }
