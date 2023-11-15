7f50090127398a5ac3d1dfdee931e1f5f499680b
hyunyyxp <qqqq_o@naver.com>
2023-11-15 15:15:13 +0900

    refactor: 0으로 나눴을 시 예외 반환하는 로직 추가

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -1,5 +1,7 @@
 package calculator.domain;
 
+import calculator.exceptions.DivideByZeroException;
+
 import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
 
@@ -7,7 +9,16 @@ public enum Operator {
     PLUS("+", (x, y) -> x + y),
     MINUS("-", (x, y) -> x - y),
     MULTIPLY("*", (x, y) -> x * y),
-    DIVIDE("/", (x, y) -> x / y);
+    DIVIDE("/", (x, y) -> {
+        validateDivideByZero(y);
+        return x / y;
+    });
+
+    private static void validateDivideByZero(int y) {
+        if (y == 0) {
+            throw new DivideByZeroException();
+        }
+    }
 
     private static final Operator[] VALUES = values();
 
@@ -19,7 +30,7 @@ public enum Operator {
         this.op = op;
     }
 
-    public int calculate(int x, int y) {
+    public int operate(int x, int y) {
         return op.applyAsInt(x, y);
     }
 
