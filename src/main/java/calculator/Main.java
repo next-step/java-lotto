e5885bda60b7c48d20a7a2d42c8e02832f3c7726
jhest <jhest@sk.com>
2023-11-15 10:08:12 +0900

    [step01][refactor] PR feedback - main logic seperation

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import calculator.domain.Operator;
+import calculator.domain.Calculator;
 import calculator.domain.Seperator;
 import calculator.view.View;
 
@@ -9,27 +9,15 @@ public class Main {
     public static void main(String[] args) {
 
         View view = new View();
+        Calculator calculator = new Calculator();
 
         //계산식 입력
         String input = view.getString();
 
         //입력 문자열 분리 & 계산
-        Integer result = calculation(new Seperator().split(input));
+        Integer result = calculator.calculation(new Seperator().split(input));
 
         //결과 출력
         view.printResult(result);
-    }
-
-    private static Integer calculation(String[] text) {
-        Operator operator = new Operator();
-        Seperator seperator = new Seperator();
-        Integer result = Integer.parseInt(text[0]);
-
-        for (int i = 1; i <= Math.floor((double) text.length / 2); i++) {
-            seperator.isOperator(text[2 * i - 1]);
-            result = operator.operation(text[2 * i - 1], String.valueOf(result), text[2 * i]);
-        }
-
-        return result;
     }
 }
--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -35,17 +35,4 @@ class OperatorTest {
         int result = operator.div("4", "2");
         assertThat(result).isEqualTo(2);
     }
-
-    @Test
-    void 사칙연산() {
-        String[] input = {"2", "+", "3", "*", "4", "/", "2"};
-        Operator operator = new Operator();
-        int result = Integer.parseInt(input[0]);
-
-        for (int i = 1; i <= Math.floor((double) input.length / 2); i++) {
-            result = operator.operation(input[2 * i - 1], String.valueOf(result), input[2 * i]);
-        }
-
-        assertThat(result).isEqualTo(10);
-    }
 }
