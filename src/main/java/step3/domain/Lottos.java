62aa9bac5515cc51405b750fb35f8c07bca2cd27
sukhyunmoon <sukhyunmoon@sk.com>
2023-11-15 23:34:17 +0900

    bugfix: 0개 매칭되는 RANK에 대해 미출력되는 현상 해결

--- src/main/java/step3/domain/Lottos.java
+++ src/main/java/step3/domain/Lottos.java
@@ -1,6 +1,7 @@
 package step3.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -20,11 +21,17 @@ public class Lottos {
     }
 
     public LinkedHashMap<LottoRank, Integer> matchingResult(WinningNumbers winningNumbers) {
-        LinkedHashMap<LottoRank, Integer> result = new LinkedHashMap<>();
+        LinkedHashMap<LottoRank, Integer> result = initResult();
         lottos.stream()
                 .map(lotto -> LottoRank.valueOf(winningNumbers.matchCount(lotto), winningNumbers.containBonus(lotto)))
                 .filter(LottoRank::isNotMiss)
                 .forEach(lottoRank -> result.put(lottoRank, result.getOrDefault(lottoRank, 0) + 1));
+        return result;
+    }
+
+    private static LinkedHashMap<LottoRank, Integer> initResult() {
+        LinkedHashMap<LottoRank, Integer> result = new LinkedHashMap<>();
+        Arrays.stream(LottoRank.values()).forEach(lottoRank -> result.put(lottoRank, 0));
         return result;
     }
 }
