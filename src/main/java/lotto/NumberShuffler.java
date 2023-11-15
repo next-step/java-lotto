85e8829d865575c6276dd9deada65506bee4f987
안현호(Hyeonho Ahn)/Biz플랫폼개발팀/SKP <hhahn@sk.com>
2023-11-15 15:21:45 +0900

    feat: 출력기능 및 로또 번호 자동생성 및 정렬 기능 구현

--- src/main/java/lotto/NumberShuffler.java
+++ src/main/java/lotto/NumberShuffler.java
@@ -1,4 +1,32 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class NumberShuffler {
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private static final int[] LOTTO_NUMBERS = IntStream.rangeClosed(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER).toArray();
+
+    private NumberShuffler() {
+    }
+
+    public static int[] getShuffledNumbers() {
+        List<Integer> shuffledNumbers = toIntegerList(LOTTO_NUMBERS);
+        Collections.shuffle(shuffledNumbers);
+        return toIntArray(shuffledNumbers.subList(0, 6));
+    }
+
+    private static List<Integer> toIntegerList(int[] array) {
+        return Arrays.stream(array).boxed().collect(Collectors.toList());
+    }
+
+    private static int[] toIntArray(List<Integer> list) {
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,4 +1,27 @@
 package lotto.domain;
 
+import java.util.Arrays;
+
 public class Lotto {
+
+    private final int[] lottoNumbers = new int[6];
+
+    private Lotto() {
+    }
+
+    public static Lotto of(int[] lottoNumbers) {
+        Lotto lotto = new Lotto();
+        System.arraycopy(lottoNumbers, 0, lotto.lottoNumbers, 0, 6);
+        return lotto;
+    }
+
+    public int[] getLottoNumbers() {
+        sortLottoNumbers();
+        return lottoNumbers;
+    }
+
+    private void sortLottoNumbers() {
+        Arrays.sort(lottoNumbers);
+    }
+
 }
