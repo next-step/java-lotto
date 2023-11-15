74a00d2c68360e9b307ca5cc36c8c5cb0f9c786e
qwe5507 <apvmffkdls@gmail.com>
2023-11-15 22:16:28 +0900

    refactor: 변수명 변경 (purchaseAmount-> amount)

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -11,14 +11,14 @@ public class Lottos {
 
     private List<Lotto> lottos;
 
-    public Lottos(final Amount purchaseAmount) {
-        this.lottos = initLottos(purchaseAmount);
+    public Lottos(final Amount amount) {
+        this.lottos = initLottos(amount);
     }
 
-    private List<Lotto> initLottos(final Amount purchaseAmount) {
+    private List<Lotto> initLottos(final Amount amount) {
         final List<Lotto> lottos = new ArrayList<>();
 
-        final int purchaseCount = purchaseAmount.divideWithoutDecimal(new Amount(LOTTO_AMOUNT));
+        final int purchaseCount = amount.divideWithoutDecimal(new Amount(LOTTO_AMOUNT));
 
         for (int i = 0; i < purchaseCount; i++) {
             lottos.add(new Lotto());
