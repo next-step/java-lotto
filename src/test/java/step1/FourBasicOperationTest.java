25226c48179f8aa68ca7ab6379dcec9c0ca30d7b
jane096 <jes7077@naver.com>
2023-11-15 22:24:35 +0900

    FourBasicOperationTest CsvSource 활용해서 테스트코드 수정

--- src/test/java/step1/FourBasicOperationTest.java
+++ src/test/java/step1/FourBasicOperationTest.java
@@ -2,6 +2,7 @@ package step1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import step1.model.FourBasicOperation;
 
@@ -11,47 +12,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class FourBasicOperationTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"2 + 3"})
-    @DisplayName("덧셈 연산자를 테스트한다. 2 + 3을 넣을경우 5가 출력된다.")
-    void plusTest(String formula) {
+    @CsvSource(value = {"2 + 3=5", "7 - 3=4", "7 * 3=21", "4 / 2=2", "2 + 3 * 4 / 2=10"}, delimiter = '=')
+    @DisplayName("두개의 숫자 기준으로 사칙연산 결과 값을 테스트한다.")
+    void basicTest(String formula, int expected) {
         int result = new FourBasicOperation(formula).run();
-        assertThat(result).isEqualTo(5);
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"7 - 3"})
-    @DisplayName("뺄셈 연산자를 테스트한다. 7 - 3을 넣을경우 4가 출력된다.")
-    void takeOutTest(String formula) {
+    @CsvSource(value = {"2 + 4 / 2=3", "7 - 3 * 3=12", "2 + 3 * 4 / 2=10", "4 / 2 + 3 - 1 * 0=0", "4 - 2 + 4 / 2 * 3=9"}, delimiter = '=')
+    @DisplayName("사칙연산을 테스트한다. 수학의 사칙연산 규칙은 무시하고 순서대로 연산을 수행한다.")
+    void complicateTest(String formula, int expected) {
         int result = new FourBasicOperation(formula).run();
-        assertThat(result).isEqualTo(4);
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"7 * 3"})
-    @DisplayName("곱셈 연산자를 테스트한다. 7 * 3을 넣을경우 21이 출력된다.")
-    void multiplyTest(String formula) {
-        int result = new FourBasicOperation(formula).run();
-        assertThat(result).isEqualTo(21);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"4 / 2"})
-    @DisplayName("뺄셈 연산자를 테스트한다. 4 / 2를 넣을경우 2가 출력된다.")
-    void divisionTest(String formula) {
-        int result = new FourBasicOperation(formula).run();
-        assertThat(result).isEqualTo(2);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"2 + 3 * 4 / 2"})
-    @DisplayName("수학의 우선순위를 무시하고 순서대로 연산을 수행한다.")
-    void formulaTest(String formula) {
-        int result = new FourBasicOperation(formula).run();
-        assertThat(result).isEqualTo(10);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"4 / 2 $ 3"})
+    @CsvSource(value = {"4 / 2 $ 3", "3 % 2 #", "8 & 0 ( 2", "1 + 2 @ 3", "9 ! 4 ; 2"})
     @DisplayName("사칙연산 기호가 아닐 경우 예외가 발생한다.")
     void invalidFormulaTest(String formula) {
         assertThatThrownBy(() -> new FourBasicOperation(formula).run())
