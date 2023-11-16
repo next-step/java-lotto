9d04eec266848ed3a90ee635d45e35dab7bf49fb
엄태권 <akfls221@gmail.com>
2023-11-16 16:35:19 +0900

    feat : LottoMachine 로또 생성로직 변경 및 테스트코드 작성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,12 @@
 package lotto.domain;
 
+import lotto.util.Parser;
+import lotto.util.Splitter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoMachine {
 
     private static final int LOTTO_PRICE = 1_000;
@@ -10,8 +17,27 @@ public class LottoMachine {
         this.lottos = Lottos.of(calcPossibleLottoCount(money));
     }
 
+    public LottoMachine(Money money, List<String> manualNumbers) {
+        int autoLottoCount = calcAutoLottoCount(money, manualNumbers.size());
+
+        List<List<Integer>> manualLottoNumbers = manualNumbers.stream()
+                .map(Splitter::splitString)
+                .map(
+                        lottoNumber -> Arrays.stream(lottoNumber)
+                        .map(Parser::parseInteger)
+                        .collect(Collectors.toList())
+                )
+                .collect(Collectors.toUnmodifiableList());
+
+        this.lottos = Lottos.createLottos(autoLottoCount, manualLottoNumbers);
+    }
+
     private int calcPossibleLottoCount(Money money) {
         return (int) money.getMoney() / LOTTO_PRICE;
+    }
+
+    private int calcAutoLottoCount(Money money, int manualLottoCount) {
+        return calcPossibleLottoCount(money) - manualLottoCount;
     }
 
     public Lottos getLottoTickets() {
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoMachineTest {
 
@@ -25,5 +28,16 @@ class LottoMachineTest {
         Lottos lottos = lottoMachine.getLottoTickets();
 
         assertThat(lottos.getLottos()).hasSize(14);
+    }
+
+    @Test
+    @DisplayName("10000원으로 Lotto를 생성할 때 수동이2개라면 자동은 8개가 생성된다.")
+    void createLotto_수동_자동() {
+        List<String> manualNumbers = List.of("1,2,3,4,5,6", "7,8,9,10,11,12");
+        LottoMachine lottoMachine = new LottoMachine(new Money(10_000), manualNumbers);
+        Lottos lottoTickets = lottoMachine.getLottoTickets();
+
+        assertThat(lottoTickets.getManualLottos()).hasSize(manualNumbers.size());
+        assertThat(lottoTickets.getLottos()).hasSize(10 - manualNumbers.size());
     }
 }
