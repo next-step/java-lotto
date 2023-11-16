6bc1e3a48f720d7768fcb39f53ede88ddd9f18f3
choomi1217 <whdudal1217@naver.com>
2023-11-16 23:43:35 +0900

    style: 사용하지 않는 import제거 및 newline at end of file

--- src/test/java/calculator/domain/ExpressionTest.java
+++ src/test/java/calculator/domain/ExpressionTest.java
@@ -2,9 +2,7 @@ package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
