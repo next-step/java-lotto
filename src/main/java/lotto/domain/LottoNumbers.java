a8cceb2aeca16ea01e5a4d2e8d706e705c28ebd3
kyunghyun-park <qqqq_o@naver.com>
2023-11-16 20:16:01 +0900

    feat, test: 로또 번호에 대한 유효성 검증 추가

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -1,17 +1,32 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class LottoNumbers {
+    private static final int NUMBERS_SIZE = 6;
     private final List<LottoNumber> numbers;
 
     public LottoNumbers(List<Integer> generatedNumbers) {
+        validateDuplicate(generatedNumbers);
+        validateSize(generatedNumbers);
         this.numbers = new ArrayList<>();
 
         for (int number : generatedNumbers) {
             this.numbers.add(new LottoNumber(number));
+        }
+    }
+
+    private void validateDuplicate(List<Integer> generatedNumbers) {
+        Set<Integer> numbersSet = new HashSet<>(generatedNumbers);
+
+        if(numbersSet.size() != generatedNumbers.size()) {
+            throw new IllegalArgumentException("중복된 번호가 있습니다.");
+        }
+    }
+
+    private void validateSize(List<Integer> generatedNumbers) {
+        if(generatedNumbers.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다. : " + generatedNumbers.size());
         }
     }
 
--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumbersTest {
 
@@ -17,4 +18,21 @@ class LottoNumbersTest {
 
         assertThat(lottoNumbers1).isEqualTo(lottoNumbers2);
     }
+
+    @Test
+    @DisplayName("로또 번호가 중복될 시 예외를 반환한다.")
+    void validate_duplicate() {
+        assertThatThrownBy(() -> new LottoNumbers(List.of(1, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 번호가 있습니다.");
+    }
+
+    @Test
+    @DisplayName("로또 번호가 6개가 아닐 시 예외를 반환한다.")
+    void validate_size() {
+        assertThatThrownBy(() -> new LottoNumbers(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또 번호는 6개여야 합니다.");
+    }
+
 }
