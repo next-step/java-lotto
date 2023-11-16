e05e9143f94d6374aa77610d2a84246e3e4d0bb4
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-16 11:22:22 +0900

    test: 사칙 연산 테스트 리팩토링

--- src/test/java/stringCalculator/BasicOperationsTest.java
+++ src/test/java/stringCalculator/BasicOperationsTest.java
@@ -2,19 +2,32 @@ package stringCalculator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BasicOperationsTest {
 
-	@Test
+	@ParameterizedTest
+	@MethodSource("provideOperatorAndFunction")
 	@DisplayName("사칙 연산 기호를 찾아 반환합니다.")
-	void return_matched_basic_operation() {
-		assertThat(BasicOperations.match("+")).isEqualTo(BasicOperations.PLUS);
-		assertThat(BasicOperations.match("-")).isEqualTo(BasicOperations.SUBTRACT);
-		assertThat(BasicOperations.match("*")).isEqualTo(BasicOperations.MULTIPLY);
-		assertThat(BasicOperations.match("/")).isEqualTo(BasicOperations.DIVIDE);
+	void return_matched_basic_operation(String operator, BasicOperations expected) {
+		assertThat(BasicOperations.match(operator)).isEqualTo(expected);
 	}
+
+	private static Stream<Arguments> provideOperatorAndFunction() {
+		return Stream.of(
+			Arguments.of("+", BasicOperations.PLUS),
+			Arguments.of("-", BasicOperations.SUBTRACT),
+			Arguments.of("*", BasicOperations.MULTIPLY),
+			Arguments.of("/", BasicOperations.DIVIDE)
+		);
+	}
+
 
 	@Test
 	@DisplayName("사칙 연산 기호에 따른 연산을 합니다.")
