d86e0dc40388ac89c85a00bf10d22a25d8d1b362
엄태권 <akfls221@gmail.com>
2023-11-16 16:35:51 +0900

    fix : 불필요 import 제거

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoMachineTest {
 
