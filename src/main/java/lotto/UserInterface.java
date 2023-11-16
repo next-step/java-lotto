7f26941889ed4ff2990d826babf291045428072a
pilming44 <pilming44@naver.com>
2023-11-17 00:45:44 +0900

    feat: view영역 추가

--- src/main/java/lotto/UserInterface.java
+++ src/main/java/lotto/UserInterface.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lotto.domain.Lottos;
+import lotto.domain.Parser;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class UserInterface {
     public static void main(String[] args) {
-
+        int money = Parser.numberParsing(InputView.purchaseMoney());
+        ResultView.purchaseCount(money);
+        Lottos lottos = new Lottos(money);
+        ResultView.lottos(lottos);
+        String winningNumber = InputView.winningNumber();
+        System.out.println();
+        ResultView.winningStaticsMessage();
+        for (int count = 3; count <= 6; count++) {
+            ResultView.winningStatics(count, lottos.winningCount(winningNumber, count));
+        }
+        ResultView.rateOfReturn(lottos.rateOfReturn(winningNumber));
     }
 }
