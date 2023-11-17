d5c533ce3359e95258f99f2e4798a1ca98be9e15
hhj <abc@def.com>
2023-11-17 15:59:00 +0900

    refactor: 빈 줄을 코드의 맥락을 구분하는 곳에만 사용되게 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,34 +1,27 @@
 package lotto.controller;
 
-import lotto.domain.Lotto;
 import lotto.domain.LottoGame;
 import lotto.domain.LottoShuffleStrategy;
+import lotto.domain.Rank;
 import lotto.domain.ResultWinner;
 import lotto.ui.InputView;
 import lotto.ui.ResultView;
 
-import javax.xml.transform.Result;
+import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 
 public class LottoController {
 
     public static void main(String[] args) {
-
-        int lottoCount = InputView.buyOfLottoPrice();
-
+        int lottoCount = new InputView().buyOfLottoPrice();
         LottoGame lotto = new LottoGame();
-
         List<List<Integer>> buyLottoList = lotto.lottoGame(lottoCount, new LottoShuffleStrategy());
-
         ResultView.buyLottoList(buyLottoList);
 
-        String lastWeekLottoNumber = InputView.getLastWeekLottoNumber();
-
+        List<String> lastWeekLottoNumber = new InputView().getLastWeekLottoNumber();
         ResultWinner winner = new ResultWinner();
 
-        Map<Integer, Integer> resultMap = winner.countOfWinner(buyLottoList, lastWeekLottoNumber);
-
+        EnumMap<Rank, Integer> resultMap = winner.countOfWinner(buyLottoList, lastWeekLottoNumber);
         ResultView.resultView(resultMap, lottoCount);
 
     }
