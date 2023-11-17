a22921e4f9a520c8e5f847fe3c4dea67010c1209
gilbertlim <9ilbert.app@gmail.com>
2023-11-17 23:45:13 +0900

    refactor: LottoGenerator 리팩토링

--- src/main/java/study/step2/domain/LottoGenerator.java
+++ src/main/java/study/step2/domain/LottoGenerator.java
@@ -9,30 +9,25 @@ public class LottoGenerator {
     public static final int POOL_START_NUMBER = 1;
     public static final int POOL_END_NUMBER = 45;
     public static final int LOTTO_NUMBERS_SIZE = 6;
-    private final List<Integer> lottoNumbers;
     private final Random random;
+    private List<Integer> numberPool = new ArrayList<>();
 
     public LottoGenerator(Random random) {
         this.random = random;
-        this.lottoNumbers = new ArrayList<>();
+        this.numberPool = pooling();
     }
 
-    public Lotto generate() {
-        List<Integer> pool = pool();
-        Collections.shuffle(pool, random);
-        IntStream.range(0, LOTTO_NUMBERS_SIZE)
-            .forEach(n -> pickAndAdd(pool));
-        Collections.sort(lottoNumbers);
-        return new Lotto(lottoNumbers);
-    }
-
-    public List<Integer> pool() {
+    private List<Integer> pooling() {
         return IntStream.rangeClosed(POOL_START_NUMBER, POOL_END_NUMBER)
             .boxed()
             .collect(Collectors.toList());
     }
 
-    private void pickAndAdd(List<Integer> pool) {
-        lottoNumbers.add(pool.remove(0));
+    public Lotto generate() {
+        Collections.shuffle(numberPool, random);
+        List<Integer> lottoNumbers = numberPool.subList(0, LOTTO_NUMBERS_SIZE);
+        Collections.sort(lottoNumbers);
+        return new Lotto(lottoNumbers);
     }
+
 }
