0ed77e56a18198bac0224f1011fe366629ffe67b
hhj <abc@def.com>
2023-11-17 15:59:38 +0900

    refactor: InputView, ResultView 의 메서드를 static 제거

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,30 +1,32 @@
 package lotto.ui;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    static Scanner SCANNER = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     static final String PURCHASE_AMOUNT = "구입금액을 입력해주세요.";
-
     static final String LOTTO_COUNT = "개를 구매했습니다.";
     static final String LAST_WEEK_LOTTO_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
 
-    public static int buyOfLottoPrice(){
+    static final int LOTTO_AMOUNT = 1000;
+
+    public int buyOfLottoPrice() {
         System.out.println(PURCHASE_AMOUNT);
         String price = SCANNER.nextLine();
         return getLottoCount(price);
     }
 
-    private static int getLottoCount(String price){
-        int lottoCount = Integer.parseInt(price)/1000;
-        System.out.println(lottoCount+LOTTO_COUNT);
+    private int getLottoCount(String price) {
+        int lottoCount = Integer.parseInt(price) / LOTTO_AMOUNT;
+        System.out.println(lottoCount + LOTTO_COUNT);
         return lottoCount;
     }
 
-    public static String getLastWeekLottoNumber(){
+    public List<String> getLastWeekLottoNumber() {
         System.out.println(LAST_WEEK_LOTTO_NUMBER);
-        return SCANNER.nextLine().replaceAll(" ", "");
+        return List.of(SCANNER.nextLine().replaceAll(" ", "").split(","));
     }
 }
--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -1,39 +1,36 @@
 package lotto.ui;
 
-import lotto.domain.WinningToLotto;
+import lotto.domain.Rank;
 
-import java.sql.SQLOutput;
-import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class ResultView {
     static final String WINNER_STATISTICS = "당첨 통계";
-
     static final String RESULT_WINNER = "%d개 일치 (%d원)- %d개\n";
-
     static final String TOTAL_AMOUNT = "총 수익률은 %.2f입니다.";
-
     static final int LOTTO_PRICE = 1000;
 
-    public static void buyLottoList(List<List<Integer>> buyLottoList){
-        for(List<Integer> list : buyLottoList){
+    public static void buyLottoList(List<List<Integer>> buyLottoList) {
+        for (List<Integer> list : buyLottoList) {
             System.out.println(list);
         }
     }
 
-    public static void resultView(Map<Integer, Integer> resultMap, int lottoCount){
+    public static void resultView(EnumMap<Rank, Integer> resultMap, int lottoCount) {
+
+        System.out.println(WINNER_STATISTICS);
 
         int sumAmount = 0;
 
-        Set<Integer> keys = resultMap.keySet();
-        for (Integer key : keys) {
-            Integer value = resultMap.get(key);
-            System.out.printf(RESULT_WINNER, key, WinningToLotto.getWinningAmount(key), value);
-            sumAmount += value*WinningToLotto.getWinningAmount(key);
+        Set<Rank> keys = resultMap.keySet();
+        for (Rank key : keys) {
+            Integer value = Math.toIntExact(resultMap.get(key));
+            System.out.printf(RESULT_WINNER, key.getRank(), key.getAmount(), value);
+            sumAmount += value * key.getAmount();
         }
 
-        System.out.printf(TOTAL_AMOUNT, (double)sumAmount/(lottoCount*LOTTO_PRICE));
+        System.out.printf(TOTAL_AMOUNT, (double) sumAmount / (lottoCount * LOTTO_PRICE));
     }
 }
