d6fd3763e20d1d5db6a59213ced3733f960362dc
jeonghuntech <jeonghun.tech@gmail.com>
2023-11-17 18:21:42 +0900

    refactor: 로또번호 캐싱 추가

---
+++ src/test/java/lotto/domain/lotto/LottoNumberTest.java
@@ -0,0 +1,22 @@
+package lotto.domain.lotto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoNumberTest {
+
+    @Test
+    @DisplayName("로또번호를 생성한다. 로또 번호가 1~45 의 숫자가 아닌 경우 얘외가 발생한다")
+    public void create() {
+        Assertions.assertThatThrownBy(() -> LottoNumber.of(46)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또번호를 생성한다. 이전에 있던 번호인 경우에는 같은 객체를 반환한다")
+    public void create_cash() {
+        LottoNumber lottoNumber = LottoNumber.of(1);
+        Assertions.assertThat(lottoNumber == LottoNumber.of(1)).isTrue();
+        Assertions.assertThat(lottoNumber == LottoNumber.of(2)).isFalse();
+    }
+}
