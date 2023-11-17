1e516591f7c5c70a2d507dce5e19c4a27f6b06ba
tgHan <hantaegune@mf.seegene.com>
2023-11-17 16:45:03 +0900

    Operation enum을 활용해서 반복되는 코드를 개선

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,16 +8,18 @@ public class Calculator {
     public static final int ZERO = 0;
     public static final List<String> OPERATOR_LIST = Arrays.asList("+", "*", "/", "-");
 
-    public static int calculate(String inputValue){
+    public static int calculate(String inputValue) {
+        Operation.Operator operator;
+
         String[] strList = splitText(inputValue);
         List<Integer> valueList = filterValue(strList);
         List<String> opsList = filterOps(strList);
 
-        if (valueList.size() == 0){
+        if (valueList.size() == 0) {
             throw new IllegalArgumentException("연산 할 값이 없습니다.");
         }
 
-        if (valueList.size() != opsList.size()+1){
+        if (valueList.size() != opsList.size() + 1) {
             throw new IllegalArgumentException("계산식이 잘못 되였습니다.");
         }
 
@@ -26,33 +28,20 @@ public class Calculator {
         /**
          * 계산 공식 돌리기
          */
-        for (int i = 0 ; i < opsList.size() ; i++){
-            resultValue = getResultValue(valueList.get(i +1), opsList.get(i), resultValue);
+        for (int i = 0; i < opsList.size(); i++) {
+
+            operator = Operation.Operator.fromString(opsList.get(i));
+            resultValue = operator.apply(resultValue, valueList.get(i + 1));
+
         }
         return resultValue;
     }
 
-    private static int getResultValue(int value, String ops, int resultValue) {
-        if ("+".equals(ops)){
-            resultValue = add(resultValue, value);
-        }
-        if ("-".equals(ops)){
-            resultValue = subtract(resultValue, value);
-        }
-        if ("*".equals(ops)){
-            resultValue = multiply(resultValue, value);
-        }
-        if ("/".equals(ops)){
-            resultValue = divide(resultValue, value);
-        }
-        return resultValue;
-    }
-
-    public static ArrayList<String> filterOps(String[] inputList){
+    public static ArrayList<String> filterOps(String[] inputList) {
         ArrayList<String> opsList = new ArrayList<String>();
 
         IntStream.range(0, inputList.length).forEach(idx -> {
-            if (idx%2 !=0 && checkOps(inputList[idx])){
+            if (idx % 2 != 0 && checkOps(inputList[idx])) {
                 opsList.add(inputList[idx]);
             }
         });
@@ -62,16 +51,16 @@ public class Calculator {
     private static boolean checkOps(String text) {
         try {
             return OPERATOR_LIST.stream().anyMatch(p -> p.equals(text));
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("연산자가 아닙니다.");
         }
     }
 
 
-    public static ArrayList<Integer> filterValue(String[] inputList){
+    public static ArrayList<Integer> filterValue(String[] inputList) {
         ArrayList<Integer> valueList = new ArrayList<Integer>();
         IntStream.range(0, inputList.length).forEach(idx -> {
-            if (idx%2 == 0){
+            if (idx % 2 == 0) {
                 valueList.add(convertNum(inputList[idx]));
             }
         });
@@ -81,46 +70,19 @@ public class Calculator {
     private static int convertNum(String text) {
         try {
             return Integer.parseInt(text);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자 변환시 오류 발생 하였습니다. 숫자 List에 숫자가 아닌 값이 존재 합니다.");
 
         }
     }
 
-    public static String[] splitText(String text){
+    public static String[] splitText(String text) {
         return text.split(" ");
     }
 
-    public static int add(int number1, int number2){
-        return number1 + number2;
-    }
-
-    public static int subtract(int number1, int number2){
-        return number1 - number2;
-    }
-
-    public static int multiply(int number1, int number2){
-        return number1 * number2;
-    }
-
-    public static int divide(int number1, int number2){
-        if (!checkDivideValue(number1,number2)){
-            throw new IllegalArgumentException("나눗셈 결과 값이 정수로 떨어지지 않습니다.");
-        }
-        return number1 / number2;
-    }
-
-    public static boolean checkDivideValue(int number1, int number2){
-        if (number1 % number2 != 0){
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isBlank(String input){
+    public static boolean isBlank(String input) {
         return " ".equals(input);
     }
-
 
 
 }
