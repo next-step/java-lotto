17fcdc2cb86b6f465523d9b73fa0747d317f5f0f
birdie <flaehdan@gmail.com>
2023-11-18 17:56:27 +0900

    feat: 로또 번호 하나를 생성할 수 있다.

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,10 +1,25 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
 
     private int value;
 
     public LottoNumber(int value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
