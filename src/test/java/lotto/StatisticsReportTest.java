f5ac4ab63c335ab7b3fc29490c91d577f8610701
choomi1217 <whdudal1217@naver.com>
2023-11-19 02:13:59 +0900

    test:StatisticsReport 메소드 이름 변경

--- src/test/java/lotto/StatisticsReportTest.java
+++ src/test/java/lotto/StatisticsReportTest.java
@@ -9,7 +9,7 @@ public class StatisticsReportTest {
 
     @DisplayName("지난주 로또와 3개가 매칭된 로또 티켓이 2개 만들어진다.")
     @Test
-    void a() {
+    void createTwoOfThirePrizeLotto() {
         Prize third = Prize.THIRD;
         StatisticsReport statisticsReport = new StatisticsReport(Map.of(third, 2));
         Assertions.assertThat(statisticsReport.countByPrize(third)).isEqualTo(2);
