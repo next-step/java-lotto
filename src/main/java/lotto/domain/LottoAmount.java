7d10c590c36b9be7cacd0825a7592408634869fb
birdie <flaehdan@gmail.com>
2023-11-18 17:41:33 +0900

    feat: 1000원 단위가 아닌 경우 예외가 발생한다.

--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -6,13 +6,16 @@ public class LottoAmount {
 
     private int amount;
 
-    // TODO: 1000원 단위로 금액을 입력받는다
     public LottoAmount(String amount) {
         int value = 0;
         try {
             value = Integer.parseInt(amount);
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력해주세요.");
         }
         this.amount = value;
     }
