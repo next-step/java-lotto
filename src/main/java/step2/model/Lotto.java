dbdf3f06bc3e49e7641fcf45e243d2eae6f11eb1
jane096 <jes7077@naver.com>
2023-11-18 16:45:11 +0900

    매직넘버 상수화

--- src/main/java/step2/model/Lotto.java
+++ src/main/java/step2/model/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Lotto {
 
     public static final int EACH_LOTTO_PRICE = 1000;
+    private static final int MAX_LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -17,7 +18,7 @@ public class Lotto {
             throw new IllegalArgumentException("로또 번호가 없습니다.");
         }
 
-        if (numbers.size() != 6) {
+        if (numbers.size() != MAX_LOTTO_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6개가 되어야 합니다.");
         }
     }
