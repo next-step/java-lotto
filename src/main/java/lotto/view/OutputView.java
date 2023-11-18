ddab27b9d4486e8786860aa60e86202de630a6cf
ByungChan Lee <lbc2415@gmail.com>
2023-11-18 16:00:17 +0900

    refactor: updated displayProfit message

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -38,6 +38,6 @@ public class OutputView {
     }
 
     public static void displayProfit(double profit) {
-        System.out.println("총 수익률은 "+profit+"입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        System.out.println("총 수익률은 "+profit+"입니다.");
     }
 }
