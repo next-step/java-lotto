ab798ad8513e49d83d5a9eb1543a8976747a3b76
birdie <flaehdan@gmail.com>
2023-11-18 17:43:43 +0900

    refactor: 코드 리팩토링

--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -4,20 +4,30 @@ import java.util.Objects;
 
 public class LottoAmount {
 
+    private static final int LOTTO_PRICE = 1000;
+    private static final int REMAIN = 0;
     private int amount;
 
     public LottoAmount(String amount) {
+        int value = validateType(amount);
+        validateUnit(value);
+        this.amount = value;
+    }
+
+    private int validateType(String amount) {
         int value = 0;
         try {
             value = Integer.parseInt(amount);
         }catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
         }
+        return value;
+    }
 
-        if (value % 1000 != 0) {
+    private void validateUnit(int value) {
+        if (value % LOTTO_PRICE != REMAIN) {
             throw new IllegalArgumentException("1000원 단위로 입력해주세요.");
         }
-        this.amount = value;
     }
 
     @Override
