99edc38c7e8f826d6e10c93c4bdec51746863b1e
이창섭 <ventulus95@gmail.com>
2023-11-18 14:49:36 +0900

    feat: 당첨번호와 일치하는 로또 갯수를 저장한다

--- src/main/java/lotto/LottoStatics.java
+++ src/main/java/lotto/LottoStatics.java
@@ -1,4 +1,22 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LottoStatics {
+
+    private Map<Integer, Integer> matchingRanks = new HashMap<>();
+
+
+    public void classifyRankLotto(List<Lotto> lottos, Lotto winningLotto) {
+        for (Lotto lotto : lottos) {
+            Integer matchingCount = winningLotto.countMatchingWinningLotto(lotto);
+            matchingRanks.put(matchingCount, matchCountRanks(matchingCount) + 1);
+        }
+    }
+
+    public Integer matchCountRanks(int count) {
+        return matchingRanks.getOrDefault(count, 0);
+    }
 }
--- src/test/java/lotto/LottoStaticsTest.java
+++ src/test/java/lotto/LottoStaticsTest.java
@@ -4,14 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class LottoStaticsTest {
 
     @Test
     void 당첨번호와_일치하는_갯수별로_로또갯수를_저장한다() {
         LottoStatics lottoStatics = new LottoStatics();
-        List<Lotto> lottos = List.of();
-        Lotto winningLotto = new Lotto();
-//        lottoStatics.classifyRankLotto(lottos, winningLotto);
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6))
+                , new Lotto(List.of(30, 31, 32, 33, 34, 35, 36)), new Lotto(List.of(30, 31, 32, 33, 34, 35, 37)),
+                new Lotto(List.of(18, 22, 43, 33, 23, 13, 3))
+        );
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lottoStatics.classifyRankLotto(lottos, winningLotto);
+        assertAll(() -> {
+            assertThat(lottoStatics.matchCountRanks(6)).isEqualTo(1);
+            assertThat(lottoStatics.matchCountRanks(5)).isEqualTo(0);
+        });
 
     }
+
 }
