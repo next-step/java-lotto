eef0d69e6cfed22c6cb346d520bc712a42fe1484
jikimee64 <jikimee64@gmail.com>
2023-11-19 02:02:58 +0900

    feat: 숫자 형식의 수동으로 구매할 로또 개수를 입력받는다.

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_PURCHASE_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_BALL = "보너스 볼을 입력해 주세요.";
 
@@ -15,6 +16,11 @@ public class InputView {
 
     public int inputPrice() {
         System.out.println(INPUT_PURCHASE_PRICE);
+        return inputNumber();
+    }
+
+    public int inputManualLottoCount() {
+        System.out.println(INPUT_PURCHASE_MANUAL_LOTTO_COUNT);
         return inputNumber();
     }
 
