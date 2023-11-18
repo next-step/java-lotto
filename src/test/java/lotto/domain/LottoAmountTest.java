c6ec16835b153aef85c36e4940e130b4417be0bb
birdie <flaehdan@gmail.com>
2023-11-18 17:40:16 +0900

    test: 실패하는 테스트 생성

--- src/test/java/lotto/domain/LottoAmountTest.java
+++ src/test/java/lotto/domain/LottoAmountTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class LottoAmountTest {
 
@@ -18,5 +19,29 @@ class LottoAmountTest {
 
         // then
         assertThat(lottoAmount).isEqualTo(new LottoAmount("1000"));
+    }
+
+    @Test
+    @DisplayName("1000원 단위가 아닌 경우 예외가 발생한다.")
+    void createLottoAmountUnitTest() {
+        // given
+        String input = "1001";
+
+        // when & then
+        assertThatThrownBy(() -> new LottoAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1000원 단위로 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("구입 금액이 숫자가 아닌 경우 예외가 발생한다.")
+    void createLottoAmountTypeTest() {
+        // given
+        String input = "A";
+
+        // when & then
+        assertThatThrownBy(() -> new LottoAmount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력할 수 있습니다.");
     }
 }
