4e6f1679c024aeeefd55d65a22c2dbb833ad870c
kmj990929 <kmj990929@dgist.ac.kr>
2023-11-18 16:41:25 +0900

    refactor: move ExpressionValidator to Expression

--- src/main/java/calculator/model/Expression.java
+++ src/main/java/calculator/model/Expression.java
@@ -3,15 +3,53 @@ package calculator.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Expression {
     private final List<String> tokens;
+    private static final String ERR_EMPTY_EXPRESSION = "Empty input values are not allowed.";
+    private static final String ERR_INVALID_SYMBOL = "Only the following symbols can be used: " + Operator.getAllSymbols();
+    private static final String ERR_INVALID_EXPRESSION = "Expression is not valid.";
 
     public Expression(String expression) {
         // validate
-        ExpressionValidator.validate(expression);
+        Expression.validate(expression);
 
         this.tokens = split(expression);
+    }
+
+    public static void validate(String expression) {
+        if (expression == null || expression.isBlank()) {
+            throw new IllegalArgumentException(ERR_EMPTY_EXPRESSION);
+        }
+
+        if (containsInvalidSymbol(expression) || containsConsecutiveOperators(expression)) {
+            throw new IllegalArgumentException(ERR_INVALID_SYMBOL);
+        }
+
+        if (!checkNumberOnEdge(expression)) {
+            throw new IllegalArgumentException(ERR_INVALID_EXPRESSION);
+        }
+    }
+
+    private static boolean containsInvalidSymbol(String expression) {
+        String regex = "^[0-9+\\-*/\\s]*$";
+        return !Pattern.matches(regex, expression);
+    }
+
+    private static boolean checkNumberOnEdge(String expression) {
+        expression = expression.trim();
+        if ((Pattern.matches("^\\d.*", expression)
+                || Pattern.matches("^-\\d.*", expression))
+                && Pattern.matches(".*\\d$", expression)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean containsConsecutiveOperators(String expression) {
+        String regex = ".*[+\\-*/]{2,}.*";
+        return Pattern.matches(regex, expression.replaceAll("\\s", ""));
     }
 
     public static ArrayList<String> split(String expression) {
--- src/test/java/calculator/model/ExpressionTest.java
+++ src/test/java/calculator/model/ExpressionTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class ExpressionTest {
     @Test
@@ -21,6 +24,54 @@ public class ExpressionTest {
     public void 숫자_하나(String input, boolean expected) {
         Assertions.assertThat(new Expression(input).isMonomial())
                 .isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("빈 입력값에 대해 exception 발생")
+    @ValueSource(strings = {"", "      "})
+    public void 빈_입력값_exception(String argument) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Expression.validate(argument);
+        });
+//        assertThatCode(() -> {
+//            ExpressionValidator.validate("abc");
+//        }).doesNotThrowAnyException();
+//        assertThatCode(() -> {
+//            ExpressionValidator.validate("  _ ");
+//        }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("사칙연산 기호가 아닌 기호가 있는 경우 exception 발생 - 괄호 비허용")
+    @ValueSource(strings = {"( 1 + 2 ) - 3", " 1 % 3 + 1"})
+    public void 사칙연산_기호_확인(String argument) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Expression.validate(argument);
+        });
+//        assertThatCode(() -> {
+//            ExpressionValidator.validate("1+2-3");
+//        }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("사칙연산 기호가 연속해서 있는 경우 exception 발생")
+    @ValueSource(strings = {"1 +  + 3 + 1", "3 * * 1 + 2"})
+    public void 사칙연산_연속_확인(String argument) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Expression.validate(argument);
+        });
+//        assertThatCode(() -> {
+//            ExpressionValidator.validate("1+2-3");
+//        }).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("사칙연산 기호로 시작하거나 끝나는 경우 exception 발생")
+    @ValueSource(strings = {"+ 1 + 3 + 1 -", "1 + 2 - 3 - "})
+    public void 가장자리_연산자_확인(String argument) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Expression.validate(argument);
+        });
     }
 
 }
