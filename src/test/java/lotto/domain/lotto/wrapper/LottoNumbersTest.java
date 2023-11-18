1622379e386c8901ae18e60b74379533d72064f0
jonghunbaek <qor6028@naver.com>
2023-11-18 13:00:54 +0900

    refactor : 테스트 코드 리팩토링

--- src/test/java/lotto/domain/lotto/wrapper/LottoNumbersTest.java
+++ src/test/java/lotto/domain/lotto/wrapper/LottoNumbersTest.java
@@ -1,10 +1,16 @@
 package lotto.domain.lotto.wrapper;
 
+import lotto.domain.lotto.LotteryRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
+import static lotto.domain.lotto.LotteryRank.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoNumbersTest {
@@ -24,16 +30,25 @@ public class LottoNumbersTest {
     }
 
     @DisplayName("자신이 가지고 있는 로또 번호와 인자로 받은 당첨번호(+보너스)의 값이 몇 개 일치하는 지 확인하고 반환한다.")
-    @Test
-    void countMatchingNumbersWithBonus() {
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void countMatchingNumbersWithBonus(LottoNumbers lotto, int expectedResult) {
         // given
-        LottoNumbers lotto = new LottoNumbers(Set.of(1, 5, 12, 21, 32, 43));
         LottoNumbers winningNumber = new LottoNumbers(Set.of(1, 5, 12, 21, 32, 43));
 
         // when
         int count = lotto.countMatchingNumbers(winningNumber);
 
         // then
-        assertThat(count).isEqualTo(6);
+        assertThat(count).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+          Arguments.of(new LottoNumbers(Set.of(1, 5, 12, 21, 32, 43)), FIRST.matchingCount()),
+          Arguments.of(new LottoNumbers(Set.of(1, 5, 12, 21, 32, 42)), THIRD.matchingCount()),
+          Arguments.of(new LottoNumbers(Set.of(1, 5, 12, 21, 31, 42)), FOURTH.matchingCount()),
+          Arguments.of(new LottoNumbers(Set.of(1, 5, 12, 20, 31, 42)), FIFTH.matchingCount())
+        );
     }
 }
