7cda88ff5b482423787813e4d02655f867e58d9f
qwe5507 <apvmffkdls@gmail.com>
2023-11-18 18:01:39 +0900

    refactor: 필요없는 test제거

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -7,19 +7,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LottoNumberTest {
-
-    @Test
-    void create() {
-        assertThat(LottoNumber.of(3)).isEqualTo(LottoNumber.of(3));
-    }
 
     @DisplayName("로또번호는 1이상 45이하 여야 한다.")
     @ParameterizedTest
     @ValueSource(ints = {-1, 46})
     void invalid_로또번호(int number) {
-        assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.of(number));
         assertThatIllegalArgumentException().isThrownBy(() -> LottoNumber.of(number));
     }
 }
