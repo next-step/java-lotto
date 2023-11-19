fe2a8913ad14fe4b2b85bff2454e123f7eb21092
yumble <hnjn1101@gmail.com>
2023-11-19 11:15:47 +0900

    feat: 주소값 같으면 같은 객체인지 테스트(Lotto.class, LottoNumber.class)

--- src/test/java/step2/model/LottoTest.java
+++ src/test/java/step2/model/LottoTest.java
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.Test;
 public class LottoTest {
 
     @Test
+    void 로또는_로또번호가_같으면_같다() {
+        Lotto lotto1 = new Lotto(new int[]{1, 2, 3, 4, 5, 6});
+        Lotto lotto2 = new Lotto(new int[]{1, 2, 3, 4, 5, 6});
+        assertThat(lotto1).isEqualTo(lotto2);
+    }
+
+    @Test
     public void 로또가_보너스번호를_포함여부_테스트() {
         Lotto lotto = new Lotto(new int[]{1, 2, 3, 4, 5, 6});
         LottoNumber bonusNumber = new LottoNumber(1);
