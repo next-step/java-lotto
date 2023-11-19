48dc31b72b18d148e7d5b4d5b68579b900fec807
hhj <hghgusrl@naver.com>
2023-11-19 11:25:43 +0900

    refactor: 테스트 코드 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,9 @@ import java.util.stream.IntStream;
 
 public class Lotto {
 
+    static final int LOTTO_MAX_NUMBER = 45;
+    static final int LOTTO_MIN_NUMBER = 1;
+
     private List<Integer> lottos;
 
     public Lotto() {
@@ -14,7 +17,25 @@ public class Lotto {
                 .collect(Collectors.toList());
     }
 
+    public Lotto(List<Integer> numbers) {
+        validation(numbers);
+        this.lottos = numbers;
+    }
+
     public List<Integer> getLotto() {
         return lottos;
+    }
+
+    private void validation(List<Integer> numbers) {
+
+        for(int number : numbers){
+            validationNumber(number);
+        }
+    }
+
+    private static void validationNumber(int number) {
+        if (number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException("유효하지 않은 로또 번호 입니다.");
+        }
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,18 +1,51 @@
 package lotto;
 
 
-import lotto.domain.WinningToLotto;
-import org.assertj.core.api.Assertions;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoTest {
 
-    @DisplayName("일등 당첨금 테스트")
+    @DisplayName("로또 생성 테스트")
+    @Test
+    public void 로또생성(){
+
+        assertThatThrownBy(() -> {
+            Lotto lotto = new Lotto(Arrays.asList(-1,2,3,4,5,6));
+        }).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+
+    @DisplayName("일등 테스트")
     @Test
     public void 일등(){
-        assertThat(WinningToLotto.getWinningAmount(6)).isEqualTo(WinningToLotto.SIX.price());
+        assertThat(Rank.valueOf(6).get()).isEqualTo(Rank.FIRST);
     }
+
+    @DisplayName("이등 테스트")
+    @Test
+    public void 이등(){
+        assertThat(Rank.valueOf(5).get()).isEqualTo(Rank.SECOND);
+    }
+
+    @DisplayName("삼등 테스트")
+    @Test
+    public void 삼등(){
+        assertThat(Rank.valueOf(4).get()).isEqualTo(Rank.THIRD);
+    }
+
+    @DisplayName("사등 테스트")
+    @Test
+    public void 사등(){
+        assertThat(Rank.valueOf(3).get()).isEqualTo(Rank.FOURTH);
+    }
+
 }
