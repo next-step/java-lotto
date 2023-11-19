9b93b87f055287619274046b017dac0223627df6
kimbro97 <rlagudwog@gmail.com>
2023-11-20 07:16:22 +0900

    refactor: 숫자 검증 로직 메서드 분리 및 매직리터럴 상수 처리 작업

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
 public class LottoNumber {
+    private static final int ONE = 1;
+    private static final int FORTY_FIVE = 1;
     private int number;
     public LottoNumber(int number) {
-        if (number < 1 || number > 45) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    private static void validateNumber(int number) {
+        if (number < ONE || number > FORTY_FIVE) {
             throw new IllegalArgumentException("로또 번호는 1부터 45사이의 숫자입니다.");
         }
-        this.number = number;
     }
 }
