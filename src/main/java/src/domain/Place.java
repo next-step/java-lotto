26dba14f2d44fcc11b52a69f63f1c8beb422f5af
solchan98 <dev.sol0427@gmail.com>
2023-11-19 13:29:39 +0900

    refactor: 피드백 반영 - 각 Place가 각자의 match 조건을 가져 match를 처리하도록 수정

--- src/main/java/src/domain/Place.java
+++ src/main/java/src/domain/Place.java
@@ -2,35 +2,42 @@ package src.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
 public enum Place {
-    FIRST_PLACE(6, 2_000_000_000, false),
-    SECOND_PLACE(5, 30_000_000, true),
-    THIRD_PLACE(5, 1_500_000, false),
-    FOURTH_PLACE(4, 50_000, false),
-    FIFTH_PLACE(3, 5_000, false),
-    OTHER(2, 0, false);
+    FIRST_PLACE(6, 2_000_000_000, (matchCount, bonusMatch) ->
+            matchCount == 6 && bonusMatch == Boolean.FALSE),
+    SECOND_PLACE(5, 30_000_000, (matchCount, bonusMatch) ->
+            matchCount == 5 && bonusMatch == Boolean.TRUE),
+
+    THIRD_PLACE(5,1_500_000, (matchCount, bonusMatch) ->
+            matchCount == 5 && bonusMatch == Boolean.FALSE),
+
+    FOURTH_PLACE(4,50_000, (matchCount, bonusMatch) ->
+            matchCount == 4 && bonusMatch == Boolean.FALSE),
+
+    FIFTH_PLACE(3,5_000, (matchCount, bonusMatch) ->
+            matchCount == 3 && bonusMatch == Boolean.FALSE),
+
+    OTHER(2,0, (matchCount, bonusMatch) ->
+            matchCount == 2 && bonusMatch == Boolean.FALSE);
 
     private final int matchCount;
 
     private final double amount;
 
-    private final boolean bonus;
+    private final BiPredicate<Integer, Boolean> match;
 
-    Place(int matchCount, double amount, boolean bonus) {
+    Place(int matchCount, double amount, BiPredicate<Integer, Boolean> match) {
         this.matchCount = matchCount;
         this.amount = amount;
-        this.bonus = bonus;
-    }
-
-    private boolean match(int matchCount, boolean matchBonus) {
-        return this.matchCount == matchCount && this.bonus == matchBonus;
+        this.match = match;
     }
 
     public static Place byMatchCount(int matchCount, boolean matchBonus) {
         return Arrays.stream(values())
-                .filter(value -> value.match(matchCount, matchBonus))
+                .filter(value -> value.match.test(matchCount, matchBonus))
                 .findFirst()
                 .orElse(Place.OTHER);
     }
