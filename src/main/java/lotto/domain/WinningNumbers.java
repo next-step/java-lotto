a6f0886539bccb545367ca94eab7c0aa4f2fe452
yoohojeong <yh132@naver.com>
2023-11-19 20:16:54 +0900

    feat : 입력받은 보너스번호가 기존 입력한 당첨번호와 중복되지않게 처리 및 기존 WinningNumbers에 포함

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WinningNumbers {
 
         private static final int WINNING_NUMBERS_COUNT = 6;
-
-        private final Set<Integer> values;
+        private static final int COUNT_OF_BONUS_NUMBER_ADDED = 7;
+        private Set<Integer> values;
+        private LottoNumber bonusNumber;
 
         public WinningNumbers(List<Integer> values) {
                 this.values = Set.copyOf(values);
@@ -25,6 +27,16 @@ public class WinningNumbers {
                 return values;
         }
 
+        public LottoNumber bonusNumber() {
+                return bonusNumber;
+        }
+
+        public void addBonusNumberInToTheWinningNumbers(int bonusNumber) {
+                this.bonusNumber = new LottoNumber(bonusNumber);
+                addBonusNumber();
+                validateBonusNumber();
+        }
+
         private void validateCountOfValues() {
                 if (!isFixedCount()) {
                         throw new IllegalArgumentException("6개의 당첨번호를 중복되지않게 입력해주세요.");
@@ -33,5 +45,20 @@ public class WinningNumbers {
 
         private boolean isFixedCount() {
                 return values.size() == WINNING_NUMBERS_COUNT;
+        }
+
+        private void validateBonusNumber() {
+                if (!isFixedCountOfBonusNumberAdded()) {
+                        throw new IllegalArgumentException("기존 입력한 6개의 당첨번호와 중복되지않게 입력해주세요.");
+                }
+        }
+
+        private boolean isFixedCountOfBonusNumberAdded() {
+                return values.size() == COUNT_OF_BONUS_NUMBER_ADDED;
+        }
+
+        private void addBonusNumber() {
+                values = Stream.concat(values.stream(), Stream.of(bonusNumber.value()))
+                    .collect(Collectors.toUnmodifiableSet());
         }
 }
--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -11,4 +11,17 @@ public class WinningNumbersTest {
                 assertThatThrownBy(() -> new WinningNumbers(new int[]{1, 2, 3, 4, 5, 6, 7})).isInstanceOf(
                     IllegalArgumentException.class).hasMessage("6개의 당첨번호를 중복되지않게 입력해주세요.");
         }
+
+        @Test
+        void 중복된_당첨번호를_입력했을_경우_에러() {
+                assertThatThrownBy(() -> new WinningNumbers(new int[]{1, 2, 3, 4, 6, 6})).isInstanceOf(
+                    IllegalArgumentException.class).hasMessage("6개의 당첨번호를 중복되지않게 입력해주세요.");
+        }
+
+        @Test
+        void 기존_입력한_당첨번호와_중복된_보너스_번호를_입력했을_경우_에러() {
+                WinningNumbers winningNumbers = new WinningNumbers(new int[]{1, 2, 3, 4, 5, 6});
+                assertThatThrownBy(() -> winningNumbers.addBonusNumberInToTheWinningNumbers(6)).isInstanceOf(
+                    IllegalArgumentException.class).hasMessage("기존 입력한 6개의 당첨번호와 중복되지않게 입력해주세요.");
+        }
 }
