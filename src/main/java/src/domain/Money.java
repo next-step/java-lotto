bd0c6f1f64d3aed359738cf0bf08720c4298ff50
solchan98 <dev.sol0427@gmail.com>
2023-11-19 13:53:34 +0900

    refactor: 피드백 반영 - 머니 생서 검증 로직 수정

--- src/main/java/src/domain/Money.java
+++ src/main/java/src/domain/Money.java
@@ -5,6 +5,9 @@ public class Money {
     private final int value;
 
     private Money(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Money는 음수일 수 없습니다.");
+        }
         this.value = value;
     }
 
@@ -18,9 +21,6 @@ public class Money {
 
     public Money minus(Money money) {
         int minusValue = value - money.value;
-        if (minusValue < 0) {
-            throw new IllegalArgumentException("Money는 음수일 수 없습니다.");
-        }
 
         return new Money(minusValue);
     }
