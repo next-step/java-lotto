3fffbeb9c4280054c057d5825dd15beb9572aa0b
jonghunbaek <qor6028@naver.com>
2023-11-19 19:27:10 +0900

    feat : 보너스 번호가 당첨 번호와 중복되는지 검증하는 기능 추가 및 테스트 작성

--- src/main/java/lotto/domain/lotto/wrapper/WinningNumber.java
+++ src/main/java/lotto/domain/lotto/wrapper/WinningNumber.java
@@ -7,14 +7,21 @@ public class WinningNumber {
     private final LottoNumbers winningNumbers;
     private final LottoNumber bonusNumber;
 
-    public WinningNumber(Set<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = new LottoNumbers(winningNumbers);
-        this.bonusNumber = new LottoNumber(bonusNumber);
-    }
-
     public WinningNumber(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
+        validateBonusNumber(winningNumbers, bonusNumber);
+
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public WinningNumber(Set<Integer> winningNumbers, int bonusNumber) {
+        this(new LottoNumbers(winningNumbers), new LottoNumber(bonusNumber));
+    }
+
+    private void validateBonusNumber(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨번호와 중복될 수 없습니다.");
+        }
     }
 
     public boolean containsBonus(LottoNumbers lotto) {
--- src/test/java/lotto/domain/lotto/wrapper/WinningNumberTest.java
+++ src/test/java/lotto/domain/lotto/wrapper/WinningNumberTest.java
@@ -22,4 +22,12 @@ class WinningNumberTest {
         // then
         assertThat(result).isTrue();
     }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외를 발생시킨다.")
+    @Test
+    void validateBonusNumber() {
+        assertThatThrownBy(() -> new WinningNumber(Set.of(1, 2, 3, 4, 5, 6), 6))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("보너스 번호는 당첨번호와 중복될 수 없습니다.");
+    }
 }
