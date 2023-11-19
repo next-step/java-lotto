3cfb9dfd398a74b61fe18cdd1fc64e82ff75fbac
HeeDoo Logan Jeong <78134917+jhd7130@users.noreply.github.com>
2023-11-19 16:33:57 +0900

    사칙 연산 계산기

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -2,4 +2,16 @@ public class Operation {
     public static int plus(int first, int second) {
         return first + second;
     }
+
+    public static int minus(int first, int second) {
+        return first - second;
+    }
+
+    public static int multiply(int first, int second) {
+        return first * second;
+    }
+
+    public static int divide(int first, int second) {
+        return first/second;
+    }
 }
--- src/test/java/OperationTest.java
+++ src/test/java/OperationTest.java
@@ -6,13 +6,50 @@ public class OperationTest {
 
     @Test
     @DisplayName("덧셈 계산기")
-    void writingMethodName() throws Exception {
+    void plus() {
         // given
-        int first = 1;
+        int first = 2;
         int second = 2;
         // when
         int result = Operation.plus(first, second);
         // then
-        Assertions.assertThat(result).isEqualTo(3);
+        Assertions.assertThat(result).isEqualTo(4);
     }
+
+    @Test
+    @DisplayName("뺄셈 계산기")
+    void minus() {
+        // given
+        int first = 2;
+        int second = 2;
+        // when
+        int result = Operation.minus(first, second);
+        // then
+        Assertions.assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("곱셈 계산기")
+    void multiply() {
+        // given
+        int first = 2;
+        int second = 2;
+        // when
+        int result = Operation.multiply(first, second);
+        // then
+        Assertions.assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("나눗셈 계산기")
+    void divide() {
+        // given
+        int first = 2;
+        int second = 2;
+        // when
+        int result = Operation.divide(first, second);
+        // then
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+
 }
