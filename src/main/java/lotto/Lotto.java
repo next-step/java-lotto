19094bda9641366e31cb46627e8132937cea07de
sunga <eert2260@naver.com>
2023-11-19 23:56:26 +0900

    feat : 로또 발급

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Lotto {
-    public static int returnLottoCount(int money) {
-        return money / 1000;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<Integer> numbers = new ArrayList<>();
+
+    Lotto(){
+        List<Integer> generatedNumbers = LottoUtil.generateLottoNumber();
+
+        for (int i = 0; i < LOTTO_NUMBER_COUNT; i++){
+            numbers.add(generatedNumbers.get(i));
+        }
+
+        Collections.sort(numbers);
+    }
+
+    List<Integer> numbers(){
+        return this.numbers;
+    }
+
+    @Override
+    public String toString() {
+        return "Lotto{" +
+                "numbers=" + numbers +
+                '}';
     }
 }
---
+++ src/main/java/lotto/LottoUtil.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoUtil {
+    private static final int PRICE_PER_LOTTO = 1000;
+    private static final int LOTTO_NUMBER_BOUNDARY = 45;
+
+    public static int returnLottoCount(int money) {
+        return money / PRICE_PER_LOTTO;
+    }
+
+    public static List<Integer> generateLottoNumber(){
+        List<Integer> numberList = new ArrayList<>();
+        for (int i = 1; i <= LOTTO_NUMBER_BOUNDARY; i++) {
+            numberList.add(i);
+        }
+
+        Collections.shuffle(numberList);
+
+        return numberList;
+    }
+}
