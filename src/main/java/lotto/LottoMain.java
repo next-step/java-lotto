af3e763d57712534c6fd372c47668c99c36fc38f
kimbro97 <rlagudwog@gmail.com>
2023-11-20 13:24:15 +0900

    feat: OutputView 로직 추가

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -2,6 +2,7 @@ package lotto;
 
 import lotto.domain.LottoTicket;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.Arrays;
 
@@ -9,5 +10,6 @@ public class LottoMain {
     public static void main(String[] args) {
         Integer number = InputView.start();
         LottoTicket lottoTicket = new LottoTicket(number);
+        OutputView.lottoTicketListView(lottoTicket.getLottoTicket());
     }
 }
