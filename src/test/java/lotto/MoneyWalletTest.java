3624c2a18e3eea72ea193da8c924c0c60d640fd4
choomi1217 <whdudal1217@naver.com>
2023-11-20 16:32:46 +0900

    style: 사용하지 않는 import 제거

--- src/test/java/lotto/MoneyWalletTest.java
+++ src/test/java/lotto/MoneyWalletTest.java
@@ -3,7 +3,6 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,22 +10,22 @@ public class MoneyWalletTest {
 
     @DisplayName("입금")
     @Test
-    void deposit(){
+    void deposit() {
         MoneyWallet wallet = new MoneyWallet();
         assertThat(wallet.deposit(100000).balance()).isEqualTo(100000);
     }
 
     @DisplayName("출금 - 성공")
     @Test
-    void withdrawSuccess(){
+    void withdrawSuccess() {
         MoneyWallet wallet = new MoneyWallet(1000);
         assertThat(wallet.withdraw(500).balance()).isEqualTo(500);
     }
 
     @DisplayName("출금 - 실패")
     @Test
-    void withdrawFail(){
+    void withdrawFail() {
         MoneyWallet wallet = new MoneyWallet(1000);
-        assertThatIllegalArgumentException().isThrownBy(()->wallet.withdraw(5000));
+        assertThatIllegalArgumentException().isThrownBy(() -> wallet.withdraw(5000));
     }
 }
--- src/test/java/lotto/PAndLReportTest.java
+++ src/test/java/lotto/PAndLReportTest.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
@@ -12,11 +11,12 @@ public class PAndLReportTest {
 
     @DisplayName("2,000원 로또를 구매하고 5,000원이 당첨 되면 손익률은 2.5입니다.")
     @Test
-    void rate(){
+    void rate() {
         Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         LottoWallet lottoWallet = LottoWallet.from(List.of(lotto1, lotto2));
-        StatisticsReport statisticsReport = new StatisticsReport(lottoWallet, Map.of(Prize.THIRD, 1));
+        StatisticsReport statisticsReport = new StatisticsReport(lottoWallet,
+            Map.of(Prize.THIRD, 1));
         PAndLReport pAndLReport = PAndLReport.of(statisticsReport);
         BigDecimal actual = pAndLReport.rate();
         Assertions.assertThat(actual).isEqualTo(BigDecimal.valueOf(2.5));
