fd7f7b8611dc155307cf383fad1f085c002034c3
ByungChan Lee <lbc2415@gmail.com>
2023-11-21 00:23:33 +0900

    refactor: deleted controller package

--- src/main/java/lotto/LottoPlayApplication.java
+++ src/main/java/lotto/LottoPlayApplication.java
@@ -1,6 +1,10 @@
 package lotto;
 
-import lotto.controller.LottoGame;
+import lotto.domain.LottoFactory;
+import lotto.domain.PrizeStatus;
+import lotto.domain.WinnerNumbers;
+import lotto.view.InputView;
+import lotto.view.OutputView;
 
 import java.util.Random;
 
@@ -8,7 +12,20 @@ public class LottoPlayApplication {
     public static void main (String [] arg) {
         Random random = new Random();
 
-        LottoGame lottoGame = new LottoGame();
-        lottoGame.start(random);
+        int purchasedAmt = InputView.getAmountOfPurchase();
+        int numOfTickets = purchasedAmt / 1000;
+        OutputView.displayNumberOfPurchasedTickets(purchasedAmt);
+
+        LottoFactory lottoFactory = new LottoFactory(numOfTickets, random);
+        OutputView.displayLottoTickets(lottoFactory);
+
+        WinnerNumbers winnerNumbers = new WinnerNumbers(lottoFactory.generateTicket(random));
+
+        OutputView.displayWinnerNumbers(winnerNumbers);
+        PrizeStatus prizeStatus = new PrizeStatus(lottoFactory, winnerNumbers);
+
+        OutputView.displayPrizeStatMessage();
+        OutputView.displayPrizeStatus(prizeStatus);
+        OutputView.displayProfit(prizeStatus.calculateProfit(purchasedAmt));
     }
 }
