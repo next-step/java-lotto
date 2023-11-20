fb97e2279fdac6769447cd166ae5eb93ac89e29b
kimbro97 <rlagudwog@gmail.com>
2023-11-20 13:20:58 +0900

    feat: 티켓리스트 출력 로직 작업

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,13 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+    public static void lottoTicketListView(List<Lotto> lottoTicket) {
+        for (int i = 0; i < lottoTicket.size(); i++) {
+            System.out.println(lottoTicket.get(i).toString());
+        }
+    }
 }
