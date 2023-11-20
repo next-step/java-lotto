61835c7644b96cb45aeaac1bca295dcd356bb058
yoohojeong <yh132@naver.com>
2023-11-20 19:45:10 +0900

    refactor : WinningTicket 일급 컬렉션화

--- src/main/java/lotto/domain/Tickets.java
+++ src/main/java/lotto/domain/Tickets.java
@@ -1,49 +1,40 @@
 package lotto.domain;
 
-import java.util.Collections;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class Tickets {
 
-    private final List<Ticket> values;
+        private final List<Ticket> values;
 
-    public Tickets(List<Ticket> tickets) {
-        this.values = tickets;
-    }
-
-    public List<Ticket> values() {
-        return values;
-    }
-
-    public Map<Rank, Integer> countWinningTickets(WinningNumbers winningNumbers) {
-        Map<Rank, Integer> matchRankCountMap = new EnumMap<>(Rank.class);
-        initMatchRankCountMap(matchRankCountMap);
-        values.forEach(ticket -> {
-            int countOfWinningNumber = ticket.countWinningNumber(winningNumbers);
-            Rank rank = Rank.valueOfRank(countOfWinningNumber,
-                ticket.isBonusNumberContained(winningNumbers));
-            matchRankCountMap.put(rank, matchRankCountMap.get(rank) + 1);
-        });
-        return Collections.unmodifiableMap(matchRankCountMap);
-    }
-
-    public double calculateRateOfBenefit(Map<Rank, Integer> matchRankCountMap, long purchaseAmount) {
-        AtomicLong benefitSum = new AtomicLong();
-        matchRankCountMap.forEach((rank, integer) -> benefitSum.set(
-            benefitSum.get() + rank.winningMoney() * matchRankCountMap.get(rank)));
-        return Math.round((double) benefitSum.get() / purchaseAmount * 100.00) / 100.00;
-    }
-
-    public int getCountOfTickets() {
-        return values.size();
-    }
-
-    private void initMatchRankCountMap(Map<Rank, Integer> matchRankCountMap) {
-        for (Rank rank : Rank.values()) {
-            matchRankCountMap.put(rank, 0);
+        public Tickets(List<Ticket> tickets) {
+                this.values = tickets;
         }
-    }
+
+        public List<Ticket> values() {
+                return values;
+        }
+
+        public WinningTickets analyzeWinningTickets(WinningNumbers winningNumbers) {
+                Map<Rank, Integer> matchRankCountMap = new EnumMap<>(Rank.class);
+                initMatchRankCountMap(matchRankCountMap);
+                values.forEach(ticket -> {
+                        int countOfWinningNumber = ticket.countWinningNumber(winningNumbers);
+                        Rank rank = Rank.valueOfRank(countOfWinningNumber,
+                            ticket.hasBonusNumber(winningNumbers));
+                        matchRankCountMap.computeIfPresent(rank, (k, v) -> v + 1);
+                });
+                return new WinningTickets(matchRankCountMap);
+        }
+
+        public int getCountOfTickets() {
+                return values.size();
+        }
+
+        private void initMatchRankCountMap(Map<Rank, Integer> matchRankCountMap) {
+                for (Rank rank : Rank.values()) {
+                        matchRankCountMap.put(rank, 0);
+                }
+        }
 }
--- src/test/java/lotto/domain/WinningTicketsTest.java
+++ src/test/java/lotto/domain/WinningTicketsTest.java
@@ -5,22 +5,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class TicketsTest {
+public class WinningTicketsTest {
 
         public Ticket newTicket(int[] numbers) {
                 return new Ticket(new LottoNumbers(numbers));
         }
 
         @Test
-        void 보너스_번호_포함_당첨_티켓_결과() {
+        void 보너스_번호_포함_로또_결과_수익률_계산() {
                 WinningNumbers winningNumbers = new WinningNumbers(new LottoNumber(10), 1, 2, 3, 4, 5, 6);
                 Ticket ticket1 = newTicket(new int[]{1, 2, 3, 7, 8, 9});
                 Ticket ticket2 = newTicket(new int[]{10, 11, 12, 13, 14, 15});
                 Ticket ticket3 = newTicket(new int[]{1, 2, 3, 10, 11, 12});
                 Tickets tickets = new Tickets(List.of(ticket1, ticket2, ticket3));
+                long purchaseAmount = 3_000;
 
-                assertThat(tickets.analyzeWinningTickets(winningNumbers).toString()).isEqualTo(
-                    "{FIRST=0, SECOND=0, THIRD=0, FOURTH=1, FIFTH=1, MISS=1}");
-                //                assertThat(winningTickets.countOfWinningTickets()).isEqualTo(2);
+                WinningTickets winningTickets = tickets.analyzeWinningTickets(winningNumbers);
+                assertThat(winningTickets.calculateRateOfBenefit(purchaseAmount)).isEqualTo(18.33);
         }
 }
