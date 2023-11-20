128fc285b33cda5b08e0f8ca05b6f392bfdb7b0c
choomi1217 <whdudal1217@naver.com>
2023-11-20 23:13:44 +0900

    style: 클래스 구성요소 순서 변경

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -3,27 +3,28 @@ package lotto;
 import java.util.Objects;
 
 public class LottoNumber {
-    private final int number;
 
     private final static int MIN_NUMBER = 1;
     private final static int MAX_NUMBER = 45;
+
+    private final int number;
 
     public LottoNumber(int number) {
         validation(number);
         this.number = number;
     }
 
-    private void validation(int number) {
-        if(number < MIN_NUMBER){
-            throw new IllegalArgumentException("로또의 숫자는 최소 1입니다.");
-        }
-        if(number > MAX_NUMBER){
-            throw new IllegalArgumentException("로또의 숫자는 최대 45입니다.");
-        }
+    public static LottoNumber from(int number) {
+        return new LottoNumber(number);
     }
 
-    public static LottoNumber from(int number){
-        return new LottoNumber(number);
+    private void validation(int number) {
+        if (number < MIN_NUMBER) {
+            throw new IllegalArgumentException("로또의 숫자는 최소 1입니다.");
+        }
+        if (number > MAX_NUMBER) {
+            throw new IllegalArgumentException("로또의 숫자는 최대 45입니다.");
+        }
     }
 
     public boolean compare(int compareInteger) {
