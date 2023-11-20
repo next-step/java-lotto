8e7ffbd6b0649f607581977dfb74dbaa8786b993
이승준 <skp.seungjun@sk.com>
2023-11-20 17:36:34 +0900

    광역 예외 처리 추가
    (cherry picked from commit d0d3aac7ad01a778d7bfa38be0fd2d62d7294ec2)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,16 @@
 import controller.LottoController;
+import view.Renderer;
 
 public class Application {
     public static void main(String[] args) {
         LottoController game = new LottoController();
-        game.play();
+        try {
+            game.play();
+        }
+        catch (Exception e) {
+            Renderer.simplePrint("게임을 진행하던 중 다음의 에러가 발생했습니다:");
+            Renderer.simplePrint(e.getMessage());
+            Renderer.simplePrint("게임을 종료합니다.");
+        }
     }
 }
