2f3be2a37a9cc08192c67034702e63affffca412
Jinhong Kim <deepred.kim@gmail.com>
2023-11-20 14:26:52 +0900

    (1단계) refactor: Enum 캐싱

--- src/main/java/camp/nextstep/edu/calculator/Operator.java
+++ src/main/java/camp/nextstep/edu/calculator/Operator.java
@@ -1,6 +1,8 @@
 package camp.nextstep.edu.calculator;
 
-import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum Operator {
     PLUS("+"),
@@ -8,30 +10,36 @@ public enum Operator {
     MULTIPLY("*"),
     DIVIDE("/");
 
-    private final String operator;
+    private static final Map<String, Operator> symbolToOperator
+        = Stream.of(values()).collect(Collectors.toMap(Operator::symbol, e -> e));
 
-    Operator(String operator) {
-        this.operator = operator;
+    private final String symbol;
+
+    Operator(String symbol) {
+        this.symbol = symbol;
     }
 
-    static Operator from(String operator) {
-        return Arrays.stream(Operator.values())
-            .filter(op -> op.operator.equals(operator))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 잘못된 연산자입니다."));
+    static Operator from(String symbol) {
+        if (!symbolToOperator.containsKey(symbol)) {
+            throw new IllegalArgumentException("지원하지 않는 잘못된 연산자입니다.");
+        }
+        return symbolToOperator.get(symbol);
     }
 
     int operate(int left, int right) {
-        if ("+".equals(this.operator)) {
+        if ("+".equals(this.symbol)) {
             return left + right;
         }
-        if ("-".equals(this.operator)) {
+        if ("-".equals(this.symbol)) {
             return left - right;
         }
-        if ("*".equals(this.operator)) {
+        if ("*".equals(this.symbol)) {
             return left * right;
         }
         return left / right;
     }
 
+    private String symbol() {
+        return this.symbol;
+    }
 }
