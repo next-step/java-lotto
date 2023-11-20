8fe3ee91b28b192021c33a31e97055eb11bc1828
ByungChan Lee <lbc2415@gmail.com>
2023-11-21 00:21:53 +0900

    refactor: added winningMoney amount in enum Rank

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,16 +3,18 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6),
-    SECOND(5),
-    THIRD(4),
-    FOURTH(3),
-    NO_MATCH(0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NO_MATCH(0, 0);
 
     private int matchCount;
+    private int winningMoney;
 
-    Rank(int matchCount) {
+    Rank(int matchCount, int winningMoney) {
         this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
     }
 
     public static Rank of(int matchCount){
