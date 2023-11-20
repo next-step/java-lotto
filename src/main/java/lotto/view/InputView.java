8f9471a235b2b45734d5088ff3f7cae219a0bdee
kimbro97 <rlagudwog@gmail.com>
2023-11-20 12:50:15 +0900

    refactor: 매직 리터널 상수 처리 작업

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,14 +3,16 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
+    private static final int MONEY_UNIT = 1000;
+    private static final int ZERO = 0;
     private static Scanner scanner = new Scanner(System.in);
 
     public static Integer start() {
         System.out.println("구입 금액을 입력해주세요");
         int money = scanner.nextInt();
-        if (money % 1000 != 0) {
+        if (money % MONEY_UNIT != ZERO) {
             throw new IllegalArgumentException("로또 구매는 1000원 단위로 구매할 수 있습니다.");
         }
-        return money / 1000;
+        return money / MONEY_UNIT;
     }
 }
