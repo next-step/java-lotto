f4878ad7848f5a6148775d852e8fafcfc3b0943f
Youngbin Kwon <youngbin.kwon@sk.com>
2023-11-20 11:40:37 +0900

    당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String INPUT_MESSAGE_OF_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
@@ -10,6 +13,19 @@ public class InputView {
 
     public static int inputPurchaseAmount(){
         System.out.println(INPUT_MESSAGE_OF_PURCHASE_AMOUNT);
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static List<Integer> inputWinningNumbers(){
+        System.out.println(INPUT_MESSAGE_OF_WINNING_NUMBER);
+        String numbers = scanner.nextLine();
+        return getWinningNumbers(numbers);
+    }
+
+    private static List<Integer> getWinningNumbers(String numbers){
+        return Arrays.asList(numbers.split(SEPARATOR))
+                .stream().mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
