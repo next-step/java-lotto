13cb5de519ce1b33641111008327a68f8faab353
kmj990929 <kmj990929@dgist.ac.kr>
2023-11-21 00:02:08 +0900

    feat: issue Lotto ticket

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,24 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.LottoMachine;
 import lotto.view.InputView;
 import lotto.view.ResultView;
+
+import java.util.List;
 
 public class LottoController {
 
     public static void main(String[] args) {
-        //buyLotto();
-        int money = InputView.inputMoney();
-        ResultView.printCheckPurchaseMessage(money/1000);
+        buyLotto();
 
         //checkLottoResult();
+    }
+
+    public static List<Lotto> buyLotto() {
+        int money = InputView.inputMoney();
+        List<Lotto> tickets = LottoMachine.issue(money);
+        ResultView.printCheckPurchaseMessage(tickets.size());
+        return tickets;
     }
 }
