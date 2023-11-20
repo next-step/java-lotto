5bde2504a3097af1b4969f8a5f500ffac5c0cef7
이승준 <skp.seungjun@sk.com>
2023-11-20 17:35:14 +0900

    로또 번호 수동 입력 추가
    (cherry picked from commit c31de401da022d544315065cb558240f9e9794a0)

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -8,10 +8,12 @@ import util.calculator.Ratio;
 import view.Question;
 import view.Renderer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
-    private int userInputPurchaseAmount = 0;
+    private int userInputPurchaseAmount;
+    private int userInputManualLottoCount;
     private List<Lotto> lottos;
     private WinningNumber winningNumber;
 
@@ -22,14 +24,37 @@ public class LottoController {
     }
 
     private void buyLottoPhase() {
+        lottos = new ArrayList<>();
+
         userInputPurchaseAmount = Question.intAsk("구입 금액을 입력해 주세요.");
+        int totalLottoCount = userInputPurchaseAmount / 1000;
 
-        int lottoAmount = userInputPurchaseAmount / 1000;
-        Renderer.simplePrint(lottoAmount + "개를 구매했습니다.");
+        userInputManualLottoCount = Question.intAsk("수동으로 구매할 로또 개수를 입력해 주세요.");
+        int autoLottoCount = totalLottoCount - userInputManualLottoCount;
 
-        lottos = LottoGenerator.generateRandomLotto(lottoAmount);
+        buyManualLottoPhase(userInputManualLottoCount);
+
+        Renderer.simplePrint("나머지는 자동으로 구매합니다.");
+        buyAutoLottoPhase(autoLottoCount);
+
+        Renderer.simplePrint("수동으로 " + userInputManualLottoCount + "장, 자동으로 " + autoLottoCount + "장 구매했습니다.");
         for (Lotto myLotto : lottos) {
             Renderer.printLotto(myLotto);
+        }
+    }
+
+    private void buyAutoLottoPhase(int autoLottoCount) {
+        lottos.addAll(
+                LottoGenerator.generateRandomLotto(autoLottoCount)
+        );
+    }
+
+    private void buyManualLottoPhase(int lottoCount) {
+        Renderer.simplePrint("수동으로 구매할 로또의 번호를 입력해 주세요.");
+        for (int i = 1; i <= lottoCount; i++) {
+            List<Integer> userInputLottoNumber = Question.intListAsk("로또 " + i + "장 째: ");
+            Lotto lotto = Lotto.of(userInputLottoNumber);
+            lottos.add(lotto);
         }
     }
 
