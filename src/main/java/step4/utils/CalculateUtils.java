ecaf9ba2275c28799a97cba6d444c6105b4c3fa4
jane096 <jes7077@naver.com>
2023-11-22 00:57:30 +0900

    수동로또 음수값 예외처리 추가

--- src/main/java/step4/utils/CalculateUtils.java
+++ src/main/java/step4/utils/CalculateUtils.java
@@ -13,11 +13,18 @@ public class CalculateUtils {
     }
 
     public static int autoLottoCount(int allLottoCount, int manualLottoCount) {
+        isPositive(manualLottoCount);
         int autoLottoCount = allLottoCount - manualLottoCount;
         if (autoLottoCount < 0) {
             throw new IllegalArgumentException("지불한 금액보다 로또 수량이 더 많습니다.");
         }
 
         return autoLottoCount;
+    }
+
+    public static void isPositive(int manualLottoCount) {
+        if (manualLottoCount < 0) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        }
     }
 }
