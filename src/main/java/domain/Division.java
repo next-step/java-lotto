ddc77c8b1e9fbed8ed59df221d3eb49d39675f45
jeseungkim <qazwsx3745@naver.com>
2023-11-21 20:03:59 +0900

    로또 1단계

---
+++ src/main/java/domain/Division.java
@@ -0,0 +1,5 @@
+package domain;
+
+public interface Division {
+    int division(int a, int b);
+}
---
+++ src/main/java/domain/Subtraction.java
@@ -0,0 +1,5 @@
+package domain;
+
+public interface Subtraction {
+    int subtraction(int a, int b);
+}
