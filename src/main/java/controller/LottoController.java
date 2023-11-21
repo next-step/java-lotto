609a89a68fdd5f62a5c948b8df73790ec9f5d0c7
이승준 <70700016+henry174@users.noreply.github.com>
2023-11-21 19:34:20 +0900

    Step4 (#3451)
    * 로또 번호 수동 입력 추가
    
    (cherry picked from commit c31de401da022d544315065cb558240f9e9794a0)
    
    * 광역 예외 처리 추가
    
    (cherry picked from commit d0d3aac7ad01a778d7bfa38be0fd2d62d7294ec2)
    
    * 유저 인풋 유효성 검증을 한 곳으로 분리
    
    (cherry picked from commit 93800ce500d12f5beae59d6e0e50fdc82b966532)
    
    * 사용자 입력을 감쌈
    
    ---------
    
    Co-authored-by: 이승준 <skp.seungjun@sk.com>

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import controller.validation.LottoCount;
+import controller.validation.LottoPurchaseAmount;
 import lotto.Lotto;
 import lotto.LottoGenerator;
 import lotto.type.WinningLevel;
@@ -8,47 +10,96 @@ import util.calculator.Ratio;
 import view.Question;
 import view.Renderer;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static view.Question.intAsk;
+
 public class LottoController {
-    private int userInputPurchaseAmount = 0;
-    private List<Lotto> lottos;
-    private WinningNumber winningNumber;
+
+    private LottoPurchaseAmount userInputPurchaseAmount;
+    private LottoCount totalLottoCount;
+    private LottoCount userInputManualLottoCount;
+    private LottoCount autoLottoCount;
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    private WinningNumber userInputWinningNumber;
+
+    private WinningStatistic result;
 
     public void play() {
+        init();
+
         buyLottoPhase();
         inputWinningNumberPhase();
         resultPhase();
     }
 
+    private void init() {
+        this.userInputPurchaseAmount = null;
+        this.userInputManualLottoCount = null;
+        this.totalLottoCount = null;
+        this.autoLottoCount = null;
+
+        this.lottos.clear();
+
+        this.userInputWinningNumber = null;
+        this.result = null;
+    }
+
     private void buyLottoPhase() {
-        userInputPurchaseAmount = Question.intAsk("구입 금액을 입력해 주세요.");
+        this.userInputPurchaseAmount = new LottoPurchaseAmount(intAsk("구입 금액을 입력해 주세요."));
+        this.totalLottoCount = this.userInputPurchaseAmount.calTotalLottoCount();
 
-        int lottoAmount = userInputPurchaseAmount / 1000;
-        Renderer.simplePrint(lottoAmount + "개를 구매했습니다.");
+        this.userInputManualLottoCount = new LottoCount(intAsk("수동으로 구매할 로또 개수를 입력해 주세요."));
+        this.autoLottoCount = this.totalLottoCount.subtractionBy(this.userInputManualLottoCount);
 
-        lottos = LottoGenerator.generateRandomLotto(lottoAmount);
-        for (Lotto myLotto : lottos) {
-            Renderer.printLotto(myLotto);
+        buyManualLottoPhase();
+
+        Renderer.simplePrint("나머지는 자동으로 구매합니다.");
+        buyAutoLottoPhase();
+
+        Renderer.simplePrint("수동으로 " + this.userInputManualLottoCount + "장, 자동으로 " + this.autoLottoCount + "장 구매했습니다.");
+        Renderer.printLottos(this.lottos);
+    }
+
+    private void buyAutoLottoPhase() {
+        this.lottos.addAll(
+                LottoGenerator.generateRandomLotto(this.autoLottoCount.toInt())
+        );
+    }
+
+    private void buyManualLottoPhase() {
+        Renderer.simplePrint("수동으로 구매할 로또의 번호를 입력해 주세요.");
+        for (int i = 1; i <= this.userInputManualLottoCount.toInt(); i++) {
+            List<Integer> userInputLottoNumber = Question.intListAsk("로또 " + i + "장 째: ");
+            Lotto lotto = Lotto.of(userInputLottoNumber);
+            lottos.add(lotto);
         }
     }
 
     private void inputWinningNumberPhase() {
         List<Integer> userInputWinningNumber = Question.intListAsk("지난 주 당첨 번호를 입력해 주세요.");
-        int userInputBonusNumber = Question.intAsk("보너스 볼을 입력해 주세요.");
+        int userInputBonusNumber = intAsk("보너스 볼을 입력해 주세요.");
 
-        winningNumber = WinningNumber.of(userInputWinningNumber, userInputBonusNumber);
+        this.userInputWinningNumber = WinningNumber.of(userInputWinningNumber, userInputBonusNumber);
     }
 
     private void resultPhase() {
         WinningStatistic result = new WinningStatistic();
-        for (Lotto myLotto : lottos) {
-            WinningLevel rank = WinningLevel.decideFinalWinningLevel(myLotto, winningNumber);
+
+        for (Lotto myLotto : this.lottos) {
+            WinningLevel rank = WinningLevel.decideFinalWinningLevel(myLotto, this.userInputWinningNumber);
 
             result.occurs(rank);
         }
-        Renderer.printResult(result);
 
-        Renderer.printRatio(Ratio.ratio(result.getTotalWinAmount().toInt(), userInputPurchaseAmount));
+        Renderer.printResult(result);
+        Renderer.printRatio(
+                Ratio.ratio(
+                        result.getTotalWinAmount().toInt(), this.userInputPurchaseAmount.toInt()
+                )
+        );
     }
 }
