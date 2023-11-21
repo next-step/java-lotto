13c2fdaec9cabeaa020f424a0fa597139e7c8efb
choomi1217 <whdudal1217@naver.com>
2023-11-21 23:14:35 +0900

    refactor: 천단위 `_` 표시

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -6,10 +6,10 @@ public enum Prize {
     EIGHTH(0, 0),
     SEVENTH(1, 0),
     FIFTH(2, 0),
-    FOURTH(3, 5000),
-    THIRD(4, 50000),
-    SECOND(5, 1500000),
-    FIRST(6, 2000000000);
+    FOURTH(3, 5_000),
+    THIRD(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     private final int rank;
     private final int price;
