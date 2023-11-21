b1c00fe3769e70bff39850695c76561576faf60d
hyeongiii <hyeongiii0811@gmail.com>
2023-11-21 21:19:35 +0900

    feat: 올바르지 않은 기호에 대한 예외 처리 (step1)

--- src/main/java/step1/util/StringUtility.java
+++ src/main/java/step1/util/StringUtility.java
@@ -2,9 +2,11 @@ package step1.util;
 
 public class StringUtility {
     private final static String EXCEPTION_MESSAGE_BLANK_INPUT = "입력값이 빈 값이거나 공백으로만 이뤄졌습니다.";
+    private final static String EXCEPTION_MESSAGE_ILLEGAL_OPERATION = "입력된 기호가 올바르지 않습니다.";
     private final static String SEPARATOR = " ";
+    private final static String LEGAL_OPERATOR = "+-*/";
 
-    public static void validateString(String input) {
+    public static void validateBlankString(String input) {
         if (blankString(input)) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_BLANK_INPUT);
         }
@@ -16,5 +18,17 @@ public class StringUtility {
 
     public static String[] split(String str) {
         return str.split(SEPARATOR);
+    }
+
+    public static void validateOperator(String[] values) {
+        for (int i = 0; i < values.length; i = i + 2) {
+            illegalOperator(values[i]);
+        }
+    }
+
+    private static void illegalOperator(String operator) {
+        if (!LEGAL_OPERATOR.contains(operator)) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_ILLEGAL_OPERATION);
+        }
     }
 }
--- src/test/java/step1/util/StringUtilityTest.java
+++ src/test/java/step1/util/StringUtilityTest.java
@@ -13,14 +13,21 @@ public class StringUtilityTest {
     @ParameterizedTest
     @DisplayName("입력값이 null 이거나 빈 문자열일 경우 예외를 발생시킨다.")
     @ValueSource(strings = {"", "    "})
-    void validateString(String value) {
+    void validateBlankString(String value) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> StringUtility.validateString(value));
+                .isThrownBy(() -> StringUtility.validateBlankString(value));
     }
 
     @Test
     @DisplayName("입력 값을 공백을 기준으로 자른다.")
     void split() {
         assertThat(StringUtility.split("1 + 2 * 3")).containsExactly("1", "+", "2", "*", "3");
+    }
+
+    @Test
+    @DisplayName("입력된 연산자가 올바르지 않을 경우 예외를 발생시킨다.")
+    void validateOperator() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> StringUtility.validateOperator(new String[] {"1", "+", "2", "(", "4"}));
     }
 }
