41941e82e06c6230e5365148d1bf8bf96dc77e56
Hanna Choi <44824602+gkssk4163@users.noreply.github.com>
2023-11-21 23:28:50 +0900

    refactor: 숫자 1000 단위 표기

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public enum LottoResult {
 
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    SIX(6, 2000000000),
+    THREE(3, 5_000),
+    FOUR(4, 50_000),
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000),
     FAIL(0, 0);
 
     private final int matchCount;
