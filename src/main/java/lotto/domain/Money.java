c423cb1cf174bd38fea548d2d596c4eb7eb029fd
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-22 00:41:03 +0900

    feat : Money 돈의 유효성 검증 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -2,8 +2,40 @@ package lotto.domain;
 
 public class Money {
     public static final int LOTTO_PRICE = 1000;
+    public static final String CANNOT_BUY_EVEN_ONLY_ONE_LOTTO_EXCEPTION = "돈이 부족해 1개의 로또도 살 수 없습니다.";
+    public static final String MONEY_TO_GO_BACK_EXCEPTION = "로또를 구매해도 거슬러줄 금액이 존재합니다.";
 
     public long lottoQuantity(long fee) {
+        validateMoney(fee);
+        return changeToLottoCnt(fee);
+    }
+
+    private void validateMoney(long fee) {
+        validateMoneyToBuyLotto(fee);
+        validateMoneyToGoBack(fee);
+    }
+
+    private void validateMoneyToBuyLotto(long fee) {
+        if (purchasable(fee)) {
+            throw new IllegalStateException(CANNOT_BUY_EVEN_ONLY_ONE_LOTTO_EXCEPTION);
+        }
+    }
+
+    private boolean purchasable(long fee) {
+        return fee < LOTTO_PRICE;
+    }
+
+    private void validateMoneyToGoBack(long fee) {
+        if (isLeft(fee)) {
+            throw new IllegalStateException(MONEY_TO_GO_BACK_EXCEPTION);
+        }
+    }
+
+    private boolean isLeft(long fee) {
+        return fee % LOTTO_PRICE != 0;
+    }
+
+    private long changeToLottoCnt(long fee) {
         return fee / LOTTO_PRICE;
     }
 }
--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import static lotto.domain.Money.CANNOT_BUY_EVEN_ONLY_ONE_LOTTO_EXCEPTION;
+import static lotto.domain.Money.MONEY_TO_GO_BACK_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoneyTest {
 
@@ -20,5 +24,31 @@ public class MoneyTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("돈으로 로또를 살 수 없으면 예외를 던진다.")
+    @ValueSource(longs = {999, 0})
+    void cannot_buy_one_lotto(long given) {
+        // given
+        Money money = new Money();
+
+        // when  // then
+        assertThatThrownBy(() -> money.lottoQuantity(given))
+                .isExactlyInstanceOf(IllegalStateException.class)
+                .hasMessage(CANNOT_BUY_EVEN_ONLY_ONE_LOTTO_EXCEPTION);
+    }
+
+    @ParameterizedTest
+    @DisplayName("거슬러줄 돈이 있으면 예외를 던진다.")
+    @ValueSource(longs = {3300, 1100, 1001})
+    void money_to_go_back_exception(long given) {
+        // given
+        Money money = new Money();
+
+        // when  // then
+        assertThatThrownBy(() -> money.lottoQuantity(given))
+                .isExactlyInstanceOf(IllegalStateException.class)
+                .hasMessage(MONEY_TO_GO_BACK_EXCEPTION);
     }
 }
