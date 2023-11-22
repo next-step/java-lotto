5f525db811743ea1e8847861ddfebc3ad52dd8ff
hyunyyxp <qqqq_o@naver.com>
2023-11-22 15:10:34 +0900

    [step1] 리뷰 내용 반영

--- src/test/java/calculator/controller/CalculatorExecutorControllerTest.java
+++ src/test/java/calculator/controller/CalculatorExecutorControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class CalculatorControllerTest {
+class CalculatorExecutorControllerTest {
 
     @ParameterizedTest
     @NullAndEmptySource
