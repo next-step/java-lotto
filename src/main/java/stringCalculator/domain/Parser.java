fd2a93a65d25baee4a9c3baf81f898fbe4d6185f
Hyesooo <hyesookim95@gmail.com>
2023-11-22 15:17:37 +0900

    typo: symbol -> operator

--- src/main/java/stringCalculator/domain/Parser.java
+++ src/main/java/stringCalculator/domain/Parser.java
@@ -3,7 +3,7 @@ package stringCalculator.domain;
 public class Parser {
 
     public static final String REGEX_BLANK = " ";
-    public static final String REGEX_NUM_OR_SYMBOL = "^[0-9]+( [+\\-*/] [0-9]+)*$";
+    public static final String REGEX_NUM_OR_OPERATOR = "^[0-9]+( [+\\-*/] [0-9]+)*$";
 
     public static String[] parseInput(String input) {
         validateString(input);
@@ -13,7 +13,7 @@ public class Parser {
     private static void validateString(String input) {
         // ^[0-9]+ 숫자로 시작되어야함
         // ()* 괄호 사이의 패턴이 반복
-        if (!input.matches(REGEX_NUM_OR_SYMBOL)) {
+        if (!input.matches(REGEX_NUM_OR_OPERATOR)) {
             throw new IllegalArgumentException("입력값을 확인해주세요.");
         }
     }
