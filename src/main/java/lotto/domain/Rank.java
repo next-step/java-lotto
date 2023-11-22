2251ae87ad67cf2e1f28acf2856aa152bb7d3923
kyunghyun-park <qqqq_o@naver.com>
2023-11-23 00:32:04 +0900

    feat: 로또 등수 enum에 2등 추가, 출력 타이틀 변수 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,30 +3,38 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
-    ZERO(0, 0);
+    ZERO("0개 일치", 0, 0),
+    FIFTH("3개 일치", 3, 5_000),
+    FOURTH("4개 일치", 4, 50_000),
+    THIRD("5개 일치", 5, 1_500_000),
+    SECOND("5개 일치, 보너스 볼 일치", 5, 30_000_000),
+    FIRST("6개 일치", 6, 2_000_000_000);
 
-    public static final Rank[] VALUES = values();
+    private static final Rank[] VALUES = values();
+
+    private final String title;
     private final int matchCount;
     private final int prizeMoney;
 
-    Rank(int matchCount, int prizeMoney) {
+    Rank(String title, int matchCount, int prizeMoney) {
+        this.title = title;
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
     }
 
-    public static Rank rankByCount(int count) {
+    public static Rank rankByCount(int count, boolean isMatchBonus) {
+        if (count == 5) {
+            return isMatchBonus ? Rank.SECOND : Rank.THIRD;
+        }
+
         return Arrays.stream(VALUES)
                 .filter(rank -> rank.matchCount == count)
                 .findFirst()
                 .orElse(Rank.ZERO);
     }
 
-    public int getMatchCount() {
-        return matchCount;
+    public String getTitle() {
+        return title;
     }
 
     public int getPrizeMoney() {
--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -9,9 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RankTest {
 
     @ParameterizedTest
-    @CsvSource({"6,FIRST", "5,SECOND", "4,THIRD", "3,FOURTH"})
+    @CsvSource({"6, FALSE, FIRST", "5, TRUE, SECOND", "5, FALSE, THIRD", "4, FALSE, FOURTH", "3, FALSE, FIFTH"})
     @DisplayName("맞춘 번호에 따라 Rank를 반환한다.")
-    void match_count_return_rank(int count, Rank rank) {
-        assertThat(Rank.rankByCount(count)).isEqualTo(rank);
+    void match_count_return_rank(int count, boolean isMatchBonus, Rank rank) {
+        assertThat(Rank.rankByCount(count, isMatchBonus)).isEqualTo(rank);
     }
 }
