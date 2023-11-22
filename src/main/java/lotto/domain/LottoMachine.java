05c09fed6584d32fb5fd90e313994bc22609ccc7
kimbro97 <rlagudwog@gmail.com>
2023-11-23 01:39:33 +0900

    docs: TODO 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,8 +1,14 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoMachine {
     public LottoTicket generateLottoTicket(int number) {
         LottoTicket lottoTicket = new LottoTicket(number);
         return lottoTicket;
+    }
+    public Rank rank(Lotto lotto, Lotto winningLotto) {
+        int matchCount = lotto.match(winningLotto);
+        return Rank.rank(matchCount);
     }
 }
