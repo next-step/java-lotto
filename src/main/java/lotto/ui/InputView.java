bac7fa3935c857c4312c58962f97bd84f1d61aed
kyunghyun-park <qqqq_o@naver.com>
2023-11-23 00:33:23 +0900

    feat: 로또 2등에 대한 입출력 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -21,4 +21,9 @@ public class InputView {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
+
+    public static int inputBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return SCANNER.nextInt();
+    }
 }
