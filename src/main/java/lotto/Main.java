f24050b3d1db4bb876e8496d79d5f60fe71c0733
choomi1217 <whdudal1217@naver.com>
2023-11-22 18:22:24 +0900

    feat: Main과 Controller 분리

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,37 +1,10 @@
 package lotto;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import lotto.view.InputView;
-import lotto.view.ResultView;
+import lotto.controller.LottoController;
 
 public class Main {
-
     public static void main(String[] args) {
-        InputView inputView = new InputView(new Scanner(System.in), System.out);
-        ResultView resultView = new ResultView(System.out);
-
-        int money = inputView.lottoInitCount();
-
-        MoneyWallet moneyWallet = new MoneyWallet(money);
-        LottoWallet lottoWallet = LottoShop.from(money).purchase();
-        moneyWallet = moneyWallet.withdraw(lottoWallet.totalPurchaseAmount());
-        resultView.calculateResult(lottoWallet.totalTicketCount());
-        resultView.out(lottoWallet);
-
-        List<LottoNumber> lastWeakLottoNumbers = createLastWeakLottoNumbers(inputView.lastWeakLottoInit());
-        WinningLotto winningLotto = WinningLotto.from(new Lotto(lastWeakLottoNumbers));
-
-        StatisticsReport report = StatisticsReport.of(lottoWallet).report(winningLotto);
-        resultView.out(report);
-        resultView.out(report.rate());
+        LottoController.of(new Scanner(System.in), System.out).start();
     }
-
-    private static List<LottoNumber> createLastWeakLottoNumbers(List<Integer> lastWeakLottoInit) {
-        return lastWeakLottoInit.stream().map(LottoNumber::new)
-            .collect(Collectors.toList());
-    }
-
 }
---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,59 @@
+package lotto.controller;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoShop;
+import lotto.domain.LottoWallet;
+import lotto.domain.MoneyWallet;
+import lotto.domain.StatisticsReport;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final ResultView resultView;
+
+    public LottoController(Scanner scanner, PrintStream out) {
+        this.inputView = new InputView(scanner, System.out);
+        this.resultView = new ResultView(System.out);
+    }
+
+    public static LottoController of(Scanner scanner, PrintStream out) {
+        return new LottoController(scanner, out);
+    }
+
+    public void start() {
+        int money = inputView.lottoInitCount();
+
+        LottoWallet lottoWallet = purchaseLotto(money);
+        resultView.calculateResult(lottoWallet.totalTicketCount());
+        resultView.out(lottoWallet);
+
+        List<LottoNumber> lastWeakLottoNumbers = createLastWeakLottoNumbers(inputView.lastWeakLottoInit());
+        WinningLotto winningLotto = WinningLotto.from(new Lotto(lastWeakLottoNumbers));
+
+        StatisticsReport report = StatisticsReport.of(lottoWallet).report(winningLotto);
+        resultView.out(report);
+        resultView.out(report.rate());
+    }
+
+    private LottoWallet purchaseLotto(int money) {
+        MoneyWallet moneyWallet = new MoneyWallet(money);
+        LottoWallet lottoWallet = LottoShop.from(money).purchase();
+        moneyWallet = moneyWallet.withdraw(lottoWallet.totalPurchaseAmount());
+        return lottoWallet;
+    }
+
+    private List<LottoNumber> createLastWeakLottoNumbers(List<Integer> lastWeakLottoInit) {
+        return lastWeakLottoInit.stream().map(LottoNumber::new)
+            .collect(Collectors.toList());
+    }
+
+
+}
