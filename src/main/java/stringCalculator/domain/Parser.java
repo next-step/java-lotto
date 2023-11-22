ee2f3f18574b77333bd56fd3a422c065e4d523d1
Hyesooo <hyesookim95@gmail.com>
2023-11-22 15:13:01 +0900

    refactor: regex 상수로 치환

--- src/main/java/stringCalculator/domain/Parser.java
+++ src/main/java/stringCalculator/domain/Parser.java
@@ -1,15 +1,19 @@
 package stringCalculator.domain;
 
 public class Parser {
+
+    public static final String REGEX_BLANK = " ";
+    public static final String REGEX_NUM_OR_SYMBOL = "^[0-9]+( [+\\-*/] [0-9]+)*$";
+
     public static String[] parseInput(String input) {
         validateString(input);
-        return input.split(" ");
+        return input.split(REGEX_BLANK);
     }
 
     private static void validateString(String input) {
         // ^[0-9]+ 숫자로 시작되어야함
         // ()* 괄호 사이의 패턴이 반복
-        if (!input.matches("^[0-9]+( [+\\-*/] [0-9]+)*$")) {
+        if (!input.matches(REGEX_NUM_OR_SYMBOL)) {
             throw new IllegalArgumentException("입력값을 확인해주세요.");
         }
     }
