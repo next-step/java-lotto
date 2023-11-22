7f98ae1997c22e177b0b0990bb0ff19a625e77e2
안현호(Hyeonho Ahn)/Biz플랫폼개발팀/SKP <hhahn@sk.com>
2023-11-22 10:39:50 +0900

    refactor: 값객체(LottoNumbers) 추가 및 최대최소값 보장을 위해 외부주입방식으로 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,45 +1,24 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
 
-    private List<Integer> lottoNumbers = new ArrayList<>();
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
-    private Lotto() {
-    }
+    private LottoNumbers lottoNumbers;
 
-    public static Lotto of(List<Integer> lottoNumbers) {
-        Lotto lotto = new Lotto();
-        lotto.lottoNumbers = lottoNumbers;
-        return lotto;
+    public Lotto() {
+        this.lottoNumbers = new LottoNumbers(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
     }
 
     public List<Integer> getLottoNumbers() {
-        sortLottoNumbers();
-        return this.lottoNumbers;
+        return this.lottoNumbers.getLottoNumbers();
     }
-
-    private void sortLottoNumbers() {
-        Collections.sort(lottoNumbers);
-    }
-
 
     public int getMatchCount(List<Integer> lastWeekLottoNumbers) {
-        int matchCount = 0;
-        for (int i = 0; i < lastWeekLottoNumbers.size(); i++) {
-            matchCount = calculateMatchCount(lastWeekLottoNumbers, i, matchCount);
-        }
-        return matchCount;
-
-    }
-
-    private int calculateMatchCount(List<Integer> lastWeekLottoNumbers, int idx, int matchCount) {
-        if (lottoNumbers.contains(lastWeekLottoNumbers.get(idx))) {
-            matchCount++;
-        }
-        return matchCount;
+        return this.lottoNumbers.matchCount(lastWeekLottoNumbers);
     }
 }
---
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoNumbers {
+
+    private List<Integer> lottoNumbers;
+
+    public LottoNumbers(int minLottoNumber, int maxLottoNumber) {
+        List<Integer> shuffledNumbers = IntStream.rangeClosed(minLottoNumber, maxLottoNumber).boxed().collect(Collectors.toList());
+        Collections.shuffle(shuffledNumbers);
+        Collections.sort(shuffledNumbers);
+        this.lottoNumbers = shuffledNumbers.subList(0, 6);
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return this.lottoNumbers;
+    }
+
+    public int matchCount(List<Integer> lastWeekLottoNumbers) {
+        int matchCount = 0;
+        for (int i = 0; i < lastWeekLottoNumbers.size(); i++) {
+            matchCount = calculateMatchCount(lastWeekLottoNumbers, i, matchCount);
+        }
+        return matchCount;
+    }
+
+    private int calculateMatchCount(List<Integer> lastWeekLottoNumbers, int idx, int matchCount) {
+        if (lottoNumbers.contains(lastWeekLottoNumbers.get(idx))) {
+            matchCount++;
+        }
+        return matchCount;
+    }
+}
