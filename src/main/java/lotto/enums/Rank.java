387ee94f8e94604ea530b246791c078a56a913be
Hyesooo <hyesookim95@gmail.com>
2023-11-23 20:20:47 +0900

    feat: Rank 등수 description 추가

--- src/main/java/lotto/enums/Rank.java
+++ src/main/java/lotto/enums/Rank.java
@@ -8,17 +8,19 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toMap;
 
 public enum Rank {
-    FIRST_PLACE(6, 2000000000),
-    SECOND_PLACE(5, 150000000),
-    THIRD_PLACE(4, 50000),
-    FOURTH_PLACE(3, 5000);
+    FIRST_PLACE(6, 2000000000, "1등"),
+    SECOND_PLACE(5, 150000000, "2등"),
+    THIRD_PLACE(4, 50000, "3등"),
+    FOURTH_PLACE(3, 5000, "4등");
 
     private final long matchCount;
     private final long prizeAmount;
+    private final String description;
 
-    Rank(int matchCount, long prizeAmount) {
+    Rank(long matchCount, long prizeAmount, String description) {
         this.matchCount = matchCount;
         this.prizeAmount = prizeAmount;
+        this.description = description;
     }
 
     private static final Map<Long, Rank> matchCountToRank = Stream.of(values()).collect(toMap(Rank::getMatchCount, Function.identity()));
@@ -27,12 +29,17 @@ public enum Rank {
         return Optional.ofNullable(matchCountToRank.get(count));
     }
 
-    private long getMatchCount() {
+    public long getMatchCount() {
         return matchCount;
     }
 
     public long getPrizeAmount() {
         return prizeAmount;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
     }
 }
 
