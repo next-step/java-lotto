b49386cd8f21836703ac5396a0090961afc236fd
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-23 12:12:39 +0900

    test: rank 테스트 수정

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -13,19 +13,20 @@ public class RankTest {
 	@DisplayName("로또의 등수를 확인한다.")
 	@ParameterizedTest
 	@MethodSource("matchNumAndRank")
-	void match_rank(int matchNum, Rank rank) {
-		Assertions.assertThat(Rank.matchRank(matchNum)).isEqualTo(rank);
+	void match_rank(int matchNum, boolean isMatchBonusNum, Rank rank) {
+		Assertions.assertThat(Rank.matchRank(matchNum, isMatchBonusNum)).isEqualTo(rank);
 	}
 
 	static Stream<Arguments> matchNumAndRank() {
 		return Stream.of(
-			Arguments.arguments(6, Rank.FIRST),
-			Arguments.arguments(5, Rank.SECOND),
-			Arguments.arguments(4, Rank.THIRD),
-			Arguments.arguments(3, Rank.FOURTH),
-			Arguments.arguments(2, Rank.NOT),
-			Arguments.arguments(1, Rank.NOT),
-			Arguments.arguments(0, Rank.NOT)
+			Arguments.arguments(6, false, Rank.FIRST),
+			Arguments.arguments(5, true, Rank.SECOND),
+			Arguments.arguments(5, false, Rank.THIRD),
+			Arguments.arguments(4, false, Rank.FOURTH),
+			Arguments.arguments(3, false, Rank.FIFTH),
+			Arguments.arguments(2, false, Rank.NOT),
+			Arguments.arguments(1, false, Rank.NOT),
+			Arguments.arguments(0, false, Rank.NOT)
 		);
 	}
 }
