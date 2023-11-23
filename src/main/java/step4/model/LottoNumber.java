d04bc0b8c1c2e19e7e1fe02191ea64de01045876
jane096 <jes7077@naver.com>
2023-11-24 02:00:45 +0900

    로또 숫자를 매번 할당해서 사용하지 않도록 재활용

--- src/main/java/step4/model/LottoNumber.java
+++ src/main/java/step4/model/LottoNumber.java
@@ -7,10 +7,10 @@ import java.util.stream.IntStream;
 
 public class LottoNumber {
 
-    private final List<Integer> allLottoNumbers = IntStream.range(1, 46).boxed().collect(Collectors.toList());
+    private static final List<Integer> allLottoNumbers = IntStream.range(1, 46).boxed().collect(Collectors.toList());
 
-    public List<Integer> getShufflingNumbers() {
-        Collections.shuffle(this.allLottoNumbers);
-        return this.allLottoNumbers.subList(0, 6);
+    public static List<Integer> getShufflingNumbers() {
+        Collections.shuffle(allLottoNumbers);
+        return allLottoNumbers.subList(0, 6);
     }
 }
