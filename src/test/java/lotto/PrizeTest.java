8ba4e4dafa4e2144fcb854200189e1d306515394
choomi1217 <whdudal1217@naver.com>
2023-11-23 23:45:15 +0900

    test: 지난주 당첨 번호와 일치하는 개수에 따른 랭크 반환 테스트

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -2,18 +2,30 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Set;
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.Prize;
+import lotto.domain.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 public class PrizeTest {
 
-    @DisplayName("개수에 맞는 상금을 반환")
-    @CsvSource(value = {"3,5000", "4,50000", "5,1500000", "6,2000000000"})
-    @ParameterizedTest(name = "{0} 개 일치시 {1}")
-    void prizeByRank(int matchedCount, int expectedPrize) {
-        assertThat(Prize.prizeByMatchedCount(matchedCount).price()).isEqualTo(expectedPrize);
+    @DisplayName("지난주 당첨 번호와 일치하는 개수에 따른 랭크 반환")
+    @Test
+    void prizeByRank() {
+        Lotto lotto = LottoGenerator.generate(Set.of(1, 2, 3, 4, 5, 6));
+
+        Lotto lastWeakLotto = LottoGenerator.generate(Set.of(1, 44, 43, 42, 41, 40));
+        WinningLotto winningLotto = WinningLotto.of(lastWeakLotto, LottoNumber.from(45));
+        Prize.applyPrize(winningLotto, lotto);
+
+        assertThat(Prize.FIRST.rank().score()).isEqualTo(0);
+        assertThat(Prize.SECOND.rank().score()).isEqualTo(0);
+        assertThat(Prize.THIRD.rank().score()).isEqualTo(0);
+        assertThat(Prize.FOURTH.rank().score()).isEqualTo(0);
+        assertThat(Prize.BONUS.rank().score()).isEqualTo(0);
     }
 
 }
