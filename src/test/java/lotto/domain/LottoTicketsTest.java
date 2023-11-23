4231191e01466e4ac75d389986c8553ac4b72f34
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-23 12:40:27 +0900

    test: LottoTickets 테스트 구현

--- src/test/java/lotto/domain/LottoTicketsTest.java
+++ src/test/java/lotto/domain/LottoTicketsTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.EnumMap;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -23,24 +25,15 @@ public class LottoTicketsTest {
 		assertThat(lottoTickets.getLottoTickets().size()).isEqualTo(quantity);
 	}
 
-	// 리팩토링 예정
 	@Test
 	@DisplayName("로또를 검사한다.")
 	void valid_tickets() {
-		GenerateStrategy strategy = new ManualGenerateStrategy(1,2,3,4,5,6);
+		GenerateStrategy strategy = new ManualGenerateStrategy(1, 2, 3, 4, 5, 6);
 		LottoTickets lottoTickets = new LottoTickets(1, strategy);
-		LottoChecker lottoChecker = new LottoChecker(1,2,3,7,8,9);
-		EnumMap<Rank, Integer> map = lottoTickets.checkTicket(lottoChecker);
-		assertThat(map.get(Rank.FOURTH)).isEqualTo(1);
-	}
-
-	// 리팩토링 예정
-	@Test
-	@DisplayName("로또 수익률을 계산한다.")
-	void calc_return_rate() {
-		GenerateStrategy strategy = new ManualGenerateStrategy(1,2,3,4,8,9);
-		LottoTickets lottoTickets = new LottoTickets(5, strategy);
-		lottoTickets.checkTicket(new LottoChecker(1,2,3,4,5,6));
-		assertThat(lottoTickets.calcReturnRate(5000)).isEqualTo(2000.0);
+		LottoChecker lottoChecker = new LottoChecker(1, 2, 3, 4, 5, 6, 7);
+		List<Rank> ranks = lottoTickets.checkTickets(lottoChecker);
+		assertThat(ranks.get(0)).isEqualTo(Rank.SECOND);
 	}
 }
+
+
