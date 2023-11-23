3878a6ae269d2d5a0806ed46998a60a6c584966f
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-23 23:41:42 +0900

    feat : LottoController 로또 게임 실행 기능 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,52 @@
+package lotto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoWinningStatistics;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.domain.RankResult;
+import lotto.domain.YieldCalculator;
+import lotto.dto.LottosDto;
+import lotto.dto.RankResultDto;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final LottoMachine lottoMachine;
+    private final OutputView outputView;
+
+    public LottoController(LottoMachine lottoMachine, OutputView outputView) {
+        this.lottoMachine = lottoMachine;
+        this.outputView = outputView;
+    }
+
+    public LottosDto buyLottos(long fee) {
+        Money money = new Money(fee);
+        long lottoQuantity = money.lottoQuantity();
+        outputView.printPurchasedLottoCnt(lottoQuantity);
+        Lottos purchasedLottos = lottoMachine.createLottos(lottoQuantity);
+        LottosDto lottosDto = LottosDto.valueOf(purchasedLottos);
+        outputView.printPurchasedLottos(lottosDto);
+        return lottosDto;
+    }
+
+    public void informRankStatistics(LottosDto lottosDto, List<Integer> winnerLotto, long cost) {
+        List<RankResult> rankResults = createLottoWinningStatistics(winnerLotto).informStatistics(lottosDto.toLottos());
+        outputView.printLottoRankStatistics(createRankResultDtos(rankResults));
+        outputView.printLottoYield(YieldCalculator.calculate(cost, rankResults));
+    }
+
+    private LottoWinningStatistics createLottoWinningStatistics(List<Integer> winnerLotto) {
+        return new LottoWinningStatistics(new Lotto(winnerLotto));
+    }
+
+    private List<RankResultDto> createRankResultDtos(List<RankResult> rankResults) {
+        List<RankResultDto> rankResultDtos = new ArrayList<>();
+        for (RankResult rankResult : rankResults) {
+            rankResultDtos.add(RankResultDto.valueOf(rankResult));
+        }
+        return rankResultDtos;
+    }
+}
