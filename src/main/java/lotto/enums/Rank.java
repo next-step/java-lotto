3b81b703023ed41e2568ba1c70a0d943ecbd2fdc
Hyesooo <hyesookim95@gmail.com>
2023-11-24 16:10:28 +0900

    refactor: 숫자 표현 방식 수정

--- src/main/java/lotto/enums/Rank.java
+++ src/main/java/lotto/enums/Rank.java
@@ -8,9 +8,9 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toMap;
 
 public enum Rank {
-    FIRST_PLACE(6, 2000000000),
-    SECOND_PLACE(5, 150000000),
-    THIRD_PLACE(4, 50000),
+    FIRST_PLACE(6, 2_000_000_000),
+    SECOND_PLACE(5, 150_000_000),
+    THIRD_PLACE(4, 50_000),
     FOURTH_PLACE(3, 5000);
 
     private final long matchCount;
