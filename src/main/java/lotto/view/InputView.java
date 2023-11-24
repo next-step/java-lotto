2a85965ed31db59433be7821e896bc883c0b5e68
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-24 21:02:50 +0900

    refactor : InputView 필드의 접근 제어자 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,14 +7,14 @@ import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
 public class InputView {
-    public static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
-    public static final String INPUT_WINNER_NUMBERS_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNER_NUMBERS_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
 
-    public static final String INPUT_PURCHASE_AMOUNT_EXCEPTION = "구입금액은 숫자만 입력할 수 있습니다.";
-    public static final String INPUT_WINNER_NUMBERS_EXCEPTION = "당첨 번호는 숫자만 입력할 수 있습니다.";
-    public static final String INPUT_PATTERN_EXCEPTION = "당첨_번호의_구분자를_콤마로_입력해주세요";
+    private static final String INPUT_PURCHASE_AMOUNT_EXCEPTION = "구입금액은 숫자만 입력할 수 있습니다.";
+    private static final String INPUT_WINNER_NUMBERS_EXCEPTION = "당첨 번호는 숫자만 입력할 수 있습니다.";
+    private static final String INPUT_PATTERN_EXCEPTION = "당첨_번호의_구분자를_콤마로_입력해주세요";
 
-    public static final String INPUT_WINNER_LOTTO_PATTERN = ",";
+    private static final String INPUT_WINNER_LOTTO_PATTERN = ",";
 
     private final Scanner scanner;
 
