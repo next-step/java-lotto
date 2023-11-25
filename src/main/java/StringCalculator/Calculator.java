89451cc27ae841184358ca0dd1d0652396aabda7
joomin <jmnl225@github.com>
2023-11-25 18:01:43 +0900

    <fix: step1> remove method

--- src/main/java/StringCalculator/Calculator.java
+++ src/main/java/StringCalculator/Calculator.java
@@ -33,26 +33,6 @@ public class Calculator {
             throw new IllegalArgumentException();
         }
     }
-
-    private int matchDirective(String directive, int a, int b){
-        if (directive.equals("+")){
-            return Operator.PLUS.calculate(a, b);
-        }
-
-        if (directive.equals("-")){
-            return Operator.MINUS.calculate(a, b);
-        }
-
-        if (directive.equals("*")){
-            return Operator.MULTIPLICATION.calculate(a, b);
-        }
-
-        if (directive.equals("/")){
-            return Operator.DIVISION.calculate(a, b);
-        }
-
-        return 0;
-    }
     
 
 }
