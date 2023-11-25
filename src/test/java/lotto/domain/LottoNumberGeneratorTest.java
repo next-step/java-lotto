1655d1a597bf50295bf59903903aeccc71b6a369
kyunghyun-park <qqqq_o@naver.com>
2023-11-26 00:04:25 +0900

    feat: 수동 로또 번호 발급 로직 추가

--- src/test/java/lotto/domain/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumberGeneratorTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberGeneratorTest {
 
     @Test
-    @DisplayName("6개 숫자의 로또 번호가 생성된다.")
-    void generated_lotto_numbers_size() {
+    @DisplayName("6개 숫자의 자동 로또 번호가 생성된다.")
+    void auto_lotto_numbers_size() {
         LottoNumberGenerator lottoNumberGenerator = new AutoLottoNumberGenerator();
 
         List<Integer> lottoNumbers = lottoNumberGenerator.generate();
@@ -21,12 +22,30 @@ public class LottoNumberGeneratorTest {
 
     @Test
     @DisplayName("로또가 다른 숫자로 생성된 게 맞는지 확인한다.")
-    void generated_lotto_numbers_randomly() {
+    void auto_lotto_numbers_randomly() {
         LottoNumberGenerator lottoNumberGenerator = new AutoLottoNumberGenerator();
 
         List<Integer> numbers1 = lottoNumberGenerator.generate();
         List<Integer> numbers2 = lottoNumberGenerator.generate();
 
         assertThat(numbers1).isNotEqualTo(numbers2);
+    }
+
+    @Test
+    @DisplayName("문자열을 입력하면 로또 번호로 변환한다.")
+    void manual_lotto_numbers() {
+        LottoNumberGenerator lottoNumberGenerator = new ManualLottoNumberGenerator("1, 22, 33, 12, 34, 6");
+
+        List<Integer> lottoNumbers = lottoNumberGenerator.generate();
+
+        assertThat(lottoNumbers).containsExactly(1, 6, 12, 22, 33, 34);
+    }
+
+    @Test
+    @DisplayName("수동 로또 번호 생성 시 입력 형식에 맞지 않으면 예외를 던진다.")
+    void manual_pattern_exception() {
+        assertThatThrownBy(() -> new ManualLottoNumberGenerator("1, 2, 3, 4,"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값이 형식에 맞지 않습니다.");
     }
 }
