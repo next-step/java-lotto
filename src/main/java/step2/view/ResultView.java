9a39906d28d448d59910453b58b8e27937b7781d
엄봉수(Bongsu Um)/VAS개발팀/SKP <michael.um@sk.com>
2023-11-27 00:04:06 +0900

    step4 진행

--- src/main/java/step2/view/ResultView.java
+++ src/main/java/step2/view/ResultView.java
@@ -5,11 +5,11 @@ import step2.domain.*;
 import java.util.*;
 
 public class ResultView {
-    public void printLottoCount(List<LottoTicket> tickets) {
+    public static void printLottoCount(List<LottoTicket> tickets) {
         System.out.println(tickets.size() + "개를 구매했습니다.");
     }
 
-    public void printLottoTickets(List<LottoTicket> tickets) {
+    public static void printLottoTickets(List<LottoTicket> tickets) {
         for (LottoTicket ticket : tickets) {
             System.out.print("[");
             printLottoNumber(ticket);
@@ -18,7 +18,7 @@ public class ResultView {
         }
     }
 
-    private void printLottoNumber(LottoTicket ticket) {
+    private static void printLottoNumber(LottoTicket ticket) {
         ArrayList<LottoNumber> tmpTicket = new ArrayList<>(ticket.lottoNumbers());
         tmpTicket.forEach(lottoNumber -> {
             System.out.print(lottoNumber.number());
@@ -26,12 +26,16 @@ public class ResultView {
         });
     }
 
-    public void printLottoResult(int ticketMoney, LottoResult lottoResult) {
+    public static void printLottoResult(int ticketMoney, LottoResult lottoResult) {
         System.out.println("당첨 통계");
         System.out.println("---------");
         Arrays.stream(LottoRank.VALUES).forEach(rank -> {
             System.out.println(rank.matchCount()+"개 일치" + "(" + rank.reward() + "원) - " + lottoResult.matchCount(rank));
         });
         System.out.println("총 수익률은 " + new LottoProfit(lottoResult.result()).profit(ticketMoney) + "입니다.");
+    }
+
+    public static void printLottoCountNum(int manualCount, int autoCount) {
+        System.out.println("수동으로 " + manualCount + "장, 자동으로 " + autoCount + "개를 구매했습니다.");
     }
 }
