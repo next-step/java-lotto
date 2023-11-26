f5b26e23a9425f7b89ce964c17d0110741c3f0ba
Jiwon Nam <njw724@naver.com>
2023-11-26 19:59:23 +0900

    feat: 공백, 빈 문자열 예외 로직 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,25 +7,36 @@ public class StringCalculator {
         return split;
     }
 
+    public static boolean isBlank(String text) {
+        if (text == null || text.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력값이 빈문자열 또는 공백입니다.");
+        }
+        return true;
+    }
+
     public static int plus(String text) {
+        isBlank(text);
         String[] split = getSplit(text);
         int result = Integer.parseInt(split[0]) + Integer.parseInt(split[2]);
         return result;
     }
 
     public static int minus(String text) {
+        isBlank(text);
         String[] split = getSplit(text);
         int result = Integer.parseInt(split[0]) - Integer.parseInt(split[2]);
         return result;
     }
 
     public static int multiply(String text) {
+        isBlank(text);
         String[] split = getSplit(text);
         int result = Integer.parseInt(split[0]) * Integer.parseInt(split[2]);
         return result;
     }
 
     public static int divide(String text) {
+        isBlank(text);
         String[] split = getSplit(text);
         int result = Integer.parseInt(split[0]) / Integer.parseInt(split[2]);
         return result;
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,9 +1,12 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static calculator.StringCalculator.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
@@ -30,6 +33,14 @@ public class StringCalculatorTest {
     @Test
     void 나눗셈() {
         assertThat(divide("6 / 2")).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    void 입력값이_빈문자열_또는_공백이면_IllegalArgumentException_에러를_발생시킨다(String input) {
+        assertThatThrownBy(() -> isBlank(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력값이 빈문자열 또는 공백입니다.");
     }
 
 }
