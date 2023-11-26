3e9a871263cdb37add131a526160eb9589d99ee1
Hyesooo <hyesookim95@gmail.com>
2023-11-26 17:22:24 +0900

    refactor: 컨트롤러에서 입력에 관련된 영역 제거

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -6,21 +6,15 @@ import lotto.domain.WinningNumbers;
 import lotto.view.LottoInputView;
 import lotto.view.LottoResultView;
 
-import java.util.List;
-
-import static lotto.utils.StringParser.stringToIntegerList;
-
 public class LottoMain {
     public static void main(String[] args) {
         LottoInputView lottoInputView = new LottoInputView();
 
         Lotto lotto = new Lotto();
-        lotto.sellLotto(lottoInputView.inputNumber("구매금액을 입력해주세요."));
+        lotto.sellLotto(lottoInputView.inputBuyingMoney());
         LottoResultView.printLotto(lotto);
 
-        List<Integer> numbers = stringToIntegerList(lottoInputView.inputStringLine("당첨번호를 입력해주세요."));
-        WinningNumbers winningNumbers = new WinningNumbers(numbers, lottoInputView.inputNumber("보너스번호를 입력해주세요."));
-
+        WinningNumbers winningNumbers = new WinningNumbers(lottoInputView.inputWinningNumbers(), lottoInputView.inputBonusNumber());
         LottoResult lottoResult = new LottoResult(lotto.makeStatistics(winningNumbers), lotto.calcRateOfReturn(winningNumbers));
 
         LottoResultView.printResult(lottoResult);
