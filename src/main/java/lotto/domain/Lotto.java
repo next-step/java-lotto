aebe805b412d5975be2355e1c124b7752446e721
안현호(Hyeonho Ahn)/Biz플랫폼개발팀/SKP <hhahn@sk.com>
2023-11-27 13:21:21 +0900

    refactor: lottoNumber 내부구현 검증 로직 LottoNumber 클래스로 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,50 +1,21 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
 
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
 
     private LottoNumbers lottoNumbers;
 
     public Lotto() {
-        this.lottoNumbers = new LottoNumbers(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+        this.lottoNumbers = new LottoNumbers();
     }
 
     public static Lotto of(int... numbers) {
         Lotto lotto = new Lotto();
-        isPossibleLottoNumbers(numbers);
+
         lotto.lottoNumbers = new LottoNumbers(numbers);
         return lotto;
-    }
-
-    private static void isPossibleLottoNumbers(int[] numbers) {
-        duplicateCheck(numbers);
-        lengthCheck(numbers);
-        for (int number : numbers) {
-            isValidNumber(number);
-        }
-    }
-
-    private static void duplicateCheck(int[] numbers) {
-        if (Arrays.stream(numbers).distinct().count() < numbers.length) {
-            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
-        }
-    }
-
-    private static void lengthCheck(int[] numbers) {
-        if (numbers.length != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개만 가능합니다.");
-        }
-    }
-
-    private static void isValidNumber(int number) {
-        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
-            throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자만 가능합니다.");
-        }
     }
 
     public List<Integer> getLottoNumbers() {
--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -1,15 +1,19 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoNumbers {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
 
     private List<Integer> numbers;
 
-    public LottoNumbers(int ... numbers) {
+    public LottoNumbers(int... numbers) {
+        isPossibleLottoNumbers(numbers);
         this.numbers = IntStream.of(numbers).boxed().collect(Collectors.toList());
     }
 
@@ -20,8 +24,37 @@ public class LottoNumbers {
         Collections.sort(this.numbers);
     }
 
+    public LottoNumbers() {
+        this(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER);
+    }
+
     public List<Integer> getNumbers() {
         return this.numbers;
     }
 
+    private static void isPossibleLottoNumbers(int[] numbers) {
+        duplicateCheck(numbers);
+        lengthCheck(numbers);
+        for (int number : numbers) {
+            isValidNumber(number);
+        }
+    }
+
+    private static void duplicateCheck(int[] numbers) {
+        if (Arrays.stream(numbers).distinct().count() < numbers.length) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private static void lengthCheck(int[] numbers) {
+        if (numbers.length != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개만 가능합니다.");
+        }
+    }
+
+    private static void isValidNumber(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("로또 번호는 1~45 사이의 숫자만 가능합니다.");
+        }
+    }
 }
