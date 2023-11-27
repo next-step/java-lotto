2027709e3bd8bf1b53ec3cc4b87831ad1ecaddc4
h3yon <khykhy1006@gmail.com>
2023-11-28 03:48:15 +0900

    chore: step1 CalculatorServiceTest 실패 테스트 보충

--- src/test/java/calculator/application/CalculatorServiceTest.java
+++ src/test/java/calculator/application/CalculatorServiceTest.java
@@ -1,5 +1,6 @@
 package calculator.application;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,11 @@ class CalculatorServiceTest {
     void calculateTest() {
         double result = CalculatorService.calculate("2 + 3 * 4 / 2");
         assertThat(result).isEqualTo(10.00);
+    }
+
+    @Test
+    void calculateFailTest() {
+        Assertions.assertThrows(NumberFormatException.class,
+                                () -> CalculatorService.calculate("2  ++"));
     }
 }
