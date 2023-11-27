193943439ec831d5056dc8472a68893936513a59
choomi1217 <whdudal1217@naver.com>
2023-11-27 16:16:48 +0900

    refactor: 랭킹 및 보너스 랭킹 출력 로직 변경

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -4,31 +4,49 @@ import java.io.PrintStream;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 import lotto.domain.Lotto;
-import lotto.domain.LottoWallet;
 import lotto.domain.LottoRank;
+import lotto.domain.LottoWallet;
 import lotto.domain.StatisticsReport;
 
 public class ResultView {
 
     private final static String LOTTO_PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다. \n";
 
+    private static final Map<LottoRank, String> outputFormats = new HashMap<>();
+
+    static {
+        String defaultFormat = "%d개 일치(%d원) - %d 개";
+        outputFormats.put(LottoRank.SECOND, "%d개 일치, 보너스 볼 일치(%d원) - %d 개");
+
+        Arrays.stream(LottoRank.values()).forEach(
+            lottoRank -> {
+                if (lottoRank != LottoRank.SECOND) {
+                    outputFormats.put(lottoRank, defaultFormat);
+                }
+            }
+        );
+    }
+
     private final PrintStream sout;
 
     public ResultView(PrintStream sout) {
         this.sout = sout;
     }
-    
+
     public void calculateResult(int result) {
         sout.printf(LOTTO_PURCHASE_RESULT_MESSAGE, result);
     }
 
     public void out(LottoWallet lottoWallet) {
-        for (int i = 0; i < lottoWallet.totalTicketCount(); i++){
+        for (int i = 0; i < lottoWallet.totalTicketCount(); i++) {
             sout.println(lottoNumbers(lottoWallet.oneTicket(i)));
         }
     }
+
     private String lottoNumbers(Lotto lotto) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[");
@@ -42,12 +60,10 @@ public class ResultView {
 
     public void resultOut(StatisticsReport report) {
         IntStream.range(0, LottoRank.values().length).forEach(i -> {
-                LottoRank lottoRank = LottoRank.values()[i];
-                if(lottoRank.equals(LottoRank.SECOND)){
-                    System.out.printf("%d개 일치, 보너스 볼 일치(%d원) - %d 개", i, lottoRank.prize(), report.count(lottoRank));
-                }else{
-                    System.out.printf("%d개 일치(%d원) - %d 개", i, lottoRank.prize(), report.count(lottoRank));
-                }
+            LottoRank lottoRank = LottoRank.values()[i];
+            String output = String.format(outputFormats.get(lottoRank), lottoRank.rank(),
+                lottoRank.prize(), report.count(lottoRank));
+            sout.println(output);
         });
     }
 
