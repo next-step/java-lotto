3cb530dc92ef3315d91b83ea3ce2b7b8822f6bf0
kmj990929 <kmj990929@dgist.ac.kr>
2023-11-27 22:37:32 +0900

    refactor: add invalid value to err message

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -3,14 +3,14 @@ package lotto.model;
 import java.util.Objects;
 
 public class LottoNumber implements Comparable<LottoNumber> {
-    private static final String ERR_NUMBER_RANGE = "Lotto numbers should by between 1 and 45";
+    private static final String ERR_NUMBER_RANGE = "Lotto numbers should by between 1 and 45. %d is an invalid value.";
     public static final int UPPER_BOUND = 45;
     public static final int LOWER_BOUND = 1;
     private final int number;
 
     public LottoNumber(int number) {
         if (number < LOWER_BOUND || number > UPPER_BOUND) {
-            throw new IllegalArgumentException(ERR_NUMBER_RANGE);
+            throw new IllegalArgumentException(String.format(ERR_NUMBER_RANGE, number));
         }
         this.number = number;
     }
