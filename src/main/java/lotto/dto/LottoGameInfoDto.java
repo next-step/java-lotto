cc50530b96b5d7291d631cb63016ab000131254d
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-27 17:45:28 +0900

    refactor : LottoController informRankStatistics 메소드 인자수 줄이기

---
+++ src/main/java/lotto/dto/LottoGameInfoDto.java
@@ -0,0 +1,33 @@
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoGameInfoDto {
+    private final LottosDto lottosDto;
+    private final List<Integer> winnerLotto;
+    private final int bonusNumber;
+    private final long cost;
+
+    public LottoGameInfoDto(LottosDto lottosDto, List<Integer> winnerLotto, int bonusNumber, long cost) {
+        this.lottosDto = lottosDto;
+        this.winnerLotto = winnerLotto;
+        this.bonusNumber = bonusNumber;
+        this.cost = cost;
+    }
+
+    public LottosDto getLottosDto() {
+        return lottosDto;
+    }
+
+    public List<Integer> getWinnerLotto() {
+        return winnerLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public long getCost() {
+        return cost;
+    }
+}
