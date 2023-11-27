60d7e89eda6c57928a9e5145a5158ee872b74def
kmj990929 <kmj990929@dgist.ac.kr>
2023-11-27 21:42:04 +0900

    refactor: revert controller main ft

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,23 +9,11 @@ import java.util.List;
 public class LottoController {
 
     public static void main(String[] args) {
-        buyLottoAndCheckResult();
-    }
+        Lottos tickets = Lottos.issue(InputView.inputMoney());
 
-    public static void buyLottoAndCheckResult() {
-        Lottos tickets = buyLotto();
         ResultView.printCheckPurchaseMessage(tickets.count());
         ResultView.printLottoTickets(tickets);
 
-        checkLottoResult(tickets);
-    }
-
-    private static Lottos buyLotto() {
-        int money = InputView.inputMoney();
-        return Lottos.issue(money);
-    }
-
-    private static void checkLottoResult(Lottos tickets) {
         LottoNumbers winningNumber = LottoNumbers.of(InputView.inputWinningNumbers());
         LottoNumber bonusNumber = new LottoNumber(InputView.inputBonusBallNumber());
         LottoReport report = new LottoReport(tickets.ranks(winningNumber, bonusNumber));
