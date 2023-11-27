6edda313d3b3182af8202c6eaa772b45561f692f
Jiwon Nam <njw724@naver.com>
2023-11-28 01:10:24 +0900

    feat: 연산자 인터페이스 및 사칙연산 구현체 클래스 추가

---
+++ src/main/java/calculator/Divide.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class Divide implements BasicOperator{
+    @Override
+    public int calculate(int number1, int number2) {
+        return number1 / number2;
+    }
+}
