e708b9a3e9a6caef65085437e355d4305d16665c
h3yon <khykhy1006@gmail.com>
2023-11-28 03:50:55 +0900

    chore: step1 StringUtils 제거

--- src/main/java/calculator/util/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package calculator.util;
-
-public class StringUtils {
-    public static boolean isBlank(String input) {
-        return input == null || input.isBlank();
-    }
-}
--- src/main/java/calculator/validator/StringValidator.java
+++ src/main/java/calculator/validator/StringValidator.java
@@ -1,11 +1,13 @@
 package calculator.validator;
 
-import calculator.util.StringUtils;
-
 public class StringValidator {
     public static void validateForBlank(String input) {
-        if (StringUtils.isBlank(input)) {
+        if (isBlank(input)) {
             throw new IllegalArgumentException("입력값을 입력해주세요");
         }
+    }
+
+    private static boolean isBlank(String input) {
+        return input == null || input.isBlank();
     }
 }
