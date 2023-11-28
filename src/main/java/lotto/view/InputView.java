44db7cf0028eb192d8032909738ce133980bc387
kmj990929 <kmj990929@dgist.ac.kr>
2023-11-28 22:03:49 +0900

    feat: input manual ticket count

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,11 +12,17 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String NUMBERS_DELIMITER = ", ";
     private static final String QUESTION_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String QUESTION_MANUAL_TICKET_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String QUESTION_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String QUESTION_BONUS_BALL = "보너스 볼을 입력해 주세요.";
 
     public static int inputMoney() {
         System.out.println(QUESTION_PURCHASE_AMOUNT);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static int inputManualTicketCount() {
+        System.out.println(QUESTION_MANUAL_TICKET_COUNT);
         return Integer.parseInt(scanner.nextLine());
     }
 
