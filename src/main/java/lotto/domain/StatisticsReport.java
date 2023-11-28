49e0fbe27eba5a17a002f0e91b07015adb5e4ccb
choomi1217 <whdudal1217@naver.com>
2023-11-28 17:03:38 +0900

    refactor: 캐싱하는 방식으로 변경

--- src/main/java/lotto/domain/StatisticsReport.java
+++ src/main/java/lotto/domain/StatisticsReport.java
@@ -2,64 +2,53 @@ package lotto.domain;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StatisticsReport {
 
     private final LottoWallet lottoWallet;
-    private final Map<LottoRank, Integer> prizeMap;
+    private final WinningLotto winningLotto;
+    private Map<LottoRank, Long> prizeMap;
 
-    private StatisticsReport(LottoWallet lottoWallet) {
+    private StatisticsReport(LottoWallet lottoWallet, WinningLotto winningLotto) {
         this.lottoWallet = lottoWallet;
-        this.prizeMap = initializationMap();
+        this.winningLotto = winningLotto;
     }
 
-    public static StatisticsReport of(LottoWallet lottoWallet) {
-        return new StatisticsReport(lottoWallet);
-    }
-
-    private static Map<LottoRank, Integer> initializationMap() {
-        return Arrays.stream(LottoRank.values())
-            .collect(Collectors.toMap(prize -> prize, prize -> 0));
-    }
-
-    public StatisticsReport report(WinningLotto winningLotto) {
-        lottoNumberJudge(winningLotto);
-        return this;
-    }
-
-    private void lottoNumberJudge(WinningLotto winningLotto) {
-        IntStream.range(0, lottoWallet.totalTicketCount())
-            .mapToObj(
-                i -> LottoRank.prizeByMatchedCount(winningLotto.compare(lottoWallet.oneTicket(i)),
-                    winningLotto.compareBonus(lottoWallet.oneTicket(i))))
-            .filter(Objects::nonNull)
-            .forEach(prize -> {
-                this.prizeMap.put(prize, prizeMap.get(prize) + 1);
-            });
+    public static StatisticsReport from(LottoWallet lottoWallet, WinningLotto winningLotto) {
+        return new StatisticsReport(lottoWallet, winningLotto);
     }
 
     public BigDecimal rate() {
-        int totalPurchasePrice = 1000 * this.lottoWallet.totalTicketCount();
+        int totalPurchasePrice = LottoShopFactory.LOTTO_PRICE * this.lottoWallet.totalTicketCount();
         return BigDecimal.valueOf(totalPrize())
             .divide(BigDecimal.valueOf(totalPurchasePrice), 2, RoundingMode.HALF_UP)
             .stripTrailingZeros();
     }
 
-    public int totalPrize() {
-        return prizeMap.keySet().stream().mapToInt(prize -> {
-            if (0 < prizeMap.get(prize)) {
-                return prize.prize();
-            }
-            return 0;
-        }).sum();
+    public long totalPrize() {
+        return prizeMap().keySet().stream()
+            .filter(lottoRank -> prizeMap.get(lottoRank) > 0)
+            .mapToLong(LottoRank::prize)
+            .sum();
     }
 
-    public int count(LottoRank prize) {
-        return prizeMap.get(prize);
+    public long count(LottoRank prize) {
+        return prizeMap().getOrDefault(prize, 0L);
+    }
+
+    private Map<LottoRank, Long> prizeMap() {
+        if (prizeMap != null) {
+            return prizeMap;
+        }
+
+        return prizeMap = IntStream.range(0, lottoWallet.totalTicketCount()).mapToObj(
+                i -> LottoRank.prizeByMatchedCount(winningLotto.compare(lottoWallet.oneTicket(i)),
+                    winningLotto.compareBonus(lottoWallet.oneTicket(i))))
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
