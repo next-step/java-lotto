4abf4712ed585ed41eb4ed46f86dbfc2ff38971e
hoon25 <cchoon95@hanmail.net>
2023-11-29 01:17:46 +0900

    feat(WinningLotto): 보너스볼 중복 검증로직 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -6,19 +6,25 @@ public class WinningLotto {
     private final Lotto lotto;
     private final LottoNum bonusNum;
 
-    public WinningLotto(List<Integer> nums, int bonusNum) {
-        this.lotto = Lotto.of(nums);
+    public WinningLotto(Lotto lotto, int bonusNum) {
+        validateBonusNum(lotto, bonusNum);
+        this.lotto = lotto;
         this.bonusNum = new LottoNum(bonusNum);
     }
 
-    public WinningLotto(Lotto lotto, int bonusNum) {
-        this.lotto = lotto;
-        this.bonusNum = new LottoNum(bonusNum);
+    public WinningLotto(List<Integer> winningNums, int bonusNum) {
+        this(Lotto.of(winningNums), bonusNum);
     }
 
     public LottoPrize getPrize(Lotto lotto) {
         int matchCount = lotto.getMatchCount(this.lotto);
         boolean bonusMatch = lotto.match(bonusNum);
         return LottoPrize.valueOf(matchCount, bonusMatch);
+    }
+
+    private static void validateBonusNum(Lotto lotto, int bonusNum) {
+        if (lotto.match(new LottoNum(bonusNum))) {
+            throw new IllegalArgumentException("보너스볼이 로또와 중복됩니다.");
+        }
     }
 }
