8dcddb16b1e9d758fb3e8b42cd32ce87c99b84f5
perhona <perhona@kakao.com>
2023-11-28 15:03:42 +0900

    refactor(step3): Lotto 내 사용되지 않는 메서드 제거 및 관련 test 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -14,23 +14,10 @@ public class Lotto {
         return lotto;
     }
 
-    public int matchingCount(Lotto winningLotto) {
-        return (int) winningLotto.lotto()
-                .stream()
-                .filter(lotto::contains)
-                .count();
-    }
-
-    private int increaseMatchingCount(int matchingCount, int number) {
-        if (lotto.contains(number)) {
-            matchingCount++;
-        }
-        return matchingCount;
-    }
-
     public Ranking ranking(WinningLotto winningLotto) {
         int matchingCount = (int) lotto.stream().filter(winningLotto.lotto().lotto()::contains).count();
         boolean hasBonusNumber = lotto.contains(winningLotto.bonusNumber());
+
         return Ranking.of(matchingCount, hasBonusNumber);
     }
 }
