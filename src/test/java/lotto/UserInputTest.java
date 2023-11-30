d8fbd9d094b762029f8b8577b9bda9278581714c
김태현 <thxogusrla@gmail.com>
2023-12-01 00:15:06 +0900

    test: 금액 가능 여부가 추가

---
+++ src/test/java/lotto/UserInputTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.domain.UserInput;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserInputTest {
+    @DisplayName("수동금액이 크면오류")
+    @Test
+    void 수동금액이크면오류() {
+        assertThatThrownBy(() -> {
+            new UserInput(5000, 6);
+        });
+    }
+}
