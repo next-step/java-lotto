272f5c059f9f32aee7aa514d43d604f20745e4c9
김태현 <thxogusrla@gmail.com>
2023-12-01 21:09:10 +0900

    fix: 금액 가능 여부 수정

--- src/main/java/lotto/domain/UserInput.java
+++ src/main/java/lotto/domain/UserInput.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.util.NumberUtil.floorDiv;
+
 public class UserInput {
     private final int money;
     private final int passiveNumber;
@@ -11,7 +13,7 @@ public class UserInput {
     }
 
     private static void checkValidInput(int money, int passiveNumber) {
-        if(money < passiveNumber) {
+        if(floorDiv(money) < passiveNumber) {
             throw new IllegalArgumentException("입력된 금액보다 큰 수동금액을 살 수 없습니다.");
         }
     }
@@ -21,7 +23,7 @@ public class UserInput {
     }
 
     public int getFloorDivCount() {
-        return Math.floorDiv(this.money, 1000);
+        return floorDiv(this.money);
     }
 
     public int getPassiveNumber() {
--- src/main/java/lotto/util/NumberUtil.java
+++ src/main/java/lotto/util/NumberUtil.java
@@ -1,21 +1,19 @@
 package lotto.util;
 
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 public class NumberUtil {
     private static final Random random = new Random();
+    private static final int DIV_NUMBER = 1_000;
 
-    private NumberUtil() {
-    }
+    private NumberUtil() {}
 
     public static Integer randomNum() {
         return random.nextInt(45) + 1;
     }
 
     public static int floorDiv(int money) {
-        return Math.floorDiv(money, 1000);
+        return Math.floorDiv(money, DIV_NUMBER);
     }
 
 
