e82f1e5fb450e0a84ebd83cf16566e2ab0d39e34
kmj990929 <kmj990929@dgist.ac.kr>
2023-12-02 16:03:47 +0900

    refactor: CANDIDATE_NUMBERS 이동 LottoNumbers -> LottoNumber

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,10 +1,16 @@
 package lotto.model;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumber implements Comparable<LottoNumber> {
     public static final int UPPER_BOUND = 45;
     public static final int LOWER_BOUND = 1;
+    public static final List<LottoNumber> CANDIDATE_NUMBERS = IntStream.range(LOWER_BOUND, UPPER_BOUND + 1)
+            .mapToObj(LottoNumber::new)
+            .collect(Collectors.toList());
     private static final String ERR_NUMBER_RANGE = "Lotto numbers should by between 1 and 45. %d is an invalid value.";
     private final int number;
 
