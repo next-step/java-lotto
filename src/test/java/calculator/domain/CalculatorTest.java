127eddb7713a1551cc873fa146b3a99347b11c5d
chunghyunleeme <chunghyun.dev@gmail.com>
2023-12-03 21:07:57 +0900

    chore: 주석 제거

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -21,24 +21,6 @@ public class CalculatorTest {
             .hasMessageContaining("empty");
     }
 
-//    @DisplayName("입력한 문자를 공백으로 분리한다.")
-//    @Test
-//    void splitInput() {
-//        // given
-//        String input = "1 + 2 * 3 - 4";
-//
-//        // when
-//        Calculator calculator = new Calculator(input);
-//
-//        // then
-//        assertThat(calculator.operands())
-//            .hasSize(4)
-//            .contains(1);
-//        assertThat(calculator.operators().gerOperators())
-//            .hasSize(3)
-//            .contains("+");
-//    }
-
     @DisplayName("사칙연산 기호가 아닌값이 있는지 확인한다.")
     @Test
     void validateOperator() {
