00d0e874f8265b419b3f6c6dd262f18431bcf8c9
이효인 <hyoin@trycatch.kr>
2023-12-07 13:53:37 +0900

    refactor: 로또 개수 검증 로직 추가

--- src/main/java/lotto/domain/LottoBuyInfo.java
+++ src/main/java/lotto/domain/LottoBuyInfo.java
@@ -12,9 +12,13 @@ public class LottoBuyInfo {
         if (money < Lotto.PRICE) {
             throw new IllegalArgumentException("유효하지 않은 값입니다.");
         }
+        int totalCount = money / Lotto.PRICE;
+        if (totalCount < manualCount) {
+            throw new IllegalArgumentException("수동 로또의 개수는 구매금액을 초과할 수 없습니다.");
+        }
         this.money = money;
         this.manualCount = manualCount;
-        this.autoCount = (money / Lotto.PRICE) - manualCount;
+        this.autoCount = totalCount - manualCount;
     }
 
     public int getMoney() {
