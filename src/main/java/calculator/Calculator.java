64a6b4d7bea623c8b3af39af47c7aef99d01b428
kgi912 <107936103+kgi912@users.noreply.github.com>
2023-12-18 08:30:00 +0900

    Step1 문자열 계산기 (#3562)
    * docs : step1 문자열 계산기 요구사항 정리
    
    * feat : step1 문자열 계산기 기능구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,53 @@
+package calculator;
+
+public class Calculator {
+
+    public int calculate(String text) {
+        if(text == null || text.isBlank()) throw new IllegalArgumentException("입력 값으로 null 또는 공백이 올수 없습니다");
+        String[] values = text.split(" ");
+        int result = Integer.parseInt(values[0]);
+
+        for (int i = 1; i < values.length; i += 2) {
+            String operator = values[i];
+            int number = Integer.parseInt(values[i+1]);
+            result = calculateResult(result, operator, number);
+        }
+
+        return result;
+    }
+
+    private int calculateResult(int result, String operator, int number) {
+        if (operator.equals("+")) {
+            return add(result, number);
+        }
+        if(operator.equals("-")){
+            return  subtract(result, number);
+        }
+        if (operator.equals("*")) {
+            return  multiply(result, number);
+        }
+        if (operator.equals("/")) {
+            return  divide(result, number);
+        }
+
+        throw new IllegalArgumentException("사칙 연산 기호가 입력되지 않았습니다.");
+    }
+
+    private int add(int first, int second) {
+        return first + second;
+    }
+
+    private int subtract(int first, int second) {
+        return first - second;
+    }
+
+    private int multiply(int first, int second) {
+        return first * second;
+    }
+
+    private int divide(int first, int second) {
+        return first / second;
+    }
+
+
+}
