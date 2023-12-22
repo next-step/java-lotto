c3552b55a73a03906f515c54fcac060194e6a8dc
ParkSeolDev <parkseol.dev@gmail.com>
2023-12-23 07:37:43 +0900

    feat : 입력한 식에 대한 공백 검증

--- src/main/java/calculator/domain/Expression.java
+++ src/main/java/calculator/domain/Expression.java
@@ -8,6 +8,7 @@ public final class Expression {
 	private static final char MINUS = '-';
 	private static final char MULTIPLY = '*';
 	private static final char DIVISION = '/';
+	private static final int CHARACTER_LENGTH = 1;
 	private final List<Digit> digits;
 	private final List<Operator> operators;
 
@@ -29,6 +30,8 @@ public final class Expression {
 	}
 
 	private void parseExpression(final String string) {
+		isEmptyCharacter(string);
+		isExpression(string);
 		char character = string.charAt(0);
 		if (character == PLUS || character == MINUS || character == MULTIPLY || character == DIVISION) {
 			operators.add(new Operator(character));
@@ -39,6 +42,18 @@ public final class Expression {
 			return;
 		}
 		throw new IllegalArgumentException("사칙 연산 기호가 아닙니다.");
+	}
+
+	private void isEmptyCharacter(final String string) {
+		if (string.isEmpty() || string.isBlank()) {
+			throw new IllegalArgumentException("연속된 공백이 입력되었습니다.");
+		}
+	}
+
+	private void isExpression(final String string) {
+		if (string.length() > CHARACTER_LENGTH) {
+			throw new IllegalArgumentException("공백을 사이에 두고 숫자나 기호를 입력하세요.");
+		}
 	}
 
 	public Digit indexOfDigits(final int index) {
--- src/test/java/calculator/controller/StringCalculatorTest.java
+++ src/test/java/calculator/controller/StringCalculatorTest.java
@@ -29,4 +29,26 @@ public class StringCalculatorTest {
 				Expression expression = new Expression(input);
 			}).withMessage("사칙 연산 기호가 아닙니다.");
 	}
+
+	@DisplayName("연속된 공백 입력")
+	@Test
+	void 연속된_공백_입력() {
+		String input = "4 *   * 3 - 4 / 3";
+
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> {
+				Expression expression = new Expression(input);
+			}).withMessage("연속된 공백이 입력되었습니다.");
+	}
+
+	@DisplayName("공백이 없는 경우 에러")
+	@Test
+	void 공백이_없는_경우_에러를_던진다() {
+		String input = "4*3-4/3";
+
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> {
+				Expression expression = new Expression(input);
+			}).withMessage("공백을 사이에 두고 숫자나 기호를 입력하세요.");
+	}
 }
