3b3c3bd7f7f2527ceb9568efcd00c2d180404dfb
ParkSeolDev <parkseol.dev@gmail.com>
2023-12-25 19:32:30 +0900

    chore : 수정

--- src/test/java/lotto/controller/LottoGeneratorTest.java
+++ src/test/java/lotto/controller/LottoGeneratorTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 
 import lotto.domain.Digit;
 import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 
 public class LottoGeneratorTest {
 	@DisplayName("로또 번호 6개 생성.")
 	@Test
 	void 로또_번호_6개_생성() {
-		assertThat(LottoGenerator.generate(1).get(0).indexOfLottos(0)
+		assertThat(new LottoGenerator(LottoGenerator.generate(1))
+			.indexOfLottosList(0).indexOfLottos(0)
 			.isBetween(new Digit(1), new Digit(45))).isTrue();
 	}
 }
