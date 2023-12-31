fa0802c76d5ed2037cf8681f30d0a36a5bcacf6c
ParkSeolDev <parkseol.dev@gmail.com>
2023-12-31 09:07:32 +0900

    test : 테스트 코드 수정

--- src/test/java/lotto/domain/TicketTest.java
+++ src/test/java/lotto/domain/TicketTest.java
@@ -23,6 +23,6 @@ public final class TicketTest {
 	@DisplayName("로또 번호 6개가 1세트 들어있는 티켓 생성.")
 	@Test
 	void 로또_번호_6개가_들어있는_1세트의_첫번째_로또_번호() {
-		// assertThat(Ticket.generate(1).get(0).indexOfLotto(0).isBetween(1, 45)).isTrue();
+		assertThat(Ticket.generate(1).get(0).lotto()).size().isEqualTo(6);
 	}
 }
