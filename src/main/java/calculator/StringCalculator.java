93a5b2cf437185961b00ee0f3dba5771ed362c40
deepredk <deepred.kim@gmail.com>
2024-03-11 23:42:48 +0900

    refactor: assertInput(), calculateOne() 메서드 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,19 +7,28 @@ import java.util.List;
 public class StringCalculator {
 
     public static int calculate(String input) {
-        if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("null 또는 공백이 들어옴");
-        }
+        assertInput(input);
+
         List<String> operands = new ArrayList<>(Arrays.asList(input.split(" ")));
         while (operands.size() > 1) {
-            Operand left = new Operand(operands.remove(0));
-            Operator operator = Operator.from(operands.remove(0));
-            Operand right = new Operand(operands.remove(0));
-
-            String result = String.valueOf(operator.compute(left, right));
-            operands.add(0, result);
+            int result = calculateOne(operands);
+            operands.add(0, String.valueOf(result));
         }
 
         return Integer.parseInt(operands.get(0));
+    }
+
+    private static void assertInput(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("null 또는 공백이 들어옴");
+        }
+    }
+
+    private static int calculateOne(List<String> operands) {
+        Operand left = new Operand(operands.remove(0));
+        Operator operator = Operator.from(operands.remove(0));
+        Operand right = new Operand(operands.remove(0));
+
+        return operator.compute(left, right);
     }
 }
