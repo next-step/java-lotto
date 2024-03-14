0aaa5fff04604ba82471c32b5940a17a434644ad
hellonayeon <hellonykwon@gmail.com>
2024-03-15 02:41:20 +0900

    Test: 사칙연산 기호가 아닌 문자열에 대한 검증 단위 테스트 추가

--- src/test/java/lotto/ExpressionValidatorTest.java
+++ src/test/java/lotto/ExpressionValidatorTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ExpressionValidatorTest {
 
@@ -11,8 +14,17 @@ public class ExpressionValidatorTest {
     @ParameterizedTest(name = "{index} ==> input {0} throw IllegalArgumentException")
     @NullAndEmptySource
     void null_공백문자(String inputString) {
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
-            ExpressionValidator.validateInputExpression(inputString);
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            ExpressionValidator.validateEmptyExpression(inputString);
+        });
+    }
+
+    @DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException 이 발생한다.")
+    @ParameterizedTest(name = "{index} ==> input {0} throw IllegalArgumentException")
+    @ValueSource(strings = {"?", "@", ".", ";"})
+    void 사칙연산_기호(String operation) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            ExpressionValidator.validateOperation(operation);
         });
     }
 
