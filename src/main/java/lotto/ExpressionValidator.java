af852751d166254ac34675ae75045c597080ef8c
hellonayeon <hellonykwon@gmail.com>
2024-03-15 02:41:41 +0900

    Feature: 사칙연산 문자열 검증 기능 추가

--- src/main/java/lotto/ExpressionValidator.java
+++ src/main/java/lotto/ExpressionValidator.java
@@ -1,8 +1,18 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExpressionValidator {
 
-    public static void validateInputExpression(String inputString) {
+    private static final Set<String> OPERATION = new HashSet<>() {{
+        add("+");
+        add("-");
+        add("*");
+        add("/");
+    }};
+
+    public static void validateEmptyExpression(String inputString) {
         if (isEmptyExpression(inputString)) {
             throw new IllegalArgumentException();
         }
@@ -10,5 +20,15 @@ public class ExpressionValidator {
 
     private static boolean isEmptyExpression(String inputString) {
         return inputString == null || inputString.isEmpty();
+    }
+
+    public static void validateOperation(String operation) {
+        if (notContains(operation)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean notContains(String operation) {
+        return !OPERATION.contains(operation);
     }
 }
