28fccf60b3c353841b22a33d6ccb037c0460f762
deepredk <deepred.kim@gmail.com>
2024-03-15 22:04:26 +0900

    refactor: `1`, `45` 상수로 선언

--- src/main/java/lotto/BallFactory.java
+++ src/main/java/lotto/BallFactory.java
@@ -5,9 +5,11 @@ import java.util.Random;
 public class BallFactory {
 
     private static final Random random = new Random();
+    private static final int LOWER_BOUND = 1;
+    private static final int UPPER_BOUND = 45;
 
     public static Ball generate() {
-        int number = random.nextInt(45) + 1;
+        int number = random.nextInt(UPPER_BOUND) + LOWER_BOUND;
         return new Ball(number);
     }
 }
