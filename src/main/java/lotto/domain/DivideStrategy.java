04a569250c4d9a279bf42d1505aad21f78f3aeb6
hellonayeon <hellonykwon@gmail.com>
2024-03-16 02:43:42 +0900

    Feature: 연산 전략 생성하는 팩토리 추가

---
+++ src/main/java/lotto/domain/DivideStrategy.java
@@ -0,0 +1,8 @@
+package lotto.domain;
+
+public class DivideStrategy implements OperationStrategy {
+    @Override
+    public int calculate(int number1, int number2) {
+        return number1 / number2;
+    }
+}
