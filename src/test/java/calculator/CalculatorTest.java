9efc35bea78316cc499ac5db4594c73fd0a99bc3
leeedohyun <ldh19738@gmail.com>
2024-03-15 10:17:36 +0900

    test: 덧셈 테스트

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import caculator.Calculator;
+
+class CalculatorTest {
+
+    @Test
+    void 덧셈_테스트() {
+        // given & when
+        final int result = Calculator.calculateTwoNumber(2, "+", 4);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+}
