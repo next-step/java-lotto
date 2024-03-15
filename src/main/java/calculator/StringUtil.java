12546218db38316852c236b49358978da5cacc1a
Sangmin <sm9171@nate.com>
2024-03-15 09:31:37 +0900

    refactor: 불변 클래스 생성자 private으로

--- src/main/java/calculator/StringUtil.java
+++ src/main/java/calculator/StringUtil.java
@@ -3,6 +3,9 @@ package calculator;
 public class StringUtil {
 	private static final String SPRIT_STRING = " ";
 
+	private StringUtil() {
+	}
+
 	public static String[] splitInput(String input) {
 		return input.split(SPRIT_STRING);
 	}
