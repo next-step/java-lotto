7ceafc370a5d4c87f2daf7937eb8caa0aa795e95
deepredk <deepred.kim@gmail.com>
2024-03-15 21:05:07 +0900

    feat: 금액에 따라 최대한 많은 로또를 구매한다

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,8 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoStore {
 
-    public static Lotto purchase(Money money) {
+    public static List<Lotto> sellAsMuchAsPossible(Money money) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        int maxAmount = money.amount() / 1000;
+        for (int i = 0; i < maxAmount; i++) {
+            Lotto lotto = sell(money);
+            lottos.add(lotto);
+        }
+
+        return lottos;
+    }
+
+    private static Lotto sell(Money money) {
         if (money.amount() < 1000) {
             throw new IllegalStateException("1000원 이상 있어야 구매할 수 있습니다");
         }
--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -2,17 +2,26 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class LottoStoreTest {
 
-    @Test
-    @DisplayName("로또 1장을 주고 1000원을 차감한다")
-    void purchase() {
-        Money money = new Money(1000);
-        Lotto lotto = LottoStore.purchase(money);
-        assertThat(lotto).isNotNull();
-        assertThat(money.amount()).isEqualTo(0);
+    @ParameterizedTest
+    @CsvSource(
+        value = {"0:0", "999:0", "1000:1", "1999:1", "2000:2", "2999:2", "3000:3"},
+        delimiter = ':'
+    )
+    @DisplayName("가진 돈에 따라 최대한 많은 로또를 판매한다")
+    void purchaseAsMuchAsPossible(int input, int expected) {
+        Money money = new Money(input);
+
+        List<Lotto> lotto = LottoStore.sellAsMuchAsPossible(money);
+        assertThat(lotto).hasSize(expected);
+
+        int change = input - (1000 * expected);
+        assertThat(money.amount()).isEqualTo(change);
     }
 }
