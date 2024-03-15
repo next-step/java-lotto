7d14b17fd3a42be485c46f6e86aa24603315f064
hellonayeon <hellonykwon@gmail.com>
2024-03-16 01:37:59 +0900

    Test: StringSplitter 테스트 수정 - DisplayName 오타 수정, ExpressionSplitter 인스턴스 생성 제거 (정적메서드 호출)

--- src/test/java/lotto/ExpressionSplitterTest.java
+++ src/test/java/lotto/ExpressionSplitterTest.java
@@ -21,8 +21,7 @@ public class ExpressionSplitterTest {
         String stringExpression = "2 + 3 * 4 / 2";
 
         // When
-        ExpressionSplitter splitter = new ExpressionSplitter();
-        Expression expression = splitter.split(stringExpression);
+        Expression expression = ExpressionSplitter.split(stringExpression);
 
         // Then
         Queue<Integer> numbers = expression.getNumbers();
@@ -33,19 +32,16 @@ public class ExpressionSplitterTest {
     }
 
     @Test
-    @DisplayName("[성공] 숫자와 연산자 개수의 조합이 맞지 않는 경우 IllegalExpressionElementSizeException 예외가 발생한다.")
+    @DisplayName("[실패] 숫자와 연산자 개수의 조합이 맞지 않는 경우 IllegalExpressionElementSizeException 예외가 발생한다.")
     void 수식_분리_실패_항개수_불일치() {
         // Given
         String stringExpression = "2 + 3 * 4 / 2 /";
 
-        // When
-        ExpressionSplitter splitter = new ExpressionSplitter();
-
-        // Then
+        // When, Then
         int actual = 8;
         assertThatExceptionOfType(IllegalExpressionElementSizeException.class)
                 .isThrownBy(() -> {
-                    splitter.split(stringExpression);
+                    ExpressionSplitter.split(stringExpression);
                 }).withMessage(MessageFormat.format("수식 항의 개수가 홀수가 아닙니다. (입력된 항 개수: {0})", actual));
     }
 }
