d4ac3a9dd148a0470b7f11850dfa2f43e47ec1d9
김지훈 <jihoon@wemakeprice.com>
2024-03-15 18:26:47 +0900

    feat: UI 로직 작성

---
+++ src/main/java/calculator/CalculatorApplication.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		Input input = new Input();
+		Output output = new Output();
+		Calculator calculator = new Calculator();
+
+		String formulaStr = input.inputFormulaStr();
+
+		int result = calculator.calculate(formulaStr);
+
+		output.showResult(result);
+	}
+
+}
---
+++ src/main/java/calculator/Output.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Output {
+
+	public void showResult(int result) {
+		System.out.println("계산 결과 = " + result);
+	}
+
+}
