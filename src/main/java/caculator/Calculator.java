f7a369db25cf761b0165b4ba06669da7ae011724
leeedohyun <ldh19738@gmail.com>
2024-03-15 10:17:51 +0900

    feat: 덧셈 구현

---
+++ src/main/java/caculator/Calculator.java
@@ -0,0 +1,11 @@
+package caculator;
+
+public class Calculator {
+
+    public static int calculateTwoNumber(final int number1, final String operation, final int number2) {
+        if ("+".equals(operation)) {
+            return number1 + number2;
+        }
+        return 0;
+    }
+}
