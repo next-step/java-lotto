440e209d35f04d0cd98e1a3eea08c84537c316b6
hellonayeon <hellonykwon@gmail.com>
2024-03-16 01:14:04 +0900

    Feature: Main 클래스 (Controller) 작성

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -3,6 +3,7 @@ package lotto;
 import lotto.domain.Calculator;
 import lotto.domain.Expression;
 import lotto.domain.ExpressionSplitter;
+import lotto.domain.ExpressionValidator;
 import lotto.exception.IllegalExpressionElementSizeException;
 import lotto.ui.InputView;
 import lotto.ui.ResultView;
@@ -13,10 +14,14 @@ public class Main {
 
         try {
             String stringExpression = InputView.readExpression();
+            ExpressionValidator.validateEmptyExpression(stringExpression);
+
             Expression expression = ExpressionSplitter.split(stringExpression);
 
             Calculator calculator = new Calculator(expression);
+            int result = calculator.calculate();
 
+            ResultView.printExpressionResult(result);
         } catch (IllegalExpressionElementSizeException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
