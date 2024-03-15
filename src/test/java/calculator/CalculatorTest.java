01610321500efb10bc55424a23f8a3afcfa8f992
kimsunghyun1995 <48992412+kimsunghyun1995@users.noreply.github.com>
2024-03-15 20:35:39 +0900

    🚀 1단계 - 문자열 계산기 (#3577)
    * doc: 사칙연산 계산기 요구사항 분석
    
    * test: 계산기 기능 구현 테스트
    
    * feat: 계산기 기능 구현
    
    * feat: 빈값 및 정수 변환 시 에외처리 추가
    
    * feat: view 구현
    
    * feat: main 단 구현
    
    * test: 사칙연산 기능 테스트 보강
    
    * refactor: format 적용

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,40 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalculatorTest {
+    @ParameterizedTest
+    @DisplayName("사칙연산 계산 기능을 테스트")
+    @CsvSource(value = {"2=2","2 + 3=5","3 - 5=-2","3 * 5=15","6 / 5=1", "2 + 3 - 5 = 0", "2 + 3 * 4 / 2 = 10"}, delimiter = '=')
+    public void calculatorLogicTest(String input, int output) {
+        int result = CalculatorLogic.calculatorLogic(input);
+        assertThat(result).isEqualTo(output);
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호 찾는 기능 테스트")
+    public void findSymbolTest() {
+        Arrays.stream(Calculator.values()).forEach(calculator -> {
+            String symbol = Calculator.findSymbol(calculator.getSymbol()).getSymbol();
+            System.out.println(symbol);
+            assertThat(symbol).isEqualTo(calculator.getSymbol());
+        });
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호가 아닐 때 에러 테스트")
+    public void findSymbolErrorTest() {
+        assertThatThrownBy(() -> Calculator.findSymbol("!").getSymbol())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("사칙연산");
+    }
+
+}
