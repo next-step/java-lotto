023608e4fd261c87c4cc2f31e1b7e2ae90664f44
hellonayeon <hellonykwon@gmail.com>
2024-03-16 02:45:44 +0900

    Feature: Calculator 클래스에 연산자 전략 팩토리 적용

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.util.OperationStrategyFactory;
+
 public class Calculator {
 
     private final Expression expression;
@@ -20,16 +22,8 @@ public class Calculator {
     }
 
     private int calculate(String operator, int number1, int number2) {
-        if (expression.isAddSign(operator)) {
-            return add(number1, number2);
-        }
-        if (expression.isSubtractSign(operator)) {
-            return subtract(number1, number2);
-        }
-        if (expression.isMultiplySign(operator)) {
-            return multiply(number1, number2);
-        }
-        return divide(number1, number2);
+        OperationStrategy operation = OperationStrategyFactory.getStrategy(operator);
+        return operation.calculate(number1, number2);
     }
 
     public int add(int num1, int num2) {
