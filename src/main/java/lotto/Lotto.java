2f8609a862190e4376e80a7874d1262be8df515d
Sangmin <sm9171@nate.com>
2024-03-16 22:58:42 +0900

    feat: 로또 숫자 생성시에 숫자 6개가 중복이 되면 안되는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Lotto {
+    private static final int RANDOM_BOUND_NUMBER = 45;
     private static final Random RANDOM = new Random();
     private List<Number> numbers;
 
@@ -17,12 +18,29 @@ public class Lotto {
         winNumbers.forEach(number -> numbers.add(new Number(number)));
     }
 
-    private static ArrayList<Number> createNumbers() {
-        ArrayList<Number> arrayList = new ArrayList<>();
+    private static List<Number> createNumbers() {
+        List<Number> newNumbers = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
-            arrayList.add(new Number(RANDOM.nextInt(46)));
+            addNumber(newNumbers);
         }
-        return arrayList;
+        return newNumbers;
+    }
+
+    private static void addNumber(List<Number> newNumbers) {
+        Number newNumber = new Number(RANDOM.nextInt(RANDOM_BOUND_NUMBER) + 1);
+        validateDuplicateNumber(newNumbers, newNumber);
+
+    }
+
+    private static void validateDuplicateNumber(List<Number> newNumbers, Number newNumber) {
+        long count = newNumbers.stream()
+                .filter(number -> number.getValue() == newNumber.getValue())
+                .count();
+        if (count > 0) {
+            addNumber(newNumbers);
+            return;
+        }
+        newNumbers.add(newNumber);
     }
 
     public int getNumberSize() {
