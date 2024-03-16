110a02f4bb0f949be9d41a2df6b8833a74b3bd8d
leejinwoo <leejinwoo1126@gmail.com>
2024-03-16 18:07:52 +0900

    test: Rank 테스트 오류 수정

--- src/test/java/lotto/model/RankTest.java
+++ src/test/java/lotto/model/RankTest.java
@@ -11,47 +11,47 @@ class RankTest {
 
     @Test
     void 전부_꽝일_경우_수익률() {
-        int money = 14_000;
+        int quantity = 14;
         Map<Integer, Integer> resultMap = Map.of();
 
-        assertThat(Rank.rateOfReturn(resultMap, money))
+        assertThat(Rank.rateOfReturn(resultMap, quantity))
                 .isEqualTo(0.0);
     }
 
 
     @Test
     void 오천원_한개_당첨될경우_수익률() {
-        int money = 14_000;
+        int quantity = 14;
         Map<Integer, Integer> resultMap = Map.of(3, 1);
 
-        assertThat(Rank.rateOfReturn(resultMap, money))
+        assertThat(Rank.rateOfReturn(resultMap, quantity))
                 .isEqualTo(0.35, Offset.offset(0.1));
     }
 
     @Test
     void 오만원_한개_당첨될경우_수익률() {
-        int money = 14_000;
+        int quantity = 14;
         Map<Integer, Integer> resultMap = Map.of(4, 1);
 
-        assertThat(Rank.rateOfReturn(resultMap, money))
+        assertThat(Rank.rateOfReturn(resultMap, quantity))
                 .isEqualTo(3.57, Offset.offset(0.1));
     }
 
     @Test
     void 이등_당첨될경우_수익률() {
-        int money = 14_000;
+        int quantity = 14;
         Map<Integer, Integer> resultMap = Map.of(5, 1);
 
-        assertThat(Rank.rateOfReturn(resultMap, money))
+        assertThat(Rank.rateOfReturn(resultMap, quantity))
                 .isEqualTo(107.14, Offset.offset(0.1));
     }
 
     @Test
     void 일등_당첨될경우_수익률() {
-        int money = 14_000;
+        int quantity = 14;
         Map<Integer, Integer> resultMap = Map.of(6, 1);
 
-        assertThat(Rank.rateOfReturn(resultMap, money))
+        assertThat(Rank.rateOfReturn(resultMap, quantity))
                 .isEqualTo(142857.14, Offset.offset(0.1));
     }
 }
