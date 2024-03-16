21a1691eaa9fec80b2a34b7f6061d518bbfab77d
hong yuseok <jericoh@naver.com>
2024-03-17 00:55:44 +0900

    feat: Operand 구현

---
+++ src/main/java/calculator/Operand.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Operand {
+    private static final String WRONG_OPERAND_VALUE_MESSAGE = "잘못된 피연사자입니다.";
+    private final double value;
+
+    private Operand(String value) {
+        if(!Validator.isNumber(value)) {
+            throw new IllegalArgumentException(WRONG_OPERAND_VALUE_MESSAGE);
+        }
+        this.value = Double.parseDouble(value);
+    }
+
+    public static Operand newOperand(String value) {
+        return new Operand(value);
+    }
+
+    public double value() {
+        return value;
+    }
+}
