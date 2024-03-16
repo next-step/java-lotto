f33c92385a3c3628113585391a5fd0f55061d9f8
Sangmin <sm9171@nate.com>
2024-03-16 16:54:44 +0900

    test: 맞춘 숫자갯수만큼 당첨금이 나온다 - 실패 케이스

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,12 +2,26 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
+    static Stream<Arguments> getAmountData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 24, 5, 11, 6), 5000),
+                Arguments.of(Arrays.asList(1, 2, 3, 5, 11, 6), 50000),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 11, 6), 1500000),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 10, 6), 2000000000)
+        );
+    }
+
     @DisplayName("로또 구매 갯수를 구한다.")
     @Test
     public void getBuyLottoNumber() {
@@ -47,5 +61,25 @@ public class LottoTest {
 
         //then
         assertThat(matchedCount).isEqualTo(4L);
+    }
+
+    @DisplayName("맞춘 숫자갯수만큼 당첨금이 나온다.")
+    @ParameterizedTest
+    @MethodSource("getAmountData")
+    public void getAmount(List<Integer> input, int result) {
+        //given
+        List<Integer> winNumbersInput = List.of(1, 2, 3, 4, 10, 6);
+        Lotto winNumber = new Lotto(winNumbersInput);
+
+        List<Integer> currentNumbers = input;
+        Lotto currentNumber = new Lotto(currentNumbers);
+
+        long matchedCount = currentNumber.compareWinNumber(winNumber);
+
+        //when
+        int amount = Amount.getAmount(matchedCount);
+
+        //then
+        assertThat(amount).isEqualTo(result);
     }
 }
