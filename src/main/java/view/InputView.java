59259753e6d39c4c0bebdd5ca876bbf26b6f457a
leeedohyun <ldh19738@gmail.com>
2024-03-16 14:19:03 +0900

    refactor: 메서드 이름 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public int input() {
+    public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return SCANNER.nextInt();
     }
