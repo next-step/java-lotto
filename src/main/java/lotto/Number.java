35946077c73516102e24f7f4ad60e8eea6ef5ca9
Sangmin <sm9171@nate.com>
2024-03-16 14:54:05 +0900

    feature: 로또 번호 Validation 기

--- src/main/java/lotto/Number.java
+++ src/main/java/lotto/Number.java
@@ -1,9 +1,18 @@
 package lotto;
 
 public class Number {
+    private static final int MINIMUM_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_LOTTO_NUMBER = 45;
     private int value;
 
     public Number(int value) {
+        if (validateLottoNumber(value)) {
+            throw new IllegalArgumentException();
+        }
         this.value = value;
+    }
+
+    private boolean validateLottoNumber(int value) {
+        return MINIMUM_LOTTO_NUMBER > value || value > MAXIMUM_LOTTO_NUMBER;
     }
 }
