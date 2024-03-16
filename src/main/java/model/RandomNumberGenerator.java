2ce257d1a41b3aaf5fb29a86b09fb6602f9aff4a
yeeji-kwon <yeji.kwon@myrealtrip.com>
2024-03-16 14:53:35 +0900

    refactor: 로또 넘버 생성기 init 기능 수정

--- src/main/java/model/RandomNumberGenerator.java
+++ src/main/java/model/RandomNumberGenerator.java
@@ -5,17 +5,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator {
-  private static final List<Integer> POSSIBLE_LOTTO_NUMBER_CANDIDATES = new ArrayList<>();
+  private static final List<Integer> POSSIBLE_LOTTO_NUMBER_CANDIDATES = generateNumbers();
 
-  private RandomNumberGenerator() {
-    for (int i = 1; i <=45; i++) {
-      POSSIBLE_LOTTO_NUMBER_CANDIDATES.add(i);
-    }
-  }
+  private RandomNumberGenerator() {}
 
   public static List<Integer> generate() {
-    RandomNumberGenerator generator = new RandomNumberGenerator();
     Collections.shuffle(POSSIBLE_LOTTO_NUMBER_CANDIDATES);
     return POSSIBLE_LOTTO_NUMBER_CANDIDATES.subList(0, 6);
+  }
+
+  private static List<Integer> generateNumbers() {
+    List<Integer> candidates = new ArrayList<>();
+    for (int i = 1; i <= 45; i++) {
+      candidates.add(i);
+    }
+    return candidates;
   }
 }
