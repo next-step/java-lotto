481d5e319dcec5ecc316440711e9be98e0e2c7d7
hong yuseok <jericoh@naver.com>
2024-03-17 03:13:47 +0900

    refactor: 테스트 코드의 DisplayName 수정

--- src/test/java/calculator/OperandTest.java
+++ src/test/java/calculator/OperandTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class OperandTest {
     @ParameterizedTest
     @ValueSource(strings = {"abc", "+123", "+", "-", "*", "/"})
-    @DisplayName("숫자가 아닌 문자열이 파라미터로 전달되는 경우 IllegalArgumentException이 발생한다.")
+    @DisplayName("Operand 인스턴스 생성 시 숫자가 아닌 문자열이 파라미터로 전달되는 경우 IllegalArgumentException이 발생한다.")
     void testInstanceCreationFail(String value) {
         assertThatThrownBy(() -> Operand.newOperand(value))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
