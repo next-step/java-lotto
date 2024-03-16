9ad6bc13e1e97b2a96b3016201159ebe6eb57f2e
hong yuseok <jericoh@naver.com>
2024-03-17 04:01:42 +0900

    feat: Operator에 메서드 추가 및 삭제 - findOperator() 추가 - isValidOperator() 삭제

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -2,18 +2,19 @@ package calculator;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.Objects.nonNull;
 
 public enum Operator {
     PLUS("+", Double::sum),
     MINUS("-", (num1, num2) -> num1 - num2),
     MULTIPLY("*", (num1, num2) -> num1 * num2),
     DIVIDE("/", (num1, num2) -> (double) (num1.intValue() / num2.intValue()));
+
+    private static final String WRONG_OPERATOR_MESSAGE = "잘못된 연산자입니다.";
 
     private final String operator;
     private final BiFunction<Double, Double, Double> formula;
@@ -26,8 +27,9 @@ public enum Operator {
     private static final Map<String, Operator> operatorMap = Collections.unmodifiableMap(Stream.of(values())
             .collect(Collectors.toMap(operator -> operator.operator, Function.identity())));
 
-    public static boolean isValidOperator(String operator) {
-        return nonNull(operatorMap.get(operator));
+    public static Operator findOperator(String operator) {
+        return Optional.ofNullable(operatorMap.get(operator))
+                .orElseThrow(() -> new IllegalArgumentException(WRONG_OPERATOR_MESSAGE));
     }
 
     public double calculate(double num1, double num2) {
