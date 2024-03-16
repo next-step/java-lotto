e2c9c04c203fa52b63fe9ea2a8e075a6201b5667
leeedohyun <ldh19738@gmail.com>
2024-03-16 13:43:17 +0900

    refactor: 로또 개수 매직 넘버 상수화

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Lotto {
 
+    private static final int MAX_LOTTO_NUMBERS = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(final List<Integer> numbers) {
@@ -12,7 +14,7 @@ public class Lotto {
     }
 
     private void validateLottoNumbers(final List<Integer> numbers) {
-        if (numbers.size() > 6) {
+        if (numbers.size() > MAX_LOTTO_NUMBERS) {
             throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
         }
     }
