35b060ccc4e182c298fee5a79cec03aee1262821
yeeji-kwon <yeji.kwon@myrealtrip.com>
2024-03-16 14:00:38 +0900

    feat: 로또 구매 금액 1000단위 테스트

--- src/main/java/model/LottoAmount.java
+++ src/main/java/model/LottoAmount.java
@@ -13,5 +13,10 @@ public class LottoAmount {
         if (amount <= 0) {
             throw new IllegalArgumentException("로또 구매 가능한 금액은 0원 이상어야합니다.");
         }
+
+        // 1000 단위니?
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("로또 구매를 위한 금액은 1000단위여야 합니다.");
+        }
     }
 }
