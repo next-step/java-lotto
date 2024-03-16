15eebd4985df227ef4bdef2d8df3e97876f5c697
Sangmin <sm9171@nate.com>
2024-03-16 14:33:32 +0900

    feat: 로또를 생성할때 숫자 6개가 나온다. - 실패테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
@@ -18,5 +19,15 @@ public class LottoTest {
 
         //then
         assertThat(buyLottoNumber).isEqualTo(14);
+    }
+
+    @DisplayName("로또를 생성할때 숫자 6개가 나온다.")
+    @Test
+    public void getLottoNumbers() {
+        //given
+        Lotto lotto = new Lotto();
+
+        //when & then
+        assertThat(lotto.getNumberSize()).isEqualTo(6);
     }
 }
