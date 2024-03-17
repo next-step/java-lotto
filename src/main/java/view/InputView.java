e92056a998940c5cf87ef0e342b4e38af89c0b75
leeedohyun <ldh19738@gmail.com>
2024-03-17 11:39:53 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,20 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String WINNING_NUMBER_SEPARATOR = ",";
 
     public int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return SCANNER.nextInt();
+    }
+
+    public List<String> inputWinningLottoNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return List.of(SCANNER.nextLine().split(WINNING_NUMBER_SEPARATOR));
     }
 }
