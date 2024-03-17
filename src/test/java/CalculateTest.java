ba0805851668e4d16392ed5f07daf80798618b64
dongock <okdong23@naver.com>
2024-03-17 15:53:39 +0900

    refactor: [1-2] CalculateTest에 DisplayName 추가

--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -1,5 +1,6 @@
 import model.Calculate;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,6 +16,7 @@ public class CalculateTest {
     }
 
     @ParameterizedTest
+    @DisplayName("더하기 계산 Test")
     @CsvSource(value = {"1:2:3", "2:4:6"}, delimiter = ':')
     void plus(int firstInput, int secondInput, int resultInput){
         int result = calculate.plus(firstInput, secondInput);
@@ -22,6 +24,7 @@ public class CalculateTest {
     }
 
     @ParameterizedTest
+    @DisplayName("빼기 계산 Test")
     @CsvSource(value = {"1:2:-1", "2:4:-2"}, delimiter = ':')
     void minus(int firstInput, int secondInput, int resultInput) {
         // when
@@ -31,6 +34,7 @@ public class CalculateTest {
     }
 
     @ParameterizedTest
+    @DisplayName("곱하기 계산 Test")
     @CsvSource(value = {"1:2:2", "2:4:8"}, delimiter = ':')
     void multiple(int firstInput, int secondInput, int resultInput) {
         // when
@@ -40,6 +44,7 @@ public class CalculateTest {
     }
 
     @ParameterizedTest
+    @DisplayName("나누기 계산 Test")
     @CsvSource(value = {"4:2:2", "32:4:8"}, delimiter = ':')
     void divide(int firstInput, int secondInput, int resultInput) {
         // when
