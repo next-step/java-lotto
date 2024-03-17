62937c835aefa5f30e1b8e6982f3a7769bf0132b
sinterhoo <whwlsdn96@gmail.com>
2024-03-17 17:11:20 +0900

    feat : 덧셈 기능 구현

---
+++ src/main/java/step1/domain/Calculator.java
@@ -0,0 +1,8 @@
+package step1.domain;
+
+public class Calculator {
+
+    public int add(int x, int y) {
+        return x+y;
+    }
+}
---
+++ src/test/java/step1/CalculatorTest.java
@@ -0,0 +1,19 @@
+package step1;
+
+import org.junit.jupiter.api.Test;
+import step1.domain.Calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void 값을_받으면_덧셈을_한다() {
+        int result = calculator.add(3,5);
+
+        assertThat(result).isEqualTo(8);
+    }
+
+}
