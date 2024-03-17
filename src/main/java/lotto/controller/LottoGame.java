867b6d11a8294e040536719d80070d34ce88e6be
nimoh <spakers38@gmail.com>
2024-03-17 22:32:43 +0900

    style: [Step2] 사용하지 않는 import 제거

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -8,7 +8,6 @@ import lotto.numberdrawer.ShuffledNumberDrawer;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class LottoGame {
