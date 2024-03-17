16c5640d6e9aafbb3882c1d1c45ba9fcd73e9839
nimoh <spakers38@gmail.com>
2024-03-17 19:44:38 +0900

    test: [Step 2] 로또 숫자가 최대 값을 넘는 경우 테스트 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.LottoConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,10 +22,20 @@ public class LottoNumberTest {
     }
 
     @Test
-    @DisplayName("from 호출할 때 매개변수 리스트의 요소가 1보다 작은 경우 IllegalArgumentException")
-    void from_number_valid_exception() {
+    @DisplayName("from 호출할 때 매개변수 리스트의 요소가 최솟값보다 작은 경우 IllegalArgumentException")
+    void from_min_number_valid_exception() {
+        int invalidNumber = LottoConstants.MIN_LOTTO_NUMBER - 1;
         assertThatThrownBy(() -> {
-            LottoNumber.from(List.of(-1, 0, 5, 10, 33, 45));
+            LottoNumber.from(List.of(invalidNumber, 3, 5, 10, 33, 45));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("from 호출할 때 매개변수 리스트의 요소가 최대값보다 작은 경우 IllegalArgumentException")
+    void from_max_number_valid_exception() {
+        int invalidNumber = LottoConstants.MAX_LOTTO_NUMBER + 1;
+        assertThatThrownBy(() -> {
+            LottoNumber.from(List.of(invalidNumber, 1, 5, 10, 33, 45));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
