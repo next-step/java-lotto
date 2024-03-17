a974029746fad1acb92ff5d61955e7e1135a67f6
dongock <okdong23@naver.com>
2024-03-17 18:35:21 +0900

    refactor: [1-8] Calcuration 구현에 따른 CalculationTest 리팩토링

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -1,5 +1,5 @@
 import model.Calculation;
-import org.junit.jupiter.api.BeforeEach;
+import model.NumberSentence;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,47 +8,50 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CalculationTest {
 
-    private Calculation calculation;
-
-    @BeforeEach
-    void setUp() {
-        calculation = new Calculation();
-    }
-
     @ParameterizedTest
     @DisplayName("더하기 계산 Test")
-    @CsvSource(value = {"1:2:3", "2:4:6"}, delimiter = ':')
-    void plus(int firstInput, int secondInput, int resultInput){
-        int result = calculation.plus(firstInput, secondInput);
+    @CsvSource(value = {"1:+:2:3", "2:+:4:6"}, delimiter = ':')
+    void plus(String firstInput, String operator, String secondInput, int resultInput){
+        // given
+        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
+        // when
+        int result = Calculation.calculate(numberSentence);
+        // then
         assertThat(result).isEqualTo(resultInput);
     }
 
     @ParameterizedTest
     @DisplayName("빼기 계산 Test")
-    @CsvSource(value = {"1:2:-1", "2:4:-2"}, delimiter = ':')
-    void minus(int firstInput, int secondInput, int resultInput) {
+    @CsvSource(value = {"1:-:2:-1", "2:-:4:-2"}, delimiter = ':')
+    void minus(String firstInput, String operator, String secondInput, int resultInput) {
+        // given
+        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
         // when
-        int result = calculation.minus(firstInput, secondInput);
+        int result = Calculation.calculate(numberSentence);
         // then
         assertThat(result).isEqualTo(resultInput);
     }
 
     @ParameterizedTest
     @DisplayName("곱하기 계산 Test")
-    @CsvSource(value = {"1:2:2", "2:4:8"}, delimiter = ':')
-    void multiple(int firstInput, int secondInput, int resultInput) {
+    @CsvSource(value = {"1:*:2:2", "2:*:4:8"}, delimiter = ':')
+    void multiple(String firstInput, String operator, String secondInput, int resultInput) {
+        // given
+        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
         // when
-        int result = calculation.multiple(firstInput, secondInput);
+        int result = Calculation.calculate(numberSentence);
         // then
         assertThat(result).isEqualTo(resultInput);
     }
 
     @ParameterizedTest
     @DisplayName("나누기 계산 Test")
-    @CsvSource(value = {"4:2:2", "32:4:8"}, delimiter = ':')
-    void divide(int firstInput, int secondInput, int resultInput) {
+    @CsvSource(value = {"4:/:2:2", "32:/:4:8"}, delimiter = ':')
+    void divide(String firstInput, String operator, String secondInput, int resultInput) {
+        // given
+        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
         // when
-        int result = calculation.divide(firstInput, secondInput);
+        int result = Calculation.calculate(numberSentence);
         // then
         assertThat(result).isEqualTo(resultInput);
     }
