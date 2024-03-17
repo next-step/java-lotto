87922948a5031f5406dffd43982e4f628db68e14
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-17 09:03:51 +0900

    Step1 - 문자열계산기 (#3588)
    * feat: 문자열계산기 짝프로그래밍
    
    * feat: 문자열계산기 indent 수정
    
    * feat: 문자열계산기 불필요한 파일 삭제 및 스타일 수정
    
    * refactor: 문자열 계산기 스타일 수정

---
+++ src/test/java/calculator/CalculateTest.java
@@ -0,0 +1,58 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static calculator.Calculator.calculate;
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("Calculate 테스트")
+public class CalculateTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("입력값이_null_또는_공백이면_IllegalArgumentException")
+    void calculate_exception(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> calculate(input));
+    }
+
+    @Test
+    void 덧셈_기본동작() {
+        assertThat(calculate("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    void 뺄셈_기본동작() {
+        assertThat(calculate("2 - 1")).isEqualTo(1);
+    }
+
+    @Test
+    void 곱셈_기본동작() {
+        assertThat(calculate("2 * 1")).isEqualTo(2);
+    }
+
+    @Test
+    void 나눗셈_기본동작() {
+        assertThat(calculate("4 / 2")).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1 + 1 - 1 * 4 / 2, 2",
+            "4 - 8 - 4 * 2 / 2, -8",
+    })
+    void 다중연산(String expression, int expected) {
+        assertThat(calculate(expression)).isEqualTo(expected);
+    }
+
+    @Test
+    void 사칙연산자_외_문자() {
+        assertThatThrownBy(() -> calculate("1 ^ 1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .withFailMessage("유요하지 않은 연산자: ^");
+    }
+}
