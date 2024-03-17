1f1265dd0582caefbc018c136ad8c347e0791c41
이승호 <seung7642@gmail.com>
2024-03-17 17:58:58 +0900

    feat: 입력 값 검증. 사칙연산 기호가 아닐 경우 예외 발생

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
 
+    private static final String DEFAULT_FORMAT_REGEX = "^\\d+(?:\\s+[+\\-*/]\\s+\\d+)*$";
+
     private Calculator() {
     }
 
@@ -7,11 +9,18 @@ public class Calculator {
         if (isBlank(input)) {
             throw new IllegalArgumentException("입력 값은 null이거나 공백일 수 없습니다.");
         }
+        if (isNotValidFormat(input)) {
+            throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
+        }
 
         return 0;
     }
 
     private static boolean isBlank(String input) {
         return input == null || input.isBlank();
+    }
+
+    private static boolean isNotValidFormat(String input) {
+        return !input.matches(DEFAULT_FORMAT_REGEX);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,16 @@ public class CalculatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력 값은 null이거나 공백일 수 없습니다.");
     }
+
+    @DisplayName("사칙연산 기호가 아닌 경우 예외 발생")
+    @Test
+    void test02() {
+        // given
+        String input = "2 + 5 | 7";
+
+        // when / then
+        Assertions.assertThatThrownBy(() -> Calculator.calculate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사칙연산 기호가 아닙니다.");
+    }
 }
