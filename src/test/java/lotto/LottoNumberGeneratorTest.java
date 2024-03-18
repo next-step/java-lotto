5a206d6eef420bfe7266a43f84162928b2f50b9c
hellonayeon <hellonykwon@gmail.com>
2024-03-18 22:32:17 +0900

    Test: 1부터 45까지의 로또 번호 랜덤 생성 단위 테스트 작성

---
+++ src/test/java/lotto/LottoNumberGeneratorTest.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import lotto.domain.LottoNumberGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoNumberGeneratorTest {
+
+    @Test
+    @DisplayName("[성공] 1부터 45까지의 중복되지 않는 번호를 생성한다.")
+    void 중복되지_않는_번호_생성() {
+        Set<Integer> numbers = LottoNumberGenerator.generate();
+        assertThat(numbers).containsAnyElementsOf(getPossibleLottoNumbers());
+    }
+
+    private Set<Integer> getPossibleLottoNumbers() {
+        return IntStream.rangeClosed(1, 45)
+                .boxed()
+                .collect(Collectors.toSet());
+    }
+}
