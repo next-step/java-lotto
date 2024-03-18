44e78a93976522ed502a0b8cd642f0522b72c8cc
nimohLee <106662308+nimohLee@users.noreply.github.com>
2024-03-18 19:12:36 +0900

    [Step1] 문자열 계산기 짝 프로그래밍 (#3592)
    * Step1 - 문자열계산기 (#3588)
    
    * feat: 문자열계산기 짝프로그래밍
    
    * feat: 문자열계산기 indent 수정
    
    * feat: 문자열계산기 불필요한 파일 삭제 및 스타일 수정
    
    * refactor: 문자열 계산기 스타일 수정
    
    * rename: [Step1] 테스트파일 이름 수정
    
    * test: [Step1] 피드백 반영 ( OperatorTest 추가 )
    
    * style: [Step1] 사용하지 않는 import문 제거, DisplayName 수정
    
    ---------
    
    Co-authored-by: boy0516 <60382864+boy0516@users.noreply.github.com>

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,60 @@
+package calculator;
+
+import java.util.LinkedList;
+import java.util.OptionalInt;
+import java.util.Queue;
+
+public class Calculator {
+
+    private static final Queue<String> expressionQueue = new LinkedList<>();
+
+    public static int calculate(String input) {
+        validEmptyInput(input);
+        initialExpressionQueue(input);
+        return calculateExpression(0, Operator.NONE);
+    }
+
+    private static void validEmptyInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void initialExpressionQueue(String input) {
+        String[] splitExpressions = input.split(" ");
+
+        for (String expression : splitExpressions) {
+            expressionQueue.offer(expression);
+        }
+    }
+
+    private static int calculateExpression(int result, Operator currentOperator) {
+        if (expressionQueue.isEmpty()) {
+            return result;
+        }
+
+        String polledExpression = expressionQueue.poll();
+        OptionalInt numberOrEmpty = getNumberOrEmpty(polledExpression);
+
+        if (numberOrEmpty.isEmpty()) {
+            currentOperator = Operator.findOperationByValue(polledExpression);
+            return calculateExpression(result, currentOperator);
+        }
+
+        result = calculate(numberOrEmpty.getAsInt(), currentOperator, result);
+
+        return calculateExpression(result, currentOperator);
+    }
+
+    private static int calculate(int expression, Operator currentOperator, int result) {
+        return currentOperator.calculate(result, expression);
+    }
+
+    private static OptionalInt getNumberOrEmpty(String expression) {
+        try {
+            return OptionalInt.of(Integer.parseInt(expression));
+        } catch (NumberFormatException e) {
+            return OptionalInt.empty();
+        }
+    }
+}
---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,35 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.function.IntBinaryOperator;
+
+public enum Operator {
+    PLUS("+", (num1, num2) -> num1 + num2),
+    MINUS("-", (num1, num2) -> num1 - num2),
+    DIVIDE("/", (num1, num2) -> num1 / num2),
+    MULTIPLY("*", (num1, num2) -> num1 * num2),
+    NONE("", (num1, num2) -> num2);
+
+    private final String value;
+    private final IntBinaryOperator operator;
+
+    Operator(String value, IntBinaryOperator operator) {
+        this.value = value;
+        this.operator = operator;
+    }
+
+    public int calculate(int result, int expression) {
+        return operator.applyAsInt(result, expression);
+    }
+
+    public static Operator findOperationByValue(String value) {
+        return Arrays.stream(Operator.values())
+                .filter(operator -> operator.isValueEqualsTo(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 연산자: " + value));
+    }
+
+    private boolean isValueEqualsTo(String value) {
+        return this.value.equals(value);
+    }
+}
