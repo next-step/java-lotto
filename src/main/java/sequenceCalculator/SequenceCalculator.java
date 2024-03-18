f011b6a69a72eaa43ef420082c6de9034ded6a42
유지웅 <poujis0@gmail.com>
2024-03-19 01:23:04 +0900

    [refactor] 사칙연산 계산 함수 리팩토링

--- src/main/java/sequenceCalculator/SequenceCalculator.java
+++ src/main/java/sequenceCalculator/SequenceCalculator.java
@@ -2,25 +2,32 @@ package sequenceCalculator;
 
 public class SequenceCalculator {
 
-    public int calculate(String input) {
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("null이나 빈 문자열은 들어올 수 없습니다.");
-        }
+    private static final String INPUT_SEPARATOR = " ";
 
-        String[] split = input.split(" ");
-        int result = 0;
-        Operator operator = Operator.PLUS;
-        for (String expression : split) {
+    private int result;
+    private Operator operator;
+
+    public SequenceCalculator() {
+        this.result = 0;
+        this.operator = Operator.PLUS;
+    }
+
+    public int calculate(String input) {
+        validInput(input);
+        for (String expression : input.split(INPUT_SEPARATOR)) {
             if (ExpressionChecker.isDigit(expression)) {
                 result = operator.calculate(result, Integer.parseInt(expression));
                 continue;
             }
-            if (ExpressionChecker.isBasicOperation(expression)) {
-                operator = Operator.findOperator(expression);
-            }
+            operator = Operator.findOperator(expression);
         }
-
         return result;
+    }
+
+    private void validInput(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("null이나 빈 문자열은 들어올 수 없습니다.");
+        }
     }
 
 }
