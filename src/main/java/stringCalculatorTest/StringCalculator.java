17bfe4571af62c0492c5af61b072374a735fa3c3
JJewon <eldertree777@gmail.com>
2024-03-19 00:57:26 +0900

    [refactor] 예외처리 로직 메서드 분리

--- src/main/java/stringCalculatorTest/StringCalculator.java
+++ src/main/java/stringCalculatorTest/StringCalculator.java
@@ -3,6 +3,8 @@ package stringCalculatorTest;
 import java.util.List;
 
 public class StringCalculator {
+    private static final List<String> OPERATION_SYMBOL = List.of("+", "-", "*", "/");
+
     public static int addition(int value1, int value2) {
         return value1 + value2;
     }
@@ -20,18 +22,15 @@ public class StringCalculator {
     }
 
     public static int calculation(String input) {
-        if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("input value is blank");
-        }
+        checkBlank(input);
 
         List<String> tokens = List.of(input.split(" "));
+        ckeckOperations(tokens);
 
-        for (int i = 1; i < tokens.size(); i += 2) {
-            if (!(tokens.get(i).equals("+") || tokens.get(i).equals("-") || tokens.get(i).equals("/") || tokens.get(i).equals("*"))) {
-                throw new IllegalArgumentException("input value is invalid operation symbol");
-            }
-        }
+        return calculate(tokens);
+    }
 
+    private static int calculate(List<String> tokens) {
         int result = Integer.parseInt(tokens.get(0));
 
         for (int i = 1; i < tokens.size(); i += 2) {
@@ -51,7 +50,24 @@ public class StringCalculator {
                 result = multiple(result, Integer.parseInt(tokens.get(i + 1)));
             }
         }
-
         return result;
+    }
+
+    private static void checkBlank(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("input value is blank");
+        }
+    }
+
+    private static void ckeckOperations(List<String> tokens) {
+        for (int i = 1; i < tokens.size(); i += 2) {
+            ckeckOpertaion(tokens.get(i));
+        }
+    }
+
+    private static void ckeckOpertaion(String token) {
+        if (!OPERATION_SYMBOL.contains(token)) {
+            throw new IllegalArgumentException("input value is invalid operation symbol");
+        }
     }
 }
