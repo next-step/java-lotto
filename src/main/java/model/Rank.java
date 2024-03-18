e45573f76d356540a57db944d2064a6d47086a9a
leeedohyun <ldh19738@gmail.com>
2024-03-18 09:42:22 +0900

    style: amount 상수 포맷 스타일 변경

--- src/main/java/model/Rank.java
+++ src/main/java/model/Rank.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public enum Rank {
 
     NONE(0, 0),
-    FIRST(2000000000, 6),
-    SECOND(150000, 5),
-    THIRD(50000, 4),
-    FOURTH(5000, 3);
+    FIRST(2_000_000_000, 6),
+    SECOND(150_000, 5),
+    THIRD(50_000, 4),
+    FOURTH(5_000, 3);
 
     private final int amount;
     private final int matchingCount;
