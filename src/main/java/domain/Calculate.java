014dbaade8ce8cb3d923ab70504b12ae0a467679
DongWooE <ehddn977@naver.com>
2024-03-18 22:02:25 +0900

    feat : 덧셈 기능 추가

--- src/main/java/domain/Calculate.java
+++ src/main/java/domain/Calculate.java
@@ -1,8 +1,34 @@
 package domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculate {
 
-    public static void process(String input) {
-        if(input == null || "".equals(input)) throw new IllegalArgumentException();
+    private static final Pattern pattern = Pattern.compile("\\d");
+
+    public static int process(String input) {
+        if(isNullOrEmpty(input)) throw new IllegalArgumentException();
+        String[] splitInput = input.split(" ");
+        int result = 0;
+        for(String str : splitInput){
+            result = handle(result, str);
+        }
+        return result;
+    }
+
+    private static int handle(int result, String str) {
+        if(isNumber(str)){
+            result += Integer.parseInt(str);
+        }
+        return result;
+    }
+
+    private static boolean isNumber(String str) {
+        return pattern.matcher(str).find();
+    }
+
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || "".equals(input);
     }
 }
