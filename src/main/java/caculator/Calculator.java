1a739728c1f6c76e63c37b91f49e76669c11a211
sseung12 <pon02129@naver.com>
2024-03-18 22:39:09 +0900

    feat : 계산로직을 OperatorType 에게 위임

--- src/main/java/caculator/Calculator.java
+++ src/main/java/caculator/Calculator.java
@@ -1,39 +1,22 @@
 package caculator;
 
-import caculator.type.OperatorType;
-
 import java.util.List;
 
 public class Calculator {
-    private final Number initNumber;
+    private Number initNumber;
     private final List<OperationPair> operationPairs;
 
-
-    public Calculator(OperationWrapper operationWrapper) {
+    public Calculator(String inputText) {
+        OperationWrapper operationWrapper = new OperationWrapper(TextParser.textToStringList(inputText));
         this.initNumber = operationWrapper.getInitNumber();
         this.operationPairs = operationWrapper.getOperationPairs();
     }
 
     public Number run() {
         for (OperationPair operationPair : operationPairs) {
-
-            Number other = operationPair.getNumber();
-            switch (operationPair.getOperatorType()) {
-                case PLUS:
-                    initNumber.plus(other);
-                    break;
-                case MINUS:
-                    initNumber.minus(other);
-                    break;
-                case DIVIDE:
-                    initNumber.divide(other);
-                    break;
-                case MULTIPLE:
-                    initNumber.multiple(other);
-                    break;
-            }
+            OperatorType operatorType = operationPair.getOperatorType();
+            initNumber = operatorType.apply(initNumber, operationPair.getNumber());
         }
-
         return initNumber;
     }
 }
--- src/test/java/caculator/CalculatorTest.java
+++ src/test/java/caculator/CalculatorTest.java
@@ -1,41 +1,23 @@
 package caculator;
 
-import caculator.type.OperatorType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class CalculatorTest {
-
-    private OperationWrapper operationWrapper;
-
-    @BeforeEach
-    void setUp() {
-        Number initNumber = new Number(5);
-        OperationPair plus = new OperationPair(OperatorType.PLUS, new Number(3));
-        OperationPair multiple = new OperationPair(OperatorType.MULTIPLE, new Number(3));
-        OperationPair divide = new OperationPair(OperatorType.DIVIDE, new Number(2));
-        OperationPair minus = new OperationPair(OperatorType.MINUS, new Number(2));
-
-        operationWrapper = new OperationWrapper(initNumber,List.of(plus,multiple,divide,minus));
-
-    }
 
     @Test
     @DisplayName("입력에 따라 연산을 진행한다 - plus test")
     void plusCalculateTest() {
 
-        Calculator calculator = new Calculator(operationWrapper);
+        Calculator calculator = new Calculator("3 + 7");
 
         Number resultNumber = calculator.run();
         Assertions.assertThat(resultNumber).usingRecursiveComparison().isEqualTo(new Number(10));
     }
-
-
 
 }
