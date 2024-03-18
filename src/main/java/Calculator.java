d01184f64ae28078338c90bcc08226f8cdf02393
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-18 15:01:29 +0900

    test 입력 1 + 3 - 3 출력 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,6 +2,16 @@ public class Calculator {
 
     public int calculate(String input) {
         String[] strings = input.split(" ");
-        return Integer.parseInt(strings[0]) + Integer.parseInt(strings[2]);
+        int result = Integer.parseInt(strings[0]);
+        for (int i = 1; i < strings.length; i++) {
+            if (i % 2 == 0 && "+".equals(strings[i - 1])) {
+                result += Integer.parseInt(strings[i]);
+            }
+
+            if (i % 2 == 0 && "-".equals(strings[i - 1])) {
+                result -= Integer.parseInt(strings[i]);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,10 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
-    @DisplayName("입력 1 + 3             || 출력 4")
+    @DisplayName("입력 1 + 3 - 3 || 출력 1")
+    @Test
+    void calculator_three_numbers() {
+        String input = "1 + 3 - 3";
+        assertThat(new Calculator().calculate(input)).isEqualTo(1);
+    }
+
+    @DisplayName("입력 1 + 3 || 출력 4")
     @Test
     void add() {
         String input = "1 + 3";
         assertThat(new Calculator().calculate(input)).isEqualTo(4);
     }
+
 }
