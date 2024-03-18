e90bb30c2e38c53174e0f7a8d33456fcddd9aed9
Hyungjun Lim <124577310+hyungjunn@users.noreply.github.com>
2024-03-18 19:36:23 +0900

    1단계 - 문자열 계산기 (#3603)
    * chore settings.gradle
    
    * delete gitkeep
    
    * docs todo리스트 작성
    
    * test 입력 1 + 3 출력 4
    
    * test 입력 1 + 3 - 3 출력 1
    
    * test 입력 1 + 3 - 3 * 3 출력 3
    
    * test 입력 1 + 3 - 3 * 3 / 3 || 출력 1
    
    * 입력 null,빈 공백 -> IllegalArgumentException throw
    
    * 사칙연산 아닐경우   -> IllegalArgumentException throw
    
    * refactor 인덴츠 1로 줄여라
    
    * refactor 고정적으로 쓰이는 것들은 상수화하라
    
    * chore package
    
    * refactor 고정적인 상수들을 enum으로 관리하라
    
    * refactor 반복적인 테스트 코드를 파리미터테스트로 리팩터링하라
    
    * test 리팩터링을 용이하게 하기위해 작은 단위로 테스트를 만들어라

---
+++ src/main/java/stringCalculator/Calculator.java
@@ -0,0 +1,98 @@
+package stringCalculator;
+
+import static stringCalculator.Operation.*;
+
+public class Calculator {
+    private static final String SPLIT_REGEX = " ";
+
+    public int calculate(String input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException("공백은 계산처리가 안됩니다!");
+        }
+
+        String[] strings = input.split(SPLIT_REGEX);
+
+        int result = Integer.parseInt(strings[0]);
+
+        for (int i = 0; i < strings.length; i++) {
+            validateOperator(i, strings);
+        }
+
+        for (int i = 1; i < strings.length; i++) {
+            result = calculate(i, result, strings);
+        }
+
+        return result;
+    }
+
+    private void validateOperator(int i, String[] strings) {
+        if (isOperationIndex(i)) {
+            judgmentFourBasicOperations(strings, i);
+        }
+    }
+
+    private int calculate(int i, int result, String[] strings) {
+        if (isNumberIndex(i)) {
+            result = add(strings, i, result);
+            result = minus(strings, i, result);
+            result = multiply(strings, i, result);
+            result = divide(strings, i, result);
+        }
+        return result;
+    }
+
+    private boolean isBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    private int divide(String[] strings, int i, int result) {
+        if (DIVIDE.isDivideOperation(strings, i)) {
+            result = DIVIDE.apply(result, Integer.parseInt(strings[i]));
+        }
+        return result;
+    }
+
+    private int multiply(String[] strings, int i, int result) {
+        if (MULTIPLY.isMultiplyOperation(strings, i)) {
+            result = MULTIPLY.apply(result, Integer.parseInt(strings[i]));
+        }
+        return result;
+    }
+
+    private int minus(String[] strings, int i, int result) {
+        if (MINUS.isMinusOperaiton(strings, i)) {
+            result = MINUS.apply(result, Integer.parseInt(strings[i]));
+        }
+        return result;
+    }
+
+    private int add(String[] strings, int i, int result) {
+        if (ADD.isAddOperaiton(strings, i)) {
+            result = ADD.apply(result, Integer.parseInt(strings[i]));
+        }
+        return result;
+    }
+
+    private boolean isNumberIndex(int i) {
+        return i % 2 == 0;
+    }
+
+    private boolean isOperationIndex(int i) {
+        return i % 2 == 1;
+    }
+
+    private void judgmentFourBasicOperations(String[] strings, int i) {
+        if (isNotFourBasicOperations(strings[i])) {
+            throw new IllegalArgumentException("사칙연산으로만 계산이 가능합니다!");
+        }
+    }
+
+    private boolean isNotFourBasicOperations(String strings) {
+        return !(
+                ADD.getOperation().equals(strings) ||
+                MINUS.getOperation().equals(strings) ||
+                MULTIPLY.getOperation().equals(strings) ||
+                DIVIDE.getOperation().equals(strings)
+        );
+    }
+}
---
+++ src/test/java/stringCalculator/CalculatorTest.java
@@ -0,0 +1,68 @@
+package stringCalculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class CalculatorTest {
+
+    @DisplayName("사칙연산 아닐경우   -> IllegalArgumentException throw")
+    @Test
+    void not_사칙연산() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> { new Calculator().calculate("1 @ 3"); })
+                .withMessage("%s!", "사칙연산으로만 계산이 가능합니다")
+                .withNoCause();
+
+    }
+
+    @DisplayName("입력 null,빈 공백 -> IllegalArgumentException throw")
+    @Test
+    void null_blank() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> { new Calculator().calculate(" "); })
+                .withMessage("%s!", "공백은 계산처리가 안됩니다")
+                .withNoCause();
+
+    }
+
+    @DisplayName("문자열 계산기에 문자열을 입력하면 정수값이 출력된다")
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 3 - 3:1", "1 + 3 - 3 * 3:3", "1 + 3 - 3 * 3 / 3:1"}, delimiter = ':')
+    void string_cal_test(String input, int expected) {
+        assertThat(new Calculator().calculate(input)).isEqualTo(expected);
+    }
+
+    @DisplayName("덧셈")
+    @Test
+    void add() {
+        String input = "1 + 3";
+        assertThat(new Calculator().calculate(input)).isEqualTo(4);
+    }
+
+    @DisplayName("뺄셈")
+    @Test
+    void minus() {
+        String input = "1 - 3";
+        assertThat(new Calculator().calculate(input)).isEqualTo(-2);
+    }
+
+    @DisplayName("곱셈")
+    @Test
+    void multiply() {
+        String input = "1 * 3";
+        assertThat(new Calculator().calculate(input)).isEqualTo(3);
+    }
+
+    @DisplayName("나눗셈")
+    @Test
+    void divide() {
+        String input = "1 / 3";
+        assertThat(new Calculator().calculate(input)).isEqualTo(0);
+    }
+
+}
