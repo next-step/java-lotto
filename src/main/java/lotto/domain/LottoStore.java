c936a703f88bb336f98e275f44e7cfb2d9304051
deepredk <deepred.kim@gmail.com>
2024-03-18 21:21:26 +0900

    refactor: 로또 가격 매직넘버 상수로 선언

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class LottoStore {
 
+    public static final int LOTTO_PRICE = 1000;
+
     public static Lottos sellAsMuchAsPossible(int money) {
         List<Lotto> lottos = new ArrayList<>();
-        int maxAmount = money / 1000;
+        int maxAmount = money / LOTTO_PRICE;
         for (int i = 0; i < maxAmount; i++) {
             Lotto lotto = sell(money);
             lottos.add(lotto);
@@ -17,7 +19,7 @@ public class LottoStore {
     }
 
     private static Lotto sell(int money) {
-        if (money < 1000) {
+        if (money < LOTTO_PRICE) {
             throw new IllegalStateException("1000원 이상 있어야 구매할 수 있습니다");
         }
         return LottoFactory.generate();
