50b93cfab31e95cf66a44f1001ddacb8f96ace04
leeedohyun <ldh19738@gmail.com>
2024-03-18 10:07:47 +0900

    test: 전체 등수에 대한 테스트 작성

--- src/test/java/model/RankTest.java
+++ src/test/java/model/RankTest.java
@@ -3,6 +3,8 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RankTest {
 
@@ -11,5 +13,48 @@ class RankTest {
         final Rank winningPrices = Rank.determine(2);
 
         assertThat(winningPrices).isEqualTo(Rank.NONE);
+    }
+
+    @Test
+    void 일치하는_개수가_6개이면서_보너스_번호가_다르면_1등을_반환한다() {
+        final Rank second = Rank.determine(6, false);
+        
+        assertThat(second).isEqualTo(Rank.FIRST);
+    }
+
+    @Test
+    void 일치하는_개수가_5개이면서_보너스_번호도_같으면_2등을_반환한다() {
+        final Rank second = Rank.determine(5, true);
+        
+        assertThat(second).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    void 일치하는_개수가_5개이면서_보너스_번호가_다르면_3등을_반환한다() {
+        final Rank second = Rank.determine(5, false);
+        
+        assertThat(second).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    void 일치하는_개수가_4개이면서_보너스_번호가_다르면_4등을_반환한다() {
+        final Rank second = Rank.determine(4, false);
+
+        assertThat(second).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
+    void 일치하는_개수가_3개이면서_보너스_번호가_다르면_5등을_반환한다() {
+        final Rank second = Rank.determine(3, false);
+
+        assertThat(second).isEqualTo(Rank.FIFTH);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void 일치하는_개수가_2개이하이면서_보너스_번호가_다르면_등수가_없다(final int matchingCount) {
+        final Rank second = Rank.determine(matchingCount, false);
+
+        assertThat(second).isEqualTo(Rank.NONE);
     }
 }
