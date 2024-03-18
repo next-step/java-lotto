1760cb76ff36f196b244789d640e991579402bc6
이승호 <seung7642@gmail.com>
2024-03-18 20:34:06 +0900

    feat: 사용자로부터 지난 주 당첨 번호를 입력받는다.

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner SCANNER = new Scanner(System.in);
-
     private InputView() {
     }
 
     public static int promptForPurchasePrice() {
         System.out.println("구입금액을 입력해 주세요.");
         try {
-            return SCANNER.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
         } catch (Exception e) {
             throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
         }
+    }
+
+    public static String promptForWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoApplication {
+
+    private static final String LOTTO_FORMAT_REGEX = "^\\d+, \\d+, \\d+, \\d+, \\d+, \\d+$";
 
     public static void main(String[] args) {
         int price = InputView.promptForPurchasePrice();
         Lottos lottos = new Lottos(price);
         ResultView.print(lottos);
+
+        Lotto lotto = new Lotto(convertWinningNumbers(InputView.promptForWinningNumbers()));
+    }
+
+    private static List<Integer> convertWinningNumbers(String input) {
+        assertionValidFormat(input);
+        List<Integer> result = new ArrayList<>();
+        for (String s : input.split(", ")) {
+            result.add(Integer.parseInt(s));
+        }
+        return result;
+    }
+
+    private static void assertionValidFormat(String input) {
+        if (!input.matches(LOTTO_FORMAT_REGEX)) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
     }
 }
