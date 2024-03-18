b38281edb488ebcffb7120728781b3f1abbb57c7
hong yuseok <jericoh@naver.com>
2024-03-18 15:04:24 +0900

    feat: Validator 구현

---
+++ src/main/java/lotto/Validator.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.regex.Pattern;
+
+import static java.util.Objects.nonNull;
+
+public class Validator {
+    private static final Pattern PATTERN_FOR_POSITIVE_INTEGER = Pattern.compile("^([1-9][0-9]*)$");
+
+    private Validator() {
+    }
+
+    public static boolean isNonBlank(String string) {
+        return nonNull(string) && !string.isBlank();
+    }
+
+    public static boolean isPositiveInteger(String numberString) {
+        return isNonBlank(numberString) && PATTERN_FOR_POSITIVE_INTEGER.matcher(numberString).find();
+    }
+
+    public static boolean isDivisible(int dividendNumber, int divisorNumber) {
+        return dividendNumber % divisorNumber == 0;
+    }
+
+    public static boolean isEqualToLength(String[] stringArray, int length) {
+        return stringArray.length == length;
+    }
+
+    public static boolean isInRange(int number, int min, int max) {
+        return min <= number && number <= max;
+    }
+}
