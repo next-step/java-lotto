0dc9368a1a8dbbbdd46ab94381ac5c80c97cb6b0
DongWooE <ehddn977@naver.com>
2024-03-18 22:54:55 +0900

    feat : 곱셈, 나눗셈, 복잡한 사칙연산 기능 추가

--- src/test/java/domain/CalculateTest.java
+++ src/test/java/domain/CalculateTest.java
@@ -32,4 +32,39 @@ class CalculateTest {
         assertThat(calculate.getResult()).isEqualTo(-1);
     }
 
+    @Test
+    void multiple(){
+        final String[] inputs = {"1", "*", "3"};
+        for(String input : inputs){
+            calculate.process(input);
+        }
+        assertThat(calculate.getResult()).isEqualTo(3);
+    }
+
+    @Test
+    void divide(){
+        final String[] inputs = {"4", "/", "2"};
+        for(String input : inputs){
+            calculate.process(input);
+        }
+        assertThat(calculate.getResult()).isEqualTo(2);
+    }
+
+    @Test
+    void complex1(){
+        final String[] inputs = {"2", "+", "2", "/", "2"};
+        for(String input : inputs){
+            calculate.process(input);
+        }
+        assertThat(calculate.getResult()).isEqualTo(2);
+    }
+
+    @Test
+    void complex2(){
+        final String[] inputs = {"2", "+", "2", "-", "1", "*", "5"};
+        for(String input : inputs){
+            calculate.process(input);
+        }
+        assertThat(calculate.getResult()).isEqualTo(15);
+    }
 }
