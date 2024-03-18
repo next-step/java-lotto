faa691dae7c152b5996e41aacbc0e8e81c079f3a
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-19 08:56:21 +0900

    로또 번호 생성기가 로또를 생성하면 6개의 숫자는 중복이 되지 않는다

--- src/main/java/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/LottoNumberGenerator.java
@@ -7,13 +7,16 @@ import java.util.List;
 public class LottoNumberGenerator {
     public List<Integer> generateLotto() {
         List<Integer> balls = new ArrayList<>();
+
         List<Integer> numbers = new ArrayList<>();
+
         for (int i = 0; i < 45; i++) {
             numbers.add(i + 1);
         }
 
+        Collections.shuffle(numbers);
+
         for (int i = 0; i < 6; i++) {
-            Collections.shuffle(numbers);
             balls.add(numbers.get(i));
         }
 
--- src/test/java/lotto/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/LottoNumberGeneratorTest.java
@@ -5,8 +5,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoNumberGeneratorTest {
+
+    @DisplayName("로또 번호 생성기가 로또를 생성하면 6개의 숫자는 중복이 되지 않는다")
+    @Test
+    void duplicate_number() {
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        List<Integer> numbers = lottoNumberGenerator.generateLotto();
+
+        List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
+
+        Assertions.assertThat(distinctNumbers.size()).isEqualTo(6);
+    }
 
     @DisplayName("로또 번호 생성기가 로또를 생성하면 6개의 숫자를 반환한다")
     @Test
