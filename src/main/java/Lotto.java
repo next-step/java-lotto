2b83b561643370518586d869be4c22ae2e1081f7
이승호 <seung7642@gmail.com>
2024-03-18 20:13:55 +0900

    refactor: 로또 번호 정렬 추가.

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -11,6 +11,7 @@ public class Lotto {
     }
 
     public Lotto(List<Integer> numbers) {
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -4,6 +4,7 @@ public class ResultView {
     }
 
     public static void print(Lottos lottos) {
+        System.out.printf("%d개를 구입하였습니다.\n", lottos.getLottoCount());
         System.out.println(lottos);
     }
 }
