2844e0a55b6972f386d4ac2583e109eb1b048079
JJewon <eldertree777@gmail.com>
2024-03-18 23:29:30 +0900

    [feat] 곱셈 기능 구현

--- src/main/java/stringCalculatorTest/StringCalculator.java
+++ src/main/java/stringCalculatorTest/StringCalculator.java
@@ -15,4 +15,9 @@ public class StringCalculator {
         String[] tokens = input.split(" ");
         return Integer.parseInt(tokens[0]) / Integer.parseInt(tokens[2]);
     }
+
+    public static int multiple(String input) {
+        String[] tokens = input.split(" ");
+        return Integer.parseInt(tokens[0]) * Integer.parseInt(tokens[2]);
+    }
 }
