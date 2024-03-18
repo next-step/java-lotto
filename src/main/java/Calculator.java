2dfffaa0d658e447095c770f0ad2f5502e593b10
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-18 15:23:32 +0900

    입력 null,빈 공백 -> IllegalArgumentException throw

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,9 @@
 public class Calculator {
 
     public int calculate(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("공백은 계산처리가 안됩니다!");
+        }
         String[] strings = input.split(" ");
         int result = Integer.parseInt(strings[0]);
         for (int i = 1; i < strings.length; i++) {
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,8 +2,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CalculatorTest {
+
+    @DisplayName("입력 null,빈 공백 -> IllegalArgumentException throw")
+    @Test
+    void null_blank() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> { new Calculator().calculate(" "); })
+                .withMessage("%s!", "공백은 계산처리가 안됩니다")
+                .withNoCause();
+
+    }
 
     @DisplayName("입력 1 + 3 - 3 * 3 / 3 || 출력 1")
     @Test
@@ -11,6 +22,7 @@ public class CalculatorTest {
         String input = "1 + 3 - 3 * 3 / 3";
         assertThat(new Calculator().calculate(input)).isEqualTo(1);
     }
+
     @DisplayName("입력 1 + 3 - 3 * 3 || 출력 3")
     @Test
     void calculator_four_numbers() {
