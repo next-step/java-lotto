9cf9184746b592a64150b3eef1ebff44b3c15d0d
leeedohyun <ldh19738@gmail.com>
2024-03-18 09:40:54 +0900

    test: 보너스 번호 예외 테스트

---
+++ src/test/java/model/BonusNumberTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BonusNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -3, 0, 46, 47})
+    void 보너스_볼의_범위가_1에서_45를_벗어나면_예외가_발생한다(final int bonusNumber) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new BonusNumber(bonusNumber));
+    }
+}
