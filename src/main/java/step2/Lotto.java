47548a0fc0929724971f7c6113f00cf9d3bd3970
sinterhoo <whwlsdn96@gmail.com>
2024-03-18 21:31:58 +0900

    feat : 로또 객체 구현

---
+++ src/main/java/step2/Lotto.java
@@ -0,0 +1,29 @@
+package step2;
+
+import java.util.*;
+
+public class Lotto {
+
+    private static final Random random = new Random();
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public Lotto() {
+        while (numbers.size() < 6) {
+            addLottoNumber(createRandomNumber());
+        }
+    }
+
+    private int createRandomNumber() {
+        return random.nextInt(45) + 1;
+    }
+
+    private void addLottoNumber(int num) {
+        if (!numbers.contains(num)) {
+            numbers.add(num);
+        }
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+}
