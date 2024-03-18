e417ed4c1cdcff248e1b29fd62e30a8607ce80b5
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-18 21:49:38 +0900

    TEST : create StringCalculatorTest

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,23 +1,22 @@
 package calculator;
 
-import calculator.fixtures.CalculatorFixture;
+import calculator.fixtures.StringCalculatorFixture;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Stack;
-import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-public class CalculatorTest {
+public class StringCalculatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
     void null_값(String expression) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Calculator().calculate(expression))
+                .isThrownBy(() -> new StringCalculator().calculate(expression))
                 .withMessageMatching("입력 값이 비어있습니다.");
     }
 
@@ -31,7 +30,7 @@ public class CalculatorTest {
     void 사칙연산_기호_아닌_경우() {
         String expression = "2 + 3 > 5";
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Calculator().calculate(expression))
+                .isThrownBy(() -> new StringCalculator().calculate(expression))
                 .withMessageMatching("사칙연산 기호가 아닙니다.");
     }
 
@@ -70,52 +69,52 @@ public class CalculatorTest {
     @Test
     void add() {
         String operator = "+";
-        assertThat(new Calculator().operate(CalculatorFixture.NUM1, CalculatorFixture.NUM2, operator))
+        assertThat(new StringCalculator().operate(StringCalculatorFixture.NUM1, StringCalculatorFixture.NUM2, operator))
                 .isEqualTo(7);
     }
 
     @Test
     void substract() {
         String operator = "-";
-        assertThat(new Calculator().operate(CalculatorFixture.NUM1, CalculatorFixture.NUM2, operator))
+        assertThat(new StringCalculator().operate(StringCalculatorFixture.NUM1, StringCalculatorFixture.NUM2, operator))
                 .isEqualTo(3);
     }
 
     @Test
     void multiply() {
         String operator = "*";
-        assertThat(new Calculator().operate(CalculatorFixture.NUM1, CalculatorFixture.NUM2, operator))
+        assertThat(new StringCalculator().operate(StringCalculatorFixture.NUM1, StringCalculatorFixture.NUM2, operator))
                 .isEqualTo(10);
     }
 
     @Test
     void divide() {
         String operator = "/";
-        assertThat(new Calculator().operate(CalculatorFixture.NUM1, CalculatorFixture.NUM2, operator))
+        assertThat(new StringCalculator().operate(StringCalculatorFixture.NUM1, StringCalculatorFixture.NUM2, operator))
                 .isEqualTo(2);
     }
 
     @Test
     void 사칙_연산() {
         String input = "2 * 3 + 4 / 2";
-        assertThat(new Calculator().calculate(input)).isEqualTo(5);
+        assertThat(new StringCalculator().calculate(input)).isEqualTo(5);
     }
 
     @Test
     void 덧셈_기호() {
         String input = "+";
-        assertThat(input).isEqualTo(CalculatorFixture.ADD);
+        assertThat(input).isEqualTo(StringCalculatorFixture.ADD);
     }
 
     @Test
     void 뺄셈_기호() {
         String input = "-";
-        assertThat(input).isEqualTo(CalculatorFixture.SUBSTRACT);
+        assertThat(input).isEqualTo(StringCalculatorFixture.SUBSTRACT);
     }
 
     @Test
     void 곱셈_기호() {
         String input = "*";
-        assertThat(input).isEqualTo(CalculatorFixture.MULTIPLY);
+        assertThat(input).isEqualTo(StringCalculatorFixture.MULTIPLY);
     }
 }
