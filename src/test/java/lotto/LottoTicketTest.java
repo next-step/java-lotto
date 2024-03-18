e57727313433f43b2f82ccedd59323533183eeb3
hellonayeon <hellonykwon@gmail.com>
2024-03-18 22:59:53 +0900

    Test: 로또의 번호와 당첨 번호 매칭하는 단위 테스트 작성

---
+++ src/test/java/lotto/LottoTicketTest.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import lotto.domain.LottoTicket;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LottoTicketTest {
+
+    @Nested
+    @DisplayName("당첨 번호와 매칭")
+    class MatchNumber {
+
+        @Test
+        @DisplayName("[성공] 당첨 번호와 3개 일치하면 3을 리턴한다.")
+        void 당첨_3개() {
+            LottoTicket lottoTicket = new LottoTicket(List.of(1, 2, 3, 4, 5, 6));
+            Assertions.assertThat(lottoTicket.matchNumbers(List.of(1, 2, 3, 43, 44, 45))).isEqualTo(3);
+        }
+
+        @Test
+        @DisplayName("[성공] 당첨 번호와 4개 일치하면 4를 리턴한다.")
+        void 당첨_4개() {
+            LottoTicket lottoTicket = new LottoTicket(List.of(1, 2, 3, 4, 5, 6));
+            Assertions.assertThat(lottoTicket.matchNumbers(List.of(1, 2, 3, 4, 44, 45))).isEqualTo(4);
+        }
+
+        @Test
+        @DisplayName("[성공] 당첨 번호와 모두 일치하면 6을 리턴한다.")
+        void 모두_일치() {
+            LottoTicket lottoTicket = new LottoTicket(List.of(1, 2, 3, 4, 5, 6));
+            Assertions.assertThat(lottoTicket.matchNumbers(List.of(1, 2, 3, 4, 5, 6))).isEqualTo(6);
+        }
+
+    }
+
+
+}
