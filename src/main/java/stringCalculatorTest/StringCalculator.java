87a4e88c8a07b6f4d909c83f7e2b6148cca62651
JJewon <eldertree777@gmail.com>
2024-03-19 00:14:48 +0900

    [refactor] 사칙연산을 하나의 메서드를 통해 연산하도록 수정

--- src/main/java/stringCalculatorTest/StringCalculator.java
+++ src/main/java/stringCalculatorTest/StringCalculator.java
@@ -1,39 +1,57 @@
 package stringCalculatorTest;
 
+import java.util.List;
+
 public class StringCalculator {
-    public static int addition(String input) {
-        String[] tokens = input.split(" ");
-        return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[2]);
+    public static int addition(int value1, int value2) {
+        return value1 + value2;
     }
 
-    public static int subtract(String input) {
-        String[] tokens = input.split(" ");
-        return Integer.parseInt(tokens[0]) - Integer.parseInt(tokens[2]);
+    public static int subtract(int value1, int value2) {
+        return value1 - value2;
     }
 
-    public static int division(String input) {
-        String[] tokens = input.split(" ");
-        return Integer.parseInt(tokens[0]) / Integer.parseInt(tokens[2]);
+    public static int division(int value1, int value2) {
+        return value1 / value2;
     }
 
-    public static int multiple(String input) {
-        String[] tokens = input.split(" ");
-        return Integer.parseInt(tokens[0]) * Integer.parseInt(tokens[2]);
+    public static int multiple(int value1, int value2) {
+        return value1 * value2;
     }
 
     public static int calculation(String input) {
-        if(input == null || input.isEmpty()){
+        if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("input value is blank");
         }
 
-        String[] tokens = input.split(" ");
+        List<String> tokens = List.of(input.split(" "));
 
-        for (int i = 1; i < tokens.length; i+=2) {
-            if(!(tokens[i] == "+" ||  tokens[i] == "-" || tokens[i] == "/" || tokens[i] == "*" )){
+        for (int i = 1; i < tokens.size(); i += 2) {
+            if (!(tokens.get(i).equals("+") || tokens.get(i).equals("-") || tokens.get(i).equals("/") || tokens.get(i).equals("*"))) {
                 throw new IllegalArgumentException("input value is invalid operation symbol");
             }
         }
 
-        return 0;
+        int result = Integer.parseInt(tokens.get(0));
+
+        for (int i = 1; i < tokens.size(); i += 2) {
+            if (tokens.get(i).equals("+")) {
+                result = addition(result, Integer.parseInt(tokens.get(i + 1)));
+            }
+
+            if (tokens.get(i).equals("-")) {
+                result = subtract(result, Integer.parseInt(tokens.get(i + 1)));
+            }
+
+            if (tokens.get(i).equals("/")) {
+                result = division(result, Integer.parseInt(tokens.get(i + 1)));
+            }
+
+            if (tokens.get(i).equals("*")) {
+                result = multiple(result, Integer.parseInt(tokens.get(i + 1)));
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/stringCalculatorTest/StringCalculatorTest.java
+++ src/test/java/stringCalculatorTest/StringCalculatorTest.java
@@ -8,33 +8,33 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringCalculatorTest {
     @ParameterizedTest(name = "덧셈만 하는 {0} 의 결과값은 {1} 이다")
-    @CsvSource(value = {"1 + 2=3", "2 + 3=5", "3 + 5=8", "5 + 8=13"}, delimiter = '=')
+    @CsvSource(value = {"1 + 2=3", "2 + 3=5", "3 + 5=8", "5 + 8 + 1=14"}, delimiter = '=')
     void additionTest(String input, int expected) {
-        int result = StringCalculator.addition(input);
+        int result = StringCalculator.calculation(input);
 
         Assertions.assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "뺄셈만 하는 {0} 의 결과값은 {1} 이다")
-    @CsvSource(value = {"1 - 2=-1", "2 - 3=-1", "3 - 5=-2", "5 - 8=-3"}, delimiter = '=')
+    @CsvSource(value = {"1 - 2=-1", "2 - 3=-1", "3 - 5=-2", "5 - 8 - 2=-5"}, delimiter = '=')
     void subtractTest(String input, int expected) {
-        int result = StringCalculator.subtract(input);
+        int result = StringCalculator.calculation(input);
 
         Assertions.assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "나눗셈만 하는 {0} 의 결과값은 {1} 이다")
-    @CsvSource(value = {"2 / 1=2", "6 / 2=3", "1 / 1=1", "12 / 3=4"}, delimiter = '=')
+    @CsvSource(value = {"2 / 1=2", "6 / 2=3", "1 / 1=1", "12 / 3 / 4=1"}, delimiter = '=')
     void divisionTest(String input, int expected) {
-        int result = StringCalculator.division(input);
+        int result = StringCalculator.calculation(input);
 
         Assertions.assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "곱셈만 하는 {0} 의 결과값은 {1} 이다")
-    @CsvSource(value = {"2 * 1=2", "6 * 2=12", "1 * 1=1", "12 * 3=36"}, delimiter = '=')
+    @CsvSource(value = {"2 * 1=2", "6 * 2=12", "1 * 1=1", "12 * 3 * 2=72"}, delimiter = '=')
     void multiplicationTest(String input, int expected) {
-        int result = StringCalculator.multiple(input);
+        int result = StringCalculator.calculation(input);
 
         Assertions.assertThat(result).isEqualTo(expected);
     }
