f8a8a239127879da04bd173cfeca8a46ca2eb0d9
ksh <tjdgus2319@gmail.com>
2024-03-19 21:46:40 +0900

    test: 로또 객체 생성시 로또 번호 정렬

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,2 +1,20 @@
-package lotto;public class lottoTest {
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTest {
+
+    @Test
+    @DisplayName("로또 객체 생성시 정렬 테스트")
+    void makeLotto() {
+        List<Integer> lottoNumbers = List.of(20,32,44,30,1);
+        Lotto lotto = new Lotto(new LottoNumbers(lottoNumbers));
+        assertThat(lotto.getLottoNumber().getLottoNumbers().get(0)).isEqualTo(1);
+    }
+
 }
