c76ce269e66586df26834cb6c7cde17021bbe6cb
026854@naver.com <026854@naver.com>
2024-03-19 15:54:25 +0900

    [STEP1] - 문자열 계산기 refactor : - assertThatIllegalArgumentException로 변경하여 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 import lotto.domain.Calculator;
 import lotto.view.InputView;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
