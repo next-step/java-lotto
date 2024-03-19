8e82e6564e5de28abacf319d559dbb1d7400be32
sunghyun <tjdgus2319@gmail.com>
2024-03-19 21:07:46 +0900

    feat: 셔플로 로또 번호 생성 기능 구현

--- src/main/java/lotto/LottoShuffle.java
+++ src/main/java/lotto/LottoShuffle.java
@@ -1,2 +1,16 @@
-package lotto;public class LottoShuffle {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoShuffle {
+    public static LottoNumbers makeLottoNumbersInRange() {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            nums.add(i);
+        }
+        Collections.shuffle(nums);
+        return new LottoNumbers(nums.subList(0,5));
+    }
 }
