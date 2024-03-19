a809b97bee82b62dfeb12ad563fff19fee667859
hong yuseok <jericoh@naver.com>
2024-03-19 13:34:17 +0900

    test: Lotto에 대한 테스트 구현

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoTest {
+    LottoNumberGeneratingStrategy lottoNumberGeneratingStrategyStub = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
+
+    @ParameterizedTest
+    @CsvSource(value = {"8, 21, 23, 41, 42, 43:0", "1, 3, 5, 14, 22, 45:3", "1, 3, 2, 4, 6, 5:6"}, delimiter = ':')
+    void testNumberOfMatch(String winningNumbersString, int expected) {
+        WinningNumbers winningNumbers = WinningNumbers.valueOf(winningNumbersString);
+        Lotto lotto = Lotto.valueOf(lottoNumberGeneratingStrategyStub);
+
+        assertThat(lotto.numberOfMatch(winningNumbers)).isEqualTo(expected);
+    }
+}
