38ed7561124d84c9b36f9dc44c59953cfca2cd7f
Nayoung Kwon <34162358+NaayoungKwon@users.noreply.github.com>
2024-03-19 18:46:12 +0900

    [Step1] 로또 문자열 계산기 구현 (#3587)
    * docs: Step1 기능 요구사항 작성
    
    * test: 실패하는 테스트 먼저 구현
    
    * test: 테스트 예상값 오류 수정
    
    * feature: calculator 구현
    
    - 사칙연산 계산
    - 정한 규칙의 표현이 들어오지 않으면 IllegalArgumentException 발생
    
    * test: 복합 사칙연산 테스트 보강
    
    * refactor: 리뷰 반영 리팩토링
    
    - Operator 클래스 분리
    - 클래스 분리에 따른 테스트 추가
    - stream 메소드 분리

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Calculator {
+
+
+  public int calculate(String expression) {
+    verifyInputIsBlank(expression);
+    String[] split = expression.split(" ");
+
+    List<Integer> numbers = findIntegerFromExpression(split);
+    List<String> strOperators = findOperatorFromExpression(split);
+    verifyInputIsAbnormalExpression(numbers, strOperators, split);
+
+    int result = numbers.get(0);
+    for(int i = 0 ; i < numbers.size() -1 ; i++){
+      result = Operator.operatorMap.get(strOperators.get(i)).apply(result, numbers.get(i+1));
+    }
+    return result;
+  }
+
+  private void verifyInputIsAbnormalExpression(List<Integer> numbers, List<String> strOperators, String[] split) {
+    if(split.length % 2 == 0){
+      throw new IllegalArgumentException("표현식의 숫자와 연산자의 배열이 올바르지 않습니다.");
+    }
+
+    if(numbers.size() + strOperators.size() != split.length){
+      throw new IllegalArgumentException("표현식에는 숫자와 연산자만 있어야 합니다.");
+    }
+  }
+
+  private List<Integer> findIntegerFromExpression(String[] expressions){
+    return  Arrays.stream(expressions)
+        .filter(this::isNumber)
+        .map(Integer::parseInt).collect(Collectors.toList());
+  }
+
+  private List<String> findOperatorFromExpression(String[] expressions){
+    return Arrays.stream(expressions)
+        .filter(Operator::supportedOperator).collect(Collectors.toList());
+  }
+
+  private void verifyInputIsBlank(String expression) {
+    if (expression == null || expression.isBlank()) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  private boolean isNumber(String number) {
+    return number.matches("[0-9]+");
+  }
+
+
+}
---
+++ src/test/java/OperatorTest.java
@@ -0,0 +1,34 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class OperatorTest {
+
+
+  @ParameterizedTest(name = "[{index}] {0} {1} {2} = {3}")
+  @CsvSource(value = {"2,+,3,5", "2,-,3,-1", "2,*,3,6", "6,/,2,3",}, delimiter = ',')
+  @DisplayName("연산자 별로 결과값을 확인한다")
+  void operate(Integer left,String operator,  Integer right, Integer expected ){
+    int result = Operator.operatorMap.get(operator).apply(left,right);
+    assertThat(result).isEqualTo(expected);
+  }
+
+  @ParameterizedTest(name = "[{index}] : {arguments}")
+  @ValueSource(strings = {"+", "-", "*", "/"})
+  @DisplayName("입력이 지원하는 연산자인지 확인")
+  void contains(String element){
+
+    assertThat(Operator.supportedOperator(element)).isTrue();
+  }
+
+  @ParameterizedTest(name = "[{index}] : {arguments}")
+  @ValueSource(strings = {"//", "%"})
+  @DisplayName("입력이 지원하지 않는 연산자인지 확인")
+  void containsFalse(String element){
+
+    assertThat(Operator.supportedOperator(element)).isFalse();
+  }
+}
