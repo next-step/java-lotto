c125763fc4c63004de911304e96fe4dd1891b4db
Sangmin <sm9171@nate.com>
2024-03-19 14:25:10 +0900

    refactor: 당첨 통계 출력 메소드 분리 * 중복되는 출력 메소드 분리로 중복제거함

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Amount;
+import lotto.domain.AmountEnum;
 import lotto.domain.Lotto;
 import lotto.domain.MyLottos;
 
@@ -32,17 +33,11 @@ public class OutputView {
 		}
 		System.out.println("당첨 통계");
 		System.out.println("---------");
-		System.out.println("3개 일치 (5000원)- " + hashMap.get(3) + "개");
-		System.out.println("4개 일치 (50000원)- " + hashMap.get(4) + "개");
-		System.out.println("5개 일치 (1500000원)- " + hashMap.get(5) + "개");
-		System.out.println("6개 일치 (2000000000원)- " + hashMap.get(6) + "개");
-		return hashMap;
-	}
-
-	private static void findWinner(final long count, final Map<Integer, Integer> hashMap) {
-		if (count >= 3) {
-			hashMap.put((int) count, hashMap.get((int) count) + 1);
+		for (int i = 3; i <= 6; i++) {
+			printWinners(hashMap, i);
 		}
+
+		return hashMap;
 	}
 
 	public void printTotalReturn(final Map<Integer, Integer> hashMap, final Integer pay) {
@@ -51,5 +46,15 @@ public class OutputView {
 			totalAmount += Amount.getAmount(i) * hashMap.get(i);
 		}
 		System.out.println("총 수익률은 " + (double) totalAmount / pay + "입니다.");
+	}
+
+	private void printWinners(final Map<Integer, Integer> hashMap, final int i) {
+		System.out.println(i + "개 일치" + AmountEnum.from(i).getAmount() + "원)- " + hashMap.get(i) + "개");
+	}
+
+	private static void findWinner(final long count, final Map<Integer, Integer> hashMap) {
+		if (count >= 3) {
+			hashMap.put((int) count, hashMap.get((int) count) + 1);
+		}
 	}
 }
