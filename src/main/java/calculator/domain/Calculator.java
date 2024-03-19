d8113369b5186dade141da702af9f69ff1b11bfb
park-cheonho <cheonho08@naver.com>
2024-03-19 22:51:52 +0900

    feat: 연산기능 구현 및 메소드 분리 작업

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,5 +1,8 @@
 package calculator.domain;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class Calculator {
 
     private static final String DELIMITER_POSITION = " ";
@@ -10,25 +13,45 @@ public class Calculator {
     private static final int START_NUMBER = 0;
 
     public static int calculator(String rawString) {
-        String[] seperateString = rawString.split(DELIMITER_POSITION);
-        int result = Integer.parseInt(seperateString[START_NUMBER]);
+        validate(rawString);
+        String[] seperateString = split(rawString);
+        int calculatingValue = setInitialValue(seperateString);
+        return calculate(seperateString, calculatingValue);
+    }
+
+    private static void validate(String rawString) {
+        if (rawString == null || rawString.isBlank()) {
+            throw new IllegalArgumentException("계산식을 입력해주세요");
+        }
+    }
+    private static int calculate(String[] seperateString, int calculatingValue) {
         for (int i = 1; i < seperateString.length-1; i+=2) {
             if (PLUS.equals(seperateString[i])) {
-                result += Integer.parseInt(seperateString[i+1]);
+                calculatingValue += Integer.parseInt(seperateString[i+1]);
                 continue;
             }
             if (MULTIPLY.equals(seperateString[i])) {
-                result *= Integer.parseInt(seperateString[i+1]);
+                calculatingValue *= Integer.parseInt(seperateString[i+1]);
                 continue;
             }
             if (SUBTRACT.equals(seperateString[i])) {
-                result -= Integer.parseInt(seperateString[i+1]);
+                calculatingValue -= Integer.parseInt(seperateString[i+1]);
                 continue;
             }
             if (DIVIDE.equals(seperateString[i])) {
-                result /= Integer.parseInt(seperateString[i+1]);
+                calculatingValue /= Integer.parseInt(seperateString[i+1]);
+                continue;
             }
+            throw new IllegalArgumentException("사칙연산 기호만 입력해주세요");
         }
-         return result;
+        return calculatingValue;
+    }
+
+    private static int setInitialValue(String[] seperateString) {
+        return Integer.parseInt(seperateString[START_NUMBER]);
+    }
+
+    private static String[] split(String rawString) {
+        return rawString.split(DELIMITER_POSITION);
     }
 }
