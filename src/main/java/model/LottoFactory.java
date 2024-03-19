934fe541cb25baf352ecd12d8048066dc22c05a7
leeedohyun <ldh19738@gmail.com>
2024-03-19 14:11:52 +0900

    feat: 수동으로 구매한 로또 생성

--- src/main/java/model/LottoFactory.java
+++ src/main/java/model/LottoFactory.java
@@ -12,4 +12,12 @@ public class LottoFactory {
         }
         return new Lottos(lottos);
     }
+
+    public static Lottos create(final List<List<Integer>> inputs) {
+        final List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < inputs.size(); i++) {
+            lottos.add(new Lotto(inputs.get(i)));
+        }
+        return new Lottos(lottos);
+    }
 }
