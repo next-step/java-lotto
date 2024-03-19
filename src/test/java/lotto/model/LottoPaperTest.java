9182870373d6cf6f63b5d0fb7f951a0f185f238b
leejinwoo <leejinwoo1126@gmail.com>
2024-03-19 21:36:31 +0900

    test: LottoPaper 여러 로또와 당첨 번호를 비교하도록 변경

--- src/test/java/lotto/model/LottoPaperTest.java
+++ src/test/java/lotto/model/LottoPaperTest.java
@@ -3,8 +3,8 @@ package lotto.model;
 import lotto.exception.InvalidLottoException;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
 import static lotto.model.LottoFactory.create;
@@ -20,41 +20,14 @@ class LottoPaperTest {
     }
 
     @Test
-    void 생성() {
-        Lotto lotto = create(1, 2, 3, 4, 5, 6);
-        LottoPaper lottoPaper = new LottoPaper(Collections.singletonList(lotto));
+    void 당첨번호와_비교하여_당첨등수를_카운팅한다() {
+        Lotto lotto1 = create(1, 2, 3, 4, 5, 6);
+        Lotto lotto2 = create(7, 8, 9, 10, 11, 12);
+        Lotto lotto3 = create(13, 8, 9, 10, 11, 14);
+        LottoPaper lottoPaper = new LottoPaper(List.of(lotto1, lotto2, lotto3));
 
-        assertThat(lottoPaper)
-                .isNotNull();
-    }
+        Prize prize = lottoPaper.matches(new WinningLotto(create(1, 8, 9, 10, 11, 12), new LottoNumber(7)));
 
-    @Test
-    void 로또번호가_모두_일치하는_경우() {
-        Lotto lotto = create(1, 2, 3, 4, 5, 6);
-        LottoPaper lottoPaper = new LottoPaper(Collections.singletonList(lotto));
-
-        Prize prize = lottoPaper.matches(new WinningLotto(create(1, 2, 3, 4, 5, 6), new LottoNumber(7)));
-
-        assertThat(prize).isEqualTo(new Prize(new EnumMap<>(Map.of(Rank.ONE, 1))));
-    }
-
-    @Test
-    void 로또번호가_모두_일치하지_않는경우() {
-        Lotto lotto = create(1, 2, 3, 4, 5, 6);
-        LottoPaper lottoPaper = new LottoPaper(Collections.singletonList(lotto));
-
-        Prize prize = lottoPaper.matches(new WinningLotto(create(7, 8, 9, 10, 11, 12), new LottoNumber(13)));
-
-        assertThat(prize).isEqualTo(new Prize(new EnumMap<>(Map.of(Rank.NO_MATCH, 1))));
-    }
-
-    @Test
-    void 로또번호가_1개만_일치하는_경우() {
-        Lotto lotto = create(1, 2, 3, 4, 5, 6);
-        LottoPaper lottoPaper = new LottoPaper(Collections.singletonList(lotto));
-
-        Prize prize = lottoPaper.matches(new WinningLotto(create(1, 8, 9, 10, 11, 12), new LottoNumber(13)));
-
-        assertThat(prize).isEqualTo(new Prize(new EnumMap<>(Map.of(Rank.NO_MATCH, 1))));
+        assertThat(prize).isEqualTo(new Prize(new EnumMap<>(Map.of(Rank.NO_MATCH, 1, Rank.FOUR, 1, Rank.TWO, 1))));
     }
 }
