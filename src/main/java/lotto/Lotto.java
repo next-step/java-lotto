bb0fba4c1aa04b36713adf22fb97725c6355079b
ksh <tjdgus2319@gmail.com>
2024-03-19 21:46:52 +0900

    feat: 로또 도메인 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,2 +1,20 @@
-package lotto;public class Lotto {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    public static final int LOTTO_PRICE = 1000;
+    private LottoNumbers lottoNumber;
+
+    public Lotto(LottoNumbers lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+
+    public LottoNumbers getLottoNumber() {
+        return lottoNumber;
+    }
+
 }
