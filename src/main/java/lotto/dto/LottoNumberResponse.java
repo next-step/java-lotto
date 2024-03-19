e59e0c39224762367d0d5ce5ff93ab13c7af4a27
leejinwoo <leejinwoo1126@gmail.com>
2024-03-19 22:20:44 +0900

    refactor: LottoNumberResponse 정적 팩토리 메소드 제거

--- src/main/java/lotto/dto/LottoNumberResponse.java
+++ src/main/java/lotto/dto/LottoNumberResponse.java
@@ -1,26 +1,12 @@
 package lotto.dto;
 
-import lotto.model.LottoNumber;
-
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class LottoNumberResponse {
     private List<String> numbers;
 
     public LottoNumberResponse(List<String> numbers) {
         this.numbers = numbers;
-    }
-
-    public static LottoNumberResponse from(Set<LottoNumber> lottoNumbers) {
-        return new LottoNumberResponse(map(lottoNumbers));
-    }
-
-    private static List<String> map(Set<LottoNumber> lottoNumbers) {
-        return lottoNumbers.stream()
-                .map(LottoNumber::toString)
-                .collect(Collectors.toList());
     }
 
     public List<String> getNumbers() {
