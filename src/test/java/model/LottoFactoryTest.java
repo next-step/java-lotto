e316bc20505dc00c4af5a0305ec52956aed0a994
leeedohyun <ldh19738@gmail.com>
2024-03-19 14:11:14 +0900

    test: 수동으로 구매한 로또 생성 테스트

--- src/test/java/model/LottoFactoryTest.java
+++ src/test/java/model/LottoFactoryTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class LottoFactoryTest {
@@ -16,5 +18,17 @@ class LottoFactoryTest {
 
         // then
         assertThat(lottos.getLottos()).hasSize(3);
+    }
+
+    @Test
+    void 수동_로또_생성_테스트() {
+        // given
+        final List<List<Integer>> numbers = List.of(List.of(1, 2, 3, 4, 5, 6), List.of(3, 4, 5, 6, 7, 8));
+
+        // when
+        final Lottos lottos = LottoFactory.create(numbers);
+
+        // then
+        assertThat(lottos.getLottos()).hasSize(2);
     }
 }
