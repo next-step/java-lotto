8404bb4b9e4d5776add76740923ca488642b6f04
Yushin Kim <kkad45@naver.com>
2024-03-19 19:23:07 +0900

    Step1  기능을 구현했습니다. (#3606)
    * java-lotto/yuus95 step1 split 테스트코드및 더하기 연산 테스트코드 구현
    
    * java-lotto/yuus95 step1 코드 리팩토링
    
    * java-lotto/yuus95 step1 마이너스 연산자 구현
    
    * java-lotto/yuus95 step1 사칙연산 기능 구현
    
    * java-lotto/yuus95 step1 코드 리팩토링
    
    * java-lotto/yuus95 step1 공백 또는 빈문자열 예외처리
    
    * java-lotto/yuus95 step1 콘솔 화면 뷰 생성
    
    * java-lotto/yuus95 step1 number 클래스 생성
    
    * java-lotto/yuus95 step1 number 클래스 생성 및 코드 리팩토링
    
    * step1/ 사칙연산 방식을 enums 방식으로 수정
    
    * step1 코드리팩토링
    
    ---------
    
    Co-authored-by: yskim <yskim@aptner.com>

---
+++ src/test/java/StringCalculationTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringCalculationTest {
+
+    @DisplayName("공백문자열 기준으로 문자를 Split할 수 있다,")
+    @Test
+    void splitByGap() {
+        assertThat(StringCalculation.splitByGap("1 + 3")).containsExactly("1", "+", "3");
+    }
+
+    @DisplayName("빈문자열 또는 Null 값이면 예외를 던진다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void splitByGapWithNullOrBlank(String str) {
+        assertThatThrownBy(() -> StringCalculation.splitByGap(str))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("문자열을 순서대로 사칙연상을 할 수 있다,")
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 3,4", "1 - 2,-1", "1 / 3,0", "5 / 2,2", "1 * 3,3", "5 * 5,25", "2 + 3 * 4 / 2,10"}, delimiterString = ",")
+    void calculate(String input, int expectedResult) {
+        int actualResult = StringCalculation.getSum(input);
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+}
---
+++ src/test/java/operations/OperationEnumTest.java
@@ -0,0 +1,28 @@
+package operations;
+
+import operations.enums.OperationEnum;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OperationEnumTest {
+
+    @DisplayName("연산자 찾기")
+    @Test
+    void calculate() {
+        Assertions.assertThat(OperationEnum.PLUS.calculate(1, 3)).isEqualTo(4);
+        Assertions.assertThat(OperationEnum.MINUS.calculate(1, 3)).isEqualTo(-2);
+        Assertions.assertThat(OperationEnum.DIVISION.calculate(1, 3)).isEqualTo(0);
+        Assertions.assertThat(OperationEnum.DIVISION.calculate(4, 3)).isEqualTo(1);
+        Assertions.assertThat(OperationEnum.MULTIPLICATION.calculate(4, 3)).isEqualTo(12);
+    }
+
+    @DisplayName("")
+    @Test
+    void calculateWithOperation() {
+        Assertions.assertThat(OperationEnum.calculate("+", new Number(1), new Number(3))).isEqualTo(4);
+        Assertions.assertThat(OperationEnum.calculate("-", new Number(1), new Number(3))).isEqualTo(-2);
+        Assertions.assertThat(OperationEnum.calculate("*", new Number(1), new Number(3))).isEqualTo(3);
+        Assertions.assertThat(OperationEnum.calculate("/", new Number(1), new Number(3))).isEqualTo(0);
+    }
+}
