8f37388dea27add51ede8396a7718637a2beb46f
naayoungkwon <kny8092@gmail.com>
2024-03-19 18:53:17 +0900

    test: lotto machine test 초안 작성

---
+++ src/test/java/domain/LottoMachineTest.java
@@ -0,0 +1,32 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoMachineTest {
+
+  LottoMachine lottoMachine;
+
+  @BeforeEach
+  void setUp() {
+    lottoMachine = new LottoMachine(1000);
+  }
+
+  @Test
+  @DisplayName("로또 한장을 생성한다")
+  void createLotto() {
+    int lottoNum = lottoMachine.createLotto(1000);
+    assertThat(lottoNum).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("로또 결과를 확인한다")
+  void getLottoResult() {
+    String result = lottoMachine.getLottoResult(List.of(1, 2, 3, 4, 5, 6));
+    assertThat(result).contains("개 일치");
+  }
+}
