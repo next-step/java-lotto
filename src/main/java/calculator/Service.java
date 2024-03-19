2f1643a18e8f0dec1c9ffb3111e2ac0cbc1fa984
박소민 <qkrthals0524@naver.com>
2024-03-19 11:51:09 +0900

    service 로직 수정

--- src/main/java/calculator/Service.java
+++ src/main/java/calculator/Service.java
@@ -2,30 +2,35 @@ package calculator;
 
 public class Service {
 
-    private Calculator calculator = new Calculator();
+    private final static String ADDITION = "+";
+    private final static String SUBTRACTION = "-";
+    private final static String MULTIPLICATION = "*";
+    private final static String DIVISION = "/";
+
 
     public int start(String input) {
+        Calculator.validateInputNullOrEmpty(input);
         String[] inputs = input.split(" ");
-        int result = calculator.validate(inputs[0]);
+        int result = Calculator.validateInputIsNumber(inputs[0]);
         for (int i = 1; i < inputs.length; i += 2) {
-            int nextInput = calculator.validate(inputs[i + 1]);
+            int nextInput = Calculator.validateInputIsNumber(inputs[i + 1]);
             result = calculator(inputs[i], result, nextInput);
         }
         return result;
     }
 
     private int calculator(String operator, int input1, int input2) {
-        if (operator.equals("+")) {
-            return calculator.add(input1, input2);
+        if (operator.equals(ADDITION)) {
+            return Calculator.add(input1, input2);
         }
-        if (operator.equals("-")) {
-            return calculator.minus(input1, input2);
+        if (operator.equals(SUBTRACTION)) {
+            return Calculator.minus(input1, input2);
         }
-        if (operator.equals("*")) {
-            return calculator.multiply(input1, input2);
+        if (operator.equals(MULTIPLICATION)) {
+            return Calculator.multiply(input1, input2);
         }
-        if (operator.equals("/")) {
-            return calculator.divide(input1, input2);
+        if (operator.equals(DIVISION)) {
+            return Calculator.divide(input1, input2);
         }
         throw new IllegalArgumentException("잘못된 기호입니다.");
     }
