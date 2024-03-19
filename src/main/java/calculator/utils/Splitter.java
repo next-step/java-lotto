7c86fd9caa3c053053cc5cfeb1e52cbbc560beea
jaeyounglee <ljyda214@gmail.com>
2024-03-19 23:58:00 +0900

    FEAT : Splitter 구현

---
+++ src/main/java/calculator/utils/Splitter.java
@@ -0,0 +1,21 @@
+package calculator.utils;
+
+public class Splitter {
+    private static final String EMPTY_SPACE_DELIMITER = " ";
+
+    private Splitter() {
+
+    }
+
+    public static String[] byEmptySpace(String input) {
+        assertNotBlank(input);
+        return input.split(EMPTY_SPACE_DELIMITER);
+    }
+
+    private static void assertNotBlank(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("문자열을 입력해주세요.");
+        }
+    }
+
+}
