bf925bd563a951cca1495ddc9b8405c77d7c314d
이동옥 <moveok@idong-og-ui-MacBookAir.local>
2024-03-19 12:27:53 +0900

    refactor:[1-11] CalculationTest 공통 Test code 정리

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -9,8 +9,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class CalculationTest {
 
     @ParameterizedTest
-    @DisplayName("더하기 계산 Test")
-    @CsvSource(value = {"1:+:2:3", "2:+:4:6"}, delimiter = ':')
+    @DisplayName("계산 Test")
+    @CsvSource(value = {"1:+:2:3", "2:+:4:6", "1:-:2:-1", "2:-:4:-2", "1:*:2:2", "2:*:4:8", "4:/:2:2", "32:/:4:8"}, delimiter = ':')
     void plus(String firstInput, String operator, String secondInput, int resultInput){
         // given
         NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
@@ -19,41 +19,4 @@ public class CalculationTest {
         // then
         assertThat(result).isEqualTo(resultInput);
     }
-
-    @ParameterizedTest
-    @DisplayName("빼기 계산 Test")
-    @CsvSource(value = {"1:-:2:-1", "2:-:4:-2"}, delimiter = ':')
-    void minus(String firstInput, String operator, String secondInput, int resultInput) {
-        // given
-        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
-        // when
-        int result = Calculation.calculate(numberSentence);
-        // then
-        assertThat(result).isEqualTo(resultInput);
-    }
-
-    @ParameterizedTest
-    @DisplayName("곱하기 계산 Test")
-    @CsvSource(value = {"1:*:2:2", "2:*:4:8"}, delimiter = ':')
-    void multiple(String firstInput, String operator, String secondInput, int resultInput) {
-        // given
-        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
-        // when
-        int result = Calculation.calculate(numberSentence);
-        // then
-        assertThat(result).isEqualTo(resultInput);
-    }
-
-    @ParameterizedTest
-    @DisplayName("나누기 계산 Test")
-    @CsvSource(value = {"4:/:2:2", "32:/:4:8"}, delimiter = ':')
-    void divide(String firstInput, String operator, String secondInput, int resultInput) {
-        // given
-        NumberSentence numberSentence = new NumberSentence(new String[]{firstInput, operator, secondInput});
-        // when
-        int result = Calculation.calculate(numberSentence);
-        // then
-        assertThat(result).isEqualTo(resultInput);
-    }
-
 }
