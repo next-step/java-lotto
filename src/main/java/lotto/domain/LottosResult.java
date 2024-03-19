32f4f8eb60469a2e64be27e1d560227b34cfe856
hong yuseok <jericoh@naver.com>
2024-03-19 21:52:15 +0900

    feat: 로또 결과 전달을 위한 LottosResult와 상수 관리를 위한 Rank 구현

---
+++ src/main/java/lotto/domain/LottosResult.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+public class LottosResult {
+    private final int[] winningStaticsArray;
+    private final double rateOfReturn;
+
+    private LottosResult(int[] winningStaticsArray, double rateOfReturn) {
+        this.winningStaticsArray = winningStaticsArray;
+        this.rateOfReturn = rateOfReturn;
+    }
+
+    public static LottosResult newLottoResult(int[] winningStaticsArray, double rateOfReturn) {
+        return new LottosResult(winningStaticsArray, rateOfReturn);
+    }
+
+    public int winningStatic(int matchCount) {
+        return winningStaticsArray[matchCount];
+    }
+
+    public double rateOfReturn() {
+        return rateOfReturn;
+    }
+}
---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    MISS(0, 0);
+
+    private final int matchCount;
+    private final int winningMoney;
+
+    Rank(int matchCount, int winningMoney) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+    }
+
+    private static final Map<Integer, Rank> rankMap = Collections.unmodifiableMap(Stream.of(values())
+            .collect(Collectors.toMap(Rank::getMatchCount, Function.identity())));
+
+    public static Rank findRank(int matchCount) {
+        return Optional.of(rankMap.get(matchCount)).orElse(MISS);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+}
