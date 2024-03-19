b1098119b9ec28a455a9223f7c42e4943b84f29b
ksh <tjdgus2319@gmail.com>
2024-03-19 23:30:27 +0900

    refactor: valid 부분 메소드로 extracted

--- src/main/java/lotto/LottoCalculator.java
+++ src/main/java/lotto/LottoCalculator.java
@@ -3,10 +3,14 @@ package lotto;
 public class LottoCalculator {
 
     public static int getLottoCount(int money) {
+        validMoney(money);
+        return money / Lotto.LOTTO_PRICE;
+    }
+
+    private static void validMoney(int money) {
         if(money < 1000) {
             throw new IllegalArgumentException("로또을 사려면 1000원 이상 있어야 합니다.");
         }
-        return money / Lotto.LOTTO_PRICE;
     }
 
     public static double getRateOfReturn(int initMoney, int getMoney) {
