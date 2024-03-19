5e2dbfb9aae31d88effbfadc5232a1aa982b164e
hellonayeon <hellonykwon@gmail.com>
2024-03-20 04:05:27 +0900

    Refactor: ResultView 출력 메서드 분리

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -11,16 +11,24 @@ import java.util.List;
 public class ResultView {
 
     public static void printLottoTickets(LottoTickets lottoTickets) {
+        String message = formatLottoTicketCount(lottoTickets) + formatLottoTicketNumbers(lottoTickets);
+        System.out.println(message);
+    }
+
+    private static StringBuilder formatLottoTicketCount(LottoTickets lottoTickets) {
         StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(formatPurchaseCount(lottoTickets.size()));
+        stringBuilder.append(System.lineSeparator());
+        return stringBuilder;
+    }
 
-        stringBuilder.append(formatPurchaseCount(lottoTickets.size())).append(System.lineSeparator());
-
+    private static String formatLottoTicketNumbers(LottoTickets lottoTickets) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (LottoTicket ticket : lottoTickets.get()) {
             stringBuilder.append(ticket.get()).append(System.lineSeparator());
         }
         stringBuilder.append(System.lineSeparator());
-
-        System.out.println(stringBuilder);
+        return stringBuilder.toString();
     }
 
     private static String formatPurchaseCount(int count) {
@@ -28,15 +36,18 @@ public class ResultView {
     }
 
     public static void printLottoStatistics(LottoStatistics statisticsMap, int purchaseAmount) {
+        String message = formatLottoPrizes(statisticsMap) + formatProfitRate(statisticsMap, purchaseAmount);
+        System.out.println(message);
+    }
+
+    private static String formatLottoPrizes(LottoStatistics statisticsMap) {
         List<LottoPrize> prizes = LottoPrize.getLottoPrizes();
 
         StringBuilder stringBuilder = new StringBuilder();
         for (LottoPrize prize : prizes) {
             stringBuilder.append(formatStatistic(statisticsMap, prize)).append(System.lineSeparator());
         }
-        stringBuilder.append(formatProfitRate(statisticsMap, purchaseAmount));
-
-        System.out.println(stringBuilder);
+        return stringBuilder.toString();
     }
 
     private static String formatStatistic(LottoStatistics statisticsMap, LottoPrize prize) {
