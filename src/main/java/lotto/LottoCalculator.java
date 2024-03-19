922c4f3b38729b4688b441cbbafeff9613e3ae31
ksh <tjdgus2319@gmail.com>
2024-03-19 23:27:02 +0900

    feat: 로또에 사용되는 계산 구현

--- src/main/java/lotto/LottoCalculator.java
+++ src/main/java/lotto/LottoCalculator.java
@@ -1,2 +1,17 @@
-package lotto;public class LottoCalculator {
+package lotto;
+
+public class LottoCalculator {
+
+    public static int getLottoCount(int money) {
+        if(money < 1000) {
+            throw new IllegalArgumentException("로또을 사려면 1000원 이상 있어야 합니다.");
+        }
+        return money / Lotto.LOTTO_PRICE;
+    }
+
+    public static double getRateOfReturn(int initMoney, int getMoney) {
+        double rate = (double) getMoney / initMoney;
+        return (Math.floor(rate * 100) / 100.0);
+    }
+
 }
