0592701ae0e83974ed03c24888037cba14c6233d
박소민 <qkrthals0524@naver.com>
2024-03-19 11:50:57 +0900

    Calculator 클래스 내 validate 메서드 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,26 +2,39 @@ package calculator;
 
 public class Calculator {
 
-    public int add(int input1, int input2) {
+    private static final String NUMBER_REGEX = "\\d+";
+
+    public static int add(int input1, int input2) {
         return input1 + input2;
     }
 
-    public int minus(int input1, int input2) {
+    public static int minus(int input1, int input2) {
         return input1 - input2;
     }
 
-    public int multiply(int input1, int input2) {
+    public static int multiply(int input1, int input2) {
         return input1 * input2;
     }
 
-    public int divide(int input1, int input2) {
+    public static int divide(int input1, int input2) {
         return input1 / input2;
     }
 
-    public int validate(String input) {
+    public static void validateInputNullOrEmpty(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException("null 또는 빈 문자열은 입력할 수 없습니다.");
         }
+    }
+
+    public static int validateInputIsNumber(String input) {
+        if (!input.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+        return convertStringToInt(input);
+
+    }
+
+    public static int convertStringToInt(String input) {
         return Integer.parseInt(input);
     }
 }
