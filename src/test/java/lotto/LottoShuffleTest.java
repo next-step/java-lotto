dcefb0b2185b41ee369dd62fc2d6d2de898b3609
sunghyun <tjdgus2319@gmail.com>
2024-03-19 21:07:17 +0900

    test: 로또 셔플 클래스 테스트

--- src/test/java/lotto/LottoShuffleTest.java
+++ src/test/java/lotto/LottoShuffleTest.java
@@ -1,2 +1,18 @@
-package lotto;public class LottoShuffle {
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoShuffleTest {
+    @Test
+    @DisplayName("로또 번호 생성 테스트")
+    void makeLottoNumberTest() {
+        LottoNumbers lottoNumbers = LottoShuffle.makeLottoNumbersInRange();
+        lottoNumbers.getLottoNumbers()
+                .forEach(number -> assertThat(number)
+                        .isGreaterThanOrEqualTo(1)
+                        .isLessThanOrEqualTo(45));
+    }
+
 }
