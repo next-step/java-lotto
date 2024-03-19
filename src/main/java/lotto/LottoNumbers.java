fac1f9fbfa7ff10495535a44f43e2007c3f3b6a5
ksh <tjdgus2319@gmail.com>
2024-03-19 23:30:00 +0900

    feat: 로또 숫자 일급컬렉션으로 기능 구현

--- src/main/java/lotto/LottoNumbers.java
+++ src/main/java/lotto/LottoNumbers.java
@@ -1,2 +1,22 @@
-package lotto;public class LottoNumbers {
+package lotto;
+
+import java.util.*;
+
+public class LottoNumbers {
+    private List<Integer> lottoNumbers;
+
+    public LottoNumbers(List<Integer> lottoNumbers) {
+        this.lottoNumbers = getSortedNumbers(lottoNumbers);
+    }
+
+    private List<Integer> getSortedNumbers(List<Integer> lottoNumbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(lottoNumbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
 }
