ff3ff401408ee51aab9c40667a18dad90de24387
JJewon <eldertree777@gmail.com>
2024-03-20 01:52:43 +0900

    [refactor] enum, 단일 추상 메서드 사용하여 계산을 간결하게 표현

---
+++ src/main/java/stringCalculator/Operation.java
@@ -0,0 +1,38 @@
+package stringCalculator;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+interface Operator {
+    int apply(int x, int y);
+}
+
+public enum Operation {
+    PLUS("+", (x, y) -> x + y),
+    MINUX("-", (x, y) -> x - y),
+    MULTIPLE("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
+
+    private String sign;
+    private final Operator operator;
+
+    public String sign() {
+        return sign;
+    }
+
+    Operation(String sign, Operator operator) {
+        this.sign = sign;
+        this.operator = operator;
+    }
+
+    public static Operation findOperator(String sign) {
+        return Arrays.stream(Operation.values())
+                .filter(operation -> Objects.equals(operation.sign(), sign))
+                .findAny()
+                .get();
+    }
+
+    public int apply(int x, int y) {
+        return operator.apply(x, y);
+    }
+}
