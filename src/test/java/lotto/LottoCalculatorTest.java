d0ba51ce89125fab99b8f7e2f1913d12e08e9499
sunghyun <tjdgus2319@gmail.com>
2024-03-21 00:06:08 +0900

    test: 0인 경우 수익률 테스트 추가

--- src/test/java/lotto/LottoCalculatorTest.java
+++ src/test/java/lotto/LottoCalculatorTest.java
@@ -11,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class LottoCalculatorTest {
 	private static final int INIT_MONEY = 14000;
 	private static final int GET_MONEY = 5000;
+	private static final int GET_MONEY_ZERO = 0;
+	private static final double RATE_OF_RETURN_ZERO = 0.0;
 	private static final double RATE_OF_RETURN = 0.35;
 
 	@Test
@@ -26,6 +28,13 @@ public class LottoCalculatorTest {
 	void rateOfReturnTest() {
 		double returnMoney = LottoCalculator.getRateOfReturn(INIT_MONEY, GET_MONEY);
 		assertThat(returnMoney).isEqualTo(RATE_OF_RETURN);
+	}
+
+	@Test
+	@DisplayName("로또 수익률 계산 테스트")
+	void rateOfReturnZeroTest() {
+		double returnMoney = LottoCalculator.getRateOfReturn(INIT_MONEY, GET_MONEY_ZERO);
+		assertThat(returnMoney).isEqualTo(RATE_OF_RETURN_ZERO);
 	}
 
 }
