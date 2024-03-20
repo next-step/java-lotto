d403b8b4c2e9cec89e455a0d7d8b20476475eb6f
유지웅 <poujis0@gmail.com>
2024-03-21 03:41:27 +0900

    [feat] 로또 진행을 위한 입출력 개발

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -39,4 +39,9 @@ public class LottoNumber {
     public int hashCode() {
         return Objects.hash(number);
     }
+
+    @Override
+    public String toString() {
+        return Integer.toString(number);
+    }
 }
