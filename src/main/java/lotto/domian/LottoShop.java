8d980cafbf217277f089991b5b43dff4bffc14f1
Shinny <siniya979@gmail.com>
2024-03-21 00:24:03 +0900

    feat : 로또 가게 클래스 구현

---
+++ src/main/java/lotto/domian/LottoShop.java
@@ -0,0 +1,30 @@
+package lotto.domian;
+
+import static lotto.common.ConstantSet.LOTTO_TICKET_PRICE;
+import static lotto.common.MessageSet.NOT_ENOUGH_MONEY;
+
+public class LottoShop {
+
+    LottoNumberGenerator generator ;
+
+    private LottoShop(LottoNumberGenerator generator) {
+        this.generator = generator;
+    }
+
+    public static LottoShop of(LottoNumberGenerator generator){
+        return new LottoShop(generator);
+    }
+
+    public LottoTickets generateLottoTickets(int money){
+        int ticketCount = getPurchasableTicketCount(money);
+        return LottoTickets.of(ticketCount,generator);
+    }
+
+    private int getPurchasableTicketCount(int money) {
+        if(money < 1000) {
+            throw new IllegalArgumentException(NOT_ENOUGH_MONEY.getErrorMessage());
+        }
+        return money/LOTTO_TICKET_PRICE;
+    }
+
+}
