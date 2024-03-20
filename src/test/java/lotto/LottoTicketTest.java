bd06ee67810d9f57e532adc4de2d2ecde6aa9744
danalee <eleeje97@gmail.com>
2024-03-21 00:44:45 +0900

    refactor: LottoTicketTest 테스트케이스 추가

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -4,11 +4,14 @@ import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
 import lotto.domain.WinningNumbers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Set;
 import java.util.TreeSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 class LottoTicketTest {
     private static final int LOTTO_NUMBER_COUNT = 6;
@@ -24,8 +27,12 @@ class LottoTicketTest {
     }
 
 
-    @Test
-    void 당첨숫자와_일치하는_숫자개수() {
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 3, 4, 5, 6:3",
+                        "1, 3, 5, 7, 8, 10:4",
+                        "1, 3, 5, 7, 9, 10:5",
+                        "1, 3, 5, 7, 9, 11:6"}, delimiter = ':')
+    void 당첨숫자와_일치하는_숫자개수(String input, int expected) {
         LottoTicket lottoTicket = new LottoTicket(new TreeSet<>(Set.of(
                 new LottoNumber(1),
                 new LottoNumber(3),
@@ -33,8 +40,8 @@ class LottoTicketTest {
                 new LottoNumber(7),
                 new LottoNumber(9),
                 new LottoNumber(11))));
-        WinningNumbers winningNumbers = new WinningNumbers("1, 2, 3, 4, 5, 6", ", ");
-        assertThat(lottoTicket.getMatchCount(winningNumbers)).isEqualTo(3);
+        WinningNumbers winningNumbers = new WinningNumbers(input, ", ");
+        assertThat(lottoTicket.getMatchCount(winningNumbers)).isEqualTo(expected);
     }
 
 }
