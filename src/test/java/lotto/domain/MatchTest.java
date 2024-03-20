bfea3dd20559704a110cea6f30763e6d8a102bd5
DongWooE <ehddn977@naver.com>
2024-03-21 01:43:24 +0900

    chore : 테스트 코드들 위치 변경

--- src/test/java/lotto/domain/MatchTest.java
+++ src/test/java/lotto/domain/MatchTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import lotto.domain.Match;
 import org.junit.jupiter.params.ParameterizedTest;
--- src/test/java/lotto/domain/ProfitTest.java
+++ src/test/java/lotto/domain/ProfitTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import lotto.domain.Cash;
 import lotto.domain.Profit;
