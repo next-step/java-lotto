a04197f135148c527033b5fbf2614639b791a934
danalee <eleeje97@gmail.com>
2024-03-21 00:50:56 +0900

    test: LottoNumberTest 추가

---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import lotto.domain.LottoNumber;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoNumberTest {
+    @Test
+    void 로또숫자_생성() {
+        LottoNumber lottoNumber = new LottoNumber(3);
+        assertThat(lottoNumber).isEqualTo(new LottoNumber(3));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, -2, 100})
+    void 범위를_벗어난_로또숫자_생성(int number) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoNumber(number));
+    }
+
+    @Test
+    void 로또숫자_비교() {
+        LottoNumber lottoNumber = new LottoNumber(3);
+        assertThat(lottoNumber.compareTo(new LottoNumber(6))).isNegative();
+    }
+}
