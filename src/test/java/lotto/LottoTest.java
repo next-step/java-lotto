5a10a25e79576b841a42ccf428d0b8d10105e663
kimsunghyun <tjdgus2319@gmail.com>
2024-03-20 17:24:50 +0900

    test: 당첨번호와 로또번호 일치하는지 확인하는 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -11,10 +11,20 @@ public class LottoTest {
 
     @Test
     @DisplayName("로또 객체 생성시 정렬 테스트")
-    void makeLotto() {
+    void makeLottoTest() {
         List<Integer> lottoNumbers = List.of(20,32,44,30,1);
         Lotto lotto = new Lotto(new LottoNumbers(lottoNumbers));
         assertThat(lotto.getLottoNumber().getLottoNumbers().get(0)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("당첨번호와 갖고있는 로또번호가 일치 테스트")
+    void lottoNumbermatchTest() {
+        List<Integer> lottoNumbers = List.of(20,32,44,30,1);
+        List<Integer> winningNumber = List.of(20,3,44,4,1);
+        Lotto lotto = new Lotto(new LottoNumbers(lottoNumbers));
+        int count = lotto.getLottoNumber().getCountOfMatchLottoNumber(winningNumber);
+        assertThat(count).isEqualTo(3);
     }
 
 }
