6ccb230dd5fc7b685915e147c069b777a828d8ef
Sangmin <sm9171@nate.com>
2024-03-20 09:25:00 +0900

    refactor : Stream.generate 사용

--- src/main/java/lotto/domain/MyLottos.java
+++ src/main/java/lotto/domain/MyLottos.java
@@ -1,50 +1,46 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.counting;
 
 public class MyLottos {
-    private final static int LOTTO_PRICE = 1000;
+	private final static int LOTTO_PRICE = 1000;
 
-    private final List<Lotto> lottos;
+	private final List<Lotto> lottos;
 
-    public MyLottos(List<Lotto> lottos) {
-        this.lottos = lottos;
-    }
+	public MyLottos(List<Lotto> lottos) {
+		this.lottos = lottos;
+	}
 
-    public static MyLottos rollLottos(int amount) {
-        List<Lotto> array = new ArrayList<>();
+	public static MyLottos rollLottos(int amount) {
+		List<Lotto> lottoList = Stream.generate(() -> new Lotto())
+				.limit(getCount(amount))
+				.collect(Collectors.toList());
 
-        int count = getCount(amount);
+		return new MyLottos(lottoList);
+	}
 
-        for (int i = 0; i < count; i++) {
-            array.add(new Lotto());
-        }
+	private static int getCount(final int amount) {
+		int count = amount / LOTTO_PRICE;
+		return count;
+	}
 
-        return new MyLottos(array);
-    }
+	public List<Lotto> getLottos() {
+		return List.copyOf(lottos);
+	}
 
-    private static int getCount(final int amount) {
-        int count = amount / LOTTO_PRICE;
-        return count;
-    }
+	public Integer getLottoCount() {
+		return lottos.size();
+	}
 
-    public List<Lotto> getLottos() {
-        return List.copyOf(lottos);
-    }
-
-    public Integer getLottoCount() {
-        return lottos.size();
-    }
-
-    public Map<AmountEnum, Long> findWinner(final Lotto winLotto) {
-        return lottos.stream()
-                .collect(Collectors.groupingBy(
-                        lotto -> lotto.getAmountEnumCompareWinNumber(winLotto)
-                        , counting()));
-    }
+	public Map<AmountEnum, Long> findWinner(final Lotto winLotto) {
+		return lottos.stream()
+				.collect(Collectors.groupingBy(
+						lotto -> lotto.getAmountEnumCompareWinNumber(winLotto)
+						, counting()));
+	}
 }
