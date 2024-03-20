27b59c8cf073eea9b22c33507fff1e239f59b6f4
astrid-dm <himax_01@naver.com>
2024-03-21 02:17:40 +0900

    refactor : 코드리뷰 반영

--- src/test/java/stringCalculator/InputValidatorTest.java
+++ src/test/java/stringCalculator/InputValidatorTest.java
@@ -1,11 +1,9 @@
 package stringCalculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.platform.commons.util.StringUtils;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -13,9 +11,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 class InputValidatorTest {
     private final InputValidator validator = new InputValidator();
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력값에 대한 유효성 검증 실패 : null값이 들어옴")
     @NullSource
-    @DisplayName("입력값에 대한 유효성 검증 실패 : null값이 들어옴")
     void validate__ShouldThrowException__WithNull(String input) {
         // when, then
         assertThatIllegalArgumentException().isThrownBy(() -> {
@@ -23,9 +20,8 @@ class InputValidatorTest {
         });
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력값에 대한 유효성 검증 실패 : 빈값, 공백값이 들어옴")
     @ValueSource(strings={"", " "})
-    @DisplayName("입력값에 대한 유효성 검증 실패 : 빈값, 공백값이 들어옴")
     void validate__ShouldThrowException__WithBlank(String input) {
         // when, then
         assertThatThrownBy(() ->  validator.validate(input))
@@ -33,9 +29,8 @@ class InputValidatorTest {
                 .hasMessageContaining("Blank input is not allowed");
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력값에 대한 유효선 검증 실패 : 숫자가 아닌 알파벳, 특수기호가 들어옴")
     @ValueSource(strings={"a + b - c", "10 - 9 ! 3"})
-    @DisplayName("입력값에 대한 유효선 검증 실패 : 숫자가 아닌 알파벳, 특수기호가 들어옴")
     void validate__ShouldThrowException__WithAlphabet(String input) {
         // when, then
         assertThatIllegalArgumentException().isThrownBy(() -> {
@@ -43,9 +38,8 @@ class InputValidatorTest {
         });
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력값에 대한 유효선 검증 실패 : 숫자가 아닌 알파벳, 순서 오류")
     @ValueSource(strings={"10 +", "- 10 +", "* 10"})
-    @DisplayName("입력값에 대한 유효선 검증 실패 : 숫자가 아닌 알파벳, 순서 오류")
     void validate__ShouldThrowException__WrongSequence(String input) {
         // when, then
         assertThatThrownBy(() ->  validator.validate(input))
