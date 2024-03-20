df5834846d8c4d11df9a128588593f30f65ed266
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-21 00:49:25 +0900

    test: 당첨 통계 계산 테스트 코드 수정

--- src/test/java/lotto/LottoStatisticComputerTest.java
+++ src/test/java/lotto/LottoStatisticComputerTest.java
@@ -3,7 +3,8 @@ package lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,39 +12,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoStatisticComputerTest {
 
     private LottoStatisticComputer lottoStatisticComputer;
+
     @BeforeEach
     void initComputer() {
-        lottoStatisticComputer = new LottoStatisticComputer();
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 4, 5, 6, 7));
+        Lotto lotto3 = new Lotto(List.of(1, 2, 5, 6, 7, 8));
+
+        lottoStatisticComputer = new LottoStatisticComputer(List.of(lotto1, lotto2, lotto3));
     }
 
 
-    @DisplayName("3개 당첨 통계 산출")
+    @DisplayName("당첨 통계 산출")
     @Test
-    @MethodSource()
     void computeStatistic_three() {
-        int result = lottoStatisticComputer.computeStatistic();
+        Lotto winLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Statistics result = lottoStatisticComputer.computeStatistic(winLotto);
 
-        assertThat(result).isEqualTo(3);
-    }
-
-    @DisplayName("4개 당첨 통계 산출")
-    void computeStatistic_four() {
-        int result = lottoStatisticComputer.computeStatistic();
-
-        assertThat(result).isEqualTo(4);
-    }
-
-    @DisplayName("5개 당첨 통계 산출")
-    void computeStatistic_five() {
-        int result = lottoStatisticComputer.computeStatistic();
-
-        assertThat(result).isEqualTo(5);
-    }
-
-    @DisplayName("6개 당첨 통계 산출")
-    void computeStatistic_six() {
-        int result = lottoStatisticComputer.computeStatistic();
-
-        assertThat(result).isEqualTo(6);
+        assertThat(result.statistics).isEqualTo(new Statistics(List.of(Statistic.SIX, Statistic.FIVE, Statistic.FOUR)).statistics);
     }
 }
