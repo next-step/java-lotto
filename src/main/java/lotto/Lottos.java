3b2372e5d902d77b7ced7cc7fdd58efca1c4a033
유지웅 <poujis0@gmail.com>
2024-03-21 00:06:44 +0900

    [feat] 모든 로또들과 당첨번호를 비교하여 결과를 담은 HashMap 리턴 구현

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lottos {
 
@@ -14,4 +16,16 @@ public class Lottos {
         return lottos.size();
     }
 
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public Map<LottoRank, Integer> getWinningResult(WinningLotto winningLotto) {
+        Map<LottoRank, Integer> result = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            LottoRank rank = LottoRank.isLottoRank(lotto.countMatch(winningLotto));
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+        return result;
+    }
 }
