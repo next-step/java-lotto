b175ebb6a960ab4731f446b5d8fb13b171f29060
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-21 03:26:14 +0900

    feat: Budget 래핑 클래스 구현

---
+++ src/main/java/lotto/domain/Budget.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public class Budget {
+
+    private int value;
+
+    public Budget(int budget) {
+        this.value = budget;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int canBuyQuantity() {
+        return value / 1000;
+    }
+}
