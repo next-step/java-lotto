eb8ffbd034c829f21b304c7034545c917c7d94e1
kimsunghyun <tjdgus2319@gmail.com>
2024-03-20 17:59:48 +0900

    feat: 당첨 통계 구하는 기능 구현

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,2 +1,24 @@
-package lotto;public class Lottos {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Lottos {
+
+	private List<Lotto> lottos;
+
+	public Lottos(List<Lotto> lottos) {
+		this.lottos = lottos;
+	}
+
+	public List<LottoRank> getWinningStatistics(List<Integer> winningNumber) {
+		List<LottoRank> result = new ArrayList<>();
+		for (Lotto lotto : this.lottos) {
+			int count = lotto.getLottoNumber().getCountOfMatchLottoNumber(winningNumber);
+			result.add(LottoRank.findLottoRankByLottoCount(count));
+		}
+		return result;
+	}
+
 }
