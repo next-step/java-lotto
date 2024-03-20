85948e423bb94f41d3d1c2f89a6bc81219ed3047
DongWooE <ehddn977@naver.com>
2024-03-21 01:44:31 +0900

    style : 코드 포멧팅 적용

--- src/test/java/lotto/domain/MatchCacheTest.java
+++ src/test/java/lotto/domain/MatchCacheTest.java
@@ -54,7 +54,7 @@ public class MatchCacheTest {
     }
 
     @Test
-    void to_string(){
+    void to_string() {
         matchCache = new MatchCache(new int[]{1, 2, 0, 1, 3, 4});
         System.out.println("3개 일치 (5000원)- 0개\n4개 일치 (50000원)- 1개\n5개 일치 (1500000원)- 3개\n6개 일치 (2000000000원) - 4개\n");
     }
