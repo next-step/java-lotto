688547bba8a96059dffe82d13e4990a0b4594a1e
Shinny <siniya979@gmail.com>
2024-03-21 00:22:06 +0900

    feat : 상수 보관 클래스

---
+++ src/main/java/lotto/common/ConstantSet.java
@@ -0,0 +1,7 @@
+package lotto.common;
+
+public class ConstantSet {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_TICKET_PRICE = 1000;
+}
