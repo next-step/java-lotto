b10f0f93fef48400a3a75ac0c80c5390fd1b662f
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-21 00:50:20 +0900

    feat: 통계 계산 클래스 구현

--- src/main/java/lotto/LottoStatisticComputer.java
+++ src/main/java/lotto/LottoStatisticComputer.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoStatisticComputer {
-    public int computeStatistic() {
-        return 3;
+
+    private List<Lotto> lottos;
+
+    public LottoStatisticComputer(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public Statistics computeStatistic(Lotto winLotto) {
+        return new Statistics(checkStatisticNumberMatch(winLotto));
+    }
+
+    private List<Statistic> checkStatisticNumberMatch(Lotto winLotto) {
+        List<Statistic> statistics = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            statistics.add(getStatistic(winLotto, lotto));
+        }
+        return statistics;
+    }
+
+    private static Statistic getStatistic(Lotto winLotto, Lotto lotto) {
+        return Statistic.valueOfMatchNumber(
+                        lotto.getFeatNumberCount(winLotto)
+                );
     }
 }
