d44aa264c260a6a1291c60b13fccaa7cd0d3ab3e
kimsunghyun <tjdgus2319@gmail.com>
2024-03-20 17:59:01 +0900

    faet: 로또 당첨 통계를 위한 enum 타입 선언

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum LottoRank {
+	FIRST(6, 2000000000),
+	SECOND(5,1500000),
+	THIRD(4,50000),
+	FOURTH(3,5000),
+	FIFTH(2,0),
+	SIXTH(1,0),
+	SEVENTH(0,0);
+
+
+	private int lottoCount;
+	private int money;
+
+	LottoRank(int lottoCount, int money) {
+		this.lottoCount = lottoCount;
+		this.money = money;
+	}
+
+	public static LottoRank findLottoRankByLottoCount(int lottoCount) {
+		return Arrays.stream(LottoRank.values())
+				.filter(v -> v.lottoCount == lottoCount)
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("존재 할 수 없는 값입니다."));
+	}
+
+	public int getLottoCount() {
+		return lottoCount;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+}
