7466dcce6ad94e99204c125a5b0c3ce0ff983ba2
danalee <eleeje97@gmail.com>
2024-03-21 00:29:26 +0900

    refactor: Arrays.stream() -> Stream.of() 변경

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WinningNumbers {
     private final static String SPLIT_DELIMITER = ", ";
@@ -23,7 +24,7 @@ public class WinningNumbers {
     }
 
     private List<LottoNumber> toWinningNumbers(String input) {
-        List<LottoNumber> winningNumbers = Arrays.stream(toStringArray(input))
+        List<LottoNumber> winningNumbers = Stream.of(toStringArray(input))
                 .map(this::toLottoNumber)
                 .collect(Collectors.toList());
 
