4108332ee0404ccab8d4653a5828d248d3cd9169
danalee <eleeje97@gmail.com>
2024-03-20 23:11:07 +0900

    refactor: LottoClient 클래스명 변경

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -6,7 +6,7 @@ import lotto.domain.WinningNumbers;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
-public class LottoClient {
+public class LottoController {
     public static void main(String[] args) {
         Amount amount = new Amount(InputView.askPurchaseAmount());
         LottoTickets lottoTickets = new LottoTickets(amount);
