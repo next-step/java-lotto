5c103620031c330ed9c128af7e95b68b6c864e72
astrid-dm <himax_01@naver.com>
2024-03-21 02:27:33 +0900

    refactor : LottoGenerator 리펙터링 (로또의 생성 과정을 담당)

--- src/main/java/AutoLotto/model/LottoGenerator.java
+++ src/main/java/AutoLotto/model/LottoGenerator.java
@@ -1,21 +1,23 @@
-package AutoLotto.model;
+package autoLotto.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    private static List<Integer> randomNumbers = new ArrayList<>();
+    private final List<Integer> randomNumbers;
 
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
 
     public LottoGenerator() {
-        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+        randomNumbers = new ArrayList<>();
+
+        for (int i = MIN_NUMBER, j = 0; i <= MAX_NUMBER; i++, j++) {
             randomNumbers.add(i);
         }
     }
 
-    public List<Integer> generateLottoNumbers(LottoGeneratorStrategy lottoGeneratorStrategy) {
+    public int[] generateLottoNumbers(LottoGeneratorStrategy lottoGeneratorStrategy) {
         return lottoGeneratorStrategy.lottoGenerator(randomNumbers);
     }
 }
