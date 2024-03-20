b1e71785765c5acee46f8a680074660d466cd2d4
astrid-dm <himax_01@naver.com>
2024-03-21 02:23:27 +0900

    feat : 로또의 당첨 번호를 입력받는 로직 추가

--- src/main/java/AutoLotto/view/InputView.java
+++ src/main/java/AutoLotto/view/InputView.java
@@ -1,22 +1,28 @@
-package AutoLotto.view;
+package autoLotto.view;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private Scanner scanner = new Scanner(System.in);
 
     private static final String VALID_NUMBER = "[0-9]\\d*";
+    private static final String VALID_WIN_NUMBER = "^[0-9]{1,2}(,[0-9]{1,2})*$";
+    private static final Long DIVISION_UNIT = 1000L;
+    private static final int WIN_NUMBER_LENGTH = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
 
     private static final String PURCHASE_AMOUNT_QUESTION = "구입금액을 입력해 주세요.";
     private static final String PURCHASE_DENIED = "1,000원 단위로 구매 가능합니다.\n구입금액을 다시 입력해주세요.";
     private static final String WIN_NUMBERS_QUESTION = "당첨 번호 6개를 입력해주세요. \n (ex : 1,2,3,4,5,6)";
     private static final String WIN_NUMBERS_DENIED = "쉼표를 기준으로 6개의 숫자를 입력하셔야 합니다.\n구입금액을 다시 입력해주세요.";
 
-    public int inputPurchase() {
-        return getChances();
+    public String inputPurchase() {
+        return startPurchase();
     }
 
-    private int getChances() {
+    private String startPurchase() {
         outputQuestion(PURCHASE_AMOUNT_QUESTION);
         String input = scanner.nextLine();
 
@@ -25,7 +31,7 @@ public class InputView {
             input = scanner.nextLine();
         }
 
-        return convertStringToNumber(input);
+        return input;
     }
 
     private void outputQuestion(String question) {
@@ -37,12 +43,58 @@ public class InputView {
     }
 
     private boolean isValidPaymentUnit(String input) {
-        return input.endsWith("000") && input.length() >= 4;
+        Long inputLong = Long.parseLong(input);
+        return inputLong / DIVISION_UNIT >= 1;
     }
 
-    private int convertStringToNumber(String input) {
-        String strippedInput = input.substring(0, input.length() - 3);
-
-        return Integer.parseInt(strippedInput);
+    public String inputWinNumbers() {
+        return confirmWinNumbers();
     }
+
+    private String confirmWinNumbers() {
+        outputQuestion(WIN_NUMBERS_QUESTION);
+        String input = scanner.nextLine();
+
+        while(!isValidInputWinNumbers(input) || !isValidWinNumbers(input)) {
+            outputQuestion(WIN_NUMBERS_DENIED);
+            input = scanner.nextLine();
+        }
+
+        return input;
+    }
+
+    private boolean isValidInputWinNumbers(String input) {
+        return input.matches(VALID_WIN_NUMBER);
+    }
+
+    private boolean isValidWinNumbers(String input) {
+        String[] values = splitNumbersByComma(input);
+
+        if (values.length != WIN_NUMBER_LENGTH) {
+            return false;
+        }
+
+        int[] numbers = stringsToInts(values);
+        return isValidWinNumber(numbers);
+    }
+
+    private String[] splitNumbersByComma(String input) {
+        return input.split(",");
+    }
+
+    private int[] stringsToInts(String[] values) {
+        int[] numbers = new int[values.length];
+
+        for (int i = 0; i < WIN_NUMBER_LENGTH; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+
+        return numbers;
+    }
+
+    private boolean isValidWinNumber(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[0] >= MIN_NUMBER && numbers[WIN_NUMBER_LENGTH - 1] <= MAX_NUMBER;
+    }
+
 }
