93beacb435f715e5c14dc0c1314ef55826cbaf18
이승호 <seung7642@gmail.com>
2024-03-20 23:05:17 +0900

    refactor: 입력 값을 검증 및 변환하는 책임을 InputView 에 할당하기.

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final String LOTTO_SPLIT_SEPARATOR = ",";
+    private static final String LOTTO_FORMAT_REGEX = String.format("^\\d+(%s\\s*\\d+){5}$", LOTTO_SPLIT_SEPARATOR);
 
     private InputView() {
     }
@@ -15,9 +20,24 @@ public class InputView {
         }
     }
 
-    public static String promptForWinningNumbers() {
+    public static List<Integer> promptForWinningNumbers() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        return convertWinningNumbers(scanner.nextLine());
+    }
+
+    private static List<Integer> convertWinningNumbers(String input) {
+        assertionValidFormat(input);
+        List<Integer> result = new ArrayList<>();
+        for (String s : input.trim().split(String.format("\\s*%s\\s*", LOTTO_SPLIT_SEPARATOR))) {
+            result.add(Integer.parseInt(s));
+        }
+        return result;
+    }
+
+    private static void assertionValidFormat(String input) {
+        if (!input.matches(LOTTO_FORMAT_REGEX)) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
     }
 }
--- src/main/java/LottoApplication.java
+++ src/main/java/LottoApplication.java
@@ -3,30 +3,13 @@ import java.util.List;
 
 public class LottoApplication {
 
-    private static final String LOTTO_FORMAT_REGEX = "^\\d+, \\d+, \\d+, \\d+, \\d+, \\d+$";
-
     public static void main(String[] args) {
         int price = InputView.promptForPurchasePrice();
         Lottos lottos = new Lottos(price);
         ResultView.print(lottos);
 
-        Lotto winningLotto = new Lotto(convertWinningNumbers(InputView.promptForWinningNumbers()));
+        Lotto winningLotto = new Lotto(InputView.promptForWinningNumbers());
         ResultView.printRanks(lottos.ranks(winningLotto));
         ResultView.printRateOfReturn(lottos.rateOfReturn(winningLotto));
-    }
-
-    private static List<Integer> convertWinningNumbers(String input) {
-        assertionValidFormat(input);
-        List<Integer> result = new ArrayList<>();
-        for (String s : input.split(", ")) {
-            result.add(Integer.parseInt(s));
-        }
-        return result;
-    }
-
-    private static void assertionValidFormat(String input) {
-        if (!input.matches(LOTTO_FORMAT_REGEX)) {
-            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
-        }
     }
 }
