ea357dc37fd3fd30601bf3cb93b40efe98253ab6
DongWooE <ehddn977@naver.com>
2024-03-21 00:17:02 +0900

    feat : Cash에 toString 재정의

--- src/main/java/lotto/domain/Cash.java
+++ src/main/java/lotto/domain/Cash.java
@@ -2,8 +2,24 @@ package lotto.domain;
 
 public class Cash {
 
+    private int amount;
+
+    public Cash(){}
+    public Cash(int amount) {
+        this.amount = amount;
+    }
+
     public int payLotto(int money){
         if(money < 1000) throw new IllegalArgumentException();
-        return money/1000;
+        return this.amount = money/1000;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d개를 구매했습니다.", amount);
     }
 }
