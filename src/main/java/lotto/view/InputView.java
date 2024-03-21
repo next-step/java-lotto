30e556eb14260356e85c072a98424196c82101d4
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-21 19:25:25 +0900

    FEAT : create InputView to input money and win numbers

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,18 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    Scanner scanner = new Scanner(System.in);
+    public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public String inputWinNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
 }
