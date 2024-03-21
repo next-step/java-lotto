bbaecebfeac598bee7668b30fb5a499709d62d90
jaeyounglee <ljyda214@gmail.com>
2024-03-21 23:25:04 +0900

    FEAT : 생성자 리펙토링 및 수익률 계산 로직 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -8,12 +8,28 @@ public class LottoGame {
 
     List<Lotto> lottos;
 
-    public LottoGame(int lottoPrice, int purchaseBudget) {
-        int numberOfPurchase = purchaseBudget / lottoPrice;
-        this.lottos = Stream.generate(Lotto::new).limit(numberOfPurchase).collect(Collectors.toList());
+    public LottoGame(long lottoPrice, long purchaseBudget) {
+        this(Stream.generate(Lotto::new)
+                .limit(purchaseBudget / lottoPrice)
+                .collect(Collectors.toList())
+        );
+    }
+
+    public LottoGame(List<Lotto> lottos) {
+        this.lottos = lottos;
     }
 
     public int hasLottoCount() {
         return lottos.size();
+    }
+
+    public double produceStatistics(long lottoPrice, Lotto lastWeekWin) {
+        long totalPrize = 0L;
+        for (Lotto l : lottos) {
+            int i = l.countMatchedNumber(lastWeekWin);
+            totalPrize += RANK.receivePrize(i);
+        }
+
+        return Math.floor((double) totalPrize / lottoPrice * 100) / 100.0;
     }
 }
--- src/main/java/lotto/RANK.java
+++ src/main/java/lotto/RANK.java
@@ -1,27 +1,37 @@
 package lotto;
 
 
+import java.util.Arrays;
+
 /**
- *  3개 일치 (5000원)- 1개
- *  4개 일치 (50000원)- 0개
- *  5개 일치 (1500000원)- 0개
- *  6개 일치 (2000000000원)- 0개
- *  수익률 = 총 당첨 금액 / 구입 금액
- *  */
+ * 3개 일치 (5000원)- 1개
+ * 4개 일치 (50000원)- 0개
+ * 5개 일치 (1500000원)- 0개
+ * 6개 일치 (2000000000원)- 0개
+ * 수익률 = 총 당첨 금액 / 구입 금액
+ */
 public enum RANK {
 
-    FIRST(2000000000),
-    SECOND(1500000),
-    THIRD(50000),
-    FOURTH(5000);
+    FIRST(6, 2000000000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
+    ;
 
+    private final int matchedCount;
     private final long prizeMoney;
 
-    RANK(long prizeMoney) {
+    RANK(int matchedCount, long prizeMoney) {
+        this.matchedCount = matchedCount;
         this.prizeMoney = prizeMoney;
     }
 
-    public long winPrize(Integer count) {
-        return this.prizeMoney * count;
+
+    public static long receivePrize(int number){
+        return Arrays.stream(RANK.values())
+                .filter(e -> e.matchedCount == number)
+                .findFirst()
+                .map(s -> s.prizeMoney)
+                .orElseGet(() -> 0L);
     }
 }
