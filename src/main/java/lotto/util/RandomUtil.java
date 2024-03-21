efb3de63b8b444b4fcd4a70563435ada8a980446
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-21 19:27:56 +0900

    FEAT : create RandomUtil to extract random value

--- src/main/java/lotto/util/RandomUtil.java
+++ src/main/java/lotto/util/RandomUtil.java
@@ -1,2 +1,13 @@
-package lotto.util;public class RandomUtil {
+package lotto.util;
+
+import java.util.Random;
+
+public class RandomUtil {
+
+    private static final int BOUND = 45;
+    private static final Random random = new Random();
+
+    public static int getRandom() {
+        return random.nextInt(BOUND) + 1;
+    }
 }
