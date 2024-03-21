f4f55cf71347450494e4384d504067d9f8159b06
kwanik-kor <rhksdlr134@naver.com>
2024-03-21 20:30:52 +0900

    feat: 로또 숫자 중복방지 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,20 +9,28 @@ public class Lotto {
 
     private final List<LottoNumber> numbers;
 
-    public Lotto(List<LottoNumber> numbers) {
-        assertNumbersLength(numbers);
-        this.numbers = numbers;
-    }
-
     public Lotto(String[] stringNumbers) {
         this(Arrays.stream(stringNumbers)
                 .map(LottoNumber::new)
                 .collect(Collectors.toList()));
     }
 
+    public Lotto(List<LottoNumber> numbers) {
+        assertNumbersLength(numbers);
+        assertNumbersNotDuplicated(numbers);
+        this.numbers = numbers;
+    }
+
     private void assertNumbersLength(List<LottoNumber> numbers) {
         if (numbers == null || numbers.isEmpty() || numbers.size() != NUMBER_LENGTH) {
             throw new IllegalArgumentException("로또는 6개의 숫자가 필요합니다.");
+        }
+    }
+
+    private void assertNumbersNotDuplicated(List<LottoNumber> numbers) {
+        int distinctCount = (int) numbers.stream().distinct().count();
+        if (distinctCount != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("중복되지 않는 번호를 입력해주세요.");
         }
     }
 
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -28,13 +28,6 @@ public class LottoTest {
                     .doesNotThrowAnyException();
         }
 
-        @DisplayName("로또는 6개의 로또 번호를 생성자로 넘기지 않으면 IllegalArgumentException을 발생시킨다.")
-        @Test
-        void it_throws_illegalException_when_size_is_not_six() {
-            assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Lotto(Collections.emptyList()));
-        }
-
         @DisplayName("로또는 6개의 숫자로 구성된 번호를 생성자로 넘기면 Lotto를 생성한다.")
         @Test
         void it_returns_lotto_when_string_numbers() {
@@ -42,6 +35,22 @@ public class LottoTest {
 
             assertThatCode(() -> new Lotto(input))
                     .doesNotThrowAnyException();
+        }
+
+        @DisplayName("로또는 6개의 로또 번호를 생성자로 넘기지 않으면 IllegalArgumentException을 발생시킨다.")
+        @Test
+        void it_throws_illegalException_when_size_is_not_six() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new Lotto(Collections.emptyList()))
+                    .withMessage("로또는 6개의 숫자가 필요합니다.");
+        }
+
+        @DisplayName("로또는 중복된 번호를 생성자로 넘기면, IllegalArgumentException을 발생시킨다.")
+        @Test
+        void it_throws_illegalException_when_numbers_duplicated() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new Lotto(new String[]{"1", "2", "3", "4", "5", "5"}))
+                    .withMessage("중복되지 않는 번호를 입력해주세요.");
         }
 
     }
