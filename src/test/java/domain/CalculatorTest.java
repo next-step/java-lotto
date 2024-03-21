800488602760bd05046c9c06bcf647d23ef5edbc
Duyan Kim <devduyan@gmail.com>
2024-03-21 13:52:48 +0900

    feat: 계산 로직 Calculator 구현

---
+++ src/test/java/domain/CalculatorTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("입력한 문자열에 따라 연산이 수행된다")
+    void run() {
+        Numbers numbers = Numbers.create(new String[]{"1", "+", "2", "*", "4", "/", "3"});
+        Symbols symbols = Symbols.create(new String[]{"1", "+", "2", "*", "4", "/", "3"});
+
+        Calculator cal = new Calculator(numbers, symbols);
+        double result = cal.run();
+        assertThat(result).isEqualTo(4.0);
+    }
+}
