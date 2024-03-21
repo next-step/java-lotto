3a88219eca8b1fb7fa12c0a74a4e02fad84d2170
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-21 15:20:13 +0900

    refactor lotto ticket에서 안쓰이는 멤버변수 제거

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -7,12 +7,6 @@ import java.util.List;
 public class LottoTicket {
 
     private final List<Integer> numbers;
-    //private final int matchCount;
-
-    public LottoTicket(List<Integer> numbers, int matchCount) {
-        this.numbers = numbers;
-        //this.matchCount = matchCount;
-    }
 
     public LottoTicket() {
         numbers = new ArrayList<>();
