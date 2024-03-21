1abd5e90ba9e45b165e33cbe9f920ce693214ca7
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-21 19:54:44 +0900

    TEST : create test about Statistics, WinNumber, RandomUtil

--- src/test/java/lotto/domain/WinNumberTest.java
+++ src/test/java/lotto/domain/WinNumberTest.java
@@ -1,2 +1,21 @@
-package lotto.domain;public class WinNumberTest {
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class WinNumberTest {
+    @Test
+    void 당첨번호_리스트_생성() {
+        assertThat(new WinNumber().createWinNumbers("1, 2, 3, 4, 5, 6"))
+                .containsExactlyInAnyOrder(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void 당첨번호_리스트_길이() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new WinNumber().createWinNumbers("1, 2, 3, 4, 5, 6, 7"))
+                .withMessageMatching("당첨 번호는 6개여야 합니다.");
+    }
 }
--- src/test/java/lotto/util/RandomUtilTest.java
+++ src/test/java/lotto/util/RandomUtilTest.java
@@ -1,2 +1,12 @@
-package lotto.util;public class RandomUtilTest {
+package lotto.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomUtilTest {
+    @Test
+    void 로또_번호_생성() {
+        assertThat(RandomUtil.getRandom()).isBetween(1,45);
+    }
 }
