a8233e381baa314deb518920805a9933b64d2104
jeonsanggi <jsg2861@gmail.com>
2024-03-21 22:55:45 +0900

    refactor - 유틸리티 클래스 private 생성자 추가

--- src/main/java/calculator/StringClassifier.java
+++ src/main/java/calculator/StringClassifier.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 public class StringClassifier {
     private static final String DELIMITER = " ";
+
+    private StringClassifier() {
+        throw new RuntimeException("유틸리티 클래스는 인스턴스화 할 수 없습니다.");
+    }
+
     public static List<String> split(String text) {
         return List.of(text.split(DELIMITER));
     }
+
     public static boolean isNumber(String str) {
         return str.matches("-?\\d+(\\.\\d+)?");
     }
