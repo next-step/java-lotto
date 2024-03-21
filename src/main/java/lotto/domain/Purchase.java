219dee2ad3f4819eca44dd788469825df1d4384a
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-21 19:29:02 +0900

    FEAT : create Purchase to calculate purchase count

--- src/main/java/lotto/domain/Purchase.java
+++ src/main/java/lotto/domain/Purchase.java
@@ -1,2 +1,11 @@
-package lotto.domain;public class Purchase {
+package lotto.domain;
+
+public class Purchase {
+    private static final int COST = 1000;
+    public int calculate(int money) {
+        if (money < COST) {
+            throw new IllegalArgumentException("구매 금액은 1000원 이상이어야 합니다.");
+        }
+        return money / COST;
+    }
 }
