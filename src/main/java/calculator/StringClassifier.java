4f0814999785030ad4b3dcfe038b46a61b0280a4
jeonsanggi <jsg2861@gmail.com>
2024-03-21 22:57:14 +0900

    refactor - 패턴 상수 선언

--- src/main/java/calculator/StringClassifier.java
+++ src/main/java/calculator/StringClassifier.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class StringClassifier {
     private static final String DELIMITER = " ";
+    private static final String NUMBER_PATTERN = "-?\\d+(\\.\\d+)?";
+    private static final String OPERATOR_PATTERN = "[+\\-*/]";
 
     private StringClassifier() {
         throw new RuntimeException("유틸리티 클래스는 인스턴스화 할 수 없습니다.");
@@ -14,10 +16,10 @@ public class StringClassifier {
     }
 
     public static boolean isNumber(String str) {
-        return str.matches("-?\\d+(\\.\\d+)?");
+        return str.matches(NUMBER_PATTERN);
     }
 
     public static boolean isOperator(String str) {
-        return str.matches("[+\\-*/]");
+        return str.matches(OPERATOR_PATTERN);
     }
 }
