f0ed36a725cf64e36ba23f8082537fecc7509ebb
kwanik-kor <rhksdlr134@naver.com>
2024-03-21 13:10:38 +0900

    feat: 로또 일급컬렉션 생성

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos(List<Lotto> lottos) {
+        if (lottos != null && lottos.isEmpty()) {
+            this.lottos.addAll(lottos);
+        }
+    }
+
+    public boolean isEmpty() {
+        return lottos.isEmpty();
+    }
+}
