f0b4610026b4d5732c99ea8a71a863a4b698c9bd
sseung12 <pon02129@naver.com>
2024-03-22 22:49:05 +0900

    feat : Lotto shop 구현

---
+++ src/main/java/lotto/LottoShop.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LottoShop {
+
+    private final static int LOTTO_VALUE = 1000;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        LottoShop lottoShop = new LottoShop();
+        lottoShop.enter();
+    }
+    public void enter(){
+        System.out.println("구매 금액을 입력하세요");
+        String inputMoney = scanner.nextLine();
+        LottoList lottoList = purchaseLotto(inputMoney);
+
+        System.out.printf("%d개를 구매했습니다.",lottoList.size());
+        System.out.println();
+        lottoList.print();
+
+        System.out.println("지난 주 당첨 번호를 입력해주세요");
+        String inputNumbers = scanner.nextLine();
+        List<Integer> answerList = extractNumber(inputNumbers);
+        LottoResult lottoResult = lottoList.getLottoResult(answerList);
+
+        System.out.println("당첨 통계");
+        System.out.println("-------");
+        lottoResult.getWinningWords().forEach(System.out::println);
+        System.out.printf("총 수익률은 %.2f 입니다.",lottoResult.getWinningPercent(LOTTO_VALUE));
+    }
+
+    private List<Integer> extractNumber(String inputNumbers) {
+        List<Integer> collect = Arrays.stream(inputNumbers.split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        if(collect.size() != 6)
+            throw new IllegalArgumentException("숫자는 6개이여야합니다.");
+        return collect;
+    }
+
+    private LottoList purchaseLotto(String requestMoney) {
+        int count = Integer.parseInt(requestMoney) / LOTTO_VALUE;
+        return new LottoList(count);
+    }
+}
