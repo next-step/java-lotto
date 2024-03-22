9594eb111a66f2aeb46161e8a5687125f475e17c
sunghyun <tjdgus2319@gmail.com>
2024-03-22 17:52:14 +0900

    fix: 당첨금액 가져오는 로직 수정

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -10,30 +10,30 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoService {
-	public static void start() {
-		long lottoMoney = InputView.inputLottoMoney();
-		int lottoCount = LottoCalculator.getAvailableLottoNumbers(lottoMoney);
+    public static void start() {
+        long lottoMoney = InputView.inputLottoMoney();
+        int lottoCount = LottoCalculator.getAvailableLottoNumbers(lottoMoney);
 
-		ResultView.printLottoCount(lottoCount);
+        ResultView.printLottoCount(lottoCount);
 
-		Lottos lottos = new Lottos(
-				Stream.generate(() -> LottoShuffle.makeLottoNumbersInRange())
-						.limit(lottoCount)
-						.collect(Collectors.toList())
-		);
+        Lottos lottos = new Lottos(
+                Stream.generate(() -> LottoShuffle.makeLottoNumbersInRange())
+                        .limit(lottoCount)
+                        .collect(Collectors.toList())
+        );
 
-		ResultView.printLottos(lottos);
+        ResultView.printLottos(lottos);
 
-		List<Integer> winningLottoNumber = InputView.inputWinningNumber();
+        List<Integer> winningLottoNumber = InputView.inputWinningNumber();
 
-		HashMap<LottoRank, Integer> lottoRanks = lottos.getWinningStatistics(new WinningLottoNumbers(winningLottoNumber));
+        HashMap<LottoRank, Integer> lottoRanks = lottos.getWinningStatistics(new WinningLottoNumbers(winningLottoNumber));
 
-		ResultView.printWinningStatistics(lottoRanks);
-		ResultView.printRateOfReturn(lottoMoney, getWinningMoney(lottoRanks));
-	}
+        ResultView.printWinningStatistics(lottoRanks);
+        ResultView.printRateOfReturn(lottoMoney, getWinningMoney(lottoRanks));
+    }
 
-	private static long getWinningMoney(HashMap<LottoRank, Integer> lottoRanks) {
-		return lottoRanks.values().stream().reduce(Integer::sum).orElse(0);
-	}
+    private static long getWinningMoney(HashMap<LottoRank, Integer> lottoRanks) {
+        return lottoRanks.entrySet().stream().map(lottoRank -> lottoRank.getKey().getWinningMoney()).reduce(Integer::sum).orElse(0);
+    }
 
 }
