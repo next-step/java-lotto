e3194966babcdbbf37e4b7d306d16e45719ffcba
hellonayeon <hellonykwon@gmail.com>
2024-03-22 18:21:50 +0900

    Fix: 로또 번호 개수 예외 메시지 수정

--- src/main/java/lotto/exception/IllegalLottoNumbersSizeException.java
+++ src/main/java/lotto/exception/IllegalLottoNumbersSizeException.java
@@ -4,6 +4,6 @@ import java.text.MessageFormat;
 
 public class IllegalLottoNumbersSizeException extends IllegalArgumentException {
     public IllegalLottoNumbersSizeException(int validSize, int size) {
-        super(MessageFormat.format("유효하지 않은 로또 번호 개수 입니다. (유효한 번호 개수: {0}, 입력된 번호 개수: {1})", size));
+        super(MessageFormat.format("유효하지 않은 로또 번호 개수 입니다. (유효한 번호 개수: {0}, 입력된 번호 개수: {1})", validSize, size));
     }
 }
