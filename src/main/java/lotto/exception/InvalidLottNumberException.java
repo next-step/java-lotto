955e11a6e7d2350bbb8d5babac15ede03dd63c81
hong yuseok <jericoh@naver.com>
2024-03-23 11:39:48 +0900

    refactor: 커스텀 예외 리팩토링

--- src/main/java/lotto/exception/InvalidLottNumberException.java
+++ src/main/java/lotto/exception/InvalidLottNumberException.java
@@ -4,10 +4,6 @@ public class InvalidLottNumberException extends IllegalArgumentException {
     private static final String WRONG_LOTTO_NUMBER_MESSAGE = "(%s): 잘못된 번호입니다.";
 
     public InvalidLottNumberException(int wrongInput) {
-        this(String.valueOf(wrongInput));
-    }
-
-    public InvalidLottNumberException(String wrongInput) {
         super(String.format(WRONG_LOTTO_NUMBER_MESSAGE, wrongInput));
     }
 }
