b9f0eef30d51d3ba7bd9eac016553e9812c6705f
m5s3 <devmyoungsoo2@gmail.com>
2024-03-23 17:27:36 +0900

    fix(LottoTest): 깨진 test 수정

--- src/test/java/lottogame/LottoTest.java
+++ src/test/java/lottogame/LottoTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.stream.Stream;
 import lottogame.domain.Lotto;
+import lottogame.domain.Rank;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,9 +13,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 class LottoTest {
 
     @ParameterizedTest
-    @MethodSource("lottoAndCountOfWinner")
-    void 로또_당첨(Lotto winnerLotto, Lotto lotto, int countOfWinner) {
-        assertThat(winnerLotto.match(lotto)).isEqualTo(countOfWinner);
+    @MethodSource("lottoAndRank")
+    void 로또_당첨(Lotto winnerLotto, Lotto lotto, Rank rank) {
+        assertThat(winnerLotto.getRank(lotto)).isEqualTo(rank);
     }
 
     @ParameterizedTest
@@ -23,12 +24,16 @@ class LottoTest {
         assertThat(winnerLotto.getPrize(lotto)).isEqualTo(prize);
     }
 
-    static Stream<Arguments> lottoAndCountOfWinner() {
+    static Stream<Arguments> lottoAndRank() {
         return Stream.of(
-                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 6, 7, 8)), 6),
-                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 6, 7, 14)), 5),
-                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 6, 14, 17)), 4),
-                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 14, 17, 21)), 3)
+                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 6, 7, 8)),
+                        Rank.FIRST),
+                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 6, 7, 14)),
+                        Rank.SECOND),
+                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 6, 14, 17)),
+                        Rank.THIRD),
+                Arguments.arguments(new Lotto(List.of(1, 4, 5, 6, 7, 8)), new Lotto(List.of(1, 4, 5, 14, 17, 21)),
+                        Rank.FOURTH)
         );
     }
 
