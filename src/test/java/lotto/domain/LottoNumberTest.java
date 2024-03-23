e13596b3ec4ba43217525e78ea3385039f04ec34
namhyeop <skaguq@gmail.com>
2024-03-23 16:04:59 +0900

    feat : LottoNumber 입력 범위값 초과시 예외 발생 검사 단위 테스트 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -2,10 +2,14 @@ package lotto.domain;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.error.SizeExceedLottoNumberException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoNumberTest {
 
@@ -18,9 +22,18 @@ class LottoNumberTest {
 
     @Test
     @DisplayName("로또번호는 1부터 45까지의 범위의 값만 가질수 있다")
-    public void Lotto_Number_Should_Have_Values_1_To_45(int input) {
+    public void Lotto_Number_Should_Have_Values_1_To_45() {
         assertThat(values)
             .extracting(LottoNumber::getLottoNumber)
             .allSatisfy(lottoNumber -> assertThat(lottoNumber).isBetween(1, 45));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-47, -1, 46, 47})
+    @DisplayName("로또번호가 1부터 45의 범위가 아닌 경우 예외가 발생한다")
+    public void If_Lotto_Number_Not_In_range1_to_45_Occurs_Exception(int input) {
+        assertThatThrownBy(() -> LottoNumber.formInt(input))
+            .isInstanceOf(SizeExceedLottoNumberException.class)
+            .hasMessage("로또 번호는 1부터 45의 범위만 가능합니다. 입력값: " + input);
     }
 }
