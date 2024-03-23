6809531c54f95c2bc60dd8e7029a3e6437579a7b
동원 <ai06021@gmail.com>
2024-03-23 21:10:56 +0900

    docs : create docs

---
+++ src/main/java/LottoApplication.java
@@ -0,0 +1,10 @@
+import view.LottoInput;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+
+        LottoInput lottoInput = new LottoInput();
+
+
+    }
+}
---
+++ src/main/java/view/LottoInput.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.Scanner;
+
+public class LottoInput {
+    private final Scanner input = new Scanner(System.in);
+
+
+    public int buyCash() {
+        int money = input.nextInt();
+        correctMoney(money);
+        return money;
+    }
+
+    private void correctMoney(int money) {
+        if (money < 0 || money % 1000 != 0)
+            throw new IllegalArgumentException("잘못된 구입금액 입니다");
+        System.out.println(money / 1000 + "개를 구매했습니다.");
+    }
+}
