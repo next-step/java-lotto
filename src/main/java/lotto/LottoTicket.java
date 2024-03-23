d91244476d02acb5ac0639159b0c7f9b0b092f25
박소민 <qkrthals0524@naver.com>
2024-03-24 03:03:51 +0900

    LottoTicket 클래스 변수 타입 수정 및 createTicket 메서드 생성

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,50 +1,47 @@
 package lotto;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoTicket {
-    private int number1;
-    private int number2;
-    private int number3;
-    private int number4;
-    private int number5;
-    private int number6;
+    private  List<Integer> numbers;
 
-
-    public LottoTicket(List<Integer> numbers) {
-        checkSizeNumbers(numbers);
-        checkUniqueNumbers(numbers);
-        checkBoundaryNumbers(numbers);
-        Collections.sort(numbers);
-        this.number1 = numbers.get(0);
-        this.number2 = numbers.get(1);
-        this.number3 = numbers.get(2);
-        this.number4 = numbers.get(3);;
-        this.number5 = numbers.get(4);;
-        this.number6 = numbers.get(5);;
+    public static LottoTicket createTicket(List<Integer> inputs) {
+        checkSizeNumbers(inputs);
+        checkUniqueNumbers(inputs);
+        checkBoundaryNumbers(inputs);
+        Collections.sort(inputs);
+        return new LottoTicket(inputs);
+    }
+    private LottoTicket(List<Integer> inputs) {
+        this.numbers = inputs;
     }
 
-    private void checkUniqueNumbers(List<Integer> numbers) {
+    private static void checkUniqueNumbers(List<Integer> numbers) {
         if(numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("중복 값은 입력할 수 없습니다.");
         }
     }
 
-    private void checkBoundaryNumbers(List<Integer> numbers) {
+    private static void checkBoundaryNumbers(List<Integer> numbers) {
         if(!numbers.stream().allMatch(number -> number >=1 && number <=45)) {
             throw new IllegalArgumentException("1이상 45이하의 숫자만 입력할 수 있습니다.");
         }
     }
 
-    private void checkSizeNumbers(List<Integer> numbers) {
+    private static void checkSizeNumbers(List<Integer> numbers) {
         if(numbers.size()!=6) {
             throw new IllegalArgumentException("로또 생성을 위해서는 6개의 숫자가 필요합니다.");
         }
     }
 
-    @Override
-    public String toString() {
-        return "[" + number1 + ", " + number2 + ", " + number3 + ", " + number4 + ", " + number5 + ", " + number6 + "]";
+//    public List<Integer> displayNumbers() {
+//        return Collections.unmodifiableList(numbers);
+//    }
+
+    public List<Integer> displayNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 }
--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -1,6 +1,5 @@
 import java.util.List;
 import lotto.LottoTicket;
-import lotto.LottoTicketBall;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -9,21 +8,21 @@ public class LottoTicketTest {
     @Test
     public void 로또는_1_에서_45_까지의_숫자만_가질_수_있음() {
         assertThatThrownBy(() ->
-            new LottoTicket(List.of(0,1,2,33,37,45))
+            LottoTicket.createTicket(List.of(0,1,2,33,37,45))
         ).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("1이상 45이하의 숫자만 입력할 수 있습니다.");
     }
 
     @Test
     public void 로또는_6개의_숫자를_가짐() {
         assertThatThrownBy(() ->
-                new LottoTicket(List.of(1,2,33,44,45))
+                LottoTicket.createTicket(List.of(1,2,33,44,45))
         ).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("로또 생성을 위해서는 6개의 숫자가 필요합니다.");
     }
 
     @Test
     public void 하나의_로또_안에서_숫자가_겹치지_않음() {
         assertThatThrownBy(() ->
-                new LottoTicket(List.of(1,1,23,34,44,45))
+                LottoTicket.createTicket(List.of(1,1,23,34,44,45))
         ).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("중복 값은 입력할 수 없습니다.");
 
     }
