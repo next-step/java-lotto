c0795b5b9049580e5b8e085ebff4942ef08fb4f6
kylekim2123 <kyle2123@naver.com>
2024-03-23 19:03:11 +0900

    [STEP 1] test: 계산식 문자열의 null, blank 여부와, 정규 표현식 매칭 여부를 판단하는 테스트 코드 추가

--- src/main/java/config/CalculatorExceptionMessage.java
+++ src/main/java/config/CalculatorExceptionMessage.java
@@ -8,7 +8,11 @@ public enum CalculatorExceptionMessage {
     OPERAND_SHOULD_BE_INTEGER_FORMAT("피연산자는 반드시 정수형 숫자여야 합니다."),
     OPERATOR_SHOULD_BE_ARITHMETIC_SYMBOL("연산자는 반드시 사칙연산 기호(+, -, *, /) 중 하나여야 합니다."),
     DIVISOR_CANNOT_BE_ZERO("0으로 나눌 수 없습니다."),
-    EXPRESSION_CANNOT_BE_NULL_OR_EMPTY("계산식은 null 이거나 비어있을 수 없습니다.");
+    EXPRESSION_CANNOT_BE_NULL_OR_EMPTY("계산식은 null 이거나 비어있을 수 없습니다."),
+    INPUT_EXPRESSION_TEXT_CANNOT_BE_NULL_OR_BLANK("계산식으로 null, 빈 문자열, 공백, 개행을 입력할 수 없습니다."),
+    INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN("형식에 맞는 계산식을 입력해야 합니다."),
+    UNEXPECTED_EXCEPTION("알 수 없는 에러가 발생하였습니다."),
+    ;
 
     private final String message;
 
---
+++ src/test/java/util/ExpressionParserTest.java
@@ -0,0 +1,85 @@
+package util;
+
+import static config.CalculatorExceptionMessage.INPUT_EXPRESSION_TEXT_CANNOT_BE_NULL_OR_BLANK;
+import static config.CalculatorExceptionMessage.INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ExpressionParserTest {
+
+    @ParameterizedTest
+    @NullSource
+    @DisplayName("계산식 문자열이 null인 경우 예외를 던진다.")
+    void ParseToTokens_NullText_Exception(final String nullText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(nullText))
+                .withMessage(INPUT_EXPRESSION_TEXT_CANNOT_BE_NULL_OR_BLANK.message(nullText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "     ", "\n", " \n"})
+    @DisplayName("계산식 문자열이 빈 문자열, 공백, 개행인 경우 예외를 던진다.")
+    void ParseToTokens_BlankText_Exception(final String blankText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(blankText))
+                .withMessage(INPUT_EXPRESSION_TEXT_CANNOT_BE_NULL_OR_BLANK.message(blankText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2 + + 5", "2 2 + 5", "2 - 1 1 / / 5"})
+    @DisplayName("계산식 문자열에 피연산자와 연산자가 번갈아가며 나열되지 않은 경우 예외를 던진다.")
+    void ParseToTokens_OperandAndOperatorNotTakeTurn_Exception(final String expressionText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(expressionText))
+                .withMessage(INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN.message(expressionText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"+ 5", "5 +", "5 + 4 -", "/ 3 - 4", "5 + 4 - 1 *"})
+    @DisplayName("계산식 문자열의 시작과 끝 요소가 피연산자가 아닌 경우 예외를 던진다.")
+    void ParseToTokens_TextStartAndEndAreNotOperand_Exception(final String expressionText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(expressionText))
+                .withMessage(INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN.message(expressionText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2+ 5", "2 +5", "2+5", "2 - 5 /3", "3/ 4 - 1", "4 + 5 / 1 -2"})
+    @DisplayName("계산식 문자열의 요소 사이에 공백이 없는 경우 예외를 던진다.")
+    void ParseToTokens_NoWhiteSpaceBetweenOperandAndOperator_Exception(final String expressionText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(expressionText))
+                .withMessage(INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN.message(expressionText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2  + 5", "2 +  5", "2  +  5", "4 -     1 / 5", "1 - 3 / 2   + 1"})
+    @DisplayName("계산식 문자열의 요소 사이에 두 칸 이상의 공백이 있는 경우 예외를 던진다.")
+    void ParseToTokens_MoreThanSingleWhiteSpaceBetweenOperandAndOperator_Exception(final String expressionText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(expressionText))
+                .withMessage(INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN.message(expressionText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a + 1", "1 + a", "1a + 1", "1 + 1a", "a + b", "2.5 * 1", "3^2 / 2 + 1"})
+    @DisplayName("계산식 문자열의 피연산자가 정수형이 아닌 경우 예외를 던진다.")
+    void ParseToTokens_OperandNotIntegerFormat_Exception(final String expressionText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(expressionText))
+                .withMessage(INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN.message(expressionText));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 % 2", "1 $ 2", "1 @ 2", "1 + 2 # 3", "1 // 2 + 4"})
+    @DisplayName("계산식 문자열의 연산자가 사칙연산 기호(+, -, *, /) 중 하나가 아닌 경우 예외를 던진다.")
+    void ParseToTokens_OperatorNotArithmeticSymbol_Exception(final String expressionText) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ExpressionParser.parseToTokens(expressionText))
+                .withMessage(INPUT_EXPRESSION_TEXT_SHOULD_FOLLOW_PATTERN.message(expressionText));
+    }
+}
