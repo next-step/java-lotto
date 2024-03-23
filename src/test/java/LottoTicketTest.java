6e97613dae854054470fc8e6d635e63932f86b76
박소민 <qkrthals0524@naver.com>
2024-03-23 17:54:35 +0900

    로또는 6개의 숫자를 가짐

--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -1,4 +1,5 @@
 import lotto.LottoTicketBall;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,5 +12,11 @@ public class LottoTicketTest {
     public void 로또는_1_에서_45_까지의_숫자만_가질_수_있음(int input) {
         System.out.println(lottoTicketBall.getTicket());
         assertThat(lottoTicketBall.getTicket().contains(input)).isFalse();
+    }
+
+    @Test
+    public void 로또는_6개의_숫자를_가짐() {
+        System.out.println(lottoTicketBall.getTicket());
+        assertThat(lottoTicketBall.getTicket().size()).isEqualTo(6);
     }
 }
