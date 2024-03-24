1e9240f26e8bf7e68485f6bea1f84523ceb47ccb
namhyeop <skaguq@gmail.com>
2024-03-25 00:38:00 +0900

    feat : PurchaseAmount 구현

---
+++ src/main/java/lotto/domain/lotto/PurchaseAmount.java
@@ -0,0 +1,20 @@
+package lotto.domain.lotto;
+
+import lotto.error.exception.PurchaseAmountException;
+
+public class PurchaseAmount {
+
+    private int purchaseAmount;
+
+    public PurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount % 1000 != 0){
+            throw new PurchaseAmountException(purchaseAmount);
+        }
+
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+}
