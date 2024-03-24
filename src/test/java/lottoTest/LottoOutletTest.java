2cb1b75141b0ef67d954a7cdcbd0ff301e291349
JJewon <eldertree777@gmail.com>
2024-03-24 21:57:08 +0900

    [feat] 당첨금들을 반환하는 기능 및 테스트 구현

--- src/test/java/lottoTest/LottoOutletTest.java
+++ src/test/java/lottoTest/LottoOutletTest.java
@@ -6,8 +6,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoOutletTest {
     @ParameterizedTest(name = "{0} 가격으로 구매가능한 로또 장수는 {1}")
@@ -26,5 +33,33 @@ public class LottoOutletTest {
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     void generateLottosTest(int count) {
         Assertions.assertThat(LottoOutlet.generateLottos(count).size()).isEqualTo(count);
+    }
+
+    @ParameterizedTest(name = "{0}회 일치하는 당청금은 {1}원 이다.")
+    @CsvSource(value = {"1:0", "2:0", "3:5000", "4:50000", "5:1500000", "6:2000000000"}, delimiter = ':')
+    void getWinning(int matchCount, int expectedWinning) {
+        Assertions.assertThat(LottoOutlet.getWinning(matchCount)).isEqualTo(expectedWinning);
+    }
+
+    @ParameterizedTest(name = "{0}번 일치하는 로또들의 상금은 총 {1} 이다.")
+    @MethodSource("generateMatchCountsWinningAmount")
+    void getWinnings(List<Integer> matchCounts, int winningAmount) {
+        Assertions.assertThat(LottoOutlet.getWinnings(matchCounts)).isEqualTo(winningAmount);
+    }
+
+    private static Stream<Arguments> generateMatchCountsWinningAmount() {
+        return Stream.of(
+                Arguments.arguments(givenNumbers(1, 1, 2, 1), 0),
+                Arguments.arguments(givenNumbers(1, 1, 2, 3), 5000),
+                Arguments.arguments(givenNumbers(3, 3, 3, 4), 5000 * 3 + 50000 * 1),
+                Arguments.arguments(givenNumbers(6, 0, 0, 0), 2000000000 * 1),
+                Arguments.arguments(givenNumbers(0, 3, 4, 5), 5000 + 50000 + 1500000)
+        );
+    }
+
+    private static List<Integer> givenNumbers(int... numbers) {
+        return Arrays.stream(numbers)
+                .mapToObj(Integer::new)
+                .collect(Collectors.toList());
     }
 }
