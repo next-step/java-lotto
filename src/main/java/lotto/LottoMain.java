f57f838752462860315da0113e24bae995f78d65
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:17:12 +0900

    CHORE : 로또 프로그램 메인

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -4,6 +4,6 @@ import lotto.controller.LottoController;
 
 public class LottoMain {
     public static void main(String[] args) {
-        new LottoController().lotto();
+        LottoController.startLotto();
     }
 }
