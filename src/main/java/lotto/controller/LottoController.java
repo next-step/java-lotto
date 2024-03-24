63f9ce9e2a3b151d84894a0630d198e8b3a7f001
JJewon <eldertree777@gmail.com>
2024-03-25 01:00:52 +0900

    [refactor] 불필요한 코드 삭제

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,5 @@
 package lotto.controller;
 
-import lotto.model.ResultLotto;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.ResultView;
@@ -16,12 +15,10 @@ public class LottoController {
 
     public void buy() {
         int seedMoney = InputView.inputSeedMoney();
-        ResultLotto resultLotto = lottoService.buyLottos(seedMoney);
 
-        ResultView.viewLottos(resultLotto);
+        ResultView.viewLottos(lottoService.buyLottos(seedMoney));
         List<Integer> winningNumbers = InputView.inputWinningNumbers();
 
-        resultLotto = lottoService.checkLottoResult(winningNumbers);
-        ResultView.viewResult(resultLotto);
+        ResultView.viewResult(lottoService.checkLottoResult(winningNumbers));
     }
 }
