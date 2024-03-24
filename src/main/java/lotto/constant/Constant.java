7af53f75a5603dcc04df41553900d0513c83c710
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:16:18 +0900

    CHORE : 로또 실행에 필요한 상수 정의

--- src/main/java/lotto/constant/Constant.java
+++ src/main/java/lotto/constant/Constant.java
@@ -1,10 +1,15 @@
-package lotto.domain;
+package lotto.constant;
 
 import java.util.HashMap;
 
 public class Constant {
+    public static final int LOTTO_SIZE = 6;
+    public static final int MIN_LOTTO = 1;
+    public static final int MAX_LOTTO = 45;
     public static final int FIRST_PLACE = 2000000000;
     public static final int SECOND_PLACE = 1500000;
     public static final int THIRD_PLACE = 50000;
     public static final int FOURTH_PLACE = 5000;
+    public static final int WINNING_SIZE = 6;
+    public static final String DELIMITER = ",";
 }
