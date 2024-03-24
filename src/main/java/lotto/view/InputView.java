30a7ab8c0ebdf325d078b20471505bd3dec7e190
JJewon <eldertree777@gmail.com>
2024-03-25 01:19:41 +0900

    [refactor] 상수 치환 적용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String SPLIT_DELIMEITER = ",";
     private static final String INPUT_SEED_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
@@ -26,7 +27,7 @@ public class InputView {
         System.out.println(INPUT_WINNING_MESSAGE);
 
         String input = scanner.nextLine();
-        List<String> tokens = List.of(input.split(","));
+        List<String> tokens = List.of(input.split(SPLIT_DELIMEITER));
         List<Integer> winningNumbers = new ArrayList<>();
 
         for (String token : tokens) {
