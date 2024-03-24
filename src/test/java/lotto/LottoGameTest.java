bc7032ebd950978c6471614c77f01fd14231a92e
jaeyounglee <ljyda214@gmail.com>
2024-03-25 00:21:52 +0900

    TEST : WinningLotto 클래스 추가에 따른 테스트 코드 변경

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,18 +11,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoGameTest {
 
-    /**
-     * 수익률 = 총 당첨 금액 / 구입 금액
-     */
-    @Test
-    void statistics_on_matched_count() {
-        Money budget = new Money(1000L);
-        Lotto winLotto = new Lotto(Set.of(1, 2, 3, 4, 5, 6));
-        LottoGame lottoGame = new LottoGame(List.of(
-                new Lotto(Set.of(1, 3, 5, 14, 22, 45))
-        ));
-        RankMap prizeByRank = lottoGame.getPrizeByRank(winLotto);
 
-        assertThat(prizeByRank).isEqualTo(new RankMap(List.of(new MatchedCount(3))));
+    @Nested
+    class when_lotto_number_is_matched_on_five{
+        @Test
+        @DisplayName("bonus 값을 만족하지 않는 경우, 2등에 당첨된다.")
+        void bonus_is_not_matched() {
+            Money budget = new Money(1000L);
+            WiningLotto winLotto = new WiningLotto(Set.of(1, 2, 3, 4, 5, 6), 7);
+            LottoGame lottoGame = new LottoGame(List.of(
+                    new Lotto(Set.of(1, 2, 3, 4, 5, 45))
+            ));
+            RankMap prizeByRank = lottoGame.getPrizeByRank(winLotto);
+
+            assertThat(prizeByRank).isEqualTo(new RankMap(List.of(new MatchedCount(5))));
+        }
+
+        @Test
+        @DisplayName("bonus 값을 만족하는 경우, 2등에 당첨된다.")
+        void bonus_is_matched() {
+            Money budget = new Money(1000L);
+            WiningLotto winLotto = new WiningLotto(Set.of(1, 2, 3, 4, 5, 6), 7);
+            LottoGame lottoGame = new LottoGame(List.of(
+                    new Lotto(Set.of(1, 2, 3, 4, 5, 7))
+            ));
+            RankMap prizeByRank = lottoGame.getPrizeByRank(winLotto);
+
+            assertThat(prizeByRank).isEqualTo(new RankMap(List.of(new MatchedCount(5, true))));
+        }
     }
 }
