1581a89e0333805523c0036bc4cb8c03fb9ea400
namhyeop <skaguq@gmail.com>
2024-03-25 00:39:21 +0900

    feat : I/O 클래스 구현

---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,47 @@
+package lotto.view;
+
+import static lotto.error.ErrorMessage.ILLEGAL_STATE;
+import static lotto.error.ErrorMessage.INVALID_FORMAT;
+import static lotto.error.ErrorMessage.INVALID_INPUT_EMPTY;
+import static lotto.error.ErrorMessage.INVALID_INPUT_TYPE_INT;
+import static lotto.error.ErrorMessage.UN_EXPECTED_ERROR;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Input {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private Input() {
+    }
+
+    public static int inputPurchaseAmount(){
+        System.out.println("구매금액을 입력해 주세요");
+        return scanner.nextInt();
+    }
+
+    public static List<Integer> inputWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        try {
+            scanner.nextLine();
+            return Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        } catch (NumberFormatException numberFormatException){
+            throw new NumberFormatException(INVALID_FORMAT.getMessage());
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException(INVALID_INPUT_TYPE_INT.getMessage());
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException(INVALID_INPUT_EMPTY.getMessage());
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(ILLEGAL_STATE.getMessage(), e);
+        } catch (Exception e) {
+            throw new RuntimeException(UN_EXPECTED_ERROR.getMessage(), e);
+        }
+    }
+}
---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,41 @@
+package lotto.view;
+
+import java.text.MessageFormat;
+import java.util.List;
+import lotto.domain.grade.Grade;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.PurchaseAmount;
+import lotto.domain.winning.WinningStatistic;
+
+public class Output {
+
+    private Output() {
+    }
+
+    public static void printAutoLotto(List<Lotto> autoLotto) {
+        autoLotto.stream().forEach(System.out::println);
+    }
+
+    public static void printPurchaseCount(PurchaseAmount purchaseAmount) {
+        System.out.println(
+            MessageFormat.format("{0}개를 구매했습니다.", purchaseAmount.getPurchaseAmount() / 1000));
+    }
+
+    public static void printLottoResult(WinningStatistic winningStatistic) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        printGradeResult(Grade.FOUR_GRADE, winningStatistic);
+        printGradeResult(Grade.THIRD_GRADE, winningStatistic);
+        printGradeResult(Grade.SECOND_GRADE, winningStatistic);
+        printGradeResult(Grade.FIRST_GRADE, winningStatistic);
+        System.out.println(MessageFormat.format("총 수익률은 {0}입니다.", winningStatistic.calculateProfitRate()));
+    }
+
+    private static void printGradeResult(Grade grade, WinningStatistic winningStatistic) {
+        System.out.println(
+            MessageFormat.format("{0}개 일치 ({1}원)- {2}",
+                grade.getCorrectingCount(),
+                grade.getPrizeMoney(),
+                winningStatistic.getGradeCount(grade)));
+    }
+}
