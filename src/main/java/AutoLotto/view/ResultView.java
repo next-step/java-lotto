80ef2d4bee1f3dadcc270251eb812a6e9d9d2863
astrid-dm <himax_01@naver.com>
2024-03-24 20:04:06 +0900

    refactor : 우승 문구 출력시, switch가 아닌 고정된 포맷으로 출력

--- src/main/java/AutoLotto/view/ResultView.java
+++ src/main/java/AutoLotto/view/ResultView.java
@@ -18,13 +18,6 @@ public class ResultView {
     private static final String START_BRACKET = "[ ";
     private static final String COMMA_DELIMITER = ", ";
     private static final String END_BRACKET = " ]";
-    private static final float INIT_PROFIT = 0f;
-    private static final String NONE_MATCHED_RESULT = "당첨된 로또가 없습니다.";
-    private static final String FIFTH_PRIZE_DESCRIPTION = "3개 일치 (5,000원) - ";
-    private static final String FOURTH_PRIZE_DESCRIPTION = "4개 일치 (50,000원) - ";
-    private static final String THIRD_PRIZE_DESCRIPTION = "5개 일치 (1,500,000원) - ";
-    private static final String SECOND_PRIZE_DESCRIPTION = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-    private static final String FIRST_PRIZE_DESCRIPTION = "6개 일치 (2,000,000,000원) - ";
 
     public void outputPurchasedLottos(LottoMachine lottoMachine) {
         outputPurchasedNumberOfLotto(lottoMachine.getNumberOfLottos());
@@ -44,7 +37,9 @@ public class ResultView {
     }
 
     private String convertSetLottoToStringLotto(Set<LottoNumber> lottoNumbers) {
-        return String.join(COMMA_DELIMITER, lottoNumbers.stream().map(Object::toString).collect(Collectors.toList()));
+        return String.join(COMMA_DELIMITER, lottoNumbers.stream()
+                .map(lottoNumber -> lottoNumber.getLottoNumberAsString())
+                .collect(Collectors.toList()));
     }
 
     private void outputString(String string) {
@@ -55,42 +50,18 @@ public class ResultView {
 
     public void outputLottoResult(Map<PrizeEnum, Integer> lottoResult, BigDecimal profit) {
         outputString(START_RESULT_TEXT);
-        outputMatchingLottos(lottoResult, profit);
+        outputMatchingLottos(lottoResult);
         outputProfit(profit);
     }
 
-    private void outputMatchingLottos(Map<PrizeEnum, Integer> lottoResult, BigDecimal profit) {
-        if (profit == BigDecimal.ZERO) {
-            outputString(NONE_MATCHED_RESULT);
-            return;
-        }
-
+    private void outputMatchingLottos(Map<PrizeEnum, Integer> lottoResult) {
         for (PrizeEnum prize : lottoResult.keySet()) {
             outputString(getPrizeResult(prize, lottoResult.get(prize)));
         }
     }
 
     private String getPrizeResult(PrizeEnum prize, int value) {
-        switch(prize.getMatchedCount()) {
-            case 3 :
-                return String.format("%s%d", FIFTH_PRIZE_DESCRIPTION, value);
-            case 4 :
-                return String.format("%s%d", FOURTH_PRIZE_DESCRIPTION, value);
-            case 5 :
-                return getFifthPrizeResult(prize, value);
-            case 6 :
-                return String.format("%s%d", FIRST_PRIZE_DESCRIPTION, value);
-            default :
-                return null;
-        }
-    }
-
-    private String getFifthPrizeResult(PrizeEnum prize, int value) {
-        if (prize.isBonusMatched()) {
-            return String.format("%s%d", SECOND_PRIZE_DESCRIPTION, value);
-        }
-
-        return String.format("%s%d", THIRD_PRIZE_DESCRIPTION, value);
+        return String.format("%d%s (%d) - %d개", prize.getMatchedCount(),"개 일치", prize.getPrize(), value);
     }
 
     private void outputProfit(BigDecimal profit) {
