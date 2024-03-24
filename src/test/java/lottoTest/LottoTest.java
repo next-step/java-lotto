93f7d10bf4511efb9cf50bb0bdb383a6df8098fa
JJewon <eldertree777@gmail.com>
2024-03-24 20:26:09 +0900

    [feat] Lotto 테스트 및 기능 구현

--- src/test/java/lottoTest/LottoTest.java
+++ src/test/java/lottoTest/LottoTest.java
@@ -1,10 +1,9 @@
 package lottoTest;
 
-import lotto.model.Lotto;
-import lotto.model.MatchNumber;
-import lotto.model.Number;
-import lotto.model.Numbers;
+import lotto.Lotto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,12 +15,9 @@ import java.util.stream.Stream;
 
 public class LottoTest {
 
-    //테스트용 더미 Number
-    private static final Number dummyNumber = new Number(-1);
-
     @ParameterizedTest(name = "{1} 로또는 {1} 값을 리스트 반환")
     @MethodSource("generateLotto")
-    void lottoGetNumbersTest(Lotto lotto, Numbers numbers) {
+    void lottoGetNumbersTest(Lotto lotto, List<Integer> numbers) {
         Assertions.assertThat(lotto.getNumbers()).isEqualTo(numbers);
     }
 
@@ -31,48 +27,13 @@ public class LottoTest {
         Assertions.assertThat(lotto.getNumbers().size()).isEqualTo(6);
     }
 
-
-    @ParameterizedTest(name = "{1} 해당 로또와 {2} 당청 번호 비교시 일치하는 수는 {3} 이다.")
-    @MethodSource("createLottoWinningnumbersMatchcount")
-    void getMatchNumberCount(Lotto lotto, List<Integer> lottoNumbers, List<Integer> winningNumbers, int expectedMatchCount) {
-        Assertions.assertThat(lotto.matchNumbers(Numbers.valueOf(winningNumbers), dummyNumber).getMatchCount()).isEqualTo(expectedMatchCount);
-    }
-
-    @ParameterizedTest(name = "{1} 로또 번호와 {2} 보너스 번호의 일치 여부는 {3} 이다.")
-    @MethodSource("createLottoWinningnumbersBonus")
-    void matchBounusTest(Lotto lotto, List<Integer> lottoNumbers, int bonusNumber, boolean expected) {
-        MatchNumber matchNumber = lotto.matchNumbers(Numbers.valueOf(lottoNumbers), new Number(bonusNumber));
-        Assertions.assertThat(matchNumber.isMatchBonus()).isEqualTo(expected);
-    }
-
-    private static Stream<Arguments> createLottoWinningnumbersBonus() {
-        return Stream.of(
-                Arguments.arguments(givenLotto(givenNumbers(1, 2, 3, 4, 5, 6)), givenNumbers(1, 2, 3, 4, 5, 6), 1, true),
-                Arguments.arguments(givenLotto(givenNumbers(2, 3, 4, 5, 6, 7)), givenNumbers(2, 3, 4, 5, 6, 7), 1, false),
-                Arguments.arguments(givenLotto(givenNumbers(3, 4, 5, 6, 7, 8)), givenNumbers(3, 4, 5, 6, 7, 8), 8, true),
-                Arguments.arguments(givenLotto(givenNumbers(4, 5, 6, 7, 8, 9)), givenNumbers(4, 5, 6, 7, 8, 9), 23, false),
-                Arguments.arguments(givenLotto(givenNumbers(5, 6, 7, 8, 9, 10)), givenNumbers(5, 6, 7, 8, 9, 10), 9, true)
-        );
-    }
-
-
-    private static Stream<Arguments> createLottoWinningnumbersMatchcount() {
-        return Stream.of(
-                Arguments.arguments(givenLotto(givenNumbers(1, 2, 3, 4, 5, 6)), givenNumbers(1, 2, 3, 4, 5, 6), givenNumbers(1, 2, 3, 4, 5, 6), 6),
-                Arguments.arguments(givenLotto(givenNumbers(2, 3, 4, 5, 6, 7)), givenNumbers(2, 3, 4, 5, 6, 7), givenNumbers(10, 3, 4, 5, 6, 11), 4),
-                Arguments.arguments(givenLotto(givenNumbers(3, 4, 5, 6, 7, 8)), givenNumbers(3, 4, 5, 6, 7, 8), givenNumbers(10, 11, 12, 13, 14, 15), 0),
-                Arguments.arguments(givenLotto(givenNumbers(4, 5, 6, 7, 8, 9)), givenNumbers(4, 5, 6, 7, 8, 9), givenNumbers(14, 15, 16, 17, 18, 9), 1),
-                Arguments.arguments(givenLotto(givenNumbers(5, 6, 7, 8, 9, 10)), givenNumbers(5, 6, 7, 8, 9, 10), givenNumbers(3, 1, 7, 8, 9, 10), 4)
-        );
-    }
-
     private static Stream<Arguments> generateLotto() {
         return Stream.of(
-                Arguments.arguments(givenLotto(givenNumbers(1, 2, 3, 4, 5, 6)), Numbers.valueOf(givenNumbers(1, 2, 3, 4, 5, 6))),
-                Arguments.arguments(givenLotto(givenNumbers(2, 3, 4, 5, 6, 7)), Numbers.valueOf(givenNumbers(2, 3, 4, 5, 6, 7))),
-                Arguments.arguments(givenLotto(givenNumbers(3, 4, 5, 6, 7, 8)), Numbers.valueOf(givenNumbers(3, 4, 5, 6, 7, 8))),
-                Arguments.arguments(givenLotto(givenNumbers(4, 5, 6, 7, 8, 9)), Numbers.valueOf(givenNumbers(4, 5, 6, 7, 8, 9))),
-                Arguments.arguments(givenLotto(givenNumbers(5, 6, 7, 8, 9, 10)), Numbers.valueOf(givenNumbers(5, 6, 7, 8, 9, 10)))
+                Arguments.arguments(givenLotto(givenNumbers(1, 2, 3, 4, 5, 6)), givenNumbers(1, 2, 3, 4, 5, 6)),
+                Arguments.arguments(givenLotto(givenNumbers(2, 3, 4, 5, 6, 7)), givenNumbers(2, 3, 4, 5, 6, 7)),
+                Arguments.arguments(givenLotto(givenNumbers(3, 4, 5, 6, 7, 8)), givenNumbers(3, 4, 5, 6, 7, 8)),
+                Arguments.arguments(givenLotto(givenNumbers(4, 5, 6, 7, 8, 9)), givenNumbers(4, 5, 6, 7, 8, 9)),
+                Arguments.arguments(givenLotto(givenNumbers(5, 6, 7, 8, 9, 10)), givenNumbers(5, 6, 7, 8, 9, 10))
         );
     }
 
