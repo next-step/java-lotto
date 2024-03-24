77d2bcc1c211338d4cefe1ec26d2cae8974309b4
park-cheonho <cheonho08@naver.com>
2024-03-24 20:04:03 +0900

    test: step2 입력 금액만큼 생성
    test: step2 입력 금액만큼 생성

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.domain.Fee;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
+import lotto.domain.Lottos;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +25,13 @@ public class LottoGeneratorTest {
     void 로또_생성() {
         Lotto lottoGame = lottoGenerator.generateLotto();
         assertThat(lottoGame.size()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("입력 금액만큼 생성")
+    void 입력_금액만큼_생성() {
+        Fee fee = new Fee(14000);
+        Lottos lottos = lottoGenerator.generateLottos(fee);
+        assertThat(lottos.getValue().size()).isEqualTo(14);
     }
 }
