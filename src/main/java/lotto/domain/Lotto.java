9feb8110450d1ad8b4990c65438434f75ddecbe4
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:08:16 +0900

    FEAT : Lotto 생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,2 +1,20 @@
-package lotto.domain;public class Lotto {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private final List<LottoNumber> lotto;
+
+    public Lotto(List<Integer> lottoNumbers) {
+        this.lotto = LottoGenerator.createLotto(lottoNumbers);
+    }
+
+    public Lotto() {
+        this.lotto = LottoGenerator.createLotto(LottoGenerator.shuffleNumbers());
+    }
+
+    public List<LottoNumber> getLotto() {
+        return this.lotto;
+    }
 }
