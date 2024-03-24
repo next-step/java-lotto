6636eac88c73702afb76eccf7b981e2db64d2291
JJewon <eldertree777@gmail.com>
2024-03-24 21:19:33 +0900

    [feat] 로또 당첨 번호와 N개 일치 하는지 반환 기능 및 테스트 구현

--- src/test/java/lottoTest/LottoTest.java
+++ src/test/java/lottoTest/LottoTest.java
@@ -2,8 +2,6 @@ package lottoTest;
 
 import lotto.Lotto;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -25,6 +23,23 @@ public class LottoTest {
     @MethodSource("generateLotto")
     void lottoListSizeTest(Lotto lotto) {
         Assertions.assertThat(lotto.getNumbers().size()).isEqualTo(6);
+    }
+
+
+    @ParameterizedTest(name = "{1} 해당 로또와 {2} 당청 번호 비교시 일치하는 수는 {3} 이다.")
+    @MethodSource("createLottoWinningnumbersMatchcount")
+    void getMatchNumberCount(Lotto lotto, List<Integer> lottoNumbers, List<Integer> winningNumbers, int expectedMatchCount) {
+        Assertions.assertThat(lotto.matchNumbers(winningNumbers)).isEqualTo(expectedMatchCount);
+    }
+
+    private static Stream<Arguments> createLottoWinningnumbersMatchcount() {
+        return Stream.of(
+                Arguments.arguments(givenLotto(givenNumbers(1, 2, 3, 4, 5, 6)), givenNumbers(1, 2, 3, 4, 5, 6), givenNumbers(1, 2, 3, 4, 5, 6), 6),
+                Arguments.arguments(givenLotto(givenNumbers(2, 3, 4, 5, 6, 7)), givenNumbers(2, 3, 4, 5, 6, 7), givenNumbers(10, 3, 4, 5, 6, 11), 4),
+                Arguments.arguments(givenLotto(givenNumbers(3, 4, 5, 6, 7, 8)), givenNumbers(3, 4, 5, 6, 7, 8), givenNumbers(10, 11, 12, 13, 14, 15), 0),
+                Arguments.arguments(givenLotto(givenNumbers(4, 5, 6, 7, 8, 9)), givenNumbers(4, 5, 6, 7, 8, 9), givenNumbers(14, 15, 16, 17, 18, 9), 1),
+                Arguments.arguments(givenLotto(givenNumbers(5, 6, 7, 8, 9, 10)), givenNumbers(5, 6, 7, 8, 9, 10), givenNumbers(3, 1, 7, 8, 9, 10), 4)
+        );
     }
 
     private static Stream<Arguments> generateLotto() {
