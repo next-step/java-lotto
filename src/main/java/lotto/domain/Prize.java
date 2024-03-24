caacc63456d94095d91caf28605da793bc3d3dc9
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:14:34 +0900

    FEAT : Prize 생성 - 로또 번호와 당천 번호 간 일치 수에 따른 상금

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -1,44 +1,48 @@
 package lotto.domain;
 
+import lotto.constant.Constant;
+
 import java.util.Arrays;
 
-public enum Match {
+public enum Prize {
     THREE_AGREEMENT(3) {
         @Override
         public int solvePrize() {
-            return PrizeConstant.FOURTH_PLACE;
+            return Constant.FOURTH_PLACE;
         }
     },
     FOUR_AGREEMENT(4) {
         @Override
         public int solvePrize() {
-            return SaveMatch.matchResult.get(PrizeConstant.THIRD_PLACE) + 1;
+            return Constant.THIRD_PLACE;
         }
     },
     FIVE_AGREEMENT(5) {
         @Override
         public int solvePrize() {
-            return SaveMatch.matchResult.get(PrizeConstant.SECOND_PLACE) + 1;
+            return Constant.SECOND_PLACE;
         }
     },
     SIX_AGREEMENT(6) {
         @Override
         public int solvePrize() {
-            return SaveMatch.matchResult.get(PrizeConstant.FIRST_PLACE) + 1;
+            return Constant.FIRST_PLACE;
         }
     },
     ;
 
-    private int matchCount;
+    private final int matchCount;
 
     Prize(int matchCount) {
-        checkMatchCount(matchCount);
         this.matchCount = matchCount;
     }
 
     public abstract int solvePrize();
 
-    public static Match checkMatchCount(int matchCount) {
-        return Arrays.stream(values()).filter(match -> match.matchCount < 3).findFirst().orElseThrow(() -> new IllegalArgumentException("적어도 3개 이상 일치해야 합니다."));
+    public static Prize checkMatchCount(int matchCount) {
+        return Arrays.stream(values())
+                .filter(match -> match.matchCount == matchCount)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("적어도 3개 이상 일치해야 합니다."));
     }
 }
