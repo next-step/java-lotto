8b093e516f57d4f027c879b86aa18dabed1d98df
dongock <okdong23@naver.com>
2024-03-24 17:53:57 +0900

    feat:[2-17] LottoNumber 객체 및 Test 생성

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    public static final int MIN = 1;
+    public static final int MAX = 45;
+    private final int number;
+
+    public LottoNumber(int number) {
+        if (number < MIN || number > MAX) {
+            throw new IllegalArgumentException(String.format("%d부터 %d 사이의 정확한 숫자를 입력해주세요", MIN, MAX));
+        }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
