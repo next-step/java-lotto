adaea8e56df72a93d9e329b8c0aa326136f9b64d
duyankim <devduyan@gmail.com>
2024-03-24 23:08:56 +0900

    feat: Lotto에 추첨번호 일치 갯수 멤버변수 및 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,14 +9,32 @@ import java.util.List;
 public class Lotto {
 
     private final List<Integer> autoLotto;
+    private int matchingCount;
 
     public Lotto() {
         List<Integer> shuffleNumbers = new ArrayList<>(Const.NUMBER_LIST);
         Collections.shuffle(shuffleNumbers);
         this.autoLotto = shuffleNumbers.subList(0, 6);
+        this.matchingCount = 0;
+    }
+
+    public Lotto(List<Integer> autoLotto) {
+        this.autoLotto = autoLotto;
+        this.matchingCount = 0;
     }
 
     public List<Integer> getAutoLotto() {
         return autoLotto;
+    }
+
+    public int getMatchingCount(List<Integer> winNo) {
+        return updateMatchingCount(winNo);
+    }
+
+    public int updateMatchingCount(List<Integer> winNo) {
+        matchingCount = (int) autoLotto.stream()
+                .filter(winNo::contains)
+                .count();
+        return matchingCount;
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
@@ -16,4 +18,17 @@ class LottoTest {
         assertThat(lotto1.getAutoLotto()).isNotEqualTo(lotto2.getAutoLotto());
     }
 
+    @Test
+    @DisplayName("당첨번호와 일치 갯수를 판별할 수 있다")
+    void matched_test() {
+        List<Integer> winNo = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> lotto1 = List.of(1, 2, 3, 10, 20, 30);
+        List<Integer> lotto2 = List.of(1, 2, 3, 4, 20, 30);
+        List<Integer> lotto3 = List.of(10, 20, 30, 40, 12, 13);
+
+        assertThat(new Lotto(lotto1).getMatchingCount(winNo)).isEqualTo(3);
+        assertThat(new Lotto(lotto2).getMatchingCount(winNo)).isEqualTo(4);
+        assertThat(new Lotto(lotto3).getMatchingCount(winNo)).isEqualTo(0);
+
+    }
 }
