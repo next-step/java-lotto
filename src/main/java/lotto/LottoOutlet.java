98bdb9a363ddb7d265f9719100f4d22539e8c191
JJewon <eldertree777@gmail.com>
2024-03-24 21:00:39 +0900

    [feat] N번 만큼 로또가 발급하는 기능 및 테스트 구현

--- src/main/java/lotto/LottoOutlet.java
+++ src/main/java/lotto/LottoOutlet.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoOutlet {
     private static final int LOTTO_PRICE = 1000;
@@ -11,5 +13,11 @@ public class LottoOutlet {
 
     public static Lotto generateLotto() {
         return new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        return Stream.generate(LottoOutlet::generateLotto)
+                .limit(count)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/lottoTest/LottoOutletTest.java
+++ src/test/java/lottoTest/LottoOutletTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoOutletTest {
     @ParameterizedTest(name = "{0} 가격으로 구매가능한 로또 장수는 {1}")
@@ -19,5 +20,12 @@ public class LottoOutletTest {
     @DisplayName("로또를 발급하는 기능")
     void generateLottoTest() {
         Assertions.assertThat(LottoOutlet.generateLotto()).isInstanceOf(Lotto.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    @DisplayName("로또를 N번 발급하는 기능은 N사이즈의 Lotto 리스트를 반환한다.")
+    void generateLottosTest(int count) {
+        Assertions.assertThat(LottoOutlet.generateLottos(count).size()).isEqualTo(count);
     }
 }
