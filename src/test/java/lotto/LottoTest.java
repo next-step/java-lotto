ad09edd2b474a23e8ec0f2eb1075ceacab04ff57
park-cheonho <cheonho08@naver.com>
2024-03-24 19:57:25 +0900

    test: step2 자동로또와 당첨번호 갯수 파악
    test: step2 자동로또와 당첨번호 갯수 파악

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class LottoTest {
 
+
+    @Test
+    @DisplayName("자동로또와 당첨번호 갯수 파악")
+    void 자동로또와_당첨번호_갯수파악() {
+        Lotto givenLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winLotto = new Lotto(List.of(1, 5, 6, 7, 8, 9));
+        int result = givenLotto.getMatchNumberCount(winLotto);
+        assertThat(result).isEqualTo(3);
+    }
 }
