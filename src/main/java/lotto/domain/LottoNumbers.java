dd898afa5c53fc234b876002455881c4a370854f
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:10:27 +0900

    FEAT : LottoNumbers 생성 - 1부터 45까지의 로또 리스트

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -1,2 +1,18 @@
-package lotto.domain;public class LottoNumbers {
+package lotto.domain;
+
+import lotto.constant.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoNumbers {
+    public static List<Integer> setUp() {
+        List<Integer> numbers = new ArrayList<>();
+
+        IntStream.range(Constant.MIN_LOTTO, Constant.MAX_LOTTO + 1)
+                .forEach(numbers::add);
+
+        return numbers;
+    }
 }
