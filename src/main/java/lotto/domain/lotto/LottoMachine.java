795ad36323c91f8f959434a56a8f22d493d81646
namhyeop <skaguq@gmail.com>
2024-03-25 01:10:16 +0900

    feat : 로또 수행 비동기 처리로 변경

--- src/main/java/lotto/domain/lotto/LottoMachine.java
+++ src/main/java/lotto/domain/lotto/LottoMachine.java
@@ -1,6 +1,8 @@
 package lotto.domain.lotto;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import lotto.domain.grade.Grade;
 import lotto.domain.winning.WinningNumbers;
 import lotto.domain.winning.WinningStatistic;
@@ -13,16 +15,29 @@ public class LottoMachine {
         this.purchaseAmount = purchaseAmount;
     }
 
-    public WinningStatistic startLotto(List<Lotto> autoLottos, WinningNumbers winningNumbers){
+    public WinningStatistic startLotto(List<Lotto> autoLottos, WinningNumbers winningNumbers)
+        throws ExecutionException, InterruptedException {
         WinningStatistic winningStatistic = new WinningStatistic(purchaseAmount);
 
-        for (Lotto autoLotto : autoLottos) {
-            Grade grade = calculatePrizeMoney(winningNumbers, autoLotto);
-            winningStatistic.calculateWinningStatistic(grade);
-        }
+        CompletableFuture<Void>[] futures = autoLottos.stream()
+            .map(autoLotto -> calculateGrade(winningNumbers, autoLotto)
+            .thenAccept(grade -> winningStatistic.calculateWinningStatistic(grade)))
+            .toArray(CompletableFuture[]::new);
+
+        waitLotto(futures);
 
         return winningStatistic;
     }
+
+    private void waitLotto(CompletableFuture<Void>[] futures)
+        throws InterruptedException, ExecutionException {
+        CompletableFuture.allOf(futures).get();
+    }
+
+    public CompletableFuture<Grade> calculateGrade(WinningNumbers winningNumbers, Lotto lotto) {
+        return CompletableFuture.supplyAsync(() -> winningNumbers.confirmWinning(lotto));
+    }
+
 
     public int calculateExecuteCount() {
         return purchaseAmount.getPurchaseAmount() / 1000;
