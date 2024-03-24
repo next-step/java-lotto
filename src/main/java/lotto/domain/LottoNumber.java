b20bd70a88aa32358f9c996df9c14c882a2bd8bd
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:09:59 +0900

    FEAT : LottoNumber 생성

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,2 +1,23 @@
-package lotto.domain;public class LottoNumber {
+package lotto.domain;
+
+import lotto.constant.Constant;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class LottoNumber {
+    private final Number number;
+
+    public LottoNumber(int number) {
+        this(new Number(number));
+    }
+
+    public LottoNumber(Number number) {
+        this.number = number;
+    }
+
+    public Number getLottoNumber() {
+        return this.number;
+    }
+
 }
