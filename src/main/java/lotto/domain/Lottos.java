da66ff45e528354ee9953672ef2533acf66df232
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:08:31 +0900

    FEAT : Lottos 생성

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,2 +1,18 @@
-package lotto.domain;public class Lottos {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return this.lottos;
+    }
+
 }
