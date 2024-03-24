4ab87bbdc6cbbcdc6241f702e4c36dd6f24e04aa
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:15:03 +0900

    FEAT : Revenue 생성 - 수익률 계산

--- src/main/java/lotto/domain/Revenue.java
+++ src/main/java/lotto/domain/Revenue.java
@@ -1,2 +1,20 @@
-package lotto.domain;public class Revenue {
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Revenue {
+    public double calculate(int money, int revenueTotal) {
+        return (double)revenueTotal / money;
+    }
+
+    public int getRevenueTotal(HashMap<Integer, Integer> matchResult) {
+        int total = 0;
+
+        for (int prize : matchResult.keySet()) {
+            total += prize * matchResult.get(prize);
+        }
+
+        return total;
+    }
 }
