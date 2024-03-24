ff273cca167dfb70caad4c1c2757e8c01c1de418
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-24 22:06:16 +0900

    FEAT : create LottoShop to purchase lotto and create lottos

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,2 +1,16 @@
-package lotto.domain;public class LottoShop {
+package lotto.domain;
+
+public class LottoShop {
+    private static final int LOTTO_PRICE = 1000;
+
+    public static Lottos purchase(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("구매 금액은 1000 단위로 입력해야 합니다.");
+        }
+        return LottoGenerator.lotteryTickets(purchaseCount(money));
+    }
+
+    public static int purchaseCount(int money) {
+        return money / LOTTO_PRICE;
+    }
 }
