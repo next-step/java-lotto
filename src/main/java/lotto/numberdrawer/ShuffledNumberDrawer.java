74f6c96b4c3879445545f1b37a72517a3bdbd59b
nimoh <spakers38@gmail.com>
2024-03-24 16:20:46 +0900

    style: [Step 4] 사용하지 않는 import 코드 정리

--- src/main/java/lotto/numberdrawer/ShuffledNumberDrawer.java
+++ src/main/java/lotto/numberdrawer/ShuffledNumberDrawer.java
@@ -3,7 +3,6 @@ package lotto.numberdrawer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ShuffledNumberDrawer implements NumberDrawer{
     @Override
