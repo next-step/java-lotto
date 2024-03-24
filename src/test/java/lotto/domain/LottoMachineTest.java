cbeba4fb79558944d41585c7be6ea14ae108af75
nimoh <spakers38@gmail.com>
2024-03-24 16:47:17 +0900

    style: [Step 4] 사용하지 않는 import 코드 제거

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
