8e1f5c01c976db2ad2a2e7990ad8244032639d2c
dongock <okdong23@naver.com>
2024-03-24 15:58:40 +0900

    refactor:[2-11] LottoNumbersTest에 compareTest 추가

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -1,26 +1,43 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LottoNumbersTest {
 
+    private LottoNumbers answerLottoNumber;
+
+    @BeforeEach
+    void setUp() {
+        answerLottoNumber = new LottoNumbers(Arrays.asList(1,2,3,4,5,6));
+    }
+
     @Test
-    @DisplayName("선택된 6개의 로또번호가 같아야 한다.")
+    @DisplayName("6개의 로또번호가 같으면 동등한 객체다.")
     void comparePickedLottoTest() {
-        // given
-        List<Integer> firstPickedLotto = Arrays.asList(1,2,3,4,5,6);
-        List<Integer> secondPickedLotto = Arrays.asList(6,5,4,3,2,1);
+        LottoNumbers selectLotto = new LottoNumbers(Arrays.asList(6,5,4,3,2,1));
 
-        // when
-        LottoNumbers firstLotto = new LottoNumbers(firstPickedLotto);
-        LottoNumbers secondLotto = new LottoNumbers(secondPickedLotto);
+        assertThat(answerLottoNumber).isEqualTo(selectLotto);
+    }
 
-        assertThat(firstLotto).isEqualTo(secondLotto);
+    @Test
+    @DisplayName("로또 번호 간 비교해서 전부 맞는지 반환된다.")
+    void compareTest_allMatched() {
+        LottoNumbers secondLotto = new LottoNumbers(Arrays.asList(6,5,4,3,2,1));
+
+        assertThat(answerLottoNumber.compare(secondLotto)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("로또 번호 간 비교해서 전부 틀린지 반환된다.")
+    void compareTest_notMatched() {
+        LottoNumbers secondLotto = new LottoNumbers(Arrays.asList(7,8,9,10,11,12));
+
+        assertThat(answerLottoNumber.compare(secondLotto)).isEqualTo(0);
     }
 }
