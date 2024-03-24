b5cb97bd2c942e967adad0743b7ffb8daa86e7e1
JJewon <eldertree777@gmail.com>
2024-03-24 20:50:58 +0900

    [feat] 로또를 발급하는 기능 및 테스트 구현

--- src/main/java/lotto/LottoOutlet.java
+++ src/main/java/lotto/LottoOutlet.java
@@ -1,9 +1,15 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoOutlet {
     private static final int LOTTO_PRICE = 1000;
 
     public static int lottoCount(int money) {
         return money / LOTTO_PRICE;
+    }
+
+    public static Lotto generateLotto() {
+        return new Lotto(List.of(1, 2, 3, 4, 5, 6));
     }
 }
