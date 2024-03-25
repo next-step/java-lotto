b7794b1b32cf083cea937d690036a4020b55e09c
namhyeop <skaguq@gmail.com>
2024-03-26 00:06:34 +0900

    feat : 가독성을 위한 개행 제거 (피드백)

--- src/main/java/lotto/domain/grade/Grade.java
+++ src/main/java/lotto/domain/grade/Grade.java
@@ -6,17 +6,11 @@ import lotto.error.exception.NotExistGradeException;
 public enum Grade {
 
     FIRST_GRADE(2_000_000_000, 6),
-
     SECOND_GRADE(1_500_000, 5),
-
     THIRD_GRADE(50_000, 4),
-
     FOUR_GRADE(5_000, 3),
-
     FIVE_GRADE(0, 2),
-
     SIX_GRADE(0, 1),
-
     SEVEN_GRADE(0, 0);
 
     private int prizeMoney;
