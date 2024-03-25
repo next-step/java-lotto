350139bc0bd9974bdbef6d8162e1f4d4fe27625f
kgi912 <rudrbdlf1123@gmail.com>
2024-03-25 23:42:25 +0900

    feat : 지난주 당첨 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import java.util.Scanner;
+import lotto.domain.Lotto;
 
 public class InputView {
 
@@ -9,5 +10,10 @@ public class InputView {
     public static int inputAmount(){
         System.out.println("로또 구입 금액을 입력해 주세요.");
         return scanner.nextInt();
+    }
+
+    public static String inputWinningLotto(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.nextLine();
     }
 }
