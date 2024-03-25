bdf59b0998582da5cf20800a0574cc8e09d08281
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-26 08:13:20 +0900

    REFACTOR: change purchase, purchaseCount method name to purchaseTicket, countPurchase

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -3,14 +3,14 @@ package lotto.domain;
 public class LottoShop {
     private static final int LOTTO_PRICE = 1000;
 
-    public static Lottos purchase(int money) {
+    public static Lottos purchaseTicket(int money) {
         if (money % 1000 != 0) {
             throw new IllegalArgumentException("구매 금액은 1000 단위로 입력해야 합니다.");
         }
-        return LottoGenerator.lotteryTickets(purchaseCount(money));
+        return LottoGenerator.lotteryTickets(countPurchase(money));
     }
 
-    public static int purchaseCount(int money) {
+    public static int countPurchase(int money) {
         return money / LOTTO_PRICE;
     }
 }
