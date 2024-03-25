eb686ce89995e24aa11470b93f85b72a1fe75f7f
sseung12 <pon02129@naver.com>
2024-03-25 18:47:32 +0900

    feat : 생성자 비니지스 로직을, 유틸클래스로 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,10 @@
 package lotto;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Lotto {
 
-    private static final List<Integer> LOTTO_NUMBERS = IntStream.range(1, 50).boxed().collect(Collectors.toList());
-    private static final int LOTTO_NUMBER_COUNT = 6;
     private final List<Integer> numbers;
-
-    public Lotto() {
-        Collections.shuffle(LOTTO_NUMBERS);
-        numbers = LOTTO_NUMBERS.stream().limit(LOTTO_NUMBER_COUNT).sorted().collect(Collectors.toList());
-    }
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
