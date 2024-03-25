947245998a18cec7a06ac643a60d57a7920d1630
dongock <okdong23@naver.com>
2024-03-25 23:57:06 +0900

    refactor:[3-4] LottoStore IntStream -> Stream (mapToObj -> map)

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -3,8 +3,10 @@ package lotto.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-import static lotto.domain.LottoNumber.*;
+import static lotto.domain.LottoNumber.END_INCLUSIVE;
+import static lotto.domain.LottoNumber.START_INCLUSIVE;
 
 public class LottoStore {
 
@@ -19,8 +21,8 @@ public class LottoStore {
     }
 
     public Lottos buy(Money money, LottoGenerateStrategy lottoPickStrategy) {
-        List<Lotto> lottos = IntStream.generate(() -> 0) // 무한 스트림 생성
-                .mapToObj(i -> lottoPickStrategy.generateLottoNumbers(this.lottoNumberPool)) // generateLottoNumbers() 메소드를 이용해 로또 번호 생성
+        List<Lotto> lottos = Stream.generate(() -> 0) // 무한 스트림 생성
+                .map(i -> lottoPickStrategy.generateLottoNumbers(this.lottoNumberPool)) // generateLottoNumbers() 메소드를 이용해 로또 번호 생성
                 .limit(money.getMoney() / PRICE) // money를 PRICE로 나눈 값만큼 번호를 생성하여 제한
                 .collect(Collectors.toList()); // 생성된 번호들을 리스트로 변환
 
