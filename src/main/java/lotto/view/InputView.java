098b760d05d341915a58f974122e6d95bc2c6d5d
jaeyounglee <ljyda214@gmail.com>
2024-03-25 22:45:39 +0900

    FEAT : 수동 로또 관련 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,10 +2,10 @@ package lotto.view;
 
 import lotto.*;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class InputView {
 
@@ -18,13 +18,24 @@ public class InputView {
 
     public static void inputBudget() {
         System.out.println("구입금액을 입력해 주세요.");
-        int input = scanner.nextInt();
-        Money budget = new Money(input);
-        long quantity = budget.purchase(lottoPrice);
+        int budgetInput = scanner.nextInt();
+        Money budget = new Money(budgetInput);
+        PurchaseLimit purchaseLimit = new PurchaseLimit(budget, lottoPrice);
 
-        System.out.println(quantity + "개를 구매했습니다.");
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        long manual = scanner.nextLong();
+        ManualCount manualCount = new ManualCount(manual, purchaseLimit);
 
-        LottoGame lottoGame = new LottoGame(budget, lottoPrice);
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        String[] manualNumbers = inputManualNumbers(manual);
+
+        ManualLottos manualLottos = new ManualLottos(manualNumbers);
+        AutoCount autoCount = purchaseLimit.decrease(manualCount);
+        AutoLottos autoLottos = new AutoLottos(autoCount);
+
+        System.out.println(String.format("수동으로 %s장, 자동으로 %s개를 구매했습니다.", manualCount, autoCount));
+        LottoGame lottoGame = autoLottos.merge(manualLottos);
+
         List<Lotto> lottos = lottoGame.getLottos();
         lottos.forEach(System.out::println);
 
@@ -32,7 +43,7 @@ public class InputView {
         String next = scanner.next();
         System.out.println("보너스 볼을 입력해 주세요.");
         int bonus = scanner.nextInt();
-        WiningLotto winningLotto = createWinningLotto(next, bonus);
+        WiningLotto winningLotto = new WiningLotto(next, bonus);
 
         RankMap countByRank = lottoGame.getPrizeByRank(winningLotto);
         StatisticView statisticView = new StatisticView();
@@ -44,11 +55,11 @@ public class InputView {
         System.out.println(String.format("총 수익률은 %s입니다.", ratio));
     }
 
-    private static WiningLotto createWinningLotto(String next, int bonus) {
-        return new WiningLotto(
-                Arrays.stream(next.split(","))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toSet()),
-                bonus);
+    private static String[] inputManualNumbers(long manual) {
+        Set<String> manualNumbers = new HashSet<>();
+        for (int i = 0; i < manual; i++) {
+            manualNumbers.add(scanner.next());
+        }
+        return (String[]) manualNumbers.toArray();
     }
 }
