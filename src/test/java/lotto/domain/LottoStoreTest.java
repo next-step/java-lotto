1a45798126fd03814d64e0ea7e89e5a63c92112e
dongock <okdong23@naver.com>
2024-03-25 20:42:22 +0900

    refactor:[3-1] 리팩토링으로 인한 LottoStoreTest 변경

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,8 +17,22 @@ class LottoStoreTest {
         LottoStore lottoStore = new LottoStore();
         Money money = new Money(1000);
 
-        PickedLottoNumbers pickedLottoNumbers = lottoStore.buy(money, new RandomLottoStrategy());
+        PickedLottoNumbers pickedLottoNumbers = lottoStore.buy(money, new TestLottoStrategy(lottoNumbers));
 
         assertThat(pickedLottoNumbers).containsExactly(lottoNumbers);
+    }
+
+    static class TestLottoStrategy implements LottoGenerateStrategy {
+        private final LottoNumbers lottoNumbers;
+
+        TestLottoStrategy(LottoNumbers lottoNumbers) {
+            this.lottoNumbers = lottoNumbers;
+        }
+
+
+        @Override
+        public LottoNumbers generateLottoNumbers(List<Integer> lottoNumberPool) {
+            return this.lottoNumbers;
+        }
     }
 }
