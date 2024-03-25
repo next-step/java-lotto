e56736dbaaf8c62e908b5513fe1a0f67799e5bb2
최지훈 <boy0516@naver.com>
2024-03-25 23:40:53 +0900

    test: 2등, 3등 테스트 반영

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,18 +1,25 @@
 package lotto.domain;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("도메인 Lottos 테스트")
 public class LottosTest {
 
-    @DisplayName("로또 번호 6자리 생성")
+    @DisplayName("당첨 통계 산출")
     @Test
-    void lottoGenerate() {
-        Lotto result = new Lotto();
+    void computeStatistic_three() {
+        Lotto givenLotto = Lotto.ofNumbers(List.of(1,2,3,4,5,6));
+        Lottos lottos = new Lottos(List.of(givenLotto));
+        WinLotto winLotto = WinLotto.ofNumbers(List.of(1, 2, 3, 4, 5, 7), 6);
 
-        assertThat(result.size()).isEqualTo(6);
+        Statistics result = lottos.computeStatistic(winLotto);
+
+        assertThat(result).isEqualTo(new Statistics(List.of(Statistic.SECOND)));
     }
 
 }
--- src/test/java/lotto/domain/StatisticsTest.java
+++ src/test/java/lotto/domain/StatisticsTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("도메인 Statistics 테스트")
 class StatisticsTest {
@@ -14,7 +13,7 @@ class StatisticsTest {
     @DisplayName("수익률 산출 테스트")
     @Test
     void getRateOfReturn() {
-        Statistics statistics = new Statistics(List.of(Statistic.THREE, Statistic.FOUR));
+        Statistics statistics = new Statistics(List.of(Statistic.FIFTH, Statistic.FOURTH));
         float result = statistics.getRateOfReturn(Lotto.LOTTO_PRICE);
 
         float expected = 27.5f;
