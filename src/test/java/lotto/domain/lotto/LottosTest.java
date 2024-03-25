f37724b14c5081508e5a4ce5d57224652e6704eb
hong yuseok <jericoh@naver.com>
2024-03-26 01:58:10 +0900

    test: Lottos.bind()에 대한 테스트 구현

--- src/test/java/lotto/domain/lotto/LottosTest.java
+++ src/test/java/lotto/domain/lotto/LottosTest.java
@@ -3,6 +3,7 @@ package lotto.domain.lotto;
 import lotto.domain.*;
 import lotto.domain.lotto.strategy.LottoGeneratingStrategy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -46,5 +47,15 @@ class LottosTest {
 
         assertThat(statisticsOfLottos.numberOfMatchCount(rank)).isEqualTo(1);
         assertThat(statisticsOfLottos.rateOfReturn()).isEqualTo(expectedRateOfReturn);
+    }
+
+    @Test
+    @DisplayName("bind(): Lottos 인스턴스 2개를 합하여 새로운 Lottos 인스턴스를 반환한다.")
+    void testBind() {
+        Lottos sizeOneLottos = lottoGeneratingStrategyStub.lottos(1, EMPTY_LOTTOS);
+        Lottos sizeTwoLottos = lottoGeneratingStrategyStub.lottos(2, EMPTY_LOTTOS);
+        Lottos sizeThreeLottos = sizeOneLottos.bind(sizeTwoLottos);
+
+        assertThat(sizeThreeLottos.lottos().size()).isEqualTo(3);
     }
 }
