8b8027720a512f6da7b85ae2ab998c1095e1dc87
dongock <okdong23@naver.com>
2024-03-25 20:27:46 +0900

    refactor:[3-1] Stream generate, limit를 이용한 리팩토링

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,7 +1,8 @@
 package lotto.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoStore {
     private static final Integer PRICE = 1000;
@@ -13,12 +14,11 @@ public class LottoStore {
     }
 
     public PickedLottoNumbers buy(Money money) {
-        List<LottoNumbers> pickedLottoNumbers = new ArrayList<>();
-        int totalCount = money.getMoney() / PRICE;
+        List<LottoNumbers> pickedLottoNumbers = IntStream.generate(() -> 0) // 무한 스트림 생성
+                .mapToObj(i -> lottoPickStrategy.generateLottoNumbers()) // generateLottoNumbers() 메소드를 이용해 로또 번호 생성
+                .limit(money.getMoney() / PRICE) // money를 PRICE로 나눈 값만큼 번호를 생성하여 제한
+                .collect(Collectors.toList()); // 생성된 번호들을 리스트로 변환
 
-        for (int count = 0; count < totalCount; count++) {
-            pickedLottoNumbers.add(lottoPickStrategy.generateLottoNumbers());
-        }
         return new PickedLottoNumbers(pickedLottoNumbers);
     }
 }
