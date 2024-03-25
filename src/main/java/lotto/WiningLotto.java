ad8f2879c5511bd853aac45870e47f54eb2569b7
jaeyounglee <ljyda214@gmail.com>
2024-03-25 22:44:07 +0900

    FEAT : 입력받은 String 을 통한 winningLotto 생성 로직을 이전

--- src/main/java/lotto/WiningLotto.java
+++ src/main/java/lotto/WiningLotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WiningLotto {
 
@@ -12,9 +14,20 @@ public class WiningLotto {
         this(new Lotto(numbers), new LottoNumber(bonus));
     }
 
+    public WiningLotto(String inputText, int bonus) {
+        this(createLotto(inputText.split(",")), new LottoNumber(bonus));
+    }
+
     public WiningLotto(Lotto winningLotto, LottoNumber bonus) {
         this.winningLotto = winningLotto;
         this.bonus = bonus;
+    }
+
+    private static Lotto createLotto(String[] numberArray) {
+        return new Lotto(Arrays.stream(numberArray)
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toSet()));
     }
 
     public MatchedCount countMatchedNumber(Lotto lotto) {
