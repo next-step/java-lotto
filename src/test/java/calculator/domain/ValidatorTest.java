d74bb0f8775657b289e4da256bdb252d868e417f
westkitekim <westkite418@gmail.com>
2024-03-25 23:36:56 +0900

    test: 연산자 적합여부 테스트코드 추가

--- src/test/java/calculator/domain/ValidatorTest.java
+++ src/test/java/calculator/domain/ValidatorTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("문자열 계산기 테스트")
 class ValidatorTest {
@@ -11,8 +12,16 @@ class ValidatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("입력값이 null이거나 빈 공백인 경우 예외를 던진다")
-    void test_validateInputNullAndEmpty(String input) {
+    void validateInputNullAndEmpty_null이나빈문자열(String input) {
         Assertions.assertThatThrownBy(() -> Validator.validateInputNullAndEmptyWithThrow(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"%", "^", "@"})
+    @DisplayName("입력값이 사칙연산이 아닌 경우 예외를 던진다")
+    void validateOperator_연산자_검증(String input) {
+        Assertions.assertThatThrownBy(() -> Validator.validateOperator(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
