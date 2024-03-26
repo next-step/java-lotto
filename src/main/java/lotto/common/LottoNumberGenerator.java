0f627025b942069ecac89b327e90e7bc35c03c67
jeonsanggi <jsg2861@gmail.com>
2024-03-26 20:30:10 +0900

    feature - IntStream.rangeClosed(1, 46); 적용

--- src/main/java/lotto/common/LottoNumberGenerator.java
+++ src/main/java/lotto/common/LottoNumberGenerator.java
@@ -3,11 +3,12 @@ package lotto.common;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumberGenerator {
 
-    private static final List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-        19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45);
+    private static final List<Integer> lottoNumbers = IntStream.rangeClosed(1, 46).boxed().collect(Collectors.toList());
     private static final int NUMBERS_SIZE = 6;
 
     private LottoNumberGenerator() {}
