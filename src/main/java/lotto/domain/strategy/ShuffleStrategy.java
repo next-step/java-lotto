71638d33b75c1f79b7428b8975268146fc1adc0f
yeeji-kwon <yeji.kwon@myrealtrip.com>
2024-03-26 13:56:59 +0900

    refactor: unmodifiableList로 수정 - 변경되면 안되는 값임을 나타내기

--- src/main/java/lotto/domain/strategy/ShuffleStrategy.java
+++ src/main/java/lotto/domain/strategy/ShuffleStrategy.java
@@ -4,19 +4,24 @@ import java.util.*;
 
 import static lotto.domain.LottoNo.LOTTO_MAX_NUMBER;
 import static lotto.domain.LottoNo.LOTTO_MIN_NUMBER;
+import static lotto.domain.LottoTicket.CORRECT_LOTTO_TICKET_SIZE;
 
 public class ShuffleStrategy implements LottoTicketCreateGenerator {
-  private static final List<Integer> POSSIBLE_LOTTO_NUMBER_CANDIDATES = new ArrayList<>();
+  private static final List<Integer> POSSIBLE_LOTTO_NUMBER_CANDIDATES;
+  private static final int START_INDEX = 0;
 
   static {
+    List<Integer> candidates = new ArrayList<>();
     for (int lottoNumber = LOTTO_MIN_NUMBER; lottoNumber <= LOTTO_MAX_NUMBER; lottoNumber++) {
-      POSSIBLE_LOTTO_NUMBER_CANDIDATES.add(lottoNumber);
+      candidates.add(lottoNumber);
     }
+    POSSIBLE_LOTTO_NUMBER_CANDIDATES = Collections.unmodifiableList(candidates);
   }
 
   @Override
   public Set<Integer> generate() {
-    Collections.shuffle(POSSIBLE_LOTTO_NUMBER_CANDIDATES);
-    return new HashSet<>(POSSIBLE_LOTTO_NUMBER_CANDIDATES.subList(0, 6));
+    List<Integer> candidates = new ArrayList<>(POSSIBLE_LOTTO_NUMBER_CANDIDATES);
+    Collections.shuffle(candidates);
+    return new HashSet<>(candidates.subList(START_INDEX, CORRECT_LOTTO_TICKET_SIZE));
   }
 }
