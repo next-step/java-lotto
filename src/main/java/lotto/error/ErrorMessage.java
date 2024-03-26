7e6c511213d7fd689e1bd08a2a6a6bfa7b7e4e1d
namhyeop <skaguq@gmail.com>
2024-03-27 02:03:21 +0900

    feat : ErrorMessage 클래스에서 사용자 정의 Exception 예외 메시지 제거, RunTime 예외에서 사용중인 메시지는 관리 함 (ex:IllegalStateException.class)

--- src/main/java/lotto/error/ErrorMessage.java
+++ src/main/java/lotto/error/ErrorMessage.java
@@ -2,14 +2,6 @@ package lotto.error;
 
 public enum ErrorMessage {
 
-    SIZE_EXCEED_LOTTO_NUMBER("로또 번호는 1부터 45의 범위만 가능합니다."),
-
-    SIZE_EXCEED_LOTTO("로또의 사이즈는 최대 6입니다."),
-
-    NOT_EXIST_GRADE("존재하지 않는 순위입니다."),
-
-    PURCHASE_AMOUNT_MOD("구매금액은 1000원 단위로 가능합니다."),
-
     INVALID_INPUT_TYPE_INT("입력값의 타입이 정수형입니다. 문자열을 입력해주세요 "),
 
     INVALID_INPUT_EMPTY("입력값이 없습니다. 당첨 번호를 다시 입력해주세요. "),
@@ -17,8 +9,6 @@ public enum ErrorMessage {
     ILLEGAL_STATE("비정상 경로로 접근하였습니다. 다시 실행해주세요. "),
 
     UN_EXPECTED_ERROR("예기치 못한 예외가 발생되었습니다. 다시 실행해주세요. "),
-
-    DUPLICATE_LOTTO_NUMBER("중복된 로또 번호가 존재합니다."),
 
     INVALID_FORMAT("당첨번호는 ', ' 로 구분되어야 합니다");
 
