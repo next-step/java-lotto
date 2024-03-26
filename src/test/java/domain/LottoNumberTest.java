82e8313823473de30cad44edbf16ba9a5958acd8
이승호 <seung7642@openbase.co.kr>
2024-03-26 12:53:46 +0900

    refactor: LottoNumber 객체 기능 요구사항 및 테스트 작성

--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -3,21 +3,13 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberTest {
 
-    @DisplayName("1~45 사이의 숫자를 가지고 LottoNumber 객체를 생성할 수 있다.")
+    @DisplayName("1~45 사이의 숫자로만 생성이 가능하다.")
     @Test
     void test01() {
-        LottoNumber lottoNumber = new LottoNumber(10);
-        assertThat(lottoNumber).isEqualTo(new LottoNumber(10));
-    }
-
-    @DisplayName("1~45 사이의 숫자가 아닌 경우 예외 발생")
-    @Test
-    void test02() {
         assertThatThrownBy(() -> new LottoNumber(46))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1~45 사이의 숫자만 가능합니다.");
