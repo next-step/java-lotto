f6480a09c556f4df5c662c03c7043f0e757cfb3d
DongWooE <ehddn977@naver.com>
2024-03-26 23:49:36 +0900

    feat : 수동으로 구매하는 번호들이 범위 체크 기능 추가

---
+++ src/main/java/lotto/domain/ManualLottoNumber.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class ManualLottoNumber {
+
+    private int number;
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+
+    public ManualLottoNumber(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    private void validateNumber(int number){
+        if(number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
