c43defd6b8e692f696637db3db7036f576f61492
DongWooE <ehddn977@naver.com>
2024-03-26 23:52:17 +0900

    refactor : ManualLotto 내의 원시값을 ManualLottoNumber로 수정

--- src/main/java/lotto/domain/ManualLotto.java
+++ src/main/java/lotto/domain/ManualLotto.java
@@ -1,14 +1,17 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ManualLotto {
 
-    private final List<Integer> numbers;
+    private final List<ManualLottoNumber> numbers;
 
     public ManualLotto(List<Integer> numbers) {
         validateNumbers(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(ManualLottoNumber::new)
+                .collect(Collectors.toList());
     }
 
     public void validateNumbers(List<Integer> numbers){
