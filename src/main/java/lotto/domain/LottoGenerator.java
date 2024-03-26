01708d303a0c8d22d9d65e3c4300389518916ef1
kylekim2123 <kyle2123@naver.com>
2024-03-26 21:37:59 +0900

    [STEP 2] feat: 주어진 개수만큼 로또를 생성하는 기능 추가

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,39 @@
+package lotto.domain;
+
+import static lotto.config.LottoExceptionMessage.LOTTO_QUANTITY_MUST_BE_POSITIVE;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoGenerator {
+
+    static final int MINIMUM_LOTTO_QUANTITY = 1;
+
+    private final LottoNumbersPicker lottoNumbersPicker;
+
+    public LottoGenerator(final LottoNumbersPicker lottoNumbersPicker) {
+        this.lottoNumbersPicker = lottoNumbersPicker;
+    }
+
+    public List<Lotto> generateLottos(final int quantity) {
+        validateQuantityIsMoreThanMinimum(quantity);
+
+        return IntStream.rangeClosed(1, quantity)
+                .mapToObj(i -> generate())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validateQuantityIsMoreThanMinimum(final int quantity) {
+        if (quantity < MINIMUM_LOTTO_QUANTITY) {
+            throw new IllegalArgumentException(LOTTO_QUANTITY_MUST_BE_POSITIVE.message(quantity));
+        }
+    }
+
+    private Lotto generate() {
+        final Set<LottoNumber> lottoNumbers = this.lottoNumbersPicker.pick();
+
+        return Lotto.from(lottoNumbers);
+    }
+}
