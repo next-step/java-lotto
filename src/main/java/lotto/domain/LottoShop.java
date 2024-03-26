04faf0d24f87a356a532356a4ce6ae2e53e2fc99
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-26 14:00:04 +0900

    FEAT : 로또 번호와 당첨 번호 일치 수와 상금을 인자로 받는 Rank enum 생성

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -4,7 +4,7 @@ public class LottoShop {
     private static final int LOTTO_PRICE = 1000;
 
     public static Lottos purchaseTicket(int money) {
-        if (money % 1000 != 0) {
+        if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구매 금액은 1000 단위로 입력해야 합니다.");
         }
         return LottoGenerator.lotteryTickets(countPurchase(money));
--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,2 +1,34 @@
-package lotto.domain;public enum Rank {
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NONE(0, 0);
+
+    private int matchCount;
+    private int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return this.matchCount;
+    }
+
+    public int getPrize() {
+        return this.prize;
+    }
+
+    public static Rank valueOf(int matchCount) {
+        return Arrays.stream(values())
+                    .filter((rank) -> rank.matchCount == matchCount)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("실패"));
+    }
 }
