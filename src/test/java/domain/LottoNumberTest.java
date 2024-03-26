8e8d083a1e4af08f1250a9bb0767c17b5d3473a3
이승호 <seung7642@gmail.com>
2024-03-26 23:08:21 +0900

    test: LottoNumber 객체의 random API 테스트.

--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -3,6 +3,9 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoNumberTest {
@@ -13,5 +16,18 @@ public class LottoNumberTest {
         assertThatThrownBy(() -> new LottoNumber(46))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1~45 사이의 숫자만 가능합니다.");
+    }
+
+    @DisplayName("1~45 사이 랜덤한 값을 구한다.")
+    @Test
+    void test02() {
+        LottoNumber random = LottoNumber.random(new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 44;
+            }
+        });
+
+        assertThat(random).isEqualTo(new LottoNumber(45));
     }
 }
