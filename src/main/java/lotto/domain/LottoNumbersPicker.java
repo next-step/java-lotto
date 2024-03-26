21c91a7ebd2907af8db8fde5ec618a7662ef5de6
kylekim2123 <kyle2123@naver.com>
2024-03-26 18:20:04 +0900

    [STEP 2] feat: 로또 번호 추출기 인터페이스 정의 및 난수 알고리즘을 바탕으로한 구현체 생성

---
+++ src/main/java/lotto/domain/LottoNumbersPicker.java
@@ -0,0 +1,8 @@
+package lotto.domain;
+
+import java.util.Set;
+
+public interface LottoNumbersPicker {
+
+    Set<LottoNumber> pick();
+}
---
+++ src/main/java/lotto/domain/RandomLottoNumbersPicker.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import static lotto.domain.Lotto.LOTTO_NUMBERS_SIZE;
+import static lotto.domain.LottoNumber.MAXIMUM_LOTTO_NUMBER;
+import static lotto.domain.LottoNumber.MINIMUM_LOTTO_NUMBER;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RandomLottoNumbersPicker implements LottoNumbersPicker {
+
+    private static final List<Integer> numbersRange = IntStream.rangeClosed(MINIMUM_LOTTO_NUMBER, MAXIMUM_LOTTO_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
+
+    @Override
+    public Set<LottoNumber> pick() {
+        Collections.shuffle(numbersRange);
+
+        return numbersRange.stream()
+                .limit(LOTTO_NUMBERS_SIZE)
+                .map(LottoNumber::from)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+}
