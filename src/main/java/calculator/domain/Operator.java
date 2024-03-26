642a808fe1ba85267d35d9df94b08c60dadd0d54
westkitekim <westkite418@gmail.com>
2024-03-27 02:01:18 +0900

    feat: 도메인 계산기능 구현

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -1,6 +1,7 @@
 package calculator.domain;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
 
 public enum Operator {
@@ -21,7 +22,15 @@ public enum Operator {
         return operator;
     }
 
-    public static Integer calculate(String operator, Integer number1, Integer number2) {
+    public static Integer calculate(List<Integer> operands, List<String> operators) {
+        int sum = operands.get(0);
+        for (int i = 0; i < operators.size(); i++) {
+            sum = Operator.calculateSingle(operators.get(i), sum, operands.get(i + 1));
+        }
+
+        return sum;
+    }
+    public static Integer calculateSingle(String operator, Integer number1, Integer number2) {
         return getOperator(operator).calculator.apply(number1, number2);
     }
 
