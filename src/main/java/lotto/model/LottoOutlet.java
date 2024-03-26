567702f0c4b78ed068ed89e0a1a88ec2ac2247f3
JJewon <eldertree777@gmail.com>
2024-03-26 20:33:57 +0900

    [refactor] 로또 구매가 1장 이상 불가능시 예외 추가

--- src/main/java/lotto/model/LottoOutlet.java
+++ src/main/java/lotto/model/LottoOutlet.java
@@ -6,10 +6,23 @@ import java.util.stream.Stream;
 
 public class LottoOutlet {
     private static final int LOTTO_PRICE = 1000;
+    private static final String LACK_MONEY_MESSAGE = "해당 금액으로는 1장도 구매하실 수 없습니다.";
     private static final NumbersGenerator numbersGenerator = new RandomNumbersGeneratorImpl();
 
     public static int lottoCount(int money) {
+        int lottoCount = calculateLottoCount(money);
+        validateLottoCount(lottoCount);
+        return lottoCount;
+    }
+
+    private static int calculateLottoCount(int money) {
         return money / LOTTO_PRICE;
+    }
+
+    private static void validateLottoCount(int lottoCount) {
+        if (lottoCount < 1) {
+            throw new IllegalArgumentException(LACK_MONEY_MESSAGE);
+        }
     }
 
     public static Lotto generateLotto() {
