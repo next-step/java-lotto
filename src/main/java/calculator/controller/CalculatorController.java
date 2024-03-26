a37fb025191ada02a178d50fdd487f9f295810b0
westkitekim <westkite418@gmail.com>
2024-03-27 02:01:39 +0900

    feat: 컨트롤러 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,18 @@
 package calculator.controller;
 
+import calculator.domain.Operator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+import java.util.List;
+
 public class CalculatorController {
+    public static void main(String[] args) {
+        String inputFormula = InputView.inputFormula();
+        List<Integer> operands = InputView.parseOperands(inputFormula);
+        List<String> operators = InputView.parseOperators(inputFormula);
+
+        int totalSum = Operator.calculate(operands, operators);
+        OutputView.printResult(totalSum);
+    }
 }
