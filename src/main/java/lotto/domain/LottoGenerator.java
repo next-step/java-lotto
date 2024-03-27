298ab851a772ec81722bfac76d6bed6056bbfbb6
park-cheonho <cheonho08@naver.com>
2024-03-27 17:46:04 +0900

    refactor: step2-feedback LottoGenerator 로또 재사용
    refactor: step2-feedback LottoGenerator 로또 재사용

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -11,6 +11,15 @@ public final class LottoGenerator {
 
     private static final int START_NUMBER = 1;
     private static final int LAST_NUMBER = 45;
+    private static final List<Integer> lottoGame = createLottoGame();
+
+    private static List<Integer> createLottoGame() {
+        List<Integer> lottoGame = new ArrayList<>();
+        for (int i = START_NUMBER; i <= LAST_NUMBER; i++) {
+            lottoGame.add(i);
+        }
+        return lottoGame;
+    }
 
     public static Lottos generateLottos(Fee fee) {
         Lottos lottos = new Lottos();
@@ -21,18 +30,10 @@ public final class LottoGenerator {
     }
 
     public static Lotto generateLotto() {
-        List<Integer> lottoGame = rangeOfLotto();
-        Collections.shuffle(lottoGame);
-        lottoGame = lottoGame.subList(0,6);
-        Collections.sort(lottoGame);
-        return new Lotto(lottoGame);
-    }
-
-    private static List<Integer> rangeOfLotto() {
-        List<Integer> lottoGame = new ArrayList<>();
-        for (int i = START_NUMBER; i <= LAST_NUMBER; i++) {
-            lottoGame.add(i);
-        }
-        return lottoGame;
+        List<Integer> clonedLottoGame = new ArrayList<>(lottoGame);
+        Collections.shuffle(clonedLottoGame);
+        clonedLottoGame = clonedLottoGame.subList(0,6);
+        Collections.sort(clonedLottoGame);
+        return new Lotto(clonedLottoGame);
     }
 }
