2e8c7a7ecf9682c36260fa56acea5da70c7922bb
namhyeop <skaguq@gmail.com>
2024-03-27 22:33:28 +0900

    feat : 상수 클래스 이름 수정 (Step2 피드백 반영)

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -1,9 +1,9 @@
 package lotto.utils;
 
-public class constants {
+public class Constants {
 
     public static final int LOTTO_TICKET_PRICE = 1000;
 
-    private constants() {
+    private Constants() {
     }
 }
