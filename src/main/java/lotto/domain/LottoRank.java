d3cb918cece8aa2473fcc5408c5462419f0a0091
jeonsanggi <jsg2861@gmail.com>
2024-03-27 22:52:01 +0900

    feature - 보너스볼 검증 로직 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public enum LottoRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 1_500_000),
-    THIRD(4, 50_000),
-    FOURTH(3, 5_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NO_MATCH(0, 0);
 
     private int matchCount;
@@ -25,10 +26,18 @@ public enum LottoRank {
         return matchCount;
     }
 
-    public static LottoRank getLottoRank(int matchCount) {
-        return Arrays.stream(values())
+    public static LottoRank getLottoRank(int matchCount, boolean matchBonus) {
+        LottoRank resultRank = Arrays.stream(values())
             .filter(lottoRank -> lottoRank.matchCount == matchCount)
             .findFirst()
             .orElseGet(() -> LottoRank.NO_MATCH);
+
+        if (resultRank.matchCount == LottoRank.SECOND.matchCount && matchBonus) {
+            return LottoRank.SECOND;
+        } else if (resultRank.matchCount == LottoRank.THIRD.matchCount) {
+            return LottoRank.THIRD;
+        }
+
+        return resultRank;
     }
 }
