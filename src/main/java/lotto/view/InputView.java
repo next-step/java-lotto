280311e8bb489cacca53b65619045baacb2c1bad
kgi912 <rudrbdlf1123@gmail.com>
2024-03-27 22:16:28 +0900

    refactor : inputAmount 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class InputView {
 
-    static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputAmount(){
         System.out.println("로또 구입 금액을 입력해 주세요.");
-        return scanner.nextInt();
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static String inputWinningLotto(){
