293944888c0c88262648f56574ec2b4b6aa4bf29
namhyeop <skaguq@gmail.com>
2024-03-28 01:12:52 +0900

    feat : 로또 머신 테스트 코드 수정 및 보너스 등급 추가로인한 테스트 코드 추가

--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ExecutionException;
 import lotto.domain.grade.Grade;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.LottoMachine;
+import lotto.domain.lotto.LottoNumber;
 import lotto.domain.lotto.PurchaseAmount;
 import lotto.domain.winning.WinningNumbers;
 import lotto.domain.winning.WinningStatistic;
@@ -35,7 +36,7 @@ class LottoMachineTest {
     public void LottoMachine_Should_Compare_Lotto_With_WinningNumbers_To_Check_Equals_All() {
         //given
         LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(1000));
-        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6), LottoNumber.ONE);
         Lotto lotto = Lotto.fromValues(List.of(1, 2, 3, 4, 5, 6));
 
         //when
@@ -46,11 +47,26 @@ class LottoMachineTest {
     }
 
     @Test
+    @DisplayName("[보너스 당첨] WinningNumbers를 사용하여 Lotto 번호가 WinningNumbers와 5개와 같으며 보너스 번호를 맞춘 경우를 확인한다")
+    public void LottoMachine_Should_Compare_Lotto_With_WinningNumbers_To_Check_Equals_Five_And_Bonus_Number() {
+        //given
+        LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(1000));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6), LottoNumber.FORTY_FIVE);
+        Lotto lotto = Lotto.fromValues(List.of(1, 2, 3, 4, 5, 45));
+
+        //when
+        Grade grade = lottoMachine.calculatePrizeMoney(winningNumbers, lotto);
+
+        //then
+        assertThat(grade).isEqualTo(Grade.BONUS_GRADE);
+    }
+
+    @Test
     @DisplayName("[2등 당첨] WinningNumbers를 사용하여 Lotto 번호가 WinningNumbers와 5개와 같을 경우 당첨 등수를 확인한다")
     public void LottoMachine_Should_Compare_Lotto_With_WinningNumbers_To_Check_Equals_Five() {
         //given
         LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(1000));
-        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6), LottoNumber.FORTY_FIVE);
         Lotto lotto = Lotto.fromValues(List.of(1, 2, 3, 4, 5, 10));
 
         //when
@@ -65,7 +81,7 @@ class LottoMachineTest {
     public void LottoMachine_Should_Compare_Lotto_With_WinningNumbers_To_Check_Equals_Four() {
         //given
         LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(1000));
-        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6), LottoNumber.ONE);
         Lotto lotto = Lotto.fromValues(List.of(1, 2, 3, 4, 11, 10));
 
         //when
@@ -80,7 +96,7 @@ class LottoMachineTest {
     public void LottoMachine_Should_Compare_Lotto_With_WinningNumbers_To_Check_Equals_Three() {
         //given
         LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(1000));
-        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6), LottoNumber.ONE);
         Lotto lotto = Lotto.fromValues(List.of(1, 2, 3, 12, 11, 10));
 
         //when
@@ -95,7 +111,7 @@ class LottoMachineTest {
     public void LottoMachine_Should_Compare_Lotto_With_WinningNumbers_To_Check_Equals_two() {
         //given
         LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(1000));
-        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(List.of(1, 2, 3, 4, 5, 6), LottoNumber.ONE);
         Lotto lotto = Lotto.fromValues(List.of(11, 12, 13, 14, 15, 16));
 
         //when
@@ -110,14 +126,15 @@ class LottoMachineTest {
     public void LottoMachine_Should_Return_Winning_Statistics_Using_Auto_Lottos_and_Winning_Number()
         throws ExecutionException, InterruptedException {
         //given
-        Lotto firstGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 5, 6));     //  1등 로또
-        Lotto secondGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 5, 10));   //  2등 로또
-        Lotto thirdGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 20, 10));   //  3등 로또
-        Lotto fourGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 30, 20, 10));   //  4등 로또
+        Lotto firstGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 5, 6));     //  1등 당첨 로또
+        Lotto bonusGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 5, 45));     //  보너스 당첨 로또
+        Lotto secondGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 5, 10));   //  2등 당첨 로또
+        Lotto thirdGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 4, 20, 10));   //  3등 당첨 로또
+        Lotto fourGradeLotto = Lotto.fromValues(Arrays.asList(1, 2, 3, 30, 20, 10));   //  4등 당첨 로또
         List<Lotto> autoLottos = new ArrayList<>(
-            Arrays.asList(firstGradeLotto, secondGradeLotto, thirdGradeLotto, fourGradeLotto));
+            Arrays.asList(firstGradeLotto, secondGradeLotto, thirdGradeLotto, fourGradeLotto, bonusGradeLotto));
 
-        WinningNumbers winningNumbers = WinningNumbers.fromValues(Arrays.asList(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = WinningNumbers.fromValues(Arrays.asList(1, 2, 3, 4, 5, 6), LottoNumber.FORTY_FIVE);
 
         LottoMachine lottoMachine = new LottoMachine(new PurchaseAmount(20000));
 
@@ -126,6 +143,7 @@ class LottoMachineTest {
 
         //then
         assertThat(winningStatistic.getGradeCount(Grade.FIRST_GRADE)).isEqualTo(1);
+        assertThat(winningStatistic.getGradeCount(Grade.BONUS_GRADE)).isEqualTo(1);
         assertThat(winningStatistic.getGradeCount(Grade.SECOND_GRADE)).isEqualTo(1);
         assertThat(winningStatistic.getGradeCount(Grade.THIRD_GRADE)).isEqualTo(1);
         assertThat(winningStatistic.getGradeCount(Grade.FOUR_GRADE)).isEqualTo(1);
