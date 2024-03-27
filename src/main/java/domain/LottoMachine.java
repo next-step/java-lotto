dfa0335d2d19f46230fef9b5040ed4a42e867041
NaayoungKwon <kny8092@gmail.com>
2024-03-28 01:28:03 +0900

    refactor: lotto result 내부에서 결과 계산하도록 리팩토링

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -23,24 +23,11 @@ public class LottoMachine {
 
   public LottoResult getLottoResult(WinNumbers winningNumbers) {
     Map<Integer, Integer> result = lottoList.getLottoResult(winningNumbers);
-    return new LottoResult(findWinningPrizeMap(result), getProfitRate(result));
+    return new LottoResult(result, lottoList.getLottoCount() * lottoPrice);
   }
 
   private int getBuyableLottoCount(int totalMoney) {
     return totalMoney / lottoPrice;
   }
 
-  private Map<Integer, Integer> findWinningPrizeMap(Map<Integer, Integer> result) {
-    Map<Integer, Integer> prizeMap = new HashMap<>(4);
-    IntStream.range(3,7).forEach(count -> prizeMap.put(count, result.getOrDefault(count, 0)));
-    return prizeMap;
-  }
-
-  private double getProfitRate(Map<Integer, Integer> result) {
-    AtomicInteger totalPrize = new AtomicInteger();
-    result.entrySet().stream()
-        .filter(e -> e.getKey() >= 3)
-        .forEach(entry -> totalPrize.addAndGet(Prize.findPrizeMoneyByCount(entry.getKey()) * entry.getValue()));
-    return new BigDecimal(totalPrize.get()).divide(new BigDecimal(lottoList.getLottoCount() * lottoPrice)).doubleValue();
-  }
 }
--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,10 +1,19 @@
 package domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class LottoResult {
     private Map<Integer, Integer> matchedResult;
     private Double profitRate;
+
+    public LottoResult(Map<Integer,Integer> lottoResult, int totalMoney){
+        this(findWinningPrizeMap(lottoResult), calculateProfitRate(lottoResult, totalMoney));
+    }
 
     public LottoResult(Map<Integer, Integer> matchedResult, Double profit){
         this.matchedResult = matchedResult;
@@ -17,5 +26,19 @@ public class LottoResult {
 
     public Double getProfitRate(){
         return this.profitRate;
+    }
+
+    private static Map<Integer, Integer> findWinningPrizeMap(Map<Integer, Integer> result) {
+        Map<Integer, Integer> prizeMap = new HashMap<>(4);
+        IntStream.range(3,7).forEach(count -> prizeMap.put(count, result.getOrDefault(count, 0)));
+        return prizeMap;
+    }
+
+    private static double calculateProfitRate(Map<Integer, Integer> result, int totalMoney) {
+        AtomicInteger totalPrize = new AtomicInteger();
+        result.entrySet().stream()
+                .filter(e -> e.getKey() >= 3)
+                .forEach(entry -> totalPrize.addAndGet(Prize.findPrizeMoneyByCount(entry.getKey()) * entry.getValue()));
+        return new BigDecimal(totalPrize.get()).divide(new BigDecimal(totalMoney), 2, RoundingMode.HALF_UP).doubleValue();
     }
 }
