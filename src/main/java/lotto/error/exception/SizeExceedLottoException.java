cf653fe909b0dffc3a5f93fe853a9cd3c26b7628
namhyeop <skaguq@gmail.com>
2024-03-27 22:34:44 +0900

    feat : 도메인과 어울리지 않는 부자연스러운 에러 메시지명 수정(피드백 반영)

--- src/main/java/lotto/error/exception/SizeExceedLottoException.java
+++ src/main/java/lotto/error/exception/SizeExceedLottoException.java
@@ -6,7 +6,7 @@ import lotto.error.ErrorMessage;
 public class SizeExceedLottoException extends RuntimeException {
 
     public SizeExceedLottoException(Object parameter) {
-        super(MessageFormat.format("{0} 입력값: {1}", "로또의 사이즈는 최대 6입니다.",
+        super(MessageFormat.format("{0} 입력값: {1}", "로또 개수는 6개여야 합니다.",
             parameter));
     }
 
