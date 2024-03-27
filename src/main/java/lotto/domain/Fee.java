97480df96ddf4d6b0070f807fc09be4a9d62eb1e
park-cheonho <cheonho08@naver.com>
2024-03-27 16:20:23 +0900

    refactor: step2-feedback Fee 입력값 검증 로직
    refactor: step2-feedback Fee 입력값 검증 로직

--- src/main/java/lotto/domain/Fee.java
+++ src/main/java/lotto/domain/Fee.java
@@ -9,7 +9,14 @@ public class Fee {
     private int value;
 
     public Fee(int fee) {
+        validate(fee);
         this.value = fee;
+    }
+
+    private void validate(int fee) {
+        if (fee < PRICE_PER_GAME) {
+            throw new RuntimeException("한 게임당 1000원 입니다. 입력 금액을 확인 해주세요.");
+        }
     }
 
     public int convertToGameUnit() {
