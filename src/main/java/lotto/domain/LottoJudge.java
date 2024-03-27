66b434bfb3c830157af3329495c2d5885ddfbc2d
kylekim2123 <kyle2123@naver.com>
2024-03-27 16:44:55 +0900

    [STEP 2] feat: 당첨 번호와 로또 번호를 비교하여 순위를 부여하는 기능 추가

---
+++ src/main/java/lotto/domain/LottoJudge.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LottoJudge {
+
+    private final Lotto winningLotto;
+
+    public LottoJudge(final Lotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+    public Map<LottoRank, Long> judge(final List<Lotto> lottos) {
+        return lottos.stream()
+                .collect(Collectors.groupingBy(this::match, Collectors.counting()));
+    }
+
+    private LottoRank match(final Lotto lotto) {
+        final int commonNumbersCount = lotto.countCommonNumbers(this.winningLotto);
+
+        return LottoRank.from(commonNumbersCount);
+    }
+}
