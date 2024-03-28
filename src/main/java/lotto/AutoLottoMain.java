8fc4b13e9f1dbd421b2f656d9e300b1cc4feb655
pado0 <bgshhd95@gmail.com>
2024-03-28 21:29:18 +0900

    refaacr:[step2] 수익을 책임지는 객체를 만든다.

--- src/main/java/lotto/AutoLottoMain.java
+++ src/main/java/lotto/AutoLottoMain.java
@@ -19,14 +19,7 @@ public class AutoLottoMain {
         IssuedLottos issuedLottos = new IssuedLottos(lottos);
         Map<WinningType, Long> winningStatic = issuedLottos.winningStatistic(winNumbers);
         OutputView.printWinningStatic(winningStatic);
-        OutputView.printReturnRate(returnRate(winningStatic, amount));
-    }
-
-    private static double returnRate(Map<WinningType, Long> winningStatistic, int amount) {
-        double revenue =  winningStatistic.entrySet()
-                .stream()
-                .mapToLong(entry -> entry.getKey().getRevenue() * entry.getValue())
-                .reduce(0, Long::sum);
-        return revenue / amount;
+        Revenue revenue = new Revenue(winningStatic);
+        OutputView.printReturnRate(revenue.returnRate(amount));
     }
 }
