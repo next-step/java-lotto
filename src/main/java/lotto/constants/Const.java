8e87f31895f0867d73dfe10e67d229f37191d888
Duyan Kim <devduyan@gmail.com>
2024-03-28 22:21:40 +0900

    feat: 로또 가격 상수화

--- src/main/java/lotto/constants/Const.java
+++ src/main/java/lotto/constants/Const.java
@@ -7,6 +7,12 @@ import java.util.stream.IntStream;
 public class Const {
 
     public static final String GET_TRY_NO = "구입금액을 입력해 주세요.";
+    public static final String GET_WINNING_NO  = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String RESULT = "당첨 통계";
+
+
+    public static final int LOTTO_COST = 1000;
+
     public static final List<Integer> NUMBER_LIST = IntStream.rangeClosed(1, 45)
             .boxed().collect(Collectors.toList());
 }
