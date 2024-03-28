f1a9b458767bc734e7f7e24a3a475cd32aafe4d1
namhyeop <skaguq@gmail.com>
2024-03-28 23:16:01 +0900

    fix : 상수 오등록 수정

--- src/main/java/lotto/utils/Constants.java
+++ src/main/java/lotto/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final int LOTTO_TICKET_PRICE = 1000;
 
-    public static final int LOTTO_SIZE = 1000;
+    public static final int LOTTO_SIZE = 6;
 
     private Constants() {
     }
