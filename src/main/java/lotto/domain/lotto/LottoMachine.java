39c1ca177381d346a4a2c6644bbb6b14ac741271
namhyeop <skaguq@gmail.com>
2024-03-29 02:24:59 +0900

    feat : PurchaseInfomation 객체 수정으로 인한 LottiMachine 코드 수정

--- src/main/java/lotto/domain/lotto/LottoMachine.java
+++ src/main/java/lotto/domain/lotto/LottoMachine.java
@@ -11,15 +11,15 @@ import lotto.domain.winning.WinningStatistic;
 
 public class LottoMachine {
 
-    private final PurchaseAmount purchaseAmount;
+    private final PurchaseInfomation purchaseInfomation;
 
-    public LottoMachine(PurchaseAmount purchaseAmount) {
-        this.purchaseAmount = purchaseAmount;
+    public LottoMachine(PurchaseInfomation purchaseInfomation) {
+        this.purchaseInfomation = purchaseInfomation;
     }
 
     public WinningStatistic startLotto(List<Lotto> autoLottos, WinningNumbers winningNumbers)
         throws ExecutionException, InterruptedException {
-        WinningStatistic winningStatistic = new WinningStatistic(purchaseAmount);
+        WinningStatistic winningStatistic = new WinningStatistic(purchaseInfomation);
 
         CompletableFuture<Void>[] futures = autoLottos.stream()
             .map(autoLotto -> calculateGrade(winningNumbers, autoLotto)
@@ -42,7 +42,7 @@ public class LottoMachine {
 
 
     public int calculateExecuteCount() {
-        return purchaseAmount.getPurchaseAmount() / LOTTO_TICKET_PRICE;
+        return purchaseInfomation.getPurchaseAmount() / LOTTO_TICKET_PRICE;
     }
 
     public Grade calculatePrizeMoney(final WinningNumbers winningNumbers, final Lotto lotto) {
