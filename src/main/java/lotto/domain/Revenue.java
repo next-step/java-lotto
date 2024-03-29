2887293e4652ceb82c5ad859124968e2de980032
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:21:43 +0900

    FEAT : 매치 결과를 바탕으로 수익을 계산하는 Revenue 생성

--- src/main/java/lotto/domain/Revenue.java
+++ src/main/java/lotto/domain/Revenue.java
@@ -1,2 +1,20 @@
-package lotto;public class Revenue {
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Revenue {
+    public static double calculateRevenue(int money, long revenueTotal) {
+        return (double) revenueTotal / money;
+    }
+
+    public static long revenueTotal(HashMap<Rank, Integer> result) {
+        long total = 0;
+
+        for (Rank rank : result.keySet()) {
+            total += (long) result.get(rank) * rank.getPrize();
+        }
+
+        return total;
+    }
 }
