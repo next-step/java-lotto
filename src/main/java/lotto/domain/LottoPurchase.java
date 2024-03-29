229fa39f09182149501605c078b151dabb93d4ef
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:20:05 +0900

    FEAT : 구매 금액 입력 시 로또 개수 반환, 로또 구매 금액 입력 단위 체크

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -1,12 +1,12 @@
-package lotto;
+package lotto.domain;
+
+import lotto.constant.Constants;
 
 public class LottoPurchase {
-    private static final int LOTTO_PRICE = 1000;
-
     public static int purchase(int money) {
-        if (money % LOTTO_PRICE != 0) {
+        if (money % Constants.LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해주세요.");
         }
-        return money / LOTTO_PRICE;
+        return money / Constants.LOTTO_PRICE;
     }
 }
