4a2d3a1d4a5c9c714b2659dc29fa09fee3fa116a
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:16:50 +0900

    FEAT : 로또 번호 셔플, lotto 및 lottos 생성

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,2 +1,37 @@
-package lotto.domain;public class LottoGenerator {
+package lotto.domain;
+
+import lotto.constant.Constants;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoGenerator {
+    public static Lottos createLottos(int count) {
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lottoList.add(createLotto(shuffleLotto()));
+        }
+
+        return new Lottos(lottoList);
+    }
+
+    public static Lotto createLotto(List<Integer> shuffledLotto) {
+        Collections.sort(shuffledLotto);
+        return new Lotto(shuffledLotto);
+    }
+
+    public static List<Integer> shuffleLotto() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+
+        for (int number = Constants.MIN_LOTTO; number <= Constants.MAX_LOTTO; number++) {
+            lottoNumbers.add(number);
+        }
+
+        Collections.shuffle(lottoNumbers);
+
+        return lottoNumbers.subList(0, Constants.LOTTO_SIZE);
+    }
+
 }
