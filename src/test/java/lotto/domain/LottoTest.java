84f9456d53da69c41e3bad11cf5c77dd19ad41e7
pado0 <bgshhd95@gmail.com>
2024-03-30 00:09:18 +0900

    feat:[step] testcode

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -7,14 +7,34 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class LottoTest {
 
     @DisplayName("로또에 숫자가 몇 개 포함되었는지 카운트한다.")
     @Test
-    void compareCount() throws Exception {
+    void compareCount() {
         List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
-        List<Integer> other = new ArrayList<>(List.of(1, 2, 3, 7, 7, 7));
+        List<Integer> other = new ArrayList<>(List.of(1, 2, 3, 10, 11, 12));
         Lotto lotto = new Lotto(numbers);
         Assertions.assertThat(lotto.findWinningType(other, 0)).isEqualTo(WinningType.HIT_COUNT_3);
+    }
+
+    @DisplayName("로또가 7자리 이상이면 오류이다.")
+    @Test
+    void lengthExceedException() {
+
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 6, 7, 8));
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또에 중복되는 숫자가 있으면 오류이다.")
+    @Test
+    void duplicatedExceltion() {
+
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 6, 7, 7));
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
