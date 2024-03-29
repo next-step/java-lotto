862644fa9b709c6a169f04e81783405eec7bee7a
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:13:51 +0900

    FEAT : Lotto 컨트롤러 생성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,28 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.domain.LottoNumber;
+import lotto.domain.*;
+import lotto.utils.StringUtils;
+import view.InputView;
+import view.ResultView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class LottoController {
+    public static void startLotto() {
+        int money = InputView.inputMoney();
+        Lottos lottos = LottoGenerator.createLottos(LottoPurchase.purchase(money));
+        ResultView.printLotto(lottos);
+
+        List<Integer> winNumbers = StringUtils.parseWinNumbers(InputView.inputWinNumbers());
+        int bonus = InputView.inputBonus();
+
+        for (Lotto lotto : lottos.getLottos()) {
+            LottoMatchResult.putMatchResult(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonus)));
+        }
+
+        HashMap<Rank, Integer> matchResult = LottoMatchResult.getMatchResult();
+        ResultView.printResult(matchResult, Revenue.calculateRevenue(money, Revenue.revenueTotal(matchResult)));
+    }
 }
