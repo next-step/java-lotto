c74755e7dc3706f8affa4bd9bbcf716396d80a65
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:22:48 +0900

    FEAT : split과 문자열 배열을 정수 배열로 변경하는 StringUtils 생성

--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -1,19 +1,19 @@
-package lotto;
+package lotto.utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StringUtils {
-    public static String[] splitTextWinNumbers(String input) {
-        return input.split(",");
-    }
-
-    public static List<Integer> parseWinNumbers(String[] textWinNumbers) {
+    public static List<Integer> parseWinNumbers(String input) {
         List<Integer> winNumbers = new ArrayList<>();
-        for (String text : textWinNumbers) {
+        for (String text : splitTextWinNumbers(input)) {
             winNumbers.add(parseTextToInt(text));
         }
         return winNumbers;
+    }
+
+    public static String[] splitTextWinNumbers(String input) {
+        return input.split(",");
     }
 
     private static Integer parseTextToInt(String text) {
