dc65cba6f47b53f80daebddf4d3ebbd79baa5bfd
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:20:25 +0900

    FEAT : 여러 개의 로또들을 관리하는 Lottos 생성

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,2 +1,29 @@
-package lotto.domain;public class Lottos {
+package lotto.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Lottos {
+    private List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return this.lottos;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Lottos)) return false;
+        Lottos lottos1 = (Lottos) object;
+        return Objects.equals(lottos, lottos1.lottos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottos);
+    }
 }
