99da5d2fde1b1d2dd49703e05cef5c10f3630e9d
hhhhhwi <aab555586@gmail.com>
2024-03-30 02:37:51 +0900

    feat: 로또의 결과값을 구하는 LottoResult 객체 구현 - 일치 개수에 따른 당첨 금액 매칭의 책임을 갖는 Prize 구현

---
+++ src/main/java/lotto/LottoResult.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResult {
+	private Map<Long, Integer> matchedMap;
+	private double rate;
+
+	public LottoResult(WinningNumbers winningNumbers, List<Lotto> lottos, int purchasePrice) {
+		this.matchedMap = new HashMap<>();
+
+		lottos.stream()
+				.map(winningNumbers::getMatchedCount)
+				.filter(count -> count >= Prize.getMinimumCount())
+				.forEach(count -> matchedMap.put(count, matchedMap.getOrDefault(count, 0) + 1));
+
+		this.rate = calculateRate(purchasePrice);
+	}
+
+	private double calculateRate(int purchasePrice) {
+		int prize = matchedMap.keySet().stream()
+				.mapToInt(key -> Prize.findPriceByCount(key) * matchedMap.get(key))
+				.sum();
+
+		return (double) prize / purchasePrice;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public int getNumberOfMatchedLotto(Long matchedCount) {
+		return matchedMap.getOrDefault(matchedCount, 0);
+	}
+}
