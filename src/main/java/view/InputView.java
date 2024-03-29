768749ab545466edc1f25dc681068b02f938d9fd
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-29 18:23:26 +0900

    FEAT : 구매 금액, 당첨번호, 보너스 번호를 입력하는 InputView 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,23 @@
-package view;public class InputView {
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int inputMoney() {
+        System.out.println("구매 금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public static String inputWinNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+
+    public static int inputBonus() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return scanner.nextInt();
+    }
 }
