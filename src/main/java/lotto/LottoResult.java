329665885a88e67e4f7cf7396e81c6d5eb7a6ea4
유지웅 <poujis0@gmail.com>
2024-03-30 23:15:21 +0900

    [feat] matchCount, Bonus여부에 따른 Rankresult increase 기능 개발

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,22 +1,31 @@
 package lotto;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 public class LottoResult {
 
+    private static final int DEFAULT_VALUE = 0;
+    private static final int INCREMENT_VALUE = 1;
+
     private final Map<LottoRank, Integer> result;
+
+    public LottoResult() {
+        this(new HashMap<>());
+        Arrays.stream(LottoRank.values())
+            .forEach(this::putRank);
+    }
 
     public LottoResult(Map<LottoRank, Integer> result) {
         this.result = result;
     }
 
-    public LottoResult(LottoRank... rank) {
-        this(Arrays.stream(rank)
-            .collect(
-                Collectors.groupingBy(e -> e, Collectors.summingInt(e -> 1))
-            ));
+    public LottoResult(LottoRank... ranks) {
+        this();
+        Arrays.stream(ranks).forEach(
+            this::putRank);
     }
 
     public double calculateReturnRate(int payed) {
@@ -28,9 +37,34 @@ public class LottoResult {
     }
 
     public int countRank(LottoRank rank) {
-        return result.getOrDefault(rank, 0);
+        return result.getOrDefault(rank, DEFAULT_VALUE);
     }
 
+    public void increaseRankCount(int matchCount, boolean matchBonus) {
+        LottoRank rank = LottoRank.isLottoRank(matchCount, matchBonus);
+        putRank(rank);
+    }
+
+    private void putRank(LottoRank rank) {
+        result.put(rank, result.getOrDefault(rank, DEFAULT_VALUE) + INCREMENT_VALUE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoResult that = (LottoResult) o;
+        return Objects.equals(result, that.result);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result);
+    }
 
     @Override
     public String toString() {
