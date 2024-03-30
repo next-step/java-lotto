de3d52f09d0cb02341987b34e1eb7a41b6fc6e7a
duyankim <devduyan@gmail.com>
2024-03-30 23:27:06 +0900

    fix: countMap에 Reward enum 적용

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -10,16 +10,16 @@ import java.util.Map;
 public class LottoGame {
 
     private Lottos lottos;
-    private Map<Integer, Integer> countMap;
+    private final Map<Integer, Integer> countMap;
 
     public LottoGame(int tryNo) {
         // 로또 장수만큼 6자리 숫자를 랜덤 뽑기한다.
         this.lottos = new Lottos(tryNo);
         this.countMap = new HashMap<>();
-        countMap.put(3, 0);
-        countMap.put(4, 0);
-        countMap.put(5, 0);
-        countMap.put(6, 0);
+        countMap.put(Reward.THREE.getMatchingCount(), 0);
+        countMap.put(Reward.FOUR.getMatchingCount(), 0);
+        countMap.put(Reward.FIVE.getMatchingCount(), 0);
+        countMap.put(Reward.SIX.getMatchingCount(), 0);
     }
 
     public LottoGame(Lottos lottos) {
