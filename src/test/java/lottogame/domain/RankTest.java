fb5a75ba01a50bbc4e39d65cb47de476f996ab96
m5s3 <devmyoungsoo2@gmail.com>
2024-03-30 22:52:33 +0900

    test(RankTest): 경계 테스트케이스 추가가

--- src/test/java/lottogame/domain/RankTest.java
+++ src/test/java/lottogame/domain/RankTest.java
@@ -18,13 +18,17 @@ class RankTest {
 
     static Stream<Arguments> countAndRank() {
         return Stream.of(
+                Arguments.arguments(6, true, Rank.FIRST),
                 Arguments.arguments(6, false, Rank.FIRST),
                 Arguments.arguments(5, true, Rank.SECOND),
                 Arguments.arguments(5, false, Rank.THIRD),
+                Arguments.arguments(4, true, Rank.FOURTH),
                 Arguments.arguments(4, false, Rank.FOURTH),
+                Arguments.arguments(3, true, Rank.FIFTH),
                 Arguments.arguments(3, false, Rank.FIFTH),
+                Arguments.arguments(2, true, Rank.EMPTY),
                 Arguments.arguments(2, false, Rank.EMPTY),
-                Arguments.arguments(1, false, Rank.EMPTY),
+                Arguments.arguments(1, true, Rank.EMPTY),
                 Arguments.arguments(0, false, Rank.EMPTY)
         );
     }
