e2c3e2fcecd0b0e5d648c66d159117e3feb5d479
westkitekim <westkite418@gmail.com>
2024-03-30 14:34:51 +0900

    refactor: 입력부 문자열split 로직 책임분리

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,14 +2,10 @@ package calculator.view;
 
 import calculator.domain.Validator;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String DELIMETER_BLANK = " ";
 
     private InputView() {
         throw new AssertionError();
@@ -27,22 +23,5 @@ public class InputView {
     private static void validateInput(String inputFormula) {
         Validator.validateInputFormula(inputFormula);
         Validator.validateArithmeticFormula(inputFormula);
-    }
-
-    public static List<Integer> parseOperands(String input) {
-        return splitInputFormula(input).stream()
-                .filter(string -> string.matches("[0-9]+"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
-
-    public static List<String> parseOperators(String input) {
-        return splitInputFormula(input).stream()
-                .filter(string -> !string.matches("[0-9]+"))
-                .collect(Collectors.toList());
-    }
-
-    private static List<String> splitInputFormula(String input) {
-        return Arrays.asList(input.split(DELIMETER_BLANK));
     }
 }
