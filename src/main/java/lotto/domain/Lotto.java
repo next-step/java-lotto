eed1597127ca53e217c448310914c253375d5fdb
hhhhhwi <aab555586@gmail.com>
2024-03-31 20:25:11 +0900

    refactor: 로또 번호를 LottoNumbers로 포장하여 불변객체를 반환하도록 수정 - 로또 번호 출력 시, lottoNumbers에서 get하도록 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,27 +3,19 @@ package lotto.domain;
 import lotto.domain.strategy.LottoNumberStrategy;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Lotto {
-	private final List<Integer> lottoNumbers;
+	private final LottoNumbers lottoNumbers;
 
 	public Lotto(LottoNumberStrategy strategy) {
-		this.lottoNumbers = strategy.generateLottoNumber();
+		this.lottoNumbers = new LottoNumbers(strategy.generateLottoNumber());
 	}
 
 	public List<Integer> getLottoNumbers() {
-		return lottoNumbers;
+		return lottoNumbers.getLottoNumbers();
 	}
 
 	public boolean isMatched(int number) {
-		return lottoNumbers.contains(number);
-	}
-
-	public String getLottoNumbersString() {
-		return lottoNumbers.stream()
-				.sorted()
-				.map(String::valueOf)
-				.collect(Collectors.joining(", ", "[", "]"));
+		return lottoNumbers.isMatched(number);
 	}
 }
