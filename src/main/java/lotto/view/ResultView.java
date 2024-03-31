a636182192d079938440b50417cc6397eb1cc526
duyankim <devduyan@gmail.com>
2024-03-31 09:28:55 +0900

    feat: 구입한 로또 목록을 출력

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,10 +1,18 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.domain.Reward;
 
 import java.util.Map;
 
 public class ResultView {
+
+    public void printLottos(Lottos lottos) {
+        for (Lotto lotto : lottos.of()) {
+            System.out.println(lotto.getAutoLotto());
+        }
+    }
 
     public void printResult(Map<Integer, Integer> countMap, double profit) {
         System.out.println("당첨 통계");
