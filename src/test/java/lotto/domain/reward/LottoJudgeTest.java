4dd4f32f2ef0222470b66baedfa421014b98327b
kylekim2123 <kyle2123@naver.com>
2024-03-31 15:50:46 +0900

    [STEP 2] test: 테스트 용 객체 생성 메서드 호출부를 별도의 메서드로 옮겨서 중복 제거

--- src/test/java/lotto/domain/reward/LottoJudgeTest.java
+++ src/test/java/lotto/domain/reward/LottoJudgeTest.java
@@ -7,24 +7,20 @@ import static lotto.domain.reward.LottoRank.SECOND;
 import static lotto.domain.reward.LottoRank.THIRD;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import lotto.domain.Lotto;
-import lotto.domain.LottoNumber;
 
 class LottoJudgeTest {
 
     @Test
     @DisplayName("당첨 번호와 로또 번호를 비교하여 동일한 갯수에 따라 순위를 부여한다.")
     void Judge_LottoRank() {
-        final Lotto winningLotto = Lotto.from(toLottoNumbers(1, 2, 3, 4, 5, 6));
+        final Lotto winningLotto = lotto(1, 2, 3, 4, 5, 6);
         final LottoResult actualLottoResult = new LottoJudge().judge(lottos(), winningLotto);
         final LottoResult expectedLottoResult = lottoResult();
 
@@ -36,23 +32,21 @@ class LottoJudgeTest {
 
     private static List<Lotto> lottos() {
         return List.of(
-                Lotto.from(toLottoNumbers(1, 2, 3, 4, 5, 6)), // FIRST
-                Lotto.from(toLottoNumbers(1, 2, 3, 4, 5, 6)), // FIRST
-                Lotto.from(toLottoNumbers(1, 2, 3, 4, 5, 7)), // SECOND
-                Lotto.from(toLottoNumbers(1, 2, 3, 4, 5, 7)), // SECOND
-                Lotto.from(toLottoNumbers(1, 2, 3, 4, 5, 7)), // SECOND
-                Lotto.from(toLottoNumbers(1, 2, 3, 4, 7, 8)), // THIRD
-                Lotto.from(toLottoNumbers(1, 2, 3, 7, 8, 9)), // FOURTH
-                Lotto.from(toLottoNumbers(1, 2, 7, 8, 9, 10)), // NONE
-                Lotto.from(toLottoNumbers(1, 7, 8, 9, 10, 11)), // NONE
-                Lotto.from(toLottoNumbers(7, 8, 9, 10, 11, 12)) // NONE
+                lotto(1, 2, 3, 4, 5, 6), // FIRST
+                lotto(1, 2, 3, 4, 5, 6), // FIRST
+                lotto(1, 2, 3, 4, 5, 7), // SECOND
+                lotto(1, 2, 3, 4, 5, 7), // SECOND
+                lotto(1, 2, 3, 4, 5, 7), // SECOND
+                lotto(1, 2, 3, 4, 7, 8), // THIRD
+                lotto(1, 2, 3, 7, 8, 9), // FOURTH
+                lotto(1, 2, 7, 8, 9, 10), // NONE
+                lotto(1, 7, 8, 9, 10, 11), // NONE
+                lotto(7, 8, 9, 10, 11, 12) // NONE
         );
     }
 
-    private static Set<LottoNumber> toLottoNumbers(final int... numbers) {
-        return Arrays.stream(numbers)
-                .mapToObj(LottoNumber::from)
-                .collect(Collectors.toUnmodifiableSet());
+    private static Lotto lotto(final int... numbers) {
+        return Lotto.from(numbers);
     }
 
     private static LottoResult lottoResult() {
--- src/test/java/lotto/domain/reward/LottoResultTest.java
+++ src/test/java/lotto/domain/reward/LottoResultTest.java
@@ -17,30 +17,29 @@ class LottoResultTest {
     @ParameterizedTest
     @MethodSource("lottoResultAndExpectedProfitRate")
     @DisplayName("구매자가 지불한 총 가격과 총 당첨 금액을 비교하여 수익률을 계산한다.")
-    void CalculateProfitRate(final Map<LottoRank, Long> result, final double expectedProfitRate) {
-        final LottoResult lottoResult = LottoResult.from(result);
-
+    void CalculateProfitRate(final LottoResult lottoResult, final double expectedProfitRate) {
         assertThat(lottoResult.profitRate())
                 .isEqualTo(expectedProfitRate);
     }
 
     private static Stream<Arguments> lottoResultAndExpectedProfitRate() {
         return Stream.of(
-                Arguments.of(toLottoResult(0, 0, 0, 0, 1), 0.0),
-                Arguments.of(toLottoResult(0, 0, 0, 1, 0), 5.0),
-                Arguments.of(toLottoResult(0, 0, 1, 0, 0), 50.0),
-                Arguments.of(toLottoResult(0, 1, 0, 0, 0), 1500.0),
-                Arguments.of(toLottoResult(1, 0, 0, 0, 0), 2000000.0),
-                Arguments.of(toLottoResult(0, 0, 0, 1, 1), 2.5),
-                Arguments.of(toLottoResult(0, 0, 0, 1, 13), 0.35)
+                Arguments.of(lottoResult(0, 0, 0, 0, 1), 0.0),
+                Arguments.of(lottoResult(0, 0, 0, 1, 0), 5.0),
+                Arguments.of(lottoResult(0, 0, 1, 0, 0), 50.0),
+                Arguments.of(lottoResult(0, 1, 0, 0, 0), 1500.0),
+                Arguments.of(lottoResult(1, 0, 0, 0, 0), 2000000.0),
+                Arguments.of(lottoResult(0, 0, 0, 1, 1), 2.5),
+                Arguments.of(lottoResult(0, 0, 0, 1, 13), 0.35)
         );
     }
 
-    private static Map<LottoRank, Long> toLottoResult(final long... rankCounts) {
+    private static LottoResult lottoResult(final long... rankCounts) {
         final LottoRank[] ranks = LottoRank.values();
-
-        return IntStream.range(0, rankCounts.length)
+        final Map<LottoRank, Long> result = IntStream.range(0, rankCounts.length)
                 .boxed()
                 .collect(Collectors.toMap(i -> ranks[i], i -> rankCounts[i]));
+
+        return LottoResult.from(result);
     }
 }
