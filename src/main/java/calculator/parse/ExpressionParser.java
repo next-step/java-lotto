2705969f6bc82d8627d67d20f60cfc935f26e8fe
giraffeb <rwpark07@gmail.com>
2024-03-31 16:41:18 +0900

    feat: 계산식 요소 반환

--- src/main/java/calculator/parse/ExpressionParser.java
+++ src/main/java/calculator/parse/ExpressionParser.java
@@ -1,2 +1,35 @@
-package calculator.parse;public class ExpressionParser {
+package calculator.parse;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ExpressionParser {
+
+    private String a;
+    private String b;
+    private String op;
+    private Queue<String> queue = new LinkedList<>();
+
+    public String[] parse(String expression){
+        String[] expressionElements = expression.split(" ");
+        queue.addAll(List.of(expressionElements));
+        return expressionElements;
+    }
+
+    public String[] next(){
+        if(this.a == null){
+            this.a = this.queue.poll();
+        }
+
+        if(this.op == null){
+            this.op = this.queue.poll();
+        }
+
+        if(this.b == null){
+            this.b = this.queue.poll();
+        }
+
+        return new String[]{this.a, this.op, this.b};
+    }
 }
