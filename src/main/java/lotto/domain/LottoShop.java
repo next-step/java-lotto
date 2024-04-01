7160c74a3a9964fbca0b1119117dadb48bdd8452
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-01 21:42:22 +0900

    REFACTOR : LottoPurchase에서 LottoShop으로 클래스명 변경. 원시값 포장

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -2,18 +2,18 @@ package lotto.domain;
 
 import lotto.constant.Constants;
 
-public class LottoPurchase {
+public class LottoShop {
 
-    private final int purchase;
+    private int money;
 
-    public LottoPurchase(final int money) {
+    public LottoShop(final int money) {
         if (money % Constants.LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해주세요.");
         }
-        this.purchase = money / Constants.LOTTO_PRICE;
+        this.money = money;
     }
 
-    public int getPurchase() {
-        return purchase;
+    public int calculatePurchaseCount() {
+        return this.money / Constants.LOTTO_PRICE;
     }
 }
