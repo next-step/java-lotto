6928d557ab9f1f30fcf02e3bd7fe4fb63ec5aa80
Jinnie-J <jjesweet@naver.com>
2024-04-02 01:01:53 +0900

    feat: Rank 결과 구하기 위한 기능 구현

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Rank {
     FIRST(6, 2000000000),
     SECOND(5, 30000000),
@@ -14,6 +19,31 @@ public enum Rank {
     Rank(int countOfMatch, int reward) {
         this.countOfMatch = countOfMatch;
         this.reward = reward;
+    }
+
+    public static List<Rank> getRanks() {
+        return Stream.of(values())
+                .filter(rank -> rank != NONE)
+                .collect(Collectors.toList());
+    }
+
+    public static Rank findRank(int countOfMatch) {
+        return Stream.of(values())
+                .filter(match(countOfMatch))
+                .findAny()
+                .orElse(NONE);
+    }
+
+    public static int findReward(int countOfMatch) {
+        return Stream.of(values())
+                .filter(match(countOfMatch))
+                .findAny()
+                .orElse(NONE)
+                .reward;
+    }
+
+    private static Predicate<Rank> match(int countOfMatch) {
+        return rank -> rank.countOfMatch == countOfMatch;
     }
 
     public int getReward() {
