fe0cbcd4d3ea9c87665fb936b05485aa967bc344
박소민 <qkrthals0524@naver.com>
2024-04-02 02:14:34 +0900

    LottoTicketCollection 생성

---
+++ src/main/java/lotto/domain/LottoTicketCollection.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoTicketCollection {
+
+    private final List<LottoTicket> autoTickets;
+    private final List<LottoTicket> manualTickets;
+
+    public LottoTicketCollection(List<LottoTicket> autoTickets, List<LottoTicket> manualTickets) {
+        this.autoTickets = autoTickets;
+        this.manualTickets = manualTickets;
+    }
+
+    public List<LottoTicket> getAutoTickets() {
+        return autoTickets;
+    }
+
+    public List<LottoTicket> getManualTickets() {
+        return manualTickets;
+    }
+
+    
+}
