3c97a12274d91f3ad0aa3503f81cc8d0d03f0ac5
park-cheonho <cheonho08@naver.com>
2024-04-01 16:15:57 +0900

    refactor: step2-feedback LottoFee로 도메인명 변경
    refactor: step2-feedback LottoFee로 도메인명 변경

--- src/test/java/lotto/LottoFeeTest.java
+++ src/test/java/lotto/LottoFeeTest.java
@@ -1,15 +1,15 @@
 package lotto;
 
-import lotto.domain.Fee;
+import lotto.domain.LottoFee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FeeTest {
+public class LottoFeeTest {
     @Test
     @DisplayName("입력값 몇게임인지 파악하는 메서드")
     void 몇게임() {
-        assertThat(new Fee(14000).convertToGameUnit()).isEqualTo(14);
+        assertThat(new LottoFee(14000).convertToGameUnit()).isEqualTo(14);
     }
 }
