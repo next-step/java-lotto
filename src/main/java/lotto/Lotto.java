f005cc83a2140c6d31bb61fa109024f1195430be
Jinnie-J <jjesweet@naver.com>
2024-04-02 00:51:59 +0900

    feat: 로또 클래스 생성

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,53 @@
+package lotto;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private final static int LOTTO_LENGTH = 6;
+    private final Set<LottoNumber> lottoNumbers;
+
+    public Lotto(Set<Integer> numbers) {
+        this.lottoNumbers = numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet());
+    }
+
+    public Set<LottoNumber> getNumbers() {
+        return lottoNumbers;
+    }
+
+    public Lotto(String lottoNumbers) {
+        Set<LottoNumber> newLottoNumbers = Arrays.stream(lottoNumbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet());
+        checkLottoCount(newLottoNumbers);
+        this.lottoNumbers = newLottoNumbers;
+    }
+
+    private static void checkLottoCount(Set<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException("6개의 숫자가 필요합니다.");
+        }
+    }
+
+    public int winningCount(Lotto lotto) {
+        return (int) lottoNumbers.stream()
+                .filter(lotto::contains)
+                .count();
+    }
+
+    private boolean contains(LottoNumber lottoNumber) {
+        return lottoNumbers.contains(lottoNumber);
+    }
+
+    public Rank getRank(Lotto lotto) {
+        return Rank.findRank(winningCount(lotto));
+    }
+
+    public int getReward(Lotto lotto) {
+        return Rank.findReward(winningCount(lotto));
+    }
+}
