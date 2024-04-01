0f52a0e30da9511144dca9109fa55e488ac8e5c1
gibumgo <gibumgo@gmail.com>
2024-04-01 22:44:53 +0900

    feat : 추가 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class InputView {
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
     public int inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Integer.parseInt(scanner.nextLine());
     }
 
