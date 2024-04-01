b9592e0afcb52cdaf4a3ebe033cfbb709f85409b
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-01 21:43:31 +0900

    CHORE : LottoNumber 인스턴스 캐싱으로 인한 변화

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -7,34 +7,21 @@ import java.util.List;
 import java.util.Objects;
 
 public class WinningLotto {
-    private final List<Integer> winNumbers;
+    private final Lotto winNumbers;
     private final LottoNumber bonusNumber;
 
     public WinningLotto(List<Integer> winNumbers, int bonusNumber) {
-        checkBonusNumberRange(bonusNumber);
         checkBonusDuplicate(winNumbers, bonusNumber);
-        this.winNumbers = winNumbers;
-        this.bonusNumber = new LottoNumber(bonusNumber);
-    }
-
-    private static List<LottoNumber> toLottoNumber(List<Integer> winNumbers) {
-        List<LottoNumber> winLottoNumbers = new ArrayList<>();
-
-        for (int winNumber : winNumbers) {
-            winLottoNumbers.add(new LottoNumber(winNumber));
-        }
-
-        return winLottoNumbers;
+        this.winNumbers = new Lotto(winNumbers);
+        this.bonusNumber = LottoNumber.of(bonusNumber);
     }
 
     public LottoNumber getBonusNumber() {
         return this.bonusNumber;
     }
 
-    private void checkBonusNumberRange(int bonus) {
-        if (bonus < Constants.MIN_LOTTO || bonus > Constants.MAX_LOTTO) {
-            throw new IllegalArgumentException("보너스 번호는 1~45 사이의 값이어야 합니다.");
-        }
+    public Lotto getWinNumbers() {
+        return this.winNumbers;
     }
 
     private void checkBonusDuplicate(List<Integer> winNumbers, int bonusNumber) {
@@ -54,9 +41,5 @@ public class WinningLotto {
     @Override
     public int hashCode() {
         return Objects.hash(winNumbers, bonusNumber);
-    }
-
-    public Lotto makeLotto() {
-        return new Lotto(this.winNumbers);
     }
 }
