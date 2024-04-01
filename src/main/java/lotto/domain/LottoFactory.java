799f4426738c85ab882b856fc18f98a5765d896f
duyankim <devduyan@gmail.com>
2024-04-01 14:37:16 +0900

    fix: LottoFactory 불필요한 인스턴스 변수 제거

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -4,20 +4,13 @@ import static lotto.LottoApplication.LOTTO_COST;
 
 public class LottoFactory {
 
-    private final Lottos lottos;
-
-    private LottoFactory(int lottoAmount) {
-        // 로또 장수만큼 6자리 숫자를 랜덤 뽑기한다.
-        this.lottos = new Lottos(lottoAmount);
-    }
-
     public static Lottos createLottos(int buyingAmount) {
         if (buyingAmount < LOTTO_COST) {
             throw new IllegalArgumentException("구매 금액이 부족합니다.");
         }
 
         int lottoAmount = buyingAmount / LOTTO_COST;
-        return new LottoFactory(lottoAmount).lottos;
+        return new Lottos(lottoAmount);
     }
 
 }
