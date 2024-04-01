f8919ae3d77c89aaa2c51f10a1ae8ad270f7cd9b
park-cheonho <cheonho08@naver.com>
2024-04-01 16:25:56 +0900

    feat: step3 보너스 추가 및 당첨 등급 찾는 메소드 추가
    feat: step3 보너스 추가 및 당첨 등급 찾는 메소드 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -3,20 +3,23 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoRank {
-    FIRST("1등", 6, 2_000_000_000),
-    SECOND("2등", 5, 1_500_000),
-    THIRD("3등", 4, 50_000),
-    FOURTH("4등", 3, 5_000),
-    NONE("낙첨", 0, 0);
+    FIRST("1등", 6, 2_000_000_000, false),
+    SECOND("보너스", 5, 30_000_000, true),
+    THIRD("2등", 5, 1_500_000, false),
+    FOURTH("3등", 4, 50_000, false),
+    FIFTH("4등", 3, 5_000, false),
+    NONE("낙첨", 0, 0, false);
 
     private final String rank;
     private final int matchCount;
     private final int prizeMoney;
+    private final boolean bonus;
 
-    LottoRank(String rank, int matchCount, int prizeMoney) {
+    LottoRank(String rank, int matchCount, int prizeMoney, boolean bonus) {
         this.rank = rank;
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.bonus = bonus;
     }
 
     public int getMatchCount() {
@@ -27,13 +30,22 @@ public enum LottoRank {
         return prizeMoney;
     }
 
-    public String getRank() {
-        return rank;
-    }
-
     public static LottoRank valueOfMatchCount(int matchCount) {
         return Arrays.stream(values())
                 .filter(lottoRank -> lottoRank.getMatchCount() == matchCount)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public static LottoRank find(int matchCount, boolean matchBonus) {
+        if(matchCount == 5 && matchBonus) {
+            return  Arrays.stream(values())
+                    .filter(lottoRank -> lottoRank.getMatchCount() == matchCount && lottoRank.bonus)
+                    .findFirst()
+                    .orElse(NONE);
+        }
+        return Arrays.stream(values())
+                .filter(lottoRank -> lottoRank.getMatchCount() == matchCount && !lottoRank.bonus)
                 .findFirst()
                 .orElse(NONE);
     }
