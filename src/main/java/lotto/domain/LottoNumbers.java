9f16c2f856cf2ddc5742d4f88f1f9a18f5310629
duyankim <devduyan@gmail.com>
2024-04-01 16:54:22 +0900

    fix: 상수 변수명 대문자로

--- src/main/java/lotto/domain/LottoNumbers.java
+++ src/main/java/lotto/domain/LottoNumbers.java
@@ -11,15 +11,15 @@ public class LottoNumbers {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int LOTTO_SIZE = 6;
-    private static final List<Integer> allNumbers = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+    private static final List<Integer> ALL_NUMBERS = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
             .boxed()
             .collect(Collectors.toList());
 
     private final Set<Integer> lottoNumbers;
 
     public LottoNumbers() {
-        Collections.shuffle(allNumbers);
-        this.lottoNumbers = allNumbers.stream()
+        Collections.shuffle(ALL_NUMBERS);
+        this.lottoNumbers = ALL_NUMBERS.stream()
                 .limit(LOTTO_SIZE)
                 .collect(Collectors.toSet());
     }
