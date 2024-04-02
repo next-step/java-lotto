85856a2aa1baec111f9de5fdea64085874665a29
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-03 00:32:40 +0900

    REFACTOR : 수동 로또 개수 예외 처리 추가

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -4,7 +4,7 @@ import lotto.constant.Constants;
 
 public class LottoShop {
 
-    private int money;
+    private final int money;
 
     public LottoShop(final int money) {
         if (money % Constants.LOTTO_PRICE != 0) {
@@ -13,7 +13,15 @@ public class LottoShop {
         this.money = money;
     }
 
-    public int calculatePurchaseCount() {
+    public int calculateTotalPurchaseCount(int passiveCount) {
+        checkPassivePurchaseCount(passiveCount);
         return this.money / Constants.LOTTO_PRICE;
+    }
+
+    private void checkPassivePurchaseCount(int passiveCount) {
+        if (this.money / Constants.LOTTO_PRICE >= passiveCount) {
+            return;
+        }
+        throw new IllegalArgumentException("구매 가능한 로또 수를 초과했습니다.");
     }
 }
