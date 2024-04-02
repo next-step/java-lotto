4b407d5b13153202fb7ff5fa0facc38bc37b4407
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-03 00:31:06 +0900

    REFACTOR : String 배열을 인자로 갖는 생성자 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,10 +3,19 @@ package lotto.domain;
 import lotto.constant.Constants;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Lotto {
     private final Set<LottoNumber> lotto;
+
+    private static List<Integer> toIntegerList(String[] textLottoNumbers) {
+        List<Integer> lotto = new ArrayList<>();
+
+        for (String textNumber : textLottoNumbers) {
+            lotto.add(Integer.parseInt(textNumber));
+        }
+
+        return lotto;
+    }
 
     private static Set<LottoNumber> toLottoNumber(List<Integer> lottoNumbers) {
         Set<LottoNumber> lotto = new HashSet<>();
@@ -18,24 +27,28 @@ public class Lotto {
         return lotto;
     }
 
+    public Lotto(String[] textNumbers) {
+        this(toIntegerList(textNumbers));
+    }
+
     public Lotto(List<Integer> numbers) {
         Set<LottoNumber> lottoNumbers = toLottoNumber(numbers);
 
-        checkLottoSize(lottoNumbers);
+        checkLottoSize(numbers);
         checkDuplicate(lottoNumbers);
 
         this.lotto = lottoNumbers;
     }
 
-    private void checkDuplicate(Set<LottoNumber> lotto) {
-        if (lotto.size() < Constants.LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또에 중복 숫자가 있습니다.");
+    private void checkLottoSize(List<Integer> lotto) {
+        if (lotto.size() != Constants.LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 개수는 6개여야 합니다.");
         }
     }
 
-    private void checkLottoSize(Set<LottoNumber> lotto) {
-        if (lotto.size() > Constants.LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또 개수는 6개여야 합니다.");
+    private void checkDuplicate(Set<LottoNumber> lotto) {
+        if (lotto.size() != Constants.LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또에 중복 숫자가 있습니다.");
         }
     }
 
