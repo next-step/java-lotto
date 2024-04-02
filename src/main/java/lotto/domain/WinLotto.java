647124f185e297ca7e0f2893c17dff9c055b7ce8
박소민 <qkrthals0524@naver.com>
2024-04-03 00:21:18 +0900

    부 생성자 추가

--- src/main/java/lotto/domain/WinLotto.java
+++ src/main/java/lotto/domain/WinLotto.java
@@ -8,13 +8,17 @@ public class WinLotto {
     private final LottoNumber bonusNumber;
 
     public WinLotto(List<Integer> winningTicket, Integer bonusNumber) {
-        isBonusIncluded(winningTicket, bonusNumber);
-        this.ticket = LottoTicket.createTicket(winningTicket);
-        this.bonusNumber = new LottoNumber(bonusNumber);
+        this(LottoTicket.createTicket(winningTicket), LottoNumber.from(bonusNumber));
     }
 
-    private static void isBonusIncluded(List<Integer> winningTicket, Integer bonusNumber) {
-        if (winningTicket.contains(bonusNumber)) {
+    public WinLotto(LottoTicket ticket, LottoNumber bonusNumber) {
+        isBonusIncluded(ticket, bonusNumber);
+        this.ticket = ticket;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private static void isBonusIncluded(LottoTicket winningTicket, LottoNumber bonusNumber) {
+        if (winningTicket.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스번호는 당첨번호에 포함될 수 없습니다.");
         }
     }
