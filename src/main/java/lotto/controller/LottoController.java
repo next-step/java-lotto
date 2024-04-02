e3f4d731b35362bda3c8d873fabedebaeae2dacb
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-03 00:30:37 +0900

    REFACTOR : startLotto 메소드를 작은 메소드 단위로 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -10,19 +10,56 @@ import java.util.List;
 
 public class LottoController {
     public static void startLotto() {
-        int money = InputView.inputMoney();
-        Lottos lottos = LottoGenerator.createLottos(new LottoShop(money).calculatePurchaseCount());
+        int money = getMoney();
+        int passiveCount = getPassiveCount();
+        int totalCount = getTotalCount(money, passiveCount);
+        Lottos lottos = getLottos(totalCount, passiveCount);
+
+        ResultView.printPassiveAndAutoCount(passiveCount, totalCount - passiveCount);
         ResultView.printLotto(lottos);
 
-        List<Integer> winNumbers = StringUtils.parseWinNumbers(InputView.inputWinNumbers());
-        int bonus = InputView.inputBonus();
+        List<Integer> winNumbers = getWinNumbers();
+        int bonus = getBonus();
 
+        proceedMatch(lottos, winNumbers, bonus);
+
+        HashMap<Rank, Integer> matchResult = Rank.getMatchResult();
+        ResultView.printResult(matchResult, getRevenue(money, matchResult));
+    }
+
+    private static int getMoney() {
+        return InputView.inputMoney();
+    }
+
+    private static int getPassiveCount() {
+        return InputView.inputPassiveLottoCount();
+    }
+
+    private static int getTotalCount(int money, int passiveCount) {
+        return new LottoShop(money).calculateTotalPurchaseCount(passiveCount);
+    }
+
+    private static Lottos getLottos(int totalCount, int passiveCount) {
+        return LottoGenerator
+                .createLottos(InputView.inputPassiveLottoNumbers(passiveCount), totalCount-passiveCount);
+    }
+
+    private static List<Integer> getWinNumbers() {
+        return StringUtils.parseWinNumbers(InputView.inputWinNumbers());
+    }
+
+    private static int getBonus() {
+        return InputView.inputBonus();
+    }
+
+    private static void proceedMatch(Lottos lottos, List<Integer> winNumbers, int bonus) {
         for (Lotto lotto : lottos.getLottos()) {
             Rank.putMatchResult(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonus)));
         }
+    }
 
+    private static double getRevenue(int money, HashMap<Rank, Integer> matchResult) {
         LottoRevenue lottoRevenue = new LottoRevenue();
-        HashMap<Rank, Integer> matchResult = Rank.getMatchResult();
-        ResultView.printResult(matchResult, lottoRevenue.calculateRevenue(money, lottoRevenue.revenueTotal(matchResult)));
+        return lottoRevenue.calculateRevenue(money, lottoRevenue.revenueTotal(matchResult));
     }
 }
