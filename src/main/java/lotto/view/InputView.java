1b197ab926c3267bfc46c9562b629d736f12f621
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-03 00:33:23 +0900

    REFACTOR : 수동 로또 개수 및 수동 로또 번호 입력 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,6 +10,26 @@ public class InputView {
     public static int inputMoney() {
         System.out.println("구매 금액을 입력해 주세요.");
         return scanner.nextInt();
+    }
+
+    public static int inputPassiveLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public static List<String> inputPassiveLottoNumbers(int passiveLottoCount) {
+        System.out.println("수동으로 구매할 로또 번호를 입력해 주세요.");
+        List<String> textLottoNumbers = new ArrayList<>();
+        String input;
+
+        scanner.nextLine();
+
+        while (scanner.hasNextLine() && passiveLottoCount > 0) {
+            input = scanner.nextLine();
+            textLottoNumbers.add(input);
+            passiveLottoCount--;
+        }
+        return textLottoNumbers;
     }
 
     public static String inputWinNumbers() {
