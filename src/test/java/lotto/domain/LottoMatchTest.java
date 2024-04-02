a2c01e00139eb6b90dc0f90b0a3bc0bffba7badc
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-02 21:17:53 +0900

    REFACTOR : @MethodSource 활용하여 Lotto와 WinningLotto를 제시했을 때 일치 수에 알맞은 Rank를 반환하는 메소드 테스트

--- src/test/java/lotto/domain/LottoMatchTest.java
+++ src/test/java/lotto/domain/LottoMatchTest.java
@@ -1,57 +1,43 @@
 package lotto.domain;
 
-import lotto.domain.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoMatchTest {
-    private List<Integer> winNumbers;
-    private int bonusNumber;
+    private static final List<Integer> winNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    private static final int bonusNumber = 7;
 
-    @BeforeEach
-    void setUp() {
-        winNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        bonusNumber = 7;
+    @DisplayName("Lotto와 WinningLotto를 제시했을 때 일치 수에 따라 그에 알맞은 Rank 반환")
+    @ParameterizedTest
+    @MethodSource("match")
+    void findRank(Lotto lotto, WinningLotto winningLotto, Rank rank) {
+        assertThat(LottoMatch.match(lotto, winningLotto)).isEqualTo(rank);
     }
 
-    @Test
-    void 순위_1등() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
-        assertThat(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonusNumber))).isEqualTo(Rank.FIRST);
-    }
-
-    @Test
-    void 순위_2등() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7));
-        assertThat(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonusNumber))).isEqualTo(Rank.SECOND);
-    }
-
-    @Test
-    void 순위_3등() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8));
-        assertThat(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonusNumber))).isEqualTo(Rank.THIRD);
-    }
-
-    @Test
-    void 순위_4등() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 4, 8, 9));
-        assertThat(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonusNumber))).isEqualTo(Rank.FOURTH);
-    }
-
-    @Test
-    void 순위_5등() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 3, 8, 9, 10));
-        assertThat(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonusNumber))).isEqualTo(Rank.FIFTH);
-    }
-
-    @Test
-    void 등수_안에_못_듬() {
-        Lotto lotto = new Lotto(Arrays.asList(1, 2, 8, 9, 10, 11));
-        assertThat(LottoMatch.match(lotto, new WinningLotto(winNumbers, bonusNumber))).isEqualTo(Rank.NONE);
+    static Stream<Arguments> match() {
+        return Stream.of(
+                Arguments.arguments(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                        new WinningLotto(winNumbers, bonusNumber), Rank.FIRST),
+                Arguments.arguments(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)),
+                        new WinningLotto(winNumbers, bonusNumber), Rank.SECOND),
+                Arguments.arguments(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 8)),
+                        new WinningLotto(winNumbers, bonusNumber), Rank.THIRD),
+                Arguments.arguments(new Lotto(Arrays.asList(1, 2, 3, 4, 8, 9)),
+                        new WinningLotto(winNumbers, bonusNumber), Rank.FOURTH),
+                Arguments.arguments(new Lotto(Arrays.asList(1, 2, 3, 8, 9, 10)),
+                        new WinningLotto(winNumbers, bonusNumber), Rank.FIFTH),
+                Arguments.arguments(new Lotto(Arrays.asList(1, 2, 8, 9, 10, 11)),
+                        new WinningLotto(winNumbers, bonusNumber), Rank.NONE)
+        );
     }
 }
