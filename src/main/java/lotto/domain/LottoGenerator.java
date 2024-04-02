6834ad956144fc56c70c3bb13ae258d1fecf007d
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-03 00:31:52 +0900

    REFACTOR : 수동 로또와 자동 로또 생성 과정 분리. 두 가지로 로또를 통합하여 Lottos 생성

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,24 +1,45 @@
 package lotto.domain;
 
 import lotto.constant.Constants;
+import lotto.utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
-    public static Lottos createLottos(int count) {
+    public static Lottos createLottos(List<String> textLottos, int autoCount) {
+        List<Lotto> lottoList = createPassiveLottos(textLottos);
+        lottoList.addAll(createAutoLottos(autoCount));
+        return new Lottos(lottoList);
+    }
+
+    private static List<Lotto> createAutoLottos(int count) {
         List<Lotto> lottoList = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
             lottoList.add(createLotto(shuffleLotto()));
         }
 
-        return new Lottos(lottoList);
+        return lottoList;
     }
 
-    public static Lotto createLotto(List<Integer> shuffledLotto) {
-        return new Lotto(shuffledLotto);
+    private static List<Lotto> createPassiveLottos(List<String> textLottos) {
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (String textLotto : textLottos) {
+            lottoList.add(createLotto(StringUtils.splitTextWinNumbers(textLotto)));
+        }
+
+        return lottoList;
+    }
+
+    public static Lotto createLotto(String[] textLotto) {
+        return new Lotto(textLotto);
+    }
+
+    public static Lotto createLotto(List<Integer> lotto) {
+        return new Lotto(lotto);
     }
 
     public static List<Integer> shuffleLotto() {
