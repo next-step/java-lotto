ce306deec567c90eb7cbac80e36e388ff924c7a7
NaayoungKwon <kny8092@gmail.com>
2024-04-03 23:19:54 +0900

    feat: Lotto 한줄 당 6개 제한

--- src/main/java/domain/lotto/Lotto.java
+++ src/main/java/domain/lotto/Lotto.java
@@ -1,20 +1,28 @@
 package domain.lotto;
 
 import domain.lotto.vo.LottoNumber;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Lotto {
 
   private final Set<LottoNumber> numbers;
 
-  public Lotto(List<Integer> numbers) {
-    this.numbers = numbers.stream().map(LottoNumber::of).collect(Collectors.toSet());
+  public Lotto(List<LottoNumber> numbers) {
+    validateNumberLength(numbers.size());
+    this.numbers = new HashSet<>(numbers);
   }
 
   public Set<LottoNumber> getNumbers() {
     return numbers;
+  }
+
+  private void validateNumberLength(Integer numberLength){
+    if(numberLength != 6){
+      throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+    }
   }
 
 }
