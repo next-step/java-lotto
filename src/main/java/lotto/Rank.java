4ab7abd4bfcd5dd4d71a7066c5b321e43039b305
Jinnie-J <jjesweet@naver.com>
2024-04-04 00:35:15 +0900

    feat: Rank 금액 수정

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -7,10 +7,9 @@ import java.util.stream.Stream;
 
 public enum Rank {
     FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
+    SECOND(5, 1500000),
+    THIRD(4, 50000),
+    FOURTH(3, 5000),
     NONE(0, 0);
 
     private final int countOfMatch;
