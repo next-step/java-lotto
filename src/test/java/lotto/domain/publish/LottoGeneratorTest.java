92f88187a9f3264a1f391f584a8a5f86ae990600
kylekim2123 <kyle2123@naver.com>
2024-04-04 00:00:04 +0900

    [STEP 4] test: 수동 로또 생성에 대한 테스트 코드 추가

--- src/test/java/lotto/domain/publish/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/publish/LottoGeneratorTest.java
@@ -4,6 +4,8 @@ import static lotto.domain.publish.LottoGenerator.MINIMUM_LOTTO_QUANTITY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,21 +15,35 @@ class LottoGeneratorTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 5, 10, 100})
-    @DisplayName("로또 구매 개수만큼 로또를 생성한다.")
-    void GenerateLottos_PositiveQuantity(final int positiveQuantity) {
-        final LottoGenerator lottoMaker = new LottoGenerator(new RandomLottoNumbersPicker());
+    @DisplayName("특정 개수만큼 자동 로또를 생성한다.")
+    void GenerateAutoLottos_PositiveQuantity(final int positiveQuantity) {
+        final LottoGenerator lottoGenerator = new LottoGenerator(new RandomLottoNumbersPicker());
 
-        assertThat(lottoMaker.generateLottos(positiveQuantity))
+        assertThat(lottoGenerator.generateAutoLottos(positiveQuantity))
                 .hasSize(positiveQuantity);
     }
 
     @Test
-    @DisplayName("로또 구매 개수가 최소 구매 개수보다 적은 경우 예외를 던진다.")
-    void GenerateLottos_LessThanMinimumQuantity() {
-        final LottoGenerator lottoMaker = new LottoGenerator(new RandomLottoNumbersPicker());
+    @DisplayName("자동 로또 개수가 최소 개수보다 적은 경우 예외를 던진다.")
+    void GenerateAutoLottos_LessThanMinimumQuantity() {
+        final LottoGenerator lottoGenerator = new LottoGenerator(new RandomLottoNumbersPicker());
         final int quantityLessThanMinimum = MINIMUM_LOTTO_QUANTITY - 1;
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> lottoMaker.generateLottos(quantityLessThanMinimum));
+                .isThrownBy(() -> lottoGenerator.generateAutoLottos(quantityLessThanMinimum));
+    }
+
+    @Test
+    @DisplayName("번호 목록 수만큼 수동 로또를 생성한다.")
+    void GenerateManualLottos_ManualNumbers() {
+        final LottoGenerator lottoGenerator = new LottoGenerator(new RandomLottoNumbersPicker());
+        final List<List<Integer>> manualNumbers = List.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(10, 11, 12, 13, 14, 15),
+                List.of(1, 4, 17, 31, 40, 45)
+        );
+
+        assertThat(lottoGenerator.generateManualLottos(manualNumbers))
+                .hasSize(manualNumbers.size());
     }
 }
