d075f255bfaffaeb727dde272c02a5a330cb90c6
park-cheonho <cheonho08@naver.com>
2024-04-03 19:30:09 +0900

    style: step3-feedback 불필요 주석 제거
    style: step3-feedback 불필요 주석 제거

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,17 +9,16 @@ import static lotto.domain.LottoGenerator.generateLottos;
 public class LottoController {
 
     public void buyLotto() {
-        // 금액만큼 Lotto 생성 하여 출력
+
         LottoFee lottoFee = InputView.payForGames();
         Lottos lottos = generateLottos(lottoFee);
         ResultView.showLottos(lottos);
 
-        // 당첨 번호 및 보너스 번호 입력 받아 WinningLotto 생성
+
         Lotto winningNumber = Lotto.from(InputView.inputWinningNumber());
         int bonusNumber = InputView.inputBonusNumber();
         WinningLotto winningLottoNumber = new WinningLotto(winningNumber, bonusNumber);
 
-        // 당첨 번호 확인 후 출력
         ResultView.showRanks(lottos.match(winningLottoNumber), lottoFee);
     }
 }
