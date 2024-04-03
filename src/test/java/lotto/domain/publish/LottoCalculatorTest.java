41c24126e4666d477a718ba2dcc9b922bc295b7e
kylekim2123 <kyle2123@naver.com>
2024-04-04 00:04:11 +0900

    [STEP 4] test: 자동 로또 구매 개수를 계산하는 기능에 대한 테스트 코드 추가

--- src/test/java/lotto/domain/publish/LottoCalculatorTest.java
+++ src/test/java/lotto/domain/publish/LottoCalculatorTest.java
@@ -7,32 +7,42 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoCalculatorTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1000, 2000, 5000, 10000, 50000, 100000})
-    @DisplayName("총 로또 구매 가격에 따라 로또 구매 개수를 반환한다.")
-    void CalculateQuantity(final int totalPrice) {
-        assertThat(new LottoCalculator().calculateQuantity(totalPrice))
-                .isEqualTo(totalPrice / LOTTO_PRICE_PER_UNIT);
+    @CsvSource(value = {"1000,1", "2000,1", "5000,3", "10000,2", "50000,0", "100000,10"})
+    @DisplayName("총 로또 구매 가격에 따라 자동 로또 구매 개수를 반환한다.")
+    void CalculateAutoQuantity(final int totalPrice, final int manualCount) {
+        final int expectedQuantity = (totalPrice / LOTTO_PRICE_PER_UNIT) - manualCount;
+
+        assertThat(new LottoCalculator().calculateAutoQuantity(totalPrice, manualCount))
+                .isEqualTo(expectedQuantity);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, LOTTO_PRICE_PER_UNIT - 1})
     @DisplayName("총 로또 구매 가격이 로또 1개 당 가격보다 적은 경우 예외를 던진다.")
-    void CalculateQuantity_TotalPriceLessThanUnitPrice_Exception(final int priceLessThanUnitPrice) {
+    void CalculateAutoQuantity_TotalPriceLessThanUnitPrice_Exception(final int priceLessThanUnitPrice) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoCalculator().calculateQuantity(priceLessThanUnitPrice));
+                .isThrownBy(() -> new LottoCalculator().calculateAutoQuantity(priceLessThanUnitPrice, 0));
     }
 
     @Test
     @DisplayName("총 로또 구매 가격이 로또 1개 당 가격의 배수가 아닌 경우 예외를 던진다.")
-    void CalculateQuantity_TotalPriceNotMultipleOfUnitPrice_Exception() {
+    void CalculateAutoQuantity_TotalPriceNotMultipleOfUnitPrice_Exception() {
         final int priceNotMultipleOfUnitPrice = LOTTO_PRICE_PER_UNIT * 2 + 1;
 
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new LottoCalculator().calculateQuantity(priceNotMultipleOfUnitPrice));
+                .isThrownBy(() -> new LottoCalculator().calculateAutoQuantity(priceNotMultipleOfUnitPrice, 0));
+    }
+
+    @Test
+    @DisplayName("수동 구매 수가 총 구매 수보다 많은 경우 예외를 던진다.")
+    void CalculateAutoQuantity_ManualCountExceedTotalCount_Exception() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoCalculator().calculateAutoQuantity(LOTTO_PRICE_PER_UNIT, 2));
     }
 }
