f13213b76cbaf0bddac88920c73e27b0a38d03af
SunheeJo <vieworksjosunhee@gmail.com>
2024-04-04 10:59:25 +0900

    refactor: [2단계-로또(자동)] LottoMain 수정

--- src/main/java/lottopackage/domain/LottoMain.java
+++ src/main/java/lottopackage/domain/LottoMain.java
@@ -2,11 +2,6 @@ package lottopackage.domain;
 
 import lottopackage.controller.LottoMachine;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class LottoMain {
 
     public static void main(String[] args) {
