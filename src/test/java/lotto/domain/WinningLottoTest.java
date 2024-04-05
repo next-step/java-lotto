891d043c65f01212f05af55d034cf6cf54da1999
jaehyeoklee <leejh9049@gmail.com>
2024-04-05 21:26:29 +0900

    [test] WinningLotto 생성자 로직 변경에 따른 테스트 수정

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -13,7 +13,7 @@ class WinningLottoTest {
     @Test
     @DisplayName("당첨 로또 초기화 테스트")
     public void initializeWinningLottoTest() {
-        WinningLotto winningLotto = new WinningLotto("3, 42, 29, 17, 23, 34", "6");
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(3, 42, 29, 17, 23, 34), 6);
 
         assertThat(winningLotto.getLotto().size()).isEqualTo(6);
         assertThat(winningLotto.getBonusNumber()).isEqualTo(6);
@@ -22,8 +22,7 @@ class WinningLottoTest {
     @Test
     @DisplayName("당첨 로또 당첨 숫자 비교 테스트")
     public void compareWinningLottoTest() {
-        WinningLotto winningLotto = new WinningLotto("3, 42, 29, 17, 23, 34", "7");
-
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(3, 42, 29, 17, 23, 34), 7);
 
         Lotto lotto1 = new Lotto(Arrays.asList(3, 42, 29, 16, 24, 32));
         Lotto lotto2 = new Lotto(Arrays.asList(2, 41, 30, 15, 24, 45));
@@ -35,7 +34,7 @@ class WinningLottoTest {
     @Test
     @DisplayName("보너스 숫자 비교 테스트")
     public void compareBonusNumberTest() {
-        WinningLotto winningLotto = new WinningLotto("3, 42, 29, 17, 23, 34", "7");
+        WinningLotto winningLotto = new WinningLotto(Arrays.asList(3, 42, 29, 17, 23, 34), 7);
 
         Lotto lotto = new Lotto(Arrays.asList(3, 42, 29, 16, 24, 7));
 
@@ -45,10 +44,16 @@ class WinningLottoTest {
     @Test
     @DisplayName("당첨 로또 초기화 - Numeric String 예외 테스트")
     public void initializeWinningLottoNumericStringErrorTest() {
-        assertThatThrownBy(() -> new WinningLotto("3, 42, 29, 17, 이십삼, 34", "9"))
+        assertThatThrownBy(() -> WinningLotto.getWinningNumbers("3, 42, 29, 17, 이십삼, 34"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.LOTTO_NUMBER_NUMERIC_ERROR);
-
     }
 
+    @Test
+    @DisplayName("당첨 로또 초기화 - BonusNumber 범위 예외 테스트")
+    public void initializeWinningLottoBonusNumberBoundErrorTest() {
+        assertThatThrownBy(() -> WinningLotto.getBonusNumber("49"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.LOTTO_NUMBER_BOUND_ERROR);
+    }
 }
