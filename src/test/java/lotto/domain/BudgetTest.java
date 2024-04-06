52223069b8fabb85696359d86fbf66749c29c5c8
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-04-06 21:09:24 +0900

    test: 예산 100원 이하 입력 추가

--- src/test/java/lotto/domain/BudgetTest.java
+++ src/test/java/lotto/domain/BudgetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("도메인 Budget 테스트")
 class BudgetTest {
@@ -14,5 +15,13 @@ class BudgetTest {
         Budget budget = new Budget(14000);
         int result = budget.purchasableQuantity(Price.LOTTO);
         assertThat(result).isEqualTo(14);
+    }
+
+    @DisplayName("100원 이하 입력 테스트")
+    @Test
+    void minBudgetTest() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Budget(14100))
+                .withMessage("100원 단위는 입력할 수 없습니다: 14100");
     }
 }
