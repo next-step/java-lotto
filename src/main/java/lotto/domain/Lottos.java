2554f16d938e868f7a02ee5bad5c12d1aa464494
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-04-06 21:23:45 +0900

    refactor: 로또 자동 발급 재귀 수정

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static lotto.domain.Price.LOTTO;
+
 public class Lottos {
 
     private List<Lotto> lottos;
@@ -23,8 +25,10 @@ public class Lottos {
     private static List<Lotto> generateLottosBy(Budget budget) {
         List<Lotto> lottos = new ArrayList<>();
 
-        for (int i = 0; i < budget.purchasableQuantity(Price.LOTTO); i++) {
+        if (budget.isEnoughToPay(LOTTO)) {
+            lottos.addAll(generateLottosBy(budget.spend(LOTTO)));
             lottos.add(new Lotto());
+            return lottos;
         }
         return lottos;
     }
@@ -33,20 +37,31 @@ public class Lottos {
         return lottos;
     }
 
+    public int getLottoSize() {
+        return lottos.size();
+    }
+
     public Statistics computeStatistic(WinLotto winLotto) {
         return new Statistics(checkStatisticNumberMatch(winLotto));
     }
 
     private List<Statistic> checkStatisticNumberMatch(WinLotto winLotto) {
         return lottos.stream()
-            .map(lotto -> getStatistic(winLotto, lotto))
-            .collect(Collectors.toList());
+                .map(lotto -> getStatistic(winLotto, lotto))
+                .collect(Collectors.toList());
     }
 
     private static Statistic getStatistic(WinLotto winLotto, Lotto lotto) {
         return Statistic.valueOfMatchNumber(
-            winLotto.getFeatNumberCount(lotto),
-            winLotto.isContainBonus(lotto)
+                winLotto.getFeatNumberCount(lotto),
+                winLotto.isContainBonus(lotto)
         );
+    }
+
+    public Lottos combine(Lottos lottos) {
+        List<Lotto> newLottos = new ArrayList<>(this.lottos);
+        List<Lotto> combineLottos = lottos.getLottos();
+        newLottos.addAll(combineLottos);
+        return new Lottos(newLottos);
     }
 }
