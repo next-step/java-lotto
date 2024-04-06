9ad4425f1f1bd1e1d4f05a70ad01e231ab2f5768
Duyan Kim <devduyan@gmail.com>
2024-04-06 15:19:41 +0900

    fix: LottoFactory에서 로또에서 사용되는 숫자들을 관리하도록 변경

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -1,8 +1,19 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static lotto.LottoApplication.LOTTO_COST;
 
 public class LottoFactory {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final List<Integer> ALL_NUMBERS = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
 
     public static Lottos createLottos(int buyingAmount) {
         if (buyingAmount < LOTTO_COST) {
