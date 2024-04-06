fb92f9202d5d053efbac1cc51e9df11860ee80dd
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-04-06 21:09:37 +0900

    feat: 100원 이하 valid 추가

--- src/main/java/lotto/domain/Budget.java
+++ src/main/java/lotto/domain/Budget.java
@@ -1,14 +1,32 @@
 package lotto.domain;
 
-public class Budget {
+public class Budget{
 
     private int budget;
 
     public Budget(int budget) {
+        validMinValue(budget);
         this.budget = budget;
+    }
+
+    private void validMinValue(int budget) {
+        if(budget % 100 == 0)
+            throw new IllegalArgumentException("100원 단위는 입력할 수 없습니다: " + budget);
     }
 
     public int purchasableQuantity(Price price) {
         return price.divide(budget);
+    }
+
+    public boolean isEnoughToPay(Price price) {
+        return budget > price.getValue();
+    }
+
+    public Budget spend(Price price) {
+        return new Budget(budget - price.getValue());
+    }
+
+    public Budget spend(Price price, SelfIssueCount issueCount) {
+        return new Budget(budget - issueCount.multiple(price));
     }
 }
