1cf832ec69cc7c1f7343324277508bc707835bc1
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-04-06 21:57:54 +0900

    feat: 발급 카운트 추가

--- src/main/java/lotto/domain/SelfIssueCount.java
+++ src/main/java/lotto/domain/SelfIssueCount.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
-public class SelfIssueCount {
+public class SelfIssueCount implements Comparable<SelfIssueCount>{
 
     private int count;
 
     public SelfIssueCount(int count) {
+        validPositiveValue(count);
         this.count = count;
+    }
+
+    private void validPositiveValue(int count) {
+        if (count < 0)
+            throw new IllegalArgumentException("양수 값만 입력할 수 있습니다: " + count);
     }
 
     public int getCount() {
@@ -17,6 +23,11 @@ public class SelfIssueCount {
     }
 
     public SelfIssueCount decrease() {
-        return new SelfIssueCount(--count);
+        return new SelfIssueCount(count - 1);
+    }
+
+    @Override
+    public int compareTo(SelfIssueCount that) {
+        return this.count - that.count;
     }
 }
