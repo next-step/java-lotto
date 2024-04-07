4c70cb9938d844c17135811df3c800fda8c106aa
gjtmdwns <gjtmdwns0815@naver.com>
2024-04-07 15:42:59 +0900

    REFACTOR : lottList 변수명 lottos로 변경

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,6 +1,5 @@
 package lotto.domain;
 
-import lotto.constant.Constants;
 import lotto.utils.StringUtils;
 
 import java.util.ArrayList;
@@ -8,6 +7,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
+
     public static Lottos createLottos(List<String> textLottos, int autoCount) {
         List<Lotto> lottoList = createPassiveLottos(textLottos);
         lottoList.addAll(createAutoLottos(autoCount));
@@ -15,23 +15,23 @@ public class LottoGenerator {
     }
 
     private static List<Lotto> createAutoLottos(int count) {
-        List<Lotto> lottoList = new ArrayList<>();
+        List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
-            lottoList.add(createLotto(shuffleLotto()));
+            lottos.add(createLotto(shuffleLotto()));
         }
 
-        return lottoList;
+        return lottos;
     }
 
     private static List<Lotto> createPassiveLottos(List<String> textLottos) {
-        List<Lotto> lottoList = new ArrayList<>();
+        List<Lotto> lottos = new ArrayList<>();
 
         for (String textLotto : textLottos) {
-            lottoList.add(createLotto(StringUtils.splitTextWinNumbers(textLotto)));
+            lottos.add(createLotto(StringUtils.splitTextWinNumbers(textLotto)));
         }
 
-        return lottoList;
+        return lottos;
     }
 
     public static Lotto createLotto(String[] textLotto) {
@@ -45,13 +45,13 @@ public class LottoGenerator {
     public static List<Integer> shuffleLotto() {
         List<Integer> lottoNumbers = new ArrayList<>();
 
-        for (int number = Constants.MIN_LOTTO; number <= Constants.MAX_LOTTO; number++) {
+        for (int number = LottoBall.MIN_LOTTO; number <= LottoBall.MAX_LOTTO; number++) {
             lottoNumbers.add(number);
         }
 
         Collections.shuffle(lottoNumbers);
 
-        return lottoNumbers.subList(0, Constants.LOTTO_SIZE);
+        return lottoNumbers.subList(0, Lotto.LOTTO_SIZE);
     }
 
 }
