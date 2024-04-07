41e61ef86f397d0fc6502dd9c8206b70ea0b0a78
유지웅 <poujis0@gmail.com>
2024-04-07 23:42:58 +0900

    [fix] 수동 구매 갯수 및 번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,12 @@
 package lotto.view;
 
+import static lotto.LottoType.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import lotto.Lotto;
+import lotto.Lottos;
 
 public class InputView {
     private static Scanner scanner;
@@ -23,6 +29,23 @@ public class InputView {
         scanner = new Scanner(System.in);
         System.out.println("보너스 볼을 입력해주세요.");
         return scanner.nextLine();
+    }
+
+    public static int inputManualLottoCount(){
+        scanner = new Scanner(System.in);
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    public static Lottos inputManualLottoNumbers(int count){
+        scanner = new Scanner(System.in);
+        System.out.println("수동으로 구매할 번호를 입력해 주세요");
+
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0;i<count; i++){
+            lottos.add(Lotto.of(MANUAL, scanner.nextLine().split(WINNING_NUMBERS_SEPARATOR)));
+        }
+        return Lottos.of(lottos);
     }
 
 
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -6,11 +6,11 @@ import java.util.Arrays;
 import lotto.LottoRank;
 import lotto.LottoResult;
 import lotto.Lottos;
+import lotto.PurchaseAmount;
 
 public class ResultView {
 
-    public static void printLottosCount(int purchaseCount, Lottos lottos) {
-        System.out.println(purchaseCount + "개를 구매했습니다.");
+    public static void printLottosCount(Lottos lottos) {
         lottos.getLottos().forEach(System.out::println);
     }
 
@@ -25,8 +25,13 @@ public class ResultView {
                     result.countRank(rank)));
     }
 
-    public static void printReturnRate(int payed, LottoResult result) {
-        System.out.println("총 수익률은 " + result.calculateReturnRate(payed) + " 입니다.");
+    public static void printReturnRate(PurchaseAmount amount, LottoResult result) {
+        System.out.println("총 수익률은 " + result.calculateReturnRate(amount.getTotal()) + " 입니다.");
+    }
+
+    public static void printPurchaseLottoByEachType(PurchaseAmount amount) {
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.\n",
+            amount.getManual(), amount.getAuto());
     }
 
 }
