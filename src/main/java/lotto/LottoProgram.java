a64d692cb5737aeaa8300656286548f0c9c3e6c2
유지웅 <poujis0@gmail.com>
2024-04-07 23:43:38 +0900

    [refactor] LottoProgram 리팩토링

--- src/main/java/lotto/LottoProgram.java
+++ src/main/java/lotto/LottoProgram.java
@@ -1,26 +1,40 @@
 package lotto;
 
+import static lotto.view.InputView.*;
 import static lotto.view.InputView.inputBonusNumber;
+import static lotto.view.InputView.inputManualLottoCount;
 import static lotto.view.InputView.inputPurchaseAmount;
 import static lotto.view.InputView.inputWinningNumbers;
 import static lotto.view.ResultView.printLottosCount;
+import static lotto.view.ResultView.printPurchaseLottoByEachType;
 import static lotto.view.ResultView.printReturnRate;
 import static lotto.view.ResultView.printWinningResult;
 
 public class LottoProgram {
 
     public void run() {
-        Money purchaseAmount = new Money(inputPurchaseAmount());
+        Money money = new Money(inputPurchaseAmount());
+        PurchaseAmount amount = PurchaseAmount.of(money, inputManualLottoCount());
 
-        Lottos lottos = Lottos.create(
-            purchaseAmount, new RandomNumberGenerator());
-        printLottosCount(purchaseAmount.purchase(), lottos);
+        Lottos lottos = buyLotto(amount);
+        printPurchaseLottoByEachType(amount);
+        printLottosCount(lottos);
 
-        WinningLotto winningLotto = new WinningLotto(
-            inputWinningNumbers(), inputBonusNumber());
+        WinningLotto winningLotto = new WinningLotto(inputWinningNumbers(), inputBonusNumber());
 
         LottoResult lottoResult = winningLotto.getWinningResult(lottos);
         printWinningResult(lottoResult);
-        printReturnRate(purchaseAmount.purchase(), lottoResult);
+        printReturnRate(amount, lottoResult);
+    }
+
+    private Lottos buyLotto(PurchaseAmount amount) {
+        Lottos lottos = Lottos.of();
+        lottos.add(
+            Lottos.create(
+                amount.getManual(),
+                new ManualNumberGenerator(inputManualLottoNumbers(amount.getManual())
+                )));
+        lottos.add(Lottos.create(amount.getAuto(), new RandomNumberGenerator()));
+        return lottos;
     }
 }
