2a888311da0126d333a73fe14964c19260b6262b
Duyan Kim <devduyan@gmail.com>
2024-04-07 22:25:47 +0900

    chore: `RankTest` DisplayName을 도메인 용어로 수정

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -9,14 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RankTest {
 
-    @DisplayName("당첨번호 갯수에 일치하는 타입이 존재하면 해당 enum을 반환한다 (보너스 당첨 안된 경우)")
+    @DisplayName("당첨번호 갯수에 일치하는 타입이 존재하면 해당 등수를 반환한다 (보너스 당첨 안된 경우)")
     @ParameterizedTest
     @CsvSource(value = {"6:FIRST", "5:THIRD", "4:FOURTH", "3:FIFTH", "2:MISS", "1:MISS", "0:MISS"}, delimiter = ':')
     void valueOf_getEnum_test(int matchingCount, String expected) {
         assertEquals(Rank.valueOf(matchingCount, false), Rank.valueOf(expected));
     }
 
-    @DisplayName("당첨번호 갯수에 일치하는 타입이 존재하면 해당 enum을 반환한다 (보너스 당첨의 경우)")
+    @DisplayName("당첨번호 갯수에 일치하는 타입이 존재하면 해당 등수를 반환한다 (보너스 당첨의 경우)")
     @ParameterizedTest
     @CsvSource(value = {"5:SECOND"}, delimiter = ':')
     void withBonusMatched_valueOf_getEnum_test(int matchingCount, String expected) {
