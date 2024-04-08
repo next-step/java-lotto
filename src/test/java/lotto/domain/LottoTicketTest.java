ebd52155550b886222f1012bf9be5c2805df2ac1
jjihye-hyun <jjihyehyun@gmail.com>
2024-04-09 01:56:38 +0900

    test: 로또 티켓 당첨 번호 비교 테스트

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -16,4 +16,27 @@ class LottoTicketTest {
         assertThat(lottoNums).hasSize(6);
     }
 
+    @Test
+    void 당첨번호_비교(){
+        List<Integer> ticketNumbers = new ArrayList<>();
+        ticketNumbers.add(1);
+        ticketNumbers.add(2);
+        ticketNumbers.add(3);
+        ticketNumbers.add(4);
+        ticketNumbers.add(5);
+        ticketNumbers.add(6);
+        LottoTicket lottoTicket = new LottoTicket(ticketNumbers);
+
+        List<Integer> targetNumbers = new ArrayList<>();
+        targetNumbers.add(1);
+        targetNumbers.add(2);
+        targetNumbers.add(3);
+        targetNumbers.add(4);
+        targetNumbers.add(5);
+        targetNumbers.add(7);
+
+        assertThat(lottoTicket.checkMatch(targetNumbers)).isEqualTo(5);
+    }
+
+
 }
