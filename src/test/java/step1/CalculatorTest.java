624404520604427ddbb6709b0185d5b02594af01
develiberta <develiberta@github.com>
2024-04-10 22:45:15 +0900

    feat: 실패하는 테스트 작성

--- src/test/java/step1/CalculatorTest.java
+++ src/test/java/step1/CalculatorTest.java
@@ -2,60 +2,67 @@ package step1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CalculatorTest {
+
+    Calculator calculator;
 
     @BeforeEach
     void setUp() {
-
+        Calculator calculator = new Calculator();
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1 + 2:3", "2 + 3:5", "3 + 4:7"}, delimiter = ':')
     @DisplayName("덧셈")
-    int addition() {
-        return 0;
+    void addition(String question, int result) {
+        calculator.calculate(question);
+        assertThat(calculator.result()).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1 - 1:0", "1 - 2:-1", "2 - 1:1"}, delimiter = ':')
     @DisplayName("뺄셈")
-    int substraction() {
-        return 0;
+    void substraction(String question, int result) {
+        calculator.calculate(question);
+        assertThat(calculator.result()).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2 * -1:-2", "2 * 0:0", "2 * 1:2"}, delimiter = ':')
     @DisplayName("곱셈")
-    int multiplication() {
-        return 0;
+    void multiplication(String question, int result) {
+        calculator.calculate(question);
+        assertThat(calculator.result()).isEqualTo(result);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"4 / 1:4", "4 / 2:2", "4 / 4:1"}, delimiter = ':')
     @DisplayName("나눗셈")
-    int division() {
-        return 0;
+    void division(String question, int result) {
+        calculator.calculate(question);
+        assertThat(calculator.result()).isEqualTo(result);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1 ! 2:3", "2 @ 3:6", "4 # 5:0"}, delimiter = ':')
+    @ValueSource(strings = {"1 ! 2", "2 @ 3", "4 # 5"})
     @DisplayName("사칙연산 기호 아님")
-    int notFourBasicOperations() {
-        return 0;
+    void notFourBasicOperations(String question) {
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(question));
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    @ValueSource(strings = {"  ", "\t", "\n"})
-    @DisplayName("사칙연산 기호 아님")
-    int nullOrEmptyInput(String input) {
-        return 0;
+    @ValueSource(strings = {" ", "  ", "   "})
+    @DisplayName("null 또는 공백")
+    void nullOrEmptyInput(String question) {
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(question));
     }
 
 }
