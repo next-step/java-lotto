5babec2112d16829ce25e19f8de133735a7cebd8
Jinnie-J <jjesweet@naver.com>
2024-04-11 00:24:14 +0900

    feat: 보너스번호 확인하도록 수정

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -6,11 +6,12 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
-    NONE(0, 0);
+    NONE(0, 0),
+    FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
 
     private final int countOfMatch;
     private final int reward;
@@ -26,14 +27,21 @@ public enum Rank {
                 .collect(Collectors.toList());
     }
 
-    public static Rank findRank(int countOfMatch) {
+
+    public static Rank findRank(int countOfMatch, boolean bonusNumber) {
+        if (countOfMatch == 5 & bonusNumber) {
+            return SECOND;
+        }
         return Stream.of(values())
                 .filter(match(countOfMatch))
                 .findAny()
                 .orElse(NONE);
     }
 
-    public static int findReward(int countOfMatch) {
+    public static int findReward(int countOfMatch, boolean bonusNumber) {
+        if (countOfMatch == 5 && bonusNumber) {
+            return SECOND.reward;
+        }
         return Stream.of(values())
                 .filter(match(countOfMatch))
                 .findAny()
