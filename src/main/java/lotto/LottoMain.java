b66f5ec27acdef87e91a30b905245f468b4c80b9
pado0 <bgshhd95@gmail.com>
2024-04-11 18:23:30 +0900

    refact:[step4] main 메서드 추출 (15라인 제약), 일부 메서드명 변경

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -6,29 +6,19 @@ import lotto.view.OutputView;
 
 import java.util.List;
 
-public class AutoLottoMain {
+public class LottoMain {
     public static void main(String[] args) {
-
-        // 로또 구매 금액 입력받기
         int amount = InputView.scanAmount();
+        Lottos manualLottos = InputView.scanManualNumbers(InputView.scanManualCount());
 
-        // 수동로또 입력받기
-        int manualLottoCount = InputView.scanManualCount();
-        Lottos manualLottos = InputView.scanManualNumbers(manualLottoCount);
-
-        // 수동 및 자동 로또 구매하기
         LottoStore lottoStore = new LottoStore();
         List<Lotto> lottos = lottoStore.buyLottos(amount, manualLottos);
         Lottos issuedLottos = new Lottos(lottos);
         OutputView.printBuyLottos(lottos);
 
-        // 당첨번호 확인하기
-        List<Integer> winNumbers = InputView.scanWinningNumbers();
-        Lotto winner = new Lotto(winNumbers);
-        int bonusNumber = InputView.scanBonusNumber();
-        WinningLotto winningLotto = new WinningLotto(winner, bonusNumber);
+        Lotto winner = new Lotto(InputView.scanWinningNumbers());
+        WinningLotto winningLotto = new WinningLotto(winner, InputView.scanBonusNumber());
 
-        // 당첨 결과 확인하기
         LottoResult lottoResult = issuedLottos.winningResult(winningLotto);
         OutputView.printWinningStatic(lottoResult);
         OutputView.printReturnRate(lottoResult.returnRate(amount));
