4d279f37475815e88b27ac7ea591d1dbbd44625d
동원 <ai06021@gmail.com>
2024-04-11 15:54:52 +0900

    refactor : error condition add

--- src/main/java/domain/TotalTry.java
+++ src/main/java/domain/TotalTry.java
@@ -5,10 +5,14 @@ public class TotalTry {
     private int manualTry;
 
     public TotalTry(int totalTry) {
+        if (totalTry < 0)
+            throw new IllegalArgumentException("0이상의 갯수를 필요로 합니다");
         this.totalTry = totalTry;
     }
 
     public void minus(int manualTry) {
+        if (manualTry < 0)
+            throw new IllegalArgumentException("0이상의 갯수를 필요로 합니다");
         this.manualTry = manualTry;
     }
 
