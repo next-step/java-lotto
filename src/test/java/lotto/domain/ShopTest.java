d7ddee2ad06fe7f63e39df3eec6394d3514f0526
jaehyeoklee <leejh9049@gmail.com>
2024-04-12 21:53:17 +0900

    [test] Shop 메서드 변경에 따른 기존 테스트 케이스 수정

--- src/test/java/lotto/domain/ShopTest.java
+++ src/test/java/lotto/domain/ShopTest.java
@@ -5,6 +5,9 @@ import lotto.service.Shop;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,13 +15,16 @@ public class ShopTest {
     @Test
     @DisplayName("로또 구매 테스트")
     public void purchaseLottoTest() {
-        assertThat(Shop.purchaseLotto(3000).size()).isEqualTo(3);
+        Purchase purchase = Shop.createLottoPurchase(3000);
+        List<String> manualInputLottoNumbers = new ArrayList<>();
+
+        assertThat(Shop.purchaseLotto(purchase, manualInputLottoNumbers).size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("로또 구매 금액 오류 테스트 - 음수 입력")
     public void purchaseLottoNegativeNumError() {
-        assertThatThrownBy(() -> Shop.purchaseLotto(-1000).size())
+        assertThatThrownBy(() -> Shop.createLottoPurchase(-1000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.NEGATIVE_PURCHASE_PRICE_ERROR.getMessage());
     }
@@ -26,7 +32,7 @@ public class ShopTest {
     @Test
     @DisplayName("로또 구매 금액 오류 테스트 - 1000 단위 예외 입력")
     public void purchaseLotto1000UnitErrorTest() {
-        assertThatThrownBy(() -> Shop.purchaseLotto(3500))
+        assertThatThrownBy(() -> Shop.createLottoPurchase(3500))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.PURCHASE_SINGULAR_PRICE_ERROR.getMessage());
     }
