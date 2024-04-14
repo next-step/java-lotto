5d94cf185f7cf40bf56439fbaea6753881041bb4
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-04-15 00:02:12 +0900

    feat: 수동 로또 개수 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 	public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
 	public static final String INPUT_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 	public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+	private static final String INPUT_MANUAL_LOTTO_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";;
 	Scanner scanner = new Scanner(System.in);
 
 	public int inputMoney(){
@@ -20,6 +21,11 @@ public class InputView {
 
 	public int inputBonusNumber(){
 		System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+		return scanner.nextInt();
+	}
+	
+	public int inputManualLottoNumberCount() {
+		System.out.println(INPUT_MANUAL_LOTTO_MESSAGE);
 		return scanner.nextInt();
 	}
 
