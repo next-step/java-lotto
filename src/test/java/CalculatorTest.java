9986e5b5e83cb5e15f3b4d1420c120d39643ed7f
jaejeong1 <wowjd555@gmail.com>
2024-05-07 14:38:25 +0900

    feat: 요구사항 정의

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void 덧셈() {}
+
+    @Test
+    public void 뺄셈() {}
+
+    @Test
+    public void 곱셈() {}
+
+    @Test
+    public void 나눗셈() {}
+
+    @Test
+    public void 입력값이null이거나_빈공백문자일경우_IllegalArgumentException을_throw한다() {}
+
+    @Test
+    public void 사칙연산기호가_아닌경우_IllegalArgumentException을_throw한다() {}
+
+    @Test
+    public void 사칙연산의_계산우선순위가아닌_입력값에따라_계산순서가결정된다() {}
+}
