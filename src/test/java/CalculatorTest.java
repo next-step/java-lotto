0121fd5bb469f6cc44fb547738b0d0c53887b95c
jaejeong1 <wowjd555@gmail.com>
2024-05-07 14:47:08 +0900

    feat: 테스트 작성

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,24 +1,51 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
     @Test
-    public void 덧셈() {}
+    public void 덧셈() {
+        int result = Calculator.calculate("1 + 1");
+        assertThat(result).isEqualTo(2);
+    }
 
     @Test
-    public void 뺄셈() {}
+    public void 뺄셈() {
+        int result = Calculator.calculate("1 - 1");
+        assertThat(result).isEqualTo(0);
+    }
 
     @Test
-    public void 곱셈() {}
+    public void 곱셈() {
+        int result = Calculator.calculate("1 * 1");
+        assertThat(result).isEqualTo(1);
+    }
 
     @Test
-    public void 나눗셈() {}
+    public void 나눗셈() {
+        int result = Calculator.calculate("3 / 2");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void 입력값이null이거나_빈공백문자일경우_IllegalArgumentException을_throw한다(String input) {
+        assertThatThrownBy(IllegalArgumentException.class, () -> Calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 ! 1", "1 ^ 1", "1 # 1"})
+    public void 사칙연산기호가_아닌경우_IllegalArgumentException을_throw한다(String input) {
+        assertThatThrownBy(IllegalArgumentException.class, () -> Calculator.calculate(input));
+    }
 
     @Test
-    public void 입력값이null이거나_빈공백문자일경우_IllegalArgumentException을_throw한다() {}
-
-    @Test
-    public void 사칙연산기호가_아닌경우_IllegalArgumentException을_throw한다() {}
-
-    @Test
-    public void 사칙연산의_계산우선순위가아닌_입력값에따라_계산순서가결정된다() {}
+    public void 사칙연산의_계산우선순위가아닌_입력값에따라_계산순서가결정된다() {
+        int result = Calculator.calculate("2 + 3 * 4 / 2");
+        assertThat(result).isEqualTo(10);
+    }
 }
