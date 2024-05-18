ccf0c0d60ab64ca96c79ee46682d94ceda91f2c9
kgi912 <rudrbdlf1123@gmail.com>
2024-05-18 10:33:59 +0900

    refactor : 테스트 코드 리팩토링

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -1,9 +1,9 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoStoreTest {
 
@@ -11,12 +11,12 @@ class LottoStoreTest {
     @DisplayName("구입한 금액으로 계산된 갯수 만큼 생성 확인")
     void calculateCount(){
         LottoStore lottoStore = new LottoStore();
-        Lotto lotto1 = lottoStore.buyAutoLotto(3000);
-        Lotto lotto2 = lottoStore.buyAutoLotto(5000);
-        Lotto lotto3 = lottoStore.buyAutoLotto(7000);
+        Lotto lotto1 = lottoStore.buyAutoLotto(500);
+        Lotto lotto2 = lottoStore.buyAutoLotto(1500);
+        Lotto lotto3 = lottoStore.buyAutoLotto(5000);
 
-        assertThat(lotto1.lotto().size()).isEqualTo(3);
-        assertThat(lotto2.lotto().size()).isEqualTo(5);
-        assertThat(lotto3.lotto().size()).isEqualTo(7);
+        assertThat(lotto1.lotto().size()).isEqualTo(0);
+        assertThat(lotto2.lotto().size()).isEqualTo(1);
+        assertThat(lotto3.lotto().size()).isEqualTo(5);
     }
 }
