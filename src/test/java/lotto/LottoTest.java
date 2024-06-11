4ab0fed60864a9eea97216f3e9754c2f1f55e911
Long9725 <develop9725@gmail.com>
2024-06-11 10:52:27 +0900

    refactor: matchTest

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -42,93 +42,24 @@ class LottoTest {
         assertThatIllegalArgumentException().isThrownBy(() -> new Lotto(numbers));
     }
 
-    @DisplayName("당첨 로또와 2개 숫자 이하는 꽝")
-    @ParameterizedTest
+    @DisplayName("로또 당첨 테스트")
+    @ParameterizedTest(name = "{displayName} - {3}")
     @CsvSource({
-            "1 2 3 4 5 6, 1 2 9 10 11 12, 13"
+            "1 2 3 4 5 6, 1 2 3 4 5 6, 7, FIRST",
+            "1 2 3 4 5 6, 1 2 3 4 5 12, 6, SECOND",
+            "1 2 3 4 5 6, 1 2 3 4 5 12, 13, THIRD",
+            "1 2 3 4 5 6, 1 2 3 4 11 12, 13, FOURTH",
+            "1 2 3 4 5 6, 1 2 3 10 11 12, 13, FIFTH",
+            "1 2 3 4 5 6, 1 2 9 10 11 12, 13, NONE",
     })
-    void underTwoMatched(
+    void matchTest(
             @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
             @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
-            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus
+            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus,
+            LottoRank lottoRank
     ) {
         Lotto lotto = new Lotto(numbers);
         WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
-        assertThat(lotto.match(winningLotto)).isEqualTo(LottoRank.NONE);
-    }
-
-    @DisplayName("당첨 로또와 3개 숫자가 일치")
-    @ParameterizedTest
-    @CsvSource({
-            "1 2 3 4 5 6, 1 2 3 10 11 12, 13"
-    })
-    void threeMatched(
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
-            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus
-    ) {
-        Lotto lotto = new Lotto(numbers);
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
-        assertThat(lotto.match(winningLotto)).isEqualTo(LottoRank.FIFTH);
-    }
-
-    @DisplayName("당첨 로또와 4개 숫자가 일치")
-    @ParameterizedTest
-    @CsvSource({
-            "1 2 3 4 5 6, 1 2 3 4 11 12, 13"
-    })
-    void fourMatched(
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
-            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus
-    ) {
-        Lotto lotto = new Lotto(numbers);
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
-        assertThat(lotto.match(winningLotto)).isEqualTo(LottoRank.FOURTH);
-    }
-
-    @DisplayName("당첨 로또와 5개 숫자가 일치")
-    @ParameterizedTest
-    @CsvSource({
-            "1 2 3 4 5 6, 1 2 3 4 5 12, 13"
-    })
-    void fiveMatched(
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
-            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus
-    ) {
-        Lotto lotto = new Lotto(numbers);
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
-        assertThat(lotto.match(winningLotto)).isEqualTo(LottoRank.THIRD);
-    }
-
-    @DisplayName("당첨 로또와 5개 숫자와 1개 보너스 숫자가 일치")
-    @ParameterizedTest
-    @CsvSource({
-            "1 2 3 4 5 6, 1 2 3 4 5 12, 6"
-    })
-    void fiveAndBonusMatched(
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
-            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus
-    ) {
-        Lotto lotto = new Lotto(numbers);
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
-        assertThat(lotto.match(winningLotto)).isEqualTo(LottoRank.SECOND);
-    }
-
-    @DisplayName("당첨 로또와 6개 숫자가 일치")
-    @ParameterizedTest(name = "당첨 로또와 6개 숫자가 일치")
-    @CsvSource({
-            "1 2 3 4 5 6, 1 2 3 4 5 6, 7"
-    })
-    void sixMatched(
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
-            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
-            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus
-    ) {
-        Lotto lotto = new Lotto(numbers);
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
-        assertThat(lotto.match(winningLotto)).isEqualTo(LottoRank.FIRST);
+        assertThat(lotto.match(winningLotto)).isEqualTo(lottoRank);
     }
 }
