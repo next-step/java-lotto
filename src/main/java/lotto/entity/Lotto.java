42633236a3c6e785a6d33ebf0e90d32a14b08a7f
hyoin <hyoin01@gmail.com>
2024-06-15 17:59:48 +0900

    feat(lotto): add lotto
    1~45의 6개의 숫자를 가지는 로또 클래스 구현

---
+++ src/main/java/lotto/entity/Lotto.java
@@ -0,0 +1,35 @@
+package lotto.entity;
+
+import java.util.List;
+
+public class Lotto {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+
+    public static final String LOTTO_NUMBERS_ARE_6_NUMBER = "로또번호는 6개입니다.";
+    public static final String LOTTO_NUMBERS_ARE_NUMBERS_1_TO_45 = "로또번호는 1~45의 숫자입니다.";
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private static void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_ARE_6_NUMBER);
+        }
+
+        numbers.forEach(integer -> {
+            if (integer < MIN_LOTTO_NUMBER || integer > MAX_LOTTO_NUMBER) {
+                throw new IllegalArgumentException(LOTTO_NUMBERS_ARE_NUMBERS_1_TO_45);
+            }
+        });
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
---
+++ src/test/java/lotto/entity/LottoTest.java
@@ -0,0 +1,50 @@
+package lotto.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.entity.Lotto.LOTTO_NUMBERS_ARE_6_NUMBER;
+import static lotto.entity.Lotto.LOTTO_NUMBERS_ARE_NUMBERS_1_TO_45;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+    @Test
+    void lotto() {
+        var lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.getNumbers().size())
+                .isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("로또는 6개의 숫자만 가져야 한다.")
+    void illegal_lotto1() {
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5, 6, 7));
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NUMBERS_ARE_6_NUMBER);
+    }
+
+    @Test
+    @DisplayName("로또는 6개의 숫자만 가져야 한다.")
+    void illegal_lotto2() {
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(1, 2, 3, 4, 5));
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NUMBERS_ARE_6_NUMBER);
+    }
+
+    @Test
+    @DisplayName("로또는 1~45의 숫자만 가져야 한다.")
+    void illegal_lotto3() {
+        assertThatThrownBy(() -> {
+            new Lotto(List.of(0, 2, 3, 4, 5, 46));
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(LOTTO_NUMBERS_ARE_NUMBERS_1_TO_45);
+    }
+}
