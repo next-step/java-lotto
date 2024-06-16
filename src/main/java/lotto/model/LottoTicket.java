6c233f143d1d116f6b57e298f2f9479461a2f7a7
Yeongseo Kim <dudtjakdl@gmail.com>
2024-06-17 00:55:56 +0900

    feat: LottoNumber 로또 티켓의 숫자 범위 제한 기능 추가

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -1,19 +1,27 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoTicket {
     public static final double LOTTO_NUBERS_SIZE = 6;
-    private List<Integer> lottoNumbers;
+    private final List<LottoNumber> lottoNumbers;
 
-    public LottoTicket(List<Integer> lottoNumbers) {
+    public LottoTicket(String[] lottoNumbers) {
+        this(Arrays.stream(lottoNumbers)
+                .map(s -> LottoNumber.of(Integer.parseInt(s.trim())))
+                .collect(Collectors.toList()));
+    }
+
+    public LottoTicket(List<LottoNumber> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_NUBERS_SIZE) {
             throw new IllegalArgumentException("로또 티켓의 로또 숫자는 6개이어야 합니다.");
         }
         this.lottoNumbers = lottoNumbers;
     }
 
-    public List<Integer> getLottoNumbers() {
+    public List<LottoNumber> getLottoNumbers() {
         return lottoNumbers;
     }
 }
