e64934dd418908180a09410d741e6bef896eccfb
Yeongseo Kim <dudtjakdl@gmail.com>
2024-06-17 01:15:42 +0900

    refactor: LottoTicket 의 List<LottoNumber> -> Set<LottoNumber> 로 변환

--- src/main/java/lotto/model/LottoTicket.java
+++ src/main/java/lotto/model/LottoTicket.java
@@ -1,27 +1,27 @@
 package lotto.model;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class LottoTicket {
-    public static final double LOTTO_NUBERS_SIZE = 6;
-    private final List<LottoNumber> lottoNumbers;
+    public static final int LOTTO_NUBERS_SIZE = 6;
+    private final Set<LottoNumber> lottoNumbers;
 
-    public LottoTicket(String[] lottoNumbers) {
-        this(Arrays.stream(lottoNumbers)
-                .map(s -> LottoNumber.of(Integer.parseInt(s.trim())))
-                .collect(Collectors.toList()));
+    public LottoTicket(List<Integer> lottoNumbers) {
+        this(lottoNumbers.stream()
+                .map(number -> LottoNumber.of(number))
+                .collect(Collectors.toSet()));
     }
 
-    public LottoTicket(List<LottoNumber> lottoNumbers) {
+    public LottoTicket(Set<LottoNumber> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_NUBERS_SIZE) {
             throw new IllegalArgumentException("로또 티켓의 로또 숫자는 6개이어야 합니다.");
         }
         this.lottoNumbers = lottoNumbers;
     }
 
-    public List<LottoNumber> getLottoNumbers() {
+    public Set<LottoNumber> getLottoNumbers() {
         return lottoNumbers;
     }
 }
