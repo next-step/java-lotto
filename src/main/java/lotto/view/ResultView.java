5cb34b996d31f27661e1dd013121ff624f197440
Yeongseo Kim <dudtjakdl@gmail.com>
2024-06-23 17:57:08 +0900

    feat: 로또 통계 출력 기능 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
 import lotto.model.LottoGame;
+import lotto.model.LottoRank;
+import lotto.model.LottoResult;
+
+import java.util.Arrays;
 
 public class ResultView {
 
@@ -11,5 +15,13 @@ public class ResultView {
     public void printUserLottoTickets(LottoGame lottoGame) {
         lottoGame.getUserLottoTickets().getLottoTickets().stream()
                 .forEach(lottoTicket -> System.out.println(lottoTicket.getLottoNumbers()));
+    }
+
+    public void printLottoResult(LottoResult lottoResult, int payAmount) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        Arrays.stream(LottoRank.values())
+                .forEach(rank -> System.out.printf("%d개 일치 (%d원)- %d개\n", rank.getMatchCount(), rank.getWinningPrize(), lottoResult.getCount(rank)));
+        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n", lottoResult.getProfitRate(payAmount));
     }
 }
