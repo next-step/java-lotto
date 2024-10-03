11d95597e2b452c536331833111f0d69ab2b5a17
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-03 20:09:45 +0900

    test: 로또기능정리

---
+++ src/test/java/step2/LottoTest.java
@@ -0,0 +1,29 @@
+package step2;
+
+/*
+- 금액을 입력받으면 1000으로 나눈값을 내려준다.
+- 로또 변호 6자리를 랜덤으로 출력한다.
+- 입력받은 숫자만큼 로또번호를 출력한다.
+- 입력받은 배열을 오름차순으로 정렬한다.
+- 당첨번호와 비교하여 일치하는 번호의 개수를 출력한다.
+- 손해율을 내려준다.
+- 입력받은 금액이 1000원 이하이면 예외를 발생시킨다.
+- 로또 생성시 음수를 입력받으면 예외를 발생시킨다.
+*/
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTest {
+
+    @DisplayName("금액을 입력받으면 1000으로 나눈값을 내려준다.")
+    @Test
+    void moneyTest(){
+    }
+
+    @DisplayName("로또 변호 6자리를 랜덤으로 출력한다.")
+    @Test
+    void lottoNumTest(){
+
+    }
+}
