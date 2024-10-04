4c1f840fb5132843aced9e3380b4429cb91322d8
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-04 11:17:53 +0900

    feat: 콤마 포함 문자열 분리 메소드

---
+++ src/main/java/step2/util/StringUtil.java
@@ -0,0 +1,14 @@
+package step2.util;
+
+import java.util.Arrays;
+
+public class StringUtil {
+
+    private final static String COMMA = ",";
+
+    public static String[] divideNum(String str) {
+        return Arrays.stream(str.split(COMMA))
+                .map(String::trim)
+                .toArray(String[]::new);
+    }
+}
