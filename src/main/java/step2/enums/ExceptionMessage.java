6afe82cd7e5f745675f443991ebb421aa3975626
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-04 19:32:26 +0900

    fix: switch문 불가확인하여, Enum으로 교체

--- src/main/java/step2/enums/ExceptionMessage.java
+++ src/main/java/step2/enums/ExceptionMessage.java
@@ -4,7 +4,8 @@ public enum ExceptionMessage {
     MINIMUM_LOTTO_PRICE("로또의 가격은 1000원입니다."),
     MINIMUM_LOTTO_NUM("로또는 최소 1장이상부터 생성이 가능합니다."),
     RANGE_LOTTO_NUM("로또번호는 1~45범위안에 포함되야 합니다."),
-    NOT_MATCH_PRIZE("로또 당첨번호를 맞춰보지 않았습니다.");
+    NOT_MATCH_PRIZE("로또 당첨번호를 맞춰보지 않았습니다."),
+    NOT_MATCH_COUNT("일치하는 당첨개수가 없습니다.");
 
     ExceptionMessage(String message) {
         this.message = message;
--- src/main/java/step2/uiView/ResultView.java
+++ src/main/java/step2/uiView/ResultView.java
@@ -2,6 +2,7 @@ package step2.uiView;
 
 
 import step2.enums.Message;
+import step2.enums.PrizeMessage;
 import step2.model.Lotto;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class ResultView {
     public void lottoPrizeView(Lotto lotto) {
         System.out.println(Message.RESULT_LOTTO_STATICS.message());
         IntStream.range(3, 7).forEach(matchCount -> {
-            checkMatchCount(matchCount, lotto.getPrizeStatics().getPrize(matchCount));
+            System.out.println(PrizeMessage.matchCountMessage(matchCount, lotto.getPrizeStatics().getPrize(matchCount)));
         });
     }
 
@@ -33,20 +34,4 @@ public class ResultView {
         System.out.println(Message.RESULT_RATE_RETURN.message().replace(LOTTO_RATE, String.valueOf(lotto.rateOfReturn())));
     }
 
-    private void checkMatchCount(int matchCount, int count) {
-        switch (matchCount) {
-            case 3:
-                System.out.println(Message.RESULT_THREE_CORRECT.message().replace(LOTTO_COUNT, String.valueOf(count)));
-                break;
-            case 4:
-                System.out.println(Message.RESULT_FOUR_CORRECT.message().replace(LOTTO_COUNT, String.valueOf(count)));
-                break;
-            case 5:
-                System.out.println(Message.RESULT_FIVE_CORRECT.message().replace(LOTTO_COUNT, String.valueOf(count)));
-                break;
-            case 6:
-                System.out.println(Message.RESULT_SIX_CORRECT.message().replace(LOTTO_COUNT, String.valueOf(count)));
-                break;
-        }
-    }
 }
