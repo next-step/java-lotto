910b0d6060e631b22b42c02bddaae9f9582b334b
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-06 01:11:09 +0900

    fix: Enum의 역할에 맞게 이름분리

--- src/main/java/step2/enums/ResultMessage.java
+++ src/main/java/step2/enums/ResultMessage.java
@@ -1,14 +1,12 @@
 package step2.enums;
 
-public enum Message {
+public enum ResultMessage {
 
-    INPUT_MONEY("구입금액을 입력해 주세요."),
-    INPUT_PRIZE_NUM("지난 주 당첨 번호를 입력해 주세요."),
     RESULT_LOTTO_COUNT("{count}개를 구매했습니다."),
     RESULT_LOTTO_STATICS("당첨통계"),
     RESULT_RATE_RETURN("총 수익률은 {rate}입니다.");
 
-    Message(String message) {
+    ResultMessage(String message) {
         this.message = message;
     }
 
