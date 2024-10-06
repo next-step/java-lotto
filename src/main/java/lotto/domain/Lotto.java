ed6ce6917fa5c436d7dd9e31add9bfdeb192683c
손병일 <byungilsohn@sonbyeong-ils-MacBook-Pro.local>
2024-10-06 15:40:52 +0900

    feat : implement Lotto class
    implement Lotto Class static method for string parameter

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,16 +2,34 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Lotto {
 
+    private static final String DELIMITER = ",";
+
     private final List<Integer> lotto;
 
     private Lotto(List<Integer> lotto) {
-        this.lotto = lotto;
+        this.lotto = Collections.unmodifiableList(lotto);
+    }
+
+    public static Lotto of(String s) {
+        String[] numbers = Objects.requireNonNull(s).split(DELIMITER);
+        validateNumbers(numbers);
+
+        return new Lotto(Stream.of(numbers)
+                .map(str -> Integer.parseInt(str.trim()))
+                .collect(Collectors.toList()));
+    }
+
+    private static void validateNumbers(String[] numbers) {
+        if (Stream.of(numbers).anyMatch(String::isBlank) || numbers.length != 6) {
+            throw new IllegalArgumentException("6개 숫자를 입력하셔야 합니다.");
+        }
     }
 
     public static Lotto create() {
@@ -23,7 +41,7 @@ public class Lotto {
                 .boxed().collect(Collectors.toList());
         prepare(lottoNumbers);
 
-        return Collections.unmodifiableList(lottoNumbers.subList(0, 6));
+        return lottoNumbers.subList(0, 6);
     }
 
     private static void prepare(List<Integer> lottoNumbers) {
@@ -33,5 +51,10 @@ public class Lotto {
 
     public Stream<Integer> stream() {
         return lotto.stream();
+    }
+
+    @Override
+    public String toString() {
+        return lotto.toString();
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Collectors;
 
@@ -9,12 +12,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoTest {
 
     @Test
-    void 로또_생성() {
+    void 로또_생성_무작위() {
         Lotto lotto = Lotto.create();
 
         assertThat(lotto.stream().allMatch(number -> number > 0 && number < 46))
                 .isTrue();
         assertThat(lotto.stream().collect(Collectors.toList()))
                 .isSorted();
+    }
+
+    @Test
+    void 로또_생성_문자열() {
+        Lotto lotto = Lotto.of("1, 2, 3, 4, 5, 6");
+
+        assertThat(lotto.stream().allMatch(number -> number > 0 && number < 46))
+                .isTrue();
+    }
+
+    @Test
+    void 로또_생성_문자열_null() {
+        Assertions.assertThatNullPointerException().isThrownBy(() -> Lotto.of(null));
+    }
+
+    @Test
+    void 로또_생성_문자열_빈문자() {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(""));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5", "1,2,3,4,5,6,7"})
+    void 로또_생성_문자열_6개_아님(String text) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Lotto.of(text));
     }
 }
