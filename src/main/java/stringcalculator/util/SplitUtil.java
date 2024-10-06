9534183ceecbbc83d97919e02fe149cbc28bb759
mitsui3point <rladbsgh0331@gmail.com>
2024-10-07 02:55:58 +0900

    refactor(stringcalculator): modify SplitUtil split method(공백 문자 " " 상수화)

--- src/main/java/stringcalculator/util/SplitUtil.java
+++ src/main/java/stringcalculator/util/SplitUtil.java
@@ -2,10 +2,12 @@ package stringcalculator.util;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SplitUtil {
+
+    public static final String SPACE = " ";
+
     public static List<String> split(String input) {
-        return Arrays.asList(input.split(" "));
+        return Arrays.asList(input.split(SPACE));
     }
 }
