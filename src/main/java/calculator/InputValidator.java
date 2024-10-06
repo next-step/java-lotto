b612c67f807d7171be6e86669a268035c03bc8c1
owljoa <needshield@gmail.com>
2024-10-06 19:01:43 +0900

    feat: 입력값 유효성 검사 - 입력된 문자열 숫자와 사칙 연산자, 공백 문자 이외의 문자 여부 검사

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -1,13 +1,36 @@
 package calculator;
 
+import java.util.Set;
+
 public class InputValidator {
+
+    public static final Set<Character> OPERATORS = Set.of('+', '-', '*', '/');
+
     public static void validate(String input) {
         validateBlank(input);
+        validateExpression(input);
     }
 
     private static void validateBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("빈 문자열이나 공백으로 구성된 문자열은 허용되지 않습니다");
         }
+    }
+
+    private static void validateExpression(String input) {
+        for (int index = 0; index < input.length(); index++) {
+            Character character = input.charAt(index);
+            if (isInValidExpression(character)) {
+                throw new IllegalArgumentException("숫자, 사칙연산자(+, -, *, /), 공백 이외의 문자는 허용되지 않습니다");
+            }
+        }
+    }
+
+    private static boolean isInValidExpression(Character character) {
+        return !isValidExpression(character);
+    }
+
+    private static boolean isValidExpression(Character character) {
+        return Character.isDigit(character) || OPERATORS.contains(character) || Character.isWhitespace(character);
     }
 }
--- src/test/java/calculator/InputValidatorTest.java
+++ src/test/java/calculator/InputValidatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,5 +12,15 @@ public class InputValidatorTest {
         // when, then
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> InputValidator.validate(input))
                         .withMessageMatching("빈 문자열이나 공백으로 구성된 문자열은 허용되지 않습니다");
+    }
+
+    @Test
+    void 숫자_사칙연산자_공백문자_이외의_문자_포함되어있으면_예외_발생() {
+        // given
+        String input = "1 + 2 ! 3";
+
+        // when, then
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> InputValidator.validate(input))
+                .withMessage("숫자, 사칙연산자(+, -, *, /), 공백 이외의 문자는 허용되지 않습니다");
     }
 }
