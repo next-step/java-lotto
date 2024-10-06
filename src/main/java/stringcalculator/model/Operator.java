90b0879b7113148eb6088a8ff6f4d712e7e3e673
mitsui3point <rladbsgh0331@gmail.com>
2024-10-07 02:36:13 +0900

    refactor(stringcalculator): modify Operator calculate method modify Operator enum constructor

--- src/main/java/stringcalculator/model/Operator.java
+++ src/main/java/stringcalculator/model/Operator.java
@@ -1,19 +1,20 @@
 package stringcalculator.model;
 
+import java.util.function.BiFunction;
+
 public enum Operator {
-    SUM, SUBTRACT, MULTIPLY, DIVIDE;
+    SUM((left, right) -> left + right),
+    SUBTRACT((left, right) -> left - right),
+    MULTIPLY((left, right) -> left * right),
+    DIVIDE((left, right) -> left / right);
 
-    public int calculate(int operandLeft, int operandRight) {
+    private final BiFunction<Integer, Integer, Integer> calculate;
 
-        if (this.equals(SUM)) {
-            return operandLeft + operandRight;
-        }
-        if (this.equals(SUBTRACT)) {
-            return operandLeft - operandRight;
-        }
-        if (this.equals(MULTIPLY)) {
-            return operandLeft * operandRight;
-        }
-        return operandLeft / operandRight;
+    Operator(BiFunction<Integer, Integer, Integer> calculate) {
+        this.calculate = calculate;
+    }
+
+    public int calculate(int left, int right) {
+        return calculate.apply(left, right);
     }
 }
