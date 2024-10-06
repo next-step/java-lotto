239a88346497c12b79c66c49d5167120bf9ee8db
mitsui3point <rladbsgh0331@gmail.com>
2024-10-07 03:54:29 +0900

    docs(stringcalculator): modify TODO 문자열을 자른다., 문자열 size 는 홀수이다, 문자열 size 는 홀수가 아니면 예외처리한다
    feat(stringcalculator): modify SplitUtil split method
    
    test(stringcalculator): modify SplitUtilTest 문자열_size_는_홀수가_아니면_예외처리한다 method

--- src/main/java/stringcalculator/util/SplitUtil.java
+++ src/main/java/stringcalculator/util/SplitUtil.java
@@ -6,8 +6,24 @@ import java.util.List;
 public class SplitUtil {
 
     public static final String SPACE = " ";
+    public static final String NOT_ALLOW_BELOW_THREE_OR_LIST_SIZE_EVEN_NUMBER = "잘못된 문자열을 입력했습니다.(공백 2개 이상, 숫자 연산자 숫자(2 * 3) 형태)";
+    public static final int MIN_SIZE = 3;
+    public static final int EVEN_CHECK_NUMBER = 2;
+    public static final int ZERO = 0;
 
-    public static List<String> split(String input) {
-        return Arrays.asList(input.split(SPACE));
+    public static List<String> split(final String input) {
+        List<String> result = Arrays.asList(input.trim()
+                .split(SPACE));
+
+        if (isInvalidateSize(result.size())) {
+            throw new IllegalArgumentException(NOT_ALLOW_BELOW_THREE_OR_LIST_SIZE_EVEN_NUMBER);
+        }
+
+        return result;
+    }
+
+    private static boolean isInvalidateSize(int size) {
+        return size < MIN_SIZE ||
+                size % EVEN_CHECK_NUMBER == ZERO;
     }
 }
--- src/test/java/stringcalculator/util/SplitUtilTest.java
+++ src/test/java/stringcalculator/util/SplitUtilTest.java
@@ -1,16 +1,28 @@
 package stringcalculator.util;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static stringcalculator.util.SplitUtil.*;
 
 public class SplitUtilTest {
     @Test
     void 문자열을_자른다() {
         String input = "2 + 3 * 4 / 2";
-        List<String> actual = SplitUtil.split(input);
+        List<String> actual = split(input);
         List<String> expected = List.of("2", "+", "3", "*", "4", "/", "2");
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 문자열_size_는_홀수가_아니면_예외처리한다() {
+        assertThatThrownBy(() -> {
+            String input = "2 + 3 * 4 / ";
+            split(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_ALLOW_BELOW_THREE_OR_LIST_SIZE_EVEN_NUMBER);
     }
 }
