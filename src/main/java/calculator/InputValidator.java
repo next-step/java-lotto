f6db95713287806959ea14fd27fdd02f9674fff0
owljoa <needshield@gmail.com>
2024-10-06 19:01:43 +0900

    feat: 입력값 유효성 검사 - 입력된 문자열 숫자와 사칙 연산자, 공백 문자 이외의 문자 여부 검사

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -1,51 +1,14 @@
 package calculator;
 
-import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
+
+    public static final Set<Character> OPERATORS = Set.of('+', '-', '*', '/');
+
     public static void validate(String input) {
         validateBlank(input);
         validateExpression(input);
-    }
-
-    public static void validate(List<String> inputItems) {
-        validateFirstItemIsNumber(inputItems);
-        for (String item : inputItems) {
-            validateSpaceBetweenItems(item);
-        }
-        validateSequentialItem(inputItems);
-    }
-
-    private static void validateSpaceBetweenItems(String item) {
-        if (isNotOperatorOrNumber(item)) {
-            throw new IllegalArgumentException("숫자, 사칙연산자(+, -, *, /) 사이에는 공백이 있어야 합니다");
-        }
-    }
-
-    private static void validateFirstItemIsNumber(List<String> items) {
-        InputItemType firstItemType = InputItemType.from(items.get(0));
-        if (!items.isEmpty() && firstItemType != InputItemType.NUMBER) {
-            throw new IllegalArgumentException("가장 첫 항목은 숫자이어야 합니다");
-        }
-    }
-
-    private static void validateSequentialItem(List<String> items) {
-        InputItemType prevItemType = null;
-
-        for (String item : items) {
-            InputItemType currentItemType = InputItemType.from(item);
-
-            if (prevItemType == currentItemType) {
-                throw new IllegalArgumentException("숫자나 사칙연산자(+, -, *, /)는 연속되어 입력될 수 없습니다");
-            }
-
-            prevItemType = currentItemType;
-        }
-    }
-
-    private static boolean isNotOperatorOrNumber(String item) {
-        InputItemType itemType = InputItemType.from(item);
-        return !itemType.isOperator() && !itemType.isNumber();
     }
 
     private static void validateBlank(String input) {
@@ -68,7 +31,6 @@ public class InputValidator {
     }
 
     private static boolean isValidExpression(Character character) {
-        String characterString = character.toString();
-        return InputItemType.from(characterString) != InputItemType.UNKNOWN;
+        return Character.isDigit(character) || OPERATORS.contains(character) || Character.isWhitespace(character);
     }
 }
