126ee1a9b5f49ec00f3a30ebab869435ff5c039a
SangHyun Kim <ksh9891@gmail.com>
2024-10-06 15:27:53 +0900

    feat(calculator): 허용되지 않는 문자열이 들어온 경우 예외를 반환하도록 작업

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
+    public static final Pattern PERMIT_INPUT_REGEX = Pattern.compile("^[0-9+\\-*/ ]+$");
+
     public static int calculate(final String userInput) {
+        final Matcher matcher = PERMIT_INPUT_REGEX.matcher(userInput);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("허용되지 않는 문자가 입력되었습니다.");
+        }
+
         return Integer.parseInt(userInput);
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
     @Test
@@ -12,5 +13,14 @@ public class CalculatorTest {
         final String userInput = "3";
 
         assertThat(Calculator.calculate(userInput)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("숫자, 공백, 부호(+, -, *, /) 외 문자가 입력된 경우 IllegalArgumentException 예외가 발생한다.")
+    void test02() {
+        final String userInput = "1 _ 3";
+
+        assertThatThrownBy(() -> Calculator.calculate(userInput))
+            .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
