3ec0ba03882e532eab2c63cc91c5dd6a08f609a9
Byungil Sohn <skylord418@gmail.com>
2024-10-06 20:58:18 +0900

    feat : add result view to report winning statistics

--- src/main/java/lotto/ConsoleLottoSalesStore.java
+++ src/main/java/lotto/ConsoleLottoSalesStore.java
@@ -1,14 +1,25 @@
 package lotto;
 
-import lotto.domain.LottoBill;
-import lotto.domain.LottoBundle;
+import lotto.domain.number.Lotto;
+import lotto.domain.prize.WinningReport;
+import lotto.domain.sales.LottoBill;
+import lotto.domain.sales.LottoBundle;
 import lotto.view.InputView;
+import lotto.view.ResultView;
 
 public class ConsoleLottoSalesStore {
 
     public static void main(String[] args) {
-        LottoBill bill = new InputView().sell();
+        InputView inputView = new InputView();
+        LottoBill bill = inputView.sell();
         LottoBundle lottoBundle = LottoBundle.of(bill.quantity());
-        System.out.println(lottoBundle);
+
+        ResultView resultView = new ResultView();
+        resultView.show(lottoBundle);
+
+        Lotto lastWinningLotto = inputView.lastWinningLotto();
+        WinningReport winningReport = WinningReport.of(bill.salesAmount(), lottoBundle, lastWinningLotto);
+
+        resultView.report(winningReport);
     }
 }
---
+++ src/main/java/lotto/view/ResultView.java
@@ -0,0 +1,44 @@
+package lotto.view;
+
+import lotto.constant.Prize;
+import lotto.domain.prize.WinningReport;
+import lotto.domain.sales.LottoBundle;
+
+public class ResultView {
+
+    private final static String BELOW_ONE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)";
+    private final static String OVER_ONE = "(기준이 1이기 때문에 결과적으로 이득이라는 의미임)";
+
+    public void show(LottoBundle lottoBundle) {
+        for (int i = 0; i < lottoBundle.count(); i++) {
+            System.out.println(lottoBundle.lotto(i));
+        }
+    }
+
+    public void report(WinningReport winningReport) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("-------------------");
+        printPrizes(winningReport);
+        printProfit(winningReport.winningRate());
+    }
+
+    private void printPrizes(WinningReport winningReport) {
+        for (Prize prize : Prize.values()) {
+            System.out.println(prize.getMatch()
+                    + "개 일치 (" + prize.getPrize() + "원)- "
+                    + winningReport.winAmount(prize) + "개");
+        }
+    }
+
+    private void printProfit(double winningRate) {
+        System.out.println("총 수익률은 "
+                + String.format("%.2f", winningRate)
+                + "입니다."
+                + profitComment(winningRate));
+    }
+
+    private String profitComment(double winningRate) {
+        return (winningRate > 1.0) ? OVER_ONE : BELOW_ONE;
+    }
+}
