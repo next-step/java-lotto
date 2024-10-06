c5213f02e8cbd5982d59e1cbcab513b364b9f443
이윤선 <yoonseon3@gmail.com>
2024-10-06 14:24:39 +0900

    test: 입력값이 덧셈 연산으로만 이루어졌을 때 calculate 메서드가 덧셈을 수행한다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,12 @@ package calculator;
 
 import calculator.util.StringUtils;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    private static final String REGEX = "[+\\-*/]";
+    private static final Pattern COMPILE = Pattern.compile(REGEX);
 
     private StringCalculator() {
         throw new UnsupportedOperationException("유틸 클래스는 생성할 수 없습니다.");
@@ -10,7 +15,33 @@ public class StringCalculator {
 
     public static int calculate(final String input) {
         validateInput(input);
-        return 0;
+        String[] elements = getElements(input);
+        int calculatedValue = Integer.parseInt(elements[0]);
+        String operator = null;
+        for (int i = 1; i < elements.length; i++) {
+            if (containsOperators(elements[i])) {
+                operator = elements[i];
+                continue;
+            }
+            calculatedValue = getResult(calculatedValue, operator, Integer.parseInt(elements[i]));
+        }
+        return calculatedValue;
+    }
+
+    private static int getResult(int calculatedValue, String operator, int number) {
+        if ("+".equals(operator)) {
+            calculatedValue = calculatedValue + number;
+        }
+        return calculatedValue;
+    }
+
+    public static boolean containsOperators(String input) {
+        return COMPILE.matcher(input).find();
+    }
+
+
+    private static String[] getElements(String input) {
+        return input.split(" ");
     }
 
     private static void validateInput(final String input) {
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,9 +1,12 @@
 package calculator;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringCalculatorTest {
@@ -14,5 +17,14 @@ class StringCalculatorTest {
         assertThatThrownBy(() -> StringCalculator.calculate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력값에 null 또는 공백만 존재하여 계산을 할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("입력값이 덧셈 연산으로만 이루어졌을 때 calculate 메서드가 덧셈을 수행한다.")
+    void sumTest() {
+        String input = "100 + 200 + 300";
+        int expectValue = 600;
+        assertThat(StringCalculator.calculate(input))
+                .isEqualTo(expectValue);
     }
 }
