be654882b637a5f4ce0596c664561ae33d00750e
StoneHee99 <kiit0901@gmail.com>
2024-10-07 13:09:41 +0900

    test: 실패하는 테스트 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,2 +1,16 @@
-package calculator;public class CalculatorTest {
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @DisplayName("덧셈 연산이 잘 수행되는지")
+    @Test
+    void additionTest() {
+        assertThat(Calculator.calculate("1 + 2")).isEqualTo(3);
+    }
+
 }
