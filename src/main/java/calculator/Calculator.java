3a100a1857c674da9cb59e8611f279cfe892c36e
StoneHee99 <kiit0901@gmail.com>
2024-10-07 14:09:41 +0900

    feat: 빈값이나 공백 문자열이 주어졌을 때 예외 발생

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,12 +3,19 @@ package calculator;
 public class Calculator {
 
     public static int calculate(String formula) {
-        String[] formulaElements = formula.split(" ");
+        String[] formulaElements = getFormulaElements(formula);
         int interimResult = getFirstElement(formulaElements);
         for(int i = 1; i < formulaElements.length; i += 2) {
             interimResult = calculate(interimResult, getOperator(i, formulaElements), getOperand(i, formulaElements));
         }
         return interimResult;
+    }
+
+    private static String[] getFormulaElements(String formula) {
+        if (formula == null || formula.isBlank()) {
+            throw new IllegalArgumentException("계산 식은 null 혹은 공백일 수 없습니다");
+        }
+        return formula.split(" ");
     }
 
     private static int getFirstElement(String[] formulaElements) {
