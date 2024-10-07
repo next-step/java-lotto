8426a5928ce797a60a9137944c48b999890d275e
hwan2-99 <thh1233123@gmail.com>
2024-10-07 22:24:56 +0900

    refactor: static, final 순서 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,10 @@
 package calculator;
 
 public class StringCalculator {
-    private final static String PLUS = "+";
-    private final static String MINUS = "-";
-    private final static String MULTIPLE = "*";
-    private final static String DIVISION = "/";
+    private static final String PLUS = "+";
+    private static final String MINUS = "-";
+    private static final String MULTIPLE = "*";
+    private static final String DIVISION = "/";
     private static final String INCORRECT_VALUE_ERROR_MESSAGE = "입력값이 비었거나 null 입니다.";
     private static final String INCORRECT_DIVISION_ERROR_MESSAGE = "0으로 나눌수 없습니다.";
     private static final String INCORRECT_OPERATOR_ERROR_MESSAGE = "올바른 사칙연산자가 아닙니다.";
@@ -14,12 +14,12 @@ public class StringCalculator {
 
     public static int run(final String inputValue) {
         validateIsBlank(inputValue);
-        String[] arr = Splitter.split(inputValue);
+        String[] array = Splitter.split(inputValue);
 
-        int result = Integer.parseInt(arr[0]);
+        int result = Integer.parseInt(array[0]);
 
-        for (int i = 1; i < arr.length; i += 2) {
-            result = calculate(arr[i], result, Integer.parseInt(arr[i + 1]));
+        for (int i = 1; i < array.length; i += 2) {
+            result = calculate(array[i], result, Integer.parseInt(array[i + 1]));
         }
 
         return result;
