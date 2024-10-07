6da98fc047e22c6a291b3509c9b62bb80a456c27
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-08 00:29:56 +0900

    feat: input, result UI

---
+++ src/main/java/step3/uiView/ResultView.java
@@ -0,0 +1,38 @@
+package step3.uiView;
+
+
+import step3.enums.LottoPrize;
+import step3.enums.PrizeMessage;
+import step3.enums.ResultMessage;
+import step3.model.Lotto;
+import step3.model.LottoConfirmation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ResultView {
+
+    private final static String LOTTO_COUNT = "{count}";
+    private final static String LOTTO_RATE = "{rate}";
+
+    //구매한 로또 번호를 출력한다.
+    public void lottoView(Lotto lotto) {
+        System.out.println(ResultMessage.RESULT_LOTTO_COUNT.message().replace(LOTTO_COUNT, String.valueOf(lotto.getLottos().size())));
+        for (List<Integer> lottoNum : lotto.getLottos()) {
+            System.out.println(lottoNum.toString());
+        }
+    }
+
+    //당첨통계를 출력한다.
+    public void lottoPrizeView(LottoConfirmation lottoConfirmation) {
+        System.out.println(ResultMessage.RESULT_LOTTO_STATICS.message());
+        Arrays.stream(LottoPrize.values())
+                    .forEach(lottoPrize -> System.out.println(PrizeMessage.matchCountMessage(lottoPrize, lottoConfirmation.getPrize(lottoPrize))));
+    }
+
+    //수익율을 출력한다.
+    public void lottoRateReturn(LottoConfirmation lottoConfirmation) {
+        System.out.println(ResultMessage.RESULT_RATE_RETURN.message().replace(LOTTO_RATE, String.valueOf(lottoConfirmation.rateOfReturn())));
+    }
+
+}
