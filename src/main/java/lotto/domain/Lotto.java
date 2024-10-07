bbc2aaf2f2726905a955e254d2196cdcde6f2154
SangHyun Kim <ksh9891@gmail.com>
2024-10-07 22:38:16 +0900

    feat(lotto): Lotto 일급컬렉션 추가
    - LottoNumber 을 목록으로 가진다.
    - 정확히 6개의 로또넘버를 가지지 못할 경우 예외를 발생시킨다.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    public static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public Lotto(final List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 번호는 정확히" + LOTTO_NUMBER_COUNT + "개 여야 합니다.");
+        }
+
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public int size() {
+        return lottoNumbers.size();
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class LottoTest {
+    @Test
+    @DisplayName("로또는 6개의 로또번호를 가진다.")
+    void shouldContainExactlySixNumbers() {
+        final Lotto lotto = new Lotto(
+            List.of(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)
+            )
+        );
+
+        assertThat(lotto.size()).isEqualTo(6);
+
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Lotto(
+                List.of(
+                    new LottoNumber(1)
+                )
+            ));
+    }
+}
