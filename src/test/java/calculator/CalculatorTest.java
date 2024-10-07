cfc0bc5ac8a2e2d744a532b228562de2087a517e
StoneHee99 <kiit0901@gmail.com>
2024-10-07 13:35:05 +0900

    test: 실패하는 테스트 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,21 +1,30 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CalculatorTest {
 
-    @DisplayName("덧셈 연산이 잘 수행되는지")
-    @Test
-    void additionTest() {
-        assertThat(Calculator.calculate("1 + 2")).isEqualTo(3);
+    @DisplayName("사칙 연산이 잘 수행되는지")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void additionTest(String formula, int result) {
+        assertThat(Calculator.calculate(formula)).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산이 잘 수행되는지")
-    @Test
-    void subtractionTest() {
-        assertThat(Calculator.calculate("2 - 1")).isEqualTo(1);
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments("1 + 2", 3),
+                arguments("5 - 3", 2),
+                arguments("6 * 4", 24),
+                arguments("8 / 2", 4)
+        );
     }
 }
