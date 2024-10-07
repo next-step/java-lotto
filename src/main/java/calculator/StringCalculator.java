a8a2bd68601e57d2cd7a3a421bc1d190924502ee
heechul <khc4190@naver.com>
2024-10-07 22:25:07 +0900

    feat: 연산자와 숫자 분류 추가
    - 연산자 아닐 경우 exception
    - 숫자 아닐 경우 exception
    - 숫자, 연산자 스택 숫자 맞지 않는 경우 exception

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,6 +1,11 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class StringCalculator {
+    public static final List<String> OPERATORS = Arrays.asList("+", "-", "*", "/");
     private static StringCalculator INSTANCE = null;
 
     private StringCalculator() {
@@ -14,7 +19,7 @@ public class StringCalculator {
     }
 
     public String[] split(String text) {
-        if(isBlank(text)){
+        if (isBlank(text)) {
             throw new IllegalStateException("문자열이 비어있습니다.");
         }
         return text.split(" ");
@@ -22,5 +27,31 @@ public class StringCalculator {
 
     private static boolean isBlank(String text) {
         return text == null || text.isBlank();
+    }
+
+    public boolean isOperator(String operator) {
+        if (!isContainOperator(operator)) {
+            throw new IllegalStateException("연산자가 아닌 문자는 허용하지 않습니다.");
+        }
+        return true;
+    }
+
+    private boolean isContainOperator(String operator) {
+        return OPERATORS.contains(operator);
+    }
+
+    public int parseNumber(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException ex) {
+            throw new IllegalStateException("숫자가 아닌 문자는 허용하지 않습니다.");
+        }
+    }
+
+    public boolean isValidStackSize(Stack<Integer> numbers, Stack<String> operators) {
+        if (numbers.size() != operators.size() + 1) {
+            throw new IllegalStateException("연산이 성립되지 않습니다.");
+        }
+        return true;
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,10 +3,64 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
+
+
+    @Test
+    @DisplayName("숫자 스택과 연산자 스택의 개수가 1개 차이가 나면 true를 반환한다.")
+    void 스택_개수_비교() {
+        Stack<Integer> numbers = new Stack<>();
+        numbers.push(1);
+        numbers.push(2);
+        Stack<String> operators = new Stack<>();
+        operators.push("*");
+        assertThat(StringCalculator.getInstance().isValidStackSize(numbers, operators)).isTrue();
+    }
+
+    @Test
+    @DisplayName("숫자 스택과 연산자 스택의 개수가 1개 차이가 나지 않으면 exception을 발생한다.")
+    void 스택_개수_비교_exception() {
+        Stack<Integer> numbers = new Stack<>();
+        numbers.push(1);
+        Stack<String> operators = new Stack<>();
+        operators.push("*");
+        assertThatThrownBy(() -> StringCalculator.getInstance().isValidStackSize(numbers, operators))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("숫자인 경우 숫자를 반환한다.")
+    void 숫자인_경우() {
+        assertThat(StringCalculator.getInstance().parseNumber("1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 경우 exception을 발생한다.")
+    void 숫자_아닌경우() {
+        assertThatThrownBy(() -> StringCalculator.getInstance().parseNumber("a"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("연산자인 경우 true를 반환한다.")
+    void 연산자인_경우() {
+        assertThat(StringCalculator.getInstance().isOperator("+")).isTrue();
+        assertThat(StringCalculator.getInstance().isOperator("*")).isTrue();
+        assertThat(StringCalculator.getInstance().isOperator("/")).isTrue();
+        assertThat(StringCalculator.getInstance().isOperator("-")).isTrue();
+    }
+
+    @Test
+    @DisplayName("연산자가 아닌 경우 exception을 발생한다.")
+    void 연산자_아닌경우() {
+        assertThatThrownBy(() -> StringCalculator.getInstance().isOperator("_"))
+                .isInstanceOf(IllegalStateException.class);
+    }
 
     @Test
     @DisplayName("문자열이 비어있는 경우 exception을 발생한다.")
