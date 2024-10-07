67abac94c06425a654f7bdacc1a957546d73265a
이윤선 <yoonseon3@gmail.com>
2024-10-07 23:05:03 +0900

    test: 로또 구매 가격 만큼 로또를 발급한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,14 @@
 package lotto;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Lotto {
     private static final int LOTTO_TICKET_PRICE = 1000;
+    private static final int LOTTO_BOUND = 46;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static Random random = new Random();
 
     private Lotto() {
         throw new UnsupportedOperationException("해당 클래스는 인스턴스를 생성할 수 없습니다.");
@@ -12,10 +16,28 @@ public class Lotto {
 
     public static List<List<Integer>> issueTickets(final int totalPurchasePrice) {
         validatePurchasePrice(totalPurchasePrice);
-        return Collections.emptyList();
+        int totalTicketCount = getTotalTicketCount(totalPurchasePrice);
+        List<List<Integer>> tickets = new ArrayList<>(totalTicketCount);
+        for (int i = 0; i < totalTicketCount; i++) {
+            tickets.add(issueTicket());
+        }
+        return tickets;
     }
 
-    private static void validatePurchasePrice(int totalPurchasePrice) {
+    private static List<Integer> issueTicket() {
+        List<Integer> ticket = new ArrayList<>(LOTTO_NUMBER_COUNT);
+        for (int j = 0; j < LOTTO_NUMBER_COUNT; j++) {
+            int lottoNumber = random.nextInt(LOTTO_BOUND);
+            ticket.add(lottoNumber);
+        }
+        return ticket;
+    }
+
+    private static int getTotalTicketCount(final int totalPurchasePrice) {
+        return totalPurchasePrice / LOTTO_TICKET_PRICE;
+    }
+
+    private static void validatePurchasePrice(final int totalPurchasePrice) {
         if (totalPurchasePrice % LOTTO_TICKET_PRICE != 0) {
             throw new IllegalArgumentException("올바른 구매금액이 아닙니다. 로또 개수에 맞게 금액을 넣어주세요.(1장당 1000원)");
         }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,8 +2,12 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -17,4 +21,11 @@ class LottoTest {
                 .hasMessage("올바른 구매금액이 아닙니다. 로또 개수에 맞게 금액을 넣어주세요.(1장당 1000원)");
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"14000:14","1000:1","3000:3"}, delimiter = ':')
+    @DisplayName("로또 구매 가격 만큼 로또를 발급한다.")
+    void lottoCountTest(int totalPurchasePrice, int count) {
+        List<List<Integer>> lottoResults = Lotto.issueTickets(totalPurchasePrice);
+        assertThat(lottoResults).hasSize(count);
+    }
 }
