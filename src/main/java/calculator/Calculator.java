9a641189ff52d91cbeb946f51d58666178ba264a
StoneHee99 <kiit0901@gmail.com>
2024-10-07 13:29:59 +0900

    feat: 뺄셈 연산 수행

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -17,7 +17,7 @@ public class Calculator {
     }
 
     private static int calculate(int operand1, String operator, int operand2) {
-        return operand1 + operand2;
+        return operator.equals("+") ? operand1 + operand2 : operand1 - operand2;
     }
 
     private static String getOperator(int index, String[] formulaElements) {
