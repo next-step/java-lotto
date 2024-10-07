91b2b5af6f411f6db0ad1ccdf166620787f1b294
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-08 00:30:15 +0900

    test: 테스트코드

---
+++ src/test/java/step3/model/LottoTest.java
@@ -0,0 +1,30 @@
+package step3.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+- 당첨번호를 입력받는다.
+*/
+public class LottoTest {
+
+    @DisplayName("당첨번호를 입력받는다.")
+    @Test
+    void addLottoNumTest() {
+        Lotto lotto = new Lotto();
+        lotto.addLotto(List.of(1, 2, 3, 4, 5, 6));
+        lotto.addLotto(List.of(7, 8, 9, 10, 11, 12));
+
+        assertThat(lotto.getLottos())
+                .containsExactly(
+                        List.of(1, 2, 3, 4, 5, 6),
+                        List.of(7, 8, 9, 10, 11, 12)
+                );
+    }
+
+
+}
