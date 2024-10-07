b38e6fc71bacb29a60a6af5d5d52a38469d1807a
StoneHee99 <kiit0901@gmail.com>
2024-10-07 13:24:18 +0900

    feat: 덧셈 연산 수행

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,30 @@
+package calculator;
+
+public class Calculator {
+
+
+    public static int calculate(String formula) {
+        String[] formulaElements = formula.split(" ");
+        int interimResult = getFirstElement(formulaElements);
+        for(int i = 1; i < formulaElements.length; i += 2) {
+            interimResult = calculate(interimResult, getOperator(i, formulaElements), getOperand(i, formulaElements));
+        }
+        return interimResult;
+    }
+
+    private static int getFirstElement(String[] formulaElements) {
+        return Integer.parseInt(formulaElements[0]);
+    }
+
+    private static int calculate(int operand1, String operator, int operand2) {
+        return operand1 + operand2;
+    }
+
+    private static String getOperator(int index, String[] formulaElements) {
+        return formulaElements[index];
+    }
+
+    private static int getOperand(int index, String[] formulaElements) {
+        return Integer.parseInt(formulaElements[index + 1]);
+    }
+}
