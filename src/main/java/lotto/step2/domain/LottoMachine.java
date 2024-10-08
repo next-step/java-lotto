ffe18c94416d3210da76f4ec3052d06ef19eb360
skdev0619 <skdev0619@naver.com>
2024-10-09 08:16:25 +0900

    로또 구매 유효성 체크

--- src/main/java/lotto/step2/domain/LottoMachine.java
+++ src/main/java/lotto/step2/domain/LottoMachine.java
@@ -4,6 +4,9 @@ public class LottoMachine {
     private final int PRICE = 1000;
 
     public int buyLottos(int purchaseAmount) {
+        if(purchaseAmount <= 0){
+            throw new IllegalArgumentException("로또를 구매할 수 없습니다");
+        }
         return purchaseAmount / PRICE;
     }
 }
