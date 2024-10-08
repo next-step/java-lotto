2e329de0de0951e086788cd154614b7934f1bcd8
SangHyun Kim <ksh9891@gmail.com>
2024-10-08 23:03:37 +0900

    refactor(lotto): bundle 의 로또 발행 부분 코드 수정

--- src/main/java/lotto/domain/LottoBundle.java
+++ src/main/java/lotto/domain/LottoBundle.java
@@ -1,12 +1,23 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoBundle {
     private final List<Lotto> lottoBundle;
 
-    public LottoBundle(final List<Lotto> lottoBundle) {
-        this.lottoBundle = lottoBundle;
+    private LottoBundle(final List<Lotto> lottoBundle) {
+        this.lottoBundle = Collections.unmodifiableList(lottoBundle);
+    }
+
+    public static LottoBundle issues(final int lottoBundleCount) {
+        final LottoNumbers lottoNumbers = new LottoNumbers();
+        final List<Lotto> lottoBundle = new ArrayList<>();
+        for (int i = 0; i < lottoBundleCount; i++) {
+            lottoBundle.add(new Lotto(lottoNumbers.shuffleAndPickLottoNumbers()));
+        }
+        return new LottoBundle(lottoBundle);
     }
 
     public int size() {
