851de621374b92bf3252f2121a2b4027cf50d8f6
이윤선 <yoonseon3@gmail.com>
2024-10-08 23:26:05 +0900

    test: getTotalWinningAmount 메서드가 로또 당첨 금액을 반환한다.

--- src/test/java/lotto/domain/LottoWinningResultsTest.java
+++ src/test/java/lotto/domain/LottoWinningResultsTest.java
@@ -13,4 +13,16 @@ class LottoWinningResultsTest {
         assertThat(result.getWinningResults())
                 .hasSize(LottoWinningStatus.values().length);
     }
+
+    @Test
+    @DisplayName("getTotalWinningAmount 메서드가 로또 당첨 금액을 반환한다.")
+    void getTotalWinningAmountTest() {
+        int winningAmountSum = LottoWinningStatus.THREE.getAmount() + LottoWinningStatus.FOUR.getAmount();
+        LottoWinningResults result = new LottoWinningResults();
+        result.incrementWinningResults(LottoWinningStatus.THREE.getWinningCount());
+        result.incrementWinningResults(LottoWinningStatus.FOUR.getWinningCount());
+        assertThat(result.getTotalWinningAmount())
+                .isEqualTo(winningAmountSum);
+
+    }
 }
