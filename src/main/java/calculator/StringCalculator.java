6b3cb756a8e333e34b5a31b3d3359d5a1c9e6030
jeonghyeonkwon <givejeong2468@gmail.com>
2024-10-08 18:56:20 +0900

    feat: 사칙연산 검증

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class StringCalculator {
+
+
+    public static int add(int result, int number) {
+        return result + number;
+    }
+
+    public static int subtract(int result, int number) {
+        return result - number;
+    }
+
+    public static int multiply(int result, int number) {
+        return result * number;
+    }
+
+    public static int divide(int result, int number) {
+        return result / number;
+    }
+}
