ba226a8b4a137cbee0e4f82495f6b1e641cadad2
hwan2-99 <thh1233123@gmail.com>
2024-10-08 22:10:26 +0900

    refactor: 로또 구입금액 최소입력값 변수명 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,11 +3,11 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final int ZERO = 0;
+    private static final int MINIMUM_VALUE = 0;
     private static final String INPUT_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String INCORRECT_VALUE_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
-    private static final String INCORRECT_RANGE_ERROR_MESSAGE = ZERO + "보다 낮은 수는 입력할 수 없습니다.";
+    private static final String INCORRECT_RANGE_ERROR_MESSAGE = MINIMUM_VALUE + "보다 낮은 수는 입력할 수 없습니다.";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -40,7 +40,7 @@ public class InputView {
 
     private static void validateRange(final String inputValue) {
         int value = Integer.parseInt(inputValue);
-        if (value < ZERO) {
+        if (value < MINIMUM_VALUE) {
             throw new IllegalArgumentException(INCORRECT_RANGE_ERROR_MESSAGE);
         }
     }
