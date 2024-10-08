dc45e6e99d2abb3d6774445fe2f6b8808ef37588
hwan2-99 <thh1233123@gmail.com>
2024-10-08 23:32:13 +0900

    feat: 구매한 개수만큼 로또구매기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,19 +1,31 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoGenerator {
     private static final int LOTTO_SIZE = 6;
+    private static final int PRICE_MONEY_UNIT = 1000;
 
     public LottoGenerator() {
     }
 
-    public Lotto generateLotto() {
-        List<LottoNumber> lottoNumbers = new ArrayList<>();
+    public List<Lotto> purchaseLotto(final int money) {
+        int purchaseCount = money / PRICE_MONEY_UNIT;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseCount; i++) {
+            lottos.add(generateLotto());
+        }
+        return lottos;
+    }
+
+    private Lotto generateLotto() {
+        Set<LottoNumber> lottoNumbers = new HashSet<>();
         while (lottoNumbers.size() != LOTTO_SIZE) {
             lottoNumbers.add(LottoNumber.generateLottoNumber());
         }
-        return new Lotto(lottoNumbers);
+        return new Lotto(new ArrayList<>(lottoNumbers));
     }
 }
--- src/test/java/lotto/domain/LottoGeneratorTest.java
+++ src/test/java/lotto/domain/LottoGeneratorTest.java
@@ -3,13 +3,23 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LottoGeneratorTest {
     @Test
-    void 로또_생성() {
+    void 로또_한개만_구매() {
         LottoGenerator lottoGenerator = new LottoGenerator();
-        Lotto lotto = lottoGenerator.generateLotto();
-        assertThat(lotto).isNotNull();
+        int purchasePrice = 1000;
+        List<Lotto> lotto = lottoGenerator.purchaseLotto(purchasePrice);
+        assertThat(lotto.size()).isEqualTo(1);
+    }
+
+    @Test
+    void 로또_여러개_구매() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        int purchasePrice = 7000;
+        List<Lotto> lotto = lottoGenerator.purchaseLotto(purchasePrice);
+        assertThat(lotto.size()).isEqualTo(7);
     }
 }
