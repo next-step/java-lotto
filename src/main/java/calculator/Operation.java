dd2eee238838c67d8ca5991f52abc400199fcdd1
hwan2-99 <thh1233123@gmail.com>
2024-10-08 13:06:35 +0900

    refactor: 사칙연산자 찾는메서드와 계산 메서드 분리

--- src/main/java/calculator/Operation.java
+++ src/main/java/calculator/Operation.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
 
 public enum Operation {
@@ -22,11 +23,13 @@ public enum Operation {
     }
 
     public static int calculate(final String operator, final int num1, final int num2) {
-        for (Operation operation : Operation.values()) {
-            if (operation.symbol.equals(operator)) {
-                return operation.operation.applyAsInt(num1, num2);
-            }
-        }
-        throw new IllegalArgumentException("올바른 사칙연산자가 아닙니다.");
+        return getOperationBySymbol(operator).operation.applyAsInt(num1, num2);
+    }
+
+    private static Operation getOperationBySymbol(final String symbol) {
+        return Arrays.stream(values())
+                .filter(operation -> operation.symbol.equals(symbol))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산자가 아닙니다."));
     }
 }
