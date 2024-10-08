3d269aadc0cac8f47bcd3783b9a0498838ae0272
sinyebin <dpqls0625@naver.com>
2024-10-08 09:20:45 +0900

    feat 24.10.07 syb 입력된 값이 숫자가 아닌 경우 에러 발생

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,11 @@
 package calculator;
 
 import static calculator.ErrorMessage.BLINK_INPUT_ERROR;
+import static calculator.ErrorMessage.INPUT_IS_NOT_NUMBER;
 
 public class Calculator {
-    public static void validateString(String input){
-        if(isBlank(input)){
+    public static void validateString(String input) {
+        if (isBlank(input)) {
             throw new IllegalArgumentException(BLINK_INPUT_ERROR);
         }
     }
@@ -12,4 +13,13 @@ public class Calculator {
     private static boolean isBlank(String input) {
         return (input == null || input.trim().isEmpty());
     }
+
+    public static int convertStringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER);
+        }
+    }
+
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,16 +1,20 @@
 package calculator;
 
+import static calculator.Calculator.convertStringToInt;
 import static calculator.Calculator.validateString;
 import static calculator.ErrorMessage.BLINK_INPUT_ERROR;
+import static calculator.ErrorMessage.INPUT_IS_NOT_NUMBER;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.platform.commons.util.StringUtils.isBlank;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTest {
+    @DisplayName("입력받은 문자가 빈 공백이거나 null일 경우 에러를 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"", "  "})
     void 빈공백문자_혹은_null일경우_에러(String input) {
@@ -18,5 +22,15 @@ class CalculatorTest {
             validateString(input);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(BLINK_INPUT_ERROR);
+    }
+
+    @DisplayName("입력받은 문자가 숫자일 경우 에러를 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a"})
+    void 숫자가_아닌경우_에러(String input) {
+        assertThatThrownBy(() -> {
+            convertStringToInt(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INPUT_IS_NOT_NUMBER);
     }
 }
