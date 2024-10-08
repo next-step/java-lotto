4c8729208807b411ea4ad92e78ea8106b2c33832
SangHyun Kim <ksh9891@gmail.com>
2024-10-08 23:04:54 +0900

    refactor(lotto): 로또상점의 구매 메서드 수정

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,48 +1,9 @@
 package lotto.domain;
 
-import java.util.List;
-
 public class LottoStore {
     public static final int LOTTO_PURCHASE_AMOUNT = 1000;
 
-    private final Money money;
-
-    public LottoStore(final Money money) {
-        if (LOTTO_PURCHASE_AMOUNT > money.getMoney()) {
-            throw new IllegalArgumentException(LOTTO_PURCHASE_AMOUNT + "이상의 금액이 필요합니다.");
-        }
-
-        if (money.getMoney() % LOTTO_PURCHASE_AMOUNT > 0) {
-            throw new IllegalArgumentException("거스름돈이 없습니다. 구매가 불가능합니다.");
-        }
-
-        this.money = money;
-    }
-
-    public LottoBundle issue() {
-        return new LottoBundle(
-            List.of(
-                new Lotto(
-                    List.of(
-                        new LottoNumber(1),
-                        new LottoNumber(2),
-                        new LottoNumber(3),
-                        new LottoNumber(44),
-                        new LottoNumber(5),
-                        new LottoNumber(6)
-                    )
-                ),
-                new Lotto(
-                    List.of(
-                        new LottoNumber(1),
-                        new LottoNumber(2),
-                        new LottoNumber(3),
-                        new LottoNumber(44),
-                        new LottoNumber(5),
-                        new LottoNumber(6)
-                    )
-                )
-            )
-        );
+    public LottoBundle purchase(final Money money) {
+        return LottoBundle.issues(money.getMoney() / LOTTO_PURCHASE_AMOUNT);
     }
 }
--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -4,28 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoStoreTest {
     @Test
-    @DisplayName("로또 1장의 금액보다 낮은 금액은 받을 수 없다.")
-    void shouldRejectAmountLessThanLottoPrice() {
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> new LottoStore(new Money(999)));
-    }
-
-    @Test
-    @DisplayName("전달받은 금액이 로또 금액으로 정확하게 나누어 떨어지지 않는 경우 예외가 발생한다.")
-    void shouldThrowExceptionIfAmountIsNotDivisibleByLottoPrice() {
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> new LottoStore(new Money(12300)));
-    }
-
-    @Test
     @DisplayName("전달받은 금액이 로또 금액으로 정확하게 나누어 떨어지면 로또를 발행한다.")
     void shouldIssueLottoWhenAmountIsDivisibleByLottoPrice() {
-        final LottoStore lottoStore = new LottoStore(new Money(2000));
+        final LottoStore lottoStore = new LottoStore();
 
-        assertThat(lottoStore.issue().size()).isEqualTo(2);
+        assertThat(lottoStore.purchase(new Money(3000)).size()).isEqualTo(3);
     }
 }
