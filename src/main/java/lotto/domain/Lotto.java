2918d4bb9d0d1a6a4b6602edd483ac58c69f6b85
SangHyun Kim <ksh9891@gmail.com>
2024-10-08 23:01:13 +0900

    refactor(lotto): LottoNumbers 일급컬렉션의 역할을 어느정도 맡고 있었는데 LottoNumbers 로 역할을 위임하면서 불필요한 행위를 제거함

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,33 +1,19 @@
 package lotto.domain;
 
-import java.util.Collections;
-import java.util.List;
-
 public class Lotto {
     public static final int LOTTO_NUMBER_COUNT = 6;
 
-    private final List<LottoNumber> lottoNumbers;
+    private final LottoNumbers lottoNumbers;
 
-    public Lotto(final List<LottoNumber> lottoNumbers) {
+    public Lotto(final LottoNumbers lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("로또 번호는 정확히" + LOTTO_NUMBER_COUNT + "개 여야 합니다.");
         }
-        validateDuplicateNumber(lottoNumbers);
 
-        this.lottoNumbers = Collections.unmodifiableList(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
     public int size() {
         return lottoNumbers.size();
-    }
-
-    private void validateDuplicateNumber(final List<LottoNumber> lottoNumbers) {
-        final long filteredLottoNumbersCount = lottoNumbers.stream()
-            .distinct()
-            .count();
-
-        if (filteredLottoNumbersCount != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException("중복된 로또 번호가 존재하여 로또 발행에 실패하였습니다.");
-        }
     }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,43 +3,15 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoTest {
     @Test
     @DisplayName("로또는 6개의 로또번호를 가진다.")
     void shouldContainExactlySixNumbers() {
-        final Lotto lotto = new Lotto(
-            List.of(
-                new LottoNumber(1), new LottoNumber(2),
-                new LottoNumber(3), new LottoNumber(4),
-                new LottoNumber(5), new LottoNumber(6)
-            )
-        );
+        final LottoNumbers lottoNumbers = new LottoNumbers();
+        final Lotto lotto = new Lotto(lottoNumbers.shuffleAndPickLottoNumbers());
 
         assertThat(lotto.size()).isEqualTo(6);
-
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Lotto(
-                List.of(
-                    new LottoNumber(1)
-                )
-            ));
-    }
-
-    @Test
-    @DisplayName("로또는 중복된 로또번호를 가질 수 없다.")
-    void shouldNotHaveDuplicateNumbers() {
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Lotto(
-                List.of(
-                    new LottoNumber(1), new LottoNumber(1),
-                    new LottoNumber(1), new LottoNumber(1),
-                    new LottoNumber(2), new LottoNumber(3)
-                )
-            ));
     }
 }
