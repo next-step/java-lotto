9e0be9365d83c6a5a6b430a3b701747126fa361b
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-09 20:52:22 +0900

    refactor : 메소드 분리하기

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,9 +3,7 @@ package calculator;
 public class StringCalculator {
 
     public int calculator(String input) {
-        if (input == null || input.trim().isBlank()) {
-            throw new IllegalArgumentException("입력값이 null이거나 공백문자인 경우 계산되지 않습니다.");
-        }
+        validationInput(input);
 
         String[] inputSplit = input.split(" ");
         int result = Integer.parseInt(inputSplit[0]);
@@ -14,25 +12,35 @@ public class StringCalculator {
             String operator = inputSplit[i];
             int number = Integer.parseInt(inputSplit[i + 1]);
 
-
-            if (operator.equals("+")) {
-                result += number;
-                continue;
-            }
-            if (operator.equals("-")) {
-                result -= number;
-                continue;
-            }
-            if (operator.equals("*")) {
-                result *= number;
-                continue;
-            }
-            if (operator.equals("/")) {
-                result /= number;
-                continue;
-            }
-            throw new IllegalArgumentException("사칙연산 기호가 아닐 경우에는 계산되지 않습니다.");
+            result = solve(operator, result, number);
+            continue;
         }
         return result;
+    }
+
+    private static int solve(String operator, int result, int number) {
+        if (operator.equals("+")) {
+            return result + number;
+        }
+
+        if (operator.equals("-")) {
+            return result - number;
+        }
+
+        if (operator.equals("*")) {
+            return result * number;
+        }
+
+        if (operator.equals("/")) {
+            return result / number;
+        }
+
+        throw new IllegalArgumentException("사칙연산 기호가 아닐 경우에는 계산되지 않습니다.");
+    }
+
+    private static void validationInput(String input) {
+        if (input == null || input.trim().isBlank()) {
+            throw new IllegalArgumentException("입력값이 null이거나 공백문자인 경우 계산되지 않습니다.");
+        }
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -9,59 +9,54 @@ import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
 
+    public static final StringCalculator CALCULATOR = new StringCalculator();
+
     @ParameterizedTest
     @DisplayName("덧셈이 정확하게 작동하는지 확인")
     @ValueSource(strings = {"3 + 5 + 7"})
     void addTest(String input) {
-        StringCalculator calculator = new StringCalculator();
-        assertThat(calculator.calculator(input)).isEqualTo(15);
+        assertThat(CALCULATOR.calculator(input)).isEqualTo(15);
     }
 
     @ParameterizedTest
     @DisplayName("뺄셈이 정확하게 작동하는지 확인")
     @ValueSource(strings = {"10 - 5 - 2"})
     void minusTest(String input) {
-        StringCalculator calculator = new StringCalculator();
-        assertThat(calculator.calculator(input)).isEqualTo(3);
+        assertThat(CALCULATOR.calculator(input)).isEqualTo(3);
     }
 
     @ParameterizedTest
     @DisplayName("곱셈이 정확하게 작동하는지 확인")
     @ValueSource(strings = {"10 * 5 * 2"})
     void multipleTest(String input) {
-        StringCalculator calculator = new StringCalculator();
-        assertThat(calculator.calculator(input)).isEqualTo(100);
+        assertThat(CALCULATOR.calculator(input)).isEqualTo(100);
     }
 
     @ParameterizedTest
     @DisplayName("나눗셈이 정확하게 작동하는지 확인")
     @ValueSource(strings = {"100 / 5 / 2"})
     void divisionTest(String input) {
-        StringCalculator calculator = new StringCalculator();
-        assertThat(calculator.calculator(input)).isEqualTo(10);
+        assertThat(CALCULATOR.calculator(input)).isEqualTo(10);
     }
 
     @ParameterizedTest
     @DisplayName("빈 공백 문자일 경우 IllegalArgumentException 나오는지 확인")
     @ValueSource(strings = {""})
     void validationBlinkTest(String input) {
-        StringCalculator calculator = new StringCalculator();
-        assertThatThrownBy(() -> calculator.calculator(input))
+        assertThatThrownBy(() -> CALCULATOR.calculator(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("입력 값이 null일 경우 IllegalArgumentException 나오는지 확인")
     void validationNullTest() {
-        StringCalculator calculator = new StringCalculator();
-        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculator(null));
+        assertThatIllegalArgumentException().isThrownBy(() -> CALCULATOR.calculator(null));
     }
 
     @ParameterizedTest
     @DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException 나오는지 확인")
     @ValueSource(strings = {"5 ! 6"})
     void validationOperatorTest(String input) {
-        StringCalculator calculator = new StringCalculator();
-        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculator(input));
+        assertThatIllegalArgumentException().isThrownBy(() -> CALCULATOR.calculator(input));
     }
 }
