cc01fbba3573f5151fb180408c04ab241038aaa5
이윤선 <yoonseon3@gmail.com>
2024-10-09 19:42:44 +0900

    test: 로또 구매 가격 / 로또 장당 가격이 나누어 떨어지지 않을 때 예외가 발생한다.

---
+++ src/test/java/lotto/domain/LottoPurchasePriceTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoPurchasePriceTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {10100, 1200, 2200})
+    @DisplayName("로또 구매 가격 / 로또 장당 가격이 나누어 떨어지지 않을 때 예외가 발생한다.")
+    void throwExceptionWhenPriceNotDivisibleByTicketPrice(int purchasePrice) {
+        assertThatThrownBy(() -> new LottoPurchasePrice(purchasePrice))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 구매금액이 아닙니다. 로또 개수에 맞게 금액을 넣어주세요.(1장당 1000원)");
+    }
+
+}
--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -4,24 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTicketTest {
-
-    @ParameterizedTest
-    @ValueSource(ints = {10100, 1200, 2200})
-    @DisplayName("총 로또 구매 가격 / 로또 장당 가격이 나누어 떨어지지 않을 때 예외가 발생한다.")
-    void throwExceptionWhenTotalPriceNotDivisibleByTicketPrice(int totalPurchasePrice) {
-        LottoPurchasePrice lottoPurchasePrice = new LottoPurchasePrice(totalPurchasePrice);
-        assertThatThrownBy(() -> new LottoTicket(lottoPurchasePrice))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("올바른 구매금액이 아닙니다. 로또 개수에 맞게 금액을 넣어주세요.(1장당 1000원)");
-    }
 
     @ParameterizedTest
     @CsvSource(value = {"14000:14","1000:1","3000:3"}, delimiter = ':')
