d70472c666c7f9da006a04d840fdc8edda34a5e3
JIHO CHOI <wlghsp@gmail.com>
2024-10-09 11:44:55 +0900

    refactor: IllegalArgumentException 와 같은 직관적 예외로 변경

--- src/main/java/calculator/validator/UserInputValidator.java
+++ src/main/java/calculator/validator/UserInputValidator.java
@@ -1,12 +1,10 @@
 package calculator.validator;
 
-import calculator.exception.ApplicationException;
-
 public class UserInputValidator {
 
     public static void checkIfInputIsEmptyOrBlank(String userInput) {
         if (userInput == null || userInput.isBlank()) {
-            throw new ApplicationException("올바른 입력값이 아닙니다.");
+            throw new IllegalArgumentException("올바른 입력값이 아닙니다.");
         }
     }
 }
