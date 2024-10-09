dcb29e4796ca0223cb143564c994bd66ff7ab6ec
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-09 19:58:02 +0900

    feat : 사용자가 문자열을 입력할 수 있도록 한다

---
+++ src/main/java/calculator/StringCalculatorMain.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculatorMain {
+
+    public static void main(String[] args) {
+        String input = InputView.getInput();
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.calculator(input);
+        System.out.println(result);
+    }
+}
