3043cd799305bf3e483c8a6d874d327a05af3247
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-09 20:52:44 +0900

    refactor : try-catch 추가하기

--- src/main/java/calculator/StringCalculatorMain.java
+++ src/main/java/calculator/StringCalculatorMain.java
@@ -4,9 +4,14 @@ public class StringCalculatorMain {
 
     public static void main(String[] args) {
         String input = InputView.getInput();
-        StringCalculator calculator = new StringCalculator();
-        int result = calculator.calculator(input);
-        OutputView.printResult(result);
+        int result = 0;
 
+        try {
+            StringCalculator calculator = new StringCalculator();
+            result = calculator.calculator(input);
+            OutputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
