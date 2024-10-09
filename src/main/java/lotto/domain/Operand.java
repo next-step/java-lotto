f21db1f28a7b29e9f6300a71b45e8184708ea04e
jara <jara0701@gmail.com>
2024-10-09 17:11:36 +0900

    feat Operand.java: operand class 작성
    operand를 나타내는 클래스를 작성합니다

---
+++ src/main/java/lotto/domain/Operand.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Objects;
+
+public class Operand {
+    private static final String NOT_NATURAL_NUMBER_MESSAGE = "자연수가 아닙니다.";
+    private final int value;
+
+    Operand(int value) {
+        this.value = value;
+    }
+
+    public static Operand valeuOf(String value) {
+        try {
+            int intValue = Integer.parseInt(value);
+            return new Operand(intValue);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NATURAL_NUMBER_MESSAGE);
+        }
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operand operand = (Operand) o;
+        return value == operand.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
