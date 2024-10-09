93ea20f1e0cee19b89937904457f3a038ece3fa5
jara <jara0701@gmail.com>
2024-10-09 16:54:45 +0900

    feat Calculator.java: convertToIntOperand 리팩토링
    Integer.parseInt()를 convertToIntOperand 메서드로 빼서 에러 메시지를 custom하게 지정함

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -4,25 +4,34 @@ import java.util.List;
 
 public class Calculator {
     private static final String INPUT_DELIMITER = " ";
+    private static final String NOT_NATURAL_NUMBER_MESSAGE = "자연수가 아닙니다.";
     private static final String EMPTY_INPUT_MESSAGE = "입력걊이 비어있습니다.";
 
     public static int calculate(String input) {
-        checkInputIsBlank(input);
+        checkInputIsNullOrEmpty(input);
         List<String> symbols = List.of(input.split(INPUT_DELIMITER));
-        int result = Integer.parseInt(symbols.get(0));
+        int result = convertToIntOperand(symbols.get(0));
         for (int i = 1; i < symbols.size(); i = i + 2) {
             String operatorSymbol = symbols.get(i);
             Operator operator = Operator.getInstance(operatorSymbol);
             String operandSymbol = symbols.get(i + 1);
-            int operand = Integer.parseInt(operandSymbol);
+            int operand = convertToIntOperand(operandSymbol);
             result = operator.calculate(result, operand);
         }
         return result;
     }
 
-    private static void checkInputIsBlank(String input) {
-        if (input == null || input.isBlank()) {
+    private static void checkInputIsNullOrEmpty(String input) {
+        if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_INPUT_MESSAGE);
+        }
+    }
+
+    private static int convertToIntOperand(String operandSymbol) {
+        try {
+            return Integer.parseInt(operandSymbol);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NATURAL_NUMBER_MESSAGE);
         }
     }
 }
