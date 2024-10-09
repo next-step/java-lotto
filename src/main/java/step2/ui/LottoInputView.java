8dc4cafe046e4092caa63e76d200b8b69f7d72af
sinsu <sinsu19012@gmail.com>
2024-10-09 18:58:17 +0900

    feat: 지난 주 당첨 번호를 사용자에게 입력받는다. 기능 구현

--- src/main/java/step2/ui/LottoInputView.java
+++ src/main/java/step2/ui/LottoInputView.java
@@ -1,6 +1,9 @@
 package step2.ui;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public final class LottoInputView {
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -12,5 +15,16 @@ public final class LottoInputView {
         System.out.println("구입금액을 입력해 주세요.");
 
         return SCANNER.nextInt();
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String input = SCANNER.nextLine();
+
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
     }
 }
