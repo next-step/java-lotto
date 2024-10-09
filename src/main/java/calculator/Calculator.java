c4edbaf4a4bf7f934cfb7584162849c95dea4ed4
배영현 <byh2949@gmail.com>
2024-10-09 21:50:29 +0900

    refactor : extract private method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
     public int calculate(String input) {
         String[] arr = input.split(" ");
         int result = Integer.parseInt(arr[0]);
+        return applyOperations(arr, result);
+    }
+
+    private static int applyOperations(String[] arr, int result) {
         for (int i = 1; i < arr.length; i += 2) {
             OperatorType operatorType = OperatorType.getOperator(arr[i]);
             int operand = Integer.parseInt(arr[i+1]);
