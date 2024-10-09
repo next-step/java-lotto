3bcc0c2293aa91b768d15dea0ab4158315055d5f
jara <jara0701@gmail.com>
2024-10-09 17:56:59 +0900

    feat ui/*.java: ui관련 파일 구현
    InputView.java와 output.java 구현

---
+++ src/main/java/lotto/ui/InputView.java
@@ -0,0 +1,13 @@
+package lotto.ui;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String CALCULATOR_QUESTION = "계산기에 값을 입력해주세요";
+
+    public static String input() {
+        System.out.println(CALCULATOR_QUESTION);
+        return scanner.nextLine();
+    }
+}
