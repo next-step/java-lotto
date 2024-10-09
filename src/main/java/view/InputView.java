97d0c3ce6bf3b3e8b580ed47b165ffb2e141dd2a
김혜영 <sunzero0116@gmail.com>
2024-10-09 22:33:09 +0900

    fix : InputView 입력값 공백 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static String getExpression() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("계산하고 싶은 식을 입력해주세요. 숫자와 연산자 사이는 공백으로 구분해주세요. (예: 1 + 2 * 3 / 4 - 5)");
         String expression = SCANNER.nextLine();
-        if (expression == null) {
-            return "";
-        }
-        return expression;
+        return expression != null ?  expression.trim() : "";
     }
 }
