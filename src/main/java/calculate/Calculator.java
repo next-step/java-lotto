aa636cd75df83aac645474087d8c8da5d7c790e8
ming <devel.moon17@gmail.com>
2024-10-09 17:08:05 +0900

    test: 1단계 - 계산기 실패하는 테스트 코드 작성

---
+++ src/main/java/calculate/Calculator.java
@@ -0,0 +1,21 @@
+package calculate;
+
+public class Calculator {
+
+    public static int addition(int number1, int number2){
+        return 0;
+    }
+    public static int subtraction(int number1, int number2){
+        return 0;
+    }
+    public static int multiplication(int number1, int number2){
+        return 0;
+    }
+    public static int division(int number1, int number2){
+        return 0;
+    }
+
+    public static int calculate(String symbol, int number1, int number2){
+        return 0;
+    }
+}
--- src/test/java/calculate/CalculatorTest.java
+++ src/test/java/calculate/CalculatorTest.java
@@ -1,5 +1,43 @@
 package calculate;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculatorTest {
+
+    @Test
+    @DisplayName("덧셈")
+    void addition(){
+        assertThat(Calculator.addition(1,2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("뺄셈")
+    void subtraction(){
+        assertThat(Calculator.subtraction(4,3)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("곱셈")
+    void multiplication(){
+        assertThat(Calculator.multiplication(4,3)).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("나눗셈")
+    void division(){
+        assertThat(Calculator.division(13,4)).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:2:+:3", "4:3:-:1", "4:3:*:12", "13:4:/:3"}, delimiter = ':')
+    @DisplayName("사칙연산 기호에 따라 다른 결과값을 출력한다.")
+    void containsCsvSource(int number, int number2, String symbol, int expected) {
+        assertThat(Calculator.calculate(symbol, number, number2)).isEqualTo(expected);
+    }
 
 }
