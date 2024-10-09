d37f754d276de06239da0507800e34dc9976fc2d
jureamer <wngud4950@gmail.com>
2024-10-09 17:01:00 +0900

    feat: add minus method to calculator

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -5,17 +5,34 @@ import java.util.Arrays;
 public class Calculator {
     public static int calculate(String input) {
         int currentNumber = 0;
-        String[] tokens = input.split(" ");
+        boolean isFirst = true;
 
+        String[] tokens = input.split(" ");
+        String operator = "";
 
         for (String token : tokens) {
-            if (token.equals("+")) {
+            if (token.equals("+") || token.equals("-")) {
+                operator = token;
                 continue;
             }
 
-            System.out.println("token : " + token);
+            if (isFirst) {
+                currentNumber = Integer.parseInt(token);
+                isFirst = false;
+                continue;
+            }
+
             try {
-                currentNumber += Integer.parseInt(token);
+                if (operator.equals("+")) {
+                    currentNumber = plus_two_number(currentNumber, Integer.parseInt(token));
+                    continue;
+                }
+
+                if (operator.equals("-")) {
+                    currentNumber = minusTwoNumber(currentNumber, Integer.parseInt(token));
+                    continue;
+                }
+
             } catch (NumberFormatException e) {
                 System.out.println("유효하지 않은 입력입니다. : " + e.getMessage());
                 return 0;
@@ -23,4 +40,14 @@ public class Calculator {
         }
         return currentNumber;
     }
+
+    private static int minusTwoNumber(int currentNumber, int i) {
+        return currentNumber - i;
+    }
+
+    private static int plus_two_number(int currentNumber, int i) {
+        return currentNumber + i;
+    }
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,5 +9,10 @@ public class CalculatorTest {
         assertThat(Calculator.calculate("3 + 2")).isEqualTo(5);
     }
 
+    @Test
+    public void 마이너스_기호가_입력되면_덧셈한다() {
+        assertThat(Calculator.calculate("3 - 2")).isEqualTo(1);
+    }
+
 
 }
