fafaefb9773caa0e9dcc657ee8e9b17164d42caf
jara <jara0701@gmail.com>
2024-10-10 01:26:53 +0900

    feat lotto.java: 중복숫자 점검, 최대 숫자 점검 기능 추가
    중복숫자를 포함한 리스트, 최대 숫자를 넘어가는 숫자를 포함하는 List로 로또 생성 시도시 Exception 발생시키게 함

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,17 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class Lotto {
     private static int LOTTO_NUMBERS_CARDINALITY = 6;
+    private static int MIN_BOUND = 1;
+    private static int MAX_BOUND = 45;
+    private static String DUPLICATED_LOTTO_NUMBERS_MESSAGE = "중복된 숫자가 존재합니다.";
     private static String INVALID_LOTTO_NUMBERS_CARDINALITY_MESSAGE = "로또 숫자 갯수가 다릅니다.";
+    private static String LOTTO_NUMBER_IS_OUT_OF_RANGE_MESSAGE = "로또 숫자가 범위 밖입니다.";
     private List<Integer> numbers;
 
     Lotto(List<Integer> numbers) {
@@ -16,6 +22,16 @@ public class Lotto {
         if (numbers.size() != LOTTO_NUMBERS_CARDINALITY) {
             throw new IllegalArgumentException(INVALID_LOTTO_NUMBERS_CARDINALITY_MESSAGE);
         }
+        for (int number : numbers) {
+            if (number < MIN_BOUND || number > MAX_BOUND) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_IS_OUT_OF_RANGE_MESSAGE);
+            }
+        }
+        Set<Integer> numSet = new HashSet<>(numbers);
+        if (numSet.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBERS_MESSAGE);
+        }
+
         return new Lotto(numbers);
     }
 
