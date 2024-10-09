51e46aa45146becc29fadead545222dd4fb0e8b7
jara <jara0701@gmail.com>
2024-10-09 17:22:12 +0900

    refactoring Calculator.java, Operator.java: operand 적용
    operator.java 에 calculate 인자를 Operand 타입으로 바꿈

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Calculator {
     private static final String INPUT_DELIMITER = " ";
-    private static final String NOT_NATURAL_NUMBER_MESSAGE = "자연수가 아닙니다.";
     private static final String EMPTY_INPUT_MESSAGE = "입력걊이 비어있습니다.";
 
     public static int calculate(String input) {
@@ -24,14 +23,6 @@ public class Calculator {
     private static void checkInputIsNullOrEmpty(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException(EMPTY_INPUT_MESSAGE);
-        }
-    }
-
-    private static int convertToIntOperand(String operandSymbol) {
-        try {
-            return Integer.parseInt(operandSymbol);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_NATURAL_NUMBER_MESSAGE);
         }
     }
 }
