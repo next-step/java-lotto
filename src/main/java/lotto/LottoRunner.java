68a472d4caec90adedbdff7c7d45e929b8c4f0de
이윤선 <yoonseon3@gmail.com>
2024-10-09 19:39:31 +0900

    chore: 기능별 개행 추가

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoPurchasePrice;
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoWinningResults;
 import lotto.ui.InputView;
@@ -8,15 +9,16 @@ import lotto.ui.ResultView;
 
 public class LottoRunner {
     public static void main(String[] args) {
-        int totalPurchasePrice = InputView.readTotalPurchasePrice();
+        LottoPurchasePrice totalPurchasePrice = InputView.readTotalPurchasePrice();
 
         LottoTicket lottoTicket = new LottoTicket(totalPurchasePrice);
         ResultView.printLottoDetail(lottoTicket);
 
         Lotto lastWeekWinningNumbers = InputView.readLastWeekWinningNumbers();
-        LottoWinningResults lottoWinningResults = lottoTicket.getWinningResult(lastWeekWinningNumbers);
 
+        LottoWinningResults lottoWinningResults = lottoTicket.getWinningResult(lastWeekWinningNumbers);
         ResultView.printWinningStatistics(lottoWinningResults);
+
         ResultView.printProfitRate(totalPurchasePrice, lottoWinningResults);
     }
 }
