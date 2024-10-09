54072dcb9edcf0a0669d1b402d851ee10d0972ae
김혜영 <sunzero0116@gmail.com>
2024-10-09 18:37:27 +0900

    refactor : InputView 사용자가 입력한 값 그대로 값 받도록 처리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    public static String[] getTokens() {
+    public static String getExpression() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)");
         String expression = SCANNER.nextLine();
-        return expression.split(" ");
+        if (expression == null) {
+            return "";
+        }
+        return expression;
     }
 }
