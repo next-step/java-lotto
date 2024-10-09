87cb73a651765bde6684081928ed1f6a8bf318e5
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-09 17:05:23 +0900

    feat: 로또 개수 출력 부분 구현
    로또 개수 출력 부분 구현

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,10 +1,14 @@
 package lotto;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Main {
 
     public static void main(String[] args) {
-        InputView.inputAmount();
+        int money = InputView.inputAmount();
+        int lottoCount = Lotto.lottoCount(money);
+
+        OutputView.lottoCount(lottoCount);
     }
 }
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void lottoCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+}
