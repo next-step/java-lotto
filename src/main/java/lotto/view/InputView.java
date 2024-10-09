9e4d141a06c5f2deee24127f9dd2454e50abfc6b
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-09 16:57:32 +0900

    feat: 금액 입력 구현
    금액 입력 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static int inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return SCANNER.nextInt();
+    }
+}
