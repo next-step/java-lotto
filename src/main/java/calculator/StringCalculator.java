ce7350e0fdf4f98002475c493d25ada6299d6b6d
Joonkb <byeongjunkim@naver.com>
2024-10-09 16:17:12 +0900

    [feat] 사칙연산 기호가 아닌 경우 검증 구현 및 테스트

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,14 +1,63 @@
 package calculator;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StringCalculator {
+
+    private final static String OPERATORS = "+-*/";
+
+    private StringCalculator() {
+    }
 
     public static void splitAndCalculate(String expression) {
         if (isBlankOrNull(expression)) {
             throw new IllegalArgumentException("입력값이 null 또는 빈 공백 문자입니다.");
         }
+
+        createExpressionStack(expression);
     }
 
     private static boolean isBlankOrNull(String expression) {
         return expression == null || expression.isEmpty();
+    }
+
+    private static Deque<String> createExpressionStack(String expression) {
+        Deque<String> stack = new ArrayDeque<>();
+        for (String item : expression.split(" ")) {
+            validateInputValue(item);
+            stack.push(item);
+        }
+        return stack;
+    }
+
+    private static void validateInputValue(String input) {
+        if (!isValidNumber(input)) {
+            throw new IllegalArgumentException("입력값에 음수가 포함되어 있습니다.");
+        }
+
+        if (!isValidOperator(input)) {
+            throw new IllegalArgumentException("사칙연산 기호가 아닌 문자는 허용되지 않습니다.");
+        }
+    }
+
+    private static boolean isValidNumber(String number) {
+        if (isNumber(number) && Integer.parseInt(number) < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidOperator(String input) {
+        return OPERATORS.contains(input);
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 class StringCalculatorTest {
@@ -20,5 +21,12 @@ class StringCalculatorTest {
                 .isThrownBy(() -> {
                     StringCalculator.splitAndCalculate("");
                 }).withMessageMatching("입력값이 null 또는 빈 공백 문자입니다.");
+    }
+
+    @Test
+    void 입력값_사칙연산기호_아닌경우() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            StringCalculator.splitAndCalculate("1 X 3");
+        }).withMessageMatching("사칙연산 기호가 아닌 문자는 허용되지 않습니다.");
     }
 }
