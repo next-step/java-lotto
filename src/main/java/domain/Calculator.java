534a3f36156b9a095791e6addfd8acc057f1e664
jureamer <wngud4950@gmail.com>
2024-10-09 17:13:17 +0900

    feat: add multiply method to calculator

--- src/main/java/domain/Calculator.java
+++ src/main/java/domain/Calculator.java
@@ -3,51 +3,63 @@ package domain;
 import java.util.Arrays;
 
 public class Calculator {
-    public static int calculate(String input) {
-        int currentNumber = 0;
-        boolean isFirst = true;
+    private int result = 0;
+    private boolean isFirst = true;
+    private String lastOperator = "";
 
+    public int calculate(String input) {
         String[] tokens = input.split(" ");
-        String operator = "";
 
         for (String token : tokens) {
-            if (token.equals("+") || token.equals("-")) {
-                operator = token;
-                continue;
-            }
-
-            if (isFirst) {
-                currentNumber = Integer.parseInt(token);
-                isFirst = false;
+            if (token.equals("+") || token.equals("-") || token.equals("*")) {
+                this.lastOperator = token;
                 continue;
             }
 
             try {
-                if (operator.equals("+")) {
-                    currentNumber = plus_two_number(currentNumber, Integer.parseInt(token));
+                if (this.isFirst) {
+                    handleFirstInteger(Integer.parseInt(token));
                     continue;
                 }
-
-                if (operator.equals("-")) {
-                    currentNumber = minusTwoNumber(currentNumber, Integer.parseInt(token));
-                    continue;
-                }
-
+                this.result = calculateByOperator(Integer.parseInt(token));
             } catch (NumberFormatException e) {
-                System.out.println("유효하지 않은 입력입니다. : " + e.getMessage());
-                return 0;
+                throw new NumberFormatException("유효하지 않은 입력입니다. : " + e.getMessage());
             }
         }
-        return currentNumber;
+        return this.result;
     }
 
-    private static int minusTwoNumber(int currentNumber, int i) {
-        return currentNumber - i;
+    private int calculateByOperator(int number) {
+        if (this.lastOperator.equals("+")) {
+            return plus_two_number(this.result, number);
+        }
+
+        if (this.lastOperator.equals("-")) {
+            return minusTwoNumber(this.result, number);
+
+        }
+
+        if (this.lastOperator.equals("*")) {
+            return multiplyTwoNumber(this.result, number);
+        }
+
+        throw new IllegalArgumentException("유효한 사칙연산 기호가 아닙니다.");
     }
 
-    private static int plus_two_number(int currentNumber, int i) {
-        return currentNumber + i;
+    private void handleFirstInteger(int number) {
+        this.result = number;
+        this.isFirst = false;
     }
 
+    private static int multiplyTwoNumber(int currentNumber, int number) {
+        return currentNumber * number;
+    }
 
+    private static int minusTwoNumber(int currentNumber, int number) {
+        return currentNumber - number;
+    }
+
+    private static int plus_two_number(int currentNumber, int number) {
+        return currentNumber + number;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,15 +4,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
     @Test
     public void 플러스_기호가_입력되면_덧셈한다() {
-        assertThat(Calculator.calculate("3 + 2")).isEqualTo(5);
+        assertThat(calculator.calculate("3 + 2")).isEqualTo(5);
     }
 
     @Test
     public void 마이너스_기호가_입력되면_덧셈한다() {
-        assertThat(Calculator.calculate("3 - 2")).isEqualTo(1);
+        assertThat(calculator.calculate("3 - 2")).isEqualTo(1);
     }
 
+    @Test
+    public void 곱셈_기호가_입력되면_곱셈한다() {
+        assertThat(calculator.calculate("3 * 2")).isEqualTo(6);
+    }
 
 }
