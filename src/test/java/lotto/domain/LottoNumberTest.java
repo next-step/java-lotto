5e7a772f3854a86c237d7f26181ac76f5d03c578
SangHyun Kim <ksh9891@gmail.com>
2024-10-10 22:05:47 +0900

    fix(lotto): 테스트 케이스 분리

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -4,18 +4,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LottoNumberTest {
     @Test
-    @DisplayName("로또번호는 1부터 45사이의 숫자만 포함한다.")
+    @DisplayName("로또번호는 1부터 45 사이의 숫자만 포함한다.")
     void shouldContainNumbersBetween1And45Only() {
         final LottoNumber lottoNumber1 = new LottoNumber(1);
         final LottoNumber lottoNumber45 = new LottoNumber(45);
 
-        assertThat(lottoNumber1).isEqualTo(new LottoNumber(1));
-        assertThat(lottoNumber45).isEqualTo(new LottoNumber(45));
+        assertAll(
+            () -> assertThat(lottoNumber1).isEqualTo(new LottoNumber(1)),
+            () -> assertThat(lottoNumber45).isEqualTo(new LottoNumber(45))
+        );
+    }
 
-        assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(0));
-        assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(46));
+    @Test
+    @DisplayName("로또번호는 1부터 45 사이가 아닌 숫자일 경우 예외가 발생한다.")
+    void shouldThrowExceptionWhenNumberIsOutOfRange() {
+        assertAll(
+            () -> assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(0)),
+            () -> assertThatIllegalArgumentException().isThrownBy(() -> new LottoNumber(46))
+        );
     }
 }
