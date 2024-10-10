bcce4765c60e8418877cdf379b34c230db807a5c
sinyebin <dpqls0625@naver.com>
2024-10-10 13:43:40 +0900

    docs 24.10.10 syb 로또 구입금액 에러

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -4,23 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGame {
+    public static final String CAN_NOT_BUY_LOTTO = "로또를 살 수 없는 금액입니다.";
+    public static final String CAN_NOT_BUY_LOTTO_AMOUNT = "로또는 1000원 단위로 구입 가능합니다.";
     private static final int gamePrice = 1000;
     private final int gameMoney;
     private final List<Lotto> lottoList;
-    public LottoGame(int gameMoney){
-        this.gameMoney = gameMoney;
-        this.lottoList = generateLotto(gameMoney/gamePrice);
+
+    public LottoGame(int gameMoney) {
+        this.gameMoney = validateGameMoney(gameMoney);
+        this.lottoList = generateLotto(gameMoney / gamePrice);
     }
 
-    private List<Lotto> generateLotto(int count){
+    private List<Lotto> generateLotto(int count) {
         List<Lotto> lottoList = new ArrayList<>();
-        for(int i=0;i<count;i++){
+        for (int i = 0; i < count; i++) {
             lottoList.add(new Lotto());
         }
         return lottoList;
     }
 
-    public List<Lotto> getLottoList(){
+    private int validateGameMoney(int gameMoney) {
+        if (gameMoney <= 0) {
+            throw new IllegalArgumentException(CAN_NOT_BUY_LOTTO);
+        }
+        if (gameMoney % 1000 != 0) {
+            throw new IllegalArgumentException(CAN_NOT_BUY_LOTTO_AMOUNT);
+        }
+        return gameMoney;
+    }
+
+    public List<Lotto> getLottoList() {
         return lottoList;
     }
 }
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -1,16 +1,38 @@
 package lotto;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static lotto.LottoGame.CAN_NOT_BUY_LOTTO;
+import static lotto.LottoGame.CAN_NOT_BUY_LOTTO_AMOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoGameTest {
     @DisplayName("로또 발급")
     @Test
-    void 로또_발급(){
+    void 로또_발급() {
         LottoGame lottoGame = new LottoGame(30000);
         assertThat(lottoGame.getLottoList()).hasSize(30);
+    }
+
+    @DisplayName("로또 금액 0이하")
+    @Test
+    void 로또_금액_0이하_에러() {
+        assertThatThrownBy(() -> {
+            new LottoGame(0);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CAN_NOT_BUY_LOTTO);
+    }
+
+    @DisplayName("로또 금액 1000단위 아닌 에러")
+    @Test
+    void 로또_금액_1000단위_에러() {
+        assertThatThrownBy(() -> {
+            new LottoGame(1001);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CAN_NOT_BUY_LOTTO_AMOUNT);
     }
 }
