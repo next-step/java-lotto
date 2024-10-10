59698f10dbd80bba5cd7e99ce6bacd3cfbb42be2
배영현 <byh2949@gmail.com>
2024-10-10 19:26:46 +0900

    refactor : displayname 사용

--- src/test/java/calculator/ValidationTest.java
+++ src/test/java/calculator/ValidationTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ValidationTest {
 
     @Test
+    @DisplayName("공백 경우 IllegalArgumentException 반환")
     void 공백인_경우() {
         assertThatThrownBy(() -> {
            Validation.checkNullOrBlank("   ");
@@ -14,6 +16,7 @@ class ValidationTest {
     }
 
     @Test
+    @DisplayName("null인 경우 IllegalArgumentException 반환")
     void null인_경우() {
         assertThatThrownBy(() -> {
            Validation.checkNullOrBlank(null);
