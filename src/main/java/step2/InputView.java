d85c70b701bad956627a11f7e30ea7db37126c65
Ryumina-edu <fbalsk8897@gmail.com>
2024-10-10 14:30:42 +0900

    add lotto ticket count

---
+++ src/main/java/step2/InputView.java
@@ -0,0 +1,29 @@
+package step2;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final int LOTTO_TICKET_PRICE = 1000;
+    private static InputValidator inputValidator = new InputValidator();
+
+    public int getInputAndPrint() {
+        print("구입금액을 입력해 주세요.");
+
+        Scanner scanner = new Scanner(System.in);
+        String inputPrice = scanner.nextLine();
+        inputValidator.validate(inputPrice);
+
+        int lottoTicketCount = getLottoTicketCount(inputPrice);
+        print(lottoTicketCount + "개를 구매했습니다.");
+
+        return lottoTicketCount;
+    }
+
+    private int getLottoTicketCount(String inputPrice) {
+        return Integer.parseInt(inputPrice) / LOTTO_TICKET_PRICE;
+    }
+
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
