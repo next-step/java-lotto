d277928a943e4707f99e43efad8f7257535b9b69
owljoa <needshield@gmail.com>
2024-10-10 19:53:21 +0900

    feat: 1~45 숫자중 무작위 숫자 6개 뽑기 - 중복 없이 뽑기

---
+++ src/main/java/lotto/FixedLottoNumberGenerator.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import java.util.Set;
+
+public class FixedLottoNumberGenerator implements LottoNumberGenerator {
+    @Override
+    public Set<Integer> generateNumbers() {
+        return Set.of(1, 2, 3, 4, 5, 6);
+    }
+}
