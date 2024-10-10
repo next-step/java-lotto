b4610b17277cd8a7314df4df7a3ea44defabd897
owljoa <needshield@gmail.com>
2024-10-10 21:44:40 +0900

    feat: 수익률 계산

--- src/main/java/lotto/ProfitCalculator.java
+++ src/main/java/lotto/ProfitCalculator.java
@@ -3,6 +3,12 @@ package lotto;
 import java.util.List;
 
 public class ProfitCalculator {
+    public static float calculateProfitRate(int totalPurchaseAmount, List<Integer> equalNumberCountList) {
+        int totalProfitAmount = calculateTotalProfit(equalNumberCountList);
+
+        float profitRate = ((float) totalProfitAmount / totalPurchaseAmount) * 100;
+        return Math.round(profitRate * 100) / 100.0f;
+    }
 
     public static int calculateTotalProfit(List<Integer> equalNumberCountList) {
         return equalNumberCountList.stream()
