8a151beda36604ae7c7c8f96fb0f81017e9ed2b6
ming <devel.moon17@gmail.com>
2024-10-10 19:03:45 +0900

    test: 2단계 - 로또 실패하는 테스트 코드 작성

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    public int countByAmount(int amount){
+        return 0;
+    }
+
+    public List<Integer> generateAutoLottoNumbers(){
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+
+    public int checkMatchNumberCount(List<Integer> winningNumbers, List<Integer> numbers){
+        return 0;
+    }
+
+    public int checkWinningLottoCount(){
+        return 0;
+    }
+
+    public double calculateRateOfReturn(){
+        return 0;
+    }
+}
