174bb9dedf020d3d4500531d8278ea697553d117
SangHyun Kim <ksh9891@gmail.com>
2024-10-11 22:51:28 +0900

    feat(lotto): 2등 결과 표시

--- src/main/java/lotto/view/LottoResultView.java
+++ src/main/java/lotto/view/LottoResultView.java
@@ -21,6 +21,7 @@ public class LottoResultView {
         writer.write("3개 일치 (" + LottoRank.FIFTH.getWinningPrice() + "원) - " + results.winningCount(LottoRank.FIFTH) + "개");
         writer.write("4개 일치 (" + LottoRank.FOURTH.getWinningPrice() + "원) - " + results.winningCount(LottoRank.FOURTH) + "개");
         writer.write("5개 일치 (" + LottoRank.THIRD.getWinningPrice() + "원) - " + results.winningCount(LottoRank.THIRD) + "개");
+        writer.write("5개 일치, 보너스 볼 일치 (" + LottoRank.SECOND.getWinningPrice() + "원) - " + results.winningCount(LottoRank.SECOND) + "개");
         writer.write("6개 일치 (" + LottoRank.FIRST.getWinningPrice() + "원) - " + results.winningCount(LottoRank.FIRST) + "개");
     }
 
