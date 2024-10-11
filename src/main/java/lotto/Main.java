e5774123df469bd74d1ebe66880f77df12f251e7
jara <jara0701@gmail.com>
2024-10-11 10:54:17 +0900

    feat Main.java: 메인 함수 구현
    메인 함수 구현

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,4 +1,25 @@
 package lotto;
 
+import lotto.domain.LottoManager;
+import lotto.domain.RandomSelectionStrategy;
+import lotto.dto.LottoStatisticsDTO;
+import lotto.dto.PurchasedLottosDTO;
+import lotto.dto.WinningNumbersDTO;
+import lotto.ui.InputView;
+import lotto.ui.ResultView;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        int price = InputView.priceInput();
+        LottoManager lottoManager = LottoManager.newInstance();
+        lottoManager.buy(price, RandomSelectionStrategy.getInstance());
+        PurchasedLottosDTO purchasedLottos = lottoManager.getPurchasedLottos();
+        ResultView.printPurchasedLottos(purchasedLottos);
+        List<Integer> winnerNumbers = InputView.winnerNumbersInput();
+        lottoManager.decideWinningNumbers(WinningNumbersDTO.valueOf(winnerNumbers));
+        LottoStatisticsDTO statistics = lottoManager.getStatistics();
+        ResultView.printLottoStatistics(statistics);
+    }
 }
