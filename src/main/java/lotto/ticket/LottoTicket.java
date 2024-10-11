09f2c656b222e1eaaa66541903362704fde9405c
green <green@mobidoc.us>
2024-10-11 19:13:23 +0900

    refactor: step2 LottoTicket의 LottoNumbers를 List에서 Set으로 변경

--- src/main/java/lotto/ticket/LottoTicket.java
+++ src/main/java/lotto/ticket/LottoTicket.java
@@ -1,40 +1,38 @@
 package lotto.ticket;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoTicket {
     public static final int NUMBER_OF_LOTTO_NUMBERS = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
 
-    private final List<Integer> lottoNumbers;
+    private final Set<Integer> lottoNumbers;
 
     private LottoTicket(List<Integer> lottoNumbers) {
+        this(new HashSet<>(lottoNumbers));
+    }
+
+    private LottoTicket(Set<Integer> lottoNumbers) {
+        validateNumberOfLottoNumbers(lottoNumbers);
+        validateNumberRange(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
     public static LottoTicket from(List<Integer> lottoNumbers) {
-        validateNumberOfLottoNumbers(lottoNumbers);
-        validateNoDuplicateNumbers(lottoNumbers);
-        validateNumberRange(lottoNumbers);
         return new LottoTicket(lottoNumbers);
     }
 
-    private static void validateNumberOfLottoNumbers(List<Integer> lottoNumbers) {
+    private static void validateNumberOfLottoNumbers(Set<Integer> lottoNumbers) {
         if (lottoNumbers.size() == NUMBER_OF_LOTTO_NUMBERS) {
             return;
         }
         throw new IllegalArgumentException("로또 번호는 6개 입니다.");
     }
 
-    private static void validateNoDuplicateNumbers(List<Integer> lottoNumbers) {
-        if (lottoNumbers.stream().distinct().count() == lottoNumbers.size()) {
-            return;
-        }
-        throw new IllegalArgumentException("로또 번호로 중복된 수를 사용할 수 없습니다.");
-    }
-
-    private static void validateNumberRange(List<Integer> lottoNumbers) {
+    private static void validateNumberRange(Set<Integer> lottoNumbers) {
         boolean isValidNumbers = lottoNumbers.stream()
                 .allMatch(number -> number >= MIN_LOTTO_NUMBER && number <= MAX_LOTTO_NUMBER);
 
