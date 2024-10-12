faf8d76976ed6a202c4556680641b32e7bf6c647
이윤선 <yoonseon3@gmail.com>
2024-10-13 00:30:16 +0900

    fix: FIVE 당첨금액 수정

--- src/main/java/lotto/domain/LottoWinningStatus.java
+++ src/main/java/lotto/domain/LottoWinningStatus.java
@@ -3,8 +3,8 @@ package lotto.domain;
 public enum LottoWinningStatus {
     THREE(3, 5_000),
     FOUR(4, 50_000),
-    FIVE(5, 150_000),
-    SIX(6, 2_000_000_000)
+    FIVE(5, 1_500_000),
+    SIX(6, 2_000_000_000),
     ;
 
     private final int winningCount;
