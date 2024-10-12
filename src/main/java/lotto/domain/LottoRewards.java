31aa9aa88bb5abb5665738a8f06a3c7f24b7ad2d
hwan2-99 <thh1233123@gmail.com>
2024-10-12 11:56:05 +0900

    feat: 당첨보상 정보를 가진 열거형 클래스 선언

---
+++ src/main/java/lotto/domain/LottoRewards.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public enum LottoRewards {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    LottoRewards(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
