3aca7e8bfca51d73abf7a04dd285a20453010a02
SangHyun Kim <ksh9891@gmail.com>
2024-10-12 21:59:31 +0900

    fix(lotto): LottoRank 에서 보너스번호 매치가 필요한지 여부를 구분하는 boolean 값을 추가하여 해당 값이 true 인 경우에 한해서만 보너스 번호를 비교하도록 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,22 +1,23 @@
 package lotto.domain;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public enum LottoRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    NONE(0, 0);
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    NONE(0, 0, false);
 
     private final int matchedNumber;
     private final int winningPrice;
+    private final boolean requiresBonusMatch;
 
-    LottoRank(final int matchedNumber, final int winningPrice) {
+    LottoRank(final int matchedNumber, final int winningPrice, final boolean requiresBonusMatch) {
         this.matchedNumber = matchedNumber;
         this.winningPrice = winningPrice;
+        this.requiresBonusMatch = requiresBonusMatch;
     }
 
     public static LottoRank findLottoRankByMatchedNumber(final int matchedNumber) {
@@ -26,17 +27,9 @@ public enum LottoRank {
     public static LottoRank findLottoRankByMatchedNumber(final int matchedNumber, final boolean isMatchedBonusNumber) {
         return Arrays.stream(values())
             .filter(value -> value.matchedNumber == matchedNumber)
-            .sorted(Comparator.comparingInt(LottoRank::getWinningPrice))
-            .map(rank -> adjustRankWithBonus(rank, isMatchedBonusNumber))
-            .findAny()
+            .filter(rank -> !rank.requiresBonusMatch || isMatchedBonusNumber)
+            .findFirst()
             .orElse(NONE);
-    }
-
-    private static LottoRank adjustRankWithBonus(final LottoRank lottoRank, final boolean isMatchedBonusNumber) {
-        if (THIRD.equals(lottoRank) && isMatchedBonusNumber) {
-            return SECOND;
-        }
-        return lottoRank;
     }
 
     public long calculateWinningPrice(final int winningLottoCount) {
