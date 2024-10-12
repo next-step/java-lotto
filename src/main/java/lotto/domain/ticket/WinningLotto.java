4939432f154984c4183d06a0fd5aa0453495b5ea
JIHO CHOI <wlghsp@gmail.com>
2024-10-12 18:11:15 +0900

    refactor: 로또 번호로 당첨 로또 생성 팩토리 메서드 추가

--- src/main/java/lotto/domain/ticket/WinningLotto.java
+++ src/main/java/lotto/domain/ticket/WinningLotto.java
@@ -2,7 +2,8 @@ package lotto.domain.ticket;
 
 import lotto.domain.number.LottoNumber;
 
-import java.util.Objects;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinningLotto {
 
@@ -16,20 +17,19 @@ public class WinningLotto {
         return new WinningLotto(winningTicket);
     }
 
+    public static WinningLotto fromWinningNumbers(List<String> winningNumbers) {
+        List<LottoNumber> lottoNumbers = generateLottoNumbers(winningNumbers);
+        return WinningLotto.of(LottoTicket.of(lottoNumbers));
+    }
+
+    private static List<LottoNumber> generateLottoNumbers(List<String> winningNumbers) {
+        return winningNumbers.stream()
+                .map(Integer::parseInt)
+                .map(LottoNumber::of)
+                .collect(Collectors.toList());
+    }
+
     public boolean winningMatch(LottoNumber number) {
         return winningTicket.match(number);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        WinningLotto that = (WinningLotto) o;
-        return Objects.equals(winningTicket, that.winningTicket);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(winningTicket);
     }
 }
