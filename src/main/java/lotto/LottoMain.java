97af7e04ea9558f0e5e645b2defc556eb636c9e5
Joonkb <byeongjunkim@naver.com>
2024-10-12 13:22:27 +0900

    [feat] view 기능 개선 및 추가

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,2 +1,34 @@
-package lotto;public class LottoMain {
+package lotto;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+import lotto.domain.WinningResult;
+import lotto.view.ResultView;
+
+import java.util.List;
+
+import static lotto.view.InputView.inputAmount;
+import static lotto.view.InputView.inputLastWinningNumbers;
+
+public class LottoMain {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public static void main(String[] args) {
+
+        int lottoQuantity = inputAmount() / LOTTO_PRICE;
+        ResultView.printPurchaseQuantity(lottoQuantity);
+
+        LottoGame lottoGame = new LottoGame(lottoQuantity);
+        for (Lotto lotto : lottoGame.getLottos()) {
+            ResultView.printLotto(lotto);
+        }
+
+        Lotto winningNumbers = new Lotto(inputLastWinningNumbers());
+        WinningResult winningResult = lottoGame.calculateLottoResult(winningNumbers);
+
+        ResultView.printMatchStaticsInfo(winningResult.getStaticsList());
+
+        ResultView.printProfitRate(winningResult.calculateProfitRate());
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,7 +8,9 @@ public class InputView {
 
     public static int inputAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+        return amount;
     }
 
     public static String inputLastWinningNumbers() {
