bc7694709b12e156aa0901994c5ce18f5460ab09
hwan2-99 <thh1233123@gmail.com>
2024-10-12 10:54:18 +0900

    feat: 구입금액 객체 구현

---
+++ src/main/java/lotto/domain/PurchasePrice.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+public class PurchasePrice {
+    private static final int PRICE_UNIT = 1000;
+    private static final String INCORRECT_UNIT_ERROR = "로또 구입금액은 " + PRICE_UNIT + "원 단위여야 합니다.";
+    private static final String INCORRECT_VALUE_ERROR = "로또 구입금액의 최소금액은 " + PRICE_UNIT + "원 입니다.";
+    private final int money;
+
+    public PurchasePrice(final int money) {
+        validateUnit(money);
+        validateValue(money);
+        this.money = money;
+    }
+
+    private void validateUnit(final int money) {
+        if (money % PRICE_UNIT != 0) {
+            throw new IllegalArgumentException(INCORRECT_UNIT_ERROR);
+        }
+    }
+
+    private void validateValue(final int money) {
+        if (money < PRICE_UNIT) {
+            throw new IllegalArgumentException(INCORRECT_VALUE_ERROR);
+        }
+    }
+
+}
---
+++ src/test/java/lotto/domain/PurchasePriceTest.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PurchasePriceTest {
+    @Test
+    void 구입금액_객체생성() {
+        PurchasePrice purchasePrice = new PurchasePrice(14000);
+        assertThat(purchasePrice).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-100, 0, 1400})
+    void 구입금액_검증(int value) {
+        assertThatThrownBy(() -> new PurchasePrice(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
