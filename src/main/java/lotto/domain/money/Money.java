d60d5621b8536df320187cebd7525d1c82e447eb
JIHO CHOI <wlghsp@gmail.com>
2024-10-12 18:09:02 +0900

    refactor: Money 클래스 생성자 검증 및 구입 가능한 로또 갯수 메서드 검증 추가
    - 구입 금액 음수 여부 체크 예외 발생
    - 로또 티켓 가격이 금액보다 크면 예외 발생

--- src/main/java/lotto/domain/money/Money.java
+++ src/main/java/lotto/domain/money/Money.java
@@ -1,14 +1,24 @@
 package lotto.domain.money;
 
+import lotto.exception.InvalidMoneyAmountException;
+
 public class Money {
 
     private final long amount;
 
     private Money(long amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("구입금액은 양수여야 합니다.");
-        }
+        checkIfNegativeAmount(amount);
         this.amount = amount;
+    }
+
+    private static void checkIfNegativeAmount(long amount) {
+        if (isLessThanZero(amount)) {
+            throw new InvalidMoneyAmountException("구입금액은 양수여야 합니다.");
+        }
+    }
+
+    private static boolean isLessThanZero(long amount) {
+        return amount < 0;
     }
 
     public static Money of(long amount) {
@@ -20,6 +30,17 @@ public class Money {
     }
 
     public int availableLottoTickets(long lottoTicketPrice) {
+        checkIfAmountIsLessThanLottoTicketPrice(lottoTicketPrice);
         return (int) (amount / lottoTicketPrice);
+    }
+
+    private void checkIfAmountIsLessThanLottoTicketPrice(long lottoTicketPrice) {
+        if (isAmountLessThanLottoTicketPrice(lottoTicketPrice)) {
+            throw new InvalidMoneyAmountException("구입금액이 로또 티켓 1개 가격보다 작습니다.");
+        }
+    }
+
+    private boolean isAmountLessThanLottoTicketPrice(long lottoTicketPrice) {
+        return amount < lottoTicketPrice;
     }
 }
