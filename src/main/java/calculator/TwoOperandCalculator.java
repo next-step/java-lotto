8be3992de4d535b178924d31fbd25c12377722ea
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-13 00:53:15 +0900

    refactor : 미사용 상수, 필드 변수  제거

--- src/main/java/calculator/TwoOperandCalculator.java
+++ src/main/java/calculator/TwoOperandCalculator.java
@@ -1,17 +1,8 @@
 package calculator;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.IntBinaryOperator;
 
 public class TwoOperandCalculator {
-    private static final Map<String, IntBinaryOperator> operaterMap = new HashMap<>();
-
-    public static final String PLUS = "+";
-    public static final String MINUS = "-";
-    public static final String MULTIPLY = "*";
-    public static final String DEVIDE = "/";
-
     private static TwoOperandCalculator twoOperandCalculator;
     Operator operator;
 
