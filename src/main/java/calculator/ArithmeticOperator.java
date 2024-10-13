0e017d8dc73b1ee2651ed1dac7636570bdfdc679
Joonkb <byeongjunkim@naver.com>
2024-10-13 20:52:52 +0900

    [refactor] step-1 람다식으로 변경 및 BinaryOperator 사용변경

--- src/main/java/calculator/ArithmeticOperator.java
+++ src/main/java/calculator/ArithmeticOperator.java
@@ -1,6 +1,7 @@
 package calculator;
 
-import java.util.function.BiFunction;
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
 
 public enum ArithmeticOperator {
 
@@ -13,15 +14,11 @@ public enum ArithmeticOperator {
     private static final String OPERATORS = "+-*/";
 
     private final String operator;
-    private final BiFunction<Integer, Integer, Integer> biFunction;
+    private final BinaryOperator<Integer> binaryOperator;
 
-    ArithmeticOperator(String operator, BiFunction<Integer, Integer, Integer> biFunction) {
+    ArithmeticOperator(String operator, BinaryOperator<Integer> binaryOperator) {
         this.operator = operator;
-        this.biFunction = biFunction;
-    }
-
-    public String getOperator() {
-        return operator;
+        this.binaryOperator = binaryOperator;
     }
 
     public static boolean isValidOperator(String operator) {
@@ -29,15 +26,13 @@ public enum ArithmeticOperator {
     }
 
     public int calculate(String operand1, String operand2) {
-        return this.biFunction.apply(Integer.parseInt(operand1), Integer.parseInt(operand2));
+        return binaryOperator.apply(Integer.parseInt(operand1), Integer.parseInt(operand2));
     }
 
     public static ArithmeticOperator findEnumByOperatorValue(String operator) {
-        for (ArithmeticOperator value : ArithmeticOperator.values()) {
-            if (operator.equals(value.getOperator())) {
-                return value;
-            }
-        }
-        throw new IllegalArgumentException("사칙연산 기호가 아닌 문자는 허용되지 않습니다.");
+        return Arrays.stream(values())
+                .filter(value -> operator.equals(value.operator))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("사칙연산 기호가 아닌 문자는 허용되지 않습니다."));
     }
 }
