160179051abfb0662ce32011308d500c1e4ae8e9
skdev0619 <skdev0619@naver.com>
2024-10-13 17:17:33 +0900

    수동 로또 입력 추가

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public interface InputView {
     int getPurchaseAmountFromUser();
+    String[] getManualLottosFromUser();
     String getWinningNumbersFromUser();
     int getBonusBallFromUser();
 }
