517db3557e6c78f2370f7e6767052be9c4fb693c
skdev0619 <skdev0619@naver.com>
2024-10-13 16:22:40 +0900

    기존 로또 구매 기능 > 자동 번호 생성 구매 기능으로 변경

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,12 +1,19 @@
 package lotto.domain;
 
 public class LottoMachine {
-    private final int PRICE = 1000;
+    private static final int PRICE = 1000;
 
-    public int buyLottos(int purchaseAmount) {
-        if(purchaseAmount <= 0){
-            throw new IllegalArgumentException("로또를 구매할 수 없습니다");
+    private int purchaseAmount;
+
+    public LottoMachine(int purchaseAmount) {
+        if(purchaseAmount < PRICE){
+            throw new IllegalArgumentException("로또 구매를 위해서 최소 1000원 이상의 금액이 필요합니다");
         }
-        return purchaseAmount / PRICE;
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public Lottos buyAutoLottos(LottoNumbersGenerater generator) {
+        int lottoCount = purchaseAmount / PRICE;
+        return Lottos.create(lottoCount, generator);
     }
 }
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -3,19 +3,38 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoMachineTest {
 
 
-    @DisplayName("로또는 구매 금액/로또 금액의 몫만큼 구매 가능하다")
+    @DisplayName("로또는 구매 금액/로또 금액의 몫만큼 자동 번호 생성 구매 가능하다")
     @Test
     void buyLottos(){
         int purchaseAmount = 12500;
-        LottoMachine machine = new LottoMachine();
+        LottoMachine machine = new LottoMachine(purchaseAmount);
 
-        int lottoCount = machine.buyLottos(purchaseAmount);
+        LottoNumbersGenerater generater = () -> List.of(
+                LottoNumber.valueOf(1), LottoNumber.valueOf(2),
+                LottoNumber.valueOf(3), LottoNumber.valueOf(4),
+                LottoNumber.valueOf(5), LottoNumber.valueOf(6)
+        );
+        Lottos lottos = machine.buyAutoLottos(generater);
 
-        assertThat(lottoCount).isEqualTo(12);
+        assertThat(lottos.count()).isEqualTo(12);
     }
+
+    @DisplayName("로또 구매를 위해서 최소 1000원 이상의 금액이 필요하다")
+    @Test
+    void limitLottoPrice(){
+        int purchaseAmount = 999;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoMachine(purchaseAmount))
+                .withMessage("로또 구매를 위해서 최소 1000원 이상의 금액이 필요합니다");
+    }
+
 }
