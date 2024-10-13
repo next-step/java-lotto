5ea04cfebf130142d5b8e46edc4a1e344809958c
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-10-13 22:15:39 +0900

    ## refactor : 문자열 계산기 리팩토링 및 테스트 코드 추가

---
+++ src/main/java/service/Calculator.java
@@ -0,0 +1,67 @@
+package service;
+
+import java.util.List;
+
+public class Calculator {
+
+    public Calculator() {}
+
+    public int calculate(List<Integer> numbers, List<Character> operations) {
+
+        int result = numbers.get(0);
+
+        for (int i = 1; i < numbers.size(); i++) {
+            int num2 = numbers.get(i);
+            char operation = operations.get(i-1);
+
+            result = calculate(result, num2, operation);
+        }
+
+        return result;
+    }
+
+    public int calculate(int num1, int num2, Character operation) {
+        if (operation == '+') {
+            return add(num1, num2);
+        }
+
+        if (operation == '-') {
+            return substract(num1, num2);
+        }
+
+        if (operation == '*') {
+            return multiply(num1, num2);
+        }
+
+        if (operation == '/') {
+            if (isInteger(num1, num2)) {
+                return devide(num1, num2);
+            }
+        }
+
+        return -1;
+    }
+
+    private int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private int substract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    private int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    private int devide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    private boolean isInteger(int num1, int num2) {
+        if(!(num1 % num2 == 0)) {
+            throw new ArithmeticException("두 수의 나눗셈 결과가 정수가 아닙니다.");
+        }
+        return true;
+    }
+}
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,34 +1,43 @@
 import org.junit.jupiter.api.Test;
+import service.Calculator;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
     @Test
-    public void 덧셈 () {
+    public void 문자열_계산기() {
         Calculator calculator = new Calculator();
-
-        assertThat(calculator.add(3, 4)).isEqualTo(7);
+        assertThat(calculator.calculate(List.of(2, 3, 4, 2), List.of('+', '*', '/'))).isEqualTo(10);
     }
 
     @Test
-    public void 뺄셈 () {
+    public void 덧셈() {
         Calculator calculator = new Calculator();
 
-        assertThat(calculator.substract(7,4)).isEqualTo(3);
+        assertThat(calculator.calculate(4, 2, '+')).isEqualTo(6);
     }
 
     @Test
-    public void 곱셈 () {
+    public void 뺄셈() {
         Calculator calculator = new Calculator();
 
-        assertThat(calculator.multiply(3, 4)).isEqualTo(12);
+        assertThat(calculator.calculate(4, 2, '-')).isEqualTo(2);
     }
 
     @Test
-    public void 나눗셈 () {
+    public void 곱셈() {
         Calculator calculator = new Calculator();
 
-        assertThat(calculator.devide(6, 3)).isEqualTo(2);
+        assertThat(calculator.calculate(4, 2, '*')).isEqualTo(8);
+    }
+
+    @Test
+    public void 나눗셈() {
+        Calculator calculator = new Calculator();
+
+        assertThat(calculator.calculate(4, 2, '/')).isEqualTo(2);
     }
 }
