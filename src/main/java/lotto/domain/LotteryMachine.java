92321a323c569c0fde634e839e960636b49b24bf
StoneHee99 <kiit0901@gmail.com>
2024-10-13 15:20:29 +0900

    refactor: 생성자 매개변수에 final 키워드 추가

--- src/main/java/lotto/domain/LotteryMachine.java
+++ src/main/java/lotto/domain/LotteryMachine.java
@@ -4,11 +4,11 @@ public class LotteryMachine {
 
     private final int purchasePrice;
 
-    public LotteryMachine(String purchasePrice) {
+    public LotteryMachine(final String purchasePrice) {
         this(Integer.parseInt(purchasePrice));
     }
 
-    public LotteryMachine(int purchasePrice) {
+    public LotteryMachine(final int purchasePrice) {
         this.purchasePrice = purchasePrice;
     }
 
