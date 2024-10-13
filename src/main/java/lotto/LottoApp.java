1466f9e5af2153bb7a780537c9d37eb0d1888cd7
soohyun <developer.shkim@gmail.com>
2024-10-13 22:02:12 +0900

    feat(lotto): 구입 금액 입력 구현

---
+++ src/main/java/lotto/LottoApp.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import static lotto.view.InputView.inputPayment;
+
+public class LottoApp {
+    public static void main(String[] args) {
+        inputPayment();
+    }
+}
+
---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,13 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int inputPayment() {
+        System.out.println("구입금액을 입력해 주세요.");
+        
+        return scanner.nextInt();
+    }
+}
