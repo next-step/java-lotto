95078225c6ef67cab7f60c03b8e9c40057a49d99
StoneHee99 <kiit0901@gmail.com>
2024-10-13 15:28:21 +0900

    feat: 로또 자판기가 발급된 로또 개수를 잘 반환하는지

--- src/main/java/lotto/domain/LotteryMachine.java
+++ src/main/java/lotto/domain/LotteryMachine.java
@@ -2,7 +2,15 @@ package lotto.domain;
 
 public class LotteryMachine {
 
+    private static final int LOTTO_PRICE = 1_000;
+
+    public int getIssuedLottoNumber() {
+        return issuedLottoNumber;
+    }
+
     private final int purchasePrice;
+
+    private final int issuedLottoNumber;
 
     public LotteryMachine(final String purchasePrice) {
         this(Integer.parseInt(purchasePrice));
@@ -10,6 +18,7 @@ public class LotteryMachine {
 
     public LotteryMachine(final int purchasePrice) {
         this.purchasePrice = purchasePrice;
+        issuedLottoNumber = purchasePrice / LOTTO_PRICE;
     }
 
     @Override
