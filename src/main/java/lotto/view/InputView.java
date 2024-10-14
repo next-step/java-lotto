1b62e19a6c2f7277ec3537359cc039f7e7d41650
Park Jungwook <cco2416@gmail.com>
2024-10-14 22:30:02 +0900

    refactor : exception 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -19,29 +19,45 @@ public class InputView {
     private static final String WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
     public int getCashAmount() {
-        System.out.println(CASH_AMOUNT_MESSAGE);
-        int cashAmount = sc.nextInt();
-        sc.nextLine();
-        return cashAmount;
+        try {
+            int cashAmount = 0;
+            System.out.println(CASH_AMOUNT_MESSAGE);
+            cashAmount = sc.nextInt();
+            sc.nextLine();
+            return cashAmount;
+        } catch (Exception e) {
+            sc.nextLine();
+            return 0;
+        }
     }
 
     public int getManualLottoCount() {
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        int count = sc.nextInt();
-        sc.nextLine();
-        return count;
+        try {
+            System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+            int count = sc.nextInt();
+            sc.nextLine();
+            return count;
+        } catch (Exception e) {
+            sc.nextLine();
+            return 0;
+        }
     }
 
     public List<Lotto> getManualLottoNumbers(int count) {
-        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Arrays.stream(sc.nextLine().split(", "))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-            lottoList.add(new Lotto(new HashSet<>(numbers)));
+        try {
+            System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+            List<Lotto> lottoList = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                List<Integer> numbers = Arrays.stream(sc.nextLine().split(", "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                lottoList.add(new Lotto(new HashSet<>(numbers)));
+            }
+            return lottoList;
+        } catch (Exception e) {
+            sc.nextLine();
+            return new ArrayList<>();
         }
-        return lottoList;
     }
 
     public void printPurchaseCount(int purchaseCount) {
@@ -57,9 +73,14 @@ public class InputView {
     }
 
     public int getBonusNumber() {
-        System.out.println("보너스 볼을 입력해 주세요.");
-        int number = sc.nextInt();
-        sc.nextLine();
-        return number;
+        try {
+            System.out.println("보너스 볼을 입력해 주세요.");
+            int number = sc.nextInt();
+            sc.nextLine();
+            return number;
+        } catch (Exception e) {
+            sc.nextLine();
+            return getBonusNumber();
+        }
     }
 }
