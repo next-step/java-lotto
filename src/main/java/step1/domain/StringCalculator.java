f1e08e1859013617cb0ab7a42460b57df9b3f741
SeongJunWi <dnltjdwns235@gmail.com>
2024-10-14 21:09:45 +0900

    feat 사칙연산 기능 , 테스트 구현 변경점: 1. 덧셈, 뺄셈, 곱셉, 나눗셈 테스트 구현

--- src/main/java/step1/domain/StringCalculator.java
+++ src/main/java/step1/domain/StringCalculator.java
@@ -2,6 +2,8 @@ package step1.domain;
 
 public class StringCalculator {
 	public int calculate(String sentence) {
+		String[] stringList = sentence.split(" ");
+
 		return 0;
 	}
 }
--- src/test/java/step1/domain/StringCalculatorTest.java
+++ src/test/java/step1/domain/StringCalculatorTest.java
@@ -1,6 +1,43 @@
 package step1.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class StringCalculatorTest {
 	StringCalculator sc = new StringCalculator();
+
+	@Test
+	@DisplayName("덧셈 테스트")
+	void sumTest() {
+		assertThat(sc.calculate("1 + 2")).isEqualTo(3);
+		assertThat(sc.calculate("3 + 4")).isEqualTo(7);
+		assertThat(sc.calculate("1 + 2 + 3")).isEqualTo(6);
+	}
+
+	@Test
+	@DisplayName("뺄셈 테스트")
+	void diffTest() {
+		assertThat(sc.calculate("2 - 1")).isEqualTo(1);
+		assertThat(sc.calculate("3 - 5")).isEqualTo(-2);
+		assertThat(sc.calculate("6 - 3 - 2")).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("곱하기 테스트")
+	void multiplyTest() {
+		assertThat(sc.calculate("3 * 2")).isEqualTo(6);
+		assertThat(sc.calculate("-1 * 2")).isEqualTo(-2);
+		assertThat(sc.calculate("1 * 2 * 0")).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("나누기 테스트")
+	void divideTest() {
+		assertThat(sc.calculate("5 % 2")).isEqualTo(2);
+		assertThat(sc.calculate("4 % 1")).isEqualTo(4);
+		assertThat(sc.calculate("8 % 2 % 2")).isEqualTo(2);
+	}
 
 }
