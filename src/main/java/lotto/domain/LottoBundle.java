ff63e22fcac6a49b214111a1648db676eb063bdd
SangHyun Kim <ksh9891@gmail.com>
2024-10-14 23:43:32 +0900

    feat(lotto): 자동로또의 크기와 수동로또의 크기를 별도로 조회 할 수 있는 메서드 추가

--- src/main/java/lotto/domain/LottoBundle.java
+++ src/main/java/lotto/domain/LottoBundle.java
@@ -3,8 +3,8 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-import static lotto.domain.Lotto.LOTTO_NUMBER_COUNT;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoBundle implements Iterable<Lotto> {
     private final List<Lotto> lottoBundle;
@@ -17,12 +17,33 @@ public class LottoBundle implements Iterable<Lotto> {
         this.lottoBundle = new ArrayList<>(lottoBundle);
     }
 
+    public static LottoBundle mergeAutoAndManualLottoBundle(final LottoBundle manualLottoBundle, final LottoBundle autoLottoBundle) {
+        return new LottoBundle(
+            Stream.concat(
+                manualLottoBundle.lottoBundle.stream(),
+                autoLottoBundle.lottoBundle.stream()
+            ).collect(Collectors.toUnmodifiableList())
+        );
+    }
+
     public static LottoBundle createLottoBundle(final int lottoBundleCount) {
         final List<Lotto> lottoBundle = new ArrayList<>();
         for (int i = 0; i < lottoBundleCount; i++) {
-            lottoBundle.add(new Lotto(LottoNumber.getNewLottoNumbers(LOTTO_NUMBER_COUNT)));
+            lottoBundle.add(Lotto.createAutoLotto());
         }
         return new LottoBundle(lottoBundle);
+    }
+
+    public int manualSize() {
+        return (int) lottoBundle.stream()
+            .filter(Lotto::isManualLotto)
+            .count();
+    }
+
+    public int autoSize() {
+        return (int) lottoBundle.stream()
+            .filter(Lotto::isAutoLotto)
+            .count();
     }
 
     public int size() {
