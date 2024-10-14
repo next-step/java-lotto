1f0dfce4557f31696abdffea4a863a5744984432
JIHO CHOI <jh.choi2@okestro.com>
2024-10-14 14:13:33 +0900

    refactor: static 블록 삭제 후 static 바로 할당

--- src/main/java/lotto/domain/number/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/domain/number/RandomLottoNumberGenerator.java
@@ -3,20 +3,18 @@ package lotto.domain.number;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static lotto.domain.number.LottoNumber.FIRST_NUMBER;
 import static lotto.domain.number.LottoNumber.LAST_NUMBER;
 import static lotto.domain.ticket.LottoTicket.SIZE_OF_LOTTO;
 
 public class RandomLottoNumberGenerator implements LottoNumberGenerator {
-    private static final List<Integer> CANDIDATES;
 
-    static {
-        CANDIDATES = new ArrayList<>();
-        for (int number = FIRST_NUMBER; number <= LAST_NUMBER; number++) {
-            CANDIDATES.add(number);
-        }
-    }
+    private static final List<Integer> CANDIDATES = IntStream.rangeClosed(FIRST_NUMBER, LAST_NUMBER)
+            .boxed()
+            .collect(Collectors.toList());
 
     @Override
     public List<Integer> create() {
