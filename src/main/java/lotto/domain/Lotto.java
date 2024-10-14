56a6b32d3a1592d876557e12da07ea09bfdb9c4b
StoneHee99 <kiit0901@gmail.com>
2024-10-14 23:03:51 +0900

    refactor: 기존의 원시값을 사용하던 로또 번호를 포장

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,29 +4,30 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
     private static final int LOTTO_SIZE = 6;
 
-    private final Set<Integer> lottoNumbers;
+    private final Set<LottoNumber> lottoNumbers;
 
-    public Lotto(final Set<Integer> lottoNumbers) {
+    public Lotto(final Set<LottoNumber> lottoNumbers) {
         valid(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
-    private void valid(Set<Integer> lottoNumbers) {
+    private void valid(Set<LottoNumber> lottoNumbers) {
         if (lottoNumbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
     }
 
     public Lotto(final int... numbers) {
-        this(new HashSet<>(Arrays.asList(
-                Arrays.stream(numbers)
-                        .boxed()
-                        .toArray(Integer[]::new))));
+        this(Arrays.stream(numbers)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toSet()));
     }
 
     public Lotto(final String numbers) {
