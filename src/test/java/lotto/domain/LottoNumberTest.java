3199d315814ec4eb1b80f775c3e6a4c1a7002fac
StoneHee99 <kiit0901@gmail.com>
2024-10-14 22:59:27 +0900

    feat: 로또 번호가 1보다 작거나 45보다 큰 경우 예외를 반환

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoNumberTest {
 
@@ -12,5 +15,13 @@ class LottoNumberTest {
     void lottoNumberTest() {
         assertThat(new LottoNumber(1))
                 .isEqualTo(new LottoNumber(1));
+    }
+
+    @DisplayName("로또 번호가 1보다 작거나 45보다 큰 경우 IllegalArgumentException을 잘 반환하는지")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void lottoNumberTest_withInvalidValue(int number) {
+        assertThatThrownBy(() -> new LottoNumber(number))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
