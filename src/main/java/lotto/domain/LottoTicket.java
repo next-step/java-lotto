dcaeff808511f6cc56a8f896c8f824fdcfab9950
이윤선 <yoonseon3@gmail.com>
2024-10-14 22:51:52 +0900

    fix: LottoPurchasePrice 내부로 금액 검증 로직 이동으로 불필요 검증 제거

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -13,11 +13,8 @@ public class LottoTicket {
     }
 
     public LottoTicket(final LottoPurchasePrice purchasePrice, final List<Lotto> manualTicket) {
-        int autoIssueCount = purchasePrice.getAutoLottoCount(manualTicket.size());
-        if (autoIssueCount < 0) {
-            throw new IllegalArgumentException("로또 구매 금액보다 많은 양의 수동 로또가 생성되었습니다.");
-        }
         this.ticket = new ArrayList<>(manualTicket);
+        int autoIssueCount = purchasePrice.getAutoLottoCount(manualTicket.size());
         issue(autoIssueCount);
     }
 
