d20e691d564e81eaf44384e9143a6d83c28df38c
StoneHee99 <kiit0901@gmail.com>
2024-10-15 00:55:25 +0900

    test: 실패하는 테스트 작성 (로또와 다른 로또를 비교해서 겹치는 숫자의 개수를 반환)

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
@@ -41,4 +42,16 @@ class LottoTest {
                 Arguments.of((Object) new int[]{})
         );
     }
+
+    @DisplayName("로또가 다른 로또와 비교해서 겹치는 숫자의 개수를 잘 반환하는지")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6/6", "1,2,3,4,5,7/5", "1,2,3,4,7,8/4", "1,2,3,7,8,9/3", "1,2,7,8,9,10/2", "1,7,8,9,10,11/1", "7,8,9,10,11,12/0"}, delimiter = '/')
+    void matchTest(String lottoNumbers, int expected) {
+        Lotto lotto = new Lotto(lottoNumbers);
+        Lotto winningLotto = new Lotto("1, 2, 3, 4, 5, 6");
+
+        assertThat(lotto.match(winningLotto))
+                .isEqualTo(expected);
+    }
+
 }
