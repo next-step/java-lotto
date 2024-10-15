25bcb5d891bc110ba55b1609ce9cabca28f02ce2
jara <jara0701@gmail.com>
2024-10-16 02:02:13 +0900

    feat InputView.java: 수동 로또 횟수 입력하는 함수 구현
    수동 로또 횟수 입력하는 함수 구현

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -8,6 +8,7 @@ import java.util.function.Function;
 public final class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String PRICE_QUESTION_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String WINNER_NUMBERS_QUESTION_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_QUESTION_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String BLANK_MESSAGE = "입력하신 값이 비어있습니다.";
@@ -18,6 +19,10 @@ public final class InputView {
 
     public static int priceInput() {
         return input(PRICE_QUESTION_MESSAGE, InputView::isNumberInputValid, InputView::convertToNumber);
+    }
+
+    public static int manualLottoCountInput() {
+        return input(MANUAL_LOTTO_COUNT_MESSAGE, InputView::isNumberInputValid, InputView::convertToNumber);
     }
 
     public static int bonusNumberInput() {
