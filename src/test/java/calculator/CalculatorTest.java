258f8c44e42a9842814c3428b5e30d3b374d4b64
최수봉 <soobong.choi@meshkorea.net>
2024-10-16 00:34:50 +0900

    feat: 두 숫자의 사칙 연산이 잘 수행되는지 확인

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,8 +1,8 @@
 package calculator;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -31,6 +31,27 @@ class CalculatorTest {
   void addCorrectly() {
     int result = Calculator.calculate("2 + 3");
     assertThat(result).isEqualTo(5);
+  }
+
+  @DisplayName("두 숫자의 뺄셈 연산이 잘 수행된다.")
+  @Test
+  void subtractCorrectly() {
+    int result = Calculator.calculate("2 - 1");
+    assertThat(result).isEqualTo(1);
+  }
+
+  @DisplayName("두 숫자의 곱셈 연산이 잘 수행된다.")
+  @Test
+  void multiplyCorrectly() {
+    int result = Calculator.calculate("2 * 3");
+    assertThat(result).isEqualTo(6);
+  }
+
+  @DisplayName("두 숫자의 나눗셈 연산이 잘 수행된다.")
+  @Test
+  void divideCorrectly() {
+    int result = Calculator.calculate("4 / 2");
+    assertThat(result).isEqualTo(2);
   }
 
 }
