1227cb18a75b1f5be21822170924805ada701a96
jeonghyeonkwon <givejeong2468@gmail.com>
2024-10-15 20:01:25 +0900

    feat,docs: LottoNumberRangeTest 작성 및 README 도메인 내용 수정

--- src/main/java/lotto/entity/PrizeMoney.java
+++ src/main/java/lotto/entity/PrizeMoney.java
@@ -5,10 +5,10 @@ import java.math.BigDecimal;
 
 public enum PrizeMoney {
     LOSE(0, BigDecimal.valueOf(0)),
-    THREE(3, BigDecimal.valueOf(5000)),
-    FOUR(4, BigDecimal.valueOf(50000)),
-    FIVE(5, BigDecimal.valueOf(1500000)),
-    SIX(6, BigDecimal.valueOf(2000000000));
+    THREE(3, BigDecimal.valueOf(5_000)),
+    FOUR(4, BigDecimal.valueOf(50_000)),
+    FIVE(5, BigDecimal.valueOf(1_500_000)),
+    SIX(6, BigDecimal.valueOf(2_000_000_000));
 
     private final int collectCount;
     private final BigDecimal prizeMoney;
