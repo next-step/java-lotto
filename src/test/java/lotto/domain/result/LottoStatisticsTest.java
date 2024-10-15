488d7ad4595d9dba6cecd8f54e7418f323560a70
JIHO CHOI <jh.choi2@okestro.com>
2024-10-15 13:52:26 +0900

    refactor: 출력관련 테스트 ConsoleOutputHandlerTest 클래스로 이동

--- src/test/java/lotto/domain/result/LottoStatisticsTest.java
+++ src/test/java/lotto/domain/result/LottoStatisticsTest.java
@@ -4,7 +4,6 @@ import lotto.domain.number.TestLottoNumberFactory;
 import lotto.domain.ticket.LottoTicket;
 import lotto.domain.ticket.LottoTickets;
 import lotto.domain.ticket.WinningLotto;
-import lotto.io.ConsoleOutputHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,12 +11,11 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Map;
 
-import static lotto.io.ConsoleOutputHandler.PROFITABLE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoStatisticsTest {
 
-    public static final int LOTTO_TICKET_PRICE = 1000;
+    private static final int LOTTO_TICKET_PRICE = 1000;
     private LottoTickets lottoTickets;
     private WinningLotto winningLotto;
 
@@ -57,12 +55,4 @@ class LottoStatisticsTest {
         assertThat(statistics.getProfitRatio()).isGreaterThan(1.0);
     }
 
-    @Test
-    @DisplayName("수익률에 따른 평가 메시지를 확인할 수 있다.")
-    void getAssessmentBasedOnProfitRatio() {
-        LottoStatistics statistics = lottoTickets.generateLottoStatistics(winningLotto, LOTTO_TICKET_PRICE);
-        ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
-
-        assertThat(consoleOutputHandler.getAssessmentText(statistics.getProfitRatio())).contains(PROFITABLE);
-    }
 }
--- src/test/java/lotto/domain/ticket/LottoTicketsTest.java
+++ src/test/java/lotto/domain/ticket/LottoTicketsTest.java
@@ -2,7 +2,6 @@ package lotto.domain.ticket;
 
 import lotto.domain.number.TestLottoNumberFactory;
 import lotto.domain.result.Rank;
-import lotto.io.ConsoleOutputHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,16 +13,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketsTest {
 
-    static List<LottoTicket> tickets;
+    private LottoTickets lottoTickets;
+    private WinningLotto winningLotto;
 
     @BeforeEach
     void setup() {
-       tickets = List.of(
+        List<LottoTicket> tickets = List.of(
                 LottoTicket.of(
-                        TestLottoNumberFactory.createLottoNumbers(1, 2, 3, 4, 5 ,6)
+                        TestLottoNumberFactory.createLottoNumbers(1, 2, 3, 4, 5, 6)
                 ),
                 LottoTicket.of(
                         TestLottoNumberFactory.createLottoNumbers(7, 8, 9, 10, 11, 12)
+                )
+        );
+
+        lottoTickets = LottoTickets.of(tickets);
+
+        winningLotto = WinningLotto.of(
+                LottoTicket.of(
+                        TestLottoNumberFactory.createLottoNumbers(1, 2, 3, 4, 5 ,6)
                 )
         );
     }
@@ -31,31 +39,12 @@ class LottoTicketsTest {
     @DisplayName("다수의 로또 티켓을 생성할 수 있다.")
     @Test
     void createMultipleLottoTickets() {
-        LottoTickets lottoTickets = LottoTickets.of(tickets);
         assertThat(lottoTickets.size()).isEqualTo(2);
     }
-
-    @DisplayName("로또 티켓 출력 테스트")
-    @Test
-    void testToStringOfLottoTickets() {
-        LottoTickets lottoTickets = LottoTickets.of(tickets);
-        ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
-
-        String expectedOutput = "[1, 2, 3, 4, 5, 6]\n[7, 8, 9, 10, 11, 12]\n";
-        assertThat(consoleOutputHandler.generateLottoTicketsText(lottoTickets)).isEqualTo(expectedOutput);
-    }
-
 
     @Test
     @DisplayName("당첨 결과에 따른 순위 집계 테스트")
     void countRanksByWinningLotto() {
-        LottoTickets lottoTickets = LottoTickets.of(tickets);
-        WinningLotto winningLotto = WinningLotto.of(
-                LottoTicket.of(
-                        TestLottoNumberFactory.createLottoNumbers(1, 2, 3, 4, 5 ,6)
-                )
-        );
-
         Map<Rank, Integer> rankCounts = lottoTickets.countRanks(winningLotto);
 
         assertThat(rankCounts.get(Rank.FIRST)).isEqualTo(1);
