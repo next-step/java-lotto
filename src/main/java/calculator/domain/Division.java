b0c338f1d933ef685ef5b26f9ced07ee3c5127de
jeonga1022 <pja1048@gmail.com>
2024-10-16 00:26:41 +0900

    feat: 나눗셈 유효성 검증 추가

--- src/main/java/calculator/domain/Division.java
+++ src/main/java/calculator/domain/Division.java
@@ -3,6 +3,17 @@ package calculator.domain;
 public class Division implements Operation {
     @Override
     public int calculate(int a, int b) {
+        validateDivision(a, b);
         return a / b;
+    }
+
+    private static void validateDivision(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (a % b > 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/calculator/domain/DivisionTest.java
+++ src/test/java/calculator/domain/DivisionTest.java
@@ -3,6 +3,7 @@ package calculator.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DivisionTest {
 
@@ -13,4 +14,23 @@ class DivisionTest {
 
         assertThat(division.calculate(6, 2)).isEqualTo(3);
     }
+
+    @Test
+    void 두번째_값은_0을_혀용하지_않는다() {
+
+        assertThatThrownBy(() ->
+                new Division().calculate(5, 0)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    void 결과값은_정수만_허용한다() {
+
+        assertThatThrownBy(() ->
+                new Division().calculate(5, 2)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
 }
