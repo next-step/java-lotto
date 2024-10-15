c24d93b184aa59bc07ab13bd616c5047d2f88710
배영현 <byh2949@gmail.com>
2024-10-15 22:43:52 +0900

    refactor : 매직넘버 상수로

--- src/main/java/lotto/entity/LottoNumber.java
+++ src/main/java/lotto/entity/LottoNumber.java
@@ -1,10 +1,12 @@
 package lotto.entity;
 
 public class LottoNumber {
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
     private final int number;
 
     public LottoNumber(int number) {
-        if(number < 1 || number > 45) {
+        if(number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이여야 합니다.");
         }
         this.number = number;
