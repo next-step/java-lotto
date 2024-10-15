7ecb7d97d6a96dd99c66b6c6006af0ad2746e2b8
minham cho <mhtg90@gmail.com>
2024-10-15 18:08:51 +0900

    fix : 테스트 코드 수정

--- src/test/java/lotto/domain/BuyerTest.java
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -35,7 +35,7 @@ public class BuyerTest {
 
         List<Integer> lottoNumber = Arrays.asList(1,2,3,4,5,6);
 
-        Buyer buyer = new Buyer(1, new TestLottoNumberGenerator());
+        Buyer buyer = new Buyer(1, new TestLottoNumberGenerator(1,2,3,4,5,6));
         assertThat(buyer.getLottoNumbers().get(0)).isEqualTo(lottoNumber);
         assertThat(buyer.getBuyCount()).isEqualTo(1);
         assertThat(buyer.getWinningResult(lottoNumber).get(LottoRankingEnum.FIRST_PRIZE)).isEqualTo(1);
@@ -43,9 +43,15 @@ public class BuyerTest {
 
     public static class TestLottoNumberGenerator implements LottoNumberGenerator {
 
+        private List<Integer> values;
+
+        public TestLottoNumberGenerator(Integer... values) {
+            this.values = Arrays.asList(values);
+        }
+
         @Override
         public List<List<Integer>> genLottoNumbers(int cnt) {
-            return Arrays.asList(Arrays.asList(1,2,3,4,5,6));
+            return Arrays.asList(values);
         }
     }
 
