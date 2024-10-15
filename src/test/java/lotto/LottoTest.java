55dae6605ef5e16f46dacac52d9ec389ebf8e5ff
배영현 <byh2949@gmail.com>
2024-10-15 17:36:11 +0900

    test : test 클래스 추가

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,33 @@
+package lotto;
+
+import lotto.entity.Lotto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LottoTest {
+    @Test
+    @DisplayName("당첨 번호화 로또 번호가 일치하지 않는 경우")
+    public void 당첨번호_로또번호_일치하지_않음() {
+        Lotto lottoTicket = new Lotto("1, 2, 3, 4, 5, 6");
+        Lotto winningLotto = new Lotto("7, 8, 9, 10, 11, 12");
+
+        int matchCount = lottoTicket.matchCount(winningLotto);
+
+        assertThat(0).isEqualTo(matchCount);
+    }
+
+    @Test
+    @DisplayName("당첨 번호화 로또 번호가 일치하는 경우")
+    public void 당첨번호_로또번호_일치하지() {
+        Lotto lottoTicket = new Lotto("1, 2, 3, 4, 5, 6");
+        Lotto winningLotto = new Lotto("1, 2, 3, 10, 11, 12");
+
+        int matchCount = lottoTicket.matchCount(winningLotto);
+
+        assertThat(3).isEqualTo(matchCount);
+
+    }
+}
