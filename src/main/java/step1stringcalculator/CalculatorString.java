85c44585f562b2d131c809ac27c857e42877d58d
leewonsuk <tjdjdjrdl123@naver.com>
2024-10-15 23:50:51 +0900

    [feat] step1 1단계 문자열 계산기 문자열 유효성 검증 및 테스트 추가

--- src/main/java/step1stringcalculator/CalculatorString.java
+++ src/main/java/step1stringcalculator/CalculatorString.java
@@ -1,8 +1,12 @@
 package step1stringcalculator;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalculatorString {
+    public static final String NON_CALCULATOR_REGEX = "[^0-9+\\-*/\\s]";
+    public static final String OPERATOR_REGEX = "[+\\-*/]";
     private final String calculatorString;
 
     public CalculatorString(String calculatorString) {
@@ -11,6 +15,22 @@ public class CalculatorString {
 
     public void checkNullAndEmpty() {
         if (Objects.isNull(calculatorString) || calculatorString.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkCalculatorExpression() {
+        Pattern pattern = Pattern.compile(NON_CALCULATOR_REGEX);
+        Matcher matcher = pattern.matcher(calculatorString);
+        if (matcher.find()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkNonOperator() {
+        Pattern pattern = Pattern.compile(OPERATOR_REGEX);
+        Matcher matcher = pattern.matcher(calculatorString);
+        if (!matcher.find()) {
             throw new IllegalArgumentException();
         }
     }
--- src/test/java/step1stringcalculator/CarculatorStringTest.java
+++ src/test/java/step1stringcalculator/CarculatorStringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -27,11 +28,25 @@ public class CarculatorStringTest {
         assertThatThrownBy(() -> {
             mathExpression.checkNullAndEmpty();
         }).isInstanceOf(IllegalArgumentException.class);
-
-
     }
 
+    @ParameterizedTest()
+    @ValueSource(strings = {"2a-?","#$%@$#@$"})
+    public void 문자열아닌값_IllegalArgumentException_테스트(String requestMathExpression) {
+        mathExpression = new CalculatorString(requestMathExpression);
+        assertThatThrownBy(() -> {
+            mathExpression.checkCalculatorExpression();
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    public void 기호없는지여부_테스트() {
+        String requestMathExpression = "2 2 3?";
+        mathExpression = new CalculatorString(requestMathExpression);
+        assertThatThrownBy(() -> {
+            mathExpression.checkNonOperator();
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
 
 
