b6d21f1c8cb41ff4f647e7fa7e9b16b0593701ca
ming <devel.moon17@gmail.com>
2024-10-16 03:50:06 +0900

    refactor: 2단계 2차 피드백 반영 - LOTTO_CANDIDATE_NUMBERS Intstream으로 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,12 +2,11 @@ package lotto;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Lotto {
     private static final int LOTTO_NUMBER_COUNT = 6;
-    private static final List<Integer> LOTTO_CANDIDATE_NUMBERS =
-            Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45);
+    private static final List<Integer> LOTTO_CANDIDATE_NUMBERS = IntStream.rangeClosed(1, 45).boxed().collect(Collectors.toList());
 
 
     private List<Integer> lottoNumbers;
