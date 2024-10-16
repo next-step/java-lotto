0c68f6ca51f8ddad0a0055e33d65afcc6f8d2055
sinyebin <dpqls0625@naver.com>
2024-10-17 00:25:26 +0900

    feat 24.10.16 syb 수동 로또 발권

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static lotto.domain.Lotto.LOTTO_SIZE;
+import static lotto.domain.LottoCount.LOTTO_MINIMUM_COUNT;
 import static lotto.domain.LottoNumber.generateAutoLottoNumbers;
 
 import java.util.ArrayList;
@@ -10,24 +11,31 @@ public class LottoGame {
     private final LottoPrice lottoPrice;
     private final List<Lotto> lottoList;
     private final LottoResults lottoResults;
+    private final LottoCount lottoCount;
 
-    public LottoGame(int gameMoney) {
-        this(new LottoPrice(gameMoney), new LottoResults());
+    public LottoGame(LottoPrice lottoPrice) {
+        this(lottoPrice, new LottoCount(lottoPrice, LOTTO_MINIMUM_COUNT), new ArrayList<>());
     }
 
-    public LottoGame(LottoPrice lottoPrice, LottoResults lottoResults) {
+    public LottoGame(LottoPrice lottoPrice, LottoCount lottoCount, List<Lotto> lottos) {
+        this(lottoPrice, lottoCount, lottos, new LottoResults());
+    }
+
+    public LottoGame(LottoPrice lottoPrice, LottoCount lottoCount, List<Lotto> lottos, LottoResults lottoResults) {
         this.lottoPrice = lottoPrice;
-        this.lottoList = generateLotto(lottoPrice.getLottoCount());
+        this.lottoList = generateLotto(lottos, lottoCount);
+        this.lottoCount = lottoCount;
         this.lottoResults = lottoResults;
     }
 
-    private List<Lotto> generateLotto(int count) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            lottoList.add(new Lotto(generateAutoLottoNumbers(LOTTO_SIZE)));
+
+    private List<Lotto> generateLotto(List<Lotto> lottos, LottoCount lottoCount) {
+        for (int i = 0; i < lottoCount.getAutoCount(); i++) {
+            lottos.add(new Lotto(generateAutoLottoNumbers(LOTTO_SIZE)));
         }
-        return lottoList;
+        return lottos;
     }
+
 
     public List<Lotto> getLottos() {
         return lottoList;
