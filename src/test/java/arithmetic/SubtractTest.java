51201e9860f8e5d64b38a483f404b6a795e3b5cf
kyoungbeom <112946948+kyoungbeom@users.noreply.github.com>
2024-10-16 11:06:43 +0900

    1단계 - 문자열 계산기 (#3873)
    * ## document : README 파일 요구사항 작성
    
    * ## document : README 파일 요구사항 수정
    
    * ## test : 사칙연산 테스트 코드 구현
    
    * ## feature : 사칙연산 메서드 구현
    
    * ## refactor : 불필요한 파일 삭제
    
    * ## feature : 입출력 관련 클래스 생성
    
    * ## refactor : 문자열 계산기 리팩토링 및 테스트 코드 추가
    
    * ## refactor : 입력 유효성 검사 관련 정규식 수정
    
    * ## refactor : 사칙연산 관련 interface 사용을 통한 객체간 결합도 낮춤
    
    * ## test : 사칙연산 리팩토링에 따른 사칙연산 테스크 코드 구현
    
    * ## refactor : 상태가 없는 입출력 관련 클래스 정적메서드만을 갖는 유틸리티 클래스로 변경
    
    * ## test : 입력받은 문자열을 매개변수로 사칙연산을 수행하는 테스트 코드 구현
    
    * ## refactor : 입력받은 문자열을 매개변수로 사칙연산을 수행하는 메서드 구현 및 불필요한 메서드 삭제
    
    * ## refactor : singleton 패턴으로 리팩토링
    
    * ## style : MINUS -> SUBTRACT
    
    ---------
    
    Co-authored-by: parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>

---
+++ src/test/java/arithmetic/SubtractTest.java
@@ -0,0 +1,15 @@
+package arithmetic;
+
+import org.junit.jupiter.api.Test;
+import service.Arithmetic.Subtract;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SubtractTest {
+
+    @Test
+    public void 빼기 () {
+        Subtract subtract = Subtract.getInstance();
+        assertThat(subtract.calculate(5,2)).isEqualTo(3);
+    }
+}
