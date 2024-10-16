74807dc519b421c0b063a0f1b76c5d9a6783f100
BananMoon <younji1115@gmail.com>
2024-10-16 23:06:26 +0900

    refactor: 객체의 equals 메서드 테스트 코드 추가

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,12 +1,24 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoNumberTest {
+    @DisplayName("LottoNumber 객체의 동등성 비교 시 객체의 실제 값을 비교한다.")
+    @Test
+    void equals_to_value() {
+        LottoNumber actual = new LottoNumber(1);
+
+        assertThat(actual)
+                .isEqualTo(new LottoNumber(1))
+                .isNotSameAs(new LottoNumber(1));
+    }
+
     @DisplayName("로또 번호는 1~45 사이의 숫자여야 한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 46})
