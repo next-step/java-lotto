84481535952083136e53893dd3828a580d7d67b9
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-10-16 21:32:18 +0900

    ## refactor : 문자열 리터럴 상수로 선언

--- src/main/java/parser/InputParser.java
+++ src/main/java/parser/InputParser.java
@@ -7,12 +7,16 @@ import java.util.regex.Pattern;
 
 public class InputParser {
 
+    private static final String spaces = "\\s+";
+    private static final String number = "\\d+";
+    private static final String operation = "[+\\-*/]";
+
     public static List<Integer> parseNumbers(String calculationString) {
-        calculationString = calculationString.replaceAll("\\s+", "");
+        calculationString = calculationString.replaceAll(spaces, "");
 
         List<Integer> numbers = new ArrayList<>();
 
-        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern = Pattern.compile(number);
         Matcher matcher = pattern.matcher(calculationString);
 
         while (matcher.find()) {
@@ -25,11 +29,11 @@ public class InputParser {
     }
 
     public static List<Character> parseOperations(String calculationString) {
-        calculationString = calculationString.replaceAll("\\s+", "");
+        calculationString = calculationString.replaceAll(spaces, "");
 
         List<Character> operations = new ArrayList<>();
 
-        Pattern pattern = Pattern.compile("[+\\-*/]");
+        Pattern pattern = Pattern.compile(operation);
         Matcher matcher = pattern.matcher(calculationString);
 
         while (matcher.find()) {
