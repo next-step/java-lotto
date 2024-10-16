1c87e634f2d65b176fbfc21c271a0aa627e2a895
김혜영 <sunzero0116@gmail.com>
2024-10-17 07:25:16 +0900

    refactor : (리뷰반영) private로 변경, 상수와 멤버변수 구분

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -8,9 +8,10 @@ import java.util.stream.IntStream;
 
 public class LottoTicket {
     public static final int PRICE = 1000;
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 45;
-    public static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
     private final List<Integer> lottoNumbers;
 
     public LottoTicket() {
