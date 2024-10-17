df969649ddbd73d58dfc442bbdee33e637e604e3
jara <jara0701@gmail.com>
2024-10-17 20:31:20 +0900

    refactor LottoNumber: 인스턴스 캐시 적용
    로또 넙버 45개를 미리 생성해서 객체 생성을 제어함

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,11 +1,14 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class LottoNumber {
     public static final int MAX_BOUND = 45;
     public static final int MIN_BOUND = 1;
     private static final String OUT_OF_RANGE_ERROR_MESSAGE = "로또 숫자가 범위 밖입니다.";
+    private static final Map<Integer, LottoNumber> LOTTO_NUMBER_CACHE = initializeLottoNumberCache();
 
     private final int value;
 
@@ -13,11 +16,19 @@ public class LottoNumber {
         this.value = value;
     }
 
+    private static Map<Integer, LottoNumber> initializeLottoNumberCache() {
+        Map<Integer, LottoNumber> lottoNumberCache = new HashMap<>();
+        for (int number = MIN_BOUND; number < MAX_BOUND + 1; number++) {
+            lottoNumberCache.put(number, new LottoNumber(number));
+        }
+        return lottoNumberCache;
+    }
+
     public static LottoNumber valueOf(int value) {
         if (value < MIN_BOUND || value > MAX_BOUND) {
             throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
         }
-        return new LottoNumber(value);
+        return LOTTO_NUMBER_CACHE.get(value);
     }
 
     public int value() {
