9fed1a641cc4a188010b982d78ad95049dc1c88b
hunko <rhwlgns4386@naver.com>
2024-10-17 14:27:26 +0900

    refactor : 테스트 명 변경

--- src/test/java/calculator/PlusExpressionTest.java
+++ src/test/java/calculator/PlusExpressionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class PlusExpressionTest {
 
     @Test
-    public void 더하기_완료테스트() {
+    public void 더하기_테스트() {
         PlusExpression plusExpression = new PlusExpression(new NumberExpression(5), new NumberExpression(5));
         NumberExpression result = plusExpression.calc();
         assertThat(result).isEqualTo(new NumberExpression(10));
