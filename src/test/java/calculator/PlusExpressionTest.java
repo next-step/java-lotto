381040346f833f028b0f8598bb588eca3790cf52
hunko <rhwlgns4386@naver.com>
2024-10-17 14:51:32 +0900

    style : 코드에 구분되는 공백 추가

--- src/test/java/calculator/PlusExpressionTest.java
+++ src/test/java/calculator/PlusExpressionTest.java
@@ -10,7 +10,9 @@ public class PlusExpressionTest {
     public void 연속된_더하기_테스트() {
         PlusExpression leftExpression = new PlusExpression(new NumberExpression(5), new NumberExpression(5));
         PlusExpression expression = new PlusExpression(leftExpression, new NumberExpression(5));
+
         NumberExpression result = expression.calc();
+
         assertThat(result).isEqualTo(new NumberExpression(15));
     }
 }
