39e49877e3745118668de2c50f6f9cef4f3d0446
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-10-17 22:30:06 +0900

    ## feature: 입출력 관련 클래스 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,13 @@
 package lotto.view;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
+    private static final String WINNING_NUMBERS_FORMAT = "^\\d{1,2}(,\\s\\d{1,2}){5}$";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -16,9 +20,24 @@ public class InputView {
         try {
             System.out.println("구입금액을 입력해 주세요.");
             purchaseAmount = scanner.nextInt();
+            scanner.nextLine();
         } catch (InputMismatchException e) {
             System.out.println("잘못된 입력입니다. 다시 입력해주세요");
         }
         return purchaseAmount;
+    }
+
+    public static String inputLastWeekWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+
+        String lastWeekWinningNumbers = scanner.nextLine();
+
+        System.out.println();
+
+        if (lastWeekWinningNumbers.matches(WINNING_NUMBERS_FORMAT)) {
+            return lastWeekWinningNumbers;
+        }
+
+        throw new IllegalArgumentException("잘못된 입력형식입니다. 1, 2, 3, 4, 5, 6과 같은 형태로 입력해주세요");
     }
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ResultView {
+    private static final String[] PRIZE = {"0", "0", "0", "5000", "50000", "1500000", "2000000000"};
 
     private ResultView() {
     }
@@ -14,12 +15,26 @@ public class ResultView {
     }
 
     public static void printLottoNumbers(Map<Integer, List<Integer>> lottos) {
-        for(int count = 1; count <= lottos.size(); count++) {
+        for (int count = 1; count <= lottos.size(); count++) {
             List<Integer> lotto = lottos.get(count);
 
             Collections.sort(lotto);
 
             System.out.println(lotto);
         }
+        System.out.println();
+    }
+
+    public static void printWinningStatics(Map<Integer, Integer> winningStatics) {
+        System.out.println("당첨 통계");
+        System.out.println("----------");
+
+        for (int count = 3; count <= 6; count++) {
+            System.out.println(count + "개 일치 (" + PRIZE[count] + "원)- " + winningStatics.get(count) + "개");
+        }
+    }
+
+    public static void printReturnRate(float returnRate) {
+        System.out.println("총 수익률은" + " " + String.format("%.2f", returnRate) + "입니다.");
     }
 }
