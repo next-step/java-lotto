757a4d45bad9e303b6edc9e47a1fe6dc69c129af
JIHO CHOI <wlghsp@gmail.com>
2024-10-18 06:45:06 +0900

    feat: 수동 입력하여 번호 로또 구매

--- src/main/java/lotto/exception/InvalidLottoNumbersException.java
+++ src/main/java/lotto/exception/InvalidLottoNumbersException.java
@@ -1,10 +1,10 @@
 package lotto.exception;
 
-public class InvalidWinningNumbersException extends RuntimeException {
+public class InvalidLottoNumbersException extends RuntimeException {
 
   private static final String WINNING_NUMBER_EXCEPTION_MESSAGE = "6개의 중복되지 않는 숫자를 입력하세요.";
 
-  public InvalidWinningNumbersException() {
+  public InvalidLottoNumbersException() {
         super(WINNING_NUMBER_EXCEPTION_MESSAGE);
     }
 }
--- src/test/java/lotto/domain/ticket/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/domain/ticket/LottoTicketGeneratorTest.java
@@ -4,6 +4,8 @@ import lotto.domain.number.RandomLottoNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTicketGeneratorTest {
@@ -11,9 +13,15 @@ class LottoTicketGeneratorTest {
     @DisplayName("구입 가능한 갯수만큼 로또 티켓을 생성한다.")
     @Test
     void generateLottoTicketsTest() {
-        LottoTickets lottoTickets = LottoTicketGenerator.generateLottoTickets(10, new RandomLottoNumberGenerator());
+        List<List<Integer>> manualPurchaseLottoNumbers = List.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(3, 4, 5, 6, 7, 8)
+        );
+        int autoPurchaseLottoTicketCount = 10;
 
-        assertThat(lottoTickets.size()).isEqualTo(10);
+        LottoTickets lottoTickets = LottoTicketGenerator.generateLottoTickets(manualPurchaseLottoNumbers,autoPurchaseLottoTicketCount, new RandomLottoNumberGenerator());
+
+        assertThat(lottoTickets.size()).isEqualTo(manualPurchaseLottoNumbers.size() + autoPurchaseLottoTicketCount);
     }
 
 }
