d10e07c1fa4d315341ac478ac070bc36414bcdcf
tjdjdjrdl123 <lee-won-suk@github.io>
2024-10-17 20:31:13 +0900

    [refactor] 1단계 문자열 계산기 코드리뷰 반영 readme 위치 변경 변수명 수정

---
+++ src/main/java/step1stringcalculator/MathOperator.java
@@ -0,0 +1,17 @@
+package step1stringcalculator;
+
+public enum MathOperator {
+   /* private final String operator;
+    MathOperator(String operator) {
+        this.operator = operator;
+    }
+
+    SUM("+") {
+        public int sum (int defaultNumber, int nextNumber) {
+            return defaultNumber + nextNumber;
+        }
+    };
+*/
+
+
+}
--- src/test/java/step1stringcalculator/CurrentCalculationResultTest.java
+++ src/test/java/step1stringcalculator/CurrentCalculationResultTest.java
@@ -1,45 +1,44 @@
 package step1stringcalculator;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CurrentNumberTest {
+public class CurrentCalculationResultTest {
 
     public static final int TEST_CURRENT_NUMBER = 5;
 
     @ParameterizedTest()
     @CsvSource(value = {"1,1,2","1,2,3","2,2,4"})
     public void 덧셈_테스트(int firstNumber, int secondNumber, int resultNumber) {
-        assertThat(CurrentNumber.sum(firstNumber,secondNumber)).isEqualTo(resultNumber);
+        assertThat(CurrentCalculationResult.sum(firstNumber,secondNumber)).isEqualTo(resultNumber);
     }
 
     @ParameterizedTest()
     @CsvSource(value = {"2,1,1","1,2,-1","3,3,0"})
     public void 뺄셈_테스트(int firstNumber, int secondNumber, int resultNumber) {
-        assertThat(CurrentNumber.decrease(firstNumber,secondNumber)).isEqualTo(resultNumber);
+        assertThat(CurrentCalculationResult.decrease(firstNumber,secondNumber)).isEqualTo(resultNumber);
     }
 
     @ParameterizedTest()
     @CsvSource(value = {"2,1,2","1,-2,-2","3,0,0"})
     public void 곱셈_테스트(int firstNumber, int secondNumber, int resultNumber) {
-        assertThat(CurrentNumber.multiplication(firstNumber,secondNumber)).isEqualTo(resultNumber);
+        assertThat(CurrentCalculationResult.multiplication(firstNumber,secondNumber)).isEqualTo(resultNumber);
     }
 
     @ParameterizedTest(name = "나눗셈 정수 반환.버림연산")
     @CsvSource(value = {"2,1,2","7,2,3","7,3,2"})
     public void 나눗셈_테스트(int firstNumber, int secondNumber, int resultNumber) {
-        assertThat(CurrentNumber.division(firstNumber,secondNumber)).isEqualTo(resultNumber);
+        assertThat(CurrentCalculationResult.division(firstNumber,secondNumber)).isEqualTo(resultNumber);
     }
 
     @ParameterizedTest()
     @CsvSource(value = {"+,1,6","-,2,3","*,1,5","/,2,2"})
     void choiceCalculate_테스트(String currentOperator, int nextNumber, int resultNumber) {
-        CurrentNumber currentNumber = new CurrentNumber(TEST_CURRENT_NUMBER);
-        currentNumber.choiceCalculate(currentOperator,nextNumber);
-        assertThat(currentNumber).isEqualTo(new CurrentNumber(resultNumber));
+        CurrentCalculationResult currentCalculationResult = new CurrentCalculationResult(TEST_CURRENT_NUMBER);
+        currentCalculationResult.choiceCalculate(currentOperator,nextNumber);
+        assertThat(currentCalculationResult).isEqualTo(new CurrentCalculationResult(resultNumber));
     }
 
 }
