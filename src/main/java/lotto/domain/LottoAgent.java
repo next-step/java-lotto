dc3679e9ba4193a2bd8f885b9683d051fb91db9a
jara <jara0701@gmail.com>
2024-10-17 19:17:55 +0900

    refactor LottoAgent: 수동구매, 자동구매 메서드 구현
    수동구매와 자동구매를 메서드 분리하여 구현함. 각 구매를 할 때 공통으로 일어나는 과정을 따로 메서드 추출함

--- src/main/java/lotto/domain/LottoAgent.java
+++ src/main/java/lotto/domain/LottoAgent.java
@@ -1,27 +1,37 @@
 package lotto.domain;
 
-
 public class LottoAgent {
     private PurchasedLottos purchasedLottos;
+    private Budget budget;
 
-    private LottoAgent() {
+    private LottoAgent(Budget budget) {
         purchasedLottos = PurchasedLottos.newInstance();
+        this.budget = budget;
     }
 
     public static LottoAgent newInstance() {
-        return new LottoAgent();
+        return new LottoAgent(Budget.valueOf(0));
     }
 
-    public void buy(int price, int manualLottoCount, LottoNumberSelectionStrategy selectionStrategy) {
-        int buyingCount = price / Lotto.LOTTO_PRICE;
+    public void deposit(int price) {
+        budget = budget.deposit(price);
+    }
+
+    public void buyAutomatically(LottoNumberSelectionStrategy selectionStrategy) {
+        int buyingCount = budget.getAmount() / Lotto.LOTTO_PRICE;
         for (int i = 0; i < buyingCount; i++) {
-            buyLotto(i, selectionStrategy);
+            LottoNumbers automaticallySelected = selectionStrategy.select();
+            buyLotto(automaticallySelected);
         }
     }
 
+    public void buyManually(LottoNumbers manuallySelected) {
+        buyLotto(manuallySelected);
+    }
 
-    private void buyLotto(int id, LottoNumberSelectionStrategy selectionStrategy) {
-        purchasedLottos = purchasedLottos.add(Lotto.of(id, selectionStrategy));
+    private void buyLotto(LottoNumbers numbers) {
+        purchasedLottos = purchasedLottos.add(Lotto.of(purchasedLottos.generateNextId(), numbers));
+        budget = budget.use(Lotto.LOTTO_PRICE);
     }
 
     public PurchasedLottos getPurchasedLottos() {
