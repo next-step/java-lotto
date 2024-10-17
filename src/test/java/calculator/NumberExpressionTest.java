302d0b18075fb1fdd186d1175e29e8f2cb135045
hunko <rhwlgns4386@naver.com>
2024-10-17 14:32:21 +0900

    refactor : 테스트 이동

--- src/test/java/calculator/NumberExpressionTest.java
+++ src/test/java/calculator/NumberExpressionTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
@@ -9,5 +10,13 @@ public class NumberExpressionTest {
     @Test
     public void 숫자형식_검사() {
         assertThatIllegalArgumentException().isThrownBy(() -> new NumberExpression("a"));
+    }
+
+    @Test
+    public void 더하기_테스트(){
+        NumberExpression five = new NumberExpression(5);
+        NumberExpression six = new NumberExpression(6);
+
+        assertThat(five.sum(six)).isEqualTo(new NumberExpression(11));
     }
 }
--- src/test/java/calculator/PlusExpressionTest.java
+++ src/test/java/calculator/PlusExpressionTest.java
@@ -6,10 +6,5 @@ import org.junit.jupiter.api.Test;
 
 public class PlusExpressionTest {
 
-    @Test
-    public void 더하기_테스트() {
-        PlusExpression plusExpression = new PlusExpression(new NumberExpression(5), new NumberExpression(5));
-        NumberExpression result = plusExpression.calc();
-        assertThat(result).isEqualTo(new NumberExpression(10));
-    }
+
 }
