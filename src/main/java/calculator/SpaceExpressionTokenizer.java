91d984a78bd708b1afdff4717e83855fde42aaeb
hunko <rhwlgns4386@naver.com>
2024-10-17 17:20:56 +0900

    refactor : 매직 벨류 상수화

--- src/main/java/calculator/SpaceExpressionTokenizer.java
+++ src/main/java/calculator/SpaceExpressionTokenizer.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 public class SpaceExpressionTokenizer implements ExpressionTokenizer {
 
+    private static final String SPLIT_PATTERN = " ";
+
     @Override
     public List<String> tokenize(String input) {
         return toList(split(input));
     }
 
     private String[] split(String input) {
-        return input.split(" ");
+        return input.split(SPLIT_PATTERN);
     }
 
     private List<String> toList(String[] value){
