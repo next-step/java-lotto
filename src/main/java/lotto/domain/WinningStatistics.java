48158420fe98c34cd19be5e2b4a1b9e78235bb9e
StoneHee99 <kiit0901@gmail.com>
2024-10-17 21:40:38 +0900

    feat: 당첨 통계가 당첨 수 정보 반환

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -6,9 +6,25 @@ public class WinningStatistics {
 
     private final EnumMap<Rank, Integer> rankCounts = new EnumMap<>(Rank.class);
 
-    public WinningStatistics() {
+    public WinningStatistics(LotteryMachine machine, Lotto winningLotto) {
+        calculateStatistics(machine, winningLotto);
+    }
+
+    private void calculateStatistics(LotteryMachine machine, Lotto winningLotto) {
+        initialRankStatistics();
+        for (Lotto lotto : machine.getLottos()) {
+            Rank rank = Rank.valueOfMatchCount(lotto.countMatchingNumbers(winningLotto));
+            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+        }
+    }
+
+    private void initialRankStatistics() {
         for (Rank rank : Rank.values()) {
             rankCounts.put(rank, 0);
         }
+    }
+
+    public int getRankCount(Rank rank) {
+        return rankCounts.get(rank);
     }
 }
