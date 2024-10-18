bedc100fba9920c3bcf5812d74375f252f081f6f
BananMoon <younji1115@gmail.com>
2024-10-19 01:14:45 +0900

    feat: 로또 랭킹 2등을 추가한다.

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -7,6 +7,7 @@ public enum LottoRank {
     FIFTH(3, 5_000),
     FOURTH(4, 50_000),
     THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
     FIRST(6, 2_000_000_000);
 
     private final int matchingCount;
@@ -17,9 +18,12 @@ public enum LottoRank {
         this.distributionRatioPrice = distributionRatioPrice;
     }
 
-    public static LottoRank from(int matchingCount) {
+    public static LottoRank from(int matchingCount, Boolean isBonusBallMatched) {
         if (isNotRankEligible(matchingCount)) {
             return NON_RANKED;
+        }
+        if (isSecondWinner(matchingCount, isBonusBallMatched)) {
+            return SECOND;
         }
         return Arrays.stream(LottoRank.values())
                 .filter(value -> value.getMatchingCount() == matchingCount)
@@ -27,8 +31,12 @@ public enum LottoRank {
                 .orElseThrow(() -> new IllegalArgumentException(String.format("로또 번호 매칭 갯수 '%s'은 적합하지 않습니다.", matchingCount)));
     }
 
+    private static boolean isSecondWinner(int matchingCount, Boolean isBonusBallMatched) {
+        return matchingCount == THIRD.getMatchingCount() && isBonusBallMatched;
+    }
+
     private static boolean isNotRankEligible(int matchingCount) {
-        return 0 <= matchingCount && matchingCount <= 2;
+        return 0 <= matchingCount && matchingCount <= NON_RANKED.matchingCount;
     }
 
     public static LottoRank[] winningRanks() {
--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -11,7 +11,7 @@ class LottoRankTest {
     @DisplayName("매칭 카운트를 전달하면 LottoRankingSystem 객체를 전달한다.")
     @Test
     void create() {
-        LottoRank actual = LottoRank.from(6);
+        LottoRank actual = LottoRank.from(6, Boolean.FALSE);
 
         assertThat(actual).isEqualTo(LottoRank.FIRST);
     }
@@ -19,16 +19,32 @@ class LottoRankTest {
     @DisplayName("로또 번호 일치 갯수가 로또 랭킹에 들지않는 갯수라면 NOT_RANKED 필드를 반환한다.")
     @Test
     void return_NOT_RANKED_when_matchingCount_is_not_included_in_rankingSystem() {
-        LottoRank actual = LottoRank.from(2);
+        LottoRank actual = LottoRank.from(2, Boolean.FALSE);
 
         assertThat(actual).isEqualTo(LottoRank.NON_RANKED);
+    }
+
+    @DisplayName("로또 번호 일치 갯수가 5개이고 보너스 번호가 일치하다면 SECOND 필드를 반환한다.")
+    @Test
+    void return_SECOND_when_matchingCount_is_fifth_and_bonusBall_is_matched() {
+        LottoRank actual = LottoRank.from(5, Boolean.TRUE);
+
+        assertThat(actual).isEqualTo(LottoRank.SECOND);
     }
 
     @DisplayName("로또 갯수를 벗어난 매칭 카운트를 전달하면 IllegalArgumentException 를 발생시킨다.")
     @Test
     void throw_IllegalArgumentException_when_matchingCount_is_not_() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> LottoRank.from(7))
+                .isThrownBy(() -> LottoRank.from(7, Boolean.FALSE))
                 .withMessage("로또 번호 매칭 갯수 '7'은 적합하지 않습니다.");
+    }
+
+    @DisplayName("우승 랭킹이 아닌 상수를 제외하고 반환한다.")
+    @Test
+    void return_winningRanks() {
+        LottoRank[] actual = LottoRank.winningRanks();
+
+        assertThat(actual).doesNotContain(LottoRank.NON_RANKED);
     }
 }
