b49f217fc402282e5d9f981a3d0b0239cd3925f8
hoya <sv05159@naver.com>
2024-10-18 20:05:08 +0900

    refactor: test fixture 추가 (2단계)

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,13 +1,21 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Number> numbers;
 
     public Lotto(final List<Number> numbers) {
         this.numbers = numbers;
+    }
+
+    public Lotto(final Integer... numbers) {
+        this.numbers = Arrays.stream(numbers)
+                .map(Number::new)
+                .collect(Collectors.toList());
     }
 
     public int sumMatchCount(final List<Number> others) {
--- src/test/java/lotto/strategy/LottoGeneratorTest.java
+++ src/test/java/lotto/strategy/LottoGeneratorTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoGeneratorTest {
     @Test
     void 로또_발급() {
-        List<Lotto> lottos = List.of(new Lotto(List.of(new Number(1), new Number(2), new Number(3), new Number(4), new Number(5), new Number(6))));
+        List<Lotto> lottos = List.of(new Lotto(1, 2, 3, 4, 5, 6));
         LottoGenerator lottoGenerator = (count) -> lottos;
 
         List<Lotto> result = lottoGenerator.generate(1);
