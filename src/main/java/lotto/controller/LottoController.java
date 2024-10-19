5d8c82078008403dc814955db2b506dc79e12b6d
mitsui3point <rladbsgh0331@gmail.com>
2024-10-20 04:13:24 +0900

    docs(lotto-auto): modify README_LOTTO_AUTO modify 구입금액이 1000원 미만이면 콘솔에 재요청 modify 구입금액이 1000원으로 나누었을때 나머지가 있으면 콘솔에 재요청 modify 수동 로또 번호 갯수를 콘솔에 입력받는다 modify 수동 로또 번호 갯수를 다시 콘솔에 입력받는다 modify 수동 번호를 콘솔에 입력받는다 modify 수동 번호 중복될 경우 번호를 다시 콘솔에 입력받는다 modify 수동 구매 로또 목록 + 자동 구매 로또 목록을 콘솔에 출력한다
    feat(lotto-auto): modify InputView static methods(exception 발생시 재귀호출로 input 다시받도록 처리)
    
    feat(lotto-auto): modfiy LottoController run method

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,28 +1,39 @@
 package lotto.controller;
 
-import lotto.model.Buyer;
+import lotto.model.Lottoes;
 import lotto.model.Lotto;
 import lotto.model.Result;
 import lotto.model.Winning;
-import lotto.model.dto.LottoNumber;
 import lotto.util.LottoNumbersCreator;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
+import static lotto.model.Result.LOTTO_PRICE;
+
 public class LottoController {
     public static void run() {
         int buyAmount = InputView.inputBuyAmount();
-        int buyCount = InputView.calculateBuyCount(buyAmount);
+        int buyCount = calculateBuyCount(buyAmount);
+        int manualBuyCount = InputView.inputManualBuyCount(buyCount);
+        int autoBuyCount = calculateAutoBuyCount(buyCount, manualBuyCount);
 
-        Buyer buyer = new Buyer(buyCount, new LottoNumbersCreator());
+        Lottoes manualLottoes = new Lottoes(InputView.inputManualLottoes(manualBuyCount));
+        Lottoes autoLottoes = new Lottoes(autoBuyCount, new LottoNumbersCreator());
+        Lottoes lottoes = new Lottoes(manualLottoes, autoLottoes);
+        ResultView.printLottoes(lottoes, manualLottoes, autoLottoes);
 
-        ResultView.printBuyerLottosInfo(buyer);
-
-        Lotto winningLotto = new Lotto(InputView::inputWinningLottoNumbers);
-        LottoNumber bonusNumber = InputView.inputBonusLottoNumber();
-        Winning winning = new Winning(winningLotto, bonusNumber);
-        Result result = new Result(buyer, winning);
+        Lotto winningLotto = InputView.inputWinningLotto();
+        Winning winning = InputView.inputWinning(winningLotto);
+        Result result = new Result(lottoes, winning);
 
         ResultView.printStatistics(buyCount, result);
+    }
+
+    public static int calculateBuyCount(int buyAmount) {
+        return buyAmount / LOTTO_PRICE;// TODO: 객체추출 buyAmount
+    }
+
+    private static int calculateAutoBuyCount(int buyCount, int manualBuyCount) {
+        return buyCount - manualBuyCount;
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.model.Lotto;
+import lotto.model.Winning;
 import lotto.model.dto.LottoNumber;
 import lotto.util.SplitUtil;
 
@@ -7,38 +9,120 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static lotto.model.Result.LOTTO_PRICE;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    public static final int DIVIDE_AMOUNT = 1000;
-    public static final String INPUT_BUY_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
-    public static final String INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요.";
+    public static final String INPUT_BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+    public static final String RETRY_INPUT_MESSAGE = "다시 입력해주세요.";
+    public static final String INPUT_MANUAL_LOTTO_BUY_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    public static final String INPUT_MANUAL_LOTTO_NUMBER_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
+    public static final int START_NUMBER = 0;
+    public static final String ERROR_MANUAL_COUNT_BIGGER_THAN_BUY_COUNT = "수동으로 구매하고자 하는 로또의 갯수가 구입금액보다 큽니다.";
+    public static final String ERROR_MANUAL_COUNT_NOT_ALLOWED_NEGATIVE_NUMBER = "수동으로 구매하고자 하는 로또의 갯수는 음수가 될 수 없습니다";
+    public static final String ERROR_LOWER_MINIMUM_BUY_AMOUNT = "최소 구매금액보다 작습니다.";
+    public static final String LOTTO_NOT_ALLOWED_REMAINDER = "로또는 천원단위로 구매가 가능합니다.";
 
     public static int inputBuyAmount() {
-        System.out.println(INPUT_BUY_AMOUNT);
-        int buyAmount = SCANNER.nextInt();
-        SCANNER.nextLine();// NOTE: nextInt 후에 Enter 키를 누르면 그 개행 문자가 입력 버퍼에 남아 있으므로 남아 있는 개행 문자 소비
-        return buyAmount;
-    }
+        try {
+            System.out.println(INPUT_BUY_AMOUNT_MESSAGE);
+            int buyAmount = scannerNextInt();// TODO: 객체 추출
 
-    public static int calculateBuyCount(int buyAmount) {
-        return buyAmount / DIVIDE_AMOUNT;
-    }
+            boolean lowerMinimumBuyAmount = buyAmount < LOTTO_PRICE;
+            if (lowerMinimumBuyAmount) {
+                throw new IllegalArgumentException(ERROR_LOWER_MINIMUM_BUY_AMOUNT);
+            }
 
-    public static List<LottoNumber> inputWinningLottoNumbers() {
-        System.out.println();
-        System.out.println(INPUT_WINNING_NUMBERS);
-        Integer[] integers = SplitUtil.stringToNumbers(SCANNER.nextLine());
-        return Arrays.stream(integers)
-                .map(LottoNumber::new)
-                .collect(Collectors.toList());
+            boolean isNotBuyAmountRemainderZero = buyAmount % LOTTO_PRICE != 0;
+            if (isNotBuyAmountRemainderZero) {
+                throw new IllegalArgumentException(LOTTO_NOT_ALLOWED_REMAINDER);
+            }
+
+            return buyAmount;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + RETRY_INPUT_MESSAGE);
+            return inputBuyAmount();
+        }
     }
 
     public static LottoNumber inputBonusLottoNumber() {
-        System.out.println(INPUT_BONUS_NUMBER);
-        LottoNumber bonusNumber = new LottoNumber(SCANNER.nextInt());
-        SCANNER.nextLine();
-        return bonusNumber;
+        try {
+            System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+            return new LottoNumber(scannerNextInt());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return inputBonusLottoNumber();
+        }
+    }
+
+    public static int inputManualBuyCount(int buyCount) {
+        try {
+            System.out.println(INPUT_MANUAL_LOTTO_BUY_COUNT_MESSAGE);
+            int manualBuyCount = scannerNextInt();
+
+            boolean isManualCountBiggerThanBuyCount = buyCount < manualBuyCount;
+            if (isManualCountBiggerThanBuyCount) {
+                throw new IllegalArgumentException(ERROR_MANUAL_COUNT_BIGGER_THAN_BUY_COUNT);
+            }
+
+            boolean isMinus = manualBuyCount < 0;
+            if (isMinus) {
+                throw new IllegalArgumentException(ERROR_MANUAL_COUNT_NOT_ALLOWED_NEGATIVE_NUMBER);
+            }
+
+            return manualBuyCount;
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage() + RETRY_INPUT_MESSAGE);
+            return inputManualBuyCount(buyCount);
+        }
+    }
+
+    public static List<Lotto> inputManualLottoes(final int manualBuyCount) {
+        System.out.println(INPUT_MANUAL_LOTTO_NUMBER_MESSAGE);
+        try {
+            return IntStream.range(START_NUMBER, manualBuyCount)
+                    .mapToObj(i -> {
+                        int[] numbers = SplitUtil.stringToNumbers(SCANNER.nextLine());
+                        return new Lotto(numbers);
+                    }).collect(Collectors.toList());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + RETRY_INPUT_MESSAGE);
+            return inputManualLottoes(manualBuyCount);
+        }
+    }
+
+    public static Winning inputWinning(Lotto winningLotto) {
+        try {
+            LottoNumber bonusNumber = InputView.inputBonusLottoNumber();
+            return new Winning(winningLotto, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinning(winningLotto);
+        }
+    }
+
+    public static Lotto inputWinningLotto() {
+        try {
+            System.out.println();
+            System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+            int[] parsedLottoNumbers = SplitUtil.stringToNumbers(SCANNER.nextLine());
+            List<LottoNumber> lottoNumbers = Arrays.stream(parsedLottoNumbers)
+                    .mapToObj(LottoNumber::new)
+                    .collect(Collectors.toList());
+            return new Lotto(() -> lottoNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputWinningLotto();
+        }
+    }
+
+    private static int scannerNextInt() {
+        int result = SCANNER.nextInt();
+        SCANNER.nextLine();// NOTE: nextInt 후에 Enter 키를 누르면 그 개행 문자가 입력 버퍼에 남아 있으므로 남아 있는 개행 문자 소비
+        return result;
     }
 }
