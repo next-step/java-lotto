22414c4476c2877fa905afc2daa34e2c6b57d204
김혜영 <sunzero0116@gmail.com>
2024-10-19 20:39:01 +0900

    feat: 출력 부분 보너스 볼 일치 추가

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import lotto.domain.LottoTicket;
 import lotto.domain.LottoTickets;
+import lotto.domain.Winning;
 import lotto.domain.WinningCalculator;
 
 public class ResultView {
@@ -19,13 +20,22 @@ public class ResultView {
         System.out.println("당첨 통계");
         System.out.println("---------");
         winningCalculator.getWinningCountMap().forEach((winning, count) -> {
-            System.out.println(winning.getMatchCount() + "개 일치 (" + winning.getPrize() + "원) - " + count + "개");
-        });
-        double rateOfReturn = winningCalculator.calculateRateOfReturn();
+            String description = getWinningDescription(winning);
+            System.out.println(description + " - " + count + "개");
 
+        });
+
+        double rateOfReturn = winningCalculator.calculateRateOfReturn();
         System.out.printf("총 수익률은 %.2f%%입니다.%n", rateOfReturn);
         if (rateOfReturn < 1) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)\n");
         }
+    }
+
+    private static String getWinningDescription(Winning winning) {
+        if (winning == Winning.SECOND) {
+            return winning.getMatchCount() + "개 일치, 보너스 볼 일치 (" + winning.getPrize() + "원)";
+        }
+        return winning.getMatchCount() + "개 일치 (" + winning.getPrize() + "원)";
     }
 }
