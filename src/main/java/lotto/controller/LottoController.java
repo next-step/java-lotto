d31c9770e367c24eeddec42ea9d048004b1fd13a
owljoa <needshield@gmail.com>
2024-10-20 18:00:04 +0900

    refactor: 일급콜렉션에 맞게 변수명 변경

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class LottoController {
     public static void purchaseAutoLotto() {
         int totalPurchaseAmount = InputView.inputTotalPurchaseAmount();
-        Lotteries purchasedLottoNumbersList = Lotteries.purchase(totalPurchaseAmount);
-        ResultView.printPurchasedLottoNumberList(purchasedLottoNumbersList);
+        Lotteries lotteries = Lotteries.purchase(totalPurchaseAmount);
+        ResultView.printPurchasedLottoNumberList(lotteries);
 
         String inputLastWeekWinningNumbers = InputView.inputLastWeekWinningNumbers();
-        Lottery winningNumbers = Lottery.from(inputLastWeekWinningNumbers);
+        Lottery winningLottery = Lottery.from(inputLastWeekWinningNumbers);
 
         Integer inputBonusNumber = InputView.inputBonusNumber();
         BonusNumber bonusNumber = new BonusNumber(inputBonusNumber);
 
-        List<LottoResult> lottoResults = purchasedLottoNumbersList.getLottoResults(winningNumbers, bonusNumber);
+        List<LottoResult> lottoResults = lotteries.getLottoResults(winningLottery, bonusNumber);
         LottoStatistics lottoStatistics = LottoStatistics.create(lottoResults);
         float profitRate = lottoStatistics.calculateProfitRate(totalPurchaseAmount);
         ResultView.printResult(lottoStatistics, profitRate);
