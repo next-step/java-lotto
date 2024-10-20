475e21719f4051d1abefe767ebcb63c6a2489586
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-20 11:10:43 +0900

    refactor : 상수선언, 메소드 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,9 @@ import java.util.*;
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_COUNT = 6;
 
     private InputView() {
     }
@@ -17,42 +20,49 @@ public class InputView {
     }
 
     public static Set<Integer> getWinningNumbers() {
-        System.out.println("지난 주 당첨 번호 [6자리]를 입력해 주세요.");
+        System.out.println(String.format("지난 주 당첨 번호 [%d자리]를 입력해 주세요.", LOTTO_COUNT));
 
-        String input = SCANNER.nextLine().trim();
+        while (true) {
+            String input = SCANNER.nextLine().trim();
+            Set<Integer> winningNumbers = parseInputToNumbers(input);
+            if (winningNumbers != null && winningNumbers.size() == LOTTO_COUNT) {
+                return winningNumbers;
+            }
+            System.out.println(String.format("%d개의 중복되지 않는 유효한 번호를 입력해 주세요.", LOTTO_COUNT));
+        }
+    }
 
+    private static Set<Integer> parseInputToNumbers(String input) {
         if (input.isEmpty()) {
             System.out.println("입력 값이 비어 있습니다. 다시 입력해 주세요.");
-            return getWinningNumbers();
+            return null;
         }
 
-        String[] inputList = input.split(",");
         Set<Integer> winningNumbers = new HashSet<>();
+        String[] inputList = input.split(",");
 
-        try {
-            for (String stringNumber : inputList) {
-                int number = Integer.parseInt(stringNumber.trim());
-
-                if (number < 1 || number > 45) {
-                    System.out.println("로또 번호는 1부터 45 범위 안에 있습니다.");
-                    return getWinningNumbers();
-                }
-
-                if (!winningNumbers.add(number)) {
-                    System.out.println("중복된 숫자가 있습니다. 다시 입력해 주세요.");
-                    return getWinningNumbers();
-                }
+        for (String stringNumber : inputList) {
+            Integer number = parseNumber(stringNumber.trim());
+            if (number == null) {
+                return null;
             }
+            winningNumbers.add(number);
+        }
+        return winningNumbers;
+    }
+    
+    private static Integer parseNumber(String stringNumber) {
+        try {
+            int number = Integer.parseInt(stringNumber);
+
+            if (number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX) {
+                System.out.println(String.format("로또 번호는 %d부터 %d 범위 안에 있어야 합니다.", LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX));
+                return null;
+            }
+            return number;
         } catch (NumberFormatException e) {
             System.out.println("숫자를 입력해 주세요.");
-            return getWinningNumbers();
+            return null;
         }
-
-        if (winningNumbers.size() != 6) {
-            System.out.println("여섯 자리를 입력해 주세요.");
-            return getWinningNumbers();
-        }
-
-        return winningNumbers;
     }
 }
