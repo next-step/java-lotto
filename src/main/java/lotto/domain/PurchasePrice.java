7e38b2fcd469b2681141726cc3c832efb45ff927
hwan2-99 <thh1233123@gmail.com>
2024-10-20 10:16:14 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/lotto/domain/PurchasePrice.java
+++ src/main/java/lotto/domain/PurchasePrice.java
@@ -4,7 +4,6 @@ public class PurchasePrice {
     private static final int PRICE_UNIT = 1000;
     private static final String INCORRECT_UNIT_ERROR = "로또 구입금액은 " + PRICE_UNIT + "원 단위여야 합니다.";
     private static final String INCORRECT_VALUE_ERROR = "로또 구입금액의 최소금액은 " + PRICE_UNIT + "원 입니다.";
-    private static final String OVER_TOTAL_LOTTO_COUNT_ERROR = "수동으로 구입하는 로또 갯수가 구입가능한 개수보다 많습니다.";
     private final int money;
 
     public PurchasePrice(final int money) {
@@ -22,17 +21,6 @@ public class PurchasePrice {
     private void validateValue(final int money) {
         if (money < PRICE_UNIT) {
             throw new IllegalArgumentException(INCORRECT_VALUE_ERROR);
-        }
-    }
-
-    public LottoPurchaseInfo calculatePurchaseInfo(final int manualCount) {
-        validateManualCount(manualCount);
-        return new LottoPurchaseInfo(manualCount, calculateAutoCount() - manualCount);
-    }
-
-    private void validateManualCount(final int manualCount) {
-        if (manualCount > calculateAutoCount()){
-            throw new IllegalArgumentException(OVER_TOTAL_LOTTO_COUNT_ERROR);
         }
     }
 
