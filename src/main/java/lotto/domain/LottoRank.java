b26fec79cdc7d41380267d639d2ea72fb0ca3f26
owljoa <needshield@gmail.com>
2024-10-20 15:05:10 +0900

    refactor: 가독성을 위한 숫자 표현 변경

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum LottoRank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
     NONE(-1, 0);
 
     public static final List<LottoRank> RANKS_WITHOUT_NONE = Arrays.asList(FIRST, SECOND, THIRD, FOURTH);
