abd689315dd2b619313beae66d03aad1b5ab569a
owljoa <needshield@gmail.com>
2024-10-20 16:50:30 +0900

    refactor: 사용되지 않는 메소드와 테스트코드 제거

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -16,16 +16,4 @@ class LottoRankTest {
         // then
         Assertions.assertThat(lottoRank).isEqualTo(LottoRank.FIRST);
     }
-
-    @Test
-    void 당첨_액수_구하기() {
-        // given
-        int equalNumberCount = 6;
-
-        // when
-        int prizeMoneyAmount = LottoRank.findPrizeMoneyFromEqualNumberCount(equalNumberCount);
-
-        // then
-        Assertions.assertThat(prizeMoneyAmount).isEqualTo(LottoRank.FIRST.prizeMoneyAmount);
-    }
 }
