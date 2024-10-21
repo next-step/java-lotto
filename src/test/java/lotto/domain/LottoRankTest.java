3b846f2a9037034a92ba54193ef403df304844e2
BananMoon <younji1115@gmail.com>
2024-10-21 21:32:33 +0900

    bugfix : LottoRank 객체 생성 시 SECOND를 기준으로 값을 다르게 체크한다.

--- src/test/java/lotto/domain/LottoRankTest.java
+++ src/test/java/lotto/domain/LottoRankTest.java
@@ -7,15 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class LottoRankTest {
-
-    @DisplayName("매칭 카운트를 전달하면 LottoRankingSystem 객체를 전달한다.")
-    @Test
-    void create() {
-        LottoRank actual = LottoRank.from(6, Boolean.FALSE);
-
-        assertThat(actual).isEqualTo(LottoRank.FIRST);
-    }
-
     @DisplayName("로또 번호 일치 갯수가 로또 랭킹에 들지않는 갯수라면 NON_RANKED 필드를 반환한다.")
     @Test
     void return_NOT_RANKED_when_matchingCount_is_not_included_in_rankingSystem() {
@@ -32,6 +23,14 @@ class LottoRankTest {
         assertThat(actual).isEqualTo(LottoRank.SECOND);
     }
 
+    @DisplayName("로또 번호 일치 갯수가 5개이지만 보너스 번호가 일치하지않다면 THIRD 필드를 반환한다.")
+    @Test
+    void return_THIRD_when_matchingCount_is_fifth_but_bonusBall_is_not_matched() {
+        LottoRank actual = LottoRank.from(5, Boolean.FALSE);
+
+        assertThat(actual).isEqualTo(LottoRank.THIRD);
+    }
+
     @DisplayName("로또 갯수를 벗어난 매칭 카운트를 전달하면 IllegalArgumentException 를 발생시킨다.")
     @Test
     void throw_IllegalArgumentException_when_matchingCount_is_not_() {
@@ -46,5 +45,15 @@ class LottoRankTest {
         LottoRank[] actual = LottoRank.winningRanks();
 
         assertThat(actual).doesNotContain(LottoRank.NON_RANKED);
+    }
+
+    @DisplayName("우승 등수에 포함되지 않는 로또 등수의 일치 갯수 호출 시 IllegalArgumentException을 발생시킨다.")
+    @Test
+    void throw_IllegalArgumentException_when_instance_is_non_ranked() {
+        LottoRank nonRanked = LottoRank.NON_RANKED;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(nonRanked::getWinningRankMatchingCount)
+                .withMessage("로또 등수에 포함되는 값만 호출해주세요.");
     }
 }
