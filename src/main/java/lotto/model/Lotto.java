f8c4481af88a234fa173eff5a715761de274f777
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-10-23 00:50:59 +0900

    ## refactor : Integer 타입 클래스로 포장

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,21 +1,33 @@
 package lotto.model;
 
+import lotto.utility.Validator;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private List<Integer> number;
+    private List<LottoNumber> lotto;
 
-    public Lotto(List<Integer> lottoNumber) {
-        this.number = lottoNumber;
+    public Lotto(List<Integer> numbers) {
+        Validator.isValidNumbers(numbers);
+
+        this.lotto = numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 
     public int countMatchingNumber(List<Integer> winningNumbers) {
-        return (int) number.stream()
-                .filter(winningNumbers::contains)
-                    .count();
+        List<LottoNumber> winningLottoNumbers = winningNumbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        return (int) lotto.stream()
+                .filter(winningLottoNumbers::contains)
+                .count();
     }
 
     public boolean containsBonusNumber(int bonusNumber) {
-        return number.contains(bonusNumber);
+        return lotto.contains(new LottoNumber(bonusNumber));
     }
+
 }
