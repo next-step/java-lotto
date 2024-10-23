f7f6d8f1f67603054ef0f6a4491cfe2b727b3de4
kimsuyeondev <rhaehl@naver.com>
2024-10-24 01:10:00 +0900

    #step3 피드백반영 - 변수명 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,13 +3,13 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputView {
-    public static final String STR1 = "구입금액을 입력해 주세요.";
-    public static final String STR2 = "지난주 당첨번호를 입력해 주세요.";
-    public static final String STR3 = "보너스 볼을 입력해 주세요.";
+    public static final String INPUT_PRICE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_ANSWER = "지난주 당첨번호를 입력해 주세요.";
+    public static final String INPUT_BONUSBALL = "보너스 볼을 입력해 주세요.";
     public static final Scanner scanner = new Scanner(System.in);
 
     public int inputPrice() {
-        System.out.print(STR1);
+        System.out.print(INPUT_PRICE);
         int price = scanner.nextInt();
         scanner.nextLine(); // 남은 개행 문자 처리
         System.out.println("입력된 가격: " + price);
@@ -17,12 +17,12 @@ public class InputView {
     }
 
     public String inputAnswer() {
-        System.out.print(STR2);
+        System.out.print(INPUT_ANSWER);
         return scanner.nextLine();
     }
 
     public int inputBonus() {
-        System.out.print(STR3);
+        System.out.print(INPUT_BONUSBALL);
         return scanner.nextInt();
 
     }
