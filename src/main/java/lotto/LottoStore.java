b49fd990aa40c03e3ffef6934aa099ef85a1c159
hunko <rhwlgns4386@naver.com>
2024-10-24 15:56:26 +0900

    refactor : 매직 넘버 제거 및 함수명 변경

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,26 +1,28 @@
 package lotto;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoStore {
+
+    private static final int BASE_AMOUNT = 1000;
+
     public List<Lotto> buy(int fee) {
-        if (isNotThousandUnit(fee)) {
+        if (isInvalidBaseUnit(fee)) {
             throw new IllegalArgumentException("1000원 단위로 입력해주세요");
         }
 
         ArrayList<Lotto> result = new ArrayList<>();
-        for (int i = 0; i < fee / 1000; i++) {
+        for (int i = 0; i < fee / BASE_AMOUNT; i++) {
             List<Integer> numbers = IntStream.range(1, 7).boxed().collect(Collectors.toList());
             result.add(new Lotto(numbers));
         }
         return result;
     }
 
-    private boolean isNotThousandUnit(int fee) {
-        return fee % 1000 != 0;
+    private boolean isInvalidBaseUnit(int fee) {
+        return fee % BASE_AMOUNT != 0;
     }
 }
