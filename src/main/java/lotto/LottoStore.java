fd899b6520eb90fe451a02849995b71b1d237a7d
hunko <rhwlgns4386@naver.com>
2024-10-24 15:52:09 +0900

    feat : 1000원단위 로또 생성 로직 구현

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,10 +1,23 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoStore {
-    public void buy(int fee) {
+    public List<Lotto> buy(int fee) {
         if (isNotThousandUnit(fee)) {
             throw new IllegalArgumentException("1000원 단위로 입력해주세요");
         }
+
+        ArrayList<Lotto> result = new ArrayList<>();
+        for (int i = 0; i < fee / 1000; i++) {
+            List<Integer> numbers = IntStream.range(1, 7).boxed().collect(Collectors.toList());
+            result.add(new Lotto(numbers));
+        }
+        return result;
     }
 
     private boolean isNotThousandUnit(int fee) {
--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -1,10 +1,23 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoStoreTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 2000, 15000})
+    public void 로또는_천원단위로_생선된다(int fee) {
+        LottoStore lottoStore = new LottoStore();
+
+        List<Lotto> result = lottoStore.buy(fee);
+        assertThat(result).hasSize(fee / 1000);
+    }
 
     @Test
     public void 로또는_천원단위가_아니면_예외가_발생한다() {
