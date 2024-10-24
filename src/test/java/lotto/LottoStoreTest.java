0394ad2ba5a8adcff59c1138c885668330657680
hunko <rhwlgns4386@naver.com>
2024-10-24 15:27:31 +0900

    refactor : 테스트 함수명 오타 수정

--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class LottoStoreTest {
 
     @Test
-    public void 로또는_천원단위가_아미녀_예외가_발생한다() {
+    public void 로또는_천원단위가_아니면_예외가_발생한다() {
         LottoStore lottoStore = new LottoStore();
         assertThatIllegalArgumentException().isThrownBy(() -> lottoStore.buy(999));
     }
