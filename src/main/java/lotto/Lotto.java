96d9589117f2e80b6a9dfe03150a0e7683e87481
hunko <rhwlgns4386@naver.com>
2024-10-24 14:36:51 +0900

    refactor : 각각의 검증로직 함수화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 public class Lotto {
     public Lotto(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(i -> i <= 0 || 45 < i) || numbers.size() != 6
-                || numbers.stream().distinct().count() != numbers.size()) {
+        if (hasInvalidNumber(numbers) || hasIncorrectSize(numbers) || hasDuplicates(numbers)) {
             throw new IllegalArgumentException("올바르지 않은 입력 입니다.");
         }
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
+    private boolean hasIncorrectSize(List<Integer> numbers) {
+        return numbers.size() != 6;
+    }
+
+    private boolean hasInvalidNumber(List<Integer> numbers) {
+        return numbers.stream().anyMatch(i -> i < 1 || 45 < i);
     }
 }
