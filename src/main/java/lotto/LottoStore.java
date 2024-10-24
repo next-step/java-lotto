5e52b6bf5a8d06c580fcc15ebd9f4e044d9edb51
hunko <rhwlgns4386@naver.com>
2024-10-24 15:37:18 +0900

    refactor : 검증로직 함수화
    1000원 검증로직 함수화

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -2,8 +2,12 @@ package lotto;
 
 public class LottoStore {
     public void buy(int fee) {
-        if (fee % 1000 != 0) {
+        if (isNotThousandUnit(fee)) {
             throw new IllegalArgumentException("1000원 단위로 입력해주세요");
         }
+    }
+
+    private boolean isNotThousandUnit(int fee) {
+        return fee % 1000 != 0;
     }
 }
