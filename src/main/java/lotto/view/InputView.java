82cc05542d765866eaeaec5614c45d040333e4d4
hunko <rhwlgns4386@naver.com>
2024-10-25 14:46:18 +0900

    feat : 금액 입력로직 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static int readAmount(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+}
