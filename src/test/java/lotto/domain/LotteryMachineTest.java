ac52850d1bca5ab778287db9961f692c9ff14e6c
stoneHee99 <kiit0901@gmail.com>
2024-10-25 11:52:52 +0900

    test: 구매 금액보다 더 많은 수의 수동 번호를 입력하면 예외 발생

--- src/test/java/lotto/domain/LotteryMachineTest.java
+++ src/test/java/lotto/domain/LotteryMachineTest.java
@@ -48,4 +48,11 @@ class LotteryMachineTest {
                 .isEqualTo(2);
     }
 
+    @DisplayName("로또 구매 금액보다 더 많은 개수의 수동 발급 로또 번호로 생성할 경우 IllegalArgumentException을 잘 반환하는지")
+    @Test
+    void lotteryMachineTest_withManualLottoCountExceed() {
+        assertThatThrownBy(() -> new LotteryMachine(List.of("1, 2, 3, 6, 7, 8", "2, 4, 5, 6, 8, 25"), "1000"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
