3e75932ae4d04ce50d205af2841aede7c9da63a3
ming <devel.moon17@gmail.com>
2024-10-25 19:11:01 +0900

    test: 4단계 - 수동 로또 개수 유효성 체크 테스트 코드 작성

--- src/test/java/lotto/LottoTicketValidatorTest.java
+++ src/test/java/lotto/LottoTicketValidatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class LottoTicketPriceTest {
+public class LottoTicketValidatorTest {
     @Test
     @DisplayName("구입금액에 맞게 구매한 로또 개수 출력")
     void 로또_구매() {
@@ -20,5 +20,14 @@ public class LottoTicketPriceTest {
                 .isThrownBy(() -> {
                     LottoTicketValidator.countByAmount(900);
                 }).withMessageMatching("지불한 금액이 로또 금액보다 적습니다.");
+    }
+
+    @Test
+    @DisplayName("구매할 수 있는 로또 수보다 수동으로 구매할 로또가 더 많은 경우")
+    void 수동_로또_개수_검증() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    LottoTicketValidator.checkManualLottoCount(4, 5);
+                }).withMessageMatching("구매할 수 있는 로또 수보다 수동으로 구매할 로또의 수가 더 많습니다.");
     }
 }
