cc7b037f455b7cfcbf81806313f641062303e860
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-26 13:15:52 +0900

    refactor : 나눗셈에 대한 검증식 추가 및 ToIntBiFunction 사용

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,7 +1,7 @@
 package calculator;
 
 import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.function.ToIntBiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,21 +9,24 @@ public enum Operator {
     PLUS("+", (a, b) -> a + b),
     MINUS("-", (a, b) -> a - b),
     MULTIPLY("*", (a, b) -> a * b),
-    DIVIDE("/", (a, b) -> a / b);
+    DIVIDE("/", (a, b) -> {
+        validateDivision(b);
+        return a / b;
+    });
 
     private final String symbol;
-    private final BiFunction<Integer, Integer, Integer> operation;
+    private final ToIntBiFunction<Integer, Integer> operation;
 
     private static final Map<String, Operator> SYMBOL_OPERATOR_MAP = Stream.of(values())
             .collect(Collectors.toMap(operator -> operator.symbol, operator -> operator));
 
-    Operator(String symbol, BiFunction<Integer, Integer, Integer> operation) {
+    Operator(String symbol, ToIntBiFunction<Integer, Integer> operation) {
         this.symbol = symbol;
         this.operation = operation;
     }
 
     public int apply(int a, int b) {
-        return operation.apply(a, b);
+        return operation.applyAsInt(a, b);
     }
 
     public static Operator symbolToOperator(String symbol) {
@@ -32,5 +35,11 @@ public enum Operator {
             throw new IllegalArgumentException("유효하지 않은 연산자입니다: " + symbol);
         }
         return operator;
+    }
+
+    private static void validateDivision(int b) {
+        if (b == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
     }
 }
