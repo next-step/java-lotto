bba9d6c4226d40d00a37d097166b44941fca8ea9
kimsuyeondev <rhaehl@naver.com>
2024-10-26 17:25:46 +0900

    #step3 피드백반영 - 결과뷰 수정

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -5,6 +5,7 @@ import lotto.dto.LottoResultDTO;
 import lotto.lotto.*;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
@@ -12,12 +13,9 @@ public class ResultView {
         System.out.println("당첨통계");
         System.out.println("============");
         Map<LottoRank, Integer> map = result.getResultMap();
-
-        System.out.println("3개 일치 (5000원)- " + map.get(LottoRank.RANK_FOUR));
-        System.out.println("4개 일치 (50000원)- " + map.get(LottoRank.RANK_THREE));
-        System.out.println("5개 일치 (1500000원)- " + map.get(LottoRank.RANK_TWO));
-        System.out.println("5개 일치 (30000000원)- " + map.get(LottoRank.RANK_TWO_BONUS));
-        System.out.println("6개 일치 (2000000000원)- " + map.get(LottoRank.RANK_ONE));
+        System.out.println(map.entrySet().stream().map(lottoRankIntegerEntry -> lottoRankIntegerEntry.getKey().getMachingCnt() + "개 일치 ("
+                        + lottoRankIntegerEntry.getKey().getWinPrice() + ") - " + lottoRankIntegerEntry.getValue())
+                .collect(Collectors.joining(System.lineSeparator())));
         System.out.printf("총 수익률은 %.2f 입니다.", result.getMarginRate());
     }
 
