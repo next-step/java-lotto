c4447f7284dc7259112288152e305a89c95d9865
maeng24 <maengindex@gmail.com>
2024-10-27 20:59:56 +0900

    feat: add exception logic for Lotto
    - 로또 생성시 입력되는 숫자를 체크하는 익셉션 로직을 추가하였습니다.
    - 중복 숫자가 있을 경우 익셉션을 발생시키는 로직을 추가하였습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,11 +7,24 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        numberExceptionCheck(numbers);
+        duplicateNumberCheck(numbers);
+
         Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    public Integer getLottoSize(){
+    public Integer getLottoSize() {
         return this.numbers.size();
+    }
+
+    private void duplicateNumberCheck(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() < 6)
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+    }
+
+    private void numberExceptionCheck(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty())
+            throw new IllegalArgumentException("로또 번호가 없습니다.");
     }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,7 +5,10 @@ import lotto.domain.LottoFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTest {
 
@@ -15,5 +18,17 @@ public class LottoTest {
         // 로또가 랜덤 난수로 제대로 만들어졌다면, 6개의 로또 번호를 가지고 있을 것
         Lotto lotto = LottoFactory.createLottoWithRandomNumbers();
         assertThat(lotto.getLottoSize()).isEqualTo(6);
+    }
+
+    @DisplayName("로또 생성시 예외사항을 확인하는 테스트")
+    @Test
+    public void lottoDuplicateNumberExceptionTest() {
+        assertThatThrownBy(() ->
+                LottoFactory.createLotto(Arrays.asList(1, 1, 3, 30, 40, 44)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() ->
+                LottoFactory.createLotto(null))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
