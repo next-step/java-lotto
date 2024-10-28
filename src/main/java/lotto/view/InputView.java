19e0cf7b922b9df79bfd54fc00944256375ccdff
김혜영 <sunzero0116@gmail.com>
2024-10-28 23:49:53 +0900

    feat : InputView 구매금액 입력, 수동으로 구매할 로또 수 입력, 수동으로  구매할 번호 입력하도록 처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,17 +2,30 @@ package lotto.view;
 
 import lotto.domain.LottoNumber;
 import lotto.domain.LottoTicket;
-import lotto.domain.LottoTickets;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static LottoTickets getLottoTickets() {
+    public static int getPrice() {
         System.out.println("구입금액을 입력해 주세요.");
-        int price = Integer.parseInt(SCANNER.nextLine());
-        return LottoTickets.createByPrice(price);
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static int getManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static List<String> getManualLottoNumbers(int manualCount) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        return Stream.generate(SCANNER::nextLine)
+                     .limit(manualCount)
+                     .collect(Collectors.toList());
     }
 
     public static LottoTicket getWinningNumbers() {
