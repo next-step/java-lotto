9a84622e8966669d86ffa71855d1b1bdcabbbbc9
praivesi <praivesi@gmail.com>
2024-12-01 17:23:54 +0900

    refac: PR 리뷰반영 4 - LottoNumberCache

--- src/main/java/LottoNumberCache.java
+++ src/main/java/LottoNumberCache.java
@@ -4,13 +4,19 @@ import java.util.Map;
 public class LottoNumberCache {
     private static final Map<Integer, LottoNumber> cache = new HashMap<>();
 
+    static {
+        for (int i = LottoNumber.MIN_LOTTO_NUMBER; i <= LottoNumber.MAX_LOTTO_NUMBER; i++) {
+            cache.put(i, new LottoNumber(i));
+        }
+    }
+
     public static LottoNumber get(String numStr) {
         return get(Integer.parseInt(numStr));
     }
 
     public static LottoNumber get(int num) {
         if (!cache.containsKey(num)) {
-            cache.put(num, new LottoNumber(num));
+            throw new IllegalArgumentException(num + " is not a lotto number");
         }
 
         return cache.get(num);
