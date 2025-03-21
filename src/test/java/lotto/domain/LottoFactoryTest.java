447a128868d1444f4fbb05a6d743231247ef37ab
jemi(이재국) <jemi.lee@kakaocorp.com>
2025-03-22 01:32:52 +0900

    test: 테스트 수정

--- src/test/java/lotto/domain/LottoFactoryTest.java
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -1,7 +1,9 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoFactoryTest {
@@ -10,7 +12,8 @@ class LottoFactoryTest {
 
     @Test
     void createLottoTest() {
-        Lotto lotto = lottoFactory.createLotto();
-        System.out.println(lotto);
+        Lotto actual = lottoFactory.createLotto();
+
+        assertThat(actual).isNotNull();
     }
 }
