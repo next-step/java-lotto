33f06bd6fc2ac0681428bb2d63d3be923a4db651
yang Jiyoung <0112jyoung@naver.com>
2025-03-21 23:26:42 +0900

    [test] 계산식 테스트 설정

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+    @Test
+    @DisplayName("표현식을 계산한다.")
+    void calculate() {
+        Expression expression = Expression.createExpression("2 + 3 * 4 / 2");
+        Calculator calculator = new Calculator();
+        calculator.calculate(expression);
+        assertThat(calculator.calculate(expression)).isEqualTo(10);
+    }
+}
