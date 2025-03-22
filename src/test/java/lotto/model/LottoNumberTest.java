7b2a888e45c9259e36ee33b957bca0c1c19b9efd
luku756 <luku756@gmail.com>
2025-03-23 02:56:49 +0900

    test 로또_동일 TC 추가

--- src/test/java/lotto/model/LottoNumberTest.java
+++ src/test/java/lotto/model/LottoNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,9 @@ class LottoNumberTest {
             new LottoNumber(0);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1부터 45 사이여야 합니다.");
+    }
+    @Test
+    void 로또_동일() {
+        assertThat(new LottoNumber(5)).isEqualTo(new LottoNumber(5));
     }
 }
