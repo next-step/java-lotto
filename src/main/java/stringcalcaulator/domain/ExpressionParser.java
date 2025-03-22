33790ab040ce5dfcd26063a811f91f96850ba57a
jemi(이재국) <jemi.lee@kakaocorp.com>
2025-03-23 04:01:24 +0900

    refactor: stack제거

--- src/main/java/stringcalcaulator/domain/ExpressionParser.java
+++ src/main/java/stringcalcaulator/domain/ExpressionParser.java
@@ -1,29 +1,24 @@
 package stringcalcaulator.domain;
 
-import java.util.LinkedList;
-
 public class ExpressionParser {
 
-    private final LinkedList<Expression> stack = new LinkedList<>();
-
     public Expression parse(ExpressionIterator iterator) {
+        Expression lastExpr = null;
+
         while (iterator.hasNext()) {
             String token = iterator.next();
 
             if (Operator.isOperator(token)) {
                 Operator operator = Operator.from(token);
-                Expression left = stack.pop();
                 OperandExpression right = new OperandExpression(iterator.next());
-                OperatorExpression expr
-                    = new OperatorExpression(left, operator, right);
-                stack.push(expr);
+                lastExpr = new OperatorExpression(lastExpr, operator, right);
                 continue;
             }
 
-            stack.push(new OperandExpression(token));
+            lastExpr = new OperandExpression(token);
         }
 
-        return stack.pop();
+        return lastExpr;
     }
 
 }
