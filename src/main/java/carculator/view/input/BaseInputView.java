c5581d504fa80be905b90bc7066df681e0478acd
홍예슬/서비스개발팀/팀원 <yeseul.dev@gmail.com>
2025-03-22 11:47:36 +0900

    feat(step1): 문자열 입력 해서 토큰 반환 하는 메서드 추가

--- src/main/java/carculator/view/input/BaseInputView.java
+++ src/main/java/carculator/view/input/BaseInputView.java
@@ -1,5 +1,16 @@
 package carculator.view.input;
 
+import carculator.domain.token.ExpressionParser;
+import carculator.domain.token.Token;
+
+import java.util.List;
+
 public abstract class BaseInputView implements InputView {
+
+    @Override
+    public List<Token> getTokens() {
+        System.out.println("계산하고 싶은 문자열을 입력하세요.");
+        return ExpressionParser.parseTokens(this.read());
+    }
 
 }
