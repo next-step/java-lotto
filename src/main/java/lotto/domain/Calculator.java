847bbefe7e59b7250ad28fde1bb789f7651fc67f
finn.h <finn.h@kakaocorp.com>
2025-03-23 10:35:32 +0900

    chore: Math 사용하여 계산

--- src/main/java/lotto/domain/Calculator.java
+++ src/main/java/lotto/domain/Calculator.java
@@ -1,21 +1,21 @@
-package lotto;
+package lotto.domain;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a + b;
+        return Math.addExact(a, b);
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        return Math.subtractExact(a, b);
     }
 
     public int multiply(int a, int b) {
-        return a * b;
+        return Math.multiplyExact(a, b);
     }
 
     public int divide(int a, int b) {
         if (a % b != 0)
             throw new ArithmeticException();
-        return a / b;
+        return Math.floorDiv(a, b);
     }
 }
