0ec1a1311d75c4d4406aa910bea9f65c1445788c
yeojin yu <yeojin@ninewatt.com>
2025-03-24 08:48:20 +0900

    feat: Operand -> toInt 분리

--- src/main/java/step1/domain/Operand.java
+++ src/main/java/step1/domain/Operand.java
@@ -3,16 +3,20 @@ package step1.domain;
 public class Operand {
     private final int number;
 
-    private Operand(String number) {
-        this.number = Integer.parseInt(number);
+    private Operand(int number) {
+        this.number = number;
     }
 
     public static Operand from(String input) {
         try {
-            return new Operand(input);
+            return new Operand(toInt(input));
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("정수가 아닙니다: " + input);
         }
+    }
+
+    private static int toInt(String stringNum) {
+        return Integer.parseInt(stringNum);
     }
 
     public int getOperand() {
