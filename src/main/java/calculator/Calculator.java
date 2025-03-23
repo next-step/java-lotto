a0a856fc82650489977d7023d51ca2ae5a256e26
selene-p <hwp0223@gmail.com>
2025-03-23 15:17:42 +0900

    feat: 잘못된 연산자 입력 시 에러

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,5 @@
 package calculator;
 
-
 import java.util.List;
 
 public class Calculator {
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CalculatorTest {
 
@@ -55,5 +56,15 @@ public class CalculatorTest {
     int result = Calculator.calculate(input);
 
     assertThat(result).isEqualTo(3);
+  }
+
+  @DisplayName("잘못된 연산자 테스트")
+  @Test
+  void testInvalidOperator() {
+    String input = "10 + 2 * 3 % 9 - 1";
+
+    assertThatThrownBy(() -> Calculator.calculate(input))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("잘못된 연산자입니다.");
   }
 }
