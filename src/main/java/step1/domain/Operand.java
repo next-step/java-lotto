a51fe0dac8197975dfae84a5d6dd2bf3ab86ece8
lyeo72 <lyeo7272@gmail.com>
2025-03-24 03:03:55 +0900

    feat: 피연산자 class 추가

---
+++ src/main/java/step1/domain/Operand.java
@@ -0,0 +1,18 @@
+package step1.domain;
+
+public class Operand {
+    private final int number;
+
+    private Operand(String number) {
+        this.number = Integer.parseInt(number);
+    }
+
+    public static Operand from(String input) {
+        try {
+            return new Operand(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수가 아닙니다: " + input);
+        }
+    }
+
+}
