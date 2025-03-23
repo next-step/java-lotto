6441cadb6c5582aa03514c470e4ecb131b12a534
jongheon1 <heon0128@gmail.com>
2025-03-23 20:05:49 +0900

    refactor(expression 필드명 수정)

--- src/main/java/step1/Expression.java
+++ src/main/java/step1/Expression.java
@@ -4,33 +4,33 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Expression {
-    private final Queue<Operand> operands;
-    private final Queue<Operator> operators;
+    private final Queue<Operand> _operands;
+    private final Queue<Operator> _operators;
 
     public Expression(Queue<Operand> operands, Queue<Operator> operators) {
         if (operands.isEmpty()) {
             throw new RuntimeException("잘못된 수식입니다.");
         }
-        this.operands = operands;
-        this.operators = operators;
+        this._operands = operands;
+        this._operators = operators;
     }
 
     public int evaluate() {
-        Queue<Operand> operandsCopy = new LinkedList<>(operands);
-        Queue<Operator> operatorsCopy = new LinkedList<>(operators);
+        Queue<Operand> operands = new LinkedList<>(_operands);
+        Queue<Operator> operators = new LinkedList<>(_operators);
 
-        if (operandsCopy.isEmpty()) {
+        if (operands.isEmpty()) {
             throw new RuntimeException("잘못된 수식입니다.");
         }
 
-        Operand left = operandsCopy.poll();
-        while (!operandsCopy.isEmpty() && !operatorsCopy.isEmpty()) {
-            Operator operator = operatorsCopy.poll();
-            Operand right = operandsCopy.poll();
+        Operand left = operands.poll();
+        while (!operands.isEmpty() && !operators.isEmpty()) {
+            Operator operator = operators.poll();
+            Operand right = operands.poll();
             left = operator.apply(left, right);
         }
 
-        if (!operandsCopy.isEmpty() || !operatorsCopy.isEmpty()) {
+        if (!operands.isEmpty() || !operators.isEmpty()) {
             throw new RuntimeException("잘못된 수식입니다.");
         }
 
