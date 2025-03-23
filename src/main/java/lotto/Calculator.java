fcfaa8a15fc4b2947f935543fd779ef6556c4bbf
finn.h <finn.h@kakaocorp.com>
2025-03-23 10:25:08 +0900

    feat: 덧셈 계산

---
+++ src/main/java/lotto/Calculator.java
@@ -0,0 +1,7 @@
+package lotto;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/lotto/CalculateTest.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculateTest {
+    @Test
+    void 덧셈() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.add(1, 2)).isEqualTo(3);
+    }
+}
