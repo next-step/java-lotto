4aaf57e7740de1b76480915fadce4a0fdff68184
JiEung2 <181757@jnu.ac.kr>
2025-03-23 23:10:38 +0900

    test 덧셈 연산 테스트 추가

---
+++ src/test/java/utils/CalculatorTest.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("+가 주어졌을 때 두 숫자에 대해 덧셈을 진행한다.")
+    void sum() {
+        Assertions.assertThat(Calculator.calculate(1, 2, "+")).isEqualTo(3);
+    }
+}
