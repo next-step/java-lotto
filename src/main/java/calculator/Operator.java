7ac033f5a26c8af4fe6b99d74f726dcc4a5527a8
YuGyeong <lyg64522@naver.com>
2025-03-23 23:07:39 +0900

    feat: 연산자 예외값 체크 기능 추가
    - 연산자 예외값 체크 기능 추가
    - 연산자 예외값 체크 테스트 추가

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,10 +1,20 @@
 package calculator;
 
+import java.util.Objects;
+
 public class Operator {
 
     private String value;
 
     public Operator(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("연산자는 null이 될 수 없습니다");
+        }
+
+        if (!(value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/"))) {
+            throw new IllegalArgumentException("잘못된 사칙연산 기호입니다");
+        }
+
         this.value = value;
     }
     public Operand calculate(Operand left, Operand right) {
@@ -13,5 +23,22 @@ public class Operator {
         }
 
         return left.add(right);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Operator operator = (Operator) o;
+        return Objects.equals(value, operator.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
