2557859bbe025b2f6a6f08d71374cd7b1d5a46f4
finn.h <finn.h@kakaocorp.com>
2025-03-23 10:45:48 +0900

    fix: 오타 수정

--- src/test/java/lotto/domain/CalculatorTest.java
+++ src/test/java/lotto/domain/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class CalculateTest {
+public class CalculatorTest {
     @Test
     void 덧셈() {
         Calculator calculator = new Calculator();
