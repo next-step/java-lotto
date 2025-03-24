08e602be5fdee001b4d99df832a3f19f80b36cfb
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-24 18:27:16 +0900

    [리팩토링] Calculator 메서드 추가 및 기존 메서드 접근제어자 변경 (테스트 코드 수정)

--- src/main/java/step2/domain/Calculator.java
+++ src/main/java/step2/domain/Calculator.java
@@ -19,7 +19,20 @@ public class Calculator {
         operations.put("/", new Division());
     }
 
-    public static int calculate(int number1, int number2, String operator) {
+    public static int calculate(ExpressionElement element) {
+        int result = element.getElements().get(0);
+
+        for (int i = 0; i < element.getOperators().size(); i++) {
+            String operator = element.getOperators().get(i);
+            int number = element.getElements().get(i + 1);
+
+            result = calculate(result, number, operator);
+        }
+
+        return result;
+    }
+
+    private static int calculate(int number1, int number2, String operator) {
         Operation operation = operations.get(operator);
 
         if (operation == null) {
--- src/test/java/step2/domain/CalculatorTest.java
+++ src/test/java/step2/domain/CalculatorTest.java
@@ -1,43 +1,48 @@
 package step2.domain;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static step2.domain.Calculator.calculate;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CalculatorTest {
     @ParameterizedTest
-    @CsvSource({
-        "10, 5, +, 15",
-        "10, 5, -, 5",
-        "10, 5, *, 50",
-        "10, 5, /, 2",
-        "-10, 5, +, -5",
-        "-10, 5, -, -15",
-        "-10, 5, *, -50",
-        "-10, 5, /, -2"
+    @CsvSource(value = {
+        "10, 20, +, 30",
+        "10, 20, -, -10",
+        "10, 20, *, 200",
+        "10, 20, /, 0"
     })
     @DisplayName("사칙연산 테스트")
-    void testCalculator(int number1, int number2, String operator, int expected) {
-        int result = calculate(number1, number2, operator);
-        assertThat(result).isEqualTo(expected);
+    void testCalculate(int num1, int num2, String operator, int expectedResult) {
+        ExpressionElement element = new ExpressionElement(List.of(num1, num2), List.of(operator));
+        int result = Calculator.calculate(element);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    @Test
-    @DisplayName("0으로 나누기 테스트")
-    void testDivisionByZero() {
-        assertThatThrownBy(() -> calculate(10, 0, "/"))
+    @ParameterizedTest
+    @CsvSource(value = {
+        "10, 20, %"
+    })
+    @DisplayName("지원하지 않는 연산자 테스트")
+    void testCalculate_UnsupportedOperator(int num1, int num2, String operator) {
+        ExpressionElement element = new ExpressionElement(List.of(num1, num2), List.of(operator));
+        assertThatIllegalArgumentException().isThrownBy(() -> Calculator.calculate(element))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    @DisplayName("지원하지 않는 연산자 테스트")
-    void testUnsupportedOperator() {
-        assertThatThrownBy(() -> calculate(10, 5, "%"))
+    @ParameterizedTest
+    @CsvSource(value = {
+        "10, 0, /"
+    })
+    @DisplayName("0으로 나누기 테스트")
+    void testCalculate_DivisionByZero(int num1, int num2, String operator) {
+        ExpressionElement element = new ExpressionElement(List.of(num1, num2), List.of(operator));
+        assertThatIllegalArgumentException().isThrownBy(() -> Calculator.calculate(element))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
