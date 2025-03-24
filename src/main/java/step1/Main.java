6e0018dc85d7183e9eebb17729ea44c36e40039e
lyeo72 <lyeo7272@gmail.com>
2025-03-24 18:46:43 +0900

    refactor: Main 가독성 개선

--- src/main/java/step1/Main.java
+++ src/main/java/step1/Main.java
@@ -2,13 +2,17 @@ package step1;
 
 import step1.domain.Calculator;
 import step1.domain.Expression;
+import step1.util.ExpressionTokens;
 import step1.view.InputView;
 import step1.view.Outputview;
+
+import java.util.List;
 
 public class Main {
     public void run() {
         Calculator calculator = new Calculator();
-        int result = calculator.calculate(new Expression(InputView.input()));
+        List<String> split = ExpressionTokens.parseTokens(InputView.input());
+        int result = calculator.calculate(new Expression(split));
         Outputview.result(result);
     }
 }
