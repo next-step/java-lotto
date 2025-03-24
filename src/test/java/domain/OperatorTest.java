449969f38c658a1c814978f4b3408d09e15ca469
JiEung2 <181757@jnu.ac.kr>
2025-03-24 20:50:57 +0900

    test 각각의 연산자 기능을 테스트하는 코드 추가

---
+++ src/test/java/domain/OperatorTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OperatorTest {
+    @Test
+    @DisplayName("덧셈 기능")
+    void plus_test() {
+        Operator operator = Operator.from("+");
+        Assertions.assertThat(operator.apply(1, 2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("뺄셈 기능")
+    void minus_test() {
+        Operator operator = Operator.from("-");
+        Assertions.assertThat(operator.apply(1, 2)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("곱셈 기능")
+    void multiply_test() {
+        Operator operator = Operator.from("*");
+        Assertions.assertThat(operator.apply(1, 2)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("나눗셈 기능")
+    void divide_test() {
+        Operator operator = Operator.from("/");
+        Assertions.assertThat(operator.apply(1, 2)).isEqualTo(0);
+    }
+}
