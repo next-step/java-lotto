a2a17393acede27921fb4d50c7e83c05a98c7b5f
panya <kjy2844@kaist.ac.kr>
2025-03-25 00:22:24 +0900

    feat: 입력받은 문자열 분리

--- src/main/java/CalculatorInput.java
+++ src/main/java/CalculatorInput.java
@@ -1,12 +1,20 @@
+import java.util.List;
+
 public class CalculatorInput {
 
     private final String input;
+    private final List<String> tokens;
 
     public CalculatorInput(String input) {
         this.input = input;
+        this.tokens = List.of(input.split(" "));
     }
 
     public String getInput() {
         return input;
+    }
+
+    public List<String> getTokens() {
+        return tokens;
     }
 }
--- src/test/java/CalculatorInputTest.java
+++ src/test/java/CalculatorInputTest.java
@@ -4,11 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorInputTest {
+
     @Test
     @DisplayName("문자열 입력 받기")
-    void inputString() {
+    void canCreateCalculatorInput() {
         String input = "2 + 3 * 4 / 2";
         CalculatorInput calculatorInput = new CalculatorInput(input);
         assertThat(calculatorInput.getInput()).isEqualTo(input);
+    }
+
+    @Test
+    @DisplayName("공백으로 문자, 숫자 분리")
+    void canTokenize() {
+        String input = "2 + 3 * 4 / 2";
+        CalculatorInput calculatorInput = new CalculatorInput(input);
+        assertThat(calculatorInput.getTokens()).containsExactly("2", "+", "3", "*", "4", "/", "2");
     }
 }
