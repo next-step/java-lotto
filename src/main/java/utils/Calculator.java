82c9dd89ccbd58d09e4c7a2ddeb098b9b6dfc041
JiEung2 <181757@jnu.ac.kr>
2025-03-24 20:55:56 +0900

    refactor Calculator는 input 자체를 받아 계산 전체를 진행하도록 수정

--- src/main/java/utils/Calculator.java
+++ src/main/java/utils/Calculator.java
@@ -1,48 +1,10 @@
 package utils;
 
-import java.util.List;
+import domain.Expression;
 
 public class Calculator {
-    private static final String PLUS_SYMBOL = "+";
-    private static final String MINUS_SYMBOL = "-";
-    private static final String MULTIPLY_SYMBOL = "*";
-    private static final String DIVIDE_SYMBOL = "/";
-
-    public static int evaluateExpression(List<String> expressions) {
-        int result = InputView.toInt(expressions.get(0));
-
-        for (int i = 1; i < expressions.size(); i += 2) {
-            String operator = expressions.get(i);
-            int nextNumber = InputView.toInt(expressions.get(i + 1));
-
-            result = calculate(result, nextNumber, operator);
-        }
-
-        return result;
-    }
-
-    public static int calculate(int number1, int number2, String operator) {
-        if (operator.equals(PLUS_SYMBOL)) return sum(number1, number2);
-        if (operator.equals(MINUS_SYMBOL)) return subtract(number1, number2);
-        if (operator.equals(MULTIPLY_SYMBOL)) return multiply(number1, number2);
-        if (operator.equals(DIVIDE_SYMBOL)) return divide(number1, number2);
-
-        throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
-    }
-
-    public static int sum(int number1, int number2) {
-        return number1 + number2;
-    }
-
-    public static int subtract(int number1, int number2) {
-        return number1 - number2;
-    }
-
-    public static int multiply(int number1, int number2) {
-        return number1 * number2;
-    }
-
-    public static int divide(int number1, int number2) {
-        return number1 / number2;
+    public static int calculate(String input) {
+        Expression expression = new Expression(input);
+        return expression.evaluate();
     }
 }
