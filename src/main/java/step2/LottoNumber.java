00107999fb202ed8c54b03efff70b65f70940f1e
jongheon1 <heon0128@gmail.com>
2025-03-24 23:11:21 +0900

    feat(lottoNumber)

---
+++ src/main/java/step2/LottoNumber.java
@@ -0,0 +1,20 @@
+package step2;
+
+public class LottoNumber {
+    private final int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        validateLottoNumber(lottoNumber);
+        this.lottoNumber = lottoNumber;
+    }
+
+    private void validateLottoNumber(int lottoNumber) {
+        if (lottoNumber < 1 || lottoNumber > 45) {
+            throw new RuntimeException("로또의 숫자는 1 - 45 사이만 가능합니다.");
+        }
+    }
+
+    public int getLottoNumber() {
+        return this.lottoNumber;
+    }
+}
