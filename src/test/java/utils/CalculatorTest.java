fee353b26d96a8c3e507d0f5003d93b16ab1bec1
JiEung2 <181757@jnu.ac.kr>
2025-03-24 15:54:40 +0900

    🚀 1단계 - 문자열 계산기 (#4000)
    * test 덧셈 연산 테스트 추가
    
    * feat 덧셈 기능 추가
    
    - operator가 "+"일 경우 덧셈을 진행하도록 설계
    
    * docs 사칙연산 기호가 아닌 경우에 대한 처리 기능목록에 추가
    
    * test 뺄셈 연산 테스트 추가
    
    * feat 뺄셈 기능 추가
    
    * test 곱셈 연산 테스트 추가
    
    * feat 곱셈 기능 추가
    
    * test 나눗셈 연산 테스트 추가
    
    * feat 나눗셈 기능 추가
    
    * test 사칙연산 기호가 아닌 경우 예외처리 테스트 추가
    
    * feat 사칙 연산 기호가 아닌 경우의 예외 처리 기능 추가
    
    * refactor 사칙연산 기호들 상수 처리
    
    * docs 문자열 toInt 기능 목록에 추가
    
    * feat 문자열을 입력받는 기능 추가
    
    * feat 문자열 타입의 숫자를 int 타입으로 변환하는 기능 추가
    
    * feat 문자열을 공백을 기준으로 파싱하는 기능 추가
    
    * test 사칙연산을 진행하는 테스트 추가
    
    * feat 파싱된 expressions를 전달받아 계산을 진행하는 기능 추가
    
    * feat 입력 값이 null이거나 빈 공백 문자열일 경우 처리 기능 추가

---
+++ src/test/java/utils/CalculatorTest.java
@@ -0,0 +1,48 @@
+package utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("+가 주어졌을 때 두 숫자에 대해 덧셈을 진행한다.")
+    void sum() {
+        assertThat(Calculator.calculate(1, 2, "+")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("-가 주어졌을 때 두 숫자에 대해 뺄셈을 진행한다.")
+    void substract() {
+        assertThat(Calculator.calculate(1, 2, "-")).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("*가 주어졌을 때 두 숫자에 대해 곱셈을 진행한다.")
+    void multiply() {
+        assertThat(Calculator.calculate(1, 2, "*")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("/가 주어졌을 때 두 숫자에 대해 나눗셈을 진행한다.")
+    void divide() {
+        assertThat(Calculator.calculate(4, 2, "/")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("계산할 때 전달된 기호가 사칙연산 기호가 아니라면 IllegalArgumentException가 발생한다.")
+    void bad_operator() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Calculator.calculate(4, 2, "&");
+        });
+    }
+
+    @Test
+    @DisplayName("문자열을 입력받았을 때 사칙연산을 진행한다.")
+    void operate() {
+        assertThat(Calculator.evaluateExpression(List.of("2", "+", "3", "*", "4", "/", "2"))).isEqualTo(10);
+    }
+}
