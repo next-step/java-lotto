51fcf744fc08effeffb8f9325c3314d490593138
JiEung2 <181757@jnu.ac.kr>
2025-03-24 20:52:20 +0900

    refactor 피연산자를 객체로 도출시켜 타입을 변환하는 책임 위임

---
+++ src/main/java/domain/Operand.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Operand {
+    private final int value;
+
+    public Operand(String input) {
+        this.value = toInt(input);
+    }
+
+    private int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
