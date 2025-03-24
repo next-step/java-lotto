9183d3f0b7a0f20b1f356a4307d22f495b4d5bf9
yang Jiyoung <0112jyoung@naver.com>
2025-03-24 22:26:07 +0900

    [feat] 당첨 로또 번호가 존재하는지 추가

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Lotto {
@@ -23,5 +24,26 @@ public class Lotto {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(INVALID_LOTTO_SIZE);
         }
+    }
+
+    public long countWinningNumbers(Lotto lotto) {
+        return lottoNumbers.stream()
+                .filter(lotto.lottoNumbers::contains)
+                .count();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Lotto lotto = (Lotto) o;
+        return lottoNumbers.equals(lotto.lottoNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(lottoNumbers);
     }
 }
--- src/test/java/model/LottoTest.java
+++ src/test/java/model/LottoTest.java
@@ -9,21 +9,37 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
 
     @ParameterizedTest
-    @MethodSource("generateLottoNumbers")
+    @MethodSource("generateInvalidLottoNumbers")
     @DisplayName("로또 번호가 6개 아니면 에러를 반환한다.")
     public void invalidLottoNumberSize(List<Integer> numbers) {
         assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    static Stream<Arguments> generateLottoNumbers() {
+    static Stream<Arguments> generateInvalidLottoNumbers() {
         return Stream.of(
-                Arguments.of(List.of("1,2,3,4,5"))
-                , Arguments.of(List.of("1,2,3,4,5,6,7"))
+                Arguments.of(List.of(1, 2, 3, 4, 5))
+                , Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateValidLottoNumber")
+    @DisplayName("당첨 로또 번호와 현재 번호가 전부 일치한다.")
+    public void countWinningNumbers(List<Integer> numbers) {
+        Lotto lotto = new Lotto(numbers);
+        Lotto winLotto = new Lotto(numbers);
+        assertEquals(lotto.countWinningNumbers(winLotto), 6);
+    }
+
+    static Stream<Arguments> generateValidLottoNumber() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6))
         );
     }
 }
