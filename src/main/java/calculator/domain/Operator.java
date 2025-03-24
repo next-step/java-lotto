09c4df319373db26e6ee31ba1ecfb50ca34b15f4
zoolake <junhomoon224@gmail.com>
2025-03-24 23:05:58 +0900

    refactor : apply step1 code review (change abstract method in Operator(Enum) to BiFunction)

--- src/main/java/calculator/domain/Operator.java
+++ src/main/java/calculator/domain/Operator.java
@@ -1,46 +1,34 @@
 package calculator.domain;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
-public enum Operator{
-    PLUS("+") {
-        @Override
-        public int calculate(int left, int right) {
-            return left + right;
-        }
-    },
-    MINUS("-") {
-        @Override
-        public int calculate(int left, int right) {
-            return left - right;
-        }
-    },
-    MULTIPLY("*") {
-        @Override
-        public int calculate(int left, int right) {
-            return left * right;
-        }
-    },
-    DIVIDE("/") {
-        @Override
-        public int calculate(int left, int right) {
-            return left / right;
-        }
-    };
+public enum Operator {
+    PLUS("+", (left, right) -> left + right),
+
+    MINUS("-", (left, right) -> left - right),
+
+    MULTIPLY("*", (left, right) -> left * right),
+
+    DIVIDE("/", (left, right) -> left / right);
 
     private final String symbol;
+    private final BiFunction<Integer, Integer, Integer> calculator;
 
-    Operator(String symbol) {
+    Operator(String symbol, BiFunction<Integer, Integer, Integer> calculator) {
         this.symbol = symbol;
+        this.calculator = calculator;
     }
-
-    public abstract int calculate(int left, int right);
 
     public static Operator getOperatorBySymbol(String symbol) {
         return Arrays.stream(Operator.values())
                 .filter(operator -> operator.isSameSymbol(symbol))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 연산자입니다. 입력된 연산자: " + symbol));
+    }
+
+    public int calculate(int left, int right) {
+        return calculator.apply(left, right);
     }
 
     private boolean isSameSymbol(String symbol) {
