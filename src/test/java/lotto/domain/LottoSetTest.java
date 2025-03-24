c98c77de09c86a1ba8d8c7e7f39aa5b8fed9bfc0
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-03-24 13:54:16 +0900

    [feat] LottoSet Test 수정

--- src/test/java/lotto/domain/LottoSetTest.java
+++ src/test/java/lotto/domain/LottoSetTest.java
@@ -2,22 +2,18 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoSetTest {
 
   @Test
-  @DisplayName("입력 금액으로 몇 장의 로또를 살 수 있는지 계산한다.")
-  void calculateLottoCount() {
-    LottoSet lottoSet = new LottoSet(14000);
-    assertThat(lottoSet.getLottoCount()).isEqualTo(14000/LottoSet.LOTTO_PRICE);
-  }
-
-  @Test
   @DisplayName("로또를 구입하고나면 그 갯수만큼 로또가 생성된다.")
   void generateLotto() {
-    LottoSet lottoSet = new LottoSet(14000);
-    assertThat(lottoSet.getLottos().size()).isEqualTo(14000/LottoSet.LOTTO_PRICE);
+    Lotto lotto1 = Lotto.generateLotto(new RandomLottoGenerator());
+    Lotto lotto2 = Lotto.generateLotto(new RandomLottoGenerator());
+    LottoSet lottoSet = new LottoSet(List.of(lotto1, lotto2));
+    assertThat(lottoSet.getLottos().size()).isEqualTo(2);
   }
 }
