c99c1c104ec44fda857c8f5534e33fa3a71db182
JiEung2 <181757@jnu.ac.kr>
2025-03-24 20:55:16 +0900

    refactor InputView는 입력만 받도록 수정

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -1,28 +1,11 @@
 package utils;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public String inputExpression() {
-        String input = scanner.nextLine();
-        isNullOrBlank(input);
-        return input;
-    }
-
-    private static void isNullOrBlank(String input) {
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("입력값이 비어 있거나 null입니다.");
-        }
-    }
-
-    public static int toInt(String number) {
-        return Integer.parseInt(number);
-    }
-
-    public static List<String> parse(String input) {
-        return List.of(input.split(" "));
+        return scanner.nextLine();
     }
 }
