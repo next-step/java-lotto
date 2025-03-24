24763803f0aa2ca50adf98db4a00a70141bf804b
jemi(이재국) <jemi.lee@kakaocorp.com>
2025-03-24 10:44:54 +0900

    refactor: new Lotto 없애기

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -17,23 +17,21 @@ class LottoTest {
 
     @ParameterizedTest
     @MethodSource("invalidLottoNumbers")
-    void invalid한_lottoNumbers는_throw한다(List<LottoNumber> lottoNumbers) {
-        IllegalArgumentException e = catchThrowableOfType(() -> new Lotto(lottoNumbers),
+    void invalid한_lottoNumbers는_throw한다(List<Integer> lottoNumbers) {
+        IllegalArgumentException e = catchThrowableOfType(() ->
+                LottoFactory.createManual(lottoNumbers),
             IllegalArgumentException.class);
 
         assertThat(e).isNotNull();
     }
 
-    static Stream<List<LottoNumber>> invalidLottoNumbers() {
+    static Stream<List<Integer>> invalidLottoNumbers() {
         return Stream.of(
             Arrays.asList(1, 2, 3, 4, 5),
             Arrays.asList(1, 2, 3, 4, 5, 6, 7),
             Arrays.asList(1, 2, 3, 4, 5, 4, 4),
             Arrays.asList(1, 2, 3, 4, 5, 1, 1)
-        ).map(lottoNumbers -> lottoNumbers
-            .stream()
-            .map(LottoNumber::new)
-            .collect(Collectors.toList()));
+        );
     }
 }
 
