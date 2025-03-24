44315ebee5c9995f2f7c91e249f08d91829fdaea
yang Jiyoung <0112jyoung@naver.com>
2025-03-24 20:36:58 +0900

    [style] 미사용 import 제거

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,19 +1,13 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,7 +15,7 @@ class LottoTest {
     @ParameterizedTest
     @MethodSource("generateLottoNumbers")
     @DisplayName("로또 번호가 6개 아니면 에러를 반환한다.")
-    public void invalidLottoNumberSize(List<Integer> numbers){
+    public void invalidLottoNumberSize(List<Integer> numbers) {
         assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
