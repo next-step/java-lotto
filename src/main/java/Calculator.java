4fca2bf44ae9d471a6a98da8122ee210940cbb4d
panya <kjy2844@kaist.ac.kr>
2025-03-25 01:08:33 +0900

    feat: 하나의 숫자로 이루어진 표현식을 계산할 수 있다.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,11 +1,21 @@
 import java.util.List;
 
 public class Calculator {
+
     public Integer calculate(CalculatorInput input) {
         List<String> tokens = input.getTokens();
         int firstNumber = parseNumber(tokens.get(0));
+        
+        if (isSingleNumber(tokens)) {
+            return firstNumber;
+        }
+        
         int secondNumber = parseNumber(tokens.get(2));
         return firstNumber + secondNumber;
+    }
+
+    private boolean isSingleNumber(List<String> tokens) {
+        return tokens.size() == 1;
     }
 
     private int parseNumber(String token) {
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
+    @DisplayName("하나의 숫자로 이루어진 표현식을 계산할 수 있다.")
+    void singleNumberTest() {
+        Calculator calculator = new Calculator();
+        CalculatorInput input = new CalculatorInput("5");
+        assertThat(calculator.calculate(input)).isEqualTo(5);
+    }
+
+    @Test
     @DisplayName("두 개의 숫자로 이루어진 덧셈을 할 수 있다.")
     void additionTest() {
         Calculator calculator = new Calculator();
