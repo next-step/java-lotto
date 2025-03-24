927bad5b6bf00171f690875ef533bf3faeef3af1
JiEung2 <181757@jnu.ac.kr>
2025-03-24 20:52:49 +0900

    test 빈 값과 Null 값을 입력받았을 때 예외처리 테스트 추가

---
+++ src/test/java/domain/ExpressionTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utils.Calculator;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class ExpressionTest {
+    @Test
+    @DisplayName("입력 값이 빈 공백 문자일 경우 IllegalArgumentException을 던져야한다.")
+    void 빈_값_테스트() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Calculator.calculate(" ");
+        });
+    }
+
+    @Test
+    @DisplayName("입력 값이 null일 경우 IllegalArgumentException을 던져야한다.")
+    void null_값_테스트() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Calculator.calculate(null);
+        });
+    }
+}
