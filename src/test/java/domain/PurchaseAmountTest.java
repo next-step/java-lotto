a6beffe67f3d347b4569a3e73fd6ecaa393f0015
JiEung2 <181757@jnu.ac.kr>
2025-03-25 00:05:45 +0900

    test 구입 금액이 1000원으로 나누어떨어지지 않을 때 테스트 추가

--- src/test/java/domain/PurchaseAmountTest.java
+++ src/test/java/domain/PurchaseAmountTest.java
@@ -15,7 +15,13 @@ public class PurchaseAmountTest {
 
     @Test
     @DisplayName("구입 금액이 1000원보다 작을 때 IllegalArgumentException을 던진다.")
-    void 로또_금액_1000미만() {
+    void 구입_금액_1000미만() {
         assertThatIllegalArgumentException().isThrownBy(() -> new PurchaseAmount(900));
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원으로 나누어떨어지지 않을 때 IllegalArgumentException을 던진다.")
+    void 구입_금액이_1000으로_안나누어떨어짐() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new PurchaseAmount(1400));
     }
 }
