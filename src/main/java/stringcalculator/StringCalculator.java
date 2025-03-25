0d6205052806971ac1c556a0ae2a57c9a2a20fe1
minakusi <seoyeon737@gmail.com>
2025-03-25 10:32:13 +0900

    feat: 덧셈 기능 구현

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,34 @@
+package stringcalculator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringCalculator {
+    public static final String DELIMITER = " ";
+
+    public static int calculate(String equation) {
+        List<String> targets = splitEquation(equation);
+
+        int result = stringToInt(targets.get(0));
+        for (int i = 1; i < targets.size(); i += 2) {
+            result = calculateOneOperation(targets, result, i);
+        }
+        return result;
+    }
+
+    private static int calculateOneOperation(List<String> targets, int result, int idx) {
+        String curTarget = targets.get(idx);
+        if (curTarget.equals("+")) {
+            result += stringToInt(targets.get(++idx));
+        }
+        return result;
+    }
+
+    private static int stringToInt(String string) {
+        return Integer.parseInt(string);
+    }
+
+    private static List<String> splitEquation(String equation) {
+        return Arrays.asList(equation.split(DELIMITER));
+    }
+}
---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    @DisplayName("덧셈 기능 테스트")
+    @Test
+    void add() {
+        assertThat(StringCalculator.calculate("1 + 2 + -3")).isEqualTo(0);
+    }
+}
