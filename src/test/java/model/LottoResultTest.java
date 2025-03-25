3f969cc02ffd5c16dce167c1ad27c7bff60d5d54
yang Jiyoung <0112jyoung@naver.com>
2025-03-25 23:10:15 +0900

    [refactor] 로또 결과 테스트 수정

--- src/test/java/model/LottoResultTest.java
+++ src/test/java/model/LottoResultTest.java
@@ -12,30 +12,26 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LottoResultTest {
-    private List<Lotto> paidLottos;
-    private int payAmount;
+    private Lotto winningLotto;
 
     @BeforeEach
     public void init() {
-        paidLottos = new ArrayList<>();
-        Lotto paidLotto = new Lotto("1, 2, 3, 4, 5, 6");
-        paidLottos.add(paidLotto);
-        payAmount = 1000;
+        winningLotto = new Lotto("1, 2, 3, 4, 5, 6");
     }
 
     @Test
     @DisplayName("구매한 로또가 전부 일치하면 2,000,00%의 수익이 난다")
     public void totalProfitTest1() {
-        Lotto winningLotto = new Lotto("1, 2, 3, 4, 5, 6");
-        LottoResult result = LottoResult.of(winningLotto, paidLottos, payAmount);
+        List<Lotto> lottos = new ArrayList<>(List.of(new Lotto("1, 2, 3, 4, 5, 6")));
+        LottoResult result = LottoResult.of(winningLotto, lottos, 1000);
         assertEquals(result.getTotalProfitPercent(), 2000000.00);
     }
 
     @Test
     @DisplayName("구매한 로또가 전부 일치하지 않으면 0.00%의 수익이 난다")
     public void totalProfitTest2() {
-        Lotto winningLotto = new Lotto("10, 11, 12, 13, 14, 15");
-        LottoResult result = LottoResult.of(winningLotto, paidLottos, payAmount);
+        List<Lotto> lottos = new ArrayList<>(List.of(new Lotto("10, 11, 12, 13, 14, 15")));
+        LottoResult result = LottoResult.of(winningLotto, lottos, 1000);
         assertEquals(result.getTotalProfitPercent(), 0.00);
     }
 }
