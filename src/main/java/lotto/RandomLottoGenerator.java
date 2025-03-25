765c744c9f5fe1b21090196ae317b725fa5b4426
selene-p <hwp0223@gmail.com>
2025-03-25 22:24:08 +0900

    fix: 인덴트

--- src/main/java/lotto/RandomLottoGenerator.java
+++ src/main/java/lotto/RandomLottoGenerator.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class RandomLottoGenerator implements LottoGenerator {
 
-    private static final List<LottoNumber> numbers = new ArrayList<>();
+  private static final List<LottoNumber> numbers = new ArrayList<>();
 
-    static {
-        for (int i = Lotto.MIN_LOTTO_NUMBER; i <= Lotto.MAX_LOTTO_NUMBER; i++) {
-            numbers.add(new LottoNumber(i));
-        }
+  static {
+    for (int i = Lotto.MIN_LOTTO_NUMBER; i <= Lotto.MAX_LOTTO_NUMBER; i++) {
+      numbers.add(new LottoNumber(i));
     }
+  }
 
-    @Override
-    public Lotto generate() {
-      List<LottoNumber> numbersCopied = new ArrayList<>(numbers);
-      Collections.shuffle(numbersCopied);
-      List<LottoNumber> sublist = numbersCopied.subList(0, Lotto.SIZE);
-      Collections.sort(sublist);
-      return new Lotto(sublist);
-    }
+  @Override
+  public Lotto generate() {
+    List<LottoNumber> numbersCopied = new ArrayList<>(numbers);
+    Collections.shuffle(numbersCopied);
+    List<LottoNumber> sublist = numbersCopied.subList(0, Lotto.SIZE);
+    Collections.sort(sublist);
+    return new Lotto(sublist);
+  }
 }
