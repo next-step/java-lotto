61d11cac3e0f3e213c0dcdee6f55d47cdc91cba7
JiEung2 <181757@jnu.ac.kr>
2025-03-25 22:19:17 +0900

    test 로또 번호와 당첨 번호를 비교하여 일치 개수를 반환하는지 테스트하는 코드 추가

---
+++ src/test/java/domain/LottoTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class LottoTest {
+    @Test
+    @DisplayName("로또 번호와 당첨 번호의 일치 개수를 반환해야한다.")
+    void 로또_번호_당첨_번호_일치_개수() {
+        WinningNumber winningNumber = new WinningNumber("1, 2, 3, 4, 5, 6");
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        Assertions.assertThat(lotto.matchCount(winningNumber)).isEqualTo(6);
+    }
+}
