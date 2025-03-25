a9d571c0510e2479cecafc5087aa9c37fde52987
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-03-25 23:39:54 +0900

    [로또] step2: LottoNumber

---
+++ src/main/java/step2/lotto/exception/LottoNumberException.java
@@ -0,0 +1,9 @@
+package step2.lotto.exception;
+
+public class LottoNumberException extends RuntimeException {
+
+    public LottoNumberException() {
+        super();
+    }
+
+}
--- src/main/java/step2/lotto/model/LottoNumber.java
+++ src/main/java/step2/lotto/model/LottoNumber.java
@@ -1,5 +1,40 @@
 package step2.lotto.model;
 
+import step2.lotto.exception.LottoNumberException;
+
 public class LottoNumber {
+
+    private final int value;
+    private final static int MIN_VALUE = 1;
+    private final static int MAX_VALUE = 45;
+
+    public LottoNumber(String value) {
+        validate(value);
+        this.value = Integer.parseInt(value);
+    }
+
+    public LottoNumber(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new LottoNumberException();
+        }
+    }
+
+    private void validate(String value) {
+        try {
+            Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new LottoNumberException();
+        }
+        validate(Integer.parseInt(value));
+    }
+
+    public int value() {
+        return value;
+    }
 
 }
