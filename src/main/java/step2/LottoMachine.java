fadb2a3632ecdc5a9b21a1669d506a4aa5a73286
jongheon1 <heon0128@gmail.com>
2025-03-26 00:52:14 +0900

    feat(lottoMachine - getLottoResult)

--- src/main/java/step2/LottoMachine.java
+++ src/main/java/step2/LottoMachine.java
@@ -2,6 +2,7 @@ package step2;
 
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,5 +19,12 @@ public class LottoMachine {
         return IntStream.range(0, numberOfLottos)
                 .mapToObj(i -> lottoGenerator.generate())
                 .collect(Collectors.toList());
+    }
+
+    public Map<LottoResult, Integer> getLottoResults(List<Lotto> lottos, Lotto winningLotto) {
+        return lottos.stream()
+                .map(lotto -> LottoResult.getResult(lotto, winningLotto))
+                .collect(Collectors.groupingBy(lottoResult ->
+                        lottoResult, Collectors.summingInt(e -> 1)));
     }
 }
