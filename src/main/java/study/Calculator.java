e2c92ba6a313f606790a3d5bc98da4058ab41588
baki <hgs6424@naver.com>
2025-03-26 00:16:28 +0900

    refactor: refactor Calculator class

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,51 +1,59 @@
 package study;
 
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public class Calculator {
-
-    private static final Operation ADDITION = Integer::sum;
-    private static final Operation SUBTRACTION = (a, b) -> a - b;
-    private static final Operation MULTIPLICATION = (a, b) -> a * b;
-    private static final Operation DIVISION = (a, b) -> a / b;
-
     public static int calculate(String expression) {
-        if (expression == null || expression.isBlank()) {
-            throw new IllegalArgumentException("Invalid expression");
-        }
+        validate(expression);
 
-        var splitExpression = expression.split(" ");
+        var splitExpression = parseExpression(expression);
 
-        int result = 0;
-        var operator = ADDITION;
+        return calculate(splitExpression);
+    }
 
-        for (int i = 0; i < splitExpression.length; i++) {
-            if (i % 2 == 0) {
-                result = operator.apply(result, Integer.parseInt(splitExpression[i]));
-            } else {
-                switch (splitExpression[i]) {
-                    case "+":
-                        operator = ADDITION;
-                        break;
-                    case "-":
-                        operator = SUBTRACTION;
-                        break;
-                    case "*":
-                        operator = MULTIPLICATION;
-                        break;
-                    case "/":
-                        operator = DIVISION;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Invalid expression");
-                }
-            }
+    private static int calculate(String[] splitExpression) {
+        int result = Integer.parseInt(splitExpression[0]);
+
+        for (int i = 1; i < splitExpression.length; i += 2) {
+            var operator = Operation.getOperationFunctionBySymbol(splitExpression[i]);
+            int operand = Integer.parseInt(splitExpression[i + 1]);
+            result = operator.apply(result, operand);
         }
 
         return result;
     }
 
-    interface Operation extends BiFunction<Integer, Integer, Integer> {
-        Integer apply(Integer a, Integer b);
+    private static String[] parseExpression(String expression) {
+        return expression.split(" ");
+    }
+
+    private static void validate(String expression) {
+        if (expression == null || expression.isBlank()) {
+            throw new IllegalArgumentException("Invalid expression");
+        }
+    }
+
+    enum Operation {
+        ADDITION("+", Integer::sum),
+        SUBTRACTION("-", (a, b) -> a - b),
+        MULTIPLICATION("*", (a, b) -> a * b),
+        DIVISION("/", (a, b) -> a / b);
+
+        private final String symbol;
+        private final BiFunction<Integer, Integer, Integer> operationFunction;
+
+        Operation(String symbol, BiFunction<Integer, Integer, Integer> operationFunction) {
+            this.symbol = symbol;
+            this.operationFunction = operationFunction;
+        }
+
+        static BiFunction<Integer, Integer, Integer> getOperationFunctionBySymbol(String symbol) {
+            return Arrays.stream(values())
+                    .filter(operation -> operation.symbol.equals(symbol))
+                    .findFirst()
+                    .map(operation -> operation.operationFunction)
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid expression"));
+        }
     }
 }
