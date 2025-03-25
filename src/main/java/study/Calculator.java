69bdeb53ef02655b4ae21966777daf2608fd237d
baki <hgs6424@naver.com>
2025-03-25 23:32:21 +0900

    feat: enhance calculate method to support subtraction

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,10 +1,31 @@
 package study;
 
+import java.util.function.BiFunction;
+
 public class Calculator {
 
-    public static int calculate(String expression) {
-        String[] values = expression.replace(" ", "").split("\\+");
+    private static final Operation ADDITION = Integer::sum;
+    private static final Operation SUBTRACTION = (a, b) -> a - b;
 
-        return Integer.parseInt(values[0]) + Integer.parseInt(values[1]);
+    public static int calculate(String expression) {
+        var replacedExpression = expression.replace(" ", "");
+
+        if (replacedExpression.contains("+")) {
+            String[] parts = replacedExpression.split("\\+");
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[1]);
+            return ADDITION.apply(a, b);
+        } else if (replacedExpression.contains("-")) {
+            String[] parts = replacedExpression.split("-");
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[1]);
+            return SUBTRACTION.apply(a, b);
+        }
+
+        throw new IllegalArgumentException("Invalid expression: " + expression);
+    }
+
+    interface Operation extends BiFunction<Integer, Integer, Integer> {
+        Integer apply(Integer a, Integer b);
     }
 }
