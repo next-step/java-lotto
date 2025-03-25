1fd88cbba60c7256800a4220002f8e2ddd708a82
baki <hgs6424@naver.com>
2025-03-26 00:58:57 +0900

    feat: add Seller class with sell method to generate Lotto tickets

---
+++ src/main/java/lotto/Seller.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.List;
+
+public class Seller {
+    public static List<Lotto> sell(int money) {
+        return List.of(
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto(),
+                new Lotto()
+        );
+    }
+}
