a5bc74c5f35779e5f069b1a568edc8e60d66d4ca
baki <hgs6424@naver.com>
2025-03-26 01:06:02 +0900

    feat: implement sell method

--- src/main/java/lotto/Seller.java
+++ src/main/java/lotto/Seller.java
@@ -1,24 +1,13 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Seller {
     public static List<Lotto> sell(int money) {
-        return List.of(
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto(),
-                new Lotto()
-        );
+        return IntStream.range(0, money / Lotto.PRICE)
+                        .mapToObj(i -> new Lotto())
+                        .collect(Collectors.toList());
     }
 }
