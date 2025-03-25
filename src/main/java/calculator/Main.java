191d62dc1375439802286366b183849244c210a7
liter.1(김성수)/kakao <liter.1@kakaocorp.com>
2025-03-25 10:39:03 +0900

    feat: Add output view class

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,14 +1,16 @@
 package calculator;
 
 import calculator.ui.InputView;
+import calculator.ui.OutputView;
 
 public class Main {
 
     public static void main(String[] args) {
         String input = InputView.get();
+
         StringCalculator calculator = new StringCalculator();
         int result = calculator.calculate(input);
 
-        System.out.println("결과는 " + result + " 입니다.");
+        OutputView.printResult(result);
     }
 }
