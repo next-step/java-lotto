c454678768b3561ebf58ae547564a7c3ff04dae6
JiEung2 <181757@jnu.ac.kr>
2025-03-25 21:34:58 +0900

    feat 당첨 번호를 입력받는 기능 추가 및 메시지 추가

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
 
     public String inputExpression() {
         return scanner.nextLine();
     }
 
     public static String inputPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_INPUT_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 }
