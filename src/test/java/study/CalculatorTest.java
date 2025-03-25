34ebf1cd2bd4bce9c12eaf4b7bfb3e4ee546f2da
baki <hgs6424@naver.com>
2025-03-25 23:06:46 +0900

    test: add add method test case

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -2,17 +2,28 @@ package study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class CalculatorTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("addTestData")
     @DisplayName("덧셈 테스트")
-    void addTest() {
-        var expression = "2 + 3";
-
+    void addTest(String expression, int expected) {
         var result = Calculator.calculate(expression);
 
-        Assertions.assertThat(result).isEqualTo(5);
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> addTestData() {
+        return Stream.of(
+                Arguments.of("2 + 3", 5),
+                Arguments.of("2 + 4", 6),
+                Arguments.of("2 + 5", 7)
+        );
     }
 }
