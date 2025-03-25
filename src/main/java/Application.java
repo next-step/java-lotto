184337dd1f33c4c94eb72fa624f7b73887091220
JiEung2 <181757@jnu.ac.kr>
2025-03-25 23:17:10 +0900

    feat application 추가

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import domain.LottoGame;
+
+public class Application {
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.run();
+    }
+}
