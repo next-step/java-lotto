b00d927b631a313cd63c4d1e15ac000516e6265e
yang Jiyoung <0112jyoung@naver.com>
2025-03-25 23:18:55 +0900

    [refactor] 로또 숫자 변수 수정

--- src/main/java/model/lotto/LottoNumber.java
+++ src/main/java/model/lotto/LottoNumber.java
@@ -6,15 +6,11 @@ public class LottoNumber {
     private static final int MAX_NUMBER = 45;
     private static final int MIN_NUMBER = 1;
     private static final String INVALID_NUMBER = "로또의 숫자는 1~45 사이의 숫자여야 한다.";
-    private int number;
+    private final int number;
 
     public LottoNumber(int number) {
         checkValidLottoNumber(number);
         this.number = number;
-    }
-
-    public LottoNumber(String value) {
-        this(Integer.parseInt(value));
     }
 
     private void checkValidLottoNumber(int number) {
