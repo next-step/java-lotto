77efdcd46e56f3bea27fe6937fd17d2b3e82615b
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-25 23:44:48 +0900

    1~45의 숫자 중 중복되지 않는 6개의 숫자를 랜덤하게 뽑는다.

---
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.util.stream.Collectors.toList;
+import static lotto.domain.Lotto.LOTTO_NUMBER_END;
+import static lotto.domain.Lotto.LOTTO_NUMBER_START;
+
+public class LottoFactory {
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public static List<Lotto> createLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+
+        return lottos;
+    }
+
+    private static List<Integer> generateLottoNumbers() {
+        return new Random().ints(LOTTO_NUMBER_START, LOTTO_NUMBER_END + 1)
+            .distinct()
+            .limit(LOTTO_NUMBER_COUNT)
+            .boxed()
+            .sorted()
+            .collect(toList());
+    }
+}
