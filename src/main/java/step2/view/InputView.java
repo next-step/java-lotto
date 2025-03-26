648075a7500332aba4703afb02a89253bbd01efa
yeojin yu <yeojin@ninewatt.com>
2025-03-26 19:55:43 +0900

    fix: view 구현

---
+++ src/main/java/step2/view/InputView.java
@@ -0,0 +1,44 @@
+package step2.view;
+
+import step2.domain.LottoNum;
+import step2.domain.Winning;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String SEPARATOR = ",";
+
+    private InputView() {}
+
+    public static int buyLottosMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String line = scanner.nextLine().trim();
+        int money = Integer.parseInt(line);
+        if (money < 0) {
+            throw new IllegalArgumentException("구매 금액은 음수 일 수 없습니다.");
+        }
+        return money;
+    }
+
+    public static Winning winningNumsLastWeek() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        Set<LottoNum> winningNums = getWinningNums();
+        return new Winning(winningNums);
+    }
+
+    private static Set<LottoNum> getWinningNums() {
+        String[] tokens = scanner.nextLine().split(",");
+        return Arrays.stream(tokens)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .map(LottoNum::new)
+                .collect(Collectors.toSet());
+    }
+
+
+
+}
