3158a9cccc7c3f7f5962528dd0ae96f77a3bd778
YuGyeong <lyg64522@naver.com>
2025-03-27 00:26:50 +0900

    feat: 랜덤 로또 번호 생성
    -  랜덤 로또 번호 생성

---
+++ src/main/java/lotto/GenerateNumber.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GenerateNumber {
+    private final static int BOUND = 45;
+    private final List<Integer> numbers;
+
+    public GenerateNumber() {
+        numbers = new ArrayList<>();
+
+        for (int i = 1; i < BOUND; i++) {
+            numbers.add(i);
+        }
+    }
+
+    public List<Integer> generateRandomNumbers() {
+        Collections.shuffle(numbers);
+        return numbers.subList(0,6);
+    }
+
+    public List<Integer> generateNumbers(List<Integer> numbers) {
+        return numbers.subList(0,6);
+    }
+}
--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -2,15 +2,20 @@ package lotto;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LottoTicket {
 
     private final int count;
-
-    private List<LottoRow> lottoRowList = new ArrayList<>();
+    private final List<LottoRow> lottoRowList = new ArrayList<>();
 
     public LottoTicket(int count) {
         this.count = count;
+
+        for (int i = 0; i < count; i++) {
+            GenerateNumber generateNumber = new GenerateNumber();
+            this.lottoRowList.add(new LottoRow(generateNumber.generateRandomNumbers()));
+        }
     }
 
     public int getCount() {
