3cf179579bb89928e5d1eb00f9442a9daeddf9d1
yang Jiyoung <0112jyoung@naver.com>
2025-03-26 23:04:33 +0900

    [refactor] RandomNumberGenerator 수정

--- src/main/java/generator/RandomNumberGenerator.java
+++ src/main/java/generator/RandomNumberGenerator.java
@@ -1,22 +1,24 @@
 package generator;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
     private final List<Integer> numbers;
 
     public RandomNumberGenerator() {
-        numbers = new ArrayList<>();
-        for (int index = 1; index <= 45; index++) {
-            numbers.add(index);
-        }
+        numbers = Stream.iterate(START_NUMBER, i -> i + 1)
+                .limit(END_NUMBER)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Integer> generate() {
         Collections.shuffle(numbers);
-        return numbers.subList(0 ,6);
+        return numbers.subList(0, 6);
     }
 }
