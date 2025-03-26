32f778b115f743f420547bb52bd6fb68cb5b9843
yang Jiyoung <0112jyoung@naver.com>
2025-03-26 21:55:54 +0900

    [refactor] 엣지값 테스트로 수정

--- src/test/java/model/LottoNumberTest.java
+++ src/test/java/model/LottoNumberTest.java
@@ -2,23 +2,26 @@ package model;
 
 import model.lotto.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoNumberTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
     @DisplayName("1~45 사이의 숫자면 로또 번호가 생성된다.")
-    public void validLottoNumber(){
-        assertThat(new LottoNumber(2)).isEqualTo(new LottoNumber(2));
+    public void validLottoNumber(int number){
+        assertThat(new LottoNumber(number)).isEqualTo(new LottoNumber(number));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {0 ,46})
     @DisplayName("1~45 사이의 숫자가 아니면 예외가 발생한다.")
-    public void invalidLottoNumber(){
-        assertThatThrownBy(() -> new LottoNumber(50))
+    public void invalidLottoNumber(int number){
+        assertThatThrownBy(() -> new LottoNumber(number))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
