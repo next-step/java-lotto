5cc31a141f9b5a03f7b649de550e12d560bd25a9
parkSeryu <pjh971212@gmail.com>
2025-03-26 17:21:51 +0900

    feat: 지난 주 당첨 번호를 입력 받는다.

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,7 +1,11 @@
 package lotto;
 
+
+import lotto.view.InputView;
+
 public class LottoMain {
     public static void main(String[] args) {
-        
+        int money = InputView.inputMoney();
+        String winLotto = InputView.inputWinLotto();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
 
-    public static int Input() {
+    public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return scanner.nextInt();
     }
+
+    public static String inputWinLotto() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return scanner.next();
+    }
+
+
 }
