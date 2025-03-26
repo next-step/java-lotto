3eb6c65ce7de6fc9186e23c57e201d1d06a1e926
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-27 00:36:50 +0900

    출력을 구현한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,8 +3,13 @@ package lotto.view;
 import java.util.List;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoWinningStatistics;
 
 public class OutputView {
+    public static void printLottoCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
     public static void printLottos(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println("[" + printLotto(lotto) + "]");
@@ -17,5 +22,14 @@ public class OutputView {
             .sorted()
             .toArray(String[]::new)
         );
+    }
+
+    public static void printWinningStatistics(int money, LottoWinningStatistics statistics) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        statistics.generateFormattedReport()
+            .forEach(System.out::println);
+
+        System.out.println("총 수익률은" + statistics.getProfitRate(money) + "입니다.");
     }
 }
