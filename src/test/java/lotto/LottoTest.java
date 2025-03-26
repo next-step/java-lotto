aa6fc3c1e4f9db19cc1a90840276f53cae98260e
baki <hgs6424@naver.com>
2025-03-26 22:48:52 +0900

    test: add validation for Lotto creation with fewer than six numbers

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -15,4 +15,16 @@ public class LottoTest {
 
         Assertions.assertThat(lotto).isInstanceOf(Lotto.class);
     }
+
+    @Test
+    @DisplayName("로또는 6자리 보다 적거나 많은 숫자를 받을 수 없다.")
+    void lottoCreateNumbersSizeTest2() {
+        Assertions.assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또는 6자리여야 합니다.");
+
+        Assertions.assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또는 6자리여야 합니다.");
+    }
 }
