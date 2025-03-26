cf32b3b77b5d83c1dbd1e80d63e9e93f6cab24bb
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-27 00:53:58 +0900

    [BugFix] 불필요한 데이터 제거하도록 로직 수정

--- src/main/java/lotto/domain/LottoWinningStatistics.java
+++ src/main/java/lotto/domain/LottoWinningStatistics.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import static java.lang.Double.parseDouble;
@@ -18,8 +19,13 @@ public class LottoWinningStatistics {
 
     public List<String> generateFormattedReport() {
        return statistics.entrySet().stream()
+            .filter(this::hasWinningPrize)
             .map(entry -> formatEntry(entry.getKey(), entry.getValue()))
             .collect(Collectors.toList());
+    }
+
+    private boolean hasWinningPrize(Entry<PrizeLevel, Integer> entry) {
+        return entry.getKey().getPrice() > 0;
     }
 
     public double getProfitRate(int money) {
