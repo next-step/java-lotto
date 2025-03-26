a1a63299b336329f0c3a870d6dc41cb2828fbe36
selene-p <hwp0223@gmail.com>
2025-03-26 19:30:55 +0900

    reafactor: 상수 변수 포맷 변경

--- src/main/java/lotto/RandomLottoGenerator.java
+++ src/main/java/lotto/RandomLottoGenerator.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class RandomLottoGenerator implements LottoGenerator {
 
-  private static final List<LottoNumber> numbers = new ArrayList<>();
+  private static final List<LottoNumber> NUMBERS = new ArrayList<>();
 
   static {
     for (int i = LottoNumber.MIN_LOTTO_NUMBER; i <= LottoNumber.MAX_LOTTO_NUMBER; i++) {
-      numbers.add(new LottoNumber(i));
+      NUMBERS.add(new LottoNumber(i));
     }
   }
 
   @Override
   public Lotto generate() {
-    List<LottoNumber> numbersCopied = new ArrayList<>(numbers);
+    List<LottoNumber> numbersCopied = new ArrayList<>(NUMBERS);
     Collections.shuffle(numbersCopied);
     List<LottoNumber> sublist = numbersCopied.subList(0, Lotto.SIZE);
     Collections.sort(sublist);
