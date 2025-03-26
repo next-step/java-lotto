8b9b46a479f6ce620e51845c2eaa5c3728dc8eeb
JiEung2 <181757@jnu.ac.kr>
2025-03-26 23:38:45 +0900

    refactor LottoGame에 부가되었던 많은 책임들을 분산

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,31 +1,25 @@
 package domain;
 
-import utils.InputView;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static utils.OutputView.*;
 
 public class LottoGame {
+    private final LottoNumberGenerator generator;
 
-    public void run() {
-        String amountInput = InputView.inputPurchaseAmount();
+    public LottoGame(LottoNumberGenerator generator) {
+        this.generator = generator;
+    }
+
+    public void run(String amountInput, String winningNumberInput) {
         PurchaseAmount purchaseAmount = new PurchaseAmount(Integer.parseInt(amountInput));
+        WinningNumber winningNumber = new WinningNumber(winningNumberInput);
+
         int lottoCount = purchaseAmount.countOfLotto();
 
-        LottoNumberGenerator generator = new LottoNumberGenerator();
-        List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < lottoCount; i++) {
-            List<Integer> numbers = generator.generate();
-            Lotto lotto = LottoMachine.create(numbers);
-            lottos.add(lotto);
-        }
+        Lottos lottos = Lottos.generate(lottoCount, generator);
+        printPurchasedLottos(lottos.getValue());
 
-        printPurchasedLottos(lottos);
-
-        WinningNumber winningNumber = new WinningNumber(InputView.inputWinningNumbers());
-        LottoStatistics statistics = new LottoStatistics(lottos, winningNumber);
+        Ranks ranks = new Ranks(lottos.match(winningNumber));
+        LottoStatistics statistics = ranks.toStatistics();
 
         printStatistics(statistics);
         printProfitRate(statistics.calculateProfitRate(Integer.parseInt(amountInput)));
