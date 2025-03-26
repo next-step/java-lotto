b303d3e7024a383ef597bb908cb150dba6146db0
more.potens <more.potens@kakaocorp.com>
2025-03-26 14:44:54 +0900

    Feat: 로또 발급 시 예외 처리 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -4,10 +4,17 @@ public class LottoGame {
     private final int lottoCount;
 
     public LottoGame(int purchaseAmount) {
+        if (isValid(purchaseAmount)) {
+            throw new IllegalArgumentException("로또 구입 금액은 0원 이상이어야 하며 1000원 단위로 가능합니다.");
+        }
         this.lottoCount = purchaseAmount / 1000;
     }
 
     public int getLottoCount() {
         return lottoCount;
+    }
+
+    private boolean isValid(int purchaseAmount) {
+        return purchaseAmount <= 0 || purchaseAmount % 1000 != 0;
     }
 }
--- src/test/java/domain/LottoGameTest.java
+++ src/test/java/domain/LottoGameTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,5 +17,15 @@ public class LottoGameTest {
 
         // then
         Assertions.assertThat(lottoGame.getLottoCount()).isEqualTo(expected);
+    }
+
+    @DisplayName("로또 구입 금액이 1000원 단위가 아니거나 0원 이하인 경우 IllegalArgumentException throw")
+    @CsvSource(value = {"0", "500", "1500", "999"})
+    @ParameterizedTest
+    void createLottoException(int purchaseAmount) {
+        // when
+        Assertions.assertThatThrownBy(() -> new LottoGame(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 구입 금액은 0원 이상이어야 하며 1000원 단위로 가능합니다.");
     }
 }
