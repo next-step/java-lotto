18d277b886113c2897deccb823509c803c5b207d
baki <hgs6424@naver.com>
2025-03-26 22:47:50 +0900

    feat: add constructor to Lotto class for initializing with a list of numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,14 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lotto {
     public static final int PRICE = 1000;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = new ArrayList<>(numbers);
+    }
 }
