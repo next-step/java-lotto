1ac326a3830749fe53addf9b9e4038f1d11c8887
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-27 00:54:09 +0900

    출력 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -5,6 +5,8 @@ import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoWinningStatistics;
 
+import static java.util.Collections.reverse;
+
 public class OutputView {
     public static void printLottoCount(int lottoCount) {
         System.out.println(lottoCount + "개를 구매했습니다.");
@@ -26,9 +28,11 @@ public class OutputView {
     public static void printWinningStatistics(int money, LottoWinningStatistics statistics) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        statistics.generateFormattedReport()
-            .forEach(System.out::println);
 
-        System.out.println("총 수익률은" + statistics.getProfitRate(money) + "입니다.");
+        List<String> report = statistics.generateFormattedReport();
+        reverse(report);
+        report.forEach(System.out::println);
+
+        System.out.println("총 수익률은 " + statistics.getProfitRate(money) + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
     }
 }
