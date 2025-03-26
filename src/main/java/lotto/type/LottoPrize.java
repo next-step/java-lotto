18fa9c9e566018f32413a94a0d679bc72993e2fa
gan-ta <gilmat@naver.com>
2025-03-26 23:50:15 +0900

    fix: 2등 상금 수정

--- src/main/java/lotto/type/LottoPrize.java
+++ src/main/java/lotto/type/LottoPrize.java
@@ -11,7 +11,7 @@ public enum LottoPrize {
   THREE_MATCHES(3, 5_000),
   FOUR_MATCHES(4, 50_000),
   FIVE_MATCHES(5, 1_500_000),
-  FIVE_WITH_BONUS_MATCHES(5, 1_500_000),
+  FIVE_WITH_BONUS_MATCHES(5, 30_000_000),
   SIX_MATCHES(6, 2_000_000_000);
 
   private final int matchCount;
