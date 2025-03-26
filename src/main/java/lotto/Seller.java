731ff20e9723e609622af10a66eaf28d6980d611
baki <hgs6424@naver.com>
2025-03-26 22:42:07 +0900

    refactor: update sell method to issue Lotto tickets using LottoTicketMachine

--- src/main/java/lotto/Seller.java
+++ src/main/java/lotto/Seller.java
@@ -1,13 +1,16 @@
 package lotto;
 
 import java.util.List;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Seller {
     public static List<Lotto> sell(int money) {
-        return IntStream.range(0, money / Lotto.PRICE)
-                        .mapToObj(i -> new Lotto())
-                        .collect(Collectors.toList());
+        int lottoCount = money/Lotto.PRICE;
+
+        return IntStream.range(0, lottoCount)
+                .mapToObj(it -> LottoTicketMachine.issue())
+                .collect(Collectors.toUnmodifiableList());
     }
 }
