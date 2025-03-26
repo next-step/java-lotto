e820e5b4a2969cb0aad5bac0fe8188ca8f547045
yeojin yu <yeojin@ninewatt.com>
2025-03-26 14:06:32 +0900

    feat: 로또번호 일치 개수 분리

---
+++ src/main/java/step2/domain/MatchResult.java
@@ -0,0 +1,12 @@
+package step2.domain;
+
+public class MatchResult {
+    private final int matchedCount;
+
+    public MatchResult(int matchedCount) {
+        this.matchedCount = matchedCount;
+    }
+    public int getMatchedCount() {
+        return matchedCount;
+    }
+}
