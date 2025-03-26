612b63dd63eacc584db95b3f5417ff8dacdeb896
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2025-03-26 19:32:47 +0900

    Step1 (#4024)
    * chore: step1 요구사항 정리
    
    * chore: Todo 정리
    
    * feat: 덧셈 기능
    
    * feat: 뺄셈 기능
    
    * feat: 곱셈, 나눗셈 기능
    
    * feat: null, blank를 입력받는 경우 에러를 던진다
    
    * feat: 문자열 분리 기능
    
    * test: 사칙연산 수행 기능 테스트
    
    * feat: 사칙연을 수행할 수 있는 입력값인지 검증
    
    * feat: 사칙연산을 수행
    
    * refactor: 일부 메서드를 10줄 이하로

---
+++ src/main/java/stringcalculator/Calculator.java
@@ -0,0 +1,83 @@
+package stringcalculator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+
+public class Calculator {
+
+    public static final String DELIMITER = " ";
+    public static final String ADD = "+";
+    public static final String SUBTRACT = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+
+    public void init() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("사칙연산을 수행하기 위한 문자열을 입력해주세요: ");
+        final String input = scanner.nextLine();
+        final List<String> subjects = splitWithSpace(input);
+        System.out.println(process(subjects));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = Map.of(
+            ADD, Calculator::add,
+            SUBTRACT, Calculator::subtract,
+            MULTIPLY, Calculator::multiply,
+            DIVIDE, Calculator::divide
+    );
+
+    public List<String> splitWithSpace(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("유효하지 않은 문자열입니다: " + input);
+        }
+
+        return List.of(input.split(DELIMITER));
+    }
+
+    public int process(List<String> input) {
+        int result = this.parseOrThrow(input.get(0));
+
+        for (int i = 1; i < input.size(); i = i + 2) {
+            int number = this.parseOrThrow(input.get(i + 1));
+            final String operator = input.get(i);
+            result = this.operate(operator, result, number);
+        }
+
+        return result;
+    }
+
+    private int operate(String operator, int a, int b) {
+        BiFunction<Integer, Integer, Integer> operation = OPERATIONS.get(operator);
+        if (operation == null) {
+            throw new IllegalArgumentException("사칙연산을 수행할 수 없는 문자열을 포함합니다: " + operator);
+        }
+        return operation.apply(a, b);
+    }
+
+    private int parseOrThrow(String element) {
+        try {
+            return Integer.parseInt(element);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("사칙연산을 수행할 수 없는 문자열을 포함합니다: " + element);
+        }
+    }
+}
---
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -0,0 +1,141 @@
+package stringcalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    @DisplayName("두 숫자를 더한다.")
+    void addTest() {
+        //given
+        int a = 1;
+        int b = 2;
+        final Calculator calculator = new Calculator();
+
+        //when
+        int result = calculator.add(a, b);
+
+        //then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("두 숫자를 뺀다.")
+    void subtractTest() {
+        //given
+        int a = 1;
+        int b = 2;
+        final Calculator calculator = new Calculator();
+
+        //when
+        int result = calculator.subtract(a, b);
+
+        //then
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("두 숫자를 곱한다.")
+    void multiplyTest() {
+        //given
+        int a = 3;
+        int b = 5;
+        final Calculator calculator = new Calculator();
+
+        //when
+        int result = calculator.multiply(a, b);
+
+        //then
+        assertThat(result).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("두 숫자를 나눈다.")
+    void divideTest() {
+        //given
+        int a = 10;
+        int b = 5;
+        final Calculator calculator = new Calculator();
+
+        //when
+        int result = calculator.divide(a, b);
+
+        //then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @DisplayName("null 이거나 blank 인 문자열을 받는 경우 에러를 던진다.")
+    @MethodSource("provideNullOrBlankStrings")
+    void nullOrBlankThenThrow(String input) {
+        //given
+        final Calculator calculator = new Calculator();
+
+        //when & then
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> calculator.splitWithSpace(input))
+                .withMessageContaining("유효하지 않은 문자열입니다");
+    }
+
+    static Stream<String> provideNullOrBlankStrings() {
+        return Stream.of(null, "", " ");
+    }
+
+    @Test
+    @DisplayName("주어진 문자열을 공백 문자열로 나눠진 문자열 리스트를 반환한다.")
+    void splitWithSpaceTest() {
+        //given
+        final String input = "2 + 3 * 4 / 2";
+        final Calculator calculator = new Calculator();
+
+        //when
+        final List<String> result = calculator.splitWithSpace(input);
+
+        //then
+        Assertions.assertThat(result)
+                .containsExactly("2", "+", "3", "*", "4", "/", "2");
+    }
+
+    @ParameterizedTest
+    @DisplayName("문자열 리스트의 짝수 번째에 사칙연산 기호, 홀수 번째에 정수가 아닌 경우 에러를 던진다.")
+    @MethodSource("provideInvalidStringListToProcess")
+    void processTestOrThrow(List<String> invalidInput) {
+        //given
+        final Calculator calculator = new Calculator();
+
+        //when & then
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> calculator.process(invalidInput))
+                .withMessageContaining("사칙연산을 수행할 수 없는 문자열을 포함합니다");
+    }
+
+    static Stream<List<String>> provideInvalidStringListToProcess() {
+        return Stream.of(
+                List.of("2", "3", "3", "*", "4", "/", "2"),
+                List.of("2", "+", "3", "*", "4", "/", "-" )
+        );
+    }
+
+    @Test
+    @DisplayName("주어진 문자열 리스트로 사칙연산을 수행한다.")
+    void processTest() {
+        //given
+        final List<String> input = List.of("2", "+", "3", "*", "4", "/", "2");
+        final Calculator calculator = new Calculator();
+
+        //when
+        final int result = calculator.process(input);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(10);
+    }
+}
