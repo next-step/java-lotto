0f724da2a8b7c31c68568f47b8cd67c4c3cc7b47
JiEung2 <181757@jnu.ac.kr>
2025-03-26 22:59:44 +0900

    refactor 로또통계는 랭크 리스트를 주입 받아서 생성하고, 상금은 계산해서 반환하도록 수정

--- src/main/java/domain/LottoStatistics.java
+++ src/main/java/domain/LottoStatistics.java
@@ -6,18 +6,11 @@ import java.util.Map;
 
 public class LottoStatistics {
     private final Map<Rank, Integer> statistics = new EnumMap<>(Rank.class);
-    private int totalPrize = 0;
 
-    public LottoStatistics(List<Lotto> lottos, WinningNumber winningNumber) {
-        for (Lotto lotto : lottos) {
-            int matchCount = lotto.matchCount(winningNumber);
-            Rank rank = Rank.of(matchCount);
-
-            if (rank != null) {
-                statistics.put(rank, statistics.getOrDefault(rank, 0) + 1);
-                totalPrize += rank.getPrize();
-            }
-        }
+    public LottoStatistics(List<Rank> ranks) {
+        ranks.forEach(rank ->
+                statistics.put(rank, statistics.getOrDefault(rank, 0) + 1)
+        );
     }
 
     public int getCountByRank(Rank rank) {
@@ -25,6 +18,12 @@ public class LottoStatistics {
     }
 
     public double calculateProfitRate(int purchaseAmount) {
-        return (double) totalPrize / purchaseAmount;
+        return (double) getTotalPrize() / purchaseAmount;
+    }
+
+    private int getTotalPrize() {
+        return statistics.entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getPrize() * entry.getValue())
+                .sum();
     }
 }
