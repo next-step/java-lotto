87ed7d1bd0e1121e31cfd57e85ddd2d3d6466e6b
minakusi <seoyeon737@gmail.com>
2025-03-26 11:47:47 +0900

    feat: 로또 당첨 번호 생성 기능 추가

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,18 +1,25 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoGame {
     public static final int LOTTO_PRICE = 1000;
 
     private int count;
+    private Lotto winningLotto;
 
     public LottoGame(int money) {
         validateMoney(money);
         count = money / LOTTO_PRICE;
     }
 
-    private static void validateMoney(int money) {
+    private void validateMoney(int money) {
         if (money < 1000) {
-            throw new IllegalArgumentException("로또 구입 금액은 1000원을 넘어야 합니다.");
+            throw new IllegalArgumentException("로또 구입 금액은 " + LOTTO_PRICE + "원을 넘어야 합니다.");
         }
+    }
+
+    public void draw(List<Integer> numbers) {
+        winningLotto = new Lotto(numbers);
     }
 }
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -3,6 +3,8 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.*;
 
 class LottoGameTest {
@@ -11,5 +13,15 @@ class LottoGameTest {
     void available_lotto_quantity() {
         assertThat(new LottoGame(14560)).extracting("count").isEqualTo(14);
         assertThatIllegalArgumentException().isThrownBy(() -> new LottoGame(0));
+    }
+
+    @DisplayName("로또 당첨 번호 생성 테스트")
+    @Test
+    void create_winning_numbers() {
+        LottoGame lottoGame = new LottoGame(1000);
+        lottoGame.draw(Arrays.asList(6, 5, 4, 3, 2, 1));
+        assertThat(lottoGame)
+                .extracting("winningLotto")
+                .isEqualTo(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
     }
 }
