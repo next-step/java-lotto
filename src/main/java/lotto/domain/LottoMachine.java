8522fcb1eec9b54d0be2c1eec71c539a26a88181
Ko Minji <kohminji10@gmail.com>
2025-03-27 10:59:33 +0900

    refactor: 로또 금액 투입을 생성자로 변경

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class LottoMachine {
     private static final int TICKET_PRICE = 1000;
+    private final int paidAmount;
 
-    public List<LottoTicket> issue(int amount) {
-        int ticketCount = amount / TICKET_PRICE;
-        if(amount < TICKET_PRICE || ticketCount * TICKET_PRICE != amount)
+    public LottoMachine(int paidAmount) {
+        this.paidAmount = paidAmount;
+        int ticketCount = paidAmount / TICKET_PRICE;
+        if(paidAmount < TICKET_PRICE || ticketCount * TICKET_PRICE != paidAmount)
             throw new IllegalArgumentException(String.format("ticket price is %d. check your amount.", TICKET_PRICE));
+    }
+
+    public List<LottoTicket> issue() {
+        int ticketCount = paidAmount / TICKET_PRICE;
 
         List<LottoTicket> tickets = new ArrayList<>(ticketCount);
         for(int i=0; i<ticketCount; i++) {
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -5,35 +5,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoMachineTest {
-    private static final LottoMachine lottoMachine = new LottoMachine();
     private static final int TICKET_AMOUNT = 1000;
 
     @Test
     @DisplayName("로또 발행 머신은 1장의 가격을 넣으면 1장이 발급된다.")
     void buyOneLottoTicket() {
-        List<LottoTicket> tickets = lottoMachine.issue(TICKET_AMOUNT);
-        assertThat(tickets).hasSize(1);
+        LottoMachine lottoMachine = new LottoMachine(TICKET_AMOUNT);
+        assertThat(lottoMachine.issue()).hasSize(1);
     }
 
     @Test
     @DisplayName("로또 발행 머신은 구입 금액에 해당하는 로또 티켓 N장을 발급해야 한다.")
     void buyLottoTickets() {
         int ticketCount = 5;
-        List<LottoTicket> tickets = lottoMachine.issue(TICKET_AMOUNT * ticketCount);
-        assertThat(tickets).hasSize(ticketCount);
+        LottoMachine lottoMachine = new LottoMachine(TICKET_AMOUNT * ticketCount);
+        assertThat(lottoMachine.issue()).hasSize(ticketCount);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {TICKET_AMOUNT-1, TICKET_AMOUNT+1})
     @DisplayName("로또 발행 머신은 구입 금액을 장당 가격으로 나누었을 때, 정수가 아니면 에러를 반환한다.")
     void validateAmount(int amount) {
-        assertThatThrownBy(() -> lottoMachine.issue(amount))
+        assertThatThrownBy(() -> new LottoMachine(amount))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
