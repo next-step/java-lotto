02068ebad6e6312f01e99b420fa463c45b4274dc
heewonham <heewonham6@gmail.com>
2025-03-27 20:59:10 +0900

    feat : main과 input, output view 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
-import step1.util.InputView;
-import step1.util.OutputView;
-import step1.Expression;
+import step2.LotteryCount;
+import step2.Lotterys;
+import step2.WinningResults;
+import step2.shuffle.RandomShuffle;
+import step2.shuffle.Shuffle;
+import step2.util.InputView;
+import step2.util.OutputView;
 
 public class Main {
     public static void main(String[] args) {
-        String input = InputView.inputExpression();
-        Expression expression = new Expression(input);
-        OutputView.printResult(expression.calc());
+        int purchaseAmount = InputView.inputPurchaseAmount();
+
+        LotteryCount lotteryCount = new LotteryCount(purchaseAmount);
+        OutputView.printPurchaseAmount(lotteryCount.getLottoCount());
+
+        Shuffle randomShuffle = new RandomShuffle();
+        Lotterys lotterys = new Lotterys(lotteryCount.getLottoCount(), randomShuffle);
+
+        OutputView.printLotteryNumbers(lotterys);
+
+        String winningNumber = InputView.inputWinningNumber();
+        WinningResults result = lotterys.prizeWinning(winningNumber);
+
+        OutputView.printWinningStatistics(result);
+        OutputView.printTotalPrize(result.calculatePrize(), purchaseAmount);
     }
 }
---
+++ src/main/java/step2/util/OutputView.java
@@ -0,0 +1,29 @@
+package step2.util;
+
+import step2.Lotterys;
+import step2.Winning;
+import step2.WinningResults;
+
+public class OutputView {
+    public static void printPurchaseAmount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public static void printLotteryNumbers(Lotterys lotterys) {
+        for (int i = 0; i < lotterys.length(); i++) {
+            System.out.println(lotterys.getLotto(i).toString());
+        }
+        System.out.println();
+    }
+    public static void printWinningStatistics(WinningResults winningResults) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println("3개 일치 (5,000원)- " + winningResults.getWinningResult(Winning.FIFTH) + "개");
+        System.out.println("4개 일치 (50,000원)- " + winningResults.getWinningResult(Winning.FOURTH) + "개");
+        System.out.println("5개 일치 (1,500,000원)- " + winningResults.getWinningResult(Winning.THIRD) + "개");
+        System.out.println("6개 일치 (2,000,000,000원)- " + winningResults.getWinningResult(Winning.FIRST) + "개");
+    }
+    public static void printTotalPrize(int totalPrize, int total ) {
+        System.out.println("총 수익률은 " + (float) totalPrize / total + "입니다.");
+    }
+}
