bc5fc9c174fbaca52fe1873454b72e5ed368fcf8
Ko Minji <kohminji10@gmail.com>
2025-03-27 09:31:07 +0900

    feat: 로또 티켓 N개 구매

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -7,8 +7,9 @@ public class LottoMachine {
     private static final int TICKET_AMOUNT = 1000;
 
     public List<LottoTicket> issue(int amount) {
-        List<LottoTicket> tickets = new ArrayList<>();
-        if(amount == TICKET_AMOUNT) {
+        int ticketCount = amount / TICKET_AMOUNT;
+        List<LottoTicket> tickets = new ArrayList<>(ticketCount);
+        for(int i=0; i<ticketCount; i++) {
             tickets.add(new LottoTicket());
         }
         return tickets;
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -8,15 +8,22 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoMachineTest {
+    private static final LottoMachine lottoMachine = new LottoMachine();
     private static final int TICKET_AMOUNT = 1000;
 
     @Test
     @DisplayName("로또 발행 머신은 1장의 가격을 넣으면 1장이 발급된다.")
     void buyOneLottoTicket() {
-        LottoMachine lottoMachine = new LottoMachine();
         List<LottoTicket> tickets = lottoMachine.issue(TICKET_AMOUNT);
-
         assertThat(tickets).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("로또 발행 머신은 구입 금액에 해당하는 로또 티켓 N장을 발급해야 한다.")
+    void buyLottoTickets() {
+        int ticketCount = 5;
+        List<LottoTicket> tickets = lottoMachine.issue(TICKET_AMOUNT * ticketCount);
+        assertThat(tickets).hasSize(ticketCount);
     }
 
 }
