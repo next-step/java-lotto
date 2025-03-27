708a63ffd728079c7b10e9ec749b15910a8d0554
kass.fresh <kass.fresh@kakaocorp.com>
2025-03-27 20:52:56 +0900

    test: add failing test for LottoService(not implement yet)

---
+++ src/test/java/Lotto/model/LottoServiceTest.java
@@ -0,0 +1,27 @@
+package Lotto.model;
+
+import Lotto.model.NumberExtractor.FixedNumberExtractor;
+import Lotto.model.NumberExtractor.NumberExtractor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoServiceTest {
+    @Test
+    void shouldBe14Lotto() {
+        final int purchaseAmount = 14000;
+        NumberExtractor extractor = new FixedNumberExtractor(new int[]{1, 2, 3, 4, 5, 6});
+        LottoService service = new LottoService(purchaseAmount, extractor);
+        assertThat(service.lottoNum()).isEqualTo(14);
+    }
+
+    @Test
+    void countMatchNum() {
+        final int purchaseAmount = 14000;
+        NumberExtractor extractor = new FixedNumberExtractor(new int[]{1, 2, 3, 4, 5, 6});
+        LottoService service = new LottoService(purchaseAmount, extractor);
+        service.decideWinning(new int[]{1, 2, 3, 7, 8, 9});
+        assertThat(service.winningCounts()).containsExactly(new int[]{14, 0, 0, 0});
+        assertThat(service.profitRate()).isEqualTo(5);
+    }
+}
