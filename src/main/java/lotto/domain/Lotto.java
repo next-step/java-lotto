08c8f503f56a13ebf9032ff9885164f9e4e1137c
yeonjiyeon <dalnim95@naver.com>
2025-03-28 18:55:30 +0900

    refactor: lotto 생성 관련 유효성 추가 및 테스트 코드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,23 +1,36 @@
 package lotto.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
 
+  private static final int LOTTO_NUMBER_COUNT = 6;
+  private static final int MIN_NUMBER = 1;
+  private static final int MAX_NUMBER = 45;
   private List<Integer> lottoNumbers = new ArrayList<>();
 
   public Lotto(List<Integer> lottoNumbers) {
+    validateLottoNumbers(lottoNumbers);
     this.lottoNumbers = lottoNumbers;
   }
 
   public Lotto(DrawingStrategy drawingStrategy) {
+    validateLottoNumbers(drawingStrategy.draw());
     this.lottoNumbers = drawingStrategy.draw();
+  }
+
+  private void validateLottoNumbers(List<Integer> numbers) {
+    if (numbers.size() != LOTTO_NUMBER_COUNT) {
+      throw new IllegalArgumentException();
+    }
+    if (numbers.stream().anyMatch(num -> num < MIN_NUMBER || num > MAX_NUMBER)) {
+      throw new IllegalArgumentException();
+    }
   }
 
   public List<Integer> getLottoNumbers() {
     return lottoNumbers;
   }
-
-
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.WinningLotto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoTest {
@@ -17,5 +19,25 @@ class LottoTest {
 
     assertThat(winningLotto.matchingWinningNumber(lotto1)).isEqualTo(6);
     assertThat(winningLotto.matchingWinningNumber(lotto2)).isEqualTo(5);
+  }
+
+  @Test
+  @DisplayName("로또 번호 개수가 6개가 아닐 경우 예외 발생")
+  void invalidLottoSize() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)));
+
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)));
+  }
+
+  @Test
+  @DisplayName("로또 번호 범위를 벗어나면 예외 발생")
+  void invalidLottoNumberRange() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 6)));
+
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 46)));
   }
 }
