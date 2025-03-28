36f29d9f68b7f3169763d549a604fe7b2fba25ad
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-03-28 12:12:38 +0900

    [feat] LottoMachine 메서드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LottoMachine {
 
@@ -12,4 +15,17 @@ public class LottoMachine {
     return new LottoSet(lottoPurchase, lottos);
   }
 
+  public static LottoSet generateManualAndRandomLotto(LottoPurchase lottoPurchase, List<Lotto> manualLottos) {
+    int manualCount = manualLottos.size();
+    int autoCount = lottoPurchase.calculateLottoCount() - manualCount;
+
+    List<Lotto> allLottos = Stream.concat(
+            manualLottos.stream(),
+            IntStream.range(0, autoCount)
+                .mapToObj(i -> Lotto.generateRandomLotto(new RandomLottoGenerator()))
+        )
+        .collect(Collectors.toUnmodifiableList());
+
+    return new LottoSet(lottoPurchase, allLottos);
+  }
 }
