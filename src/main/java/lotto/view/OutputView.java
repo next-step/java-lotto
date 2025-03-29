e45e4f207d834155f889ee6226bd84632ba2cf22
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-30 00:51:23 +0900

    DTO Return으로 인한 출력 형식 지정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,11 +3,15 @@ package lotto.view;
 import java.util.List;
 
 import lotto.domain.Lotto;
+import lotto.dto.LottoStatisticsDTO;
 import lotto.domain.LottoWinningStatistics;
 
 import static java.util.Collections.reverse;
 
 public class OutputView {
+    private static final String RESULT_FORMAT = "%s - %d개";
+    private static final String PROFIT_RATE_FORMAT = "%.2f";
+
     public static void printLottoCount(int lottoCount) {
         System.out.println(lottoCount + "개를 구매했습니다.");
     }
@@ -29,10 +33,13 @@ public class OutputView {
         System.out.println("당첨 통계");
         System.out.println("---------");
 
-        List<String> report = statistics.generateFormattedReport();
+        List<LottoStatisticsDTO> report = statistics.getLottoStatisticsDTOs();
         reverse(report);
-        report.forEach(System.out::println);
+        report.stream()
+            .map(dto -> String.format(RESULT_FORMAT, dto.getPrizeLevel().getMessage(), dto.getCount()))
+            .forEach(System.out::println);
 
-        System.out.println("총 수익률은 " + statistics.getProfitRate(money) + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
+        String profitRate = String.format(PROFIT_RATE_FORMAT, statistics.getProfitRate(money));
+        System.out.println("총 수익률은 " + profitRate + "입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
     }
 }
