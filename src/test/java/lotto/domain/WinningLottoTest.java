53a47bd6544863a2e92a8143ed4728d821483bfc
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-30 01:09:16 +0900

    테스트 코드 수정

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,37 +1,34 @@
 package lotto.domain;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WinningLottoTest {
-    private final WinningLotto winningLotto = new WinningLotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+    static Stream<Arguments> provideLottoTestCases() {
+        return Stream.of(
+            Arguments.of(List.of(1, 2, 3, 4, 5, 6), PrizeLevel.FIRST),
+            Arguments.of(List.of(1, 2, 3, 4, 5, 7), PrizeLevel.SECOND),
+            Arguments.of(List.of(1, 2, 3, 4, 7, 8), PrizeLevel.THIRD),
+            Arguments.of(List.of(1, 2, 3, 7, 8, 9), PrizeLevel.FOURTH),
+            Arguments.of(List.of(1, 2, 7, 8, 9, 10), PrizeLevel.NO_PRIZE),
+            Arguments.of(List.of(7, 8, 9, 10, 11, 12), PrizeLevel.NO_PRIZE)
+        );
+    }
 
     @ParameterizedTest
-    @CsvSource({
-        "1|2|3|4|5|6, FIRST",
-        "1|2|3|4|5|7, SECOND",
-        "1|2|3|4|7|8, THIRD",
-        "1|2|3|7|8|9, FOURTH",
-        "1|2|7|8|9|10, NO_PRIZE",
-        "7|8|9|10|11|12, NO_PRIZE"
-    })
+    @MethodSource("provideLottoTestCases")
     @DisplayName("로또 번호 일치 개수 검증")
-    void countMatchingNumbers_다양한_케이스_검증(String numbers, PrizeLevel expectedLevel) {
-        List<Integer> numberList = Arrays.stream(numbers.split("\\|"))
-            .map(String::trim)
-            .map(Integer::parseInt)
-            .collect(toList());
+    void countMatchingNumbers_다양한_케이스_검증(List<Integer> numbers, PrizeLevel expectedLevel) {
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6));
+        PrizeLevel actualLevel = winningLotto.countMatchingNumbers(new Lotto(numbers));
 
-        Lotto lotto = new Lotto(numberList);
-        assertThat(winningLotto.countMatchingNumbers(lotto))
-            .as("로또 번호: %s", numbers)
-            .isEqualTo(expectedLevel);
+        assertThat(actualLevel).isEqualTo(expectedLevel);
     }
 }
