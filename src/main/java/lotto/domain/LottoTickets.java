3250dfd77be8272daf98ab3af76e1e1cdf649638
Jiyoung Won <jiyoung928@gmail.com>
2025-03-29 16:34:28 +0900

    refactor : 로또 가격 및 가격당 로또 개수 로직 LottoTickets으로 위임

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,11 +1,13 @@
 package lotto.domain;
 
 import lotto.strategy.AutoLottoStrategy;
+import lotto.strategy.LottoStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTickets {
+    public static final int LOTTO_PRICE = 1000;
     private final List<LottoTicket> lottoTickets;
 
     public LottoTickets(List<LottoTicket> lottoTickets) {
@@ -26,6 +28,10 @@ public class LottoTickets {
 
     public void generateAllLottoNumbers(AutoLottoStrategy autoLottoStrategy){
         lottoTickets.forEach(lottoTickets -> lottoTickets.generateLottoNumbers(autoLottoStrategy));
+    public static LottoTickets purchase(int payment, LottoStrategy lottoStrategy) {
+        int count = payment / LOTTO_PRICE;
+        return fromNumbers(count, lottoStrategy);
+    }
     }
 
 }
