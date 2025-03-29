6117901eff43dbfd471e95de1a881fc7d5f61785
JiEung2 <181757@jnu.ac.kr>
2025-03-29 22:36:01 +0900

    test 보너스 번호가 당첨번호와 중복되는지 검증하는 기능을 테스트하는 코드 추가

--- src/test/java/domain/WinningNumberTest.java
+++ src/test/java/domain/WinningNumberTest.java
@@ -11,4 +11,10 @@ class WinningNumberTest {
     void 당첨번호_중복() {
         assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumber("1, 2, 3, 4, 5, 5", "7"));
     }
+
+    @Test
+    @DisplayName("보너스 번호 입력 시 당첨번호와 중복된 숫자가 있으면 IllegalArgumentException 이 발생한다.")
+    void 보너스번호_중복() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new WinningNumber("1, 2, 3, 4, 5, 6", "6"));
+    }
 }
