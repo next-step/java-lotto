d51ec401e317cbceb8481041aba8f0748a355b05
heewonham <heewonham6@gmail.com>
2025-03-29 23:42:50 +0900

    feat : (review) 당첨 결과 1등에 다른 등수 상금 테스트 추가

--- src/main/java/step2/WinningResults.java
+++ src/main/java/step2/WinningResults.java
@@ -1,7 +1,5 @@
 package step2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class WinningResults {
--- src/test/java/step2/WinningResultsTest.java
+++ src/test/java/step2/WinningResultsTest.java
@@ -10,11 +10,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningResultsTest {
     @Test
-    @DisplayName("당첨 금액을 확인한다.")
-    void calculatePrizeTest() {
+    @DisplayName("1등 당첨 금액을 확인한다.")
+    void FirstCalculatePrizeTest() {
         Map<Winning, Integer> matchResult = new HashMap<>();
         matchResult.put(Winning.FIRST, 1);
         WinningResults winningResults = new WinningResults(matchResult);
         assertThat(winningResults.calculatePrize()).isEqualTo(2000000000);
+    }
+
+    @Test
+    @DisplayName("3등 당첨 금액을 확인한다.")
+    void ThirdCalculatePrizeTest() {
+        Map<Winning, Integer> matchResult = new HashMap<>();
+        matchResult.put(Winning.THIRD, 1);
+        WinningResults winningResults = new WinningResults(matchResult);
+        assertThat(winningResults.calculatePrize()).isEqualTo(1500000);
+    }
+
+    @Test
+    @DisplayName("4등 당첨 금액을 확인한다.")
+    void FourthCalculatePrizeTest() {
+        Map<Winning, Integer> matchResult = new HashMap<>();
+        matchResult.put(Winning.FOURTH, 1);
+        WinningResults winningResults = new WinningResults(matchResult);
+        assertThat(winningResults.calculatePrize()).isEqualTo(50000);
+    }
+
+    @Test
+    @DisplayName("5등 당첨 금액을 확인한다.")
+    void FifthCalculatePrizeTest() {
+        Map<Winning, Integer> matchResult = new HashMap<>();
+        matchResult.put(Winning.FIFTH, 1);
+        WinningResults winningResults = new WinningResults(matchResult);
+        assertThat(winningResults.calculatePrize()).isEqualTo(5000);
     }
 }
