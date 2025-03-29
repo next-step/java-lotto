65ae12ba976c2223aa8f1012d58c6fa13b0ff34f
JiEung2 <181757@jnu.ac.kr>
2025-03-29 22:20:22 +0900

    feat 보너스 번호를 입력받는 기능 추가

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
 
     public String inputExpression() {
         return scanner.nextLine();
@@ -19,5 +20,14 @@ public class InputView {
     public static String inputWinningNumbers() {
         System.out.println(WINNING_NUMBERS_INPUT_MESSAGE);
         return scanner.nextLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static int toInt(String input) {
+        return Integer.parseInt(input);
     }
 }
