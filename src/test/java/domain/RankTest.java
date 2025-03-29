f55d50bc8ce8a95e77ba87778a47654ef6c0d024
JiEung2 <181757@jnu.ac.kr>
2025-03-29 22:13:30 +0900

    test 2등 등수 변경에 따른 테스트 변경

--- src/test/java/domain/RankTest.java
+++ src/test/java/domain/RankTest.java
@@ -10,14 +10,14 @@ class RankTest {
 
     @ParameterizedTest
     @DisplayName("일치 개수에 따라 올바른 Rank를 반환한다")
-    @CsvSource({"3, FOURTH", "4, THIRD", "5, SECOND", "6, FIRST"})
-    void Rank_매치(int matchCount, Rank expectedRank) {
-        assertThat(Rank.of(matchCount)).isEqualTo(expectedRank);
+    @CsvSource({"3, false, FIFTH", "4, false, FOURTH", "5, false, THIRD", "5, true, SECOND", "6, false, FIRST"})
+    void Rank_매치(int matchCount, boolean isBonusNumberMatched,Rank expectedRank) {
+        assertThat(Rank.of(matchCount, isBonusNumberMatched)).isEqualTo(expectedRank);
     }
 
     @ParameterizedTest
     @DisplayName("Rank 별 상금을 반환한다")
-    @CsvSource({"FOURTH, 5000", "THIRD, 50000", "SECOND, 1500000", "FIRST, 2000000000"})
+    @CsvSource({"FIFTH, 5000", "FOURTH, 50000", "THIRD, 1500000", "SECOND, 30000000", "FIRST, 2000000000"})
     void 상금_반환(Rank rank, int expectedPrize) {
         assertThat(rank.getPrize()).isEqualTo(expectedPrize);
     }
