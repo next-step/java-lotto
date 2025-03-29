948f59a0edd07185ffe4d55de0966e8872c26baf
JiEung2 <181757@jnu.ac.kr>
2025-03-29 22:27:43 +0900

    feat 보너스 번호를 WinningNumber에 추가

--- src/main/java/domain/WinningNumber.java
+++ src/main/java/domain/WinningNumber.java
@@ -6,22 +6,29 @@ import java.util.stream.Collectors;
 
 public class WinningNumber {
     private final List<Integer> numbers;
+    private final int bonusNumber;
 
-    public WinningNumber(String input) {
-        List<Integer> inputNumbers = from(input);
+    public WinningNumber(String numberInput, String bonusNumberInput) {
+        List<Integer> inputNumbers = fromNumberInput(numberInput);
+        int bonusNumber = fromBonusNumberInput(bonusNumberInput);
         validate(inputNumbers);
         this.numbers = inputNumbers;
+        this.bonusNumber = bonusNumber;
     }
 
     public boolean contains(int lottoNumber) {
         return numbers.contains(lottoNumber);
     }
 
-    private List<Integer> from(String input) {
+    private List<Integer> fromNumberInput(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private int fromBonusNumberInput(String bonusNumberInput) {
+        return Integer.parseInt(bonusNumberInput);
     }
 
     private void validate(List<Integer> numbers) {
