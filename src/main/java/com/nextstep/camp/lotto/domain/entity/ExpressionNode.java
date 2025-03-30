097b1d55acdd0dbf0af26539ddce2a2843b92d58
안준헌 <frozengrape@mz.co.kr>
2025-03-30 21:41:57 +0900

    refactor: 불필요 메서드 삭제

--- src/main/java/com/nextstep/camp/lotto/domain/entity/ExpressionNode.java
+++ src/main/java/com/nextstep/camp/lotto/domain/entity/ExpressionNode.java
@@ -2,10 +2,6 @@ package com.nextstep.camp.lotto.domain.entity;
 
 public interface ExpressionNode {
 
-    static boolean isNotNumberNode(ExpressionNode node) {
-        return !(node instanceof NumberNode);
-    }
-
     int evaluate();
 
 }
