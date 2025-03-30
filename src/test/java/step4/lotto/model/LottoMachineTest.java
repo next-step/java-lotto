3009239bb81b27f3da0b4acef95b5137de79e867
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-03-30 22:28:40 +0900

    [로또] step4: LottoMachineTest

--- src/test/java/step4/lotto/model/LottoMachineTest.java
+++ src/test/java/step4/lotto/model/LottoMachineTest.java
@@ -1,11 +1,14 @@
 package step4.lotto.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.lotto.exception.LottoMachineNotEnoughMoneyException;
+import step4.lotto.exception.LottoNumberInvalidFormatException;
 
 public class LottoMachineTest {
 
@@ -17,7 +20,7 @@ public class LottoMachineTest {
     }
 
     @Test
-    @DisplayName("로또 구입 테스트")
+    @DisplayName("로또 자동 구입 테스트")
     void givenPurchaseAmount_whenBuyLotto_thenSuccess() {
         // given
         String purchaseAmount = "1234";
@@ -30,7 +33,21 @@ public class LottoMachineTest {
     }
 
     @Test
-    @DisplayName("로또 발행 테스트")
+    @DisplayName("로또 수동 구입 돈부족 테스트")
+    void givenPurchaseAmountAndManualLottoCount_whenBuyLotto_thenSuccess() {
+        // given
+        String purchaseAmount = "1234";
+        String manualLottoCount = "2";
+
+        // when
+        assertThatThrownBy(() -> lottoMachine.buyLottoTickets(new Money(purchaseAmount), new Count(manualLottoCount)))
+
+            // then
+            .isExactlyInstanceOf(LottoMachineNotEnoughMoneyException.class);
+    }
+
+    @Test
+    @DisplayName("로또 자동 발행 테스트")
     void givenLottoMachine_whenCreateLottoMachine_thenSuccess() {
         // given && when
         LottoTicket lottoTicket = lottoMachine.issueLottoTicketAuto();
