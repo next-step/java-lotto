8f028b02c89426221115a854b44d244211dcee9f
kass.fresh <kass.fresh@kakaocorp.com>
2025-03-30 11:58:10 +0900

    test: add test case for validating purchaseAmount

--- src/test/java/Lotto/model/LottoServiceTest.java
+++ src/test/java/Lotto/model/LottoServiceTest.java
@@ -2,27 +2,47 @@ package Lotto.model;
 
 import Lotto.model.NumberExtractor.FixedNumberExtractor;
 import Lotto.model.NumberExtractor.NumberExtractor;
+import Lotto.model.NumberExtractor.RandomNumberExtractor;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoServiceTest {
     @Test
     void shouldBe14Lotto() {
-        final int purchaseAmount = 14000;
+        final int PURCHASE_AMOUNT = 14000;
         NumberExtractor extractor = new FixedNumberExtractor(new int[]{1, 2, 3, 4, 5, 6});
-        LottoService service = new LottoService(purchaseAmount, extractor);
+        LottoService service = new LottoService(PURCHASE_AMOUNT, extractor);
         assertThat(service.lottoNum()).isEqualTo(14);
     }
 
     @Test
     void countMatchNum() {
-        final int purchaseAmount = 14000;
+        final int PURCHASE_AMOUNT = 14000;
         NumberExtractor extractor = new FixedNumberExtractor(new int[]{1, 2, 3, 4, 5, 6});
-        LottoService service = new LottoService(purchaseAmount, extractor);
+        LottoService service = new LottoService(PURCHASE_AMOUNT, extractor);
         service.draw();
         service.decideWinning(new int[]{1, 2, 3, 7, 8, 9});
         assertThat(service.winningCounts()).containsExactly(new int[]{14, 0, 0, 0});
         assertThat(service.profitRate()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldNotAllowPurchase_WhenPurchaseAmountIsUnder1000Won(){
+        final int PURCHASE_AMOUNT = 900;
+        NumberExtractor extractor = new FixedNumberExtractor(new int[]{1, 2, 3, 4, 5, 6});
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->new LottoService(PURCHASE_AMOUNT, extractor))
+                .withMessage("The minimum of purchase price is 1,000 won.");
+    }
+
+    @Test
+    void shouldNotAllowPurchase_WhenPurchaseAmountIsNotDivisibleBy1000(){
+        final int PURCHASE_AMOUNT = 1001;
+        NumberExtractor extractor = new FixedNumberExtractor(new int[]{1, 2, 3, 4, 5, 6});
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->new LottoService(PURCHASE_AMOUNT, extractor))
+                .withMessage("The purchase price is in units of 1,000 won.");
     }
 }
