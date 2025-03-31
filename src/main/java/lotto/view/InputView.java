53721955f9e24b9a2e6ecbab6dd25d74b2bd1dec
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-31 23:28:59 +0900

    수동으로 구매할 로또 번호를 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import lotto.vo.LottoManualTicket;
 
 import static java.lang.Integer.parseInt;
 import static lotto.util.StringUtil.split;
@@ -31,5 +34,17 @@ public class InputView {
     public int inputManualTicketCount() {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
         return parseInt(scanner.nextLine());
+    }
+
+    public List<LottoManualTicket> inputManualTicket(int count) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+
+        List<LottoManualTicket> manualTickets = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            manualTickets.add(new LottoManualTicket(split(scanner.nextLine())));
+        }
+
+        return manualTickets;
     }
 }
