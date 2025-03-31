a35bd062b821adc5a93150528fa8b67bdc3e00c6
yang Jiyoung <0112jyoung@naver.com>
2025-03-31 21:52:30 +0900

    [feat] 로또 총 개수 설정

---
+++ src/main/java/model/lotto/LottoCount.java
@@ -0,0 +1,27 @@
+package model.lotto;
+
+public class LottoCount {
+    private static final int LOTTO_PRICE = 1000;
+    private final int totalCount;
+    private final int manualCount;
+
+    public LottoCount(int payAmount, int manualCount) {
+        this.totalCount = getTotalCount(payAmount);
+        this.manualCount = manualCount;
+        checkLottoCountValidation();
+    }
+
+    private void checkLottoCountValidation() {
+        if (totalCount < manualCount) {
+            throw new IllegalArgumentException("수동으로 구매한 개수는 총 구매 개수를 초과할 수 없습니다.");
+        }
+    }
+
+    public int getAutoCount() {
+        return totalCount - manualCount;
+    }
+
+    private int getTotalCount(int payAmount) {
+        return payAmount / LOTTO_PRICE;
+    }
+}
---
+++ src/test/java/model/lotto/LottoCountTest.java
@@ -0,0 +1,23 @@
+package model.lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LottoCountTest {
+    @Test
+    @DisplayName("금액보다 더 많이 수동 로또를 발급하면 에러가 발생된다.")
+    public void checkValidLottoCounts() {
+        assertThatThrownBy(() -> new LottoCount(2000, 4))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("금액/1000한 횟수에서 수동 로또를 빼면 자동 로또 개수가 된다.")
+    void getAutoCount() {
+        LottoCount lottoCount = new LottoCount(2000, 1);
+        assertEquals(lottoCount.getAutoCount(), 1);
+    }
+}
