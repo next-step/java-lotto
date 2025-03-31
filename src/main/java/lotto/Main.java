114b831b6d93e2ca9eb290f4b4f61fcc3d947091
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-31 23:30:14 +0900

    Main 클래스를 수정한다.

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -7,18 +7,21 @@ import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
 import lotto.domain.WinningLotto;
 import lotto.service.LottoCheckerService;
-import lotto.strategy.LottoAutoStrategy;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+import lotto.vo.LottoManualTicket;
 
 public class Main {
     public static void main(String[] args) {
         InputView inputView = new InputView(new Scanner(System.in));
         int money = inputView.inputPurchaseAmount();
+        int manualTicketCount = inputView.inputManualTicketCount();
+        List<LottoManualTicket> manualTickets = inputView.inputManualTicket(manualTicketCount);
 
-        LottoMachine machine = new LottoMachine(money);
-        List<Lotto> lottos = machine.getLottos(new LottoAutoStrategy());
-        OutputView.printLottoCount(lottos.size());
+        LottoMachine machine = new LottoMachine(money, manualTickets);
+        List<Lotto> lottos = machine.buyLottos();
+
+        OutputView.printLottoCount(manualTicketCount, lottos.size());
         OutputView.printLottos(lottos);
 
         WinningLotto winningLotto = new WinningLotto(inputView.inputWinningNumbers(), inputView.inputBonusNumber());
