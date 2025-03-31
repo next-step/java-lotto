74d9b67ee892c596908f4191e6b82eaeb72e2e3a
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-31 23:28:10 +0900

    수동으로 구매할 로또 수를 입력받는다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -27,4 +27,9 @@ public class InputView {
         System.out.println("보너스 볼을 입력해 주세요.");
         return parseInt(scanner.nextLine());
     }
+
+    public int inputManualTicketCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return parseInt(scanner.nextLine());
+    }
 }
