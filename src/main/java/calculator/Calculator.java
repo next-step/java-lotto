69dd946a82ec8e8b03384a77c7b2772384e94fa9
east.bridge <east.bridge@kakaocorp.com>
2025-04-01 22:18:16 +0900

    refactor: 파라미터로 넘어가는 값 변수로 한 번 분리해서 끊어가기

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,13 +4,15 @@ import view.InputView;
 import view.ResultView;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Calculator {
     private final CalculateStrategy calculateStrategy = new InOrderCalculator();
 
     public void calculate() {
         String input = InputView.read("계산식을 입력해주세요.");
-        BigDecimal result = calculateStrategy.calculate(Tokenizer.tokenize(input));
+        List<String> tokens = Tokenizer.tokenize(input);
+        BigDecimal result = calculateStrategy.calculate(tokens);
         ResultView.printResult(result);
     }
 }
