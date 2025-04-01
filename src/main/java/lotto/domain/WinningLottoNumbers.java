83a07e4820018459eeb0070908b4cb56163d9602
victor.shin <victor.shin@victorshinui-MacBookPro.local>
2025-04-01 23:33:10 +0900

    refactor:변수명 가독성 개선

--- src/main/java/lotto/domain/WinningLottoNumbers.java
+++ src/main/java/lotto/domain/WinningLottoNumbers.java
@@ -5,16 +5,16 @@ import java.util.Set;
 
 public class WinningLottoNumbers {
 
-    private LottoNumbers lottoNumbers;
+    private LottoNumbers winningNumbers;
     private LottoNumber bonusNumber;
 
 
-    public WinningLottoNumbers(LottoNumbers lottoNumbers, LottoNumber bonusNumber) {
-        if(lottoNumbers.numbers().contains(bonusNumber.number())) {
+    public WinningLottoNumbers(LottoNumbers winningNumbers, LottoNumber bonusNumber) {
+        if(winningNumbers.numbers().contains(bonusNumber.number())) {
             throw new IllegalArgumentException("보너스 번호는 로또 당첨 번호에 포함될 수 없습니다.");
         }
 
-        this.lottoNumbers = new LottoNumbers(lottoNumbers);
+        this.winningNumbers = new LottoNumbers(winningNumbers);
         this.bonusNumber = new LottoNumber(bonusNumber);
     }
 
@@ -25,13 +25,13 @@ public class WinningLottoNumbers {
         return new WinningLottoNumbers(lottoNumber, bonusNumber);
     }
     public LottoRank lottoRank(LottoNumbers lottoNumbers) {
-        Set<LottoNumber> numberSet = new HashSet<>(lottoNumbers.numbers());
-        int match = (int) this.lottoNumbers.numbers()
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(lottoNumbers.numbers());
+        int match = (int) this.winningNumbers.numbers()
                 .stream()
-                .filter(numberSet::contains)
+                .filter(lottoNumberSet::contains)
                 .count();
-
-        boolean bonusMatch = numberSet.contains(this.bonusNumber);
+        boolean bonusMatch = lottoNumberSet.contains(this.bonusNumber);
+        
         return LottoRank.fromMatch(match, bonusMatch);
     }
 }
