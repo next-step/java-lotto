6d1e1aa1d648224114da2277195583e020377765
bo-10000 <seot100@naver.com>
2025-04-01 17:02:01 +0900

    feat: LottoNumbers 검사로직 및 테스트 추가

--- src/main/java/domain/model/LottoNumbers.java
+++ src/main/java/domain/model/LottoNumbers.java
@@ -7,9 +7,24 @@ public class LottoNumbers {
 
     public LottoNumbers(List<Integer> numbers) {
         this.numbers = numbers;
+        validate();
     }
 
     public List<Integer> getNumbers() {
         return this.numbers;
+    }
+
+    private void validate() {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("Lotto numbers must contain exactly 6 numbers.");
+        }
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException("Lotto numbers must be between 1 and 45.");
+            }
+        }
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException("Lotto numbers must not contain duplicates.");
+        }
     }
 }
--- src/test/java/domain/model/LottoNumbersTest.java
+++ src/test/java/domain/model/LottoNumbersTest.java
@@ -8,11 +8,41 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoNumbersTest {
-    @DisplayName("로또 번호를 생성한다.")ㅈ
+    @DisplayName("로또 번호를 생성한다.")
     @Test
     void createTest() {
         LottoNumbers lottoNumbers = new LottoNumbers(List.of(1, 2, 3, 4, 5, 6));
 
         assertThat(lottoNumbers.getNumbers()).containsExactlyInAnyOrder(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("중복이 있는 로또 번호를 생성할 수 없다.")
+    @Test
+    void createDuplicateTest() {
+        try {
+            new LottoNumbers(List.of(1, 2, 3, 4, 5, 5));
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("Lotto numbers must not contain duplicates.");
+        }
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 아니면 예외를 발생시킨다.")
+    @Test
+    void createSizeTest() {
+        try {
+            new LottoNumbers(List.of(1, 2, 3, 4, 5));
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("Lotto numbers must contain exactly 6 numbers.");
+        }
+    }
+
+    @DisplayName("로또 번호의 범위가 1~45가 아니면 예외를 발생시킨다.")
+    @Test
+    void createRangeTest() {
+        try {
+            new LottoNumbers(List.of(0, 2, 3, 4, 5, 6));
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("Lotto numbers must be between 1 and 45.");
+        }
     }
 }
