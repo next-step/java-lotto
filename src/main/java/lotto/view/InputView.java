6e948ce5199f03b09cff82de8a602672d68d58c9
jayden(김준기) <jayden.i@kakaocorp.com>
2025-04-01 14:23:18 +0900

    InputView 에서 수동 구매가 0일 때 빈값 반환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -37,6 +37,10 @@ public class InputView {
     }
 
     public List<LottoManualTicket> inputManualTicket(int count) {
+        if (count == 0) {
+            return new ArrayList<>();
+        }
+
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
 
         List<LottoManualTicket> manualTickets = new ArrayList<>();
