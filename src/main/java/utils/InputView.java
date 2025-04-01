e2cf7448b9c8f415f5b78472693a55f5e74cfb0c
JiEung2 <181757@jnu.ac.kr>
2025-04-01 12:28:27 +0900

    feat 수동으로 구매할 로또 수를 입력받는 기능 추가

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
+    private static final String MANUAL_LOTTO_COUNT_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
 
     public String inputExpression() {
         return scanner.nextLine();
@@ -24,6 +25,11 @@ public class InputView {
 
     public static String inputBonusNumber() {
         System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String inputManualLottoCount() {
+        System.out.println(MANUAL_LOTTO_COUNT_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 
