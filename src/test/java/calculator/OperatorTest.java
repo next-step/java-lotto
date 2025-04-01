b5506bc43dcd674bc844bb5d7728c2eb2eb9f2e7
east.bridge <east.bridge@kakaocorp.com>
2025-04-01 23:26:25 +0900

    refactor: parameterizedTest 로 변경

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -3,21 +3,30 @@ package calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 public class OperatorTest {
-    @Test
-    @DisplayName("연산자 찾기")
-    void find() {
-        Operator operator = Operator.find("+");
-        Assertions.assertThat(operator).isEqualTo(Operator.ADDITION);
-        Operator operator2 = Operator.find("-");
-        Assertions.assertThat(operator2).isEqualTo(Operator.SUBTRACTION);
-        Operator operator3 = Operator.find("*");
-        Assertions.assertThat(operator3).isEqualTo(Operator.MULTIPLICATION);
-        Operator operator4 = Operator.find("/");
-        Assertions.assertThat(operator4).isEqualTo(Operator.DIVISION);
+
+    @ParameterizedTest(name = "연산자 찾기 : {0}")
+    @MethodSource("operatorProvider")
+    @DisplayName("연산자에 맞는 오퍼레이터를 찾아온다.")
+    void find(String symbol, Operator operator) {
+        Operator findOperator = Operator.find(symbol);
+        Assertions.assertThat(findOperator).isEqualTo(operator);
+    }
+
+    static Stream<Arguments> operatorProvider() {
+        return Stream.of(
+                Arguments.arguments("+", Operator.ADDITION),
+                Arguments.arguments("-", Operator.SUBTRACTION),
+                Arguments.arguments("*", Operator.MULTIPLICATION),
+                Arguments.arguments("/", Operator.DIVISION)
+        );
     }
 
     @Test
