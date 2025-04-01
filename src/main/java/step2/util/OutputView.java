da991b7a60628c5ef239db3ee551fdc38640a27f
heewonham <heewonham6@gmail.com>
2025-04-01 17:49:26 +0900

    feat : 2등 기능 추가에 따른 main, ioview 수정

--- src/main/java/step2/util/OutputView.java
+++ src/main/java/step2/util/OutputView.java
@@ -27,6 +27,7 @@ public class OutputView {
         System.out.println("3개 일치 (5,000원)- " + winningResults.getWinningResult(Winning.FIFTH) + "개");
         System.out.println("4개 일치 (50,000원)- " + winningResults.getWinningResult(Winning.FOURTH) + "개");
         System.out.println("5개 일치 (1,500,000원)- " + winningResults.getWinningResult(Winning.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원)- " + winningResults.getWinningResult(Winning.SECOND) + "개");
         System.out.println("6개 일치 (2,000,000,000원)- " + winningResults.getWinningResult(Winning.FIRST) + "개");
     }
     public static void printTotalPrize(int totalPrize, int total ) {
