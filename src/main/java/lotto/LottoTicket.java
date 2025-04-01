8979b44529dea87c48ce39164d0b307afe12f532
Jenny.SSong <jenny.ssong@kakaocorp.com>
2025-04-01 23:20:17 +0900

    feat: lottoTicket 은, 숫자로 생성할 수 있고, 당첨번호로부터 몇개가 hit 되었는지 체크할 수 있다

--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,12 +1,44 @@
 package lotto;
 
-public class LottoTicket {
+import java.util.List;
 
+public class LottoTicket {
 
   private final LottoTicketNumberSet numberSet;
 
-  public LottoTicket(LottoTicketNumberSet numberSet) {
-    this.numberSet = numberSet;
+  public LottoTicket(int number1, int number2, int number3, int number4, int number5, int number6) {
+    this.numberSet = new LottoTicketNumberSet(
+        List.of(
+            new LottoTicketNumber(number1),
+            new LottoTicketNumber(number2),
+            new LottoTicketNumber(number3),
+            new LottoTicketNumber(number4),
+            new LottoTicketNumber(number5),
+            new LottoTicketNumber(number6)
+        )
+    );
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    LottoTicket that = (LottoTicket) o;
+    return numberSet.equals(that.numberSet);
+  }
+
+  @Override
+  public int hashCode() {
+    return numberSet.hashCode();
+  }
+
+  public int countMatchingNumbers(LottoTicketNumberSet winningNumbers) {
+    return (int) numberSet.getNumbers().stream()
+        .filter(winningNumbers.getNumbers()::contains)
+        .count();
   }
 
 }
+
--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -1,6 +1,36 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoTicketTest {
+
+  @Test
+  @DisplayName("로또 티켓은 숫자로 생성할 수 있다")
+  void createLottoTicketWithNumbers() {
+    assertThat(new LottoTicket(5, 1, 2, 3, 4, 6)).isEqualTo(new LottoTicket(1, 2, 3, 4, 5, 6));
+  }
+
+
+  @Test
+  @DisplayName("로또 티켓은, 당첨번호와 일치하는 숫자의 갯수를 반환한다")
+  void countMatchedNumbers() {
+    LottoTicket lottoTicket = new LottoTicket(1, 2, 3, 4, 5, 6);
+    LottoTicketNumberSet winningNumberSet = new LottoTicketNumberSet(
+        List.of(
+            new LottoTicketNumber(1),
+            new LottoTicketNumber(2),
+            new LottoTicketNumber(3),
+            new LottoTicketNumber(4),
+            new LottoTicketNumber(5),
+            new LottoTicketNumber(6)
+        )
+    );
+    assertThat(lottoTicket.countMatchingNumbers(winningNumberSet)).isEqualTo(6);
+  }
 
 
 }
