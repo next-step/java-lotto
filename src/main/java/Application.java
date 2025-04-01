f6c12f7c8c1947e56dbcc37883cf1c3f25f93bf1
JiEung2 <181757@jnu.ac.kr>
2025-04-01 23:47:55 +0900

    fix LottoGame의 메서드 분리로 인한 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,14 @@
 import domain.LottoGame;
 import domain.LottoNumberGenerator;
+import domain.Lottos;
 import utils.InputView;
 
 public class Application {
     public static void main(String[] args) {
         LottoGame lottoGame = new LottoGame(new LottoNumberGenerator());
-        lottoGame.run(InputView.inputPurchaseAmount(), InputView.inputWinningNumbers(), InputView.inputBonusNumber());
+        String amount = InputView.inputPurchaseAmount();
+        int lottoCount = InputView.inputManualLottoCount();
+        Lottos lottos = lottoGame.buyLotto(amount, lottoCount, InputView.inputManualLotto(lottoCount));
+        lottoGame.displayStatistic(amount, InputView.inputWinningNumbers(), InputView.inputBonusNumber(), lottos);
     }
 }
