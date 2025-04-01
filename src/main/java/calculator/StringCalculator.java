062984a969b3f16342148357e6b2331edeff49ab
picosw <tony.ahn@kakaocorp.com>
2025-04-01 09:48:14 +0900

    [step1] 공백 input 예외처리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,13 +3,23 @@ package calculator;
 public class StringCalculator {
 
     public static int calculate(String input) {
+        validateNotBlank(input);
 
         String[] values = input.split(" ");
-        String operation;
+        return calculate(values);
+    }
+
+    private static void validateNotBlank(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("Input cannot be null or empty");
+        }
+    }
+
+    private static int calculate(String[] values) {
         int result = Integer.parseInt(values[0]);
 
         for (int i = 1; i < values.length; i++) {
-            operation = values[i];
+            String operation = values[i];
             i++;
             int number = Integer.parseInt(values[i]);
             result = calculate(result, operation, number);
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,32 +1,43 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
 
     @Test
-    public void testAdd() {
+    public void add() {
         int result = StringCalculator.calculate("1 + 2 + 3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void testSubtract() {
+    public void subtract() {
         int result = StringCalculator.calculate("10 - 2 - 3");
         assertThat(result).isEqualTo(5);
     }
 
     @Test
-    public void testMultiply() {
+    public void multiply() {
         int result = StringCalculator.calculate("2 * 3 * 4");
         assertThat(result).isEqualTo(24);
     }
 
     @Test
-    public void testDivide() {
+    public void divide() {
         int result = StringCalculator.calculate("10 / 2");
         assertThat(result).isEqualTo(5);
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void blankInput(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> StringCalculator.calculate(input));
+    }
+
+
 }
