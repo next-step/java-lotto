02744e1c7c5d568cbd9d52a799c4d154275e863b
JiEung2 <181757@jnu.ac.kr>
2025-04-01 22:59:21 +0900

    feat 수동 로또 번호를 입력받는 기능 추가

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,6 +10,7 @@ public class InputView {
     private static final String WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 볼을 입력해 주세요.";
     private static final String MANUAL_LOTTO_COUNT_INPUT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String MANUAL_LOTTO_INPUT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 
     public String inputExpression() {
         return scanner.nextLine();
@@ -31,6 +34,15 @@ public class InputView {
     public static String inputManualLottoCount() {
         System.out.println(MANUAL_LOTTO_COUNT_INPUT_MESSAGE);
         return scanner.nextLine();
+    }
+
+    public static List<String> inputManualLotto(int count) {
+        System.out.println(MANUAL_LOTTO_INPUT_MESSAGE);
+        List<String> manualLottoNumber = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            manualLottoNumber.add(scanner.nextLine());
+        }
+        return manualLottoNumber;
     }
 
     public static int toInt(String input) {
