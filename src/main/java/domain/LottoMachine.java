992304e7d1e93652984e4af74bdc136dfa4d55e7
JiEung2 <181757@jnu.ac.kr>
2025-04-01 23:47:09 +0900

    refactor Lottos 생성을 LottoMachine 에서 하도록 분리

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -1,8 +1,21 @@
 package domain;
 
+import utils.Parser;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoMachine {
+    public static Lottos generate(int count, LottoNumberGenerator generator, List<String> manualLottoList) {
+        List<List<Integer>> manualLottoNumbers = manualLottoList.stream().map(Parser::fromNumberInput).collect(Collectors.toList());
+        List<Lotto> lottos = manualLottoNumbers.stream().map(Lotto::new).collect(Collectors.toList());
+        lottos.addAll(IntStream.range(0, count)
+                .mapToObj(i -> LottoMachine.create(generator.generate()))
+                .collect(Collectors.toList()));
+        return new Lottos(lottos);
+    }
+
     public static Lotto create(List<Integer> lottoNumber) {
         return new Lotto(lottoNumber);
     }
