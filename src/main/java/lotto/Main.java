55b3e88b2c4f5ce7cffaee63dbc4845e1f1bfe12
minakusi <seoyeon737@gmail.com>
2025-04-01 11:21:55 +0900

    feat: 수동 UI 추가

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,15 +1,29 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoAmount;
 import lotto.domain.LottoGame;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        LottoGame lottoGame = new LottoGame(InputView.inputPurchaseAmount());
-        OutputView.printPurchaseList(lottoGame.getLottos());
+        int money = InputView.inputPurchaseAmount();
+        LottoAmount manualLottoAmount = new LottoAmount(InputView.inputManualLottoAmount());
+
+        List<Lotto> lottos = new ArrayList<>();
+        if (manualLottoAmount.biggerThanZero()) {
+            OutputView.printManualInputHeader();
+            for (int i = 0; manualLottoAmount.isNotEqual(i); ++i) {
+                lottos.add(Lotto.createManually(InputView.inputManualNumbers()));
+            }
+        }
+
+        LottoGame lottoGame = new LottoGame(money, lottos);
+        OutputView.printPurchaseList(manualLottoAmount, lottoGame.getLottos());
 
         List<Integer> winningNumbers = InputView.inputWinningNumbers();
         int bonusNumber = InputView.inputBonusNumber();
