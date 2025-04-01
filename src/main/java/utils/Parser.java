457e3def44c9499c7209f555e5a3853b66be3ab4
JiEung2 <181757@jnu.ac.kr>
2025-04-01 23:45:30 +0900

    refactor 로또 번호 input을 int로 변환하기 위한 기능 재사용을 위해 분리

---
+++ src/main/java/utils/Parser.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+    public static List<Integer> fromNumberInput(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
