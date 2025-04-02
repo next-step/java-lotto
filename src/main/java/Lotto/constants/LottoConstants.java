f1367a34937d24e1b8e1c0ad37d8f14a5b32de62
jlee0505 <hihjlee0505@gmail.com>
2025-04-02 19:59:32 +0900

    refactor: 여러 클래스에서 쓰이는 상수 분리 및 공통 로직 유틸로 분리

---
+++ src/main/java/Lotto/constants/LottoConstants.java
@@ -0,0 +1,7 @@
+package Lotto.constants;
+
+public class LottoConstants {
+    public static final int LOTTO_PICK_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+}
