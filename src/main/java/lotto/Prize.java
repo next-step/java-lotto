fdcff014458f862663e0b2e474c9a0929bf2e8fb
YuGyeong <lyg64522@naver.com>
2025-04-02 15:05:46 +0900

    feat: 로또 2등 추가
    - 로또 2등 추가

--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public enum Prize {
-    ONE(6,2000000000),
-    TWO(5, 1500000),
-    THREE(4, 50000),
-    FOUR(3, 50000),
+    FIRST(6,2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
     NONE(0,0);
 
     private final int count;
