ff53ebf056d5b492941174c2821e452a5bc7ba28
jlee0505 <hihjlee0505@gmail.com>
2025-04-02 15:48:26 +0900

    refactor: Lotto 생성 시 인자로 받는 lottoNumbers 검증 로직 추가

--- src/main/java/Lotto/domain/Lotto.java
+++ src/main/java/Lotto/domain/Lotto.java
@@ -3,10 +3,21 @@ package Lotto.domain;
 import java.util.*;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
     private final List<LottoNumber> lottoNumbers;
 
     public Lotto(List<LottoNumber> lottoNumbers) {
+        validateLottoNumbers(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
+    }
+
+    private void validateLottoNumbers(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 반드시 6개여야 합니다.");
+        }
+        if (new HashSet<>(lottoNumbers).size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
     }
 
     public List<LottoNumber> getLottoNumbers() {
