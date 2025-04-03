c977ad5843205181acbbc88f7f0c975bee1d96a6
박준형 <park.jh@altsoft.kr>
2025-04-03 14:24:56 +0900

    refactor: 리뷰 피드백 적용 - 생성시에 완전한 객체 만들도록

--- src/main/java/lotto/domain/Seller.java
+++ src/main/java/lotto/domain/Seller.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Seller {
     private static final Money PRICE = new Money(1000);
 
-    private final List<String> manualLottos;
+    private final List<Lotto> lottos = new ArrayList<>();
     private PurchaseCount count;
 
     public Seller(int input) {
@@ -15,33 +15,25 @@ public class Seller {
 
     public Seller(int input, List<String> manualLottos) {
         this.count = new PurchaseCount(calculateCount(new Money(input)));
-        this.manualLottos = manualLottos;
+        this.generateManual(manualLottos);
     }
 
     private int calculateCount(Money money) {
         return money.divide(PRICE);
     }
 
-    public List<Lotto> generateLottos() {
-        List<Lotto> lottos = new ArrayList<>();
-
-        generateManual(lottos);
-        generateAuto(lottos);
-
-        return lottos;
-    }
-
-    private void generateManual(List<Lotto> lottos) {
+    private void generateManual(List<String> manualLottos) {
         for (String manualLotto : manualLottos) {
-            lottos.add(new ManualStrategy(manualLotto).generate());
+            this.lottos.add(new ManualStrategy(manualLotto).generate());
             this.count = count.decrease();
         }
     }
 
-    private void generateAuto(List<Lotto> lottos) {
+    public List<Lotto> generateLottos() {
         while (count.isPositive()) {
-            lottos.add(new AutomaticStrategy().generate());
+            this.lottos.add(new AutomaticStrategy().generate());
             this.count = count.decrease();
         }
+        return lottos;
     }
 }
