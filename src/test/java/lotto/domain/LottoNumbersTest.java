d1cf13b5127f7e67a13cfdf755a153a1e5191819
victor.shin <victor.shin@victorshinui-MacBookPro.local>
2025-04-03 19:28:58 +0900

    docs: PR항목들에 대한 TODO정리

--- src/test/java/lotto/domain/LottoNumbersTest.java
+++ src/test/java/lotto/domain/LottoNumbersTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static lotto.domain.LottoRank.MATCH_3;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
