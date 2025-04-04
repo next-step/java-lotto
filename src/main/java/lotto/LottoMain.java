21253ac69eab8df54df79aa9314689faac6a28f6
Jiyoung Won <jiyoung928@gmail.com>
2025-04-05 00:13:29 +0900

    feat : 로또 수동 생성 숫자, 수동 생성 입력 기능 구현

--- src/main/java/lotto/LottoMain.java
+++ src/main/java/lotto/LottoMain.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import lotto.domain.LottoGame;
-import lotto.domain.LottoNumber;
 import lotto.domain.LottoTickets;
 import lotto.strategy.AutoLottoStrategy;
 import lotto.ui.InputView;
@@ -10,10 +9,14 @@ import lotto.ui.ResultView;
 public class LottoMain {
     public static void main(String[] args) {
         int lottoPrice = InputView.inputLottoPrice();
-        LottoTickets tickets = LottoTickets.purchase(lottoPrice, new AutoLottoStrategy());
-        ResultView.viewLottoCount(tickets.getCount());
 
-        LottoGame lottoGame = new LottoGame(tickets);
+        int manualCount = InputView.inputManualLottoCount();
+        LottoTickets manualTickets = InputView.inputManualLottoTickets(manualCount);
+
+        LottoTickets autoTickets = LottoTickets.purchase(lottoPrice, manualCount, new AutoLottoStrategy());
+        ResultView.viewAutoManualLottoCount(manualTickets.getCount(), autoTickets.getCount());
+
+        LottoGame lottoGame = new LottoGame(LottoTickets.merge(manualTickets, autoTickets));
         ResultView.viewLottoTickets(lottoGame);
 
         ResultView.viewWinningStatistics(lottoGame.gameStart(InputView.inputWinningLottoNumbers()));
--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -1,8 +1,11 @@
 package lotto.ui;
 
 import lotto.domain.LottoNumber;
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoTickets;
 import lotto.domain.WinningLottoTicket;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -14,6 +17,22 @@ public class InputView {
     public static int inputLottoPrice() {
         System.out.println("구입금액을 입력해 주세요.");
         return SCANNER.nextInt();
+    }
+
+    public static int inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        return SCANNER.nextInt();
+    }
+
+    public static LottoTickets inputManualLottoTickets(int count) {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        String test = SCANNER.nextLine();
+        LottoTickets lottoTickets = new LottoTickets();
+        for(int i = 0; i < count; i++) {
+            String input = SCANNER.nextLine();
+            lottoTickets.addTicket(new LottoTicket(input));
+        }
+        return lottoTickets;
     }
 
     public static WinningLottoTicket inputWinningLottoNumbers() {
