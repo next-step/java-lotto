cea6c302e92f8e428a259a8e838b4e021ec4a128
liter.1(김성수)/kakao <liter.1@kakaocorp.com>
2025-04-07 20:09:42 +0900

    refactor: Use first-class collection for Lotto.class

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,31 +2,57 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private static final int LOTTO_SIZE = 6;
-    private final List<Integer> numbers;
+  private static final int LOTTO_SIZE = 6;
+  private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+  public Lotto(List<Integer> numbers) {
+    validateSize(numbers);
+    validateDuplicate(numbers);
+    this.numbers = numbers.stream().map(LottoNumber::new).collect(Collectors.toList());
+  }
+
+  public Lotto(List<LottoNumber> numbers, boolean isLottoNumber) {
+    validateSize(numbers);
+    validateDuplicate(numbers);
+    this.numbers = numbers;
+  }
+
+  private void validateSize(List<?> numbers) {
+    if (numbers.size() != LOTTO_SIZE) {
+      throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
     }
+  }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
-        }
-
-        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("로또 번호에 중복이 있습니다.");
-        }
+  private void validateDuplicate(List<?> numbers) {
+    if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+      throw new IllegalArgumentException("로또 번호에 중복이 있습니다.");
     }
+  }
 
-    public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(numbers);
-    }
+  public List<Integer> getNumbers() {
+    return numbers.stream().map(LottoNumber::getNumber).collect(Collectors.toUnmodifiableList());
+  }
 
-    public int countMatchingNumbers(Lotto winningLotto) {
-        return (int) numbers.stream().filter(winningLotto.getNumbers()::contains).count();
-    }
+  public List<LottoNumber> getLottoNumbers() {
+    return Collections.unmodifiableList(numbers);
+  }
+
+  public int countMatchingNumbers(Lotto winningLotto) {
+    return (int) numbers.stream().filter(number -> winningLotto.contains(number)).count();
+  }
+
+  public boolean contains(LottoNumber lottoNumber) {
+    return numbers.contains(lottoNumber);
+  }
+
+  public boolean containsNumber(int number) {
+    return numbers.stream().anyMatch(lottoNumber -> lottoNumber.getNumber() == number);
+  }
+
+  public boolean matchBonus(LottoNumber bonusNumber) {
+    return numbers.contains(bonusNumber);
+  }
 }
