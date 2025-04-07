9cf9e4fe3a59981aaf250c86665d4fd3828fb065
east.bridge <east.bridge@kakaocorp.com>
2025-04-07 17:50:57 +0900

    test: 테스트코드 수정

--- src/test/java/lotto/domain/RewardsTest.java
+++ src/test/java/lotto/domain/RewardsTest.java
@@ -5,64 +5,69 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 public class RewardsTest {
     @Test
     @DisplayName("보상이 없는 객체 생성 불가")
     void create() {
-        Assertions.assertThatThrownBy(() -> new Rewards(List.of()))
+        Assertions.assertThatThrownBy(() -> new Rewards(Map.of()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("1등이 1개 있으면 1리턴")
     void firstPrizeCount() {
-        Rewards rewards = new Rewards(List.of(Reward.FIRST,
-                Reward.SECOND,
-                Reward.SECOND,
-                Reward.THIRD,
-                Reward.FOURTH
-                ));
-        Assertions.assertThat(rewards.firstPrizeCount())
-                .isEqualTo(1);
+        Rewards rewards = new Rewards(Map.of(Reward.FIRST, 1,
+                Reward.SECOND, 2,
+                Reward.THIRD, 1,
+                Reward.FOURTH, 1
+        ));
+        Assertions.assertThat(rewards.prizeCount(Reward.FIRST)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("2등이 2개 있으면 2리턴")
     void secondPrizeCount() {
-        Rewards rewards = new Rewards(List.of(Reward.FIRST,
-                Reward.SECOND,
-                Reward.SECOND,
-                Reward.THIRD,
-                Reward.FOURTH
-                ));
-        Assertions.assertThat(rewards.secondPrizeCount())
-                .isEqualTo(2);
+        Rewards rewards = new Rewards(Map.of(Reward.FIRST, 1,
+                Reward.SECOND, 2,
+                Reward.THIRD, 1,
+                Reward.FOURTH, 1
+        ));
+        Assertions.assertThat(rewards.prizeCount(Reward.SECOND)).isEqualTo(2);
     }
 
     @Test
     @DisplayName("3등이 1개 있으면 1리턴")
     void thirdPrizeCount() {
-        Rewards rewards = new Rewards(List.of(Reward.FIRST,
-                Reward.SECOND,
-                Reward.SECOND,
-                Reward.THIRD,
-                Reward.FOURTH
-                ));
-        Assertions.assertThat(rewards.thirdPrizeCount())
-                .isEqualTo(1);
+        Rewards rewards = new Rewards(Map.of(Reward.FIRST, 1,
+                Reward.SECOND, 2,
+                Reward.THIRD, 1,
+                Reward.FOURTH, 1
+        ));
+        Assertions.assertThat(rewards.prizeCount(Reward.THIRD)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("4등이 1개 있으면 1리턴")
     void fourthPrizeCount() {
-        Rewards rewards = new Rewards(List.of(Reward.FIRST,
-                Reward.SECOND,
-                Reward.SECOND,
-                Reward.THIRD,
-                Reward.FOURTH
-                ));
-        Assertions.assertThat(rewards.fourthPrizeCount())
-                .isEqualTo(1);
+        Rewards rewards = new Rewards(Map.of(Reward.FIRST, 1,
+                Reward.SECOND, 2,
+                Reward.THIRD, 1,
+                Reward.FOURTH, 1
+        ));
+        Assertions.assertThat(rewards.prizeCount(Reward.FOURTH)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("5등이 1개 있으면 1리턴")
+    void fifthPrizeCount() {
+        Rewards rewards = new Rewards(Map.of(Reward.FIRST, 1,
+                Reward.SECOND, 2,
+                Reward.THIRD, 1,
+                Reward.FOURTH, 1,
+                Reward.FIFTH, 1
+        ));
+        Assertions.assertThat(rewards.prizeCount(Reward.FOURTH)).isEqualTo(1);
     }
 }
