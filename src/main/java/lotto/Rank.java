192ef94875f419f9ed2466a70f283b2fd9221eb1
picosw <tony.ahn@kakaocorp.com>
2025-04-08 20:30:48 +0900

    MatchStatus 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -2,35 +2,44 @@ package lotto;
 
 import java.util.Arrays;
 
+import static lotto.MatchStatus.*;
+
 public enum Rank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0);
+    FIRST(6, INCONCLUSIVE, 2_000_000_000),
+    SECOND(5, MATCHED, 30_000_000),
+    THIRD(5, UNMATCHED, 1_500_000),
+    FOURTH(4, INCONCLUSIVE, 50_000),
+    FIFTH(3, INCONCLUSIVE, 5_000),
+    MISS(0, INCONCLUSIVE, 0);
 
     private final long matchCount;
+    private final MatchStatus matchStatus;
     private final int prize;
 
-    Rank(long matchCount, int prize) {
+    Rank(long matchCount, MatchStatus matchStatus, int prize) {
         this.matchCount = matchCount;
+        this.matchStatus = matchStatus;
         this.prize = prize;
     }
 
     public static Rank valueOf(long matchCount, boolean matchBonus) {
-        if (matchCount == 5 && matchBonus) {
-            return SECOND;
-        }
-
-        if (matchCount == 5) {
-            return THIRD;
-        }
-
         return Arrays.stream(values())
-                .filter(rank -> rank.matchCount == matchCount)
+                .filter(rank -> rank.matchCount == matchCount && matchBonus(rank.matchStatus, matchBonus))
                 .findFirst()
                 .orElse(MISS);
+    }
+
+    private static boolean matchBonus(MatchStatus matchStatus, boolean matchBonus) {
+        if (matchStatus == INCONCLUSIVE) {
+            return true;
+        }
+
+        if (matchStatus == MATCHED) {
+            return matchBonus;
+        }
+
+        // UNMATCHED
+        return !matchBonus;
     }
 
     public int getPrize() {
