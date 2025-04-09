0006aca51198cd59946903f07638fdb84722508e
waterdrag0n <sychang0422@gmail.com>
2025-04-09 13:58:13 +0900

    refactor: add controller

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.controller.LottoController;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.ResultView;
@@ -7,6 +8,6 @@ import lotto.view.ResultView;
 public class LottoApplication {
 
   public static void main(String[] args) {
-    new LottoService(new InputView(), new ResultView()).run();
+    new LottoController(new LottoService(), new InputView(), new ResultView()).run();
   }
 }
