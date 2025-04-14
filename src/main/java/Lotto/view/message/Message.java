41d081da1c39aefe02f9e2f3f9bff5f0cd6a9870
jlee0505 <hihjlee0505@gmail.com>
2025-04-14 20:40:21 +0900

    chore: 불필요한 주석 제거

--- src/main/java/Lotto/view/message/Message.java
+++ src/main/java/Lotto/view/message/Message.java
@@ -3,13 +3,6 @@ package Lotto.view.message;
 import java.util.ResourceBundle;
 
 public enum Message {
-//    PAID_MONEY("구입금액을 입력해 주세요."),
-//    WINNING_NUMBERS("지난 주 당첨 번호를 입력해 주세요."),
-//    BONUS_NUMBER("보너스 볼을 입력해 주세요."),
-//    MANUAL_COUNT("수동으로 구매할 로또 수를 입력해 주세요."),
-//    MANUAL_NUMBERS("수동으로 구매할 번호를 입력해 주세요."),
-//    VALID_NUMBER_INPUT("유효한 숫자를 입력해 주세요.");
-
     PAID_MONEY("message.paidMoney"),
     WINNING_NUMBERS("message.winningNumbers"),
     BONUS_NUMBER("message.bonusNumber"),
