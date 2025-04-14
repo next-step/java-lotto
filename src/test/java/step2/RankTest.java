b46c35240e3d3d214efa1d04f84a91adb4a0fbd4
choincnp <whdudwns7777@gmail.com>
2025-04-15 01:45:16 +0900

    refactor : divide validate lotto logic and tests(missing file)

--- src/test/java/step2/RankTest.java
+++ src/test/java/step2/RankTest.java
@@ -3,36 +3,29 @@ package step2;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import step2.domain.Rank;
 
 public class RankTest {
-    public void match(Rank request, Rank actual, Integer expectedPrice) {
-        assertAll(
-            () -> assertEquals(request, actual),
-            () -> assertEquals(request.getPrize(), expectedPrice)
-        );
-    }
 
-    @Test
-    @DisplayName("로또 순위 테스트")
-    public void testRank() {
-        Rank zero = Rank.of(0);
-        Rank one = Rank.of(1);
-        Rank two = Rank.of(2);
-        Rank three = Rank.of(3);
-        Rank four = Rank.of(4);
-        Rank five = Rank.of(5);
-        Rank six = Rank.of(6);
+    @ParameterizedTest
+    @CsvSource({
+        "0, NONE, 0",
+        "1, NONE, 0",
+        "2, NONE, 0",
+        "3, FOURTH, 5000",
+        "4, THIRD, 50000",
+        "5, SECOND, 1500000",
+        "6, FIRST, 2000000000"
+    })
+    @DisplayName("Rank Enum 매칭 테스트")
+    void testRankMatching(int matchCount, Rank expectedRank, int expectedPrize) {
+        Rank rank = Rank.of(matchCount);
         assertAll(
-            () -> match(zero, Rank.NONE, 0),
-            () -> match(one, Rank.NONE, 0),
-            () -> match(two, Rank.NONE, 0),
-            () -> match(three, Rank.FOURTH, 5000),
-            () -> match(four, Rank.THIRD, 50000),
-            () -> match(five, Rank.SECOND, 1500000),
-            () -> match(six, Rank.FIRST, 2000000000)
+            () -> assertEquals(expectedRank, rank),
+            () -> assertEquals(expectedPrize, rank.getPrize())
         );
     }
 
