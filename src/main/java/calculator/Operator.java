16185f2a265910e9d68799b3e0f0550e9b8f9b47
yangseung-in <rhfpdk92@naver.com>
2025-11-16 16:29:38 +0900

    feat: 연산자를 구현한다

---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,39 @@
+package calculator;
+
+public class Operator {
+
+    public static int operator(int operand1, int operand2, String operator) {
+        if (operator.equals("+")) {
+            return add(operand1, operand2);
+        }
+
+        if (operator.equals("-")) {
+            return subtract(operand1, operand2);
+        }
+
+        if (operator.equals("*")) {
+            return multiply(operand1, operand2);
+        }
+
+        if (operator.equals("/")) {
+            return divide(operand1, operand2);
+        }
+        throw new IllegalArgumentException("지원하지 않는 연산자");
+    }
+
+    private static int add(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+
+    private static int subtract(int operand1, int operand2) {
+        return operand1 - operand2;
+    }
+
+    private static int multiply(int operand1, int operand2) {
+        return operand1 * operand2;
+    }
+
+    private static int divide(int operand1, int operand2) {
+        return operand1 / operand2;
+    }
+}
---
+++ src/test/java/calculator/OperatorTest.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OperatorTest {
+
+    @Test
+    public void 덧셈연산을_수행한다() {
+        int result = Operator.operator(2, 3, "+");
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    public void 뺄셈연산을_수행한다() {
+        int result = Operator.operator(3, 2, "-");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void 곱셈연산을_수행한다() {
+        int result = Operator.operator(3, 2, "*");
+        assertThat(result).isEqualTo(6);
+    }
+
+
+    @Test
+    public void 나눗셈연산을_수행한다() {
+        int result = Operator.operator(6, 2, "/");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void 사칙연산_기호가_아닌경우_예외를_발생시킨다() {
+        assertThatThrownBy(() -> Operator.operator(6, 2, "&"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("지원하지 않는 연산자");
+    }
+
+
+
+}
