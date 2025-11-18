4a6bb65cb31e0359b5b4d661a44e4c9b7fd1d223
yangseung-in <rhfpdk92@naver.com>
2025-11-19 01:15:14 +0900

    feat: 로또번호 Generator 분리

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,14 +1,10 @@
 package lotto.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoTickets {
     private static final int LOTTO_PRICE = 1000;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int LOTTO_NUMBER_COUNT = 6;
 
     private final List<Lotto> lottos;
 
@@ -20,25 +16,9 @@ public class LottoTickets {
         int count = purchaseAmount.getAmount() / LOTTO_PRICE;
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            lottos.add(createRandomLotto());
+            lottos.add(Lotto.from(LottoNumberGenerator.generate()));
         }
         return new LottoTickets(lottos);
-    }
-
-    private static Lotto createRandomLotto() {
-        //TODO 많은 일을 하고 있음
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++) {
-            numbers.add(i);
-        }
-        Collections.shuffle(numbers);
-
-        List<Integer> selectedNumbers = new ArrayList<>();
-        for (int i = 0; i < LOTTO_NUMBER_COUNT; i++) {
-            selectedNumbers.add(numbers.get(i));
-        }
-
-        return Lotto.from(selectedNumbers);
     }
 
     public int size() {
