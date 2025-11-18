07830149680e371414347e7930b157923fcf24cd
yangseung-in <rhfpdk92@naver.com>
2025-11-18 23:49:20 +0900

    chore: 패키지 구조 변경

--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 //- [ ] 로또 번호는 총 6개이다
 //- [ ] 로또 번호는 1부터 45 사이의 숫자이다
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,8 +1,6 @@
-package lotto;
+package lotto.domain;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
