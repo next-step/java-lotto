54bff76605f7ee7ff15f03b754d239219ec50b4b
yangseung-in <rhfpdk92@naver.com>
2025-11-20 00:52:47 +0900

    feat: 보너스 번호를 구현한다.

---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final Lotto lotto;
+    private final BonusNumber bonusNumber;
+
+    public WinningNumbers(Lotto lotto, BonusNumber bonusNumber) {
+        validateDuplicate(lotto, bonusNumber);
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateDuplicate(Lotto lotto, BonusNumber bonusNumber) {
+        List<Integer> numbers = lotto.getNumbers();
+        if (numbers.contains(bonusNumber.getValue())) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public BonusNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public boolean containsInLotto(LottoNumber number) {
+        List<Integer> numbers = lotto.getNumbers();
+        return numbers.contains(number.getValue());
+    }
+
+    public boolean matchBonus(LottoNumber number) {
+        return bonusNumber.getValue() == number.getValue();
+    }
+}
---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningNumbersTest {
+
+    @Test
+    void 로또_번호와_보너스_번호로_생성한다() {
+        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        WinningNumbers winningNumbers = new WinningNumbers(lotto, bonusNumber);
+
+        assertThat(winningNumbers.getLotto()).isEqualTo(lotto);
+        assertThat(winningNumbers.getBonusNumber()).isEqualTo(bonusNumber);
+    }
+
+    @Test
+    void 보너스_번호가_당첨_번호와_중복이면_예외를_발생시킨다() {
+        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(6);
+
+        assertThatThrownBy(() -> new WinningNumbers(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+
+    @Test
+    void 특정_번호가_당첨_번호에_포함되는지_확인() {
+        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningNumbers winningNumbers = new WinningNumbers(lotto, bonusNumber);
+
+        assertThat(winningNumbers.containsInLotto(new LottoNumber(1))).isTrue();
+        assertThat(winningNumbers.containsInLotto(new LottoNumber(10))).isFalse();
+    }
+
+    @Test
+    void 특정_번호가_보너스_번호와_일치하는지_확인() {
+        Lotto lotto = new Lotto(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+        WinningNumbers winningNumbers = new WinningNumbers(lotto, bonusNumber);
+
+        assertThat(winningNumbers.matchBonus(new LottoNumber(7))).isTrue();
+        assertThat(winningNumbers.matchBonus(new LottoNumber(1))).isFalse();
+    }
+}
