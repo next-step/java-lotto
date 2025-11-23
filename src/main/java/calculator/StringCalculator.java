e28b689a7f38ecb62b505f1128363baf1100d956
ho-jun97 <hojun9707@naver.com>
2025-11-23 22:23:37 +0900

    feat: add 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringCalculator {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
