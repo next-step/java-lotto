fde7d579ac8041cf5156c4948f01f1bb6d733b82
username0w <163955522+username0w@users.noreply.github.com>
2025-11-23 17:50:56 +0900

    test: 테스트 보완
    - 음수·순차·연산자 예외 테스트 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
 
@@ -12,5 +13,28 @@ public class StringCalculatorTest {
     void calculate() {
         String[] tokens = {"1", "+", "2", "+", "3"};
         assertThat(StringCalculator.calculate(tokens)).isEqualTo(6);
+    }
+
+    @DisplayName("순서대로 연산한다")
+    @Test
+    void multipleOperations() {
+        String[] tokens = {"2", "+", "3", "*", "4"};
+        assertThat(StringCalculator.calculate(tokens)).isEqualTo(20);
+    }
+
+    @DisplayName("음수를 연산한다")
+    @Test
+    void negativeNumbers() {
+        String[] tokens = {"-5", "+", "3"};
+        assertThat(StringCalculator.calculate(tokens)).isEqualTo(-2);
+    }
+
+    @DisplayName("유효하지 않은 연산자를 사용하면 예외가 발생한다")
+    @Test
+    void invalidOperator() {
+        String[] tokens = {"2", "^", "3"};
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringCalculator.calculate(tokens))
+                .withMessageContaining("연산 불가");
     }
 }
