185a326eb44df94cad788bf9900f9ef5f69b29a3
ho-jun97 <hojun9707@naver.com>
2025-11-25 12:13:30 +0900

    feat: 1~45 validation 추가

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -3,7 +3,13 @@ package lotto;
 public class LottoNumber {
     private int number;
     public LottoNumber(int number) {
+        validation(number);
         this.number = number;
+    }
+    private void validation(int number) {
+        if (number < 1 || number > 45) {
+            throw new RuntimeException("로또 번호는 1부터 45입니다.");
+        }
     }
 
     public int value() {
