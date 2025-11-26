e67feb0d4733ead3bb37bb4f22df6504e4133665
qwer920414-ctrl <qwer920414@gmail.com>
2025-11-27 01:05:19 +0900

    feat: LottoChecker 클래스 구현(개선 전)

--- src/main/java/lotto/domain/LottoChecker.java
+++ src/main/java/lotto/domain/LottoChecker.java
@@ -1,2 +1,42 @@
-package lotto.domain;public class LottoChecker {
+package lotto.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoChecker {
+
+    private Map<Integer, Integer> lottoResult = new HashMap<>();
+
+    public LottoChecker() {
+        lottoResult.put(3, 0);
+        lottoResult.put(4, 0);
+        lottoResult.put(5, 0);
+        lottoResult.put(6, 0);
+    }
+
+    public int countMatch(Lotto lotto, Lotto winning) {
+        int count = 0;
+        for (Integer number : lotto.numbers()) {
+            if (winning.numbers().contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Map<Integer, Integer> check(List<Lotto> lottos, Lotto winning) {
+        for (Lotto lotto : lottos) {
+            int count = countMatch(lotto, winning);
+            plusCount(count);
+        }
+        return lottoResult;
+    }
+
+    private void plusCount(int count) {
+        Integer value = lottoResult.get(count);
+        if (value != null) {
+            lottoResult.put(count, value + 1);
+        }
+    }
 }
--- src/test/java/lotto/domain/LottoCheckerTest.java
+++ src/test/java/lotto/domain/LottoCheckerTest.java
@@ -1,4 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LottoCheckerTest {
-  
+
+    @Test
+    @DisplayName("내가 산 복권과 당첨 복권이 3개 일치")
+    void countMatch() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winning = new Lotto(List.of(1, 2, 3, 9, 10, 11));
+        assertThat(new LottoChecker().countMatch(lotto, winning)).isEqualTo(3);
+    }
+
+    @Test
+    void check() {
+        Lotto winning = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(new Lotto(List.of(1, 2, 3, 10, 11, 12)));
+        lottos.add(new Lotto(List.of(1, 2, 11, 12, 13, 14)));
+
+        assertThat(new LottoChecker().check(lottos, winning).get(3)).isEqualTo(1);
+
+    }
 }
