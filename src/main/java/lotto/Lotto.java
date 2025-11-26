c42e32062c1b2d86ab7415ba2378754095d559f0
username0w <163955522+username0w@users.noreply.github.com>
2025-11-26 12:05:03 +0900

    feat: Lotto 클래스 추가

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import java.util.List;
+
+public class Lotto {
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTest {
+
+    @DisplayName("로또를 생성한다")
+    @Test
+    void createLotto() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto).isNotNull();
+    }
+}
