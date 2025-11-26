76c769679369f75cf4a2a3dab53f213e035de33f
username0w <163955522+username0w@users.noreply.github.com>
2025-11-26 12:10:32 +0900

    feat: 로또 숫자 개수 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,6 +7,14 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validateNumbersCount(numbers);
         this.numbers = numbers;
     }
+
+    private static void validateNumbersCount(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 한다");
+        }
+    }
+
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,10 +2,14 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LottoTest {
 
@@ -15,4 +19,22 @@ public class LottoTest {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThat(lotto).isNotNull();
     }
+
+    @DisplayName("로또 번호는 6개여야 한다")
+    @ParameterizedTest
+    @MethodSource("invalidLottoSizes")
+    void validLottoSize(List<Integer> numbers) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Lotto(numbers))
+                .withMessageContaining("6개");
+    }
+
+    static Stream<List<Integer>> invalidLottoSizes() {
+        return Stream.of(
+                List.of(),
+                List.of(1, 2, 3, 4, 5),
+                List.of(1, 2, 3, 4, 5, 6, 7)
+        );
+    }
+
 }
