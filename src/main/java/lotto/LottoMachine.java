249fb31e221d184574ccd1db7604d83ef5ee7d83
username0w <163955522+username0w@users.noreply.github.com>
2025-11-26 16:47:02 +0900

    refactor: LottoResult 클래스 추가에 따른 LottoMachine 리팩터링

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,8 +1,6 @@
 package lotto;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LottoMachine {
 
@@ -15,34 +13,8 @@ public class LottoMachine {
         return LottoGenerator.generateLottos(money / PRICE_PER_LOTTO);
     }
 
-    public static int countMatches(Lotto lotto, Lotto winningNumbers) {
-        return lotto.countMatchingNumbers(winningNumbers);
-    }
-
-    public static Map<Integer, Integer> calculateResult(List<Lotto> lottos, Lotto winningNumbers) {
-        Map<Integer, Integer> result = new HashMap<>();
-        for (Lotto lotto : lottos) {
-            int match = countMatches(lotto, winningNumbers);
-            result.put(match, result.getOrDefault(match, 0) + 1);
-        }
-        return result;
-    }
-
-    public static long calculateTotalPrize(Map<Integer, Integer> result) {
-        Map<Integer, Integer> rank = Map.of(
-                6, 2_000_000_000,
-                5, 1_500_000,
-                4, 50_000,
-                3, 5_000);
-        long totalPrize = 0;
-        for (int i = 3; i <= 6; i++) {
-            totalPrize += (long) result.getOrDefault(i, 0) * rank.get(i);
-        }
-        return totalPrize;
-    }
-
-    public static double calculateProfitRate(long totalPrize, int money) {
-        return (double) totalPrize / money;
+    public static LottoResult generateResult(List<Lotto> lottos, Lotto winningNumbers, int money) {
+        return new LottoResult(lottos, winningNumbers, money);
     }
 
     private static void validateMoney(int money) {
--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -3,11 +3,9 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -30,42 +28,4 @@ public class LottoMachineTest {
                 .withMessageContaining("1000원 단위");
     }
 
-    @DisplayName("당첨 통계를 계산한다")
-    @ParameterizedTest
-    @CsvSource({
-            "6, 1",
-            "5, 1",
-            "0, 1",
-            "4, 0",
-            "3, 0",
-            "2, 0",
-            "1, 0",
-    })
-    void lottoResult(int matchCount, int expectedCount) {
-        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new Lotto(List.of(1, 2, 3, 4, 5, 7)), new Lotto(List.of(7, 8, 9, 10, 11, 12)));
-        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Map<Integer, Integer> result = LottoMachine.calculateResult(lottos, winningNumbers);
-        assertThat(result).hasSize(lottos.size());
-        assertThat(result.getOrDefault(matchCount, 0)).isEqualTo(expectedCount);
-    }
-
-    @DisplayName("총 당첨금을 계산한다")
-    @Test
-    void calculateTotalPrize() {
-        Map<Integer, Integer> result = Map.of(
-                6, 1,
-                5, 1,
-                3, 2
-        );
-        long totalPrize = LottoMachine.calculateTotalPrize(result);
-        assertThat(totalPrize).isEqualTo(2_000_000_000 + 1_500_000 + 2 * 5_000);
-    }
-
-    @DisplayName("당첨금 수익률을 계산한다")
-    @Test
-    void calculateProfitRate() {
-        long totalPrize = 15_000;
-        int money = 100_000;
-        assertThat(LottoMachine.calculateProfitRate(totalPrize, money)).isEqualTo(0.15);
-    }
 }
