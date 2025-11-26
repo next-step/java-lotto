220311b7b952c8ec05b3d999ec5846ef1c01f3b5
qwer920414-ctrl <qwer920414@gmail.com>
2025-11-27 01:10:06 +0900

    feat: InputView 클래스 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static long initLottoPrice() {
+        System.out.println("구매금액을 입력해 주세요.");
+        return SCANNER.nextLong();
+    }
+
+    public static String initWinningLotto() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return SCANNER.nextLine();
+    }
+}
