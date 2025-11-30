b97a1a84865e0864e4b133f42a3b6c85108fe915
qwer920414-ctrl <qwer920414@gmail.com>
2025-11-30 14:03:52 +0900

    feat: Lotto 클래스 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    public static final int LOTTO_NUMBER_SIZE = 6;
+    private final List<Integer> numbers;
+    public Lotto(Integer... numbers) {
+        this(Arrays.asList(numbers));
+    }
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = List.copyOf(numbers);
+    }
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+
+        Set<Integer> noDuplicateNumbers = new HashSet<>(numbers);
+        if (noDuplicateNumbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class LottoTest {
+    @Test
+    @DisplayName("로또번호가 6개만 허용된다")
+    void lotto_generate() {
+        assertThatThrownBy(() -> new Lotto(1, 2, 3, 4, 5)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(1, 2, 3, 4, 5, 6, 7)).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또번호는 중복되면 안된다")
+    void lotto_duplicate() {
+        assertThatThrownBy(() -> new Lotto(1, 2, 2, 3, 4, 5)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
