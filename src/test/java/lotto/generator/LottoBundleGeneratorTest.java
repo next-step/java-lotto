cafe6b9c83356adacb53cfe63a5b3ae18bbff89f
pobi <pobi@pobi.local>
2025-12-01 16:45:56 +0900

    LottoBundleGenerator에 대한 테스트 추가

---
+++ src/test/java/lotto/generator/LottoBundleGeneratorTest.java
@@ -0,0 +1,27 @@
+package lotto.generator;
+
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoBundleGeneratorTest {
+
+    @Test
+    void generate() {
+        Money money = new Money(5_000);
+        List<String> values = List.of(
+                "1,2,3,4,5,6",
+                "2,3,4,5,6,7",
+                "3,4,5,6,7,8"
+        );
+        LottoBundleGenerator lottoBundleGenerator = new LottoBundleGenerator(money, values);
+        Lottos lottos = lottoBundleGenerator.generate();
+        assertThat(lottos.size()).isEqualTo(money.countOfBuyingLotto());
+        System.out.println(lottos);
+    }
+}
