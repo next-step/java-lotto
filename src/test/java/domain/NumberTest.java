3cfd68d76dc0fc139c8ce3af8671658084dd62e5
이국신 <nw4611@naver.com>
2025-12-02 21:36:48 +0900

    [feat] Multiplication 구현

--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -9,6 +9,7 @@ public class NumberTest {
     public void 숫자_생성() {
         assertThat(new Number("1")).isEqualTo(new Number(1));
     }
+
     @Test
     public void 덧셈() {
         Number first = new Number("1");
@@ -22,9 +23,17 @@ public class NumberTest {
     public void 뺄셈() {
         Number first = new Number("3");
         Number second = new Number("2");
-
         Operator operator = new Substraction();
 
         assertThat(first.calculate(operator, second)).isEqualTo(new Number(1));
+    }
+
+    @Test
+    public void 곱셈() {
+        Number first = new Number("3");
+        Number second = new Number("2");
+
+        Operator operator = new Multiplication();
+        assertThat(first.calculate(operator, second)).isEqualTo(new Number(6));
     }
 }
