08a845b95d5b9220ba84075d51e8c17aec680e81
이국신 <nw4611@naver.com>
2025-12-02 21:29:20 +0900

    [feat] Substraction 구현

--- src/test/java/domain/NumberTest.java
+++ src/test/java/domain/NumberTest.java
@@ -17,4 +17,14 @@ public class NumberTest {
 
         assertThat(first.calculate(operator, second)).isEqualTo(new Number(3));
     }
+
+    @Test
+    public void 뺄셈() {
+        Number first = new Number("3");
+        Number second = new Number("2");
+
+        Operator operator = new Substraction();
+
+        assertThat(first.calculate(operator, second)).isEqualTo(new Number(1));
+    }
 }
