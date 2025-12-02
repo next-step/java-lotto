e8d778cf246d50e24a8b952c74c85948429e562e
이국신 <nw4611@naver.com>
2025-12-02 21:53:41 +0900

    [feat] 연산자 팩토리 구현

---
+++ src/test/java/domain/OperatorFactoryTest.java
@@ -0,0 +1,27 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OperatorFactoryTest {
+    @Test
+    public void 덧셈_객체_생성() {
+        assertThat(OperatorFactory.getOperator("+")).isInstanceOf(Plus.class);
+    }
+
+    @Test
+    public void 뺄셈_객체_생성() {
+        assertThat(OperatorFactory.getOperator("-")).isInstanceOf(Substraction.class);
+    }
+
+    @Test
+    public void 곰셉_객체_생성() {
+        assertThat(OperatorFactory.getOperator("*")).isInstanceOf(Multiplication.class);
+    }
+
+    @Test
+    public void 나눗셈_객체_생성() {
+        assertThat(OperatorFactory.getOperator("/")).isInstanceOf(Division.class);
+    }
+}
