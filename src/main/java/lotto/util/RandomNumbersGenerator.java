940e243f188f24f3283e6f5b5d99e942e19dd382
이국신 <nw4611@naver.com>
2025-12-05 12:06:22 +0900

    [feat] 랜덤 숫자들을 뽑아주는 유틸 구현

---
+++ src/main/java/lotto/util/RandomNumbersGenerator.java
@@ -0,0 +1,26 @@
+package lotto.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomNumbersGenerator {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static List<Integer> numbers = new ArrayList<>();
+
+    static {
+        for (int i = MIN_LOTTO_NUMBER; i <= MAX_LOTTO_NUMBER; i++ ) {
+            numbers.add(i);
+        }
+    }
+
+    public static List<Integer> randomNumbers() {
+        Collections.shuffle(numbers);
+        List<Integer> subList = numbers.subList(0, LOTTO_NUMBER_COUNT);
+        Collections.sort(subList);
+
+        return subList;
+    }
+}
