5292b547f63382811b68c4db47d46cbed8b22957
이국신 <nw4611@naver.com>
2025-12-05 12:06:39 +0900

    [feat] 로또 머신 구현

---
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -0,0 +1,31 @@
+package lotto.controller;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumbers;
+import lotto.domain.LottoPrice;
+import lotto.util.RandomNumbersGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private LottoPrice lottoPrice;
+
+    public LottoMachine(int price) {
+        this(new LottoPrice(price));
+    }
+
+    public LottoMachine(LottoPrice lottoPrice) {
+        this.lottoPrice = lottoPrice;
+    }
+
+    public Lotto generate() {
+        List<LottoNumbers> lottoNumbersList = new ArrayList<>();
+
+        for (int i = 0; i < lottoPrice.count(); i++) {
+            lottoNumbersList.add(new LottoNumbers(RandomNumbersGenerator.randomNumbers()));
+        }
+
+        return new Lotto(lottoPrice, lottoNumbersList);
+    }
+}
