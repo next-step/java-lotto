d92adbcc159d2f97fdccd39611c53dc28098f0b2
qwer920414-ctrl <qwer920414@gmail.com>
2025-12-07 08:17:38 +0900

    refactor: 불필요 LottoGame 필드 삭제

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -4,19 +4,17 @@ import java.util.stream.Collectors;
 
 public class LottoGame {
     private final Lottos lottos;
-    private final Money money;
 
     public LottoGame(long amount) {
         this(new Money(amount));
     }
 
     public LottoGame(Money money) {
-        this(new Lottos(money.buyCount()), money);
+        this(new Lottos(money.buyCount()));
     }
 
-    public LottoGame(Lottos lottos, Money money) {
+    public LottoGame(Lottos lottos) {
         this.lottos = lottos;
-        this.money = money;
     }
 
     public LottoResult findWinner(WinningLotto winningLotto) {
