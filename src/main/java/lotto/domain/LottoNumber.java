21c1eaf0826cf2699441596add9ad82463baa455
이국신 <nw4611@naver.com>
2025-12-07 21:54:45 +0900

    [feat] toString 추가

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -20,4 +20,9 @@ public class LottoNumber {
     public int hashCode() {
         return Objects.hashCode(number);
     }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
 }
