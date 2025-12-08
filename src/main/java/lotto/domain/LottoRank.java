2f009a12a0d1799df8776cdaf4f11f6dcfb8c0fe
이국신 <nw4611@naver.com>
2025-12-08 21:59:26 +0900

    [feat] 2등 보너스 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -2,9 +2,10 @@ package lotto.domain;
 
 public enum LottoRank {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(4, 1_500_000),
-    FOURTH(3, 50_000),
+    SECOND_BONUS(5, 30_000_000),
+    SECOND(5, 1_500_000),
+    THIRD(4, 50_000),
+    FOURTH(3, 5_000),
     NONE(0, 0);
 
 
