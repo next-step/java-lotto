db48a73211109a3ab0a9086c2ad9b34b2ec60843
이국신 <nw4611@naver.com>
2025-12-16 08:14:40 +0900

    [refactor] LottoCount 리팩토링

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -3,37 +3,26 @@ package lotto.domain;
 import java.util.Objects;
 
 public class LottoCount {
-    private static final int PRICE_PER_LOTTO = 1000;
-    private final int totalCount;
-    private final int manualCount;
+    private int count;
 
 
-    public LottoCount(String purchaseAmount, String manualCount) {
-        this(Integer.parseInt(purchaseAmount), Integer.parseInt(manualCount));
+    public LottoCount(String count) {
+        this(Integer.parseInt(count));
     }
 
-    public LottoCount(int purchaseAmount, int manualCount) {
-        if (!validateLottoCount(purchaseAmount / PRICE_PER_LOTTO, manualCount)) {
-            throw new RuntimeException("유효하지 않은 로또 개수입니다.");
-        }
-
-        this.totalCount = purchaseAmount /  PRICE_PER_LOTTO;
-        this.manualCount = manualCount;
-    }
-
-    private static boolean validateLottoCount(int totalCount, int manualCount) {
-        return totalCount >= manualCount;
+    public LottoCount(int count) {
+       this.count = count;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         LottoCount that = (LottoCount) o;
-        return totalCount == that.totalCount && manualCount == that.manualCount;
+        return count == that.count;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(totalCount, manualCount);
+        return Objects.hashCode(count);
     }
 }
