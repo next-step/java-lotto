2a09ff9258b87bf499d43ac57832ffead2b7dfda
이국신 <nw4611@naver.com>
2025-12-19 08:38:36 +0900

    [feat] 수동 로또 로직 추가

--- src/main/java/lotto/controller/LottoMachine.java
+++ src/main/java/lotto/controller/LottoMachine.java
@@ -3,7 +3,6 @@ package lotto.controller;
 import lotto.domain.Lotto;
 import lotto.domain.LottoCount;
 import lotto.domain.LottoNumbers;
-import lotto.domain.LottoPrice;
 import lotto.util.LottoNumberParser;
 import lotto.util.RandomNumbersGenerator;
 
@@ -12,30 +11,24 @@ import java.util.List;
 
 public class LottoMachine {
     private static final int PRICE_PER_LOTTO = 1000;
-    private final LottoPrice lottoPrice;
     private LottoCount totalCount;
     private LottoCount manualCount;
     private LottoCount inputCount;
-    private List<LottoNumbers> manualLottoNumbers;
-
-    public LottoMachine(String price) {
-        this(new LottoPrice(price), new LottoCount(price, PRICE_PER_LOTTO), new LottoCount(0));
-    }
+    private List<LottoNumbers> lottoNumbersList;
 
     public LottoMachine(String price , String manualCount) {
-        this(new LottoPrice(price), new LottoCount(price, PRICE_PER_LOTTO), new LottoCount(manualCount));
+        this(new LottoCount(price, PRICE_PER_LOTTO), new LottoCount(manualCount));
     }
 
-    public LottoMachine(LottoPrice lottoPrice, LottoCount totalCount,  LottoCount manualCount) {
+    public LottoMachine(LottoCount totalCount,  LottoCount manualCount) {
         if (!validateLottoCount(totalCount, manualCount)) {
             throw new RuntimeException("유효하지 않은 입력입니다");
         }
 
-        this.lottoPrice = lottoPrice;
         this.totalCount = totalCount;
         this.manualCount = manualCount;
-        this.inputCount = new  LottoCount(0);
-        this.manualLottoNumbers = new ArrayList<>();
+        this.inputCount = new LottoCount(0);
+        this.lottoNumbersList = new ArrayList<>();
     }
 
     public boolean isManualNumberInputEnd() {
@@ -44,17 +37,17 @@ public class LottoMachine {
 
     public void getManualLottoNumbers(String lottoNumbers) {
         this.inputCount = this.inputCount.increase();
-        manualLottoNumbers.add(new LottoNumbers(lottoNumbers));
+        lottoNumbersList.add(new LottoNumbers(lottoNumbers));
     }
 
     public Lotto generate() {
-        List<LottoNumbers> lottoNumbersList = new ArrayList<>();
+        LottoCount autoCount = this.totalCount.diff(this.manualCount);
 
-        for (int i = 0; i < lottoPrice.count(); i++) {
-            lottoNumbersList.add(new LottoNumbers(LottoNumberParser.parse(RandomNumbersGenerator.randomNumbers())));
+        for (int i = 0; !autoCount.isCountSame(i); i++) {
+            this.lottoNumbersList.add(new LottoNumbers(LottoNumberParser.parse(RandomNumbersGenerator.randomNumbers())));
         }
 
-        return new Lotto(lottoPrice, lottoNumbersList);
+        return new Lotto(totalCount, manualCount, this.lottoNumbersList);
     }
 
     private static boolean validateLottoCount(LottoCount total, LottoCount manual) {
