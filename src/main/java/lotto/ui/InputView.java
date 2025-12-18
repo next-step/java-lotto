86081b3ce65fc7c7eb4f55eadf8cb5c15e46b33a
이국신 <nw4611@naver.com>
2025-12-19 08:03:27 +0900

    [feat] 수동숫자 입력 UI 구현

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -6,8 +6,9 @@ public class InputView {
     public static final String BONUS_BALL_MESSAGE = "보너스볼을 입력해주세요";
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호 6개를 입력해 주세요.";
-    private static final Scanner scanner = new Scanner(System.in);
     public static final String MANUAL_NUMBER_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해주세요.";
+    public static final String MANUAL_NUMBERS_INPUT_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String getPurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
@@ -24,8 +25,17 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public static String getManualNumber() {
+    public static String getManualNumberCount() {
         System.out.println(MANUAL_NUMBER_COUNT_MESSAGE);
         return scanner.nextLine();
     }
+
+    public static void printStartManualNumbersInput() {
+        System.out.println(MANUAL_NUMBERS_INPUT_MESSAGE);
+    }
+
+    public static String getManualNumbers() {
+        return scanner.nextLine();
+    }
+
 }
