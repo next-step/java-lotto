e8a9c672f1b92c9d1aa2e1356b64c821f83e574c
이국신 <nw4611@naver.com>
2025-12-23 00:03:21 +0900

    [refactor] 가격에서 로또 개수 차이를 구하는 함수 구현

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import java.util.Objects;
 
 public class LottoCount {
-    private static final int PRICE_PER_LOTTO = 1000;
     private final int count;
 
     public LottoCount(String count) {
@@ -22,29 +21,8 @@ public class LottoCount {
         return this.count == count;
     }
 
-    private static int calculateLottoCount(int purchaseAmount) {
-        if (purchaseAmount < PRICE_PER_LOTTO) {
-            throw new RuntimeException("로또를 구매할 수 없는 가격입니다.");
-        }
-
-        return purchaseAmount / PRICE_PER_LOTTO;
-    }
-
-    public static LottoCount getAutoCount(String purchaseAmountInput, String manualCountInput) {
-        return getAutoCount(Integer.parseInt(purchaseAmountInput), Integer.parseInt(manualCountInput));
-    }
-
-    private static LottoCount getAutoCount(int purchaseAmount, int manualCount) {
-        int totalLottoCount = calculateLottoCount(purchaseAmount);
-        if (!validateLottoCount(totalLottoCount, manualCount)) {
-            throw new RuntimeException("수동 개수가 전체 로또 개수보다 많습니다.");
-        }
-
-        return new LottoCount(totalLottoCount - manualCount);
-    }
-
-    private static boolean validateLottoCount(int totalCount, int manualCount) {
-        return totalCount >= manualCount;
+    public LottoCount diffFromPrice(LottoPrice price) {
+        return new LottoCount(price.count() - count);
     }
 
     @Override
